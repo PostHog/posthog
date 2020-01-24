4994709403e7bc4ae60e2b88e8a9a96568f4e174
Tim Glaser <tim.glaser@hiberly.com>
2020-01-23 17:50:53 -0800

    fix cors

--- posthog/settings.py
+++ posthog/settings.py
@@ -60,14 +60,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posthog.apps.PostHogConfig',
-    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
