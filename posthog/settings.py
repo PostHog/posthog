1edee15609fd6e78fa35a10321661f12f06f1179
Tim Glaser <tim.glaser@hiberly.com>
2020-01-24 16:22:12 -0800

    Fix settings

--- posthog/settings.py
+++ posthog/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/dist')
