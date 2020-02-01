ae150bf56efe6407d468c817a44d1f32af067e80
Tim Glaser <tim.glaser@hiberly.com>
2020-02-01 15:53:57 -0800

    Fix cors headers

--- posthog/settings.py
+++ posthog/settings.py
@@ -62,11 +62,13 @@ INSTALLED_APPS = [
     'posthog.apps.PostHogConfig',
     'rest_framework',
     'loginas',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
