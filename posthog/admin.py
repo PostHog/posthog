be12f6c13c817d3c68e4438f6af9fce5b6ae101f
Tim Glaser <tim.glaser@hiberly.com>
2020-02-01 16:46:23 -0800

    Fix endpoints being open to world

--- posthog/admin.py
+++ posthog/admin.py
@@ -31,6 +31,7 @@ class UserAdmin(DjangoUserAdmin):
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('PostHog'), {'fields': ('temporary_token', )}),
     )
     add_fieldsets = (
         (None, {
--- posthog/settings.py
+++ posthog/settings.py
@@ -163,5 +163,8 @@ APPEND_SLASH = False
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
