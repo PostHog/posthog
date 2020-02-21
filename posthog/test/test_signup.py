9e37dc7b25bb911d4590894db5428bc0e388bc56
Tim Glaser <tim.glaser@hiberly.com>
2020-02-21 15:25:17 -0800

    Closes #160 preloaded dashboards and actions

---
+++ posthog/test/test_signup.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from posthog.models import User, Team, DashboardItem, Action
+from django.test import Client
+
+class TestSignup(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.client = Client()
+     
+    def test_signup_new_team(self):
+        with self.settings(TEST=False):
+            response = self.client.post('/setup_admin', {'company_name': 'ACME Inc.', 'name': 'Jane', 'email': 'jane@acme.com', 'password': 'hunter2'}, follow=True)
+        self.assertRedirects(response, '/')
+
+        user = User.objects.get() 
+        self.assertEqual(user.first_name, 'Jane')
+        self.assertEqual(user.email, 'jane@acme.com')
+
+        team = user.team_set.get()
+        self.assertEqual(team.name, 'ACME Inc.')
+        self.assertEqual(team.users.all()[0], user)
+
+        action = Action.objects.get(team=team)
+        self.assertEqual(action.name, 'Pageviews')
+        self.assertEqual(action.steps.all()[0].event, '$pageview')
+
+        items = DashboardItem.objects.filter(team=team)
+        self.assertEqual(items[0].name, 'Pageviews this week')
+        self.assertEqual(items[0].type, 'ActionsLineGraph')
+        self.assertEqual(items[0].filters['actions'], [action.pk])
+
+        self.assertEqual(items[1].filters['actions'], [action.pk])
+        self.assertEqual(items[1].type, 'ActionsTable')
+
+        self.assertEqual(items[2].name, 'All actions')
