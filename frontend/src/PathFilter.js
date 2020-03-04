e965848be097c1b7ae48e99f4027e13a6f1b3950
Marius Andra <marius.andra@gmail.com>
2020-03-04 15:21:50 +0100

    css for label

--- frontend/src/PathFilter.js
+++ frontend/src/PathFilter.js
@@ -5,11 +5,11 @@ export default function PathFilter ({ filter: { startDate, endDate }, updateFilt
   return (
     <div className='row' style={{margin: '1rem -15px'}}>
       <div className='col-3'>
-        <label>Start Date</label>
+        <label class='control-label' style={{ display: 'block' }}>Start Date</label>
         <DatePicker className='form-control' selected={startDate} onChange={date => updateFilter({ startDate: date })} />
       </div>
       <div className='col-3'>
-        <label>End Date</label>
+        <label class='control-label' style={{ display: 'block' }}>End Date</label>
         <DatePicker className='form-control' selected={endDate} onChange={date => updateFilter({ endDate: date })} />
       </div>
     </div>
