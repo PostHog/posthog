39182b92738f04ea4b4c77b6678d5a7977d0afa6
Marius Andra <marius.andra@gmail.com>
2020-03-10 23:49:04 +0100

    refactor App.js to use kea and functional react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { kea, useActions, useValues } from 'kea'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "react-datepicker/dist/react-datepicker.css";
@@ -22,88 +23,95 @@ import SendEventsOverlay from "./SendEventsOverlay";
 import Paths from "./Paths";
 import Cohorts from "./Cohorts";
 
-class PrivateRoute extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+export const appLogic = kea({
+    actions: () => ({
+        loadUser: true,
+        setUser: user => ({ user })
+    }),
 
-    render() {
-        let Component = this.props.component;
-        return this.props.user ? (
+    reducers: ({ actions }) => ({
+        user: [null, {
+            [actions.setUser]: (_, payload) => payload.user
+        }]
+    }),
+
+    events: ({ actions }) => ({
+       afterMount: actions.loadUser
+    }),
+
+    listeners: ({ actions }) => ({
+        [actions.loadUser]: async () => {
+            try {
+                const user = await api.get('api/user')
+                actions.setUser(user)
+
+                if (user && user.id) {
+                    window.Sentry && window.Sentry.setUser({ email: user.email, id: user.id });
+                    window.posthog && window.posthog.identify(user.distinct_id);
+                }
+            } catch (error) {
+                actions.setUser(null)
+            }
+        }
+    })
+})
+
+function PrivateRoute ({ component: Component, ...props }) {
+    return (
         <Route
-            path={this.props.path}
+            path={props.path}
             exact
-            render={props => {
-                if(window.posthog) posthog.capture('$pageview');
-                if(this.props.user) return (<div>
-                    <Component {...this.props} {...props} user={this.props.user} history={props.history} />
-                </div>);
-                return <Redirect
-                    to={{
-                        pathname: "/login",
-                        state: { from: props.location }
-                    }}
-                    />
+            render={routeProps => {
+                window.posthog && window.posthog.capture('$pageview')
+                return <Component {...props} {...routeProps} />
             }}
         />
-        ) : null;
-    }
+    )
 }
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props)
+export default function App () {
+    const { user } = useValues(appLogic)
+    const { setUser } = useActions(appLogic)
 
-        this.state = {};
-        this.getUser.call(this);
-    }
-    getUser() {
-        api.get('api/user').then((user) => {
-            this.setState({user: user});
-            if(user && user.id) {
-                if(window.Sentry) Sentry.setUser({"email": user.email, "id": user.id});
-                if(window.posthog) posthog.identify(user.distinct_id);
-            }
-        }).catch(() => this.setState({user: false}));
+    if (!user) {
+        return null
     }
 
-    render() {
-        return this.state.user != null && (
-            <Router>
-                <div className="container-fluid flex-grow-1 d-flex">
-                    <div className="row flex-fill flex-column flex-sm-row">
-                        <Sidebar user={this.state.user} />
-                        <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 flex-grow-1 py-3 content">
-                            <Topcontent user={this.state.user} />
-                            <div style={{marginTop: '3rem'}}>
-                                <SendEventsOverlay user={this.state.user} />
-                                {this.state.user.has_events && <div>
-                                    <PrivateRoute path="/" exact component={Dashboard} user={this.state.user} />
-                                    <PrivateRoute path="/actions" exact component={Actions} user={this.state.user} onUpdateUser={(user) => this.setState({user})} />
-                                    <PrivateRoute path="/trends" exact component={ActionsGraph} user={this.state.user} />
-                                    <PrivateRoute path="/actions/live" component={ActionEvents} user={this.state.user} />
-                                    <PrivateRoute path="/funnel" exact component={Funnels} user={this.state.user} />
-                                    <PrivateRoute path="/paths" component={Paths} user={this.state.user} />
-                                </div>}
-                                <PrivateRoute path="/setup" component={Setup} user={this.state.user} onUpdateUser={(user) => this.setState({user})} />
-                                <PrivateRoute path="/events" component={Events} user={this.state.user} />
-                                <PrivateRoute exact path="/person_by_id/:id" component={Person} user={this.state.user} />
-                                <PrivateRoute exact path="/person/:distinct_id" component={Person} user={this.state.user} />
-                                <PrivateRoute path="/people" component={People} user={this.state.user} />
-                                <PrivateRoute path="/people/cohorts" component={Cohorts} user={this.state.user} />
-                                <PrivateRoute path="/action/:id" component={Action} user={this.state.user} />
-                                <PrivateRoute path="/action" component={Action} user={this.state.user} />
-                                <PrivateRoute path="/new-funnel" component={EditFunnel} user={this.state.user} />
-                                <PrivateRoute path="/funnel/:id" exact component={Funnel} user={this.state.user} />
-                            </div>
+    return (
+        <Router>
+            <div className="container-fluid flex-grow-1 d-flex">
+                <div className="row flex-fill flex-column flex-sm-row">
+                    <Sidebar user={user} />
+                    <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 flex-grow-1 py-3 content">
+                        <Topcontent user={user} />
+                        <div style={{marginTop: '3rem'}}>
+                            <SendEventsOverlay user={user} />
+                            {user.has_events && <div>
+                                <PrivateRoute path="/" exact component={Dashboard} user={user} />
+                                <PrivateRoute path="/actions" exact component={Actions} user={user} onUpdateUser={user => setUser({ ...user })} />
+                                <PrivateRoute path="/trends" exact component={ActionsGraph} user={user} />
+                                <PrivateRoute path="/actions/live" component={ActionEvents} user={user} />
+                                <PrivateRoute path="/funnel" exact component={Funnels} user={user} />
+                                <PrivateRoute path="/paths" component={Paths} user={user} />
+                            </div>}
+                            <PrivateRoute path="/setup" component={Setup} user={user} onUpdateUser={user => setUser({ ...user })} />
+                            <PrivateRoute path="/events" component={Events} user={user} />
+                            <PrivateRoute exact path="/person_by_id/:id" component={Person} user={user} />
+                            <PrivateRoute exact path="/person/:distinct_id" component={Person} user={user} />
+                            <PrivateRoute path="/people" component={People} user={user} />
+                            <PrivateRoute path="/people/cohorts" component={Cohorts} user={user} />
+                            <PrivateRoute path="/action/:id" component={Action} user={user} />
+                            <PrivateRoute path="/action" component={Action} user={user} />
+                            <PrivateRoute path="/new-funnel" component={EditFunnel} user={user} />
+                            <PrivateRoute path="/funnel/:id" exact component={Funnel} user={user} />
                         </div>
-                        <ToastContainer
-                            autoClose={8000}
-                            transition={Slide}
-                            position='bottom-center' />
                     </div>
+                    <ToastContainer
+                        autoClose={8000}
+                        transition={Slide}
+                        position='bottom-center' />
                 </div>
-            </Router>
-        );
-    }
+            </div>
+        </Router>
+    )
 }
