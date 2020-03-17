5141475ae9be5a5e3a50d014d97e5d6dd0d7b2c9
Tim Glaser <tim.glaser@hiberly.com>
2020-03-17 19:43:48 +0000

    Use properties table in events

---
+++ frontend/src/lib/components/PropertiesTable.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export function PropertiesTable({ properties }) {
+    if (Array.isArray(properties))
+        return (
+            <div>
+                {properties.map(item => (
+                    <span>
+                        <PropertiesTable properties={item} />
+                        <br />
+                    </span>
+                ))}
+            </div>
+        )
+    if (properties instanceof Object)
+        return (
+            <table className="table">
+                <tbody>
+                    {Object.keys(properties)
+                        .sort()
+                        .map(key => (
+                            <tr>
+                                <th>{key}</th>
+                                <td>
+                                    <PropertiesTable
+                                        properties={properties[key]}
+                                    />
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        )
+    if (properties === true) return 'true'
+    if (properties === false) return 'false'
+    return properties
+}
+PropertiesTable.propTypes = {
+    properties: PropTypes.any.isRequired,
+}
