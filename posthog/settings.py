ef30498c44735b864bea00c4d64a818150adaa16
Tim Glaser <tim.glaser@hiberly.com>
2020-03-18 20:31:13 +0000

    Bump version

--- posthog/settings.py
+++ posthog/settings.py
@@ -18,7 +18,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 from sentry_sdk.integrations.django import DjangoIntegration
 
-VERSION = '1.0.8.1'
+VERSION = '1.0.8.3'
 
 def get_env(key):
     try:
