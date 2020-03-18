71f0922017a001c1df0a53672c5f5f6c9d2fa980
Marius Andra <marius.andra@gmail.com>
2020-03-18 15:23:31 +0100

    add django-extensions

--- posthog/settings.py
+++ posthog/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        'django_extensions',
     'posthog.apps.PostHogConfig',
     'rest_framework',
     'loginas',
