7d4486400fb267ec843f24a67b7b7f12efa66163
Marius Andra <marius.andra@gmail.com>
2020-03-20 11:35:43 +0100

    refactor Person.js

--- frontend/src/scenes/users/Person.js
+++ frontend/src/scenes/users/Person.js
@@ -1,72 +1,55 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { EventsTable } from '../events/EventsTable'
-import api from '../../lib/api'
-import { PropertiesTable } from '../../lib/components/PropertiesTable'
+import api from 'lib/api'
+import { PropertiesTable } from 'lib/components/PropertiesTable'
 import { toast } from 'react-toastify'
 
-export class Person extends Component {
-    constructor(props) {
-        super(props)
+export function Person({ match, history }) {
+    const [person, setPerson] = useState(null)
 
-        this.state = {}
-        this.fetchPerson.call(this)
-    }
-    fetchPerson() {
+    useEffect(() => {
         let url = ''
-        if (this.props.match.params.distinct_id) {
-            url =
-                'api/person/by_distinct_id/?distinct_id=' +
-                this.props.match.params.distinct_id
+        if (match.params.distinct_id) {
+            url = `api/person/by_distinct_id/?distinct_id=${match.params.distinct_id}`
         } else {
-            url = 'api/person/' + this.props.match.params.id
+            url = `api/person/${match.params.id}`
         }
-        api.get(url).then(person => this.setState({ person }))
-    }
+        api.get(url).then(setPerson)
+    }, [match.params.distinct_id, match.params.id])
 
-    render() {
-        return this.state.person ? (
-            <div>
-                <button
-                    className="btn btn-outline-danger btn-sm float-right"
-                    onClick={e =>
-                        confirm(
-                            'Are you sure you want to delete this user? This cannot be undone'
-                        ) &&
-                        api
-                            .delete('api/person/' + this.state.person.id)
-                            .then(() => {
-                                toast('Person succesfully deleted.')
-                                this.props.history.push('/people')
-                            })
-                    }
-                >
-                    Delete all data on this person
-                </button>
-                <h1>{this.state.person.name}</h1>
-                <div style={{ maxWidth: 750 }}>
-                    <PropertiesTable properties={this.state.person.properties} />
-                    <table className="table">
-                        <tbody>
-                            <tr>
-                                <td>Distinct IDs</td>
-                                <td>
-                                    {this.state.person.distinct_ids.map(
-                                        distinct_id => (
-                                            <pre style={{ margin: 0 }}>
-                                                {distinct_id}
-                                            </pre>
-                                        )
-                                    )}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <EventsTable
-                    fixedFilters={{ person_id: this.state.person.id }}
-                    history={this.props.history}
-                />
+    return person ? (
+        <div>
+            <button
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={e =>
+                    window.confirm('Are you sure you want to delete this user? This cannot be undone') &&
+                    api.delete('api/person/' + person.id).then(() => {
+                        toast('Person succesfully deleted.')
+                        history.push('/people')
+                    })
+                }
+            >
+                Delete all data on this person
+            </button>
+            <h1>{person.name}</h1>
+            <div style={{ maxWidth: 750 }}>
+                <PropertiesTable properties={person.properties} />
+                <table className="table">
+                    <tbody>
+                        <tr>
+                            <td>Distinct IDs</td>
+                            <td>
+                                {person.distinct_ids.map(distinct_id => (
+                                    <pre style={{ margin: 0 }} key={distinct_id}>
+                                        {distinct_id}
+                                    </pre>
+                                ))}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-        ) : null
-    }
+            <EventsTable fixedFilters={{ person_id: person.id }} history={history} />
+        </div>
+    ) : null
 }
