adf1624aa56d7bd0f5ba0da1d2606c22b7b0e4d5
Marius Andra <marius.andra@gmail.com>
2020-03-20 12:16:09 +0100

    extract PeopleTable

--- frontend/src/scenes/users/People.js
+++ frontend/src/scenes/users/People.js
@@ -1,30 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import api from 'lib/api'
-import { Link } from 'react-router-dom'
-import moment from 'moment'
-import { fromParams, Loading, toParams } from 'lib/utils'
+import { fromParams } from 'lib/utils'
 import { Cohort } from './Cohort'
-
-function FilterLink({ value, onClick }) {
-    return (
-        <a
-            href="#"
-            onClick={e => {
-                e.preventDefault()
-                onClick && onClick(value)
-            }}
-        >
-            {typeof value === 'object' ? JSON.stringify(value) : value}
-        </a>
-    )
-}
+import { PeopleTable } from './PeopleTable'
 
 export function People({ history }) {
     const [loading, setLoading] = useState(true)
     const [people, setPeople] = useState(null)
     const [search, setSearch] = useState(undefined)
     const [hasNext, setHasNext] = useState(null)
-    const [personSelected, setPersonSelected] = useState(null)
 
     function fetchPeople(search, cohort_id) {
         if (search !== undefined) {
@@ -77,83 +61,17 @@ export function People({ history }) {
                 />
             )}
             <br />
-            <table className="table" style={{ position: 'relative' }}>
-                {loading ? (
-                    <tbody>
-                        <tr>
-                            <td>
-                                <Loading />
-                            </td>
-                        </tr>
-                    </tbody>
-                ) : (
-                    <tbody>
-                        <tr>
-                            <th>Person</th>
-                            <th>Last seen</th>
-                        </tr>
-                        {people && people.length === 0 && (
-                            <tr>
-                                <td colSpan="2">
-                                    We haven't seen any data yet. If you haven't integrated PostHog,{' '}
-                                    <Link to="/setup">click here to set PostHog up on your app</Link>
-                                </td>
-                            </tr>
-                        )}
-                        {people &&
-                            people.map(person => [
-                                <tr
-                                    key={person.id}
-                                    className="cursor-pointer"
-                                    onClick={() => setPersonSelected(person.id)}
-                                >
-                                    <td>
-                                        <Link
-                                            to={'/person/' + encodeURIComponent(person.distinct_ids[0])}
-                                            className="ph-no-capture"
-                                        >
-                                            {person.name}
-                                        </Link>
-                                    </td>
-                                    <td>{person.last_event && moment(person.last_event.timestamp).fromNow()}</td>
-                                </tr>,
-                                personSelected === person.id && (
-                                    <tr key={person.id + '_open'}>
-                                        <td colSpan="4">
-                                            {Object.keys(person.properties).length === 0 &&
-                                                'This person has no properties.'}
-                                            <div className="d-flex flex-wrap flex-column" style={{ height: 200 }}>
-                                                {Object.keys(person.properties)
-                                                    .sort()
-                                                    .map(key => (
-                                                        <div
-                                                            style={{
-                                                                flex: '0 1',
-                                                            }}
-                                                            key={key}
-                                                        >
-                                                            <strong>{key}:</strong>{' '}
-                                                            <FilterLink
-                                                                property={key}
-                                                                value={person.properties[key]}
-                                                                onClick={value => {
-                                                                    const newSearch = search
-                                                                        ? `${search.trim()} ${value}`
-                                                                        : value
-                                                                    setSearch(newSearch)
-                                                                    fetchPeople(newSearch)
-                                                                }}
-                                                            />
-                                                        </div>
-                                                    ))}
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ),
-                            ])}
-                    </tbody>
-                )}
-            </table>
+
+            <PeopleTable
+                loading={loading}
+                people={people}
+                onClickProperty={(property, value) => {
+                    const newSearch = search ? `${search.trim()} ${value}` : value
+                    setSearch(newSearch)
+                    fetchPeople(newSearch)
+                }}
+            />
+
             {people && people.length > 0 && hasNext && (
                 <button
                     className="btn btn-primary"
---
+++ frontend/src/scenes/users/PeopleTable.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import { Loading } from 'lib/utils'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+
+function FilterLink({ property, value, onClick }) {
+    const label = typeof value === 'object' ? JSON.stringify(value) : value
+
+    if (onClick) {
+        return (
+            <a
+                href="#"
+                onClick={e => {
+                    e.preventDefault()
+                    onClick(property, value)
+                }}
+            >
+                {label}
+            </a>
+        )
+    } else {
+        return <>{label}</>
+    }
+}
+
+export function PeopleTable({ loading, people, onClickProperty }) {
+    const [personSelected, setPersonSelected] = useState(null)
+
+    return (
+        <table className="table" style={{ position: 'relative' }}>
+            {loading ? (
+                <tbody>
+                    <tr>
+                        <td>
+                            <Loading />
+                        </td>
+                    </tr>
+                </tbody>
+            ) : (
+                <tbody>
+                    <tr>
+                        <th>Person</th>
+                        <th>Last seen</th>
+                    </tr>
+                    {people && people.length === 0 && (
+                        <tr>
+                            <td colSpan="2">
+                                We haven't seen any data yet. If you haven't integrated PostHog,{' '}
+                                <Link to="/setup">click here to set PostHog up on your app</Link>
+                            </td>
+                        </tr>
+                    )}
+                    {people &&
+                        people.map(person => [
+                            <tr key={person.id} className="cursor-pointer" onClick={() => setPersonSelected(person.id)}>
+                                <td>
+                                    <Link
+                                        to={'/person/' + encodeURIComponent(person.distinct_ids[0])}
+                                        className="ph-no-capture"
+                                    >
+                                        {person.name}
+                                    </Link>
+                                </td>
+                                <td>{person.last_event && moment(person.last_event.timestamp).fromNow()}</td>
+                            </tr>,
+                            personSelected === person.id && (
+                                <tr key={person.id + '_open'}>
+                                    <td colSpan="4">
+                                        {Object.keys(person.properties).length === 0 &&
+                                            'This person has no properties.'}
+                                        <div className="d-flex flex-wrap flex-column" style={{ height: 200 }}>
+                                            {Object.keys(person.properties)
+                                                .sort()
+                                                .map(key => (
+                                                    <div
+                                                        style={{
+                                                            flex: '0 1',
+                                                        }}
+                                                        key={key}
+                                                    >
+                                                        <strong>{key}:</strong>{' '}
+                                                        <FilterLink
+                                                            property={key}
+                                                            value={person.properties[key]}
+                                                            onClick={onClickProperty}
+                                                        />
+                                                    </div>
+                                                ))}
+                                        </div>
+                                    </td>
+                                </tr>
+                            ),
+                        ])}
+                </tbody>
+            )}
+        </table>
+    )
+}
