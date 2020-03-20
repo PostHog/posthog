25d028eed6dea38a9c3687c12cee7c1dc21dd0ff
Marius Andra <marius.andra@gmail.com>
2020-03-20 14:34:45 +0100

    improve people modal label text

--- frontend/src/scenes/trends/PeopleModal.js
+++ frontend/src/scenes/trends/PeopleModal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useActions, useValues } from 'kea'
+import moment from 'moment'
 import { trendsLogic } from 'scenes/trends/trendsLogic'
 import { Modal } from 'lib/components/Modal'
 import { PeopleTable } from 'scenes/users/PeopleTable'
@@ -9,11 +10,14 @@ export function PeopleModal() {
     const { hidePeople } = useActions(trendsLogic)
 
     return (
-        <Modal title={`Users for "${peopleAction?.name || '...'}" on ${peopleDay || '...'}`} onDismiss={hidePeople}>
+        <Modal
+            title={`"${peopleAction?.name || '...'}" on ${peopleDay ? moment(peopleDay).format('ll') : '...'}`}
+            onDismiss={hidePeople}
+        >
             {people ? (
                 <p>
                     Found {peopleCount} {peopleCount === 1 ? 'user' : 'users'}
-                    {peopleCount > 100 ? '. Showing the first 100 below: ' : '.'}
+                    {peopleCount > 100 ? '. Showing the first 100 below.' : ''}
                 </p>
             ) : (
                 <p>Loading users...</p>
