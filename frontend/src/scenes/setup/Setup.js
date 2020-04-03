0aefb5df8b25da80771782ebec08f9c5d72fc87e
George Zografos <george.p.zografos@gmail.com>
2020-04-03 21:09:23 +0200

    Add UI for change password

--- frontend/src/scenes/setup/Setup.js
+++ frontend/src/scenes/setup/Setup.js
@@ -6,7 +6,8 @@ import { OptOutCapture } from './OptOutCapture'
 import { SetupAppUrls } from './SetupAppUrls'
 
 import { userLogic } from '../userLogic'
-import { DeleteDemoData } from './DeleteDemoData';
+import { DeleteDemoData } from './DeleteDemoData'
+import { ChangePassword } from './ChangePassword'
 
 export function Setup() {
     const { user } = useValues(userLogic)
@@ -18,25 +19,23 @@ export function Setup() {
             <br />
             <br />
             <h2>Integrate PostHog</h2>
-            To integrate PostHog, copy + paste the following snippet to your
-            website. Ideally, put it just above the{' '}
+            To integrate PostHog, copy + paste the following snippet to your website. Ideally, put it just above the{' '}
             <pre style={{ display: 'inline' }}>&lt;/head&gt;</pre> tag.
             <a href="https://github.com/PostHog/posthog/wiki/JS-integration">
                 See docs for instructions on how to identify users.
             </a>
             <br />
             <JSSnippet user={user} />
-            <a href="https://github.com/PostHog/posthog/wiki/Integrations">
-                Using Python/Ruby/Node/Go/PHP instead?
-            </a>
+            <a href="https://github.com/PostHog/posthog/wiki/Integrations">Using Python/Ruby/Node/Go/PHP instead?</a>
             <br />
             <br />
             <br />
             <h2>API key</h2>
-            You can use this api key in any of our   
+            You can use this api key in any of our
             <a href="https://github.com/PostHog/posthog/wiki/Integrations"> libraries</a>.
-            <pre className='code'>{user.team.api_token}</pre>
-            This key is write-only, in that it can only create new events. It can't read any events or any of your other data stored on PostHog.
+            <pre className="code">{user.team.api_token}</pre>
+            This key is write-only, in that it can only create new events. It can't read any events or any of your other
+            data stored on PostHog.
             <br />
             <br />
             <br />
@@ -48,13 +47,16 @@ export function Setup() {
             </div>
             <br />
             <br />
-
             <h2>Delete HogFlix demo data</h2>
             <DeleteDemoData />
             <br />
             <br />
             <br />
-
+            <h2>Change Password</h2>
+            <ChangePassword />
+            <br />
+            <br />
+            <br />
             <h2>Opt out of capturing</h2>
             <OptOutCapture />
         </div>
