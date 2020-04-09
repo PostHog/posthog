18e4038a3d4c3df4dd9b50ebf6ac0ebd9776a3f3
Eric <eeoneric@gmail.com>
2020-04-09 12:08:13 -0400

    initial commit for dropin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import './style.scss'
+import 'antd/dist/antd.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
--- frontend/src/layout/Sidebar.js
+++ frontend/src/layout/Sidebar.js
@@ -1,122 +1,102 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
+import { withRouter, NavLink } from 'react-router-dom'
 import { InviteTeam } from '../lib/components/InviteTeam'
 import { Modal } from '../lib/components/Modal'
 
+import { Menu } from 'antd'
 class Sidebar extends Component {
     constructor(props) {
         super(props)
         this.state = {}
     }
+
+    showSubgroup = keys => {
+        let show = false
+        keys.forEach(key => {
+            if (this.props.history.location.pathname == key) show = true
+        })
+        return show
+    }
+
     render() {
-        let matches = path =>
-            this.props.history.location.pathname.indexOf(path) > -1
         return (
-            <div
-                className="sidebar col-sm-3 col-md-2 flex-shrink-1 bg-light pt-3"
-                style={{ minHeight: '100vh' }}
+            <Menu
+                className="sidebar vh-100 col-sm-3 col-md-2 flex-shrink-1 bg-light pt-3"
+                selectedKeys={[this.props.history.location.pathname]}
+                mode="inline"
+                onOpenChange={openKeys => console.log(openKeys)}
             >
-                <div className="row logo-row">
+                <div className="row logo-row d-flex align-items-center">
                     <img className="logo" src="/static/posthog-logo.png" />
                     <div className="posthog-title">PostHog</div>
                 </div>
-                <ul className="nav flex-sm-column">
-                    <li>
-                        <NavLink className="nav-link" exact to="/">
-                            <i className="fi flaticon-home" /> Dashboard
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/trends">
-                            <i className="fi flaticon-target" /> Trends
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/events">
-                            <i className="fi flaticon-zoom-in" /> Events
-                        </NavLink>
-                    </li>
-                    {(matches('/action') || matches('/events')) && [
-                        <li key="1" className="nav-indent">
-                            <NavLink className="nav-link" exact to="/actions">
-                                <i className="fi flaticon-click" /> Actions
-                            </NavLink>
-                        </li>,
-                        <li key="2" className="nav-indent">
-                            <NavLink className="nav-link" to="/actions/live">
-                                <i className="fi flaticon-refresh" /> Live
-                                actions
-                            </NavLink>
-                        </li>,
-                    ]}
-                    <li>
-                        <NavLink
-                            className={
-                                'nav-link ' + (matches('/person') && 'active')
-                            }
-                            to="/people"
-                        >
-                            <i className="fi flaticon-speech-bubble" /> Users
-                        </NavLink>
-                    </li>
-                    {matches('/people') && [
-                        <li key="1" className="nav-indent">
-                            <NavLink
-                                className={'nav-link'}
-                                to="/people/cohorts"
-                            >
-                                <i
-                                    className="fi flaticon-user"
-                                    style={{ margin: 0 }}
-                                />
-                                <i
-                                    className="fi flaticon-user"
-                                    style={{ marginLeft: -4 }}
-                                />
-                                Cohorts
-                            </NavLink>
-                        </li>,
-                    ]}
-                    <li>
-                        <NavLink className="nav-link" to="/funnel">
-                            <i className="fi flaticon-cursor-1" /> Funnels
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/paths">
-                            <i
-                                className="fi flaticon-shuffle-1"
-                                style={{ transform: 'rotate(180deg)' }}
-                            />{' '}
-                            Paths
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/setup">
-                            <i className="fi flaticon-settings" /> Setup
-                        </NavLink>
-                    </li>
-                </ul>
+                <Menu.Item {...this.props} key="/">
+                    <i className="fi flaticon-home" />
+                    <span className="menu-label">Dashboard</span>
+                    <NavLink className="nav-link" exact to="/"></NavLink>
+                </Menu.Item>
+                <Menu.Item {...this.props} key="/trends">
+                    <i className="fi flaticon-target" />
+                    <span className="menu-label">Trends</span>
+                    <NavLink className="nav-link" to="/trends"></NavLink>
+                </Menu.Item>
+                <Menu.Item {...this.props} key="/events">
+                    <i className="fi flaticon-zoom-in" />
+                    <span className="menu-label">Events</span>
+                    <NavLink className="nav-link" to="/events"></NavLink>
+                </Menu.Item>
+                {this.showSubgroup(['/events', '/actions', '/actions/live']) && (
+                    <Menu.Item className="inner-menu-item" {...this.props} key="/actions">
+                        <i className="fi flaticon-target" />
+                        <span className="menu-label">Actions</span>
+                        <NavLink className="nav-link" to="/actions"></NavLink>
+                    </Menu.Item>
+                )}
+                {this.showSubgroup(['/events', '/actions', '/actions/live']) && (
+                    <Menu.Item className="inner-menu-item" {...this.props} key="/actions/live">
+                        <i className="fi flaticon-refresh" />
+                        <span className="menu-label">Live Actions</span>
+                        <NavLink className="nav-link" to="/actions/live"></NavLink>
+                    </Menu.Item>
+                )}
+                <Menu.Item {...this.props} key="/people">
+                    <i className="fi flaticon-speech-bubble" />
+                    <span className="menu-label">Users</span>
+                    <NavLink className={'nav-link'} to="/people"></NavLink>
+                </Menu.Item>
+                {this.showSubgroup(['/people', '/people/cohorts']) && (
+                    <Menu.Item className="inner-menu-item" {...this.props} key="/people/cohorts">
+                        <i className="fi flaticon-user" />
+                        <span className="menu-label">Cohorts</span>
+                        <NavLink className={'nav-link'} to="/people/cohorts"></NavLink>
+                    </Menu.Item>
+                )}
+                <Menu.Item {...this.props} key="/funnel">
+                    <i className="fi flaticon-cursor-1" />
+                    <span className="menu-label">Funnels</span>
+                    <NavLink className={'nav-link'} to="/funnel"></NavLink>
+                </Menu.Item>
+                <Menu.Item {...this.props} key="/paths">
+                    <i className="fi flaticon-shuffle-1" style={{ transform: 'rotate(180deg)' }} />
+                    <span className="menu-label">Paths</span>
+                    <NavLink className={'nav-link'} to="/paths"></NavLink>
+                </Menu.Item>
+                <Menu.Item {...this.props} key="/setup">
+                    <i className="fi flaticon-settings" />
+                    <span className="menu-label">Setup</span>
+                    <NavLink className={'nav-link'} to="/setup"></NavLink>
+                </Menu.Item>
                 <div className="col-sm-3 col-md-2 invite-team">
-                    <button
-                        className="secondary"
-                        onClick={() => this.setState({ inviteModalOpen: true })}
-                    >
+                    <button className="secondary" onClick={() => this.setState({ inviteModalOpen: true })}>
                         Invite your team
                     </button>
                 </div>
                 {this.state.inviteModalOpen && (
-                    <Modal
-                        onDismiss={() =>
-                            this.setState({ inviteModalOpen: false })
-                        }
-                        hideFooter={true}
-                    >
+                    <Modal onDismiss={() => this.setState({ inviteModalOpen: false })} hideFooter={true}>
                         <InviteTeam user={this.props.user} />
                     </Modal>
                 )}
-            </div>
+            </Menu>
         )
     }
 }
