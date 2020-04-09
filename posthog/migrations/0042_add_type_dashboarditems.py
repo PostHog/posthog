581aa9c0af0bf158855449c5db137f9e5270e5bb
Eric <eeoneric@gmail.com>
2020-04-09 07:57:36 -0400

    updated migration

--- posthog/migrations/0042_add_type_dashboarditems.py
+++ posthog/migrations/0042_add_type_dashboarditems.py
@@ -6,13 +6,26 @@ from posthog.constants import TREND_FILTER_TYPE_ACTIONS, TREND_FILTER_TYPE_EVENT
 def update_filter_types(apps, schema_editor):
     DashboardItem = apps.get_model('posthog', 'DashboardItem')
     for item in DashboardItem.objects.filter(filters__actions__isnull=False):
-        item.filters['actions'] = [{'id': action['id'], 'type': TREND_FILTER_TYPE_ACTIONS if isinstance(action['id'], int) else TREND_FILTER_TYPE_EVENTS} for action in item.filters['actions']]
+        new_actions = []
+        for action in item.filters['actions']:
+            if isinstance(action['id'], int):
+                action.update({'type': TREND_FILTER_TYPE_ACTIONS})
+            else:
+                action.update({'type': TREND_FILTER_TYPE_EVENTS})
+            new_actions.append(action)
+        item.filters['actions'] = new_actions
         item.save()
 
 def reverse_filter_types(apps, schema_editor):
     DashboardItem = apps.get_model('posthog', 'DashboardItem')
     for item in DashboardItem.objects.filter(filters__actions__isnull=False):
-        item.filters['actions'] = [{'id': action['id']} if isinstance(action['id'], int) else {'id': action['id'], 'type': TREND_FILTER_TYPE_EVENTS} for action in item.filters['actions']]
+        old_actions = []
+        for action in item.filters['actions']:
+            if isinstance(action['id'], int):
+                action.pop('type')
+            else:
+                action.update({'type': TREND_FILTER_TYPE_EVENTS})
+            old_actions.append(action)
         item.save()
 
 class Migration(migrations.Migration):
