3fa189c12e8af2dea3e2380dd31513ec25a048b9
Eric <eeoneric@gmail.com>
2020-04-09 12:31:29 -0400

    recover original styling of modal and invite buttons

--- frontend/src/layout/Sidebar.js
+++ frontend/src/layout/Sidebar.js
@@ -20,71 +20,72 @@ class Sidebar extends Component {
 
     render() {
         return (
-            <Menu
-                className="sidebar vh-100 col-sm-3 col-md-2 flex-shrink-1 bg-light pt-3"
-                selectedKeys={[this.props.history.location.pathname]}
-                mode="inline"
+            <div
+                className="sidebar col-sm-3 col-md-2 no-gutters flex-shrink-1 bg-light pt-3"
+                style={{ minHeight: '100vh' }}
             >
-                <div className="row logo-row d-flex align-items-center">
-                    <img className="logo" src="/static/posthog-logo.png" />
-                    <div className="posthog-title">PostHog</div>
-                </div>
-                <Menu.Item {...this.props} key="/">
-                    <i className="fi flaticon-home" />
-                    <span className="menu-label">Dashboard</span>
-                    <NavLink className="nav-link" exact to="/"></NavLink>
-                </Menu.Item>
-                <Menu.Item {...this.props} key="/trends">
-                    <i className="fi flaticon-target" />
-                    <span className="menu-label">Trends</span>
-                    <NavLink className="nav-link" to="/trends"></NavLink>
-                </Menu.Item>
-                <Menu.Item {...this.props} key="/events">
-                    <i className="fi flaticon-zoom-in" />
-                    <span className="menu-label">Events</span>
-                    <NavLink className="nav-link" to="/events"></NavLink>
-                </Menu.Item>
-                {this.showSubgroup(['/events', '/actions', '/actions/live']) && (
-                    <Menu.Item {...this.props} key="/actions">
-                        <i className="fi flaticon-target inner-menu-icon" />
-                        <span className="menu-label">Actions</span>
-                        <NavLink className="nav-link" to="/actions"></NavLink>
+                <Menu className="h-100" selectedKeys={[this.props.history.location.pathname]} mode="inline">
+                    <div className="row logo-row d-flex align-items-center">
+                        <img className="logo" src="/static/posthog-logo.png" />
+                        <div className="posthog-title">PostHog</div>
+                    </div>
+                    <Menu.Item {...this.props} key="/">
+                        <i className="fi flaticon-home" />
+                        <span className="menu-label">Dashboard</span>
+                        <NavLink className="nav-link" exact to="/"></NavLink>
                     </Menu.Item>
-                )}
-                {this.showSubgroup(['/events', '/actions', '/actions/live']) && (
-                    <Menu.Item {...this.props} key="/actions/live">
-                        <i className="fi flaticon-refresh inner-menu-icon" />
-                        <span className="menu-label">Live Actions</span>
-                        <NavLink className="nav-link" to="/actions/live"></NavLink>
+                    <Menu.Item {...this.props} key="/trends">
+                        <i className="fi flaticon-target" />
+                        <span className="menu-label">Trends</span>
+                        <NavLink className="nav-link" to="/trends"></NavLink>
                     </Menu.Item>
-                )}
-                <Menu.Item {...this.props} key="/people">
-                    <i className="fi flaticon-speech-bubble" />
-                    <span className="menu-label">Users</span>
-                    <NavLink className={'nav-link'} to="/people"></NavLink>
-                </Menu.Item>
-                {this.showSubgroup(['/people', '/people/cohorts']) && (
-                    <Menu.Item {...this.props} key="/people/cohorts">
-                        <i className="fi flaticon-user inner-menu-icon" />
-                        <span className="menu-label ">Cohorts</span>
-                        <NavLink className={'nav-link'} to="/people/cohorts"></NavLink>
+                    <Menu.Item {...this.props} key="/events">
+                        <i className="fi flaticon-zoom-in" />
+                        <span className="menu-label">Events</span>
+                        <NavLink className="nav-link" to="/events"></NavLink>
                     </Menu.Item>
-                )}
-                <Menu.Item {...this.props} key="/funnel">
-                    <i className="fi flaticon-cursor-1" />
-                    <span className="menu-label">Funnels</span>
-                    <NavLink className={'nav-link'} to="/funnel"></NavLink>
-                </Menu.Item>
-                <Menu.Item {...this.props} key="/paths">
-                    <i className="fi flaticon-shuffle-1" style={{ transform: 'rotate(180deg)' }} />
-                    <span className="menu-label">Paths</span>
-                    <NavLink className={'nav-link'} to="/paths"></NavLink>
-                </Menu.Item>
-                <Menu.Item {...this.props} key="/setup">
-                    <i className="fi flaticon-settings" />
-                    <span className="menu-label">Setup</span>
-                    <NavLink className={'nav-link'} to="/setup"></NavLink>
-                </Menu.Item>
+                    {this.showSubgroup(['/events', '/actions', '/actions/live']) && (
+                        <Menu.Item {...this.props} key="/actions">
+                            <i className="fi flaticon-target inner-menu-icon" />
+                            <span className="menu-label">Actions</span>
+                            <NavLink className="nav-link" to="/actions"></NavLink>
+                        </Menu.Item>
+                    )}
+                    {this.showSubgroup(['/events', '/actions', '/actions/live']) && (
+                        <Menu.Item {...this.props} key="/actions/live">
+                            <i className="fi flaticon-refresh inner-menu-icon" />
+                            <span className="menu-label">Live Actions</span>
+                            <NavLink className="nav-link" to="/actions/live"></NavLink>
+                        </Menu.Item>
+                    )}
+                    <Menu.Item {...this.props} key="/people">
+                        <i className="fi flaticon-speech-bubble" />
+                        <span className="menu-label">Users</span>
+                        <NavLink className={'nav-link'} to="/people"></NavLink>
+                    </Menu.Item>
+                    {this.showSubgroup(['/people', '/people/cohorts']) && (
+                        <Menu.Item {...this.props} key="/people/cohorts">
+                            <i className="fi flaticon-user inner-menu-icon" />
+                            <span className="menu-label ">Cohorts</span>
+                            <NavLink className={'nav-link'} to="/people/cohorts"></NavLink>
+                        </Menu.Item>
+                    )}
+                    <Menu.Item {...this.props} key="/funnel">
+                        <i className="fi flaticon-cursor-1" />
+                        <span className="menu-label">Funnels</span>
+                        <NavLink className={'nav-link'} to="/funnel"></NavLink>
+                    </Menu.Item>
+                    <Menu.Item {...this.props} key="/paths">
+                        <i className="fi flaticon-shuffle-1" style={{ transform: 'rotate(180deg)' }} />
+                        <span className="menu-label">Paths</span>
+                        <NavLink className={'nav-link'} to="/paths"></NavLink>
+                    </Menu.Item>
+                    <Menu.Item {...this.props} key="/setup">
+                        <i className="fi flaticon-settings" />
+                        <span className="menu-label">Setup</span>
+                        <NavLink className={'nav-link'} to="/setup"></NavLink>
+                    </Menu.Item>
+                </Menu>
                 <div className="col-sm-3 col-md-2 invite-team">
                     <button className="secondary" onClick={() => this.setState({ inviteModalOpen: true })}>
                         Invite your team
@@ -95,7 +96,7 @@ class Sidebar extends Component {
                         <InviteTeam user={this.props.user} />
                     </Modal>
                 )}
-            </Menu>
+            </div>
         )
     }
 }
