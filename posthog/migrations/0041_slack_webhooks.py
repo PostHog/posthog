24a7fd2b1318e47181ca7987b7cf177fe0f07028
Marius Andra <marius.andra@gmail.com>
2020-04-09 15:28:40 +0200

    update migration dependency (rebase fix)

--- posthog/migrations/0041_slack_webhooks.py
+++ posthog/migrations/0041_slack_webhooks.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posthog', '0040_remove_event_ip'),
+        ('posthog', '0041_merge_20200407_1805'),
     ]
 
     operations = [
