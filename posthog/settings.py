ee2f2d1469b06dc72b83e7a267814f3aaa17d651
Marius Andra <marius.andra@gmail.com>
2020-04-13 17:11:56 +0200

    add localhost redis url for tests

--- posthog/settings.py
+++ posthog/settings.py
@@ -183,7 +183,11 @@ else:
     raise ImproperlyConfigured(f'The environment vars "DATABASE_URL" or "POSTHOG_DB_NAME" are absolutely required to run this software')
 
 # Broker
-REDIS_URL = os.environ.get('REDIS_URL', '')
+
+if TEST or DEBUG:
+    REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost/')
+else:
+    REDIS_URL = os.environ.get('REDIS_URL', '')
 
 if not REDIS_URL and os.environ.get('POSTHOG_REDIS_HOST', ''):
     REDIS_URL = "redis://:{}@{}:{}/".format(os.environ.get('POSTHOG_REDIS_PASSWORD', ''), os.environ.get('POSTHOG_REDIS_HOST', ''), os.environ.get('POSTHOG_REDIS_PORT', '6379'))
