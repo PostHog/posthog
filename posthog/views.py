14f12846ed85c26239f47c3efed2e67b5e692f4f
Marius Andra <marius.andra@gmail.com>
2020-04-13 17:22:06 +0200

    Helm redis (#558)
    * ignore copying frontend/dist folder - otherwise whatever you build in docker will get overridden by local build artifacts if any exist in frontend/dist
    
    * support configuring redis with POSTHOG_REDIS_HOST and other vars in addition to REDIS_URL
    
    * remove "the next version" in worker requirement modal
    
    * split beat and celery scripts
    
    * remove chart folder
    
    * celery heartbeat every 10sec, reduce distributed beat lock hold time
    
    * remove dockerfile local link
    
    * add localhost redis url for tests

--- posthog/views.py
+++ posthog/views.py
@@ -20,7 +20,7 @@ def stats(request):
     last_heartbeat = redis_instance.get("POSTHOG_HEARTBEAT") if redis_instance else None
     worker_heartbeat = int(time.time()) - int(last_heartbeat) if last_heartbeat else None
 
-    if worker_heartbeat and worker_heartbeat < 300:
+    if worker_heartbeat == 0 or worker_heartbeat < 300:
         stats_response['worker_heartbeat'] = worker_heartbeat
     else:
         stats_response['worker_heartbeat'] = 'offline'
