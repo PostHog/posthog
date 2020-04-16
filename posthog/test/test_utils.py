204a5b9f0408479fe675ee854de7148546592667
Tim Glaser <tim.glaser@hiberly.com>
2020-04-16 11:17:27 +0100

    Fix use 'and' for properties filters

--- posthog/test/test_utils.py
+++ posthog/test/test_utils.py
@@ -69,3 +69,11 @@ class TestPropertiesToQ(BaseTest):
         self.assertEqual(events[0], event1)
         self.assertEqual(events[1], event2)
         self.assertEqual(len(events), 2)
+
+    def test_multiple(self):
+        event2 = Event.objects.create(team=self.team, event='$pageview', properties={'$current_url': 'https://something.com', 'another_key': 'value'})
+        Event.objects.create(team=self.team, event='$pageview', properties={'$current_url': 'https://something.com'})
+        properties = {'$current_url__icontains': 'something.com', 'another_key': 'value'}
+        events = Event.objects.filter(properties_to_Q(properties))
+        self.assertEqual(events[0], event2)
+        self.assertEqual(len(events), 1)
