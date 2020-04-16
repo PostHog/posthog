204aca479f04629d48de34c41a1f09d02de362a9
Eric <eeoneric@gmail.com>
2020-04-15 23:28:40 -0400

    round seconds

--- posthog/api/event.py
+++ posthog/api/event.py
@@ -227,7 +227,7 @@ class EventViewSet(viewsets.ModelViewSet):
             cursor = connection.cursor()
             cursor.execute(overall_average_length(all_sessions), sessions_sql_params)
             calculated = cursor.fetchall()
-            result = [{'label': 'Number of Sessions', 'count': calculated[0][0]}, {'label': 'Average Duration of Session (seconds)', 'count': calculated[0][1]}]
+            result = [{'label': 'Number of Sessions', 'count': calculated[0][0]}, {'label': 'Average Duration of Session (seconds)', 'count': round(calculated[0][1], 2)}]
         else: 
             dist_labels = ['0 seconds (1 event)', '0-3 seconds', '3-10 seconds', '10-30 seconds', '30-60 seconds', '1-3 minutes', '3-10 minutes', '10-30 minutes', '30-60 minutes', '1+ hours']
             cursor = connection.cursor()
