6bc87a61be0b131d6f936ac2e5615e5960b5c039
Eric Duong <eeoneric@gmail.com>
2020-04-20 12:18:06 -0400

    Updated Prop Filters to be "tokenized" (#616)
    * initial commit
    
    * formatting
    
    * adjusted margins
    
    * missing condition
    
    * reused operatormap and fixed spacing
    
    * check more specific key first
    
    * reverse operator keys once

--- frontend/src/lib/components/PropertyFilters/propertyFilterLogic.js
+++ frontend/src/lib/components/PropertyFilters/propertyFilterLogic.js
@@ -59,13 +59,18 @@ export const propertyFilterLogic = kea({
         [actions.setFilter]: ({ value }) => value && actions.update(),
         [actions.remove]: () => actions.update(),
         [actions.update]: () => {
-            if (values.filters.length == 0) return props.onChange({})
+            if (values.filters.length == 0) {
+                actions.newFilter()
+                return props.onChange({})
+            }
+            if (Object.keys(values.filters[values.filters.length - 1]).length != 0) actions.newFilter()
             let dict = values.filters.reduce((result, item) => ({ ...result, ...item }))
             props.onChange(dict)
         },
     }),
     events: ({ actions, props, values }) => ({
         afterMount: () => {
+            actions.newFilter()
             if (props.endpoint == 'person') {
                 actions.loadPeopleProperties()
             } else {
