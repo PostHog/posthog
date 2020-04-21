3e69b78db6566c9ce7c5a0aa566a2f95bceb4673
Marius Andra <marius.andra@gmail.com>
2020-04-21 16:34:28 +0200

    gunzip request body json

--- posthog/api/capture.py
+++ posthog/api/capture.py
@@ -7,6 +7,7 @@ from urllib.parse import urlparse
 from posthog.tasks.process_event import process_event
 import json
 import base64
+import gzip
 import datetime
 
 
@@ -32,7 +33,10 @@ def cors_response(request, response):
 def _load_data(request) -> Optional[Union[Dict, List]]:
     if request.method == 'POST':
         if request.content_type == 'application/json':
-            data = request.body
+            if request.headers['content-encoding'] == 'gzip':
+                data = gzip.decompress(request.body)
+            else:
+                data = request.body
         else:
             data = request.POST.get('data')
     else:
