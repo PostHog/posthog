ea1a3a6c87779f440267b8a1212027c90d8d16e5
Tim Glaser <tim.glaser@hiberly.com>
2020-04-22 00:04:53 +0100

    Use ipaddress to figure out network etc.

--- posthog/middleware.py
+++ posthog/middleware.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.core.exceptions import MiddlewareNotUsed
 from django.http import HttpResponse, HttpRequest
+from ipaddress import ip_address, ip_network
 import re
 
 class AllowIP(object):
     def __init__(self, get_response):
         if getattr(settings, 'ALLOWED_IP_BLOCKS', False):
-            self.re_compiled = re.compile(settings.ALLOWED_IP_BLOCKS) # type: ignore
+            self.ip_blocks = settings.ALLOWED_IP_BLOCKS.split(',') # type: ignore
         else:
             # this will make Django skip this middleware for all future requests
             raise MiddlewareNotUsed()
@@ -18,6 +19,6 @@ class AllowIP(object):
         if request.path.split('/')[1] in ['decide', 'engage', 'track', 'capture', 'batch', 'e']:
             return response
         ip = request.META['REMOTE_ADDR']
-        if  self.re_compiled.match(ip):
+        if any(ip_address(ip) in ip_network(block, strict=False) for block in self.ip_blocks):
             return response
         return HttpResponse("Your IP is not allowed. Check your ALLOWED_IP_BLOCKS settings")
--- posthog/test/test_middleware.py
+++ posthog/test/test_middleware.py
@@ -5,16 +5,19 @@ class TestSignup(TestCase):
         super().setUp()
         self.client = Client()
      
-    def test_signup_new_team(self):
-        with self.settings(ALLOWED_IP_BLOCKS='127\.0\.0\.1'):
+    def test_ip_range(self):
+        with self.settings(ALLOWED_IP_BLOCKS='192.168.0.0/31,127.0.0.0/31,128.0.0.1'):
+            response = self.client.get('/', REMOTE_ADDR='10.0.0.1')
+            self.assertIn(b'IP is not allowed', response.content)
+
+            response = self.client.get('/', REMOTE_ADDR='192.168.0.1')
+            self.assertNotIn(b'IP is not allowed', response.content)
+
+            response = self.client.get('/batch/', REMOTE_ADDR='10.0.0.1')
+            self.assertEqual(b'1', response.content)
+
             response = self.client.get('/', REMOTE_ADDR='127.0.0.1')
             self.assertNotIn(b'IP is not allowed', response.content)
 
-            response = self.client.get('/', REMOTE_ADDR='127.0.0.2')
-            self.assertIn(b'IP is not allowed', response.content)
-
-            response = self.client.get('/batch/', REMOTE_ADDR='127.0.0.2')
-            self.assertEqual(b'1', response.content)
-
-        response = self.client.get('/', REMOTE_ADDR='127.0.0.1')
-        self.assertNotIn(b'IP is not allowed', response.content)
+            response = self.client.get('/', REMOTE_ADDR='128.0.0.1')
+            self.assertNotIn(b'IP is not allowed', response.content)
