69d2e61123b3e6081e13929702cc3b47aa90882a
Marius Andra <marius.andra@gmail.com>
2020-04-23 12:03:40 +0200

    fix random crash, possibly due to old/broken data (#652)

--- frontend/src/scenes/trends/ActionSelectInfo.js
+++ frontend/src/scenes/trends/ActionSelectInfo.js
@@ -17,68 +17,56 @@ export class ActionSelectInfo extends Component {
         let { entity, isOpen } = this.props
         if (!entity) return null
         return (
-            <div
-                className="select-box-info"
-                ref={this.infoDiv}
-                style={{ opacity: isOpen ? 1 : 0 }}
-            >
+            <div className="select-box-info" ref={this.infoDiv} style={{ opacity: isOpen ? 1 : 0 }}>
                 <div style={{ marginBottom: '0.5rem' }}>{entity.name}</div>
-                {entity.steps && entity.steps.map((step, index) => (
-                    <div key={step.id}>
-                        <Card key={step.id} style={{ marginBottom: 0 }}>
-                            <div className="card-body">
-                                <strong>
-                                    {step.event[0] == '$'
-                                        ? step.event[1].toUpperCase() +
-                                          step.event.slice(2)
-                                        : step.event}
-                                </strong>
-                                <ul style={{ listStyle: 'none' }}>
-                                    {step.selector && (
-                                        <li>
-                                            CSS selector matches
-                                            <pre>{step.selector}</pre>
-                                        </li>
-                                    )}
-                                    {step.tag_name && (
-                                        <li>
-                                            Tag name matches{' '}
-                                            <pre>{step.tag_name}</pre>
-                                        </li>
-                                    )}
-                                    {step.text && (
-                                        <li>
-                                            Text matches <pre>{step.text}</pre>
-                                        </li>
-                                    )}
-                                    {step.href && (
-                                        <li>
-                                            Link HREF matches{' '}
-                                            <pre>{step.href}</pre>
-                                        </li>
-                                    )}
-                                    {step.url && (
-                                        <li>
-                                            URL{' '}
-                                            {step.url_matching == 'contains'
-                                                ? 'contains'
-                                                : 'matches'}{' '}
-                                            <pre>{step.url}</pre>
-                                        </li>
-                                    )}
-                                </ul>
-                            </div>
-                        </Card>
-                        {index < entity.steps.length - 1 && (
-                            <div
-                                className="secondary"
-                                style={{ textAlign: 'center', margin: '1rem' }}
-                            >
-                                OR
-                            </div>
-                        )}
-                    </div>
-                ))}
+                {entity.steps &&
+                    entity.steps.map((step, index) => (
+                        <div key={step.id}>
+                            <Card key={step.id} style={{ marginBottom: 0 }}>
+                                <div className="card-body">
+                                    <strong>
+                                        {step.event && step.event[0] == '$'
+                                            ? step.event[1].toUpperCase() + step.event.slice(2)
+                                            : step.event}
+                                    </strong>
+                                    <ul style={{ listStyle: 'none' }}>
+                                        {step.selector && (
+                                            <li>
+                                                CSS selector matches
+                                                <pre>{step.selector}</pre>
+                                            </li>
+                                        )}
+                                        {step.tag_name && (
+                                            <li>
+                                                Tag name matches <pre>{step.tag_name}</pre>
+                                            </li>
+                                        )}
+                                        {step.text && (
+                                            <li>
+                                                Text matches <pre>{step.text}</pre>
+                                            </li>
+                                        )}
+                                        {step.href && (
+                                            <li>
+                                                Link HREF matches <pre>{step.href}</pre>
+                                            </li>
+                                        )}
+                                        {step.url && (
+                                            <li>
+                                                URL {step.url_matching == 'contains' ? 'contains' : 'matches'}{' '}
+                                                <pre>{step.url}</pre>
+                                            </li>
+                                        )}
+                                    </ul>
+                                </div>
+                            </Card>
+                            {index < entity.steps.length - 1 && (
+                                <div className="secondary" style={{ textAlign: 'center', margin: '1rem' }}>
+                                    OR
+                                </div>
+                            )}
+                        </div>
+                    ))}
             </div>
         )
     }
