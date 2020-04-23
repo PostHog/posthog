9cc7d6e48100c77c82d64050681e5a0f8aa17bf5
Eric Duong <eeoneric@gmail.com>
2020-04-23 11:41:20 -0400

    Antd trends updated layout (#648)
    * initial reformat
    
    * minor adjustments
    
    * screen size adjustments
    
    * fix breakdown button
    
    * fix breakdown button
    
    * session filter width
    
    * adjust sizing
    
    * removed minheight

---
+++ frontend/src/lib/hooks/useWindowSize.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+
+export function useWindowSize() {
+    const isClient = typeof window === 'object'
+
+    function getSize() {
+        return {
+            width: isClient ? window.innerWidth : undefined,
+            height: isClient ? window.innerHeight : undefined,
+        }
+    }
+
+    const [windowSize, setWindowSize] = useState(getSize)
+
+    useEffect(() => {
+        if (!isClient) {
+            return false
+        }
+
+        function handleResize() {
+            setWindowSize(getSize())
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, []) // Empty array ensures that effect is only run on mount and unmount
+
+    return windowSize
+}
--- frontend/src/scenes/trends/BreakdownFilter.js
+++ frontend/src/scenes/trends/BreakdownFilter.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react'
 import { selectStyle } from '../../lib/utils'
-import { Select } from 'antd'
+import { Select, Row } from 'antd'
 
 export class BreakdownFilter extends Component {
     render() {
         return (
-            <div style={{ width: 200, display: 'inline-block' }}>
-                <Select
-                    style={{ width: 200 }}
-                    placeholder={'Break down by'}
-                    value={this.props.breakdown ? this.props.breakdown : undefined}
-                    onChange={value => this.props.onChange(value)}
-                    styles={selectStyle}
-                >
-                    {Object.entries(this.props.properties).map(([key, item]) => {
-                        return (
-                            <Select.Option key={key} value={item.value}>
-                                {item.label}
-                            </Select.Option>
-                        )
-                    })}
-                </Select>
-            </div>
+            <Select
+                style={{ width: '80%', maxWidth: 200 }}
+                placeholder={'Break down by'}
+                value={this.props.breakdown ? this.props.breakdown : undefined}
+                onChange={value => this.props.onChange(value)}
+                styles={selectStyle}
+            >
+                {Object.entries(this.props.properties).map(([key, item]) => {
+                    return (
+                        <Select.Option key={key} value={item.value}>
+                            {item.label}
+                        </Select.Option>
+                    )
+                })}
+            </Select>
         )
     }
 }
