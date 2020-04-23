9787b1fb93ba3ef4290d48e3f0a4bb173a33ba9c
Eric <eeoneric@gmail.com>
2020-04-22 20:29:30 -0400

    session filter width

--- frontend/src/lib/components/SessionsFilter.js
+++ frontend/src/lib/components/SessionsFilter.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Select } from 'antd'
+import { Select, Row } from 'antd'
 
 export function SessionFilter(props) {
     let { onChange, value } = props
     return (
-        <Select defaultValue={value} value={value} dropdownMatchSelectWidth={false} onChange={onChange}>
+        <Select
+            style={{ maxWidth: '100%' }}
+            defaultValue={value}
+            value={value}
+            dropdownMatchSelectWidth={false}
+            onChange={onChange}
+        >
             <Select.Option value="avg">Average Session Length</Select.Option>
             <Select.Option value="dist">Distribution of Session Lengths</Select.Option>
         </Select>
