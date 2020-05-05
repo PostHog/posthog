b0feeb5def6b49894d3ffe6e020f33a33e329f24
Tim Glaser <tim.glaser@hiberly.com>
2020-05-05 15:20:44 +0100

    Add eslint to package

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/prop-types': [0],
+    },
+}
