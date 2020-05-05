e3a24fcf5a24e6a9a14e6d893c9a120343ba5f1b
Eric <eeoneric@gmail.com>
2020-05-05 17:16:15 -0400

    add cron job that will keep partitions updated

--- posthog/celery.py
+++ posthog/celery.py
@@ -1,7 +1,9 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
+from django.db import connection
 import redis
 import time
 
@@ -27,10 +29,21 @@ def setup_periodic_tasks(sender, **kwargs):
     # Heartbeat every 10sec to make sure the worker is alive
     sender.add_periodic_task(10.0, redis_heartbeat.s(), name='10 sec heartbeat')
 
+    # Keep event partitions a week ahead and check on this daily
+    sender.add_periodic_task(
+        crontab(),
+        update_event_partitions.s(),
+    )
 
 @app.task
 def redis_heartbeat():
     redis_instance.set("POSTHOG_HEARTBEAT", int(time.time()))
+
+@app.task
+def update_event_partitions():
+    with connection.cursor() as cursor:
+        cursor.execute("DO $$ BEGIN PERFORM update_partitions(); END $$;")
+    
 
 
 @app.task(bind=True)
