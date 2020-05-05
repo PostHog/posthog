b5713ff169374e8c48671a0a7b195e8760c88ae7
Marius Andra <marius.andra@gmail.com>
2020-05-05 14:47:00 +0200

    preload logic before scene

--- frontend/src/scenes/actions/LiveActions.js
+++ frontend/src/scenes/actions/LiveActions.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 import { LiveActionsTable } from './LiveActionsTable'
+import { eventsTableLogic } from 'scenes/events/eventsTableLogic'
+import { connect } from 'kea'
+
+export const logic = connect(() => [
+    eventsTableLogic({ fixedFilters: undefined, apiUrl: 'api/event/actions/', live: true }),
+])
 
 export class LiveActions extends Component {
     render() {
