6e0d799765064cd2a96dec318d32777cd20c53a6
Eric <eeoneric@gmail.com>
2020-05-06 14:39:47 -0400

    add test

--- posthog/api/test/test_paths.py
+++ posthog/api/test/test_paths.py
@@ -2,6 +2,7 @@ from .base import BaseTest
 from posthog.models import Person, Event
 from django.utils.timezone import now
 from dateutil.relativedelta import relativedelta
+from freezegun import freeze_time
 
 class TestPaths(BaseTest):
     TESTS_API = True
@@ -67,3 +68,25 @@ class TestPaths(BaseTest):
         date_to = now() - relativedelta(days=7)
         response = self.client.get('/api/paths/?date_from=' + date_from.strftime("%Y-%m-%d") + '&date_to=' + date_to.strftime("%Y-%m-%d")).json()
         self.assertEqual(len(response), 0)
+
+    def test_paths_in_window(self):
+        person1 = Person.objects.create(team=self.team, distinct_ids=['person_1'])
+
+        first_day = now() - relativedelta(days=5)
+        second_day = now() - relativedelta(days=4)
+        begin_query_range = now() - relativedelta(days=7)
+
+        with freeze_time(first_day.strftime("%Y-%m-%d")):
+            Event.objects.create(properties={'$current_url': '/'}, distinct_id='person_1', event='$pageview', team=self.team)
+            Event.objects.create(properties={'$current_url': '/about'}, distinct_id='person_1', event='$pageview', team=self.team)
+        
+        with freeze_time(second_day.strftime("%Y-%m-%d")):
+            Event.objects.create(properties={'$current_url': '/'}, distinct_id='person_1', event='$pageview', team=self.team)
+            Event.objects.create(properties={'$current_url': '/about'}, distinct_id='person_1', event='$pageview', team=self.team)
+
+        response = self.client.get('/api/paths/?date_from=' + begin_query_range.strftime("%Y-%m-%d")).json()
+
+        self.assertEqual(response[0]['source'], '1_/')
+        self.assertEqual(response[0]['target'], '2_/about')
+        self.assertEqual(response[0]['value'], 2)
+
