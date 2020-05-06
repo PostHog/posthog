eb158e1b4735e32f10750108f0f1e1deb37205c8
Eric <eeoneric@gmail.com>
2020-05-06 15:07:48 -0400

    dummy test

--- posthog/api/test/test_paths.py
+++ posthog/api/test/test_paths.py
@@ -72,17 +72,11 @@ class TestPaths(BaseTest):
     def test_paths_in_window(self):
         Person.objects.create(team=self.team, distinct_ids=['person_1'])
 
-        with freeze_time("2020-04-14T03:25:34.000Z"):
-            Event.objects.create(properties={'$current_url': '/'}, distinct_id='person_1', event='$pageview', team=self.team)
-            Event.objects.create(properties={'$current_url': '/about'}, distinct_id='person_1', event='$pageview', team=self.team)
-        
-        with freeze_time("2020-04-15T03:25:34.000Z"):
-            Event.objects.create(properties={'$current_url': '/'}, distinct_id='person_1', event='$pageview', team=self.team)
-            Event.objects.create(properties={'$current_url': '/about'}, distinct_id='person_1', event='$pageview', team=self.team)
+        Event.objects.create(properties={'$current_url': '/'}, distinct_id='person_1', event='$pageview', team=self.team)
+        Event.objects.create(properties={'$current_url': '/about'}, distinct_id='person_1', event='$pageview', team=self.team)
 
-        response = self.client.get('/api/paths/?date_from=2020-04-13').json()
-        print(response)
+        response = self.client.get('/api/paths/').json()
         self.assertEqual(response[0]['source'], '1_/')
         self.assertEqual(response[0]['target'], '2_/about')
-        self.assertEqual(response[0]['value'], 2)
+        self.assertEqual(response[0]['value'], 1)
 
