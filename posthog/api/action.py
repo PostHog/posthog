0d85b4219ac903dd29baf24642355c97079c6e25
Tim Glaser <tim.glaser@hiberly.com>
2020-05-06 18:06:26 +0100

    Just raise explicitely

--- posthog/api/action.py
+++ posthog/api/action.py
@@ -258,7 +258,9 @@ class ActionViewSet(viewsets.ModelViewSet):
         return cursor.fetchall()
 
     def _stickiness(self, filtered_events: QuerySet, entity: Entity, filter: Filter) -> Dict[str, Any]:
-        range_days = (filter.date_to - filter.date_from).days + 2 if filter.date_from else 0 # filter.date_from should be set
+        if not filter.date_to or not filter.date_from:
+            raise ValueError('_stickiness needs date_to and date_from set')
+        range_days = (filter.date_to - filter.date_from).days + 2
 
         events = filtered_events\
             .filter(self._filter_events(filter, entity))\
