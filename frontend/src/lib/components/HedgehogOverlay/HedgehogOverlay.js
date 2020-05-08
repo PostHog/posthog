7c0f6b7a8056dc4cd5faedf258d87d98efc12e26
Marius Andra <marius.andra@gmail.com>
2020-05-08 15:43:42 +0200

    use a hedgehog with a better license

--- frontend/src/lib/components/HedgehogOverlay/HedgehogOverlay.js
+++ frontend/src/lib/components/HedgehogOverlay/HedgehogOverlay.js
@@ -2,17 +2,9 @@ import './HedgehogOverlay.scss'
 import React from 'react'
 
 import sad from './assets/sad.svg'
-import confused from './assets/confused.svg'
-import dead from './assets/dead.svg'
-import sick from './assets/sick.svg'
-import excited from './assets/excited.svg'
 
 const images = {
     sad,
-    confused,
-    dead,
-    sick,
-    excited,
 }
 
 export function HedgehogOverlay({ type = 'sad' }) {
