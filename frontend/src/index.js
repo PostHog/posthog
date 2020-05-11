d854a935f7c39418a5827046d584f09b21e94cef
Marius Andra <marius.andra@gmail.com>
2020-05-11 16:30:16 +0200

    remove nprogress

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,35 +5,15 @@ import { Provider } from 'react-redux'
 import { resetContext, getContext } from 'kea'
 import { routerPlugin } from 'kea-router'
 import { loadersPlugin } from 'kea-loaders'
-import NProgress from 'nprogress'
 
 import App from './scenes/App'
 import { toast } from 'react-toastify'
-
-let loadingCounter = 0
-let loadingTimeout
-
-const startLoading = () => {
-    loadingTimeout = window.setTimeout(() => NProgress.start(), 500)
-}
-
-const stopLoading = () => {
-    window.clearTimeout(loadingTimeout)
-    NProgress.done()
-}
 
 resetContext({
     plugins: [
         routerPlugin,
         loadersPlugin({
-            onStart() {
-                loadingCounter++ === 0 && startLoading()
-            },
-            onSuccess() {
-                --loadingCounter === 0 && stopLoading()
-            },
             onFailure({ error, reducerKey, actionKey, logic }) {
-                --loadingCounter === 0 && stopLoading()
                 toast.error(
                     <div>
                         <h1>Error loading "{reducerKey}".</h1>
