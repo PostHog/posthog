426d1aa3f34f831c4058e9ede8418c829cc8e02c
Tim Glaser <tim.glaser@hiberly.com>
2020-05-13 17:16:40 +0100

    Closes #737 filter by person properties

--- frontend/src/lib/components/PropertyFilters/PropertyFilter.js
+++ frontend/src/lib/components/PropertyFilters/PropertyFilter.js
@@ -1,50 +1,66 @@
 import React from 'react'
-import Select from 'react-select'
-import { selectStyle, operatorMap } from 'lib/utils'
+import { Select } from 'antd'
+import { operatorMap } from 'lib/utils'
 import { PropertyValue } from './PropertyValue'
 import { useValues, useActions } from 'kea'
 
-const operatorOptions = Object.entries(operatorMap).map(([key, value]) => ({
-    label: value,
-    value: key,
-}))
-
 export function PropertyFilter({ index, endpoint, onComplete, logic }) {
-    const { properties, filters } = useValues(logic)
+    const { eventProperties, personProperties, filters } = useValues(logic)
     const { setFilter } = useActions(logic)
     let { key, value, operator, type } = filters[index]
     return (
         <div className="row" style={{ margin: '0.5rem -15px', minWidth: key ? 700 : 200 }}>
-            {properties && (
-                <div className={key ? 'col-4' : 'col'}>
-                    <Select
-                        options={properties}
-                        value={[{ label: key, value: key }]}
-                        isLoading={!properties}
-                        placeholder="Property key"
-                        onChange={item => setFilter(index, item.value, value, operator, type)}
-                        styles={selectStyle}
-                        autoFocus={!key}
-                        openMenuOnFocus={true}
-                    />
-                </div>
-            )}
+            <div className={key ? 'col-4' : 'col'}>
+                <Select
+                    showSearch
+                    autoFocus={!key}
+                    defaultOpen={!key}
+                    placeholder="Property key"
+                    value={key}
+                    filterOption={(input, option) => option.children?.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    onChange={(_, new_key) => setFilter(index, new_key.value, undefined, operator, new_key.type)}
+                    style={{ width: '100%' }}
+                >
+                    {eventProperties && (
+                        <Select.OptGroup key="Event properties" lable="Event properties">
+                            {eventProperties.map(item => (
+                                <Select.Option key={item.value} value={item.value} type="event">
+                                    {item.value}
+                                </Select.Option>
+                            ))}
+                        </Select.OptGroup>
+                    )}
+                    {personProperties && (
+                        <Select.OptGroup key="User properties" lable="User properties">
+                            {personProperties.map(item => (
+                                <Select.Option key={item.value} value={item.value} type="person">
+                                    {item.value}
+                                </Select.Option>
+                            ))}
+                        </Select.OptGroup>
+                    )}
+                </Select>
+            </div>
 
             {key && (
                 <div className="col-3 pl-0">
                     <Select
-                        options={operatorOptions}
-                        style={{ width: 200 }}
-                        value={[
-                            {
-                                label: operatorMap[operator] || '= equals',
-                                value: operator,
-                            },
-                        ]}
+                        style={{ width: '100%' }}
+                        defaultActiveFirstOption
+                        labelInValue
+                        value={{
+                            value: operator || '=',
+                            label: operatorMap[operator] || '= equals',
+                        }}
                         placeholder="Property key"
-                        onChange={operator => setFilter(index, key, value, operator.value, type)}
-                        styles={selectStyle}
-                    />
+                        onChange={(_, operator) => setFilter(index, key, value, operator.value, type)}
+                    >
+                        {Object.keys(operatorMap).map(operator => (
+                            <Select.Option key={operator} value={operator}>
+                                {operatorMap[operator] || '= equals'}
+                            </Select.Option>
+                        ))}
+                    </Select>
                 </div>
             )}
             {key && (
@@ -54,7 +70,7 @@ export function PropertyFilter({ index, endpoint, onComplete, logic }) {
                         key={key}
                         propertyKey={key}
                         value={value}
-                        onSet={(key, value) => {
+                        onSet={value => {
                             onComplete()
                             setFilter(index, key, value, operator, type)
                         }}
