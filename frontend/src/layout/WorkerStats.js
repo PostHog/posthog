e05f3edc67dfc08895d121c500a0f0c9a85bed9b
Tim Glaser <tim.glaser@hiberly.com>
2020-05-13 20:23:20 +0100

    Update worker stats modal

--- frontend/src/layout/WorkerStats.js
+++ frontend/src/layout/WorkerStats.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import api from './../lib/api'
-import { Modal } from 'lib/components/Modal'
+import { Modal, Button } from 'antd'
 
 export function WorkerStats() {
     const [heartbeat, setHeartbeat] = useState(null)
@@ -34,34 +34,32 @@ export function WorkerStats() {
                     ⚠️ Configuration Error
                 </span>
             ) : null}
-            {showErrorModal ? (
-                <Modal onDismiss={() => setShowErrorModal(false)}>
-                    <h2>Configuration Issue</h2>
-                    <p>
-                        Starting with <strong>PostHog 1.1.0</strong>, every installation <em>requires</em> a background
-                        worker to function properly.
-                    </p>
-                    <p>
-                        These workers will make PostHog a lot faster and will pave the road for other goodies, such as
-                        slack integration, scheduled reports and free pizzas for everyone!
-                    </p>
-                    <p>
-                        While workers are <em>currently</em> still optional, we <strong>strongly</strong> recommend you
-                        already enable them to make the next upgrade painless.
-                    </p>
-                    <p>
-                        Please{' '}
-                        <a
-                            href="https://docs.posthog.com/#/upgrading-posthog?id=upgrading-from-before-1011"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            see the documentation
-                        </a>{' '}
-                        for more information
-                    </p>
-                </Modal>
-            ) : null}
+            <Modal visible={showErrorModal} footer={<Button onClick={() => setShowErrorModal(false)}>Close</Button>}>
+                <h2>Configuration Issue</h2>
+                <p>
+                    Starting with <strong>PostHog 1.1.0</strong>, every installation <em>requires</em> a background
+                    worker to function properly.
+                </p>
+                <p>
+                    We can't seem to reach your worker. There could be a few reasons for this.
+                    <ol>
+                        <li>Your Redis server wasn't started or is down</li>
+                        <li>Your worker wasn't started or is down</li>
+                        <li>Your web server has trouble reaching Redis</li>
+                    </ol>
+                </p>
+                <p>
+                    Please{' '}
+                    <a
+                        href="https://docs.posthog.com/#/upgrading-posthog?id=upgrading-from-before-1011"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        see the documentation
+                    </a>{' '}
+                    for more information
+                </p>
+            </Modal>
         </span>
     ) : null
 }
