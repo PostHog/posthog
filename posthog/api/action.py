e998a98b9c21c8be64b523a30c4720286dd34cc5
Eric <eeoneric@gmail.com>
2020-05-14 11:35:12 -0400

    correct all types

--- posthog/api/action.py
+++ posthog/api/action.py
@@ -331,8 +331,8 @@ class ActionViewSet(viewsets.ModelViewSet):
             'count': sum(data)
         }
 
-    def _breakdown_label(self, entity: Entity, value: Union[str, int]) -> Dict[str, Optional[str]]:
-        ret_dict: Dict[str, Optional[str]] = {}
+    def _breakdown_label(self, entity: Entity, value: Union[str, int]) -> Dict[str, Optional[Union[str, int]]]:
+        ret_dict: Dict[str, Optional[Union[str, int]]] = {}
         if not value or not isinstance(value, str) or 'cohort_' not in value:
             ret_dict['label'] = '{} - {}'.format(entity.name, value if value and value != "None" and value != "nan" else 'Other') 
             ret_dict['breakdown_value'] = value if value and not pd.isna(value) else None
