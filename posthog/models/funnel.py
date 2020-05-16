fe1bd320225a6958e49f8ada158f62f9fcc07398
James Greenhill <jams@uber.com>
2020-05-16 12:39:44 -0700

    lateral joins for funnel render

--- posthog/models/funnel.py
+++ posthog/models/funnel.py
@@ -1,8 +1,11 @@
+from collections import namedtuple
+
 from django.db import models
 from django.contrib.postgres.fields import JSONField, ArrayField
-
+from django.db import connection
 from django.db.models import (
     Exists,
+    Min,
     OuterRef,
     Q,
     Subquery,
@@ -12,7 +15,8 @@ from django.db.models import (
     QuerySet,
 )
 from typing import List, Dict, Any, Optional
-import datetime
+
+from psycopg2 import sql
 
 from .event import Event
 from .person import PersonDistinctId
@@ -83,6 +87,33 @@ class Funnel(models.Model):
             )
         return annotations
 
+    def _gen_lateral_bodies(
+        self,
+        team_id,
+        filter
+    ):
+        annotations = {}
+        for index, step in enumerate(filter.entities):
+            filter_key = (
+                "event"
+                if step.type == TREND_FILTER_TYPE_EVENTS
+                else "action__pk"
+            )
+            annotations["step_{}".format(index)] = Event.objects.values("distinct_id")\
+                .annotate(step_ts=Min("timestamp"))\
+                .filter(
+                    filter.date_filter_Q,
+                    **{filter_key: step.id},
+                    team_id=team_id,
+                    **(
+                        {"distinct_id": "{prev_step_distinct_id}"}
+                        if index > 0
+                        else {}
+                    ),
+                ).filter(filter.properties_to_Q())\
+                .filter(step.properties_to_Q())
+        return annotations
+
     def _serialize_step(
         self, step: Entity, people: Optional[List[int]] = None
     ) -> Dict[str, Any]:
@@ -99,21 +130,84 @@ class Funnel(models.Model):
             "type": step.type,
         }
 
+    def namedtuplefetchall(self, cursor):
+        "Return all rows from a cursor as a namedtuple"
+        desc = cursor.description
+        nt_result = namedtuple('Result', [col[0] for col in desc])
+        return [nt_result(*row) for row in cursor.fetchall()]
+
+    def _build_query(self, query_bodies: dict):
+        ON_TRUE = "ON TRUE"
+        LEFT_JOIN_LATERAL = "LEFT JOIN LATERAL"
+        QUERY_HEADER = "SELECT {people}, {step}.distinct_id, {fields} FROM "
+        LAT_JOIN_BODY = """({query}) {step} {on_true} {join}"""
+        PERSON_FIELDS = [[sql.Identifier("posthog_person"), sql.Identifier("id")],
+                         [sql.Identifier("posthog_person"), sql.Identifier("created_at")],
+                         [sql.Identifier("posthog_person"), sql.Identifier("team_id")],
+                         [sql.Identifier("posthog_person"), sql.Identifier("properties")],
+                         [sql.Identifier("posthog_person"), sql.Identifier("is_user_id")]]
+        QUERY_FOOTER = sql.SQL("""
+            JOIN posthog_persondistinctid pdi ON pdi.distinct_id = {step0}.distinct_id
+            JOIN posthog_person ON pdi.id = posthog_person.id
+            WHERE {step0}.distinct_id IS NOT NULL""")
+
+        person_fields = sql.SQL(",").join([sql.SQL(".").join(col) for col in PERSON_FIELDS])
+
+        steps = [sql.Identifier(step) for step, query in query_bodies.items()]
+        select_steps = [sql.Composed([step, sql.SQL("."), sql.Identifier("step_ts"), sql.SQL(" as "), step]) for step in steps]
+        lateral_joins = []
+        with connection.cursor() as cursor:
+            query_bodies = {step: cursor.mogrify(*q.query.sql_with_params()) for step, q in query_bodies.items()}
+        query = sql.SQL(QUERY_HEADER).format(
+            step=steps[0],
+            fields=sql.SQL(',').join(select_steps),
+            people=person_fields
+        )
+        i = 0
+        for step, qb in query_bodies.items():
+            q = sql.SQL(qb.decode('utf-8').replace("'{", "{").replace("}'", "}"))
+            if i == 0:
+                base_body = sql.SQL(LAT_JOIN_BODY).format(
+                    query=q,
+                    step=sql.SQL(step),
+                    on_true=sql.SQL(""),
+                    join=sql.SQL(LEFT_JOIN_LATERAL)
+                )
+                lateral_joins.append(base_body)
+            elif i == len(query_bodies) - 1:
+                q = q.format(prev_step_distinct_id=sql.Composed([steps[i-1], sql.SQL("."), sql.Identifier("distinct_id")]))
+                base_body = sql.SQL(LAT_JOIN_BODY).format(
+                    query=q,
+                    step=sql.SQL(step),
+                    on_true=sql.SQL(ON_TRUE),
+                    join=sql.SQL("")
+                )
+                lateral_joins.append(base_body)
+            else:
+                q = q.format(prev_step_distinct_id=sql.Composed([steps[i-1], sql.SQL("."), sql.Identifier("distinct_id")]))
+                base_body = sql.SQL(LAT_JOIN_BODY).format(
+                    query=q,
+                    step=sql.SQL(step),
+                    on_true=sql.SQL(ON_TRUE),
+                    join=sql.SQL(LEFT_JOIN_LATERAL)
+                )
+                lateral_joins.append(base_body)
+            i += 1
+        query_footer = QUERY_FOOTER.format(
+            step0=steps[0]
+        )
+        query = query + sql.SQL(" ").join(lateral_joins) + query_footer
+        return query
+
     def get_steps(self) -> List[Dict[str, Any]]:
         filter = Filter(data=self.filters)
-        people = (
-            Person.objects.all()
-            .filter(team_id=self.team_id, persondistinctid__distinct_id__isnull=False)
-            .annotate(
-                **self._annotate_steps(
-                    team_id=self.team_id,
-                    filter=filter
-                )
-            )
-            .filter(step_0__isnull=False)
-            .distinct("pk")
-        )
-
+        with connection.cursor() as cursor:
+            qstring = self._build_query(self._gen_lateral_bodies(
+                team_id=self.team_id,
+                filter=filter)).as_string(cursor.connection)
+            print(qstring)
+            cursor.execute(qstring)
+            people = self.namedtuplefetchall(cursor)
         steps = []
         for index, funnel_step in enumerate(filter.entities):
             relevant_people = [
