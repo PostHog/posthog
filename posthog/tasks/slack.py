999a1847b082b713c8b52e9fd6799ede9974e1e5
Marius Andra <marius.andra@gmail.com>
2020-05-18 14:17:37 +0200

    different webhook message format for ms teams

--- posthog/tasks/slack.py
+++ posthog/tasks/slack.py
@@ -19,27 +19,44 @@ def post_event_to_slack(event_id: int, site_url: str) -> None:
         except:
             user_name = event.distinct_id
 
-        user_markdown = "<{}/person/{}|{}>".format(site_url, event.distinct_id, user_name)
+        webhook_type = 'teams'
+        if "slack.com" in team.slack_incoming_webhook:
+            webhook_type = 'slack'
+
+        if webhook_type == 'slack':
+            user_markdown = "<{}/person/{}|{}>".format(site_url, event.distinct_id, user_name)
+        else:
+            user_markdown = "[{}]({}/person/{})".format(user_name, site_url, event.distinct_id)
 
         actions = [action for action in event.action_set.all() if action.post_to_slack]
 
         if actions:
-            action_links = ', '.join('"<{}/action/{}|{}>"'.format(site_url, action.id, action.name) for action in actions)
+            if webhook_type == 'slack':
+                action_links = ', '.join('"<{}/action/{}|{}>"'.format(site_url, action.id, action.name) for action in actions)
+            else:
+                action_links = ', '.join('"[{}]({}/action/{})"'.format(action.name, site_url, action.id) for action in actions)
+
             action_names = ', '.join('"{}"'.format(action.name) for action in actions)
 
             actions_markdown = "Action{} {}".format("" if len(actions) == 1 else "s", action_links)
             actions_plain = "Action{} {}".format("" if len(actions) == 1 else "s", action_names)
 
-            message = {
-                "text": "{} triggered by user {}".format(actions_plain, user_name),
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "{} triggered by user {}".format(actions_markdown, user_markdown)
+            if webhook_type == 'slack':
+                message = {
+                    "text": "{} triggered by user {}".format(actions_plain, user_name),
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": "{} triggered by user {}".format(actions_markdown, user_markdown)
+                            }
                         }
-                    }
-                ]
-            }
+                    ]
+                }
+            else:
+                message = {
+                    "text": "{} triggered by user {}".format(actions_markdown, user_markdown),
+                }
+
             requests.post(team.slack_incoming_webhook, verify=False, json=message)
