bfbc1f146a769fefceb58c335f67ecf8836a10cd
Tim Glaser <tim.glaser@hiberly.com>
2020-05-18 19:42:32 +0100

    Cache team id

--- posthog/api/capture.py
+++ posthog/api/capture.py
@@ -13,6 +13,7 @@ import json
 import base64
 import gzip
 
+TEAM_ID_CACHE: Dict[str, int] = {}
 
 def cors_response(request, response):
     if not request.META.get('HTTP_ORIGIN'):
@@ -58,12 +59,12 @@ def _datetime_from_seconds_or_millis(timestamp: str) -> datetime:
 
 
 def _get_sent_at(data, request) -> Optional[datetime]:
-    if isinstance(data, dict) and data.get('sent_at'):  # posthog-android, posthog-ios
+    if request.GET.get('_'):  # posthog-js
+        sent_at = request.GET['_']
+    elif isinstance(data, dict) and data.get('sent_at'):  # posthog-android, posthog-ios
         sent_at = data['sent_at']
     elif request.POST.get('sent_at'):  # when urlencoded body and not JSON (in some test)
         sent_at = request.POST['sent_at']
-    elif request.GET.get('_'):  # posthog-js
-        sent_at = request.GET['_']
     else:
         return None
 
@@ -73,25 +74,25 @@ def _get_sent_at(data, request) -> Optional[datetime]:
     return parser.isoparse(sent_at)
 
 
-def _get_token(data, request) -> Union[str, bool]:
+def _get_token(data, request) -> Optional[str]:
     if request.POST.get('api_key'):
         return request.POST['api_key']
     if isinstance(data, list) and len(data) > 0:
         return data[0]['properties']['token'] # Mixpanel Swift SDK
-    if data.get('api_key'):
-        return data['api_key'] # server-side libraries like posthog-python and posthog-ruby
     if data.get('$token'):
         return data['$token'] # JS identify call
+    if data.get('api_key'):
+        return data['api_key'] # server-side libraries like posthog-python and posthog-ruby
     if data.get('properties') and data['properties'].get('token'):
         return data['properties']['token'] # JS capture call
-    return False
+    return None
 
 def _get_distinct_id(data: Dict[str, Any]) -> str:
     try:
-        return str(data['properties']['distinct_id'])
+        return str(data['$distinct_id'])
     except KeyError:
         try:
-            return str(data['$distinct_id'])
+            return str(data['properties']['distinct_id'])
         except KeyError:
             return str(data['distinct_id'])
 
@@ -106,10 +107,14 @@ def get_event(request):
     if not token:
         return cors_response(request, JsonResponse({'code': 'validation', 'message': "No api_key set. You can find your API key in the /setup page in posthog"}, status=400))
 
-    try:
-        team = Team.objects.only('pk').get(api_token=token)
-    except Team.DoesNotExist:
-        return cors_response(request, JsonResponse({'code': 'validation', 'message': "API key is incorrect. You can find your API key in the /setup page in PostHog."}, status=400))
+    team_id = TEAM_ID_CACHE.get(token)
+    if not team_id:
+        try:
+            team_id = Team.objects.only('pk').get(api_token=token).pk
+            if team_id:
+                TEAM_ID_CACHE[token] = team_id
+        except Team.DoesNotExist:
+            return cors_response(request, JsonResponse({'code': 'validation', 'message': "API key is incorrect. You can find your API key in the /setup page in PostHog."}, status=400))
 
     if isinstance(data, dict):
         if data.get('batch'): # posthog-python and posthog-ruby
@@ -133,7 +138,7 @@ def get_event(request):
             ip=get_ip_address(request),
             site_url=request.build_absolute_uri('/')[:-1],
             data=event,
-            team_id=team.pk,
+            team_id=team_id,
             now=now,
             sent_at=sent_at,
         )
