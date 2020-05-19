223f400e050b1b052182a9f219487577a1459d40
Eric Duong <eeoneric@gmail.com>
2020-05-19 14:04:32 -0400

    use antd for user dropdown (#801)

--- frontend/src/layout/User.js
+++ frontend/src/layout/User.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { useValues } from 'kea'
-import { Dropdown } from 'lib/components/Dropdown'
+import { Dropdown, Menu } from 'antd'
 import { userLogic } from 'scenes/userLogic'
+
+const options = (
+    <Menu>
+        <Menu.Item key="0">
+            <a href="/logout">Logout</a>
+        </Menu.Item>
+    </Menu>
+)
 
 export function User() {
     const { user } = useValues(userLogic)
 
     return (
-        <span>
-            <Dropdown title={user.email} buttonClassName="btn btn-sm btn-light">
-                <a className={'dropdown-item'} href="/logout">
-                    Logout
-                </a>
-            </Dropdown>
-        </span>
+        <Dropdown overlay={options}>
+            <span className="btn btn-sm btn-light">{user.email}</span>
+        </Dropdown>
     )
 }
