192255e7ac8ece843fbc2e38ed76949938ea8668
Eric <eeoneric@gmail.com>
2020-05-19 19:52:11 -0400

    uncomment

--- cypress/integration/actions.js
+++ cypress/integration/actions.js
@@ -4,14 +4,14 @@ describe('Actions', () => {
         cy.get('[data-attr=menu-item-actions]').click()
     })
 
-    // it('Actions loaded', () => {
-    //     cy.get('h1').should('contain', 'Actions')
-    // })
+    it('Actions loaded', () => {
+        cy.get('h1').should('contain', 'Actions')
+    })
 
-    // it('Go to new action screen', () => {
-    //     cy.get('[data-attr=create-action]').click()
-    //     cy.get('h1').should('contain', 'New action')
-    // })
+    it('Go to new action screen', () => {
+        cy.get('[data-attr=create-action]').click()
+        cy.get('h1').should('contain', 'New action')
+    })
 
     it('Create action', () => {
         cy.get('[data-attr=create-action]').click()
