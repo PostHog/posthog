917e76ae39c2702f0769d6a6eb52efe5fcf547d6
Eric <eeoneric@gmail.com>
2020-05-21 12:41:24 -0400

    remove unnecessary

--- cypress/integration/events.js
+++ cypress/integration/events.js
@@ -19,9 +19,6 @@ describe('Events', () => {
     })
 
     it('Apply 1 overall filter', () => {
-        cy.get('[data-attr=menu-item-live-actions]').click()
-        cy.get('[data-attr=menu-item-events]').click()
-
         cy.get('[data-attr=new-prop-filter-EventsTable]').click()
         cy.get('[data-attr=prop-filter-event-0]').click()
         cy.get('[data-attr=prop-val]').click()
