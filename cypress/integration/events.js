4738a85671e39cc6e2c8752a99bbd295e4c5e4f4
Eric <eeoneric@gmail.com>
2020-05-21 12:15:47 -0400

    uncomment

--- cypress/integration/events.js
+++ cypress/integration/events.js
@@ -3,20 +3,20 @@ describe('Events', () => {
         cy.get('[data-attr=menu-item-events]').click()
     })
 
-    // it('Events loaded', () => {
-    //     cy.get('[data-attr=events-table').should('exist')
-    // })
+    it('Events loaded', () => {
+        cy.get('[data-attr=events-table').should('exist')
+    })
 
-    // it('Click on an event', () => {
-    //     cy.get('[data-attr=event-name-0').click()
-    //     cy.get('[data-attr=event-details').should('exist')
-    // })
+    it('Click on an event', () => {
+        cy.get('[data-attr=event-name-0').click()
+        cy.get('[data-attr=event-details').should('exist')
+    })
 
-    // it('All events route works', () => {
-    //     cy.get('[data-attr=menu-item-all-events]').click()
+    it('All events route works', () => {
+        cy.get('[data-attr=menu-item-all-events]').click()
 
-    //     cy.get('[data-attr=events-table').should('exist')
-    // })
+        cy.get('[data-attr=events-table').should('exist')
+    })
 
     it('Apply 1 overall filter', () => {
         cy.get('[data-attr=menu-item-live-actions]').click()
