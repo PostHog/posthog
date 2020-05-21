ca4963e441840cff85f5b0a11b754db5269be013
Tim Glaser <tim.glaser@hiberly.com>
2020-05-21 18:47:19 +0100

    Fix nits

--- frontend/src/lib/components/AppEditorLink/AppEditorLink.js
+++ frontend/src/lib/components/AppEditorLink/AppEditorLink.js
@@ -4,7 +4,7 @@ import { useValues } from 'kea'
 import { EditAppUrls } from './EditAppUrls'
 import { appEditorUrl } from './utils'
 import { userLogic } from 'scenes/userLogic'
-import { Modal } from 'antd'
+import { Modal, Button } from 'antd'
 
 export function AppEditorLink({ actionId, style, className, children }) {
     const [modalOpen, setModalOpen] = useState(false)
@@ -28,10 +28,11 @@ export function AppEditorLink({ actionId, style, className, children }) {
                 visible={modalOpen}
                 title={
                     actionId
-                        ? 'Where do you want to edit this action?'
-                        : 'On which domain do you want to create an action?'
+                        ? 'Choose the domain on which to edit this action'
+                        : 'Choose the domain on which to create this action'
                 }
-                onDismiss={() => setModalOpen(false)}
+                footer={<Button onClick={() => setModalOpen(false)}>Close</Button>}
+                onCancel={() => setModalOpen(false)}
             >
                 <EditAppUrls actionId={actionId} allowNavigation={true} dismissModal={() => setModalOpen(false)} />
             </Modal>
