8aba7a5398a44673706b5442c7d5a3f78dd8bd37
Eric <eeoneric@gmail.com>
2020-05-21 09:56:22 -0400

    uncomment test for single funnel

--- cypress/integration/funnels.js
+++ cypress/integration/funnels.js
@@ -17,20 +17,19 @@ describe('Funnels', () => {
         cy.get('[data-attr=funnel-editor]').should('exist')
     })
 
-    // TODO: fix failing test for 1 action funnel
-    // it('Add 1 action to funnel', () => {
-    //     cy.get('[data-attr=create-funnel]').click()
-    //     cy.get('[data-attr=funnel-editor]').should('exist')
+    it('Add 1 action to funnel', () => {
+        cy.get('[data-attr=create-funnel]').click()
+        cy.get('[data-attr=funnel-editor]').should('exist')
 
-    //     cy.get('[data-attr=edit-funnel-input]').type('Test funnel')
-    //     cy.get('[data-attr=add-action-event-button]').click()
-    //     cy.get('[data-attr=trend-element-subject-0]').click()
-    //     cy.contains('Pageviews').click()
+        cy.get('[data-attr=edit-funnel-input]').type('Test funnel')
+        cy.get('[data-attr=add-action-event-button]').click()
+        cy.get('[data-attr=trend-element-subject-0]').click()
+        cy.contains('Pageviews').click()
 
-    //     cy.get('[data-attr=save-funnel-button]').click()
+        cy.get('[data-attr=save-funnel-button]').click()
 
-    //     cy.get('[data-attr=funnel-viz]').should('exist')
-    // })
+        cy.get('[data-attr=funnel-viz]').should('exist')
+    })
 
     it('Add 2 actions to funnel', () => {
         cy.get('[data-attr=create-funnel]').click()
