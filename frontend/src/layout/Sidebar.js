b68f7a6aae83193135b9b9d73be964c11a429292
Marius Andra <marius.andra@gmail.com>
2020-05-27 13:01:26 +0200

    787 mobile floating sidebar (#839)
    * floating collapsible sidebar
    
    * close sidebar with ESC
    
    * don't close sidebar in full screen mode
    
    * sidebar is static also in full reso
    
    * get rid of tooltips when collapsing menu on mobile
    
    * update sidebar open/closed default value based on resolution
    
    * antd modals come on top of the sidebar, now below

--- frontend/src/layout/Sidebar.js
+++ frontend/src/layout/Sidebar.js
@@ -24,6 +24,7 @@ import { sceneLogic } from 'scenes/sceneLogic'
 import { dashboardsModel } from '~/models/dashboardsModel'
 import whiteLogo from './_assets/white-logo.svg'
 import { triggerResizeAfterADelay } from 'lib/utils'
+import { useEscapeKey } from 'lib/hooks/useEscapeKey'
 
 const itemStyle = { display: 'flex', alignItems: 'center' }
 
@@ -31,7 +32,7 @@ function Logo() {
     return (
         <div
             className="row logo-row d-flex align-items-center justify-content-center"
-            style={{ margin: 16, height: 42 }}
+            style={{ margin: 16, height: 42, whiteSpace: 'nowrap', width: 168, overflow: 'hidden' }}
         >
             <img className="logo posthog-logo" src={whiteLogo} style={{ maxHeight: '100%' }} />
             <div className="posthog-title">PostHog</div>
@@ -55,13 +56,19 @@ const submenuOverride = {
     cohorts: 'people',
 }
 
-export default function Sidebar(props) {
+export function Sidebar({ user, sidebarCollapsed, setSidebarCollapsed }) {
     const [inviteModalOpen, setInviteModalOpen] = useState(false)
-
+    const collapseSidebar = () => {
+        if (!sidebarCollapsed && window.innerWidth <= 991) {
+            setSidebarCollapsed(true)
+        }
+    }
     const { scene, loadingScene } = useValues(sceneLogic)
     const { location } = useValues(router)
     const { push } = useActions(router)
     const { dashboards, pinnedDashboards } = useValues(dashboardsModel)
+
+    useEscapeKey(collapseSidebar, [sidebarCollapsed])
 
     let activeScene = sceneOverride[loadingScene || scene] || loadingScene || scene
     const openSubmenu = submenuOverride[activeScene] || activeScene
@@ -75,118 +82,140 @@ export default function Sidebar(props) {
     }
 
     return (
-        <Layout.Sider breakpoint="lg" collapsedWidth="0" className="bg-dark" onCollapse={triggerResizeAfterADelay}>
-            <Menu
-                className="h-100 bg-dark"
-                theme="dark"
-                selectedKeys={[activeScene]}
-                openKeys={[openSubmenu]}
-                mode="inline"
+        <>
+            <div
+                className={`sidebar-responsive-overlay${!sidebarCollapsed ? ' open' : ''}`}
+                onClick={collapseSidebar}
+            />
+            <Layout.Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                className="bg-dark"
+                collapsed={sidebarCollapsed}
+                onCollapse={sidebarCollapsed => {
+                    setSidebarCollapsed(sidebarCollapsed)
+                    triggerResizeAfterADelay()
+                }}
             >
-                <Logo />
+                <Menu
+                    className="h-100 bg-dark"
+                    theme="dark"
+                    selectedKeys={[activeScene]}
+                    openKeys={[openSubmenu]}
+                    mode="inline"
+                >
+                    <Logo />
 
-                {pinnedDashboards.map((dashboard, index) => (
-                    <Menu.Item
-                        key={`dashboard-${dashboard.id}`}
-                        style={itemStyle}
-                        data-attr={'pinned-dashboard-' + index}
+                    {pinnedDashboards.map((dashboard, index) => (
+                        <Menu.Item
+                            key={`dashboard-${dashboard.id}`}
+                            style={itemStyle}
+                            data-attr={'pinned-dashboard-' + index}
+                            title=""
+                        >
+                            <LineChartOutlined />
+                            <span className="sidebar-label">{dashboard.name}</span>
+                            <Link to={`/dashboard/${dashboard.id}`} onClick={collapseSidebar} />
+                        </Menu.Item>
+                    ))}
+
+                    <Menu.Item key="dashboards" style={itemStyle} data-attr="menu-item-dashboards" title="">
+                        <FundOutlined />
+                        <span className="sidebar-label">Dashboards</span>
+                        <Link to="/dashboard" onClick={collapseSidebar} />
+                    </Menu.Item>
+
+                    {pinnedDashboards.length > 0 ? <Menu.Divider /> : null}
+
+                    <Menu.Item key="trends" style={itemStyle} data-attr="menu-item-trends" title="">
+                        <RiseOutlined />
+                        <span className="sidebar-label">{'Trends'}</span>
+                        <Link to={'/trends'} onClick={collapseSidebar} />
+                    </Menu.Item>
+
+                    <Menu.SubMenu
+                        key="events"
+                        title={
+                            <span style={itemStyle} data-attr="menu-item-events">
+                                <ContainerOutlined />
+                                <span className="sidebar-label">{'Events'}</span>
+                            </span>
+                        }
+                        onTitleClick={() => {
+                            collapseSidebar()
+                            location.pathname !== '/events' && push('/events')
+                        }}
                     >
-                        <LineChartOutlined />
-                        <span className="sidebar-label">{dashboard.name}</span>
-                        <Link to={`/dashboard/${dashboard.id}`} />
-                    </Menu.Item>
-                ))}
-
-                <Menu.Item key="dashboards" style={itemStyle} data-attr="menu-item-dashboards">
-                    <FundOutlined />
-                    <span className="sidebar-label">Dashboards</span>
-                    <Link to="/dashboard" />
-                </Menu.Item>
-
-                {pinnedDashboards.length > 0 ? <Menu.Divider /> : null}
-
-                <Menu.Item key="trends" style={itemStyle} data-attr="menu-item-trends">
-                    <RiseOutlined />
-                    <span className="sidebar-label">{'Trends'}</span>
-                    <Link to={'/trends'} />
-                </Menu.Item>
-
-                <Menu.SubMenu
-                    key="events"
-                    title={
-                        <span style={itemStyle} data-attr="menu-item-events">
+                        <Menu.Item key="events" style={itemStyle} data-attr="menu-item-all-events">
                             <ContainerOutlined />
-                            <span className="sidebar-label">{'Events'}</span>
-                        </span>
-                    }
-                    onTitleClick={() => (location.pathname !== '/events' ? push('/events') : null)}
-                >
-                    <Menu.Item key="events" style={itemStyle} data-attr="menu-item-all-events">
-                        <ContainerOutlined />
-                        <span className="sidebar-label">{'All Events'}</span>
-                        <Link to={'/events'} />
-                    </Menu.Item>
-                    <Menu.Item key="actions" style={itemStyle} data-attr="menu-item-actions">
-                        <AimOutlined />
-                        <span className="sidebar-label">{'Actions'}</span>
-                        <Link to={'/actions'} />
-                    </Menu.Item>
-                    <Menu.Item key="liveActions" style={itemStyle} data-attr="menu-item-live-actions">
-                        <SyncOutlined />
-                        <span className="sidebar-label">{'Live Actions'}</span>
-                        <Link to={'/actions/live'} />
-                    </Menu.Item>
-                </Menu.SubMenu>
-                <Menu.SubMenu
-                    key="people"
-                    title={
-                        <span style={itemStyle} data-attr="menu-item-people">
+                            <span className="sidebar-label">{'All Events'}</span>
+                            <Link to={'/events'} onClick={collapseSidebar} />
+                        </Menu.Item>
+                        <Menu.Item key="actions" style={itemStyle} data-attr="menu-item-actions">
+                            <AimOutlined />
+                            <span className="sidebar-label">{'Actions'}</span>
+                            <Link to={'/actions'} onClick={collapseSidebar} />
+                        </Menu.Item>
+                        <Menu.Item key="liveActions" style={itemStyle} data-attr="menu-item-live-actions">
+                            <SyncOutlined />
+                            <span className="sidebar-label">{'Live Actions'}</span>
+                            <Link to={'/actions/live'} onClick={collapseSidebar} />
+                        </Menu.Item>
+                    </Menu.SubMenu>
+                    <Menu.SubMenu
+                        key="people"
+                        title={
+                            <span style={itemStyle} data-attr="menu-item-people">
+                                <UserOutlined />
+                                <span className="sidebar-label">{'People'}</span>
+                            </span>
+                        }
+                        onTitleClick={() => {
+                            collapseSidebar()
+                            location.pathname !== '/people' && push('/people')
+                        }}
+                    >
+                        <Menu.Item key="people" style={itemStyle} data-attr="menu-item-all-people">
                             <UserOutlined />
-                            <span className="sidebar-label">{'People'}</span>
-                        </span>
-                    }
-                    onTitleClick={() => (location.pathname !== '/people' ? push('/people') : null)}
-                >
-                    <Menu.Item key="people" style={itemStyle} data-attr="menu-item-all-people">
-                        <UserOutlined />
-                        <span className="sidebar-label">{'All Users'}</span>
-                        <Link to={'/people'} />
+                            <span className="sidebar-label">{'All Users'}</span>
+                            <Link to={'/people'} onClick={collapseSidebar} />
+                        </Menu.Item>
+                        <Menu.Item key="cohorts" style={itemStyle} data-attr="menu-item-cohorts">
+                            <UsergroupAddOutlined />
+                            <span className="sidebar-label">{'Cohorts'}</span>
+                            <Link to={'/people/cohorts'} onClick={collapseSidebar} />
+                        </Menu.Item>
+                    </Menu.SubMenu>
+                    <Menu.Item key="funnels" style={itemStyle} data-attr="menu-item-funnels">
+                        <FunnelPlotOutlined />
+                        <span className="sidebar-label">{'Funnels'}</span>
+                        <Link to={'/funnel'} onClick={collapseSidebar} />
                     </Menu.Item>
-                    <Menu.Item key="cohorts" style={itemStyle} data-attr="menu-item-cohorts">
-                        <UsergroupAddOutlined />
-                        <span className="sidebar-label">{'Cohorts'}</span>
-                        <Link to={'/people/cohorts'} />
+                    <Menu.Item key="paths" style={itemStyle} data-attr="menu-item-paths">
+                        <ForkOutlined />
+                        <span className="sidebar-label">{'Paths'}</span>
+                        <Link to={'/paths'} onClick={collapseSidebar} />
                     </Menu.Item>
-                </Menu.SubMenu>
-                <Menu.Item key="funnels" style={itemStyle} data-attr="menu-item-funnels">
-                    <FunnelPlotOutlined />
-                    <span className="sidebar-label">{'Funnels'}</span>
-                    <Link to={'/funnel'} />
-                </Menu.Item>
-                <Menu.Item key="paths" style={itemStyle} data-attr="menu-item-paths">
-                    <ForkOutlined />
-                    <span className="sidebar-label">{'Paths'}</span>
-                    <Link to={'/paths'} />
-                </Menu.Item>
-                <Menu.Item key="setup" style={itemStyle} data-attr="menu-item-setup">
-                    <SettingOutlined />
-                    <span className="sidebar-label">{'Setup'}</span>
-                    <Link to={'/setup'} />
-                </Menu.Item>
-                <Menu.Item
-                    key="invite"
-                    style={itemStyle}
-                    onClick={() => setInviteModalOpen(true)}
-                    data-attr="menu-item-invite-team"
-                >
-                    <PlusOutlined />
-                    <span className="sidebar-label">{'Invite your team'}</span>
-                </Menu.Item>
-            </Menu>
+                    <Menu.Item key="setup" style={itemStyle} data-attr="menu-item-setup">
+                        <SettingOutlined />
+                        <span className="sidebar-label">{'Setup'}</span>
+                        <Link to={'/setup'} onClick={collapseSidebar} />
+                    </Menu.Item>
+                    <Menu.Item
+                        key="invite"
+                        style={itemStyle}
+                        onClick={() => setInviteModalOpen(true)}
+                        data-attr="menu-item-invite-team"
+                    >
+                        <PlusOutlined />
+                        <span className="sidebar-label">{'Invite your team'}</span>
+                    </Menu.Item>
+                </Menu>
 
-            <Modal visible={inviteModalOpen} footer={null} onCancel={() => setInviteModalOpen(false)}>
-                <InviteTeam user={props.user} />
-            </Modal>
-        </Layout.Sider>
+                <Modal visible={inviteModalOpen} footer={null} onCancel={() => setInviteModalOpen(false)}>
+                    <InviteTeam user={user} />
+                </Modal>
+            </Layout.Sider>
+        </>
     )
 }
