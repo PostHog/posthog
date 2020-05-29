88923b9634944851827d13f73a8904d8f66a011f
Eric <eeoneric@gmail.com>
2020-05-29 08:56:30 -0400

    reduce repeat

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -3,5 +3,5 @@ export const ACTIONS_LINE_GRAPH_CUMULATIVE = 'ActionsLineGraphCumulative'
 
 export const STICKINESS = 'Stickiness'
 
-export const LINEAR_CHART_LABEL = 'Linear Chart'
-export const CUMULATIVE_CHART_LABEL = 'Cumulative Chart'
+export const LINEAR_CHART_LABEL = 'Linear'
+export const CUMULATIVE_CHART_LABEL = 'Cumulative'
