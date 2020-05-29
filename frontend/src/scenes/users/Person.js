1d21b4c97a3e253e5af6d8d92ce7c6b38df33f54
Tim Glaser <tim.glaser@hiberly.com>
2020-05-29 12:29:40 +0100

    Move events tables to antd

--- frontend/src/scenes/users/Person.js
+++ frontend/src/scenes/users/Person.js
@@ -29,21 +29,12 @@ export function Person({ _: distinctId, id }) {
             </Button>
             <h1>{person.name}</h1>
             <div style={{ maxWidth: 750 }}>
-                <PropertiesTable properties={person.properties} />
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <td>Distinct IDs</td>
-                            <td>
-                                {person.distinct_ids.map(distinct_id => (
-                                    <pre style={{ margin: 0 }} key={distinct_id}>
-                                        {distinct_id}
-                                    </pre>
-                                ))}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <PropertiesTable
+                    properties={{
+                        ...person.properties,
+                        distinct_id: person.distinct_ids,
+                    }}
+                />
                 <small>
                     <a
                         href="https://posthog.com/docs/integrations/js-integration#identifying-users"
