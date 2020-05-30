be7a775f1c9dada8829e142426b75d9ef8ded644
Eric <eeoneric@gmail.com>
2020-05-30 14:18:34 -0400

    refactor invite team

--- frontend/src/lib/components/InviteTeam.js
+++ frontend/src/lib/components/InviteTeam.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react'
+import React, { useRef } from 'react'
 import { toast } from 'react-toastify'
 import { Button, Tooltip, Input } from 'antd'
 import { CopyOutlined } from '@ant-design/icons'
 
-export class InviteTeam extends Component {
-    urlRef = React.createRef()
-    copyToClipboard = () => {
-        this.urlRef.current.focus()
-        this.urlRef.current.select()
+export function InviteTeam({ user }) {
+    const urlRef = useRef()
+
+    function copyToClipboard() {
+        urlRef.current.focus()
+        urlRef.current.select()
         document.execCommand('copy')
-        this.urlRef.current.blur()
+        urlRef.current.blur()
         toast('Link copied!')
     }
-    render() {
-        let url = window.location.origin
-        return (
-            <div data-attr="invite-team-modal">
-                <br />
-                Send your team the following URL:
-                <br />
-                <br />
-                <div>
-                    <Input
-                        data-attr="copy-invite-to-clipboard-input"
-                        type="text"
-                        ref={this.urlRef}
-                        value={url + '/signup/' + this.props.user.team.signup_token}
-                        suffix={
-                            <Tooltip title="Copy to Clipboard">
-                                <Button onClick={this.copyToClipboard} type="default" icon={<CopyOutlined />} />
-                            </Tooltip>
-                        }
-                    />
-                </div>
-                <br />
+
+    const url = window.location.origin
+    return (
+        <div data-attr="invite-team-modal">
+            <br />
+            Send your team the following URL:
+            <br />
+            <br />
+            <div>
+                <Input
+                    data-attr="copy-invite-to-clipboard-input"
+                    type="text"
+                    ref={urlRef}
+                    value={url + '/signup/' + user.team.signup_token}
+                    suffix={
+                        <Tooltip title="Copy to Clipboard">
+                            <Button onClick={copyToClipboard} type="default" icon={<CopyOutlined />} />
+                        </Tooltip>
+                    }
+                />
             </div>
-        )
-    }
+            <br />
+        </div>
+    )
 }
