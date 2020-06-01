b9549a21e3647a70462d23fa2c7d2008448790ed
Tim Glaser <tim.glaser@hiberly.com>
2020-06-01 08:59:04 +0100

    Fix test

--- cypress/integration/live_actions.js
+++ cypress/integration/live_actions.js
@@ -9,7 +9,7 @@ describe('Live Actions', () => {
     })
 
     it('Apply 1 overall filter', () => {
-        cy.get('[data-attr=new-prop-filter-LiveActionsTable]').click()
+        cy.get('[data-attr=new-prop-filter-EventsTable]').click()
         cy.get('[data-attr=prop-filter-event-0]').click()
         cy.get('[data-attr=prop-val]').click()
         cy.get('[data-attr=prop-val-0]').click()
