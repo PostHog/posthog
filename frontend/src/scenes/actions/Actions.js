bfefa31f96e586aeb4be0050ee24567d7b261faa
Marius Andra <marius.andra@gmail.com>
2020-06-02 10:58:17 +0200

    only one export from actions

--- frontend/src/scenes/actions/Actions.js
+++ frontend/src/scenes/actions/Actions.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ActionsTable } from './ActionsTable'
 import { hot } from 'react-hot-loader/root'
 
-export function _Actions(props) {
+function _Actions(props) {
     return <ActionsTable {...props} />
 }
 export const Actions = hot(_Actions)
