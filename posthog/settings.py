c52157dc671735533f350d7839f00f4aee5b9e60
Tim Glaser <tim.glaser@hiberly.com>
2020-06-03 23:40:09 +0100

    Bump vesion 1.8.0

--- posthog/settings.py
+++ posthog/settings.py
@@ -17,7 +17,7 @@ import sentry_sdk
 from django.core.exceptions import ImproperlyConfigured
 from sentry_sdk.integrations.django import DjangoIntegration
 
-VERSION = '1.7.0'
+VERSION = '1.8.0'
 
 def get_env(key):
     try:
