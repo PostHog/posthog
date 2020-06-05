f3b516459c4cb97e50edecc66fc3bfcc44127ddb
James Greenhill <jams@uber.com>
2020-06-05 11:28:47 -0700

    see if this is ssl redirect

--- cypress/wait.py
+++ cypress/wait.py
@@ -18,8 +18,10 @@ def main():
                 continue
             else:
                 # recieved not 200 from PostHog, but service is up
+                print(r)
                 print("PostHog is still booting. Sleeping for 1 second")
         except:
+            print(r)
             print("PostHog is still booting. Sleeping for 1 second")
         time.sleep(1)
 
