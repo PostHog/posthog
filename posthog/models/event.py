ac46f09429810591f03cc16fc8c63c67cbf38515
Tim Glaser <tim.glaser@hiberly.com>
2020-06-08 14:58:51 +0100

    Closes #888 filter on elements (#917)
    * Closes #888 filter on elements
    
    * Add joinfield as requirement
    
    * Fix tests
    
    * Use correct element
    
    * Fix paths query
    
    * Delete old migration test
    
    * Clean up
    
    * Add frontend filters for elements, closes #419
    
    * Add element api
    
    * Fix paths and add test
    
    * Keep elements field, otherwise breaks parititons
    
    * Don't use join field
    
    * Remove other join field references
    
    * Fix test

--- posthog/models/event.py
+++ posthog/models/event.py
@@ -12,6 +12,7 @@ from django.db.models import (
 from django.db import connection
 from django.contrib.postgres.fields import JSONField
 from django.utils import timezone
+from django.forms.models import model_to_dict
 
 from .element_group import ElementGroup
 from .element import Element
@@ -107,19 +108,19 @@ class EventManager(models.QuerySet):
                     )
         return (subqueries, filter)
 
-    def filter_by_element(self, action_step):
-        groups = ElementGroup.objects.filter(team=action_step.action.team_id)
+    def filter_by_element(self, filters: Dict):
+        groups = ElementGroup.objects.filter(team_id=OuterRef('team_id'))
 
-        if action_step.selector:
-            selector = Selector(action_step.selector)
+        if filters.get('selector'):
+            selector = Selector(filters['selector'])
             subqueries, filter = self._element_subquery(selector)
             groups = groups.annotate(**subqueries)  # type: ignore
         else:
             filter = {}
 
         for key in ["tag_name", "text", "href"]:
-            if getattr(action_step, key):
-                filter["element__{}".format(key)] = getattr(action_step, key)
+            if filters.get(key):
+                filter["element__{}".format(key)] = filters[key]
 
         if not filter:
             return {}
@@ -166,8 +167,8 @@ class EventManager(models.QuerySet):
             subquery = Event.objects.add_person_id(team_id=action.team_id).filter(
                 Filter(data={'properties': step.properties}).properties_to_Q(),
                 pk=OuterRef("id"),
-                **self.filter_by_element(step),
                 **self.filter_by_event(step),
+                **self.filter_by_element(model_to_dict(step))
             )
             subquery = self.filter_by_url(step, subquery)
             any_step |= Q(Exists(subquery))
@@ -339,10 +340,12 @@ class Event(models.Model):
     event: models.CharField = models.CharField(max_length=200, null=True, blank=True)
     distinct_id: models.CharField = models.CharField(max_length=200)
     properties: JSONField = JSONField(default=dict)
-    elements: JSONField = JSONField(default=list, null=True, blank=True)
     timestamp: models.DateTimeField = models.DateTimeField(
         default=timezone.now, blank=True
     )
     elements_hash: models.CharField = models.CharField(
         max_length=200, null=True, blank=True
     )
+
+    # DEPRECATED: elements are stored against element groups now
+    elements: JSONField = JSONField(default=list, null=True, blank=True)
