a1f236f5b154acf6563f8ae409821db4504af055
Tim Glaser <tim.glaser@hiberly.com>
2020-06-09 17:13:59 +0100

    Revert "181 create actions from trends page (#948)" (#967)
    This reverts commit 172e7354f26c10adeb477f4b7e1f7a0b060080e4.

--- frontend/src/scenes/events/EventDetails.js
+++ frontend/src/scenes/events/EventDetails.js
@@ -2,41 +2,25 @@ import React from 'react'
 import { PropertiesTable } from 'lib/components/PropertiesTable'
 import moment from 'moment'
 import { EventElements } from 'scenes/events/EventElements'
-import { Tabs, Button } from 'antd'
-
-import { createActionFromEvent } from './createActionFromEvent'
+import { Tabs } from 'antd'
 const { TabPane } = Tabs
 
 export function EventDetails({ event }) {
     return (
-        <>
-            <Button
-                onClick={() => createActionFromEvent(event, 0)}
-                style={{ float: 'right', zIndex: 9999 }}
-                type="primary"
-            >
-                Create action from event
-            </Button>
-            <Tabs
-                style={{ float: 'left', width: '100%', marginTop: -40 }}
-                data-attr="event-details"
-                defaultActiveKey="properties"
-                animated={false}
-            >
-                <TabPane tab="Properties" key="properties">
-                    <PropertiesTable
-                        properties={{
-                            Timestamp: moment(event.timestamp).toISOString(),
-                            ...event.properties,
-                        }}
-                    />
+        <Tabs data-attr="event-details" defaultActiveKey="properties" animated={false}>
+            <TabPane tab="Properties" key="properties">
+                <PropertiesTable
+                    properties={{
+                        Timestamp: moment(event.timestamp).toISOString(),
+                        ...event.properties,
+                    }}
+                />
+            </TabPane>
+            {event.elements.length > 0 && (
+                <TabPane tab="Elements" key="elements">
+                    <EventElements event={event} />
                 </TabPane>
-                {event.elements.length > 0 && (
-                    <TabPane tab="Elements" key="elements">
-                        <EventElements event={event} />
-                    </TabPane>
-                )}
-            </Tabs>
-        </>
+            )}
+        </Tabs>
     )
 }
