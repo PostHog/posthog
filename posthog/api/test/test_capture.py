3ab8b73817bb674d80d30d84311c3f4e10981fb6
Marius Andra <marius.andra@gmail.com>
2020-06-09 11:55:56 +0200

    Upgrade to posthog-js 1.2.0 to support dynamic params. (#957)
    * Refactor editor params. Specify default editor tab. Upgrade posthog-js.
    
    * add backwards compatible 'token'
    
    * fix test
    
    * keep things simple

--- posthog/api/test/test_capture.py
+++ posthog/api/test/test_capture.py
@@ -327,18 +327,18 @@ class TestDecide(BaseTest):
         self.team.save()
         response = self.client.get('/decide/', HTTP_ORIGIN='https://example.com').json()
         self.assertEqual(response['isAuthenticated'], True)
-        self.assertEqual(response['toolbarVersion'], settings.TOOLBAR_VERSION)
+        self.assertEqual(response['editorParams']['toolbarVersion'], settings.TOOLBAR_VERSION)
 
     def test_user_on_evil_site(self):
         self.team.app_urls = ['https://example.com']
         self.team.save()
         response = self.client.get('/decide/', HTTP_ORIGIN='https://evilsite.com').json()
         self.assertEqual(response['isAuthenticated'], False)
-        self.assertIsNone(response.get('toolbarVersion', None))
+        self.assertIsNone(response['editorParams'].get('toolbarVersion', None))
 
     def test_user_on_local_host(self):
         self.team.app_urls = ['https://example.com']
         self.team.save()
         response = self.client.get('/decide/', HTTP_ORIGIN='http://127.0.0.1:8000').json()
         self.assertEqual(response['isAuthenticated'], True)
-        self.assertEqual(response['toolbarVersion'], settings.TOOLBAR_VERSION)
+        self.assertEqual(response['editorParams']['toolbarVersion'], settings.TOOLBAR_VERSION)
