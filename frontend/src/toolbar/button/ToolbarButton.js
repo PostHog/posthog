d3c2e092b6a499ad6bccd24bec4f82ff5205eb26
Marius Andra <marius.andra@gmail.com>
2020-06-15 10:30:06 +0200

    clean toolbarButton code

--- frontend/src/toolbar/button/ToolbarButton.js
+++ frontend/src/toolbar/button/ToolbarButton.js
@@ -40,36 +40,33 @@ export function ToolbarButton() {
     const { isAuthenticated } = useValues(toolbarLogic)
     const { authenticate } = useActions(toolbarLogic)
 
-    function setQuarterFromShadowRef() {
-        const shadowRoot = getShadowRoot()
-        if (shadowRoot) {
-            const element = shadowRoot.getElementById('button-toolbar')
-            if (element) {
-                const rect = element.getBoundingClientRect()
-                const x = rect.x + rect.width / 2
-                const y = rect.y + rect.height / 2
-                const ns = y < window.innerHeight / 2 ? 's' : 'n'
-                const we = x < window.innerWidth / 2 ? 'e' : 'w'
-                // use toolbarButtonLogic.values.quarter to always get the last state
-                if (toolbarButtonLogic.values.quarter !== `${ns}${we}`) {
-                    setQuarter(`${ns}${we}`)
-                }
+    function updateQuarter(buttonDiv = getShadowRoot()?.getElementById('button-toolbar')) {
+        if (buttonDiv) {
+            const rect = buttonDiv.getBoundingClientRect()
+            const x = rect.x + rect.width / 2
+            const y = rect.y + rect.height / 2
+            const ns = y < window.innerHeight / 2 ? 's' : 'n'
+            const we = x < window.innerWidth / 2 ? 'e' : 'w'
+            // use toolbarButtonLogic.values.quarter to always get the last state
+            if (toolbarButtonLogic.values.quarter !== `${ns}${we}`) {
+                setQuarter(`${ns}${we}`)
             }
         }
     }
 
-    useEffect(() => {
-        setQuarterFromShadowRef()
-    }, [])
+    useEffect(() => updateQuarter(), [])
 
     const longPressEvents = useLongPress(
         clicked => {
-            setQuarterFromShadowRef()
-
-            if (clicked) {
-                isAuthenticated ? dock() : authenticate()
+            updateQuarter()
+            if (isAuthenticated) {
+                if (clicked) {
+                    dock()
+                } else {
+                    // extendButtons()
+                }
             } else {
-                // extendButtons()
+                authenticate()
             }
         },
         { ms: 700, clickMs: 1 }
@@ -78,30 +75,26 @@ export function ToolbarButton() {
     const globalMouseMove = useRef(null)
     useEffect(() => {
         globalMouseMove.current = function(e) {
-            const shadowRoot = getShadowRoot()
-            if (shadowRoot) {
-                setQuarterFromShadowRef()
+            const buttonDiv = getShadowRoot()?.getElementById('button-toolbar')
+            if (buttonDiv) {
+                updateQuarter(buttonDiv)
+                const rect = buttonDiv.getBoundingClientRect()
+                const x = rect.left + rect.width / 2
+                const y = rect.top + rect.height / 2
+                const distance = Math.sqrt((e.clientX - x) * (e.clientX - x) + (e.clientY - y) * (e.clientY - y))
 
-                const element = shadowRoot.getElementById('button-toolbar')
-                if (element) {
-                    const rect = element.getBoundingClientRect()
-                    const x = rect.left + rect.width / 2
-                    const y = rect.top + rect.height / 2
-                    const distance = Math.sqrt((e.clientX - x) * (e.clientX - x) + (e.clientY - y) * (e.clientY - y))
+                const startDistance = isAuthenticated ? 230 : 130
+                const endDistance = isAuthenticated ? 160 : 60
 
-                    const startDistance = isAuthenticated ? 230 : 130
-                    const endDistance = isAuthenticated ? 160 : 60
-
-                    if (distance >= startDistance) {
-                        if (toolbarButtonLogic.values.extensionPercentage !== 0) {
-                            setExtensionPercentage(0)
-                        }
-                    } else if (distance >= endDistance && distance < startDistance) {
-                        setExtensionPercentage((startDistance - distance) / (startDistance - endDistance))
-                    } else if (distance < endDistance) {
-                        if (toolbarButtonLogic.values.extensionPercentage !== 1) {
-                            setExtensionPercentage(1)
-                        }
+                if (distance >= startDistance) {
+                    if (toolbarButtonLogic.values.extensionPercentage !== 0) {
+                        setExtensionPercentage(0)
+                    }
+                } else if (distance >= endDistance && distance < startDistance) {
+                    setExtensionPercentage((startDistance - distance) / (startDistance - endDistance))
+                } else if (distance < endDistance) {
+                    if (toolbarButtonLogic.values.extensionPercentage !== 1) {
+                        setExtensionPercentage(1)
                     }
                 }
             }
@@ -119,148 +112,146 @@ export function ToolbarButton() {
     const heatmapDistance = heatmapEnabled ? Math.max(50, distance) : distance
 
     return (
-        <>
+        <Circle
+            rootNode
+            width={64}
+            className="floating-toolbar-button"
+            content={<Logo style={{ width: 54, height: 54, filter: 'invert(1)', cursor: 'pointer' }} />}
+            label={isAuthenticated ? 'Toolbar' : 'Oh, Hello!'}
+            labelStyle={{ opacity: extensionPercentage > 0.8 ? (extensionPercentage - 0.8) / 0.2 : 0 }}
+            {...longPressEvents}
+            zIndex={3}
+        >
             <Circle
-                rootNode
-                width={64}
-                className="floating-toolbar-button"
-                content={<Logo style={{ width: 54, height: 54, filter: 'invert(1)', cursor: 'pointer' }} />}
-                label={isAuthenticated ? 'Toolbar' : 'Oh, Hello!'}
-                labelStyle={{ opacity: extensionPercentage > 0.8 ? (extensionPercentage - 0.8) / 0.2 : 0 }}
-                {...longPressEvents}
-                zIndex={3}
-            >
-                <Circle
-                    width={32}
-                    distance={closeDistance}
-                    rotate={getQuarterRotation({ itemCount: 1, quarter: quarters[reverseQuarter(quarter)] })}
-                    content={<CloseOutlined />}
-                    zIndex={5}
-                    onClick={hideButton}
-                    style={{ cursor: 'pointer', background: 'black', color: 'white' }}
-                />
-                {isAuthenticated ? (
-                    <>
-                        <Circle
-                            width={48}
-                            distance={inspectDistance}
-                            rotate={getQuarterRotation({
-                                itemCount,
-                                index: index++,
-                                padding,
-                                quarter: quarters[quarter],
-                            })}
-                            label="Inspect"
-                            labelStyle={{ opacity: inspectDistance > 80 ? (inspectDistance - 80) / 20 : 0 }}
-                            content={
-                                <div style={{ position: 'relative' }}>
-                                    <SearchOutlined />
-                                    {selectedInspectElement ? (
-                                        <div
-                                            style={{
-                                                position: 'absolute',
-                                                top: 12,
-                                                left: 6,
-                                                fontSize: 13,
-                                                color: 'white',
-                                            }}
-                                        >
-                                            <CloseOutlined />
-                                        </div>
-                                    ) : null}
-                                </div>
-                            }
-                            zIndex={1}
-                            onClick={inspectingElement || selectedInspectElement ? stop : start}
-                            style={{
-                                cursor: 'pointer',
-                                background:
-                                    inspectingElement || selectedInspectElement
-                                        ? 'rgb(84, 138, 248)'
-                                        : 'hsla(220, 52%, 96%, 1)',
-                                color:
-                                    inspectingElement || selectedInspectElement
-                                        ? 'hsla(220, 52%, 96%, 1)'
-                                        : 'rgb(84, 138, 248)',
-                                fontSize: '32px',
-                                transition: 'transform 0.2s, color 0.2s, background: 0.2s',
-                                transform: `scale(${0.2 + (0.8 * inspectDistance) / 100})`,
-                            }}
-                        />
-                        <Circle
-                            width={48}
-                            distance={heatmapDistance}
-                            rotate={getQuarterRotation({
-                                itemCount,
-                                index: index++,
-                                padding,
-                                quarter: quarters[quarter],
-                            })}
-                            label="Heatmap"
-                            labelStyle={{ opacity: heatmapDistance > 80 ? (heatmapDistance - 80) / 20 : 0 }}
-                            content={<FireFilled />}
-                            zIndex={1}
-                            onClick={() => setHeatmapEnabled(!heatmapEnabled)}
-                            style={{
-                                cursor: 'pointer',
-                                background: heatmapEnabled ? '#FF5722' : 'hsl(14, 100%, 97%)',
-                                color: heatmapEnabled ? '#FFEB3B' : '#FF5722',
-                                fontSize: '32px',
-                                transition: 'transform 0.2s',
-                                transform: `scale(${0.2 + (0.8 * heatmapDistance) / 100})`,
-                            }}
-                        >
-                            {heatmapLoading ? (
-                                <Circle
-                                    width={12}
-                                    distance={30 * (0.2 + (0.8 * heatmapDistance) / 100)}
-                                    rotate={0}
-                                    animate
-                                    animationId="heatmap-loading"
-                                    animationDuration={0.5 + (0.5 * heatmapDistance) / 100}
-                                    spin="1s linear infinite"
-                                    content={<FireFilled />}
-                                    zIndex={3}
-                                    style={{
-                                        cursor: 'pointer',
-                                        background: '#FF5722',
-                                        color: '#FFEB3B',
-                                        fontSize: '12px',
-                                        transition: 'transform 0.2s',
-                                        transform: `rotate(${-getQuarterRotation({
-                                            itemCount,
-                                            index: index - 1,
-                                            padding,
-                                            quarter: quarters[quarter],
-                                        })}deg)`,
-                                    }}
-                                />
-                            ) : null}
-                        </Circle>
-                        <Circle
-                            width={48}
-                            distance={distance}
-                            rotate={getQuarterRotation({
-                                itemCount,
-                                index: index++,
-                                padding,
-                                quarter: quarters[quarter],
-                            })}
-                            label="Float"
-                            labelStyle={{ opacity: distance > 80 ? (distance - 80) / 20 : 0 }}
-                            content={<ProfileOutlined />}
-                            zIndex={1}
-                            onClick={float}
-                            style={{
-                                cursor: 'pointer',
-                                fontSize: '24px',
-                                transition: 'transform 0.2s',
-                                transform: `scale(${0.2 + (0.8 * distance) / 100})`,
-                            }}
-                        />
-                    </>
-                ) : null}
-            </Circle>
-        </>
+                width={32}
+                distance={closeDistance}
+                rotate={getQuarterRotation({ itemCount: 1, quarter: quarters[reverseQuarter(quarter)] })}
+                content={<CloseOutlined />}
+                zIndex={5}
+                onClick={hideButton}
+                style={{ cursor: 'pointer', background: 'black', color: 'white' }}
+            />
+            {isAuthenticated ? (
+                <>
+                    <Circle
+                        width={48}
+                        distance={inspectDistance}
+                        rotate={getQuarterRotation({
+                            itemCount,
+                            index: index++,
+                            padding,
+                            quarter: quarters[quarter],
+                        })}
+                        label="Inspect"
+                        labelStyle={{ opacity: inspectDistance > 80 ? (inspectDistance - 80) / 20 : 0 }}
+                        content={
+                            <div style={{ position: 'relative' }}>
+                                <SearchOutlined />
+                                {selectedInspectElement ? (
+                                    <div
+                                        style={{
+                                            position: 'absolute',
+                                            top: 12,
+                                            left: 6,
+                                            fontSize: 13,
+                                            color: 'white',
+                                        }}
+                                    >
+                                        <CloseOutlined />
+                                    </div>
+                                ) : null}
+                            </div>
+                        }
+                        zIndex={1}
+                        onClick={inspectingElement || selectedInspectElement ? stop : start}
+                        style={{
+                            cursor: 'pointer',
+                            background:
+                                inspectingElement || selectedInspectElement
+                                    ? 'rgb(84, 138, 248)'
+                                    : 'hsla(220, 52%, 96%, 1)',
+                            color:
+                                inspectingElement || selectedInspectElement
+                                    ? 'hsla(220, 52%, 96%, 1)'
+                                    : 'rgb(84, 138, 248)',
+                            fontSize: '32px',
+                            transition: 'transform 0.2s, color 0.2s, background: 0.2s',
+                            transform: `scale(${0.2 + (0.8 * inspectDistance) / 100})`,
+                        }}
+                    />
+                    <Circle
+                        width={48}
+                        distance={heatmapDistance}
+                        rotate={getQuarterRotation({
+                            itemCount,
+                            index: index++,
+                            padding,
+                            quarter: quarters[quarter],
+                        })}
+                        label="Heatmap"
+                        labelStyle={{ opacity: heatmapDistance > 80 ? (heatmapDistance - 80) / 20 : 0 }}
+                        content={<FireFilled />}
+                        zIndex={1}
+                        onClick={() => setHeatmapEnabled(!heatmapEnabled)}
+                        style={{
+                            cursor: 'pointer',
+                            background: heatmapEnabled ? '#FF5722' : 'hsl(14, 100%, 97%)',
+                            color: heatmapEnabled ? '#FFEB3B' : '#FF5722',
+                            fontSize: '32px',
+                            transition: 'transform 0.2s',
+                            transform: `scale(${0.2 + (0.8 * heatmapDistance) / 100})`,
+                        }}
+                    >
+                        {heatmapLoading ? (
+                            <Circle
+                                width={12}
+                                distance={30 * (0.2 + (0.8 * heatmapDistance) / 100)}
+                                rotate={0}
+                                animate
+                                animationId="heatmap-loading"
+                                animationDuration={0.5 + (0.5 * heatmapDistance) / 100}
+                                spin="1s linear infinite"
+                                content={<FireFilled />}
+                                zIndex={3}
+                                style={{
+                                    cursor: 'pointer',
+                                    background: '#FF5722',
+                                    color: '#FFEB3B',
+                                    fontSize: '12px',
+                                    transition: 'transform 0.2s',
+                                    transform: `rotate(${-getQuarterRotation({
+                                        itemCount,
+                                        index: index - 1,
+                                        padding,
+                                        quarter: quarters[quarter],
+                                    })}deg)`,
+                                }}
+                            />
+                        ) : null}
+                    </Circle>
+                    <Circle
+                        width={48}
+                        distance={distance}
+                        rotate={getQuarterRotation({
+                            itemCount,
+                            index: index++,
+                            padding,
+                            quarter: quarters[quarter],
+                        })}
+                        label="Float"
+                        labelStyle={{ opacity: distance > 80 ? (distance - 80) / 20 : 0 }}
+                        content={<ProfileOutlined />}
+                        zIndex={1}
+                        onClick={float}
+                        style={{
+                            cursor: 'pointer',
+                            fontSize: '24px',
+                            transition: 'transform 0.2s',
+                            transform: `scale(${0.2 + (0.8 * distance) / 100})`,
+                        }}
+                    />
+                </>
+            ) : null}
+        </Circle>
     )
 }
