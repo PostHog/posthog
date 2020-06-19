c85679422cce46d59554d9883b98a0e11bf4adf3
Tim Glaser <tim.glaser@hiberly.com>
2020-06-19 14:30:33 +0200

    Instrumentation and fix

--- posthog/api/feature_flag.py
+++ posthog/api/feature_flag.py
@@ -4,29 +4,53 @@ from rest_framework import request, serializers, viewsets
 from django.db.models import QuerySet
 from django.db import IntegrityError
 from typing import List, Dict, Any
+import posthoganalytics
 import json
+
 
 class FeatureFlagSerializer(serializers.HyperlinkedModelSerializer):
     created_by = UserSerializer(required=False, read_only=True)
 
     class Meta:
         model = FeatureFlag
-        fields = ['id', 'name', 'key', 'rollout_percentage', 'filters', 'deleted', 'active', 'created_by', 'created_at']
+        fields = [
+            "id",
+            "name",
+            "key",
+            "rollout_percentage",
+            "filters",
+            "deleted",
+            "active",
+            "created_by",
+            "created_at",
+        ]
 
     def create(self, validated_data: Dict, *args: Any, **kwargs: Any) -> FeatureFlag:
-        request = self.context['request']
-        validated_data['created_by'] = request.user
-        
+        request = self.context["request"]
+        validated_data["created_by"] = request.user
+        validated_data["team"] = request.user.team_set.get()
         try:
-            return super().create(validated_data)
+            feature_flag = super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError("key-exists")
+        posthoganalytics.capture(
+            request.user.distinct_id,
+            "feature flag created",
+            {
+                "rollout_percentage": feature_flag.rollout_percentage,
+                "has_filters": True
+                if feature_flag.filters and feature_flag.filters.get("properties")
+                else False,
+            },
+        )
+        return feature_flag
 
-    def update(self, instance: FeatureFlag, validated_data: Dict, *args: Any, **kwargs: Any) -> FeatureFlag: # type: ignore
+    def update(self, instance: FeatureFlag, validated_data: Dict, *args: Any, **kwargs: Any) -> FeatureFlag:  # type: ignore
         try:
             return super().update(instance, validated_data)
         except IntegrityError:
             raise serializers.ValidationError("key-exists")
+
 
 class FeatureFlagViewSet(viewsets.ModelViewSet):
     queryset = FeatureFlag.objects.all()
@@ -34,8 +58,8 @@ class FeatureFlagViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self) -> QuerySet:
         queryset = super().get_queryset()
-        if self.action == 'list': # type: ignore
+        if self.action == "list":  # type: ignore
             queryset = queryset.filter(deleted=False)
-        return queryset\
-            .filter(team=self.request.user.team_set.get())\
-            .order_by('-created_at')
+        return queryset.filter(team=self.request.user.team_set.get()).order_by(
+            "-created_at"
+        )
