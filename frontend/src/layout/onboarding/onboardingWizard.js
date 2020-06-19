e49c0dea3935194954d6a6c15a920e2ea4a9bdbb
Eric <eeoneric@gmail.com>
2020-06-19 16:32:03 -0400

    more cleanup

--- frontend/src/layout/onboarding/onboardingWizard.js
+++ frontend/src/layout/onboarding/onboardingWizard.js
@@ -13,6 +13,7 @@ import {
     RNInstructions,
     RubyInstructions,
     JSInstructions,
+    APIInstructions,
 } from './FrameworkInstructions'
 import { userLogic } from 'scenes/userLogic'
 import { useInterval } from 'lib/hooks/useInterval'
@@ -40,6 +41,7 @@ const GO = 'GO'
 const RUBY = 'RUBY'
 const PYTHON = 'PYTHON'
 const PHP = 'PHP'
+const API = 'API'
 
 const webFrameworks = {
     [PURE_JS]: 'Pure Javascript',
@@ -93,14 +95,17 @@ const content = {
             </Row>
         </CardContainer>
     ),
-    FRAMEWORK: ({ platformType, reverse, onSubmit }) => {
+    FRAMEWORK: ({ platformType, reverse, onSubmit, onApiContinue }) => {
         let frameworks = {}
         if (platformType === WEB) frameworks = webFrameworks
         else if (platformType === MOBILE) frameworks = mobileFrameworks
 
         return (
             <CardContainer index={1} totalSteps={4} onBack={reverse}>
-                <p className="prompt-text">Choose the framework your app is built in</p>
+                <p className="prompt-text">
+                    Choose the framework your app is built in. We'll provide you with snippets that you can easily add
+                    to your codebase to get started!
+                </p>
                 <Row>
                     <List
                         style={{ width: '100%' }}
@@ -116,6 +121,12 @@ const content = {
                             </List.Item>
                         )}
                     ></List>
+                </Row>
+                <Row align="middle" style={{ float: 'right', marginTop: 8 }}>
+                    Don't see a supported framework?
+                    <b style={{ marginLeft: 5 }} className="back-button" onClick={() => onApiContinue()}>
+                        Continue with API
+                    </b>
                 </Row>
             </CardContainer>
         )
@@ -152,12 +163,17 @@ export function OnboardingWizard({ user }) {
         setIndex(index - 1)
     }
 
+    function onApiContinue() {
+        setFramework(API)
+        setIndex(index + 1)
+    }
+
     return (
         <div
             className="background"
             style={{ display: 'flex', height: '100vh', width: '100vw', alignItems: 'center', justifyContent: 'center' }}
         >
-            {content[states[index]]({ onSubmit, platformType, user, reverse, framework })}
+            {content[states[index]]({ onSubmit, platformType, user, reverse, framework, onApiContinue })}
         </div>
     )
 }
@@ -194,6 +210,10 @@ function VerificationPanel({ reverse }) {
             ) : (
                 <>
                     <h2>Successfully sent events!</h2>
+                    <p className="prompt-text">
+                        You will now be able to explore Posthog and take advantage of all its features to understand
+                        your users.
+                    </p>
                     <Button
                         type="primary"
                         style={{ float: 'right' }}
@@ -209,9 +229,34 @@ function VerificationPanel({ reverse }) {
 
 function InstructionsPanel({ user, onSubmit, reverse, platformType, framework }) {
     const [selected, setSelected] = useState(0)
+    if (framework === API) {
+        return (
+            <CardContainer index={2} totalSteps={4} nextButton={true} onSubmit={onSubmit} onBack={reverse}>
+                <h2>API</h2>
+                <p className="prompt-text">
+                    {"Below is an easy format for capturing events using the api we've provided"}
+                </p>
+                <APIInstructions user={user}></APIInstructions>
+            </CardContainer>
+        )
+    }
+
+    if (framework === PURE_JS) {
+        return (
+            <CardContainer index={2} totalSteps={4} nextButton={true} onSubmit={onSubmit} onBack={reverse}>
+                <h2>Posthog-JS</h2>
+                <p className="prompt-text">
+                    {
+                        'Posthog-JS will automatically capture page views, page leaves, and interactions with specific elements (<a>, <button>, <input>, <textarea>, <form>)'
+                    }
+                </p>
+                {webFrameworksSnippet[framework]({ user })}
+            </CardContainer>
+        )
+    }
     return (
         <CardContainer index={2} totalSteps={4} nextButton={true} onSubmit={onSubmit} onBack={reverse}>
-            {platformType === WEB && framework !== PURE_JS ? (
+            {platformType === WEB ? (
                 <Row style={{ marginLeft: -5 }}>
                     <h2
                         className="back-button"
@@ -231,18 +276,7 @@ function InstructionsPanel({ user, onSubmit, reverse, platformType, framework })
             ) : (
                 <h2>Setup</h2>
             )}
-            {platformType === WEB && framework == PURE_JS && (
-                <>
-                    <p className="prompt-text">
-                        {
-                            'Posthog-JS will automatically capture page views, page leaves, and interactions with specific elements (<a>, <button>, <input>, <textarea>, <form>)'
-                        }
-                    </p>
-                    {webFrameworksSnippet[framework]({ user })}
-                </>
-            )}
-
-            {platformType === WEB && framework !== PURE_JS && selected === 0 && (
+            {platformType === WEB && selected === 0 && (
                 <>
                     <p className="prompt-text">
                         {
@@ -255,7 +289,7 @@ function InstructionsPanel({ user, onSubmit, reverse, platformType, framework })
                     <JSSnippet user={user}></JSSnippet>
                 </>
             )}
-            {platformType === WEB && framework !== PURE_JS && selected == 1 && (
+            {platformType === WEB && selected == 1 && (
                 <>
                     <p className="prompt-text">
                         {
