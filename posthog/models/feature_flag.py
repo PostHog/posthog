4d56f4226f186255d90795dce37a2c0fb78bb754
Tim Glaser <tim.glaser@hiberly.com>
2020-06-19 14:05:02 +0200

    Use <= instead of <

--- posthog/models/feature_flag.py
+++ posthog/models/feature_flag.py
@@ -8,13 +8,10 @@ import hashlib
 __LONG_SCALE__ = float(0xFFFFFFFFFFFFFFF)
 
 
-
 class FeatureFlag(models.Model):
     class Meta:
         constraints = [
-            models.UniqueConstraint(
-                fields=["team", "key"], name="unique key for team"
-            )
+            models.UniqueConstraint(fields=["team", "key"], name="unique key for team")
         ]
 
     name: models.CharField = models.CharField(max_length=400)
@@ -30,18 +27,25 @@ class FeatureFlag(models.Model):
     active: models.BooleanField = models.BooleanField(default=True)
 
     def distinct_id_matches(self, distinct_id: str) -> bool:
-        if len(self.filters.get('properties', [])) > 0:
-            if not Person.objects\
-                .filter(team_id=self.team_id, persondistinctid__distinct_id=distinct_id)\
-                .filter(Filter(data=self.filters).properties_to_Q(team_id=self.team_id, is_person_query=True))\
-                .exists():
+        if len(self.filters.get("properties", [])) > 0:
+            if (
+                not Person.objects.filter(
+                    team_id=self.team_id, persondistinctid__distinct_id=distinct_id
+                )
+                .filter(
+                    Filter(data=self.filters).properties_to_Q(
+                        team_id=self.team_id, is_person_query=True
+                    )
+                )
+                .exists()
+            ):
                 return False
             elif not self.rollout_percentage:
                 return True
 
         if self.rollout_percentage:
             hash = self._hash(self.key, distinct_id)
-            if hash < (self.rollout_percentage / 100):
+            if hash <= (self.rollout_percentage / 100):
                 return True
         return False
 
@@ -50,7 +54,6 @@ class FeatureFlag(models.Model):
     # uniformly distributed between 0 and 1, so if we want to show this feature to 20% of traffic
     # we can do _hash(key, distinct_id) < 0.2
     def _hash(self, key: str, distinct_id: str) -> float:
-        hash_key = '%s.%s' % (key, distinct_id)
-        hash_val = int(hashlib.sha1(hash_key.encode('utf-8')).hexdigest()[:15], 16)
+        hash_key = "%s.%s" % (key, distinct_id)
+        hash_val = int(hashlib.sha1(hash_key.encode("utf-8")).hexdigest()[:15], 16)
         return hash_val / __LONG_SCALE__
-
