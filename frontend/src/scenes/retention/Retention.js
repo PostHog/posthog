b2a6e2be415e2ef9323bd3a8ed0513d847eb745c
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-06-23 10:11:54 +0300

    Add empty views for retention

---
+++ frontend/src/scenes/retention/Retention.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { hot } from 'react-hot-loader/root'
+
+export const Retention = hot(_Retention)
+function _Retention() {
+    return (
+        <div>
+            <h1 className="page-header">Retention</h1>
+        </div>
+    )
+}
