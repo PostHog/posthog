9e9021570985b3a81459a51ffb7c82d3e59c0818
Eric Duong <eeoneric@gmail.com>
2020-06-24 09:03:06 -0400

    Add created_at to team model (#1075)
    * team-timestamp
    
    * add updated_at field
    
    * update manifest

--- posthog/models/team.py
+++ posthog/models/team.py
@@ -91,6 +91,12 @@ class Team(models.Model):
     )
     event_names: JSONField = JSONField(default=list)
     event_properties: JSONField = JSONField(default=list)
+    created_at: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True, blank=True
+    )
+    updated_at: models.DateTimeField = models.DateTimeField(
+        auto_now=True, blank=True
+    )
     anonymize_ips: models.BooleanField = models.BooleanField(default=False)
 
     # DEPRECATED: this field is deprecated in favour of OPT_OUT_CAPTURE env variable and anonymized data
