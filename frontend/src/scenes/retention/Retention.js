a9b09ed0caaf37e321afb2ccd102b588f8c0ce0d
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-06-24 13:20:41 +0300

    Add subheader for retention

--- frontend/src/scenes/retention/Retention.js
+++ frontend/src/scenes/retention/Retention.js
@@ -9,6 +9,9 @@ function _Retention() {
     return (
         <div>
             <h1 className="page-header">Retention</h1>
+            <p style={{ maxWidth: 600 }}>
+                <i>Retention table shows how many users return on subsequent days after visiting the site.</i>
+            </p>
 
             <RetentionTable logic={retentionTableLogic} />
         </div>
