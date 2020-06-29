04031dc2eca17cf5fcd0d36d7e7e73183379950a
Twixes <dev@twixes.com>
2020-06-29 12:06:53 +0200

    Update tests

--- posthog/api/test/test_person.py
+++ posthog/api/test/test_person.py
@@ -44,7 +44,7 @@ class TestPerson(BaseTest):
 
         response = self.client.get(
             "/api/person/?properties=%s"
-            % json.dumps([{"key": "email", "operator": "is_set", "value": "true"}])
+            % json.dumps([{"key": "email", "operator": "is_set", "value": "is_set"}])
         ).json()
         self.assertEqual(len(response["results"]), 2)
 
