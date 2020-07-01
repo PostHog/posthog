15eca6c3cda01965d39ab9a5c3677537c0acc888
Gabriel L. Maljkovich <gm@camba.coop>
2020-07-01 18:40:54 -0300

    WIP - funnel step reordering

--- frontend/src/scenes/trends/ActionFilter/ActionFilter.js
+++ frontend/src/scenes/trends/ActionFilter/ActionFilter.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react'
 import { useActions, useValues } from 'kea'
+import GridLayout, { WidthProvider } from '@mariusandra/react-grid-layout'
 import { entityFilterLogic } from './entityFilterLogic'
 import { ActionFilterRow } from './ActionFilterRow'
 import { Button } from 'antd'
+
+const ReactGridLayout = WidthProvider(GridLayout)
 
 export function ActionFilter({ setFilters, filters, typeKey, hideMathSelector }) {
     const logic = entityFilterLogic({ setFilters, filters, typeKey })
@@ -16,26 +19,46 @@ export function ActionFilter({ setFilters, filters, typeKey, hideMathSelector })
         setLocalFilters(filters)
     }, [filters])
 
+    const layouts = localFilters.map((filter, index) => ({ i: index.toString(), x: 1, y: index, w: 1, h: 1, isDraggable:true}))
+    const renderLocalFilters = () => localFilters.map((filter, index) => (
+        <div key={index.toString()}>
+          <ActionFilterRow
+              logic={logic}
+              filter={filter}
+              index={index}
+              key={index}
+              hideMathSelector={hideMathSelector}
+          />
+        </div>
+      ));
+
+    const updateFilterRowIndex = (_layout, _oldItem, newItem) => {
+
+    }
+
     return (
-        <div>
-            {localFilters &&
-                localFilters.map((filter, index) => (
-                    <ActionFilterRow
-                        logic={logic}
-                        filter={filter}
-                        index={index}
-                        key={index}
-                        hideMathSelector={hideMathSelector}
-                    />
-                ))}
-            <Button
-                type="primary"
-                onClick={() => addFilter()}
-                style={{ marginTop: '0.5rem' }}
-                data-attr="add-action-event-button"
+        <div style={{position: 'relative'}}>
+          {localFilters &&
+            <ReactGridLayout
+              cols={1}
+              layouts={layouts}
+              rowHeight={39}
+              onLayoutChange={() => {}}
+              draggableHandle=".mt-2"
+              isResizable={false}
+              onDragStop={updateFilterRowIndex}
             >
-                Add action/event
-            </Button>
+              { renderLocalFilters() }
+            </ReactGridLayout>
+          }
+          <Button
+            type="primary"
+            onClick={() => addFilter()}
+            style={{ marginTop: '0.5rem' }}
+            data-attr="add-action-event-button"
+            >
+            Add action/event
+          </Button>
         </div>
     )
 }
