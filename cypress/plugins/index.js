6d264fcae3eff6974b454512bace6823295c8f01
Yakko Majuri <yakko.majuri@gmail.com>
2020-07-03 12:29:14 -0300

    uncommented cypress/plugins/index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-// <reference types="cypress" />
+<reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -15,7 +15,7 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// module.exports = (on, config) => {
+module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
-// }
+}
