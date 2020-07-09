7049787533b5a9ae4e6a35ac4530d85bf73366fa
kacppian <aspkaushik@gmail.com>
2020-07-09 23:24:59 +0530

    Closes 1167 Adds new chart labels

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,7 +1,11 @@
 export const ACTIONS_LINE_GRAPH_LINEAR = 'ActionsLineGraph'
 export const ACTIONS_LINE_GRAPH_CUMULATIVE = 'ActionsLineGraphCumulative'
+export const ACTIONS_LINE_GRAPH_STACKED = 'ActionsLineGraphStacked'
+export const ACTIONS_LINE_GRAPH_CUMULATIVE_STACKED = 'ActionsLineGraphCumulativeStacked'
 
 export const STICKINESS = 'Stickiness'
 
 export const LINEAR_CHART_LABEL = 'Linear'
 export const CUMULATIVE_CHART_LABEL = 'Cumulative'
+export const STACKED_CHART_LABEL = 'Stacked'
+export const CUMULATIVE_STACKED_CHART_LABEL = 'Cumulative Stacked'
