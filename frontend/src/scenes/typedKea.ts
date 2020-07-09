b0f6b43c38cf3bb0a34a91e8ade360dd2bf5ceab
Marius Andra <marius.andra@gmail.com>
2020-07-09 09:23:17 +0200

    add version of kea that infers types

---
+++ frontend/src/scenes/typedKea.ts
@@ -0,0 +1,43 @@
+// This will be integrated into Kea soon... It's just faster to iterate here.
+
+import { kea as regularKea, useValues as useRegularValues } from 'kea'
+
+type BlankLogic = {
+    actions?: any
+    reducers?: any
+    listeners?: any
+    values?: any
+}
+
+type ReducerActions<LogicType extends BlankLogic, ReducerType> = {
+    [K in keyof LogicType['actions']]?: (state: any, payload: ReturnType<LogicType['actions'][K]>['payload']) => ReducerType
+}
+type ReducerDefinitions<LogicType extends BlankLogic> = {
+    [K in keyof LogicType['reducers']]?:
+        | [ReturnType<LogicType['reducers'][K]>, ReducerActions<LogicType, ReturnType<LogicType['reducers'][K]>>]
+        | ReducerActions<LogicType, ReturnType<LogicType['reducers'][K]>>
+}
+type ListenerDefinitions<LogicType extends BlankLogic> = {
+    [K in keyof LogicType['actions']]?:
+        | ((
+              payload: ReturnType<LogicType['actions'][K]>['payload'],
+              breakpoint?: () => void,
+              action?: ReturnType<LogicType['actions'][K]>,
+              previousState?: any
+          ) => void | Promise<void>)
+        | (() => void | Promise<void>)
+}
+type LogicInput<LogicType> = {
+    actions?: (logic: LogicType) => any | any
+    reducers?: ReducerDefinitions<LogicType> | ((logic: LogicType) => ReducerDefinitions<LogicType>)
+    events?: (logic: LogicType) => any | any
+    selectors?: (logic: LogicType) => any | any
+    listeners?: ListenerDefinitions<LogicType> | ((logic: LogicType) => ListenerDefinitions<LogicType>)
+}
+
+export function kea<LogicType>(input: LogicInput<LogicType>): LogicType {
+    return regularKea(input)
+}
+export function useValues<L extends BlankLogic>(logic: L): L['values'] {
+    return useRegularValues(logic)
+}
