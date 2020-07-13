4e1d1e70033ed2612c442345849578375f00bae5
Michael Matloka <dev@twixes.com>
2020-07-13 16:22:16 +0200

    Cohort filters (solves #952) (#1130)
    * Update UI for cohort filters
    
    * Change cohort filter interface slightly
    
    In TS notation a cohort filter is: `{
        key: string, // hashtag followed by cohort ID
        value: true,
        type: 'cohort'
    }`
    
    * Add server-side support for cohort filters
    
    * Optimize PropertyFilter and change tabs width
    
    * Refactor PropertyFilter for better maintainability
    
    * Fix property of undefined bug
    
    * Fix Select values
    
    * Test API
    
    * Resolve merge conflicts with master
    
    * Prettier JS
    
    * Add search to CohortPaneContents and element attributes
    
    * Make cohort filter property more elegant
    
    The relevant type is now
    ```TypeScript
    interface CohortProperty {
    	key: 'id',
    	value: number, // cohort ID, e.g. 42
    	type: 'cohort'
    }
    ```
    
    * Update and enhance test
    
    * Improve code style

--- posthog/api/test/test_action.py
+++ posthog/api/test/test_action.py
@@ -344,6 +344,46 @@ class TestTrends(TransactionBaseTest):
 
         self.assertTrue(self._compare_entity_response(action_response, event_response))
 
+    def test_filter_events_by_cohort(self):
+        person1 = Person.objects.create(team=self.team, distinct_ids=["person_1"], properties={"name": "John"})
+        person2 = Person.objects.create(team=self.team, distinct_ids=["person_2"], properties={"name": "Jane"})
+
+        event1 = Event.objects.create(
+            event="event_name",
+            team=self.team,
+            distinct_id="person_1",
+            properties={"$browser": "Safari"},
+            timestamp=datetime.now(),
+        )
+        event2 = Event.objects.create(
+            event="event_name",
+            team=self.team,
+            distinct_id="person_2",
+            properties={"$browser": "Chrome"},
+            timestamp=datetime.now(),
+        )
+        event3 = Event.objects.create(
+            event="event_name",
+            team=self.team,
+            distinct_id="person_2",
+            properties={"$browser": "Safari"},
+            timestamp=datetime.now(),
+        )
+
+        cohort = Cohort.objects.create(team=self.team, groups=[{"properties": {"name": "Jane"}}])
+        cohort.calculate_people()
+
+        with self.assertNumQueries(6):
+            response = self.client.get(
+                "/api/action/trends/",
+                data={
+                    "properties": jdumps([{"key": "id", "value": cohort.pk, "type": "cohort"}]),
+                    "events": jdumps([{"id": "event_name"}]),
+                },
+            ).json()
+        self.assertEqual(response[0]["count"], 2)
+        self.assertEqual(response[0]["data"][-1], 2)
+
     def test_date_filtering(self):
         self._create_events()
         with freeze_time("2020-01-02"):
