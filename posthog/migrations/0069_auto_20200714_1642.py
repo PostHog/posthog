98cbf1d281e23012a9a885b3068cd401f9501aa1
James Greenhill <jams@uber.com>
2020-07-14 09:43:01 -0700

    black format migration

--- posthog/migrations/0069_auto_20200714_1642.py
+++ posthog/migrations/0069_auto_20200714_1642.py
@@ -7,23 +7,27 @@ import django.utils.timezone
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posthog', '0068_auto_20200629_1322'),
+        ("posthog", "0068_auto_20200629_1322"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='action',
-            name='last_calculated_at',
+            model_name="action",
+            name="last_calculated_at",
             field=models.DateTimeField(blank=True, default=django.utils.timezone.now),
         ),
         migrations.AddField(
-            model_name='event',
-            name='created_at',
-            field=models.DateTimeField(auto_now_add=True, null=True),
+            model_name="event", name="created_at", field=models.DateTimeField(auto_now_add=True, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='toolbar_mode',
-            field=models.CharField(blank=True, choices=[('default', 'default'), ('toolbar', 'toolbar')], default='default', max_length=200, null=True),
+            model_name="user",
+            name="toolbar_mode",
+            field=models.CharField(
+                blank=True,
+                choices=[("default", "default"), ("toolbar", "toolbar")],
+                default="default",
+                max_length=200,
+                null=True,
+            ),
         ),
     ]
