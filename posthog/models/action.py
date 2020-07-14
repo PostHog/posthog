e1f94548bb2dff1d382d1924a08ab30895134dd8
James Greenhill <jams@uber.com>
2020-07-13 17:56:14 -0700

    Consolidate logic and handle nulls in created_at gracefully

--- posthog/models/action.py
+++ posthog/models/action.py
@@ -14,41 +14,58 @@ class Action(models.Model):
         ]
 
     def calculate_events(self, start=None, end=None):
+        recalculate_all = False
+        if start is None and end is None:
+            recalculate_all = True
         if start is None:
             start = datetime.date(1990, 1, 1)
         if end is None:
             end = timezone.now() + datetime.timedelta(days=1)
 
+        calculated_at = timezone.now()
         self.is_calculating = True
         self.save()
         from .event import Event
 
         try:
-            event_query, params = (
-                Event.objects.query_db_by_action(self, start=start, end=end).only("pk").query.sql_with_params()
-            )
+            if recalculate_all:
+                event_query, params = Event.objects.query_db_by_action(self).only("pk").query.sql_with_params()
+            else:
+                event_query, params = (
+                    Event.objects.query_db_by_action(self, start=start, end=end).only("pk").query.sql_with_params()
+                )
+
         except EmptyResultSet:
+            self.last_calculated_at = calculated_at
             self.is_calculating = False
             self.save()
             self.events.all().delete()
             return
 
-        query = """DELETE FROM "posthog_action_events"
-                   WHERE "action_id" = {}
-                   AND "event_id" in
+        query = """DELETE FROM "posthog_action_events" WHERE "action_id" = {}""".format(self.pk)
+
+        period_delete_query = """AND "event_id" in
                        (SELECT id
                         FROM posthog_event
-                        WHERE "timestamp" >= '{}'
-                        AND "timestamp" < '{}');
+                        WHERE "created_at" >= '{}'
+                        AND "created_at" < '{}');
                 """.format(
-            self.pk, start.isoformat(), end.isoformat()
+            start.isoformat(), end.isoformat()
         )
-        query += """INSERT INTO "posthog_action_events" ("action_id", "event_id")
+
+        insert_query = """INSERT INTO "posthog_action_events" ("action_id", "event_id")
                         {}
                     ON CONFLICT DO NOTHING
                  """.format(
             event_query.replace("SELECT ", "SELECT {}, ".format(self.pk), 1)
         )
+
+        if not recalculate_all:
+            query += period_delete_query
+        else:
+            query += ";"
+
+        query += insert_query
 
         cursor = connection.cursor()
         with transaction.atomic():
@@ -58,7 +75,7 @@ class Action(models.Model):
                 capture_exception()
 
         self.is_calculating = False
-        self.last_calculated_at = timezone.now()
+        self.last_calculated_at = calculated_at
         self.save()
 
     name: models.CharField = models.CharField(max_length=400, null=True, blank=True)
