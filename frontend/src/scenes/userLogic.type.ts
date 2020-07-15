c9dd205d851f050f550ad8a130ea200cddb19172
Marius Andra <marius.andra@gmail.com>
2020-07-15 09:54:52 +0200

    kea-typegen watch mode

--- frontend/src/scenes/userLogic.type.ts
+++ frontend/src/scenes/userLogic.type.ts
@@ -1,4 +1,4 @@
-// Auto-generated with kea-typegen v0.0.7. DO NOT EDIT!
+// Auto-generated with kea-typegen v0.0.10. DO NOT EDIT!
 
 export interface userLogicType<UserType> {
     actionCreators: {
