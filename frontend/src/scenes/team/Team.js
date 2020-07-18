56520b6f04a9113fc12e37243242aa14e2ff3562
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-07-18 14:59:46 -0500

    feat(team): delete users from your team, front-end

--- frontend/src/scenes/team/Team.js
+++ frontend/src/scenes/team/Team.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import { Card, Loading } from 'lib/utils'
-import { Table } from 'antd'
-import { useValues } from 'kea'
+import { Table, Modal } from 'antd'
+import { useValues, useActions } from 'kea'
 import { teamLogic } from './teamLogic'
+import { DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
 
 function Team() {
     const logic = teamLogic()
     const { users, usersLoading } = useValues(logic)
+    const { deleteUser } = useActions(logic)
+    const { confirm } = Modal
+
+    const ActionsComponent = (_text, record) => {
+        const handleClick = () => {
+            confirm({
+                title: 'Are you sure delete this user?',
+                icon: <ExclamationCircleOutlined />,
+                content: 'The user will be permanently deleted. This action cannot be undone.',
+                okText: 'Yes',
+                okType: 'danger',
+                cancelText: 'No',
+                onOk() {
+                    deleteUser(record)
+                },
+            })
+        }
+
+        return (
+            <div>
+                <a className="text-danger" onClick={handleClick}>
+                    <DeleteOutlined />
+                </a>
+            </div>
+        )
+    }
+
+    ActionsComponent.displayName = 'ActionsComponent'
 
     const columns = [
         {
@@ -23,12 +52,15 @@ function Team() {
             title: 'Actions',
             dataIndex: 'actions',
             key: 'actions',
+            align: 'center',
+            render: ActionsComponent,
         },
     ]
 
     return (
         <div>
             <h1 className="page-header">Team</h1>
+            <p>This is the list of all the users with access to PostHog for your team.</p>
             <Card>
                 {usersLoading && (
                     <div className="loading-overlay mt-5">
