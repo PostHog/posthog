722a6b651b0badbaeef638f350a2e4a47e5867c1
Eric <eeoneric@gmail.com>
2020-07-20 15:29:02 -0400

    add modal

--- frontend/src/scenes/annotations/AnnotationsTable.tsx
+++ frontend/src/scenes/annotations/AnnotationsTable.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useValues } from 'kea'
-import { Table, Tag } from 'antd'
+import { Table, Tag, Button, Modal, Input } from 'antd'
 import { Link } from 'lib/components/Link'
 import { humanFriendlyDetailedTime } from 'lib/utils'
+
+const { TextArea } = Input
 
 interface Props {
     logic: any
@@ -11,7 +13,8 @@ interface Props {
 export function AnnotationsTable(props: Props) {
     const { logic } = props;
     const { annotations, annotationsLoading } = useValues(logic)
-    
+    const [open, setOpen] = useState(false)
+    console.log(annotations)
     let columns = [
         {
             title: 'Annotation',
@@ -64,17 +67,66 @@ export function AnnotationsTable(props: Props) {
             },
         },
     ]
-    console.log(annotations)
+
     return <div>
+        <h1 className="page-header">Annotations</h1>
+        <Button className="mb-4" type="primary" data-attr="create-annotation" onClick={() => setOpen(true)}>
+            + Create Global Annotation
+        </Button>
         <Table
-                data-attr="annotations-table"
-                size="small"
-                rowKey={(item) => item.id}
-                pagination={{ pageSize: 99999, hideOnSinglePage: true }}
-                rowClassName="cursor-pointer"
-                dataSource={annotations}
-                columns={columns}
-                loading={annotationsLoading}
-            />
+            data-attr="annotations-table"
+            size="small"
+            rowKey={(item) => item.id}
+            pagination={{ pageSize: 99999, hideOnSinglePage: true }}
+            rowClassName="cursor-pointer"
+            dataSource={annotations}
+            columns={columns}
+            loading={annotationsLoading}
+        />
+        <CreateAnnotationModal visible={open} onCancel={() => setOpen(false)} onSubmit={(input) => {}}></CreateAnnotationModal>
     </div>
+}
+
+interface CreateAnnotationModalProps {
+    visible: boolean,
+    onCancel: () => void,
+    onSubmit: (input: string) => void
+}
+
+function CreateAnnotationModal(props: CreateAnnotationModalProps) {
+    const [textInput, setTextInput] = useState('')
+
+    return (
+        <Modal 
+            footer={[
+                <Button key="create-annotation-cancel" onClick={() => props.onCancel()}>
+                    Cancel
+                </Button>
+                ,
+                <Button
+                    type="primary"
+                    key="create-annotation-submit"
+                    onClick={() => {
+                        props.onSubmit(textInput)
+                    }}
+                >
+                    Submit
+                </Button>,
+            ]} 
+            closable={false} 
+            visible={props.visible} 
+            onCancel={props.onCancel}
+            title={"Create a Global Annotation"}
+        >
+            <span>This annotation will appear on all charts</span>
+            <TextArea 
+                maxLength={300}
+                style={{ marginBottom: 12, marginTop: 12 }}
+                rows={4}
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value)}>
+
+            </TextArea>
+        </Modal>
+    )
 }
