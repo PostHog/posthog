d9fe0cd21467b55d914eb2cd31bf8afde35a5ed1
Sandeep Guptan <sandeepguptan1998@gmail.com>
2020-07-20 10:27:09 -0700

    Date interval enhancement (#1253)
    * added modification to date selection as per the issue for automation selection
    
    * added modification to date selection as per the issue for automation selection

--- frontend/src/lib/components/IntervalFilter.js
+++ frontend/src/lib/components/IntervalFilter.js
@@ -11,7 +11,8 @@ let intervalMapping = {
 }
 
 export function IntervalFilter({ filters, setFilters, disabled = false }) {
-    const { interval, date_from } = filters
+    const { interval } = filters
+    let date_from
     return (
         <Select
             bordered={false}
@@ -20,12 +21,34 @@ export function IntervalFilter({ filters, setFilters, disabled = false }) {
             value={intervalMapping[interval]}
             dropdownMatchSelectWidth={false}
             onChange={(key) => {
+                switch (key) {
+                    case 'minute':
+                        date_from = 'dStart'
+                        break
+                    case 'hour':
+                        date_from = 'dStart'
+                        break
+                    case 'week':
+                        date_from = '-30d'
+                        break
+                    case 'month':
+                        date_from = '-90d'
+                        break
+                    default:
+                        date_from = undefined
+                        break
+                }
                 const minute_disabled = key === 'minute' && disableMinuteFor[date_from]
                 const hour_disabled = key === 'hour' && disableHourFor[date_from]
                 if (minute_disabled || hour_disabled) {
                     return false
                 }
-                setFilters({ interval: key })
+
+                if (date_from) {
+                    setFilters({ interval: key, date_from: date_from })
+                } else {
+                    setFilters({ interval: key })
+                }
             }}
             data-attr="interval-filter"
         >
