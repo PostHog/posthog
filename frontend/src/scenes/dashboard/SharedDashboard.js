6cd73870e8c8a3bab21e3ab9137222fad53043c1
Tim Glaser <tim.glaser@hiberly.com>
2020-07-21 10:21:28 +0200

    Use grid for responsive header

--- frontend/src/scenes/dashboard/SharedDashboard.js
+++ frontend/src/scenes/dashboard/SharedDashboard.js
@@ -9,20 +9,27 @@ import { initKea } from '~/initKea'
 import { Dashboard } from './Dashboard'
 
 import PostHogLogo from './../../../public/posthog-logo.svg'
+import { Col, Row } from 'antd'
 
 initKea()
 
 let dashboard = window.__SHARED_DASHBOARD__
 ReactDOM.render(
     <Provider store={getContext().store}>
-        <div style={{ background: 'var(--gray-background)', minHeight: '100vh', top: 0, padding: '2rem' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem' }}>
-                <a href="https://posthog.com" target="_blank" rel="noopener noreferrer">
-                    <img src={PostHogLogo} style={{ height: '2rem', marginTop: 4 }} />
-                </a>
-                <h1 style={{ display: 'inline-block' }}>{dashboard.name}</h1>
-                <span style={{ marginTop: 15 }}>{dashboard.team_name}</span>
-            </div>
+        <div style={{ background: 'var(--gray-background)', minHeight: '100vh', top: 0 }}>
+            <Row style={{ marginBottom: '1rem' }}>
+                <Col sm={7} xs={24} style={{ padding: '1rem' }}>
+                    <a href="https://posthog.com" target="_blank" rel="noopener noreferrer">
+                        <img src={PostHogLogo} style={{ height: '2rem', marginTop: 4 }} />
+                    </a>
+                </Col>
+                <Col sm={10} xs={24} style={{ padding: '1rem' }}>
+                    <h1 style={{ textAlign: 'center' }}>{dashboard.name}</h1>
+                </Col>
+                <Col sm={7} xs={0} style={{ padding: '1rem', textAlign: 'right' }}>
+                    <span style={{ paddingTop: 15, display: 'inline-block' }}>{dashboard.team_name}</span>
+                </Col>
+            </Row>
             <Dashboard id={dashboard.id} share_token={dashboard.share_token} />
 
             <div style={{ textAlign: 'center' }}>
