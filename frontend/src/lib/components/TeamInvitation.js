c5b61012cbbfa9a29e4f72342ff1b11d35e45fd8
Twixes <dev@twixes.com>
2020-07-24 11:17:06 +0200

    Refactor TeamInvitation

--- frontend/src/lib/components/TeamInvitation.js
+++ frontend/src/lib/components/TeamInvitation.js
@@ -1,32 +1,12 @@
-import React, { useRef, useCallback } from 'react'
-import { toast } from 'react-toastify'
-import { Tooltip, Modal, Input } from 'antd'
-import { CopyOutlined } from '@ant-design/icons'
+import React from 'react'
+import { Modal } from 'antd'
+import { CopyToClipboard } from 'lib/components/CopyToClipboard'
 
 export function TeamInvitationLink({ user }) {
-    const url = window.location.origin
-
-    const inputRef = useRef()
-
-    const copyToClipboard = useCallback(() => {
-        inputRef.current.focus()
-        inputRef.current.select()
-        document.execCommand('copy')
-        inputRef.current.blur()
-        toast('Team invitation link copied!')
-    }, [inputRef])
-
     return (
-        <Input
+        <CopyToClipboard
             data-attr="copy-invite-to-clipboard-input"
-            type="text"
-            ref={inputRef}
-            value={url + '/signup/' + user.team.signup_token}
-            suffix={
-                <Tooltip title="Copy to clipboard">
-                    <CopyOutlined onClick={copyToClipboard} />
-                </Tooltip>
-            }
+            url={window.location.origin + '/signup/' + user.team.signup_token}
         />
     )
 }
