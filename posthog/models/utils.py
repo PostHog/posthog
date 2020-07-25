07085c6242dbdd0fd86b021a6477dd323e890b03
Twixes <dev@twixes.com>
2020-07-25 23:52:17 +0200

    Add generate_random_token() model util

--- posthog/models/utils.py
+++ posthog/models/utils.py
@@ -1,8 +1,14 @@
 from collections import namedtuple
+import secrets
 
 
 def namedtuplefetchall(cursor):
-    "Return all rows from a cursor as a namedtuple"
+    """Return all rows from a cursor as a namedtuple"""
     desc = cursor.description
     nt_result = namedtuple("Result", [col[0] for col in desc])  # type: ignore
     return [nt_result(*row) for row in cursor.fetchall()]
+
+
+def generate_random_token(length: int = 32) -> str:
+    """Generate a securely random token"""
+    return secrets.token_urlsafe(length)
