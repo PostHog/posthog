c26386581940ce6f92f457e6a60dea434c50b571
Marius Andra <marius.andra@gmail.com>
2020-07-31 13:11:19 +0200

    respect userIntent prop when opening the toolbar

--- frontend/src/toolbar/toolbarLogic.ts
+++ frontend/src/toolbar/toolbarLogic.ts
@@ -1,19 +1,28 @@
 import { kea } from 'kea'
 import { toolbarLogicType } from '~/toolbar/toolbarLogicType'
 import { EditorProps } from '~/types'
+import { dockLogic } from '~/toolbar/dockLogic'
+import { actionsTabLogic } from '~/toolbar/actions/actionsTabLogic'
+import { toolbarButtonLogic } from '~/toolbar/button/toolbarButtonLogic'
+import { toolbarTabLogic } from '~/toolbar/toolbarTabLogic'
+import { actionsLogic } from '~/toolbar/actions/actionsLogic'
 
 // input: props = all editorProps
 export const toolbarLogic = kea<toolbarLogicType>({
+    props: {} as EditorProps,
+
     actions: () => ({
         authenticate: true,
+        processUserIntent: true,
+        clearUserIntent: true,
     }),
 
     reducers: ({ props }: { props: EditorProps }) => ({
         rawApiURL: [props.apiURL as string],
         rawJsURL: [(props.jsURL || props.apiURL) as string],
         temporaryToken: [props.temporaryToken || null],
-        actionId: [props.actionId || null],
-        userIntent: [props.userIntent || null],
+        actionId: [props.actionId || null, { clearUserIntent: () => null }],
+        userIntent: [props.userIntent || null, { clearUserIntent: () => null }],
     }),
 
     selectors: ({ selectors }) => ({
@@ -22,10 +31,46 @@ export const toolbarLogic = kea<toolbarLogicType>({
         isAuthenticated: [() => [selectors.temporaryToken], (temporaryToken) => !!temporaryToken],
     }),
 
-    listeners: ({ values }) => ({
+    listeners: ({ values, props, actions }) => ({
         authenticate: () => {
             const encodedUrl = encodeURIComponent(window.location.href)
             window.location.href = `${values.apiURL}authorize_and_redirect/?redirect=${encodedUrl}`
+        },
+        processUserIntent: async () => {
+            if (props.userIntent === 'add-action' || props.userIntent === 'edit-action') {
+                const { mode } = dockLogic.values
+
+                if (mode === '') {
+                    dockLogic.actions.setMode('button')
+                }
+
+                if (mode === '' || mode === 'button') {
+                    actionsTabLogic.actions.showButtonActions()
+                    toolbarButtonLogic.actions.showActionsInfo()
+                } else if (mode === 'dock') {
+                    toolbarTabLogic.actions.setTab('actions')
+                }
+
+                if (props.userIntent === 'add-action') {
+                    actionsTabLogic.actions.newAction()
+                    actions.clearUserIntent()
+                }
+
+                if (props.userIntent === 'edit-action') {
+                    if (actionsLogic.values.allActions.length > 0) {
+                        actionsTabLogic.actions.selectAction(props.actionId)
+                        actions.clearUserIntent()
+                    } else {
+                        // listen to getActionsSuccess in actionsTabLogic
+                    }
+                }
+            }
+        },
+    }),
+
+    events: ({ actions }) => ({
+        afterMount() {
+            actions.processUserIntent()
         },
     }),
 })
