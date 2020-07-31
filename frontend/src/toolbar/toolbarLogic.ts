72cfee38e5c311b6f48c1e54bbe3d36b5af75815
Marius Andra <marius.andra@gmail.com>
2020-07-31 13:36:29 +0200

    refactor opening the right actions view

--- frontend/src/toolbar/toolbarLogic.ts
+++ frontend/src/toolbar/toolbarLogic.ts
@@ -4,8 +4,6 @@ import { EditorProps } from '~/types'
 import { dockLogic } from '~/toolbar/dockLogic'
 import { actionsTabLogic } from '~/toolbar/actions/actionsTabLogic'
 import { toolbarButtonLogic } from '~/toolbar/button/toolbarButtonLogic'
-import { toolbarTabLogic } from '~/toolbar/toolbarTabLogic'
-import { actionsLogic } from '~/toolbar/actions/actionsLogic'
 
 // input: props = all editorProps
 export const toolbarLogic = kea<toolbarLogicType>({
@@ -31,39 +29,17 @@ export const toolbarLogic = kea<toolbarLogicType>({
         isAuthenticated: [() => [selectors.temporaryToken], (temporaryToken) => !!temporaryToken],
     }),
 
-    listeners: ({ values, props, actions }) => ({
+    listeners: ({ values, props }) => ({
         authenticate: () => {
             const encodedUrl = encodeURIComponent(window.location.href)
             window.location.href = `${values.apiURL}authorize_and_redirect/?redirect=${encodedUrl}`
         },
         processUserIntent: async () => {
             if (props.userIntent === 'add-action' || props.userIntent === 'edit-action') {
-                const { mode } = dockLogic.values
-
-                if (mode === '') {
-                    dockLogic.actions.setMode('button')
-                }
-
-                if (mode === '' || mode === 'button') {
-                    actionsTabLogic.actions.showButtonActions()
-                    toolbarButtonLogic.actions.showActionsInfo()
-                } else if (mode === 'dock') {
-                    toolbarTabLogic.actions.setTab('actions')
-                }
-
-                if (props.userIntent === 'add-action') {
-                    actionsTabLogic.actions.newAction()
-                    actions.clearUserIntent()
-                }
-
-                if (props.userIntent === 'edit-action') {
-                    if (actionsLogic.values.allActions.length > 0) {
-                        actionsTabLogic.actions.selectAction(props.actionId)
-                        actions.clearUserIntent()
-                    } else {
-                        // listen to getActionsSuccess in actionsTabLogic
-                    }
-                }
+                dockLogic.actions.button()
+                actionsTabLogic.actions.showButtonActions()
+                toolbarButtonLogic.actions.showActionsInfo()
+                // the right view will next be opened in `actionsTabLogic` on `getActionsSuccess`
             }
         },
     }),
