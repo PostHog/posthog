0bed9b879f236680429a4aff3277740b82399216
Marius Andra <marius.andra@gmail.com>
2020-08-07 02:58:36 +0200

    remove samesite middleware (#1384)

--- posthog/settings.py
+++ posthog/settings.py
@@ -120,7 +120,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "posthog.middleware.SameSiteSessionMiddleware",  # keep this at the top
     "django.middleware.security.SecurityMiddleware",
     "posthog.middleware.AllowIP",
     "django.contrib.sessions.middleware.SessionMiddleware",
