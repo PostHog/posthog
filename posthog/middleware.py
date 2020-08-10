d71997500123c6df1e2399978a0d33eff03c6133
Marius Andra <marius.andra@gmail.com>
2020-08-10 10:42:10 +0200

    skip adding new toolbar cookie on API requests

--- posthog/middleware.py
+++ posthog/middleware.py
@@ -63,13 +63,18 @@ class ToolbarCookieMiddleware(SessionMiddleware):
     def process_response(self, request, response):
         response = super(ToolbarCookieMiddleware, self).process_response(request, response)
 
+        # skip adding the toolbar 3rd party cookie on API requests
+        if request.path.startswith("/api/") or request.path.startswith("/e/") or request.path.startswith("/decide/"):
+            return response
+
         toolbar_cookie_name = settings.TOOLBAR_COOKIE_NAME  # type: str
         toolbar_cookie_secure = settings.TOOLBAR_COOKIE_SECURE  # type: bool
 
         if (
-            request.user.is_authenticated
+            toolbar_cookie_name not in response.cookies
+            and request.user
+            and request.user.is_authenticated
             and request.user.toolbar_mode == "toolbar"
-            and toolbar_cookie_name not in response.cookies
         ):
             response.set_cookie(
                 toolbar_cookie_name,  # key
