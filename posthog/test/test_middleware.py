e34f4f732041f6736d83636583a68b9cbd481bea
Marius Andra <marius.andra@gmail.com>
2020-08-10 22:16:43 +0200

    add test for toolbar cookie middleware

--- posthog/test/test_middleware.py
+++ posthog/test/test_middleware.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.test import Client, TestCase
+
+from posthog.models import Team, User
 
 
 class TestSignup(TestCase):
@@ -68,3 +71,69 @@ class TestSignup(TestCase):
             with self.settings(TRUST_ALL_PROXIES=True):
                 response = self.client.get("/", REMOTE_ADDR="10.0.0.1", HTTP_X_FORWARDED_FOR="192.168.0.1,10.0.0.1",)
                 self.assertNotIn(b"IP is not allowed", response.content)
+
+
+class TestToolbarCookieMiddleware(TestCase):
+    def _create_user(self, email, **kwargs) -> User:
+        user = User.objects.create_user(email, **kwargs)
+        self.team.users.add(user)
+        self.team.save()
+        return user
+
+    def setUp(self):
+        super().setUp()
+        self.team: Team = Team.objects.create(api_token="token123")
+        self.user = self._create_user("user1", password="pass123")
+        self.client = Client()
+
+    def test_logged_out_client(self):
+        response = self.client.get("/")
+        self.assertEqual(0, len(response.cookies))
+
+    def test_logged_in_client(self):
+        with self.settings(TOOLBAR_COOKIE_NAME="phtoolbar", TOOLBAR_COOKIE_SECURE=False):
+            self.client.force_login(self.user)
+
+            response = self.client.get("/")
+            toolbar_cookie = response.cookies[settings.TOOLBAR_COOKIE_NAME]
+
+            self.assertEqual(toolbar_cookie.key, settings.TOOLBAR_COOKIE_NAME)
+            self.assertEqual(toolbar_cookie.value, "yes")
+            self.assertEqual(toolbar_cookie["path"], "/")
+            self.assertEqual(toolbar_cookie["samesite"], "None")
+            self.assertEqual(toolbar_cookie["httponly"], True)
+            self.assertEqual(toolbar_cookie["domain"], "")
+            self.assertEqual(toolbar_cookie["comment"], "")
+            self.assertEqual(toolbar_cookie["secure"], "")
+            self.assertEqual(toolbar_cookie["max-age"], 31536000)
+
+    def test_logged_in_client_secure(self):
+        with self.settings(TOOLBAR_COOKIE_NAME="phtoolbar", TOOLBAR_COOKIE_SECURE=True):
+            self.client.force_login(self.user)
+
+            response = self.client.get("/")
+            toolbar_cookie = response.cookies[settings.TOOLBAR_COOKIE_NAME]
+
+            self.assertEqual(toolbar_cookie.key, "phtoolbar")
+            self.assertEqual(toolbar_cookie.value, "yes")
+            self.assertEqual(toolbar_cookie["path"], "/")
+            self.assertEqual(toolbar_cookie["samesite"], "None")
+            self.assertEqual(toolbar_cookie["httponly"], True)
+            self.assertEqual(toolbar_cookie["domain"], "")
+            self.assertEqual(toolbar_cookie["comment"], "")
+            self.assertEqual(toolbar_cookie["secure"], True)
+            self.assertEqual(toolbar_cookie["max-age"], 31536000)
+
+    def test_logout(self):
+        with self.settings(TOOLBAR_COOKIE_NAME="phtoolbar"):
+            self.client.force_login(self.user)
+
+            response = self.client.get("/")
+            self.assertEqual(response.cookies[settings.TOOLBAR_COOKIE_NAME].key, "phtoolbar")
+            self.assertEqual(response.cookies[settings.TOOLBAR_COOKIE_NAME].value, "yes")
+            self.assertEqual(response.cookies[settings.TOOLBAR_COOKIE_NAME]["max-age"], 31536000)
+
+            response = self.client.get("/logout")
+            self.assertEqual(response.cookies[settings.TOOLBAR_COOKIE_NAME].key, "phtoolbar")
+            self.assertEqual(response.cookies[settings.TOOLBAR_COOKIE_NAME].value, "")
+            self.assertEqual(response.cookies[settings.TOOLBAR_COOKIE_NAME]["max-age"], 0)
