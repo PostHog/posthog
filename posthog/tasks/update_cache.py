1b16cc5910b4c2ce0aba7e94ea52d7508f5d2bb1
Eric <eeoneric@gmail.com>
2020-08-11 11:13:45 -0400

    fix types

--- posthog/tasks/update_cache.py
+++ posthog/tasks/update_cache.py
@@ -2,16 +2,14 @@ import json
 import logging
 from typing import Any, Dict, List, Optional, Union
 
-from celery import group, shared_task
 from dateutil.relativedelta import relativedelta
 from django.core.cache import cache
 from django.db.models import Prefetch, Q
 from django.utils import timezone
 
-from posthog.api.funnel import FunnelSerializer
-from posthog.celery import app, update_cache_item_task
+from posthog.celery import update_cache_item_task
 from posthog.decorators import FUNNEL_ENDPOINT, TRENDS_ENDPOINT
-from posthog.models import Action, ActionStep, DashboardItem, Entity, Filter, Funnel, Team
+from posthog.models import Action, ActionStep, DashboardItem, Filter, Team
 from posthog.queries.funnel import Funnel
 from posthog.queries.trends import Trends
 from posthog.utils import generate_cache_key
