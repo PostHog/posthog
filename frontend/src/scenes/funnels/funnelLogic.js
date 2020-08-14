0bce9b4a71418e1c3fc842246d32f3422b6ce770
Eric <eeoneric@gmail.com>
2020-08-14 10:42:13 -0400

    fix clear condition

--- frontend/src/scenes/funnels/funnelLogic.js
+++ frontend/src/scenes/funnels/funnelLogic.js
@@ -69,7 +69,7 @@ export const funnelLogic = kea({
             {},
             {
                 setFilters: (state, { filters }) => ({ ...state, ...filters }),
-                clearFunnel: () => ({}),
+                clearFunnel: (state) => ({ new_entity: state.new_entity }),
             },
         ],
         stepsWithCount: [
