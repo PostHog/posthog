295e59b6c26c85393bff3213bfbcf2841f2b3d65
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-08-16 10:05:24 -0500

    fix: minor comment adjustments

--- posthog/test/test_team_model.py
+++ posthog/test/test_team_model.py
@@ -5,24 +5,24 @@ from ..models.team import Team
 
 class TestTeam(TestCase):
     def test_team_str(self):
-        # 1. Use the team.name by default
+        # Use the team.name by default
         team: Team = Team.objects.create(name="The Mighty Library")
         self.assertEqual(str(team), "The Mighty Library")
 
-        # 2. If the team has no name:
+        # If the team has no name:
 
-        # 2.1 Team has one app_url
+        # Team has one app_url
         team = Team.objects.create(app_urls=["app.posthog.com"])
         self.assertEqual(str(team), "app.posthog.com")
 
-        # 2.2 Team has multiple app_urls
+        # Team has multiple app_urls
         team = Team.objects.create(app_urls=["app.posthog.com", "custom.posthog.com"])
         self.assertEqual(str(team), "app.posthog.com, custom.posthog.com")
 
-        # 2.3 Team has empty app_url
+        # Team has empty app_url
         team = Team.objects.create(app_urls=[""])
         self.assertEqual(str(team), str(team.pk))
 
-        # 2.3 Team has no app_urls
+        # Team has no app_urls
         team = Team.objects.create(app_urls=[])
         self.assertEqual(str(team), str(team.pk))
