ef165017f4ec54fe00d817f5e59bab0167f42538
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-08-17 12:33:42 -0500

    fix(billing): handle custom plan/pricing schemes (#1450)

--- posthog/test/test_user_model.py
+++ posthog/test/test_user_model.py
@@ -23,6 +23,14 @@ class TestUser(BaseTest):
         patch_team_billing.objects.get().price_id = "price_1234567890"
         self.assertTrue(self.user.feature_available("whatever"))
 
+    @patch("posthog.models.user.MULTI_TENANCY_MISSING", False)
+    @patch("posthog.models.user.TeamBilling")
+    def test_custom_pricing_no_extra_features(self, patch_team_billing):
+        patch_team_billing.objects.get().price_id = (
+            "price_test_1"  # price_test_1 is not on posthog.models.user.License.PLANS
+        )
+        self.assertFalse(self.user.feature_available("whatever"))
+
     @patch("posthog.models.user.License.PLANS", {"enterprise": ["whatever"]})
     @patch("ee.models.license.requests.post")
     def test_feature_available_self_hosted_has_license(self, patch_post):
