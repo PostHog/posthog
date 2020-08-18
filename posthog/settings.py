44f45b86c668eea1c22f8750059dcd17a0430552
Michael Matloka <dev@twixes.com>
2020-08-18 12:02:31 +0200

    Auto update VERSION (#1452)
    * Auto update VERSION
    
    * Use SYNC_GITHUB_TOKEN
    
    * Fix minor oversights
    
    * Fix auto commit

--- posthog/settings.py
+++ posthog/settings.py
@@ -22,8 +22,6 @@ import sentry_sdk
 from django.core.exceptions import ImproperlyConfigured
 from sentry_sdk.integrations.django import DjangoIntegration
 
-VERSION = "1.13.0"
-
 
 def get_env(key):
     try:
