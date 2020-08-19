4fd8dd7ff5acf3311eac2bebf008166cb2f47c6c
Twixes <dev@twixes.com>
2020-08-19 09:22:20 +0200

    Use TestMixin

--- posthog/api/test/base.py
+++ posthog/api/test/base.py
@@ -3,7 +3,7 @@ from django.test import Client, TestCase, TransactionTestCase
 from posthog.models import Team, User
 
 
-class BaseTest(TestCase):
+class TestMixin:
     TESTS_API: bool = False
     TESTS_PASSWORD: str = "testpassword12345"
 
@@ -22,20 +22,9 @@ class BaseTest(TestCase):
             self.client.force_login(self.user)
 
 
-class TransactionBaseTest(TransactionTestCase):
-    TESTS_API: bool = False
-    TESTS_PASSWORD: str = "testpassword12345"
+class BaseTest(TestMixin, TestCase):
+    pass
 
-    def _create_user(self, email, **kwargs) -> User:
-        user = User.objects.create_user(email, **kwargs)
-        self.team.users.add(user)
-        self.team.save()
-        return user
 
-    def setUp(self):
-        super().setUp()
-        self.team: Team = Team.objects.create(api_token="token123")
-        if self.TESTS_API:
-            self.client = Client()
-            self.user = self._create_user("user1", password=self.TESTS_PASSWORD)
-            self.client.force_login(self.user)
+class TransactionBaseTest(TestMixin, TransactionTestCase):
+    pass
