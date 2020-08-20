d0421975db8ba098f81bd6792d03584461597e9a
Twixes <dev@twixes.com>
2020-08-20 16:38:20 +0200

    Improve typing

--- frontend/src/lib/components/PersonalAPIKeys/personalAPIKeysLogic.tsx
+++ frontend/src/lib/components/PersonalAPIKeys/personalAPIKeysLogic.tsx
@@ -3,18 +3,19 @@ import React from 'react'
 import { toast } from 'react-toastify'
 import api from 'lib/api'
 import { PersonalAPIKeyType } from '~/types'
+import { personalAPIKeysLogicType } from '~/types/lib/components/PersonalAPIKey/personalAPIKeysLogicType'
 
-export const personalAPIKeysLogic = kea({
+export const personalAPIKeysLogic = kea<personalAPIKeysLogicType<PersonalAPIKeyType>>({
     loaders: ({ values }) => ({
         keys: [
             [] as PersonalAPIKeyType[],
             {
                 loadKeys: async () => {
-                    const response = await api.get('api/personal_api_keys/')
+                    const response: PersonalAPIKeyType[] = await api.get('api/personal_api_keys/')
                     return response
                 },
                 createKey: async (label: string) => {
-                    const newKey = await api.create('api/personal_api_keys/', { label })
+                    const newKey: PersonalAPIKeyType = await api.create('api/personal_api_keys/', { label })
                     return [newKey, ...values.keys]
                 },
                 deleteKey: async (key: PersonalAPIKeyType) => {
@@ -26,10 +27,10 @@ export const personalAPIKeysLogic = kea({
     }),
 
     listeners: () => ({
-        createKeySuccess: ({ keys }) => {
+        createKeySuccess: ({ keys }: { keys: PersonalAPIKeyType[] }) => {
             toast(<div className="text-success">Personal API key "{keys[0].label}" created.</div>)
         },
-        deleteKeySuccess: ({ keys }) => {
+        deleteKeySuccess: ({ keys }: { keys: PersonalAPIKeyType[] }) => {
             toast(<div className="text-success">Personal API key "{keys[0].label}" deleted.</div>)
         },
     }),
