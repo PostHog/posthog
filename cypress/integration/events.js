ab0115392ea76a0783d905ab189dcec01c8a841b
Twixes <dev@twixes.com>
2020-08-22 17:57:18 +0200

    Fix selector

--- cypress/integration/events.js
+++ cypress/integration/events.js
@@ -4,18 +4,18 @@ describe('Events', () => {
     })
 
     it('Events loaded', () => {
-        cy.get('[data-attr=events-table').should('exist')
+        cy.get('[data-attr=events-table]').should('exist')
     })
 
     it('Click on an event', () => {
         cy.get('[data-attr=events-table] .event-row:first-child td:first-child').click()
-        cy.get('[data-attr=event-details').should('exist')
+        cy.get('[data-attr=event-details]').should('exist')
     })
 
     it('All events route works', () => {
         cy.get('[data-attr=menu-item-all-events]').click()
 
-        cy.get('[data-attr=events-table').should('exist')
+        cy.get('[data-attr=events-table]').should('exist')
     })
 
     it('Apply 1 overall filter', () => {
@@ -23,13 +23,13 @@ describe('Events', () => {
         cy.get('[data-attr=prop-filter-event-0]').click()
         cy.get('[data-attr=prop-val]').click()
         cy.get('[data-attr=prop-val-0]').click()
-        cy.get('[data-attr=events-table').should('exist')
+        cy.get('[data-attr=events-table]').should('exist')
     })
 
     it('Filter by event', () => {
         cy.get('[data-attr=event-filter-trigger]').click()
         cy.get('[data-attr=event-name-box]').click()
         cy.get('[data-attr=prop-val-0]').click()
-        cy.get('[data-attr=events-table').should('exist')
+        cy.get('[data-attr=events-table]').should('exist')
     })
 })
