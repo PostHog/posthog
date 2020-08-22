0a488be7b89f4660b40407533c7f4d717c0ea152
Twixes <dev@twixes.com>
2020-08-22 22:54:42 +0200

    User kea-router for All Users category tab

--- frontend/src/scenes/users/People.js
+++ frontend/src/scenes/users/People.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
+import { useValues, useActions } from 'kea'
+import { router } from 'kea-router'
 import api from 'lib/api'
 import { fromParams } from 'lib/utils'
 import { Cohort } from './Cohort'
@@ -17,50 +19,61 @@ const defaultPaginationObj = {
 
 export const People = hot(_People)
 function _People() {
-    const [loading, setLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
     const [people, setPeople] = useState(null)
     const [search, setSearch] = useState('')
     const [cohortId, setCohortId] = useState(fromParams()['cohort'])
-    const [usersType, setUsersType] = useState('all')
     const [pagination, setPagination] = useState({ ...defaultPaginationObj })
+    const { push } = useActions(router)
+    const {
+        searchParams: { category: categoryRaw = 'all' },
+    } = useValues(router)
 
-    function fetchPeople({ url, scrollTop, selection }) {
-        setLoading(true)
-        let currentTab = selection ? selection : usersType
-        if (selection) setUsersType(selection)
+    // ensure that there's no invalid category error
+    const category = useMemo(
+        () => (['all', 'identified', 'anonymous'].includes(categoryRaw) ? categoryRaw : 'all'),
+        categoryRaw
+    )
+
+    function fetchPeople(url, scrollTop, categoryOverride) {
+        setIsLoading(true)
+        let categoryLocal = categoryOverride || category
         if (scrollTop)
             document.querySelector('section.ant-layout > .content').parentNode.scrollTo({ top: 0, behavior: 'smooth' })
         if (!url) {
             url = 'api/person/'
-            const query_params = []
+            const query_params = [`category=${categoryLocal}`]
             if (search) query_params.push(`search=${search}`)
             if (cohortId) query_params.push(`cohort=${cohortId}`)
-            if (currentTab === 'identified' || currentTab === 'anonymous') query_params.push(`category=${currentTab}`)
             if (query_params.length) url += `?${query_params.join('&')}`
         }
-        api.get(url).then((data) => {
-            let newPagination = { ...pagination }
-            newPagination[currentTab].next = data.next
-            newPagination[currentTab].previous = data.previous
-            setPagination(newPagination)
-            setPeople(data.results)
-            setLoading(false)
-        })
+        api.get(url)
+            .then((data) => {
+                let newPagination = { ...pagination }
+                newPagination[categoryLocal].next = data.next
+                newPagination[categoryLocal].previous = data.previous
+                setPagination(newPagination)
+                setPeople(data.results)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }
 
     function tabHasPagination(direction) {
-        if (usersType === undefined) {
-            return direction === 'next' ? pagination['all'].next : pagination['all'].previous
-        }
-        return direction === 'next' ? pagination[usersType].next : pagination[usersType].previous
+        return direction === 'next' ? pagination[category].next : pagination[category].previous
     }
 
     useEffect(() => {
-        fetchPeople({})
+        fetchPeople()
     }, [cohortId])
 
+    useEffect(() => {
+        if (!['all', 'identified', 'anonymous'].includes(categoryRaw)) push('/people?category=all')
+    }, [categoryRaw])
+
     const exampleEmail =
-        (people && people.map((person) => person.properties.email).filter((d) => d)[0]) || 'example@gmail.com'
+        (people && people.find((person) => person?.properties?.email)?.properties?.email) || 'example@gmail.com'
 
     return (
         <div>
@@ -80,12 +93,19 @@ function _People() {
                 autoFocus
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
-                onKeyDown={(e) => e.keyCode === 13 && fetchPeople({})}
+                onKeyDown={(e) => e.keyCode === 13 && fetchPeople()}
                 placeholder={people && 'Try ' + exampleEmail + ' or has:email'}
                 style={{ maxWidth: 400 }}
             />
             <br />
-            <Tabs defaultActiveKey="all" onChange={(key) => fetchPeople({ selection: key })} type="card">
+            <Tabs
+                defaultActiveKey={category}
+                onChange={(key) => {
+                    push('/people', { category: key })
+                    fetchPeople(undefined, undefined, key)
+                }}
+                type="card"
+            >
                 <TabPane
                     tab={<span data-attr="people-types-tab">All Users</span>}
                     key="all"
@@ -102,20 +122,20 @@ function _People() {
                     data-attr="people-types-tab"
                 ></TabPane>
             </Tabs>
-            <PeopleTable people={people} loading={loading} actions={true} onChange={() => fetchPeople({})} />
+            <PeopleTable people={people} loading={isLoading} actions={true} onChange={() => fetchPeople()} />
 
             <div style={{ margin: '3rem auto 10rem', width: 200 }}>
                 <Button
                     type="link"
                     disabled={!tabHasPagination('previous')}
-                    onClick={() => fetchPeople({ url: pagination[usersType].previous, scrollTop: true })}
+                    onClick={() => fetchPeople(pagination[category].previous, true)}
                 >
                     <LeftOutlined style={{ verticalAlign: 'initial' }} /> Previous
                 </Button>
                 <Button
                     type="link"
                     disabled={!tabHasPagination('next')}
-                    onClick={() => fetchPeople({ url: pagination[usersType].next, scrollTop: true })}
+                    onClick={() => fetchPeople(pagination[category].next, true)}
                 >
                     Next <RightOutlined style={{ verticalAlign: 'initial' }} />
                 </Button>
