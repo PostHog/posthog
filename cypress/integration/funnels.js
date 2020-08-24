b172eee9201023f6dcd361b03a9f8cb11b1999f1
Eric Duong <eeoneric@gmail.com>
2020-08-24 17:02:13 -0400

    Insight history (#1379)
    * add insight history
    
    * add table
    
    * edit format
    
    * links working
    
    * add modal
    
    * add tab switch
    
    * saving insights
    
    * remove insights model
    
    * no model for insight history
    
    * initial working
    
    * remove wrong typegen
    
    * made the history per user
    
    * missing param
    
    * added funnel support
    
    * add details
    
    * add delete saved insight functionality
    
    * add line break logic and funnel name
    
    * fix reload logic
    
    * fix rendering error
    
    * add pagination component
    
    * fix bugs
    
    * add backend tests
    
    * add insight-history-panel check
    
    * add drawer and replace table
    
    * add loading next
    
    * add date
    
    * add callback
    
    * update test
    
    * use table
    
    * fix spacing
    
    * fix rows
    
    * adjust spacing
    
    * fix spacing and styling
    
    * fix spacing and styling
    
    * .
    
    * add missing package
    
    * Funnel refactor (#1381)
    
    * main funnel-insight page working
    
    * consolidated funnels
    
    * refactor funnel dashboard item
    
    * dashboard modals working
    
    * update demo creation
    
    * updated migration
    
    * fix frontend for funnels
    
    * remove funnel code and fix bugs
    
    * fix test and remove caching temp
    
    * remove old tests
    
    * fix types
    
    * added panel for past funnels
    
    * implement polling for funnel refactor
    
    * .
    
    * fix dashbaord item bug
    
    * add params
    
    * fix params
    
    * clean up caching functionality
    
    * fix types
    
    * caching working properly
    
    * fix dashboard update bug
    
    * fix migrations
    
    * fix check
    
    * update caching to work properly
    
    * Fix default
    
    * fix api and styling for insight history
    
    * more spacing
    
    * update funnel automatically
    
    * update tests
    
    * fix clear condition
    
    * merge insight history
    
    * return save functionality
    
    * remove file
    
    * added extra save functionality

--- cypress/integration/funnels.js
+++ cypress/integration/funnels.js
@@ -1,49 +1,32 @@
 describe('Funnels', () => {
     beforeEach(() => {
-        cy.get('[data-attr=menu-item-funnels]').click()
+        cy.visit('/')
+        cy.get('[data-attr=insight-funnels-tab]').click()
     })
 
-    it('Funnels loaded', () => {
-        cy.get('h1').should('contain', 'Funnels')
-    })
+    it('Add 1 action to funnel', () => {
+        cy.get('[data-attr=add-action-event-button]').click()
+        cy.get('[data-attr=trend-element-subject-0]').click()
+        cy.contains('Pageviews').click()
 
-    it('Click on a funnel', () => {
-        cy.get('[data-attr=funnel-link-0]').click()
-        cy.get('[data-attr=funnel-tab]').should('exist')
+        cy.get('[data-attr=save-funnel-button]').click()
+
+        cy.get('[data-attr=funnel-viz]').should('exist')
     })
 
     it('Apply date filter to funnel', () => {
-        cy.get('[data-attr=funnel-link-0]').click()
-        cy.get('[data-attr=funnel-tab]').should('exist')
+        cy.get('[data-attr=add-action-event-button]').click()
+        cy.get('[data-attr=trend-element-subject-0]').click()
+        cy.contains('Pageviews').click()
+        cy.get('[data-attr=save-funnel-button]', { timeout: 7000 }).click()
+
         cy.get('[data-attr=date-filter]').click()
         cy.contains('Last 30 days').click()
 
         cy.get('[data-attr=funnel-viz]').should('exist')
     })
 
-    it('Go to new funnel screen', () => {
-        cy.get('[data-attr=create-funnel]').click()
-        cy.get('[data-attr=funnel-tab]').should('exist')
-    })
-
-    it('Add 1 action to funnel', () => {
-        cy.get('[data-attr=create-funnel]').click()
-        cy.get('[data-attr=funnel-tab]').should('exist')
-
-        cy.get('[data-attr=edit-funnel-input]').type('Test funnel')
-        cy.get('[data-attr=add-action-event-button]').click()
-        cy.get('[data-attr=trend-element-subject-0]').click()
-        cy.contains('Pageviews').click()
-        cy.get('[data-attr=save-funnel-button]', { timeout: 7000 }).click()
-
-        cy.get('[data-attr=funnel-viz]').should('exist')
-    })
-
     it('Add 2 actions to funnel', () => {
-        cy.get('[data-attr=create-funnel]').click()
-        cy.get('[data-attr=funnel-tab]').should('exist')
-
-        cy.get('[data-attr=edit-funnel-input]').type('Test funnel')
         cy.get('[data-attr=add-action-event-button]').click()
         cy.get('[data-attr=trend-element-subject-0]').click()
         cy.contains('Pageviews').click()
--- frontend/src/scenes/insights/InsightTabs/FunnelTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/FunnelTab.tsx
@@ -8,80 +8,89 @@ import { userLogic } from 'scenes/userLogic'
 import { ActionFilter } from '../ActionFilter/ActionFilter'
 import { Link } from 'lib/components/Link'
 import { Button, Row } from 'antd'
+import { useState } from 'react'
+import SaveModal from '../SaveModal'
 
 export function FunnelTab(): JSX.Element {
-    const { funnel, isStepsEmpty } = useValues(funnelLogic({ id: null }))
-    const { setFunnel, updateFunnel, createFunnel, clearFunnel } = useActions(funnelLogic({ id: null }))
-    const { actions, actionsLoading } = useValues(actionsModel())
+    const { isStepsEmpty, filters, stepsWithCount } = useValues(funnelLogic)
+    const { loadFunnel, clearFunnel, setFilters, saveFunnelInsight } = useActions(funnelLogic)
+    const { actions, actionsLoading } = useValues(actionsModel)
     const { eventProperties } = useValues(userLogic)
+    const [savingModal, setSavingModal] = useState<boolean>(false)
+
+    const showModal = (): void => setSavingModal(true)
+    const closeModal = (): void => setSavingModal(false)
+    const onSubmit = (input: string): void => {
+        saveFunnelInsight(input)
+        closeModal()
+    }
 
     return (
         <div data-attr="funnel-tab">
             <form
                 onSubmit={(e): void => {
                     e.preventDefault()
-                    if (!funnel.id) {
-                        createFunnel(funnel)
-                    } else {
-                        updateFunnel(funnel)
-                    }
+                    loadFunnel()
                 }}
             >
-                <h4 className="secondary">Funnel Name</h4>
-                <input
-                    required
-                    placeholder="Name of Funnel: (e.g. User drop off through signup)"
-                    type="text"
-                    autoFocus
-                    onChange={(e): void => setFunnel({ name: e.target.value })}
-                    value={funnel.name || ''}
-                    className="form-control"
-                    data-attr="edit-funnel-input"
-                />
                 {!actionsLoading && actions.length === 0 && (
                     <div className="alert alert-warning" style={{ marginTop: '1rem' }}>
                         You don't have any actions set up. <Link to="/actions">Click here to set up an action</Link>
                     </div>
                 )}
-                <hr />
                 <h4 className="secondary">Steps</h4>
                 <ActionFilter
-                    filters={funnel.filters}
-                    setFilters={(filters): void => setFunnel({ filters }, false)}
-                    typeKey={`EditFunnel-${funnel.id || 'new'}`}
+                    filters={filters}
+                    setFilters={(filters): void => setFilters(filters, false)}
+                    typeKey={`EditFunnel-action`}
                     hideMathSelector={true}
                 />
                 <hr />
                 <h4 className="secondary">Filters</h4>
                 <PropertyFilters
-                    pageKey={`EditFunnel-${funnel.id || 'new'}`}
+                    pageKey={`EditFunnel-property`}
                     properties={eventProperties}
-                    propertyFilters={funnel.filters.properties || []}
+                    propertyFilters={filters.properties || []}
                     onChange={(properties): void =>
-                        setFunnel({
-                            filters: {
-                                properties,
-                            },
+                        setFilters({
+                            properties,
                         })
                     }
                     style={{ marginBottom: 20 }}
                 />
                 <hr />
-                <Row justify="start">
-                    <Button type="primary" htmlType="submit" disabled={isStepsEmpty} data-attr="save-funnel-button">
-                        Save funnel
-                    </Button>
-                    {funnel.id && (
+                <Row justify="space-between">
+                    <Row justify="start">
                         <Button
-                            onClick={(): void => clearFunnel()}
-                            className="ml-2"
-                            data-attr="save-funnel-clear-button"
+                            className="mr-1"
+                            type="primary"
+                            htmlType="submit"
+                            disabled={isStepsEmpty}
+                            data-attr="save-funnel-button"
                         >
-                            Clear
+                            Calculate
+                        </Button>
+                        {!isStepsEmpty && (
+                            <Button onClick={(): void => clearFunnel()} data-attr="save-funnel-clear-button">
+                                Clear
+                            </Button>
+                        )}
+                    </Row>
+                    {!isStepsEmpty && Array.isArray(stepsWithCount) && !!stepsWithCount.length && (
+                        <Button type="primary" onClick={showModal}>
+                            Save
                         </Button>
                     )}
                 </Row>
             </form>
+            <SaveModal
+                title="Save Funnel"
+                prompt="Enter the name of the funnel"
+                textLabel="Name"
+                visible={savingModal}
+                onCancel={closeModal}
+                onSubmit={onSubmit}
+            />
         </div>
     )
 }
