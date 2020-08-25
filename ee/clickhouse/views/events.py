27b40711488037974b9d18bdd34a815a9fac3cd7
Eric <eeoneric@gmail.com>
2020-08-25 13:50:00 -0400

    add endpoint

--- ee/clickhouse/views/events.py
+++ ee/clickhouse/views/events.py
@@ -1,8 +1,13 @@
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.request import Request
 from rest_framework.response import Response
+
+from ee.clickhouse.queries.clickhouse_sessions import ClickhouseSessions
 
 # NOTE: bad django practice but /ee specifically depends on /posthog so it should be fine
 from posthog.api.event import EventSerializer
+from posthog.models.filter import Filter
 
 
 class ClickhouseEvents(viewsets.ViewSet):
@@ -19,3 +24,10 @@ class ClickhouseEvents(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         # TODO: implement retrieve event by id
         return Response([])
+
+    @action(methods=["GET"], detail=False)
+    def sessions(self, request: Request) -> Response:
+        team = self.request.user.team_set.get()
+        session_type = request.GET.get("session", None)
+        result = ClickhouseSessions().run(Filter(request=request), team, session_type=session_type)
+        return Response(result)
