a2480567728f7d58962b0b42535426cb1ba77b75
Michael Matloka <dev@twixes.com>
2020-08-25 15:12:53 +0200

    Use okaidia (PostHog/posthog.com#362)

--- frontend/src/scenes/onboarding/FrameworkInstructions/CodeSnippet.tsx
+++ frontend/src/scenes/onboarding/FrameworkInstructions/CodeSnippet.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { toast } from 'react-toastify'
 import { CopyOutlined } from '@ant-design/icons'
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
-import vscDarkPlus from 'react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus'
+import okaidia from 'react-syntax-highlighter/dist/esm/styles/prism/okaidia'
 import bash from 'react-syntax-highlighter/dist/esm/languages/prism/bash'
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx'
 import javascript from 'react-syntax-highlighter/dist/esm/languages/prism/javascript'
@@ -87,7 +87,7 @@ export function CodeSnippet({
                 }}
             />
             <SyntaxHighlighter
-                style={vscDarkPlus}
+                style={okaidia}
                 language={language}
                 customStyle={{ borderRadius: 2 }}
                 wrapLines={wrap}
