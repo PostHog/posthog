62a4ba42e3f4504af2e2efc0edc079e4ef3f60f8
Michael Matloka <dev@twixes.com>
2020-08-27 00:47:42 +0200

    Improve dev instance analytics

--- posthog/apps.py
+++ posthog/apps.py
@@ -11,5 +11,13 @@ class PostHogConfig(AppConfig):
 
     def ready(self):
         posthoganalytics.api_key = "sTMFPsFhdP1Ssg"
-        if settings.TEST or os.environ.get("OPT_OUT_CAPTURE"):
+        if settings.DEBUG:
+            if os.getenv("RUN_MAIN") == "true":
+                first_team = self.get_model("Team").objects.first()
+                if first_team is not None:
+                    first_user = first_team.users.first()
+                    if first_user is not None:
+                        posthoganalytics.capture(first_user.distinct_id, "development server launched")
+            posthoganalytics.disabled = True
+        elif settings.TEST or os.environ.get("OPT_OUT_CAPTURE"):
             posthoganalytics.disabled = True
