5eb4cf4bd035edd09179b0c7e7f4e0918a4702aa
Eric <eeoneric@gmail.com>
2020-08-25 20:36:53 -0400

    update tests

--- ee/clickhouse/queries/test/test_stickiness.py
+++ ee/clickhouse/queries/test/test_stickiness.py
@@ -1,6 +1,7 @@
 from ee.clickhouse.queries.clickhouse_stickiness import ClickhouseStickiness
+from ee.clickhouse.queries.test.util import ClickhouseTestMixin
 from posthog.queries.test.test_stickiness import stickiness_test_factory
 
 
-class TestClickhouseStickiness(stickiness_test_factory(ClickhouseStickiness)):
+class TestClickhouseStickiness(ClickhouseTestMixin, stickiness_test_factory(ClickhouseStickiness)):
     pass
