4ea24e3c49298a789fc70966c9985c82d1f1264c
Eric <eeoneric@gmail.com>
2020-08-27 13:31:51 -0400

    run super

--- ee/clickhouse/clickhouse_test_runner.py
+++ ee/clickhouse/clickhouse_test_runner.py
@@ -6,7 +6,9 @@ TEST_DB = "ch_test"
 
 class ClickhouseTestRunner(DiscoverRunner):
     def setup_databases(self, **kwargs):
+        super().setup_databases(**kwargs)
         Database(TEST_DB).migrate("ee.clickhouse.migrations")
 
     def teardown_databases(self, old_config, **kwargs):
+        super().teardown_databases(old_config, **kwargs)
         Database(TEST_DB).drop_database()
