7191219c939f0be5cff62ca063031be51527df53
Eric <eeoneric@gmail.com>
2020-08-27 13:17:05 -0400

    remove django_clickhouse

--- posthog/settings.py
+++ posthog/settings.py
@@ -407,23 +407,6 @@ CLICKHOUSE = "clickhouse"
 
 PRIMARY_DB = os.environ.get("PRIMARY_DB", POSTGRES)
 
-CLICKHOUSE_DATABASES = {
-    "default": {"db_name": "default", "username": "default", "password": "", "migrate": True, "readonly": False},
-}
-CLICKHOUSE_REDIS_CONFIG = {"host": "localhost", "port": 6379, "db": 8}
-CLICKHOUSE_CELERY_QUEUE = "clickhouse"
-CLICKHOUSE_MODELS_MODULE = "ee.clickhouse.models"
-CLICKHOUSE_MIGRATIONS_PACKAGE = "clickhouse.migrations"
-from datetime import timedelta
-
-CELERYBEAT_SCHEDULE = {
-    "clickhouse_auto_sync": {
-        "task": "django_clickhouse.tasks.clickhouse_auto_sync",
-        "schedule": timedelta(seconds=2),  # Every 2 seconds
-        "options": {"expires": 1, "queue": CLICKHOUSE_CELERY_QUEUE},
-    }
-}
-
 # Extend and override these settings with EE's ones
 if "ee.apps.EnterpriseConfig" in INSTALLED_APPS:
     from ee.settings import *
