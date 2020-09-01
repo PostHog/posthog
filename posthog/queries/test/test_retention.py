de7352f44723cf8a48ed1720e545ea82356016f2
Eric <eeoneric@gmail.com>
2020-09-01 11:04:56 -0400

    fix tests

--- posthog/queries/test/test_retention.py
+++ posthog/queries/test/test_retention.py
@@ -164,49 +164,6 @@ def retention_test_factory(retention):
                     [[1, 1, 1, 0, 0, 1, 1], [1, 1, 0, 0, 1, 1], [1, 0, 0, 1, 1], [0, 0, 0, 0], [0, 0, 0], [1, 1], [1]],
                 )
 
-        def test_retention_action_start_point(self):
-            person1 = Person.objects.create(team=self.team, distinct_ids=["person1", "alias1"])
-            person2 = Person.objects.create(team=self.team, distinct_ids=["person2"])
-
-            action = self._create_signup_actions(
-                [
-                    ("person1", self._date(0)),
-                    ("person1", self._date(1)),
-                    ("person1", self._date(2)),
-                    ("person1", self._date(5)),
-                    ("alias1", self._date(5, 9)),
-                    ("person1", self._date(6)),
-                    ("person2", self._date(1)),
-                    ("person2", self._date(2)),
-                    ("person2", self._date(3)),
-                    ("person2", self._date(6)),
-                ]
-            )
-
-            start_entity = {"id": action.pk, "type": TREND_FILTER_TYPE_ACTIONS}
-            result = retention().run(
-                Filter(data={"date_from": self._date(0, hour=0), "target_entity": json.dumps(start_entity)}),
-                self.team,
-                total_days=7,
-            )
-
-            self.assertEqual(len(result), 7)
-            self.assertEqual(
-                self.pluck(result, "label"), ["Day 0", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6"],
-            )
-            self.assertEqual(result[0]["date"], "Wed. 10 June")
-
-            self.assertEqual(
-                self.pluck(result, "values", "count"),
-                [[1, 1, 1, 0, 0, 1, 1], [2, 2, 1, 0, 1, 2], [2, 1, 0, 1, 2], [1, 0, 0, 1], [0, 0, 0], [1, 1], [2],],
-            )
-
-        def _create_pageviews(self, user_and_timestamps):
-            for distinct_id, timestamp in user_and_timestamps:
-                Event.objects.create(
-                    team=self.team, event="$pageview", distinct_id=distinct_id, timestamp=timestamp,
-                )
-
         def _create_signup_actions(self, user_and_timestamps):
             sign_up_action = Action.objects.create(team=self.team, name="sign up")
             ActionStep.objects.create(action=sign_up_action, event="sign up")
