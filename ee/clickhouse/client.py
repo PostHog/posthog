fe472efc74d26fcacac50f4f1d34213b0d20404f
James Greenhill <jams@uber.com>
2020-09-01 18:24:52 -0700

    remove bootstrapping clickhouse database routine

--- ee/clickhouse/client.py
+++ ee/clickhouse/client.py
@@ -9,18 +9,6 @@ from posthog.settings import (
     CLICKHOUSE_VERIFY,
 )
 
-# Bootstrap ClickHouse with Database
-ch_client = Client(
-    host=CLICKHOUSE_HOST,
-    secure=CLICKHOUSE_SECURE,
-    password=CLICKHOUSE_PASSWORD,
-    ca_certs=CLICKHOUSE_CA,
-    verify=CLICKHOUSE_VERIFY,
-)
-
-# We have to do this because if the database doesn't exist no clickhouse commands will function correctly
-ch_client.execute("CREATE DATABASE IF NOT EXISTS {database}".format(database=CLICKHOUSE_DATABASE))
-
 # Reconnect scoped to the database provided for security
 ch_client = Client(
     host=CLICKHOUSE_HOST,
