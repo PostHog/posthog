6f56133b0240c5bab18d6d3ebc334fd3e34cab73
James Greenhill <jams@uber.com>
2020-09-01 18:35:11 -0700

    Don't initialize the clickhouse connection unless we say it's primary

--- ee/clickhouse/client.py
+++ ee/clickhouse/client.py
@@ -1,20 +1,24 @@
 from clickhouse_driver import Client  # type: ignore
 
 from posthog.settings import (
+    CLICKHOUSE,
     CLICKHOUSE_CA,
     CLICKHOUSE_DATABASE,
     CLICKHOUSE_HOST,
     CLICKHOUSE_PASSWORD,
     CLICKHOUSE_SECURE,
     CLICKHOUSE_VERIFY,
+    PRIMARY_DB,
 )
 
-# Reconnect scoped to the database provided for security
-ch_client = Client(
-    host=CLICKHOUSE_HOST,
-    database=CLICKHOUSE_DATABASE,
-    secure=CLICKHOUSE_SECURE,
-    password=CLICKHOUSE_PASSWORD,
-    ca_certs=CLICKHOUSE_CA,
-    verify=CLICKHOUSE_VERIFY,
-)
+if PRIMARY_DB != CLICKHOUSE:
+    ch_client = None
+else:
+    ch_client = Client(
+        host=CLICKHOUSE_HOST,
+        database=CLICKHOUSE_DATABASE,
+        secure=CLICKHOUSE_SECURE,
+        password=CLICKHOUSE_PASSWORD,
+        ca_certs=CLICKHOUSE_CA,
+        verify=CLICKHOUSE_VERIFY,
+    )
