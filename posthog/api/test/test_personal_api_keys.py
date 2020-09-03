835166ab1b7e642c7b0416c0b3c4d06fd9c459a5
Michael Matloka <dev@twixes.com>
2020-09-03 16:32:52 +0200

    Update test_personal_api_keys.py

--- posthog/api/test/test_personal_api_keys.py
+++ posthog/api/test/test_personal_api_keys.py
@@ -8,13 +8,17 @@ from .base import TransactionBaseTest
 class TestPersonalAPIKeysAPI(TransactionBaseTest):
     TESTS_API = True
 
+    def setUp(self) -> None:
+        super().setUp()
+        self.personal_api_key = PersonalAPIKey.objects.create(label="Test", team=self.team, user=self.user)
+
     def test_create_personal_api_key(self):
         label = "Test key uno"
         response = self.client.post("/api/personal_api_keys/", {"label": label})
         self.assertEqual(response.status_code, 201)
-        key: PersonalAPIKey = PersonalAPIKey.objects.get()
         response_data = response.json()
         response_data.pop("created_at")
+        key: PersonalAPIKey = PersonalAPIKey.objects.latest()
         self.assertDictEqual(
             response_data,
             {
@@ -34,21 +38,16 @@ class TestPersonalAPIKeysAPI(TransactionBaseTest):
         self.assertDictEqual(response_data, {"label": ["This field may not be blank."]})
 
     def test_delete_personal_api_key(self):
-        key = PersonalAPIKey(label="Test", team=self.team, user=self.user)
-        key.save()
-        self.assertEqual(len(PersonalAPIKey.objects.all()), 1)
-        response = self.client.delete(f"/api/personal_api_keys/{key.id}/")
+        self.assertEqual(PersonalAPIKey.objects.count(), 1)
+        response = self.client.delete(f"/api/personal_api_keys/{self.personal_api_key.id}/")
         self.assertEqual(response.status_code, 204)
-        self.assertEqual(len(PersonalAPIKey.objects.all()), 0)
+        self.assertEqual(PersonalAPIKey.objects.count(), 0)
 
     def test_list_only_user_personal_api_keys(self):
-        my_label = "Test"
-        my_key = PersonalAPIKey(label=my_label, team=self.team, user=self.user)
-        my_key.save()
         other_user = self._create_user("abc@def.xyz")
         other_key = PersonalAPIKey(label="Other test", team=self.team, user=other_user)
         other_key.save()
-        self.assertEqual(len(PersonalAPIKey.objects.all()), 2)
+        self.assertEqual(PersonalAPIKey.objects.count(), 2)
         response = self.client.get("/api/personal_api_keys/")
         self.assertEqual(response.status_code, 200)
         response_data = response.json()
@@ -57,8 +56,8 @@ class TestPersonalAPIKeysAPI(TransactionBaseTest):
         self.assertDictEqual(
             response_data[0],
             {
-                "id": my_key.id,
-                "label": my_label,
+                "id": self.personal_api_key.id,
+                "label": self.personal_api_key.label,
                 "last_used_at": None,
                 "user_id": self.user.id,
                 "team_id": self.team.id,
@@ -66,18 +65,15 @@ class TestPersonalAPIKeysAPI(TransactionBaseTest):
         )
 
     def test_get_own_personal_api_key(self):
-        my_label = "Test"
-        my_key = PersonalAPIKey(label=my_label, team=self.team, user=self.user)
-        my_key.save()
-        response = self.client.get(f"/api/personal_api_keys/{my_key.id}/")
+        response = self.client.get(f"/api/personal_api_keys/{self.personal_api_key.id}/")
         self.assertEqual(response.status_code, 200)
         response_data = response.json()
         response_data.pop("created_at")
         self.assertDictEqual(
             response_data,
             {
-                "id": my_key.id,
-                "label": my_label,
+                "id": self.personal_api_key.id,
+                "label": self.personal_api_key.label,
                 "last_used_at": None,
                 "user_id": self.user.id,
                 "team_id": self.team.id,
@@ -98,39 +94,46 @@ class TestPersonalAPIKeysAPIAuthentication(TransactionBaseTest):
     TESTS_API = True
     TESTS_FORCE_LOGIN = False
 
+    def setUp(self) -> None:
+        super().setUp()
+        self.personal_api_key = PersonalAPIKey.objects.create(label="Test", team=self.team, user=self.user)
+
     def test_no_key(self):
         response = self.client.get("/api/dashboard/")
         self.assertEqual(response.status_code, 403)
 
-    def test_header_resilient(self):
-        key = PersonalAPIKey(label="Test", team=self.team, user=self.user)
-        key.save()
-        response = self.client.get("/api/dashboard/", HTTP_AUTHORIZATION=f"Bearer  {key.value}  ")
+    def test_header_resilience(self):
+        response = self.client.get("/api/dashboard/", HTTP_AUTHORIZATION=f"Bearer  {self.personal_api_key.value}  ")
         self.assertEqual(response.status_code, 200)
 
     def test_query_string(self):
-        key = PersonalAPIKey(label="Test", team=self.team, user=self.user)
-        key.save()
-        response = self.client.get(f"/api/dashboard/?personal_api_key={key.value}")
+        response = self.client.get(f"/api/dashboard/?personal_api_key={self.personal_api_key.value}")
         self.assertEqual(response.status_code, 200)
 
     def test_body(self):
-        key = PersonalAPIKey(label="Test", team=self.team, user=self.user)
-        key.save()
-        response = self.client.get("/api/dashboard/", {"personal_api_key": key.value})
+        response = self.client.get("/api/dashboard/", {"personal_api_key": self.personal_api_key.value})
         self.assertEqual(response.status_code, 200)
 
     def test_user_not_active(self):
         self.user.is_active = False
         self.user.save()
-        key = PersonalAPIKey(label="Test", team=self.team, user=self.user)
-        key.save()
-        response = self.client.get("/api/user/", HTTP_AUTHORIZATION=f"Bearer {key.value}")
+        response = self.client.get("/api/user/", HTTP_AUTHORIZATION=f"Bearer {self.personal_api_key.value}")
         self.assertEqual(response.status_code, 401)
 
     def test_user_endpoint(self):
         # special case as /api/user/ is (or used to be) uniquely not DRF (vanilla Django)
-        key = PersonalAPIKey(label="Test", team=self.team, user=self.user)
-        key.save()
-        response = self.client.get("/api/user/", HTTP_AUTHORIZATION=f"Bearer {key.value}")
+        response = self.client.get("/api/user/", HTTP_AUTHORIZATION=f"Bearer {self.personal_api_key.value}")
         self.assertEqual(response.status_code, 200)
+
+    def test_capture(self):
+        response = self.client.post(
+            "/capture/",
+            {"event": "x", "distinct_id": "y", "personal_api_key": self.personal_api_key.value},
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_decide(self):
+        response = self.client.get("/decide/", {"personal_api_key": self.personal_api_key.value})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json()["isAuthenticated"])
