d4a486b9deb7bf028c2664dff98b229daa0bf310
Eric <eeoneric@gmail.com>
2020-09-04 14:56:01 -0400

    reformat and add cohort processing

--- posthog/tasks/calculate_cohort.py
+++ posthog/tasks/calculate_cohort.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 from django.utils import timezone
 
 from posthog.celery import app
+from posthog.ee import check_ee_enabled
 from posthog.models import Cohort
 
 logger = logging.getLogger(__name__)
@@ -17,6 +18,7 @@ def calculate_cohort(cohort_id: int) -> None:
     start_time = time.time()
     cohort = Cohort.objects.get(pk=cohort_id)
     cohort.calculate_people()
+    calculate_cohorts_ch(cohort)
     logger.info("Calculating cohort {} took {:.2f} seconds".format(cohort.pk, (time.time() - start_time)))
 
 
@@ -27,6 +29,17 @@ def calculate_cohorts() -> None:
     ).order_by("id"):
         cohort_start = time.time()
         cohort.calculate_people()
+        calculate_cohorts_ch(cohort)
         logger.info(" - Calculating cohort {} took {:.2f} seconds".format(cohort.pk, (time.time() - cohort_start)))
 
     logger.info("Calculating all cohorts took {:.2f} seconds".format(time.time() - start_time))
+
+
+def calculate_cohorts_ch(cohort: Cohort) -> None:
+    if check_ee_enabled():
+        try:
+            from ee.clickhouse.models.cohort import populate_cohort_person_table
+
+            populate_cohort_person_table(cohort)
+        except:
+            logger.error("Could not update clickhouse cohort tables")
