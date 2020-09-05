e25f636a62960ef2a2c0446e6673f795a867816b
James Greenhill <jams@uber.com>
2020-09-04 17:33:57 -0700

    mypy checks

--- ee/clickhouse/client.py
+++ ee/clickhouse/client.py
@@ -1,8 +1,8 @@
 import asyncio
 
 from aioch import Client  # type: ignore
-from asgiref.sync import async_to_sync
-from clickhouse_driver import Client as SyncClient
+from asgiref.sync import async_to_sync  # type: ignore
+from clickhouse_driver import Client as SyncClient  # type: ignore
 
 from posthog.settings import (
     CLICKHOUSE,
