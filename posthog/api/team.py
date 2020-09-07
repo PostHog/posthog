4624c8b43262d2e4623e13fb125593ffc4ce6bdb
Michael Matloka <dev@twixes.com>
2020-09-07 17:25:58 +0200

    Update team.py

--- posthog/api/team.py
+++ posthog/api/team.py
@@ -31,69 +31,12 @@ class TeamSerializer(serializers.ModelSerializer):
         return person.pk
 
 
-class PersonViewSet(viewsets.ModelViewSet):
+class TeamViewSet(mixins.DestroyModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        team = self.request.user.team
+        team = self.request.user.organization.team
         queryset = queryset.filter(team=team)
         return self._filter_request(self.request, queryset, team)
-
-    @action(methods=["GET"], detail=False)
-    def by_distinct_id(self, request):
-        person = self.get_queryset().get(persondistinctid__distinct_id=str(request.GET["distinct_id"]))
-        return response.Response(PersonSerializer(person, context={"request": request}).data)
-
-    @action(methods=["GET"], detail=False)
-    def properties(self, request: request.Request) -> response.Response:
-        class JsonKeys(Func):
-            function = "jsonb_object_keys"
-
-        people = self.get_queryset()
-        people = (
-            people.annotate(keys=JsonKeys("properties")).values("keys").annotate(count=Count("id")).order_by("-count")
-        )
-
-        return response.Response([{"name": event["keys"], "count": event["count"]} for event in people])
-
-    @action(methods=["GET"], detail=False)
-    def values(self, request: request.Request) -> response.Response:
-        people = self.get_queryset()
-        key = "properties__{}".format(request.GET.get("key"))
-        people = (
-            people.values(key)
-            .annotate(count=Count("id"))
-            .filter(**{"{}__isnull".format(key): False})
-            .order_by("-count")
-        )
-
-        if request.GET.get("value"):
-            people = people.extra(
-                where=["properties ->> %s LIKE %s"], params=[request.GET["key"], "%{}%".format(request.GET["value"])],
-            )
-
-        return response.Response(
-            [{"name": convert_property_value(event[key]), "count": event["count"]} for event in people[:50]]
-        )
-
-    @action(methods=["GET"], detail=False)
-    def references(self, request: request.Request) -> response.Response:
-        reference_id = request.GET.get("id", None)
-        offset = request.GET.get("offset", None)
-
-        if not reference_id or not offset:
-            return response.Response({})
-
-        offset_value = int(offset)
-        cached_result = cache.get(reference_id)
-        if cached_result:
-            return response.Response(
-                {
-                    "result": cached_result[offset_value : offset_value + 100],
-                    "offset": offset_value + 100 if len(cached_result) > offset_value + 100 else None,
-                }
-            )
-        else:
-            return response.Response({})
