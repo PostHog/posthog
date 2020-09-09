6e8bbc0d9c0e372914418707204f641926a8c1cc
Michael Matloka <dev@twixes.com>
2020-09-10 00:01:02 +0200

    Update setup_review.py

--- posthog/management/commands/setup_review.py
+++ posthog/management/commands/setup_review.py
@@ -5,23 +5,26 @@ from django.core.management.base import BaseCommand
 from django.db import connection
 
 from posthog.demo import _create_anonymous_users, _create_funnel, _recalculate
-from posthog.models import Team, User
+from posthog.models import MembershipLevel, Organization, OrganizationMembership, Team, User
 
 
 class Command(BaseCommand):
     help = "Set up review instance with demo data"
 
     def handle(self, *args, **options):
-        user = User.objects.create(email="test@posthog.com", is_staff=True)
-        user.set_password("pass")
-        user.save()
+        organization = Organization.objects.create(name="Hogflix")
         team = Team.objects.create_with_data(
-            users=[user],
+            organization=organization,
             name="PostHog",
             completed_snippet_onboarding=True,
             event_names=["$pageview", "$autocapture"],
             event_properties=["$current_url", "$browser", "$os"],
         )
+        user = User.objects.create_superuser(email="test@posthog.com", password="pass")
+        OrganizationMembership.objects.create(organization=organization, user=user, level=MembershipLevel.ADMIN)
+        user.current_organization = organization
+        user.current_team = team
+        user.save()
         heroku_app_name = os.getenv("HEROKU_APP_NAME")
         base_url = f"https://{heroku_app_name}.herokuapp.com/demo/" if heroku_app_name else f"{settings.SITE_URL}/demo/"
         _create_anonymous_users(team=team, base_url=base_url)
