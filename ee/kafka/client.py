2b02ee4dcb00d5d97ad0301b42151df0476d88b9
James Greenhill <jams@uber.com>
2020-09-10 18:02:08 -0700

    Publish events to Kafka for consumption

--- ee/kafka/client.py
+++ ee/kafka/client.py
@@ -2,6 +2,7 @@ import kafka_helper
 from kafka import KafkaProducer as KP
 
 from posthog.settings import IS_HEROKU, KAFKA_HOSTS
+from posthog.settings import KAFKA_HOSTS
 from posthog.utils import SingletonDecorator
 
 
@@ -14,6 +15,19 @@ class KafkaProducer:
 
     def produce(self, topic: str, data):
         self.producer.send(topic, data.encode("utf-8"))
+
+    @staticmethod
+    def delivery_report(err, msg):
+        """ Called once for each message produced to indicate delivery result.
+            Triggered by poll() or flush(). """
+        if err is not None:
+            print("Message delivery failed: {}".format(err))
+        else:
+            print("Message delivered to {} [{}]".format(msg.topic(), msg.partition()))
+
+    def produce(self, topic, data):
+        self.producer.poll(0)
+        self.producer.produce(topic, data.encode("utf-8"), callback=self.delivery_report)
 
     def close(self):
         self.producer.flush()
