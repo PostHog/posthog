3010793ef441625d1bf2af70fb4ae25c351ce455
James Greenhill <jams@uber.com>
2020-09-10 21:10:49 -0700

    convert client to use github.com/dpkp/kafka-python

--- ee/kafka/client.py
+++ ee/kafka/client.py
@@ -13,21 +13,8 @@ class KafkaProducer:
         else:
             self.producer = kafka_helper.get_kafka_producer()
 
-    def produce(self, topic: str, data):
-        self.producer.send(topic, data.encode("utf-8"))
-
-    @staticmethod
-    def delivery_report(err, msg):
-        """ Called once for each message produced to indicate delivery result.
-            Triggered by poll() or flush(). """
-        if err is not None:
-            print("Message delivery failed: {}".format(err))
-        else:
-            print("Message delivered to {} [{}]".format(msg.topic(), msg.partition()))
-
     def produce(self, topic, data):
-        self.producer.poll(0)
-        self.producer.produce(topic, data.encode("utf-8"), callback=self.delivery_report)
+        self.producer.send(topic, data.encode("utf-8"))
 
     def close(self):
         self.producer.flush()
