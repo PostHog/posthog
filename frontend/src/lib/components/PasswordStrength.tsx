1394c3009e1281cc3da05f5a4a88d6ca7e1b48d3
Yakko Majuri <yakko.majuri@gmail.com>
2020-09-11 12:43:06 +0000

    updated strength algo

--- frontend/src/lib/components/PasswordStrength.tsx
+++ frontend/src/lib/components/PasswordStrength.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
 import { Progress } from 'antd'
-import { green, orange, red } from '@ant-design/colors'
-
-const PASSWORD_TEST_REGEXES: RegExp[] = [
-    /.{8,}/, // at least 8 characters
-    /.{12,}/, // bonus if longer than 12 characters
-    /[a-z]/, // at least one lowercase letter
-    /[A-Z]/, // at least one uppercase letter
-    /\d/, // at least one number
-    /[^A-Za-z0-9]/, // at least one special character
-]
+import { green, orange, red, yellow } from '@ant-design/colors'
 
 function scorePassword(password?: string): number {
     if (!password) return 0
-    const testsPassed: number = PASSWORD_TEST_REGEXES.reduce(
-        (accumulator: number, regex: RegExp) => accumulator + Number(regex.test(password)),
-        0
-    )
-    return (testsPassed / PASSWORD_TEST_REGEXES.length) * 100
+
+    let score: number = 0
+    const passwordLength = password.length
+
+    for (let i = 0; i < Math.min(passwordLength, 21); ++i) {
+        const char = password[i]
+        if (/[a-z]/.test(char)) score += 1
+        // 1 point for lowercase characters
+        else if (/[A-Z]/.test(char)) score += 2
+        // 2 points for uppercase characters
+        else if (/\d/.test(char)) score += 3
+        // 3 points for number
+        else if (/[^A-Za-z0-9]/.test(char)) score += 4 // 4 points for special characters
+    }
+
+    // 2 points for being longer than 12
+    if (passwordLength >= 12) score += 2
+
+    // Password strength can only improve after 20 characters
+    const lengthBonusPoints = passwordLength >= 20 ? (passwordLength - 20) * 2 : 0
+    score += lengthBonusPoints
+
+    // Penalty for character repetition and short length
+    const distinctChars = new Set(password)
+    if (distinctChars.size * 3 < passwordLength || passwordLength <= 8) score /= 2
+
+    // topScore = All symbols up to 20 characters + 2 points for length > 12 + Bonus points for length over 20
+    const topScore = Math.min(passwordLength, 20) * 4 + 2 + lengthBonusPoints
+    return (score / topScore) * 100
 }
 
 export default function PasswordStrength({ password }: { password: string }): JSX.Element {
@@ -27,7 +42,15 @@ export default function PasswordStrength({ password }: { password: string }): JS
         <Progress
             percent={passwordScore}
             size="small"
-            strokeColor={passwordScore < 50 ? red.primary : passwordScore < 80 ? orange.primary : green.primary}
+            strokeColor={
+                passwordScore < 25
+                    ? red.primary
+                    : passwordScore < 50
+                    ? orange.primary
+                    : passwordScore < 75
+                    ? yellow.primary
+                    : green.primary
+            }
             showInfo={false}
         />
     )
