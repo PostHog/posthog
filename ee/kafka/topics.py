7271c6c8d130378bfee75c41fde2874a9540fe86
James Greenhill <fuziontech@gmail.com>
2020-09-11 18:14:58 -0700

    include kafka migrations

---
+++ ee/kafka/topics.py
@@ -0,0 +1,5 @@
+KAFKA_EVENTS = "clickhouse_events"
+KAFKA_ELEMENTS = "clickhouse_elements"
+KAFKA_ELEMENTS_GROUP = "clickhouse_elements_group"
+KAFKA_PERSON = "clickhouse_person"
+KAFKA_PERSON_UNIQUE_ID = "clickhouse_person_unique_id"
