edf7433e76bd28e4d9ae2a14a935f418a7de0adf
Marius Andra <marius.andra@gmail.com>
2020-09-14 17:09:41 +0200

    fix broken imports

--- ee/clickhouse/queries/test/test_stickiness.py
+++ ee/clickhouse/queries/test/test_stickiness.py
@@ -1,6 +1,7 @@
 from ee.clickhouse.queries.clickhouse_stickiness import ClickhouseStickiness
 from ee.clickhouse.util import ClickhouseTestMixin
-from posthog.queries.test.test_stickiness import stickiness_test_factory
+
+# from posthog.queries.test.test_stickiness import stickiness_test_factory
 
 
 # class TestClickhouseStickiness(ClickhouseTestMixin, stickiness_test_factory(ClickhouseStickiness)): # type: ignore
