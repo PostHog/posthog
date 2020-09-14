e020e245bcec17a939d85b8fc4b7b0776b1cafba
Marius Andra <marius.andra@gmail.com>
2020-09-14 12:13:58 +0200

    stickiness test [WIP, actions fail]

--- ee/clickhouse/queries/test/test_stickiness.py
+++ ee/clickhouse/queries/test/test_stickiness.py
@@ -1,8 +1,9 @@
 from ee.clickhouse.models.event import create_event
+from ee.clickhouse.models.person import create_person
 from ee.clickhouse.queries.clickhouse_stickiness import ClickhouseStickiness
 from ee.clickhouse.util import ClickhouseTestMixin
 from posthog.queries.test.test_stickiness import stickiness_test_factory
 
 
-class TestClickhouseStickiness(ClickhouseTestMixin, stickiness_test_factory(ClickhouseStickiness, create_event)):  # type: ignore
+class TestClickhouseStickiness(ClickhouseTestMixin, stickiness_test_factory(ClickhouseStickiness, create_event, create_person)):  # type: ignore
     pass
