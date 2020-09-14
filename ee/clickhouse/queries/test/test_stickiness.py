10758dd2298e8241f9cf65acc66eb5ffb8a955a5
Marius Andra <marius.andra@gmail.com>
2020-09-14 10:23:39 +0200

    Clickhouse Stickiness tests

--- ee/clickhouse/queries/test/test_stickiness.py
+++ ee/clickhouse/queries/test/test_stickiness.py
@@ -1,3 +1,8 @@
-# class TestClickhouseStickiness(ClickhouseTestMixin, stickiness_test_factory(ClickhouseStickiness)): # type: ignore
-class TestClickhouseStickiness:
+from ee.clickhouse.models.event import create_event
+from ee.clickhouse.queries.clickhouse_stickiness import ClickhouseStickiness
+from ee.clickhouse.util import ClickhouseTestMixin
+from posthog.queries.test.test_stickiness import stickiness_test_factory
+
+
+class TestClickhouseStickiness(ClickhouseTestMixin, stickiness_test_factory(ClickhouseStickiness, create_event)):  # type: ignore
     pass
