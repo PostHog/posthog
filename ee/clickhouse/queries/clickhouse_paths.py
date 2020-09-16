0c61eec79bb29b42957b4e9c8c1ee683993f5cdc
Marius Andra <marius.andra@gmail.com>
2020-09-16 10:43:28 +0200

    hardcoded query that returns good results for $pageviews, no filters yet

--- ee/clickhouse/queries/clickhouse_paths.py
+++ ee/clickhouse/queries/clickhouse_paths.py
@@ -1,10 +1,251 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
+from ee.clickhouse.client import sync_execute
+from ee.clickhouse.queries.util import parse_timestamps
+from posthog.constants import AUTOCAPTURE_EVENT, CUSTOM_EVENT, SCREEN_EVENT
 from posthog.models.filter import Filter
 from posthog.models.team import Team
 from posthog.queries.base import BaseQuery
+from posthog.utils import relative_date_parse, request_to_date_query
 
 
 class ClickhousePaths(BaseQuery):
+    # def _determine_path_type(self, requested_type=None):
+    #     # Default
+    #     event: Optional[str] = "$pageview"
+    #     event_filter = {"event": event}
+    #     path_type = "properties->> '$current_url'"
+    #     start_comparator = "{} ~".format(path_type)
+    #
+    #     # determine requested type
+    #     if requested_type:
+    #         if requested_type == SCREEN_EVENT:
+    #             event = SCREEN_EVENT
+    #             event_filter = {"event": event}
+    #             path_type = "properties->> '$screen_name'"
+    #             start_comparator = "{} ~".format(path_type)
+    #         elif requested_type == AUTOCAPTURE_EVENT:
+    #             event = AUTOCAPTURE_EVENT
+    #             event_filter = {"event": event}
+    #             path_type = "tag_name_source"
+    #             start_comparator = "group_id ="
+    #         elif requested_type == CUSTOM_EVENT:
+    #             event = None
+    #             event_filter = {}
+    #             path_type = "event"
+    #             start_comparator = "event ="
+    #     return event, path_type, event_filter, start_comparator
+    #
+    # def _apply_start_point(self, start_comparator: str, query_string: str, start_point: str) -> str:
+    #     marked = "\
+    #         SELECT *, CASE WHEN {} '{}' THEN timestamp ELSE NULL END as mark from ({}) as sessionified\
+    #     ".format(
+    #         start_comparator, start_point, query_string
+    #     )
+    #
+    #     marked_plus = "\
+    #         SELECT *, MIN(mark) OVER (\
+    #                 PARTITION BY distinct_id\
+    #                 , session ORDER BY timestamp\
+    #                 ) AS max from ({}) as marked order by session\
+    #     ".format(
+    #         marked
+    #     )
+    #
+    #     sessionified = "\
+    #         SELECT * FROM ({}) as something where timestamp >= max \
+    #     ".format(
+    #         marked_plus
+    #     )
+    #     return sessionified
+    #
+    # def _add_elements(self, query_string: str) -> str:
+    #     element = 'SELECT \'<\'|| e."tag_name" || \'> \'  || e."text" as tag_name_source, e."text" as text_source FROM "posthog_element" e JOIN \
+    #                 ( SELECT group_id, MIN("posthog_element"."order") as minOrder FROM "posthog_element" GROUP BY group_id) e2 ON e.order = e2.minOrder AND e.group_id = e2.group_id where e.group_id = v2.group_id'
+    #     element_group = 'SELECT g."id" as group_id FROM "posthog_elementgroup" g where v1."elements_hash" = g."hash"'
+    #     sessions_sql = "SELECT * FROM ({}) as v1 JOIN LATERAL ({}) as v2 on true JOIN LATERAL ({}) as v3 on true".format(
+    #         query_string, element_group, element
+    #     )
+    #     return sessions_sql
+
+    def calculate_paths(self, filter: Filter, team: Team):
+        # date_query = request_to_date_query({"date_from": filter._date_from, "date_to": filter._date_to}, exact=False)
+        # resp = []
+        # event, path_type, event_filter, start_comparator = self._determine_path_type(
+        #     filter.path_type if filter else None
+        # )
+        #
+        # sessions = (
+        #     Event.objects.add_person_id(team.pk)
+        #     .filter(team=team, **(event_filter), **date_query)
+        #     .filter(~Q(event__in=["$autocapture", "$pageview", "$identify", "$pageleave"]) if event is None else Q())
+        #     .filter(filter.properties_to_Q(team_id=team.pk) if filter and filter.properties else Q())
+        #     .annotate(
+        #         previous_timestamp=Window(
+        #             expression=Lag("timestamp", default=None),
+        #             partition_by=F("distinct_id"),
+        #             order_by=F("timestamp").asc(),
+        #         )
+        #     )
+        # )
+
+        # sessions_sql, sessions_sql_params = sessions.query.sql_with_params()
+
+        SESSIONS_QUERY = """
+            -- 2
+                SELECT distinct_id,
+                         event_id,
+                         timestamp,
+                         path_type,
+                         neighbor(distinct_id, -1) as possible_prev_distinct_id,
+                         neighbor(event_id, -1)       as possible_prev_event_id,
+                         neighbor(timestamp, -1)   as possible_prev_timestamp,
+                         if(possible_prev_distinct_id != distinct_id or
+                            dateDiff('minute', toDateTime(timestamp), toDateTime(possible_prev_timestamp)) > 30, 1,
+                            0)                     as new_session
+                  FROM (
+            -- 1
+                        SELECT timestamp,
+                               distinct_id,
+                               id as event_id,
+                               JSONExtractString(properties, %(property)s) as path_type
+                        FROM events
+                        WHERE team_id = %(team_id)s
+                          and event = %(event)s
+                        GROUP BY distinct_id, timestamp, event_id, properties
+                        ORDER BY distinct_id, timestamp
+            -- /1
+
+                           )
+            -- /2
+        """
+
+        # if event == "$autocapture":
+        #     sessions_sql = self._add_elements(query_string=sessions_sql)
+
+        # events_notated = "\
+        # SELECT *, CASE WHEN EXTRACT('EPOCH' FROM (timestamp - previous_timestamp)) >= (60 * 30) OR previous_timestamp IS NULL THEN 1 ELSE 0 END AS new_session\
+        # FROM ({}) AS inner_sessions\
+        # ".format(
+        #     sessions_sql
+        # )
+
+        # sessionified = "\
+        # SELECT events_notated.*, SUM(new_session) OVER (\
+        #     ORDER BY distinct_id\
+        #             ,timestamp\
+        #     ) AS session\
+        # FROM ({}) as events_notated\
+        # ".format(
+        #     events_notated
+        # )
+        #
+        # if filter and filter.start_point:
+        #     sessionified = self._apply_start_point(
+        #         start_comparator=start_comparator, query_string=sessionified, start_point=filter.start_point,
+        #     )
+        #
+        # final = "\
+        # SELECT {} as path_type, id, sessionified.session\
+        #     ,ROW_NUMBER() OVER (\
+        #             PARTITION BY distinct_id\
+        #             ,session ORDER BY timestamp\
+        #             ) AS event_number\
+        # FROM ({}) as sessionified\
+        # ".format(
+        #     path_type, sessionified
+        # )
+        #
+        # counts = "\
+        # SELECT event_number || '_' || path_type as target_event, id as target_id, LAG(event_number || '_' || path_type, 1) OVER (\
+        #     PARTITION BY session\
+        #     ) AS source_event , LAG(id, 1) OVER (\
+        #     PARTITION BY session\
+        #     ) AS source_id from \
+        # ({}) as final\
+        # where event_number <= 4\
+        # ".format(
+        #     final
+        # )
+
+        # cursor = connection.cursor()
+        # cursor.execute(
+        #     "\
+        # SELECT source_event, target_event, MAX(target_id), MAX(source_id), count(*) from ({}) as counts\
+        # where source_event is not null and target_event is not null\
+        # group by source_event, target_event order by count desc limit 20\
+        # ".format(
+        #         counts
+        #     ),
+        #     sessions_sql_params,
+        # )
+        # rows = cursor.fetchall()
+
+        AGGREGATE_QUERY = """
+            -- 4
+            SELECT concat(toString(group_index), '_', path_type) as target_event,
+                   event_id as target_event_id,
+                   if(group_index > 1, neighbor(concat(toString(group_index), '_', path_type), -1), null) as source_event,
+                   if(group_index > 1, neighbor(event_id, -1), null)       as source_event_id
+            FROM (
+            -- 3
+                  SELECT distinct_id,
+                         event_id,
+                         timestamp,
+                         path_type,
+                         arraySum(arraySlice(gids, 1, idx))                 AS gid,
+                         indexOf(arrayReverse(arraySlice(gids, 1, idx)), 1) as group_index
+                  FROM (
+                        SELECT groupArray(timestamp)   as timestamps,
+                               groupArray(path_type)   as path_types,
+                               groupArray(event_id)       as event_ids,
+                               groupArray(distinct_id) as distinct_ids,
+                               groupArray(new_session) AS gids
+                        FROM ({sessions_query})
+                       )
+                      ARRAY JOIN
+                       distinct_ids as distinct_id,
+                       event_ids as event_id,
+                       timestamps as timestamp,
+                       path_types as path_type,
+                       arrayEnumerate(gids) AS idx
+            -- /3
+                )
+            -- /4
+        """
+
+        COUNT_QUERY = """
+            -- 5
+            SELECT 
+                source_event, 
+                any(source_event_id) as source_event_id, 
+                target_event, 
+                any(target_event_id) as target_event_id, 
+                count(*) as event_count
+            from (
+                {aggregate_query}
+            ) 
+            where source_event is not null and target_event is not null
+            group by source_event, target_event
+            order by event_count desc, source_event, target_event
+            -- /5
+        """
+
+        FINAL_QUERY = COUNT_QUERY.format(aggregate_query=AGGREGATE_QUERY.format(sessions_query=SESSIONS_QUERY))
+
+        params = {"team_id": team.pk, "property": "$current_url", "event": "$pageview"}
+
+        resp: List[Dict[str, str]] = []
+
+        rows = sync_execute(FINAL_QUERY, params)
+
+        for row in rows:
+            resp.append(
+                {"source": row[0], "target": row[2], "target_id": row[3], "source_id": row[1], "value": row[4],}
+            )
+
+        resp = sorted(resp, key=lambda x: x["value"], reverse=True)
+        return resp
+
     def run(self, filter: Filter, team: Team, *args, **kwargs) -> List[Dict[str, Any]]:
-        return []
+        return self.calculate_paths(filter=filter, team=team)
