410f0339da77f05785d58963960042fb2ee8d4ec
Michael Matloka <dev@twixes.com>
2020-09-17 16:35:37 +0200

    Update only models

--- posthog/models/user.py
+++ posthog/models/user.py
@@ -1,23 +1,25 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Sequence, Union
 
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.dispatch import receiver
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 from rest_framework.fields import BooleanField
 
+from .organization import Organization, OrganizationMembership
 from .team import Team
-from .utils import generate_random_token
+from .utils import generate_random_token, sane_repr
 
 MULTI_TENANCY_MISSING = False
 if settings.EE_AVAILABLE:
     from ee.models.license import License
 
 try:
-    from multi_tenancy.models import TeamBilling  # type: ignore
+    from multi_tenancy.models import BilledOrganization  # type: ignore
 except ImportError:
-    TeamBilling = False
+    BilledOrganization = False
     MULTI_TENANCY_MISSING = True
 
 
@@ -42,7 +44,7 @@ class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(self, email: Optional[str], password: str, **extra_fields):
+    def _create_user(self, email: str, password: str, **extra_fields):
         """Create and save a User with the given email and password."""
         if email is None:
             raise ValueError("The given email must be set")
@@ -50,6 +52,8 @@ class UserManager(BaseUserManager):
         email = self.normalize_email(email)
         if is_email_restricted_from_signup(email):
             raise ValueError("Can't sign up with this email")
+
+        extra_fields.setdefault("distinct_id", generate_random_token())
 
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -60,7 +64,6 @@ class UserManager(BaseUserManager):
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        extra_fields.setdefault("distinct_id", generate_random_token())
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
@@ -85,9 +88,13 @@ class User(AbstractUser):
     ]
 
     username = None  # type: ignore
+    current_organization = models.ForeignKey(
+        "posthog.Organization", models.SET_NULL, null=True, related_name="users_currently+"
+    )
+    current_team = models.ForeignKey("posthog.Team", models.SET_NULL, null=True, related_name="teams_currently+")
     email = models.EmailField(_("email address"), unique=True)
-    temporary_token: models.CharField = models.CharField(max_length=200, null=True, blank=True)
-    distinct_id: models.CharField = models.CharField(max_length=200, null=True, blank=True)
+    temporary_token: models.CharField = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    distinct_id: models.CharField = models.CharField(max_length=200, null=True, blank=True, unique=True)
     email_opt_in: models.BooleanField = models.BooleanField(default=False, null=True, blank=True)
     anonymize_data: models.BooleanField = models.BooleanField(default=False, null=True, blank=True)
     toolbar_mode: models.CharField = models.CharField(
@@ -115,8 +122,8 @@ class User(AbstractUser):
         # If we're on multi-tenancy grab the team's price
         if not MULTI_TENANCY_MISSING:
             try:
-                return TeamBilling.objects.get(team=self.team).price_id
-            except TeamBilling.DoesNotExist:
+                return BilledOrganization.objects.get(team=self.team).price_id
+            except BilledOrganization.DoesNotExist:
                 return None
         # Otherwise, try to find a valid license on this instance
         license = License.objects.filter(valid_until__gte=now()).first()
@@ -134,8 +141,19 @@ class User(AbstractUser):
         return License.PLANS[user_plan]
 
     @property
+    def teams(self):
+        return Team.objects.filter(organization__in=self.organizations.all())
+
+    @property
+    def organization(self) -> Organization:
+        if self.current_organization is None:
+            raise Organization.DoesNotExist
+        return self.current_organization
+
+    @property
     def team(self) -> Team:
-        if hasattr(self, "_team"):
-            return self._team
-        self._team: Team = self.team_set.get()
-        return self._team
+        if self.current_team is None:
+            raise Team.DoesNotExist
+        return self.current_team
+
+    __repr__ = sane_repr("email", "first_name", "distinct_id")
