2fd4fc90d4604470c8ac450d5c75481c15bb02f2
James Greenhill <fuziontech@gmail.com>
2020-09-17 11:22:00 -0700

    Materialize Views to wrap data coming in from Kafka for Events, Elements, People (#1678)

--- ee/clickhouse/migrations/0004_elements.py
+++ ee/clickhouse/migrations/0004_elements.py
@@ -1,7 +1,7 @@
 from infi.clickhouse_orm import migrations
 
 from ee.clickhouse.sql.elements import (
-    ELEMENT_GROUP_TABLE_SQL,
+    ELEMENTS_GROUP_TABLE_SQL,
     ELEMENTS_PROPERTIES_MAT,
     ELEMENTS_TABLE_SQL,
     ELEMENTS_WITH_ARRAY_PROPS,
@@ -10,7 +10,7 @@ from ee.clickhouse.sql.elements import (
 
 operations = [
     migrations.RunSQL(ELEMENTS_TABLE_SQL),
-    migrations.RunSQL(ELEMENT_GROUP_TABLE_SQL),
+    migrations.RunSQL(ELEMENTS_GROUP_TABLE_SQL),
     migrations.RunSQL(ELEMENTS_WITH_ARRAY_PROPS),
     migrations.RunSQL(ELEMENTS_WITH_ARRAY_PROPS_MAT),
     migrations.RunSQL(ELEMENTS_PROPERTIES_MAT),
--- ee/clickhouse/migrations/0005_kafka.py
+++ ee/clickhouse/migrations/0005_kafka.py
@@ -1,8 +1,18 @@
 from infi.clickhouse_orm import migrations
 
-from ee.clickhouse.sql.elements import KAFKA_ELEMENTS_GROUP_TABLE_SQL, KAFKA_ELEMENTS_TABLE_SQL
-from ee.clickhouse.sql.events import KAFKA_EVENTS_TABLE_SQL
-from ee.clickhouse.sql.person import KAFKA_PERSONS_DISTINCT_ID_TABLE_SQL, KAFKA_PERSONS_TABLE_SQL
+from ee.clickhouse.sql.elements import (
+    ELEMENTS_GROUP_TABLE_MV_SQL,
+    ELEMENTS_TABLE_MV_SQL,
+    KAFKA_ELEMENTS_GROUP_TABLE_SQL,
+    KAFKA_ELEMENTS_TABLE_SQL,
+)
+from ee.clickhouse.sql.events import EVENTS_TABLE_MV_SQL, KAFKA_EVENTS_TABLE_SQL
+from ee.clickhouse.sql.person import (
+    KAFKA_PERSONS_DISTINCT_ID_TABLE_SQL,
+    KAFKA_PERSONS_TABLE_SQL,
+    PERSONS_DISTINCT_ID_TABLE_MV_SQL,
+    PERSONS_TABLE_MV_SQL,
+)
 
 operations = [
     migrations.RunSQL(KAFKA_EVENTS_TABLE_SQL),
@@ -10,4 +20,9 @@ operations = [
     migrations.RunSQL(KAFKA_ELEMENTS_GROUP_TABLE_SQL),
     migrations.RunSQL(KAFKA_PERSONS_TABLE_SQL),
     migrations.RunSQL(KAFKA_PERSONS_DISTINCT_ID_TABLE_SQL),
+    migrations.RunSQL(EVENTS_TABLE_MV_SQL),
+    migrations.RunSQL(ELEMENTS_TABLE_MV_SQL),
+    migrations.RunSQL(ELEMENTS_GROUP_TABLE_MV_SQL),
+    migrations.RunSQL(PERSONS_TABLE_MV_SQL),
+    migrations.RunSQL(PERSONS_DISTINCT_ID_TABLE_MV_SQL),
 ]
