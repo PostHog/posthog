944783d79b077a77a70f193fe6c015541757c1b1
Eric <eeoneric@gmail.com>
2020-09-18 16:27:56 -0400

    condensed code

--- ee/clickhouse/queries/clickhouse_stickiness.py
+++ ee/clickhouse/queries/clickhouse_stickiness.py
@@ -39,17 +39,7 @@ SELECT DISTINCT pid FROM (
     SELECT DISTINCT person_distinct_id.person_id as pid, countDistinct(toDate(timestamp)) as day_count
     FROM events
     LEFT JOIN person_distinct_id ON person_distinct_id.distinct_id = events.distinct_id
-    WHERE team_id = %(team_id)s AND event = %(event)s {filters} {parsed_date_from} {parsed_date_to}
-    GROUP BY person_distinct_id.person_id
-) WHERE day_count = %(stickiness_day)s
-"""
-
-STICKINESS_ACTIONS_PEOPLE_SQL = """
-SELECT DISTINCT pid FROM (
-    SELECT DISTINCT person_distinct_id.person_id as pid, countDistinct(toDate(timestamp)) as day_count
-    FROM events
-    LEFT JOIN person_distinct_id ON person_distinct_id.distinct_id = events.distinct_id
-    WHERE team_id = %(team_id)s AND id IN ({actions_query}) {filters} {parsed_date_from} {parsed_date_to}
+    WHERE team_id = %(team_id)s {entity_filter} {filters} {parsed_date_from} {parsed_date_to}
     GROUP BY person_distinct_id.person_id
 ) WHERE day_count = %(stickiness_day)s
 """
--- ee/clickhouse/views/actions.py
+++ ee/clickhouse/views/actions.py
@@ -1,6 +1,6 @@
 # NOTE: bad django practice but /ee specifically depends on /posthog so it should be fine
 import json
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Tuple
 
 from rest_framework.decorators import action
 from rest_framework.request import Request
@@ -10,22 +10,17 @@ from ee.clickhouse.client import ch_client
 from ee.clickhouse.models.action import format_action_filter
 from ee.clickhouse.models.person import ClickhousePersonSerializer
 from ee.clickhouse.models.property import parse_prop_clauses
-from ee.clickhouse.queries.clickhouse_stickiness import STICKINESS_ACTIONS_PEOPLE_SQL, STICKINESS_PEOPLE_SQL
+from ee.clickhouse.queries.clickhouse_stickiness import STICKINESS_PEOPLE_SQL
 from ee.clickhouse.queries.util import parse_timestamps
 from posthog.api.action import ActionViewSet
 from posthog.constants import TREND_FILTER_TYPE_ACTIONS
 from posthog.models.action import Action
 from posthog.models.entity import Entity
 from posthog.models.filter import Filter
-from posthog.models.property import Property
 from posthog.models.team import Team
 
 PERSON_TREND_SQL = """
-SELECT DISTINCT distinct_id FROM events WHERE team_id = %(team_id)s AND event = %(event)s {filters} {parsed_date_from} {parsed_date_to}
-"""
-
-PERSON_TREND_ACTIONS_SQL = """
-SELECT DISTINCT distinct_id FROM events WHERE team_id = %(team_id)s AND id IN ({actions_query}) {filters} {parsed_date_from} {parsed_date_to}
+SELECT DISTINCT distinct_id FROM events WHERE team_id = %(team_id)s {entity_filter} {filters} {parsed_date_from} {parsed_date_to}
 """
 
 PEOPLE_THROUGH_DISTINCT_SQL = """
@@ -70,31 +65,34 @@ class ClickhouseActions(ActionViewSet):
             }
         )
 
-    def _calculate_stickiness_entity_people(self, team: Team, entity: Entity, filter: Filter, stickiness_day: int):
-        parsed_date_from, parsed_date_to = parse_timestamps(filter=filter)
-        prop_filters, prop_filter_params = parse_prop_clauses("id", filter.properties, team)
-        params: Dict = {"team_id": team.pk, **prop_filter_params, "stickiness_day": stickiness_day}
-
+    def _format_entity_filter(self, entity: Entity) -> Tuple[str, Dict]:
         if entity.type == TREND_FILTER_TYPE_ACTIONS:
             try:
                 action = Action.objects.get(pk=entity.id)
-                action_query, action_params = format_action_filter(action)
-                params = {**params, **action_params}
-                content_sql = STICKINESS_ACTIONS_PEOPLE_SQL.format(
-                    actions_query=action_query,
-                    parsed_date_from=(parsed_date_from or ""),
-                    parsed_date_to=(parsed_date_to or ""),
-                    filters="{filters}".format(filters=prop_filters) if filter.properties else "",
-                )
+                action_query, params = format_action_filter(action)
+                entity_filter = "AND id IN ({})".format(action_query)
+
             except Action.DoesNotExist:
-                return []
+                raise ValueError("This action does not exist")
         else:
-            content_sql = STICKINESS_PEOPLE_SQL.format(
-                parsed_date_from=(parsed_date_from or ""),
-                parsed_date_to=(parsed_date_to or ""),
-                filters="{filters}".format(filters=prop_filters) if filter.properties else "",
-            )
-            params = {**params, "event": entity.id}
+            entity_filter = "AND event = %(event)s"
+            params = {"event": entity.id}
+
+        return entity_filter, params
+
+    def _calculate_stickiness_entity_people(self, team: Team, entity: Entity, filter: Filter, stickiness_day: int):
+        parsed_date_from, parsed_date_to = parse_timestamps(filter=filter)
+        prop_filters, prop_filter_params = parse_prop_clauses("id", filter.properties, team)
+        entity_sql, entity_params = self._format_entity_filter(entity=entity)
+
+        params: Dict = {"team_id": team.pk, **prop_filter_params, "stickiness_day": stickiness_day, **entity_params}
+
+        content_sql = STICKINESS_PEOPLE_SQL.format(
+            entity_filter=entity_sql,
+            parsed_date_from=(parsed_date_from or ""),
+            parsed_date_to=(parsed_date_to or ""),
+            filters="{filters}".format(filters=prop_filters) if filter.properties else "",
+        )
 
         people = ch_client.execute(PEOPLE_SQL.format(content_sql=content_sql), params)
         serialized_people = ClickhousePersonSerializer(people, many=True).data
@@ -104,30 +102,17 @@ class ClickhouseActions(ActionViewSet):
     def _calculate_entity_people(self, team: Team, entity: Entity, filter: Filter):
         parsed_date_from, parsed_date_to = parse_timestamps(filter=filter)
         prop_filters, prop_filter_params = parse_prop_clauses("id", filter.properties, team)
-        params: Dict = {"team_id": team.pk, **prop_filter_params}
+        entity_sql, entity_params = self._format_entity_filter(entity=entity)
 
-        if entity.type == TREND_FILTER_TYPE_ACTIONS:
-            try:
-                action = Action.objects.get(pk=entity.id)
-                action_query, action_params = format_action_filter(action)
-                params = {**params, **action_params}
-                content_sql = PERSON_TREND_ACTIONS_SQL.format(
-                    actions_query=action_query,
-                    parsed_date_from=(parsed_date_from or ""),
-                    parsed_date_to=(parsed_date_to or ""),
-                    filters="{filters}".format(filters=prop_filters) if filter.properties else "",
-                    breakdown_filter="",
-                )
-            except:
-                people = []
-        else:
-            content_sql = PERSON_TREND_SQL.format(
-                parsed_date_from=(parsed_date_from or ""),
-                parsed_date_to=(parsed_date_to or ""),
-                filters="{filters}".format(filters=prop_filters) if filter.properties else "",
-                breakdown_filter="",
-            )
-            params = {**params, "event": entity.id}
+        params: Dict = {"team_id": team.pk, **prop_filter_params, **entity_params}
+
+        content_sql = PERSON_TREND_SQL.format(
+            entity_filter=entity_sql,
+            parsed_date_from=(parsed_date_from or ""),
+            parsed_date_to=(parsed_date_to or ""),
+            filters="{filters}".format(filters=prop_filters) if filter.properties else "",
+            breakdown_filter="",
+        )
         people = ch_client.execute(PEOPLE_THROUGH_DISTINCT_SQL.format(content_sql=content_sql), params)
         serialized_people = ClickhousePersonSerializer(people, many=True).data
 
