76da2c4ed8a127b204c2f7285e1eec894dc07080
Michael Matloka <dev@twixes.com>
2020-09-22 14:06:23 +0200

    Make score numbers nicer and add a color between red and orange
    Slightly pedantically made passwordScore always a multiple of 20%. :)

--- frontend/src/lib/components/PasswordStrength.tsx
+++ frontend/src/lib/components/PasswordStrength.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Progress } from 'antd'
-import { green, orange, red, yellow } from '@ant-design/colors'
+import { red, volcano, orange, yellow, green } from '@ant-design/colors'
 import zxcvbn from 'zxcvbn'
 
 export default function PasswordStrength({ password = '' }: { password: string }): JSX.Element {
-    const MAX_ZXCVBN_SCORE = 4
-    const passwordScore: number = (zxcvbn(password).score / MAX_ZXCVBN_SCORE) * 100
+    // passwordScore is 0 if no password input
+    // passwordScore is 20, 40, 60, 80, or 100 if password input, based on zxcvbn score (which is 0, 1, 2, 3, or 4)
+    const passwordScore: number = password.length && (zxcvbn(password).score * 20 + 20)
 
     return (
         <Progress
-            percent={passwordScore === 0 && password.length > 0 ? 10 : passwordScore}
+            percent={passwordScore}
             size="small"
             strokeColor={
-                passwordScore < 25
+                passwordScore <= 20
                     ? red.primary
-                    : passwordScore < 50
+                    : passwordScore <= 40
+                    ? volcano.primary
+                    : passwordScore <= 60
                     ? orange.primary
-                    : passwordScore < 75
+                    : passwordScore <= 80
                     ? yellow.primary
                     : green.primary
             }
