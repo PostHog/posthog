66e29bad8917a35d20bfd752a8fc1852ee88fc7e
Michael Matloka <dev@twixes.com>
2020-09-23 09:52:56 +0200

    Update team_user.py

--- posthog/api/team_user.py
+++ posthog/api/team_user.py
@@ -6,7 +6,7 @@ from posthog.api.user import UserSerializer
 from posthog.models import Team, User
 
 
-class TeamUserViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TeamUserViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.none()
     lookup_field = "distinct_id"
