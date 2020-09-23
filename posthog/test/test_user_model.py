e166a81ad45096d028bddc733cde9bdef256a805
Michael Matloka <dev@twixes.com>
2020-09-23 09:48:09 +0200

    Move multitenancy price tests to posthog-production

--- posthog/test/test_user_model.py
+++ posthog/test/test_user_model.py
@@ -17,22 +17,6 @@ class TestUser(BaseTest):
         self.assertFalse(self.user.is_feature_available("whatever"))
 
     @tag("ee")
-    @patch("posthog.models.user.MULTI_TENANCY_MISSING", False)
-    @patch("posthog.models.user.License.PLANS", {"price_1234567890": ["whatever"]})
-    @patch("posthog.models.user.BilledOrganization")
-    def test_feature_available_multi_tenancy(self, patch_billed_organization):
-        patch_billed_organization.objects.get().price_id = "price_1234567890"
-        self.assertTrue(self.user.is_feature_available("whatever"))
-
-    @patch("posthog.models.user.MULTI_TENANCY_MISSING", False)
-    @patch("posthog.models.user.BilledOrganization")
-    def test_custom_pricing_no_extra_features(self, patch_billed_organization):
-        patch_billed_organization.objects.get().price_id = (
-            "price_test_1"  # price_test_1 is not on posthog.models.user.License.PLANS
-        )
-        self.assertFalse(self.user.is_feature_available("whatever"))
-
-    @tag("ee")
     @patch("posthog.models.user.License.PLANS", {"enterprise": ["whatever"]})
     @patch("ee.models.license.requests.post")
     @patch("posthog.models.user.MULTI_TENANCY_MISSING", True)
