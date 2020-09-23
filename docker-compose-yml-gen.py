af80f091139eb955c4b62f93b45378b7f08bcdc7
Michael Matloka <dev@twixes.com>
2020-09-23 23:35:36 +0200

    Improve error handling

--- docker-compose-yml-gen.py
+++ docker-compose-yml-gen.py
@@ -10,19 +10,24 @@ FILE_PATH = "docker-compose.yml"
 
 def generate_secret_key() -> str:
     """Securely generate random 50 ASCII letters and digits."""
-    return "".join(
-        secrets.choice(string.ascii_letters + string.digits) for _ in range(50)
-    )  # random 50 ASCII letters and digits
+    return "".join(secrets.choice(string.ascii_letters + string.digits) for _ in range(50))
 
 
-def save_yml(*, host_port: int, secret_key: str):
+def read_config() -> dict:
     with open(FILE_PATH, "r") as file:
         config = yaml.load(file, Loader=FullLoader)
     if not config:
-        raise ValueError("Cannot edit a malformed docker-compose.yml! Get a fresh one from the PostHog repository.")
+        raise ValueError("Current docker-compose.yml is malformed! Get a fresh one from the PostHog repository.")
+    return config
+
+
+def save_config(config: dict, *, host_port: int, secret_key: str) -> None:
     with open(FILE_PATH, "w") as file:
-        config["services"]["web"]["ports"] = [f"{host_port}:8000"]
-        config["services"]["web"]["environment"]["SECRET_KEY"] = secret_key
+        try:
+            config["services"]["web"]["ports"] = [f"{host_port}:8000"]
+            config["services"]["web"]["environment"]["SECRET_KEY"] = secret_key
+        except KeyError:
+            raise ValueError("Current docker-compose.yml is malformed! Get a fresh one from the PostHog repository.")
         yaml.dump(
             config, stream=file, indent=4,
         )
@@ -30,7 +35,7 @@ def save_yml(*, host_port: int, secret_key: str):
 
 def input_host_port(n: int, default: int = 80) -> int:
     print(
-        f"{n}. On which host port should the PostHog server be exposed? ({default})\n"
+        f"{n}. On which host port should the PostHog server be exposed? (default: {default})\n"
         "Leave on default value 80 if not using a reverse proxy such as nginx.\n"
         "Otherwise select a higher-numbered port (like 8000)\n"
         "and point your reverse proxy server to it.\n"
@@ -45,7 +50,7 @@ def input_host_port(n: int, default: int = 80) -> int:
 
 def input_secret_key(n: int) -> str:
     print(
-        f"{n}. Do you have a specific Django SECRET_KEY? (random)\n"
+        f"{n}. Do you have a specific Django SECRET_KEY? (default: random)\n"
         "Django uses the SECRET_KEY variable for e.g. encrypting sessions and tokens,\n"
         "so it's important that it is secret and only yours.\n"
         "If you don't have one already, skip this and let us generate you one.\n"
@@ -56,15 +61,16 @@ def input_secret_key(n: int) -> str:
 def main():
     host_port: Optional[int] = None
     secret_key: Optional[str] = None
+    config: dict = read_config()
 
     print("Let's build the right production docker-compose.yml for your PostHog instance.\n")
 
-    print("If you're OK with a step's default value (in parentheses),\n" "you can skip it by pressing Enter/Return.\n")
+    print("If you're OK with a step's default value, you can skip by pressing Enter/Return.\n")
 
     host_port = input_host_port(1)
     secret_key = input_secret_key(2)
 
-    save_yml(host_port=host_port, secret_key=secret_key)
+    save_config(config, host_port=host_port, secret_key=secret_key)
 
     print("\nGenerated and saved docker-compose.yml. You can now use it in production.")
 
