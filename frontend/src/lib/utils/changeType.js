05785691758f7b9745b145e2723fa8e1380b2edf
cr33dx <shashwatmahar12@gmail.com>
2020-09-24 00:33:11 +0530

    added function description

--- frontend/src/lib/utils/changeType.js
+++ frontend/src/lib/utils/changeType.js
@@ -1,15 +1,19 @@
-export function changeType(type, value){
-    switch(type)
-    {
-      case "text":
-        return String(value);
-      case "number":
-        return Number(value);
-      case "boolean":
-        return value == "true" ? true : false;
-      case "string":
-        return String(value);
-      default:
-        throw "Unsupported type";
+/** 
+ * Change Type Of Passed Value
+ * @param {Type} type The type to be casted to 
+ * @param {Number/String} value the value to be changed 
+**/
+export function changeType(type, value) {
+    switch (type) {
+        case 'text':
+            return String(value)
+        case 'number':
+            return Number(value)
+        case 'boolean':
+            return value == 'true' ? true : false
+        case 'string':
+            return String(value)
+        default:
+            throw 'Unsupported type'
     }
 }
