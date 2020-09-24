80b702a7fd1bc81314acda2f485e51846b6c68e9
Marius Andra <marius.andra@gmail.com>
2020-09-24 16:27:12 +0200

    create persons via postgres in paths test

--- ee/clickhouse/queries/test/test_paths.py
+++ ee/clickhouse/queries/test/test_paths.py
@@ -1,9 +1,9 @@
 from ee.clickhouse.models.event import create_event
-from ee.clickhouse.models.person import create_person
 from ee.clickhouse.queries.clickhouse_paths import ClickhousePaths
 from ee.clickhouse.util import ClickhouseTestMixin
+from posthog.models.person import Person
 from posthog.queries.test.test_paths import paths_test_factory
 
 
-class TestClickhousePaths(ClickhouseTestMixin, paths_test_factory(ClickhousePaths, create_event, create_person)):  # type: ignore
+class TestClickhousePaths(ClickhouseTestMixin, paths_test_factory(ClickhousePaths, create_event, Person.objects.create)):  # type: ignore
     pass
