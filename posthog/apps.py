8a6a0c1c3d05b3e739621c937a0e64337d0ac979
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-09-24 07:10:09 +0100

    add posthog_version to development server launched

--- posthog/apps.py
+++ posthog/apps.py
@@ -5,6 +5,7 @@ from django.apps import AppConfig
 from django.conf import settings
 
 from posthog.utils import get_machine_id
+from posthog.version import VERSION
 
 
 class PostHogConfig(AppConfig):
@@ -16,7 +17,9 @@ class PostHogConfig(AppConfig):
         if settings.DEBUG:
             # log development server launch to posthog
             if os.getenv("RUN_MAIN") == "true":
-                posthoganalytics.capture(get_machine_id(), "development server launched")
+                posthoganalytics.capture(
+                    get_machine_id(), "development server launched", {"posthog_version": VERSION},
+                )
             posthoganalytics.disabled = True
         elif settings.TEST or os.environ.get("OPT_OUT_CAPTURE"):
             posthoganalytics.disabled = True
