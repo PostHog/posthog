d4459ab448b00a1e2bb69f6775cba9b4fdaf59da
Shashwat <shashwatmahar12@gmail.com>
2020-09-30 20:52:55 +0530

    Changes to make person editable (resolves #89) (#1491)
    * added editable table for person data
    
    * update to setPerson, changes to make person editable
    
    * Ran Prettier
    
    * ran Eslint
    
    * update user properties
    
    * ran prettier
    
    * don't allow person edit before change
    
    * wakey wakey GitHub Actions
    
    * fix to send person state to api.update
    
    * Added antd's confirm modal to save/delete data , removed browser alert
    
    * Delete Person Modal not in scope of current PR reverting delete moadal changes
    
    * ran prettier
    
    * Update Person.js
    
    Remove unused import
    
    * minor style change
    
    * Fixed showing name which caused user with only is_demo true not show, save button now vsisble on all dimensions, is_demo is now editable
    
    * minor style fixes
    
    * Made Person's Number non-editable
    
    * Disable input for PostHog internal varaiables
    
    * added change to typecast to desired value
    
    * added function description
    
    * Handle diffrenet properties type.
    
    * ran prettier
    
    Co-authored-by: Yakko Majuri <yakko.majuri@gmail.com>
    Co-authored-by: Michael Matloka <dev@twixes.com>

--- frontend/src/scenes/users/Person.js
+++ frontend/src/scenes/users/Person.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { Events } from '../events/Events'
 import api from 'lib/api'
-import { PropertiesTable } from 'lib/components/PropertiesTable'
-import { deletePersonData } from 'lib/utils'
-import { Button } from 'antd'
+import { PersonTable } from './PersonTable'
+import { deletePersonData, savePersonData } from 'lib/utils'
+import { changeType } from 'lib/utils/changeType'
+import { Button, Modal } from 'antd'
+import { CheckCircleTwoTone, DeleteOutlined } from '@ant-design/icons'
 import { hot } from 'react-hot-loader/root'
 
+const confirm = Modal.confirm
 export const Person = hot(_Person)
 function _Person({ _: distinctId, id }) {
-    const [person, setPerson] = useState(null)
+    const { innerWidth } = window
+    const isScreenSmall = innerWidth < 700
 
+    const [person, setPerson] = useState(null)
+    const [personChanged, setPersonChanged] = useState(false)
     useEffect(() => {
         let url = ''
         if (distinctId) {
@@ -20,6 +26,54 @@ function _Person({ _: distinctId, id }) {
         api.get(url).then(setPerson)
     }, [distinctId, id])
 
+    function _handleChange(event) {
+        setPersonChanged(true)
+        let tag, value
+        let newState = {}
+        if (typeof event.item != 'undefined') {
+            tag = event.item.props.name
+            value = event.key === 'true' ? true : false
+        } else {
+            tag = event.target.getAttribute('tag')
+            value = event.target.value.length === 0 ? null : changeType(event.target.type, event.target.value)
+        }
+        if (tag == 'first' || tag == 'last') {
+            newState = {
+                ...person,
+                properties: {
+                    ...person.properties,
+                    name: {
+                        ...person.properties.name,
+                        [tag]: value,
+                    },
+                },
+            }
+        } else {
+            newState = {
+                ...person,
+                properties: {
+                    ...person.properties,
+                    [tag]: value,
+                },
+            }
+        }
+        setPerson(newState)
+    }
+
+    function showConfirm(type, text) {
+        confirm({
+            centered: true,
+            title: text,
+            icon: <CheckCircleTwoTone twoToneColor="#52c41a" />,
+            content: '',
+            okType: 'primary',
+            okText: 'Yes',
+            onOk() {
+                savePersonData(person)
+            },
+            onCancel() {},
+        })
+    }
     return person ? (
         <div>
             <Button
@@ -27,14 +81,26 @@ function _Person({ _: distinctId, id }) {
                 danger
                 onClick={() => deletePersonData(person, () => history.push('/people'))}
             >
-                Delete all data on this person
+                {isScreenSmall ? <DeleteOutlined></DeleteOutlined> : 'Delete all data on this person'}
             </Button>
-            <h1 className="page-header">{person.name}</h1>
+            <Button
+                className="float-right"
+                onClick={() => showConfirm('save', "Are you sure you want to update this person's properties?")}
+                disabled={!personChanged}
+                style={{ marginRight: '10px' }}
+            >
+                Save updated data
+            </Button>
+            <h1 className="page-header">
+                {'name' in person.properties ? person.properties.name.first : person.name}{' '}
+                {person.properties.name ? person.properties.name.last : ''}
+            </h1>
             <div style={{ maxWidth: 750 }}>
-                <PropertiesTable
+                <PersonTable
                     properties={{
-                        ...person.properties,
+                        props: { ...person.properties },
                         distinct_id: person.distinct_ids,
+                        onChange: { _handleChange },
                     }}
                 />
                 <small>
