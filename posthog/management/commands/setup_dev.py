ea4ca4f3bf8a4b33e752c634459ccbd70e5a1ec4
Michael Matloka <dev@twixes.com>
2020-10-01 00:37:06 +0200

    Update setup_dev.py

--- posthog/management/commands/setup_dev.py
+++ posthog/management/commands/setup_dev.py
@@ -2,37 +2,38 @@ import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db import connection
+from django.db import transaction
 
 from posthog.demo import _create_anonymous_users, _create_funnel, _recalculate
-from posthog.models import Organization, OrganizationMembership, Team, User
+from posthog.models import User
 
 
 class Command(BaseCommand):
     help = "Set up the instance for development/review with demo data"
 
     def handle(self, *args, **options):
-        organization, team, user = User.objects.bootstrap(
-            company_name="Hogflix",
-            email="test@posthog.com",
-            password="pass",
-            first_name="Mr. Pokee",
-            team_fields={
-                "name": "Hogflix App",
-                "completed_snippet_onboarding": True,
-                "event_names": ["$pageview", "$autocapture"],
-                "event_properties": ["$current_url", "$browser", "$os"],
-            },
-        )
-        user = User.objects.create_superuser(email="test@posthog.com", password="pass")
-        OrganizationMembership.objects.create(
-            organization=organization, user=user, level=OrganizationMembership.Level.ADMIN
-        )
-        user.current_organization = organization
-        user.current_team = team
-        user.save()
-        heroku_app_name = os.getenv("HEROKU_APP_NAME")
-        base_url = f"https://{heroku_app_name}.herokuapp.com/demo/" if heroku_app_name else f"{settings.SITE_URL}/demo/"
-        _create_anonymous_users(team=team, base_url=base_url)
-        _create_funnel(team=team, base_url=base_url)
-        _recalculate(team=team)
+        with transaction.atomic():
+            organization, team, user = User.objects.bootstrap(
+                company_name="Hogflix",
+                email="test@posthog.com",
+                password="pass",
+                first_name="Mr. Pokee",
+                is_staff=True,
+                is_superuser=True,
+                team_fields={
+                    "name": "Hogflix App",
+                    "completed_snippet_onboarding": True,
+                    "event_names": ["$pageview", "$autocapture"],
+                    "event_properties": ["$current_url", "$browser", "$os"],
+                },
+            )
+            user.current_organization = organization
+            user.current_team = team
+            user.save()
+            heroku_app_name = os.getenv("HEROKU_APP_NAME")
+            base_url = (
+                f"https://{heroku_app_name}.herokuapp.com/demo/" if heroku_app_name else f"{settings.SITE_URL}/demo/"
+            )
+            _create_anonymous_users(team=team, base_url=base_url)
+            _create_funnel(team=team, base_url=base_url)
+            _recalculate(team=team)
