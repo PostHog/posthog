034aa1791437d61860316853063e684a5321865a
Michael Matloka <dev@twixes.com>
2020-10-01 16:53:11 +0200

    Use default DRF exception handler for debugging

--- posthog/settings.py
+++ posthog/settings.py
@@ -402,7 +402,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated",],
-    "EXCEPTION_HANDLER": "exceptions_hog.exception_handler",
+    "EXCEPTION_HANDLER": "rest_framework.views.exception_handler" if DEBUG else "exceptions_hog.exception_handler",
     "PAGE_SIZE": 100,
 }
 
