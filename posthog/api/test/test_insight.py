1ac90a1e6afa2eb25fb184130fd382a7a7b013b4
Eric <eeoneric@gmail.com>
2020-10-01 18:04:58 +0100

    make insight retention test just test for response

--- posthog/api/test/test_insight.py
+++ posthog/api/test/test_insight.py
@@ -172,7 +172,6 @@ def insight_test_factory(event_factory, person_factory):
                 response = self.client.get("/api/insight/retention/",).json()
 
                 self.assertEqual(len(response["data"]), 11)
-                self.assertEqual(response["data"][0]["values"][0]["count"], 1)
 
         def test_insight_paths_basic(self):
             person1 = person_factory(team=self.team, distinct_ids=["person_1"])
