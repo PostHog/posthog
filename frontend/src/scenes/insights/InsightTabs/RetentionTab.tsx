2922048f8c7f6f2d1af902a57c9666b9f9dd3f41
Eric <eeoneric@gmail.com>
2020-10-02 11:35:19 +0100

    remove feature flag

--- frontend/src/scenes/insights/InsightTabs/RetentionTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/RetentionTab.tsx
@@ -7,7 +7,6 @@ import { entityFilterLogic } from '../ActionFilter/entityFilterLogic'
 import { DownOutlined } from '@ant-design/icons'
 import { retentionTableLogic, dateOptions } from 'scenes/retention/retentionTableLogic'
 import { DatePicker, Select } from 'antd'
-import posthog from 'posthog-js'
 
 export function RetentionTab(): JSX.Element {
     const node = useRef()
@@ -54,35 +53,33 @@ export function RetentionTab(): JSX.Element {
             <hr />
             <h4 className="secondary">Filters</h4>
             <PropertyFilters pageKey="insight-retention" />
-            {posthog.isFeatureEnabled('ch-retention-endpoint') && (
-                <>
-                    <hr />
-                    <h4 className="secondary">Start Day</h4>
-                    <div>
-                        <DatePicker
-                            className="mb-2"
-                            value={selectedDate}
-                            onChange={(date): void => setFilters({ selectedDate: date })}
-                            allowClear={false}
-                        />
-                    </div>
-                    <hr />
-                    <h4 className="secondary">Period</h4>
-                    <div>
-                        <Select
-                            value={dateOptions[period]}
-                            onChange={(value): void => setFilters({ period: value })}
-                            dropdownMatchSelectWidth={false}
-                        >
-                            {Object.entries(dateOptions).map(([key, value]) => (
-                                <Select.Option key={key} value={key}>
-                                    {value}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </div>
-                </>
-            )}
+            <>
+                <hr />
+                <h4 className="secondary">Start Day</h4>
+                <div>
+                    <DatePicker
+                        className="mb-2"
+                        value={selectedDate}
+                        onChange={(date): void => setFilters({ selectedDate: date })}
+                        allowClear={false}
+                    />
+                </div>
+                <hr />
+                <h4 className="secondary">Period</h4>
+                <div>
+                    <Select
+                        value={dateOptions[period]}
+                        onChange={(value): void => setFilters({ period: value })}
+                        dropdownMatchSelectWidth={false}
+                    >
+                        {Object.entries(dateOptions).map(([key, value]) => (
+                            <Select.Option key={key} value={key}>
+                                {value}
+                            </Select.Option>
+                        ))}
+                    </Select>
+                </div>
+            </>
         </div>
     )
 }
