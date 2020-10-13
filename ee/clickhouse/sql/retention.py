0b96fda2bc485b07216f640bc2772776668bff5c
James Greenhill <fuziontech@gmail.com>
2020-10-13 17:18:35 +0200

    query fixes for person_retention_period

--- ee/clickhouse/sql/retention.py
+++ ee/clickhouse/sql/retention.py
@@ -2,7 +2,7 @@
 
 RETENTION_SQL = """
 SELECT
-    datediff(%(period)s, {trunc_func}(toDateTime(%(start_Date)s)), first_date) as first_date,
+    datediff(%(period)s, {trunc_func}(toDateTime(%(start_date)s)), first_date) as first_date,
     datediff(%(period)s, first_event_date.first_date, {trunc_func}(toDateTime(timestamp))) as date,
     COUNT(DISTINCT events.person_id) count
 FROM (
@@ -10,21 +10,20 @@ FROM (
     timestamp,
     person_id
     from events e join person_distinct_id pdi on e.distinct_id = pdi.distinct_id
-    WHERE toDateTime(e.timestamp) >= toDateTime(%(start_Date)s) AND toDateTime(e.timestamp) <= toDateTime(%(end_date)s)
+    WHERE toDateTime(e.timestamp) >= toDateTime(%(start_date)s) AND toDateTime(e.timestamp) <= toDateTime(%(end_date)s)
     AND e.team_id = %(team_id)s {target_query} {filters}
 ) events
 JOIN (
     SELECT
     person_id,
-        if({trunc_func}(minMerge(first_observation)) >= toDateTime(%(start_date)), {trunc_func}(minMerge(first_observation)),toDateTime(%(start_date))) as first_date
-    FROM events_retention_period_start_test
+        if({trunc_func}(minMerge(first_observation)) >= toDateTime(%(start_date)s), {trunc_func}(minMerge(first_observation)),toDateTime(%(start_date)s)) as first_date
+    FROM  person_retention_period e
+    WHERE
+        team_id = %(team_id)s {target_query} {filters}
     GROUP BY
-        person_id,
-        team_id,
-        event
+        person_id
     HAVING toDateTime(maxMerge(last_observation)) >= %(start_date)s
     AND toDateTime(maxMerge(last_observation)) <= %(end_date)s
-    AND team_id = %(team_id)s {target_query} {filters}
 ) first_event_date
     ON (events.person_id = first_event_date.person_id)
 WHERE {trunc_func}(events.timestamp) >= {trunc_func}(first_event_date.first_date)
