cc86afcd1381a526026de6ab0bb0d0c3cdd7b559
Michael Matloka <dev@twixes.com>
2020-10-14 11:37:39 +0200

    Delete console.log

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -378,7 +378,6 @@ export function isURL(string: string): boolean {
     if (!string) return false
     // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
     const regexp = /^\s*https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
-    console.log(string, string.match, string.match && string.match(regexp))
     return !!string.match?.(regexp)
 }
 
