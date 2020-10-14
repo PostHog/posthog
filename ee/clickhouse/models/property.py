d776c99a57cde30632520d5ad0e9cfe41fcdc404
Eric <eeoneric@gmail.com>
2020-10-14 16:29:27 +0000

    change parsing to include operators'

--- ee/clickhouse/models/property.py
+++ ee/clickhouse/models/property.py
@@ -2,7 +2,6 @@ from typing import Any, Dict, List, Optional, Tuple
 
 from ee.clickhouse.client import sync_execute
 from ee.clickhouse.models.cohort import format_filter_query
-from ee.clickhouse.sql.cohort import COHORT_DISTINCT_ID_FILTER_SQL
 from ee.clickhouse.sql.events import EVENT_PROP_CLAUSE, SELECT_PROP_VALUES_SQL, SELECT_PROP_VALUES_SQL_WITH_FILTER
 from ee.clickhouse.sql.person import GET_DISTINCT_IDS_BY_PROPERTY_SQL
 from posthog.models.cohort import Cohort
@@ -22,61 +21,55 @@ def parse_prop_clauses(key: str, filters: List[Property], team: Team, prepend: s
             final += "{cond} ({clause}) ".format(cond="AND distinct_id IN", clause=person_id_query)
 
         elif prop.type == "person":
+
             prepend = "person"
-            filter = "(ep.key = %(k{prepend}_{idx})s) AND (ep.value {operator} %(v{prepend}_{idx})s)".format(
-                idx=idx, operator=get_operator(prop.operator), prepend=prepend
+
+            arg = "v{}_{}".format(prepend, idx)
+            operator_clause, value = get_operator(prop, arg)
+
+            filter = "(ep.key = %(k{prepend}_{idx})s) AND {operator_clause}".format(
+                idx=idx, operator_clause=operator_clause, prepend=prepend
             )
             clause = GET_DISTINCT_IDS_BY_PROPERTY_SQL.format(filters=filter)
             final += "{cond} ({clause}) ".format(cond="AND distinct_id IN", clause=clause)
-            params.update(
-                {"k{}_{}".format(prepend, idx): prop.key, "v{}_{}".format(prepend, idx): _pad_value(prop.value)}
-            )
+            params.update({"k{}_{}".format(prepend, idx): prop.key, arg: prop.value})
 
         else:
-            filter = "(ep.key = %(k{prepend}_{idx})s) AND (ep.value {operator} %(v{prepend}_{idx})s)".format(
-                idx=idx, operator=get_operator(prop.operator), prepend=prepend
+
+            arg = "v{}_{}".format(prepend, idx)
+            operator_clause, value = get_operator(prop, arg)
+
+            filter = "(ep.key = %(k{prepend}_{idx})s) AND {operator_clause}".format(
+                idx=idx, operator_clause=operator_clause, prepend=prepend
             )
             clause = EVENT_PROP_CLAUSE.format(team_id=team.pk, filters=filter)
             final += "{cond} ({clause}) ".format(cond="AND {key} IN".format(key=key), clause=clause)
-            params.update(
-                {"k{}_{}".format(prepend, idx): prop.key, "v{}_{}".format(prepend, idx): _pad_value(prop.value)}
-            )
+            params.update({"k{}_{}".format(prepend, idx): prop.key, arg: value})
 
     return final, params
 
 
-def _pad_value(val: str):
+def get_operator(prop: Property, arg: str):
+    operator = prop.operator
 
-    if val == "true" or val == "false" or val.isdigit():
-        return val
-
-    if not val.startswith('"'):
-        val = '"' + val
-
-    if not val.endswith('"'):
-        val = val + '"'
-
-    return val
-
-
-# TODO: handle all operators
-def get_operator(operator: Optional[str]):
     if operator == "is_not":
-        return "!="
+        return "(trim(BOTH '\"' FROM ep.value) != %({})s)".format(arg), prop.value
     elif operator == "icontains":
-        return "LIKE"
+        value = "%{}%".format(prop.value)
+        return "(trim(BOTH '\"' FROM ep.value) LIKE %({})s)".format(arg), value
     elif operator == "not_icontains":
-        return "NOT LIKE"
+        value = "%{}%".format(prop.value)
+        return "(trim(BOTH '\"' FROM ep.value) NOT LIKE %({})s)".format(arg), value
     elif operator == "regex":
-        return "="
+        return "match(trim(BOTH '\"' FROM ep.value), %({})s)".format(arg), prop.value
     elif operator == "not_regex":
-        return "="
+        return "NOT match(trim(BOTH '\"' FROM ep.value), %({})s)".format(arg), prop.value
     elif operator == "gt":
-        return ">"
+        return "(trim(BOTH '\"' FROM ep.value) >  %({})s)".format(arg), prop.value
     elif operator == "lt":
-        return "<"
+        return "(trim(BOTH '\"' FROM ep.value) <  %({})s)".format(arg), prop.value
     else:
-        return "="
+        return "(trim(BOTH '\"' FROM ep.value) =  %({})s)".format(arg), prop.value
 
 
 def get_property_values_for_key(key: str, team: Team, value: Optional[str] = None):
