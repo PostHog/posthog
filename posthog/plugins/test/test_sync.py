3a74b6559e282d39243d8fe9b36bba85985892f4
Marius Andra <marius.andra@gmail.com>
2020-10-15 10:52:11 +0200

    test plugin deletion

--- posthog/plugins/test/test_sync.py
+++ posthog/plugins/test/test_sync.py
@@ -1,10 +1,24 @@
 import json
 import os
 import tempfile
+from contextlib import contextmanager
 
 from posthog.api.test.base import BaseTest
 from posthog.models import Plugin
 from posthog.plugins.sync import sync_posthog_json_plugins
+
+
+@contextmanager
+def plugins_in_posthog_json(plugins):
+    filename = None
+    try:
+        fd, filename = tempfile.mkstemp(prefix="posthog-", suffix=".json")
+        os.write(fd, str.encode(json.dumps({"plugins": plugins})))
+        os.close(fd)
+        yield filename
+    finally:
+        if filename:
+            os.unlink(filename)
 
 
 class TestPluginsSync(BaseTest):
@@ -17,7 +31,7 @@ class TestPluginsSync(BaseTest):
     def test_load_plugin(self):
         self.assertEqual(len(Plugin.objects.all()), 0)
 
-        filename = self._write_json_plugins(
+        with plugins_in_posthog_json(
             [
                 {
                     "name": "posthog-currency-normalization-plugin",
@@ -25,13 +39,12 @@ class TestPluginsSync(BaseTest):
                     "config": {},
                 }
             ]
-        )
+        ) as filename:
+            sync_posthog_json_plugins(raise_errors=True, filename=filename)
+            self.assertEqual(len(Plugin.objects.all()), 1)
 
-        sync_posthog_json_plugins(raise_errors=True, filename=filename)
-        self.assertEqual(len(Plugin.objects.all()), 1)
-        sync_posthog_json_plugins(raise_errors=True, filename=filename)
-        self.assertEqual(len(Plugin.objects.all()), 1)
-        os.unlink(filename)
+            sync_posthog_json_plugins(raise_errors=True, filename=filename)
+            self.assertEqual(len(Plugin.objects.all()), 1)
 
         plugin = Plugin.objects.get()
         self.assertEqual(plugin.name, "posthog-currency-normalization-plugin")
@@ -40,3 +53,8 @@ class TestPluginsSync(BaseTest):
         self.assertEqual(plugin.from_web, False)
         self.assertEqual(plugin.archive, None)
         self.assertEqual(plugin.tag, "")
+
+        with plugins_in_posthog_json([]) as filename:
+            sync_posthog_json_plugins(raise_errors=True, filename=filename)
+
+        self.assertEqual(len(Plugin.objects.all()), 0)
