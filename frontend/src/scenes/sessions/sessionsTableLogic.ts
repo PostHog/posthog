8d84eda5450c5103f05e51c76c6297d296a0c3b8
Tim Glaser <tim.glaser@hiberly.com>
2020-10-15 12:43:36 +0100

    Add property filters to sessions (#1835)
    * Add property filters to sessions
    
    * Fix backwards+forwards and use debounce
    
    * Renamed filters to properties

--- frontend/src/scenes/sessions/sessionsTableLogic.ts
+++ frontend/src/scenes/sessions/sessionsTableLogic.ts
@@ -3,9 +3,25 @@ import api from 'lib/api'
 import moment from 'moment'
 import { toParams } from 'lib/utils'
 import { sessionsTableLogicType } from 'types/scenes/sessions/sessionsTableLogicType'
-import { SessionType } from '~/types'
+import { PropertyFilter, SessionType } from '~/types'
+import { router } from 'kea-router'
 
 type Moment = moment.Moment
+
+const buildURL = (selectedDateURLparam: string): [string, Record<string, any>] => {
+    const today = moment().startOf('day').format('YYYY-MM-DD')
+    const params: Record<string, any> = {}
+
+    const { properties } = router.values.searchParams // eslint-disable-line
+    if (selectedDateURLparam !== today) {
+        params.date = selectedDateURLparam
+    }
+    if (properties) {
+        params.properties = properties
+    }
+
+    return [router.values.location.pathname, params]
+}
 
 export const sessionsTableLogic = kea<sessionsTableLogicType<Moment, SessionType>>({
     props: {} as {
@@ -21,7 +37,9 @@ export const sessionsTableLogic = kea<sessionsTableLogicType<Moment, SessionType
                     date_to: selectedDateURLparam,
                     offset: 0,
                     distinct_id: props.personIds ? props.personIds[0] : '',
+                    properties: values.properties,
                 })
+                await breakpoint(10)
                 const response = await api.get(`api/insight/session/?${params}`)
                 breakpoint()
                 if (response.offset) {
@@ -35,9 +53,9 @@ export const sessionsTableLogic = kea<sessionsTableLogicType<Moment, SessionType
         setNextOffset: (nextOffset: number | null) => ({ nextOffset }),
         fetchNextSessions: true,
         appendNewSessions: (sessions) => ({ sessions }),
-        dateChanged: (date: Moment | null) => ({ date }),
         previousDay: true,
         nextDay: true,
+        setFilters: (properties: Array<PropertyFilter>, selectedDate: Moment | null) => ({ properties, selectedDate }),
     }),
     reducers: {
         sessions: {
@@ -52,7 +70,13 @@ export const sessionsTableLogic = kea<sessionsTableLogicType<Moment, SessionType
                 loadSessionsFailure: () => null,
             },
         ],
-        selectedDate: [null as null | Moment, { dateChanged: (_, { date }) => date }],
+        selectedDate: [null as null | Moment, { setFilters: (_, { selectedDate }) => selectedDate }],
+        properties: [
+            [],
+            {
+                setFilters: (_, { properties }) => properties,
+            },
+        ],
     },
     selectors: {
         selectedDateURLparam: [(s) => [s.selectedDate], (selectedDate) => selectedDate?.format('YYYY-MM-DD')],
@@ -73,37 +97,31 @@ export const sessionsTableLogic = kea<sessionsTableLogicType<Moment, SessionType
             }
             actions.appendNewSessions(response.result)
         },
-        dateChanged: () => {
-            actions.loadSessions(true)
+        setFilters: () => {
             actions.setNextOffset(null)
+            actions.loadSessions(true)
         },
         previousDay: () => {
-            actions.dateChanged(moment(values.selectedDate).add(-1, 'day'))
+            actions.setFilters(values.properties, moment(values.selectedDate).add(-1, 'day'))
         },
         nextDay: () => {
-            actions.dateChanged(moment(values.selectedDate).add(1, 'day'))
+            actions.setFilters(values.properties, moment(values.selectedDate).add(1, 'day'))
         },
     }),
     actionToUrl: ({ values }) => ({
-        dateChanged: () => {
-            const { selectedDateURLparam } = values
-            const today = moment().startOf('day').format('YYYY-MM-DD')
-            if (window && !window.location.href.includes('person')) {
-                return [`/sessions`, selectedDateURLparam === today ? {} : { date: selectedDateURLparam }]
-            }
+        setFilters: () => {
+            return buildURL(values.selectedDateURLparam)
         },
     }),
     urlToAction: ({ actions, values }) => ({
-        '/sessions': (_: any, { date }: { date: string }) => {
+        '/sessions': (_: any, { date, properties }: { date: string; properties: Array<PropertyFilter> }) => {
             const newDate = date ? moment(date).startOf('day') : moment().startOf('day')
-            if (!values.selectedDate || values.selectedDate.format('YYYY-MM-DD') !== newDate.format('YYYY-MM-DD')) {
-                actions.dateChanged(newDate)
-            }
+            actions.setFilters(properties || [], newDate)
         },
-        '/person/*': (_: any, { date }: { date: string }) => {
+        '/person/*': (_: any, { date, properties }: { date: string; properties: Array<PropertyFilter> }) => {
             const newDate = date ? moment(date).startOf('day') : moment().startOf('day')
             if (!values.selectedDate || values.selectedDate.format('YYYY-MM-DD') !== newDate.format('YYYY-MM-DD')) {
-                actions.dateChanged(newDate)
+                actions.setFilters(properties || [], newDate)
             }
         },
     }),
