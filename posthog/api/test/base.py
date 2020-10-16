0fea379750d0898a720c3d799e2e9f04274a5868
Marius Andra <marius.andra@gmail.com>
2020-10-16 17:30:57 +0200

    reload/reset plugins before each test

--- posthog/api/test/base.py
+++ posthog/api/test/base.py
@@ -5,6 +5,7 @@ from rest_framework.test import APITestCase
 
 from posthog.cache import clear_cache
 from posthog.models import Organization, Team, User
+from posthog.plugins import Plugins
 
 
 class TestMixin:
@@ -29,6 +30,7 @@ class TestMixin:
             self.client = Client()
             if self.TESTS_FORCE_LOGIN and self.TESTS_EMAIL:
                 self.client.force_login(self.user)
+        Plugins().reload_plugins()
 
 
 class ErrorResponsesMixin:
