89940a6cbe104faea5e43b75da314672decbf526
Marius Andra <marius.andra@gmail.com>
2020-10-17 18:56:47 +0200

    throw if no redis

--- posthog/cache.py
+++ posthog/cache.py
@@ -7,7 +7,7 @@ from django.conf import settings
 redis_instance = None  # type: Optional[redis.Redis]
 
 
-def get_redis_instance() -> Optional[redis.Redis]:
+def get_redis_instance() -> redis.Redis:
     global redis_instance
 
     if redis_instance:
@@ -17,6 +17,8 @@ def get_redis_instance() -> Optional[redis.Redis]:
         redis_instance = fakeredis.FakeStrictRedis()
     elif settings.REDIS_URL:
         redis_instance = redis.from_url(settings.REDIS_URL, db=0)
+    else:
+        raise Exception("Redis not configured!")
 
     return redis_instance
 
@@ -26,21 +28,14 @@ def get_cache_key(team_id: int, key: str) -> str:
 
 
 def get_cached_value(team_id: int, key: str) -> Optional[str]:
-    if not redis_instance:
-        raise Exception("Redis not configured!")
-    return redis_instance.get(get_cache_key(team_id, key))
+    return get_redis_instance().get(get_cache_key(team_id, key))
 
 
 def set_cached_value(team_id: int, key: str, value: str) -> None:
-    if not redis_instance:
-        raise Exception("Redis not configured!")
-    redis_instance.set(get_cache_key(team_id, key), value)
+    get_redis_instance().set(get_cache_key(team_id, key), value)
 
 
 def clear_cache() -> None:
     if not settings.TEST and not settings.DEBUG:
         raise Exception("Can only clear redis cache in TEST or DEBUG mode!")
-    if not redis_instance:
-        raise Exception("Redis not configured!")
-
-    redis_instance.flushdb()
+    get_redis_instance().flushdb()
