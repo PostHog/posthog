fc51dd7351e5f33d2b899dfd699d87a3e5c55c41
Tim Glaser <tim@glsr.nl>
2020-10-19 14:35:52 +0200

    aaaaa

--- posthog/api/test/test_event.py
+++ posthog/api/test/test_event.py
@@ -278,7 +278,8 @@ def test_event_api_factory(event_factory, person_factory, action_factory):
                 page2 = self.client.get(response["next"]).json()
                 from ee.clickhouse.client import sync_execute
 
-            self.assertEqual(sync_execute("select count(*) from events")[0][0], 150)
+                self.assertEqual(sync_execute("select count(*) from events")[0][0], 150)
+
             self.assertEqual(len(page2["results"]), 50)
 
     return TestEvents
