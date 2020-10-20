856cde7626efa08ffae62f67ea1adbfa6cb46f39
Tim Glaser <tim@glsr.nl>
2020-10-20 18:19:47 +0200

    Add usage button to feature flags

--- frontend/src/layout/TopContent/index.tsx
+++ frontend/src/layout/TopContent/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { LatestVersion } from './LatestVersion'
 import { User } from './User'
 import { CommandPaletteButton } from './CommandPaletteButton'
 import { isMobile } from 'lib/utils'
+import { router } from 'kea-router'
 import './index.scss'
+import { Link } from 'lib/components/Link'
+import { ArrowLeftOutlined } from '@ant-design/icons'
 
 export function TopContent(): JSX.Element {
+    const [{ backTo, backToURL }, setHashParams] = useState(router.values.hashParams)
+    useEffect(() => {
+        setHashParams(router.values.hashParams)
+    }, [router.values.hashParams])
+
     return (
         <div className="content py-3 layout-top-content">
             <div
@@ -18,7 +26,13 @@ export function TopContent(): JSX.Element {
                     fontSize: 13,
                 }}
             >
-                {!isMobile() && <CommandPaletteButton />}
+                {backTo ? (
+                    <Link to={backToURL}>
+                        <ArrowLeftOutlined /> Back to {backTo}
+                    </Link>
+                ) : (
+                    !isMobile() && <CommandPaletteButton />
+                )}
             </div>
             <div
                 className="layout-top-content"
