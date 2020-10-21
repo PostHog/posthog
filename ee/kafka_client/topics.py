7ab30a836c11948e81619afd3dfbb2f08ef06cd8
James Greenhill <fuziontech@gmail.com>
2020-10-21 14:06:45 -0700

    Remove Omni-Person logic for ee (#1972)
    * Remove Omni-Person logic for ee
    
    * remove more omni person references

--- ee/kafka_client/topics.py
+++ ee/kafka_client/topics.py
@@ -2,6 +2,5 @@ KAFKA_EVENTS = "clickhouse_events"
 KAFKA_ELEMENTS = "clickhouse_elements"
 KAFKA_PERSON = "clickhouse_person"
 KAFKA_PERSON_UNIQUE_ID = "clickhouse_person_unique_id"
-KAFKA_OMNI_PERSON = "clickhouse_omni_person"
 
 KAFKA_EVENTS_WAL = "events_write_ahead_log"
