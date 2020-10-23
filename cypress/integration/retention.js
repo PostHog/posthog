217ffeb7c1c6b07aaa32f587c5e7ecd9bfe29cbf
Marius Andra <marius.andra@gmail.com>
2020-10-23 12:03:01 +0200

    try to fix flakyness by clicking the dropdown item even if it's not visible

--- cypress/integration/retention.js
+++ cypress/integration/retention.js
@@ -7,9 +7,9 @@ describe('Retention', () => {
     it('Apply 1 overall filter', () => {
         cy.get('[data-attr=new-prop-filter-insight-retention]').click()
         cy.get('[data-attr=property-filter-dropdown]').click()
-        cy.get('[data-attr=prop-filter-person-0]').click()
+        cy.get('[data-attr=prop-filter-person-0]').click({ force: true })
         cy.get('[data-attr=prop-val]').click()
-        cy.get('[data-attr=prop-val-0]').click()
+        cy.get('[data-attr=prop-val-0]').click({ force: true })
         cy.get('[data-attr=retention-table').should('exist')
     })
 })
