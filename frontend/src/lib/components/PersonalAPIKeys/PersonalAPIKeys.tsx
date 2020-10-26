cacad460a5550000a588dda2f4570dc59e8b8a4a
Michael Matloka <dev@twixes.com>
2020-10-26 11:02:45 +0100

    Add Never support to humanFriendlyDetailedTime

--- frontend/src/lib/components/PersonalAPIKeys/PersonalAPIKeys.tsx
+++ frontend/src/lib/components/PersonalAPIKeys/PersonalAPIKeys.tsx
@@ -110,7 +110,7 @@ function PersonalAPIKeysTable(): JSX.Element {
             title: 'Last Used',
             dataIndex: 'last_used_at',
             key: 'lastUsedAt',
-            render: (lastUsedAt: string) => (lastUsedAt ? humanFriendlyDetailedTime(lastUsedAt) : 'never'),
+            render: (lastUsedAt: string | null) => humanFriendlyDetailedTime(lastUsedAt),
         },
         {
             title: 'Created',
