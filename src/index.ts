96fba0178308c660f1ae7013631d074f2c64f880
Marius Andra <marius.andra@gmail.com>
2020-10-27 15:02:48 +0100

    [plugin-server] use celery instead

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,24 @@
-const grpc = require('grpc')
-import { struct } from 'pb-util'
-import { setupPlugins, runPlugins } from './plugins'
-const protoLoader = require('@grpc/proto-loader')
-require('google-protobuf/google/protobuf/struct_pb')
-const packageDefinition = protoLoader.loadSync('../protos/posthog/grpc/plugins.proto', {
-    keepCase: true,
-    longs: Number,
-    enums: String,
-    objects: true,
-    defaults: true,
-    oneofs: true,
-})
-const { PluginService } = grpc.loadPackageDefinition(packageDefinition)
+const celery = require('celery-node')
 
-const server = new grpc.Server()
-server.addService(PluginService.service, {
-    OnCapture: async (call, callback) => {
-        const { event } = call.request
-        const processedEvent = await runPlugins({ ...event, properties: struct.decode(event.properties) })
-        const eventResponse = { ...processedEvent, properties: struct.encode(processedEvent.properties) }
-        callback(null, { event: eventResponse })
-    },
-})
+const worker = celery.createWorker('redis://localhost/', 'redis://localhost/', 'posthog-plugins')
+const client = celery.createClient('redis://localhost/', 'redis://localhost/', 'celery')
 
-setupPlugins()
+const processEvent = client.createTask('process_event')
 
-server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure())
-console.log('Server running at http://127.0.0.1:50051')
-server.start()
+worker.register(
+    'process_event_with_plugins',
+    (
+        distinct_id: string,
+        ip: string,
+        site_url: string,
+        data: Record<string, any>,
+        team_id: number,
+        now: string,
+        sent_at?: string
+    ) => {
+        console.log(data)
+
+        client.sendTask('process_event', [], { distinct_id, ip, site_url, data, team_id, now, sent_at })
+    }
+)
+worker.start()
