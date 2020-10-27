96eb7f984646f4dee37ec381770f9323bb9213ae
Tim Glaser <tim@glsr.nl>
2020-10-27 17:51:19 +0100

    Fix cohorts in clickhouse

--- ee/clickhouse/models/property.py
+++ ee/clickhouse/models/property.py
@@ -22,19 +22,15 @@ def parse_prop_clauses(
             person_id_query, cohort_filter_params = format_filter_query(cohort)
             params = {**params, **cohort_filter_params}
             final += "AND distinct_id IN ({clause}) ".format(clause=person_id_query)
-
         elif prop.type == "person":
-
-            if json_extract:
-                filter_query, filter_params = prop_filter_json_extract(prop, idx, "person", "person_properties")
-                final += " {filter_query} AND team_id = %(team_id)s".format(filter_query=filter_query)
-                params.update(filter_params)
-            else:
-                filter_query, filter_params = person_prop_filter_kv(prop, idx)
-                final += " {filter_query}".format(filter_query=filter_query)
-                params.update(filter_params)
+            filter_query, filter_params = prop_filter_json_extract(prop, idx, "{}person".format(prepend))
+            final += " AND distinct_id IN ({filter_query})".format(
+                filter_query=GET_DISTINCT_IDS_BY_PROPERTY_SQL.format(
+                    filters=filter_query, negation="NOT " if prop.operator and "not" in prop.operator else ""
+                )
+            )
+            params.update(filter_params)
         else:
-
             if json_extract:
                 filter_query, filter_params = prop_filter_json_extract(prop, idx, prepend)
                 final += " {filter_query} AND team_id = %(team_id)s".format(filter_query=filter_query)
@@ -142,27 +138,6 @@ def prop_filter_json_extract(
                 ),
                 params,
             )
-
-
-def person_prop_filter_kv(prop: Property, idx: int, prepend: str = "person") -> Tuple[str, Dict[str, Any]]:
-
-    arg = "v{}_{}".format(prepend, idx)
-    operator_clause, value = get_operator(prop, arg)
-
-    params = {"k{}_{}".format(prepend, idx): prop.key, arg: value}
-
-    key_statement = "(ep.key = %(k{prepend}_{idx})s)".format(idx=idx, prepend=prepend)
-    filter = "{key_statement} {and_statement} {operator_clause}".format(
-        key_statement=key_statement, and_statement="AND" if operator_clause else "", operator_clause=operator_clause,
-    )
-    clause = GET_DISTINCT_IDS_BY_PROPERTY_SQL.format(
-        key_statement=key_statement,
-        filters=filter,
-        negation="NOT " if prop.operator and "not" in prop.operator else "",
-    )
-    filter_query = "AND distinct_id IN ({clause}) ".format(clause=clause)
-
-    return filter_query, params
 
 
 def prop_filter_kv(key: str, prop: Property, team: Team, idx: int, prepend: str = "") -> Tuple[str, Dict[str, Any]]:
