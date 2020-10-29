3ad92ff9a49b8a2ff58787182ae0ccc42d04161a
Tim Glaser <tim.glaser@hiberly.com>
2020-10-29 11:25:02 +0000

    Clickhouse improve funnel speed (#2100)

--- ee/clickhouse/sql/funnels/funnel.py
+++ ee/clickhouse/sql/funnels/funnel.py
@@ -1,17 +1,16 @@
 FUNNEL_SQL = """
     SELECT 
-        person_id as id,
+        pid.person_id as id,
         windowFunnel(6048000000000000)(toUInt64(toUnixTimestamp64Micro(timestamp)),
             {steps}
         )
-    FROM (
-        SELECT
-            events.*, pid.person_id
-        FROM
+    FROM 
             events
-        JOIN (SELECT person_id, distinct_id FROM person_distinct_id WHERE team_id = %(team_id)s) as pid
-        ON pid.distinct_id = events.distinct_id
-        WHERE team_id = %(team_id)s {filters} {parsed_date_from} {parsed_date_to}
-    )
-    GROUP BY person_id
+    JOIN (
+        SELECT person_id, distinct_id FROM person_distinct_id WHERE team_id = %(team_id)s
+    ) as pid
+    ON pid.distinct_id = events.distinct_id
+    WHERE
+        team_id = %(team_id)s {filters} {parsed_date_from} {parsed_date_to}
+    GROUP BY pid.person_id
 """
