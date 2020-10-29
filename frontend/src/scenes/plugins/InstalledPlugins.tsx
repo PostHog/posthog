8c68d7334b6c93acdd98ef40d96737d1450e89d7
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-10-29 11:00:10 +0000

    remove button when installation is not available

--- frontend/src/scenes/plugins/InstalledPlugins.tsx
+++ frontend/src/scenes/plugins/InstalledPlugins.tsx
@@ -4,19 +4,23 @@ import { useActions, useValues } from 'kea'
 import { pluginsLogic } from 'scenes/plugins/pluginsLogic'
 import { PlusOutlined } from '@ant-design/icons'
 import { PluginCard, PluginLoading } from './PluginCard'
+import { userLogic } from 'scenes/userLogic'
 
 export function InstalledPlugins(): JSX.Element {
     const { installedPlugins, loading } = useValues(pluginsLogic)
     const { setPluginTab } = useActions(pluginsLogic)
+    const { user } = useValues(userLogic)
 
     return (
         <div>
             <h2 className="oh-page-subtitle">Installed {!loading && <>({installedPlugins.length})</>}</h2>
-            <div className="text-right oh-spaced-bottom">
-                <Button type="primary" icon={<PlusOutlined />} onClick={() => setPluginTab('available')}>
-                    Install new plugin
-                </Button>
-            </div>
+            {user?.plugin_access?.install && (
+                <div className="text-right oh-spaced-bottom">
+                    <Button type="primary" icon={<PlusOutlined />} onClick={() => setPluginTab('available')}>
+                        Install new plugin
+                    </Button>
+                </div>
+            )}
             <Row gutter={16}>
                 {!loading && (
                     <>
