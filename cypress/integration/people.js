b9892fc9ed5b9364c5e260bcb2ba27e60be603bf
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-10-31 10:26:36 +0000

    fix cypress

--- cypress/integration/people.js
+++ cypress/integration/people.js
@@ -7,11 +7,6 @@ describe('People', () => {
         cy.get('h1').should('contain', 'Persons')
     })
 
-    it('Go to new cohort from people screen', () => {
-        cy.get('[data-attr=create-cohort]').click()
-        cy.get('span').should('contain', 'New Cohort')
-    })
-
     it('All tabs work', () => {
         cy.get('.form-control').type('has:email').type('{enter}').should('have.value', 'has:email')
         cy.wait(200)
@@ -24,8 +19,9 @@ describe('People', () => {
 
     it('All people route works', () => {
         cy.get('[data-attr=menu-item-people-cohorts]').click()
-        cy.get('[data-attr=menu-item-people-persons]').click()
+        cy.get('h1').should('contain', 'Cohorts')
 
+        cy.get('[data-attr=menu-item-people-persons]').click()
         cy.get('h1').should('contain', 'Persons')
     })
 })
