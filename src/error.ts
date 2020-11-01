22fc3ecf46da39dd0db7e800f75719c7cba949be
Marius Andra <marius.andra@gmail.com>
2020-11-01 22:10:38 +0100

    save errors in the database

---
+++ src/error.ts
@@ -0,0 +1,36 @@
+import { Plugin, PluginConfig, PluginsServer } from './types'
+
+export async function processError(
+    server: PluginsServer,
+    plugin: Plugin,
+    teamPlugin: PluginConfig | null,
+    error: Error | string
+) {
+    console.error(error)
+
+    const errorJson =
+        typeof error === 'string'
+            ? {
+                  message: error,
+                  time: new Date().toISOString(),
+              }
+            : {
+                  message: error.message,
+                  time: new Date().toISOString(),
+                  name: error.name,
+                  stack: error.stack,
+              }
+    if (teamPlugin) {
+        await server.db.query('UPDATE posthog_pluginconfig SET error = $1 WHERE id = $2', [errorJson, teamPlugin.id])
+    } else {
+        await server.db.query('UPDATE posthog_plugin SET error = $1 WHERE id = $2', [errorJson, plugin.id])
+    }
+}
+
+export async function clearError(server: PluginsServer, plugin: Plugin, teamPlugin: PluginConfig | null) {
+    if (teamPlugin) {
+        await server.db.query('UPDATE posthog_pluginconfig SET error = NULL WHERE id = $1', [teamPlugin.id])
+    } else {
+        await server.db.query('UPDATE posthog_plugin SET error = NULL WHERE id = $1', [plugin.id])
+    }
+}
