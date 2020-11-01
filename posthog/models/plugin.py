47a02651ddda8ba665534ca0e8d7b5d644b4ecb4
Marius Andra <marius.andra@gmail.com>
2020-11-01 21:50:39 +0100

    add hint for errors

--- posthog/models/plugin.py
+++ posthog/models/plugin.py
@@ -13,13 +13,16 @@ class Plugin(models.Model):
     name: models.CharField = models.CharField(max_length=200, null=True, blank=True)
     description: models.TextField = models.TextField(null=True, blank=True)
     url: models.CharField = FileURLField(max_length=800, null=True, blank=True)
-    # Describe the fields we must ask in the interface, answers stored in PluginConfig.config
-    # config_schema = { [fieldKey]: { name: 'api key', type: 'string', default: '', required: true }  }
+    # Describe the fields to ask in the interface; store answers in PluginConfig->config
+    # - config_schema = { [fieldKey]: { name: 'api key', type: 'string', default: '', required: true }  }
     config_schema: JSONField = JSONField(default=dict)
     tag: models.CharField = models.CharField(max_length=200, null=True, blank=True)
     archive: models.BinaryField = models.BinaryField(blank=True, null=True)
     from_json: models.BooleanField = models.BooleanField(default=False)
     from_web: models.BooleanField = models.BooleanField(default=False)
+    # Error installing or configuring this plugin
+    # - e.g: "could not find plugin.json" / "syntax error in index.js")
+    # - error = { message: "Could not find plugin.json", time: "iso-string", ...meta }
     error: JSONField = JSONField(default=None, null=True)
 
 
@@ -29,4 +32,7 @@ class PluginConfig(models.Model):
     enabled: models.BooleanField = models.BooleanField(default=False)
     order: models.IntegerField = models.IntegerField(null=True, blank=True)
     config: JSONField = JSONField(default=dict)
+    # Error when running this plugin on an event
+    # - e.g: "undefined is not a function on index.js line 23"
+    # - error = { message: "Exception in processEvent()", time: "iso-string", ...meta }
     error: JSONField = JSONField(default=None, null=True)
