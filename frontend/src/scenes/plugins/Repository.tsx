4888e6e2bbdd1fc4510d3b10d477eb4226230b03
Marius Andra <marius.andra@gmail.com>
2020-11-02 13:55:10 +0100

    fix loading flickering on installing plugins

--- frontend/src/scenes/plugins/Repository.tsx
+++ frontend/src/scenes/plugins/Repository.tsx
@@ -5,13 +5,13 @@ import { pluginsLogic } from 'scenes/plugins/pluginsLogic'
 import { PluginCard, PluginLoading } from './PluginCard'
 
 export function Repository(): JSX.Element {
-    const { loading, repositoryLoading, uninstalledPlugins } = useValues(pluginsLogic)
+    const { repositoryLoading, uninstalledPlugins } = useValues(pluginsLogic)
 
     return (
         <div>
             <h2 className="subtitle">Available</h2>
             <Row gutter={16}>
-                {((!loading && !repositoryLoading) || uninstalledPlugins.length > 0) && (
+                {(!repositoryLoading || uninstalledPlugins.length > 0) && (
                     <>
                         {uninstalledPlugins.map((plugin) => {
                             return (
@@ -31,7 +31,11 @@ export function Repository(): JSX.Element {
                     </>
                 )}
             </Row>
-            {(loading || repositoryLoading) && uninstalledPlugins.length === 0 && <PluginLoading />}
+            {repositoryLoading && uninstalledPlugins.length === 0 && (
+                <Row gutter={16}>
+                    <PluginLoading />
+                </Row>
+            )}
         </div>
     )
 }
