6d92d4b9e01716797356f09ec1079214332999f6
Eric <eeoneric@gmail.com>
2020-11-03 11:39:28 +0100

    filter event matcher

--- ee/tasks/webhooks_ee.py
+++ ee/tasks/webhooks_ee.py
@@ -1,11 +1,18 @@
-from typing import Any, Dict
+import re
+from typing import Any, Dict, List
 
 import requests
 from celery import Task
 from django.conf import settings
+from django.db.models.expressions import Exists, OuterRef
+from django.db.models.query_utils import Q
+from django.forms.models import model_to_dict
 
 from posthog.celery import app
+from posthog.constants import AUTOCAPTURE_EVENT
 from posthog.models import Action, Event, Team
+from posthog.models.action_step import ActionStep
+from posthog.models.filter import Filter
 from posthog.tasks.webhooks import determine_webhook_type, get_formatted_message
 
 
@@ -37,3 +44,41 @@ def post_event_to_webhook_ee(self: Task, event: Dict[str, Any], team_id: int, si
                 requests.post(team.slack_incoming_webhook, verify=False, json=message)
     except:
         self.retry(countdown=2 ** self.request.retries)
+
+
+def determine_matching_actions(event: Event) -> List:
+    actions = Action.objects.filter(team=event.team, post_to_slack=True).all()
+
+    result = []
+
+    for action in actions:
+        steps = action.steps.all()
+        for index, step in enumerate(steps):
+            # filter element
+            if step.event == AUTOCAPTURE_EVENT:
+                pass
+            # filter event
+            else:
+                if not matches_event(step, event):
+                    break
+
+            if step.properties:
+                pass
+        result.append(action)
+
+    return []
+
+
+def matches_event(step: ActionStep, event: Event) -> bool:
+    if event.event != step.event:
+        return False
+    if step.url:
+        current_url = event.properties.get("$current_url", "")
+        if step.url_matching == ActionStep.EXACT and current_url != step.url:
+            return False
+        elif step.url_matching == ActionStep.REGEX and re.search(step.url, current_url):
+            return False
+        elif step.url not in current_url:
+            return False
+
+    return True
