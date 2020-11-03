916fd3928e9f1217502f513483fe15f63ac61301
Tim Glaser <tim.glaser@hiberly.com>
2020-11-03 13:32:00 +0000

    Ignore sentry error for now (#2174)

--- posthog/tasks/process_event.py
+++ posthog/tasks/process_event.py
@@ -7,7 +7,6 @@ from celery import shared_task
 from dateutil import parser
 from dateutil.relativedelta import relativedelta
 from django.db import IntegrityError
-from sentry_sdk import capture_exception
 
 from posthog.models import Element, Event, Person, SessionRecordingEvent, Team
 
@@ -220,7 +219,7 @@ def handle_timestamp(data: dict, now: str, sent_at: Optional[str]) -> datetime.d
                 # otherwise we can't get a diff to add to now
                 return parser.isoparse(now) + (parser.isoparse(data["timestamp"]) - parser.isoparse(sent_at))
             except TypeError as e:
-                capture_exception(e)
+                pass
         return parser.isoparse(data["timestamp"])
     now_datetime = parser.parse(now)
     if data.get("offset"):
