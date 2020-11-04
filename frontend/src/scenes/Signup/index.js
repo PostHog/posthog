88779ff04bc26a12cbc777d3c0b0b4672344cf2a
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-11-04 16:45:21 +0000

    change ordering of fields in signup

--- frontend/src/scenes/Signup/index.js
+++ frontend/src/scenes/Signup/index.js
@@ -81,30 +81,6 @@ function Signup() {
                 >
                     <form onSubmit={handleSubmit}>
                         <div className="input-set">
-                            <label htmlFor="signupFirstName">First Name</label>
-                            <Input
-                                placeholder="Jane"
-                                autoFocus
-                                value={formState.firstName.value}
-                                onChange={(e) => updateForm('firstName', e.target)}
-                                required
-                                disabled={accountLoading}
-                                id="signupFirstName"
-                            />
-                        </div>
-
-                        <div className="input-set">
-                            <label htmlFor="signupCompanyName">Company or Project</label>
-                            <Input
-                                placeholder="Hogflix Movies"
-                                value={formState.companyName.value}
-                                onChange={(e) => updateForm('companyName', e.target)}
-                                disabled={accountLoading}
-                                id="signupCompanyName"
-                            />
-                        </div>
-
-                        <div className="input-set">
                             <label htmlFor="signupEmail">Email</label>
                             <Input
                                 placeholder="jane@hogflix.io"
@@ -133,6 +109,30 @@ function Signup() {
                             {!formState.password.valid && (
                                 <span className="caption">Your password must have at least 8 characters.</span>
                             )}
+                        </div>
+
+                        <div className="input-set">
+                            <label htmlFor="signupFirstName">First Name</label>
+                            <Input
+                                placeholder="Jane"
+                                autoFocus
+                                value={formState.firstName.value}
+                                onChange={(e) => updateForm('firstName', e.target)}
+                                required
+                                disabled={accountLoading}
+                                id="signupFirstName"
+                            />
+                        </div>
+
+                        <div className="input-set">
+                            <label htmlFor="signupCompanyName">Company or Project</label>
+                            <Input
+                                placeholder="Hogflix Movies"
+                                value={formState.companyName.value}
+                                onChange={(e) => updateForm('companyName', e.target)}
+                                disabled={accountLoading}
+                                id="signupCompanyName"
+                            />
                         </div>
 
                         <div>
