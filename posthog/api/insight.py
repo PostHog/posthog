b54caa72f38b1c3854f1f961f73d0f6b12e903e1
Eric <eeoneric@gmail.com>
2020-11-04 11:23:53 +0100

    fix params

--- posthog/api/insight.py
+++ posthog/api/insight.py
@@ -52,8 +52,9 @@ class InsightSerializer(serializers.ModelSerializer):
             dashboard_item = DashboardItem.objects.create(team=team, created_by=request.user, **validated_data)
             return dashboard_item
         elif validated_data["dashboard"].team == team:
+            created_by = validated_data.pop('created_by', request.user)
             dashboard_item = DashboardItem.objects.create(
-                team=team, last_refresh=now(), created_by=request.user, **validated_data
+                team=team, last_refresh=now(), created_by=created_by, **validated_data
             )
             return dashboard_item
         else:
