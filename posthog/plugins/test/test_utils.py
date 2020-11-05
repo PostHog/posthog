3236e13532214e74c6ccee1472c4e78c311ec8f7
Marius Andra <marius.andra@gmail.com>
2020-11-05 17:10:35 +0100

    fix

--- posthog/plugins/test/test_utils.py
+++ posthog/plugins/test/test_utils.py
@@ -148,7 +148,7 @@ class TestPluginsUtils(BaseTest):
         plugin_json_tgz = get_json_from_archive(base64.b64decode(HELLO_WORLD_PLUGIN_NPM_TGZ[1]), "plugin.json")
         self.assertEqual(plugin_json_tgz["name"], "helloworldplugin")
         self.assertEqual(plugin_json_tgz["url"], "https://github.com/PostHog/helloworldplugin")
-        self.assertEqual(plugin_json_tgz["description"], "Greet the World and Foo a Bar")
+        self.assertEqual(plugin_json_tgz["description"], "Greet the World and Foo a Bar, JS edition")
 
         plugin_json_tgz = get_json_from_archive(base64.b64decode(HELLO_WORLD_PLUGIN_GITHUB_TGZ[1]), "plugin.json")
         self.assertEqual(plugin_json_tgz["name"], "helloworldplugin")
