8343e66fd19745b6dddba5d2a71f4b136d33c5f3
Tim Glaser <tim@glsr.nl>
2020-11-05 13:16:38 +0100

    Revert to snippet

--- frontend/src/loadPostHogJS.tsx
+++ frontend/src/loadPostHogJS.tsx
@@ -2,10 +2,6 @@ import posthog from 'posthog-js'
 import * as Sentry from '@sentry/browser'
 
 export function loadPostHogJS(): void {
-    if (window.JS_POSTHOG_API_KEY) {
-        posthog.init(window.JS_POSTHOG_API_KEY, { api_host: window.JS_POSTHOG_HOST })
-    }
-
     if ((window as any).SENTRY_DSN) {
         Sentry.init({
             dsn: (window as any).SENTRY_DSN,
