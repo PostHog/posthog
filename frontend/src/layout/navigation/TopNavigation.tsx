3a2a5c84665473d4b0d6dcba6d7a3fa91a4ed154
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-11-06 12:58:57 +0000

    right dropdown

--- frontend/src/layout/navigation/TopNavigation.tsx
+++ frontend/src/layout/navigation/TopNavigation.tsx
@@ -2,16 +2,55 @@ import React from 'react'
 import './Navigation.scss'
 import { useActions, useValues } from 'kea'
 import { navigationLogic } from './navigationLogic'
-import { IconMenu } from './icons'
+import { IconBuilding, IconMenu } from './icons'
 import { userLogic } from 'scenes/userLogic'
 import { Badge } from 'lib/components/Badge'
 import { ChangelogModal } from '~/layout/ChangelogModal'
 import { router } from 'kea-router'
+import { Button, Dropdown } from 'antd'
 
 export function TopNavigation(): JSX.Element {
     const { setMenuCollapsed, setChangelogModalOpen } = useActions(navigationLogic)
     const { menuCollapsed, systemStatus, updateAvailable, changelogModalOpen } = useValues(navigationLogic)
     const { user } = useValues(userLogic)
+    const { logout } = useActions(userLogic)
+    const { push } = router.actions
+
+    const whoAmIDropdown = (
+        <div className="navigation-top-dropdown whoami-dropdown">
+            <div className="whoami" style={{ paddingRight: 16, paddingLeft: 16 }}>
+                <div className="pp">{user?.name[0].toUpperCase()}</div>
+                <div className="details">
+                    <span>{user?.email}</span>
+                    <span>{user?.organization.name}</span>
+                </div>
+            </div>
+            <div className="text-center">
+                <div>
+                    <Button className="mt" onClick={() => push('/organization/settings')}>
+                        Organization settings
+                    </Button>
+                </div>
+                <div className="mt-05">
+                    <a href="#" onClick={() => push('/me/settings')}>
+                        My account
+                    </a>
+                </div>
+            </div>
+            <div className="divider mt-05" />
+            <div className="organizations">
+                <a href="#">
+                    <IconBuilding /> Hogflix, Inc.
+                </a>
+            </div>
+            <div className="divider mb-05" />
+            <div className="text-center">
+                <a href="#" onClick={logout}>
+                    Log out
+                </a>
+            </div>
+        </div>
+    )
 
     return (
         <>
@@ -25,7 +64,7 @@ export function TopNavigation(): JSX.Element {
                         {!user?.is_multi_tenancy && (
                             <Badge
                                 type={systemStatus ? 'success' : 'danger'}
-                                onClick={() => router.actions.push('/instance/status')}
+                                onClick={() => push('/instance/status')}
                                 tooltip={systemStatus ? 'All systems operational' : 'Potential system issue'}
                                 className="mr"
                             />
@@ -40,11 +79,15 @@ export function TopNavigation(): JSX.Element {
                 </div>
                 <div className="middle">Project chooser</div>
                 <div>
-                    <div className="pp">{user?.name[0].toUpperCase()}</div>
-                    <div className="whoami hide-lte-lg">
-                        <span>{user?.name}</span>
-                        <span>{user?.organization.name}</span>
-                    </div>
+                    <Dropdown overlay={whoAmIDropdown} visible={true}>
+                        <div className="whoami">
+                            <div className="pp">{user?.name[0].toUpperCase()}</div>
+                            <div className="details hide-lte-lg">
+                                <span>{user?.name}</span>
+                                <span>{user?.organization.name}</span>
+                            </div>
+                        </div>
+                    </Dropdown>
                 </div>
             </div>
             {changelogModalOpen && <ChangelogModal onDismiss={() => setChangelogModalOpen(false)} />}
