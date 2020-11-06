63c35d20bfa2e0b3f40aec0029b23acfba05d9e2
Tim Glaser <tim.glaser@hiberly.com>
2020-11-06 08:50:53 +0000

    Setup sentry integration (#1833)
    * Setup sentry integration
    
    * Oops
    
    * Add Sentry as dependency
    
    * Fix posthog-js
    
    * Fix
    
    * Fix sentry
    
    * Move everything to posthog-js import
    
    * Extract into component
    
    * Revert "Move everything to posthog-js import"
    
    This reverts commit 9b34869aa3c8bd26ae9256e6dfaf116a81d30907.
    
    * Revert to snippet
    
    * Revert "Revert to snippet"
    
    This reverts commit 8343e66fd19745b6dddba5d2a71f4b136d33c5f3.
    
    * Revert "Revert "Move everything to posthog-js import""
    
    This reverts commit 91010274dfa3ec1f9adda1a65f846e09b6fa5ae5.
    
    * Don't capture if opted out
    
    * Fix opt out
    
    * Fix globals

---
+++ frontend/src/loadPostHogJS.tsx
@@ -0,0 +1,26 @@
+import posthog from 'posthog-js'
+import * as Sentry from '@sentry/browser'
+
+export function loadPostHogJS(): void {
+    if (window.JS_POSTHOG_API_KEY) {
+        posthog.init(window.JS_POSTHOG_API_KEY, { api_host: window.JS_POSTHOG_HOST })
+        // Make sure we have access to the object in window for debugging
+        window.posthog = posthog
+    } else {
+        posthog.init('fake token', {
+            autocapture: false,
+            loaded: function (posthog) {
+                posthog.opt_out_capturing()
+            },
+        })
+    }
+
+    if ((window as any).SENTRY_DSN) {
+        Sentry.init({
+            dsn: (window as any).SENTRY_DSN,
+            ...(window.location.host.indexOf('app.posthog.com') > -1 && {
+                integrations: [new posthog.SentryIntegration(posthog, 'posthog', 1899813)],
+            }),
+        })
+    }
+}
