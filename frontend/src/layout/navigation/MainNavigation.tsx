f4a1a6155a2acefe8f11bbaff77b0e8adacdec47
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-11-08 18:41:56 +0000

    actual nav items

--- frontend/src/layout/navigation/MainNavigation.tsx
+++ frontend/src/layout/navigation/MainNavigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Layout } from 'antd'
-import { FundOutlined } from '@ant-design/icons'
+import { FundOutlined, ProjectFilled, ApiFilled, ClockCircleFilled } from '@ant-design/icons'
 import { useActions, useValues } from 'kea'
 import { Link } from 'lib/components/Link'
 import { sceneLogic } from 'scenes/sceneLogic'
@@ -10,7 +10,16 @@ import lgLogo from 'public/posthog-logo-white.svg'
 import smLogo from 'public/icon-white.svg'
 import { hot } from 'react-hot-loader/root'
 import './Navigation.scss'
-import { IconDashboard, IconPerson } from './icons'
+import {
+    IconCohorts,
+    IconDashboard,
+    IconEvents,
+    IconFeatureFlags,
+    IconFunnel,
+    IconInsights,
+    IconPerson,
+    IconToolbar,
+} from './icons'
 import { navigationLogic } from './navigationLogic'
 
 // to show the right page in the sidebar
@@ -18,7 +27,6 @@ const sceneOverride = {
     action: 'actions',
     person: 'persons',
     dashboard: 'dashboards',
-    featureFlags: 'experiments',
 }
 
 interface MenuItemProps {
@@ -77,12 +85,33 @@ function _MainNavigation(): JSX.Element {
                     <MenuItem title="Dashboards" icon={<IconDashboard />} identifier="dashboards" to="/dashboard" />
                     <MenuItem
                         title="Insights"
-                        icon={<FundOutlined />}
+                        icon={<IconInsights />}
                         identifier="insights"
                         to="/insights?insight=TRENDS"
                     />
                     <div className="divider" />
+                    <MenuItem title="Events" icon={<IconEvents />} identifier="events" to="/events" />
+                    <MenuItem title="Sessions" icon={<ClockCircleFilled />} identifier="sessions" to="/sessions" />
+                    <div className="divider" />
                     <MenuItem title="Persons" icon={<IconPerson />} identifier="persons" to="/persons" />
+                    <MenuItem title="Cohorts" icon={<IconCohorts />} identifier="cohorts" to="/cohorts" />
+                    <div className="divider" />
+                    <MenuItem
+                        title="Feat Flags"
+                        icon={<IconFeatureFlags />}
+                        identifier="featureFlags"
+                        to="/feature_flags"
+                    />
+                    <div className="divider" />
+                    <MenuItem title="Plugins" icon={<ApiFilled />} identifier="plugins" to="/project/plugins" />
+                    <MenuItem
+                        title="Project"
+                        icon={<ProjectFilled />}
+                        identifier="projectSettings"
+                        to="/project/settings"
+                    />
+                    <div className="divider" />
+                    <MenuItem title="Toolbar" icon={<IconToolbar />} identifier="toolbar" to="/toolbar" />
                 </div>
             </Layout.Sider>
         </>
