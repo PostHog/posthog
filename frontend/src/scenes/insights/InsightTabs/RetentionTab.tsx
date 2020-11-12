8758869ed7406714544527e987b145ddcf0ab3b8
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-11-12 11:32:08 +0000

    UX improvements & copy updates

--- frontend/src/scenes/insights/InsightTabs/RetentionTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/RetentionTab.tsx
@@ -44,41 +44,42 @@ export function RetentionTab(): JSX.Element {
 
     return (
         <div data-attr="retention-tab">
-            <h4 className="secondary">Retention Type</h4>
-            <div>
-                <Select
-                    value={retentionOptions[retentionType]}
-                    onChange={(value): void => setFilters({ retentionType: value })}
-                    dropdownMatchSelectWidth={false}
-                >
-                    {Object.entries(retentionOptions).map(([key, value]) => (
-                        <Select.Option key={key} value={key}>
-                            {value}
-                            <Tooltip placement="right" title={retentionOptionDescriptions[key]}>
-                                <InfoCircleOutlined className="info-indicator" />
-                            </Tooltip>
-                        </Select.Option>
-                    ))}
-                </Select>
-            </div>
-            <hr />
             <h4 className="secondary">
-                {retentionType === 'retention_first_time' ? 'Cohortizing Event' : 'Target Event'}
-                {retentionType === 'retention_first_time' && (
-                    <Tooltip
-                        key="2"
-                        placement="right"
-                        title="Choose the event that will determine which users are considered in the cohort at the start period"
-                    >
-                        <InfoCircleOutlined className="info-indicator" />
-                    </Tooltip>
-                )}
+                Cohortizing Event
+                <Tooltip
+                    key="2"
+                    placement="right"
+                    title={`Event that determines which users are considered to form each cohort (i.e. performed event in ${
+                        dateOptions[filters.period]
+                    } 0)`}
+                >
+                    <InfoCircleOutlined className="info-indicator" />
+                </Tooltip>
             </h4>
-
-            <Button ref={node} data-attr="retention-action" onClick={(): void => setOpen(!open)}>
+            <Button
+                ref={node}
+                data-attr="retention-action"
+                onClick={(): void => setOpen(!open)}
+                style={{ marginRight: 8 }}
+            >
                 {startEntity?.name || 'Select action'}
                 <DownOutlined className="text-muted" style={{ marginRight: '-6px' }} />
             </Button>
+            <Select
+                value={retentionOptions[retentionType]}
+                onChange={(value): void => setFilters({ retentionType: value })}
+                dropdownMatchSelectWidth={false}
+                style={{ marginTop: 8 }}
+            >
+                {Object.entries(retentionOptions).map(([key, value]) => (
+                    <Select.Option key={key} value={key}>
+                        {value}
+                        <Tooltip placement="right" title={retentionOptionDescriptions[key]}>
+                            <InfoCircleOutlined className="info-indicator" />
+                        </Tooltip>
+                    </Select.Option>
+                ))}
+            </Select>
             {open && (
                 <ActionFilterDropdown
                     logic={entityLogic}
@@ -90,42 +91,38 @@ export function RetentionTab(): JSX.Element {
                     }}
                 />
             )}
-            {retentionType === 'retention_first_time' && (
-                <>
-                    <h4 style={{ marginTop: '0.5rem' }} className="secondary">
-                        {'Retained event'}
-                        {retentionType === 'retention_first_time' && (
-                            <Tooltip
-                                key="3"
-                                placement="right"
-                                title="Choose the event that will determine if users in a cohort come back"
-                            >
-                                <InfoCircleOutlined className="info-indicator" />
-                            </Tooltip>
-                        )}
-                    </h4>
+            <hr />
+            <h4 style={{ marginTop: '0.5rem' }} className="secondary">
+                Retaining event
+                <Tooltip
+                    key="3"
+                    placement="right"
+                    title="Event that determines if each user came back on each period (i.e. if they were retained)"
+                >
+                    <InfoCircleOutlined className="info-indicator" />
+                </Tooltip>
+            </h4>
 
-                    <Button
-                        ref={returningNode}
-                        data-attr="retention-returning-action"
-                        onClick={(): void => setReturningOpen(!returningOpen)}
-                    >
-                        {returningEntity?.name || 'Select action'}
-                        <DownOutlined className="text-muted" style={{ marginRight: '-6px' }} />
-                    </Button>
-                    {returningOpen && (
-                        <ActionFilterDropdown
-                            logic={entityLogicReturning}
-                            onClickOutside={(e): void => {
-                                if (node.current.contains(e.target)) {
-                                    return
-                                }
-                                setReturningOpen(false)
-                            }}
-                        />
-                    )}
-                </>
+            <Button
+                ref={returningNode}
+                data-attr="retention-returning-action"
+                onClick={(): void => setReturningOpen(!returningOpen)}
+            >
+                {returningEntity?.name || 'Select action'}
+                <DownOutlined className="text-muted" style={{ marginRight: '-6px' }} />
+            </Button>
+            {returningOpen && (
+                <ActionFilterDropdown
+                    logic={entityLogicReturning}
+                    onClickOutside={(e): void => {
+                        if (node.current.contains(e.target)) {
+                            return
+                        }
+                        setReturningOpen(false)
+                    }}
+                />
             )}
+
             <hr />
             <h4 className="secondary">Filters</h4>
             <PropertyFilters pageKey="insight-retention" />
