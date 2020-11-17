d99590c7f75c8c7cf6c72e8c7846f39371105cfa
Michael Matloka <dev@twixes.com>
2020-11-17 14:00:42 +0100

    Remove console.log

--- frontend/src/scenes/organization/Create/index.tsx
+++ frontend/src/scenes/organization/Create/index.tsx
@@ -2,6 +2,5 @@ import React from 'react'
 import { CreateOrganizationModal } from '../CreateOrganizationModal'
 
 export function Create(): JSX.Element {
-    console.log('xx')
     return <CreateOrganizationModal isVisible={true} />
 }
