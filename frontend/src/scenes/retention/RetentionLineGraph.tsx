e6af199cc4c9d583bbd0e72f10517ad4fe39e428
Eric <eeoneric@gmail.com>
2020-11-18 10:31:08 -0500

    click on datapoint works

--- frontend/src/scenes/retention/RetentionLineGraph.tsx
+++ frontend/src/scenes/retention/RetentionLineGraph.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
-import { retentionTableLogic } from './retentionTableLogic'
+import { dateOptions, retentionTableLogic } from './retentionTableLogic'
 import { LineGraph } from '../insights/LineGraph'
-import { useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import { Loading } from '../../lib/utils'
 import { router } from 'kea-router'
 import { LineGraphEmptyState } from '../insights/EmptyStates'
+import { Modal, Button } from 'antd'
+import { PeopleTable } from 'scenes/users/PeopleTable'
 
 interface RetentionLineGraphProps {
     dashboardItemId?: number | null
@@ -18,25 +20,72 @@ export function RetentionLineGraph({
     inSharedMode = false,
 }: RetentionLineGraphProps): JSX.Element {
     const logic = retentionTableLogic({ dashboardItemId: dashboardItemId })
-    const { filters, retention, retentionLoading } = useValues(logic)
+    const { filters, retention, retentionLoading, people, peopleLoading } = useValues(logic)
+    const { loadPeople } = useActions(logic)
     const [{ fromItem }] = useState(router.values.hashParams)
+    const [modalVisible, setModalVisible] = useState(false)
+    const [day, setDay] = useState(0)
+    function closeModal(): void {
+        setModalVisible(false)
+    }
 
     return retentionLoading ? (
         <Loading />
     ) : retention && retention.data && !retentionLoading ? (
-        <LineGraph
-            pageKey={'trends-annotations'}
-            data-attr="trend-line-graph"
-            type="line"
-            color={color}
-            datasets={retention.data}
-            labels={(retention.data[0] && retention.data[0].labels) || []}
-            isInProgress={!filters.selectedDate}
-            dashboardItemId={dashboardItemId || fromItem}
-            inSharedMode={inSharedMode}
-            percentage={true}
-            onClick={() => {}}
-        />
+        <>
+            <LineGraph
+                pageKey={'trends-annotations'}
+                data-attr="trend-line-graph"
+                type="line"
+                color={color}
+                datasets={retention.data}
+                labels={(retention.data[0] && retention.data[0].labels) || []}
+                isInProgress={!filters.selectedDate}
+                dashboardItemId={dashboardItemId || fromItem}
+                inSharedMode={inSharedMode}
+                percentage={true}
+                onClick={
+                    dashboardItemId
+                        ? null
+                        : (point) => {
+                              const { day } = point
+                              loadPeople(day - 1) // start from 0
+                              setDay(day - 1)
+                              setModalVisible(true)
+                          }
+                }
+            />
+            <Modal
+                title={dateOptions[filters.period] + ' ' + day + ' people'}
+                visible={modalVisible}
+                onOk={closeModal}
+                onCancel={closeModal}
+                footer={<Button onClick={closeModal}>Close</Button>}
+                width={700}
+            >
+                {/* {people ? (
+                <p>
+                    Found {people.count === 99 ? '99+' : people.count} {people.count === 1 ? 'user' : 'users'}
+                </p>
+            ) : (
+                <p>Loading users...</p>
+            )} */}
+
+                <PeopleTable loading={peopleLoading} people={people} />
+                <div
+                    style={{
+                        margin: '1rem',
+                        textAlign: 'center',
+                    }}
+                >
+                    {/* {people?.next && (
+                    <Button type="primary" onClick={loadMorePeople}>
+                        {people?.loadingMore ? <Spin /> : 'Load more people'}
+                    </Button>
+                )} */}
+                </div>
+            </Modal>
+        </>
     ) : (
         <LineGraphEmptyState color={color} />
     )
--- frontend/src/scenes/retention/retentionTableLogic.ts
+++ frontend/src/scenes/retention/retentionTableLogic.ts
@@ -6,6 +6,7 @@ import { ViewType, insightLogic } from 'scenes/insights/insightLogic'
 import { insightHistoryLogic } from 'scenes/insights/InsightHistoryPanel/insightHistoryLogic'
 import moment, { Moment } from 'moment'
 import { retentionTableLogicType } from 'types/scenes/retention/retentionTableLogicType'
+import { ACTIONS_LINE_GRAPH_LINEAR, ACTIONS_TABLE } from 'lib/constants'
 
 export const dateOptions = {
     h: 'Hour',
@@ -53,6 +54,29 @@ function cleanFilters(filters): any {
     }
 }
 
+function toUrlParams(values: Record<string, unknown>, extraVals?: Record<string, unknown>): string {
+    let params: Record<string, any> = { ...values.filters }
+    params['properties'] = values.properties
+    if (values.selectedDate) params['date_to'] = values.selectedDate.toISOString()
+    if (values.period) params['period'] = dateOptions[values.period]
+    if (values.startEntity) params['target_entity'] = values.startEntity
+    if (values.retentionType) params['retention_type'] = values.retentionType
+    if (values.returningEntity) {
+        params['actions'] = Array.isArray(values.filters.returningEntity.actions)
+            ? values.filters.returningEntity.actions
+            : []
+        params['events'] = Array.isArray(values.filters.returningEntity.events)
+            ? values.filters.returningEntity.events
+            : []
+    }
+    params = {
+        ...params,
+        ...extraVals,
+    }
+    const urlParams = toParams(params)
+    return urlParams
+}
+
 export const retentionTableLogic = kea<retentionTableLogicType<Moment>>({
     key: (props) => {
         return props.dashboardItemId || DEFAULT_RETENTION_LOGIC_KEY
@@ -61,39 +85,32 @@ export const retentionTableLogic = kea<retentionTableLogicType<Moment>>({
         retention: {
             __default: ({} as Record<string, unknown>) || Array,
             loadRetention: async (_: any, breakpoint) => {
-                const params: Record<string, any> = { ...values.filters }
-                params['properties'] = values.properties
-                if (values.selectedDate) params['date_to'] = values.selectedDate.toISOString()
-                if (values.period) params['period'] = dateOptions[values.period]
-                if (values.startEntity) params['target_entity'] = values.startEntity
-                if (values.retentionType) params['retention_type'] = values.retentionType
-                if (values.returningEntity) {
-                    params['actions'] = Array.isArray(values.filters.returningEntity.actions)
-                        ? values.filters.returningEntity.actions
-                        : []
-                    params['events'] = Array.isArray(values.filters.returningEntity.events)
-                        ? values.filters.returningEntity.events
-                        : []
-                }
-                const urlParams = toParams(params)
+                const urlParams = toUrlParams(values)
                 const res = await api.get(`api/insight/retention/?${urlParams}`)
                 breakpoint()
                 return res
             },
         },
         people: {
-            __default: {},
+            __default: [],
             loadPeople: async (rowIndex) => {
-                const people = values.retention.data[rowIndex].values[0].people
+                if (values.filters.display === ACTIONS_LINE_GRAPH_LINEAR) {
+                    const urlParams = toUrlParams(values, { intervals: rowIndex })
+                    const res = await api.get(`api/person/retention/?${urlParams}`)
 
-                if (people.length === 0) return []
-                const results = (await api.get('api/person/?id=' + people.join(','))).results
-                results.sort(function (a, b) {
-                    return people.indexOf(a.id) - people.indexOf(b.id)
-                })
-                return {
-                    ...values.people,
-                    [`${rowIndex}`]: results,
+                    return res['result']
+                } else {
+                    const people = values.retention.data[rowIndex].values[0].people
+
+                    if (people.length === 0) return []
+                    const results = (await api.get('api/person/?id=' + people.join(','))).results
+                    results.sort(function (a, b) {
+                        return people.indexOf(a.id) - people.indexOf(b.id)
+                    })
+                    return {
+                        ...values.people,
+                        [`${rowIndex}`]: results,
+                    }
                 }
             },
         },
@@ -134,6 +151,7 @@ export const retentionTableLogic = kea<retentionTableLogicType<Moment>>({
                       },
                       period: props.filters.period || 'd',
                       retentionType: props.filters.retentionType || RETENTION_FIRST_TIME,
+                      display: props.filters.display || ACTIONS_TABLE,
                   }
                 : (state) => cleanFilters(router.selectors.searchParams(state)),
             {
@@ -141,7 +159,7 @@ export const retentionTableLogic = kea<retentionTableLogicType<Moment>>({
             },
         ],
         people: {
-            clearPeople: () => ({}),
+            clearPeople: () => [],
             updatePeople: (state, { selectedIndex, people }) => ({
                 ...state,
                 [`${selectedIndex}`]: [...state[selectedIndex], ...people],
@@ -223,22 +241,19 @@ export const retentionTableLogic = kea<retentionTableLogicType<Moment>>({
     urlToAction: ({ actions, values, key }) => ({
         '/insights': (_, searchParams: Record<string, any>) => {
             if (searchParams.insight === ViewType.RETENTION) {
-                if (key != DEFAULT_RETENTION_LOGIC_KEY) {
-                    return
-                }
+                if (key != DEFAULT_RETENTION_LOGIC_KEY) return
+
                 const cleanSearchParams = cleanFilters(searchParams)
                 const cleanedFilters = cleanFilters(values.filters)
 
                 if (cleanSearchParams.display !== cleanedFilters.display) {
                     actions.clearRetention()
+                    actions.clearPeople()
                 }
 
-                if (!objectsEqual(cleanSearchParams, cleanedFilters)) {
-                    actions.setFilters(cleanSearchParams)
-                }
-                if (!objectsEqual(searchParams.properties, values.properties)) {
+                if (!objectsEqual(cleanSearchParams, cleanedFilters)) actions.setFilters(cleanSearchParams)
+                if (!objectsEqual(searchParams.properties, values.properties))
                     actions.setProperties(searchParams.properties || [])
-                }
             }
         },
     }),
