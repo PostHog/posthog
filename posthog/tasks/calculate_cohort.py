1df7a01302b36811f20e600ff7dff5c90e45d478
Tim Glaser <tim@glsr.nl>
2020-11-19 11:37:52 +0100

    explicit nulls first

--- posthog/tasks/calculate_cohort.py
+++ posthog/tasks/calculate_cohort.py
@@ -3,17 +3,15 @@ import time
 
 from celery import shared_task
 from dateutil.relativedelta import relativedelta
-from django.db.models import Q
-from django.db.models.query import QuerySet
+from django.db.models import F, Q
 from django.utils import timezone
 
-from posthog.celery import app
-from posthog.ee import is_ee_enabled
 from posthog.models import Cohort
 
 logger = logging.getLogger(__name__)
 
 MAX_AGE_MINUTES = 15
+PARALLEL_COHORTS = 15
 
 
 def calculate_cohorts() -> None:
@@ -21,7 +19,7 @@ def calculate_cohorts() -> None:
     # Every minute, grab a few cohorts off the list and execute them
     for cohort in Cohort.objects.filter(
         Q(is_calculating=False) | Q(last_calculation__lte=timezone.now() - relativedelta(minutes=MAX_AGE_MINUTES))
-    ).order_by("last_calculation")[0:15]:
+    ).order_by(F("last_contacted").asc(nulls_first=True))[0:PARALLEL_COHORTS]:
         calculate_cohort.delay(cohort.id)
 
 
