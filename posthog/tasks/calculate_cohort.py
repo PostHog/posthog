5cfcf53aaf0f9dd8a1142d2c6a1a4c2ad4198b22
Tim Glaser <tim@glsr.nl>
2020-11-19 10:39:52 +0100

    Fix cohorts stuck on empty querysets

--- posthog/tasks/calculate_cohort.py
+++ posthog/tasks/calculate_cohort.py
@@ -13,12 +13,15 @@ from posthog.models import Cohort
 
 logger = logging.getLogger(__name__)
 
+MAX_AGE_MINUTES = 15
 
-def calculate_cohorts(max_age_minutes: int = 15) -> None:
-    start_time = time.time()
+
+def calculate_cohorts() -> None:
+    # This task will be run every minute
+    # Every minute, grab a few cohorts off the list and execute them
     for cohort in Cohort.objects.filter(
-        Q(is_calculating=False) | Q(last_calculation__lte=timezone.now() - relativedelta(minutes=max_age_minutes))
-    ).order_by("id"):
+        Q(is_calculating=False) | Q(last_calculation__lte=timezone.now() - relativedelta(minutes=MAX_AGE_MINUTES))
+    ).order_by("last_calculation")[0:15]:
         calculate_cohort.delay(cohort.id)
 
 
