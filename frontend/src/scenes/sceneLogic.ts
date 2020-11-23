5455c7856c3118c62b4d05b1be54370924684ca9
Michael Matloka <dev@twixes.com>
2020-11-23 22:29:25 +0100

    Improve style

--- frontend/src/scenes/sceneLogic.ts
+++ frontend/src/scenes/sceneLogic.ts
@@ -75,13 +75,13 @@ export const scenes: Record<Scene, () => any> = {
     [Scene.Plugins]: () => import(/* webpackChunkName: 'plugins' */ './plugins/Plugins'),
 }
 
-interface sceneConfigType {
+interface SceneConfig {
     unauthenticated?: boolean // If route is to be accessed when logged out (N.B. add to posthog/urls.py too)
     dark?: boolean // Background is $bg_mid
     plain?: boolean // Only keeps the main content and the top navigation bar
 }
 
-export const sceneConfigurations: Record<string, sceneConfigType> = {
+export const sceneConfigurations: Partial<Record<Scene, SceneConfig>> = {
     [Scene.PreflightCheck]: {
         unauthenticated: true,
     },
@@ -201,11 +201,8 @@ export const sceneLogic = kea<sceneLogicType>({
     selectors: () => ({
         sceneConfig: [
             (selectors) => [selectors.scene],
-            (scene: string) => {
-                if (scene in sceneConfigurations) {
-                    return sceneConfigurations[scene]
-                }
-                return {} as sceneConfigType
+            (scene: Scene): SceneConfig => {
+                return sceneConfigurations[scene] ?? {}
             },
         ],
     }),
