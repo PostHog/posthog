5821ff0be7bfc62362cbfb8a577cfafddb4b1ad3
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-23 12:09:08 +0200

    Show session recording player
    Alignment issue is back in weird ways.

--- frontend/src/scenes/sessions/SessionsPlayerDrawer.tsx
+++ frontend/src/scenes/sessions/SessionsPlayerDrawer.tsx
@@ -4,12 +4,32 @@ import { useActions, useValues } from 'kea'
 import { Loading } from 'lib/utils'
 import { sessionsTableLogic } from 'scenes/sessions/sessionsTableLogic'
 import SessionsPlayer from 'scenes/sessions/SessionsPlayer'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
+import { Player } from 'posthog-react-rrweb-player'
+
+import 'posthog-react-rrweb-player/dist/index.css'
 
 export default function SessionsPlayerDrawer(): JSX.Element {
+    const { featureFlags } = useValues(featureFlagLogic)
+
     const { sessionPlayerData, sessionPlayerDataLoading, sessionRecordingNavigation: nav } = useValues(
         sessionsTableLogic
     )
     const { loadSessionPlayer, closeSessionPlayer } = useActions(sessionsTableLogic)
+
+    if (featureFlags['posthog-rrweb-player']) {
+        return (
+            <Drawer
+                title="Session recording"
+                width={window.innerWidth - 300}
+                onClose={closeSessionPlayer}
+                destroyOnClose={true}
+                visible={true}
+            >
+                {sessionPlayerDataLoading ? <Loading /> : <Player events={sessionPlayerData} />}
+            </Drawer>
+        )
+    }
 
     return (
         <Drawer
