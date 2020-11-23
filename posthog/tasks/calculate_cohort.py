c066732b52671c9287c50b52dccc45a4facb3a35
Tim Glaser <tim.glaser@hiberly.com>
2020-11-23 13:49:27 +0000

    env variable for parallel cohorts (#2472)
    * env variable for parallel cohorts
    
    * fi

--- posthog/tasks/calculate_cohort.py
+++ posthog/tasks/calculate_cohort.py
@@ -1,9 +1,10 @@
 import logging
+import os
 import time
 
 from celery import shared_task
 from dateutil.relativedelta import relativedelta
-from django.db.models import F, Q
+from django.db.models import F
 from django.utils import timezone
 
 from posthog.models import Cohort
@@ -11,14 +12,14 @@ from posthog.models import Cohort
 logger = logging.getLogger(__name__)
 
 MAX_AGE_MINUTES = 15
-PARALLEL_COHORTS = 5
+PARALLEL_COHORTS = int(os.environ.get("PARALLEL_COHORTS", 5))
 
 
 def calculate_cohorts() -> None:
     # This task will be run every minute
     # Every minute, grab a few cohorts off the list and execute them
     for cohort in Cohort.objects.filter(
-        Q(is_calculating=False) | Q(last_calculation__lte=timezone.now() - relativedelta(minutes=MAX_AGE_MINUTES))
+        is_calculating=False, last_calculation__lte=timezone.now() - relativedelta(minutes=MAX_AGE_MINUTES)
     ).order_by(F("last_calculation").asc(nulls_first=True))[0:PARALLEL_COHORTS]:
         calculate_cohort.delay(cohort.id)
 
