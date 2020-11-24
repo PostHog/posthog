a0987a3ba10e5459eb61d7fa6a1ad60edc34766c
Michael Matloka <dev@twixes.com>
2020-11-24 02:14:55 +0100

    Extract out of scope changes

--- frontend/src/scenes/organization/Members/index.tsx
+++ frontend/src/scenes/organization/Members/index.tsx
@@ -145,23 +145,18 @@ function ActionsComponent(_, member: OrganizationMemberType): JSX.Element | null
         })
     }
 
-    const allowDeletion =
-        // lower-ranked users cannot be removed, at the same time the currently logged-in user can leave any time
-        (member.level < currentMembershipLevel || member.user_id === user.id) &&
-        // unless that user is the organization's owner, in which case they can't leave
-        member.level !== OrganizationMembershipLevel.Owner
-
     return (
         <div>
-            {allowDeletion && (
-                <a className="text-danger" onClick={handleClick}>
-                    {member.user_id !== user.id ? (
-                        <DeleteOutlined title="Remove Member" />
-                    ) : (
-                        <LogoutOutlined title="Leave Organization" />
-                    )}
-                </a>
-            )}
+            {member.level !== OrganizationMembershipLevel.Owner &&
+                (member.level < currentMembershipLevel || member.user_id === user.id) && (
+                    <a className="text-danger" onClick={handleClick}>
+                        {member.user_id !== user.id ? (
+                            <DeleteOutlined title="Remove Member" />
+                        ) : (
+                            <LogoutOutlined title="Leave Organization" />
+                        )}
+                    </a>
+                )}
         </div>
     )
 }
