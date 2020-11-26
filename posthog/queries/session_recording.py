38f8364d7af74e455451fa7d490d8b40d500e72d
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2020-11-26 15:06:50 +0100

    Return person data together with session recording (#2528)
    * Return person data together with session
    
    This will be used in the new session recording page
    
    * Add names to anonymous types

--- posthog/queries/session_recording.py
+++ posthog/queries/session_recording.py
@@ -1,21 +1,35 @@
 import datetime
-from typing import Any, Callable, Dict, List
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from django.db.models import F, Max, Min
 
-from posthog.models import Filter, SessionRecordingEvent, Team
+from posthog.models import Filter, Person, SessionRecordingEvent, Team
 from posthog.queries.base import BaseQuery
 
+DistinctId = str
+Snapshots = List[Any]
 
-class SessionRecording(BaseQuery):
-    def query_recording_snapshots(self, team: Team, session_id: str) -> List[Any]:
+
+class SessionRecording:
+    def query_recording_snapshots(self, team: Team, session_id: str) -> Tuple[Optional[DistinctId], Snapshots]:
         events = SessionRecordingEvent.objects.filter(team=team, session_id=session_id)
-        return [e.snapshot_data for e in events]
 
-    def run(self, filter: Filter, team: Team, *args, **kwargs) -> List[Dict[str, Any]]:
-        return list(
-            sorted(self.query_recording_snapshots(team, kwargs["session_recording_id"]), key=lambda s: s["timestamp"])
+        if len(events) == 0:
+            return None, []
+
+        return events[0].distinct_id, [e.snapshot_data for e in events]
+
+    def run(self, filter: Filter, team: Team, *args, **kwargs) -> Dict[str, Any]:
+        from posthog.api.person import PersonSerializer
+
+        distinct_id, snapshots = self.query_recording_snapshots(team, kwargs["session_recording_id"])
+        person = (
+            PersonSerializer(Person.objects.get(team=team, persondistinctid__distinct_id=distinct_id)).data
+            if distinct_id
+            else {}
         )
+
+        return {"snapshots": list(sorted(snapshots, key=lambda s: s["timestamp"])), "person": person}
 
 
 def query_sessions_in_range(team: Team, start_time: datetime.datetime, end_time: datetime.datetime) -> List[dict]:
