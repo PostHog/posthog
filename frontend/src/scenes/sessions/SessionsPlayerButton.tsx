72188d141ba9744e208d7052d2c59da91807bacf
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-26 16:49:24 +0200

    Toggle new player page with a feature flag

--- frontend/src/scenes/sessions/SessionsPlayerButton.tsx
+++ frontend/src/scenes/sessions/SessionsPlayerButton.tsx
@@ -5,10 +5,12 @@ import { SessionType } from '~/types'
 import './Sessions.scss'
 import { fromParams, toParams } from 'lib/utils'
 import { Link } from 'lib/components/Link'
+import { useValues } from 'kea'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
-function sessionPlayerUrl(sessionRecordingId: string): string {
+function sessionPlayerUrl(sessionRecordingId: string, baseUrl: string): string {
     const params = { ...fromParams(), sessionRecordingId }
-    return location.pathname + '?' + toParams(params)
+    return baseUrl + '?' + toParams(params)
 }
 
 interface SessionsPlayerButtonProps {
@@ -16,6 +18,8 @@ interface SessionsPlayerButtonProps {
 }
 
 export default function SessionsPlayerButton({ session }: SessionsPlayerButtonProps): JSX.Element | null {
+    const { featureFlags } = useValues(featureFlagLogic)
+
     if (!session.session_recording_ids) {
         return null
     }
@@ -24,7 +28,11 @@ export default function SessionsPlayerButton({ session }: SessionsPlayerButtonPr
         <>
             {session.session_recording_ids.map((sessionRecordingId: string) => (
                 <Link
-                    to={sessionPlayerUrl(sessionRecordingId)}
+                    to={sessionPlayerUrl(
+                        sessionRecordingId,
+                        featureFlags['full-page-player'] ? '/sessions/play' : location.pathname
+                    )}
+                    target={featureFlags['full-page-player'] ? '_blank' : undefined}
                     className="sessions-player-button"
                     key={sessionRecordingId}
                 >
