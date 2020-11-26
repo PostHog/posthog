5a8654ea0d91f1ab2688bf22b765c041da80969f
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-11-26 16:27:06 -0500

    fix linting

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify'
 import { Spin } from 'antd'
 import moment from 'moment'
 import { EventType } from '~/types'
-import { lightColors } from 'lib/colors'
+import { lightColors } from './colors'
 
 const SI_PREFIXES: { value: number; symbol: string }[] = [
     { value: 1e18, symbol: 'E' },
