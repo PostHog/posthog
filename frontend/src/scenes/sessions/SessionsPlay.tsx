c46bae8628a8569a79483aec3a2b02fcda34a2bc
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-11-27 04:39:43 -0500

    UI improvements to sessions page (#2538)
    * move some logic to kea
    
    * sessions loading state
    
    * URL max width & copy to clipboard

--- frontend/src/scenes/sessions/SessionsPlay.tsx
+++ frontend/src/scenes/sessions/SessionsPlay.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react'
-import { EventIndex, Player, PlayerRef } from 'posthog-react-rrweb-player'
-import { Card, Col, Input, Row, Tag } from 'antd'
+import { Player, PlayerRef } from 'posthog-react-rrweb-player'
+import { Card, Col, Input, Row, Skeleton, Tag } from 'antd'
 import {
     AppleOutlined,
     ChromeOutlined,
@@ -19,6 +19,7 @@ import { IconExternalLink } from 'lib/components/icons'
 import rrwebBlockClass from 'lib/utils/rrwebBlockClass'
 import './Sessions.scss'
 import './SessionsPlayer.scss'
+import { CopyToClipboardInline } from 'lib/components/CopyToClipboard'
 
 export const SessionsPlay = hot(_SessionsPlay)
 function _SessionsPlay(): JSX.Element {
@@ -30,17 +31,15 @@ function _SessionsPlay(): JSX.Element {
         tags,
         tagsLoading,
         sessionTimestamp,
+        eventIndex,
+        pageVisitEvents,
     } = useValues(sessionsPlayLogic)
     const { toggleAddingTagShown, setAddingTag, createTag } = useActions(sessionsPlayLogic)
     const addTagInput = useRef<Input>(null)
 
     const [playerTime, setCurrentPlayerTime] = useState(0)
     const playerRef = useRef<PlayerRef>(null)
-    const eventIndex: EventIndex = useMemo(() => new EventIndex(sessionPlayerData?.snapshots || []), [
-        sessionPlayerData,
-    ])
     const [pageEvent, atPageIndex] = useMemo(() => eventIndex.getPageMetadata(playerTime), [eventIndex, playerTime])
-    const pageVisitEvents = useMemo(() => eventIndex.pageChangeEvents(), [eventIndex])
 
     useEffect(() => {
         if (addingTagShown && addTagInput.current) {
@@ -52,17 +51,26 @@ function _SessionsPlay(): JSX.Element {
         <div className="session-player">
             <Row gutter={16} style={{ height: '100%' }}>
                 <Col span={18} style={{ paddingRight: 0 }}>
-                    <div className="mb-05">
-                        {pageEvent ? (
+                    <div className="mb-05" style={{ display: 'flex' }}>
+                        {sessionPlayerDataLoading && <Skeleton paragraph={{ rows: 0 }} active />}
+
+                        {!sessionPlayerDataLoading && (
                             <>
-                                <b>Current URL: </b>
-                                {pageEvent.href}
+                                {pageEvent ? (
+                                    <>
+                                        <b>Current URL:</b>
+                                        <span className="url-info ml-05">{pageEvent.href}</span>
+                                        <CopyToClipboardInline explicitValue={pageEvent.href} isValueSensitive>
+                                            &nbsp;
+                                        </CopyToClipboardInline>
+                                    </>
+                                ) : null}
+                                {/* TODO: Not implemented */}
+                                <span className="float-right" style={{ display: 'none' }}>
+                                    <ChromeOutlined /> Chrome on <AppleOutlined /> macOS (1400 x 600)
+                                </span>
                             </>
-                        ) : null}
-                        {/* TODO: Not implemented */}
-                        <span className="float-right" style={{ display: 'none' }}>
-                            <ChromeOutlined /> Chrome on <AppleOutlined /> macOS (1400 x 600)
-                        </span>
+                        )}
                     </div>
                     <div className="ph-no-capture player-container">
                         {sessionPlayerDataLoading ? (
@@ -79,62 +87,78 @@ function _SessionsPlay(): JSX.Element {
                 <Col span={6} className="sidebar" style={{ paddingLeft: 16 }}>
                     <Card className="card-elevated">
                         <h3 className="l3">Session Information</h3>
-                        <div className="mb-05" style={{ display: 'none' }}>
-                            {/* TODO: Add session duration information */}
-                            <FieldTimeOutlined /> {sessionTimestamp}
-                        </div>
-                        {sessionPlayerData?.person && (
+                        {sessionPlayerDataLoading && (
                             <div>
-                                <UserOutlined style={{ marginRight: 4 }} />
-                                <Link
-                                    to={`/person/${encodeURIComponent(sessionPlayerData.person.distinct_ids[0])}`}
-                                    className={rrwebBlockClass + ' ph-no-capture'}
-                                    target="_blank"
-                                    style={{ display: 'inline-flex', alignItems: 'center' }}
-                                >
-                                    <span style={{ marginRight: 4 }}>{sessionPlayerData.person.name}</span>
-                                    <IconExternalLink />
-                                </Link>
+                                <Skeleton paragraph={{ rows: 3 }} active />
                             </div>
                         )}
-                        <div className="mt" style={{ display: 'none' }}>
-                            <div>
-                                <b>Tags</b>
-                            </div>
-                            {tags.map((tag, index) => {
-                                return (
-                                    <Tag key={index} color={colorForString(tag)} closable className="tag-wrapper">
-                                        {tag}
-                                    </Tag>
-                                )
-                            })}
-                            <span className="tag-wrapper" style={{ display: 'inline-flex' }}>
-                                <Tag
-                                    onClick={toggleAddingTagShown}
-                                    data-attr="button-add-tag"
-                                    style={{
-                                        cursor: 'pointer',
-                                        borderStyle: 'dashed',
-                                        backgroundColor: '#ffffff',
-                                        display: addingTagShown ? 'none' : 'initial',
-                                    }}
-                                >
-                                    <PlusOutlined /> New Tag
-                                </Tag>
-                                <Input
-                                    type="text"
-                                    size="small"
-                                    onBlur={toggleAddingTagShown}
-                                    ref={addTagInput}
-                                    style={{ width: 78, display: !addingTagShown ? 'none' : 'flex' }}
-                                    value={addingTag}
-                                    onChange={(e) => setAddingTag(e.target.value)}
-                                    onPressEnter={createTag}
-                                    disabled={tagsLoading}
-                                    prefix={tagsLoading ? <SyncOutlined spin /> : null}
-                                />
-                            </span>
-                        </div>
+                        {!sessionPlayerDataLoading && (
+                            <>
+                                <div className="mb-05" style={{ display: 'none' }}>
+                                    {/* TODO: Add session duration information */}
+                                    <FieldTimeOutlined /> {sessionTimestamp}
+                                </div>
+                                {sessionPlayerData?.person && (
+                                    <div>
+                                        <UserOutlined style={{ marginRight: 4 }} />
+                                        <Link
+                                            to={`/person/${encodeURIComponent(
+                                                sessionPlayerData.person.distinct_ids[0]
+                                            )}`}
+                                            className={rrwebBlockClass + ' ph-no-capture'}
+                                            target="_blank"
+                                            style={{ display: 'inline-flex', alignItems: 'center' }}
+                                        >
+                                            <span style={{ marginRight: 4 }}>{sessionPlayerData.person.name}</span>
+                                            <IconExternalLink />
+                                        </Link>
+                                    </div>
+                                )}
+                                <div className="mt" style={{ display: 'none' }}>
+                                    <div>
+                                        <b>Tags</b>
+                                    </div>
+                                    {tags.map((tag, index) => {
+                                        return (
+                                            <Tag
+                                                key={index}
+                                                color={colorForString(tag)}
+                                                closable
+                                                className="tag-wrapper"
+                                            >
+                                                {tag}
+                                            </Tag>
+                                        )
+                                    })}
+                                    <span className="tag-wrapper" style={{ display: 'inline-flex' }}>
+                                        <Tag
+                                            onClick={toggleAddingTagShown}
+                                            data-attr="button-add-tag"
+                                            style={{
+                                                cursor: 'pointer',
+                                                borderStyle: 'dashed',
+                                                backgroundColor: '#ffffff',
+                                                display: addingTagShown ? 'none' : 'initial',
+                                            }}
+                                        >
+                                            <PlusOutlined /> New Tag
+                                        </Tag>
+                                        <Input
+                                            type="text"
+                                            size="small"
+                                            onBlur={toggleAddingTagShown}
+                                            ref={addTagInput}
+                                            style={{ width: 78, display: !addingTagShown ? 'none' : 'flex' }}
+                                            value={addingTag}
+                                            onChange={(e) => setAddingTag(e.target.value)}
+                                            onPressEnter={createTag}
+                                            disabled={tagsLoading}
+                                            prefix={tagsLoading ? <SyncOutlined spin /> : null}
+                                        />
+                                    </span>
+                                </div>
+                            </>
+                        )}
                     </Card>
                     <div className="mt" />
                     <Card className="card-elevated">
@@ -142,16 +166,23 @@ function _SessionsPlay(): JSX.Element {
                         <p className="text-muted text-small">
                             Click on an item to jump to that point in the recording.
                         </p>
-                        <div className="timeline">
-                            <div className="line" />
-                            <div className="timeline-items">
-                                {pageVisitEvents.map(({ href, playerTime }, index) => (
-                                    <div className={index === atPageIndex ? 'current' : undefined} key={index}>
-                                        <Tag onClick={() => playerRef.current?.seek(playerTime)}>{href}</Tag>
-                                    </div>
-                                ))}
+                        {sessionPlayerDataLoading && (
+                            <div>
+                                <Skeleton paragraph={{ rows: 6 }} active />
                             </div>
-                        </div>
+                        )}
+                        {!sessionPlayerDataLoading && (
+                            <div className="timeline">
+                                <div className="line" />
+                                <div className="timeline-items">
+                                    {pageVisitEvents.map(({ href, playerTime }, index) => (
+                                        <div className={index === atPageIndex ? 'current' : undefined} key={index}>
+                                            <Tag onClick={() => playerRef.current?.seek(playerTime)}>{href}</Tag>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
                     </Card>
                 </Col>
             </Row>
