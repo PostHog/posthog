4b7a3bec090aa6a9627a93a2d0418f0aedda520b
Eric <eeoneric@gmail.com>
2020-12-01 11:14:34 -0500

    add diasbled to chartfilter

--- frontend/src/lib/components/ChartFilter/ChartFilter.js
+++ frontend/src/lib/components/ChartFilter/ChartFilter.js
@@ -15,7 +15,7 @@ import { chartFilterLogic } from './chartFilterLogic'
 export function ChartFilter(props) {
     let { filters, displayMap, onChange } = props
 
-    const { chartFilter } = useValues(chartFilterLogic)
+    const { chartFilter, disabled } = useValues(chartFilterLogic)
     const { setChartFilter } = useActions(chartFilterLogic)
 
     const cumulativeDisabled = filters.session || filters.shown_as === STICKINESS || filters.retentionType
@@ -44,7 +44,7 @@ export function ChartFilter(props) {
             bordered={false}
             dropdownMatchSelectWidth={false}
             data-attr="chart-filter"
-            disabled={filters.shown_as === 'Lifecycle'}
+            disabled={disabled}
         >
             <Select.OptGroup label={'Line Chart'}>
                 <Select.Option value={ACTIONS_LINE_GRAPH_LINEAR} disabled={linearDisabled}>
--- frontend/src/lib/components/ChartFilter/chartFilterLogic.js
+++ frontend/src/lib/components/ChartFilter/chartFilterLogic.js
@@ -1,5 +1,5 @@
 import { kea } from 'kea'
-import { ACTIONS_LINE_GRAPH_LINEAR, ACTIONS_TABLE } from '~/lib/constants'
+import { ACTIONS_BAR_CHART, ACTIONS_LINE_GRAPH_LINEAR, ACTIONS_TABLE } from '~/lib/constants'
 import { router } from 'kea-router'
 import { objectsEqual } from 'lib/utils'
 import { ViewType } from 'scenes/insights/insightLogic'
@@ -7,12 +7,19 @@ import { ViewType } from 'scenes/insights/insightLogic'
 export const chartFilterLogic = kea({
     actions: () => ({
         setChartFilter: (filter) => ({ filter }),
+        setDisabled: (disabled) => ({ disabled }),
     }),
     reducers: ({ actions }) => ({
         chartFilter: [
             ACTIONS_LINE_GRAPH_LINEAR,
             {
                 [actions.setChartFilter]: (_, { filter }) => filter,
+            },
+        ],
+        disabled: [
+            false,
+            {
+                [actions.setDisabled]: (_, { disabled }) => disabled,
             },
         ],
     }),
@@ -29,11 +36,17 @@ export const chartFilterLogic = kea({
         },
     }),
     urlToAction: ({ actions }) => ({
-        '/insights': (_, { display, insight }) => {
+        '/insights': (_, { display, insight, shown_as }) => {
             if (display) {
                 actions.setChartFilter(display)
             } else if (insight === ViewType.RETENTION) {
                 actions.setChartFilter(ACTIONS_TABLE)
+            }
+            if (shown_as === 'Lifecycle') {
+                actions.setChartFilter(ACTIONS_BAR_CHART)
+                actions.setDisabled(true)
+            } else {
+                actions.setDisabled(false)
             }
         },
     }),
