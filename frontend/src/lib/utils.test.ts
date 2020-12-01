16fd5d3ea243c104c6138e9d05d79ef5e741cab1
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-12-01 10:10:29 +0200

    Use standard jest test formatting

--- frontend/src/lib/utils.test.ts
+++ frontend/src/lib/utils.test.ts
@@ -1,14 +1,16 @@
 import { identifierToHuman } from './utils'
 
-test('identifier humanizes properly', () => {
-    expect(identifierToHuman('testIdentifier')).toBe('Test Identifier')
-    expect(identifierToHuman('testIdentifierX')).toBe('Test Identifier X')
-    expect(identifierToHuman('something     ')).toBe('Something')
-    expect(identifierToHuman('  some_property')).toBe('Some Property')
-    expect(identifierToHuman(' Number666')).toBe('Number 666')
-    expect(identifierToHuman('7x')).toBe('7x')
-    expect(identifierToHuman('7X')).toBe('7 X')
-    expect(identifierToHuman('500')).toBe('500')
-    expect(identifierToHuman(404)).toBe('404')
-    expect(identifierToHuman('CreateProject')).toBe('Create Project')
+describe('identifierToHuman()', () => {
+    it('humanizes properly', () => {
+        expect(identifierToHuman('testIdentifier')).toEqual('Test Identifier')
+        expect(identifierToHuman('testIdentifierX')).toEqual('Test Identifier X')
+        expect(identifierToHuman('something     ')).toEqual('Something')
+        expect(identifierToHuman('  some_property')).toEqual('Some Property')
+        expect(identifierToHuman(' Number666')).toEqual('Number 666')
+        expect(identifierToHuman('7x')).toEqual('7x')
+        expect(identifierToHuman('7X')).toEqual('7 X')
+        expect(identifierToHuman('500')).toEqual('500')
+        expect(identifierToHuman(404)).toEqual('404')
+        expect(identifierToHuman('CreateProject')).toEqual('Create Project')
+    })
 })
