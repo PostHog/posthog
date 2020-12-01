ba871b834fe604cd3e0e649becce5e8653f3fcc7
Marius Andra <marius.andra@gmail.com>
2020-12-01 20:27:24 +0100

    clarify redis-mock's origins

--- jest.setup.redis-mock.js
+++ jest.setup.redis-mock.js
@@ -1,8 +1,9 @@
+// Adapted from https://github.com/stipsan/ioredis-mock/issues/568#issuecomment-492558489
 jest.mock('ioredis', () => {
     const Redis = require('ioredis-mock')
     if (typeof Redis === 'object') {
         // the first mock is an ioredis shim because ioredis-mock depends on it
-        // https://github.com/stipsan/ioredis-mock/blob/master/src/index.js#L101-L111
+        // https://github.com/stipsan/ioredis-mock/blob/2ba837f07c0723cde993fb8f791a5fcfdabce719/src/index.js#L100-L109
         return {
             Command: { _transformer: { argument: {}, reply: {} } },
         }
@@ -10,6 +11,7 @@ jest.mock('ioredis', () => {
     // second mock for our code
     return function (...args) {
         const redis = new Redis(args)
+        // adapted from copy/paste - our own brpop function!
         redis.brpop = async (...args) => {
             args.pop()
             return [args[0], await redis.rpop(...args)]
