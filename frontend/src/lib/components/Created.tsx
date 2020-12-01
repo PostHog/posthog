ff3aa805cd1a2c8b9587e0723d49f354009ccbfc
Tim Glaser <tim@glsr.nl>
2020-12-01 12:37:01 +0100

    thing

--- frontend/src/lib/components/Created.tsx
+++ frontend/src/lib/components/Created.tsx
@@ -1,9 +1,7 @@
-import { Tooltip } from "antd";
+import { Tooltip } from 'antd'
 import moment from 'moment'
 import React from 'react'
 
-export function Created({timestamp} : {timestamp: string}): JSX.Element {
-    return <Tooltip title={moment(timestamp).format('LLL')}>
-        {moment(timestamp).fromNow()}
-    </Tooltip>
+export function Created({ timestamp }: { timestamp: string }): JSX.Element {
+    return <Tooltip title={moment(timestamp).format('LLL')}>{moment(timestamp).fromNow()}</Tooltip>
 }
