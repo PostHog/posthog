f655bbfeaeddb5543ea5d40d227d7f611aacbb8d
Michael Matloka <dev@twixes.com>
2020-12-02 05:11:22 +0100

    Improve logging and error handling

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import { PluginsServer, PluginsServerConfig } from './types'
 import { version } from '../package.json'
 import { setupPlugins } from './plugins'
 import { startWorker } from './worker'
-import schedule from 'node-schedule'
+import schedule, { Job } from 'node-schedule'
 import Redis from 'ioredis'
 import { startFastifyInstance, stopFastifyInstance } from './web/server'
 import { FastifyInstance } from 'fastify'
+import Worker from 'celery/worker'
 
 export const defaultConfig: PluginsServerConfig = {
     CELERY_DEFAULT_QUEUE: 'celery',
@@ -21,62 +22,81 @@ export const defaultConfig: PluginsServerConfig = {
 }
 
 export async function startPluginsServer(config: PluginsServerConfig): Promise<void> {
-    console.info(`⚡ Starting posthog-plugin-server v${version}…`)
+    console.info(`⚡ posthog-plugin-server v${version}`)
 
-    const serverConfig: PluginsServerConfig = {
-        ...defaultConfig,
-        ...config,
-    }
+    let serverConfig: PluginsServerConfig | undefined
+    let db: Pool | undefined
+    let redis: Redis.Redis | undefined
+    let pubSub: Redis.Redis | undefined
+    let server: PluginsServer | undefined
+    let fastifyInstance: FastifyInstance | undefined
+    let worker: Worker | undefined
+    let job: Job | undefined
 
-    const db = new Pool({
-        connectionString: serverConfig.DATABASE_URL,
-    })
-
-    const redis = new Redis(serverConfig.REDIS_URL)
-
-    const server: PluginsServer = {
-        ...serverConfig,
-        db,
-        redis,
-    }
-
-    await setupPlugins(server)
-
-    let fastifyInstance: FastifyInstance | null = null
-    if (!serverConfig.DISABLE_WEB) {
-        fastifyInstance = await startFastifyInstance(serverConfig.WEB_PORT, serverConfig.WEB_HOSTNAME)
-    }
-
-    let stopWorker = startWorker(server)
-
-    const pubSub = new Redis(serverConfig.REDIS_URL)
-    pubSub.subscribe(serverConfig.PLUGINS_RELOAD_PUBSUB_CHANNEL)
-    pubSub.on('message', async (channel, message) => {
-        if (channel === serverConfig.PLUGINS_RELOAD_PUBSUB_CHANNEL) {
-            console.log('Reloading plugins!')
-            await stopWorker()
-            await setupPlugins(server)
-            stopWorker = startWorker(server)
+    async function cleanUp() {
+        console.info()
+        if (fastifyInstance && !serverConfig?.DISABLE_WEB) {
+            await stopFastifyInstance(fastifyInstance!)
         }
-    })
-
-    // every 5 sec set a @posthog-plugin-server/ping redis key
-    const job = schedule.scheduleJob('*/5 * * * * *', function () {
-        redis.set('@posthog-plugin-server/ping', new Date().toISOString())
-        redis.expire('@posthog-plugin-server/ping', 60)
-    })
-    console.info(`✅ Started posthog-plugin-server v${version}!`)
+        await worker?.stop()
+        pubSub?.disconnect()
+        if (job) {
+            schedule?.cancelJob(job)
+        }
+        await redis?.quit()
+        await db?.end()
+    }
 
     for (const signal of ['SIGINT', 'SIGTERM', 'SIGHUP']) {
-        process.on(signal, async () => {
-            if (!serverConfig.DISABLE_WEB) {
-                await stopFastifyInstance(fastifyInstance!)
-            }
-            await stopWorker()
-            pubSub.disconnect()
-            schedule.cancelJob(job)
-            await redis.quit()
-            await db.end()
+        process.on(signal, cleanUp)
+    }
+
+    try {
+        serverConfig = {
+            ...defaultConfig,
+            ...config,
+        }
+
+        db = new Pool({
+            connectionString: serverConfig.DATABASE_URL,
         })
+
+        redis = new Redis(serverConfig.REDIS_URL)
+
+        server = {
+            ...serverConfig,
+            db,
+            redis,
+        }
+
+        await setupPlugins(server)
+
+        if (!serverConfig.DISABLE_WEB) {
+            fastifyInstance = await startFastifyInstance(serverConfig.WEB_PORT, serverConfig.WEB_HOSTNAME)
+        }
+
+        worker = startWorker(server)
+
+        pubSub = new Redis(serverConfig.REDIS_URL)
+        pubSub.subscribe(serverConfig.PLUGINS_RELOAD_PUBSUB_CHANNEL)
+        pubSub.on('message', async (channel, message) => {
+            if (channel === serverConfig!.PLUGINS_RELOAD_PUBSUB_CHANNEL) {
+                console.log('Reloading plugins!')
+                await worker!.stop()
+                await setupPlugins(server!)
+                worker = startWorker(server!)
+            }
+        })
+
+        // every 5 sec set a @posthog-plugin-server/ping redis key
+        job = schedule.scheduleJob('*/5 * * * * *', () => {
+            redis!.set('@posthog-plugin-server/ping', new Date().toISOString())
+            redis!.expire('@posthog-plugin-server/ping', 60)
+        })
+        console.info(`🚀 All systems go.`)
+    } catch (error) {
+        console.error(`💥 Launchpad failure!\n${error.stack}`)
+        await cleanUp()
+        process.exit(1)
     }
 }
