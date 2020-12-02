763805f5c63e94f5641449625b02d9a7d813b7d9
Michael Matloka <dev@twixes.com>
2020-12-02 04:17:28 +0100

    Refactor web server

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,25 +1,46 @@
-import { fastify, FastifyRequest, FastifyReply } from 'fastify'
+import { fastify, FastifyInstance } from 'fastify'
+import { parse as querystringParse, ParsedUrlQuery } from 'querystring'
+import { parse as urlParse } from 'url'
 
-export const webServer = fastify()
-
-async function getEvent(request: FastifyRequest, reply: FastifyReply): Promise<Record<string, unknown>> {
-    return {}
-}
-
-webServer.get('*', getEvent)
-webServer.post('*', getEvent)
-
-export async function startWebServer(port: string | number, hostname?: string): Promise<void> {
-    console.info(`👾 Starting web server…`)
-    try {
-        const address = await webServer.listen(port, hostname)
-        console.info(`✅ Web server listening on ${address}!`)
-    } catch (e) {
-        console.error(`🛑 Web server could not start! ${e}`)
+declare module 'fastify' {
+    export interface FastifyRequest {
+        GET: ParsedUrlQuery
+        POST: ParsedUrlQuery
     }
 }
 
-export async function stopWebServer(): Promise<void> {
-    await webServer.close()
+export function buildFastifyInstance(): FastifyInstance {
+    const fastifyInstance = fastify()
+    fastifyInstance.addHook('preHandler', async (request) => {
+        // Mimic Django HttpRequest with GET and POST properties
+        request.GET = urlParse(request.url, true).query
+        try {
+            request.POST = querystringParse(String(request.body))
+        } catch {
+            request.POST = {}
+        }
+    })
+    return fastifyInstance
+}
+
+export async function stopFastifyInstance(fastifyInstance: FastifyInstance): Promise<void> {
+    await fastifyInstance.close()
     console.info(`\n🛑 Web server cleaned up!`)
+}
+
+export async function startFastifyInstance(
+    port: string | number = 3008,
+    hostname?: string,
+    withSignalHandling = true
+): Promise<FastifyInstance> {
+    console.info(`👾 Starting web server…`)
+    const fastifyInstance = buildFastifyInstance()
+    try {
+        const address = await fastifyInstance.listen(port, hostname)
+        console.info(`✅ Web server listening on ${address}!`)
+    } catch (e) {
+        console.error(`🛑 Web server could not start! ${e}`)
+        return fastifyInstance
+    }
+    return fastifyInstance
 }
