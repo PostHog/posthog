aa2ae1b210486d6e65fa4871b460b0dbc56d4af6
Tim Glaser <tim@glsr.nl>
2020-12-04 16:34:15 +0100

    tests

--- ee/clickhouse/middleware.py
+++ ee/clickhouse/middleware.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from loginas.utils import is_impersonated_session
 
+from posthog.ee import is_ee_enabled
+
 
 class CHQueries(object):
     def __init__(self, get_response):
@@ -16,7 +18,11 @@ class CHQueries(object):
         """
         from ee.clickhouse import client
 
-        if request.user.pk and (request.user.is_staff or is_impersonated_session(request) or settings.DEBUG):
+        if (
+            is_ee_enabled()
+            and request.user.pk
+            and (request.user.is_staff or is_impersonated_session(request) or settings.DEBUG)
+        ):
             client._save_query_user_id = request.user.pk
 
         response: HttpResponse = self.get_response(request)
