bd4bd8e6081b9cc551bd4403815954856c1b0aa2
Marius Andra <marius.andra@gmail.com>
2020-12-08 11:27:51 +0100

    [plugin-server] disable just for line

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
         curly: 'error',
     },
     overrides: [
