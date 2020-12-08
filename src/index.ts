cddc2bb433c7efa2f7075bc08f33b700051e517a
Marius Andra <marius.andra@gmail.com>
2020-12-08 11:48:44 +0100

    [plugin-server] just always start

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as yargs from 'yargs'
-import { PluginsServerConfig } from './types'
+import { PluginsServerConfig, PluginsServerConfigKey } from './types'
 import { startPluginsServer } from './server'
 import { makePiscina } from './worker/piscina'
 import { defaultConfig, configHelp } from './config'
@@ -19,30 +19,26 @@ let app: any = yargs
     .option('config', { alias: 'c', describe: 'Config options JSON.', type: 'string' })
 
 for (const [key, value] of Object.entries(defaultConfig)) {
-    app = app.option(key.toLowerCase().replaceAll('_', '-'), {
-        describe: `${configHelp[key] || key} [${value}]`,
+    app = app.option(key.toLowerCase().split('_').join('-'), {
+        describe: `${configHelp[key as PluginsServerConfigKey] || key} [${value}]`,
         type: typeof value,
     })
 }
 
-app = app.help().command({
-    command: ['start', '$0'],
-    describe: 'start the server',
-    handler: ({ config, ...otherArgs }: Argv) => {
-        const parsedConfig: Record<string, any> = config ? JSON.parse(config) : {}
-        for (const [key, value] of Object.entries(otherArgs)) {
-            if (typeof value !== 'undefined') {
-                // convert camelCase argument keys to under_score
-                const newKey = key
-                    .replace(/(?:^|\.?)([A-Z])/g, (x, y) => '_' + y.toUpperCase())
-                    .replace(/^_/, '')
-                    .toUpperCase()
-                if (newKey in defaultConfig) {
-                    parsedConfig[newKey] = value
-                }
-            }
+const { config, ...otherArgs }: Argv = app.help().argv
+
+const parsedConfig: Record<string, any> = config ? JSON.parse(config) : {}
+for (const [key, value] of Object.entries(otherArgs)) {
+    if (typeof value !== 'undefined') {
+        // convert camelCase argument keys to under_score
+        const newKey = key
+            .replace(/(?:^|\.?)([A-Z])/g, (x, y) => '_' + y.toUpperCase())
+            .replace(/^_/, '')
+            .toUpperCase()
+        if (newKey in defaultConfig) {
+            parsedConfig[newKey] = value
         }
-        setLogLevel(parsedConfig.LOG_LEVEL || defaultConfig.LOG_LEVEL)
-        startPluginsServer(parsedConfig as PluginsServerConfig, makePiscina)
-    },
-}).argv
+    }
+}
+setLogLevel(parsedConfig.LOG_LEVEL || defaultConfig.LOG_LEVEL)
+startPluginsServer(parsedConfig as PluginsServerConfig, makePiscina)
