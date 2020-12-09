fc2cf2a45cd440fa6c07f4a44bfcefe52002a81f
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-12-09 13:13:03 +0200

    WIP: make filtering by session recording length possible on ch

--- posthog/models/filters/sessions_filter.py
+++ posthog/models/filters/sessions_filter.py
@@ -10,6 +10,7 @@ RETENTION_DEFAULT_INTERVALS = 11
 
 class SessionsFilter(Filter):
     distinct_id: Optional[str]
+    min_recording_duration: Optional[int]
 
     def __init__(self, data: Optional[Dict[str, Any]] = None, request: Optional[HttpRequest] = None, **kwargs) -> None:
         super().__init__(data, request, **kwargs)
@@ -21,3 +22,8 @@ class SessionsFilter(Filter):
             raise ValueError("You need to define either a data dict or a request")
 
         self.distinct_id = data.get(DISTINCT_ID_FILTER)
+        self.min_recording_duration = data.get("min_recording_duration")
+
+    @property
+    def limit_by_recordings(self):
+        return self.min_recording_duration is not None
