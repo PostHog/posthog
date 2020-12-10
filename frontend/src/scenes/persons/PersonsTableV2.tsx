9f5e42b5bd8a0704c325dd80d68b72638aebbdb8
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-12-10 09:30:30 -0600

    Fix persons page V2 handle person with no distinct IDs (#2731)

--- frontend/src/scenes/persons/PersonsTableV2.tsx
+++ frontend/src/scenes/persons/PersonsTableV2.tsx
@@ -70,13 +70,15 @@ export function PersonsTable({
             render: function Render(_: string, person: PersonType) {
                 return (
                     <div>
-                        <CopyToClipboardInline
-                            explicitValue={person.distinct_ids[0]}
-                            tooltipMessage=""
-                            iconStyle={{ color: 'var(--primary)' }}
-                        >
-                            {midEllipsis(person.distinct_ids[0], 32)}
-                        </CopyToClipboardInline>
+                        {person.distinct_ids.length && (
+                            <CopyToClipboardInline
+                                explicitValue={person.distinct_ids[0]}
+                                tooltipMessage=""
+                                iconStyle={{ color: 'var(--primary)' }}
+                            >
+                                {midEllipsis(person.distinct_ids[0], 32)}
+                            </CopyToClipboardInline>
+                        )}
                     </div>
                 )
             },
