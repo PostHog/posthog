9a7b6918adff40c51e0103fbf9b452d807538545
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-12-11 11:37:46 -0600

    updates isURL utility with tests

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -383,13 +383,13 @@ export function stripHTTP(url: string): string {
     return url
 }
 
-export function isURL(string: string): boolean {
-    if (!string) {
+export function isURL(input: any): boolean {
+    if (!input || typeof input !== 'string') {
         return false
     }
     // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
     const regexp = /^\s*https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
-    return !!string.match?.(regexp)
+    return !!input.match?.(regexp)
 }
 
 export function isEmail(string: string): boolean {
