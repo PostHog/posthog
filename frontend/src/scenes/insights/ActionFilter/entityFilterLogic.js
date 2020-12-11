b8470c4634665d34d7d2cfa4b975269ec7f51bf1
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-12-11 15:11:49 +0200

    when adding rows to entity filters, keep visibility as well

--- frontend/src/scenes/insights/ActionFilter/entityFilterLogic.js
+++ frontend/src/scenes/insights/ActionFilter/entityFilterLogic.js
@@ -50,6 +50,7 @@ export const entityFilterLogic = kea({
         updateFilterProperty: (filter) => ({ properties: filter.properties, index: filter.index }),
         setFilters: (filters) => ({ filters }),
         setLocalFilters: (filters) => ({ filters }),
+        setEntityFilterVisibility: (index, value) => ({ index, value }),
     }),
 
     reducers: ({ props }) => ({
@@ -63,6 +64,12 @@ export const entityFilterLogic = kea({
             toLocalFilters(props.filters),
             {
                 setLocalFilters: (_, { filters }) => toLocalFilters(filters),
+            },
+        ],
+        entityFilterVisible: [
+            {},
+            {
+                setEntityFilterVisibility: (state, { index, value }) => ({ ...state, [index]: value }),
             },
         ],
     }),
@@ -103,9 +110,11 @@ export const entityFilterLogic = kea({
         addFilter: () => {
             if (values.localFilters.length > 0) {
                 const lastFilter = values.localFilters[values.localFilters.length - 1]
-                actions.setFilters([...values.localFilters, { ...lastFilter, order: values.localFilters.length }])
+                const order = lastFilter.order + 1
+                actions.setFilters([...values.localFilters, { ...lastFilter, order }])
+                actions.setEntityFilterVisibility(order, values.entityFilterVisible[lastFilter.order])
             } else {
-                actions.setFilters([{ id: null, type: EntityTypes.NEW_ENTITY, order: values.localFilters.length }])
+                actions.setFilters([{ id: null, type: EntityTypes.NEW_ENTITY, order: 0 }])
             }
         },
         setFilters: ({ filters }) => {
