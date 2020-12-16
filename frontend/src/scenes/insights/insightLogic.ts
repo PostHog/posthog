b75ce076c601f953d777b7ade5ee743f7e11e6c0
Paolo D'Amico <paolodamico@users.noreply.github.com>
2020-12-15 18:30:52 -0600

    insight viewed event

--- frontend/src/scenes/insights/insightLogic.ts
+++ frontend/src/scenes/insights/insightLogic.ts
@@ -1,5 +1,6 @@
 import { kea } from 'kea'
 import { toParams, fromParams } from 'lib/utils'
+import posthog from 'posthog-js'
 import { insightLogicType } from 'types/scenes/insights/insightLogicType'
 
 export const ViewType = {
@@ -20,6 +21,7 @@ export const insightLogic = kea<insightLogicType>({
         updateActiveView: (type) => ({ type }),
         setCachedUrl: (type, url) => ({ type, url }),
         setAllFilters: (filters) => ({ filters }),
+        setNotFirstLoad: () => {},
     }),
     reducers: () => ({
         cachedUrls: [
@@ -43,6 +45,58 @@ export const insightLogic = kea<insightLogicType>({
                 setAllFilters: (_, { filters }) => filters,
             },
         ],
+        /*
+        isFirstLoad determines if this is the first graph being shown in the session (used for analytics)
+        */
+        isFirstLoad: [
+            true,
+            {
+                setNotFirstLoad: () => false,
+            },
+        ],
+    }),
+    listeners: ({ values, actions }) => ({
+        setAllFilters: (payload) => {
+            // Reports `insight viewed` event
+            const { insight, display, interval, date_from, date_to } = payload.filters
+            const properties: Record<string, any> = {
+                is_first_load: values.isFirstLoad,
+                insight,
+                display,
+                interval,
+                date_from,
+                date_to,
+                filters_count: payload.filters.properties.length, // Only counts general filters (i.e. not per-event filters)
+                events_count: payload.filters.events ? payload.filters.events.length : undefined, // Number of lines in insights graph; number of steps in funnel
+            }
+
+            // Custom properties for each insight
+            if (insight === 'TRENDS') {
+                properties.breakdown_type = payload.filters.breakdown_type
+                properties.shown_as = payload.filters.shown_as
+            } else if (insight === 'SESSIONS') {
+                properties.session_distribution = payload.filters.session
+            } else if (insight === 'FUNNELS') {
+                properties.session_distribution = payload.filters.session
+            } else if (insight === 'RETENTION') {
+                properties.period = payload.filters.period
+                properties.date_to = payload.filters.date_to
+                properties.retention_type = payload.filters.retentionType
+                properties.same_retention_and_cohortizing_event =
+                    payload.filters.returningEntity.events[0].id === payload.filters.startEntity.events[0].id
+            } else if (insight === 'PATHS') {
+                properties.path_type = payload.filters.path_type
+            }
+
+            const sanitizedProperties: Record<string, any> = {}
+            Object.entries(properties).map(([key, value]) => {
+                if (value !== undefined) {
+                    sanitizedProperties[key] = value
+                }
+            })
+            posthog.capture('insight viewed', sanitizedProperties)
+            actions.setNotFirstLoad()
+        },
     }),
     actionToUrl: ({ actions, values }) => ({
         setActiveView: ({ type }) => {
