a5744ef0b7acd8cea8ba441f862ce927ba7ea3ce
Eric <eeoneric@gmail.com>
2021-01-05 15:10:09 -0500

    remove unnecessary imports

--- posthog/tasks/test/test_update_cache.py
+++ posthog/tasks/test/test_update_cache.py
@@ -1,14 +1,10 @@
-import json
 from datetime import timedelta
 from unittest.mock import MagicMock, patch
 
 from django.core.cache import cache
 from django.utils.timezone import now
-from freezegun import freeze_time
 
-from ee.clickhouse.queries.util import get_earliest_timestamp
 from posthog.models import Dashboard, DashboardItem, Event, Filter
-from posthog.models.filters.path_filter import PathFilter
 from posthog.models.filters.retention_filter import RetentionFilter
 from posthog.models.filters.stickiness_filter import StickinessFilter
 from posthog.tasks.update_cache import update_cache_item, update_cached_items
