4a8cfc1ae18bc238e146f436db4747e58f9a9193
Michael Matloka <dev@twixes.com>
2021-01-08 12:21:37 +0100

    Don't .only() Team in EE process_event to optimize REST hook firing

--- ee/clickhouse/process_event.py
+++ ee/clickhouse/process_event.py
@@ -54,19 +54,7 @@ def _capture_ee(
             for index, el in enumerate(elements)
         ]
 
-    team = (
-        Team.objects.only(
-            "slack_incoming_webhook",
-            "event_names",
-            "event_properties",
-            "event_names_with_usage",
-            "event_properties_with_usage",
-            "anonymize_ips",
-            "organization_id",
-        )
-        .select_related("organization")
-        .get(pk=team_id)
-    )
+    team = Team.objects.select_related("organization").get(pk=team_id)
 
     if not team.anonymize_ips and "$ip" not in properties:
         properties["$ip"] = ip
