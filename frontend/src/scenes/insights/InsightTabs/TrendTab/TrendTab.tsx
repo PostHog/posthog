c46262b68bc630d2789cef05838daed50152e5a4
Eric <eeoneric@gmail.com>
2021-01-08 10:38:33 -0500

    put separation behind feature flag

--- frontend/src/scenes/insights/InsightTabs/TrendTab/TrendTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/TrendTab/TrendTab.tsx
@@ -10,12 +10,55 @@ import { InfoCircleOutlined } from '@ant-design/icons'
 import { trendsLogic } from '../../trendsLogic'
 import { ViewType } from '../../insightLogic'
 import { LIFECYCLE } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
 export function TrendTab(): JSX.Element {
     const { filters } = useValues(trendsLogic({ dashboardItemId: null, view: ViewType.TRENDS }))
     const { setFilters } = useActions(trendsLogic({ dashboardItemId: null, view: ViewType.TRENDS }))
+    const { featureFlags } = useValues(featureFlagLogic)
 
-    return (
+    return featureFlags['remove-shownas'] ? (
+        <>
+            <h4 className="secondary">{'Actions & Events'}</h4>
+            <ActionFilter
+                filters={filters}
+                setFilters={(payload): void => setFilters(payload)}
+                typeKey="trends"
+                hideMathSelector={filters.shown_as === LIFECYCLE}
+                copy="Add graph series"
+                disabled={filters.shown_as === LIFECYCLE && (filters.events?.length || filters.actions?.length)}
+            />
+            <hr />
+            <h4 className="secondary">Filters</h4>
+            <PropertyFilters pageKey="trends-filters" />
+            {filters.insight === ViewType.TRENDS && <hr />}
+            {filters.insight === ViewType.TRENDS && (
+                <h4 className="secondary">
+                    Break down by
+                    <Tooltip
+                        placement="right"
+                        title="Use breakdown to see the volume of events for each variation of that property. For example, breaking down by $current_url will give you the event volume for each url your users have visited."
+                    >
+                        <InfoCircleOutlined className="info-indicator" />
+                    </Tooltip>
+                </h4>
+            )}
+            {filters.insight === ViewType.TRENDS && (
+                <Row>
+                    <BreakdownFilter
+                        filters={filters}
+                        onChange={(breakdown, breakdown_type): void => setFilters({ breakdown, breakdown_type })}
+                    />
+                    {filters.breakdown && (
+                        <CloseButton
+                            onClick={(): void => setFilters({ breakdown: false, breakdown_type: null })}
+                            style={{ marginTop: 1, marginLeft: 10 }}
+                        />
+                    )}
+                </Row>
+            )}
+        </>
+    ) : (
         <>
             <h4 className="secondary">{'Actions & Events'}</h4>
             <ActionFilter
