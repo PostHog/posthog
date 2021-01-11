e1d25dcdb47f9d40ee0aa59f6347db9b4f3eee11
Michael Matloka <dev@twixes.com>
2021-01-11 01:58:16 +0100

    Add user_id to Papercups customer metadata

--- frontend/src/lib/components/Papercups.tsx
+++ frontend/src/lib/components/Papercups.tsx
@@ -17,6 +17,7 @@ export function Papercups({ user }: { user: UserType | null }): JSX.Element {
                     name: user.name,
                     external_id: user.distinct_id,
                     metadata: {
+                        user_id: user.id,
                         organization_name: user.organization?.name,
                         organization_id: user.organization?.id,
                         organization_plan: user.organization?.billing_plan,
