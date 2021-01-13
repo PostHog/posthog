5e7f980f9c55967ba28cfda7af0df0c3e789719b
Michael Matloka <dev@twixes.com>
2021-01-13 03:10:36 +0100

    Move around PLUGIN_SERVER_INGESTION_HANDOFF ifs

--- ee/clickhouse/process_event.py
+++ ee/clickhouse/process_event.py
@@ -100,7 +100,7 @@ def handle_timestamp(data: dict, now: datetime.datetime, sent_at: Optional[datet
     return now_datetime
 
 
-if is_ee_enabled() and not settings.PLUGIN_SERVER_INGESTION_HANDOFF:
+if is_ee_enabled():
 
     def process_event_ee(
         distinct_id: str,
--- posthog/api/capture.py
+++ posthog/api/capture.py
@@ -190,7 +190,7 @@ def get_event(request):
         event_uuid = UUIDT()
 
         if is_ee_enabled():
-            process_event_ee(
+            log_event(
                 distinct_id=distinct_id,
                 ip=get_ip_address(request),
                 site_url=request.build_absolute_uri("/")[:-1],
@@ -200,9 +200,20 @@ def get_event(request):
                 sent_at=sent_at,
                 event_uuid=event_uuid,
             )
+            if not settings.PLUGIN_SERVER_INGESTION_HANDOFF:
+                process_event_ee(
+                    distinct_id=distinct_id,
+                    ip=get_ip_address(request),
+                    site_url=request.build_absolute_uri("/")[:-1],
+                    data=event,
+                    team_id=team.id,
+                    now=now,
+                    sent_at=sent_at,
+                    event_uuid=event_uuid,
+                )
         else:
             task_name = "posthog.tasks.process_event.process_event"
-            if team.plugins_opt_in:
+            if settings.PLUGIN_SERVER_INGESTION_HANDOFF or team.plugins_opt_in:
                 task_name += "_with_plugins"
                 celery_queue = settings.PLUGINS_CELERY_QUEUE
             else:
@@ -220,18 +231,6 @@ def get_event(request):
                     now.isoformat(),
                     sent_at,
                 ],
-            )
-
-        if is_ee_enabled():
-            log_event(
-                distinct_id=distinct_id,
-                ip=get_ip_address(request),
-                site_url=request.build_absolute_uri("/")[:-1],
-                data=event,
-                team_id=team.id,
-                now=now,
-                sent_at=sent_at,
-                event_uuid=event_uuid,
             )
     timer.stop("event_endpoint")
     return cors_response(request, JsonResponse({"status": 1}))
