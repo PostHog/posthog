182f8dfa09a9b3daaa61708c790d128fc4be46e6
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-01-13 14:00:20 +0200

    Fix database model

--- posthog/migrations/0112_sessions_filter.py
+++ posthog/migrations/0112_sessions_filter.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name="SessionsFilter",
             fields=[
                 ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
-                ("name", models.CharField(blank=True, max_length=400, null=True)),
+                ("name", models.CharField(blank=True, max_length=400, null=False)),
                 ("created_at", models.DateTimeField(auto_now_add=True)),
                 ("updated_at", models.DateTimeField(auto_now=True)),
                 ("filters", django.contrib.postgres.fields.jsonb.JSONField(default=dict)),
--- posthog/models/sessions_filter.py
+++ posthog/models/sessions_filter.py
@@ -8,7 +8,7 @@ class SessionsFilter(models.Model):
             models.Index(fields=["team_id", "name"]),
         ]
 
-    name: models.CharField = models.CharField(max_length=400, null=True, blank=True)
+    name: models.CharField = models.CharField(max_length=400, null=False, blank=True)
     team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True, blank=True)
     created_by: models.ForeignKey = models.ForeignKey("User", on_delete=models.SET_NULL, null=True, blank=True)
