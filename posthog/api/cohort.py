a50eff61909ce3472a77e607b226a6b09bc49b67
Eric <eeoneric@gmail.com>
2021-01-13 15:19:48 -0500

    initial scaffold for cohort static logic

--- posthog/api/cohort.py
+++ posthog/api/cohort.py
@@ -3,15 +3,18 @@ from typing import Any, Dict, Optional
 
 import posthoganalytics
 from django.db.models import Count, QuerySet
+from django.db.models.sql.query import Query
 from rest_framework import serializers, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from posthog.api.action import calculate_people, filter_by_type
 from posthog.api.routing import StructuredViewSetMixin
 from posthog.api.user import UserSerializer
 from posthog.models import Cohort
+from posthog.models.filters.filter import Filter
 from posthog.permissions import ProjectMembershipNecessaryPermissions
 from posthog.tasks.calculate_cohort import calculate_cohort, calculate_cohort_from_csv
 
@@ -42,6 +45,9 @@ class CohortSerializer(serializers.ModelSerializer):
         distinct_ids_and_emails = [row[0] for row in reader if len(row) > 0 and row]
         calculate_cohort_from_csv.delay(cohort.pk, distinct_ids_and_emails)
 
+    def _handle_static(self, people: QuerySet, cohort: Cohort) -> None:
+        calculate_cohort_from_csv.delay(cohort.pk, [person.distinct_id for person in people])
+
     def create(self, validated_data: Dict, *args: Any, **kwargs: Any) -> Cohort:
         request = self.context["request"]
         validated_data["created_by"] = request.user
@@ -50,6 +56,13 @@ class CohortSerializer(serializers.ModelSerializer):
 
         if request.FILES.get("csv"):
             self._handle_csv(request.FILES["csv"], cohort)
+
+        if request.GET.get("static"):
+            filter = Filter(request=request)
+            team = request.user.team
+            events = filter_by_type(team=team, filter=filter)
+            people = calculate_people(team=team, events=events, filter=filter)
+            self._handle_static(people, cohort)
 
         posthoganalytics.capture(request.user.distinct_id, "cohort created", cohort.get_analytics_metadata())
         if not cohort.is_static:
