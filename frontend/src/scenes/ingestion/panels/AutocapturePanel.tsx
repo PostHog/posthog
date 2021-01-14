44d811ef6612afe0bc26047a5394508bf95fb605
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-01-14 14:21:07 -0600

    initial JS refactoring + JS library adjustments

--- frontend/src/scenes/ingestion/panels/AutocapturePanel.tsx
+++ frontend/src/scenes/ingestion/panels/AutocapturePanel.tsx
@@ -1,13 +1,16 @@
 import { CardContainer } from 'scenes/ingestion/CardContainer'
-import { Row } from 'antd'
-import { JSSnippet } from 'lib/components/JSSnippet'
+import { Collapse, Tag } from 'antd'
 import React from 'react'
 import { useActions, useValues } from 'kea'
 import { ingestionLogic } from 'scenes/ingestion/ingestionLogic'
+import { BulbOutlined } from '@ant-design/icons'
+import { Link } from 'lib/components/Link'
+import { JSInstructions } from '../frameworks'
+import { JSSnippet } from 'lib/components/JSSnippet'
 
 export function AutocapturePanel(): JSX.Element {
     const { index, totalSteps } = useValues(ingestionLogic)
-    const { setPlatform, setCustomEvent, setVerify } = useActions(ingestionLogic)
+    const { setPlatform, setVerify } = useActions(ingestionLogic)
     return (
         <CardContainer
             index={index}
@@ -16,29 +19,55 @@ export function AutocapturePanel(): JSX.Element {
             onSubmit={() => setVerify(true)}
             onBack={() => setPlatform(null)}
         >
-            <Row style={{ marginLeft: -5 }} justify="space-between" align="middle">
-                <h2 style={{ color: 'black', marginLeft: 8 }}>{'Autocapture'}</h2>
-                <b
-                    style={{ marginLeft: 5, color: '#007bff', marginBottom: 10, marginRight: 0 }}
-                    onClick={() => setCustomEvent(true)}
-                    className="button-border clickable"
+            <Collapse>
+                <Collapse.Panel
+                    header={
+                        <>
+                            <BulbOutlined style={{ color: 'var(--warning)' }} /> <b>Just exploring?</b> Immediately run
+                            PostHog in your website for some initial exploring.
+                        </>
+                    }
+                    key="1"
                 >
-                    I also want to capture custom events
-                </b>
-            </Row>
-            <p className="prompt-text">
-                Since you're running a web application, we suggest using our header snippet. This snippet will
-                automatically capture page views, page leaves, and interactions with specific elements (
-                {'<a>, <button>, <input>, <textarea>, <form>'}).
-            </p>
-            <p className="prompt-text">
-                Just insert this snippet into your website where you configure {'<head> or <meta>'} tags.
-            </p>
-            <JSSnippet />
-            <h2>Send an Event</h2>
-            <p className="prompt-text">
-                Once you've inserted the snippet, click on a button or form on your website to send an event!
-            </p>
+                    Hello world!
+                </Collapse.Panel>
+            </Collapse>
+            <div style={{ marginTop: 16 }}>
+                <h2>
+                    Option 1. Autocapture <Tag color="green">Recommended</Tag>
+                </h2>
+                <p>
+                    Faster option. Particularly recommended for new projects where you don't know what your analytics
+                    will look like just yet. Just add this snippet to your website and we'll{' '}
+                    <b>automatically capture page views, sessions and all relevant interactions</b> within your website.{' '}
+                    <Link to="https://posthog.com/product-features/event-autocapture" target="_blank" rel="noopener">
+                        Learn more
+                    </Link>
+                    .
+                </p>
+                <b>Steps:</b>
+                <ol>
+                    <li>
+                        Insert this snippet in your website within the <code>&lt;head&gt;</code> tag. <JSSnippet />
+                    </li>
+                    <li>
+                        <b>Visit your site</b> to generate some initial events.
+                    </li>
+                </ol>
+            </div>
+            <div style={{ marginTop: 32 }}>
+                <h2>Option 2. Javascript Library</h2>
+                <p>
+                    Use this option if you want more granular control of how PostHog runs in your website and the events
+                    you capture. Recommended for teams with more stable products and more defined analytics
+                    requirements.{' '}
+                    <Link to="https://posthog.com/docs/integrations/js-integration/" target="_blank" rel="noopener">
+                        Learn more
+                    </Link>
+                    .
+                    <JSInstructions />
+                </p>
+            </div>
         </CardContainer>
     )
 }
