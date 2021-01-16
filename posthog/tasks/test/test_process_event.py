41c2b627572d910a0a40d53158d4a27436c416bc
Tim Glaser <tim@glsr.nl>
2021-01-16 11:11:26 +0100

    fix num queries again again

--- posthog/tasks/test/test_process_event.py
+++ posthog/tasks/test/test_process_event.py
@@ -94,6 +94,9 @@ def test_process_event_factory(
             )
 
             # See effect of caching
+            num_queries = 22
+            if settings.EE_AVAILABLE:  # extra queries to check for hooks
+                num_queries += 2
             with self.assertNumQueries(num_queries - 4):
                 process_event(
                     2,
