0ffd243cf4874390520ed5922472d01872354593
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-01-18 21:42:52 +0200

    Use feature flags

--- frontend/src/scenes/sessions/Sessions.tsx
+++ frontend/src/scenes/sessions/Sessions.tsx
@@ -4,10 +4,13 @@ import { hot } from 'react-hot-loader/root'
 import { SavedFiltersMenu } from 'scenes/sessions/filters/SavedFiltersMenu'
 import { PageHeader } from 'lib/components/PageHeader'
 import { Divider } from 'antd'
+import { useValues } from 'kea'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
 export const Sessions = hot(_Sessions)
 function _Sessions(): JSX.Element {
-    if (true) {
+    const { featureFlags } = useValues(featureFlagLogic)
+    if (featureFlags['filter_by_session_props']) {
         return (
             <div style={{ display: 'flex', flexDirection: 'row' }}>
                 <div>
