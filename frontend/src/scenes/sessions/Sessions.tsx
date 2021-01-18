917d28fd59b57f667cc444066818e008e2c9578e
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-01-18 11:44:00 +0200

    Filter new menu

--- frontend/src/scenes/sessions/Sessions.tsx
+++ frontend/src/scenes/sessions/Sessions.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import { SessionsView } from './SessionsView'
 import { hot } from 'react-hot-loader/root'
+import { SavedFiltersMenu } from 'scenes/sessions/filters/SavedFiltersMenu'
+import { PageHeader } from 'lib/components/PageHeader'
+import { Divider } from 'antd'
 
 export const Sessions = hot(_Sessions)
 function _Sessions(): JSX.Element {
-    return <SessionsView />
+    if (true) {
+        return (
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <div>
+                    <PageHeader title="Sessions" />
+                    <SavedFiltersMenu />
+                </div>
+                <Divider type="vertical" className="sessions-divider" />
+                <div style={{ marginTop: 30 }}>
+                    <SessionsView />
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <>
+                <PageHeader title="Sessions" />
+                <SessionsView />
+            </>
+        )
+    }
 }
