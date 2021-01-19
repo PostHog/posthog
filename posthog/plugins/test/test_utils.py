45811dd9e650d7ff161de5c7f5aa1a99573452f6
Marius Andra <marius.andra@gmail.com>
2021-01-19 11:07:41 +0100

    Support plugins installed from GitLab (#2970)
    * support gitlab urls
    
    * Fix error messages
    
    * Fix type
    
    * do dark magic and sacrifice a goat to make mypy happy
    
    Co-authored-by: Michael Matloka <dev@twixes.com>

--- posthog/plugins/test/test_utils.py
+++ posthog/plugins/test/test_utils.py
@@ -5,12 +5,12 @@ from posthog.plugins.utils import download_plugin_archive, get_json_from_archive
 from posthog.test.base import BaseTest
 
 from .mock import mocked_plugin_requests_get
-from .plugin_archives import HELLO_WORLD_PLUGIN_GITHUB_ZIP, HELLO_WORLD_PLUGIN_NPM_TGZ
+from .plugin_archives import HELLO_WORLD_PLUGIN_GITHUB_ZIP, HELLO_WORLD_PLUGIN_GITLAB_ZIP, HELLO_WORLD_PLUGIN_NPM_TGZ
 
 
 @mock.patch("requests.get", side_effect=mocked_plugin_requests_get)
 class TestPluginsUtils(BaseTest):
-    def test_parse_urls(self, mock_get):
+    def test_parse_github_urls(self, mock_get):
         parsed_url = parse_url("https://github.com/PostHog/posthog")
         self.assertEqual(parsed_url["type"], "github")
         self.assertEqual(parsed_url["user"], "PostHog")
@@ -67,6 +67,37 @@ class TestPluginsUtils(BaseTest):
         self.assertEqual(parsed_url["repo"], "posthog")
         self.assertEqual(parsed_url["tag"], "82c9218ee40f561b7f37a22d6b6a0ca82887ee3e")
 
+    def test_parse_gitlab_urls(self, mock_get):
+        parsed_url = parse_url("https://gitlab.com/mariusandra/helloworldplugin")
+        self.assertEqual(parsed_url["type"], "gitlab")
+        self.assertEqual(parsed_url["project"], "mariusandra/helloworldplugin")
+        self.assertEqual(parsed_url.get("tag", None), None)
+
+        parsed_url = parse_url(
+            "https://gitlab.com/gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline/-/tree/master"
+        )
+        self.assertEqual(parsed_url["project"], "gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline")
+        self.assertEqual(parsed_url["tag"], "master")
+
+        parsed_url = parse_url(
+            "https://gitlab.com/gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline/-/tree/2b6494bdf8ad35073aafe36ca8a1bdfaf3dc72d1"
+        )
+        self.assertEqual(parsed_url["project"], "gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline")
+        self.assertEqual(parsed_url["tag"], "2b6494bdf8ad35073aafe36ca8a1bdfaf3dc72d1")
+
+        parsed_url = parse_url(
+            "https://gitlab.com/gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline/-/commit/2b6494bdf8ad35073aafe36ca8a1bdfaf3dc72d1"
+        )
+        self.assertEqual(parsed_url["project"], "gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline")
+        self.assertEqual(parsed_url["tag"], "2b6494bdf8ad35073aafe36ca8a1bdfaf3dc72d1")
+
+        parsed_url = parse_url(
+            "https://gitlab.com/gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline/-/archive/master/openshift-custom-pipeline-master.zip"
+        )
+        self.assertEqual(parsed_url["project"], "gitlab-org/gl-openshift/openshift-demos/openshift-custom-pipeline")
+        self.assertEqual(parsed_url["tag"], "master")
+
+    def test_parse_npm_urls(self, mock_get):
         parsed_url = parse_url("https://www.npmjs.com/package/posthog-helloworld-plugin")
         self.assertEqual(parsed_url["type"], "npm")
         self.assertEqual(parsed_url["pkg"], "posthog-helloworld-plugin")
@@ -89,7 +120,7 @@ class TestPluginsUtils(BaseTest):
         self.assertEqual(parsed_url["pkg"], "posthog-helloworld-plugin")
         self.assertEqual(parsed_url["version"], "0.0.0")
 
-    def test_download_plugin_archive(self, mock_get):
+    def test_download_plugin_archive_github(self, mock_get):
         plugin_github_zip_1 = download_plugin_archive(
             "https://www.github.com/PostHog/helloworldplugin/commit/82c9218ee40f561b7f37a22d6b6a0ca82887ee3e",
             HELLO_WORLD_PLUGIN_GITHUB_ZIP[0],
@@ -101,6 +132,14 @@ class TestPluginsUtils(BaseTest):
         )
         self.assertEqual(plugin_github_zip_2, base64.b64decode(HELLO_WORLD_PLUGIN_GITHUB_ZIP[1]))
 
+    def test_download_plugin_archive_gitlab(self, mock_get):
+        plugin_gitlab = download_plugin_archive(
+            "https://www.gitlab.com/mariusandra/helloworldplugin/-/commit/ff78cbe1d70316055c610a962a8355a4616d874b",
+            HELLO_WORLD_PLUGIN_GITLAB_ZIP[0],
+        )
+        self.assertEqual(plugin_gitlab, base64.b64decode(HELLO_WORLD_PLUGIN_GITLAB_ZIP[1]))
+
+    def test_download_plugin_archive_npm(self, mock_get):
         plugin_npm_tgz = download_plugin_archive("https://www.npmjs.com/package/posthog-helloworld-plugin/v/0.0.0")
         self.assertEqual(plugin_npm_tgz, base64.b64decode(HELLO_WORLD_PLUGIN_NPM_TGZ[1]))
 
@@ -108,6 +147,11 @@ class TestPluginsUtils(BaseTest):
         plugin_json_zip = get_json_from_archive(base64.b64decode(HELLO_WORLD_PLUGIN_GITHUB_ZIP[1]), "plugin.json")
         self.assertEqual(plugin_json_zip["name"], "helloworldplugin")
         self.assertEqual(plugin_json_zip["url"], "https://github.com/PostHog/helloworldplugin")
+        self.assertEqual(plugin_json_zip["description"], "Greet the World and Foo a Bar, JS edition!")
+
+        plugin_json_zip = get_json_from_archive(base64.b64decode(HELLO_WORLD_PLUGIN_GITLAB_ZIP[1]), "plugin.json")
+        self.assertEqual(plugin_json_zip["name"], "hellojsplugin")
+        self.assertEqual(plugin_json_zip["url"], "https://github.com/PosthHog/helloworldplugin")
         self.assertEqual(plugin_json_zip["description"], "Greet the World and Foo a Bar, JS edition!")
 
         plugin_json_tgz = get_json_from_archive(base64.b64decode(HELLO_WORLD_PLUGIN_NPM_TGZ[1]), "plugin.json")
--- posthog/plugins/utils.py
+++ posthog/plugins/utils.py
@@ -4,12 +4,13 @@ import os
 import re
 import tarfile
 from typing import Dict, Optional
+from urllib.parse import quote
 from zipfile import BadZipFile, ZipFile
 
 import requests
 
 
-def parse_github_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, str]]:
+def parse_github_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, Optional[str]]]:
     url = url.strip("/")
     match = re.search(
         r"^https?:\/\/(?:www\.)?github\.com\/([A-Za-z0-9_.-]+)\/([A-Za-z0-9_.-]+)((\/commit|\/tree|\/releases\/tag)\/([A-Za-z0-9_.\-\/]+))?$",
@@ -22,7 +23,12 @@ def parse_github_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, s
         )
     if not match:
         return None
-    parsed = {"type": "github", "user": match.group(1), "repo": match.group(2), "tag": match.group(5)}
+    parsed: Dict[str, Optional[str]] = {
+        "type": "github",
+        "user": match.group(1),
+        "repo": match.group(2),
+        "tag": match.group(5),
+    }
     parsed["root_url"] = "https://github.com/{}/{}".format(parsed["user"], parsed["repo"])
     if get_latest_if_none and not parsed["tag"]:
         try:
@@ -38,11 +44,42 @@ def parse_github_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, s
     return parsed
 
 
-def parse_npm_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, str]]:
+def parse_gitlab_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, Optional[str]]]:
+    url = url.strip("/")
+    match = re.search(r"^https?:\/\/(?:www\.)?gitlab\.com\/([A-Za-z0-9_.\-\/]+)$", url)
+    if not match:
+        return None
+
+    parsed: Dict[str, Optional[str]] = {"type": "gitlab", "project": match.group(1), "tag": None}
+
+    if parsed["project"] is None:  # really just needed for mypy
+        return None
+
+    if "/-/" in parsed["project"]:
+        project, path = parsed["project"].split("/-/")
+        parsed["project"] = project
+        parsed["tag"] = path.split("/")[1]
+
+    parsed["root_url"] = "https://gitlab.com/{}".format(parsed["project"])
+    if get_latest_if_none and not parsed["tag"]:
+        try:
+            commits_url = "https://gitlab.com/api/v4/projects/{}/repository/commits".format(quote(parsed["project"]))
+            commits = requests.get(commits_url).json()
+            if len(commits) > 0 and commits[0].get("web_url", None):
+                return parse_gitlab_url(commits[0]["web_url"])
+            raise
+        except Exception:
+            raise Exception("Could not get latest commit for: {}".format(parsed["root_url"]))
+    if parsed["tag"]:
+        parsed["tagged_url"] = "https://gitlab.com/{}/-/tree/{}".format(parsed["project"], parsed["tag"])
+    return parsed
+
+
+def parse_npm_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, Optional[str]]]:
     match = re.search(r"^https?:\/\/(?:www\.)?npmjs\.com\/package\/([a-z0-9_-]+)\/?(v\/([A-Za-z0-9_.-]+)\/?|)$", url)
     if not match:
         return None
-    parsed = {"type": "npm", "pkg": match.group(1), "version": match.group(3)}
+    parsed: Dict[str, Optional[str]] = {"type": "npm", "pkg": match.group(1), "version": match.group(3)}
     parsed["root_url"] = "https://www.npmjs.com/package/{}".format(parsed["pkg"])
     if get_latest_if_none and not parsed["version"]:
         try:
@@ -55,14 +92,17 @@ def parse_npm_url(url: str, get_latest_if_none=False) -> Optional[Dict[str, str]
     return parsed
 
 
-def parse_url(url: str, get_latest_if_none=False) -> Dict[str, str]:
+def parse_url(url: str, get_latest_if_none=False) -> Dict[str, Optional[str]]:
     parsed_url = parse_github_url(url, get_latest_if_none)
     if parsed_url:
         return parsed_url
     parsed_url = parse_npm_url(url, get_latest_if_none)
     if parsed_url:
         return parsed_url
-    raise Exception("Must be a Github Repository or NPM package URL!")
+    parsed_url = parse_gitlab_url(url, get_latest_if_none)
+    if parsed_url:
+        return parsed_url
+    raise Exception("Must be a GitHub/GitLab repository or npm package URL!")
 
 
 # passing `tag` overrides whatever is in the URL
@@ -71,9 +111,17 @@ def download_plugin_archive(url: str, tag: Optional[str] = None):
 
     if parsed_url["type"] == "github":
         if not (tag or parsed_url.get("tag", None)):
-            raise Exception("No Github tag given!")
+            raise Exception("No GitHub tag given!")
         url = "https://github.com/{user}/{repo}/archive/{tag}.zip".format(
             user=parsed_url["user"], repo=parsed_url["repo"], tag=tag or parsed_url["tag"]
+        )
+    elif parsed_url["type"] == "gitlab":
+        url_tag = tag or parsed_url.get("tag", None)
+        url_project = parsed_url["project"]
+        if not url_tag or not url_project:
+            raise Exception("No GitLab tag or project given!")
+        url = "https://gitlab.com/{project}/-/archive/{tag}/{repo}-{tag}.zip".format(
+            project=url_project, repo=url_project.split("/")[-1], tag=url_tag
         )
     elif parsed_url["type"] == "npm":
         if not (tag or parsed_url.get("version", None)):
