4f137f7c44a557ff7408bcda55aacdb0bae7d669
Tim Glaser <tim@glsr.nl>
2021-01-20 14:49:11 +0100

    Fix multiple actions in funnel

--- posthog/queries/test/test_funnel.py
+++ posthog/queries/test/test_funnel.py
@@ -299,8 +299,36 @@ def funnel_test_factory(Funnel, event_factory, person_factory):
             self.assertEqual(result[1]["count"], 1)
             self.assertEqual(result[2]["count"], 1)
 
+        def test_funnel_multiple_actions(self):
+            # we had an issue on clickhouse where multiple actions with different property filters would incorrectly grab only the last
+            # properties.
+            # This test prevents a regression
+            person_factory(distinct_ids=["person1"], team_id=self.team.pk)
+            event_factory(distinct_id="person1", event="event1", team=self.team)
+            event_factory(distinct_id="person1", event="event2", properties={"test_prop": "a"}, team=self.team)
+
+            action1 = Action.objects.create(team_id=self.team.pk, name="event2")
+            ActionStep.objects.create(action=action1, event="event2", properties=[{"key": "test_prop", "value": "a"}])
+            action1.calculate_events()
+            action2 = Action.objects.create(team_id=self.team.pk, name="event2")
+            ActionStep.objects.create(action=action2, event="event2", properties=[{"key": "test_prop", "value": "c"}])
+            action2.calculate_events()
+
+            result = Funnel(
+                filter=Filter(
+                    data={
+                        "events": [{"id": "event1", "order": 0}],
+                        "actions": [{"id": action1.pk, "order": 1,}, {"id": action2.pk, "order": 2,},],
+                    }
+                ),
+                team=self.team,
+            ).run()
+            self.assertEqual(result[0]["count"], 1)
+            self.assertEqual(result[1]["count"], 1)
+            self.assertEqual(result[2]["count"], 0)
+
     return TestGetFunnel
 
 
-class DjangoFunnelTest(funnel_test_factory(Funnel, Event.objects.create, Person.objects.create)):  # type: ignore
+class TestFunnel(funnel_test_factory(Funnel, Event.objects.create, Person.objects.create)):  # type: ignore
     pass
