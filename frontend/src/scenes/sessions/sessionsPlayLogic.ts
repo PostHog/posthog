819635ea93270d03ffb8203e5284429e5e8be1cb
Eric <eeoneric@gmail.com>
2021-01-21 11:15:37 -0500

    patch null check

--- frontend/src/scenes/sessions/sessionsPlayLogic.ts
+++ frontend/src/scenes/sessions/sessionsPlayLogic.ts
@@ -152,7 +152,7 @@ export const sessionsPlayLogic = kea<sessionsPlayLogicType<SessionPlayerData, Ev
         session: [
             (selectors) => [selectors.sessionRecordingId, selectors.sessions],
             (id: SessionRecordingId, sessions: Array<SessionType>): SessionType | null => {
-                const [session] = sessions.filter((s) => s.session_recording_ids.includes(id))
+                const [session] = sessions.filter((s) => (s.session_recording_ids || []).includes(id))
                 return session
             },
         ],
