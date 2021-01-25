a45aca15d4251b878cdc83af6e720fc12a35721a
Eric <eeoneric@gmail.com>
2021-01-25 15:24:25 -0500

    fix time formatting

--- frontend/src/scenes/retention/RetentionTable.js
+++ frontend/src/scenes/retention/RetentionTable.js
@@ -88,7 +88,7 @@ export function RetentionTable({ dashboardItemId = null }) {
                         minWidth: results[selectedRow]?.values[0]?.count === 0 ? '10%' : '90%',
                         fontSize: 16,
                     }}
-                    title={results[selectedRow] ? moment(results[selectedRow].date).format('MMMM d, YYYY') : ''}
+                    title={results[selectedRow] ? moment.utc(results[selectedRow].date).format('MMMM D, YYYY') : ''}
                 >
                     {results && !peopleLoading ? (
                         <div>
