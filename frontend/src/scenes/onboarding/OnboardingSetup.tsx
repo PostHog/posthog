3f4125097f8e923dba57138714fe54bc338f76cf
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-01-27 10:24:59 +0100

    panel headers

--- frontend/src/scenes/onboarding/OnboardingSetup.tsx
+++ frontend/src/scenes/onboarding/OnboardingSetup.tsx
@@ -2,26 +2,81 @@ import { PageHeader } from 'lib/components/PageHeader'
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import { Collapse } from 'antd'
+import './OnboardingSetup.scss'
 
 const { Panel } = Collapse
+
+function PanelHeader({
+    title,
+    caption,
+    stepNumber,
+}: {
+    title: string
+    caption: string | JSX.Element
+    stepNumber: number
+}): JSX.Element {
+    return (
+        <div className="panel-title">
+            <div className="step-number">{stepNumber}</div>
+            <div>
+                <h3 className="l3">{title}</h3>
+                <div className="caption">{caption}</div>
+            </div>
+        </div>
+    )
+}
 
 export const OnboardingSetup = hot(_OnboardingSetup)
 function _OnboardingSetup(): JSX.Element {
     return (
-        <div>
+        <div className="onboarding-setup">
             <PageHeader
                 title="Setup"
                 caption="Get your PostHog instance up and running with all the bells and whistles"
             />
 
-            <Collapse defaultActiveKey={['1']}>
-                <Panel header="This is panel header 1" key="1">
+            <Collapse defaultActiveKey={['1']} expandIconPosition="right">
+                <Panel
+                    header={
+                        <PanelHeader
+                            title="Event Ingestion"
+                            caption="First things first, you need to connect PostHog to your website. You’ll be able to add more sources later."
+                            stepNumber={1}
+                        />
+                    }
+                    key="1"
+                >
                     <p>text</p>
                 </Panel>
-                <Panel header="This is panel header 2" key="2">
+                <Panel
+                    header={
+                        <PanelHeader
+                            title="Configuration"
+                            caption="Tune the settings of PostHog to make sure it works best for you and your team."
+                            stepNumber={2}
+                        />
+                    }
+                    key="2"
+                    disabled
+                >
                     <p>text</p>
                 </Panel>
-                <Panel header="This is panel header 3" key="3">
+                <Panel
+                    header={
+                        <PanelHeader
+                            title="First steps"
+                            caption={
+                                <>
+                                    Walk through some initial steps (like creating your first action). Optional, but you
+                                    get <b>extra free event allocation for every month, forever</b>.
+                                </>
+                            }
+                            stepNumber={3}
+                        />
+                    }
+                    key="3"
+                    disabled
+                >
                     <p>text</p>
                 </Panel>
             </Collapse>
