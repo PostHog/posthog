8087f5f1dd3b3eeeaf7f274ad9d9f015c187d9d1
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2021-02-03 09:22:42 +0000

    Handle corrupted cache (#3169)
    * Handle corrupted cache
    
    * fix errors
    
    * code quality
    
    * minor update

--- posthog/utils.py
+++ posthog/utils.py
@@ -26,6 +26,7 @@ import pytz
 from dateutil import parser
 from dateutil.relativedelta import relativedelta
 from django.conf import settings
+from django.core.cache import cache
 from django.db.models.query import QuerySet
 from django.db.utils import DatabaseError
 from django.http import HttpRequest, HttpResponse
@@ -520,3 +521,15 @@ def get_daterange(
             time_range.append(start_date)
             start_date = (start_date.replace(day=1) + delta).replace(day=1)
     return time_range
+
+
+def get_safe_cache(cache_key: str):
+    try:
+        cached_result = cache.get(cache_key)  # cache.get is safe in most cases
+        return cached_result
+    except:  # if it errors out, the cache is probably corrupted
+        try:
+            cache.delete(cache_key)  # in that case, try to delete the cache
+        except:
+            pass
+    return None
