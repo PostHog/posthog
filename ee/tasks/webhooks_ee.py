5c9f2029e93bfe6663f2c4a33d0291ed4b3da6ca
Michael Matloka <dev@twixes.com>
2021-02-04 12:38:04 +0100

    Address feedback

--- ee/tasks/webhooks_ee.py
+++ ee/tasks/webhooks_ee.py
@@ -1,9 +1,9 @@
-import re
 from typing import Any, Dict, Sequence, cast
 
 import requests
 from celery import Task
 from django.conf import settings
+from sentry_sdk import capture_exception
 
 from posthog.celery import app
 from posthog.models import Action, Event, Team
@@ -17,15 +17,6 @@ def post_event_to_webhook_ee(self: Task, event: Dict[str, Any], team_id: int, si
     postgres_event = None
     try:
         team = Team.objects.select_related("organization").get(pk=team_id)
-        postgres_event = Event.objects.create(
-            event=event["event"],
-            distinct_id=event["distinct_id"],
-            properties=event["properties"],
-            team=team,
-            site_url=site_url,
-            **({"timestamp": event["timestamp"]} if event["timestamp"] else {}),
-            **({"elements": event["elements_list"]} if event["elements_list"] else {})
-        )
         is_zapier_available = team.organization.is_feature_available("zapier")
 
         actionFilters = {"team_id": team_id}
@@ -35,6 +26,16 @@ def post_event_to_webhook_ee(self: Task, event: Dict[str, Any], team_id: int, si
             else:
                 actionFilters["post_to_slack"] = True  # We only need to fire for actions that are posted to webhook URL
         actions = cast(Sequence[Action], Action.objects.filter(**actionFilters).all())
+
+        postgres_event = Event.objects.create(
+            event=event["event"],
+            distinct_id=event["distinct_id"],
+            properties=event["properties"],
+            team=team,
+            site_url=site_url,
+            **({"timestamp": event["timestamp"]} if event["timestamp"] else {}),
+            **({"elements": event["elements_list"]} if event["elements_list"] else {})
+        )
 
         for action in actions:
             qs = Event.objects.filter(pk=postgres_event.pk).query_db_by_action(action)
@@ -57,6 +58,7 @@ def post_event_to_webhook_ee(self: Task, event: Dict[str, Any], team_id: int, si
                     }
                 requests.post(team.slack_incoming_webhook, verify=False, json=message)
     except:
+        capture_exception()
         self.retry(countdown=(2 ** self.request.retries) / 2)
     finally:
         # Ensure that the temporary Postgres event is deleted
