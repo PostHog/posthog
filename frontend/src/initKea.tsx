f8e1157ed5e140b548d9a48362fda4f452b8b147
Tim Glaser <tim@glsr.nl>
2021-02-05 15:52:54 +0100

    Remove any loaders errors

--- frontend/src/initKea.tsx
+++ frontend/src/initKea.tsx
@@ -3,9 +3,6 @@ import localStoragePlugin from 'kea-localstorage'
 import { routerPlugin } from 'kea-router'
 import { loadersPlugin } from 'kea-loaders'
 import { windowValuesPlugin } from 'kea-window-values'
-import { toast } from 'react-toastify'
-import React from 'react'
-import { identifierToHuman } from 'lib/utils'
 
 export function initKea(): void {
     resetContext({
@@ -14,16 +11,7 @@ export function initKea(): void {
             windowValuesPlugin({ window: window }),
             routerPlugin,
             loadersPlugin({
-                onFailure({ error, reducerKey, actionKey }) {
-                    toast.error(
-                        <div>
-                            <h1>Error on {identifierToHuman(reducerKey)}</h1>
-                            <p>
-                                Attempting to {identifierToHuman(actionKey, false)} returned an error:{' '}
-                                <span className="error-details">{error.detail || 'Unknown exception.'}</span>
-                            </p>
-                        </div>
-                    )
+                onFailure({ error }) {
                     window['Sentry'] ? window['Sentry'].captureException(error) : console.error(error)
                 },
             }),
