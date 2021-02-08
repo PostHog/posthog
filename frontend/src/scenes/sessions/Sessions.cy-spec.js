f793905ec759df416edba037cf966421f2d3cba3
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-02-08 11:21:51 +0200

    Test for session filters

--- frontend/src/scenes/sessions/Sessions.cy-spec.js
+++ frontend/src/scenes/sessions/Sessions.cy-spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Sessions } from '~/scenes/sessions/Sessions'
-import { mountPage, setLocation, getSearchParameters } from 'cypress/support/helpers'
+import { Sessions } from './Sessions'
+import * as helpers from 'cypress/support/helpers'
 
 describe('<Sessions />', () => {
     beforeEach(() => {
@@ -9,18 +9,19 @@ describe('<Sessions />', () => {
         cy.intercept('/api/personal_api_keys/', { fixture: 'api/personal_api_keys' })
         cy.intercept('/api/projects/@current/', { fixture: 'api/projects/@current' })
         cy.intercept('/api/person/properties/', { fixture: 'api/person/properties' })
-        cy.interceptLazy('/api/sessions_filter/', () => given.sessionsFilter).as('api_sessions_filter')
-        cy.intercept('/api/event/sessions', { fixture: 'api/event/sessions/demo_sessions' }).as('api_sessions')
+        cy.interceptLazy('/api/event/sessions/', () => given.sessions).as('api_sessions')
+
+        helpers.mockPosthog()
+        helpers.setLocation('/sessions')
     })
 
-    given('sessionsFilter', () => ({ results: [] }))
+    given('sessions', () => ({ fixture: 'api/event/sessions/demo_sessions' }))
 
-    it('loads sessions data', () => {
-        setLocation('/sessions')
-        mountPage(<Sessions />, { featureFlags: ['filter_by_session_props'], cssFile: 'sessions.css' })
+    it('can navigate within sessions page', () => {
+        helpers.mountPage(<Sessions />, { cssFile: 'sessions.css' })
 
         cy.contains('Sessions').should('be.visible')
-        cy.wait('@api_sessions').map(getSearchParameters).should('include', {
+        cy.wait('@api_sessions').map(helpers.getSearchParameters).should('include', {
             date_from: '2021-01-05',
             date_to: '2021-01-05',
             distinct_id: '',
@@ -29,24 +30,42 @@ describe('<Sessions />', () => {
             properties: '[]',
         })
 
+        cy.log('Play all disabled')
+        cy.get('[data-attr="play-all-recordings"]').should('have.attr', 'disabled')
+
+        cy.log('Load more should work')
         cy.get('[data-attr="load-more-sessions"]').click()
         cy.wait('@api_sessions')
-            .map(getSearchParameters)
+            .map(helpers.getSearchParameters)
             .should('include', {
                 date_from: '2021-01-05',
                 date_to: '2021-01-05',
                 pagination: JSON.stringify({ offset: 10 }),
             })
+    })
 
-        // Navigate back/forward in time.
-        // Session recording disabled
-        // Toggle feature flags on-off
+    describe('sessions filters', () => {
+        given('featureFlags', () => ['filter_by_session_props'])
+
+        beforeEach(() => {
+            cy.intercept('/api/sessions_filter/', { fixture: 'api/sessions_filter' }).as('sessions_filter')
+        })
+
+        it('renders sessions filters', () => {
+            helpers.mountPage(<Sessions />, { cssFile: 'sessions.css' })
+
+            cy.wait('@sessions_filter')
+            cy.contains('Unseen recordings').should('be.visible')
+            cy.contains('ChromeUsers').should('be.visible')
+
+            cy.get('[data-attr="sessions-filter-link"]').last().click()
+            cy.wait('@api_sessions')
+
+            cy.get('@api_sessions').map(helpers.getSearchParameters).should('include', {
+                filters: '[{"key":"$browser","type":"person","label":"$browser","value":"Chrome","operator":"exact"}]',
+            })
+
+            cy.pause()
+        })
     })
 })
-
-// const readFixture = (name) => {
-//     import fs from 'fs'
-
-//     const filepath =
-//     return JSON.parse(fs.readFileSync())
-// }
