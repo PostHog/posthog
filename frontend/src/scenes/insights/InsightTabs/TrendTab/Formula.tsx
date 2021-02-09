fb98cc2e6082ca1d3a80650524910a7ec2504280
Tim Glaser <tim@glsr.nl>
2021-02-09 20:29:57 +0100

    Add frontend for formulas

---
+++ frontend/src/scenes/insights/InsightTabs/TrendTab/Formula.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Input } from 'antd'
+import { FilterType } from '~/types'
+
+export function Formula({
+    filters,
+    onChange,
+}: {
+    filters: Partial<FilterType>
+    onChange: CallableFunction
+}): JSX.Element {
+    return (
+        <div style={{ maxWidth: 300 }}>
+            <Input.Search
+                placeholder="(A + B)/(A - B) * 100"
+                allowClear
+                defaultValue={filters.formula}
+                enterButton="Apply"
+                onSearch={onChange}
+            />
+        </div>
+    )
+}
