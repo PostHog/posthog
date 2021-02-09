569b1d79e1654a5286616595fa79aba68a763f54
Michael Matloka <dev@twixes.com>
2021-02-09 18:37:31 +0100

    Make test_return_non_anonymous_name a bit more robust

--- posthog/api/test/test_person.py
+++ posthog/api/test/test_person.py
@@ -309,8 +309,15 @@ def test_person_factory(event_factory, person_factory, get_events, get_people):
                 team=self.team,
                 distinct_ids=["distinct_id", "17787c3099427b-0e8f6c86323ea9-33647309-1aeaa0-17787c30995b7c"],
             )
+            person_factory(
+                team=self.team,
+                distinct_ids=["0451E507-9619-4330-9DAD-68588B8045D6", "distinct_id"],
+            )
+            
             response = self.client.get("/api/person/").json()
+            
             self.assertEqual(response["results"][0]["name"], "distinct_id")
+            self.assertEqual(response["results"][1]["name"], "distinct_id")
 
     return TestPerson
 
