0de65b0576acaa643f00b9ccb8def4213f48a775
Michael Matloka <dev@twixes.com>
2021-02-10 15:24:58 +0100

    Fix typing

--- posthog/plugins/access.py
+++ posthog/plugins/access.py
@@ -22,9 +22,9 @@ def guard_cloud(organization_or_id: Optional[Union[Organization, UUID]]):
     )
 
 
-def can_install_plugins_via_api(organization_or_id: Optional[Union[Organization, UUID]]):
-    return settings.PLUGINS_INSTALL_VIA_API and guard_cloud(organization_or_id)
+def can_install_plugins_via_api(organization_or_id: Optional[Union[Organization, UUID]]) -> bool:
+    return bool(settings.PLUGINS_INSTALL_VIA_API and guard_cloud(organization_or_id))
 
 
-def can_configure_plugins_via_api(organization_or_id: Optional[Union[Organization, UUID]]):
-    return settings.PLUGINS_CONFIGURE_VIA_API and guard_cloud(organization_or_id)
+def can_configure_plugins_via_api(organization_or_id: Optional[Union[Organization, UUID]]) -> bool:
+    return bool(settings.PLUGINS_CONFIGURE_VIA_API and guard_cloud(organization_or_id))
