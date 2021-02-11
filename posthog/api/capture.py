8499f591593ff5a9fbf42a9c70364abcdb199caf
Michael Matloka <dev@twixes.com>
2021-02-11 13:10:56 +0100

    Simplify capture condition

--- posthog/api/capture.py
+++ posthog/api/capture.py
@@ -207,9 +207,8 @@ def get_event(request):
         if is_ee_enabled():
             log_topics = [KAFKA_EVENTS_WAL]
 
-            ingest_via_plugin_server = settings.PLUGIN_SERVER_INGESTION
 
-            if ingest_via_plugin_server:
+            if settings.PLUGIN_SERVER_INGESTION:
                 log_topics.append(KAFKA_EVENTS_PLUGIN_INGESTION)
                 statsd.Counter("%s_posthog_cloud_plugin_server_ingestion" % (settings.STATSD_PREFIX,)).increment()
 
@@ -226,7 +225,7 @@ def get_event(request):
             )
 
             # must done after logging because process_event_ee modifies the event, e.g. by removing $elements
-            if not ingest_via_plugin_server:
+            if not settings.PLUGIN_SERVER_INGESTION:
                 process_event_ee(
                     distinct_id=distinct_id,
                     ip=get_ip_address(request),
