95630f7a6bc434a1de6649423893be5505481ef4
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-02-11 21:53:58 +0200

    Return no data for invalid regexes (#3248)
    * Return no data if invalid regular expression
    
    This currently returns a 500. Fixes 3191 together with frontend
    validation.
    
    * Add browser validation
    
    * Typing errors

--- frontend/src/lib/components/PropertyFilters/PropertyValue.js
+++ frontend/src/lib/components/PropertyFilters/PropertyValue.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Select } from 'antd'
 import api from '../../api'
-import { isOperatorFlag } from 'lib/utils'
+import { isOperatorFlag, isOperatorRegex, isValidRegex } from 'lib/utils'
 import { SelectGradientOverflow } from 'lib/components/SelectGradientOverflow'
 
 export function PropertyValue({
@@ -55,38 +55,51 @@ export function PropertyValue({
         (option) => input === '' || (option && option.name?.toLowerCase().indexOf(input.toLowerCase()) > -1)
     )
 
+    const validationError = getValidationError(operator, value)
+
     return (
-        <SelectGradientOverflow
-            showSearch
-            autoFocus={!value}
-            style={{ width: '100%', ...style }}
-            onChange={(_, payload) => onSet((payload && payload.value) || null)}
-            value={value || placeholder}
-            loading={optionsCache[input] === 'loading'}
-            onSearch={(input) => {
-                setInput(input)
-                if (!optionsCache[input] && !isOperatorFlag(operator)) {
-                    loadPropertyValues(input)
-                }
-            }}
-            data-attr="prop-val"
-            dropdownMatchSelectWidth={350}
-            bordered={bordered}
-            placeholder={placeholder}
-            allowClear={value}
-        >
-            {input && (
-                <Select.Option key={input} value={input}>
-                    Specify: {input}
-                </Select.Option>
-            )}
-            {displayOptions.map(({ name, id }, index) => (
-                <Select.Option key={id || name} value={id || name} data-attr={'prop-val-' + index}>
-                    {name === true && 'true'}
-                    {name === false && 'false'}
-                    {name}
-                </Select.Option>
-            ))}
-        </SelectGradientOverflow>
+        <>
+            <SelectGradientOverflow
+                showSearch
+                autoFocus={!value}
+                style={{ width: '100%', ...style }}
+                onChange={(_, payload) => onSet((payload && payload.value) || null)}
+                value={value || placeholder}
+                loading={optionsCache[input] === 'loading'}
+                onSearch={(newInput) => {
+                    setInput(newInput)
+                    if (!optionsCache[newInput] && !isOperatorFlag(operator)) {
+                        loadPropertyValues(newInput)
+                    }
+                }}
+                data-attr="prop-val"
+                dropdownMatchSelectWidth={350}
+                bordered={bordered}
+                placeholder={placeholder}
+                allowClear={value}
+            >
+                {input && (
+                    <Select.Option key={input} value={input}>
+                        Specify: {input}
+                    </Select.Option>
+                )}
+                {displayOptions.map(({ name, id }, index) => (
+                    <Select.Option key={id || name} value={id || name} data-attr={'prop-val-' + index}>
+                        {name === true && 'true'}
+                        {name === false && 'false'}
+                        {name}
+                    </Select.Option>
+                ))}
+            </SelectGradientOverflow>
+            {validationError && <p className="text-danger">{validationError}</p>}
+        </>
     )
+}
+
+function getValidationError(operator, value) {
+    if (isOperatorRegex(operator) && !isValidRegex(value)) {
+        return 'Value is not a valid regular expression'
+    }
+
+    return null
 }
