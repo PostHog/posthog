b55426fbc126ded80e16efbd22264084cbd11840
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-02-11 20:14:16 +0200

    Return 404 instead of 500 when person to be deleted is not found (#3314)
    Sentry error: https://sentry.io/organizations/posthog/issues/1977037233/?project=1899813

--- ee/clickhouse/views/person.py
+++ ee/clickhouse/views/person.py
@@ -1,4 +1,5 @@
 from rest_framework import request, response
+from rest_framework.exceptions import NotFound
 
 from ee.clickhouse.models.person import delete_person
 from ee.clickhouse.queries.clickhouse_retention import ClickhouseRetention
@@ -16,12 +17,13 @@ class ClickhousePersonViewSet(PersonViewSet):
     stickiness_class = ClickhouseStickiness
 
     def destroy(self, request: request.Request, pk=None, **kwargs):  # type: ignore
-        team = self.team
-        person = Person.objects.get(team=team, pk=pk)
-        # TODO: Probably won't need this after a while
+        try:
+            person = Person.objects.get(team=self.team, pk=pk)
 
-        events = Event.objects.filter(team=team, distinct_id__in=person.distinct_ids)
-        events.delete()
-        delete_person(person.uuid, delete_events=True, team_id=team.pk)
-        person.delete()
-        return response.Response(status=204)
+            events = Event.objects.filter(team=self.team, distinct_id__in=person.distinct_ids)
+            events.delete()
+            delete_person(person.uuid, delete_events=True, team_id=self.team.pk)
+            person.delete()
+            return response.Response(status=204)
+        except Person.DoesNotExist:
+            raise NotFound(detail="Person not found.")
