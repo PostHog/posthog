2dd0424e8477138b74d7de970bbfefc75ada5246
Eric Duong <eeoneric@gmail.com>
2021-02-11 18:49:36 -0500

    Add try except back to trend class (#3320)
    * add try
    
    * capture exceptions

--- ee/clickhouse/queries/trends/clickhouse_trends.py
+++ ee/clickhouse/queries/trends/clickhouse_trends.py
@@ -2,6 +2,7 @@ from typing import Any, Callable, Dict, List, Tuple
 
 from django.db.models.query import Prefetch
 from django.utils import timezone
+from sentry_sdk.api import capture_exception
 
 from ee.clickhouse.client import sync_execute
 from ee.clickhouse.queries.trends.breakdown import ClickhouseTrendsBreakdown
@@ -44,7 +45,11 @@ class ClickhouseTrends(
 
     def _run_query(self, filter: Filter, entity: Entity, team_id: int) -> List[Dict[str, Any]]:
         sql, params, parse_function = self._get_sql_for_entity(filter, entity, team_id)
-        result = sync_execute(sql, params)
+        try:
+            result = sync_execute(sql, params)
+        except Exception as e:
+            capture_exception(e)
+            result = []
         result = parse_function(result)
         serialized_data = self._format_serialized(entity, result)
 
