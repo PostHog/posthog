b2e67bf6556e75e65124407993b8f93129b856fc
Michael Matloka <dev@twixes.com>
2021-02-16 20:12:22 +0100

    Fix Formula typing

--- frontend/src/scenes/insights/InsightTabs/TrendTab/Formula.tsx
+++ frontend/src/scenes/insights/InsightTabs/TrendTab/Formula.tsx
@@ -8,7 +8,7 @@ export function Formula({
     onChange,
 }: {
     filters: Partial<FilterType>
-    onChange: CallableFunction
+    onChange: (formula: string) => void
 }): JSX.Element {
     const [value, setValue] = useState(filters.formula)
     useEffect(() => {
@@ -17,13 +17,13 @@ export function Formula({
     return (
         <div style={{ maxWidth: 300 }}>
             <Input.Search
-                placeholder="(A + B)/(A - B) * 100"
+                placeholder="e.g. (A + B)/(A - B) * 100"
                 allowClear
                 value={value}
                 onChange={(e) => setValue(e.target.value.toLocaleUpperCase())}
                 disabled={filters.shown_as === STICKINESS || filters.shown_as === LIFECYCLE}
                 enterButton="Apply"
-                onSearch={(value) => onChange(value)}
+                onSearch={onChange}
             />
         </div>
     )
