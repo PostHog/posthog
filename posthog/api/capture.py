0eb06f7bfdb394d2e82a4c408d144576378476df
Marius Andra <marius.andra@gmail.com>
2021-02-18 20:39:54 +0100

    3% to plugin server

--- posthog/api/capture.py
+++ posthog/api/capture.py
@@ -210,7 +210,7 @@ def get_event(request):
             plugin_server_ingestion = settings.PLUGIN_SERVER_INGESTION and (
                 not getattr(settings, "MULTI_TENANCY", False)
                 or str(team.organization_id) in getattr(settings, "PLUGINS_CLOUD_WHITELISTED_ORG_IDS", [])
-                or random() < 0.01
+                or random() < 0.03
             )
 
             if plugin_server_ingestion:
