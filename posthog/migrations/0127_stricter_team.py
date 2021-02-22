f5e9e556260fac610fc9bb95806f1a25d23d5f98
Michael Matloka <dev@twixes.com>
2021-02-22 13:05:31 +0100

    Rename prepare_teams

--- posthog/migrations/0127_stricter_team.py
+++ posthog/migrations/0127_stricter_team.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 import posthog.models.utils
 
 
-def prepare_teams(apps, schema_editor):
+def adjust_teams_for_stricter_requirements(apps, schema_editor):
     Team = apps.get_model("posthog", "Team")
     Organization = apps.get_model("posthog", "Organization")
     first_organization = Organization.objects.order_by("id").first()
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(prepare_teams, migrations.RunPython.noop),
+        migrations.RunPython(adjust_teams_for_stricter_requirements, migrations.RunPython.noop),
         migrations.AlterField(
             model_name="team",
             name="api_token",
