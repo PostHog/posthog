8f86eb4d1b76f59a4d0a24fb55b551d93896dedd
Michael Matloka <dev@twixes.com>
2021-02-26 00:56:57 +0100

    Update access.py

--- posthog/plugins/access.py
+++ posthog/plugins/access.py
@@ -6,22 +6,11 @@ from django.conf import settings
 from posthog.models.organization import Organization
 
 
-# We disable Plugins on Cloud, except for whitelisted organizations
-# Disregarding this in TEST mode, so that we can be sure plugins actually work in EE if/when needed
-def guard_cloud(organization_or_id: Optional[Union[Organization, UUID, str]]):
-    organization_id: Optional[str] = (
-        None
-        if not organization_or_id
-        else str(organization_or_id if isinstance(organization_or_id, (str, UUID)) else organization_or_id.id)
-    )
-    return settings.TEST or not getattr(settings, "MULTI_TENANCY", False)
-
-
-def can_install_plugins_via_api(organization_or_id: Union[Organization, str, UUID]) -> bool:
-    if not settings.PLUGINS_INSTALL_VIA_API:
+def can_install_plugins_via_api(organization_or_id: Optional[Union[Organization, str, UUID]]) -> bool:
+    if not settings.MULTI_TENANCY:
+        return settings.PLUGINS_INSTALL_VIA_API
+    if organization_or_id is None:
         return False
-    if settings.MULTI_TENANCY:
-        return True
     organization: Organization = (
         organization_or_id
         if isinstance(organization_or_id, Organization)
@@ -30,11 +19,11 @@ def can_install_plugins_via_api(organization_or_id: Union[Organization, str, UUI
     return organization.plugins_access_level >= Organization.PluginsAccessLevel.INSTALLATION
 
 
-def can_configure_plugins_via_api(organization_or_id: Union[Organization, str, UUID]) -> bool:
-    if not settings.PLUGINS_CONFIGURE_VIA_API:
+def can_configure_plugins_via_api(organization_or_id: Optional[Union[Organization, str, UUID]]) -> bool:
+    if not settings.MULTI_TENANCY:
+        return settings.PLUGINS_CONFIGURE_VIA_API
+    if organization_or_id is None:
         return False
-    if settings.MULTI_TENANCY:
-        return True
     organization: Organization = (
         organization_or_id
         if isinstance(organization_or_id, Organization)
