f9216a107ecf774699c43e7c4cd6c95fc22b8fa2
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-02-25 14:23:07 +0200

    Make sure images are the same size on CI and locally

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,5 +17,14 @@ module.exports = (on, config) => {
 
     initPlugin(on, config)
 
+    on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+            // https://www.ghacks.net/2013/10/06/list-useful-google-chrome-command-line-switches/
+            // Compatibility with gh actions
+            launchOptions.args.push('--window-size=1280,720')
+            return launchOptions
+        }
+    })
+
     return config
 }
