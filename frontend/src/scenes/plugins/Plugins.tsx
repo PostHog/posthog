e79a3bce2e520d2d4d0bfada595e581280b1519b
Michael Matloka <dev@twixes.com>
2021-03-04 16:29:14 +0100

    Add end of Plugins beta banner

--- frontend/src/scenes/plugins/Plugins.tsx
+++ frontend/src/scenes/plugins/Plugins.tsx
@@ -7,7 +7,7 @@ import { InstalledTab } from 'scenes/plugins/tabs/installed/InstalledTab'
 import { useActions, useValues } from 'kea'
 import { userLogic } from 'scenes/userLogic'
 import { pluginsLogic } from './pluginsLogic'
-import { Tabs } from 'antd'
+import { Alert, Tabs } from 'antd'
 import { PageHeader } from 'lib/components/PageHeader'
 import { PluginTab } from 'scenes/plugins/types'
 import { AdvancedTab } from 'scenes/plugins/tabs/advanced/AdvancedTab'
@@ -21,7 +21,9 @@ function _Plugins(): JSX.Element | null {
     const { TabPane } = Tabs
 
     useEffect(() => {
-        if (user && !user.plugin_access.configure) {window.location.href = '/'}
+        if (user && !user.plugin_access.configure) {
+            window.location.href = '/'
+        }
         posthog.persistence.register({ has_checked_out_plugins: true })
     }, [user])
 
@@ -32,6 +34,22 @@ function _Plugins(): JSX.Element | null {
     return (
         <div className="plugins-scene">
             <PageHeader title="Plugins" caption="Plugins enable you to freely extend PostHog's core functionality." />
+            {!posthog.persistence.properties()['has_closed_plugins_end_of_beta'] && (
+                <Alert
+                    message="Beta Phase Completed"
+                    description={
+                        <>
+                            Plugins are now a core feature of PostHog. Read more about this next step in our journey on
+                            the PostHog blog.
+                        </>
+                    }
+                    type="info"
+                    showIcon
+                    closable
+                    onClose={() => posthog.persistence.register({ has_closed_plugins_end_of_beta: true })}
+                    style={{ marginBottom: 32 }}
+                />
+            )}
             {user.plugin_access.install ? (
                 <Tabs activeKey={pluginTab} onChange={(activeKey) => setPluginTab(activeKey as PluginTab)}>
                     <TabPane tab="Installed" key={PluginTab.Installed}>
