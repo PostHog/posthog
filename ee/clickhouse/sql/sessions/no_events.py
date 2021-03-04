22cd761ab13aa036cdef9fdc70fbeca6543dff0b
Tim Glaser <tim@glsr.nl>
2021-03-04 15:33:54 +0100

    Fix session tests

--- ee/clickhouse/sql/sessions/no_events.py
+++ ee/clickhouse/sql/sessions/no_events.py
@@ -1,27 +1,17 @@
 SESSIONS_NO_EVENTS_SQL = """
 SELECT
-    distinct_id,
-    uuid,
-    session_uuid,
     session_duration_seconds,
-    timestamp,
-    session_end_ts
+    timestamp
 FROM
 (
     SELECT
-        distinct_id,
-        uuid,
-        if(is_new_session, uuid, NULL) AS session_uuid,
         is_new_session,
         is_end_session,
         if(is_end_session AND is_new_session, 0, if(is_new_session AND (NOT is_end_session), dateDiff('second', toDateTime(timestamp), toDateTime(neighbor(timestamp, 1))), NULL)) AS session_duration_seconds,
-        timestamp,
-        if(is_end_session AND is_new_session, timestamp, if(is_new_session AND (NOT is_end_session), neighbor(timestamp, 1), NULL)) AS session_end_ts
+        timestamp
     FROM
     (
         SELECT
-            distinct_id,
-            uuid,
             timestamp,
             neighbor(distinct_id, -1) AS start_possible_neighbor,
             neighbor(timestamp, -1) AS start_possible_prev_ts,
@@ -42,6 +32,10 @@ FROM
                 {date_from}
                 {date_to} 
                 {filters}
+            GROUP BY
+                uuid,
+                timestamp,
+                distinct_id
             ORDER BY
                 distinct_id ASC,
                 timestamp ASC
