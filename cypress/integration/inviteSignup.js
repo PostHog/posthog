31f64941d66c8a7ed075e61be4f7a1bae609c7cc
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-03-04 15:35:58 -0800

    using an invite

--- cypress/integration/inviteSignup.js
+++ cypress/integration/inviteSignup.js
@@ -1,5 +1,5 @@
 describe('Invite Signup', () => {
-    it('Creates an invite ', () => {
+    beforeEach(() => {
         cy.get('[data-attr=top-navigation-whoami]').click()
         cy.get('[data-attr=top-menu-item-org-settings').click()
         cy.get('[data-attr=invite-teammate-button]').click()
@@ -9,16 +9,37 @@ describe('Invite Signup', () => {
 
         cy.get('.Toastify__toast-body h1').should('contain', 'Invite sent!')
         cy.get('.invites-table tbody td:first-of-type').should('contain', 'newuser@posthog.com')
+    })
 
-        // Logged in user cannot accept invite for someone else
+    it('Authenticated user cannot use invite for someone else', () => {
+        // Tests invite creation flow too
         cy.get('.invites-table tbody td:nth-last-child(2)').then((element) => {
-            cy.log('Hiya there!')
-            cy.log(element)
-            cy.log(element.get(0).innerText)
             cy.visit(element.text())
         })
         cy.get('.error-view-container').should('exist')
         cy.get('h1.page-title').should('contain', 'Oops! You cannot use this invite link')
         cy.get('.error-message div').should('contain', 'This invite is intended for another email address')
+
+        // Delete the invite
+        cy.visit('/organization/members')
+        cy.get('.invites-table .anticon-delete').click()
+        cy.get('.ant-modal-confirm-btns button').contains('Delete').click()
+    })
+
+    it('New user can use invite', () => {
+        cy.get('.invites-table tbody td:nth-last-child(2)').then((element) => {
+            cy.get('[data-attr=top-navigation-whoami]').click()
+            cy.get('[data-attr=top-menu-item-logout]').click()
+            cy.visit(element.text())
+        })
+        cy.get('.error-view-container').should('not.exist')
+        cy.get('h1.page-title').should('contain', "You've been invited to join")
+        cy.get('#email').should('have.value', 'n*****r@posthog.com')
+        cy.get('#password').type('12345678')
+        cy.get('.ant-progress-bg').should('not.have.css', 'width', '0px') // Password strength indicator is working
+        cy.get('#first_name').type('Bob')
+        cy.get('[data-attr=password-signup]').click()
+        cy.get('.Toastify__toast-body').should('contain', 'You have joined')
+        cy.location('pathname').should('include', '/insights')
     })
 })
