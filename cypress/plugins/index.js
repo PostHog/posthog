6b63fe79a01aa8841f48ddd4d5e02e8aacb5ed93
Marius Andra <marius.andra@gmail.com>
2021-03-05 15:26:42 +0100

    revert to webpack 4

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-const webpackPreprocessor = require('cypress-webpack-preprocessor-v5')
+const webpackPreprocessor = require('@cypress/webpack-preprocessor')
 const { initPlugin } = require('cypress-plugin-snapshots/plugin')
 
 const { createEntry } = require('../../webpack.config')
