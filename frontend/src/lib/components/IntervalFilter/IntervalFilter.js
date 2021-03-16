c174f0fdbf88cceb50ec289b0b03c2287d07ede4
yakkomajuri <yakko.majuri@gmail.com>
2021-03-16 16:11:36 +0000

    Remove date from defaults for intervals

--- frontend/src/lib/components/IntervalFilter/IntervalFilter.js
+++ frontend/src/lib/components/IntervalFilter/IntervalFilter.js
@@ -26,23 +26,6 @@ export function IntervalFilter({ view, disabled = false }) {
             value={intervalMapping[interval]}
             dropdownMatchSelectWidth={false}
             onChange={(key) => {
-                switch (key) {
-                    case 'minute':
-                        date_from = 'dStart'
-                        break
-                    case 'hour':
-                        date_from = 'dStart'
-                        break
-                    case 'week':
-                        date_from = '-30d'
-                        break
-                    case 'month':
-                        date_from = '-90d'
-                        break
-                    default:
-                        date_from = undefined
-                        break
-                }
                 const minute_disabled = key === 'minute' && disableMinuteFor[date_from]
                 const hour_disabled = key === 'hour' && disableHourFor[date_from]
                 if (minute_disabled || hour_disabled) {
