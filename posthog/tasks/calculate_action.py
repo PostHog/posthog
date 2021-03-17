9da8f792e006784f35535ccbfef05172188975f4
Michael Matloka <dev@twixes.com>
2021-03-17 02:38:46 +0100

    Return early from calculate_actions_from_last_calculation if CH

--- posthog/tasks/calculate_action.py
+++ posthog/tasks/calculate_action.py
@@ -1,8 +1,10 @@
 import logging
 import time
+from typing import Sequence, cast
 
 from celery import shared_task
 
+from posthog.ee import is_ee_enabled
 from posthog.models import Action
 
 logger = logging.getLogger(__name__)
@@ -17,9 +19,10 @@ def calculate_action(action_id: int) -> None:
 
 
 def calculate_actions_from_last_calculation() -> None:
-    actions = Action.objects.filter(deleted=False).only("pk")
+    if is_ee_enabled():
+        return
+    actions = cast(Sequence[Action], Action.objects.filter(deleted=False).only("pk"))
     for action in actions:
         start_time = time.time()
         action.calculate_events(start=action.last_calculated_at)
-
         logger.info("Calculating action {} took {:.2f} seconds".format(action.pk, (time.time() - start_time)))
