4c94d16098fb83383b9dc2a9f9e98b3d2e6f78c6
Michael Matloka <dev@twixes.com>
2021-03-17 10:33:58 +0100

    Update calculate_action.py

--- posthog/tasks/calculate_action.py
+++ posthog/tasks/calculate_action.py
@@ -15,14 +15,17 @@ def calculate_action(action_id: int) -> None:
     start_time = time.time()
     action = Action.objects.get(pk=action_id)
     action.calculate_events()
-    logger.info("Calculating action {} took {:.2f} seconds".format(action.pk, (time.time() - start_time)))
+    total_time = time.time() - start_time
+    logger.info(f"Calculating action {action.pk} took {total_time:.2f} seconds")
 
 
 def calculate_actions_from_last_calculation() -> None:
-    if is_ee_enabled():
+    if is_ee_enabled():  # In EE actions are not precalculated
         return
+    start_time = time.time()
     actions = cast(Sequence[Action], Action.objects.filter(deleted=False).only("pk"))
     for action in actions:
         start_time = time.time()
         action.calculate_events(start=action.last_calculated_at)
-        logger.info("Calculating action {} took {:.2f} seconds".format(action.pk, (time.time() - start_time)))
+    total_time = time.time() - start_time
+    logger.info(f"Calculated new event-action pairs in {total_time:.2f} s")
