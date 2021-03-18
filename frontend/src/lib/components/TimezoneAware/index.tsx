81e3e2b91b00b913d1f27b6c4e274872dabec4c8
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-03-18 12:00:11 -0700

    adjust component visuals

--- frontend/src/lib/components/TimezoneAware/index.tsx
+++ frontend/src/lib/components/TimezoneAware/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './index.scss'
 import dayjs from 'dayjs'
-import { Popover } from 'antd'
+import { Col, Popover, Row } from 'antd'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 import { useValues } from 'kea'
 import { teamLogic } from 'scenes/teamLogic'
+import { ProjectOutlined, LaptopOutlined, GlobalOutlined, SettingOutlined } from '@ant-design/icons'
+import { Link } from '../Link'
+
+const DATE_OUTPUT_FORMAT = 'llll'
 
 dayjs.extend(LocalizedFormat)
 dayjs.extend(relativeTime)
@@ -21,26 +25,39 @@ export function TZLabel({ time }: { time: string | dayjs.Dayjs }): JSX.Element {
 
     const PopoverContent = (
         <div className="tz-label-popover">
-            <h3 className="l3">Timezone conversion</h3>
+            <h3 className="l3">
+                Timezone conversion
+                <span className="float-right">
+                    <Link to="/project/settings#timezone">
+                        <SettingOutlined />
+                    </Link>
+                </span>
+            </h3>
             <div className="divider" />
             <div className="timezones">
                 {currentTeam && (
-                    <div className="timezone">
-                        <div className="name">EST</div>
-                        <div className="scope">| Project</div>
-                        <div className="time">{parsedTime.tz(currentTeam.timezone).format('LLL')}</div>
-                    </div>
+                    <Row className="timezone">
+                        <Col className="name">
+                            <ProjectOutlined /> EST
+                        </Col>
+                        <Col className="scope">| Project</Col>
+                        <Col className="time">{parsedTime.tz(currentTeam.timezone).format(DATE_OUTPUT_FORMAT)}</Col>
+                    </Row>
                 )}
-                <div className="timezone">
-                    <div className="name">EST</div>
-                    <div className="scope">| Your computer</div>
-                    <div className="time">{parsedTime.format('LLL')}</div>
-                </div>
-                <div className="timezone">
-                    <div className="name">UTC</div>
-                    <div className="scope" />
-                    <div className="time">{parsedTime.tz('UTC').format('LLL')}</div>
-                </div>
+                <Row className="timezone">
+                    <Col className="name">
+                        <LaptopOutlined /> EST
+                    </Col>
+                    <Col className="scope">| Your computer</Col>
+                    <Col className="time">{parsedTime.format(DATE_OUTPUT_FORMAT)}</Col>
+                </Row>
+                <Row className="timezone">
+                    <Col className="name">
+                        <GlobalOutlined /> UTC
+                    </Col>
+                    <Col className="scope" />
+                    <Col className="time">{parsedTime.tz('UTC').format(DATE_OUTPUT_FORMAT)}</Col>
+                </Row>
             </div>
         </div>
     )
