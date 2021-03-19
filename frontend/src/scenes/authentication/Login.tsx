aad497067c178f4e7fa0bbb21d0875a54c590abf
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-03-19 12:42:28 -0700

    fully working login page

--- frontend/src/scenes/authentication/Login.tsx
+++ frontend/src/scenes/authentication/Login.tsx
@@ -1,9 +1,14 @@
-import { Col, Row } from 'antd'
+import { Col, Row, Form, Input, Button, Alert } from 'antd'
 import React from 'react'
 import smLogo from 'public/icon-white.svg'
 import './Login.scss'
+import { useActions, useValues } from 'kea'
+import { loginLogic } from './loginLogic'
 
 export function Login(): JSX.Element {
+    const [form] = Form.useForm()
+    const { authenticate } = useActions(loginLogic)
+    const { authenticateResponseLoading, authenticateResponse } = useValues(loginLogic)
     return (
         <div className="login">
             <Row>
@@ -13,11 +18,79 @@ export function Login(): JSX.Element {
                         <div className="main-logo">
                             <img src={smLogo} alt="" />
                         </div>
-                        <div className="showcase-content" />
+                        <div className="showcase-content">
+                            <h1 className="page-title">Welcome back to PostHog!</h1>
+                        </div>
                     </div>
                 </Col>
-                <Col span={24} md={14}>
-                    Hello
+                <Col span={24} md={14} className="auth-main-content">
+                    <div className="inner">
+                        <h2 className="subtitle" style={{ justifyContent: 'center' }}>
+                            Login to PostHog
+                        </h2>
+                        {!authenticateResponseLoading && authenticateResponse?.errorCode && (
+                            <Alert
+                                message="Could not complete your login"
+                                description={authenticateResponse?.errorDetail}
+                                type="error"
+                                showIcon
+                                style={{ marginBottom: 16 }}
+                            />
+                        )}
+                        <Form
+                            layout="vertical"
+                            form={form}
+                            onFinish={(values) => authenticate(values)}
+                            requiredMark={false}
+                        >
+                            <Form.Item
+                                name="email"
+                                label="Email"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please enter your email to continue',
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    className="ph-ignore-input"
+                                    autoFocus
+                                    data-attr="login-email"
+                                    placeholder="email@yourcompany.com"
+                                    type="email"
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="password"
+                                label="Password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please enter your password to continue',
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    className="ph-ignore-input"
+                                    type="password"
+                                    data-attr="login-password"
+                                    placeholder="********"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button
+                                    type="primary"
+                                    htmlType="submit"
+                                    data-attr="password-signup"
+                                    loading={authenticateResponseLoading}
+                                    block
+                                >
+                                    Login
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
                 </Col>
             </Row>
         </div>
