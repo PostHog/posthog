14fad37c3322cec31c39e844cac76c1b9eaded13
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-03-19 14:10:41 -0700

    adjust login tests

--- cypress/integration/auth.js
+++ cypress/integration/auth.js
@@ -11,22 +11,52 @@ describe('Auth', () => {
     it('Logout and login', () => {
         cy.get('[data-attr=top-menu-item-logout]').click()
 
-        cy.get('#inputEmail').type('fake@posthog.com').should('have.value', 'fake@posthog.com')
+        cy.get('[data-attr=login-email]').type('fake@posthog.com').should('have.value', 'fake@posthog.com')
 
-        cy.get('#inputPassword').type('12345678').should('have.value', '12345678')
+        cy.get('[data-attr=login-password]').type('12345678').should('have.value', '12345678')
 
-        cy.get('.btn').click()
+        cy.get('[type=submit]').click()
     })
 
     it('Try logging in improperly', () => {
         cy.get('[data-attr=top-menu-item-logout]').click()
 
-        cy.get('#inputEmail').type('fake@posthog.com').should('have.value', 'fake@posthog.com')
+        cy.get('[data-attr=login-email]').type('fake@posthog.com').should('have.value', 'fake@posthog.com')
+        cy.get('[data-attr=login-password]').type('wrong password').should('have.value', 'wrong password')
+        cy.get('[type=submit]').click()
 
-        cy.get('#inputPassword').type('wrong password').should('have.value', 'wrong password')
+        cy.get('.ant-alert-error').should('contain', 'Invalid email or password.')
+    })
 
-        cy.get('.btn').click()
+    it('Redirect to appropriate place after login', () => {
+        cy.get('[data-attr=top-menu-item-logout]').click()
 
-        cy.get('[data-attr=login-error]').should('exist')
+        cy.visit('/events')
+        cy.location('pathname').should('include', '/login') // Should be redirected to login because we're now logged out
+
+        cy.get('[data-attr=login-email]').type('test@posthog.com')
+        cy.get('[data-attr=login-password]').type('12345678')
+        cy.get('[type=submit]').click()
+
+        cy.location('pathname').should('include', '/events')
+    })
+
+    it('Redirect to appropriate place after login with complex URL', () => {
+        cy.get('[data-attr=top-menu-item-logout]').click()
+
+        cy.visit(
+            '/insights?insight=TRENDS&interval=day&display=ActionsLineGraph&actions=%5B%5D&events=%5B%7B"id"%3A"%24pageview"%2C"name"%3A"%24pageview"%2C"type"%3A"events"%2C"order"%3A0%7D%2C%7B"id"%3A"%24autocapture"%2C"name"%3A"%24autocapture"%2C"type"%3A"events"%2C"order"%3A1%7D%5D&properties=%5B%5D&filter_test_accounts=false&new_entity=%5B%5D'
+        )
+        cy.location('pathname').should('include', '/login') // Should be redirected to login because we're now logged out
+
+        cy.get('[data-attr=login-email]').type('test@posthog.com')
+        cy.get('[data-attr=login-password]').type('12345678')
+        cy.get('[type=submit]').click()
+
+        cy.location('pathname').should(
+            'include',
+            '/insights?insight=TRENDS&interval=day&display=ActionsLineGraph&actions=%5B%5D&events=%5B%7B"id"%3A"%24pageview"%2C"name"%3A"%24pageview"%2C"type"%3A"events"%2C"order"%3A0%7D%2C%7B"id"%3A"%24autocapture"%2C"name"%3A"%24autocapture"%2C"type"%3A"events"%2C"order"%3A1%7D%5D&properties=%5B%5D&filter_test_accounts=false&new_entity=%5B%5D'
+        )
+        cy.get('.trend-element-subject-1').should('contain', '$autocapture') // Ensure the URL was properly parsed and components shown correctly
     })
 })
