d24be4c4585099737fbb6aaef235a44730613cc4
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-03-23 15:04:42 -0700

    address feedback

--- posthog/api/event.py
+++ posthog/api/event.py
@@ -61,8 +61,8 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
     def get_person(self, event: Event) -> Any:
-        if hasattr(event, "person_properties"):
-            return event.person_properties  # type: ignore
+        if hasattr(event, "serialized_person"):
+            return event.serialized_person  # type: ignore
         return None
 
     def get_elements(self, event: Event):
@@ -150,7 +150,7 @@ class EventViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
             try:
                 for person in people:
                     if event.distinct_id in person.distinct_ids:
-                        event.person_properties = {  # type: ignore
+                        event.serialized_person = {  # type: ignore
                             "is_identified": person.is_identified,
                             "distinct_id": person.distinct_ids[0],
                             "properties": {
@@ -161,7 +161,7 @@ class EventViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
                         }
                         break
             except IndexError:
-                event.person_properties = None  # type: ignore
+                event.serialized_person = None  # type: ignore
             try:
                 event.elements_group_cache = [group for group in groups if group.hash == event.elements_hash][0]  # type: ignore
             except IndexError:
