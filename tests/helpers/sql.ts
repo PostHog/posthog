dfc2eadcd01ee2118aba27a7e15fe633d4937d5c
Marius Andra <marius.andra@gmail.com>
2021-03-24 07:09:48 +0100

    [plugin-server] timezone to tests (https://github.com/PostHog/plugin-server/pull/277)

--- tests/helpers/sql.ts
+++ tests/helpers/sql.ts
@@ -120,6 +120,7 @@ export async function createUserTeamAndOrganization(
         is_demo: false,
         api_token: `THIS IS NOT A TOKEN FOR TEAM ${teamId}`,
         test_account_filters: [],
+        timezone: 'UTC',
     })
 }
 
