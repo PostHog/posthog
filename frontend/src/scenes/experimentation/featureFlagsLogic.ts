083bb02e622a54de31c1dc6d5eba78007045f9d5
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-03-24 13:08:41 -0700

    move featureflag update logic to loader

--- frontend/src/scenes/experimentation/featureFlagsLogic.ts
+++ frontend/src/scenes/experimentation/featureFlagsLogic.ts
@@ -4,29 +4,17 @@ import { featureFlagsLogicType } from './featureFlagsLogicType'
 import { FeatureFlagType } from '~/types'
 
 export const featureFlagsLogic = kea<featureFlagsLogicType<FeatureFlagType>>({
-    actions: {
-        setFeatureFlag: (featureFlag: FeatureFlagType) => ({ featureFlag }),
-        updateFeatureFlag: (id: number, payload: Partial<FeatureFlagType>) => ({ id, payload }),
-    },
-    loaders: {
+    loaders: ({ values }) => ({
         featureFlags: {
             __default: [] as FeatureFlagType[],
             loadFeatureFlags: async () => {
                 const response = await api.get('api/feature_flag/')
                 return response.results as FeatureFlagType[]
             },
-        },
-    },
-    listeners: ({ actions }) => ({
-        updateFeatureFlag: async ({ id, payload }) => {
-            const response = await api.update(`api/feature_flag/${id}`, payload)
-            actions.setFeatureFlag(response)
-        },
-    }),
-    reducers: () => ({
-        featureFlags: {
-            setFeatureFlag: (state, { featureFlag }) =>
-                [...state].map((flag) => (flag.id === featureFlag.id ? featureFlag : flag)),
+            updateFeatureFlag: async ({ id, payload }: { id: string; payload: Partial<FeatureFlagType> }) => {
+                const response = await api.update(`api/feature_flag/${id}`, payload)
+                return [...values.featureFlags].map((flag) => (flag.id === response.id ? response : flag))
+            },
         },
     }),
     events: ({ actions }) => ({
