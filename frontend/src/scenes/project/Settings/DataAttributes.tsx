ccda5dd2ca80da7b0e58ec61f2b78e174a81c422
Marius Andra <marius.andra@gmail.com>
2021-03-26 07:16:38 +0100

    scarier exampe

--- frontend/src/scenes/project/Settings/DataAttributes.tsx
+++ frontend/src/scenes/project/Settings/DataAttributes.tsx
@@ -27,7 +27,7 @@ export function DataAttributes(): JSX.Element {
             </p>
             <p>
                 For example, when creating an action on your CTA button, the best selector could be something like:{' '}
-                <code>div &gt; form &gt; button</code>. However all buttons in your app have a{' '}
+                <code>div &gt; form &gt; button:nth-child(2)</code>. However all buttons in your app have a{' '}
                 <code>data-custom-id</code> attribute. If you whitelist it here, the selector for your button will
                 instead be <code>button[data-custom-id='cta-button']</code>.
             </p>
