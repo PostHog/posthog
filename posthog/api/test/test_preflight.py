d024ea923633e4f8f42467d26d90b03d4834119b
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-02 10:49:59 -0500

    update preflight tests

--- posthog/api/test/test_preflight.py
+++ posthog/api/test/test_preflight.py
@@ -2,8 +2,10 @@ from typing import cast
 
 from rest_framework import status
 
+from posthog.constants import RDBMS
 from posthog.models import User
 from posthog.test.base import APIBaseTest
+from posthog.version import VERSION
 
 
 class TestPreflight(APIBaseTest):
@@ -24,7 +26,15 @@ class TestPreflight(APIBaseTest):
                     "db": True,
                     "initiated": True,
                     "cloud": False,
+                    "ee_available": True,
+                    "ee_enabled": False,
                     "available_social_auth_providers": {"google-oauth2": False, "github": False, "gitlab": False},
+                    "opt_out_capture": False,
+                    "posthog_version": VERSION,
+                    "email_service_available": False,
+                    "is_debug": False,
+                    "is_event_property_usage_enabled": False,
+                    "is_async_event_action_mapping_enabled": True,
                 },
             )
             self.assertDictContainsSubset({"Europe/Moscow": 3, "UTC": 0}, available_timezones)
@@ -34,7 +44,7 @@ class TestPreflight(APIBaseTest):
         self.client.logout()  # make sure it works anonymously
         User.objects.all().delete()
 
-        with self.settings(MULTI_TENANCY=True):
+        with self.settings(MULTI_TENANCY=True, PRIMARY_DB=RDBMS.CLICKHOUSE):
             response = self.client.get("/_preflight/")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             response = response.json()
@@ -50,7 +60,15 @@ class TestPreflight(APIBaseTest):
                     "db": True,
                     "initiated": False,
                     "cloud": True,
+                    "ee_available": True,
+                    "ee_enabled": True,
                     "available_social_auth_providers": {"google-oauth2": False, "github": False, "gitlab": False},
+                    "opt_out_capture": False,
+                    "posthog_version": VERSION,
+                    "email_service_available": False,
+                    "is_debug": False,
+                    "is_event_property_usage_enabled": False,
+                    "is_async_event_action_mapping_enabled": True,
                 },
             )
             self.assertDictContainsSubset({"Europe/Moscow": 3, "UTC": 0}, available_timezones)
@@ -64,6 +82,7 @@ class TestPreflight(APIBaseTest):
             SOCIAL_AUTH_GOOGLE_OAUTH2_KEY="test_key",
             SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET="test_secret",
             MULTI_TENANCY=True,
+            EMAIL_HOST="localhost",
         ):
             response = self.client.get("/_preflight/")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -80,7 +99,15 @@ class TestPreflight(APIBaseTest):
                     "db": True,
                     "initiated": False,
                     "cloud": True,
+                    "ee_available": True,
+                    "ee_enabled": False,
                     "available_social_auth_providers": {"google-oauth2": True, "github": False, "gitlab": False},
+                    "opt_out_capture": False,
+                    "posthog_version": VERSION,
+                    "email_service_available": True,
+                    "is_debug": False,
+                    "is_event_property_usage_enabled": False,
+                    "is_async_event_action_mapping_enabled": True,
                 },
             )
             self.assertDictContainsSubset({"Europe/Moscow": 3, "UTC": 0}, available_timezones)
