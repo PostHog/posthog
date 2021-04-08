938000b2ca65df791bfac2bddaad15a47fc85c11
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-08 16:35:40 -0700

    adjust copy

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -77,9 +77,9 @@ export const MATHS: Record<string, any> = {
         name: 'Unique users',
         description: (
             <>
-                Users active in the time interval.
+                Unique users who performed the event in the specified time interval.
                 <br />
-                If a user performs an event 3 times in a given day/week/month, it counts only as 1.
+                If a single user performs an event 3 times in a given day/week/month, it counts only as 1.
             </>
         ),
         onProperty: false,
