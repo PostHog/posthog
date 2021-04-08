587516e72432547d94e6876d06b59fde70bfe93e
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-08 14:25:35 -0700

    add E2E test case

--- cypress/integration/dashboard.js
+++ cypress/integration/dashboard.js
@@ -113,4 +113,12 @@ describe('Dashboard', () => {
         cy.get('[data-attr="dashboard-item-0-dropdown-move-0"]').click({ force: true })
         cy.get('[data-attr=success-toast]').should('exist')
     })
+
+    it('Opens dashboard item in insights', () => {
+        cy.get('[data-attr=dashboard-name]').contains('My App Dashboard').click()
+        cy.get('[data-attr=dashboard-item-0] .dashboard-item-title a').click()
+        cy.location('pathname').should('include', '/insights')
+        cy.get('[data-attr=math-selector-0]').contains('Active users').should('exist')
+        cy.get('[data-attr=trend-line-graph]').should('exist')
+    })
 })
