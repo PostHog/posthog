833584dbbcd9456ffa2822a4d2f2fe8a26934525
Marius Andra <marius.andra@gmail.com>
2021-04-12 20:45:29 +0200

    [plugin-server] add generic pool (https://github.com/PostHog/plugin-server/pull/312)

--- src/worker/vm/imports.ts
+++ src/worker/vm/imports.ts
@@ -2,11 +2,13 @@ import { BigQuery } from '@google-cloud/bigquery'
 import * as contrib from '@posthog/plugin-contrib'
 import * as AWS from 'aws-sdk'
 import crypto from 'crypto'
+import * as genericPool from 'generic-pool'
 import fetch from 'node-fetch'
 import snowflake from 'snowflake-sdk'
 
 export const imports = {
     crypto: crypto,
+    'generic-pool': genericPool,
     'node-fetch': fetch,
     'snowflake-sdk': snowflake,
     '@google-cloud/bigquery': { BigQuery },
