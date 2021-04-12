65c3d7b1015a39b35e2e38b7ad133c4b0d657022
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-12 08:17:33 -0700

    fix test filters

--- frontend/src/scenes/project/Settings/TestAccountFiltersConfig.tsx
+++ frontend/src/scenes/project/Settings/TestAccountFiltersConfig.tsx
@@ -8,7 +8,7 @@ import { teamLogic } from 'scenes/teamLogic'
 export function TestAccountFiltersConfig(): JSX.Element {
     const { updateCurrentTeam } = useActions(teamLogic)
     const { reportTestAccountFiltersUpdated } = useActions(eventUsageLogic)
-    const { currentTeam } = useValues(teamLogic)
+    const { currentTeam, currentTeamLoading } = useValues(teamLogic)
 
     const handleChange = (filters: FilterType[]): void => {
         updateCurrentTeam({ test_account_filters: filters })
@@ -18,11 +18,13 @@ export function TestAccountFiltersConfig(): JSX.Element {
     return (
         <div style={{ marginBottom: 16 }}>
             <div style={{ marginBottom: 8 }}>
-                <PropertyFilters
-                    pageKey="testaccountfilters"
-                    propertyFilters={currentTeam?.test_account_filters}
-                    onChange={handleChange}
-                />
+                {!currentTeamLoading && (
+                    <PropertyFilters
+                        pageKey="testaccountfilters"
+                        propertyFilters={currentTeam?.test_account_filters}
+                        onChange={handleChange}
+                    />
+                )}
             </div>
         </div>
     )
