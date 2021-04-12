c1d975013a710d295d53df16fb6daa14ed3a5f59
Marius Andra <marius.andra@gmail.com>
2021-04-12 14:54:24 +0200

    [plugin-server] add plugin-contrib and AWS sdk (https://github.com/PostHog/plugin-server/pull/306)

--- src/worker/vm/imports.ts
+++ src/worker/vm/imports.ts
@@ -1,4 +1,6 @@
 import { BigQuery } from '@google-cloud/bigquery'
+import * as contrib from '@posthog/plugin-contrib'
+import * as AWS from 'aws-sdk'
 import crypto from 'crypto'
 import fetch from 'node-fetch'
 import snowflake from 'snowflake-sdk'
@@ -8,4 +10,6 @@ export const imports = {
     'node-fetch': fetch,
     'snowflake-sdk': snowflake,
     '@google-cloud/bigquery': { BigQuery },
+    '@posthog/plugin-contrib': contrib,
+    'aws-sdk': AWS,
 }
