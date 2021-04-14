869ed4213515325f76e89a2d95ff7a907541bea5
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-14 12:27:44 -0700

    make tests less random

--- cypress/integration/inviteSignup.js
+++ cypress/integration/inviteSignup.js
@@ -1,19 +1,17 @@
 describe('Invite Signup', () => {
-    beforeEach(() => {
+    it('Authenticated user can invite user but cannot use invite for someone else', () => {
         cy.get('[data-attr=top-navigation-whoami]').click()
         cy.get('[data-attr=top-menu-item-org-settings]').click()
 
         cy.location('pathname').should('eq', '/organization/settings')
+
+        // Test invite creation flow
         cy.get('[data-attr=invite-teammate-button]').click({ force: true }) // The button gets nested under some weird Ant divs
-
-        cy.get('[data-attr=invite-email-input]').type('newuser@posthog.com').should('have.value', 'newuser@posthog.com')
+        cy.get('[data-attr=invite-email-input]').type('charlie@posthog.com').should('have.value', 'charlie@posthog.com')
         cy.get('[data-attr=invite-team-member-submit]').click()
+        cy.get('[data-attr=invites-table] tbody td').should('contain', 'charlie@posthog.com')
 
-        cy.get('[data-attr=invites-table] tbody td').should('contain', 'newuser@posthog.com')
-    })
-
-    it('Authenticated user cannot use invite for someone else', () => {
-        // Tests invite creation flow too
+        // Assert user cannot use invite for someone else
         cy.get('[data-attr=invites-table] tbody tr:last-of-type td:nth-last-child(2)').then((element) => {
             cy.visit(element.text())
         })
@@ -25,13 +23,21 @@ describe('Invite Signup', () => {
         cy.visit('/organization/members')
         cy.get('[data-attr=invites-table] .anticon-delete').click()
         cy.get('.ant-modal-confirm-btns button').contains('Delete').click()
+        cy.get('.Toastify__toast-body').should('contain', 'removed')
+        cy.get('[data-attr=invites-table] tbody td').should('not.contain', 'charlie@posthog.com')
     })
 
     it('New user can use invite', () => {
-        cy.get('[data-attr=invites-table] tbody td:nth-last-child(2)').then((element) => {
+        cy.request({
+            method: 'POST',
+            url: '/api/organizations/@current/invites/',
+            body: { target_email: 'newuser@posthog.com' },
+            headers: { Authorization: 'Bearer e2e_demo_api_key' },
+        }).then((response) => {
+            expect(response.status).to.eq(201)
             cy.get('[data-attr=top-navigation-whoami]').click()
             cy.get('[data-attr=top-menu-item-logout]').click()
-            cy.visit(element.text())
+            cy.visit('/signup/' + response.body.id)
         })
         cy.get('.error-view-container').should('not.exist')
         cy.get('h1.page-title').should('contain', "You've been invited to join")
@@ -42,22 +48,5 @@ describe('Invite Signup', () => {
         cy.get('[data-attr=password-signup]').click()
         cy.get('.Toastify__toast-body').should('contain', 'You have joined')
         cy.location('pathname').should('include', '/insights')
-    })
-})
-
-describe('Invite Signup II', () => {
-    it('Can leave the organization', () => {
-        // Logout & log in with alt user
-        cy.visit('/logout')
-        cy.get('[data-attr=login-email]').type('newuser@posthog.com')
-        cy.get('[data-attr=password]').type('12345678')
-        cy.get('button[type=submit]').click()
-        cy.location('pathname').should('not.eq', '/login') // Wait until login request fully completes
-
-        // Leave current organization
-        cy.visit('/organization/members')
-        cy.get('[data-attr=org-members-table] .anticon-logout').click()
-        cy.get('.ant-modal-confirm-btns button').contains('Leave').click()
-        cy.location('pathname').should('include', '/organization/create')
     })
 })
