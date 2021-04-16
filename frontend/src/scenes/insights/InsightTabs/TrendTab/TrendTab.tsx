PostHog/posthog

commit 41995cccf52c770bc8dcd434f8038b8bcd0a4951

Lifecycle toggle (#3961)
* test singular toggle

* allow for multitoggling and clean up code

* remove wip code

* updated styling

* retain lifecycle toggles on filter selections

* convert undefined values to string

* prettier fix

* typescript strict check fix

Co-authored-by: Li Yi Yu <li@posthog.com>

frontend/src/scenes/insights/InsightTabs/TrendTab/TrendTab.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useValues, useActions } from 'kea'
 import { PropertyFilters } from 'lib/components/PropertyFilters/PropertyFilters'
 import { ActionFilter } from '../../ActionFilter/ActionFilter'
-import { Tooltip, Row, Skeleton } from 'antd'
+import { Tooltip, Row, Skeleton, Switch } from 'antd'
 import { BreakdownFilter } from '../../BreakdownFilter'
 import { CloseButton } from 'lib/components/CloseButton'
 import { InfoCircleOutlined } from '@ant-design/icons'
@@ -14,6 +14,7 @@ import { FilterType } from '~/types'
 import { Formula } from './Formula'
 import { TestAccountFilter } from 'scenes/insights/TestAccountFilter'
 import { preflightLogic } from 'scenes/PreflightCheck/logic'
+import './TrendTab.scss'
 
 interface TrendTabProps {
     view: string
@@ -25,6 +26,8 @@ export function TrendTab({ view }: TrendTabProps): JSX.Element {
     const { featureFlags } = useValues(featureFlagLogic)
     const { preflight } = useValues(preflightLogic)
     const [isUsingFormulas, setIsUsingFormulas] = useState(filters.formula ? true : false)
+    const { toggleLifecycle } = useActions(trendsLogic)
+    const lifecycleNames = ['new', 'resurrecting', 'returning', 'dormant']
 
     return (
         <>
@@ -50,6 +53,24 @@ export function TrendTab({ view }: TrendTabProps): JSX.Element {
                 />
             )}
 
+            <hr />
+            {filters.insight === ViewType.LIFECYCLE && (
+                <>
+                    <h4 className="secondary">Lifecycle Toggles</h4>
+                    {filtersLoading ? (
+                        <Skeleton active />
+                    ) : (
+                        <div className="toggles">
+                            {lifecycleNames.map((cycle, idx) => (
+                                <div key={idx}>
+                                    {cycle}{' '}
+                                    <Switch size="small" defaultChecked onChange={() => toggleLifecycle(cycle)} />
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </>
+            )}
             <hr />
             <h4 className="secondary">Filters</h4>
             {filtersLoading ? (

frontend/src/scenes/insights/InsightTabs/TrendTab/TrendTab.scss
@@ -1,0 +1,9 @@
+.toggles {
+    text-transform: uppercase;
+    div {
+        font-size: 12px;
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 4px;
+    }
+}

frontend/src/scenes/trends/trendsLogic.ts
@@ -228,6 +228,7 @@ export const trendsLogic = kea<
         setVisibilityById: (entry: Record<number, boolean>) => ({ entry }),
         loadMoreBreakdownValues: true,
         setBreakdownValuesLoading: (loading: boolean) => ({ loading }),
+        toggleLifecycle: (lifecycleName: string) => ({ lifecycleName }),
     }),
 
     reducers: ({ props }) => ({
@@ -270,6 +271,18 @@ export const trendsLogic = kea<
             [] as IndexedTrendResult[],
             {
                 setIndexedResults: ({}, { results }) => results,
+            },
+        ],
+        toggledLifecycles: [
+            ['new', 'resurrecting', 'returning', 'dormant'],
+            {
+                toggleLifecycle: (state, { lifecycleName }) => {
+                    if (state.includes(lifecycleName)) {
+                        return state.filter((lifecycles) => lifecycles !== lifecycleName)
+                    }
+                    state.push(lifecycleName)
+                    return state
+                },
             },
         ],
         visibilityMap: [
@@ -349,6 +362,12 @@ export const trendsLogic = kea<
     listeners: ({ actions, values, props }) => ({
         setDisplay: async ({ display }) => {
             actions.setFilters({ display })
+        },
+        toggleLifecycle: () => {
+            const toggledResults = values.results
+                .filter((result) => values.toggledLifecycles.includes(String(result.status)))
+                .map((result, idx) => ({ ...result, id: idx }))
+            actions.setIndexedResults(toggledResults)
         },
         refreshCohort: () => {
             cohortLogic({
@@ -438,10 +457,20 @@ export const trendsLogic = kea<
                 })
             }
 
-            const indexedResults = values.results.map((element, index) => {
-                actions.setVisibilityById({ [`${index}`]: true })
-                return { ...element, id: index }
-            })
+            let indexedResults
+            if (values.filters.insight !== ViewType.LIFECYCLE) {
+                indexedResults = values.results.map((element, index) => {
+                    actions.setVisibilityById({ [`${index}`]: true })
+                    return { ...element, id: index }
+                })
+            } else {
+                indexedResults = values.results
+                    .filter((result) => values.toggledLifecycles.includes(String(result.status)))
+                    .map((result, idx) => {
+                        actions.setVisibilityById({ [`${idx}`]: true })
+                        return { ...result, id: idx }
+                    })
+            }
             actions.setIndexedResults(indexedResults)
         },
         [dashboardItemsModel.actionTypes.refreshAllDashboardItems]: (filters: Record<string, any>) => {

frontend/src/types.ts
@@ -547,6 +547,7 @@ export interface TrendResult {
     label: string
     labels: string[]
     breakdown_value?: string | number
+    status?: string
 }
 
 export interface TrendResultWithAggregate extends TrendResult {

commit 60d48a3fadf4ad61a3bfabc2a8d074d7669d2a57

Avoid double-stacked <hr /> lines in trends
Broken by https://github.com/PostHog/posthog/pull/3961

frontend/src/scenes/insights/InsightTabs/TrendTab/TrendTab.tsx
@@ -53,9 +53,9 @@ export function TrendTab({ view }: TrendTabProps): JSX.Element {
                 />
             )}
 
-            <hr />
             {filters.insight === ViewType.LIFECYCLE && (
                 <>
+                    <hr />
                     <h4 className="secondary">Lifecycle Toggles</h4>
                     {filtersLoading ? (
                         <Skeleton active />