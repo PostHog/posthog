24baf1c7ed2461abdf2c80aefb66e37731bda919
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-18 23:55:46 -0700

    release filter_by_session_props

--- frontend/src/scenes/sessions/Sessions.tsx
+++ frontend/src/scenes/sessions/Sessions.tsx
@@ -3,32 +3,20 @@ import { SessionsView } from './SessionsView'
 import { SavedFiltersMenu } from 'scenes/sessions/filters/SavedFiltersMenu'
 import { PageHeader } from 'lib/components/PageHeader'
 import { Divider } from 'antd'
-import { useValues } from 'kea'
-import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
 export function Sessions(): JSX.Element {
-    const { featureFlags } = useValues(featureFlagLogic)
-    if (featureFlags['filter_by_session_props']) {
-        return (
-            <div className="sessions-wrapper">
-                <div className="sessions-sidebar">
-                    <div>
-                        <PageHeader title="Sessions" />
-                        <SavedFiltersMenu />
-                    </div>
-                    <Divider type="vertical" className="sessions-divider" />
+    return (
+        <div className="sessions-wrapper">
+            <div className="sessions-sidebar">
+                <div>
+                    <PageHeader title="Sessions" />
+                    <SavedFiltersMenu />
                 </div>
-                <div className="sessions-with-filters">
-                    <SessionsView />
-                </div>
+                <Divider type="vertical" className="sessions-divider" />
             </div>
-        )
-    } else {
-        return (
-            <>
-                <PageHeader title="Sessions" />
+            <div className="sessions-with-filters">
                 <SessionsView />
-            </>
-        )
-    }
+            </div>
+        </div>
+    )
 }
