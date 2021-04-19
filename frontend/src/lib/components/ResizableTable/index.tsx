7ee5b08db8dc7c70d72a2da10159214ea93c12eb
Sam Winslow <sammywinslow@gmail.com>
2021-04-19 14:00:29 -0400

    wip: wrapper resizes cols proportionally

--- frontend/src/lib/components/ResizableTable/index.tsx
+++ frontend/src/lib/components/ResizableTable/index.tsx
@@ -42,16 +42,19 @@ export function ResizableTable<RecordType extends Record<any, any> = any>({
             width: index === lastIndex ? undefined : minColumnWidth,
         })) as InternalColumnType<RecordType>[]
     })
+    const [headerColumns, setHeaderColumns] = useState(columns)
     const [headerShouldRender, setHeaderShouldRender] = useState(false)
     const scrollWrapperRef = useRef<HTMLDivElement>(null)
     const overlayRef = useRef<HTMLDivElement>(null)
     const timeout: any = useRef()
+
     function setScrollableRight(value: boolean): void {
         if (value) {
             return overlayRef?.current?.classList.add('scrollable-right')
         }
         return overlayRef?.current?.classList.remove('scrollable-right')
     }
+
     function updateScrollGradient(): void {
         if (overlayRef.current) {
             const overlay = overlayRef.current
@@ -62,6 +65,7 @@ export function ResizableTable<RecordType extends Record<any, any> = any>({
             }
         }
     }
+
     function updateColumnWidth(index: number, width: number): void {
         const col = scrollWrapperRef.current?.querySelector(
             // nth-child is 1-indexed. first column is fixed. last column width must be uncontrolled.
@@ -69,6 +73,13 @@ export function ResizableTable<RecordType extends Record<any, any> = any>({
         )
         col?.setAttribute('style', `width: ${width}px;`)
     }
+
+    function unsetLastColumnStyle(): void {
+        // last column width must be uncontrolled.
+        const col = scrollWrapperRef.current?.querySelector('.ant-table-content colgroup col:last-child')
+        col?.removeAttribute('style')
+    }
+
     function updateTableWidth(): void {
         // <table> elements have super strange auto-sizing: (https://css-tricks.com/fixing-tables-long-strings/)
         // We control the width of the <table> based on the width of the virtual header.
@@ -81,8 +92,10 @@ export function ResizableTable<RecordType extends Record<any, any> = any>({
                 table?.setAttribute('style', `width: ${headerWidth}px;`)
             }
         }
+        unsetLastColumnStyle()
     }
-    const handleResize = (index: number): ResizeHandler => (_, { size: { width } }) => {
+
+    const handleColumnResize = (index: number): ResizeHandler => (_, { size: { width } }) => {
         if (timeout.current) {
             cancelAnimationFrame(timeout.current)
         }
@@ -92,24 +105,62 @@ export function ResizableTable<RecordType extends Record<any, any> = any>({
         })
         updateScrollGradient()
     }
+
+    function handleWrapperResize(newWidth: number): void {
+        if (timeout.current) {
+            cancelAnimationFrame(timeout.current)
+        }
+        timeout.current = requestAnimationFrame(function () {
+            const table = scrollWrapperRef.current?.querySelector('.ant-table table')
+            const oldWidth = table?.clientWidth
+            if (!oldWidth || oldWidth === newWidth) {
+                return
+            }
+            setHeaderShouldRender(false)
+            setHeaderColumns((cols) => {
+                const lastIndex = initialColumns.length
+                const nextColumns = cols.map((column, index) =>
+                    index === lastIndex
+                        ? column
+                        : {
+                              ...column,
+                              width: Math.max(((column.width ?? 0) / oldWidth) * newWidth, minColumnWidth),
+                          }
+                )
+                nextColumns.slice(0, lastIndex).forEach((col, index) => {
+                    updateColumnWidth(index, col.width ?? minColumnWidth)
+                })
+                updateTableWidth()
+                return nextColumns
+            })
+            setHeaderShouldRender(true)
+            updateTableWidth()
+        })
+    }
+
+    const resizeObserver = new ResizeObserver((entries) => {
+        entries.forEach(({ contentRect: { width } }) => handleWrapperResize(width))
+    })
+
     useLayoutEffect(() => {
         // Calculate relative column widths (px) once the wrapper is mounted.
         if (scrollWrapperRef.current) {
+            resizeObserver.observe(scrollWrapperRef.current)
             const wrapperWidth = scrollWrapperRef.current.clientWidth
             const gridBasis = columns.reduce((total, { span }) => total + span, 0)
             const columnSpanWidth = getFullwidthColumnSize(wrapperWidth, gridBasis)
             setColumns((cols) => {
                 const lastIndex = cols.length
-                const nextColumns = cols.map((column, index) => {
-                    if (index === lastIndex) {
-                        return column
-                    }
-                    return {
-                        ...column,
-                        render: initialColumns[index].render,
-                        width: Math.max(columnSpanWidth * column.span, minColumnWidth),
-                    }
-                })
+                const nextColumns = cols.map((column, index) =>
+                    index === lastIndex
+                        ? column
+                        : {
+                              ...column,
+                              render: initialColumns[index].render,
+                              width: Math.max(columnSpanWidth * column.span, minColumnWidth),
+                          }
+                )
+                setHeaderColumns(nextColumns)
                 return nextColumns
             })
             updateScrollGradient()
@@ -122,8 +173,8 @@ export function ResizableTable<RecordType extends Record<any, any> = any>({
             <div ref={overlayRef} className="table-gradient-overlay">
                 {headerShouldRender && (
                     <VirtualTableHeader
-                        columns={columns}
-                        handleResize={handleResize}
+                        columns={headerColumns}
+                        handleResize={handleColumnResize}
                         layoutEffect={updateTableWidth}
                         minColumnWidth={minColumnWidth}
                     />
