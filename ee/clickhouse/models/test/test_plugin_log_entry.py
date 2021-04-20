a9016f2833fc89e61f0dc62e375007f02808ec4f
Michael Matloka <dev@twixes.com>
2021-04-21 00:30:55 +0200

    Fix plugin log entry tests across PG/CH

--- ee/clickhouse/models/test/test_plugin_log_entry.py
+++ ee/clickhouse/models/test/test_plugin_log_entry.py
@@ -2,38 +2,25 @@ from django.utils import timezone
 
 from ee.clickhouse.client import sync_execute
 from ee.clickhouse.sql.plugin_log_entries import INSERT_PLUGIN_LOG_ENTRY_SQL
-from ee.clickhouse.util import ClickhouseTestMixin
-from posthog.models import Plugin, PluginLogEntry
-from posthog.models.plugin import fetch_plugin_log_entries
+from posthog.models import PluginLogEntry
 from posthog.models.utils import UUIDT
-from posthog.test.test_plugin_log_entry import TestPluginLogEntry
+from posthog.test.test_plugin_log_entry import factory_test_plugin_log_entry
 
 
-class TestClickhousePluginLogEntry(ClickhouseTestMixin, TestPluginLogEntry):
-    def test_simple_log_is_fetched(self):
-        plugin_server_instance_id = str(UUIDT())
+def plugin_log_factory_ch(*, team_id: int, plugin_id: int, type: PluginLogEntry.Type, message: str, instance_id: str):
+    sync_execute(
+        INSERT_PLUGIN_LOG_ENTRY_SQL,
+        {
+            "id": UUIDT(),
+            "team_id": team_id,
+            "plugin_id": plugin_id,
+            "type": type,
+            "instance_id": instance_id,
+            "message": message,
+            "timestamp": timezone.now().strftime("%Y-%m-%dT%H:%M:%S.%f"),
+        },
+    )
 
-        some_plugin = Plugin.objects.create(organization=self.organization)
 
-        sync_execute(
-            INSERT_PLUGIN_LOG_ENTRY_SQL,
-            {
-                "id": UUIDT(),
-                "team_id": self.team.pk,
-                "plugin_id": some_plugin.pk,
-                "type": PluginLogEntry.Type.INFO,
-                "instance_id": plugin_server_instance_id,
-                "message": "Something occured!",
-                "timestamp": timezone.now().strftime("%Y-%m-%dT%H:%M:%S.%f"),
-            },
-        )
-
-        results = fetch_plugin_log_entries(
-            team_id=self.team.pk,
-            plugin_id=some_plugin.pk,
-            after=timezone.datetime.min,
-            before=timezone.now() + timezone.timedelta(seconds=5),
-        )
-
-        self.assertEqual(len(results), 1)
-        self.assertEqual(results[0].message, "Something occured!")
+class TestEvent(factory_test_plugin_log_entry(plugin_log_factory_ch)):
+    pass
--- posthog/test/test_plugin_log_entry.py
+++ posthog/test/test_plugin_log_entry.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from django.utils import timezone
 
 from posthog.models import Plugin, PluginLogEntry
@@ -6,25 +8,38 @@ from posthog.models.utils import UUIDT
 from posthog.test.base import BaseTest
 
 
-class TestPluginLogEntry(BaseTest):
-    def test_simple_log_is_fetched(self):
-        plugin_server_instance_id = str(UUIDT())
+def factory_test_plugin_log_entry(plugin_log_entry_factory: Callable):
+    class TestPluginLogEntry(BaseTest):
+        def test_simple_log_is_fetched(self):
+            plugin_server_instance_id = str(UUIDT())
 
-        some_plugin = Plugin.objects.create(organization=self.organization)
-        PluginLogEntry.objects.create(
-            team=self.team,
-            plugin=some_plugin,
-            type=PluginLogEntry.Type.INFO,
-            message="Something happened!",
-            instance_id=plugin_server_instance_id,
-        )
+            some_plugin: Plugin = Plugin.objects.create(organization=self.organization)
+            plugin_log_entry_factory(
+                team_id=self.team.pk,
+                plugin_id=some_plugin.pk,
+                type=PluginLogEntry.Type.INFO,
+                message="Something happened!",
+                instance_id=plugin_server_instance_id,
+            )
 
-        results = fetch_plugin_log_entries(
-            team_id=self.team.pk,
-            plugin_id=some_plugin.pk,
-            after=timezone.datetime.min,
-            before=timezone.now() + timezone.timedelta(seconds=5),
-        )
+            results = fetch_plugin_log_entries(
+                team_id=self.team.pk,
+                plugin_id=some_plugin.pk,
+                after=timezone.datetime.min,
+                before=timezone.now() + timezone.timedelta(seconds=5),
+            )
 
-        self.assertEqual(len(results), 1)
-        self.assertEqual(results[0].message, "Something happened!")
+            self.assertEqual(len(results), 1)
+            self.assertEqual(results[0].message, "Something happened!")
+
+    return TestPluginLogEntry
+
+
+def plugin_log_factory_pg(*, team_id: int, plugin_id: int, type: PluginLogEntry.Type, message: str, instance_id: str):
+    PluginLogEntry.objects.create(
+        team_id=team_id, plugin_id=plugin_id, type=type, message=message, instance_id=instance_id,
+    )
+
+
+class TestEvent(factory_test_plugin_log_entry(plugin_log_factory_pg)):
+    pass
