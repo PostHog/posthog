d4cc7a593b07a03fc6a6bb5dbde33278378ebb6c
Marius Andra <marius.andra@gmail.com>
2021-04-22 14:07:00 +0200

    Teardown on reload (#314)
    * call teardown if reloading plugin
    
    * call teardown if reloading plugin
    
    * dont mind me, just testing something
    
    * refactor `teardownPlugins` params to accept just one plugin_config
    
    * no need to create an array, just use the iterator
    
    * skip calling the same code twice and leaking abstractions
    
    * revert test change
    
    * clean args and increase delay for test
    
    * play with delays
    
    Co-authored-by: yakkomajuri <yakko.majuri@gmail.com>
    Co-authored-by: Michael Matloka <dev@twixes.com>

--- tests/postgres/teardown.test.ts
+++ tests/postgres/teardown.test.ts
@@ -1,4 +1,5 @@
 import { startPluginsServer } from '../../src/main/pluginsServer'
+import { delay } from '../../src/shared/utils'
 import { LogLevel } from '../../src/types'
 import { makePiscina } from '../../src/worker/piscina'
 import { pluginConfig39 } from '../helpers/plugins'
@@ -8,7 +9,7 @@ jest.mock('../../src/shared/status')
 jest.setTimeout(60000) // 60 sec timeout
 
 describe('teardown', () => {
-    test('teardown code runs', async () => {
+    test('teardown code runs when stopping', async () => {
         await resetTestDatabase(`
             async function processEvent (event) {
                 event.properties.processed = 'hell yes'
@@ -22,8 +23,6 @@ describe('teardown', () => {
         const { stop } = await startPluginsServer(
             {
                 WORKER_CONCURRENCY: 2,
-                PLUGINS_CELERY_QUEUE: 'test-plugins-celery-queue',
-                CELERY_DEFAULT_QUEUE: 'test-celery-default-queue',
                 LOG_LEVEL: LogLevel.Log,
                 KAFKA_ENABLED: false,
             },
@@ -38,5 +37,46 @@ describe('teardown', () => {
         // verify the teardownPlugin code runs
         const error2 = await getErrorForPluginConfig(pluginConfig39.id)
         expect(error2.message).toBe('This Happened In The Teardown Palace')
+    })
+
+    test('teardown code runs when reloading', async () => {
+        const resetDatabasePlugin = () =>
+            resetTestDatabase(`
+            async function processEvent (event) {
+                event.properties.processed = 'hell yes'
+                event.properties.upperUuid = event.properties.uuid?.toUpperCase()
+                return event
+            }
+            async function teardownPlugin() {
+                console.log('tearing down')
+                throw new Error('This Happened In The Teardown Palace')
+            }
+        `)
+        await resetDatabasePlugin()
+        const { piscina, stop } = await startPluginsServer(
+            {
+                WORKER_CONCURRENCY: 2,
+                LOG_LEVEL: LogLevel.Log,
+                KAFKA_ENABLED: false,
+            },
+            makePiscina
+        )
+
+        const error1 = await getErrorForPluginConfig(pluginConfig39.id)
+        expect(error1).toBe(null)
+
+        await delay(1000)
+        await resetDatabasePlugin()
+        await delay(1000)
+        await piscina?.broadcastTask({ task: 'reloadPlugins' })
+
+        // this teardown will happen async
+        await delay(3000)
+
+        // verify the teardownPlugin code runs
+        const error2 = await getErrorForPluginConfig(pluginConfig39.id)
+        expect(error2.message).toBe('This Happened In The Teardown Palace')
+
+        await stop()
     })
 })
