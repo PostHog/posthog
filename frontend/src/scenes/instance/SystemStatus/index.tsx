331f4d5abd67f1c83ca7bf43eff71f9568c3149d
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-29 16:07:08 -0700

    site url config warning

--- frontend/src/scenes/instance/SystemStatus/index.tsx
+++ frontend/src/scenes/instance/SystemStatus/index.tsx
@@ -6,9 +6,21 @@ import { systemStatusLogic } from './systemStatusLogic'
 import { useValues } from 'kea'
 import { PageHeader } from 'lib/components/PageHeader'
 import { SystemStatusSubrows } from '~/types'
+import { preflightLogic } from 'scenes/PreflightCheck/logic'
+
+function RenderValue(value: any): JSX.Element | string {
+    if (typeof value === 'boolean') {
+        return <Tag color={value ? 'success' : 'error'}>{value ? 'Yes' : 'No'}</Tag>
+    }
+    if (value === null || value === undefined || value === '') {
+        return <Tag>Unknown</Tag>
+    }
+    return value.toString()
+}
 
 export function SystemStatus(): JSX.Element {
     const { systemStatus, systemStatusLoading, error } = useValues(systemStatusLogic)
+    const { configOptions, preflight, preflightLoading, siteUrlMisconfigured } = useValues(preflightLogic)
 
     const columns = [
         {
@@ -19,15 +31,7 @@ export function SystemStatus(): JSX.Element {
         {
             title: 'Value',
             dataIndex: 'value',
-            render: function RenderValue(value: any) {
-                if (typeof value === 'boolean') {
-                    return <Tag color={value ? 'success' : 'error'}>{value ? 'Yes' : 'No'}</Tag>
-                }
-                if (value === null || value === undefined) {
-                    return <Tag>Unknown</Tag>
-                }
-                return value.toString()
-            },
+            render: RenderValue,
         },
     ]
 
@@ -39,11 +43,34 @@ export function SystemStatus(): JSX.Element {
             />
             {error && (
                 <Alert
-                    message={error || <span>Something went wrong. Please try again or contact us.</span>}
+                    message="Something went wrong"
+                    description={error || <span>An unknown error occurred. Please try again or contact us.</span>}
                     type="error"
+                    showIcon
+                />
+            )}
+            {siteUrlMisconfigured && (
+                <Alert
+                    message="Misconfiguration detected"
+                    description={
+                        <>
+                            Your <code>SITE_URL</code> environment variable seems misconfigured. Your{' '}
+                            <code>SITE_URL</code> is set to <b>{RenderValue(preflight?.site_url)}</b> but you're
+                            currently browsing this page from{' '}
+                            <b>
+                                <code>{window.location.origin}</code>
+                            </b>
+                            . In order for PostHog to work properly, please set this to the origin where your instance
+                            is hosted.
+                        </>
+                    }
+                    showIcon
+                    type="warning"
+                    style={{ marginBottom: 32 }}
                 />
             )}
             <Card>
+                <h3 className="l3">Key metrics</h3>
                 <Table
                     className="system-status-table"
                     size="small"
@@ -59,6 +86,18 @@ export function SystemStatus(): JSX.Element {
                         rowExpandable: (row) => !!row.subrows && row.subrows.rows.length > 0,
                         expandRowByClick: true,
                     }}
+                />
+            </Card>
+            <Card style={{ marginTop: 32 }}>
+                <h3 className="l3">Configuration options</h3>
+                <Table
+                    className="system-config-table"
+                    size="small"
+                    rowKey="metric"
+                    pagination={{ pageSize: 99999, hideOnSinglePage: true }}
+                    dataSource={configOptions}
+                    columns={columns}
+                    loading={preflightLoading}
                 />
             </Card>
         </div>
