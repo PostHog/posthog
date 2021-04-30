ba98c63914f33f93c1da3616c06d8f6aa67ed0d4
Sam Winslow <sammywinslow@gmail.com>
2021-04-30 14:28:37 -0400

    attempt to fix cy spec

--- frontend/src/scenes/sessions/Sessions.cy-spec.js
+++ frontend/src/scenes/sessions/Sessions.cy-spec.js
@@ -89,7 +89,7 @@ describe('<Sessions />', () => {
         })
 
         cy.get('[data-attr="edit-session-filter"]').click()
-        cy.focused().type('unseen').wait(150).type('{downarrow}').type('{enter}')
+        cy.focused().type('unseen').wait(150).type('{downarrow}').wait(150).type('{enter}').wait(150)
         cy.get('[data-attr="sessions-apply-filters"]').click()
 
         cy.wait('@api_sessions').map(helpers.getSearchParameters).should('include', {
