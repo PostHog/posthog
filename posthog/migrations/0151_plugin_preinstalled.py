63fb77e95c61ac283235145e6ffcb4cf8d89832d
Michael Matloka <dev@twixes.com>
2021-05-07 00:45:02 +0200

    Bump migration

--- posthog/migrations/0151_plugin_preinstalled.py
+++ posthog/migrations/0151_plugin_preinstalled.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("posthog", "0148_merge_20210506_0823"),
+        ("posthog", "0150_increase_element_varchars"),
     ]
 
     operations = [
