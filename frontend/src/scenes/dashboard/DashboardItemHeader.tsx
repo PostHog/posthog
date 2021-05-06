05b97c5ad1c55d2e685c4133a886fe309344af9b
Li Yi Yu <li@posthog.com>
2021-05-06 13:21:36 -0400

    set up frontend for dashboard item editing

--- frontend/src/scenes/dashboard/DashboardItemHeader.tsx
+++ frontend/src/scenes/dashboard/DashboardItemHeader.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'lib/components/Link'
 import { PageHeader } from 'lib/components/PageHeader'
-import { ArrowLeftOutlined } from '@ant-design/icons'
+import { ArrowLeftOutlined, EditOutlined, SaveOutlined } from '@ant-design/icons'
 import { IconDashboard } from 'lib/components/icons'
-import { useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import { dashboardLogic } from './dashboardLogic'
 import './Dashboard.scss'
 import { insightLogic } from 'scenes/insights/insightLogic'
 import { userLogic } from 'scenes/userLogic'
+import { dashboardItemsModel } from '~/models/dashboardItemsModel'
+import { Button, Input } from 'antd'
+import { DashboardItemMode } from '../../types'
 
 interface Props {
     dashboardId: number
@@ -16,6 +19,10 @@ interface Props {
 export function DashboardItemHeader({ dashboardId }: Props): JSX.Element {
     const { dashboard } = useValues(dashboardLogic({ id: dashboardId }))
     const { dashboardItem } = useValues(insightLogic)
+    const { dashboardItemMode } = useValues(dashboardItemsModel)
+    const { setDashboardItemMode, updateDashboardItem } = useActions(dashboardItemsModel)
+    const [newDescription, setNewDescription] = useState(dashboardItem.description) // Used to update the input immediately, debouncing API calls
+
     const { user } = useValues(userLogic)
 
     return (
@@ -33,9 +40,45 @@ export function DashboardItemHeader({ dashboardId }: Props): JSX.Element {
                             <Link to={`/dashboard/${dashboardId}`}>{dashboard?.name}</Link> dashboard.
                         </span>
                     </div>
-                    {user?.organization?.available_features?.includes('dashboard_collaboration') && (
+                    {true && (
                         <>
-                            <div className="description">{dashboardItem.description}</div>
+                            <div className="description">
+                                {dashboardItemMode === DashboardItemMode.Edit ? (
+                                    <>
+                                        <Input.TextArea
+                                            placeholder="Add a description to your dashboard item"
+                                            value={newDescription}
+                                            onChange={(e) => {
+                                                setNewDescription(e.target.value)
+                                            }}
+                                        />
+                                        <Button
+                                            icon={<SaveOutlined />}
+                                            onClick={() => updateDashboardItem(dashboardItem.id, {description: newDescription})}
+                                            type="primary"
+                                            data-attr="dashboard-item-description-submit"
+                                            htmlType="submit"
+                                        >
+                                            Save changes
+                                        </Button>
+                                    </>
+
+                                ) : (
+                                    <div
+                                        className="edit-box"
+                                        onClick={() =>
+                                            setDashboardItemMode(DashboardItemMode.Edit)
+                                        }
+                                    >
+                                    {dashboardItem.description ? (
+                                        <span>{dashboardItem.description}</span>
+                                    ) : (
+                                        <span className="add-description">Add a description...</span>
+                                    )}
+                                    <EditOutlined />
+                                </div>
+                                )}
+                            </div>
                         </>
                     )}
                 </div>
