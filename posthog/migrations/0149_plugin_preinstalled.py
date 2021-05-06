a5ddbe959d856a8efc9878cdcaf1b500ce26216a
Michael Matloka <dev@twixes.com>
2021-05-06 16:07:36 +0200

    Bump migration

--- posthog/migrations/0149_plugin_preinstalled.py
+++ posthog/migrations/0149_plugin_preinstalled.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("posthog", "0146_eventproperty_sync"),
+        ("posthog", "0148_merge_20210506_0823"),
     ]
 
     operations = [
