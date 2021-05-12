76fb1a2192ce26bb3ca747d535b0bfdebaffa95d
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-05-12 05:21:28 -0700

    basic insight name

--- frontend/src/scenes/insights/InsightTabs/InsightTitle.tsx
+++ frontend/src/scenes/insights/InsightTabs/InsightTitle.tsx
@@ -1,8 +1,9 @@
 import { Button } from 'antd'
 import { SaveToDashboard } from 'lib/components/SaveToDashboard/SaveToDashboard'
-import React from 'react'
-import { SaveOutlined } from '@ant-design/icons'
+import React, { useState } from 'react'
+import { SaveOutlined, DashboardOutlined } from '@ant-design/icons'
 import { FilterType } from '~/types'
+import { router } from 'kea-router'
 
 export interface InsightTitleProps {
     filters: FilterType
@@ -10,10 +11,17 @@ export interface InsightTitleProps {
 }
 
 export function InsightTitle({ annotations, filters }: InsightTitleProps): JSX.Element {
+    const [{ fromItemName, fromDashboard }] = useState(router.values.hashParams)
     return (
         <>
             <h3 className="l3" style={{ display: 'flex', alignItems: 'center' }}>
-                Unsaved query{' '}
+                {fromDashboard && (
+                    <DashboardOutlined
+                        style={{ color: 'var(--warning)', marginRight: 4 }}
+                        title="Insight saved on dashboard"
+                    />
+                )}
+                {fromItemName || 'Unsaved query'}{' '}
                 <SaveToDashboard
                     displayComponent={<Button type="link" size="small" icon={<SaveOutlined />} />}
                     item={{
