b1589b3bb6e45864221374cc374bc521caf89b6d
Sam Winslow <sammywinslow@gmail.com>
2021-05-14 07:23:38 -0400

    Add reset button for insights

--- frontend/src/scenes/insights/InsightTabs/InsightTitle.tsx
+++ frontend/src/scenes/insights/InsightTabs/InsightTitle.tsx
@@ -1,7 +1,7 @@
 import { Button } from 'antd'
 import { SaveToDashboard } from 'lib/components/SaveToDashboard/SaveToDashboard'
 import React, { useState } from 'react'
-import { SaveOutlined, DashboardOutlined } from '@ant-design/icons'
+import { SaveOutlined, DashboardOutlined, UndoOutlined } from '@ant-design/icons'
 import { FilterType } from '~/types'
 import { router } from 'kea-router'
 
@@ -14,27 +14,37 @@ export function InsightTitle({ annotations, filters }: InsightTitleProps): JSX.E
     const [{ fromItemName, fromDashboard }] = useState(router.values.hashParams)
     return (
         <>
-            <h3 className="l3" style={{ display: 'flex', alignItems: 'center' }}>
+            <h3 className="l3" style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
                 {fromDashboard && (
                     <DashboardOutlined
                         style={{ color: 'var(--warning)', marginRight: 4 }}
                         title="Insight saved on dashboard"
                     />
                 )}
-                {fromItemName || 'Unsaved query'}{' '}
-                <SaveToDashboard
-                    displayComponent={
-                        <Button type="link" size="small" icon={<SaveOutlined />}>
-                            Add to dashboard
-                        </Button>
-                    }
-                    item={{
-                        entity: {
-                            filters,
-                            annotations,
-                        },
-                    }}
-                />
+                <div style={{ paddingRight: '0.5em' }}>{fromItemName || 'Unsaved query'}</div>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <SaveToDashboard
+                        displayComponent={
+                            <Button type="link" size="small" icon={<SaveOutlined />}>
+                                Add to dashboard
+                            </Button>
+                        }
+                        item={{
+                            entity: {
+                                filters,
+                                annotations,
+                            },
+                        }}
+                    />
+                    <Button
+                        type="link"
+                        size="small"
+                        icon={<UndoOutlined />}
+                        onClick={() => router.actions.push(`/insights?insight=${filters.insight || ''}`)}
+                    >
+                        Reset
+                    </Button>
+                </div>
             </h3>
         </>
     )
