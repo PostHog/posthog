4a6d5dd07a3675f32398d630ddf347c6a3a16bd5
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-05-20 16:18:04 +0300

    Move instance/status api code under viewSet
    I will be adding other endpoints, which should live under it rather than
    separately

--- posthog/views.py
+++ posthog/views.py
@@ -1,41 +1,30 @@
 import os
 from functools import wraps
-from typing import Dict, List, Union
+from typing import Dict, Union
 
 import sentry_sdk
 from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required as base_login_required
-from django.db import DEFAULT_DB_ALIAS, connection, connections
+from django.db import DEFAULT_DB_ALIAS, connections
 from django.db.migrations.executor import MigrationExecutor
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect
 from django.views.decorators.cache import never_cache
-from rest_framework.exceptions import AuthenticationFailed
 
 from posthog.ee import is_clickhouse_enabled
 from posthog.email import is_email_available
-from posthog.internal_metrics.team import get_internal_metrics_dashboards
 from posthog.models import User
 from posthog.utils import (
-    get_plugin_server_job_queues,
-    get_redis_info,
-    get_redis_queue_depth,
-    get_table_approx_count,
-    get_table_size,
+    get_available_social_auth_providers,
+    get_available_timezones_with_offsets,
+    get_celery_heartbeat,
     is_celery_alive,
     is_plugin_server_alive,
     is_postgres_alive,
     is_redis_alive,
 )
 from posthog.version import VERSION
-
-from .utils import (
-    get_available_social_auth_providers,
-    get_available_timezones_with_offsets,
-    get_celery_heartbeat,
-    get_plugin_server_version,
-)
 
 ROBOTS_TXT_CONTENT = "User-agent: *\nDisallow: /"
 
@@ -85,120 +74,6 @@ def stats(request):
 
 def robots_txt(request):
     return HttpResponse(ROBOTS_TXT_CONTENT, content_type="text/plain")
-
-
-@never_cache
-@login_required
-def system_status(request):
-    is_multitenancy: bool = getattr(settings, "MULTI_TENANCY", False)
-
-    if is_multitenancy and not request.user.is_staff:
-        raise AuthenticationFailed(detail="You're not authorized.")
-
-    from .models import Element, Event, SessionRecordingEvent
-
-    redis_alive = is_redis_alive()
-    postgres_alive = is_postgres_alive()
-
-    metrics: List[Dict[str, Union[str, bool, int, float]]] = []
-
-    metrics.append({"key": "posthog_version", "metric": "PostHog version", "value": VERSION})
-
-    metrics.append(
-        {
-            "key": "analytics_database",
-            "metric": "Analytics database in use",
-            "value": "ClickHouse" if is_clickhouse_enabled() else "Postgres",
-        }
-    )
-
-    metrics.append({"key": "plugin_sever_alive", "metric": "Plugin server alive", "value": is_plugin_server_alive()})
-    metrics.append(
-        {
-            "key": "plugin_sever_version",
-            "metric": "Plugin server version",
-            "value": get_plugin_server_version() or "unknown",
-        }
-    )
-
-    plugin_server_queues = get_plugin_server_job_queues()
-    metrics.append(
-        {
-            "key": "plugin_sever_job_queues",
-            "metric": "Job queues enabled in plugin server",
-            "value": ", ".join([q.capitalize() for q in plugin_server_queues]) if plugin_server_queues else "unknown",
-        }
-    )
-
-    metrics.append({"key": "db_alive", "metric": "Postgres database alive", "value": postgres_alive})
-    if postgres_alive:
-        postgres_version = connection.cursor().connection.server_version
-        metrics.append(
-            {
-                "key": "pg_version",
-                "metric": "Postgres version",
-                "value": f"{postgres_version // 10000}.{(postgres_version // 100) % 100}.{postgres_version % 100}",
-            }
-        )
-
-        if not is_clickhouse_enabled():
-            event_table_count = get_table_approx_count(Event._meta.db_table)
-            event_table_size = get_table_size(Event._meta.db_table)
-
-            element_table_count = get_table_approx_count(Element._meta.db_table)
-            element_table_size = get_table_size(Element._meta.db_table)
-
-            session_recording_event_table_count = get_table_approx_count(SessionRecordingEvent._meta.db_table)
-            session_recording_event_table_size = get_table_size(SessionRecordingEvent._meta.db_table)
-
-            metrics.append(
-                {
-                    "metric": "Postgres elements table size",
-                    "value": f"{element_table_count} rows (~{element_table_size})",
-                }
-            )
-            metrics.append(
-                {"metric": "Postgres events table size", "value": f"{event_table_count} rows (~{event_table_size})"}
-            )
-            metrics.append(
-                {
-                    "metric": "Postgres session recording table size",
-                    "value": f"{session_recording_event_table_count} rows (~{session_recording_event_table_size})",
-                }
-            )
-    if is_clickhouse_enabled():
-        from ee.clickhouse.system_status import system_status
-
-        metrics.extend(list(system_status()))
-
-    metrics.append({"key": "redis_alive", "metric": "Redis alive", "value": redis_alive})
-    if redis_alive:
-        import redis
-
-        try:
-            redis_info = get_redis_info()
-            redis_queue_depth = get_redis_queue_depth()
-            metrics.append({"metric": "Redis version", "value": f"{redis_info.get('redis_version')}"})
-            metrics.append({"metric": "Redis current queue depth", "value": f"{redis_queue_depth}"})
-            metrics.append(
-                {"metric": "Redis connected client count", "value": f"{redis_info.get('connected_clients')}"}
-            )
-            metrics.append({"metric": "Redis memory used", "value": f"{redis_info.get('used_memory_human', '?')}B"})
-            metrics.append(
-                {"metric": "Redis memory peak", "value": f"{redis_info.get('used_memory_peak_human', '?')}B"}
-            )
-            metrics.append(
-                {
-                    "metric": "Redis total memory available",
-                    "value": f"{redis_info.get('total_system_memory_human', '?')}B",
-                }
-            )
-        except redis.exceptions.ConnectionError as e:
-            metrics.append(
-                {"metric": "Redis metrics", "value": f"Redis connected but then failed to return metrics: {e}"}
-            )
-
-    return JsonResponse({"results": {"overview": metrics, "internal_metrics": get_internal_metrics_dashboards()}})
 
 
 @never_cache
