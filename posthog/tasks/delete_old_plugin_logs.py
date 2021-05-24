eaf2459724cfedc29a0d995040d4a261bb81ffef
Michael Matloka <dev@twixes.com>
2021-05-24 22:17:01 +0200

    Save Organization.available_features as a DB column (#4426)
    * Save Organization.available_features as a DB column
    
    * `update_available_features()` before an organization is created too
    
    * Run the task half past the hour
    
    * Adjust tests and fix available_features sync on start
    
    * Remove redundant null=False
    
    * Fix `Organization.update_available_features()`
    
    * Run Cloud tests in respect to `posthog-cloud` `4426-fix` branch
    
    * Revert "Run Cloud tests in respect to `posthog-cloud` `4426-fix` branch"
    
    This reverts commit 421e8541b35cbb4b247c6a7970a081c592bfdb5a.

--- posthog/tasks/delete_old_plugin_logs.py
+++ posthog/tasks/delete_old_plugin_logs.py
@@ -1,14 +1,10 @@
-import logging
-
 from django.utils import timezone
 
 from posthog.models import PluginLogEntry
-
-logger = logging.getLogger(__name__)
 
 TTL_WEEKS = 1
 
 
 def delete_old_plugin_logs() -> None:
-    """Plugin log entries have an in-DB TTL of 1 week in ClickHouse, for Postgres we need a periodic task."""
+    """Plugin log entries have an in-DB TTL in ClickHouse, for Postgres we need a periodic task."""
     PluginLogEntry.objects.filter(timestamp__lte=timezone.now() - timezone.timedelta(weeks=TTL_WEEKS)).delete()
