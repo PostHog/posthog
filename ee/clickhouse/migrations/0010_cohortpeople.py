152c6369c90d976280147bd5fb8ac3b09f602685
eric <eeoneric@gmail.com>
2021-05-24 13:17:39 -0400

    use var

--- ee/clickhouse/migrations/0010_cohortpeople.py
+++ ee/clickhouse/migrations/0010_cohortpeople.py
@@ -1,16 +1,7 @@
 from infi.clickhouse_orm import migrations
 
+from ee.clickhouse.sql.cohort import CREATE_COHORTPEOPLE_TABLE_SQL
+
 operations = [
-    migrations.RunSQL(
-        """
-    CREATE TABLE cohortpeople
-    (
-        person_id UUID,
-        cohort_id Int64,
-        team_id Int64,
-        sign Int8
-    ) ENGINE = CollapsingMergeTree(sign)
-    Order By (team_id, cohort_id, person_id)
-    """
-    ),
+    migrations.RunSQL(CREATE_COHORTPEOPLE_TABLE_SQL),
 ]
