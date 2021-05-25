349fb5210e183d057982b2a00c1deed25fffbb42
Buddy Williams <buddy@posthog.com>
2021-05-25 17:02:58 -0400

    wip: added in new query for testing

--- ee/clickhouse/queries/test/test_funnel.py
+++ ee/clickhouse/queries/test/test_funnel.py
@@ -46,6 +46,25 @@ class TestFunnelTrends(ClickhouseTestMixin, funnel_trends_test_factory(Clickhous
         _create_event(event="step none", distinct_id="query_four", team=self.team, timestamp="2021-05-06 00:00:00")
 
         raw_sql = """
+select addDays(toDate('{start_timestamp}'), number) as date,
+       E.distinct_id,
+       E.max_step
+from numbers(
+    dateDiff('day', toDateTime('{start_timestamp}'), toDateTime('{end_timestamp}')) + 1
+) as N
+left outer join (
+    select
+        distinct_id,
+        windowFunnel(6048000000000000)(toUInt64(toUnixTimestamp64Micro(timestamp)), event = 'step one', event = 'step two', event = 'step three') as max_step,
+        arrayJoin(groupArray(toDate(timestamp))) as date
+    from events
+    group by distinct_id
+) as E
+on addDays(toDate('{start_timestamp}'), number) = E.date
+;
+        """
+
+        raw_sql2 = """
 select date,
        count(*),
        groupArray(max_step),
