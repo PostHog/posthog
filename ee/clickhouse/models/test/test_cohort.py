776dba2a64af0af5018455a7cbeb8d8f43a982cd
eric <eeoneric@gmail.com>
2021-05-26 14:07:05 -0400

    add test for event_id

--- ee/clickhouse/models/test/test_cohort.py
+++ ee/clickhouse/models/test/test_cohort.py
@@ -109,6 +109,53 @@ class TestCohort(ClickhouseTestMixin, BaseTest):
         result = sync_execute(final_query, {**params, "team_id": self.team.pk})
         self.assertEqual(len(result), 1)
 
+    def test_prop_cohort_basic_event_days(self):
+
+        _create_person(distinct_ids=["some_other_id"], team_id=self.team.pk, properties={"$some_prop": "something"})
+
+        _create_person(
+            distinct_ids=["some_id"],
+            team_id=self.team.pk,
+            properties={"$some_prop": "something", "$another_prop": "something"},
+        )
+
+        _create_event(
+            event="$pageview",
+            team=self.team,
+            distinct_id="some_id",
+            properties={"attr": "some_val"},
+            timestamp=datetime(2020, 1, 9, 12, 0, 1),
+        )
+
+        _create_event(
+            event="$pageview",
+            team=self.team,
+            distinct_id="some_other_id",
+            properties={"attr": "some_val"},
+            timestamp=datetime(2020, 1, 5, 12, 0, 1),
+        )
+
+        with freeze_time("2020-01-10"):
+            cohort1 = Cohort.objects.create(
+                team=self.team, groups=[{"event_id": "$pageview", "days": 1}], name="cohort1",
+            )
+
+            filter = Filter(data={"properties": [{"key": "id", "value": cohort1.pk, "type": "cohort"}],})
+            query, params = parse_prop_clauses(filter.properties, self.team.pk)
+            final_query = "SELECT uuid FROM events WHERE team_id = %(team_id)s {}".format(query)
+            result = sync_execute(final_query, {**params, "team_id": self.team.pk})
+            self.assertEqual(len(result), 1)
+
+            cohort2 = Cohort.objects.create(
+                team=self.team, groups=[{"event_id": "$pageview", "days": 7}], name="cohort2",
+            )
+
+            filter = Filter(data={"properties": [{"key": "id", "value": cohort2.pk, "type": "cohort"}],})
+            query, params = parse_prop_clauses(filter.properties, self.team.pk)
+            final_query = "SELECT uuid FROM events WHERE team_id = %(team_id)s {}".format(query)
+            result = sync_execute(final_query, {**params, "team_id": self.team.pk})
+            self.assertEqual(len(result), 2)
+
     def test_prop_cohort_basic_action_days(self):
 
         _create_person(distinct_ids=["some_other_id"], team_id=self.team.pk, properties={"$some_prop": "something"})
