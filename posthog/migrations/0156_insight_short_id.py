05fee3ff4d93f979ab8f2ccf8c74bb15ef991d4d
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-05-26 17:23:09 -0700

    fix migration to generate IDs in stages

--- posthog/migrations/0156_insight_short_id.py
+++ posthog/migrations/0156_insight_short_id.py
@@ -5,6 +5,13 @@ from django.db import migrations, models
 import posthog.models.dashboard_item
 
 
+def create_short_ids(apps, schema_editor):
+    DashboardItem = apps.get_model("posthog", "DashboardItem")
+    for obj in DashboardItem.objects.all():
+        obj.short_id = posthog.models.dashboard_item.generate_short_id()
+        obj.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -13,9 +20,13 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
+            model_name="dashboarditem", name="short_id", field=models.CharField(blank=True, max_length=12),
+        ),
+        migrations.RunPython(create_short_ids, migrations.RunPython.noop),
+        migrations.AlterField(
             model_name="dashboarditem",
             name="short_id",
-            field=models.CharField(blank=True, default=posthog.models.dashboard_item.generate_short_id, max_length=12),
+            field=models.CharField(blank=True, max_length=12, default=posthog.models.dashboard_item.generate_short_id),
         ),
         migrations.AlterUniqueTogether(name="dashboarditem", unique_together={("team", "short_id")},),
     ]
