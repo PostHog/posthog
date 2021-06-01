40fec975636a897efa1e4e2f61a7fb0f054c8757
Alex Kim <alexgkim205@gmail.com>
2021-06-01 13:07:46 -0700

    Fix long funnels bad request bug (#4554)

--- cypress/integration/funnels.js
+++ cypress/integration/funnels.js
@@ -64,4 +64,20 @@ describe('Funnels', () => {
 
         cy.get('[data-attr=funnel-viz]').should('exist')
     })
+
+    // Request line too large issue - https://github.com/PostHog/posthog/issues/4554
+    it('Create long funnel', () => {
+        cy.get('[data-attr=add-action-event-button]').click()
+        cy.get('[data-attr=trend-element-subject-0]').click()
+        cy.contains('HogFlix homepage view').click()
+
+        // Create request line that's larger than 4094 bytes by adding action events.
+        const iters = Array.from({ length: 50 }, (v, k) => k + 1)
+        cy.wrap(iters).each(() => {
+            cy.get('[data-attr=add-action-event-button]').click()
+        })
+
+        cy.get('[data-attr=save-funnel-button]').click()
+        cy.get('[data-attr=funnel-viz]').should('exist')
+    })
 })
