3a211bc5888bc5f652d8011e02126add760b3479
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-06-03 10:44:30 -0700

    nps submission

--- frontend/src/lib/experimental/NPSPrompt.tsx
+++ frontend/src/lib/experimental/NPSPrompt.tsx
@@ -3,14 +3,16 @@ import { kea, useActions, useValues } from 'kea'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { CloseOutlined, ArrowLeftOutlined } from '@ant-design/icons'
-import React from 'react'
+import React, { useState } from 'react'
 import './NPSPrompt.scss'
 import { npsLogicType } from './NPSPromptType'
+import posthog from 'posthog-js'
+import nps from './nps.svg'
 
 interface NPSPayload {
-    score: 1 | 3 | 5 // 1 = not disappointed; 3 = somewhat disappointed; 5 = very disappointed
+    score?: 1 | 3 | 5 // 1 = not disappointed; 3 = somewhat disappointed; 5 = very disappointed
+    feedback_score?: string
     feedback_persona?: string
-    feedback_benefits?: string
 }
 
 const npsLogic = kea<npsLogicType<NPSPayload>>({
@@ -18,6 +20,7 @@ const npsLogic = kea<npsLogicType<NPSPayload>>({
         setStep: (step: number) => ({ step }),
         setPayload: (payload: NPSPayload | null, merge: boolean = true) => ({ payload, merge }),
         stepBack: true,
+        submit: true,
     },
     reducers: {
         step: [0, { setStep: (_, { step }) => step }],
@@ -35,21 +38,44 @@ const npsLogic = kea<npsLogicType<NPSPayload>>({
                 actions.setPayload(null)
             }
         },
+        submit: () => {
+            if (!values.payload) {
+                return
+            }
+            posthog.capture('nps feedback', values.payload)
+            actions.setStep(3)
+        },
     }),
 })
 
 export function NPSPrompt(): JSX.Element | null {
     const { featureFlags } = useValues(featureFlagLogic)
-    const { setStep, setPayload, stepBack } = useActions(npsLogic)
+    const { setStep, setPayload, stepBack, submit } = useActions(npsLogic)
     const { step } = useValues(npsLogic)
+    const [step2Content, setStep2Content] = useState('')
+    const [step3Content, setStep3Content] = useState('')
 
     if (!featureFlags[FEATURE_FLAGS.NPS_PROMPT]) {
         return null
     }
 
-    const handleScoreSelect = (score: 1 | 3 | 5): void => {
+    const handleStep1 = (score: 1 | 3 | 5): void => {
         setPayload({ score })
         setStep(1)
+    }
+
+    const handleStep2 = (sendSubmission: boolean = false): void => {
+        setPayload({ feedback_score: step2Content })
+        if (sendSubmission) {
+            // TODO
+        } else {
+            setStep(2)
+        }
+    }
+
+    const handleStep3 = (): void => {
+        setPayload({ feedback_persona: step3Content })
+        submit()
     }
 
     const Header = (
@@ -59,7 +85,7 @@ export function NPSPrompt(): JSX.Element | null {
             </div>
             <div className="nps-progress">
                 {[0, 1, 2].map((val) => (
-                    <div className={`pg-item${val >= step ? ' completed' : ''}`} key={val} />
+                    <div className={`pg-item${val <= step ? ' completed' : ''}`} key={val} />
                 ))}
             </div>
         </div>
@@ -78,13 +104,13 @@ export function NPSPrompt(): JSX.Element | null {
                             <div className="question">How would you feel if you could no longer use PostHog?</div>
 
                             <div className="action-buttons">
-                                <Button className="prompt-button" onClick={() => handleScoreSelect(1)}>
+                                <Button className="prompt-button" onClick={() => handleStep1(1)}>
                                     Not disappointed
                                 </Button>
-                                <Button className="prompt-button" onClick={() => handleScoreSelect(3)}>
+                                <Button className="prompt-button" onClick={() => handleStep1(3)}>
                                     Somewhat disappointed
                                 </Button>
-                                <Button className="prompt-button" onClick={() => handleScoreSelect(5)}>
+                                <Button className="prompt-button" onClick={() => handleStep1(5)}>
                                     Very disappointed
                                 </Button>
                             </div>
@@ -93,10 +119,48 @@ export function NPSPrompt(): JSX.Element | null {
                     {step === 1 && (
                         <div data-attr="nps-step-1">
                             {Header}
-                            <div className="question">
-                                What type of person or company could benefit most from PostHog?
+                            <div className="question">What's the main reason behind this score?</div>
+                            <Input.TextArea
+                                autoFocus
+                                placeholder="You can describe the key benefits you get from PostHog, shortcomings or anything else..."
+                                value={step2Content}
+                                onChange={(e) => setStep2Content(e.target.value)}
+                            />
+                            <div style={{ textAlign: 'left' }} className="mt">
+                                <Button type="link" style={{ paddingLeft: 0 }} onClick={() => handleStep2(true)}>
+                                    Finish
+                                </Button>
+                                <Button style={{ float: 'right' }} onClick={() => handleStep2()}>
+                                    Continue
+                                </Button>
                             </div>
-                            <Input.TextArea autoFocus />
+                        </div>
+                    )}
+                    {step === 2 && (
+                        <div data-attr="nps-step-2">
+                            {Header}
+                            <div className="question">
+                                Last one. What type of person or company do you think could benefit most from PostHog?
+                            </div>
+                            <Input.TextArea
+                                autoFocus
+                                placeholder="You can describe their role, background, company or team size, ..."
+                                value={step3Content}
+                                onChange={(e) => setStep3Content(e.target.value)}
+                            />
+                            <div style={{ textAlign: 'left' }} className="mt">
+                                <Button style={{ float: 'right' }} onClick={handleStep3}>
+                                    Finish
+                                </Button>
+                            </div>
+                        </div>
+                    )}
+                    {step === 3 && (
+                        <div data-attr="nps-step-2" style={{ display: 'flex', alignItems: 'center' }}>
+                            <img src={nps} alt="" height={40} />
+                            <div className="prompt-title" style={{ margin: 0 }}>
+                                Thanks for helping us improve PostHog!
+                            </div>
                         </div>
                     )}
                 </div>
