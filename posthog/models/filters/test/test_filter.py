472b6c411fbc29b481bcc52df7752a19d923bf74
Buddy Williams <buddy@posthog.com>
2021-06-03 16:40:32 -0400

    corrected test to dict test with funnel_window_days

--- posthog/models/filters/test/test_filter.py
+++ posthog/models/filters/test/test_filter.py
@@ -35,7 +35,8 @@ class TestFilter(BaseTest):
             }
         )
         self.assertCountEqual(
-            list(filter.to_dict().keys()), ["events", "display", "compare", "insight", "date_from",],
+            list(filter.to_dict().keys()),
+            ["events", "display", "compare", "insight", "date_from", "funnel_window_days",],
         )
 
 
