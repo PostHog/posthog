665111c8df8cf4ef4afc21931edd187b54bcfbc6
Buddy Williams <buddy@posthog.com>
2021-06-03 13:54:09 -0400

    fixes for automated tests

--- posthog/models/filters/mixins/funnel_window_days.py
+++ posthog/models/filters/mixins/funnel_window_days.py
@@ -9,10 +9,6 @@ class FunnelWindowDaysMixin(BaseParamMixin):
         _days = int(self._data.get(FUNNEL_WINDOW_DAYS, 14))
         return _days
 
-    @include_dict
-    def funnel_window_days_to_dict(self):
-        return {"funnel_window_days": self.funnel_window_days} if self.funnel_window_days else {}
-
     @staticmethod
     def milliseconds_from_days(days):
         second, minute, hour, day = [1000, 60, 60, 24]
