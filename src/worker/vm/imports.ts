98edc49f81590b1a25c3e228d87c8212bcdf81d5
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-06-03 14:46:59 +0300

    [plugin-server] Add `url` as a library (https://github.com/PostHog/plugin-server/pull/457)
    We can do some url-based analysis/changes based on this.

--- src/worker/vm/imports.ts
+++ src/worker/vm/imports.ts
@@ -7,12 +7,14 @@ import * as genericPool from 'generic-pool'
 import fetch from 'node-fetch'
 import * as pg from 'pg'
 import snowflake from 'snowflake-sdk'
+import * as url from 'url'
 import * as zlib from 'zlib'
 
 import { writeToFile } from './extensions/test-utils'
 
 export const imports = {
     crypto: crypto,
+    url: url,
     zlib: zlib,
     'generic-pool': genericPool,
     'node-fetch': fetch,
