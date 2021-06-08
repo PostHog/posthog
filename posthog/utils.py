27e69d25dafffdcd59951d58714f3259ed505e98
Michael Matloka <dev@twixes.com>
2021-06-08 13:44:11 +0200

    Add docstring

--- posthog/utils.py
+++ posthog/utils.py
@@ -664,6 +664,9 @@ def get_available_timezones_with_offsets() -> Dict[str, float]:
 
 
 def str_to_bool(value: Any) -> bool:
+    """Return whether the provided string (or any value really) represents true. Otherwise false.
+    Just like plugin server stringToBoolean.
+    """
     if not value:
         return False
     return str(value).lower() in ("y", "yes", "t", "true", "on", "1")
