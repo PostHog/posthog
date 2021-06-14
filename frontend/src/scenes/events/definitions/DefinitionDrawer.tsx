7d5a71de2aeca22c64483ad743c7de42fbc7292b
Marius Andra <marius.andra@gmail.com>
2021-06-14 16:40:46 +0200

    tags can be undefined

--- frontend/src/scenes/events/definitions/DefinitionDrawer.tsx
+++ frontend/src/scenes/events/definitions/DefinitionDrawer.tsx
@@ -68,7 +68,7 @@ export function DefinitionDrawer(): JSX.Element {
                                             <Col>
                                                 <h4 className="l4">Tags</h4>
                                                 <ObjectTags
-                                                    tags={definition.tags}
+                                                    tags={definition.tags || []}
                                                     onTagSave={saveNewTag}
                                                     onTagDelete={deleteTag}
                                                     saving={definitionLoading}
