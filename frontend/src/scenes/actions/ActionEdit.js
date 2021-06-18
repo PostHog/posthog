af266c8bc7edbab7799650843a1283f9c4d9fec5
Michael Matloka <dev@twixes.com>
2021-06-18 14:43:14 +0200

    Update `AsyncActionMappingNotice`

--- frontend/src/scenes/actions/ActionEdit.js
+++ frontend/src/scenes/actions/ActionEdit.js
@@ -16,6 +16,10 @@ import dayjs from 'dayjs'
 import { compactNumber } from 'lib/utils'
 import { teamLogic } from 'scenes/teamLogic'
 
+function AsyncActionMappingNotice() {
+    return <p>Please note that actions may be delayed up to 5 minutes due to open-source PostHog configuration.</p>
+}
+
 export function ActionEdit({ action: loadedAction, actionId, apiURL, onSave, temporaryToken }) {
     let logic = actionEditLogic({
         id: actionId,
