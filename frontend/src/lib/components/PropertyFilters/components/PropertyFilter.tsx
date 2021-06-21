16e69c153be9e478eb899414780d19efbef0c535
Sam Winslow <sammywinslow@gmail.com>
2021-06-21 18:42:48 -0400

    init TaxonomicPropertyFilter based on Tabbed variant

--- frontend/src/lib/components/PropertyFilters/components/PropertyFilter.tsx
+++ frontend/src/lib/components/PropertyFilters/components/PropertyFilter.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SelectGradientOverflowProps } from 'lib/components/SelectGradientOverflow'
 import { propertyFilterLogic } from '../propertyFilterLogic'
 import { TabbedPropertyFilter } from './TabbedPropertyFilter'
+import { TaxonomicPropertyFilter } from './TaxonomicPropertyFilter'
 import { UnifiedPropertyFilter } from './UnifiedPropertyFilter'
 
 export interface PropertyFilterInternalProps {
@@ -12,14 +13,16 @@ export interface PropertyFilterInternalProps {
 }
 
 export interface PropertyFilterProps extends PropertyFilterInternalProps {
-    variant: 'tabs' | 'unified'
+    variant: 'tabs' | 'taxonomic' | 'unified'
 }
 
 export function PropertyFilter({ variant = 'tabs', ...props }: PropertyFilterProps): JSX.Element {
     switch (variant) {
-        case 'unified':
-            return <UnifiedPropertyFilter {...props} />
         case 'tabs':
             return <TabbedPropertyFilter {...props} />
+        case 'taxonomic':
+            return <TaxonomicPropertyFilter {...props} />
+        case 'unified':
+            return <UnifiedPropertyFilter {...props} />
     }
 }
