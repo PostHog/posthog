539581852a6c4d7d68c33f32c9181659187692fd
Michael Matloka <dev@twixes.com>
2021-06-22 12:33:53 +0200

    Use prefix + base62

--- posthog/models/utils.py
+++ posthog/models/utils.py
@@ -1,10 +1,13 @@
 import secrets
+import string
 import uuid
 from collections import defaultdict, namedtuple
 from time import time
 from typing import Any, Callable, Dict, Optional
 
 from django.db import models
+
+BASE62 = string.digits + string.ascii_letters  # All lowercase ASCII letters + all uppercase ASCII letters + digits
 
 
 class UUIDT(uuid.UUID):
@@ -98,26 +101,32 @@ def namedtuplefetchall(cursor):
 
 
 def generate_random_token(nbytes: int = 32) -> str:
-
     """Generate a securely random token.
 
     Random 32 bytes - default value here - is believed to be sufficiently secure for practically all purposes:
     https://docs.python.org/3/library/secrets.html#how-many-bytes-should-tokens-use
     """
-    return number_to_b36(secrets.randbits(nbytes * 8))
+    return int_to_base(secrets.randbits(nbytes * 8), 62)
 
 
-def number_to_b36(number: int) -> str:
-    alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
+def generate_random_token_project() -> str:
+    return "phc_" + generate_random_token()  # "c" standing for "client"
+
+
+def generate_random_token_personal() -> str:
+    return "phx_" + generate_random_token()  # "x" standing for nothing in particular
+
+
+def int_to_base(number: int, base: int) -> str:
+    if base > 62:
+        raise ValueError("Cannot convert integer to base above 62")
+    alphabet = BASE62[:base]
     if number < 0:
-        return "-" + number_to_b36(-number)
-
+        return "-" + int_to_base(-number, base)
     value = ""
-
     while number != 0:
         number, index = divmod(number, len(alphabet))
         value = alphabet[index] + value
-
     return value or "0"
 
 
