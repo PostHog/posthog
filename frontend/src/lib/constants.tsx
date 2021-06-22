d2b1a573d20d4afac7d403ab19920db1fbd9965f
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-06-22 21:54:34 +0200

    remove crosshair from lifecycle chart

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -226,7 +226,6 @@ export const WEBHOOK_SERVICES: Record<string, string> = {
 export const FEATURE_FLAGS: Record<string, string> = {
     INGESTION_GRID: 'ingestion-grid-exp-3',
     PROJECT_HOME: 'project-home-exp-5',
-    QUERY_UX_V2: '4050-query-ui-optB',
     EVENT_COLUMN_CONFIG: '4141-event-columns',
     NPS_PROMPT: '4562-nps',
     INGESTION_TAXONOMY: '4267-event-property-taxonomy',
