a5e5c0bae96528737e29adcf109465d6f798e7be
Michael Matloka <dev@twixes.com>
2021-06-25 00:02:47 +0200

    Add ClickhouseFunnelTrendsNew

--- ee/clickhouse/queries/funnels/funnel_trends.py
+++ ee/clickhouse/queries/funnels/funnel_trends.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
+from typing import Dict, List, Tuple, cast
 
 from ee.clickhouse.client import sync_execute
 from ee.clickhouse.models.property import parse_prop_clauses
 from ee.clickhouse.queries.funnels.base import ClickhouseFunnelBase
-from ee.clickhouse.queries.util import get_time_diff, get_trunc_func_ch, parse_timestamps
+from ee.clickhouse.queries.funnels.funnel import ClickhouseFunnelNew
+from ee.clickhouse.queries.util import format_ch_timestamp, get_time_diff, get_trunc_func_ch, parse_timestamps
 from ee.clickhouse.sql.events import NULL_SQL_FUNNEL_TRENDS
 from ee.clickhouse.sql.funnels.funnel_trend import FUNNEL_TREND_SQL
 from ee.clickhouse.sql.person import GET_LATEST_PERSON_DISTINCT_ID_SQL
@@ -91,7 +93,7 @@ class ClickhouseFunnelTrends(ClickhouseFunnelBase):
     def _get_funnel_trend_null_sql(self):
         interval_annotation = get_trunc_func_ch(self._filter.interval)
         num_intervals, seconds_in_interval, round_interval = get_time_diff(
-            self._filter.interval or "day", self._filter.date_from, self._filter.date_to, team_id=self._team.id
+            self._filter.interval or "day", self._filter.date_from, self._filter.date_to, team_id=self._team.pk
         )
         funnel_trend_null_sql = NULL_SQL_FUNNEL_TRENDS.format(
             interval=interval_annotation,
@@ -130,3 +132,134 @@ class ClickhouseFunnelTrends(ClickhouseFunnelBase):
 
     def get_query(self, format_properties):
         pass
+
+
+class ClickhouseFunnelTrendsNew(ClickhouseFunnelBase):
+    def get_query(self, format_properties) -> str:
+        formatted_query = ""
+        max_steps = len(self._filter.entities)
+        if max_steps >= 2:
+            formatted_query = self.build_step_subquery(2, max_steps)
+        else:
+            formatted_query = super()._get_inner_event_query()
+
+        breakdown_prop = self._get_breakdown_select_prop()  # TODO: allow breakdown
+        # TODO: allow intervals other than a day
+        print("!xxx\n", formatted_query, "\n!yyy")
+        query = f"""
+            SELECT toStartOfDay(toDateTime('{self._filter.date_from.strftime(TIMESTAMP_FORMAT)}') + day_index * 86400) AS day,
+                total,
+                completed,
+                percentage,
+                person_ids_total,
+                person_ids_completed
+            FROM numbers({cast(timedelta, self._filter.date_to - self._filter.date_from).days + 1}) AS day_index
+            LEFT OUTER JOIN (
+                SELECT day, start_step + final_step AS total, final_step AS completed, completed / total AS percentage, person_ids_total, person_ids_completed FROM (
+                    SELECT day, countIf(furthest = 1) AS start_step, countIf(furthest={len(self._filter.entities)}) AS final_step, groupArray(person_id) AS person_ids_total, groupArray(person_id_completed) AS person_ids_completed FROM (
+                        SELECT person_id, if(furthest = {len(self._filter.entities)-1}, person_id, NULL) AS person_id_completed, toStartOfDay(timestamp) AS day, max(steps) AS furthest FROM (
+                            SELECT *, {self._get_sorting_condition(max_steps, max_steps)} AS steps FROM (
+                                {formatted_query}
+                            ) WHERE step_0 = 1
+                        ) GROUP BY person_id, day
+                    ) GROUP BY day
+                ) 
+            ) data
+            ON data.day = day 
+            ORDER BY day ASC
+            SETTINGS allow_experimental_window_functions = 1
+            """
+        return query
+
+    def build_step_subquery(self, level_index: int, max_steps: int):
+        breakdown_prop = self._get_breakdown_prop()
+        if level_index >= max_steps:
+            return f"""
+            SELECT 
+            person_id,
+            timestamp,
+            {self.get_partition_cols(1, max_steps)}
+            {breakdown_prop}
+            FROM ({super()._get_inner_event_query()})
+            """
+        else:
+            return f"""
+            SELECT 
+            person_id,
+            timestamp,
+            {self.get_partition_cols(level_index, max_steps)}
+            {breakdown_prop}
+            FROM (
+                SELECT 
+                person_id,
+                timestamp,
+                {self.get_comparison_cols(level_index, max_steps)}
+                {breakdown_prop}
+                FROM ({self.build_step_subquery(level_index + 1, max_steps)})
+            )
+            """
+
+    def get_partition_cols(self, level_index: int, max_steps: int):
+        cols: List[str] = []
+        for i in range(0, max_steps):
+            cols.append(f"step_{i}")
+            if i < level_index:
+                cols.append(f"latest_{i}")
+            else:
+                cols.append(
+                    f"min(latest_{i}) over (PARTITION by person_id ORDER BY timestamp DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) latest_{i}"
+                )
+        return ", ".join(cols)
+
+    def get_comparison_cols(self, level_index: int, max_steps: int):
+        cols: List[str] = []
+        for i in range(0, max_steps):
+            cols.append(f"step_{i}")
+            if i < level_index:
+                cols.append(f"latest_{i}")
+            else:
+                comparison = self._get_comparison_at_step(i, level_index)
+                cols.append(f"if({comparison}, NULL, latest_{i}) as latest_{i}")
+        return ", ".join(cols)
+
+    def _get_breakdown_prop(self) -> str:
+        if self._filter.breakdown:
+            return ", prop"
+        else:
+            return ""
+
+    def _get_comparison_at_step(self, index: int, level_index: int):
+        or_statements: List[str] = []
+
+        for i in range(level_index, index + 1):
+            or_statements.append(f"latest_{i} < latest_{level_index - 1}")
+
+        return " OR ".join(or_statements)
+
+    def _get_sorting_condition(self, curr_index: int, max_steps: int):
+
+        if curr_index == 1:
+            return "1"
+
+        conditions: List[str] = []
+        for i in range(1, curr_index):
+            conditions.append(f"latest_{i - 1} <= latest_{i }")
+            conditions.append(f"latest_{i} <= latest_0 + INTERVAL {self._filter.funnel_window_days} DAY")
+
+        return f"if({' AND '.join(conditions)}, {curr_index}, {self._get_sorting_condition(curr_index - 1, max_steps)})"
+
+    def _get_step_times(self, max_steps: int):
+        conditions: List[str] = []
+        for i in range(1, max_steps):
+            conditions.append(
+                f"if(isNotNull(latest_{i}), dateDiff('second', toDateTime(latest_{i - 1}), toDateTime(latest_{i})), NULL) step{i-1}ToStep{i}Time"
+            )
+
+        return ", ".join(conditions)
+
+    def _get_step_time_avgs(self, max_steps: int):
+        conditions: List[str] = []
+        for i in range(1, max_steps):
+            conditions.append(f"avg(step{i-1}ToStep{i}Time) step{i-1}ToStep{i}Time")
+
+        return ", ".join(conditions)
