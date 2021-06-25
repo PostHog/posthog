0e24d1b2d7c85dfa8b925f44bfa4be290d42e248
Tim Glaser <tim@glsr.nl>
2021-06-25 13:48:18 +0100

    fix errors

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -26,12 +26,5 @@ module.exports = (on, config) => {
         }
     })
 
-    const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
-    on('uncaught:exception', (err) => {
-        /* returning false here prevents Cypress from failing the test */
-        if (resizeObserverLoopErrRe.test(err.message)) {
-            return false
-        }
-    })
     return config
 }
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -42,3 +42,11 @@ afterEach(() => {
         unmount()
     }
 })
+
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
+Cypress.on('uncaught:exception', (err) => {
+    /* returning false here prevents Cypress from failing the test */
+    if (resizeObserverLoopErrRe.test(err.message)) {
+        return false
+    }
+})
