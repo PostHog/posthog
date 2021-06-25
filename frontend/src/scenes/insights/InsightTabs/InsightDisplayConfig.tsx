382923a9ed7b4674234eb92484af97cf7e6b42a2
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-06-25 15:57:12 +0200

    bring back save to dashboard button on funnels

--- frontend/src/scenes/insights/InsightTabs/InsightDisplayConfig.tsx
+++ frontend/src/scenes/insights/InsightTabs/InsightDisplayConfig.tsx
@@ -11,12 +11,13 @@ import { ViewType } from '../insightLogic'
 import { CalendarOutlined } from '@ant-design/icons'
 import { InsightDateFilter } from '../InsightDateFilter'
 import { RetentionDatePicker } from '../RetentionDatePicker'
+import { SaveToDashboard } from 'lib/components/SaveToDashboard/SaveToDashboard'
 
 interface InsightDisplayConfigProps {
     clearAnnotationsToCreate: () => void
     allFilters: FilterType
     activeView: ViewType
-    annotationsToCreate: any[] // TODO: Annotate properly
+    annotationsToCreate: Record<string, any>[] // TODO: Annotate properly
 }
 
 const showIntervalFilter = function (activeView: ViewType, filter: FilterType): boolean {
@@ -79,6 +80,7 @@ const isFunnelEmpty = (filters: FilterType): boolean => {
 export function InsightDisplayConfig({
     allFilters,
     activeView,
+    annotationsToCreate,
     clearAnnotationsToCreate,
 }: InsightDisplayConfigProps): JSX.Element {
     const { featureFlags } = useValues(featureFlagLogic)
@@ -121,6 +123,17 @@ export function InsightDisplayConfig({
                 )}
 
                 {showComparePrevious[activeView] && <CompareFilter />}
+
+                {activeView === ViewType.FUNNELS && (
+                    <SaveToDashboard
+                        item={{
+                            entity: {
+                                filters: allFilters,
+                                annotations: annotationsToCreate,
+                            },
+                        }}
+                    />
+                )}
             </div>
         </div>
     )
