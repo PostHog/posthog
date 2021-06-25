73a9476e78a10f76d5515d9cda74b392f8ce70b3
eric <eeoneric@gmail.com>
2021-06-25 17:48:38 -0400

    test api

--- ee/clickhouse/views/test/test_clickhouse_funnel_person.py
+++ ee/clickhouse/views/test/test_clickhouse_funnel_person.py
@@ -1,22 +1,37 @@
 import json
+from uuid import uuid4
 
 from rest_framework import status
 
-from ee.clickhouse.generate_local import GenerateLocal
+from ee.clickhouse.models.event import create_event
 from ee.clickhouse.util import ClickhouseTestMixin
 from posthog.constants import INSIGHT_FUNNELS, TRENDS_FUNNEL
+from posthog.models.person import Person
 from posthog.test.base import APIBaseTest
 
 
+def _create_person(**kwargs):
+    person = Person.objects.create(**kwargs)
+    return Person(id=person.uuid, uuid=person.uuid)
+
+
+def _create_event(**kwargs):
+    kwargs.update({"event_uuid": uuid4()})
+    create_event(**kwargs)
+
+
 class TestFunnelPerson(ClickhouseTestMixin, APIBaseTest):
-    def setUp(self):
-        super().setUp()
-        GenerateLocal(self.team).generate()
+    def _create_sample_data(self):
+        for i in range(250):
+            _create_person(distinct_ids=[f"user_{i}"], team=self.team)
+            _create_event(event="step one", distinct_id=f"user_{i}", team=self.team, timestamp="2021-05-01 00:00:00")
+            _create_event(event="step two", distinct_id=f"user_{i}", team=self.team, timestamp="2021-05-03 00:00:00")
+            _create_event(event="step three", distinct_id=f"user_{i}", team=self.team, timestamp="2021-05-05 00:00:00")
 
     def test_basic_pagination(self):
+        self._create_sample_data()
         request_data = {
             "insight": INSIGHT_FUNNELS,
-            "display": TRENDS_FUNNEL,
             "interval": "day",
             "actions": json.dumps([]),
             "events": json.dumps(
