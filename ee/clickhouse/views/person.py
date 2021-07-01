4b647a872e09961e8a778630674dd62a52602331
Neil Kakkar <neilkakkar@gmail.com>
2021-07-01 10:41:37 +0100

    cleanup and fix TODOs

--- ee/clickhouse/views/person.py
+++ ee/clickhouse/views/person.py
@@ -19,16 +19,13 @@ from posthog.decorators import cached_function
 from posthog.models import Event, Filter, Person
 
 
-# TODO: Move grabbing all this to Clickhouse. See WIP-people-from-clickhouse branch.
-# TODO: wut, can I remove this TODO? TODO recurse.
 class ClickhousePersonViewSet(PersonViewSet):
 
     lifecycle_class = ClickhouseLifecycle
     retention_class = ClickhouseRetention
     stickiness_class = ClickhouseStickiness
 
-    # TODO: Why isn't POST allowed?? Test & check - request is symmetric with /api/insight/funnel
-    @action(methods=["GET"], detail=False)
+    @action(methods=["GET", "POST"], detail=False)
     def funnel(self, request: Request, **kwargs) -> Response:
         if request.user.is_anonymous or not request.user.team:
             return Response(data=[])
@@ -48,8 +45,6 @@ class ClickhousePersonViewSet(PersonViewSet):
 
         if filter.funnel_viz_type == FunnelVizType.TRENDS:
             funnel_class = ClickhouseFunnelTrendsPersons
-        # elif filter.funnel_order_type == FunnelOrderType.UNORDERED:
-        #     funnel_class = ClickhouseFunnelUnorderedPersons
 
         results = funnel_class(filter, team).run()
         next_url = format_next_absolute_url(request, filter.offset, 100) if len(results) > 99 else None
