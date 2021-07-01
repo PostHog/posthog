7d0ca2bde544f74f02f2a1a68cfc3cf05c3eb882
Michael Matloka <dev@twixes.com>
2021-07-01 09:54:28 +0200

    Add comments to query

--- ee/clickhouse/queries/funnels/funnel_time_to_convert.py
+++ ee/clickhouse/queries/funnels/funnel_time_to_convert.py
@@ -28,12 +28,15 @@ class ClickhouseFunnelTimeToConvert(ClickhouseFunnelNew):
             WITH step_runs AS (
                 {steps_per_person_query}
             ), (
+                -- Automatic bin interval which makes sure that there's always number_of_bins bins
+                -- and that each step run will belong to one bin in results
                 SELECT ceil(max(step_{to_step}_average_conversion_time) / {number_of_bins}) AS bin_base_seconds FROM step_runs
             ) AS bin_base_seconds
             SELECT
                 bin_to_seconds,
                 person_count
             FROM (
+                -- Calculating bins from step runs
                 SELECT
                     floor(step_{to_step}_average_conversion_time / bin_base_seconds + 1) * bin_base_seconds AS bin_to_seconds,
                     count() AS person_count
@@ -42,6 +45,8 @@ class ClickhouseFunnelTimeToConvert(ClickhouseFunnelNew):
                 GROUP BY bin_to_seconds
             ) results
             FULL OUTER JOIN (
+                -- Making sure number_of_bins bins are returned
+                -- Those not present in the results query due to lack of data simply get person_count 0
                 SELECT (number + 1) * bin_base_seconds AS bin_to_seconds FROM numbers({number_of_bins})
             ) fill
             USING (bin_to_seconds)
