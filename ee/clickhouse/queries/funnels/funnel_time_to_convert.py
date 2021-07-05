df77587bb942f1b7525effce17bcc5794268c522
Michael Matloka <dev@twixes.com>
2021-07-05 19:33:17 +0200

    Update SQL comments

--- ee/clickhouse/queries/funnels/funnel_time_to_convert.py
+++ ee/clickhouse/queries/funnels/funnel_time_to_convert.py
@@ -42,8 +42,8 @@ class ClickhouseFunnelTimeToConvert(ClickhouseFunnelNew):
                     {steps_per_person_query}
                 ),
                 histogram_params AS (
-                    -- Automatic bin interval which makes sure that there's always number_of_bins bins
-                    -- and that each step run will belong to one bin in results
+                    -- Binning ensures that each sample belongs to a bin in results
+                    -- If bin_count is not a custom number, it's calculated in bin_count_expression
                     SELECT
                         floor(min(step_{to_step}_average_conversion_time)) AS from_seconds,
                         ceil(max(step_{to_step}_average_conversion_time)) AS to_seconds,
@@ -51,8 +51,13 @@ class ClickhouseFunnelTimeToConvert(ClickhouseFunnelNew):
                         ceil((to_seconds - from_seconds) / {bin_count_identifier}) AS bin_width_seconds
                     FROM step_runs
                 ),
-                {f"( SELECT {bin_count_identifier} FROM histogram_params ) AS {bin_count_identifier}," if bin_count_expression else ""}
+                -- Below CTEs make histogram_params columns available to the query below as straightforward identifiers
                 ( SELECT bin_width_seconds FROM histogram_params ) AS bin_width_seconds,
+                -- bin_count is only made available as an identifier if it had to be calculated
+                {
+                    f"( SELECT {bin_count_identifier} FROM histogram_params ) AS {bin_count_identifier},"
+                    if bin_count_expression else ""
+                }
                 ( SELECT from_seconds FROM histogram_params ) AS histogram_from_seconds,
                 ( SELECT to_seconds FROM histogram_params ) AS histogram_to_seconds
             SELECT
@@ -68,7 +73,7 @@ class ClickhouseFunnelTimeToConvert(ClickhouseFunnelNew):
                 GROUP BY bin_to_seconds
             ) results
             FULL OUTER JOIN (
-                -- Making sure number_of_bins bins are returned
+                -- Making sure bin_count bins are returned
                 -- Those not present in the results query due to lack of data simply get person_count 0
                 SELECT histogram_from_seconds + number * bin_width_seconds AS bin_to_seconds FROM system.numbers LIMIT {bin_count_identifier} + 1
             ) fill
