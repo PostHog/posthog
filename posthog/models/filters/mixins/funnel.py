cf2fc729ef2367e1faf2e973f77f91705217c633
Michael Matloka <dev@twixes.com>
2021-07-08 16:34:09 +0200

    Fix `FunnelTypeMixin.funnel_viz_type`

--- posthog/models/filters/mixins/funnel.py
+++ posthog/models/filters/mixins/funnel.py
@@ -90,7 +90,7 @@ class FunnelTypeMixin(BaseParamMixin):
     def funnel_viz_type(self) -> Optional[FunnelVizType]:
         funnel_viz_type = self._data.get(FUNNEL_VIZ_TYPE)
         if (
-            not funnel_viz_type is None
+            funnel_viz_type is None
             and self._data.get(INSIGHT) == INSIGHT_FUNNELS
             and self._data.get(DISPLAY) == TRENDS_LINEAR
         ):
