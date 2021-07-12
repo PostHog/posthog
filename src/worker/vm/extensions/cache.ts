210df68d6022e67c7abacb573c8943d2fccd365a
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2021-07-12 14:10:11 -0300

    [plugin-server] Add lpush and lrange (https://github.com/PostHog/plugin-server/pull/498)
    * add lpush and lrange
    
    * add llen, max lpush elements
    
    * update scaffold
    
    * fix wrong import

--- src/worker/vm/extensions/cache.ts
+++ src/worker/vm/extensions/cache.ts
@@ -1,6 +1,7 @@
 import { CacheExtension } from '@posthog/plugin-scaffold'
 
 import { Hub } from '../../../types'
+import { IllegalOperationError } from '../../../utils/utils'
 
 export function createCache(server: Hub, pluginId: number, teamId: number): CacheExtension {
     const getKey = (key: string) => `@plugin/${pluginId}/${typeof teamId === 'undefined' ? '@all' : teamId}/${key}`
@@ -16,6 +17,28 @@ export function createCache(server: Hub, pluginId: number, teamId: number): Cach
         },
         expire: async function (key, ttlSeconds) {
             return await server.db.redisExpire(getKey(key), ttlSeconds)
+        },
+        lpush: async function (key, elementOrArray) {
+            const isString = typeof elementOrArray === 'string'
+            if (!Array.isArray(elementOrArray) && !isString) {
+                throw new Error('cache.lpush expects a string value or an array of strings')
+            }
+            if (!isString) {
+                if (elementOrArray.length > 1000) {
+                    throw new IllegalOperationError('Too many elements in array for cache.lpush. Maximum: 1000')
+                }
+                elementOrArray = elementOrArray.map((el) => String(el))
+            }
+            return await server.db.redisLPush(getKey(key), elementOrArray, { jsonSerialize: false })
+        },
+        lrange: async function (key, startIndex, endIndex) {
+            if (typeof startIndex !== 'number' || typeof endIndex !== 'number') {
+                throw new Error('cache.lrange expects a number for the start and end indexes')
+            }
+            return await server.db.redisLRange(getKey(key), startIndex, endIndex)
+        },
+        llen: async function (key) {
+            return await server.db.redisLLen(getKey(key))
         },
     }
 }
