787957690cd04981ddca6004e1a760a817e87683
yakkomajuri <yakko.majuri@gmail.com>
2021-07-14 19:05:18 -0300

    remove unnecessary fragment

--- frontend/src/scenes/plugins/PluginsSearch.tsx
+++ frontend/src/scenes/plugins/PluginsSearch.tsx
@@ -7,15 +7,13 @@ export function PluginsSearch(): JSX.Element {
     const { searchTerm, rearranging } = useValues(pluginsLogic)
     const { setSearchTerm } = useActions(pluginsLogic)
     return (
-        <>
-            <Input
-                placeholder="Start typing to search for a plugin"
-                onChange={(e) => setSearchTerm(e.target.value)}
-                style={{ maxWidth: 400, height: 40, borderRadius: 5, marginTop: 5, marginBottom: 10 }}
-                disabled={rearranging}
-                value={searchTerm || ''}
-                allowClear
-            />
-        </>
+        <Input
+            placeholder="Start typing to search for a plugin"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            style={{ maxWidth: 400, height: 40, borderRadius: 5, marginTop: 5, marginBottom: 10 }}
+            disabled={rearranging}
+            value={searchTerm || ''}
+            allowClear
+        />
     )
 }
