0721f963521e88d7d586ef816253725eee885cb1
Li Yi Yu <li@posthog.com>
2021-07-14 13:08:37 -0400

    do not request time to convert funnel if there are less than 2 steps (#5129)

--- frontend/src/scenes/funnels/funnelLogic.ts
+++ frontend/src/scenes/funnels/funnelLogic.ts
@@ -150,7 +150,7 @@ export const funnelLogic = kea<funnelLogicType<TimeStepOption>>({
                     insightLogic.actions.endQuery(queryId, ViewType.FUNNELS, result.last_refresh)
                     actions.setSteps(result.result as FunnelStep[])
                     let binsResult: FunnelsTimeConversionResult
-                    if (params.display === ChartDisplayType.FunnelsTimeToConvert) {
+                    if (params.display === ChartDisplayType.FunnelsTimeToConvert && values.stepsWithCount.length > 1) {
                         try {
                             params.funnel_viz_type = 'time_to_convert'
                             params.funnel_to_step = values.histogramStep
