32a2a35c7a369c79e0dff29f6f7aa461bbb053e5
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-07-15 10:08:02 +0300

    Remove dead import

--- ee/clickhouse/sql/trends/lifecycle.py
+++ ee/clickhouse/sql/trends/lifecycle.py
@@ -1,5 +1,3 @@
-from posthog.queries.lifecycle import LIFECYCLE_PEOPLE_SQL
-
 LIFECYCLE_SQL = """
 SELECT groupArray(day_start) as date, groupArray(counts) as data, status FROM (
     SELECT if(status = 'dormant', toInt64(SUM(counts)) * toInt16(-1), toInt64(SUM(counts))) as counts, day_start, status
@@ -19,7 +17,7 @@ SELECT groupArray(day_start) as date, groupArray(counts) as data, status FROM (
                 SELECT *, if(base_day = toDateTime('0000-00-00 00:00:00'), 'dormant', if(subsequent_day = base_day + INTERVAL {interval}, 'returning', if(subsequent_day > earliest + INTERVAL {interval}, 'resurrecting', 'new'))) as status FROM (
                     SELECT person_id, base_day, min(subsequent_day) as subsequent_day FROM (
                         SELECT person_id, day as base_day, events.subsequent_day as subsequent_day  FROM (
-                            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events 
+                            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events
                             JOIN
                             (SELECT person_id,
                                         distinct_id
@@ -29,7 +27,7 @@ SELECT groupArray(day_start) as date, groupArray(counts) as data, status FROM (
                             GROUP BY person_id, day HAVING day <= toDateTime(%(date_to)s) AND day >= toDateTime(%(prev_date_from)s)
                         ) base
                         JOIN (
-                            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) subsequent_day FROM events 
+                            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) subsequent_day FROM events
                             JOIN
                             (SELECT person_id,
                                         distinct_id
@@ -37,13 +35,13 @@ SELECT groupArray(day_start) as date, groupArray(counts) as data, status FROM (
                                 WHERE team_id = %(team_id)s) pdi on events.distinct_id = pdi.distinct_id
                             WHERE team_id = %(team_id)s AND {event_query} {filters}
                             GROUP BY person_id, subsequent_day HAVING subsequent_day <= toDateTime(%(date_to)s) AND subsequent_day >= toDateTime(%(prev_date_from)s)
-                        ) events ON base.person_id = events.person_id 
+                        ) events ON base.person_id = events.person_id
                         WHERE subsequent_day > base_day
                     )
                     GROUP BY person_id, base_day
                     UNION ALL
                     SELECT person_id, min(day) as base_day, min(day) as subsequent_day  FROM (
-                        SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events 
+                        SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events
                         JOIN
                         (SELECT person_id,
                                     distinct_id
@@ -56,7 +54,7 @@ SELECT groupArray(day_start) as date, groupArray(counts) as data, status FROM (
                     UNION ALL
                     SELECT person_id, base_day, subsequent_day FROM (
                         SELECT person_id, total as base_day, day_start as subsequent_day FROM (
-                            SELECT DISTINCT person_id, groupArray({trunc_func}(events.timestamp)) day FROM events 
+                            SELECT DISTINCT person_id, groupArray({trunc_func}(events.timestamp)) day FROM events
                             JOIN
                             (SELECT person_id,
                                         distinct_id
@@ -76,7 +74,7 @@ SELECT groupArray(day_start) as date, groupArray(counts) as data, status FROM (
                         ( (neighbor(person_id, -1) = person_id) AND neighbor(subsequent_day, -1) < subsequent_day - INTERVAL {interval})
                     ) e
                 JOIN (
-                    SELECT DISTINCT person_id, {trunc_func}(min(events.timestamp)) earliest FROM events 
+                    SELECT DISTINCT person_id, {trunc_func}(min(events.timestamp)) earliest FROM events
                     JOIN
                     (SELECT person_id,
                                 distinct_id
@@ -100,7 +98,7 @@ SELECT person_id FROM (
     SELECT *, if(base_day = toDateTime('0000-00-00 00:00:00'), 'dormant', if(subsequent_day = base_day + INTERVAL {interval}, 'returning', if(subsequent_day > earliest + INTERVAL {interval}, 'resurrecting', 'new'))) as status FROM (
         SELECT person_id, base_day, min(subsequent_day) as subsequent_day FROM (
             SELECT person_id, day as base_day, events.subsequent_day as subsequent_day  FROM (
-                SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events 
+                SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events
                 JOIN
                 (SELECT person_id,
                             distinct_id
@@ -110,7 +108,7 @@ SELECT person_id FROM (
                 GROUP BY person_id, day HAVING day <= toDateTime(%(date_to)s) AND day >= toDateTime(%(prev_date_from)s)
             ) base
             JOIN (
-                SELECT DISTINCT person_id, {trunc_func}(events.timestamp) subsequent_day FROM events 
+                SELECT DISTINCT person_id, {trunc_func}(events.timestamp) subsequent_day FROM events
                 JOIN
                 (SELECT person_id,
                             distinct_id
@@ -118,13 +116,13 @@ SELECT person_id FROM (
                     WHERE team_id = %(team_id)s) pdi on events.distinct_id = pdi.distinct_id
                 WHERE team_id = %(team_id)s AND {event_query} {filters}
                 GROUP BY person_id, subsequent_day HAVING subsequent_day <= toDateTime(%(date_to)s) AND subsequent_day >= toDateTime(%(prev_date_from)s)
-            ) events ON base.person_id = events.person_id 
+            ) events ON base.person_id = events.person_id
             WHERE subsequent_day > base_day
         )
         GROUP BY person_id, base_day
         UNION ALL
         SELECT person_id, min(day) as base_day, min(day) as subsequent_day  FROM (
-            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events 
+            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events
             JOIN
             (SELECT person_id,
                         distinct_id
@@ -137,7 +135,7 @@ SELECT person_id FROM (
         UNION ALL
         SELECT person_id, base_day, subsequent_day FROM (
             SELECT person_id, dummy as base_day, day_start as subsequent_day FROM (
-                SELECT DISTINCT person_id, groupArray({trunc_func}(events.timestamp)) day FROM events 
+                SELECT DISTINCT person_id, groupArray({trunc_func}(events.timestamp)) day FROM events
                 JOIN
                 (SELECT person_id,
                             distinct_id
@@ -157,7 +155,7 @@ SELECT person_id FROM (
             ( (neighbor(person_id, -1) = person_id) AND neighbor(subsequent_day, -1) < subsequent_day - INTERVAL {interval})
         ) e
     JOIN (
-        SELECT DISTINCT person_id, {trunc_func}(min(events.timestamp)) earliest FROM events 
+        SELECT DISTINCT person_id, {trunc_func}(min(events.timestamp)) earliest FROM events
         JOIN
         (SELECT person_id,
                     distinct_id
