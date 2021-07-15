b82040d3b1bb0d992114ef1874cb44a8ced59110
yakkomajuri <yakko.majuri@gmail.com>
2021-07-15 08:33:08 -0300

    update capabilities info wording

--- frontend/src/scenes/plugins/edit/CapabilitiesInfo.ts
+++ frontend/src/scenes/plugins/edit/CapabilitiesInfo.ts
@@ -1,6 +1,6 @@
 export const capabilitiesInfo: Record<string, string> = {
-    processEvent: 'Used to read and modify events before ingestion',
-    processEventBatch: 'Used to read and modify and modify events in batches before ingestion',
+    processEvent: 'Provides the ability to read and modify events before ingestion',
+    processEventBatch: 'Provides the ability t read and modify and modify events in batches before ingestion',
     onEvent: 'Provides read-only access to events',
     onSnapshot: 'Provides read-only access to session recording events',
     exportEvents: 'Provides a way to easily export events to any destination',
