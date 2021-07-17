a9988edc0e3cff09583aeae42ca1641021ae340c
Marius Andra <marius.andra@gmail.com>
2021-07-18 01:57:28 +0200

    popups with popper

---
+++ frontend/src/lib/components/Popup/Popup.tsx
@@ -0,0 +1,61 @@
+import './Popup.scss'
+import React, { ReactElement, useState } from 'react'
+import { usePopper } from 'react-popper'
+import { useOutsideClickHandler } from 'lib/hooks/useOutsideClickHandler'
+import { Placement } from '@popperjs/core'
+
+interface PopupProps {
+    visible?: boolean
+    onClickOutside?: () => void
+    children: React.ReactChild
+    overlay: React.ReactNode
+    placement?: Placement
+    fallbackPlacements?: Placement[]
+}
+
+export function Popup({
+    children,
+    overlay,
+    visible,
+    onClickOutside,
+    placement = 'bottom-start',
+    fallbackPlacements = ['bottom-end', 'top-start', 'top-end'],
+}: PopupProps): JSX.Element {
+    const [referenceElement, setReferenceElement] = useState<HTMLDivElement | null>(null)
+    const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null)
+    const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(null)
+    useOutsideClickHandler([popperElement, referenceElement, arrowElement] as HTMLElement[], onClickOutside)
+
+    const { styles, attributes } = usePopper(referenceElement, popperElement, {
+        placement: placement,
+        modifiers: [
+            { name: 'arrow', options: { element: arrowElement } },
+            fallbackPlacements
+                ? {
+                      name: 'flip',
+                      options: {
+                          fallbackPlacements: fallbackPlacements,
+                      },
+                  }
+                : {},
+        ],
+    })
+
+    const clonedChildren = React.Children.toArray(children).map((child) =>
+        React.cloneElement(child as ReactElement, {
+            ref: setReferenceElement,
+        })
+    )
+
+    return (
+        <>
+            {clonedChildren}
+            {visible && (
+                <div className="popper-tooltip" ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+                    {overlay}
+                    <div ref={setArrowElement} style={styles.arrow} />
+                </div>
+            )}
+        </>
+    )
+}
