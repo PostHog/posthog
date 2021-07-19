f945462ca66427f7e8a92ad2b3f6ef5d83ee9421
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2021-07-19 14:41:33 +0000

    Move imports inside patch.

--- ee/conftest.py
+++ ee/conftest.py
@@ -4,8 +4,6 @@ import pytest
 from django.conf import settings
 from infi.clickhouse_orm import Database
 
-from ee.clickhouse.client import sync_execute
-from ee.clickhouse.sql.plugin_log_entries import DROP_PLUGIN_LOG_ENTRIES_TABLE_SQL, PLUGIN_LOG_ENTRIES_TABLE_SQL
 from posthog.settings import (
     CLICKHOUSE_DATABASE,
     CLICKHOUSE_HTTP_URL,
@@ -38,6 +36,8 @@ def django_db_setup(django_db_setup, django_db_keepdb, worker_id):
         database.create_database()
 
     with patch.object(settings, "CLICKHOUSE_DATABASE", CLICKHOUSE_TEST_DB):
+        from ee.clickhouse.client import sync_execute
+
         database.migrate("ee.clickhouse.migrations")
         # Make DELETE / UPDATE synchronous to avoid flaky tests
         sync_execute("SET mutations_sync = 1")
@@ -52,39 +52,44 @@ def django_db_setup(django_db_setup, django_db_keepdb, worker_id):
 
 
 @pytest.fixture
-def db(db):
-    from ee.clickhouse.sql.events import DROP_EVENTS_TABLE_SQL, EVENTS_TABLE_SQL
-    from ee.clickhouse.sql.person import (
-        DROP_PERSON_DISTINCT_ID_TABLE_SQL,
-        DROP_PERSON_STATIC_COHORT_TABLE_SQL,
-        DROP_PERSON_TABLE_SQL,
-        PERSON_STATIC_COHORT_TABLE_SQL,
-        PERSONS_DISTINCT_ID_TABLE_SQL,
-        PERSONS_TABLE_SQL,
-    )
-    from ee.clickhouse.sql.session_recording_events import (
-        DROP_SESSION_RECORDING_EVENTS_TABLE_SQL,
-        SESSION_RECORDING_EVENTS_TABLE_SQL,
-    )
+def db(db, worker_id):
+    CLICKHOUSE_TEST_DB = f"{CLICKHOUSE_DATABASE}_{worker_id}"
 
-    yield
+    with patch.object(settings, "CLICKHOUSE_DATABASE", CLICKHOUSE_TEST_DB):
+        from ee.clickhouse.client import sync_execute
+        from ee.clickhouse.sql.events import DROP_EVENTS_TABLE_SQL, EVENTS_TABLE_SQL
+        from ee.clickhouse.sql.person import (
+            DROP_PERSON_DISTINCT_ID_TABLE_SQL,
+            DROP_PERSON_STATIC_COHORT_TABLE_SQL,
+            DROP_PERSON_TABLE_SQL,
+            PERSON_STATIC_COHORT_TABLE_SQL,
+            PERSONS_DISTINCT_ID_TABLE_SQL,
+            PERSONS_TABLE_SQL,
+        )
+        from ee.clickhouse.sql.plugin_log_entries import DROP_PLUGIN_LOG_ENTRIES_TABLE_SQL, PLUGIN_LOG_ENTRIES_TABLE_SQL
+        from ee.clickhouse.sql.session_recording_events import (
+            DROP_SESSION_RECORDING_EVENTS_TABLE_SQL,
+            SESSION_RECORDING_EVENTS_TABLE_SQL,
+        )
 
-    try:
-        sync_execute(DROP_EVENTS_TABLE_SQL)
-        sync_execute(DROP_PERSON_TABLE_SQL)
-        sync_execute(DROP_PERSON_DISTINCT_ID_TABLE_SQL)
-        sync_execute(DROP_PERSON_STATIC_COHORT_TABLE_SQL)
-        sync_execute(DROP_SESSION_RECORDING_EVENTS_TABLE_SQL)
-        sync_execute(DROP_PLUGIN_LOG_ENTRIES_TABLE_SQL)
+        yield
 
-        sync_execute(EVENTS_TABLE_SQL)
-        sync_execute(SESSION_RECORDING_EVENTS_TABLE_SQL)
-        sync_execute(PERSONS_TABLE_SQL)
-        sync_execute(PERSONS_DISTINCT_ID_TABLE_SQL)
-        sync_execute(PERSON_STATIC_COHORT_TABLE_SQL)
-        sync_execute(PLUGIN_LOG_ENTRIES_TABLE_SQL)
-    except:
-        pass
+        try:
+            sync_execute(DROP_EVENTS_TABLE_SQL)
+            sync_execute(DROP_PERSON_TABLE_SQL)
+            sync_execute(DROP_PERSON_DISTINCT_ID_TABLE_SQL)
+            sync_execute(DROP_PERSON_STATIC_COHORT_TABLE_SQL)
+            sync_execute(DROP_SESSION_RECORDING_EVENTS_TABLE_SQL)
+            sync_execute(DROP_PLUGIN_LOG_ENTRIES_TABLE_SQL)
+
+            sync_execute(EVENTS_TABLE_SQL)
+            sync_execute(SESSION_RECORDING_EVENTS_TABLE_SQL)
+            sync_execute(PERSONS_TABLE_SQL)
+            sync_execute(PERSONS_DISTINCT_ID_TABLE_SQL)
+            sync_execute(PERSON_STATIC_COHORT_TABLE_SQL)
+            sync_execute(PLUGIN_LOG_ENTRIES_TABLE_SQL)
+        except:
+            pass
 
 
 @pytest.fixture
