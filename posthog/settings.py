3fad77dfbcabb4a5078b22fb4fabb0081908766e
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-07-19 15:30:15 +0300

    Fix axes warning
    Previously, `manage.py notes` failed with the following issue:
    
    ```
    ?: (axes.W001) You are using the django-axes cache handler for login attempt tracking. Your cache configuration is however invalid and will not work correctly with django-axes.
    This can leave security holes in your login systems as attempts are not tracked correctly. Reconfigure settings.AXES_CACHE and settings.CACHES per django-axes configuration documentation.
    ```
    
    This fixes it

--- posthog/settings.py
+++ posthog/settings.py
@@ -222,6 +222,7 @@ CAPTURE_INTERNAL_METRICS = get_from_env("CAPTURE_INTERNAL_METRICS", False, type_
 
 # django-axes settings to lockout after too many attempts
 AXES_ENABLED = get_from_env("AXES_ENABLED", not TEST, type_cast=str_to_bool)
+AXES_CACHE = "default"
 AXES_HANDLER = "axes.handlers.cache.AxesCacheHandler"
 AXES_FAILURE_LIMIT = int(os.getenv("AXES_FAILURE_LIMIT", 5))
 AXES_COOLOFF_TIME = timedelta(minutes=15)
