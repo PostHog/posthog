c37dac3ad1ff6799eb0b0aedcf1b6ff4c615e9cc
Sam Winslow <sammywinslow@gmail.com>
2021-07-19 19:56:09 -0400

    adjustments to test_ee_property_definition

--- posthog/api/test/test_ee_property_definition.py
+++ posthog/api/test/test_ee_property_definition.py
@@ -8,7 +8,7 @@ from posthog.tasks.calculate_event_property_usage import calculate_event_propert
 from posthog.test.base import APIBaseTest
 
 
-class TestPropertyDefinitionAPI(APIBaseTest):
+class TestEEPropertyDefinitionAPI(APIBaseTest):
 
     demo_team: Team = None  # type: ignore
 
@@ -19,13 +19,15 @@ class TestPropertyDefinitionAPI(APIBaseTest):
         org = Organization.objects.create(name="Enterprise Org", available_features=["ingestion_taxonomy"])
         cls.demo_team = create_demo_team(org)
         calculate_event_property_usage_for_team(cls.demo_team.pk)
+        cls.user.current_organization = org
         cls.user.current_team = cls.demo_team
         cls.user.save()
 
-    def test_query_property_definitions(self):
+    def test_query_ee_property_definitions(self):
 
         # Fuzzy search works
         response = self.client.get("/api/projects/@current/property_definitions/?search=hase%20")
+        print(response.json())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertEqual(response.json()["count"], 2)
