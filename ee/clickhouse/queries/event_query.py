fc5c6476a07c8e78c5881fc273a0055c8b15e47a
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-07-20 05:47:41 +0300

    Revert "Revert "Add is_deleted column to person_distinct_id"" (#5194)
    * Revert "Revert "Add is_deleted column to person_distinct_id (#5151)" (#5193)"
    
    This reverts commit 401268bdba18663dd8100d26df584d420bbd2692.
    
    * A tweak for docker-compose builds
    
    Co-authored-by: James Greenhill <fuziontech@gmail.com>

--- ee/clickhouse/queries/event_query.py
+++ ee/clickhouse/queries/event_query.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, List, Tuple
 from ee.clickhouse.models.cohort import format_person_query, get_precalculated_query, is_precalculated_query
 from ee.clickhouse.models.property import filter_element, prop_filter_json_extract
 from ee.clickhouse.queries.util import parse_timestamps
+from ee.clickhouse.sql.person import GET_TEAM_PERSON_DISTINCT_IDS
 from posthog.models import Cohort, Filter, Property, Team
 
 
@@ -56,25 +57,7 @@ class ClickhouseEventQuery(metaclass=ABCMeta):
     def _get_disintct_id_query(self) -> str:
         if self._should_join_distinct_ids:
             return f"""
-            INNER JOIN (
-                SELECT
-                    person_id,
-                    distinct_id
-                FROM (
-                    SELECT *
-                    FROM person_distinct_id
-                    JOIN (
-                        SELECT distinct_id,
-                            max(_offset) as _offset
-                        FROM person_distinct_id
-                        WHERE team_id = %(team_id)s
-                        GROUP BY distinct_id
-                    ) as person_max
-                    USING (distinct_id, _offset)
-                    WHERE team_id = %(team_id)s
-                    )
-                WHERE team_id = %(team_id)s
-            ) AS {self.DISTINCT_ID_TABLE_ALIAS}
+            INNER JOIN ({GET_TEAM_PERSON_DISTINCT_IDS}) AS {self.DISTINCT_ID_TABLE_ALIAS}
             ON events.distinct_id = {self.DISTINCT_ID_TABLE_ALIAS}.distinct_id
             """
         else:
@@ -132,7 +115,7 @@ class ClickhouseEventQuery(metaclass=ABCMeta):
                     GROUP BY id
                     HAVING is_deleted = 0
                 )
-            ) {self.PERSON_TABLE_ALIAS} 
+            ) {self.PERSON_TABLE_ALIAS}
             ON {self.PERSON_TABLE_ALIAS}.id = {self.DISTINCT_ID_TABLE_ALIAS}.person_id
             """
         else:
