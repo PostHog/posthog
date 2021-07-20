774831520d037586d6ae0a5c62b3c89867f8f380
Marius Andra <marius.andra@gmail.com>
2021-07-20 16:46:34 +0200

    jest setup and fetch polyfill

--- jest.config.ts
+++ jest.config.ts
@@ -81,7 +81,11 @@ export default {
     // ],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-    // moduleNameMapper: {},
+    moduleNameMapper: {
+        '^~/(.*)$': '<rootDir>/$1',
+        '^lib/(.*)$': '<rootDir>/lib/$1',
+        '^scenes/(.*)$': '<rootDir>/scenes/$1',
+    },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
@@ -126,7 +130,7 @@ export default {
     // runner: "jest-runner",
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
-    // setupFiles: [],
+    setupFiles: ['../../jest.setup.ts'],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
     setupFilesAfterEnv: ['givens/setup'],
