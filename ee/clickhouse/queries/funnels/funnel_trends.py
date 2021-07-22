b8c946cf99c02b99ee3570e6f9c6f74afd4af0e4
Neil Kakkar <neilkakkar@gmail.com>
2021-07-22 12:49:15 +0100

    Fix Funnel Trends Persons with month/week granularity (#5277)
    * resolve #5275
    
    * address comment

--- ee/clickhouse/queries/funnels/funnel_trends.py
+++ ee/clickhouse/queries/funnels/funnel_trends.py
@@ -78,7 +78,7 @@ class ClickhouseFunnelTrends(ClickhouseFunnelBase):
             FROM (
                 {steps_per_person_query}
             )
-            {"WHERE entrance_period_start = %(entrance_period_start)s" if specific_entrance_period_start else ""}
+            {"WHERE toDateTime(entrance_period_start) = %(entrance_period_start)s" if specific_entrance_period_start else ""}
             GROUP BY person_id, entrance_period_start"""
 
     def get_query(self) -> str:
