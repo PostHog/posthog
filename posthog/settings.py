f3722d16d919eac24e72d67f4591fb6c53855f1c
Marius Andra <marius.andra@gmail.com>
2021-07-22 16:22:19 +0200

    fully release "save-cohort-on-modal" instead of persisting it, show button only under clickhouse

--- posthog/settings.py
+++ posthog/settings.py
@@ -69,7 +69,6 @@ PERSISTED_FEATURE_FLAGS = get_list(os.getenv("PERSISTED_FEATURE_FLAGS", "")) or 
     # Add flags here
     "4267-taxonomic-property-filter",
     "4535-funnel-bar-viz",
-    "save-cohort-on-modal",
 ]
 
 SELF_CAPTURE = get_from_env("SELF_CAPTURE", DEBUG, type_cast=str_to_bool)
