3d537bd57b179abfa30c15aae934220395732a83
Kunal <kunal@posthog.com>
2021-07-29 09:23:49 -0700

    Ingestion - add confirmation to skip + help button (#5348)
    * Initial commit: wire up test w/ helper button
    
    * check in the feat flag
    
    * update copy, add data-attrs
    
    * move bool check up
    
    * update mgmt - fix double click issue
    
    * reset changes to settings.py
    
    Co-authored-by: kunal <kunal@kunals-MacBook-Pro.local>
    Co-authored-by: kunal <kunal@Kunals-iMac.hsd1.ca.comcast.net>

--- frontend/src/scenes/ingestion/panels/VerificationPanel.tsx
+++ frontend/src/scenes/ingestion/panels/VerificationPanel.tsx
@@ -1,20 +1,119 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useActions, useValues } from 'kea'
 import { userLogic } from 'scenes/userLogic'
 import { useInterval } from 'lib/hooks/useInterval'
 import { CardContainer } from 'scenes/ingestion/CardContainer'
-import { Button, Row, Spin } from 'antd'
+import { Button, Row, Spin, Space, Popconfirm, Dropdown, Menu, Typography } from 'antd'
 import { ingestionLogic } from 'scenes/ingestion/ingestionLogic'
+import { DownOutlined, SlackSquareOutlined } from '@ant-design/icons'
+import { CreateInviteModalWithButton } from 'scenes/organization/Settings/CreateInviteModal'
+
+const { Text } = Typography
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
+import { FEATURE_FLAGS } from 'lib/constants'
 
 export function VerificationPanel(): JSX.Element {
     const { loadUser } = useActions(userLogic)
     const { user } = useValues(userLogic)
     const { setVerify, completeOnboarding } = useActions(ingestionLogic)
     const { index, totalSteps } = useValues(ingestionLogic)
+    const { featureFlags } = useValues(featureFlagLogic)
+    const [isPopConfirmShowing, setPopConfirmShowing] = useState(false)
+    const [isHelpMenuShowing, setHelpMenuShowing] = useState(false)
 
     useInterval(() => {
-        !user?.team?.ingested_event && loadUser()
-    }, 1500)
+        if (!user?.team?.ingested_event && !isPopConfirmShowing && !isHelpMenuShowing) {
+            loadUser()
+        }
+    }, 2000)
+
+    function HelperButtonRow(): JSX.Element {
+        function HelpButton(): JSX.Element {
+            const menu = (
+                <Menu selectable>
+                    <Menu.Item key="1" data-attr="ingestion-help-item-invite">
+                        <CreateInviteModalWithButton type="link" />
+                    </Menu.Item>
+                    <Menu.Item key="2" data-attr="ingestion-help-item-slack">
+                        <Button
+                            type="link"
+                            onClick={() => {
+                                window.open('https://posthog.com/slack?s=app', '_blank')
+                            }}
+                        >
+                            <SlackSquareOutlined />
+                            Ask in Slack
+                        </Button>
+                    </Menu.Item>
+                </Menu>
+            )
+            return (
+                <Dropdown
+                    overlay={menu}
+                    trigger={['click']}
+                    visible={isHelpMenuShowing}
+                    onVisibleChange={(v) => {
+                        setHelpMenuShowing(v)
+                    }}
+                >
+                    <Button type="primary" data-attr="ingestion-help-button" onClick={() => setHelpMenuShowing(true)}>
+                        Need help? <DownOutlined />
+                    </Button>
+                </Dropdown>
+            )
+        }
+
+        const popoverTitle = (
+            <Space direction="vertical">
+                <Text strong>Are you sure you want to continue without data?</Text>
+                <Text>You won't be able to conduct analysis or use most tools without event data.</Text>
+                <Text>For the best experience, we recommend adding event data before continuing.</Text>
+            </Space>
+        )
+        return (
+            <Space style={{ float: 'right' }}>
+                <Popconfirm
+                    title={popoverTitle}
+                    okText="Yes, I know what I'm doing."
+                    okType="danger"
+                    cancelText="No, go back."
+                    onVisibleChange={(v) => {
+                        setPopConfirmShowing(v)
+                    }}
+                    onCancel={() => {
+                        setPopConfirmShowing(false)
+                    }}
+                    visible={isPopConfirmShowing}
+                    onConfirm={completeOnboarding}
+                    cancelButtonProps={{ type: 'primary' }}
+                >
+                    <Button
+                        type="dashed"
+                        data-attr="ingestion-continue-anyway"
+                        onClick={() => {
+                            setPopConfirmShowing(true)
+                        }}
+                    >
+                        Continue without verifying
+                    </Button>
+                </Popconfirm>
+                <HelpButton />
+            </Space>
+        )
+    }
+
+    function DefaultSkipCta(): JSX.Element {
+        return (
+            <b
+                data-attr="wizard-complete-button"
+                style={{ float: 'right' }}
+                className="button-border clickable"
+                onClick={completeOnboarding}
+            >
+                Continue without verifying
+            </b>
+        )
+    }
 
     return (
         <CardContainer index={index} totalSteps={totalSteps} onBack={() => setVerify(false)}>
@@ -27,16 +126,9 @@ export function VerificationPanel(): JSX.Element {
                     <p className="prompt-text">
                         {' '}
                         Once you have integrated the snippet and sent an event, we will verify it sent properly and
-                        continue
+                        continue.
                     </p>
-                    <b
-                        data-attr="wizard-complete-button"
-                        style={{ float: 'right' }}
-                        className="button-border clickable"
-                        onClick={completeOnboarding}
-                    >
-                        Continue without verifying
-                    </b>
+                    {featureFlags[FEATURE_FLAGS.INGESTION_HELP_BUTTON] ? <HelperButtonRow /> : <DefaultSkipCta />}
                 </>
             ) : (
                 <>
