2940dfee96c1435615f31ada5addf02fcf1b1ea3
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-08-03 13:55:02 +0300

    Fix "ambigious column distinct_id" (#5423)
    * Fix "ambigious column distinct_id"
    
    The problem came in a trends query:
    1. Which filters by action having person property filters
    2. The overall query having a person property filter
    
    See also the regression test
    
    Closes https://github.com/PostHog/posthog/issues/5388
    
    * Add calculate_events call

--- ee/clickhouse/queries/trends/util.py
+++ ee/clickhouse/queries/trends/util.py
@@ -92,11 +92,11 @@ def get_active_user_params(filter: Filter, entity: Entity, team_id: int) -> Dict
     return params
 
 
-def populate_entity_params(entity: Entity) -> Tuple[Dict, Dict]:
+def populate_entity_params(entity: Entity, table_name: str = "") -> Tuple[Dict, Dict]:
     params, content_sql_params = {}, {}
     if entity.type == TREND_FILTER_TYPE_ACTIONS:
         action = entity.get_action()
-        action_query, action_params = format_action_filter(action)
+        action_query, action_params = format_action_filter(action, table_name=table_name)
         params = {**action_params}
         content_sql_params = {"entity_query": "AND {action_query}".format(action_query=action_query)}
     else:
