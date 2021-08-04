7b951e6aa4d02e297349387a0174a2f2c1f391b9
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-08-04 10:31:58 +0300

    Use f-strings in trends/formula

--- ee/clickhouse/queries/trends/formula.py
+++ ee/clickhouse/queries/trends/formula.py
@@ -10,23 +10,14 @@ from posthog.models.filters.filter import Filter
 
 
 class ClickhouseTrendsFormula:
-    def _label(self, filter: Filter, item: List, team_id: int) -> str:
-        if filter.breakdown:
-            if filter.breakdown_type == "cohort":
-                if item[2] == 0:
-                    return "all users"
-                return Cohort.objects.get(team=team_id, pk=item[2]).name
-            return item[2]
-        return "Formula ({})".format(filter.formula)
-
     def _run_formula_query(self, filter: Filter, team_id: int):
         letters = [chr(65 + i) for i in range(0, len(filter.entities))]
         queries = []
         params: Dict[str, Any] = {}
         for idx, entity in enumerate(filter.entities):
             sql, entity_params, _ = self._get_sql_for_entity(filter, entity, team_id)  # type: ignore
-            sql = sql.replace("%(", "%({}_".format(idx))
-            entity_params = {"{}_{}".format(idx, key): value for key, value in entity_params.items()}
+            sql = sql.replace("%(", f"%({idx}_")
+            entity_params = {f"{idx}_{key}": value for key, value in entity_params.items()}
             queries.append(sql)
             params = {**params, **entity_params}
 
@@ -49,10 +40,7 @@ class ClickhouseTrendsFormula:
             formula=filter.formula,  # formula is properly escaped in the filter
             # Need to wrap aggregates in arrays so we can still use arrayMap
             selects=", ".join(
-                [
-                    ("[sub_{}.data]" if is_aggregate else "sub_{}.data").format(letters[i])
-                    for i in range(0, len(filter.entities))
-                ]
+                [(f"[sub_{letter}.data]" if is_aggregate else f"sub_{letter}.data") for letter in letters]
             ),
             breakdown_value=breakdown_value if filter.breakdown else "",
             first_query=queries[0],
@@ -87,3 +75,12 @@ class ClickhouseTrendsFormula:
             additional_values["count"] = float(sum(additional_values["data"]))
             response.append(parse_response(item, filter, additional_values))
         return response
+
+    def _label(self, filter: Filter, item: List, team_id: int) -> str:
+        if filter.breakdown:
+            if filter.breakdown_type == "cohort":
+                if item[2] == 0:
+                    return "all users"
+                return Cohort.objects.get(team=team_id, pk=item[2]).name
+            return item[2]
+        return "Formula ({})".format(filter.formula)
