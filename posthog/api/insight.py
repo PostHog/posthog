4366e7fa18f1b8436a10802d2f3c20e815bc96cd
Li Yi Yu <li@posthog.com>
2021-08-05 09:00:43 -0400

    add saved false filter back

--- posthog/api/insight.py
+++ posthog/api/insight.py
@@ -164,6 +164,8 @@ class InsightViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
             if key == "saved":
                 if str_to_bool(request.GET["saved"]):
                     queryset = queryset.filter(Q(saved=True) | Q(dashboard__isnull=False))
+                else:
+                    queryset = queryset.filter(Q(saved=False))
             elif key == "user":
                 queryset = queryset.filter(created_by=request.user)
             elif key == "favorited":
