86583c3651098d5d75f135da877fdbb80b95466f
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-08-05 14:29:18 +0300

    Improve cached_property typing
    Noticed that e.g. `filter.breakdown` was getting inferred to be `Any`
    which is not correct. Added generics to fix it :)

--- posthog/models/filters/mixins/utils.py
+++ posthog/models/filters/mixins/utils.py
@@ -1,9 +1,11 @@
 from functools import lru_cache
+from typing import Any, Callable, TypeVar
 
+T = TypeVar("T")
 
 # can't use cached_property directly from functools because of 3.7 compatibilty
-def cached_property(func):
-    return property(lru_cache(maxsize=1)(func))
+def cached_property(func: Callable[..., T]) -> T:
+    return property(lru_cache(maxsize=1)(func))  # type: ignore
 
 
 def include_dict(f):
