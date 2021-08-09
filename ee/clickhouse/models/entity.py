4f61edd19a763ddcbf769e0a31383d71ebf1de79
Michael Matloka <dev@twixes.com>
2021-08-10 00:30:54 +0200

    Always include prop filters in `get_entity_filtering_params`

--- ee/clickhouse/models/entity.py
+++ ee/clickhouse/models/entity.py
@@ -6,9 +6,7 @@ from posthog.constants import TREND_FILTER_TYPE_ACTIONS
 from posthog.models.entity import Entity
 
 
-def get_entity_filtering_params(
-    entity: Entity, team_id: int, table_name: str = "", *, with_prop_filters: bool = False
-) -> Tuple[Dict, Dict]:
+def get_entity_filtering_params(entity: Entity, team_id: int, table_name: str = "") -> Tuple[Dict, Dict]:
     params: Dict[str, Any]
     content_sql_params: Dict[str, str]
     if entity.type == TREND_FILTER_TYPE_ACTIONS:
@@ -16,12 +14,7 @@ def get_entity_filtering_params(
         action_query, params = format_action_filter(action, table_name=table_name)
         content_sql_params = {"entity_query": "AND {action_query}".format(action_query=action_query)}
     else:
-        prop_filters = ""
-        if with_prop_filters:
-            prop_filters, params = parse_prop_clauses(entity.properties, team_id)
-        else:
-            prop_filters, params = "", {}
+        prop_filters, params = parse_prop_clauses(entity.properties, team_id)
         params["event"] = entity.id
         content_sql_params = {"entity_query": f"AND event = %(event)s {prop_filters}"}
-
     return params, content_sql_params
