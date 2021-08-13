dd9b4982ade44d9787c019f1cb82bc7a34b5073d
Alex Kim <alexgkim205@gmail.com>
2021-08-13 14:27:21 -0700

    cautionary comment

--- frontend/src/lib/components/Tooltip.tsx
+++ frontend/src/lib/components/Tooltip.tsx
@@ -4,6 +4,7 @@ import { TooltipProps } from 'antd/lib/tooltip'
 
 const DEFAULT_DELAY = 0.5 //s
 
+// CAUTION: any changes here will affect tooltips across the entire app.
 export function Tooltip({ children, ...props }: TooltipProps): JSX.Element {
     return (
         <AntdTooltip mouseEnterDelay={DEFAULT_DELAY} {...props}>
