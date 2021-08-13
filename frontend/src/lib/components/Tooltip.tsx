792d9a7bb2a6beaa06606f54fbfba1fb8099866d
Alex Kim <alexgkim205@gmail.com>
2021-08-13 14:23:47 -0700

    change delay to 500ms

--- frontend/src/lib/components/Tooltip.tsx
+++ frontend/src/lib/components/Tooltip.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Tooltip as AntdTooltip } from 'antd'
 import { TooltipProps } from 'antd/lib/tooltip'
 
-const DEFAULT_DELAY = 0.2 //s
+const DEFAULT_DELAY = 0.5 //s
 
 export function Tooltip({ children, ...props }: TooltipProps): JSX.Element {
     return (
