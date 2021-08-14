3ad9ad21582de1272efdcfa787cc35a5a92309d4
Li Yi Yu <li@posthog.com>
2021-08-13 21:30:38 -0400

    Fix retention filters (#5590)
    * fix filter params
    
    * add comment
    
    * clean up with selector

--- frontend/src/scenes/insights/InsightTabs/RetentionTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/RetentionTab.tsx
@@ -24,7 +24,9 @@ import { GlobalFiltersTitle } from '../common'
 import { ActionFilter } from '../ActionFilter/ActionFilter'
 
 export function RetentionTab({ annotationsToCreate }: BaseTabProps): JSX.Element {
-    const { filters, filtersLoading } = useValues(retentionTableLogic({ dashboardItemId: null }))
+    const { filters, filtersLoading, actionFilterTargetEntity, actionFilterReturningEntity } = useValues(
+        retentionTableLogic({ dashboardItemId: null })
+    )
     const { setFilters } = useActions(retentionTableLogic({ dashboardItemId: null }))
 
     const screens = useBreakpoint()
@@ -58,7 +60,7 @@ export function RetentionTab({ annotationsToCreate }: BaseTabProps): JSX.Element
                                 hideMathSelector
                                 hideFilter
                                 buttonCopy="Add graph series"
-                                filters={filters}
+                                filters={actionFilterTargetEntity} // retention filters use target and returning entity instead of events
                                 setFilters={(newFilters: FilterType) => {
                                     if (newFilters.events && newFilters.events.length > 0) {
                                         setFilters({ target_entity: newFilters.events[0] })
@@ -117,14 +119,14 @@ export function RetentionTab({ annotationsToCreate }: BaseTabProps): JSX.Element
                                 hideMathSelector
                                 hideFilter
                                 buttonCopy="Add graph series"
-                                filters={filters}
+                                filters={actionFilterReturningEntity}
                                 setFilters={(newFilters: FilterType) => {
                                     if (newFilters.events && newFilters.events.length > 0) {
-                                        setFilters({ target_entity: newFilters.events[0] })
+                                        setFilters({ returning_entity: newFilters.events[0] })
                                     } else if (newFilters.actions && newFilters.actions.length > 0) {
-                                        setFilters({ target_entity: newFilters.actions[0] })
+                                        setFilters({ returning_entity: newFilters.actions[0] })
                                     } else {
-                                        setFilters({ target_entity: null })
+                                        setFilters({ returning_entity: null })
                                     }
                                 }}
                                 typeKey="retention-table-returning"
