4a9b254c66a9f5ed58298bf5adf73546c5d613a3
Alex Kim <alexgkim205@gmail.com>
2021-08-13 17:03:01 -0700

    debouncing visible

--- frontend/src/lib/components/Tooltip.tsx
+++ frontend/src/lib/components/Tooltip.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tooltip as AntdTooltip } from 'antd'
 import { TooltipProps } from 'antd/lib/tooltip'
+import { useDebounce } from 'use-debounce'
 
 const DEFAULT_DELAY = 0.5 //s
 
 // CAUTION: any changes here will affect tooltips across the entire app.
-export function Tooltip({ children, ...props }: TooltipProps): JSX.Element {
+export function Tooltip({ children, visible, ...props }: TooltipProps): JSX.Element {
+    const [localVisible, setVisible] = useState(visible)
+    const [debouncedLocalVisible] = useDebounce(visible ?? localVisible, DEFAULT_DELAY)
+
+    console.log('VISIBLE', debouncedLocalVisible, localVisible, visible)
+
     return (
-        <AntdTooltip mouseEnterDelay={DEFAULT_DELAY} {...props}>
-            {children}
+        <AntdTooltip {...props} mouseEnterDelay={DEFAULT_DELAY} visible={debouncedLocalVisible}>
+            <span
+                onMouseEnter={() => {
+                    setVisible(true)
+                }}
+                onMouseLeave={() => {
+                    setVisible(false)
+                }}
+            >
+                {children}
+            </span>
         </AntdTooltip>
     )
 }
