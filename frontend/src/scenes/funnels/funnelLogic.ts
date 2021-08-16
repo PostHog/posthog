45dd85b3b9f8599d82e54f3bf6a4c931305d1871
Alex Kim <alexgkim205@gmail.com>
2021-08-16 12:44:54 -0700

    remove console

--- frontend/src/scenes/funnels/funnelLogic.ts
+++ frontend/src/scenes/funnels/funnelLogic.ts
@@ -187,15 +187,6 @@ export const funnelLogic = kea<funnelLogicType>({
                     }
 
                     // Don't load results if layout was the only thing changed
-                    console.log(
-                        'refresh',
-                        refresh,
-                        !refresh &&
-                            equal(
-                                Object.assign({}, values.filters, { layout: undefined }),
-                                Object.assign({}, values.lastAppliedFilters, { layout: undefined })
-                            )
-                    )
                     if (
                         !refresh &&
                         equal(
