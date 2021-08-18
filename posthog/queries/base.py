07197be13bfba9f05b78beb7898ba3e9be980f35
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-08-18 15:21:21 +0300

    Remove unused vars

--- posthog/queries/base.py
+++ posthog/queries/base.py
@@ -41,7 +41,6 @@ def determine_compared_filter(filter) -> Filter:
 
 def convert_to_comparison(trend_entity: List[Dict[str, Any]], filter, label: str) -> List[Dict[str, Any]]:
     for entity in trend_entity:
-        days = [i for i in range(len(entity["days"]))]
         labels = [
             "{} {}".format(filter.interval if filter.interval is not None else "day", i)
             for i in range(len(entity["labels"]))
