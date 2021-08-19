b484d34c902192c292d200006e8ea1967810d3cd
Neil Kakkar <neilkakkar@gmail.com>
2021-08-19 15:04:06 +0100

    Remove unused breakdown query variables (#5655)

--- ee/clickhouse/sql/trends/breakdown.py
+++ ee/clickhouse/sql/trends/breakdown.py
@@ -80,23 +80,9 @@ ON person_id = ep.id WHERE e.team_id = %(team_id)s {event_filter} {filters} {par
 AND breakdown_value in (%(values)s) {actions_query}
 """
 
-NONE_BREAKDOWN_PERSON_PROP_JOIN_SQL = """
-INNER JOIN (
-    SELECT * FROM ({latest_person_sql}) ep WHERE team_id = %(team_id)s AND NOT JSONHas(properties, %(key)s)
-) ep
-ON person_id = ep.id WHERE e.team_id = %(team_id)s {event_filter} {filters} {parsed_date_from} {parsed_date_to}
-{actions_query}
-"""
-
 BREAKDOWN_PROP_JOIN_SQL = """
 WHERE e.team_id = %(team_id)s {event_filter} {filters} {parsed_date_from} {parsed_date_to}
   AND {breakdown_value_expr} in (%(values)s)
-  {actions_query}
-"""
-
-NONE_BREAKDOWN_PROP_JOIN_SQL = """
-WHERE e.team_id = %(team_id)s {event_filter} {filters} {parsed_date_from} {parsed_date_to}
-  AND NOT JSONHas(properties, %(key)s)
   {actions_query}
 """
 
