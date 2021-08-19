19862b0b11f03a3963beaa88f85eba13eceff1f7
Neil Kakkar <neilkakkar@gmail.com>
2021-08-19 13:25:42 +0100

    remove unused breakdown query variables

--- ee/clickhouse/sql/trends/breakdown.py
+++ ee/clickhouse/sql/trends/breakdown.py
@@ -80,25 +80,12 @@ ON person_id = ep.id WHERE e.team_id = %(team_id)s {event_filter} {filters} {par
 AND breakdown_value in (%(values)s) {actions_query}
 """
 
-NONE_BREAKDOWN_PERSON_PROP_JOIN_SQL = """
-INNER JOIN (
-    SELECT * FROM ({latest_person_sql}) ep WHERE team_id = %(team_id)s AND NOT JSONHas(properties, %(key)s)
-) ep
-ON person_id = ep.id WHERE e.team_id = %(team_id)s {event_filter} {filters} {parsed_date_from} {parsed_date_to}
-{actions_query}
-"""
-
 BREAKDOWN_PROP_JOIN_SQL = """
 WHERE e.team_id = %(team_id)s {event_filter} {filters} {parsed_date_from} {parsed_date_to}
   AND trim(BOTH '\"' FROM JSONExtractRaw(properties, %(key)s)) in (%(values)s) 
   {actions_query}
 """
 
-NONE_BREAKDOWN_PROP_JOIN_SQL = """
-WHERE e.team_id = %(team_id)s {event_filter} {filters} {parsed_date_from} {parsed_date_to}
-  AND NOT JSONHas(properties, %(key)s) 
-  {actions_query}
-"""
 
 BREAKDOWN_COHORT_JOIN_SQL = """
 INNER JOIN (
