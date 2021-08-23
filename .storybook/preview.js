a0af2bde06be7315fd0e8e44f6b766fc9e8543c0
Marius Andra <marius.andra@gmail.com>
2021-08-24 01:30:21 +0200

    fix storybook babel config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+import React from 'react'
+import { Provider } from 'kea'
+import { initKea } from '../frontend/src/initKea'
+import '~/styles'
+
+initKea()
+
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
@@ -7,3 +14,14 @@ export const parameters = {
         },
     },
 }
+
+const withKea = (Story, context) => {
+    // const theme = getTheme(context.globals.theme)
+    return (
+        <Provider>
+            <Story {...context} />
+        </Provider>
+    )
+}
+
+export const decorators = [withKea]
