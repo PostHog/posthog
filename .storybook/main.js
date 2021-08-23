da0179e73b13f365f14588a662f1f938ef3b2216
Marius Andra <marius.andra@gmail.com>
2021-08-23 15:56:52 +0200

    initial storybook setup

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+    stories: ['../frontend/stories/**/*.stories.mdx', '../frontend/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+}
--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
     plugins: [
         '@babel/plugin-transform-runtime',
         '@babel/plugin-transform-react-jsx',
-        '@babel/plugin-proposal-class-properties',
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
         ['babel-plugin-kea', { path: './frontend/src' }],
     ],
     presets: [
