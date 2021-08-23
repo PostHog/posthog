59397c18c1588a9526033e39c212ae9da5cd4de8
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-08-23 11:16:10 +0300

    Cleanup path_event_query

--- ee/clickhouse/queries/paths/path_event_query.py
+++ ee/clickhouse/queries/paths/path_event_query.py
@@ -16,7 +16,7 @@ class PathEventQuery(ClickhouseEventQuery):
         self.params.update(date_params)
 
         prop_filters = self._filter.properties
-        prop_query, prop_params = self._get_props(prop_filters, allow_denormalized_props=True)
+        prop_query, prop_params = self._get_props(prop_filters)
         self.params.update(prop_params)
 
         query = f"""
@@ -43,13 +43,9 @@ class PathEventQuery(ClickhouseEventQuery):
         self._should_join_distinct_ids = True
 
     def _get_current_url_parsing(self):
-        path_type, _ = get_property_string_expr(
-            "events", "$current_url", "'$current_url'", "properties", allow_denormalized_props=True
-        )
+        path_type, _ = get_property_string_expr("events", "$current_url", "'$current_url'", "properties")
         return path_type
 
     def _get_screen_name_parsing(self):
-        path_type, _ = get_property_string_expr(
-            "events", "$screen_name", "'$screen_name'", "properties", allow_denormalized_props=True
-        )
+        path_type, _ = get_property_string_expr("events", "$screen_name", "'$screen_name'", "properties")
         return path_type
