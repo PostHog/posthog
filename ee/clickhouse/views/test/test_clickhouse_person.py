c1e1eb7459e090ed6644b35452d98e2141748f85
eric <eeoneric@gmail.com>
2021-08-24 15:15:24 -0400

    remove duplicate test

--- ee/clickhouse/views/test/test_clickhouse_person.py
+++ ee/clickhouse/views/test/test_clickhouse_person.py
@@ -25,21 +25,4 @@ def _create_person(**kwargs):
 class ClickhouseTestPersonApi(
     ClickhouseTestMixin, factory_test_person(_create_event, _create_person, _get_events)  # type: ignore
 ):
-    def test_delete_person(self):
-        person = _create_person(
-            team=self.team, distinct_ids=["person_1", "anonymous_id"], properties={"$os": "Chrome"},
-        )
-        _create_event(event="test", team=self.team, distinct_id="person_1")
-        _create_event(event="test", team=self.team, distinct_id="anonymous_id")
-        _create_event(event="test", team=self.team, distinct_id="someone_else")
-
-        response = self.client.delete(f"/api/person/{person.pk}/")
-        print(sync_execute("show create events"))
-        print(sync_execute(f"SELECT * FROM events where team_id = {self.team.pk}"))
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(response.content, b"")  # Empty response
-        self.assertEqual(len(Person.objects.filter(team=self.team)), 0)
-        self.assertEqual(len(_get_events(team_id=self.team.pk)), 1)
-
-        response = self.client.delete(f"/api/person/{person.pk}/")
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    pass
