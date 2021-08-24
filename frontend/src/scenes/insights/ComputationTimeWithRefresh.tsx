faa036210bdb5b3ac9fa04b2dd549dfe8adfadb8
Michael Matloka <dev@twixes.com>
2021-08-24 11:51:34 +0200

    Fix Refresh button Tooltip

--- frontend/src/scenes/insights/ComputationTimeWithRefresh.tsx
+++ frontend/src/scenes/insights/ComputationTimeWithRefresh.tsx
@@ -2,7 +2,7 @@ import { Button } from 'antd'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import React, { useEffect, useState } from 'react'
-import { Tooltip } from 'lib/components/Tooltip'
+import { Tooltip } from 'antd'
 
 dayjs.extend(relativeTime)
 
@@ -26,7 +26,7 @@ export function ComputationTimeWithRefresh({ lastRefresh, loadResults }: Computa
     return (
         <div className="text-muted-alt" style={{ marginLeft: 'auto' }}>
             Computed {lastRefresh ? dayjs(lastRefresh).fromNow() : 'a while ago'}
-            {' • '}
+            <span style={{ padding: '0 4px' }}>•</span>
             <Tooltip
                 title={
                     <>
