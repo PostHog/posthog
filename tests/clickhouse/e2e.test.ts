44a20b7a3e1ca869de1b5d12108d1745edee7ecc
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2021-08-25 13:47:40 +0000

    [plugin-server] Add delay to CH logs fetching (https://github.com/PostHog/plugin-server/pull/544)

--- tests/clickhouse/e2e.test.ts
+++ tests/clickhouse/e2e.test.ts
@@ -120,6 +120,8 @@ describe('e2e clickhouse ingestion', () => {
         await delayUntilEventIngested(() => hub.db.fetchEvents())
         await delayUntilEventIngested(() => hub.db.fetchPluginLogEntries())
 
+        await delay(2000)
+
         const pluginLogEntries = await getLogsSinceStart()
         expect(
             pluginLogEntries.filter(({ message, type }) => message.includes('amogus') && type === 'INFO').length
