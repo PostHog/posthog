a8a33251a966357388437c80ba90bfc8037644c2
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-08-26 21:00:49 +0300

    Use materialized person properties in trends & funnels (#5705)
    * ColumnOptimizer: Add functionality for person materialized columns
    
    * WIP: Use person properties in trends/funnels
    
    * Test person materialized props in trends cohort breakdown query
    
    * Make use of materialized person properties in breakdowns
    
    * Mark some cases working with materialized columns
    
    * Test and fix breakdown by person props without filtering
    
    * Make filtering by entity/person props on a join work better
    
    By not assuming everything is under `properties`
    
    * Add test case around breakdown with person properties
    
    * Add test cases around materialization
    
    * Add another materialization test
    
    * Test cohorts and fix breakdowns with person filter
    
    This exposes a limitation in the current implementation
    
    * Fix some cohort tests
    
    * Fix event query tests
    
    * Get a funnel materialized column test running
    
    * Cover more funnel breakdown tests with materialized
    
    * Handle person property breakdowns in funnels
    
    * Fixup funnel typo
    
    * Add tests, fix an indentation issue
    
    * TestFunnelPersons with materialized columns
    
    * Test funnels against actions with person filters
    
    * Add failing test for entity filtering failing
    
    * Add test case for filtering with entity properties
    
    * Show my 'broken' test is actually doing a subquery
    
    * Resolve linting issues

--- ee/clickhouse/queries/funnels/funnel_event_query.py
+++ ee/clickhouse/queries/funnels/funnel_event_query.py
@@ -24,15 +24,21 @@ class FunnelEventQuery(ClickhouseEventQuery):
                 else ""
             ),
             f"{self.DISTINCT_ID_TABLE_ALIAS}.person_id as person_id" if self._should_join_distinct_ids else "",
-            f"{self.PERSON_TABLE_ALIAS}.person_props as person_props" if self._should_join_persons else "",
+            f"{self.PERSON_TABLE_ALIAS}.person_props as person_props"
+            if self._should_join_persons and self._column_optimizer.should_query_person_properties_column
+            else "",
         ]
 
         _fields.extend(
-            [
-                f"{self.EVENT_TABLE_ALIAS}.{column_name} as {column_name}"
-                for column_name in column_optimizer.materialized_event_columns_to_query
-            ]
+            f"{self.EVENT_TABLE_ALIAS}.{column_name} as {column_name}"
+            for column_name in column_optimizer.materialized_event_columns_to_query
         )
+
+        if self._should_join_persons:
+            _fields.extend(
+                f"{self.PERSON_TABLE_ALIAS}.{column_name} as {column_name}"
+                for column_name in self._column_optimizer.materialized_person_columns_to_query
+            )
 
         _fields = list(filter(None, _fields))
 
@@ -52,7 +58,7 @@ class FunnelEventQuery(ClickhouseEventQuery):
         self.params.update(entity_params)
 
         query = f"""
-            SELECT {','.join(_fields)} FROM events {self.EVENT_TABLE_ALIAS}
+            SELECT {', '.join(_fields)} FROM events {self.EVENT_TABLE_ALIAS}
             {self._get_disintct_id_query()}
             {self._get_person_query()}
             WHERE team_id = %(team_id)s
