57feb8389c005d94585ca86d615f6855600838ba
James Greenhill <fuziontech@gmail.com>
2021-09-08 02:47:07 +0000

    add table example

--- ee/clickhouse/migrations/0017_materialized_postgres_database.py
+++ ee/clickhouse/migrations/0017_materialized_postgres_database.py
@@ -6,5 +6,16 @@ operations = [
 											ENGINE = MaterializedPostgreSQL('db:5432', 'posthog', 'posthog', 'posthog')
 											SETTINGS materialized_postgresql_max_block_size = 65536,
 											materialized_postgresql_tables_list = 'posthog_person, posthog_persondistinctid';"""
-    )
+    ),
+    migrations.RunSQL(
+        """CREATE TABLE posthog.pg_person (
+					id UInt64,
+					created_at DateTime64,
+					team_id UInt64,
+					properties String,
+					is_identified bool,
+					is_deleted bool)
+				ENGINE = MaterializedPostgreSQL('db:5432', 'posthog', 'posthog_person', 'posthog', 'posthog')
+				PRIMARY KEY id;"""
+    ),
 ]
