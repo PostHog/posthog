3d1a5eeb6f445bc4e86a50f2b17c8f155ef7043f
Marius Andra <marius.andra@gmail.com>
2021-09-10 23:48:07 +0200

    add arm64 build scripts

--- docker/clickhouse-builder/build.sh
+++ docker/clickhouse-builder/build.sh
@@ -2,13 +2,13 @@
 
 # Practical advice: Use this in development only!
 #
-# This builds an docker image for the M1 Mac, and pretends it's the one we use from Yandex.
+# This builds an docker image for the M1 Mac.
 # The build will take about 90min.
-#
-# The image is published with the version: 21.6.5
-# The Clickhouse version you're actually getting is: 21.9.2.17
+# The Clickhouse version you're getting is: 21.9.2.17
 #
 # Related issue you'll run into: https://github.com/PostHog/posthog/issues/5684
 # Just uncomment "SAMPLE BY uuid" in ee/clickhouse/sql/events.py:38
 
-docker build assets -f assets/arm64.compile.Dockerfile -t yandex/clickhouse-server:21.6.5
+cd "$(dirname "$0")"
+
+docker build assets -f assets/arm64.compile.Dockerfile -t clickhouse-dev-arm64:latest
