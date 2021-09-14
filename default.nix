60237668d87175eebb62b76250cff7577af7d537
Harry Waye <harry@scalexp.com>
2021-09-14 11:56:13 +0100

    chore: update nix config to include python 3.9

--- default.nix
+++ default.nix
@@ -1,22 +1,30 @@
-# Installs postgresql, yarn redis and kafka so I can run locally.
+# Defines a dev env with python 3.9 and node. Also includes docker for running
+# services. Perhaps this is a bit overkill for doing so little, but I've been
+# having some issues with python env not being isolated, specifically running
+# pip-compile was picking up version information from somewhere else.
 
 let
   pkgs = import <nixpkgs> {};
 in
 pkgs.mkShell {
   buildInputs = [
+    pkgs.python39
+    pkgs.python39Packages.pip
+    pkgs.python39Packages.setuptools
+    pkgs.python39Packages.virtualenv
+
     # Required to build psycopg2
     pkgs.openssl
 
     # Required for frontend build
-    pkgs.nodejs-12_x
+    pkgs.nodejs
     pkgs.yarn
     
     # Service dependencies
-    pkgs.postgresql 
-    pkgs.redis 
-    pkgs.apacheKafka];
+    pkgs.docker];
     
   shellHook = ''
+    python -m venv env
+    source env/bin/activate
   '';
 }
