76b3528f6ca62d5c67a4732a5c61c90acc6cf3d5
eric <eeoneric@gmail.com>
2021-09-14 11:40:30 -0400

    fix race condition on update

--- posthog/api/cohort.py
+++ posthog/api/cohort.py
@@ -74,8 +74,10 @@ class CohortSerializer(serializers.ModelSerializer):
         if cohort.is_static:
             self._handle_static(cohort, request)
         else:
-            calculate_cohort.delay(cohort_id=cohort.pk)
-            calculate_cohort_ch.delay(cohort_id=cohort.pk)
+            if is_clickhouse_enabled():
+                calculate_cohort_ch.delay(cohort.id)
+            else:
+                calculate_cohort.delay(cohort.id)
 
         posthoganalytics.capture(request.user.distinct_id, "cohort created", cohort.get_analytics_metadata())
         return cohort
