28cf8e2301cb9032f81fbd9c4f415d881d2c38b6
Rick Marron <rcmarron@gmail.com>
2021-09-14 15:32:47 -0700

    revert black formatting with bad config

--- posthog/api/decide.py
+++ posthog/api/decide.py
@@ -131,9 +131,6 @@ def get_decide(request: HttpRequest):
             if team.session_recording_opt_in and (on_permitted_domain(team, request) or len(team.app_urls) == 0):
                 response["sessionRecording"] = {"endpoint": "/s/"}
     statsd.incr(
-        f"posthog_cloud_raw_endpoint_success",
-        tags={
-            "endpoint": "decide",
-        },
+        f"posthog_cloud_raw_endpoint_success", tags={"endpoint": "decide",},
     )
     return cors_response(request, JsonResponse(response))
