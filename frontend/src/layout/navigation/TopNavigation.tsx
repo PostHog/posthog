c0483d734dd047eb634cd22a0416bef6dc5e0342
Michael Matloka <dev@twixes.com>
2021-09-17 23:54:25 +0200

    Restore `TopNavigation` changes
    This reverts commit 05fd9e4ed665c51ab6d4e45443570694f56b3324.

--- frontend/src/layout/navigation/TopNavigation.tsx
+++ frontend/src/layout/navigation/TopNavigation.tsx
@@ -21,6 +21,7 @@ import {
     CreditCardOutlined,
     KeyOutlined,
     SmileOutlined,
+    StopOutlined,
 } from '@ant-design/icons'
 import { sceneLogic, urls } from 'scenes/sceneLogic'
 import { CreateProjectModal } from 'scenes/project/CreateProjectModal'
@@ -30,7 +31,7 @@ import { commandPaletteLogic } from 'lib/components/CommandPalette/commandPalett
 import { Link } from 'lib/components/Link'
 import { LinkButton } from 'lib/components/LinkButton'
 import { BulkInviteModal } from 'scenes/organization/Settings/BulkInviteModal'
-import { AvailableFeature, UserType } from '~/types'
+import { AvailableFeature, TeamBasicType, UserType } from '~/types'
 import { CreateInviteModalWithButton } from 'scenes/organization/Settings/CreateInviteModal'
 import { preflightLogic } from 'scenes/PreflightCheck/logic'
 import { billingLogic } from 'scenes/billing/billingLogic'
@@ -38,6 +39,7 @@ import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { Environments, FEATURE_FLAGS } from 'lib/constants'
 import { ProfilePicture } from 'lib/components/ProfilePicture'
 import { Tooltip } from 'lib/components/Tooltip'
+import { teamLogic } from '../../scenes/teamLogic'
 
 export function WhoAmI({ user }: { user: UserType }): JSX.Element {
     return (
@@ -48,6 +50,53 @@ export function WhoAmI({ user }: { user: UserType }): JSX.Element {
                 <span>{user.organization?.name}</span>
             </div>
         </div>
+    )
+}
+
+function ProjectRow({ team }: { team: TeamBasicType }): JSX.Element {
+    const { currentTeam } = useValues(teamLogic)
+    const { updateCurrentTeam } = useActions(userLogic)
+    const { push } = router.actions
+
+    const isCurrent = team.id === currentTeam?.id
+    const isRestricted = !!team.effective_membership_level
+
+    return (
+        <button
+            key={team.id}
+            className="plain-button"
+            type="button"
+            onClick={(e) => {
+                if (!isCurrent && !isRestricted) {
+                    updateCurrentTeam(team.id, '/')
+                } else {
+                    e.preventDefault() // Prevent dropdown from hiding if can't switch project
+                }
+            }}
+            disabled={isCurrent || isRestricted}
+            style={{
+                cursor: isCurrent || isRestricted ? 'default' : undefined,
+                color: isRestricted ? 'var(--text-muted)' : undefined,
+            }}
+        >
+            {isRestricted ? <StopOutlined className="mr-05" /> : <ProjectOutlined className="mr-05" />}
+            <span style={{ flexGrow: 1, fontWeight: isCurrent ? 700 : 400 }}>{team.name}</span>
+            {!isRestricted && (
+                <span
+                    className="subaction"
+                    onClick={(e) => {
+                        e.stopPropagation()
+                        if (isCurrent) {
+                            push(urls.projectSettings())
+                        } else {
+                            updateCurrentTeam(team.id, '/project/settings')
+                        }
+                    }}
+                >
+                    <ToolOutlined />
+                </span>
+            )}
+        </button>
     )
 }
 
@@ -70,13 +119,13 @@ export function TopNavigation(): JSX.Element {
         projectModalShown,
         organizationModalShown,
     } = useValues(navigationLogic)
+    const { currentTeam } = useValues(teamLogic)
     const { user } = useValues(userLogic)
     const { preflight } = useValues(preflightLogic)
     const { billing } = useValues(billingLogic)
-    const { logout, updateCurrentTeam, updateCurrentOrganization } = useActions(userLogic)
+    const { logout, updateCurrentOrganization } = useActions(userLogic)
     const { guardAvailableFeature } = useActions(sceneLogic)
     const { sceneConfig } = useValues(sceneLogic)
-    const { push } = router.actions
     const { showPalette } = useActions(commandPaletteLogic)
     const { featureFlags } = useValues(featureFlagLogic)
 
@@ -204,39 +253,13 @@ export function TopNavigation(): JSX.Element {
                 {user?.organization?.teams &&
                     user.organization.teams
                         .sort((teamA, teamB) =>
-                            teamA.id === user?.team?.id ? -2 : teamA.name.localeCompare(teamB.name)
+                            teamA.id === currentTeam?.id
+                                ? -2
+                                : teamA.effective_membership_level
+                                ? 2
+                                : teamA.name.localeCompare(teamB.name)
                         )
-                        .map((team) => {
-                            const isCurrentTeam = team.id === user?.team?.id
-                            return (
-                                <button
-                                    key={team.id}
-                                    className="plain-button"
-                                    type="button"
-                                    onClick={() => updateCurrentTeam(team.id, '/')}
-                                    style={isCurrentTeam ? { cursor: 'default' } : undefined}
-                                    disabled={isCurrentTeam}
-                                >
-                                    <ProjectOutlined className="mr-05" />
-                                    <span style={{ flexGrow: 1, fontWeight: isCurrentTeam ? 700 : 400 }}>
-                                        {team.name}
-                                    </span>
-                                    <span
-                                        className="subaction"
-                                        onClick={(e) => {
-                                            e.stopPropagation()
-                                            if (isCurrentTeam) {
-                                                push(urls.projectSettings())
-                                            } else {
-                                                updateCurrentTeam(team.id, '/project/settings')
-                                            }
-                                        }}
-                                    >
-                                        <ToolOutlined />
-                                    </span>
-                                </button>
-                            )
-                        })}
+                        .map((team) => <ProjectRow key={team.id} team={team} />)}
             </div>
             <button
                 type="button"
@@ -308,7 +331,7 @@ export function TopNavigation(): JSX.Element {
                     >
                         <div>
                             <ProjectOutlined className="mr-05" />
-                            {user?.team?.name}
+                            {currentTeam?.name}
                             <DownOutlined className="ml-05" />
                         </div>
                     </Dropdown>
