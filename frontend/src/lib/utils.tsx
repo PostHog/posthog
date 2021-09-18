f73894f40830642085c6645ac20a104d4ec6f0de
Marius Andra <marius.andra@gmail.com>
2021-09-19 00:19:03 +0200

    make nodejs compatible

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -53,7 +53,13 @@ export const ANTD_TOOLTIP_PLACEMENTS: Record<any, AlignType> = {
 
 export function uuid(): string {
     return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
-        (parseInt(c) ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c) / 4)))).toString(16)
+        (
+            parseInt(c) ^
+            ((typeof window?.crypto !== 'undefined'
+                ? window.crypto.getRandomValues(new Uint8Array(1))[0]
+                : Math.floor(Math.random() * 256)) &
+                (15 >> (parseInt(c) / 4)))
+        ).toString(16)
     )
 }
 
