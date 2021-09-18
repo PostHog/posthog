a80de4c2380d7ef3087b1424fe41232d3bbab704
Marius Andra <marius.andra@gmail.com>
2021-09-19 00:50:20 +0200

    add more mocks

--- frontend/src/lib/api.mock.ts
+++ frontend/src/lib/api.mock.ts
@@ -12,15 +12,24 @@ type APIRoute = {
     hashParams: Record<string, any>
     url: string
     data?: Record<string, any>
+    method: string
 }
 
 export const api = (apiNoMock as any) as APIMockReturnType
 
-export const mockAPIGet = (cb: (url: APIRoute) => any): void => {
+export const mockAPI = (cb: (url: APIRoute) => any): void => {
     beforeEach(async () => {
         api.get.mockImplementation(async (url, data?: Record<string, any>) => {
-            // kea-router is mocked out, must `await import()` to get access to the utility
-            return cb({ ...(await import('kea-router')).combineUrl(url), data })
+            return cb({ ...(await import('kea-router')).combineUrl(url), data, method: 'get' })
+        }),
+            api.update.mockImplementation(async (url, data?: Record<string, any>) => {
+                return cb({ ...(await import('kea-router')).combineUrl(url), data, method: 'update' })
+            })
+        api.create.mockImplementation(async (url, data?: Record<string, any>) => {
+            return cb({ ...(await import('kea-router')).combineUrl(url), data, method: 'create' })
+        })
+        api.delete.mockImplementation(async (url, data?: Record<string, any>) => {
+            return cb({ ...(await import('kea-router')).combineUrl(url), data, method: 'delete' })
         })
     })
 }
