ac966ab08b05f5fd99f6923b4e8067f635b9858a
Marius Andra <marius.andra@gmail.com>
2021-09-20 21:18:09 +0200

    events design proposal in storybook

--- frontend/src/scenes/events/__stories__/Events.stories.tsx
+++ frontend/src/scenes/events/__stories__/Events.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Meta } from '@storybook/react'
 import { keaStory } from 'lib/storybook/kea-story'
 
@@ -10,3 +11,10 @@ export default {
 } as Meta
 
 export const AllEvents = keaStory(ManageEvents, eventsState)
+
+export const DesignProposal = (): JSX.Element => (
+    <img
+        src="https://user-images.githubusercontent.com/254612/130522734-4503a8b7-dead-4e66-a78e-b1cd773239a1.png"
+        style={{ width: '100%' }}
+    />
+)
