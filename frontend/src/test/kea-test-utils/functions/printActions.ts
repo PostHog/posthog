a5490284f01fad7d2a5db5ed30bf9834f19104a1
Marius Andra <marius.andra@gmail.com>
2021-09-21 13:14:06 +0200

    fix type bug

--- frontend/src/test/kea-test-utils/functions/printActions.ts
+++ frontend/src/test/kea-test-utils/functions/printActions.ts
@@ -3,12 +3,11 @@ import { ExpectFunction, testUtilsContext } from '~/test/kea-test-utils'
 export const printActions: ExpectFunction<any> = {
     common(logic, payload) {
         const { recordedActions, pointerMap } = testUtilsContext()
-        const logicPointer = pointerMap.get(logic)
 
         console.log(`💈 Logging actions for logic "${logic.pathString}": ${payload ?? ''}`)
 
         recordedActions.forEach(({ action }, index) => {
-            console.log(`💥 ${index}. ${logicPointer === index ? ' ⬅️ POINTER' : ''}${JSON.stringify(action, null, 2)}`)
+            console.log(`💥 ${index}. ${pointerMap === index ? ' ⬅️ POINTER' : ''}${JSON.stringify(action, null, 2)}`)
         })
     },
 }
