be37c59a1939024d60c6783401a453b6cefb5428
Harry Waye <harry@scalexp.com>
2021-09-21 16:34:44 +0100

    fix(trends): avoid including one week too many when deciding on bounds
    Previously there was a test in here that was saying that if we request a
    graph of events, with week interval ticks starting on a Sunday, we'd
    actually end up also including the Sunday before due to some arithmetic
    mistakes.

--- posthog/queries/test/test_trends.py
+++ posthog/queries/test/test_trends.py
@@ -1349,10 +1349,10 @@ def trend_test_factory(trends, event_factory, person_factory, action_factory, co
                     Filter(data={"date_from": "2019-11-24", "interval": "week", "events": [{"id": "sign up"}]}),
                     self.team,
                 )
-            self.assertEqual(response[0]["labels"][5], "22-Dec-2019")
-            self.assertEqual(response[0]["data"][5], 1.0)
-            self.assertEqual(response[0]["labels"][6], "29-Dec-2019")
-            self.assertEqual(response[0]["data"][6], 4.0)
+            self.assertEqual(
+                response[0]["labels"][:5], ["24-Nov-2019", "1-Dec-2019", "8-Dec-2019", "15-Dec-2019", "22-Dec-2019"]
+            )
+            self.assertEqual(response[0]["data"][:5], [0.0, 0.0, 0.0, 0.0, 1.0])
 
             # test month
             with freeze_time("2020-01-02"):
