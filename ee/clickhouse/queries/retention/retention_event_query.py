b32485b8b16de10a718f5511644a6bedbc0b1675
eric <eeoneric@gmail.com>
2021-09-22 14:11:27 -0400

    update fields pattern

--- ee/clickhouse/queries/retention/retention_event_query.py
+++ ee/clickhouse/queries/retention/retention_event_query.py
@@ -27,20 +27,21 @@ class RetentionEventsQuery(ClickhouseEventQuery):
         self._trunc_func = get_trunc_func_ch(self._filter.period)
 
     def get_query(self) -> Tuple[str, Dict[str, Any]]:
-        _fields = (
-            (self.get_timestamp_field())
-            + (f", {self.DISTINCT_ID_TABLE_ALIAS}.person_id as person_id" if self._should_join_distinct_ids else "")
-            + (
+        _fields = [
+            self.get_timestamp_field(),
+            (f", {self.DISTINCT_ID_TABLE_ALIAS}.person_id as person_id" if self._should_join_distinct_ids else ""),
+            (
                 f", argMin(e.uuid, {self._trunc_func}(e.timestamp)) as min_uuid"
                 if self._event_query_type == RetentionQueryType.TARGET_FIRST_TIME
                 else f", {self.EVENT_TABLE_ALIAS}.uuid AS uuid"
-            )
-            + (
+            ),
+            (
                 f", argMin(e.event, {self._trunc_func}(e.timestamp)) as min_event"
                 if self._event_query_type == RetentionQueryType.TARGET_FIRST_TIME
                 else f", {self.EVENT_TABLE_ALIAS}.event AS event"
-            )
-        )
+            ),
+        ]
+        _fields = list(filter(None, _fields))
 
         date_query, date_params = self._get_date_filter()
         self.params.update(date_params)
@@ -58,7 +59,7 @@ class RetentionEventsQuery(ClickhouseEventQuery):
         self.params.update(entity_params)
 
         query = f"""
-            SELECT {_fields} FROM events {self.EVENT_TABLE_ALIAS}
+            SELECT {','.join(_fields)} FROM events {self.EVENT_TABLE_ALIAS}
             {self._get_disintct_id_query()}
             {self._get_person_query()}
             WHERE team_id = %(team_id)s
