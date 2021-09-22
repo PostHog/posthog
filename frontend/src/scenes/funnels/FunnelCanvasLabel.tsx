59f668198b06bba10923b827f92b5840dbe43987
Alex Kim <alexgkim205@gmail.com>
2021-09-22 10:30:52 -0700

    remove unnecessary check

--- frontend/src/scenes/funnels/FunnelCanvasLabel.tsx
+++ frontend/src/scenes/funnels/FunnelCanvasLabel.tsx
@@ -14,7 +14,12 @@ import { Tooltip } from 'lib/components/Tooltip'
 import { FunnelStepsPicker } from 'scenes/insights/InsightTabs/FunnelTab/FunnelStepsPicker'
 
 export function FunnelCanvasLabel(): JSX.Element | null {
-    const { conversionMetrics, clickhouseFeaturesEnabled } = useValues(funnelLogic)
+    const { conversionMetrics, clickhouseFeaturesEnabled } = useValues(
+        funnelLogic({
+            dashboardItemId: null,
+            filters: null,
+        })
+    )
     const { allFilters } = useValues(insightLogic)
     const { setChartFilter } = useActions(chartFilterLogic)
 
