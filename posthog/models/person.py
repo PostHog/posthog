45f966ec3b11e32746a94da986a9f20e10cbef9f
Tim Glaser <tim@posthog.com>
2021-09-23 14:31:38 +0200

    Rewrite migration 0169 to use null (#6081)

--- posthog/models/person.py
+++ posthog/models/person.py
@@ -67,7 +67,7 @@ class Person(models.Model):
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True, blank=True)
 
     # used to prevent race conditions with set and set_once
-    properties_last_updated_at: models.JSONField = models.JSONField(default=dict)
+    properties_last_updated_at: models.JSONField = models.JSONField(default=dict, null=True, blank=True)
     team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
     properties: models.JSONField = models.JSONField(default=dict)
     is_user: models.ForeignKey = models.ForeignKey("User", on_delete=models.CASCADE, null=True, blank=True)
