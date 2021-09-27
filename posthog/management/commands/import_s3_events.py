6162d87662d51ce9d150127ec0abe1f48c02268a
yakkomajuri <yakko.majuri@gmail.com>
2021-09-27 05:36:34 +0000

    wip untested

--- posthog/management/commands/import_s3_events.py
+++ posthog/management/commands/import_s3_events.py
@@ -1,52 +1,88 @@
+import datetime
 import json
 import os
 
-import boto3
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connection
 from smart_open import smart_open
 
-from ee.kafka_client.helper import get_kafka_consumer
+from posthog.api.capture import log_event
+from posthog.celery import app as celery_app
+from posthog.utils import is_clickhouse_enabled
 
 
 class Command(BaseCommand):
     help = "Consume from a Kafka topic into S3"
 
     def add_arguments(self, parser):
-        parser.add_argument("--bucket", default=None)
+        parser.add_argument("--bucket-path", default=None)
 
     def handle(self, *args, **options):
-        if not options["bucket"]:
-            print("The argument --bucket is required")
+        if not options["bucket-path"]:
+            print("The argument --bucket-path is required")
             exit(1)
 
-        i = 0
-        # stream lines from an S3 object
-        for line in smart_open(
-            "s3://yakko-athena-test/results/Unsaved/2021/09/24/f3bc23f5-de44-492c-83fb-981af768e9ad.csv", "rb"
-        ):
-            if i == 1:
-                print(
-                    json.loads(
-                        line.decode("utf8")
-                        .replace('"""', '"')
-                        .replace('""', '"')
-                        .replace("\\", "")
-                        .replace('"{', "{")
-                        .replace('}"', "}")
-                    )
+        bucket_path = options["bucket-path"]
+
+        files_to_read = os.environ.get("S3_IMPORT_FILES").split(",")
+
+        ordered_files = []
+
+        # read one line from each bucket and sort by timestamp
+        for file_name in files_to_read:
+            line_index = 0
+            for line in smart_open(f"s3://{bucket_path}{file_name}", "rb"):
+                if line_index == 1:
+                    timestamp = datetime.datetime.fromisoformat(line.split(",")[5])
+                    ordered_files.append((timestamp, file_name))
+                    break
+                line_index += 1
+
+        ordered_files.sort()
+
+        # read all files and capture events
+        for _, file_name in ordered_files:
+            is_first_line = True
+            for line in smart_open(f"s3://{bucket_path}{file_name}", "rb"):
+                if is_first_line:
+                    is_first_line = False
+                    continue
+                [
+                    distinct_id,
+                    ip,
+                    site_url,
+                    data,
+                    team_id,
+                    now,
+                    sent_at,
+                    event_uuid,
+                ] = line.split(",")
+
+            if is_clickhouse_enabled():
+                log_event(
+                    distinct_id=distinct_id,
+                    ip=ip,
+                    site_url=site_url,
+                    data=json.loads(data),
+                    team_id=team_id,
+                    now=now,
+                    sent_at=sent_at,
+                    event_uuid=event_uuid,
                 )
-            i += 1
-            if i == 2:
-                break
-
-        # objects_to_read = set(os.getenv('S3_FILES', ',').split(','))
-        # s3 = boto3.resource("s3")
-
-        # bucket = s3.Bucket(options["bucket"])
-        # for obj in bucket.objects.all():
-        #     key = obj.key
-        #     print(key)
-        #     if key in objects_to_read:
-        #         print('woop')
-        # body = obj.get()['Body'].read()
+            else:
+                task_name = "posthog.tasks.process_event.process_event_with_plugins"
+                celery_queue = settings.PLUGINS_CELERY_QUEUE
+                celery_app.send_task(
+                    name=task_name,
+                    queue=celery_queue,
+                    args=[
+                        distinct_id,
+                        ip,
+                        site_url,
+                        json.loads(data),
+                        team_id,
+                        now.isoformat(),
+                        sent_at,
+                    ],
+                )
