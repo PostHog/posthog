6bffed6b847c931c3d6eaa6e59ec52acbbf0bfd6
Tim Glaser <tim@glsr.nl>
2021-09-27 12:18:35 +0100

    fix

--- posthog/management/commands/test_migrations_are_null.py
+++ posthog/management/commands/test_migrations_are_null.py
@@ -12,14 +12,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         def run_and_check_migration(variable):
-            results = re.findall(r"([a-z]+)\/migrations\/([a-zA-Z_0-9]+)\.py", variable)[0]
-            sql = call_command("sqlmigrate", results[0], results[1])
-            if "NOT NULL" in sql and "Create model" not in sql:
-                print(
-                    f"\n\n\033[91mFound a non-null field added to an existing model. This will lock up the table while migrating. Please add 'null=True, blank=True' to the field",
-                    "red",
-                )
-                sys.exit(1)
+            try:
+                results = re.findall(r"([a-z]+)\/migrations\/([a-zA-Z_0-9]+)\.py", variable)[0]
+                sql = call_command("sqlmigrate", results[0], results[1])
+                if "NOT NULL" in sql and "Create model" not in sql:
+                    print(
+                        f"\n\n\033[91mFound a non-null field added to an existing model. This will lock up the table while migrating. Please add 'null=True, blank=True' to the field",
+                        "red",
+                    )
+                    sys.exit(1)
+            except IndexError:
+                pass
 
         for data in sys.stdin:
             run_and_check_migration(data)
