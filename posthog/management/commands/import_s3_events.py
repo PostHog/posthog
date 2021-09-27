a7121e344ffc0fcfbaf674a7c8117fff042c4688
yakkomajuri <yakko.majuri@gmail.com>
2021-09-27 10:50:13 +0000

    save

--- posthog/management/commands/import_s3_events.py
+++ posthog/management/commands/import_s3_events.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import json
 import os
@@ -7,36 +8,41 @@ from django.core.management.base import BaseCommand
 from django.db import connection
 from smart_open import smart_open
 
-from posthog.api.capture import log_event
 from posthog.celery import app as celery_app
 from posthog.utils import is_clickhouse_enabled
+
+# skip snapshots?
 
 
 class Command(BaseCommand):
     help = "Consume from a Kafka topic into S3"
 
     def add_arguments(self, parser):
-        parser.add_argument("--bucket-path", default=None)
+        parser.add_argument("--bucketpath", default=None)
 
     def handle(self, *args, **options):
-        if not options["bucket-path"]:
+        if not options["bucketpath"]:
             print("The argument --bucket-path is required")
             exit(1)
 
-        bucket_path = options["bucket-path"]
+        bucket_path = options["bucketpath"]
 
         files_to_read = os.environ.get("S3_IMPORT_FILES").split(",")
 
         ordered_files = []
 
+        csv.field_size_limit(9999999999999)
         # read one line from each bucket and sort by timestamp
         for file_name in files_to_read:
             line_index = 0
             for line in smart_open(f"s3://{bucket_path}{file_name}", "rb"):
                 if line_index == 1:
-                    timestamp = datetime.datetime.fromisoformat(line.split(",")[5])
-                    ordered_files.append((timestamp, file_name))
-                    break
+                    for row in csv.reader([line.decode("utf-8")]):
+                        print(row)
+                        # print(line.decode('utf-8')[:-1])
+                        # timestamp = datetime.datetime.fromisoformat(line.decode('utf-8')[:-1].split(",")[5])
+                        # ordered_files.append((timestamp, file_name))
+                        break
                 line_index += 1
 
         ordered_files.sort()
@@ -59,30 +65,43 @@ class Command(BaseCommand):
                     event_uuid,
                 ] = line.split(",")
 
-            if is_clickhouse_enabled():
-                log_event(
-                    distinct_id=distinct_id,
-                    ip=ip,
-                    site_url=site_url,
-                    data=json.loads(data),
-                    team_id=team_id,
-                    now=now,
-                    sent_at=sent_at,
-                    event_uuid=event_uuid,
-                )
-            else:
-                task_name = "posthog.tasks.process_event.process_event_with_plugins"
-                celery_queue = settings.PLUGINS_CELERY_QUEUE
-                celery_app.send_task(
-                    name=task_name,
-                    queue=celery_queue,
-                    args=[
-                        distinct_id,
-                        ip,
-                        site_url,
-                        json.loads(data),
-                        team_id,
-                        now.isoformat(),
-                        sent_at,
-                    ],
-                )
+            print(
+                {
+                    "distinct_id": distinct_id,
+                    "ip": ip,
+                    "site_url": site_url,
+                    "data": json.loads(data),
+                    "team_id": team_id,
+                    "now": now,
+                    "sent_at": sent_at,
+                    "event_uuid": event_uuid,
+                }
+            )
+            # if is_clickhouse_enabled():
+            # from posthog.api.capture import log_event
+            #     log_event(
+            #         distinct_id=distinct_id,
+            #         ip=ip,
+            #         site_url=site_url,
+            #         data=json.loads(data),
+            #         team_id=team_id,
+            #         now=now,
+            #         sent_at=sent_at,
+            #         event_uuid=event_uuid,
+            #     )
+            # else:
+            #     task_name = "posthog.tasks.process_event.process_event_with_plugins"
+            #     celery_queue = settings.PLUGINS_CELERY_QUEUE
+            #     celery_app.send_task(
+            #         name=task_name,
+            #         queue=celery_queue,
+            #         args=[
+            #             distinct_id,
+            #             ip,
+            #             site_url,
+            #             json.loads(data),
+            #             team_id,
+            #             now.isoformat(),
+            #             sent_at,
+            #         ],
+            #     )
