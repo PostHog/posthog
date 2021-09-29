b8bc05661f2bc65f2b899803a359135c9d461fab
Neil Kakkar <neilkakkar@gmail.com>
2021-09-29 10:44:47 +0100

    Proposal: Getting rid of Neighbors in Path Queries (#6125)

--- ee/clickhouse/sql/paths/path.py
+++ ee/clickhouse/sql/paths/path.py
@@ -4,13 +4,14 @@ PATH_ARRAY_QUERY = """
             conversion_time,
             event_in_session_index,
             concat(toString(event_in_session_index), '_', path) as path_key,
-            if(event_in_session_index > 1, neighbor(path_key, -1), null) AS last_path_key,
+            if(event_in_session_index > 1, concat(toString(event_in_session_index-1), '_', prev_path), null) AS last_path_key,
             path_dropoff_key
         FROM (
         
             SELECT person_id
                 , joined_path_tuple.1 as path
                 , joined_path_tuple.2 as conversion_time
+                , joined_path_tuple.3 as prev_path
                 , event_in_session_index
                 , session_index
                 , arrayPopFront(arrayPushBack(path_basic, '')) as path_basic_0
@@ -19,7 +20,7 @@ PATH_ARRAY_QUERY = """
                 , arrayFilter((x,y)->y, path_basic, mapping) as compact_path
                 {target_clause}
                 , arrayDifference(limited_timings) as timings_diff
-                , arrayZip(limited_path, timings_diff) as limited_path_timings
+                , arrayZip(limited_path, timings_diff, arrayPopBack(arrayPushFront(limited_path, ''))) as limited_path_timings
                 , concat(toString(length(limited_path)), '_', limited_path[-1]) as path_dropoff_key /* last path item */
             FROM (
                 SELECT person_id
@@ -34,14 +35,11 @@ PATH_ARRAY_QUERY = """
                                 groupArray(path_item) as paths
                         FROM ({path_event_query})
                         GROUP BY person_id
-                        ORDER BY person_id
                         )
                 /* this array join splits paths for a single personID per session */
                 ARRAY JOIN session_paths AS path_time_tuple, arrayEnumerate(session_paths) AS session_index
             )
             ARRAY JOIN limited_path_timings AS joined_path_tuple, arrayEnumerate(limited_path_timings) AS event_in_session_index
             {boundary_event_filter}
-            ORDER BY person_id, session_index, event_in_session_index
             )
-
 """
