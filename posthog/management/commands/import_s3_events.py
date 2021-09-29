ab13cce810ac63b4be9b5b92c2977ff78ae05d47
yakkomajuri <yakko.majuri@gmail.com>
2021-09-29 11:54:59 +0100

    multithreaded processing

--- posthog/management/commands/import_s3_events.py
+++ posthog/management/commands/import_s3_events.py
@@ -2,6 +2,8 @@ import csv
 import json
 import os
 import re
+import threading
+from multiprocessing import Queue
 
 from dateutil import parser
 from django.conf import settings
@@ -11,6 +13,50 @@ from smart_open import smart_open
 
 from posthog.celery import app as celery_app
 from posthog.utils import is_clickhouse_enabled
+
+TARGET_QUEUES = 20
+
+
+def produce_event(q, i):
+    while True:
+        payload = q.get()
+        if payload == "done":
+            print(f"Queue {i} done.", i)
+            q.close()
+            return
+
+        row = payload
+        # print(f'Queue {i} processing ', row[7])
+
+        if is_clickhouse_enabled():
+            from posthog.api.capture import log_event
+
+            log_event(
+                distinct_id=row[0],
+                site_url=row[1],
+                ip=row[2],
+                data=json.loads(row[3]),
+                team_id=int(row[4]),
+                now=parser.parse(row[5]),
+                sent_at=parser.parse(row[6]) if row[6] else None,
+                event_uuid=row[7],
+            )
+        else:
+            task_name = "posthog.tasks.process_event.process_event_with_plugins"
+            celery_queue = settings.PLUGINS_CELERY_QUEUE
+            celery_app.send_task(
+                name=task_name,
+                queue=celery_queue,
+                args=[
+                    row[0],
+                    row[2],
+                    row[1],
+                    json.loads(row[3]),
+                    int(row[4]),
+                    row[5],
+                    row[6],
+                ],
+            )
 
 
 def match_snapshot(haystack):
@@ -32,6 +78,21 @@ class Command(BaseCommand):
             print("The argument --bucket-path is required")
             exit(1)
 
+        queues = []
+        threads = []
+        for i in range(TARGET_QUEUES):
+            q = Queue()
+            thread = threading.Thread(
+                target=produce_event,
+                args=(
+                    q,
+                    i,
+                ),
+            )
+            thread.start()
+            threads.append(thread)
+            queues.append(q)
+
         bucket_path = options["bucketpath"]
 
         files_to_read = os.environ.get("S3_IMPORT_FILES").split(",")
@@ -40,7 +101,7 @@ class Command(BaseCommand):
 
         print(files_to_read)
 
-        csv.field_size_limit(9999999999999)
+        csv.field_size_limit(99999999999)
 
         # read one line from each file and sort by timestamp to process events in order
         for file_name in files_to_read:
@@ -91,46 +152,18 @@ class Command(BaseCommand):
                     continue
 
                 for row in csv.reader([decoded_line]):
+
                     try:
                         if (len(team_ids) and row[4] not in team_ids) or row[4] in team_ids_ignore:
                             continue
-
                         print(row[5])
-                        if is_clickhouse_enabled():
-                            from posthog.api.capture import log_event
+                        part_key = int(row[0].encode("utf-8").hex(), 16) % TARGET_QUEUES
+                        queues[part_key].put(row)
 
-                            log_event(
-                                distinct_id=row[0],
-                                site_url=row[1],
-                                ip=row[2],
-                                data=json.loads(row[3]),
-                                team_id=int(row[4]),
-                                now=parser.parse(row[5]),
-                                sent_at=parser.parse(row[6]) if row[6] else None,
-                                event_uuid=row[7],
-                            )
-                        else:
-                            task_name = "posthog.tasks.process_event.process_event_with_plugins"
-                            celery_queue = settings.PLUGINS_CELERY_QUEUE
-                            celery_app.send_task(
-                                name=task_name,
-                                queue=celery_queue,
-                                args=[
-                                    row[0],
-                                    row[2],
-                                    row[1],
-                                    json.loads(row[3]),
-                                    int(row[4]),
-                                    row[5],
-                                    row[6],
-                                ],
-                            )
                     except:
                         failed_events += 1
                         print("events failed:", failed_events)
 
-                    try:
-                        with open("/tmp/uuids", "a") as f:
-                            f.write(row[7] + "\n")
-                    except:
-                        pass
+        for i in range(TARGET_QUEUES):
+            queues[i].put("done")
+            threads[i].join()
