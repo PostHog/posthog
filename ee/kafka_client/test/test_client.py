f755793a1a46ae6fcaf86e029fbac298f62b5ae4
James Greenhill <fuziontech@gmail.com>
2021-09-30 22:43:04 +0100

    consolidate env vars for github actions

--- ee/kafka_client/test/test_client.py
+++ ee/kafka_client/test/test_client.py
@@ -8,9 +8,6 @@ class KafkaClientTestCase(TestCase):
         self.topic = "test_topic"
         self.payload = {"foo": "bar"}
 
-        self.producer = _KafkaProducer(test=False)
-        self.consumer = build_kafka_consumer(topic=self.topic, auto_offset_reset="earliest", test=False)
-
     def test_kafka_interface(self):
         producer = _KafkaProducer(test=True)
         consumer = build_kafka_consumer(topic=self.topic, test=True)
@@ -21,10 +18,13 @@ class KafkaClientTestCase(TestCase):
         self.assertEqual(msg, "message 1 from test_topic topic")
 
     def test_kafka_produce(self):
-        self.producer.produce(topic=self.topic, data=self.payload)
-        self.producer.close()
+        producer = _KafkaProducer(test=False)
+        producer.produce(topic=self.topic, data=self.payload)
+        producer.close()
 
     def test_kafka_produce_and_consume(self):
-        self.producer.produce(topic=self.topic, data=self.payload)
-        payload = next(self.consumer)
+        producer = _KafkaProducer(test=False)
+        consumer = build_kafka_consumer(topic=self.topic, auto_offset_reset="earliest", test=False)
+        producer.produce(topic=self.topic, data=self.payload)
+        payload = next(consumer)
         self.assertEqual(payload.value, self.payload)
