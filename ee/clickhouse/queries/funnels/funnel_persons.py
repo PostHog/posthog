8d7f40942b192eccae3395b5e331ec2ad15fbf97
eric <eeoneric@gmail.com>
2021-10-01 15:26:15 -0400

    remove funnel limit when querying path funnels

--- ee/clickhouse/queries/funnels/funnel_persons.py
+++ ee/clickhouse/queries/funnels/funnel_persons.py
@@ -1,17 +1,36 @@
-from typing import cast
+from typing import Optional, cast
 
 from ee.clickhouse.queries.funnels.funnel import ClickhouseFunnel
 from ee.clickhouse.sql.funnels.funnel import FUNNEL_PERSONS_BY_STEP_SQL
 from posthog.models import Person
+from posthog.models.filters.filter import Filter
+from posthog.models.team import Team
 
 
 class ClickhouseFunnelPersons(ClickhouseFunnel):
+
+    _no_limit: Optional[bool]  # used when paths are querying for filter people
+
+    def __init__(
+        self,
+        filter: Filter,
+        team: Team,
+        include_timestamp: Optional[bool] = None,
+        include_preceding_timestamp: Optional[bool] = None,
+        no_limit: Optional[bool] = False,
+    ) -> None:
+        self._no_limit = no_limit
+        super().__init__(
+            filter, team, include_timestamp=include_timestamp, include_preceding_timestamp=include_preceding_timestamp
+        )
+
     def get_query(self):
         return FUNNEL_PERSONS_BY_STEP_SQL.format(
             offset=self._filter.offset,
             steps_per_person_query=self.get_step_counts_query(),
             persons_steps=self._get_funnel_person_step_condition(),
             extra_fields=self._get_timestamp_outer_select(),
+            limit="" if self._no_limit else "LIMIT %(limit)s",
         )
 
     def _format_results(self, results):
