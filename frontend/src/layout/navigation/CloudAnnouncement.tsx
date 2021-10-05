ad4a1aca0a2a184d34dcf78334bcd8b71a56e810
Marcus Hyett (PostHog) <85295485+marcushyett-ph@users.noreply.github.com>
2021-10-05 15:34:07 +0100

    Linking to GH issue in Cloud Announcement (#6251)
    * Parse issue link from cloud announcement
    * Add link to button in announcement banner

--- frontend/src/layout/navigation/CloudAnnouncement.tsx
+++ frontend/src/layout/navigation/CloudAnnouncement.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { Alert } from 'antd'
+import { Alert, Space, Button } from 'antd'
 
 export function CloudAnnouncement({ message }: { message: string }): JSX.Element | null {
+    const parsedMessage = message.split('_').join(' ')
+    const githubIssueIds = message.match(/ph-([0-9]{1,8})/i)
     return (
         <div style={{ marginTop: 15 }}>
-            <Alert message={message.split('_').join(' ')} type="warning" showIcon closable />
+            <Alert
+                message={parsedMessage.replace(/ph-[0-9]{1,8}/gi, '')}
+                action={
+                    githubIssueIds ? (
+                        <Space>
+                            <Button
+                                size="small"
+                                type="ghost"
+                                href={'https://github.com/PostHog/posthog/issues/' + githubIssueIds[1]}
+                                target="_blank"
+                            >
+                                More details
+                            </Button>
+                        </Space>
+                    ) : null
+                }
+                type="warning"
+                showIcon
+                closable
+            />
         </div>
     )
 }
