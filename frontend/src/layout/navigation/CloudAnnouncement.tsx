0ba12bb5fdbd287de0eb31d93d02f26f284d6d2a
Marcus Hyett <marcus@posthog.com>
2021-10-05 10:34:05 +0000

    Parse issue link from cloud announcememt

--- frontend/src/layout/navigation/CloudAnnouncement.tsx
+++ frontend/src/layout/navigation/CloudAnnouncement.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Alert } from 'antd'
+import { Alert, Space, Button } from 'antd'
 
 export function CloudAnnouncement({ message }: { message: string }): JSX.Element | null {
+    const parsed_message = message.split('_').join(' ');
+    const github_issue_regex = /ph-([0-9]{1,8})/ig;
+    const github_issue_ids = github_issue_regex.exec(message);
     return (
         <div style={{ marginTop: 15 }}>
-            <Alert message={message.split('_').join(' ')} type="warning" showIcon closable />
+            <Alert 
+                message={parsed_message.replace(github_issue_regex, '')} 
+                    action={
+                        github_issue_ids ? (
+                        <Space>
+                            <Button size="small" type="ghost" href={'https://github.com/PostHog/posthog/issues/' + github_issue_ids[1]} target='_blank'>
+                                More details
+                            </Button>
+                        </Space>
+                        ): null
+                    }
+            type="warning" showIcon closable />
         </div>
     )
 }
