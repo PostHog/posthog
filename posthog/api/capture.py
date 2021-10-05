3ed14bedb10d8c1ef0454f3b4c8abd6399fb83e2
yakkomajuri <yakko.majuri@gmail.com>
2021-10-05 10:30:00 +0000

    add comments

--- posthog/api/capture.py
+++ posthog/api/capture.py
@@ -48,7 +48,10 @@ if is_clickhouse_enabled():
             "sent_at": sent_at.isoformat() if sent_at else "",
         }
 
-    def log_event(data: Dict, topic: str = KAFKA_EVENTS_PLUGIN_INGESTION,) -> None:
+    def log_event(
+        data: Dict,
+        topic: str = KAFKA_EVENTS_PLUGIN_INGESTION,
+    ) -> None:
         if settings.DEBUG:
             print(f'Logging event {data["event"]} to Kafka topic {topic}')
         KafkaProducer().produce(topic=topic, key=data["ip"], data=data)
@@ -147,7 +150,8 @@ def get_event(request):
     except RequestParsingError as error:
         capture_exception(error)  # We still capture this on Sentry to identify actual potential bugs
         return cors_response(
-            request, generate_exception_response("capture", f"Malformed request data: {error}", code="invalid_payload"),
+            request,
+            generate_exception_response("capture", f"Malformed request data: {error}", code="invalid_payload"),
         )
     if not data:
         return cors_response(
@@ -185,6 +189,8 @@ def get_event(request):
         capture_exception(e)
         statsd.incr("capture_endpoint_fetch_team_fail")
 
+        # Postgres deployments don't have a dead letter queue, so
+        # just return an error to the client
         if not is_clickhouse_enabled():
             return cors_response(
                 request,
@@ -199,6 +205,8 @@ def get_event(request):
 
         fetch_team_error = getattr(e, "message", repr(e))
 
+        # We use this approach because each individual event needs to go through some parsing
+        # before being added to the dead letter queue
         send_events_to_dead_letter_queue = True
 
     if team is None and not send_events_to_dead_letter_queue:
@@ -322,7 +330,10 @@ def get_event(request):
 
     timer.stop()
     statsd.incr(
-        f"posthog_cloud_raw_endpoint_success", tags={"endpoint": "capture",},
+        f"posthog_cloud_raw_endpoint_success",
+        tags={
+            "endpoint": "capture",
+        },
     )
     return cors_response(request, JsonResponse({"status": 1}))
 
@@ -346,5 +357,13 @@ def capture_internal(event, distinct_id, ip, site_url, now, sent_at, team_id, ev
         celery_app.send_task(
             name=task_name,
             queue=celery_queue,
-            args=[distinct_id, ip, site_url, event, team_id, now.isoformat(), sent_at,],
+            args=[
+                distinct_id,
+                ip,
+                site_url,
+                event,
+                team_id,
+                now.isoformat(),
+                sent_at,
+            ],
         )
