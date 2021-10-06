051ad6acb643fa8ac81009e1443d71b3f9108e43
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-10-06 16:27:49 +0300

    Simplify event query

--- ee/clickhouse/queries/event_query.py
+++ ee/clickhouse/queries/event_query.py
@@ -44,7 +44,7 @@ class ClickhouseEventQuery(metaclass=ABCMeta):
         self._team_id = team_id
         self._column_optimizer = ColumnOptimizer(self._filter, self._team_id)
         self._person_query = ClickhousePersonQuery(
-            self._filter, self._team_id, self._column_optimizer, self._extra_person_fields
+            self._filter, self._team_id, self._column_optimizer, extra_person_fields
         )
         self.params: Dict[str, Any] = {
             "team_id": self._team_id,
