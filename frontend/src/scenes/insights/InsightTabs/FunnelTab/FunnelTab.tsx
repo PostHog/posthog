c93fa84c3a0db010594a834297c0e47747044490
Alex Kim <alexgkim205@gmail.com>
2021-10-07 08:25:58 -0700

    ff

--- frontend/src/scenes/insights/InsightTabs/FunnelTab/FunnelTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/FunnelTab/FunnelTab.tsx
@@ -21,6 +21,7 @@ import { BreakdownFilter } from 'scenes/insights/BreakdownFilter'
 import { CloseButton } from 'lib/components/CloseButton'
 import { FunnelConversionWindowFilter } from 'scenes/insights/InsightTabs/FunnelTab/FunnelConversionWindowFilter'
 import { FunnelExclusionsFilter } from 'scenes/insights/InsightTabs/FunnelTab/FunnelExclusionsFilter'
+import { SavedFunnels } from 'scenes/insights/SavedCard'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
@@ -201,6 +202,14 @@ export function FunnelTab(): JSX.Element {
                     <hr />
                     <h4 className="secondary">Options</h4>
                     <FunnelConversionWindowFilter />
+                    {!featureFlags[FEATURE_FLAGS.SAVED_INSIGHTS] && (
+                        <>
+                            <hr />
+                            {/* TODO: Remove saved funnels after #3408 is wrapped up. */}
+                            <h4 className="secondary">Saved Funnels</h4>
+                            <SavedFunnels />
+                        </>
+                    )}
                 </Col>
             </Row>
         </>
