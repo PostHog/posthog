90f5b8a576c2ffcc2cc63ec9dd48c80a28002197
eric <eeoneric@gmail.com>
2021-10-07 00:11:29 -0400

    make sure cohort is created after identify so it includes the person association in cohort

--- posthog/api/test/test_trends.py
+++ posthog/api/test/test_trends.py
@@ -41,13 +41,13 @@ def test_includes_only_intervals_within_range(client: Client):
     #  this is what was used demonstrated in
     #  https://github.com/PostHog/posthog/issues/2675 but it might not be the
     #  simplest way to reproduce
-    cohort = create_cohort_ok(client=client, name="test cohort", groups=[{"properties": {"cohort_identifier": 1}}])
 
     # "2021-09-19" is a sunday, i.e. beginning of week
     with freeze_time("2021-09-20T16:00:00"):
         #  First identify as a member of the cohort
         distinct_id = "abc"
         identify(distinct_id=distinct_id, team_id=team.id, properties={"cohort_identifier": 1})
+        cohort = create_cohort_ok(client=client, name="test cohort", groups=[{"properties": {"cohort_identifier": 1}}])
 
         for date in ["2021-09-04", "2021-09-05", "2021-09-12", "2021-09-19"]:
             capture_event(
@@ -67,7 +67,7 @@ def test_includes_only_intervals_within_range(client: Client):
                 date_to="2021-09-21",
                 interval="week",
                 insight="TRENDS",
-                breakdown=[cohort["id"]],
+                breakdown=json.dumps([cohort["id"]]),
                 breakdown_type="cohort",
                 display="ActionsLineGraph",
                 events=[
