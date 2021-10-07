f72e9792b7255964787460dda2ba3aaddbd36f70
yakkomajuri <yakko.majuri@gmail.com>
2021-10-07 11:07:34 +0000

    refactor get_event into smaller modular functions

--- posthog/api/capture.py
+++ posthog/api/capture.py
@@ -142,26 +142,15 @@ def _ensure_web_feature_flags_in_properties(event: Dict[str, Any], team: Team, d
 def get_event(request):
     timer = statsd.timer("posthog_cloud_event_endpoint").start()
     now = timezone.now()
-    try:
-        data = load_data_from_request(request)
-    except RequestParsingError as error:
-        capture_exception(error)  # We still capture this on Sentry to identify actual potential bugs
-        return cors_response(
-            request, generate_exception_response("capture", f"Malformed request data: {error}", code="invalid_payload"),
-        )
-    if not data:
-        return cors_response(
-            request,
-            generate_exception_response(
-                "capture",
-                "No data found. Make sure to use a POST request when sending the payload in the body of the request.",
-                code="no_data",
-            ),
-        )
+
+    data, error_response = extract_data_from_request(request)
+
+    if error_response:
+        return error_response
 
     sent_at = _get_sent_at(data, request)
 
-    token, is_test_environment = get_token(data, request)
+    token, is_test_env = get_token(data, request)
 
     if not token:
         return cors_response(
@@ -175,70 +164,12 @@ def get_event(request):
             ),
         )
 
-    send_events_to_dead_letter_queue = False
-    fetch_team_error = None
-    team = None
+    team, send_events_to_dead_letter_queue, fetch_team_error, error_response = determine_team_from_request_data(
+        request, data, token
+    )
 
-    try:
-        team = Team.objects.get_team_from_token(token)
-    except Exception as e:
-        capture_exception(e)
-        statsd.incr("capture_endpoint_fetch_team_fail")
-
-        # Postgres deployments don't have a dead letter queue, so
-        # just return an error to the client
-        if not is_clickhouse_enabled():
-            return cors_response(
-                request,
-                generate_exception_response(
-                    "capture",
-                    "Unable to fetch team from database.",
-                    type="server_error",
-                    code="fetch_team_fail",
-                    status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                ),
-            )
-
-        fetch_team_error = getattr(e, "message", repr(e))
-
-        # We use this approach because each individual event needs to go through some parsing
-        # before being added to the dead letter queue
-        send_events_to_dead_letter_queue = True
-
-    if team is None and not send_events_to_dead_letter_queue:
-        try:
-            project_id = _get_project_id(data, request)
-        except ValueError:
-            return cors_response(
-                request,
-                generate_exception_response(
-                    "capture", "Invalid Project ID.", code="invalid_project", attr="project_id"
-                ),
-            )
-        if not project_id:
-            return cors_response(
-                request,
-                generate_exception_response(
-                    "capture",
-                    "Project API key invalid. You can find your project API key in PostHog project settings.",
-                    type="authentication_error",
-                    code="invalid_api_key",
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                ),
-            )
-        user = User.objects.get_from_personal_api_key(token)
-        if user is None:
-            return cors_response(
-                request,
-                generate_exception_response(
-                    "capture",
-                    "Invalid Personal API key.",
-                    type="authentication_error",
-                    code="invalid_personal_api_key",
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                ),
-            )
-        team = user.teams.get(id=project_id)
+    if error_response:
+        return error_response
 
     if isinstance(data, dict):
         if data.get("batch"):  # posthog-python and posthog-ruby
@@ -259,76 +190,78 @@ def get_event(request):
             request, generate_exception_response("capture", f"Invalid payload: {e}", code="invalid_payload")
         )
 
+    site_url = request.build_absolute_uri("/")[:-1]
+    ip = None if team.anonymize_ips else get_ip_address(request)
     for event in events:
-        try:
-            distinct_id = _get_distinct_id(event)
-        except KeyError:
-            statsd.incr("invalid_event", tags={"error": "missing_distinct_id"})
-            continue
-        except ValueError:
-            statsd.incr("invalid_event", tags={"error": "invalid_distinct_id"})
-            continue
-        if not event.get("event"):
-            statsd.incr("invalid_event", tags={"error": "missing_event_name"})
-            continue
-
-        event_uuid = UUIDT()
-        site_url = request.build_absolute_uri("/")[:-1]
-
-        if not event.get("properties"):
-            event["properties"] = {}
-
-        # Support test_[apiKey] for users with multiple environments
-        if event["properties"].get("$environment") is None and is_test_environment:
-            event["properties"]["$environment"] = ENVIRONMENT_TEST
-
-        # Dead Letter Queue is a EE-only feature, as it uses Kafka + CH
-        # The first check is redundant but added for typing and explictness
-        if is_clickhouse_enabled() and send_events_to_dead_letter_queue:
-            kafka_event = parse_kafka_event_data(
-                distinct_id=distinct_id,
-                ip=None,
-                site_url=site_url,
-                team_id=None,
-                now=now,
-                event_uuid=event_uuid,
-                data=event,
-                sent_at=sent_at,
-            )
-
-            log_event_to_dead_letter_queue(
-                data,
-                event["event"],
-                kafka_event,
-                f"Unable to fetch team from Postgres. Error: {fetch_team_error}",
-                "django_server_capture_endpoint",
-            )
-            continue
-
-        # this should not happen, but is needed to satisfy typing as team "could be" None
-        if not team:
-            return cors_response(
-                request,
-                generate_exception_response(
-                    "capture",
-                    "No team found for API Key",
-                    type="authentication_error",
-                    code="invalid_personal_api_key",
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                ),
-            )
-
-        ip = None if team.anonymize_ips else get_ip_address(request)
-        _ensure_web_feature_flags_in_properties(event, team, distinct_id)
-
-        statsd.incr("posthog_cloud_plugin_server_ingestion")
-        capture_internal(event, distinct_id, ip, site_url, now, sent_at, team.pk, event_uuid)
+        parse_and_enqueue_event(
+            data,
+            event,
+            site_url,
+            ip,
+            now,
+            sent_at,
+            team,
+            is_test_env,
+            send_events_to_dead_letter_queue,
+            fetch_team_error,
+        )
 
     timer.stop()
     statsd.incr(
         f"posthog_cloud_raw_endpoint_success", tags={"endpoint": "capture",},
     )
     return cors_response(request, JsonResponse({"status": 1}))
+
+
+def parse_and_enqueue_event(
+    data, event, site_url, ip, now, sent_at, team, is_test_env, send_to_dead_letter_queue=False, fetch_team_error=""
+):
+    try:
+        distinct_id = _get_distinct_id(event)
+    except KeyError:
+        statsd.incr("invalid_event", tags={"error": "missing_distinct_id"})
+        return
+    except ValueError:
+        statsd.incr("invalid_event", tags={"error": "invalid_distinct_id"})
+        return
+    if not event.get("event"):
+        statsd.incr("invalid_event", tags={"error": "missing_event_name"})
+        return
+
+    event_uuid = UUIDT()
+
+    if not event.get("properties"):
+        event["properties"] = {}
+
+    # Support test_[apiKey] for users with multiple environments
+    if event["properties"].get("$environment") is None and is_test_env:
+        event["properties"]["$environment"] = ENVIRONMENT_TEST
+
+    if send_to_dead_letter_queue:
+        kafka_event = parse_kafka_event_data(
+            distinct_id=distinct_id,
+            ip=None,
+            site_url=site_url,
+            team_id=None,
+            now=now,
+            event_uuid=event_uuid,
+            data=event,
+            sent_at=sent_at,
+        )
+
+        log_event_to_dead_letter_queue(
+            data,
+            event["event"],
+            kafka_event,
+            f"Unable to fetch team from Postgres. Error: {fetch_team_error}",
+            "django_server_capture_endpoint",
+        )
+        return
+
+    _ensure_web_feature_flags_in_properties(event, team, distinct_id)
+
+    statsd.incr("posthog_cloud_plugin_server_ingestion")
+    capture_internal(event, distinct_id, ip, site_url, now, sent_at, team.pk, event_uuid)
 
 
 def capture_internal(event, distinct_id, ip, site_url, now, sent_at, team_id, event_uuid=UUIDT()):
@@ -352,3 +285,123 @@ def capture_internal(event, distinct_id, ip, site_url, now, sent_at, team_id, ev
             queue=celery_queue,
             args=[distinct_id, ip, site_url, event, team_id, now.isoformat(), sent_at,],
         )
+
+
+def extract_data_from_request(request):
+    data = None
+    try:
+        data = load_data_from_request(request)
+    except RequestParsingError as error:
+        capture_exception(error)  # We still capture this on Sentry to identify actual potential bugs
+        return (
+            data,
+            cors_response(
+                request,
+                generate_exception_response("capture", f"Malformed request data: {error}", code="invalid_payload"),
+            ),
+        )
+
+    if not data:
+        return (
+            data,
+            cors_response(
+                request,
+                generate_exception_response(
+                    "capture",
+                    "No data found. Make sure to use a POST request when sending the payload in the body of the request.",
+                    code="no_data",
+                ),
+            ),
+        )
+
+    return data, None
+
+
+def determine_team_from_request_data(request, data, token):
+    send_events_to_dead_letter_queue = False
+    fetch_team_error = None
+    team = None
+    error_response = None
+
+    try:
+        team = Team.objects.get_team_from_token(token)
+    except Exception as e:
+        capture_exception(e)
+        statsd.incr("capture_endpoint_fetch_team_fail")
+
+        # Postgres deployments don't have a dead letter queue, so
+        # just return an error to the client
+        if not is_clickhouse_enabled():
+            error_response = cors_response(
+                request,
+                generate_exception_response(
+                    "capture",
+                    "Unable to fetch team from database.",
+                    type="server_error",
+                    code="fetch_team_fail",
+                    status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                ),
+            )
+        fetch_team_error = getattr(e, "message", repr(e))
+
+        # We use this approach because each individual event needs to go through some parsing
+        # before being added to the dead letter queue
+        send_events_to_dead_letter_queue = True
+
+        return team, send_events_to_dead_letter_queue, fetch_team_error, error_response
+
+    if team is None:
+        try:
+            project_id = _get_project_id(data, request)
+        except ValueError:
+            error_response = cors_response(
+                request,
+                generate_exception_response(
+                    "capture", "Invalid Project ID.", code="invalid_project", attr="project_id"
+                ),
+            )
+            return team, send_events_to_dead_letter_queue, fetch_team_error, error_response
+
+        if not project_id:
+            error_response = cors_response(
+                request,
+                generate_exception_response(
+                    "capture",
+                    "Project API key invalid. You can find your project API key in PostHog project settings.",
+                    type="authentication_error",
+                    code="invalid_api_key",
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                ),
+            )
+            return team, send_events_to_dead_letter_queue, fetch_team_error, error_response
+
+        user = User.objects.get_from_personal_api_key(token)
+        if user is None:
+            error_response = cors_response(
+                request,
+                generate_exception_response(
+                    "capture",
+                    "Invalid Personal API key.",
+                    type="authentication_error",
+                    code="invalid_personal_api_key",
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                ),
+            )
+            return team, send_events_to_dead_letter_queue, fetch_team_error, error_response
+
+        team = user.teams.get(id=project_id)
+
+        # if we still haven't found a team, return an error to the client
+        if not team:
+            error_response = cors_response(
+                request,
+                generate_exception_response(
+                    "capture",
+                    "No team found for API Key",
+                    type="authentication_error",
+                    code="invalid_personal_api_key",
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                ),
+            )
+
+    return team, send_events_to_dead_letter_queue, fetch_team_error, error_response
