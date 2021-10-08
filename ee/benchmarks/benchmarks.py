52be28cb484bd1b4e850f917f2f91ed0df7f4ecf
Neil Kakkar <neilkakkar@gmail.com>
2021-10-08 08:31:56 +0100

    Update param name in benchmarks

--- ee/benchmarks/benchmarks.py
+++ ee/benchmarks/benchmarks.py
@@ -192,7 +192,7 @@ class QuerySuite:
                 "events": [{"id": "user signed up"}, {"id": "insight analyzed"}],
                 **SHORT_DATE_RANGE,
                 "funnel_correlation_type": FunnelCorrelationType.PROPERTIES,
-                "funnel_correlation_value": "$browser",
+                "funnel_correlation_names": ["$browser"],
             },
             team=self.team,
         )
@@ -205,7 +205,7 @@ class QuerySuite:
                 "events": [{"id": "user signed up"}, {"id": "insight analyzed"}],
                 **SHORT_DATE_RANGE,
                 "funnel_correlation_type": FunnelCorrelationType.PROPERTIES,
-                "funnel_correlation_value": "$browser",
+                "funnel_correlation_names": ["$browser"],
             },
             team=self.team,
         )
