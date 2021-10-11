4b9ec01f97f033d7e5e5bf53d2a94ba68cf1469a
Marius Andra <marius.andra@gmail.com>
2021-10-11 20:47:19 +0200

    loadResults doesn't only load funnel results, bail if so

--- frontend/src/scenes/funnels/funnelLogic.ts
+++ frontend/src/scenes/funnels/funnelLogic.ts
@@ -615,7 +615,10 @@ export const funnelLogic = kea<funnelLogicType>({
     }),
 
     listeners: ({ actions, values, props }) => ({
-        loadResultsSuccess: async () => {
+        loadResultsSuccess: async ({ insight }) => {
+            if (insight.filters?.insight !== ViewType.FUNNELS) {
+                return
+            }
             // hide all but the first five breakdowns for each step
             values.steps?.forEach((step) => {
                 values.flattenedStepsByBreakdown
