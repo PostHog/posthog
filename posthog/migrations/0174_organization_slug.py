97cfbadb03599b68588ffeb98ba365cf14cd4feb
Michael Matloka <dev@twixes.com>
2021-10-13 13:00:26 +0200

    Ignore `test_migrations_are_null`

--- posthog/migrations/0174_organization_slug.py
+++ posthog/migrations/0174_organization_slug.py
@@ -40,6 +40,8 @@ class Migration(migrations.Migration):
             field=LowercaseSlugField(max_length=MAX_SLUG_LENGTH, null=True, unique=True),
         ),
         migrations.RunPython(slugify_all, migrations.RunPython.noop),
+        # Make test_migrations_are_null ignore slug being an IS NOT NULL column
+        migrations.RunSQL("-- not-null-ignore"),
         migrations.AlterField(
             model_name="organization", name="slug", field=LowercaseSlugField(max_length=MAX_SLUG_LENGTH, unique=True),
         ),
