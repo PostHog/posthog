378ff2efc3751a30049c4437d02df6a028d29f72
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-10-14 00:56:15 +0300

    Release ingestion help button (#6411)
    * remove ingestion help button
    
    * and feature flag

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -233,7 +233,6 @@ export const FEATURE_FLAGS = {
     NPS_PROMPT: '4562-nps',
     PLUGIN_METRICS: '4871-plugin-metrics',
     SESSIONS_TABLE: '4964-sessions-table', // Expand/collapse all in sessions table (performance consideration)
-    INGESTION_HELP_BUTTON: '112-ingestion-help-button',
     SAVED_INSIGHTS: '3408-saved-insights',
     MULTIVARIATE_SUPPORT: '5440-multivariate-support',
     FUNNEL_HORIZONTAL_UI: '5730-funnel-horizontal-ui',
