e7b7c18ded539fac2716d6a7bc2670eca8000d12
Eric Duong <eeoneric@gmail.com>
2021-10-14 12:26:23 -0400

    make sure to remove shown_as when switching to trends from stickiness (#6443)

--- frontend/src/scenes/insights/utils/cleanFilters.test.ts
+++ frontend/src/scenes/insights/utils/cleanFilters.test.ts
@@ -1,5 +1,6 @@
 import { cleanFilters } from './cleanFilters'
 import { ChartDisplayType } from '~/types'
+import { ShownAsValue } from 'lib/constants'
 
 describe('cleanFilters', () => {
     it('switches display to table if moving from TRENDS to RETENTION', () => {
@@ -9,5 +10,14 @@ describe('cleanFilters', () => {
                 { insight: 'TRENDS', display: ChartDisplayType.ActionsLineGraphLinear }
             )
         ).toEqual(expect.objectContaining({ insight: 'RETENTION', display: ChartDisplayType.ActionsTable }))
+    })
+
+    it('removes shownas if moving from stickiness to trends', () => {
+        expect(
+            cleanFilters(
+                { insight: 'TRENDS', shown_as: ShownAsValue.STICKINESS },
+                { insight: 'STICKINESS', shown_as: ShownAsValue.STICKINESS }
+            )
+        ).toEqual(expect.objectContaining({ insight: 'TRENDS', shown_as: undefined }))
     })
 })
