4477c81f1f3ca7dbfce99e0bd66c2424d724582b
Michael Matloka <dev@twixes.com>
2021-10-16 00:07:45 +0200

    Optimize `dashboardsModel` a tiny bit (#6455)
    * Optimize `dashboardsModel` a tiny bit
    
    * Fix typing
    
    * Make sorting more reliable
    
    * Use more correct `dashboardModel` selectors, with better names
    
    * Fix value name
    
    * Fix and simplify `nextDashboard`
    
    * Rename selectors

--- frontend/src/scenes/saved-insights/SavedInsights.tsx
+++ frontend/src/scenes/saved-insights/SavedInsights.tsx
@@ -77,7 +77,7 @@ export function SavedInsights(): JSX.Element {
         searchTerm,
         dates: { dateFrom, dateTo },
     } = useValues(savedInsightsLogic)
-    const { dashboards } = useValues(dashboardsModel)
+    const { nameSortedDashboards } = useValues(dashboardsModel)
     const { hasDashboardCollaboration } = useValues(organizationLogic)
     const { members } = useValues(membersLogic)
     const insightTypes: InsightType[] = [
@@ -178,13 +178,13 @@ export function SavedInsights(): JSX.Element {
                             overlayStyle={{ minWidth: 240, border: '1px solid var(--primary)' }}
                             overlay={
                                 <Menu style={{ padding: '12px 4px' }} data-attr={`insight-${item.id}-dropdown-menu`}>
-                                    {dashboards.filter((d) => d.id !== item.id).length > 0 ? (
+                                    {nameSortedDashboards.filter((d) => d.id !== item.id).length > 0 ? (
                                         <Menu.SubMenu
                                             data-attr={'insight-' + item.id + '-dropdown-move'}
                                             key="move"
                                             title="Add to dashboard"
                                         >
-                                            {dashboards
+                                            {nameSortedDashboards
                                                 .filter((d) => d.id !== item.id)
                                                 .map((dashboard, moveIndex) => (
                                                     <Menu.Item
