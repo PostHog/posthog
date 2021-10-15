4b90b2c9705011f63e4048090619c05f3a10bc56
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-10-15 12:40:23 +0300

    dismiss pay gate

--- frontend/src/lib/components/PayCard/PayCard.tsx
+++ frontend/src/lib/components/PayCard/PayCard.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './PayCard.scss'
-import { ArrowRightOutlined, RightOutlined } from '@ant-design/icons'
+import { ArrowRightOutlined, RightOutlined, CloseOutlined } from '@ant-design/icons'
 import { useActions, useValues } from 'kea'
 import { preflightLogic } from 'scenes/PreflightCheck/logic'
 import { Col, Row } from 'antd'
 import { AvailableFeature } from '~/types'
 import { router } from 'kea-router'
-
+import { eventUsageLogic } from 'lib/utils/eventUsageLogic'
 interface PayCardProps {
     title: string
     caption: string
@@ -14,9 +14,11 @@ interface PayCardProps {
     identifier: AvailableFeature
 }
 
-export function PayCard({ title, caption, docsLink, identifier }: PayCardProps): JSX.Element {
+export function PayCard({ title, caption, docsLink, identifier }: PayCardProps): JSX.Element | null {
     const { preflight } = useValues(preflightLogic)
     const { push } = useActions(router)
+    const [shown, setShown] = useState(false)
+    const storageKey = `pay-gate-dismissed-${identifier}`
 
     const handleClick = (): void => {
         if (preflight?.cloud) {
@@ -25,8 +27,31 @@ export function PayCard({ title, caption, docsLink, identifier }: PayCardProps):
             window.open('https://posthog.com/pricing', '_blank')
         }
     }
+
+    const close = (e: React.MouseEvent): void => {
+        // TODO
+        e.stopPropagation()
+        setShown(false)
+        window.localStorage.setItem(storageKey, '1')
+        eventUsageLogic.actions.reportPayGateDismissed(identifier)
+    }
+
+    useEffect(() => {
+        if (!window.localStorage.getItem(storageKey)) {
+            setShown(true)
+            eventUsageLogic.actions.reportPayGateShown(identifier)
+        }
+    }, [])
+
+    if (!shown) {
+        return null
+    }
+
     return (
         <div className="pay-card" onClick={handleClick}>
+            <div className="close-button" onClick={close}>
+                <CloseOutlined />
+            </div>
             <Row>
                 <Col span={23}>
                     <h3>{title}</h3>
@@ -56,7 +81,7 @@ export function PayCard({ title, caption, docsLink, identifier }: PayCardProps):
                         </p>
                     )}
                 </Col>
-                <Col span={1} style={{ display: 'flex', fontSize: '2em', alignItems: 'center' }}>
+                <Col span={1} style={{ display: 'flex', fontSize: '1.4em', alignItems: 'center' }}>
                     <RightOutlined />
                 </Col>
             </Row>
