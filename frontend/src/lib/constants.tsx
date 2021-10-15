b0309c2390bca859b55bb9abe6536e30e0b97cbf
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-10-15 10:17:49 +0300

    Release 4871-plugin-metrics FF (#6437)

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -230,7 +230,6 @@ export const FEATURE_FLAGS = {
     TRAILING_WAU_MAU: '3638-trailing-wau-mau',
     EVENT_COLUMN_CONFIG: '4141-event-columns',
     NPS_PROMPT: '4562-nps',
-    PLUGIN_METRICS: '4871-plugin-metrics',
     SESSIONS_TABLE: '4964-sessions-table', // Expand/collapse all in sessions table (performance consideration)
     SAVED_INSIGHTS: '3408-saved-insights',
     MULTIVARIATE_SUPPORT: '5440-multivariate-support',
