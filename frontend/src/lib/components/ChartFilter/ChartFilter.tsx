215f11ee1af159b7bfe54cf5dcfc5814967e6e56
Li Yi Yu <li@posthog.com>
2021-10-18 11:11:09 -0400

    fix chart type dropdown bug and remove unused code

--- frontend/src/lib/components/ChartFilter/ChartFilter.tsx
+++ frontend/src/lib/components/ChartFilter/ChartFilter.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import { useActions, useValues } from 'kea'
-import { Select, Tag } from 'antd'
+import { Select } from 'antd'
 import { chartFilterLogic } from './chartFilterLogic'
 import {
     AreaChartOutlined,
     BarChartOutlined,
     LineChartOutlined,
-    OrderedListOutlined,
     PieChartOutlined,
     TableOutlined,
 } from '@ant-design/icons'
 import { ChartDisplayType, FilterType, FunnelVizType, ViewType } from '~/types'
-import { preflightLogic } from 'scenes/PreflightCheck/logic'
-
 interface ChartFilterProps {
     filters: FilterType
     onChange: (chartFilter: ChartDisplayType | FunnelVizType) => void
@@ -22,7 +19,6 @@ interface ChartFilterProps {
 export function ChartFilter({ filters, onChange, disabled }: ChartFilterProps): JSX.Element {
     const { chartFilter } = useValues(chartFilterLogic)
     const { setChartFilter } = useActions(chartFilterLogic)
-    const { preflight } = useValues(preflightLogic)
 
     const linearDisabled = !!filters.session && filters.session === 'dist'
     const cumulativeDisabled =
@@ -48,80 +44,48 @@ export function ChartFilter({ filters, onChange, disabled }: ChartFilterProps): 
         )
     }
 
-    function WarningTag({ children = null }: { children: React.ReactNode }): JSX.Element {
-        return (
-            <Tag color="orange" style={{ marginLeft: 8, fontSize: 10 }}>
-                {children}
-            </Tag>
-        )
-    }
-
-    const options =
-        filters.insight === ViewType.FUNNELS
-            ? preflight?.is_clickhouse_enabled
-                ? [
-                      {
-                          value: FunnelVizType.Steps,
-                          label: <Label icon={<OrderedListOutlined />}>Steps</Label>,
-                      },
-                      {
-                          value: FunnelVizType.Trends,
-                          label: (
-                              <Label icon={<LineChartOutlined />}>
-                                  Trends
-                                  <WarningTag>BETA</WarningTag>
-                              </Label>
-                          ),
-                      },
-                  ]
-                : [
-                      {
-                          value: FunnelVizType.Steps,
-                          label: <Label icon={<OrderedListOutlined />}>Steps</Label>,
-                      },
-                  ]
-            : [
-                  {
-                      label: 'Line Chart',
-                      options: [
-                          {
-                              value: ChartDisplayType.ActionsLineGraphLinear,
-                              label: <Label icon={<LineChartOutlined />}>Linear</Label>,
-                              disabled: linearDisabled,
-                          },
-                          {
-                              value: ChartDisplayType.ActionsLineGraphCumulative,
-                              label: <Label icon={<AreaChartOutlined />}>Cumulative</Label>,
-                              disabled: cumulativeDisabled,
-                          },
-                      ],
-                  },
-                  {
-                      label: 'Bar Chart',
-                      options: [
-                          {
-                              value: ChartDisplayType.ActionsBarChart,
-                              label: <Label icon={<BarChartOutlined />}>Time</Label>,
-                              disabled: barDisabled,
-                          },
-                          {
-                              value: ChartDisplayType.ActionsBarChartValue,
-                              label: <Label icon={<BarChartOutlined />}>Value</Label>,
-                              disabled: barValueDisabled,
-                          },
-                      ],
-                  },
-                  {
-                      value: ChartDisplayType.ActionsTable,
-                      label: <Label icon={<TableOutlined />}>Table</Label>,
-                      disabled: tableDisabled,
-                  },
-                  {
-                      value: ChartDisplayType.ActionsPieChart,
-                      label: <Label icon={<PieChartOutlined />}>Pie</Label>,
-                      disabled: pieDisabled,
-                  },
-              ]
+    const options = [
+        {
+            label: 'Line Chart',
+            options: [
+                {
+                    value: ChartDisplayType.ActionsLineGraphLinear,
+                    label: <Label icon={<LineChartOutlined />}>Linear</Label>,
+                    disabled: linearDisabled,
+                },
+                {
+                    value: ChartDisplayType.ActionsLineGraphCumulative,
+                    label: <Label icon={<AreaChartOutlined />}>Cumulative</Label>,
+                    disabled: cumulativeDisabled,
+                },
+            ],
+        },
+        {
+            label: 'Bar Chart',
+            options: [
+                {
+                    value: ChartDisplayType.ActionsBarChart,
+                    label: <Label icon={<BarChartOutlined />}>Time</Label>,
+                    disabled: barDisabled,
+                },
+                {
+                    value: ChartDisplayType.ActionsBarChartValue,
+                    label: <Label icon={<BarChartOutlined />}>Value</Label>,
+                    disabled: barValueDisabled,
+                },
+            ],
+        },
+        {
+            value: ChartDisplayType.ActionsTable,
+            label: <Label icon={<TableOutlined />}>Table</Label>,
+            disabled: tableDisabled,
+        },
+        {
+            value: ChartDisplayType.ActionsPieChart,
+            label: <Label icon={<PieChartOutlined />}>Pie</Label>,
+            disabled: pieDisabled,
+        },
+    ]
     return (
         <Select
             key="2"
