5e077f1eb24f3a91effeafd21292b7f2689ed185
Marius Andra <marius.andra@gmail.com>
2021-10-18 15:50:00 +0200

    Revert "Show user hash in events table for identified users with only distinct ids (#6403)"
    This reverts commit 3a899e57

--- frontend/src/scenes/persons/PersonHeader.tsx
+++ frontend/src/scenes/persons/PersonHeader.tsx
@@ -1,27 +1,50 @@
 import { PersonType } from '~/types'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { IconPerson } from 'lib/components/icons'
 import './PersonHeader.scss'
 import { Link } from 'lib/components/Link'
-import { useValues } from 'kea'
-import { personHeaderLogic } from 'scenes/persons/personHeaderLogic'
-import clsx from 'clsx'
+import { urls } from 'scenes/urls'
 
-export interface PersonHeaderProps {
+export interface PersonHeader {
     person?: Partial<PersonType> | null
     withIcon?: boolean
 }
 
-export function PersonHeader(props: PersonHeaderProps): JSX.Element {
-    const logic = personHeaderLogic(props)
-    const { withIcon, personDisplay, personLink, isIdentified } = useValues(logic)
+export function PersonHeader({ person, withIcon = true }: PersonHeader): JSX.Element {
+    const propertyIdentifier = person?.properties
+        ? person.properties.email || person.properties.name || person.properties.username
+        : null
+    const customIdentifier =
+        typeof propertyIdentifier === 'object' ? JSON.stringify(propertyIdentifier) : propertyIdentifier
+
+    const displayId = useMemo(() => {
+        if (!person?.distinct_ids?.length) {
+            return null
+        }
+        const baseId = person.distinct_ids[0].replace(/\W/g, '')
+        return baseId.substr(baseId.length - 5).toUpperCase()
+    }, [person])
 
     return (
-        <Link to={personLink.length ? personLink : undefined} data-attr="goto-person-email">
-            <div className={clsx('person-header', { identified: isIdentified, anonymous: !isIdentified })}>
-                {withIcon && <IconPerson style={{ marginRight: 8 }} />}
-                {isIdentified ? <span className="ph-no-capture text-ellipsis">{personDisplay}</span> : personDisplay}
-            </div>
+        <Link
+            to={person?.distinct_ids?.length ? urls.person(person.distinct_ids[0]) : undefined}
+            data-attr="goto-person-email"
+        >
+            {person?.is_identified ? (
+                <div className="person-header identified">
+                    {withIcon && <IconPerson style={{ marginRight: 8 }} />}
+                    {customIdentifier ? (
+                        <span className="ph-no-capture text-ellipsis">{customIdentifier}</span>
+                    ) : (
+                        <i>No email or name set</i>
+                    )}
+                </div>
+            ) : (
+                <div className="person-header anonymous">
+                    {withIcon && <IconPerson style={{ marginRight: 8 }} />}Unidentified{' '}
+                    {customIdentifier || `user ${displayId}`}
+                </div>
+            )}
         </Link>
     )
 }
