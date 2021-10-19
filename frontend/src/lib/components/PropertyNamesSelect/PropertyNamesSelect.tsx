c44a6f98a9525c91823a3c742d17167206b166d8
Harry Waye <harry@scalexp.com>
2021-10-19 13:28:06 +0100

    add regex split comment

--- frontend/src/lib/components/PropertyNamesSelect/PropertyNamesSelect.tsx
+++ frontend/src/lib/components/PropertyNamesSelect/PropertyNamesSelect.tsx
@@ -207,6 +207,10 @@ const usePropertySearch = (properties: PersonProperty[]) => {
             properties
                 // First we split on query term, case insensitive, and globally,
                 // not just the first
+                // NOTE: it's important to use a capture group here, otherwise
+                // the query string match will not be included as a part. See
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split#splitting_with_a_regexp_to_include_parts_of_the_separator_in_the_result
+                // for details
                 .map((property) => ({
                     ...property,
                     nameParts: property.name.split(new RegExp(`(${query})`, 'gi')),
