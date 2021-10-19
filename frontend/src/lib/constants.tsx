f472b163e244b314c26de8bb715659c3118ff81f
Paul D'Ambra <paul@posthog.com>
2021-10-19 17:32:30 +0100

    adds a feature flag constant for #938 (#6555)

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -247,6 +247,7 @@ export const FEATURE_FLAGS = {
     CORRELATION_ANALYSIS: 'correlation-analysis', // owner: @neilkakkar
     SIGMA_ANALYSIS: 'sigma-analysis', // owner: @neilkakkar
     NEW_SESSIONS_PLAYER: 'new-sessions-player', // owner: @rcmarron
+    BREAKDOWN_BY_MULTIPLE_PROPERTIES: '938-breakdown-by-multiple-properties', // owner: @pauldambra
 }
 
 export const ENTITY_MATCH_TYPE = 'entities'
