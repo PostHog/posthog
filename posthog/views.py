c9a4785d7a2385435c97a40603d6410e6b4732b0
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-10-20 14:17:30 -0700

    Debug queries (#6577)

--- posthog/views.py
+++ posthog/views.py
@@ -108,6 +108,7 @@ def preflight_check(request: HttpRequest) -> JsonResponse:
             "is_event_property_usage_enabled": settings.ASYNC_EVENT_PROPERTY_USAGE,
             "licensed_users_available": get_licensed_users_available(),
             "site_url": settings.SITE_URL,
+            "debug_queries": settings.DEBUG_QUERIES,
         }
 
     return JsonResponse(response)
