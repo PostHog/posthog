366ebba1da77a0df69b08af9d21bf87e5c037845
Paul D'Ambra <paul.dambra@gmail.com>
2021-10-21 08:51:41 +0100

    better markup and spacing

--- frontend/src/toolbar/actions/ActionsList.tsx
+++ frontend/src/toolbar/actions/ActionsList.tsx
@@ -13,21 +13,25 @@ export function ActionsList(): JSX.Element {
     const { newAction } = useActions(actionsTabLogic)
 
     return (
-        <div className="actions-list">
+        <>
             <Input.Search
                 allowClear
                 autoFocus
                 placeholder="Search"
                 value={searchTerm}
-                className={'feature-flag-row'}
+                style={{
+                    paddingBottom: '4px',
+                }}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <Row className="actions-list-header">
-                <Button type="primary" size="small" onClick={() => newAction()} style={{ float: 'right' }}>
-                    <PlusOutlined /> New Action
-                </Button>
-            </Row>
-            {allActions.length === 0 && allActionsLoading ? <Spin /> : <ActionsListView actions={sortedActions} />}
-        </div>
+            <div className="actions-list">
+                <Row className="actions-list-header">
+                    <Button type="primary" size="small" onClick={() => newAction()} style={{ float: 'right' }}>
+                        <PlusOutlined /> New Action
+                    </Button>
+                </Row>
+                {allActions.length === 0 && allActionsLoading ? <Spin /> : <ActionsListView actions={sortedActions} />}
+            </div>
+        </>
     )
 }
