16389580bf5aed28ee1238b702759d6094526adb
Paul D'Ambra <paul.dambra@gmail.com>
2021-10-21 08:26:26 +0100

    add search to toolbar actions display for #6202

--- frontend/src/toolbar/actions/ActionsList.tsx
+++ frontend/src/toolbar/actions/ActionsList.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { useActions, useValues } from 'kea'
 import { actionsLogic } from '~/toolbar/actions/actionsLogic'
-import { Button, Spin, Row } from 'antd'
+import { Button, Spin, Row, Input } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import { actionsTabLogic } from '~/toolbar/actions/actionsTabLogic'
 import { ActionsListView } from '~/toolbar/actions/ActionsListView'
 
 export function ActionsList(): JSX.Element {
-    const { allActions, sortedActions, allActionsLoading } = useValues(actionsLogic)
+    const { allActions, sortedActions, allActionsLoading, searchTerm } = useValues(actionsLogic)
+    const { setSearchTerm } = useActions(actionsLogic)
 
     const { newAction } = useActions(actionsTabLogic)
 
     return (
         <div className="actions-list">
+            <Input.Search
+                allowClear
+                autoFocus
+                placeholder="Search"
+                value={searchTerm}
+                className={'feature-flag-row'}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <Row className="actions-list-header">
                 <Button type="primary" size="small" onClick={() => newAction()} style={{ float: 'right' }}>
                     <PlusOutlined /> New Action
