6a8945580d90486971eb83f80e23700dc41e7bfb
Michael Matloka <dev@twixes.com>
2021-10-22 15:56:19 +0200

    Migrate feature flags-related logics to be project-based (#6603)
    * Migrate feature flags-related logics to be project-based
    
    * Add comment

--- frontend/src/scenes/experimentation/featureFlagsLogic.ts
+++ frontend/src/scenes/experimentation/featureFlagsLogic.ts
@@ -2,17 +2,21 @@ import { kea } from 'kea'
 import api from 'lib/api'
 import { featureFlagsLogicType } from './featureFlagsLogicType'
 import { FeatureFlagType } from '~/types'
+import { teamLogic } from '../teamLogic'
 
 export const featureFlagsLogic = kea<featureFlagsLogicType>({
+    connect: {
+        values: [teamLogic, ['currentTeamId']],
+    },
     loaders: ({ values }) => ({
         featureFlags: {
             __default: [] as FeatureFlagType[],
             loadFeatureFlags: async () => {
-                const response = await api.get('api/feature_flag/')
+                const response = await api.get(`api/projects/${values.currentTeamId}/feature_flags/`)
                 return response.results as FeatureFlagType[]
             },
             updateFeatureFlag: async ({ id, payload }: { id: number; payload: Partial<FeatureFlagType> }) => {
-                const response = await api.update(`api/feature_flag/${id}`, payload)
+                const response = await api.update(`api/projects/${values.currentTeamId}/feature_flags/${id}`, payload)
                 return [...values.featureFlags].map((flag) => (flag.id === response.id ? response : flag))
             },
         },
