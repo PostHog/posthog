421091aad732aa1b0c4ea600b497dedb16766df0
Marius Andra <marius.andra@gmail.com>
2021-10-22 15:19:16 +0200

    fetch result from dashboard if mounted

--- frontend/src/scenes/insights/insightLogic.tsx
+++ frontend/src/scenes/insights/insightLogic.tsx
@@ -20,6 +20,7 @@ import { pollFunnel } from 'scenes/funnels/funnelUtils'
 import { preflightLogic } from 'scenes/PreflightCheck/logic'
 import { extractObjectDiffKeys } from './utils'
 import * as Sentry from '@sentry/browser'
+import { dashboardLogic } from 'scenes/dashboard/dashboardLogic'
 
 const IS_TEST_MODE = process.env.NODE_ENV === 'test'
 
@@ -557,7 +558,23 @@ export const insightLogic = kea<insightLogicType>({
                         actions.setInsightMode(ItemMode.Edit, null)
                     }
                 } else if (hashParams.fromItem) {
-                    if (!values.insight?.id || values.insight?.id !== hashParams.fromItem) {
+                    const insightIdChanged = !values.insight.id || values.insight.id !== hashParams.fromItem
+
+                    let loadedFromDashboard = false
+                    if (hashParams.fromDashboard && (!values.insight.result || insightIdChanged)) {
+                        const logic = dashboardLogic.build({ id: hashParams.fromDashboard }, false)
+                        if (logic.isMounted()) {
+                            const insight = logic.values.allItems?.items?.find(
+                                (item: DashboardItemType) => item.id === Number(hashParams.fromItem)
+                            )
+                            if (insight?.result) {
+                                actions.setInsight(insight, false)
+                                loadedFromDashboard = true
+                            }
+                        }
+                    }
+
+                    if (!loadedFromDashboard && insightIdChanged) {
                         // Do not load the result if missing, as setFilters below will do so anyway.
                         actions.loadInsight(hashParams.fromItem, { doNotLoadResults: true })
                     }
