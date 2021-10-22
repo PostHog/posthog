b48adcaa9983f2c32c62e87521015209cdfb43e8
Tim Glaser <tim@posthog.com>
2021-10-22 17:49:42 +0200

    Try localStorage+cookie as persistence type (#6540)

--- frontend/src/loadPostHogJS.tsx
+++ frontend/src/loadPostHogJS.tsx
@@ -26,6 +26,7 @@ export function loadPostHogJS(): void {
                 _capture_metrics: true,
                 rageclick: true,
                 debug: window.JS_POSTHOG_SELF_CAPTURE,
+                persistence: 'localStorage+cookie',
             })
         )
         // Make sure we have access to the object in window for debugging
