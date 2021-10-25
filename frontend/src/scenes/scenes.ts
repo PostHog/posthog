5071e1633261bf7dc5b2a86bf9be23f86a569ca6
Marius Andra <marius.andra@gmail.com>
2021-10-25 09:22:00 +0200

    this works

--- frontend/src/scenes/scenes.ts
+++ frontend/src/scenes/scenes.ts
@@ -38,7 +38,7 @@ export const scenes: Record<Scene, () => any> = {
     [Scene.SessionRecordings]: () =>
         import(/* webpackChunkName: 'sessionRecordings' */ './session-recordings/SessionRecordings'),
     [Scene.Person]: () => import(/* webpackChunkName: 'person' */ './persons/Person'),
-    [Scene.Persons]: () => import(/* webpackChunkName: 'persons' */ './persons/Persons'), // recheck
+    [Scene.Persons]: () => import(/* webpackChunkName: 'persons' */ './persons/Persons'),
     [Scene.Action]: () => import(/* webpackChunkName: 'action' */ './actions/Action'), // TODO
     [Scene.FeatureFlags]: () => import(/* webpackChunkName: 'featureFlags' */ './experimentation/FeatureFlags'),
     [Scene.FeatureFlag]: () => import(/* webpackChunkName: 'featureFlag' */ './experimentation/FeatureFlag'),
