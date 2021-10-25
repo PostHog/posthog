da721cd3a86a1e81ea099e15e9aec1c6dc82b613
Harry Waye <harry@scalexp.com>
2021-10-25 13:57:40 +0100

    fix typing

--- frontend/src/lib/components/PropertyNamesSelect/PropertyNamesSelect.test.tsx
+++ frontend/src/lib/components/PropertyNamesSelect/PropertyNamesSelect.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { PropertyNamesSelect } from './PropertyNamesSelect'
-import { render, within } from '@testing-library/react'
+import { render, waitFor, waitForElementToBeRemoved, within } from '@testing-library/react'
 import { setupServer } from 'msw/node'
 import userEvent from '@testing-library/user-event'
-import { GetPersonPropertiesRequest, GetPersonPropertiesResponse } from 'lib/api/person-properties'
 import { ResponseResolver, RestRequest, RestContext, rest } from 'msw'
 import { initKeaTestLogic } from '~/test/init'
 import { Provider } from 'kea'
+import { PersonProperty } from '~/types'
 
 initKeaTestLogic()
 
@@ -110,6 +110,41 @@ test('Can load, deselect and select all, and receive selection via onChange', as
     await within(combo).findByText(/0 of 3 selected/)
     expect(onChange).toHaveBeenLastCalledWith([])
 })
+
+test('Can filter properties by case insensitive substring match', async () => {
+    const server = setupServer(
+        mockGetPersonProperties((_, res, ctx) =>
+            res(
+                ctx.json([
+                    { id: 1, name: 'Property A', count: 10 },
+                    { id: 2, name: 'Property B', count: 20 },
+                    { id: 3, name: 'Property C', count: 30 },
+                ])
+            )
+        )
+    )
+    server.listen()
+
+    const onChange = jest.fn()
+
+    const { findByRole, queryByRole } = render(
+        <Provider>
+            <PropertyNamesSelect onChange={onChange} />
+        </Provider>
+    )
+    const combo = await findByRole('combobox')
+    const summaryText = await within(combo).findByText(/0 of 3 selected/)
+    userEvent.click(summaryText)
+
+    await findByRole('checkbox', { name: /Property B/ })
+    const searchBox = await findByRole('textbox')
+    userEvent.type(searchBox, 'property a')
+
+    await waitFor(() => expect(queryByRole('checkbox', { name: /Property B/ })).toBeNull())
+})
+
+export type GetPersonPropertiesResponse = PersonProperty[]
+export type GetPersonPropertiesRequest = undefined
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type
 const mockGetPersonProperties = (
