7c68971627e9ab98d14ea566eee20f5a5fbddb6f
Harry Waye <harry@scalexp.com>
2021-10-26 12:56:25 +0100

    chore: simplify storybook for `PropertyNamesSelect`

--- frontend/src/lib/components/PropertyNamesSelect/__stories__/PropertyNamesSelect.stories.tsx
+++ frontend/src/lib/components/PropertyNamesSelect/__stories__/PropertyNamesSelect.stories.tsx
@@ -1,6 +1,7 @@
+import { Provider } from 'kea'
 import { mockGetPersonProperties } from 'lib/components/TaxonomicFilter/__stories__/TaxonomicFilter.stories'
-import { keaStory } from 'lib/storybook/kea-story'
 import React from 'react'
+import { initKea } from '~/initKea'
 import { worker } from '~/mocks/browser'
 import { PropertyNamesSelect } from '../PropertyNamesSelect'
 
@@ -30,64 +31,13 @@ export const EmptyWithOptions = (): JSX.Element => {
         )
     )
 
-    return keaStory(
-        () => (
+    initKea()
+
+    return (
+        <Provider>
             <PropertyNamesSelect
                 onChange={(selectedProperties) => console.log('Selected Properties', selectedProperties)}
             />
-        ),
-        {
-            kea: {
-                router: {
-                    location: {
-                        pathname: '/insights',
-                        search: '?insight=FUNNELS&properties=%5B%5D&filter_test_accounts=false&events=%5B%7B%22id%22%3A%22%24pageview%22%2C%22name%22%3A%22%24pageview%22%2C%22type%22%3A%22events%22%2C%22order%22%3A0%7D%2C%7B%22id%22%3A%22%24pageview%22%2C%22name%22%3A%22%24pageview%22%2C%22type%22%3A%22events%22%2C%22order%22%3A1%7D%2C%7B%22id%22%3A%22%24pageview%22%2C%22name%22%3A%22%24pageview%22%2C%22type%22%3A%22events%22%2C%22order%22%3A2%7D%5D&actions=%5B%5D&funnel_viz_type=steps&display=FunnelViz&interval=day&new_entity=%5B%5D&date_from=-14d',
-                        hash: '',
-                    },
-                    searchParams: {
-                        insight: 'FUNNELS',
-                        properties: [],
-                        filter_test_accounts: false,
-                        events: [
-                            {
-                                id: '$pageview',
-                                name: '$pageview',
-                                type: 'events',
-                                order: 0,
-                            },
-                            {
-                                id: '$pageview',
-                                name: '$pageview',
-                                type: 'events',
-                                order: 1,
-                            },
-                            {
-                                id: '$pageview',
-                                name: '$pageview',
-                                type: 'events',
-                                order: 2,
-                            },
-                        ],
-                        actions: [],
-                        funnel_viz_type: 'steps',
-                        display: 'FunnelViz',
-                        interval: 'day',
-                        new_entity: [],
-                        date_from: '-14d',
-                    },
-                    hashParams: {},
-                },
-            },
-        }
-    )()
-}
-
-export const RequestFailure = (): JSX.Element => {
-    worker.use(mockGetPersonProperties((_, res, ctx) => res(ctx.delay(1500), ctx.status(500))))
-
-    return (
-        <PropertyNamesSelect
-            onChange={(selectedProperties) => console.log('Selected Properties', selectedProperties)}
-        />
+        </Provider>
     )
 }
