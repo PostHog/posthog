8e171d8d1115888bbfab81308ff3a71271e33741
Michael Matloka <dev@twixes.com>
2021-10-27 12:53:13 +0200

    Add `LemonPopover`

---
+++ frontend/src/lib/components/LemonPopover/index.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { LemonPopover } from '.'
+import { Button } from 'antd'
+
+export default {
+    title: 'PostHog/Components/LemonPopover',
+    component: LemonPopover,
+    parameters: { options: { showPanel: true } },
+} as ComponentMeta<typeof LemonPopover>
+
+const Template: ComponentStory<typeof LemonPopover> = (args) => <LemonPopover {...args} />
+
+export const Scream = Template.bind({})
+Scream.args = {
+    children: <Button type="primary">Click here…</Button>,
+    content: (
+        <>
+            <h3>Surprise! 😱</h3>You have been gnomed.
+        </>
+    ),
+    placement: 'bottom-left',
+}
