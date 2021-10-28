348a587dc41601a7a31576c226351cd49f58969b
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-10-28 11:14:28 +0300

    Add /api/projects/@current/groups/property_definitions
    To load property definitions for groups

---
+++ ee/clickhouse/views/groups.py
@@ -0,0 +1,39 @@
+import json
+from collections import defaultdict
+
+from rest_framework import exceptions, request, response, serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+
+from ee.clickhouse.client import sync_execute
+from ee.clickhouse.sql.person import GET_TEAM_PERSON_DISTINCT_IDS
+from posthog.api.routing import StructuredViewSetMixin
+from posthog.models.group_type_mapping import GroupTypeMapping
+
+
+class GroupTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupTypeMapping
+        fields = ["type_key", "type_id"]
+
+
+class ClickhouseGroupsView(StructuredViewSetMixin, viewsets.ViewSet):
+    @action(methods=["GET"], detail=False)
+    def property_definitions(self, request: request.Request, **kw):
+        rows = sync_execute(
+            f"""
+            SELECT group_type_index, tupleElement(keysAndValues, 1) as key, count(*) as count
+            FROM groups
+            ARRAY JOIN JSONExtractKeysAndValuesRaw(group_properties) as keysAndValues
+            WHERE team_id = %(team_id)s
+            GROUP BY group_type_index, tupleElement(keysAndValues, 1)
+            ORDER BY group_type_index ASC, count DESC, key ASC
+            """,
+            {"team_id": self.team.pk},
+        )
+
+        group_type_index_to_properties = defaultdict(list)
+        for group_type_index, key, count in rows:
+            group_type_index_to_properties[group_type_index].append({"name": key, "count": count})
+
+        return response.Response(group_type_index_to_properties)
