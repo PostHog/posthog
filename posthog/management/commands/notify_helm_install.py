bd970808827f7cffe010849efbe7a66c1070cec5
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-10-29 14:25:06 +0300

    Use groups for instance status reports

--- posthog/management/commands/notify_helm_install.py
+++ posthog/management/commands/notify_helm_install.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import posthoganalytics
 from django.core.management.base import BaseCommand
 
+from posthog.settings import SITE_URL
 from posthog.tasks.status_report import get_helm_info_env
 from posthog.utils import get_machine_id
 from posthog.version import VERSION
@@ -27,5 +28,5 @@ class Command(BaseCommand):
             posthoganalytics.api_key = "sTMFPsFhdP1Ssg"
             disabled = posthoganalytics.disabled
             posthoganalytics.disabled = False
-            posthoganalytics.capture(get_machine_id(), "helm_install", report)
+            posthoganalytics.capture(get_machine_id(), "helm_install", report, groups={"instance": SITE_URL})
             posthoganalytics.disabled = disabled
