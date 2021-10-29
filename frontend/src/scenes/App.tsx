eb01d3a4235a9442bcb7cb5077e242a25efbbe61
Michael Matloka <dev@twixes.com>
2021-10-29 22:35:38 +0200

    Fix sidebar sizing

--- frontend/src/scenes/App.tsx
+++ frontend/src/scenes/App.tsx
@@ -153,10 +153,7 @@ function AppScene(): JSX.Element | null {
     return (
         <>
             {featureFlags[FEATURE_FLAGS.LEMONADE] ? (
-                <Layout
-                    className={`${sceneConfig.dark ? 'bg-mid' : ''}`}
-                    style={{ minHeight: '100vh', height: '100%' }}
-                >
+                <Layout className={`${sceneConfig.dark ? 'bg-mid' : ''}`} style={{ minHeight: '100vh' }}>
                     {!sceneConfig.hideTopNav && <TopNavigation />}
                     <SideBar>{layoutContent}</SideBar>
                 </Layout>
