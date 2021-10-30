c8c50d36ca48170c967600734fac2f1906e2e0eb
Marius Andra <marius.andra@gmail.com>
2021-10-30 12:18:09 +0200

    make toolbar and its styles work

--- frontend/src/toolbar/ToolbarApp.tsx
+++ frontend/src/toolbar/ToolbarApp.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useSecondRender } from 'lib/hooks/useSecondRender'
 import root from 'react-shadow'
 import { ToolbarContainer } from '~/toolbar/ToolbarContainer'
@@ -13,30 +13,25 @@ export function ToolbarApp(props: ToolbarProps = {}): JSX.Element {
     useMountedLogic(toolbarLogic(props))
 
     const shadowRef = useRef<HTMLElementWithShadowRoot | null>(null)
+    const [didLoadStyles, setDidLoadStyles] = useState(false)
 
     // this runs after the shadow root has been added to the dom
     const didRender = useSecondRender(() => {
-        function addStyleElementToShadowRoot(element: HTMLElement): void {
-            const shadowRoot =
-                shadowRef.current?.shadowRoot || window.document.getElementById('__POSTHOG_TOOLBAR__')?.shadowRoot
-            shadowRoot?.getElementById('posthog-toolbar-styles')?.appendChild(element)
-        }
-
-        // add styles that webpack has rendered until now
-        const styles = ((window as any)['__PHGTLB_STYLES__'] || []) as unknown as HTMLStyleElement[]
-        if (styles) {
-            styles.forEach((element) => addStyleElementToShadowRoot(element))
-        }
-
-        // add function to render styles in the future
-        ;(window as any)['__PHGTLB_ADD_STYLES__'] = (element: HTMLStyleElement) => addStyleElementToShadowRoot(element)
+        const styleLink = document.createElement('link')
+        styleLink.rel = 'stylesheet'
+        styleLink.type = 'text/css'
+        styleLink.href = `${props.jsURL}/static/toolbar.css`
+        styleLink.onload = () => setDidLoadStyles(true)
+        const shadowRoot =
+            shadowRef.current?.shadowRoot || window.document.getElementById('__POSTHOG_TOOLBAR__')?.shadowRoot
+        shadowRoot?.getElementById('posthog-toolbar-styles')?.appendChild(styleLink)
     })
 
     return (
         <>
             <root.div id="__POSTHOG_TOOLBAR__" className="ph-no-capture" ref={shadowRef}>
                 <div id="posthog-toolbar-styles" />
-                {didRender ? <ToolbarContainer /> : null}
+                {didRender && didLoadStyles ? <ToolbarContainer /> : null}
                 <ToastContainer autoClose={8000} transition={Slide} position="bottom-center" />
             </root.div>
         </>
