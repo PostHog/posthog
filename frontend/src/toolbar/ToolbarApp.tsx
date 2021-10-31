f0348fb0e61b1b9e1acc71dc0b3828fc22269e93
Marius Andra <marius.andra@gmail.com>
2021-10-31 21:42:09 +0100

    fix toolbar url

--- frontend/src/toolbar/ToolbarApp.tsx
+++ frontend/src/toolbar/ToolbarApp.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import { useSecondRender } from 'lib/hooks/useSecondRender'
 import root from 'react-shadow'
 import { ToolbarContainer } from '~/toolbar/ToolbarContainer'
-import { useMountedLogic } from 'kea'
+import { useValues } from 'kea'
 import { toolbarLogic } from '~/toolbar/toolbarLogic'
 import { ToolbarProps } from '~/types'
 import { Slide, ToastContainer } from 'react-toastify'
@@ -10,7 +10,7 @@ import { Slide, ToastContainer } from 'react-toastify'
 type HTMLElementWithShadowRoot = HTMLElement & { shadowRoot: ShadowRoot }
 
 export function ToolbarApp(props: ToolbarProps = {}): JSX.Element {
-    useMountedLogic(toolbarLogic(props))
+    const { jsURL } = useValues(toolbarLogic(props))
 
     const shadowRef = useRef<HTMLElementWithShadowRoot | null>(null)
     const [didLoadStyles, setDidLoadStyles] = useState(false)
@@ -20,7 +20,7 @@ export function ToolbarApp(props: ToolbarProps = {}): JSX.Element {
         const styleLink = document.createElement('link')
         styleLink.rel = 'stylesheet'
         styleLink.type = 'text/css'
-        styleLink.href = `${props.jsURL}/static/toolbar.css`
+        styleLink.href = `${jsURL}/static/toolbar.css`
         styleLink.onload = () => setDidLoadStyles(true)
         const shadowRoot =
             shadowRef.current?.shadowRoot || window.document.getElementById('__POSTHOG_TOOLBAR__')?.shadowRoot
