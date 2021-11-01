09f9ba11506018fb046fc9b5366173605dc62b16
Marius Andra <marius.andra@gmail.com>
2021-11-01 23:23:16 +0100

    make toolbar with external styles work in storybook

--- frontend/src/toolbar/ToolbarApp.tsx
+++ frontend/src/toolbar/ToolbarApp.tsx
@@ -16,22 +16,26 @@ export function ToolbarApp(props: ToolbarProps = {}): JSX.Element {
     const [didLoadStyles, setDidLoadStyles] = useState(false)
 
     // this runs after the shadow root has been added to the dom
-    const didRender = useSecondRender(() => {
-        const styleLink = document.createElement('link')
-        styleLink.rel = 'stylesheet'
-        styleLink.type = 'text/css'
-        styleLink.href = `${jsURL}/static/toolbar.css`
-        styleLink.onload = () => setDidLoadStyles(true)
-        const shadowRoot =
-            shadowRef.current?.shadowRoot || window.document.getElementById('__POSTHOG_TOOLBAR__')?.shadowRoot
-        shadowRoot?.getElementById('posthog-toolbar-styles')?.appendChild(styleLink)
-    })
+    const didRender = useSecondRender(
+        props.disableExternalStyles
+            ? () => {}
+            : () => {
+                  const styleLink = document.createElement('link')
+                  styleLink.rel = 'stylesheet'
+                  styleLink.type = 'text/css'
+                  styleLink.href = `${jsURL}/static/toolbar.css`
+                  styleLink.onload = () => setDidLoadStyles(true)
+                  const shadowRoot =
+                      shadowRef.current?.shadowRoot || window.document.getElementById('__POSTHOG_TOOLBAR__')?.shadowRoot
+                  shadowRoot?.getElementById('posthog-toolbar-styles')?.appendChild(styleLink)
+              }
+    )
 
     return (
         <>
             <root.div id="__POSTHOG_TOOLBAR__" className="ph-no-capture" ref={shadowRef}>
                 <div id="posthog-toolbar-styles" />
-                {didRender && didLoadStyles ? <ToolbarContainer /> : null}
+                {didRender && (didLoadStyles || props.disableExternalStyles) ? <ToolbarContainer /> : null}
                 <ToastContainer autoClose={8000} transition={Slide} position="bottom-center" />
             </root.div>
         </>
