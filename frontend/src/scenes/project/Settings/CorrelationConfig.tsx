6f257406f627c8f7cd2d9b6d20ebb8c8a76061df
Harry Waye <harry@scalexp.com>
2021-11-02 17:43:54 +0000

    Make correlation config select styling like other configs
    This just adds the ability to select person properties to be excluded,
    as before but this time with the blue pill styling as with the others on
    the Project settings page.

--- frontend/src/scenes/project/Settings/CorrelationConfig.tsx
+++ frontend/src/scenes/project/Settings/CorrelationConfig.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useActions, useValues } from 'kea'
 import { teamLogic } from 'scenes/teamLogic'
-import { personPropertiesModel } from '~/models/personPropertiesModel'
-import { Select } from 'antd'
-import { DEFAULT_EXCLUDED_PERSON_PROPERTIES } from 'scenes/funnels/funnelLogic'
+
+import '../../../scenes/actions/Actions.scss'
+import { TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
+import { TaxonomicFilter } from 'lib/components/TaxonomicFilter/TaxonomicFilter'
+import { Button, Row } from 'antd'
+import { Popup } from 'lib/components/Popup/Popup'
+import PlusCircleOutlined from '@ant-design/icons/lib/icons/PlusCircleOutlined'
+import { CloseButton } from 'lib/components/CloseButton'
 
 export function CorrelationConfig(): JSX.Element {
     const { updateCurrentTeam } = useActions(teamLogic)
     const { currentTeam } = useValues(teamLogic)
-    const { personProperties } = useValues(personPropertiesModel)
 
     const handleChange = (excludedProperties: string[]): void => {
         updateCurrentTeam({ correlation_config: { excluded_person_property_names: excludedProperties } })
@@ -18,31 +22,103 @@ export function CorrelationConfig(): JSX.Element {
         <div style={{ marginBottom: 16 }}>
             <div style={{ marginBottom: 8 }}>
                 {currentTeam && (
-                    <>
-                        Excluded person properties:{' '}
-                        <Select
-                            mode="multiple"
-                            allowClear
-                            showSearch
-                            value={
-                                currentTeam.correlation_config?.excluded_person_property_names ||
-                                DEFAULT_EXCLUDED_PERSON_PROPERTIES
-                            }
-                            onChange={handleChange}
-                            style={{ width: 500 }}
-                        >
-                            {personProperties
-                                .map(({ name }) => name)
-                                .sort()
-                                .map((name) => (
-                                    <Select.Option key={name} value={name}>
-                                        {name}
-                                    </Select.Option>
-                                ))}
-                        </Select>
-                    </>
+                    <PersonPropertySelect
+                        onChange={handleChange}
+                        selectedProperties={currentTeam.correlation_config.excluded_person_property_names || []}
+                    />
                 )}
             </div>
         </div>
     )
+}
+
+interface PersonPropertySelectProps {
+    onChange: (names: string[]) => void
+    selectedProperties: string[]
+}
+
+const PersonPropertySelect = ({ onChange, selectedProperties }: PersonPropertySelectProps): JSX.Element => {
+    const { open, toggle, hide } = usePopup()
+
+    const handleChange = (name: string): void => {
+        onChange(Array.from(new Set(selectedProperties.concat([name]))))
+    }
+
+    const handleRemove = (name: string): void => {
+        onChange(selectedProperties.filter((p) => p !== name))
+    }
+
+    return (
+        <div className="mb">
+            {selectedProperties.length > 0 &&
+                selectedProperties.map((name) => {
+                    return (
+                        <Row
+                            key={name}
+                            align="middle"
+                            className="property-filter-row mt-05 mb-05"
+                            style={{
+                                width: '100%',
+                                margin: '0.25rem 0',
+                                padding: '0.25rem 0',
+                            }}
+                        >
+                            <Button type="primary" shape="round">
+                                {name}
+                            </Button>{' '}
+                            <CloseButton
+                                className="ml-1"
+                                onClick={() => handleRemove(name)}
+                                style={{ cursor: 'pointer', float: 'none', marginLeft: 5 }}
+                            />
+                        </Row>
+                    )
+                })}
+            <Row>
+                <Popup
+                    visible={open}
+                    overlay={
+                        <TaxonomicFilter
+                            onChange={(_, value) => {
+                                handleChange(value as string)
+                                hide()
+                            }}
+                            taxonomicGroupTypes={[TaxonomicFilterGroupType.PersonProperties]}
+                        />
+                    }
+                >
+                    {({ setRef }) => (
+                        <Button
+                            ref={setRef}
+                            onClick={() => toggle()}
+                            type="link"
+                            className="new-prop-filter"
+                            icon={<PlusCircleOutlined />}
+                        >
+                            Exclude person property
+                        </Button>
+                    )}
+                </Popup>
+            </Row>
+        </div>
+    )
+}
+
+const popupLogic = {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    toggle: (open: boolean) => !open,
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    hide: () => false,
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const usePopup = () => {
+    const [open, setOpen] = React.useState<boolean>(false)
+
+    return {
+        open,
+        toggle: () => setOpen(popupLogic.toggle(open)),
+        hide: () => setOpen(popupLogic.hide()),
+    }
 }
