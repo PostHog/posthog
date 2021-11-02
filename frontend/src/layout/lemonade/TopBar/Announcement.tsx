9eca47d38fa0a90a06f591aa5ee3046f6c6c599d
Michael Matloka <dev@twixes.com>
2021-11-02 12:51:36 +0100

    Mock `process` for VFile used by ReactMarkdown
    I was getting this otherwise:
    
    core.js:55 Uncaught ReferenceError: process is not defined
        at new VFile (core.js:55)
        at VFile (core.js:49)
        at Function.parse (index.js:273)
        at ReactMarkdown2 (react-markdown.js:42)
        at renderWithHooks (react-dom.development.js:14803)
        at mountIndeterminateComponent (react-dom.development.js:17482)
        at beginWork (react-dom.development.js:18596)
        at HTMLUnknownElement.callCallback2 (react-dom.development.js:188)
        at Object.invokeGuardedCallbackDev (react-dom.development.js:237)
        at invokeGuardedCallback (react-dom.development.js:292)

--- frontend/src/layout/lemonade/TopBar/Announcement.tsx
+++ frontend/src/layout/lemonade/TopBar/Announcement.tsx
@@ -3,6 +3,9 @@ import ReactMarkdown from 'react-markdown'
 import clsx from 'clsx'
 import { CloseOutlined } from '@ant-design/icons'
 
+// Mocking Node.js process to avoid https://github.com/remarkjs/react-markdown/issues/339
+window.process = { cwd: () => '' } as unknown as NodeJS.Process
+
 export interface AnnouncementProps {
     message: string
     visible: boolean
