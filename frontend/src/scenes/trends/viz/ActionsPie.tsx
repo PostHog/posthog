d69078401eabcd9252c6c3f799e128209817c4ee
eric <eeoneric@gmail.com>
2021-11-03 11:27:20 -0400

    populate 'all' when there's no dates

--- frontend/src/scenes/trends/viz/ActionsPie.tsx
+++ frontend/src/scenes/trends/viz/ActionsPie.tsx
@@ -74,8 +74,8 @@ export function ActionsPie({
                             const { dataset } = point
                             const action = dataset.actions[point.index]
                             const label = dataset.labels[point.index]
-                            const date_from = dataset.days[0] || 'all' // if there are no dates, it's all time
-                            const date_to = dataset.days[dataset.days.length - 1]
+                            const date_from = filtersParam.date_from || ''
+                            const date_to = filtersParam.date_to || ''
                             const breakdown_value = dataset.breakdownValues[point.index]
                                 ? dataset.breakdownValues[point.index]
                                 : null
