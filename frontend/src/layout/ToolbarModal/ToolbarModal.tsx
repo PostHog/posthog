e2848fe050609c18025943dfaf64b21b70fb7cf7
Michael Matloka <dev@twixes.com>
2021-11-03 17:49:02 +0100

    Wire up `ToolbarModal` in `SideBar`

--- frontend/src/layout/ToolbarModal/ToolbarModal.tsx
+++ frontend/src/layout/ToolbarModal/ToolbarModal.tsx
@@ -4,29 +4,32 @@ import { useValues } from 'kea'
 import { EditAppUrls } from 'lib/components/AppEditorLink/EditAppUrls'
 import { userLogic } from 'scenes/userLogic'
 import { ToolbarSettings } from 'scenes/project/Settings/ToolbarSettings'
+import { Modal, ModalProps } from 'antd'
 
-export function ToolbarModal(): JSX.Element {
+export function ToolbarModal({ visible, onCancel }: Pick<ModalProps, 'visible' | 'onCancel'>): JSX.Element {
     const { user } = useValues(userLogic)
     const toolbarEnabled = user?.toolbar_mode !== 'disabled'
 
     return (
-        <div className="toolbar-modal">
-            {!toolbarEnabled ? (
-                <>
-                    <h2>Enable the Toolbar to continue</h2>
-                    <ToolbarSettings />
-                </>
-            ) : (
-                <>
-                    <h2>Toolbar – Permitted Domains/URLs</h2>
-                    <p>
-                        Make sure you're using the snippet or the latest <code>posthog-js</code> version.
-                        <br />
-                        Clicking a URL launches it with the Toolbar.
-                    </p>
-                    <EditAppUrls allowNavigation />
-                </>
-            )}
-        </div>
+        <Modal bodyStyle={{ padding: 0 }} visible={visible} onCancel={onCancel} footer={null}>
+            <div className="toolbar-modal">
+                {!toolbarEnabled ? (
+                    <>
+                        <h2>Enable the Toolbar to continue</h2>
+                        <ToolbarSettings />
+                    </>
+                ) : (
+                    <>
+                        <h2>Toolbar – Permitted Domains/URLs</h2>
+                        <p>
+                            Make sure you're using the snippet or the latest <code>posthog-js</code> version.
+                            <br />
+                            Clicking a URL launches it with the Toolbar.
+                        </p>
+                        <EditAppUrls allowNavigation />
+                    </>
+                )}
+            </div>
+        </Modal>
     )
 }
