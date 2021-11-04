57ffb2233903c21c3503912afde7d0b92df306a0
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-11-04 13:56:26 +0200

    Add tests for trends breakdown_props function on group breakdowns

--- ee/clickhouse/queries/test/test_breakdown_props.py
+++ ee/clickhouse/queries/test/test_breakdown_props.py
@@ -3,11 +3,13 @@ from uuid import uuid4
 from freezegun import freeze_time
 
 from ee.clickhouse.models.event import create_event
+from ee.clickhouse.models.group import create_group
 from ee.clickhouse.queries.breakdown_props import get_breakdown_prop_values
-from ee.clickhouse.util import ClickhouseTestMixin
+from ee.clickhouse.util import ClickhouseTestMixin, snapshot_clickhouse_queries
 from posthog.models.cohort import Cohort
 from posthog.models.entity import Entity
 from posthog.models.filters import Filter
+from posthog.models.group_type_mapping import GroupTypeMapping
 from posthog.models.person import Person
 from posthog.test.base import APIBaseTest, test_with_materialized_columns
 
@@ -19,6 +21,7 @@ def _create_event(**kwargs):
 
 class TestBreakdownProps(ClickhouseTestMixin, APIBaseTest):
     @test_with_materialized_columns(event_properties=["$host", "distinct_id"], person_properties=["$browser", "email"])
+    @snapshot_clickhouse_queries
     def test_breakdown_person_props(self):
         p1 = Person.objects.create(team_id=self.team.pk, distinct_ids=["p1"], properties={"$browser": "test"})
         _create_event(
@@ -62,6 +65,7 @@ class TestBreakdownProps(ClickhouseTestMixin, APIBaseTest):
             )
             self.assertEqual(res, ["test"])
 
+    @snapshot_clickhouse_queries
     def test_breakdown_person_props_with_entity_filter(self):
         p1 = Person.objects.create(team_id=self.team.pk, distinct_ids=["p1"], properties={"$browser": "test"})
         _create_event(
@@ -113,3 +117,41 @@ class TestBreakdownProps(ClickhouseTestMixin, APIBaseTest):
                 )
                 res = get_breakdown_prop_values(filter, Entity(entity_params[0]), "count(*)", self.team.pk, 5)
                 self.assertEqual(res, ["test"])
+
+    @snapshot_clickhouse_queries
+    def test_breakdown_group_props(self):
+        GroupTypeMapping.objects.create(team=self.team, group_type="organization", group_type_index=0)
+        GroupTypeMapping.objects.create(team=self.team, group_type="company", group_type_index=1)
+
+        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:5", properties={"industry": "finance"})
+        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:6", properties={"industry": "technology"})
+        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:7", properties={"industry": "finance"})
+        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:8", properties={"industry": "another", "out": 1})
+        create_group(
+            team_id=self.team.pk, group_type_index=1, group_key="company:10", properties={"industry": "foobar"}
+        )
+
+        for org_index in range(5, 9):
+            _create_event(
+                event="$pageview",
+                distinct_id="person1",
+                team=self.team,
+                properties={"$group_0": f"org:{org_index}"},
+                timestamp="2020-01-02T12:00:00Z",
+            )
+
+
+        filter = Filter(
+            data={
+                "date_from": "2020-01-01T00:00:00Z",
+                "date_to": "2020-01-12T00:00:00Z",
+                "breakdown": "industry",
+                "breakdown_type": "group",
+                "breakdown_group_type_index": 0,
+                "events": [{"id": "$pageview", "type": "events", "order": 0,}],
+                "properties": [{"key": "out", "value": "", "type": "group", "group_type_index": 0, "operator": "is_not_set"}],
+            },
+            team=self.team
+        )
+        result = get_breakdown_prop_values(filter, filter.entities[0], "count(*)", self.team.pk, 5)
+        self.assertEqual(result, ['finance', 'technology'])
