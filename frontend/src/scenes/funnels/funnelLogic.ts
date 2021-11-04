d631de1c3700810db95feb10f915c1eadcc0bf20
Eric Duong <eeoneric@gmail.com>
2021-11-04 10:06:37 -0400

    [funnel ui] make sure interval state updates (#6717)
    * make sure state changes
    
    * change reducer to selector
    
    * use existing debounce dependency to debounce the local state change of conversion window
    
    Co-authored-by: Paul D'Ambra <paul.dambra@gmail.com>

--- frontend/src/scenes/funnels/funnelLogic.ts
+++ frontend/src/scenes/funnels/funnelLogic.ts
@@ -18,7 +18,6 @@ import {
     FlattenedFunnelStep,
     FunnelStepWithConversionMetrics,
     BinCountValue,
-    FunnelConversionWindow,
     FunnelConversionWindowTimeUnit,
     FunnelStepRangeEntityFilter,
     InsightLogicProps,
@@ -118,7 +117,6 @@ export const funnelLogic = kea<funnelLogicType>({
             index,
         }),
         saveFunnelInsight: (name: string) => ({ name }),
-        setConversionWindow: (conversionWindow: FunnelConversionWindow) => ({ conversionWindow }),
         openPersonsModal: (
             step: FunnelStep | FunnelStepWithNestedBreakdown,
             stepNumber: number,
@@ -266,24 +264,6 @@ export const funnelLogic = kea<funnelLogicType>({
         people: {
             clearFunnel: () => [],
         },
-        conversionWindow: [
-            {
-                funnel_window_interval_unit: FunnelConversionWindowTimeUnit.Day,
-                funnel_window_interval: 14,
-            } as FunnelConversionWindow,
-            {
-                setConversionWindow: (
-                    state,
-                    { conversionWindow: { funnel_window_interval_unit, funnel_window_interval } }
-                ) => {
-                    return {
-                        ...state,
-                        ...(funnel_window_interval_unit ? { funnel_window_interval_unit } : {}),
-                        ...(funnel_window_interval ? { funnel_window_interval } : {}),
-                    }
-                },
-            },
-        ],
         stepReference: [
             FunnelStepReference.total as FunnelStepReference,
             {
@@ -408,6 +388,13 @@ export const funnelLogic = kea<funnelLogicType>({
             ({ filters, result }): FunnelAPIResponse => (filters?.insight === ViewType.FUNNELS ? result : []),
         ],
         resultsLoading: [(s) => [s.insightLoading], (insightLoading) => insightLoading],
+        conversionWindow: [
+            (s) => [s.filters],
+            ({ funnel_window_interval, funnel_window_interval_unit }) => ({
+                funnel_window_interval: funnel_window_interval || 14,
+                funnel_window_interval_unit: funnel_window_interval_unit || FunnelConversionWindowTimeUnit.Day,
+            }),
+        ],
         stepResults: [
             (s) => [s.results, s.filters],
             (results, filters) =>
@@ -1133,10 +1120,6 @@ export const funnelLogic = kea<funnelLogicType>({
         setBinCount: async ({ binCount }) => {
             actions.setFilters(binCount && binCount !== BinCountAuto ? { bin_count: binCount } : {})
         },
-        setConversionWindow: async () => {
-            actions.setFilters(values.conversionWindow)
-        },
-
         excludeEventProperty: async ({ eventName, propertyName }) => {
             actions.loadEventWithPropertyCorrelations(eventName)
             eventUsageLogic.actions.reportCorrelationInteraction(
