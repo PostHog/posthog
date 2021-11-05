7bc628b380914a7641b9904170cc062b43e42c6b
Michael Matloka <dev@twixes.com>
2021-11-05 13:11:39 +0100

    Only show `RedesignOptIn` on cloud or in dev

--- frontend/src/layout/lemonade/RedesignOptIn.tsx
+++ frontend/src/layout/lemonade/RedesignOptIn.tsx
@@ -5,16 +5,18 @@ import { featureFlagLogic } from '../../lib/logic/featureFlagLogic'
 import { FEATURE_FLAGS } from '../../lib/constants'
 import { useValues } from 'kea'
 import './RedesignOptIn.scss'
+import { preflightLogic } from '../../scenes/PreflightCheck/logic'
 
-export function RedesignOptIn(): JSX.Element {
+export function RedesignOptIn(): JSX.Element | null {
     const { featureFlags } = useValues(featureFlagLogic)
+    const { preflight } = useValues(preflightLogic)
 
-    return (
+    return preflight?.cloud || preflight?.is_debug ? (
         <span className="RedesignOptIn">
             <span className="info">
                 {featureFlags[FEATURE_FLAGS.LEMONADE]
                     ? "You're using the new design"
-                    : "Try out PostHog's navigation redesign!"}
+                    : "Try PostHog's navigation redesign!"}
             </span>
             <Switch
                 size="small"
@@ -27,5 +29,5 @@ export function RedesignOptIn(): JSX.Element {
                 checked={featureFlags[FEATURE_FLAGS.LEMONADE] as boolean}
             />
         </span>
-    )
+    ) : null
 }
