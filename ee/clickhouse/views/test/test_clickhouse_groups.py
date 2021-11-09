55e2176efa2d85e0c0ece5a92fac5206cca367aa
Li Yi Yu <li@posthog.com>
2021-11-09 18:41:20 -0500

    fix

--- ee/clickhouse/views/test/test_clickhouse_groups.py
+++ ee/clickhouse/views/test/test_clickhouse_groups.py
@@ -1,21 +1,8 @@
-from unittest.mock import patch
-
 from ee.clickhouse.models.group import create_group
 from ee.clickhouse.util import ClickhouseTestMixin
 from posthog.test.base import APIBaseTest
 
 class ClickhouseTestGroupsApi(ClickhouseTestMixin, APIBaseTest):
-
-    def test_property_values(self):
-        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:5", properties={"industry": "finance"})
-        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:6", properties={"industry": "technology"})
-        create_group(team_id=self.team.pk, group_type_index=1, group_key="org:1", properties={"industry": "finance"})
-        group_type_index = 0
-        key = "industry"
-        response = self.client.get(f"/api/projects/{self.team.id}/groups/property_values/?key={key}&group_type_index={group_type_index}").json()
-        self.assertEqual(len(response), 2)
-        self.assertEqual(response[0]['name'][0], "finance")
-
     def test_property_definitions(self):
         create_group(team_id=self.team.pk, group_type_index=0, group_key="org:5", properties={"industry": "finance", "name": "Mr. Krabs"})
         create_group(team_id=self.team.pk, group_type_index=0, group_key="org:6", properties={"industry": "technology"})
@@ -27,3 +14,13 @@ class ClickhouseTestGroupsApi(ClickhouseTestMixin, APIBaseTest):
         self.assertEqual(response["0"][0]["count"], 2)
         self.assertEqual(response["1"][0]["name"], "name")
         self.assertEqual(response["1"][0]["count"], 1)
+
+    def test_property_values(self):
+        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:5", properties={"industry": "finance"})
+        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:6", properties={"industry": "technology"})
+        create_group(team_id=self.team.pk, group_type_index=1, group_key="org:1", properties={"industry": "finance"})
+        group_type_index = 0
+        key = "industry"
+        response = self.client.get(f"/api/projects/{self.team.id}/groups/property_values/?key={key}&group_type_index={group_type_index}").json()
+        self.assertEqual(len(response), 2)
+        self.assertEqual(response[0]['name'][0], "finance")
