f8b3a0534ea50979559cba635225044c9590fe93
Li Yi Yu <li@posthog.com>
2021-11-09 18:58:31 -0500

    python black

--- ee/clickhouse/views/test/test_clickhouse_groups.py
+++ ee/clickhouse/views/test/test_clickhouse_groups.py
@@ -2,9 +2,15 @@ from ee.clickhouse.models.group import create_group
 from ee.clickhouse.util import ClickhouseTestMixin
 from posthog.test.base import APIBaseTest
 
+
 class ClickhouseTestGroupsApi(ClickhouseTestMixin, APIBaseTest):
     def test_property_definitions(self):
-        create_group(team_id=self.team.pk, group_type_index=0, group_key="org:5", properties={"industry": "finance", "name": "Mr. Krabs"})
+        create_group(
+            team_id=self.team.pk,
+            group_type_index=0,
+            group_key="org:5",
+            properties={"industry": "finance", "name": "Mr. Krabs"},
+        )
         create_group(team_id=self.team.pk, group_type_index=0, group_key="org:6", properties={"industry": "technology"})
         create_group(team_id=self.team.pk, group_type_index=1, group_key="company:1", properties={"name": "Plankton"})
         create_group(team_id=self.team.pk, group_type_index=1, group_key="company:2", properties={})
@@ -21,7 +27,9 @@ class ClickhouseTestGroupsApi(ClickhouseTestMixin, APIBaseTest):
         create_group(team_id=self.team.pk, group_type_index=1, group_key="org:1", properties={"industry": "finance"})
         group_type_index = 0
         key = "industry"
-        response = self.client.get(f"/api/projects/{self.team.id}/groups/property_values/?key={key}&group_type_index={group_type_index}").json()
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/groups/property_values/?key={key}&group_type_index={group_type_index}"
+        ).json()
         self.assertEqual(len(response), 2)
-        self.assertIn("finance", response[0]['name'])
-        self.assertIn("technology", response[1]['name'])
+        self.assertIn("finance", response[0]["name"])
+        self.assertIn("technology", response[1]["name"])
