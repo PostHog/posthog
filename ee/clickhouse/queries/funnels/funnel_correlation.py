d8e6e4ea148c352d285d4bc568a9d5cf6cbc4955
Harry Waye <harry@scalexp.com>
2021-11-09 09:26:16 +0000

    Remove funnel_steps

--- ee/clickhouse/queries/funnels/funnel_correlation.py
+++ ee/clickhouse/queries/funnels/funnel_correlation.py
@@ -607,7 +607,6 @@ class FunnelCorrelation:
                 "breakdown": property_name,
                 "funnel_step_breakdown": property_value,
                 "breakdown_type": "person",
-                "funnel_steps": len(params["events"]) if success else -2,
                 "display": "FunnelViz",
                 "funnel_custom_steps": json.dumps(
                     [len(params["events"])] if success else list(range(1, len(params["events"])))
