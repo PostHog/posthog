59d8c40427e0ac622d7b6b0c3d347aa456acb03c
Alex Kim <alexgkim205@gmail.com>
2021-11-09 15:14:11 -0800

    hide events list behind FF

--- frontend/src/scenes/session-recordings/player/SessionRecordingPlayerV2.tsx
+++ frontend/src/scenes/session-recordings/player/SessionRecordingPlayerV2.tsx
@@ -1,16 +1,19 @@
 import './styles.scss'
 import React, { useEffect, useRef } from 'react'
 import { useActions, useValues } from 'kea'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { PLAYBACK_SPEEDS, sessionRecordingPlayerLogic } from './sessionRecordingPlayerLogic'
 import { PlayerFrame } from 'scenes/session-recordings/player/PlayerFrame'
 import { PlayerController } from 'scenes/session-recordings/player/PlayerController'
 import { PlayerEvents } from 'scenes/session-recordings/player/PlayerEvents'
 import { Col, Row } from 'antd'
+import { FEATURE_FLAGS } from 'lib/constants'
 
 export function SessionRecordingPlayerV2(): JSX.Element {
     const { togglePlayPause, seekForward, seekBackward, setSpeed, initReplayer, stopAnimation } =
         useActions(sessionRecordingPlayerLogic)
     const { isPlayable } = useValues(sessionRecordingPlayerLogic)
+    const { featureFlags } = useValues(featureFlagLogic)
     const frame = useRef<HTMLDivElement | null>(null)
 
     // Need useEffect to populate replayer on component paint
@@ -44,12 +47,17 @@ export function SessionRecordingPlayerV2(): JSX.Element {
     return (
         <Col className="session-player-v2" onKeyDown={handleKeyDown} tabIndex={0} flex={1}>
             <Row className="session-player-body" wrap={false}>
-                <Col className="player-container ph-no-capture" span={16}>
+                <Col
+                    className="player-container ph-no-capture"
+                    span={featureFlags[FEATURE_FLAGS.NEW_SESSIONS_PLAYER_EVENTS_LIST] ? 16 : 24}
+                >
                     <PlayerFrame ref={frame} />
                 </Col>
-                <Col className="player-events" span={8}>
-                    <PlayerEvents />
-                </Col>
+                {featureFlags[FEATURE_FLAGS.NEW_SESSIONS_PLAYER_EVENTS_LIST] && (
+                    <Col className="player-events" span={8}>
+                        <PlayerEvents />
+                    </Col>
+                )}
             </Row>
             <Row className="session-player-controller" align="middle">
                 <PlayerController />
