7989fd24b8e93fcda8d8df6861564531090ac08c
Harry Waye <harry@scalexp.com>
2021-11-10 15:50:32 +0000

    Revert "Revert "feat(correlation): use people_url to load people modal (#6992)" (#7029)" (#7032)
    This reverts commit de15fcda3f4d5a911256a630ef8089f3d1686d6b.

--- frontend/src/scenes/funnels/funnelLogic.ts
+++ frontend/src/scenes/funnels/funnelLogic.ts
@@ -26,10 +26,13 @@ import {
     FunnelStepReference,
     FunnelAPIResponse,
     TrendResult,
-    BreakdownType,
     FunnelCorrelationResultsType,
     AvailableFeature,
     TeamType,
+    EntityTypes,
+    PropertyFilter,
+    PropertyOperator,
+    BreakdownType,
 } from '~/types'
 import { FunnelLayout, BinCountAuto, FEATURE_FLAGS } from 'lib/constants'
 import { preflightLogic } from 'scenes/PreflightCheck/logic'
@@ -54,6 +57,7 @@ import { teamLogic } from '../teamLogic'
 import { personPropertiesModel } from '~/models/personPropertiesModel'
 import { userLogic } from 'scenes/userLogic'
 import { visibilitySensorLogic } from 'lib/components/VisibilitySensor/visibilitySensorLogic'
+import { elementsToAction } from 'scenes/events/createActionFromEvent'
 
 const DEVIATION_SIGNIFICANCE_MULTIPLIER = 1.5
 // Chosen via heuristics by eyeballing some values
@@ -134,14 +138,9 @@ export const funnelLogic = kea<funnelLogicType>({
             breakdown_type,
             customSteps,
         }),
-        openCorrelationPersonsModal: (
-            entity: Record<string, any>,
-            converted: boolean,
-            resultType: FunnelCorrelation['result_type']
-        ) => ({
-            entity,
-            converted,
-            resultType,
+        openCorrelationPersonsModal: (correlation: FunnelCorrelation, success: boolean) => ({
+            correlation,
+            success,
         }),
         setStepReference: (stepReference: FunnelStepReference) => ({ stepReference }),
         changeStepRange: (funnel_from_step?: number, funnel_to_step?: number) => ({
@@ -1115,39 +1114,43 @@ export const funnelLogic = kea<funnelLogicType>({
                 saved: true,
             })
         },
-        openPersonsModal: ({ step, stepNumber, breakdown_value, breakdown, breakdown_type, customSteps }) => {
+        openCorrelationPersonsModal: ({ correlation, success }) => {
             // :TODO: Support 'person' modal for groups
             if (values.filters.aggregation_group_type_index != undefined) {
                 return
             }
-            personsModalLogic.actions.loadPeople({
-                action: 'session',
-                breakdown_value: breakdown_value !== undefined ? breakdown_value : undefined,
-                label: step.name,
-                date_from: '',
-                date_to: '',
-                filters: { ...values.filters, breakdown, breakdown_type, funnel_custom_steps: customSteps },
-                saveOriginal: true,
-                funnelStep: stepNumber,
-            })
-        },
-        openCorrelationPersonsModal: ({ entity, converted, resultType }) => {
-            // :TODO: Support 'person' modal for groups
-            if (values.filters.aggregation_group_type_index != undefined) {
-                return
+            if (correlation.result_type === FunnelCorrelationResultsType.Properties) {
+                const { breakdown, breakdown_value } = parseBreakdownValue(correlation.event.event)
+                personsModalLogic.actions.loadPeopleFromUrl({
+                    url: success ? correlation.success_people_url : correlation.failure_people_url,
+                    // just display that we either completed the last step, or
+                    // dropped at the second to last step
+                    funnelStep: success ? values.stepsWithCount.length : -2,
+                    label: breakdown,
+                    breakdown_value,
+                })
+
+                eventUsageLogic.actions.reportCorrelationInteraction(
+                    FunnelCorrelationResultsType.Properties,
+                    'person modal',
+                    values.filters.funnel_correlation_person_entity
+                )
+            } else {
+                const { name, properties } = parseEventAndProperty(correlation.event)
+
+                personsModalLogic.actions.loadPeopleFromUrl({
+                    url: success ? correlation.success_people_url : correlation.failure_people_url,
+                    funnelStep: success ? values.stepsWithCount.length : -2,
+                    label: name,
+                })
+
+                eventUsageLogic.actions.reportCorrelationInteraction(correlation.result_type, 'person modal', {
+                    id: name,
+                    type: EntityTypes.EVENTS,
+                    properties,
+                    converted: success,
+                })
             }
-            personsModalLogic.actions.loadPeople({
-                action: { id: entity.id, name: entity.name, properties: entity.properties, type: entity.type },
-                label: entity.id,
-                date_from: '',
-                date_to: '',
-                filters: {
-                    ...values.filters,
-                    funnel_correlation_person_converted: converted ? 'true' : 'false',
-                    funnel_correlation_person_entity: entity,
-                },
-            })
-            eventUsageLogic.actions.reportCorrelationInteraction(resultType, 'person modal', { ...entity, converted })
         },
         changeStepRange: ({ funnel_from_step, funnel_to_step }) => {
             actions.setFilters({
@@ -1299,4 +1302,45 @@ const appendToCorrelationConfig = (
     teamLogic.actions.updateCurrentTeam({
         correlation_config: correlationConfig,
     })
+}
+
+const parseBreakdownValue = (item: string): { breakdown: string; breakdown_value: string } => {
+    const components = item.split('::')
+    if (components.length === 1) {
+        return { breakdown: components[0], breakdown_value: '' }
+    } else {
+        return {
+            breakdown: components[0],
+            breakdown_value: components[1],
+        }
+    }
+}
+
+const parseEventAndProperty = (event: FunnelCorrelation['event']): { name: string; properties?: PropertyFilter[] } => {
+    const components = event.event.split('::')
+    /*
+      The `event` is either an event name, or event::property::property_value
+    */
+    if (components.length === 1) {
+        return { name: components[0] }
+    } else if (components[0] === '$autocapture') {
+        // We use elementsToAction to generate the required property filters
+        const elementData = elementsToAction(event.elements)
+        return {
+            name: components[0],
+            properties: Object.entries(elementData)
+                .filter(([, propertyValue]) => !!propertyValue)
+                .map(([propertyKey, propertyValue]) => ({
+                    key: propertyKey,
+                    operator: PropertyOperator.Exact,
+                    type: 'element',
+                    value: [propertyValue as string],
+                })),
+        }
+    } else {
+        return {
+            name: components[0],
+            properties: [{ key: components[1], operator: PropertyOperator.Exact, value: components[2], type: 'event' }],
+        }
+    }
 }
