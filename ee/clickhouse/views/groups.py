c7b596c3a920f7a797f8a7a4a6e6450d3e04c387
Li Yi Yu <li@posthog.com>
2021-11-10 13:07:38 -0500

    get list of groups

--- ee/clickhouse/views/groups.py
+++ ee/clickhouse/views/groups.py
@@ -3,13 +3,12 @@ from collections import defaultdict
 
 from rest_framework import exceptions, request, response, serializers, viewsets
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.mixins import ListModelMixin
 
 from ee.clickhouse.client import sync_execute
-from ee.clickhouse.sql.person import GET_TEAM_PERSON_DISTINCT_IDS
 from posthog.api.routing import StructuredViewSetMixin
 from posthog.models.group_type_mapping import GroupTypeMapping
-import pdb 
+
 
 class GroupTypeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,43 +21,25 @@ class ClickhouseGroupsView(StructuredViewSetMixin, ListModelMixin, viewsets.Gene
     queryset = GroupTypeMapping.objects.all()
     pagination_class = None
 
-    def retrieve(self, request, *args, **kwargs):
-        instance = sync_execute(
-            "SELECT id, team_id, type_id, created_at, properties FROM groups WHERE team_id = %(team_id)s AND group_type_index = %(group_type_index)s AND id = %(id)s",
-            {"team_id": self.team_id, "id": self.kwargs["id"], "type_id": self.kwargs["type_id"]},
-        )
-        if not instance:
-            raise exceptions.NotFound(detail="Group not found.")
-        serializer = self.serializer_class(instance[0])
-        serializer.is_valid()
-        return response.Response(serializer.data)
-    
     def list(self, request, *args, **kwargs):
-        # group_type_mapping = GroupTypeMapping.objects.get(
-        #     team_id=self.team_id, type_key=self.kwargs["parent_lookup_type_key"]
-        # )
-        # instances = (
-        #     {
-        #         "id": row[0],
-        #         "team_id": row[1],
-        #         "type_id": row[2],
-        #         "created_at": row[3],
-        #         "team_id": row[4],
-        #         "properties": json.loads(row[5]),
-        #     }
-            # for row in sync_execute(
-        rows = sync_execute(
-            """
-            SELECT team_id, group_type_index, group_key, created_at, group_properties FROM groups
-        """,
-            # WHERE team_id = %(team_id)s AND group_type_index = %(group_type_index)s
-            # {"team_id": self.team_id, "group_type_index": 1},
+        instances = (
+            {
+                "team_id": row[0],
+                "group_type_index": row[1],
+                "group_key": row[2],
+                "created_at": row[3],
+                "group_properties": json.loads(row[4]),
+            }
+            for row in sync_execute(
+                """
+                SELECT team_id, group_type_index, group_key, created_at, group_properties FROM groups
+                WHERE team_id = %(team_id)s AND group_type_index = %(group_type_index)s
+            """,
+                {"team_id": self.team_id, "group_type_index": request.GET["group_type_index"]},
+            )
         )
-        # )
-        # pdb.set_trace()
-        # serializer = self.serializer_class(data=rows, many=True)
-        # serializer.is_valid()
-        # return response.Response(serializer.data)
+
+        return response.Response(instances)
 
     @action(methods=["GET"], detail=False)
     def property_definitions(self, request: request.Request, **kw):
@@ -79,5 +60,3 @@ class ClickhouseGroupsView(StructuredViewSetMixin, ListModelMixin, viewsets.Gene
             group_type_index_to_properties[group_type_index].append({"name": key, "count": count})
 
         return response.Response(group_type_index_to_properties)
-
-    
