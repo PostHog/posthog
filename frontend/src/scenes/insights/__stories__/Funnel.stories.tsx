510185a3959377dcac4fd57030dc25595b1e1ea7
Harry Waye <harry@scalexp.com>
2021-11-10 11:05:26 +0000

    remove some funnel stories

--- frontend/src/scenes/insights/__stories__/Funnel.stories.tsx
+++ frontend/src/scenes/insights/__stories__/Funnel.stories.tsx
@@ -1,11 +1,7 @@
 import { Meta } from '@storybook/react'
 
-import { keaStory } from 'lib/storybook/kea-story'
-
 import { Insights } from '../Insights'
 
-import funnelsJson from './funnels.json'
-import funnelsWithCorrelationJson from './funnelsWithCorrelation.json'
 import { rest } from 'msw'
 import { worker } from '../../../mocks/browser'
 import { FunnelResult, FunnelStep } from '~/types'
@@ -38,50 +34,6 @@ export default {
         },
     ],
 } as Meta
-
-export const NoEvents = (): JSX.Element => {
-    setFeatureFlags({ 'correlation-analysis': false })
-
-    worker.use(
-        rest.post('/api/insight/funnel/', (_, res, ctx) => {
-            return res(
-                ctx.json({
-                    result: [],
-                    last_refresh: '2021-10-11T15:00:52.117340Z',
-                    is_cached: true,
-                } as FunnelResponse)
-            )
-        })
-    )
-
-    return keaStory(Insights, funnelsJson)()
-}
-
-export const WithCorrelation = (): JSX.Element => {
-    setFeatureFlags({ 'correlation-analysis': true })
-
-    worker.use(
-        mockGetPersonProperties((_, res, ctx) =>
-            res(
-                ctx.json([
-                    { id: 1, name: '$geoip_country_code', count: 1 },
-                    { id: 2, name: '$os', count: 2 },
-                    { id: 3, name: '$browser', count: 3 },
-                ])
-            )
-        ),
-        rest.post('/api/insight/funnel/', (_, res, ctx) => {
-            return res(ctx.json(sampleFunnelResponse))
-        }),
-        rest.post<FunnelCorrelationRequest>('/api/insight/funnel/correlation/', (req, res, ctx) =>
-            req.body.funnel_correlation_type === 'properties'
-                ? res(ctx.json(samplePropertyCorrelationResponse))
-                : res(ctx.json(sampleEventCorrelationResponse))
-        )
-    )
-
-    return keaStory(Insights, funnelsWithCorrelationJson)()
-}
 
 export const WithCorrelationAndSkew = (): JSX.Element => {
     setFeatureFlags({ 'correlation-analysis': true })
@@ -327,46 +279,6 @@ const sampleEventWithPropertyCorrelationResponse: FunnelCorrelationResponse = {
     },
     last_refresh: '2021-10-26T15:36:39.921274Z',
     is_cached: false,
-}
-
-const sampleFunnelResponse: FunnelResponse = {
-    result: [
-        {
-            action_id: '$pageview',
-            name: '$pageview',
-            custom_name: null,
-            order: 0,
-            people: ['017c567f-1f26-0000-bdb3-d29a6484acb6'],
-            count: 10726,
-            type: 'events',
-            average_conversion_time: null,
-            median_conversion_time: null,
-        },
-        {
-            action_id: '$pageview',
-            name: '$pageview',
-            custom_name: null,
-            order: 1,
-            people: ['017c567f-1f26-0000-bdb3-d29a6484acb6'],
-            count: 7627,
-            type: 'events',
-            average_conversion_time: 3605.594525238891,
-            median_conversion_time: 2.0,
-        },
-        {
-            action_id: '$pageview',
-            name: '$pageview',
-            custom_name: null,
-            order: 2,
-            people: ['017c567f-1f26-0000-bdb3-d29a6484acb6'],
-            count: 3721,
-            type: 'events',
-            average_conversion_time: 7734.935688918132,
-            median_conversion_time: 6.0,
-        },
-    ],
-    last_refresh: '2021-10-11T15:00:52.117340Z',
-    is_cached: true,
 }
 
 const sampleSkewedFunnelResponse: FunnelResponse = {
