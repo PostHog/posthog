43a621de2117a90468217a42357ca603adac1e34
yakkomajuri <yakko.majuri@gmail.com>
2021-11-10 18:01:27 +0000

    test a new approach - make this a non-large migration?

--- ee/clickhouse/migrations/0021_person_table_version_column.py
+++ ee/clickhouse/migrations/0021_person_table_version_column.py
@@ -6,26 +6,15 @@ from posthog.settings import CLICKHOUSE_CLUSTER
 TEMPORARY_TABLE_NAME = "person_tmp_migration_0019"
 
 operations = [
+    migrations.RunSQL(
+        "ALTER TABLE person ADD COLUMN IF NOT EXISTS version UInt64, ADD COLUMN IF NOT EXISTS _partition UInt32"
+    ),
     migrations.RunSQL(PERSONS_TABLE_SQL.replace(PERSONS_TABLE, TEMPORARY_TABLE_NAME, 1)),
     migrations.RunSQL(f"DROP TABLE person_mv ON CLUSTER {CLICKHOUSE_CLUSTER}"),
     migrations.RunSQL(f"DROP TABLE kafka_person ON CLUSTER {CLICKHOUSE_CLUSTER}"),
-    migrations.RunSQL(
-        f"""
-        INSERT INTO {TEMPORARY_TABLE_NAME} (id, team_id, created_at, properties, is_identified, is_deleted, version, _partition, _timestamp, _offset)
-        SELECT
-            id,
-            team_id,
-            created_at,
-            properties,
-            is_identified,
-            is_deleted,
-            0,
-            0,
-            _timestamp,
-            _offset
-        FROM {PERSONS_TABLE}
-    """
-    ),
+    # Check partition names with: `SELECT partition FROM system.parts WHERE table = 'person' and active = 1 GROUP BY partition;`
+    # The persons table currently doesn't have a PARTITION BY clause so tuple() is the default
+    migrations.RunSQL(f"ALTER TABLE {TEMPORARY_TABLE_NAME} ATTACH PARTITION tuple() FROM person"),
     migrations.RunSQL(
         f"""
         RENAME TABLE
@@ -34,6 +23,7 @@ operations = [
         ON CLUSTER {CLICKHOUSE_CLUSTER}
     """
     ),
+    # migrations.RunSQL("OPTIMIZE TABLE person FINAL") # TODO: Investigate - do we need this?
     migrations.RunSQL(KAFKA_PERSONS_TABLE_SQL),
     migrations.RunSQL(PERSONS_TABLE_MV_SQL),
 ]
