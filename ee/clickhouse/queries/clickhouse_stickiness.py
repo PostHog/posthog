3c5c3a9a8ab20dd49b38151fce951f3059c01fec
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-11-15 13:05:44 +0200

    Refactor stickiness to have its own event_query
    This will speed up queries significantly and allow for filtering by
    group properties

--- ee/clickhouse/queries/clickhouse_stickiness.py
+++ ee/clickhouse/queries/clickhouse_stickiness.py
@@ -12,6 +12,7 @@ from ee.clickhouse.client import sync_execute
 from ee.clickhouse.models.action import format_action_filter
 from ee.clickhouse.models.person import ClickhousePersonSerializer
 from ee.clickhouse.models.property import parse_prop_clauses
+from ee.clickhouse.queries.stickiness.stickiness_event_query import StickinessEventsQuery
 from ee.clickhouse.queries.util import get_trunc_func_ch, parse_timestamps
 from ee.clickhouse.sql.person import (
     GET_LATEST_PERSON_SQL,
@@ -20,8 +21,6 @@ from ee.clickhouse.sql.person import (
     PEOPLE_SQL,
     PERSON_STATIC_COHORT_TABLE,
 )
-from ee.clickhouse.sql.stickiness.stickiness import STICKINESS_SQL
-from ee.clickhouse.sql.stickiness.stickiness_actions import STICKINESS_ACTIONS_SQL
 from ee.clickhouse.sql.stickiness.stickiness_people import STICKINESS_PEOPLE_SQL
 from posthog.constants import TREND_FILTER_TYPE_ACTIONS
 from posthog.models.cohort import Cohort
@@ -33,41 +32,18 @@ from posthog.queries.stickiness import Stickiness
 
 class ClickhouseStickiness(Stickiness):
     def stickiness(self, entity: Entity, filter: StickinessFilter, team_id: int) -> Dict[str, Any]:
+        events_query, event_params = StickinessEventsQuery(entity, filter, team_id).get_query()
 
-        parsed_date_from, parsed_date_to, date_params = parse_timestamps(filter=filter, team_id=team_id)
-        prop_filters, prop_filter_params = parse_prop_clauses(filter.properties + entity.properties, team_id)
-        trunc_func = get_trunc_func_ch(filter.interval)
+        query = f"""
+        SELECT countDistinct(aggregation_target), num_intervals FROM ({events_query})
+        WHERE num_intervals <= %(num_intervals)s
+        GROUP BY num_intervals
+        ORDER BY num_intervals
+        """
 
-        params: Dict = {"team_id": team_id}
-        params = {**params, **prop_filter_params, "num_intervals": filter.total_intervals, **date_params}
-        if entity.type == TREND_FILTER_TYPE_ACTIONS:
-            action = entity.get_action()
-            action_query, action_params = format_action_filter(action)
-            if action_query == "":
-                return {}
+        params = {**event_params, "num_intervals": filter.total_intervals}
 
-            params = {**params, **action_params}
-            content_sql = STICKINESS_ACTIONS_SQL.format(
-                team_id=team_id,
-                actions_query=action_query,
-                parsed_date_from=parsed_date_from,
-                parsed_date_to=parsed_date_to,
-                filters=prop_filters,
-                trunc_func=trunc_func,
-                GET_TEAM_PERSON_DISTINCT_IDS=GET_TEAM_PERSON_DISTINCT_IDS,
-            )
-        else:
-            content_sql = STICKINESS_SQL.format(
-                team_id=team_id,
-                event=entity.id,
-                parsed_date_from=parsed_date_from,
-                parsed_date_to=parsed_date_to,
-                filters=prop_filters,
-                trunc_func=trunc_func,
-                GET_TEAM_PERSON_DISTINCT_IDS=GET_TEAM_PERSON_DISTINCT_IDS,
-            )
-
-        counts = sync_execute(content_sql, params)
+        counts = sync_execute(query, params)
         return self.process_result(counts, filter)
 
     def _retrieve_people(
