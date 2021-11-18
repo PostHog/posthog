7802ccf2a8ec4abc66522b3f9a435c672e05059f
Michael Matloka <dev@twixes.com>
2021-11-18 15:30:11 +0100

    Fix unused import

--- frontend/src/layout/navigation/TopNavigation.tsx
+++ frontend/src/layout/navigation/TopNavigation.tsx
@@ -44,7 +44,6 @@ import { featureFlagLogic } from '../../lib/logic/featureFlagLogic'
 import { TopBar } from '../lemonade/TopBar'
 import { HelpButton } from 'lib/components/HelpButton/HelpButton'
 import { CommandPalette } from '../../lib/components/CommandPalette'
-import { RedesignOptIn } from '../lemonade/RedesignOptIn'
 
 export function WhoAmI({ user }: { user: UserType }): JSX.Element {
     return (
