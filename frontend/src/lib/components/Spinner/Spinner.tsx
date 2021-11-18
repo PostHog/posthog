1757f000ec59d2ba5dcc6802b0ec383fc56c6d81
Michael Matloka <dev@twixes.com>
2021-11-18 21:21:01 +0100

    Update spinner style

--- frontend/src/lib/components/Spinner/Spinner.tsx
+++ frontend/src/lib/components/Spinner/Spinner.tsx
@@ -2,12 +2,22 @@ import clsx from 'clsx'
 import React from 'react'
 import './Spinner.scss'
 
-interface SpinnerProps {
+export interface SpinnerProps {
     size?: 'sm' | 'md' | 'lg'
     type?: 'primary' | 'inverse'
     style?: React.CSSProperties
 }
 
 export function Spinner({ size = 'md', type = 'primary', style }: SpinnerProps): JSX.Element {
-    return <div className={clsx('loader-spinner', size, type)} style={style} />
+    return (
+        <svg
+            className={clsx('spinner', size, type)}
+            style={style}
+            viewBox="0 0 48 48"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <circle className="base" cx="24" cy="24" r="20" />
+            <circle className="overlay" cx="24" cy="24" r="20" />
+        </svg>
+    )
 }
