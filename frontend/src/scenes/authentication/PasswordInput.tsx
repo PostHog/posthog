89039734153df8b762e0b26c2c06b822fa9c1ac4
Yuvaraj J <jyuvaraj03@gmail.com>
2021-11-23 20:40:24 +0530

    Closes #6721 Adds password reveal in password input

--- frontend/src/scenes/authentication/PasswordInput.tsx
+++ frontend/src/scenes/authentication/PasswordInput.tsx
@@ -27,7 +27,7 @@ export const PasswordInput = React.forwardRef(function PasswordInputInternal(
         inputName = 'password',
         ...props
     }: PasswordInputProps,
-    ref: React.LegacyRef<Input>
+    ref: React.Ref<Input>
 ): JSX.Element {
     return (
         <div style={{ marginBottom: 24, ...style }} className="password-input">
@@ -84,7 +84,7 @@ export const PasswordInput = React.forwardRef(function PasswordInputInternal(
                 style={showStrengthIndicator ? { marginBottom: 0 } : undefined}
                 {...props}
             >
-                <Input
+                <Input.Password
                     ref={ref}
                     className="ph-ignore-input"
                     type="password"
