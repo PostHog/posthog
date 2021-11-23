0f430c1d16a4d0a9ec0fd5d215d24d44bd728e36
yakkomajuri <yakko.majuri@gmail.com>
2021-11-23 10:38:57 +0000

    fix

--- posthog/apps.py
+++ posthog/apps.py
@@ -38,7 +38,7 @@ class PostHogConfig(AppConfig):
             [in_range, version] = version_requirement.is_service_in_accepted_version()
             if not in_range:
                 start_anyway = input(
-                    f"Service {version_requirement.service} is in version {version}. Expected range: {str(version_requirement.version_range)}. PostHog may not work correctly with the current version. Continue? [y/n]"
+                    f"Service {version_requirement.service} is in version {version}. Expected range: {str(version_requirement.supported_version)}. PostHog may not work correctly with the current version. Continue? [y/n]"
                 )
                 if start_anyway.lower() != "y":
                     print(f"Unsupported version for service {version_requirement.service}, exiting...")
