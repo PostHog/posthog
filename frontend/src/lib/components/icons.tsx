128ee6609754210aa63dd4bb27cc0c186a1cd18a
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-11-22 22:48:15 -0800

    refactor inline message

--- frontend/src/lib/components/icons.tsx
+++ frontend/src/lib/components/icons.tsx
@@ -879,3 +879,15 @@ export function EventIcon(): JSX.Element {
         </svg>
     )
 }
+
+/** Material Design light bulb icon. */
+export function IconLightBulb(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+            <path
+                d="M12,2A7,7 0 0,0 5,9C5,11.38 6.19,13.47 8,14.74V17A1,1 0 0,0 9,18H15A1,1 0 0,0 16,17V14.74C17.81,13.47 19,11.38 19,9A7,7 0 0,0 12,2M9,21A1,1 0 0,0 10,22H14A1,1 0 0,0 15,21V20H9V21Z"
+                fill="currentColor"
+            />
+        </svg>
+    )
+}
