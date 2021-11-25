2638174909b5c5774a1a794488378f877c186235
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-11-25 11:02:32 -0800

    Fix recording close layout (#7367)

--- frontend/src/lib/components/icons.tsx
+++ frontend/src/lib/components/icons.tsx
@@ -891,3 +891,15 @@ export function EventIcon(): JSX.Element {
         </svg>
     )
 }
+
+/** Material Design Close icon. */
+export function IconClose(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+            <path
+                d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+                fill="currentColor"
+            />
+        </svg>
+    )
+}
