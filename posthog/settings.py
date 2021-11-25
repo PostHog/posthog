PostHog/posthog

commit 322e253413335a0a7d4e2e73466a0dcdd3e6482b

Add version requirements for deploying (#7283)
* add version requirements for deploying

* update postgres range

* postgres -> postgresql, version_range -> supported_version

* updates

* add test, revert ee app change

* fix

* fix

* rename to service version requirement

* add redis

* remove ee requirements from ee settings

* update redis v

* update postgres v

* Update posthog/version_requirement.py

Co-authored-by: Guido Iaquinti <4038041+guidoiaquinti@users.noreply.github.com>

* add SKIP_SERVICE_VERSION_REQUIREMENTS setting

* add warning for skipping reqs

* update redis version

* skip service req checks for collecstatic

* update settings

Co-authored-by: Guido Iaquinti <4038041+guidoiaquinti@users.noreply.github.com>

posthog/settings.py
@@ -29,6 +29,7 @@ from sentry_sdk.integrations.redis import RedisIntegration
 
 from posthog.constants import AnalyticsDBMS
 from posthog.utils import print_warning, str_to_bool
+from posthog.version_requirement import ServiceVersionRequirement
 
 
 def get_from_env(key: str, default: Any = None, *, optional: bool = False, type_cast: Optional[Callable] = None) -> Any:
@@ -502,8 +503,10 @@ else:
 
 # Broker
 
+IS_COLLECT_STATIC = len(sys.argv) > 1 and sys.argv[1] == "collectstatic"
+
 # The last case happens when someone upgrades Heroku but doesn't have Redis installed yet. Collectstatic gets called before we can provision Redis.
-if TEST or DEBUG or (len(sys.argv) > 1 and sys.argv[1] == "collectstatic"):
+if TEST or DEBUG or IS_COLLECT_STATIC:
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost/")
 else:
     REDIS_URL = os.getenv("REDIS_URL", "")
@@ -731,3 +734,20 @@ structlog.configure(
 
 # keep in sync with plugin-server
 EVENTS_DEAD_LETTER_QUEUE_STATSD_METRIC = "events_added_to_dead_letter_queue"
+
+SKIP_SERVICE_VERSION_REQUIREMENTS = get_from_env(
+    "SKIP_SERVICE_VERSION_REQUIREMENTS", TEST or IS_COLLECT_STATIC, type_cast=str_to_bool
+)
+
+if SKIP_SERVICE_VERSION_REQUIREMENTS:
+    print_warning("Skipping service version requirements. This is dangerous and PostHog might not work as expected!")
+
+SERVICE_VERSION_REQUIREMENTS = [
+    ServiceVersionRequirement(service="postgresql", supported_version=">=11.0.0,<=14.1.0",),
+    ServiceVersionRequirement(service="redis", supported_version=">=5.0.0,<=6.3.0",),
+]
+
+if PRIMARY_DB == AnalyticsDBMS.CLICKHOUSE:
+    SERVICE_VERSION_REQUIREMENTS = SERVICE_VERSION_REQUIREMENTS + [
+        ServiceVersionRequirement(service="clickhouse", supported_version=">=21.6.0,<21.7.0"),
+    ]

posthog/apps.py
@@ -32,3 +32,14 @@ class PostHogConfig(AppConfig):
             posthoganalytics.disabled = True
         elif settings.TEST or os.environ.get("OPT_OUT_CAPTURE", False):
             posthoganalytics.disabled = True
+
+        if not settings.SKIP_SERVICE_VERSION_REQUIREMENTS:
+            for service_version_requirement in settings.SERVICE_VERSION_REQUIREMENTS:
+                [in_range, version] = service_version_requirement.is_service_in_accepted_version()
+                if not in_range:
+                    start_anyway = input(
+                        f"Service {service_version_requirement.service} is in version {version}. Expected range: {str(service_version_requirement.supported_version)}. PostHog may not work correctly with the current version. Continue? [y/n]"
+                    )
+                    if start_anyway.lower() != "y":
+                        print(f"Unsupported version for service {service_version_requirement.service}, exiting...")
+                        exit(1)

posthog/test/test_version_requirement.py
@@ -1,0 +1,93 @@
+from unittest.mock import patch
+
+from django.test.testcases import TestCase
+from semantic_version.base import SimpleSpec, Version
+
+from posthog.version_requirement import ServiceVersionRequirement
+
+
+class TestServiceVersionRequirement(TestCase):
+    def test_accepted_services(self):
+        v1 = ServiceVersionRequirement(service="postgresql", supported_version="==14.0.0")
+        v2 = ServiceVersionRequirement(service="clickhouse", supported_version="==21.6.0")
+        v3 = ServiceVersionRequirement(service="redis", supported_version="==6.2.6")
+
+        self.assertEqual(v1.service, "postgresql")
+        self.assertEqual(v2.service, "clickhouse")
+        self.assertEqual(v3.service, "redis")
+
+        self.assertEqual(type(v1.supported_version), SimpleSpec)
+        self.assertEqual(type(v2.supported_version), SimpleSpec)
+        self.assertEqual(type(v3.supported_version), SimpleSpec)
+
+        self.assertEqual(str(v1.supported_version), "==14.0.0")
+        self.assertEqual(str(v2.supported_version), "==21.6.0")
+        self.assertEqual(str(v3.supported_version), "==6.2.6")
+
+        try:
+            ServiceVersionRequirement(service="kea", supported_version="==2.5.0")
+        except Exception as e:
+            self.assertEqual(
+                str(e),
+                "service kea cannot be used to specify a version requirement. service should be one of clickhouse, postgresql, redis",
+            )
+
+    def test_service_versions(self):
+
+        version1 = ServiceVersionRequirement.version_string_to_semver("14")
+        self.assertEqual(version1.major, 14)
+        self.assertEqual(version1.minor, 0)
+        self.assertEqual(version1.patch, 0)
+
+        version2 = ServiceVersionRequirement.version_string_to_semver("14.1")
+        self.assertEqual(version2.major, 14)
+        self.assertEqual(version2.minor, 1)
+        self.assertEqual(version2.patch, 0)
+
+        version3 = ServiceVersionRequirement.version_string_to_semver("14.1.2")
+        self.assertEqual(version3.major, 14)
+        self.assertEqual(version3.minor, 1)
+        self.assertEqual(version3.patch, 2)
+
+        version4 = ServiceVersionRequirement.version_string_to_semver("14.1.2.5")
+        self.assertEqual(version4.major, 14)
+        self.assertEqual(version4.minor, 1)
+        self.assertEqual(version4.patch, 2)
+
+        version5 = ServiceVersionRequirement.version_string_to_semver("15.0.0-alpha")
+        self.assertEqual(version5.major, 15)
+        self.assertEqual(version5.minor, 0)
+        self.assertEqual(version5.patch, 0)
+
+        version5 = ServiceVersionRequirement.version_string_to_semver("16.0.0.2-alpha")
+        self.assertEqual(version5.major, 16)
+        self.assertEqual(version5.minor, 0)
+        self.assertEqual(version5.patch, 0)
+
+        version6 = ServiceVersionRequirement.version_string_to_semver("11.13 (Ubuntu 11.13-2.heroku1+1)")
+        self.assertEqual(version6.major, 11)
+        self.assertEqual(version6.minor, 13)
+        self.assertEqual(version6.patch, 0)
+
+    @patch("posthog.version_requirement.ServiceVersionRequirement.get_service_version", lambda x: Version("12.1.2"))
+    def test_ranges(self):
+        v1 = ServiceVersionRequirement(service="postgresql", supported_version="==14.0.0")
+        in_range, service_version = v1.is_service_in_accepted_version()
+        self.assertEqual(in_range, False)
+        self.assertEqual(str(service_version), "12.1.2")
+
+        v2 = ServiceVersionRequirement(service="postgresql", supported_version="==12.1.2")
+        in_range, _ = v2.is_service_in_accepted_version()
+        self.assertEqual(in_range, True)
+
+        v3 = ServiceVersionRequirement(service="postgresql", supported_version=">=12.0.0,<12.1.2")
+        in_range, _ = v3.is_service_in_accepted_version()
+        self.assertEqual(in_range, False)
+
+        v4 = ServiceVersionRequirement(service="postgresql", supported_version=">=12.0.0,<=12.1.2")
+        in_range, _ = v4.is_service_in_accepted_version()
+        self.assertEqual(in_range, True)
+
+        v5 = ServiceVersionRequirement(service="postgresql", supported_version=">=11.0.0,<=13.0.0")
+        in_range, _ = v5.is_service_in_accepted_version()
+        self.assertEqual(in_range, True)

posthog/version_requirement.py
@@ -1,0 +1,79 @@
+from semantic_version.base import SimpleSpec, Version
+
+from posthog import redis
+
+
+class ServiceVersionRequirement:
+    accepted_services = ("clickhouse", "postgresql", "redis")
+
+    def __init__(self, service, supported_version):
+        if service not in self.accepted_services:
+            services_str = ", ".join(self.accepted_services)
+            raise Exception(
+                f"service {service} cannot be used to specify a version requirement. service should be one of {services_str}"
+            )
+
+        self.service = service
+
+        try:
+            self.supported_version = SimpleSpec(supported_version)
+        except:
+            raise Exception(
+                f"The provided supported_version for service {service} is invalid. See the Docs for SimpleSpec: https://pypi.org/project/semantic-version/"
+            )
+
+    def is_service_in_accepted_version(self):
+        service_version = self.get_service_version()
+        return [service_version in self.supported_version, service_version]
+
+    def get_service_version(self):
+        if self.service == "postgresql":
+            return self.get_postgres_version()
+
+        if self.service == "clickhouse":
+            return self.get_clickhouse_version()
+
+        if self.service == "redis":
+            return self.get_redis_version()
+
+    def get_postgres_version(self):
+        from django.db import connection
+
+        with connection.cursor() as cursor:
+            cursor.execute("SHOW server_version")
+
+            rows = cursor.fetchone()
+            version = rows[0]
+
+        return self.version_string_to_semver(version)
+
+    def get_clickhouse_version(self):
+        from ee.clickhouse.client import sync_execute
+
+        rows = sync_execute("SELECT version()")
+        version = rows[0][0]
+
+        return self.version_string_to_semver(version)
+
+    def get_redis_version(self):
+        client = redis.get_client()
+        version = client.execute_command("INFO")["redis_version"]
+        return self.version_string_to_semver(version)
+
+    @staticmethod
+    def version_string_to_semver(version):
+        minor = 0
+        patch = 0
+
+        # remove e.g. `-alpha`, Postgres metadata (`11.13 (Ubuntu 11.13-2.heroku1+1)`), etc
+        version_parts = version.split("(")[0].split("-")[0].split(".")
+
+        major = int(version_parts[0])
+
+        if len(version_parts) > 1:
+            minor = int(version_parts[1])
+
+        if len(version_parts) > 2:
+            patch = int(version_parts[2])
+
+        return Version(major=major, minor=minor, patch=patch)

production.Dockerfile
@@ -113,7 +113,7 @@ RUN yarn build && \
     rm -rf ./node_modules
 
 # Generate Django's static files
-RUN SECRET_KEY='unsafe secret key for collectstatic only' DATABASE_URL='postgres:///' REDIS_URL='redis:///' python manage.py collectstatic --noinput
+RUN SKIP_SERVICE_VERSION_REQUIREMENTS=1 SECRET_KEY='unsafe secret key for collectstatic only' DATABASE_URL='postgres:///' REDIS_URL='redis:///' python manage.py collectstatic --noinput
 
 # Add a dedicated 'posthog' user and group, move files into its home dir and set the
 # proper file permissions. This alleviates compliance issue for not running a

commit fe312baeda8c3f8535ace7470c643ec883876568

Silence warning in tests/debug mode
Issue was introduced in https://github.com/PostHog/posthog/pull/7283

This is causing /bin/tests, mypy and others to spam unneeded messages
every time these utilities are needed.

posthog/settings.py
@@ -738,7 +738,7 @@ SKIP_SERVICE_VERSION_REQUIREMENTS = get_from_env(
     "SKIP_SERVICE_VERSION_REQUIREMENTS", TEST or IS_COLLECT_STATIC, type_cast=str_to_bool
 )
 
-if SKIP_SERVICE_VERSION_REQUIREMENTS:
+if SKIP_SERVICE_VERSION_REQUIREMENTS and not (TEST or DEBUG):
     print_warning(["Skipping service version requirements. This is dangerous and PostHog might not work as expected!"])
 
 SERVICE_VERSION_REQUIREMENTS = [