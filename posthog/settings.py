1e330ac2495da5d70aa624b67a7246d3569f5d04
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-11-25 10:23:47 -0800

    keep cloud flags separate

--- posthog/settings.py
+++ posthog/settings.py
@@ -114,8 +114,9 @@ if E2E_TESTING:
 # The features here are released, but the flags are just not yet removed from the code.
 # To ignore this persisted feature flag behavior, set `PERSISTED_FEATURE_FLAGS = 0`
 env_feature_flags = os.getenv("PERSISTED_FEATURE_FLAGS", "")
-PERSISTED_FEATURE_FLAGS = [
-    # Add hard-coded feature flags for static releases here
+PERSISTED_FEATURE_FLAGS: List[str] = []
+default_flag_persistence = [
+    # Add hard-coded feature flags for static self-hosted releases here
     "3638-trailing-wau-mau",  # pending UI/UX improvements; functionality ready
     "5440-multivariate-support",
     "4141-event-columns",
@@ -124,7 +125,7 @@ PERSISTED_FEATURE_FLAGS = [
 ]
 
 if env_feature_flags != "0" and env_feature_flags.lower() != "false" and not DEBUG:
-    PERSISTED_FEATURE_FLAGS += get_list(env_feature_flags)
+    PERSISTED_FEATURE_FLAGS = default_flag_persistence + get_list(env_feature_flags)
 
 
 USE_PRECALCULATED_CH_COHORT_PEOPLE = not TEST
