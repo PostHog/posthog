0d1e3acda5b487d2f61c98988fc66bed5f98031b
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-11-25 10:37:16 -0800

    Fix recording close layout

--- frontend/src/scenes/session-recordings/SessionPlayerDrawer.tsx
+++ frontend/src/scenes/session-recordings/SessionPlayerDrawer.tsx
@@ -3,6 +3,7 @@ import { Drawer } from 'lib/components/Drawer'
 import { SessionRecordingPlayerV2 } from 'scenes/session-recordings/player/SessionRecordingPlayerV2'
 import { Button, Col, Row } from 'antd'
 import { ArrowLeftOutlined } from '@ant-design/icons'
+import { IconClose } from 'lib/components/icons'
 
 interface SessionPlayerDrawerProps {
     isPersonPage?: boolean
@@ -11,7 +12,14 @@ interface SessionPlayerDrawerProps {
 
 export function SessionPlayerDrawer({ isPersonPage = false, onClose }: SessionPlayerDrawerProps): JSX.Element {
     return (
-        <Drawer destroyOnClose visible width="100%" onClose={onClose} className="session-player-drawer-v2">
+        <Drawer
+            destroyOnClose
+            visible
+            width="100%"
+            onClose={onClose}
+            className="session-player-drawer-v2"
+            closable={false}
+        >
             <Col style={{ height: '100vh' }}>
                 <Row
                     style={{ height: 48, borderBottom: '1px solid var(--border)' }}
@@ -21,6 +29,13 @@ export function SessionPlayerDrawer({ isPersonPage = false, onClose }: SessionPl
                     <Button type="link" onClick={onClose}>
                         <ArrowLeftOutlined /> Back to {isPersonPage ? 'persons' : 'recordings'}
                     </Button>
+                    <div
+                        className="text-muted cursor-pointer flex-center"
+                        style={{ fontSize: '1.5em', paddingRight: 8 }}
+                        onClick={onClose}
+                    >
+                        <IconClose />
+                    </div>
                 </Row>
                 <Row className="session-drawer-body">
                     <SessionRecordingPlayerV2 />
