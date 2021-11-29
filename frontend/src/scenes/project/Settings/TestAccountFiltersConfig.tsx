065947877e139747b65eac352a3007166fba87a3
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-11-29 14:41:10 +0200

    Groups: Make test_account_filters work with groups (sessions, lifecycle support) (#7387)
    * Extract common subquery into a variable
    
    * BE: handle group properties in more cases
    
    * Add tests for lifecycle and sessions query changes
    
    * Better docs
    
    * Stable date range

--- frontend/src/scenes/project/Settings/TestAccountFiltersConfig.tsx
+++ frontend/src/scenes/project/Settings/TestAccountFiltersConfig.tsx
@@ -4,11 +4,14 @@ import { PropertyFilters } from 'lib/components/PropertyFilters'
 import { eventUsageLogic } from 'lib/utils/eventUsageLogic'
 import { teamLogic } from 'scenes/teamLogic'
 import { AnyPropertyFilter } from '~/types'
+import { TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
+import { groupsModel } from '~/models/groupsModel'
 
 export function TestAccountFiltersConfig(): JSX.Element {
     const { updateCurrentTeam } = useActions(teamLogic)
     const { reportTestAccountFiltersUpdated } = useActions(eventUsageLogic)
     const { currentTeam } = useValues(teamLogic)
+    const { groupsTaxonomicTypes } = useValues(groupsModel)
 
     const handleChange = (filters: AnyPropertyFilter[]): void => {
         updateCurrentTeam({ test_account_filters: filters })
@@ -23,6 +26,13 @@ export function TestAccountFiltersConfig(): JSX.Element {
                         pageKey="testaccountfilters"
                         propertyFilters={currentTeam?.test_account_filters}
                         onChange={handleChange}
+                        taxonomicGroupTypes={[
+                            TaxonomicFilterGroupType.EventProperties,
+                            TaxonomicFilterGroupType.PersonProperties,
+                            ...groupsTaxonomicTypes,
+                            TaxonomicFilterGroupType.Cohorts,
+                            TaxonomicFilterGroupType.Elements,
+                        ]}
                     />
                 )}
             </div>
