e481edcfd746668f165233f74f777c07d8a2fa4e
Paul D'Ambra <paul.dambra@gmail.com>
2021-11-29 15:29:14 +0000

    improved comment in vars files to call out manual duplication

--- frontend/src/vars.ts
+++ frontend/src/vars.ts
@@ -1,3 +1,7 @@
-// z-indexes from vars.scss
+/*
+values duplicated from ./vars.scss
+
+if change values here, check there too
+*/
 export const styles = { zDrawer: 950, zGraphAnnotationPrompt: 99 }
 export const colors = { $primary: '#5375ff' }
