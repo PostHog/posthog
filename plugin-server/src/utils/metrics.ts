9ce9a347d6efa8b86ab9f5c6b642cc3bd53872fe
yakkomajuri <yakko.majuri@gmail.com>
2021-11-30 10:23:49 -0300

    silence plugin server sentry logspam

--- plugin-server/src/utils/metrics.ts
+++ plugin-server/src/utils/metrics.ts
@@ -14,7 +14,6 @@ export async function instrumentQuery<T>(
     try {
         return await runQuery()
     } catch (error) {
-        Sentry.captureException(error, { extra: { query_tag: tag } })
         throw error
     } finally {
         statsd?.timing(metricName, timer, tags)
