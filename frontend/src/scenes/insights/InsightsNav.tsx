dce1b1c96ce0b977c9bc5edbdc3a352d36bb41fb
Michael Matloka <dev@twixes.com>
2021-11-30 18:43:21 +0100

    Fix data-attrs

--- frontend/src/scenes/insights/InsightsNav.tsx
+++ frontend/src/scenes/insights/InsightsNav.tsx
@@ -40,25 +40,19 @@ export function InsightsNav(): JSX.Element {
             >
                 <TabPane
                     tab={
-                        <Tooltip
-                            placement="top"
-                            title={INSIGHT_TYPES_METADATA[InsightType.TRENDS].description}
-                            data-attr="insight-trends-tab"
-                        >
-                            Trends
-                            <InsightHotkey hotkey="t" />
+                        <Tooltip placement="top" title={INSIGHT_TYPES_METADATA[InsightType.TRENDS].description}>
+                            <span data-attr="insight-trends-tab">
+                                Trends
+                                <InsightHotkey hotkey="t" />
+                            </span>
                         </Tooltip>
                     }
                     key={InsightType.TRENDS}
                 />
                 <TabPane
                     tab={
-                        <Tooltip
-                            placement="top"
-                            title={INSIGHT_TYPES_METADATA[InsightType.FUNNELS].description}
-                            data-attr="insight-funnels-tab"
-                        >
-                            <span ref={funnelTab}>
+                        <Tooltip placement="top" title={INSIGHT_TYPES_METADATA[InsightType.FUNNELS].description}>
+                            <span data-attr="insight-funnels-tab" ref={funnelTab}>
                                 Funnels
                                 <InsightHotkey hotkey="f" />
                             </span>
@@ -68,64 +62,55 @@ export function InsightsNav(): JSX.Element {
                 />
                 <TabPane
                     tab={
-                        <Tooltip
-                            placement="top"
-                            title={INSIGHT_TYPES_METADATA[InsightType.RETENTION].description}
-                            data-attr="insight-retention-tab"
-                        >
-                            Retention
-                            <InsightHotkey hotkey="r" />
+                        <Tooltip placement="top" title={INSIGHT_TYPES_METADATA[InsightType.RETENTION].description}>
+                            <span data-attr="insight-retention-tab">
+                                Retention
+                                <InsightHotkey hotkey="r" />
+                            </span>
                         </Tooltip>
                     }
                     key={InsightType.RETENTION}
                 />
                 <TabPane
                     tab={
-                        <Tooltip
-                            placement="top"
-                            title={INSIGHT_TYPES_METADATA[InsightType.PATHS].description}
-                            data-attr="insight-path-tab"
-                        >
-                            User Paths
-                            <InsightHotkey hotkey="p" />
+                        <Tooltip placement="top" title={INSIGHT_TYPES_METADATA[InsightType.PATHS].description}>
+                            <span data-attr="insight-paths-tab">
+                                User Paths
+                                <InsightHotkey hotkey="p" />
+                            </span>
                         </Tooltip>
                     }
                     key={InsightType.PATHS}
                 />
                 <TabPane
                     tab={
-                        <Tooltip
-                            placement="top"
-                            title={INSIGHT_TYPES_METADATA[InsightType.STICKINESS].description}
-                            data-attr="insight-stickiness-tab"
-                        >
-                            Stickiness
-                            <InsightHotkey hotkey="i" />
+                        <Tooltip placement="top" title={INSIGHT_TYPES_METADATA[InsightType.STICKINESS].description}>
+                            <span data-attr="insight-stickiness-tab">
+                                Stickiness
+                                <InsightHotkey hotkey="i" />
+                            </span>
                         </Tooltip>
                     }
                     key={InsightType.STICKINESS}
                 />
                 <TabPane
                     tab={
-                        <Tooltip
-                            placement="top"
-                            title={INSIGHT_TYPES_METADATA[InsightType.LIFECYCLE].description}
-                            data-attr="insight-lifecycle-tab"
-                        >
-                            Lifecycle
-                            <InsightHotkey hotkey="l" />
+                        <Tooltip placement="top" title={INSIGHT_TYPES_METADATA[InsightType.LIFECYCLE].description}>
+                            <span data-attr="insight-lifecycle-tab">
+                                Lifecycle
+                                <InsightHotkey hotkey="l" />
+                            </span>
                         </Tooltip>
                     }
                     key={InsightType.LIFECYCLE}
                 />
                 <TabPane
                     tab={
-                        <Tooltip
-                            placement="top"
-                            title={INSIGHT_TYPES_METADATA[InsightType.SESSIONS].description}
-                            data-attr="insight-sessions-tab"
-                        >
-                            <div className={clsx(featureFlags[FEATURE_FLAGS.SESSION_INSIGHT_REMOVAL] && 'deprecated')}>
+                        <Tooltip placement="top" title={INSIGHT_TYPES_METADATA[InsightType.SESSIONS].description}>
+                            <div
+                                className={clsx(featureFlags[FEATURE_FLAGS.SESSION_INSIGHT_REMOVAL] && 'deprecated')}
+                                data-attr="insight-sessions-tab"
+                            >
                                 Sessions
                                 <InsightHotkey hotkey="o" />
                             </div>
