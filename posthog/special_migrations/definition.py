b80501895a49174909b8a2aa992e07579fba1a1a
yakkomajuri <yakko.majuri@gmail.com>
2021-11-30 14:44:33 -0300

    add special migration definition and example

---
+++ posthog/special_migrations/definition.py
@@ -0,0 +1,31 @@
+from typing import Optional, Tuple
+
+from posthog.models.special_migration import SpecialMigration
+
+
+class SpecialMigrationOperation:
+    def __init__(self, sql="", database="clickhouse", timeout_seconds=60, rollback=None, resumable=False):
+        self.sql = sql
+        self.database = database
+        self.timeout_seconds = timeout_seconds
+        self.resumable = resumable
+
+
+class SpecialMigrationDefinition:
+    posthog_min_version = "0.0.0"
+    posthog_max_version = "10000.0.0"
+
+    service_version_requirements = []
+    operations = []
+
+    def is_required(self) -> bool:
+        return True
+
+    def healthcheck(self) -> Tuple[bool, Optional[str]]:
+        return (True, None)
+
+    def progress(self, migration_instance: SpecialMigration) -> int:
+        return int(100 * migration_instance.current_operation_index / len(self.operations))
+
+    def rollback(self, migration_instance: SpecialMigration) -> Tuple[bool, Optional[str]]:
+        return (False, None)
