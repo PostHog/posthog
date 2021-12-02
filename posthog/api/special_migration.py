2c27b828a0d774996562a2148acf42ba4d1655f5
yakkomajuri <yakko.majuri@gmail.com>
2021-12-02 16:04:38 -0300

    add api handling for out of range

--- posthog/api/special_migration.py
+++ posthog/api/special_migration.py
@@ -5,7 +5,12 @@ from posthog.api.routing import StructuredViewSetMixin
 from posthog.celery import app
 from posthog.models.special_migration import MigrationStatus, SpecialMigration, get_all_running_special_migrations
 from posthog.permissions import StaffUser
-from posthog.special_migrations.runner import force_rollback_migration, force_stop_migration, trigger_migration
+from posthog.special_migrations.runner import (
+    force_rollback_migration,
+    force_stop_migration,
+    is_migration_in_range,
+    trigger_migration,
+)
 
 # important to prevent us taking up too many celery workers
 # and running migrations sequentially
@@ -62,7 +67,18 @@ class SpecialMigrationsViewset(StructuredViewSetMixin, viewsets.ModelViewSet):
                 },
                 status=400,
             )
+
         migration_instance = self.get_object()
+
+        if not is_migration_in_range(migration_instance.posthog_min_version, migration_instance.posthog_max_version):
+            return response.Response(
+                {
+                    "success": False,
+                    "error": f"Can't run migration. Minimum PostHog version: {migration_instance.posthog_min_version}. Maximum PostHog version: {migration_instance.posthog_max_version}",
+                },
+                status=400,
+            )
+
         trigger_migration(migration_instance)
         return response.Response({"success": True}, status=200)
 
