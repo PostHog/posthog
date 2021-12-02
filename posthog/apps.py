3287c48f53d7826a34b0ac03af07bd5274e3218f
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-12-02 16:08:26 +0200

    Add logging to all postgresql queries with query context (#7486)
    * Add logging to all postgresql queries with query context
    
    Uses the exact same pattern as we do currently for clickhouse, just
    hooking in there differently
    
    * Support psycopg2.sql.SQL
    
    * Better docs
    
    * update a test

--- posthog/apps.py
+++ posthog/apps.py
@@ -3,6 +3,8 @@ import os
 import posthoganalytics
 from django.apps import AppConfig
 from django.conf import settings
+from django.db.backends.signals import connection_created
+from django.dispatch.dispatcher import receiver
 
 from posthog.special_migrations.manager import init_special_migrations
 from posthog.utils import get_git_branch, get_git_commit, get_machine_id
@@ -46,3 +48,10 @@ class PostHogConfig(AppConfig):
                         exit(1)
 
         init_special_migrations()
+
+
+@receiver(connection_created)
+def on_db_connection_ready(sender, connection, **kwargs):
+    from posthog.management.query_logging import execute_pg_query_with_logging
+
+    connection.execute_wrappers.append(execute_pg_query_with_logging)
