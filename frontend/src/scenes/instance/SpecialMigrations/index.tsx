40c63cec4a2d40326de88e97b56f6b4190bb2f38
yakkomajuri <yakko.majuri@gmail.com>
2021-12-03 12:03:37 -0300

    add refresh button

--- frontend/src/scenes/instance/SpecialMigrations/index.tsx
+++ frontend/src/scenes/instance/SpecialMigrations/index.tsx
@@ -3,7 +3,7 @@ import './index.scss'
 import React from 'react'
 import { PageHeader } from 'lib/components/PageHeader'
 import { SceneExport } from 'scenes/sceneTypes'
-import { Modal, Progress, Table } from 'antd'
+import { Button, Modal, Progress, Space, Table } from 'antd'
 import { useActions, useValues } from 'kea'
 import { SpecialMigration, migrationStatusNumberToMessage, specialMigrationsLogic } from './specialMigrationsLogic'
 import {
@@ -31,7 +31,7 @@ export const tooltipMessageForStatus = {
 
 export function SpecialMigrations(): JSX.Element {
     const { specialMigrations, specialMigrationsLoading } = useValues(specialMigrationsLogic)
-    const { triggerMigration, forceStopMigration } = useActions(specialMigrationsLogic)
+    const { triggerMigration, forceStopMigration, loadSpecialMigrations } = useActions(specialMigrationsLogic)
 
     const columns = [
         {
@@ -182,7 +182,15 @@ export function SpecialMigrations(): JSX.Element {
     return (
         <div className="special-migrations-scene">
             <PageHeader title="Special Migrations" caption="Manage special migrations in your instance" />
-
+            <div className="mb float-right">
+                <Button
+                    icon={specialMigrationsLoading ? <Spinner size="sm" /> : <RedoOutlined />}
+                    onClick={loadSpecialMigrations}
+                >
+                    Refresh
+                </Button>
+            </div>
+            <Space />
             <Table
                 pagination={{ pageSize: 99999, hideOnSinglePage: true }}
                 loading={specialMigrationsLoading}
