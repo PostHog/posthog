8100a1c66ea3bef139fbcfd878361d26aacdd704
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-12-03 15:59:06 +0200

    Finish initial empty state page

--- frontend/src/scenes/groups/GroupsIntroduction.tsx
+++ frontend/src/scenes/groups/GroupsIntroduction.tsx
@@ -1,38 +1,75 @@
+import { useValues } from 'kea'
+import { IconExternalLink } from 'lib/components/icons'
+import { LinkButton } from 'lib/components/LinkButton'
 import React from 'react'
+import { preflightLogic } from 'scenes/PreflightCheck/logic'
 import { GroupsAccessStatus } from '~/models/groupsModel'
+import './GroupsIntroduction.scss'
 
 interface Props {
     access: GroupsAccessStatus.NoAccess | GroupsAccessStatus.HasAccess | GroupsAccessStatus.HasGroupTypes
 }
 
 export function GroupsIntroduction({ access }: Props): JSX.Element {
-    let title, subtext, actionButton, learnMoreButton
+    let title, subtext, primaryButton, secondaryButton
+
+    const { preflight } = useValues(preflightLogic)
+    const isCloud = !!preflight?.cloud
+
+    const upgradeButton = (
+        <LinkButton
+            to={isCloud ? '/organization/billing' : '/instance/licenses'}
+            type="primary"
+            data-attr="group-analytics-upgrade"
+            className="groups-introduction__action-button"
+        >
+            Upgrade to get Group Analytics
+        </LinkButton>
+    )
+
+    const learnMoreButton = (
+        <LinkButton
+            type={access === GroupsAccessStatus.HasAccess ? 'primary' : undefined}
+            to="https://posthog.com/docs/user-guides/group-analytics?utm_medium=in-product&utm_campaign=group-analytics-introduction"
+            target="_blank"
+            data-attr="group-analytics-learn-more"
+            className="groups-introduction__action-button"
+        >
+            Learn how to track groups in PostHog <IconExternalLink style={{ marginLeft: 8 }} />
+        </LinkButton>
+    )
 
     if (access === GroupsAccessStatus.NoAccess) {
-        title = <>Introducing Group Analytics</>
+        title = (
+            <>
+                Introducing <span className="highlight">Group Analytics</span>!
+            </>
+        )
         subtext = (
             <>
                 Analyze how groups interact with your product as a whole instead of individual users (e.g. retention by
                 companies instead of by users)
             </>
         )
-        actionButton = <>Upgrade to get Group Analytics</>
-        learnMoreButton = <>Learn how to track groups in PostHog</>
+        primaryButton = upgradeButton
+        secondaryButton = learnMoreButton
     } else if (access === GroupsAccessStatus.HasAccess) {
         title = <>You're almost done!</>
         subtext = <>Learn how to track groups in your code</>
+        primaryButton = learnMoreButton
     } else {
+        // HasGroupTypes
         title = <>Looks like you're tracking groups!</>
-        subtext = <>Upgrade today to use groups in insights.</>
-        actionButton = <>Upgrade to get Group Analytics</>
+        subtext = <>Upgrade today to use groups in Insights.</>
+        primaryButton = upgradeButton
     }
 
     return (
         <div className="groups-introduction">
-            {title}
-            {subtext}
-            {actionButton}
-            {learnMoreButton}
+            <h2>{title}</h2>
+            <div className="groups-introduction__subtext">{subtext}</div>
+            {primaryButton}
+            {secondaryButton}
         </div>
     )
 }
