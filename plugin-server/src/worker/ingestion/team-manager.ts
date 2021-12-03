4cbc1cca56ab970ec0bc79a48515c5a89ae73625
Marius Andra <marius.andra@gmail.com>
2021-12-03 12:33:50 +0100

    perform all updates through a buffer object

--- plugin-server/src/worker/ingestion/team-manager.ts
+++ plugin-server/src/worker/ingestion/team-manager.ts
@@ -16,11 +16,26 @@ enum EventPropertyType {
     DateTime = 'DATETIME',
 }
 
+interface EventPropertiesCounter {
+    createdAt: number
+    lastSeenAt: number
+    totalVolume: number
+    propertyType: EventPropertyType | null
+    propertyTypeFormat: string | null
+}
+
+interface EventNamePropertiesBuffer {
+    totalVolume: number
+    uniqueVolume: number
+    buffer: Map<TeamId, Map<string, EventPropertiesCounter>>
+}
+
 export class TeamManager {
     db: DB
     teamCache: TeamCache<Team | null>
     eventDefinitionsCache: Map<TeamId, Set<string>>
     propertyDefinitionsCache: Map<TeamId, Set<string>>
+    eventPropertiesBuffer: EventNamePropertiesBuffer
     instanceSiteUrl: string
 
     constructor(db: DB, instanceSiteUrl?: string | null) {
@@ -28,6 +43,7 @@ export class TeamManager {
         this.teamCache = new Map()
         this.eventDefinitionsCache = new Map()
         this.propertyDefinitionsCache = new Map()
+        this.eventPropertiesBuffer = { totalVolume: 0, uniqueVolume: 0, buffer: new Map() }
         this.instanceSiteUrl = instanceSiteUrl || 'unknown'
     }
 
@@ -116,7 +132,8 @@ export class TeamManager {
             }
         }
 
-        await this.updateEventProperties(team, event, properties, eventTimestamp)
+        this.updateEventPropertiesBuffer(team, event, properties, eventTimestamp)
+        await this.flushEventPropertiesBuffer()
 
         clearTimeout(timeout)
     }
@@ -145,44 +162,93 @@ export class TeamManager {
         }
     }
 
-    public async updateEventProperties(
+    /** Save information about event properties into a custom buffer */
+    public updateEventPropertiesBuffer(
         team: Team,
         event: string,
         properties: Record<string, any>,
         eventTimestamp: DateTime
-    ): Promise<void> {
-        await this.db.postgresTransaction(async (client) => {
-            for (const [property, value] of Object.entries(properties)) {
-                let propertyType =
-                    typeof value === 'number'
-                        ? EventPropertyType.Number
-                        : typeof value === 'boolean'
-                        ? EventPropertyType.Boolean
-                        : typeof value === 'string'
-                        ? EventPropertyType.String
-                        : null
-                let propertyTypeFormat = null
-
-                if (propertyType === EventPropertyType.String) {
-                    const dateFormat = detectDateFormat(value)
-                    if (dateFormat) {
-                        propertyType = EventPropertyType.DateTime
-                        propertyTypeFormat = dateFormat
-                    }
+    ): void {
+        const timestamp = eventTimestamp.toSeconds()
+        let bufferForTeam = this.eventPropertiesBuffer.buffer.get(team.id)
+        if (!bufferForTeam) {
+            bufferForTeam = new Map()
+            this.eventPropertiesBuffer.buffer.set(team.id, bufferForTeam)
+        }
+        for (const [property, value] of Object.entries(properties)) {
+            const key = JSON.stringify([event, property])
+            let propertyBuffer = bufferForTeam.get(key)
+            if (!propertyBuffer) {
+                propertyBuffer = {
+                    createdAt: timestamp,
+                    lastSeenAt: timestamp,
+                    totalVolume: 1,
+                    propertyType: null,
+                    propertyTypeFormat: null,
                 }
+                bufferForTeam.set(key, propertyBuffer)
+                this.eventPropertiesBuffer.uniqueVolume += 1
+            } else {
+                propertyBuffer.createdAt = Math.min(timestamp, propertyBuffer.createdAt)
+                propertyBuffer.lastSeenAt = Math.max(timestamp, propertyBuffer.lastSeenAt)
+                propertyBuffer.totalVolume += 1
+            }
+            this.eventPropertiesBuffer.totalVolume += 1
 
-                const totalVolume = 1
+            propertyBuffer.propertyType =
+                typeof value === 'number'
+                    ? EventPropertyType.Number
+                    : typeof value === 'boolean'
+                    ? EventPropertyType.Boolean
+                    : typeof value === 'string'
+                    ? EventPropertyType.String
+                    : null
+            propertyBuffer.propertyTypeFormat = null
 
-                // starting with a naive implementation
-                await client.query(
-                    'INSERT INTO posthog_eventproperty (team_id, event, property, property_type, property_type_format, total_volume, created_at, last_seen_at) ' +
-                        'VALUES ($1, $2, $3, $4, $5, $6, NOW(), $7) ON CONFLICT ON CONSTRAINT posthog_eventproperty_team_id_event_property_10910b3b_uniq DO UPDATE SET ' +
-                        'total_volume = posthog_eventproperty.total_volume+$6, ' +
-                        'last_seen_at = GREATEST(posthog_eventproperty.last_seen_at, $7), ' +
-                        'property_type = CASE WHEN posthog_eventproperty.property_type IS NULL THEN $4 ELSE posthog_eventproperty.property_type END, ' +
-                        'property_type_format = CASE WHEN posthog_eventproperty.property_type_format IS NULL THEN $5 ELSE posthog_eventproperty.property_type_format END, ',
-                    [team.id, event, property, propertyType, propertyTypeFormat, totalVolume, eventTimestamp]
-                )
+            if (propertyBuffer.propertyType === EventPropertyType.String) {
+                const dateFormat = detectDateFormat(value)
+                if (dateFormat) {
+                    propertyBuffer.propertyType = EventPropertyType.DateTime
+                    propertyBuffer.propertyTypeFormat = dateFormat
+                }
+            }
+        }
+    }
+
+    public async flushEventPropertiesBuffer(): Promise<void> {
+        if (this.eventPropertiesBuffer.totalVolume === 0) {
+            return
+        }
+
+        await this.db.postgresTransaction(async (client) => {
+            const oldBuffer = this.eventPropertiesBuffer
+            this.eventPropertiesBuffer = { totalVolume: 0, uniqueVolume: 0, buffer: new Map() }
+
+            for (const [teamId, teamBuffer] of oldBuffer.buffer.entries()) {
+                for (const [key, propertyBuffer] of teamBuffer.entries()) {
+                    const [event, property] = JSON.parse(key)
+                    const { propertyType, propertyTypeFormat, totalVolume, lastSeenAt, createdAt } = propertyBuffer
+
+                    await client.query(
+                        'INSERT INTO posthog_eventproperty (team_id, event, property, property_type, property_type_format, total_volume, created_at, last_seen_at) ' +
+                            'VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT ON CONSTRAINT posthog_eventproperty_team_id_event_property_10910b3b_uniq DO UPDATE SET ' +
+                            'total_volume = posthog_eventproperty.total_volume+$6, ' +
+                            'created_at = LEAST(posthog_eventproperty.created_at, $7), ' +
+                            'last_seen_at = GREATEST(posthog_eventproperty.last_seen_at, $8), ' +
+                            'property_type = CASE WHEN posthog_eventproperty.property_type IS NULL THEN $4 ELSE posthog_eventproperty.property_type END, ' +
+                            'property_type_format = CASE WHEN posthog_eventproperty.property_type_format IS NULL THEN $5 ELSE posthog_eventproperty.property_type_format END',
+                        [
+                            teamId,
+                            event,
+                            property,
+                            propertyType,
+                            propertyTypeFormat,
+                            totalVolume,
+                            DateTime.fromSeconds(createdAt),
+                            DateTime.fromSeconds(lastSeenAt),
+                        ]
+                    )
+                }
             }
         })
     }
