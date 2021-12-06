a54eed43ccab7b2bf874948363c6e4d2913d2eb4
yakkomajuri <yakko.majuri@gmail.com>
2021-12-06 16:20:10 -0300

    no longer support postgres from 1.31.0 onwards

--- posthog/version.py
+++ posthog/version.py
@@ -1,1 +1,13 @@
-VERSION = "1.30.0"
+from posthog.constants import AnalyticsDBMS
+from posthog.settings import PRIMARY_DB
+from posthog.utils import print_warning
+
+VERSION = "1.31.0"
+
+if PRIMARY_DB == AnalyticsDBMS.POSTGRES:
+    print_warning(
+        [
+            "Postgres as the primary database is no longer supported from PostHog 1.31.0 onwwards. Learn how to migrate here: https://posthog.com/docs/self-host/migrate-from-postgres-to-clickhouse"
+        ]
+    )
+    exit(1)
