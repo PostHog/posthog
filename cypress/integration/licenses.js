9a6691658069b277fb7f897abb9d93e16a8fb0ad
Michael Matloka <dev@twixes.com>
2021-12-06 20:17:28 +0100

    Fix breadcrumbs and add some Cypress tests

--- cypress/integration/licenses.js
+++ cypress/integration/licenses.js
@@ -2,7 +2,8 @@ describe('Licenses', () => {
     it('Licenses loaded', () => {
         cy.get('[data-attr=top-menu-toggle]').click()
         cy.get('[data-attr=top-menu-item-licenses]').click()
+        cy.get('[data-attr=breadcrumb-1]').should('have.text', 'Licenses') // Breadcrumbs work
         cy.get('h1').should('contain', 'Licenses')
-        cy.title().should('equal', 'Licenses • PostHog')
+        cy.title().should('equal', 'Licenses • PostHog') // Page title works
     })
 })
