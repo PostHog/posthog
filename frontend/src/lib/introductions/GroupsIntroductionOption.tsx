d003737482ed7718c256aa06635b54d46165cfe6
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-12-06 10:43:46 +0200

    Update when to show groups dropdowns
    1. If showing upsell information
    2. Or it's enabled and we have groups

--- frontend/src/lib/introductions/GroupsIntroductionOption.tsx
+++ frontend/src/lib/introductions/GroupsIntroductionOption.tsx
@@ -40,7 +40,7 @@ export function GroupsIntroductionOption({ value }: { value: any }): JSX.Element
             >
                 <span>
                     <LockOutlined style={{ marginRight: 4, color: 'var(--warning)' }} />
-                    Unique groups.
+                    Unique groups
                     <Link
                         to="https://posthog.com/docs/user-guides/group-analytics?utm_medium=in-product&utm_campaign=group-analytics-learn-more"
                         target="_blank"
