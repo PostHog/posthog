e37aca6cdc7034d2dda63c41ebaf8488664b6fcf
yakkomajuri <yakko.majuri@gmail.com>
2021-12-06 16:15:27 -0300

    fix test

--- posthog/special_migrations/test/test_definition.py
+++ posthog/special_migrations/test/test_definition.py
@@ -1,7 +1,6 @@
 import pytest
 from infi.clickhouse_orm.utils import import_submodules
 
-from ee.clickhouse.sql.person import PERSONS_DISTINCT_ID_TABLE_MV_SQL
 from posthog.constants import AnalyticsDBMS
 from posthog.special_migrations.definition import SpecialMigrationDefinition, SpecialMigrationOperation
 from posthog.special_migrations.setup import SPECIAL_MIGRATIONS_EXAMPLE_MODULE_PATH
@@ -12,6 +11,8 @@ from posthog.version_requirement import ServiceVersionRequirement
 class TestSpecialMigrationDefinition(BaseTest):
     @pytest.mark.ee
     def test_get_special_migrations_without_staff_status(self):
+        from ee.clickhouse.sql.person import PERSONS_DISTINCT_ID_TABLE_MV_SQL
+
         modules = import_submodules(SPECIAL_MIGRATIONS_EXAMPLE_MODULE_PATH)
         example_migration = modules["example"].Migration()
 
