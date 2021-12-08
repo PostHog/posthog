3a4beda2ad168c7148a44a5340f7e5cf1ab2d9cb
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-12-08 15:59:41 +0200

    Speed up person_distinct_id queries for select clients (#7577)
    * Create a new way to get distinct id queries thats gated by team_id
    
    * Update most cases to use the new query
    
    * Convert EVENT_JOIN_PERSON_SQL to new query
    
    * Mostly convert GET_DISTINCT_IDS_BY_PERSON_ID_FILTER
    
    * Mostly convert GET_DISTINCT_IDS_BY_PROPERTY_SQL
    
    * Convert GET_PERSON_IDS_BY_FILTER
    
    * Flag benchmarks
    
    * Resolve circular imports
    
    * Update a snapshot test
    
    * Add a test for the new query logic

--- ee/clickhouse/queries/__init__.py
+++
@@ -1,7 +0,0 @@
-from ee.clickhouse.queries.funnels.funnel import ClickhouseFunnel
-
-from .clickhouse_retention import ClickhouseRetention
-from .paths import ClickhousePaths
-from .session_recordings.clickhouse_session_recording import SessionRecording
-from .stickiness.clickhouse_stickiness import ClickhouseStickiness
-from .trends.clickhouse_trends import ClickhouseTrends
