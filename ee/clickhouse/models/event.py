d24b55684ef18dc8d8fd17f43b549c7e19e826da
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-12-09 14:53:55 +0200

    Track organization groups usage

--- ee/clickhouse/models/event.py
+++ ee/clickhouse/models/event.py
@@ -219,6 +219,22 @@ def get_agg_event_count_for_teams_and_period(
     return result
 
 
+def get_agg_events_with_groups_count_for_teams_and_period(
+    team_ids: List[Union[str, int]], begin: timezone.datetime, end: timezone.datetime
+) -> int:
+    result = sync_execute(
+        """
+        SELECT count(1) as count
+        FROM events
+        WHERE team_id IN (%(team_id_clause)s)
+        AND timestamp between %(begin)s AND %(end)s
+        AND ($group_0 != '' OR $group_1 != '' OR $group_2 != '' OR $group_3 != '' OR $group_4 != '')
+    """,
+        {"team_id_clause": team_ids, "begin": begin, "end": end},
+    )[0][0]
+    return result
+
+
 def get_event_count_for_team(team_id: Union[str, int]) -> int:
     result = sync_execute(
         """
