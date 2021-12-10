02079337be84dc821f5657cb1b35c90a0b8a96b4
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-12-10 12:14:41 +0200

    Move base variables to base_variables.py

--- posthog/settings/__init__.py
+++ posthog/settings/__init__.py
@@ -28,6 +28,7 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 
 from posthog.constants import AnalyticsDBMS
+from posthog.settings.base_variables import *
 from posthog.settings.utils import get_from_env, get_list, print_warning, str_to_bool
 from posthog.version_requirement import ServiceVersionRequirement
 
@@ -73,21 +74,6 @@ if runner:
             # there was no path, we don't want to set PRIMARY_DB
             pass
 
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DEBUG = get_from_env("DEBUG", False, type_cast=str_to_bool)
-TEST = (
-    "test" in sys.argv or sys.argv[0].endswith("pytest") or get_from_env("TEST", False, type_cast=str_to_bool)
-)  # type: bool
-E2E_TESTING = get_from_env(
-    "E2E_TESTING", False, type_cast=str_to_bool,
-)  # whether the app is currently running for E2E tests
-if E2E_TESTING:
-    print_warning(
-        ["️WARNING! E2E_TESTING is set to `True`. This is a security vulnerability unless you are running tests."]
-    )
 
 # These flags will be force-enabled on the frontend **and OVERRIDE all** flags from `/decide`
 # The features here are released, but the flags are just not yet removed from the code.
---
+++ posthog/settings/base_variables.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+from posthog.settings.utils import get_from_env, print_warning, str_to_bool
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = get_from_env("DEBUG", False, type_cast=str_to_bool)
+TEST = (
+    "test" in sys.argv or sys.argv[0].endswith("pytest") or get_from_env("TEST", False, type_cast=str_to_bool)
+)  # type: bool
+E2E_TESTING = get_from_env(
+    "E2E_TESTING", False, type_cast=str_to_bool,
+)  # whether the app is currently running for E2E tests
+if E2E_TESTING:
+    print_warning(
+        ["️WARNING! E2E_TESTING is set to `True`. This is a security vulnerability unless you are running tests."]
+    )
