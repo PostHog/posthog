79d02059b9694af7f3dfecfb7593af7c7fd2ef8e
Harry Waye <harry@posthog.com>
2021-12-10 12:38:25 +0000

    ci(e2e): improve debug logging for github action workflow (#7629)
    * github action artifact cypress videos
    
    * artifact logs
    
    * Fix cypress video export
    
    * Redirect all output
    
    * wait for initial retention load
    
    * specify retry in ci
    
    * use bin/start instead of bin/docker-*
    
    * capture django app output in gunicorn log
    
    * use docker-worker and docker-server again
    
    * Remove gunicorn capture output settings

--- cypress/integration/retention.js
+++ cypress/integration/retention.js
@@ -7,13 +7,17 @@ describe('Retention', () => {
     })
 
     it('should apply filter and navigate to persons', () => {
+        // NOTE: First wait for results to load, try and make the test more
+        // stable. This is to try and avoid an issue where after selecting a
+        // filter property, the results section would be blank
+        cy.get('[data-attr=retention-table').should('exist')
         cy.get('[data-attr=new-prop-filter-insight-retention]').click()
         cy.get('[data-attr=taxonomic-filter-searchfield]').click()
         cy.get('[data-attr=taxonomic-filter-searchfield]').type('is_demo')
         cy.get('[data-attr=taxonomic-tab-person_properties]').click()
-        cy.get('[data-attr=prop-filter-person_properties-0]').click({ force: true })
+        cy.get('[data-attr=prop-filter-person_properties-0]').click()
         cy.get('[data-attr=prop-val]').click()
-        cy.get('[data-attr=prop-val-0]').click({ force: true })
+        cy.get('[data-attr=prop-val-0]').click()
         cy.get('[data-attr=retention-table').should('exist')
 
         cy.get('.percentage-cell').last().click()
