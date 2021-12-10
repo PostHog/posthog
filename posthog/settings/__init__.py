14b751ddf9666c7eec67825acdbee9037af01443
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-12-10 12:21:29 +0200

    WIP: Move service requirements

--- posthog/settings/__init__.py
+++ posthog/settings/__init__.py
@@ -30,8 +30,8 @@ from sentry_sdk.integrations.redis import RedisIntegration
 from posthog.constants import AnalyticsDBMS
 from posthog.settings.base_variables import *
 from posthog.settings.feature_flags import *
+from posthog.settings.service_requirements import *
 from posthog.settings.utils import get_from_env, get_list, print_warning, str_to_bool
-from posthog.version_requirement import ServiceVersionRequirement
 
 """
 There are several options:
@@ -685,20 +685,3 @@ EVENTS_DEAD_LETTER_QUEUE_STATSD_METRIC = "events_added_to_dead_letter_queue"
 
 # Teams with access to an experimental query optimization. Only ready to be used on cloud.
 PERSON_DISTINCT_ID_OPTIMIZATION_TEAM_IDS = get_list(os.getenv("PERSON_DISTINCT_ID_OPTIMIZATION_TEAM_IDS", ""))
-
-SKIP_SERVICE_VERSION_REQUIREMENTS = get_from_env(
-    "SKIP_SERVICE_VERSION_REQUIREMENTS", TEST or IS_COLLECT_STATIC, type_cast=str_to_bool
-)
-
-if SKIP_SERVICE_VERSION_REQUIREMENTS and not (TEST or DEBUG):
-    print_warning(["Skipping service version requirements. This is dangerous and PostHog might not work as expected!"])
-
-SERVICE_VERSION_REQUIREMENTS = [
-    ServiceVersionRequirement(service="postgresql", supported_version=">=11.0.0,<=14.1.0",),
-    ServiceVersionRequirement(service="redis", supported_version=">=5.0.0,<=6.3.0",),
-]
-
-if PRIMARY_DB == AnalyticsDBMS.CLICKHOUSE:
-    SERVICE_VERSION_REQUIREMENTS = SERVICE_VERSION_REQUIREMENTS + [
-        ServiceVersionRequirement(service="clickhouse", supported_version=">=21.6.0,<21.7.0"),
-    ]
