46477391e72f44960fbfacdd46c54a04df5aded1
Michael Matloka <dev@twixes.com>
2021-12-10 20:47:25 +0100

    Refresh property value display (#7640)
    * Fix boolean/null property value display and refresh property type
    
    * Use default cursor for type
    
    * Improve link style
    
    * Improve regex
    
    * Run prettier
    
    * Improve a couple of edge cases (null, empty array)

--- frontend/src/scenes/groups/GroupsIntroduction.tsx
+++ frontend/src/scenes/groups/GroupsIntroduction.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useValues } from 'kea'
-import { IconExternalLink } from 'lib/components/icons'
+import { IconOpenInNew } from 'lib/components/icons'
 import { groupsAccessLogic, GroupsAccessStatus } from 'lib/introductions/groupsAccessLogic'
 import './GroupsIntroduction.scss'
 import { LemonButton } from 'lib/components/LemonButton'
@@ -33,7 +33,7 @@ export function GroupsIntroduction({ access }: Props): JSX.Element {
             data-attr="group-analytics-learn-more"
             className="groups-introduction__action-button"
         >
-            Learn how to track groups in PostHog <IconExternalLink style={{ marginLeft: 8 }} />
+            Learn how to track groups in PostHog <IconOpenInNew style={{ marginLeft: 8 }} />
         </LemonButton>
     )
 
