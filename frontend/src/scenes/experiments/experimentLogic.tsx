1c12e14dfed2ab8dd71cf4177d2abbe6be5518b3
Li Yi Yu <li@posthog.com>
2021-12-12 13:17:57 -0500

    experiment draft mode fixes

--- frontend/src/scenes/experiments/experimentLogic.tsx
+++ frontend/src/scenes/experiments/experimentLogic.tsx
@@ -8,23 +8,28 @@ import { toast } from 'react-toastify'
 import { funnelLogic } from 'scenes/funnels/funnelLogic'
 import { cleanFilters } from 'scenes/insights/utils/cleanFilters'
 import { teamLogic } from 'scenes/teamLogic'
-import { Experiment, InsightType, InsightModel, FunnelVizType } from '~/types'
+import { Experiment, InsightType, InsightModel, FunnelVizType, FilterType } from '~/types'
 
 import { experimentLogicType } from './experimentLogicType'
 import { urls } from 'scenes/urls'
 import { router } from 'kea-router'
+import { experimentsLogic } from './experimentsLogic'
 
 export const experimentLogic = kea<experimentLogicType>({
     path: ['scenes', 'experiment', 'experimentLogic'],
     connect: { values: [teamLogic, ['currentTeamId']] },
     actions: {
         setExperiment: (experiment: Experiment) => ({ experiment }),
-        createExperiment: (draft?: boolean) => ({ draft }),
+        createExperiment: (draft: boolean) => ({ draft }),
         setExperimentFunnel: (funnel: InsightModel) => ({ funnel }),
         createNewExperimentFunnel: true,
-        setFilters: (filters) => ({ filters }),
+        setFilters: (filters: Partial<FilterType>) => ({ filters }),
         setExperimentId: (experimentId: number | 'new') => ({ experimentId }),
         setNewExperimentData: (experimentData: Partial<Experiment>) => ({ experimentData }),
+        nextPage: true,
+        prevPage: true,
+        setPage: (page: number) => ({ page }),
+        emptyData: true,
     },
     reducers: {
         experimentId: [
@@ -43,6 +48,7 @@ export const experimentLogic = kea<experimentLogicType>({
                     }
                     return { ...vals, ...experimentData }
                 },
+                emptyData: () => null,
             },
         ],
         experimentFunnel: [
@@ -51,14 +57,28 @@ export const experimentLogic = kea<experimentLogicType>({
                 setExperimentFunnel: (_, { funnel }) => funnel,
             },
         ],
+        newExperimentCurrentPage: [
+            0,
+            {
+                nextPage: (page) => page + 1,
+                prevPage: (page) => page - 1,
+                setPage: (_, { page }) => page,
+            },
+        ],
     },
     listeners: ({ values, actions }) => ({
         createExperiment: async ({ draft }) => {
             try {
-                await api.create(`api/projects/${values.currentTeamId}/experiments`, {
-                    ...values.newExperimentData,
-                    ...(!draft && { start_date: dayjs() }),
-                })
+                if (values.newExperimentData?.id) {
+                    await api.update(`api/projects/${values.currentTeamId}/experiments/${values.experimentId}`, {
+                        start_date: dayjs(),
+                    })
+                } else {
+                    await api.create(`api/projects/${values.currentTeamId}/experiments`, {
+                        ...values.newExperimentData,
+                        ...(!draft && { start_date: dayjs() }),
+                    })
+                }
             } catch (error) {
                 errorToast(
                     'Error creating your experiment',
@@ -73,10 +93,11 @@ export const experimentLogic = kea<experimentLogicType>({
             toast.success(
                 <div data-attr="success-toast">
                     <h1>Experimentation created successfully!</h1>
-                    <p>Click here to go back to the feature flag list.</p>
+                    <p>Click here to go back to the experiments list.</p>
                 </div>,
                 {
                     onClick: () => {
+                        experimentsLogic.actions.loadExperiments()
                         router.actions.push(urls.experiments())
                     },
                     closeOnClick: true,
@@ -99,6 +120,12 @@ export const experimentLogic = kea<experimentLogicType>({
         },
         setFilters: ({ filters }) => {
             funnelLogic.findMounted({ dashboardItemId: values.experimentFunnel?.short_id })?.actions.setFilters(filters)
+        },
+        loadExperimentSuccess: ({ experimentData }) => {
+            if (!experimentData?.start_date) {
+                actions.setPage(2)
+                actions.setNewExperimentData({ ...experimentData })
+            }
         },
     }),
     loaders: ({ values }) => ({
@@ -125,6 +152,8 @@ export const experimentLogic = kea<experimentLogicType>({
                 // like in featureFlagLogic.tsx
                 if (parsedId === 'new') {
                     actions.createNewExperimentFunnel()
+                    actions.emptyData()
+                    actions.setPage(0)
                 }
                 if (parsedId !== values.experimentId) {
                     actions.setExperimentId(parsedId)
