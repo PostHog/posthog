51a5e17b60efb0fb89b08c1ca206a26189268d0f
Li Yi Yu <li@posthog.com>
2021-12-13 18:03:42 -0500

    prettier

--- frontend/src/scenes/insights/InsightContainer.tsx
+++ frontend/src/scenes/insights/InsightContainer.tsx
@@ -190,8 +190,8 @@ export function InsightContainer({ disableTable }: { disableTable?: boolean } = 
                     {!!BlockingEmptyState ? (
                         BlockingEmptyState
                     ) : featureFlags[FEATURE_FLAGS.INSIGHT_LEGENDS] &&
-                        (activeView === InsightType.TRENDS || activeView === InsightType.STICKINESS) &&
-                        !filters.legend_hidden ? (
+                      (activeView === InsightType.TRENDS || activeView === InsightType.STICKINESS) &&
+                      !filters.legend_hidden ? (
                         <Row className="insights-graph-container-row" wrap={false}>
                             <Col className="insights-graph-container-row-left">{VIEW_MAP[activeView]}</Col>
                             <Col className="insights-graph-container-row-right">
