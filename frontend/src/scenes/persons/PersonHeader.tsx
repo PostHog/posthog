159d9726f1b6e3d08007b6b495b1a7416819c1dd
Michael Matloka <dev@twixes.com>
2021-12-13 15:25:41 +0100

    Only do minor fixes

--- frontend/src/scenes/persons/PersonHeader.tsx
+++ frontend/src/scenes/persons/PersonHeader.tsx
@@ -1,9 +1,9 @@
-import { PersonType } from '~/types'
+import { PersonActorType, PersonType } from '~/types'
 import React from 'react'
-import { IconPersonFilled } from 'lib/components/icons'
 import './PersonHeader.scss'
 import { Link } from 'lib/components/Link'
 import { urls } from 'scenes/urls'
+import { ProfilePicture } from 'lib/components/ProfilePicture'
 
 export interface PersonHeaderProps {
     person?: Partial<Pick<PersonType, 'properties' | 'distinct_ids'>> | null
@@ -11,7 +11,7 @@ export interface PersonHeaderProps {
     noLink?: boolean
 }
 
-export const asDisplay = (person: Partial<PersonType> | null | undefined): string => {
+export const asDisplay = (person: Partial<PersonType> | PersonActorType | null | undefined): string => {
     let displayId
     const propertyIdentifier = person?.properties
         ? person.properties.email || person.properties.name || person.properties.username
@@ -34,17 +34,32 @@ export const asLink = (person: Partial<PersonType> | null | undefined): string |
 
 export function PersonHeader(props: PersonHeaderProps): JSX.Element {
     const content = (
-        <div className="person-header">
-            {props.withIcon && <IconPersonFilled className="icon" />}
+        <div className="flex-center">
+            {props.withIcon && (
+                <ProfilePicture
+                    name={
+                        props.person?.properties?.email ||
+                        props.person?.properties?.name ||
+                        props.person?.properties?.username ||
+                        'U'
+                    }
+                    size="md"
+                    className="mr-05"
+                />
+            )}
             <span className="ph-no-capture text-ellipsis">{asDisplay(props.person)}</span>
         </div>
     )
 
-    return props.noLink ? (
-        content
-    ) : (
-        <Link to={asLink(props.person)} data-attr={`goto-person-email-${props.person?.distinct_ids?.[0]}`}>
-            {content}
-        </Link>
+    return (
+        <div className="person-header">
+            {props.noLink ? (
+                content
+            ) : (
+                <Link to={asLink(props.person)} data-attr={`goto-person-email-${props.person?.distinct_ids?.[0]}`}>
+                    {content}
+                </Link>
+            )}
+        </div>
     )
 }
