b56b1411262a90dc9e3c2d8116e0982a72286c71
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2021-12-13 16:00:27 +0000

    Special Migrations Runner (#7054 pt. 4) (#7446)
    * add special migration definition and example
    
    * types
    
    * special migrations runner
    
    * fix tests
    
    * fix tests 2
    
    * add clickhouse runner
    
    * add temp fix for tests
    
    * wip
    
    * add special migrations api (#7448)
    
    * wip new structure
    
    * update example sourcing
    
    * Update .gitignore
    
    * yet another wip structure
    
    * code quality
    
    * cypress
    
    * test docker image build
    
    * implement resumable ops
    
    * code quality
    
    * add comments
    
    * add warning
    
    * add conditional requirements for migration
    
    * add comment on is_required
    
    * add dependency map
    
    * wip dependencies and run migration on startup
    
    * code quality
    
    * fix bugs
    
    * fix more bugs
    
    * format
    
    * types
    
    * remove api from this branch
    
    * types
    
    * types
    
    * update clickhouse script
    
    * add is_migration_in_range util
    
    * fix type
    
    * fix runner
    
    * add AUTO_START_SPECIAL_MIGRATIONS env var
    
    * reset migration on start
    
    * cleanup
    
    * wip per op rollback
    
    * prevent accidental status rollback
    
    * add utils and definition test
    
    * update example with rollback per op
    
    * wip test special migration
    
    * add first runner tests
    
    * add runner tests
    
    * add util for code paths
    
    * fix test
    
    * fix types
    
    * fix types again
    
    * cleanup
    
    * cleanup
    
    * add periodic healthcheck task tests
    
    * remove unused imports
    
    * safer row updates
    
    * fix coalescing none checks
    
    * code quality
    
    * add docstrings
    
    * fix
    
    * fix deploys issue
    
    * update scripts
    
    * add delay
    
    * address reviews
    
    * address review comments
    
    * address review comments
    
    * address final comments
    
    * fix import error
    
    * fix tests
    
    * remove unused imports
    
    * fix tests
    
    * fix task test
    
    * remove unused return value
    
    * remove unused special migrations code from migrate_clickhouse
    
    * tweaks to support fresh deployments

--- posthog/apps.py
+++ posthog/apps.py
@@ -4,8 +4,8 @@ import posthoganalytics
 from django.apps import AppConfig
 from django.conf import settings
 
-from posthog.special_migrations.manager import init_special_migrations
-from posthog.utils import get_git_branch, get_git_commit, get_machine_id
+from posthog.settings import SKIP_SPECIAL_MIGRATIONS_SETUP
+from posthog.utils import get_git_branch, get_git_commit, get_machine_id, print_warning
 from posthog.version import VERSION
 
 
@@ -36,11 +36,16 @@ class PostHogConfig(AppConfig):
 
         if not settings.SKIP_SERVICE_VERSION_REQUIREMENTS:
             for service_version_requirement in settings.SERVICE_VERSION_REQUIREMENTS:
-                [in_range, version] = service_version_requirement.is_service_in_accepted_version()
+                in_range, version = service_version_requirement.is_service_in_accepted_version()
                 if not in_range:
                     print(
                         f"\033[91mService {service_version_requirement.service} is in version {version}. Expected range: {str(service_version_requirement.supported_version)}. PostHog may not work correctly with the current version. To continue anyway, add SKIP_SERVICE_VERSION_REQUIREMENTS=1 as an environment variable\033[0m",
                     )
                     exit(1)
 
-        init_special_migrations()
+        from posthog.special_migrations.setup import setup_special_migrations
+
+        if SKIP_SPECIAL_MIGRATIONS_SETUP:
+            print_warning(["Skipping special migrations setup. This is unsafe in production!"])
+        else:
+            setup_special_migrations()
--- posthog/special_migrations/examples/example.py
+++ posthog/special_migrations/examples/example.py
@@ -5,6 +5,7 @@ from ee.clickhouse.sql.person import (
     PERSONS_DISTINCT_ID_TABLE_MV_SQL,
     PERSONS_DISTINCT_ID_TABLE_SQL,
 )
+from posthog.constants import AnalyticsDBMS
 from posthog.settings import CLICKHOUSE_CLUSTER, CLICKHOUSE_DATABASE
 from posthog.special_migrations.definition import SpecialMigrationDefinition, SpecialMigrationOperation
 from posthog.version_requirement import ServiceVersionRequirement
@@ -16,6 +17,8 @@ TEMPORARY_TABLE_NAME = "person_distinct_id_special_migration"
 
 class Migration(SpecialMigrationDefinition):
 
+    description = "An example special migration."
+
     posthog_min_version = "1.29.0"
     posthog_max_version = "1.30.0"
 
@@ -23,27 +26,24 @@ class Migration(SpecialMigrationDefinition):
         ServiceVersionRequirement(service="clickhouse", supported_version=">=21.6.0,<21.7.0"),
     ]
 
-    # ideas:
-    # 1. support functions as operations instead of just sql?
-    #   1.1. receive the output of the previous op?
     operations = [
         SpecialMigrationOperation(
-            database="clickhouse",
+            database=AnalyticsDBMS.CLICKHOUSE,
             sql=PERSONS_DISTINCT_ID_TABLE_SQL().replace(PERSONS_DISTINCT_ID_TABLE, TEMPORARY_TABLE_NAME, 1),
-            rollback=f"DROP TABLE {TEMPORARY_TABLE_NAME} ON CLUSTER {CLICKHOUSE_CLUSTER}",
+            rollback=f"DROP TABLE IF EXISTS {TEMPORARY_TABLE_NAME} ON CLUSTER {CLICKHOUSE_CLUSTER}",
         ),
         SpecialMigrationOperation(
-            database="clickhouse",
+            database=AnalyticsDBMS.CLICKHOUSE,
             sql=f"DROP TABLE person_distinct_id_mv ON CLUSTER {CLICKHOUSE_CLUSTER}",
             rollback=PERSONS_DISTINCT_ID_TABLE_MV_SQL,
         ),
         SpecialMigrationOperation(
-            database="clickhouse",
+            database=AnalyticsDBMS.CLICKHOUSE,
             sql=f"DROP TABLE kafka_person_distinct_id ON CLUSTER {CLICKHOUSE_CLUSTER}",
             rollback=KAFKA_PERSONS_DISTINCT_ID_TABLE_SQL,
         ),
         SpecialMigrationOperation(
-            database="clickhouse",
+            database=AnalyticsDBMS.CLICKHOUSE,
             timeout_seconds=ONE_DAY,
             sql=f"""
                 INSERT INTO {TEMPORARY_TABLE_NAME} (distinct_id, person_id, team_id, _sign, _timestamp, _offset)
@@ -56,26 +56,40 @@ class Migration(SpecialMigrationDefinition):
                     _offset
                 FROM {PERSONS_DISTINCT_ID_TABLE}
             """,
-            rollback="",
+            rollback=f"DROP TABLE IF EXISTS {TEMPORARY_TABLE_NAME}",
         ),
         SpecialMigrationOperation(
-            database="clickhouse",
+            database=AnalyticsDBMS.CLICKHOUSE,
             sql=f"""
                 RENAME TABLE
-                    {CLICKHOUSE_DATABASE}.{PERSONS_DISTINCT_ID_TABLE} to {CLICKHOUSE_DATABASE}.person_distinct_id_backup,
+                    {CLICKHOUSE_DATABASE}.{PERSONS_DISTINCT_ID_TABLE} to {CLICKHOUSE_DATABASE}.person_distinct_id_special_migration_backup,
                     {CLICKHOUSE_DATABASE}.{TEMPORARY_TABLE_NAME} to {CLICKHOUSE_DATABASE}.{PERSONS_DISTINCT_ID_TABLE}
                 ON CLUSTER {CLICKHOUSE_CLUSTER}
             """,
+            rollback=f"""
+                RENAME TABLE
+                    {CLICKHOUSE_DATABASE}.{PERSONS_DISTINCT_ID_TABLE} to {CLICKHOUSE_DATABASE}.{TEMPORARY_TABLE_NAME}
+                    {CLICKHOUSE_DATABASE}.person_distinct_id_special_migration_backup to {CLICKHOUSE_DATABASE}.person_distinct_id,
+                ON CLUSTER {CLICKHOUSE_CLUSTER}
+            """,
         ),
-        SpecialMigrationOperation(database="clickhouse", sql=KAFKA_PERSONS_DISTINCT_ID_TABLE_SQL,),
-        SpecialMigrationOperation(database="clickhouse", sql=PERSONS_DISTINCT_ID_TABLE_MV_SQL,),
+        SpecialMigrationOperation(
+            database=AnalyticsDBMS.CLICKHOUSE,
+            sql=KAFKA_PERSONS_DISTINCT_ID_TABLE_SQL,
+            rollback=f"DROP TABLE IF EXISTS kafka_person_distinct_id ON CLUSTER {CLICKHOUSE_CLUSTER}",
+        ),
+        SpecialMigrationOperation(
+            database=AnalyticsDBMS.CLICKHOUSE,
+            sql=PERSONS_DISTINCT_ID_TABLE_MV_SQL,
+            rollback=f"DROP TABLE IF EXISTS person_distinct_id_mv ON CLUSTER {CLICKHOUSE_CLUSTER}",
+        ),
     ]
 
     def healthcheck(self):
         result = sync_execute("SELECT total_space, free_space FROM system.disks")
         total_space = result[0][0]
         free_space = result[0][1]
-        if free_space > total_space / 2:
+        if free_space > total_space / 3:
             return (True, None)
         else:
             return (False, "Upgrade your ClickHouse storage.")
@@ -89,24 +103,6 @@ class Migration(SpecialMigrationDefinition):
         progress = 100 * total_events_moved / total_events_to_move
         return progress
 
-    def rollback(self, migration_instance):
-        current_operation_index = migration_instance.current_operation_index
-        if current_operation_index > 4:
-            sync_execute(
-                f"""
-                RENAME TABLE
-                    {CLICKHOUSE_DATABASE}.{TEMPORARY_TABLE_NAME} to {CLICKHOUSE_DATABASE}.person_distinct_id_backup,
-                    {CLICKHOUSE_DATABASE}.{PERSONS_DISTINCT_ID_TABLE} to {CLICKHOUSE_DATABASE}.{TEMPORARY_TABLE_NAME}
-                ON CLUSTER {CLICKHOUSE_CLUSTER}
-            """
-            )
-
-        sync_execute(f"DROP TABLE IF EXISTS {TEMPORARY_TABLE_NAME} ON CLUSTER {CLICKHOUSE_CLUSTER}")
-
-        if current_operation_index < 5:
-            sync_execute(KAFKA_PERSONS_DISTINCT_ID_TABLE_SQL)
-
-        if current_operation_index < 6:
-            sync_execute(PERSONS_DISTINCT_ID_TABLE_MV_SQL)
-
-        return True
+    def is_required(self):
+        res = sync_execute("SHOW CREATE TABLE person_distinct_id")
+        return "ReplacingMergeTree" in res[0][0]
