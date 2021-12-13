5f4d2b3572add22c6f43592fe7edf88c732010a0
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-12-13 13:09:53 -0700

    Persisted flags for release 1.31.0 (#7651)

--- posthog/settings.py
+++ posthog/settings.py
@@ -119,6 +119,9 @@ default_flag_persistence = [
     # Add hard-coded feature flags for static self-hosted releases here
     "5440-multivariate-support",
     "new-paths-ui-edge-weights",
+    "new-sessions-player-events-list",
+    "session-insight-removal",
+    "funnel-simple-mode",
 ]
 
 if env_feature_flags != "0" and env_feature_flags.lower() != "false" and not DEBUG:
