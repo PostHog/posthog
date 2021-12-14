c881f1c2be135efa27e30583ee44be0f66acbd6f
Alex Kim <alexgkim205@gmail.com>
2021-12-14 11:23:21 -0800

    update tests

--- frontend/src/scenes/insights/InsightDateFilter/insightDateFilterLogic.test.ts
+++ frontend/src/scenes/insights/InsightDateFilter/insightDateFilterLogic.test.ts
@@ -1,43 +1,105 @@
 import { initKeaTestLogic } from '~/test/init'
-import { router } from 'kea-router'
 import { expectLogic } from 'kea-test-utils'
 import { insightDateFilterLogic } from 'scenes/insights/InsightDateFilter/insightDateFilterLogic'
-import { urls } from 'scenes/urls'
+import { insightLogic } from 'scenes/insights/insightLogic'
 import { InsightShortId } from '~/types'
+import { defaultAPIMocks, mockAPI } from 'lib/api.mock'
+
+jest.mock('lib/api')
 
 describe('the insightDateFilterLogic', () => {
     let logic: ReturnType<typeof insightDateFilterLogic.build>
+    const props = { dashboardItemId: 'test' as InsightShortId }
+
+    mockAPI(async (url) => {
+        return defaultAPIMocks(url)
+    })
 
     initKeaTestLogic({
         logic: insightDateFilterLogic,
-        props: {
-            dateFrom: '2021-12-11',
-            dateTo: '2021-12-13',
-        },
+        props,
         onLogic: (l) => (logic = l),
     })
 
-    it('defaults to prop dates', () => {
-        expectLogic(logic).toMatchValues({ dates: { dateFrom: '2021-12-11', dateTo: '2021-12-13' } })
+    describe('core assumptions', () => {
+        it('mounts all sorts of logics', async () => {
+            await expectLogic(logic).toMount([insightLogic(props)])
+        })
     })
 
-    it('reads "date from" and "date to" from URL when editing', () => {
-        const url = urls.insightEdit('12345' as InsightShortId, {
-            date_from: '2021-12-13',
-            date_to: '2021-12-14',
+    describe('date from and date to', () => {
+        it('use fallback date range when insight is set but filter is empty', async () => {
+            await insightLogic(props).actions.setInsight(
+                {
+                    result: [
+                        {
+                            filter: {
+                                date_from: '2020-01-12',
+                                date_to: '2020-03-12',
+                            },
+                        },
+                    ],
+                },
+                {}
+            )
+            await expectLogic()
+                .toMatchValues(insightLogic(props), {
+                    fallbackDateRange: expect.objectContaining({
+                        dateFrom: '2020-01-12',
+                        dateTo: '2020-03-12',
+                    }),
+                })
+                .toMatchValues(logic, {
+                    dates: {
+                        dateFrom: '2021-12-13',
+                        dateTo: '2021-12-14',
+                    },
+                })
         })
-        expectLogic(logic, () => {
-            router.actions.push(url)
-        }).toMatchValues({ dates: { dateFrom: '2021-12-13', dateTo: '2021-12-14' } })
-    })
 
-    it('reads "date from" and "date to" from URL when viewing', () => {
-        const url = urls.insightView('12345' as InsightShortId, {
-            date_from: '2021-12-14',
-            date_to: '2021-12-15',
+        initKeaTestLogic({
+            logic: insightDateFilterLogic,
+            props: {
+                ...props,
+                filters: {
+                    date_from: '2021-12-13',
+                    date_to: '2021-12-14',
+                },
+            },
+            onLogic: (l) => (logic = l),
         })
-        expectLogic(logic, () => {
-            router.actions.push(url)
-        }).toMatchValues({ dates: { dateFrom: '2021-12-14', dateTo: '2021-12-15' } })
+
+        it('syncs with insightLogic filter dates', () => {
+            expectLogic(logic).toMatchValues({ filters: { date_from: '2021-12-13', date_to: '2021-12-14' } })
+        })
+
+        it("don't fallback date range when insight and filter is set", async () => {
+            await insightLogic(props).actions.setInsight(
+                {
+                    result: [
+                        {
+                            filter: {
+                                date_from: '2020-01-12',
+                                date_to: '2020-03-12',
+                            },
+                        },
+                    ],
+                },
+                {}
+            )
+            await expectLogic()
+                .toMatchValues(insightLogic(props), {
+                    fallbackDateRange: expect.objectContaining({
+                        dateFrom: '2020-01-12',
+                        dateTo: '2020-03-12',
+                    }),
+                })
+                .toMatchValues(logic, {
+                    dates: {
+                        dateFrom: '2021-12-13',
+                        dateTo: '2021-12-14',
+                    },
+                })
+        })
     })
 })
