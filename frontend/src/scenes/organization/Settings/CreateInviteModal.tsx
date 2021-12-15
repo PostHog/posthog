7293a77aced7cd4dd12a9eda54d2065b0c6b3a2b
Prithvi Tharun <ptrithu8@gmail.com>
2021-12-15 22:31:14 +0530

    Closes #7733 Updates button name

--- frontend/src/scenes/organization/Settings/CreateInviteModal.tsx
+++ frontend/src/scenes/organization/Settings/CreateInviteModal.tsx
@@ -56,7 +56,7 @@ export function CreateInviteModalWithButton(buttonProps: ButtonProps): JSX.Eleme
                 icon={<UserAddOutlined />}
                 {...buttonProps}
             >
-                Invite team member
+                Invite Team Member
             </Button>
             <Modal
                 title={`Inviting Team Member${user?.organization ? ' to ' + user?.organization?.name : ''}`}
