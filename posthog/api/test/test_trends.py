9d17abb566675682703d6d5f420295bdefc9740e
eric <eeoneric@gmail.com>
2021-12-14 20:01:10 -0700

    misname

--- posthog/api/test/test_trends.py
+++ posthog/api/test/test_trends.py
@@ -196,7 +196,7 @@ def get_trends_aggregate_ok(client: Client, request: TrendsRequest, team: Team) 
         res[item["label"]] = NormalizedTrendResult(
             value=item["aggregated_value"],
             label=item["action"]["name"],
-            actor_url=item["persons"]["url"],
+            actor_url=item["actors"]["url"],
             breakdown_value=item.get("breakdown_value", None),
         )
 
