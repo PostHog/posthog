5e6e3796654d422ed1b5f3544dc8efab626bea28
Harry Waye <harry@posthog.com>
2021-12-17 15:40:18 +0000

    chore(lifecycle): add comments and CTEs to clickhouse sql
    It was really big and confusing, but hopefully this clarifies a little
    what is going on. As a followup PR I'll be doing some work to make the
    query faster :fingerscrossed: but I think worth at least getting this
    in, assuming I haven't broken any tests!

--- ee/clickhouse/sql/trends/lifecycle.py
+++ ee/clickhouse/sql/trends/lifecycle.py
@@ -1,59 +1,161 @@
 _LIFECYCLE_EVENTS_QUERY = """
-SELECT *, if(base_day = toDateTime('0000-00-00 00:00:00'), 'dormant', if(subsequent_day = base_day + INTERVAL {interval}, 'returning', if(subsequent_day > earliest + INTERVAL {interval}, 'resurrecting', 'new'))) as status
-FROM (
-    SELECT person_id, base_day, min(subsequent_day) as subsequent_day FROM (
-        SELECT person_id, day as base_day, events.subsequent_day as subsequent_day  FROM (
-            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events
-            JOIN
-            ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi on events.distinct_id = pdi.distinct_id
-            WHERE team_id = %(team_id)s AND {event_query} {filters}
-            GROUP BY person_id, day HAVING day <= toDateTime(%(date_to)s) AND day >= toDateTime(%(prev_date_from)s)
-        ) base
-        JOIN (
-            SELECT DISTINCT person_id, {trunc_func}(events.timestamp) subsequent_day FROM events
-            JOIN
-            ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi on events.distinct_id = pdi.distinct_id
-            WHERE team_id = %(team_id)s AND {event_query} {filters}
-            GROUP BY person_id, subsequent_day HAVING subsequent_day <= toDateTime(%(date_to)s) AND subsequent_day >= toDateTime(%(prev_date_from)s)
-        ) events ON base.person_id = events.person_id
-        WHERE subsequent_day > base_day
-    )
-    GROUP BY person_id, base_day
-    UNION ALL
-    SELECT person_id, min(day) as base_day, min(day) as subsequent_day  FROM (
-        SELECT DISTINCT person_id, {trunc_func}(events.timestamp) day FROM events
-        JOIN
-        ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi on events.distinct_id = pdi.distinct_id
-        WHERE team_id = %(team_id)s AND {event_query} {filters}
-        GROUP BY person_id, day HAVING day <= toDateTime(%(date_to)s) AND day >= toDateTime(%(prev_date_from)s)
-    ) base
-    GROUP BY person_id
-    UNION ALL
-    SELECT person_id, base_day, subsequent_day FROM (
-        SELECT person_id, total as base_day, day_start as subsequent_day FROM (
-            SELECT DISTINCT person_id, groupArray({trunc_func}(events.timestamp)) day FROM events
-            JOIN
-            ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi on events.distinct_id = pdi.distinct_id
-            WHERE team_id = %(team_id)s AND {event_query} {filters}
-            AND toDateTime(events.timestamp) <= toDateTime(%(date_to)s) AND {trunc_func}(events.timestamp) >= toDateTime(%(date_from)s)
-            GROUP BY person_id
-        ) as e
-        CROSS JOIN (
-            SELECT toDateTime('0000-00-00 00:00:00') AS total, {trunc_func}(toDateTime(%(date_to)s) - number * %(seconds_in_interval)s) as day_start from numbers(%(num_intervals)s)
-        ) as b WHERE has(day, subsequent_day) = 0
-        ORDER BY person_id, subsequent_day ASC
-        ) WHERE
-        ((empty(toString(neighbor(person_id, -1))) OR neighbor(person_id, -1) != person_id) AND subsequent_day != {trunc_func}(toDateTime(%(date_from)s) + INTERVAL {interval} - INTERVAL {sub_interval}))
-        OR
-        ( (neighbor(person_id, -1) = person_id) AND neighbor(subsequent_day, -1) < subsequent_day - INTERVAL {interval})
-) e
-JOIN (
-    SELECT DISTINCT person_id, {trunc_func}(min(events.timestamp)) earliest FROM events
-    JOIN
-    ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi on events.distinct_id = pdi.distinct_id
+WITH person_activity_including_previous_period AS (
+    SELECT DISTINCT 
+        person_id, 
+        {trunc_func}(events.timestamp) day 
+
+    FROM events
+        JOIN ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi 
+            ON events.distinct_id = pdi.distinct_id
+
     WHERE team_id = %(team_id)s AND {event_query} {filters}
+
+    GROUP BY 
+        person_id, 
+        day
+        
+    HAVING 
+        day <= toDateTime(%(date_to)s) 
+        AND day >= toDateTime(%(prev_date_from)s)
+
+), person_activity_as_array AS (
+    SELECT DISTINCT 
+        person_id, 
+        groupArray({trunc_func}(events.timestamp)) day 
+
+    FROM events
+        JOIN ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi 
+            ON events.distinct_id = pdi.distinct_id
+
+    WHERE 
+        team_id = %(team_id)s 
+        AND {event_query} {filters}
+        AND toDateTime(events.timestamp) <= toDateTime(%(date_to)s) 
+        AND {trunc_func}(events.timestamp) >= toDateTime(%(date_from)s)
+        
     GROUP BY person_id
-) earliest ON e.person_id = earliest.person_id
+), period_starts AS (
+    SELECT 
+        {trunc_func}(toDateTime(%(date_to)s) - number * %(seconds_in_interval)s) AS day_start 
+        
+    FROM numbers(%(num_intervals)s)
+)
+
+SELECT 
+    activity_pairs.person_id AS person_id,
+    activity_pairs.base_day AS base_day,
+    activity_pairs.subsequent_day AS subsequent_day, 
+    if(
+        base_day = toDateTime('0000-00-00 00:00:00'), 
+        'dormant', 
+        if(
+            subsequent_day = base_day + INTERVAL {interval}, 
+            'returning', 
+            if(
+                subsequent_day > earliest + INTERVAL {interval}, 
+                'resurrecting', 
+                'new'
+            )
+        )
+    ) as status
+
+FROM (
+    /*
+         Get person period activity paired with the next adjacent period activity
+    */
+    SELECT 
+        person_id, 
+        base_day, 
+        min(subsequent_day) as subsequent_day 
+
+    FROM (
+        SELECT 
+            person_id, 
+            base.day as base_day, 
+            subsequent.day as subsequent_day
+
+        FROM person_activity_including_previous_period base
+            JOIN person_activity_including_previous_period subsequent 
+                ON base.person_id = subsequent.person_id
+
+        WHERE subsequent.day > base.day
+    )
+
+    GROUP BY 
+        person_id, 
+        base_day
+
+    UNION ALL
+
+    /* 
+        Get the first active period for each user within the extended range 
+        i.e. including the previous period
+        
+        NOTE: base_day and subsequent_day are the same
+    */ 
+    SELECT
+        base.person_id, 
+        min(base.day) as base_day, 
+        min(base.day) as subsequent_day 
+        
+    FROM person_activity_including_previous_period base
+    GROUP BY person_id
+
+    UNION ALL
+
+    /*
+        Get activity status rows for all dormant periods and all persons
+    */
+    SELECT 
+        person_id, 
+        base_day, 
+        subsequent_day 
+
+    FROM (
+        SELECT 
+            person_activity.person_id AS person_id, 
+
+            -- Use datetime null value as marker that this refers to dormant
+            toDateTime('0000-00-00 00:00:00') as base_day, 
+            period_starts.day_start as subsequent_day
+
+        FROM person_activity_as_array as person_activity
+            CROSS JOIN period_starts
+
+        WHERE has(person_activity.day, period_starts.day_start) = 0
+
+        ORDER BY 
+            person_id, 
+            period_starts.day_start ASC
+    ) 
+    
+    WHERE
+        -- exclude first period ofr dormant
+        ( 
+            (empty(toString(neighbor(person_id, -1))) 
+            OR neighbor(person_id, -1) != person_id
+        ) 
+        AND subsequent_day != {trunc_func}(toDateTime(%(date_from)s) + INTERVAL {interval} - INTERVAL {sub_interval}))
+        OR (
+            (neighbor(person_id, -1) = person_id) 
+            AND neighbor(subsequent_day, -1) < subsequent_day - INTERVAL {interval}
+        )
+) activity_pairs
+
+    -- Get the earliest event for each person
+    JOIN (
+        SELECT DISTINCT 
+            person_id, 
+            {trunc_func}(min(events.timestamp)) earliest 
+        FROM events
+
+            JOIN ({GET_TEAM_PERSON_DISTINCT_IDS}) pdi 
+                ON events.distinct_id = pdi.distinct_id
+
+        WHERE team_id = %(team_id)s AND {event_query} {filters}
+        GROUP BY person_id
+    ) earliest ON activity_pairs.person_id = earliest.person_id
+
 """
 
 LIFECYCLE_SQL = f"""
