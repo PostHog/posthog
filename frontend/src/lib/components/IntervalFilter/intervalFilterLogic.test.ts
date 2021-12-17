ef05820f0031fa171998c1b6ba50bb0ea1e5e269
Harry Waye <harry@posthog.com>
2021-12-17 12:00:07 +0000

    fix tests

--- frontend/src/lib/components/IntervalFilter/intervalFilterLogic.test.ts
+++ frontend/src/lib/components/IntervalFilter/intervalFilterLogic.test.ts
@@ -3,29 +3,39 @@ import { router } from 'kea-router'
 import { expectLogic } from 'kea-test-utils'
 import { intervalFilterLogic } from 'lib/components/IntervalFilter/intervalFilterLogic'
 import { urls } from 'scenes/urls'
-import { InsightShortId } from '~/types'
+import { InsightShortId, InsightType } from '~/types'
 
 describe('the intervalFilterLogic', () => {
     let logic: ReturnType<typeof intervalFilterLogic.build>
 
     initKeaTestLogic({
         logic: intervalFilterLogic,
-        props: {},
+        props: {
+            dashboardItemId: undefined,
+            syncWithUrl: true,
+            filters: {
+                insight: InsightType.TRENDS,
+            },
+        },
         onLogic: (l) => (logic = l),
     })
 
-    it('defaults to null', () => {
-        expectLogic(logic).toMatchValues({ dateFrom: null, interval: null })
+    it('sets a default for interval, and excludes date_from', () => {
+        const url = urls.insightEdit('12345' as InsightShortId, {})
+
+        expectLogic(logic, () => {
+            router.actions.push(url)
+        }).toMatchValues({ filters: expect.objectContaining({ interval: 'day' }) })
     })
 
     it('reads "date from" and "interval" from URL when editing', () => {
         const url = urls.insightEdit('12345' as InsightShortId, {
-            date_from: '-90d',
+            date_from: '-30d',
             interval: 'hour',
         })
         expectLogic(logic, () => {
             router.actions.push(url)
-        }).toMatchValues({ dateFrom: '-90d', interval: 'hour' })
+        }).toMatchValues({ filters: expect.objectContaining({ date_from: '-30d', interval: 'hour' }) })
     })
 
     it('reads "date from" and "interval" from URL when viewing', () => {
@@ -33,8 +43,9 @@ describe('the intervalFilterLogic', () => {
             date_from: '-14d',
             interval: 'week',
         })
+
         expectLogic(logic, () => {
             router.actions.push(url)
-        }).toMatchValues({ dateFrom: '-14d', interval: 'week' })
+        }).toMatchValues({ filters: expect.objectContaining({ date_from: '-14d', interval: 'week' }) })
     })
 })
