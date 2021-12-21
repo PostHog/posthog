be12ab1633494337aab71cac66dac7c1f838d5d1
Paul D'Ambra <paul.dambra@gmail.com>
2021-12-21 16:58:57 +0000

    fix merge error

--- posthog/api/property_definition.py
+++ posthog/api/property_definition.py
@@ -37,7 +37,7 @@ class PropertyDefinitionSerializer(serializers.ModelSerializer):
             "is_numerical",
             "query_usage_30_day",
             "property_type",
-            "property_type_format"
+            "property_type_format",
             # This is a calculated property, used only when "event_names" is passed to the API.
             "is_event_property",
         )
