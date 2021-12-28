c55e31c5f320aabec92c1977825d913357cc668e
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-12-28 12:54:28 +0200

    Remove `minute` interval support from frontend

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -993,20 +993,6 @@ export function midEllipsis(input: string, maxLength: number): string {
     return `${input.substring(0, middle - excess)}...${input.substring(middle + excess)}`
 }
 
-export const disableMinuteFor: Record<string, boolean> = {
-    dStart: false,
-    '-1d': false,
-    '-7d': true,
-    '-14d': true,
-    '-30d': true,
-    '-90d': true,
-    mStart: true,
-    '-1mStart': true,
-    yStart: true,
-    all: true,
-    other: false,
-}
-
 export const disableHourFor: Record<string, boolean> = {
     dStart: false,
     '-1d': false,
@@ -1026,7 +1012,8 @@ export function autocorrectInterval(filters: Partial<FilterType>): IntervalType 
         return 'day'
     } // undefined/uninitialized
 
-    const minute_disabled = disableMinuteFor[filters.date_from || 'other'] && filters.interval === 'minute'
+    // @ts-expect-error - Old legacy interval support
+    const minute_disabled = filters.interval === 'minute'
     const hour_disabled = disableHourFor[filters.date_from || 'other'] && filters.interval === 'hour'
 
     if (minute_disabled) {
