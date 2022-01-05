a05daa36a26e22279a817dd1db587f29a09b2646
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-01-05 06:06:27 -0600

    Don't make project-scoped requests if teamId is not properly set (#7879)
    * use connected value
    
    * validate project URL
    
    * Update code style
    
    * Add tests
    
    Co-authored-by: Michael Matloka <dev@twixes.com>

--- frontend/src/lib/api.test.ts
+++ frontend/src/lib/api.test.ts
@@ -1,15 +1,17 @@
 import api from 'lib/api'
 
 describe('API helper', () => {
+    let fakeFetch: jest.Mock<any, any>
+
+    const FAKE_FETCH_RESULT = ['fake API result']
+
+    beforeEach(() => {
+        fakeFetch = jest.fn()
+        fakeFetch.mockResolvedValue({ ok: true, json: () => Promise.resolve(FAKE_FETCH_RESULT) })
+        window.fetch = fakeFetch
+    })
+
     describe('getting URLs', () => {
-        let fakeFetch: jest.Mock<any, any>
-
-        beforeEach(() => {
-            fakeFetch = jest.fn()
-            fakeFetch.mockReturnValue({ ok: true, json: () => Promise.resolve('["fake api"]') })
-            window.fetch = fakeFetch
-        })
-
         const testCases = [
             {
                 url: 'relative/url',
@@ -41,11 +43,38 @@ describe('API helper', () => {
 
         verbs.forEach((verb) => {
             testCases.forEach((testCase) => {
-                it(`when API is using verb ${verb} it normalises ${testCase.url} to ${testCase.expected}`, () => {
+                it(`when API is using verb ${verb} it normalizes ${testCase.url} to ${testCase.expected}`, () => {
                     api[verb](testCase.url)
                     expect(fakeFetch.mock.calls[0][0]).toEqual(testCase.expected)
                 })
             })
+        })
+    })
+
+    it('rejects project-based requests with void project ID', async () => {
+        await expect(api.get('/api/projects/2/')).resolves.not.toThrow()
+        await expect(api.get('/api/projects/089908')).resolves.not.toThrow()
+        await expect(api.get('/api/projects/089908?x')).resolves.not.toThrow()
+        await expect(api.get('/api/projects/xyz/dings/')).resolves.not.toThrow()
+        await expect(api.get('/api/projects/null/')).rejects.toStrictEqual({
+            detail: 'Cannot make request - project ID is unknown.',
+            status: 0,
+        })
+        await expect(api.get('/api/projects/null')).rejects.toStrictEqual({
+            detail: 'Cannot make request - project ID is unknown.',
+            status: 0,
+        })
+        await expect(api.get('/api/projects/null?x')).rejects.toStrictEqual({
+            detail: 'Cannot make request - project ID is unknown.',
+            status: 0,
+        })
+        await expect(api.get('/api/projects/null#x')).rejects.toStrictEqual({
+            detail: 'Cannot make request - project ID is unknown.',
+            status: 0,
+        })
+        await expect(api.get('/api/projects/null/dings')).rejects.toStrictEqual({
+            detail: 'Cannot make request - project ID is unknown.',
+            status: 0,
         })
     })
 })
