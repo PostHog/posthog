23caa9a036597895df11ee33dd8100ac5389f51b
Marius Andra <marius.andra@gmail.com>
2022-01-11 10:03:58 +0100

    Migrate insight filters away from urls (#7837)
    * migrate insight date filter away from urls
    
    * slight cleanup
    
    * refactor FunnelsCue
    
    * add missing keys
    
    * clean up compareFilter
    
    * clean up chartFilter
    
    * do not blank the old filteres
    
    * address feedback

--- frontend/src/scenes/insights/InsightDateFilter/InsightDateFilter.tsx
+++ frontend/src/scenes/insights/InsightDateFilter/InsightDateFilter.tsx
@@ -2,26 +2,24 @@ import React from 'react'
 import { useValues, useActions } from 'kea'
 import { insightDateFilterLogic } from './insightDateFilterLogic'
 import { DateFilterProps, DateFilter } from 'lib/components/DateFilter/DateFilter'
-import './index.scss'
+import { insightLogic } from 'scenes/insights/insightLogic'
 
 export function InsightDateFilter(props: DateFilterProps): JSX.Element {
+    const { insightProps } = useValues(insightLogic)
     const {
         dates: { dateFrom, dateTo },
-        highlightDateChange,
-    } = useValues(insightDateFilterLogic)
-    const { setDates } = useActions(insightDateFilterLogic)
+    } = useValues(insightDateFilterLogic(insightProps))
+    const { setDates } = useActions(insightDateFilterLogic(insightProps))
 
     return (
-        <span className={highlightDateChange ? 'insights-date-filter highlighted' : ''}>
-            <DateFilter
-                {...props}
-                dateFrom={dateFrom}
-                dateTo={dateTo}
-                onChange={(changedDateFrom, changedDateTo) => {
-                    setDates(changedDateFrom, changedDateTo)
-                    props.onChange?.(changedDateFrom, changedDateTo)
-                }}
-            />
-        </span>
+        <DateFilter
+            {...props}
+            dateFrom={dateFrom ?? undefined}
+            dateTo={dateTo ?? undefined}
+            onChange={(changedDateFrom, changedDateTo) => {
+                setDates(changedDateFrom, changedDateTo)
+                props.onChange?.(changedDateFrom, changedDateTo)
+            }}
+        />
     )
 }
