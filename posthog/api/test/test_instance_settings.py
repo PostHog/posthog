25ab15b700a06a2609e32837d649a467aa7db852
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-01-12 09:44:07 -0600

    extra permissions tests

--- posthog/api/test/test_instance_settings.py
+++ posthog/api/test/test_instance_settings.py
@@ -8,9 +8,9 @@ from posthog.test.base import APIBaseTest
 
 class TestInstanceSettings(APIBaseTest):
     def setUp(self):
+        super().setUp()
         self.user.is_staff = True
         self.user.save()
-        return super().setUp()
 
     def test_list_instance_settings(self):
 
@@ -50,6 +50,22 @@ class TestInstanceSettings(APIBaseTest):
         self.assertEqual(json_response["value_type"], "bool")
         self.assertEqual(json_response["editable"], True)
 
+    def test_non_staff_user_cant_list_or_retrieve(self):
+        self.user.is_staff = False
+        self.user.save()
+
+        response = self.client.get(f"/api/instance_settings/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            response.json(), self.permission_denied_response("You are not a staff user, contact your instance admin.")
+        )
+
+        response = self.client.get(f"/api/instance_settings/AUTO_START_ASYNC_MIGRATIONS")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            response.json(), self.permission_denied_response("You are not a staff user, contact your instance admin.")
+        )
+
     def test_update_setting(self):
         response = self.client.get(f"/api/instance_settings/AUTO_START_ASYNC_MIGRATIONS")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -83,3 +99,16 @@ class TestInstanceSettings(APIBaseTest):
             },
         )
         self.assertEqual(getattr(config, "MATERIALIZED_COLUMNS_ENABLED"), True)
+
+    def test_non_staff_user_cant_update(self):
+        self.user.is_staff = False
+        self.user.save()
+
+        response = self.client.get(f"/api/instance_settings/AUTO_START_ASYNC_MIGRATIONS", {"value": True})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            response.json(), self.permission_denied_response("You are not a staff user, contact your instance admin.")
+        )
+
+        self.assertEqual(get_instance_setting("AUTO_START_ASYNC_MIGRATIONS").value, False)
+        self.assertEqual(getattr(config, "AUTO_START_ASYNC_MIGRATIONS"), False)
