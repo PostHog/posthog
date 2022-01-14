39171aa49a2010f3094f8d765c124c20a7380ff2
Rick Marron <rcmarron@gmail.com>
2022-01-13 17:39:30 -0800

    fix recording test errors

--- frontend/src/scenes/session-recordings/player/metaLogic.test.ts
+++ frontend/src/scenes/session-recordings/player/metaLogic.test.ts
@@ -1,17 +1,26 @@
 import { expectLogic } from 'kea-test-utils'
-import { mockAPI } from 'lib/api.mock'
+import { mockAPI, MOCK_TEAM_ID } from 'lib/api.mock'
 import { initKeaTestLogic } from '~/test/init'
 import { sessionRecordingLogic } from 'scenes/session-recordings/sessionRecordingLogic'
 import { sessionRecordingPlayerLogic } from 'scenes/session-recordings/player/sessionRecordingPlayerLogic'
 import { metaLogic } from 'scenes/session-recordings/player/metaLogic'
-import { SessionPlayerData } from '~/types'
+import recordingMetaJson from '../__mocks__/recording_meta.json'
+import recordingEventsJson from '../__mocks__/recording_events.json'
 
 jest.mock('lib/api')
+const EVENTS_SESSION_RECORDING_META_ENDPOINT = `api/projects/${MOCK_TEAM_ID}/session_recordings`
+const EVENTS_SESSION_RECORDING_EVENTS_ENDPOINT = `api/projects/${MOCK_TEAM_ID}/events`
 
 describe('metaLogic', () => {
     let logic: ReturnType<typeof metaLogic.build>
 
-    mockAPI()
+    mockAPI(async ({ pathname }) => {
+        if (pathname.startsWith(EVENTS_SESSION_RECORDING_META_ENDPOINT)) {
+            return { result: recordingMetaJson }
+        } else if (pathname.startsWith(EVENTS_SESSION_RECORDING_EVENTS_ENDPOINT)) {
+            return { results: recordingEventsJson }
+        }
+    })
 
     initKeaTestLogic({
         logic: metaLogic,
@@ -33,7 +42,7 @@ describe('metaLogic', () => {
         it('stops loading after meta load is successful', async () => {
             await expectLogic(logic).toMatchValues({ loading: true })
             await expectLogic(logic, () => {
-                logic.actions.loadRecordingMetaSuccess({} as SessionPlayerData)
+                logic.connections['scenes.session-recordings.sessionRecordingLogic'].actions.loadRecordingMeta('1')
             })
                 .toDispatchActions(['loadRecordingMetaSuccess'])
                 .toMatchValues({ loading: false })
