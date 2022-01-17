394e7757a7e344c8c03dfa6ab2048cd7fadb3ef1
Alex Gyujin Kim <alexgkim205@gmail.com>
2022-01-17 12:44:19 -0800

    Add FF for collaborations taxonomy (#8102)

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -90,6 +90,7 @@ export const FEATURE_FLAGS = {
     PATHS_ADVANCED_EXPERIMENT: 'paths-advanced-2101', // owner: @paolodamico; `control`, `direct` (A), `no-advanced` (B)
     APM: 'hackathon-apm', //owner @pauldambra
     NEW_INSIGHT_COHORTS: '7569-insight-cohorts',
+    COLLABORATIONS_TAXONOMY: 'collaborations-taxonomy', // owner @alexkim205
 }
 
 export const ENTITY_MATCH_TYPE = 'entities'
