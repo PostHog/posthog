92f588ee14bf235e6be362993c458e1cc71ae8cd
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-01-17 10:23:10 -0600

    initial email config

--- posthog/settings/dynamic_settings.py
+++ posthog/settings/dynamic_settings.py
@@ -30,10 +30,32 @@ CONSTANCE_CONFIG = {
         "Used to disable automatic rollback of failed async migrations",
         bool,
     ),
+    "EMAIL_ENABLED": (get_from_env("EMAIL_ENABLED", True, type_cast=str_to_bool), "", bool,),
+    "EMAIL_HOST": (get_from_env("EMAIL_HOST"), "", str,),
+    "EMAIL_PORT": (get_from_env("EMAIL_PORT", 25, type_cast=int), "", int,),
+    "EMAIL_HOST_USER": (get_from_env("EMAIL_HOST_USER"), "", str,),
+    "EMAIL_HOST_PASSWORD": (get_from_env("EMAIL_HOST_PASSWORD"), "", str,),
+    "EMAIL_USE_TLS": (get_from_env("EMAIL_USE_TLS", False, type_cast=str_to_bool), "", bool,),
+    "EMAIL_USE_SSL": (get_from_env("EMAIL_USE_SSL", False, type_cast=str_to_bool), "", bool,),
+    "DEFAULT_FROM_EMAIL": (
+        get_from_env("EMAIL_DEFAULT_FROM", get_from_env("DEFAULT_FROM_EMAIL", "root@localhost")),
+        "",
+        str,
+    ),
+    "EMAIL_REPLY_TO": (get_from_env("EMAIL_REPLY_TO"), "", str,),
 }
 
 SETTINGS_ALLOWING_API_OVERRIDE = (
     "AUTO_START_ASYNC_MIGRATIONS",
     "ASYNC_MIGRATIONS_ROLLBACK_TIMEOUT",
     "ASYNC_MIGRATIONS_DISABLE_AUTO_ROLLBACK",
+    "EMAIL_ENABLED",
+    "EMAIL_HOST",
+    "EMAIL_PORT",
+    "EMAIL_HOST_USER",
+    "EMAIL_HOST_PASSWORD",
+    "EMAIL_USE_TLS",
+    "EMAIL_USE_SSL",
+    "DEFAULT_FROM_EMAIL",
+    "EMAIL_REPLY_TO",
 )
--- posthog/settings/emails.py
+++ posthog/settings/emails.py
@@ -1,17 +1,26 @@
-import os
+from constance.signals import config_updated
+from django.dispatch.dispatcher import receiver
 
 from posthog.settings.utils import get_from_env, str_to_bool
 
-# Email
-EMAIL_ENABLED = get_from_env("EMAIL_ENABLED", True, type_cast=str_to_bool)
-EMAIL_HOST = os.getenv("EMAIL_HOST", None)
-EMAIL_PORT = os.getenv("EMAIL_PORT", "25")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = get_from_env("EMAIL_USE_TLS", False, type_cast=str_to_bool)
-EMAIL_USE_SSL = get_from_env("EMAIL_USE_SSL", False, type_cast=str_to_bool)
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL_DEFAULT_FROM", os.getenv("DEFAULT_FROM_EMAIL", "root@localhost"))
-EMAIL_REPLY_TO = os.getenv("EMAIL_REPLY_TO", None)
+EMAIL_SETTINGS = [
+    "EMAIL_ENABLED",
+    "EMAIL_HOST",
+    "EMAIL_PORT",
+    "EMAIL_HOST_USER",
+    "EMAIL_HOST_PASSWORD",
+    "EMAIL_USE_TLS",
+    "EMAIL_USE_SSL",
+    "EMAIL_DEFAULT_FROM",
+    "EMAIL_REPLY_TO",
+]
+
+
+@receiver(config_updated)
+def constance_updated(sender, key, old_value, new_value, **kwargs):
+    if key in EMAIL_SETTINGS:
+        globals()[key] = new_value
+
 
 # TODO: Temporary
 EMAIL_REPORTS_ENABLED: bool = get_from_env("EMAIL_REPORTS_ENABLED", False, type_cast=str_to_bool)
