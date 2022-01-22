09188274ba3192169d0bbb5e3b1400beb53204a2
Rick Marron <rcmarron@gmail.com>
2022-01-22 12:02:27 -0800

    add utils test

---
+++ ee/clickhouse/queries/funnels/test/test_utils.py
@@ -0,0 +1,23 @@
+from ee.clickhouse.queries.funnels import ClickhouseFunnel, ClickhouseFunnelStrict, ClickhouseFunnelUnordered
+from ee.clickhouse.queries.funnels.utils import get_funnel_order_class
+from posthog.constants import FunnelOrderType
+from posthog.models.filters import Filter
+from posthog.test.base import BaseTest
+
+
+class TestGetFunnelOrderClass(BaseTest):
+    def test_filter_missing_order(self):
+        filter = Filter({"foo": "bar"})
+        self.assertEqual(get_funnel_order_class(filter), ClickhouseFunnel)
+
+    def test_unordered(self):
+        filter = Filter({"funnel_order_type": FunnelOrderType.UNORDERED})
+        self.assertEqual(get_funnel_order_class(filter), ClickhouseFunnelUnordered)
+
+    def test_strict(self):
+        filter = Filter({"funnel_order_type": FunnelOrderType.STRICT})
+        self.assertEqual(get_funnel_order_class(filter), ClickhouseFunnelStrict)
+
+    def test_ordered(self):
+        filter = Filter({"funnel_order_type": FunnelOrderType.ORDERED})
+        self.assertEqual(get_funnel_order_class(filter), ClickhouseFunnel)
