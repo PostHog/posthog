c9623eeda37fa65b94877c49d35888343ffed095
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-01-24 09:39:46 +0000

    updated persisted flags

--- posthog/settings/feature_flags.py
+++ posthog/settings/feature_flags.py
@@ -12,7 +12,9 @@ PERSISTED_FEATURE_FLAGS: List[str] = []
 default_flag_persistence = [
     # Add hard-coded feature flags for static self-hosted releases here
     "new-paths-ui-edge-weights",
-    "new-sessions-player-events-list",
+    "5730-funnel-horizontal-ui",
+    "938-breakdown-by-multiple-properties",
+    "insight-legends",
 ]
 
 if env_feature_flags != "0" and env_feature_flags.lower() != "false" and not DEBUG and not E2E_TESTING:
