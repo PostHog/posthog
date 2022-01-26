17e04ad24937d9a84c031bb72d706dfff83486aa
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-01-26 08:05:46 +0000

    Remove @alexkim205 flags (#8265)

--- posthog/settings/feature_flags.py
+++ posthog/settings/feature_flags.py
@@ -12,8 +12,6 @@ PERSISTED_FEATURE_FLAGS: List[str] = []
 default_flag_persistence = [
     # Add hard-coded feature flags for static self-hosted releases here
     "new-paths-ui-edge-weights",
-    "5730-funnel-horizontal-ui",
-    "insight-legends",
 ]
 
 if env_feature_flags != "0" and env_feature_flags.lower() != "false" and not DEBUG and not E2E_TESTING:
