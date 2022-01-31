bf9af4a10fac5277585d9ce7c39ec37386cdd396
yakkomajuri <yakko.majuri@gmail.com>
2022-01-31 11:40:57 -0300

    run tests

--- posthog/async_migrations/test/test_migrations_not_required.py
+++ posthog/async_migrations/test/test_migrations_not_required.py
@@ -1,7 +1,9 @@
 import pytest
 from infi.clickhouse_orm.utils import import_submodules
 
+from ee.clickhouse.client import sync_execute
 from posthog.async_migrations.setup import ASYNC_MIGRATIONS_MODULE_PATH
+from posthog.settings.data_stores import CLICKHOUSE_DATABASE
 from posthog.test.base import BaseTest
 
 
@@ -12,6 +14,32 @@ from posthog.test.base import BaseTest
 class TestAsyncMigrationsNotRequired(BaseTest):
     def test_async_migrations_not_required_on_fresh_instances(self):
         modules = import_submodules(ASYNC_MIGRATIONS_MODULE_PATH)
+
+        test1 = sync_execute(
+            """
+            SELECT comment
+            FROM system.columns
+            WHERE database = %(database)s AND table = 'person_distinct_id' AND name = 'distinct_id'
+        """,
+            {"database": CLICKHOUSE_DATABASE},
+        )
+
+        test2 = sync_execute(
+            """
+            SELECT database, table, name, comment
+            FROM system.columns
+            WHERE table = 'person_distinct_id'
+        """,
+            {"database": CLICKHOUSE_DATABASE},
+        )
+
+        print("########")
+
+        print(CLICKHOUSE_DATABASE)
+        print(test1)
+        print(test2)
+
+        print("########")
 
         for module in modules.values():
             migration = module.Migration()
