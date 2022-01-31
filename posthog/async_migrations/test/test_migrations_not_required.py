8908bde6a6c6193cb1a68317f78c4157eb2e6c7b
yakkomajuri <yakko.majuri@gmail.com>
2022-01-31 15:18:56 -0300

    remove ee tag

--- posthog/async_migrations/test/test_migrations_not_required.py
+++ posthog/async_migrations/test/test_migrations_not_required.py
@@ -1,7 +1,6 @@
 import pytest
 from infi.clickhouse_orm.utils import import_submodules
 
-from ee.conftest import run_additional_setup_ops
 from posthog.async_migrations.setup import ASYNC_MIGRATIONS_MODULE_PATH
 from posthog.test.base import BaseTest
 
@@ -9,11 +8,8 @@ from posthog.test.base import BaseTest
 # Async migrations are data migrations aimed at getting users from an old schema to a new schema
 # Fresh installs should have the new schema, however. So check that async migrations are being
 # written correctly such that this is the case
-@pytest.mark.ee
 class TestAsyncMigrationsNotRequired(BaseTest):
     def test_async_migrations_not_required_on_fresh_instances(self):
-
-        run_additional_setup_ops()
 
         modules = import_submodules(ASYNC_MIGRATIONS_MODULE_PATH)
 
