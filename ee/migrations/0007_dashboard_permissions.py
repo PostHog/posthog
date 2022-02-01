3fa27bc79cf90b40d0560fd0c16c4755237b6d66
Michael Matloka <dev@twixes.com>
2022-02-01 20:49:02 +0100

    Update migrations

--- ee/migrations/0007_dashboard_permissions.py
+++ ee/migrations/0007_dashboard_permissions.py
@@ -10,7 +10,7 @@ import posthog.models.utils
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("posthog", "0201_dashboard_permissions"),
+        ("posthog", "0202_dashboard_permissions"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("ee", "0006_event_definition_verification"),
     ]
