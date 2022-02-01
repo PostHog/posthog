91f13d6c88d1ef60b289be64a88f18c2449503f0
yakkomajuri <yakko.majuri@gmail.com>
2022-02-01 18:22:19 -0300

    fix test

--- ee/clickhouse/migrations/0023_dead_letter_queue_tags.py
+++ ee/clickhouse/migrations/0023_dead_letter_queue_tags.py
@@ -9,6 +9,6 @@ operations = [
     migrations.RunSQL(
         f"ALTER TABLE events_dead_letter_queue ON CLUSTER {CLICKHOUSE_CLUSTER} ADD COLUMN IF NOT EXISTS tags Array(VARCHAR)"
     ),
-    migrations.RunSQL(KAFKA_DEAD_LETTER_QUEUE_TABLE_SQL),
+    migrations.RunSQL(KAFKA_DEAD_LETTER_QUEUE_TABLE_SQL()),
     migrations.RunSQL(DEAD_LETTER_QUEUE_TABLE_MV_SQL),
 ]
