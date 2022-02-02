ddb3b9f1984638abd6307bb82419692b796fca7b
James Greenhill <fuziontech@gmail.com>
2022-02-01 17:17:17 -0800

    really leveraging that CI today

--- posthog/test/test_static_urls.py
+++ posthog/test/test_static_urls.py
@@ -3,7 +3,7 @@ from posthog.test.base import APIBaseTest
 
 class TestStaticUrls(APIBaseTest):
     def test_default_static_site_serving(self):
-        response = self.client.post("/static/empty.txt", follow=True)
+        response = self.client.get("/static/empty.txt", follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b"")
 
