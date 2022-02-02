e8ee8d90cd9652aee76a066ccd4ae230988d0a7a
Marius Andra <marius.andra@gmail.com>
2022-02-02 17:05:37 +0100

    Taxonomic popup component (#8399)
    * refactor storybook taxonomic filter scene
    
    * taxonomic popup component and scene
    
    * refactor EventName
    
    * add kea support to a story
    
    * fix import paths

---
+++ frontend/src/lib/components/TaxonomicPopup/__stories__/TaxonomicPopup.stories.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { TaxonomicPopup } from '../TaxonomicPopup'
+import { personPropertiesModel } from '~/models/personPropertiesModel'
+import { cohortsModel } from '~/models/cohortsModel'
+import { TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
+import { defaultFilterMocks } from 'lib/components/TaxonomicFilter/__stories__/mocks'
+import { KeaStory } from 'lib/storybook/kea-story'
+import { PropertyKeyInfo } from 'lib/components/PropertyKeyInfo'
+
+export default {
+    title: 'PostHog/Components/TaxonomicPopup',
+}
+
+export const OneCategory = (): JSX.Element => {
+    const [value, setValue] = useState<string | undefined>('$browser')
+
+    return (
+        <KeaStory
+            onInit={() => {
+                personPropertiesModel.mount()
+                cohortsModel.mount()
+                defaultFilterMocks()
+            }}
+        >
+            <TaxonomicPopup
+                groupType={TaxonomicFilterGroupType.PersonProperties}
+                value={value}
+                onChange={setValue}
+                renderValue={(v) => <PropertyKeyInfo value={v} />}
+            />
+        </KeaStory>
+    )
+}
+
+export const MultipleCategories = (): JSX.Element => {
+    const [value, setValue] = useState<string | undefined>(undefined)
+    const [group, setGroup] = useState(TaxonomicFilterGroupType.PersonProperties)
+
+    return (
+        <KeaStory
+            onInit={() => {
+                personPropertiesModel.mount()
+                cohortsModel.mount()
+                defaultFilterMocks()
+            }}
+        >
+            <TaxonomicPopup
+                groupType={group}
+                value={value}
+                onChange={(v, g) => {
+                    setValue(v)
+                    setGroup(g)
+                }}
+                groupTypes={[
+                    TaxonomicFilterGroupType.EventProperties,
+                    TaxonomicFilterGroupType.PersonProperties,
+                    TaxonomicFilterGroupType.Cohorts,
+                ]}
+            />
+        </KeaStory>
+    )
+}
