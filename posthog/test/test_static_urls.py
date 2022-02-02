6ad0032cf8d7ce1f5afa1ae405728df9cf804cf6
James Greenhill <fuziontech@gmail.com>
2022-02-01 16:55:42 -0800

    test of black locally

--- posthog/test/test_static_urls.py
+++ posthog/test/test_static_urls.py
@@ -8,6 +8,6 @@ class TestStaticUrls(APIBaseTest):
         self.assertEqual(response.content, b"")
 
     def test_cdn_static_site_serving(self):
-        with self.settings(CDN_URL="https://cdn.example.com")):
+        with self.settings(CDN_URL="https://cdn.example.com"):
             response = self.client.get("/static/array.js", follow=True)
             self.assertRedirects(response, "https://cdn.example.com/static/array.js")
