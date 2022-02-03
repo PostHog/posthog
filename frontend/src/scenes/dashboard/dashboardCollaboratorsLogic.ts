ea7d375c7aed9ed8c246ea803595119e81b97f3a
Michael Matloka <dev@twixes.com>
2022-02-03 21:33:12 +0100

    Improve TS typing

--- frontend/src/scenes/dashboard/dashboardCollaboratorsLogic.ts
+++ frontend/src/scenes/dashboard/dashboardCollaboratorsLogic.ts
@@ -8,9 +8,10 @@ export const dashboardCollaboratorsLogic = kea<dashboardCollaboratorsLogicType>(
     props: {} as {
         dashboardId: DashboardType['id']
     },
-
     key: (props) => props.dashboardId,
-
+    actions: () => ({
+        deleteExplicitCollaborator: (userUuid: UserType['uuid']) => ({ userUuid }),
+    }),
     loaders: ({ values, props }) => ({
         explicitCollaborators: [
             [] as DashboardCollaboratorType[],
@@ -18,7 +19,7 @@ export const dashboardCollaboratorsLogic = kea<dashboardCollaboratorsLogicType>(
                 loadExplicitCollaborators: async () => {
                     return await api.dashboards.collaborators.list(props.dashboardId)
                 },
-                deleteExplicitCollaborator: async (userUuid: UserType['uuid']) => {
+                deleteExplicitCollaborator: async ({ userUuid }) => {
                     await api.dashboards.collaborators.delete(props.dashboardId, userUuid)
                     return values.explicitCollaborators.filter((collaborator) => collaborator.user.uuid !== userUuid)
                 },
