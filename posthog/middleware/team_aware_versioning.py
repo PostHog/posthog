d3c56c858fbad84f3b9aaab3fbdc57bfeb110666
Paul D'Ambra <paul.dambra@gmail.com>
2022-02-08 17:43:45 +0000

    wip

--- posthog/middleware/team_aware_versioning.py
+++ posthog/middleware/team_aware_versioning.py
@@ -1,6 +1,12 @@
+from typing import Optional
+
 import structlog
 from django.http.request import HttpRequest
 from reversion.middleware import RevisionMiddleware
+
+from posthog.api.utils import get_data, get_event_ingestion_context, get_token
+
+VERSIONING_EXCLUSIONS = ["/e/", "/s/", "/decide/"]
 
 
 class TeamAwareVersioning(RevisionMiddleware):
@@ -9,11 +15,40 @@ class TeamAwareVersioning(RevisionMiddleware):
         self.logger = structlog.get_logger(__name__)
 
     def request_creates_revision(self, request: HttpRequest):
-        does_this_request_create_a_revision = super().request_creates_revision(request)
-        self.logger.info(f"maybe creating revision for {request}: {does_this_request_create_a_revision}")
-        return does_this_request_create_a_revision
+        """
+        Reversion tries to create a revision for any request that isn't OPTION, GET or HEAD
+
+        There are some URLs that can be on a deny-list since we know they are called frequently
+        but never generate a version
+        """
+        reversion_would_create_a_revision = super().request_creates_revision(request)
+
+        if request.path in VERSIONING_EXCLUSIONS:
+            return False
+
+        if reversion_would_create_a_revision:
+            team_id = self._get_team_id_from_request(request)
+
+            self.logger.info(
+                f"[VERSIONING] maybe creating revision",
+                team_id=team_id,
+                reversion_would_create_a_revision=reversion_would_create_a_revision,
+                request_path=request.path,
+            )
+            return reversion_would_create_a_revision and team_id
+        else:
+            return False
 
     def __call__(self, request):
         response = self.get_response(request)
-        self.logger.info(f"versioning from {request} to {response}")
         return response
+
+    @staticmethod
+    def _get_team_id_from_request(request) -> Optional[int]:
+        try:
+            data, error_response = get_data(request)
+            request_token = get_token(data, request)
+            ingestion_context, db_error, error_response = get_event_ingestion_context(request, data, request_token)
+            return ingestion_context.team_id if ingestion_context else None
+        except:
+            return None
