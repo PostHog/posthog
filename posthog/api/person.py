caa6b1b4fddb6a7fbccfe1626e7e23b59991a82d
Marius Andra <marius.andra@gmail.com>
2022-02-09 10:35:44 +0100

    ignore type

--- posthog/api/person.py
+++ posthog/api/person.py
@@ -368,7 +368,7 @@ class PersonViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
                 for (value, count) in result:
                     try:
                         # Try loading as json for dicts or arrays
-                        flattened.append({"name": json.loads(value), "count": count})
+                        flattened.append({"name": json.loads(value), "count": count})  # type: ignore
                     except json.decoder.JSONDecodeError:
                         flattened.append({"name": value, "count": count})
             statsd.incr("person_property_values_success")
