96286ed63c79486bcee0b22c643b7934de04f04f
Paul D'Ambra <paul.dambra@gmail.com>
2022-02-10 12:10:01 +0000

    explain why RevisionTeamMetadata exists

--- posthog/models/revision_team_metadata.py
+++ posthog/models/revision_team_metadata.py
@@ -3,6 +3,13 @@ from reversion.models import Revision
 
 
 class RevisionTeamMetadata(models.Model):
+    """
+    This model hooks into the mechanism that the reversion library provides for storing additional data.
+    See reversion.add_meta(model, **values) in https://django-reversion.readthedocs.io/en/stable/api.html#revision-api
+
+    It allows us to associate each revision with a team
+    """
+
     # A foreign key to Reversion called revision is required
     revision = models.OneToOneField(Revision, on_delete=models.CASCADE)
     team_id = models.PositiveIntegerField(null=False)
