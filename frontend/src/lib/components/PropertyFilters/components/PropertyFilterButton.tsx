1d3e669515f4b1df9f584b1cfdd0e195d6af8bfa
Paul D'Ambra <paul.dambra@gmail.com>
2022-02-14 15:34:09 +0000

    Add icons to property filters

--- frontend/src/lib/components/PropertyFilters/components/PropertyFilterButton.tsx
+++ frontend/src/lib/components/PropertyFilters/components/PropertyFilterButton.tsx
@@ -7,6 +7,8 @@ import { AnyPropertyFilter } from '~/types'
 import { keyMapping } from 'lib/components/PropertyKeyInfo'
 import { propertyDefinitionsModel } from '~/models/propertyDefinitionsModel'
 import { CloseButton } from 'lib/components/CloseButton'
+import { IconCohort, IconPerson, UnverifiedEventStack } from 'lib/components/icons'
+import { Tooltip } from 'lib/components/Tooltip'
 
 export interface PropertyFilterButtonProps {
     item: AnyPropertyFilter
@@ -24,7 +26,7 @@ export function PropertyFilterText({ item }: PropertyFilterButtonProps): JSX.Ele
 
 export function PropertyFilterButton({ item, ...props }: PropertyFilterButtonProps): JSX.Element {
     return (
-        <FilterButton {...props}>
+        <FilterButton {...props} item={item}>
             <PropertyFilterText item={item} />
         </FilterButton>
     )
@@ -35,9 +37,40 @@ interface FilterRowProps {
     onClose?: () => void
     setRef?: (ref: HTMLElement) => void
     children: string | JSX.Element
+    item: AnyPropertyFilter
 }
 
-export function FilterButton({ onClick, onClose, setRef, children }: FilterRowProps): JSX.Element {
+function PropertyFilterIcon({ item }: { item: AnyPropertyFilter }): JSX.Element {
+    const isEventProperty = item?.type === 'event'
+    const isPersonProperty = item?.type === 'person'
+    const isCohortProperty = item?.type === 'cohort'
+
+    return (
+        <>
+            {isEventProperty && (
+                <Tooltip title={'an event property'}>
+                    <UnverifiedEventStack
+                        style={{ marginRight: '0.5em', verticalAlign: 'middle' }}
+                        width={'14'}
+                        height={'14'}
+                    />
+                </Tooltip>
+            )}
+            {isPersonProperty && (
+                <Tooltip title={'a person property'}>
+                    <IconPerson style={{ marginRight: '0.5em', verticalAlign: 'middle', marginBottom: '2px' }} />
+                </Tooltip>
+            )}
+            {isCohortProperty && (
+                <Tooltip title={'a cohort property'}>
+                    <IconCohort style={{ marginRight: '0.5em', verticalAlign: 'middle', marginBottom: '2px' }} />
+                </Tooltip>
+            )}
+        </>
+    )
+}
+
+export function FilterButton({ onClick, onClose, setRef, children, item }: FilterRowProps): JSX.Element {
     return (
         <Button
             type="primary"
@@ -51,6 +84,7 @@ export function FilterButton({ onClick, onClose, setRef, children }: FilterRowPr
                 className="ph-no-capture property-filter-button-label"
                 style={{ width: '100%', overflow: 'hidden', textOverflow: 'ellipsis' }}
             >
+                <PropertyFilterIcon item={item} />
                 {children}
                 {onClose && (
                     <CloseButton
