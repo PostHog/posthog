9f035aaaac2fcc086f8c4e260f00ad092ce0cec7
Guido Iaquinti <guido@posthog.com>
2022-02-15 16:36:09 +0100

    posthog/settings: add 'google-cloud-sqlcommenter'

--- posthog/settings/web.py
+++ posthog/settings/web.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django_structlog.middlewares.CeleryMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "posthog.middleware.AllowIP",
+    "google.cloud.sqlcommenter.django.middleware.SqlCommenter",
     # NOTE: we need healthcheck high up to avoid hitting middlewares that may be
     # using dependencies that the healthcheck should be checking. It should be
     # ok below the above middlewares however.
