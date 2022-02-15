9937c9473dbeb680e15113ec2eec2def56f3d7a5
Paul D'Ambra <paul.dambra@gmail.com>
2022-02-15 14:43:24 +0000

    refactor to ternary

--- frontend/src/lib/components/PropertyFilters/components/PropertyFilterButton.tsx
+++ frontend/src/lib/components/PropertyFilters/components/PropertyFilterButton.tsx
@@ -45,24 +45,20 @@ function PropertyFilterIcon({ item }: { item: AnyPropertyFilter }): JSX.Element 
     const isPersonProperty = item?.type === 'person'
     const isCohortProperty = item?.type === 'cohort'
 
-    return (
-        <>
-            {isEventProperty && (
-                <Tooltip title={'Event property'}>
-                    <UnverifiedEventStack style={{ marginRight: '0.5em' }} width={'14'} height={'14'} />
-                </Tooltip>
-            )}
-            {isPersonProperty && (
-                <Tooltip title={'Person property'}>
-                    <IconPerson style={{ marginRight: '0.5em' }} />
-                </Tooltip>
-            )}
-            {isCohortProperty && (
-                <Tooltip title={'Cohort filter'}>
-                    <IconCohort style={{ marginRight: '0.5em' }} />
-                </Tooltip>
-            )}
-        </>
+    return isEventProperty ? (
+        <Tooltip title={'Event property'}>
+            <UnverifiedEventStack style={{ marginRight: '0.5em' }} width={'14'} height={'14'} />
+        </Tooltip>
+    ) : isPersonProperty ? (
+        <Tooltip title={'Person property'}>
+            <IconPerson style={{ marginRight: '0.5em' }} />
+        </Tooltip>
+    ) : isCohortProperty ? (
+        <Tooltip title={'Cohort filter'}>
+            <IconCohort style={{ marginRight: '0.5em' }} />
+        </Tooltip>
+    ) : (
+        <></>
     )
 }
 
