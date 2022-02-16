c009e964ab46c3d93379a2b26f6fc73c6da0de35
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-02-16 11:36:27 +0200

    Remove dead code

--- posthog/tasks/org_usage_report.py
+++ posthog/tasks/org_usage_report.py
@@ -73,14 +73,6 @@ OrgReport = TypedDict(
 )
 
 
-def send_all_org_usage_reports(*, dry_run: bool = False) -> List[OrgReport]:
-    """
-    Creates and sends usage reports for all teams.
-    Returns a list of all the successfully sent reports.
-    """
-    return send_all_reports(dry_run=dry_run)
-
-
 def send_all_reports(*, dry_run: bool = False) -> List[OrgReport]:
     """
     Generic way to generate and send org usage reports.
