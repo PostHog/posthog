2fa8f9868260a48c78afe501188523520dc2afac
eric <eeoneric@gmail.com>
2022-02-23 11:06:23 +0100

    add query and test

--- ee/clickhouse/sql/cohort.py
+++ ee/clickhouse/sql/cohort.py
@@ -1,3 +1,4 @@
+from ee.clickhouse.sql.person import PERSON_STATIC_COHORT_TABLE
 from posthog.settings import CLICKHOUSE_CLUSTER
 
 from .clickhouse import COLLAPSING_MERGE_TREE, table_engine
@@ -96,3 +97,15 @@ ORDER BY person_id
 LIMIT %(limit)s
 OFFSET %(offset)s
 """
+
+GET_STATIC_COHORTPEOPLE_BY_COHORT_ID = """
+SELECT person_id
+FROM {}
+WHERE team_id = %(team_id)s AND cohort_id = %(cohort_id)s
+GROUP BY person_id, cohort_id, team_id
+ORDER BY person_id
+LIMIT %(limit)s
+OFFSET %(offset)s
+""".format(
+    PERSON_STATIC_COHORT_TABLE
+)
