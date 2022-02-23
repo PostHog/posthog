dd110eace78af6cc11188d9ddc8dd4d144a6c54d
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-02-23 09:20:20 +0200

    Make event inserts, mv table lazy

--- ee/clickhouse/sql/events.py
+++ ee/clickhouse/sql/events.py
@@ -70,7 +70,7 @@ KAFKA_EVENTS_TABLE_SQL = lambda: EVENTS_TABLE_BASE_SQL.format(
 # You must include the database here because of a bug in clickhouse
 # related to https://github.com/ClickHouse/ClickHouse/issues/10471
 EVENTS_TABLE_MV_SQL = lambda: """
-CREATE MATERIALIZED VIEW {table_name}_mv ON CLUSTER {cluster}
+CREATE MATERIALIZED VIEW events_mv ON CLUSTER {cluster}
 TO {database}.{table_name}
 AS SELECT
 uuid,
@@ -88,10 +88,12 @@ FROM {database}.kafka_{table_name}
     table_name=EVENTS_DATA_TABLE(), cluster=settings.CLICKHOUSE_CLUSTER, database=settings.CLICKHOUSE_DATABASE,
 )
 
-INSERT_EVENT_SQL = """
-INSERT INTO events (uuid, event, properties, timestamp, team_id, distinct_id, elements_chain, created_at, _timestamp, _offset)
+INSERT_EVENT_SQL = (
+    lambda: f"""
+INSERT INTO {EVENTS_DATA_TABLE()} (uuid, event, properties, timestamp, team_id, distinct_id, elements_chain, created_at, _timestamp, _offset)
 SELECT %(uuid)s, %(event)s, %(properties)s, %(timestamp)s, %(team_id)s, %(distinct_id)s, %(elements_chain)s, %(created_at)s, now(), 0
 """
+)
 
 GET_EVENTS_SQL = """
 SELECT
