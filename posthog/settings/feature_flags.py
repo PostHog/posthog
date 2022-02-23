6e047baea8d3458939d546ea8cefc41e9e131834
Michael Matloka <dev@twixes.com>
2022-02-23 10:31:37 +0100

    Add `dashboard-permissions` to `PERSISTED_FEATURE_FLAGS`

--- posthog/settings/feature_flags.py
+++ posthog/settings/feature_flags.py
@@ -12,6 +12,7 @@ PERSISTED_FEATURE_FLAGS: List[str] = []
 default_flag_persistence = [
     # Add hard-coded feature flags for static self-hosted releases here
     "new-paths-ui-edge-weights",
+    "dashboard-permissions",
 ]
 
 if env_feature_flags != "0" and env_feature_flags.lower() != "false" and not DEBUG and not E2E_TESTING:
