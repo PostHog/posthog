b53088ea0cf6b8b70b7319557d15ef26d9c09420
Neil Kakkar <neilkakkar@gmail.com>
2022-02-28 17:09:25 +0000

    don't touch correlation props in this PR

--- ee/clickhouse/queries/funnels/funnel_correlation_persons.py
+++ ee/clickhouse/queries/funnels/funnel_correlation_persons.py
@@ -72,8 +72,8 @@ class _FunnelEventsCorrelationActors(ActorBaseQuery):
 
         funnel_persons_query, funnel_persons_params = self._funnel_correlation.get_funnel_actors_cte()
 
-        prop_filters = self._filter.correlation_person_entity.property_groups
-        prop_query, prop_params = FunnelEventQuery(self._filter, self._team.pk)._get_prop_groups(prop_filters)
+        prop_filters = self._filter.correlation_person_entity.properties
+        prop_query, prop_params = FunnelEventQuery(self._filter, self._team.pk)._get_props(prop_filters)
 
         conversion_filter = (
             f'AND actors.steps {"=" if self._filter.correlation_persons_converted else "<>"} target_step'
