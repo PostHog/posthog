8d66e0de035c817f5021345b9961754b29555f09
Tim Glaser <tim@glsr.nl>
2022-03-08 17:47:03 -0800

    fix

--- ee/clickhouse/queries/trends/clickhouse_trends.py
+++ ee/clickhouse/queries/trends/clickhouse_trends.py
@@ -36,7 +36,7 @@ class ClickhouseTrends(ClickhouseTrendsTotalVolume, ClickhouseLifecycle, Clickho
         if filter.breakdown:
             sql, params, parse_function = ClickhouseTrendsBreakdown(entity, filter, team).get_query()
         elif filter.shown_as == TRENDS_LIFECYCLE:
-            sql, params, parse_function = self._format_lifecycle_query(entity, filter, team)
+            sql, params, parse_function = self._format_lifecycle_query(entity, filter, team.pk)
         else:
             sql, params, parse_function = self._total_volume_query(entity, filter, team)
 
