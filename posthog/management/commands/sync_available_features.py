13e18a64ede4ec3b9b725c49b1ba62fe631b43ce
Rick Marron <rcmarron@gmail.com>
2022-03-10 08:02:53 -0800

    fix: simplify sync avail feat command (#8964)

--- posthog/management/commands/sync_available_features.py
+++ posthog/management/commands/sync_available_features.py
@@ -1,7 +1,7 @@
 import structlog
 from django.core.management.base import BaseCommand
 
-from posthog.models import Organization
+from posthog.tasks.sync_all_organization_available_features import sync_all_organization_available_features
 
 logger = structlog.get_logger(__name__)
 
@@ -10,8 +10,5 @@ class Command(BaseCommand):
     help = "Sync available features for all organizations"
 
     def handle(self, *args, **options):
-        for org in Organization.objects.all():
-            org.update_available_features()
-            org.save()
-            billing_plan, _ = org._billing_plan_details
-            logger.info(f"{billing_plan} features synced for org: {org.name}")
+        sync_all_organization_available_features()
+        logger.info("Features synced for all organizations")
