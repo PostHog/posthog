aa5f741da5c566a926a1085a682c7791fbcff3ba
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-03-14 12:01:44 +0000

    fix(deploy-race-condition): retrigger deploy (#9006)

--- plugin-server/src/init.ts
+++ plugin-server/src/init.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/node'
 import { PluginsServerConfig } from './types'
 import { setLogLevel } from './utils/utils'
 
-// Must require as `tsc` strips unused `import` statements and just requiring this seems to init some globals
+// Must require as `tsc` strips unused `import` statements and just requiring this seems to init some globals 
 require('@sentry/tracing')
 
 // Code that runs on app start, in both the main and worker threads
