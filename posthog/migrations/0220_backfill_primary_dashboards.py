f0cd73ebfe900bc4848f817a40a373e59eda812c
Rick Marron <rcmarron@gmail.com>
2022-03-14 16:38:57 -0600

    move to bulk_update

--- posthog/migrations/0220_backfill_primary_dashboards.py
+++ posthog/migrations/0220_backfill_primary_dashboards.py
@@ -44,12 +44,25 @@ def backfill_primary_dashboards(apps, _):
 
     num_teams_to_update = len(team_dashboards)
     logger.info(f"fetched {num_teams_to_update} teams")
+    batch_size = 500
 
-    # Now iterate the list of teams and update the primary dashboard
-    for team_id, primary_dashboard_id in team_dashboards:
-        Team.objects.filter(id=team_id).update(primary_dashboard_id=primary_dashboard_id)
+    # From the list fetched above, we now update the teams in batches of 500
+    for i in range(0, num_teams_to_update, batch_size):
+        logger.info(f"Updating team {i} to {i + batch_size}")
+        team_dashboards_in_batch = team_dashboards[i : i + batch_size]
 
-    logger.info(f"Successful update of {num_teams_to_update} teams")
+        # Get the Django team object for all the team ids in this batch
+        team_ids_in_batch = [team_dashboard[0] for team_dashboard in team_dashboards_in_batch]
+        teams_obj_to_update = Team.objects.filter(id__in=team_ids_in_batch).only("id", "primary_dashboard_id")
+
+        # Set the new primary_dashboard_id for each team
+        team_to_primary_dashboard = dict(team_dashboards_in_batch)
+        for team in teams_obj_to_update:
+            team.primary_dashboard_id = team_to_primary_dashboard[team.id]
+
+        # Bulk update the teams in the DB
+        Team.objects.bulk_update(teams_obj_to_update, ["primary_dashboard_id"])
+        logger.info(f"Successful update of team {i} to {i + batch_size}")
 
 
 # Because of the nature of this backfill, there's no way to reverse it without potentially destroying customer data
