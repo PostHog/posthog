c528e931efdcd633c568d82c23623bd1ffd24bb6
Marius Andra <marius.andra@gmail.com>
2022-03-15 22:57:18 +0100

    new insight stories

--- frontend/src/scenes/insights/__stories__/Insights.stories.tsx
+++ frontend/src/scenes/insights/__stories__/Insights.stories.tsx
@@ -5,25 +5,7 @@ import { mswDecorator, useStorybookMocks } from '~/mocks/browser'
 import { InsightScene } from '../InsightScene'
 import { router } from 'kea-router'
 import { InsightModel } from '~/types'
-import {
-    cohortBreakdownInsight,
-    cohortRetentionInsight,
-    sampleRetentionPeopleResponse,
-} from 'scenes/insights/__stories__/insight.mocks'
-
-export const RetentionCohort = createInsightScene(cohortRetentionInsight)
-export const RetentionBreakdown = createInsightScene(cohortBreakdownInsight)
-
-export const TrendsBar = createInsightScene(require('./trendsBar.json'))
-export const TrendsBarBreakdown = createInsightScene(require('./trendsBarBreakdown.json'))
-export const TrendsLine = createInsightScene(require('./trendsLine.json'))
-export const TrendsLineBreakdown = createInsightScene(require('./trendsLineBreakdown.json'))
-export const TrendsValue = createInsightScene(require('./trendsValue.json'))
-export const TrendsValueBreakdown = createInsightScene(require('./trendsValueBreakdown.json'))
-export const TrendsTable = createInsightScene(require('./trendsTable.json'))
-export const TrendsTableBreakdown = createInsightScene(require('./trendsTableBreakdown.json'))
-export const TrendsPie = createInsightScene(require('./trendsPie.json'))
-export const TrendsPieBreakdown = createInsightScene(require('./trendsPieBreakdown.json'))
+import { samplePersonProperties, sampleRetentionPeopleResponse } from 'scenes/insights/__stories__/insight.mocks'
 
 export default {
     title: 'Scenes/Insights',
@@ -31,11 +13,7 @@ export default {
         mswDecorator({
             get: {
                 '/api/person/retention': sampleRetentionPeopleResponse,
-                '/api/person/properties': [
-                    { id: 1, name: 'location', count: 1 },
-                    { id: 2, name: 'role', count: 2 },
-                    { id: 3, name: 'height', count: 3 },
-                ],
+                '/api/person/properties': samplePersonProperties,
             },
             post: {
                 '/api/projects/:projectId/cohorts/': { id: 1 },
@@ -63,3 +41,27 @@ function createInsightScene(insight: Partial<InsightModel>): () => JSX.Element {
         return <InsightScene />
     }
 }
+
+export const Retention = createInsightScene(require('./retention.json'))
+export const RetentionBreakdown = createInsightScene(require('./retentionBreakdown.json'))
+export const Lifecycle = createInsightScene(require('./lifecycle.json'))
+export const Stickiness = createInsightScene(require('./stickiness.json'))
+export const UserPaths = createInsightScene(require('./userPaths.json'))
+
+export const FunnelLeftToRight = createInsightScene(require('./funnelLeftToRight.json'))
+export const FunnelLeftToRightBreakdown = createInsightScene(require('./funnelLeftToRightBreakdown.json'))
+export const FunnelTopToBottom = createInsightScene(require('./funnelTopToBottom.json'))
+export const FunnelTopToBottomBreakdown = createInsightScene(require('./funnelTopToBottomBreakdown.json'))
+export const funnelHistoricalTrends = createInsightScene(require('./funnelHistoricalTrends.json'))
+export const FunnelTimeToConvert = createInsightScene(require('./funnelTimeToConvert.json'))
+
+export const TrendsBar = createInsightScene(require('./trendsBar.json'))
+export const TrendsBarBreakdown = createInsightScene(require('./trendsBarBreakdown.json'))
+export const TrendsLine = createInsightScene(require('./trendsLine.json'))
+export const TrendsLineBreakdown = createInsightScene(require('./trendsLineBreakdown.json'))
+export const TrendsValue = createInsightScene(require('./trendsValue.json'))
+export const TrendsValueBreakdown = createInsightScene(require('./trendsValueBreakdown.json'))
+export const TrendsTable = createInsightScene(require('./trendsTable.json'))
+export const TrendsTableBreakdown = createInsightScene(require('./trendsTableBreakdown.json'))
+export const TrendsPie = createInsightScene(require('./trendsPie.json'))
+export const TrendsPieBreakdown = createInsightScene(require('./trendsPieBreakdown.json'))
