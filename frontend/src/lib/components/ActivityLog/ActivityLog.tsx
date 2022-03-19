c7223c502366505d4f8fe1a1517e872ed1e06d8b
Paul D'Ambra <paul.dambra@gmail.com>
2022-03-19 09:28:21 +0000

    Add loading state to activity log

--- frontend/src/lib/components/ActivityLog/ActivityLog.tsx
+++ frontend/src/lib/components/ActivityLog/ActivityLog.tsx
@@ -5,6 +5,7 @@ import { useValues } from 'kea'
 import './ActivityLog.scss'
 import { activityLogLogic } from 'lib/components/ActivityLog/activityLogLogic'
 import clsx from 'clsx'
+import { Skeleton } from 'antd'
 
 export interface ActivityLogProps {
     scope: 'FeatureFlag'
@@ -13,17 +14,39 @@ export interface ActivityLogProps {
 }
 
 const Empty = (): JSX.Element => (
-    <div className="activity-log">
+    <div className="activity-log activity-log__loader">
         <div className="text-muted">There is no history for this item</div>
     </div>
 )
 
+const SkeletonLog = (): JSX.Element => {
+    return (
+        <div className="activity-log-row">
+            <Skeleton.Avatar active={true} size={40} />
+            <div className="details">
+                <Skeleton paragraph={{ rows: 1 }} />
+            </div>
+        </div>
+    )
+}
+
+const Loading = (): JSX.Element => {
+    return (
+        <>
+            <SkeletonLog />
+            <SkeletonLog />
+            <SkeletonLog />
+            <SkeletonLog />
+        </>
+    )
+}
+
 export const ActivityLog = ({ scope, id }: ActivityLogProps): JSX.Element | null => {
     const logic = activityLogLogic({ scope, id })
-    const { activity } = useValues(logic)
-
+    const { activity, activityLoading } = useValues(logic)
+    console.log({ activityLoading })
     return (
-        <div className={clsx('activity-log')}>
+        <div className={clsx('activity-log', activityLoading && 'activity-log__loading')}>
             {activity && activity.length ? (
                 activity.map((logItem, index) => {
                     return (
@@ -40,6 +63,8 @@ export const ActivityLog = ({ scope, id }: ActivityLogProps): JSX.Element | null
                         </div>
                     )
                 })
+            ) : activityLoading ? (
+                <Loading />
             ) : (
                 <Empty />
             )}
