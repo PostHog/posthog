d157f94e1c8e2db327124d446ed64b3aaee06704
Marius Andra <marius.andra@gmail.com>
2022-03-22 00:09:58 +0100

    hackity hack

--- cypress/integration/dashboard.js
+++ cypress/integration/dashboard.js
@@ -15,6 +15,7 @@ describe('Dashboard', () => {
     it('Adding new insight to dashboard works', () => {
         cy.get('[data-attr=menu-item-insight]').click() // Create a new insight
         cy.get('[data-attr="insight-save-button"]').click() // Save the insight
+        cy.wait(100)
         cy.get('[data-attr="edit-prop-name"]').click({ force: true }) // Rename insight, out of view, must force
         cy.focused().clear().type('Test Insight Zeus')
         cy.get('button').contains('Save').click() // Save the new name
