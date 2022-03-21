33d0afa82269c735250116351a93cdff4932dd1d
Marius Andra <marius.andra@gmail.com>
2022-03-21 21:42:08 +0100

    urls.insightNew()

--- cypress/integration/trends.js
+++ cypress/integration/trends.js
@@ -3,7 +3,7 @@ import { urls } from 'scenes/urls'
 describe('Trends', () => {
     beforeEach(() => {
         cy.visit(urls.insightNew())
-        cy.location('pathname').should('include', '/edit')
+        cy.location('pathname').should('include', urls.insightNew())
     })
 
     it('Can load a graph from a URL directly', () => {
