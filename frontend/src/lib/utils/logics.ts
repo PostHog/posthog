503e76db9e4572af61e87153197fba13008337b9
Michael Matloka <dev@twixes.com>
2022-03-22 11:36:16 +0100

    Fix `getCurrentTeamId`

--- frontend/src/lib/utils/logics.ts
+++ frontend/src/lib/utils/logics.ts
@@ -3,8 +3,7 @@ import { TeamType } from '../../types'
 import { getAppContext } from './getAppContext'
 
 export function getCurrentTeamId(providedMaybeTeamId?: TeamType['id'] | null): TeamType['id'] {
-    const maybeTeamId =
-        providedMaybeTeamId !== undefined ? providedMaybeTeamId : teamLogic.findMounted()?.values.currentTeamId
+    const maybeTeamId = providedMaybeTeamId !== undefined ? providedMaybeTeamId : teamLogic.values.currentTeamId
     if (!maybeTeamId) {
         throw new Error(`Project ID is not known.${getAppContext()?.anonymous ? ' User is anonymous.' : ''}`)
     }
