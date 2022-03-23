05f1676146dee1ffa9b1d605be18954b56c941be
Marius Andra <marius.andra@gmail.com>
2022-03-23 16:22:23 +0100

    improve form story

--- frontend/src/lib/forms/Field.stories.tsx
+++ frontend/src/lib/forms/Field.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { kea, useValues } from 'kea'
 import { ComponentMeta } from '@storybook/react'
-
 import { VerticalForm } from 'lib/forms/VerticalForm'
 import { Field, FieldProps } from './Field'
-import type { formLogicType } from './Field.storiesType'
+import type { formLogicType, textFieldLogicType } from './Field.storiesType'
+import { Input } from 'antd'
 
 export default {
     title: 'Forms-Core/Field',
@@ -37,7 +37,6 @@ export default {
         },
     },
 } as ComponentMeta<typeof Field>
-
 const formLogic = kea<formLogicType>({
     path: ['lib', 'forms', 'Field', 'stories'],
     forms: {
@@ -45,16 +44,34 @@ const formLogic = kea<formLogicType>({
             defaults: {
                 name: '',
                 email: '',
+
                 pineappleOnPizza: false,
             },
+            validator: ({ name, email }) => ({
+                name: !name ? 'Please enter your name' : null,
+                email: !email ? 'Please enter your email' : null,
+            }),
         },
     },
 })
-
+const textFieldLogic = kea<textFieldLogicType>({
+    path: ['lib', 'forms', 'Field', 'stories'],
+    forms: {
+        myForm: {
+            defaults: {
+                name: '',
+            },
+            validator: ({ name }) => ({
+                name: !name ? 'Please enter your name' : null,
+            }),
+        },
+    },
+})
 export function Field_(props: FieldProps): JSX.Element {
-    const { myForm } = useValues(formLogic)
+    const { myForm, myFormValidationErrors } = useValues(formLogic)
     return (
         <VerticalForm logic={formLogic} formKey="myForm">
+            <pre>{JSON.stringify({ myForm, myFormValidationErrors }, null, 2)}</pre>
             <Field {...props} name={props.name || 'name'} label={props.label || 'Name'}>
                 <input />
             </Field>
@@ -71,8 +88,20 @@ export function Field_(props: FieldProps): JSX.Element {
             >
                 <input />
             </Field>
+            <input type="submit" value="Submit" />
+        </VerticalForm>
+    )
+}
 
-            <pre>{JSON.stringify(myForm, null, 2)}</pre>
+export function TextField(props: FieldProps): JSX.Element {
+    const { myForm, myFormValidationErrors } = useValues(formLogic)
+    return (
+        <VerticalForm logic={textFieldLogic} formKey="myForm">
+            <pre>{JSON.stringify({ myForm, myFormValidationErrors }, null, 2)}</pre>
+            <Field {...props} name={props.name || 'name'} label={props.label || 'Name'}>
+                <Input />
+            </Field>
+            <input type="submit" value="Submit" />
         </VerticalForm>
     )
 }
