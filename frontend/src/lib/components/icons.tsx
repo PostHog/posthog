c54e246853384dbecddcd94383e7e392421c74a6
Chris Clark <clarkus@users.noreply.github.com>
2022-03-24 11:32:49 -0700

    Changes the icon viewbox to a standard 24x sizing.

--- frontend/src/lib/components/icons.tsx
+++ frontend/src/lib/components/icons.tsx
@@ -1314,7 +1314,7 @@ export function LiveIcon({ width = 24, height = 24, ...props }: React.SVGProps<S
         <svg
             width={width}
             height={height}
-            viewBox="0 0 20 16"
+            viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             {...props}
