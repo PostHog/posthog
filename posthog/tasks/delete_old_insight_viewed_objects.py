f25e4551cfe5b125e93c4859986e645747854338
Rick Marron <rcmarron@gmail.com>
2022-03-24 13:06:23 -0600

    remove clean up job

--- posthog/tasks/delete_old_insight_viewed_objects.py
+++
@@ -1,8 +0,0 @@
-from django.utils import timezone
-
-from posthog.models import InsightViewed
-
-
-def delete_old_insight_viewed_objects() -> None:
-    """Adds a 30 day TTL to the InsightViewed model."""
-    InsightViewed.objects.filter(last_viewed_at__lte=timezone.now() - timezone.timedelta(days=30)).delete()
