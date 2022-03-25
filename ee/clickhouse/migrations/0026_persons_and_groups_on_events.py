6a4387409130f22326336b41f762579f3cbeddb7
yakkomajuri <yakko.majuri@gmail.com>
2022-03-25 10:46:15 +0000

    address review

--- ee/clickhouse/migrations/0026_persons_and_groups_on_events.py
+++ ee/clickhouse/migrations/0026_persons_and_groups_on_events.py
@@ -1,8 +1,8 @@
 from infi.clickhouse_orm import migrations
 
+from ee.clickhouse.replication.utils import clickhouse_is_replicated
 from posthog.client import sync_execute
 from posthog.settings import CLICKHOUSE_CLUSTER
-from posthog.settings.data_stores import CLICKHOUSE_DATABASE, CLICKHOUSE_REPLICATION
 
 ADD_COLUMNS_BASE_SQL = """
 ALTER TABLE {table}
@@ -13,28 +13,17 @@ ADD COLUMN IF NOT EXISTS group0_properties VARCHAR,
 ADD COLUMN IF NOT EXISTS group1_properties VARCHAR,
 ADD COLUMN IF NOT EXISTS group2_properties VARCHAR,
 ADD COLUMN IF NOT EXISTS group3_properties VARCHAR,
-ADD COLUMN IF NOT EXISTS group4_properties VARCHAR;
+ADD COLUMN IF NOT EXISTS group4_properties VARCHAR
 """
+
+
+def add_columns_to_sharded_tabled_if_nedded(_):
+    if clickhouse_is_replicated():
+        sync_execute(ADD_COLUMNS_BASE_SQL.format(table="writable_events", cluster=CLICKHOUSE_CLUSTER))
+        sync_execute(ADD_COLUMNS_BASE_SQL.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
+
 
 operations = [
     migrations.RunSQL(ADD_COLUMNS_BASE_SQL.format(table="events", cluster=CLICKHOUSE_CLUSTER)),
+    migrations.RunPython(add_columns_to_sharded_tabled_if_nedded),
 ]
-
-
-if CLICKHOUSE_REPLICATION:
-
-    # CLICKHOUSE_REPLICATION may be turned on but the instance might still
-    # be on the old schema if it didn't run the sharding async migration (0004_replicated_schema)
-    verify_table_exists_result = sync_execute(
-        f"""
-            SELECT count(*) from system.tables WHERE database='{CLICKHOUSE_DATABASE}' and name='writable_events'
-        """
-    )
-
-    if verify_table_exists_result[0][0] > 0:
-        extra_operations = [
-            migrations.RunSQL(ADD_COLUMNS_BASE_SQL.format(table="writable_events", cluster=CLICKHOUSE_CLUSTER)),
-            migrations.RunSQL(ADD_COLUMNS_BASE_SQL.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER)),
-        ]
-
-        operations += extra_operations
