f359ec7b924112642ed0afca6436f0ac6be282f8
Li Yi Yu <li@posthog.com>
2022-03-30 20:51:05 +0000

    table

--- frontend/src/scenes/userSQL/UserSQL.tsx
+++ frontend/src/scenes/userSQL/UserSQL.tsx
@@ -1,5 +1,33 @@
+import { useValues } from 'kea'
+import { LemonTable } from 'lib/components/LemonTable'
 import React from 'react'
+import { insightLogic } from 'scenes/insights/insightLogic'
 
 export function UserSQLInsight(): JSX.Element {
-    return <p>heLLO</p>
+    const { insight } = useValues(insightLogic)
+
+    const keys = Object.keys(insight?.result?.[0] || [])
+
+    const columns = keys.map((key) => {
+        return {
+            title: key,
+            dataIndex: key,
+            render: function RenderKey(result): JSX.Element {
+                return <div>{result}</div>
+            },
+        }
+    })
+
+    return (
+        <LemonTable
+            columns={columns}
+            // showHeader={!embedded}
+            size="small"
+            rowKey="0"
+            // embedded={embedded}
+            dataSource={insight.result}
+            // className={className}
+            emptyState="This property value is an empty object."
+        />
+    )
 }
