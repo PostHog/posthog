0f9909415e43596135985fd64ed6187c1f4b96a1
eric <eeoneric@gmail.com>
2022-03-30 17:52:16 +0000

    don't render line

--- frontend/src/scenes/insights/InsightTabs/UserSQLTab/UserSQLTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/UserSQLTab/UserSQLTab.tsx
@@ -33,6 +33,7 @@ export function UserSQLTab(): JSX.Element {
                 }}
                 options={{
                     minimap: { enabled: false },
+                    renderLineHighlight: 'none',
                 }}
             />
             <LemonRow>
