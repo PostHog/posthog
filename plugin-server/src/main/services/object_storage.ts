af20f74994905fc51187ddcce5cc70cd0f681a56
Paul D'Ambra <paul.dambra@gmail.com>
2022-03-30 15:17:53 +0000

    remove some comments

--- plugin-server/src/main/services/object_storage.ts
+++ plugin-server/src/main/services/object_storage.ts
@@ -7,25 +7,3 @@ export const S3 = new aws.S3({
     s3ForcePathStyle: true, // needed with minio?
     signatureVersion: 'v4',
 })
-
-// // putObject operation.
-
-// let params = {Bucket: 'posthog', Key: 'testobject', Body: 'Hello from MinIO!!'};
-
-// s3.putObject(params, function(err, data) {
-//       if (err)
-//        console.log(err)
-//       else
-//        console.log("Successfully uploaded data to testbucket/testobject");
-// });
-
-// // getObject operation.
-
-// var params = {Bucket: 'testbucket', Key: 'testobject'};
-
-// var file = require('fs').createWriteStream('/tmp/mykey');
-
-// s3.getObject(params).
-// on('httpData', function(chunk) { file.write(chunk); }).
-// on('httpDone', function() { file.end(); }).
-// send();
