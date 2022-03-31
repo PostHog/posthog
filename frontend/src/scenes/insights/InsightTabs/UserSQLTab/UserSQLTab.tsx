1f7a542730d2582dc663ca8165ba8957050be334
Li Yi Yu <li@posthog.com>
2022-03-31 15:48:36 +0000

    dark mode

--- frontend/src/scenes/insights/InsightTabs/UserSQLTab/UserSQLTab.tsx
+++ frontend/src/scenes/insights/InsightTabs/UserSQLTab/UserSQLTab.tsx
@@ -6,12 +6,14 @@ import { insightLogic } from 'scenes/insights/insightLogic'
 import { LemonButton } from 'lib/components/LemonButton'
 import { format } from 'sql-formatter'
 import { LemonRow } from 'lib/components/LemonRow'
+import { Switch } from 'antd'
 
 export function UserSQLTab(): JSX.Element {
     const { insightProps, filters } = useValues(insightLogic)
     const { loadResultsWithProgress } = useActions(insightLogic)
     const { setFilters } = useActions(userSQLlogic(insightProps))
     const [query, setQuery] = useState<string | undefined>(format(filters.user_sql || ''))
+    const [theme, setTheme] = useState('light')
 
     const onSubmit = (): void => {
         if (query !== filters.user_sql) {
@@ -28,9 +30,19 @@ export function UserSQLTab(): JSX.Element {
 
     return (
         <div>
+            <Switch
+                checkedChildren="dark"
+                unCheckedChildren="light"
+                style={{ marginBottom: 8, float: 'right' }}
+                onChange={() => {
+                    const newTheme = theme === 'light' ? 'vs-dark' : 'light'
+                    setTheme(newTheme)
+                }}
+            />
             <MonacoEditor
                 language="sql"
                 height={400}
+                theme={theme}
                 value={query}
                 onChange={(value) => {
                     setQuery(value)
