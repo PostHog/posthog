d445639d0c58654de92ecb15effad9cdacb23bc1
Tiina Turban <tiina303@gmail.com>
2022-03-31 16:37:38 +0000

    fix

--- posthog/celery.py
+++ posthog/celery.py
@@ -60,7 +60,9 @@ def setup_periodic_tasks(sender: Celery, **kwargs):
     # Cloud (posthog-cloud) cron jobs
     if getattr(settings, "MULTI_TENANCY", False):
         # TODO: used for testing
-        sender.add_periodic_task(crontab(second=0), calculate_billing_daily_usage.s())  # every day midnight UTC
+        sender.add_periodic_task(
+            crontab(hour="*", minute="*"), calculate_billing_daily_usage.s()
+        )  # every day midnight UTC
 
     sender.add_periodic_task(crontab(day_of_week="fri", hour=0, minute=0), clean_stale_partials.s())
 
