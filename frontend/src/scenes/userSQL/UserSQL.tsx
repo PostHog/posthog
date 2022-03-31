1e63bacfb3cf78aab704704e44cf2674fa09813c
Rick Marron <rcmarron@gmail.com>
2022-03-31 15:32:29 +0000

    some json goodies

--- frontend/src/scenes/userSQL/UserSQL.tsx
+++ frontend/src/scenes/userSQL/UserSQL.tsx
@@ -1,6 +1,7 @@
 import { useValues } from 'kea'
 import { LemonTable } from 'lib/components/LemonTable'
 import React, { useEffect, useState } from 'react'
+import { JSONTree } from 'react-json-tree'
 import { insightLogic } from 'scenes/insights/insightLogic'
 
 function convertArrayToString(payload): any {
@@ -36,7 +37,39 @@ export function UserSQLInsight(): JSX.Element {
             title: key,
             dataIndex: key,
             render: function RenderKey(result): JSX.Element {
-                return <div>{result}</div>
+                try {
+                    const data = JSON.parse(result)
+                    return (
+                        <div style={{ minWidth: 300 }}>
+                            <JSONTree
+                                data={data}
+                                shouldExpandNode={() => false}
+                                theme={{
+                                    scheme: 'bright',
+                                    author: 'chris kempson (http://chriskempson.com)',
+                                    base00: '#000000',
+                                    base01: '#303030',
+                                    base02: '#505050',
+                                    base03: '#b0b0b0',
+                                    base04: '#d0d0d0',
+                                    base05: '#e0e0e0',
+                                    base06: '#f5f5f5',
+                                    base07: '#ffffff',
+                                    base08: '#fb0120',
+                                    base09: '#fc6d24',
+                                    base0A: '#fda331',
+                                    base0B: '#a1c659',
+                                    base0C: '#76c7b7',
+                                    base0D: '#6fb3d2',
+                                    base0E: '#d381c3',
+                                    base0F: '#be643c',
+                                }}
+                            />
+                        </div>
+                    )
+                } catch {
+                    return <div>{result}</div>
+                }
             },
         }
     })
