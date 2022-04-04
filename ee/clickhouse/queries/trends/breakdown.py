18ccf3eea1307711ee9ccbc7332caf9a850f2ffd
eric <eeoneric@gmail.com>
2022-04-04 11:56:09 -0400

    fix breakdown prop name

--- ee/clickhouse/queries/trends/breakdown.py
+++ ee/clickhouse/queries/trends/breakdown.py
@@ -42,6 +42,8 @@ from posthog.utils import encode_get_request_params
 
 
 class ClickhouseTrendsBreakdown:
+    DISTINCT_ID_TABLE_ALIAS = "pdi"
+
     def __init__(
         self, entity: Entity, filter: Filter, team: Team, column_optimizer: Optional[EnterpriseColumnOptimizer] = None
     ):
@@ -69,6 +71,7 @@ class ClickhouseTrendsBreakdown:
             property_group=outer_properties,
             table_name="e",
             person_properties_mode=PersonPropertiesMode.USING_PERSON_PROPERTIES_COLUMN,
+            person_id_joined_alias=f"{self.DISTINCT_ID_TABLE_ALIAS}.person_id",
         )
         aggregate_operation, _, math_params = process_math(self.entity, self.team, event_table_alias="e")
 
@@ -341,7 +344,7 @@ class ClickhouseTrendsBreakdown:
                 f"""
             {event_join}
             INNER JOIN ({query}) person
-            ON person.id = pdi.person_id
+            ON person.id = {self.DISTINCT_ID_TABLE_ALIAS}.person_id
             """,
                 params,
             )
