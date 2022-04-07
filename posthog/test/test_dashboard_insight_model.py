ce2567e8766b82aacebd00fa6a324c47562dfcdd
Paul D'Ambra <paul.dambra@gmail.com>
2022-04-07 21:13:03 +0100

    team id in test needs to be consistent

--- posthog/test/test_dashboard_insight_model.py
+++ posthog/test/test_dashboard_insight_model.py
@@ -1,27 +1,29 @@
-from posthog.models import Dashboard, Insight
+from posthog.models import Dashboard, Insight, Team
 from posthog.models.dashboard import DashboardInsight
 from posthog.test.base import BaseTest
 
 
 class TestDashboardInsightModel(BaseTest):
     def test_saving_the_model_generates_a_filter_hash(self):
-        dashboard = Dashboard.objects.create(team=self.team)
+        team: Team = Team.objects.create(id=7, organization=self.organization)
+        dashboard = Dashboard.objects.create(team=team)
         insight = Insight.objects.create(
-            team=self.team,
+            team=team,
             filters={"events": [{"id": "$pageview"}], "properties": [{"key": "$browser", "value": "Mac OS X"}],},
         )
 
         dashboard_insight: DashboardInsight = DashboardInsight.objects.create(dashboard=dashboard, insight=insight)
 
-        self.assertEqual(dashboard_insight.filters_hash, "cache_fb5a5750179393024e866d2a06102feb")
+        self.assertEqual(dashboard_insight.filters_hash, "cache_a04161f5c9b35f28f105caba6f009cb7")
 
     def test_saving_the_model_generates_a_filter_hash_affected_by_dashboard_filters(self):
-        dashboard = Dashboard.objects.create(team=self.team, filters={"date_from": "-3d", "date_to": None})
+        team: Team = Team.objects.create(id=7, organization=self.organization)
+        dashboard = Dashboard.objects.create(team=team, filters={"date_from": "-3d", "date_to": None})
         insight = Insight.objects.create(
-            team=self.team,
+            team=team,
             filters={"events": [{"id": "$pageview"}], "properties": [{"key": "$browser", "value": "Mac OS X"}],},
         )
 
         dashboard_insight: DashboardInsight = DashboardInsight.objects.create(dashboard=dashboard, insight=insight)
 
-        self.assertEqual(dashboard_insight.filters_hash, "cache_1873ab2156058cfabafdc3c5d8a15a7a")
+        self.assertEqual(dashboard_insight.filters_hash, "cache_2a842d9a3d5c2a543cfb057e7af52f2f")
