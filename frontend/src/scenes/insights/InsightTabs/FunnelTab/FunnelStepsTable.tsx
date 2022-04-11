c79172a7638351c4a4532d7a26e1c0a4ee672b58
Michael Matloka <dev@twixes.com>
2022-04-11 16:01:04 +0200

    Display something in `FunnelStepsTable`

--- frontend/src/scenes/insights/InsightTabs/FunnelTab/FunnelStepsTable.tsx
+++ frontend/src/scenes/insights/InsightTabs/FunnelTab/FunnelStepsTable.tsx
@@ -2,12 +2,47 @@ import React from 'react'
 import { useValues } from 'kea'
 import { funnelLogic } from 'scenes/funnels/funnelLogic'
 import { insightLogic } from 'scenes/insights/insightLogic'
-import { LemonTable } from 'lib/components/LemonTable'
+import { LemonTable, LemonTableColumnGroup } from 'lib/components/LemonTable'
+import { FlattenedFunnelStep } from '~/types'
+import { EntityFilterInfo } from 'lib/components/EntityFilterInfo'
+import { getSeriesColor } from 'scenes/funnels/funnelUtils'
+import { getActionFilterFromFunnelStep } from './funnelStepTableUtils'
 
 export function FunnelStepsTable(): JSX.Element | null {
     const { insightProps } = useValues(insightLogic)
     const logic = funnelLogic(insightProps)
-    const { insightLoading } = useValues(logic)
+    const { insightLoading, steps, flattenedSteps } = useValues(logic)
 
-    return <LemonTable dataSource={[]} columns={[]} loading={insightLoading} />
+    const columnsGrouped = [
+        {
+            children: [
+                {
+                    title: 'Breakdown',
+                    dataIndex: 'breakdown_value',
+                },
+            ],
+        },
+        ...steps.map((step) => ({
+            title: <EntityFilterInfo filter={getActionFilterFromFunnelStep(step)} />,
+            children: [
+                {
+                    title: 'Completed',
+                    dataIndex: `count`,
+                },
+                {
+                    title: 'Rate',
+                    dataIndex: 'conversionRate',
+                },
+            ],
+        })),
+    ] as LemonTableColumnGroup<FlattenedFunnelStep>[]
+
+    return (
+        <LemonTable
+            dataSource={flattenedSteps}
+            columns={columnsGrouped}
+            loading={insightLoading}
+            rowRibbonColor={(series) => getSeriesColor(series?.breakdownIndex, flattenedSteps.length === 1)}
+        />
+    )
 }
