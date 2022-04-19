a03b3403ebd65175e1fa651d1ecc6bca213c6f7a
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-04-19 14:55:19 +0000

    Update frontend/src/layout/navigation/navigationLogic.ts

--- frontend/src/layout/navigation/navigationLogic.ts
+++ frontend/src/layout/navigation/navigationLogic.ts
@@ -83,7 +83,7 @@ export const navigationLogic = kea<navigationLogicType<WarningType>>({
     },
     windowValues: () => ({
         fullscreen: (window) => !!window.document.fullscreenElement,
-        mobileLayout: (window) => window.innerWidth < 992,
+        mobileLayout: (window) => window.innerWidth < 992, // Sync width threshold with Sass variable $lg!
     }),
     selectors: {
         /** `bareNav` whether the current scene should display a sidebar at all */
