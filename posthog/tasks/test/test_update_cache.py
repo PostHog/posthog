d781251b313c8311b2dff29266bf0435eaa147cf
Paul D'Ambra <paul.dambra@gmail.com>
2022-04-19 16:36:20 +0100

    fix patch reference

--- posthog/tasks/test/test_update_cache.py
+++ posthog/tasks/test/test_update_cache.py
@@ -402,7 +402,7 @@ class TestUpdateCache(APIBaseTest):
         self.assertEquals(Insight.objects.get().last_refresh.isoformat(), "2021-08-25T22:09:14.252000+00:00")
 
     @freeze_time("2021-08-25T22:09:14.252Z")
-    @patch("posthog.tasks.update_cache.dashboard_item_update_task_params")
+    @patch("posthog.tasks.update_cache.insight_update_task_params")
     def test_broken_insights(self, dashboard_item_update_task_params: MagicMock) -> None:
         # sometimes we have broken insights, add a test to catch
         dashboard = Dashboard.objects.create(team=self.team, is_shared=True)
@@ -413,7 +413,7 @@ class TestUpdateCache(APIBaseTest):
 
         self.assertEqual(dashboard_item_update_task_params.call_count, 0)
 
-    @patch("posthog.tasks.update_cache.dashboard_item_update_task_params")
+    @patch("posthog.tasks.update_cache.insight_update_task_params")
     def test_broken_exception_insights(self, dashboard_item_update_task_params: MagicMock) -> None:
         dashboard_item_update_task_params.side_effect = Exception()
         dashboard = Dashboard.objects.create(team=self.team, is_shared=True)
