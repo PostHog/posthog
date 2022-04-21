583292305ad139f43d3a22da5c186bc89b23ee8d
Marius Andra <marius.andra@gmail.com>
2022-04-21 16:14:00 +0200

    reduce some storybook build noise

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = {
     plugins: [
         '@babel/plugin-transform-runtime',
         '@babel/plugin-transform-react-jsx',
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: false }],
+        ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
         ['babel-plugin-kea', { path: './frontend/src' }],
     ],
     presets: [
