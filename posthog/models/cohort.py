69ae616d7dc412e10360e6a6b780af753691bfb5
Neil Kakkar <neilkakkar@gmail.com>
2022-04-25 13:17:11 +0100

    add migration

--- posthog/models/cohort.py
+++ posthog/models/cohort.py
@@ -76,6 +76,7 @@ class Cohort(models.Model):
     description: models.CharField = models.CharField(max_length=1000, blank=True)
     team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
     deleted: models.BooleanField = models.BooleanField(default=False)
+    filters: models.JSONField = models.JSONField(null=True)
     people: models.ManyToManyField = models.ManyToManyField("Person", through="CohortPeople")
     version: models.IntegerField = models.IntegerField(blank=True, null=True)
     pending_version: models.IntegerField = models.IntegerField(blank=True, null=True)
@@ -159,6 +160,12 @@ class Cohort(models.Model):
                     raise ValueError("Cohort group needs properties or action_id or event_id")
 
             return PropertyGroup(PropertyOperatorType.OR, property_groups)
+
+        if self.filters:
+            properties = Filter(data=self.filters.get("properties"), team=self.team).property_groups
+            if not properties.values:
+                raise ValueError("Cohort has no properties")
+            return properties
 
         raise ValueError("Cohort has no properties")
 
