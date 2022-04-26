dd7bae24ba2dd04c7f4068b2ea341ab3fd5a5c9d
eric <eeoneric@gmail.com>
2022-04-25 23:16:09 -0400

    update snapshots

--- ee/clickhouse/queries/cohort_query.py
+++ ee/clickhouse/queries/cohort_query.py
@@ -193,6 +193,7 @@ class CohortQuery(EnterpriseEventQuery):
         {q}
         WHERE 1 = 1
         {conditions}
+        SETTINGS allow_experimental_window_functions = 1
         """
 
         return final_query, self.params
