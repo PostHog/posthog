68ee1db357a49e58aa4deee84427987e7451f6c7
Alex Kim <alexgkim205@gmail.com>
2022-04-27 14:32:08 -0400

    fix: funnel breakdown baseline persons modal bug

--- frontend/src/scenes/funnels/funnelLogic.ts
+++ frontend/src/scenes/funnels/funnelLogic.ts
@@ -50,6 +50,7 @@ import {
     isValidBreakdownParameter,
     getBreakdownStepValues,
     getIncompleteConversionWindowStartDate,
+    generateBaselineConversionUrl,
 } from './funnelUtils'
 import { personsModalLogic } from 'scenes/trends/personsModalLogic'
 import { dashboardsModel } from '~/models/dashboardsModel'
@@ -809,7 +810,6 @@ export const funnelLogic = kea<funnelLogicType<openPersonsModelProps>>({
                         step.nested_breakdown.forEach((breakdownStep, i) => {
                             flattenedSteps.push({
                                 ...breakdownStep,
-                                order: step.order,
                                 breakdownIndex: i,
                                 rowKey: getVisibilityIndex(step, breakdownStep.breakdown_value),
                                 isBreakdownParent: false,
@@ -841,9 +841,14 @@ export const funnelLogic = kea<funnelLogicType<openPersonsModelProps>>({
                             ...getBreakdownStepValues(baseStep, 0, true),
                             isBaseline: true,
                             breakdownIndex: 0,
-                            steps: steps.map((s) =>
-                                Object.assign({}, s, { nested_breakdown: undefined, breakdown_value: 'Baseline' })
-                            ),
+                            steps: steps.map((s) => {
+                                return Object.assign({}, s, {
+                                    nested_breakdown: undefined,
+                                    breakdown_value: 'Baseline',
+                                    converted_people_url: generateBaselineConversionUrl(s.converted_people_url),
+                                    dropped_people_url: generateBaselineConversionUrl(s.dropped_people_url),
+                                })
+                            }),
                             conversionRates: {
                                 total: (lastStep?.count ?? 0) / (baseStep?.count ?? 1),
                             },
