2ab16b46a818752178a8819ea61ca54cb04d6fd1
Paul D'Ambra <paul@posthog.com>
2022-04-28 15:36:37 +0100

    allow null in refreshing column

--- posthog/models/dashboard_tile.py
+++ posthog/models/dashboard_tile.py
@@ -18,7 +18,7 @@ class DashboardTile(models.Model):
     # caching for this dashboard & insight filter combination
     filters_hash: models.CharField = models.CharField(max_length=400, null=True, blank=True)
     last_refresh: models.DateTimeField = models.DateTimeField(blank=True, null=True)
-    refreshing: models.BooleanField = models.BooleanField(default=False)
+    refreshing: models.BooleanField = models.BooleanField(default=False, null=True)
     refresh_attempt: models.IntegerField = models.IntegerField(null=True, blank=True)
 
     class Meta:
