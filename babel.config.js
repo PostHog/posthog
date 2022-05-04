e31ba85510e5aad75c975ca750fffda4bf702d80
Marius Andra <marius.andra@gmail.com>
2022-05-04 23:55:01 +0200

    get storybook working

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 /* global module */
 module.exports = {
     plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-transform-react-jsx',
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+        ['@babel/plugin-transform-runtime'],
+        ['@babel/plugin-transform-react-jsx'],
+        ['@babel/plugin-proposal-class-properties'],
+        ['@babel/plugin-proposal-private-property-in-object'],
     ],
     presets: [
         [
