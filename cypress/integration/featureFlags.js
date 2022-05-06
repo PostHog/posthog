49a30bdfca21d55381eb7d1114e9c1757c111fb5
Paul D'Ambra <paul@posthog.com>
2022-05-06 13:31:36 +0100

    try and avoid not always typing the first character

--- cypress/integration/featureFlags.js
+++ cypress/integration/featureFlags.js
@@ -10,7 +10,7 @@ describe('Feature Flags', () => {
         // ensure unique names to avoid clashes
         cy.get('h1').should('contain', 'Feature Flags')
         cy.get('[data-attr=new-feature-flag]').click()
-        cy.get('[data-attr=feature-flag-key]').type(name).should('have.value', name)
+        cy.get('[data-attr=feature-flag-key]').focus().type(name).should('have.value', name)
         cy.get('[data-attr=feature-flag-description]')
             .type('This is a new feature.')
             .should('have.value', 'This is a new feature.')
@@ -62,7 +62,7 @@ describe('Feature Flags', () => {
     it('Delete feature flag', () => {
         cy.get('h1').should('contain', 'Feature Flags')
         cy.get('[data-attr=new-feature-flag]').click()
-        cy.get('[data-attr=feature-flag-key]').type(name).should('have.value', name)
+        cy.get('[data-attr=feature-flag-key]').focus().type(name).should('have.value', name)
         cy.get('[data-attr=feature-flag-submit]').click()
 
         // after save there should be a delete button
