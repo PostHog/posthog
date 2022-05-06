5f7b27aaa4acd284f56885ba957abe9fa218758f
Paul D'Ambra <paul@posthog.com>
2022-05-06 10:12:12 +0100

    be more specific cypress

--- cypress/integration/dashboard.js
+++ cypress/integration/dashboard.js
@@ -17,7 +17,7 @@ describe('Dashboard', () => {
         cy.get('[data-attr="insight-save-button"]').click() // Save the insight
         cy.wait(100)
         cy.get('[data-attr="edit-prop-name"]').click({ force: true }) // Rename insight, out of view, must force
-        cy.focused().clear().type('Test Insight Zeus')
+        cy.get('[data-attr="insight-name"] input').clear().type('Test Insight Zeus')
         cy.get('button').contains('Save').click() // Save the new name
         cy.get('[data-attr="save-to-dashboard-button"]').click() // Open the Save to dashboard modal
         cy.get('button').contains('Add insight to dashboard').click() // Add the insight to a dashboard
