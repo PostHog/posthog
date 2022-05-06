65782759ac50fb1af0e9469d5e9b89c5d310bee3
Ben White <ben@posthog.com>
2022-05-06 08:05:50 +0100

    Fixed breakdownfilter to respect feature flag

--- frontend/src/scenes/insights/EditorFilters/EFTrendsBreakdown.tsx
+++ frontend/src/scenes/insights/EditorFilters/EFTrendsBreakdown.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { useActions } from 'kea'
+import { useActions, useValues } from 'kea'
 import { trendsLogic } from 'scenes/trends/trendsLogic'
 import { EditorFilterProps } from '~/types'
 import { BreakdownFilter } from 'scenes/insights/BreakdownFilter'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
+import { FEATURE_FLAGS } from 'lib/constants'
 
 export function EFTrendsBreakdown({ filters, insightProps }: EditorFilterProps): JSX.Element {
     const { setFilters } = useActions(trendsLogic(insightProps))
+    const { featureFlags } = useValues(featureFlagLogic)
 
-    return <BreakdownFilter filters={filters} setFilters={setFilters} />
+    return (
+        <BreakdownFilter
+            filters={filters}
+            setFilters={setFilters}
+            buttonType="default"
+            useMultiBreakdown={!!featureFlags[FEATURE_FLAGS.BREAKDOWN_BY_MULTIPLE_PROPERTIES]}
+        />
+    )
 }
