89fb44bdda6eeebe85af1d0f2fdd3729e766090e
Paul D'Ambra <paul@posthog.com>
2022-05-06 13:27:41 +0100

    find save button differently

--- cypress/integration/dashboard.js
+++ cypress/integration/dashboard.js
@@ -19,7 +19,8 @@ describe('Dashboard', () => {
         cy.get('[data-attr="edit-prop-name"]').click({ force: true }) // Rename insight, out of view, must force
 
         cy.get('[data-attr="insight-name"].EditableField--editing input').type('Test Insight Zeus')
-        cy.get('button').contains('Save').click() // Save the new name
+        // Save the new name
+        cy.get('[data-attr="insight-name"].EditableField--editing button').contains('Save').click()
 
         cy.get('[data-attr="save-to-dashboard-button"]').click() // Open the Save to dashboard modal
         cy.get('button').contains('Add insight to dashboard').click() // Add the insight to a dashboard
