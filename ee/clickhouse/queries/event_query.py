897f94b7079988777d5619b62497137178b7110f
James Greenhill <fuziontech@gmail.com>
2022-05-06 14:57:43 -0700

    cleaning up merge (was very sloppy)

--- ee/clickhouse/queries/event_query.py
+++ ee/clickhouse/queries/event_query.py
@@ -8,19 +8,14 @@ from posthog.models.filters.path_filter import PathFilter
 from posthog.models.filters.retention_filter import RetentionFilter
 from posthog.models.filters.session_recordings_filter import SessionRecordingsFilter
 from posthog.models.filters.stickiness_filter import StickinessFilter
+from posthog.models.property import PropertyName
 from posthog.models.team import Team
 from posthog.queries.event_query import EventQuery
 
 
 class EnterpriseEventQuery(EventQuery):
 
-    _filter: Union[Filter, PathFilter, RetentionFilter, StickinessFilter, SessionRecordingsFilter]
-    _team_id: int
     _column_optimizer: EnterpriseColumnOptimizer
-    _should_join_distinct_ids = False
-    _should_join_persons = False
-    _should_round_interval = False
-    _extra_fields: List[ColumnName]
 
     def __init__(
         self,
@@ -31,11 +26,11 @@ class EnterpriseEventQuery(EventQuery):
         should_join_persons=False,
         # Extra events/person table columns to fetch since parent query needs them
         extra_fields: List[ColumnName] = [],
+        extra_event_properties: List[PropertyName] = [],
         extra_person_fields: List[ColumnName] = [],
         override_aggregate_users_by_distinct_id: Optional[bool] = None,
         **kwargs,
     ) -> None:
-
         super().__init__(
             filter=filter,
             team=team,
@@ -43,6 +38,7 @@ class EnterpriseEventQuery(EventQuery):
             should_join_distinct_ids=should_join_distinct_ids,
             should_join_persons=should_join_persons,
             extra_fields=extra_fields,
+            extra_event_properties=extra_event_properties,
             extra_person_fields=extra_person_fields,
             override_aggregate_users_by_distinct_id=override_aggregate_users_by_distinct_id,
             **kwargs,
