498b2a6df9040a23987a969fa5427d11aeb37e71
Paul D'Ambra <paul@posthog.com>
2022-05-06 12:37:37 +0100

    try and avoid cypress deciding that a visible field is not valid

--- cypress/integration/dashboard.js
+++ cypress/integration/dashboard.js
@@ -12,13 +12,15 @@ describe('Dashboard', () => {
         cy.get('[data-attr=breadcrumb-2]').should('have.text', 'Dashboards')
     })
 
-    it('Adding new insight to dashboard works', () => {
+    it.only('Adding new insight to dashboard works', () => {
         cy.get('[data-attr=menu-item-insight]').click() // Create a new insight
         cy.get('[data-attr="insight-save-button"]').click() // Save the insight
         cy.wait(100)
         cy.get('[data-attr="edit-prop-name"]').click({ force: true }) // Rename insight, out of view, must force
-        cy.get('[data-attr="insight-name"] input').clear().type('Test Insight Zeus')
+
+        cy.get('[data-attr="insight-name"].EditableField--editing input').type('Test Insight Zeus')
         cy.get('button').contains('Save').click() // Save the new name
+
         cy.get('[data-attr="save-to-dashboard-button"]').click() // Open the Save to dashboard modal
         cy.get('button').contains('Add insight to dashboard').click() // Add the insight to a dashboard
         cy.get('[data-attr="save-to-dashboard-button"]').click() // Go to the dashboard
