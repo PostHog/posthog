76606830fa19bd694cc71ba6bb9cb4995845b450
Marius Andra <marius.andra@gmail.com>
2022-05-08 16:12:33 +0200

    upgrade packages, replace subscriptionsPlugin

--- frontend/src/initKea.ts
+++ frontend/src/initKea.ts
@@ -6,7 +6,7 @@ import { windowValuesPlugin } from 'kea-window-values'
 import { identifierToHuman } from 'lib/utils'
 import { waitForPlugin } from 'kea-waitfor'
 import { lemonToast } from 'lib/components/lemonToast'
-import { subscriptionsPlugin } from '~/subscriptionsPlugin'
+import { subscriptionsPlugin } from 'kea-subscriptions'
 import { formsPlugin } from 'kea-forms'
 
 /*
