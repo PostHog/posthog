1930cbf2c11fd63c4c7ac5818a69e0e3193a6b6d
Paul D'Ambra <paul@posthog.com>
2022-05-09 20:31:35 +0100

    use headers not MEta

--- posthog/test/test_utils.py
+++ posthog/test/test_utils.py
@@ -134,11 +134,10 @@ class TestDefaultEventName(BaseTest):
 class TestLoadDataFromRequest(TestCase):
     def _create_request_with_headers(self, origin: str, referer: str) -> WSGIRequest:
         rf = RequestFactory()
-        post_request = rf.post("/e/?ver=1.20.0", "content", "text/plain")
         # the server presents any http headers in upper case with http_ as a prefix
         # see https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpRequest.META
-        post_request.META["HTTP_ORIGIN"] = origin
-        post_request.META["HTTP_REFERER"] = referer
+        headers = {"HTTP_ORIGIN": origin, "HTTP_REFERER": referer}
+        post_request = rf.post("/e/?ver=1.20.0", "content", "text/plain", False, **headers)
         return post_request
 
     @patch("posthog.utils.configure_scope")
