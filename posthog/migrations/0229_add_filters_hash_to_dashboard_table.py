0005872940e08aa98c34f7be79cc5fd2b73a1afe
Marius Andra <marius.andra@gmail.com>
2022-05-10 13:22:54 +0200

    fix migration

--- posthog/migrations/0229_add_filters_hash_to_dashboard_table.py
+++ posthog/migrations/0229_add_filters_hash_to_dashboard_table.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("posthog", "0230_cohort_filters"),
+        ("posthog", "0228_fix_tile_layouts"),
     ]
 
     operations = [
