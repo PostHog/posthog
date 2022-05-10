ad50669d769ddc617fffcb641041412b40bde91a
Michael Matloka <dev@twixes.com>
2022-05-10 15:17:14 +0200

    Update dry_run_matrix.py

--- posthog/management/commands/dry_run_matrix.py
+++ posthog/management/commands/dry_run_matrix.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from time import time
 
 from django.core.management.base import BaseCommand
 
@@ -10,17 +11,27 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--seed", type=str, help="Simulation seed for deterministic output")
+        parser.add_argument("--n-clusters", type=int, default=1, help="Number of clusters")
 
     def handle(self, *args, **options):
+        timer = time()
         matrix = HedgeboxMatrix(
-            options["seed"], start=dt.datetime(2022, 2, 1), end=dt.datetime(2022, 5, 1), n_clusters=1
+            options["seed"],
+            start=dt.datetime(2022, 2, 1),
+            end=dt.datetime(2022, 5, 1),
+            n_clusters=options["n_clusters"],
         )
         matrix.simulate()
-        seen_people = [person for person in matrix.persons if person.first_seen_at is not None]
-        print(f"Simulated {len(seen_people)} person{'s' if len(seen_people) != 1 else ''} with events")
-        for person in seen_people:
-            print()
-            print(person)
-            for event in person.events:
-                print(event)
-            print()
+        duration = time() - timer
+        for cluster in matrix.clusters:
+            print(
+                f"Cluster {cluster}. Radius = {cluster.radius}. Population = {len(cluster.people_matrix) * len(cluster.people_matrix[0])}."
+            )
+            for y, person_row in enumerate(cluster.people_matrix):
+                for x, person in enumerate(person_row):
+                    print(f"    Person {x, y}: {person}")
+                    for event in person.events:
+                        print(f"        {event}")
+        print(
+            f"All in all, simulated {len(matrix.people)} {'person' if len(matrix.people) == 1 else 'people'} within {len(matrix.clusters)} cluster{'' if len(matrix.clusters) == 1 else 's'} in {round(duration * 1000)} ms."
+        )
