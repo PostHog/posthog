690bf3d5d8b13443d6ca82de4079ba34aada5557
Paul D'Ambra <paul@posthog.com>
2022-05-11 12:41:55 +0100

    fix: use filter row key that changes with data changes

--- frontend/src/lib/components/PropertyFilters/PropertyFilters.tsx
+++ frontend/src/lib/components/PropertyFilters/PropertyFilters.tsx
@@ -61,7 +61,7 @@ export function PropertyFilters({
                 {filtersWithNew.map((item: AnyPropertyFilter, index: number) => {
                     return (
                         <FilterRow
-                            key={index}
+                            key={JSON.stringify(item)}
                             item={item}
                             index={index}
                             totalCount={filtersWithNew.length - 1} // empty state
