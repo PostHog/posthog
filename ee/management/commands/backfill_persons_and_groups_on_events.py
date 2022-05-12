6317277409415f156f520a3f8ae8ef8baa4e0021
yakkomajuri <yakko.majuri@gmail.com>
2022-05-12 16:52:58 +0000

    cleanup + query id feature

--- ee/management/commands/backfill_persons_and_groups_on_events.py
+++ ee/management/commands/backfill_persons_and_groups_on_events.py
@@ -1,9 +1,15 @@
+from datetime import datetime
+from time import sleep
+from typing import Any
+from uuid import uuid4
+
 import structlog
 from django.core.management.base import BaseCommand
 
 from ee.clickhouse.sql.events import EVENTS_DATA_TABLE
 from ee.clickhouse.sql.groups import GROUPS_TABLE
 from ee.clickhouse.sql.person import PERSON_DISTINCT_ID2_TABLE, PERSONS_TABLE
+from posthog import client
 from posthog.client import sync_execute
 from posthog.settings import CLICKHOUSE_CLUSTER, CLICKHOUSE_DATABASE
 
@@ -30,11 +36,17 @@ Also note that minor inconsitencies can occur from distinct IDs and properties c
 3. Drop the dictionaries
 """
 
+
 logger = structlog.get_logger(__name__)
 
-GROUPS_DICT_TABLE_NAME = f"{GROUPS_TABLE}_dict"
-PERSONS_DICT_TABLE_NAME = f"{PERSONS_TABLE}_dict"
-PERSON_DISTINCT_IDS_DICT_TABLE_NAME = f"{PERSON_DISTINCT_ID2_TABLE}_dict"
+backfill_query_id = str(uuid4())
+
+now = datetime.now()
+dict_suffix = now.strftime("%Y_%m_%d__%H_%M_%S")
+
+GROUPS_DICT_TABLE_NAME = f"{GROUPS_TABLE}_dict_{dict_suffix}"
+PERSONS_DICT_TABLE_NAME = f"{PERSONS_TABLE}_dict_{dict_suffix}"
+PERSON_DISTINCT_IDS_DICT_TABLE_NAME = f"{PERSON_DISTINCT_ID2_TABLE}_dict_{dict_suffix}"
 
 GROUPS_DICTIONARY_SQL = f"""
 CREATE DICTIONARY IF NOT EXISTS {GROUPS_DICT_TABLE_NAME}
@@ -76,7 +88,7 @@ LAYOUT(cache(size_in_cells 1000))
 Lifetime(60000)
 """
 
-BACKFILL_BASE_SQL = f"""
+BACKFILL_SQL = f"""
 ALTER TABLE {EVENTS_DATA_TABLE()}
 ON CLUSTER '{CLICKHOUSE_CLUSTER}'
 UPDATE
@@ -87,6 +99,17 @@ UPDATE
     group2_properties=dictGetString('{GROUPS_DICT_TABLE_NAME}', 'group_properties', $group_2),
     group3_properties=dictGetString('{GROUPS_DICT_TABLE_NAME}', 'group_properties', $group_3),
     group4_properties=dictGetString('{GROUPS_DICT_TABLE_NAME}', 'group_properties', $group_4)
+WHERE team_id = %(team_id)s
+"""
+
+GET_QUERY_ID_SQL = f"""
+SELECT query_id
+FROM system.query_log
+WHERE
+    query_start_time > (now() - INTERVAL 10 MINUTE) AND
+    query LIKE '%backfill:{backfill_query_id}%' AND
+    query NOT LIKE '%query_log%'
+LIMIT 1
 """
 
 DROP_GROUPS_DICTIONARY_SQL = f"DROP DICTIONARY IF EXISTS {GROUPS_DICT_TABLE_NAME}"
@@ -96,7 +119,7 @@ DROP_PERSONS_DICTIONARY_SQL = f"DROP DICTIONARY IF EXISTS {PERSONS_DICT_TABLE_NA
 query_number = 0
 
 
-def print_and_execute_query(sql, name, dry_run, timeout=180):
+def print_and_execute_query(sql: str, name: str, dry_run: bool, timeout=180, query_args={}) -> Any:
     global query_number
 
     print(f"> {query_number}. {name}", end="\n\n")
@@ -106,7 +129,10 @@ def print_and_execute_query(sql, name, dry_run, timeout=180):
     query_number = query_number + 1
 
     if not dry_run:
-        sync_execute(sql, {"max_execution_time": timeout, "max_memory_usage": 1000000000000})
+        res = sync_execute(sql, settings={"max_execution_time": timeout}, args=query_args)
+        return res
+
+    return None
 
 
 class Command(BaseCommand):
@@ -114,11 +140,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
 
-        parser.add_argument("--team-id", default=None, type=int, help="Specify a team to backfill data for.")
-
-        parser.add_argument(
-            "--timeout", default=1000, type=int, help="ClickHouse max_execution_time setting (seconds)."
-        )
+        parser.add_argument("--team-id", default=None, type=str, help="Specify a team to backfill data for.")
 
         parser.add_argument(
             "--live-run", action="store_true", help="Opts out of default 'dry run' mode and actually runs the queries."
@@ -130,8 +152,6 @@ class Command(BaseCommand):
             logger.error("You must specify --team-id to run this script")
             exit(1)
 
-        BACKFILL_SQL = BACKFILL_BASE_SQL + " WHERE team_id = {team_id}".format(team_id=options["team_id"])
-
         dry_run = not options["live_run"]
 
         if dry_run:
@@ -140,9 +160,26 @@ class Command(BaseCommand):
         print_and_execute_query(GROUPS_DICTIONARY_SQL, "GROUPS_DICTIONARY_SQL", dry_run)
         print_and_execute_query(PERSON_DISTINCT_IDS_DICTIONARY_SQL, "PERSON_DISTINCT_IDS_DICTIONARY_SQL", dry_run)
         print_and_execute_query(PERSONS_DICTIONARY_SQL, "PERSONS_DICTIONARY_SQL", dry_run)
-        print_and_execute_query(BACKFILL_SQL, "BACKFILL_SQL", dry_run, options["timeout"])
+
+        client._request_information = {"kind": "backfill", "id": backfill_query_id}
+
+        print_and_execute_query(
+            BACKFILL_SQL, "BACKFILL_SQL", dry_run, 0, {"team_id": options["team_id"], "id": backfill_query_id}
+        )
+        client._request_information = None
+
         print_and_execute_query(DROP_GROUPS_DICTIONARY_SQL, "DROP_GROUPS_DICTIONARY_SQL", dry_run)
         print_and_execute_query(
             DROP_PERSON_DISTINCT_IDS_DICTIONARY_SQL, "DROP_PERSON_DISTINCT_IDS_DICTIONARY_SQL", dry_run
         )
         print_and_execute_query(DROP_PERSONS_DICTIONARY_SQL, "DROP_PERSONS_DICTIONARY_SQL", dry_run)
+
+        sleep(10)
+        query_id_res = print_and_execute_query(GET_QUERY_ID_SQL, "GET_QUERY_ID_SQL", dry_run)
+
+        if query_id_res:
+            query_id = query_id_res[0][0]
+            print()
+            print(
+                f"Backfill running. Cancel backfill by running:\n`KILL QUERY ON CLUSTER {CLICKHOUSE_CLUSTER} WHERE query_id='{query_id}'`"
+            )
