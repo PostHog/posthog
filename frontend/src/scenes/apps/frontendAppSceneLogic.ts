9cec0d2c8b534e1376610d1717ff9e91333a4e17
Marius Andra <marius.andra@gmail.com>
2022-05-12 15:16:19 +0200

    update api config

--- frontend/src/scenes/apps/frontendAppSceneLogic.ts
+++ frontend/src/scenes/apps/frontendAppSceneLogic.ts
@@ -1,31 +1,65 @@
-import { BuiltLogic, connect, kea, key, LogicWrapper, path, props, selectors } from 'kea'
+import { actions, BuiltLogic, connect, kea, key, listeners, LogicWrapper, props, selectors, path } from 'kea'
 import { frontendAppsLogic } from 'scenes/apps/frontendAppsLogic'
 import { FrontendApp } from '~/types'
 import type { frontendAppSceneLogicType } from './frontendAppSceneLogicType'
 import { subscriptions } from 'kea-subscriptions'
 import { objectsEqual } from 'lib/utils'
 import { urls } from 'scenes/urls'
-import { FrontendAppSceneLogicProps, FrontendAppSceneProps } from 'scenes/apps/types'
+import { FrontendAppConfig, FrontendAppSceneLogicProps, FrontendAppSceneProps } from 'scenes/apps/types'
+import api from 'lib/api'
 
 /** Logic responsible for loading the injected frontend scene */
 export const frontendAppSceneLogic = kea<frontendAppSceneLogicType>([
-    path(['scenes', 'plugins', 'Frontend']),
-    connect({ values: [frontendAppsLogic, ['frontendApps', 'appConfigs']] }),
+    path(['scenes', 'apps', 'frontendAppSceneLogic']),
     props({} as FrontendAppSceneLogicProps),
     key((props) => props.id),
-    selectors({
+    connect({
+        actions: [frontendAppsLogic, ['setAppConfig']],
+        values: [frontendAppsLogic, ['frontendApps', 'appConfigs']],
+    }),
+    actions({
+        updateAppConfig: (appConfig: Record<string, any>, callback: (error?: any) => void) => ({ appConfig, callback }),
+    }),
+    listeners(({ props }) => ({
+        updateAppConfig: async ({ appConfig, callback }) => {
+            try {
+                // TODO: this doesn't work yet
+                const response = await api.update(`api/plugin_config/${props.id}/`, {
+                    config: appConfig,
+                })
+                console.log(response)
+                callback()
+            } catch (e) {
+                callback(e)
+            }
+        },
+    })),
+    selectors(({ actions }) => ({
         frontendApp: [
             (s) => [s.frontendApps, (_, props) => props.id],
             (frontendApps, id): FrontendApp => frontendApps[id],
         ],
         appConfig: [
             (s) => [s.appConfigs, (_, props) => props.id],
-            (appConfigs, id): Record<string, any> => appConfigs[id],
+            (appConfigs, id): FrontendAppConfig => appConfigs[id],
         ],
         logic: [(s) => [s.frontendApp], (frontendApp): LogicWrapper | undefined => frontendApp?.logic],
         logicProps: [
             (s) => [(_, props) => props.id, s.appConfig],
-            (id, appConfig): FrontendAppSceneProps => ({ id, url: urls.frontendApp(id), config: appConfig }),
+            (id, appConfig): FrontendAppSceneProps => ({
+                ...appConfig,
+                id,
+                name: appConfig.name,
+                url: urls.frontendApp(id),
+                config: appConfig.config,
+                setConfig: async (config: Record<string, any>) => {
+                    return new Promise((resolve, reject) => {
+                        actions.updateAppConfig(config, (error) => {
+                            error ? reject(error) : resolve()
+                        })
+                    })
+                },
+            }),
         ],
         builtLogic: [(s) => [s.logic, s.logicProps], (logic: any, props: any) => logic?.(props)],
         Component: [(s) => [s.frontendApp], (frontendApp: any) => frontendApp?.component],
@@ -43,7 +77,7 @@ export const frontendAppSceneLogic = kea<frontendAppSceneLogicType>([
             },
             { resultEqualityCheck: objectsEqual },
         ],
-    }),
+    })),
     subscriptions(({ cache }) => ({
         builtLogic: (builtLogic: BuiltLogic) => {
             cache.unmount?.()
