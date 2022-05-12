db2682043cd63eae7903fd32249b9efa1a631ed8
Paul D'Ambra <paul@posthog.com>
2022-05-12 19:54:09 +0100

    make input always be a string

--- frontend/src/lib/components/PropertyFilters/components/PropertyValue.tsx
+++ frontend/src/lib/components/PropertyFilters/components/PropertyValue.tsx
@@ -73,16 +73,19 @@ export function PropertyValue({
     const isMultiSelect = operator && isOperatorMulti(operator)
     const isDateTimeProperty = operator && isOperatorDate(operator)
 
-    const [input, setInput] = useState(isMultiSelect ? '' : toString(value))
-    const [shouldBlur, setShouldBlur] = useState(false)
+    // what the human has typed into the box
+    const [input, setInput] = useState(Array.isArray(value) ? '' : toString(value) ?? '')
+    // options from the server for search
     const [options, setOptions] = useState({} as Record<string, Option>)
+
+    const [shouldBlur, setShouldBlur] = useState(false)
     const autoCompleteRef = useRef<HTMLElement>(null)
 
     const { formatForDisplay } = useValues(propertyDefinitionsModel)
 
     // update the input field if passed a new `value` prop
     useEffect(() => {
-        if (!value) {
+        if (value == null) {
             setInput('')
         } else if (value !== input) {
             const valueObject = options[propertyKey]?.values?.find((v) => v.id === value)
