96268972e19fdd346a26fec5562f14abab05ed3a
eric <eeoneric@gmail.com>
2022-05-16 10:12:56 -0400

    fix: add frontend condition

--- frontend/src/scenes/cohorts/Cohort.tsx
+++ frontend/src/scenes/cohorts/Cohort.tsx
@@ -82,7 +82,7 @@ export function Cohort({ id }: { id?: CohortType['id'] } = {}): JSX.Element {
                                 type="primary"
                                 data-attr="save-cohort"
                                 htmlType="submit"
-                                loading={cohortLoading}
+                                loading={cohortLoading || cohort.is_calculating}
                             >
                                 Save
                             </LemonButton>
