2cac16cc0c447f82a7dd18db304d17f6b68af1f9
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-05-17 12:43:55 +0300

    Refactor method location, WIP

--- plugin-server/src/worker/ingestion/event-pipeline/determineShouldBufferStep.ts
+++ plugin-server/src/worker/ingestion/event-pipeline/determineShouldBufferStep.ts
@@ -1,20 +1,51 @@
-import { PreIngestionEvent } from '../../../types'
-import { shouldSendEventToBuffer } from '../ingest-event'
+import { DateTime } from 'luxon'
+
+import { Hub, Person, PreIngestionEvent, TeamId } from '../../../types'
 import { EventPipelineRunner, StepResult } from './runner'
 
 export async function determineShouldBufferStep(
     runner: EventPipelineRunner,
-    event: PreIngestionEvent
+    event: PreIngestionEvent,
+    shouldBuffer: (
+        hub: Hub,
+        event: PreIngestionEvent,
+        person: Person | undefined,
+        teamId: TeamId
+    ) => boolean = shouldSendEventToBuffer
 ): Promise<StepResult> {
     const person = await runner.hub.db.fetchPerson(event.teamId, event.distinctId)
 
-    // even if the buffer is disabled we want to get metrics on how many events would have gone to it
-    const sendEventToBuffer = shouldSendEventToBuffer(runner.hub, event, person, event.teamId)
-
-    if (sendEventToBuffer) {
+    if (shouldBuffer(runner.hub, event, person, event.teamId)) {
         await runner.hub.eventsProcessor.produceEventToBuffer(event)
         return null
     } else {
         return runner.nextStep('createEventStep', event, person)
     }
+}
+
+// context: https://github.com/PostHog/posthog/issues/9182
+// TL;DR: events from a recently created non-anonymous person are sent to a buffer
+// because their person_id might change. We merge based on the person_id of the anonymous user
+// so ingestion is delayed for those events to increase our chances of getting person_id correctly
+export function shouldSendEventToBuffer(
+    hub: Hub,
+    event: PreIngestionEvent,
+    person: Person | undefined,
+    teamId: TeamId
+): boolean {
+    const isAnonymousEvent =
+        event.properties && event.properties['$device_id'] && event.distinctId === event.properties['$device_id']
+    const isRecentPerson = !person || DateTime.now().diff(person.created_at).seconds < hub.BUFFER_CONVERSION_SECONDS
+    const ingestEventDirectly = isAnonymousEvent || event.event === '$identify' || !isRecentPerson
+    const sendToBuffer = !ingestEventDirectly
+
+    if (sendToBuffer) {
+        hub.statsd?.increment('conversion_events_buffer_size', { teamId: event.teamId.toString() })
+    }
+
+    if (!hub.CONVERSION_BUFFER_ENABLED && !hub.conversionBufferEnabledTeams.has(teamId)) {
+        return false
+    }
+
+    return sendToBuffer
 }
