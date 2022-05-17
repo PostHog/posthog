865f7913ec51b013fda74a0804acc00ce7d62d71
Li Yi Yu <li@posthog.com>
2022-05-17 19:52:42 -0400

    sidebar

---
+++ frontend/src/scenes/ingestion/Sidebar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import posthogLogo from 'public/posthog-logo.png'
+import { ingestionLogic } from './ingestionLogic'
+import { useValues } from 'kea'
+import './IngestionWizard.scss'
+import { InviteMembersButton } from '~/layout/navigation/TopBar/SitePopover'
+
+export function Sidebar(): JSX.Element {
+    const { index } = useValues(ingestionLogic)
+
+    return (
+        <div style={{ background: 'white', width: 300, display: 'flex', flexDirection: 'column' }}>
+            <div style={{ padding: '2em' }}>
+                <img src={posthogLogo} style={{ width: 157, height: 30 }} />
+            </div>
+            <div className="text-muted-alt" style={{ fontSize: 14, padding: '2em' }}>
+                <b>
+                    <div className={`mb ${index === 0 && 'ingestion-current-nav-step'}`}>
+                        {index === 0 && <div className="step-indicator" />}
+                        <span>Get started</span>
+                    </div>
+                    <div className="mb">Connect your product</div>
+                    <div className="mb">Listen for events</div>
+                </b>
+            </div>
+            <div className="sidebar-bottom">
+                <InviteMembersButton />
+                <div className="sidebar-help">
+                    <a style={{ marginBottom: '1.5em' }}>Get support on Slack</a>
+                    <a>Read our documentation</a>
+                </div>
+            </div>
+        </div>
+    )
+}
