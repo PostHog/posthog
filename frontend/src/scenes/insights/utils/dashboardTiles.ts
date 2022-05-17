ccce9abc73fb47266a07f056b4b8d0bef22ee431
Paul D'Ambra <paul@posthog.com>
2022-05-17 12:51:06 +0100

    resolve naming nit

--- frontend/src/scenes/insights/utils/dashboardTiles.ts
+++ frontend/src/scenes/insights/utils/dashboardTiles.ts
@@ -1,28 +1,17 @@
 import { DashboardTile, InsightModel } from '~/types'
 
-/**
- * When changing an InsightModel there is some data that applied no matter where the insight is displayed or changed
- * And other data that varies depending on whether the insight is on a dashboard and which dashboard it is on
- * This is a convenience method to ensure that when receiving changes to an insight
- * the correct dashboard specific data is kept
- *
- * returns an updated copy of the target
- *
- * @param target the InsightModel to update
- * @param source the Insight data that is within a particular dashboard context
- */
 export function mergeWithDashboardTile<T extends InsightModel | Partial<InsightModel>>(
-    target: T,
-    source: DashboardTile
+    insight: T,
+    tile: DashboardTile
 ): T {
-    const updatedTarget = { ...target }
+    const updatedTarget = { ...insight }
 
-    updatedTarget.result = source.result || []
-    updatedTarget.layouts = source.layouts || {}
-    updatedTarget.color = source.color || null
-    updatedTarget.last_refresh = source.last_refresh || null
-    updatedTarget.filters = source.filters
-    updatedTarget.filters_hash = source.filters_hash || ''
+    updatedTarget.result = tile.result || []
+    updatedTarget.layouts = tile.layouts || {}
+    updatedTarget.color = tile.color || null
+    updatedTarget.last_refresh = tile.last_refresh || null
+    updatedTarget.filters = tile.filters
+    updatedTarget.filters_hash = tile.filters_hash || ''
 
     return updatedTarget
 }
