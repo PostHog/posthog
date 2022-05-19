2fcc6d8cd90b4707021ff8cb58f44da98b207f1d
Marius Andra <marius.andra@gmail.com>
2022-05-19 14:58:36 +0200

    fix merge

--- posthog/migrations/0235_plugin_source_transpilation.py
+++ posthog/migrations/0235_plugin_source_transpilation.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("posthog", "0233_plugin_source_file"),
+        ("posthog", "0234_create_plugin_jsons"),
     ]
 
     operations = [
