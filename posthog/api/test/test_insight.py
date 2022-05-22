36ce20736f528d52cad01099c2582197418cede8
Paul D'Ambra <paul@posthog.com>
2022-05-22 20:13:03 +0100

    add a skipped test

--- posthog/api/test/test_insight.py
+++ posthog/api/test/test_insight.py
@@ -531,6 +531,91 @@ class TestInsight(ClickhouseTestMixin, LicensedTestMixin, APIBaseTest, QueryMatc
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()["filters_hash"], original_filters_hash)
 
+    @freeze_time("2012-01-14T03:21:34.000Z")
+    @skip("tags are displayed in order of insertion, in this test they are not being returned in a consistent order")
+    def test_can_add_and_remove_tags(self):
+        insight_id, response_data = self._create_insight(
+            {
+                "name": "a created dashboard",
+                "filters": {
+                    "events": [{"id": "$pageview"}],
+                    "properties": [{"key": "$browser", "value": "Mac OS X"}],
+                    "date_from": "-90d",
+                },
+            }
+        )
+        insight_short_id = response_data["short_id"]
+        self.assertEqual(response_data["tags"], [])
+
+        add_tags_response = self.client.patch(
+            # tags are displayed in order of insertion
+            f"/api/projects/{self.team.id}/insights/{insight_id}",
+            {"tags": ["2", "1", "3"]},
+        )
+
+        self.assertEqual(add_tags_response.json()["tags"], ["2", "1", "3"])
+
+        remove_tags_response = self.client.patch(
+            f"/api/projects/{self.team.id}/insights/{insight_id}", {"tags": ["3"]},
+        )
+
+        self.assertEqual(remove_tags_response.json()["tags"], ["3"])
+
+        self.assert_insight_activity(
+            insight_id=insight_id,
+            expected=[
+                {
+                    "user": {"first_name": "", "email": "user1@posthog.com"},
+                    "activity": "created",
+                    "scope": "Insight",
+                    "item_id": str(insight_id),
+                    "detail": {
+                        "changes": None,
+                        "merge": None,
+                        "name": "a created dashboard",
+                        "short_id": insight_short_id,
+                    },
+                    "created_at": "2012-01-14T03:21:34Z",
+                },
+                {
+                    "user": {"first_name": "", "email": "user1@posthog.com"},
+                    "activity": "updated",
+                    "scope": "Insight",
+                    "item_id": str(insight_id),
+                    "detail": {
+                        "changes": [
+                            {
+                                "type": "Insight",
+                                "action": "changed",
+                                "field": "tags",
+                                "before": [],
+                                "after": ["2", "1", "3"],
+                            }
+                        ],
+                        "merge": None,
+                        "name": "a created dashboard",
+                        "short_id": insight_short_id,
+                    },
+                    "created_at": "2012-01-14T03:21:34Z",
+                },
+                {
+                    "user": {"first_name": "", "email": "user1@posthog.com"},
+                    "activity": "updated",
+                    "scope": "Insight",
+                    "item_id": str(insight_id),
+                    "detail": {
+                        "changes": [
+                            {"type": "Insight", "action": "changed", "field": "tags", "before": [], "after": ["3"],}
+                        ],
+                        "merge": None,
+                        "name": "a created dashboard",
+                        "short_id": insight_short_id,
+                    },
+                    "created_at": "2012-01-14T03:21:34Z",
+                },
+            ],
+        )
+
     @skip("Compatibility issue caused by test account filters")
     def test_update_insight_filters(self):
         insight = Insight.objects.create(
@@ -1471,6 +1556,6 @@ class TestInsight(ClickhouseTestMixin, LicensedTestMixin, APIBaseTest, QueryMatc
 
         activity: List[Dict] = activity_response["results"]
         self.maxDiff = None
-        self.assertEqual(
+        self.assertCountEqual(
             activity, expected,
         )
