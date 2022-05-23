5168d720ffafa714a6c7aa8198c32bf9f6ffb8a6
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-05-23 18:09:06 +0000

    refactor: yeet handleAlert (#9918)
    * refactor: yeet handleAlert
    
    * YEET

--- plugin-server/src/types.ts
+++ plugin-server/src/types.ts
@@ -408,7 +408,6 @@ export type VMMethods = {
     onSnapshot?: (event: ProcessedPluginEvent) => Promise<void>
     exportEvents?: (events: PluginEvent[]) => Promise<void>
     processEvent?: (event: PluginEvent) => Promise<PluginEvent>
-    handleAlert?: (alert: Alert) => Promise<void>
 }
 
 export enum AlertLevel {
@@ -905,7 +904,7 @@ export interface EventPropertyType {
     team_id: number
 }
 
-export type PluginFunction = 'onEvent' | 'onAction' | 'processEvent' | 'onSnapshot' | 'pluginTask' | 'handleAlert'
+export type PluginFunction = 'onEvent' | 'onAction' | 'processEvent' | 'onSnapshot' | 'pluginTask'
 
 export enum CeleryTriggeredJobOperation {
     Start = 'start',
