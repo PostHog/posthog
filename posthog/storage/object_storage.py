230071902179ca58027d23880f101a4aca32a424
Paul D'Ambra <paul@posthog.com>
2022-05-23 15:23:10 +0100

    only try to create storage client if storage is enabled

--- posthog/storage/object_storage.py
+++ posthog/storage/object_storage.py
@@ -7,6 +7,7 @@ logger = structlog.get_logger(__name__)
 from posthog.settings import (
     OBJECT_STORAGE_ACCESS_KEY_ID,
     OBJECT_STORAGE_BUCKET,
+    OBJECT_STORAGE_ENABLED,
     OBJECT_STORAGE_ENDPOINT,
     OBJECT_STORAGE_SECRET_ACCESS_KEY,
 )
@@ -18,22 +19,24 @@ s3_client = None
 # noinspection PyMissingTypeHints
 def storage_client():
     global s3_client
-    if not s3_client:
+    if OBJECT_STORAGE_ENABLED and not s3_client:
         s3_client = client(
             "s3",
             endpoint_url=OBJECT_STORAGE_ENDPOINT,
             aws_access_key_id=OBJECT_STORAGE_ACCESS_KEY_ID,
             aws_secret_access_key=OBJECT_STORAGE_SECRET_ACCESS_KEY,
-            config=Config(signature_version="s3v4"),
+            config=Config(signature_version="s3v4", connect_timeout=1, retries={"max_attempts": 1}),
             region_name="us-east-1",
         )
+
     return s3_client
 
 
 def health_check() -> bool:
     # noinspection PyBroadException
     try:
-        response = storage_client().head_bucket(Bucket=OBJECT_STORAGE_BUCKET)
+        client = storage_client()
+        response = client.head_bucket(Bucket=OBJECT_STORAGE_BUCKET) if client else False
         return bool(response)
     except Exception as e:
         logger.warn("object_storage.health_check_failed", error=e)
