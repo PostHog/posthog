9a03a7f413664d6ebdbc3c84d029e50ceed6db6a
Harry Waye <harry@posthog.com>
2022-05-24 13:53:56 +0000

    Move KAFKA_PREFIX

--- ee/kafka_client/topics.py
+++ ee/kafka_client/topics.py
@@ -1,19 +1,19 @@
 # Keep this in sync with plugin-server/src/config/kafka-topics.ts
 
-import os
-
 from posthog.settings import TEST
+from posthog.settings.data_stores import KAFKA_PREFIX
 
 suffix = "_test" if TEST else ""
-prefix = os.getenv("KAFKA_PREFIX", "")
 
-KAFKA_EVENTS_PLUGIN_INGESTION: str = f"{prefix}events_plugin_ingestion{suffix}"  # can be overridden in settings.py
-KAFKA_EVENTS = f"{prefix}clickhouse_events_proto{suffix}"
-KAFKA_EVENTS_JSON = f"{prefix}clickhouse_events_json{suffix}"
-KAFKA_PERSON = f"{prefix}clickhouse_person{suffix}"
-KAFKA_PERSON_UNIQUE_ID = f"{prefix}clickhouse_person_unique_id{suffix}"
-KAFKA_PERSON_DISTINCT_ID = f"{prefix}clickhouse_person_distinct_id{suffix}"
-KAFKA_SESSION_RECORDING_EVENTS = f"{prefix}clickhouse_session_recording_events{suffix}"
-KAFKA_PLUGIN_LOG_ENTRIES = f"{prefix}plugin_log_entries{suffix}"
-KAFKA_DEAD_LETTER_QUEUE = f"{prefix}events_dead_letter_queue{suffix}"
-KAFKA_GROUPS = f"{prefix}clickhouse_groups{suffix}"
+KAFKA_EVENTS_PLUGIN_INGESTION: str = (
+    f"{KAFKA_PREFIX}events_plugin_ingestion{suffix}"  # can be overridden in settings.py
+)
+KAFKA_EVENTS = f"{KAFKA_PREFIX}clickhouse_events_proto{suffix}"
+KAFKA_EVENTS_JSON = f"{KAFKA_PREFIX}clickhouse_events_json{suffix}"
+KAFKA_PERSON = f"{KAFKA_PREFIX}clickhouse_person{suffix}"
+KAFKA_PERSON_UNIQUE_ID = f"{KAFKA_PREFIX}clickhouse_person_unique_id{suffix}"
+KAFKA_PERSON_DISTINCT_ID = f"{KAFKA_PREFIX}clickhouse_person_distinct_id{suffix}"
+KAFKA_SESSION_RECORDING_EVENTS = f"{KAFKA_PREFIX}clickhouse_session_recording_events{suffix}"
+KAFKA_PLUGIN_LOG_ENTRIES = f"{KAFKA_PREFIX}plugin_log_entries{suffix}"
+KAFKA_DEAD_LETTER_QUEUE = f"{KAFKA_PREFIX}events_dead_letter_queue{suffix}"
+KAFKA_GROUPS = f"{KAFKA_PREFIX}clickhouse_groups{suffix}"
