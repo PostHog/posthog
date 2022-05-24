1ed90be556331fbb87ec11ba556cca4e73e6d4d7
Harry Waye <harry@posthog.com>
2022-05-24 16:00:19 +0100

    chore(kafka): support `KAFKA_PREFIX`, enable using basic Heroku Kafka (#9941)
    * chore(kafka): support `KAFKA_PREFIX`, enable using basic Heroku Kafka
    
    Kafka on Heroku multi-tenented instances requires that all topics have a
    prefix. See
    https://devcenter.heroku.com/articles/multi-tenant-kafka-on-heroku#differences-to-dedicated-kafka-plans
    for details.
    
    * Move KAFKA_PREFIX

--- plugin-server/src/config/kafka-topics.ts
+++ plugin-server/src/config/kafka-topics.ts
@@ -4,16 +4,17 @@ import { determineNodeEnv, NodeEnv } from '../utils/env-utils'
 
 const isTestEnv = determineNodeEnv() === NodeEnv.Test
 const suffix = isTestEnv ? '_test' : ''
+const prefix = process.env.KAFKA_TOPIC_PREFIX || ''
 
-export const KAFKA_EVENTS = `clickhouse_events_proto${suffix}`
-export const KAFKA_EVENTS_JSON = `clickhouse_events_json${suffix}`
-export const KAFKA_PERSON = `clickhouse_person${suffix}`
-export const KAFKA_PERSON_UNIQUE_ID = `clickhouse_person_unique_id${suffix}`
-export const KAFKA_PERSON_DISTINCT_ID = `clickhouse_person_distinct_id${suffix}`
-export const KAFKA_SESSION_RECORDING_EVENTS = `clickhouse_session_recording_events${suffix}`
-export const KAFKA_EVENTS_PLUGIN_INGESTION = `events_plugin_ingestion${suffix}`
-export const KAFKA_PLUGIN_LOG_ENTRIES = `plugin_log_entries${suffix}`
-export const KAFKA_EVENTS_DEAD_LETTER_QUEUE = `events_dead_letter_queue${suffix}`
-export const KAFKA_GROUPS = `clickhouse_groups${suffix}`
-export const KAFKA_BUFFER = `conversion_events_buffer${suffix}`
-export const KAFKA_HEALTHCHECK = `healthcheck${suffix}`
+export const KAFKA_EVENTS = `${prefix}clickhouse_events_proto${suffix}`
+export const KAFKA_EVENTS_JSON = `${prefix}clickhouse_events_json${suffix}`
+export const KAFKA_PERSON = `${prefix}clickhouse_person${suffix}`
+export const KAFKA_PERSON_UNIQUE_ID = `${prefix}clickhouse_person_unique_id${suffix}`
+export const KAFKA_PERSON_DISTINCT_ID = `${prefix}clickhouse_person_distinct_id${suffix}`
+export const KAFKA_SESSION_RECORDING_EVENTS = `${prefix}clickhouse_session_recording_events${suffix}`
+export const KAFKA_EVENTS_PLUGIN_INGESTION = `${prefix}events_plugin_ingestion${suffix}`
+export const KAFKA_PLUGIN_LOG_ENTRIES = `${prefix}plugin_log_entries${suffix}`
+export const KAFKA_EVENTS_DEAD_LETTER_QUEUE = `${prefix}events_dead_letter_queue${suffix}`
+export const KAFKA_GROUPS = `${prefix}clickhouse_groups${suffix}`
+export const KAFKA_BUFFER = `${prefix}conversion_events_buffer${suffix}`
+export const KAFKA_HEALTHCHECK = `${prefix}healthcheck${suffix}`
