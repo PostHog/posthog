6a8d00568836459df34777640b0624e468ce62ea
Harry Waye <harry@posthog.com>
2022-05-24 12:32:57 +0000

    chore(kafka): support `KAFKA_PREFIX`, enable using basic Heroku Kafka
    Kafka on Heroku multi-tenented instances requires that all topics have a
    prefix. See
    https://devcenter.heroku.com/articles/multi-tenant-kafka-on-heroku#differences-to-dedicated-kafka-plans
    for details.

--- ee/kafka_client/topics.py
+++ ee/kafka_client/topics.py
@@ -1,16 +1,19 @@
 # Keep this in sync with plugin-server/src/config/kafka-topics.ts
 
+import os
+
 from posthog.settings import TEST
 
 suffix = "_test" if TEST else ""
+prefix = os.getenv("KAFKA_PREFIX", "")
 
-KAFKA_EVENTS_PLUGIN_INGESTION: str = f"events_plugin_ingestion{suffix}"  # can be overridden in settings.py
-KAFKA_EVENTS = f"clickhouse_events_proto{suffix}"
-KAFKA_EVENTS_JSON = f"clickhouse_events_json{suffix}"
-KAFKA_PERSON = f"clickhouse_person{suffix}"
-KAFKA_PERSON_UNIQUE_ID = f"clickhouse_person_unique_id{suffix}"
-KAFKA_PERSON_DISTINCT_ID = f"clickhouse_person_distinct_id{suffix}"
-KAFKA_SESSION_RECORDING_EVENTS = f"clickhouse_session_recording_events{suffix}"
-KAFKA_PLUGIN_LOG_ENTRIES = f"plugin_log_entries{suffix}"
-KAFKA_DEAD_LETTER_QUEUE = f"events_dead_letter_queue{suffix}"
-KAFKA_GROUPS = f"clickhouse_groups{suffix}"
+KAFKA_EVENTS_PLUGIN_INGESTION: str = f"{prefix}events_plugin_ingestion{suffix}"  # can be overridden in settings.py
+KAFKA_EVENTS = f"{prefix}clickhouse_events_proto{suffix}"
+KAFKA_EVENTS_JSON = f"{prefix}clickhouse_events_json{suffix}"
+KAFKA_PERSON = f"{prefix}clickhouse_person{suffix}"
+KAFKA_PERSON_UNIQUE_ID = f"{prefix}clickhouse_person_unique_id{suffix}"
+KAFKA_PERSON_DISTINCT_ID = f"{prefix}clickhouse_person_distinct_id{suffix}"
+KAFKA_SESSION_RECORDING_EVENTS = f"{prefix}clickhouse_session_recording_events{suffix}"
+KAFKA_PLUGIN_LOG_ENTRIES = f"{prefix}plugin_log_entries{suffix}"
+KAFKA_DEAD_LETTER_QUEUE = f"{prefix}events_dead_letter_queue{suffix}"
+KAFKA_GROUPS = f"{prefix}clickhouse_groups{suffix}"
