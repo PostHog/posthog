3fd59809664d91a1a8716aa4e6cf96753ce887e4
Harry Waye <harry@posthog.com>
2022-05-25 13:12:45 +0000

    fix typing

--- posthog/settings/object_storage.py
+++ posthog/settings/object_storage.py
@@ -10,8 +10,11 @@ if TEST or DEBUG:
     OBJECT_STORAGE_SECRET_ACCESS_KEY = os.getenv("OBJECT_STORAGE_SECRET_ACCESS_KEY", "object_storage_root_password")
 else:
     OBJECT_STORAGE_ENDPOINT = os.getenv("OBJECT_STORAGE_ENDPOINT", "")
-    OBJECT_STORAGE_ACCESS_KEY_ID = os.getenv("OBJECT_STORAGE_ACCESS_KEY_ID", None)
-    OBJECT_STORAGE_SECRET_ACCESS_KEY = os.getenv("OBJECT_STORAGE_SECRET_ACCESS_KEY", None)
+    # To enable us to specify that the AWS provided credentials for e.g. the EC2
+    # or Fargate task, we default to `None` rather than "" as this will, when
+    # passed to boto, result in the correct credentials being used.
+    OBJECT_STORAGE_ACCESS_KEY_ID = os.getenv("OBJECT_STORAGE_ACCESS_KEY_ID", "") or None
+    OBJECT_STORAGE_SECRET_ACCESS_KEY = os.getenv("OBJECT_STORAGE_SECRET_ACCESS_KEY", "") or None
 
 OBJECT_STORAGE_ENABLED = get_from_env("OBJECT_STORAGE_ENABLED", True if DEBUG else False, type_cast=str_to_bool)
 OBJECT_STORAGE_BUCKET = os.getenv("OBJECT_STORAGE_BUCKET", "posthog")
