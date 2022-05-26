6d1829f839c89878251bbacf8448c079c15f3664
Li Yi Yu <li@posthog.com>
2022-05-26 12:59:06 -0400

    feat(onboarding-1.5): Sidebar layout (#9848)
    * sidebar setup
    
    * sidebar
    
    * hide top bar and add site popover
    
    * sticky sidebar
    
    * update user settings display
    
    * make nav steps work
    
    * match nav steps
    
    * sidebar support links update
    
    * fix
    
    * clean up
    
    * prettier
    
    * import correct kea value
    
    * use person add icon for invite members btn
    
    * fix back button and top bar display
    
    * convert divs to lemon buttons
    
    * address feedback
    
    * fix
    
    * update
    
    * updates
    
    * add top bar
    
    * styling
    
    * final clean up
    
    * warning

--- frontend/src/layout/navigation/Navigation.tsx
+++ frontend/src/layout/navigation/Navigation.tsx
@@ -2,18 +2,21 @@ import { Layout } from 'antd'
 import { useValues } from 'kea'
 import { BillingAlerts } from 'lib/components/BillingAlerts'
 import React from 'react'
+import { ingestionLogic } from 'scenes/ingestion/ingestionLogic'
 import { sceneLogic } from 'scenes/sceneLogic'
+import { Scene } from 'scenes/sceneTypes'
 import { Breadcrumbs } from './Breadcrumbs/Breadcrumbs'
 import { DemoWarnings } from './DemoWarnings/DemoWarnings'
 import { SideBar } from './SideBar/SideBar'
 import { TopBar } from './TopBar/TopBar'
 
 export function Navigation({ children }: { children: any }): JSX.Element {
-    const { sceneConfig } = useValues(sceneLogic)
+    const { sceneConfig, activeScene } = useValues(sceneLogic)
+    const { onboardingSidebarEnabled } = useValues(ingestionLogic)
 
     return (
         <Layout style={{ minHeight: '100vh' }}>
-            <TopBar />
+            {(!onboardingSidebarEnabled || (onboardingSidebarEnabled && activeScene !== Scene.Ingestion)) && <TopBar />}
             <SideBar>
                 <Layout.Content className={!sceneConfig?.plain ? 'main-app-content' : undefined}>
                     {!sceneConfig?.plain && (
