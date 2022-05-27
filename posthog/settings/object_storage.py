cc847a1ff6a368f3457ebd85224f13286a73d25d
Paul D'Ambra <paul@posthog.com>
2022-05-27 09:15:53 +0100

    fix: soft or hard delete insight after soft or hard dashboard deletion (#9998)
    * delete insight to dashboard relation on soft delete of either
    
    * refactor tests
    
    * obey mypy
    
    * don't allow hard delete of insights or dashboards
    
    * soft deleted insights return 404 on get
    
    * prove soft delete can be reversed
    
    * no need to test permissions on delete that nobody can do
    
    * no need to test permissions on delete that nobody can do

--- posthog/settings/object_storage.py
+++ posthog/settings/object_storage.py
@@ -13,8 +13,8 @@ else:
     # To enable us to specify that the AWS provided credentials for e.g. the EC2
     # or Fargate task, we default to `None` rather than "" as this will, when
     # passed to boto, result in the correct credentials being used.
-    OBJECT_STORAGE_ACCESS_KEY_ID = os.getenv("OBJECT_STORAGE_ACCESS_KEY_ID")
-    OBJECT_STORAGE_SECRET_ACCESS_KEY = os.getenv("OBJECT_STORAGE_SECRET_ACCESS_KEY")
+    OBJECT_STORAGE_ACCESS_KEY_ID = os.getenv("OBJECT_STORAGE_ACCESS_KEY_ID", "") or None
+    OBJECT_STORAGE_SECRET_ACCESS_KEY = os.getenv("OBJECT_STORAGE_SECRET_ACCESS_KEY", "") or None
 
 OBJECT_STORAGE_ENABLED = get_from_env("OBJECT_STORAGE_ENABLED", True if DEBUG else False, type_cast=str_to_bool)
 OBJECT_STORAGE_BUCKET = os.getenv("OBJECT_STORAGE_BUCKET", "posthog")
