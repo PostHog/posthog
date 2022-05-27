222bc8a8d33ad8c793f5b9ff8a91193ba39de4de
Paul D'Ambra <paul@posthog.com>
2022-05-27 13:54:44 +0100

    simplify objectStorage

--- plugin-server/src/main/services/objectStorage.ts
+++ plugin-server/src/main/services/objectStorage.ts
@@ -24,57 +24,49 @@ export const noOpStorage: ObjectStorage = {
 // Object Storage added without any uses to flush out deployment concerns.
 // see https://github.com/PostHog/posthog/pull/9901
 export const connectObjectStorage = (serverConfig: Partial<PluginsServerConfig>): ObjectStorage => {
-    try {
-        const {
-            OBJECT_STORAGE_ENDPOINT,
-            OBJECT_STORAGE_ACCESS_KEY_ID,
-            OBJECT_STORAGE_SECRET_ACCESS_KEY,
-            OBJECT_STORAGE_ENABLED,
-            OBJECT_STORAGE_BUCKET,
-        } = serverConfig
+    const {
+        OBJECT_STORAGE_ENDPOINT,
+        OBJECT_STORAGE_ACCESS_KEY_ID,
+        OBJECT_STORAGE_SECRET_ACCESS_KEY,
+        OBJECT_STORAGE_ENABLED,
+        OBJECT_STORAGE_BUCKET,
+    } = serverConfig
 
-        if (!OBJECT_STORAGE_ENABLED) {
-            return { ...noOpStorage }
-        }
-
-        if (!S3) {
-            S3 = new aws.S3({
-                endpoint: OBJECT_STORAGE_ENDPOINT,
-                accessKeyId: OBJECT_STORAGE_ACCESS_KEY_ID,
-                secretAccessKey: OBJECT_STORAGE_SECRET_ACCESS_KEY,
-                s3ForcePathStyle: true, // needed with minio?
-                signatureVersion: 'v4',
-            })
-        }
-
-        return {
-            isEnabled: OBJECT_STORAGE_ENABLED,
-            putObject: OBJECT_STORAGE_ENABLED ? (params) => S3.putObject(params).promise() : () => Promise.resolve(),
-            sessionRecordingAllowList: Array.from(
-                new Set(
-                    serverConfig.OBJECT_STORAGE_SESSION_RECORDING_ENABLED_TEAMS?.split(',').filter(String).map(Number)
-                )
-            ),
-            healthcheck: async () => {
-                if (!OBJECT_STORAGE_BUCKET) {
-                    status.error('😢', 'No object storage bucket configured')
-                    return false
-                }
-
-                try {
-                    await S3.headBucket({
-                        Bucket: OBJECT_STORAGE_BUCKET,
-                    }).promise()
-                    return true
-                } catch (error) {
-                    status.error('💣', 'Could not access bucket:', error)
-                    return false
-                }
-            },
-        }
-    } catch (e) {
-        // only warn here... object storage is not mandatory until after #9901 at the earliest
-        status.warn('😢', 'could not initialise storage:', e)
+    if (!OBJECT_STORAGE_ENABLED) {
         return { ...noOpStorage }
+    }
+
+    if (!S3) {
+        S3 = new aws.S3({
+            endpoint: OBJECT_STORAGE_ENDPOINT,
+            accessKeyId: OBJECT_STORAGE_ACCESS_KEY_ID,
+            secretAccessKey: OBJECT_STORAGE_SECRET_ACCESS_KEY,
+            s3ForcePathStyle: true, // needed with minio?
+            signatureVersion: 'v4',
+        })
+    }
+
+    return {
+        isEnabled: OBJECT_STORAGE_ENABLED,
+        putObject: OBJECT_STORAGE_ENABLED ? (params) => S3.putObject(params).promise() : () => Promise.resolve(),
+        sessionRecordingAllowList: Array.from(
+            new Set(serverConfig.OBJECT_STORAGE_SESSION_RECORDING_ENABLED_TEAMS?.split(',').filter(String).map(Number))
+        ),
+        healthcheck: async () => {
+            if (!OBJECT_STORAGE_BUCKET) {
+                status.error('😢', 'No object storage bucket configured')
+                return false
+            }
+
+            try {
+                await S3.headBucket({
+                    Bucket: OBJECT_STORAGE_BUCKET,
+                }).promise()
+                return true
+            } catch (error) {
+                status.error('💣', 'Could not access bucket:', error)
+                return false
+            }
+        },
     }
 }
