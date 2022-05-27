d2c2204a470bb235f3652189f9b6f7d24be4a4a4
Michael Matloka <dev@twixes.com>
2022-05-27 17:29:06 +0200

    Revert "Fix and optimize `console.test.ts`"
    This reverts commit 273dbc1244df190fdf9bb6e1ab3cc35f0586af96.

--- plugin-server/tests/worker/console.test.ts
+++ plugin-server/tests/worker/console.test.ts
@@ -1,11 +1,11 @@
 import { ConsoleExtension } from '@posthog/plugin-scaffold'
 
-import { Hub, PluginLogEntrySource, PluginLogEntryType } from '../../src/types'
+import { Hub, PluginLogEntryType } from '../../src/types'
 import { createHub } from '../../src/utils/db/hub'
+import { getPluginConfigRows } from '../../src/utils/db/sql'
 import { createConsole } from '../../src/worker/vm/extensions/console'
 import { delayUntilEventIngested, resetTestDatabaseClickhouse } from '../helpers/clickhouse'
 import { resetKafka } from '../helpers/kafka'
-import { pluginConfig39 } from '../helpers/plugins'
 import { resetTestDatabase } from '../helpers/sql'
 
 jest.setTimeout(60000) // 60 sec timeout
@@ -16,79 +16,94 @@ describe('console extension', () => {
 
     beforeAll(async () => {
         await resetKafka()
+    })
+
+    beforeEach(async () => {
         ;[hub, closeHub] = await createHub()
         await resetTestDatabase()
         await resetTestDatabaseClickhouse()
     })
 
-    afterAll(async () => {
+    afterEach(async () => {
         await closeHub()
-    })
-
-    beforeEach(async () => {
-        await hub.clickhouse.querying('TRUNCATE plugin_log_entries')
     })
 
     Object.values(PluginLogEntryType).map((type) => {
         const method = type.toLowerCase() as keyof ConsoleExtension
-        describe(`console.${method}()`, () => {
-            it('stores logs based on various types of args in the database', async () => {
-                const console = createConsole(hub, pluginConfig39)
+        describe(`console#${method}`, () => {
+            it('leaves an empty entry in the database', async () => {
+                const pluginConfig = (await getPluginConfigRows(hub))[0]
 
-                // Empty log
+                const console = createConsole(hub, pluginConfig)
+
                 await (console[method]() as unknown as Promise<void>)
-                // String and number
+
+                await delayUntilEventIngested(() => hub.db.fetchPluginLogEntries())
+                const pluginLogEntries = await hub.db.fetchPluginLogEntries()
+
+                expect(pluginLogEntries.length).toBe(1)
+                expect(pluginLogEntries[0].type).toEqual(type)
+                expect(pluginLogEntries[0].message).toEqual('')
+            })
+
+            it('leaves a string + number entry in the database', async () => {
+                const pluginConfig = (await getPluginConfigRows(hub))[0]
+
+                const console = createConsole(hub, pluginConfig)
+
                 await (console[method]('number =', 987) as unknown as Promise<void>)
-                // Object
-                await (console[method]({ 1: 'ein', 2: 'zwei' }) as unknown as Promise<void>)
-                // Error
+
+                await delayUntilEventIngested(() => hub.db.fetchPluginLogEntries())
+                const pluginLogEntries = await hub.db.fetchPluginLogEntries()
+
+                expect(pluginLogEntries.length).toBe(1)
+                expect(pluginLogEntries[0].type).toEqual(type)
+                expect(pluginLogEntries[0].message).toEqual('number = 987')
+            })
+
+            it('leaves an error entry in the database', async () => {
+                const pluginConfig = (await getPluginConfigRows(hub))[0]
+
+                const console = createConsole(hub, pluginConfig)
+
                 await (console[method](new Error('something')) as unknown as Promise<void>)
-                // Array
+
+                await delayUntilEventIngested(() => hub.db.fetchPluginLogEntries())
+                const pluginLogEntries = await hub.db.fetchPluginLogEntries()
+
+                expect(pluginLogEntries.length).toBe(1)
+                expect(pluginLogEntries[0].type).toEqual(type)
+                expect(pluginLogEntries[0].message).toEqual('Error: something')
+            })
+
+            it('leaves an object entry in the database', async () => {
+                const pluginConfig = (await getPluginConfigRows(hub))[0]
+
+                const console = createConsole(hub, pluginConfig)
+
+                await (console[method]({ 1: 'ein', 2: 'zwei' }) as unknown as Promise<void>)
+
+                await delayUntilEventIngested(() => hub.db.fetchPluginLogEntries())
+                const pluginLogEntries = await hub.db.fetchPluginLogEntries()
+
+                expect(pluginLogEntries.length).toBe(1)
+                expect(pluginLogEntries[0].type).toEqual(type)
+                expect(pluginLogEntries[0].message).toEqual(`{"1":"ein","2":"zwei"}`)
+            })
+
+            it('leaves an object entry in the database', async () => {
+                const pluginConfig = (await getPluginConfigRows(hub))[0]
+
+                const console = createConsole(hub, pluginConfig)
+
                 await (console[method]([99, 79]) as unknown as Promise<void>)
-                await hub.kafkaProducer.flush()
 
-                const newPluginLogEntries = await delayUntilEventIngested(() => hub.db.fetchPluginLogEntries(), 5)
+                await delayUntilEventIngested(() => hub.db.fetchPluginLogEntries())
+                const pluginLogEntries = await hub.db.fetchPluginLogEntries()
 
-                expect(newPluginLogEntries.length).toBe(5)
-                expect(newPluginLogEntries).toEqual(
-                    expect.arrayContaining([
-                        expect.objectContaining({
-                            type: type,
-                            message: '',
-                            team_id: pluginConfig39.team_id,
-                            plugin_config_id: pluginConfig39.id,
-                            source: PluginLogEntrySource.Console,
-                        }),
-                        expect.objectContaining({
-                            type: type,
-                            message: 'number = 987',
-                            team_id: pluginConfig39.team_id,
-                            plugin_config_id: pluginConfig39.id,
-                            source: PluginLogEntrySource.Console,
-                        }),
-                        expect.objectContaining({
-                            type: type,
-                            message: '{"1":"ein","2":"zwei"}',
-                            team_id: pluginConfig39.team_id,
-                            plugin_config_id: pluginConfig39.id,
-                            source: PluginLogEntrySource.Console,
-                        }),
-                        expect.objectContaining({
-                            type: type,
-                            message: 'Error: something',
-                            team_id: pluginConfig39.team_id,
-                            plugin_config_id: pluginConfig39.id,
-                            source: PluginLogEntrySource.Console,
-                        }),
-                        expect.objectContaining({
-                            type: type,
-                            message: '[99,79]',
-                            team_id: pluginConfig39.team_id,
-                            plugin_config_id: pluginConfig39.id,
-                            source: PluginLogEntrySource.Console,
-                        }),
-                    ])
-                )
+                expect(pluginLogEntries.length).toBe(1)
+                expect(pluginLogEntries[0].type).toEqual(type)
+                expect(pluginLogEntries[0].message).toEqual(`[99,79]`)
             })
         })
     })
