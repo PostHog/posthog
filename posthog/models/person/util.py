77cc8324e60c92d7e701b4213a100f5ad684527e
eric <eeoneric@gmail.com>
2022-05-31 14:40:09 -0400

    fix: patch

--- posthog/models/person/util.py
+++ posthog/models/person/util.py
@@ -22,7 +22,7 @@ from ee.clickhouse.sql.person import (
 from ee.kafka_client.client import ClickhouseProducer
 from ee.kafka_client.topics import KAFKA_PERSON, KAFKA_PERSON_DISTINCT_ID, KAFKA_PERSON_UNIQUE_ID
 from posthog.client import sync_execute
-from posthog.models.person.person import Person, PersonDistinctId
+from posthog.models.person import Person, PersonDistinctId
 from posthog.models.team import Team
 from posthog.models.utils import UUIDT
 from posthog.queries.person_distinct_id_query import fetch_person_distinct_id2_ready
