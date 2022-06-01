4c3e713177231c79ed99f99ec410ec667350157f
Eric Duong <eeoneric@gmail.com>
2022-06-01 16:26:11 -0400

    refactor: move persons util (#10079)
    * refactor: move persons /clickhouse to /posthog
    
    * remove leak
    
    * fix: patch
    
    * move import
    
    * remove imports

--- posthog/test/test_team_model.py
+++ posthog/test/test_team_model.py
@@ -3,10 +3,10 @@ from uuid import UUID, uuid4
 from ee.clickhouse.models.cohort import insert_static_cohort
 from ee.clickhouse.models.event import create_event
 from ee.clickhouse.models.group import create_group  # move this to /ee
-from ee.clickhouse.models.person import create_person, create_person_distinct_id
 from ee.clickhouse.util import ClickhouseDestroyTablesMixin, ClickhouseTestMixin
 from posthog.client import sync_execute
 from posthog.models import Team
+from posthog.models.person.util import create_person, create_person_distinct_id
 from posthog.models.team.util import delete_teams_data
 from posthog.test.base import BaseTest
 
