5c4d7c68c2d62dbbe4a502064340a41b0bc937b5
Marius Andra <marius.andra@gmail.com>
2022-06-02 10:50:10 +0200

    handle opacity via a class

--- frontend/src/lib/components/LoadingHog/LoadingHog.tsx
+++ frontend/src/lib/components/LoadingHog/LoadingHog.tsx
@@ -2,17 +2,19 @@ import './LoadingHog.scss'
 import laptophog from 'public/animations/laptophog.json'
 import { Player } from '@lottiefiles/react-lottie-player'
 import React, { useEffect, useState } from 'react'
+import clsx from 'clsx'
 
 interface LoadingHogProps {
+    className?: string
     style?: React.CSSProperties
 }
 
-export function LoadingHog({ style }: LoadingHogProps): JSX.Element {
-    const [showing, setShowing] = useState(false)
+export function LoadingHog({ className, style }: LoadingHogProps): JSX.Element {
+    // Delay 300ms before showing LoadingHog, to not confuse users with subliminal hedgehogs
+    // that flash before their eyes. Then take 400ms to fade in the animation.
+    const [visible, setVisible] = useState(false)
     useEffect(() => {
-        const timeout = window.setTimeout(() => {
-            setShowing(true)
-        }, 300)
+        const timeout = window.setTimeout(() => setVisible(true), 300)
         return () => window.clearTimeout(timeout)
     }, [])
     return (
@@ -20,11 +22,8 @@ export function LoadingHog({ style }: LoadingHogProps): JSX.Element {
             autoplay
             loop
             src={laptophog}
-            className="LoadingHog"
-            style={{
-                opacity: showing ? 1 : 0,
-                ...style,
-            }}
+            className={clsx('LoadingHog', { LoadingHog__hidden: !visible }, className)}
+            style={style}
         />
     )
 }
