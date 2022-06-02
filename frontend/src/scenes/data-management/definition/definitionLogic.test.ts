f66420ac2f9672a7e7c34ecb99f465dfce8b6d0f
Alex Kim <alexgkim205@gmail.com>
2022-06-02 11:11:18 -0400

    fix tests

--- frontend/src/scenes/data-management/definition/definitionLogic.test.ts
+++ frontend/src/scenes/data-management/definition/definitionLogic.test.ts
@@ -2,7 +2,7 @@ import { createNewDefinition, definitionLogic } from 'scenes/data-management/def
 import { initKeaTests } from '~/test/init'
 import { expectLogic } from 'kea-test-utils'
 import { useMocks } from '~/mocks/jest'
-import { mockEventDefinitions } from '~/test/mocks'
+import { mockEventDefinitions, mockEventPropertyDefinition } from '~/test/mocks'
 import { router } from 'kea-router'
 import { urls } from 'scenes/urls'
 
@@ -13,29 +13,55 @@ describe('definitionLogic', () => {
         useMocks({
             get: {
                 '/api/projects/:team/event_definitions/:id': mockEventDefinitions[0],
+                '/api/projects/:team/property_definitions/:id': mockEventPropertyDefinition,
             },
         })
         initKeaTests()
     })
 
-    it('load definition on mount', async () => {
-        router.actions.push(urls.eventDefinition('1'))
-        logic = definitionLogic({ id: '1' })
-        logic.mount()
-        await expectLogic(logic).toDispatchActions(['loadDefinition', 'loadDefinitionSuccess']).toMatchValues({
-            definition: mockEventDefinitions[0],
+    describe('event definition', () => {
+        it('load definition on mount', async () => {
+            router.actions.push(urls.eventDefinition('1'))
+            logic = definitionLogic({ id: '1' })
+            logic.mount()
+            await expectLogic(logic).toDispatchActions(['loadDefinition', 'loadDefinitionSuccess']).toMatchValues({
+                definition: mockEventDefinitions[0],
+            })
+        })
+
+        it('load new definition on mount', async () => {
+            router.actions.push(urls.eventDefinition('new'))
+            logic = definitionLogic({ id: 'new' })
+            logic.mount()
+            await expectLogic(logic)
+                .toDispatchActions(['setDefinition', 'setDefinitionSuccess'])
+                .toNotHaveDispatchedActions(['loadDefinition'])
+                .toMatchValues({
+                    definition: createNewDefinition(true),
+                })
         })
     })
 
-    it('load new definition on mount', async () => {
-        router.actions.push(urls.eventDefinition('new'))
-        logic = definitionLogic({ id: 'new' })
-        logic.mount()
-        await expectLogic(logic)
-            .toDispatchActions(['setDefinition', 'setDefinitionSuccess'])
-            .toNotHaveDispatchedActions(['loadDefinition'])
-            .toMatchValues({
-                definition: createNewDefinition(true),
+    describe('event property definition', () => {
+        it('load definition on mount', async () => {
+            router.actions.push(urls.eventPropertyDefinition('1'))
+            logic = definitionLogic({ id: '1' })
+            logic.mount()
+            await expectLogic(logic).toDispatchActions(['loadDefinition', 'loadDefinitionSuccess']).toMatchValues({
+                definition: mockEventPropertyDefinition,
             })
+        })
+
+        it('load new definition on mount', async () => {
+            router.actions.push(urls.eventPropertyDefinition('new'))
+            logic = definitionLogic({ id: 'new' })
+            logic.mount()
+            await expectLogic(logic)
+                .toDispatchActions(['setDefinition', 'setDefinitionSuccess'])
+                .toNotHaveDispatchedActions(['loadDefinition'])
+                .toMatchValues({
+                    definition: createNewDefinition(false),
+                })
+        })
     })
 })
