5d8aa9b86d509cc5a1e698a136ac23f94fb5d51f
yakkomajuri <yakko.majuri@gmail.com>
2022-06-07 14:16:31 +0000

    fix prettier

--- plugin-server/src/utils/event.ts
+++ plugin-server/src/utils/event.ts
@@ -4,7 +4,6 @@ import { ClickhouseEventKafka, IngestionEvent } from '../types'
 import { chainToElements } from './db/utils'
 import { clickHouseTimestampToISO } from './utils'
 
-
 export function convertToProcessedPluginEvent(event: IngestionEvent): ProcessedPluginEvent {
     const timestamp = typeof event.timestamp === 'string' ? event.timestamp : event.timestamp.toUTC().toISO()
 
