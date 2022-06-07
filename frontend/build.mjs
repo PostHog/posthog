421016be818b2aa9ba349ed0bcb47a70730800b3
Marius Andra <marius.andra@gmail.com>
2022-06-07 15:15:35 +0200

    move more logic to `utils.mjs`, support builds scripts from other folders

--- frontend/build.mjs
+++ frontend/build.mjs
@@ -1,100 +1,63 @@
 #!/usr/bin/env node
 import * as path from 'path'
+import { fileURLToPath } from 'url'
 import {
-    __dirname,
     copyPublicFolder,
     isDev,
-    startServer,
+    startDevServer,
     createHashlessEntrypoints,
     buildInParallel,
     copyIndexHtml,
 } from './utils.mjs'
 import fse from 'fs-extra'
 
-let server
-function startDevServer() {
-    if (isDev) {
-        console.log(`👀 Starting dev server`)
-        return startServer()
-    } else {
-        console.log(`🛳 Starting production build`)
-        return null
-    }
-}
+export const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-export function writeSourceCodeEditorTypes() {
-    const readFile = (p) => fse.readFileSync(path.resolve(__dirname, p), { encoding: 'utf-8' })
-    const types = {
-        'react/index.d.ts': readFile('../node_modules/@types/react/index.d.ts'),
-        'react/global.d.ts': readFile('../node_modules/@types/react/global.d.ts'),
-        'kea/index.d.ts': readFile('../node_modules/kea/lib/index.d.ts'),
-    }
-    fse.writeFileSync(
-        path.resolve(__dirname, './src/scenes/plugins/source/types/packages.json'),
-        JSON.stringify(types, null, 4) + '\n'
-    )
-}
-
-export function writeIndexHtml(chunks = {}, entrypoints = []) {
-    copyIndexHtml('src/index.html', 'dist/index.html', 'index', chunks, entrypoints)
-    copyIndexHtml('src/layout.html', 'dist/layout.html', 'index', chunks, entrypoints)
-}
-
-export function writeSharedDashboardHtml(chunks = {}, entrypoints = []) {
-    copyIndexHtml('src/shared_dashboard.html', 'dist/shared_dashboard.html', 'shared_dashboard', chunks, entrypoints)
-}
-
-export function writeExporterHtml(chunks = {}, entrypoints = []) {
-    copyIndexHtml('src/exporter.html', 'dist/exporter.html', 'exporter', chunks, entrypoints)
-}
-
-startDevServer()
-copyPublicFolder()
+startDevServer(__dirname)
+copyPublicFolder(path.resolve(__dirname, 'public'), path.resolve(__dirname, 'dist'))
 writeSourceCodeEditorTypes()
 writeIndexHtml()
 writeSharedDashboardHtml()
 
-let buildsInProgress = 0
-buildInParallel(
+const common = {
+    absWorkingDir: __dirname,
+    bundle: true,
+}
+
+await buildInParallel(
     [
         {
             name: 'PostHog App',
             entryPoints: ['src/index.tsx'],
-            bundle: true,
             splitting: true,
             format: 'esm',
             outdir: path.resolve(__dirname, 'dist'),
+            ...common,
         },
         {
             name: 'Shared Dashboard',
             entryPoints: ['src/scenes/dashboard/SharedDashboard.tsx'],
-            bundle: true,
             format: 'iife',
             outfile: path.resolve(__dirname, 'dist', 'shared_dashboard.js'),
+            ...common,
         },
         {
             name: 'Exporter',
             entryPoints: ['src/exporter/ExportViewer.tsx'],
-            bundle: true,
             format: 'iife',
             outfile: path.resolve(__dirname, 'dist', 'exporter.js'),
+            ...common,
         },
         {
             name: 'Toolbar',
             entryPoints: ['src/toolbar/index.tsx'],
-            bundle: true,
             format: 'iife',
             outfile: path.resolve(__dirname, 'dist', 'toolbar.js'),
+            ...common,
         },
     ],
     {
-        onBuildStart: () => {
-            if (buildsInProgress === 0) {
-                server?.pauseServer()
-            }
-            buildsInProgress++
-        },
-        onBuildComplete(config, buildResponse) {
+        async onBuildComplete(config, buildResponse) {
             const { chunks, entrypoints } = buildResponse
 
             if (config.name === 'PostHog App') {
@@ -115,12 +78,52 @@ buildInParallel(
                 writeExporterHtml(chunks, entrypoints)
             }
 
-            createHashlessEntrypoints(entrypoints)
-
-            buildsInProgress--
-            if (buildsInProgress === 0) {
-                server?.resumeServer()
-            }
+            createHashlessEntrypoints(__dirname, entrypoints)
         },
     }
 )
+
+export function writeSourceCodeEditorTypes() {
+    const readFile = (p) => {
+        try {
+            return fse.readFileSync(path.resolve(__dirname, p), { encoding: 'utf-8' })
+        } catch (e) {
+            if (isDev) {
+                console.warn(
+                    `🙈 Didn't find "${p}" for the app source editor. Build it with: yarn build:packages:types`
+                )
+            } else {
+                throw e
+            }
+        }
+    }
+    const types = {
+        '@types/react/index.d.ts': readFile('../node_modules/@types/react/index.d.ts'),
+        '@types/react/global.d.ts': readFile('../node_modules/@types/react/global.d.ts'),
+        '@types/kea/index.d.ts': readFile('../node_modules/kea/lib/index.d.ts'),
+    }
+    fse.writeFileSync(
+        path.resolve(__dirname, './src/scenes/plugins/source/types/packages.json'),
+        JSON.stringify(types, null, 4) + '\n'
+    )
+}
+
+export function writeIndexHtml(chunks = {}, entrypoints = []) {
+    copyIndexHtml(__dirname, 'src/index.html', 'dist/index.html', 'index', chunks, entrypoints)
+    copyIndexHtml(__dirname, 'src/layout.html', 'dist/layout.html', 'index', chunks, entrypoints)
+}
+
+export function writeSharedDashboardHtml(chunks = {}, entrypoints = []) {
+    copyIndexHtml(
+        __dirname,
+        'src/shared_dashboard.html',
+        'dist/shared_dashboard.html',
+        'shared_dashboard',
+        chunks,
+        entrypoints
+    )
+}
+
+export function writeExporterHtml(chunks = {}, entrypoints = []) {
+    copyIndexHtml(__dirname, 'src/exporter.html', 'dist/exporter.html', 'exporter', chunks, entrypoints)
+}
