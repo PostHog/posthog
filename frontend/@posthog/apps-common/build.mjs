d336a157f2710bb76bb5c799da3c9dac466b40aa
Marius Andra <marius.andra@gmail.com>
2022-06-07 18:02:41 +0200

    simplify

--- frontend/@posthog/apps-common/build.mjs
+++ frontend/@posthog/apps-common/build.mjs
@@ -7,27 +7,22 @@ export const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 startDevServer(__dirname)
 
-const shared = {
-    name: '@posthog/apps-common',
-    absWorkingDir: __dirname,
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    chunkNames: '[name]',
-    entryNames: '[dir]/[name]',
-    external: ['react', 'react-dom'],
-    publicPath: '',
-    minify: false,
-    target: 'esnext',
-    sourcemap: false,
-}
-
 await buildInParallel(
     [
         {
-            ...shared,
-            name: `${shared.name} CJS`,
+            name: '@posthog/apps-common',
+            absWorkingDir: __dirname,
+            entryPoints: ['./src/index.ts'],
             format: 'cjs',
             outfile: 'dist/index.js',
+            bundle: true,
+            chunkNames: '[name]',
+            entryNames: '[dir]/[name]',
+            external: ['react', 'react-dom'],
+            publicPath: '',
+            minify: false,
+            target: 'esnext',
+            sourcemap: false,
         },
     ],
     {
