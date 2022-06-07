dfab280401ab75850e0716c86044c7696c4b7577
Marius Andra <marius.andra@gmail.com>
2022-06-07 17:48:51 +0200

    export everything

--- frontend/@posthog/apps-common/src/index.ts
+++ frontend/@posthog/apps-common/src/index.ts
@@ -1,28 +1,22 @@
 // import '~/globals.d'
-// export * from 'lib/components/LemonBubble/LemonBubble'
-// export * from 'lib/components/LemonButton/LemonButton'
-// export * from 'lib/components/LemonCheckbox/LemonCheckbox'
-// export * from 'lib/components/LemonDivider/LemonDivider'
-// export * from 'lib/components/LemonInput/LemonInput'
-// export * from 'lib/components/LemonModal/LemonModal'
+import '~/styles'
+import '~/initKea'
+export * from 'lib/components/LemonBubble/LemonBubble'
+export * from 'lib/components/LemonButton/LemonButton'
+export * from 'lib/components/LemonCheckbox/LemonCheckbox'
+export * from 'lib/components/LemonDivider/LemonDivider'
+export * from 'lib/components/LemonInput/LemonInput'
+export * from 'lib/components/LemonModal/LemonModal'
 export * from 'lib/components/LemonRow/LemonRow'
-// export * from 'lib/components/LemonSelect'
-// export * from 'lib/components/LemonSwitch/LemonSwitch'
-// export * from 'lib/components/LemonTable/LemonTable'
-// export * from 'lib/components/LemonTag/LemonTag'
-// export * from 'lib/components/LemonTextArea/LemonTextArea'
-// export * from 'lib/components/lemonToast'
-//
-// export * from 'lib/components/AdHocInsight/AdHocInsight'
-// export * from 'lib/components/Link'
-//
-// import api_ from 'lib/api'
-// export const api = api_
-// //
-// // import 'chart.js'
-//
-// const bla = 'hello'
-// export default bla
-// export function LemonButton() {
-//     return 'hello'
-// }
+export * from 'lib/components/LemonSelect'
+export * from 'lib/components/LemonSwitch/LemonSwitch'
+export * from 'lib/components/LemonTable/LemonTable'
+export * from 'lib/components/LemonTag/LemonTag'
+export * from 'lib/components/LemonTextArea/LemonTextArea'
+export * from 'lib/components/lemonToast'
+
+export * from 'lib/components/AdHocInsight/AdHocInsight'
+export * from 'lib/components/Link'
+
+import api_ from 'lib/api'
+export const api = api_
