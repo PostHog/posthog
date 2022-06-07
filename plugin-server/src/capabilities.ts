9874191da503f8cc7ebe185f8482fbbea39920b5
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-06-07 15:12:10 +0300

    Fix http capabilities

--- plugin-server/src/capabilities.ts
+++ plugin-server/src/capabilities.ts
@@ -3,7 +3,7 @@ import { isTestEnv } from './utils/env-utils'
 
 export function getPluginServerCapabilities(config: PluginsServerConfig): PluginServerCapabilities {
     const mode = config.PLUGIN_SERVER_MODE
-    const sharedCapabilities = isTestEnv() ? { http: true } : {}
+    const sharedCapabilities = !isTestEnv() ? { http: true } : {}
 
     switch (mode) {
         case null:
