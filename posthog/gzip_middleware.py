5a865a1a7889a5476efd88ea67276154d8eec881
Paul D'Ambra <paul@posthog.com>
2022-06-08 11:41:55 +0100

    correct placement of docstring

--- posthog/gzip_middleware.py
+++ posthog/gzip_middleware.py
@@ -14,19 +14,19 @@ def allowed_path(path: str, allowed_paths: List) -> bool:
 
 
 class PostHogGZipMiddleware(GZipMiddleware):
-    def __init__(self, get_response=None) -> None:
-        super().__init__(get_response)
-        try:
-            self.allowed_paths = [re.compile(pattern) for pattern in settings.GZIP_RESPONSE_ALLOW_LIST]
-        except re.error as ex:
-            raise InvalidGZipAllowList(str(ex)) from ex
-
     """
     The Django GZip Middleware comes with security warnings
     see: https://docs.djangoproject.com/en/4.0/ref/middleware/#module-django.middleware.gzip
 
     Rather than solve for those across the whole app. We can add it to specific paths
     """
+
+    def __init__(self, get_response=None) -> None:
+        super().__init__(get_response)
+        try:
+            self.allowed_paths = [re.compile(pattern) for pattern in settings.GZIP_RESPONSE_ALLOW_LIST]
+        except re.error as ex:
+            raise InvalidGZipAllowList(str(ex)) from ex
 
     def process_response(self, request, response):
         if request.method == "GET" and allowed_path(request.path, self.allowed_paths):
