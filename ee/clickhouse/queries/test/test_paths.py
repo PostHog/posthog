1d0fb758d717889d7472e10d56211f8b835b64a6
eric <eeoneric@gmail.com>
2022-06-08 12:52:38 -0400

    restore factory pattern

--- ee/clickhouse/queries/test/test_paths.py
+++ ee/clickhouse/queries/test/test_paths.py
@@ -11,7 +11,7 @@ from ee.clickhouse.models.group import create_group
 from ee.clickhouse.models.session_recording_event import create_session_recording_event
 from ee.clickhouse.queries.paths import ClickhousePaths, ClickhousePathsActors
 from ee.clickhouse.queries.paths.paths_event_query import PathEventQuery
-from ee.clickhouse.util import snapshot_clickhouse_queries
+from ee.clickhouse.util import ClickhouseTestMixin, snapshot_clickhouse_queries
 from posthog.constants import (
     FUNNEL_PATH_AFTER_STEP,
     FUNNEL_PATH_BEFORE_STEP,
@@ -20,13 +20,13 @@ from posthog.constants import (
 )
 from posthog.models.filters import Filter, PathFilter
 from posthog.models.group_type_mapping import GroupTypeMapping
-from posthog.queries.test.test_paths import TestPaths
+from posthog.queries.test.test_paths import paths_test_factory
 from posthog.test.base import _create_event, _create_person, test_with_materialized_columns
 
 ONE_MINUTE = 60_000  # 1 minute in milliseconds
 
 
-class TestClickhousePaths(TestPaths):
+class TestClickhousePaths(ClickhouseTestMixin, paths_test_factory(ClickhousePaths)):  # type: ignore
 
     maxDiff = None
 
