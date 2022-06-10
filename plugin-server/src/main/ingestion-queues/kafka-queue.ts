57f63f25355e4df9edb402c9d8153c077eb416bb
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2022-06-10 13:05:15 +0300

    chore: re-enable kafka consumer group metrics (#10238)

--- plugin-server/src/main/ingestion-queues/kafka-queue.ts
+++ plugin-server/src/main/ingestion-queues/kafka-queue.ts
@@ -7,7 +7,7 @@ import { killGracefully } from '../../utils/utils'
 import { KAFKA_BUFFER, KAFKA_EVENTS_JSON, prefix as KAFKA_PREFIX } from './../../config/kafka-topics'
 import { eachBatchAsyncHandlers } from './batch-processing/each-batch-async-handlers'
 import { eachBatchIngestion } from './batch-processing/each-batch-ingestion'
-import { emitConsumerGroupMetrics } from './kafka-metrics'
+import { addMetricsEventListeners, emitConsumerGroupMetrics } from './kafka-metrics'
 
 type ConsumerManagementPayload = {
     topic: string
@@ -72,7 +72,8 @@ export class KafkaQueue {
 
     async start(): Promise<void> {
         const startPromise = new Promise<void>(async (resolve, reject) => {
-            // addMetricsEventListeners(this.consumer, this.pluginsServer.statsd)
+            addMetricsEventListeners(this.consumer, this.pluginsServer.statsd)
+
             this.consumer.on(this.consumer.events.GROUP_JOIN, ({ payload }) => {
                 status.info('ℹ️', 'Kafka joined consumer group', JSON.stringify(payload))
                 this.consumerGroupMemberId = payload.memberId
