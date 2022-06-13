633a3fd3c0694a48708df652bf9ed04e1c0c7664
Alex Kim <alexgkim205@gmail.com>
2022-06-13 13:34:45 -0400

    correct import

--- posthog/models/insight.py
+++ posthog/models/insight.py
@@ -2,7 +2,7 @@ import secrets
 import string
 from typing import Optional
 
-from django.contrib.postgres.fields.array import ArrayField
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
