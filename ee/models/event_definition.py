a30c4575c4da30eb5950df3e620565f09c21f9d3
Alex Kim <alexgkim205@gmail.com>
2022-06-13 13:27:48 -0400

    revert migration

--- ee/models/event_definition.py
+++ ee/models/event_definition.py
@@ -1,4 +1,6 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django_deprecate_fields import deprecate_field
 
 from posthog.models.event_definition import EventDefinition
 
@@ -12,4 +14,12 @@ class EnterpriseEventDefinition(EventDefinition):
     verified_at: models.DateTimeField = models.DateTimeField(null=True, blank=True)
     verified_by = models.ForeignKey(
         "posthog.User", null=True, on_delete=models.SET_NULL, blank=True, related_name="verifying_user",
+    )
+
+    # Deprecated in favour of app-wide tagging model. See EnterpriseTaggedItem
+    deprecated_tags: ArrayField = deprecate_field(
+        ArrayField(models.CharField(max_length=32), null=True, blank=True, default=list), return_instead=[],
+    )
+    tags: ArrayField = deprecate_field(
+        ArrayField(models.CharField(max_length=32), null=True, blank=True, default=None), return_instead=[],
     )
