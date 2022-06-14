733db4c6bbfd27d9ddaeb9908b447f1f96e439bf
Eric Duong <eeoneric@gmail.com>
2022-06-14 12:05:21 -0400

    refactor: use cache for historical data (#10267)
    * initial working
    
    * initial working
    
    * adjust args
    
    * refactor: remove test changes
    
    * refactor: uncomment
    
    * remove junk
    
    * fix: cleanup
    
    * add test
    
    * order of params
    
    * fix tests
    
    * Update snapshots
    
    * fix typing
    
    * add condition
    
    * fix: cleanup
    
    * fix: cleanup
    
    * Update snapshots
    
    * fix flag
    
    * change snapshots
    
    * change date
    
    * add tests on merge results
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- posthog/queries/trends/trends.py
+++ posthog/queries/trends/trends.py
@@ -1,8 +1,20 @@
 import copy
 import threading
+from datetime import datetime, timedelta
 from itertools import accumulate
-from typing import Any, Callable, Dict, List, Tuple, Union, cast
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Union,
+    cast,
+)
 
+import pytz
+from dateutil import parser
 from django.db.models.query import Prefetch
 
 from posthog.client import sync_execute
@@ -17,10 +29,11 @@ from posthog.queries.trends.breakdown import TrendsBreakdown
 from posthog.queries.trends.formula import TrendsFormula
 from posthog.queries.trends.lifecycle import Lifecycle
 from posthog.queries.trends.total_volume import TrendsTotalVolume
+from posthog.utils import generate_cache_key, get_safe_cache
 
 
 class Trends(TrendsTotalVolume, Lifecycle, TrendsFormula):
-    def _get_sql_for_entity(self, filter: Filter, entity: Entity, team: Team) -> Tuple[str, Dict, Callable]:
+    def _get_sql_for_entity(self, filter: Filter, team: Team, entity: Entity) -> Tuple[str, Dict, Callable]:
         if filter.breakdown:
             sql, params, parse_function = TrendsBreakdown(
                 entity, filter, team, using_person_on_events=team.actor_on_events_querying_enabled
@@ -32,17 +45,70 @@ class Trends(TrendsTotalVolume, Lifecycle, TrendsFormula):
 
         return sql, params, parse_function
 
-    def _run_query(self, filter: Filter, entity: Entity, team: Team) -> List[Dict[str, Any]]:
-        sql, params, parse_function = self._get_sql_for_entity(filter, entity, team)
+    # Use cached result even on refresh if team has strict caching enabled
+    def get_cached_result(self, filter: Filter, team: Team) -> Optional[List[Dict[str, Any]]]:
+
+        if not team.strict_caching_enabled:
+            return None
+
+        cache_key = generate_cache_key(f"{filter.toJSON()}_{team.pk}")
+        cached_result_package = get_safe_cache(cache_key)
+        return cached_result_package.get("result") if cached_result_package else None
+
+    # Determine if the current timerange is present in the cache
+    def is_present_timerange(self, filter: Filter, team: Team) -> bool:
+        _is_cached = self.get_cached_result(filter, team)
+        if _is_cached and len(_is_cached) > 0:
+            latest_date = _is_cached[0]["days"].pop()
+            parsed_latest_date = parser.parse(latest_date)
+            parsed_latest_date = parsed_latest_date.replace(tzinfo=pytz.timezone(team.timezone))
+            _is_present = is_filter_date_present(filter, parsed_latest_date)
+        else:
+            _is_present = False
+
+        return _is_present
+
+    # Use a condensed filter if a cached result exists in the current timerange
+    def adjusted_filter(self, filter: Filter, team: Team) -> Filter:
+        _is_present = self.is_present_timerange(filter, team)
+
+        new_filter = filter.with_data({"date_from": interval_unit(filter.interval)}) if _is_present else filter
+
+        return new_filter
+
+    def merge_results(self, result, filter: Filter, team: Team):
+        cached_result = self.get_cached_result(filter, team)
+        is_present = self.is_present_timerange(filter, team)
+
+        if is_present and cached_result and filter.display != TRENDS_CUMULATIVE:
+            label_to_val = {}
+            new_res = []
+            for payload in result:
+                label_to_val[payload["label"]] = payload["data"].pop()
+
+            for series in cached_result:
+                data = series["data"]
+                data.pop()
+                data.append(label_to_val[series["label"]])
+                series["data"] = data
+                new_res.append(series)
+
+            return new_res
+        elif filter.display == TRENDS_CUMULATIVE:
+            return self._handle_cumulative(result)
+        else:
+            return result
+
+    def _run_query(self, filter: Filter, team: Team, entity: Entity) -> List[Dict[str, Any]]:
+        adjusted_filter = self.adjusted_filter(filter, team)
+        sql, params, parse_function = self._get_sql_for_entity(adjusted_filter, team, entity)
 
         result = sync_execute(sql, params)
-
         result = parse_function(result)
         serialized_data = self._format_serialized(entity, result)
+        merged_results = self.merge_results(serialized_data, filter, team)
 
-        if filter.display == TRENDS_CUMULATIVE:
-            serialized_data = self._handle_cumulative(serialized_data)
-        return serialized_data
+        return merged_results
 
     def _run_query_for_threading(self, result: List, index: int, sql, params):
         result[index] = sync_execute(sql, params)
@@ -53,7 +119,8 @@ class Trends(TrendsTotalVolume, Lifecycle, TrendsFormula):
         jobs = []
 
         for entity in filter.entities:
-            sql, params, parse_function = self._get_sql_for_entity(filter, entity, team)
+            adjusted_filter = self.adjusted_filter(filter, team)
+            sql, params, parse_function = self._get_sql_for_entity(adjusted_filter, team, entity)
             parse_functions[entity.index] = parse_function
             thread = threading.Thread(target=self._run_query_for_threading, args=(result, entity.index, sql, params),)
             jobs.append(thread)
@@ -131,3 +198,29 @@ class Trends(TrendsTotalVolume, Lifecycle, TrendsFormula):
         for metrics in entity_metrics:
             metrics.update(data=list(accumulate(metrics["data"])))
         return entity_metrics
+
+
+def is_filter_date_present(filter: Filter, latest_cached_datetime: datetime) -> bool:
+    diff = filter.date_to - latest_cached_datetime
+
+    if filter.interval == "hour":
+        return diff < timedelta(hours=1)
+    elif filter.interval == "day":
+        return diff < timedelta(days=1)
+    elif filter.interval == "week":
+        return diff < timedelta(weeks=1)
+    elif filter.interval == "month":
+        return diff < timedelta(days=30)
+
+
+def interval_unit(interval: str) -> str:
+    if interval == "hour":
+        return "-1hr"
+    if interval == "day":
+        return "-1d"
+    elif interval == "week":
+        return "-1w"
+    elif interval == "month":
+        return "-1m"
+    else:
+        raise ValueError("Invalid interval")
