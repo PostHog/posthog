ea5b901683ae71ace46ee77088d20c3519dc1c8a
eric <eeoneric@gmail.com>
2022-06-16 20:30:58 -0400

    run black

--- posthog/queries/column_optimizer/column_optimizer.py
+++ posthog/queries/column_optimizer/column_optimizer.py
@@ -3,5 +3,6 @@ from posthog.settings import EE_AVAILABLE
 if EE_AVAILABLE:
     from ee.clickhouse.queries.column_optimizer import EnterpriseColumnOptimizer as ColumnOptimizer
 else:
-    from posthog.queries.column_optimizer.foss_column_optimizer import \
-        FOSSColumnOptimizer as ColumnOptimizer  # type: ignore
+    from posthog.queries.column_optimizer.foss_column_optimizer import (
+        FOSSColumnOptimizer as ColumnOptimizer,
+    )  # type: ignore
