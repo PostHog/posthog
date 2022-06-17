9d8ab440c5eea8d08d867d014ed5044bef1330b0
eric <eeoneric@gmail.com>
2022-06-16 20:18:41 -0400

    remove comment

--- posthog/queries/column_optimizer/column_optimizer.py
+++ posthog/queries/column_optimizer/column_optimizer.py
@@ -1,9 +1,7 @@
-# flake8: noqa
 from posthog.settings import EE_AVAILABLE
 
 if EE_AVAILABLE:
     from ee.clickhouse.queries.column_optimizer import EnterpriseColumnOptimizer as ColumnOptimizer
 else:
-    from posthog.queries.column_optimizer.foss_column_optimizer import (
-        FOSSColumnOptimizer as ColumnOptimizer,
-    )  # type: ignore
+    from posthog.queries.column_optimizer.foss_column_optimizer import \
+        FOSSColumnOptimizer as ColumnOptimizer  # type: ignore
