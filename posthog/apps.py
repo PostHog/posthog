ceda554342a8656a875ced150eac1e9c0a6a71ae
Ben White <ben@benjackwhite.co.uk>
2022-06-20 09:50:58 +0200

    fix: Debug analytics capture by creating dedicated client for remote posthog tracking (#10336)

--- posthog/apps.py
+++ posthog/apps.py
@@ -3,6 +3,7 @@ import os
 import posthoganalytics
 from django.apps import AppConfig
 from django.conf import settings
+from posthoganalytics.client import Client
 
 from posthog.settings import SELF_CAPTURE, SKIP_ASYNC_MIGRATIONS_SETUP
 from posthog.utils import get_git_branch, get_git_commit, get_machine_id, get_self_capture_api_token, print_warning
@@ -20,14 +21,6 @@ class PostHogConfig(AppConfig):
         if settings.TEST or os.environ.get("OPT_OUT_CAPTURE", False):
             posthoganalytics.disabled = True
         elif settings.DEBUG:
-            # NOTE: This has to come before any call to posthoganalytics as the first call creates a client with all set values
-            local_api_key = get_self_capture_api_token(None)
-            if SELF_CAPTURE and local_api_key:
-                posthoganalytics.api_key = local_api_key
-                posthoganalytics.host = settings.SITE_URL
-            else:
-                posthoganalytics.disabled = True
-
             # log development server launch to posthog
             if os.getenv("RUN_MAIN") == "true":
                 # Sync all organization.available_features once on launch, in case plans changed
@@ -35,11 +28,21 @@ class PostHogConfig(AppConfig):
 
                 sync_all_organization_available_features()
 
-                posthoganalytics.capture(
+                # NOTE: This has to be created as a separate client so that the "capture" call doesn't lock in the properties
+                phcloud_client = Client(posthoganalytics.api_key)
+
+                phcloud_client.capture(
                     get_machine_id(),
                     "development server launched",
                     {"posthog_version": VERSION, "git_rev": get_git_commit(), "git_branch": get_git_branch(),},
                 )
+
+            local_api_key = get_self_capture_api_token(None)
+            if SELF_CAPTURE and local_api_key:
+                posthoganalytics.api_key = local_api_key
+                posthoganalytics.host = settings.SITE_URL
+            else:
+                posthoganalytics.disabled = True
 
         if not settings.SKIP_SERVICE_VERSION_REQUIREMENTS:
             for service_version_requirement in settings.SERVICE_VERSION_REQUIREMENTS:
