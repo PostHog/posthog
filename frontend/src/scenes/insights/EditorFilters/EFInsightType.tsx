dfc952c057fd40dbe8ad45231ed8075489fc119a
Ben White <ben@benjackwhite.co.uk>
2022-06-20 15:39:20 +0200

    fix: Reverted back to dropdown picker for now

--- frontend/src/scenes/insights/EditorFilters/EFInsightType.tsx
+++ frontend/src/scenes/insights/EditorFilters/EFInsightType.tsx
@@ -1,51 +1,10 @@
-import { useActions, useValues } from 'kea'
+import { useActions } from 'kea'
 import { insightLogic } from 'scenes/insights/insightLogic'
 import { INSIGHT_TYPES_METADATA } from 'scenes/saved-insights/SavedInsights'
 import React from 'react'
-import { EditorFilterProps, InsightType } from '~/types'
-import clsx from 'clsx'
-import { CSSTransition } from 'react-transition-group'
+import { EditorFilterProps } from '~/types'
 import './EFInsightType.scss'
-import { LemonButton, LemonSelect, LemonSelectOptions } from '@posthog/lemon-ui'
-
-export function EFInsightTypeHorizontal(): JSX.Element {
-    const { setActiveView } = useActions(insightLogic)
-    const { insight } = useValues(insightLogic)
-
-    return (
-        <div className="EFInsightType">
-            {Object.entries(INSIGHT_TYPES_METADATA).map(([key, meta]) => {
-                const selected = insight.filters?.insight === key
-
-                return (
-                    <LemonButton
-                        key={key}
-                        type={selected ? 'highlighted' : 'stealth'}
-                        onClick={() => setActiveView(key as InsightType)}
-                        title={meta.name}
-                    >
-                        <span
-                            className={clsx('EFInsightType-button-content', {
-                                'EFInsightType-button-content--selected': selected,
-                            })}
-                        >
-                            <meta.icon color="var(--primary-alt)" noBackground />
-                            <CSSTransition
-                                in={selected}
-                                timeout={200}
-                                classNames="EFInsightType-button-content-text-"
-                                mountOnEnter
-                                unmountOnExit
-                            >
-                                <span className="EFInsightType-button-content-text">{meta.name}</span>
-                            </CSSTransition>
-                        </span>
-                    </LemonButton>
-                )
-            })}
-        </div>
-    )
-}
+import { LemonSelect, LemonSelectOptions } from '@posthog/lemon-ui'
 
 const INSIGHT_TYPE_OPTIONS: LemonSelectOptions = Object.entries(INSIGHT_TYPES_METADATA).reduce((acc, [key, meta]) => {
     return {
