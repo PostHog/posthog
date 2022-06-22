d295a60596437a763c97c46af4bd594197138cb1
Ben White <ben@benjackwhite.co.uk>
2022-06-22 18:26:25 +0200

    fix: Removed persisted flags that we don't want (#10425)

--- posthog/settings/feature_flags.py
+++ posthog/settings/feature_flags.py
@@ -8,6 +8,4 @@ PERSISTED_FEATURE_FLAGS = get_list(os.getenv("PERSISTED_FEATURE_FLAGS", "")) + [
     "invite-teammates-prompt",
     "insight-legends",
     "multi-dashboard-insights",
-    "export-dashboard-insights",
-    "insight-subscriptions",
 ]
