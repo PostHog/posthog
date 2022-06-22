414f08c197b581b6ab4c339477dff9c313d66b3b
Michael Matloka <dev@twixes.com>
2022-06-22 13:07:41 +0200

    Explain query counts

--- posthog/test/test_plugin.py
+++ posthog/test/test_plugin.py
@@ -46,6 +46,7 @@ class TestPluginSourceFile(BaseTest):
         test_plugin: Plugin = Plugin.objects.create(organization=self.organization, name="Contoso")
 
         with self.assertNumQueries(0):
+            # No queries made on an error
             with self.assertRaises(ValidationError) as cm:
                 PluginSourceFile.objects.sync_from_plugin_archive(test_plugin)
 
@@ -59,17 +60,25 @@ class TestPluginSourceFile(BaseTest):
         )
 
         with self.assertNumQueries(0):
+            # No queries made on an error
             with self.assertRaises(ValidationError) as cm:
                 PluginSourceFile.objects.sync_from_plugin_archive(test_plugin)
 
         self.assertEqual(cm.exception.message, f"Could not find plugin.json in plugin Contoso")
 
-    def test_sync_from_plugin_archive_twice_from_zip_with_explicit_index_js_works(self):
+    def test_sync_from_plugin_archive_from_zip_with_explicit_index_js_works(self):
         test_plugin: Plugin = Plugin.objects.create(
             organization=self.organization, name="Contoso", archive=base64.b64decode(HELLO_WORLD_PLUGIN_GITHUB_ZIP[1])
         )
 
         with self.assertNumQueries(13):
+            # Create plugin.json
+            # 1. SAVEPOINT 2. SELECT plugin.json 3. SAVEPOINT 4. INSERT plugin.json 5. RELEASE SAVEPOINT
+            # 6. RELEASE SAVEPOINT 7. SAVEPOINT
+            # Create index.ts
+            # 8. SELECT index.ts 9. SAVEPOINT 10. INSERT index.ts 11. RELEASE SAVEPOINT 12. RELEASE SAVEPOINT
+            # Make sure frontend.tsx is not present (Django does a SELECT first, for signals)
+            # 13. SELECT frontend.tsx
             (plugin_json_file, index_ts_file, frontend_tsx_file,) = PluginSourceFile.objects.sync_from_plugin_archive(
                 test_plugin
             )
@@ -80,13 +89,20 @@ class TestPluginSourceFile(BaseTest):
         self.assertEqual(index_ts_file.source, HELLO_WORLD_PLUGIN_GITHUB_INDEX_JS)
         self.assertIsNone(frontend_tsx_file)
 
-    def test_sync_from_plugin_archive_twice_from_tgz_with_explicit_index_js_works(self):
+    def test_sync_from_plugin_archive_from_tgz_with_explicit_index_js_works(self):
         test_plugin: Plugin = Plugin.objects.create(
             organization=self.organization, name="Contoso", archive=base64.b64decode(HELLO_WORLD_PLUGIN_NPM_TGZ[1])
         )
 
         # First time - create
         with self.assertNumQueries(13):
+            # Create plugin.json
+            # 1. SAVEPOINT 2. SELECT plugin.json 3. SAVEPOINT 4. INSERT plugin.json 5. RELEASE SAVEPOINT
+            # 6. RELEASE SAVEPOINT 7. SAVEPOINT
+            # Create index.ts
+            # 8. SELECT index.ts 9. SAVEPOINT 10. INSERT index.ts 11. RELEASE SAVEPOINT 12. RELEASE SAVEPOINT
+            # Make sure frontend.tsx is not present (Django does a SELECT first, for signals)
+            # 13. SELECT frontend.tsx
             (plugin_json_file, index_ts_file, frontend_tsx_file,) = PluginSourceFile.objects.sync_from_plugin_archive(
                 test_plugin
             )
@@ -99,6 +115,12 @@ class TestPluginSourceFile(BaseTest):
 
         # Second time - update
         with self.assertNumQueries(9):
+            # Update plugin.json
+            # 1. SAVEPOINT 2. SELECT plugin.json 3. UPDATE plugin.json 4. RELEASE SAVEPOINT
+            # Update index.ts
+            # 5. SAVEPOINT 6. SELECT index.ts 7. UPDATE index.ts 8. RELEASE SAVEPOINT
+            # Make sure frontend.tsx is not present (Django does a SELECT first, for signals)
+            # 9. SELECT frontend.tsx
             (plugin_json_file, index_ts_file, frontend_tsx_file,) = PluginSourceFile.objects.sync_from_plugin_archive(
                 test_plugin
             )
@@ -109,7 +131,7 @@ class TestPluginSourceFile(BaseTest):
         self.assertEqual(index_ts_file.source, HELLO_WORLD_PLUGIN_NPM_INDEX_JS)
         self.assertIsNone(frontend_tsx_file)
 
-    def test_sync_from_plugin_archive_twice_from_zip_with_index_ts_works(self):
+    def test_sync_from_plugin_archive_from_zip_with_index_ts_works(self):
         test_plugin: Plugin = Plugin.objects.create(
             organization=self.organization,
             name="Contoso",
@@ -117,6 +139,13 @@ class TestPluginSourceFile(BaseTest):
         )
 
         with self.assertNumQueries(13):
+            # Create plugin.json
+            # 1. SAVEPOINT 2. SELECT plugin.json 3. SAVEPOINT 4. INSERT plugin.json 5. RELEASE SAVEPOINT
+            # 6. RELEASE SAVEPOINT 7. SAVEPOINT
+            # Create index.ts
+            # 8. SELECT index.ts 9. SAVEPOINT 10. INSERT index.ts 11. RELEASE SAVEPOINT 12. RELEASE SAVEPOINT
+            # Make sure frontend.tsx is not present (Django does a SELECT first, for signals)
+            # 13. SELECT frontend.tsx
             (plugin_json_file, index_ts_file, frontend_tsx_file,) = PluginSourceFile.objects.sync_from_plugin_archive(
                 test_plugin
             )
@@ -127,7 +156,7 @@ class TestPluginSourceFile(BaseTest):
         self.assertEqual(index_ts_file.source, HELLO_WORLD_PLUGIN_GITHUB_INDEX_JS)
         self.assertIsNone(frontend_tsx_file)
 
-    def test_sync_from_plugin_archive_twice_from_zip_without_index_ts_but_frontend_tsx_works(self):
+    def test_sync_from_plugin_archive_from_zip_without_index_ts_but_frontend_tsx_works(self):
         test_plugin: Plugin = Plugin.objects.create(
             organization=self.organization,
             name="Contoso",
@@ -135,6 +164,13 @@ class TestPluginSourceFile(BaseTest):
         )
 
         with self.assertNumQueries(13):
+            # Create plugin.json
+            # 1. SAVEPOINT 2. SELECT plugin.json 3. SAVEPOINT 4. INSERT plugin.json 5. RELEASE SAVEPOINT
+            # 6. RELEASE SAVEPOINT 7. SAVEPOINT
+            # Create frontend.tsx
+            # 8. SELECT frontend.tsx 9. SAVEPOINT 10. INSERT frontend.tsx 11. RELEASE SAVEPOINT 12. RELEASE SAVEPOINT
+            # Make sure index.ts is not present (Django does a SELECT first, for signals)
+            # 13. SELECT index.ts
             (plugin_json_file, index_ts_file, frontend_tsx_file,) = PluginSourceFile.objects.sync_from_plugin_archive(
                 test_plugin
             )
@@ -145,7 +181,7 @@ class TestPluginSourceFile(BaseTest):
         assert frontend_tsx_file is not None
         self.assertEqual(frontend_tsx_file.source, HELLO_WORLD_PLUGIN_FRONTEND_TSX)
 
-    def test_sync_from_plugin_archive_twice_from_zip_without_any_code_fails(self):
+    def test_sync_from_plugin_archive_from_zip_without_any_code_fails(self):
         test_plugin: Plugin = Plugin.objects.create(
             organization=self.organization,
             name="Contoso",
@@ -153,6 +189,7 @@ class TestPluginSourceFile(BaseTest):
         )
 
         with self.assertNumQueries(0):
+            # No queries made on an error
             with self.assertRaises(ValidationError) as cm:
                 PluginSourceFile.objects.sync_from_plugin_archive(test_plugin)
 
@@ -166,6 +203,13 @@ class TestPluginSourceFile(BaseTest):
         )
 
         with self.assertNumQueries(13):
+            # Create plugin.json
+            # 1. SAVEPOINT 2. SELECT plugin.json 3. SAVEPOINT 4. INSERT plugin.json 5. RELEASE SAVEPOINT
+            # 6. RELEASE SAVEPOINT 7. SAVEPOINT
+            # Create index.ts
+            # 8. SELECT index.ts 9. SAVEPOINT 10. INSERT index.ts 11. RELEASE SAVEPOINT 12. RELEASE SAVEPOINT
+            # Make sure frontend.tsx is not present (Django does a SELECT first, for signals)
+            # 13. SELECT frontend.tsx
             (plugin_json_file, index_ts_file, frontend_tsx_file,) = PluginSourceFile.objects.sync_from_plugin_archive(
                 test_plugin
             )
@@ -180,6 +224,13 @@ class TestPluginSourceFile(BaseTest):
         test_plugin.save()
 
         with self.assertNumQueries(12):
+            # Update plugin.json
+            # 1. SAVEPOINT 2. SELECT plugin.json 3. UPDATE plugin.json 4. RELEASE SAVEPOINT
+            # Create frontend.tsx
+            # 5. SAVEPOINT 6. SELECT frontend.tsx 7. SAVEPOINT 8. INSERT frontend.tsx 9. RELEASE SAVEPOINT
+            # 10. RELEASE SAVEPOINT
+            # Make sure index.ts is not present (Django does a SELECT first, for signals)
+            # 11. SELECT index.ts 12. DELETE index.ts
             (plugin_json_file, index_ts_file, frontend_tsx_file,) = PluginSourceFile.objects.sync_from_plugin_archive(
                 test_plugin
             )
