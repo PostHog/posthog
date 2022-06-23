f4668ed8552d27092d7658cff834d39e995ee502
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2022-06-23 10:27:01 +0300

    refactor(plugin-server): move buffer as first step of event pipeline & more (#10360)
    * WIP: Move person creation earlier
    
    * WIP: move person updating, handle person property changing
    
    * WIP: leverage person information
    
    * Update `updatePersonDeprecated` signature
    
    * Avoid (and test avoiding) unneeded lookups whether 'creating' person is needed
    
    Note there were two tricky interactions within handleIdentify, which
    again got solved by indirect message passing.
    
    * Solve TODO
    
    * Normalize event before updatePersonIfTouchedByPlugins
    
    * Avoid another lookup for person in updatePersonProperties
    
    * Avoid lookup for newPerson in handleIdentifyOrAlias
    
    * Add kludge comments
    
    * Fix runBufferEventPipeline
    
    * Rename upsertPersonsStep => processPersonsStep
    
    * Update emitToBufferStep tests
    
    * Update some event pipeline step tests
    
    * Update prepareEventStep tests
    
    * Test processPersonStep
    
    * Add tests for updatePersonIfTouchedByPlugins step
    
    * Update runner tests
    
    * verify person vesrion in event-pipeline-integration test
    
    * Update process-event test suite
    
    * Argument ordering for person state tests
    
    * Update runner test snapshots
    
    * Cast to UTC
    
    * Fixup person-state tests
    
    * Dont refetch persons needlessly on $identify
    
    * Add missing version assertion
    
    * Cast everything to UTC
    
    * Remove version assertion
    
    * Undo radical change to event pipeline - will re-add it later!
    
    * Resolve comments

--- plugin-server/src/worker/ingestion/event-pipeline/4-prepareEventStep.ts
+++ plugin-server/src/worker/ingestion/event-pipeline/4-prepareEventStep.ts
@@ -1,27 +1,31 @@
 import { PluginEvent } from '@posthog/plugin-scaffold'
 
-import { normalizeEvent } from '../../../utils/event'
+import { IngestionEvent, IngestionPersonData } from '../../../types'
 import { parseEventTimestamp } from '../timestamps'
 import { EventPipelineRunner, StepResult } from './runner'
 
-export async function prepareEventStep(runner: EventPipelineRunner, event: PluginEvent): Promise<StepResult> {
-    // :TRICKY: plugins might have modified the event, so re-sanitize
-    const { ip, site_url, team_id, uuid } = normalizeEvent(event)
+export async function prepareEventStep(
+    runner: EventPipelineRunner,
+    event: PluginEvent,
+    person: IngestionPersonData | undefined
+): Promise<StepResult> {
+    const { ip, site_url, team_id, uuid } = event
     const preIngestionEvent = await runner.hub.eventsProcessor.processEvent(
         String(event.distinct_id),
         ip,
         event,
         team_id,
         parseEventTimestamp(event, runner.hub.statsd),
-        uuid! // it will throw if it's undefined,
+        uuid!, // it will throw if it's undefined,
+        person
     )
 
     await runner.hub.siteUrlManager.updateIngestionSiteUrl(site_url)
 
     if (preIngestionEvent && preIngestionEvent.event !== '$snapshot') {
-        return runner.nextStep('emitToBufferStep', preIngestionEvent)
+        return runner.nextStep('createEventStep', preIngestionEvent)
     } else if (preIngestionEvent && preIngestionEvent.event === '$snapshot') {
-        return runner.nextStep('runAsyncHandlersStep', preIngestionEvent)
+        return runner.nextStep('runAsyncHandlersStep', preIngestionEvent as IngestionEvent)
     } else {
         return null
     }
