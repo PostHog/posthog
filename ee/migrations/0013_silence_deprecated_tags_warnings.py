c7c3bb2142dd28225efbefb3ef197d37da724c60
Alex Kim <alexgkim205@gmail.com>
2022-06-23 12:25:51 -0400

    format

--- ee/migrations/0013_silence_deprecated_tags_warnings.py
+++ ee/migrations/0013_silence_deprecated_tags_warnings.py
@@ -7,28 +7,36 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ee', '0012_migrate_tags_v2'),
+        ("ee", "0012_migrate_tags_v2"),
     ]
 
     operations = [
+        migrations.RenameField(model_name="enterpriseeventdefinition", old_name="tags", new_name="deprecated_tags_v2",),
         migrations.RenameField(
-            model_name='enterpriseeventdefinition',
-            old_name='tags',
-            new_name='deprecated_tags_v2',
-        ),
-        migrations.RenameField(
-            model_name='enterprisepropertydefinition',
-            old_name='tags',
-            new_name='deprecated_tags_v2',
+            model_name="enterprisepropertydefinition", old_name="tags", new_name="deprecated_tags_v2",
         ),
         migrations.AlterField(
-            model_name='enterpriseeventdefinition',
-            name='deprecated_tags_v2',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=32), blank=True, db_column='tags', default=None, null=True, size=None),
+            model_name="enterpriseeventdefinition",
+            name="deprecated_tags_v2",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(max_length=32),
+                blank=True,
+                db_column="tags",
+                default=None,
+                null=True,
+                size=None,
+            ),
         ),
         migrations.AlterField(
-            model_name='enterprisepropertydefinition',
-            name='deprecated_tags_v2',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=32), blank=True, db_column='tags', default=None, null=True, size=None),
+            model_name="enterprisepropertydefinition",
+            name="deprecated_tags_v2",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(max_length=32),
+                blank=True,
+                db_column="tags",
+                default=None,
+                null=True,
+                size=None,
+            ),
         ),
     ]
