b34f02e5687e434eac923a7104858881e0c9d7e5
yakkomajuri <yakko.majuri@gmail.com>
2022-06-23 12:54:53 +0100

    remove ch replicated check

--- posthog/clickhouse/migrations/0030_created_at_persons_and_groups_on_events.py
+++ posthog/clickhouse/migrations/0030_created_at_persons_and_groups_on_events.py
@@ -1,6 +1,5 @@
 from infi.clickhouse_orm import migrations
 
-from posthog.clickhouse.replication.utils import clickhouse_is_replicated
 from posthog.client import sync_execute
 from posthog.models.event.sql import EVENTS_TABLE_JSON_MV_SQL, KAFKA_EVENTS_TABLE_JSON_SQL
 from posthog.settings import CLICKHOUSE_CLUSTER
@@ -19,10 +18,8 @@ ADD COLUMN IF NOT EXISTS group4_created_at DateTime64
 
 def add_columns_to_required_tables(_):
     sync_execute(ADD_COLUMNS_BASE_SQL.format(table="events", cluster=CLICKHOUSE_CLUSTER))
-
-    if clickhouse_is_replicated():
-        sync_execute(ADD_COLUMNS_BASE_SQL.format(table="writable_events", cluster=CLICKHOUSE_CLUSTER))
-        sync_execute(ADD_COLUMNS_BASE_SQL.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
+    sync_execute(ADD_COLUMNS_BASE_SQL.format(table="writable_events", cluster=CLICKHOUSE_CLUSTER))
+    sync_execute(ADD_COLUMNS_BASE_SQL.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
 
 
 operations = [
