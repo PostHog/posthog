2a269b694e72c4e8b61918147fe869c412d87508
Ben White <ben@benjackwhite.co.uk>
2022-06-24 14:28:43 +0200

    fix: Fix ee celery takss  Inline the setup of periodic tasks

--- ee/tasks/tasks.py
+++ ee/tasks/tasks.py
@@ -8,7 +8,7 @@ from posthog.celery import app
 from posthog.utils import get_crontab
 
 
-@app.on_after_configure.connect
+# This is called by the parent setup func in posthog/celery
 def setup_periodic_tasks(sender: Celery, **kwargs):
     sender.add_periodic_task(
         crontab(
