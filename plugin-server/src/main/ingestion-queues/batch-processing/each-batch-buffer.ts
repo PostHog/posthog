fa54a63ada61c228ade36d29b2a4c14f94bc41bb
Michael Matloka <dev@twixes.com>
2022-06-27 13:00:02 +0200

    Send heartbeats every so often

--- plugin-server/src/main/ingestion-queues/batch-processing/each-batch-buffer.ts
+++ plugin-server/src/main/ingestion-queues/batch-processing/each-batch-buffer.ts
@@ -6,6 +6,7 @@ import { KafkaQueue } from '../kafka-queue'
 export async function eachMessageBuffer(
     message: KafkaMessage,
     resolveOffset: EachBatchPayload['resolveOffset'],
+    heartbeat: EachBatchPayload['heartbeat'],
     queue: KafkaQueue
 ): Promise<void> {
     const bufferEvent = JSON.parse(message.value!.toString())
@@ -17,10 +18,11 @@ export async function eachMessageBuffer(
         timeoutMessage: 'After 30 seconds still running runBufferEventPipeline',
     })
     resolveOffset(message.offset)
+    await heartbeat()
 }
 
 export async function eachBatchBuffer(
-    { batch, resolveOffset, commitOffsetsIfNecessary }: EachBatchPayload,
+    { batch, resolveOffset, heartbeat, commitOffsetsIfNecessary }: EachBatchPayload,
     queue: KafkaQueue
 ): Promise<void> {
     if (batch.messages.length === 0) {
@@ -28,27 +30,32 @@ export async function eachBatchBuffer(
     }
     const batchStartTimer = new Date()
 
-    const messagesForLater: KafkaMessage[] = []
+    /** Index of first message to be processed post-sleep. -1 means there's no sleep needed. */
+    let messagesCutoffIndex = -1
+    /** How long should we sleep for until we have a desired delay in processing. */
     let consumerSleepMs = 0
-    for (const message of batch.messages) {
-        // kafka timestamps are unix timestamps in string format
+    for (let i = 0; i < batch.messages.length; i++) {
+        const message = batch.messages[i]
+        // Kafka timestamps are Unix timestamps in string format
         const processAt = Number(message.timestamp) + queue.pluginsServer.BUFFER_CONVERSION_SECONDS * 1000
         const delayUntilTimeToProcess = processAt - Date.now()
 
-        if (delayUntilTimeToProcess <= 0) {
-            await eachMessageBuffer(message, resolveOffset, queue)
+        if (delayUntilTimeToProcess <= 0 && messagesCutoffIndex < 0) {
+            await eachMessageBuffer(message, resolveOffset, heartbeat, queue)
         } else {
-            if (consumerSleepMs < delayUntilTimeToProcess) {
+            if (messagesCutoffIndex < 0) {
+                messagesCutoffIndex = i
+            }
+            if (delayUntilTimeToProcess > consumerSleepMs) {
                 consumerSleepMs = delayUntilTimeToProcess
             }
-            messagesForLater.push(message)
         }
     }
-    if (messagesForLater.length > 0) {
-        // pause the consumer for this partition until we can process all unprocessed messages from this batch
-        await queue.bufferSleep(consumerSleepMs, batch.partition)
-        for (const message of messagesForLater) {
-            await eachMessageBuffer(message, resolveOffset, queue)
+    if (messagesCutoffIndex >= 0) {
+        // Pause the consumer for this partition until we can process all unprocessed messages from this batch
+        await queue.bufferSleep(consumerSleepMs, batch.partition, heartbeat)
+        for (const message of batch.messages.slice(messagesCutoffIndex)) {
+            await eachMessageBuffer(message, resolveOffset, heartbeat, queue)
         }
     }
     await commitOffsetsIfNecessary()
