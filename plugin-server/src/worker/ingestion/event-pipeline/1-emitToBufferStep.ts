67d695afdaf3730342bdd3694dd136744589cd84
Michael Matloka <dev@twixes.com>
2022-06-29 14:06:07 +0200

    Update comment

--- plugin-server/src/worker/ingestion/event-pipeline/1-emitToBufferStep.ts
+++ plugin-server/src/worker/ingestion/event-pipeline/1-emitToBufferStep.ts
@@ -27,10 +27,31 @@ export async function emitToBufferStep(
     }
 }
 
-// context: https://github.com/PostHog/posthog/issues/9182
-// TL;DR: events from a recently created non-anonymous person are sent to a buffer
-// because their person_id might change. We merge based on the person_id of the anonymous user
-// so ingestion is delayed for those events to increase our chances of getting person_id correctly
+/** Returns whether the event should be delayed using the event buffer mechanism.
+ *
+ * Why? Non-anonymous non-$identify events with no exisitng person matching their distinct ID are sent to a buffer.
+ * This is so that we can better handle the case where one client uses a fresh distinct ID before another client
+ * has managed to send the $identify event aliasing an existing anonymous distinct ID to the fresh distinct ID.
+ *
+ * See this example scenario:
+ * 1. User visits signup page,
+ *    in turn frontend captures anonymous `$pageview` for distinct ID `XYZ` (anonymous distinct ID = device ID).
+ *    This event gets person ID A.
+ * 2. User click signup button, initiating in a backend request,
+ *    in turn frontend captures anonymous `$autocapture` (click) for distinct ID `XYZ`
+ *    This event gets person ID A.
+ * 3. Signup request is processed in the backend,
+ *    in turn backend captures identified `signup` for distinct ID `alice@example.com`,
+ *    OOPS! We haven't seen `alice@example.com` before, so this event gets person ID B.
+ * 4. Signup request finishes successfully,
+ *    in turn frontend captures identified `$identify` aliasing distinct ID `XYZ` to `alice@example.com`.
+ *    This event gets person ID A.
+ *
+ * Without a buffer, the event from step 3 gets a new person ID, which messes up analysis by unique users.
+ * By delaying the event from step 3, all events get the desired person ID A.
+ *
+ * More context: https://github.com/PostHog/posthog/issues/9182
+ */
 export function shouldSendEventToBuffer(
     hub: Hub,
     event: PluginEvent,
