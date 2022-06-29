7a0a5eaa0648b8fe4fa40922159b156f09514aaf
Ben White <ben@benjackwhite.co.uk>
2022-06-29 09:22:22 +0200

    Added migrations

--- posthog/migrations/0248_sharingconfiguration.py
+++ posthog/migrations/0248_sharingconfiguration.py
@@ -9,20 +9,35 @@ import posthog.models.sharing_configuration
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('posthog', '0247_feature_flags_experience_continuity'),
+        ("posthog", "0247_feature_flags_experience_continuity"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='SharingConfiguration',
+            name="SharingConfiguration",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('enabled', models.BooleanField(default=False)),
-                ('access_token', models.CharField(blank=True, default=posthog.models.sharing_configuration.get_default_access_token, max_length=400, null=True, unique=True)),
-                ('dashboard', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='posthog.dashboard')),
-                ('insight', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='posthog.insight')),
-                ('team', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='posthog.team')),
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("enabled", models.BooleanField(default=False)),
+                (
+                    "access_token",
+                    models.CharField(
+                        blank=True,
+                        default=posthog.models.sharing_configuration.get_default_access_token,
+                        max_length=400,
+                        null=True,
+                        unique=True,
+                    ),
+                ),
+                (
+                    "dashboard",
+                    models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to="posthog.dashboard"),
+                ),
+                (
+                    "insight",
+                    models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to="posthog.insight"),
+                ),
+                ("team", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="posthog.team")),
             ],
         ),
     ]
