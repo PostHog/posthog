b277b1a3af151712a7828a668b2b72a2148d5ed3
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-07-01 13:25:55 +0000

    feat: plugins activity log pt. 2 (#10590)
    * feat: plugins activity log pt. 1
    
    * Update frontend/src/scenes/plugins/Plugins.tsx
    
    * add initial tests
    
    * wip
    
    * refactor and plugin config activities
    
    * return_activity_page -> activity_page_response
    
    * add story for SentenceList
    
    * type fixes
    
    * fix
    
    * more tests and refactors
    
    * add feature flag
    
    * update test
    
    * hacky fix
    
    * refactor test

--- frontend/src/scenes/plugins/tabs/installed/InstalledTab.tsx
+++ frontend/src/scenes/plugins/tabs/installed/InstalledTab.tsx
@@ -7,6 +7,7 @@ import { PluginsEmptyState } from './sections/PluginsEmptyState'
 import { DisabledPluginSection } from './sections/DisabledPluginsSection'
 import { UpgradeSection } from './sections/UpgradeSection'
 import { EnabledPluginSection } from './sections/EnabledPluginsSection'
+import { HistoryDrawer } from 'scenes/plugins/plugin/HistoryDrawer'
 
 export function InstalledTab(): JSX.Element {
     const { installedPlugins } = useValues(pluginsLogic)
@@ -24,6 +25,7 @@ export function InstalledTab(): JSX.Element {
                 <DisabledPluginSection />
             </div>
             <LogsDrawer />
+            <HistoryDrawer />
         </>
     )
 }
