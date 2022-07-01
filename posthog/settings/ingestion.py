a1e874eab6d4e5d322325689dcfd98b78cdab782
Michael Matloka <dev@twixes.com>
2022-07-01 13:33:37 +0200

    chore(event-buffer): Add event buffer notice to UI again
    This reverts commit 49d23565f2b4e67fa468c2bc57a8814409f95d2e.

--- posthog/settings/ingestion.py
+++ posthog/settings/ingestion.py
@@ -1,5 +1,8 @@
 import os
 
-from posthog.settings.utils import get_list
+from posthog.settings.utils import get_from_env, get_list
 
 INGESTION_LAG_METRIC_TEAM_IDS = get_list(os.getenv("INGESTION_LAG_METRIC_TEAM_IDS", ""))
+
+# KEEP IN SYNC WITH plugin-server/src/config/config.ts
+BUFFER_CONVERSION_SECONDS = get_from_env("BUFFER_CONVERSION_SECONDS", default=60, type_cast=int)
