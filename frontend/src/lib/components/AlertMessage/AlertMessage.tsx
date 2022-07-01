cc8ad0aa4c0a4c87e426e64f1d79d4c7af1b3f62
Michael Matloka <dev@twixes.com>
2022-07-01 14:39:37 +0200

    feat(lemon-ui): Add optional close button to `AlertMessage` (#10604)

--- frontend/src/lib/components/AlertMessage/AlertMessage.tsx
+++ frontend/src/lib/components/AlertMessage/AlertMessage.tsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import './AlertMessage.scss'
 import { WarningOutlined } from '@ant-design/icons'
-import { IconInfo } from '../icons'
+import { IconClose, IconInfo } from '../icons'
 import clsx from 'clsx'
+import { LemonButton } from '../LemonButton'
 
 export interface AlertMessageProps {
     type: 'info' | 'warning' | 'error'
-    children: string | JSX.Element
+    /** If onClose is provided, a close button will be shown and this callback will be fired when it's clicked. */
+    onClose?: () => void
+    children: React.ReactChild | React.ReactChild[]
     style?: React.CSSProperties
 }
 
 /** Generic alert message. */
-export function AlertMessage({ type, children, style }: AlertMessageProps): JSX.Element {
+export function AlertMessage({ type, onClose, children, style }: AlertMessageProps): JSX.Element {
     return (
         <div className={clsx('AlertMessage', type)} style={style}>
             <div className="AlertMessage__icon">
                 {type === 'warning' || type === 'error' ? <WarningOutlined /> : <IconInfo />}
             </div>
-            <div>{children}</div>
+            <div className="AlertMessage__text">{children}</div>
+            {onClose && (
+                <LemonButton
+                    type="tertiary"
+                    className="AlertMessage__close"
+                    icon={<IconClose />}
+                    onClick={() => onClose()}
+                />
+            )}
         </div>
     )
 }
