4cc3bf3abc4d1ec9b8497ca5e7c9ab3af7c8d034
luax.eth <yann.guineau@voidmx.net>
2022-07-04 15:27:27 +0000

    fix(capture): format test capture file

--- posthog/api/test/test_capture.py
+++ posthog/api/test/test_capture.py
@@ -929,7 +929,9 @@ class TestCapture(BaseTest):
             HTTP_ACCESS_CONTROL_REQUEST_METHOD="POST",
         )
         self.assertEqual(response.status_code, 200)  # type: ignore
-        self.assertEqual(response.headers["Access-Control-Allow-Headers"], "X-Requested-With,Content-Type,traceparent,request-id")
+        self.assertEqual(
+            response.headers["Access-Control-Allow-Headers"], "X-Requested-With,Content-Type,traceparent,request-id"
+        )
 
         response = self.client.generic(
             "OPTIONS",
@@ -939,4 +941,6 @@ class TestCapture(BaseTest):
             HTTP_ACCESS_CONTROL_REQUEST_METHOD="POST",
         )
         self.assertEqual(response.status_code, 200)  # type: ignore
-        self.assertEqual(response.headers["Access-Control-Allow-Headers"], "X-Requested-With,Content-Type,traceparent,request-id")
+        self.assertEqual(
+            response.headers["Access-Control-Allow-Headers"], "X-Requested-With,Content-Type,traceparent,request-id"
+        )
