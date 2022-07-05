fcf454d72c022786b8181f8232c56de8dcc9d42d
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-07-05 11:18:03 +0300

    Add default setting to connection pool

--- posthog/client.py
+++ posthog/client.py
@@ -99,6 +99,8 @@ def make_ch_pool(**overrides) -> ChPool:
         "connections_min": CLICKHOUSE_CONN_POOL_MIN,
         "connections_max": CLICKHOUSE_CONN_POOL_MAX,
         "settings": {"mutations_sync": "1"} if TEST else {},
+        # Without this, OPTIMIZE table and other queries will regularly run into timeouts
+        "send_receive_timeout": 999_999_999,
         **overrides,
     }
 
