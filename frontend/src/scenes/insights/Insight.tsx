a0c880ce772eeaab0b85191f7da6508a720d9685
Paul D'Ambra <paul@posthog.com>
2022-07-06 10:51:59 +0100

    fix insight png export

--- frontend/src/scenes/insights/Insight.tsx
+++ frontend/src/scenes/insights/Insight.tsx
@@ -179,6 +179,7 @@ export function Insight({ insightId }: { insightId: InsightShortId | 'new' }): J
                                                             items={[
                                                                 {
                                                                     format: ExporterFormat.PNG,
+                                                                    insight: insight.id,
                                                                     resource: exporterResourceParams,
                                                                 },
                                                                 {
