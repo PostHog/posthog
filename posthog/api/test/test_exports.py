41036bb6853acafaf78f3b14e70bf11b2de74699
Paul D'Ambra <paul@posthog.com>
2022-07-06 14:32:45 +0100

    get integration test passing

--- posthog/api/test/test_exports.py
+++ posthog/api/test/test_exports.py
@@ -255,31 +255,8 @@ class TestExports(APIBaseTest):
             },
         )
 
-    @patch("posthog.api.exports.exporter")
-    @freeze_time("2021-08-25T22:09:14.252Z")
-    def test_can_create_new_valid_export_csv(self, mock_exporter_task) -> None:
-        response = self.client.post(
-            f"/api/projects/{self.team.id}/exports",
-            {
-                "export_format": "text/csv",
-                "properties": [  # anything the list events end-point will accept
-                    {
-                        "key": "prop_that_is_a_unix_timestamp",
-                        "value": "2012-01-07 18:30:00",
-                        "operator": "is_date_after",
-                        "type": "event",
-                    }
-                ],
-            },
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        exported_instance = response.json()
-
-        self.assertEqual(exported_instance["export_context"]["file_export_type"], "list_events")
-
-        mock_exporter_task.export_asset.delay.assert_called_once_with(exported_instance["id"])
-
-    def test_can_download_a_csv(self) -> None:
+    @patch("posthog.tasks.exports.csv_exporter.requests.request")
+    def test_can_download_a_csv(self, patched_request) -> None:
         _create_event(
             event="event_name", team=self.team, distinct_id="2", properties={"$browser": "Chrome"},
         )
@@ -297,13 +274,16 @@ class TestExports(APIBaseTest):
             insight=None,
             export_format=ExportedAsset.ExportFormat.CSV,
             export_context={
-                "file_export_type": "list_events",
-                "filter": {"properties": {"$browser": "Safari"}},
-                "request_get_query_dict": {},
-                "order_by": ["-timestamp"],
-                "action_id": None,
+                "path": f"/api/projects/{self.team.id}/events?properties=%5B%7B%22key%22%3A%22%24browser%22%2C%22value%22%3A%5B%22Safari%22%5D%2C%22operator%22%3A%22exact%22%2C%22type%22%3A%22event%22%7D%5D&orderBy=%5B%22-timestamp%22%5D"
             },
+            created_by=self.user,
         )
+
+        def requests_side_effect(*args, **kwargs):
+            return self.client.get(kwargs["url"], kwargs["data"], **kwargs["headers"])
+
+        patched_request.side_effect = requests_side_effect
+
         # pass the root in because django/celery refused to override it otherwise
         exporter.export_asset(instance.id, TEST_ROOT_BUCKET)
 
@@ -327,7 +307,7 @@ class TestExports(APIBaseTest):
         self.assertIn(second_expected_event_id, file_content)
         for line in file_lines[1:]:  # every result has to match the filter though
             if line != "":  # skip the final empty line of the file
-                self.assertIn('"{""$browser"": ""Safari""}"', line)
+                self.assertIn("Safari", line)
 
     def _get_insight_activity(self, insight_id: int, expected_status: int = status.HTTP_200_OK):
         url = f"/api/projects/{self.team.id}/insights/{insight_id}/activity"
