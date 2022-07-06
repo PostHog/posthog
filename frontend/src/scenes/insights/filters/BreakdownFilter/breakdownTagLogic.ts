42a9d61494003de7fd097e70a2f31163425f2c37
Rick Marron <rcmarron@gmail.com>
2022-07-06 16:17:29 -0700

    fix(session-analysis): breakdown tag control

---
+++ frontend/src/scenes/insights/filters/BreakdownFilter/breakdownTagLogic.ts
@@ -0,0 +1,43 @@
+import { actions, kea, key, listeners, path, props, reducers } from 'kea'
+import { FilterType } from '~/types'
+
+import type { breakdownTagLogicType } from './breakdownTagLogicType'
+
+export interface BreakdownTagLogicProps {
+    setFilters?: (filters: Partial<FilterType>) => void
+    logicKey?: string
+    filters?: Partial<FilterType>
+}
+
+export const breakdownTagLogic = kea<breakdownTagLogicType>([
+    props({} as BreakdownTagLogicProps),
+    key(({ logicKey }) => logicKey || 'global'),
+    path((key) => ['scenes', 'insights', 'BreakdownFilter', 'breakdownTagLogic', key]),
+    actions(() => ({
+        setUseHistogram: (useHistogram: boolean) => ({ useHistogram }),
+        setBinCount: (binCount: number | undefined) => ({ binCount }),
+    })),
+    reducers(({ props }) => ({
+        useHistogram: [
+            props.filters?.breakdown_histogram_bin_count !== undefined,
+            {
+                setUseHistogram: (_, { useHistogram }) => useHistogram,
+            },
+        ],
+        binCount: [
+            (props.filters?.breakdown_histogram_bin_count ?? 10) as number | undefined,
+            {
+                setBinCount: (_, { binCount }) => binCount,
+            },
+        ],
+    })),
+    listeners(({ props, values }) => ({
+        setUseHistogram: ({ useHistogram }) => {
+            props.setFilters?.({ breakdown_histogram_bin_count: useHistogram ? values.binCount : undefined })
+        },
+        setBinCount: ({ binCount }, breakpoint) => {
+            breakpoint(500)
+            props.setFilters?.({ breakdown_histogram_bin_count: values.useHistogram ? binCount : undefined })
+        },
+    })),
+])
