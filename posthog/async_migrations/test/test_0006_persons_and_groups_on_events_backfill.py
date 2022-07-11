6d147488eba7ae75ec1066d62b6996b2a46896a5
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-07-11 14:16:27 +0300

    Test for person columns

--- posthog/async_migrations/test/test_0006_persons_and_groups_on_events_backfill.py
+++ posthog/async_migrations/test/test_0006_persons_and_groups_on_events_backfill.py
@@ -1,30 +1,56 @@
 import json
-import re
+from typing import Dict, List
 
 import pytest
-from django.conf import settings
-from django.test import override_settings
 
 from posthog.async_migrations.runner import start_async_migration
-from posthog.async_migrations.setup import (
-    get_async_migration_definition,
-    reload_migration_definitions,
-    setup_async_migrations,
-)
+from posthog.async_migrations.setup import setup_async_migrations
 from posthog.async_migrations.test.util import AsyncMigrationBaseTest
-from posthog.client import query_with_columns, sync_execute
-from posthog.models.async_migration import AsyncMigration, MigrationStatus
-from posthog.models.person import Person
-from posthog.models.person.sql import KAFKA_PERSONS_TABLE_SQL, PERSONS_TABLE_MV_SQL, PERSONS_TABLE_SQL
-from posthog.models.signals import mute_selected_signals
-from posthog.redis import get_client
-from posthog.test.base import ClickhouseTestMixin
+from posthog.client import query_with_columns
+from posthog.models.event.util import create_event
+from posthog.models.person.util import create_person, create_person_distinct_id
+from posthog.models.utils import UUIDT
+from posthog.test.base import ClickhouseTestMixin, run_clickhouse_statement_in_parallel
 
 MIGRATION_NAME = "0006_persons_and_groups_on_events_backfill"
+
+
+uuid1, uuid2, uuid3 = [UUIDT() for _ in range(3)]
+
 
 def run_migration():
     setup_async_migrations(ignore_posthog_version=True)
     return start_async_migration(MIGRATION_NAME, ignore_posthog_version=True)
+
+
+def query_events() -> List[Dict]:
+    return query_with_columns(
+        """
+        SELECT
+            distinct_id,
+            person_id,
+            person_properties,
+            group0_properties,
+            group1_properties,
+            group2_properties,
+            group3_properties,
+            group4_properties,
+            $group_0,
+            $group_1,
+            $group_2,
+            $group_3,
+            $group_4,
+            formatDateTime(events.person_created_at, %(format)s) AS person_created_at,
+            formatDateTime(events.group0_created_at, %(format)s) AS group0_created_at,
+            formatDateTime(events.group1_created_at, %(format)s) AS group1_created_at,
+            formatDateTime(events.group2_created_at, %(format)s) AS group2_created_at,
+            formatDateTime(events.group3_created_at, %(format)s) AS group3_created_at,
+            formatDateTime(events.group4_created_at, %(format)s) AS group4_created_at
+        FROM events
+        ORDER BY distinct_id
+        """,
+        {"format": "%Y-%m-%dT%H:%M:%SZ"},
+    )
 
 
 @pytest.mark.async_migrations
@@ -38,9 +64,16 @@ class Test0006PersonsAndGroupsOnEventsBackfill(AsyncMigrationBaseTest, Clickhous
         super().tearDown()
 
     def clear_tables(self):
-        sync_execute("DROP TABLE IF EXISTS tmp_person_0006")
-        sync_execute("DROP TABLE IF EXISTS tmp_person_distinct_id2_0006")
-        sync_execute("DROP TABLE IF EXISTS tmp_groups_0006")
+        run_clickhouse_statement_in_parallel(
+            [
+                "DROP TABLE IF EXISTS tmp_person_0006",
+                "DROP TABLE IF EXISTS tmp_person_distinct_id2_0006",
+                "DROP TABLE IF EXISTS tmp_groups_0006",
+                "DROP DICTIONARY IF EXISTS person_dict",
+                "DROP DICTIONARY IF EXISTS person_distinct_id2_dict",
+                "DROP DICTIONARY IF EXISTS groups_dict",
+            ]
+        )
 
     def test_is_required_without_compression(self):
         pass
@@ -52,7 +85,65 @@ class Test0006PersonsAndGroupsOnEventsBackfill(AsyncMigrationBaseTest, Clickhous
         self.assertTrue(run_migration())
 
     def test_data_copy_persons(self):
-        pass
+        create_event(
+            event_uuid=uuid1, team=self.team, distinct_id="1", event="$pageview",
+        )
+        create_event(
+            event_uuid=uuid2, team=self.team, distinct_id="2", event="$pageview",
+        )
+        create_event(
+            event_uuid=uuid3, team=self.team, distinct_id="3", event="$pageview",
+        )
+        create_person(
+            team_id=self.team.pk,
+            version=0,
+            uuid=str(uuid1),
+            properties={"personprop": 1},
+            timestamp="2022-01-01T00:00:00Z",
+        )
+        create_person(
+            team_id=self.team.pk,
+            version=0,
+            uuid=str(uuid2),
+            properties={"personprop": 2},
+            timestamp="2022-01-02T00:00:00Z",
+        )
+        create_person_distinct_id(self.team.pk, "1", str(uuid1))
+        create_person_distinct_id(self.team.pk, "2", str(uuid1))
+        create_person_distinct_id(self.team.pk, "3", str(uuid2))
+
+        self.assertTrue(run_migration())
+
+        events = query_events()
+
+        self.assertEqual(len(events), 3)
+        self.assertDictContainsSubset(
+            {
+                "distinct_id": "1",
+                "person_id": uuid1,
+                "person_properties": json.dumps({"personprop": 1}),
+                "person_created_at": "2022-01-01T00:00:00Z",
+            },
+            events[0],
+        )
+        self.assertDictContainsSubset(
+            {
+                "distinct_id": "2",
+                "person_id": uuid1,
+                "person_properties": json.dumps({"personprop": 1}),
+                "person_created_at": "2022-01-01T00:00:00Z",
+            },
+            events[1],
+        )
+        self.assertDictContainsSubset(
+            {
+                "distinct_id": "3",
+                "person_id": uuid2,
+                "person_properties": json.dumps({"personprop": 2}),
+                "person_created_at": "2022-01-02T00:00:00Z",
+            },
+            events[2],
+        )
 
     def test_duplicated_data_persons(self):
         # Assert count in temporary tables.
