5cf4d23261599a267844a4e3cc61181d41543ef2
Guido Iaquinti <guido@posthog.com>
2022-07-13 10:56:22 +0200

    0.1 is not 0.01

--- posthog/settings/sentry.py
+++ posthog/settings/sentry.py
@@ -21,5 +21,5 @@ if not TEST:
             request_bodies="always",
             sample_rate=1.0,  # Configures the sample rate for error events, in the range of 0.0 to 1.0. The default is 1.0 which means that 100% of error events are sent. If set to 0.1 only 10% of error events will be sent. Events are picked randomly.
             send_default_pii=True,
-            traces_sample_rate=0.1,  # A number between 0 and 1, controlling the percentage chance a given transaction will be sent to Sentry. (0 represents 0% while 1 represents 100%.) Applies equally to all transactions created in the app.
+            traces_sample_rate=0.01,  # A number between 0 and 1, controlling the percentage chance a given transaction will be sent to Sentry. (0 represents 0% while 1 represents 100%.) Applies equally to all transactions created in the app.
         )
