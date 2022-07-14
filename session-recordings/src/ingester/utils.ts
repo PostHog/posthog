3573e40cba23bd08dc93c6899a40447a825abada
Rick Marron <rcmarron@gmail.com>
2022-07-14 07:52:28 -0700

    add metadata writing

--- session-recordings/src/ingester/utils.ts
+++ session-recordings/src/ingester/utils.ts
@@ -5,3 +5,18 @@ export const getEventGroupDataString = (recordingEventGroup: RecordingEventGroup
     const eventDataStrings = events.sort((a, b) => a.timestamp - b.timestamp).map((event) => event.value)
     return eventDataStrings.join('\n')
 }
+
+export const getEventGroupMetadata = (recordingEventGroup: RecordingEventGroup) => {
+    const eventSummaries = Object.values(recordingEventGroup.events)
+        .filter((event) => event.complete)
+        .sort((a, b) => a.timestamp - b.timestamp)
+        .map((event) =>
+            JSON.stringify({
+                eventSource: event.eventSource,
+                eventType: event.eventType,
+                windowId: event.windowId,
+                timestamp: event.timestamp,
+            })
+        )
+    return eventSummaries.join('\n')
+}
