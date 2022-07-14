2d6379452846f85606d305568fd074c528b44bd5
Paul D'Ambra <paul@posthog.com>
2022-07-14 21:11:15 +0100

    chore: measure the age of the dashboard queue

--- posthog/tasks/update_cache.py
+++ posthog/tasks/update_cache.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 from typing import Any, Dict, List, Optional, Tuple
@@ -230,10 +231,20 @@ def update_cached_items() -> Tuple[int, int]:
     logger.info("Found {} items to refresh".format(len(tasks)))
     taskset = group(tasks)
     taskset.apply_async()
+
+    # this is the number of cacheable items that match the query
     queue_depth = dashboard_tiles.count() + shared_insights.count()
     statsd.gauge("update_cache_queue_depth.shared_insights", shared_insights.count())
     statsd.gauge("update_cache_queue_depth.dashboards", dashboard_tiles.count())
     statsd.gauge("update_cache_queue_depth", queue_depth)
+
+    statsd.gauge("update_cache_queue.never_refreshed", dashboard_tiles.filter(last_refresh=None).count())
+
+    # how old is the next to be refreshed
+    oldest_previously_refreshed_tile: Optional[DashboardTile] = dashboard_tiles.exclude(last_refresh=None).first()
+    if oldest_previously_refreshed_tile and oldest_previously_refreshed_tile.last_refresh:
+        dashboard_cache_age = (datetime.datetime.now() - oldest_previously_refreshed_tile.last_refresh).total_seconds()
+        statsd.gauge("update_cache_queue.dashboards_lag", round(dashboard_cache_age))
 
     return len(tasks), queue_depth
 
