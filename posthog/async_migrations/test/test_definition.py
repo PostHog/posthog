1aa0e2a767bfa45f51d84a2414aa071b0cae8582
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-07-14 13:34:35 +0300

    Add tests for get_parameter

--- posthog/async_migrations/test/test_definition.py
+++ posthog/async_migrations/test/test_definition.py
@@ -2,13 +2,19 @@ import pytest
 from infi.clickhouse_orm.utils import import_submodules
 
 from posthog.async_migrations.definition import AsyncMigrationDefinition, AsyncMigrationOperation
-from posthog.async_migrations.setup import ASYNC_MIGRATIONS_EXAMPLE_MODULE_PATH
+from posthog.async_migrations.setup import (
+    ASYNC_MIGRATIONS_EXAMPLE_MODULE_PATH,
+    get_async_migration_definition,
+    setup_async_migrations,
+)
+from posthog.models.async_migration import AsyncMigration
+from posthog.models.instance_setting import set_instance_setting
 from posthog.test.base import BaseTest
 from posthog.version_requirement import ServiceVersionRequirement
 
 
+@pytest.mark.ee
 class TestAsyncMigrationDefinition(BaseTest):
-    @pytest.mark.ee
     def test_get_async_migration_definition(self):
         from posthog.async_migrations.examples.example import example_fn, example_rollback_fn
 
@@ -23,3 +29,22 @@ class TestAsyncMigrationDefinition(BaseTest):
         self.assertEqual(example_migration.operations[-1].fn, example_fn)
         self.assertEqual(example_migration.operations[-1].rollback_fn, example_rollback_fn)
         self.assertTrue(isinstance(example_migration.service_version_requirements[0], ServiceVersionRequirement))
+
+    def test_get_migration_instance_and_parameters(self):
+        set_instance_setting("ASYNC_MIGRATIONS_SHOW_PERSON_ON_EVENTS_MIGRATION", True)
+        setup_async_migrations(ignore_posthog_version=True)
+
+        MIGRATION_NAME = "0006_persons_and_groups_on_events_backfill"
+
+        definition = get_async_migration_definition(MIGRATION_NAME)
+        instance = AsyncMigration.objects.get(name=MIGRATION_NAME)
+
+        self.assertEqual(definition.migration_instance(), instance)
+
+        self.assertEqual(
+            definition.get_parameter("PERSON_DICT_CACHE_SIZE"), definition.parameters["PERSON_DICT_CACHE_SIZE"][0]
+        )
+
+        instance.parameters = {"PERSON_DICT_CACHE_SIZE": 123}
+        instance.save()
+        self.assertEqual(definition.get_parameter("PERSON_DICT_CACHE_SIZE"), 123)
