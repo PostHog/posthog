a81222794b8a1f2ed208af8bbbd304c6186e9d40
Paul D'Ambra <paul@posthog.com>
2022-07-18 22:05:07 +0100

    chore: gauge age of more tiles

--- posthog/tasks/test/test_update_cache.py
+++ posthog/tasks/test/test_update_cache.py
@@ -755,7 +755,10 @@ class TestUpdateCache(APIBaseTest):
     @patch("posthog.tasks.update_cache.statsd.gauge")
     def test_refresh_age_of_tiles_is_gauged(self, statsd_gauge: MagicMock) -> None:
         tile_one = self._a_dashboard_tile_with_known_last_refresh(datetime.now(pytz.utc) - timedelta(hours=1))
-        self._a_dashboard_tile_with_known_last_refresh(datetime.now(pytz.utc) - timedelta(hours=0.5))
+        tile_two = self._a_dashboard_tile_with_known_last_refresh(datetime.now(pytz.utc) - timedelta(hours=0.5))
+
+        # should not gauge because no last_refresh
+        self._a_dashboard_tile_with_known_last_refresh(None)
 
         update_cached_items()
 
@@ -769,7 +772,25 @@ class TestUpdateCache(APIBaseTest):
             },
         )
 
-    def _a_dashboard_tile_with_known_last_refresh(self, last_refresh_date: datetime) -> DashboardTile:
+        statsd_gauge.assert_any_call(
+            "update_cache_queue.dashboards_lag",
+            1800,
+            tags={
+                "insight_id": tile_two.insight_id,
+                "dashboard_id": tile_two.dashboard_id,
+                "cache_key": tile_two.filters_hash,
+            },
+        )
+
+        # the tile with no last refresh isn't gauged for lag
+        lag_calls = [
+            x.args[0]
+            for x in statsd_gauge.mock_calls
+            if len(x.args) > 0 and x.args[0] == "update_cache_queue.dashboards_lag"
+        ]
+        assert len(lag_calls) == 2
+
+    def _a_dashboard_tile_with_known_last_refresh(self, last_refresh_date: Optional[datetime]) -> DashboardTile:
         dashboard = create_shared_dashboard(team=self.team, is_shared=True)
         filter = {"events": [{"id": "$pageview"}]}
         item = Insight.objects.create(filters=filter, team=self.team)
