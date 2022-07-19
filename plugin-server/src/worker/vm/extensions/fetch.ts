7f1d2223ad3e7cf730c28c7d744198ecfab20221
yakkomajuri <yakko.majuri@gmail.com>
2022-07-19 14:30:45 +0000

    fixes

--- plugin-server/src/worker/vm/extensions/fetch.ts
+++ plugin-server/src/worker/vm/extensions/fetch.ts
@@ -1,6 +1,6 @@
 import http from 'node:http'
 import https from 'node:https'
-import fetch, { RequestInfo, RequestInit } from 'node-fetch'
+import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch'
 
 // set keepAlive to make requests faster as we're constantly hitting the same endpoints for e.g. event exports
 const httpAgent = new http.Agent({
@@ -11,16 +11,10 @@ const httpsAgent = new https.Agent({
     keepAlive: true,
 })
 
-export async function fetchExtension(url: RequestInfo, init?: RequestInit) {
+export async function fetchExtension(url: RequestInfo, init?: RequestInit): Promise<Response> {
     init = init ?? {}
     return await fetch(url, {
-        agent: function (_parsedURL) {
-            if (_parsedURL.protocol == 'http:') {
-                return httpAgent
-            } else {
-                return httpsAgent
-            }
-        },
+        agent: (_parsedURL) => (_parsedURL.protocol == 'http:' ? httpAgent : httpsAgent),
         ...init,
     })
 }
