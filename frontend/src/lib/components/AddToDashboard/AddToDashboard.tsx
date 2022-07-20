f8a661c5fe25af921f3717ccbfc015401a0c28d4
Paul D'Ambra <paul@posthog.com>
2022-07-20 15:48:31 +0100

    fix: have only way to create new dashboards (#10841)
    * chore: remove the multi dashboard insight flag
    
    * rename modal now flag is removed
    
    * hook the add to dashboard modal and the new dashboard modal together, allowing user to choose whether to load the dashboard on success
    
    * set show on success to false from insight add to dashboard modal
    
    * wire up description
    
    * use checkbox in form, not switch
    
    * use buttons not checkbox

--- frontend/src/lib/components/AddToDashboard/AddToDashboard.tsx
+++ frontend/src/lib/components/AddToDashboard/AddToDashboard.tsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react'
-import { AddToDashboardModal } from './SaveToDashboardModal'
+import { AddToDashboardModal } from './AddToDashboardModal'
 import { InsightModel } from '~/types'
 import { dashboardsModel } from '~/models/dashboardsModel'
 import { useValues } from 'kea'
 import { LemonButton } from '../LemonButton'
 import { IconGauge, IconWithCount } from 'lib/components/icons'
+import { NewDashboardModal } from 'scenes/dashboard/NewDashboardModal'
 
 interface SaveToDashboardProps {
     insight: Partial<InsightModel>
     canEditInsight: boolean
 }
 
-export function SaveToDashboard({ insight, canEditInsight }: SaveToDashboardProps): JSX.Element {
+export function AddToDashboard({ insight, canEditInsight }: SaveToDashboardProps): JSX.Element {
     const [openModal, setOpenModal] = useState<boolean>(false)
     const { rawDashboards } = useValues(dashboardsModel)
     const dashboards = insight.dashboards?.map((dashboard) => rawDashboards[dashboard]).filter((d) => !!d) || []
 
     return (
         <span className="save-to-dashboard" data-attr="save-to-dashboard-button">
+            <NewDashboardModal />
             <AddToDashboardModal
                 visible={openModal}
                 closeModal={() => setOpenModal(false)}
