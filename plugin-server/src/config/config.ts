225a41db7219f5dfe64dad9e0897de2608855ce8
Harry Waye <harry@posthog.com>
2022-07-21 15:29:31 +0100

    chore(dev): use network mode host for docker-compose services (#10917)
    * chore(dev): use network mode host for docker-compose services
    
    This removes the need to add kafka to /etc/hosts.
    
    As far as I can tell this should be fine for peoples local dev except
    they will be required to reset and re-migrate ClickHouse tables as they
    will be trying to pull from `kafka` instead of `localhost`.
    
    * remove ports from redis
    
    * Update a few more references

--- plugin-server/src/config/config.ts
+++ plugin-server/src/config/config.ts
@@ -29,7 +29,7 @@ export function getDefaultConfig(): PluginsServerConfig {
         CLICKHOUSE_CA: null,
         CLICKHOUSE_SECURE: false,
         CLICKHOUSE_DISABLE_EXTERNAL_SCHEMAS: true,
-        KAFKA_HOSTS: 'kafka:9092', // KEEP IN SYNC WITH posthog/settings/data_stores.py
+        KAFKA_HOSTS: 'localhost:9092', // KEEP IN SYNC WITH posthog/settings/data_stores.py
         KAFKA_CLIENT_CERT_B64: null,
         KAFKA_CLIENT_CERT_KEY_B64: null,
         KAFKA_TRUSTED_CERT_B64: null,
