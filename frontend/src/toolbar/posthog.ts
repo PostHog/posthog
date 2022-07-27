178334698bc34b8bb7a898bb50ccd3fa4b8e05a0
Ben White <ben@benjackwhite.co.uk>
2022-07-27 11:29:19 +0200

    Swap to alpha versions of other libs

--- frontend/src/toolbar/posthog.ts
+++ frontend/src/toolbar/posthog.ts
@@ -1,9 +1,10 @@
-import { browserPostHog } from 'posthog-js-lite/dist/src/targets/browser'
+import PostHog from 'posthog-js-lite'
 
 const apiKey = 'sTMFPsFhdP1Ssg'
 const apiHost = 'https://app.posthog.com'
 
-export const posthog = browserPostHog(apiKey, {
-    apiHost: apiHost,
-    optedIn: false, // must call .optIn() before any events are sent
+export const posthog = new PostHog(apiKey, {
+    host: apiHost,
+    enable: false, // must call .optIn() before any events are sent
+    persistence_name: apiKey + '_toolbar',
 })
