c085c1c280b6ad0d79579020d25ee11945e7fa4c
Paul D'Ambra <paul@posthog.com>
2022-07-28 22:31:53 +0100

    correct the comment

--- posthog/celery.py
+++ posthog/celery.py
@@ -446,9 +446,6 @@ def update_cache_item_task(key: str, cache_type, payload: dict) -> List[Dict[str
     Tasks used in a group (as this is) must not ignore their results
     https://docs.celeryq.dev/en/latest/userguide/canvas.html#groups:~:text=Similarly%20to%20chords%2C%20tasks%20used%20in%20a%20group%20must%20not%20ignore%20their%20results.
 
-    When running to an unconstrained rate limit with a high rate of producer enqueuing this task
-    we reached 65 executions per minute across 5 workers before ClickHouse queries started to slow down.
-
     By setting a rate limit we can keep the queue of tasks full so that workers aren't idle
     Without overwhelming ClickHouse
 
