3c6602602be2ba904be921873427b71348945e6b
Ben White <ben@benjackwhite.co.uk>
2022-07-28 10:27:24 +0200

    Update to allow toolbar to selfcapture

--- frontend/src/toolbar/posthog.ts
+++ frontend/src/toolbar/posthog.ts
@@ -1,10 +1,15 @@
 import PostHog from 'posthog-js-lite'
 
-const apiKey = 'sTMFPsFhdP1Ssg'
-const apiHost = 'https://app.posthog.com'
+const runningOnPosthog = !!window.POSTHOG_APP_CONTEXT
+const apiKey = runningOnPosthog ? window.JS_POSTHOG_API_KEY : 'sTMFPsFhdP1Ssg'
+const apiHost = runningOnPosthog ? window.JS_POSTHOG_HOST : 'https://app.posthog.com'
 
 export const posthog = new PostHog(apiKey, {
     host: apiHost,
     enable: false, // must call .optIn() before any events are sent
     persistence_name: apiKey + '_toolbar',
 })
+
+if (runningOnPosthog && window.JS_POSTHOG_SELF_CAPTURE) {
+    posthog.debug()
+}
