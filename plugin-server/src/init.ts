71bd7f11f04b1a9605f4fadc93993b1381a665cc
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-07-28 11:54:40 +0300

    Experimental tracing support for plugin server

--- plugin-server/src/init.ts
+++ plugin-server/src/init.ts
@@ -1,10 +1,15 @@
 import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
+import { Span, SpanContext, TransactionContext } from '@sentry/types'
+import { AsyncLocalStorage } from 'node:async_hooks'
 
 import { PluginsServerConfig } from './types'
 import { setLogLevel } from './utils/utils'
 
 // Must require as `tsc` strips unused `import` statements and just requiring this seems to init some globals
 require('@sentry/tracing')
+
+const asyncLocalStorage = new AsyncLocalStorage<Tracing.Span>()
 
 // Code that runs on app start, in both the main and worker threads
 export function initApp(config: PluginsServerConfig): void {
@@ -19,6 +24,40 @@ export function initApp(config: PluginsServerConfig): void {
                     PLUGIN_SERVER_MODE: config.PLUGIN_SERVER_MODE,
                 },
             },
+            tracesSampleRate: 1,
         })
+    }
+}
+
+export function getSpan(): Tracing.Span | undefined {
+    return asyncLocalStorage.getStore()
+}
+
+export function runInTransaction<T>(transactionContext: TransactionContext, callback: () => Promise<T>): Promise<T> {
+    const transaction = Sentry.startTransaction(transactionContext)
+    return asyncLocalStorage.run(transaction, async () => {
+        try {
+            const result = await callback()
+            return result
+        } finally {
+            transaction.finish()
+        }
+    })
+}
+
+export function runInSpan<T>(spanContext: SpanContext, callback: (span?: Span) => Promise<T>): Promise<T> {
+    const parentSpan = getSpan()
+    if (parentSpan) {
+        const span = parentSpan.startChild(spanContext)
+        return asyncLocalStorage.run(span, async () => {
+            try {
+                const result = await callback()
+                return result
+            } finally {
+                span.finish()
+            }
+        })
+    } else {
+        return callback()
     }
 }
--- plugin-server/src/worker/worker.ts
+++ plugin-server/src/worker/worker.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/node'
 
-import { initApp } from '../init'
+import { initApp, runInTransaction } from '../init'
 import { Hub, PluginsServerConfig } from '../types'
 import { processError } from '../utils/db/error'
 import { createHub } from '../utils/db/hub'
@@ -30,35 +30,43 @@ export async function createWorker(config: PluginsServerConfig, threadId: number
 
 export const createTaskRunner =
     (hub: Hub): PiscinaTaskWorker =>
-    async ({ task, args }) => {
-        const timer = new Date()
-        let response
+    ({ task, args }) =>
+        runInTransaction(
+            {
+                op: 'piscina task',
+                name: task,
+                data: args,
+            },
+            async () => {
+                const timer = new Date()
+                let response
 
-        Sentry.setContext('task', { task, args })
+                Sentry.setContext('task', { task, args })
 
-        if (task in workerTasks) {
-            try {
-                // must clone the object, as we may get from VM2 something like { ..., properties: Proxy {} }
-                response = cloneObject(await workerTasks[task](hub, args))
-            } catch (e) {
-                status.info('🔔', e)
-                Sentry.captureException(e)
-                response = { error: e.message }
+                if (task in workerTasks) {
+                    try {
+                        // must clone the object, as we may get from VM2 something like { ..., properties: Proxy {} }
+                        response = cloneObject(await workerTasks[task](hub, args))
+                    } catch (e) {
+                        status.info('🔔', e)
+                        Sentry.captureException(e)
+                        response = { error: e.message }
+                    }
+                } else {
+                    response = { error: `Worker task "${task}" not found in: ${Object.keys(workerTasks).join(', ')}` }
+                }
+
+                hub.statsd?.timing(`piscina_task.${task}`, timer)
+                if (task === 'runPluginJob') {
+                    hub.statsd?.timing('plugin_job', timer, {
+                        type: String(args.job?.type),
+                        pluginConfigId: String(args.job?.pluginConfigId),
+                        pluginConfigTeam: String(args.job?.pluginConfigTeam),
+                    })
+                }
+                return response
             }
-        } else {
-            response = { error: `Worker task "${task}" not found in: ${Object.keys(workerTasks).join(', ')}` }
-        }
-
-        hub.statsd?.timing(`piscina_task.${task}`, timer)
-        if (task === 'runPluginJob') {
-            hub.statsd?.timing('plugin_job', timer, {
-                type: String(args.job?.type),
-                pluginConfigId: String(args.job?.pluginConfigId),
-                pluginConfigTeam: String(args.job?.pluginConfigTeam),
-            })
-        }
-        return response
-    }
+        )
 
 export function processUnhandledRejections(error: Error, server: Hub): void {
     const pluginConfigId = pluginConfigIdFromStack(error.stack || '', server.pluginConfigSecretLookup)
