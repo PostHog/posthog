e527227431c47d6b0b362e9e495af6537de9cbbf
Ben White <ben@benjackwhite.co.uk>
2022-07-28 11:19:27 +0200

    Fix posthog toolbar to be memory only

--- frontend/src/toolbar/posthog.ts
+++ frontend/src/toolbar/posthog.ts
@@ -7,7 +7,8 @@ const apiHost = runningOnPosthog ? window.JS_POSTHOG_HOST : 'https://app.posthog
 export const posthog = new PostHog(apiKey, {
     host: apiHost,
     enable: false, // must call .optIn() before any events are sent
-    persistence_name: apiKey + '_toolbar',
+    persistence: 'memory', // We don't want to persist anything, all events are in-memory
+    persistence_name: apiKey + '_toolbar', // We don't need this but it ensures we don't accidentally mess with the standard persistence
 })
 
 if (runningOnPosthog && window.JS_POSTHOG_SELF_CAPTURE) {
