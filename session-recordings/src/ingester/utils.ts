580f488cd711b89e859808e0312b1ea661e4cdcd
Rick Marron <rcmarron@gmail.com>
2022-07-27 18:32:17 -0700

    fix(recording-ingester): handle concurrency in the ingester

--- session-recordings/src/ingester/utils.ts
+++ session-recordings/src/ingester/utils.ts
@@ -1,8 +1,11 @@
-import { RecordingEventGroup } from '../types'
+import { KafkaMessage, Message } from 'kafkajs'
+import { RecordingEvent, RecordingEventGroup, RecordingMessage } from '../types'
 
 export const getEventGroupDataString = (recordingEventGroup: RecordingEventGroup) => {
     const events = Object.values(recordingEventGroup.events)
-    const eventDataStrings = events.sort((a, b) => a.timestamp - b.timestamp).map((event) => event.value)
+    const eventDataStrings = events
+        .sort((a, b) => a.timestamp - b.timestamp)
+        .map((event) => event.messages.map((message) => message.value).join(''))
     return eventDataStrings.join('\n')
 }
 
@@ -19,4 +22,51 @@ export const getEventSummaryMetadata = (recordingEventGroup: RecordingEventGroup
             })
         )
     return eventSummaries.join('\n')
+}
+
+export const getEventSize = (recordingEvent: RecordingEvent) => {
+    return recordingEvent.messages.reduce((acc, message) => acc + message.value.length, 0)
+}
+
+export const convertRecordingMessageToKafkaMessage = (recordingMessage: RecordingMessage): Message => {
+    return {
+        key: recordingMessage.kafkaKey,
+        value: recordingMessage.value,
+        headers: {
+            sessionId: recordingMessage.sessionId,
+            windowId: recordingMessage.windowId,
+            eventId: recordingMessage.eventId,
+            distinctId: recordingMessage.distinctId,
+            eventSource: recordingMessage.eventSource.toString(),
+            eventType: recordingMessage.eventType.toString(),
+            teamId: recordingMessage.teamId.toString(),
+            unixTimestamp: recordingMessage.timestamp.toString(),
+            chunkCount: recordingMessage.chunkCount.toString(),
+            chunkIndex: recordingMessage.chunkIndex.toString(),
+        },
+    }
+}
+
+export const convertKafkaMessageToRecordingMessage = (
+    kafkaMessage: KafkaMessage,
+    topic: string,
+    partition: number
+): RecordingMessage => {
+    return {
+        sessionId: kafkaMessage.headers.sessionId.toString(),
+        windowId: kafkaMessage.headers.windowId.toString(),
+        eventId: kafkaMessage.headers.eventId.toString(),
+        distinctId: kafkaMessage.headers.distinctId.toString(),
+        eventSource: Number.parseInt(kafkaMessage.headers.eventSource.toString()),
+        eventType: Number.parseInt(kafkaMessage.headers.eventType.toString()),
+        teamId: Number.parseInt(kafkaMessage.headers.teamId.toString()),
+        timestamp: Number.parseInt(kafkaMessage.headers.unixTimestamp.toString()),
+        chunkCount: Number.parseInt(kafkaMessage.headers.chunkCount.toString()),
+        chunkIndex: Number.parseInt(kafkaMessage.headers.chunkIndex.toString()),
+        value: kafkaMessage.value.toString(),
+        kafkaOffset: Number.parseInt(kafkaMessage.offset),
+        kafkaTopic: topic,
+        kafkaPartition: partition,
+        kafkaKey: kafkaMessage.key?.toString(),
+    }
 }
--- session-recordings/src/types.ts
+++ session-recordings/src/types.ts
@@ -1,4 +1,5 @@
 export type RecordingEventGroup = {
+    id: string
     teamId: number
     sessionId: string
     distinctId: string // OK if this distinct ID changes through the recording, we just need to store a single distinct ID
@@ -6,17 +7,43 @@ export type RecordingEventGroup = {
     events: Record<string, RecordingEvent>
     size: number
     oldestEventTimestamp: number
-    oldestOffset: string
-    newestOffset: string
+    oldestOffset: number
+    newestOffset: number
     timer?: NodeJS.Timeout
+    status?: 'sending' | 'active'
 }
 
 export type RecordingEvent = {
     eventId: string
-    value: string
+    messages: RecordingMessage[]
     complete: boolean
     timestamp: number
     eventType: number
     eventSource: number
     windowId: string
+    sessionId: string
+    distinctId: string
+    teamId: number
+    kafkaTopic: string
+    kafkaPartition: number
+    oldestOffset: number
+    newestOffset: number
+}
+
+export type RecordingMessage = {
+    eventId: string
+    value: string
+    timestamp: number
+    eventType: number
+    eventSource: number
+    windowId: string
+    sessionId: string
+    distinctId: string
+    chunkCount: number
+    chunkIndex: number
+    teamId: number
+    kafkaTopic: string
+    kafkaPartition: number
+    kafkaOffset: number
+    kafkaKey: string
 }
