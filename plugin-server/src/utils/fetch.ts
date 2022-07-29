17b98f4e8c985bca1f406201ecc061f4e7d35566
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-07-29 10:19:39 +0300

    Trace node-fetch

---
+++ plugin-server/src/utils/fetch.ts
@@ -0,0 +1,21 @@
+// This module wraps node-fetch with a sentry tracing-aware extension
+
+import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch'
+import { Request } from 'node-fetch'
+
+import { runInSpan } from '../sentry'
+
+function fetchWrapper(url: RequestInfo, init?: RequestInit): Promise<Response> {
+    const request = new Request(url, init)
+    return runInSpan(
+        {
+            op: 'fetch',
+            description: `${request.method} ${request.hostname}`,
+        },
+        () => fetch(url, init)
+    )
+}
+
+fetchWrapper.isRedirect = fetch.isRedirect
+
+export default fetchWrapper
