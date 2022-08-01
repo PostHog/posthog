8381b3e2c2ac54c0059c1643504a4a2772f96857
Ben White <ben@benjackwhite.co.uk>
2022-08-01 10:15:24 +0200

    feat: Embedded dashboard dimension emits

--- frontend/src/exporter/Exporter.tsx
+++ frontend/src/exporter/Exporter.tsx
@@ -1,18 +1,34 @@
 import '~/styles'
 import './Exporter.scss'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ExportedData, ExportType } from '~/exporter/types'
 import { DashboardPlacement } from '~/types'
 import { ExportedInsight } from '~/exporter/ExportedInsight/ExportedInsight'
 import { FriendlyLogo } from '~/toolbar/assets/FriendlyLogo'
 import { Dashboard } from 'scenes/dashboard/Dashboard'
+import { useResizeObserver } from 'lib/hooks/useResizeObserver'
+import clsx from 'clsx'
 
 export function Exporter(props: ExportedData): JSX.Element {
     const { type, dashboard, insight, team, ...exportOptions } = props
     const { whitelabel } = exportOptions
 
+    const { ref: elementRef, height, width } = useResizeObserver()
+
+    useEffect(() => {
+        // NOTE: For embedded views we emit an event to indicate the content width / height to allow the parent to correctly resize
+        // NOTE: We post the window name to allow the parent to identify the iframe
+        window.parent?.postMessage({ event: 'posthog:dimensions', name: window.name, height, width }, '*')
+    }, [height, width])
+
     return (
-        <div className="Exporter">
+        <div
+            className={clsx('Exporter', {
+                'Export--insight': !!insight,
+                'Export--dashboard': !!dashboard,
+            })}
+            ref={elementRef}
+        >
             {!whitelabel && dashboard ? (
                 type === ExportType.Scene ? (
                     <div className="SharedDashboard-header">
@@ -57,7 +73,7 @@ export function Exporter(props: ExportedData): JSX.Element {
             )}
 
             {!whitelabel && dashboard && (
-                <div className="text-center pb ma">
+                <div className="text-center pb">
                     {type === ExportType.Image ? <FriendlyLogo style={{ fontSize: '1.125rem' }} /> : null}
                     <div>
                         Made with{' '}
