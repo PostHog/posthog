0d8f0065379c76f08179c1ea5638d3d091ac3a62
Rick Marron <rickmarron@Ricks-MacBook-Pro.local>
2022-08-01 19:12:03 -0700

    fix issue from merge with master

--- posthog/helpers/session_recording.py
+++ posthog/helpers/session_recording.py
@@ -4,7 +4,7 @@ import gzip
 import json
 from collections import defaultdict
 from datetime import datetime, timedelta, timezone
-from typing import DefaultDict, Dict, Generator, List, Optional
+from typing import DefaultDict, Dict, Generator, List, Optional, Tuple, cast
 
 from sentry_sdk.api import capture_exception, capture_message
 
@@ -435,3 +435,93 @@ def get_session_recording_events_for_object_storage(
                     )
                 )
     return recording_events_for_object_storage
+
+
+def get_metadata_from_event_summaries(
+    event_summaries: List[RecordingEventSummary],
+) -> Tuple[List[RecordingSegment], Dict[WindowId, Dict]]:
+    """
+    This function processes the recording events into metadata.
+    A recording can be composed of events from multiple windows/tabs. Recording events are seperated by
+    `window_id`, so the playback experience is consistent (changes in one tab don't impact the recording
+    of a different tab). However, we still want to playback the recording to the end user as the user interacted
+    with their product.
+    This function creates a "playlist" of recording segments that designates the order in which the front end
+    should flip between players of different windows/tabs. To create this playlist, this function does the following:
+    (1) For each recording event, we determine if it is "active" or not. An active event designates user
+    activity (e.g. mouse movement).
+    (2) We then generate "active segments" based on these lists of events. Active segments are segments
+    of recordings where the maximum time between events determined to be active is less than a threshold (set to 60 seconds).
+    (3) Next, we merge the active segments from all of the window_ids + sort them by start time. We now have the
+    list of active segments. (note, it's very possible that active segments overlap if a user is flipping back
+    and forth between tabs)
+    (4) To complete the recording, we fill in the gaps between active segments with "inactive segments". In
+    determining which window should be used for the inactive segment, we try to minimize the switching of windows.
+    """
+
+    event_summaries_by_window_id: DefaultDict[WindowId, List[RecordingEventSummary]] = defaultdict(list)
+    for event_summary in event_summaries:
+        event_summaries_by_window_id[event_summary.window_id].append(event_summary)
+
+    start_and_end_times_by_window_id = {}
+
+    # Get the active segments for each window_id
+    all_active_segments: List[RecordingSegment] = []
+    for window_id, event_list in event_summaries_by_window_id.items():
+        # Not sure why, but events are sometimes slightly out of order
+        event_list.sort(key=lambda x: x.timestamp)
+
+        active_segments_for_window_id = get_active_segments_from_event_list(event_list, window_id)
+
+        all_active_segments.extend(active_segments_for_window_id)
+
+        start_and_end_times_by_window_id[window_id] = {
+            "start_time": event_list[0].timestamp,
+            "end_time": event_list[-1].timestamp,
+        }
+
+    # Sort the active segments by start time. This will interleave active segments
+    # from different windows
+    all_active_segments.sort(key=lambda segment: segment.start_time)
+
+    # These start and end times are used to make sure the segments span the entire recording
+    first_start_time = min([cast(datetime, x["start_time"]) for x in start_and_end_times_by_window_id.values()])
+    last_end_time = max([cast(datetime, x["end_time"]) for x in start_and_end_times_by_window_id.values()])
+
+    # Now, we fill in the gaps between the active segments with inactive segments
+    all_segments = []
+    current_timestamp = first_start_time
+    current_window_id: WindowId = sorted(
+        start_and_end_times_by_window_id, key=lambda x: start_and_end_times_by_window_id[x]["start_time"]
+    )[0]
+
+    for index, segment in enumerate(all_active_segments):
+        # It's possible that segments overlap and we don't need to fill a gap
+        if segment.start_time > current_timestamp:
+            all_segments.extend(
+                generate_inactive_segments_for_range(
+                    current_timestamp,
+                    segment.start_time,
+                    current_window_id,
+                    start_and_end_times_by_window_id,
+                    is_first_segment=index == 0,
+                )
+            )
+        all_segments.append(segment)
+        current_window_id = segment.window_id
+        current_timestamp = max(segment.end_time, current_timestamp)
+
+    # If the last segment ends before the recording ends, we need to fill in the gap
+    if current_timestamp < last_end_time:
+        all_segments.extend(
+            generate_inactive_segments_for_range(
+                current_timestamp,
+                last_end_time,
+                current_window_id,
+                start_and_end_times_by_window_id,
+                is_last_segment=True,
+                is_first_segment=current_timestamp == first_start_time,
+            )
+        )
+
+    return all_segments, start_and_end_times_by_window_id
