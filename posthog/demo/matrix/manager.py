35b0a1c703a3c32525e94966d65d35bc28c46f03
Michael Matloka <dev@twixes.com>
2022-08-02 17:27:07 +0200

    Save future events via Graphile queue

--- posthog/demo/matrix/manager.py
+++ posthog/demo/matrix/manager.py
@@ -1,9 +1,10 @@
 import datetime as dt
 import json
-from typing import Any, Dict, Literal, Optional, Tuple, cast
+from typing import Any, Dict, List, Literal, Optional, Tuple, cast
 
 from django.conf import settings
 from django.core import exceptions
+from django.db import connection
 
 from posthog.client import query_with_columns, sync_execute
 from posthog.models import (
@@ -20,7 +21,7 @@ from posthog.models.utils import UUIDT
 from posthog.tasks.calculate_event_property_usage import calculate_event_property_usage_for_team
 
 from .matrix import Matrix
-from .models import SimPerson
+from .models import SimEvent, SimPerson
 
 
 class MatrixManager:
@@ -77,42 +78,42 @@ class MatrixManager:
         return team
 
     def run_on_team(self, team: Team, user: User):
-        if not self.pre_save or not self.is_demo_data_pre_saved():
+        if not self.pre_save or not self._is_demo_data_pre_saved():
             if self.matrix.simulation_complete is None:
                 self.matrix.simulate()
-            self.save_analytics_data(self.MASTER_TEAM if self.pre_save else team)
+            self._save_analytics_data(self.MASTER_TEAM if self.pre_save else team)
         if self.pre_save:
-            self.copy_analytics_data_from_master_team(team)
-        self.sync_postgres_with_clickhouse_data(team)
+            self._copy_analytics_data_from_master_team(team)
+        self._sync_postgres_with_clickhouse_data(team)
         self.matrix.set_project_up(team, user)
         calculate_event_property_usage_for_team(team.pk)
         team.save()
 
-    def save_analytics_data(self, target_team: Team):
+    def _save_analytics_data(self, target_team: Team):
         if target_team.pk == self.MASTER_TEAM_ID:
-            self.prepare_master_team()
+            self._prepare_master_team()
         bulk_group_type_mappings = []
         for group_type_index, (group_type, groups) in enumerate(self.matrix.groups.items()):
             bulk_group_type_mappings.append(
                 GroupTypeMapping(team=target_team, group_type_index=group_type_index, group_type=group_type)
             )
             for group_key, group in groups.items():
-                self.save_sim_group(
+                self._save_sim_group(
                     target_team, cast(Literal[0, 1, 2, 3, 4], group_type_index), group_key, group, self.matrix.now
                 )
         GroupTypeMapping.objects.bulk_create(bulk_group_type_mappings)
         sim_persons = self.matrix.people
         for sim_person in sim_persons:
-            self.save_sim_person(target_team, sim_person)
+            self._save_sim_person(target_team, sim_person)
 
     @classmethod
-    def prepare_master_team(cls):
+    def _prepare_master_team(cls):
         if not Team.objects.filter(id=cls.MASTER_TEAM_ID).exists():
             organization = Organization.objects.create(name="PostHog")
             cls.create_team(organization, id=cls.MASTER_TEAM_ID, name="Master")
 
     @classmethod
-    def copy_analytics_data_from_master_team(cls, target_team: Team):
+    def _copy_analytics_data_from_master_team(cls, target_team: Team):
         from posthog.models.event.sql import COPY_EVENTS_BETWEEN_TEAMS
         from posthog.models.group.sql import COPY_GROUPS_BETWEEN_TEAMS
         from posthog.models.person.sql import COPY_PERSON_DISTINCT_ID2S_BETWEEN_TEAMS, COPY_PERSONS_BETWEEN_TEAMS
@@ -132,7 +133,7 @@ class MatrixManager:
         )
 
     @classmethod
-    def sync_postgres_with_clickhouse_data(cls, target_team: Team):
+    def _sync_postgres_with_clickhouse_data(cls, target_team: Team):
         from posthog.models.group.sql import SELECT_GROUPS_OF_TEAM
         from posthog.models.person.sql import SELECT_PERSON_DISTINCT_ID2S_OF_TEAM, SELECT_PERSONS_OF_TEAM
 
@@ -172,18 +173,26 @@ class MatrixManager:
             bulk_groups.append(Group(team_id=target_team.pk, version=0, **row))
         Group.objects.bulk_create(bulk_groups)
 
-    @staticmethod
-    def save_sim_person(team: Team, subject: SimPerson):
-        if not subject.past_events:
-            return  # Don't save a person who never participated
-        from posthog.models.event.util import create_event
-        from posthog.models.person.util import create_person, create_person_distinct_id
+    @classmethod
+    def _save_sim_person(cls, team: Team, subject: SimPerson):
+        # We only want to save directly if there are past events
+        if subject.past_events:
+            from posthog.models.person.util import create_person, create_person_distinct_id
 
-        person_uuid_str = str(UUIDT(unix_time_ms=int(subject.past_events[0].timestamp.timestamp() * 1000)))
-        create_person(uuid=person_uuid_str, team_id=team.pk, properties=subject.properties_at_now, version=0)
-        for distinct_id in subject.distinct_ids_at_now:
-            create_person_distinct_id(team_id=team.pk, distinct_id=str(distinct_id), person_id=person_uuid_str)
-        for event in subject.past_events:
+            person_uuid_str = str(UUIDT(unix_time_ms=int(subject.past_events[0].timestamp.timestamp() * 1000)))
+            create_person(uuid=person_uuid_str, team_id=team.pk, properties=subject.properties_at_now, version=0)
+            for distinct_id in subject.distinct_ids_at_now:
+                create_person_distinct_id(team_id=team.pk, distinct_id=str(distinct_id), person_id=person_uuid_str)
+            cls._save_past_sim_events(team, subject.past_events)
+        # We only want to queue future events if there are any
+        if subject.future_events:
+            cls._save_future_sim_events(team, subject.future_events)
+
+    @staticmethod
+    def _save_past_sim_events(team: Team, events: List[SimEvent]):
+        from posthog.models.event.util import create_event
+
+        for event in events:
             event_uuid = UUIDT(unix_time_ms=int(event.timestamp.timestamp() * 1000))
             create_event(
                 event_uuid=event_uuid,
@@ -195,7 +204,33 @@ class MatrixManager:
             )
 
     @staticmethod
-    def save_sim_group(
+    def _save_future_sim_events(team: Team, events: List[SimEvent]):
+        params: List[str] = []
+        for event in events:
+            event_uuid = UUIDT(unix_time_ms=int(event.timestamp.timestamp() * 1000))
+            timestamp_iso = event.timestamp.isoformat()
+            payload = {
+                "eventPayload": {
+                    "distinct_id": event.properties["$distinct_id"],
+                    "team_id": team.pk,
+                    "now": timestamp_iso,
+                    "timestamp": timestamp_iso,
+                    "event": event.event,
+                    "uuid": str(event_uuid),
+                }
+            }
+            params.append(json.dumps(payload))
+            params.append(timestamp_iso)
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"""
+                INSERT INTO graphile_worker.jobs (task_identifier, payload, run_at, max_attempts)
+                VALUES {", ".join(("('bufferJob', %s::json, %s::timestamptz, 1)" for _ in range(len(events))))}""",
+                params,
+            )
+
+    @staticmethod
+    def _save_sim_group(
         team: Team, type_index: Literal[0, 1, 2, 3, 4], key: str, properties: Dict[str, Any], timestamp: dt.datetime
     ):
         from posthog.models.group.util import raw_create_group_ch
@@ -203,7 +238,7 @@ class MatrixManager:
         raw_create_group_ch(team.pk, type_index, key, properties, timestamp)
 
     @classmethod
-    def is_demo_data_pre_saved(cls) -> bool:
+    def _is_demo_data_pre_saved(cls) -> bool:
         from posthog.models.event.sql import GET_TOTAL_EVENTS_VOLUME
 
         total_events_volume = sync_execute(GET_TOTAL_EVENTS_VOLUME, {"team_id": cls.MASTER_TEAM_ID})[0][0]
