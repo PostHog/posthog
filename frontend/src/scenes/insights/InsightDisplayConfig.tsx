a4faf6b49395faef637870415cf38ddc4d799631
Paul D'Ambra <paul@posthog.com>
2022-08-02 20:59:21 +0100

    more spacing

--- frontend/src/scenes/insights/InsightDisplayConfig.tsx
+++ frontend/src/scenes/insights/InsightDisplayConfig.tsx
@@ -157,7 +157,7 @@ export function InsightDisplayConfig({ filters, activeView, disableTable }: Insi
                     </ConfigFilter>
                 )}
             </div>
-            <div className="flex items-center space-x-2 flex-wrap my-2">
+            <div className="flex items-center space-x-4 flex-wrap my-2">
                 {activeView === InsightType.TRENDS && (
                     <ConfigFilter>
                         <span>Aggregation Axis format</span>
