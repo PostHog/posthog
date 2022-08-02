ff65854b3164e9375a876e0a7af5c535205a7763
Paul D'Ambra <paul@posthog.com>
2022-08-02 07:48:22 +0100

    make your mind up: arrow functions or not

--- frontend/src/scenes/insights/yAxisFormat.ts
+++ frontend/src/scenes/insights/yAxisFormat.ts
@@ -4,22 +4,21 @@ import { compactNumber, humanFriendlyDuration } from 'lib/utils'
 const formats = ['numeric', 'duration', 'percentage'] as const
 export type YAxisFormat = typeof formats[number]
 
-export const isYAxisFormat = (candidate: unknown): candidate is YAxisFormat => {
-    return formats.includes(candidate as YAxisFormat)
-}
+export const isYAxisFormat = (candidate: unknown): candidate is YAxisFormat =>
+    formats.includes(candidate as YAxisFormat)
 
 export const yAxisFormatSelectOptions = formats.reduce((target, format) => {
     target[format as string] = { label: format as string }
     return target
 }, {} as LemonSelectOptions)
 
-function toPercentage(value: number | string): string {
+const toPercentage = (value: number | string): string => {
     const numVal = Number(value)
     const fixedValue = numVal < 1 ? numVal.toFixed(2) : numVal.toFixed(0)
     return `${fixedValue}%`
 }
 
-export function formatYAxisValue(yAxisFormat: YAxisFormat, value: number | string): string {
+export const formatYAxisValue = (yAxisFormat: YAxisFormat, value: number | string): string => {
     switch (yAxisFormat) {
         case 'duration':
             return humanFriendlyDuration(value)
