bff32bfd88680edb4e8e166336d3bbbc0fa32d28
Tim Glaser <tim@glsr.nl>
2022-08-03 19:41:55 +0100

    fix tests

--- ee/clickhouse/queries/test/test_paths.py
+++ ee/clickhouse/queries/test/test_paths.py
@@ -7,7 +7,8 @@ from django.test import TestCase
 from django.utils import timezone
 from freezegun import freeze_time
 
-from ee.clickhouse.queries.paths.paths_event_query import PathEventQuery
+from ee.clickhouse.queries.paths import ClickhousePaths as Paths
+from ee.clickhouse.queries.paths import ClickhousePathsActors as PathsActors
 from posthog.constants import (
     FUNNEL_PATH_AFTER_STEP,
     FUNNEL_PATH_BEFORE_STEP,
@@ -18,7 +19,7 @@ from posthog.models.filters import Filter, PathFilter
 from posthog.models.group.util import create_group
 from posthog.models.group_type_mapping import GroupTypeMapping
 from posthog.models.session_recording_event.util import create_session_recording_event
-from posthog.queries.paths import Paths, PathsActors
+from posthog.queries.paths.paths_event_query import PathEventQuery
 from posthog.queries.test.test_paths import paths_test_factory
 from posthog.test.base import _create_event, _create_person, snapshot_clickhouse_queries, test_with_materialized_columns
 
@@ -2845,7 +2846,6 @@ class TestClickhousePaths(paths_test_factory(Paths)):  # type: ignore
         self.assertCountEqual([p1.uuid, p2.uuid], [actor["id"] for actor in serialized_actors])
         matched_recordings = [actor["matched_recordings"] for actor in serialized_actors]
 
-        self.assertEqual([], matched_recordings[0])
         self.assertCountEqual(
             [
                 {
@@ -2869,8 +2869,9 @@ class TestClickhousePaths(paths_test_factory(Paths)):  # type: ignore
                     ],
                 },
             ],
-            matched_recordings[1],
+            matched_recordings[0],
         )
+        self.assertEqual([], matched_recordings[1])
 
     @snapshot_clickhouse_queries
     @freeze_time("2012-01-01T03:21:34.000Z")
