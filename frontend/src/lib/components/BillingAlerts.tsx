d3cf46d8ca860ff967c3ac4bc6533877c22ac06d
Emanuele Capparelli <k@emkpp.com>
2022-08-03 16:56:03 +0100

    feat: billing alerts redesign

--- frontend/src/lib/components/BillingAlerts.tsx
+++ frontend/src/lib/components/BillingAlerts.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useValues } from 'kea'
-import { WarningOutlined, AlertOutlined, ToolFilled } from '@ant-design/icons'
-import { Button, Card } from 'antd'
 import { billingLogic, BillingAlertType } from 'scenes/billing/billingLogic'
-import { LinkButton } from './LinkButton'
+import { Link } from 'lib/components/Link'
+import { IconWarningAmber } from './icons'
 
 export function BillingAlerts(): JSX.Element | null {
     const { billing } = useValues(billingLogic)
@@ -12,69 +11,57 @@ export function BillingAlerts(): JSX.Element | null {
     if (!alertToShow) {
         return null
     }
+    let message: JSX.Element | undefined
+    let isWarning = false
+    let isAlert = false
+    if (alertToShow === BillingAlertType.SetupBilling) {
+        isWarning = true
+        message = (
+            <p>
+                <b>Action needed!&nbsp;</b>
+                <Link href={billing?.subscription_url} data-attr="alert_setup_billing">
+                    {billing?.plan?.custom_setup_billing_message ||
+                        'Please finish setting up your billing information.'}
+                </Link>
+            </p>
+        )
+    }
+
+    if (alertToShow === BillingAlertType.UsageNearLimit) {
+        isWarning = true
+        message = (
+            <p>
+                <b>Warning!</b> You have already used{' '}
+                <b style={{ color: typeof strokeColor === 'string' ? strokeColor : 'inherit' }}>
+                    {percentage && percentage * 100}%
+                </b>{' '}
+                of your event allocation this month.{' '}
+                <Link to="/organization/billing" data-attr="trial_expired_link">
+                    {billing?.plan?.custom_setup_billing_message ||
+                        'To avoid losing data or access to it, upgrade your billing plan now.'}
+                </Link>
+            </p>
+        )
+    }
+
+    if (alertToShow === BillingAlertType.UsageLimitExceeded) {
+        isAlert = true
+        message = (
+            <p>
+                <b>Alert!</b> The monthly limit of events or billing limit for your organization has been exceeded.{' '}
+                <Link to="/organization/billing" data-attr="trial_expired_link">
+                    {billing?.plan?.custom_setup_billing_message ||
+                        'To avoid losing data or access to it, increase your billing limit now.'}
+                </Link>
+            </p>
+        )
+    }
 
     return (
-        <>
-            <div style={{ marginTop: '1.5rem' }} />
-            {alertToShow === BillingAlertType.SetupBilling && (
-                <Card>
-                    <div style={{ display: 'flex' }}>
-                        <div style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-                            <WarningOutlined className="text-warning" style={{ paddingRight: 8 }} />
-                            <b>Action needed!&nbsp;</b>
-                            {billing?.plan?.custom_setup_billing_message ||
-                                'Please finish setting up your billing information.'}
-                        </div>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                            <Button type="primary" href={billing?.subscription_url} icon={<ToolFilled />}>
-                                Set up now
-                            </Button>
-                        </div>
-                    </div>
-                </Card>
-            )}
-            {alertToShow === BillingAlertType.UsageNearLimit && (
-                <Card>
-                    <div style={{ display: 'flex' }}>
-                        <div style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-                            <WarningOutlined className="text-warning" style={{ paddingRight: 16 }} />
-                            <div>
-                                <b>Warning!</b> Nearing the monthly limit of events or billing limit for your
-                                organization. You have already used{' '}
-                                <b style={{ color: typeof strokeColor === 'string' ? strokeColor : 'inherit' }}>
-                                    {percentage && percentage * 100}%
-                                </b>{' '}
-                                of your event allocation this month. To avoid losing data or access to it,{' '}
-                                <b>we recommend upgrading</b> your billing plan now.
-                            </div>
-                        </div>
-                        <div style={{ display: 'flex', alignItems: 'center', paddingLeft: 16 }}>
-                            <LinkButton type="primary" to="/organization/billing">
-                                <ToolFilled /> Manage billing
-                            </LinkButton>
-                        </div>
-                    </div>
-                </Card>
-            )}
-            {alertToShow === BillingAlertType.UsageLimitExceeded && (
-                <Card>
-                    <div style={{ display: 'flex' }}>
-                        <div style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-                            <AlertOutlined className="text-warning" style={{ paddingRight: 16 }} />
-                            <div>
-                                <b>Alert!</b> The monthly limit of events or billing limit for your organization has
-                                been exceeded. To avoid losing data or access to it, <b>we recommend increasing</b> your
-                                billing limit.
-                            </div>
-                        </div>
-                        <div style={{ display: 'flex', alignItems: 'center', paddingLeft: 16 }}>
-                            <LinkButton type="primary" to="/organization/billing">
-                                <ToolFilled /> Manage billing
-                            </LinkButton>
-                        </div>
-                    </div>
-                </Card>
-            )}
-        </>
+        <div className={'Announcement'}>
+            {isWarning && <IconWarningAmber className="text-warning" style={{ paddingRight: '1rem' }} />}
+            {isAlert && <IconWarningAmber className="text-warning" style={{ paddingRight: '1rem' }} />}
+            {message}
+        </div>
     )
 }
