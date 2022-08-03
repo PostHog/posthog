965cb199ed9d16d9cb447566c37942d8e81b5065
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-08-03 15:55:50 +0300

    Resolve typing-related issues

--- plugin-server/src/utils/fetch.ts
+++ plugin-server/src/utils/fetch.ts
@@ -1,17 +1,17 @@
 // This module wraps node-fetch with a sentry tracing-aware extension
 
-import fetch, { Request, RequestInfo, RequestInit, Response } from 'node-fetch'
+import fetch, { Request, Response } from 'node-fetch'
 
 import { runInSpan } from '../sentry'
 
-function fetchWrapper(url: RequestInfo, init?: RequestInit): Promise<Response> {
-    const request = new Request(url, init)
+function fetchWrapper(...args: Parameters<typeof fetch>): Promise<Response> {
+    const request = new Request(...args)
     return runInSpan(
         {
             op: 'fetch',
             description: `${request.method} ${request.hostname}`,
         },
-        () => fetch(url, init)
+        () => fetch(...args)
     )
 }
 
