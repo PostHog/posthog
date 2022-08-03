8691f6ba722b128f849971722ea19daa88c912ec
Alex Kim <alexgkim205@gmail.com>
2022-08-03 16:31:43 -0400

    new pause

--- frontend/src/scenes/session-recordings/player/icons.tsx
+++ frontend/src/scenes/session-recordings/player/icons.tsx
@@ -73,10 +73,7 @@ export function IconPause({ onClick, style, className = '' }: IconProps): JSX.El
     return (
         <BaseIcon onClick={onClick} className={className} style={style}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M9 16H11V8H9V16ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM13 16H15V8H13V16Z"
-                    fill="currentColor"
-                />
+                <path d="M6 19H10V5H6V19ZM14 5V19H18V5H14Z" fill="currentColor" />
             </svg>
         </BaseIcon>
     )
