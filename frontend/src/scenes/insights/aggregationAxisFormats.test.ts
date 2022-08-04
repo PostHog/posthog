660a509383a02b26a254cd3eebc43a587ce6a940
Michael Matloka <dev@twixes.com>
2022-08-04 12:49:57 +0200

    Update unit tests

--- frontend/src/scenes/insights/aggregationAxisFormats.test.ts
+++ frontend/src/scenes/insights/aggregationAxisFormats.test.ts
@@ -3,11 +3,11 @@ import { formatAggregationAxisValue, AggregationAxisFormat } from 'scenes/insigh
 describe('formatAggregationAxisValue', () => {
     const formatTestcases = [
         { candidate: 34, format: 'duration', expected: '34s' },
-        { candidate: 340, format: 'duration', expected: '5m 40s' },
-        { candidate: 3940, format: 'duration', expected: '1h 5m 40s' },
+        { candidate: 340, format: 'duration', expected: '5m 40s' },
+        { candidate: 3940, format: 'duration', expected: '1h 5m 40s' },
         { candidate: 3.944, format: 'percentage', expected: '3.94%' },
         { candidate: 3.956, format: 'percentage', expected: '3.96%' },
-        { candidate: 3940, format: 'percentage', expected: '3 940%' },
+        { candidate: 3940, format: 'percentage', expected: '3,940%' },
         { candidate: 34, format: 'numeric', expected: '34' },
         { candidate: 394, format: 'numeric', expected: '394' },
         { candidate: 3940, format: 'numeric', expected: '3.94K' },
