be1d832b41ae976b5cf990a78b318dac81066b03
Ben White <ben@benjackwhite.co.uk>
2022-08-08 18:53:46 +0200

    Fixed async modal

--- frontend/src/scenes/instance/AsyncMigrations/AsyncMigrationParametersModal.tsx
+++ frontend/src/scenes/instance/AsyncMigrations/AsyncMigrationParametersModal.tsx
@@ -15,63 +15,63 @@ export function AsyncMigrationParametersModal(props: AsyncMigrationModalProps): 
     const [collapsed, setCollapsed] = useState(true)
 
     return (
-        <VerticalForm
-            logic={asyncMigrationParameterFormLogic}
-            props={props}
-            formKey="parameters"
-            enableFormOnSubmit
-            id="async-migration-parameters-form"
-        >
-            <LemonModal
-                title="Advanced migration configuration"
-                onClose={closeAsyncMigrationsModal}
-                isOpen={true}
-                footer={
-                    <>
-                        <LemonButton
-                            form="async-migration-parameters-form"
-                            type="secondary"
-                            data-attr="async-migration-parameters-cancel"
-                            className="mr-2"
-                            onClick={closeAsyncMigrationsModal}
-                        >
-                            Cancel
-                        </LemonButton>
-                        <LemonButton
-                            form="async-migration-parameters-form"
-                            htmlType="submit"
-                            type="primary"
-                            data-attr="async-migration-parameters-submit"
-                        >
-                            Run migration
-                        </LemonButton>
-                    </>
-                }
+        <LemonModal title="" onClose={closeAsyncMigrationsModal} isOpen={true} simple>
+            <VerticalForm
+                logic={asyncMigrationParameterFormLogic}
+                props={props}
+                formKey="parameters"
+                enableFormOnSubmit
+                id="async-migration-parameters-form"
+                className="LemonModal__layout"
             >
-                <p>
-                    This async migration allows tuning parameters used in the async migration.
-                    {collapsed && (
-                        <>
-                            <br />
-                            <a
-                                onClick={() => {
-                                    setCollapsed(!collapsed)
-                                }}
-                            >
-                                Click here to show advanced configuration.
-                            </a>
-                        </>
-                    )}
-                </p>
+                <LemonModal.Header>
+                    <h3>Advanced migration configuration</h3>
+                </LemonModal.Header>
+                <LemonModal.Content>
+                    <p>
+                        This async migration allows tuning parameters used in the async migration.
+                        {collapsed && (
+                            <>
+                                <br />
+                                <a
+                                    onClick={() => {
+                                        setCollapsed(!collapsed)
+                                    }}
+                                >
+                                    Click here to show advanced configuration.
+                                </a>
+                            </>
+                        )}
+                    </p>
 
-                <AnimatedCollapsible collapsed={collapsed}>
-                    {Object.keys(props.migration.parameter_definitions).map((key) => (
-                        <Field name={key} key={key} label={<>{props.migration.parameter_definitions[key][1]}</>}>
-                            <LemonInput type="number" />
-                        </Field>
-                    ))}
-                </AnimatedCollapsible>
-            </LemonModal>
-        </VerticalForm>
+                    <AnimatedCollapsible collapsed={collapsed}>
+                        {Object.keys(props.migration.parameter_definitions).map((key) => (
+                            <Field name={key} key={key} label={<>{props.migration.parameter_definitions[key][1]}</>}>
+                                <LemonInput type="number" />
+                            </Field>
+                        ))}
+                    </AnimatedCollapsible>
+                </LemonModal.Content>
+                <LemonModal.Footer>
+                    <LemonButton
+                        form="async-migration-parameters-form"
+                        type="secondary"
+                        data-attr="async-migration-parameters-cancel"
+                        className="mr-2"
+                        onClick={closeAsyncMigrationsModal}
+                    >
+                        Cancel
+                    </LemonButton>
+                    <LemonButton
+                        form="async-migration-parameters-form"
+                        htmlType="submit"
+                        type="primary"
+                        data-attr="async-migration-parameters-submit"
+                    >
+                        Run migration
+                    </LemonButton>
+                </LemonModal.Footer>
+            </VerticalForm>
+        </LemonModal>
     )
 }
