9e5dad1a825a21f9520081a281a1c2fcf930d96b
Paul D'Ambra <paul@posthog.com>
2022-08-09 11:50:18 +0100

    axe test more pages

--- cypress/e2e/home.js
+++
@@ -1,9 +0,0 @@
-import { reportA11y } from '../support/accessibility'
-
-describe('Home', () => {
-    it('should have no accessibility violations', () => {
-        cy.get('[data-attr="menu-item-projecthomepage"]').click()
-        cy.injectAxe()
-        reportA11y({ includedImpacts: ['serious', 'critical'] }, 'home-page')
-    })
-})
