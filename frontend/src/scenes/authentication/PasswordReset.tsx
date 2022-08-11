6b15f28c02f0eb983b62cea592c410427d647fc0
Ben White <ben@benjackwhite.co.uk>
2022-08-11 18:42:49 +0200

    Fixed password reset

--- frontend/src/scenes/authentication/PasswordReset.tsx
+++ frontend/src/scenes/authentication/PasswordReset.tsx
@@ -1,11 +1,9 @@
 /*
 Scene to request a password reset email.
 */
-import { Col, Row, Form, Input, Button, Skeleton, Divider } from 'antd'
-import { InlineMessage } from 'lib/components/InlineMessage/InlineMessage'
 import React from 'react'
 import { WelcomeLogo } from './WelcomeLogo'
-import { ExclamationCircleFilled, CheckCircleOutlined } from '@ant-design/icons'
+import { CheckCircleOutlined } from '@ant-design/icons'
 import './PasswordReset.scss'
 import { useActions, useValues } from 'kea'
 import { preflightLogic } from 'scenes/PreflightCheck/preflightLogic'
@@ -13,6 +11,11 @@ import { CodeSnippet, Language } from 'scenes/ingestion/frameworks/CodeSnippet'
 import { passwordResetLogic } from './passwordResetLogic'
 import { router } from 'kea-router'
 import { SceneExport } from 'scenes/sceneTypes'
+import { Spinner } from 'lib/components/Spinner/Spinner'
+import { LemonButton, LemonDivider, LemonInput } from '@posthog/lemon-ui'
+import { Form } from 'kea-forms'
+import { Field } from 'lib/forms/Field'
+import { AlertMessage } from 'lib/components/AlertMessage'
 
 export const scene: SceneExport = {
     component: PasswordReset,
@@ -25,30 +28,26 @@ export function PasswordReset(): JSX.Element {
 
     return (
         <div className="bridge-page password-reset">
-            <Row>
-                <Col span={24} className="auth-main-content">
-                    <WelcomeLogo view="login" />
-                    <div className="inner">
-                        {resetResponse?.success && (
-                            <div className="text-center">
-                                <CheckCircleOutlined style={{ color: 'var(--success)', fontSize: '4em' }} />
-                            </div>
-                        )}
-                        <h2 className="subtitle" style={{ justifyContent: 'center' }}>
-                            Reset password
-                        </h2>
-                        {preflightLoading ? (
-                            <Skeleton active paragraph={{ rows: 4 }} />
-                        ) : !preflight?.email_service_available ? (
-                            <EmailUnavailable />
-                        ) : resetResponse?.success ? (
-                            <ResetSuccess />
-                        ) : (
-                            <ResetForm />
-                        )}
-                    </div>
-                </Col>
-            </Row>
+            <div className="auth-main-content">
+                <WelcomeLogo view="login" />
+                <div className="inner">
+                    {resetResponse?.success && (
+                        <div className="text-center">
+                            <CheckCircleOutlined style={{ color: 'var(--success)', fontSize: '4em' }} />
+                        </div>
+                    )}
+                    <h2 className="subtitle justify-center">Reset password</h2>
+                    {preflightLoading ? (
+                        <Spinner />
+                    ) : !preflight?.email_service_available ? (
+                        <EmailUnavailable />
+                    ) : resetResponse?.success ? (
+                        <ResetSuccess />
+                    ) : (
+                        <ResetForm />
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
@@ -60,7 +59,7 @@ function EmailUnavailable(): JSX.Element {
                 Self-serve password reset is unavailable. Please <b>contact your instance administrator</b> to reset
                 your password.
             </div>
-            <Divider />
+            <LemonDivider className="my-6" />
             <div className="mt-4">
                 If you're an administrator:
                 <ul>
@@ -83,63 +82,40 @@ function EmailUnavailable(): JSX.Element {
 
 function ResetForm(): JSX.Element {
     const { resetResponseLoading, resetResponse } = useValues(passwordResetLogic)
-    const { reset } = useActions(passwordResetLogic)
-    const [form] = Form.useForm()
 
     return (
-        <>
-            <div className="text-center mb-4">
+        <Form logic={passwordResetLogic} formKey={'resetPassword'} className="space-y-4" enableFormOnSubmit>
+            <div className="text-center">
                 Enter your email address. If an account exists, you’ll receive an email with a password reset link soon.
             </div>
             {!resetResponseLoading && resetResponse?.errorCode && (
-                <InlineMessage style={{ marginBottom: 16 }} type="danger">
+                <AlertMessage type="error">
                     {resetResponse.errorDetail || 'Could not complete your password reset request. Please try again.'}
-                </InlineMessage>
+                </AlertMessage>
             )}
-            <Form
-                layout="vertical"
-                form={form}
-                onFinish={(values) => reset({ email: values.email })}
-                requiredMark={false}
-                noValidate
+            <Field name="email" label="Email">
+                <LemonInput
+                    className="ph-ignore-input"
+                    autoFocus
+                    data-attr="reset-email"
+                    placeholder="email@yourcompany.com"
+                    type="email"
+                    disabled={resetResponseLoading}
+                />
+            </Field>
+            <LemonButton
+                htmlType="submit"
+                type="primary"
+                status="primary-alt"
+                data-attr="password-reset"
+                fullWidth
+                center
+                size="large"
+                loading={resetResponseLoading}
             >
-                <Form.Item
-                    name="email"
-                    label="Email"
-                    rules={[
-                        {
-                            required: true,
-                            message: (
-                                <>
-                                    <ExclamationCircleFilled style={{ marginLeft: 4 }} /> Please enter your email to
-                                    continue
-                                </>
-                            ),
-                        },
-                    ]}
-                >
-                    <Input
-                        className="ph-ignore-input"
-                        autoFocus
-                        data-attr="reset-email"
-                        placeholder="email@yourcompany.com"
-                        type="email"
-                        disabled={resetResponseLoading}
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Button
-                        className="btn-bridge"
-                        htmlType="submit"
-                        data-attr="password-reset"
-                        loading={resetResponseLoading}
-                        block
-                    >
-                        Continue
-                    </Button>
-                </Form.Item>
-            </Form>
-        </>
+                Continue
+            </LemonButton>
+        </Form>
     )
 }
 
@@ -151,9 +127,17 @@ function ResetSuccess(): JSX.Element {
             Request received successfully! If the email <b>{resetResponse?.email || 'you typed'}</b> exists, you’ll
             receive an email with a reset link soon.
             <div className="mt-4">
-                <Button className="btn-bridge" data-attr="back-to-login" block onClick={() => push('/login')}>
+                <LemonButton
+                    type="primary"
+                    status="primary-alt"
+                    size="large"
+                    data-attr="back-to-login"
+                    center
+                    fullWidth
+                    onClick={() => push('/login')}
+                >
                     Back to login
-                </Button>
+                </LemonButton>
             </div>
         </div>
     )
