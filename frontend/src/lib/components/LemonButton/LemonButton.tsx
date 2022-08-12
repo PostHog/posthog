0a913382c311b538ce47585e74c26a780d4ce88d
Paul D'Ambra <paul@posthog.com>
2022-08-12 11:17:53 +0100

    remove button muted alt

--- frontend/src/lib/components/LemonButton/LemonButton.tsx
+++ frontend/src/lib/components/LemonButton/LemonButton.tsx
@@ -16,7 +16,7 @@ export interface LemonButtonPropsBase
     children?: React.ReactNode
     type?: 'primary' | 'secondary' | 'tertiary'
     /** What color scheme the button should follow */
-    status?: 'primary' | 'danger' | 'primary-alt' | 'muted' | 'muted-alt' | 'stealth'
+    status?: 'primary' | 'danger' | 'primary-alt' | 'muted' | 'stealth'
     /** Whether hover style should be applied, signaling that the button is held active in some way. */
     active?: boolean
     /** URL to link to. */
