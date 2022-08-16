00e5da90f6cab68ec765bc5591c8f392f52527f0
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-08-16 15:03:18 +0300

    Update truncate query

--- posthog/test/base.py
+++ posthog/test/base.py
@@ -17,13 +17,22 @@ from django.test.utils import CaptureQueriesContext
 from django.utils.timezone import now
 from rest_framework.test import APITestCase as DRFTestCase
 
+from posthog.clickhouse.plugin_log_entries import TRUNCATE_PLUGIN_LOG_ENTRIES_TABLE_SQL
 from posthog.client import ch_pool, sync_execute
 from posthog.models import Organization, Team, User
+from posthog.models.cohort.sql import TRUNCATE_COHORTPEOPLE_TABLE_SQL
 from posthog.models.event.sql import DISTRIBUTED_EVENTS_TABLE_SQL, DROP_EVENTS_TABLE_SQL, EVENTS_TABLE_SQL
 from posthog.models.event.util import bulk_create_events
+from posthog.models.group.sql import TRUNCATE_GROUPS_TABLE_SQL
 from posthog.models.organization import OrganizationMembership
 from posthog.models.person import Person
-from posthog.models.person.sql import DROP_PERSON_TABLE_SQL, PERSONS_TABLE_SQL, TRUNCATE_PERSON_DISTINCT_ID_TABLE_SQL
+from posthog.models.person.sql import (
+    DROP_PERSON_TABLE_SQL,
+    PERSONS_TABLE_SQL,
+    TRUNCATE_PERSON_DISTINCT_ID2_TABLE_SQL,
+    TRUNCATE_PERSON_DISTINCT_ID_TABLE_SQL,
+    TRUNCATE_PERSON_STATIC_COHORT_TABLE_SQL,
+)
 from posthog.models.person.util import bulk_create_persons, create_person
 from posthog.models.session_recording_event.sql import (
     DISTRIBUTED_SESSION_RECORDING_EVENTS_TABLE_SQL,
@@ -486,7 +495,12 @@ class ClickhouseDestroyTablesMixin(BaseTest):
                 DROP_EVENTS_TABLE_SQL(),
                 DROP_PERSON_TABLE_SQL,
                 TRUNCATE_PERSON_DISTINCT_ID_TABLE_SQL,
+                TRUNCATE_PERSON_DISTINCT_ID2_TABLE_SQL,
                 DROP_SESSION_RECORDING_EVENTS_TABLE_SQL(),
+                TRUNCATE_GROUPS_TABLE_SQL,
+                TRUNCATE_COHORTPEOPLE_TABLE_SQL,
+                TRUNCATE_PERSON_STATIC_COHORT_TABLE_SQL,
+                TRUNCATE_PLUGIN_LOG_ENTRIES_TABLE_SQL,
             ]
         )
         run_clickhouse_statement_in_parallel(
