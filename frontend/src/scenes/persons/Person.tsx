24b5cf1989e33d674281fc6ef2db3db379c88924
Ben White <ben@benjackwhite.co.uk>
2022-08-16 11:25:48 +0200

    More typos

--- frontend/src/scenes/persons/Person.tsx
+++ frontend/src/scenes/persons/Person.tsx
@@ -173,7 +173,7 @@ export function Person(): JSX.Element | null {
                     {!currentTeam?.session_recording_opt_in ? (
                         <div className="mb-4">
                             <AlertMessage type="info">
-                                Session recordings are currently disabled for this Project. To use this feature, please
+                                Session recordings are currently disabled for this project. To use this feature, please
                                 go to your <Link to={`${urls.projectSettings()}#recordings`}>project settings</Link> and
                                 enable it.
                             </AlertMessage>
