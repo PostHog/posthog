74ab1d27a62a54db5fbdfb3fb74abe49a4411e37
Ben White <ben@benjackwhite.co.uk>
2022-08-16 11:20:44 +0200

    fix: Show recordings for a Person even if disabled for the project

--- frontend/src/scenes/session-recordings/SessionRecordings.tsx
+++ frontend/src/scenes/session-recordings/SessionRecordings.tsx
@@ -1,38 +1,26 @@
 import React from 'react'
 import { SessionRecordingsTable } from './SessionRecordingsTable'
 import { PageHeader } from 'lib/components/PageHeader'
-import { Alert, Button, Row } from 'antd'
 import { teamLogic } from 'scenes/teamLogic'
 import { useValues } from 'kea'
-import { router } from 'kea-router'
 import { urls } from 'scenes/urls'
-import { ArrowRightOutlined } from '@ant-design/icons'
 import { SceneExport } from 'scenes/sceneTypes'
 import { sessionRecordingsTableLogic } from 'scenes/session-recordings/sessionRecordingsTableLogic'
+import { AlertMessage } from 'lib/components/AlertMessage'
+import { Link } from '@posthog/lemon-ui'
 
 export function SessionsRecordings(): JSX.Element {
     const { currentTeam } = useValues(teamLogic)
     return (
         <div>
-            <PageHeader title={<Row align="middle">Recordings</Row>} />
+            <PageHeader title={<div>Recordings</div>} />
             {currentTeam && !currentTeam?.session_recording_opt_in ? (
-                <Alert
-                    style={{ marginBottom: 16, display: 'flex', alignItems: 'center' }}
-                    message="Recordings are not yet enabled for this project"
-                    description="To use this feature, please go to your project settings and enable it."
-                    type="info"
-                    showIcon
-                    action={
-                        <Button
-                            type="primary"
-                            onClick={() => {
-                                router.actions.push(urls.projectSettings(), {}, 'recordings')
-                            }}
-                        >
-                            Go to settings <ArrowRightOutlined />
-                        </Button>
-                    }
-                />
+                <div className="mb-4">
+                    <AlertMessage type="info">
+                        Session Recordings are currently disabled for this Project. To use this feature, please go to
+                        your <Link to={`${urls.projectSettings()}#recordings`}>project settings</Link> and enable it.
+                    </AlertMessage>
+                </div>
             ) : null}
 
             <SessionRecordingsTable key="global" />
