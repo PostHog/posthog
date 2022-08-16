121a2ade910b347ad0f37d5c8e7f69257b99aaa7
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-08-16 14:08:30 -0300

    remove todos

--- plugin-server/src/worker/vm/extensions/storage.ts
+++ plugin-server/src/worker/vm/extensions/storage.ts
@@ -60,7 +60,3 @@ export function createStorage(server: Hub, pluginConfig: PluginConfig): StorageE
         del,
     }
 }
-
-// TODO:
-// 1. Add index to timestamp
-// 2. Add cron to deduplicate (or do on insert?)
