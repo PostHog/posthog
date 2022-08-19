71bc18409ebe7babb40698bdc1ab35c9273cf7d5
Ben White <ben@benjackwhite.co.uk>
2022-08-19 12:28:47 +0100

    New styles

--- frontend/src/lib/components/Spinner/Spinner.stories.tsx
+++ frontend/src/lib/components/Spinner/Spinner.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ComponentMeta } from '@storybook/react'
 
 import { Spinner as Spinner } from './Spinner'
+import { LemonButton } from '@posthog/lemon-ui'
 
 export default {
     title: 'Lemon UI/Spinner',
@@ -18,4 +19,29 @@ export function Small(): JSX.Element {
 
 export function Large(): JSX.Element {
     return <Spinner size="lg" />
+}
+
+export function Monocolor(): JSX.Element {
+    return (
+        <div className="bg-default p-4">
+            <Spinner size="lg" monocolor className="text-white" />
+        </div>
+    )
+}
+
+export function InButtons(): JSX.Element {
+    return (
+        <div className="flex gap-2 items-center">
+            <LemonButton type="primary" loading>
+                Primary
+            </LemonButton>
+            <LemonButton type="secondary" loading>
+                Secondary Button
+            </LemonButton>
+
+            <LemonButton type="secondary" status="danger" loading>
+                Secondary Danger
+            </LemonButton>
+        </div>
+    )
 }
--- frontend/src/lib/components/Spinner/Spinner.tsx
+++ frontend/src/lib/components/Spinner/Spinner.tsx
@@ -5,10 +5,11 @@ import './Spinner.scss'
 
 export interface SpinnerProps {
     size?: 'sm' | 'md' | 'lg'
+    monocolor?: boolean
     className?: string
 }
 
 /** Smoothly animated spinner for loading states. It does not indicate progress, only that something's happening. */
-export function Spinner({ size = 'md', className }: SpinnerProps): JSX.Element {
-    return <IconSpinner className={clsx('Spinner', size && `Spinner--${size}`, className)} />
+export function Spinner({ size = 'md', monocolor, className }: SpinnerProps): JSX.Element {
+    return <IconSpinner monocolor={monocolor} className={clsx('Spinner', size && `Spinner--${size}`, className)} />
 }
