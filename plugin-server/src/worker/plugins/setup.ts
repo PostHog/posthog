6448e77dc4743dbdb0664651ee6c2cd334569783
Michael Matloka <dev@twixes.com>
2022-08-19 17:08:11 +0200

    Roll back `loadPluginsFromDB` change

--- plugin-server/src/worker/plugins/setup.ts
+++ plugin-server/src/worker/plugins/setup.ts
@@ -101,7 +101,7 @@ async function loadPluginsFromDB(hub: Hub): Promise<Pick<Hub, 'plugins' | 'plugi
         }
         pluginConfigs.set(row.id, pluginConfig)
 
-        if (row.team_id === undefined || row.team_id === null) {
+        if (!row.team_id) {
             console.error(`🔴 PluginConfig(id=${row.id}) without team_id!`)
             continue
         }
