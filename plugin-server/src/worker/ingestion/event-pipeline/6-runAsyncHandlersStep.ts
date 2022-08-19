38932ce8febc9d2e4356392a59bc25066fe8783c
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2022-08-19 11:13:57 +0300

    chore(plugin-server): re-introduce LazyPersonContainer to async server (#11383)
    * chore(plugin-server): re-introduce LazyPersonContainer to async server
    
    We were seeing problems in the async server running into various
    timeouts and LazyPersonContainer PR seemed the culprit. Reverting it in
    async server did the trick, but we don't understand why.
    
    There's two current suspects:
    1. Serialization-related issues due to LazyPersonContainer
    2. Promise-related headaches in actions/webhooks
    
    This reverts part of https://github.com/PostHog/posthog/pull/11370,
    except webhooks and action matching, checking if (1) was the issue.
    
    * Update runAsyncHandlersStep

--- plugin-server/src/worker/ingestion/event-pipeline/6-runAsyncHandlersStep.ts
+++ plugin-server/src/worker/ingestion/event-pipeline/6-runAsyncHandlersStep.ts
@@ -1,16 +1,20 @@
 import { runInstrumentedFunction } from '../../../main/utils'
-import { Element, IngestionPersonData, PostIngestionEvent } from '../../../types'
+import { Element, PostIngestionEvent } from '../../../types'
 import { convertToProcessedPluginEvent } from '../../../utils/event'
 import { runOnEvent, runOnSnapshot } from '../../plugins/run'
+import { LazyPersonContainer } from '../lazy-person-container'
 import { EventPipelineRunner, StepResult } from './runner'
 
 export async function runAsyncHandlersStep(
     runner: EventPipelineRunner,
     event: PostIngestionEvent,
-    person: IngestionPersonData | undefined
+    personContainer: LazyPersonContainer
 ): Promise<StepResult> {
     if (runner.hub.capabilities.processAsyncHandlers) {
-        await Promise.all([processOnEvent(runner, event), processWebhooks(runner, event, person, event.elementsList)])
+        await Promise.all([
+            processOnEvent(runner, event),
+            processWebhooks(runner, event, personContainer, event.elementsList),
+        ])
     }
 
     return null
@@ -33,11 +37,12 @@ async function processOnEvent(runner: EventPipelineRunner, event: PostIngestionE
 async function processWebhooks(
     runner: EventPipelineRunner,
     event: PostIngestionEvent,
-    personContainer: IngestionPersonData | undefined,
+    personContainer: LazyPersonContainer,
     elements: Element[] | undefined
 ) {
     if (event.event !== '$snapshot') {
-        const actionMatches = await runner.hub.actionMatcher.match(event, personContainer, elements)
-        await runner.hub.hookCannon.findAndFireHooks(event, personContainer, actionMatches)
+        const person = await personContainer.get()
+        const actionMatches = await runner.hub.actionMatcher.match(event, person, elements)
+        await runner.hub.hookCannon.findAndFireHooks(event, person, actionMatches)
     }
 }
