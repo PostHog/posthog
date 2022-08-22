c2ef696a8f1eb89dc2440ebfe930b84f0dd13c74
Michael Matloka <dev@twixes.com>
2022-08-22 12:38:48 +0200

    Satisfy mypy

--- posthog/tasks/demo_reset_master_team.py
+++ posthog/tasks/demo_reset_master_team.py
@@ -5,7 +5,7 @@ from posthog.demo.matrix import MatrixManager
 from posthog.demo.products import HedgeboxMatrix
 
 
-def demo_reset_master_team():
+def demo_reset_master_team() -> None:
     matrix = HedgeboxMatrix(n_clusters=settings.DEMO_MATRIX_N_CLUSTERS)
     manager = MatrixManager(matrix)
     with transaction.atomic():
