8241366f3a27e406b691a58a0e074e7627a3fe91
Michael Matloka <dev@twixes.com>
2022-08-22 12:38:00 +0200

    Only run complete inference in demo

--- posthog/tasks/calculate_event_property_usage.py
+++ posthog/tasks/calculate_event_property_usage.py
@@ -16,16 +16,93 @@ def calculate_event_property_usage() -> None:
 
 
 @dataclass
-class EventDefinitionPayload:
+class _EventDefinitionPayload:
     volume_30_day: int = field(default_factory=int)
     query_usage_30_day: int = field(default_factory=int)
     last_seen_at: timezone.datetime = field(default_factory=timezone.now)
 
 
 @dataclass
-class PropertyDefinitionPayload:
+class _PropertyDefinitionPayload:
     property_type: Optional[PropertyType] = field(default=None)
     query_usage_30_day: int = field(default_factory=int)
+
+
+@shared_task(ignore_result=True, max_retries=1)
+def calculate_event_property_usage_for_team(team_id: int, *, complete_inference: bool = False) -> None:
+    """Calculate Data Management stats for a specific team.
+
+    The complete_inference flag enables much more extensive inference of event/actor taxonomy based on ClickHouse data.
+    This is not needed in production - where the plugin server is responsible for this - but in the demo environment
+    data comes preloaded, necessitating complete inference."""
+    team = Team.objects.get(pk=team_id)
+
+    # Prepare update payloads
+    event_definition_payloads: DefaultDict[str, _EventDefinitionPayload] = defaultdict(
+        _EventDefinitionPayload,
+        {
+            known_event.name: _EventDefinitionPayload()
+            for known_event in EventDefinition.objects.filter(team_id=team_id)
+        },
+    )
+    property_definition_payloads: DefaultDict[str, _PropertyDefinitionPayload] = defaultdict(
+        _PropertyDefinitionPayload,
+        {
+            known_property.name: _PropertyDefinitionPayload(
+                property_type=cast(PropertyType, known_property.property_type)
+                or (PropertyType.Numeric if known_property.is_numerical else None)
+            )
+            for known_property in PropertyDefinition.objects.filter(team_id=team_id)
+        },
+    )
+
+    since = timezone.now() - timezone.timedelta(days=30)
+
+    for item in Insight.objects.filter(team=team, created_at__gt=since):
+        for event in item.filters.get("events", []):
+            event_definition_payloads[event["id"]].query_usage_30_day += 1
+        for prop in item.filters.get("properties", []):
+            if isinstance(prop, dict) and prop.get("key"):
+                property_definition_payloads[prop["key"]].query_usage_30_day += 1
+
+    events_volume = _get_events_volume(team, since)
+    for event, (volume, last_seen_at) in events_volume.items():
+        event_definition_payloads[event].volume_30_day = volume
+        event_definition_payloads[event].last_seen_at = last_seen_at
+
+    if complete_inference:
+        # Infer (event, property) pairs
+        event_properties = _get_event_properties(team, since)
+        EventProperty.objects.bulk_create(
+            [
+                EventProperty(team_id=team_id, event=event, property=property_key)
+                for event, property_key in event_properties
+            ],
+            ignore_conflicts=True,
+        )
+        # Infer property types
+        property_types = _get_property_types(team, since)
+        for property_key, property_type in property_types.items():
+            if property_definition_payloads[property_key].property_type is not None:
+                continue  # Don't override property type if it's already set
+            property_definition_payloads[property_key].property_type = property_type
+
+    # Update event definitions
+    for event, event_definition_payload in event_definition_payloads.items():
+        # TODO: Use Django 4.1's bulk_create() with update_conflicts=True
+        EventDefinition.objects.update_or_create(name=event, team_id=team_id, defaults=asdict(event_definition_payload))
+
+    # Update property definitions
+    for property_key, property_definition_payload in property_definition_payloads.items():
+        # TODO: Use Django 4.1's bulk_create() with update_conflicts=True
+        PropertyDefinition.objects.update_or_create(
+            name=property_key,
+            team_id=team_id,
+            defaults={
+                **asdict(property_definition_payload),
+                "is_numerical": property_definition_payload.property_type == PropertyType.Numeric,
+            },
+        )
 
 
 def _get_events_volume(team: Team, since: timezone.datetime) -> Dict[str, Tuple[int, timezone.datetime]]:
@@ -50,13 +127,8 @@ def _infer_property_type(sample_json_value: str) -> Optional[PropertyType]:
     return None
 
 
-def _get_property_types(
-    team: Team, since: timezone.datetime, *, include_actor_properties: bool
-) -> Dict[str, Optional[PropertyType]]:
-    """Determine property types based on ClickHouse data.
-
-    Only event properties are analyzed in production PostHog, but for the purposes of the demo environment,
-    actor properties can be included too optionally."""
+def _get_property_types(team: Team, since: timezone.datetime) -> Dict[str, Optional[PropertyType]]:
+    """Determine property types based on ClickHouse data."""
     from posthog.client import sync_execute
     from posthog.models.event.sql import GET_EVENT_PROPERTY_SAMPLE_JSON_VALUES
     from posthog.models.group.sql import GET_GROUP_PROPERTY_SAMPLE_JSON_VALUES
@@ -69,19 +141,12 @@ def _get_property_types(
         )
     }
 
-    if include_actor_properties:
-        # In the periodic job we only care about event properties, but in the demo environment – where data is ingested
-        # bypassing the plugin server - we also want to calculate person and group properties for taxonomy integrity
-        for property_key, sample_json_value in sync_execute(
-            GET_PERSON_PROPERTY_SAMPLE_JSON_VALUES, {"team_id": team.pk}
-        ):
-            if property_key not in property_types:
-                property_types[property_key] = _infer_property_type(sample_json_value)
-        for property_key, sample_json_value in sync_execute(
-            GET_GROUP_PROPERTY_SAMPLE_JSON_VALUES, {"team_id": team.pk}
-        ):
-            if property_key not in property_types:
-                property_types[property_key] = _infer_property_type(sample_json_value)
+    for property_key, sample_json_value in sync_execute(GET_PERSON_PROPERTY_SAMPLE_JSON_VALUES, {"team_id": team.pk}):
+        if property_key not in property_types:
+            property_types[property_key] = _infer_property_type(sample_json_value)
+    for property_key, sample_json_value in sync_execute(GET_GROUP_PROPERTY_SAMPLE_JSON_VALUES, {"team_id": team.pk}):
+        if property_key not in property_types:
+            property_types[property_key] = _infer_property_type(sample_json_value)
 
     return property_types
 
@@ -92,58 +157,3 @@ def _get_event_properties(team: Team, since: timezone.datetime) -> List[Tuple[st
     from posthog.models.event.sql import GET_EVENT_PROPERTIES
 
     return sync_execute(GET_EVENT_PROPERTIES, {"team_id": team.pk, "timestamp": since})
-
-
-@shared_task(ignore_result=True, max_retries=1)
-def calculate_event_property_usage_for_team(team_id: int, *, include_actor_properties: bool = False) -> None:
-    team = Team.objects.get(pk=team_id)
-    event_definition_payloads: DefaultDict[str, EventDefinitionPayload] = defaultdict(
-        EventDefinitionPayload,
-        {known_event.name: EventDefinitionPayload() for known_event in EventDefinition.objects.filter(team_id=team_id)},
-    )
-    property_definition_payloads: DefaultDict[str, PropertyDefinitionPayload] = defaultdict(
-        PropertyDefinitionPayload,
-        {
-            known_property.name: PropertyDefinitionPayload(
-                property_type=cast(PropertyType, known_property.property_type)
-                or (PropertyType.Numeric if known_property.is_numerical else None)
-            )
-            for known_property in PropertyDefinition.objects.filter(team_id=team_id)
-        },
-    )
-
-    since = timezone.now() - timezone.timedelta(days=30)
-
-    for item in Insight.objects.filter(team=team, created_at__gt=since):
-        for event in item.filters.get("events", []):
-            event_definition_payloads[event["id"]].query_usage_30_day += 1
-        for prop in item.filters.get("properties", []):
-            if isinstance(prop, dict) and prop.get("key"):
-                property_definition_payloads[prop["key"]].query_usage_30_day += 1
-
-    events_volume = _get_events_volume(team, since)
-    for event, (volume, last_seen_at) in events_volume.items():
-        event_definition_payloads[event].volume_30_day = volume
-        event_definition_payloads[event].last_seen_at = last_seen_at
-
-    property_types = _get_property_types(team, since, include_actor_properties=include_actor_properties)
-    for property_key, property_type in property_types.items():
-        if property_definition_payloads[property_key].property_type is None:
-            property_definition_payloads[property_key].property_type = property_type
-    event_properties = _get_event_properties(team, since)
-
-    for event, event_definition_payload in event_definition_payloads.items():
-        EventDefinition.objects.update_or_create(name=event, team_id=team_id, defaults=asdict(event_definition_payload))
-
-    for property_key, property_definition_payload in property_definition_payloads.items():
-        PropertyDefinition.objects.update_or_create(
-            name=property_key,
-            team_id=team_id,
-            defaults={
-                **asdict(property_definition_payload),
-                "is_numerical": property_definition_payload.property_type == PropertyType.Numeric,
-            },
-        )
-
-    for event, property_key in event_properties:
-        EventProperty.objects.update_or_create(team_id=team_id, event=event, property=property_key)
