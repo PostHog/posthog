661d6874901da4b15ec9bae6ecc1f2c76d1c5c10
Alex Kim <alexgkim205@gmail.com>
2022-08-24 14:22:02 -0400

    refactor to alertmessage

--- frontend/src/scenes/events/UsageDisabledWarning.tsx
+++ frontend/src/scenes/events/UsageDisabledWarning.tsx
@@ -1,23 +1,17 @@
-import { Alert } from 'antd'
 import React from 'react'
+import { AlertMessage } from 'lib/components/AlertMessage'
 
 export function UsageDisabledWarning({ tab }: { tab: string }): JSX.Element {
     return (
-        <Alert
-            style={{ marginBottom: 16, display: 'flex', alignItems: 'center' }}
-            message={`${tab} are not enabled for your instance.`}
-            description={
-                <>
-                    You will still see the list of events and properties, but usage information will be unavailable. If
-                    you want to enable event usage please set the follow environment variable:{' '}
-                    <pre style={{ display: 'inline' }}>ASYNC_EVENT_PROPERTY_USAGE=1</pre>. Please note, enabling this
-                    environment variable <b>may increase load considerably in your infrastructure</b>, particularly if
-                    you have a large volume of events.
-                </>
-            }
-            type="info"
-            showIcon
-            closable
-        />
+        <AlertMessage type="info">
+            <div className="text-base mb-1">{tab} are not enabled for your instance.</div>
+            <p className="font-normal">
+                You will still see the list of events and properties, but usage information will be unavailable. If you
+                want to enable event usage please set the follow environment variable:{' '}
+                <pre style={{ display: 'inline' }}>ASYNC_EVENT_PROPERTY_USAGE=1</pre>. Please note, enabling this
+                environment variable <b>may increase load considerably in your infrastructure</b>, particularly if you
+                have a large volume of events.
+            </p>
+        </AlertMessage>
     )
 }
