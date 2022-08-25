42f3b60147f0d186dfba3c500d83936ee67a1d66
Rick Marron <rcmarron@gmail.com>
2022-08-25 15:31:10 -0700

    Add loaded + fix styles

--- frontend/src/scenes/session-recordings/SessionRecordingsPlaylist.tsx
+++ frontend/src/scenes/session-recordings/SessionRecordingsPlaylist.tsx
@@ -7,11 +7,12 @@ import { sessionRecordingsTableLogic } from './sessionRecordingsTableLogic'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons'
 import { asDisplay } from 'scenes/persons/PersonHeader'
 import { RecordingWatchedSource } from 'lib/utils/eventUsageLogic'
-import './SessionRecordingTable.scss'
+import './SessionRecordingPlaylist.scss'
 import { LemonTable, LemonTableColumns } from 'lib/components/LemonTable'
 import { TZLabel } from 'lib/components/TimezoneAware'
 import { SessionRecordingPlayerV3 } from './player/SessionRecordingPlayer'
 import { EmptyMessage } from 'lib/components/EmptyMessage/EmptyMessage'
+import { Spinner } from 'lib/components/Spinner/Spinner'
 
 interface SessionRecordingsTableProps {
     personUUID?: string
@@ -48,72 +49,70 @@ export function SessionRecordingsPlaylist({ personUUID }: SessionRecordingsTable
         },
     ]
     return (
-        <div className="session-recordings-table" data-attr="session-recordings-table">
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-                <div style={{ width: 300, flexShrink: 0 }} className="mr-4">
-                    <LemonTable
-                        dataSource={sessionRecordings}
-                        columns={columns}
-                        loading={sessionRecordingsResponseLoading}
-                        onRow={(sessionRecording) => ({
-                            onClick: (e) => {
-                                // Lets the link to the person open the person's page and not the session recording
-                                if (!(e.target as HTMLElement).closest('a')) {
-                                    console.log('clicked on row', sessionRecording)
-                                    openSessionPlayer(sessionRecording.id, RecordingWatchedSource.RecordingsList)
-                                }
-                            },
-                        })}
-                        rowStatus={(recording) => (activeSessionRecordingId === recording.id ? 'highlighted' : null)}
-                        rowClassName="cursor-pointer"
-                        data-attr="session-recording-table"
-                        data-tooltip="session-recording-table"
-                        emptyState="No matching recordings found"
-                    />
-                </div>
-                <div style={{ flex: 1 }}>
-                    {activeSessionRecordingId ? (
-                        <div className="border rounded-lg">
-                            <SessionRecordingPlayerV3
-                                playerKey="playlist"
-                                sessionRecordingId={activeSessionRecordingId}
-                            />
-                        </div>
-                    ) : (
-                        <EmptyMessage
-                            title="No recording selected"
-                            description="Please select a recording from the list on the left"
-                            buttonText="Learn more about recordings"
-                            buttonHref="https://posthog.com/docs/user-guides/recordings"
-                        />
-                    )}
-                </div>
+        <div className="SessionRecordingPlaylist" data-attr="session-recordings-playlist">
+            <div className="SessionRecordingPlaylist__left-column mr-4">
+                <LemonTable
+                    dataSource={sessionRecordings}
+                    columns={columns}
+                    loading={sessionRecordingsResponseLoading}
+                    onRow={(sessionRecording) => ({
+                        onClick: (e) => {
+                            // Lets the link to the person open the person's page and not the session recording
+                            if (!(e.target as HTMLElement).closest('a')) {
+                                console.log('clicked on row', sessionRecording)
+                                openSessionPlayer(sessionRecording.id, RecordingWatchedSource.RecordingsList)
+                            }
+                        },
+                    })}
+                    rowStatus={(recording) => (activeSessionRecordingId === recording.id ? 'highlighted' : null)}
+                    rowClassName="cursor-pointer"
+                    data-attr="session-recording-table"
+                    data-tooltip="session-recording-table"
+                    emptyState="No matching recordings found"
+                />
+                {(hasPrev || hasNext) && (
+                    <Row className="SessionRecordingPlaylist__pagination-control">
+                        <Button
+                            type="link"
+                            disabled={!hasPrev}
+                            onClick={() => {
+                                loadPrev()
+                                window.scrollTo(0, 0)
+                            }}
+                        >
+                            <LeftOutlined /> Previous
+                        </Button>
+                        <Button
+                            type="link"
+                            disabled={!hasNext}
+                            onClick={() => {
+                                loadNext()
+                                window.scrollTo(0, 0)
+                            }}
+                        >
+                            Next <RightOutlined />
+                        </Button>
+                    </Row>
+                )}
             </div>
-            {(hasPrev || hasNext) && (
-                <Row className="pagination-control">
-                    <Button
-                        type="link"
-                        disabled={!hasPrev}
-                        onClick={() => {
-                            loadPrev()
-                            window.scrollTo(0, 0)
-                        }}
-                    >
-                        <LeftOutlined /> Previous
-                    </Button>
-                    <Button
-                        type="link"
-                        disabled={!hasNext}
-                        onClick={() => {
-                            loadNext()
-                            window.scrollTo(0, 0)
-                        }}
-                    >
-                        Next <RightOutlined />
-                    </Button>
-                </Row>
-            )}
-            <div style={{ marginBottom: 64 }} />
+            <div className="SessionRecordingPlaylist__right-column">
+                {activeSessionRecordingId ? (
+                    <div className="border rounded">
+                        <SessionRecordingPlayerV3 playerKey="playlist" sessionRecordingId={activeSessionRecordingId} />
+                    </div>
+                ) : sessionRecordingsResponseLoading ? (
+                    <div className="SessionRecordingPlaylist__loading">
+                        <Spinner />
+                    </div>
+                ) : (
+                    <EmptyMessage
+                        title="No recording selected"
+                        description="Please select a recording from the list on the left"
+                        buttonText="Learn more about recordings"
+                        buttonHref="https://posthog.com/docs/user-guides/recordings"
+                    />
+                )}
+            </div>
         </div>
     )
 }
