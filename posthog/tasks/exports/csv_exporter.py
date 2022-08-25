64929fe7158832bfa7b5de94926094c0893f3b12
Paul D'Ambra <paul@posthog.com>
2022-08-25 16:02:10 +0100

    feat: allow csv exporter to filter and order columns

--- posthog/tasks/exports/csv_exporter.py
+++ posthog/tasks/exports/csv_exporter.py
@@ -154,6 +154,7 @@ def _export_to_csv(exported_asset: ExportedAsset, limit: int = 1000, max_limit: 
     resource = exported_asset.export_context
 
     path: str = resource["path"]
+    columns: List[str] = resource.get("columns", [])
 
     method: str = resource.get("method", "GET")
     body = resource.get("body", None)
@@ -196,7 +197,7 @@ def _export_to_csv(exported_asset: ExportedAsset, limit: int = 1000, max_limit: 
             # If values are serialised then keep the order of the keys, else allow it to be unordered
             renderer.header = all_csv_rows[0].keys()
 
-    rendered_csv_content = renderer.render(all_csv_rows)
+    rendered_csv_content = renderer.render(all_csv_rows, renderer_context={"header": columns})
     save_content(exported_asset, rendered_csv_content)
 
 
