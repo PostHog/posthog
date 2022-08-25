b8fe0628cd6fe4a4b0d2d2761c751b0037563439
Neil Kakkar <neilkakkar@gmail.com>
2022-08-25 14:28:41 +0100

    chore: Use instance setting for self-hosted person on events (#11472)

--- posthog/models/team/util.py
+++ posthog/models/team/util.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
 from typing import Any, List
 
+from posthog.cache_utils import cache_for
+from posthog.models.async_migration import is_async_migration_complete
 from posthog.models.person import Person, PersonDistinctId
 
 
@@ -13,3 +16,21 @@ def delete_bulky_postgres_data(team_ids: List[int]):
 def _raw_delete(queryset: Any):
     "Issues a single DELETE statement for the queryset"
     queryset._raw_delete(queryset.db)
+
+
+can_enable_person_on_events = False
+
+# :TRICKY: Avoid overly eagerly checking whether the migration is complete.
+# We instead cache negative responses for a minute and a positive one forever.
+def person_on_events_ready() -> bool:
+    global can_enable_person_on_events
+
+    if can_enable_person_on_events:
+        return True
+    can_enable_person_on_events = _person_on_events_ready()
+    return can_enable_person_on_events
+
+
+@cache_for(timedelta(minutes=1))
+def _person_on_events_ready() -> bool:
+    return is_async_migration_complete("0006_persons_and_groups_on_events_backfill")
