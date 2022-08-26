561180a7ced74484159f0e0e51a7e6df114ba94e
Paul D'Ambra <paul@posthog.com>
2022-08-26 12:31:28 +0100

    wat

--- bin/visual-regression.js
+++ bin/visual-regression.js
@@ -1,10 +1,38 @@
-const puppeteer = require('puppeteer')
+import { launch } from 'puppeteer'
+import { readFileSync, writeFileSync } from 'fs'
+import { PNG } from 'pngjs'
+import pixelmatch from 'pixelmatch'
 
 ;(async () => {
-    const browser = await puppeteer.launch()
+    const browser = await launch()
     const page = await browser.newPage()
-    await page.goto('https://storybook.posthog.net/?path=/docs/lemon-ui-lemon-button--default#types-and-statuses')
-    await page.screenshot({ path: 'screenshot-button-types-and-statuses.png' })
+    try {
+        await page.goto('https://storybook.posthog.net/?path=/docs/lemon-ui-lemon-button--default#types-and-statuses')
+        await page.click('#lemon-ui-lemon-button--types-and-statuses')
+        // await page.waitForTimeout(1500)
+        await page.screenshot({ path: './visual-regression-screenshots/screenshot-button-types-and-statuses.png' })
 
-    await browser.close()
+        const screenshot = PNG.sync.read(
+            readFileSync('./visual-regression-screenshots/screenshot-button-types-and-statuses.png')
+        )
+        const baseline = PNG.sync.read(
+            readFileSync('./visual-regression-screenshots/baseline/screenshot-button-types-and-statuses.png')
+        )
+        const { width, height } = screenshot
+        const diff = new PNG({ width, height })
+
+        const numDiffPixels = pixelmatch(screenshot.data, baseline.data, diff.data, width, height, { threshold: 0.1 })
+        if (numDiffPixels) {
+            console.log('detected ', numDiffPixels, ' pixels difference')
+            writeFileSync(
+                './visual-regression-screenshots/diffs/screenshot-button-types-and-statuses.png',
+                PNG.sync.write(diff)
+            )
+        }
+    } catch (error) {
+        console.error(error)
+        await page.screenshot({ path: './visual-regression-screenshots/error.png' })
+    } finally {
+        await browser.close()
+    }
 })()
