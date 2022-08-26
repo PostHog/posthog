0d642837dad823d984b50de518448d6600697dd9
Paul D'Ambra <paul@posthog.com>
2022-08-26 14:39:27 +0100

    wat

--- bin/visual-regression.mjs
+++ bin/visual-regression.mjs
@@ -2,6 +2,7 @@ import { launch } from 'puppeteer'
 import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs'
 import { PNG } from 'pngjs'
 import pixelmatch from 'pixelmatch'
+import { pathToFileURL } from 'url'
 ;(async () => {
     if (!existsSync('./visual-regression-screenshots/diffs')) {
         mkdirSync('./visual-regression-screenshots/diffs', { recursive: true })
@@ -13,6 +14,8 @@ import pixelmatch from 'pixelmatch'
         await page.goto('https://storybook.posthog.net/?path=/docs/lemon-ui-lemon-button--default#types-and-statuses')
         await page.waitForSelector('#lemon-ui-lemon-button--types-and-statuses')
         await page.click('#lemon-ui-lemon-button--types-and-statuses')
+        await page.waitForSelector('#story--lemon-ui-lemon-button--types-and-statuses')
+        await page.waitForTimeout(100)
         await page.screenshot({ path: './visual-regression-screenshots/screenshot-button-types-and-statuses.png' })
 
         const screenshot = PNG.sync.read(
