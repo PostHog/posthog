5fbfe398f11ac1e1290d0eeca1c15c8281da37d6
Paul D'Ambra <paul@posthog.com>
2022-08-26 11:51:12 +0100

    wat

--- bin/visual-regression.js
+++ bin/visual-regression.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const puppeteer = require('puppeteer')
+
+;(async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://storybook.posthog.net/?path=/docs/lemon-ui-lemon-button--default#types-and-statuses')
+    await page.screenshot({ path: 'screenshot-button-types-and-statuses.png' })
+
+    await browser.close()
+})()
