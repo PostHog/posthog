11dde259ba1f70d978bbe6705266067e9a674618
Rick Marron <rcmarron@gmail.com>
2022-08-26 15:09:43 -0400

    drawer logic tests

--- frontend/src/scenes/session-recordings/sessionPlayerDrawerLogic.ts
+++ frontend/src/scenes/session-recordings/sessionPlayerDrawerLogic.ts
@@ -1,9 +1,7 @@
-import { actions, connect, kea, listeners, path, reducers } from 'kea'
-import { EntityTypes, PropertyOperator, RecordingDurationFilter, SessionRecordingId } from '~/types'
+import { actions, connect, kea, path, reducers } from 'kea'
+import { SessionRecordingId } from '~/types'
 import { actionToUrl, router, urlToAction } from 'kea-router'
 import { eventUsageLogic, RecordingWatchedSource } from 'lib/utils/eventUsageLogic'
-
-import { getDefaultEventName } from 'lib/utils/getAppContext'
 
 import type { sessionPlayerDrawerLogicType } from './sessionPlayerDrawerLogicType'
 import { sessionRecordingDataLogic } from './player/sessionRecordingDataLogic'
@@ -11,30 +9,6 @@ import { subscriptions } from 'kea-subscriptions'
 
 interface HashParams {
     sessionRecordingId?: SessionRecordingId
-}
-
-export const DEFAULT_DURATION_FILTER: RecordingDurationFilter = {
-    type: 'recording',
-    key: 'duration',
-    value: 60,
-    operator: PropertyOperator.GreaterThan,
-}
-
-export const DEFAULT_PROPERTY_FILTERS = []
-
-const event = getDefaultEventName()
-
-export const DEFAULT_ENTITY_FILTERS = {
-    events: [],
-    actions: [],
-    new_entity: [
-        {
-            id: event,
-            type: EntityTypes.EVENTS,
-            order: 0,
-            name: event,
-        },
-    ],
 }
 
 export const sessionPlayerDrawerLogic = kea<sessionPlayerDrawerLogicType>([
@@ -58,18 +32,9 @@ export const sessionPlayerDrawerLogic = kea<sessionPlayerDrawerLogicType>([
             },
         ],
     }),
-    listeners({
-        openSessionPlayer: ({ sessionRecordingId }) => {
-            console.log('openSessionPlayer', sessionRecordingId)
-        },
-    }),
     subscriptions({
         activeSessionRecordingId: (sessionRecordingId, oldSessionRecordingId) => {
             if (sessionRecordingId !== oldSessionRecordingId) {
-                // if (sessionRecordingDataLogic({ sessionRecordingId: oldSessionRecordingId }).isMounted()) {
-                //     sessionRecordingDataLogic({ sessionRecordingId: oldSessionRecordingId }).unmount()
-                // }
-                console.log('drawer activeSessionRecordingId', sessionRecordingId)
                 if (sessionRecordingId) {
                     if (!sessionRecordingDataLogic({ sessionRecordingId }).isMounted()) {
                         sessionRecordingDataLogic({ sessionRecordingId }).mount()
