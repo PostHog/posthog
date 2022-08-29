5b9ea5f8699ae3c36a8dee4211b2ad124d05e647
Guido Iaquinti <guido@posthog.com>
2022-08-29 15:55:34 +0200

    WIP

--- posthog/middleware.py
+++ posthog/middleware.py
@@ -1,6 +1,7 @@
 from ipaddress import ip_address, ip_network
 from typing import List, Optional, cast
 
+import ipware.ip
 from django.conf import settings
 from django.core.exceptions import MiddlewareNotUsed
 from django.db.models import QuerySet
@@ -29,33 +30,16 @@ class AllowIPMiddleware:
             self.trusted_proxies = [item.strip() for item in settings.TRUSTED_PROXIES.split(",")]
         self.get_response = get_response
 
-    def get_forwarded_for(self, request: HttpRequest):
-        forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-        if forwarded_for is not None:
-            return [ip.strip() for ip in forwarded_for.split(",")]
-        else:
-            return []
-
-    def extract_client_ip(self, request: HttpRequest):
-        client_ip = request.META["REMOTE_ADDR"]
-        if getattr(settings, "USE_X_FORWARDED_HOST", False):
-            forwarded_for = self.get_forwarded_for(request)
-            if forwarded_for:
-                closest_proxy = client_ip
-                client_ip = forwarded_for.pop(0)
-                if settings.TRUST_ALL_PROXIES:
-                    return client_ip
-                proxies = [closest_proxy] + forwarded_for
-                for proxy in proxies:
-                    if proxy not in self.trusted_proxies:
-                        return None
-        return client_ip
+    def get_client_ip(self, request: HttpRequest):
+        proxy_trusted_ips = [] if settings.TRUST_ALL_PROXIES else self.trusted_proxies
+        client_ip_address, _ = ipware.ip.get_client_ip(request, proxy_trusted_ips=proxy_trusted_ips,)
+        return client_ip_address
 
     def __call__(self, request: HttpRequest):
         response: HttpResponse = self.get_response(request)
         if request.path.split("/")[1] in ["decide", "engage", "track", "capture", "batch", "e", "static", "_health"]:
             return response
-        ip = self.extract_client_ip(request)
+        ip = self.get_client_ip(request)
         if ip and any(ip_address(ip) in ip_network(block, strict=False) for block in self.ip_blocks):
             return response
         return HttpResponse(
