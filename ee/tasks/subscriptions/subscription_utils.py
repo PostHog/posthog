5e594486a66896e6a6dc10191fbf946b5d7061fb
Ben White <ben@benjackwhite.co.uk>
2022-08-30 09:54:43 +0100

    fix: Increased subscription asset gen to 10 minutes (#11543)

--- ee/tasks/subscriptions/subscription_utils.py
+++ ee/tasks/subscriptions/subscription_utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from time import sleep
 from typing import List, Tuple, Union
 
@@ -14,8 +15,8 @@ from posthog.tasks import exporter
 logger = structlog.get_logger(__name__)
 
 UTM_TAGS_BASE = "utm_source=posthog&utm_campaign=subscription_report"
-
 DEFAULT_MAX_ASSET_COUNT = 6
+ASSET_GENERATION_MAX_TIMEOUT = timedelta(minutes=10)
 
 
 def generate_assets(
@@ -42,11 +43,10 @@ def generate_assets(
     tasks = [exporter.export_asset.s(asset.id) for asset in assets]
     parallel_job = group(tasks).apply_async()
 
-    max_wait = 30
+    start_time = datetime.now()
     while not parallel_job.ready():
-        max_wait = max_wait - 1
         sleep(1)
-        if max_wait < 0:
+        if datetime.now() > start_time + ASSET_GENERATION_MAX_TIMEOUT:
             raise Exception("Timed out waiting for exports")
 
     return insights, assets
