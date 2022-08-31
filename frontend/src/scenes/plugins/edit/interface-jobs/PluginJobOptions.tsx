7608299616787a6ef06e346cf7b700c6ade93681
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-08-31 13:47:35 +0300

    Add feature flag for limiting historical exports v2

--- frontend/src/scenes/plugins/edit/interface-jobs/PluginJobOptions.tsx
+++ frontend/src/scenes/plugins/edit/interface-jobs/PluginJobOptions.tsx
@@ -1,7 +1,14 @@
 import { LemonTag } from '@posthog/lemon-ui'
-import React from 'react'
+import { useValues } from 'kea'
+import React, { useMemo } from 'react'
 import { JobSpec } from '~/types'
-import { PluginJobConfiguration } from './PluginJobConfiguration'
+import {
+    HISTORICAL_EXPORT_JOB_NAME,
+    HISTORICAL_EXPORT_JOB_NAME_V2,
+    PluginJobConfiguration,
+} from './PluginJobConfiguration'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
+import { FEATURE_FLAGS } from '../../../../lib/constants'
 
 interface PluginJobOptionsProps {
     pluginId: number
@@ -16,6 +23,26 @@ export function PluginJobOptions({
     capabilities,
     publicJobs,
 }: PluginJobOptionsProps): JSX.Element {
+    const { featureFlags } = useValues(featureFlagLogic)
+
+    const jobs = useMemo(() => {
+        return capabilities.jobs
+            .filter((jobName) => jobName in publicJobs)
+            .filter((jobName) => {
+                // Hide either old or new export depending on the feature flag value
+                if (jobName === HISTORICAL_EXPORT_JOB_NAME && featureFlags[FEATURE_FLAGS.HISTORICAL_EXPORTS_V2]) {
+                    return false
+                } else if (
+                    jobName === HISTORICAL_EXPORT_JOB_NAME_V2 &&
+                    !featureFlags[FEATURE_FLAGS.HISTORICAL_EXPORTS_V2]
+                ) {
+                    return false
+                }
+
+                return true
+            })
+    }, [capabilities, publicJobs, featureFlags])
+
     return (
         <>
             <h3 className="l3" style={{ marginTop: 32 }}>
@@ -25,23 +52,17 @@ export function PluginJobOptions({
                 </LemonTag>
             </h3>
 
-            {capabilities.jobs
-                .filter((jobName) => jobName in publicJobs)
-                .map((jobName) => (
-                    <div key={jobName}>
-                        {jobName.includes('Export historical events') ? (
-                            <i>Export historical events</i>
-                        ) : (
-                            <i>{jobName}</i>
-                        )}
-                        <PluginJobConfiguration
-                            jobName={jobName}
-                            jobSpec={publicJobs[jobName]}
-                            pluginConfigId={pluginConfigId}
-                            pluginId={pluginId}
-                        />
-                    </div>
-                ))}
+            {jobs.map((jobName) => (
+                <div key={jobName}>
+                    {jobName.includes('Export historical events') ? <i>Export historical events</i> : <i>{jobName}</i>}
+                    <PluginJobConfiguration
+                        jobName={jobName}
+                        jobSpec={publicJobs[jobName]}
+                        pluginConfigId={pluginConfigId}
+                        pluginId={pluginId}
+                    />
+                </div>
+            ))}
         </>
     )
 }
