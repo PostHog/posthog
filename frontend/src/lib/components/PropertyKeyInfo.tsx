ebbc83149903249b8c4b0ae3641fea375623d08c
GitStart <gitstart@users.noreply.github.com>
2022-08-31 16:12:17 +0000

    fix: addressed comments

--- frontend/src/lib/components/PropertyKeyInfo.tsx
+++ frontend/src/lib/components/PropertyKeyInfo.tsx
@@ -746,8 +746,8 @@ export function PropertyKeyInfo({
 
     // By this point, property is a PH defined property
     const innerContent = (
-        <span className="property-key-info align-item-center">
-            {!disableIcon && !!data && <span className="property-key-info-logo w-24" />}
+        <span className="property-key-info flex items-center">
+            {!disableIcon && !!data && <span className="property-key-info-logo w-6" />}
             <Typography.Text
                 ellipsis={ellipsis}
                 style={{
