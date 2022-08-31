60c4d48db5733e9d32634fe93ea3307c8f5c4739
Marius Andra <marius.andra@gmail.com>
2022-08-31 15:23:30 +0200

    even more fixes

--- frontend/src/lib/components/LemonCalendar/LemonCalendarSelect.tsx
+++ frontend/src/lib/components/LemonCalendar/LemonCalendarSelect.tsx
@@ -1,4 +1,4 @@
-import { LemonCalendar, LemonCalendarProps } from 'lib/components/LemonCalendar/LemonCalendar'
+import { LemonCalendar } from 'lib/components/LemonCalendar/LemonCalendar'
 import React, { useState } from 'react'
 import { dayjs } from 'lib/dayjs'
 import { LemonButton } from 'lib/components/LemonButton'
@@ -8,7 +8,6 @@ export interface LemonCalendarSelectProps {
     value?: string | null
     onChange: (date: string) => void
     months?: number
-    getLemonButtonProps?: LemonCalendarProps['getLemonButtonProps']
     onClose?: () => void
 }
 
