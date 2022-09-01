510ddd48adc20c95c42788ea26375d696564c80c
Guido Iaquinti <guido@posthog.com>
2022-09-01 18:00:38 +0200

    fix: minor code cleanup

--- posthog/queries/trends/total_volume.py
+++ posthog/queries/trends/total_volume.py
@@ -119,11 +119,10 @@ class TrendsTotalVolume:
     def _parse_total_volume_result(self, filter: Filter, entity: Entity, team: Team) -> Callable:
         def _parse(result: List) -> List:
             parsed_results = []
-            for _, stats in enumerate(result):
+            for stats in result:
                 parsed_result = parse_response(stats, filter)
                 parsed_result.update({"persons_urls": self._get_persons_url(filter, entity, team.pk, stats[0])})
                 parsed_results.append(parsed_result)
-
                 parsed_result.update({"filter": filter.to_dict()})
             return parsed_results
 
