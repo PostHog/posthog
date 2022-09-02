112a248e980ae22e8f97396c850f74062cad8d56
Ben White <ben@benjackwhite.co.uk>
2022-09-02 09:56:53 +0200

    Updated Skeleton with some useful defaults

--- frontend/src/lib/components/Skeleton/Skeleton.stories.tsx
+++ frontend/src/lib/components/Skeleton/Skeleton.stories.tsx
@@ -3,6 +3,8 @@ import { ComponentMeta } from '@storybook/react'
 
 import { Skeleton } from './Skeleton'
 import { LemonLabel } from '../LemonLabel/LemonLabel'
+import { LemonModal } from '../LemonModal'
+import { LemonButton } from '../LemonButton'
 
 export default {
     title: 'Lemon UI/Skeleton',
@@ -13,13 +15,49 @@ export function Default(): JSX.Element {
     return <Skeleton />
 }
 
-export function Sizes(): JSX.Element {
+export function Presets(): JSX.Element {
     return (
         <div className="space-y-2">
+            <p>Skeletons have a bunch of presets to help with simulating other LemonUI Components</p>
+
+            <div className="flex items-center gap-2">
+                <Skeleton.Circle />
+                <Skeleton />
+                <Skeleton.Button />
+            </div>
+
+            <p>Here is an example of "skeletoning" a LemonModal</p>
+
+            <LemonModal
+                isOpen
+                onClose={() => {}}
+                inline
+                title="Loading..."
+                footer={
+                    <>
+                        <Skeleton.Button />
+                        <Skeleton.Button />
+                    </>
+                }
+            >
+                <div className="space-y-2">
+                    <Skeleton width={'50%'} />
+                    <Skeleton.Row repeat={3} />
+                </div>
+            </LemonModal>
+        </div>
+    )
+}
+
+export function Customisation(): JSX.Element {
+    return (
+        <div className="space-y-2 mb-2">
             <p>Skeletons are most easily styled with utility classNames</p>
 
             <LemonLabel>Default</LemonLabel>
             <Skeleton />
+            <LemonLabel>Custom classNames</LemonLabel>
+            <Skeleton className="h-10 rounded-lg" width={200} />
         </div>
     )
 }
--- frontend/src/lib/components/Skeleton/Skeleton.tsx
+++ frontend/src/lib/components/Skeleton/Skeleton.tsx
@@ -1,13 +1,57 @@
 import clsx from 'clsx'
+import { range } from 'lib/utils'
 import React from 'react'
+import { LemonButtonProps } from '../LemonButton'
+import './Skeleton.scss'
 
 export interface SkeletonProps {
     className?: string
     width?: string | number
+    repeat?: number
 }
 
-export function Skeleton({ className, width }: SkeletonProps): JSX.Element {
-    // NOTE: this is purposefully lowercase as it is also a utility class
+export function Skeleton({ className, width, repeat }: SkeletonProps): JSX.Element {
     // eslint-disable-next-line react/forbid-dom-props
-    return <div className={clsx('skeleton', className)} style={{ width }} />
+    const content = <div className={clsx('Skeleton', className || 'h-6 w-full rounded')} style={{ width }} />
+
+    if (repeat) {
+        return (
+            <>
+                {range(repeat).map((i) => (
+                    <React.Fragment key={i}>{content}</React.Fragment>
+                ))}
+            </>
+        )
+    }
+    return content
+}
+
+Skeleton.Text = function SkeletonText({ className, ...props }: SkeletonProps) {
+    return <Skeleton className={clsx('rounded h-6 w-full', className)} {...props} />
+}
+
+Skeleton.Row = function SkeletonRow({ className, ...props }: SkeletonProps) {
+    return <Skeleton className={clsx('rounded h-10 w-full', className)} {...props} />
+}
+
+Skeleton.Circle = function SkeletonCircle({ className, ...props }: SkeletonProps) {
+    return <Skeleton className={clsx('rounded-full h-10 w-10 shrink-0', className)} {...props} />
+}
+
+Skeleton.Button = function SkeletonButton({
+    className,
+    size,
+    ...props
+}: SkeletonProps & { size?: LemonButtonProps['size'] }) {
+    return (
+        <Skeleton
+            className={clsx(
+                'rounded w-20 px-3',
+                size === 'small' && 'h-10',
+                (!size || size === 'medium') && 'h-10',
+                className
+            )}
+            {...props}
+        />
+    )
 }
