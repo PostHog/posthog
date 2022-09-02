dc2b96a26b1775f0ca7b427efce58325147f1d6a
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-09-02 14:58:33 +0300

    Separate LemonCalendarRangeInline
    Note breaking change: The selected dates selector is now not conditional on no of months shown

--- frontend/src/scenes/plugins/edit/interface-jobs/PluginJobConfiguration.tsx
+++ frontend/src/scenes/plugins/edit/interface-jobs/PluginJobConfiguration.tsx
@@ -10,9 +10,9 @@ import { JobPayloadFieldOptions, JobSpec } from '~/types'
 import { interfaceJobsLogic } from './interfaceJobsLogic'
 import { LemonInput } from '../../../../lib/components/LemonInput/LemonInput'
 import moment from 'moment'
-import { LemonCalendarRange } from '../../../../lib/components/LemonCalendarRange/LemonCalendarRange'
 import { LemonModal } from '../../../../lib/components/LemonModal'
 import { LemonButton } from '../../../../lib/components/LemonButton'
+import { LemonCalendarRangeInline } from '../../../../lib/components/LemonCalendarRange/LemonCalendarRangeInline'
 
 interface PluginJobConfigurationProps {
     jobName: string
@@ -148,6 +148,6 @@ function FieldInput({
                 />
             )
         case 'daterange':
-            return <LemonCalendarRange value={value || null} onChange={onChange} hideHeader autoApply />
+            return <LemonCalendarRangeInline value={value || null} onChange={onChange} />
     }
 }
