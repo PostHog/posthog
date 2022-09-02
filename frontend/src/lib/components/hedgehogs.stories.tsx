c31bacfdc5084e2c56b174040404714d2312362a
Emanuele Capparelli <k@emkpp.com>
2022-09-02 15:59:49 +0100

    feat(design system): add hedgehog illustration components

---
+++ frontend/src/lib/components/hedgehogs.stories.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+import { LemonTable } from '@posthog/lemon-ui'
+import * as hedgehogs from './hedgehogs'
+
+interface HedgehogDefinition {
+    name: string
+    hedgehog: (...args: any[]) => JSX.Element
+}
+
+const allHedgehogs: HedgehogDefinition[] = Object.entries(hedgehogs).map(([key, Hedgehog]) => ({
+    name: key,
+    hedgehog: Hedgehog,
+}))
+
+export default {
+    title: 'Lemon UI/Hog illustrations',
+    parameters: {
+        options: { showPanel: false },
+        docs: {
+            description: {
+                component: `
+
+[Related Figma area](https://www.figma.com/file/Y9G24U4r04nEjIDGIEGuKI/PostHog-Design-System-One?node-id=3775%3A2092)
+
+Our hedgehog has many professions so it’s vital you choose the correct one for whatever project
+or page you are working on.s
+
+Singular hedgehog illustrations should be kept in a 200x200px frame
+and scaled up or down accordingly. Wider hedgehog frames containing one or more will keep the
+same height of 200px but the width may change dependant on the illustration.
+
+As we continue to
+grow more and more hedgehogs of different professions and positions will appear, but if you have
+a specific idea in mind, please submit new hedgehog requests to Lottie our Graphic Designer and
+she will get to it dependant on work load.
+`,
+            },
+        },
+    },
+} as Meta
+
+export function Library(): JSX.Element {
+    return (
+        <div className="space-y-2">
+            <LemonTable
+                dataSource={allHedgehogs}
+                columns={[
+                    {
+                        title: 'Name',
+                        key: 'name',
+                        dataIndex: 'name',
+                        render: function RenderName(name) {
+                            return <code>{`<${name as string} />`}</code>
+                        },
+                    },
+                    {
+                        title: 'Hedgehog',
+                        key: 'hedgehog',
+                        dataIndex: 'hedgehog',
+                        render: function RenderHedgehog(Hedgehog) {
+                            Hedgehog = Hedgehog as HedgehogDefinition['hedgehog']
+                            return <Hedgehog />
+                        },
+                    },
+                ]}
+            />
+        </div>
+    )
+}
---
+++ frontend/src/lib/components/hedgehogs.tsx
@@ -0,0 +1,87 @@
+// Loads custom icons (some icons may come from a third-party library)
+import React, { ImgHTMLAttributes } from 'react'
+import surprisedHog from 'public/hedgehog/surprised-hog.png'
+import xRayHog from 'public/hedgehog/x-ray-hog.png'
+import hospitalHog from 'public/hedgehog/hospital-hog.png'
+import blushingHog from 'public/hedgehog/blushing-hog.png'
+import laptopHog1 from 'public/hedgehog/laptop-hog-01.png'
+import laptopHog2 from 'public/hedgehog/laptop-hog-02.png'
+import explorerHog from 'public/hedgehog/explorer-hog.png'
+import runningHog from 'public/hedgehog/running-hog.png'
+import spaceHog from 'public/hedgehog/space-hog.png'
+import tronHog from 'public/hedgehog/tron-hog.png'
+import heartHog from 'public/hedgehog/heart-hog.png'
+import starHog from 'public/hedgehog/star-hog.png'
+import policeHog from 'public/hedgehog/police-hog.png'
+import sleepingHog from 'public/hedgehog/sleeping-hog.png'
+import builderHog1 from 'public/hedgehog/builder-hog-01.png'
+import builderHog2 from 'public/hedgehog/builder-hog-02.png'
+import builderHog3 from 'public/hedgehog/builder-hog-03.png'
+import professorHog from 'public/hedgehog/professor-hog.png'
+import supportHeroHog from 'public/hedgehog/support-hero-hog.png'
+import xRayHog2 from 'public/hedgehog/x-ray-hogs-02.png'
+import laptopHog3 from 'public/hedgehog/laptop-hog-03.png'
+
+export const SurprisedHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={surprisedHog} width={200} height={200} {...props} />
+}
+export const XRayHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={xRayHog} width={200} height={200} {...props} />
+}
+export const XRayHog2 = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={xRayHog2} height={200} {...props} />
+}
+export const HospitalHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={hospitalHog} width={200} height={200} {...props} />
+}
+export const BlushingHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={blushingHog} height={200} width={200} {...props} />
+}
+export const LaptopHog1 = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={laptopHog1} height={200} {...props} />
+}
+export const LaptopHog2 = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={laptopHog2} height={200} {...props} />
+}
+export const LaptopHog3 = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={laptopHog3} height={200} {...props} />
+}
+export const ExplorerHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={explorerHog} {...props} width={200} height={200} {...props} />
+}
+export const RunningHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={runningHog} width={200} height={200} {...props} />
+}
+export const SpaceHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={spaceHog} height={200} {...props} />
+}
+export const TronHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={tronHog} width={200} height={200} {...props} />
+}
+export const HeartHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={heartHog} width={200} height={200} {...props} />
+}
+export const StarHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={starHog} width={200} height={200} {...props} />
+}
+export const PoliceHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={policeHog} width={200} height={200} {...props} />
+}
+export const SleepingHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={sleepingHog} width={200} height={200} {...props} />
+}
+export const BuilderHog1 = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={builderHog1} width={200} height={200} {...props} />
+}
+export const BuilderHog2 = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={builderHog2} height={200} {...props} />
+}
+export const BuilderHog3 = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={builderHog3} width={200} height={200} {...props} />
+}
+export const ProfessorHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={professorHog} width={200} height={200} {...props} />
+}
+export const SupportHeroHog = (props: ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+    return <img src={supportHeroHog} width={200} height={200} {...props} />
+}
