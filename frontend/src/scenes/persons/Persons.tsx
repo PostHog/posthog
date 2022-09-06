95c3a312c9274479d674ea62cd55c0a57602dddd
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-09-06 13:04:52 +0100

    Update Persons.tsx

--- frontend/src/scenes/persons/Persons.tsx
+++ frontend/src/scenes/persons/Persons.tsx
@@ -46,9 +46,9 @@ export function PersonsScene(): JSX.Element {
                         placement="topRight"
                         title={
                             <>
-                                Exporting by csv is limited to 10,000 users.
+                                Exporting by CSV is limited to 10,000 users.
                                 <br />
-                                To return more, please use <a href={apiDocsURL}>the API</a>. Do you want to export by
+                                To export more, please use <a href={apiDocsURL}>the API</a>. Do you want to export by
                                 CSV?
                             </>
                         }
