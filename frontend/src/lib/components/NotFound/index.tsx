c4bc166912254e0747f4102b1777281fb0e72e98
Ben White <ben@benjackwhite.co.uk>
2022-09-06 17:13:25 +0200

    Added to 404 page

--- frontend/src/lib/components/NotFound/index.tsx
+++ frontend/src/lib/components/NotFound/index.tsx
@@ -5,9 +5,10 @@ import './NotFound.scss'
 
 interface NotFoundProps {
     object: string // Type of object that was not found (e.g. `dashboard`, `insight`, `action`, ...)
+    caption?: string
 }
 
-export function NotFound({ object }: NotFoundProps): JSX.Element {
+export function NotFound({ object, caption }: NotFoundProps): JSX.Element {
     return (
         <div className="NotFoundComponent space-y-2">
             <div className="NotFoundComponent__graphic" />
@@ -16,16 +17,20 @@ export function NotFound({ object }: NotFoundProps): JSX.Element {
                 <b>It seems this page may have been lost in space.</b>
             </p>
             <p>
-                It's possible this {object} may have been deleted or its sharing settings changed. Please check with the
-                person who sent you here, or{' '}
-                <Link
-                    to={`https://posthog.com/support?utm_medium=in-product&utm_campaign=${object}-not-found`}
-                    target="_blank"
-                    rel="noopener"
-                >
-                    contact support
-                </Link>{' '}
-                if you think this is a mistake.
+                {caption || (
+                    <>
+                        It's possible this {object} may have been deleted or its sharing settings changed. Please check
+                        with the person who sent you here, or{' '}
+                        <Link
+                            to={`https://posthog.com/support?utm_medium=in-product&utm_campaign=${object}-not-found`}
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            contact support
+                        </Link>{' '}
+                        if you think this is a mistake.
+                    </>
+                )}
             </p>
         </div>
     )
