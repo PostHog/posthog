94f10b14c18b5a3a93e8ab530a60c8a322f8afbe
Alex Kim <alexgkim205@gmail.com>
2022-09-06 14:36:14 -0400

    upgrade react-syntax-highlighter

--- jest.config.ts
+++ jest.config.ts
@@ -185,8 +185,7 @@ export default {
     // transform: undefined,
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    transformIgnorePatterns: ['node_modules/(?!(query-selector-shadow-dom)|react-syntax-highlighter/)'],
-
+    transformIgnorePatterns: ['node_modules/(?!(query-selector-shadow-dom)/)'],
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
 
