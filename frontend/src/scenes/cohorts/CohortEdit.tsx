606e773fdcc7a9fbe39bbdb6d43009c75c0e4dc6
Ben White <ben@benjackwhite.co.uk>
2022-09-07 11:46:16 +0200

    feat: Astrohog for not found (#11687)

--- frontend/src/scenes/cohorts/CohortEdit.tsx
+++ frontend/src/scenes/cohorts/CohortEdit.tsx
@@ -25,15 +25,19 @@ import { Persons } from 'scenes/persons/Persons'
 import React from 'react'
 import { LemonLabel } from 'lib/components/LemonLabel/LemonLabel'
 import { Form } from 'kea-forms'
+import { NotFound } from 'lib/components/NotFound'
 
 export function CohortEdit({ id }: CohortLogicProps): JSX.Element {
     const logicProps = { id }
     const logic = cohortEditLogic(logicProps)
     const { deleteCohort, setOuterGroupsType } = useActions(logic)
-    const { cohort, cohortLoading } = useValues(logic)
+    const { cohort, cohortLoading, cohortMissing } = useValues(logic)
     const { hasAvailableFeature } = useValues(userLogic)
     const isNewCohort = cohort.id === 'new' || cohort.id === undefined
 
+    if (cohortMissing) {
+        return <NotFound object="cohort" />
+    }
     return (
         <div className="cohort">
             <Form logic={cohortEditLogic} props={logicProps} formKey="cohort" enableFormOnSubmit>
