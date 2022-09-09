a7e2dce6606c629445f0bfeedc1a9a921dbe0998
Ben White <ben@benjackwhite.co.uk>
2022-09-09 09:49:52 +0200

    Update frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx

--- frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx
+++ frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx
@@ -178,7 +178,7 @@ export function ActionFilterRow({
     const filterElement = (
         <LemonButtonWithPopup
             data-attr={'trend-element-subject-' + index}
-            className="LemonButton--full-width"
+            fullWidth
             popup={{
                 overlay: (
                     <TaxonomicFilter
