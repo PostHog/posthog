f6f2ad3cf3cdb1ddcfddd56a77ac5916befb76c6
Michael Matloka <dev@twixes.com>
2022-09-19 19:48:09 +0200

    Improve `fallbackPlacements`

--- frontend/src/lib/components/AnnotationsOverlay/AnnotationsOverlay.tsx
+++ frontend/src/lib/components/AnnotationsOverlay/AnnotationsOverlay.tsx
@@ -172,6 +172,7 @@ function AnnotationsPopover({
             additionalRefs={overlayRefs}
             className="AnnotationsPopover"
             placement="top"
+            fallbackPlacements={['top-end', 'top-start']}
             referenceElement={activeBadgeElement as HTMLElement}
             visible={isPopoverShown}
             onClickOutside={closePopover}
