e4f69240779623edd15ca619d252ff287d33a37e
Harry Waye <harry@posthog.com>
2022-09-20 10:28:57 +0100

    fix(toolbar): pass apiURL to the toolbar explicitly (#11834)
    Previously we were not sending apiURL and as a result we just use the
    [api_host](https://github.com/PostHog/posthog-js/blob/master/src/extensions/toolbar.ts#L82)
    in the posthog-js config.
    
    This has the issue that we use this apiURL to e.g. construct urls
    linking to the app, which when using a reverse proxy will result in the
    user needing to login. If they are using SSO they will not be able to as
    the redirect_uri will not match the registered uri for the OAuth client.
    
    See
    https://github.com/PostHog/posthog/blob/806ad4b43b8e05413dd7ddd38b042173c5e0cf58/frontend/src/toolbar/actions/ActionsTab.tsx#L29:L29
    for an example of where we use this to construct urls.

--- frontend/src/scenes/toolbar-launch/authorizedUrlsLogic.test.ts
+++ frontend/src/scenes/toolbar-launch/authorizedUrlsLogic.test.ts
@@ -26,7 +26,7 @@ describe('the authorized urls logic', () => {
 
     it('encodes an app url correctly', () => {
         expect(appEditorUrl('http://127.0.0.1:8000')).toEqual(
-            '/api/user/redirect_to_site/?userIntent=add-action&appUrl=http%3A%2F%2F127.0.0.1%3A8000'
+            '/api/user/redirect_to_site/?userIntent=add-action&apiURL=http%3A%2F%2Flocalhost&appUrl=http%3A%2F%2F127.0.0.1%3A8000'
         )
     })
 
