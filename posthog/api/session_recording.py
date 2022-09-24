e9047f868f6f6e153a716ae11d6f93906b3a3619
Alex Gyujin Kim <alexgkim205@gmail.com>
2022-09-24 10:49:16 -0400

    feat(recordings): add matching events to recordings list query (#11919)
    Co-authored-by: alexkim205 <alexkim205@users.noreply.github.com>

--- posthog/api/session_recording.py
+++ posthog/api/session_recording.py
@@ -37,6 +37,7 @@ class SessionRecordingSerializer(serializers.Serializer):
     start_time = serializers.DateTimeField()
     end_time = serializers.DateTimeField()
     distinct_id = serializers.CharField()
+    matching_events = serializers.ListField(required=False)
 
     def to_representation(self, instance):
         return {
@@ -46,6 +47,7 @@ class SessionRecordingSerializer(serializers.Serializer):
             "start_time": instance["start_time"],
             "end_time": instance["end_time"],
             "distinct_id": instance["distinct_id"],
+            "matching_events": instance["matching_events"],
         }
 
 
