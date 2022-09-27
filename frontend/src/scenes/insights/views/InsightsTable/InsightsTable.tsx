a725008a9a9fa768182d3af194d9a0f76ac55e7e
Eric Duong <eeoneric@gmail.com>
2022-09-27 19:13:56 -0400

    fix: conditionally hide aggregation options (#11939)
    * fix: conditionally hide aggregation options
    
    * move logic and remove prop
    
    * add comment
    
    * add sum
    
    * use selector
    
    * add type

--- frontend/src/scenes/insights/views/InsightsTable/InsightsTable.tsx
+++ frontend/src/scenes/insights/views/InsightsTable/InsightsTable.tsx
@@ -35,7 +35,6 @@ interface InsightsTableProps {
     isLegend?: boolean
     /** Whether this is table is embedded in another card or whether it should be a card of its own. Default: false. */
     embedded?: boolean
-    showTotalCount?: boolean
     /** Key for the entityFilterLogic */
     filterKey: string
     canEditSeriesNameInline?: boolean
@@ -58,7 +57,7 @@ export function DashboardInsightsTable(): JSX.Element {
     const { insightProps } = useValues(insightLogic)
     return (
         <BindLogic logic={trendsLogic} props={insightProps}>
-            <InsightsTable showTotalCount filterKey={`dashboard_${insightProps.dashboardItemId}`} embedded />
+            <InsightsTable filterKey={`dashboard_${insightProps.dashboardItemId}`} embedded />
         </BindLogic>
     )
 }
@@ -66,7 +65,6 @@ export function DashboardInsightsTable(): JSX.Element {
 export function InsightsTable({
     isLegend = false,
     embedded = false,
-    showTotalCount = false,
     filterKey,
     canEditSeriesNameInline = false,
     canCheckUncheckSeries = true,
@@ -84,8 +82,8 @@ export function InsightsTable({
     const hasMathUniqueFilter = !!(
         filters.actions?.find(({ math }) => math === 'dau') || filters.events?.find(({ math }) => math === 'dau')
     )
-    const logic = insightsTableLogic({ hasMathUniqueFilter })
-    const { calcColumnState } = useValues(logic)
+    const logic = insightsTableLogic({ hasMathUniqueFilter, filters })
+    const { calcColumnState, showTotalCount } = useValues(logic)
     const { setCalcColumnState } = useActions(logic)
 
     const showCountedByTag = !!indexedResults.find(({ action }) => action?.math && action.math !== 'total')
