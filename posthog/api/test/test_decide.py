d3e72b9c26a04b95a56eb74c6ed85fbd30bed896
Rick Marron <rcmarron@gmail.com>
2022-09-27 15:51:22 -0700

    add test for decide

--- posthog/api/test/test_decide.py
+++ posthog/api/test/test_decide.py
@@ -82,8 +82,20 @@ class TestDecide(BaseTest):
         self.team.save()
 
         response = self._post_decide().json()
-        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/"})
+        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/", "consoleLogRecordingEnabled": False})
         self.assertEqual(response["supportedCompression"], ["gzip", "gzip-js", "lz64"])
+
+    def test_user_console_log_opt_in(self):
+        # :TRICKY: Test for regression around caching
+        response = self._post_decide().json()
+        self.assertEqual(response["sessionRecording"], False)
+
+        self.team.session_recording_opt_in = True
+        self.team.capture_console_log_opt_in = True
+        self.team.save()
+
+        response = self._post_decide().json()
+        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/", "consoleLogRecordingEnabled": True})
 
     def test_user_session_recording_opt_in_wildcard_domain(self):
         # :TRICKY: Test for regression around caching
@@ -95,7 +107,7 @@ class TestDecide(BaseTest):
         self.team.save()
 
         response = self._post_decide(origin="https://random.example.com").json()
-        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/"})
+        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/", "consoleLogRecordingEnabled": False})
         self.assertEqual(response["supportedCompression"], ["gzip", "gzip-js", "lz64"])
 
         # Make sure the domain matches exactly
@@ -111,7 +123,7 @@ class TestDecide(BaseTest):
         self.assertEqual(response["sessionRecording"], False)
 
         response = self._post_decide(origin="https://example.com").json()
-        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/"})
+        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/", "consoleLogRecordingEnabled": False})
 
     def test_user_session_recording_allowed_when_no_permitted_domains_are_set(self):
         self.team.recording_domains = []
@@ -119,7 +131,7 @@ class TestDecide(BaseTest):
         self.team.save()
 
         response = self._post_decide(origin="any.site.com").json()
-        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/"})
+        self.assertEqual(response["sessionRecording"], {"endpoint": "/s/", "consoleLogRecordingEnabled": False})
 
     def test_feature_flags(self):
         self.team.app_urls = ["https://example.com"]
