9ed5616c21e0a54c4f8da54c03b8eeb5214b4b7c
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-09-27 11:58:48 +0300

    Initial nested table design
    Open TODOs regarding design:
    - Where do we explain how much data is fetched (e.g. last 30 days)
    - How to display "urgency" per row - e.g. number of events
    - How to combine this with rate limiting (e.g. up to 5 events per type per hour are captured)

--- frontend/src/scenes/data-management/ingestion-warnings/IngestionWarningsView.tsx
+++ frontend/src/scenes/data-management/ingestion-warnings/IngestionWarningsView.tsx
@@ -1,15 +1,45 @@
 import React from 'react'
+import { useValues } from 'kea'
 import { SceneExport } from 'scenes/sceneTypes'
+import { urls } from 'scenes/urls'
 import { PageHeader } from 'lib/components/PageHeader'
 import { DataManagementPageTabs, DataManagementTab } from 'scenes/data-management/DataManagementPageTabs'
-import { ingestionWarningsLogic } from './ingestionWarningsLogic'
+import { IngestionWarning, ingestionWarningsLogic, IngestionWarningSummary } from './ingestionWarningsLogic'
+import { LemonTable } from 'lib/components/LemonTable'
+import { TZLabel } from 'lib/components/TimezoneAware'
+import { Link } from 'lib/components/Link'
 
 export const scene: SceneExport = {
     component: IngestionWarningsView,
     logic: ingestionWarningsLogic,
 }
 
+const WARNING_TYPE_TO_DESCRIPTION = {
+    cannot_merge_already_identified: 'Refused to merge an already identified user via $identify or $create_alias call',
+}
+
+const WARNING_TYPE_RENDERER = {
+    cannot_merge_already_identified: function Render(warning: IngestionWarning): JSX.Element {
+        const details = warning.details as {
+            sourcePerson: string
+            sourcePersonDistinctId: string
+            targetPerson: string
+            targetPersonDistinctId: string
+        }
+        return (
+            <>
+                Refused to merge already identified person{' '}
+                <Link to={urls.person(details.sourcePersonDistinctId)}>{details.sourcePersonDistinctId}</Link> into{' '}
+                <Link to={urls.person(details.targetPersonDistinctId)}>{details.targetPersonDistinctId}</Link> via an
+                $identify or $create_alias call
+            </>
+        )
+    },
+}
+
 export function IngestionWarningsView(): JSX.Element {
+    const { data } = useValues(ingestionWarningsLogic)
+
     return (
         <div data-attr="manage-events-table">
             <PageHeader
@@ -18,6 +48,59 @@ export function IngestionWarningsView(): JSX.Element {
                 tabbedPage
             />
             <DataManagementPageTabs tab={DataManagementTab.IngestionWarnings} />
+
+            <LemonTable
+                dataSource={data}
+                columns={[
+                    {
+                        title: 'Warning',
+                        dataIndex: 'type',
+                        render: function Render(_, summary: IngestionWarningSummary) {
+                            return <>{WARNING_TYPE_TO_DESCRIPTION[summary.type] || summary.type}</>
+                        },
+                    },
+                    {
+                        title: 'Last Seen',
+                        dataIndex: 'lastSeen',
+                        render: function Render(_, summary: IngestionWarningSummary) {
+                            return <TZLabel time={summary.lastSeen} showSeconds />
+                        },
+                        align: 'right',
+                    },
+                ]}
+                expandable={{
+                    expandedRowRender: RenderNestedWarnings,
+                }}
+            />
         </div>
+    )
+}
+
+function RenderNestedWarnings(warningSummary: IngestionWarningSummary): JSX.Element {
+    return (
+        <LemonTable
+            dataSource={warningSummary.warnings}
+            columns={[
+                {
+                    title: 'Description',
+                    key: 'description',
+                    render: function Render(_, warning: IngestionWarning) {
+                        const renderer = WARNING_TYPE_RENDERER[warning.type]
+                        return renderer ? renderer(warning) : <pre>{JSON.stringify(warning.details, null, 2)}</pre>
+                    },
+                },
+                {
+                    title: 'Time',
+                    dataIndex: 'timestamp',
+                    render: function Render(_, warning: IngestionWarning) {
+                        return <TZLabel time={warning.timestamp} showSeconds />
+                    },
+                    align: 'right',
+                },
+            ]}
+            embedded
+            size="small"
+            showHeader={false}
+        />
     )
 }
--- frontend/src/scenes/data-management/ingestion-warnings/ingestionWarningsLogic.ts
+++ frontend/src/scenes/data-management/ingestion-warnings/ingestionWarningsLogic.ts
@@ -4,6 +4,18 @@ import { Breadcrumb } from '~/types'
 
 import type { ingestionWarningsLogicType } from './ingestionWarningsLogicType'
 
+export interface IngestionWarningSummary {
+    type: string
+    lastSeen: string
+    warnings: IngestionWarning[]
+}
+
+export interface IngestionWarning {
+    type: string
+    timestamp: string
+    details: Record<string, any>
+}
+
 export const ingestionWarningsLogic = kea<ingestionWarningsLogicType>([
     path(['scenes', 'data-management', 'ingestion-warnings', 'ingestionWarningsLogic']),
 
@@ -22,6 +34,37 @@ export const ingestionWarningsLogic = kea<ingestionWarningsLogicType>([
                     },
                 ]
             },
+        ],
+        data: [
+            () => [],
+            () => [
+                {
+                    type: 'cannot_merge_already_identified',
+                    lastSeen: '2022-09-27T07:49:51.713000+00:00',
+                    warnings: [
+                        {
+                            type: 'cannot_merge_already_identified',
+                            timestamp: '2022-09-27T07:49:51.713000+00:00',
+                            details: {
+                                sourcePerson: 'some-uuid',
+                                sourcePersonDistinctId: 'some-distinct-id',
+                                targetPerson: 'another-uuid',
+                                targetPersonDistinctId: 'another-distinct-id',
+                            },
+                        },
+                        {
+                            type: 'cannot_merge_already_identified',
+                            timestamp: '2022-08-27T07:49:51.713000+00:00',
+                            details: {
+                                sourcePerson: 'x-uuid',
+                                sourcePersonDistinctId: 'Alice',
+                                targetPerson: 'y-uuid',
+                                targetPersonDistinctId: 'Bob',
+                            },
+                        },
+                    ],
+                } as IngestionWarningSummary,
+            ],
         ],
     }),
 ])
