6c672e76feacde2d63e50431c899587ede06442d
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-09-27 14:27:41 -0300

    Revert "chore(api): move rate limit to team" (#12002)
    Revert "chore(api): move rate limit to team (#11993)"
    
    This reverts commit 66d38c23f8abc0501036ea81e5b7fadd0be33ede.

--- posthog/rate_limit.py
+++ posthog/rate_limit.py
@@ -1,16 +1,14 @@
-from rest_framework.throttling import SimpleRateThrottle
+from rest_framework.throttling import UserRateThrottle
 from sentry_sdk.api import capture_exception
 
 from posthog.internal_metrics import incr
 
 
-class PassThroughTeamRateThrottle(SimpleRateThrottle):
+class PassThroughThrottle(UserRateThrottle):
+    # This class is being used as we're figuring out what our throttle limits should be.
+    # This ensures no rate limits are actually applied, but rather logs that they would have been applied.
+    # Allowing us to determine appropriate limits without affecting users.
     def allow_request(self, request, view):
-        """
-        This function is being used as we're figuring out what our throttle limits should be.
-        This ensures no rate limits are actually applied, but rather logs that they would have been applied.
-        Allowing us to determine appropriate limits without affecting users.
-        """
         request_would_be_allowed = super().allow_request(request, view)
         if not request_would_be_allowed:
             try:
@@ -20,38 +18,22 @@ class PassThroughTeamRateThrottle(SimpleRateThrottle):
                 capture_exception(e)
         return True
 
-    def get_cache_key(self, request, view):
-        """
-        Attempts to throttle based on the team_id of the request. If it can't do that, it falls back to the user_id.
-        And then finally to the IP address.
-        """
-        if request.user.is_authenticated:
-            team_id = getattr(view, "team_id", None)
-            if team_id:
-                ident = team_id
-            else:
-                ident = request.user.pk
-        else:
-            ident = self.get_ident(request)
 
-        return self.cache_format % {"scope": self.scope, "ident": ident}
-
-
-class PassThroughBurstRateThrottle(PassThroughTeamRateThrottle):
+class PassThroughBurstRateThrottle(PassThroughThrottle):
     # Throttle class that's applied on all endpoints (except for capture + decide)
     # Intended to block quick bursts of requests
     scope = "burst"
     rate = "480/minute"
 
 
-class PassThroughSustainedRateThrottle(PassThroughTeamRateThrottle):
+class PassThroughSustainedRateThrottle(PassThroughThrottle):
     # Throttle class that's applied on all endpoints (except for capture + decide)
     # Intended to block slower but sustained bursts of requests
     scope = "sustained"
     rate = "4800/hour"
 
 
-class PassThroughClickHouseBurstRateThrottle(PassThroughTeamRateThrottle):
+class PassThroughClickHouseBurstRateThrottle(PassThroughThrottle):
     # Throttle class that's a bit more aggressive and is used specifically
     # on endpoints that generally hit ClickHouse
     # Intended to block quick bursts of requests
@@ -59,7 +41,7 @@ class PassThroughClickHouseBurstRateThrottle(PassThroughTeamRateThrottle):
     rate = "240/minute"
 
 
-class PassThroughClickHouseSustainedRateThrottle(PassThroughTeamRateThrottle):
+class PassThroughClickHouseSustainedRateThrottle(PassThroughThrottle):
     # Throttle class that's a bit more aggressive and is used specifically
     # on endpoints that generally hit ClickHouse
     # Intended to block slower but sustained bursts of requests
