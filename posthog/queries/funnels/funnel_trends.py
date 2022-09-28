8cfdde87151a3cc2a83d54b715c62c34c1b432a1
eric <eeoneric@gmail.com>
2022-09-28 18:36:36 -0400

    add fix

--- posthog/queries/funnels/funnel_trends.py
+++ posthog/queries/funnels/funnel_trends.py
@@ -77,7 +77,7 @@ class ClickhouseFunnelTrends(ClickhouseFunnelBase):
         return f"""
             SELECT
                 aggregation_target,
-                {trunc_func}(toDateTime(timestamp), %(timezone)s) AS entrance_period_start,
+                {trunc_func}(toDateTime(timestamp) {start_of_week_fix(self._filter.interval)}, %(timezone)s) AS entrance_period_start,
                 max(steps) AS steps_completed
                 {event_select_clause}
                 {breakdown_clause}
