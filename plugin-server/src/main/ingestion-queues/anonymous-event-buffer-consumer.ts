463644f0bf5c14173cd1ee4c4fb03eec1b7a3009
Harry Waye <harry@posthog.com>
2022-10-03 17:40:31 +0000

    chore: add comment re todos for consumer

--- plugin-server/src/main/ingestion-queues/anonymous-event-buffer-consumer.ts
+++ plugin-server/src/main/ingestion-queues/anonymous-event-buffer-consumer.ts
@@ -5,6 +5,28 @@ import { JobQueueManager } from 'main/job-queues/job-queue-manager'
 import { JobName } from '../../types'
 
 export const startAnonymousEventBufferConsumer = (kafka: Kafka, jobQueueManager: JobQueueManager) => {
+    /*
+        Consumes from the anonymous event topic, and enqueues the events for
+        processing at a later date as per the message header `processEventAt`.
+
+        We do this delayed processing to allow for the anonymous users that
+        these events are assosiated to be merged or identified as other
+        "identified" users, at which point to can denormalize data to improve
+        query performance.
+
+        On failure to enqueue we will fail the batch. only on successfully
+        enqueuing an entire batch to Graphile Worker (a PostgreSQL backed async
+        worker library).
+
+        TODO:
+
+        1. handle unhandled exceptions behaviour. At the moment we will end up 
+           with an unhandled exception and I think the consumer will just stop.
+        2. catch deserialization errors and send to Dead Letter Queue.
+        3. catch Graphile Worker errors and send to Dead Letter Queue on all 
+           but PostgreSQL availability errors.
+    */
+
     const consumer = kafka.consumer({ groupId: 'clickhouse-ingester' })
 
     void consumer.run({
