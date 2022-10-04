74ea41863e86d58511fd6c4cbfcc7039497082de
Marius Andra <marius.andra@gmail.com>
2022-10-04 21:09:34 +0200

    we're down under

--- posthog/api/geoip.py
+++ posthog/api/geoip.py
@@ -40,8 +40,12 @@ def get_geoip_properties(ip_address: Optional[str]) -> Dict[str, str]:
         $geoip_postal_code
         $geoip_time_zone
     """
-    if not ip_address or not geoip or ip_address == "127.0.0.1":
-        # "127.0.0.1" would throw "The address 127.0.0.1 is not in the database." below
+    if ip_address == "127.0.0.1":
+        # Spoofing an Australian IP address for local development
+        # Synced with https://github.com/PostHog/posthog-plugin-geoip/blob/main/index.ts
+        ip_address = "13.106.122.3"
+
+    if not ip_address or not geoip:
         return {}
 
     try:
