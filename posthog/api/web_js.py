93bf0d04d186bb58ee2eb1ec30014ebe6bbc768c
Marius Andra <marius.andra@gmail.com>
2022-10-04 21:21:28 +0200

    raise on 404

--- posthog/api/web_js.py
+++ posthog/api/web_js.py
@@ -20,13 +20,14 @@ def get_web_js(request: HttpRequest, id: int, token: str):
         return cors_response(request, JsonResponse({"status": 1}))
 
     try:
-        response = ""
         source_file = get_transpiled_web_source(id, token) if token else None
         if source_file:
             id = source_file.id
             source = source_file.source
             config = get_web_config_from_schema(source_file.config_schema, source_file.config)
             response = f"{source}().inject({{config:{json.dumps(config)},posthog:window['__$$ph_web_js_{id}']}})"
+        else:
+            raise Exception("No source file found")
 
         statsd.incr(f"posthog_cloud_raw_endpoint_success", tags={"endpoint": "web_js"})
         return cors_response(request, HttpResponse(content=response, content_type="application/javascript"))
@@ -37,7 +38,7 @@ def get_web_js(request: HttpRequest, id: int, token: str):
             request,
             generate_exception_response(
                 "web_js",
-                "Unable to render javascript. Please try again. If you are the owner of this app you can check the logs for further details.",
+                "Unable to serve app source code.",
                 code="server_error",
                 type="server_error",
                 status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
