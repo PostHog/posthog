26ffa07f8712c86b0be0c87e9ac7e2ae884bab27
Paul Hultgren <pjhultgren5@gmail.com>
2022-10-04 20:23:06 -0700

    don't show DEBUG logs by default

--- frontend/src/scenes/plugins/plugin/pluginLogsLogic.ts
+++ frontend/src/scenes/plugins/plugin/pluginLogsLogic.ts
@@ -94,7 +94,7 @@ export const pluginLogsLogic = kea<pluginLogsLogicType>({
     }),
     reducers: {
         pluginLogsTypes: [
-            Object.values(PluginLogEntryType),
+            Object.values(PluginLogEntryType).filter((type) => type !== 'DEBUG'),
             {
                 setPluginLogsTypes: (_, { typeFilters }) => typeFilters.map((tf) => tf as PluginLogEntryType),
             },
