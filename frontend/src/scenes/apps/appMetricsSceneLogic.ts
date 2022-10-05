0a9894fad96b554b69701c38248240a894a284e9
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-10-05 11:53:28 +0300

    Placeholder breadcrumbs

--- frontend/src/scenes/apps/appMetricsSceneLogic.ts
+++ frontend/src/scenes/apps/appMetricsSceneLogic.ts
@@ -1,6 +1,8 @@
-import { kea, key, props, path } from 'kea'
+import { kea, key, props, path, selectors } from 'kea'
 
 import type { appMetricsSceneLogicType } from './appMetricsSceneLogicType'
+import { urls } from 'scenes/urls'
+import { Breadcrumb } from '~/types'
 
 export interface AppMetricsLogicProps {
     /** Used as the logic's key */
@@ -11,4 +13,21 @@ export const appMetricsSceneLogic = kea<appMetricsSceneLogicType>([
     path(['scenes', 'apps', 'appMetricsSceneLogic']),
     props({} as AppMetricsLogicProps),
     key((props) => props.pluginConfigId),
+
+    selectors({
+        breadcrumbs: [
+            () => [(_, props) => props.pluginConfigId],
+            (pluginConfigId: number): Breadcrumb[] => [
+                {
+                    name: 'Apps',
+                    path: urls.projectApps(),
+                },
+                {
+                    // :TODO: Load and show plugin name here
+                    name: `Metrics for ${pluginConfigId}`,
+                    path: urls.appMetrics(pluginConfigId),
+                },
+            ],
+        ],
+    }),
 ])
