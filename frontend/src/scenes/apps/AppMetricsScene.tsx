f11c58dd5873d510030d07be6c0b866d7cf8614a
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-10-05 14:13:56 +0300

    Display simple overview stats

--- frontend/src/scenes/apps/AppMetricsScene.tsx
+++ frontend/src/scenes/apps/AppMetricsScene.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { SceneExport } from 'scenes/sceneTypes'
 import { appMetricsSceneLogic } from 'scenes/apps/appMetricsSceneLogic'
+import { Card } from 'antd'
 
 export function AppMetrics(): JSX.Element {
-    return <></>
+    return (
+        <div className="mt-4">
+            <Card title="Metrics overview">
+                <div>
+                    <div className="card-secondary">Events delivered on first try</div>
+                    <div>568,048</div>
+                </div>
+                <div>
+                    <div className="card-secondary">Events delivered on retry</div>
+                    <div>134</div>
+                </div>
+                <div>
+                    <div className="card-secondary">Events failed</div>
+                    <div>0</div>
+                </div>
+            </Card>
+        </div>
+    )
 }
 
 export const scene: SceneExport = {
