097df2494a45d67ee62109e6740870d6c554e3ff
Ben White <ben@posthog.com>
2022-10-06 16:39:22 +0200

    fix migrations

--- posthog/migrations/0265_alter_organization_plugins_access_level.py
+++ posthog/migrations/0265_alter_organization_plugins_access_level.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("posthog", "0261_team_capture_console_log_opt_in"),
+        ("posthog", "0264_user_partial_notification_settings"),
     ]
 
     operations = [
