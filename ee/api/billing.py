65a68be292bf9709ecdf8a01f95b602475e0e4e8
Ben White <ben@posthog.com>
2022-10-06 16:11:42 +0200

    Fixed cache key

--- ee/api/billing.py
+++ ee/api/billing.py
@@ -81,7 +81,7 @@ def get_cached_current_usage(organization: Organization) -> Dict[str, int]:
     """
     Calculate the actual current usage for an organization - only used if a subscription does not exist
     """
-    cache_key: str = f"org_monthly_usage_{organization.id}"
+    cache_key: str = f"monthly_usage_{organization.id}"
     usage: Optional[Dict[str, int]] = cache.get(cache_key)
 
     if usage is None:
