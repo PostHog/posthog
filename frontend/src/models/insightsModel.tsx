d6f83b0d865e310644a7cd64f5a7b464d545d8eb
Paul D'Ambra <paul@posthog.com>
2022-10-07 10:26:12 +0100

    chore: tile ify dashboard tiles with added 🤞 performance boost (#12135)
    Reverts #12134
    
    see #12067 for reason and review
    
    When merged this slowed down the dashboards list endpoint
    
    There are three changes here:
    
    most importantly this no longer prefetches tiles when listing dashboards
    
    the tiles are not returned in the response anyway
    when not listing a dashboard
    
    no longer prefetches insights separately, we're not using them
    limits the queryset that is loading the dashboard tiles to the dashboard being loaded. There may be a more correct way to do this but 🤷

--- frontend/src/models/insightsModel.tsx
+++ frontend/src/models/insightsModel.tsx
@@ -3,13 +3,9 @@ import { kea } from 'kea'
 import api from 'lib/api'
 import { prompt } from 'lib/logic/prompt'
 import { InsightModel } from '~/types'
-import { dashboardsModel } from './dashboardsModel'
-import { Link } from 'lib/components/Link'
-import { urls } from 'scenes/urls'
 import { teamLogic } from 'scenes/teamLogic'
 import type { insightsModelType } from './insightsModelType'
 import { lemonToast } from 'lib/components/lemonToast'
-import { router } from 'kea-router'
 
 export const insightsModel = kea<insightsModelType>({
     path: ['models', 'insightsModel'],
@@ -20,12 +16,6 @@ export const insightsModel = kea<insightsModelType>({
         duplicateInsight: (item: InsightModel, dashboardId?: number) => ({
             item,
             dashboardId,
-        }),
-        moveToDashboard: (item: InsightModel, fromDashboard: number, toDashboard: number, toDashboardName: string) => ({
-            item,
-            fromDashboard,
-            toDashboard,
-            toDashboardName,
         }),
         duplicateInsightSuccess: (item: InsightModel) => ({ item }),
     }),
@@ -52,70 +42,17 @@ export const insightsModel = kea<insightsModelType>({
                 },
             })
         },
-        moveToDashboard: async ({ item, fromDashboard, toDashboard, toDashboardName }) => {
+        duplicateInsight: async ({ item }) => {
             if (!item) {
                 return
             }
-
-            const originalDashboards = item.dashboards || []
-            const dashboards = [...originalDashboards.filter((d: number) => d !== fromDashboard), toDashboard]
-
-            const updatedItem = await api.update(`api/projects/${teamLogic.values.currentTeamId}/insights/${item.id}`, {
-                dashboards,
-            })
-            dashboardsModel.actions.updateDashboardInsight(updatedItem, [fromDashboard])
-
-            lemonToast.success(
-                <>
-                    Insight moved to{' '}
-                    <b>
-                        <Link to={urls.dashboard(toDashboard)}>{toDashboardName}</Link>
-                    </b>
-                </>,
-                {
-                    button: {
-                        label: 'Undo',
-                        action: async () => {
-                            const restoredItem = await api.update(
-                                `api/projects/${teamLogic.values.currentTeamId}/insights/${item.id}`,
-                                {
-                                    dashboards: originalDashboards,
-                                }
-                            )
-                            lemonToast.success('Panel move reverted')
-                            dashboardsModel.actions.updateDashboardInsight(restoredItem, [toDashboard])
-                        },
-                    },
-                }
-            )
-        },
-        duplicateInsight: async ({ item, dashboardId }) => {
-            if (!item) {
-                return
-            }
-
-            const layouts: Record<string, any> = {}
-            Object.entries(item.layouts || {}).forEach(([size, { w, h }]) => {
-                layouts[size] = { w, h }
-            })
 
             const { id: _discard, short_id: __discard, ...rest } = item
-            const newItem = dashboardId ? { ...rest, dashboards: [dashboardId], layouts } : { ...rest, layouts }
+            const newItem = { ...rest, name: (rest.name || rest.derived_name) + ' (copy)' }
             const addedItem = await api.create(`api/projects/${teamLogic.values.currentTeamId}/insights`, newItem)
 
-            const dashboard = dashboardId ? dashboardsModel.values.rawDashboards[dashboardId] : null
-
-            if (dashboardId && dashboard) {
-                lemonToast.success('Insight copied', {
-                    button: {
-                        label: `View ${dashboard.name}`,
-                        action: () => router.actions.push(urls.dashboard(dashboard.id)),
-                    },
-                })
-            } else {
-                actions.duplicateInsightSuccess(addedItem)
-                lemonToast.success('Insight duplicated')
-            }
+            actions.duplicateInsightSuccess(addedItem)
+            lemonToast.success('Insight duplicated')
         },
     }),
 })
