4594aaa3ddd8a6c05e64f377a14e06acaf927ae9
Harry Waye <harry@posthog.com>
2022-10-07 13:43:44 +0100

    Update plugin-server/jest.setup.fetch-mock.js
    Co-authored-by: Guido Iaquinti <4038041+guidoiaquinti@users.noreply.github.com>

--- plugin-server/jest.setup.fetch-mock.js
+++ plugin-server/jest.setup.fetch-mock.js
@@ -36,5 +36,5 @@ beforeEach(() => {
 })
 
 // NOTE: in testing we use the pino-pretty transport, which results in a handle
-// that we need to close to allos Jest to exit properly.
+// that we need to close to allow Jest to exit properly.
 afterAll(() => status.close())
