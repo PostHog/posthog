b17734bd746777845334ae0a38c9e50d0857284c
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-10-07 10:35:02 +0300

    Add mising app-metrics file

---
+++ plugin-server/src/worker/ingestion/app-metrics.ts
@@ -0,0 +1,125 @@
+import { Message } from 'kafkajs'
+
+import { Hub, TeamId } from '../../types'
+
+export interface AppMetricIdentifier {
+    teamId: TeamId
+    pluginConfigId: number
+    jobId?: string
+    // :TODO: Verify this list
+    category: 'processEvent' | 'onEvent' | 'exportEvents'
+}
+
+export interface AppMetric extends AppMetricIdentifier {
+    successes?: number
+    successesOnRetry?: number
+    failures?: number
+}
+
+interface QueuedMetric {
+    successes: number
+    successesOnRetry: number
+    failures: number
+    lastTimestamp: number
+    queuedAt: number
+    metric: AppMetricIdentifier
+}
+
+export class AppMetrics {
+    hub: Hub
+    queuedData: Record<string, QueuedMetric>
+
+    flushFrequencyMs: number
+    maxQueueSize: number
+
+    timer: NodeJS.Timeout | null
+
+    constructor(hub: Hub) {
+        this.hub = hub
+        this.queuedData = {}
+
+        this.flushFrequencyMs = hub.APP_METRICS_FLUSH_FREQUENCY_MS
+        this.maxQueueSize = hub.APP_METRICS_MAX_QUEUE_SIZE
+        this.timer = null
+    }
+
+    async queueMetric(metric: AppMetric, timestamp?: number): Promise<void> {
+        timestamp = timestamp || Date.now()
+        const key = this._key(metric)
+
+        if (!(await this.hub.organizationManager.hasAvailableFeature(metric.teamId, 'app_metrics'))) {
+            return
+        }
+
+        const { successes, successesOnRetry, failures, ...metricInfo } = metric
+
+        if (!this.queuedData[key]) {
+            if (this.shouldFlushQueue()) {
+                this.hub.promiseManager.trackPromise(this.flush())
+                await this.hub.promiseManager.awaitPromisesIfNeeded()
+            }
+
+            this.queuedData[key] = {
+                successes: 0,
+                successesOnRetry: 0,
+                failures: 0,
+                lastTimestamp: timestamp,
+                queuedAt: timestamp,
+                metric: metricInfo,
+            }
+        }
+
+        if (successes) {
+            this.queuedData[key].successes += successes
+        }
+        if (successesOnRetry) {
+            this.queuedData[key].successesOnRetry += successesOnRetry
+        }
+        if (failures) {
+            this.queuedData[key].failures += failures
+        }
+        this.queuedData[key].lastTimestamp = timestamp
+
+        if (this.timer === null) {
+            this.timer = setTimeout(() => {
+                this.hub.promiseManager.trackPromise(this.flush())
+                this.timer = null
+            }, this.flushFrequencyMs)
+        }
+    }
+
+    async flush(): Promise<void> {
+        if (Object.keys(this.queuedData).length === 0) {
+            return
+        }
+
+        const queue = this.queuedData
+        this.queuedData = {}
+
+        const kafkaMessages: Message[] = Object.values(queue).map((value) => ({
+            value: JSON.stringify({
+                teamId: value.metric.teamId,
+                pluginConfigId: value.metric.pluginConfigId,
+                jobId: value.metric.jobId ?? null,
+                category: value.metric.category,
+
+                successes: value.successes,
+                successesOnRetry: value.successesOnRetry,
+                failures: value.failures,
+            }),
+        }))
+
+        await this.hub.db.kafkaProducer.queueMessage({
+            topic: 'TODO',
+            messages: kafkaMessages,
+        })
+    }
+
+    shouldFlushQueue(): boolean {
+        return Object.keys(this.queuedData).length >= this.maxQueueSize
+    }
+
+    _key(metric: AppMetric): string {
+        return `${metric.teamId}.${metric.pluginConfigId}.${metric.category}.${metric.jobId}`
+    }
+}
