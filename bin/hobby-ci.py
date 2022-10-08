239c712632dcbd32a78b18e14b4904bf8bdf9ce4
James Greenhill <fuziontech@gmail.com>
2022-10-07 18:27:32 -0700

    Use env vars because its too tempting to check in a token...

--- bin/hobby-ci.py
+++ bin/hobby-ci.py
@@ -10,6 +10,7 @@ import digitalocean
 import random
 import string
 import time
+import os
 
 letters = string.ascii_lowercase
 random_bit = ''.join(random.choice(letters) for i in range(4))
@@ -23,7 +24,7 @@ user_data = f'#!/bin/bash \n' \
 			f'wget https://raw.githubusercontent.com/posthog/posthog/HEAD/bin/deploy-hobby \n' \
 			f'chmod +x deploy-hobby \n' \
 			f'./deploy-hobby {release_tag} {hostname}\n'
-token = 'hah-no'
+token = os.getenv("DIGITALOCEAN_ACCESS_TOKEN")
 
 
 def block_until_droplet_is_started(droplet):
