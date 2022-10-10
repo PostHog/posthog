facd89e83eaf1af7e2ce502206fe4db5b7752320
Ben White <ben@posthog.com>
2022-10-10 13:59:28 +0200

    Added more buttons for viewing recordings from data management

--- cypress/e2e/eventDefinitions.js
+++ cypress/e2e/eventDefinitions.js
@@ -6,8 +6,8 @@ describe('Event Definitions', () => {
     it('See recordings action', () => {
         cy.get('[data-attr=events-definition-table]').should('exist')
         cy.get('[data-attr=event-definitions-table-more-button-entered_free_trial]').first().click()
-        cy.get('[data-attr=event-definitions-table-see-recordings]').should('exist')
-        cy.get('[data-attr=event-definitions-table-see-recordings]').click()
+        cy.get('[data-attr=event-definitions-table-view-recordings]').should('exist')
+        cy.get('[data-attr=event-definitions-table-view-recordings]').click()
         cy.url().should('contain', 'entered_free_trial')
     })
 })
