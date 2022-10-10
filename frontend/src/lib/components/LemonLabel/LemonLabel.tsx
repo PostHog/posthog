cd8c9b4be61f8e3eba964ec7b7774e2dfce889e9
Emanuele Capparelli <k@emkpp.com>
2022-10-10 19:23:19 +0200

    feat: add explanation modal to cloud region selector

--- frontend/src/lib/components/LemonLabel/LemonLabel.tsx
+++ frontend/src/lib/components/LemonLabel/LemonLabel.tsx
@@ -8,14 +8,28 @@ export interface LemonLabelProps
     extends Pick<React.LabelHTMLAttributes<HTMLLabelElement>, 'htmlFor' | 'form' | 'children' | 'className'> {
     info?: React.ReactNode
     showOptional?: boolean
+    showExplanationCTA?: boolean
 }
 
-export function LemonLabel({ children, info, className, showOptional, ...props }: LemonLabelProps): JSX.Element {
+export function LemonLabel({
+    children,
+    info,
+    className,
+    showOptional,
+    showExplanationCTA,
+    ...props
+}: LemonLabelProps): JSX.Element {
     return (
         <label className={clsx('LemonLabel', className)} {...props}>
             {children}
 
             {showOptional ? <span className="LemonLabel__extra">(optional)</span> : null}
+
+            {showExplanationCTA ? (
+                <a>
+                    <span className="LemonLabel__extra">(what is this?)</span>
+                </a>
+            ) : null}
 
             {info ? (
                 <Tooltip title={info}>
