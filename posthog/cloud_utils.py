85e091934de29afe615d8d12f8e964e4df547d9f
Ben White <ben@posthog.com>
2022-10-11 13:59:04 +0200

    Fix

--- posthog/cloud_utils.py
+++ posthog/cloud_utils.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db.utils import ProgrammingError
 
 
 def is_cloud():
@@ -6,7 +7,10 @@ def is_cloud():
     try:
         from ee.models import License
 
+        # TRICKY - The license table may not exist if a migration is running
         license = License.objects.first_valid()
         return license.plan == "cloud" if license else settings.MULTI_TENANCY
-    except ImportError:
+
+    # TRICKY - The license table may not exist if a migration is running
+    except (ImportError, ProgrammingError):
         return False
