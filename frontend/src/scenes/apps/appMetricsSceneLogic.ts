594c17624a827ec9ef8bf70c90023cb4889b7ee4
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-10-11 13:05:01 +0300

    Simple tabbing system + loading data

--- frontend/src/scenes/apps/appMetricsSceneLogic.ts
+++ frontend/src/scenes/apps/appMetricsSceneLogic.ts
@@ -1,4 +1,4 @@
-import { kea, key, props, path, selectors } from 'kea'
+import { kea, key, props, path, actions, selectors, reducers } from 'kea'
 import { loaders } from 'kea-loaders'
 
 import type { appMetricsSceneLogicType } from './appMetricsSceneLogicType'
@@ -6,17 +6,44 @@ import { urls } from 'scenes/urls'
 import { Breadcrumb, PluginConfigWithPluginInfo } from '~/types'
 import api from '../../lib/api'
 import { teamLogic } from '../teamLogic'
-import { urlToAction } from 'kea-router'
+import { actionToUrl, urlToAction } from 'kea-router'
 
 export interface AppMetricsLogicProps {
     /** Used as the logic's key */
     pluginConfigId: number
 }
 
+export enum AppMetricsTab {
+    Metrics = 'Metrics',
+    Exports = 'Exports',
+}
+
+export interface HistoricalExportInfo {
+    job_id: string
+    status: 'success' | 'fail' | 'not_finished'
+    payload: Record<string, any>
+    started_at: string
+    finished_at?: string
+    duration?: number
+}
+
 export const appMetricsSceneLogic = kea<appMetricsSceneLogicType>([
     path(['scenes', 'apps', 'appMetricsSceneLogic']),
     props({} as AppMetricsLogicProps),
     key((props) => props.pluginConfigId),
+
+    actions({
+        setTab: (tab: AppMetricsTab) => ({ tab }),
+    }),
+
+    reducers({
+        activeTab: [
+            AppMetricsTab.Metrics as AppMetricsTab,
+            {
+                setTab: (_, { tab }) => tab,
+            },
+        ],
+    }),
 
     loaders(({ props }) => ({
         pluginConfig: [
@@ -26,6 +53,17 @@ export const appMetricsSceneLogic = kea<appMetricsSceneLogicType>([
                     return await api.get(
                         `api/projects/${teamLogic.values.currentTeamId}/plugin_configs/${props.pluginConfigId}`
                     )
+                },
+            },
+        ],
+        historicalExports: [
+            [] as Array<HistoricalExportInfo>,
+            {
+                loadHistoricalExports: async () => {
+                    const { results } = await api.get(
+                        `api/projects/${teamLogic.values.currentTeamId}/app_metrics/${props.pluginConfigId}/historical_exports`
+                    )
+                    return results as Array<HistoricalExportInfo>
                 },
             },
         ],
@@ -47,9 +85,17 @@ export const appMetricsSceneLogic = kea<appMetricsSceneLogicType>([
         ],
     })),
 
-    urlToAction(({ actions }) => ({
-        '/app/:pluginConfigId/metrics': () => {
-            actions.loadPluginConfig()
+    actionToUrl(({ values, props }) => ({
+        setTab: () => `/app/${props.pluginConfigId}/${values.activeTab}`,
+    })),
+
+    urlToAction(({ values, actions }) => ({
+        '/app/:pluginConfigId/:tab': (params: Record<string, string | undefined>) => {
+            actions.loadHistoricalExports()
+            actions.setTab(params.tab as AppMetricsTab)
+            if (!values.pluginConfig) {
+                actions.loadPluginConfig()
+            }
         },
     })),
 ])
