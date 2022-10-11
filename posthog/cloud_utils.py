a67c31e4a109653c678608af822b525501c81b23
Ben White <ben@posthog.com>
2022-10-11 14:26:33 +0200

    Fix caching of is_cloud

--- posthog/cloud_utils.py
+++ posthog/cloud_utils.py
@@ -1,16 +1,23 @@
+from typing import Optional
 from django.conf import settings
 from django.db.utils import ProgrammingError
 
 
+is_cloud_cached: Optional[bool] = None
+
+# NOTE: This is cached for the lifetime of the instance but this is not an issue as the value is not expected to change
 def is_cloud():
-    # TODO: Possibly cache this for a time?
+    global is_cloud_cached
+    if is_cloud_cached != None:
+        return is_cloud_cached
+
     try:
         from ee.models import License
 
         # TRICKY - The license table may not exist if a migration is running
         license = License.objects.first_valid()
-        return license.plan == "cloud" if license else settings.MULTI_TENANCY
-
+        is_cloud_cached = license.plan == "cloud" if license else settings.MULTI_TENANCY
+        return is_cloud_cached
     # TRICKY - The license table may not exist if a migration is running
     except (ImportError, ProgrammingError):
         return False
