c45a1f7537a7b2e54e08396a715edd14a5c4cdf6
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-10-12 10:20:41 +0300

    WIP: Export summary +- 1 hour to avoid graphs looking ugly

--- posthog/queries/app_metrics/historical_exports.py
+++ posthog/queries/app_metrics/historical_exports.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from typing import Dict, Optional
 
 import pytz
@@ -25,7 +26,7 @@ def historical_exports_activity(team_id: int, plugin_config_id: int, job_id: Opt
     historical_exports = []
     for job_id, trigger_entry in by_category["job_triggered"].items():
         record = {
-            "started_at": trigger_entry.created_at.astimezone(pytz.utc).isoformat(),
+            "started_at": trigger_entry.created_at,
             "job_id": job_id,
             "payload": trigger_entry.detail["trigger"]["payload"],
         }
@@ -33,12 +34,12 @@ def historical_exports_activity(team_id: int, plugin_config_id: int, job_id: Opt
         if job_id in by_category["export_success"]:
             entry = by_category["export_success"][job_id]
             record["status"] = "success"
-            record["finished_at"] = entry.created_at.astimezone(pytz.utc).isoformat()
+            record["finished_at"] = entry.created_at
             record["duration"] = (entry.created_at - trigger_entry.created_at).total_seconds()
         elif job_id in by_category["export_fail"]:
             entry = by_category["export_fail"][job_id]
             record["status"] = "fail"
-            record["finished_at"] = entry.created_at.astimezone(pytz.utc).isoformat()
+            record["finished_at"] = entry.created_at
             record["duration"] = (entry.created_at - trigger_entry.created_at).total_seconds()
         else:
             record["status"] = "not_finished"
@@ -55,10 +56,10 @@ def historical_export_metrics(team: Team, plugin_config_id: int, job_id: str):
     filter_data = {
         "category": "exportEvents",
         "job_id": job_id,
-        "date_from": export_summary["started_at"],
+        "date_from": (export_summary["started_at"] - timedelta(hours=1)).astimezone(pytz.utc).isoformat(),
     }
     if "finished_at" in export_summary:
-        filter_data["date_to"] = export_summary["finished_at"]
+        filter_data["date_to"] = (export_summary["finished_at"] + timedelta(hours=1)).astimezone(pytz.utc).isoformat()
 
     filter = AppMetricsRequestSerializer(data=filter_data)
     filter.is_valid(raise_exception=True)
