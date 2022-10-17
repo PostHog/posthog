69a77439e837f4dcb704822ad0fb8e4a90fc177e
Paul D'Ambra <paul@posthog.com>
2022-10-17 22:01:29 +0100

    more tests

--- posthog/models/test/test_insight_model.py
+++ posthog/models/test/test_insight_model.py
@@ -1,6 +1,7 @@
 from django.db.utils import IntegrityError
 
 from posthog.models import Dashboard, Insight, Team
+from posthog.models.insight import generate_insight_cache_key
 from posthog.test.base import BaseTest
 
 
@@ -50,3 +51,24 @@ class TestInsightModel(BaseTest):
         )
 
         assert filters_with_dashboard_with_same_date_from["date_from"] == "-30d"
+
+    def test_dashboard_does_not_affect_filters_hash_with_absent_or_null_date_from(self) -> None:
+        insight = Insight.objects.create(team=self.team, filters={"date_from": "-30d"})
+        dashboard_one = Dashboard.objects.create(team=self.team, filters={"date_from": None})
+        dashboard_two = Dashboard.objects.create(team=self.team, filters={})
+
+        filters_hash_no_dashboard = generate_insight_cache_key(insight, None)
+        filters_hash_with_null_date_from = generate_insight_cache_key(insight, dashboard_one)
+        filters_hash_with_absent_date_from = generate_insight_cache_key(insight, dashboard_two)
+
+        assert filters_hash_no_dashboard == filters_hash_with_null_date_from
+        assert filters_hash_with_null_date_from == filters_hash_with_absent_date_from
+
+    def test_dashboard_with_date_from_changes_filters_hash(self) -> None:
+        insight = Insight.objects.create(team=self.team, filters={"date_from": "-30d"})
+        dashboard = Dashboard.objects.create(team=self.team, filters={"date_from": "-90d"})
+
+        filters_hash_one = generate_insight_cache_key(insight, None)
+        filters_hash_two = generate_insight_cache_key(insight, dashboard)
+
+        assert filters_hash_one != filters_hash_two
