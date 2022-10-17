aa61ad9c01bf707956c3723c6f9bc983596951e7
Michael Matloka <dev@twixes.com>
2022-10-17 12:57:41 +0200

    Update plugins-api-key-manager.test.ts

--- plugin-server/tests/worker/plugins-api-key-manager.test.ts
+++ plugin-server/tests/worker/plugins-api-key-manager.test.ts
@@ -1,9 +1,11 @@
 import { Hub } from '../../src/types'
 import { createHub } from '../../src/utils/db/hub'
 import { PluginsApiKeyManager } from '../../src/worker/vm/extensions/helpers/api-key-manager'
-import { pluginConfig39 } from '../helpers/plugins'
 import { createUserTeamAndOrganization } from '../helpers/sql'
 import { POSTGRES_TRUNCATE_TABLES_QUERY } from '../helpers/sql'
+
+const ORG_ID_1 = '0174f81e-36f5-0000-7ef8-cc26c1fbab1c'
+const ORG_ID_2 = '4dc8564d-bd82-1065-2f40-97f7c50f67cf'
 
 describe('PluginsApiKeyManager', () => {
     let hub: Hub
@@ -14,48 +16,45 @@ describe('PluginsApiKeyManager', () => {
             TASK_TIMEOUT: 1,
         })
         await hub.db.postgresQuery(POSTGRES_TRUNCATE_TABLES_QUERY, [], 'truncateTablesTest')
-        await hub.db.redisExpire(`@plugin/60/88/_bot_api_key`, 0)
-        await hub.db.redisExpire(`@plugin/60/99/_bot_api_key`, 0)
+        await hub.db.redisExpire(`plugins-api-key-manager/${ORG_ID_1}`, 0)
+        await hub.db.redisExpire(`plugins-api-key-manager/${ORG_ID_2}`, 0)
     })
 
     afterEach(async () => {
         await closeHub()
     })
 
-    test('PluginsApiKeyManager', async () => {
+    test('fetchOrCreatePersonalApiKey', async () => {
         const pluginsApiKeyManager = new PluginsApiKeyManager(hub.db)
-
-        const orgId1 = '0174f81e-36f5-0000-7ef8-cc26c1fbab1c'
-        const orgId2 = '4dc8564d-bd82-1065-2f40-97f7c50f67cf'
 
         jest.spyOn(hub.db, 'createUser')
 
-        await createUserTeamAndOrganization(hub!.postgres, 88, 8888, 'a73fc995-a63f-4e4e-bf65-2a5e9f93b2b2', orgId1)
+        await createUserTeamAndOrganization(hub!.postgres, 88, 8888, 'a73fc995-a63f-4e4e-bf65-2a5e9f93b2b2', ORG_ID_1)
 
         await createUserTeamAndOrganization(
             hub!.postgres,
             99,
             9999,
             '017d107d-219a-0000-ddef-84f776dcf22b',
-            orgId2,
+            ORG_ID_2,
             '017d107d-21a2-0001-4032-b47cdf5e09dc'
         )
 
-        const key1 = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(orgId1, { ...pluginConfig39, team_id: 88 })
+        const key1 = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(ORG_ID_1)
         expect(hub.db.createUser).toHaveBeenCalledTimes(1)
         expect(hub.db.createUser).toHaveBeenCalledWith(
             expect.objectContaining({
                 organizationMembershipLevel: 8,
-                organization_id: orgId1,
+                organization_id: ORG_ID_1,
             })
         )
 
-        const key2 = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(orgId2, { ...pluginConfig39, team_id: 99 })
+        const key2 = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(ORG_ID_2)
         expect(hub.db.createUser).toHaveBeenCalledTimes(2)
         expect(hub.db.createUser).toHaveBeenCalledWith(
             expect.objectContaining({
                 organizationMembershipLevel: 8,
-                organization_id: orgId2,
+                organization_id: ORG_ID_2,
             })
         )
 
@@ -63,19 +62,14 @@ describe('PluginsApiKeyManager', () => {
         expect(key1).not.toEqual(key2)
 
         // check that we hit the cache
-        const key = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(orgId1, { ...pluginConfig39, team_id: 88 })
-        expect(key).toMatch(/^pbkdf2_sha256\$480000\$posthog_personal_api_key/)
+        const key = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(ORG_ID_1)
 
         expect(hub.db.createUser).toHaveBeenCalledTimes(2)
 
         // What happens when the key still exists, but it's not in the cache anymore
-        await hub.db.redisExpire(`@plugin/60/88/_bot_api_key`, 0)
+        await hub.db.redisExpire(`plugins-api-key-manager/${ORG_ID_1}`, 0)
 
-        const newkey = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(orgId1, {
-            ...pluginConfig39,
-            team_id: 88,
-        })
-        expect(newkey).toMatch(/^pbkdf2_sha256\$480000\$posthog_personal_api_key/)
-        expect(newkey).not.toEqual(key)
+        const newKey = await pluginsApiKeyManager.fetchOrCreatePersonalApiKey(ORG_ID_1)
+        expect(newKey).not.toEqual(key)
     })
 })
