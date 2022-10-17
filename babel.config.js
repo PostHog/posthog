5bda5c5c8dcd2e993bbf29d2c2d9c39611e40c7c
Michael Matloka <dev@twixes.com>
2022-10-17 18:59:05 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@
 module.exports = {
     plugins: [
         '@babel/plugin-transform-runtime',
-        '@babel/plugin-transform-react-jsx',
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-private-property-in-object',
     ],
