f99b11d5ad80bb02e1febe18ee22140dacc8aeb7
Paul Hultgren <pjhultgren5@gmail.com>
2022-10-17 23:05:59 -0700

    make frontend github url validation more lenient

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -1059,7 +1059,7 @@ export function identifierToHuman(identifier: string | number, caseType: 'senten
 }
 
 export function parseGithubRepoURL(url: string): Record<string, string> {
-    const match = url.match(/^https?:\/\/(?:www\.)?github\.com\/([A-Za-z0-9_.-]+)\/([A-Za-z0-9_.-]+)\/?$/)
+    const match = url.match(/^https?:\/\/(?:www\.)?github\.com\/([A-Za-z0-9_.-]+)\/([A-Za-z0-9_.-]+)\/?/)
     if (!match) {
         throw new Error('Must be in the format: https://github.com/user/repo')
     }
