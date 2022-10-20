b2f1150cdb6374ac38939a1ddcb121405d091b15
eric <eeoneric@gmail.com>
2022-10-20 12:58:22 -0400

    rollback test

--- ee/tasks/test/test_auto_rollback_feature_flag.py
+++ ee/tasks/test/test_auto_rollback_feature_flag.py
@@ -1,6 +1,6 @@
 from freezegun import freeze_time
 
-from ee.tasks.auto_rollback_feature_flag import check_condition
+from ee.tasks.auto_rollback_feature_flag import check_condition, check_feature_flag_rollback_conditions
 from posthog.models.feature_flag import FeatureFlag
 from posthog.test.base import APIBaseTest, _create_event
 
@@ -71,3 +71,50 @@ class AutoRollbackTest(APIBaseTest):
         # Go another day with 0 events
         with freeze_time("2021-08-25T20:00:00.000Z"):
             self.assertEqual(check_condition(rollback_condition, flag), True)
+
+    def test_feature_flag_rolledback(self):
+        rollback_condition = {
+            "threshold": 5,
+            "threshold_metric": {
+                "insight": "trends",
+                "events": [{"order": 0, "id": "$pageview"}],
+            },
+            "operator": "lt",
+            "threshold_type": "insight",
+        }
+
+        for _ in range(10):
+            _create_event(
+                event="$pageview",
+                distinct_id="1",
+                team=self.team,
+                timestamp="2021-08-21 00:00:00",
+                properties={"prop": 1},
+            )
+            _create_event(
+                event="$pageview",
+                distinct_id="1",
+                team=self.team,
+                timestamp="2021-08-22 00:00:00",
+                properties={"prop": 1},
+            )
+
+        with freeze_time("2021-08-21T20:00:00.000Z"):
+            flag = FeatureFlag.objects.create(
+                team=self.team,
+                created_by=self.user,
+                key="test-ff",
+                rollout_percentage=50,
+                auto_rollback=True,
+                rollback_conditions=[rollback_condition],
+            )
+
+        flag = FeatureFlag.objects.get(pk=flag.pk)
+        self.assertEqual(flag.performed_rollback, False)
+        self.assertEqual(flag.active, True)
+
+        with freeze_time("2021-08-25T20:00:00.000Z"):
+            check_feature_flag_rollback_conditions(feature_flag_id=flag.pk)
+            flag = FeatureFlag.objects.get(pk=flag.pk)
+            self.assertEqual(flag.performed_rollback, True)
+            self.assertEqual(flag.active, False)
