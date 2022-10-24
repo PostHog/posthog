7f687c1d9fef02b46858925014b9b4b1a5324686
Ben White <ben@posthog.com>
2022-10-24 12:22:10 +0200

    Updated to use shared function

--- ee/tasks/subscriptions/subscription_utils.py
+++ ee/tasks/subscriptions/subscription_utils.py
@@ -11,6 +11,7 @@ from posthog.models.insight import Insight
 from posthog.models.sharing_configuration import SharingConfiguration
 from posthog.models.subscription import Subscription
 from posthog.tasks import exporter
+from posthog.utils import wait_for_parallel_celery_group
 
 logger = structlog.get_logger(__name__)
 
@@ -41,10 +42,6 @@ def generate_assets(
     tasks = [exporter.export_asset.s(asset.id) for asset in assets]
     parallel_job = group(tasks).apply_async()
 
-    start_time = datetime.now()
-    while not parallel_job.ready():
-        sleep(1)
-        if datetime.now() > start_time + ASSET_GENERATION_MAX_TIMEOUT:
-            raise Exception("Timed out waiting for exports")
+    wait_for_parallel_celery_group(parallel_job, max_timeout=ASSET_GENERATION_MAX_TIMEOUT)
 
     return insights, assets
