9a607808736e0c4184a60b2da4b9dbf1aa5a08b8
Luke Harries <lharries@users.noreply.github.com>
2022-10-24 13:52:44 +0100

    improved toolbar styling

--- frontend/src/layout/navigation/TopBar/FeedbackButton.tsx
+++ frontend/src/layout/navigation/TopBar/FeedbackButton.tsx
@@ -1,18 +1,27 @@
 import { useValues } from 'kea'
-import { LemonButton } from 'lib/components/LemonButton'
 import { preflightLogic } from 'scenes/PreflightCheck/preflightLogic'
 import { Realm } from '~/types'
+import './FeedbackButton.scss'
+
+function isOnPagesWithHighZIndex(): boolean {
+    // currently the feedback apps isn't updating properly
+    // it's occluded on the home page and the dashboard page
+    // and so the feedback button should be hidden on those pages
+    const brokenPageUrlFragments = ['/home', '/dashboard']
+    return brokenPageUrlFragments.some((fragment) => window.location.href.includes(fragment))
+}
 
 export function FeedbackButton(): JSX.Element {
     const { realm } = useValues(preflightLogic)
 
-    if (realm && realm === Realm.Cloud) {
+    if (realm && realm === Realm.Cloud && !isOnPagesWithHighZIndex()) {
         return (
-            <LemonButton data-attr="posthog-feedback-button">
-                <span className="text-default grow" data-attr="posthog-feedback-button">
-                    Feedback
-                </span>
-            </LemonButton>
+            <div
+                data-attr="posthog-feedback-button"
+                className="h-10 items-center cursor-pointer flex text-primary-alt font-semibold"
+            >
+                Feedback
+            </div>
         )
     }
     return <></>
