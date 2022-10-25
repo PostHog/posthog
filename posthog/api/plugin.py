a85dda7a1bb493567f78b8e0afa4e11bf61c1ce0
Paul Hultgren <pjhultgren5@gmail.com>
2022-10-25 16:08:27 -0700

    update repository url

--- posthog/api/plugin.py
+++ posthog/api/plugin.py
@@ -285,7 +285,7 @@ class PluginViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
 
     @action(methods=["GET"], detail=False)
     def repository(self, request: request.Request, **kwargs):
-        url = "https://raw.githubusercontent.com/PostHog/integrations-repository/main/plugins.json"
+        url = "https://raw.githubusercontent.com/PostHog/apps/main/manifest.json"
         plugins = requests.get(url)
         return Response(json.loads(plugins.text))
 
