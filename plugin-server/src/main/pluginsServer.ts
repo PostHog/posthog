cc2f424452c87724d455c5a21f10cbe670761455
Harry Waye <harry@posthog.com>
2022-10-28 11:05:15 +0100

    chore(plugins-server): use Kafka to buffer app jobs requests (#12345)
    * chore(plugins-server): use Kafka to buffer app jobs requests
    
    To remove the dependency on the Graphile Worker database on things that
    may be requesting app job runs we push the jobs to a Kafka topic.
    
    * chore: use KAFKA_JOBS instead of string literal `'jobs'`
    
    * chore: rename startJobsBufferConsumer -> startJobsConsumer
    
    * avoid checking eventId
    
    * fix lint
    
    * fix producer wrapper tests
    
    * fix retries test
    
    * handle offset sync
    
    * wip
    
    * wip
    
    * remove exports
    
    * do better
    
    * use Producer not wrapper
    
    * reset db
    
    * mock once
    
    * Add test for raising to the consumer
    
    * Update plugin-server/tests/main/ingestion-queues/run-async-handlers-event-pipeline.test.ts
    
    Co-authored-by: Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
    
    * and in the darkness bind them
    
    * fix tests
    
    * don't forget the name update!
    
    * rename DependencyError to DependencyUnavailable
    
    * separate dlq
    
    * update comment
    
    Co-authored-by: Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>

--- plugin-server/src/main/pluginsServer.ts
+++ plugin-server/src/main/pluginsServer.ts
@@ -19,6 +19,7 @@ import { makePiscina as defaultMakePiscina } from '../worker/piscina'
 import { loadPluginSchedule } from './graphile-worker/schedule'
 import { startGraphileWorker } from './graphile-worker/worker-setup'
 import { startAnonymousEventBufferConsumer } from './ingestion-queues/anonymous-event-buffer-consumer'
+import { startJobsConsumer } from './ingestion-queues/jobs-consumer'
 import { KafkaQueue } from './ingestion-queues/kafka-queue'
 import { startQueues } from './ingestion-queues/queue'
 import { createHttpServer } from './services/http-server'
@@ -82,6 +83,7 @@ export async function startPluginsServer(
     // (default 60 seconds) to allow for the person to be created in the
     // meantime.
     let bufferConsumer: Consumer | undefined
+    let jobsConsumer: Consumer | undefined
 
     let httpServer: Server | undefined // healthcheck server
     let mmdbServer: net.Server | undefined // geoip server
@@ -103,6 +105,7 @@ export async function startPluginsServer(
             pubSub?.stop(),
             hub?.graphileWorker.stop(),
             bufferConsumer?.disconnect(),
+            jobsConsumer?.disconnect(),
         ])
 
         await new Promise<void>((resolve, reject) =>
@@ -230,6 +233,15 @@ export async function startPluginsServer(
             bufferConsumer = await startAnonymousEventBufferConsumer({
                 kafka: hub.kafka,
                 producer: hub.kafkaProducer,
+                graphileWorker: hub.graphileWorker,
+                statsd: hub.statsd,
+            })
+        }
+
+        if (hub.capabilities.processPluginJobs) {
+            jobsConsumer = await startJobsConsumer({
+                kafka: hub.kafka,
+                producer: hub.kafkaProducer.producer,
                 graphileWorker: hub.graphileWorker,
                 statsd: hub.statsd,
             })
