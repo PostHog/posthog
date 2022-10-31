2f5226b2a7cbd55f23d6a80b3a5bfd5eec18a720
Ben White <ben@posthog.com>
2022-10-31 11:26:00 +0100

    Use the icon in the Alertmessage

--- frontend/src/scenes/session-recordings/SessionRecordings.tsx
+++ frontend/src/scenes/session-recordings/SessionRecordings.tsx
@@ -1,40 +1,50 @@
 import { PageHeader } from 'lib/components/PageHeader'
 import { teamLogic } from 'scenes/teamLogic'
 import { useValues } from 'kea'
-import { urls } from 'scenes/urls'
 import { SceneExport } from 'scenes/sceneTypes'
 import { sessionRecordingsListLogic } from 'scenes/session-recordings/playlist/sessionRecordingsListLogic'
 import { SessionRecordingsPlaylist } from './playlist/SessionRecordingsPlaylist'
 import { SessionRecordingsTopBar } from './filters/SessionRecordingsTopBar'
 import { AlertMessage } from 'lib/components/AlertMessage'
-import { LemonButton, Link } from '@posthog/lemon-ui'
+import { LemonButton } from '@posthog/lemon-ui'
 import { openSessionRecordingSettingsDialog } from './settings/SessionRecordingSettings'
 import { IconSettings } from 'lib/components/icons'
 
 export function SessionsRecordings(): JSX.Element {
     const { currentTeam } = useValues(teamLogic)
 
+    const recordingsDisabled = currentTeam && !currentTeam?.session_recording_opt_in
+
     return (
         <div className="space-y-4">
             <PageHeader
                 title={<div>Recordings</div>}
                 buttons={
-                    <>
-                        <LemonButton
-                            type="secondary"
-                            sideIcon={<IconSettings />}
-                            onClick={() => openSessionRecordingSettingsDialog()}
-                        >
-                            Configure
-                        </LemonButton>
-                    </>
+                    !recordingsDisabled ? (
+                        <>
+                            <LemonButton
+                                type="secondary"
+                                icon={<IconSettings />}
+                                onClick={() => openSessionRecordingSettingsDialog()}
+                            >
+                                Configure
+                            </LemonButton>
+                        </>
+                    ) : undefined
                 }
             />
-            {currentTeam && !currentTeam?.session_recording_opt_in ? (
+            {recordingsDisabled ? (
                 <div className="mb-4">
-                    <AlertMessage type="info">
-                        Session recordings are currently disabled for this project. To use this feature, please go to
-                        your <Link to={`${urls.projectSettings()}#recordings`}>project settings</Link> and enable it.
+                    <AlertMessage
+                        type="info"
+                        action={{
+                            type: 'secondary',
+                            icon: <IconSettings />,
+                            onClick: () => openSessionRecordingSettingsDialog(),
+                            children: 'Configure',
+                        }}
+                    >
+                        Session recordings are currently disabled for this project.
                     </AlertMessage>
                 </div>
             ) : null}
