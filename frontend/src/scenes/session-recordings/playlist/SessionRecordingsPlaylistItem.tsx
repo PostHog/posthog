ad67a2627d2e769692f518d276bc78b8f1d20c52
Ben White <ben@benjackwhite.co.uk>
2022-11-01 16:31:00 +0100

    fix: Fully rollout new recordings list icons (#12531)

--- frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylistItem.tsx
+++ frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylistItem.tsx
@@ -1,8 +1,5 @@
 import { SessionRecordingType } from '~/types'
 import { colonDelimitedDuration } from 'lib/utils'
-import { useValues } from 'kea'
-import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
-import { FEATURE_FLAGS } from 'lib/constants'
 import clsx from 'clsx'
 import { PropertyIcon } from 'lib/components/PropertyIcon'
 import { IconSchedule } from 'lib/components/icons'
@@ -31,9 +28,6 @@ export function SessionRecordingPlaylistItem({
     const formattedDuration = colonDelimitedDuration(recording.recording_duration)
     const durationParts = formattedDuration.split(':')
 
-    const { featureFlags } = useValues(featureFlagLogic)
-
-    const listIcons = featureFlags[FEATURE_FLAGS.RECORDING_LIST_ICONS] || 'none'
     const iconClassnames = clsx(
         'SessionRecordingsPlaylist__list-item__property-icon text-base text-muted-alt',
         !isActive && 'opacity-75'
@@ -44,10 +38,8 @@ export function SessionRecordingPlaylistItem({
             ? recordingProperties
             : recording.person?.properties || {}
 
-    const indicatorRight = listIcons === 'bottom' || listIcons === 'none' || listIcons === 'middle' || !listIcons
-
     const propertyIcons = (
-        <div className="flex flex-row flex-nowrap shrink-0 gap-1">
+        <div className="flex flex-row flex-nowrap shrink-0 gap-1 h-6">
             {!recordingPropertiesLoading ? (
                 iconPropertyKeys.map((property) => {
                     const value =
@@ -72,26 +64,9 @@ export function SessionRecordingPlaylistItem({
                     )
                 })
             ) : (
-                <LemonSkeleton className="w-16 py-1" />
+                <LemonSkeleton className="w-18 my-1" />
             )}
         </div>
-    )
-
-    const duration = (
-        <span className="flex items-center font-semibold">
-            <IconSchedule className={iconClassnames} />
-            <span>
-                <span className={clsx(durationParts[0] === '00' && 'opacity-50 font-normal')}>{durationParts[0]}:</span>
-                <span
-                    className={clsx({
-                        'opacity-50 font-normal': durationParts[0] === '00' && durationParts[1] === '00',
-                    })}
-                >
-                    {durationParts[1]}:
-                </span>
-                {durationParts[2]}
-            </span>
-        </span>
     )
 
     return (
@@ -100,23 +75,20 @@ export function SessionRecordingPlaylistItem({
             className={clsx(
                 'SessionRecordingsPlaylist__list-item',
                 'flex flex-row py-2 pr-4 pl-0 cursor-pointer relative overflow-hidden',
-                isActive && 'bg-primary-highlight font-semibold',
-                indicatorRight && 'pl-4'
+                isActive && 'bg-primary-highlight font-semibold'
             )}
             onClick={() => onClick()}
         >
-            {!indicatorRight && (
-                <div className="w-2 h-2 mx-2">
-                    {!recording.viewed ? (
-                        <Tooltip title={'Indicates the recording has not been watched yet'}>
-                            <div
-                                className="w-2 h-2 mt-2 rounded-full bg-primary-light"
-                                aria-label="unwatched-recording-label"
-                            />
-                        </Tooltip>
-                    ) : null}
-                </div>
-            )}
+            <div className="w-2 h-2 mx-2">
+                {!recording.viewed ? (
+                    <Tooltip title={'Indicates the recording has not been watched yet'}>
+                        <div
+                            className="w-2 h-2 mt-2 rounded-full bg-primary-light"
+                            aria-label="unwatched-recording-label"
+                        />
+                    </Tooltip>
+                ) : null}
+            </div>
             <div className="grow">
                 <div className="flex items-center justify-between">
                     <div className="truncate font-medium text-primary ph-no-capture">
@@ -125,19 +97,8 @@ export function SessionRecordingPlaylistItem({
 
                     <div className="flex-1" />
 
-                    {listIcons === 'top-right' && propertyIcons}
-                    {listIcons === 'bottom-right' && duration}
-                    {indicatorRight && !recording.viewed && (
-                        <Tooltip title={'Indicates the recording has not been watched yet'}>
-                            <div
-                                className="w-2 h-2 rounded-full bg-primary-light"
-                                aria-label="unwatched-recording-label"
-                            />
-                        </Tooltip>
-                    )}
+                    {propertyIcons}
                 </div>
-
-                {listIcons === 'middle' && <div>{propertyIcons}</div>}
 
                 <div className="flex items-center justify-between">
                     <TZLabel
@@ -146,9 +107,21 @@ export function SessionRecordingPlaylistItem({
                         formatDate="MMMM DD, YYYY"
                         formatTime="h:mm A"
                     />
-                    <div className="flex items-center gap-2 flex-1 justify-end">
-                        {listIcons === 'bottom' && propertyIcons}
-                        {listIcons !== 'bottom-right' ? duration : propertyIcons}
+                    <div className="flex items-center flex-1 justify-end font-semibold">
+                        <IconSchedule className={iconClassnames} />
+                        <span>
+                            <span className={clsx(durationParts[0] === '00' && 'opacity-50 font-normal')}>
+                                {durationParts[0]}:
+                            </span>
+                            <span
+                                className={clsx({
+                                    'opacity-50 font-normal': durationParts[0] === '00' && durationParts[1] === '00',
+                                })}
+                            >
+                                {durationParts[1]}:
+                            </span>
+                            {durationParts[2]}
+                        </span>
                     </div>
                 </div>
             </div>
