aa5231307ef1c8950bbcb3b6b0c6d9fa43bf28e6
Harry Waye <harry@posthog.com>
2022-11-03 13:06:12 +0000

    ci(plugin-server): add coverage for functional_tests (#12593)
    * ci(plugin-server): add coverage for functional_tests
    
    Now I've completely separated out the test runner from the running
    server (i.e. they are in completely separate processes), we can pull out
    a coverage report using `c8` (which is using node v8 built in coverage
    tooling and outputing to an istambul compatible html format).
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ plugin-server/bin/ci_functional_tests.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Script for running the functional tests in CI, outputting an Istambul coverage
+# report. When running the intetgration tests locally, it's probably better to
+# simply run `yarn functional_tests` directly which will allow e.g. to watch for
+# changes. This script is intended to handle the complexities of spinning up the
+# plugin server with the appropriate environment vars setup, and ensuring we
+# bring down the server such that c8 produces the coverage report.
+# Context is this was originally written in the GitHub Actions workflow file,
+# but it's easier to debug in a script.
+
+set -ex -o pipefail
+
+export WORKER_CONCURRENCY=1
+export CONVERSION_BUFFER_ENABLED=true
+export BUFFER_CONVERSION_SECONDS=2 # Make sure we don't have to wait for the default 60 seconds
+export KAFKA_MAX_MESSAGE_BATCH_SIZE=0
+
+./node_modules/.bin/c8 --reporter html node dist/index.js &
+SERVER_PID=$!
+
+until curl http://localhost:6738/_ready; do
+    echo ''
+    echo 'Waiting for plugin-server to be ready...'
+    sleep 1
+done
+
+set +e
+yarn functional_tests --maxConcurrency=10
+exit_code=$?
+set -e
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+exit $exit_code
