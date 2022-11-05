7fbf0810f4e146669e1bc2ea41142f1e19f097e9
Raquel M Smith <hello@raquelmsmith.com>
2022-11-04 17:53:57 -0700

    Fix ts error

--- frontend/src/scenes/ingestion/v2/Ingestion.stories.tsx
+++ frontend/src/scenes/ingestion/v2/Ingestion.stories.tsx
@@ -1,10 +1,10 @@
 import { Meta } from '@storybook/react'
 
-import { IngestionWizard } from './IngestionWizard'
+import { IngestionWizardV2 } from './IngestionWizard'
 
 export default {
     title: 'Scenes-Other/Onboarding',
     parameters: { layout: 'fullscreen', options: { showPanel: false }, viewMode: 'story' },
 } as Meta
 
-export const Ingestion = (): JSX.Element => <IngestionWizard />
+export const Ingestion = (): JSX.Element => <IngestionWizardV2 />
