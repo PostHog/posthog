5403e01851c537a41eb2e20739548847211f365b
Harry Waye <harry@posthog.com>
2022-11-08 11:29:32 +0000

    wip

--- plugin-server/src/main/pluginsServer.ts
+++ plugin-server/src/main/pluginsServer.ts
@@ -102,8 +102,8 @@ export async function startPluginsServer(
         status.info('💤', ' Shutting down gracefully...')
         lastActivityCheck && clearInterval(lastActivityCheck)
 
-        // Stop all consumers and the graphile worker, as well as the http
-        // server. Note that we close the http server before the others to
+        // HACKY: Stop all consumers and the graphile worker, as well as the
+        // http server. Note that we close the http server before the others to
         // ensure that e.g. if something goes wrong and we deadlock, then if
         // we're running in k8s, the liveness check will fail, and thus k8s will
         // kill the pod.
