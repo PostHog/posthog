cefed7563dcdc2f84d58764c700f1367c6d8614e
Ben White <ben@benjackwhite.co.uk>
2022-11-10 14:25:42 +0100

    fix: Fixed stylings for homepage items (#12533)

---
+++ frontend/src/scenes/project-homepage/ProjectHomePageCompactListItem.tsx
@@ -0,0 +1,33 @@
+import { LemonButton, LemonButtonProps } from '@posthog/lemon-ui'
+import React from 'react'
+
+export type RecentItemRowProps = Pick<LemonButtonProps, 'onClick' | 'to'> & {
+    title: string
+    subtitle: string
+    prefix?: React.ReactNode
+    suffix?: React.ReactNode
+}
+
+export function ProjectHomePageCompactListItem({
+    to,
+    onClick,
+    title,
+    subtitle,
+    prefix,
+    suffix,
+}: RecentItemRowProps): JSX.Element {
+    return (
+        <LemonButton fullWidth to={to} onClick={onClick}>
+            <div className="flex items-center justify-between h-10 overflow-hidden gap-2 flex-1">
+                {prefix ? <span className="shrink-0">{prefix}</span> : null}
+
+                <div className="truncate space-y-1 flex-1">
+                    <div className="text-primary font-semibold truncate">{title}</div>
+                    <div className="truncate text-default font-normal">{subtitle}</div>
+                </div>
+
+                {suffix ? <span className="shrink-0">{suffix}</span> : null}
+            </div>
+        </LemonButton>
+    )
+}
