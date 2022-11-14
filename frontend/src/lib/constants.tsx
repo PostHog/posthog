8891aea77e909f0e8cfe781e611bf3ab65aa686f
Paul D'Ambra <paul@posthog.com>
2022-11-14 17:53:51 +0000

    feat: text card paygate instead of hiding (#12729)
    Problem
    closes #12645
    
    Changes
    removes feature flag because it is on for everyone
    adds paygate instead of hiding text cards

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -123,7 +123,6 @@ export const FEATURE_FLAGS = {
     INGESTION_WARNINGS_ENABLED: 'ingestion-warnings-enabled', // owner: @tiina303
     HOG_BOOK: 'hog-book', // owner: @pauldambra
     EVENT_COUNT_PER_ACTOR: 'event-count-per-actor', // owner: @Twixes
-    TEXT_CARDS: 'text-cards', // owner: @pauldambra
     SESSION_RESET_ON_LOAD: 'session-reset-on-load', // owner: @benjackwhite
     FEEDBACK_BUTTON: 'feedback-button', // owner: @luke
     RECORDINGS_ON_FEATURE_FLAGS: 'recordings-on-feature-flags', // owner: @EDsCODE
