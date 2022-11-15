d67b0ec5d27191ea9caceb9ea48692f2e494db2e
Marius Andra <marius.andra@gmail.com>
2022-11-15 23:10:04 +0100

    fix(insights): fix breakdown regression

--- frontend/src/scenes/insights/EditorFilters/Breakdown.tsx
+++ frontend/src/scenes/insights/EditorFilters/Breakdown.tsx
@@ -1,16 +1,16 @@
 import { useActions, useValues } from 'kea'
-import { trendsLogic } from 'scenes/trends/trendsLogic'
 import { EditorFilterProps, InsightType } from '~/types'
 import { BreakdownFilter } from 'scenes/insights/filters/BreakdownFilter'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { FEATURE_FLAGS } from 'lib/constants'
+import { insightLogic } from 'scenes/insights/insightLogic'
 
-export function TrendsBreakdown({ filters, insightProps }: EditorFilterProps): JSX.Element {
-    const { setFilters } = useActions(trendsLogic(insightProps))
+export function Breakdown({ filters }: EditorFilterProps): JSX.Element {
+    const { setFiltersMerge } = useActions(insightLogic)
     const { featureFlags } = useValues(featureFlagLogic)
 
     const useMultiBreakdown =
         filters.insight !== InsightType.TRENDS && !!featureFlags[FEATURE_FLAGS.BREAKDOWN_BY_MULTIPLE_PROPERTIES]
 
-    return <BreakdownFilter filters={filters} setFilters={setFilters} useMultiBreakdown={useMultiBreakdown} />
+    return <BreakdownFilter filters={filters} setFilters={setFiltersMerge} useMultiBreakdown={useMultiBreakdown} />
 }
