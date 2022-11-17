8be5e5c6eec766fe5a00866e862ef1ceef909de2
Marius Andra <marius.andra@gmail.com>
2022-11-17 21:20:01 +0100

    be a bit more helpful in the error

--- frontend/src/layout/ErrorBoundary/ErrorBoundary.tsx
+++ frontend/src/layout/ErrorBoundary/ErrorBoundary.tsx
@@ -7,30 +7,44 @@ import './ErrorBoundary.scss'
 export function ErrorBoundary({ children }: { children: React.ReactElement }): JSX.Element {
     return (
         <SentryErrorBoundary
-            fallback={({ error, eventId }) => (
-                <div className="ErrorBoundary">
-                    <>
-                        <h2>An error has occurred</h2>
-                        <pre>
-                            <code>
-                                {error.name} (ID {eventId})
-                                <br />
-                                {error.message}
-                            </code>
-                        </pre>
-                        We've registered this event for analysis, but feel free to contact us directly too.
-                        <HelpButton
-                            customComponent={
-                                <LemonButton type="primary" sideIcon={<IconArrowDropDown />}>
-                                    Contact PostHog
-                                </LemonButton>
-                            }
-                            customKey="error-boundary"
-                            contactOnly
-                        />
-                    </>
-                </div>
-            )}
+            fallback={({ error, eventId }) => {
+                let lines = (error.stack || '')
+                    .split('\n')
+                    .filter((l) => l.startsWith('    at'))
+                    .map((l) => l.split(' ')[5])
+                lines = lines.slice(0, lines.indexOf('renderWithHooks'))
+
+                return (
+                    <div className="ErrorBoundary">
+                        <>
+                            <h2>An error has occurred</h2>
+                            <pre>
+                                <code>
+                                    {error.name} (ID {eventId})
+                                    <br />
+                                    {error.message}
+                                </code>
+                                {lines.length > 0 ? (
+                                    <code>
+                                        {'\n> '}
+                                        {lines.join('\n> ')}
+                                    </code>
+                                ) : null}
+                            </pre>
+                            We've registered this event for analysis, but feel free to contact us directly too.
+                            <HelpButton
+                                customComponent={
+                                    <LemonButton type="primary" sideIcon={<IconArrowDropDown />}>
+                                        Contact PostHog
+                                    </LemonButton>
+                                }
+                                customKey="error-boundary"
+                                contactOnly
+                            />
+                        </>
+                    </div>
+                )
+            }}
         >
             {children}
         </SentryErrorBoundary>
