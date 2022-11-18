b3c00f4324d3814cac384fcf572d1a49c93aed06
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2022-11-18 10:50:05 +0200

    feat(time-to-see-data): Track insight loading time in ClickHouse (#12838)
    * refactor: Move signal argument into an options object
    
    * JS_CAPTURE_TIME_TO_SEE_DATA
    
    * quote ON CLUSTER for clusters with hyphens
    
    * WIP: Capture time to see data in insights
    
    * Stricter typing
    
    * Extract dictionary source construction
    
    * Set up (unmigrated) file with schema for time_to_see_data metrics
    
    * Cleanup
    
    * Reformat file
    
    * Change dictionary settings
    
    * Revert async migration change

--- frontend/src/lib/internalMetrics.ts
+++ frontend/src/lib/internalMetrics.ts
@@ -1,3 +1,4 @@
+import posthog from 'posthog-js'
 import api from 'lib/api'
 
 interface InternalMetricsPayload {
@@ -7,8 +8,30 @@ interface InternalMetricsPayload {
     tags: Record<string, any>
 }
 
+interface TimeToSeeDataPayload {
+    query_id: string
+    status: 'failure' | 'success'
+    time_to_see_data_ms: number
+    cached: boolean
+    current_url: string
+    api_response_bytes?: number
+    api_url?: string
+    insight: string
+}
+
 export async function captureInternalMetric(payload: InternalMetricsPayload): Promise<void> {
     if (window.JS_CAPTURE_INTERNAL_METRICS) {
         await api.create('api/instance_status/capture', payload)
+    }
+}
+
+export async function captureTimeToSeeData(teamId: number, payload: TimeToSeeDataPayload): Promise<void> {
+    if (window.JS_CAPTURE_TIME_TO_SEE_DATA) {
+        const sessionDetails = posthog.sessionManager?.checkAndGetSessionAndWindowId?.(true)
+
+        await api.create(`api/projects/${teamId}/insights/timing`, {
+            session_id: sessionDetails?.sessionId ?? '',
+            ...payload,
+        })
     }
 }
