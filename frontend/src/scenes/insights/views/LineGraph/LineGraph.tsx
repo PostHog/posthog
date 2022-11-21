02675ff21d6d3db3d9ed15fe0051c09b87c5e987
Thomas Obermüller <thomas.obermueller@gmail.com>
2022-11-21 17:20:28 +0100

    implement missing data for area charts

--- frontend/src/scenes/insights/views/LineGraph/LineGraph.tsx
+++ frontend/src/scenes/insights/views/LineGraph/LineGraph.tsx
@@ -12,14 +12,14 @@ import {
     ChartType,
     Color,
     InteractionItem,
-    ScriptableLineSegmentContext,
     TickOptions,
     TooltipModel,
     TooltipOptions,
+    ScriptableLineSegmentContext,
 } from 'chart.js'
 import { CrosshairOptions } from 'chartjs-plugin-crosshair'
 import 'chartjs-adapter-dayjs-3'
-import { areObjectValuesEmpty, lightenDarkenColor } from '~/lib/utils'
+import { areObjectValuesEmpty, lightenDarkenColor, hexToRGBA } from '~/lib/utils'
 import { getBarColorFromStatus, getGraphColors, getSeriesColor } from 'lib/colors'
 import { AnnotationsOverlay } from 'lib/components/AnnotationsOverlay'
 import { GraphDataset, GraphPoint, GraphPointPayload, GraphType, InsightType, TrendsFilterType } from '~/types'
@@ -189,6 +189,35 @@ export const filterNestedDataset = (
     })
 }
 
+function createPinstripePattern(color: string): CanvasPattern {
+    const stripeWidth = 8 // 0.5rem
+    const stripeAngle = -22.5
+
+    // create the canvas and context
+    const canvas = document.createElement('canvas')
+    canvas.width = 1
+    canvas.height = stripeWidth * 2
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const ctx = canvas.getContext('2d')!
+
+    // fill the canvas with given color
+    ctx.fillStyle = color
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    // overlay half-transparent white stripe
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
+    ctx.fillRect(0, stripeWidth, 1, 2 * stripeWidth)
+
+    // create a canvas pattern and rotate it
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const pattern = ctx.createPattern(canvas, 'repeat')!
+    const xAx = Math.cos(stripeAngle)
+    const xAy = Math.sin(stripeAngle)
+    pattern.setTransform(new DOMMatrix([xAx, xAy, -xAy, xAx, 0, 0]))
+
+    return pattern
+}
+
 export function LineGraph_({
     datasets: _datasets,
     hiddenLegendKeys,
@@ -244,19 +273,22 @@ export function LineGraph_({
             ? getBarColorFromStatus(dataset.status)
             : getSeriesColor(dataset.id, isCompare)
         const hoverColor = dataset?.status ? getBarColorFromStatus(dataset.status, true) : mainColor
+        const areaBackgroundColor = hexToRGBA(mainColor, 0.5)
+        const areaIncompletePattern = createPinstripePattern(areaBackgroundColor)
+        let backgroundColor: string | undefined = undefined
+        if (isBackgroundBasedGraphType) {
+            backgroundColor = mainColor
+        } else if (isArea) {
+            backgroundColor = areaBackgroundColor
+        }
 
         // `horizontalBar` colors are set in `ActionsHorizontalBar.tsx` and overridden in spread of `dataset` below
         return {
             borderColor: mainColor,
             hoverBorderColor: isBackgroundBasedGraphType ? lightenDarkenColor(mainColor, -20) : hoverColor,
             hoverBackgroundColor: isBackgroundBasedGraphType ? lightenDarkenColor(mainColor, -20) : undefined,
-            backgroundColor: isBackgroundBasedGraphType ? mainColor : undefined,
-            fill: isArea
-                ? {
-                      target: 'origin',
-                      above: mainColor + 'a9', // d8 = 85% opacity in hex
-                  }
-                : false,
+            fill: isArea ? 'origin' : false,
+            backgroundColor,
             segment: {
                 borderDash: (ctx: ScriptableLineSegmentContext) => {
                     // If chart is line graph, show dotted lines for incomplete data
@@ -269,8 +301,19 @@ export function LineGraph_({
                     // if last date is still active show dotted line
                     return isIncomplete && isActive ? [10, 10] : undefined
                 },
+                backgroundColor: (ctx: ScriptableLineSegmentContext) => {
+                    // If chart is area graph, show pinstripe pattern for incomplete data
+                    if (!(type === GraphType.Line && isInProgress && isArea)) {
+                        return undefined
+                    }
+
+                    const isIncomplete = ctx.p1DataIndex >= dataset.data.length + incompletenessOffsetFromEnd
+                    const isActive = !dataset.compare || dataset.compare_label != 'previous'
+                    // if last date is still active show dotted line
+                    return isIncomplete && isActive ? areaIncompletePattern : undefined
+                },
             },
-            borderWidth: isBar || isArea ? 0 : 2,
+            borderWidth: isBar ? 0 : 2,
             pointRadius: 0,
             hitRadius: 0,
             order: 1,
