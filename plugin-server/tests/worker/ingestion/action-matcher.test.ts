2889e84a7ba0c79f6d019e41c16d5602cddffc04
Neil Kakkar <neilkakkar@gmail.com>
2022-11-22 13:29:57 +0000

    one more test

--- plugin-server/tests/worker/ingestion/action-matcher.test.ts
+++ plugin-server/tests/worker/ingestion/action-matcher.test.ts
@@ -941,7 +941,7 @@ describe('ActionMatcher', () => {
             expect(await actionMatcher.match(eventExampleOk1)).toEqual([actionDefinition])
         })
 
-        it('properly handles null string coercion', async () => {
+        it('properly handles is_not null string coercion', async () => {
             const actionDefinitionOpIsSet: Action = await createTestAction([
                 {
                     properties: [
@@ -972,6 +972,36 @@ describe('ActionMatcher', () => {
             expect(await actionMatcher.match(eventFigNumber)).toEqual([])
             expect(await actionMatcher.match(eventFooTrue)).toEqual([actionDefinitionOpIsSet])
             expect(await actionMatcher.match(eventFooNull)).toEqual([])
+        })
+
+        it('properly handles exact null string coercion', async () => {
+            const actionDefinitionOpIsSet: Action = await createTestAction([
+                {
+                    properties: [{ type: 'event', key: 'foo', value: ['null'] }],
+                },
+            ])
+
+            const eventFooBar = createTestEvent({ properties: { foo: 'bar' } })
+            const eventFooBarPolPot = createTestEvent({ properties: { foo: null, pol: 'pot' } })
+            const eventFooBaR = createTestEvent({ properties: { foo: 'baR' } })
+            const eventFooBaz = createTestEvent({ properties: { foo: 'baz' } })
+            const eventFooRabarbar = createTestEvent({ properties: { foo: 'rabarbar' } })
+            const eventFooNumber = createTestEvent({ properties: { foo: 7 } })
+            const eventNoNothing = createTestEvent()
+            const eventFigNumber = createTestEvent({ properties: { fig: 999 } })
+            const eventFooTrue = createTestEvent({ properties: { foo: true } })
+            const eventFooNull = createTestEvent({ properties: { foo: null } })
+
+            expect(await actionMatcher.match(eventFooBar)).toEqual([])
+            expect(await actionMatcher.match(eventFooBarPolPot)).toEqual([actionDefinitionOpIsSet])
+            expect(await actionMatcher.match(eventFooBaR)).toEqual([])
+            expect(await actionMatcher.match(eventFooBaz)).toEqual([])
+            expect(await actionMatcher.match(eventFooRabarbar)).toEqual([])
+            expect(await actionMatcher.match(eventFooNumber)).toEqual([])
+            expect(await actionMatcher.match(eventNoNothing)).toEqual([])
+            expect(await actionMatcher.match(eventFigNumber)).toEqual([])
+            expect(await actionMatcher.match(eventFooTrue)).toEqual([])
+            expect(await actionMatcher.match(eventFooNull)).toEqual([actionDefinitionOpIsSet])
         })
     })
 
