fa94f13e6391b48a4ec8fd000a8c06f08126a858
Paul D'Ambra <paul@posthog.com>
2022-11-24 19:42:08 +0000

    wiring parts together

--- frontend/src/models/tagsModel.ts
+++ frontend/src/models/tagsModel.ts
@@ -1,20 +1,18 @@
-import { kea } from 'kea'
+import { afterMount, kea, path } from 'kea'
 import api from 'lib/api'
 
 import type { tagsModelType } from './tagsModelType'
+import { loaders } from 'kea-loaders'
 
-export const tagsModel = kea<tagsModelType>({
-    path: ['models', 'tagsModel'],
-    loaders: () => ({
+export const tagsModel = kea<tagsModelType>([
+    path(['models', 'tagsModel']),
+    loaders(() => ({
         tags: {
             __default: [] as string[],
             loadTags: async () => {
                 return (await api.tags.list()) || []
             },
         },
-    }),
-
-    events: ({ actions }) => ({
-        afterMount: () => actions.loadTags(),
-    }),
-})
+    })),
+    afterMount(({ actions }) => actions.loadTags()),
+])
