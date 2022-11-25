d45fb2c55431464207d7be2324139fd14a675394
Paul D'Ambra <paul@posthog.com>
2022-11-25 14:09:12 +0000

    feat: export dashboard as json template

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -135,6 +135,7 @@ export const FEATURE_FLAGS = {
     SIGNUP_FORM_EXPERIMENT: 'signup-form-experiment', // owner: #team-growth
     SIGNUP_PRODUCT_BENEFITS_EXPERIMENT: 'signup-product-benefits-experiment', // owner: #team-growth
     ROLE_BASED_ACCESS: 'role-based-access', // owner: #team-experiments, @liyiy
+    DASHBOARD_TEMPLATES: 'dashboard-templates', // owner @pauldambra
 }
 
 /** Which self-hosted plan's features are available with Cloud's "Standard" plan (aka card attached). */
