15f92e3680e41df0c2b2b18d55a1f64ec3f7f893
Michael Matloka <dev@twixes.com>
2022-11-29 15:09:13 +0100

    Use the PostHog app itself for E2E-testing the toolbar

--- cypress/e2e/toolbar.js
+++ cypress/e2e/toolbar.js
@@ -3,7 +3,7 @@ describe('Toolbar', () => {
         cy.get('[data-attr="menu-item-toolbar-launch"]').click()
         cy.get('[data-attr="sidebar-launch-toolbar"]').contains('Add toolbar URL').click()
         cy.location().then((loc) => {
-            cy.get('[data-attr="url-input"]').clear().type(`http://${loc.host}/demo`)
+            cy.get('[data-attr="url-input"]').clear().type(`http://${loc.host}`)
             cy.get('[data-attr="url-save"]').click()
             cy.get('[data-attr="toolbar-open"]')
                 .first()
