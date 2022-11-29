a2c27d9cfc076d3a4ce05acff9b0aaaa4d853348
Michael Matloka <dev@twixes.com>
2022-11-29 11:30:59 +0100

    Simplify call count assertion

--- cypress/e2e/projectHomepage.js
+++ cypress/e2e/projectHomepage.js
@@ -6,7 +6,7 @@ describe('Project Homepage', () => {
 
     it('Shows home dashboard on load', () => {
         cy.wait('@getDashboard').its('response.statusCode').should('eq', 200)
-        cy.verifyCallCount(`getDashboard`, 1) // Only loads once
+        cy.get(`getDashboard.all`).should('have.length', 1) // Only loads once
         cy.get('[data-attr=insight-card]').its('length').should('be.gte', 1)
     })
 })
