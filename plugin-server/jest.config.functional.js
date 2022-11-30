0869801a8ea69fc127586d84a83a873740629118
Harry Waye <harry@posthog.com>
2022-11-30 12:49:17 +0000

    chore(plugin-server): split functional tests into feature based files (#13031)
    * chore(plugin-server): split functional tests into feature based files
    
    This is intended to make it more obvious what we are testing, and to try
    and identify the major themes of the plugin-server functionality.
    
    As a by product it should make things more parallelizable for jest as
    the tests in different files will be isolated, runnable in separate
    workers.
    
    * use random api token, avoid db constraints
    
    * make tests silent
    
    * format
    
    * chore: set number of jest workers
    
    These tests should be pretty light given they just hit other APIs and
    don't do much themselves. Memory could be an issue on constrained
    environments. We shall see.

--- plugin-server/jest.config.functional.js
+++ plugin-server/jest.config.functional.js
@@ -14,4 +14,6 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['<rootDir>/functional_tests/**/*.test.ts'],
     testTimeout: 60000,
+    maxConcurrency: 10,
+    maxWorkers: 6,
 }
