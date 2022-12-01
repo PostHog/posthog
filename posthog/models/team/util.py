9d86c9039906f7ec502b5db62e7447037b8662d9
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-12-01 10:26:25 +0200

    Wipe InsightCachingStates within bulky_postgres_data

--- posthog/models/team/util.py
+++ posthog/models/team/util.py
@@ -3,16 +3,19 @@ from typing import Any, List
 
 from posthog.cache_utils import cache_for
 from posthog.models.async_migration import is_async_migration_complete
-from posthog.models.cohort import CohortPeople
-from posthog.models.person import Person, PersonDistinctId
 
 
 def delete_bulky_postgres_data(team_ids: List[int]):
     "Efficiently delete large tables for teams from postgres. Using normal CASCADE delete here can time out"
 
+    from posthog.models.cohort import CohortPeople
+    from posthog.models.insight_caching_state import InsightCachingState
+    from posthog.models.person import Person, PersonDistinctId
+
     _raw_delete(PersonDistinctId.objects.filter(team_id__in=team_ids))
     _raw_delete(CohortPeople.objects.filter(cohort__team_id__in=team_ids))
     _raw_delete(Person.objects.filter(team_id__in=team_ids))
+    _raw_delete(InsightCachingState.filter(team_id__in=team_ids))
 
 
 def _raw_delete(queryset: Any):
