e82749da61db415dd953c1bfd826aecb00d4184e
yakkomajuri <yakko.majuri@gmail.com>
2022-12-01 10:09:40 -0300

    remove print

--- posthog/clickhouse/migrations/0037_live_events.py
+++ posthog/clickhouse/migrations/0037_live_events.py
@@ -1,13 +1,11 @@
 from infi.clickhouse_orm import migrations
 
 from posthog.models.live_events.sql import (
+    DISTRIBUTED_LIVE_EVENTS_TABLE_SQL,
+    KAFKA_LIVE_EVENTS_TABLE_SQL,
     LIVE_EVENTS_DATA_TABLE_SQL,
     LIVE_EVENTS_MV_TABLE_SQL,
-    KAFKA_LIVE_EVENTS_TABLE_SQL,
-    DISTRIBUTED_LIVE_EVENTS_TABLE_SQL
 )
-
-print(LIVE_EVENTS_DATA_TABLE_SQL())
 
 operations = [
     migrations.RunSQL(LIVE_EVENTS_DATA_TABLE_SQL()),
