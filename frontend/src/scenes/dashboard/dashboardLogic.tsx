4968339c6ff44f865adad20fb4a386c62a8294df
Paul D'Ambra <paul@posthog.com>
2022-12-01 11:44:29 +0000

    chore: no need to explicitly set date when loading a dashboard (#13060)
    Problem
    When loading a dashboard we call setDates so that the correct reducer is updated and the UI correctly reflects the date range of the dashboard.
    
    If we don't do this then the dashboard always shows as custom even if the date is set.
    
    This sends us analytics as if the dashboard date range was changed. Which it wasn't. Very misleading, a horror 🙀
    
    Changes
    Listen to the loading success action in the reducer so the date range is set without sending analytics

--- frontend/src/scenes/dashboard/dashboardLogic.tsx
+++ frontend/src/scenes/dashboard/dashboardLogic.tsx
@@ -178,8 +178,6 @@ export const dashboardLogic = kea<dashboardLogicType>({
 
                         actions.setInitialLoadResponseBytes(getResponseBytes(dashboardResponse))
 
-                        actions.setDates(dashboard.filters.date_from, dashboard.filters.date_to, false)
-
                         return dashboard
                     } catch (error: any) {
                         if (error.status === 404) {
@@ -280,6 +278,11 @@ export const dashboardLogic = kea<dashboardLogicType>({
                 setProperties: (state, { properties }) => ({
                     ...state,
                     properties: properties || null,
+                }),
+                loadDashboardItemsSuccess: (state, { allItems }) => ({
+                    ...state,
+                    date_from: allItems?.filters.date_from || null,
+                    date_to: allItems?.filters.date_to || null,
                 }),
             },
         ],
