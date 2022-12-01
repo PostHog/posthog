7c62f9a382e0454512aa4e3d3ed462da6f053708
Thomas Obermüller <thomas.obermueller@gmail.com>
2022-12-01 13:38:16 +0100

    add playwright

---
+++ playwright/e2e-vrt/lemon-ui/lemon-button.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test'
+
+const storybookUrl: string = process.env.STORYBOOK_URL || 'https://storybook.posthog.net'
+
+test.describe('Lemon Button', () => {
+    test('displays the button correctly', async ({ page }) => {
+        const storyId = 'lemon-ui-lemon-button--default'
+        const storyUrl = `${storybookUrl}/iframe.html?id=${storyId}&viewMode=story`
+        await page.goto(storyUrl)
+
+        const locator = page.locator('#root')
+        await expect(locator).toHaveScreenshot()
+    })
+
+    // TODO: hover and focus state - https://www.chromatic.com/docs/hoverfocus
+})
---
+++ playwright/e2e-vrt/scenes-app/insights.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test'
+
+const storybookUrl: string = process.env.STORYBOOK_URL || 'https://storybook.posthog.net'
+
+test.describe('insights vrt', () => {
+    test('displays the trends line insight correctly', async ({ page }) => {
+        const storyId = 'scenes-app-insights--trends-line'
+        const storyUrl = `${storybookUrl}/iframe.html?id=${storyId}&viewMode=story`
+        await page.goto(storyUrl)
+
+        await expect(page).toHaveScreenshot('full.png', { fullPage: true })
+
+        const content = page.locator('.main-app-content')
+        await expect(content).toHaveScreenshot('content.png')
+    })
+
+    // TODO: hover
+})
