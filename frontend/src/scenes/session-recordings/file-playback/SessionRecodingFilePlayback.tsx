69bbcee0fd8d9c565e6cd5551d2c5894f0630abd
Ben White <ben@posthog.com>
2022-12-02 14:55:13 +0100

    Tidying

--- frontend/src/scenes/session-recordings/file-playback/SessionRecodingFilePlayback.tsx
+++ frontend/src/scenes/session-recordings/file-playback/SessionRecodingFilePlayback.tsx
@@ -35,27 +35,13 @@ export function SessionRecordingFilePlayback(): JSX.Element {
                 <Dragger
                     name="file"
                     multiple={false}
-                    // fileList={}
                     accept=".json"
                     showUploadList={false}
-                    className="dragger2"
                     beforeUpload={(file) => {
                         loadFromFile(file)
                         return false
                     }}
                 >
-                    {/* {cohort.csv ? (
-                        <>
-                            <IconUploadFile style={{ fontSize: '3rem', color: 'var(--muted-alt)' }} />
-                            <div className="ant-upload-text">{cohort.csv?.name ?? 'File chosen'}</div>
-                        </>
-                    ) : (
-                        <>
-                            <IconUploadFile style={{ fontSize: '3rem', color: 'var(--muted-alt)' }} />
-                            <div className="ant-upload-text">Drag a file here or click to browse for a file</div>
-                        </>
-                    )} */}
-
                     <div className="p-20 flex flex-col items-center justify-center space-y-2 text-muted-alt">
                         <p className="flex items-center gap-2 font-semibold">
                             <IconUploadFile className="text-xl" />
