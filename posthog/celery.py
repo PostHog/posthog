045ff2dd27c347dbf7628914de24c12297255056
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-12-05 12:57:13 +0200

    Task for scheduling cache updates

--- posthog/celery.py
+++ posthog/celery.py
@@ -1,7 +1,7 @@
 import os
 import time
 from random import randrange
-from typing import Any, Dict, List, Optional
+from typing import Optional
 from uuid import UUID
 
 from celery import Celery
@@ -102,7 +102,7 @@ def setup_periodic_tasks(sender: Celery, **kwargs):
 
     sender.add_periodic_task(
         settings.UPDATE_CACHED_DASHBOARD_ITEMS_INTERVAL_SECONDS,
-        check_cached_items.s(),
+        schedule_cache_updates_task.s(),
         name="check dashboard items",
     )
 
@@ -471,21 +471,10 @@ def calculate_cohort():
 
 
 @app.task(ignore_result=True)
-def check_cached_items():
-    from posthog.caching.update_cache import update_cached_items
+def schedule_cache_updates_task():
+    from posthog.caching.insight_cache import schedule_cache_updates
 
-    update_cached_items()
-
-
-@app.task(ignore_result=False)
-def update_cache_item_task(key: str, cache_type, payload: dict) -> List[Dict[str, Any]]:
-    """
-    Tasks used in a group (as this is) must not ignore their results
-    https://docs.celeryq.dev/en/latest/userguide/canvas.html#groups:~:text=Similarly%20to%20chords%2C%20tasks%20used%20in%20a%20group%20must%20not%20ignore%20their%20results.
-    """
-    from posthog.caching.update_cache import update_cache_item
-
-    return update_cache_item(key, cache_type, payload)
+    schedule_cache_updates()
 
 
 @app.task(ignore_result=True)
