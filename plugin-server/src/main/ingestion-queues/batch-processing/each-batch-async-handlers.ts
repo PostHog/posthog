830e13201154adf84b4880bb441dfbc4fda10e66
Harry Waye <harry@posthog.com>
2022-12-05 19:33:34 +0000

    fix import

--- plugin-server/src/main/ingestion-queues/batch-processing/each-batch-async-handlers.ts
+++ plugin-server/src/main/ingestion-queues/batch-processing/each-batch-async-handlers.ts
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/react'
+import * as Sentry from '@sentry/node'
 import { EachBatchPayload, KafkaMessage } from 'kafkajs'
 
 import { PostIngestionEvent, RawClickHouseEvent } from '../../../types'
