e64a777df89b0d473f6de254e137c248adb105bc
Thomas Obermüller <thomas.obermueller@gmail.com>
2022-12-06 14:10:05 +0100

    transparent screenshots

--- playwright/e2e-vrt/lemon-ui/lemon-button.spec.ts
+++ playwright/e2e-vrt/lemon-ui/lemon-button.spec.ts
@@ -8,8 +8,23 @@ test.describe('Lemon Button', () => {
         const storyUrl = `${storybookUrl}/iframe.html?id=${storyId}&viewMode=story`
         await page.goto(storyUrl)
 
+        await page.evaluate(() => {
+            // what?: don't expand the container
+            // why?: limits screenshots to the component's size
+            const element = document.getElementById('root')
+            if (element) {
+                element.style.display = 'inline-block'
+            }
+
+            // what?: make body transparent
+            // why?: allows taking screenshots without background
+            document.body.style.background = 'transparent'
+        })
+
+        await page.pause()
+
         const locator = page.locator('#root')
-        await expect(locator).toHaveScreenshot()
+        await expect(locator).toHaveScreenshot({ omitBackground: true })
     })
 
     // TODO: hover and focus state - https://www.chromatic.com/docs/hoverfocus
