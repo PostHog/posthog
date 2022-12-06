73270e3a8efcc2919832f1661603771f91417aa4
Thomas Obermüller <thomas.obermueller@gmail.com>
2022-12-06 15:06:32 +0100

    add component hover states

--- playwright/e2e-vrt/lemon-ui/lemon-button.spec.ts
+++ playwright/e2e-vrt/lemon-ui/lemon-button.spec.ts
@@ -8,5 +8,8 @@ test.describe('Lemon Button', () => {
         await storyPage.screenshotStoryRoot()
     })
 
-    // TODO: hover and focus state - https://www.chromatic.com/docs/hoverfocus
+    test('displays hover state correctly', async ({ storyPage }) => {
+        await storyPage.goto(toId('Lemon UI/Lemon Button', 'Default'))
+        await storyPage.screenshotStoryRoot({ pseudo: { hover: true } })
+    })
 })
