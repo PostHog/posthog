fcc3f0fbe831b6771aa7fc793a1dc36a9a361c82
Marius Andra <marius.andra@gmail.com>
2022-12-07 14:29:05 +0100

    persons scene url

--- frontend/src/scenes/persons/PersonsScene.tsx
+++ frontend/src/scenes/persons/PersonsScene.tsx
@@ -1,11 +1,11 @@
 import { SceneExport } from 'scenes/sceneTypes'
-import { useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { Query } from '~/queries/Query/Query'
-import { NodeKind } from '~/queries/schema'
 import { Persons } from 'scenes/persons/Persons'
 import { PersonPageHeader } from 'scenes/persons/PersonPageHeader'
+import { personsSceneLogic } from 'scenes/persons/personsSceneLogic'
 
 export const scene: SceneExport = {
     component: PersonsScene,
@@ -14,31 +14,13 @@ export const scene: SceneExport = {
 export function PersonsScene(): JSX.Element {
     const { featureFlags } = useValues(featureFlagLogic)
     const featureDataExploration = featureFlags[FEATURE_FLAGS.DATA_EXPLORATION_LIVE_EVENTS]
+    const { query } = useValues(personsSceneLogic)
+    const { setQuery } = useActions(personsSceneLogic)
+
     return (
         <>
             <PersonPageHeader />
-            {featureDataExploration ? (
-                <Query
-                    query={{
-                        kind: NodeKind.DataTableNode,
-                        source: { kind: NodeKind.PersonsNode },
-                        columns: [
-                            'person',
-                            'id',
-                            'created_at',
-                            'properties.$geoip_country_name',
-                            'properties.$browser',
-                        ],
-                        propertiesViaUrl: true,
-                        showSearch: true,
-                        showPropertyFilter: true,
-                        showExport: true,
-                        showReload: true,
-                    }}
-                />
-            ) : (
-                <Persons />
-            )}
+            {featureDataExploration ? <Query query={query} setQuery={setQuery} /> : <Persons />}
         </>
     )
 }
