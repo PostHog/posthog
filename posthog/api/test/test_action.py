90a25bfc2e69f3e1de9a6d9b39603c25e1974062
Harry Waye <harry@posthog.com>
2022-12-08 15:21:26 +0000

    chore: make snapshots more specific for posthog/api/test/test_action.py (#13217)
    * chore: make snapshots more specific for posthog/api/test/test_action.py
    
    Avoids picking up any queries that aren't part of the system under test.
    
    I'm not sure why I needed to change the first database query count
    however :shrug:
    
    * ensure is_cloud_cached is reset before test run
    
    * clear is_cloud

--- posthog/api/test/test_action.py
+++ posthog/api/test/test_action.py
@@ -3,13 +3,14 @@ from unittest.mock import patch
 from freezegun import freeze_time
 from rest_framework import status
 
+from posthog.cloud_utils import TEST_clear_cloud_cache
 from posthog.models import Action, ActionStep, Organization, Tag, User
 from posthog.test.base import (
     APIBaseTest,
     ClickhouseTestMixin,
     QueryMatchingTest,
     _create_event,
-    snapshot_postgres_queries,
+    snapshot_postgres_queries_context,
 )
 
 
@@ -256,23 +257,25 @@ class TestActionApi(ClickhouseTestMixin, APIBaseTest, QueryMatchingTest):
         response = self.client.get(f"/api/projects/{self.team.id}/actions/{action.id}/count").json()
         self.assertEqual(response, {"count": 1})
 
-    @snapshot_postgres_queries
+    @freeze_time("2021-12-12")
     def test_listing_actions_is_not_nplus1(self) -> None:
-        with self.assertNumQueries(6):
+        TEST_clear_cloud_cache()  # HACK: if we do not clear the cache, we get different results in CI
+
+        with self.assertNumQueries(7), snapshot_postgres_queries_context(self):
             self.client.get(f"/api/projects/{self.team.id}/actions/")
 
         Action.objects.create(
             team=self.team, name="first", created_by=User.objects.create_and_join(self.organization, "a", "")
         )
 
-        with self.assertNumQueries(7):
+        with self.assertNumQueries(7), snapshot_postgres_queries_context(self):
             self.client.get(f"/api/projects/{self.team.id}/actions/")
 
         Action.objects.create(
             team=self.team, name="second", created_by=User.objects.create_and_join(self.organization, "b", "")
         )
 
-        with self.assertNumQueries(7):
+        with self.assertNumQueries(7), snapshot_postgres_queries_context(self):
             self.client.get(f"/api/projects/{self.team.id}/actions/")
 
     def test_get_tags_on_non_ee_returns_empty_list(self):
