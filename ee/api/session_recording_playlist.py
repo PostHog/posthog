bd4ef7771fe11cb29076d5664d6361ef241b681a
Alex Kim <alexgkim205@gmail.com>
2022-12-08 17:48:16 -0500

    fix api

--- ee/api/session_recording_playlist.py
+++ ee/api/session_recording_playlist.py
@@ -283,7 +283,7 @@ class SessionRecordingPlaylistViewSet(StructuredViewSetMixin, ForbidDestroyModel
 
         # TODO: Maybe we need to save the created_at date here properly to help with filtering
         if request.method == "POST":
-            recording = SessionRecording.objects.get_or_create(session_id=session_recording_id, team=self.team)
+            recording, _ = SessionRecording.objects.get_or_create(session_id=session_recording_id, team=self.team)
             playlist_item, created = SessionRecordingPlaylistItem.objects.get_or_create(
                 playlist=playlist, session_id=session_recording_id, team=self.team, recording=recording
             )
