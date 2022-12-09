72beda4c3b1c050cd8fbac7fd0fd6a3a878cf708
Marius Andra <marius.andra@gmail.com>
2022-12-09 23:21:55 +0100

    fix a few issues

--- frontend/src/queries/nodes/DataTable/defaults.ts
+++ frontend/src/queries/nodes/DataTable/defaults.ts
@@ -1,4 +1,5 @@
 import { DataNode, DataTableColumn, DataTableNode, NodeKind } from '~/queries/schema'
+import { isEventsNode } from '~/queries/utils'
 
 export const defaultDataTableEventColumns: DataTableColumn[] = [
     'event',
@@ -8,18 +9,14 @@ export const defaultDataTableEventColumns: DataTableColumn[] = [
     'timestamp',
 ]
 
-export const defaultDataTablePersonColumns: DataTableColumn[] = [
-    'person',
-    'id',
-    'created_at',
-    'properties.$geoip_country_name',
-    'properties.$browser',
-]
+export const defaultDataTablePersonColumns: DataTableColumn[] = ['person', 'id', 'created_at', 'person.$delete']
 
 export function defaultDataTableColumns(query: DataNode): DataTableColumn[] {
     return query.kind === NodeKind.PersonsNode ? defaultDataTablePersonColumns : defaultDataTableEventColumns
 }
 
 export function defaultsForDataTable(query: DataTableNode, defaultColumns?: DataTableColumn[]): DataTableColumn[] {
-    return query.columns ?? defaultColumns ?? defaultDataTableColumns(query.source)
+    return (
+        query.columns ?? (isEventsNode(query.source) ? defaultColumns : null) ?? defaultDataTableColumns(query.source)
+    )
 }
