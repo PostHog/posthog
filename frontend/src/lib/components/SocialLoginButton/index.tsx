82b2eed2db204796c2deb91e659621e24e9ebf2b
Raquel M Smith <hello@raquelmsmith.com>
2022-12-09 10:22:54 -0800

    Sort the providers by a defined order

--- frontend/src/lib/components/SocialLoginButton/index.tsx
+++ frontend/src/lib/components/SocialLoginButton/index.tsx
@@ -79,6 +79,8 @@ export function SocialLoginButtons({
     const { preflight } = useValues(preflightLogic)
     const { featureFlags } = useValues(featureFlagLogic)
 
+    const order: string[] = ['google-oauth2', 'github', 'gitlab', 'saml']
+
     if (
         !preflight?.available_social_auth_providers ||
         !Object.values(preflight.available_social_auth_providers).filter((val) => !!val).length
@@ -94,13 +96,31 @@ export function SocialLoginButtons({
                 {title && <h3>{title}</h3>}
                 {caption && <span className="text-muted">{caption}</span>}
                 <div className="flex gap-2 justify-center flex-wrap">
-                    {Object.keys(preflight.available_social_auth_providers).map((provider) =>
-                        featureFlags[FEATURE_FLAGS.SOCIAL_AUTH_BUTTONS_EXPERIMENT] === 'test' ? (
-                            <SocialLoginLinkTestVersion key={provider} provider={provider as SSOProviders} {...props} />
-                        ) : (
-                            <SocialLoginLink key={provider} provider={provider as SSOProviders} {...props} />
+                    {Object.keys(preflight.available_social_auth_providers)
+                        .sort((a, b) =>
+                            // This is a bit more complicated than it could be in case we add a new provider but
+                            // don't update the order array, we'll make sure it goes to the back.
+
+                            // if both in the order array compare indexes
+                            // if a is not in the array return 1 (sort it to back)
+                            // if a is in the array, but b isn't return -1 (move it ahead of b)
+                            order.includes(a) && order.includes(b)
+                                ? order.indexOf(a) - order.indexOf(b)
+                                : !order.includes(a)
+                                ? 1
+                                : -1
                         )
-                    )}
+                        .map((provider) =>
+                            featureFlags[FEATURE_FLAGS.SOCIAL_AUTH_BUTTONS_EXPERIMENT] === 'test' ? (
+                                <SocialLoginLinkTestVersion
+                                    key={provider}
+                                    provider={provider as SSOProviders}
+                                    {...props}
+                                />
+                            ) : (
+                                <SocialLoginLink key={provider} provider={provider as SSOProviders} {...props} />
+                            )
+                        )}
                 </div>
             </div>
         </>
