c3b2544cb38f362b9e2240574a3e879afc356464
Paul D'Ambra <paul@posthog.com>
2022-12-13 16:19:55 +0000

    be specific, more or less

--- posthog/clickhouse/migrations/0037_performance_events.py
+++ posthog/clickhouse/migrations/0037_performance_events.py
@@ -1,6 +1,10 @@
 from infi.clickhouse_orm import migrations
 
-from posthog.performance.schema import *
+from posthog.performance.schema import (
+    KAFKA_PERFORMANCE_EVENTS_TABLE_SQL,
+    PERFORMANCE_EVENTS_TABLE_MV_SQL,
+    PERFORMANCE_EVENTS_TABLE_SQL,
+)
 
 operations = [
     migrations.RunSQL(PERFORMANCE_EVENTS_TABLE_SQL),
