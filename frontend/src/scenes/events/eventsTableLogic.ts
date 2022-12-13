b43a2c53333ac8ff67b8ef5c9d527f920fe728c6
Marius Andra <marius.andra@gmail.com>
2022-12-13 14:18:55 +0100

    put it back for the tests

--- frontend/src/scenes/events/eventsTableLogic.ts
+++ frontend/src/scenes/events/eventsTableLogic.ts
@@ -1,5 +1,5 @@
 import { kea } from 'kea'
-import { convertPropertyGroupToProperties, daysAgo, toParams } from 'lib/utils'
+import { convertPropertyGroupToProperties, toParams } from 'lib/utils'
 import { router } from 'kea-router'
 import api from 'lib/api'
 import type { eventsTableLogicType } from './eventsTableLogicType'
@@ -45,6 +45,8 @@ const formatEvents = (events: EventType[], newEvents: EventType[]): EventsTableR
     }
     return eventsFormatted
 }
+
+const daysAgo = (days: number): string => now().subtract(days, 'day').toISOString()
 
 export interface EventsTableLogicProps {
     fixedFilters?: FixedFilters
