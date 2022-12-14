876e67c1ef5ba073aeda7e566e1f979ca4d01128
Thomas Obermüller <thomas.obermueller@gmail.com>
2022-12-14 11:35:33 +0100

    add data node logic

--- frontend/src/queries/nodes/InsightViz/InsightViz.tsx
+++ frontend/src/queries/nodes/InsightViz/InsightViz.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react'
 import { InsightVizNode } from '../../schema'
+import { dataNodeLogic, DataNodeLogicProps } from '~/queries/nodes/DataNode/dataNodeLogic'
+import { useValues } from 'kea'
 
 type InsightVizProps = {
     query: InsightVizNode
     setQuery?: (node: InsightVizNode) => void
 }
 
-export function InsightViz({}: InsightVizProps): JSX.Element {
-    return <span>InsightViz</span>
+let uniqueNode = 0
+
+export function InsightViz({ query }: InsightVizProps): JSX.Element {
+    const [key] = useState(() => `InsightViz.${uniqueNode++}`)
+
+    const dataNodeLogicProps: DataNodeLogicProps = { query: query.source, key }
+    const {
+        response,
+        responseLoading,
+        // canLoadNextData,
+        // canLoadNewData,
+        // nextDataLoading,
+        // newDataLoading,
+    } = useValues(dataNodeLogic(dataNodeLogicProps))
+
+    return (
+        <div>
+            <h3>InsightViz</h3>
+            <h4>Query</h4>
+            <pre>{JSON.stringify(query, null, 2)}</pre>
+            <h4>Response</h4>
+            {responseLoading ? <span>Loading...</span> : <pre>{JSON.stringify(response, null, 2)}</pre>}
+        </div>
+    )
 }
