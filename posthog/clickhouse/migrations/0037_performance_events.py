50b3c25d408e8ec5bd529ce511af42bce1be5dc0
Paul D'Ambra <paul@posthog.com>
2022-12-16 15:06:32 +0000

    Revert "obey replication setting"
    This reverts commit afea11ea085fa2b8a917525028cd24c4e3baf640.

--- posthog/clickhouse/migrations/0037_performance_events.py
+++ posthog/clickhouse/migrations/0037_performance_events.py
@@ -1,6 +1,5 @@
 from infi.clickhouse_orm import migrations
 
-from posthog import settings
 from posthog.performance.sql import (
     DISTRIBUTED_PERFORMANCE_EVENTS_TABLE_SQL,
     KAFKA_PERFORMANCE_EVENTS_TABLE_SQL,
@@ -10,13 +9,9 @@ from posthog.performance.sql import (
 )
 
 operations = [
+    migrations.RunSQL(WRITABLE_PERFORMANCE_EVENTS_TABLE_SQL()),
+    migrations.RunSQL(DISTRIBUTED_PERFORMANCE_EVENTS_TABLE_SQL()),
     migrations.RunSQL(PERFORMANCE_EVENTS_TABLE_SQL()),
     migrations.RunSQL(KAFKA_PERFORMANCE_EVENTS_TABLE_SQL()),
     migrations.RunSQL(PERFORMANCE_EVENTS_TABLE_MV_SQL()),
 ]
-
-if settings.CLICKHOUSE_REPLICATION:
-    operations = [
-        migrations.RunSQL(WRITABLE_PERFORMANCE_EVENTS_TABLE_SQL()),
-        migrations.RunSQL(DISTRIBUTED_PERFORMANCE_EVENTS_TABLE_SQL()),
-    ] + operations
