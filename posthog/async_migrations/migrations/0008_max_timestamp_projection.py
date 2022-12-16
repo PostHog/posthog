1c56d756e30b3eb0bc5fd65d70143cf0795cc662
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-12-16 14:45:39 +0200

    Create projection at table creation time

--- posthog/async_migrations/migrations/0008_max_timestamp_projection.py
+++ posthog/async_migrations/migrations/0008_max_timestamp_projection.py
@@ -2,6 +2,7 @@ import structlog
 from django.conf import settings
 
 from posthog.async_migrations.definition import AsyncMigrationDefinition, AsyncMigrationOperationSQL
+from posthog.client import sync_execute
 from posthog.constants import AnalyticsDBMS
 from posthog.version_requirement import ServiceVersionRequirement
 
@@ -17,6 +18,17 @@ class Migration(AsyncMigrationDefinition):
     posthog_max_version = "1.45.99"
 
     service_version_requirements = [ServiceVersionRequirement(service="clickhouse", supported_version=">=22.3.0")]
+
+    def is_required(self):
+        __import__("IPython").embed()  # FIXME
+        return "fast_max_kafka_timestamp" not in self.get_table_definition()
+
+    def get_table_definition(self) -> str:
+        result = sync_execute(
+            "SELECT create_table_query FROM system.tables WHERE database = %(database)s AND name = %(name)s",
+            {"database": settings.CLICKHOUSE_DATABASE, "name": "sharded_events"},
+        )
+        return result[0][0] if len(result) > 0 else ""
 
     operations = [
         AsyncMigrationOperationSQL(
