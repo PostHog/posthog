5016f3159bc2bbd4ea0340ffa1284999565da4f9
Michael Matloka <dev@twixes.com>
2022-12-20 13:22:20 +0100

    Instrument ActivityLog visual regression testing

---
+++ playwright/e2e-vrt/components/activity-log.spec.ts
@@ -0,0 +1,20 @@
+import { toId } from '../../helpers/storybook'
+
+import { test } from '../../fixtures/storybook'
+
+test.describe('Activity Log', () => {
+    test('displays feature flag activity', async ({ storyPage }) => {
+        await storyPage.goto(toId('Components/ActivityLog', 'Feature Flag Activity'))
+        await storyPage.expectComponentScreenshot()
+    })
+
+    test('displays insight activity', async ({ storyPage }) => {
+        await storyPage.goto(toId('Components/ActivityLog', 'Insight Activity'))
+        await storyPage.expectComponentScreenshot()
+    })
+
+    test('displays persons activity', async ({ storyPage }) => {
+        await storyPage.goto(toId('Components/ActivityLog', 'Persons Activity'))
+        await storyPage.expectComponentScreenshot()
+    })
+})
