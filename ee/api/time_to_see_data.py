731c98d9d4303207b8f24666260d10484146759f
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-12-20 15:07:10 +0200

    Fetch a single sessions events

--- ee/api/time_to_see_data.py
+++ ee/api/time_to_see_data.py
@@ -3,12 +3,18 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from posthog.permissions import IsStaffUser
-from posthog.queries.query_metrics.sessions import get_sessions
+from posthog.queries.query_metrics.sessions import SessionEventsQuerySerializer, get_session_events, get_sessions
 
 
 class TimeToSeeDataViewSet(viewsets.ViewSet):
     permission_classes = [IsStaffUser]
 
-    @action(methods=["POST", "GET"], detail=False)
+    @action(methods=["POST"], detail=False)
     def sessions(self, request):
-        return Response(get_sessions())
+        return Response(get_sessions().data)
+
+    @action(methods=["POST"], detail=False)
+    def session_events(self, request):
+        query = SessionEventsQuerySerializer(data=request.data)
+        query.is_valid(raise_exception=True)
+        return Response(get_session_events(query).data)
--- posthog/queries/query_metrics/sessions.py
+++ posthog/queries/query_metrics/sessions.py
@@ -28,6 +28,15 @@ GROUP BY session_id
 ORDER BY session_end DESC
 """
 
+GET_SESSION_EVENTS = f"""
+SELECT *, {IS_FRUSTRATING_INTERACTION} AS is_frustrating
+FROM metrics_time_to_see_data
+WHERE team_id = %(team_id)s
+  AND session_id = %(session_id)s
+  AND timestamp >= %(session_start)s
+  AND timestamp <= %(session_end)s
+"""
+
 
 class SessionResponseSerializer(serializers.Serializer):
     session_id = serializers.CharField()
@@ -50,6 +59,30 @@ class SessionResponseSerializer(serializers.Serializer):
         return UserBasicSerializer(user).data
 
 
+class SessionEventsQuerySerializer(serializers.Serializer):
+    session_id = serializers.CharField()
+    team_id = serializers.IntegerField()
+    session_start = serializers.DateTimeField()
+    session_end = serializers.DateTimeField()
+
+
+class SessionEventSerializer(serializers.Serializer):
+    timestamp = serializers.DateTimeField()
+    status = serializers.CharField()
+
+    type = serializers.CharField()
+    is_primary_interaction = serializers.BooleanField()
+    api_response_bytes = serializers.IntegerField()
+    current_url = serializers.CharField()
+    api_url = serializers.CharField()
+    insight = serializers.CharField()
+    action = serializers.CharField(allow_blank=True)
+    insights_fetched = serializers.CharField()
+    insights_fetched_cached = serializers.CharField()
+
+    is_frustrating = serializers.BooleanField()
+
+
 class UserLookup:
     def __init__(self, results):
         self.user_ids = [row["user_id"] for row in results]
@@ -63,8 +96,26 @@ class UserLookup:
         return self.users.get(id)
 
 
-def get_sessions():
+def get_sessions() -> SessionResponseSerializer:
     results = query_with_columns(GET_SESSIONS)
-    serializer = SessionResponseSerializer(data=results, many=True, context={"user_lookup": UserLookup(results)})
-    serializer.is_valid(raise_exception=True)
-    return serializer.data
+    response_serializer = SessionResponseSerializer(
+        data=results, many=True, context={"user_lookup": UserLookup(results)}
+    )
+    response_serializer.is_valid(raise_exception=True)
+    return response_serializer
+
+
+def get_session_events(query: SessionEventsQuerySerializer) -> SessionEventSerializer:
+    events = query_with_columns(
+        GET_SESSION_EVENTS,
+        {
+            "team_id": query.validated_data["team_id"],
+            "session_id": query.validated_data["session_id"],
+            "session_start": query.validated_data["session_start"].strftime("%Y-%m-%d %H:%M:%S"),
+            "session_end": query.validated_data["session_end"].strftime("%Y-%m-%d %H:%M:%S"),
+        },
+    )
+
+    response_serializer = SessionEventSerializer(data=events, many=True)
+    response_serializer.is_valid(raise_exception=True)
+    return response_serializer
