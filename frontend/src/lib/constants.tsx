03af8f608e8f72846896a7340e975779c58cf803
Ben White <ben@posthog.com>
2022-12-21 13:58:03 +0100

    Fix

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -171,6 +171,7 @@ export const FEATURE_MINIMUM_PLAN: Record<AvailableFeature, LicensePlan> = {
     [AvailableFeature.RECORDINGS_PLAYLISTS]: LicensePlan.Scale,
     [AvailableFeature.ROLE_BASED_ACCESS]: LicensePlan.Enterprise,
     [AvailableFeature.RECORDINGS_FILE_EXPORT]: LicensePlan.Scale,
+    [AvailableFeature.RECORDINGS_PERFORMANCE]: LicensePlan.Scale,
 }
 
 export const ENTITY_MATCH_TYPE = 'entities'
