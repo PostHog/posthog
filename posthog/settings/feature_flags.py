92c3abb66ef31bc96c85ca620ca1636600f7ed82
Michael Matloka <dev@twixes.com>
2022-12-21 10:52:59 +0100

    chore: Remove `event-count-per-user` feature flag

--- posthog/settings/feature_flags.py
+++ posthog/settings/feature_flags.py
@@ -7,6 +7,5 @@ from posthog.settings.utils import get_list
 PERSISTED_FEATURE_FLAGS = get_list(os.getenv("PERSISTED_FEATURE_FLAGS", "")) + [
     "simplify-actions",
     "historical-exports-v2",
-    "event-count-per-actor",
     "ingestion-warnings-enabled",
 ]
