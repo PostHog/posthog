84240f9ed44bef984ca3a2f8367894be995e8070
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-12-22 16:16:46 +0200

    Create api tests for session events

--- ee/api/test/test_time_to_see_data.py
+++ ee/api/test/test_time_to_see_data.py
@@ -1,6 +1,9 @@
+import json
 from dataclasses import asdict, dataclass
 from typing import List
 from unittest import mock
+
+import pytest
 
 from posthog.client import sync_execute
 from posthog.models.query_metrics.sql import (
@@ -12,6 +15,7 @@ from posthog.models.query_metrics.sql import (
 from posthog.test.base import APIBaseTest
 
 
+@pytest.mark.usefixtures("unittest_snapshot")
 class TestTimeToSeeDataApi(APIBaseTest):
     maxDiff = None
 
@@ -81,6 +85,59 @@ class TestTimeToSeeDataApi(APIBaseTest):
             ],
         )
 
+    def test_session_events_api(self):
+        insert(
+            "metrics_time_to_see_data",
+            [
+                MetricsRow(session_id="456", timestamp="2022-10-05 12:20:30", time_to_see_data_ms=7000),
+                MetricsRow(
+                    session_id="123",
+                    timestamp="2022-10-05 10:10:30",
+                    time_to_see_data_ms=2000,
+                    primary_interaction_id="111-222-333",
+                ),
+                MetricsRow(
+                    session_id="123",
+                    timestamp="2022-10-05 10:30:25",
+                    time_to_see_data_ms=1000,
+                    is_primary_interaction=False,
+                    primary_interaction_id="111-222-333",
+                    query_id="777",
+                ),
+                MetricsRow(session_id="123", timestamp="2022-10-05 10:30:30", time_to_see_data_ms=7000),
+            ],
+        )
+
+        insert(
+            "metrics_query_log",
+            [
+                QueryLogRow(
+                    session_id="123",
+                    timestamp="2022-10-05 10:10:30",
+                    query_duration_ms=400,
+                    client_query_id="111-222-333::777",
+                ),
+                QueryLogRow(
+                    session_id="123",
+                    timestamp="2022-10-05 10:10:30",
+                    query_duration_ms=200,
+                    client_query_id="111-222-333::999",
+                ),
+            ],
+        )
+
+        response = self.client.post(
+            "/api/time_to_see_data/session_events",
+            {
+                "team_id": 2,
+                "session_start": "2022-10-05T10:10:28Z",
+                "session_end": "2022-10-05T10:30:30Z",
+                "session_id": "123",
+            },
+        ).json()
+
+        assert json.dumps(response, indent=4) == self.snapshot
+
 
 @dataclass
 class MetricsRow:
@@ -105,6 +162,41 @@ class MetricsRow:
     insights_fetched_cached: int = 0
     min_last_refresh: str = ""
     max_last_refresh: str = ""
+
+
+@dataclass
+class QueryLogRow:
+    host: str = ""
+    timestamp: str = ""
+    query_duration_ms: int = 555
+    read_rows: int = 0
+    read_bytes: int = 0
+    result_rows: int = 0
+    result_bytes: int = 0
+    memory_usage: int = 0
+    is_initial_query: int = 1
+    exception_code: int = 0
+    team_id: int = 2
+    team_events_last_month: int = 0
+    user_id: int = 123
+    session_id: str = "123"
+    kind: str = ""
+    query_type: str = ""
+    client_query_id: str = ""
+    id: str = ""
+    route_id: str = ""
+    query_time_range_days: int = 1
+    has_joins: int = 0
+    has_json_operations: int = 0
+    filter_by_type = []
+    breakdown_by = []
+    entity_math = []
+    filter: str = ""
+    ProfileEvents = {}
+    tables = []
+    columns = []
+    query: str = ""
+    log_comment = ""
 
 
 def insert(table: str, rows: List):
