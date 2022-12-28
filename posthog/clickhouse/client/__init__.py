f31705c8ce292501a830103bd6e2e9be9b2765f7
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-12-28 13:50:01 +0200

    Refactor: move remaining posthog.client code

--- posthog/clickhouse/client/__init__.py
+++ posthog/clickhouse/client/__init__.py
@@ -1,3 +1,9 @@
+from posthog.clickhouse.client.execute import query_with_columns, substitute_params, sync_execute
 from posthog.clickhouse.client.execute_async import execute_with_progress
 
-__all__ = ["execute_with_progress"]
+__all__ = [
+    "sync_execute",
+    "query_with_columns",
+    "substitute_params",
+    "execute_with_progress",
+]
