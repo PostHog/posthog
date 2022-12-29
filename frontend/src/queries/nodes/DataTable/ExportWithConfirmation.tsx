b236ac892992211955e0d3b85c4b08f5e912480a
Paul D'Ambra <paul@posthog.com>
2022-12-29 19:21:56 +0000

    tweak text

--- frontend/src/queries/nodes/DataTable/ExportWithConfirmation.tsx
+++ frontend/src/queries/nodes/DataTable/ExportWithConfirmation.tsx
@@ -23,16 +23,15 @@ export function ExportWithConfirmation({
                 <>
                     Exporting by csv is limited to {limit} {actor}.
                     <br />
+                    For larger, ad-hoc exports you can use{' '}
+                    <Link to={`https://posthog.com/docs/api/${actor}`}>the API</Link>.
                     {actor === 'events' && (
                         <>
-                            The best way to export is to use{' '}
-                            <Link to="https://posthog.com/apps">our app ecosystem</Link>.
                             <br />
+                            The best way to export events is to use{' '}
+                            <Link to="https://posthog.com/apps">our app ecosystem</Link>.
                         </>
                     )}
-                    For larger, infrequent exports you can use{' '}
-                    <Link to="https://posthog.com/docs/api/events">the API</Link>
-                    .
                     <br />
                     Do you want to export by CSV?
                 </>
