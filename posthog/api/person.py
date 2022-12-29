1a4dc0e8a37c18aba34741d631e8b9086fc6fc51
Luke Harries <lharries@users.noreply.github.com>
2022-12-29 13:46:44 +0000

    Update posthog/api/person.py
    Co-authored-by: Paul D'Ambra <paul@posthog.com>

--- posthog/api/person.py
+++ posthog/api/person.py
@@ -213,7 +213,7 @@ class PersonViewSet(PKorUUIDViewSet, StructuredViewSetMixin, viewsets.ModelViewS
             OpenApiParameter(
                 "delete_events",
                 OpenApiTypes.BOOL,
-                description="If true, a task to delete all events associated with this person will be created and queued. The task does not run immediately and instead is batched together and at 5AM UTC every Sunday",
+                description="If true, a task to delete all events associated with this person will be created and queued. The task does not run immediately and instead is batched together and at 5AM UTC every Sunday (controlled by environment variable CLEAR_CLICKHOUSE_REMOVED_DATA_SCHEDULE_CRON)",
                 default=False,
             ),
         ],
