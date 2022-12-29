1a28fba9acd83b068f6ceb476d0b84ddf38b07fd
Paul D'Ambra <paul@posthog.com>
2022-12-29 23:40:35 +0000

    selecting on click

--- frontend/src/lib/components/Performance/Traces/Trace.tsx
+++ frontend/src/lib/components/Performance/Traces/Trace.tsx
@@ -1,5 +1,6 @@
 import { useResizeObserver } from 'lib/hooks/useResizeObserver'
-import { RefCallback } from 'react'
+import { RefCallback, useState } from 'react'
+import clsx from 'clsx'
 
 export interface SpanProps {
     duration: number
@@ -7,13 +8,26 @@ export interface SpanProps {
     maxSpan: number
     durationContainerWidth: number | undefined
     widthTrackingRef?: RefCallback<HTMLElement>
+    isSelected?: boolean
 }
 
-export function Span({ start, duration, maxSpan, durationContainerWidth, widthTrackingRef }: SpanProps): JSX.Element {
+export function Span({
+    isSelected,
+    start,
+    duration,
+    maxSpan,
+    durationContainerWidth,
+    widthTrackingRef,
+}: SpanProps): JSX.Element {
     const durationWidth = (duration / maxSpan) * 100
     const startMargin = (start / maxSpan) * 100
     return (
-        <div className={'w-full border px-4 py-2 flex flex-row gap-2 justify-between'}>
+        <div
+            className={clsx(
+                'w-full border px-4 py-2 flex flex-row gap-2 justify-between cursor-pointer',
+                isSelected && 'bg-muted'
+            )}
+        >
             <div className={'whitespace-nowrap'}>Span name</div>
             <div
                 ref={widthTrackingRef}
@@ -39,6 +53,7 @@ export function Span({ start, duration, maxSpan, durationContainerWidth, widthTr
 }
 
 export function Trace(): JSX.Element {
+    const [selectedSpan, setSelectedSpan] = useState<number | null>(null)
     // duration of spans are always ordered descending
     // even if nested
     const spans = [
@@ -62,15 +77,22 @@ export function Trace(): JSX.Element {
                     ref = parentSpanRef
                 }
                 return (
-                    <Span
+                    <div
                         key={i}
-                        widthTrackingRef={ref}
-                        // don't set duration container width back onto the element that is generating it
-                        durationContainerWidth={!!ref ? undefined : parentSpanWidth}
-                        start={start}
-                        duration={duration}
-                        maxSpan={1800}
-                    />
+                        onClick={() => {
+                            setSelectedSpan(selectedSpan === i ? null : i)
+                        }}
+                    >
+                        <Span
+                            widthTrackingRef={ref}
+                            // don't set duration container width back onto the element that is generating it
+                            durationContainerWidth={!!ref ? undefined : parentSpanWidth}
+                            start={start}
+                            duration={duration}
+                            maxSpan={1800}
+                            isSelected={selectedSpan === i}
+                        />
+                    </div>
                 )
             })}
         </div>
