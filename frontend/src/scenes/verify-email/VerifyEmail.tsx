061ee302a19cdc3bef7f8d3bb4e6bbcb7fb55e6c
Raquel M Smith <hello@raquelmsmith.com>
2023-01-03 14:26:38 -0800

    Add a button to contact support

--- frontend/src/scenes/verify-email/VerifyEmail.tsx
+++ frontend/src/scenes/verify-email/VerifyEmail.tsx
@@ -1,3 +1,4 @@
+import { LemonButton } from '@posthog/lemon-ui'
 import { useValues } from 'kea'
 import { BridgePage } from 'lib/components/BridgePage/BridgePage'
 import { BuilderHog2 } from 'lib/components/hedgehogs'
@@ -23,10 +24,10 @@ export function VerifyEmail(): JSX.Element {
             </div>
             <div className="flex h-full">
                 <BridgePage view="verifyEmail" noLogo fixedWidth={false} className="VerifyEmailContent p-32">
-                    <div className="p-12 text-center flex flex-col items-center max-w-200">
+                    <div className="px-12 py-8 text-center flex flex-col items-center max-w-200">
                         <h1 className="text-xl">Welcome to PostHog!</h1>
                         <h1 className="text-3xl font-bold">Let's verify your email address.</h1>
-                        <div className="max-w-100 mb-12">
+                        <div className="max-w-80 mb-12">
                             <BuilderHog2 className="w-full h-full" />
                         </div>
                         <p>
@@ -34,6 +35,9 @@ export function VerifyEmail(): JSX.Element {
                             verify your email address. If you have not received the email in a few minutes, please check
                             your spam folder.
                         </p>
+                        <LemonButton type="secondary" className="mt-8" to={'mailto:hey@posthog.com'}>
+                            Contact Support
+                        </LemonButton>
                     </div>
                 </BridgePage>
             </div>
