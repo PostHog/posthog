f77c3039a76ed03deca9e87ef8cf747bb2c3b6e2
Raquel M Smith <hello@raquelmsmith.com>
2023-01-02 17:13:49 -0800

    stub out the verify Email page

--- frontend/src/scenes/verify-email/VerifyEmail.tsx
+++ frontend/src/scenes/verify-email/VerifyEmail.tsx
@@ -1,9 +1,42 @@
+import { useValues } from 'kea'
+import { BridgePage } from 'lib/components/BridgePage/BridgePage'
+import { BuilderHog2 } from 'lib/components/hedgehogs'
+import { HelpButton } from 'lib/components/HelpButton/HelpButton'
 import { SceneExport } from 'scenes/sceneTypes'
+import { userLogic } from 'scenes/userLogic'
+import { FriendlyLogo } from '~/toolbar/assets/FriendlyLogo'
 
 export const scene: SceneExport = {
     component: VerifyEmail,
 }
 
 export function VerifyEmail(): JSX.Element {
-    return <h1>Verify Email</h1>
+    const { user } = useValues(userLogic)
+
+    return (
+        <div className="flex h-full flex-col">
+            <div className="IngestionTopbar">
+                <FriendlyLogo style={{ fontSize: '1.125rem' }} />
+                <div className="flex">
+                    <HelpButton />
+                </div>
+            </div>
+            <div className="flex h-full">
+                <BridgePage view="verifyEmail" noLogo fixedWidth={false} className="VerifyEmailContent p-32">
+                    <div className="p-12 text-center flex flex-col items-center max-w-200">
+                        <h1 className="text-xl">Welcome to PostHog!</h1>
+                        <h1 className="text-3xl font-bold">Let's verify your email address.</h1>
+                        <div className="max-w-100 mb-12">
+                            <BuilderHog2 className="w-full h-full" />
+                        </div>
+                        <p>
+                            An email has been sent to <span className="font-bold">{user?.email}</span> with a link to
+                            verify your email address. If you have not received the email in a few minutes, please check
+                            your spam folder.
+                        </p>
+                    </div>
+                </BridgePage>
+            </div>
+        </div>
+    )
 }
