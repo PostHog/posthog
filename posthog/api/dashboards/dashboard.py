7373497d813a93baf52447eac5e530aeedb8b454
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2023-01-03 16:41:46 +0200

    Remove unneeded context update

--- posthog/api/dashboards/dashboard.py
+++ posthog/api/dashboards/dashboard.py
@@ -312,7 +312,6 @@ class DashboardSerializer(TaggedItemSerializerMixin, serializers.ModelSerializer
 
             if isinstance(tile.layouts, str):
                 tile.layouts = json.loads(tile.layouts)
-            self.context.update({"filters_hash": tile.filters_hash})
             tile_data = DashboardTileSerializer(tile, many=False, context=self.context).data
             serialized_tiles.append(tile_data)
 
