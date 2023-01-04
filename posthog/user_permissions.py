f8a2375661e51563aba54bb6a7d6ca5b059b39d2
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2023-01-04 16:55:56 +0200

    Extract method into class

--- posthog/user_permissions.py
+++ posthog/user_permissions.py
@@ -1,6 +1,9 @@
 from functools import cached_property
 from typing import Optional
-from posthog.models import User, Team, Organization, OrganizationMembership
+
+from posthog.constants import AvailableFeature
+from posthog.models import Organization, OrganizationMembership, Team, User
+
 
 class UserPermissions:
     def __init__(self, user: User, team: Team, organization: Organization):
@@ -9,7 +12,48 @@ class UserPermissions:
         self.organization = organization
 
     @cached_property
-    def team_effective_membership_level(self) -> Optional[OrganizationMembership.Level]:
-        return self.team.get_effective_membership_level(self.user.pk)
+    def team_effective_membership_level(self) -> Optional["OrganizationMembership.Level"]:
+        """Return an effective membership level.
+        None returned if the user has no explicit membership and organization access is too low for implicit membership.
+        """
+        from posthog.models.organization import OrganizationMembership
 
+        try:
+            requesting_parent_membership: OrganizationMembership = OrganizationMembership.objects.select_related(
+                "organization"
+            ).get(organization_id=self.organization.pk, user_id=self.user.pk)
+        except OrganizationMembership.DoesNotExist:
+            return None
+        return self.get_effective_membership_level_for_parent_membership(requesting_parent_membership)
 
+    def get_effective_membership_level_for_parent_membership(
+        self, requesting_parent_membership: "OrganizationMembership"
+    ) -> Optional["OrganizationMembership.Level"]:
+        if (
+            not requesting_parent_membership.organization.is_feature_available(
+                AvailableFeature.PROJECT_BASED_PERMISSIONING
+            )
+            or not self.team.access_control
+        ):
+            return requesting_parent_membership.level
+        from posthog.models.organization import OrganizationMembership
+
+        try:
+            from ee.models import ExplicitTeamMembership
+        except ImportError:
+            # Only organizations admins and above get implicit project membership
+            if requesting_parent_membership.level < OrganizationMembership.Level.ADMIN:
+                return None
+            return requesting_parent_membership.level
+        else:
+            try:
+                return (
+                    requesting_parent_membership.explicit_team_memberships.only("parent_membership", "level")
+                    .get(team=self.team)
+                    .effective_level
+                )
+            except ExplicitTeamMembership.DoesNotExist:
+                # Only organizations admins and above get implicit project membership
+                if requesting_parent_membership.level < OrganizationMembership.Level.ADMIN:
+                    return None
+                return requesting_parent_membership.level
