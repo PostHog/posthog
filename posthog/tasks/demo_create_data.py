7f61e3e5358d02e12dd1df57241a5ea331174f98
Raquel M Smith <hello@raquelmsmith.com>
2023-01-04 11:24:56 -0800

    remove unnecessary try block

--- posthog/tasks/demo_create_data.py
+++ posthog/tasks/demo_create_data.py
@@ -1,5 +1,4 @@
 from celery import shared_task
-from sentry_sdk import capture_exception
 
 from posthog.demo.matrix import manager
 from posthog.demo.products.hedgebox.matrix import HedgeboxMatrix
@@ -12,8 +11,4 @@ def create_data_for_demo_team(team_id: int, user_id: int) -> None:
     team = Team.objects.get(pk=team_id)
     user = User.objects.get(pk=user_id)
     if team and user:
-        try:
-            manager.MatrixManager(HedgeboxMatrix(), use_pre_save=True).run_on_team(team, user)
-        except Exception as e:  # TODO: Remove this after 2022-12-22, the except is just temporary for debugging
-            capture_exception(e)
-            raise e
+        manager.MatrixManager(HedgeboxMatrix(), use_pre_save=True).run_on_team(team, user)
