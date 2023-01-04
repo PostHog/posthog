99a12c2e3d567576d784614ffaea942d422446eb
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2023-01-04 21:28:32 +0200

    Test and scaffolding for dashboard methods

--- posthog/user_permissions.py
+++ posthog/user_permissions.py
@@ -1,33 +1,55 @@
 from functools import cached_property
-from typing import Optional
+from typing import Dict, Optional
 
 from posthog.constants import AvailableFeature
-from posthog.models import Organization, OrganizationMembership, Team, User
+from posthog.models import Dashboard, Organization, OrganizationMembership, Team, User
 
 
 class UserPermissions:
     def __init__(self, user: User, team: Team, organization: Organization):
-        self.user = user
-        self.team = team
-        self.organization = organization
+        self.user_instance = user
+        self.team_instance = team
+        self.organization_instance = organization
+
+        self._dashboard_permissions: Dict[int, UserDashboardPermissions] = {}
 
     @cached_property
-    def team_effective_membership_level(self) -> Optional["OrganizationMembership.Level"]:
+    def team(self) -> "UserTeamPermissions":
+        return UserTeamPermissions(self)
+
+    def dashboard(self, dashboard: Dashboard) -> "UserDashboardPermissions":
+        if dashboard.pk not in self._dashboard_permissions:
+            self._dashboard_permissions[dashboard.pk] = UserDashboardPermissions(self, dashboard)
+        return self._dashboard_permissions[dashboard.pk]
+
+    @cached_property
+    def organization_membership(self) -> Optional[OrganizationMembership]:
+        return OrganizationMembership.objects.filter(
+            organization=self.organization_instance, user=self.user_instance
+        ).first()
+
+
+class UserTeamPermissions:
+    def __init__(self, user_permissions: "UserPermissions"):
+        self.p = user_permissions
+
+    @cached_property
+    def effective_membership_level(self) -> Optional["OrganizationMembership.Level"]:
         """Return an effective membership level.
         None returned if the user has no explicit membership and organization access is too low for implicit membership.
         """
 
-        return self.team_effective_membership_level_for_parent_membership(self._organization_membership)
+        return self.effective_membership_level_for_parent_membership(self.p.organization_membership)
 
-    def team_effective_membership_level_for_parent_membership(
+    def effective_membership_level_for_parent_membership(
         self, organization_membership: Optional["OrganizationMembership"]
     ) -> Optional["OrganizationMembership.Level"]:
         if organization_membership is None:
             return None
 
         if (
-            not self.organization.is_feature_available(AvailableFeature.PROJECT_BASED_PERMISSIONING)
-            or not self.team.access_control
+            not self.p.organization_instance.is_feature_available(AvailableFeature.PROJECT_BASED_PERMISSIONING)
+            or not self.p.team_instance.access_control
         ):
             return organization_membership.level
 
@@ -41,6 +63,24 @@ class UserPermissions:
                 return None
             return organization_membership.level
 
+
+class UserDashboardPermissions:
+    def __init__(self, user_permissions: "UserPermissions", dashboard: Dashboard):
+        self.p = user_permissions
+        self.dashboard = dashboard
+
     @cached_property
-    def _organization_membership(self) -> Optional[OrganizationMembership]:
-        return OrganizationMembership.objects.filter(organization=self.organization, user=self.user).first()
+    def restriction_level(self) -> Dashboard.RestrictionLevel:
+        return self.dashboard.effective_restriction_level
+
+    @cached_property
+    def can_restrict(self) -> bool:
+        return self.dashboard.can_user_restrict(self.p.user_instance.pk)
+
+    @cached_property
+    def effective_privilege_level(self) -> Dashboard.PrivilegeLevel:
+        return self.dashboard.get_effective_privilege_level(self.p.user_instance.pk)
+
+    @cached_property
+    def can_edit(self) -> bool:
+        return self.dashboard.can_user_edit(self.p.user_instance.pk)
