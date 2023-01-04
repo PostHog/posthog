cc717a8cfc525ed152f7a61c728e7e074220925a
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2023-01-04 17:16:09 +0200

    Refactor team_effective_membership_level to require less lookups

--- posthog/test/test_user_permissions.py
+++ posthog/test/test_user_permissions.py
@@ -1,8 +1,6 @@
-
 from ee.models.explicit_team_membership import ExplicitTeamMembership
 from posthog.constants import AvailableFeature
-from posthog.models.organization import Organization, OrganizationMembership
-from posthog.models.user import User
+from posthog.models.organization import OrganizationMembership
 from posthog.test.base import BaseTest
 from posthog.user_permissions import UserPermissions
 
@@ -16,28 +14,26 @@ class TestUserPermissions(BaseTest):
         self.organization.save()
 
     def permissions(self, **kwargs):
-        options = {
-            "user": self.user,
-            "team": self.team,
-            "organization": self.team.organization,
-            **kwargs
-        }
+        options = {"user": self.user, "team": self.team, "organization": self.organization, **kwargs}
 
         return UserPermissions(**options)
 
     def test_team_effective_membership_level(self):
-        assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.MEMBER
+        with self.assertNumQueries(1):
+            assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.MEMBER
 
     def test_team_effective_membership_level_updated(self):
         self.organization_membership.level = OrganizationMembership.Level.ADMIN
         self.organization_membership.save()
 
-        assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.ADMIN
+        with self.assertNumQueries(1):
+            assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.ADMIN
 
     def test_team_effective_membership_level_does_not_belong(self):
         self.organization_membership.delete()
 
-        assert self.permissions().team_effective_membership_level is None
+        with self.assertNumQueries(1):
+            assert self.permissions().team_effective_membership_level is None
 
     def test_team_effective_membership_level_with_explicit_membership_returns_current_level(self):
         self.team.access_control = True
@@ -45,7 +41,17 @@ class TestUserPermissions(BaseTest):
         self.organization_membership.level = OrganizationMembership.Level.ADMIN
         self.organization_membership.save()
 
-        assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.ADMIN
+        with self.assertNumQueries(2):
+            assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.ADMIN
+
+    def test_team_effective_membership_level_with_member(self):
+        self.team.access_control = True
+        self.team.save()
+        self.organization_membership.level = OrganizationMembership.Level.MEMBER
+        self.organization_membership.save()
+
+        with self.assertNumQueries(2):
+            assert self.permissions().team_effective_membership_level is None
 
     def test_team_effective_membership_level_with_explicit_membership_returns_explicit_membership(self):
         self.team.access_control = True
@@ -57,5 +63,5 @@ class TestUserPermissions(BaseTest):
             team=self.team, parent_membership=self.organization_membership, level=ExplicitTeamMembership.Level.ADMIN
         )
 
-        assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.ADMIN
-
+        with self.assertNumQueries(2):
+            assert self.permissions().team_effective_membership_level == OrganizationMembership.Level.ADMIN
