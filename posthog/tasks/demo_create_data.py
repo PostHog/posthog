0c1ff714a7f4d2b82a8fe6743076dd6a07c93b7b
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-01-06 05:57:53 -0800

    fix(demo): allow regular org members to create demo proj (#13582)
    * Revert "fix(demo): revert "wait until demo data is ready before redirecting" (#13575)"
    
    This reverts commit b6a284211d1bd42278750b5b0db2d05cd58570de.
    
    * add a test for allowing members to create demo proj
    
    * allow members to create demo proj
    
    * Put it behind a feature flag again just in case

--- posthog/tasks/demo_create_data.py
+++ posthog/tasks/demo_create_data.py
@@ -1,5 +1,5 @@
 from celery import shared_task
-from sentry_sdk import capture_exception
+from django.core.cache import cache
 
 from posthog.demo.matrix import manager
 from posthog.demo.products.hedgebox.matrix import HedgeboxMatrix
@@ -8,12 +8,9 @@ from posthog.models.user import User
 
 
 @shared_task(ignore_result=True)
-def create_data_for_demo_team(team_id: int, user_id: int) -> None:
+def create_data_for_demo_team(team_id: int, user_id: int, cache_key: str) -> None:
     team = Team.objects.get(pk=team_id)
     user = User.objects.get(pk=user_id)
     if team and user:
-        try:
-            manager.MatrixManager(HedgeboxMatrix(), use_pre_save=True).run_on_team(team, user)
-        except Exception as e:  # TODO: Remove this after 2022-12-22, the except is just temporary for debugging
-            capture_exception(e)
-            raise e
+        manager.MatrixManager(HedgeboxMatrix(), use_pre_save=True).run_on_team(team, user)
+        cache.delete(cache_key)
