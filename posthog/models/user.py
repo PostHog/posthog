d71ef548d8181dbf87dcdc720a2188e7b3504c9c
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-01-09 21:06:17 -0800

    Actually add the field..

--- posthog/models/user.py
+++ posthog/models/user.py
@@ -136,6 +136,7 @@ class User(AbstractUser, UUIDClassicModel):
     toolbar_mode: models.CharField = models.CharField(
         max_length=200, null=True, blank=True, choices=TOOLBAR_CHOICES, default=TOOLBAR
     )
+    is_verified: models.BooleanField = models.BooleanField(default=False, null=True, blank=True)
     # DEPRECATED
     events_column_config: models.JSONField = models.JSONField(default=events_column_config_default)
 
