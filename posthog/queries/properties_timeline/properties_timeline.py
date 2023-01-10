096794844f1d1f237429073131ac3cc686bb7709
Michael Matloka <dev@twixes.com>
2023-01-10 16:48:03 +0100

    refactor(actors-modal): Fully qualify actors URLs `date_to` (#13584)
    * refactor(actors-modal): Fully qualify actor URLs `date_to`
    
    * Update snapshots
    
    * Fix `filter` assignment and move `offset_time_series_date_by_interval`
    
    * Update test_persons_trends.py
    
    * Make sure timezone isn't mangled
    
    * Update test_persons_trends.py
    
    * Update `TestPerson`
    
    * Update `TestTrends`
    
    * Update snapshots
    
    * Update `TestSessionRecordings` snapshots
    
    * Update snapshots
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- posthog/queries/properties_timeline/properties_timeline.py
+++ posthog/queries/properties_timeline/properties_timeline.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 from typing import Any, Dict, List, Set, TypedDict, Union, cast
 
@@ -7,7 +8,7 @@ from posthog.models.person.person import Person
 from posthog.models.property.util import extract_tables_and_properties, get_single_or_multi_property_string_expr
 from posthog.models.team.team import Team
 from posthog.queries.insight import insight_sync_execute
-from posthog.queries.trends.trends_actors import handle_date_to_with_interval_for_data_point_actors
+from posthog.queries.trends.util import offset_time_series_date_by_interval
 
 from .properties_timeline_event_query import PropertiesTimelineEventQuery
 
@@ -81,7 +82,15 @@ class PropertiesTimeline:
     def run(
         self, filter: PropertiesTimelineFilter, team: Team, actor: Union[Person, Group]
     ) -> PropertiesTimelineResult:
-        filter = handle_date_to_with_interval_for_data_point_actors(filter, team)
+        if filter._date_from is not None and filter._date_to is not None and filter._date_from == filter._date_to:
+            # Search for `offset_time_series_date_by_interval` in the `TrendsActors` class for context on this handling
+            filter = filter.with_data(
+                {
+                    "date_to": offset_time_series_date_by_interval(
+                        cast(datetime.datetime, filter.date_from), filter=filter, team=team
+                    )
+                }
+            )
 
         event_query = PropertiesTimelineEventQuery(
             filter=filter,
