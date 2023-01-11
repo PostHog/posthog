c26e13ac56b196491f7f45524f203e6f727ad249
Paul D'Ambra <paul.dambra@gmail.com>
2023-01-11 12:06:21 +0000

    Revert "feat: element stats paging (#13620)"
    This reverts commit 88f1a701f8607a2086a62a559e9694512841e2df.

--- posthog/api/element.py
+++ posthog/api/element.py
@@ -12,7 +12,6 @@ from posthog.models.element.sql import GET_ELEMENTS, GET_VALUES
 from posthog.models.property.util import parse_prop_grouped_clauses
 from posthog.permissions import ProjectMembershipNecessaryPermissions, TeamMemberAccessPermission
 from posthog.queries.query_date_range import QueryDateRange
-from posthog.utils import format_query_params_absolute_url
 
 
 class ElementSerializer(serializers.ModelSerializer):
@@ -55,22 +54,16 @@ class ElementViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
         date_to, date_to_params = query_date_range.date_to
         date_params.update(date_from_params)
         date_params.update(date_to_params)
-
         try:
-            limit = int(request.query_params.get("limit", 100))
+            limit = int(request.GET.get("limit", 100))
         except ValueError:
             raise ValidationError("Limit must be an integer")
-
-        try:
-            offset = int(request.query_params.get("offset", 0))
-        except ValueError:
-            raise ValidationError("offset must be an integer")
 
         prop_filters, prop_filter_params = parse_prop_grouped_clauses(
             team_id=self.team.pk, property_group=filter.property_groups
         )
         result = sync_execute(
-            GET_ELEMENTS.format(date_from=date_from, date_to=date_to, query=prop_filters, limit=limit, offset=offset),
+            GET_ELEMENTS.format(date_from=date_from, date_to=date_to, query=prop_filters, limit=limit),
             {
                 "team_id": self.team.pk,
                 "timezone": self.team.timezone,
@@ -78,20 +71,16 @@ class ElementViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
                 **date_params,
             },
         )
-        serialized_elements = [
-            {
-                "count": elements[1],
-                "hash": None,
-                "elements": [ElementSerializer(element).data for element in chain_to_elements(elements[0])],
-            }
-            for elements in result
-        ]
-
-        _should_paginate = len(serialized_elements) > 0
-        next_url = format_query_params_absolute_url(request, offset + limit) if _should_paginate else None
-        previous_url = format_query_params_absolute_url(request, offset - limit) if offset - limit >= 0 else None
-
-        return response.Response({"results": serialized_elements, "next": next_url, "previous": previous_url})
+        return response.Response(
+            [
+                {
+                    "count": elements[1],
+                    "hash": None,
+                    "elements": [ElementSerializer(element).data for element in chain_to_elements(elements[0])],
+                }
+                for elements in result
+            ]
+        )
 
     @action(methods=["GET"], detail=False)
     def values(self, request: request.Request, **kwargs) -> response.Response:
