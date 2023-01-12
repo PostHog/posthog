eaab2b953c9f74c69dab0cd89d93181743c86b0f
Marius Andra <marius.andra@gmail.com>
2023-01-12 12:42:21 +0100

    data node test

--- frontend/src/queries/nodes/DataNode/dataNodeLogic.test.ts
+++ frontend/src/queries/nodes/DataNode/dataNodeLogic.test.ts
@@ -76,9 +76,226 @@ describe('dataNodeLogic', () => {
             .toMatchValues({ responseLoading: false, response: partial({ results: results3 }) })
     })
 
-    it('can load new data if EventsQuery sorted by timestamp', async () => {})
+    it('can load new data if EventsQuery sorted by timestamp', async () => {
+        const commonResult = {
+            uuid: '01853a90-ba94-0000-8776-e8df5617c3ec',
+            event: 'update user properties',
+            properties: {},
+            team_id: 1,
+            distinct_id: '123',
+        }
+        const results = [
+            [
+                { ...commonResult, timestamp: '2022-12-24T17:00:41.165000Z' },
+                'update user properties',
+                '2022-12-24T17:00:41.165000Z',
+            ],
+        ]
+        ;(query as any).mockResolvedValueOnce({
+            columns: ['*', 'event', 'timestamp'],
+            results: results,
+            hasMore: true,
+        })
+
+        logic = dataNodeLogic({
+            key: testUniqueKey,
+            query: {
+                kind: NodeKind.EventsQuery,
+                select: ['*', 'event', 'timestamp'],
+            },
+        })
+        logic.mount()
+        await expectLogic(logic)
+            .toMatchValues({ responseLoading: true, canLoadNewData: false, newQuery: null, response: null })
+            .delay(0)
+        await expectLogic(logic).toMatchValues({
+            responseLoading: false,
+            canLoadNewData: true,
+            newQuery: {
+                kind: NodeKind.EventsQuery,
+                select: ['*', 'event', 'timestamp'],
+                after: '2022-12-24T17:00:41.165000Z',
+            },
+            response: partial({ results }),
+        })
+
+        // load new data
+
+        const results2 = [
+            [
+                { ...commonResult, uuid: 'new', timestamp: '2022-12-25T17:00:41.165000Z' },
+                'update user properties',
+                '2022-12-25T17:00:41.165000Z',
+            ],
+        ]
+        ;(query as any).mockResolvedValueOnce({
+            columns: ['*', 'event', 'timestamp'],
+            results: results2,
+            hasMore: true,
+        })
+
+        logic.actions.loadNewData()
+        await expectLogic(logic)
+            .toMatchValues({
+                responseLoading: true,
+                canLoadNewData: true,
+                newQuery: {
+                    kind: NodeKind.EventsQuery,
+                    select: ['*', 'event', 'timestamp'],
+                    after: '2022-12-24T17:00:41.165000Z',
+                },
+                response: partial({ results }),
+            })
+            .delay(0)
+
+        await expectLogic(logic).toMatchValues({
+            responseLoading: false,
+            canLoadNewData: true,
+            newQuery: {
+                kind: NodeKind.EventsQuery,
+                select: ['*', 'event', 'timestamp'],
+                after: '2022-12-25T17:00:41.165000Z',
+            },
+            response: partial({ results: [...results2, ...results] }),
+        })
+
+        // higlights new rows
+        expect(Array.from(logic.values.highlightedRows)).toEqual([results2[0]])
+    })
+
+    it('can not load new data if EventsQuery not sorted by timestamp', async () => {
+        logic = dataNodeLogic({
+            key: testUniqueKey,
+            query: {
+                kind: NodeKind.EventsQuery,
+                select: ['*', 'event', 'timestamp'],
+                orderBy: ['event'],
+            },
+        })
+        const results: any[][] = []
+        ;(query as any).mockResolvedValueOnce({
+            columns: ['*', 'event', 'timestamp'],
+            results,
+            hasMore: true,
+        })
+        logic.mount()
+        await expectLogic(logic)
+            .toMatchValues({ responseLoading: true, canLoadNewData: false, newQuery: null, response: null })
+            .delay(0)
+        await expectLogic(logic).toMatchValues({
+            responseLoading: false,
+            canLoadNewData: false,
+            newQuery: null,
+            response: partial({ results }),
+        })
+    })
+
+    it('can load next data for EventsQuery', async () => {
+        const commonResult = {
+            uuid: '01853a90-ba94-0000-8776-e8df5617c3ec',
+            event: 'update user properties',
+            properties: {},
+            team_id: 1,
+            distinct_id: '123',
+        }
+        const results = [
+            [
+                { ...commonResult, timestamp: '2022-12-24T17:00:41.165000Z' },
+                'update user properties',
+                '2022-12-24T17:00:41.165000Z',
+            ],
+        ]
+        ;(query as any).mockResolvedValueOnce({
+            columns: ['*', 'event', 'timestamp'],
+            results: results,
+            hasMore: true,
+        })
+
+        logic = dataNodeLogic({
+            key: testUniqueKey,
+            query: {
+                kind: NodeKind.EventsQuery,
+                select: ['*', 'event', 'timestamp'],
+            },
+        })
+        logic.mount()
+        await expectLogic(logic)
+            .toMatchValues({ responseLoading: true, canLoadNextData: false, nextQuery: null, response: null })
+            .delay(0)
+        await expectLogic(logic).toMatchValues({
+            responseLoading: false,
+            canLoadNextData: true,
+            nextQuery: {
+                kind: NodeKind.EventsQuery,
+                select: ['*', 'event', 'timestamp'],
+                before: '2022-12-24T17:00:41.165000Z',
+            },
+            response: partial({ results }),
+        })
+
+        // load next results
+
+        const results2 = [
+            [
+                { ...commonResult, uuid: 'new', timestamp: '2022-12-23T17:00:41.165000Z' },
+                'update user properties',
+                '2022-12-23T17:00:41.165000Z',
+            ],
+        ]
+        ;(query as any).mockResolvedValueOnce({
+            columns: ['*', 'event', 'timestamp'],
+            results: results2,
+            hasMore: true,
+        })
+
+        logic.actions.loadNextData()
+        await expectLogic(logic)
+            .toMatchValues({
+                responseLoading: true,
+                canLoadNextData: true,
+                nextQuery: {
+                    kind: NodeKind.EventsQuery,
+                    select: ['*', 'event', 'timestamp'],
+                    before: '2022-12-24T17:00:41.165000Z',
+                },
+                response: partial({ results }),
+            })
+            .delay(0)
+
+        await expectLogic(logic).toMatchValues({
+            responseLoading: false,
+            canLoadNextData: true,
+            nextQuery: {
+                kind: NodeKind.EventsQuery,
+                select: ['*', 'event', 'timestamp'],
+                before: '2022-12-23T17:00:41.165000Z',
+            },
+            response: partial({ results: [...results, ...results2] }),
+        })
+    })
+
+    it('can load next data for PersonsNode', async () => {
+        logic = dataNodeLogic({
+            key: testUniqueKey,
+            query: { kind: NodeKind.PersonsNode },
+        })
+        const results = [{}, {}, {}]
+        ;(query as any).mockResolvedValueOnce({ results, next: 'next url' })
+        logic.mount()
+        await expectLogic(logic)
+            .toMatchValues({ responseLoading: true, canLoadNextData: false, nextQuery: null, response: null })
+            .delay(0)
+        await expectLogic(logic).toMatchValues({
+            responseLoading: false,
+            canLoadNextData: true,
+            nextQuery: {
+                kind: NodeKind.PersonsNode,
+                limit: 100,
+                offset: 3,
+            },
+            response: partial({ results }),
+        })
+    })
+
     it('can autoload new data for EventsQuery', async () => {})
-    it('will highlight new rows for EventsQuery', async () => {})
-    it('can load next data for EventsQuery', async () => {})
-    it('can load next data for PersonsNode', async () => {})
 })
