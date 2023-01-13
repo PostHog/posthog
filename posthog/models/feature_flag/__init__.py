901ee2aefc6a809e6bc2f3ed068f6af8e8f29b1a
Neil Kakkar <neilkakkar@gmail.com>
2023-01-13 14:00:25 +0000

    refactor in a follow up

--- posthog/models/feature_flag/__init__.py
+++
@@ -1,4 +0,0 @@
-from .feature_flag import FeatureFlag
-from .flag_matching import FeatureFlagMatcher, get_all_feature_flags
-from .permissions import can_user_edit_feature_flag
-from .user_blast_radius import get_user_blast_radius
