8285d8f9a57685b22aeffba967a9a188da85de47
Neil Kakkar <neilkakkar@gmail.com>
2023-01-13 14:02:36 +0000

    move around

--- posthog/models/feature_flag.py
+++ posthog/models/feature_flag.py
@@ -253,21 +253,6 @@ class FeatureFlagHashKeyOverride(models.Model):
     hash_key: models.CharField = models.CharField(max_length=400)
 
 
-# DEPRECATED: This model is no longer used, but it's not deleted to avoid downtime
-class FeatureFlagOverride(models.Model):
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["user", "feature_flag", "team"], name="unique feature flag for a user/team combo"
-            )
-        ]
-
-    feature_flag: models.ForeignKey = models.ForeignKey("FeatureFlag", on_delete=models.CASCADE)
-    user: models.ForeignKey = models.ForeignKey("User", on_delete=models.CASCADE)
-    override_value: models.JSONField = models.JSONField()
-    team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
-
-
 __LONG_SCALE__ = float(0xFFFFFFFFFFFFFFF)
 
 
@@ -759,50 +744,6 @@ def set_feature_flag_hash_key_overrides(
         FeatureFlagHashKeyOverride.objects.bulk_create(new_overrides, ignore_conflicts=True)
 
 
-def can_user_edit_feature_flag(request, feature_flag):
-    # self hosted check for enterprise models that may not exist
-    try:
-        from ee.models.feature_flag_role_access import FeatureFlagRoleAccess
-        from ee.models.organization_resource_access import OrganizationResourceAccess
-    except:
-        return True
-    else:
-        if not request.user.organization.is_feature_available(AvailableFeature.ROLE_BASED_ACCESS):
-            return True
-        if feature_flag.created_by == request.user:
-            return True
-        if (
-            request.user.organization_memberships.get(organization=request.user.organization).level
-            >= OrganizationMembership.Level.ADMIN
-        ):
-            return True
-        all_role_memberships = request.user.role_memberships.select_related("role").all()
-        try:
-            feature_flag_resource_access = OrganizationResourceAccess.objects.get(
-                organization=request.user.organization, resource=OrganizationResourceAccess.Resources.FEATURE_FLAGS
-            )
-            if feature_flag_resource_access.access_level >= OrganizationResourceAccess.AccessLevel.CAN_ALWAYS_EDIT:
-                return True
-            org_level = feature_flag_resource_access.access_level
-        except OrganizationResourceAccess.DoesNotExist:
-            org_level = OrganizationResourceAccess.AccessLevel.CAN_ALWAYS_EDIT
-
-        role_level = max([membership.role.feature_flags_access_level for membership in all_role_memberships], default=0)
-
-        if role_level == 0:
-            final_level = org_level
-        else:
-            final_level = role_level
-        if final_level == OrganizationResourceAccess.AccessLevel.CAN_ONLY_VIEW:
-            can_edit = FeatureFlagRoleAccess.objects.filter(
-                feature_flag__id=feature_flag.pk,
-                role__id__in=[membership.role.pk for membership in all_role_memberships],
-            ).exists()
-            return can_edit
-        else:
-            return final_level == OrganizationResourceAccess.AccessLevel.CAN_ALWAYS_EDIT
-
-
 def get_user_blast_radius(team: Team, feature_flag_condition: dict, group_type_index: Optional[GroupTypeIndex] = None):
 
     from posthog.queries.person_query import PersonQuery
@@ -878,3 +819,62 @@ def get_user_blast_radius(team: Team, feature_flag_condition: dict, group_type_i
     total_users = team.persons_seen_so_far
 
     return blast_radius, total_users
+
+
+def can_user_edit_feature_flag(request, feature_flag):
+    # self hosted check for enterprise models that may not exist
+    try:
+        from ee.models.feature_flag_role_access import FeatureFlagRoleAccess
+        from ee.models.organization_resource_access import OrganizationResourceAccess
+    except:
+        return True
+    else:
+        if not request.user.organization.is_feature_available(AvailableFeature.ROLE_BASED_ACCESS):
+            return True
+        if feature_flag.created_by == request.user:
+            return True
+        if (
+            request.user.organization_memberships.get(organization=request.user.organization).level
+            >= OrganizationMembership.Level.ADMIN
+        ):
+            return True
+        all_role_memberships = request.user.role_memberships.select_related("role").all()
+        try:
+            feature_flag_resource_access = OrganizationResourceAccess.objects.get(
+                organization=request.user.organization, resource=OrganizationResourceAccess.Resources.FEATURE_FLAGS
+            )
+            if feature_flag_resource_access.access_level >= OrganizationResourceAccess.AccessLevel.CAN_ALWAYS_EDIT:
+                return True
+            org_level = feature_flag_resource_access.access_level
+        except OrganizationResourceAccess.DoesNotExist:
+            org_level = OrganizationResourceAccess.AccessLevel.CAN_ALWAYS_EDIT
+
+        role_level = max([membership.role.feature_flags_access_level for membership in all_role_memberships], default=0)
+
+        if role_level == 0:
+            final_level = org_level
+        else:
+            final_level = role_level
+        if final_level == OrganizationResourceAccess.AccessLevel.CAN_ONLY_VIEW:
+            can_edit = FeatureFlagRoleAccess.objects.filter(
+                feature_flag__id=feature_flag.pk,
+                role__id__in=[membership.role.pk for membership in all_role_memberships],
+            ).exists()
+            return can_edit
+        else:
+            return final_level == OrganizationResourceAccess.AccessLevel.CAN_ALWAYS_EDIT
+
+
+# DEPRECATED: This model is no longer used, but it's not deleted to avoid downtime
+class FeatureFlagOverride(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "feature_flag", "team"], name="unique feature flag for a user/team combo"
+            )
+        ]
+
+    feature_flag: models.ForeignKey = models.ForeignKey("FeatureFlag", on_delete=models.CASCADE)
+    user: models.ForeignKey = models.ForeignKey("User", on_delete=models.CASCADE)
+    override_value: models.JSONField = models.JSONField()
+    team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
