23655440064b79c0881cefdbc419e516e955782f
yakkomajuri <yakko.majuri@gmail.com>
2023-01-17 14:18:28 -0300

    fix(trends): disallow certain interval groupings for WAU and MAU queries

--- frontend/src/lib/components/IntervalFilter/intervals.ts
+++ frontend/src/lib/components/IntervalFilter/intervals.ts
@@ -1,20 +1,32 @@
-export const intervals = {
+export type IntervalKeyType = 'hour' | 'day' | 'week' | 'month'
+
+export type Intervals = {
+    [key in IntervalKeyType]: {
+        label: string
+        newDateFrom?: string
+        disabledReason: string | undefined
+    }
+}
+
+export const intervals: Intervals = {
     hour: {
         label: 'Hour',
         newDateFrom: 'dStart',
+        disabledReason: undefined,
     },
     day: {
         label: 'Day',
         newDateFrom: undefined,
+        disabledReason: undefined,
     },
     week: {
         label: 'Week',
         newDateFrom: '-30d',
+        disabledReason: undefined,
     },
     month: {
         label: 'Month',
         newDateFrom: '-90d',
+        disabledReason: undefined,
     },
 }
-
-export type IntervalKeyType = keyof typeof intervals
