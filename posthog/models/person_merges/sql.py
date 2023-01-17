d1d28a40202d45c7232d1d659123fe24fdb75098
Harry Waye <harry@posthog.com>
2023-01-17 11:35:32 +0000

    add some todos

--- posthog/models/person_merges/sql.py
+++ posthog/models/person_merges/sql.py
@@ -10,3 +10,9 @@ CREATE TABLE IF NOT EXISTS posthog_personmerges (
     merged_at TIMESTAMP WITH TIME ZONE NOT NULL
 ) ENGINE = MergeTree()
 """
+
+# TODO: add sort key
+# TODO: add appropriate merge strategy for allowing cycling out person_ids that
+# are no longer required.
+# TODO: add replication or distributed table for writes
+# TODO: add KafkaTables
