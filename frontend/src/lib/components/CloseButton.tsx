4df050f5ab2dc126e418c02b692b9404db05b294
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-01-18 19:11:56 +0100

    fix formatting

--- frontend/src/lib/components/CloseButton.tsx
+++ frontend/src/lib/components/CloseButton.tsx
@@ -3,7 +3,7 @@ import { CloseOutlined } from '@ant-design/icons'
 // TODO: Remove, but de-ant PropertyFilterButton and SelectGradientOverflow first
 export function CloseButton(props: Record<string, any>): JSX.Element {
     return (
-         /* eslint-disable-next-line react/forbid-dom-props */
+        /* eslint-disable-next-line react/forbid-dom-props */
         <span {...props} className={'btn-close cursor-pointer ' + (props.className ?? '')} style={{ ...props.style }}>
             <CloseOutlined />
         </span>
