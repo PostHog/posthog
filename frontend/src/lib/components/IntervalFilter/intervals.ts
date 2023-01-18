f05959e399666569b2124da564d03c950cf0d9ef
yakkomajuri <yakko.majuri@gmail.com>
2023-01-18 10:26:19 -0300

    address review

--- frontend/src/lib/components/IntervalFilter/intervals.ts
+++ frontend/src/lib/components/IntervalFilter/intervals.ts
@@ -4,7 +4,7 @@ export type Intervals = {
     [key in IntervalKeyType]: {
         label: string
         newDateFrom?: string
-        disabledReason: string | undefined
+        disabledReason?: string
     }
 }
 
@@ -12,21 +12,17 @@ export const intervals: Intervals = {
     hour: {
         label: 'Hour',
         newDateFrom: 'dStart',
-        disabledReason: undefined,
     },
     day: {
         label: 'Day',
         newDateFrom: undefined,
-        disabledReason: undefined,
     },
     week: {
         label: 'Week',
         newDateFrom: '-30d',
-        disabledReason: undefined,
     },
     month: {
         label: 'Month',
         newDateFrom: '-90d',
-        disabledReason: undefined,
     },
 }
