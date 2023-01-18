a643ebbd347d6f0bb6cb05476e0321de3e02f6de
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-01-18 14:06:26 +0100

    fix: Use BigAutoField to avoid issues with int32 limit

--- posthog/migrations/0291_create_person_override_model.py
+++ posthog/migrations/0291_create_person_override_model.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="PersonOverride",
             fields=[
-                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
                 ("old_person_id", models.UUIDField(db_index=True)),
                 ("override_person_id", models.UUIDField(db_index=True)),
                 ("created_at", models.DateTimeField(auto_now_add=True)),
