55ff800e157afe25757a5b9fe1b42988f79b3a14
yakkomajuri <yakko.majuri@gmail.com>
2023-01-19 14:08:50 -0300

    refactor(query-engine): yeet substitute_params

--- posthog/queries/person_distinct_id_query.py
+++ posthog/queries/person_distinct_id_query.py
@@ -2,6 +2,8 @@ from posthog.models.person.sql import GET_TEAM_PERSON_DISTINCT_IDS
 
 
 def get_team_distinct_ids_query(team_id: int) -> str:
-    from posthog.client import substitute_params
 
-    return substitute_params(GET_TEAM_PERSON_DISTINCT_IDS, {"team_id": team_id})
+    # make sure team_id is actually an int so we can safely interpolate into the query
+    assert type(team_id) == int
+
+    return GET_TEAM_PERSON_DISTINCT_IDS % {"team_id": team_id}
