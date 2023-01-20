2ed411ef5c74ddc2ca53a6a0d6bef27e42314ca4
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-01-20 15:28:40 +0100

    clean up path clean filters

--- frontend/src/lib/components/PathCleanFilters/PathCleanFilters.tsx
+++ frontend/src/lib/components/PathCleanFilters/PathCleanFilters.tsx
@@ -1,28 +1,21 @@
 import '../../../scenes/actions/Actions.scss'
-import { TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
 import { FilterRow } from '../PropertyFilters/components/FilterRow'
 import { PathRegexPopup } from './PathCleanFilter'
 import { PathCleaningFilter } from '~/types'
 
-interface PropertyFiltersProps {
-    endpoint?: string | null
+interface PathCleanFilterProps {
+    pageKey: string
+    pathCleaningFilters: PathCleaningFilter[]
     onChange: (newItem: PathCleaningFilter) => void
     onRemove: (index: number) => void
-    pathCleaningFilters: PathCleaningFilter[]
-    pageKey: string
-    showConditionBadge?: boolean
-    disablePopover?: boolean
-    taxonomicGroupTypes?: TaxonomicFilterGroupType[]
 }
 
 export function PathCleanFilters({
     pageKey,
+    pathCleaningFilters,
     onChange,
     onRemove,
-    pathCleaningFilters,
-    showConditionBadge = false,
-    disablePopover = false, // use bare PropertyFilter without popover
-}: PropertyFiltersProps): JSX.Element {
+}: PathCleanFilterProps): JSX.Element {
     return (
         <div className="flex items-center gap-2 flex-wrap">
             {pathCleaningFilters.length > 0 &&
@@ -35,8 +28,6 @@ export function PathCleanFilters({
                             totalCount={pathCleaningFilters.length - 1} // empty state
                             filters={pathCleaningFilters}
                             pageKey={pageKey}
-                            showConditionBadge={showConditionBadge}
-                            disablePopover={disablePopover}
                             label="Add rule"
                             onRemove={onRemove}
                             filterComponent={(onComplete) => {
