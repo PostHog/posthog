f33bde9f3641019cc163099c63dc1fde807284db
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2023-01-20 08:09:45 -0300

    trigger new test run

--- posthog/queries/person_distinct_id_query.py
+++ posthog/queries/person_distinct_id_query.py
@@ -3,7 +3,7 @@ from posthog.models.person.sql import GET_TEAM_PERSON_DISTINCT_IDS
 
 def get_team_distinct_ids_query(team_id: int) -> str:
 
-    # make sure team_id is actually an int so we can safely interpolate into the query
+    # ensure team_id is actually an int so we can safely interpolate into the query
     assert type(team_id) == int
 
     return GET_TEAM_PERSON_DISTINCT_IDS % {"team_id": team_id}
