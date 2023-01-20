30fa240333c17959903ee118ec0ca436431e0154
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2023-01-20 11:25:58 -0300

    Update posthog/queries/person_distinct_id_query.py
    Co-authored-by: Karl-Aksel Puulmann <macobo@users.noreply.github.com>

--- posthog/queries/person_distinct_id_query.py
+++ posthog/queries/person_distinct_id_query.py
@@ -4,6 +4,6 @@ from posthog.models.person.sql import GET_TEAM_PERSON_DISTINCT_IDS
 def get_team_distinct_ids_query(team_id: int) -> str:
 
     # ensure team_id is actually an int so we can safely interpolate into the query
-    assert type(team_id) == int
+    assert isinstance(team_id, int)
 
     return GET_TEAM_PERSON_DISTINCT_IDS % {"team_id": team_id}
