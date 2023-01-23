b408ffe29070d67b93f821c5a1831d80bd6b4ac0
Paul D'Ambra <paul@posthog.com>
2023-01-23 13:57:46 +0000

    chore: remove majority of year in PostHog

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2171,8 +2171,6 @@ export interface AppContext {
     switched_team: TeamType['id'] | null
     /** First day of the week (0 = Sun, 1 = Mon, ...) */
     week_start: number
-
-    year_in_hog_url?: string
 }
 
 export type StoredMetricMathOperations = 'max' | 'min' | 'sum'
