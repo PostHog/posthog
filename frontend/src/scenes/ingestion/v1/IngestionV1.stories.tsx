79b20a5541fbc04dafa66a29f3b70cdd4f62d13a
Michael Matloka <dev@twixes.com>
2023-01-25 14:44:04 +0100

    Fix problem with overshadowed story

--- frontend/src/scenes/ingestion/v1/IngestionV1.stories.tsx
+++ frontend/src/scenes/ingestion/v1/IngestionV1.stories.tsx
@@ -3,8 +3,8 @@ import { Meta } from '@storybook/react'
 import { IngestionWizardV1 } from './IngestionWizard'
 
 export default {
-    title: 'Scenes-Other/Onboarding',
+    title: 'Scenes-Other/Ingestion v1',
     parameters: { layout: 'fullscreen', options: { showPanel: false }, viewMode: 'story' },
 } as Meta
 
-export const IngestionV1 = (): JSX.Element => <IngestionWizardV1 />
+export const Ingestion = (): JSX.Element => <IngestionWizardV1 />
