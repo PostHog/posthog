bf127f18e67d46fa6cea2764a56c1d5a2fc4ce3f
Michael Matloka <dev@twixes.com>
2023-01-26 19:47:00 +0100

    Don't snapshoot "Animation"

--- frontend/src/lib/components/Animation/Animation.stories.tsx
+++ frontend/src/lib/components/Animation/Animation.stories.tsx
@@ -11,6 +11,7 @@ export default {
                     'Animations are [LottieFiles.com](https://lottiefiles.com/) animations that we load asynchronously.',
             },
         },
+        chromatic: { disableSnapshot: true },
     },
     argTypes: {
         size: {
