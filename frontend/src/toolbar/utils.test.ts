fdf0b32ed871e6132fa6329e9dc0685fba9a38b1
Paul D'Ambra <paul@posthog.com>
2023-01-26 15:09:17 +0000

    update tests for finder library

--- frontend/src/toolbar/utils.test.ts
+++ frontend/src/toolbar/utils.test.ts
@@ -1,15 +1,16 @@
 import { denyAllAttributesExceptAllowlist } from './utils'
 
 describe('matching data attributes', () => {
+    // the finder dependency sends just the name of the attribute in an attribute matcher
+    // so we don't need to handle matching non-attribute values like #an-id or .a-class
     const testCases = [
-        { selector: '[data-attr="something"]', dataAttributes: ['data-attr'], expected: true },
-        { selector: '[data-attr="something"]', dataAttributes: ['data-*'], expected: true },
-        { selector: '[data-not-me="something"]', dataAttributes: ['data-attr'], expected: false },
-        { selector: '[data-attr="something"]', dataAttributes: ['data-not-me'], expected: false },
-        { selector: '[href="something"]', dataAttributes: ['data-not-me'], expected: false },
-        { selector: '[data-attr="something"]', dataAttributes: [], expected: false },
-        { selector: '#something', dataAttributes: ['data-attr'], expected: false },
-        { selector: '.something', dataAttributes: ['data-attr'], expected: false },
+        { selector: 'data-attr', dataAttributes: ['data-attr'], expected: true },
+        { selector: 'data-attr', dataAttributes: ['data-*'], expected: true },
+        { selector: 'data-not-me', dataAttributes: ['data-attr'], expected: false },
+        { selector: 'data-attr', dataAttributes: ['data-not-me'], expected: false },
+        { selector: 'href', dataAttributes: ['data-not-me'], expected: false },
+        { selector: 'href', dataAttributes: [], expected: false },
+        { selector: 'data-attr', dataAttributes: [], expected: false },
     ]
     testCases.forEach(({ selector, dataAttributes, expected }) => {
         it(`should ${expected ? '' : 'not '}match ${selector} with ${JSON.stringify(dataAttributes)}`, () => {
