dd72cbe22660c9136d31146a73abef29dc8a6cdf
Marius Andra <marius.andra@gmail.com>
2023-01-26 11:32:07 +0100

    refactor

--- posthog/api/query.py
+++ posthog/api/query.py
@@ -1,26 +1,56 @@
 import json
 
 from django.http import HttpResponse, JsonResponse
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from warlock import model_factory
 
+from posthog.api.documentation import extend_schema
 from posthog.api.routing import StructuredViewSetMixin
 from posthog.exceptions import RequestParsingError
 from posthog.models.filters.mixins.utils import cached_property
+from posthog.permissions import ProjectMembershipNecessaryPermissions, TeamMemberAccessPermission
+from posthog.rate_limit import PassThroughClickHouseBurstRateThrottle, PassThroughClickHouseSustainedRateThrottle
 
 
 class QueryViewSet(StructuredViewSetMixin, viewsets.ViewSet):
+    permission_classes = [IsAuthenticated, ProjectMembershipNecessaryPermissions, TeamMemberAccessPermission]
+    throttle_classes = [PassThroughClickHouseBurstRateThrottle, PassThroughClickHouseSustainedRateThrottle]
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "query",
+                OpenApiTypes.STR,
+                description="Query node JSON object",
+            ),
+        ]
+    )
     def list(self, request: Request, **kw) -> HttpResponse:
-        query_param = self._extract_query_param(request)
-        query = self._query_model(query_param)
+        query = self._query_from_request(request)
 
         if query.kind == "EventsQuery":
             return JsonResponse({"success": "Query is valid!", "query": query})
         else:
             raise RequestParsingError("Invalid query kind: %s" % query.kind)
 
-    def _extract_query_param(self, request):
+    def _query_from_request(self, request: Request):
+        query_json = self._query_json_from_request(request)
+        return self._query_from_json(query_json)
+
+    @cached_property
+    def _json_schema(self):
+        with open("frontend/src/queries/schema.json") as f:
+            return json.load(f)
+
+    @cached_property
+    def _query_from_json(self):
+        return model_factory(self._json_schema)
+
+    def _query_json_from_request(self, request):
         if request.method == "POST":
             if request.content_type in ["", "text/plain", "application/json"]:
                 query_source = request.body
@@ -40,12 +70,3 @@ class QueryViewSet(StructuredViewSetMixin, viewsets.ViewSet):
         except (json.JSONDecodeError, UnicodeDecodeError) as error_main:
             raise RequestParsingError("Invalid JSON: %s" % (str(error_main)))
         return query
-
-    @cached_property
-    def _json_schema(self):
-        with open("frontend/src/queries/schema.json") as f:
-            return json.load(f)
-
-    @cached_property
-    def _query_model(self):
-        return model_factory(self._json_schema)
