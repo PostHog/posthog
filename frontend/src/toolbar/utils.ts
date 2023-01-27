23acf2e96b1fad6041cb278d2936e3961dea0cfa
Paul D'Ambra <paul@posthog.com>
2023-01-27 10:49:06 +0000

    be specific more or less

--- frontend/src/toolbar/utils.ts
+++ frontend/src/toolbar/utils.ts
@@ -34,7 +34,7 @@ export function elementToQuery(element: HTMLElement, dataAttributes: string[]): 
     return finder(element, {
         attr: (name) => dataAttributes.some((dataAttribute) => wildcardMatch(dataAttribute)(name)),
         className: (name) => !name.startsWith('is-'),
-        idName: (name) => !name.startsWith('root'),
+        idName: (name) => name !== 'root',
         tagName: (name) => !TAGS_TO_IGNORE.includes(name),
         seedMinLength: 5, // include several selectors e.g. prefer .project-homepage > .project-header > .project-title over .project-title
     })
