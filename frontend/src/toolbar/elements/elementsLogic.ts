1e6565cb4622db73171c7339d1acb1fb3531f677
Paul D'Ambra <paul@posthog.com>
2023-01-28 10:20:47 +0000

    slightly nicer typing

--- frontend/src/toolbar/elements/elementsLogic.ts
+++ frontend/src/toolbar/elements/elementsLogic.ts
@@ -19,7 +19,7 @@ function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
     func: F,
     waitFor: number
 ): (...args: Parameters<F>) => void {
-    let timeout: NodeJS.Timeout
+    let timeout: ReturnType<typeof setTimeout>
     return (...args: Parameters<F>): void => {
         clearTimeout(timeout)
         timeout = setTimeout(() => func(...args), waitFor)
