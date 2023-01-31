ac6994c5804dbfaf231b04af6061bb5595557a37
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-01-31 10:04:11 +0100

    remove unused disabled prop

--- frontend/src/scenes/insights/RetentionDatePicker.tsx
+++ frontend/src/scenes/insights/RetentionDatePicker.tsx
@@ -5,7 +5,7 @@ import { insightLogic } from 'scenes/insights/insightLogic'
 import { dayjs } from 'lib/dayjs'
 import { DatePicker } from 'lib/components/DatePicker'
 
-export function RetentionDatePicker({ disabled }: { disabled?: boolean }): JSX.Element {
+export function RetentionDatePicker(): JSX.Element {
     const { insightProps } = useValues(insightLogic)
     const { filters } = useValues(retentionTableLogic(insightProps))
     const { setFilters } = useActions(retentionTableLogic(insightProps))
@@ -13,22 +13,19 @@ export function RetentionDatePicker({ disabled }: { disabled?: boolean }): JSX.E
     const yearSuffix = filters.date_to && dayjs(filters.date_to).year() !== dayjs().year() ? ', YYYY' : ''
 
     return (
-        <>
-            <Tooltip title="Cohorts up to this end date">
-                <span style={{ maxWidth: 100 }} className="flex inline-flex items-center pl-2">
-                    <DatePicker
-                        showTime={filters.period === 'Hour'}
-                        use12Hours
-                        format={filters.period === 'Hour' ? `MMM D${yearSuffix}, h a` : `MMM D${yearSuffix}`}
-                        value={filters.date_to ? dayjs(filters.date_to) : undefined}
-                        onChange={(date_to) => setFilters({ date_to: date_to && dayjs(date_to).toISOString() })}
-                        allowClear
-                        placeholder="Today"
-                        className="retention-date-picker"
-                        disabled={disabled}
-                    />
-                </span>
-            </Tooltip>
-        </>
+        <Tooltip title="Cohorts up to this end date">
+            <span style={{ maxWidth: 100 }} className="flex inline-flex items-center pl-2 max-w-40">
+                <DatePicker
+                    showTime={filters.period === 'Hour'}
+                    use12Hours
+                    format={filters.period === 'Hour' ? `MMM D${yearSuffix}, h a` : `MMM D${yearSuffix}`}
+                    value={filters.date_to ? dayjs(filters.date_to) : undefined}
+                    onChange={(date_to) => setFilters({ date_to: date_to && dayjs(date_to).toISOString() })}
+                    allowClear
+                    placeholder="Today"
+                    className="retention-date-picker"
+                />
+            </span>
+        </Tooltip>
     )
 }
