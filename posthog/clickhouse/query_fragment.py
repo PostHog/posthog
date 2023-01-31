b9cb5eb4e9c0c094025fb0d9770475c8b20f10f0
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2023-01-31 11:22:58 +0200

    Add query fragment utilities

---
+++ posthog/clickhouse/query_fragment.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import Dict, Sequence, Tuple, Union
+
+ParamType = Union[str, int, float, None]
+ParamsType = Dict[str, ParamType]
+QueryFragmentLike = Union["QueryFragment", str]
+
+
+@dataclass
+class QueryFragment:
+    sql: str
+    params: ParamsType = {}
+
+    def __init__(self, sql: Union[str, "QueryFragment", Tuple[str, ParamsType]], params={}):
+        if isinstance(sql, str):
+            self.sql = sql
+            self.params = params
+        elif isinstance(sql, QueryFragment):
+            self.sql = sql.sql
+            self.params = sql.params
+        else:
+            self.sql, self.params = sql
+
+    def format(self, **fragments: QueryFragmentLike) -> "QueryFragment":
+        new_params = {**self.params}
+        for fragment in fragments.values():
+            if isinstance(fragment, QueryFragment):
+                new_params.update(fragment.params)
+
+        return QueryFragment(
+            self.sql.format(**{key: _get_sql(fragment) for key, fragment in fragments.items()}), new_params
+        )
+
+    @staticmethod
+    def join(by: str, fragments: Sequence[QueryFragmentLike]) -> "QueryFragment":
+        indexed = {key: fragment for key, fragment in enumerate(fragments)}
+        return QueryFragment(by.join("{" + key + "}" for key in indexed.keys())).format(**indexed)
+
+
+QF = QueryFragment
+
+
+def _get_sql(fragment: QueryFragmentLike) -> str:
+    if isinstance(fragment, QueryFragment):
+        return fragment.sql
+    else:
+        return fragment
