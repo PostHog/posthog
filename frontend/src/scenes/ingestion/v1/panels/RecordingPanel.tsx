dfb8075819d1fb78cba40f54564acd4ef4ab2d5e
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-02-01 10:56:13 -0800

    Update v1 panel

--- frontend/src/scenes/ingestion/v1/panels/RecordingPanel.tsx
+++ frontend/src/scenes/ingestion/v1/panels/RecordingPanel.tsx
@@ -1,29 +1,70 @@
-import { SessionRecordingSettings } from 'scenes/session-recordings/settings/SessionRecordingSettings'
 import { useActions } from 'kea'
 import { ingestionLogic } from 'scenes/ingestion/v1/ingestionLogic'
-import { LemonButton, LemonDivider, Link } from '@posthog/lemon-ui'
+import { LemonButton, LemonDivider, LemonSwitch, Link } from '@posthog/lemon-ui'
+import { teamLogic } from 'scenes/teamLogic'
+import { SupportHeroHog } from 'lib/components/hedgehogs'
+import { useState } from 'react'
 
 export function RecordingPanel(): JSX.Element {
     const { setVerify } = useActions(ingestionLogic)
+    const { updateCurrentTeam } = useActions(teamLogic)
+    const [sessionRecordingsChecked, setSessionRecordingsChecked] = useState(true)
 
     return (
         <div style={{ maxWidth: 800 }}>
-            <h1 className="font-extrabold">Setup Session Recordings</h1>
-            <p className="prompt-text">
-                Session recordings allow you to see recordings of how your users are really using your product. It
-                includes powerful features like error tracking, filtering, and analytics to <b>diagnose UI issues</b>,{' '}
-                <b>improve support</b>, and <b>generally get inspired</b>.
-            </p>
-            <p>
-                No further configuration is required to immediately start capturing recordings. Learn more about
-                specific recordings settings in our{' '}
-                <Link to="https://posthog.com/manual/recordings#using-session-recording" target="_blank">
-                    docs
-                </Link>
-                .
-            </p>
+            <div className="flex justify-between items-center -mt-4">
+                <div>
+                    <h1 className="font-extrabold pt-4">Enable your product superpowers</h1>
+                    <p className="m-0">
+                        Collecting events from your app is just the first step toward building great products. PostHog
+                        gives you other superpowers, too, like recording user sessions and automagically capturing
+                        frontend interactions.
+                    </p>
+                </div>
+                <div className="ml-8">
+                    <SupportHeroHog className="w-full h-full" />
+                </div>
+            </div>
+            <div className="my-8">
+                <LemonSwitch
+                    data-attr="opt-in-session-recording-switch"
+                    onChange={(checked) => {
+                        setSessionRecordingsChecked(checked)
+                        updateCurrentTeam({
+                            session_recording_opt_in: checked,
+                            capture_console_log_opt_in: checked,
+                            capture_performance_opt_in: checked,
+                        })
+                    }}
+                    label="Record user sessions"
+                    fullWidth={true}
+                    labelClassName={'text-base font-semibold'}
+                    checked={sessionRecordingsChecked}
+                />
+                <p className="prompt-text ml-0">
+                    See recordings of how your users are really using your product with powerful features like error
+                    tracking, filtering, and analytics.{' '}
+                    <Link to={'https://posthog.com/manual/recordings'} target="blank">
+                        Learn more
+                    </Link>{' '}
+                    about Session recordings.
+                </p>
+            </div>
+            <div>
+                <div className="flex justify-between w-full">
+                    <p className="text-base font-semibold m-0">Autocapture frontend interactions</p>
+                    <LemonSwitch data-attr="opt-in-autocapture-switch" checked={true} disabled={true} />
+                </div>
+                <p className="prompt-text ml-0">
+                    If you use our JavaScript or React Native libraries, we'll automagically capture frontend
+                    interactions like pageviews, clicks, and more.{' '}
+                    <Link to={'https://posthog.com/docs/integrate/client/js#autocapture'} target="blank">
+                        Fine-tune what you capture
+                    </Link>{' '}
+                    directly in your code snippet.
+                </p>
+            </div>
 
-            <SessionRecordingSettings inModal />
             <LemonDivider thick dashed className="my-6" />
             <div>
                 <LemonButton
@@ -32,7 +73,15 @@ export function RecordingPanel(): JSX.Element {
                     fullWidth
                     center
                     className="mb-2"
-                    onClick={() => setVerify(true)}
+                    onClick={() => {
+                        // If they haven't changed the default value, we need to update the team with that value
+                        updateCurrentTeam({
+                            session_recording_opt_in: sessionRecordingsChecked,
+                            capture_console_log_opt_in: sessionRecordingsChecked,
+                            capture_performance_opt_in: sessionRecordingsChecked,
+                        })
+                        setVerify(true)
+                    }}
                 >
                     Continue
                 </LemonButton>
