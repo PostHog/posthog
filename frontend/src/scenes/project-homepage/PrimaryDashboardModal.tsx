45406909746a77930e42974300e52e18296bf94e
Paul D'Ambra <paul@posthog.com>
2023-02-06 17:18:42 +0000

    styling more betterer

--- frontend/src/scenes/project-homepage/PrimaryDashboardModal.tsx
+++ frontend/src/scenes/project-homepage/PrimaryDashboardModal.tsx
@@ -7,7 +7,7 @@ import { IconCottage } from 'lib/lemon-ui/icons'
 import { LemonRow } from 'lib/lemon-ui/LemonRow'
 import { LemonModal } from 'lib/lemon-ui/LemonModal'
 import { LemonSkeleton } from 'lib/lemon-ui/LemonSkeleton'
-import { LemonInput } from '@posthog/lemon-ui'
+import { LemonDivider, LemonInput } from '@posthog/lemon-ui'
 
 export function PrimaryDashboardModal(): JSX.Element {
     const { isOpen, primaryDashboardId, dashboards, searchTerm } = useValues(primaryDashboardModalLogic)
@@ -36,7 +36,7 @@ export function PrimaryDashboardModal(): JSX.Element {
                     <LemonSkeleton.Row repeat={4} />
                 </div>
             ) : (
-                <div className="space-y-2">
+                <>
                     <LemonInput
                         type="search"
                         placeholder="Search for dashboards"
@@ -44,50 +44,53 @@ export function PrimaryDashboardModal(): JSX.Element {
                         value={searchTerm}
                         fullWidth={true}
                         allowClear={true}
+                        className="mb-4"
                     />
-                    <hr />
-                    {dashboards.map((dashboard: DashboardType) => {
-                        const isPrimary = dashboard.id === primaryDashboardId
-                        const rowContents = (
-                            <div className="flex flex-1 items-center justify-between overflow-hidden">
-                                <div className="flex-1 flex flex-col justify-center overflow-hidden">
-                                    <strong>{dashboard.name}</strong>
-                                    <span className="text-default font-normal text-ellipsis">
-                                        {dashboard.description}
-                                    </span>
+                    <LemonDivider />
+                    <div className="space-y-2 min-h-100">
+                        {dashboards.map((dashboard: DashboardType) => {
+                            const isPrimary = dashboard.id === primaryDashboardId
+                            const rowContents = (
+                                <div className="flex flex-1 items-center justify-between overflow-hidden">
+                                    <div className="flex-1 flex flex-col justify-center overflow-hidden">
+                                        <strong>{dashboard.name}</strong>
+                                        <span className="text-default font-normal text-ellipsis">
+                                            {dashboard.description}
+                                        </span>
+                                    </div>
+                                    {isPrimary ? (
+                                        <>
+                                            <IconCottage className="mr-2 text-warning text-lg" />
+                                            <span>Default</span>
+                                        </>
+                                    ) : (
+                                        <strong className="set-default-text">Set as default</strong>
+                                    )}
                                 </div>
-                                {isPrimary ? (
-                                    <>
-                                        <IconCottage className="mr-2 text-warning text-lg" />
-                                        <span>Default</span>
-                                    </>
-                                ) : (
-                                    <strong className="set-default-text">Set as default</strong>
-                                )}
-                            </div>
-                        )
-                        if (isPrimary) {
-                            return (
-                                <LemonRow key={dashboard.id} fullWidth status="muted" className="dashboard-row">
-                                    {rowContents}
-                                </LemonRow>
                             )
-                        }
-                        return (
-                            <LemonButton
-                                key={dashboard.id}
-                                fullWidth
-                                className="dashboard-row"
-                                onClick={() => {
-                                    setPrimaryDashboard(dashboard.id)
-                                    closePrimaryDashboardModal()
-                                }}
-                            >
-                                {rowContents}
-                            </LemonButton>
-                        )
-                    })}
-                </div>
+                            if (isPrimary) {
+                                return (
+                                    <LemonRow key={dashboard.id} fullWidth status="muted" className="dashboard-row">
+                                        {rowContents}
+                                    </LemonRow>
+                                )
+                            }
+                            return (
+                                <LemonButton
+                                    key={dashboard.id}
+                                    fullWidth
+                                    className="dashboard-row"
+                                    onClick={() => {
+                                        setPrimaryDashboard(dashboard.id)
+                                        closePrimaryDashboardModal()
+                                    }}
+                                >
+                                    {rowContents}
+                                </LemonButton>
+                            )
+                        })}
+                    </div>
+                </>
             )}
         </LemonModal>
     )
