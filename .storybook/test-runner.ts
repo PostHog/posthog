70baf8faa4eeda088f2f6b1e6b8332aa386397ee
Michael Matloka <dev@twixes.com>
2023-02-10 11:08:47 +0100

    Make `test-runner.ts` more organized

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -3,6 +3,7 @@ import { OptionsParameter } from '@storybook/addons'
 import { getStoryContext, TestRunnerConfig, TestContext } from '@storybook/test-runner'
 import type { Locator, Page, LocatorScreenshotOptions } from 'playwright-core'
 import type { Mocks } from '~/mocks/utils'
+import { StoryContext } from '@storybook/react'
 
 type SupportedBrowserName = 'chromium' | 'firefox' | 'webkit'
 
@@ -37,6 +38,54 @@ const RETRY_TIMES = 5
 
 const customSnapshotsDir = `${process.cwd()}/frontend/__snapshots__`
 const updateSnapshot = expect.getState().snapshotState._updateSnapshot === 'all'
+
+module.exports = {
+    setup() {
+        expect.extend({ toMatchImageSnapshot })
+        jest.retryTimes(RETRY_TIMES, { logErrorsBeforeRetry: true })
+    },
+    async postRender(page, context) {
+        const storyContext = await getStoryContext(page, context)
+
+        await page.evaluate(() => {
+            // Stop all animations for consistent snapshots
+            document.body.classList.add('storybook-test-runner')
+        })
+
+        if (!storyContext.parameters?.testOptions?.skip) {
+            const currentBrowser = page.context().browser()!.browserType().name() as 'chromium' | 'firefox' | 'webkit'
+            const snapshotBrowsers = storyContext.parameters?.testOptions?.snapshotBrowsers ?? ['chromium']
+            if (snapshotBrowsers.includes(currentBrowser)) {
+                await expectStoryToMatchSnapshot(page, context, storyContext, currentBrowser)
+            }
+        }
+    },
+} as TestRunnerConfig
+
+async function expectStoryToMatchSnapshot(
+    page: Page,
+    context: TestContext,
+    storyContext: StoryContext,
+    browser: SupportedBrowserName
+): Promise<void> {
+    let check: (page: Page, context: TestContext, browser: SupportedBrowserName) => Promise<void>
+    if (storyContext.parameters?.layout === 'fullscreen') {
+        if (storyContext.parameters.testOptions?.excludeNavigationFromSnapshot) {
+            check = expectStoryToMatchSceneSnapshot
+        } else {
+            check = expectStoryToMatchFullPageSnapshot
+        }
+    } else {
+        check = expectStoryToMatchComponentSnapshot
+    }
+    // You'd expect that the 'load' state which @storybook/test-runner waits for would already mean
+    // the story is ready, and definitely that 'networkidle' would indicate all assets to be ready.
+    // But that's not the case, so we need to introduce a bit of a delay.
+    // The delay is extended when updating snapshots, so that we're 100% sure they represent the final state.
+    const delayMultiplier: number = updateSnapshot ? RETRY_TIMES : 1
+    await page.waitForTimeout(100 * delayMultiplier)
+    await check(page, context, browser)
+}
 
 async function expectStoryToMatchFullPageSnapshot(
     page: Page,
@@ -88,56 +137,10 @@ async function expectLocatorToMatchStorySnapshot(
     expect(image).toMatchImageSnapshot({
         customSnapshotsDir,
         customSnapshotIdentifier,
-        // Compare structural similarity instead of raw pixels
+        // Compare structural similarity instead of raw pixels - reducing false positives
         // See https://github.com/americanexpress/jest-image-snapshot#recommendations-when-using-ssim-comparison
         comparisonMethod: 'ssim',
         failureThreshold: 0.01,
         failureThresholdType: 'percent',
     })
 }
-
-module.exports = {
-    setup() {
-        expect.extend({ toMatchImageSnapshot })
-        jest.retryTimes(RETRY_TIMES, { logErrorsBeforeRetry: true })
-    },
-    async postRender(page, context) {
-        const storyContext = await getStoryContext(page, context)
-
-        await page.evaluate(() => {
-            // Stop all animations for consistent snapshots
-            document.body.classList.add('storybook-test-runner')
-        })
-
-        if (!storyContext.parameters?.testOptions?.skip) {
-            const currentBrowser = page.context().browser()!.browserType().name() as 'chromium' | 'firefox' | 'webkit'
-            const snapshotBrowsers = storyContext.parameters?.testOptions?.snapshotBrowsers ?? ['chromium']
-            if (!snapshotBrowsers.includes(currentBrowser)) {
-                return
-            }
-            let expectStoryToMatchSnapshot: (
-                page: Page,
-                context: TestContext,
-                browser: SupportedBrowserName
-            ) => Promise<void>
-            if (storyContext.parameters?.layout === 'fullscreen') {
-                if (storyContext.parameters.testOptions?.excludeNavigationFromSnapshot) {
-                    expectStoryToMatchSnapshot = expectStoryToMatchSceneSnapshot
-                } else {
-                    expectStoryToMatchSnapshot = expectStoryToMatchFullPageSnapshot
-                }
-            } else {
-                expectStoryToMatchSnapshot = expectStoryToMatchComponentSnapshot
-            }
-
-            // You'd expect that the 'load' state which @storybook/test-runner waits for would already mean
-            // the story is ready, and definitely that 'networkidle' would indicate all assets to be ready.
-            // But that's not the case, so we need to introduce a bit of a delay.
-            // The delay is extended when updating snapshots, so that we're 100% sure they represent the final state.
-            const delayMultiplier: number = updateSnapshot ? RETRY_TIMES : 1
-            // Get number of Jest retry, without using test name
-            await page.waitForTimeout(100 * delayMultiplier)
-            await expectStoryToMatchSnapshot(page, context, currentBrowser) // Don't retry when updating
-        }
-    },
-} as TestRunnerConfig
