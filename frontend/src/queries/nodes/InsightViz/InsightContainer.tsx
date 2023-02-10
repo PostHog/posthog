dae88da8bee7ce49d2a1e2344db72c4995279469
yakkomajuri <yakko.majuri@gmail.com>
2023-02-10 14:25:36 -0300

    bring back comments

--- frontend/src/queries/nodes/InsightViz/InsightContainer.tsx
+++ frontend/src/queries/nodes/InsightViz/InsightContainer.tsx
@@ -70,8 +70,24 @@ export function InsightContainer({
     disableLastComputation?: boolean
     insightMode?: ItemMode
 }): JSX.Element {
-    const { insightProps, insightLoading, activeView, loadedView, filters, timedOutQueryId, erroredQueryId } =
-        useValues(insightLogic)
+    const {
+        insightProps,
+        // canEditInsight,
+        insightLoading,
+        activeView,
+        loadedView,
+        filters,
+        timedOutQueryId,
+        erroredQueryId,
+        // exporterResourceParams,
+        // isUsingSessionAnalysis,
+    } = useValues(insightLogic)
+    // const {
+    //     areFiltersValid,
+    //     isValidFunnel,
+    //     areExclusionFiltersValid,
+    //     // correlationAnalysisAvailable
+    // } = useValues(funnelLogic(insightProps))
     const { querySource } = useValues(funnelDataLogic(insightProps))
     // TODO: convert to data exploration with insightLogic
     const { areExclusionFiltersValid } = useValues(funnelLogic(insightProps))
@@ -113,8 +129,82 @@ export function InsightContainer({
         return null
     })()
 
+    // function renderTable(): JSX.Element | null {
+    //     if (
+    //         isFunnelsFilter(filters) &&
+    //         !showErrorMessage &&
+    //         !showTimeoutMessage &&
+    //         areFiltersValid &&
+    //         isValidFunnel &&
+    //         filters.funnel_viz_type === FunnelVizType.Steps &&
+    //         !disableTable
+    //     ) {
+    //         return (
+    //             <>
+    //                 <h2 className="my-4 mx-0">Detailed results</h2>
+    //                 <FunnelStepsTable />
+    //             </>
+    //         )
+    //     }
+
+    //     // InsightsTable is loaded for all trend views (except below), plus the sessions view.
+    //     // Exclusions:
+    //     // 1. Table view. Because table is already loaded anyways in `Trends.tsx` as the main component.
+    //     // 2. Bar value chart. Because this view displays data in completely different dimensions.
+    //     if (
+    //         isTrendsFilter(filters) &&
+    //         (!filters.display ||
+    //             (filters?.display !== ChartDisplayType.ActionsTable &&
+    //                 filters?.display !== ChartDisplayType.ActionsBarValue)) &&
+    //         !disableTable
+    //     ) {
+    //         return (
+    //             <>
+    //                 {exporterResourceParams && (
+    //                     <div className="flex items-center justify-between my-4 mx-0">
+    //                         <h2 className="m-0">Detailed results</h2>
+    //                         <Tooltip title="Export this table in CSV format" placement="left">
+    //                             <ExportButton
+    //                                 type="secondary"
+    //                                 status="primary"
+    //                                 items={[
+    //                                     {
+    //                                         export_format: ExporterFormat.CSV,
+    //                                         export_context: exporterResourceParams,
+    //                                     },
+    //                                 ]}
+    //                             />
+    //                         </Tooltip>
+    //                     </div>
+    //                 )}
+    //                 <BindLogic logic={trendsLogic} props={insightProps}>
+    //                     <InsightsTable
+    //                         isLegend
+    //                         filterKey={isTrendsFilter(filters) ? `trends_${activeView}` : ''}
+    //                         canEditSeriesNameInline={
+    //                             isTrendsFilter(filters) && !filters.formula && insightMode === ItemMode.Edit
+    //                         }
+    //                         canCheckUncheckSeries={canEditInsight}
+    //                     />
+    //                 </BindLogic>
+    //             </>
+    //         )
+    //     }
+
+    //     return null
+    // }
+
     return (
         <>
+            {/* {isUsingSessionAnalysis ? (
+                <div className="mb-4">
+                    <AlertMessage type="info">
+                        When using sessions and session properties, events without session IDs will be excluded from the
+                        set of results.{' '}
+                        <a href="https://posthog.com/docs/user-guides/sessions">Learn more about sessions.</a>
+                    </AlertMessage>
+                </div>
+            ) : null} */}
             {/* These are filters that are reused between insight features. They each have generic logic that updates the url */}
             <Card
                 title={disableHeader ? null : <InsightDisplayConfig disableTable={!!disableTable} />}
@@ -153,6 +243,10 @@ export function InsightContainer({
                     )}
                 </div>
             </Card>
+            {/* {renderTable()} */}
+            {/* {!disableCorrelationTable && correlationAnalysisAvailable && activeView === InsightType.FUNNELS && (
+                <FunnelCorrelation />
+            )} */}
         </>
     )
 }
