bf8a1a439ef2ea3846b3981c557ce7a5df61e1a9
yakkomajuri <yakko.majuri@gmail.com>
2023-02-15 14:01:29 -0300

    change back to antd

--- frontend/src/scenes/insights/ComputationTimeWithRefresh.tsx
+++ frontend/src/scenes/insights/ComputationTimeWithRefresh.tsx
@@ -2,7 +2,8 @@ import { useActions, useValues } from 'kea'
 import { insightLogic } from 'scenes/insights/insightLogic'
 import { dayjs } from 'lib/dayjs'
 import { usePeriodicRerender } from 'lib/hooks/usePeriodicRerender'
-import { LemonButton } from '@posthog/lemon-ui'
+import { Button } from 'antd'
+import { Tooltip } from 'lib/lemon-ui/Tooltip'
 
 export function ComputationTimeWithRefresh(): JSX.Element | null {
     const { lastRefresh, insightRefreshButtonDisabledReason } = useValues(insightLogic)
@@ -14,14 +15,16 @@ export function ComputationTimeWithRefresh(): JSX.Element | null {
         <div className="flex items-center text-muted-alt">
             Computed {lastRefresh ? dayjs(lastRefresh).fromNow() : 'a while ago'}
             <span className="px-1">•</span>
-            <LemonButton
-                size="small"
-                onClick={() => loadResults(true)}
-                disabledReason={insightRefreshButtonDisabledReason}
-                className="p-0"
-            >
-                <span className="text-sm">Refresh</span>
-            </LemonButton>
+            <Tooltip title={insightRefreshButtonDisabledReason}>
+                <Button
+                    size="small"
+                    onClick={() => loadResults(true)}
+                    disabled={!!insightRefreshButtonDisabledReason}
+                    className="p-0"
+                >
+                    <span className="text-sm">Refresh</span>
+                </Button>
+            </Tooltip>
         </div>
     )
 }
