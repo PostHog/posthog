03cce94b7839376a224c1956488ffac162cb3470
yakkomajuri <yakko.majuri@gmail.com>
2023-02-17 12:15:56 -0300

    fix(refresh): make sure tooltip appears on disabled button state

--- frontend/src/scenes/insights/ComputationTimeWithRefresh.tsx
+++ frontend/src/scenes/insights/ComputationTimeWithRefresh.tsx
@@ -11,20 +11,24 @@ export function ComputationTimeWithRefresh(): JSX.Element | null {
 
     usePeriodicRerender(15000)
 
+    console.log(insightRefreshButtonDisabledReason)
+
     return (
         <div className="flex items-center text-muted-alt">
             Computed {lastRefresh ? dayjs(lastRefresh).fromNow() : 'a while ago'}
             <span className="px-1">•</span>
             <Tooltip title={insightRefreshButtonDisabledReason}>
-                <Button
-                    type="link"
-                    size="small"
-                    onClick={() => loadResults(true)}
-                    disabled={!!insightRefreshButtonDisabledReason}
-                    className="p-0"
-                >
-                    <span className="text-sm">Refresh</span>
-                </Button>
+                <div>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => loadResults(true)}
+                        disabled={!!insightRefreshButtonDisabledReason}
+                        className="p-0 z-0"
+                    >
+                        <span className="text-sm">Refresh</span>
+                    </Button>
+                </div>
             </Tooltip>
         </div>
     )
