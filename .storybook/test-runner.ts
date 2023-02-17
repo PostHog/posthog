d6e7957a43429e4b62388dcc430f8badacd9aedb
Michael Matloka <dev@twixes.com>
2023-02-17 11:48:41 +0100

    Build Storybook locally again

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -56,15 +56,6 @@ module.exports = {
             const currentBrowser = page.context().browser()!.browserType().name() as 'chromium' | 'firefox' | 'webkit'
             const snapshotBrowsers = storyContext.parameters?.testOptions?.snapshotBrowsers ?? ['chromium']
 
-            let customSnapshotIdentifier = context.id
-            if (currentBrowser !== 'chromium') {
-                customSnapshotIdentifier += `--${currentBrowser}`
-            }
-            await page.screenshot({
-                path: `${process.cwd()}/test-results/${customSnapshotIdentifier}-full.png`,
-                fullPage: true,
-            })
-
             if (snapshotBrowsers.includes(currentBrowser)) {
                 await expectStoryToMatchSnapshot(page, context, storyContext, currentBrowser)
             }
