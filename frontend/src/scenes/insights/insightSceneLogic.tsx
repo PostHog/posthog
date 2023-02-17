983ac3a5c1df980e43dd394b8f7e4bd7db87b541
Paul D'Ambra <paul@posthog.com>
2023-02-17 18:44:15 +0000

    fix import

--- frontend/src/scenes/insights/insightSceneLogic.tsx
+++ frontend/src/scenes/insights/insightSceneLogic.tsx
@@ -12,8 +12,6 @@ import { cleanFilters } from 'scenes/insights/utils/cleanFilters'
 import { teamLogic } from 'scenes/teamLogic'
 import { insightSceneLogicType } from 'scenes/insights/insightSceneLogicType'
 
-import type { insightSceneLogicType } from './insightSceneLogicType'
-
 export const insightSceneLogic = kea<insightSceneLogicType>([
     path(['scenes', 'insights', 'insightSceneLogic']),
     connect({
