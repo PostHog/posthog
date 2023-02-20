74bf09cbfa631f990020d975cf7c81dfeb5311be
Marius Andra <marius.andra@gmail.com>
2023-02-20 23:08:26 +0100

    yeet

--- posthog/hogql/test/test_resolver.py
+++ posthog/hogql/test/test_resolver.py
@@ -234,17 +234,3 @@ class TestResolver(BaseTest):
             with self.assertRaises(ResolverException) as e:
                 resolve_symbols(parse_select(query))
             self.assertIn("Unable to resolve field:", str(e.exception))
-
-
-# "with 2 as a select 1 as a" -> "Different expressions with the same alias a:"
-# "with 2 as b, 3 as c select (select 1 as b) as a, b, c" -> "Different expressions with the same alias b:"
-# "select a, b, e.c from (select 1 as a, 2 as b, 3 as c) as e" -> 1, 2, 3
-
-# # good
-# SELECT t.x FROM (SELECT 1 AS x) AS t;
-# SELECT t.x FROM (SELECT x FROM tbl) AS t;
-# SELECT x FROM (SELECT x FROM tbl) AS t;
-# SELECT 1 AS x, x, x + 1;
-# SELECT x, x + 1, 1 AS x;
-# SELECT x, 1 + (2 + (3 AS x));
-# "SELECT x IN (SELECT 1 AS x) FROM (SELECT 1 AS x)",
