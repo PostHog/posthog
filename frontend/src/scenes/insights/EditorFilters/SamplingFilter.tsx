8aa2a111ff2e6d6a156f4107e4014b76a2c198ca
yakkomajuri <yakko.majuri@gmail.com>
2023-02-22 15:20:51 -0300

    some progress

--- frontend/src/scenes/insights/EditorFilters/SamplingFilter.tsx
+++ frontend/src/scenes/insights/EditorFilters/SamplingFilter.tsx
@@ -5,18 +5,25 @@ import { insightLogic } from 'scenes/insights/insightLogic'
 import { useActions, useValues } from 'kea'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { FEATURE_FLAGS } from 'lib/constants'
+import { funnelLogic } from 'scenes/funnels/funnelLogic'
 
 const DEFAULT_SAMPLING_FACTOR = 0.1
 
 export function SamplingFilter({ filters: editorFilters, insightProps }: EditorFilterProps): JSX.Element {
-    const logic = insightLogic(insightProps)
-    const { setFilters } = useActions(logic)
-    const { filters } = useValues(logic)
+    const initializedInsightLogic = insightLogic(insightProps)
+    const initializedFunnelLogic = funnelLogic(insightProps)
+
+    const { setFilters: setInsightFilters } = useActions(initializedInsightLogic)
+    const { setFilters: setFunnelFilters } = useActions(initializedFunnelLogic)
+
+    const { filters } = useValues(initializedInsightLogic)
+
     const { featureFlags } = useValues(featureFlagLogic)
 
     // Sampling is currently behind a feature flag and only available on lifecycle queries
     const insightSupportsSampling =
-        editorFilters.insight === InsightType.LIFECYCLE || editorFilters.insight === InsightType.FUNNELS
+        (featureFlags[FEATURE_FLAGS.SAMPLING] && editorFilters.insight === InsightType.LIFECYCLE) ||
+        editorFilters.insight === InsightType.FUNNELS
 
     if (insightSupportsSampling) {
         return (
@@ -29,7 +36,19 @@ export function SamplingFilter({ filters: editorFilters, insightProps }: EditorF
                                 <span>Show sampled results</span>
                             </>
                         }
-                        onChange={(newChecked) => setFilters({ ...filters, sampling_factor: newChecked ? DEFAULT_SAMPLING_FACTOR : null })}
+                        onChange={(newChecked) => {
+                            if (editorFilters.insight === InsightType.FUNNELS) {
+                                setFunnelFilters({
+                                    ...filters,
+                                    sampling_factor: newChecked ? DEFAULT_SAMPLING_FACTOR : null,
+                                })
+                                return
+                            }
+                            setInsightFilters({
+                                ...filters,
+                                sampling_factor: newChecked ? DEFAULT_SAMPLING_FACTOR : null,
+                            })
+                        }}
                     />
                 </div>
             </>
