c4e3b807b0dc5e09ee268bdb527d08fe6ec74dd1
Luke Harries <lharries@users.noreply.github.com>
2023-02-23 18:03:05 +0100

    fixed migrations

--- posthog/models/dashboard_templates.py
+++ posthog/models/dashboard_templates.py
@@ -15,7 +15,7 @@ class DashboardTemplate(UUIDModel):
     objects_including_soft_deleted = models.Manager()
 
     team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE, null=True)
-    template_name: models.CharField = models.CharField(max_length=400, null=True)
+    template_name: models.CharField = models.CharField(max_length=400, null=True, blank=True)
     dashboard_description: models.CharField = models.CharField(max_length=400, null=True, blank=True)
     dashboard_filters: models.JSONField = models.JSONField(null=True, blank=True)
     tiles: models.JSONField = models.JSONField(blank=True, null=True)
@@ -23,7 +23,7 @@ class DashboardTemplate(UUIDModel):
     tags: ArrayField = ArrayField(models.CharField(max_length=255), blank=True, null=True)
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     created_by: models.ForeignKey = models.ForeignKey("User", on_delete=models.SET_NULL, null=True, blank=True)
-    deleted: models.BooleanField = models.BooleanField(default=False, blank=True, null=True)
+    deleted: models.BooleanField = models.BooleanField(blank=True, null=True)
     image_url: models.CharField = models.CharField(max_length=8201, null=True, blank=True)
     # URL length for browsers can be as much as 64Kb
     # see https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
