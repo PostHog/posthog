dee2c087d13c23be6cb37ca79615c2268c34e7c4
Paul D'Ambra <paul@posthog.com>
2023-02-26 10:41:49 +0000

    story doesn't need useeffect

--- frontend/src/scenes/dashboard/DashboardTemplateEditor.stories.tsx
+++ frontend/src/scenes/dashboard/DashboardTemplateEditor.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentMeta } from '@storybook/react'
-import { useEffect } from 'react'
 import { DashboardTemplateEditor } from './DashboardTemplateEditor'
 import { dashboardTemplateEditorLogic } from './dashboardTemplateEditorLogic'
 
@@ -9,21 +8,19 @@ export default {
 } as ComponentMeta<typeof DashboardTemplateEditor>
 
 export const CreateTemplate = (): JSX.Element => {
-    useEffect(() => {
-        const unmount = dashboardTemplateEditorLogic.mount()
-        dashboardTemplateEditorLogic.actions.openDashboardTemplateEditor()
-        dashboardTemplateEditorLogic.actions.setEditorValue(
-            JSON.stringify(
-                {
-                    id: '123',
-                    template_name: 'My Template',
-                },
-                null,
-                4
-            )
+    dashboardTemplateEditorLogic.mount()
+    dashboardTemplateEditorLogic.actions.clear()
+    dashboardTemplateEditorLogic.actions.openDashboardTemplateEditor()
+    dashboardTemplateEditorLogic.actions.setEditorValue(
+        JSON.stringify(
+            {
+                id: '123',
+                template_name: 'My Template',
+            },
+            null,
+            4
         )
-        return unmount
-    }, [])
+    )
 
     return (
         <div className="bg-default p-4">
@@ -33,22 +30,19 @@ export const CreateTemplate = (): JSX.Element => {
 }
 
 export const EditTemplate = (): JSX.Element => {
-    useEffect(() => {
-        const unmount = dashboardTemplateEditorLogic.mount()
-        dashboardTemplateEditorLogic.actions.openDashboardTemplateEditor()
-        dashboardTemplateEditorLogic.actions.setEditorValue(
-            JSON.stringify(
-                {
-                    id: '123',
-                    template_name: 'My Template',
-                },
-                null,
-                4
-            )
+    dashboardTemplateEditorLogic.mount()
+    dashboardTemplateEditorLogic.actions.setDashboardTemplateId('123')
+    dashboardTemplateEditorLogic.actions.openDashboardTemplateEditor()
+    dashboardTemplateEditorLogic.actions.setEditorValue(
+        JSON.stringify(
+            {
+                id: '123',
+                template_name: 'My Template',
+            },
+            null,
+            4
         )
-        dashboardTemplateEditorLogic.actions.setDashboardTemplateId('123')
-        return unmount
-    }, [])
+    )
 
     return (
         <div className="bg-default p-4">
