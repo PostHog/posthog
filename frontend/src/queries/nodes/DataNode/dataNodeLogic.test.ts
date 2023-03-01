f743a8152ccd61484193cbfbb79123276cf41693
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-03-01 18:12:14 +0100

    improve tests

--- frontend/src/queries/nodes/DataNode/dataNodeLogic.test.ts
+++ frontend/src/queries/nodes/DataNode/dataNodeLogic.test.ts
@@ -4,10 +4,14 @@ import { expectLogic, partial } from 'kea-test-utils'
 import { dataNodeLogic } from '~/queries/nodes/DataNode/dataNodeLogic'
 import { NodeKind } from '~/queries/schema'
 import { query } from '~/queries/query'
-import { useMocks } from '~/mocks/jest'
-import * as libUtils from 'lib/utils'
 
-jest.mock('~/queries/query')
+jest.mock('~/queries/query', () => {
+    return {
+        __esModules: true,
+        ...jest.requireActual('~/queries/query'),
+        query: jest.fn(),
+    }
+})
 const mockedQuery = query as jest.MockedFunction<typeof query>
 
 const testUniqueKey = 'testUniqueKey'
@@ -426,62 +430,5 @@ describe('dataNodeLogic', () => {
                 autoLoadRunning: true,
                 response: partial({ results: [...results3, ...results2, ...results] }),
             })
-    })
-
-    describe('query cancellation', () => {
-        beforeEach(() => {
-            ;(query as any).mockImplementation(jest.requireActual('~/queries/query').query)
-            useMocks({
-                get: {
-                    '/api/projects/:team/insights/trend/': async (req) => {
-                        if (req.url.searchParams.get('date_from') === '-180d') {
-                            // delay for 2 seconds before response without pausing
-                            return new Promise((resolve) =>
-                                setTimeout(() => {
-                                    resolve([200, { result: ['slow result from api'] }])
-                                }, 2000)
-                            )
-                        }
-                        return [200, { result: ['result from api'] }]
-                    },
-                },
-                post: {
-                    '/api/projects/997/insights/cancel/': [201],
-                },
-            })
-        })
-
-        it('cancels a running query', async () => {
-            ;(libUtils as any).uuid = jest.fn().mockReturnValueOnce('uuid-first').mockReturnValueOnce('uuid-second')
-            logic = dataNodeLogic({
-                key: testUniqueKey,
-                query: {
-                    kind: NodeKind.TrendsQuery,
-                    series: [],
-                    dateRange: { date_from: '-180d' },
-                },
-            })
-            logic.mount()
-
-            setTimeout(() => {
-                dataNodeLogic({
-                    key: testUniqueKey,
-                    query: {
-                        kind: NodeKind.TrendsQuery,
-                        series: [],
-                        dateRange: { date_from: '-90d' },
-                    },
-                })
-            }, 200)
-
-            await expectLogic(logic).toDispatchActions([
-                'loadData',
-                'abortAnyRunningQuery',
-                'loadData',
-                'abortAnyRunningQuery',
-                logic.actionCreators.abortQuery({ queryId: 'uuid-first' }),
-                logic.actionCreators.loadDataSuccess({ result: ['result from api'] }),
-            ])
-        })
     })
 })
