03b010a62820e001e69a7c85eeb4a28a2cbcfe2b
Eric Duong <eeoneric@gmail.com>
2023-03-02 14:40:46 -0500

    feat(feature-flag): dashboard on usage tab (#14404)
    * try again
    
    * Update query snapshots
    
    * Update query snapshots
    
    * Update query snapshots
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update query snapshots
    
    * merge master snapshot
    
    * Update query snapshots
    
    * Update query snapshots
    
    * Update query snapshots
    
    * add flow to generate dashboard if not exists
    
    * typing
    
    * use master
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `webkit` (2)
    
    * fix test
    
    * Update query snapshots
    
    * Update UI snapshots for `chromium` (2)
    
    * Update query snapshots
    
    * Update UI snapshots for `chromium` (2)
    
    * merge master
    
    * reset snapshots
    
    * Update query snapshots
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * merge master
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * change copy
    
    * Update UI snapshots for `chromium` (2)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1970,6 +1970,7 @@ export interface FeatureFlagType {
     performed_rollback: boolean
     can_edit: boolean
     tags: string[]
+    usage_dashboard?: number
 }
 
 export interface FeatureFlagRollbackConditions {
@@ -2051,6 +2052,7 @@ export enum ItemMode { // todo: consolidate this and dashboardmode
 export enum DashboardPlacement {
     Dashboard = 'dashboard', // When on the standard dashboard page
     ProjectHomepage = 'project-homepage', // When embedded on the project homepage
+    FeatureFlag = 'feature-flag',
     Public = 'public', // When viewing the dashboard publicly
     Export = 'export', // When the dashboard is being exported (alike to being printed)
 }
