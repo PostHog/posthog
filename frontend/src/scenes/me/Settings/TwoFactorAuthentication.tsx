e6e53045aaf726dc876f0089dd162cc534c8c542
Li Yi Yu <li@posthog.com>
2023-03-03 09:33:52 -0500

    Update frontend/src/scenes/me/Settings/TwoFactorAuthentication.tsx

--- frontend/src/scenes/me/Settings/TwoFactorAuthentication.tsx
+++ frontend/src/scenes/me/Settings/TwoFactorAuthentication.tsx
@@ -17,7 +17,7 @@ export function TwoFactorAuthentication(): JSX.Element {
             {modalVisible && (
                 <LemonModal title="Set up or manage 2FA" onClose={() => setModalVisible(false)}>
                     <>
-                        <b>Use an authenticator app like Google Auth or 1Password to scan the QR code below.</b>
+                        <b>Use an authenticator app like Google Authenticator or 1Password to scan the QR code below.</b>
                         <Setup2FA
                             onSuccess={() => {
                                 setModalVisible(false)
