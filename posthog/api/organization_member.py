c7d1900dd1840de790b37c23671aaa111d6d1ca1
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-03-03 19:30:49 -0800

    fix: email verification fixes (#14428)
    * use is_email_available instead of is_cloud
    
    * don't show need to verify warn if have social auth
    
    * use multivariate flag so can run experiment
    
    * frontend fixes
    
    * evaluate flag locally
    not passing in email meant we'd get the wrong flag on signup for some reason
    
    * Update query snapshots
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * update tests
    
    * prefetch social auth query
    
    * Update UI snapshots for `chromium` (1)
    
    * Fix types
    
    * fix
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- posthog/api/organization_member.py
+++ posthog/api/organization_member.py
@@ -7,6 +7,7 @@ from rest_framework import exceptions, mixins, serializers, viewsets
 from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.serializers import raise_errors_on_nested_writes
+from social_django.admin import UserSocialAuth
 
 from posthog.api.routing import StructuredViewSetMixin
 from posthog.api.shared import UserBasicSerializer
@@ -39,23 +40,20 @@ class OrganizationMemberObjectPermissions(BasePermission):
 class OrganizationMemberSerializer(serializers.ModelSerializer):
     user = UserBasicSerializer(read_only=True)
     is_2fa_enabled = serializers.SerializerMethodField()
+    has_social_auth = serializers.SerializerMethodField()
 
     class Meta:
         model = OrganizationMembership
-        fields = [
-            "id",
-            "user",
-            "level",
-            "joined_at",
-            "updated_at",
-            "is_2fa_enabled",
-        ]
+        fields = ["id", "user", "level", "joined_at", "updated_at", "is_2fa_enabled", "has_social_auth"]
         read_only_fields = ["id", "joined_at", "updated_at"]
 
     def get_is_2fa_enabled(self, instance: OrganizationMembership) -> bool:
         # If we add other forms of 2FA we need to use default_device here instead
         # But not using that here as it increased the number of queries we did by a lot
         return len(instance.user.totpdevice_set.all()) > 0
+
+    def get_has_social_auth(self, instance: OrganizationMembership) -> bool:
+        return len(instance.user.social_auth.all()) > 0
 
     def update(self, updated_membership, validated_data, **kwargs):
         updated_membership = cast(OrganizationMembership, updated_membership)
@@ -88,7 +86,10 @@ class OrganizationMemberViewSet(
             user__is_active=True,
         )
         .select_related("user")
-        .prefetch_related(Prefetch("user__totpdevice_set", queryset=TOTPDevice.objects.filter(name="default")))
+        .prefetch_related(
+            Prefetch("user__totpdevice_set", queryset=TOTPDevice.objects.filter(name="default")),
+            Prefetch("user__social_auth", queryset=UserSocialAuth.objects.all()),
+        )
     )
     lookup_field = "user__uuid"
 
