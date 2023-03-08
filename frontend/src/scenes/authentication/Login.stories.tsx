9f5d6b5253cedf868dda30d0baef52d26a53256d
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-03-08 10:53:20 -0800

    add stories

--- frontend/src/scenes/authentication/Login.stories.tsx
+++ frontend/src/scenes/authentication/Login.stories.tsx
@@ -38,6 +38,22 @@ export const Cloud = (): JSX.Element => {
     })
     return <Login />
 }
+export const CloudEU = (): JSX.Element => {
+    useStorybookMocks({
+        get: {
+            '/_preflight': {
+                ...preflightJson,
+                cloud: true,
+                region: 'EU',
+                realm: 'cloud',
+                can_create_org: true,
+                available_social_auth_providers: { github: true, gitlab: true, 'google-oauth2': true, saml: false },
+            },
+        },
+        ...sharedMocks,
+    })
+    return <Login />
+}
 export const SelfHosted = (): JSX.Element => {
     useStorybookMocks({
         get: {
