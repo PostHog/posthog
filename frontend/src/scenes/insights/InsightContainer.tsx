eac0e0ce1ad1d5d974c7b6994d31946bf8d26a90
yakkomajuri <yakko.majuri@gmail.com>
2023-03-08 15:30:36 -0300

    feat(sampling): show more clearly that an insight is sampled on the insight card

--- frontend/src/scenes/insights/InsightContainer.tsx
+++ frontend/src/scenes/insights/InsightContainer.tsx
@@ -202,6 +202,18 @@ export function InsightContainer({
                 className="insights-graph-container"
             >
                 <div>
+                    {isFunnelsFilter(filters) ? (
+                        <Row
+                            className={clsx('insights-graph-header', {
+                                funnels: isFunnelsFilter(filters),
+                            })}
+                            align="middle"
+                            justify="space-between"
+                        >
+                            <Col>{isFunnelsFilter(filters) ? <FunnelCanvasLabel /> : null}</Col>
+                        </Row>
+                    ) : null}
+
                     <Row
                         className={clsx('insights-graph-header', {
                             funnels: isFunnelsFilter(filters),
@@ -210,17 +222,23 @@ export function InsightContainer({
                         justify="space-between"
                     >
                         {/*Don't add more than two columns in this row.*/}
-                        {!disableLastComputation && (
-                            <Col>
-                                <ComputationTimeWithRefresh />
-                            </Col>
-                        )}
+                        <Col className="flex items-center gap-1">
+                            {!disableLastComputation && <ComputationTimeWithRefresh />}
+                            {!!filters.sampling_factor ? (
+                                <span className="text-muted-alt">
+                                    {!disableLastComputation ? '• ' : ' '}
+                                    Results calculated from {filters.sampling_factor * 100}% of users
+                                </span>
+                            ) : null}
+                        </Col>
+
                         <Col>
-                            {isFunnelsFilter(filters) ? <FunnelCanvasLabel /> : null}
                             {isPathsFilter(filters) ? <PathCanvasLabel /> : null}
+
                             <InsightLegendButton />
                         </Col>
                     </Row>
+
                     {!!BlockingEmptyState ? (
                         BlockingEmptyState
                     ) : isFilterWithDisplay(filters) && filters.show_legend ? (
