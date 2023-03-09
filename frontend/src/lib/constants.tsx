0bed3336c7766602e6b25c59fbe501fd5f9e7183
Paul D'Ambra <paul@posthog.com>
2023-03-09 11:21:48 +0000

    feat: delete all whole bunch o' stuff (#14645)
    * delete all whole bunch o' stuff
    
    * using the wrong loading value

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -142,7 +142,6 @@ export const FEATURE_FLAGS = {
     REQUIRE_EMAIL_VERIFICATION: 'require-email-verification', // owner: @raquelmsmith
     DATA_EXPLORATION_QUERIES_ON_DASHBOARDS: 'data-exploration-queries-on-dashboards', // owner: @pauldambra
     SAMPLING: 'sampling', // owner: @yakkomajuri
-    DASHBOARD_TEMPLATES: 'dashboard-templates', // owner @pauldambra and @lharries
     TEMPLUKES: 'templukes', // owner: @pauldambra and @lharries
     RECORDINGS_V2_RECORDER: 'recordings-v2-recorder', // owner: #team-session-recordings
     ENABLE_PROMPTS: 'enable-prompts', // owner: @lharries
--- posthog/api/dashboards/test/test_dashboard_templates.py
+++ posthog/api/dashboards/test/test_dashboard_templates.py
@@ -1,7 +1,3 @@
-import json
-from typing import Any, Dict, List
-from unittest.mock import Mock, PropertyMock, patch
-
 from rest_framework import status
 
 from posthog.models.dashboard_templates import DashboardTemplate
@@ -14,68 +10,6 @@ def assert_template_equals(received, expected):
     for key in keys_to_check:
         assert received[key] == expected[key], f"key {key} failed, expected {expected[key]} but got {received[key]}"
 
-
-# github does not return the OG template
-github_response_json: List[Dict] = [
-    {
-        "name": "Website traffic",
-        "url": "a github url",
-        "description": "The website analytics dashboard that PostHog uses",
-        "verified": True,
-        "maintainer": "official",
-    },
-]
-
-expected_template_listing_json: List[Dict] = [
-    {
-        "name": "Product analytics",
-        "url": None,
-        "description": "The OG PostHog product analytics dashboard template",
-        "verified": True,
-        "maintainer": "official",
-    },
-    {
-        "name": "Website traffic",
-        "url": "a github url",
-        "description": "The website analytics dashboard that PostHog uses",
-        "verified": True,
-        "maintainer": "official",
-    },
-]
-
-updated_template_listing_json: List[Dict] = [
-    {
-        **expected_template_listing_json[1],
-        "url": "https://github.com/PostHog/templates-repository/blob/a-new-commit-hash/dashboards/posthog-website-traffic.json",
-    },
-]
-
-website_traffic_template_listing: Dict = {
-    "template_name": "Website traffic",
-    "dashboard_description": "",
-    "dashboard_filters": {},
-    "tiles": [
-        {
-            "name": "Website Unique Users (Total)",
-            "type": "INSIGHT",
-            "color": "blue",
-            "filters": {
-                "events": [{"id": "$pageview", "math": "dau", "type": "events"}],
-                "compare": True,
-                "display": "BoldNumber",
-                "insight": "TRENDS",
-                "interval": "day",
-                "date_from": "-30d",
-            },
-            "layouts": {
-                "sm": {"h": 5, "i": "21", "w": 6, "x": 0, "y": 0, "minH": 5, "minW": 3},
-                "xs": {"h": 5, "i": "21", "w": 1, "x": 0, "y": 0, "minH": 5, "minW": 1},
-            },
-            "description": "Shows the number of unique users that use your app every day.",
-        },
-    ],
-    "tags": [],
-}
 
 variable_template = {
     "template_name": "Sign up conversion template with variables",
@@ -130,10 +64,7 @@ class TestDashboardTemplates(APIBaseTest):
         self.user.is_staff = True
         self.user.save()
 
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_create_and_get_dashboard_template_with_tile(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, website_traffic_template_listing)
-
+    def test_create_and_get_dashboard_template_with_tile(self) -> None:
         response = self.client.post(
             f"/api/projects/{self.team.pk}/dashboard_templates",
             variable_template,
@@ -156,10 +87,7 @@ class TestDashboardTemplates(APIBaseTest):
             variable_template,
         )
 
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_non_staff_user_cannot_create_dashboard(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, website_traffic_template_listing)
-
+    def test_non_staff_user_cannot_create_dashboard(self) -> None:
         self.user.is_staff = False
         self.user.save()
 
@@ -177,13 +105,9 @@ class TestDashboardTemplates(APIBaseTest):
             variable_template,
         )
         assert response.status_code == status.HTTP_201_CREATED, response
-
         assert DashboardTemplate.objects.count() == 1
-        dashboardTemplate = DashboardTemplate.objects.first()
-        assert dashboardTemplate is not None
-        id = dashboardTemplate.id
 
-        response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates/{id}")
+        response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates/{response.json()['id']}")
 
         assert response.status_code == status.HTTP_200_OK, response
 
@@ -198,46 +122,38 @@ class TestDashboardTemplates(APIBaseTest):
             variable_template,
         )
         assert response.status_code == status.HTTP_201_CREATED, response
-
         assert DashboardTemplate.objects.count() == 1
 
-        dashboardTemplate = DashboardTemplate.objects.first()
-        assert dashboardTemplate is not None
-        id = dashboardTemplate.id
+        update_response = self.client.patch(
+            f"/api/projects/{self.team.pk}/dashboard_templates/{response.json()['id']}", {"deleted": True}
+        )
+        assert update_response.status_code == status.HTTP_200_OK, update_response
 
-        response = self.client.patch(f"/api/projects/{self.team.pk}/dashboard_templates/{id}", {"deleted": True})
+        get_response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates")
+        assert get_response.status_code == status.HTTP_200_OK, get_response
 
-        assert response.status_code == status.HTTP_200_OK, response
-
-        response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates")
-        assert response.status_code == status.HTTP_200_OK, response
-
-        assert response.json()["results"] == [], response.json()
+        assert get_response.json()["results"] == [], get_response.json()
 
     def test_non_staff_user_cannot_delete_dashboard_template_by_id(self) -> None:
-
         response = self.client.post(
             f"/api/projects/{self.team.pk}/dashboard_templates",
             variable_template,
         )
         assert response.status_code == status.HTTP_201_CREATED, response
-
         assert DashboardTemplate.objects.count() == 1
-        dashboardTemplate = DashboardTemplate.objects.first()
-        assert dashboardTemplate is not None
-        id = dashboardTemplate.id
 
         self.user.is_staff = False
         self.user.save()
 
-        response = self.client.patch(f"/api/projects/{self.team.pk}/dashboard_templates/{id}", {"deleted": True})
+        patch_response = self.client.patch(
+            f"/api/projects/{self.team.pk}/dashboard_templates/{response.json()['id']}", {"deleted": True}
+        )
+        assert patch_response.status_code == status.HTTP_403_FORBIDDEN, patch_response
 
-        assert response.status_code == status.HTTP_403_FORBIDDEN, response
+        get_response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates")
+        assert get_response.status_code == status.HTTP_200_OK, get_response
 
-        response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates")
-        assert response.status_code == status.HTTP_200_OK, response
-
-        assert response.json()["results"] != [], response.json()
+        assert get_response.json()["results"] != [], get_response.json()
 
     def test_update_dashboard_template_by_id(self) -> None:
         response = self.client.post(
@@ -245,163 +161,14 @@ class TestDashboardTemplates(APIBaseTest):
             variable_template,
         )
         assert response.status_code == status.HTTP_201_CREATED
-
         assert DashboardTemplate.objects.count() == 1
-        dashboardTemplate = DashboardTemplate.objects.first()
-        assert dashboardTemplate is not None
-        id = dashboardTemplate.id
 
-        response = self.client.patch(
-            f"/api/projects/{self.team.pk}/dashboard_templates/{id}",
+        update_response = self.client.patch(
+            f"/api/projects/{self.team.pk}/dashboard_templates/{response.json()['id']}",
             {"template_name": "new name"},
         )
-
-        assert response.status_code == status.HTTP_200_OK, response
-
-        assert DashboardTemplate.objects.count() == 1
-        dashboardTemplate = DashboardTemplate.objects.first()
-        assert dashboardTemplate is not None
-        assert dashboardTemplate.template_name == "new name"
-
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_repository_calls_to_github_and_returns_the_listing(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, github_response_json)
-
-        response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates/repository")
-        assert response.status_code == status.HTTP_200_OK, response
-
-        expected_listing: List[Dict[str, Any]] = []
-        for tl in expected_template_listing_json:
-            expected_listing.append({**tl, "installed": tl["name"] == "Product analytics", "has_new_version": False})
-
-        assert response.json() == expected_listing
-
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_repository_can_install_from_github(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, website_traffic_template_listing)
-
-        assert DashboardTemplate.objects.count() == 0
-
-        response = self.client.post(
-            f"/api/projects/{self.team.pk}/dashboard_templates",
-            {"name": "Website traffic", "url": "a github url"},
-        )
-        assert response.status_code == status.HTTP_201_CREATED, response
-
-        patched_requests.assert_called_with("a github url")
-
-        assert DashboardTemplate.objects.count() == 1
-
-        # all now show as installed
-
-        self._patch_request_get(patched_requests, github_response_json)
-
-        response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates/repository")
-        assert response.status_code == status.HTTP_200_OK, response
-        assert len(response.json()) == 2
-
-        expected_listing: List[Dict[str, Any]] = []
-        for tl in expected_template_listing_json:
-            expected_listing.append({**tl, "installed": True, "has_new_version": False})
-
-        assert response.json() == expected_listing
-
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_non_staff_user_cannot_install_templates(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, website_traffic_template_listing)
-
-        assert DashboardTemplate.objects.count() == 0
-
-        self.user.is_staff = False
-        self.user.save()
-        response = self.client.post(
-            f"/api/projects/{self.team.pk}/dashboard_templates",
-            {"name": "Website traffic", "url": "a github url"},
-        )
-        assert response.status_code == status.HTTP_403_FORBIDDEN, response
-
-        assert DashboardTemplate.objects.count() == 0
-
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_dashboards_are_installed_with_no_team_id(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, website_traffic_template_listing)
-
-        response = self.client.post(
-            f"/api/projects/{self.team.pk}/dashboard_templates",
-            {"name": "Website traffic", "url": "a github url"},
-        )
-        assert response.status_code == status.HTTP_201_CREATED, response
-
-        patched_requests.assert_called_with("a github url")
-
-        assert DashboardTemplate.objects.count() == 1
-        assert DashboardTemplate.objects.filter(team_id__isnull=True).count() == 1
-
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_repository_can_update_from_github(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, website_traffic_template_listing)
-
-        assert DashboardTemplate.objects.count() == 0
-
-        response = self.client.post(
-            f"/api/projects/{self.team.pk}/dashboard_templates",
-            {"name": "Website traffic", "url": "a github url", "tiles": website_traffic_template_listing["tiles"]},
-        )
-        assert response.status_code == status.HTTP_201_CREATED, response
-
-        patched_requests.assert_called_with("a github url")
-
-        assert DashboardTemplate.objects.count() == 1
-        assert DashboardTemplate.objects.first().tags == []  # type: ignore
-
-        self._patch_request_get(patched_requests, updated_template_listing_json)
-
-        response = self.client.get(f"/api/projects/{self.team.pk}/dashboard_templates/repository")
-        assert response.status_code == status.HTTP_200_OK, response
-        assert [r["has_new_version"] for r in response.json()] == [False, True]
-
-        self._patch_request_get(
-            patched_requests,
-            {
-                **website_traffic_template_listing,
-                "tags": ["updated"],
-            },
-        )
-
-        response = self.client.post(
-            f"/api/projects/{self.team.pk}/dashboard_templates",
-            {"name": "Website traffic", "url": "a github url"},
-        )
-        assert response.status_code == status.HTTP_201_CREATED, response
-
-        assert DashboardTemplate.objects.count() == 1
-        assert DashboardTemplate.objects.first().tags == ["updated"]  # type: ignore
-
-    @patch("posthog.api.dashboards.dashboard_templates.requests.get")
-    def test_validation_that_names_have_to_match(self, patched_requests) -> None:
-        self._patch_request_get(patched_requests, website_traffic_template_listing)
-
-        assert DashboardTemplate.objects.count() == 0
-
-        response = self.client.post(
-            f"/api/projects/{self.team.pk}/dashboard_templates",
-            {"name": "this is never going to match", "url": "a github url"},
-        )
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-        response_json = response.json()
-        assert (
-            response_json["detail"]
-            == 'The requested template "this is never going to match" does not match the requested template URL which loaded the template "Website traffic"'
-        )
-
-    @staticmethod
-    def _patch_request_get(patched_requests, json_response):
-        mock_response = Mock()
-        mock_response.status_code = 200
-        mock_text = PropertyMock(return_value=json.dumps(json_response))
-        type(mock_response).text = mock_text
-        mock_response.json.return_value = json_response
-        patched_requests.return_value = mock_response
+        assert update_response.status_code == status.HTTP_200_OK, update_response
+        assert update_response.json()["template_name"] == "new name"
 
     def test_dashboard_template_schema(self) -> None:
         dashboard_template_schema = {
