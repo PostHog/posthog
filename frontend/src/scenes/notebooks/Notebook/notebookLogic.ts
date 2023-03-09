c81112ba312f9d02b190e9cc35cb26efb146eec7
Ben White <ben@posthog.com>
2023-03-09 14:52:30 +0100

    Changes

--- frontend/src/scenes/notebooks/Notebook/notebookLogic.ts
+++ frontend/src/scenes/notebooks/Notebook/notebookLogic.ts
@@ -8,7 +8,7 @@ import { notebookSidebarLogic } from 'scenes/notebooks/Notebook/notebookSidebarL
 const START_CONTENT = `
 <h2>Introducing Notebook!</h2>
 <blockquote>This is experimental</blockquote>
-<ph-insight></ph-insight>
+<ph-query></ph-query>
 <ph-recording sessionRecordingId="186c620122516e6-0ebf2e4cc8b8da-1f525634-16a7f0-186c62012262dfa"></ph-recording>
 `
 
