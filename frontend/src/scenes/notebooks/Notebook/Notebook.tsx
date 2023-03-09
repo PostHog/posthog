c73b8bf1a08d1b5cae830b3c41ca344e128d652a
Ben White <ben@posthog.com>
2023-03-09 13:40:11 +0100

    Styles changes

--- frontend/src/scenes/notebooks/Notebook/Notebook.tsx
+++ frontend/src/scenes/notebooks/Notebook/Notebook.tsx
@@ -1,7 +1,7 @@
 import { LemonButton, LemonDivider } from '@posthog/lemon-ui'
 import { useEditor, EditorContent, FloatingMenu } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import { IconLock, IconLockOpen } from 'lib/lemon-ui/icons'
+import { IconChevronRight, IconJournal, IconLock, IconLockOpen } from 'lib/lemon-ui/icons'
 import { useEffect, useState } from 'react'
 import { InsightNode } from '../Nodes/InsightNode'
 import { QueryNode } from '../Nodes/QueryNode'
@@ -37,51 +37,59 @@ export function Notebook(): JSX.Element {
     }, [isEditable, editor])
 
     return (
-        <div className="border-l bg-side h-full p-2 flex flex-col">
-            <header className="flex items-center justify-between gap-2 uppercase font-semibold shrink-0">
-                <span>Notebook</span>
-                <span className="flex gap-2">
-                    <LemonButton
-                        size="small"
-                        onClick={() => setIsEditable(!isEditable)}
-                        status="primary-alt"
-                        type={!isEditable ? 'primary' : undefined}
-                    >
-                        {!isEditable ? <IconLock /> : <IconLockOpen />}
-                    </LemonButton>
-                </span>
-            </header>
-            <LemonDivider dashed />
+        <div className="h-full p-2 flex flex-col">
+            <div className="border rounded bg-side flex-1 p-3 shadow">
+                <header className="flex items-center justify-between gap-2 font-semibold shrink-0">
+                    <span>
+                        <IconJournal /> Notebook
+                    </span>
+                    <span className="flex gap-2">
+                        <LemonButton
+                            size="small"
+                            onClick={() => setIsEditable(!isEditable)}
+                            status="primary-alt"
+                            type={!isEditable ? 'primary' : undefined}
+                            noPadding
+                        >
+                            <div className="m-1">{!isEditable ? <IconLock /> : <IconLockOpen />}</div>
+                        </LemonButton>
+                        <LemonButton size="small" onClick={() => alert('TODO!')} status="primary-alt" noPadding>
+                            <IconChevronRight className="text-lg" />
+                        </LemonButton>
+                    </span>
+                </header>
+                <LemonDivider dashed />
 
-            {editor && (
-                <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }} className="flex items-center gap-2">
-                    <LemonButton
-                        size="small"
-                        status="primary-alt"
-                        onClick={() => editor.chain().focus().insertContent('<ph-query />').run()}
-                    >
-                        Query
-                    </LemonButton>
+                {editor && (
+                    <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }} className="flex items-center gap-2">
+                        <LemonButton
+                            size="small"
+                            status="primary-alt"
+                            onClick={() => editor.chain().focus().insertContent('<ph-query />').run()}
+                        >
+                            Query
+                        </LemonButton>
 
-                    <LemonButton
-                        size="small"
-                        status="primary-alt"
-                        onClick={() => editor.chain().focus().insertContent('<ph-playlist />').run()}
-                    >
-                        Recordings
-                    </LemonButton>
+                        <LemonButton
+                            size="small"
+                            status="primary-alt"
+                            onClick={() => editor.chain().focus().insertContent('<ph-playlist />').run()}
+                        >
+                            Recordings
+                        </LemonButton>
 
-                    <LemonButton
-                        size="small"
-                        status="primary-alt"
-                        onClick={() => editor.chain().focus().insertContent('<ph-embed />').run()}
-                    >
-                        Embed
-                    </LemonButton>
-                </FloatingMenu>
-            )}
+                        <LemonButton
+                            size="small"
+                            status="primary-alt"
+                            onClick={() => editor.chain().focus().insertContent('<ph-embed />').run()}
+                        >
+                            Embed
+                        </LemonButton>
+                    </FloatingMenu>
+                )}
 
-            <EditorContent editor={editor} className="flex-1 overflow-y-auto" />
+                <EditorContent editor={editor} className="flex-1 overflow-y-auto" />
+            </div>
         </div>
     )
 }
