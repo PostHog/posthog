47273197726a4a9353c90f670ec133edb30674c4
timgl <tim@posthog.com>
2023-03-09 16:22:10 +0000

    fix(auth): Remove 2fa requirement if user is using 2fa

--- posthog/settings/web.py
+++ posthog/settings/web.py
@@ -151,7 +151,6 @@ SOCIAL_AUTH_PIPELINE = (
     "social_core.pipeline.social_auth.associate_user",
     "social_core.pipeline.social_auth.load_extra_data",
     "social_core.pipeline.user.user_details",
-    "social_2fa.social_pipeline.two_factor_auth",
 )
 
 SOCIAL_AUTH_STRATEGY = "social_django.strategy.DjangoStrategy"
