4f2fdbc284ef398503b2093ecbb17f08b5d87116
Ben White <ben@posthog.com>
2023-03-09 16:13:33 +0100

    Added drag handle

--- frontend/src/scenes/notebooks/Nodes/NodeWrapper.tsx
+++ frontend/src/scenes/notebooks/Nodes/NodeWrapper.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
 import { NodeViewWrapper } from '@tiptap/react'
 import clsx from 'clsx'
+import { IconDragHandle, IconLink } from 'lib/lemon-ui/icons'
+import { LemonButton } from '@posthog/lemon-ui'
 
 export interface NodeWrapperProps {
     className: string
@@ -9,12 +11,14 @@ export interface NodeWrapperProps {
 
 export function NodeWrapper({ className, children }: NodeWrapperProps): JSX.Element {
     return (
-        <NodeViewWrapper
-            as="div"
-            className={clsx(className, 'border bg-white rounded-lg mb-2 overflow-y-auto')}
-            data-drag-handle
-        >
-            {children}
+        <NodeViewWrapper as="div" className={clsx(className, 'flex items-start gap-1')}>
+            <div className="flex flex-col gap-2">
+                <LemonButton size="small" noPadding icon={<IconDragHandle />} status="primary-alt" data-drag-handle />
+
+                <LemonButton size="small" noPadding icon={<IconLink />} status="primary-alt" />
+            </div>
+
+            <div className={clsx('border bg-white rounded-lg mb-2 overflow-y-auto flex-1')}>{children}</div>
         </NodeViewWrapper>
     )
 }
