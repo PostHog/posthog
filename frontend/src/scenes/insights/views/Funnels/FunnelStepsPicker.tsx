3eb5fcbcd4232d1bd4dacc4ac7ffbf490915ab37
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-03-14 13:59:23 +0100

    de-ant

--- frontend/src/scenes/insights/views/Funnels/FunnelStepsPicker.tsx
+++ frontend/src/scenes/insights/views/Funnels/FunnelStepsPicker.tsx
@@ -1,12 +1,12 @@
 import { useActions, useValues } from 'kea'
 import { funnelLogic } from 'scenes/funnels/funnelLogic'
 import { EntityFilter } from '~/types'
-import { Select } from 'antd'
-import { ANTD_TOOLTIP_PLACEMENTS } from 'lib/utils'
+
 import { EntityFilterInfo } from 'lib/components/EntityFilterInfo'
 import { insightLogic } from 'scenes/insights/insightLogic'
 import { funnelDataLogic } from 'scenes/funnels/funnelDataLogic'
 import { FunnelsFilter } from '~/queries/schema'
+import { LemonSelect, LemonSelectOptions, LemonSelectOption } from '@posthog/lemon-ui'
 
 export function FunnelStepsPickerDataExploration(): JSX.Element | null {
     const { insightProps } = useValues(insightLogic)
@@ -67,49 +67,50 @@ export function FunnelStepsPickerComponent({
         ? Array.from(Array(Math.max(numberOfSeries)).keys()).slice((funnelsFilter?.funnel_from_step ?? 0) + 1)
         : [1]
 
-    const renderStepOptions = (range: number[]): React.ReactNode => {
-        return range.map((stepIndex) => {
-            const stepFilter = filterSteps.find((f) => f.order === stepIndex)
-
-            return stepFilter ? (
-                <Select.Option key={stepIndex} value={stepIndex} label={`Step ${stepIndex + 1}`}>
-                    <div className="flex flex-row">
-                        <span className="mr-1">Step {stepIndex + 1}:</span>
-                        <EntityFilterInfo filter={stepFilter as EntityFilter} />
-                    </div>
-                </Select.Option>
-            ) : null
-        })
+    const optionsForRange = (range: number[]): LemonSelectOptions<number> => {
+        return range
+            .map((stepIndex): LemonSelectOption<number> | null => {
+                const stepFilter = filterSteps.find((f) => f.order === stepIndex)
+                return stepFilter
+                    ? {
+                          value: stepIndex,
+                          label: `Step ${stepIndex + 1}`,
+                          element: (
+                              <div className="flex flex-row">
+                                  <span className="mr-1">Step {stepIndex + 1}:</span>
+                                  <EntityFilterInfo filter={stepFilter as EntityFilter} />
+                              </div>
+                          ),
+                      }
+                    : null
+            })
+            .filter((option): option is LemonSelectOption<number> => option !== null)
     }
 
     return (
         <div className="flex items-center">
             <span className="text-muted-alt">&nbsp;from</span>
-            <Select
-                disabled={!isFunnelWithEnoughSteps}
+            <LemonSelect
+                size="small"
+                className="mx-1"
                 dropdownMatchSelectWidth={false}
-                dropdownAlign={ANTD_TOOLTIP_PLACEMENTS.bottomLeft}
-                data-attr="funnel-header-steps-funnel_from_step-selector"
-                optionLabelProp="label"
+                optionTooltipPlacement="bottomLeft"
+                disabled={!isFunnelWithEnoughSteps}
+                options={optionsForRange(fromRange)}
                 value={funnelsFilter?.funnel_from_step || 0}
-                onChange={(fromStep: number) => onChange(fromStep, funnelsFilter?.funnel_to_step)}
-                style={{ marginLeft: 4, marginRight: 4 }}
-            >
-                {renderStepOptions(fromRange)}
-            </Select>
+                onChange={(fromStep: number | null) => fromStep && onChange(fromStep, funnelsFilter?.funnel_to_step)}
+            />
             <span className="text-muted-alt">to</span>
-            <Select
-                disabled={!isFunnelWithEnoughSteps}
+            <LemonSelect
+                size="small"
+                className="mx-1"
                 dropdownMatchSelectWidth={false}
-                dropdownAlign={ANTD_TOOLTIP_PLACEMENTS.bottomLeft}
-                data-attr="funnel-header-steps-funnel_to_step-selector"
-                optionLabelProp="label"
+                optionTooltipPlacement="bottomLeft"
+                disabled={!isFunnelWithEnoughSteps}
+                options={optionsForRange(toRange)}
                 value={funnelsFilter?.funnel_to_step || Math.max(numberOfSeries - 1, 1)}
-                onChange={(toStep: number) => onChange(funnelsFilter?.funnel_from_step, toStep)}
-                style={{ marginLeft: 4, marginRight: 4 }}
-            >
-                {renderStepOptions(toRange)}
-            </Select>
+                onChange={(toStep: number | null) => toStep && onChange(funnelsFilter?.funnel_from_step, toStep)}
+            />
         </div>
     )
 }
