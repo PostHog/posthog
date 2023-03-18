6ac7d53900ec752561af9e5c435f24214a420245
mukeshmahato18 <124282280+mukeshmahato18@users.noreply.github.com>
2023-03-18 17:53:14 +0545

    Update changeType.js
    Here are the changes that were made:
    
        1. Changed the value == 'true' expression to value === 'true'. This is a more reliable way to check whether the string value is equal to the boolean true.
    
        2. Added a default case to the switch statement that throws an error if the type is not recognized. This is a good practice to ensure that the function handles all possible input values.
    
       3. Changed the throw statement to use the Error constructor instead of a string. This makes it easier to identify the type of error that occurred when debugging the code.
    
    With these changes, the changeType function should be more reliable and easier to maintain.

--- frontend/src/lib/utils/changeType.js
+++ frontend/src/lib/utils/changeType.js
@@ -4,16 +4,17 @@
  * @param {Number/String} value the value to be changed
  **/
 export function changeType(type, value) {
-    switch (type) {
-        case 'text':
-            return String(value)
-        case 'number':
-            return Number(value)
-        case 'boolean':
-            return value == 'true' ? true : false
-        case 'string':
-            return String(value)
-        default:
-            throw 'Unsupported type'
-    }
+  switch (type) {
+    case 'text':
+      return String(value)
+    case 'number':
+      return Number(value)
+    case 'boolean':
+      return value === 'true'
+    case 'string':
+      return String(value)
+    default:
+      throw new Error('Unsupported type: ' + type)
+  }
 }
+
