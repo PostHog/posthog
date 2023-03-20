5c41697442753c5dd8a004af8b415b1b0faecf85
Paul D'Ambra <paul@posthog.com>
2023-03-20 11:11:05 +0000

    don't reset the insight logic

--- frontend/src/scenes/insights/insightLogic.ts
+++ frontend/src/scenes/insights/insightLogic.ts
@@ -120,7 +120,6 @@ export const insightLogic = kea<insightLogicType>([
     }),
 
     actions({
-        resetNewInsight: true,
         setFilters: (filters: Partial<FilterType>, insightMode?: ItemMode) => ({ filters, insightMode }),
         setFiltersMerge: (filters: Partial<FilterType>) => ({ filters }),
         reportInsightViewedForRecentInsights: () => true,
@@ -441,17 +440,6 @@ export const insightLogic = kea<insightLogicType>([
             },
         ],
         insight: {
-            resetNewInsight: (state) => {
-                if (props.dashboardItemId !== 'new') {
-                    return state
-                }
-                return {
-                    ...state,
-                    filters: {},
-                    query: undefined,
-                    results: null,
-                }
-            },
             loadInsight: (state, { shortId }) =>
                 shortId === state.short_id
                     ? state
