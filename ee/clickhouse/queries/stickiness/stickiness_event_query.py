b452ab09e058ac444d320f3438431ea803725aa2
yakkomajuri <yakko.majuri@gmail.com>
2023-03-21 12:06:42 -0300

    feat(poe): poe v2 support for stickiness

--- ee/clickhouse/queries/stickiness/stickiness_event_query.py
+++ ee/clickhouse/queries/stickiness/stickiness_event_query.py
@@ -1,6 +1,5 @@
 from posthog.models.group.util import get_aggregation_target_field
 from posthog.queries.stickiness.stickiness_event_query import StickinessEventsQuery
-from posthog.utils import PersonOnEventsMode
 
 
 class ClickhouseStickinessEventsQuery(StickinessEventsQuery):
@@ -8,5 +7,5 @@ class ClickhouseStickinessEventsQuery(StickinessEventsQuery):
         return get_aggregation_target_field(
             self._entity.math_group_type_index,
             self.EVENT_TABLE_ALIAS,
-            f"{self.DISTINCT_ID_TABLE_ALIAS if self._person_on_events_mode == PersonOnEventsMode.DISABLED else self.EVENT_TABLE_ALIAS}.person_id",
+            self._person_id_alias,
         )
