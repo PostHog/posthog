62b0426b4e7c2312fe0c52edc441fdd80e443c5d
Paul D'Ambra <paul@posthog.com>
2023-03-22 12:50:25 +0000

    Update frontend/src/scenes/saved-insights/SavedInsights.tsx

--- frontend/src/scenes/saved-insights/SavedInsights.tsx
+++ frontend/src/scenes/saved-insights/SavedInsights.tsx
@@ -115,7 +115,7 @@ export const INSIGHT_TYPES_METADATA: Record<InsightType, InsightTypeMetadata> = 
     },
     [InsightType.JSON]: {
         name: 'Custom',
-        description: 'Save any data table as an insight, and edit the JSON directly.',
+        description: 'Save components powered by our JSON query language.',
         icon: InsightSQLIcon,
         inMenu: true,
     },
