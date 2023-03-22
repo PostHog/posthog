80e7621bf67de019227f2fd1bc082784a414dc5e
James Greenhill <fuziontech@gmail.com>
2023-03-21 21:13:29 -0600

    type hint

--- posthog/temporal/client.py
+++ posthog/temporal/client.py
@@ -2,7 +2,7 @@ from temporalio.client import Client, TLSConfig
 
 
 async def connect(host, port, namespace, server_root_ca_cert=None, client_cert=None, client_key=None):
-    tls = False
+    tls: TLSConfig | bool = False
     if server_root_ca_cert and client_cert and client_key:
         tls = TLSConfig(
             server_root_ca_cert=server_root_ca_cert,
