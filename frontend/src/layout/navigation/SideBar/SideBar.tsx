0414f83888473e4b65b0d8a9c0a5a815872e1452
Paul D'Ambra <paul@posthog.com>
2023-03-29 17:11:27 -0400

    feat: Add a bug/issue tracking list view (#14916)
    * add empty page to UI
    
    * add some queries to the page
    
    * a step towards filtering
    
    * almost working list view
    
    * obey typegen
    
    * use different default
    
    * fiddle a little
    
    * fiddle a little
    
    * better detection of change
    
    * allow queries to override insight empty state text
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * add a type to help typegen
    
    * update shield bug icon
    
    * Update frontend/src/scenes/issues/Issues.tsx
    
    Co-authored-by: Tiina Turban <tiina303@gmail.com>
    
    * Update frontend/src/lib/constants.tsx
    
    Co-authored-by: Tiina Turban <tiina303@gmail.com>
    
    * Update frontend/src/scenes/issues/Issues.tsx
    
    Co-authored-by: Tiina Turban <tiina303@gmail.com>
    
    * prettierify
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `webkit` (2)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tiina Turban <tiina303@gmail.com>

--- frontend/src/layout/navigation/SideBar/SideBar.tsx
+++ frontend/src/layout/navigation/SideBar/SideBar.tsx
@@ -6,6 +6,7 @@ import { ProjectName, ProjectSwitcherOverlay } from '~/layout/navigation/Project
 import {
     IconApps,
     IconBarChart,
+    IconBugShield,
     IconCoffee,
     IconCohort,
     IconComment,
@@ -215,6 +216,9 @@ function Pages(): JSX.Element {
                     ) : null}
                     {featureFlags[FEATURE_FLAGS.FEEDBACK_SCENE] && (
                         <PageButton icon={<IconMessages />} identifier={Scene.Feedback} to={urls.feedback()} />
+                    )}
+                    {featureFlags[FEATURE_FLAGS.ARUBUG] && (
+                        <PageButton icon={<IconBugShield />} identifier={Scene.Issues} to={urls.issues()} />
                     )}
                     <div className="SideBar__heading">Configuration</div>
 
