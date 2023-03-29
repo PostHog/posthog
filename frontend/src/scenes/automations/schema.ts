c38beb72c93cd2bcb12f9317e949ecca29063d13
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-03-28 21:51:10 -0400

    add schema

---
+++ frontend/src/scenes/automations/schema.ts
@@ -0,0 +1,34 @@
+export enum AutomationStepKind {
+    // Sources
+    EventSource = 'EventSource',
+
+    // Business Logic
+    // e.g. Delay, Branch
+
+    // Destinations
+    WebhookDestination = 'WebhookDestination',
+}
+
+export type AutomationEventSourceStep = {
+    kind: AutomationStepKind.EventSource
+    // event
+    // filters
+}
+
+export type AutomationWebhookDestinationStep = {
+    kind: AutomationStepKind.WebhookDestination
+    url: string
+}
+
+export type AnyAutomationStep = AutomationEventSourceStep
+
+export type AutomationEdge = {
+    from: AnyAutomationStep
+    to: AnyAutomationStep
+}
+
+export type Automation = {
+    name: string
+    steps: AnyAutomationStep[]
+    edges: AutomationEdge[]
+}
