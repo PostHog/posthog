27e960f25f03b788b96e14df0f75b97c24d6bcd7
Luke Harries <lharries@users.noreply.github.com>
2023-03-29 12:09:49 -0400

    temp

--- frontend/src/scenes/automations/automationStepConfigLogic.tsx
+++ frontend/src/scenes/automations/automationStepConfigLogic.tsx
@@ -1,26 +1,109 @@
-import { actions, kea, path, reducers } from 'kea'
-import { automationStepConfigLogicType } from './automationStepConfigLogicType'
-import { AnyAutomationStep } from './schema'
+import { actions, kea, path, reducers, selectors } from 'kea'
+import type { automationStepConfigLogicType } from './automationStepConfigLogicType'
+import { AnyAutomationStep, AutomationStepCategory, AutomationStepConfigType, AutomationStepKind } from './schema'
+
+import {
+    GithubIcon,
+    IconAction,
+    IconApps,
+    IconArticle,
+    IconCoffee,
+    IconCohort,
+    IconEvent,
+    IconFlag,
+    IconMonitor,
+    IconPerson,
+    IconSlack,
+    IconWebhook,
+} from 'lib/lemon-ui/icons'
+
+const stepOptions: AnyAutomationStep[] = [
+    { kind: AutomationStepKind.EventSource, id: 'Event sent', category: AutomationStepCategory.Source },
+    { kind: AutomationStepKind.ActionSource, id: 'Action triggered', category: AutomationStepCategory.Source },
+    { kind: AutomationStepKind.PauseForLogic, id: 'Pause for', category: AutomationStepCategory.Logic },
+    { kind: AutomationStepKind.PauseUntilLogic, id: 'Pause until', category: AutomationStepCategory.Logic },
+    {
+        kind: AutomationStepKind.GithubIssueDestination,
+        id: 'Create a Github ticket',
+        category: AutomationStepCategory.Destination,
+    },
+    {
+        kind: AutomationStepKind.UserPropertyDestination,
+        id: 'Set user property',
+        category: AutomationStepCategory.Destination,
+    },
+    { kind: AutomationStepKind.CohortDestination, id: 'Add to cohort', category: AutomationStepCategory.Destination },
+    {
+        kind: AutomationStepKind.FeatureFlagDestination,
+        id: 'Add to feature flags',
+        category: AutomationStepCategory.Destination,
+    },
+    { kind: AutomationStepKind.WebhookDestination, id: 'Send a webhook', category: AutomationStepCategory.Destination },
+    { kind: AutomationStepKind.SlackDestination, id: 'Send to slack', category: AutomationStepCategory.Destination },
+    { kind: AutomationStepKind.ZapierDestination, id: 'Send to Zapier', category: AutomationStepCategory.Destination },
+    { kind: AutomationStepKind.EmailDestination, id: 'Send an email', category: AutomationStepCategory.Destination },
+    {
+        kind: AutomationStepKind.InAppMessageDestination,
+        id: 'In-app message',
+        category: AutomationStepCategory.Destination,
+    },
+]
+
+export const kindToConfig: Record<string, AutomationStepConfigType> = {
+    'Event sent': { icon: <IconEvent />, label: 'Event sent' },
+    'Action triggered': { icon: <IconAction />, label: 'Action triggered' },
+    'Pause for': { icon: <IconCoffee />, label: 'Pause for' },
+    'Pause until': { icon: <IconCoffee />, label: 'Pause until' },
+    'Create a Github ticket': { icon: <GithubIcon />, label: 'Create a Github ticket' },
+    'Set user property': { icon: <IconPerson />, label: 'Set user property' },
+    'Add to cohort': { icon: <IconCohort />, label: 'Add to cohort' },
+    'Add to feature flags': { icon: <IconFlag />, label: 'Add to feature flags' },
+    'Send a webhook': { icon: <IconWebhook />, label: 'Send a webhook' },
+    'Send to slack': { icon: <IconSlack />, label: 'Send to slack' },
+    'Send to Zapier': { icon: <IconApps />, label: 'Send to Zapier' },
+    'Send an email': { icon: <IconArticle />, label: 'Send an email' },
+    'In-app message': { icon: <IconMonitor />, label: 'In-app message' },
+}
 
 export const automationStepConfigLogic = kea<automationStepConfigLogicType>([
     path(['scenes', 'automations', 'automationStepConfigLogic']),
     actions({
         openStepConfig: true,
         closeStepConfig: true,
-        setStep: (step: AnyAutomationStep) => ({ step }),
+        setActiveStepId: (id: string) => ({ id }),
     }),
     reducers({
         stepConfigOpen: [
-            true as boolean,
+            false as boolean,
             {
                 openStepConfig: () => true,
                 closeStepConfig: () => false,
             },
         ],
-        step: [
-            null as null | AnyAutomationStep,
+        activeStepId: [
+            null as null | string,
             {
-                setStep: (_, { step }) => step,
+                setActiveStepId: (_, { id }) => id,
+                closeStepConfig: () => null,
+            },
+        ],
+        stepOptions: [stepOptions as AnyAutomationStep[], {}],
+        stepCategories: [Object.values(AutomationStepCategory), {}],
+    }),
+    selectors({
+        activeStep: [
+            (selectors) => [selectors.activeStepId],
+            (activeStepId: string): AnyAutomationStep | null => {
+                return stepOptions.find((step) => step.id === activeStepId) || null
+            },
+        ],
+        activeStepConfig: [
+            (selectors) => [selectors.activeStep],
+            (activeStep: AnyAutomationStep | null): AutomationStepConfigType | null => {
+                if (!activeStep) {
+                    return null
+                }
+                return kindToConfig[activeStep.id]
             },
         ],
     }),
--- frontend/src/scenes/automations/hooks/usePlaceholderClick.ts
+++ frontend/src/scenes/automations/hooks/usePlaceholderClick.ts
@@ -2,13 +2,14 @@ import { automationStepConfigLogic } from './../automationStepConfigLogic'
 import { useActions } from 'kea'
 import { NodeProps, useReactFlow } from 'reactflow'
 
-import { uuid, randomLabel } from '../utils'
+// import { uuid, randomLabel } from '../utils'
 
 // this hook implements the logic for clicking a placeholder node
 // on placeholder node click: turn the placeholder and connecting edge into a workflow node
 export function usePlaceholderClick(id: NodeProps['id']): () => void {
     const { openStepConfig } = useActions(automationStepConfigLogic)
-    const { getNode, setNodes, setEdges } = useReactFlow()
+    const { getNode } = useReactFlow()
+    // const { getNode, setNodes, setEdges } = useReactFlow()
 
     const onClick = (): void => {
         // we need the parent node object for getting its position
@@ -20,59 +21,59 @@ export function usePlaceholderClick(id: NodeProps['id']): () => void {
 
         openStepConfig()
 
-        // create a unique id for the placeholder node that will be added as a child of the clicked node
-        const childPlaceholderId = uuid()
+        // // create a unique id for the placeholder node that will be added as a child of the clicked node
+        // const childPlaceholderId = uuid()
 
-        // create a placeholder node that will be added as a child of the clicked node
-        const childPlaceholderNode = {
-            id: childPlaceholderId,
-            // the placeholder is placed at the position of the clicked node
-            // the layout function will animate it to its new position
-            position: { x: parentNode.position.x, y: parentNode.position.y },
-            type: 'placeholder',
-            data: { label: '+' },
-        }
+        // // create a placeholder node that will be added as a child of the clicked node
+        // const childPlaceholderNode = {
+        //     id: childPlaceholderId,
+        //     // the placeholder is placed at the position of the clicked node
+        //     // the layout function will animate it to its new position
+        //     position: { x: parentNode.position.x, y: parentNode.position.y },
+        //     type: 'placeholder',
+        //     data: { label: '+' },
+        // }
 
-        // we need a connection from the clicked node to the new placeholder
-        const childPlaceholderEdge = {
-            id: `${parentNode.id}=>${childPlaceholderId}`,
-            source: parentNode.id,
-            target: childPlaceholderId,
-            type: 'placeholder',
-        }
+        // // we need a connection from the clicked node to the new placeholder
+        // const childPlaceholderEdge = {
+        //     id: `${parentNode.id}=>${childPlaceholderId}`,
+        //     source: parentNode.id,
+        //     target: childPlaceholderId,
+        //     type: 'placeholder',
+        // }
 
-        setNodes((nodes) =>
-            nodes
-                .map((node) => {
-                    // here we are changing the type of the clicked node from placeholder to workflow
-                    if (node.id === id) {
-                        return {
-                            ...node,
-                            type: 'workflow',
-                            data: { label: randomLabel() },
-                        }
-                    }
-                    return node
-                })
-                // add the new placeholder node
-                .concat([childPlaceholderNode])
-        )
+        // setNodes((nodes) =>
+        //     nodes
+        //         .map((node) => {
+        //             // here we are changing the type of the clicked node from placeholder to workflow
+        //             if (node.id === id) {
+        //                 return {
+        //                     ...node,
+        //                     type: 'workflow',
+        //                     data: { label: randomLabel() },
+        //                 }
+        //             }
+        //             return node
+        //         })
+        //         // add the new placeholder node
+        //         .concat([childPlaceholderNode])
+        // )
 
-        setEdges((edges) =>
-            edges
-                .map((edge) => {
-                    // here we are changing the type of the connecting edge from placeholder to workflow
-                    if (edge.target === id) {
-                        return {
-                            ...edge,
-                            type: 'workflow',
-                        }
-                    }
-                    return edge
-                })
-                // add the new placeholder edge
-                .concat([childPlaceholderEdge])
-        )
+        // setEdges((edges) =>
+        //     edges
+        //         .map((edge) => {
+        //             // here we are changing the type of the connecting edge from placeholder to workflow
+        //             if (edge.target === id) {
+        //                 return {
+        //                     ...edge,
+        //                     type: 'workflow',
+        //                 }
+        //             }
+        //             return edge
+        //         })
+        //         // add the new placeholder edge
+        //         .concat([childPlaceholderEdge])
+        // )
     }
 
     return onClick
