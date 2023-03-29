d84ad6329a9fcf16341b551b0c647a2a61915ff9
Paul D'Ambra <paul@posthog.com>
2023-03-29 18:03:06 -0400

    a little UI fangling

--- frontend/src/scenes/events/CommunicationDetails.tsx
+++ frontend/src/scenes/events/CommunicationDetails.tsx
@@ -1,4 +1,4 @@
-import { LemonTextArea } from 'lib/lemon-ui/LemonTextArea/LemonTextArea'
+import { LemonTextArea, LemonTextMarkdown } from 'lib/lemon-ui/LemonTextArea/LemonTextArea'
 import { communicationDetailsLogic } from './CommunicationDetailsLogic'
 import { useActions, useValues } from 'kea'
 import { IconComment, IconMail } from 'lib/lemon-ui/icons/icons'
@@ -10,7 +10,7 @@ export function CommunicationDetails({ uuid }: { uuid: string }): JSX.Element {
     const { setReplyType, saveNote, setNoteContent } = useActions(communicationDetailsLogic({ eventUUID: uuid }))
 
     return (
-        <>
+        <div className={'flex flex-col space-y-2 p-4'}>
             <LemonSegmentedButton
                 onChange={(value) => setReplyType(value)}
                 options={[
@@ -27,25 +27,36 @@ export function CommunicationDetails({ uuid }: { uuid: string }): JSX.Element {
                 ]}
                 value={replyType}
             />
-            <LemonTextArea
-                id="user-context reply"
-                className="definition-popover-edit-form-value"
-                autoFocus
-                placeholder={publicReplyEnabled ? 'Public reply to the customer via email' : 'Internal note'}
-                value={noteContent}
-                onChange={(noteContent) => setNoteContent(noteContent)}
-                minRows={3}
-                maxRows={20}
-                data-attr="support-reply-or-internal-note"
-            />
-            <LemonButton
-                icon={publicReplyEnabled ? <IconMail /> : <IconComment />}
-                onClick={() => saveNote(noteContent)} // TODO: that should probably just pull the content in the logic side
-                tooltip={publicReplyEnabled ? 'Send an email to the customer' : 'Save internal note'}
-            >
-                {publicReplyEnabled ? 'Send email' : 'Save internal note'}
-            </LemonButton>
+            {publicReplyEnabled ? (
+                <LemonTextArea
+                    autoFocus
+                    placeholder={publicReplyEnabled ? 'Public reply to the customer via email' : 'Internal note'}
+                    value={noteContent}
+                    onChange={(noteContent) => setNoteContent(noteContent)}
+                    minRows={3}
+                    maxRows={20}
+                    data-attr={'support-reply-email-text'}
+                />
+            ) : (
+                <LemonTextMarkdown
+                    value={noteContent}
+                    onChange={(noteContent) => setNoteContent(noteContent)}
+                    data-attr={"'support-reply-internal-note-text'"}
+                />
+            )}
+            <div className="flex flex-row justify-end">
+                <LemonButton
+                    status="primary"
+                    type={'primary'}
+                    icon={publicReplyEnabled ? <IconMail /> : <IconComment />}
+                    onClick={() => saveNote(noteContent)} // TODO: that should probably just pull the content in the logic side
+                    tooltip={publicReplyEnabled ? 'Send an email to the customer' : 'Save internal note'}
+                    fullWidth={false}
+                >
+                    {publicReplyEnabled ? 'Send email' : 'Save internal note'}
+                </LemonButton>
+            </div>
             {/* TODO: pull all the historical communication, i.e. load events in the logic first */}
-        </>
+        </div>
     )
 }
