8ab496ddf68179ee2c71fe6a009797eeb1a17c77
Paul D'Ambra <paul@posthog.com>
2023-03-28 22:03:56 -0400

    add some queries to the page

--- frontend/src/scenes/issues/Issues.tsx
+++ frontend/src/scenes/issues/Issues.tsx
@@ -1,3 +1,68 @@
+import { LemonDivider, LemonTag } from '@posthog/lemon-ui'
+import { PageHeader } from 'lib/components/PageHeader'
+import { SceneExport } from 'scenes/sceneTypes'
+import { Query } from '~/queries/Query/Query'
+import { NodeKind } from '~/queries/schema'
+import { ChartDisplayType, InsightType } from '~/types'
+
 export const Issues = (): JSX.Element => {
-    return <>I R YOUR ISSUES</>
+    return (
+        <div>
+            <PageHeader
+                title={
+                    <div className="flex items-center gap-2">
+                        Issues
+                        <LemonTag type="warning" className="uppercase">
+                            BETA
+                        </LemonTag>
+                    </div>
+                }
+                buttons={undefined}
+            />
+
+            <LemonDivider />
+            <Query
+                query={{
+                    kind: NodeKind.LegacyQuery,
+                    filters: {
+                        insight: InsightType.TRENDS,
+                        date_from: '-7d',
+                        events: [
+                            {
+                                id: '$pageview',
+                                math: 'avg_count_per_actor',
+                                name: '$pageview',
+                                type: 'events',
+                                order: 0,
+                            },
+                        ],
+                        display: ChartDisplayType.ActionsLineGraph,
+                        interval: 'day',
+                    },
+                }}
+            />
+            <LemonDivider />
+            <Query
+                query={{
+                    kind: NodeKind.DataTableNode,
+                    full: true,
+                    showOpenEditorButton: false,
+                    source: {
+                        kind: NodeKind.EventsQuery,
+                        select: ['*', 'person', 'timestamp'],
+                        properties: [],
+                        fixedProperties: [
+                            // todo fix by defined user event/action
+                        ],
+                        after: '-24h',
+                        limit: 100,
+                    },
+                }}
+            />
+        </div>
+    )
+}
+
+export const scene: SceneExport = {
+    component: Issues,
 }
