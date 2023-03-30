c95bb1f4fe96919fe8a6a73cbf70e3d6f3e4c397
Paul D'Ambra <paul@posthog.com>
2023-03-30 10:42:05 -0400

    feat(aruba): listing messages for issues (#14928)
    * feat(aruba): simple API for person communications by bug id
    
    * Update query snapshots
    
    * a little UI fangling
    
    * sending communications
    
    * draw the rest of the owl
    
    * wip styling
    
    * more wip styling
    
    * Update query snapshots
    
    * nicerer layout
    
    * nicerer layout
    
    * nicerer layout
    
    * some types on things
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * eagerly update the frontend on send
    
    * remove bad comment
    
    * 7 days of message history
    
    * breathing space
    
    * breathing space
    
    * bar displays when line doesn't 🤷
    
    * a little space for large messages
    
    * skip test that passes locally but not in CI
    
    * Update query snapshots
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/scenes/events/CommunicationDetailsLogic.ts
+++ frontend/src/scenes/events/CommunicationDetailsLogic.ts
@@ -1,21 +1,67 @@
-import { actions, kea, key, listeners, path, props, reducers, selectors } from 'kea'
+import { actions, connect, kea, key, listeners, path, props, reducers, selectors } from 'kea'
 
 import type { communicationDetailsLogicType } from './CommunicationDetailsLogicType'
+import { teamLogic } from 'scenes/teamLogic'
+import PostHog from 'posthog-js-lite'
+import { userLogic } from 'scenes/userLogic'
+import { loaders } from 'kea-loaders'
+import api from 'lib/api'
 
 export interface CommunicationDetailsLogicProps {
     eventUUID: string | null
+}
+
+export interface CommunicationMessage {
+    event?: string
+    body_plain: string
+    body_html?: string
+    subject: string
+    from: string
+    to?: string
+    bug_report_uuid: string
+    timestamp?: string
+}
+
+export interface CommunicationResponseItem extends CommunicationMessage {
+    event: string
+}
+
+export interface CommunicationResponse {
+    results: CommunicationResponseItem[]
 }
 
 export const communicationDetailsLogic = kea<communicationDetailsLogicType>([
     path(['scenes', 'events', 'communicationDetailsLogic']),
     props({ eventUUID: null } as CommunicationDetailsLogicProps),
     key((props) => `communicationDetailsLogic-${props.eventUUID}`),
+    connect({
+        values: [teamLogic, ['currentTeam'], userLogic, ['user']],
+    }),
     actions({
         saveNote: (content: string) => ({ content }),
         setReplyType: (type: 'internal' | 'public') => ({ type }),
         setNoteContent: (content: string) => ({ content }),
+        sentSuccessfully: (message: CommunicationResponseItem) => ({ message }),
+        sendingFailed: true,
     }),
+    loaders(({ props }) => ({
+        communications: {
+            loadCommunications: async () => {
+                return await api.personCommunications.list({
+                    bug_report_uuid: props.eventUUID,
+                })
+            },
+        },
+    })),
     reducers({
+        communications: {
+            sentSuccessfully: (state, { message }) => {
+                return {
+                    ...state,
+                    results: [{ ...message, timestamp: new Date(Date.now()).toISOString() }, ...state.results],
+                }
+            },
+        },
         replyType: [
             'internal' as 'internal' | 'public',
             {
@@ -26,18 +72,49 @@ export const communicationDetailsLogic = kea<communicationDetailsLogicType>([
             '',
             {
                 setNoteContent: (_, { content }) => content,
+                sentSuccessfully: () => '',
             },
         ],
     }),
     selectors({
         publicReplyEnabled: [(s) => [s.replyType], (type) => type === 'public'],
+        posthogSDK: [
+            (s) => [s.currentTeam],
+            (currentTeam) => {
+                if (!currentTeam) {
+                    return null
+                }
+                return new PostHog(currentTeam.api_token, {
+                    host: window.JS_POSTHOG_HOST || 'https://app.posthog.com',
+                    enable: true,
+                    persistence: 'memory', // We don't want to persist anything, all events are in-memory
+                    persistence_name: currentTeam.id + '_communications_sdk',
+                })
+            },
+        ],
     }),
-    listeners({
+    listeners(({ values, actions, props }) => ({
         saveNote: async ({ content }: { content: string }) => {
             // TODO: tests to make sure we don't send emails when toggle is off and do when on
-            console.log(content)
             // TODO: make it send an event to PostHog - should be whatever team we're on ??? -
-            // and normally we need to hide these events???
+            const event = values.publicReplyEnabled ? '$communication_email_sent' : '$communication_note_saved'
+
+            if (values.posthogSDK && props.eventUUID) {
+                const messageProperties: CommunicationMessage = {
+                    ...{
+                        body_plain: content,
+                        subject: `HogDesk Bug Report [${props.eventUUID}]`,
+                        from: values.user?.email || 'support agent',
+                        bug_report_uuid: props.eventUUID,
+                    },
+                    ...(values.publicReplyEnabled ? { to: values.user?.email || 'unknown' } : {}),
+                }
+
+                values.posthogSDK.capture(event, messageProperties)
+                actions.sentSuccessfully({ ...messageProperties, event })
+            } else {
+                actions.sendingFailed()
+            }
         },
-    }),
+    })),
 ])
