9a2c10710a54d1453a1adea2b708324b8cd63466
Luke Harries <lharries@users.noreply.github.com>
2023-03-30 11:12:10 -0400

    polish

--- frontend/src/scenes/automations/AutomationStepSidebar/automationStepMenuLogic.ts
+++ frontend/src/scenes/automations/AutomationStepSidebar/automationStepMenuLogic.ts
@@ -1,9 +1,13 @@
-import { actions, kea, path, reducers } from 'kea'
+import { actions, connect, kea, listeners, path, reducers } from 'kea'
+import { automationStepConfigLogic } from './automationStepConfigLogic'
 
 import type { automationStepMenuLogicType } from './automationStepMenuLogicType'
 
 export const automationStepMenuLogic = kea<automationStepMenuLogicType>([
     path(['scenes', 'automations', 'AutomationStepSidebard', 'automationStepMenuLogic']),
+    connect({
+        actions: [automationStepConfigLogic, ['setActiveStepId']],
+    }),
     actions({
         openMenu: true,
         closeMenu: true,
@@ -17,4 +21,10 @@ export const automationStepMenuLogic = kea<automationStepMenuLogicType>([
             },
         ],
     }),
+    // connect to automationStepConfigLogic to set the active step as null when a menu is opened
+    listeners(({ actions }) => ({
+        openMenu: () => {
+            actions.setActiveStepId(null)
+        },
+    })),
 ])
