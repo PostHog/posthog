7bce235e911c54fa2ca9644e5864825a4844b2ef
Luke Harries <lharries@users.noreply.github.com>
2023-03-30 11:33:33 -0400

    fix circular dependency

--- frontend/src/scenes/automations/AutomationStepSidebar/automationStepMenuLogic.ts
+++ frontend/src/scenes/automations/AutomationStepSidebar/automationStepMenuLogic.ts
@@ -1,13 +1,9 @@
-import { actions, connect, kea, listeners, path, reducers } from 'kea'
-import { automationStepConfigLogic } from './automationStepConfigLogic'
+import { actions, kea, path, reducers } from 'kea'
 
 import type { automationStepMenuLogicType } from './automationStepMenuLogicType'
 
 export const automationStepMenuLogic = kea<automationStepMenuLogicType>([
     path(['scenes', 'automations', 'AutomationStepSidebard', 'automationStepMenuLogic']),
-    connect({
-        actions: [automationStepConfigLogic, ['setActiveStepId']],
-    }),
     actions({
         openMenu: true,
         closeMenu: true,
@@ -21,10 +17,4 @@ export const automationStepMenuLogic = kea<automationStepMenuLogicType>([
             },
         ],
     }),
-    // connect to automationStepConfigLogic to set the active step as null when a menu is opened
-    listeners(({ actions }) => ({
-        openMenu: () => {
-            actions.setActiveStepId(null)
-        },
-    })),
 ])
