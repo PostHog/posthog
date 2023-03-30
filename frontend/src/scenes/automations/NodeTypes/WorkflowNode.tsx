4bdc69e160c7e4f97a90ce7c0c32bd6200993592
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-03-30 00:19:52 -0400

    styling

--- frontend/src/scenes/automations/NodeTypes/WorkflowNode.tsx
+++ frontend/src/scenes/automations/NodeTypes/WorkflowNode.tsx
@@ -5,10 +5,30 @@ import './NodeTypes.scss'
 import useNodeClickHandler from '../hooks/useNodeClick'
 import { AnyAutomationStep, AutomationStepKind } from '../schema'
 import { kindToConfig } from '../AutomationStepSidebar/automationStepConfigLogic'
+import { PropertyFilters } from 'lib/components/PropertyFilters/PropertyFilters'
+import { PropertyOperator } from '~/types'
+import { PropertyKeyInfo } from 'lib/components/PropertyKeyInfo'
 
-const summarizeWorflowStep = (step: AnyAutomationStep): string => {
+const renderBodyForStep = (step: AnyAutomationStep): React.ReactNode => {
     if (step.kind === AutomationStepKind.EventSource) {
-        return 'event'
+        if (!step.filters || step.filters.length === 0) {
+            return <span className="italic">Not setup.</span>
+        }
+
+        return (
+            <div className="">
+                {step.filters.map((f) => (
+                    <div className="mb-1">
+                        <span className="mr-1">Triggers on</span>
+                        <PropertyKeyInfo
+                            className="p-1 bg-primary-highlight rounded-sm"
+                            value={f.name}
+                            disablePopover
+                        />
+                    </div>
+                ))}
+            </div>
+        )
     } else if (step.kind === AutomationStepKind.WebhookDestination) {
         return 'webhook'
     }
@@ -24,10 +44,10 @@ const WorkflowNode = ({ id, data }: NodeProps<AnyAutomationStep>): JSX.Element =
     return (
         <div onClick={onClick} className="bg-white p-3 w-60" title="click to add a child node">
             <div className="font-medium border-b border-primary-highlight pb-1 mb-2">
-                <span className="mr-0.5 text-primary-light">{kindToConfig[data.kind].icon}</span>{' '}
+                <span className="mr-1 text-primary-light">{kindToConfig[data.kind].icon}</span>
                 <span>{kindToConfig[data.kind].label}</span>
             </div>
-            <div>{summarizeWorflowStep(data)}</div>
+            {renderBodyForStep(data)}
             <Handle className="handle" type="target" position={Position.Top} isConnectable={false} />
             <Handle className="handle" type="source" position={Position.Bottom} isConnectable={false} />
         </div>
