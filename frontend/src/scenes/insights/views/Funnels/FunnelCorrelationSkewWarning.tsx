90df0a05eedd892b3c4578f12c00062a6cb05ca4
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-04-03 13:39:24 -0400

    separate funnel correlation into sub-components

---
+++ frontend/src/scenes/insights/views/Funnels/FunnelCorrelationSkewWarning.tsx
@@ -0,0 +1,38 @@
+import { useActions, useValues } from 'kea'
+import { Card } from 'antd'
+
+import { insightLogic } from 'scenes/insights/insightLogic'
+import { funnelLogic } from 'scenes/funnels/funnelLogic'
+
+import { IconFeedbackWarning } from 'lib/lemon-ui/icons'
+import { CloseOutlined } from '@ant-design/icons'
+
+export const FunnelCorrelationSkewWarning = (): JSX.Element | null => {
+    const { insightProps } = useValues(insightLogic)
+    const { isSkewed } = useValues(funnelLogic(insightProps))
+    const { hideSkewWarning } = useActions(funnelLogic(insightProps))
+
+    if (!isSkewed) {
+        return null
+    }
+
+    return (
+        <Card className="skew-warning">
+            <h4>
+                <IconFeedbackWarning style={{ fontSize: 24, marginRight: 4, color: 'var(--warning)' }} /> Adjust your
+                funnel definition to improve correlation analysis
+                <CloseOutlined className="close-button" onClick={hideSkewWarning} />
+            </h4>
+            <div>
+                <b>Tips for adjusting your funnel:</b>
+                <ol>
+                    <li>
+                        Adjust your first funnel step to be more specific. For example, choose a page or an event that
+                        occurs less frequently.
+                    </li>
+                    <li>Choose an event that happens more frequently for subsequent funnels steps.</li>
+                </ol>
+            </div>
+        </Card>
+    )
+}
