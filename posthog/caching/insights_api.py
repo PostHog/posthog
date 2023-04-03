84f4314b0702e19a1924247bc0c9edfeb2067864
Michael Matloka <dev@twixes.com>
2023-04-03 20:05:07 +0200

    fix(insights): Refresh shared insights/dashboards to prevent stale data

--- posthog/caching/insights_api.py
+++ posthog/caching/insights_api.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from math import ceil
 from typing import Optional, Tuple, Union
+from rest_framework import request
 
 import pytz
 
@@ -8,6 +9,7 @@ from posthog.caching.calculate_results import calculate_cache_key
 from posthog.caching.insight_caching_state import InsightCachingState
 from posthog.models import DashboardTile, Insight
 from posthog.models.filters.utils import get_filter
+from posthog.utils import refresh_requested_by_client
 
 """
 Utilities used by the insights API to determine whether
@@ -19,7 +21,9 @@ DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY = timedelta(minutes=15)
 
 
 # returns should_refresh, refresh_frequency
-def should_refresh_insight(insight: Insight, dashboard_tile: Optional[DashboardTile]) -> Tuple[bool, timedelta]:
+def should_refresh_insight(
+    insight: Insight, dashboard_tile: Optional[DashboardTile], *, request: request.Request, is_shared=False
+) -> Tuple[bool, timedelta]:
     filter = get_filter(
         data=insight.dashboard_filters(dashboard_tile.dashboard if dashboard_tile is not None else None),
         team=insight.team,
@@ -29,18 +33,24 @@ def should_refresh_insight(insight: Insight, dashboard_tile: Optional[DashboardT
     cache_key = calculate_cache_key(target)
     caching_state = InsightCachingState.objects.filter(team_id=insight.team.pk, cache_key=cache_key, insight=insight)
 
-    refresh_frequency = DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY
-
     delta_days: Optional[int] = None
-
     if filter.date_from and filter.date_to:
         delta = filter.date_to - filter.date_from
         delta_days = ceil(delta.total_seconds() / timedelta(days=1).total_seconds())
 
+    refresh_frequency = DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY
     if (hasattr(filter, "interval") and filter.interval == "hour") or (delta_days is not None and delta_days <= 7):
         refresh_frequency = timedelta(minutes=3)
 
+    refresh_insight_now = False
     if len(caching_state) == 0 or caching_state[0].last_refresh is None:
-        return True, refresh_frequency
+        # Always refresh if there are no cached results
+        refresh_insight_now = True
+    elif (refresh_requested_by_client(request) or is_shared) and (
+        caching_state[0].last_refresh + refresh_frequency <= datetime.now(tz=pytz.timezone("UTC"))
+    ):
+        # Also refresh if the user has requested and refresh and enough time has passed since last refresh
+        # We treat loading a shared dashboard/insight as a refresh request
+        refresh_insight_now = True
 
-    return caching_state[0].last_refresh + refresh_frequency <= datetime.now(tz=pytz.timezone("UTC")), refresh_frequency
+    return refresh_insight_now, refresh_frequency
--- posthog/caching/test/test_insight_api.py
+++ posthog/caching/test/test_insight_api.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta
+from django.http import HttpRequest
 
 import pytz
 from freezegun import freeze_time
-
+from rest_framework.request import Request
 from posthog.caching.insight_caching_state import InsightCachingState
 from posthog.caching.insights_api import DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY, should_refresh_insight
 from posthog.test.base import BaseTest, ClickhouseTestMixin, _create_insight
@@ -10,44 +11,94 @@ from posthog.test.base import BaseTest, ClickhouseTestMixin, _create_insight
 
 @freeze_time("2012-01-14T03:21:34.000Z")
 class TestInsightAPI(ClickhouseTestMixin, BaseTest):
-    @freeze_time("2012-01-14T03:21:34.000Z")
-    def test_should_refresh_insight(self) -> None:
-        # should_refresh_now should always be true if the insight doesn't have last_refresh
+    def test_should_refresh_now_should_always_be_true_if_the_insight_doesnt_have_last_refresh(self):
         insight, _, _ = _create_insight(self.team, {"events": [{"id": "$pageview"}], "interval": "month"}, {})
-        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None)
+        request = HttpRequest()
+
+        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None, request=Request(request))
         self.assertEqual(should_refresh_now, True)
         self.assertEqual(refresh_frequency, DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY)
 
-        # dashboard filters override insight filters when deciding on refresh time
+    def test_dashboard_filters_should_override_insight_filters_when_deciding_on_refresh_time(self):
         insight, _, dashboard_tile = _create_insight(
             self.team, {"events": [{"id": "$pageview"}], "interval": "month"}, {"interval": "hour"}
         )
-        should_refresh_now, refresh_frequency = should_refresh_insight(insight, dashboard_tile)
+        request = HttpRequest()
+
+        should_refresh_now, refresh_frequency = should_refresh_insight(
+            insight, dashboard_tile, request=Request(request)
+        )
+
         self.assertEqual(should_refresh_now, True)
         self.assertEqual(refresh_frequency, timedelta(minutes=3))
 
-        # insights with hour intervals can be refreshed more often
+    def test_insights_with_hour_intervals_can_be_refreshed_more_often(self):
         insight, _, _ = _create_insight(self.team, {"events": [{"id": "$pageview"}], "interval": "hour"}, {})
+        request = HttpRequest()
 
-        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None)
+        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None, request=Request(request))
+
         self.assertEqual(should_refresh_now, True)
         self.assertEqual(refresh_frequency, timedelta(minutes=3))
 
-        # insight with ranges equal or lower than 7 days can also be refreshed more often
+    def test_insights_with_ranges_lower_than_7_days_can_be_refreshed_more_often(self):
         insight, _, _ = _create_insight(
             self.team, {"events": [{"id": "$pageview"}], "interval": "day", "date_from": "-3d"}, {}
         )
+        request = HttpRequest()
 
-        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None)
+        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None, request=Request(request))
+
         self.assertEqual(should_refresh_now, True)
         self.assertEqual(refresh_frequency, timedelta(minutes=3))
 
-        # insights recently refreshed should return False for should_refresh_now
+    def test_insights_recently_refreshed_should_return_false_for_should_refresh_now(self):
         insight, _, _ = _create_insight(self.team, {"events": [{"id": "$autocapture"}], "interval": "month"}, {})
         InsightCachingState.objects.filter(team=self.team, insight_id=insight.pk).update(
             last_refresh=datetime.now(tz=pytz.timezone("UTC"))
         )
+        request = HttpRequest()
 
-        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None)
+        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None, request=Request(request))
+
         self.assertEqual(should_refresh_now, False)
+        self.assertEqual(refresh_frequency, DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY)
+
+    def test_insights_without_refresh_requested_should_return_false_for_should_refresh_now(self):
+        insight, _, _ = _create_insight(self.team, {"events": [{"id": "$autocapture"}], "interval": "month"}, {})
+        InsightCachingState.objects.filter(team=self.team, insight_id=insight.pk).update(
+            last_refresh=datetime.now(tz=pytz.timezone("UTC")) - timedelta(days=1)
+        )
+        request = HttpRequest()
+
+        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None, request=Request(request))
+
+        self.assertEqual(should_refresh_now, False)
+        self.assertEqual(refresh_frequency, DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY)
+
+    def test_insights_with_refresh_requested_should_return_true_for_should_refresh_now(self):
+        insight, _, _ = _create_insight(self.team, {"events": [{"id": "$autocapture"}], "interval": "month"}, {})
+        InsightCachingState.objects.filter(team=self.team, insight_id=insight.pk).update(
+            last_refresh=datetime.now(tz=pytz.timezone("UTC")) - timedelta(days=1)
+        )
+        request = HttpRequest()
+        request.GET["refresh"] = "true"
+
+        should_refresh_now, refresh_frequency = should_refresh_insight(insight, None, request=Request(request))
+
+        self.assertEqual(should_refresh_now, True)
+        self.assertEqual(refresh_frequency, DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY)
+
+    def test_insights_without_refresh_requested_but_being_shared_should_return_true_for_should_refresh_now(self):
+        insight, _, _ = _create_insight(self.team, {"events": [{"id": "$autocapture"}], "interval": "month"}, {})
+        InsightCachingState.objects.filter(team=self.team, insight_id=insight.pk).update(
+            last_refresh=datetime.now(tz=pytz.timezone("UTC")) - timedelta(days=1)
+        )
+        request = HttpRequest()
+
+        should_refresh_now, refresh_frequency = should_refresh_insight(
+            insight, None, request=Request(request), is_shared=True
+        )
+
+        self.assertEqual(should_refresh_now, True)
         self.assertEqual(refresh_frequency, DEFAULT_CLIENT_INSIGHT_ALLOWED_REFRESH_FREQUENCY)
