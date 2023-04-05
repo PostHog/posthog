deec49d16330d1eee799351c209ee07d1c80f9a5
Neil Kakkar <neilkakkar@gmail.com>
2023-04-05 11:49:31 +0100

    fix

--- frontend/src/scenes/cohorts/CohortEdit.tsx
+++ frontend/src/scenes/cohorts/CohortEdit.tsx
@@ -134,9 +134,9 @@ export function CohortEdit({ id }: CohortLogicProps): JSX.Element {
                             {({ onChange }) => (
                                 <>
                                     <span>
-                                        Upload a CSV file to add users to your cohort. The CSV file requires a
-                                        single column of the user’s distinct IDs. The very first row (the header) will
-                                        be skipped during import.
+                                        Upload a CSV file to add users to your cohort. The CSV file requires a single
+                                        column of the user's distinct IDs. The very first row (the header) will be
+                                        skipped during import.
                                     </span>
                                     <Dragger
                                         name="file"
