4f913e05e1d195b7ba3401e90a310eaead787546
Michael Matloka <dev@twixes.com>
2023-04-06 14:31:15 +0200

    Rearrange conditions

--- posthog/caching/insights_api.py
+++ posthog/caching/insights_api.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from math import ceil
 from time import sleep
-from typing import Optional, Tuple, Union, cast
+from typing import Optional, Tuple, Union
 from rest_framework import request
 
 import pytz
@@ -60,36 +60,40 @@ def should_refresh_insight(
         refresh_frequency = INCREASED_MINIMUM_INSIGHT_REFRESH_INTERVAL
 
     refresh_insight_now = False
+    if caching_state is None or caching_state.last_refresh is None:
+        refresh_insight_now = True
     # Only refresh if the user has requested a refresh or if we're on a shared dashboard/insight
     # (those have no explicit way of reloading)
-    if refresh_requested_by_client(request) or is_shared:
-        are_there_cached_results = caching_state is not None and caching_state.last_refresh is not None
-        is_data_cached_but_stale = are_there_cached_results and (
-            cast(InsightCachingState, caching_state).last_refresh + refresh_frequency <= now
-        )
-        refresh_insight_now = (not are_there_cached_results) or is_data_cached_but_stale
+    elif refresh_requested_by_client(request) or is_shared:
+        refresh_insight_now = caching_state.last_refresh + refresh_frequency <= now
 
     # Check if the refresh is might already be running for this cache key - if so, let's wait until that's done
-    if refresh_insight_now and caching_state is not None and caching_state.last_refresh_queued_at is not None:
-        while (
-            # Refresh must have been queued recently enough that the query might still be running
-            caching_state.last_refresh_queued_at > now - timedelta(seconds=CLICKHOUSE_MAX_EXECUTION_TIME)
-            # Also, refreshing must have either never finished or last finished before it was queued now
-            and (
-                caching_state.last_refresh is None or caching_state.last_refresh < caching_state.last_refresh_queued_at
-            )
-        ):
-            # This looks a bit ugly because it blocks the thread, but it's BETTER than running the already-running query
-            # from this thread concurrently. Either way we have to wait for results, this way just hammers the DB less
-            sleep(3)
-            caching_state.refresh_from_db()
-            if (
-                caching_state.last_refresh is not None
-                and caching_state.last_refresh >= caching_state.last_refresh_queued_at
-            ):
-                refresh_insight_now = False
-                break  # Refreshed successfully!
-        # Otherwise we're sure the refresh isn't running at the moment - either it's not been queued or it's timed out
-        # (barring the occasional race condition related to fetching state from PG, but that much uncertainty is okay)
+    if refresh_insight_now and caching_state is not None:
+        refresh_insight_now = _wait_until_refresh_must_be_done(caching_state, now)
 
     return refresh_insight_now, refresh_frequency
+
+
+def _wait_until_refresh_must_be_done(caching_state: InsightCachingState, now: datetime) -> bool:
+    while (
+        # A refresh must have been queued at some point in the past
+        caching_state.last_refresh_queued_at is not None
+        # That point must be recent enough that the query might still be running
+        and caching_state.last_refresh_queued_at > now - timedelta(seconds=CLICKHOUSE_MAX_EXECUTION_TIME)
+        # Also, refreshing must have either never finished or last finished before it was queued now
+        and (caching_state.last_refresh is None or caching_state.last_refresh < caching_state.last_refresh_queued_at)
+    ):
+        # This looks a bit ugly because it blocks the thread, but it's BETTER than running the already-running query
+        # from this thread concurrently. Either way we have to wait for results, this way just hammers the DB less
+        sleep(3)
+        caching_state.refresh_from_db()
+        if (
+            caching_state.last_refresh is not None
+            and caching_state.last_refresh >= caching_state.last_refresh_queued_at
+        ):
+            # Refreshed successfully! Refresh is no longer needed
+            return False
+    else:
+        # Otherwise we're sure the refresh isn't running at the moment - either it's not been queued or it's timed out
+        # (barring the occasional race condition related to fetching state from PG, but that much uncertainty is okay)
+        return True
