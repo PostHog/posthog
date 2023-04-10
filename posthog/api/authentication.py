ffc2fb492f60605d8ccd400693047301c72e64ac
Charlotte McGinn <charlotte@posthog.com>
2023-04-10 14:34:10 -0700

    add cache logic for authentication.py

--- posthog/api/authentication.py
+++ posthog/api/authentication.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.tokens import default_token_generator
+from django.core.cache import cache
 from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.http import HttpRequest, HttpResponse, JsonResponse
@@ -257,10 +258,20 @@ class PasswordResetSerializer(serializers.Serializer):
         except User.DoesNotExist:
             user = None
 
+        cache_key = f"num_password_reset_requests{user.id}"
+
+        num_requests = cache.get(cache_key)
+        if num_requests is None:
+            cache.set(cache_key, 1, 60 * 60 * 24)  # number of seconds in 24 hours
+        elif num_requests < 3:  # limit the number of requests to 3 in 24 hours
+            cache.incr(cache_key, 1)
+        else:
+            raise serializers.ValidationError(
+                "Too many password reset requests. Please try again in 24 hours.", code="email_too_many_requests"
+            )
+
         if user:
             send_password_reset(user.id)
-
-        # TODO: Limit number of requests for password reset emails
 
         return True
 
