d8b57a6e1ac462dd671a08cbff3deffa9e6540e8
Xavier Vello <xavier@posthog.com>
2023-04-11 16:05:37 +0200

    update preview size limit

--- posthog/admin.py
+++ posthog/admin.py
@@ -80,6 +80,9 @@ class TeamAdmin(admin.ModelAdmin):
         )
 
 
+ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES = 1024 * 1024
+
+
 class PluginAttachmentInline(admin.StackedInline):
     extra = 0
     model = PluginAttachment
@@ -88,19 +91,23 @@ class PluginAttachmentInline(admin.StackedInline):
 
     def raw_contents(self, attachment: PluginAttachment):
         try:
-            if attachment.file_size > 1024 * 1024:
-                raise ValueError("too big")
+            if attachment.file_size > ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES:
+                raise ValueError(
+                    f"file size {attachment.file_size} is larger than {ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES} bytes"
+                )
             return attachment.contents.tobytes()
         except Exception as err:
-            return format_html(f"cannot preview: {err=}")
+            return format_html(f"cannot preview: {err}")
 
     def json_contents(self, attachment: PluginAttachment):
         try:
-            if attachment.file_size > 1024 * 1024:
-                raise ValueError("too big")
+            if attachment.file_size > ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES:
+                raise ValueError(
+                    f"file size {attachment.file_size} is larger than {ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES} bytes"
+                )
             return json.loads(attachment.contents.tobytes())
         except Exception as err:
-            return format_html(f"cannot preview: {err=}")
+            return format_html(f"cannot preview: {err}")
 
     def has_add_permission(self, request, obj):
         return False
