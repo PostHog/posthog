5d1da4de07e2b129ec518b759c4942d8e564639b
Michael Matloka <dev@twixes.com>
2023-04-11 19:48:10 +0200

    Update HogQLParserVisitor.py

--- posthog/hogql/grammar/HogQLParserVisitor.py
+++ posthog/hogql/grammar/HogQLParserVisitor.py
@@ -1,4 +1,4 @@
-# Generated from HogQLParser.g4 by ANTLR 4.12.0
+# Generated from HogQLParser.g4 by ANTLR 4.11.1
 from antlr4 import *
 if __name__ is not None and "." in __name__:
     from .HogQLParser import HogQLParser
