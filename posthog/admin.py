6150a2adc73cc1342179edaa331372b7dd654838
Xavier Vello <xavier.vello@gmail.com>
2023-04-11 16:25:47 +0200

    feat(admin): show PluginAttachment in PluginConfig pages (#15029)

--- posthog/admin.py
+++ posthog/admin.py
@@ -1,3 +1,5 @@
+import json
+
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
@@ -20,6 +22,7 @@ from posthog.models import (
     PluginConfig,
     Team,
     User,
+    PluginAttachment,
 )
 
 admin.site.register(Person)
@@ -77,6 +80,45 @@ class TeamAdmin(admin.ModelAdmin):
         )
 
 
+ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES = 1024 * 1024
+
+
+class PluginAttachmentInline(admin.StackedInline):
+    extra = 0
+    model = PluginAttachment
+    fields = ("key", "content_type", "file_size", "raw_contents", "json_contents")
+    readonly_fields = fields
+
+    def raw_contents(self, attachment: PluginAttachment):
+        try:
+            if attachment.file_size > ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES:
+                raise ValueError(
+                    f"file size {attachment.file_size} is larger than {ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES} bytes"
+                )
+            return attachment.contents.tobytes()
+        except Exception as err:
+            return format_html(f"cannot preview: {err}")
+
+    def json_contents(self, attachment: PluginAttachment):
+        try:
+            if attachment.file_size > ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES:
+                raise ValueError(
+                    f"file size {attachment.file_size} is larger than {ATTACHMENT_PREVIEW_SIZE_LIMIT_BYTES} bytes"
+                )
+            return json.loads(attachment.contents.tobytes())
+        except Exception as err:
+            return format_html(f"cannot preview: {err}")
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj):
+        return False
+
+    def has_delete_permission(self, request, obj):
+        return False
+
+
 @admin.register(PluginConfig)
 class PluginConfigAdmin(admin.ModelAdmin):
     list_select_related = ("plugin", "team")
@@ -88,6 +130,7 @@ class PluginConfigAdmin(admin.ModelAdmin):
     )
     search_fields = ("team__name", "team__organization__name", "plugin__name")
     ordering = ("-created_at",)
+    inlines = [PluginAttachmentInline]
 
     def plugin_name(self, config: PluginConfig):
         return format_html(f"{config.plugin.name} ({config.plugin_id})")
