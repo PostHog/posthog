2acdd7a6732f5972485c453760f72f4be36795a4
Neil Kakkar <neilkakkar@gmail.com>
2023-04-14 10:37:45 +0100

    fix: Make code examples nicer, fix some bugs (#15070)

--- frontend/src/scenes/feature-flags/FeatureFlagCodeExample.tsx
+++ frontend/src/scenes/feature-flags/FeatureFlagCodeExample.tsx
@@ -7,11 +7,7 @@ export function FeatureFlagCodeExample({ featureFlag }: { featureFlag: FeatureFl
             <h3>
                 <b>How to implement</b>
             </h3>
-            <FeatureFlagInstructions
-                newCodeExample={true}
-                featureFlag={featureFlag}
-                featureFlagKey={featureFlag.key || 'my-flag'}
-            />
+            <FeatureFlagInstructions featureFlag={featureFlag} />
         </div>
     )
 }
