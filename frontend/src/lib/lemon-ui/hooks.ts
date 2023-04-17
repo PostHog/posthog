074737aa7a3271b487fb15d04d538b9383fea2ae
Michael Matloka <dev@twixes.com>
2023-04-17 16:10:53 +0200

    ci(frontend): Fix `LemonTabs` causing UI snapshot flakiness

--- frontend/src/lib/lemon-ui/hooks.ts
+++ frontend/src/lib/lemon-ui/hooks.ts
@@ -6,17 +6,20 @@ import { useLayoutEffect, useRef, useState } from 'react'
  * @private
  */
 export function useSliderPositioning<C extends HTMLElement, S extends HTMLElement>(
-    currentValue: any
+    currentValue: string | number,
+    transitionMs: number
 ): {
     containerRef: React.RefObject<C>
     selectionRef: React.RefObject<S>
     sliderWidth: number
     sliderOffset: number
+    transitioning: boolean
 } {
     const containerRef = useRef<C>(null)
     const selectionRef = useRef<S>(null)
     const [selectionWidth, setSelectionWidth] = useState(0)
     const [selectionOffset, setSelectionOffset] = useState(0)
+    const [transitioning, setTransitioning] = useState(false)
     const { width: containerWidth } = useResizeObserver({ ref: containerRef })
 
     useLayoutEffect(() => {
@@ -25,6 +28,9 @@ export function useSliderPositioning<C extends HTMLElement, S extends HTMLElemen
             const { width, left: selectedOptionleft } = selectionRef.current.getBoundingClientRect()
             setSelectionWidth(width)
             setSelectionOffset(selectedOptionleft - containerLeft)
+            setTransitioning(true)
+            const transitioningTimeout = setTimeout(() => setTransitioning(false), transitionMs)
+            return () => clearTimeout(transitioningTimeout)
         }
     }, [currentValue, containerWidth])
 
@@ -33,5 +39,6 @@ export function useSliderPositioning<C extends HTMLElement, S extends HTMLElemen
         selectionRef,
         sliderWidth: selectionWidth,
         sliderOffset: selectionOffset,
+        transitioning,
     }
 }
