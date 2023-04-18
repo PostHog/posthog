bb556b5ec3bb6f3f2ac0a7822bf9fb87d1081ed9
Ellie Huxtable <ellie@posthog.com>
2023-04-18 12:26:32 +0100

    Add basic server (#3)
    * Add dependencies
    
    Tokio for async
    Axum for HTTP things
    Tracing for... tracing
    Serde for serialization
    
    * Setup simple hello world server
    
    * fmt

---
+++ src/capture.rs
@@ -0,0 +1,3 @@
+pub async fn root() -> &'static str {
+    "Hello, World!"
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+mod capture;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(capture::root));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
