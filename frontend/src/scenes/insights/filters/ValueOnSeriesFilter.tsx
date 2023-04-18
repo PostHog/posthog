792ec7d2992899576b824b5a7ec7069eb64deeda
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-04-18 15:44:02 +0200

    extract props type for values on series filter

--- frontend/src/scenes/insights/filters/ValueOnSeriesFilter.tsx
+++ frontend/src/scenes/insights/filters/ValueOnSeriesFilter.tsx
@@ -1,10 +1,12 @@
 import { LemonCheckbox } from 'lib/lemon-ui/LemonCheckbox'
 
-export function ValueOnSeriesFilter(props: { onChange: (checked: boolean) => void; checked: boolean }): JSX.Element {
+type ValuesOnSeriesFilterProps = { onChange: (checked: boolean) => void; checked: boolean }
+
+export function ValueOnSeriesFilter({ checked, onChange }: ValuesOnSeriesFilterProps): JSX.Element {
     return (
         <LemonCheckbox
-            onChange={props.onChange}
-            checked={props.checked}
+            onChange={onChange}
+            checked={checked}
             label={<span className="font-normal">Show values on series</span>}
             bordered
             size="small"
