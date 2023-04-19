98676103dd85554430ea793bcc9c9e181880c8f3
Harry Waye <harry@posthog.com>
2023-04-19 16:36:48 +0100

    fix: use roundrobin instead of range (#15159)

--- plugin-server/src/kafka/batch-consumer.ts
+++ plugin-server/src/kafka/batch-consumer.ts
@@ -65,6 +65,7 @@ export const startBatchConsumer = async ({
         'enable.partition.eof': true,
         'queued.min.messages': 100000, // 100000 is the default
         'queued.max.messages.kbytes': 1048576, // 1048576 is the default
+        'partition.assignment.strategy': 'roundrobin', // KafkaJS uses round robin, and we need to be compatible with it.
         rebalance_cb: true,
         offset_commit_cb: true,
     })
