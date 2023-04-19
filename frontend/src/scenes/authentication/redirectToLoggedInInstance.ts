c16f8e88802b2d8280c8b3f3dedb30a6d008c382
Luke Harries <lharries@users.noreply.github.com>
2023-04-19 10:42:41 +0100

    added check that they are actually logged in

--- frontend/src/scenes/authentication/redirectToLoggedInInstance.ts
+++ frontend/src/scenes/authentication/redirectToLoggedInInstance.ts
@@ -1,5 +1,23 @@
+/**
+ * To test this locally, you can edit your /etc/hosts to add a subdomain redirect
+ *
+ * Add the following line to your host file:
+ * 127.0.0.1 app.posthogtest.com
+ *
+ * Then set the following cookies locally:
+ * document.cookie = "ph_current_instance=eu.posthog.com";
+ * document.cookie = "is-logged-in=1";
+ *
+ * Then go to http://app.posthogtest.com:8000/login?next=/apps
+ * And you'll be redirected to http://eu.posthogtest.com:8000/apps
+ */
+
 import { lemonToast } from '@posthog/lemon-ui'
-import { PH_CURRENT_INSTANCE } from 'lib/constants'
+import { getCookie } from 'lib/api'
+
+const PH_CURRENT_INSTANCE = 'ph_current_instance'
+
+const IS_LOGGED_IN = 'is-logged-in'
 
 const REDIRECT_TIMEOUT = 3000
 
@@ -16,35 +34,35 @@ export function redirectIfLoggedInOtherInstance(): (() => void) | undefined {
 
     const currentSubdomain = window.location.hostname.split('.')[0]
 
-    const loggedInSubdomain = document.cookie
-        ?.split('; ')
-        ?.find((row) => row.startsWith(PH_CURRENT_INSTANCE))
-        ?.split('=')[1]
+    const phCurrentInstance = getCookie(PH_CURRENT_INSTANCE)
         ?.split('.')[0]
         ?.replace('https://', '')
         ?.replace('http://', '')
 
-    if (!loggedInSubdomain) {
+    // when they are logged out, ph_instance is not removed.
+    // therefore, use is-logged-in cookie to determine if they are logged in
+    // note: this seems to be set when logged in and removed when not
+    // however I can't find the code where it is set and removed so potentially unreliable
+    const isLoggedIn = getCookie(IS_LOGGED_IN)
+
+    if (!phCurrentInstance || !isLoggedIn) {
         return // not logged into another subdomain
     }
 
-    // TODO: check they are actually logged in i.e. valid cookie rather than just ph_instance
-    // as the ph_instance doesn't seem to be removed when they are logged out
-
-    if (loggedInSubdomain !== US_SUBDOMAIN && loggedInSubdomain !== EU_SUBDOMAIN) {
+    if (phCurrentInstance !== US_SUBDOMAIN && phCurrentInstance !== EU_SUBDOMAIN) {
         return // don't redirect to invalid subdomains
     }
 
-    const loggedIntoOtherSubdomain = loggedInSubdomain !== currentSubdomain
+    const loggedIntoOtherSubdomain = phCurrentInstance !== currentSubdomain
 
     if (loggedIntoOtherSubdomain) {
         const newUrl = new URL(window.location.href)
-        newUrl.hostname = newUrl.hostname.replace(currentSubdomain, loggedInSubdomain)
+        newUrl.hostname = newUrl.hostname.replace(currentSubdomain, phCurrentInstance)
         const redirectTimeout = setTimeout(() => {
             window.location.assign(newUrl)
         }, REDIRECT_TIMEOUT)
 
-        lemonToast.info('Redirecting to ' + (loggedInSubdomain == US_SUBDOMAIN ? 'the US cloud' : 'the EU cloud'), {
+        lemonToast.info('Redirecting to ' + (phCurrentInstance == US_SUBDOMAIN ? 'the US cloud' : 'the EU cloud'), {
             button: {
                 label: 'Cancel',
                 action: () => {
