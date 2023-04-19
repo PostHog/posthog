adf5a8492b24a277ee141be25173d136b53d8831
Paul D'Ambra <paul@posthog.com>
2023-04-19 12:10:37 +0100

    try setting smaller buffer in test node_env

--- plugin-server/src/config/config.ts
+++ plugin-server/src/config/config.ts
@@ -116,7 +116,9 @@ export function getDefaultConfig(): PluginsServerConfig {
         SESSION_RECORDING_BLOB_PROCESSING_TEAMS: 'all',
         SESSION_RECORDING_LOCAL_DIRECTORY: '.tmp/sessions',
         SESSION_RECORDING_MAX_BUFFER_AGE_SECONDS: 60 * 10, // NOTE: 10 minutes
-        SESSION_RECORDING_MAX_BUFFER_SIZE_KB: process.env.NODE_ENV === 'dev' ? 1024 : 1024 * 50, // NOTE: ~1MB in dev, ~50MB in prod
+        SESSION_RECORDING_MAX_BUFFER_SIZE_KB: ['dev', 'test'].includes(process.env.NODE_ENV || 'undefined')
+            ? 1024
+            : 1024 * 50, // NOTE: ~1MB in dev or test, ~50MB in prod
         SESSION_RECORDING_REMOTE_FOLDER: 'session_recordings',
     }
 }
