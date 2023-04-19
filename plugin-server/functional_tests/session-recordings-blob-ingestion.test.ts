66532a90779937bd077ab8833a606af8f4e7d91a
Paul D'Ambra <paul@posthog.com>
2023-04-19 10:29:48 +0100

    working e2e test

--- plugin-server/functional_tests/session-recordings-blob-ingestion.test.ts
+++ plugin-server/functional_tests/session-recordings-blob-ingestion.test.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import { Consumer, Kafka, KafkaMessage, logLevel } from 'kafkajs'
 
 import { defaultConfig } from '../src/config/config'
+import { compressToString } from '../src/main/ingestion-queues/session-recording/blob-ingester/utils'
 import { UUIDT } from '../src/utils/utils'
 import { capture, createOrganization, createTeam } from './api'
 import { waitForExpect } from './expectations'
@@ -32,40 +33,44 @@ afterAll(async () => {
     await Promise.all([await dlqConsumer.disconnect()])
 })
 
-test.skip(`single recording event writes data to local tmp file`, async () => {
-    const teamId = await createTeam(organizationId)
-    const distinctId = new UUIDT().toString()
-    const uuid = new UUIDT().toString()
-    const sessionId = new UUIDT().toString()
+test.concurrent(
+    `single recording event writes data to local tmp file`,
+    async () => {
+        const teamId = await createTeam(organizationId)
+        const distinctId = new UUIDT().toString()
+        const uuid = new UUIDT().toString()
+        const sessionId = new UUIDT().toString()
 
-    await capture({
-        teamId,
-        distinctId,
-        uuid,
-        event: '$snapshot',
-        properties: {
-            $session_id: sessionId,
-            $window_id: 'abc1234',
-            $snapshot_data: { content: 'yes way' },
-        },
-    })
+        await capture({
+            teamId,
+            distinctId,
+            uuid,
+            event: '$snapshot',
+            properties: {
+                $session_id: sessionId,
+                $window_id: 'abc1234',
+                $snapshot_data: { data: compressToString('yes way'), chunk_count: 1 },
+            },
+        })
 
-    let tempFiles: string[] = []
+        let tempFiles: string[] = []
 
-    await waitForExpect(async () => {
-        const files = await fs.promises.readdir(defaultConfig.SESSION_RECORDING_LOCAL_DIRECTORY)
-        tempFiles = files.filter((f) => f.startsWith(`${teamId}.${sessionId}`))
-        expect(tempFiles.length).toBe(1)
-    }, 20000)
+        await waitForExpect(async () => {
+            const files = await fs.promises.readdir(defaultConfig.SESSION_RECORDING_LOCAL_DIRECTORY)
+            tempFiles = files.filter((f) => f.startsWith(`${teamId}.${sessionId}`))
+            expect(tempFiles.length).toBe(1)
+        })
 
-    await waitForExpect(async () => {
-        const currentFile = tempFiles[0]
+        await waitForExpect(async () => {
+            const currentFile = tempFiles[0]
 
-        const fileContents = await fs.promises.readFile(
-            `${defaultConfig.SESSION_RECORDING_LOCAL_DIRECTORY}/${currentFile}`,
-            'utf8'
-        )
-        // check that the fileContents is equal to the string "yes way"
-        expect(fileContents).toEqual('yes way')
-    }, 20000)
-}, 40000)
+            const fileContents = await fs.promises.readFile(
+                `${defaultConfig.SESSION_RECORDING_LOCAL_DIRECTORY}/${currentFile}`,
+                'utf8'
+            )
+            // check that the fileContents is equal to the string "yes way"
+            expect(fileContents).toEqual('{"window_id":"abc1234","data":"yes way"}\n')
+        })
+    },
+    20000
+)
