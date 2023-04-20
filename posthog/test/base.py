b27fe593b169b354b60191b5d5920ebfcd6287bd
Paul D'Ambra <paul@posthog.com>
2023-04-20 10:40:58 +0100

    maybe it needs a raw string

--- posthog/test/base.py
+++ posthog/test/base.py
@@ -329,7 +329,7 @@ class QueryMatchingTest:
         # hog ql checks team ids differently
         query = re.sub(
             r"equals\((events\.)?team_id, \d+\)",
-            "equals(\1team_id, 2)",
+            r"equals(\1team_id, 2)",
             query,
         )
 
