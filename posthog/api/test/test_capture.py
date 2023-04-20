fdbd306a7da6de2e70bab36fc02361dec7f6edae
Harry Waye <harry@posthog.com>
2023-04-20 10:44:39 +0000

    chore(recordings): use teamid and session_id as Kafka key
    This is so we can rely on all events for a session being in the same
    partition, and handle events in order on one consumer member. Needed for
    the S3 storage PR.

--- posthog/api/test/test_capture.py
+++ posthog/api/test/test_capture.py
@@ -1156,12 +1156,13 @@ class TestCapture(BaseTest):
 
     @patch("posthog.kafka_client.client._KafkaProducer.produce")
     def test_recording_data_sent_to_kafka(self, kafka_produce) -> None:
-        self._send_session_recording_event()
+        session_id = "some_session_id"
+        self._send_session_recording_event(session_id=session_id)
         self.assertEqual(kafka_produce.call_count, 1)
         kafka_topic_used = kafka_produce.call_args_list[0][1]["topic"]
         self.assertEqual(kafka_topic_used, KAFKA_SESSION_RECORDING_EVENTS)
         key = kafka_produce.call_args_list[0][1]["key"]
-        self.assertEqual(key, None)
+        self.assertEqual(key, f"{self.team.pk}:{session_id}")
 
     @patch("posthog.kafka_client.client._KafkaProducer.produce")
     def test_performance_events_go_to_session_recording_events_topic(self, kafka_produce):
@@ -1194,7 +1195,7 @@ class TestCapture(BaseTest):
         kafka_topic_used = kafka_produce.call_args_list[0][1]["topic"]
         self.assertEqual(kafka_topic_used, KAFKA_SESSION_RECORDING_EVENTS)
         key = kafka_produce.call_args_list[0][1]["key"]
-        self.assertEqual(key, None)
+        self.assertEqual(key, f"{self.team.pk}:{session_id}")
 
     @patch("posthog.models.utils.UUIDT", return_value="fake-uuid")
     @patch("posthog.kafka_client.client._KafkaProducer.produce")
