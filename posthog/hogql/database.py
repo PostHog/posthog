cb346a9ea21bbae6727691fa090757ece44975ee
Marius Andra <marius.andra@gmail.com>
2023-04-21 10:45:55 +0200

    errors

--- posthog/hogql/database.py
+++ posthog/hogql/database.py
@@ -119,19 +119,19 @@ class SQLExprField(BaseModel):
         from posthog.hogql import ast
 
         if self.sql and self.expr:
-            raise HogQLException("Cannot have both SQL and expr fields set.")
+            raise HogQLException('Can\'t have both "sql" and "expr" fields set.')
 
         if self.sql:
             try:
                 return parse_expr(self.sql)
             except Exception as e:
-                raise HogQLException(f'Error parsing SQL field "{self.sql}": {e}')
+                raise HogQLException(f'Error parsing SQL expression "{self.sql}": {e}')
         elif self.expr:
             if not isinstance(self.expr, ast.Expr):
-                raise HogQLException(f"expr field must be of type ast.Expr, not {type(self.expr).__name__}")
+                raise HogQLException(f'"expr" must be of type ast.Expr, not {type(self.expr).__name__}')
             return clone_expr(self.expr)
 
-        raise HogQLException("SQLExprField must have either sql or expr field set.")
+        raise HogQLException('SQLExprField must have one of "sql" or "expr" fields set.')
 
 
 def select_from_persons_table(requested_fields: Dict[str, Any]):
