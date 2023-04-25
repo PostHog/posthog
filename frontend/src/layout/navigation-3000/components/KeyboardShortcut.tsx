6ff5d68acad3991636aa45bcdc51639992a9a347
Michael Matloka <dev@twixes.com>
2023-04-25 19:05:00 +0200

    style(3000): Support keyboard navigation in sidebar

--- frontend/src/layout/navigation-3000/components/KeyboardShortcut.tsx
+++ frontend/src/layout/navigation-3000/components/KeyboardShortcut.tsx
@@ -1,28 +1,44 @@
 import { isMac } from 'lib/utils'
 import { HotKeyOrModifier } from '~/types'
 import './KeyboardShortcut.scss'
+import clsx from 'clsx'
 
-type KeyboardShortcutProps = Partial<Record<HotKeyOrModifier, true>>
+const IS_MAC = isMac()
+const KEY_TO_SYMBOL: Partial<Record<HotKeyOrModifier, string>> = {
+    shift: '⇧',
+    command: IS_MAC ? '⌘' : 'ctrl',
+    option: IS_MAC ? '⌥' : 'alt',
+    arrowup: '↑',
+    arrowdown: '↓',
+    arrowleft: '←',
+    arrowright: '→',
+    enter: '↩',
+    escape: 'esc',
+    tab: '⇥',
+    space: '␣',
+}
+/** For consistency, we always show modifiers in this order, before other keys. */
+const MODIFIER_PRIORITY: HotKeyOrModifier[] = ['shift', 'command', 'option']
 
-export function KeyboardShortcut(props: KeyboardShortcutProps): JSX.Element {
-    const useMacSymbols = isMac()
+interface KeyboardShortcutProps extends Partial<Record<HotKeyOrModifier, true>> {
+    /** Whether this shortcut should be shown with muted opacity. */
+    muted?: boolean
+}
+
+export function KeyboardShortcut({ muted, ...keys }: KeyboardShortcutProps): JSX.Element {
+    const sortedKeys = Object.keys(keys).sort(
+        (a, b) =>
+            (MODIFIER_PRIORITY.indexOf(a as HotKeyOrModifier) || 0) -
+            (MODIFIER_PRIORITY.indexOf(b as HotKeyOrModifier) || 0)
+    ) as HotKeyOrModifier[]
 
     return (
-        <span className="KeyboardShortcut">
-            {'shift' in props && props.shift && <span className="KeyboardShortcut__key">⇧</span>}
-            {'command' in props && props.command && (
-                <span className="KeyboardShortcut__key">{useMacSymbols ? '⌘' : 'ctrl'}</span>
-            )}
-            {'option' in props && props.option && (
-                <span className="KeyboardShortcut__key">{useMacSymbols ? '⌥' : 'alt'}</span>
-            )}
-            {Object.keys(props)
-                .filter((key) => !['shift', 'command', 'option'].includes(key))
-                .map((key) => (
-                    <span key={key} className="KeyboardShortcut__key">
-                        {key}
-                    </span>
-                ))}
+        <span className={clsx('KeyboardShortcut', muted && 'KeyboardShortcut--muted')}>
+            {sortedKeys.map((key) => (
+                <span key={key} className="KeyboardShortcut__key">
+                    {KEY_TO_SYMBOL[key] || key}
+                </span>
+            ))}
         </span>
     )
 }
