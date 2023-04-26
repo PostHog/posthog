184b426d09f409b9fdcb6cd951cb6de7c0dbb21c
yakkomajuri <yakko.majuri@gmail.com>
2023-04-26 13:56:35 -0300

    fix

--- posthog/test/base.py
+++ posthog/test/base.py
@@ -777,7 +777,7 @@ def _create_insight(
 # for a person with a given distinct ID `distinct_id_from` to a given distinct ID
 # `distinct_id_to` such that with person_on_events_mode set to V2_ENABLED these
 # persons will both count as 1
-def create_person_id_override_by_distinct_id(distinct_id_from: str, distinct_id_to: str, team_id: int, version: int):
+def create_person_id_override_by_distinct_id(distinct_id_from: str, distinct_id_to: str, team_id: int, version: int = 0g):
     person_ids_result = sync_execute(
         f"""
         SELECT distinct_id, person_id
