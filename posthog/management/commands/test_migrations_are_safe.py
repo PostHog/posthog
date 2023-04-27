4e08659544a70408f9af7b3ba1d269e7b688114a
timgl <tim@posthog.com>
2023-04-27 06:50:06 -0400

    chore(ci/cd): Allow CONSTRAINTS on new tables

--- posthog/management/commands/test_migrations_are_safe.py
+++ posthog/management/commands/test_migrations_are_safe.py
@@ -8,11 +8,15 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
     help = "Automated test to make sure there are no non-null, dropping, renaming, or multiple migrations"
 
+    def _get_new_tables(self, sql: str):
+        return re.findall(r'CREATE TABLE "([a-zA-Z0-9_]*)"', sql)
+
     def handle(self, *args, **options):
         def run_and_check_migration(variable):
             try:
                 results = re.findall(r"([a-z]+)\/migrations\/([a-zA-Z_0-9]+)\.py", variable)[0]
                 sql = call_command("sqlmigrate", results[0], results[1])
+                new_tables = self._get_new_tables(sql)
                 operations = sql.split("\n")
                 for operation_sql in operations:
                     if (
@@ -43,9 +47,21 @@ class Command(BaseCommand):
                         )
                         sys.exit(1)
 
-                    if "CONSTRAINT" in operation_sql and "CREATE TABLE" not in operation_sql:
+                    if (
+                        "CONSTRAINT" in operation_sql
+                        and re.match(r'ALTER TABLE "([a-zA-Z0-9_]*)"', operation_sql)[1] not in new_tables
+                    ):
                         print(
                             f"\n\n\033[91mFound a CONSTRAINT command. This locks tables which causes downtime. Please avoid adding constraints to existing tables.\nSource: `{operation_sql}`"
+                        )
+                        sys.exit(1)
+                    if (
+                        "CREATE INDEX" in operation_sql
+                        and "CONCURRENTLY" not in operation_sql
+                        and re.match(r'.* ON "([a-zA-Z0-9_]*)"', operation_sql)[1] not in new_tables
+                    ):
+                        print(
+                            f"\n\n\033[91mFound a CREATE INDEX command that isn't run CONCURRENTLY. This locks tables which causes downtime. Please add this index CONCURRENTLY instead.\nSource: `{operation_sql}`"
                         )
                         sys.exit(1)
 
