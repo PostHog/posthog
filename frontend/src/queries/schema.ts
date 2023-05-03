42df79d32c0ee1662efd8a4dbac2ebd51ed38d2a
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-05-04 00:13:25 +0200

    fix breakdown type

--- frontend/src/queries/schema.ts
+++ frontend/src/queries/schema.ts
@@ -499,7 +499,7 @@ export interface BreakdownFilter {
     breakdowns?: Breakdown[]
     breakdown_value?: string | number
     breakdown_group_type_index?: number | null
-    aggregation_group_type_index?: number | undefined // Groups aggregation
+    breakdown_histogram_bin_count?: number | null
 }
 
 /** Pass custom metadata to queries. Used for e.g. custom columns in the DataTable. */
