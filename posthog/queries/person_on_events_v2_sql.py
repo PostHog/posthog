4de45b9afe594c1530d05affed59aff32ef57922
Neil Kakkar <neilkakkar@gmail.com>
2023-05-03 08:30:20 +0100

    fix

--- posthog/queries/person_on_events_v2_sql.py
+++ posthog/queries/person_on_events_v2_sql.py
@@ -4,4 +4,4 @@ PERSON_OVERRIDES_JOIN_SQL = """LEFT OUTER JOIN (
     WHERE team_id = %(team_id)s
     GROUP BY old_person_id
 ) AS {person_overrides_table_alias}
-ON {event_table_alias}.person_id = {person_overrides_table_alias}.old_person_id"""
+ON {event_table_alias}.{person_id_alias} = {person_overrides_table_alias}.old_person_id"""
