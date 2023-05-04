c02e32d01bb57c231948be1f2ff5da8e243039e3
Marius Andra <marius.andra@gmail.com>
2023-05-04 11:55:11 +0200

    chore(insights): remove multi property breakdown code

--- frontend/src/scenes/insights/EditorFilters/Breakdown.tsx
+++ frontend/src/scenes/insights/EditorFilters/Breakdown.tsx
@@ -1,22 +1,10 @@
-import { useActions, useValues } from 'kea'
-import { EditorFilterProps, InsightType } from '~/types'
+import { useActions } from 'kea'
+import { EditorFilterProps } from '~/types'
 import { TaxonomicBreakdownFilter } from 'scenes/insights/filters/BreakdownFilter/TaxonomicBreakdownFilter'
-import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
-import { FEATURE_FLAGS } from 'lib/constants'
 import { insightLogic } from 'scenes/insights/insightLogic'
 
 export function Breakdown({ filters }: EditorFilterProps): JSX.Element {
     const { setFiltersMerge } = useActions(insightLogic)
-    const { featureFlags } = useValues(featureFlagLogic)
 
-    const useMultiBreakdown =
-        filters.insight !== InsightType.TRENDS && !!featureFlags[FEATURE_FLAGS.BREAKDOWN_BY_MULTIPLE_PROPERTIES]
-
-    return (
-        <TaxonomicBreakdownFilter
-            filters={filters}
-            setFilters={setFiltersMerge}
-            useMultiBreakdown={useMultiBreakdown}
-        />
-    )
+    return <TaxonomicBreakdownFilter filters={filters} setFilters={setFiltersMerge} />
 }
