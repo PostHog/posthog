f8438c12c7406b3e5d5d3b7be089557d7d4c99ed
Ben White <ben@posthog.com>
2023-05-05 18:47:03 +0200

    Fix up new playlist style

--- frontend/src/scenes/notebooks/Nodes/NotebookNodePlaylist.tsx
+++ frontend/src/scenes/notebooks/Nodes/NotebookNodePlaylist.tsx
@@ -3,13 +3,22 @@ import { ReactNodeViewRenderer } from '@tiptap/react'
 import { NodeWrapper } from 'scenes/notebooks/Nodes/NodeWrapper'
 import { NotebookNodeType } from 'scenes/notebooks/Nodes/types'
 import {
-    SessionRecordingsPlaylist,
+    RecordingsLists,
     SessionRecordingsPlaylistProps,
 } from 'scenes/session-recordings/playlist/SessionRecordingsPlaylist'
 import { useJsonNodeState } from './utils'
+import { sessionRecordingsListLogic } from 'scenes/session-recordings/playlist/sessionRecordingsListLogic'
+import { useActions, useValues } from 'kea'
+import { SessionRecordingPlayer } from 'scenes/session-recordings/player/SessionRecordingPlayer'
+import { useRef } from 'react'
+import { uuid } from 'lib/utils'
+import { LemonButton } from '@posthog/lemon-ui'
+import { IconChevronLeft } from 'lib/lemon-ui/icons'
 
 const Component = (props: NodeViewProps): JSX.Element => {
     const [filters, setFilters] = useJsonNodeState(props, 'filters')
+
+    const playerKey = useRef(`notebook-${uuid()}`).current
 
     const recordingPlaylistLogicProps: SessionRecordingsPlaylistProps = {
         filters,
@@ -19,16 +28,35 @@ const Component = (props: NodeViewProps): JSX.Element => {
         onFiltersChange: setFilters,
     }
 
+    const logic = sessionRecordingsListLogic(recordingPlaylistLogicProps)
+    const { activeSessionRecording, nextSessionRecording } = useValues(logic)
+    const { setSelectedRecordingId } = useActions(logic)
+
+    const content = !activeSessionRecording?.id ? (
+        <RecordingsLists {...recordingPlaylistLogicProps} />
+    ) : (
+        <>
+            <LemonButton
+                size="small"
+                type="secondary"
+                icon={<IconChevronLeft />}
+                onClick={() => setSelectedRecordingId(null)}
+                className="self-start"
+            />
+            <SessionRecordingPlayer
+                playerKey={playerKey}
+                sessionRecordingId={activeSessionRecording.id}
+                matching={activeSessionRecording?.matching_events}
+                recordingStartTime={activeSessionRecording ? activeSessionRecording.start_time : undefined}
+                nextSessionRecording={nextSessionRecording}
+            />
+        </>
+    )
+
     return (
-        <NodeWrapper
-            {...props}
-            className={NotebookNodeType.RecordingPlaylist}
-            title="Playlist"
-            // preview={<RecordingsLists {...recordingPlaylistLogicProps} />}
-        >
-            {/* TODO: replace hardcoded height, 32 (top) + 500 (player) + 16 (margins) + 88 (seekbar) = 620 */}
-            <div style={{ maxHeight: 600 }} contentEditable={false}>
-                <SessionRecordingsPlaylist {...recordingPlaylistLogicProps} />
+        <NodeWrapper {...props} className={NotebookNodeType.RecordingPlaylist} title="Playlist">
+            <div className="flex flex-row overflow-hidden gap-2 flex-1" style={{ height: 600 }} contentEditable={false}>
+                {content}
             </div>
         </NodeWrapper>
     )
