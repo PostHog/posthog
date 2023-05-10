80418d2d73d19450c547a105cf62168dc4ff5c2b
Paul D'Ambra <paul@posthog.com>
2023-05-10 13:16:35 +0100

    wip

---
+++ posthog/models/notebook/notebook.py
@@ -0,0 +1,21 @@
+from django.utils import timezone
+
+from django.db import models
+
+from posthog.models.utils import UUIDModel
+from posthog.utils import generate_short_id
+
+
+class Notebook(UUIDModel):
+    short_id: models.CharField = models.CharField(max_length=12, blank=True, default=generate_short_id)
+    team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
+
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True, blank=True)
+    created_by: models.ForeignKey = models.ForeignKey("User", on_delete=models.SET_NULL, null=True, blank=True)
+    last_modified_at: models.DateTimeField = models.DateTimeField(default=timezone.now)
+    last_modified_by: models.ForeignKey = models.ForeignKey(
+        "User", on_delete=models.SET_NULL, null=True, blank=True, related_name="modified_playlists"
+    )
+
+    class Meta:
+        unique_together = ("team", "short_id")
