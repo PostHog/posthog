ce37d4e65774465212c243d858e71b4b8a813759
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-05-11 11:12:13 +0200

    remove dead code

--- frontend/src/scenes/insights/filters/BreakdownFilter/taxonomicBreakdownFilterUtils.ts
+++ frontend/src/scenes/insights/filters/BreakdownFilter/taxonomicBreakdownFilterUtils.ts
@@ -7,37 +7,3 @@ export const isCohortBreakdown = (t: number | string): t is number | string => i
 export const isURLNormalizeable = (propertyName: string): boolean => {
     return ['$current_url', '$pathname'].includes(propertyName)
 }
-
-interface FilterChange {
-    breakdownCohortArray: (string | number)[]
-    setFilters: (filters: Partial<FilterType>, mergeFilters?: boolean) => void
-    getPropertyDefinition: (propertyName: string | number) => PropertyDefinition | null
-}
-
-export function onFilterChange({ breakdownCohortArray, setFilters, getPropertyDefinition }: FilterChange) {
-    return (changedBreakdown: TaxonomicFilterValue, taxonomicGroup: TaxonomicFilterGroup): void => {
-        const changedBreakdownType = taxonomicFilterTypeToPropertyFilterType(taxonomicGroup.type) as BreakdownType
-
-        if (changedBreakdownType && changedBreakdown !== null) {
-            const isHistogramable = !!getPropertyDefinition(changedBreakdown)?.is_numerical
-
-            const newFilters: Partial<TrendsFilterType> = {
-                breakdown_type: changedBreakdownType,
-                breakdown_group_type_index: taxonomicGroup.groupTypeIndex,
-                breakdown_histogram_bin_count: isHistogramable ? 10 : undefined,
-                // if property definitions are not loaded when this runs then a normalizeable URL will not be normalized.
-                // For now, it is safe to fall back to `changedBreakdown`
-                breakdown_normalize_url: isURLNormalizeable(
-                    getPropertyDefinition(changedBreakdown)?.name || (changedBreakdown as string)
-                ),
-            }
-
-            newFilters.breakdown =
-                taxonomicGroup.type === TaxonomicFilterGroupType.CohortsWithAllUsers
-                    ? [...breakdownCohortArray, changedBreakdown].filter((b): b is string | number => !!b)
-                    : changedBreakdown
-
-            setFilters(newFilters, true)
-        }
-    }
-}
