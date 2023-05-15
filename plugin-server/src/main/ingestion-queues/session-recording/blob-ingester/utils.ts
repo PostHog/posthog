d88de7ec16ffd9a2a509276d57905c6310954439
Paul D'Ambra <paul@posthog.com>
2023-05-15 10:40:58 +0100

    fix: support double decoding base64 on decompress

--- plugin-server/src/main/ingestion-queues/session-recording/blob-ingester/utils.ts
+++ plugin-server/src/main/ingestion-queues/session-recording/blob-ingester/utils.ts
@@ -8,11 +8,27 @@ export function compressToString(input: string): string {
     return compressed_data.toString('base64')
 }
 
-export function decompressFromString(input: string): string {
+export function decompressFromString(input: string, doubleDecode = false): string {
+    if (doubleDecode) {
+        input = Buffer.from(input, 'base64').toString()
+    }
     const compressedData = Buffer.from(input, 'base64')
-    const uncompressed = zlib.gunzipSync(compressedData)
-    // Trim is quick way to get rid of BOMs created by python
-    return uncompressed.toString('utf16le').trim()
+    let uncompressed: Buffer | null = null
+    try {
+        uncompressed = zlib.gunzipSync(compressedData)
+        // Trim is a quick way to get rid of BOMs created by python
+        return uncompressed.toString('utf16le').trim()
+    } catch (e: any) {
+        if (e.code === 'Z_DATA_ERROR' && e.errno === -3 && !doubleDecode) {
+            // some received data (particularly chunks) are double encoded
+            return decompressFromString(input, true)
+        }
+        let message = `Failed to decompress data: ${e.message}`
+        if (doubleDecode) {
+            message += 'even accounting for double decoding'
+        }
+        throw new Error(message)
+    }
 }
 
 export const convertToPersistedMessage = (message: IncomingRecordingMessage): PersistedRecordingMessage => {
