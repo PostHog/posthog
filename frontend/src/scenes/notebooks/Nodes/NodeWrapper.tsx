2cfa1c179e864c9d0e7fc7e7ca26db50b847d92c
Ben White <ben@posthog.com>
2023-05-16 10:51:35 +0200

    Only render if on screen

--- frontend/src/scenes/notebooks/Nodes/NodeWrapper.tsx
+++ frontend/src/scenes/notebooks/Nodes/NodeWrapper.tsx
@@ -7,6 +7,7 @@ import './NodeWrapper.scss'
 import { LemonSkeleton } from 'lib/lemon-ui/LemonSkeleton'
 import { useValues } from 'kea'
 import { notebookLogic } from '../Notebook/notebookLogic'
+import { useInView } from 'react-intersection-observer'
 
 export interface NodeWrapperProps extends NodeViewProps {
     title: string
@@ -25,50 +26,51 @@ export function NodeWrapper({
     heightEstimate = '4rem',
 }: NodeWrapperProps): JSX.Element {
     const { ready } = useValues(notebookLogic)
-
-    // We delay rendering heavy components until the logic says it is "ready"
-    if (!ready) {
-        return (
-            <>
-                <div className="h-4" /> {/* Placeholder for the drag handle */}
-                <div style={{ height: heightEstimate }}>
-                    <LemonSkeleton className="h-full" />
-                </div>
-            </>
-        )
-    }
+    const [ref, inView] = useInView({ triggerOnce: true })
 
     return (
         <NodeViewWrapper
+            ref={ref}
             as="div"
             className={clsx(className, 'NotebookNode flex flex-col gap-1 overflow-hidden', {
                 'NotebookNode--selected': selected,
             })}
         >
-            <div
-                className={clsx(
-                    'NotebookNode__meta flex items-center justify-between text-xs truncate text-muted-alt',
-                    {
-                        'font-semibold': selected,
-                    }
-                )}
-                data-drag-handle
-            >
-                <div className="shrink-0">
-                    <IconDragHandle className="cursor-move text-base shrink-0" />
-                    <span>{title}</span>
-                </div>
-                <div className="shrink-0 flex gap-4">
-                    {href && (
-                        <Link to={href}>
-                            <IconLink /> Link
-                        </Link>
-                    )}
-                </div>
-            </div>
-            <div className="flex flex-row gap-4 relative z-10">
-                <div className={clsx('relative mb-2 overflow-y-auto flex-1')}>{children}</div>
-            </div>
+            {!ready || !inView ? (
+                <>
+                    <div className="h-4" /> {/* Placeholder for the drag handle */}
+                    <div style={{ height: heightEstimate }}>
+                        <LemonSkeleton className="h-full" />
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div
+                        className={clsx(
+                            'NotebookNode__meta flex items-center justify-between text-xs truncate text-muted-alt',
+                            {
+                                'font-semibold': selected,
+                            }
+                        )}
+                        data-drag-handle
+                    >
+                        <div className="shrink-0">
+                            <IconDragHandle className="cursor-move text-base shrink-0" />
+                            <span>{title}</span>
+                        </div>
+                        <div className="shrink-0 flex gap-4">
+                            {href && (
+                                <Link to={href}>
+                                    <IconLink /> Link
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-4 relative z-10">
+                        <div className={clsx('relative mb-2 overflow-y-auto flex-1')}>{children}</div>
+                    </div>
+                </>
+            )}
         </NodeViewWrapper>
     )
 }
