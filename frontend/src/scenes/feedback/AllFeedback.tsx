6435d2f74d3d11a5643a2a2648236ebc6f1c42c8
Li Yi Yu <li@posthog.com>
2023-05-18 20:43:09 -0400

    clean up feedback UX

--- frontend/src/scenes/feedback/AllFeedback.tsx
+++ frontend/src/scenes/feedback/AllFeedback.tsx
@@ -1,61 +1,15 @@
-import { LemonButton, LemonDivider, LemonSwitch, LemonTable, LemonTag, Link } from '@posthog/lemon-ui'
+import { LemonButton, LemonDivider, LemonSwitch, LemonTable, Link } from '@posthog/lemon-ui'
 import { PageHeader } from 'lib/components/PageHeader'
 import { SceneExport } from 'scenes/sceneTypes'
 
 import { feedbackLogic } from './feedbackLogic'
 
 import './Feedback.scss'
-import { LemonTabs } from 'lib/lemon-ui/LemonTabs'
-import { InAppFeedback, InAppFeedbackHeaderButtons } from './InAppFeedback'
-import { UserInterviewScheduler, UserInterviewSchedulerHeaderButtons } from './UserInterviewScheduler'
-import { useActions, useValues } from 'kea'
 import { More } from 'lib/lemon-ui/LemonButton/More'
 import stringWithWBR from 'lib/utils/stringWithWBR'
 import { urls } from 'scenes/urls'
 
 export const Feedback = (): JSX.Element => {
-    // const { activeTab } = useValues(feedbackLogic)
-    // const { setActiveTab } = useActions(feedbackLogic)
-
-    // return (
-    //     <div className="Feedback">
-    //         <PageHeader
-    //             title={
-    //                 <div className="flex items-center gap-2">
-    //                     Feedback
-    //                     <LemonTag type="warning" className="uppercase">
-    //                         Alpha
-    //                     </LemonTag>
-    //                 </div>
-    //             }
-    //             buttons={
-    //                 activeTab === 'in-app-feedback' ? (
-    //                     <InAppFeedbackHeaderButtons />
-    //                 ) : (
-    //                     <UserInterviewSchedulerHeaderButtons />
-    //                 )
-    //             }
-    //         />
-    //         <LemonTabs
-    //             activeKey={activeTab}
-    //             onChange={(key) => {
-    //                 setActiveTab(key)
-    //             }}
-    //             tabs={[
-    //                 {
-    //                     content: <InAppFeedback />,
-    //                     key: 'in-app-feedback',
-    //                     label: 'In-app feedback',
-    //                 },
-    //                 {
-    //                     content: <UserInterviewScheduler />,
-    //                     key: 'user-interview-scheduler',
-    //                     label: 'User interview scheduler',
-    //                 },
-    //             ]}
-    //         />
-    //     </div>
-    // )
     return (
         <div className="mt-10">
             <PageHeader
@@ -75,31 +29,24 @@ export const Feedback = (): JSX.Element => {
                         render: function RenderName(_, feedback) {
                             return (
                                 <>
-                                    <Link
-                                        to={urls.feedback(feedback.id)
-                                            // feedback.id ? urls.featureFlag(featureFlag.id) : undefined
-                                        }
-                                        className="row-name"
-                                    >
+                                    <Link to={urls.feedback(feedback.id)} className="row-name">
                                         {stringWithWBR(feedback.name, 17)}
                                     </Link>
                                 </>
                             )
-                        }
+                        },
                     },
                     {
                         dataIndex: 'responses',
-                        title: 'Responses'
+                        title: 'Responses',
                     },
                     {
                         dataIndex: 'type',
-                        title: 'Type'
+                        title: 'Type',
                     },
                     {
                         dataIndex: 'created_by',
-                        // sorter: function noRefCheck() { },
                         title: 'Created by',
-                        // tooltip: 'What they are primarily working on.'
                     },
                     {
                         dataIndex: 'created_at',
@@ -108,20 +55,11 @@ export const Feedback = (): JSX.Element => {
                     {
                         title: 'Status',
                         dataIndex: 'active',
-                        // sorter: (a: FeatureFlagType, b: FeatureFlagType) => Number(a.active) - Number(b.active),
                         width: 100,
                         render: function RenderActive(_, feedback) {
                             return (
                                 <>
-                                    <LemonSwitch checked={feedback.active} onChange={() => { }} />
-                                    {/* {feedback.active ? (<LemonTag type="success" className="uppercase">
-                                        Enabled
-                                    </LemonTag>
-                                    ) : (
-                                        <LemonTag type="default" className="uppercase">
-                                            Disabled
-                                        </LemonTag>
-                                    )} */}
+                                    <LemonSwitch checked={feedback.active} onChange={() => {}} />
                                 </>
                             )
                         },
@@ -133,35 +71,16 @@ export const Feedback = (): JSX.Element => {
                                 <More
                                     overlay={
                                         <>
-                                            <LemonButton status="stealth" fullWidth>View results</LemonButton>
-                                            {/* {feedback?.id && ( */}
-                                            <LemonButton
-                                                status="stealth"
-                                                fullWidth
-                                                onClick={() => { }
-                                                    // featureFlag.id && router.actions.push(urls.featureFlag(featureFlag.id))
-                                                }
-                                            >
+                                            <LemonButton status="stealth" fullWidth>
+                                                View results
+                                            </LemonButton>
+                                            <LemonButton status="stealth" fullWidth onClick={() => {}}>
                                                 Edit
                                             </LemonButton>
-                                            {/* )} */}
                                             <LemonDivider />
-                                            {/* {featureFlag.id && ( */}
-                                            <LemonButton
-                                                status="danger"
-                                                onClick={() => {
-                                                    // deleteWithUndo({
-                                                    //     endpoint: `projects/${currentTeamId}/feature_flags`,
-                                                    //     object: { name: featureFlag.key, id: featureFlag.id },
-                                                    //     callback: loadFeatureFlags,
-                                                    // })
-                                                }}
-                                                // disabled={!featureFlag.can_edit}
-                                                fullWidth
-                                            >
+                                            <LemonButton status="danger" onClick={() => {}} fullWidth>
                                                 Delete {feedback.type.toLowerCase()}
                                             </LemonButton>
-                                            {/* )} */}
                                         </>
                                     }
                                 />
@@ -196,12 +115,11 @@ export const Feedback = (): JSX.Element => {
                         created_by: 'Annika',
                         created_at: '10 days ago',
                         active: true,
-                    }
+                    },
                 ]}
-                onRow={function noRefCheck() { }}
-                onSort={function noRefCheck() { }}
+                onRow={function noRefCheck() {}}
+                onSort={function noRefCheck() {}}
             />
-
         </div>
     )
 }
--- frontend/src/scenes/feedback/Feedback.tsx
+++ frontend/src/scenes/feedback/Feedback.tsx
@@ -1,20 +1,30 @@
-import { LemonButton, LemonCollapse, LemonDivider, LemonInput, LemonTable, LemonTextArea, Link } from "@posthog/lemon-ui"
-import { useValues } from "kea"
-import { EditableField } from "lib/components/EditableField/EditableField"
-import { ObjectTags } from "lib/components/ObjectTags/ObjectTags"
-import { PageHeader } from "lib/components/PageHeader"
-import { IconPlus } from "lib/lemon-ui/icons"
-import { LemonMenu } from "lib/lemon-ui/LemonMenu/LemonMenu"
-import { LemonTabs } from "lib/lemon-ui/LemonTabs"
-import { useState } from "react"
-import { FlagSelector } from "scenes/early-access-features/EarlyAccessFeature"
-import { SceneExport } from "scenes/sceneTypes"
-import { tagsModel } from "~/models/tagsModel"
+import {
+    LemonButton,
+    LemonCollapse,
+    LemonDivider,
+    LemonInput,
+    LemonTable,
+    LemonTextArea,
+    Link,
+} from '@posthog/lemon-ui'
+import { useValues } from 'kea'
+import { EditableField } from 'lib/components/EditableField/EditableField'
+import { ObjectTags } from 'lib/components/ObjectTags/ObjectTags'
+import { PageHeader } from 'lib/components/PageHeader'
+import { IconPlus } from 'lib/lemon-ui/icons'
+import { LemonMenu } from 'lib/lemon-ui/LemonMenu/LemonMenu'
+import { LemonTabs } from 'lib/lemon-ui/LemonTabs'
+import { useState } from 'react'
+import { FlagSelector } from 'scenes/early-access-features/EarlyAccessFeature'
+import { SceneExport } from 'scenes/sceneTypes'
+import { userLogic } from 'scenes/userLogic'
+import { tagsModel } from '~/models/tagsModel'
+import { AvailableFeature } from '~/types'
 
 export const scene: SceneExport = {
     component: Feedback,
-    // logic: featureFlagLogic,
-    paramsToProps: ({ params: { id } }): (any)['props'] => ({
+    // logic: feedbackLogic,
+    paramsToProps: ({ params: { id } }): any['props'] => ({
         id: id && id !== 'new' ? parseInt(id) : 'new',
     }),
 }
@@ -22,14 +32,7 @@ export const scene: SceneExport = {
 export function Feedback({ id }: { id?: string } = {}): JSX.Element {
     const isNewFeedback = id === 'new' || id === undefined
 
-    return (
-        <div>
-            {isNewFeedback ?
-                <FeedbackForm /> :
-                <FeedbackSummary />}
-        </div>
-
-    )
+    return <div>{isNewFeedback ? <FeedbackForm /> : <FeedbackSummary />}</div>
 }
 
 export function FeedbackForm(): JSX.Element {
@@ -37,6 +40,7 @@ export function FeedbackForm(): JSX.Element {
     const feedbackLoading = false
     const canEditFeedback = true
     const { tags } = useValues(tagsModel)
+    const { hasAvailableFeature } = useValues(userLogic)
 
     return (
         <div>
@@ -48,23 +52,14 @@ export function FeedbackForm(): JSX.Element {
                             value={feedback?.name || (feedbackLoading ? 'Loading…' : '')}
                             placeholder="Name this feedback"
                             onSave={
-                                // dashboard
-                                //     ? (value) => updateDashboard({ id: feedback.id, name: value, allowUndo: true })
-                                //     : 
+                                // feedback
+                                //     ? (value) => updateFeedback({ id: feedback.id, name: value, allowUndo: true })
+                                //     :
                                 undefined
                             }
                             saveOnBlur={true}
                             minLength={1}
-                            maxLength={400} // Sync with Dashboard model
-                            // mode={!canEditDashboard ? 'view' : undefined}
-                            // notice={
-                            //     dashboard && !canEditDashboard
-                            //         ? {
-                            //             icon: <IconLock />,
-                            //             tooltip: DASHBOARD_CANNOT_EDIT_MESSAGE,
-                            //         }
-                            //         : undefined
-                            // }
+                            maxLength={400}
                             data-attr="feedback-name"
                         />
                     </div>
@@ -77,13 +72,13 @@ export function FeedbackForm(): JSX.Element {
                                 name="description"
                                 value={feedback.description || ''}
                                 placeholder="Description (optional)"
-                                onSave={(value) => { }
-                                    // updateDashboard({ id: dashboard.id, description: value, allowUndo: true })
+                                onSave={
+                                    () => {}
+                                    // updateFeedback({ id: feedback.id, description: value, allowUndo: true })
                                 }
                                 saveOnBlur={true}
                                 compactButtons
-                            // mode={!canEditDashboard ? 'view' : undefined}
-                            // paywall={!hasAvailableFeature(AvailableFeature.DASHBOARD_COLLABORATION)}
+                                paywall={!hasAvailableFeature(AvailableFeature.DASHBOARD_COLLABORATION)}
                             />
                         )}
                         {feedback?.tags && (
@@ -91,7 +86,7 @@ export function FeedbackForm(): JSX.Element {
                                 {canEditFeedback ? (
                                     <ObjectTags
                                         tags={feedback.tags}
-                                        // onChange={(_, tags) => triggerDashboardUpdate({ tags })}
+                                        // onChange={(_, tags) => triggerFeedbackUpdate({ tags })}
                                         saving={feedbackLoading}
                                         tagsAvailable={tags.filter((tag) => !feedback.tags?.includes(tag))}
                                         className="insight-metadata-tags"
@@ -114,7 +109,7 @@ export function FeedbackForm(): JSX.Element {
                             data-attr="cancel-feedback"
                             type="secondary"
                             onClick={() => {
-                                // if (isEditingFlag) {
+                                // if (isEditingFeedback) {
                                 //     editFeatureFlag(false)
                                 //     loadFeatureFlag()
                                 // } else {
@@ -142,12 +137,16 @@ export function FeedbackForm(): JSX.Element {
                         <h3>Display conditions</h3>
                         <div>
                             <h4>Link to feature (optional)</h4>
-                            <div className="text-muted">Connect to a feature flag to track qualitative feature success.</div>
+                            <div className="text-muted">
+                                Connect to a feature flag to track qualitative feature success.
+                            </div>
                             <div className="mb-2">
-                                <FlagSelector value={'5'} onChange={() => { }} />
+                                <FlagSelector value={0} onChange={() => {}} />
                             </div>
 
-                            <div className="text-muted">Choose where your feedback prompt will show either on a url or based on a CSS selector.</div>
+                            <div className="text-muted">
+                                Choose where your feedback prompt will show either on a url or based on a CSS selector.
+                            </div>
                             <span>Url</span>
                             <LemonInput />
 
@@ -158,10 +157,11 @@ export function FeedbackForm(): JSX.Element {
                     <div>
                         <LemonDivider className="my-2" />
                         <h3>Questions</h3>
-                        <LemonCollapse className="w-180 border rounded p-2"
+                        <LemonCollapse
+                            className="w-180 border rounded p-2"
                             panels={[
                                 {
-                                    content:
+                                    content: (
                                         <>
                                             <div className="mb-2">
                                                 <span>Title</span>
@@ -171,10 +171,11 @@ export function FeedbackForm(): JSX.Element {
                                                 <span>Description (optional)</span>
                                                 <LemonTextArea />
                                             </div>
-                                        </>,
+                                        </>
+                                    ),
                                     header: 'New feedback question',
                                     key: '1',
-                                }
+                                },
                             ]}
                         />
 
@@ -186,10 +187,8 @@ export function FeedbackForm(): JSX.Element {
                             <LemonButton type="primary" className="my-3" icon={<IconPlus />}>
                                 New question
                             </LemonButton>
-
                         </LemonMenu>
                     </div>
-
                 </div>
                 <div className="ml-4 pl-2 w-full flex justify-center flex-col" style={{ borderLeft: '2px solid' }}>
                     <div className="text-center">Preview</div>
@@ -197,80 +196,107 @@ export function FeedbackForm(): JSX.Element {
                         <h2>Do you have any feedback for this feature?</h2>
                         <span>optional description text</span>
                         <LemonTextArea className="mt-2" />
-                        <LemonButton className="mt-4" type="primary">Finish</LemonButton>
+                        <LemonButton className="mt-4" type="primary">
+                            Finish
+                        </LemonButton>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
 
 export function FeedbackSummary(): JSX.Element {
-    const [tabKey, setTabKey] = useState("results")
+    const [tabKey, setTabKey] = useState('results')
     return (
         <div>
             <PageHeader
                 title={'Early access beta feature survey'}
                 buttons={
                     <div className="flex items-center gap-2">
-                        <LemonButton type="secondary" status="danger">Stop</LemonButton>
+                        <LemonButton type="secondary" status="danger">
+                            Stop
+                        </LemonButton>
                     </div>
                 }
             />
-            <LemonTabs activeKey={tabKey} onChange={(key) => setTabKey(key)} tabs={[
-                {
-                    content: <div>
-                        <div>
-                            <h3>Connected feature</h3>
-                            <Link>early-access-features</Link>
-                        </div>
+            <LemonTabs
+                activeKey={tabKey}
+                onChange={(key) => setTabKey(key)}
+                tabs={[
+                    {
+                        content: (
+                            <div>
+                                <div>
+                                    <h3>Connected feature</h3>
+                                    <Link>early-access-features</Link>
+                                </div>
 
-                        <div>
-                            <h3>Display conditions</h3>
-                            <span>Url: app.posthog.com/early-access-features</span>
-                        </div>
-                        <div className="my-4">
-                            <h3>Questions</h3>
-                            <span>Do you have any feedback for early access features?</span>
-                        </div>
-                        </div>,
-                    key: 'overview',
-                    label: 'Overview'
-                },
-                {
-                    content: <div>
-                        <div className="flex flex-row gap-4">
-                            <div className="border rounded p-4">
-                                <span>Impressions</span>
-                                <h2>257</h2>
+                                <div>
+                                    <h3>Display conditions</h3>
+                                    <span>Url: app.posthog.com/early-access-features</span>
+                                </div>
+                                <div className="my-4">
+                                    <h3>Questions</h3>
+                                    <span>Do you have any feedback for early access features?</span>
+                                </div>
                             </div>
-                            <div className="border rounded p-4">
-                                <span>Started</span>
-                                <h2>78</h2>
+                        ),
+                        key: 'overview',
+                        label: 'Overview',
+                    },
+                    {
+                        content: (
+                            <div>
+                                <div className="flex flex-row gap-4">
+                                    <div className="border rounded p-4">
+                                        <span>Impressions</span>
+                                        <h2>257</h2>
+                                    </div>
+                                    <div className="border rounded p-4">
+                                        <span>Started</span>
+                                        <h2>78</h2>
+                                    </div>
+                                    <div className="border rounded p-4">
+                                        <span>Completed</span>
+                                        <h2>55</h2>
+                                    </div>
+                                </div>
+                                <LemonDivider />
+                                <LemonButton type="secondary" className="mb-2">
+                                    filter buttons and such
+                                </LemonButton>
+                                <LemonTable
+                                    columns={[
+                                        { dataIndex: 'response', title: 'Response', width: 600 },
+                                        { dataIndex: 'email', title: 'Email' },
+                                    ]}
+                                    dataSource={[
+                                        {
+                                            response:
+                                                'This feature has changed my life!! Before this feature I wasnt able to test my releases and now I can. Thanks so much posthog team!',
+                                            email: 'iloveposthog@email.com',
+                                        },
+                                        {
+                                            response:
+                                                "I don't really understand how to use this feature, are there any tutorials or docs on it?",
+                                            email: 'kendall@email.com',
+                                        },
+                                        {
+                                            response: 'my sesion recording is broken, help????',
+                                            email: 'communityengineer12@email.com',
+                                        },
+                                        { response: 'survey response', email: '' },
+                                        { response: 'survey response', email: '' },
+                                        { response: 'survey response', email: '' },
+                                    ]}
+                                />
                             </div>
-                            <div className="border rounded p-4">
-                                <span>Completed</span>
-                                <h2>55</h2>
-                            </div>
-                        </div>
-                        <LemonDivider />
-                        <LemonButton type="secondary" className="mb-2">filter buttons and such</LemonButton>
-                        <LemonTable
-                            columns={[{ dataIndex: 'response', title: 'Response', width: 600 }, { dataIndex: 'email', title: 'Email' }]}
-                            dataSource={[
-                                { response: 'This feature has changed my life!! Before this feature I wasnt able to test my releases and now I can. Thanks so much posthog team!', email: 'iloveposthog@email.com' },
-                                { response: "I don't really understand how to use this feature, are there any tutorials or docs on it?", email: 'kendall@email.com' },
-                                { response: "my sesion recording is broken, help????", email: 'communityengineer12@email.com' },
-                                { response: "survey response", email: '' },
-                                { response: "survey response", email: '' },
-                                { response: "survey response", email: '' }
-                            ]}
-                        /></div>,
-                    key: 'results',
-                    label: 'Results'
-                }
-            ]}
+                        ),
+                        key: 'results',
+                        label: 'Results',
+                    },
+                ]}
             />
         </div>
     )
