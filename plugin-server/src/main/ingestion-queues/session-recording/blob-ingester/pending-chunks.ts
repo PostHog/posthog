7858d5ad4c99349423963d6bc78604366f37944b
Paul D'Ambra <paul@posthog.com>
2023-05-23 14:48:55 +0100

    chore: worrying at ingestion still (#15673)
    * less pending chunk logging
    
    * the flush threshold multiplies is too confusing to operate
    
    * must provide all chunk offsets and the correct chunks when processing pending chunks

--- plugin-server/src/main/ingestion-queues/session-recording/blob-ingester/pending-chunks.ts
+++ plugin-server/src/main/ingestion-queues/session-recording/blob-ingester/pending-chunks.ts
@@ -14,16 +14,35 @@ export class PendingChunks {
     }
 
     get isComplete() {
-        const fullSet = this.chunks.slice(0, this.expectedSize)
+        const fullSet = this.deduplicatedChunks()
+
         const expectedChunkIndexes = Array.from(Array(this.expectedSize).keys())
         return expectedChunkIndexes.every((x, i) => i < fullSet.length && fullSet[i].chunk_index === x)
+    }
+
+    private deduplicatedChunks() {
+        return Object.values(
+            // keep only one of each chunk_index, assumes any duplicates are ignorable duplicates
+            this.chunks.reduce((acc, curr) => {
+                // If the chunk_index doesn't exist in the accumulator or
+                // the existing object is older than the current one, update the accumulator
+                if (!acc[curr.chunk_index]) {
+                    acc[curr.chunk_index] = curr
+                }
+                return acc
+            }, {} as Record<number, IncomingRecordingMessage>)
+        ).slice(0, this.expectedSize)
     }
 
     get completedChunks() {
         if (!this.isComplete) {
             throw new Error('Cannot get completed chunks from incomplete set')
         }
-        return this.chunks.slice(0, this.expectedSize)
+        return this.deduplicatedChunks()
+    }
+
+    get allChunkOffsets(): number[] {
+        return this.chunks.map((x) => x.metadata.offset)
     }
 
     isIdle(referenceNow: number, idleThreshold: number) {
