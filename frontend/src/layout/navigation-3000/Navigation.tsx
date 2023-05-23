1c72db123c37b5394eac3d7f517a8eb60646027e
Ben White <ben@posthog.com>
2023-05-23 11:21:50 +0200

    Fix

--- frontend/src/layout/navigation-3000/Navigation.tsx
+++ frontend/src/layout/navigation-3000/Navigation.tsx
@@ -7,8 +7,6 @@ import { Navbar } from './components/Navbar'
 import { Sidebar } from './components/Sidebar'
 import './Navigation.scss'
 import { themeLogic } from './themeLogic'
-import { FlaggedFeature } from 'lib/components/FlaggedFeature'
-import { FEATURE_FLAGS } from 'lib/constants'
 import { navigation3000Logic } from './navigationLogic'
 
 export function Navigation({ children }: { children: ReactNode }): JSX.Element {
