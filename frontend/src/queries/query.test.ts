639d9d732cdb8a71499a73fdc620690de4086b69
Marius Andra <marius.andra@gmail.com>
2023-05-23 10:59:14 +0200

    feat(query): capture queries

--- frontend/src/queries/query.test.ts
+++ frontend/src/queries/query.test.ts
@@ -1,11 +1,27 @@
-import { queryExportContext } from '~/queries/query'
-import { EventsQuery, NodeKind } from '~/queries/schema'
+import { query, queryExportContext } from '~/queries/query'
+import { EventsQuery, HogQLQuery, NodeKind } from '~/queries/schema'
 import { PropertyFilterType, PropertyOperator } from '~/types'
 import { initKeaTests } from '~/test/init'
 import { MOCK_TEAM_ID } from 'lib/api.mock'
+import posthog from 'posthog-js'
+import { useMocks } from '~/mocks/jest'
 
 describe('query', () => {
     beforeEach(() => {
+        useMocks({
+            post: {
+                '/api/projects/:team/query': (req) => {
+                    const data = req.body as any
+                    if (data.query?.kind === 'HogQLQuery') {
+                        return [200, { results: [], clickhouse: 'clickhouse string', hogql: 'hogql string' }]
+                    }
+                    if (data.query?.kind === 'EventsQuery' && data.query.select[0] === 'error') {
+                        return [500, { detail: 'error' }]
+                    }
+                    return [200, {}]
+                },
+            },
+        })
         initKeaTests()
     })
 
@@ -56,5 +72,47 @@ describe('query', () => {
             method: 'POST',
             path: `/api/projects/${MOCK_TEAM_ID}/query`,
         })
+    })
+
+    it('emits an event when a query is run', async () => {
+        jest.spyOn(posthog, 'capture')
+        const q: EventsQuery = {
+            kind: NodeKind.EventsQuery,
+            select: ['timestamp'],
+            limit: 100,
+        }
+        await query(q)
+        expect(posthog.capture).toHaveBeenCalledWith('Query', { query: q, duration: expect.any(Number) })
+    })
+
+    it('emits a specific event on a HogQLQuery', async () => {
+        jest.spyOn(posthog, 'capture')
+        const q: HogQLQuery = {
+            kind: NodeKind.HogQLQuery,
+            query: 'select * from events',
+        }
+        await query(q)
+        expect(posthog.capture).toHaveBeenCalledWith('Query', {
+            query: q,
+            duration: expect.any(Number),
+            clickhouse_sql: expect.any(String),
+        })
+    })
+
+    it('emits an event when a query errors', async () => {
+        jest.spyOn(posthog, 'capture')
+        const q: EventsQuery = {
+            kind: NodeKind.EventsQuery,
+            select: ['error'],
+            limit: 100,
+        }
+        await expect(async () => {
+            await query(q)
+        }).rejects.toStrictEqual({
+            status: 500,
+            detail: 'error',
+        })
+
+        expect(posthog.capture).toHaveBeenCalledWith('Query Error', { query: q, duration: expect.any(Number) })
     })
 })
