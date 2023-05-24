50f7897120f73fcefb7d03e101b37f9bfd477886
Marius Andra <marius.andra@gmail.com>
2023-05-24 16:28:05 +0200

    Update frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx
    Co-authored-by: Michael Matloka <dev@twixes.com>

--- frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx
+++ frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx
@@ -525,11 +525,7 @@ function useMathSelectorOptions({
 
     options.push({
         value: HogQLMathType.HogQL,
-        label: (
-            <div className="flex items-center gap-2">
-                <span>HogQL Expression</span>
-            </div>
-        ),
+       label: 'HogQL expression',
         tooltip: 'Aggregate events by custom SQL expression.',
         'data-attr': `math-node-hogql-expression-${index}`,
     })
