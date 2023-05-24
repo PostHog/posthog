0303bb6edb9c0e1a699fcab5787fbe159b61fc83
James Greenhill <fuziontech@gmail.com>
2023-05-24 19:38:58 +0000

    chore: Rekey posthog_persondistinctid table. PK id => (team_id, distinct_id)

---
+++ posthog/migrations/0314_rekey_persondistinctid.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+REKEY_PERSON_DISTINCT_ID_SQL = """
+    BEGIN;
+    DROP SEQUENCE IF EXISTS posthog_persondistinctid_id_seq;
+    ALTER TABLE posthog_persondistinctid DROP CONSTRAINT posthog_persondistinctid_pkey;
+    ALTER TABLE posthog_persondistinctid ALTER COLUMN id SET DEFAULT 2147483647;
+    ALTER TABLE posthog_persondistinctid ADD PRIMARY KEY (team_id, distinct_id);
+    COMMIT;
+"""
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("posthog", "0313_early_access_feature"),
+    ]
+
+    operations = [
+        migrations.RunSQL(REKEY_PERSON_DISTINCT_ID_SQL, reverse_sql=migrations.RunSQL.noop),
+    ]
