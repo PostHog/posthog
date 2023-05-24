0bfb4d6c49044d6809f5fab6f1791ca36145aee4
James Greenhill <fuziontech@gmail.com>
2023-05-24 19:40:49 +0000

    remove if exists, all or nothing transaction

--- posthog/migrations/0314_rekey_persondistinctid.py
+++ posthog/migrations/0314_rekey_persondistinctid.py
@@ -3,7 +3,7 @@ from django.db import migrations
 
 REKEY_PERSON_DISTINCT_ID_SQL = """
     BEGIN;
-    DROP SEQUENCE IF EXISTS posthog_persondistinctid_id_seq;
+    DROP SEQUENCE posthog_persondistinctid_id_seq;
     ALTER TABLE posthog_persondistinctid DROP CONSTRAINT posthog_persondistinctid_pkey;
     ALTER TABLE posthog_persondistinctid ALTER COLUMN id SET DEFAULT 2147483647;
     ALTER TABLE posthog_persondistinctid ADD PRIMARY KEY (team_id, distinct_id);
