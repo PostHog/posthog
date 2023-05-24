33113b049463238bb7c92048eac2cdfb6997f321
Ben White <ben@posthog.com>
2023-05-24 12:44:57 +0200

    feat: Improved Notebooks UX (#15561)

--- frontend/src/scenes/urls.ts
+++ frontend/src/scenes/urls.ts
@@ -149,4 +149,10 @@ export const urls = {
         combineUrl('/debug', {}, query ? { q: typeof query === 'string' ? query : JSON.stringify(query) } : {}).url,
     feedback: (): string => '/feedback',
     issues: (): string => '/issues',
+    notebooks: (): string =>
+        combineUrl(urls.dashboards(), {
+            tab: 'notebooks',
+        }).url,
+    notebook: (id: string | number): string => `/notebooks/${id}`,
+    notebookEdit: (id: string | number): string => `/notebooks/${id}/edit`,
 }
