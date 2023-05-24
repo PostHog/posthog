5a125b6f96f0d71b971f0413cb559d76db05a1e3
Marius Andra <marius.andra@gmail.com>
2023-05-24 10:22:13 +0200

    split out HogQLEditor

---
+++ frontend/src/lib/components/HogQLEditor/HogQLEditor.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+import { LemonTextArea } from 'lib/lemon-ui/LemonTextArea/LemonTextArea'
+import { CLICK_OUTSIDE_BLOCK_CLASS } from 'lib/hooks/useOutsideClickHandler'
+import { LemonButton } from 'lib/lemon-ui/LemonButton'
+
+export interface HogQLEditorProps {
+    onChange: (value: string) => void
+    value: string | undefined
+    disablePersonProperties?: boolean
+    disableAutoFocus?: boolean
+    disableCmdEnter?: boolean
+    submitText?: string
+    placeholder?: string
+}
+
+export function HogQLEditor({
+    onChange,
+    value,
+    disablePersonProperties,
+    disableAutoFocus,
+    disableCmdEnter,
+    submitText,
+    placeholder,
+}: HogQLEditorProps): JSX.Element {
+    const [localValue, setLocalValue] = useState(value || '')
+    useEffect(() => {
+        setLocalValue(value || '')
+    }, [value])
+
+    return (
+        <>
+            <LemonTextArea
+                data-attr="inline-hogql-editor"
+                value={localValue || ''}
+                onChange={(newValue) => setLocalValue(newValue)}
+                autoFocus={!disableAutoFocus}
+                onFocus={
+                    disableAutoFocus
+                        ? undefined
+                        : (e) => {
+                              e.target.selectionStart = localValue.length // Focus at the end of the input
+                          }
+                }
+                onPressCmdEnter={disableCmdEnter ? undefined : () => onChange(localValue)}
+                className={`font-mono ${CLICK_OUTSIDE_BLOCK_CLASS}`}
+                minRows={6}
+                maxRows={6}
+                placeholder={
+                    placeholder ??
+                    (disablePersonProperties
+                        ? "Enter HogQL expression, such as:\n- properties.$current_url\n- toInt(properties.`Long Field Name`) * 10\n- concat(event, ' ', distinct_id)\n- if(1 < 2, 'small', 'large')"
+                        : "Enter HogQL Expression, such as:\n- properties.$current_url\n- person.properties.$geoip_country_name\n- toInt(properties.`Long Field Name`) * 10\n- concat(event, ' ', distinct_id)\n- if(1 < 2, 'small', 'large')")
+                }
+            />
+            <LemonButton
+                fullWidth
+                type="primary"
+                onClick={() => onChange(localValue)}
+                disabledReason={!localValue ? 'Please enter a HogQL expression' : undefined}
+                center
+            >
+                {submitText ?? 'Update HogQL expression'}
+            </LemonButton>
+            <div className="flex w-full">
+                <div className="flex-1 text-muted">
+                    NB: <code>person.properties</code> can't be used here.
+                </div>
+                <div className={`text-right ${CLICK_OUTSIDE_BLOCK_CLASS}`}>
+                    <a href="https://posthog.com/manual/hogql" target={'_blank'}>
+                        Learn more about HogQL
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+}
