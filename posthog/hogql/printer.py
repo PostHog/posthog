0081fb58bf3048b4df188775091c6ff4921cdc9a
Marius Andra <marius.andra@gmail.com>
2023-05-26 09:40:22 +0200

    not for this PR

--- posthog/hogql/printer.py
+++ posthog/hogql/printer.py
@@ -601,9 +601,9 @@ class _Printer(Visitor):
 
         args: List[str] = []
         if materialized_property_sql is not None:
-            # When reading materialized columns, treat the value "null" as NULL-s.
+            # When reading materialized columns, treat the values "" and "null" as NULL-s.
             # TODO: rematerialize all columns to support empty strings and "null" string values.
-            # materialized_property_sql = f"nullIf({materialized_property_sql}, 'null')"
+            materialized_property_sql = f"nullIf(nullIf({materialized_property_sql}, ''), 'null')"
 
             if len(type.chain) == 1:
                 return materialized_property_sql
@@ -674,7 +674,7 @@ class _Printer(Visitor):
         return escape_hogql_string(name, timezone=self._get_timezone())
 
     def _unsafe_json_extract_trim_quotes(self, unsafe_field: str, unsafe_args: List[str]) -> str:
-        return f"replaceRegexpAll(ifNull(nullIf(JSONExtractRaw({', '.join([unsafe_field] + unsafe_args)}), 'null'), ''), '^\"|\"$', '')"
+        return f"replaceRegexpAll(nullIf(nullIf(JSONExtractRaw({', '.join([unsafe_field] + unsafe_args)}), ''), 'null'), '^\"|\"$', '')"
 
     def _get_materialized_column(
         self, table_name: str, property_name: PropertyName, field_name: TableColumn
