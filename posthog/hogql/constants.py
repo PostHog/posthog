462e64653f94baa45ad78ef5475146a7610891ca
Marius Andra <marius.andra@gmail.com>
2023-05-26 22:28:42 +0200

    fix(exports): lower export default to 10k (#15759)
    * fix(exports): lower export default to 10k
    
    * change export default to 10k
    
    * change export default to 10k
    
    * Update query snapshots
    
    * Update query snapshots
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- posthog/hogql/constants.py
+++ posthog/hogql/constants.py
@@ -642,7 +642,7 @@ RESERVED_KEYWORDS = KEYWORDS + ["team_id"]
 # Limit applied to SELECT statements without LIMIT clause when queried via the API
 DEFAULT_RETURNED_ROWS = 100
 # Max limit for all SELECT queries, and the default for CSV exports.
-MAX_SELECT_RETURNED_ROWS = 100000
+MAX_SELECT_RETURNED_ROWS = 10000
 
 # Settings applied on top of all HogQL queries.
 class HogQLSettings(BaseModel):
