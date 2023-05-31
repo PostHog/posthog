4e99b80f835856cb34840213da9382991ebe3427
Ben White <ben@posthog.com>
2023-05-31 12:05:35 +0200

    Added dismissable banner about scratchpad

---
+++ frontend/src/lib/lemon-ui/LemonBanner/lemonBannerLogic.ts
@@ -0,0 +1,26 @@
+import { actions, kea, key, path, props, reducers } from 'kea'
+
+import type { lemonBannerLogicType } from './lemonBannerLogicType'
+
+export type LemonBannerLogicProps = {
+    /** The key to be used for persisting the fact this modal is dismissed */
+    dismissKey: string
+}
+
+export const lemonBannerLogic = kea<lemonBannerLogicType>([
+    path((key) => ['components', 'lemon-banner', 'lemonBannerLogic', key]),
+    key(({ dismissKey }) => dismissKey),
+    props({} as LemonBannerLogicProps),
+    actions({
+        dismiss: true,
+    }),
+    reducers({
+        isDismissed: [
+            false,
+            { persist: true },
+            {
+                dismiss: () => true,
+            },
+        ],
+    }),
+])
