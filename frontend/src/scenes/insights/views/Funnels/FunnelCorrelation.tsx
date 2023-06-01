384cd12e9709c31145455a6e5e255732789d45f4
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-06-01 10:51:09 +0200

    refactor(data-exploration): remove dead code from funnel correlation components (#15496)

--- frontend/src/scenes/insights/views/Funnels/FunnelCorrelation.tsx
+++ frontend/src/scenes/insights/views/Funnels/FunnelCorrelation.tsx
@@ -1,32 +1,24 @@
 import { useMountedLogic, useValues } from 'kea'
 
 import { insightLogic } from 'scenes/insights/insightLogic'
-import { funnelLogic } from 'scenes/funnels/funnelLogic'
 import { funnelDataLogic } from 'scenes/funnels/funnelDataLogic'
 import { funnelCorrelationUsageLogic } from 'scenes/funnels/funnelCorrelationUsageLogic'
 
 import { PayGateMini } from 'lib/components/PayGateMini/PayGateMini'
-import {
-    FunnelCorrelationSkewWarning,
-    FunnelCorrelationSkewWarningDataExploration,
-} from './FunnelCorrelationSkewWarning'
-import { FunnelCorrelationTable, FunnelCorrelationTableDataExploration } from './FunnelCorrelationTable'
+import { FunnelCorrelationSkewWarning } from './FunnelCorrelationSkewWarning'
+import { FunnelCorrelationTable } from './FunnelCorrelationTable'
 import { FunnelCorrelationFeedbackForm } from './FunnelCorrelationFeedbackForm'
-import {
-    FunnelPropertyCorrelationTable,
-    FunnelPropertyCorrelationTableDataExploration,
-} from './FunnelPropertyCorrelationTable'
+import { FunnelPropertyCorrelationTable } from './FunnelPropertyCorrelationTable'
 import { AvailableFeature } from '~/types'
 
 import './FunnelCorrelation.scss'
 
 export const FunnelCorrelation = (): JSX.Element | null => {
-    const { insightProps, isUsingDataExploration: dx } = useValues(insightLogic)
-    const { steps: legacySteps } = useValues(funnelLogic(insightProps))
+    const { insightProps } = useValues(insightLogic)
     const { steps } = useValues(funnelDataLogic(insightProps))
     useMountedLogic(funnelCorrelationUsageLogic(insightProps))
 
-    if (dx ? steps.length <= 1 : legacySteps.length <= 1) {
+    if (steps.length <= 1) {
         return null
     }
 
@@ -35,10 +27,10 @@ export const FunnelCorrelation = (): JSX.Element | null => {
             <h2 className="my-4">Correlation analysis</h2>
             <PayGateMini feature={AvailableFeature.CORRELATION_ANALYSIS}>
                 <div className="funnel-correlation">
-                    {dx ? <FunnelCorrelationSkewWarningDataExploration /> : <FunnelCorrelationSkewWarning />}
-                    {dx ? <FunnelCorrelationTableDataExploration /> : <FunnelCorrelationTable />}
+                    <FunnelCorrelationSkewWarning />
+                    <FunnelCorrelationTable />
                     <FunnelCorrelationFeedbackForm />
-                    {dx ? <FunnelPropertyCorrelationTableDataExploration /> : <FunnelPropertyCorrelationTable />}
+                    <FunnelPropertyCorrelationTable />
                 </div>
             </PayGateMini>
         </>
