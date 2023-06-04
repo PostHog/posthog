021f92746cbc5c54bf470b4750064006055a9bbc
Paul D'Ambra <paul@posthog.com>
2023-06-04 08:39:04 +0100

    fiddling

--- frontend/src/lib/components/Errors/ErrorDisplay.tsx
+++ frontend/src/lib/components/Errors/ErrorDisplay.tsx
@@ -1,10 +1,38 @@
 import { EventType, RecordingEventType } from '~/types'
 import { LemonTag } from 'lib/lemon-ui/LemonTag/LemonTag'
-import { LemonSnack } from 'lib/lemon-ui/LemonSnack/LemonSnack'
 import { IconFlag } from 'lib/lemon-ui/icons'
+import clsx from 'clsx'
 
 function StackTrace({}: { rawTrace: string }): JSX.Element | null {
     return null
+}
+
+function TitledSnack({ title, value }: { title: string; value: string }): JSX.Element {
+    return (
+        <div className={'flex flex-row items-center'}>
+            <span
+                className={clsx(
+                    'pl-1.5 pr-1 py-1 max-w-full',
+                    'border-r',
+                    'rounded-l rounded-r-none',
+                    'text-primary-alt overflow-hidden text-ellipsis bg-primary-highlight',
+                    'inline-flex items-center'
+                )}
+            >
+                <strong>{title}:</strong>
+            </span>
+            <span
+                className={clsx(
+                    'pr-1.5 pl-1 py-1 max-w-full',
+                    'rounded-r rounded-l-none',
+                    'text-primary-alt overflow-hidden text-ellipsis bg-primary-highlight',
+                    'inline-flex items-center'
+                )}
+            >
+                {value}
+            </span>
+        </div>
+    )
 }
 
 export function ErrorDisplay({ event }: { event: EventType | RecordingEventType }): JSX.Element {
@@ -16,6 +44,7 @@ export function ErrorDisplay({ event }: { event: EventType | RecordingEventType 
         $exception_type,
         $exception_message,
         $exception_stack_trace_raw,
+        $exception_synthetic,
         $lib,
         $lib_version,
         $browser,
@@ -29,15 +58,10 @@ export function ErrorDisplay({ event }: { event: EventType | RecordingEventType 
             <h1 className={'mb-0'}>{$exception_message}</h1>
             <div className={'flex flex-row gap-2 flex-wrap'}>
                 <LemonTag type={'caution'}>{$exception_type}</LemonTag>
-                <LemonSnack color={'primary-highlight'}>
-                    library: {$lib} {$lib_version}
-                </LemonSnack>
-                <LemonSnack color={'primary-highlight'}>
-                    browser: {$browser} {$browser_version}
-                </LemonSnack>
-                <LemonSnack color={'primary-highlight'}>
-                    OS: {$os} {$os_version}
-                </LemonSnack>
+                <TitledSnack title={'synthetic'} value={$exception_synthetic ? 'true' : 'false'} />
+                <TitledSnack title={'library'} value={`${$lib} ${$lib_version}`} />
+                <TitledSnack title={'browser'} value={`${$browser} ${$browser_version}`} />
+                <TitledSnack title={'os'} value={`${$os} ${$os_version}`} />
             </div>
             <div className={'flex flex-col gap-1 mt-6'}>
                 <h2 className={'text-sm'}>Active Feature Flags</h2>
--- frontend/src/queries/nodes/DataTable/DataTable.tsx
+++ frontend/src/queries/nodes/DataTable/DataTable.tsx
@@ -469,6 +469,7 @@ export function DataTable({ query, setQuery, context, cachedResults }: DataTable
                                 'DataTable__row--category_row': !!label,
                                 'border border-danger-dark bg-danger-highlight':
                                     result && result[0] && result[0]['event'] === '$exception',
+                                [`row-type-${result?.[0]?.event}`]: result && result[0] && result[0]['event'],
                             })
                         }
                     />
