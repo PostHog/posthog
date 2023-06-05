d70c62694e7b12c184c14eac2cf1b99374b6aa82
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-06-05 14:03:53 -0700

    actually re-fetch

--- posthog/api/authentication.py
+++ posthog/api/authentication.py
@@ -345,9 +345,10 @@ class PasswordResetCompleteViewSet(NonCreatingViewSetMixin, mixins.RetrieveModel
 
 class PasswordResetTokenGenerator(DefaultPasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
-        if not isinstance(user, User):
-            raise TypeError("user is not of type User")
-        return f"{user.pk}{user.email}{user.requested_password_reset_at}{timestamp}"
+        # Due to type differences between the user model and the token generator, we need to
+        # re-fetch the user from the database to get the correct type.
+        usable_user: User = User.objects.get(pk=user.pk)
+        return f"{user.pk}{user.email}{usable_user.requested_password_reset_at}{timestamp}"
 
 
 password_reset_token_generator = PasswordResetTokenGenerator()
