785d998e52734ab3e4a0c22edd4d352912612e8e
Paul D'Ambra <paul@posthog.com>
2023-06-07 16:16:19 +0100

    fix

--- ee/models/test/test_property_definition_model.py
+++ ee/models/test/test_property_definition_model.py
@@ -8,11 +8,9 @@ class TestPropertyDefinition(BaseTest):
     def test_errors_on_invalid_verified_by_type(self):
         with pytest.raises(ValueError):
             EnterprisePropertyDefinition.objects.create(
-                team=self.team, name="enterprise property", owner=self.user, verified_by="Not user id"  # type: ignore
+                team=self.team, name="enterprise property", verified_by="Not user id"  # type: ignore
             )
 
     def test_default_verified_false(self):
-        definition = EnterprisePropertyDefinition.objects.create(
-            team=self.team, name="enterprise property", owner=self.user
-        )
+        definition = EnterprisePropertyDefinition.objects.create(team=self.team, name="enterprise property")
         assert definition.verified is False
