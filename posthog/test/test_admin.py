695caf3eeabff494be28bba20f0eca6b21559047
Ben White <ben@posthog.com>
2023-06-07 15:21:27 +0200

    Fix test

--- posthog/test/test_admin.py
+++ posthog/test/test_admin.py
@@ -6,13 +6,6 @@ from posthog.test.base import APIBaseTest
 class TestAdminCloud(APIBaseTest):
     initial_cloud_mode = True
 
-    def test_non_staff_cant_view_django_admin(self):
-        self.user.is_staff = False
-        self.user.save()
-
-        response = self.client.get("/admin/", follow=True)
-        self.assertRedirects(response, "/login?next=/admin/")
-
     def test_staff_can_view_django_admin(self):
         self.user.is_staff = True
         self.user.save()
