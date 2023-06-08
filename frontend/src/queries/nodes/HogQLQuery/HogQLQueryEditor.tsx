e84315a5090bae5c05676de6eeb78947cce56dc6
Marius Andra <marius.andra@gmail.com>
2023-06-08 17:01:53 +0200

    this was causing tooltips to hide

--- frontend/src/queries/nodes/HogQLQuery/HogQLQueryEditor.tsx
+++ frontend/src/queries/nodes/HogQLQuery/HogQLQueryEditor.tsx
@@ -44,7 +44,7 @@ export function HogQLQueryEditor(props: HogQLQueryEditorProps): JSX.Element {
             </div>
             <div
                 data-attr="hogql-query-editor"
-                className={'flex flex-col p-2 bg-border space-y-2 resize-y overflow-auto h-80 w-full'}
+                className={'flex flex-col p-2 bg-border space-y-2 resize-y h-80 w-full'}
             >
                 <div className="flex-1">
                     <AutoSizer disableWidth>
