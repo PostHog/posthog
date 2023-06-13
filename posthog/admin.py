db939601751bb410281847768da2c6dc451b3d22
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-06-14 00:12:12 +0200

    organize team settings in fieldsets

--- posthog/admin.py
+++ posthog/admin.py
@@ -74,17 +74,64 @@ class GroupTypeMappingInline(admin.TabularInline):
 class TeamAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "organization_link", "organization_id")
     search_fields = ("id", "name", "organization__id", "organization__name", "api_token")
-    exclude = [
-        "event_names",
-        "event_names_with_usage",
-        "plugins_opt_in",
-        "event_properties",
-        "event_properties_with_usage",
-        "event_properties_numerical",
-        "session_recording_retention_period_days",
-    ]
     readonly_fields = ["organization", "primary_dashboard", "test_account_filters"]
     inlines = [GroupTypeMappingInline]
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "name",
+                    "organization",
+                ],
+            },
+        ),
+        (
+            "General",
+            {
+                "classes": ["collapse"],
+                "fields": [
+                    "api_token",
+                    "timezone",
+                    "slack_incoming_webhook",
+                    "primary_dashboard",
+                ],
+            },
+        ),
+        (
+            "Onboarding",
+            {
+                "classes": ["collapse"],
+                "fields": ["is_demo", "completed_snippet_onboarding", "ingested_event", "signup_token"],
+            },
+        ),
+        (
+            "Settings",
+            {
+                "classes": ["collapse"],
+                "fields": [
+                    "anonymize_ips",
+                    "autocapture_opt_out",
+                    "autocapture_exceptions_opt_in",
+                    "session_recording_opt_in",
+                    "capture_console_log_opt_in",
+                    "capture_performance_opt_in",
+                    "data_attributes",
+                    "session_recording_version",
+                    "access_control",
+                    "inject_web_apps",
+                    "extra_settings",
+                ],
+            },
+        ),
+        (
+            "Filters",
+            {
+                "classes": ["collapse"],
+                "fields": ["test_account_filters", "test_account_filters_default_checked", "path_cleaning_filters"],
+            },
+        ),
+    ]
 
     def organization_link(self, team: Team):
         return format_html(
