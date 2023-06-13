d16eebbe4a2c23ef79993883d20d0b8199540526
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-06-13 16:44:04 +0200

    add more tests

--- posthog/hogql/test/test_query.py
+++ posthog/hogql/test/test_query.py
@@ -1017,3 +1017,39 @@ class TestQuery(ClickhouseTestMixin, APIBaseTest):
                 f"GROUP BY PIVOT_FUNCTION_1.col_a) AS PIVOT_FUNCTION_2) AS final ORDER BY final.col_a ASC LIMIT 100 "
                 f"SETTINGS readonly=1, max_execution_time=60",
             )
+
+    def test_time_window_functions(self):
+        query = """
+            SELECT 
+                tumble(toDateTime('2020-01-01'), toIntervalDay('1')),
+                tumbleStart(toDateTime('2020-01-01'), toIntervalDay('1')),
+                tumbleEnd(toDateTime('2020-01-01'), toIntervalDay('1')),
+                hop(toDateTime('2020-01-01'), toIntervalDay('1'), toIntervalDay('2')),
+                hopStart(toDateTime('2020-01-01'), toIntervalDay('1'), toIntervalDay('2')),
+                hopEnd(toDateTime('2020-01-01'), toIntervalDay('1'), toIntervalDay('2'))
+        """
+
+        response = execute_hogql_query(
+            query,
+            team=self.team,
+        )
+
+        self.assertEqual(
+            response.results,
+            [
+                (
+                    (
+                        datetime.datetime(2020, 1, 1, 0, 0, tzinfo=timezone.utc),
+                        datetime.datetime(2020, 1, 2, 0, 0, tzinfo=timezone.utc),
+                    ),
+                    datetime.datetime(2020, 1, 1, 0, 0, tzinfo=timezone.utc),
+                    datetime.datetime(2020, 1, 2, 0, 0, tzinfo=timezone.utc),
+                    (
+                        datetime.datetime(2019, 12, 31, 0, 0, tzinfo=timezone.utc),
+                        datetime.datetime(2020, 1, 2, 0, 0, tzinfo=timezone.utc),
+                    ),
+                    datetime.datetime(2019, 12, 31, 0, 0, tzinfo=timezone.utc),
+                    datetime.datetime(2020, 1, 2, 0, 0, tzinfo=timezone.utc),
+                )
+            ],
+        )
