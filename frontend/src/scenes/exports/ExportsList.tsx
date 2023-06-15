a3f54c95e5a889acc9ca16d298187b1f94b14cae
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-06-15 10:45:48 +0200

    feat: Add delete button for exports

--- frontend/src/scenes/exports/ExportsList.tsx
+++ frontend/src/scenes/exports/ExportsList.tsx
@@ -8,6 +8,7 @@ import { useCurrentTeamId, useExports, useExportAction, useDeleteExport } from '
 import { LemonTable } from '../../lib/lemon-ui/LemonTable'
 import { Link } from 'lib/lemon-ui/Link'
 import clsx from 'clsx'
+import { useCallback } from 'react'
 
 export const scene: SceneExport = {
     component: Exports,
@@ -106,6 +107,16 @@ export function Exports(): JSX.Element {
                             )
 
                             const { deleteExport, error: deleteError } = useDeleteExport(currentTeamId, export_.id)
+
+                            const deleteCallback = useCallback(() => {
+                                exports.pop(index)
+                            })
+
+                            const { deleteExport, deleting, _ } = useDeleteExport(
+                                currentTeamId,
+                                export_.id,
+                                deleteCallback
+                            )
 
                             return (
                                 <div className={clsx('flex flex-wrap gap-2')}>
