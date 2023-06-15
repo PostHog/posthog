41b348e03b060f2bbdf8812298ca017b23b623c3
Paul D'Ambra <paul@posthog.com>
2023-06-15 21:04:19 +0100

    remove dangling data table change

--- frontend/src/queries/nodes/DataTable/dataTableLogic.ts
+++ frontend/src/queries/nodes/DataTable/dataTableLogic.ts
@@ -169,7 +169,6 @@ export const dataTableLogic = kea<dataTableLogicType>([
                                 ? context.showOpenEditorButton
                                 : query.showOpenEditorButton ?? true,
                     }),
-                    columnFormats: query.columnFormats ?? {},
                 }
             },
         ],
