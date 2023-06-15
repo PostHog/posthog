37f207997ffdcec76b2c7f2d99147657d2c738ab
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2023-06-15 09:46:27 -0300

    fix: remove 0044 ch migration (#16081)

--- posthog/clickhouse/migrations/0044_drop_projections.py
+++
@@ -1,7 +0,0 @@
-from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-
-operations = [
-    run_sql_with_exceptions(
-        "ALTER TABLE sharded_events DROP PROJECTION IF EXISTS fast_max_kafka_timestamp_sharded_events"
-    ),
-]
