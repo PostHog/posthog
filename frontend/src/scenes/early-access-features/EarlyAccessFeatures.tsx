1d00487566e1bd280df02febe525bbf4b16c3b96
eric <eeoneric@gmail.com>
2023-06-16 15:49:26 -0400

    adjustments

--- frontend/src/scenes/early-access-features/EarlyAccessFeatures.tsx
+++ frontend/src/scenes/early-access-features/EarlyAccessFeatures.tsx
@@ -5,9 +5,7 @@ import { SceneExport } from 'scenes/sceneTypes'
 import { urls } from 'scenes/urls'
 import { EarlyAccessFeatureType, ProductKey } from '~/types'
 import { earlyAccessFeaturesLogic } from './earlyAccessFeaturesLogic'
-import { FEATURE_FLAGS } from 'lib/constants'
 import { userLogic } from 'scenes/userLogic'
-import { featureFlagLogic as enabledFeatureFlagsLogic } from 'lib/logic/featureFlagLogic'
 import { ProductIntroduction } from 'lib/components/ProductIntroduction/ProductIntroduction'
 import { router } from 'kea-router'
 
@@ -28,12 +26,9 @@ const STAGES_IN_ORDER: Record<EarlyAccessFeatureType['stage'], number> = {
 export function EarlyAccessFeatures(): JSX.Element {
     const { earlyAccessFeatures, earlyAccessFeaturesLoading } = useValues(earlyAccessFeaturesLogic)
     const { user } = useValues(userLogic)
-    const { featureFlags } = useValues(enabledFeatureFlagsLogic)
 
     const shouldShowEmptyState = earlyAccessFeatures.length == 0 && !earlyAccessFeaturesLoading
-    const shouldShowProductIntroduction =
-        !user?.has_seen_product_intro_for?.[ProductKey.EARLY_ACCESS_FEATURES] &&
-        !!featureFlags[FEATURE_FLAGS.SHOW_PRODUCT_INTRO_EXISTING_PRODUCTS]
+    const shouldShowProductIntroduction = !user?.has_seen_product_intro_for?.[ProductKey.EARLY_ACCESS_FEATURES]
 
     const showIntro = shouldShowProductIntroduction || shouldShowEmptyState
     return (
@@ -74,9 +69,9 @@ export function EarlyAccessFeatures(): JSX.Element {
                 <ProductIntroduction
                     productName="Early Access Features"
                     productKey={ProductKey.EARLY_ACCESS_FEATURES}
-                    thingName="an early access feature"
+                    thingName="feature"
                     description="Allow your users to individually enable or disable features that are in public beta."
-                    isEmpty={earlyAccessFeatures.length == 0}
+                    isEmpty={shouldShowEmptyState}
                     docsURL="https://posthog.com/docs/feature-flags/early-access-feature-management"
                     action={() => router.actions.push(urls.earlyAccessFeature('new'))}
                 />
