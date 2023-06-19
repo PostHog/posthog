4259b2a001b291f96c383886f0dbc7f2bb7a774c
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-06-19 13:24:43 +0200

    test: Test pause button in BatchExports frontend

---
+++ frontend/src/scenes/exports/ExportsList.spec.tsx
@@ -0,0 +1,57 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+
+import { createExportServiceHandlers } from './api-mocks'
+import { ExportActionButtons } from './ExportsList'
+import { initKeaTests } from '../../test/init'
+import { useMocks } from '../../mocks/jest'
+
+describe('ExportActionButtons', () => {
+    it('renders a pause button that can be clicked to pause an export', async () => {
+        const exportId = 123
+        const name = `test-export-${Math.random().toString(36).substring(7)}`
+
+        const testExports = {
+            123: {
+                id: exportId,
+                name: name,
+                team_id: 1,
+                status: 'RUNNING',
+                paused: false,
+                created_at: new Date().toISOString(),
+                last_updated_at: new Date().toISOString(),
+            },
+        }
+        const { exports, handlers } = createExportServiceHandlers(testExports)
+        useMocks(handlers)
+        initKeaTests()
+
+        render(
+            <ExportActionButtons
+                currentTeamId={1}
+                export_={exports[exportId]}
+                loading={false}
+                updateCallback={() => {}}
+            />
+        )
+
+        const pauseButton = await waitFor(() => {
+            const pauseButton = screen.getByRole('button', { name: 'Pause this BatchExport' })
+            expect(pauseButton).toBeInTheDocument()
+            return pauseButton
+        })
+
+        userEvent.click(pauseButton)
+
+        await waitFor(() => {
+            const [export_] = Object.values(exports).filter((export_: any) => export_.name === name)
+            expect(export_).toEqual(
+                expect.objectContaining({
+                    name,
+                    paused: true,
+                })
+            )
+        })
+    })
+})
