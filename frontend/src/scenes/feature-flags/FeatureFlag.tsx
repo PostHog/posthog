11ff3bc4ff6216bc3e689b411476856228fef544
Michael Matloka <michal@matloka.com>
2023-06-22 18:48:44 +0200

    Fix more edge cases

--- frontend/src/scenes/feature-flags/FeatureFlag.tsx
+++ frontend/src/scenes/feature-flags/FeatureFlag.tsx
@@ -125,7 +125,7 @@ export function FeatureFlag({ id }: { id?: string } = {}): JSX.Element {
     if (featureFlagLoading) {
         return (
             // TODO: This should be skeleton loaders
-            <SpinnerOverlay />
+            <SpinnerOverlay sceneLevel />
         )
     }
 
