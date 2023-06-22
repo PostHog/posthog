96d9b715f9946c1dcd93c4ef5c41f2024ef48eac
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-06-22 15:19:57 +0200

    fix: Make default NOW64 to use type precision

--- posthog/clickhouse/migrations/0045_add_insertion_timestamp_to_events.py
+++ posthog/clickhouse/migrations/0045_add_insertion_timestamp_to_events.py
@@ -6,7 +6,7 @@ from posthog.settings import CLICKHOUSE_CLUSTER
 ADD_COLUMNS_BASE_SQL = """
 ALTER TABLE {table}
 ON CLUSTER '{cluster}'
-ADD COLUMN IF NOT EXISTS inserted_at DateTime64(6, 'UTC') DEFAULT NOW()
+ADD COLUMN IF NOT EXISTS inserted_at DateTime64(6, 'UTC') DEFAULT NOW64()
 """
 
 
