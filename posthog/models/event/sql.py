e2aff1c3374220f27ec6fa7047d60b61a24712c4
Michael Matloka <michal@matloka.com>
2023-06-23 19:57:43 +0200

    Fix excess parenthesis

--- posthog/models/event/sql.py
+++ posthog/models/event/sql.py
@@ -324,7 +324,7 @@ FROM events WHERE uuid = %(event_id)s AND team_id = %(team_id)s
 
 NULL_SQL = """
 -- Creates zero values for all date axis ticks for the given date_from, date_to range
-SELECT toUInt16(0) AS total, {date_to_truncated} - {interval_func}(number)) AS day_start
+SELECT toUInt16(0) AS total, {date_to_truncated} - {interval_func}(number) AS day_start
 
 -- Get the number of `intervals` between date_from and date_to.
 --
