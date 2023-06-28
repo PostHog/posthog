717b13224c99e0861cc49cdf332bcefeb6e1eabe
Paul D'Ambra <paul@posthog.com>
2023-06-28 12:14:12 +0100

    fix up?

--- posthog/clickhouse/client/migration_tools.py
+++ posthog/clickhouse/client/migration_tools.py
@@ -1,16 +1,19 @@
 from typing import Dict
 
-from infi.clickhouse_orm import migrations
+from infi.clickhouse_orm import migrations, RunPython
 
 from posthog.clickhouse.client.execute import sync_execute
 
 
-def run_sql_with_exceptions(sql: str, settings=Dict | None):
+def run_sql_with_exceptions(sql: str, settings=Dict | None) -> RunPython:
     """
     migrations.RunSQL does not raise exceptions, so we need to wrap it in a function that does.
     """
 
+    if settings is None:
+        settings = {}
+
     def run_sql(database):
-        sync_execute(sql, settings=settings or {})
+        sync_execute(sql, settings=settings)
 
     return migrations.RunPython(run_sql)
