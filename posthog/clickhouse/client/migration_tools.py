aebf4549db142d20bd9fece7ebed0975dae2d80e
Paul D'Ambra <paul@posthog.com>
2023-06-28 11:52:20 +0100

    fix: migration settings apply to ddl timeout and drop tables

--- posthog/clickhouse/client/migration_tools.py
+++ posthog/clickhouse/client/migration_tools.py
@@ -1,14 +1,16 @@
+from typing import Dict
+
 from infi.clickhouse_orm import migrations
 
 from posthog.clickhouse.client.execute import sync_execute
 
 
-def run_sql_with_exceptions(sql: str, settings={}):
+def run_sql_with_exceptions(sql: str, settings=Dict | None):
     """
     migrations.RunSQL does not raise exceptions, so we need to wrap it in a function that does.
     """
 
     def run_sql(database):
-        sync_execute(sql, settings=settings)
+        sync_execute(sql, settings=settings or {})
 
     return migrations.RunPython(run_sql)
