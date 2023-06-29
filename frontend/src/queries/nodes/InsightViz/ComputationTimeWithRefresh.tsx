a33d8bab2a6583ab03c7ee8461999475d0557dd7
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-06-29 16:54:00 +0200

    fix computation time display

--- frontend/src/queries/nodes/InsightViz/ComputationTimeWithRefresh.tsx
+++ frontend/src/queries/nodes/InsightViz/ComputationTimeWithRefresh.tsx
@@ -1,16 +1,13 @@
 import { useValues } from 'kea'
 import { dayjs } from 'lib/dayjs'
-import { usePeriodicRerender } from 'lib/hooks/usePeriodicRerender'
 import { dataNodeLogic } from '../DataNode/dataNodeLogic'
 
 export function ComputationTimeWithRefresh(): JSX.Element | null {
     const { lastRefresh } = useValues(dataNodeLogic)
 
-    usePeriodicRerender(15000)
+    if (!lastRefresh) {
+        return null
+    }
 
-    return (
-        <div className="flex items-center text-muted-alt">
-            Computed {lastRefresh ? dayjs(lastRefresh).fromNow() : 'a while ago'}
-        </div>
-    )
+    return <div className="flex items-center text-muted-alt">Computed {dayjs(lastRefresh).fromNow()}</div>
 }
