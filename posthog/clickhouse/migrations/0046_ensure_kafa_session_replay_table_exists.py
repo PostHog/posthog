f23292aa09b2dcf9ba7aefe5b8f2fb00ba3ff7e6
Paul D'Ambra <paul@posthog.com>
2023-06-29 15:38:46 +0100

    typo

--- posthog/clickhouse/migrations/0046_ensure_kafa_session_replay_table_exists.py
+++ posthog/clickhouse/migrations/0046_ensure_kafa_session_replay_table_exists.py
@@ -1,6 +1,6 @@
 operations = [
     # this migration has been amended to be entirely No-op
-    # it has applied successfuly in Prod US where it was a no-op
+    # it has applied successfully in Prod US where it was a no-op
     # as all tables/columns it affected already existed
     # it failed to apply in prod EU but there the change has been applied manually
     # keeping it here so that hopefully it applies in EU
