2fb27f2bac91637fce3483b03776a308b50e1d38
Paul D'Ambra <paul@posthog.com>
2023-07-04 11:52:17 +0100

    chore: fix session recording storybook

--- frontend/src/scenes/session-recordings/SessionsRecordings.stories.tsx
+++ frontend/src/scenes/session-recordings/SessionsRecordings.stories.tsx
@@ -8,7 +8,6 @@ import { App } from 'scenes/App'
 import recordingSnapshotsJson from 'scenes/session-recordings/__mocks__/recording_snapshots.json'
 import recordingMetaJson from 'scenes/session-recordings/__mocks__/recording_meta.json'
 import recordingEventsJson from 'scenes/session-recordings/__mocks__/recording_events_query'
-import { SessionRecordingPlayer } from 'scenes/session-recordings/player/SessionRecordingPlayer'
 
 export default {
     title: 'Scenes-App/Recordings',
@@ -16,13 +15,14 @@ export default {
         layout: 'fullscreen',
         options: { showPanel: false },
         viewMode: 'story',
-        testOptions: { skip: true }, // FIXME: Start taking snapshots once the stories no longer crash
     },
     decorators: [
         mswDecorator({
             get: {
                 '/api/projects/:team_id/session_recordings': { results: recordings },
-                '/api/projects/:team/session_recordings/:id/snapshots': { result: recordingSnapshotsJson },
+                '/api/projects/:team_id/session_recording_playlists': { results: [] },
+                // without the session-recording-blob-replay feature flag, we only load via ClickHouse
+                '/api/projects/:team/session_recordings/:id/snapshots': recordingSnapshotsJson,
                 '/api/projects/:team/session_recordings/:id': { result: recordingMetaJson },
             },
             post: {
@@ -39,17 +39,9 @@ export function RecordingsList(): JSX.Element {
     return <App />
 }
 
-export function Recording(): JSX.Element {
+export function SecondRecordingInList(): JSX.Element {
     useEffect(() => {
-        router.actions.push(combineUrl(urls.replay(), undefined, { sessionRecordingId: recordings[0].id }).url)
+        router.actions.push(combineUrl(urls.replay(), undefined, { sessionRecordingId: recordings[1].id }).url)
     }, [])
     return <App />
-}
-
-export function NewRecording(): JSX.Element {
-    return (
-        <div>
-            <SessionRecordingPlayer sessionRecordingId={recordings[0].id} playerKey={'storybook'} />
-        </div>
-    )
 }
