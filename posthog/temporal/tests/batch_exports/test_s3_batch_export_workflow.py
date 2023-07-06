1e80f3adfab6a6f5daa6fc327812495f98ff998e
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-07-06 14:26:48 +0200

    test: Add unit tests for de-duplication

--- posthog/temporal/tests/batch_exports/test_s3_batch_export_workflow.py
+++ posthog/temporal/tests/batch_exports/test_s3_batch_export_workflow.py
@@ -134,6 +134,36 @@ def amaterialize(table: Literal["events", "person", "groups"], column: str):
     return materialize(table, column)
 
 
+def assert_events_in_s3(s3_client, bucket_name, key_prefix, events):
+    """Assert provided events written to JSON in key_prefix in S3 bucket_name."""
+    # List the objects in the bucket with the prefix.
+    objects = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=key_prefix)
+
+    # Check that there is only one object.
+    assert len(objects.get("Contents", [])) == 1
+
+    # Get the object.
+    key = objects["Contents"][0].get("Key")
+    assert key
+    object = s3_client.get_object(Bucket=bucket_name, Key=key)
+    data = object["Body"].read()
+
+    # Check that the data is correct.
+    json_data = [json.loads(line) for line in data.decode("utf-8").split("\n") if line]
+    # Pull out the fields we inserted only
+
+    json_data.sort(key=lambda x: x["timestamp"])
+
+    # Remove team_id, _timestamp from events
+    expected_events = [{k: v for k, v in event.items() if k not in ["team_id", "_timestamp"]} for event in events]
+    expected_events.sort(key=lambda x: x["timestamp"])
+
+    # First check one event, the first one, so that we can get a nice diff if
+    # the included data is different.
+    assert json_data[0] == expected_events[0]
+    assert json_data == expected_events
+
+
 @pytest.mark.django_db
 @pytest.mark.asyncio
 async def test_insert_into_s3_activity_puts_data_into_s3(bucket_name, s3_client, activity_environment):
@@ -278,38 +308,12 @@ async def test_insert_into_s3_activity_puts_data_into_s3(bucket_name, s3_client,
         with mock.patch("posthog.temporal.workflows.s3_batch_export.boto3.client", side_effect=create_test_client):
             await activity_environment.run(insert_into_s3_activity, insert_inputs)
 
-    # Check that the data was written to S3.
-    # List the objects in the bucket with the prefix.
-    objects = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
-
-    # Check that there is only one object.
-    assert len(objects.get("Contents", [])) == 1
-
-    # Get the object.
-    key = objects["Contents"][0].get("Key")
-    assert key
-    object = s3_client.get_object(Bucket=bucket_name, Key=key)
-    data = object["Body"].read()
-
-    # Check that the data is correct.
-    json_data = [json.loads(line) for line in data.decode("utf-8").split("\n") if line]
-    # Pull out the fields we inserted only
-
-    json_data.sort(key=lambda x: x["timestamp"])
-
-    # Remove team_id, _timestamp from events
-    expected_events = [{k: v for k, v in event.items() if k not in ["team_id", "_timestamp"]} for event in events]
-    expected_events.sort(key=lambda x: x["timestamp"])
-
-    # First check one event, the first one, so that we can get a nice diff if
-    # the included data is different.
-    assert json_data[0] == expected_events[0]
-    assert json_data == expected_events
+    assert_events_in_s3(s3_client, bucket_name, prefix, events)
 
 
 @pytest.mark.django_db
 @pytest.mark.asyncio
-async def test_s3_export_workflow_with_minio_bucket(client: HttpClient, s3_client):
+async def test_s3_export_workflow_with_minio_bucket(client: HttpClient, s3_client, bucket_name):
     """Test that S3 Export Workflow end-to-end by using a local MinIO bucket instead of S3.
 
     It should update the batch export run status to completed, as well as updating the record
@@ -322,12 +326,13 @@ async def test_s3_export_workflow_with_minio_bucket(client: HttpClient, s3_clien
         database=settings.CLICKHOUSE_DATABASE,
     )
 
+    prefix = f"posthog-events-{str(uuid4())}"
     destination_data = {
         "type": "S3",
         "config": {
-            "bucket_name": "posthog",
+            "bucket_name": bucket_name,
             "region": "us-east-1",
-            "prefix": "posthog-events",
+            "prefix": prefix,
             "batch_window_size": 3600,
             "aws_access_key_id": "object_storage_root_user",
             "aws_secret_access_key": "object_storage_root_password",
@@ -414,3 +419,134 @@ async def test_s3_export_workflow_with_minio_bucket(client: HttpClient, s3_clien
 
         run = runs[0]
         assert run.status == "Completed"
+
+    assert_events_in_s3(s3_client, bucket_name, prefix, events)
+
+
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_s3_export_workflow_with_minio_bucket_produces_no_duplicates(client: HttpClient, s3_client, bucket_name):
+    """Test that S3 Export Workflow end-to-end by using a local MinIO bucket instead of S3.
+
+    In this particular instance of the test, we assert no duplicates are exported to S3.
+    """
+    ch_client = ChClient(
+        url=settings.CLICKHOUSE_HTTP_URL,
+        user=settings.CLICKHOUSE_USER,
+        password=settings.CLICKHOUSE_PASSWORD,
+        database=settings.CLICKHOUSE_DATABASE,
+    )
+
+    prefix = f"posthog-events-{str(uuid4())}"
+    destination_data = {
+        "type": "S3",
+        "config": {
+            "bucket_name": bucket_name,
+            "region": "us-east-1",
+            "prefix": prefix,
+            "batch_window_size": 3600,
+            "aws_access_key_id": "object_storage_root_user",
+            "aws_secret_access_key": "object_storage_root_password",
+        },
+    }
+
+    batch_export_data = {
+        "name": "my-production-s3-bucket-destination",
+        "destination": destination_data,
+        "interval": "hour",
+    }
+
+    organization = await acreate_organization("test")
+    team = await acreate_team(organization=organization)
+    batch_export = await acreate_batch_export(
+        team_id=team.pk,
+        name=batch_export_data["name"],
+        destination_data=batch_export_data["destination"],
+        interval=batch_export_data["interval"],
+    )
+
+    duplicate_id = str(uuid4())
+    duplicate_distinct_id = str(uuid4())
+    duplicate_person_id = str(uuid4())
+    events: list[EventValues] = [
+        {
+            "uuid": str(uuid4()),
+            "event": "test",
+            "timestamp": "2023-04-25 13:30:00.000000",
+            "created_at": "2023-04-25 13:30:00.000000",
+            "_timestamp": "2023-04-25 13:30:00",
+            "person_id": str(uuid4()),
+            "person_properties": {"$browser": "Chrome", "$os": "Mac OS X"},
+            "team_id": team.pk,
+            "properties": {"$browser": "Chrome", "$os": "Mac OS X"},
+            "distinct_id": str(uuid4()),
+            "elements_chain": "this is a comman, separated, list, of css selectors(?)",
+        },
+        {
+            "uuid": duplicate_id,
+            "event": "test",
+            "timestamp": "2023-04-25 14:29:00.000000",
+            "created_at": "2023-04-25 14:29:00.000000",
+            "_timestamp": "2023-04-25 14:29:00",
+            "person_id": duplicate_person_id,
+            "person_properties": {"$browser": "Chrome", "$os": "Mac OS X"},
+            "team_id": team.pk,
+            "properties": {"$browser": "Chrome", "$os": "Mac OS X"},
+            "distinct_id": duplicate_distinct_id,
+            "elements_chain": "this is a comman, separated, list, of css selectors(?)",
+        },
+    ]
+    events_with_duplicates = events + [
+        {
+            "uuid": duplicate_id,
+            "event": "test",
+            "timestamp": "2023-04-25 14:29:00.000000",
+            "created_at": "2023-04-25 14:29:00.000000",
+            "_timestamp": "2023-04-25 14:29:00",
+            "person_id": duplicate_person_id,
+            "person_properties": {"$browser": "Chrome", "$os": "Mac OS X"},
+            "team_id": team.pk,
+            "properties": {"$browser": "Chrome", "$os": "Mac OS X"},
+            "distinct_id": duplicate_distinct_id,
+            "elements_chain": "this is a comman, separated, list, of css selectors(?)",
+        }
+    ]
+
+    # Insert some data into the `sharded_events` table.
+    await insert_events(
+        client=ch_client,
+        events=events_with_duplicates,
+    )
+
+    workflow_id = str(uuid4())
+    inputs = S3BatchExportInputs(
+        team_id=team.pk,
+        batch_export_id=str(batch_export.id),
+        data_interval_end="2023-04-25 14:30:00.000000",
+        **batch_export.destination.config,
+    )
+
+    async with await WorkflowEnvironment.start_time_skipping() as activity_environment:
+        async with Worker(
+            activity_environment.client,
+            task_queue=settings.TEMPORAL_TASK_QUEUE,
+            workflows=[S3BatchExportWorkflow],
+            activities=[create_export_run, insert_into_s3_activity, update_export_run_status],
+            workflow_runner=UnsandboxedWorkflowRunner(),
+        ):
+            with mock.patch("posthog.temporal.workflows.s3_batch_export.boto3.client", side_effect=create_test_client):
+                await activity_environment.client.execute_workflow(
+                    S3BatchExportWorkflow.run,
+                    inputs,
+                    id=workflow_id,
+                    task_queue=settings.TEMPORAL_TASK_QUEUE,
+                    retry_policy=RetryPolicy(maximum_attempts=1),
+                )
+
+        runs = await afetch_batch_export_runs(batch_export_id=batch_export.id)
+        assert len(runs) == 1
+
+        run = runs[0]
+        assert run.status == "Completed"
+
+    assert_events_in_s3(s3_client, bucket_name, prefix, events)
