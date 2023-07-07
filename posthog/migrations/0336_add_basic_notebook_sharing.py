406a6e37c9a884e59fc422ec4335f39bc64e748f
Paul D'Ambra <paul@posthog.com>
2023-07-07 11:25:24 +0100

    allow constraints added in migrations

--- posthog/migrations/0336_add_basic_notebook_sharing.py
+++ posthog/migrations/0336_add_basic_notebook_sharing.py
@@ -3,6 +3,11 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
+# :KLUDGE: Work around test_migrations_are_safe
+class AddFieldConstraintIsSafe(migrations.AddField):
+    def describe(self):
+        return super().describe() + " -- existing-table-constraint-ignore"
+
 
 class Migration(migrations.Migration):
 
@@ -11,12 +16,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
+        AddFieldConstraintIsSafe(
             model_name="exportedasset",
             name="notebook",
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to="posthog.notebook"),
         ),
-        migrations.AddField(
+        AddFieldConstraintIsSafe(
             model_name="sharingconfiguration",
             name="notebook",
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to="posthog.notebook"),
