25ac1d603cbfaa89f88e27aa0472606f8f309d12
David Newell <david@posthog.com>
2023-07-10 15:50:16 +0100

    Add backlink node

---
+++ frontend/src/scenes/notebooks/Nodes/NotebookNodeBacklink.tsx
@@ -0,0 +1,82 @@
+import { mergeAttributes, Node, NodeViewProps } from '@tiptap/core'
+import { NodeViewWrapper, ReactNodeViewRenderer } from '@tiptap/react'
+import { InsightModel, NotebookNodeType } from '~/types'
+import { Link } from '@posthog/lemon-ui'
+import { IconGauge, IconBarChart, IconFlag, IconExperiment, IconLive, IconPerson, IconCohort } from 'lib/lemon-ui/icons'
+import { TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
+import { urls } from 'scenes/urls'
+
+const ICON_MAP = {
+    dashboards: <IconGauge />,
+    insights: <IconBarChart />,
+    feature_flags: <IconFlag />,
+    experiments: <IconExperiment />,
+    events: <IconLive />,
+    persons: <IconPerson />,
+    cohorts: <IconCohort />,
+}
+
+const Component = (props: NodeViewProps): JSX.Element => {
+    const type: TaxonomicFilterGroupType = props.node.attrs.type
+    const href: string = redirectOnSelectItems(props.node.attrs.id, type)
+    const title: string = props.node.attrs.title
+
+    return (
+        <NodeViewWrapper as="span">
+            <Link to={href}>
+                <span>{ICON_MAP[type]}</span>
+                <span>{title}</span>
+            </Link>
+        </NodeViewWrapper>
+    )
+}
+
+function redirectOnSelectItems(id: string, type: TaxonomicFilterGroupType): string {
+    if (type === TaxonomicFilterGroupType.Events) {
+        return urls.eventDefinition(id)
+    } else if (type === TaxonomicFilterGroupType.Cohorts) {
+        return urls.cohort(id)
+    } else if (type === TaxonomicFilterGroupType.Persons) {
+        return urls.person(id)
+    } else if (type === TaxonomicFilterGroupType.Insights) {
+        return urls.insightView(id as InsightModel['short_id'])
+    } else if (type === TaxonomicFilterGroupType.FeatureFlags) {
+        return urls.featureFlag(id)
+    } else if (type === TaxonomicFilterGroupType.Experiments) {
+        return urls.experiment(id)
+    } else if (type === TaxonomicFilterGroupType.Dashboards) {
+        return urls.dashboard(id)
+    }
+    return ''
+}
+
+export const NotebookNodeBacklink = Node.create({
+    name: NotebookNodeType.Backlink,
+    inline: true,
+    group: 'inline',
+    atom: true,
+
+    addAttributes() {
+        return {
+            id: { default: '' },
+            type: { default: '' },
+            title: { default: '' },
+        }
+    },
+
+    parseHTML() {
+        return [
+            {
+                tag: NotebookNodeType.Backlink,
+            },
+        ]
+    },
+
+    renderHTML({ HTMLAttributes }) {
+        return [NotebookNodeType.Backlink, mergeAttributes(HTMLAttributes)]
+    },
+
+    addNodeView() {
+        return ReactNodeViewRenderer(Component)
+    },
+})
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2979,6 +2979,7 @@ export enum NotebookNodeType {
     FeatureFlag = 'ph-feature-flag',
     Person = 'ph-person',
     Link = 'ph-link',
+    Backlink = 'ph-backlink',
 }
 
 export type NotebookSyncStatus = 'synced' | 'saving' | 'unsaved' | 'local'
