baee4044abb94a492331f5fb5550844ef90c39be
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-07-10 17:26:32 +0200

    fix: Do not timeout on CH reads in batch exports

--- posthog/temporal/workflows/clickhouse.py
+++ posthog/temporal/workflows/clickhouse.py
@@ -1,7 +1,8 @@
-from aiochclient import ChClient
-from aiohttp import ClientSession, TCPConnector
-from django.conf import settings
 from contextlib import asynccontextmanager
+
+from aiochclient import ChClient
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
+from django.conf import settings
 
 
 @asynccontextmanager
@@ -35,8 +36,9 @@ async def get_client():
     #        ssl_context.load_verify_locations(settings.CLICKHOUSE_CA)
     #    elif ssl_context.verify_mode is ssl.CERT_REQUIRED:
     #        ssl_context.load_default_certs(ssl.Purpose.SERVER_AUTH)
+    timeout = ClientTimeout(total=None, connect=None, sock_connect=None, sock_read=None)
     with TCPConnector(verify_ssl=False) as connector:
-        async with ClientSession(connector=connector) as session:
+        async with ClientSession(connector=connector, timeout=timeout) as session:
             client = ChClient(
                 session,
                 url=settings.CLICKHOUSE_HTTP_URL,
