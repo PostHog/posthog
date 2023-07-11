dc8a205be14d8e09400d0c096463458e9cc53382
Harry Waye <harry@posthog.com>
2023-07-11 15:48:36 +0000

    chore: add term signal handler to temporal worker
    At the moment it seems that the temporal worker is immediately
    terminated when the pod is shut down. This is not ideal as it
    means that any in-flight workflows are not given a chance to complete.

--- posthog/temporal/worker.py
+++ posthog/temporal/worker.py
@@ -1,11 +1,14 @@
+from datetime import timedelta
 from temporalio.worker import UnsandboxedWorkflowRunner, Worker
 
 from posthog.temporal.client import connect
 from posthog.temporal.workflows import ACTIVITIES, WORKFLOWS
 
+import signal
+import sys
+
 
 async def start_worker(host, port, namespace, task_queue, server_root_ca_cert=None, client_cert=None, client_key=None):
-
     client = await connect(host, port, namespace, server_root_ca_cert, client_cert, client_key)
     worker = Worker(
         client,
@@ -13,5 +16,15 @@ async def start_worker(host, port, namespace, task_queue, server_root_ca_cert=No
         workflows=WORKFLOWS,
         activities=ACTIVITIES,
         workflow_runner=UnsandboxedWorkflowRunner(),
+        graceful_shutdown_timeout=timedelta(minutes=5),
     )
+
+    # catch the TERM signal, and stop the worker gracefully
+    # https://docs.temporal.io/docs/python/worker/#graceful-shutdown
+    async def signal_handler(sig, frame):
+        await worker.shutdown()
+        sys.exit(0)
+
+    signal.signal(signal.SIGTERM, signal_handler)
+
     await worker.run()
