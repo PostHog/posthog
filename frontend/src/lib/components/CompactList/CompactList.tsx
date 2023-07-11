56d1a074bff1ea952662eb2bc68584aa6fc4b1d5
Marius Andra <marius.andra@gmail.com>
2023-07-11 15:28:58 +0200

    remove test

--- frontend/src/lib/components/CompactList/CompactList.tsx
+++ frontend/src/lib/components/CompactList/CompactList.tsx
@@ -22,7 +22,7 @@ export function CompactList({
     renderRow,
 }: CompactListProps): JSX.Element {
     return (
-        <div className="compact-list border" style={{ zoom: 2 }}>
+        <div className="compact-list border">
             <div className="compact-list-header">
                 <h3>{title}</h3>
                 {viewAllURL && <LemonButton to={viewAllURL}>View all</LemonButton>}
