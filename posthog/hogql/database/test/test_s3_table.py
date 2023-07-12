4fa0e275c42c652bb7d03ec7bd27e74a977312d0
eric <eeoneric@gmail.com>
2023-07-12 10:54:51 -0400

    adjust test

--- posthog/hogql/database/test/test_s3_table.py
+++ posthog/hogql/database/test/test_s3_table.py
@@ -83,7 +83,7 @@ class TestS3Table(BaseTest):
 
         self.assertEqual(
             clickhouse,
-            "WITH a AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_0)s, %(hogql_val_1)s)), b AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_3)s, %(hogql_val_4)s)) SELECT a.High, a.Low FROM aapl_stock AS a JOIN aapl_stock AS b ON equals(a.High, b.High) LIMIT 10",
+            "WITH a AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_0)s, %(hogql_val_1)s)), b AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_3)s, %(hogql_val_4)s)) SELECT a.High, a.Low FROM a JOIN b ON equals(a.High, b.High) LIMIT 10",
         )
 
     def test_s3_table_select_and_non_s3_join(self):
