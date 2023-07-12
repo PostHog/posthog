5b15c3d65804d6f194aec509d6cc8cf9cb03594a
eric <eeoneric@gmail.com>
2023-07-12 10:50:22 -0400

    make sure alias is accounted for

--- posthog/hogql/database/test/test_s3_table.py
+++ posthog/hogql/database/test/test_s3_table.py
@@ -40,7 +40,7 @@ class TestS3Table(BaseTest):
 
         self.assertEqual(
             clickhouse,
-            "WITH a AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_0)s, %(hogql_val_1)s)) SELECT a.High, a.Low FROM aapl_stock AS a LIMIT 10",
+            "WITH a AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_0)s, %(hogql_val_1)s)) SELECT a.High, a.Low FROM a LIMIT 10",
         )
 
     def test_s3_table_select_join(self):
