0aab9d650956412e01f13f094e5ba18351b8791f
Harry Waye <harry@microwayes.net>
2023-07-12 10:24:34 +0100

    Revert "chore(plugin-server): we split onevent and webhooks consumers (#16509)"
    This reverts commit eb86f150b3dea45db69c825da02187639db9c7b8.

--- plugin-server/src/main/ingestion-queues/on-event-handler-consumer.ts
+++ plugin-server/src/main/ingestion-queues/on-event-handler-consumer.ts
@@ -5,8 +5,43 @@ import { KAFKA_EVENTS_JSON, prefix as KAFKA_PREFIX } from '../../config/kafka-to
 import { Hub } from '../../types'
 import { status } from '../../utils/status'
 import Piscina from '../../worker/piscina'
-import { eachBatchAppsOnEventHandlers, eachBatchWebhooksHandlers } from './batch-processing/each-batch-async-handlers'
+import {
+    eachBatchAppsOnEventHandlers,
+    eachBatchAsyncHandlers,
+    eachBatchWebhooksHandlers,
+} from './batch-processing/each-batch-async-handlers'
 import { KafkaJSIngestionConsumer } from './kafka-queue'
+
+export const startAsyncHandlerConsumer = async ({
+    hub, // TODO: remove needing to pass in the whole hub and be more selective on dependency injection.
+    piscina,
+}: {
+    hub: Hub
+    piscina: Piscina
+}) => {
+    /*
+        Consumes analytics events from the Kafka topic `clickhouse_events_json`
+        and processes any onEvent plugin handlers configured for the team. This
+        also includes `exportEvents` handlers defined in plugins as these are
+        also handled via modifying `onEvent` to call `exportEvents`.
+
+        At the moment this is just a wrapper around `IngestionConsumer`. We may
+        want to further remove that abstraction in the future.
+    */
+    status.info('🔁', `Starting async handler consumer`)
+
+    const queue = buildAsyncIngestionConsumer({ hub, piscina })
+
+    await queue.start()
+
+    schedule.scheduleJob('0 * * * * *', async () => {
+        await queue.emitConsumerGroupMetrics()
+    })
+
+    const isHealthy = makeHealthCheck(queue.consumer, queue.sessionTimeout)
+
+    return { queue, isHealthy: () => isHealthy() }
+}
 
 export const startAsyncOnEventHandlerConsumer = async ({
     hub, // TODO: remove needing to pass in the whole hub and be more selective on dependency injection.
@@ -68,6 +103,17 @@ export const startAsyncWebhooksHandlerConsumer = async ({
     const isHealthy = makeHealthCheck(queue.consumer, queue.sessionTimeout)
 
     return { queue, isHealthy: () => isHealthy() }
+}
+
+// TODO: remove once we've migrated
+export const buildAsyncIngestionConsumer = ({ hub, piscina }: { hub: Hub; piscina: Piscina }) => {
+    return new KafkaJSIngestionConsumer(
+        hub,
+        piscina,
+        KAFKA_EVENTS_JSON,
+        `${KAFKA_PREFIX}clickhouse-plugin-server-async`,
+        eachBatchAsyncHandlers
+    )
 }
 
 export const buildOnEventIngestionConsumer = ({ hub, piscina }: { hub: Hub; piscina: Piscina }) => {
