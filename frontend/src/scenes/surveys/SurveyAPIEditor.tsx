59a92ea7e9dd5687acb6a099cd911b57452207fd
Li Yi Yu <li@posthog.com>
2023-07-13 13:25:22 -0400

    clean up survey api response preview

--- frontend/src/scenes/surveys/SurveyAPIEditor.tsx
+++ frontend/src/scenes/surveys/SurveyAPIEditor.tsx
@@ -3,12 +3,28 @@ import { NewSurvey } from './surveyLogic'
 import { CodeSnippet, Language } from 'lib/components/CodeSnippet'
 
 export function SurveyAPIEditor({ survey }: { survey: Survey | NewSurvey }): JSX.Element {
-    const { appearance, ...apiSurvey } = survey
+    // Make sure this is synced to SurveyAPISerializer
+    const {
+        appearance,
+        archived,
+        linked_flag,
+        targeting_flag,
+        targeting_flag_filters,
+        created_at,
+        created_by,
+        linked_flag_id,
+        ...apiSurvey
+    } = survey
+    const apiSurveyWithFlagKeys = {
+        ...apiSurvey,
+        ...(linked_flag ? { linked_flag: linked_flag.key } : {}),
+        ...(targeting_flag ? { targeting_flag: targeting_flag.key } : {}),
+    }
     return (
         <div className="flex flex-col">
             <h4 className="text-center">API survey response</h4>
             <CodeSnippet wrap language={Language.JSON}>
-                {JSON.stringify(apiSurvey, null, 2)}
+                {JSON.stringify(apiSurveyWithFlagKeys, null, 2)}
             </CodeSnippet>
         </div>
     )
