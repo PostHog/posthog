ff66de08d395815c7ac804babc48fc662be1558b
Eric Duong <eeoneric@gmail.com>
2023-07-17 19:08:14 -0400

    chore(data-warehouse): Add field names so they can be filtered (#16537)

--- posthog/hogql/database/test/test_database.py
+++ posthog/hogql/database/test/test_database.py
@@ -44,5 +44,5 @@ class TestDatabase(BaseTest):
 
         self.assertEqual(
             response.clickhouse,
-            f"WITH whatever AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_0)s, %(hogql_val_3)s, %(hogql_val_4)s, %(hogql_val_1)s, %(hogql_val_2)s)) SELECT whatever.id FROM whatever LIMIT 100 SETTINGS readonly=2, max_execution_time=60, allow_experimental_object_type=True",
+            f"WITH whatever AS (SELECT * FROM s3Cluster('posthog', %(hogql_val_0_sensitive)s, %(hogql_val_3_sensitive)s, %(hogql_val_4_sensitive)s, %(hogql_val_1)s, %(hogql_val_2)s)) SELECT whatever.id FROM whatever LIMIT 100 SETTINGS readonly=2, max_execution_time=60, allow_experimental_object_type=True",
         )
