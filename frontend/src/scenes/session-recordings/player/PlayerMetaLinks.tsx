4889d56ab3d58dcf2735acff8d0a1a4bd9dff207
David Newell <d.newell1@outlook.com>
2023-07-20 13:12:11 +0100

    feat: notebook timestamp nodes (#16613)
    * Add comment button to session recordings
    
    * Add timestamped playback node
    
    * Insert next timestamp item
    
    * Style timestamps
    
    * Bind node logic
    
    * Insert comment at the end of a timestamp list following a recording
    
    * Cleanup implementation
    
    * Appease the linter
    
    * Tweaks from pairing
    
    * Improve naming and cleanup unused references
    
    ---------
    
    Co-authored-by: Ben White <ben@posthog.com>

--- frontend/src/scenes/session-recordings/player/PlayerMetaLinks.tsx
+++ frontend/src/scenes/session-recordings/player/PlayerMetaLinks.tsx
@@ -8,17 +8,24 @@ import { IconDelete, IconLink } from 'lib/lemon-ui/icons'
 import { openPlayerShareDialog } from 'scenes/session-recordings/player/share/PlayerShare'
 import { PlaylistPopoverButton } from './playlist-popover/PlaylistPopover'
 import { LemonDialog } from 'lib/lemon-ui/LemonDialog'
+import { buildTimestampCommentContent } from 'scenes/notebooks/Nodes/NotebookNodeReplayTimestamp'
+import { useNotebookNode } from 'scenes/notebooks/Nodes/notebookNodeLogic'
+import { NotebookNodeType } from '~/types'
 
 export function PlayerMetaLinks(): JSX.Element {
     const { sessionRecordingId, logicProps } = useValues(sessionRecordingPlayerLogic)
     const { setPause, deleteRecording } = useActions(sessionRecordingPlayerLogic)
+    const nodeLogic = useNotebookNode()
+
+    const getCurrentPlayerTime = (): number => {
+        // NOTE: We pull this value at call time as otherwise it would trigger rerenders if pulled from the hook
+        return sessionRecordingPlayerLogic.findMounted(logicProps)?.values.currentPlayerTime || 0
+    }
 
     const onShare = (): void => {
         setPause()
-        // NOTE: We pull this value at call time as otherwise it would trigger rerenders if pulled from the hook
-        const currentPlayerTime = sessionRecordingPlayerLogic.findMounted(logicProps)?.values.currentPlayerTime || 0
         openPlayerShareDialog({
-            seconds: Math.floor(currentPlayerTime / 1000),
+            seconds: Math.floor(getCurrentPlayerTime() / 1000),
             id: sessionRecordingId,
         })
     }
@@ -38,16 +45,34 @@ export function PlayerMetaLinks(): JSX.Element {
         })
     }
 
+    const onComment = (): void => {
+        if (nodeLogic) {
+            const currentPlayerTime = getCurrentPlayerTime()
+
+            nodeLogic.actions.insertAfterLastNodeOfType(
+                NotebookNodeType.ReplayTimestamp,
+                buildTimestampCommentContent(currentPlayerTime, sessionRecordingId)
+            )
+        }
+    }
+
     const commonProps: Partial<LemonButtonProps> = {
         size: 'small',
     }
 
     const mode = logicProps.mode ?? SessionRecordingPlayerMode.Standard
+    const isInNotebook = !!nodeLogic
 
     return (
         <div className="flex flex-row gap-1 items-center justify-end">
             {![SessionRecordingPlayerMode.Notebook, SessionRecordingPlayerMode.Sharing].includes(mode) ? (
                 <>
+                    {isInNotebook && (
+                        <LemonButton icon={<IconLink />} onClick={onComment} {...commonProps}>
+                            <span>Comment</span>
+                        </LemonButton>
+                    )}
+
                     <LemonButton icon={<IconLink />} onClick={onShare} {...commonProps}>
                         <span>Share</span>
                     </LemonButton>
