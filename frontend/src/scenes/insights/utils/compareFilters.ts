dd71a494b611e9d53582c12b9610272b8f79ac39
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-07-20 16:35:33 +0200

    fix(insights): fix test account filter changes - alternative approach

--- frontend/src/scenes/insights/utils/compareFilters.ts
+++ frontend/src/scenes/insights/utils/compareFilters.ts
@@ -4,9 +4,12 @@ import { objectCleanWithEmpty, objectsEqual } from 'lib/utils'
 import { cleanFilters } from './cleanFilters'
 
 /** clean filters so that we can check for semantic equality with a deep equality check */
-const clean = (f: Partial<AnyFilterType>): Partial<AnyFilterType> => {
+const clean = (
+    f: Partial<AnyFilterType>,
+    test_account_filters_default_checked: boolean | undefined
+): Partial<AnyFilterType> => {
     // remove undefined values, empty array and empty objects
-    const cleanedFilters = objectCleanWithEmpty(cleanFilters(f))
+    const cleanedFilters = objectCleanWithEmpty(cleanFilters(f, test_account_filters_default_checked))
 
     cleanedFilters.events = cleanedFilters.events?.map((e) => {
         // event math `total` is the default
@@ -19,8 +22,12 @@ const clean = (f: Partial<AnyFilterType>): Partial<AnyFilterType> => {
 }
 
 /** compares to filter objects for semantical equality */
-export function compareFilters(a: Partial<AnyFilterType>, b: Partial<AnyFilterType>): boolean {
+export function compareFilters(
+    a: Partial<AnyFilterType>,
+    b: Partial<AnyFilterType>,
+    test_account_filters_default_checked: boolean | undefined
+): boolean {
     // this is not optimized for speed and does not work for many cases yet
     // e.g. falsy values are not treated the same as undefined values, unset filters are not handled, ordering of series isn't checked
-    return objectsEqual(clean(a), clean(b))
+    return objectsEqual(clean(a, test_account_filters_default_checked), clean(b, test_account_filters_default_checked))
 }
