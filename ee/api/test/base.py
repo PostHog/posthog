8d072707db79177a335727fa1660a47e123a94f5
Michael Matloka <michal@matloka.com>
2023-07-21 14:42:12 +0200

    Update messages in tests too

--- ee/api/test/base.py
+++ ee/api/test/base.py
@@ -19,7 +19,7 @@ class LicensedTestMixin:
 
     def license_required_response(
         self,
-        message: str = "This premium feature is only available in PostHog Cloud, as self-hosted licenses are no longer available for purchase. Find out how to migrate to PostHog Cloud: https://posthog.com/docs/migrate/migrate-to-cloud",
+        message: str = "This feature is part of the premium PostHog offering. Self-hosted licenses are no longer available for purchase. Please contact sales@posthog.com to discuss options.",
     ) -> Dict[str, Optional[str]]:
         return {"type": "server_error", "code": "payment_required", "detail": message, "attr": None}
 
