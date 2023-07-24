a175980abda4c95bbc94b5dcf8990678f5c76cb0
Michael Matloka <michal@matloka.com>
2023-07-24 15:34:11 +0200

    Revert "fix(insights): fix test account filter changes (#16690)"
    This reverts commit 043231fa46dd9e9c66f132d7513118d1812f16a0.

--- frontend/src/scenes/insights/utils/compareFilters.ts
+++ frontend/src/scenes/insights/utils/compareFilters.ts
@@ -4,48 +4,23 @@ import { objectCleanWithEmpty, objectsEqual } from 'lib/utils'
 import { cleanFilters } from './cleanFilters'
 
 /** clean filters so that we can check for semantic equality with a deep equality check */
-const clean = (
-    f: Partial<AnyFilterType>,
-    test_account_filters_default_checked: boolean | undefined
-): Partial<AnyFilterType> => {
+const clean = (f: Partial<AnyFilterType>): Partial<AnyFilterType> => {
     // remove undefined values, empty array and empty objects
-    const cleanedFilters = objectCleanWithEmpty(cleanFilters(f, test_account_filters_default_checked))
+    const cleanedFilters = objectCleanWithEmpty(cleanFilters(f))
 
-    // do we need an order property on events or actions?
-    const needsOrder = (cleanedFilters.events || []).length + (cleanedFilters.actions || []).length > 1
-
-    cleanedFilters.events?.forEach((e) => {
+    cleanedFilters.events = cleanedFilters.events?.map((e) => {
         // event math `total` is the default
         if (e.math === 'total') {
             delete e.math
         }
-
-        // delete unnecessary event order
-        if (!needsOrder) {
-            delete e.order
-        }
+        return e
     })
-
-    cleanedFilters.actions?.forEach((a) => {
-        // delete unnecessary action order
-        if (!needsOrder) {
-            delete a.order
-        }
-    })
-
-    // used only for persons endpoint
-    delete f.entity_type
-
     return cleanedFilters
 }
 
 /** compares to filter objects for semantical equality */
-export function compareFilters(
-    a: Partial<AnyFilterType>,
-    b: Partial<AnyFilterType>,
-    test_account_filters_default_checked: boolean | undefined
-): boolean {
+export function compareFilters(a: Partial<AnyFilterType>, b: Partial<AnyFilterType>): boolean {
     // this is not optimized for speed and does not work for many cases yet
     // e.g. falsy values are not treated the same as undefined values, unset filters are not handled, ordering of series isn't checked
-    return objectsEqual(clean(a, test_account_filters_default_checked), clean(b, test_account_filters_default_checked))
+    return objectsEqual(clean(a), clean(b))
 }
