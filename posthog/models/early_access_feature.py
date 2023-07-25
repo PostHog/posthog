af52de96a1988f0827519e2f37f5af4c3c924e82
eric <eeoneric@gmail.com>
2023-07-25 15:03:50 -0400

    update test

--- posthog/models/early_access_feature.py
+++ posthog/models/early_access_feature.py
@@ -18,7 +18,7 @@ class EarlyAccessFeature(UUIDModel):
         "posthog.FeatureFlag",
         null=True,
         blank=True,
-        on_delete=models.CASCADE,
+        on_delete=models.PROTECT,
         related_name="features",
         related_query_name="feature",
     )
