410e10ed3ca0b163aaf80fce20fd76ac45331720
David Newell <d.newell1@outlook.com>
2023-07-26 18:22:53 +0100

    feat: notebooks new line suggestions (#16737)
    * single insertion suggestion
    
    * Add tab completion function
    
    * preview styling
    
    * implement proper types
    
    * Do not expose TipTap editor externally
    
    * maintain focus after tabbing
    
    * reference child timestamp node
    
    * cleanup all Node type issues
    
    * fix type missing
    
    * add keyboard handler insertion suggestion tests
    
    * Update UI snapshots for `chromium` (2)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- cypress/e2e/notebooks.cy.ts
+++ cypress/e2e/notebooks.cy.ts
@@ -1,3 +1,5 @@
+import { urls } from 'scenes/urls'
+
 describe('Notebooks', () => {
     beforeEach(() => {
         cy.fixture('api/session-recordings/recordings.json').then((recordings) => {
@@ -10,6 +12,12 @@ describe('Notebooks', () => {
                 'loadSessionRecording'
             )
         })
+        cy.fixture('api/notebooks/notebooks.json').then((notebook) => {
+            cy.intercept('GET', /api\/projects\/\d+\/notebooks\//, { body: notebook }).as('loadNotebooksList')
+        })
+        cy.fixture('api/notebooks/notebook.json').then((notebook) => {
+            cy.intercept('GET', /api\/projects\/\d+\/notebooks\/.*\//, { body: notebook }).as('loadNotebook')
+        })
 
         cy.clickNavMenu('dashboards')
         cy.location('pathname').should('include', '/dashboard')
@@ -18,5 +26,20 @@ describe('Notebooks', () => {
     it('Notebooks are enabled', () => {
         cy.get('h1').should('contain', 'Dashboards & Notebooks')
         cy.get('li').contains('Notebooks').should('exist').click()
+    })
+
+    it('Notebooks can render rich nodes', () => {
+        cy.visit(urls.notebook('h11RoiwV'))
+        cy.get('.ph-recording.NotebookNode').should('exist')
+    })
+
+    it('Insertion suggestions can be dismissed', () => {
+        cy.visit(urls.notebookEdit('h11RoiwV'))
+        cy.get('.NotebookEditor').type('{enter}')
+
+        cy.get('.NotebookRecordingTimestamp--preview').should('exist')
+
+        cy.get('.NotebookEditor').type('{esc}')
+        cy.get('.NotebookFloatingButton .LemonButton').should('exist')
     })
 })
