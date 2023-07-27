416bb33e44dc507c2f62f21d8b5e5a4703e6492d
Ben White <ben@posthog.com>
2023-07-27 15:02:23 +0200

    Correct documentation on Session Duration

--- frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx
+++ frontend/src/scenes/insights/filters/ActionFilter/ActionFilterRow/ActionFilterRow.tsx
@@ -348,13 +348,33 @@ export function ActionFilterRow({
                                                 renderValue={(currentValue) => (
                                                     <Tooltip
                                                         title={
-                                                            <>
-                                                                Calculate{' '}
-                                                                {mathDefinitions[math ?? ''].name.toLowerCase()} from
-                                                                property <code>{currentValue}</code>. Note that only{' '}
-                                                                {name} occurences where <code>{currentValue}</code> is
-                                                                set with a numeric value will be taken into account.
-                                                            </>
+                                                            currentValue === '$session_duration' ? (
+                                                                <>
+                                                                    Calculate{' '}
+                                                                    {mathDefinitions[math ?? ''].name.toLowerCase()} of
+                                                                    the Session Duration. Unlike other properties this
+                                                                    is based on the
+                                                                    <code>$session_id</code> property associated with
+                                                                    events. The duration is derived from the time
+                                                                    difference between the first and last event for each
+                                                                    distinct
+                                                                    <code>$session_id</code>.
+                                                                    <br />
+                                                                    Learn more about how PostHog tracks sessions in{' '}
+                                                                    <a href="https://posthog.com/docs/user-guides/sessions">
+                                                                        our documentation.
+                                                                    </a>
+                                                                </>
+                                                            ) : (
+                                                                <>
+                                                                    Calculate{' '}
+                                                                    {mathDefinitions[math ?? ''].name.toLowerCase()}{' '}
+                                                                    from property <code>{currentValue}</code>. Note that
+                                                                    only {name} occurences where{' '}
+                                                                    <code>{currentValue}</code> is set with a numeric
+                                                                    value will be taken into account.
+                                                                </>
+                                                            )
                                                         }
                                                         placement="right"
                                                     >
