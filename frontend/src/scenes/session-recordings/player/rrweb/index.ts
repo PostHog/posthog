c29a164ca2278394d35612094c0de5cb5c9c5fa8
Ben White <ben@posthog.com>
2023-07-28 12:14:40 +0200

    Fix to only use proxy on cloud

--- frontend/src/scenes/session-recordings/player/rrweb/index.ts
+++ frontend/src/scenes/session-recordings/player/rrweb/index.ts
@@ -1,6 +1,7 @@
 import { ReplayPlugin } from 'rrweb/typings/types'
 import { Replayer } from 'rrweb'
 import { eventWithTime } from '@rrweb/types'
+import { preflightLogic } from 'scenes/PreflightCheck/preflightLogic'
 
 const PROXY_URL = 'https://replay.ph-proxy.dev' as const
 
@@ -30,12 +31,18 @@ export const CorsPlugin: ReplayPlugin & {
 }
 
 export const createReplayer = (events: eventWithTime[], root: Element): Replayer => {
+    const plugins: ReplayPlugin[] = []
+
+    if (preflightLogic.findMounted()?.values.preflight?.cloud) {
+        plugins.push(CorsPlugin)
+    }
+
     return new Replayer(events, {
         root,
         triggerFocus: false,
         insertStyleRules: [
             `.ph-no-capture {   background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjE2IiBoZWlnaHQ9IjE2IiBmaWxsPSJibGFjayIvPgo8cGF0aCBkPSJNOCAwSDE2TDAgMTZWOEw4IDBaIiBmaWxsPSIjMkQyRDJEIi8+CjxwYXRoIGQ9Ik0xNiA4VjE2SDhMMTYgOFoiIGZpbGw9IiMyRDJEMkQiLz4KPC9zdmc+Cg=="); }`,
         ],
-        plugins: [CorsPlugin],
+        plugins,
     })
 }
