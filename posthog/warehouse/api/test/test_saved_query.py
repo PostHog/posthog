ec15c510ff4691078e169bbfebc47556e9f4c671
Eric Duong <eeoneric@gmail.com>
2023-07-28 10:35:54 -0400

    feat(data-warehouse): Views backend (#16573)
    * backend basics for a view
    
    * view parsing "working"
    
    * add tests and make view name unique
    
    * adjust tests
    
    * api tests and edge cases
    
    * typing
    
    * try comment
    
    * fix migration check
    
    * rename
    
    * block overlapping names at api level
    
    * add model validator
    
    * more naming changes and full integration test
    
    * update migration
    
    * update migration
    
    * remove repeat
    
    * ClickHouse
    
    * regex
    
    * update validator name
    
    * update migration
    
    * update constraint
    
    * casing
    
    ---------
    
    Co-authored-by: Michael Matloka <dev@twixes.com>

---
+++ posthog/warehouse/api/test/test_saved_query.py
@@ -0,0 +1,113 @@
+from posthog.test.base import (
+    APIBaseTest,
+)
+
+
+class TestSavedQuery(APIBaseTest):
+    def test_create(self):
+        response = self.client.post(
+            f"/api/projects/{self.team.id}/warehouse_view/",
+            {
+                "name": "event_view",
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select event from events LIMIT 100",
+                },
+            },
+        )
+        self.assertEqual(response.status_code, 201, response.content)
+        saved_query = response.json()
+        self.assertEqual(saved_query["name"], "event_view")
+        self.assertEqual(saved_query["columns"], {"event": "String"})
+
+    def test_create_name_overlap_error(self):
+        response = self.client.post(
+            f"/api/projects/{self.team.id}/warehouse_view/",
+            {
+                "name": "events",
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select event from events LIMIT 100",
+                },
+            },
+        )
+        self.assertEqual(response.status_code, 400, response.content)
+
+    def test_saved_query_doesnt_exist(self):
+        saved_query_1_response = self.client.post(
+            f"/api/projects/{self.team.id}/warehouse_view/",
+            {
+                "name": "event_view",
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select * from event_view LIMIT 100",
+                },
+            },
+        )
+        self.assertEqual(saved_query_1_response.status_code, 400, saved_query_1_response.content)
+
+    def test_view_updated(self):
+        response = self.client.post(
+            f"/api/projects/{self.team.id}/warehouse_view/",
+            {
+                "name": "event_view",
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select event from events LIMIT 100",
+                },
+            },
+        )
+        self.assertEqual(response.status_code, 201, response.content)
+        saved_query_1_response = response.json()
+        saved_query_1_response = self.client.patch(
+            f"/api/projects/{self.team.id}/warehouse_view/" + saved_query_1_response["id"],
+            {
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select distinct_id from events LIMIT 100",
+                },
+            },
+        )
+
+        self.assertEqual(saved_query_1_response.status_code, 200, saved_query_1_response.content)
+        view_1 = saved_query_1_response.json()
+        self.assertEqual(view_1["name"], "event_view")
+        self.assertEqual(view_1["columns"], {"distinct_id": "String"})
+
+    def test_circular_view(self):
+        saved_query_1_response = self.client.post(
+            f"/api/projects/{self.team.id}/warehouse_view/",
+            {
+                "name": "event_view",
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select * from events LIMIT 100",
+                },
+            },
+        )
+        self.assertEqual(saved_query_1_response.status_code, 201, saved_query_1_response.content)
+        saved_query_1 = saved_query_1_response.json()
+
+        saved_view_2_response = self.client.post(
+            f"/api/projects/{self.team.id}/warehouse_view/",
+            {
+                "name": "outer_event_view",
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select event from event_view LIMIT 100",
+                },
+            },
+        )
+        self.assertEqual(saved_view_2_response.status_code, 201, saved_view_2_response.content)
+
+        saved_view_1_response = self.client.patch(
+            f"/api/projects/{self.team.id}/warehouse_view/" + saved_query_1["id"],
+            {
+                "name": "event_view",
+                "query": {
+                    "kind": "HogQLQuery",
+                    "query": f"select * from outer_event_view LIMIT 100",
+                },
+            },
+        )
+        self.assertEqual(saved_view_1_response.status_code, 400, saved_view_1_response.content)
