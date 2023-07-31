2d8e06bfcd38725cbf9bb45882a4e1e583f3e7b5
eric <eeoneric@gmail.com>
2023-07-31 15:27:34 -0400

    update test

--- posthog/warehouse/api/test/test_saved_query.py
+++ posthog/warehouse/api/test/test_saved_query.py
@@ -18,7 +18,7 @@ class TestSavedQuery(APIBaseTest):
         self.assertEqual(response.status_code, 201, response.content)
         saved_query = response.json()
         self.assertEqual(saved_query["name"], "event_view")
-        self.assertEqual(saved_query["columns"], {"event": "String"})
+        self.assertEqual(saved_query["columns"], [{"key": "event", "type": "string"}])
 
     def test_create_name_overlap_error(self):
         response = self.client.post(
@@ -72,7 +72,7 @@ class TestSavedQuery(APIBaseTest):
         self.assertEqual(saved_query_1_response.status_code, 200, saved_query_1_response.content)
         view_1 = saved_query_1_response.json()
         self.assertEqual(view_1["name"], "event_view")
-        self.assertEqual(view_1["columns"], {"distinct_id": "String"})
+        self.assertEqual(view_1["columns"], [{"key": "distinct_id", "type": "string"}])
 
     def test_circular_view(self):
         saved_query_1_response = self.client.post(
