d84e15c039d2b4f22eb001fea65471dfa3fc8e45
Paul D'Ambra <paul@posthog.com>
2023-07-31 20:58:32 +0100

    feat: a little analytics fangling (#16829)
    * fix age reporting and add play time tracking
    
    * count clicks as engagement measure
    
    * let's decouple from clicks

--- frontend/src/scenes/session-recordings/player/SessionRecordingPlayer.tsx
+++ frontend/src/scenes/session-recordings/player/SessionRecordingPlayer.tsx
@@ -65,8 +65,16 @@ export function SessionRecordingPlayer(props: SessionRecordingPlayerProps): JSX.
         mode,
         playerRef,
     }
-    const { setIsFullScreen, setPause, togglePlayPause, seekBackward, seekForward, setSpeed, closeExplorer } =
-        useActions(sessionRecordingPlayerLogic(logicProps))
+    const {
+        incrementClickCount,
+        setIsFullScreen,
+        setPause,
+        togglePlayPause,
+        seekBackward,
+        seekForward,
+        setSpeed,
+        closeExplorer,
+    } = useActions(sessionRecordingPlayerLogic(logicProps))
     const { isNotFound } = useValues(sessionRecordingDataLogic(logicProps))
     const { isFullScreen, explorerMode } = useValues(sessionRecordingPlayerLogic(logicProps))
     const speedHotkeys = useMemo(() => createPlaybackSpeedKey(setSpeed), [setSpeed])
@@ -148,6 +156,7 @@ export function SessionRecordingPlayer(props: SessionRecordingPlayerProps): JSX.
                         'SessionRecordingPlayer--inspector-focus': inspectorFocus,
                         'SessionRecordingPlayer--inspector-hidden': noInspector,
                     })}
+                    onClick={incrementClickCount}
                 >
                     <div className="SessionRecordingPlayer__main">
                         {includeMeta || isFullScreen ? <PlayerMeta /> : null}
