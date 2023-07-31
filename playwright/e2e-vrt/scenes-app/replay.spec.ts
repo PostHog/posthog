1a06a95e62db7932d5f883ec2537f57107b651bc
Paul D'Ambra <paul@posthog.com>
2023-07-31 19:26:30 +0100

    like this?

---
+++ playwright/e2e-vrt/scenes-app/replay.spec.ts
@@ -0,0 +1,18 @@
+import { toId } from '../../helpers/storybook'
+import { test } from '../../fixtures/storybook'
+
+test.describe('session replay', () => {
+    const replayScenes = [
+        'Recordings List',
+        'Recordings Play Lists',
+        'Recordings Play List No Pinned Recordings',
+        'Recordings Play List With Pinned Recordings',
+        'Session Recording In List',
+    ]
+    for (const scene in replayScenes) {
+        test(`displays {scene} page`, async ({ storyPage }) => {
+            await storyPage.goto(toId('Scenes-App/Recordings', scene))
+            await storyPage.expectFullPageScreenshot()
+        })
+    }
+})
