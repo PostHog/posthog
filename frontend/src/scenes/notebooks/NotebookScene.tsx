53dafb6830deefcee0ed4d685339a9350af61a6b
Ben White <ben@posthog.com>
2023-08-01 15:03:28 +0200

    Fix layouts

--- frontend/src/scenes/notebooks/NotebookScene.tsx
+++ frontend/src/scenes/notebooks/NotebookScene.tsx
@@ -15,6 +15,7 @@ import { notebooksListLogic } from './Notebook/notebooksListLogic'
 import { router } from 'kea-router'
 import { urls } from 'scenes/urls'
 import { LOCAL_NOTEBOOK_TEMPLATES } from './NotebookTemplates/notebookTemplates'
+import './NotebookScene.scss'
 
 interface NotebookSceneProps {
     shortId?: string
@@ -63,7 +64,7 @@ export function NotebookScene(): JSX.Element {
 
     return (
         <div className="NotebookScene">
-            <div className="flex items-center justify-between border-b py-2 mb-2 sticky top-0 bg-bg-light z-10">
+            <div className="flex items-center justify-between border-b py-2 mb-2 sticky top-0 bg-bg-3000 z-10">
                 <div className="flex gap-2 items-center">
                     {notebook?.is_template && <LemonTag type="highlight">TEMPLATE</LemonTag>}
                     <UserActivityIndicator at={notebook?.last_modified_at} by={notebook?.last_modified_by} />
