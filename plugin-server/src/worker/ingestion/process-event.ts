6cd147cfced7c21acb75658c53893e8aec60aee5
Tiina Turban <tiina303@gmail.com>
2023-08-04 19:45:03 +0200

    chore: skip sentry logging for RangeErrors

--- plugin-server/src/worker/ingestion/process-event.ts
+++ plugin-server/src/worker/ingestion/process-event.ts
@@ -128,7 +128,9 @@ export class EventsProcessor {
         try {
             await this.propertyDefinitionsManager.updateEventNamesAndProperties(team.id, event, properties)
         } catch (err) {
-            Sentry.captureException(err, { tags: { team_id: team.id } })
+            if (!(err instanceof RangeError)) {
+                Sentry.captureException(err, { tags: { team_id: team.id } })
+            }
             status.warn('⚠️', 'Failed to update property definitions for an event', {
                 event,
                 properties,
