413b2b2f45037e3a28f9b2b4cd39bb0a9d608fc3
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-08-08 00:02:50 +0200

    fix: Add support for legacy export apps schema in BatchExports

--- posthog/batch_exports/service.py
+++ posthog/batch_exports/service.py
@@ -68,7 +68,7 @@ class SnowflakeBatchExportInputs:
     table_name: str = "events"
     data_interval_end: str | None = None
     role: str | None = None
-    legacy_schema: bool = True
+    use_exports_schema: bool = True
 
 
 DESTINATION_WORKFLOWS = {
