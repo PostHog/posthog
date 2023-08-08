cf9664e86c86e37272731eddc64b5334e2b564c0
Michael Matloka <dev@twixes.com>
2023-08-08 14:14:28 +0200

    chore(actions): Replace hint about Postgres regex with RE2 (#16897)
    * chore: Replace hint about Postgres regex to RE2
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/scenes/actions/hints.tsx
+++ frontend/src/scenes/actions/hints.tsx
@@ -1,3 +1,5 @@
+import { Link } from '@posthog/lemon-ui'
+
 export const URL_MATCHING_HINTS = {
     exact: undefined,
     contains: (
@@ -6,15 +8,8 @@ export const URL_MATCHING_HINTS = {
         </>
     ),
     regex: (
-        <>
-            <a
-                href="https://www.postgresql.org/docs/current/functions-matching.html#FUNCTIONS-POSIX-REGEXP"
-                target="_blank"
-                rel="noreferrer"
-            >
-                PostgreSQL regular expression syntax
-            </a>{' '}
-            applies.
-        </>
+        <Link to="https://github.com/google/re2/wiki/Syntax" target="_blank">
+            RE2 syntax applies.
+        </Link>
     ),
 }
