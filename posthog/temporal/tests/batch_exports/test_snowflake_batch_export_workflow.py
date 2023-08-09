ddd00617990059bb1fdb3ce0c397143d7f2e45ce
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-08-09 13:33:43 +0200

    test: Do not use legacy schema in test

--- posthog/temporal/tests/batch_exports/test_snowflake_batch_export_workflow.py
+++ posthog/temporal/tests/batch_exports/test_snowflake_batch_export_workflow.py
@@ -250,6 +250,7 @@ async def test_snowflake_export_workflow_exports_events_in_the_last_hour_for_the
             "schema": "test",
             "warehouse": "COMPUTE_WH",
             "table_name": "events",
+            "legacy_schema": False,
         },
     }
 
@@ -476,6 +477,7 @@ async def test_snowflake_export_workflow_raises_error_on_put_fail():
             "schema": "test",
             "warehouse": "COMPUTE_WH",
             "table_name": "events",
+            "legacy_schema": False,
         },
     }
 
@@ -571,6 +573,7 @@ async def test_snowflake_export_workflow_raises_error_on_copy_fail():
             "schema": "test",
             "warehouse": "COMPUTE_WH",
             "table_name": "events",
+            "legacy_schema": False,
         },
     }
 
