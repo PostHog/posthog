014fefcd42ac2795910c4bf6b1a28eb0bbc9cc07
Adewole Goodnews Oluwaseun <105173154+Greatseun02@users.noreply.github.com>
2023-08-14 00:13:45 +0100

    Added Icon to copyToClipboard

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -1026,7 +1026,9 @@ export async function copyToClipboard(value: string, description: string = 'text
 
   try {
     await navigator.clipboard.writeText(value);
-    lemonToast.info(`Copied ${description} to clipboard`);
+     lemonToast.info(`Copied ${description} to clipboard`, {
+            icon: <IconCopy />,
+        })
     return true;
   } catch (e) {
     // If the Clipboard API fails, fallback to textarea method
@@ -1037,7 +1039,9 @@ export async function copyToClipboard(value: string, description: string = 'text
       textArea.select();
       document.execCommand('copy');
       document.body.removeChild(textArea);
-      lemonToast.info(`Copied ${description} to clipboard`);
+       lemonToast.info(`Copied ${description} to clipboard`, {
+            icon: <IconCopy />,
+        })
       return true;
     } catch (err) {
       lemonToast.error(`Could not copy ${description} to clipboard: ${err}`);
