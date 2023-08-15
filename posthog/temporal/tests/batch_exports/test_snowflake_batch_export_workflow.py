03ae1ddfa428982e48618fdd8b5df4711788a8e4
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-08-15 02:00:00 +0200

    refactor: Support all the schemas and be specific in Snowflake Workflow

--- posthog/temporal/tests/batch_exports/test_snowflake_batch_export_workflow.py
+++ posthog/temporal/tests/batch_exports/test_snowflake_batch_export_workflow.py
@@ -212,7 +212,6 @@ async def test_snowflake_export_workflow_exports_events_in_the_last_hour_for_the
             "schema": "test",
             "warehouse": "COMPUTE_WH",
             "table_name": "events",
-            "use_apps_schema": False,
         },
     }
 
@@ -460,7 +459,6 @@ async def test_snowflake_export_workflow_raises_error_on_put_fail():
             "schema": "test",
             "warehouse": "COMPUTE_WH",
             "table_name": "events",
-            "use_apps_schema": False,
         },
     }
 
@@ -560,7 +558,6 @@ async def test_snowflake_export_workflow_raises_error_on_copy_fail():
             "schema": "test",
             "warehouse": "COMPUTE_WH",
             "table_name": "events",
-            "use_apps_schema": False,
         },
     }
 
