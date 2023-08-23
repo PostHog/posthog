a1ef97b36515c74dea64134ed6935f64388986ea
Michael Matloka <michal@matloka.com>
2023-08-23 23:34:22 +0200

    Ensure scene is loaded before proceeding

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -115,7 +115,6 @@ async function expectStoryToMatchSnapshot(
         document.body.classList.add('storybook-test-runner')
     })
     if (waitForLoadersToDisappear) {
-        await page.waitForTimeout(300) // Wait for initial UI to load
         await Promise.all(LOADER_SELECTORS.map((selector) => page.waitForSelector(selector, { state: 'detached' })))
         if (typeof waitForLoadersToDisappear === 'string') {
             await page.waitForSelector(waitForLoadersToDisappear)
