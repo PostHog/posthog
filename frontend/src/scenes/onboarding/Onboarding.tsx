eeb94c72b0b0f44bba3da8ba30d94a292e80c985
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-08-24 13:57:51 -0700

    change design, add resources and pricing modal

--- frontend/src/scenes/onboarding/Onboarding.tsx
+++ frontend/src/scenes/onboarding/Onboarding.tsx
@@ -1,5 +1,5 @@
 import { SceneExport } from 'scenes/sceneTypes'
-import { useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import { useEffect } from 'react'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { FEATURE_FLAGS } from 'lib/constants'
@@ -10,6 +10,8 @@ import { billingProductLogic } from 'scenes/billing/billingProductLogic'
 import { convertLargeNumberToWords } from 'scenes/billing/billing-utils'
 import { BillingProductV2Type } from '~/types'
 import { LemonCard } from 'lib/lemon-ui/LemonCard/LemonCard'
+import { ProductPricingModal } from 'scenes/billing/ProductPricingModal'
+import { IconCheckCircleOutline, IconOpenInNew } from 'lib/lemon-ui/icons'
 
 export const scene: SceneExport = {
     component: Onboarding,
@@ -17,15 +19,63 @@ export const scene: SceneExport = {
 }
 
 const OnboardingProductIntro = ({ product }: { product: BillingProductV2Type }): JSX.Element => {
-    const { currentAndUpgradePlans } = useValues(billingProductLogic({ product }))
+    const { currentAndUpgradePlans, isPricingModalOpen } = useValues(billingProductLogic({ product }))
+    const { toggleIsPricingModalOpen } = useActions(billingProductLogic({ product }))
     const upgradePlan = currentAndUpgradePlans?.upgradePlan
+
+    const pricingBenefits = [
+        'Only pay for what you use',
+        'Control spend with billing limits as low as $0/mo',
+        'Generous free volume every month, forever',
+    ]
+
+    const productWebsiteKey = product.type.replace('_', '-')
+    const communityUrl = 'https://posthog.com/questions/topic/' + productWebsiteKey
+    const tutorialsUrl = 'https://posthog.com/tutorials/categories/' + productWebsiteKey
+    const productPageUrl = 'https://posthog.com/' + productWebsiteKey
+    const productImageUrl = `https://posthog.com/images/product/billing-products/${productWebsiteKey}.png`
+
     return (
         <div className="w-full">
             <div className="flex flex-col w-full p-6 bg-mid items-center justify-center">
                 <div className="max-w-lg flex flex-wrap my-8 items-center">
                     <div className="w-1/2 pr-6 min-w-80">
                         <h1 className="text-5xl font-bold">{product.name}</h1>
-                        <h2 className="font-bold">{product.description}</h2>
+                        <h2 className="font-bold mb-6">{product.description}</h2>
+                        <div className="flex gap-x-2">
+                            <LemonButton type="primary">Get started</LemonButton>
+                            {product.docs_url && (
+                                <LemonButton type="secondary" to={productPageUrl}>
+                                    Learn more
+                                </LemonButton>
+                            )}
+                        </div>
+                    </div>
+                    <div className="shrink w-1/2 min-w-80">
+                        <img src={productImageUrl} className="w-full" />
+                    </div>
+                </div>
+            </div>
+            <div className="my-12 flex justify-between mx-auto max-w-lg gap-x-8">
+                <div className="flex flex-col">
+                    <h2 className="text-3xl">Features</h2>
+                    <div className="flex flex-wrap gap-y-4 my-6 max-w-lg">
+                        {upgradePlan.features.map((feature, i) => (
+                            <li className="flex mb-2" key={`product-features-${i}`}>
+                                <div>
+                                    <IconCheckCircleOutline className="text-success mr-2 mt-1 w-6" />
+                                </div>
+                                <div>
+                                    <h4 className="font-bold mb-0">{feature.name}</h4>
+                                    <p className="m-0">{feature.description}</p>
+                                </div>
+                            </li>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <LemonCard hoverEffect={false}>
+                        <h2 className="text-3xl">Pricing</h2>
                         <p>
                             {upgradePlan?.tiers?.[0].unit_amount_usd &&
                                 parseInt(upgradePlan?.tiers?.[0].unit_amount_usd) === 0 && (
@@ -37,43 +87,54 @@ const OnboardingProductIntro = ({ product }: { product: BillingProductV2Type }):
                                         , then{' '}
                                         <span className="font-bold">${upgradePlan?.tiers?.[1].unit_amount_usd}</span>
                                         <span className="text-muted">/{product.unit}</span>.{' '}
-                                        <Link>
+                                        <Link
+                                            onClick={() => {
+                                                toggleIsPricingModalOpen()
+                                            }}
+                                        >
                                             <span className="font-bold text-brand-red">Volume discounts</span>
                                         </Link>{' '}
                                         after {convertLargeNumberToWords(upgradePlan?.tiers?.[1].up_to, null)}/mo.
                                     </p>
                                 )}
                         </p>
-                        <div className="flex gap-x-2">
-                            <LemonButton type="primary">Get started</LemonButton>
-                            {product.docs_url && (
-                                <LemonButton type="secondary" to={product.docs_url}>
-                                    Learn more
-                                </LemonButton>
-                            )}
-                        </div>
-                    </div>
-                    <div className="shrink w-1/2 min-w-80">
-                        <img
-                            src="https://posthog.com/static/fa61e27ed8df786a6f6d309db72f757b/6a3c8/product-analytics.webp"
-                            className="w-full"
-                        />
-                    </div>
+                        <ul>
+                            {pricingBenefits.map((benefit, i) => (
+                                <li className="flex mb-2 ml-6" key={`pricing-benefits-${i}`}>
+                                    <IconCheckCircleOutline className="text-success mr-2 mt-1" />
+                                    {benefit}
+                                </li>
+                            ))}
+                        </ul>
+                    </LemonCard>
+                    <LemonCard className="mt-8" hoverEffect={false}>
+                        <h2 className="text-3xl">Resources</h2>
+                        {product.docs_url && (
+                            <p>
+                                <Link to={product.docs_url}>
+                                    Documentation <IconOpenInNew />
+                                </Link>
+                            </p>
+                        )}
+                        <p>
+                            <Link to={communityUrl}>
+                                Community forum <IconOpenInNew />
+                            </Link>
+                        </p>
+                        <p>
+                            <Link to={tutorialsUrl}>
+                                Tutorials <IconOpenInNew />
+                            </Link>
+                        </p>
+                    </LemonCard>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center my-12">
-                <h2 className="text-3xl">Features</h2>
-                <div className="flex flex-wrap gap-x-6 gap-y-6 px-12 my-6 justify-center max-w-lg">
-                    {upgradePlan.features.map((feature) => (
-                        <LemonCard className="w-60" key={feature.key}>
-                            <div className="mb-2">
-                                <h3 className="bold mb-0">{feature.name}</h3>
-                            </div>
-                            <p className="grow">{feature.description}</p>
-                        </LemonCard>
-                    ))}
-                </div>
-            </div>
+            <ProductPricingModal
+                modalOpen={isPricingModalOpen}
+                onClose={toggleIsPricingModalOpen}
+                product={product}
+                planKey={upgradePlan.plan_key}
+            />
         </div>
     )
 }
