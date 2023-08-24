35c9b5b5ceea0b44fb1a24124b217d0827719d7a
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-08-24 14:08:02 -0700

    remove product type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -114,15 +114,6 @@ export enum ProductKey {
     PRODUCT_ANALYTICS = 'product_analytics',
 }
 
-export type Product = {
-    name: string
-    key: ProductKey
-    description: string
-    productUrl: string
-    productInfoUrl?: string
-    docsUrl?: string
-}
-
 export enum LicensePlan {
     Scale = 'scale',
     Enterprise = 'enterprise',
