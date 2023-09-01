adfb1c6a1c4fd529beda0f050aa399d5c949c753
David Newell <david@posthog.com>
2023-09-01 09:40:12 +0100

    feat: insight viz view props

--- frontend/src/queries/schema.ts
+++ frontend/src/queries/schema.ts
@@ -327,6 +327,10 @@ export interface InsightVizNode extends Node {
     showCorrelationTable?: boolean
     showLastComputation?: boolean
     showLastComputationRefresh?: boolean
+    showFilters?: boolean
+    showResults?: boolean
+    /** Query is embedded inside another bordered component */
+    embedded?: boolean
 }
 
 /** Base class for insight query nodes. Should not be used directly. */
