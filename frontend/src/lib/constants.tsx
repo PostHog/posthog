269de380688c2e7c20d7415f9518d379659a31cd
Paul D'Ambra <paul@posthog.com>
2023-09-05 17:06:28 +0100

    chore: flag clickhouse fallback

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -165,6 +165,8 @@ export const FEATURE_FLAGS = {
     PRODUCT_SPECIFIC_ONBOARDING: 'product-specific-onboarding', // owner: @raquelmsmith
     REDIRECT_SIGNUPS_TO_INSTANCE: 'redirect-signups-to-instance', // owner: @raquelmsmith
     APPS_AND_EXPORTS_UI: 'apps-and-exports-ui', // owner: @benjackwhite
+    // owner: #team-monitoring
+    SESSION_RECORDING_FORBID_V1_SNAPSHOTS: 'session-recording-forbid-v1-snapshots',
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
