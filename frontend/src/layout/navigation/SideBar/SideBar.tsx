ae68df6e01dde3a003e3ca61ef141233a4aea8b9
David Newell <david@posthog.com>
2023-09-07 16:24:50 +0100

    remove other changes

--- frontend/src/layout/navigation/SideBar/SideBar.tsx
+++ frontend/src/layout/navigation/SideBar/SideBar.tsx
@@ -70,34 +70,32 @@ function Pages(): JSX.Element {
     return (
         <ul>
             <div className="SideBar__heading">Project</div>
-            {currentOrganization && (
-                <PageButton
-                    title={
-                        isAuthenticatedTeam(currentTeam) ? (
-                            <>
-                                <span>
-                                    <ProjectName team={currentTeam} />
-                                </span>
-                            </>
-                        ) : (
-                            'Choose project'
-                        )
-                    }
-                    icon={<Lettermark name={currentOrganization.name} />}
-                    identifier={Scene.ProjectHomepage}
-                    to={urls.projectHomepage()}
-                    sideAction={{
-                        'aria-label': 'switch project',
-                        onClick: () => toggleProjectSwitcher(),
-                        dropdown: {
-                            visible: isProjectSwitcherShown,
-                            onClickOutside: hideProjectSwitcher,
-                            overlay: <ProjectSwitcherOverlay />,
-                            actionable: true,
-                        },
-                    }}
-                />
-            )}
+            <PageButton
+                title={
+                    isAuthenticatedTeam(currentTeam) ? (
+                        <>
+                            <span>
+                                <ProjectName team={currentTeam} />
+                            </span>
+                        </>
+                    ) : (
+                        'Choose project'
+                    )
+                }
+                icon={<Lettermark name={currentOrganization?.name} />}
+                identifier={Scene.ProjectHomepage}
+                to={urls.projectHomepage()}
+                sideAction={{
+                    'aria-label': 'switch project',
+                    onClick: () => toggleProjectSwitcher(),
+                    dropdown: {
+                        visible: isProjectSwitcherShown,
+                        onClickOutside: hideProjectSwitcher,
+                        overlay: <ProjectSwitcherOverlay />,
+                        actionable: true,
+                    },
+                }}
+            />
             {currentTeam && (
                 <>
                     <LemonDivider />
