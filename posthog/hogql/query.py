1f269b0a1d1fcc479f41a37499c2e24d4c5eda16
Marius Andra <marius.andra@gmail.com>
2023-09-08 16:37:03 +0200

    fix(hogql-timings): do not nest everything under replace_placeholders

--- posthog/hogql/query.py
+++ posthog/hogql/query.py
@@ -57,79 +57,79 @@ def execute_hogql_query(
                 )
             select_query = replace_placeholders(select_query, placeholders)
 
-        if select_query.limit is None:
-            with timings.measure("max_limit"):
-                # One more "max" of MAX_SELECT_RETURNED_ROWS (100k) in applied in the query printer, overriding this if higher.
-                from posthog.hogql.constants import DEFAULT_RETURNED_ROWS
+    if select_query.limit is None:
+        with timings.measure("max_limit"):
+            # One more "max" of MAX_SELECT_RETURNED_ROWS (100k) in applied in the query printer, overriding this if higher.
+            from posthog.hogql.constants import DEFAULT_RETURNED_ROWS
 
-                select_query.limit = ast.Constant(value=default_limit or DEFAULT_RETURNED_ROWS)
+            select_query.limit = ast.Constant(value=default_limit or DEFAULT_RETURNED_ROWS)
 
-        # Get printed HogQL query, and returned columns. Using a cloned query.
-        with timings.measure("hogql"):
-            with timings.measure("prepare_ast"):
-                hogql_query_context = HogQLContext(
-                    team_id=team.pk,
-                    enable_select_queries=True,
-                    person_on_events_mode=team.person_on_events_mode,
-                    timings=timings,
-                )
-                with timings.measure("clone"):
-                    cloned_query = clone_expr(select_query, True)
-                select_query_hogql = cast(
-                    ast.SelectQuery,
-                    prepare_ast_for_printing(node=cloned_query, context=hogql_query_context, dialect="hogql"),
-                )
-
-            with timings.measure("print_ast"):
-                hogql = print_prepared_ast(select_query_hogql, hogql_query_context, "hogql")
-                print_columns = []
-                for node in select_query_hogql.select:
-                    if isinstance(node, ast.Alias):
-                        print_columns.append(node.alias)
-                    else:
-                        print_columns.append(
-                            print_prepared_ast(
-                                node=node, context=hogql_query_context, dialect="hogql", stack=[select_query_hogql]
-                            )
-                        )
-
-        # Print the ClickHouse SQL query
-        with timings.measure("print_ast"):
-            clickhouse_context = HogQLContext(
+    # Get printed HogQL query, and returned columns. Using a cloned query.
+    with timings.measure("hogql"):
+        with timings.measure("prepare_ast"):
+            hogql_query_context = HogQLContext(
                 team_id=team.pk,
                 enable_select_queries=True,
                 person_on_events_mode=team.person_on_events_mode,
                 timings=timings,
             )
-            clickhouse_sql = print_ast(
-                select_query, context=clickhouse_context, dialect="clickhouse", settings=settings or HogQLSettings()
+            with timings.measure("clone"):
+                cloned_query = clone_expr(select_query, True)
+            select_query_hogql = cast(
+                ast.SelectQuery,
+                prepare_ast_for_printing(node=cloned_query, context=hogql_query_context, dialect="hogql"),
             )
 
-        timings_dict = timings.to_dict()
-        with timings.measure("clickhouse_execute"):
-            tag_queries(
-                team_id=team.pk,
-                query_type=query_type,
-                has_joins="JOIN" in clickhouse_sql,
-                has_json_operations="JSONExtract" in clickhouse_sql or "JSONHas" in clickhouse_sql,
-                timings=timings_dict,
-            )
+        with timings.measure("print_ast"):
+            hogql = print_prepared_ast(select_query_hogql, hogql_query_context, "hogql")
+            print_columns = []
+            for node in select_query_hogql.select:
+                if isinstance(node, ast.Alias):
+                    print_columns.append(node.alias)
+                else:
+                    print_columns.append(
+                        print_prepared_ast(
+                            node=node, context=hogql_query_context, dialect="hogql", stack=[select_query_hogql]
+                        )
+                    )
 
-            results, types = sync_execute(
-                clickhouse_sql,
-                clickhouse_context.values,
-                with_column_types=True,
-                workload=workload,
-                team_id=team.pk,
-                readonly=True,
-            )
-
-        return HogQLQueryResponse(
-            query=query,
-            hogql=hogql,
-            clickhouse=clickhouse_sql,
-            timings=timings.to_list(),
-            results=results,
-            columns=print_columns,
-            types=types,
+    # Print the ClickHouse SQL query
+    with timings.measure("print_ast"):
+        clickhouse_context = HogQLContext(
+            team_id=team.pk,
+            enable_select_queries=True,
+            person_on_events_mode=team.person_on_events_mode,
+            timings=timings,
         )
+        clickhouse_sql = print_ast(
+            select_query, context=clickhouse_context, dialect="clickhouse", settings=settings or HogQLSettings()
+        )
+
+    timings_dict = timings.to_dict()
+    with timings.measure("clickhouse_execute"):
+        tag_queries(
+            team_id=team.pk,
+            query_type=query_type,
+            has_joins="JOIN" in clickhouse_sql,
+            has_json_operations="JSONExtract" in clickhouse_sql or "JSONHas" in clickhouse_sql,
+            timings=timings_dict,
+        )
+
+        results, types = sync_execute(
+            clickhouse_sql,
+            clickhouse_context.values,
+            with_column_types=True,
+            workload=workload,
+            team_id=team.pk,
+            readonly=True,
+        )
+
+    return HogQLQueryResponse(
+        query=query,
+        hogql=hogql,
+        clickhouse=clickhouse_sql,
+        timings=timings.to_list(),
+        results=results,
+        columns=print_columns,
+        types=types,
+    )
