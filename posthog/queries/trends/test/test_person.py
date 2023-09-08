563f5e65b7a92b8d7532bcb65ba898128b951d48
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-09-08 18:41:25 +0200

    add tests

--- posthog/queries/trends/test/test_person.py
+++ posthog/queries/trends/test/test_person.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime
 from uuid import UUID
 
 from dateutil.relativedelta import relativedelta
@@ -15,12 +17,12 @@ from posthog.test.base import (
     ClickhouseTestMixin,
     _create_event,
     _create_person,
+    flush_persons_and_events,
     snapshot_clickhouse_queries,
 )
 
 
 class TestPerson(ClickhouseTestMixin, APIBaseTest):
-
     # Note: not using `@snapshot_clickhouse_queries` here because the ordering of the session_ids in the recording
     # query is not guaranteed, so adding it would lead to a flaky test.
     @freeze_time("2021-01-21T20:00:00.000Z")
@@ -155,3 +157,103 @@ class TestPerson(ClickhouseTestMixin, APIBaseTest):
                 }
             ],
         )
+
+
+class TestPersonIntegration(ClickhouseTestMixin, APIBaseTest):
+    def test_weekly_active_users(self):
+        for d in range(10, 18):  # create a person and event for each day 10. Sep - 17. Sep
+            _create_person(team_id=self.team.pk, distinct_ids=[f"u_{d}"])
+            _create_event(
+                event="pageview",
+                distinct_id=f"u_{d}",
+                team=self.team,
+                timestamp=datetime(2023, 9, d, 00, 42),
+            )
+        flush_persons_and_events()
+
+        # request weekly active users in the following week
+        filter = {
+            "insight": "TRENDS",
+            "date_from": "2023-09-17T13:37:00",
+            "date_to": "2023-09-24T13:37:00",
+            "events": json.dumps([{"id": "pageview", "math": "weekly_active"}]),
+        }
+        insight_response = self.client.get(f"/api/projects/{self.team.pk}/insights/trend", data=filter)
+        insight_response = (insight_response.json()).get("result")
+
+        self.assertEqual(insight_response[0].get("labels")[5], "22-Sep-2023")
+        self.assertEqual(insight_response[0].get("data")[5], 2)
+
+        persons_url = insight_response[0].get("persons_urls")[5].get("url")
+        response = self.client.get("/" + persons_url)
+
+        data = response.json()
+        self.assertEqual(data.get("results")[0].get("count"), 2)
+        self.assertEqual([item["name"] for item in data.get("results")[0].get("people")], ["u_17", "u_16"])
+
+    def test_weekly_active_users_grouped_by_week(self):
+        for d in range(10, 18):  # create a person and event for each day 10. Sep - 17. Sep
+            _create_person(team_id=self.team.pk, distinct_ids=[f"u_{d}"])
+            _create_event(
+                event="pageview",
+                distinct_id=f"u_{d}",
+                team=self.team,
+                timestamp=datetime(2023, 9, d, 00, 42),
+            )
+        flush_persons_and_events()
+
+        # request weekly active users in the following week
+        filter = {
+            "insight": "TRENDS",
+            "date_from": "2023-09-17T13:37:00",
+            "date_to": "2023-09-24T13:37:00",
+            "interval": "week",
+            "events": json.dumps([{"id": "pageview", "math": "weekly_active"}]),
+        }
+        insight_response = self.client.get(f"/api/projects/{self.team.pk}/insights/trend", data=filter)
+        insight_response = (insight_response.json()).get("result")
+
+        self.assertEqual(insight_response[0].get("labels")[0], "17-Sep-2023")
+        self.assertEqual(insight_response[0].get("data")[0], 7)
+
+        persons_url = insight_response[0].get("persons_urls")[0].get("url")
+        response = self.client.get("/" + persons_url)
+
+        data = response.json()
+        self.assertEqual(data.get("results")[0].get("count"), 7)
+        self.assertEqual(
+            [item["name"] for item in data.get("results")[0].get("people")],
+            ["u_17", "u_16", "u_15", "u_14", "u_13", "u_12", "u_11"],
+        )
+
+    def test_weekly_active_users_cumulative(self):
+        for d in range(10, 18):  # create a person and event for each day 10. Sep - 17. Sep
+            _create_person(team_id=self.team.pk, distinct_ids=[f"u_{d}"])
+            _create_event(
+                event="pageview",
+                distinct_id=f"u_{d}",
+                team=self.team,
+                timestamp=datetime(2023, 9, d, 00, 42),
+            )
+        flush_persons_and_events()
+
+        # request weekly active users in the following week
+        filter = {
+            "insight": "TRENDS",
+            "date_from": "2023-09-10T13:37:00",
+            "date_to": "2023-09-24T13:37:00",
+            "events": json.dumps([{"id": "pageview", "math": "weekly_active"}]),
+            "display": "ActionsLineGraphCumulative",
+        }
+        insight_response = self.client.get(f"/api/projects/{self.team.pk}/insights/trend", data=filter)
+        insight_response = (insight_response.json()).get("result")
+
+        self.assertEqual(insight_response[0].get("labels")[1], "11-Sep-2023")
+        self.assertEqual(insight_response[0].get("data")[1], 3)
+
+        persons_url = insight_response[0].get("persons_urls")[1].get("url")
+        response = self.client.get("/" + persons_url)
+
+        data = response.json()
+        self.assertEqual(data.get("results")[0].get("count"), 2)
+        self.assertEqual([item["name"] for item in data.get("results")[0].get("people")], ["u_11", "u_10"])
