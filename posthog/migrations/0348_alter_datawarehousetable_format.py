de754d04eafa237783bcc76fef592df414d37d6a
eric <eeoneric@gmail.com>
2023-09-11 12:11:57 -0400

    edit dependency

--- posthog/migrations/0348_alter_datawarehousetable_format.py
+++ posthog/migrations/0348_alter_datawarehousetable_format.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("posthog", "0348_alter_datawarehousetable_format"),
+        ("posthog", "0347_add_bigquery_export_type"),
     ]
 
     operations = [
