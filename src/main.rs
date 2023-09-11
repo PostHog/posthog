a9cc8a2d6724e1be13f0d8f65a172b444d48cf40
Ellie Huxtable <ellie@posthog.com>
2023-09-11 12:48:57 +0100

    Allow setting bind address (#21)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::net::SocketAddr;
 
 use crate::time::SystemTime;
 
@@ -14,6 +13,7 @@ mod token;
 #[tokio::main]
 async fn main() {
     let use_print_sink = env::var("PRINT_SINK").is_ok();
+    let address = env::var("ADDRESS").unwrap_or(String::from("127.0.0.1:3000"));
 
     let app = if use_print_sink {
         router::router(SystemTime {}, sink::PrintSink {})
@@ -31,11 +31,10 @@ async fn main() {
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    tracing::debug!("listening on {}", addr);
+    tracing::info!("listening on {}", address);
 
-    axum::Server::bind(&addr)
+    axum::Server::bind(&address.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
