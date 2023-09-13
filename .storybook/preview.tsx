da0c95f48439e4a68d9622cea0c452cbc668200d
Paul D'Ambra <paul@posthog.com>
2023-09-13 23:22:35 +0100

    chore: clear the console in storybook a little

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -15,6 +15,18 @@ const setupMsw = () => {
     // Make sure the msw worker is started
     worker.start({
         quiet: true,
+        onUnhandledRequest(request, print) {
+            // MSW warns on all unhandled requests, but we don't necessarily care
+            const pathAllowList = ['/images/']
+
+            // return early if any request.url.pathname starts with a path in pathAllowList
+            if (pathAllowList.some((path) => request.url.pathname.startsWith(path))) {
+                return
+            }
+
+            // Otherwise, default MSW warning behavior
+            print.warning()
+        },
     })
     ;(window as any).__mockServiceWorker = worker
     ;(window as any).POSTHOG_APP_CONTEXT = getStorybookAppContext()
