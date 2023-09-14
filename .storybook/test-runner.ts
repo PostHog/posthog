3eee25cf18d8e6b0a5339f6538ef0f601a34b311
Paul D'Ambra <paul@posthog.com>
2023-09-14 19:28:53 +0100

    chore: make visual regression less sensitive to differences

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -208,7 +208,9 @@ async function expectLocatorToMatchStorySnapshot(
         // Compare structural similarity instead of raw pixels - reducing false positives
         // See https://github.com/americanexpress/jest-image-snapshot#recommendations-when-using-ssim-comparison
         comparisonMethod: 'ssim',
-        failureThreshold: 0.0003,
+        // 0.01 would be a 1% difference
+        // 0.005 is a 0.5% difference
+        failureThreshold: 0.005,
         failureThresholdType: 'percent',
     })
 }
