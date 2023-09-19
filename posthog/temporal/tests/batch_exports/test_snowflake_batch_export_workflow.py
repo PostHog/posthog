d1f709f67182093380f8c761d1f3282634e7f5fc
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-09-19 21:19:36 +0200

    feat: Handle Snowflake export errors

--- posthog/temporal/tests/batch_exports/test_snowflake_batch_export_workflow.py
+++ posthog/temporal/tests/batch_exports/test_snowflake_batch_export_workflow.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime as dt
 import gzip
 import json
@@ -6,10 +7,13 @@ from collections import deque
 from uuid import uuid4
 
 import pytest
+import pytest_asyncio
 import responses
+from asgiref.sync import sync_to_async
 from django.conf import settings
 from django.test import override_settings
 from requests.models import PreparedRequest
+from temporalio import activity
 from temporalio.client import WorkflowFailureError
 from temporalio.common import RetryPolicy
 from temporalio.exceptions import ActivityError, ApplicationError
@@ -18,6 +22,7 @@ from temporalio.worker import UnsandboxedWorkflowRunner, Worker
 
 from posthog.api.test.test_organization import acreate_organization
 from posthog.api.test.test_team import acreate_team
+from posthog.temporal.client import connect
 from posthog.temporal.tests.batch_exports.base import (
     EventValues,
     insert_events,
@@ -25,6 +30,7 @@ from posthog.temporal.tests.batch_exports.base import (
 )
 from posthog.temporal.tests.batch_exports.fixtures import (
     acreate_batch_export,
+    adelete_batch_export,
     afetch_batch_export_runs,
 )
 from posthog.temporal.workflows.base import create_export_run, update_export_run_status
@@ -32,6 +38,7 @@ from posthog.temporal.workflows.clickhouse import ClickHouseClient
 from posthog.temporal.workflows.snowflake_batch_export import (
     SnowflakeBatchExportInputs,
     SnowflakeBatchExportWorkflow,
+    SnowflakeInsertInputs,
     insert_into_snowflake_activity,
 )
 
@@ -645,3 +652,145 @@ async def test_snowflake_export_workflow_raises_error_on_copy_fail():
                 assert isinstance(err.__cause__, ActivityError)
                 assert isinstance(err.__cause__.__cause__, ApplicationError)
                 assert err.__cause__.__cause__.type == "SnowflakeFileNotLoadedError"
+
+
+@pytest_asyncio.fixture
+async def organization():
+    organization = await acreate_organization("test")
+    yield organization
+    await sync_to_async(organization.delete)()  # type: ignore
+
+
+@pytest_asyncio.fixture
+async def team(organization):
+    team = await acreate_team(organization=organization)
+    yield team
+    await sync_to_async(team.delete)()  # type: ignore
+
+
+@pytest_asyncio.fixture
+async def batch_export(team):
+    destination_data = {
+        "type": "Snowflake",
+        "config": {
+            "user": "hazzadous",
+            "password": "password",
+            "account": "account",
+            "database": "PostHog",
+            "schema": "test",
+            "warehouse": "COMPUTE_WH",
+            "table_name": "events",
+        },
+    }
+    batch_export_data = {
+        "name": "my-production-snowflake-export",
+        "destination": destination_data,
+        "interval": "hour",
+    }
+
+    batch_export = await acreate_batch_export(
+        team_id=team.pk,
+        name=batch_export_data["name"],
+        destination_data=batch_export_data["destination"],
+        interval=batch_export_data["interval"],
+    )
+
+    yield batch_export
+
+    client = await connect(
+        settings.TEMPORAL_HOST,
+        settings.TEMPORAL_PORT,
+        settings.TEMPORAL_NAMESPACE,
+        settings.TEMPORAL_CLIENT_ROOT_CA,
+        settings.TEMPORAL_CLIENT_CERT,
+        settings.TEMPORAL_CLIENT_KEY,
+    )
+    await adelete_batch_export(batch_export, client)
+
+
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_snowflake_export_workflow_handles_insert_activity_errors(team, batch_export):
+    """Test that Snowflake Export Workflow can gracefully handle errors when inserting Snowflake data."""
+    workflow_id = str(uuid4())
+    inputs = SnowflakeBatchExportInputs(
+        team_id=team.pk,
+        batch_export_id=str(batch_export.id),
+        data_interval_end="2023-04-25 14:30:00.000000",
+        **batch_export.destination.config,
+    )
+
+    @activity.defn(name="insert_into_snowflake_activity")
+    async def insert_into_snowflake_activity_mocked(_: SnowflakeInsertInputs) -> str:
+        raise ValueError("A useful error message")
+
+    async with await WorkflowEnvironment.start_time_skipping() as activity_environment:
+        async with Worker(
+            activity_environment.client,
+            task_queue=settings.TEMPORAL_TASK_QUEUE,
+            workflows=[SnowflakeBatchExportWorkflow],
+            activities=[create_export_run, insert_into_snowflake_activity_mocked, update_export_run_status],
+            workflow_runner=UnsandboxedWorkflowRunner(),
+        ):
+            with pytest.raises(WorkflowFailureError):
+                await activity_environment.client.execute_workflow(
+                    SnowflakeBatchExportWorkflow.run,
+                    inputs,
+                    id=workflow_id,
+                    task_queue=settings.TEMPORAL_TASK_QUEUE,
+                    retry_policy=RetryPolicy(maximum_attempts=1),
+                )
+
+        runs = await afetch_batch_export_runs(batch_export_id=batch_export.id)
+        assert len(runs) == 1
+
+        run = runs[0]
+        assert run.status == "Failed"
+        assert run.latest_error == "ValueError: A useful error message"
+
+
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_snowflake_export_workflow_handles_cancellation(team, batch_export):
+    """Test that Snowflake Export Workflow can gracefully handle cancellations when inserting Snowflake data."""
+    workflow_id = str(uuid4())
+    inputs = SnowflakeBatchExportInputs(
+        team_id=team.pk,
+        batch_export_id=str(batch_export.id),
+        data_interval_end="2023-04-25 14:30:00.000000",
+        **batch_export.destination.config,
+    )
+
+    @activity.defn(name="insert_into_snowflake_activity")
+    async def never_finish_activity(_: SnowflakeInsertInputs) -> str:
+        while True:
+            activity.heartbeat()
+            await asyncio.sleep(1)
+
+    async with await WorkflowEnvironment.start_time_skipping() as activity_environment:
+        async with Worker(
+            activity_environment.client,
+            task_queue=settings.TEMPORAL_TASK_QUEUE,
+            workflows=[SnowflakeBatchExportWorkflow],
+            activities=[create_export_run, never_finish_activity, update_export_run_status],
+            workflow_runner=UnsandboxedWorkflowRunner(),
+        ):
+            handle = await activity_environment.client.start_workflow(
+                SnowflakeBatchExportWorkflow.run,
+                inputs,
+                id=workflow_id,
+                task_queue=settings.TEMPORAL_TASK_QUEUE,
+                retry_policy=RetryPolicy(maximum_attempts=1),
+            )
+            await asyncio.sleep(5)
+            await handle.cancel()
+
+            with pytest.raises(WorkflowFailureError):
+                await handle.result()
+
+        runs = await afetch_batch_export_runs(batch_export_id=batch_export.id)
+        assert len(runs) == 1
+
+        run = runs[0]
+        assert run.status == "Cancelled"
+        assert run.latest_error == "Cancelled"
