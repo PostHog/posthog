d08d2895d8b460fb7ce62e7e79c12f2f80604b1f
David Newell <david@posthog.com>
2023-09-21 12:00:06 +0100

    reimplement sorting

--- frontend/src/scenes/plugins/tabs/apps/InstalledAppsReorderModal.tsx
+++ frontend/src/scenes/plugins/tabs/apps/InstalledAppsReorderModal.tsx
@@ -4,9 +4,9 @@ import { pluginsLogic } from 'scenes/plugins/pluginsLogic'
 import { LemonBadge, LemonButton } from '@posthog/lemon-ui'
 import { PluginTypeWithConfig } from 'scenes/plugins/types'
 import { PluginImage } from 'scenes/plugins/plugin/PluginImage'
-import { SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable'
-import { DndContext } from '@dnd-kit/core'
-import { restrictToVerticalAxis } from '@dnd-kit/modifiers'
+import { SortableContext, arrayMove, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import { DndContext, DragEndEvent } from '@dnd-kit/core'
+import { restrictToParentElement, restrictToVerticalAxis } from '@dnd-kit/modifiers'
 import { CSS } from '@dnd-kit/utilities'
 
 const MinimalAppView = ({ plugin, order }: { plugin: PluginTypeWithConfig; order: number }): JSX.Element => {
@@ -37,27 +37,28 @@ export function InstalledAppsReorderModal(): JSX.Element {
     const { reorderModalOpen, sortableEnabledPlugins, temporaryOrder, pluginConfigsLoading } = useValues(pluginsLogic)
     const { closeReorderModal, setTemporaryOrder, cancelRearranging, savePluginOrders } = useActions(pluginsLogic)
 
-    const onSortEnd = ({ oldIndex, newIndex }: { oldIndex: number; newIndex: number }): void => {
-        const cloned = [...sortableEnabledPlugins]
-        const [removed] = cloned.splice(oldIndex, 1)
-        cloned.splice(newIndex, 0, removed)
+    const onDragEnd = ({ active, over }: DragEndEvent): void => {
+        const sortableEnabledPluginsIds = sortableEnabledPlugins.map((p) => p.id)
 
-        const newTemporaryOrder = cloned.reduce((acc, plugin, index) => {
-            return {
-                ...acc,
-                [plugin.id]: index + 1,
-            }
-        }, {})
+        if (over && active.id !== over.id) {
+            const oldIndex = sortableEnabledPluginsIds.indexOf(Number(active.id))
+            const newIndex = sortableEnabledPluginsIds.indexOf(Number(over.id))
 
-        setTemporaryOrder(newTemporaryOrder, removed.id)
+            const newTemporaryOrder = arrayMove(sortableEnabledPlugins, oldIndex, newIndex).reduce(
+                (acc, plugin, index) => {
+                    return { ...acc, [plugin.id]: index + 1 }
+                },
+                {}
+            )
+
+            setTemporaryOrder(newTemporaryOrder, Number(active.id))
+        }
     }
 
     const onClose = (): void => {
         cancelRearranging()
         closeReorderModal()
     }
-
-    const sortableEnabledPluginsIds = sortableEnabledPlugins.map((p) => p.id)
 
     return (
         <LemonModal
@@ -87,29 +88,16 @@ export function InstalledAppsReorderModal(): JSX.Element {
             }
         >
             <div className="flex flex-col gap-2">
-                <DndContext
-                    onDragEnd={({ active, over }) => {
-                        if (over && active.id !== over.id) {
-                            onSortEnd({
-                                oldIndex: sortableEnabledPluginsIds.indexOf(Number(active.id)),
-                                newIndex: sortableEnabledPluginsIds.indexOf(Number(over.id)),
-                            })
-                        }
-                    }}
-                    modifiers={[restrictToVerticalAxis]}
-                >
-                    <SortableContext items={sortableEnabledPluginsIds} strategy={verticalListSortingStrategy}>
+                <DndContext onDragEnd={onDragEnd} modifiers={[restrictToVerticalAxis, restrictToParentElement]}>
+                    <SortableContext
+                        items={sortableEnabledPlugins.map((p) => p.id)}
+                        strategy={verticalListSortingStrategy}
+                    >
                         {sortableEnabledPlugins.map((plugin, index) => (
                             <MinimalAppView key={`item-${index}`} order={index} plugin={plugin} />
                         ))}
                     </SortableContext>
                 </DndContext>
-
-                {/* <SortableAppList onSortEnd={onSortEnd} axis="y" lockAxis="y" lockToContainerEdges distance={5}>
-                    {sortableEnabledPlugins.map((plugin, index) => (
-                        <SortableAppView key={`item-${index}`} index={index} order={index} plugin={plugin} />
-                    ))}
-                </SortableAppList> */}
             </div>
         </LemonModal>
     )
