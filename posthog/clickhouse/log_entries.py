c0431a02def22b3c036090d22973358904e93457
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-09-22 14:13:16 +0200

    chore: Add comments on the fields of log_entries

--- posthog/clickhouse/log_entries.py
+++ posthog/clickhouse/log_entries.py
@@ -10,11 +10,23 @@ LOG_ENTRIES_TABLE_BASE_SQL = """
 CREATE TABLE IF NOT EXISTS {table_name} ON CLUSTER '{cluster}'
 (
     team_id UInt64,
+    -- The name of the service or product that generated the logs.
+    -- Examples: batch_exports
     log_source LowCardinality(String),
+    -- An id for the log source.
+    -- Set log_source to avoid collision with ids from other log sources if the id generation is not safe.
+    -- Examples: A batch export id, a cronjob id, a plugin id.
     log_source_id String,
+    -- An id for the instance of log_source that generated this log.
+    -- This may be ommitted if log_source is a singleton.
+    -- Examples: A batch export run id, a plugin_config id, a thread id, a process id, a machine id.
     instance_id String,
+    -- Timestamp indicating when the log was generated.
     timestamp DateTime64(6, 'UTC'),
+    -- The log level.
+    -- Examples: INFO, WARNING, DEBUG, ERROR.
     level LowCardinality(String),
+    -- The actual log message.
     message String
     {extra_fields}
 ) ENGINE = {engine}
