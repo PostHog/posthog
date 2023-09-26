26e76bc2bbb1a0c638a64527a0a51fc44e5e55db
Paul D'Ambra <paul@posthog.com>
2023-09-26 12:50:39 +0100

    keep the sql in the migration file

--- posthog/clickhouse/migrations/0049_drop_session_recording_tables.py
+++ posthog/clickhouse/migrations/0049_drop_session_recording_tables.py
@@ -1,19 +1,24 @@
 import structlog
-
+from django.conf import settings
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.cloud_utils import is_cloud
-from posthog.session_recordings.sql.session_recording_event_sql import (
-    DEPRECATE_CH_RECORDINGS_DROP_SESSION_RECORDING_EVENTS_MV_TABLE_SQL,
-    DEPRECATE_CH_RECORDINGS_DROP_SESSION_RECORDING_EVENTS_PARTITION_STATISTICS_MV_TABLE_SQL,
-    DEPRECATE_CH_RECORDINGS_DROP_KAFKA_SESSION_RECORDING_EVENTS_TABLE_SQL,
-    DEPRECATE_CH_RECORDINGS_DROP_KAFKA_SESSION_RECORDING_EVENTS_PARTITION_STATISTICS_TABLE_SQL,
-    DEPRECATE_CH_RECORDINGS_DROP_WRITABLE_SESSION_RECORDING_EVENTS_TABLE_SQL,
-    DEPRECATE_CH_RECORDINGS_DROP_SHARDED_SESSION_RECORDING_EVENTS_TABLE_SQL,
-    DEPRECATE_CH_RECORDINGS_DROP_SESSION_RECORDING_EVENTS_TABLE_SQL,
-)
 from posthog.settings import DEBUG, TEST
 
 logger = structlog.get_logger(__name__)
+
+drop_sql_commands = [
+    lambda: (f"DROP TABLE IF EXISTS session_recording_events_mv ON CLUSTER '{settings.CLICKHOUSE_CLUSTER}'"),
+    lambda: (
+        f"DROP TABLE IF EXISTS session_recording_events_partition_statistics_mv ON CLUSTER '{settings.CLICKHOUSE_CLUSTER}'"
+    ),
+    lambda: (f"DROP TABLE IF EXISTS kafka_session_recording_events ON CLUSTER '{settings.CLICKHOUSE_CLUSTER}'"),
+    lambda: (
+        f"DROP TABLE IF EXISTS kafka_session_recording_events_partition_statistics ON CLUSTER '{settings.CLICKHOUSE_CLUSTER}'"
+    ),
+    lambda: (f"DROP TABLE IF EXISTS writable_session_recording_events ON CLUSTER '{settings.CLICKHOUSE_CLUSTER}'"),
+    lambda: (f"DROP TABLE IF EXISTS sharded_session_recording_events ON CLUSTER '{settings.CLICKHOUSE_CLUSTER}'"),
+    lambda: (f"DROP TABLE IF EXISTS session_recording_events ON CLUSTER '{settings.CLICKHOUSE_CLUSTER}'"),
+]
 
 
 def no_op_migration(sql: str):
@@ -27,12 +32,4 @@ def run_sql_on_cloud(sql: str):
         return no_op_migration(sql)
 
 
-operations = [
-    run_sql_on_cloud(DEPRECATE_CH_RECORDINGS_DROP_SESSION_RECORDING_EVENTS_MV_TABLE_SQL()),
-    run_sql_on_cloud(DEPRECATE_CH_RECORDINGS_DROP_SESSION_RECORDING_EVENTS_PARTITION_STATISTICS_MV_TABLE_SQL()),
-    run_sql_on_cloud(DEPRECATE_CH_RECORDINGS_DROP_KAFKA_SESSION_RECORDING_EVENTS_TABLE_SQL()),
-    run_sql_on_cloud(DEPRECATE_CH_RECORDINGS_DROP_KAFKA_SESSION_RECORDING_EVENTS_PARTITION_STATISTICS_TABLE_SQL()),
-    run_sql_on_cloud(DEPRECATE_CH_RECORDINGS_DROP_WRITABLE_SESSION_RECORDING_EVENTS_TABLE_SQL()),
-    run_sql_on_cloud(DEPRECATE_CH_RECORDINGS_DROP_SHARDED_SESSION_RECORDING_EVENTS_TABLE_SQL()),
-    run_sql_on_cloud(DEPRECATE_CH_RECORDINGS_DROP_SESSION_RECORDING_EVENTS_TABLE_SQL()),
-]
+operations = [run_sql_on_cloud(sql()) for sql in drop_sql_commands]
