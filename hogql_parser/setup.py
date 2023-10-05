4af40ccacbb5e3c5d1eec97ab8790bf729b5d7c8
Michael Matloka <michal@matloka.com>
2023-10-05 16:27:16 +0200

    Build wheels for the parser

--- hogql_parser/setup.py
+++ hogql_parser/setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup, Extension
 import platform
 
-arch = "aarch64" if platform.machine() == "arm64" else "x86_64"
+system = platform.system()
+if system not in ("Darwin", "Linux"):
+    raise Exception("Only Linux and macOS are supported by hogql_parser")
+
+is_macos = system == "Darwin"
+
 module = Extension(
     "hogql_parser",
     sources=[
@@ -14,14 +19,18 @@ module = Extension(
         "parser.cpp",
     ],
     include_dirs=[
+        "/opt/homebrew/include/" if is_macos else "/usr/include/",
         "/opt/homebrew/include/antlr4-runtime/"
-        if platform.system() == "Darwin"
+        if is_macos
         else "/usr/local/include/antlr4-runtime/",
-        "/opt/homebrew/include/" if platform.system() == "Darwin" else "/usr/include/",
     ],
     library_dirs=[
-        "/opt/homebrew/Cellar/antlr4-cpp-runtime/4.13.1/lib/" if platform.system() == "Darwin" else "/usr/local/lib/",
-        "/opt/homebrew/Cellar/boost/1.82.0_1/lib/" if platform.system() == "Darwin" else f"/usr/lib/{arch}-linux-gnu/",
+        "/opt/homebrew/Cellar/boost/1.82.0_1/lib/"
+        if is_macos == "Darwin"
+        else "/usr/lib64/",
+        "/opt/homebrew/Cellar/antlr4-cpp-runtime/4.13.1/lib/"
+        if is_macos == "Darwin"
+        else "/usr/local/lib/",
     ],
     libraries=["antlr4-runtime"],
     extra_compile_args=["-std=c++20"],
@@ -30,6 +39,20 @@ module = Extension(
 setup(
     name="hogql_parser",
     version="0.1",
-    description="HogQL parser",
+    url="https://github.com/PostHog/posthog/tree/master/hogql_parser",
+    author="PostHog Inc.",
+    author_email="hey@posthog.com",
+    maintainer="PostHog Inc.",
+    maintainer_email="hey@posthog.com",
+    description="HogQL parser for internal PostHog use",
     ext_modules=[module],
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
