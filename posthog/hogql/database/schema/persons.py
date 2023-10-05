dca9717a4dca3a97412e3a45ee7bf0e67cf332e2
Marius Andra <marius.andra@gmail.com>
2023-10-05 16:49:17 +0200

    feat(database): faster queries from the persons table

--- posthog/hogql/database/schema/persons.py
+++ posthog/hogql/database/schema/persons.py
@@ -1,6 +1,5 @@
 from typing import Dict, List
 
-from posthog.hogql.database.argmax import argmax_select
 from posthog.hogql.database.models import (
     Table,
     StringDatabaseField,
@@ -30,13 +29,30 @@ PERSONS_FIELDS: Dict[str, FieldOrTable] = {
 
 
 def select_from_persons_table(requested_fields: Dict[str, List[str]]):
-    return argmax_select(
-        table_name="raw_persons",
-        select_fields=requested_fields,
-        group_fields=["id"],
-        argmax_field="version",
-        deleted_field="is_deleted",
+    from posthog.hogql.parser import parse_select
+    from posthog.hogql import ast
+
+    query = parse_select(
+        """
+        SELECT id FROM raw_persons WHERE (id, version) IN (
+           SELECT id, max(version) as version
+           FROM raw_persons
+           GROUP BY id
+           HAVING ifNull(equals(argMax(raw_persons.is_deleted, raw_persons.version), 0), 0)
+           limit 10
+        )
+    """
     )
+
+    query.select = []
+    for field_name, field_chain in requested_fields.items():
+        query.select.append(
+            ast.Alias(
+                alias=field_name,
+                expr=ast.Field(chain=["raw_persons"] + field_chain),
+            )
+        )
+    return query
 
 
 def join_with_persons_table(from_table: str, to_table: str, requested_fields: Dict[str, List[str]]):
