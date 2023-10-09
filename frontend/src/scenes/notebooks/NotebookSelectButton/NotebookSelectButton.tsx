30761893dc2b753cb8f56926baf05957cf16f6f7
David Newell <david@posthog.com>
2023-10-09 11:45:15 +0100

    cleanup button styling

--- frontend/src/scenes/notebooks/NotebookSelectButton/NotebookSelectButton.tsx
+++ frontend/src/scenes/notebooks/NotebookSelectButton/NotebookSelectButton.tsx
@@ -103,8 +103,8 @@ export function NotebookSelectList(props: NotebookSelectProps): JSX.Element {
     }, [])
 
     return (
-        <div className="space-y-2 flex flex-col flex-1 h-full overflow-hidden">
-            <div className="border-b space-y-2 flex-0 pb-2">
+        <div className="flex flex-col flex-1 h-full overflow-hidden">
+            <div className="space-y-2 flex-0">
                 <LemonInput
                     type="search"
                     placeholder="Search notebooks..."
@@ -116,6 +116,7 @@ export function NotebookSelectList(props: NotebookSelectProps): JSX.Element {
                     New notebook
                 </LemonButton>
             </div>
+            <LemonDivider />
             <div className="overflow-y-auto flex-1">
                 {notebooksLoading && !notebooksNotContainingResource.length && !notebooksContainingResource.length ? (
                     <div className={'px-2 py-1 flex flex-row items-center space-x-1'}>
