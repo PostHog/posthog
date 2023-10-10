7bf7d676d5ed58e30ae9557bbc99688be769602b
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-10-10 13:26:05 +0200

    return ranked dashboards

--- posthog/api/search.py
+++ posthog/api/search.py
@@ -1,3 +1,5 @@
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.db import models
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -6,10 +8,17 @@ from rest_framework.response import Response
 
 from posthog.api.routing import StructuredViewSetMixin
 from posthog.permissions import ProjectMembershipNecessaryPermissions, TeamMemberAccessPermission
+from posthog.models import Dashboard
 
 
 class SearchViewSet(StructuredViewSetMixin, viewsets.ViewSet):
     permission_classes = [IsAuthenticated, ProjectMembershipNecessaryPermissions, TeamMemberAccessPermission]
 
     def list(self, request: Request, **kw) -> HttpResponse:
-        return Response({"a": 1})
+        vector = SearchVector("name", "description")
+        query = SearchQuery("metric")
+        q = Dashboard.objects.annotate(
+            rank=SearchRank(vector, query), type=models.Value("dashboard", output_field=models.CharField())
+        ).order_by("-rank")
+
+        return Response({"ranked": q.values("type", "pk", "rank")})
