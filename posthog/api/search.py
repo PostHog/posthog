e24f1144bbd1dcee8a6d37deb36ae73147c00ee5
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-10-11 15:43:23 +0200

    refactor search endpoint

--- posthog/api/search.py
+++ posthog/api/search.py
@@ -1,5 +1,6 @@
+import re
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
-from django.db.models import Value, CharField
+from django.db.models import Model, Value, CharField
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -8,41 +9,58 @@ from rest_framework.response import Response
 
 from posthog.api.routing import StructuredViewSetMixin
 from posthog.permissions import ProjectMembershipNecessaryPermissions, TeamMemberAccessPermission
-from posthog.models import Dashboard, FeatureFlag, Experiment
+from posthog.models import Dashboard, FeatureFlag, Experiment, Team
 
 
 class SearchViewSet(StructuredViewSetMixin, viewsets.ViewSet):
     permission_classes = [IsAuthenticated, ProjectMembershipNecessaryPermissions, TeamMemberAccessPermission]
 
     def list(self, request: Request, **kw) -> HttpResponse:
-        q = request.GET.get("q", "").strip()
-        if len(q) == 0:
-            return Response(
-                {
-                    "results": [],
-                    "counts": {"dashboard": None, "feature_flag": None, "experiment": None},
-                }
-            )
+        query = request.GET.get("q", "").strip()
 
-        vector = SearchVector("name", "description")
-        query = SearchQuery(":* & ".join(q.split()).strip() + ":*", search_type="raw")
-        q1 = Dashboard.objects.annotate(
-            rank=SearchRank(vector, query, cover_density=True, normalization=Value(1)),
-            type=Value("dashboard", output_field=CharField()),
-        ).filter(rank__gt=0.0, team=self.team)
-        q2 = FeatureFlag.objects.annotate(
-            rank=SearchRank(SearchVector("name"), query, cover_density=True, normalization=Value(1)),
-            type=Value("feature_flag", output_field=CharField()),
-        ).filter(rank__gt=0.0, team=self.team)
-        q3 = q1 = Experiment.objects.annotate(
-            rank=SearchRank(vector, query, cover_density=True, normalization=Value(1)),
-            type=Value("experiment", output_field=CharField()),
-        ).filter(rank__gt=0.0, team=self.team)
+        # empty queryset to union things onto it
+        qs = Dashboard.objects.annotate(type=Value("empty", output_field=CharField())).filter(team=self.team).none()
 
-        q = q1.union(q2, q3).order_by("-rank")
-        # having rank > 0
-        d = q.values("type", "pk", "rank", "name")
+        for klass in (Dashboard, Experiment, FeatureFlag):
+            klass_qs = class_queryset(klass, team=self.team, query=query)
+            qs = qs.union(klass_qs)
 
-        counts = {"dashboard": q1.count(), "feature_flag": q2.count(), "experiment": q3.count()}
+        return Response({"results": qs})
 
-        return Response({"results": d, "counts": counts, "sql": str(q.query)})
+
+UNSAFE_CHARACTERS = r"[\'&|!<>():]"
+"""Characters unsafe in a `tsquery`."""
+
+
+def process_query(query: str):
+    """
+    Converts a query string into a to_tsquery compatible string, where
+    the last word is a prefix match. This allows searching as you type.
+    """
+    query = re.sub(UNSAFE_CHARACTERS, " ", query).strip()
+    query = re.sub(r"\s+", " & ", query)  # combine words with &
+    query += ":*"  # prefix match last word
+    return query
+
+
+def class_queryset(klass: type[Model], team: Team, query: str):
+    """Builds a queryset for the class."""
+    type = class_to_type(klass)
+    values = ["type", "pk", "name"]
+
+    qs = klass.objects.filter(team=team)
+    qs = qs.annotate(type=Value(type, output_field=CharField()))
+
+    if query:
+        qs = qs.annotate(rank=SearchRank(SearchVector("name"), SearchQuery(process_query(query), search_type="raw")))
+        qs = qs.filter(rank__gt=0.05)
+        qs = qs.order_by("-rank")
+        values.append("rank")
+
+    qs = qs.values(*values)
+    return qs
+
+
+def class_to_type(klass: type[Model]):
+    """Converts the class name to snake case."""
+    return re.sub("(?!^)([A-Z]+)", r"_\1", klass.__name__).lower()
