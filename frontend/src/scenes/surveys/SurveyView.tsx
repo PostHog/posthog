31727bc80a423f989ddd48f1a0443fddc145f0b8
Juraj Majerik <juro.majerik@gmail.com>
2023-10-12 12:53:41 +0200

    feat(surveys): Single Choice question results (#17923)

--- frontend/src/scenes/surveys/SurveyView.tsx
+++ frontend/src/scenes/surveys/SurveyView.tsx
@@ -28,7 +28,7 @@ import { dayjs } from 'lib/dayjs'
 import { defaultSurveyAppearance, SURVEY_EVENT_NAME } from './constants'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
-import { RatingQuestionBarChart, Summary } from './surveyViewViz'
+import { Summary, RatingQuestionBarChart, SingleChoiceQuestionPieChart } from './surveyViewViz'
 
 export function SurveyView({ id }: { id: string }): JSX.Element {
     const { survey, surveyLoading } = useValues(surveyLogic)
@@ -255,6 +255,8 @@ export function SurveyResult({ disableEventsTable }: { disableEventsTable?: bool
         surveyUserStatsLoading,
         surveyRatingResults,
         surveyRatingResultsReady,
+        surveySingleChoiceResults,
+        surveySingleChoiceResultsReady,
     } = useValues(surveyLogic)
     const { setCurrentQuestionIndexAndType } = useActions(surveyLogic)
     const { featureFlags } = useValues(featureFlagLogic)
@@ -265,14 +267,22 @@ export function SurveyResult({ disableEventsTable }: { disableEventsTable?: bool
                 <>
                     <Summary surveyUserStatsLoading={surveyUserStatsLoading} surveyUserStats={surveyUserStats} />
                     {survey.questions.map((question, i) => {
-                        if (question.type === 'rating') {
+                        if (question.type === SurveyQuestionType.Rating) {
                             return (
                                 <RatingQuestionBarChart
                                     key={`survey-q-${i}`}
                                     surveyRatingResults={surveyRatingResults}
                                     surveyRatingResultsReady={surveyRatingResultsReady}
                                     questionIndex={i}
-                                    question={question}
+                                />
+                            )
+                        } else if (question.type === SurveyQuestionType.SingleChoice) {
+                            return (
+                                <SingleChoiceQuestionPieChart
+                                    key={`survey-q-${i}`}
+                                    surveySingleChoiceResults={surveySingleChoiceResults}
+                                    surveySingleChoiceResultsReady={surveySingleChoiceResultsReady}
+                                    questionIndex={i}
                                 />
                             )
                         }
