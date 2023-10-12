4b5e8a4efc082fb25cf7a66f059e2e500d4b5f06
Michael Matloka <dev@twixes.com>
2023-10-12 16:51:51 +0000

    Fix error start and end initialization

--- hogql_parser/error.cpp
+++ hogql_parser/error.cpp
@@ -5,8 +5,8 @@ using namespace std;
 #define EXCEPTION_CLASS_IMPLEMENTATION(NAME, BASE)                                                       \
   NAME::NAME(const string& message, size_t start, size_t end) : BASE(message), start(start), end(end) {} \
   NAME::NAME(const char* message, size_t start, size_t end) : BASE(message), start(start), end(end) {}   \
-  NAME::NAME(const string& message) : BASE(message) {}                                                   \
-  NAME::NAME(const char* message) : BASE(message) {}
+  NAME::NAME(const string& message) : BASE(message), start(0), end(0) {}                                 \
+  NAME::NAME(const char* message) : BASE(message), start(0), end(0) {}
 
 EXCEPTION_CLASS_IMPLEMENTATION(HogQLException, runtime_error)
 
