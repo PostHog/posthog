8f7171067b878be69d91fbe76a21b076732758db
Michael Matloka <dev@twixes.com>
2023-10-13 15:58:08 +0200

    perf: Parse HogQL with C++ for a huge speedup (#17659)
    * Add partial C++ parser of HogQL
    
    * Support all the rules and add proper error handling
    
    * Use `AlignAfterOpenBracket: BlockIndent`
    
    * Reuse all the parser tests for the C++ backend
    
    * Fix relationship between headers and implementations
    
    * Add more error handling and fix minor issues
    
    * Build both Python and C++ parsers in the package.json script
    
    * Update ARRAY JOIN error assertion
    
    * Improve timeit
    
    * Move the C extension to the top level
    
    * Refactor `vector_to_list_string`
    
    * Build the parser on Linux
    
    * Build wheels for the parser
    
    * Simplify Linux build and fix macOS
    
    * Fix Homebrew paths on x86 and don't fail fast in CI
    
    * Set MACOSX_DEPLOYMENT_TARGET for C++20
    
    * Set up QEMU for Linux ARM builds
    
    * Publish the wheels on PyPI
    
    * Avoiding Linux ARM emulation in CI for now
    
    * Build sdist too
    
    * Revert Dockerfile changes
    
    * Fix PyPI publish
    
    * Add README and optimize sdist build
    
    * Use setup.py directly instead of build
    
    * Use PyPI hogql-parser instead of local
    
    * Also revert production-unit.Dockerfile
    
    * Fix sdist upload and add Linux ARM back
    
    * No Linux ARM build in the end
    
    * Fix artifact uploading
    
    * Do try building Linux ARM
    
    We need this for prod.
    
    * Use `npm` in `grammar:build`
    
    `pnpm` is not available in that job.
    
    * Fix formatting of hogql_parser
    
    * Build everything on macOS
    
    * Revert "Build everything on macOS"
    
    Not so fast actually.
    
    * Use hogql-parser=0.1.1
    
    * Fix dylib in macOS wheel
    
    * Bump hogql-parser version
    
    * Fix missing module error
    
    * Delete timeit.py
    
    * Make error handling robust
    
    * Format the C++
    
    * Use `hogql-parser==0.1.1`
    
    * Fix reserved keyword error assertions
    
    * Use HEAD hogql_paresr in CI
    
    * Fix `apt` usage
    
    * Add some sudo in CI
    
    * Ensure package will be releasable before build
    
    * Bump version to 0.1.3
    
    * Cover C++ `unquote_string` with tests
    
    * Use BuildJet ARM runners for ARM builds
    
    * Add some instructions
    
    * Add HogQL version check to backend CI
    
    * Update requirements.txt
    
    * Use `setuptools` instead of the deprecated `distutils`
    
    * Fix working dir in backend CI
    
    * Align ANTLR versions
    
    * Add test for "mismatched input"
    
    This is thrown differently than other HogQLSyntaxExceptions in C++, so might help reveal what's going on with tests failing only on Linux CI and not macOS dev
    
    * Add types and bump version
    
    * Comment instead of failing version check
    
    * Automate hogql-release version bump
    
    * Fix checkout token
    
    * Don't build hogql-parser if there were no changes
    
    * Update query snapshots
    
    * Update query snapshots
    
    * Update query snapshots
    
    * Update query snapshots
    
    * Improve documentation
    
    * Use new hogql-parser version
    
    * Fix error start and end initialization
    
    * Note `antlr4-cpp-runtime`
    
    Co-authored-by: Marius Andra <marius.andra@gmail.com>
    
    * Also remove NUL chars in C++
    
    * Check ANTLR4 runtime archive checksum for security
    
    * Note more decrefs to add
    
    * Add vector size checks
    
    * Use new hogql-parser version
    
    * Don't support the `start` arg in C++ `parse_expr`
    
    * Use new hogql-parser version
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Marius Andra <marius.andra@gmail.com>

--- posthog/hogql/parser.py
+++ posthog/hogql/parser.py
@@ -12,6 +12,24 @@ from posthog.hogql.grammar.HogQLParser import HogQLParser
 from posthog.hogql.parse_string import parse_string, parse_string_literal
 from posthog.hogql.placeholders import replace_placeholders
 from posthog.hogql.timings import HogQLTimings
+from hogql_parser import (
+    parse_expr as _parse_expr_cpp,
+    parse_order_expr as _parse_order_expr_cpp,
+    parse_select as _parse_select_cpp,
+)
+
+RULE_TO_PARSE_FUNCTION = {
+    "python": {
+        "expr": lambda string, start: HogQLParseTreeConverter(start=start).visit(get_parser(string).expr()),
+        "order_expr": lambda string: HogQLParseTreeConverter().visit(get_parser(string).orderExpr()),
+        "select": lambda string: HogQLParseTreeConverter().visit(get_parser(string).select()),
+    },
+    "cpp": {
+        "expr": lambda string, _: _parse_expr_cpp(string),  # The start arg is ignored in the C++ version
+        "order_expr": lambda string: _parse_order_expr_cpp(string),
+        "select": lambda string: _parse_select_cpp(string),
+    },
+}
 
 
 def parse_expr(
@@ -19,12 +37,13 @@ def parse_expr(
     placeholders: Optional[Dict[str, ast.Expr]] = None,
     start: Optional[int] = 0,
     timings: Optional[HogQLTimings] = None,
+    *,
+    backend: Literal["python", "cpp"] = "python",
 ) -> ast.Expr:
     if timings is None:
         timings = HogQLTimings()
-    with timings.measure("parse_expr"):
-        parse_tree = get_parser(expr).expr()
-        node = HogQLParseTreeConverter(start=start).visit(parse_tree)
+    with timings.measure(f"parse_expr_{backend}"):
+        node = RULE_TO_PARSE_FUNCTION[backend]["expr"](expr, start)
         if placeholders:
             with timings.measure("replace_placeholders"):
                 return replace_placeholders(node, placeholders)
@@ -32,13 +51,16 @@ def parse_expr(
 
 
 def parse_order_expr(
-    order_expr: str, placeholders: Optional[Dict[str, ast.Expr]] = None, timings: Optional[HogQLTimings] = None
+    order_expr: str,
+    placeholders: Optional[Dict[str, ast.Expr]] = None,
+    timings: Optional[HogQLTimings] = None,
+    *,
+    backend: Literal["python", "cpp"] = "python",
 ) -> ast.Expr:
     if timings is None:
         timings = HogQLTimings()
-    with timings.measure("parse_order_expr"):
-        parse_tree = get_parser(order_expr).orderExpr()
-        node = HogQLParseTreeConverter().visit(parse_tree)
+    with timings.measure(f"parse_order_expr_{backend}"):
+        node = RULE_TO_PARSE_FUNCTION[backend]["order_expr"](order_expr)
         if placeholders:
             with timings.measure("replace_placeholders"):
                 return replace_placeholders(node, placeholders)
@@ -46,13 +68,16 @@ def parse_order_expr(
 
 
 def parse_select(
-    statement: str, placeholders: Optional[Dict[str, ast.Expr]] = None, timings: Optional[HogQLTimings] = None
+    statement: str,
+    placeholders: Optional[Dict[str, ast.Expr]] = None,
+    timings: Optional[HogQLTimings] = None,
+    *,
+    backend: Literal["python", "cpp"] = "python",
 ) -> ast.SelectQuery | ast.SelectUnionQuery:
     if timings is None:
         timings = HogQLTimings()
-    with timings.measure("parse_select"):
-        parse_tree = get_parser(statement).select()
-        node = HogQLParseTreeConverter().visit(parse_tree)
+    with timings.measure(f"parse_select_{backend}"):
+        node = RULE_TO_PARSE_FUNCTION[backend]["select"](statement)
         if placeholders:
             with timings.measure("replace_placeholders"):
                 node = replace_placeholders(node, placeholders)
@@ -166,7 +191,7 @@ class HogQLParseTreeConverter(ParseTreeVisitor):
         if ctx.arrayJoinClause():
             array_join_clause = ctx.arrayJoinClause()
             if select_query.select_from is None:
-                raise HogQLException("Using ARRAY JOIN without a FROM clause is not permitted")
+                raise SyntaxException("Using ARRAY JOIN without a FROM clause is not permitted")
             if array_join_clause.LEFT():
                 select_query.array_join_op = "LEFT ARRAY JOIN"
             elif array_join_clause.INNER():
@@ -176,7 +201,7 @@ class HogQLParseTreeConverter(ParseTreeVisitor):
             select_query.array_join_list = self.visit(array_join_clause.columnExprList())
             for expr in select_query.array_join_list:
                 if not isinstance(expr, ast.Alias):
-                    raise HogQLException("ARRAY JOIN arrays must have an alias", start=expr.start, end=expr.end)
+                    raise SyntaxException("ARRAY JOIN arrays must have an alias", start=expr.start, end=expr.end)
 
         if ctx.topClause():
             raise NotImplementedException(f"Unsupported: SelectStmt.topClause()")
@@ -301,7 +326,7 @@ class HogQLParseTreeConverter(ParseTreeVisitor):
 
     def visitJoinOpFull(self, ctx: HogQLParser.JoinOpFullContext):
         tokens = []
-        if ctx.LEFT():
+        if ctx.FULL():
             tokens.append("FULL")
         if ctx.OUTER():
             tokens.append("OUTER")
@@ -421,6 +446,7 @@ class HogQLParseTreeConverter(ParseTreeVisitor):
         )
 
     def visitColumnExprAlias(self, ctx: HogQLParser.ColumnExprAliasContext):
+        alias: str
         if ctx.alias():
             alias = self.visit(ctx.alias())
         elif ctx.identifier():
@@ -431,8 +457,8 @@ class HogQLParseTreeConverter(ParseTreeVisitor):
             raise NotImplementedException(f"Must specify an alias")
         expr = self.visit(ctx.columnExpr())
 
-        if alias in RESERVED_KEYWORDS:
-            raise HogQLException(f"Alias '{alias}' is a reserved keyword")
+        if alias.lower() in RESERVED_KEYWORDS:
+            raise SyntaxException(f'"{alias}" cannot be an alias or identifier, as it\'s a reserved keyword')
 
         return ast.Alias(expr=expr, alias=alias)
 
@@ -749,9 +775,9 @@ class HogQLParseTreeConverter(ParseTreeVisitor):
         return ast.Placeholder(field=parse_string_literal(ctx.PLACEHOLDER()))
 
     def visitTableExprAlias(self, ctx: HogQLParser.TableExprAliasContext):
-        alias = self.visit(ctx.alias() or ctx.identifier())
-        if alias in RESERVED_KEYWORDS:
-            raise HogQLException(f"Alias '{alias}' is a reserved keyword")
+        alias: str = self.visit(ctx.alias() or ctx.identifier())
+        if alias.lower() in RESERVED_KEYWORDS:
+            raise SyntaxException(f'"{alias}" cannot be an alias or identifier, as it\'s a reserved keyword')
         table = self.visit(ctx.tableExpr())
         if isinstance(table, ast.JoinExpr):
             table.alias = alias
