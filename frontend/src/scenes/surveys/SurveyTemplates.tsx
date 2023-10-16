93e8ccc4eb983a760bf2c46b5756e0a49ef01b5b
Li Yi Yu <li@posthog.com>
2023-10-16 16:32:07 -0400

    feat(surveys): templates (#17904)
    * routes for survey templates
    
    * basic on click setup logic
    
    * Update UI snapshots for `chromium` (2)
    
    * import type
    
    * Apply suggestions from code review
    
    Co-authored-by: Joe Martin <84011561+joethreepwood@users.noreply.github.com>
    
    * address feedback from cory
    
    * fix logic
    
    * remove unused logic file
    
    * Update frontend/src/scenes/surveys/constants.tsx
    
    Co-authored-by: Joe Martin <84011561+joethreepwood@users.noreply.github.com>
    
    * Update frontend/src/scenes/surveys/constants.tsx
    
    Co-authored-by: Neil Kakkar <neilkakkar@gmail.com>
    
    * address comments
    
    * Update UI snapshots for `chromium` (2)
    
    * more fixes
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * move description and title together
    
    * fix e2e
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Joe Martin <84011561+joethreepwood@users.noreply.github.com>
    Co-authored-by: Neil Kakkar <neilkakkar@gmail.com>

---
+++ frontend/src/scenes/surveys/SurveyTemplates.tsx
@@ -0,0 +1,62 @@
+import { SceneExport } from 'scenes/sceneTypes'
+import { SurveyAppearance } from './SurveyAppearance'
+import { defaultSurveyTemplates } from './constants'
+import { SurveyQuestion } from '~/types'
+import './SurveyTemplates.scss'
+import { useActions } from 'kea'
+import { PageHeader } from 'lib/components/PageHeader'
+import { LemonButton } from '@posthog/lemon-ui'
+import { urls } from 'scenes/urls'
+import { surveyLogic } from './surveyLogic'
+
+export const scene: SceneExport = {
+    component: SurveyTemplates,
+}
+
+export function SurveyTemplates(): JSX.Element {
+    const { setSurveyTemplateValues } = useActions(surveyLogic({ id: 'new' }))
+
+    return (
+        <>
+            <PageHeader
+                title={'New survey'}
+                buttons={
+                    <LemonButton type="primary" to={urls.survey('new')} data-attr="new-blank-survey">
+                        Create blank survey
+                    </LemonButton>
+                }
+            />
+            <div className="flex flex-row flex-wrap gap-8 ml-8 mt-8">
+                {defaultSurveyTemplates.map((template, idx) => {
+                    return (
+                        <div
+                            className="flex flex-col items-center"
+                            key={idx}
+                            onClick={() =>
+                                setSurveyTemplateValues({ name: template.type, questions: template.questions })
+                            }
+                        >
+                            <span className="mb-2 text-md">
+                                <b>{template.type}</b>
+                            </span>
+                            <span className="flex flex-wrap text-xs text-muted max-w-80 font-medium mb-2">
+                                {template.description}
+                            </span>
+                            <div className="SurveyTemplateContainer">
+                                <div className="SurveyTemplate">
+                                    <SurveyAppearance
+                                        key={idx}
+                                        type={template.questions[0].type}
+                                        question={template.questions[0].question}
+                                        appearance={{ whiteLabel: true, ...template.appearance }}
+                                        surveyQuestionItem={template.questions[0] as SurveyQuestion}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
