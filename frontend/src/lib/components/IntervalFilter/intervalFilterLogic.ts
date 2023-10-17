a15da1508e48fc8715b4db6434ac12b066da2115
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-10-17 16:15:30 +0200

    refactor enableIntervals from reducer to selector

--- frontend/src/lib/components/IntervalFilter/intervalFilterLogic.ts
+++ frontend/src/lib/components/IntervalFilter/intervalFilterLogic.ts
@@ -16,17 +16,36 @@ export const intervalFilterLogic = kea<intervalFilterLogicType>({
     path: (key) => ['lib', 'components', 'IntervalFilter', 'intervalFilterLogic', key],
     connect: (props: InsightLogicProps) => ({
         actions: [insightVizDataLogic(props), ['updateQuerySource']],
-        values: [insightVizDataLogic(props), ['interval', 'querySource']],
+        values: [insightVizDataLogic(props), ['interval', 'querySource', 'activeUsersMath']],
     }),
     actions: () => ({
         setInterval: (interval: IntervalKeyType) => ({ interval }),
-        setEnabledIntervals: (enabledIntervals: Intervals) => ({ enabledIntervals }),
     }),
-    reducers: () => ({
+    selectors: () => ({
         enabledIntervals: [
-            { ...intervals } as Intervals,
-            {
-                setEnabledIntervals: (_, { enabledIntervals }) => enabledIntervals,
+            (s) => [s.activeUsersMath],
+            (activeUsersMath) => {
+                const enabledIntervals: Intervals = { ...intervals }
+
+                if (activeUsersMath) {
+                    // Disallow grouping by hour for WAUs/MAUs as it's an expensive query that produces a view that's not useful for users
+                    enabledIntervals.hour = {
+                        ...enabledIntervals.hour,
+                        disabledReason:
+                            'Grouping by hour is not supported on insights with weekly or monthly active users series.',
+                    }
+
+                    // Disallow grouping by month for WAUs as the resulting view is misleading to users
+                    if (activeUsersMath === BaseMathType.WeeklyActiveUsers) {
+                        enabledIntervals.month = {
+                            ...enabledIntervals.month,
+                            disabledReason:
+                                'Grouping by month is not supported on insights with weekly active users series.',
+                        }
+                    }
+                }
+
+                return enabledIntervals
             },
         ],
     }),
@@ -40,61 +59,26 @@ export const intervalFilterLogic = kea<intervalFilterLogicType>({
             const { date_from, date_to } = querySource.dateRange || {}
             const previousDateRange = selectors.querySource(previousState)?.dateRange || {}
 
-            let activeUsersMath: BaseMathType.WeeklyActiveUsers | BaseMathType.MonthlyActiveUsers | null = null
-
-            // We disallow grouping by certain intervals for weekly active users and monthly active users views
-            // e.g. WAUs grouped by month. Here, look for the first event/action running WAUs/MAUs math and
-            // pass that down to the interval filter to determine what groupings are allowed.
-            for (const series of (values.querySource as TrendsQuery)?.series || []) {
-                if (series.math === BaseMathType.WeeklyActiveUsers) {
-                    activeUsersMath = BaseMathType.WeeklyActiveUsers
-                    break
-                }
-
-                if (series.math === BaseMathType.MonthlyActiveUsers) {
-                    activeUsersMath = BaseMathType.MonthlyActiveUsers
-                    break
-                }
-            }
-
-            const enabledIntervals: Intervals = { ...intervals }
-
-            if (activeUsersMath) {
-                // Disallow grouping by hour for WAUs/MAUs as it's an expensive query that produces a view that's not useful for users
-                enabledIntervals.hour = {
-                    ...enabledIntervals.hour,
-                    disabledReason:
-                        'Grouping by hour is not supported on insights with weekly or monthly active users series.',
-                }
-
-                // Disallow grouping by month for WAUs as the resulting view is misleading to users
-                if (activeUsersMath === BaseMathType.WeeklyActiveUsers) {
-                    enabledIntervals.month = {
-                        ...enabledIntervals.month,
-                        disabledReason:
-                            'Grouping by month is not supported on insights with weekly active users series.',
-                    }
-                }
-            }
-
-            actions.setEnabledIntervals(enabledIntervals)
-
             // If the user just flipped an event action to use WAUs/MAUs math and their
             // current interval is unsupported by the math type, switch their interval
             // to an appropriate allowed interval and inform them of the change via a toast
             if (
-                activeUsersMath &&
+                values.activeUsersMath &&
                 (values.querySource as TrendsQuery)?.interval &&
-                enabledIntervals[(values.querySource as TrendsQuery).interval as IntervalType].disabledReason
+                values.enabledIntervals[(values.querySource as TrendsQuery).interval as IntervalType].disabledReason
             ) {
                 if (values.interval === 'hour') {
                     lemonToast.info(
-                        `Switched to grouping by day, because "${BASE_MATH_DEFINITIONS[activeUsersMath].name}" does not support grouping by ${values.interval}.`
+                        `Switched to grouping by day, because "${
+                            BASE_MATH_DEFINITIONS[values.activeUsersMath].name
+                        }" does not support grouping by ${values.interval}.`
                     )
                     actions.updateQuerySource({ interval: 'day' } as Partial<InsightQueryNode>)
                 } else {
                     lemonToast.info(
-                        `Switched to grouping by week, because "${BASE_MATH_DEFINITIONS[activeUsersMath].name}" does not support grouping by ${values.interval}.`
+                        `Switched to grouping by week, because "${
+                            BASE_MATH_DEFINITIONS[values.activeUsersMath].name
+                        }" does not support grouping by ${values.interval}.`
                     )
                     actions.updateQuerySource({ interval: 'week' } as Partial<InsightQueryNode>)
                 }
