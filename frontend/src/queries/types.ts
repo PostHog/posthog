101fa137fa11d728584292ea86f11fc95f225604
Robbie Coomber <robbie.coomber@gmail.com>
2023-10-18 20:46:01 +0100

    Convert all of schema.ts to schema.py

---
+++ frontend/src/queries/types.ts
@@ -0,0 +1,35 @@
+import { InsightLogicProps } from '~/types'
+import { ComponentType } from 'react'
+import { DataTableNode } from '~/queries/schema'
+
+/** Pass custom metadata to queries. Used for e.g. custom columns in the DataTable. */
+export interface QueryContext {
+    /** Column templates for the DataTable */
+    columns?: Record<string, QueryContextColumn>
+    /** used to override the value in the query */
+    showOpenEditorButton?: boolean
+    showQueryEditor?: boolean
+    /* Adds help and examples to the query editor component */
+    showQueryHelp?: boolean
+    insightProps?: InsightLogicProps
+    emptyStateHeading?: string
+    emptyStateDetail?: string
+}
+
+export type QueryContextColumnTitleComponent = ComponentType<{
+    columnName: string
+    query: DataTableNode
+}>
+
+export type QueryContextColumnComponent = ComponentType<{
+    columnName: string
+    query: DataTableNode
+    record: unknown
+    value: unknown
+}>
+
+interface QueryContextColumn {
+    title?: string
+    renderTitle?: QueryContextColumnTitleComponent
+    render?: QueryContextColumnComponent
+}
