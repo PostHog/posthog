60e4c77075592f7beb6a81d5e4aefc27d4adf6b5
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-10-20 11:02:59 +0200

    add type annotation

--- frontend/src/scenes/notebooks/Nodes/notebookNodePersonFeedLogic.tsx
+++ frontend/src/scenes/notebooks/Nodes/notebookNodePersonFeedLogic.tsx
@@ -4,11 +4,13 @@ import { loaders } from 'kea-loaders'
 import { query } from '~/queries/query'
 import { NodeKind, SessionsTimelineQuery, SessionsTimelineQueryResponse } from '~/queries/schema'
 
+import type { notebookNodePersonFeedLogicType } from './notebookNodePersonFeedLogicType'
+
 export type NotebookNodePersonFeedLogicProps = {
     personId: string
 }
 
-export const notebookNodePersonFeedLogic = kea([
+export const notebookNodePersonFeedLogic = kea<notebookNodePersonFeedLogicType>([
     props({} as NotebookNodePersonFeedLogicProps),
     path((key) => ['scenes', 'notebooks', 'Notebook', 'Nodes', 'notebookNodePersonFeedLogic', key]),
     key(({ personId }) => personId),
