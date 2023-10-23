933b537c018163f8f4316275c346b3db92cf2728
Paul D'Ambra <paul@posthog.com>
2023-10-23 11:22:02 +0100

    all the options

--- frontend/src/scenes/session-recordings/settings/SessionRecordingSettings.tsx
+++ frontend/src/scenes/session-recordings/settings/SessionRecordingSettings.tsx
@@ -116,7 +116,7 @@ export function SessionRecordingSettings({ inModal = false }: SessionRecordingSe
                             }}
                             options={[
                                 {
-                                    label: '100%',
+                                    label: '100% (no sampling)',
                                     value: '1.00',
                                 },
                                 {
@@ -128,12 +128,76 @@ export function SessionRecordingSettings({ inModal = false }: SessionRecordingSe
                                     value: '0.90',
                                 },
                                 {
+                                    label: '85%',
+                                    value: '0.85',
+                                },
+                                {
                                     label: '80%',
                                     value: '0.80',
                                 },
                                 {
+                                    label: '75%',
+                                    value: '0.75',
+                                },
+                                {
+                                    label: '70%',
+                                    value: '0.70',
+                                },
+                                {
+                                    label: '65%',
+                                    value: '0.65',
+                                },
+                                {
+                                    label: '60%',
+                                    value: '0.60',
+                                },
+                                {
+                                    label: '55%',
+                                    value: '0.55',
+                                },
+                                {
                                     label: '50%',
                                     value: '0.50',
+                                },
+                                {
+                                    label: '45%',
+                                    value: '0.45',
+                                },
+                                {
+                                    label: '40%',
+                                    value: '0.40',
+                                },
+                                {
+                                    label: '35%',
+                                    value: '0.35',
+                                },
+                                {
+                                    label: '30%',
+                                    value: '0.30',
+                                },
+                                {
+                                    label: '25%',
+                                    value: '0.25',
+                                },
+                                {
+                                    label: '20%',
+                                    value: '0.20',
+                                },
+                                {
+                                    label: '15%',
+                                    value: '0.15',
+                                },
+                                {
+                                    label: '10%',
+                                    value: '0.10',
+                                },
+                                {
+                                    label: '5%',
+                                    value: '0.05',
+                                },
+                                {
+                                    label: '0% (replay disabled)',
+                                    value: '0.00',
                                 },
                             ]}
                             value={
