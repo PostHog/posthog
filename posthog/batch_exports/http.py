1a9adb40505c86608f9341029a9fb5e0f22fe0d9
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-10-23 20:48:32 +0200

    test(batch-exports-backfills): Add test on delete

--- posthog/batch_exports/http.py
+++ posthog/batch_exports/http.py
@@ -266,9 +266,9 @@ class BatchExportViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
 
         batch_export = self.get_object()
         temporal = sync_connect()
-        backfill_export(temporal, str(batch_export.pk), team_id, start_at, end_at)
+        backfill_id = backfill_export(temporal, str(batch_export.pk), team_id, start_at, end_at)
 
-        return response.Response()
+        return response.Response({"backfill_id": backfill_id})
 
     @action(methods=["POST"], detail=True)
     def pause(self, request: request.Request, *args, **kwargs) -> response.Response:
@@ -328,7 +328,7 @@ class BatchExportViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
 
         for backfill in BatchExportBackfill.objects.filter(batch_export=instance):
             if backfill.status == BatchExportBackfill.Status.RUNNING:
-                cancel_running_batch_export_backfill(temporal, str(backfill.pk))
+                cancel_running_batch_export_backfill(temporal, backfill.workflow_id)
 
 
 class BatchExportLogEntrySerializer(DataclassSerializer):
