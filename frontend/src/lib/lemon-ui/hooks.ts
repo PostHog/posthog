8b8cedf0524aa61de37a17074da869f0f4bbb31c
David Newell <d.newell1@outlook.com>
2023-10-25 17:08:29 +0100

    chore: update LemonUI types (#17755)

--- frontend/src/lib/lemon-ui/hooks.ts
+++ frontend/src/lib/lemon-ui/hooks.ts
@@ -6,7 +6,7 @@ import { useLayoutEffect, useRef, useState } from 'react'
  * @private
  */
 export function useSliderPositioning<C extends HTMLElement, S extends HTMLElement>(
-    currentValue: string | number | null | undefined,
+    currentValue: React.Key | null | undefined,
     transitionMs: number
 ): {
     containerRef: React.RefObject<C>
