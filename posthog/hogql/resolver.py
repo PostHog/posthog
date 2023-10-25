ccdc2ae9b588fabd1b2ec64ecb5cf541fc5753d5
Tom Owers <tom@posthog.com>
2023-10-25 14:22:45 +0100

    feat: Adding session duration lazy join (#18132)
    * Added session duration as a lazy join
    
    * Moved the session field within events
    
    * Moved session duration logic to the join itself
    
    * More improvements
    
    * Updated the extractor to not use a traverser
    
    * Fixed unused var
    
    * Fixed broken join func and removed unused join func
    
    * Update query snapshots
    
    * Resolve aliased table field properly and clean the field chain
    
    * Update query snapshots
    
    * Update query snapshots
    
    * Update query snapshots
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- posthog/hogql/resolver.py
+++ posthog/hogql/resolver.py
@@ -11,6 +11,7 @@ from posthog.hogql.errors import ResolverException
 from posthog.hogql.functions.mapping import validate_function_args
 from posthog.hogql.functions.sparkline import sparkline
 from posthog.hogql.parser import parse_select
+from posthog.hogql.resolver_utils import lookup_cte_by_name, lookup_field_by_name
 from posthog.hogql.visitor import CloningVisitor, clone_expr
 from posthog.models.utils import UUIDT
 from posthog.hogql.database.schema.events import EventsTable
@@ -563,30 +564,3 @@ class Resolver(CloningVisitor):
         if isinstance(node.type, ast.TableAliasType):
             return isinstance(node.type.table_type.table, S3Table)
         return False
-
-
-def lookup_field_by_name(scope: ast.SelectQueryType, name: str) -> Optional[ast.Type]:
-    """Looks for a field in the scope's list of aliases and children for each joined table."""
-    if name in scope.aliases:
-        return scope.aliases[name]
-    else:
-        named_tables = [table for table in scope.tables.values() if table.has_child(name)]
-        anonymous_tables = [table for table in scope.anonymous_tables if table.has_child(name)]
-        tables_with_field = named_tables + anonymous_tables
-
-        if len(tables_with_field) > 1:
-            raise ResolverException(f"Ambiguous query. Found multiple sources for field: {name}")
-        elif len(tables_with_field) == 1:
-            return tables_with_field[0].get_child(name)
-
-        if scope.parent:
-            return lookup_field_by_name(scope.parent, name)
-
-        return None
-
-
-def lookup_cte_by_name(scopes: List[ast.SelectQueryType], name: str) -> Optional[ast.CTE]:
-    for scope in reversed(scopes):
-        if scope and scope.ctes and name in scope.ctes:
-            return scope.ctes[name]
-    return None
