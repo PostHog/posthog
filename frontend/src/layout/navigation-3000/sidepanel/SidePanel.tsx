8ec0ff92ac16a043a0317b74e6a66005bbe01b16
Ben White <ben@posthog.com>
2023-10-26 12:16:09 +0200

    Fix

--- frontend/src/layout/navigation-3000/sidepanel/SidePanel.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/SidePanel.tsx
@@ -4,7 +4,7 @@ import { IconNotebook, IconQuestion } from '@posthog/icons'
 
 export function SidePanel(): JSX.Element {
     return (
-        <nav className="SidePanel3000">
+        <div className="SidePanel3000">
             <div className="SidePanel3000__content">
                 <div className="SidePanel3000__top">
                     <div className="rotate-90 flex items-center gap-2 px-2">
@@ -13,6 +13,6 @@ export function SidePanel(): JSX.Element {
                     </div>
                 </div>
             </div>
-        </nav>
+        </div>
     )
 }
