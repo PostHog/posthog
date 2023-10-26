ac823c0fccb0a522c343f302f2fba7c339216cc9
Ben White <ben@posthog.com>
2023-10-26 14:52:35 +0200

    fix

--- frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelDocs.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelDocs.tsx
@@ -1,6 +1,6 @@
 export const SidePanelDocs = (): JSX.Element => {
     return (
-        <div className="border-l w-full h-full">
+        <div className="border-l w-full h-full overflow-hidden">
             <iframe src="https://posthog.com/docs" title="Docs" className="w-full h-full" />
         </div>
     )
