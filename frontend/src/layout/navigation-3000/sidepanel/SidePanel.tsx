c79e2f55c861018c6a5f8cbbf0644d4c01f003bc
Ben White <ben@posthog.com>
2023-10-26 12:09:59 +0200

    Remove button from breadcrumbs and correct icons

--- frontend/src/layout/navigation-3000/sidepanel/SidePanel.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/SidePanel.tsx
@@ -1,6 +1,6 @@
 import { LemonButton } from '@posthog/lemon-ui'
 import './SidePanel.scss'
-import { IconComment, IconNotebook } from 'lib/lemon-ui/icons'
+import { IconNotebook, IconQuestion } from '@posthog/icons'
 
 export function SidePanel(): JSX.Element {
     return (
@@ -8,8 +8,8 @@ export function SidePanel(): JSX.Element {
             <div className="SidePanel3000__content">
                 <div className="SidePanel3000__top">
                     <div className="rotate-90 flex items-center gap-2 px-2">
-                        <LemonButton icon={<IconNotebook className="rotate-270" />}>Notebooks</LemonButton>
-                        <LemonButton icon={<IconComment className="rotate-270" />}>Feedback</LemonButton>
+                        <LemonButton icon={<IconNotebook className="rotate-270 w-6" />}>Notebooks</LemonButton>
+                        <LemonButton icon={<IconQuestion className="rotate-270 w-6" />}>Feedback</LemonButton>
                     </div>
                 </div>
             </div>
