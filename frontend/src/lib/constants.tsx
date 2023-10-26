3fafef639d2586034b0c2e95262054d3f7f9832d
Ben White <ben@posthog.com>
2023-10-26 08:45:54 +0200

    feat: Persons Feed (#18183)

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -167,6 +167,7 @@ export const FEATURE_FLAGS = {
     PERSONS_HOGQL_QUERY: 'persons-hogql-query', // owner: @mariusandra
     NOTEBOOK_CANVASES: 'notebook-canvases', // owner: #team-monitoring
     SESSION_RECORDING_SAMPLING: 'session-recording-sampling', // owner: #team-monitoring
+    PERSON_FEED_CANVAS: 'person-feed-canvas', // owner: #project-canvas
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
