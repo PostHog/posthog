0825c97c072bf4f6cba19e753b11fcc31103ac05
Ellie Huxtable <ellie@elliehuxtable.com>
2023-10-26 12:04:51 +0100

    Run 'ruff format .'

--- posthog/models/team/team.py
+++ posthog/models/team/team.py
@@ -175,7 +175,7 @@ class Team(UUIDClassicModel):
         decimal_places=2,
         validators=[MinValueValidator(Decimal(0)), MaxValueValidator(Decimal(1))],
     )
-    session_recording_minimum_duration_milliseconds: (models.IntegerField) = models.IntegerField(
+    session_recording_minimum_duration_milliseconds: models.IntegerField = models.IntegerField(
         null=True,
         blank=True,
         validators=[MinValueValidator(0), MaxValueValidator(15000)],
