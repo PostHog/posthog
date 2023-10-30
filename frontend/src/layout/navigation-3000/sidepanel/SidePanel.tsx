e2cdee074e13145123a99ed1bb909f77a7be2f4e
Ben White <ben@posthog.com>
2023-10-30 09:54:51 +0100

    Added multiple resizers

--- frontend/src/layout/navigation-3000/sidepanel/SidePanel.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/SidePanel.tsx
@@ -91,6 +91,7 @@ export function SidePanel(): JSX.Element {
                         ))}
                 </div>
             </div>
+            <Resizer {...resizerLogicProps} offset={'3rem'} />
 
             {PanelConent ? (
                 <div className="SidePanel3000__content">
