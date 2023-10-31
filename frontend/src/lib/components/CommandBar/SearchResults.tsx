e7ae614d37c94259acab63540b367788898fb1ef
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-10-31 16:10:55 +0100

    add empty state

--- frontend/src/lib/components/CommandBar/SearchResults.tsx
+++ frontend/src/lib/components/CommandBar/SearchResults.tsx
@@ -1,5 +1,6 @@
 import { useActions, useValues } from 'kea'
 import { useEventListener } from 'lib/hooks/useEventListener'
+import { DetectiveHog } from '../hedgehogs'
 
 import { searchBarLogic } from './searchBarLogic'
 import SearchResult from './SearchResult'
@@ -27,6 +28,13 @@ const SearchResults = (): JSX.Element => {
 
     return (
         <div className="grow overscroll-none overflow-y-auto">
+            {filterSearchResults?.length === 0 && (
+                <div className="h-full flex flex-col items-center justify-center p-3">
+                    <h3 className="mb-0 text-xl">No results</h3>
+                    <p className="opacity-75 mb-0">This doesn't happen often, but we're stumped!</p>
+                    <DetectiveHog height={150} width={150} />
+                </div>
+            )}
             {filterSearchResults?.map((result, index) => (
                 <SearchResult
                     key={`${result.type}_${result.result_id}`}
