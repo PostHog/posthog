f9a5ee74a884f1613a67368e95c4e2f2b2be5796
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-02 16:44:07 +0100

    separate out action result

--- frontend/src/lib/components/CommandBar/ActionResults.tsx
+++ frontend/src/lib/components/CommandBar/ActionResults.tsx
@@ -3,6 +3,7 @@ import { useEventListener } from 'lib/hooks/useEventListener'
 import { Command, CommandResult, CommandResultDisplayable } from '../CommandPalette/commandPaletteLogic'
 import { DetectiveHog } from '../hedgehogs'
 import { actionBarLogic } from './actionBarLogic'
+import ActionResult from './ActionResult'
 
 import { searchBarLogic } from './searchBarLogic'
 import SearchResult, { SearchResultSkeleton } from './SearchResult'
@@ -12,16 +13,6 @@ const ActionResults = (): JSX.Element => {
     //     useValues(searchBarLogic)
     // const { onArrowUp, onArrowDown, openResult } = useActions(searchBarLogic)
     const { searchResults } = useValues(actionBarLogic)
-    return (
-        <>
-            {searchResults.map((command: CommandResultDisplayable) => (
-                <div key={command.index} className="flex">
-                    <command.icon className="" />
-                    <div className="">{command.display}</div>
-                </div>
-            ))}
-        </>
-    )
 
     // useEventListener('keydown', (event) => {
     //     if (!filterSearchResults) {
@@ -40,34 +31,26 @@ const ActionResults = (): JSX.Element => {
     //     }
     // })
 
-    // return (
-    //     <div className="grow overscroll-none overflow-y-auto">
-    //         {searchResponseLoading && (
-    //             <>
-    //                 <SearchResultSkeleton />
-    //                 <SearchResultSkeleton />
-    //                 <SearchResultSkeleton />
-    //             </>
-    //         )}
-    //         {!searchResponseLoading && filterSearchResults?.length === 0 && (
-    //             <div className="h-full flex flex-col items-center justify-center p-3">
-    //                 <h3 className="mb-0 text-xl">No results</h3>
-    //                 <p className="opacity-75 mb-0">This doesn't happen often, but we're stumped!</p>
-    //                 <DetectiveHog height={150} width={150} />
-    //             </div>
-    //         )}
-    //         {!searchResponseLoading &&
-    //             filterSearchResults?.map((result, index) => (
-    //                 <SearchResult
-    //                     key={`${result.type}_${result.result_id}`}
-    //                     result={result}
-    //                     resultIndex={index}
-    //                     focused={index === activeResultIndex}
-    //                     keyboardFocused={index === keyboardResultIndex}
-    //                 />
-    //             ))}
-    //     </div>
-    // )
+    return (
+        <div className="grow overscroll-none overflow-y-auto">
+            {searchResults?.length === 0 && (
+                <div className="h-full flex flex-col items-center justify-center p-3">
+                    <h3 className="mb-0 text-xl">No results</h3>
+                    <p className="opacity-75 mb-0">This doesn't happen often, but we're stumped!</p>
+                    <DetectiveHog height={150} width={150} />
+                </div>
+            )}
+            {searchResults?.map((result, index) => (
+                <ActionResult
+                    key={`${result.type}_${result.result_id}`}
+                    result={result}
+                    resultIndex={index}
+                    // focused={index === activeResultIndex}
+                    // keyboardFocused={index === keyboardResultIndex}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default ActionResults
