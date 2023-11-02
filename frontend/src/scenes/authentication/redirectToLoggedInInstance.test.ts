6f5ee4576c5e2310841df28541bf2974e9da6258
David Newell <david@posthog.com>
2023-11-02 18:34:56 +0000

    fix: logged in redirection

--- frontend/src/scenes/authentication/redirectToLoggedInInstance.test.ts
+++ frontend/src/scenes/authentication/redirectToLoggedInInstance.test.ts
@@ -6,13 +6,13 @@ describe('redirectToLoggedInInstance', () => {
         ['handles the empty string', '', null],
         ['handles the sneaky string', '         ', null],
         ['handles not URLs', 'yo ho ho', null],
-        ['handles EU', 'https://eu.posthog.com', 'EU'],
-        ['handles US', 'https://app.posthog.com', 'US'],
-        ['handles leading quotes', '"https://eu.posthog.com', 'EU'],
-        ['handles trailing quotes', 'https://eu.posthog.com"', 'EU'],
-        ['handles wrapping quotes', '"https://eu.posthog.com"', 'EU'],
-        ['handles ports', 'https://app.posthog.com:8123', 'US'],
-        ['handles longer urls', 'https://app.posthog.com:1234?query=parameter#hashParam', 'US'],
+        ['handles EU', 'https://eu.posthog.com', 'eu'],
+        ['handles US', 'https://app.posthog.com', 'app'],
+        ['handles leading quotes', '"https://eu.posthog.com', 'eu'],
+        ['handles trailing quotes', 'https://eu.posthog.com"', 'eu'],
+        ['handles wrapping quotes', '"https://eu.posthog.com"', 'eu'],
+        ['handles ports', 'https://app.posthog.com:8123', 'app'],
+        ['handles longer urls', 'https://app.posthog.com:1234?query=parameter#hashParam', 'app'],
     ])('%s', (_name, cookie, expected) => {
         expect(cleanedCookieSubdomain(cookie)).toEqual(expected)
     })
