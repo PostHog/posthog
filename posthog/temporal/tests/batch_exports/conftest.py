c539a61a49a7ef3d004662fdb4e1364a91f16d32
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-11-03 13:58:15 +0100

    refactor: Make exclude_events and interval fixtures available for all

--- posthog/temporal/tests/batch_exports/conftest.py
+++ posthog/temporal/tests/batch_exports/conftest.py
@@ -1,4 +1,35 @@
+import pytest
 import pytest_asyncio
+
+
+@pytest.fixture
+def interval(request) -> str:
+    """A parametrizable fixture to configure a batch export interval.
+
+    By decorating a test function with @pytest.mark.parametrize("interval", ..., indirect=True)
+    it's possible to set the interval that will be used to create a BatchExport.
+    Possible values are "hour", "day", or "every {value} {unit}".
+    As interval must be defined for every BatchExport, so we default to "hour" to
+    support tests that do not parametrize this.
+    """
+    try:
+        return request.param
+    except AttributeError:
+        return "hour"
+
+
+@pytest.fixture
+def exclude_events(request) -> list[str] | None:
+    """A parametrizable fixture to configure event names to exclude from a BatchExport.
+
+    By decorating a test function with @pytest.mark.parametrize("exclude_events", ..., indirect=True)
+    it's possible to set the exclude_events that will be used to create a BatchExport.
+    Any list of event names can be used, or None (the default).
+    """
+    try:
+        return request.param
+    except AttributeError:
+        return None
 
 
 @pytest_asyncio.fixture(autouse=True)
