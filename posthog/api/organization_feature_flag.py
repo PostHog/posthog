2af436022af160e12d599885db53d7de15c65508
Juraj Majerik <juro.majerik@gmail.com>
2023-11-03 17:14:43 +0100

    use ViewSet instead of APIView

--- posthog/api/organization_feature_flag.py
+++ posthog/api/organization_feature_flag.py
@@ -1,32 +1,29 @@
-from rest_framework.views import APIView
+from posthog.api.routing import StructuredViewSetMixin
 from posthog.models import FeatureFlag
-from posthog.models.organization import Organization
-from rest_framework.response import Response
 from posthog.permissions import OrganizationMemberPermissions
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import serializers
+from rest_framework import (
+    mixins,
+    viewsets,
+)
 
 
-class OrganizationFeatureFlagView(APIView):
+class OrganizationFeatureFlagView(
+    viewsets.ViewSet,
+    StructuredViewSetMixin,
+    mixins.RetrieveModelMixin,
+):
     """
     Retrieves all feature flags for a given organization and key.
     """
 
-    basename = "org_feature_flags"
     permission_classes = [IsAuthenticated, OrganizationMemberPermissions]
 
-    def initial(self, request, *args, **kwargs):
-        # Organization needs to be set before permissions are checked
-        try:
-            self.organization = Organization.objects.get(id=kwargs.get("organization_id"))
-        except Organization.DoesNotExist:
-            raise serializers.ValidationError("Organization does not exist")
+    def retrieve(self, request, *args, **kwargs):
+        feature_flag_key = kwargs.get("pk")
 
-        super().initial(request, *args, **kwargs)
-
-    def get(self, request, organization_id, feature_flag_key):
-        organization = Organization.objects.get(id=organization_id)
-        teams = organization.teams.all()
+        teams = self.organization.teams.all()
 
         flags = FeatureFlag.objects.filter(key=feature_flag_key, team_id__in=[team.id for team in teams])
         flags_data = [{"team_id": flag.team_id, "active": flag.active} for flag in flags]
--- posthog/api/test/test_organization_feature_flag.py
+++ posthog/api/test/test_organization_feature_flag.py
@@ -1,10 +1,10 @@
 from rest_framework import status
 from posthog.models.team.team import Team
 from posthog.models import FeatureFlag
-from posthog.test.base import APIBaseTest
+from posthog.test.base import APIBaseTest, QueryMatchingTest, snapshot_postgres_queries
 
 
-class OrganizationFeatureFlagViewTests(APIBaseTest):
+class OrganizationFeatureFlagViewTests(APIBaseTest, QueryMatchingTest):
     def setUp(self):
         self.team_1 = self.team
         self.team_2 = Team.objects.create(organization=self.organization)
@@ -20,6 +20,7 @@ class OrganizationFeatureFlagViewTests(APIBaseTest):
 
         super().setUp()
 
+    @snapshot_postgres_queries
     def test_get_feature_flag_success(self):
         url = f"/api/organizations/{self.organization.id}/feature_flags/{self.feature_flag_key}"
         response = self.client.get(url)
@@ -27,14 +28,14 @@ class OrganizationFeatureFlagViewTests(APIBaseTest):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         expected_data = [{"team_id": self.team_1.id, "active": True}, {"team_id": self.team_2.id, "active": True}]
-        self.assertCountEqual(response.data, expected_data)
+        self.assertCountEqual(response.json(), expected_data)
 
     def test_get_feature_flag_not_found(self):
         url = f"/api/organizations/{self.organization.id}/feature_flags/nonexistent-flag"
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, [])
+        self.assertEqual(response.json(), [])
 
     def test_get_feature_flag_unauthorized(self):
         self.client.logout()
