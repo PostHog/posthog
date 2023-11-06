645c0160131e497500f65d217e743ebffd25873d
David Newell <david@posthog.com>
2023-11-06 18:22:48 +0000

    fix PropertyDisplayFilter

--- frontend/src/lib/components/PropertyFilters/components/PropertyFiltersDisplay.tsx
+++ frontend/src/lib/components/PropertyFilters/components/PropertyFiltersDisplay.tsx
@@ -3,7 +3,7 @@ import { PropertyFilterButton } from './PropertyFilterButton'
 
 const PropertyFiltersDisplay = ({ filters }: { filters: AnyPropertyFilter[] }): JSX.Element => {
     return (
-        <div className="PropertyFilters m-0 flex-col">
+        <div className="PropertyFilters flex-wrap">
             {filters &&
                 filters.map((item) => {
                     return <PropertyFilterButton key={item.key} item={item} />
