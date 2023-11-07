7c7e34ec931663ec92fb2fd53b48672bc7f6d0b8
Tiina Turban <tiina303@gmail.com>
2023-11-07 12:21:35 +0100

    null=True back

--- posthog/migrations/0361_add_plugin_config_ui_fields.py
+++ posthog/migrations/0361_add_plugin_config_ui_fields.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="pluginconfig",
             name="deleted",
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(default=False, null=True),
         ),
         migrations.AddField(
             model_name="pluginconfig",
--- posthog/models/plugin.py
+++ posthog/models/plugin.py
@@ -241,7 +241,7 @@ class PluginConfig(models.Model):
     name: models.CharField = models.CharField(max_length=400, null=True, blank=True)
     description: models.CharField = models.CharField(max_length=1000, null=True, blank=True)
     # Used in the frontend to hide pluginConfgis that user deleted
-    deleted: models.BooleanField = models.BooleanField(default=False)
+    deleted: models.BooleanField = models.BooleanField(default=False, null=True)
 
 
 class PluginAttachment(models.Model):
