50c1912258b99c011dbcfebe4fabfa81f7117afc
eric <eeoneric@gmail.com>
2023-11-07 16:32:27 -0500

    add tests

---
+++ posthog/tasks/test/test_warehouse.py
@@ -0,0 +1,110 @@
+from posthog.test.base import APIBaseTest
+import datetime
+from unittest.mock import patch, MagicMock
+from posthog.tasks.warehouse import _traverse_jobs_by_field, calculate_workspace_rows_synced_by_team
+from freezegun import freeze_time
+
+
+class TestWarehouse(APIBaseTest):
+    @patch("posthog.tasks.warehouse.send_request")
+    @freeze_time("2023-11-07")
+    def test_traverse_jobs_by_field(self, send_request_mock):
+        send_request_mock.return_value = {
+            "data": [
+                {
+                    "jobId": 5827835,
+                    "status": "succeeded",
+                    "jobType": "sync",
+                    "startTime": "2023-11-07T16:50:49Z",
+                    "connectionId": "fake",
+                    "lastUpdatedAt": "2023-11-07T16:52:54Z",
+                    "duration": "PT2M5S",
+                    "rowsSynced": 93353,
+                },
+                {
+                    "jobId": 5783573,
+                    "status": "succeeded",
+                    "jobType": "sync",
+                    "startTime": "2023-11-05T18:32:41Z",
+                    "connectionId": "fake-2",
+                    "lastUpdatedAt": "2023-11-05T18:35:11Z",
+                    "duration": "PT2M30S",
+                    "rowsSynced": 97747,
+                },
+            ]
+        }
+        mock_capture = MagicMock()
+        response = _traverse_jobs_by_field(mock_capture, self.team, "fake-url", "rowsSynced")
+
+        self.assertEqual(
+            response,
+            [
+                {"count": 93353, "startTime": "2023-11-07T16:50:49Z"},
+                {"count": 97747, "startTime": "2023-11-05T18:32:41Z"},
+            ],
+        )
+
+        self.assertEqual(mock_capture.capture.call_count, 2)
+        mock_capture.capture.assert_called_with(
+            self.team.pk,
+            "external data sync job",
+            {"count": 97747, "team_id": self.team.pk, "team_uuid": self.team.uuid, "startTime": "2023-11-05T18:32:41Z"},
+        )
+
+    @patch("posthog.tasks.warehouse._traverse_jobs_by_field")
+    @freeze_time("2023-11-07")
+    def test_calculate_workspace_rows_synced_by_team(self, traverse_jobs_mock):
+        traverse_jobs_mock.return_value = [
+            {"count": 97747, "startTime": "2023-11-05T18:32:41Z"},
+            {"count": 93353, "startTime": "2023-11-07T16:50:49Z"},
+        ]
+
+        mock_capture = MagicMock()
+        calculate_workspace_rows_synced_by_team(mock_capture, self.team.pk)
+
+        self.team.refresh_from_db()
+        self.assertEqual(self.team.external_data_workspace_rows_synced_in_month, 191100)
+        self.assertEqual(
+            self.team.external_data_workspace_last_synced,
+            datetime.datetime(2023, 11, 7, 16, 50, 49, tzinfo=datetime.timezone.utc),
+        )
+
+    @patch("posthog.tasks.warehouse._traverse_jobs_by_field")
+    @freeze_time("2023-11-07")
+    def test_calculate_workspace_rows_synced_by_team_month_cutoff(self, traverse_jobs_mock):
+        # external_data_workspace_last_synced unset
+        traverse_jobs_mock.return_value = [
+            {"count": 97747, "startTime": "2023-10-30T18:32:41Z"},
+            {"count": 93353, "startTime": "2023-11-07T16:50:49Z"},
+        ]
+
+        mock_capture = MagicMock()
+        calculate_workspace_rows_synced_by_team(mock_capture, self.team.pk)
+
+        self.team.refresh_from_db()
+        self.assertEqual(self.team.external_data_workspace_rows_synced_in_month, 93353)
+        self.assertEqual(
+            self.team.external_data_workspace_last_synced,
+            datetime.datetime(2023, 11, 7, 16, 50, 49, tzinfo=datetime.timezone.utc),
+        )
+
+    @patch("posthog.tasks.warehouse._traverse_jobs_by_field")
+    @freeze_time("2023-11-07")
+    def test_calculate_workspace_rows_synced_by_team_month_cutoff_field_set(self, traverse_jobs_mock):
+        self.team.external_data_workspace_last_synced = datetime.datetime(
+            2023, 10, 29, 18, 32, 41, tzinfo=datetime.timezone.utc
+        )
+        traverse_jobs_mock.return_value = [
+            {"count": 97747, "startTime": "2023-10-30T18:32:41Z"},
+            {"count": 93353, "startTime": "2023-11-07T16:50:49Z"},
+        ]
+
+        mock_capture = MagicMock()
+        calculate_workspace_rows_synced_by_team(mock_capture, self.team.pk)
+
+        self.team.refresh_from_db()
+        self.assertEqual(self.team.external_data_workspace_rows_synced_in_month, 93353)
+        self.assertEqual(
+            self.team.external_data_workspace_last_synced,
+            datetime.datetime(2023, 11, 7, 16, 50, 49, tzinfo=datetime.timezone.utc),
+        )
--- posthog/tasks/warehouse.py
+++ posthog/tasks/warehouse.py
@@ -5,18 +5,22 @@ from urllib.parse import urlencode
 
 AIRBYTE_JOBS_URL = "https://api.airbyte.com/v1/jobs"
 
+DEFAULT_DATE_TIME = datetime.datetime(2023, 11, 7, tzinfo=datetime.timezone.utc)
+
 
 # TODO: split these into their own tasks
 def calculate_workspace_rows_synced_by_team(ph_client, team_id):
     team = Team.objects.get(pk=team_id)
-    begin = team.external_data_workspace_last_synced
-    end = datetime.datetime.now(datetime.timezone.utc)
+    now = datetime.datetime.now(datetime.timezone.utc)
+    begin = team.external_data_workspace_last_synced or now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+    end = now
 
     params = {
         "workspaceIds": team.external_data_workspace_id,
         "limit": 100,
         "offset": 0,
         "status": "succeeded",
+        "orderBy": "createdAt|ASC",
         "updatedAtStart": begin.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "updatedAtEnd": end.strftime("%Y-%m-%dT%H:%M:%SZ"),
     }
@@ -25,14 +29,20 @@ def calculate_workspace_rows_synced_by_team(ph_client, team_id):
     # reset accumulated to new period if the month has changed
     if end.month != begin.month:
         total = sum([result["count"] for result in result_totals if result["startTime"].month == end.month])
-    elif team.external_data_workspace_last_synced.month != begin.month:
+    elif team.external_data_workspace_last_synced and team.external_data_workspace_last_synced.month != begin.month:
         total = sum([result["count"] for result in result_totals])
     else:
         total = (
             team.external_data_workspace_rows_synced_in_month
             if team.external_data_workspace_rows_synced_in_month is not None
             else 0
-        ) + sum([result["count"] for result in result_totals if result["startTime"].month == end.month])
+        ) + sum(
+            [
+                result["count"]
+                for result in result_totals
+                if datetime.datetime.strptime(result["startTime"], "%Y-%m-%dT%H:%M:%SZ").month == end.month
+            ]
+        )
 
     team = Team.objects.get(pk=team_id)
     team.external_data_workspace_last_synced = result_totals[-1]["startTime"] if result_totals else end
