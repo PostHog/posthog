49d8fdf5371a5a70ff67751540a6902f9a2cd36a
Ben White <ben@posthog.com>
2023-11-08 17:23:01 +0100

    fix: Persons search loop (#18490)

--- frontend/src/scenes/persons-management/tabs/personsSceneLogic.ts
+++ frontend/src/scenes/persons-management/tabs/personsSceneLogic.ts
@@ -1,11 +1,6 @@
 import { actions, connect, kea, path, reducers, selectors } from 'kea'
 
-import { actionToUrl, urlToAction } from 'kea-router'
-import equal from 'fast-deep-equal'
 import { DataTableNode, Node, NodeKind } from '~/queries/schema'
-import { urls } from 'scenes/urls'
-import { objectsEqual } from 'lib/utils'
-import { lemonToast } from 'lib/lemon-ui/lemonToast'
 
 import type { personsSceneLogicType } from './personsSceneLogicType'
 import { defaultDataTableColumns } from '~/queries/nodes/DataTable/utils'
@@ -39,34 +34,36 @@ export const personsSceneLogic = kea<personsSceneLogicType>([
         ],
     })),
 
-    actionToUrl(({ values }) => ({
-        setQuery: () => [
-            urls.persons(),
-            {},
-            objectsEqual(values.query, getDefaultQuery(values.queryFlagEnabled)) ? {} : { q: values.query },
-            { replace: true },
-        ],
-    })),
+    // NOTE: Temp disabled as it triggers a loop bug
 
-    urlToAction(({ actions, values }) => ({
-        [urls.persons()]: (_, __, { q: queryParam }): void => {
-            if (!equal(queryParam, values.query)) {
-                // nothing in the URL
-                if (!queryParam) {
-                    const defaultQuery = getDefaultQuery(values.queryFlagEnabled)
-                    // set the default unless it's already there
-                    if (!objectsEqual(values.query, defaultQuery)) {
-                        actions.setQuery(defaultQuery)
-                    }
-                } else {
-                    if (typeof queryParam === 'object') {
-                        actions.setQuery(queryParam)
-                    } else {
-                        lemonToast.error('Invalid query in URL')
-                        console.error({ queryParam })
-                    }
-                }
-            }
-        },
-    })),
+    // actionToUrl(({ values }) => ({
+    //     setQuery: () => [
+    //         urls.persons(),
+    //         {},
+    //         objectsEqual(values.query, getDefaultQuery(values.queryFlagEnabled)) ? {} : { q: values.query },
+    //         { replace: true },
+    //     ],
+    // })),
+
+    // urlToAction(({ actions, values }) => ({
+    //     [urls.persons()]: (_, __, { q: queryParam }): void => {
+    //         if (!equal(queryParam, values.query)) {
+    //             // nothing in the URL
+    //             if (!queryParam) {
+    //                 const defaultQuery = getDefaultQuery(values.queryFlagEnabled)
+    //                 // set the default unless it's already there
+    //                 if (!objectsEqual(values.query, defaultQuery)) {
+    //                     actions.setQuery(defaultQuery)
+    //                 }
+    //             } else {
+    //                 if (typeof queryParam === 'object') {
+    //                     actions.setQuery(queryParam)
+    //                 } else {
+    //                     lemonToast.error('Invalid query in URL')
+    //                     console.error({ queryParam })
+    //                 }
+    //             }
+    //         }
+    //     },
+    // })),
 ])
