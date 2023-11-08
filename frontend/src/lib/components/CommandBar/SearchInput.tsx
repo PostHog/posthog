565bd8e0a5a029444b498bb24a703f04ff3dba81
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-08 14:04:38 +0000

    bem class names

--- frontend/src/lib/components/CommandBar/SearchInput.tsx
+++ frontend/src/lib/components/CommandBar/SearchInput.tsx
@@ -14,7 +14,7 @@ const SearchInput = (): JSX.Element => {
             <LemonInput
                 type="search"
                 size="small"
-                className="command-bar__search-input"
+                className="CommandBar__search-input"
                 fullWidth
                 suffix={<KeyboardShortcut escape muted />}
                 autoFocus
