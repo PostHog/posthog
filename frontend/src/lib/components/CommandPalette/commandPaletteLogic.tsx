6299368ed29e2cf0e03fd97073bee17525d6a4f1
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-10 16:52:58 +0100

    hide dashboard search from commands

--- frontend/src/lib/components/CommandPalette/commandPaletteLogic.tsx
+++ frontend/src/lib/components/CommandPalette/commandPaletteLogic.tsx
@@ -46,6 +46,8 @@ import {
     IconTrendingUp,
 } from 'lib/lemon-ui/icons'
 import { ProfilePicture } from 'lib/lemon-ui/ProfilePicture'
+import { FEATURE_FLAGS } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
 // If CommandExecutor returns CommandFlow, flow will be entered
 export type CommandExecutor = () => CommandFlow | void
@@ -116,7 +118,7 @@ export const commandPaletteLogic = kea<commandPaletteLogicType>([
     path(['lib', 'components', 'CommandPalette', 'commandPaletteLogic']),
     connect({
         actions: [personalAPIKeysLogic, ['createKey'], router, ['push']],
-        values: [teamLogic, ['currentTeam'], userLogic, ['user']],
+        values: [teamLogic, ['currentTeam'], userLogic, ['user'], featureFlagLogic, ['featureFlags']],
         logic: [preflightLogic],
     }),
     actions({
@@ -198,6 +200,10 @@ export const commandPaletteLogic = kea<commandPaletteLogicType>([
         ],
     }),
     selectors({
+        isUsingCmdKSearch: [
+            (selectors) => [selectors.featureFlags],
+            (featureFlags) => featureFlags[FEATURE_FLAGS.CMD_K_SEARCH],
+        ],
         isSqueak: [
             (selectors) => [selectors.input],
             (input: string) => {
@@ -213,25 +219,37 @@ export const commandPaletteLogic = kea<commandPaletteLogicType>([
         commandRegistrations: [
             (selectors) => [
                 selectors.rawCommandRegistrations,
+                selectors.isUsingCmdKSearch,
                 dashboardsModel.selectors.nameSortedDashboards,
                 teamLogic.selectors.currentTeam,
             ],
-            (rawCommandRegistrations: CommandRegistrations, dashboards: DashboardType[]): CommandRegistrations => ({
-                ...rawCommandRegistrations,
-                custom_dashboards: {
-                    key: 'custom_dashboards',
-                    resolver: dashboards.map((dashboard: DashboardType) => ({
-                        key: `dashboard_${dashboard.id}`,
-                        icon: IconTableChart,
-                        display: `Go to dashboard: ${dashboard.name}`,
-                        executor: () => {
-                            const { push } = router.actions
-                            push(urls.dashboard(dashboard.id))
-                        },
-                    })),
-                    scope: GLOBAL_COMMAND_SCOPE,
-                },
-            }),
+            (
+                rawCommandRegistrations: CommandRegistrations,
+                isUsingCmdKSearch,
+                dashboards: DashboardType[]
+            ): CommandRegistrations => {
+                if (isUsingCmdKSearch) {
+                    // do not add dashboards to commands, as they can be navigated to via search
+                    return rawCommandRegistrations
+                }
+
+                return {
+                    ...rawCommandRegistrations,
+                    custom_dashboards: {
+                        key: 'custom_dashboards',
+                        resolver: dashboards.map((dashboard: DashboardType) => ({
+                            key: `dashboard_${dashboard.id}`,
+                            icon: IconTableChart,
+                            display: `Go to dashboard: ${dashboard.name}`,
+                            executor: () => {
+                                const { push } = router.actions
+                                push(urls.dashboard(dashboard.id))
+                            },
+                        })),
+                        scope: GLOBAL_COMMAND_SCOPE,
+                    },
+                }
+            },
         ],
         regexpCommandPairs: [
             (selectors) => [selectors.commandRegistrations],
