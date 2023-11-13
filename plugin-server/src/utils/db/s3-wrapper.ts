cb59f9b840bb16bfd7d37b35da44e3259e926019
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-13 18:29:48 +0000

    chore: run codemod on s3-wrapper.ts

--- plugin-server/src/utils/db/s3-wrapper.ts
+++ plugin-server/src/utils/db/s3-wrapper.ts
@@ -1,25 +1,35 @@
-import { S3 } from 'aws-sdk'
+import {
+    DeleteObjectCommandInput,
+    DeleteObjectCommandOutput,
+    GetObjectCommandInput,
+    GetObjectCommandOutput,
+    ListObjectsV2CommandInput,
+    ListObjectsV2CommandOutput,
+    PutObjectCommandInput,
+    S3,
+    S3ClientConfig,
+} from '@aws-sdk/client-s3';
 
 export class S3Wrapper {
     s3: S3
 
-    constructor(options: S3.Types.ClientConfiguration) {
+    constructor(options: S3ClientConfig) {
         this.s3 = new S3(options)
     }
 
-    async upload(params: S3.Types.PutObjectRequest): Promise<S3.ManagedUpload.SendData> {
+    async upload(params: PutObjectCommandInput): Promise<S3.ManagedUpload.SendData> {
         return this.s3.upload(params).promise()
     }
 
-    async getObject(params: S3.GetObjectRequest): Promise<S3.Types.GetObjectOutput> {
+    async getObject(params: GetObjectCommandInput): Promise<GetObjectCommandOutput> {
         return this.s3.getObject(params).promise()
     }
 
-    async deleteObject(params: S3.DeleteObjectRequest): Promise<S3.Types.DeleteObjectOutput> {
+    async deleteObject(params: DeleteObjectCommandInput): Promise<DeleteObjectCommandOutput> {
         return this.s3.deleteObject(params).promise()
     }
 
-    async listObjectsV2(params: S3.ListObjectsV2Request): Promise<S3.Types.ListObjectsV2Output> {
+    async listObjectsV2(params: ListObjectsV2CommandInput): Promise<ListObjectsV2CommandOutput> {
         return this.s3.listObjectsV2(params).promise()
     }
 }
