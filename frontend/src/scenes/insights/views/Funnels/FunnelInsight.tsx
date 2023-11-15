470be00508f205dc58fab109fc9c55ea22d2bba9
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-15 12:46:42 +0100

    fix(notebooks): move funnel series and exclusion selectors (#18613)

--- frontend/src/scenes/insights/views/Funnels/FunnelInsight.tsx
+++ frontend/src/scenes/insights/views/Funnels/FunnelInsight.tsx
@@ -3,10 +3,12 @@ import { useValues } from 'kea'
 import { Funnel } from 'scenes/funnels/Funnel'
 import { insightLogic } from 'scenes/insights/insightLogic'
 import { funnelDataLogic } from 'scenes/funnels/funnelDataLogic'
+import { insightVizDataLogic } from 'scenes/insights/insightVizDataLogic'
 
 export function FunnelInsight(): JSX.Element {
     const { insightLoading, insightProps } = useValues(insightLogic)
-    const { isFunnelWithEnoughSteps, hasFunnelResults } = useValues(funnelDataLogic(insightProps))
+    const { isFunnelWithEnoughSteps } = useValues(insightVizDataLogic(insightProps))
+    const { hasFunnelResults } = useValues(funnelDataLogic(insightProps))
 
     const nonEmptyState = (hasFunnelResults && isFunnelWithEnoughSteps) || insightLoading
 
