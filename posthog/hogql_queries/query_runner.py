0f05400da99786a480ef5be285dc7e44e53f7c98
Marius Andra <marius.andra@gmail.com>
2023-11-16 15:27:42 +0100

    also via the get_query_runner method

--- posthog/hogql_queries/query_runner.py
+++ posthog/hogql_queries/query_runner.py
@@ -87,6 +87,7 @@ def get_query_runner(
     team: Team,
     timings: Optional[HogQLTimings] = None,
     in_export_context: Optional[bool] = False,
+    modifiers: Optional[HogQLQueryModifiers] = None,
 ) -> "QueryRunner":
     kind = None
     if isinstance(query, dict):
@@ -104,6 +105,7 @@ def get_query_runner(
             team=team,
             timings=timings,
             in_export_context=in_export_context,
+            modifiers=modifiers,
         )
     if kind == "TrendsQuery":
         from .insights.trends.trends_query_runner import TrendsQueryRunner
@@ -113,6 +115,7 @@ def get_query_runner(
             team=team,
             timings=timings,
             in_export_context=in_export_context,
+            modifiers=modifiers,
         )
     if kind == "EventsQuery":
         from .events_query_runner import EventsQueryRunner
@@ -122,6 +125,7 @@ def get_query_runner(
             team=team,
             timings=timings,
             in_export_context=in_export_context,
+            modifiers=modifiers,
         )
     if kind == "PersonsQuery":
         from .persons_query_runner import PersonsQueryRunner
@@ -131,6 +135,7 @@ def get_query_runner(
             team=team,
             timings=timings,
             in_export_context=in_export_context,
+            modifiers=modifiers,
         )
     if kind == "InsightPersonsQuery":
         from .insights.insight_persons_query_runner import InsightPersonsQueryRunner
@@ -140,6 +145,7 @@ def get_query_runner(
             team=team,
             timings=timings,
             in_export_context=in_export_context,
+            modifiers=modifiers,
         )
     if kind == "HogQLQuery":
         from .hogql_query_runner import HogQLQueryRunner
@@ -149,6 +155,7 @@ def get_query_runner(
             team=team,
             timings=timings,
             in_export_context=in_export_context,
+            modifiers=modifiers,
         )
     if kind == "SessionsTimelineQuery":
         from .sessions_timeline_query_runner import SessionsTimelineQueryRunner
@@ -157,19 +164,20 @@ def get_query_runner(
             query=cast(SessionsTimelineQuery | Dict[str, Any], query),
             team=team,
             timings=timings,
+            modifiers=modifiers,
         )
     if kind == "WebOverviewQuery":
         from .web_analytics.web_overview import WebOverviewQueryRunner
 
-        return WebOverviewQueryRunner(query=query, team=team, timings=timings)
+        return WebOverviewQueryRunner(query=query, team=team, timings=timings, modifiers=modifiers)
     if kind == "WebTopClicksQuery":
         from .web_analytics.top_clicks import WebTopClicksQueryRunner
 
-        return WebTopClicksQueryRunner(query=query, team=team, timings=timings)
+        return WebTopClicksQueryRunner(query=query, team=team, timings=timings, modifiers=modifiers)
     if kind == "WebStatsTableQuery":
         from .web_analytics.stats_table import WebStatsTableQueryRunner
 
-        return WebStatsTableQueryRunner(query=query, team=team, timings=timings)
+        return WebStatsTableQueryRunner(query=query, team=team, timings=timings, modifiers=modifiers)
 
     raise ValueError(f"Can't get a runner for an unknown query kind: {kind}")
 
