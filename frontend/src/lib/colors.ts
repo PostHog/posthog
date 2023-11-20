7bcedaae3d75d9b79460f72225a3f447a5eda000
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-20 15:02:22 +0100

    feat(3000): add data colors

--- frontend/src/lib/colors.ts
+++ frontend/src/lib/colors.ts
@@ -5,20 +5,21 @@ export const BRAND_BLUE_HSL: [number, number, number] = [228, 100, 56]
 
 /* Insight series colors. */
 const dataColorVars = [
-    'brand-blue',
-    'purple',
-    'viridian',
-    'magenta',
-    'vermilion',
-    'brown',
-    'green',
-    'blue',
-    'pink',
-    'navy',
-    'turquoise',
-    'brick',
-    'yellow',
-    'lilac',
+    'color-1',
+    'color-2',
+    'color-3',
+    'color-4',
+    'color-5',
+    'color-6',
+    'color-7',
+    'color-8',
+    'color-9',
+    'color-10',
+    'color-11',
+    'color-12',
+    'color-13',
+    'color-14',
+    'color-15',
 ]
 
 export const tagColors = [
