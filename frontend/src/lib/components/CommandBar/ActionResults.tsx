840bc85c569395d223f0330e8ad7c8420def9a50
Michael Matloka <michal@matloka.com>
2023-11-20 17:29:51 +0100

    Fix hard-coded light mode color

--- frontend/src/lib/components/CommandBar/ActionResults.tsx
+++ frontend/src/lib/components/CommandBar/ActionResults.tsx
@@ -15,7 +15,7 @@ type ResultsGroupProps = {
 const ResultsGroup = ({ scope, results, activeResultIndex }: ResultsGroupProps): JSX.Element => {
     return (
         <>
-            <div className="border-b pl-3 pr-2 pt-1 pb-1 bg-bg-3000-light text-xs font-bold text-muted-3000 uppercase">
+            <div className="border-b pl-3 pr-2 pt-1 pb-1 bg-bg-3000 text-xs font-bold text-muted-3000 uppercase">
                 {getNameFromActionScope(scope)}
             </div>
             {results.map((result) => (
