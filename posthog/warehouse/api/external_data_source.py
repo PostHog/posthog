4ae99596e2fbc74245d6424d42050ea339f650f1
eric <eeoneric@gmail.com>
2023-11-19 22:45:02 -0500

    temp

--- posthog/warehouse/api/external_data_source.py
+++ posthog/warehouse/api/external_data_source.py
@@ -5,7 +5,7 @@ from posthog.permissions import OrganizationMemberPermissions
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters, serializers, viewsets
-from posthog.warehouse.models import ExternalDataSource
+from posthog.warehouse.models import ExternalDataSource, ExternalDataJob
 from posthog.warehouse.external_data_source.source import delete_source
 from posthog.warehouse.external_data_source.destination import delete_destination
 from posthog.warehouse.sync_resource import sync_resource
@@ -26,11 +26,20 @@ logger = structlog.get_logger(__name__)
 class ExternalDataSourceSerializers(serializers.ModelSerializer):
     account_id = serializers.CharField(write_only=True)
     client_secret = serializers.CharField(write_only=True)
+    status = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = ExternalDataSource
         fields = ["id", "source_id", "created_at", "created_by", "status", "client_secret", "account_id", "source_type"]
         read_only_fields = ["id", "source_id", "created_by", "created_at", "status", "source_type"]
+
+    # TODO: temporary just to test
+    def get_status(self, instance: ExternalDataSource) -> str:
+        job = ExternalDataJob.objects.filter(pipeline_id=instance.id).order_by("-created_at").first()
+        if job:
+            return job.status
+
+        return instance.status
 
 
 class ExternalDataSourceViewSet(StructuredViewSetMixin, viewsets.ModelViewSet):
