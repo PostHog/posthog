f34d89ca1b14d525d18369c6d0f3aa9c71111823
Neil Kakkar <neilkakkar@gmail.com>
2023-11-21 13:44:16 +0000

    fix(flags): Alter constraints to not delete flags and experiments (#18514)

--- posthog/api/test/test_organization_feature_flag.py
+++ posthog/api/test/test_organization_feature_flag.py
@@ -53,7 +53,7 @@ class TestOrganizationFeatureFlagGet(APIBaseTest, QueryMatchingTest):
                     "email": self.user.email,
                     "is_email_verified": self.user.is_email_verified,
                 },
-                "filters": flag.filters,
+                "filters": flag.get_filters(),
                 "created_at": flag.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f") + "Z",
                 "active": flag.active,
             }
@@ -242,6 +242,29 @@ class TestOrganizationFeatureFlagCopy(APIBaseTest, QueryMatchingTest):
             set(expected_flag_response.keys()),
             set(flag_response.keys()),
         )
+
+    def test_copy_feature_flag_with_old_legacy_flags(self):
+        url = f"/api/organizations/{self.organization.id}/feature_flags/copy_flags"
+        target_project = self.team_2
+
+        flag_to_copy = FeatureFlag.objects.create(
+            team=self.team_1,
+            created_by=self.user,
+            key="flag-to-copy-here",
+            filters={},
+            rollout_percentage=self.rollout_percentage_to_copy,
+        )
+
+        data = {
+            "feature_flag_key": flag_to_copy.key,
+            "from_project": self.feature_flag_to_copy.team_id,
+            "target_project_ids": [target_project.id],
+        }
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()["success"]), 1)
+        self.assertEqual(len(response.json()["failed"]), 0)
 
     def test_copy_feature_flag_update_override_deleted(self):
         target_project = self.team_2
