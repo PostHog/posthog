57312d07a59f3a2cbbffc9d186ef1a8540d21e38
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-22 11:03:02 +0100

    feat(command-bar): add shortcut help

---
+++ frontend/src/lib/components/CommandBar/shortcutsLogic.ts
@@ -0,0 +1,24 @@
+import { kea, path, connect, afterMount, beforeUnmount } from 'kea'
+
+import { commandBarLogic } from './commandBarLogic'
+
+export const shortcutsLogic = kea([
+    path(['lib', 'components', 'CommandBar', 'shortcutsLogic']),
+    connect({
+        actions: [commandBarLogic, ['hideCommandBar']],
+    }),
+    afterMount(({ actions, cache }) => {
+        // register keyboard shortcuts
+        cache.onKeyDown = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                // hide command bar
+                actions.hideCommandBar()
+            }
+        }
+        window.addEventListener('keydown', cache.onKeyDown)
+    }),
+    beforeUnmount(({ cache }) => {
+        // unregister keyboard shortcuts
+        window.removeEventListener('keydown', cache.onKeyDown)
+    }),
+])
