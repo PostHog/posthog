20cdc11c74c48f3bb6d250b35b9b245947861fce
Michael Matloka <michal@matloka.com>
2023-11-22 10:42:40 +0100

    Refactor `> span` to `.LemonButton__chrome`

--- frontend/src/lib/lemon-ui/LemonButton/LemonButton.tsx
+++ frontend/src/lib/lemon-ui/LemonButton/LemonButton.tsx
@@ -188,7 +188,7 @@ export const LemonButton: React.FunctionComponent<LemonButtonProps & React.RefAt
                     {...linkDependentProps}
                     {...buttonProps}
                 >
-                    <span>
+                    <span className="LemonButton__chrome">
                         {icon ? <span className="LemonButton__icon">{icon}</span> : null}
                         {children ? <span className="LemonButton__content">{children}</span> : null}
                         {sideIcon ? <span className="LemonButton__icon">{sideIcon}</span> : null}
