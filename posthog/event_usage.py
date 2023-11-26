f9c44cfe107084f5d4f89a089077f8ef7bd99c18
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-11-25 20:15:30 -0800

    also set whenever user is updated since that includes updating current org

--- posthog/event_usage.py
+++ posthog/event_usage.py
@@ -116,7 +116,7 @@ def report_user_updated(user: User, updated_attrs: List[str]) -> None:
     posthoganalytics.capture(
         user.distinct_id,
         "user updated",
-        properties={"updated_attrs": updated_attrs},
+        properties={"updated_attrs": updated_attrs, "$set": user.get_analytics_metadata()},
         groups=groups(user.current_organization, user.current_team),
     )
 
