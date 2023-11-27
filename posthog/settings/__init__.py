30cbe7cd921ca16321cdde87a61f33ba21447a82
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-27 21:42:03 +0100

    feat(hogql): run filter based insights via hogql (undo revert) (#17645)

--- posthog/settings/__init__.py
+++ posthog/settings/__init__.py
@@ -93,6 +93,9 @@ PERSON_ON_EVENTS_OVERRIDE = get_from_env("PERSON_ON_EVENTS_OVERRIDE", optional=T
 # Only written in specific scripts - do not use outside of them.
 PERSON_ON_EVENTS_V2_OVERRIDE = get_from_env("PERSON_ON_EVENTS_V2_OVERRIDE", optional=True, type_cast=str_to_bool)
 
+# Wether to use insight queries converted to HogQL.
+HOGQL_INSIGHTS_OVERRIDE = get_from_env("HOGQL_INSIGHTS_OVERRIDE", optional=True, type_cast=str_to_bool)
+
 HOOK_EVENTS: Dict[str, str] = {}
 
 # Support creating multiple organizations in a single instance. Requires a premium license.
