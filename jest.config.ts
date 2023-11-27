15c2855179f54445ad5967f6d396c7c4994541ae
Paul D'Ambra <paul@posthog.com>
2023-11-27 11:07:30 +0000

    fix

--- jest.config.ts
+++ jest.config.ts
@@ -133,9 +133,7 @@ const config: Config = {
     modulePaths: ['<rootDir>/'],
 
     // A list of paths to directories that Jest should use to search for files in
-    // roots: [
-    //   "<rootDir>"
-    // ],
+    roots: ['<rootDir>', '../../ee/frontend'],
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
