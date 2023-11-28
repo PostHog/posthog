16f875573f4c1dd4a177d0a11d91340e3121bdce
Paul D'Ambra <paul@posthog.com>
2023-11-28 16:47:29 +0000

    Update jest.config.ts
    Co-authored-by: David Newell <d.newell1@outlook.com>

--- jest.config.ts
+++ jest.config.ts
@@ -138,7 +138,6 @@ const config: Config = {
     // restoreMocks: false,
 
     // The root directory that Jest should scan for tests and modules within
-    // rootDir: 'frontend/src',
     modulePaths: ['<rootDir>/'],
 
     // A list of paths to directories that Jest should use to search for files in
