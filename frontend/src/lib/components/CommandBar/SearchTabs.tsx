d77b095c0f5af23c549ac245c9be1bb686bb1e86
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-11-28 10:43:12 +0100

    always show tabs

--- frontend/src/lib/components/CommandBar/SearchTabs.tsx
+++ frontend/src/lib/components/CommandBar/SearchTabs.tsx
@@ -1,26 +1,16 @@
-import { useValues } from 'kea'
 import { RefObject } from 'react'
 
 import { Tab } from './constants'
-import { searchBarLogic } from './searchBarLogic'
 import { SearchBarTab } from './SearchBarTab'
 
 type SearchTabsProps = {
     inputRef: RefObject<HTMLInputElement>
 }
 
-export const SearchTabs = ({ inputRef }: SearchTabsProps): JSX.Element | null => {
-    const { combinedSearchResults } = useValues(searchBarLogic)
-
-    if (!combinedSearchResults) {
-        return null
-    }
-
-    return (
-        <div className="flex items-center border-t shrink-0 overflow-x-auto bg-bg-light">
-            {Object.values(Tab).map((tab) => (
-                <SearchBarTab key={tab} tab={tab} inputRef={inputRef} />
-            ))}
-        </div>
-    )
-}
+export const SearchTabs = ({ inputRef }: SearchTabsProps): JSX.Element | null => (
+    <div className="flex items-center border-t shrink-0 overflow-x-auto bg-bg-light">
+        {Object.values(Tab).map((tab) => (
+            <SearchBarTab key={tab} tab={tab} inputRef={inputRef} />
+        ))}
+    </div>
+)
