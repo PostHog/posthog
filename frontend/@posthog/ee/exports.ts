57e01f983dbbb098aab2bb0a7c85ce86442c000a
Paul D'Ambra <paul@posthog.com>
2023-11-29 12:02:13 +0000

    fix: try different import mechanism

--- frontend/@posthog/ee/exports.ts
+++ frontend/@posthog/ee/exports.ts
@@ -1,14 +1,19 @@
 import { PostHogEE } from './types'
 
 export default async (): Promise<PostHogEE> => {
-    // eslint-disable-next-line import/no-restricted-paths
-    return import('../../../ee/frontend/exports')
-        .then((ee) => {
-            return ee.default()
-        })
-        .catch(() => {
-            return {
-                enabled: false,
-            }
-        })
+    // this is slightly ridiculous, as far as I can tell esbuild doesn't support dynamic imports
+    // unless you pass string concatenations to it, so we have to do this
+    // the `wat` variable - because it is preceded by a slash
+    // is expanded to \**\*
+    // so we try to import a file *exports.ts from the ee\frontend folder
+    // instead of simply importing the file using a static string
+    // I basically can't believe this
+    const wat = 'exports'
+    try {
+        return (await import(`../../../ee/frontend/${wat}exports.ts`)).default()
+    } catch (e) {
+        return {
+            enabled: false,
+        }
+    }
 }
