11b96d95fbf5a028fd2f7e63c1dd692c7b9126ea
Michael Matloka <michal@matloka.com>
2023-11-29 17:19:17 +0100

    Rework the welcome copy and tune styles

--- frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelWelcome.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelWelcome.tsx
@@ -1,5 +1,5 @@
-import { IconCursorClick, IconGear, IconHome, IconInfo, IconNight, IconNotebook, IconSearch } from '@posthog/icons'
-import { LemonButton, Link } from '@posthog/lemon-ui'
+import { IconCursorClick, IconHome, IconInfo, IconNight, IconNotebook, IconSearch } from '@posthog/icons'
+import { LemonButton } from '@posthog/lemon-ui'
 import { useActions } from 'kea'
 import posthog from 'posthog-js'
 import { useEffect } from 'react'
@@ -8,6 +8,22 @@ import { KeyboardShortcut } from '../../components/KeyboardShortcut'
 import { sidePanelStateLogic } from '../sidePanelStateLogic'
 
 const blogPostUrl = 'https://posthog.com/blog/why-redesign'
+
+type WelcomeBannerProps = {
+    icon: React.ReactNode
+    title: string
+    description: string | JSX.Element
+}
+
+const WelcomeBanner = ({ icon, title, description }: WelcomeBannerProps): JSX.Element => (
+    <li className="bg-bg-3000 border rounded-md px-4 py-3 flex gap-4 items-center">
+        <div className="flex shrink-0 text-2xl">{icon}</div>
+        <div className="flex-1">
+            <h3 className="mb-0 font-semibold">{title}</h3>
+            <span>{description}</span>
+        </div>
+    </li>
+)
 
 export const SidePanelWelcome = (): JSX.Element => {
     const { closeSidePanel } = useActions(sidePanelStateLogic)
@@ -24,87 +40,67 @@ export const SidePanelWelcome = (): JSX.Element => {
     }, [])
 
     return (
-        <div className="flex flex-col items-center justify-center m-4 my-10 flex-1">
-            <h1 className="font-bold text-4xl text-center">
+        <div className="flex flex-col items-center justify-center m-4 my-6 flex-1">
+            <h1 className="font-bold text-4xl text-center mb-4">
                 Welcome to <span className="text-primary-3000">PostHog&nbsp;3000</span>
             </h1>
-
-            <p className="max-w-120 text-center">
-                <b>PostHog 3000</b> is the codename for our revamped user experience. You can{' '}
-                <Link to={blogPostUrl}>read more about it here</Link> but the headline features you will notice are:
+            <p className="max-w-120 text-center text-sm font-medium mb-6">
+                We're past zero to one.
+                <br />
+                In this new version of PostHog, we're going from one to 3000.
             </p>
-            <ul className="space-y-4">
-                <li className="bg-bg-3000 border rounded p-4 flex gap-4 items-center">
-                    <IconNight className="text-3xl" />
-                    <div className="flex-1">
-                        <h3 className="mb-0 font-semibold">Colors, data density, dark mode</h3>
-                        <span>
-                            A general refresh of our look and feel for enhanced visibility with improved data density
-                            and a dark mode option
-                        </span>
-                    </div>
-                </li>
-                <li className="bg-bg-3000 border rounded p-4 flex gap-4 items-center">
-                    <IconHome className="text-3xl" />
-                    <div className="flex-1">
-                        <h3 className="mb-0 font-semibold">New navigation menu </h3>
-                        <span>
-                            Organized into two main sections: 'project & data' and 'products', collapsible for even more
-                            space
-                        </span>
-                    </div>
-                </li>
-                <li className="bg-bg-3000 border rounded p-4 flex gap-4 items-center">
-                    <IconInfo className="text-3xl" />
-                    <div className="flex-1">
-                        <h3 className="mb-0 font-semibold">Side panel</h3>
-                        <span>
-                            A collapsible side panel for easy access to notebooks, in-app documentation, and support
-                        </span>
-                    </div>
-                </li>
-                <li className="bg-bg-3000 border rounded p-4 flex gap-4 items-center">
-                    <IconNotebook className="text-3xl" />
-                    <div className="flex-1">
-                        <h3 className="mb-0 font-semibold">Notebooks</h3>
-                        <span>
-                            A feature for aggregating multiple insights, replays, persons or anything else on a single
-                            page, aiding in data exploration and team collaboration
-                        </span>
-                    </div>
-                </li>
-                <li className="bg-bg-3000 border rounded p-4 flex gap-4 items-center">
-                    <IconCursorClick className="text-3xl" />
-                    <div className="flex-1">
-                        <h3 className="mb-0 font-semibold">Toolbar 3000</h3>
-                        <span>An updated toolbar with a modern design and dark mode compatibility</span>
-                    </div>
-                </li>
-                <li className="bg-bg-3000 border rounded p-4 flex gap-4 items-center">
-                    <IconSearch className="text-3xl" />
-                    <div className="flex-1">
-                        <h3 className="mb-0 font-semibold">Search + commands bar</h3>
-                        <span>
-                            A unified feature enabling navigation to most of PostHog's built-in features using keyboard
-                            shortcuts - try it with <KeyboardShortcut shift k /> for the search and{' '}
-                            <KeyboardShortcut command shift k /> for the command bar
-                        </span>
-                    </div>
-                </li>
-                <li className="bg-bg-3000 border rounded p-4 flex gap-4 items-center">
-                    <IconGear className="text-3xl" />
-                    <div className="flex-1">
-                        <h3 className="mb-0 font-semibold">Reorganized settings</h3>
-                        <span>
-                            An improved organization of the project settings page for easier access and management
-                        </span>
-                    </div>
-                </li>
+            <ul className="space-y-2">
+                <WelcomeBanner
+                    icon={<IconNight />}
+                    title="Light and dark mode"
+                    description="We've brought the dark mode you know from our website into&nbsp;the&nbsp;app. At&nbsp;the same&nbsp;time, light mode finally follows our&nbsp;brand&nbsp;style."
+                />
+                <WelcomeBanner
+                    icon={<IconHome />}
+                    title="Revamped navigation"
+                    description={
+                        <>
+                            The navbar is now divided into two sections: <strong>Project&nbsp;&&nbsp;data</strong>{' '}
+                            and&nbsp;<strong>Products</strong>. The&nbsp;new top bar&nbsp;is sticky, so that the key
+                            actions and info stay within reach.
+                        </>
+                    }
+                />
+                <WelcomeBanner
+                    icon={<IconNotebook />}
+                    title="Notebooks"
+                    description="Explore ideas more easily than ever with PostHog notebooks. Analyze&nbsp;your data from different angles and then share findings with&nbsp;the&nbsp;team."
+                />
+                <WelcomeBanner
+                    icon={<IconSearch />}
+                    title="Search and command bar"
+                    description={
+                        <>
+                            Search for anything in PostHog with <KeyboardShortcut command k />. Get&nbsp;things done
+                            faster using the command bar under <KeyboardShortcut command shift k />.
+                        </>
+                    }
+                />
+                <WelcomeBanner
+                    icon={<IconInfo />}
+                    title="Side panel"
+                    description="You're looking at it now. Access notebooks, support, or in-app docs while staying in&nbsp;the&nbsp;flow."
+                />
+                <WelcomeBanner
+                    icon={<IconCursorClick />}
+                    title="Toolbar 3000"
+                    description={
+                        <>
+                            We've applied the same thinking as above to the Toolbar. It's now easier to use,
+                            cleaner-looking, and finally something you can call a <i>bar</i>.
+                        </>
+                    }
+                />
             </ul>
 
-            <div className="flex items-center gap-2 mt-8">
+            <div className="flex items-center gap-2 mt-4">
                 <LemonButton to={blogPostUrl} size="large" type="secondary">
-                    Tell me more
+                    Read the blog post
                 </LemonButton>
                 <LemonButton size="large" type="primary" onClick={() => closeSidePanel()}>
                     Get started
