5f09c4f5601ff5f2e4c4b3ab2095315b9e2eaa1e
Paul D'Ambra <paul@posthog.com>
2023-11-29 12:13:22 +0000

    but without concatenation

--- frontend/@posthog/ee/exports.ts
+++ frontend/@posthog/ee/exports.ts
@@ -1,16 +1,10 @@
 import { PostHogEE } from './types'
 
 export default async (): Promise<PostHogEE> => {
-    // this is slightly ridiculous, as far as I can tell esbuild doesn't support dynamic imports
-    // unless you pass string concatenations to it, so we have to do this
-    // the `wat` variable - because it is preceded by a slash
-    // is expanded to \**\*
-    // so we try to import a file *exports.ts from the ee\frontend folder
-    // instead of simply importing the file using a static string
-    // I basically can't believe this
-    const wat = 'exports'
     try {
-        return (await import(`../../../ee/frontend/${wat}exports.ts`)).default()
+        // this has to import it...
+        // eslint-disable-next-line import/no-restricted-paths
+        return (await import('../../../ee/frontend/exports')).default()
     } catch (e) {
         return {
             enabled: false,
