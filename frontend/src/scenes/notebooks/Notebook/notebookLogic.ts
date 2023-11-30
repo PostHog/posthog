df588d261a5db48e6893b08eb08ccab62b0ac66c
Ben White <ben@posthog.com>
2023-11-30 08:28:37 +0100

    Fix

--- frontend/src/scenes/notebooks/Notebook/notebookLogic.ts
+++ frontend/src/scenes/notebooks/Notebook/notebookLogic.ts
@@ -22,7 +22,7 @@ import type { notebookLogicType } from './notebookLogicType'
 import { EditorRange, JSONContent, NotebookEditor } from './utils'
 
 const SYNC_DELAY = 1000
-const NOTEBOOK_REFRESH_MS = 10000
+const NOTEBOOK_REFRESH_MS = 30000
 
 export type NotebookLogicMode = 'notebook' | 'canvas'
 
