d2deac8852d2af0317bd4dcb13dfe479aa5a6485
Paul D'Ambra <paul@posthog.com>
2023-11-30 11:15:11 +0000

    feat: support font styles

--- ee/frontend/mobile-replay/mobile.types.ts
+++ ee/frontend/mobile-replay/mobile.types.ts
@@ -95,6 +95,14 @@ export type MobileStyles = {
      * @description horizontal alignment with respect to its parent
      */
     horizontalAlign?: 'left' | 'right' | 'center'
+    /**
+     * @description maps to CSS font-size. Accepts any valid CSS font-size value. Expects a number (treated as pixels) or a string that is a number followed by px e.g. 16px
+     */
+    fontSize?: string | number
+    /**
+     * @description maps to CSS font-family. Accepts any valid CSS font-family value.
+     */
+    fontFamily?: string
 }
 
 type wireframeBase = {
