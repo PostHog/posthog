4b682a50f6c68ad1a1614fc88c82c6a45cb05e1d
Paul D'Ambra <paul@posthog.com>
2023-11-30 10:01:07 +0000

    fix

--- frontend/src/scenes/session-recordings/playlist/SessionRecordingPreview.stories.tsx
+++ frontend/src/scenes/session-recordings/playlist/SessionRecordingPreview.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta } from '@storybook/react'
+import { Meta, StoryFn } from '@storybook/react'
 
 import { PropertyIcons, PropertyIconsProps } from './SessionRecordingPreview'
 
@@ -13,7 +13,7 @@ const meta: Meta<typeof PropertyIcons> = {
 }
 export default meta
 
-const Template = (args: PropertyIconsProps): JSX.Element => {
+const Template: StoryFn<typeof PropertyIcons> = (args: PropertyIconsProps) => {
     return (
         <div className={'border rounded px-4 py-2 w-fit'}>
             <PropertyIcons {...args} />
@@ -21,38 +21,41 @@ const Template = (args: PropertyIconsProps): JSX.Element => {
     )
 }
 
-export const WebRecording = (): JSX.Element => {
-    return (
-        <Template
-            iconClassnames={'wat'}
-            loading={false}
-            onPropertyClick={() => {}}
-            recordingProperties={[
-                { value: 'Mac OS X', property: '$os', tooltipValue: 'Mac OS X' },
-                { value: 'Chrome', property: '$browser', tooltipValue: 'Chrome' },
-                { value: 'United States', property: '$geoip_country_code', tooltipValue: 'United States' },
-                { value: 'Desktop', property: '$device_type', tooltipValue: 'Desktop' },
-            ]}
-        />
-    )
+export const WebRecording = Template.bind({})
+WebRecording.args = {
+    iconClassnames: 'wat',
+    loading: false,
+    onPropertyClick: () => {},
+    recordingProperties: [
+        { value: 'Mac OS X', property: '$os', tooltipValue: 'Mac OS X' },
+        { value: 'Chrome', property: '$browser', tooltipValue: 'Chrome' },
+        { value: 'United States', property: '$geoip_country_code', tooltipValue: 'United States' },
+        { value: 'Desktop', property: '$device_type', tooltipValue: 'Desktop' },
+    ],
 }
 
-export const AndroidRecording = (): JSX.Element => {
-    return (
-        <Template
-            iconClassnames={'wat'}
-            loading={false}
-            onPropertyClick={() => {}}
-            recordingProperties={[
-                { value: 'Android', property: '$os_name', tooltipValue: 'Android' },
-                { value: 'Awesome Fun App', property: '$app_name', tooltipValue: 'Awesome Fun App' },
-                { value: 'United States', property: '$geoip_country_code', tooltipValue: 'United States' },
-                { value: 'Mobile', property: '$device_type', tooltipValue: 'Mobile' },
-            ]}
-        />
-    )
+export const AndroidRecording = Template.bind({})
+AndroidRecording.args = {
+    iconClassnames: 'wat',
+    loading: false,
+    onPropertyClick: () => {},
+    recordingProperties: [
+        { value: 'Android', property: '$os_name', tooltipValue: 'Android' },
+        { value: 'Awesome Fun App', property: '$app_name', tooltipValue: 'Awesome Fun App' },
+        { value: 'United States', property: '$geoip_country_code', tooltipValue: 'United States' },
+        { value: 'Mobile', property: '$device_type', tooltipValue: 'Mobile' },
+    ],
 }
 
-export const Loading = (): JSX.Element => {
-    return <Template iconClassnames={'wat'} loading={true} onPropertyClick={() => {}} recordingProperties={[]} />
+export const Loading = Template.bind({})
+Loading.args = {
+    iconClassnames: 'wat',
+    loading: true,
+    onPropertyClick: () => {},
+    recordingProperties: [],
+}
+Loading.parameters = {
+    testOptions: {
+        waitForLoadersToDisappear: false,
+    },
 }
