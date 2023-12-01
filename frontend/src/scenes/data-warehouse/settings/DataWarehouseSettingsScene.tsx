e8ce34cbde8220fe0b055b2504c3a238bbe69333
eric <eeoneric@gmail.com>
2023-12-01 13:31:35 -0500

    state

--- frontend/src/scenes/data-warehouse/settings/DataWarehouseSettingsScene.tsx
+++ frontend/src/scenes/data-warehouse/settings/DataWarehouseSettingsScene.tsx
@@ -19,6 +19,7 @@ const StatusTagSetting = {
     Running: 'primary',
     Completed: 'success',
     Error: 'danger',
+    Failed: 'danger',
 }
 
 export function DataWarehouseSettingsScene(): JSX.Element {
