e4c374dbef2566b03a6e7d2d1db0de64cb67b114
David Newell <david@posthog.com>
2023-12-04 11:58:14 +0000

    chore: migrate tabs from custom to lemon

--- frontend/src/scenes/web-analytics/WebTabs.tsx
+++ frontend/src/scenes/web-analytics/WebTabs.tsx
@@ -1,8 +1,7 @@
+import { LemonTabs } from '@posthog/lemon-ui'
 import clsx from 'clsx'
-import { useSliderPositioning } from 'lib/lemon-ui/hooks'
 import React from 'react'
 
-const TRANSITION_MS = 200
 export const WebTabs = ({
     className,
     activeTabId,
@@ -15,49 +14,18 @@ export const WebTabs = ({
     setActiveTabId: (id: string) => void
 }): JSX.Element => {
     const activeTab = tabs.find((t) => t.id === activeTabId)
-    const { containerRef, selectionRef, sliderWidth, sliderOffset, transitioning } = useSliderPositioning<
-        HTMLUListElement,
-        HTMLLIElement
-    >(activeTabId, TRANSITION_MS)
 
     return (
         <div className={clsx(className, 'flex flex-col')}>
             <div className="flex flex-row items-center self-stretch mb-3">
                 {<h2 className="flex-1 m-0">{activeTab?.title}</h2>}
-                <div className="flex flex-col items-stretch relative">
-                    {tabs.length > 1 && (
-                        // TODO switch to a select if more than 3
-                        <ul className="flex flex-row items-center space-x-2" ref={containerRef}>
-                            {tabs.map(({ id, linkText }) => (
-                                <li key={id} ref={id === activeTabId ? selectionRef : undefined}>
-                                    <button
-                                        className={clsx(
-                                            'bg-transparent border-none cursor-pointer',
-                                            id === activeTabId ? 'text-bold text-link' : 'text-current hover:text-link'
-                                        )}
-                                        onClick={() => setActiveTabId(id)}
-                                    >
-                                        {linkText}
-                                    </button>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                    <div className="w-full relative">
-                        <div
-                            className="h-px bg-link absolute"
-                            // eslint-disable-next-line react/forbid-dom-props
-                            style={{
-                                width: sliderWidth,
-                                left: 0,
-                                transform: `translateX(${sliderOffset}px)`,
-                                transition: transitioning
-                                    ? `width ${TRANSITION_MS}ms ease, transform ${TRANSITION_MS}ms ease`
-                                    : undefined,
-                            }}
-                        />
-                    </div>
-                </div>
+                <LemonTabs
+                    inline
+                    borderless
+                    activeKey={activeTabId}
+                    onChange={setActiveTabId}
+                    tabs={tabs.map(({ id, linkText }) => ({ key: id, label: linkText }))}
+                />
             </div>
             <div className="flex-1 flex flex-col">{activeTab?.content}</div>
         </div>
