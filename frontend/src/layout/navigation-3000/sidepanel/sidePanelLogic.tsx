f20d70691d93fc4ea38c9cbf1fd237c949247d0d
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-12-05 11:34:07 +0100

    feat(commands): add commands for sidebar items (#19020)

--- frontend/src/layout/navigation-3000/sidepanel/sidePanelLogic.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/sidePanelLogic.tsx
@@ -32,8 +32,8 @@ export const sidePanelLogic = kea<sidePanelLogicType>([
 
     selectors({
         enabledTabs: [
-            (s) => [s.featureFlags, s.isCloudOrDev],
-            (featureFlags, isCloudOrDev) => {
+            (s) => [s.featureFlags, s.isCloudOrDev, s.isReady, s.hasCompletedAllTasks],
+            (featureFlags, isCloudOrDev, isReady, hasCompletedAllTasks) => {
                 const tabs: SidePanelTab[] = []
 
                 if (featureFlags[FEATURE_FLAGS.NOTEBOOKS]) {
@@ -46,7 +46,9 @@ export const sidePanelLogic = kea<sidePanelLogicType>([
 
                 tabs.push(SidePanelTab.Docs)
                 tabs.push(SidePanelTab.Settings)
-                tabs.push(SidePanelTab.Activation)
+                if (isReady && !hasCompletedAllTasks) {
+                    tabs.push(SidePanelTab.Activation)
+                }
                 tabs.push(SidePanelTab.Activity)
 
                 if (featureFlags[FEATURE_FLAGS.EARLY_ACCESS_FEATURE_SITE_BUTTON]) {
@@ -59,7 +61,7 @@ export const sidePanelLogic = kea<sidePanelLogicType>([
 
         visibleTabs: [
             (s) => [s.enabledTabs, s.selectedTab, s.sidePanelOpen, s.isReady, s.hasCompletedAllTasks],
-            (enabledTabs, selectedTab, sidePanelOpen, isReady, hasCompletedAllTasks): SidePanelTab[] => {
+            (enabledTabs, selectedTab, sidePanelOpen): SidePanelTab[] => {
                 return enabledTabs.filter((tab: any) => {
                     if (tab === selectedTab && sidePanelOpen) {
                         return true
@@ -67,10 +69,6 @@ export const sidePanelLogic = kea<sidePanelLogicType>([
 
                     // Hide certain tabs unless they are selected
                     if (ALWAYS_EXTRA_TABS.includes(tab)) {
-                        return false
-                    }
-
-                    if (tab === SidePanelTab.Activation && (!isReady || hasCompletedAllTasks)) {
                         return false
                     }
 
