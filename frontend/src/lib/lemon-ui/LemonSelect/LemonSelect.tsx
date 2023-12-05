f3165250ad35c00082b041d800318789f9e54747
David Newell <david@posthog.com>
2023-12-05 18:19:29 +0000

    chore: LemonSelect caret position

--- frontend/src/lib/lemon-ui/LemonSelect/LemonSelect.tsx
+++ frontend/src/lib/lemon-ui/LemonSelect/LemonSelect.tsx
@@ -140,7 +140,7 @@ export function LemonSelect<T extends string | number | boolean | null>({
             closeParentPopoverOnClickInside={menu?.closeParentPopoverOnClickInside}
         >
             <LemonButton
-                className={clsx(className, isClearButtonShown && 'LemonSelect--clearable')}
+                className={clsx(className, 'LemonSelect', isClearButtonShown && 'LemonSelect--clearable')}
                 icon={activeLeaf?.icon}
                 // so that the pop-up isn't shown along with the close button
                 sideIcon={isClearButtonShown ? <></> : undefined}
