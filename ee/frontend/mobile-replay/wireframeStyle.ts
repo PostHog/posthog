e1ef2dd4e607c8dfa09dc533611a87b0cf3797e7
Paul D'Ambra <paul@posthog.com>
2023-12-05 14:51:49 +0000

    fix: be more safe checking for presence of styles

--- ee/frontend/mobile-replay/wireframeStyle.ts
+++ ee/frontend/mobile-replay/wireframeStyle.ts
@@ -1,5 +1,17 @@
 import { MobileStyles, wireframe } from './mobile.types'
 
+function isNumber(candidate: unknown): candidate is number {
+    return typeof candidate === 'number'
+}
+
+function isString(candidate: unknown): candidate is string {
+    return typeof candidate === 'string'
+}
+
+function isUnitLike(candidate: unknown): candidate is string | number {
+    return isNumber(candidate) || (isString(candidate) && candidate.length > 0)
+}
+
 function ensureUnit(value: string | number): string {
     return typeof value === 'number' ? `${value}px` : value.replace(/px$/g, '') + 'px'
 }
@@ -7,11 +19,15 @@ function ensureUnit(value: string | number): string {
 function makeBorderStyles(wireframe: wireframe): string {
     let styles = ''
 
-    if (wireframe.style?.borderWidth) {
+    if (!wireframe.style) {
+        return styles
+    }
+
+    if (isUnitLike(wireframe.style.borderWidth)) {
         const borderWidth = ensureUnit(wireframe.style.borderWidth)
         styles += `border-width: ${borderWidth};`
     }
-    if (wireframe.style?.borderRadius) {
+    if (isUnitLike(wireframe.style.borderRadius)) {
         const borderRadius = ensureUnit(wireframe.style.borderRadius)
         styles += `border-radius: ${borderRadius};`
     }
@@ -29,13 +45,17 @@ function makeBorderStyles(wireframe: wireframe): string {
 export function makeSvgBorder(style: MobileStyles | undefined): Record<string, string> {
     const svgBorderStyles: Record<string, string> = {}
 
-    if (style?.borderWidth) {
+    if (!style) {
+        return svgBorderStyles
+    }
+
+    if (style.borderWidth) {
         svgBorderStyles['stroke-width'] = style.borderWidth.toString()
     }
-    if (style?.borderColor) {
+    if (style.borderColor) {
         svgBorderStyles.stroke = style.borderColor
     }
-    if (style?.borderRadius) {
+    if (isUnitLike(style.borderRadius)) {
         svgBorderStyles.rx = ensureUnit(style.borderRadius)
     }
 
@@ -44,18 +64,18 @@ export function makeSvgBorder(style: MobileStyles | undefined): Record<string, s
 
 export function makePositionStyles(wireframe: wireframe): string {
     let styles = ''
-    if (wireframe.width) {
+    if (isNumber(wireframe.width)) {
         styles += `width: ${ensureUnit(wireframe.width)};`
     }
-    if (wireframe.height) {
+    if (isNumber(wireframe.height)) {
         styles += `height: ${ensureUnit(wireframe.height)};`
     }
-    if (wireframe.x || wireframe.y) {
+    if (isNumber(wireframe.x) || isNumber(wireframe.y)) {
         styles += `position: fixed;`
-        if (wireframe.x) {
+        if (isNumber(wireframe.x)) {
             styles += `left: ${ensureUnit(wireframe.x)};`
         }
-        if (wireframe.y) {
+        if (isNumber(wireframe.y)) {
             styles += `top: ${ensureUnit(wireframe.y)};`
         }
     }
@@ -82,11 +102,16 @@ function makeLayoutStyles(wireframe: wireframe): string {
 
 function makeFontStyles(wireframe: wireframe): string {
     let styles = ''
-    if (wireframe.style?.fontSize) {
+
+    if (!wireframe.style) {
+        return styles
+    }
+
+    if (isUnitLike(wireframe.style.fontSize)) {
         styles += `font-size: ${ensureUnit(wireframe.style?.fontSize)};`
     }
-    if (wireframe.style?.fontFamily) {
-        styles += `font-family: ${wireframe.style?.fontFamily};`
+    if (wireframe.style.fontFamily) {
+        styles += `font-family: ${wireframe.style.fontFamily};`
     }
     return styles
 }
