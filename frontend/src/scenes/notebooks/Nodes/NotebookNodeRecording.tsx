64801654190dcbe64ad2242f49901dea044b5a9b
David Newell <david@posthog.com>
2023-12-05 10:40:08 +0000

    fix: session replay naming

--- frontend/src/scenes/notebooks/Nodes/NotebookNodeRecording.tsx
+++ frontend/src/scenes/notebooks/Nodes/NotebookNodeRecording.tsx
@@ -142,7 +142,7 @@ type NotebookNodeRecordingAttributes = {
 
 export const NotebookNodeRecording = createPostHogWidgetNode<NotebookNodeRecordingAttributes>({
     nodeType: NotebookNodeType.Recording,
-    titlePlaceholder: 'Session replay',
+    titlePlaceholder: 'Session recording',
     Component,
     heightEstimate: HEIGHT,
     minHeight: MIN_HEIGHT,
