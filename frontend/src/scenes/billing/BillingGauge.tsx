ac1a60d8c1f08a9e288cd8ba770b416e6c85836c
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-12-06 19:45:36 +0100

    move rendering into billing gauge item

--- frontend/src/scenes/billing/BillingGauge.tsx
+++ frontend/src/scenes/billing/BillingGauge.tsx
@@ -8,29 +8,29 @@ import { useMemo } from 'react'
 import { BillingGaugeItem } from './types'
 
 type BillingGaugeItemProps = {
-    width: string
-    className: string
-    tooltip: string | JSX.Element
-    top: boolean
-    value: number
+    item: BillingGaugeItem
+    maxValue: number
 }
 
-const BillingGaugeItem = ({ width, className, tooltip, top, value }: BillingGaugeItemProps): JSX.Element => {
+const BillingGaugeItem = ({ item, maxValue }: BillingGaugeItemProps): JSX.Element => {
+    const width = `${(item.value / maxValue) * 100}%`
+    const colorClassName = `bg-${item.color}`
+
     return (
         <div
-            className={`BillingGaugeItem absolute top-0 left-0 bottom-0 h-2 ${className}`}
+            className={`BillingGaugeItem absolute top-0 left-0 bottom-0 h-2 ${colorClassName}`}
             // eslint-disable-next-line react/forbid-dom-props
             style={{ '--billing-gauge-item-width': width } as React.CSSProperties}
         >
             <div className="absolute right-0 w-px h-full bg-bg-light" />
-            <Tooltip title={value.toLocaleString()} placement={'right'}>
+            <Tooltip title={item.value.toLocaleString()} placement={'right'}>
                 <div
                     className={clsx('BillingGaugeItem__info', {
-                        'BillingGaugeItem__info--bottom': !top,
+                        'BillingGaugeItem__info--bottom': !item.top,
                     })}
                 >
-                    {tooltip}
-                    <div>{compactNumber(value)}</div>
+                    <b>{item.text}</b>
+                    <div>{compactNumber(item.value)}</div>
                 </div>
             </Tooltip>
         </div>
@@ -42,21 +42,14 @@ export type BillingGaugeProps = {
 }
 
 export function BillingGauge({ items }: BillingGaugeProps): JSX.Element {
-    const maxScale = useMemo(() => {
+    const maxValue = useMemo(() => {
         return Math.max(100, ...items.map((item) => item.value)) * 1.3
     }, [items])
 
     return (
         <div className="relative h-2 bg-border-light my-16">
             {items.map((item, i) => (
-                <BillingGaugeItem
-                    key={i}
-                    width={`${(item.value / maxScale) * 100}%`}
-                    className={`bg-${item.color}`}
-                    tooltip={<b>{item.text}</b>}
-                    top={item.top}
-                    value={item.value}
-                />
+                <BillingGaugeItem key={i} item={item} maxValue={maxValue} />
             ))}
         </div>
     )
