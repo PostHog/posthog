7824d69da6dd104650ef3f8fcc245be8b8b6a6cb
Tomás Farías Santana <tomas@tomasfarias.dev>
2023-12-06 13:55:21 +0100

    feat: First consumer implementation

---
+++ hook-consumer/src/config.rs
@@ -0,0 +1,56 @@
+use std::str::FromStr;
+use std::time;
+
+use envconfig::Envconfig;
+
+#[derive(Envconfig, Clone)]
+pub struct Config {
+    #[envconfig(default = "postgres://posthog:posthog@localhost:15432/test_database")]
+    pub database_url: String,
+
+    #[envconfig(default = "consumer")]
+    pub consumer_name: String,
+
+    #[envconfig(default = "default")]
+    pub queue_name: String,
+
+    #[envconfig(default = "100")]
+    pub poll_interval: EnvMsDuration,
+
+    #[envconfig(default = "5000")]
+    pub request_timeout: EnvMsDuration,
+
+    #[envconfig(nested = true)]
+    pub retry_policy: RetryPolicyConfig,
+
+    #[envconfig(default = "job_queue")]
+    pub table_name: String,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct EnvMsDuration(pub time::Duration);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseEnvMsDurationError;
+
+impl FromStr for EnvMsDuration {
+    type Err = ParseEnvMsDurationError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let ms = s.parse::<u64>().map_err(|_| ParseEnvMsDurationError)?;
+
+        Ok(EnvMsDuration(time::Duration::from_millis(ms)))
+    }
+}
+
+#[derive(Envconfig, Clone)]
+pub struct RetryPolicyConfig {
+    #[envconfig(default = "2")]
+    pub backoff_coefficient: u32,
+
+    #[envconfig(default = "1000")]
+    pub initial_interval: EnvMsDuration,
+
+    #[envconfig(default = "100000")]
+    pub maximum_interval: EnvMsDuration,
+}
--- hook-consumer/src/main.rs
+++ hook-consumer/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use envconfig::Envconfig;
+
+use hook_common::pgqueue::{PgQueue, RetryPolicy};
+use hook_consumer::config::Config;
+use hook_consumer::consumer::WebhookConsumer;
+
+#[tokio::main]
+async fn main() {
+    let config = Config::init_from_env().expect("Invalid configuration:");
+
+    let retry_policy = RetryPolicy::new(
+        config.retry_policy.backoff_coefficient,
+        config.retry_policy.initial_interval.0,
+        Some(config.retry_policy.maximum_interval.0),
+    );
+    let queue = PgQueue::new(
+        &config.queue_name,
+        &config.table_name,
+        &config.database_url,
+        retry_policy,
+    )
+    .await
+    .expect("failed to initialize queue");
+
+    let consumer = WebhookConsumer::new(
+        &config.consumer_name,
+        &queue,
+        config.poll_interval.0,
+        config.request_timeout.0,
+    );
+
+    let _ = consumer.run().await;
 }
