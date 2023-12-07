bcf547a5a1ddcae58a58012acbe5d62b4fd48498
Ben White <ben@posthog.com>
2023-12-07 16:30:06 +0100

    Fix comment

--- frontend/src/lib/hooks/usePopoverContainerContext.ts
+++ frontend/src/lib/hooks/usePopoverContainerContext.ts
@@ -1,8 +1,18 @@
 import { createContext, RefObject, useContext } from 'react'
 
+/**
+ * PopoverContainerContext
+ *
+ * Typically popovers are portaled to the root of the document, but sometimes
+ * you want to portal them to a specific container. This context allows you to
+ * specify a container for popovers to portal to.
+ *
+ * For example the Toolbar or the SessionRecordingPlayer so that in full screen mode
+ * the popover is not portaled to the root of the document but to the player.
+ */
+
 export const PopoverContainerContext = createContext<RefObject<HTMLElement> | undefined>(undefined)
 
-// Currently there is no way to optionally get bound logics so this context allows us to maybe get a logic if it is "bound" via the provider
 export const usePopoverContainerContext = (): RefObject<HTMLElement> | undefined => {
     return useContext(PopoverContainerContext)
 }
