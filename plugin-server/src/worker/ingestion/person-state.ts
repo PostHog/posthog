432bddc042be9608b8880e7556777d597b5db432
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2023-12-07 10:49:01 -0800

    Add `instrumenr` call to periodic task.

--- plugin-server/src/worker/ingestion/person-state.ts
+++ plugin-server/src/worker/ingestion/person-state.ts
@@ -10,6 +10,7 @@ import { Person, PropertyUpdateOperation, TimestampFormat } from '../../types'
 import { DB } from '../../utils/db/db'
 import { PostgresRouter, PostgresUse, TransactionClient } from '../../utils/db/postgres'
 import { timeoutGuard } from '../../utils/db/utils'
+import { instrument } from '../../utils/metrics'
 import { PeriodicTask } from '../../utils/periodic-task'
 import { promiseRetry } from '../../utils/retries'
 import { status } from '../../utils/status'
@@ -813,7 +814,9 @@ export class DeferredPersonOverrideWorker {
     public runTask(intervalMs: number): PeriodicTask {
         return new PeriodicTask(async () => {
             status.debug('👥', 'Processing pending overrides...')
-            const overridesCount = await this.processPendingOverrides()
+            const overridesCount = await instrument({ metricName: 'processPendingOverrides' }, () =>
+                this.processPendingOverrides()
+            )
             ;(overridesCount > 0 ? status.info : status.debug)('👥', `Processed ${overridesCount} pending overrides.`)
         }, intervalMs)
     }
