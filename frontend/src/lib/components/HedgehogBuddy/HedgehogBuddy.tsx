f2554dcd34775f879c3e0e0f8441dd6fa60e3dc6
Michael Matloka <dev@twixes.com>
2023-12-07 21:45:54 +0100

    chore: Turn hedgehog mode into a feature preview (#19165)
    * Turn hedgehog mode into a feature preview
    
    * Fix "url" capitalization
    
    * Don't allow standing on inactive stealth buttons
    
    * Remove unused empty TS file
    
    * Fix typing
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (1)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/lib/components/HedgehogBuddy/HedgehogBuddy.tsx
+++ frontend/src/lib/components/HedgehogBuddy/HedgehogBuddy.tsx
@@ -10,7 +10,7 @@ import { range, sampleOne, shouldIgnoreInput } from 'lib/utils'
 import { MutableRefObject, useEffect, useRef, useState } from 'react'
 
 import { HedgehogAccessories } from './HedgehogAccessories'
-import { hedgehogbuddyLogic } from './hedgehogbuddyLogic'
+import { hedgehogBuddyLogic } from './hedgehogBuddyLogic'
 import {
     AccessoryInfo,
     baseSpriteAccessoriesPath,
@@ -263,7 +263,7 @@ export class HedgehogActor {
                     // Only calculate block bounding rects once we need to
                     blocksWithBoundingRects = Array.from(
                         document.querySelectorAll(
-                            '.border, .border-t, .LemonButton--primary, .LemonButton--secondary, .LemonInput, .LemonSelect, .LemonTable'
+                            '.border, .border-t, .LemonButton--primary, .LemonButton--secondary:not(.LemonButton--is-stealth:not(.LemonButton--active)), .LemonInput, .LemonSelect, .LemonTable'
                         )
                     ).map((block) => [block, block.getBoundingClientRect()])
                 }
@@ -403,8 +403,8 @@ export function HedgehogBuddy({
     }
 
     const actor = actorRef.current
-    const { accessories } = useValues(hedgehogbuddyLogic)
-    const { addAccessory } = useActions(hedgehogbuddyLogic)
+    const { accessories } = useValues(hedgehogBuddyLogic)
+    const { addAccessory } = useActions(hedgehogBuddyLogic)
 
     useEffect(() => {
         return actor.setupKeyboardListeners()
