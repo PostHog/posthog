22f22161fdb0fc1595151b189d7a3c392e7da7db
Julian Bez <julian.bez@gmail.com>
2023-12-11 17:44:53 +0000

    Revert "chore(deps): Upgrade to Django 4.1 (#18830)"
    This reverts commit 580c7b1aba096d254abbdd1b33baf0451a97d2ea.

--- posthog/middleware.py
+++ posthog/middleware.py
@@ -361,7 +361,7 @@ class CaptureMiddleware:
                 # Some middlewares raise MiddlewareNotUsed if they are not
                 # needed. In this case we want to avoid the default middlewares
                 # being used.
-                middlewares.append(middleware_class(get_response=get_response))
+                middlewares.append(middleware_class(get_response=None))
             except MiddlewareNotUsed:
                 pass
 
