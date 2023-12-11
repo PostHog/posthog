a438f1b8c2899f71cc023a42a857533af5cc29cc
Ben White <ben@posthog.com>
2023-12-11 08:55:44 +0100

    Fix sharing link

--- frontend/src/scenes/notebooks/Notebook/NotebookShare.tsx
+++ frontend/src/scenes/notebooks/Notebook/NotebookShare.tsx
@@ -1,17 +1,25 @@
 import { LemonBanner, LemonButton, LemonDivider } from '@posthog/lemon-ui'
+import { useValues } from 'kea'
 import { combineUrl } from 'kea-router'
 import { IconCopy } from 'lib/lemon-ui/icons'
 import { LemonDialog } from 'lib/lemon-ui/LemonDialog'
+import { base64Encode } from 'lib/utils'
 import { copyToClipboard } from 'lib/utils/copyToClipboard'
 import posthog from 'posthog-js'
 import { useState } from 'react'
 import { urls } from 'scenes/urls'
 
+import { notebookLogic } from './notebookLogic'
+
 export type NotebookShareProps = {
     shortId: string
 }
 export function NotebookShare({ shortId }: NotebookShareProps): JSX.Element {
+    const { content } = useValues(notebookLogic({ shortId }))
     const url = combineUrl(`${window.location.origin}${urls.notebook(shortId)}`).url
+
+    const canvasUrl =
+        combineUrl(`${window.location.origin}${urls.canvas()}`).url + `#🦔=${base64Encode(JSON.stringify(content))}`
 
     const [interestTracked, setInterestTracked] = useState(false)
 
@@ -35,6 +43,24 @@ export function NotebookShare({ shortId }: NotebookShareProps): JSX.Element {
                 title={url}
             >
                 <span className="truncate">{url}</span>
+            </LemonButton>
+
+            <LemonDivider className="my-4" />
+
+            <h3>Template Link</h3>
+            <p>
+                The link below will open a Canvas with the contents of this Notebook, allowing the receiver to view it,
+                edit it or create their own Notebook without affecting this one.
+            </p>
+            <LemonButton
+                type="secondary"
+                fullWidth
+                center
+                sideIcon={<IconCopy />}
+                onClick={() => void copyToClipboard(canvasUrl, 'canvas link')}
+                title={url}
+            >
+                <span className="truncate">{canvasUrl}</span>
             </LemonButton>
 
             <LemonDivider className="my-4" />
