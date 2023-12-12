78fdf5f256e0eaba79510ef1c470400d34821536
Bianca Yang <ipacifics@gmail.com>
2023-12-12 09:28:59 -0800

    feat: Ask Users to Invite Team Members During Onboarding (#19114)
    * add feature flag for inviting team member step, implement step
    
    * undo launch json changes
    
    * fix feature flag check
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (1)
    
    * spice up the onboarding copy
    
    * extract invite modal logic into common component
    
    * move hogs / update language
    
    ---------
    
    Co-authored-by: Bianca Yang <bianca@posthog.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Raquel Smith <raquelmsmith@users.noreply.github.com>

--- frontend/src/scenes/onboarding/sdks/SDKs.tsx
+++ frontend/src/scenes/onboarding/sdks/SDKs.tsx
@@ -1,6 +1,7 @@
 import { IconArrowLeft } from '@posthog/icons'
 import { LemonButton, LemonCard, LemonDivider, LemonSelect } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
+import { LaptopHog1 } from 'lib/components/hedgehogs'
 import { useWindowSize } from 'lib/hooks/useWindowSize'
 import { useEffect } from 'react'
 import React from 'react'
@@ -47,6 +48,7 @@ export function SDKs({
             stepKey={stepKey}
             continueOverride={!showSideBySide && panel === 'options' ? <></> : undefined}
             backActionOverride={!showSideBySide && panel === 'instructions' ? () => setPanel('options') : undefined}
+            hedgehog={<LaptopHog1 />}
         >
             <LemonDivider className="my-8" />
             <div className="flex gap-x-8 mt-8">
