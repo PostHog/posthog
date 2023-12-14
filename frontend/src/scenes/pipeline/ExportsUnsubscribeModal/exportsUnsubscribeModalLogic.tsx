244fbcd3fa9009b26fd6a197b6209e487d826dcd
Michael Matloka <michal@matloka.com>
2023-12-14 09:59:56 +0000

    Make modal work for plugin configs

--- frontend/src/scenes/pipeline/ExportsUnsubscribeModal/exportsUnsubscribeModalLogic.tsx
+++ frontend/src/scenes/pipeline/ExportsUnsubscribeModal/exportsUnsubscribeModalLogic.tsx
@@ -1,56 +1,39 @@
 import { actions, afterMount, connect, kea, path, reducers, selectors } from 'kea'
 import { loaders } from 'kea-loaders'
 import api from 'lib/api'
+import { batchExportsListLogic } from 'scenes/batch_exports/batchExportsListLogic'
 import { pluginsLogic } from 'scenes/plugins/pluginsLogic'
 
-import { BatchExportConfiguration, PluginConfigTypeNew } from '~/types'
+import { PluginConfigTypeNew } from '~/types'
 
 import type { exportsUnsubscribeModalLogicType } from './exportsUnsubscribeModalLogicType'
 
 export const exportsUnsubscribeModalLogic = kea<exportsUnsubscribeModalLogicType>([
     path(['scenes', 'pipeline', 'exportsUnsubscribeModalLogic']),
-    connect({ values: [pluginsLogic, ['plugins']] }),
+    connect({ values: [pluginsLogic, ['plugins'], batchExportsListLogic, ['batchExportConfigs']] }),
     actions({
         openModal: true,
         closeModal: true,
     }),
-    loaders(({ values }) => ({
-        pluginConfigs: [
-            {} as Record<number, PluginConfigTypeNew>,
+    loaders(() => ({
+        pluginConfigsToDisable: [
+            [] as PluginConfigTypeNew[],
             {
                 loadPluginConfigs: async () => {
-                    // TODO: need the api to give all the plugin configs for the organization that need to be disabled.
-                    const pluginConfigs: Record<number, PluginConfigTypeNew> = {}
-                    const results: PluginConfigTypeNew[] = await api.loadPaginatedResults(
+                    return await api.get<PluginConfigTypeNew[]>(
                         `api/organizations/@current/plugins/exports_unsubscribe_configs`
                     )
-
-                    for (const pluginConfig of results) {
-                        pluginConfigs[pluginConfig.id] = {
-                            ...pluginConfig,
-                            // If this pluginConfig doesn't have a name of desciption, use the plugin's
-                            name: pluginConfig.name || values.plugins[pluginConfig.plugin]?.name || 'Unknown app',
-                            description: pluginConfig.description || values.plugins[pluginConfig.plugin]?.description,
-                        }
-                    }
-                    return pluginConfigs
                 },
-            },
-        ],
-        batchExports: [
-            {} as Record<number, BatchExportConfiguration>,
-            {
-                // TODO: need the api to give all the batch export configs for the organization that need to be disabled & their disabling action
             },
         ],
     })),
     selectors({
         loading: [
-            (s) => [s.batchExportsLoading, s.pluginConfigsLoading],
+            (s) => [s.batchExportsLoading, s.pluginConfigsToDisableLoading],
             (batchExportsLoading, pluginConfigsLoading) => batchExportsLoading || pluginConfigsLoading,
         ],
         unsubscribeDisabled: [
-            (s) => [s.pluginConfigs, s.batchExports],
+            (s) => [s.pluginConfigsToDisable, s.batchExports],
             (pluginConfigs, batchExports) => pluginConfigs || batchExports,
         ],
     }),
