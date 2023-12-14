2b28a3a3e345ee1439e4d6d90c3315e1a717364e
Michael Matloka <michal@matloka.com>
2023-12-14 14:25:37 +0000

    feat(trends): Add "Decimal places" as a Trends viz option

--- posthog/schema.py
+++ posthog/schema.py
@@ -609,6 +609,7 @@ class TrendsFilter(BaseModel):
     aggregation_axis_prefix: Optional[str] = None
     breakdown_histogram_bin_count: Optional[float] = None
     compare: Optional[bool] = None
+    decimal_places: Optional[float] = None
     display: Optional[ChartDisplayType] = None
     formula: Optional[str] = None
     hidden_legend_indexes: Optional[List[float]] = None
