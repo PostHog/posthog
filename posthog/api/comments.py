c3fb8c8a36894acff988c98a58be4b0518b67cb3
Ben White <ben@posthog.com>
2023-12-15 10:30:05 +0100

    Fix deleted comments

--- posthog/api/comments.py
+++ posthog/api/comments.py
@@ -75,6 +75,9 @@ class CommentViewSet(StructuredViewSetMixin, ForbidDestroyModel, viewsets.ModelV
         if params.get("user"):
             queryset = queryset.filter(user=params.get("user"))
 
+        if self.action == "list" and params.get("deleted", "false") == "false":
+            queryset = queryset.filter(deleted=False)
+
         if params.get("scope"):
             queryset = queryset.filter(scope=params.get("scope"))
 
