1bb278ace5f436271fbcc00ed3e97b7c8166496e
Ben White <ben@posthog.com>
2023-12-15 15:16:45 +0100

    Fix

--- posthog/migrations/0376_add_comments.py
+++ posthog/migrations/0376_add_comments.py
@@ -7,29 +7,43 @@ import posthog.models.utils
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('posthog', '0373_externaldataschema'),
+        ("posthog", "0375_alter_survey_type"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Comment',
+            name="Comment",
             fields=[
-                ('id', models.UUIDField(default=posthog.models.utils.UUIDT, editable=False, primary_key=True, serialize=False)),
-                ('content', models.TextField(blank=True, null=True)),
-                ('version', models.IntegerField(default=0)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('deleted', models.BooleanField(blank=True, null=True)),
-                ('item_id', models.CharField(max_length=72, null=True)),
-                ('scope', models.CharField(max_length=79)),
-                ('created_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
-                ('source_comment_id', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='posthog.comment')),
-                ('team', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='posthog.team')),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=posthog.models.utils.UUIDT, editable=False, primary_key=True, serialize=False
+                    ),
+                ),
+                ("content", models.TextField(blank=True, null=True)),
+                ("version", models.IntegerField(default=0)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("deleted", models.BooleanField(blank=True, null=True)),
+                ("item_id", models.CharField(max_length=72, null=True)),
+                ("scope", models.CharField(max_length=79)),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL
+                    ),
+                ),
+                (
+                    "source_comment_id",
+                    models.ForeignKey(
+                        blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to="posthog.comment"
+                    ),
+                ),
+                ("team", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="posthog.team")),
             ],
         ),
         migrations.AddIndex(
-            model_name='comment',
-            index=models.Index(fields=['team_id', 'scope', 'item_id'], name='posthog_com_team_id_be2206_idx'),
+            model_name="comment",
+            index=models.Index(fields=["team_id", "scope", "item_id"], name="posthog_com_team_id_be2206_idx"),
         ),
     ]
