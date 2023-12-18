894dfc3c0ad15e1faa93cdcb17a65fbfac9d880f
Bianca Yang <bianca@posthog.com>
2023-12-18 10:37:11 -0800

    update the insights redirect url to include onboarding_completed

--- frontend/src/scenes/onboarding/onboardingLogic.tsx
+++ frontend/src/scenes/onboarding/onboardingLogic.tsx
@@ -33,7 +33,7 @@ export const getProductUri = (productKey: ProductKey, featureFlags: FeatureFlags
     switch (productKey) {
         case ProductKey.PRODUCT_ANALYTICS:
             return featureFlags[FEATURE_FLAGS.REDIRECT_INGESTION_PRODUCT_ANALYTICS_ONBOARDING] === 'test'
-                ? urls.insights()
+                ? combineUrl(urls.insights(), { onboarding_completed: true }).url
                 : combineUrl(urls.events(), { onboarding_completed: true }).url
         case ProductKey.SESSION_REPLAY:
             return urls.replay()
