a5a3d7aff8ae6a21b3cb9986b5845ac66a22c62c
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-12-20 12:43:44 +0100

    wip

--- frontend/src/scenes/trends/persons-modal/personsModalLogic.ts
+++ frontend/src/scenes/trends/persons-modal/personsModalLogic.ts
@@ -1,5 +1,5 @@
 import { lemonToast } from '@posthog/lemon-ui'
-import { actions, afterMount, connect, kea, key, listeners, path, props, reducers, selectors } from 'kea'
+import { actions, afterMount, connect, kea, listeners, path, props, propsChanged, reducers, selectors } from 'kea'
 import { loaders } from 'kea-loaders'
 import { router, urlToAction } from 'kea-router'
 import api from 'lib/api'
@@ -43,7 +43,7 @@ export interface ListActorsResponse {
 export const personsModalLogic = kea<personsModalLogicType>([
     path(['scenes', 'trends', 'personsModalLogic']),
     props({} as PersonModalLogicProps),
-    key((props) => props.url),
+    // key((props) => props.url),
     actions({
         setSearchTerm: (search: string) => ({ search }),
         saveAsCohort: (cohortName: string) => ({ cohortName }),
@@ -311,4 +311,10 @@ export const personsModalLogic = kea<personsModalLogicType>([
             }
         },
     })),
+
+    propsChanged(({ props, actions }, oldProps) => {
+        if (props.url !== oldProps.url) {
+            actions.loadActors({ query: props.query, url: props.url, clear: true })
+        }
+    }),
 ])
