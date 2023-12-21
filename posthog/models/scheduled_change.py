40563373012ebf73e85d4039ed9bbf46bf028711
Juraj Majerik <juro.majerik@gmail.com>
2023-12-21 15:04:07 +0100

    tweak migration

--- posthog/models/scheduled_change.py
+++ posthog/models/scheduled_change.py
@@ -9,8 +9,8 @@ class ScheduledChange(models.Model):
     record_id: models.CharField = models.CharField(max_length=200)
     model_name: models.CharField = models.CharField(max_length=100, choices=AllowedModels.choices)
     payload: models.JSONField = models.JSONField(default=dict)
-    scheduled_at: models.DateTimeField = models.DateTimeField(db_index=True)
-    executed_at: models.DateTimeField = models.DateTimeField(null=True, blank=True, db_index=True)
+    scheduled_at: models.DateTimeField = models.DateTimeField()
+    executed_at: models.DateTimeField = models.DateTimeField(null=True, blank=True)
     failure_reason = models.CharField(max_length=400, null=True, blank=True)
 
     team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
