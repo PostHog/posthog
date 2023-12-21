8535009813f066beb6b6af302e4468f3ca93f3e1
David Newell <david@posthog.com>
2023-12-21 17:40:24 +0000

    make Card components error

--- .eslintrc.js
+++ .eslintrc.js
@@ -133,6 +133,11 @@ module.exports = {
                         importNames: ['Col'],
                         message: 'use flex utility classes instead - most of the time can simply be a plain <div>',
                     },
+                    {
+                        name: 'antd',
+                        importNames: ['Card'],
+                        message: 'use utility classes instead',
+                    },
                 ],
             },
         ],
@@ -155,10 +160,6 @@ module.exports = {
                     {
                         element: 'Divider',
                         message: 'use <LemonDivider> instead',
-                    },
-                    {
-                        element: 'Card',
-                        message: 'use utility classes instead',
                     },
                     {
                         element: 'Button',
