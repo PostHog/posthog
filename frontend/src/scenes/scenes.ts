eded370af9d9eb5f0761a3e815991e3f74560a8c
David Newell <david@posthog.com>
2023-12-21 11:19:30 +0000

    fix tiptap flush sync error

--- frontend/src/scenes/scenes.ts
+++ frontend/src/scenes/scenes.ts
@@ -495,7 +495,3 @@ export const routes: Record<string, Scene> = {
     [urls.canvas()]: Scene.Canvas,
     [urls.settings(':section' as any)]: Scene.Settings,
 }
-
-export const routesAsRegexes: Record<any, Scene> = Object.fromEntries(
-    Object.entries(routes).map(([route, scene]) => [new RegExp(`^${route.replace(/:\w+/g, '[^/]+')}$`), scene])
-)
