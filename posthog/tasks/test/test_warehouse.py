9ba5cec41cf4693c64aeb4bb0281e709fd45fc6d
eric <eeoneric@gmail.com>
2023-12-21 11:45:28 -0500

    test

--- posthog/tasks/test/test_warehouse.py
+++ posthog/tasks/test/test_warehouse.py
@@ -5,8 +5,9 @@ from posthog.tasks.warehouse import (
     _traverse_jobs_by_field,
     capture_workspace_rows_synced_by_team,
     check_external_data_source_billing_limit_by_team,
+    check_synced_row_limits_of_team,
 )
-from posthog.warehouse.models import ExternalDataSource
+from posthog.warehouse.models import ExternalDataSource, ExternalDataJob
 from freezegun import freeze_time
 
 
@@ -165,3 +166,33 @@ class TestWarehouse(APIBaseTest):
 
         external_source.refresh_from_db()
         self.assertEqual(external_source.status, "running")
+
+    @patch("posthog.tasks.warehouse.MONTHLY_LIMIT", 100)
+    @patch("posthog.tasks.warehouse.cancel_external_data_workflow")
+    @patch("posthog.tasks.warehouse.pause_external_data_schedule")
+    def test_check_synced_row_limits_of_team(
+        self, pause_schedule_mock: MagicMock, cancel_workflow_mock: MagicMock
+    ) -> None:
+        source = ExternalDataSource.objects.create(
+            source_id="test_id",
+            connection_id="fake connectino_id",
+            destination_id="fake destination_id",
+            team=self.team,
+            status="Running",
+            source_type="Stripe",
+        )
+
+        job = ExternalDataJob.objects.create(
+            pipeline=source, workflow_id="fake_workflow_id", team=self.team, status="Running", rows_synced=100000
+        )
+
+        check_synced_row_limits_of_team(self.team.pk)
+
+        source.refresh_from_db()
+        self.assertEqual(source.status, ExternalDataJob.Status.CANCELLED)
+
+        job.refresh_from_db()
+        self.assertEqual(job.status, ExternalDataJob.Status.CANCELLED)
+
+        self.assertEqual(pause_schedule_mock.call_count, 1)
+        self.assertEqual(cancel_workflow_mock.call_count, 1)
