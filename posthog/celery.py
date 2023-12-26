15c2f8abf58dfea46fab1a4668cf142fc06945b7
Eric Duong <eeoneric@gmail.com>
2023-12-26 10:56:08 -0500

    chore(data-warehouse): dw heartbeat limits 2 (#19486)
    * progress
    
    * test
    
    * add pause and unpause logic
    
    * typing
    
    * typing
    
    * typing
    
    * tested
    
    * add log

--- posthog/celery.py
+++ posthog/celery.py
@@ -355,6 +355,12 @@ def setup_periodic_tasks(sender: Celery, **kwargs):
         name="calculate external data rows synced",
     )
 
+    sender.add_periodic_task(
+        crontab(minute="23", hour="*"),
+        calculate_external_data_rows_synced.s(),
+        name="check external data rows synced",
+    )
+
 
 # Set up clickhouse query instrumentation
 @task_prerun.connect
@@ -1118,3 +1124,13 @@ def sync_datawarehouse_sources():
         pass
     else:
         sync_resources()
+
+
+@app.task(ignore_result=True)
+def check_data_import_row_limits():
+    try:
+        from posthog.tasks.warehouse import check_synced_row_limits
+    except ImportError:
+        pass
+    else:
+        check_synced_row_limits()
