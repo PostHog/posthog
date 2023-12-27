0fedb207af5b4d0ca27e520da80c1732c76b4102
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-12-27 14:56:20 +0100

    remove command palette

--- frontend/src/layout/navigation/TopBar/TopBar.tsx
+++ frontend/src/layout/navigation/TopBar/TopBar.tsx
@@ -1,14 +1,11 @@
 import './TopBar.scss'
 
-import { CommandPalette } from 'lib/components/CommandPalette/CommandPalette'
-
 import { Announcement } from './Announcement'
 
 export function TopBar(): JSX.Element {
     return (
         <>
             <Announcement />
-            <CommandPalette />
         </>
     )
 }
