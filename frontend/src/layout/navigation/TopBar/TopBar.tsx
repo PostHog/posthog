b3d2dc1b35aa6c35eca6cf9c9ff9142eea806dff
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-12-27 14:57:11 +0100

    remove announcement from top bar

--- frontend/src/layout/navigation/TopBar/TopBar.tsx
+++ frontend/src/layout/navigation/TopBar/TopBar.tsx
@@ -1,11 +1,5 @@
 import './TopBar.scss'
 
-import { Announcement } from './Announcement'
-
 export function TopBar(): JSX.Element {
-    return (
-        <>
-            <Announcement />
-        </>
-    )
+    return <></>
 }
