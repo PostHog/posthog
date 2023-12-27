378deec0baf3b9d2699a701cf84a8576ff1bde1b
baristaGeek <estebanvargas94@gmail.com>
2023-12-27 18:37:03 -0500

    Add conditional rendering based on the length of the sorted dashboards array

--- frontend/src/scenes/saved-insights/SavedInsightsFilters.tsx
+++ frontend/src/scenes/saved-insights/SavedInsightsFilters.tsx
@@ -1,9 +1,9 @@
 import { IconCalendar } from '@posthog/icons'
 import { useActions, useValues } from 'kea'
 import { DateFilter } from 'lib/components/DateFilter/DateFilter'
-import { MemberSelect } from 'lib/components/MemberSelect'
 import { LemonInput } from 'lib/lemon-ui/LemonInput/LemonInput'
 import { LemonSelect } from 'lib/lemon-ui/LemonSelect'
+import { membersLogic } from 'scenes/organization/membersLogic'
 import { INSIGHT_TYPE_OPTIONS } from 'scenes/saved-insights/SavedInsights'
 import { savedInsightsLogic } from 'scenes/saved-insights/savedInsightsLogic'
 
@@ -17,6 +17,8 @@ export function SavedInsightsFilters(): JSX.Element {
 
     const { tab, createdBy, insightType, dateFrom, dateTo, dashboardId, search } = filters
 
+    const { meFirstMembers } = useValues(membersLogic)
+
     return (
         <div className="flex justify-between gap-2 mb-2 items-center flex-wrap">
             <LemonInput
@@ -25,64 +27,76 @@ export function SavedInsightsFilters(): JSX.Element {
                 onChange={(value) => setSavedInsightsFilters({ search: value })}
                 value={search || ''}
             />
-            <div className="flex items-center gap-2 flex-wrap">
-                <div className="flex items-center gap-2">
-                    <span>On dashboard:</span>
-                    <LemonSelect
-                        size="small"
-                        options={nameSortedDashboards.map((nsd) => ({
-                            value: nsd.id,
-                            label: nsd.name,
-                        }))}
-                        value={dashboardId}
-                        onChange={(newValue) => {
-                            setSavedInsightsFilters({ dashboardId: newValue })
-                        }}
-                        dropdownMatchSelectWidth={false}
-                        data-attr="insight-on-dashboard"
-                        allowClear={true}
-                    />
-                </div>
-                <div className="flex items-center gap-2">
-                    <span>Type:</span>
-                    <LemonSelect
-                        size="small"
-                        options={INSIGHT_TYPE_OPTIONS}
-                        value={insightType}
-                        onChange={(v: any): void => setSavedInsightsFilters({ insightType: v })}
-                        dropdownMatchSelectWidth={false}
-                        data-attr="insight-type"
-                    />
-                </div>
-                <div className="flex items-center gap-2">
-                    <span>Last modified:</span>
-                    <DateFilter
-                        disabled={false}
-                        dateFrom={dateFrom}
-                        dateTo={dateTo}
-                        onChange={(fromDate, toDate) =>
-                            setSavedInsightsFilters({ dateFrom: fromDate, dateTo: toDate ?? undefined })
-                        }
-                        makeLabel={(key) => (
-                            <>
-                                <IconCalendar />
-                                <span className="hide-when-small"> {key}</span>
-                            </>
-                        )}
-                    />
-                </div>
-                {tab !== SavedInsightsTabs.Yours ? (
+            {nameSortedDashboards.length > 0 && (
+                <div className="flex items-center gap-2 flex-wrap">
                     <div className="flex items-center gap-2">
-                        <span>Created by:</span>
-                        <MemberSelect
+                        <span>On dashboard:</span>
+                        <LemonSelect
                             size="small"
-                            type="secondary"
-                            value={createdBy === 'All users' ? null : createdBy}
-                            onChange={(user) => setSavedInsightsFilters({ createdBy: user?.id || 'All users' })}
+                            options={nameSortedDashboards.map((nsd) => ({
+                                value: nsd.id,
+                                label: nsd.name,
+                            }))}
+                            value={dashboardId}
+                            onChange={(newValue) => {
+                                setSavedInsightsFilters({ dashboardId: newValue })
+                            }}
+                            dropdownMatchSelectWidth={false}
+                            data-attr="insight-on-dashboard"
+                            allowClear={true}
                         />
                     </div>
-                ) : null}
-            </div>
+                    <div className="flex items-center gap-2">
+                        <span>Type:</span>
+                        <LemonSelect
+                            size="small"
+                            options={INSIGHT_TYPE_OPTIONS}
+                            value={insightType}
+                            onChange={(v: any): void => setSavedInsightsFilters({ insightType: v })}
+                            dropdownMatchSelectWidth={false}
+                            data-attr="insight-type"
+                        />
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <span>Last modified:</span>
+                        <DateFilter
+                            disabled={false}
+                            dateFrom={dateFrom}
+                            dateTo={dateTo}
+                            onChange={(fromDate, toDate) =>
+                                setSavedInsightsFilters({ dateFrom: fromDate, dateTo: toDate ?? undefined })
+                            }
+                            makeLabel={(key) => (
+                                <>
+                                    <IconCalendar />
+                                    <span className="hide-when-small"> {key}</span>
+                                </>
+                            )}
+                        />
+                    </div>
+                    {tab !== SavedInsightsTabs.Yours ? (
+                        <div className="flex items-center gap-2">
+                            <span>Created by:</span>
+                            {/* TODO: Fix issues with user name order due to numbers having priority */}
+                            <LemonSelect
+                                size="small"
+                                options={[
+                                    { value: 'All users' as number | 'All users', label: 'All Users' },
+                                    ...meFirstMembers.map((x) => ({
+                                        value: x.user.id,
+                                        label: x.user.first_name,
+                                    })),
+                                ]}
+                                value={createdBy}
+                                onChange={(v: any): void => {
+                                    setSavedInsightsFilters({ createdBy: v })
+                                }}
+                                dropdownMatchSelectWidth={false}
+                            />
+                        </div>
+                    ) : null}
+                </div>
+            )}
         </div>
     )
 }
