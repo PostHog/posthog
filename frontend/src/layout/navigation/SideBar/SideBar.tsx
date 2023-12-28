a279a8156f7944c45199d862e141a36d74161944
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-12-28 15:57:19 +0100

    remove styles

--- frontend/src/layout/navigation/SideBar/SideBar.tsx
+++ frontend/src/layout/navigation/SideBar/SideBar.tsx
@@ -1,23 +1,11 @@
-import './SideBar.scss'
-
-import clsx from 'clsx'
-import { useValues } from 'kea'
 import { ActivationSidebar } from 'lib/components/ActivationSidebar/ActivationSidebar'
 import { DebugNotice } from 'lib/components/DebugNotice'
 import { NotebookPopover } from 'scenes/notebooks/NotebookPanel/NotebookPopover'
 
-import { navigationLogic } from '../navigationLogic'
-
 export function SideBar(): JSX.Element {
-    const { isSideBarShown } = useValues(navigationLogic)
-
     return (
-        <div className={clsx('SideBar', !isSideBarShown && 'SideBar--hidden')}>
-            <div className="SideBar__slider">
-                <div className="SideBar__slider__content">
-                    <DebugNotice />
-                </div>
-            </div>
+        <div>
+            <DebugNotice />
             <NotebookPopover />
             <ActivationSidebar />
         </div>
