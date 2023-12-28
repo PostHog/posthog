632bcbf5883f227fdddc6ae6ed5a38c22c9daaf2
Thomas Obermüller <thomas.obermueller@gmail.com>
2023-12-28 12:21:30 +0100

    chore(3000): remove main-app-content (#19526)

--- frontend/src/layout/navigation/Navigation.tsx
+++ frontend/src/layout/navigation/Navigation.tsx
@@ -1,37 +1,9 @@
-import clsx from 'clsx'
-import { BillingAlertsV2 } from 'lib/components/BillingAlertsV2'
-import { ReactNode } from 'react'
-import { SceneConfig } from 'scenes/sceneTypes'
-
-import { ProjectNotice } from './ProjectNotice'
 import { SideBar } from './SideBar/SideBar'
 
-export function Navigation({
-    children,
-    sceneConfig,
-}: {
-    children: ReactNode
-    sceneConfig: SceneConfig | null
-}): JSX.Element {
+export function Navigation(): JSX.Element {
     return (
-        <div className="h-screen flex flex-col">
-            <SideBar>
-                <div
-                    className={clsx(
-                        'main-app-content',
-                        sceneConfig?.layout === 'plain' && 'main-app-content--plain',
-                        sceneConfig?.layout === 'app-container' && 'main-app-content--container'
-                    )}
-                >
-                    {sceneConfig?.layout !== 'plain' && (
-                        <>
-                            <BillingAlertsV2 />
-                            {!sceneConfig?.hideProjectNotice && <ProjectNotice />}
-                        </>
-                    )}
-                    {children}
-                </div>
-            </SideBar>
-        </div>
+        <SideBar>
+            <div />
+        </SideBar>
     )
 }
