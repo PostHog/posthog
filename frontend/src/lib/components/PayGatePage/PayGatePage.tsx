ac4fa889341f8649eccd4132d043eb6d1b3c4ca6
Cory Watilo <corywatilo@gmail.com>
2024-01-03 22:22:32 +0000

    added features

--- frontend/src/lib/components/PayGatePage/PayGatePage.tsx
+++ frontend/src/lib/components/PayGatePage/PayGatePage.tsx
@@ -1,7 +1,6 @@
 import './PayGatePage.scss'
 
-import { IconOpenSidebar } from '@posthog/icons'
-import { IconCheckCircle } from '@posthog/icons'
+import { IconCheckCircle, IconOpenSidebar } from '@posthog/icons'
 import { useValues } from 'kea'
 import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { identifierToHuman } from 'lib/utils'
@@ -17,6 +16,45 @@ const AbTestingPaidFeatures = [
     'Statistical analysis',
     '7 year data retention',
 ]
+
+const AbTestingFeatures = [
+    {
+        name: 'Customizable goals',
+        description: 'Conversion funnels or trends, secondary metrics, and range for statistical significance',
+        // image: <StaticImage src="./images/goals.png" width={428} />,
+        image: 'https://posthog.com/images/products/ab-testing.png',
+    },
+    {
+        name: 'Targeting & exclusion rules',
+        description: 'Set criteria for user location, user property, cohort, or group',
+        // image: <StaticImage src="./images/targeting-ab.png" width={428} />,
+        image: 'https://posthog.com/images/products/ab-testing.png',
+    },
+    {
+        name: 'Recommendations',
+        description: 'Automatic suggestions for duration, sample size, and confidence threshold in a winning variant',
+        // image: <StaticImage src="./images/recommendations.png" width={428} />,
+        image: 'https://posthog.com/images/products/ab-testing.png',
+    },
+]
+
+interface FeatureProps {
+    name: string
+    description: string
+    image: any
+}
+
+export const Feature = ({ name, description, image }: FeatureProps): JSX.Element => {
+    return (
+        <li className="text-center">
+            <div className={`mb-2 w-full`}>
+                <img src={image} className="w-full" />
+            </div>
+            <h4 className="mb-1 leading-tight text-lg">{name}</h4>
+            <p className="text-[15px]" dangerouslySetInnerHTML={{ __html: description }} />
+        </li>
+    )
+}
 
 interface PayGatePageInterface {
     header: string | JSX.Element
@@ -72,11 +110,8 @@ export function PayGatePage({
                             with product analytics and session replay.
                         </p>
                     </div>
-                    <aside>
-                        <img
-                            src="https://posthog.com/static/2beccf3e3a0dc26babd46bff2358581e/70464/ab-testing-hog.webp"
-                            className="max-w-full"
-                        />
+                    <aside className="text-right">
+                        <img src="https://posthog.com/images/products/ab-testing.png" className="max-w-full" />
                     </aside>
                 </header>
                 <div className="flex gap-12 p-8">
@@ -94,25 +129,28 @@ export function PayGatePage({
                         <p>
                             <span className="">Show volume discounts</span>
                         </p>
-                        <LemonButton
-                            to={upgradeLink}
-                            type="primary"
-                            status="alt"
-                            data-attr={`${featureKey}-upgrade`}
-                            center
-                        >
-                            Set up {featureName} - free
-                        </LemonButton>
+                        <div className="flex">
+                            <LemonButton
+                                to={upgradeLink}
+                                type="primary"
+                                status="alt"
+                                data-attr={`${featureKey}-upgrade`}
+                                center
+                                className="self-start"
+                            >
+                                Set up {featureName} - free
+                            </LemonButton>
+                        </div>
                     </div>
                     <div className="shrink-0">
                         <p>
                             <strong>Use for free, or upgrade to get:</strong>
                         </p>
-                        <ul>
+                        <ul className="space-y-1">
                             {AbTestingPaidFeatures.map((feature, index) => (
-                                <li key={index} className="flex gap-1 items-center">
-                                    <span className="text-green">
-                                        <IconCheckCircle className="text-xl fill-current" />
+                                <li key={index} className="flex gap-1.5 items-center leading-0">
+                                    <span className="text-greeen">
+                                        <IconCheckCircle className="text-2xl fill-current" />
                                     </span>
                                     <span>{feature}</span>
                                 </li>
@@ -120,16 +158,12 @@ export function PayGatePage({
                         </ul>
                     </div>
                 </div>
-                <div className="features py-12">
+                <div className="features p-8 py-12">
                     <h3 className="mb-4">Features</h3>
-                    <ul className="grid grid-cols-3 gap-8">
-                        <li>feature 1</li>
-                        <li>feature 2</li>
-                        <li>feature 3</li>
-                        <li>feature 4</li>
-                        <li>feature 5</li>
-                        <li>feature 5</li>
-                        <li>feature 7</li>
+                    <ul className="list-none p-0 grid grid-cols-3 gap-8">
+                        {AbTestingFeatures.map((feature, index) => {
+                            return <Feature {...feature} key={index} />
+                        })}
                     </ul>
                 </div>
             </div>
