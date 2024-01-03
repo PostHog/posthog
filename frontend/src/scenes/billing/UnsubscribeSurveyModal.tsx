d840c0452b14f273b5194d08d5c998ff4104278e
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-01-03 13:39:13 -0800

    fix overlap

--- frontend/src/scenes/billing/UnsubscribeSurveyModal.tsx
+++ frontend/src/scenes/billing/UnsubscribeSurveyModal.tsx
@@ -35,7 +35,7 @@ export const UnsubscribeSurveyModal = ({
                 {includesPipelinesAddon && itemsToDisable.length > 0 ? (
                     <div className="mb-6">
                         <div className="mb-4">
-                            <h3 className="mt-2 mb-2">{`Important: Disable remaining export apps`}</h3>
+                            <h3 className="mt-2 mb-2 mr-8">{`Important: Disable remaining export apps`}</h3>
                             <p>
                                 To avoid unexpected impact on your data, you must explicitly disable the following apps
                                 and exports before unsubscribing:
@@ -46,7 +46,7 @@ export const UnsubscribeSurveyModal = ({
                 ) : (
                     <></>
                 )}
-                <h3 className="mt-2 mb-4">{`Why are you unsubscribing from ${product.name}?`}</h3>
+                <h3 className="mt-2 mb-4 mr-8">{`Why are you unsubscribing from ${product.name}?`}</h3>
                 <div className="flex flex-col gap-3.5">
                     <LemonTextArea
                         data-attr="unsubscribe-reason-survey-textarea"
