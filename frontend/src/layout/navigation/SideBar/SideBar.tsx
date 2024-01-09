dbcbf387e4fc0b7b85980aad5d9b7b5b26c07852
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-01-09 12:46:03 +0100

    fix(3000): re-implement debug notice (#19538)
    ---------
    
    Co-authored-by: Michael Matloka <michal@matloka.com>

--- frontend/src/layout/navigation/SideBar/SideBar.tsx
+++ frontend/src/layout/navigation/SideBar/SideBar.tsx
@@ -1,11 +1,9 @@
 import { ActivationSidebar } from 'lib/components/ActivationSidebar/ActivationSidebar'
-import { DebugNotice } from 'lib/components/DebugNotice'
 import { NotebookPopover } from 'scenes/notebooks/NotebookPanel/NotebookPopover'
 
 export function SideBar(): JSX.Element {
     return (
         <div>
-            <DebugNotice />
             <NotebookPopover />
             <ActivationSidebar />
         </div>
