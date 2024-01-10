d6cc49f4d290a8125a3dc390c3664511c0d68694
Brett Hoerner <brett@bretthoerner.com>
2024-01-10 08:53:09 -0700

    Sync default ports with charts (#33)

--- hook-janitor/src/config.rs
+++ hook-janitor/src/config.rs
@@ -5,7 +5,7 @@ pub struct Config {
     #[envconfig(from = "BIND_HOST", default = "0.0.0.0")]
     pub host: String,
 
-    #[envconfig(from = "BIND_PORT", default = "8000")]
+    #[envconfig(from = "BIND_PORT", default = "3302")]
     pub port: u16,
 
     #[envconfig(default = "postgres://posthog:posthog@localhost:15432/test_database")]
--- hook-worker/src/config.rs
+++ hook-worker/src/config.rs
@@ -8,7 +8,7 @@ pub struct Config {
     #[envconfig(from = "BIND_HOST", default = "0.0.0.0")]
     pub host: String,
 
-    #[envconfig(from = "BIND_PORT", default = "8001")]
+    #[envconfig(from = "BIND_PORT", default = "3301")]
     pub port: u16,
 
     #[envconfig(default = "postgres://posthog:posthog@localhost:15432/test_database")]
