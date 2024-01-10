d6e866f711dd891cb0479593547f4429d419049e
Julian Bez <julian@posthog.com>
2024-01-10 09:24:52 +0100

    Adjust test

--- posthog/api/test/test_query.py
+++ posthog/api/test/test_query.py
@@ -696,7 +696,7 @@ class TestQuery(ClickhouseTestMixin, APIBaseTest):
         api_response = self.client.post(f"/api/projects/{self.team.id}/query/", {"query": {"kind": "Tomato Soup"}})
         assert api_response.status_code == 400
         assert api_response.json()["code"] == "parse_error"
-        assert "validation errors for QuerySchema" in api_response.json()["detail"]
+        assert "1 validation error for QueryRequest" in api_response.json()["detail"]
         assert "type=literal_error, input_value='Tomato Soup'" in api_response.json()["detail"]
 
     @snapshot_clickhouse_queries
