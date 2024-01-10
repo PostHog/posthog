8270401f75a24d2c192eda103e5ffc6a3dc89632
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-01-10 18:07:30 +0100

    use log levels instead of types

--- frontend/src/scenes/plugins/plugin/PluginLogs.tsx
+++ frontend/src/scenes/plugins/plugin/PluginLogs.tsx
@@ -4,25 +4,25 @@ import { LOGS_PORTION_LIMIT } from 'lib/constants'
 import { dayjs } from 'lib/dayjs'
 import { pluralize } from 'lib/utils'
 
-import { PluginLogEntryType } from '../../../types'
+import { BatchExportLogEntryLevel } from '../../../types'
 import { pluginLogsLogic, PluginLogsProps } from './pluginLogsLogic'
 
-function PluginLogEntryTypeDisplay(type: PluginLogEntryType): JSX.Element {
+function LogLevelDisplay(type: BatchExportLogEntryLevel): JSX.Element {
     let color: string | undefined
     switch (type) {
-        case PluginLogEntryType.Debug:
+        case BatchExportLogEntryLevel.Debug:
             color = 'text-muted'
             break
-        case PluginLogEntryType.Log:
+        case BatchExportLogEntryLevel.Log:
             color = 'text-default'
             break
-        case PluginLogEntryType.Info:
+        case BatchExportLogEntryLevel.Info:
             color = 'text-primary'
             break
-        case PluginLogEntryType.Warn:
+        case BatchExportLogEntryLevel.Warning:
             color = 'text-warning'
             break
-        case PluginLogEntryType.Error:
+        case BatchExportLogEntryLevel.Error:
             color = 'text-danger'
             break
         default:
@@ -44,10 +44,10 @@ const columns: LemonTableColumns<Record<string, any>> = [
         key: 'source',
     },
     {
-        title: 'Type',
-        key: 'type',
-        dataIndex: 'type',
-        render: PluginLogEntryTypeDisplay,
+        title: 'Level',
+        key: 'level',
+        dataIndex: 'level',
+        render: LogLevelDisplay,
     },
     {
         title: 'Message',
@@ -60,8 +60,9 @@ const columns: LemonTableColumns<Record<string, any>> = [
 export function PluginLogs({ id, kind }: PluginLogsProps): JSX.Element {
     const logic = pluginLogsLogic({ id, kind })
 
-    const { pluginLogs, pluginLogsLoading, pluginLogsBackground, isThereMoreToLoad, pluginLogsTypes } = useValues(logic)
-    const { revealBackground, loadPluginLogsMore, setPluginLogsTypes, setSearchTerm } = useActions(logic)
+    const { pluginLogs, pluginLogsLoading, pluginLogsBackground, isThereMoreToLoad, selectedLogLevels } =
+        useValues(logic)
+    const { revealBackground, loadPluginLogsMore, setSelectedLogLevels, setSearchTerm } = useActions(logic)
 
     return (
         <div className="ph-no-capture space-y-2 flex-1">
@@ -74,17 +75,17 @@ export function PluginLogs({ id, kind }: PluginLogsProps): JSX.Element {
             />
             <div className="flex items-center gap-4">
                 <span>Show logs of type:&nbsp;</span>
-                {Object.values(PluginLogEntryType).map((type) => {
+                {Object.values(BatchExportLogEntryLevel).map((level) => {
                     return (
                         <LemonCheckbox
-                            key={type}
-                            label={type}
-                            checked={pluginLogsTypes.includes(type)}
+                            key={level}
+                            label={level}
+                            checked={selectedLogLevels.includes(level)}
                             onChange={(checked) => {
-                                const newPluginLogsTypes = checked
-                                    ? [...pluginLogsTypes, type]
-                                    : pluginLogsTypes.filter((t) => t != type)
-                                setPluginLogsTypes(newPluginLogsTypes)
+                                const newLogLevels = checked
+                                    ? [...selectedLogLevels, level]
+                                    : selectedLogLevels.filter((t) => t != level)
+                                setSelectedLogLevels(newLogLevels)
                             }}
                         />
                     )
