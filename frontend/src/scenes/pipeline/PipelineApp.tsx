02c656aed0954b469be05b9cdae91d7147917a91
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-01-10 17:39:52 +0100

    re-use paramsToProps function for scene component

--- frontend/src/scenes/pipeline/PipelineApp.tsx
+++ frontend/src/scenes/pipeline/PipelineApp.tsx
@@ -15,26 +15,29 @@ import { PipelineAppTabs, PipelineTabs } from '~/types'
 
 import { AppMetrics } from './AppMetrics'
 import { PipelineAppConfiguration } from './PipelineAppConfiguration'
-import { pipelineAppLogic } from './pipelineAppLogic'
+import { pipelineAppLogic, PipelineAppLogicProps } from './pipelineAppLogic'
+
+const paramsToProps = ({ params: { kind, id } }: { params: { kind?: string; id?: string } }): PipelineAppLogicProps => {
+    const numericId = id && /^\d+$/.test(id) ? parseInt(id) : undefined
+    return {
+        kind: (kind as PipelineTabs) || PipelineTabs.Destinations,
+        id: (numericId && !isNaN(numericId) ? numericId : id) || 'missing',
+    }
+}
 
 export const scene: SceneExport = {
     component: PipelineApp,
     logic: pipelineAppLogic,
-    paramsToProps: ({ params: { kind, id } }: { params: { kind?: string; id?: string } }) => {
-        const numericId = id ? parseInt(id) : undefined
-        return {
-            kind: kind,
-            id: numericId && !isNaN(numericId) ? numericId : id,
-        }
-    },
+    paramsToProps,
 }
 
-export function PipelineApp({ kind, id }: { kind?: string; id?: string | number } = {}): JSX.Element {
+export function PipelineApp(params: { kind?: string; id?: string } = {}): JSX.Element {
+    const { kind, id } = paramsToProps({ params })
     const { featureFlags } = useValues(featureFlagLogic)
     if (!featureFlags[FEATURE_FLAGS.PIPELINE_UI]) {
         return <p>Pipeline 3000 not available yet</p>
     }
-    if (!Object.values(PipelineTabs).includes(kind as PipelineTabs)) {
+    if (!Object.values(PipelineTabs).includes(kind)) {
         return <NotFound object="pipeline app" />
     }
     const { currentTab } = useValues(pipelineAppLogic)
@@ -46,7 +49,7 @@ export function PipelineApp({ kind, id }: { kind?: string; id?: string | number 
     const tabToContent: Record<PipelineAppTabs, JSX.Element> = {
         [PipelineAppTabs.Configuration]: <PipelineAppConfiguration />,
         [PipelineAppTabs.Metrics]: <AppMetrics pluginConfigId={id as number} />,
-        [PipelineAppTabs.Logs]: <PluginLogs id={id} kind={kind as PipelineTabs} />,
+        [PipelineAppTabs.Logs]: <PluginLogs id={id} kind={kind} />,
     }
 
     return (
@@ -54,9 +57,7 @@ export function PipelineApp({ kind, id }: { kind?: string; id?: string | number 
             <PageHeader />
             <LemonTabs
                 activeKey={currentTab}
-                onChange={(tab) =>
-                    router.actions.push(urls.pipelineApp(kind as PipelineTabs, id, tab as PipelineAppTabs))
-                }
+                onChange={(tab) => router.actions.push(urls.pipelineApp(kind, id, tab as PipelineAppTabs))}
                 tabs={Object.values(PipelineAppTabs).map((tab) => ({
                     label: capitalizeFirstLetter(tab),
                     key: tab,
