40ecf80022ca8cc918146d543d8f469b8c6b8905
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-01-11 11:40:46 +0100

    refactor pipelineAppLogsLogic

--- frontend/src/scenes/pipeline/pipelineAppLogsLogic.tsx
+++ frontend/src/scenes/pipeline/pipelineAppLogsLogic.tsx
@@ -13,6 +13,8 @@ import { teamLogic } from '../teamLogic'
 import type { pipelineAppLogsLogicType } from './pipelineAppLogsLogicType'
 import { LogLevelDisplay, logLevelsToTypeFilters, LogTypeDisplay } from './utils'
 
+type LogEntry = BatchExportLogEntry | PluginLogEntry
+
 export enum PipelineAppLogLevel {
     Debug = 'DEBUG',
     Log = 'LOG',
@@ -29,23 +31,23 @@ export interface PipelineAppLogsProps {
 export const pipelineAppLogsLogic = kea<pipelineAppLogsLogicType>([
     props({} as PipelineAppLogsProps),
     key(({ id }: PipelineAppLogsProps) => id),
-    path((key) => ['scenes', 'plugins', 'plugin', 'pluginLogsLogic', key]),
+    path((key) => ['scenes', 'pipeline', 'pipelineAppLogsLogic', key]),
     connect((props: PipelineAppLogsProps) => ({
         values: [teamLogic, ['currentTeamId'], pipelineAppLogic(props), ['appType']],
     })),
     actions({
-        clearPluginLogsBackground: true,
-        markLogsEnd: true,
         setSelectedLogLevels: (levels: PipelineAppLogLevel[]) => ({
             levels,
         }),
         setSearchTerm: (searchTerm: string) => ({ searchTerm }),
+        clearBackgroundLogs: true,
+        markLogsEnd: true,
     }),
     loaders(({ props: { id }, values, actions, cache }) => ({
         logs: {
-            __default: [] as BatchExportLogEntry[] | PluginLogEntry[],
+            __default: [] as LogEntry[],
             loadLogs: async () => {
-                let results: BatchExportLogEntry[] | PluginLogEntry[]
+                let results: LogEntry[]
                 if (values.appType === DestinationTypeKind.BatchExport) {
                     results = await api.batchExportLogs.search(
                         id as string,
@@ -63,12 +65,12 @@ export const pipelineAppLogsLogic = kea<pipelineAppLogsLogicType>([
                 }
 
                 if (!cache.pollingInterval) {
-                    cache.pollingInterval = setInterval(actions.loadPluginLogsBackgroundPoll, 2000)
+                    cache.pollingInterval = setInterval(actions.pollBackgroundLogs, 5000)
                 }
-                actions.clearPluginLogsBackground()
+                actions.clearBackgroundLogs()
                 return results
             },
-            loadPluginLogsMore: async () => {
+            loadMoreLogs: async () => {
                 const results = await api.pluginLogs.search(
                     id,
                     values.currentTeamId,
@@ -83,16 +85,18 @@ export const pipelineAppLogsLogic = kea<pipelineAppLogsLogicType>([
                 return [...values.logs, ...results]
             },
             revealBackground: () => {
-                const newArray = [...values.pluginLogsBackground, ...values.logs]
-                actions.clearPluginLogsBackground()
+                const newArray = [...values.backgroundLogs, ...values.logs]
+                actions.clearBackgroundLogs()
                 return newArray
             },
         },
-        pluginLogsBackground: {
-            __default: [] as PluginLogEntry[],
-            loadPluginLogsBackgroundPoll: async () => {
+        backgroundLogs: {
+            __default: [] as LogEntry[],
+            pollBackgroundLogs: async () => {
+                // we fetch new logs in the background and allow the user to expand
+                // them into the array of visible logs
                 if (values.logsLoading) {
-                    return values.pluginLogsBackground
+                    return values.backgroundLogs
                 }
 
                 const results = await api.pluginLogs.search(
@@ -104,7 +108,7 @@ export const pipelineAppLogsLogic = kea<pipelineAppLogsLogicType>([
                     values.leadingEntry
                 )
 
-                return [...results, ...values.pluginLogsBackground]
+                return [...results, ...values.backgroundLogs]
             },
         },
     })),
@@ -115,10 +119,10 @@ export const pipelineAppLogsLogic = kea<pipelineAppLogsLogicType>([
                 setSelectedLogLevels: (_, { levels }) => levels,
             },
         ],
-        pluginLogsBackground: [
+        backgroundLogs: [
             [] as PluginLogEntry[],
             {
-                clearPluginLogsBackground: () => [],
+                clearBackgroundLogs: () => [],
             },
         ],
         searchTerm: [
@@ -135,27 +139,27 @@ export const pipelineAppLogsLogic = kea<pipelineAppLogsLogicType>([
             },
         ],
     }),
-    selectors(({ selectors }) => ({
+    selectors({
         leadingEntry: [
-            () => [selectors.logs, selectors.pluginLogsBackground],
-            (pluginLogs: PluginLogEntry[], pluginLogsBackground: PluginLogEntry[]): PluginLogEntry | null => {
-                if (pluginLogsBackground.length) {
-                    return pluginLogsBackground[0]
+            (s) => [s.logs, s.backgroundLogs],
+            (logs: LogEntry[], backgroundLogs: LogEntry[]): LogEntry | null => {
+                if (backgroundLogs.length) {
+                    return backgroundLogs[0]
                 }
-                if (pluginLogs.length) {
-                    return pluginLogs[0]
+                if (logs.length) {
+                    return logs[0]
                 }
                 return null
             },
         ],
         trailingEntry: [
-            () => [selectors.logs, selectors.pluginLogsBackground],
-            (pluginLogs: PluginLogEntry[], pluginLogsBackground: PluginLogEntry[]): PluginLogEntry | null => {
-                if (pluginLogs.length) {
-                    return pluginLogs[pluginLogs.length - 1]
+            (s) => [s.logs, s.backgroundLogs],
+            (logs: LogEntry[], backgroundLogs: LogEntry[]): LogEntry | null => {
+                if (logs.length) {
+                    return logs[logs.length - 1]
                 }
-                if (pluginLogsBackground.length) {
-                    return pluginLogsBackground[pluginLogsBackground.length - 1]
+                if (backgroundLogs.length) {
+                    return backgroundLogs[backgroundLogs.length - 1]
                 }
                 return null
             },
@@ -190,7 +194,7 @@ export const pipelineAppLogsLogic = kea<pipelineAppLogsLogicType>([
                 ]
             },
         ],
-    })),
+    }),
     listeners(({ actions }) => ({
         setSelectedLogLevels: () => {
             actions.loadLogs()
