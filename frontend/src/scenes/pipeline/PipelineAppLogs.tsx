98786a2445c161774122326bd02d50a2bd3d0179
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-01-11 10:08:05 +0100

    handle both log types

--- frontend/src/scenes/pipeline/PipelineAppLogs.tsx
+++ frontend/src/scenes/pipeline/PipelineAppLogs.tsx
@@ -1,65 +1,14 @@
-import { LemonButton, LemonCheckbox, LemonInput, LemonTable, LemonTableColumns } from '@posthog/lemon-ui'
+import { LemonButton, LemonCheckbox, LemonInput, LemonTable } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
 import { LOGS_PORTION_LIMIT } from 'lib/constants'
-import { dayjs } from 'lib/dayjs'
 import { pluralize } from 'lib/utils'
 
 import { PipelineAppLogLevel, pipelineAppLogsLogic, PipelineAppLogsProps } from './pipelineAppLogsLogic'
 
-function LogLevelDisplay(type: PipelineAppLogLevel): JSX.Element {
-    let color: string | undefined
-    switch (type) {
-        case PipelineAppLogLevel.Debug:
-            color = 'text-muted'
-            break
-        case PipelineAppLogLevel.Log:
-            color = 'text-default'
-            break
-        case PipelineAppLogLevel.Info:
-            color = 'text-primary'
-            break
-        case PipelineAppLogLevel.Warning:
-            color = 'text-warning'
-            break
-        case PipelineAppLogLevel.Error:
-            color = 'text-danger'
-            break
-        default:
-            break
-    }
-    return <span className={color}>{type}</span>
-}
-
-const columns: LemonTableColumns<Record<string, any>> = [
-    {
-        title: 'Timestamp',
-        key: 'timestamp',
-        dataIndex: 'timestamp',
-        render: (timestamp: string) => dayjs(timestamp).format('YYYY-MM-DD HH:mm:ss.SSS UTC'),
-    },
-    {
-        title: 'Source',
-        dataIndex: 'source',
-        key: 'source',
-    },
-    {
-        title: 'Level',
-        key: 'level',
-        dataIndex: 'level',
-        render: LogLevelDisplay,
-    },
-    {
-        title: 'Message',
-        key: 'message',
-        dataIndex: 'message',
-        render: (message: string) => <code className="whitespace-pre-wrap">{message}</code>,
-    },
-]
-
 export function PipelineAppLogs({ id, kind }: PipelineAppLogsProps): JSX.Element {
     const logic = pipelineAppLogsLogic({ id, kind })
 
-    const { pluginLogs, pluginLogsLoading, pluginLogsBackground, isThereMoreToLoad, selectedLogLevels } =
+    const { pluginLogs, pluginLogsLoading, pluginLogsBackground, columns, isThereMoreToLoad, selectedLogLevels } =
         useValues(logic)
     const { revealBackground, loadPluginLogsMore, setSelectedLogLevels, setSearchTerm } = useActions(logic)
 
