05a67868c339e478526f4786485126ee2c5147f4
Marcus Hof <13001502+MarconLP@users.noreply.github.com>
2024-01-16 16:29:33 +0100

    Update frontend/src/scenes/settings/project/ProjectSettings.tsx
    Co-authored-by: Ben White <ben@posthog.com>

--- frontend/src/scenes/settings/project/ProjectSettings.tsx
+++ frontend/src/scenes/settings/project/ProjectSettings.tsx
@@ -150,7 +150,7 @@ export function ProjectVariables(): JSX.Element {
                         Project Region
                     </h3>
                     <p>This is the region where your PostHog data is hosted.</p>
-                    <CodeSnippet thing="project ID">{`${region} Cloud`}</CodeSnippet>
+                    <CodeSnippet thing="project region">{`${region} Cloud`}</CodeSnippet>
                 </div>
             ) : null}
         </div>
