6b22e4bcc20227c50a52ae4f3f2de926d4bc6285
Ben White <ben@posthog.com>
2024-01-17 09:16:10 +0100

    Fix?

--- frontend/src/lib/components/ProductIntroduction/ProductIntroduction.stories.tsx
+++ frontend/src/lib/components/ProductIntroduction/ProductIntroduction.stories.tsx
@@ -12,53 +12,61 @@ export default meta
 
 export function ProductIntroduction_(): JSX.Element {
     return (
-        <ProductIntroduction
-            productName="Cohorts"
-            productKey={ProductKey.COHORTS}
-            thingName="cohort"
-            description="Use cohorts to group people together, such as users who used your app in the last week, or people who viewed the signup page but didn’t convert."
-            docsURL="https://posthog.com/docs/data/cohorts"
-            action={() => alert('You clicked the button!')}
-            isEmpty={true}
-        />
+        <div className="w-full">
+            <ProductIntroduction
+                productName="Cohorts"
+                productKey={ProductKey.COHORTS}
+                thingName="cohort"
+                description="Use cohorts to group people together, such as users who used your app in the last week, or people who viewed the signup page but didn’t convert."
+                docsURL="https://posthog.com/docs/data/cohorts"
+                action={() => alert('You clicked the button!')}
+                isEmpty={true}
+            />
+        </div>
     )
 }
 
 export function emptyWithAction(): JSX.Element {
     return (
-        <ProductIntroduction
-            productName="Cohorts"
-            productKey={ProductKey.COHORTS}
-            thingName="cohort"
-            description="Use cohorts to group people together, such as users who used your app in the last week, or people who viewed the signup page but didn’t convert."
-            docsURL="https://posthog.com/docs/data/cohorts"
-            action={() => alert('You clicked the button!')}
-            isEmpty={true}
-        />
+        <div className="w-full">
+            <ProductIntroduction
+                productName="Cohorts"
+                productKey={ProductKey.COHORTS}
+                thingName="cohort"
+                description="Use cohorts to group people together, such as users who used your app in the last week, or people who viewed the signup page but didn’t convert."
+                docsURL="https://posthog.com/docs/data/cohorts"
+                action={() => alert('You clicked the button!')}
+                isEmpty={true}
+            />
+        </div>
     )
 }
 
 export function emptyNoAction(): JSX.Element {
     return (
-        <ProductIntroduction
-            productName="Feature Flags"
-            productKey={ProductKey.FEATURE_FLAGS}
-            thingName="history record"
-            description={`History shows any feature flag changes that have been made. After making changes you'll see them logged here.`}
-        />
+        <div className="w-full">
+            <ProductIntroduction
+                productName="Feature Flags"
+                productKey={ProductKey.FEATURE_FLAGS}
+                thingName="history record"
+                description={`History shows any feature flag changes that have been made. After making changes you'll see them logged here.`}
+            />
+        </div>
     )
 }
 
 export function notEmptyWithAction(): JSX.Element {
     return (
-        <ProductIntroduction
-            productName="Cohorts"
-            productKey={ProductKey.COHORTS}
-            thingName="cohort"
-            description="Use cohorts to group people together, such as users who used your app in the last week, or people who viewed the signup page but didn’t convert."
-            docsURL="https://posthog.com/docs/data/cohorts"
-            action={() => alert('You clicked the button!')}
-            isEmpty={false}
-        />
+        <div className="w-full">
+            <ProductIntroduction
+                productName="Cohorts"
+                productKey={ProductKey.COHORTS}
+                thingName="cohort"
+                description="Use cohorts to group people together, such as users who used your app in the last week, or people who viewed the signup page but didn’t convert."
+                docsURL="https://posthog.com/docs/data/cohorts"
+                action={() => alert('You clicked the button!')}
+                isEmpty={false}
+            />
+        </div>
     )
 }
