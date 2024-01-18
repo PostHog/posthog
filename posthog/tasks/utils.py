4841918e33aec99c2b7a786b26b7f7355c9b335c
Ben White <ben@posthog.com>
2024-01-18 11:19:36 +0100

    Fix

--- posthog/tasks/utils.py
+++ posthog/tasks/utils.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
 
+# NOTE: Keep in sync with bin/celery-queues.env
 class CeleryQueue(Enum):
     DEFAULT = "celery"
     EMAIL = "email"
     INSIGHT_EXPORT = "insight_export"
     INSIGHT_REFRESH = "insight_refresh"
     ANALYTICS_QUERIES = "analytics_queries"
+    # EXPORTS = "exports"
