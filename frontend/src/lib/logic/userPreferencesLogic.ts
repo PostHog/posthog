c5aaa97cdd592718838ed126c7c50ab4aa3af9f0
Ben White <ben@posthog.com>
2024-01-18 16:58:41 +0100

    Move the logic

--- frontend/src/lib/logic/userPreferencesLogic.ts
+++ frontend/src/lib/logic/userPreferencesLogic.ts
@@ -5,7 +5,7 @@ import type { userPreferencesLogicType } from './userPreferencesLogicType'
 // This logic contains player settings that should persist across players
 // If key is not specified, it is global so it does not reset when recordings change in the main recordings page
 export const userPreferencesLogic = kea<userPreferencesLogicType>([
-    path(['scenes', 'userPreferencesLogic']),
+    path(['lib', 'logic', 'userPreferencesLogic']),
     actions({
         setHidePostHogPropertiesInTable: (enabled: boolean) => ({ enabled }),
     }),
