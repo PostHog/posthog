8d9445f94cea07ba66274422f2afeff61c9238fc
Ben White <ben@posthog.com>
2024-01-18 16:54:42 +0100

    Added userPreferencesLogic for saving local settings

---
+++ frontend/src/scenes/userPreferencesLogic.ts
@@ -0,0 +1,21 @@
+import { actions, kea, path, reducers } from 'kea'
+
+import type { userPreferencesLogicType } from './userPreferencesLogicType'
+
+// This logic contains player settings that should persist across players
+// If key is not specified, it is global so it does not reset when recordings change in the main recordings page
+export const userPreferencesLogic = kea<userPreferencesLogicType>([
+    path(['scenes', 'userPreferencesLogic']),
+    actions({
+        setHidePostHogPropertiesInTable: (enabled: boolean) => ({ enabled }),
+    }),
+    reducers(() => ({
+        hidePostHogPropertiesInTable: [
+            false,
+            { persist: true },
+            {
+                setHidePostHogPropertiesInTable: (_, { enabled }) => enabled,
+            },
+        ],
+    })),
+])
