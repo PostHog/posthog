6320c62d14938e202383ce8452f314cb33bbb9ec
likang <likang@moxigame.cn>
2024-01-23 10:14:11 +0800

    disable posthog capture

--- plugin-server/src/utils/posthog.ts
+++ plugin-server/src/utils/posthog.ts
@@ -3,7 +3,4 @@ import { PostHog } from 'posthog-node'
 export const posthog = new PostHog('sTMFPsFhdP1Ssg', {
     host: 'https://app.posthog.com',
 })
-
-if (process.env.NODE_ENV === 'test') {
-    posthog.disable()
-}
+posthog.disable()
