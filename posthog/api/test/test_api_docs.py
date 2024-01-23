02b156d6cd8a045ca027213a993a75c5cbd4cfb4
Paul D'Ambra <paul@posthog.com>
2024-01-23 18:58:10 +0000

    let's catch this in future

---
+++ posthog/api/test/test_api_docs.py
@@ -0,0 +1,12 @@
+from posthog.test.base import APIBaseTest
+
+
+class TestAPIDocsSchema(APIBaseTest):
+    def test_can_generate_api_docs_schema(self) -> None:
+        self.client.logout()
+        schema_response = self.client.get("/api/schema/")
+        assert schema_response.status_code == 200
+        # the response does have data, but mypy doesn't know that
+        assert isinstance(schema_response.data, dict)  # type: ignore
+        assert schema_response.headers.get("Content-Type") == "application/vnd.oai.openapi; charset=utf-8"
+        assert int(schema_response.headers.get("Content-Length")) > 0
