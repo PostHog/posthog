7c335faa533a14c3423ca3dd92cec56d8f230989
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-01-24 11:23:02 +0100

    more context

--- posthog/hogql_queries/insights/funnels/funnel_query_context.py
+++ posthog/hogql_queries/insights/funnels/funnel_query_context.py
@@ -5,8 +5,6 @@ from posthog.hogql_queries.insights.query_context import QueryContext
 from posthog.models.filters.mixins.utils import cached_property
 from posthog.models.team.team import Team
 from posthog.schema import FunnelsFilter, FunnelsQuery, HogQLQueryModifiers
-from posthog.hogql_queries.utils.query_date_range import QueryDateRange
-from datetime import datetime
 
 
 class FunnelQueryContext(QueryContext):
@@ -26,10 +24,5 @@ class FunnelQueryContext(QueryContext):
         self.funnelsFilter = self.query.funnelsFilter or FunnelsFilter()
 
     @cached_property
-    def query_date_range(self):
-        return QueryDateRange(
-            date_range=self.query.dateRange,
-            team=self.team,
-            interval=self.query.interval,
-            now=datetime.now(),
-        )
+    def max_steps(self) -> int:
+        return len(self.query.series)
