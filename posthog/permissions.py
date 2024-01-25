05a3c84ff2dab452f5a0bf043485813aa05492cd
Juraj Majerik <juro.majerik@gmail.com>
2024-01-25 10:54:16 +0100

    revert back to available_features

--- posthog/permissions.py
+++ posthog/permissions.py
@@ -226,7 +226,7 @@ class PremiumFeaturePermission(BasePermission):
         if not request.user or not request.user.organization:  # type: ignore
             return True
 
-        if view.premium_feature not in request.user.organization.available_product_features:  # type: ignore
+        if view.premium_feature not in request.user.organization.available_features:  # type: ignore
             raise EnterpriseFeatureException()
 
         return True
