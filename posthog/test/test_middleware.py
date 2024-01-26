380797b6c33ddee1039da928821b2b03358a8f95
Ben White <ben@posthog.com>
2024-01-26 12:46:17 +0100

    Fix

--- posthog/test/test_middleware.py
+++ posthog/test/test_middleware.py
@@ -119,7 +119,7 @@ class TestAutoProjectMiddleware(APIBaseTest):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
-        cls.base_app_num_queries = 41
+        cls.base_app_num_queries = 42
         # Create another team that the user does have access to
         cls.second_team = create_team(organization=cls.organization, name="Second Life")
         other_org = create_organization(name="test org")
