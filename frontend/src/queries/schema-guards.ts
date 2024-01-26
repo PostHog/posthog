be626f69bf887246591caee0ad2ba61c6ddc0aab
Robbie <robbie.coomber@gmail.com>
2024-01-26 11:02:23 +0000

    fix(web-analytics): validate web analytics filters from url (#19967)
    * fix(web-analytics): validate web analytics filters from url
    
    * Hide warning
    
    * Import order

---
+++ frontend/src/queries/schema-guards.ts
@@ -0,0 +1,17 @@
+import Ajv from 'ajv'
+
+import { WebAnalyticsPropertyFilters } from '~/queries/schema'
+
+import schema from './schema.json'
+const ajv = new Ajv({
+    allowUnionTypes: true,
+})
+ajv.addSchema(schema)
+
+export const isWebAnalyticsPropertyFilters = (data: unknown): data is WebAnalyticsPropertyFilters => {
+    const validator = ajv.getSchema('#/definitions/WebAnalyticsPropertyFilters')
+    if (!validator) {
+        throw new Error('Could not find validator for WebAnalyticsPropertyFilters')
+    }
+    return validator(data) as boolean
+}
