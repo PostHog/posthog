d8454fb73bb595cb875d9165a526be51fedc2b7e
Michael Matloka <michal@matloka.com>
2024-01-30 14:22:57 +0100

    Refactor `MoveToPostHogCloud` a bit

--- frontend/src/scenes/moveToPostHogCloud/MoveToPostHogCloud.tsx
+++ frontend/src/scenes/moveToPostHogCloud/MoveToPostHogCloud.tsx
@@ -11,15 +11,11 @@ import {
     IconUpload,
 } from '@posthog/icons'
 import { LemonButton, Link } from '@posthog/lemon-ui'
-import { useValues } from 'kea'
-import { router } from 'kea-router'
 import { ExperimentsHog } from 'lib/components/hedgehogs'
-import { preflightLogic } from 'scenes/PreflightCheck/preflightLogic'
 import { SceneExport } from 'scenes/sceneTypes'
-import { urls } from 'scenes/urls'
 
 export const scene: SceneExport = {
-    component: moveToPostHogCloud,
+    component: MoveToPostHogCloud,
 }
 
 type CloudFeature = {
@@ -29,80 +25,74 @@ type CloudFeature = {
     link?: string
 }
 
-export function moveToPostHogCloud(): JSX.Element {
-    const { isCloudOrDev } = useValues(preflightLogic)
-    if (isCloudOrDev) {
-        router.actions.push(urls.default())
-    }
-    const cloudFeatures: CloudFeature[] = [
-        {
-            name: 'Hosted for you',
-            description: "No need to worry about servers, databases, or data ingestion. We've got it all covered.",
-            icon: <IconDatabase />,
-        },
-        {
-            name: 'EU and US data centers',
-            description: 'Host your data in the EU or US, whichever works best for your customer base.',
-            icon: <IconFlag />,
-        },
-        {
-            name: 'Easy migration',
-            description:
-                "We've done this before. It's just a few clicks to get your data moving from self-hosted to Cloud.",
-            icon: <IconUpload />,
-            link: 'https://posthog.com/docs/migrate/migrate-to-cloud',
-        },
-        {
-            name: 'Auto-scaling',
-            description:
-                'As your product grows, so does your data. PostHog Cloud scales for you, so you never have to worry about spikes or downtime.',
-            icon: <IconTrending />,
-        },
-        {
-            name: 'Highly available',
-            description:
-                'PostHog Cloud is highly available, so you can rest easy knowing your data is always accessible.',
-            icon: <IconBolt />,
-        },
-        {
-            name: 'Automatic upgrades',
-            description:
-                'PostHog Cloud is always up to date with the latest features and security updates - no upgrades required.',
-            icon: <IconUpload />,
-        },
-        {
-            name: 'Automatic backups',
-            description: "Don't worry about backups - we've got it covered.",
-            icon: <IconLock />,
-        },
-        {
-            name: 'Access to all features',
-            description:
-                'Group analytics, data pipelines, A/B testing, and other premium features are only available on PostHog Cloud.',
-            icon: <IconFeatures />,
-            link: 'https://posthog.com/pricing',
-        },
-        {
-            name: 'World-class support',
-            description:
-                'PostHog Cloud customers get access to our world-class support team, not just the community forum.',
-            icon: <IconSupport />,
-            link: 'https://posthog.com/handbook/growth/customer-support',
-        },
-        {
-            name: 'SOC 2 compliant',
-            description: "We're SOC-2 compliant, so you can rest easy knowing your data is secure.",
-            icon: <IconPrivacy />,
-            link: 'https://posthog.com/handbook/company/security',
-        },
-        {
-            name: 'HIPAA compliant',
-            description:
-                "Rest easy knowing your customers' data is secure.",
-            icon: <IconHeart />,
-        },
-    ]
+const CLOUD_FEATURES: CloudFeature[] = [
+    {
+        name: 'Hosted for you',
+        description: "No need to worry about servers, databases, or data ingestion. We've got it all covered.",
+        icon: <IconDatabase />,
+    },
+    {
+        name: 'EU and US data centers',
+        description: 'Host your data in the EU or US, whichever works best for your customer base.',
+        icon: <IconFlag />,
+    },
+    {
+        name: 'Easy migration',
+        description:
+            "We've done this before. It's just a few clicks to get your data moving from self-hosted to Cloud.",
+        icon: <IconUpload />,
+        link: 'https://posthog.com/docs/migrate/migrate-to-cloud',
+    },
+    {
+        name: 'Auto-scaling',
+        description:
+            'As your product grows, so does your data. PostHog Cloud scales for you, so you never have to worry about spikes or downtime.',
+        icon: <IconTrending />,
+    },
+    {
+        name: 'Highly available',
+        description: 'PostHog Cloud is highly available, so you can rest easy knowing your data is always accessible.',
+        icon: <IconBolt />,
+    },
+    {
+        name: 'Automatic upgrades',
+        description:
+            'PostHog Cloud is always up to date with the latest features and security updates - no upgrades required.',
+        icon: <IconUpload />,
+    },
+    {
+        name: 'Automatic backups',
+        description: "Don't worry about backups - we've got it covered.",
+        icon: <IconLock />,
+    },
+    {
+        name: 'Access to all features',
+        description:
+            'Group analytics, data pipelines, A/B testing, and other premium features are only available on PostHog Cloud.',
+        icon: <IconFeatures />,
+        link: 'https://posthog.com/pricing',
+    },
+    {
+        name: 'World-class support',
+        description:
+            'PostHog Cloud customers get access to our world-class support team, not just the community forum.',
+        icon: <IconSupport />,
+        link: 'https://posthog.com/handbook/growth/customer-support',
+    },
+    {
+        name: 'SOC 2 compliant',
+        description: "We're SOC-2 compliant, so you can rest easy knowing your data is secure.",
+        icon: <IconPrivacy />,
+        link: 'https://posthog.com/handbook/company/security',
+    },
+    {
+        name: 'HIPAA compliant',
+        description: "Rest easy knowing your customers' data is secure.",
+        icon: <IconHeart />,
+    },
+]
 
+export function MoveToPostHogCloud(): JSX.Element {
     return (
         <div className="-m-4">
             <header className="bg-primary-alt-highlight border-b border-t border-border flex justify-center p-8">
@@ -137,7 +127,7 @@ export function moveToPostHogCloud(): JSX.Element {
                 <div className="max-w-screen-xl">
                     <h3 className="mb-6 text-2xl font-bold">Features</h3>
                     <ul className="list-none p-0 grid sm:grid-cols-2 md:grid-cols-3 gap-8 mb-8 ">
-                        {cloudFeatures.map((feature, i) => {
+                        {CLOUD_FEATURES.map((feature, i) => {
                             return (
                                 <li
                                     className="rounded-lg p-4 sm:p-6 sm:pb-8 bg-primary-alt-highlight"
