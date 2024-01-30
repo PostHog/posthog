d51a725c0ac786ee18c82eada2c76345f90ded4a
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-01-30 12:15:43 +0100

    fix: BigQuery batch exports supports iter_records

--- posthog/temporal/batch_exports/bigquery_batch_export.py
+++ posthog/temporal/batch_exports/bigquery_batch_export.py
@@ -43,7 +43,10 @@ async def load_jsonl_file_to_bigquery_table(jsonl_file, table, table_schema, big
     )
 
     load_job = bigquery_client.load_table_from_file(jsonl_file, table, job_config=job_config, rewind=True)
-    await asyncio.to_thread(load_job.result)
+    try:
+        await asyncio.to_thread(load_job.result)
+    except Exception as e:
+        raise e
 
 
 async def create_table_in_bigquery(
