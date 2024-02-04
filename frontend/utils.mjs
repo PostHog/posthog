2694909be1f5129fd4ae7e9abbd2ec6abd5b3751
Paul D'Ambra <paul@posthog.com>
2024-02-04 20:57:26 +0000

    fix

--- frontend/utils.mjs
+++ frontend/utils.mjs
@@ -319,7 +319,7 @@ export async function buildOrWatch(config) {
         log({ name })
         try {
             const buildResult = await esbuildContext.rebuild()
-            await fs.writeFile('b.json', JSON.stringify(buildResult))
+
             if (config.name === 'Toolbar') {
                 await fs.writeFile('toolbar-esbuild-meta.json', JSON.stringify(buildResult.metafile))
             }
