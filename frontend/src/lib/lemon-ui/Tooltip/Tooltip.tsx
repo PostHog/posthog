8b333564c0773173379b441b3ec387de3031e389
David Newell <david@posthog.com>
2024-02-06 18:03:32 +0000

    add caret

--- frontend/src/lib/lemon-ui/Tooltip/Tooltip.tsx
+++ frontend/src/lib/lemon-ui/Tooltip/Tooltip.tsx
@@ -1,4 +1,5 @@
 import {
+    arrow,
     autoUpdate,
     flip,
     FloatingPortal,
@@ -13,13 +14,13 @@ import {
     useRole,
 } from '@floating-ui/react'
 import clsx from 'clsx'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const DEFAULT_DELAY_MS = 500
 
 export type TooltipProps = {
     title: string | React.ReactNode
-    children?: React.ReactNode
+    children: React.ReactNode
     open?: boolean
     delayMs?: number
     placement?: Placement
@@ -35,8 +36,9 @@ export const Tooltip = ({
     className = '',
 }: TooltipProps): JSX.Element => {
     const [isOpen, setIsOpen] = useState(false)
+    const caretRef = useRef(null)
 
-    const { refs, floatingStyles, context } = useFloating({
+    const { refs, floatingStyles, context, middlewareData } = useFloating({
         open: isOpen,
         onOpenChange: setIsOpen,
         placement: placement,
@@ -47,6 +49,7 @@ export const Tooltip = ({
                 fallbackAxisSideDirection: 'start',
             }),
             shift(),
+            arrow({ element: caretRef }),
         ],
     })
 
@@ -58,7 +61,16 @@ export const Tooltip = ({
 
     const child = React.isValidElement(children) ? children : <span>{children}</span>
 
-    return (
+    const placementSide = placement.split('-')[0]
+    const staticSide =
+        {
+            top: 'bottom',
+            right: 'left',
+            bottom: 'top',
+            left: 'right',
+        }[placementSide] || 'top'
+
+    return title ? (
         <>
             {React.cloneElement(child, {
                 ref: refs.setReference,
@@ -66,17 +78,38 @@ export const Tooltip = ({
             })}
             <FloatingPortal>
                 {isOpen && (
-                    <div
-                        ref={refs.setFloating}
-                        className={clsx('bg-tooltip-bg py-1 px-2 break-words rounded text-start', className)}
-                        // eslint-disable-next-line react/forbid-dom-props
-                        style={floatingStyles}
-                        {...getFloatingProps()}
-                    >
-                        {title}
-                    </div>
+                    <>
+                        <div
+                            ref={refs.setFloating}
+                            className={clsx(
+                                'bg-tooltip-bg py-1.5 px-2 z-[1070] break-words rounded text-start text-white',
+                                className
+                            )}
+                            // eslint-disable-next-line react/forbid-dom-props
+                            style={floatingStyles}
+                            {...getFloatingProps()}
+                        >
+                            {title}
+                            <div
+                                ref={caretRef}
+                                className="absolute w-1.5 h-1.5 bg-tooltip-bg rotate-45"
+                                // eslint-disable-next-line react/forbid-dom-props
+                                style={{
+                                    left: middlewareData.arrow?.x != null ? `${middlewareData.arrow.x}px` : '',
+                                    top: middlewareData.arrow?.y != null ? `${middlewareData.arrow.y}px` : '',
+                                    // Ensure the static side gets unset when
+                                    // flipping to other placements' axes.
+                                    right: '',
+                                    bottom: '',
+                                    [staticSide]: `-3px`,
+                                }}
+                            />
+                        </div>
+                    </>
                 )}
             </FloatingPortal>
         </>
+    ) : (
+        children
     )
 }
