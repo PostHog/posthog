523997523e04004ab7f2c6ee1ce79cf1cb4aea37
Ben White <ben@posthog.com>
2024-02-06 15:59:39 +0000

    Fixed tests

--- posthog/api/test/test_personal_api_keys.py
+++ posthog/api/test/test_personal_api_keys.py
@@ -39,6 +39,42 @@ class TestPersonalAPIKeysAPI(APIBaseTest):
             "attr": "label",
         }
 
+    def test_create_personal_api_key_scopes_required(self):
+        response = self.client.post("/api/personal_api_keys/", {"label": "test"})
+        assert response.status_code == 400
+        assert response.json() == {
+            "type": "validation_error",
+            "code": "required",
+            "detail": "This field is required.",
+            "attr": "scopes",
+        }
+
+    def test_update_api_key(self):
+        key = PersonalAPIKey.objects.create(
+            label="Test",
+            user=self.user,
+            secure_value=hash_key_value(generate_random_token_personal()),
+            scopes="insight:read",
+        )
+        response = self.client.patch(
+            f"/api/personal_api_keys/{key.id}", {"label": "test-update", "scopes": ["insight:write"]}
+        )
+        assert response.status_code == 200
+        data = response.json()
+        assert data["id"] == key.id
+        assert data["label"] == "test-update"
+        assert data["scopes"] == ["insight:write"]
+
+    def test_only_allows_valid_scopes(self):
+        response = self.client.post("/api/personal_api_keys/", {"label": "test", "scopes": ["invalid"]})
+        assert response.status_code == 400
+        assert response.json() == {
+            "type": "validation_error",
+            "code": "required",
+            "detail": "This field is required.",
+            "attr": "scopes",
+        }
+
     def test_delete_personal_api_key(self):
         key = PersonalAPIKey.objects.create(
             label="Test",
@@ -74,135 +110,125 @@ class TestPersonalAPIKeysAPI(APIBaseTest):
             "label": my_label,
             "last_used_at": None,
             "user_id": self.user.id,
-            "scopes": [],
+            "scopes": None,
+            "value": None,
         }
 
+    def test_get_own_personal_api_key(self):
+        my_label = "Test"
+        my_key = PersonalAPIKey.objects.create(
+            label=my_label,
+            user=self.user,
+            secure_value=hash_key_value(generate_random_token_personal()),
+        )
+        response = self.client.get(f"/api/personal_api_keys/{my_key.id}/")
+        assert response.status_code == 200
+        assert response.json()["id"] == my_key.id
 
-#     def test_get_own_personal_api_key(self):
-#         my_label = "Test"
-#         my_key = PersonalAPIKey.objects.create(
-#             label=my_label,
-#             user=self.user,
-#             secure_value=hash_key_value(generate_random_token_personal()),
-#         )
-#         response = self.client.get(f"/api/personal_api_keys/{my_key.id}/")
-#         self.assertEqual(response.status_code, 200)
-#         response_data = response.json()
-#         response_data.pop("created_at")
-#         self.assertDictEqual(
-#             response_data,
-#             {
-#                 "id": my_key.id,
-#                 "label": my_label,
-#                 "last_used_at": None,
-#                 "user_id": self.user.id,
-#             },
-#         )
-
-#     def test_get_someone_elses_personal_api_key(self):
-#         other_user = self._create_user("abc@def.xyz")
-#         other_key = PersonalAPIKey.objects.create(
-#             label="Other test",
-#             user=other_user,
-#             secure_value=hash_key_value(generate_random_token_personal()),
-#         )
-#         response = self.client.get(f"/api/personal_api_keys/{other_key.id}/")
-#         self.assertEqual(response.status_code, 404)
-#         response_data = response.json()
-#         self.assertDictEqual(response_data, self.not_found_response())
+    def test_get_someone_elses_personal_api_key(self):
+        other_user = self._create_user("abc@def.xyz")
+        other_key = PersonalAPIKey.objects.create(
+            label="Other test",
+            user=other_user,
+            secure_value=hash_key_value(generate_random_token_personal()),
+        )
+        response = self.client.get(f"/api/personal_api_keys/{other_key.id}/")
+        assert response.status_code == 404
+        response_data = response.json()
+        assert response_data, self.not_found_response()
 
 
-# class TestPersonalAPIKeysAPIAuthentication(APIBaseTest):
-#     CONFIG_AUTO_LOGIN = False
+class TestPersonalAPIKeysAPIAuthentication(APIBaseTest):
+    CONFIG_AUTO_LOGIN = False
 
-#     value: str
-#     key: PersonalAPIKey
+    value: str
+    key: PersonalAPIKey
 
-#     def setUp(self):
-#         self.value = generate_random_token_personal()
-#         self.key = PersonalAPIKey.objects.create(label="Test", user=self.user, secure_value=hash_key_value(self.value))
-#         self.value_390000 = generate_random_token_personal()
-#         self.key_390000 = PersonalAPIKey.objects.create(
-#             label="Test", user=self.user, secure_value=hash_key_value(self.value_390000, iterations=390000)
-#         )
-#         self.value_hardcoded = "phx_0a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p"
-#         self.key_hardcoded = PersonalAPIKey.objects.create(
-#             label="Test",
-#             user=self.user,
-#             secure_value="pbkdf2_sha256$260000$posthog_personal_api_key$dUOOjl6bYdigHd+QfhYzN6P2vM01ZbFROS8dm9KRK7Y=",
-#         )
-#         return super().setUp()
+    def setUp(self):
+        self.value = generate_random_token_personal()
+        self.key = PersonalAPIKey.objects.create(label="Test", user=self.user, secure_value=hash_key_value(self.value))
+        self.value_390000 = generate_random_token_personal()
+        self.key_390000 = PersonalAPIKey.objects.create(
+            label="Test", user=self.user, secure_value=hash_key_value(self.value_390000, iterations=390000)
+        )
+        self.value_hardcoded = "phx_0a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p"
+        self.key_hardcoded = PersonalAPIKey.objects.create(
+            label="Test",
+            user=self.user,
+            secure_value="pbkdf2_sha256$260000$posthog_personal_api_key$dUOOjl6bYdigHd+QfhYzN6P2vM01ZbFROS8dm9KRK7Y=",
+        )
+        return super().setUp()
 
-#     def test_no_key(self):
-#         response = self.client.get(f"/api/projects/{self.team.id}/dashboards/")
-#         self.assertEqual(response.status_code, 401)
-#         self.assertEqual(
-#             response.json(),
-#             {
-#                 "attr": None,
-#                 "code": "not_authenticated",
-#                 "detail": "Authentication credentials were not provided.",
-#                 "type": "authentication_error",
-#             },
-#         )
+    def test_no_key(self):
+        response = self.client.get(f"/api/projects/{self.team.id}/dashboards/")
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(),
+            {
+                "attr": None,
+                "code": "not_authenticated",
+                "detail": "Authentication credentials were not provided.",
+                "type": "authentication_error",
+            },
+        )
 
-#     def test_header_resilient(self):
-#         response = self.client.get(
-#             f"/api/projects/{self.team.id}/dashboards/",
-#             HTTP_AUTHORIZATION=f"Bearer  {self.value}  ",
-#         )
-#         self.assertEqual(response.status_code, 200)
+    def test_header_resilient(self):
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/dashboards/",
+            HTTP_AUTHORIZATION=f"Bearer  {self.value}  ",
+        )
+        self.assertEqual(response.status_code, 200)
 
-#     def test_header_alternative_iteration_count(self):
-#         response = self.client.get(
-#             f"/api/projects/{self.team.id}/dashboards/",
-#             HTTP_AUTHORIZATION=f"Bearer {self.value_390000}",
-#         )
-#         self.assertEqual(response.status_code, 200)
+    def test_header_alternative_iteration_count(self):
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/dashboards/",
+            HTTP_AUTHORIZATION=f"Bearer {self.value_390000}",
+        )
+        self.assertEqual(response.status_code, 200)
 
-#     def test_header_hardcoded(self):
-#         response = self.client.get(
-#             f"/api/projects/{self.team.id}/dashboards/",
-#             HTTP_AUTHORIZATION=f"Bearer {self.value_hardcoded}",
-#         )
-#         self.assertEqual(response.status_code, 200)
+    def test_header_hardcoded(self):
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/dashboards/",
+            HTTP_AUTHORIZATION=f"Bearer {self.value_hardcoded}",
+        )
+        self.assertEqual(response.status_code, 200)
 
-#     def test_query_string(self):
-#         response = self.client.get(f"/api/projects/{self.team.id}/dashboards/?personal_api_key={self.value}")
-#         self.assertEqual(response.status_code, 200)
+    def test_query_string(self):
+        response = self.client.get(f"/api/projects/{self.team.id}/dashboards/?personal_api_key={self.value}")
+        self.assertEqual(response.status_code, 200)
 
-#     def test_body(self):
-#         response = self.client.get(
-#             f"/api/projects/{self.team.id}/dashboards/",
-#             {"personal_api_key": self.value},
-#         )
-#         self.assertEqual(response.status_code, 200)
+    def test_body(self):
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/dashboards/",
+            {"personal_api_key": self.value},
+        )
+        self.assertEqual(response.status_code, 200)
 
-#     def test_user_not_active(self):
-#         self.user.is_active = False
-#         self.user.save()
-#         response = self.client.get("/api/users/@me/", HTTP_AUTHORIZATION=f"Bearer {self.value}")
-#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_user_not_active(self):
+        self.user.is_active = False
+        self.user.save()
+        response = self.client.get("/api/users/@me/", HTTP_AUTHORIZATION=f"Bearer {self.value}")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-#     def test_user_endpoint(self):
-#         response = self.client.get("/api/users/@me/", HTTP_AUTHORIZATION=f"Bearer {self.value}")
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_user_endpoint(self):
+        response = self.client.get("/api/users/@me/", HTTP_AUTHORIZATION=f"Bearer {self.value}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-#     def test_does_not_interfere_with_temporary_token_auth(self):
-#         response = self.client.get(
-#             f"/api/projects/{self.team.id}/dashboards/",
-#             HTTP_AUTHORIZATION=f"Bearer {self.value}",
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_does_not_interfere_with_temporary_token_auth(self):
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/dashboards/",
+            HTTP_AUTHORIZATION=f"Bearer {self.value}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-#         impersonated_access_token = encode_jwt(
-#             {"id": self.user.id},
-#             timedelta(minutes=15),
-#             PosthogJwtAudience.IMPERSONATED_USER,
-#         )
+        impersonated_access_token = encode_jwt(
+            {"id": self.user.id},
+            timedelta(minutes=15),
+            PosthogJwtAudience.IMPERSONATED_USER,
+        )
 
-#         response = self.client.get(
-#             f"/api/projects/{self.team.id}/dashboards/",
-#             HTTP_AUTHORIZATION=f"Bearer {impersonated_access_token}",
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/dashboards/",
+            HTTP_AUTHORIZATION=f"Bearer {impersonated_access_token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
