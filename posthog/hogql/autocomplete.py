d25307596bf0b244ec29d61076011ce3d9298272
Tom Owers <owerstom@gmail.com>
2024-02-08 11:16:02 +0000

    Added autocomplete to hogql fields

--- posthog/hogql/autocomplete.py
+++ posthog/hogql/autocomplete.py
@@ -1,7 +1,13 @@
-from typing import cast
+from typing import List, Optional, cast
+from posthog.hogql.database.database import Database, create_hogql_database
+from posthog.hogql.database.models import LazyJoin, LazyTable, StringJSONDatabaseField, Table, VirtualTable
+from posthog.hogql.errors import SyntaxException
 from posthog.hogql.filters import replace_filters
 from posthog.hogql.parser import parse_select
 from posthog.hogql import ast
+from posthog.hogql.base import AST
+from posthog.hogql.visitor import TraversingVisitor
+from posthog.models.property_definition import PropertyDefinition
 from posthog.models.team.team import Team
 from posthog.schema import (
     HogQLAutocomplete,
@@ -11,22 +17,127 @@ from posthog.schema import (
 )
 
 
+class GetNodeAtPositionTraverser(TraversingVisitor):
+    start: int
+    end: int
+    selects: List[ast.SelectQuery] = []
+    node: Optional[AST] = None
+    nearest_select_query: Optional[ast.SelectQuery] = None
+
+    def __init__(self, expr: ast.Expr, start: int, end: int):
+        super().__init__()
+        self.start = start
+        self.end = end
+        super().visit(expr)
+
+    def visit(self, node: AST):
+        if node is not None and node.start is not None and node.end is not None:
+            if self.start >= node.start and node.end <= self.end:
+                self.node = node
+                self.nearest_select_query = self.selects[-1]
+
+        super().visit(node)
+
+    def visit_select_query(self, node):
+        self.selects.append(node)
+        node = super().visit_select_query(node)
+        self.selects.pop()
+
+
+def get_table(database: Database, join_expr: ast.JoinExpr) -> None | Table:
+    if isinstance(join_expr.table, ast.Field):
+        table_name = str(join_expr.table.chain[0])
+        if database.has_table(table_name):
+            return database.get_table(table_name)
+    return None
+
+
+def extend_responses(keys: List[str], suggestions: List[AutocompleteCompletionItem]) -> None:
+    suggestions.extend(
+        [
+            AutocompleteCompletionItem(
+                insertText=key,
+                label=key,
+                kind=Kind.Field,
+            )
+            for key in keys
+        ]
+    )
+
+
+MATCH_ANY_CHARACTER = "$$_POSTHOG_ANY_$$"
+PROPERTY_DEFINITION_LIMIT = 220
+
+
 def get_hogql_autocomplete(query: HogQLAutocomplete, team: Team) -> HogQLAutocompleteResponse:
     response = HogQLAutocompleteResponse(suggestions=[])
+
+    database = create_hogql_database(team_id=team.pk, team_arg=team)
 
     try:
         select_ast = parse_select(query.select)
         if query.filters:
             select_ast = cast(ast.SelectQuery, replace_filters(select_ast, query.filters, team))
+
+        find_node = GetNodeAtPositionTraverser(select_ast, query.startPosition, query.endPosition)
+        node = find_node.node
+        nearest_select = find_node.nearest_select_query or select_ast
+
+        if (
+            isinstance(node, ast.Field)
+            and isinstance(nearest_select, ast.SelectQuery)
+            and nearest_select.select_from is not None
+        ):
+            table = get_table(database, nearest_select.select_from)
+            if table is not None:
+                if len(node.chain) == 1:
+                    fields = list(table.fields.keys())
+                    extend_responses(fields, response.suggestions)
+                # TODO: we should do this recursively for deeper joins, e.g. properties.pdi.person.properties.$browser
+                # TODO: add logic for FieldTraverser field types
+                elif len(node.chain) == 2:
+                    field = table.fields[str(node.chain[0])]
+                    if isinstance(field, StringJSONDatabaseField):
+                        if table.to_printed_hogql() == "events":
+                            property_type = PropertyDefinition.Type.EVENT
+                        elif table.to_printed_hogql() == "persons":
+                            property_type = PropertyDefinition.Type.PERSON
+                        elif table.to_printed_hogql() == "groups":
+                            property_type = PropertyDefinition.Type.GROUP
+                        else:
+                            property_type = None
+
+                        if property_type is not None:
+                            match_term = query.select[query.startPosition : query.endPosition]
+                            if match_term == MATCH_ANY_CHARACTER:
+                                match_term = ""
+
+                            properties = PropertyDefinition.objects.filter(
+                                name__contains=match_term,
+                                team_id=team.pk,
+                                type=property_type,
+                            )[:PROPERTY_DEFINITION_LIMIT].values("name")
+
+                            extend_responses([prop["name"] for prop in properties], response.suggestions)
+                    elif isinstance(field, VirtualTable) or isinstance(field, LazyTable):
+                        fields = list(table.fields.keys())
+                        extend_responses(fields, response.suggestions)
+                    elif isinstance(field, LazyJoin):
+                        fields = list(field.join_table.fields.keys())
+                        extend_responses(fields, response.suggestions)
+
+    except SyntaxException as e:
+        if "no viable alternative at input" in e.args[0]:
+            # Append _any_ character to the end of the field if it ends with a dot and try again
+            last_character = query.select[query.endPosition - 1]
+            if last_character.endswith("."):
+                query.select = (
+                    query.select[: query.endPosition] + MATCH_ANY_CHARACTER + query.select[query.endPosition :]
+                )
+                query.endPosition = query.endPosition + len(MATCH_ANY_CHARACTER)
+
+                return get_hogql_autocomplete(query, team)
     except Exception:
         pass
-
-    response.suggestions.append(
-        AutocompleteCompletionItem(
-            insertText="some_field",
-            label="some_field",
-            kind=Kind.Field,
-        )
-    )
 
     return response
