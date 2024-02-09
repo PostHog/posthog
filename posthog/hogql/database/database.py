4ec5d0b3f8ff3e6044a77c1c5a2643f5256d2255
Tom Owers <owerstom@gmail.com>
2024-02-09 15:57:01 +0000

    Fixed tests

--- posthog/hogql/database/database.py
+++ posthog/hogql/database/database.py
@@ -95,7 +95,7 @@ class Database(BaseModel):
         "log_entries",
     ]
 
-    warehouse_table_names: List[str] = []
+    _warehouse_table_names: List[str] = []
 
     _timezone: Optional[str]
     _week_start_day: Optional[WeekStartDay]
@@ -123,12 +123,12 @@ class Database(BaseModel):
         raise HogQLException(f'Table "{table_name}" not found in database')
 
     def get_all_tables(self) -> List[str]:
-        return self._table_names + self.warehouse_table_names
+        return self._table_names + self._warehouse_table_names
 
     def add_warehouse_tables(self, **field_definitions: Any):
         for f_name, f_def in field_definitions.items():
             setattr(self, f_name, f_def)
-            self.warehouse_table_names.append(f_name)
+            self._warehouse_table_names.append(f_name)
 
 
 def create_hogql_database(
