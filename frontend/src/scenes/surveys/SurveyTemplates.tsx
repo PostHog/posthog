81111182933119585801eeae8d688600d5bc02c8
Li Yi <li@posthog.com>
2024-02-11 14:59:30 -0500

    replace for survey templates

--- frontend/src/scenes/surveys/SurveyTemplates.tsx
+++ frontend/src/scenes/surveys/SurveyTemplates.tsx
@@ -7,10 +7,8 @@ import { eventUsageLogic } from 'lib/utils/eventUsageLogic'
 import { SceneExport } from 'scenes/sceneTypes'
 import { urls } from 'scenes/urls'
 
-import { SurveyQuestion } from '~/types'
-
 import { defaultSurveyAppearance, defaultSurveyTemplates } from './constants'
-import { SurveyAppearance } from './SurveyAppearance'
+import { SurveyAppearancePreview } from './SurveyAppearancePreview'
 import { surveyLogic } from './surveyLogic'
 
 export const scene: SceneExport = {
@@ -54,15 +52,17 @@ export function SurveyTemplates(): JSX.Element {
                             </span>
                             <div className="SurveyTemplateContainer">
                                 <div className="SurveyTemplate">
-                                    <SurveyAppearance
+                                    <SurveyAppearancePreview
                                         key={idx}
-                                        surveyType={template.type}
-                                        appearance={{
-                                            ...defaultSurveyAppearance,
-                                            whiteLabel: true,
-                                            ...template.appearance,
+                                        survey={{
+                                            id: `templateMock-${idx}`,
+                                            questions: template.questions,
+                                            appearance: {
+                                                ...defaultSurveyAppearance,
+                                                whiteLabel: true,
+                                                ...template.appearance,
+                                            },
                                         }}
-                                        surveyQuestionItem={template.questions[0] as SurveyQuestion}
                                     />
                                 </div>
                             </div>
