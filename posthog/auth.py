eca8d397171ec124be78af38053d9c261866b9fe
Ben White <ben@posthog.com>
2024-02-12 14:35:25 +0000

    Remove old code

--- posthog/auth.py
+++ posthog/auth.py
@@ -119,8 +119,6 @@ class PersonalAPIKeyAuthentication(authentication.BaseAuthentication):
         request.using_personal_api_key = True  # type: ignore
         request.scopes = personal_api_key_object.scopes  # type: ignore
 
-        self.personal_api_key = personal_api_key_object
-
         return personal_api_key_object.user, None
 
     @classmethod
