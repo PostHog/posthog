0c4d1cd91276ac38e1db95d1b953795603adcaf5
Ben White <ben@posthog.com>
2024-02-12 11:00:57 +0000

    Fix

--- posthog/api/uploaded_media.py
+++ posthog/api/uploaded_media.py
@@ -81,7 +81,6 @@ def download(request, *args, **kwargs) -> HttpResponse:
 
 
 class MediaViewSet(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
-    include_in_docs = False
     base_scope = "not_supported"
     queryset = UploadedMedia.objects.all()
     parser_classes = (MultiPartParser, FormParser)
