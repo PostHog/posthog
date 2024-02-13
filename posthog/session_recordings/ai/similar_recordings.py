584e1d61f18bf17788164b569f5c8d253a22e5ce
David Newell <david@posthog.com>
2024-02-13 10:52:45 +0000

    better names

--- posthog/session_recordings/ai/similar_recordings.py
+++ posthog/session_recordings/ai/similar_recordings.py
@@ -2,18 +2,18 @@ from posthog.models import Team, SessionRecording
 from posthog.clickhouse.client import sync_execute
 
 
-def generate_team_embeddings(session_recording: SessionRecording, team: Team):
-    recording_embeddings = find_recording_embeddings(recording=session_recording, team=team)[0]
+def similar_recordings(session_id: str, team_id: int):
+    target_embeddings = find_target_embeddings(session_id=session_id, team_id=team_id)
 
-    if recording_embeddings is None:
-        # TODO: generate embeddings for a recording
+    if target_embeddings is None:
+        # TODO: generate embeddings for session
         raise
 
-    similar_sessions_ids = compare_recordings(target_embeddings=recording_embeddings)
-    return similar_sessions_ids
+    similar_sessions = closest_embeddings(target=target_embeddings)
+    return similar_sessions
 
 
-def find_recording_embeddings(recording: SessionRecording, team: Team):
+def find_target_embeddings(session_id: str, team_id: int):
     query = """
             SELECT
                 embeddings
@@ -22,44 +22,36 @@ def find_recording_embeddings(recording: SessionRecording, team: Team):
             WHERE
                 team_id = %(team_id)s
                 -- don't load all data for all time
-                and generation_timestamp > now() - INTERVAL 7 DAY
+                AND generation_timestamp > now() - INTERVAL 7 DAY
+                -- don't load all data for all time
+                AND session_id = %(session_id)s
             LIMIT 1
         """
 
-    return sync_execute(query, {"team_id": team.pk, "session_id": recording.id})
+    result = sync_execute(query, {"team_id": team_id, "session_id": session_id})
+    return result[0]
 
 
-def compare_recordings(target_embeddings: [], team: Team, recording: SessionRecording):
+def closest_embeddings(target: [any], team: Team, recording: SessionRecording):
     query = """
             SELECT
-                session_id
-            FROM
-                session_replay_embeddings
-            WHERE
-                team_id = %(team_id)s
-                -- don't load all data for all time
-                and generation_timestamp > now() - INTERVAL 7 DAY
-            LIMIT 1
-
-            SELECT
                 session_id,
-                L2Distance(embeddings, %(target_embeddings)s) AS score
+                -- distance function choice based on https://help.openai.com/en/articles/6824809-embeddings-frequently-asked-questions
+                -- OpenAI normalizes embeddings so L2 should produce the same score but is slightly slower
+                cosine(embeddings, %(target)s) AS similarity_score
             FROM session_replay_embeddings
             WHERE
                 team_id = %(team_id)s
                 -- don't load all data for all time
                 AND generation_timestamp > now() - INTERVAL 7 DAY
-                -- don't load all data for all time
+                -- skip the target recording
                 AND session_id != %(session_id)s
-            ORDER BY score DESC
-            LIMIT 3;
+            ORDER BY similarity_score DESC
+            -- only return up to three results
+            LIMIT %(batch_flush_size)s;
         """
 
     return sync_execute(
         query,
-        {
-            "target_embeddings": target_embeddings,
-            "team_id": team.pk,
-            "session_id": recording.id,
-        },
+        {"target": target, "team_id": team.pk, "session_id": recording.id, "limit": 3},
     )
