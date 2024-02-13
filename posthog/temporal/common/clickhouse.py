ec210b3692cd28a2ba6be40a85e8bc6cebfb4221
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-02-13 16:58:48 +0100

    fix: Handle dict values as JSON when encoding

--- posthog/temporal/common/clickhouse.py
+++ posthog/temporal/common/clickhouse.py
@@ -11,7 +11,7 @@ import requests
 from django.conf import settings
 
 
-def encode_clickhouse_data(data: typing.Any) -> bytes:
+def encode_clickhouse_data(data: typing.Any, quote_char="'") -> bytes:
     """Encode data for ClickHouse.
 
     Depending on the type of data the encoding is different.
@@ -24,7 +24,7 @@ def encode_clickhouse_data(data: typing.Any) -> bytes:
             return b"NULL"
 
         case uuid.UUID():
-            return f"'{data}'".encode("utf-8")
+            return f"{quote_char}{data}{quote_char}".encode("utf-8")
 
         case int():
             return b"%d" % data
@@ -49,12 +49,29 @@ def encode_clickhouse_data(data: typing.Any) -> bytes:
             return result
 
         case dict():
-            return json.dumps(data).encode("utf-8")
+            # Assume we are working with JSON data.
+            encoded_data = []
+
+            for key, value in data.items():
+                if isinstance(value, dt.datetime):
+                    value = value.timestamp()
+                elif isinstance(value, uuid.UUID) or isinstance(value, str):
+                    value = str(value)
+                    quote_char = '"'  # JSON requires double quotes.
+                else:
+                    quote_char = "'"
+
+                encoded_data.append(
+                    f'"{str(key)}"'.encode("utf-8") + b":" + encode_clickhouse_data(value, quote_char=quote_char)
+                )
+
+            result = b"{" + b",".join(encoded_data) + b"}"
+            return result
 
         case _:
             str_data = str(data)
             str_data = str_data.replace("\\", "\\\\").replace("'", "\\'")
-            return f"'{str_data}'".encode("utf-8")
+            return f"{quote_char}{str_data}{quote_char}".encode("utf-8")
 
 
 class ClickHouseError(Exception):
@@ -138,13 +155,13 @@ class ClickHouseClient:
         Returns:
             The formatted query.
         """
-        if query_parameters:
-            format_parameters = {k: encode_clickhouse_data(v).decode("utf-8") for k, v in query_parameters.items()}
-        else:
-            format_parameters = {}
+        if query_parameters is None or not query_parameters:
+            return query
 
+        format_parameters = {k: encode_clickhouse_data(v).decode("utf-8") for k, v in query_parameters.items()}
         query = query % format_parameters
         query = query.format(**format_parameters)
+
         return query
 
     def prepare_request_data(self, data: collections.abc.Sequence[typing.Any]) -> bytes | None:
@@ -196,6 +213,7 @@ class ClickHouseClient:
         Returns:
             The response received from the ClickHouse HTTP interface.
         """
+
         params = {**self.params}
         if query_id is not None:
             params["query_id"] = query_id
@@ -320,7 +338,7 @@ class ClickHouseClient:
 
 
 @contextlib.asynccontextmanager
-async def get_client() -> collections.abc.AsyncIterator[ClickHouseClient]:
+async def get_client(**kwargs) -> collections.abc.AsyncIterator[ClickHouseClient]:
     """
     Returns a ClickHouse client based on the aiochclient library. This is an
     async context manager.
@@ -363,5 +381,6 @@ async def get_client() -> collections.abc.AsyncIterator[ClickHouseClient]:
                 max_execution_time=0,
                 max_block_size=10000,
                 output_format_arrow_string_as_string="true",
+                **kwargs,
             ) as client:
                 yield client
