94b03d6bd624bd541e76c373453257bc66d45a8c
Julian Bez <julian@posthog.com>
2024-02-13 09:40:48 +0100

    Update refresh stuff

--- frontend/src/scenes/dashboard/dashboardLogic.tsx
+++ frontend/src/scenes/dashboard/dashboardLogic.tsx
@@ -1076,6 +1076,7 @@ export const dashboardLogic = kea<dashboardLogicType>([
             if (values.autoRefresh.enabled) {
                 // Refresh right now after enabling if we haven't refreshed recently
                 if (
+                    !values.itemsLoading &&
                     values.lastRefreshed &&
                     values.lastRefreshed.isBefore(now().subtract(values.autoRefresh.interval, 'seconds'))
                 ) {
