3188dea3b797f418b6dea9e272c8f65e6ea0d7f5
David Newell <david@posthog.com>
2024-02-14 10:37:32 +0000

    final fixes

--- frontend/src/lib/components/ExportButton/ExportButton.tsx
+++ frontend/src/lib/components/ExportButton/ExportButton.tsx
@@ -1,5 +1,6 @@
 import { LemonButton, LemonButtonProps, LemonButtonWithDropdown } from 'lib/lemon-ui/LemonButton'
 import { LemonDivider } from 'lib/lemon-ui/LemonDivider'
+import { forwardRef } from 'react'
 
 import { ExporterFormat, OnlineExportContext } from '~/types'
 
@@ -17,56 +18,59 @@ export interface ExportButtonProps extends Pick<LemonButtonProps, 'icon' | 'type
     items: ExportButtonItem[]
 }
 
-export function ExportButton({ items, ...buttonProps }: ExportButtonProps): JSX.Element {
-    return (
-        <LemonButtonWithDropdown
-            data-attr="export-button"
-            {...buttonProps}
-            dropdown={{
-                actionable: true,
-                placement: 'right-start',
-                closeParentPopoverOnClickInside: true,
-                overlay: (
-                    <>
-                        <h5>File type</h5>
-                        <LemonDivider />
-                        {items.map(({ title, ...triggerExportProps }, i) => {
-                            const exportFormatExtension = triggerExportProps.export_format.split('/').pop()
+export const ExportButton: React.FunctionComponent<ExportButtonProps & React.RefAttributes<HTMLButtonElement>> =
+    forwardRef(function ExportButton({ items, ...buttonProps }, ref): JSX.Element {
+        return (
+            <LemonButtonWithDropdown
+                ref={ref}
+                data-attr="export-button"
+                {...buttonProps}
+                dropdown={{
+                    actionable: true,
+                    placement: 'right-start',
+                    closeParentPopoverOnClickInside: true,
+                    overlay: (
+                        <>
+                            <h5>File type</h5>
+                            <LemonDivider />
+                            {items.map(({ title, ...triggerExportProps }, i) => {
+                                const exportFormatExtension = triggerExportProps.export_format.split('/').pop()
 
-                            let target: string
-                            let exportBody: string = ''
-                            if (triggerExportProps.insight) {
-                                target = `insight-${triggerExportProps.insight}`
-                            } else if (triggerExportProps.dashboard) {
-                                target = `dashboard-${triggerExportProps.dashboard}`
-                            } else if ('path' in (triggerExportProps.export_context || {})) {
-                                target = (triggerExportProps.export_context as OnlineExportContext)?.path || 'unknown'
-                                exportBody =
-                                    (triggerExportProps.export_context as OnlineExportContext)?.body || 'unknown'
-                            } else {
-                                target = 'unknown'
-                            }
+                                let target: string
+                                let exportBody: string = ''
+                                if (triggerExportProps.insight) {
+                                    target = `insight-${triggerExportProps.insight}`
+                                } else if (triggerExportProps.dashboard) {
+                                    target = `dashboard-${triggerExportProps.dashboard}`
+                                } else if ('path' in (triggerExportProps.export_context || {})) {
+                                    target =
+                                        (triggerExportProps.export_context as OnlineExportContext)?.path || 'unknown'
+                                    exportBody =
+                                        (triggerExportProps.export_context as OnlineExportContext)?.body || 'unknown'
+                                } else {
+                                    target = 'unknown'
+                                }
 
-                            return (
-                                <LemonButton
-                                    key={i}
-                                    fullWidth
-                                    onClick={() => void triggerExport(triggerExportProps)}
-                                    data-attr={`export-button-${exportFormatExtension}`}
-                                    data-ph-capture-attribute-export-target={target}
-                                    data-ph-capture-attribute-export-body={
-                                        exportBody.length ? JSON.stringify(exportBody) : null
-                                    }
-                                >
-                                    {title ? title : `.${exportFormatExtension}`}
-                                </LemonButton>
-                            )
-                        })}
-                    </>
-                ),
-            }}
-        >
-            Export
-        </LemonButtonWithDropdown>
-    )
-}
+                                return (
+                                    <LemonButton
+                                        key={i}
+                                        fullWidth
+                                        onClick={() => void triggerExport(triggerExportProps)}
+                                        data-attr={`export-button-${exportFormatExtension}`}
+                                        data-ph-capture-attribute-export-target={target}
+                                        data-ph-capture-attribute-export-body={
+                                            exportBody.length ? JSON.stringify(exportBody) : null
+                                        }
+                                    >
+                                        {title ? title : `.${exportFormatExtension}`}
+                                    </LemonButton>
+                                )
+                            })}
+                        </>
+                    ),
+                }}
+            >
+                Export
+            </LemonButtonWithDropdown>
+        )
+    })
