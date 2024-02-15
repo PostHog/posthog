fcbd5e7a340df020df3a56cbbbf396c445bdb3c3
Marius Andra <marius.andra@gmail.com>
2024-02-15 09:59:36 +0100

    feat(hogql): trends comparison (#20310)

--- posthog/management/commands/compare_hogql_insights.py
+++ posthog/management/commands/compare_hogql_insights.py
@@ -7,20 +7,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         from typing import cast
         from posthog.schema import HogQLQueryModifiers, HogQLQueryResponse, MaterializationMode
-        from posthog.models import Insight, Filter, RetentionFilter
-        from posthog.models.filters import StickinessFilter
-        from posthog.queries.retention import Retention
+        from posthog.models import Insight, Filter
         from posthog.queries.trends.trends import Trends
-        from posthog.queries.stickiness.stickiness import Stickiness
         from posthog.hogql_queries.legacy_compatibility.filter_to_query import filter_to_query
         from posthog.hogql_queries.query_runner import get_query_runner
 
         insights = (
-            Insight.objects.filter(filters__contains={"insight": "STICKINESS"}, saved=True, deleted=False)
+            Insight.objects.filter(filters__contains={"insight": "TRENDS"}, saved=True, deleted=False, team_id=2)
             .order_by("id")
             .all()
         )
-        for insight in insights[200:300]:
+        for insight in insights[0:30]:
             try:
                 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")  # noqa: T201
                 insight_type = insight.filters.get("insight")
@@ -30,15 +27,8 @@ class Command(BaseCommand):
                 )
                 if insight.filters.get("aggregation_group_type_index", None) is not None:
                     del insight.filters["aggregation_group_type_index"]
-                if insight_type == "STICKINESS":
-                    sticky_filter = StickinessFilter(insight.filters, team=insight.team)
-                    legacy_results = Stickiness().run(sticky_filter, insight.team)
-                elif insight_type == "RETENTION":
-                    retention_filter = RetentionFilter(insight.filters, team=insight.team)
-                    legacy_results = Retention().run(retention_filter, insight.team)
-                else:
-                    filter = Filter(insight.filters, team=insight.team)
-                    legacy_results = Trends().run(filter, insight.team)
+                filter = Filter(insight.filters, team=insight.team)
+                legacy_results = Trends().run(filter, insight.team)
                 for row in legacy_results:
                     if row.get("persons_urls"):
                         del row["persons_urls"]
@@ -48,27 +38,11 @@ class Command(BaseCommand):
                 hogql_results = cast(HogQLQueryResponse, query_runner.calculate()).results or []
                 all_ok = True
                 for legacy_result, hogql_result in zip(legacy_results, hogql_results):
-                    if insight_type == "LIFECYCLE":
-                        fields = ["data", "days", "count", "labels", "label", "status"]
-                    elif insight_type == "RETENTION":
-                        if legacy_result.get("date") != hogql_result.date:
-                            all_ok = False
-                            print("Date: ", legacy_result.get("date"), hogql_result.date)  # noqa: T201
-                        if legacy_result.get("label") != hogql_result.label:
-                            all_ok = False
-                            print("Label: ", legacy_result.get("label"), hogql_result.label)  # noqa: T201
-                        legacy_values = [c.get("count") for c in legacy_result.get("values") or []]
-                        hogql_values = [c.count for c in hogql_result.values or []]
-                        if legacy_values != hogql_values:
-                            all_ok = False
-                            print("Values: ", legacy_values, hogql_values)  # noqa: T201
-                        continue
-                    elif insight_type == "STICKINESS":
-                        fields = ["label", "count", "data", "days"]
-                    else:
-                        fields = ["label", "count", "data", "labels", "days"]
+                    fields = ["label", "count", "data", "labels", "days"]
                     for field in fields:
                         if legacy_result.get(field) != hogql_result.get(field):
+                            if field == "labels" and insight.filters.get("interval") == "month":
+                                continue
                             print(  # noqa: T201
                                 f"Insight https://us.posthog.com/project/{insight.team_id}/insights/{insight.short_id}/edit"
                                 f" ({insight.id}). MISMATCH in {legacy_result.get('status')} row, field {field}"
