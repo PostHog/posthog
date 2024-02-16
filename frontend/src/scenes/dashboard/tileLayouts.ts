8bb02eb3b40dc6eedd03cfd844c91082e24efe10
Michael Matloka <michal@matloka.com>
2024-02-16 12:57:49 +0100

    feat(dashboards): Smarter tile sizing with smaller minimums

--- frontend/src/scenes/dashboard/tileLayouts.ts
+++ frontend/src/scenes/dashboard/tileLayouts.ts
@@ -1,6 +1,6 @@
 import { Layout } from 'react-grid-layout'
-import { BREAKPOINT_COLUMN_COUNTS, MIN_ITEM_HEIGHT_UNITS, MIN_ITEM_WIDTH_UNITS } from 'scenes/dashboard/dashboardLogic'
-import { isPathsFilter, isRetentionFilter, isTrendsFilter } from 'scenes/insights/sharedUtils'
+import { BREAKPOINT_COLUMN_COUNTS } from 'scenes/dashboard/dashboardLogic'
+import { isFunnelsFilter, isPathsFilter, isRetentionFilter, isTrendsFilter } from 'scenes/insights/sharedUtils'
 
 import { ChartDisplayType, DashboardLayoutSize, DashboardTile, FilterType } from '~/types'
 
@@ -25,14 +25,12 @@ export const calculateLayouts = (tiles: DashboardTile[]): Partial<Record<Dashboa
 
     let referenceOrder: number[] | undefined = undefined
 
-    for (const col of Object.keys(BREAKPOINT_COLUMN_COUNTS) as (keyof typeof BREAKPOINT_COLUMN_COUNTS)[]) {
-        // The dashboard redesign includes constraints on the size of dashboard items
-        const minW = col === 'xs' ? 1 : MIN_ITEM_WIDTH_UNITS
-        const minH = MIN_ITEM_HEIGHT_UNITS
+    for (const breakpoint of Object.keys(BREAKPOINT_COLUMN_COUNTS) as (keyof typeof BREAKPOINT_COLUMN_COUNTS)[]) {
+        const columnCount = BREAKPOINT_COLUMN_COUNTS[breakpoint]
 
         let sortedDashboardTiles: DashboardTile[] | undefined
         if (referenceOrder === undefined) {
-            sortedDashboardTiles = sortTilesByLayout(tiles, col)
+            sortedDashboardTiles = sortTilesByLayout(tiles, breakpoint)
             referenceOrder = sortedDashboardTiles.map((tile) => tile.id)
         } else {
             sortedDashboardTiles = tiles.sort((a, b) => {
@@ -42,77 +40,105 @@ export const calculateLayouts = (tiles: DashboardTile[]): Partial<Record<Dashboa
 
         const layouts = (sortedDashboardTiles || []).map((tile) => {
             const filters: Partial<FilterType> | undefined = tile.insight?.filters
-            const isRetention = isRetentionFilter(filters)
-            const isPathsViz = isPathsFilter(filters)
-            const isBoldNumber = isTrendsFilter(filters) && filters.display === ChartDisplayType.BoldNumber
+            // Base constraints
+            let minW = 3
+            let minH = 3
+            let defaultW = 6
+            let defaultH = 5
+            // Content-adjusted constraints
+            if (tile.text) {
+                minW = 1
+                minH = 1
+                defaultH = 2
+            } else if (isFunnelsFilter(filters)) {
+                minW = 4
+                minH = 4
+            } else if (isRetentionFilter(filters)) {
+                minW = 6
+                minH = 7
+                defaultW = 6
+                defaultH = 7
+            } else if (isPathsFilter(filters)) {
+                minW = columnCount // Paths take up so much space that they need to be full width to be readable
+                minH = 7
+                defaultW = columnCount
+                defaultH = 7
+            } else if (isTrendsFilter(filters) && filters.display === ChartDisplayType.BoldNumber) {
+                minW = 2
+                minH = 3
+            }
+            // Single-column layout width override
+            if (breakpoint === 'xs') {
+                minW = 1
+                defaultW = 1
+            }
 
-            const defaultWidth = isRetention || isPathsViz ? 8 : 6
-            const defaultHeight = tile.text ? minH + 1 : isRetention ? 8 : isPathsViz ? 12.5 : 5
-            const layout = tile.layouts && tile.layouts[col]
+            const layout = tile.layouts && tile.layouts[breakpoint]
             const { x, y, w, h } = layout || {}
-            const width = Math.min(w || defaultWidth, BREAKPOINT_COLUMN_COUNTS[col])
+
+            const realW = Math.min(w || defaultW, columnCount)
+            const realH = h || defaultH
 
             return {
                 i: tile.id?.toString(),
-                x: Number.isInteger(x) && x + width - 1 < BREAKPOINT_COLUMN_COUNTS[col] ? x : 0,
+                x: Number.isInteger(x) && x + realW - 1 < columnCount ? x : 0,
                 y: Number.isInteger(y) ? y : Infinity,
-                w: width,
-                h: h || defaultHeight,
+                w: realW,
+                h: realH,
                 minW,
-                minH: tile.text ? 2 : isBoldNumber ? 4 : minH,
+                minH,
             }
         })
 
         const cleanLayouts = layouts?.filter(({ y }) => y !== Infinity)
+        const dirtyLayouts = layouts?.filter(({ y }) => y === Infinity)
 
         // array of -1 for each column
-        const lowestPoints = Array.from(Array(BREAKPOINT_COLUMN_COUNTS[col])).map(() => -1)
+        const lowestPoints = Array.from(Array(columnCount)).map(() => -1)
 
         // set the lowest point for each column
-        cleanLayouts?.forEach(({ x, y, w, h }) => {
+        for (const { x, y, w, h } of cleanLayouts) {
             for (let i = x; i <= x + w - 1; i++) {
                 lowestPoints[i] = Math.max(lowestPoints[i], y + h - 1)
             }
-        })
+        }
 
-        layouts
-            ?.filter(({ y }) => y === Infinity)
-            .forEach(({ i, w, h }) => {
-                // how low are things in "w" consecutive of columns
-                const segmentCount = BREAKPOINT_COLUMN_COUNTS[col] - w + 1
-                const lowestSegments = Array.from(Array(segmentCount)).map(() => -1)
-                for (let k = 0; k < segmentCount; k++) {
-                    for (let j = k; j <= k + w - 1; j++) {
-                        lowestSegments[k] = Math.max(lowestSegments[k], lowestPoints[j])
-                    }
+        for (const { i, w, h, minW, minH } of dirtyLayouts) {
+            // how low are things in "w" consecutive of columns
+            const segmentCount = columnCount - w + 1
+            const lowestSegments = Array.from(Array(segmentCount)).map(() => -1)
+            for (let k = 0; k < segmentCount; k++) {
+                for (let j = k; j <= k + w - 1; j++) {
+                    lowestSegments[k] = Math.max(lowestSegments[k], lowestPoints[j])
                 }
+            }
 
-                let lowestIndex = 0
-                let lowestDepth = lowestSegments[0]
-
-                lowestSegments.forEach((depth, index) => {
-                    if (depth < lowestDepth) {
-                        lowestIndex = index
-                        lowestDepth = depth
-                    }
-                })
-
-                cleanLayouts?.push({
-                    i,
-                    x: lowestIndex,
-                    y: lowestDepth + 1,
-                    w,
-                    h,
-                    minW,
-                    minH,
-                })
-
-                for (let k = lowestIndex; k <= lowestIndex + w - 1; k++) {
-                    lowestPoints[k] = Math.max(lowestPoints[k], lowestDepth + h)
+            let lowestIndex = 0
+            let lowestDepth = lowestSegments[0]
+            for (let index = 1; index < segmentCount; index++) {
+                const depth = lowestSegments[index]
+                if (depth < lowestDepth) {
+                    lowestIndex = index
+                    lowestDepth = depth
                 }
+            }
+
+            cleanLayouts.push({
+                i,
+                x: lowestIndex,
+                y: lowestDepth + 1,
+                w,
+                h,
+                minW,
+                minH,
             })
 
-        allLayouts[col] = cleanLayouts
+            for (let k = lowestIndex; k <= lowestIndex + w - 1; k++) {
+                lowestPoints[k] = Math.max(lowestPoints[k], lowestDepth + h)
+            }
+        }
+
+        allLayouts[breakpoint] = cleanLayouts
     }
 
     return allLayouts
