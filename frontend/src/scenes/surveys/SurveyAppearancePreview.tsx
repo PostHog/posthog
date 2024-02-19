559e4342158749d2139ada3b6ed0c1c5cf33098f
Li Yi <li@posthog.com>
2024-02-19 15:19:13 -0500

    add feedback widget preview

--- frontend/src/scenes/surveys/SurveyAppearancePreview.tsx
+++ frontend/src/scenes/surveys/SurveyAppearancePreview.tsx
@@ -1,7 +1,7 @@
-import { renderSurveysPreview } from 'posthog-js/dist/surveys.esm'
+import { renderFeedbackWidgetPreview, renderSurveysPreview } from 'posthog-js/dist/surveys.esm'
 import { useEffect, useRef } from 'react'
 
-import { Survey } from '~/types'
+import { Survey, SurveyType } from '~/types'
 
 export function SurveyAppearancePreview({
     survey,
@@ -13,12 +13,41 @@ export function SurveyAppearancePreview({
     questionIndex: number
 }): JSX.Element {
     const surveyPreviewRef = useRef<HTMLDivElement>(null)
+    const feedbackWidgetPreviewRef = useRef<HTMLDivElement>(null)
+
     useEffect(() => {
         if (surveyPreviewRef.current) {
             // remove any existing survey preview
             surveyPreviewRef.current.innerHTML = ''
             renderSurveysPreview(survey, surveyPreviewRef.current, activePreview, questionIndex)
         }
+
+        if (feedbackWidgetPreviewRef.current) {
+            if (survey.type === SurveyType.Widget && survey.appearance.widgetType === 'tab') {
+                // remove any existing feedback widget preview
+                feedbackWidgetPreviewRef.current.innerHTML = ''
+                renderFeedbackWidgetPreview(survey, feedbackWidgetPreviewRef.current)
+            } else {
+                feedbackWidgetPreviewRef.current.innerHTML = ''
+            }
+        }
     }, [survey, activePreview, questionIndex])
-    return <div ref={surveyPreviewRef}>SurveyAppearancePreview</div>
+    return (
+        <>
+            <div ref={surveyPreviewRef} />
+            <div ref={feedbackWidgetPreviewRef} />
+        </>
+    )
+}
+
+export function FeedbackWidgetAppearancePreview({ survey }: { survey: Survey }): JSX.Element {
+    const feedbackWidgetPreviewRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        if (feedbackWidgetPreviewRef.current) {
+            // remove any existing feedback widget preview
+            feedbackWidgetPreviewRef.current.innerHTML = ''
+            renderFeedbackWidgetPreview(survey, feedbackWidgetPreviewRef.current)
+        }
+    }, [survey.type, survey.appearance])
+    return <div ref={feedbackWidgetPreviewRef}>FeedbackWidgetAppearancePreview</div>
 }
--- frontend/src/scenes/surveys/SurveyEdit.tsx
+++ frontend/src/scenes/surveys/SurveyEdit.tsx
@@ -567,7 +567,11 @@ export default function SurveyEdit(): JSX.Element {
                 />
             </div>
             <LemonDivider vertical />
-            <div className="max-w-80 mx-4 flex flex-col items-center h-full w-full sticky top-0 pt-8">
+            <div
+                className="max-w-80 mx-4 flex flex-col items-center h-full w-full sticky top-0 pt-8"
+                // eslint-disable-next-line react/forbid-dom-props
+                style={{ zIndex: 999999 }}
+            >
                 <SurveyFormAppearance
                     activePreview={selectedQuestion || 0}
                     survey={survey}
