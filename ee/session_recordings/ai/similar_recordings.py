f5894bdf061a890a1c1bc7a9391ca2b0a89696e8
David Newell <david@posthog.com>
2024-02-20 10:45:06 +0000

    simplify with subquery

--- ee/session_recordings/ai/similar_recordings.py
+++ ee/session_recordings/ai/similar_recordings.py
@@ -1,6 +1,5 @@
-from prometheus_client import Histogram, Counter
+from prometheus_client import Histogram
 
-from typing import List
 
 from posthog.clickhouse.client import sync_execute
 from posthog.models.team import Team
@@ -11,55 +10,37 @@ FIND_RECORDING_NEIGHBOURS_TIMING = Histogram(
     "Time spent finding the most similar recording embeddings for a single session",
 )
 
-CANNOT_FIND_NEIGHBORS_WITHOUT_EMBEDDINGS = Counter(
-    "posthog_session_recordings_cannot_find_neighbours_without_embeddings",
-    "Number of sessions skipped because recording didn't have embeddings",
-)
-
 
 def similar_recordings(recording: SessionRecording, team: Team):
-    target_embeddings = find_target_embeddings(session_id=recording.session_id, team_id=team.pk)
-
-    if target_embeddings is None:
-        CANNOT_FIND_NEIGHBORS_WITHOUT_EMBEDDINGS.inc()
-        return []
-
     with FIND_RECORDING_NEIGHBOURS_TIMING.time():
-        similar_embeddings = closest_embeddings(
-            target=target_embeddings, session_id=recording.session_id, team_id=team.pk
-        )
+        similar_embeddings = closest_embeddings(session_id=recording.session_id, team_id=team.pk)
 
     # TODO: join session recording context (person, duration, etc) to show in frontend
 
     return similar_embeddings
 
 
-def find_target_embeddings(session_id: str, team_id: int):
+def closest_embeddings(session_id: str, team_id: int):
     query = """
-            SELECT
-                embeddings
-            FROM
-                session_replay_embeddings
-            WHERE
-                team_id = %(team_id)s
-                -- don't load all data for all time
-                AND generation_timestamp > now() - INTERVAL 7 DAY
-                -- don't load all data for all time
-                AND session_id = %(session_id)s
-            LIMIT 1
-        """
-
-    result = sync_execute(query, {"team_id": team_id, "session_id": session_id})
-    return result[0][0] if len(result) > 0 else None
-
-
-def closest_embeddings(target: List[float], session_id: str, team_id: int):
-    query = """
+            WITH target_embeddings as
+            (
+                SELECT
+                    embeddings
+                FROM
+                    session_replay_embeddings
+                WHERE
+                    team_id = %(team_id)s
+                    -- don't load all data for all time
+                    AND generation_timestamp > now() - INTERVAL 7 DAY
+                    -- don't load all data for all time
+                    AND session_id = %(session_id)s
+                LIMIT 1
+            )
             SELECT
                 session_id,
                 -- distance function choice based on https://help.openai.com/en/articles/6824809-embeddings-frequently-asked-questions
                 -- OpenAI normalizes embeddings so L2 should produce the same score but is slightly slower
-                cosineDistance(embeddings, %(target)s) AS similarity_score
+                cosineDistance(embeddings, target_embeddings) AS similarity_score
             FROM session_replay_embeddings
             WHERE
                 team_id = %(team_id)s
@@ -74,5 +55,5 @@ def closest_embeddings(target: List[float], session_id: str, team_id: int):
 
     return sync_execute(
         query,
-        {"target": target, "team_id": team_id, "session_id": session_id, "limit": 3},
+        {"team_id": team_id, "session_id": session_id, "limit": 3},
     )
