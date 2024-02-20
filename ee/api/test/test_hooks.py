2e2e3168e7253457fccca1c278eeac6a8555f451
Ben White <ben@posthog.com>
2024-02-20 16:32:16 +0000

    Fix tests

--- ee/api/test/test_hooks.py
+++ ee/api/test/test_hooks.py
@@ -6,6 +6,13 @@ from posthog.test.base import ClickhouseTestMixin
 
 
 class TestHooksAPI(ClickhouseTestMixin, APILicensedTest):
+    def _create_hook(self, target="https://example.com/hook/", event="action_performed", resource_id=1):
+        response = self.client.post(
+            f"/api/projects/{self.team.id}/hooks/", {"target": target, "event": event, "resource_id": resource_id}
+        )
+        assert response.status_code == 201
+        return response.json()
+
     def test_create_hook(self):
         data = {"target": "https://hooks.zapier.com/abcd/", "event": "action_performed"}
         response = self.client.post(f"/api/projects/{self.team.id}/hooks/", data)
@@ -54,16 +61,30 @@ class TestHooksAPI(ClickhouseTestMixin, APILicensedTest):
             cast(dict, response.json()),
         )
 
+    def test_validates_url(self):
+        response = self.client.post(
+            f"/api/projects/{self.team.id}/hooks/",
+            {
+                "target": "NOT_A_URL",
+                "event": "action_performed",
+                "resource_id": "66",
+            },
+        )
+        assert response.status_code == 400, response.json()
+        assert response.json()["detail"] == "Enter a valid URL."
+
     def test_delete_hook(self):
         hook_id = "abc123"
         Hook.objects.create(id=hook_id, user=self.user, team=self.team, resource_id=20)
         response = self.client.delete(f"/api/projects/{self.team.id}/hooks/{hook_id}")
         self.assertEqual(response.status_code, 204)
 
-    def test_invalid_target(self):
-        data = {
-            "target": "https://hooks.non-zapier.com/abcd/",
-            "event": "action_performed",
-        }
-        response = self.client.post(f"/api/projects/{self.team.id}/hooks/", data)
-        self.assertEqual(response.status_code, 400)
+    def test_list_hooks(self):
+        self._create_hook(resource_id=1)
+        self._create_hook(resource_id=1)
+        self._create_hook(resource_id=2)
+        response = self.client.get(f"/api/projects/{self.team.id}/hooks/")
+        self.assertEqual(len(response.json()["results"]), 3)
+
+        response = self.client.get(f"/api/projects/{self.team.id}/hooks/?resource_id=1")
+        self.assertEqual(len(response.json()["results"]), 2)
