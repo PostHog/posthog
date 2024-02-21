4f43621a8bb3bd754b0c9bcc1e58b73e2a293e0f
Bianca Yang <bianca@posthog.com>
2024-02-21 11:09:59 -0800

    fix some state problems

--- frontend/src/scenes/sceneLogic.ts
+++ frontend/src/scenes/sceneLogic.ts
@@ -15,7 +15,7 @@ import { AvailableFeature, ProductKey } from '~/types'
 
 import { appContextLogic } from './appContextLogic'
 import { handleLoginRedirect } from './authentication/loginLogic'
-import { OnboardingStepKey } from './onboarding/onboardingLogic'
+import { onboardingLogic, OnboardingStepKey } from './onboarding/onboardingLogic'
 import { organizationLogic } from './organizationLogic'
 import { preflightLogic } from './PreflightCheck/preflightLogic'
 import type { sceneLogicType } from './sceneLogicType'
@@ -289,6 +289,7 @@ export const sceneLogic = kea<sceneLogicType>([
                                 console.warn(
                                     `Onboarding not completed for ${productKeyFromUrl}, redirecting to onboarding intro`
                                 )
+                                onboardingLogic.actions.setIncludeIntro(true)
                                 router.actions.replace(
                                     urls.onboarding(productKeyFromUrl, OnboardingStepKey.PRODUCT_INTRO)
                                 )
