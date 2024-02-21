d4e199e8e736b51a5d5b33ac7b5b79e465361a8d
Li Yi <li@posthog.com>
2024-02-20 19:02:56 -0500

    fix types

--- frontend/src/scenes/surveys/SurveyAppearancePreview.tsx
+++ frontend/src/scenes/surveys/SurveyAppearancePreview.tsx
@@ -3,12 +3,14 @@ import { useEffect, useRef } from 'react'
 
 import { Survey, SurveyType } from '~/types'
 
+import { NewSurvey } from './constants'
+
 export function SurveyAppearancePreview({
     survey,
     activePreview,
     questionIndex,
 }: {
-    survey: Survey
+    survey: Survey | NewSurvey
     activePreview: 'survey' | 'confirmation'
     questionIndex: number
 }): JSX.Element {
