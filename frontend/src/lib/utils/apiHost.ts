51190fafd6037f3b7d195a606db8fd5b2dc9d8fa
Frank Hamand <frankhamand@gmail.com>
2024-02-23 11:34:21 +0000

    Add asset_host to JS snippet and use new api hosts

--- frontend/src/lib/utils/apiHost.ts
+++ frontend/src/lib/utils/apiHost.ts
@@ -2,7 +2,21 @@ export function apiHostOrigin(): string {
     let apiHost = window.location.origin
     // similar to https://github.com/PostHog/posthog-js/blob/b79315b7a4fa0caded7026bda2fec01defb0ba73/src/posthog-core.ts#L1742
     if (apiHost === 'https://us.posthog.com') {
-        apiHost = 'https://app.posthog.com'
+        apiHost = 'https://us.i.posthog.com'
+    }
+    if (apiHost === 'https://eu.posthog.com') {
+        apiHost = 'https://eu.i.posthog.com'
     }
     return apiHost
+}
+export function assetHostOrigin(): string {
+    let assetHost = window.location.origin
+    // similar to https://github.com/PostHog/posthog-js/blob/b79315b7a4fa0caded7026bda2fec01defb0ba73/src/posthog-core.ts#L1742
+    if (assetHost === 'https://us.posthog.com') {
+        assetHost = 'https://us-assets.i.posthog.com'
+    }
+    if (assetHost === 'https://eu.posthog.com') {
+        assetHost = 'https://eu-assets.i.posthog.com'
+    }
+    return assetHost
 }
