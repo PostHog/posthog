3cfe0408c57f7b3b0dcf64d96020edfe8bd07572
Eli Kinsey <eli@ekinsey.dev>
2024-02-23 22:23:48 -0800

    ux improvements

--- frontend/src/layout/navigation-3000/components/AlgoliaSearch.tsx
+++ frontend/src/layout/navigation-3000/components/AlgoliaSearch.tsx
@@ -1,28 +1,51 @@
 import { LemonButton, LemonInput } from '@posthog/lemon-ui'
 import algoliasearch from 'algoliasearch/lite'
-import { Key, useState } from 'react'
+import { useActions } from 'kea'
+import { useEffect, useState } from 'react'
 import { InstantSearch, useHits, useSearchBox } from 'react-instantsearch'
+import { AutoSizer } from 'react-virtualized/dist/es/AutoSizer'
+import { List } from 'react-virtualized/dist/es/List'
+
+import { sidePanelStateLogic } from '~/layout/navigation-3000/sidepanel/sidePanelStateLogic'
+import { SidePanelTab } from '~/types'
 
 const searchClient = algoliasearch('7VNQB5W0TX', '37f41fd37095bc85af76ed4edc85eb5a')
 
-const Hits = (): JSX.Element => {
-    const { hits } = useHits()
-
+const rowRenderer = ({ key, index, style, hits, activeOption }: any): JSX.Element => {
+    const { slug, title } = hits[index]
     return (
-        <div className="relative">
-            <ol className="list-none m-0 p-0 absolute w-full bg-white z-50 border rounded-sm max-h-[300px] overflow-auto mt-1 divide-y">
-                {hits.map(({ title, excerpt, slug, id }) => {
-                    return (
-                        <li className="p-1" key={id as Key}>
-                            <LemonButton to={`https://posthog.com/${slug}`}>
-                                <span>
-                                    <p className="m-0 font-bold mb-1">{title}</p>
-                                    <p className="text-xs m-0 opacity-80">{excerpt}</p>
-                                </span>
-                            </LemonButton>
-                        </li>
-                    )
-                })}
+        // eslint-disable-next-line react/forbid-dom-props
+        <li key={key} style={style} role="listitem" tabIndex={-1} className="p-1">
+            <LemonButton active={activeOption === index} to={`https://posthog.com/${slug}`}>
+                <span>
+                    <p className="m-0 font-bold mb-1 line-clamp-1">{title}</p>
+                    <p className="text-xs m-0 opacity-80 line-clamp-1">/{slug}</p>
+                </span>
+            </LemonButton>
+        </li>
+    )
+}
+
+const Hits = ({ activeOption }: { activeOption?: number }): JSX.Element => {
+    const { hits } = useHits()
+    return (
+        <div className="relative flex">
+            <ol
+                role="listbox"
+                className="list-none m-0 p-0 absolute w-full bg-white z-50 border rounded-lg mt-1 divide-y flex-grow h-[300px]"
+            >
+                <AutoSizer>
+                    {({ height, width }: { height: number; width: number }) => (
+                        <List
+                            scrollToIndex={activeOption}
+                            width={width}
+                            height={height}
+                            rowCount={hits.length}
+                            rowHeight={50}
+                            rowRenderer={(options: any) => rowRenderer({ ...options, hits, activeOption })}
+                        />
+                    )}
+                </AutoSizer>
             </ol>
         </div>
     )
@@ -31,10 +54,16 @@ const Hits = (): JSX.Element => {
 const Search = ({
     value,
     setValue,
+    setActiveOption,
+    activeOption,
 }: {
     value: string
     setValue: React.Dispatch<React.SetStateAction<string>>
+    setActiveOption: React.Dispatch<React.SetStateAction<number | undefined>>
+    activeOption?: number
 }): JSX.Element => {
+    const { openSidePanel } = useActions(sidePanelStateLogic)
+    const { hits } = useHits()
     const { refine } = useSearchBox()
 
     const handleChange = (value: string): void => {
@@ -42,16 +71,75 @@ const Search = ({
         refine(value)
     }
 
-    return <LemonInput onChange={handleChange} value={value} type="search" fullWidth placeholder="Search..." />
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+        switch (e.key) {
+            case 'Enter':
+                if (activeOption !== undefined) {
+                    openSidePanel(SidePanelTab.Docs, `https://posthog.com/${hits[activeOption].slug}`)
+                }
+                break
+            case 'Escape':
+                setActiveOption(undefined)
+                break
+            case 'ArrowDown':
+                setActiveOption((currOption) => {
+                    if (currOption === undefined) {
+                        return 0
+                    }
+                    if (currOption >= hits.length - 1) {
+                        return currOption
+                    }
+                    return currOption + 1
+                })
+                break
+            case 'ArrowUp':
+                setActiveOption((currOption) => {
+                    if (currOption !== undefined) {
+                        return currOption <= 0 ? undefined : currOption - 1
+                    }
+                })
+        }
+    }
+
+    const handleBlur = (): void => {
+        setActiveOption(undefined)
+    }
+
+    return (
+        <LemonInput
+            onBlur={handleBlur}
+            onKeyDown={handleKeyDown}
+            onChange={handleChange}
+            value={value}
+            type="search"
+            fullWidth
+            placeholder="Search..."
+        />
+    )
 }
 
 export default function AlgoliaSearch(): JSX.Element {
     const [searchValue, setSearchValue] = useState<string>('')
+    const [activeOption, setActiveOption] = useState<undefined | number>()
+    const [searchOpen, setSearchOpen] = useState(false)
+
+    useEffect(() => {
+        setSearchOpen(!!searchValue)
+    }, [searchValue])
+
+    useEffect(() => {
+        setSearchOpen(!!searchValue && activeOption !== undefined && activeOption >= -1)
+    }, [activeOption])
 
     return (
         <InstantSearch searchClient={searchClient} indexName="prod_posthog_com">
-            <Search value={searchValue} setValue={setSearchValue} />
-            {searchValue && <Hits />}
+            <Search
+                activeOption={activeOption}
+                setActiveOption={setActiveOption}
+                value={searchValue}
+                setValue={setSearchValue}
+            />
+            {searchOpen && <Hits activeOption={activeOption} />}
         </InstantSearch>
     )
 }
