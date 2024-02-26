2d0abf38356fcbfdcd139ec4d932f0cc26b0d4e7
Ben White <ben@posthog.com>
2024-02-26 16:28:14 +0100

    Fixed up api calls to allow v3

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -203,6 +203,7 @@ export const FEATURE_FLAGS = {
     REPLAY_SIMILAR_RECORDINGS: 'session-replay-similar-recordings', // owner: #team-replay
     SAVED_NOT_PINNED: 'saved-not-pinned', // owner: #team-replay
     EXPORTS_SIDEPANEL: 'exports-sidepanel', // owner: #team-product-analytics
+    SESSION_REPLAY_V3_INGESTION_PLAYBACK: 'session-replay-v3-ingestion-playback', // owner: @benjackwhite
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
