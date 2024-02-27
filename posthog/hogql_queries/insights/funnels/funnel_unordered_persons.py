67c678df90cd2a1fd0fe9fcb8fd35b2946a4c5e8
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-02-27 20:28:43 +0100

    extra fields

--- posthog/hogql_queries/insights/funnels/funnel_unordered_persons.py
+++ posthog/hogql_queries/insights/funnels/funnel_unordered_persons.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from posthog.hogql import ast
 from posthog.hogql.parser import parse_expr
@@ -19,13 +19,13 @@ class FunnelUnorderedActors(FunnelUnordered):
 
     def actor_query(
         self,
-        # extra_fields: Optional[List[str]] = None,
+        extra_fields: Optional[List[str]] = None,
     ) -> ast.SelectQuery:
         select: List[ast.Expr] = [
             ast.Alias(alias="actor_id", expr=ast.Field(chain=["aggregation_target"])),
             *self._get_funnel_person_step_events(),
             *self._get_timestamp_outer_select(),
-            # {extra_fields}
+            *([ast.Field(chain=[field]) for field in extra_fields or []]),
         ]
         select_from = ast.JoinExpr(table=self.get_step_counts_query())
         where = self._get_funnel_person_step_condition()
