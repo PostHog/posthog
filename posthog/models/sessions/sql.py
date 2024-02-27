743543df7cb545b1cb38ea69e1077240b73a0930
Robbie <robbie.coomber@gmail.com>
2024-02-27 17:40:33 +0000

    Update table definition

--- posthog/models/sessions/sql.py
+++ posthog/models/sessions/sql.py
@@ -22,21 +22,29 @@ CREATE TABLE IF NOT EXISTS {table_name} ON CLUSTER '{cluster}'
     -- this is fine since even if the distinct_id changes during a session
     -- it will still (or should still) map to the same person
     distinct_id SimpleAggregateFunction(any, String),
+
     min_first_timestamp SimpleAggregateFunction(min, DateTime64(6, 'UTC')),
     max_last_timestamp SimpleAggregateFunction(max, DateTime64(6, 'UTC')),
 
     urls SimpleAggregateFunction(groupUniqArrayArray, Array(String)),
     entry_url AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
     exit_url AggregateFunction(argMax, String, DateTime64(6, 'UTC')),
+
+    initial_referring_domain AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
     initial_utm_source AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
     initial_utm_campaign AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
     initial_utm_medium AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
     initial_utm_term AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
     initial_utm_content AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
-    initial_referring_domain AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
+    initial_gclid AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
+    initial_gad_source AggregateFunction(argMin, String, DateTime64(6, 'UTC')),
 
-    event_count SimpleAggregateFunction(sum, Int64),
+    -- create a map of how many times we saw each event
+    event_count_map AggregateFunction(sumMap, Tuple(Array(String), Array(Int64))),
+    -- duplicate the event count as a specific column for pageviews and autocaptures,
+    -- as these are used in some key queries and need to be fast
     pageview_count SimpleAggregateFunction(sum, Int64),
+    autocapture_count SimpleAggregateFunction(sum, Int64),
 ) ENGINE = {engine}
 """
 
@@ -54,9 +62,9 @@ SESSIONS_TABLE_SQL = lambda: (
     -- we want the fewest rows possible but also the fastest queries
     -- since we query by date and not by time
     -- and order by must be in order of increasing cardinality
-    -- so we order by date first, then team_id, then distinct_id, then session_id
+    -- so we order by date first, then team_id, then session_id
     -- hopefully, this is a good balance between the two
-    ORDER BY (toDate(min_first_timestamp), team_id, distinct_id, session_id)
+    ORDER BY (toDate(min_first_timestamp), team_id, session_id)
 SETTINGS index_granularity=512
 """
 ).format(
@@ -70,33 +78,33 @@ SESSIONS_TABLE_MV_SQL = (
 CREATE MATERIALIZED VIEW IF NOT EXISTS {table_name} ON CLUSTER '{cluster}'
 TO {database}.{target_table}
 AS SELECT
-$session_id as session_id,
+
+`$session_id` as session_id,
 team_id,
+
+-- it doesn't matter which distinct_id gets picked (it'll be somewhat random) as they can all join to the right person
 any(distinct_id) as distinct_id,
+
 min(timestamp) AS min_first_timestamp,
 max(timestamp) AS max_last_timestamp,
--- TRICKY: ClickHouse will pick a relatively random "entry"
--- when it collapses the aggregating merge tree
--- unless we teach it what we want...
--- argMin ignores null values
--- so this will get the first non-null value of "entry"
--- for each group of session_id and team_id
--- by min of first_timestamp in the batch
--- this is an aggregate function, not a simple aggregate function
--- so we have to write to argMinState, and query with argMinMerge
 
 groupUniqArray(JSONExtractString(properties, '$current_url')) AS urls,
 argMinState(JSONExtractString(properties, '$current_url'), timestamp) as entry_url,
 argMaxState(JSONExtractString(properties, '$current_url'), timestamp) as exit_url,
+
+argMinState(JSONExtractString(properties, '$referring_domain'), timestamp) as initial_referring_domain,
 argMinState(JSONExtractString(properties, 'utm_source'), timestamp) as initial_utm_source,
 argMinState(JSONExtractString(properties, 'utm_campaign'), timestamp) as initial_utm_campaign,
 argMinState(JSONExtractString(properties, 'utm_medium'), timestamp) as initial_utm_medium,
 argMinState(JSONExtractString(properties, 'utm_term'), timestamp) as initial_utm_term,
--- NOTE: this isn't materialized...
 argMinState(JSONExtractString(properties, 'utm_content'), timestamp) as initial_utm_content,
-argMinState(JSONExtractString(properties, '$referring_domain'), timestamp) as initial_referring_domain,
+argMinState(JSONExtractString(properties, 'gclid'), timestamp) as initial_gclid,
+argMinState(JSONExtractString(properties, 'gad_source'), timestamp) as initial_gad_source,
+
+sumMapState(([event], [toInt64(1)])) as event_count_map,
 count(*) as event_count,
-sumIf(1, event='$pageview') as pageview_count
+sumIf(1, event='$pageview') as pageview_count,
+sumIf(1, event='$autocapture') as autocapture_count
 
 FROM {database}.sharded_events
 group by session_id, team_id
@@ -116,7 +124,8 @@ WRITABLE_SESSIONS_TABLE_SQL = lambda: SESSIONS_TABLE_BASE_SQL.format(
     cluster=settings.CLICKHOUSE_CLUSTER,
     engine=Distributed(
         data_table=SESSIONS_DATA_TABLE(),
-        sharding_key="sipHash64(distinct_id)",
+        # shard via session_id so that all events for a session are on the same shard
+        sharding_key="sipHash64(session_id)",
     ),
 )
 
@@ -126,6 +135,6 @@ DISTRIBUTED_SESSIONS_TABLE_SQL = lambda: SESSIONS_TABLE_BASE_SQL.format(
     cluster=settings.CLICKHOUSE_CLUSTER,
     engine=Distributed(
         data_table=SESSIONS_DATA_TABLE(),
-        sharding_key="sipHash64(distinct_id)",
+        sharding_key="sipHash64(session_id)",
     ),
 )
