493a17f8402f4ca48bc91d2e0f123aed25109c9f
Paul D'Ambra <paul@posthog.com>
2024-02-28 16:45:36 +0000

    don't change so much at once

--- posthog/settings/session_replay.py
+++ posthog/settings/session_replay.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from posthog.settings import get_from_env, get_list, DEBUG
+from posthog.settings import get_from_env, get_list
 from posthog.utils import str_to_bool
 
 # TRICKY: we saw unusual memory usage behavior in EU clickhouse cluster
@@ -18,13 +18,12 @@ REALTIME_SNAPSHOTS_FROM_REDIS_ATTEMPT_TIMEOUT_SECONDS = get_from_env(
     "REALTIME_SNAPSHOTS_FROM_REDIS_ATTEMPT_TIMEOUT_SECONDS", 0.2, type_cast=float
 )
 
-REPLAY_EMBEDDINGS_CALCULATION_CELERY_INTERVAL_SECONDS = get_from_env(
-    "REPLAY_EMBEDDINGS_CALCULATION_CELERY_INTERVAL_SECONDS", 30 if DEBUG else 150, type_cast=int
-)
-
-REPLAY_EMBEDDINGS_ALLOWED_TEAMS: List[str] = get_list(get_from_env("REPLAY_EMBEDDINGS_ALLOWED_TEAM", "", type_cast=str))
 
 RECORDINGS_INGESTER_URL = get_from_env("RECORDINGS_INGESTER_URL", "")
 
-REPLAY_EMBEDDINGS_BATCH_SIZE = get_from_env("REPLAY_EMBEDDINGS_BATCH_SIZE", 20, type_cast=int)
+REPLAY_EMBEDDINGS_ALLOWED_TEAMS: List[str] = get_list(get_from_env("REPLAY_EMBEDDINGS_ALLOWED_TEAM", "", type_cast=str))
+REPLAY_EMBEDDINGS_BATCH_SIZE = get_from_env("REPLAY_EMBEDDINGS_BATCH_SIZE", 10, type_cast=int)
 REPLAY_EMBEDDINGS_MIN_DURATION_SECONDS = get_from_env("REPLAY_EMBEDDINGS_MIN_DURATION_SECONDS", 30, type_cast=int)
+REPLAY_EMBEDDINGS_CALCULATION_CELERY_INTERVAL_SECONDS = get_from_env(
+    "REPLAY_EMBEDDINGS_CALCULATION_CELERY_INTERVAL_SECONDS", 150, type_cast=int
+)
