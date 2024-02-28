1ae1cf9fd414371b4b3b3e6ac1131f09ca4c1869
Tom Owers <owerstom@gmail.com>
2024-02-28 15:14:57 +0000

    Fixed migration

--- posthog/migrations/0393_datawarehousejoin_delete_datawarehouseviewlink.py
+++ posthog/migrations/0393_datawarehousejoin_delete_datawarehouseviewlink.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0391_alter_batchexportbackfill_status_and_more"),
+        ("posthog", "0392_alter_exportedasset_export_format"),
     ]
 
     operations = [
