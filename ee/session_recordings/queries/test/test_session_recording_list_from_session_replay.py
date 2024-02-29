0604f4e6584ebb1705196d110e834274b39aeae8
Paul D'Ambra <paul@posthog.com>
2024-02-29 16:00:28 +0000

    test all settings combos for a person query

--- ee/session_recordings/queries/test/test_session_recording_list_from_session_replay.py
+++ ee/session_recordings/queries/test/test_session_recording_list_from_session_replay.py
@@ -13,7 +13,13 @@ from posthog.session_recordings.queries.session_recording_list_from_replay_summa
     SessionRecordingListFromReplaySummary,
 )
 from posthog.session_recordings.queries.test.session_replay_sql import produce_replay_summary
-from posthog.test.base import APIBaseTest, ClickhouseTestMixin, QueryMatchingTest, snapshot_clickhouse_queries
+from posthog.test.base import (
+    APIBaseTest,
+    ClickhouseTestMixin,
+    QueryMatchingTest,
+    snapshot_clickhouse_queries,
+    _create_event,
+)
 from posthog.utils import PersonOnEventsMode
 
 
@@ -21,6 +27,26 @@ class TestClickhouseSessionRecordingsListFromSessionReplay(ClickhouseTestMixin, 
     @property
     def base_time(self):
         return (now() - relativedelta(hours=1)).replace(microsecond=0, second=0)
+
+    def create_event(
+        self,
+        distinct_id,
+        timestamp,
+        team=None,
+        event_name="$pageview",
+        properties=None,
+    ):
+        if team is None:
+            team = self.team
+        if properties is None:
+            properties = {"$os": "Windows 95", "$current_url": "aloha.com/2"}
+        return _create_event(
+            team=team,
+            event=event_name,
+            timestamp=timestamp,
+            distinct_id=distinct_id,
+            properties=properties,
+        )
 
     @override_settings(
         PERSON_ON_EVENTS_OVERRIDE=True, PERSON_ON_EVENTS_V2_OVERRIDE=False, ALLOW_DENORMALIZED_PROPS_IN_LISTING=False
@@ -212,6 +238,8 @@ class TestClickhouseSessionRecordingsListFromSessionReplay(ClickhouseTestMixin, 
     def test_event_filter_with_person_properties_materialized(
         self, _name: str, poe2_enabled: bool, allow_denormalised_props: bool
     ) -> None:
+        # KLUDGE: I couldn't figure out how to use @also_test_with_materialized_columns(person_properties=["email"])
+        # KLUDGE: and the parameterized.expand decorator at the same time, so I'm manually materializing here
         materialize("events", "email", table_column="person_properties")
         materialize("person", "email")
 
@@ -226,6 +254,11 @@ class TestClickhouseSessionRecordingsListFromSessionReplay(ClickhouseTestMixin, 
             Person.objects.create(team=self.team, distinct_ids=[user_one], properties={"email": "bla"})
             Person.objects.create(team=self.team, distinct_ids=[user_two], properties={"email": "bla2"})
 
+            self.create_event(
+                user_one,
+                self.base_time,
+                properties={"$session_id": session_id_one, "$window_id": str(uuid4())},
+            )
             produce_replay_summary(
                 distinct_id=user_one,
                 session_id=session_id_one,
@@ -237,6 +270,11 @@ class TestClickhouseSessionRecordingsListFromSessionReplay(ClickhouseTestMixin, 
                 session_id=session_id_one,
                 first_timestamp=(self.base_time + relativedelta(seconds=30)),
                 team_id=self.team.id,
+            )
+            self.create_event(
+                user_two,
+                self.base_time,
+                properties={"$session_id": session_id_two, "$window_id": str(uuid4())},
             )
             produce_replay_summary(
                 distinct_id=user_two,
@@ -251,7 +289,19 @@ class TestClickhouseSessionRecordingsListFromSessionReplay(ClickhouseTestMixin, 
                 team_id=self.team.id,
             )
 
-            filter = SessionRecordingsFilter(
+            match_everyone_filter = SessionRecordingsFilter(
+                team=self.team,
+                data={"properties": []},
+            )
+
+            session_recording_list_instance = SessionRecordingListFromReplaySummary(
+                filter=match_everyone_filter, team=self.team
+            )
+            (session_recordings, _) = session_recording_list_instance.run()
+
+            assert sorted([x["session_id"] for x in session_recordings]) == sorted([session_id_one, session_id_two])
+
+            match_bla_filter = SessionRecordingsFilter(
                 team=self.team,
                 data={
                     "properties": [
@@ -265,7 +315,105 @@ class TestClickhouseSessionRecordingsListFromSessionReplay(ClickhouseTestMixin, 
                 },
             )
 
-            session_recording_list_instance = SessionRecordingListFromReplaySummary(filter=filter, team=self.team)
+            session_recording_list_instance = SessionRecordingListFromReplaySummary(
+                filter=match_bla_filter, team=self.team
+            )
+            (session_recordings, _) = session_recording_list_instance.run()
+
+            assert len(session_recordings) == 1
+            assert session_recordings[0]["session_id"] == session_id_one
+
+    @parameterized.expand(
+        [
+            ["poe and materialized columns allowed", True, True],
+            ["poe and materialized columns off", True, False],
+            ["poe off and materialized columns allowed", False, True],
+            ["neither poe nor materialized columns", False, False],
+        ]
+    )
+    @snapshot_clickhouse_queries
+    def test_event_filter_with_person_properties_not_materialized(
+        self, _name: str, poe2_enabled: bool, allow_denormalised_props: bool
+    ) -> None:
+        # KLUDGE: I couldn't figure out how to use @also_test_with_materialized_columns(person_properties=["email"])
+        # KLUDGE: and the parameterized.expand decorator at the same time,
+        # KLUDGE: so I'm manually duplicating and not materializing here
+
+        with self.settings(
+            PERSON_ON_EVENTS_V2_OVERRIDE=poe2_enabled, ALLOW_DENORMALIZED_PROPS_IN_LISTING=allow_denormalised_props
+        ):
+            user_one = "test_event_filter_with_person_properties-user"
+            user_two = "test_event_filter_with_person_properties-user2"
+            session_id_one = f"test_event_filter_with_person_properties-1-{str(uuid4())}"
+            session_id_two = f"test_event_filter_with_person_properties-2-{str(uuid4())}"
+
+            Person.objects.create(team=self.team, distinct_ids=[user_one], properties={"email": "bla"})
+            Person.objects.create(team=self.team, distinct_ids=[user_two], properties={"email": "bla2"})
+
+            self.create_event(
+                user_one,
+                self.base_time,
+                properties={"$session_id": session_id_one, "$window_id": str(uuid4())},
+            )
+            produce_replay_summary(
+                distinct_id=user_one,
+                session_id=session_id_one,
+                first_timestamp=self.base_time,
+                team_id=self.team.id,
+            )
+            produce_replay_summary(
+                distinct_id=user_one,
+                session_id=session_id_one,
+                first_timestamp=(self.base_time + relativedelta(seconds=30)),
+                team_id=self.team.id,
+            )
+            self.create_event(
+                user_two,
+                self.base_time,
+                properties={"$session_id": session_id_two, "$window_id": str(uuid4())},
+            )
+            produce_replay_summary(
+                distinct_id=user_two,
+                session_id=session_id_two,
+                first_timestamp=self.base_time,
+                team_id=self.team.id,
+            )
+            produce_replay_summary(
+                distinct_id=user_two,
+                session_id=session_id_two,
+                first_timestamp=(self.base_time + relativedelta(seconds=30)),
+                team_id=self.team.id,
+            )
+
+            match_everyone_filter = SessionRecordingsFilter(
+                team=self.team,
+                data={"properties": []},
+            )
+
+            session_recording_list_instance = SessionRecordingListFromReplaySummary(
+                filter=match_everyone_filter, team=self.team
+            )
+            (session_recordings, _) = session_recording_list_instance.run()
+
+            assert sorted([x["session_id"] for x in session_recordings]) == sorted([session_id_one, session_id_two])
+
+            match_bla_filter = SessionRecordingsFilter(
+                team=self.team,
+                data={
+                    "properties": [
+                        {
+                            "key": "email",
+                            "value": ["bla"],
+                            "operator": "exact",
+                            "type": "person",
+                        }
+                    ]
+                },
+            )
+
+            session_recording_list_instance = SessionRecordingListFromReplaySummary(
+                filter=match_bla_filter, team=self.team
+            )
             (session_recordings, _) = session_recording_list_instance.run()
 
             assert len(session_recordings) == 1
