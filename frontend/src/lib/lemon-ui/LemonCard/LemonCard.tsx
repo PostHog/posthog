1cc29db368170a95fde67faac50e6aa3ad3ea217
Bianca Yang <21014901+xrdt@users.noreply.github.com>
2024-02-29 12:41:23 -0800

    fix: Tweak the /products and config onboarding pages to better match mocks. (#20390)
    * fix up /products page, fix up config pages
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * remove commented out code
    
    * Revert "Update UI snapshots for `chromium` (1)"
    
    This reverts commit d0994ba16aa92fb5096c9c55ca1990cd9094360a.
    
    * Revert "Update UI snapshots for `chromium` (2)"
    
    This reverts commit f78b780a5be794394d656173ddd324b6d94f9207.
    
    * type errors
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * pr comments
    
    * fix small visual nit
    
    * merge, fix up test
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * types!
    
    * fix: allow usage exceeded alert to be dismissed (#20282)
    
    * allow usage exceeded alert to be dismissed
    
    * Update UI snapshots for `chromium` (1)
    
    * forgot a css change
    
    * reset dismissKey if status reverts to not usage exceeded
    
    * fix the cookie reset logic
    
    * hide billing notice on intro screens
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `webkit` (2)
    
    * Revert "Update UI snapshots for `chromium` (1)"
    
    This reverts commit 58f0a2dd0525678fdd3c4d151a8e271e7ed5b73f.
    
    * temp for attempting to use logic to reset localstorage
    
    * fix up my banner logic reset
    
    * Update UI snapshots for `webkit` (2)
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * undo changes made for testing purposes
    
    * Revert "Update UI snapshots for `chromium` (2)"
    
    This reverts commit 9a70d4fce4bea975af395aa5d03d01ce28e24d26.
    
    * Revert "Update UI snapshots for `webkit` (2)"
    
    This reverts commit 8bd00799be40158ffe8436fd9f09b8e03f942eae.
    
    * Revert "Update UI snapshots for `chromium` (1)"
    
    This reverts commit cc8b660001a3094d36fe10517ee78fac5a47ccb8.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    
    * remove scss file, center vert /products
    
    * Revert "Update UI snapshots for `chromium` (2)"
    
    This reverts commit 6954c5f56322690aeac988aa898eeca399e6353f.
    
    * Revert "Update UI snapshots for `chromium` (2)"
    
    This reverts commit ce9cdaeb57292c95a831e60fe9d1d127ee415db9.
    
    * Revert "Update UI snapshots for `chromium` (2)"
    
    This reverts commit 1a3d6a7e516205890126cacedc1898f041bc8437.
    
    * Revert "Update UI snapshots for `chromium` (2)"
    
    This reverts commit 0f419bf773fac85120cbfb49a875e2fc1df054d5.
    
    * delete snapshots
    
    * clean up some more snapshots
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/lib/lemon-ui/LemonCard/LemonCard.tsx
+++ frontend/src/lib/lemon-ui/LemonCard/LemonCard.tsx
@@ -6,15 +6,24 @@ export interface LemonCardProps {
     children?: React.ReactNode
     onClick?: () => void
     focused?: boolean
+    'data-attr'?: string
 }
 
-export function LemonCard({ hoverEffect = true, className, children, onClick, focused }: LemonCardProps): JSX.Element {
+export function LemonCard({
+    hoverEffect = true,
+    className,
+    children,
+    onClick,
+    focused,
+    ...props
+}: LemonCardProps): JSX.Element {
     return (
         <div
             className={`LemonCard ${hoverEffect && 'LemonCard--hoverEffect'} border ${
                 focused ? 'border-2 border-primary' : 'border-border'
             } rounded p-6 bg-bg-light ${className}`}
             onClick={onClick}
+            {...props}
         >
             {children}
         </div>
