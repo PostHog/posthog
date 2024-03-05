e437a3fbcb47d19a38458b9e09553d5b2de950ee
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-03-05 12:53:41 -0800

    remove UI_HOST

--- frontend/src/toolbar/posthog.ts
+++ frontend/src/toolbar/posthog.ts
@@ -5,11 +5,9 @@ const DEFAULT_API_KEY = 'sTMFPsFhdP1Ssg'
 const runningOnPosthog = !!window.POSTHOG_APP_CONTEXT
 const apiKey = runningOnPosthog ? window.JS_POSTHOG_API_KEY : DEFAULT_API_KEY
 const apiHost = runningOnPosthog ? window.JS_POSTHOG_HOST : 'https://internal-e.posthog.com'
-const uiHost = runningOnPosthog ? window.JS_POSTHOG_UI_HOST : 'https://app.posthog.com'
 
 export const posthog = new PostHog(apiKey || DEFAULT_API_KEY, {
     host: apiHost,
-    ui_host: uiHost,
     enable: false, // must call .optIn() before any events are sent
     persistence: 'memory', // We don't want to persist anything, all events are in-memory
     persistence_name: apiKey + '_toolbar', // We don't need this but it ensures we don't accidentally mess with the standard persistence
