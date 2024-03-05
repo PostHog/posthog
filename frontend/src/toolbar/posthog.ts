1b4701bf270201e04b546b21175f433615aadd13
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-03-05 12:42:08 -0800

    Update frontend/src/toolbar/posthog.ts
    Co-authored-by: danielxnj <115043334+danielxnj@users.noreply.github.com>

--- frontend/src/toolbar/posthog.ts
+++ frontend/src/toolbar/posthog.ts
@@ -4,7 +4,7 @@ const DEFAULT_API_KEY = 'sTMFPsFhdP1Ssg'
 
 const runningOnPosthog = !!window.POSTHOG_APP_CONTEXT
 const apiKey = runningOnPosthog ? window.JS_POSTHOG_API_KEY : DEFAULT_API_KEY
-const apiHost = runningOnPosthog ? window.JS_POSTHOG_HOST : 'https://d1iyf54ud053a4.cloudfront.net'
+const apiHost = runningOnPosthog ? window.JS_POSTHOG_HOST : 'https://internal-e.posthog.com'
 const uiHost = runningOnPosthog ? window.JS_POSTHOG_UI_HOST : 'https://app.posthog.com'
 
 export const posthog = new PostHog(apiKey || DEFAULT_API_KEY, {
