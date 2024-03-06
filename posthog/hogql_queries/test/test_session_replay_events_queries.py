20d4c9b1bfe0b4849d211d04cec79a1a1485562e
Paul D'Ambra <paul@posthog.com>
2024-03-06 10:57:51 +0000

    fix the test

--- posthog/hogql_queries/test/test_session_replay_events_queries.py
+++ posthog/hogql_queries/test/test_session_replay_events_queries.py
@@ -1,26 +1,42 @@
-from typing import cast
-
-from posthog.hogql import ast
-from posthog.hogql.ast import SelectQuery
-from posthog.hogql.visitor import clear_locations
 from posthog.hogql_queries.hogql_query_runner import HogQLQueryRunner
 from posthog.schema import HogQLQuery
+from posthog.session_recordings.queries.test.session_replay_sql import produce_replay_summary
 from posthog.test.base import ClickhouseTestMixin, APIBaseTest
 
 
 class TestSessionReplayEventsHogQLQueries(ClickhouseTestMixin, APIBaseTest):
     def test_session_replay_events_table_is_always_grouped_by_session_id(self):
-        # test that the "not raw" table is always grouped by session id
-        runner = HogQLQueryRunner(
-            team=self.team, query=HogQLQuery(query="select session_id from session_replay_events")
+        produce_replay_summary(
+            team_id=self.team.id,
+            session_id="session_id_one",
+            distinct_id="distinct_id_one",
+            mouse_activity_count=100,
+        )
+        produce_replay_summary(
+            team_id=self.team.id,
+            session_id="session_id_two",
+            distinct_id="distinct_id_two",
+            mouse_activity_count=200,
+        )
+        produce_replay_summary(
+            team_id=self.team.id,
+            session_id="session_id_two",
+            distinct_id="distinct_id_two",
+            mouse_activity_count=300,
         )
 
-        query: SelectQuery = cast(SelectQuery, clear_locations(runner.to_query()))
+        # test that the "not raw" table is always grouped by session id
+        runner = HogQLQueryRunner(
+            team=self.team,
+            query=HogQLQuery(
+                query="""
+            select distinct_id, sum(mouse_activity_count) as mouse_activity_count
+            from session_replay_events
+            group by distinct_id -- this table is also always implicitly grouped by session id
+            order by mouse_activity_count desc
+            """
+            ),
+        )
 
-        assert query.select == [ast.Field(chain=["session_id"])]
-        assert query.select_from == ast.JoinExpr(table=ast.Field(chain=["session_replay_events"]))
-        assert query.group_by == [ast.Field(chain=["session_replay_events", "session_id"])]
-
-        # TODO add data so this does something
         response = runner.calculate()
-        assert response.results == ["definitely fail when you get here"]
+        assert response.results == [("distinct_id_two", 500), ("distinct_id_one", 100)]
