616ffafac27996a3d58c987e93a6a76dd6a286d3
Robbie <robbie.coomber@gmail.com>
2024-03-06 10:41:32 +0000

    Make the backfill work by day

--- posthog/management/commands/backfill_sessions_table.py
+++ posthog/management/commands/backfill_sessions_table.py
@@ -5,11 +5,11 @@ from dataclasses import dataclass
 from typing import Optional
 
 import structlog
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from posthog.clickhouse.client.execute import sync_execute
 from posthog.models.property.util import get_property_string_expr
-from posthog.models.team.team import Team
+from datetime import datetime, timedelta
 
 logger = structlog.get_logger(__name__)
 
@@ -19,17 +19,23 @@ TARGET_TABLE = "sessions"
 
 @dataclass
 class BackfillQuery:
-    team_id: int
+    start_date: datetime
+    end_date: datetime
 
-    def execute(self, dry_run: bool = True, month: Optional[str] = None) -> None:
-        where = "true"
-        if month is not None:
-            where = f"toYYYYMM(timestamp) < {month}"
-
+    def execute(
+        self,
+        dry_run: bool = True,
+    ) -> None:
         def source_column(column_name: str) -> str:
             return get_property_string_expr(
                 "events", property_name=column_name, var=f"'{column_name}'", column="properties"
             )[0]
+
+        num_days = (self.end_date - self.start_date).days + 1
+
+        logger.info(
+            f"Backfilling sessions table from {self.start_date.strftime('%Y-%m-%d')} to {self.end_date.strftime('%Y-%m-%d')}, total numbers of days to insert: {num_days}"
+        )
 
         current_url_property = source_column("$current_url")
         referring_domain_property = source_column("$referring_domain")
@@ -41,7 +47,13 @@ class BackfillQuery:
         gclid_property = source_column("gclid")
         gad_source_property = source_column("gad_source")
 
-        select_query = f"""
+        def select_query(date: Optional[datetime] = None) -> str:
+            if date:
+                where = f"toStartOfDay(timestamp) = '{date.strftime('%Y-%m-%d')}'"
+            else:
+                where = "true"
+
+            return f"""
 SELECT
     `$session_id` as session_id,
     team_id,
@@ -69,26 +81,29 @@ SELECT
     if(event='$autocapture', 1, NULL) as autocapture_count
 
 FROM events
-WHERE team_id = %(team_id)s AND `$session_id` IS NOT NULL AND `$session_id` != '' AND {where}
+WHERE `$session_id` IS NOT NULL AND `$session_id` != '' AND {where}
         """
 
         # print the count of entries in the main sessions table
         count_query = f"SELECT count(), uniq(session_id) FROM {TARGET_TABLE}"
-        [(sessions_row_count, sessions_event_count)] = sync_execute(count_query, {"team_id": self.team_id})
+        [(sessions_row_count, sessions_event_count)] = sync_execute(count_query)
         logger.info(f"{sessions_row_count} rows and {sessions_event_count} unique session_ids in sessions table")
 
         if dry_run:
-            count_query = f"SELECT count(), uniq(session_id) FROM ({select_query})"
-            [(events_count, sessions_count)] = sync_execute(count_query, {"team_id": self.team_id})
-            logger.info(f"{events_count} events and {sessions_count} sessions to backfill for team {self.team_id}")
+            count_query = f"SELECT count(), uniq(session_id) FROM ({select_query()})"
+            [(events_count, sessions_count)] = sync_execute(count_query)
+            logger.info(f"{events_count} events and {sessions_count} sessions to backfill for")
+            logger.info(f"The first select query would be:\n{select_query(self.start_date)}")
             return
 
-        logging.info("Populating the import table with the data we want to backfill, using a partition expression")
-        sync_execute(f"""INSERT INTO writable_sessions {select_query}""", {"team_id": self.team_id})
+        for i in range(num_days):
+            date = self.start_date + timedelta(days=i)
+            logging.info("Writing the sessions for day %s", date.strftime("%Y-%m-%d"))
+            sync_execute(f"""INSERT INTO writable_sessions {select_query(date=date)}""")
 
         # print the count of entries in the main sessions table
         count_query = f"SELECT count(), uniq(session_id) FROM {TARGET_TABLE}"
-        [(sessions_row_count, sessions_event_count)] = sync_execute(count_query, {"team_id": self.team_id})
+        [(sessions_row_count, sessions_event_count)] = sync_execute(count_query)
         logger.info(f"{sessions_row_count} rows and {sessions_event_count} unique session_ids in sessions table")
 
 
@@ -96,16 +111,20 @@ class Command(BaseCommand):
     help = "Backfill person_distinct_id_overrides records."
 
     def add_arguments(self, parser):
-        parser.add_argument("--team-id", required=True, type=int, help="team to backfill for")
-        parser.add_argument("--month", type=str, help="month to backfill for (format: YYYYMM)")
+        parser.add_argument(
+            "--start-date", required=True, type=str, help="first day to run backfill on (format YYYY-MM-DD)"
+        )
+        parser.add_argument(
+            "--end-date", required=True, type=str, help="last day to run backfill, inclusive, on (format YYYY-MM-DD)"
+        )
         parser.add_argument(
             "--live-run", action="store_true", help="actually execute INSERT queries (default is dry-run)"
         )
 
-    def handle(self, *, live_run: bool, team_id: int, month: str, **options):
+    def handle(self, *, live_run: bool, start_date: str, end_date: str, **options):
         logger.setLevel(logging.INFO)
 
-        if not Team.objects.filter(id=team_id).exists():
-            raise CommandError(f"Team with id={team_id!r} does not exist")
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
 
-        BackfillQuery(team_id).execute(dry_run=not live_run)
+        BackfillQuery(start_date, end_date).execute(dry_run=not live_run)
