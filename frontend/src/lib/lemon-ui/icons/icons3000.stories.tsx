b0e538c14661648314dff8c9404d5debc7e9f72d
David Newell <david@posthog.com>
2024-03-07 15:47:50 +0000

    categorise most icons

--- frontend/src/lib/lemon-ui/icons/icons3000.stories.tsx
+++ frontend/src/lib/lemon-ui/icons/icons3000.stories.tsx
@@ -1,4 +1,4 @@
-import * as icons from '@posthog/icons'
+import * as packageIcons from '@posthog/icons'
 import { Meta } from '@storybook/react'
 
 import { LemonCollapse } from '../LemonCollapse'
@@ -9,7 +9,7 @@ const meta: Meta = {
 }
 export default meta
 
-const posthogIcons = Object.entries(icons)
+const posthogIcons = Object.entries(packageIcons)
     .filter(([key]) => key !== 'IconBase')
     .map(([key, Icon]) => ({ name: key, icon: Icon }))
 
@@ -21,6 +21,125 @@ const CATEGORIES = {
         'IconArrowCircleRight',
         'IconArrowRightDown',
         'IconArrowUpRight',
+        'IconCollapse',
+        'IconExpand',
+        'IconCollapse45',
+        'IconExpand45',
+        'IconChevronDown',
+    ],
+    Logos: ['IconLogomark', 'IconGithub', 'IconHogQL'],
+    Analytics: [
+        'IconCorrelationAnalysis',
+        'IconGraph',
+        'IconLineGraph',
+        'IconRetention',
+        'IconFunnels',
+        'IconGanttChart',
+        'IconTrending',
+        'IconTrends',
+        'IconLifecycle',
+        'IconPieChart',
+        'IconUserPaths',
+    ],
+    Replay: [
+        'IconPlay',
+        'IconPlayFilled',
+        'IconPlaylist',
+        'IconPause',
+        'IconPauseFilled',
+        'IconRewind',
+        'IconRecord',
+        'IconRewindPlay',
+        'IconVideoCamera',
+    ],
+    'Feature Success': ['IconFlask', 'IconTestTube', 'IconMultivariateTest', 'IconSplitTesting'],
+    Actions: [
+        'IconCopy',
+        'IconTrash',
+        'IconUndo',
+        'IconRedo',
+        'IconRevert',
+        'IconSearch',
+        'IconUpload',
+        'IconShare',
+        'IconDownload',
+        'IconLeave',
+        'IconPin',
+        'IconPinFilled',
+        'IconPencil',
+        'IconOpenSidebar',
+        'IconFilter',
+        'IconArchive',
+    ],
+    Mathematics: [
+        'IconPlus',
+        'IconPlusSmall',
+        'IconPlusSquare',
+        'IconMinus',
+        'IconMinusSmall',
+        'IconMinusSquare',
+        'IconMultiply',
+        'IconPercentage',
+        'IconCalculator',
+    ],
+    'Messaging & Communication': [
+        'IconSend',
+        'IconHeadset',
+        'IconMessage',
+        'IconNotification',
+        'IconLetter',
+        'IconNewspaper',
+        'IconChat',
+        'IconThoughtBubble',
+    ],
+    Devices: [
+        'IconLaptop',
+        'IconPhone',
+        'IconWebcam',
+        'IconMicrophone',
+        'IconKeyboard',
+        'IconServer',
+        'IconDatabase',
+        'IconHardDrive',
+    ],
+    Objects: [
+        'IconPalette',
+        'IconCloud',
+        'IconCart',
+        'IconMegaphone',
+        'IconRocket',
+        'IconMap',
+        'IconClock',
+        'IconTie',
+        'IconClock',
+        'IconCoffee',
+        'IconPiggyBank',
+        'IconFlag',
+        'IconCreditCard',
+        'IconHourglass',
+        'IconCalendar',
+        'IconCrown',
+        'IconBolt',
+        'IconBook',
+        'IconStore',
+        'IconConfetti',
+        'IconPresent',
+        'IconMagicWand',
+        'IconMagic',
+        'IconHelmet',
+        'IconSpotlight',
+    ],
+    UI: [
+        'IconPassword',
+        'IconToggle',
+        'IconLoading',
+        'IconSpinner',
+        'IconBrightness',
+        'IconCursor',
+        'IconCursorBox',
+        'IconCursorClick',
+        'IconToolbar',
+        'IconToolbarFilled',
     ],
 }
 
@@ -29,7 +148,7 @@ const IconTemplate = ({ icons }: { icons: { name: string; icon: any }[] }): JSX.
         <div className="grid grid-cols-6 gap-4">
             {icons.map(({ name, icon: Icon }) => {
                 return (
-                    <div key={name} className="flex flex-col items-center">
+                    <div key={name} className="flex flex-col items-center space-y-2">
                         <Icon className="w-10 h-10" />
                         <span className="text-xs">{name}</span>
                     </div>
@@ -48,14 +167,14 @@ export function CategoricalIcons(): JSX.Element {
         <LemonCollapse
             multiple
             panels={[
-                ...Object.keys(CATEGORIES).map((key) => {
+                ...Object.entries(CATEGORIES).map(([key, icons]) => {
                     return {
                         key,
                         header: key,
                         content: (
                             <IconTemplate
-                                icons={Object.entries(key).map(([_, icon]) => {
-                                    return { name: icon.name, icon: icon }
+                                icons={icons.map((icon) => {
+                                    return { name: icon, icon: packageIcons[icon] }
                                 })}
                             />
                         ),
