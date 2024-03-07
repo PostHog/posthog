3b0d3725e89d0a64454d0491772d3a475c2554dc
David Newell <david@posthog.com>
2024-03-07 10:25:42 +0000

    feat: add error embedding method

--- ee/session_recordings/ai/generate_embeddings.py
+++ ee/session_recordings/ai/generate_embeddings.py
@@ -76,8 +76,52 @@ BATCH_FLUSH_SIZE = settings.REPLAY_EMBEDDINGS_BATCH_SIZE
 MIN_DURATION_INCLUDE_SECONDS = settings.REPLAY_EMBEDDINGS_MIN_DURATION_SECONDS
 MAX_TOKENS_FOR_MODEL = 8191
 
+openai_client = OpenAI()
 
-def fetch_recordings_without_embeddings(team: Team | int, offset=0) -> List[str]:
+
+def fetch_errors_by_session_without_embeddings(team: Team | int, offset=0) -> List[str]:
+    if isinstance(team, int):
+        team = Team.objects.get(id=team)
+
+    query = """
+            WITH embedded_sessions AS
+            (
+                SELECT
+                    session_id
+                from
+                    session_replay_embeddings
+                where
+                    team_id = %(team_id)s
+                    -- don't load all data for all time
+                    AND generation_timestamp > now() - INTERVAL 7 DAY
+                    AND source_type = 'error'
+            ),
+            SELECT log_source_id as session_id, message
+            FROM log_entries
+            PREWHERE team_id = 2
+                    AND log_source = 'session_replay'
+                    AND timestamp <= now()
+                    AND timestamp >= now() - INTERVAL 7 DAY
+                    AND session_id NOT IN embedded_sessions
+            GROUP BY session_id, message
+            LIMIT %(batch_flush_size)s
+            -- when running locally the offset is used for paging
+            -- when running in celery the offset is not used
+            OFFSET %(offset)s
+        """
+
+    return sync_execute(
+        query,
+        {
+            "team_id": team.pk,
+            "batch_flush_size": BATCH_FLUSH_SIZE,
+            "offset": offset,
+            "min_duration_include_seconds": MIN_DURATION_INCLUDE_SECONDS,
+        },
+    )
+
+
+def fetch_recordings_without_embeddings(team: int, offset=0) -> List[str]:
     if isinstance(team, int):
         team = Team.objects.get(id=team)
 
@@ -127,25 +171,44 @@ def fetch_recordings_without_embeddings(team: Team | int, offset=0) -> List[str]
             OFFSET %(offset)s
         """
 
-    return [
-        x[0]
-        for x in sync_execute(
-            query,
-            {
-                "team_id": team.pk,
-                "batch_flush_size": BATCH_FLUSH_SIZE,
-                "offset": offset,
-                "min_duration_include_seconds": MIN_DURATION_INCLUDE_SECONDS,
-            },
-        )
-    ]
+    return sync_execute(
+        query,
+        {
+            "team_id": team.pk,
+            "batch_flush_size": BATCH_FLUSH_SIZE,
+            "offset": offset,
+            "min_duration_include_seconds": MIN_DURATION_INCLUDE_SECONDS,
+        },
+    )
 
 
-def embed_batch_of_recordings(recordings: List[str], team: Team | int) -> None:
+def embed_batch_of_errors(team: Team) -> None:
+    results = fetch_errors_by_session_without_embeddings(team)
+
+    batched_embeddings = []
+
+    for session_id in results:
+        try:
+            with GENERATE_RECORDING_EMBEDDING_TIMING.time():
+                embeddings = generate_recording_embeddings(session_id=session_id, team=team)
+
+            if embeddings:
+                SESSION_EMBEDDINGS_GENERATED.inc()
+                batched_embeddings.append(
+                    {"session_id": session_id, "team_id": team.pk, "embeddings": embeddings, "source_type": "error"}
+                )
+        # we don't want to fail the whole batch if only a single recording fails
+        except Exception as e:
+            SESSION_EMBEDDINGS_FAILED.inc()
+            logger.error(f"embed individual recording error", flow="embeddings", error=e)
+            # so we swallow errors here
+
+    if len(batched_embeddings) > 0:
+        flush_embeddings_to_clickhouse(embeddings=batched_embeddings)
+
+
+def embed_batch_of_recordings(recordings: List[str], team: Team) -> None:
     try:
-        if isinstance(team, int):
-            team = Team.objects.get(id=team)
-
         logger.info(
             f"processing {len(recordings)} recordings to embed for team {team.pk}", flow="embeddings", team_id=team.pk
         )
@@ -184,7 +247,9 @@ def embed_batch_of_recordings(recordings: List[str], team: Team | int) -> None:
 
 def flush_embeddings_to_clickhouse(embeddings: List[Dict[str, Any]]) -> None:
     try:
-        sync_execute("INSERT INTO session_replay_embeddings (session_id, team_id, embeddings) VALUES", embeddings)
+        sync_execute(
+            "INSERT INTO session_replay_embeddings (session_id, team_id, embeddings, source_type) VALUES", embeddings
+        )
         SESSION_EMBEDDINGS_WRITTEN_TO_CLICKHOUSE.inc(len(embeddings))
     except Exception as e:
         logger.error(f"flush embeddings error", flow="embeddings", error=e)
@@ -196,8 +261,6 @@ def generate_recording_embeddings(session_id: str, team: Team | int) -> List[flo
     logger.info(f"generating embedding for session", flow="embeddings", session_id=session_id)
     if isinstance(team, int):
         team = Team.objects.get(id=team)
-
-    client = OpenAI()
 
     eight_days_ago = datetime.datetime.now(pytz.UTC) - datetime.timedelta(days=8)
     session_metadata = SessionReplayEvents().get_metadata(
@@ -254,30 +317,33 @@ def generate_recording_embeddings(session_id: str, team: Team | int) -> List[flo
 
     logger.info(f"generating embedding input for session", flow="embeddings", session_id=session_id)
 
+    embeddings = embed_input(input)
+
+    logger.info(f"generated embedding input for session", flow="embeddings", session_id=session_id)
+
+    return embeddings
+
+
+def embed_input(input: str):
     token_count = num_tokens_for_input(input)
     RECORDING_EMBEDDING_TOKEN_COUNT.observe(token_count)
     if token_count > MAX_TOKENS_FOR_MODEL:
         logger.error(
             f"embedding input exceeds max token count for model",
             flow="embeddings",
-            session_id=session_id,
             input=json.dumps(input),
         )
         SESSION_SKIPPED_WHEN_GENERATING_EMBEDDINGS.inc()
         return None
 
-    embeddings = (
-        client.embeddings.create(
+    return (
+        openai_client.embeddings.create(
             input=input,
             model="text-embedding-3-small",
         )
         .data[0]
         .embedding
     )
-
-    logger.info(f"generated embedding input for session", flow="embeddings", session_id=session_id)
-
-    return embeddings
 
 
 def num_tokens_for_input(string: str) -> int:
