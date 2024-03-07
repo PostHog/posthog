eb5c4116d89e63c4fb8d1b067f7a0b5fb1195402
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-03-07 18:37:05 +0100

    fix: Import path

--- posthog/clickhouse/migrations/0055_add_minmax_index_on_inserted_at.py
+++ posthog/clickhouse/migrations/0055_add_minmax_index_on_inserted_at.py
@@ -1,5 +1,5 @@
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.models.events.sql import EVENTS_TABLE_INSERTED_AT_INDEX_SQL, EVENTS_TABLE_MATERIALIZE_INSERTED_AT_INDEX_SQL
+from posthog.models.event.sql import EVENTS_TABLE_INSERTED_AT_INDEX_SQL, EVENTS_TABLE_MATERIALIZE_INSERTED_AT_INDEX_SQL
 
 operations = [
     run_sql_with_exceptions(EVENTS_TABLE_INSERTED_AT_INDEX_SQL),
