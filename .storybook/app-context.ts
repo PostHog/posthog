92811992d721b85e58932ce4df55003abfd0c3be
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-03-07 09:01:59 -0800

    fix typo

--- .storybook/app-context.ts
+++ .storybook/app-context.ts
@@ -4,7 +4,7 @@ import { AppContext } from '~/types'
 export const getStorybookAppContext = (): AppContext => ({
     anonymous: false,
     // Ideally we wouldn't set `current_team` here, the same way we don't set `current_user`, but unfortunately
-    // as of March 2024, a bunch of logics make the assumption that this is set, via `ApiConfig`
+    // as of March 2024, a bunch of logics make the assumption that this is set, via `AppConfig`
     current_team: MOCK_DEFAULT_TEAM,
     current_user: undefined as any, // `undefined` triggers a fetch and lets us mock the data
     default_event_name: '$pageview',
