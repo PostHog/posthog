79b90cdc5987e43ad0ec45e166f0b65b6e756481
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-03-12 11:12:56 +0100

    fix: Typo in dictionary
    Co-authored-by: James Greenhill <fuziontech@gmail.com>

--- posthog/models/person/sql.py
+++ posthog/models/person/sql.py
@@ -534,7 +534,7 @@ SOURCE(CLICKHOUSE(
     query 'SELECT team_id, distinct_id, argMax(person_id, version) AS person_id FROM {database}.person_distinct_id_overrides GROUP BY team_id, distinct_id'
 ))
 LAYOUT(complex_key_hashed())
--- ClickHouse will choose a time uniformly within 1 to 5 hours to reload the dictionry (update if necessary to meet SLAs).
+-- ClickHouse will choose a time uniformly within 1 to 5 hours to reload the dictionary (update if necessary to meet SLAs).
 LIFETIME(MIN 3600 MAX 18000)
 """.format(
     cluster=CLICKHOUSE_CLUSTER,
