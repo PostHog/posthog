17d9c0ddbd8e0ae63337298afc10d2d37225f76f
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-03-14 17:01:24 +0100

    wip

--- posthog/management/commands/compare_hogql_funnel_insights.py
+++ posthog/management/commands/compare_hogql_funnel_insights.py
@@ -1,19 +1,35 @@
 from django.core.management.base import BaseCommand
+import json
+from typing import cast
+from django.db.models import Q
+from rest_framework.exceptions import ValidationError
+from posthog.queries.funnels.funnel_time_to_convert import ClickhouseFunnelTimeToConvert
+from posthog.queries.funnels.funnel_trends import ClickhouseFunnelTrends
+from posthog.schema import FunnelsQuery, HogQLQueryModifiers, MaterializationMode
+from posthog.models import Insight, Filter
+
+from posthog.hogql_queries.legacy_compatibility.filter_to_query import filter_to_query
+from posthog.hogql_queries.insights.funnels.funnels_query_runner import FunnelsQueryRunner
+
+from posthog.constants import FunnelOrderType, FunnelVizType
+from posthog.queries.funnels.utils import get_funnel_order_class
+
+
+def get_legacy_class(filter: Filter):
+    if filter.funnel_viz_type == FunnelVizType.TRENDS:
+        klass = ClickhouseFunnelTrends
+    elif filter.funnel_viz_type == FunnelVizType.TIME_TO_CONVERT:
+        klass = ClickhouseFunnelTimeToConvert
+    else:
+        klass = get_funnel_order_class(filter)
+
+    return klass
 
 
 class Command(BaseCommand):
     help = "Test if HogQL insights match their legacy counterparts"
 
     def handle(self, *args, **options):
-        import json
-        from typing import cast
-        from django.db.models import Q
-        from rest_framework.exceptions import ValidationError
-        from posthog.schema import FunnelsQuery, HogQLQueryModifiers, MaterializationMode
-        from posthog.models import Insight, Filter
-        from posthog.queries.funnels import ClickhouseFunnel
-        from posthog.hogql_queries.legacy_compatibility.filter_to_query import filter_to_query
-        from posthog.hogql_queries.insights.funnels.funnels_query_runner import FunnelsQueryRunner
 
         IGNORED_INSIGHTS = [
             "qCf2qVXC",
@@ -80,14 +96,14 @@ class Command(BaseCommand):
                 Q(filters__insight="FUNNELS")  # funnel insights
                 ## funnel viz type (pick one):
                 # & (Q(filters__funnel_viz_type="steps") | Q(filters__funnel_viz_type__isnull=True))  # steps viz
-                & Q(filters__funnel_viz_type="time_to_convert")  # time to convert viz
+                # & Q(filters__funnel_viz_type="time_to_convert")  # time to convert viz
                 # & Q(filters__funnel_viz_type="trends")  # trends viz
                 ## funnel order type (pick one):
                 # & (Q(filters__funnel_order_type="ordered") | Q(filters__funnel_order_type__isnull=True))  # ordered
                 # & Q(filters__funnel_order_type="unordered")  # unordered
                 # & Q(filters__funnel_order_type="strict")  # strict
                 # & Q(filters__breakdown__isnull=True)  # without breakdown
-                # & Q(short_id="1jrKJ0n7")
+                # & Q(short_id="A1KGprpv")
                 & ~Q(short_id__in=IGNORED_INSIGHTS) & Q(team_id=1),
                 saved=True,
                 deleted=False,
@@ -97,15 +113,21 @@ class Command(BaseCommand):
         )
 
         for insight in insights[0:99]:
-            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")  # noqa: T201
-            print(f"Checking Funnel Insight {insight.pk} {insight.short_id} - {insight.name} ")  # noqa: T201
-
             BASE_URL = f"https://us.posthog.com/project/{insight.team.pk}"
+            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")  # noqa: T201
+            print(
+                f'Checking Funnel Insight {insight.pk} {insight.short_id} - "{insight.name or insight.derived_name}"'
+            )  # noqa: T201
+            print(f"    {BASE_URL}/insights/{insight.short_id}/edit")  # noqa: T201
             legacy_results, hogql_results, legacy_error, hogql_error = None, None, None, None
 
             try:
                 filter = Filter(data=insight.filters, team=insight.team)
-                legacy_results = ClickhouseFunnel(filter, insight.team).run()
+                print(
+                    f"    {filter.funnel_order_type or FunnelOrderType.ORDERED} {filter.funnel_viz_type or FunnelVizType.STEPS}"
+                )
+                funnel_class = get_legacy_class(filter)
+                legacy_results = funnel_class(filter, insight.team).run()
             except ValidationError as e:
                 legacy_error = e
             except Exception as e:
@@ -160,31 +182,56 @@ class Command(BaseCommand):
                 assert legacy_results is not None
                 assert hogql_results is not None
 
-                sorted_legacy_results = legacy_results
-                sorted_hogql_results = hogql_results
+                if filter.funnel_viz_type == "time_to_convert":
 
-                if isinstance(legacy_results[0], list):
-                    sorter = lambda step: (step[0]["breakdown_value"])
-                    sorted_legacy_results = sorted(legacy_results, key=sorter)
-                    sorted_hogql_results = sorted(hogql_results, key=sorter)
+                    if legacy_results["average_conversion_time"] != hogql_results.average_conversion_time:
+                        all_ok = False
+                        print("MISMATCH in average_conversion_time")  # noqa: T201
+                        print("Legacy:", legacy_results["average_conversion_time"])  # noqa: T201
+                        print("HogQL:", hogql_results.average_conversion_time)  # noqa: T201
 
-                for legacy_result, hogql_result in zip(sorted_legacy_results, sorted_hogql_results):  # type: ignore
-                    if isinstance(legacy_result, list) and isinstance(hogql_result, list):
-                        for sub_legacy_result, sub_hogql_result in zip(legacy_result, hogql_result):
-                            if compare_result(insight, sub_legacy_result, sub_hogql_result) is False:
+                    bin_mismatch = False
+                    for legacy, hogql in zip(legacy_results["bins"], hogql_results.bins):
+                        if int(legacy[0]) != hogql[0]:
+                            bin_mismatch = True
+
+                        if legacy[1] != hogql[1]:
+                            bin_mismatch = True
+
+                    if bin_mismatch:
+                        print(f"MISMATCH in bins")
+                        print("Legacy:", legacy_results["bins"])  # noqa: T201
+                        print("HogQL:", hogql_results.bins)  # noqa: T201
+                else:
+                    # print(".------")
+                    # print(legacy_results["aver"])
+                    # print(hogql_results)
+                    # print("...............")
+
+                    sorted_legacy_results = legacy_results
+                    sorted_hogql_results = hogql_results
+
+                    if isinstance(legacy_results[0], list):
+                        sorter = lambda step: (step[0]["breakdown_value"])
+                        sorted_legacy_results = sorted(legacy_results, key=sorter)
+                        sorted_hogql_results = sorted(hogql_results, key=sorter)
+
+                    for legacy_result, hogql_result in zip(sorted_legacy_results, sorted_hogql_results):  # type: ignore
+                        if isinstance(legacy_result, list) and isinstance(hogql_result, list):
+                            for sub_legacy_result, sub_hogql_result in zip(legacy_result, hogql_result):
+                                if compare_result(insight, sub_legacy_result, sub_hogql_result) is False:
+                                    all_ok = False
+                        elif isinstance(legacy_result, list) or isinstance(hogql_result, list):
+                            print("Error: Inconsistent data structures.")
+                        else:
+                            if compare_result(insight, legacy_result, hogql_result) is False:
                                 all_ok = False
-                    elif isinstance(legacy_result, list) or isinstance(hogql_result, list):
-                        print("Error: Inconsistent data structures.")
-                    else:
-                        if compare_result(insight, legacy_result, hogql_result) is False:
-                            all_ok = False
 
             if all_ok:
                 print("ALL OK!")  # noqa: T201
 
 
 def compare_result(insight, legacy_result, hogql_result) -> bool:
-    BASE_URL = f"https://us.posthog.com/project/{insight.team.pk}"
     fields = [
         "action_id",
         "name",
@@ -212,10 +259,7 @@ def compare_result(insight, legacy_result, hogql_result) -> bool:
             if field == "average_conversion_time" and round(legacy, 1) == round(hogql, 1):
                 continue
 
-            print(  # noqa: T201
-                f"Insight {BASE_URL}/insights/{insight.short_id}/edit"
-                f" ({insight.pk}). MISMATCH in {legacy_result.get('order')} field {field}"
-            )
+            print(f"MISMATCH in {legacy_result.get('order')} field {field}")  # noqa: T201
             print("Legacy:", legacy_result.get(field))  # noqa: T201
             print("HogQL:", hogql_result.get(field))  # noqa: T201
             print("")  # noqa: T201
