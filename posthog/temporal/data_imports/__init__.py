505fdc3e0c317bca121a3b5d8514983274db1338
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-03-14 09:39:45 +0100

    refactor(style): do not use star imports for locals

--- posthog/temporal/data_imports/__init__.py
+++ posthog/temporal/data_imports/__init__.py
@@ -1,4 +1,10 @@
-from posthog.temporal.data_imports.external_data_job import *
+from posthog.temporal.data_imports.external_data_job import (
+    ExternalDataJobWorkflow,
+    create_external_data_job_model,
+    update_external_data_job_model,
+    run_external_data_job,
+    validate_schema_activity,
+)
 
 WORKFLOWS = [ExternalDataJobWorkflow]
 
