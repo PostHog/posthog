57f5ebfe2fcfe36090d2231b46eb7a80a5e094a0
David Newell <david@posthog.com>
2024-03-15 17:55:54 +0000

    remove categories from story

--- frontend/src/lib/lemon-ui/icons/icons.test.ts
+++ frontend/src/lib/lemon-ui/icons/icons.test.ts
@@ -1,11 +1,12 @@
 import * as packageIcons from '@posthog/icons'
 
-import { CATEGORIES, UNUSED_ICONS } from './icons3000.stories'
+import { ELEMENTS, OBJECTS, TEAMS_AND_COMPANIES, TECHNOLOGY, UNUSED_ICONS } from './categories'
 
 describe('icons', () => {
     it('ensures all icons are categorised', async () => {
         const validPackageIcons = Object.keys(packageIcons).filter((i) => !['BaseIcon', 'default'].includes(i))
-        const categorisedIcons = Object.values(CATEGORIES)
+        const categories = { ...OBJECTS, ...TECHNOLOGY, ...ELEMENTS, ...TEAMS_AND_COMPANIES }
+        const categorisedIcons = Object.values(categories)
             .map((category) => Object.values(category))
             .flat(2)
 
