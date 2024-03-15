4f265953d4727bd9e51ad9356571c23465ba04f2
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-03-15 11:27:02 +0100

    times

--- posthog/management/commands/compare_hogql_funnel_insights.py
+++ posthog/management/commands/compare_hogql_funnel_insights.py
@@ -1,3 +1,5 @@
+# ruff: noqa: T201
+from datetime import datetime
 from django.core.management.base import BaseCommand
 from typing import cast
 from django.db.models import Q
@@ -30,83 +32,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        IGNORED_INSIGHTS = [
-            "qCf2qVXC",
-            "8oivI5Si",
-            "BGkyzMWV",
-            "P5z6kXjT",
-            "pEIfnI83",
-            "3IY1Nhij",
-            "u6ZPGsST",
-            "-x48Xi9K",
-            "BiW9wKf4",
-            "9xcmkvsq",
-            "EoBcQHus",
-            "x4cC38rP",
-            "0SLe31v1",
-            "lx37J7XJ",
-            "viGPH2P0",
-            "nj6p2sTO",
-            "vESGjN2P",
-            "_xBU_hGJ",
-            "5-B56Dvr",
-            "ajtprsdX",
-            "bqKiND7s",
-            "X67ch-fD",
-            "kwHeMuGG",
-            "PvEZvJ7E",
-            "ryFmelek",
-            "nY_XOJGH",
-            "SEJPBoya",
-            "8MFOFasJ",
-            "6WDMm0mQ",
-            "0-xlBF7z",
-            "7lx1xVyK",
-            "rCNzPg4u",
-            "E2w5B3uz",
-            "RIPjUaQp",
-            "yh-gVcLp",
-            "bVpCCOCW",
-            "H92CWNx7",
-            "_XVLTYJh",
-            "RKTr47ZY",
-            "Nyvk3N8p",
-            "7QOOBtaD",
-            "6jA4uN4i",
-            "oR0c6Gd6",
-            "JpNtVpZb",
-            "VS98D3k9",
-            "0CVUZLki",
-            "HB8ws1Rb",
-            "fThH0WHC",
-            "t648bVVo",
-            "eyW4Sw4J",
-            "TyhobRHV",
-            "XptgB0nS",
-            "lGa8G5jG",
-            "5VDqFB2d",
-            "RAW5HDlU",
-            "OCcLyGC-",
-            "hsQln2PI",
-            "Sz7-Ro-e",
-            "yj7DIktG",
-            "AVVuI2nU",
-            "gNq2A52O",
-            "MM8RL9NK",
-            "bgBau9XL",
-            "H0u7MiEH",
-            "M5KDFdvX",
-            "jmSuun7l",
-            "YaNmxUGc",
-            "lJJn5Qx3",
-            "awZVSmnu",
-            "rqjfOxEj",
-            "H5pNTnWd",
-            "BEL16Bpa",
-            "lfkZUMYK",
-            "GqV3RFZA",
-            "cBhW2PKS",
-        ]
+        IGNORED_INSIGHTS = []
 
         insights = (
             Insight.objects.filter(
@@ -132,34 +58,36 @@ class Command(BaseCommand):
 
         for insight in insights[0:99]:
             BASE_URL = f"https://us.posthog.com/project/{insight.team.pk}"
-            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")  # noqa: T201
-            print(
-                f'Checking Funnel Insight {insight.pk} {insight.short_id} - "{insight.name or insight.derived_name}"'
-            )  # noqa: T201
-            print(f"    {BASE_URL}/insights/{insight.short_id}/edit")  # noqa: T201
+            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            print(f'Checking Funnel Insight {insight.pk} {insight.short_id} - "{insight.name or insight.derived_name}"')
+            print(f"\t{BASE_URL}/insights/{insight.short_id}/edit")
             legacy_results, hogql_results, legacy_error, hogql_error = None, None, None, None
+            filter = Filter(data=insight.filters, team=insight.team)
+            query = filter_to_query(insight.filters)
+            query = cast(FunnelsQuery, query)
+            modifiers = HogQLQueryModifiers(materializationMode=MaterializationMode.legacy_null_as_string)
+            funnel_order_type = filter.funnel_order_type or FunnelOrderType.ORDERED
+            funnel_viz_type = filter.funnel_viz_type or FunnelVizType.STEPS
+            print(f"\t{funnel_order_type} {funnel_viz_type}")
 
             try:
-                filter = Filter(data=insight.filters, team=insight.team)
-                print(
-                    f"    {filter.funnel_order_type or FunnelOrderType.ORDERED} {filter.funnel_viz_type or FunnelVizType.STEPS}"
-                )
+                now = datetime.now()
                 funnel_class = get_legacy_class(filter)
                 legacy_results = funnel_class(filter, insight.team).run()
+                legacy_time = (datetime.now() - now).total_seconds()
             except ValidationError as e:
                 legacy_error = e
             except Exception as e:
                 url = f"{BASE_URL}/insights/{insight.short_id}/edit"
-                print(f"LEGACY Insight ERROR: {e}")  # noqa: T201
+                print(f"LEGACY Insight ERROR: {e}")
                 continue
 
             try:
-                query = filter_to_query(insight.filters)
-                query = cast(FunnelsQuery, query)
-                modifiers = HogQLQueryModifiers(materializationMode=MaterializationMode.legacy_null_as_string)
+                now = datetime.now()
                 hogql_results = (
                     FunnelsQueryRunner(query=query, team=insight.team, modifiers=modifiers).calculate().results
                 )
+                hogql_time = (datetime.now() - now).total_seconds()
             except ValidationError as e:
                 if e.get_full_details()[0]["message"] == "Funnels require at least two steps before calculating.":
                     print("Funnels require at least two steps before calculating.")
@@ -167,20 +95,20 @@ class Command(BaseCommand):
                 hogql_error = e
             except Exception as e:
                 url = f"{BASE_URL}/insights/{insight.short_id}/edit"
-                print(f"HogQL Insight ERROR: {e}")  # noqa: T201
+                print(f"HogQL Insight ERROR: {e}")
                 continue
 
             all_ok = True
 
             if legacy_error is not None:
                 if hogql_error is None:
-                    print(  # noqa: T201
+                    print(
                         f"Insight {BASE_URL}/insights/{insight.short_id}/edit"
                         f" ({insight.pk}). MISMATCH legacy has an error, but hogql has not: {legacy_error}"
                     )
                     all_ok = False
                 elif hogql_error.detail != legacy_error.detail:
-                    print(  # noqa: T201
+                    print(
                         f"Insight {BASE_URL}/insights/{insight.short_id}/edit"
                         f" ({insight.pk}). MISMATCH legacy error does not match hogql error"
                         f" legacy: {legacy_error}"
@@ -188,7 +116,7 @@ class Command(BaseCommand):
                     )
                     all_ok = False
             elif hogql_error is not None:
-                print(  # noqa: T201
+                print(
                     f"Insight {BASE_URL}/insights/{insight.short_id}/edit"
                     f" ({insight.pk}). MISMATCH hogql has an error, but legacy has not: {hogql_error}"
                 )
@@ -201,9 +129,9 @@ class Command(BaseCommand):
                 if filter.funnel_viz_type == "time_to_convert":
                     if legacy_results["average_conversion_time"] != hogql_results.average_conversion_time:
                         all_ok = False
-                        print("MISMATCH in average_conversion_time")  # noqa: T201
-                        print("Legacy:", legacy_results["average_conversion_time"])  # noqa: T201
-                        print("HogQL:", hogql_results.average_conversion_time)  # noqa: T201
+                        print("MISMATCH in average_conversion_time")
+                        print("Legacy:", legacy_results["average_conversion_time"])
+                        print("HogQL:", hogql_results.average_conversion_time)
 
                     bin_mismatch = False
                     for legacy, hogql in zip(legacy_results["bins"], hogql_results.bins):
@@ -217,8 +145,8 @@ class Command(BaseCommand):
 
                     if bin_mismatch:
                         print(f"MISMATCH in bins")
-                        print("Legacy:", legacy_results["bins"])  # noqa: T201
-                        print("HogQL:", hogql_results.bins)  # noqa: T201
+                        print("Legacy:", legacy_results["bins"])
+                        print("HogQL:", hogql_results.bins)
                 elif filter.funnel_viz_type == "trends":
                     for legacy_result, hogql_result in zip(legacy_results, hogql_results):
                         for field in ["count", "data", "days", "labels"]:
@@ -226,10 +154,10 @@ class Command(BaseCommand):
                             hogql = hogql_result[field]
 
                             if legacy != hogql:
-                                print(f"MISMATCH in field {field}")  # noqa: T201
-                                print("Legacy:", legacy)  # noqa: T201
-                                print("HogQL:", hogql)  # noqa: T201
-                                print("")  # noqa: T201
+                                print(f"MISMATCH in field {field}")
+                                print("Legacy:", legacy)
+                                print("HogQL:", hogql)
+                                print("")
                                 all_ok = False
                 else:
                     sorted_legacy_results = legacy_results
@@ -252,7 +180,14 @@ class Command(BaseCommand):
                                 all_ok = False
 
             if all_ok:
-                print("ALL OK!")  # noqa: T201
+                diff = hogql_time - legacy_time
+                percentage = hogql_time / legacy_time
+                significant = percentage > 1.1 and diff > 1
+                warn = ""
+                if significant:
+                    warn = "-- /!\\ {:.1%} /!\\".format(percentage)
+                print(f"\tLegacy time: {legacy_time} HogQL time: {hogql_time} {warn}")
+                print("ALL OK!")
 
 
 def compare_result(insight, legacy_result, hogql_result) -> bool:
@@ -283,10 +218,10 @@ def compare_result(insight, legacy_result, hogql_result) -> bool:
             if field == "average_conversion_time" and round(legacy, 1) == round(hogql, 1):
                 continue
 
-            print(f"MISMATCH in {legacy_result.get('order')} field {field}")  # noqa: T201
-            print("Legacy:", legacy_result.get(field))  # noqa: T201
-            print("HogQL:", hogql_result.get(field))  # noqa: T201
-            print("")  # noqa: T201
+            print(f"MISMATCH in {legacy_result.get('order')} field {field}")
+            print("Legacy:", legacy_result.get(field))
+            print("HogQL:", hogql_result.get(field))
+            print("")
             return False
 
     return True
