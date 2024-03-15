dbc2fc342353672b87dd43e913ab822147a53c6d
David Newell <david@posthog.com>
2024-03-15 11:12:29 +0000

    view percentage

--- frontend/src/scenes/session-recordings/errors/SessionRecordingErrors.tsx
+++ frontend/src/scenes/session-recordings/errors/SessionRecordingErrors.tsx
@@ -52,6 +52,11 @@ export function SessionRecordingErrors(): JSX.Element {
                 {
                     title: 'Viewed',
                     dataIndex: 'viewed',
+                    render: function Render(_, cluster) {
+                        return `${(cluster.viewed / cluster.unique_sessions) * 100}% (${cluster.viewed} of ${
+                            cluster.unique_sessions
+                        })`
+                    },
                     sorter: (a, b) => a.viewed - b.viewed,
                 },
                 {
@@ -73,60 +78,5 @@ export function SessionRecordingErrors(): JSX.Element {
             dataSource={errors}
             expandable={{ expandedRowRender: () => <div>Hello</div> }}
         />
-    )
-
-    return (
-        <LemonCollapse
-            panels={errors.map((error) => ({
-                key: error.cluster,
-                header: (
-                    <ErrorPanelHeader
-                        occurrenceCount={error.occurrences}
-                        sessionCount={error.unique_sessions}
-                        example={error.samples[0]}
-                    />
-                ),
-                content: <ErrorPanelContent samples={error.samples} />,
-            }))}
-        />
-    )
-}
-
-const ErrorPanelHeader = ({
-    occurrenceCount,
-    sessionCount,
-    example,
-}: {
-    occurrenceCount: number
-    sessionCount: number
-    example: ErrorClusterSample
-}): JSX.Element => {
-    return (
-        <div className="w-full flex justify-between items-center gap-2">
-            <span className="truncate">{example.input}</span>
-            <div className="flex items-center gap-2">
-                <span className="text-muted">
-                    {occurrenceCount} occurrences / {sessionCount} sessions
-                </span>
-                <LemonButton type="secondary" to={urls.replaySingle(example.session_id)}>
-                    Watch recording
-                </LemonButton>
-            </div>
-        </div>
-    )
-}
-
-const ErrorPanelContent = ({ samples }: { samples: ErrorClusterSample[] }): JSX.Element => {
-    return (
-        <div className="flex flex-col space-y-2">
-            {samples.map((error) => (
-                <div key={error.session_id} className="flex justify-between items-center">
-                    <span>{error.input}</span>
-                    <LemonButton type="secondary" to={urls.replaySingle(error.session_id)}>
-                        Watch recording
-                    </LemonButton>
-                </div>
-            ))}
-        </div>
     )
 }
