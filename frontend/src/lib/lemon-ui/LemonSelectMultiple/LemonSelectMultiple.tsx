f941563a948bd214eaf2e363188d2b833f1643fb
Ben White <ben@posthog.com>
2024-03-15 10:37:13 +0100

    Removed old code

--- frontend/src/lib/lemon-ui/LemonSelectMultiple/LemonSelectMultiple.tsx
+++ frontend/src/lib/lemon-ui/LemonSelectMultiple/LemonSelectMultiple.tsx
@@ -13,8 +13,6 @@ import { LemonInput } from '../LemonInput'
 
 export interface LemonSelectMultipleOption {
     label: string
-    disabled?: boolean
-    'data-attr'?: string
     labelComponent?: React.ReactNode
 }
 
@@ -25,110 +23,20 @@ export interface LemonSelectMultipleOptionItem extends LemonSelectMultipleOption
 export type LemonSelectMultipleOptions = Record<string, LemonSelectMultipleOption>
 
 export type LemonSelectMultipleProps = {
-    selectClassName?: string
-    options?: LemonSelectMultipleOptions | LemonSelectMultipleOptionItem[]
+    selectClassName?: string // TODO: Find use cases of this and remove it or fix it
+    options?: LemonSelectMultipleOptions | LemonSelectMultipleOptionItem[] // TOOD: Make this array only?
     value?: string[] | null
     disabled?: boolean
     loading?: boolean
     placeholder?: string
-    labelInValue?: boolean
-    onSearch?: (value: string) => void
-    onFocus?: () => void
     onBlur?: () => void
-    filterOption?: boolean
+    filterOptions?: boolean
     mode?: 'single' | 'multiple' | 'multiple-custom'
     onChange?: (newValue: string[]) => void
     'data-attr'?: string
 }
 
-export type LabelInValue = { value: string; label: ReactNode }
-
-export function LemonSelectMultiple(allProps: LemonSelectMultipleProps): JSX.Element {
-    const {
-        value,
-        options,
-        disabled,
-        loading,
-        placeholder,
-        labelInValue,
-        onChange,
-        onSearch,
-        onFocus,
-        onBlur,
-        filterOption = true,
-        mode = 'single',
-        selectClassName,
-        ...props
-    } = allProps
-    const optionsAsList: LemonSelectMultipleOptionItem[] = Array.isArray(options)
-        ? options
-        : Object.entries(options || {}).map(([key, option]) => ({
-              key: key,
-              ...option,
-          }))
-
-    const antOptions = optionsAsList.map((option) => ({
-        key: option.key,
-        value: option.key,
-        label: option.labelComponent || option.label,
-        labelString: option.label,
-    }))
-
-    return <LemonSelectMultipleV2 {...allProps} />
-
-    return (
-        <div className="LemonSelectMultiple" {...props}>
-            <Select
-                className={selectClassName}
-                mode={mode === 'multiple' ? 'multiple' : mode === 'multiple-custom' ? 'tags' : undefined}
-                showSearch
-                labelInValue={labelInValue}
-                disabled={disabled}
-                loading={loading}
-                onSearch={onSearch}
-                onFocus={onFocus}
-                onBlur={onBlur}
-                showAction={['focus']}
-                onChange={(v) => {
-                    if (onChange) {
-                        // TRICKY: V is typed poorly and will be a string if the "mode" is undefined
-                        if (!v || typeof v === 'string') {
-                            const typedValues = v as string | null
-                            const typedOnChange = onChange as (newValue: string | null) => void
-                            typedOnChange(typedValues)
-                        } else {
-                            const typedValues = v.map((token) => token.toString().trim())
-                            const typedOnChange = onChange as (newValue: string[]) => void
-                            typedOnChange(typedValues)
-                        }
-                    }
-                }}
-                tokenSeparators={[',']}
-                value={value ? value : []}
-                dropdownRender={(menu) => <div className="LemonSelectMultipleDropdown">{menu}</div>}
-                optionFilterProp="labelString"
-                options={antOptions}
-                placeholder={placeholder}
-                notFoundContent={
-                    loading ? (
-                        <div>
-                            {range(5).map((x) => (
-                                <div key={x} className="LemonSelectMultipleDropdown__skeleton">
-                                    <LemonSkeleton.Circle className="w-6 h-6" />
-                                    <LemonSkeleton />
-                                </div>
-                            ))}
-                        </div>
-                    ) : null
-                }
-                filterOption={filterOption}
-                tagRender={({ label, onClose }) => <LemonSnack onClose={onClose}>{label}</LemonSnack>}
-            />
-        </div>
-    )
-}
-
-export function LemonSelectMultipleV2({
+export function LemonSelectMultiple({
     placeholder,
     options,
     value,
@@ -136,6 +44,8 @@ export function LemonSelectMultipleV2({
     onChange,
     mode,
     disabled,
+    filterOptions = true,
+    ...props
 }: LemonSelectMultipleProps): JSX.Element {
     const [showPopover, setShowPopover] = useState(false)
     const [inputValue, setInputValue] = useState('')
@@ -153,19 +63,21 @@ export function LemonSelectMultipleV2({
     // TODO: Derive selected as part of the optionsList
     const values = value ?? []
 
-    const filteredOptions = inputValue
-        ? optionsAsList?.filter((option) => {
-              return option.label.toLowerCase().includes(inputValue.toLowerCase())
-          })
-        : optionsAsList
+    const filteredOptions =
+        inputValue && filterOptions
+            ? optionsAsList?.filter((option) => {
+                  return option.label.toLowerCase().includes(inputValue.toLowerCase())
+              })
+            : optionsAsList
 
     if (mode === 'multiple-custom' && inputValue && !values.includes(inputValue)) {
         filteredOptions.unshift({ key: inputValue, label: inputValue })
     }
 
     useEffect(() => {
+        console.log(filteredOptions.length, selectedIndex)
         if (selectedIndex >= filteredOptions.length) {
-            setSelectedIndex(filteredOptions.length - 1)
+            setSelectedIndex(Math.max(0, filteredOptions.length - 1))
         }
     }, [filteredOptions.length])
 
