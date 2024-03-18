79a4fbc180072878c7193dd851da02709b185241
David Newell <david@posthog.com>
2024-03-18 09:29:08 +0000

    copyable

--- frontend/src/lib/lemon-ui/icons/icons3000.stories.tsx
+++ frontend/src/lib/lemon-ui/icons/icons3000.stories.tsx
@@ -1,8 +1,9 @@
 import * as packageIcons from '@posthog/icons'
 import { Meta, StoryObj } from '@storybook/react'
-import { CopyToClipboardInline } from 'lib/components/CopyToClipboard'
+import { copyToClipboard } from 'lib/utils/copyToClipboard'
 
 import { LemonCollapse } from '../LemonCollapse'
+import { Tooltip } from '../Tooltip'
 import { ELEMENTS, OBJECTS, TEAMS_AND_COMPANIES, TECHNOLOGY } from './categories'
 
 const meta: Meta = {
@@ -23,13 +24,21 @@ const posthogIcons = Object.entries(packageIcons)
     .map(([key, Icon]) => ({ name: key, icon: Icon }))
 
 const IconTemplate = ({ icons }: { icons: { name: string; icon: any }[] }): JSX.Element => {
+    const onClick = (name: string): void => {
+        void copyToClipboard(name)
+    }
+
     return (
         <div className="grid grid-cols-6 gap-4">
             {icons.map(({ name, icon: Icon }) => {
                 return (
-                    <div key={name} className="flex flex-col items-center space-y-2">
-                        <Icon className="w-10 h-10" />
-                        <CopyToClipboardInline className="text-xs">{name}</CopyToClipboardInline>
+                    <div onClick={() => onClick(name)} key={name} className="flex justify-center">
+                        <Tooltip title="Click to copy">
+                            <div className="flex flex-col items-center space-y-2 max-w-24 py-2 px-4 cursor-pointer rounded hover:bg-secondary-3000">
+                                <Icon className="w-10 h-10" />
+                                <span className="text-xs">{name}</span>
+                            </div>
+                        </Tooltip>
                     </div>
                 )
             })}
