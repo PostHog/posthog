b31251991c1a602899d6620ad35417f31d734cab
Marius Andra <marius.andra@gmail.com>
2024-03-21 15:14:55 +0100

    sorting works

--- frontend/src/scenes/insights/views/InsightsTable/InsightsTable.tsx
+++ frontend/src/scenes/insights/views/InsightsTable/InsightsTable.tsx
@@ -152,7 +152,14 @@ export function InsightsTable({
                 />
             ),
             key: 'breakdown',
-            sorter: compareFn(),
+            sorter: (a, b) => {
+                if (typeof a.breakdown_value === 'number' && typeof b.breakdown_value === 'number') {
+                    return a.breakdown_value - b.breakdown_value
+                }
+                const labelA = formatItemBreakdownLabel(a)
+                const labelB = formatItemBreakdownLabel(b)
+                return compareFn()(labelA, labelB)
+            },
         })
         if (isTrends && display === ChartDisplayType.WorldMap) {
             columns.push({
