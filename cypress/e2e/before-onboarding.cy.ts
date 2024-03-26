3e8eb8a231ead32c96867e046f55b58dbc6cf7c2
Zach Waterfield <zlwaterfield@gmail.com>
2024-03-26 16:47:32 -0400

    Update before-onboarding.cy.ts

--- cypress/e2e/before-onboarding.cy.ts
+++ cypress/e2e/before-onboarding.cy.ts
@@ -1,11 +1,11 @@
 describe('Before Onboarding', () => {
-    it.only('Navigate to /products when a product has not been set up', () => {
+    it('Navigate to /products when a product has not been set up', () => {
         cy.visit('/project/1/data-management/events')
 
         cy.get('[data-attr=top-bar-name] > span').contains('Products')
     })
 
-    it.only('Navigate to a settings page even when a product has not been set up', () => {
+    it('Navigate to a settings page even when a product has not been set up', () => {
         cy.visit('/settings/user')
 
         cy.get('[data-attr=top-bar-name] > span').contains('User')
