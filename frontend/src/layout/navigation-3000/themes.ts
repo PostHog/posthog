fc6812a97054b51561e308e2e0e96b85c6b3747a
Marius Andra <marius.andra@gmail.com>
2024-03-27 23:11:55 +0100

    fix(3000): interface triangulator

---
+++ frontend/src/layout/navigation-3000/themes.ts
@@ -0,0 +1,25 @@
+export interface Theme {
+    id: string
+    name: string
+    sidebarStyle?: React.CSSProperties
+    mainStyle?: React.CSSProperties
+    boxStyle?: React.CSSProperties
+}
+
+export const themes: Theme[] = [
+    {
+        id: 'bleachedTom',
+        name: 'Hi, My name is Tom, and I am the light.',
+        sidebarStyle: {
+            background:
+                'linear-gradient(rgba(255,255,255,0.7),rgba(255,255,255,0.7)),url(https://www.shutterstock.com/image-photo/soft-wave-blue-ocean-on-600nw-396969259.jpg)',
+        },
+        mainStyle: {
+            background:
+                'linear-gradient(rgba(255,255,255,0.7),rgba(255,255,255,0.7)),url(https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg)',
+        },
+        boxStyle: {
+            background: 'linear-gradient(rgba(255,255,255,0.7),rgba(255,255,255,0.7))',
+        },
+    },
+]
