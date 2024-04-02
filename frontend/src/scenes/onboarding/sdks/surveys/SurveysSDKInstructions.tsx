53f5e534c7db1925935333a80a2b9dc1d2b23717
Lior539 <lneuner@gmail.com>
2024-04-02 13:37:38 +0100

    feat: Add frameworks to onboarding part 2 (#21091)
    * astro
    
    * add bubble
    
    * framer
    
    * laravel
    
    * nuxt
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * add remix
    
    * Update UI snapshots for `chromium` (1)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update UI snapshots for `chromium` (2)
    
    * Update frontend/src/scenes/onboarding/sdks/feature-flags/angular.tsx
    
    * feat: Add frameworks to onboarding part 3 (#21158)
    
    * svelte
    
    * missing export
    
    * export
    
    * webflow
    
    * vue
    
    * django
    
    * flag code
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Raquel Smith <raquelmsmith@users.noreply.github.com>

--- frontend/src/scenes/onboarding/sdks/surveys/SurveysSDKInstructions.tsx
+++ frontend/src/scenes/onboarding/sdks/surveys/SurveysSDKInstructions.tsx
@@ -2,16 +2,32 @@ import { SDKInstructionsMap, SDKKey } from '~/types'
 
 import {
     AngularInstructions,
+    AstroInstructions,
+    BubbleInstructions,
+    FramerInstructions,
     HTMLSnippetInstructions,
     JSWebInstructions,
     NextJSInstructions,
+    NuxtJSInstructions,
     ReactInstructions,
+    RemixInstructions,
+    SvelteInstructions,
+    VueInstructions,
+    WebflowInstructions,
 } from '.'
 
 export const SurveysSDKInstructions: SDKInstructionsMap = {
     [SDKKey.JS_WEB]: JSWebInstructions,
     [SDKKey.HTML_SNIPPET]: HTMLSnippetInstructions,
     [SDKKey.ANGULAR]: AngularInstructions,
+    [SDKKey.ASTRO]: AstroInstructions,
+    [SDKKey.BUBBLE]: BubbleInstructions,
+    [SDKKey.FRAMER]: FramerInstructions,
     [SDKKey.NEXT_JS]: NextJSInstructions,
+    [SDKKey.NUXT_JS]: NuxtJSInstructions,
     [SDKKey.REACT]: ReactInstructions,
+    [SDKKey.REMIX]: RemixInstructions,
+    [SDKKey.SVELTE]: SvelteInstructions,
+    [SDKKey.VUE_JS]: VueInstructions,
+    [SDKKey.WEBFLOW]: WebflowInstructions,
 }
