ee6d55774ab6e9a01b1e5b6058bab10cd4444737
Zach Waterfield <zlwaterfield@gmail.com>
2024-04-03 09:15:02 -0400

    Update frontend/src/scenes/authentication/signup/verify-email/VerifyEmail.tsx

--- frontend/src/scenes/authentication/signup/verify-email/VerifyEmail.tsx
+++ frontend/src/scenes/authentication/signup/verify-email/VerifyEmail.tsx
@@ -58,7 +58,6 @@ export const VerifyEmailHelpLinks = (): JSX.Element => {
         'Wait 5 minutes. Sometimes it takes a bit for email providers to deliver emails.',
         'Check your spam folder and any firewalls you may have active',
         'Ask your company IT department to allow any emails from @posthog.com',
-        'Make sure you can receive emails to your email address, especially if it\'s an alias',
         'Channel your inner hedgehog and take another peek at your inbox',
     ]
 
