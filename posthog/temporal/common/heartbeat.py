16b3b0d53f1fc4a46cfe7d248627bdfefa870022
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-04-05 00:39:21 +0200

    feat: Add docstrings

--- posthog/temporal/common/heartbeat.py
+++ posthog/temporal/common/heartbeat.py
@@ -5,23 +5,40 @@ from temporalio import activity
 
 
 class Heartbeatter:
-    def __init__(self, details: None | tuple[typing.Any, ...] = None, factor: int = 2):
-        self._details: None | tuple[typing.Any, ...] = details
+    """Regular heartbeatting during Temporal activity execution.
+
+    This class manages two heartbeat tasks via a context manager:
+    * A task that hearbeats regularly every 'heartbeat_timeout' / 'factor'.
+    * A task that heartbeats after worker shutdown is detected.
+
+    Attributes:
+        details: Set this attribute to a tuple to send as heartbeat details.
+        factor: Used to determine interval between regular heartbeatting.
+        heartbeat_task: A reference to regular heartbeatting task maintained while in the
+            context manager to avoid garbage collection.
+        heartbeat_on_shutdown_task: A reference to task that heartbeats on shutdown
+            maintained while in the context manager to avoid garbage collection.
+    """
+
+    def __init__(self, details: tuple[typing.Any, ...] = (), factor: int = 2):
+        self._details: tuple[typing.Any, ...] = details
         self.factor = factor
         self.heartbeat_task: asyncio.Task | None = None
         self.heartbeat_on_shutdown_task: asyncio.Task | None = None
 
     @property
     def details(self) -> tuple[typing.Any, ...]:
-        if self._details is None:
-            return ()
+        """Return details if available, otherwise an empty tuple."""
         return self._details
 
     @details.setter
     def details(self, details: tuple[typing.Any, ...]) -> None:
+        """Set tuple to be passed as heartbeat details."""
         self._details = details
 
     async def __aenter__(self):
+        """Enter managed heartbeatting context."""
+
         async def heartbeat_forever(delay: float) -> None:
             """Heartbeat forever every delay seconds."""
             while True:
@@ -48,16 +65,18 @@ class Heartbeatter:
         return self
 
     async def __aexit__(self, *args, **kwargs):
-        if self.heartbeat_task:
+        """Cancel heartbeatting tasks on exit."""
+        tasks_to_wait = []
+        if self.heartbeat_task is not None:
             self.heartbeat_task.cancel()
+            tasks_to_wait.append(self.heartbeat_task)
 
-            await asyncio.wait([self.heartbeat_task])
-
-            self.heartbeat_task = None
-
-        if self.heartbeat_on_shutdown_task:
+        if self.heartbeat_on_shutdown_task is not None:
             self.heartbeat_on_shutdown_task.cancel()
+            tasks_to_wait.append(self.heartbeat_on_shutdown_task)
 
-            await asyncio.wait([self.heartbeat_on_shutdown_task])
+        if tasks_to_wait:
+            await asyncio.wait(tasks_to_wait)
 
-            self.heartbeat_task = None
+        self.heartbeat_task = None
+        self.heartbeat_on_shutdown_task = None
