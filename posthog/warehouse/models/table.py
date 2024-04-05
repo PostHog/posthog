88fba2fd89b6b923bf2b2efcee09a21a7c9a5dc3
Tom Owers <owerstom@gmail.com>
2024-04-05 13:32:47 +0100

    Fix for non-dlt tables

--- posthog/warehouse/models/table.py
+++ posthog/warehouse/models/table.py
@@ -163,9 +163,10 @@ class DataWarehouseTable(CreatedMetaFields, UUIDModel, DeletedMetaFields):
             fields[column] = hogql_type(name=column)
 
         # Replace fields with any redefined fields if they exist
-        default_fields = external_tables.get("*", {})
-        fields = external_tables.get(self.table_name_without_prefix(), fields)
-        fields = {**fields, **default_fields}
+        external_table_fields = external_tables.get(self.table_name_without_prefix())
+        if external_table_fields is not None:
+            default_fields = external_tables.get("*", {})
+            fields = {**external_table_fields, **default_fields}
 
         return S3Table(
             name=self.name,
