b6cc5087da38badf7c21fb162f847fbc54529ffc
MarconLP <13001502+MarconLP@users.noreply.github.com>
2024-04-09 20:59:57 +0200

    make the new ticket button more visible for paying users

--- frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelSupport.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelSupport.tsx
@@ -182,22 +182,48 @@ export const SidePanelSupport = (): JSX.Element => {
                         </ul>
                     </Section>
 
-                    <Section title="Ask the community">
-                        <p>
-                            Questions about features, how to's, or use cases? There are thousands of discussions in our
-                            community forums.
-                        </p>
-                        <LemonButton
-                            type="primary"
-                            fullWidth
-                            center
-                            to="https://posthog.com/questions"
-                            targetBlank
-                            className="mt-2"
-                        >
-                            Ask a question
-                        </LemonButton>
-                    </Section>
+                    {hasAvailableFeature(AvailableFeature.EMAIL_SUPPORT) ? (
+                        <>
+                            <Section title="Ask an engineer">
+                                <p>Can't find what you need in the docs?</p>
+                                <LemonButton
+                                    type="primary"
+                                    fullWidth
+                                    center
+                                    onClick={() => openEmailForm()}
+                                    targetBlank
+                                    className="mt-2"
+                                >
+                                    Open a ticket
+                                </LemonButton>
+                            </Section>
+                            {isEmailFormOpen ? <SupportFormBlock onCancel={() => closeEmailForm()} /> : null}
+                            <Section title="Ask the community">
+                                <p>
+                                    Questions about features, how to's, or use cases? There are thousands of discussions
+                                    in our community forums.{' '}
+                                    <Link to="https://posthog.com/questions">Ask a question</Link>
+                                </p>
+                            </Section>
+                        </>
+                    ) : (
+                        <Section title="Ask the community">
+                            <p>
+                                Questions about features, how to's, or use cases? There are thousands of discussions in
+                                our community forums.
+                            </p>
+                            <LemonButton
+                                type="primary"
+                                fullWidth
+                                center
+                                to="https://posthog.com/questions"
+                                targetBlank
+                                className="mt-2"
+                            >
+                                Ask a question
+                            </LemonButton>
+                        </Section>
+                    )}
 
                     <Section title="Share feedback">
                         <ul>
@@ -248,18 +274,7 @@ export const SidePanelSupport = (): JSX.Element => {
                         </ul>
                     </Section>
 
-                    {hasAvailableFeature(AvailableFeature.EMAIL_SUPPORT) ? (
-                        <Section title="More options">
-                            {isEmailFormOpen ? (
-                                <SupportFormBlock onCancel={() => closeEmailForm()} />
-                            ) : (
-                                <p>
-                                    Can't find what you need in the docs?{' '}
-                                    <Link onClick={() => openEmailForm()}>Email an engineer</Link>
-                                </p>
-                            )}
-                        </Section>
-                    ) : (
+                    {hasAvailableFeature(AvailableFeature.EMAIL_SUPPORT) ? null : (
                         <Section title="Contact support">
                             <p>
                                 Due to our large userbase, we're unable to offer email support to organizations on the
