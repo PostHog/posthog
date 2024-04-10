6f4bf343359e0d7ef3db370a47f6d09674ccc108
timgl <tim@posthog.com>
2024-04-10 15:18:13 -0700

    fix schema

--- posthog/schema.py
+++ posthog/schema.py
@@ -427,7 +427,7 @@ class PersonsOnEventsMode(str, Enum):
     v1_mixed = "v1_mixed"
     v2_enabled = "v2_enabled"
     v3_enabled = "v3_enabled"
-    PERSON_ID_OVERRIDE_PROPERTIES_JOINED = "person_id_override_properties_joined"
+    person_id_override_properties_joined = "person_id_override_properties_joined"
 
 
 class HogQLQueryModifiers(BaseModel):
