6e8459d0ca0c6df3a67def533b6c450e9acfe665
Paul D'Ambra <paul@posthog.com>
2024-04-11 18:25:05 +0200

    messy first filtering

--- posthog/heatmaps/heatmaps_api.py
+++ posthog/heatmaps/heatmaps_api.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from typing import Any
 
 from rest_framework import viewsets, request, response, serializers, status
@@ -15,12 +16,16 @@ class HeatmapResponseItemSerializer(serializers.Serializer):
     pointer_target_fixed = serializers.BooleanField(required=True)
 
 
+def default_start_date():
+    return (datetime.now() - timedelta(days=7)).date()
+
+
 class HeatmapsRequestSerializer(serializers.Serializer):
     viewport_min_width = serializers.IntegerField(required=False)
     viewport_max_width = serializers.IntegerField(required=False)
     type = serializers.CharField(required=False)
-    date_from = serializers.CharField(required=False)
-    date_to = serializers.CharField(required=False)
+    date_from = serializers.DateField(required=False, default=default_start_date)
+    date_to = serializers.DateField(required=False)
     url_exact = serializers.CharField(required=False)
     url_pattern = serializers.CharField(required=False)
 
@@ -38,23 +43,65 @@ class HeatmapViewSet(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
         return None
 
     def list(self, request: request.Request, *args: Any, **kwargs: Any) -> response.Response:
-        request_serializer = HeatmapsRequestSerializer(data=request.data)
+        request_serializer = HeatmapsRequestSerializer(data=request.query_params)
         request_serializer.is_valid(raise_exception=True)
 
-        doohickies = execute_hogql_query(
-            """
+        placeholders = {
+            "team_id": Constant(value=self.team.pk),
+            "date_from": Constant(value=request_serializer.validated_data["date_from"]),
+        }
+        if request_serializer.validated_data.get("viewport_min_width", None):
+            placeholders["vp_min_w"] = Constant(value=request_serializer.viewport_min_width)
+        if request_serializer.validated_data.get("viewport_max_width", None):
+            placeholders["vp_max_w"] = Constant(value=request_serializer.viewport_max_width)
+        if request_serializer.validated_data.get("date_to", None):
+            placeholders["date_to"] = Constant(value=request_serializer.date_to)
+        if request_serializer.validated_data.get("url_exact", None):
+            placeholders["url_exact"] = Constant(value=request_serializer.url_exact)
+        if request_serializer.validated_data.get("url_pattern", None):
+            placeholders["url_pattern"] = Constant(value=request_serializer.url_pattern)
+
+        q = """
             select *, count() as cnt
             from (
                      select `$pointer_target_fixed`, round((x / `$viewport_width`), 2) as relative_client_x,
                             y * scale_factor                  as client_y
                      from heatmaps
-                     where ceil(`$viewport_width` / 16) == ceil(1512 / 16)
-                       and `$current_url` like '%'
-                     and team_id = {team_id}
+                     where 1=1
+                     {date_from_predicate}
+                    {date_to_predicate}
+                    {viewport_min_width_predicate}
+                    {viewport_max_width_predicate}
+                    {url_exact_predicate}
+                    {url_pattern_predicate}
+                     {team_id_predicate}
                      )
             group by `$pointer_target_fixed`, relative_client_x, client_y
-            """,
-            placeholders={"team_id": Constant(value=self.team.pk)},
+            """.format(
+            # required
+            date_from_predicate="and timestamp >= {date_from}",
+            team_id_predicate="and team_id = {team_id}",
+            # optional
+            date_to_predicate="and timestamp <= {date_to}"
+            if request_serializer.validated_data.get("date_to", None)
+            else "",
+            viewport_min_width_predicate="and `$viewport_width` >= ceil({vp_min_w} / 16)"
+            if request_serializer.validated_data.get("viewport_min_width", None)
+            else "",
+            viewport_max_width_predicate="and `$viewport_width` <= ceil({vp_max_w} / 16)"
+            if request_serializer.validated_data.get("viewport_max_width", None)
+            else "",
+            url_exact_predicate="and `$current_url` = {url_exact}"
+            if request_serializer.validated_data.get("url_exact", None)
+            else "",
+            url_pattern_predicate="and `$current_url` like {url_pattern}"
+            if request_serializer.validated_data.get("url_pattern", None)
+            else "",
+        )
+
+        doohickies = execute_hogql_query(
+            query=q,
+            placeholders=placeholders,
             team=self.team,
         )
 
