d233be2e4395a2482603304d443982bc5605ad8a
Paul D'Ambra <paul@posthog.com>
2024-04-11 17:35:51 +0200

    slightly more API-ing

--- posthog/heatmaps/heatmaps_api.py
+++ posthog/heatmaps/heatmaps_api.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from rest_framework import viewsets, request, response, serializers
+from rest_framework import viewsets, request, response, serializers, status
 
 from posthog.api.routing import TeamAndOrgViewSetMixin
 from posthog.hogql.ast import Constant
@@ -8,8 +8,25 @@ from posthog.hogql.query import execute_hogql_query
 from posthog.rate_limit import ClickHouseSustainedRateThrottle, ClickHouseBurstRateThrottle
 
 
+class HeatmapResponseItemSerializer(serializers.Serializer):
+    count = serializers.IntegerField(required=True)
+    pointer_y = serializers.IntegerField(required=True)
+    pointer_relative_x = serializers.FloatField(required=True)
+    pointer_target_fixed = serializers.BooleanField(required=True)
+
+
+class HeatmapsRequestSerializer(serializers.Serializer):
+    viewport_min_width = serializers.IntegerField(required=False)
+    viewport_max_width = serializers.IntegerField(required=False)
+    type = serializers.CharField(required=False)
+    date_from = serializers.CharField(required=False)
+    date_to = serializers.CharField(required=False)
+    url_exact = serializers.CharField(required=False)
+    url_pattern = serializers.CharField(required=False)
+
+
 class HeatmapsResponseSerializer(serializers.Serializer):
-    pass
+    results: serializers.ListSerializer = HeatmapResponseItemSerializer(many=True)
 
 
 class HeatmapViewSet(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
@@ -21,9 +38,12 @@ class HeatmapViewSet(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
         return None
 
     def list(self, request: request.Request, *args: Any, **kwargs: Any) -> response.Response:
+        request_serializer = HeatmapsRequestSerializer(data=request.data)
+        request_serializer.is_valid(raise_exception=True)
+
         doohickies = execute_hogql_query(
             """
-            select *, count()
+            select *, count() as cnt
             from (
                      select `$pointer_target_fixed`, round((x / `$viewport_width`), 2) as relative_client_x,
                             y * scale_factor                  as client_y
@@ -37,4 +57,17 @@ class HeatmapViewSet(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
             placeholders={"team_id": Constant(value=self.team.pk)},
             team=self.team,
         )
-        return response.Response(doohickies.results)
+
+        data = [
+            {
+                "pointer_target_fixed": item[0],
+                "pointer_relative_x": item[1],
+                "pointer_y": item[2],
+                "count": item[3],
+            }
+            for item in doohickies.results
+        ]
+
+        response_serializer = HeatmapsResponseSerializer(data={"results": data})
+        response_serializer.is_valid(raise_exception=True)
+        return response.Response(response_serializer.data, status=status.HTTP_200_OK)
