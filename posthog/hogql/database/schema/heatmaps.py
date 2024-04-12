1bfbb651c5a7e4d7085b982275308498b0ae1fae
Paul D'Ambra <paul@posthog.com>
2024-04-12 12:46:59 +0200

    fix

--- posthog/hogql/database/schema/heatmaps.py
+++ posthog/hogql/database/schema/heatmaps.py
@@ -12,25 +12,25 @@ from posthog.hogql.database.models import (
 
 class HeatmapsTable(Table):
     fields: Dict[str, FieldOrTable] = {
-        "$session_id": StringDatabaseField(name="$session_id"),
+        "session_id": StringDatabaseField(name="session_id"),
         "team_id": IntegerDatabaseField(name="team_id"),
         "x": IntegerDatabaseField(name="x"),
         "y": IntegerDatabaseField(name="y"),
         "scale_factor": IntegerDatabaseField(name="scale_factor"),
-        "$viewport_width": IntegerDatabaseField(name="$viewport_width"),
-        "$viewport_height": IntegerDatabaseField(name="$viewport_height"),
-        "$pointer_target_fixed": BooleanDatabaseField(name="$pointer_target_fixed"),
-        "$current_url": StringDatabaseField(name="$current_url"),
+        "viewport_width": IntegerDatabaseField(name="viewport_width"),
+        "viewport_height": IntegerDatabaseField(name="viewport_height"),
+        "pointer_target_fixed": BooleanDatabaseField(name="pointer_target_fixed"),
+        "current_url": StringDatabaseField(name="current_url"),
         "timestamp": DateTimeDatabaseField(name="timestamp"),
         # join to get analytics for heatmap areas?
         # "session": LazyJoin(
-        #     from_field=["$session_id"],
+        #     from_field=["session_id"],
         #     join_table=SessionsTable(),
         #     join_function=join_events_table_to_sessions_table,
         # ),
         # join to get example replays?
         # "recordings": LazyJoin(
-        #     from_field=["$session_id"],
+        #     from_field=["session_id"],
         #     join_table=SessionReplayEventsTable(),
         #     join_function=join_events_table_to_replay_table,
         # ),
