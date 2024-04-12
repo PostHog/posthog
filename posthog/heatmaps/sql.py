80c3160803119ba500aeb920438702cbdfb26a8f
Paul D'Ambra <paul@posthog.com>
2024-04-12 10:50:47 +0200

    Update posthog/heatmaps/sql.py
    Co-authored-by: James Greenhill <fuziontech@gmail.com>

--- posthog/heatmaps/sql.py
+++ posthog/heatmaps/sql.py
@@ -13,7 +13,7 @@ HEATMAPS_DATA_TABLE = lambda: "sharded_heatmaps"
 
 """
 We intend to send specific $heatmap events to build a heatmap instead of building from autocapture like the click map
-We'll be storing indiviudal clicks per url/team/session
+We'll be storing individual clicks per url/team/session
 And we'll be querying for those clicks at day level of granularity
 And we'll be querying by URL exact or wildcard match
 And we'll _sometimes_ be querying by width
