f16baf636c11bb751f08f74772bef7594e3ee980
Paul D'Ambra <paul@posthog.com>
2024-04-15 21:16:25 +0100

    empty state message

--- frontend/src/toolbar/debug/EventDebugMenu.tsx
+++ frontend/src/toolbar/debug/EventDebugMenu.tsx
@@ -33,34 +33,37 @@ export const EventDebugMenu = (): JSX.Element => {
             </ToolbarMenu.Header>
             <ToolbarMenu.Body>
                 <div className="flex flex-col space-y-1">
-                    {filteredEvents.map((e, i) => {
-                        return (
-                            <div
-                                className={clsx('-mx-1 py-1 px-2 cursor-pointer', i === 0 && 'bg-mark')}
-                                key={e.uuid}
-                                onClick={() => {
-                                    expandedEvent === e.uuid ? markExpanded(null) : markExpanded(e.uuid || null)
-                                }}
-                            >
-                                <div className="flex flex-row justify-between">
-                                    <div>{e.event}</div>
-                                    <div>
-                                        <TZLabel time={e.timestamp} />
-                                    </div>
-                                </div>
-                                <AnimatedCollapsible
-                                    collapsed={e.uuid === undefined ? true : isCollapsedEventRow(e.uuid)}
+                    {filteredEvents.length ? (
+                        filteredEvents.map((e, i) => {
+                            return (
+                                <div
+                                    className={clsx('-mx-1 py-1 px-2 cursor-pointer', i === 0 && 'bg-mark')}
+                                    key={e.uuid}
+                                    onClick={() => {
+                                        expandedEvent === e.uuid ? markExpanded(null) : markExpanded(e.uuid || null)
+                                    }}
                                 >
-                                    <div className="mt-1 ml-1 pl-2 border-l-2">
-                                        <SimpleKeyValueList
-                                            item={e.event === '$snapshot' ? e : e.properties}
-                                            emptyMessage="No events seen yet."
-                                        />
+                                    <div className="flex flex-row justify-between">
+                                        <div>{e.event}</div>
+                                        <div>
+                                            <TZLabel time={e.timestamp} />
+                                        </div>
                                     </div>
-                                </AnimatedCollapsible>
-                            </div>
-                        )
-                    })}
+                                    <AnimatedCollapsible
+                                        collapsed={e.uuid === undefined ? true : isCollapsedEventRow(e.uuid)}
+                                    >
+                                        <div className="mt-1 ml-1 pl-2 border-l-2">
+                                            <SimpleKeyValueList item={e.event === '$snapshot' ? e : e.properties} />
+                                        </div>
+                                    </AnimatedCollapsible>
+                                </div>
+                            )
+                        })
+                    ) : (
+                        <div className="px-4 py-2">
+                            Interact with your page and then come back to the toolbar to see what events were generated.
+                        </div>
+                    )}
                 </div>
             </ToolbarMenu.Body>
             <ToolbarMenu.Footer>
