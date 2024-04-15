48c42c23b83e66a11994eac5b61368e3ea490f73
Marius Andra <marius.andra@gmail.com>
2024-04-15 18:17:54 +0200

    feat(hogql): query cohort with version

--- posthog/hogql/functions/cohort.py
+++ posthog/hogql/functions/cohort.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from posthog.hogql import ast
 from posthog.hogql.context import HogQLContext
@@ -7,12 +7,16 @@ from posthog.hogql.escape_sql import escape_clickhouse_string
 from posthog.hogql.parser import parse_expr
 
 
-def cohort_subquery(cohort_id, is_static) -> ast.Expr:
+def cohort_subquery(cohort_id, is_static, version: Optional[int] = None) -> ast.Expr:
     if is_static:
         sql = "(SELECT person_id FROM static_cohort_people WHERE cohort_id = {cohort_id})"
+    elif version is not None:
+        sql = "(SELECT person_id FROM raw_cohort_people WHERE cohort_id = {cohort_id} AND version = {version})"
     else:
         sql = "(SELECT person_id FROM raw_cohort_people WHERE cohort_id = {cohort_id} GROUP BY person_id, cohort_id, version HAVING sum(sign) > 0)"
-    return parse_expr(sql, {"cohort_id": ast.Constant(value=cohort_id)}, start=None)  # clear the source start position
+    return parse_expr(
+        sql, {"cohort_id": ast.Constant(value=cohort_id), "version": ast.Constant(value=version)}, start=None
+    )  # clear the source start position
 
 
 def cohort_query_node(node: ast.Expr, context: HogQLContext) -> ast.Expr:
@@ -28,20 +32,22 @@ def cohort(node: ast.Expr, args: List[ast.Expr], context: HogQLContext) -> ast.E
 
     if (isinstance(arg.value, int) or isinstance(arg.value, float)) and not isinstance(arg.value, bool):
         cohorts = Cohort.objects.filter(id=int(arg.value), team_id=context.team_id).values_list(
-            "id", "is_static", "name"
+            "id", "is_static", "version", "name"
         )
         if len(cohorts) == 1:
             context.add_notice(
                 start=arg.start,
                 end=arg.end,
-                message=f"Cohort #{cohorts[0][0]} can also be specified as {escape_clickhouse_string(cohorts[0][2])}",
-                fix=escape_clickhouse_string(cohorts[0][2]),
+                message=f"Cohort #{cohorts[0][0]} can also be specified as {escape_clickhouse_string(cohorts[0][3])}",
+                fix=escape_clickhouse_string(cohorts[0][3]),
             )
-            return cohort_subquery(cohorts[0][0], cohorts[0][1])
+            return cohort_subquery(cohorts[0][0], cohorts[0][1], cohorts[0][2])
         raise QueryError(f"Could not find cohort with ID {arg.value}", node=arg)
 
     if isinstance(arg.value, str):
-        cohorts = Cohort.objects.filter(name=arg.value, team_id=context.team_id).values_list("id", "is_static")
+        cohorts = Cohort.objects.filter(name=arg.value, team_id=context.team_id).values_list(
+            "id", "is_static", "version"
+        )
         if len(cohorts) == 1:
             context.add_notice(
                 start=arg.start,
@@ -49,7 +55,7 @@ def cohort(node: ast.Expr, args: List[ast.Expr], context: HogQLContext) -> ast.E
                 message=f"Searching for cohort by name. Replace with numeric ID {cohorts[0][0]} to protect against renaming.",
                 fix=str(cohorts[0][0]),
             )
-            return cohort_subquery(cohorts[0][0], cohorts[0][1])
+            return cohort_subquery(cohorts[0][0], cohorts[0][1], cohorts[0][2])
         elif len(cohorts) > 1:
             raise QueryError(f"Found multiple cohorts with name '{arg.value}'", node=arg)
         raise QueryError(f"Could not find a cohort with the name '{arg.value}'", node=arg)
