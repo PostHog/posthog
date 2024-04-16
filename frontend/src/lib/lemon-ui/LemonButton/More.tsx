9f1b2fba0473adf685a4c8a416b8083f4468fbd3
David Newell <david@posthog.com>
2024-04-16 18:24:32 +0100

    chore: cleanup More button component

--- frontend/src/lib/lemon-ui/LemonButton/More.tsx
+++ frontend/src/lib/lemon-ui/LemonButton/More.tsx
@@ -2,35 +2,24 @@ import { IconEllipsis } from '@posthog/icons'
 
 import { PopoverProps } from '../Popover/Popover'
 import { LemonButtonWithDropdown } from '.'
-import { LemonButtonProps, LemonButtonWithDropdownProps } from './LemonButton'
+import { LemonButtonProps } from './LemonButton'
 
-export type MoreProps = Partial<Pick<PopoverProps, 'overlay' | 'placement'>> &
-    LemonButtonProps & {
-        'data-attr'?: string
-        onClick?: LemonButtonWithDropdownProps['onClick']
-    }
+export type MoreProps = Partial<Pick<PopoverProps, 'overlay'>> & LemonButtonProps
 
-export function More({
-    overlay,
-    placement = 'bottom-end',
-    'data-attr': dataAttr,
-    onClick,
-    ...buttonProps
-}: MoreProps): JSX.Element {
+export function More({ overlay, 'data-attr': dataAttr, ...buttonProps }: MoreProps): JSX.Element {
     return (
         <LemonButtonWithDropdown
             aria-label="more"
             data-attr={dataAttr ?? 'more-button'}
             icon={<IconEllipsis />}
             dropdown={{
-                placement,
+                placement: 'bottom-end',
                 actionable: true,
                 overlay,
             }}
             size="small"
             {...buttonProps}
             disabled={!overlay}
-            onClick={onClick}
         />
     )
 }
