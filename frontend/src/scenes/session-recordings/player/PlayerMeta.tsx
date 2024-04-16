d97e467cbdc39a3061ba303b8be22236368de67a
David Newell <david@posthog.com>
2024-04-16 18:16:20 +0100

    chore: move actions into single ellipsis menu

--- frontend/src/scenes/session-recordings/player/PlayerMeta.tsx
+++ frontend/src/scenes/session-recordings/player/PlayerMeta.tsx
@@ -1,11 +1,14 @@
 import './PlayerMeta.scss'
 
-import { Link } from '@posthog/lemon-ui'
+import { IconEllipsis, IconTrash } from '@posthog/icons'
+import { IconDownload, IconMagic, IconSearch } from '@posthog/icons'
+import { LemonButton, LemonDialog, LemonMenu, Link } from '@posthog/lemon-ui'
 import clsx from 'clsx'
-import { useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import { CopyToClipboardInline } from 'lib/components/CopyToClipboard'
 import { TZLabel } from 'lib/components/TZLabel'
 import { dayjs } from 'lib/dayjs'
+import { useFeatureFlag } from 'lib/hooks/useFeatureFlag'
 import { useResizeBreakpoints } from 'lib/hooks/useResizeObserver'
 import { LemonSkeleton } from 'lib/lemon-ui/LemonSkeleton'
 import { ProfilePicture } from 'lib/lemon-ui/ProfilePicture'
@@ -23,6 +26,7 @@ import { getCurrentExporterData } from '~/exporter/exporterViewLogic'
 import { Logo } from '~/toolbar/assets/Logo'
 
 import { PlayerMetaLinks } from './PlayerMetaLinks'
+import { sessionRecordingDataLogic } from './sessionRecordingDataLogic'
 import { sessionRecordingPlayerLogic, SessionRecordingPlayerMode } from './sessionRecordingPlayerLogic'
 
 function SessionPropertyMeta(props: {
@@ -190,6 +194,7 @@ export function PlayerMeta(): JSX.Element {
                                         formatTime="h:mm A"
                                         showPopover={false}
                                     />
+                                    {mode === SessionRecordingPlayerMode.Standard && <MenuActions />}
                                 </div>
                             )}
                         </div>
@@ -248,5 +253,70 @@ export function PlayerMeta(): JSX.Element {
                 </div>
             </div>
         </DraggableToNotebook>
+    )
+}
+
+const MenuActions = (): JSX.Element => {
+    const { logicProps } = useValues(sessionRecordingPlayerLogic)
+    const { exportRecordingToFile, openExplorer, deleteRecording, setIsFullScreen } =
+        useActions(sessionRecordingPlayerLogic)
+    const { fetchSimilarRecordings } = useActions(sessionRecordingDataLogic(logicProps))
+
+    const hasMobileExport = useFeatureFlag('SESSION_REPLAY_EXPORT_MOBILE_DATA')
+    const hasSimilarRecordings = useFeatureFlag('REPLAY_SIMILAR_RECORDINGS')
+
+    const onDelete = (): void => {
+        setIsFullScreen(false)
+        LemonDialog.open({
+            title: 'Delete recording',
+            description: 'Are you sure you want to delete this recording? This cannot be undone.',
+            secondaryButton: {
+                children: 'Cancel',
+            },
+            primaryButton: {
+                children: 'Delete',
+                status: 'danger',
+                onClick: deleteRecording,
+            },
+        })
+    }
+
+    const items = [
+        {
+            label: 'Export to file',
+            onClick: exportRecordingToFile,
+            icon: <IconDownload />,
+            tooltip: 'Export recording to a file. This can be loaded later into PostHog for playback.',
+        },
+        {
+            label: 'Explore DOM',
+            onClick: openExplorer,
+            icon: <IconSearch />,
+        },
+        hasMobileExport && {
+            label: 'Export mobile replay to file',
+            onClick: () => exportRecordingToFile(true),
+            tooltip:
+                'DEBUG ONLY - Export untransformed recording to a file. This can be loaded later into PostHog for playback.',
+            icon: <IconDownload />,
+        },
+        hasSimilarRecordings && {
+            label: 'Find similar recordings',
+            onClick: fetchSimilarRecordings,
+            icon: <IconMagic />,
+            tooltip: 'DEBUG ONLY - Find similar recordings based on distance calculations via embeddings.',
+        },
+        logicProps.playerKey !== 'modal' && {
+            label: 'Delete recording',
+            status: 'danger',
+            onClick: onDelete,
+            icon: <IconTrash />,
+        },
+    ]
+
+    return (
+        <LemonMenu items={items}>
+            <LemonButton size="xsmall" icon={<IconEllipsis className="rotate-90" />} />
+        </LemonMenu>
     )
 }
