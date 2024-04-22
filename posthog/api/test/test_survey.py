ef4fa0aef3c38a8c4542d5cafe5cb4500ce1d1cd
Ben White <ben@posthog.com>
2024-04-22 13:54:48 +0200

    Fixes

--- posthog/api/test/test_survey.py
+++ posthog/api/test/test_survey.py
@@ -261,7 +261,7 @@ class TestSurvey(APIBaseTest):
             format="json",
         ).json()
 
-        with self.assertNumQueries(12):
+        with self.assertNumQueries(15):
             response = self.client.get(f"/api/projects/{self.team.id}/feature_flags")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             result = response.json()
