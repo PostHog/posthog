d8f33206209f1861e68a4aa3b9ca04f9850c30ce
Paul D'Ambra <paul@posthog.com>
2024-04-23 14:32:22 +0100

    polling isn't a signal we're loading

--- frontend/src/scenes/session-recordings/player/sessionRecordingDataLogic.ts
+++ frontend/src/scenes/session-recordings/player/sessionRecordingDataLogic.ts
@@ -651,7 +651,7 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
             }
         },
     })),
-    selectors({
+    selectors(({ cache }) => ({
         sessionPlayerData: [
             (s, p) => [
                 s.sessionPlayerMetaData,
@@ -690,7 +690,9 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
         snapshotsLoading: [
             (s) => [s.snapshotSourcesLoading, s.snapshotsForSourceLoading],
             (snapshotSourcesLoading, snapshotsForSourceLoading): boolean => {
-                return snapshotSourcesLoading || snapshotsForSourceLoading
+                // if there's a realTimePollingTimeoutID, don't signal that we're loading
+                // we don't want the UI to flip to "loading" every time we poll
+                return !cache.realTimePollingTimeoutID && (snapshotSourcesLoading || snapshotsForSourceLoading)
             },
         ],
         snapshotsLoaded: [(s) => [s.snapshotSources], (snapshotSources): boolean => !!snapshotSources],
@@ -867,7 +869,7 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
                 })
             },
         ],
-    }),
+    })),
     afterMount(({ cache }) => {
         resetTimingsCache(cache)
     }),
