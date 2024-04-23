3c1791e713549feebf70c005ae227757702dedb8
Ben White <ben@posthog.com>
2024-04-23 12:15:55 +0200

    Fixes

--- posthog/api/tagged_item.py
+++ posthog/api/tagged_item.py
@@ -95,7 +95,7 @@ class TaggedItemViewSetMixin(viewsets.GenericViewSet):
             )
         return queryset
 
-    def safely_get_queryset(self, queryset: QuerySet) -> QuerySet:
+    def filter_queryset(self, queryset: QuerySet) -> QuerySet:
         super().filter_queryset(queryset)
         return self.prefetch_tagged_items_if_available(queryset)
 
