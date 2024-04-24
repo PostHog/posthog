7fe0954eda067afb447ba07286e70719352b205b
Ben White <ben@posthog.com>
2024-04-24 12:46:24 +0200

    Fixes

--- ee/clickhouse/views/test/test_clickhouse_experiments.py
+++ ee/clickhouse/views/test/test_clickhouse_experiments.py
@@ -56,7 +56,7 @@ class TestExperimentCRUD(APILicensedTest):
             format="json",
         ).json()
 
-        with self.assertNumQueries(FuzzyInt(8, 9)):
+        with self.assertNumQueries(FuzzyInt(10, 11)):
             response = self.client.get(f"/api/projects/{self.team.id}/experiments")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -916,7 +916,7 @@ class TestExperimentCRUD(APILicensedTest):
         ).json()
 
         # TODO: Make sure permission bool doesn't cause n + 1
-        with self.assertNumQueries(12):
+        with self.assertNumQueries(16):
             response = self.client.get(f"/api/projects/{self.team.id}/feature_flags")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             result = response.json()
