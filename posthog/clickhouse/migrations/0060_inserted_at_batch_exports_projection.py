f7679df992dd544ed22dfd28b9d1a62b7b6ead3a
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-04-25 10:47:26 +0200

    fix: Rename file and sort columns

--- posthog/clickhouse/migrations/0060_inserted_at_batch_exports_projection.py
+++ posthog/clickhouse/migrations/0060_inserted_at_batch_exports_projection.py
@@ -7,19 +7,19 @@ EVENTS_TABLE_INSERTED_AT_PROJECTION_SQL = """
 ALTER TABLE {table_name} ON CLUSTER {cluster}
 ADD PROJECTION `inserted_at_batch_exports_projection` (
     SELECT
-        uuid,
-        event,
-        properties,
-        timestamp,
-        team_id,
+        _timestamp,
+        created_at,
         distinct_id,
         elements_chain,
-        created_at,
-        person_id,
+        event,
         inserted_at,
-        _timestamp,
         person_created_at,
-        person_properties
+        person_id,
+        person_properties,
+        properties,
+        team_id,
+        timestamp,
+        uuid
     ORDER BY (team_id, COALESCE(`inserted_at`, `_timestamp`), event, cityHash64(distinct_id), cityHash64(uuid))
 )
 """.format(table_name=EVENTS_DATA_TABLE(), cluster=settings.CLICKHOUSE_CLUSTER)
