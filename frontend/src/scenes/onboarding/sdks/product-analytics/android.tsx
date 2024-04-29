052eef744a5e8d9c5f8e6c20cd223bd8340d84b7
Paul D'Ambra <paul@posthog.com>
2024-04-29 15:15:08 +0100

    room to breathe

--- frontend/src/scenes/onboarding/sdks/product-analytics/android.tsx
+++ frontend/src/scenes/onboarding/sdks/product-analytics/android.tsx
@@ -2,6 +2,7 @@ import { CodeSnippet, Language } from 'lib/components/CodeSnippet'
 import { FlaggedFeature } from 'lib/components/FlaggedFeature'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { LemonBanner } from 'lib/lemon-ui/LemonBanner'
+import { LemonDivider } from 'lib/lemon-ui/LemonDivider'
 import { LemonTag } from 'lib/lemon-ui/LemonTag'
 import { Link } from 'lib/lemon-ui/Link'
 import { OnboardingStepKey } from 'scenes/onboarding/onboardingLogic'
@@ -17,8 +18,9 @@ function AndroidCaptureSnippet(): JSX.Element {
 
 function AdvertiseAndroidReplay(): JSX.Element {
     return (
-        <LemonBanner type="info" className="mt-8">
-            <div>
+        <div>
+            <LemonDivider className="my-8" />
+            <LemonBanner type="info">
                 <h3>
                     Session Replay for Android <LemonTag type="highlight">NEW</LemonTag>
                 </h3>
@@ -31,20 +33,20 @@ function AdvertiseAndroidReplay(): JSX.Element {
                         Learn how to set it up
                     </Link>
                 </div>
-            </div>
-        </LemonBanner>
+            </LemonBanner>
+        </div>
     )
 }
 
 export function ProductAnalyticsAndroidInstructions(): JSX.Element {
     return (
         <>
-            <FlaggedFeature flag={FEATURE_FLAGS.SESSION_REPLAY_MOBILE_ONBOARDING} match={true}>
-                <AdvertiseAndroidReplay />
-            </FlaggedFeature>
             <SDKInstallAndroidInstructions />
             <h3>Send an Event</h3>
             <AndroidCaptureSnippet />
+            <FlaggedFeature flag={FEATURE_FLAGS.SESSION_REPLAY_MOBILE_ONBOARDING} match={true}>
+                <AdvertiseAndroidReplay />
+            </FlaggedFeature>
         </>
     )
 }
