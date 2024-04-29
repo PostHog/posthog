597c03c8b84d1aaaf185a615312f9f9b42843a68
David Newell <david@posthog.com>
2024-04-29 14:56:20 +0100

    find event

--- frontend/src/scenes/session-recordings/player/rrweb/canvas/canvas-plugin.ts
+++ frontend/src/scenes/session-recordings/player/rrweb/canvas/canvas-plugin.ts
@@ -14,15 +14,21 @@ function isCanvasMutation(e: eventWithTime): e is CanvasEventWithTime {
     return e.type === EventType.IncrementalSnapshot && e.data.source === IncrementalSource.CanvasMutation
 }
 
-const PRELOAD_BUFFER_SIZE = 30;
+function findEvent(events: CanvasEventWithTime[], target: CanvasEventWithTime): number {
+    return events.findIndex((event) => event.data.id === target.data.id)
+}
+
+const PRELOAD_BUFFER_SIZE = 30
 
 export const CanvasReplayerPlugin = (events: eventWithTime[]): ReplayPlugin => {
     const canvases = new Map<number, HTMLCanvasElement>([])
     const containers = new Map<number, HTMLImageElement>([])
     const imageMap = new Map<eventWithTime | string, HTMLImageElement>()
     const canvasEventMap = new Map<eventWithTime | string, canvasMutationParam>()
-    const preloadBuffer = new Set<CanvasEventWithTime>();
-    let nextPreloadIndex: number | null = null;
+    const preloadBuffer = new Set<CanvasEventWithTime>()
+    let nextPreloadIndex: number | null = null
+
+    const canvasMutationEvents = events.filter(isCanvasMutation)
 
     const deserializeAndPreloadCanvasEvents = async (data: canvasMutationData, event: eventWithTime): Promise<void> => {
         if (!canvasEventMap.has(event)) {
@@ -58,34 +64,70 @@ export const CanvasReplayerPlugin = (events: eventWithTime[]): ReplayPlugin => {
         return cloneNode
     }
 
-    const promises: Promise<any>[] = []
-    for (const event of events) {
-        if (event.type === EventType.IncrementalSnapshot && event.data.source === IncrementalSource.CanvasMutation) {
-            promises.push(deserializeAndPreloadCanvasEvents(event.data, event))
+    const processMutation = async (e: CanvasEventWithTime, replayer: Replayer): Promise<void> => {
+        const data = e.data as canvasMutationData
+        const source = replayer.getMirror().getNode(data.id) as HTMLCanvasElement
+        const target = canvases.get(data.id) || (source && cloneCanvas(data.id, source))
+
+        if (!target) {
+            return
+        }
+
+        target.width = source.clientWidth
+        target.height = source.clientHeight
+
+        await canvasMutation({
+            event: e,
+            mutation: data,
+            target: target,
+            imageMap,
+            canvasEventMap,
+            errorHandler: (error: any) => {
+                if (error instanceof Error) {
+                    captureException(error)
+                } else {
+                    console.error(error)
+                }
+            },
+        })
+
+        const img = containers.get(data.id)
+        if (img) {
+            img.src = target.toDataURL()
         }
     }
 
-    const canvasMutationEvents = events.filter(isCanvasMutation);
+    // const promises: Promise<any>[] = []
+    // for (const event of events) {
+    //     if (event.type === EventType.IncrementalSnapshot && event.data.source === IncrementalSource.CanvasMutation) {
+    //         promises.push(deserializeAndPreloadCanvasEvents(event.data, event))
+    //     }
+    // }
 
-    const preload = async (currentEvent?: eventWithTime): Promise<void> => {
-        const nextIndex = nextPreloadIndex ? nextPreloadIndex : findIndex(currentEvent)
-        const eventIndex = nextIndex > -1 ? nextIndex : 0;
-        const startIndex = eventIndex > -1 ? eventIndex : 0;
-        const eventsToPreload = canvasMutationEvents
-          .slice(startIndex, startIndex + PRELOAD_BUFFER_SIZE - preloadBuffer.size)
-    
-        nextPreloadIndex = nextPreloadIndex ? nextPreloadIndex + 1 : startIndex;
-    
+    const preload = async (currentEvent?: CanvasEventWithTime): Promise<void> => {
+        const currentIndex = nextPreloadIndex
+            ? nextPreloadIndex
+            : currentEvent
+            ? findEvent(canvasMutationEvents, currentEvent)
+            : 0
+
+        const eventsToPreload = canvasMutationEvents.slice(
+            currentIndex,
+            currentIndex + PRELOAD_BUFFER_SIZE - preloadBuffer.size
+        )
+
+        nextPreloadIndex = currentIndex + 1
+
         for (const event of eventsToPreload) {
-          if (!preloadBuffer.has(event) && !canvasEventMap.has(event)) {
-            preloadBuffer.add(event);
-            await deserializeAndPreloadCanvasEvents(event.data as canvasMutationData, event);
-            preloadBuffer.delete(event);
-          }
+            if (!preloadBuffer.has(event) && !canvasEventMap.has(event)) {
+                preloadBuffer.add(event)
+                await deserializeAndPreloadCanvasEvents(event.data as canvasMutationData, event)
+                preloadBuffer.delete(event)
+            }
         }
-      }
+    }
 
-      preload()
+    void preload()
 
     return {
         onBuild: (node, { id }) => {
@@ -104,39 +146,11 @@ export const CanvasReplayerPlugin = (events: eventWithTime[]): ReplayPlugin => {
         handler: async (e: eventWithTime, isSync: boolean, { replayer }: { replayer: Replayer }) => {
             // skip when fast forwarding
             if (isSync) {
-                const isCanvas = isCanvasMutation(e)
+                // reset preload index if scrubbing
+                nextPreloadIndex = null
 
-                if (isCanvas) {
-                    const data = e.data as canvasMutationData
-                    const source = replayer.getMirror().getNode(data.id) as HTMLCanvasElement
-                    const target = canvases.get(data.id) || (source && cloneCanvas(data.id, source))
-
-                    if (!target) {
-                        return
-                    }
-
-                    target.width = source.clientWidth
-                    target.height = source.clientHeight
-
-                    await canvasMutation({
-                        event: e,
-                        mutation: data,
-                        target: target,
-                        imageMap,
-                        canvasEventMap,
-                        errorHandler: (error: any) => {
-                            if (error instanceof Error) {
-                                captureException(error)
-                            } else {
-                                console.error(error)
-                            }
-                        },
-                    })
-
-                    const img = containers.get(data.id)
-                    if (img) {
-                        img.src = target.toDataURL()
-                    }
+                if (isCanvasMutation(e)) {
+                    void processMutation(e, replayer)
                 }
             }
         },
