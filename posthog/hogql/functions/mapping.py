1ea69538cae4e16365eaf5dbd0a13c40be98d320
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-04-30 11:02:16 +0200

    fix(hogql): fix correlation analysis aggregation mapping

--- posthog/hogql/functions/mapping.py
+++ posthog/hogql/functions/mapping.py
@@ -579,7 +579,7 @@ HOGQL_AGGREGATIONS: dict[str, HogQLFunctionMeta] = {
     # Standard aggregate functions
     "count": HogQLFunctionMeta("count", 0, 1, aggregate=True, case_sensitive=False),
     "countIf": HogQLFunctionMeta("countIf", 1, 2, aggregate=True),
-    "countDistinctIf": HogQLFunctionMeta("countIf", 1, 2, aggregate=True),
+    "countDistinctIf": HogQLFunctionMeta("countDistinctIf", 1, 2, aggregate=True),
     "min": HogQLFunctionMeta("min", 1, 1, aggregate=True, case_sensitive=False),
     "minIf": HogQLFunctionMeta("minIf", 2, 2, aggregate=True),
     "max": HogQLFunctionMeta("max", 1, 1, aggregate=True, case_sensitive=False),
