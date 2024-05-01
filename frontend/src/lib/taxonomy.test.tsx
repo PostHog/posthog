32de2ec25e7bd05a6d3dff0188c73c6324db5a8d
Robbie <robbie.coomber@gmail.com>
2024-05-01 20:41:24 +0100

    feat(web-analytics): Use $entry in session prop names (#22019)
    Use $entry in session prop names

--- frontend/src/lib/taxonomy.test.tsx
+++ frontend/src/lib/taxonomy.test.tsx
@@ -29,12 +29,12 @@ describe('taxonomy', () => {
         const sessionPropertyNames = Object.keys(CORE_FILTER_DEFINITIONS_BY_GROUP.session_properties)
         it('should have a $session_referring_domain property', () => {
             const property: CoreFilterDefinition =
-                CORE_FILTER_DEFINITIONS_BY_GROUP.session_properties['$session_referring_domain']
-            expect(property.label).toEqual('Session Referring Domain')
+                CORE_FILTER_DEFINITIONS_BY_GROUP.session_properties['$entry_referring_domain']
+            expect(property.label).toEqual('Entry Referring Domain')
         })
         it(`should have every property in SESSION_PROPERTIES_ADAPTED_FROM_PERSON`, () => {
             for (const property of Array.from(SESSION_INITIAL_PROPERTIES_ADAPTED_FROM_EVENTS.keys())) {
-                expect(sessionPropertyNames).toContain('$session_' + property.replace('$', ''))
+                expect(sessionPropertyNames).toContain('$entry_' + property.replace('$', ''))
             }
         })
     })
