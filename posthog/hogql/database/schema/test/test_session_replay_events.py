bdea97e531abeead9c2b99c4c3ea866028c3ab2c
Paul D'Ambra <paul@posthog.com>
2024-05-02 11:35:31 +0100

    basic tests on the different tabl joins

--- posthog/hogql/database/schema/test/test_session_replay_events.py
+++ posthog/hogql/database/schema/test/test_session_replay_events.py
@@ -1,17 +1,23 @@
+from posthog.clickhouse.client import sync_execute
 from posthog.hogql import ast
 from posthog.hogql.parser import parse_select
 from posthog.hogql.query import execute_hogql_query
 from posthog.session_recordings.queries.test.session_replay_sql import produce_replay_summary
+from posthog.session_recordings.sql.session_replay_event_sql import TRUNCATE_SESSION_REPLAY_EVENTS_TABLE_SQL
 from posthog.test.base import (
     APIBaseTest,
     ClickhouseTestMixin,
     _create_event,
+    _create_person,
+    flush_persons_and_events,
 )
 
 
 class TestFilterSessionReplaysByEvents(ClickhouseTestMixin, APIBaseTest):
     def setUp(self):
         super().setUp()
+
+        sync_execute(TRUNCATE_SESSION_REPLAY_EVENTS_TABLE_SQL())
 
         produce_replay_summary(
             team_id=self.team.pk,
@@ -90,9 +96,106 @@ class TestFilterSessionReplaysByEvents(ClickhouseTestMixin, APIBaseTest):
         ]
 
 
+class TestFilterSessionReplaysByPerson(ClickhouseTestMixin, APIBaseTest):
+    def setUp(self):
+        super().setUp()
+
+        sync_execute(TRUNCATE_SESSION_REPLAY_EVENTS_TABLE_SQL())
+
+        self._p1 = _create_person(distinct_ids=["d1"], team=self.team)
+
+        produce_replay_summary(
+            team_id=self.team.pk,
+            distinct_id="d1",
+            session_id="session_for_person_p1",
+        )
+
+        _create_event(
+            event="$pageview",
+            team=self.team,
+            distinct_id="d1",
+            properties={"$current_url": "https://example.com", "$session_id": "session_for_person_p1"},
+        )
+
+        self._p2 = _create_person(distinct_ids=["d2"], team=self.team, properties={"person_property": True})
+
+        produce_replay_summary(
+            team_id=self.team.pk,
+            distinct_id="d2",
+            session_id="session_with_person_with_person_property",
+        )
+
+        _create_event(
+            event="$pageview",
+            team=self.team,
+            distinct_id="d2",
+            properties={
+                "$current_url": "https://different.com",
+                "$session_id": "session_with_person_with_person_property",
+            },
+        )
+
+        flush_persons_and_events()
+
+    def test_select_by_replay_person(self):
+        response = execute_hogql_query(
+            parse_select(
+                "select distinct session_id from raw_session_replay_events where person.id = {person_id} order by session_id asc",
+                placeholders={"person_id": ast.Constant(value=self._p1.uuid)},
+            ),
+            self.team,
+        )
+
+        assert response.results == [
+            ("session_for_person_p1",),
+        ]
+
+    def test_select_by_person_distinct_id(self):
+        response = execute_hogql_query(
+            parse_select(
+                "select distinct session_id from raw_session_replay_events where pdi.distinct_id = {person_distinct_id} order by session_id asc",
+                placeholders={"person_distinct_id": ast.Constant(value="d1")},
+            ),
+            self.team,
+        )
+
+        assert response.results == [
+            ("session_for_person_p1",),
+        ]
+
+    def test_select_by_event_person(self):
+        response = execute_hogql_query(
+            parse_select(
+                "select distinct session_id from raw_session_replay_events where ifNull(events.person.properties.person_property, 'false') = {prop_value} order by session_id asc",
+                placeholders={"prop_value": ast.Constant(value="true")},
+            ),
+            self.team,
+        )
+
+        assert response.results == [
+            ("session_with_person_with_person_property",),
+        ]
+
+    def test_select_person_property(self):
+        response = execute_hogql_query(
+            parse_select(
+                "select session_id, any(person.properties.person_property) from raw_session_replay_events group by session_id order by session_id asc",
+            ),
+            self.team,
+        )
+
+        assert response.results == [
+            ("session_for_person_p1", None),
+            # todo: why is this the string true?
+            ("session_with_person_with_person_property", "true"),
+        ]
+
+
 class TestFilterSessionReplaysByConsoleLogs(ClickhouseTestMixin, APIBaseTest):
     def setUp(self):
         super().setUp()
+
+        sync_execute(TRUNCATE_SESSION_REPLAY_EVENTS_TABLE_SQL())
 
         produce_replay_summary(
             team_id=self.team.pk,
