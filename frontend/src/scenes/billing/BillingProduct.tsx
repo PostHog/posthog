68731b0bce4668d3dbc9618b2a66e968e4b4513e
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-05-02 10:23:37 -0700

    don't show the inclusion-only addon in the UI yet

--- frontend/src/scenes/billing/BillingProduct.tsx
+++ frontend/src/scenes/billing/BillingProduct.tsx
@@ -294,9 +294,11 @@ export const BillingProduct = ({ product }: { product: BillingProductV2Type }): 
                         <div className="pb-8">
                             <h4 className="my-4">Addons</h4>
                             <div className="gap-y-4 flex flex-col">
-                                {product.addons.map((addon, i) => {
-                                    return <BillingProductAddon key={i} addon={addon} />
-                                })}
+                                {product.addons
+                                    .filter((addon) => !addon.inclusion_only)
+                                    .map((addon, i) => {
+                                        return <BillingProductAddon key={i} addon={addon} />
+                                    })}
                             </div>
                         </div>
                     )}
