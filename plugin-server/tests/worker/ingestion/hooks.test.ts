0dfc6c44d13f8af72fb57e87a07b815460d4e2b3
Ben White <ben@posthog.com>
2024-05-02 17:03:36 +0200

    Fix tests

--- plugin-server/tests/worker/ingestion/hooks.test.ts
+++ plugin-server/tests/worker/ingestion/hooks.test.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import fetch, { FetchError } from 'node-fetch'
 
-import { Action, PostIngestionEvent, Team } from '../../../src/types'
+import { Action, ISOTimestamp, PostIngestionEvent, Team } from '../../../src/types'
 import { UUIDT } from '../../../src/utils/utils'
 import { AppMetrics } from '../../../src/worker/ingestion/app-metrics'
 import {
@@ -490,68 +490,89 @@ describe('hooks', () => {
         test('person = undefined', async () => {
             await hookCommander.postWebhook({ event: 'foo', properties: {} } as PostIngestionEvent, action, team, hook)
 
-            expect(fetch).toHaveBeenCalledWith('https://example.com/', {
-                body: JSON.stringify(
-                    {
-                        hook: {
-                            id: 'id',
-                            event: 'foo',
-                            target: 'https://example.com/',
-                        },
-                        data: {
-                            event: 'foo',
-                            person: {}, // person becomes empty object if undefined
-                        },
+            expect(fetch).toHaveBeenCalledTimes(1)
+            expect(fetch.mock.calls[0]).toMatchInlineSnapshot(`
+                Array [
+                  "https://example.com/",
+                  Object {
+                    "body": "{
+                    \\"hook\\": {
+                        \\"id\\": \\"id\\",
+                        \\"event\\": \\"foo\\",
+                        \\"target\\": \\"https://example.com/\\"
                     },
-                    undefined,
-                    4
-                ),
-                headers: { 'Content-Type': 'application/json' },
-                method: 'POST',
-                timeout: 20000,
-            })
+                    \\"data\\": {
+                        \\"event\\": \\"foo\\",
+                        \\"properties\\": {},
+                        \\"elementsList\\": [],
+                        \\"person\\": {}
+                    }
+                }",
+                    "headers": Object {
+                      "Content-Type": "application/json",
+                    },
+                    "method": "POST",
+                    "timeout": 20000,
+                  },
+                ]
+            `)
         })
 
         test('person data from the event', async () => {
-            const now = new Date().toISOString()
-            const uuid = new UUIDT().toString()
+            const now = DateTime.utc(2024, 1, 1).toISO()
+            const uuid = '018f39d3-d94c-0000-eeef-df4a793f8844'
             await hookCommander.postWebhook(
                 {
+                    eventUuid: uuid,
+                    distinctId: 'WALL-E',
+                    timestamp: now as ISOTimestamp,
                     event: 'foo',
                     teamId: hook.team_id,
+                    properties: {},
                     person_id: uuid,
                     person_properties: { foo: 'bar' },
-                    person_created_at: DateTime.fromISO(now).toUTC(),
-                } as any,
+                    person_created_at: now as ISOTimestamp,
+                } as PostIngestionEvent,
                 action,
                 team,
                 hook
             )
-            expect(fetch).toHaveBeenCalledWith('https://example.com/', {
-                body: JSON.stringify(
-                    {
-                        hook: {
-                            id: 'id',
-                            event: 'foo',
-                            target: 'https://example.com/',
-                        },
-                        data: {
-                            event: 'foo',
-                            teamId: hook.team_id,
-                            person: {
-                                uuid: uuid,
-                                properties: { foo: 'bar' },
-                                created_at: now,
-                            },
-                        },
+            expect(fetch).toHaveBeenCalledTimes(1)
+            expect(fetch.mock.calls[0]).toMatchInlineSnapshot(`
+                Array [
+                  "https://example.com/",
+                  Object {
+                    "body": "{
+                    \\"hook\\": {
+                        \\"id\\": \\"id\\",
+                        \\"event\\": \\"foo\\",
+                        \\"target\\": \\"https://example.com/\\"
                     },
-                    undefined,
-                    4
-                ),
-                headers: { 'Content-Type': 'application/json' },
-                method: 'POST',
-                timeout: 20000,
-            })
+                    \\"data\\": {
+                        \\"eventUuid\\": \\"018f39d3-d94c-0000-eeef-df4a793f8844\\",
+                        \\"distinctId\\": \\"WALL-E\\",
+                        \\"timestamp\\": \\"2024-01-01T00:00:00.000Z\\",
+                        \\"event\\": \\"foo\\",
+                        \\"teamId\\": 1,
+                        \\"properties\\": {},
+                        \\"elementsList\\": [],
+                        \\"person\\": {
+                            \\"uuid\\": \\"018f39d3-d94c-0000-eeef-df4a793f8844\\",
+                            \\"properties\\": {
+                                \\"foo\\": \\"bar\\"
+                            },
+                            \\"created_at\\": \\"2024-01-01T00:00:00.000Z\\"
+                        }
+                    }
+                }",
+                    "headers": Object {
+                      "Content-Type": "application/json",
+                    },
+                    "method": "POST",
+                    "timeout": 20000,
+                  },
+                ]
+            `)
         })
 
         test('private IP hook forbidden in prod', async () => {
