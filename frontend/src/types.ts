22b6711d865bcf249b4a679ed63edd950901e384
Tiina Turban <tiina303@gmail.com>
2024-05-06 19:15:01 +0200

    feat: Add Batch export destinations for new + nits (#22021)

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -626,7 +626,7 @@ export enum PipelineStage {
     Transformation = 'transformation',
     Destination = 'destination',
     SiteApp = 'site-app',
-    ImportApp = 'import-app',
+    ImportApp = 'legacy-source',
 }
 
 export enum PipelineNodeTab {
@@ -3730,6 +3730,7 @@ export type BatchExportServiceRedshift = {
 // When adding a new option here also add a icon for it to
 // src/scenes/pipeline/icons/
 // and update RenderBatchExportIcon
+// and update batchExportServiceNames in pipelineNodeNewLogic
 export type BatchExportService =
     | BatchExportServiceS3
     | BatchExportServiceSnowflake
