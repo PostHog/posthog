f0f88d103d375b358e1112ec3783af9ef11e3d8a
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-05-06 20:27:02 +0200

    clean up

--- posthog/test/test_decorators.py
+++ posthog/test/test_decorators.py
@@ -29,8 +29,6 @@ router.register(r"dummy", DummyViewSet, "dummy")
 
 
 class TestCachedByFiltersDecorator(APIBaseTest):
-    maxDiff = None
-
     def setUp(self) -> None:
         cache.clear()
 
@@ -39,7 +37,6 @@ class TestCachedByFiltersDecorator(APIBaseTest):
     def test_returns_fresh_result(self) -> None:
         response = self.client.get(f"/api/dummy").json()
 
-        assert response is {}
         assert response["result"] == "bla"
         assert response["is_cached"] is False
         assert isinstance(response["last_refresh"], str)
