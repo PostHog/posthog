c411eb7eb2ad3b449dbf5a1ac3b78c3468128015
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-05-06 20:42:49 +0200

    clean up

--- posthog/hogql_queries/insights/trends/test/test_trends_persons.py
+++ posthog/hogql_queries/insights/trends/test/test_trends_persons.py
@@ -80,28 +80,6 @@ def _create_cohort(**kwargs):
     return cohort
 
 
-# - [x] test_trends_single_series_persons
-# - [x] test_trends_multiple_series_persons
-# - [x] test_trends_event_breakdown_persons
-# - [x] test_trends_person_breakdown_persons
-# - [x] test_trends_cohort_breakdown_persons
-# - [x] test_trends_mau_persons
-# - [x] test_trends_group_persons
-# - [ ] test_trends_formula_persons
-# - [ ] test_trends_formula_with_breakdown_persons
-# - [ ] test_trends_total_value_persons
-# - [ ] test_trends_total_value_breakdown_persons
-# - [ ] test_trends_compare_persons
-# other breakdown
-# null breakdown
-# hogql breakdown
-# sampling
-# out of date range?
-# property math,
-# count math
-# interval
-
-
 def get_distinct_id(result):
     return result[0]["distinct_ids"][0]
 
@@ -197,34 +175,22 @@ class TestTrendsPersons(ClickhouseTestMixin, APIBaseTest):
     #                 )
 
     def _create_events(self):
-        person1 = self._create_person(
+        self._create_person(
             team_id=self.team.pk,
             distinct_ids=["person1"],
             properties={"$geoip_country_code": "US"},
         )
-        person2 = self._create_person(
+        self._create_person(
             team_id=self.team.pk,
             distinct_ids=["person2"],
             properties={"$geoip_country_code": "DE"},
         )
-        person3 = self._create_person(
+        self._create_person(
             team_id=self.team.pk,
             distinct_ids=["person3"],
             properties={"$geoip_country_code": "DE"},
         )
 
-        # freeze_without_time = ["2019-12-24", "2020-01-01", "2020-01-02"]
-        #     freeze_with_time = [
-        #         "2019-12-24 03:45:34",
-        #         "2020-01-01 00:06:34",
-        #         "2020-01-02 16:34:34",
-        #     ]
-
-        # freeze_args = freeze_without_time
-        #     if use_time:
-        #         freeze_args = freeze_with_time
-
-        # with freeze_time("2024-04-29 16:34:12"):
         self._create_event(
             event="$pageview",
             distinct_id="person1",
@@ -296,157 +262,6 @@ class TestTrendsPersons(ClickhouseTestMixin, APIBaseTest):
             properties={"$browser": "Safari"},
             team=self.team,
         )
-
-    #     with freeze_time(freeze_args[1]):
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="blabla",
-    #             properties={"$some_property": "value", "$bool_prop": False},
-    #         )
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="anonymous_id",
-    #             properties={"$bool_prop": False},
-    #         )
-    #         self._create_event(team=self.team, event="sign up", distinct_id="blabla")
-    #     with freeze_time(freeze_args[2]):
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="blabla",
-    #             properties={
-    #                 "$some_property": "other_value",
-    #                 "$some_numerical_prop": 80,
-    #             },
-    #         )
-    #         self._create_event(team=self.team, event="no events", distinct_id="blabla")
-
-    #     _create_action(team=self.team, name="no events")
-    #     sign_up_action = _create_action(team=self.team, name="sign up")
-
-    #     flush_persons_and_events()
-
-    #     return sign_up_action, person
-
-    # def _create_breakdown_events(self):
-    #     freeze_without_time = ["2020-01-02"]
-
-    #     with freeze_time(freeze_without_time[0]):
-    #         for i in range(25):
-    #             self._create_event(
-    #                 team=self.team,
-    #                 event="sign up",
-    #                 distinct_id="blabla",
-    #                 properties={"$some_property": i},
-    #             )
-    #     _create_action(team=self.team, name="sign up")
-
-    # def _create_breakdown_url_events(self):
-    #     freeze_without_time = ["2020-01-02"]
-
-    #     with freeze_time(freeze_without_time[0]):
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="blabla",
-    #             properties={"$current_url": "http://hogflix/first"},
-    #         )
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="blabla",
-    #             properties={"$current_url": "http://hogflix/first/"},
-    #         )
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="blabla",
-    #             properties={"$current_url": "http://hogflix/second"},
-    #         )
-
-    # def _create_event_count_per_actor_events(self):
-    #     self._create_person(
-    #         team_id=self.team.pk,
-    #         distinct_ids=["blabla", "anonymous_id"],
-    #         properties={"fruit": "mango"},
-    #     )
-    #     self._create_person(team_id=self.team.pk, distinct_ids=["tintin"], properties={"fruit": "mango"})
-    #     self._create_person(team_id=self.team.pk, distinct_ids=["murmur"], properties={})  # No fruit here
-    #     self._create_person(
-    #         team_id=self.team.pk,
-    #         distinct_ids=["reeree"],
-    #         properties={"fruit": "tomato"},
-    #     )
-
-    #     with freeze_time("2020-01-01 00:06:02"):
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="anonymous_id",
-    #             properties={"color": "red", "$group_0": "bouba"},
-    #         )
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="blabla",
-    #             properties={"$group_0": "bouba"},
-    #         )  # No color here
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="reeree",
-    #             properties={"color": "blue", "$group_0": "bouba"},
-    #         )
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="tintin",
-    #             properties={"$group_0": "kiki"},
-    #         )
-
-    #     with freeze_time("2020-01-03 19:06:34"):
-    #         self._create_event(
-    #             team=self.team,
-    #             event="sign up",
-    #             distinct_id="murmur",
-    #             properties={"$group_0": "kiki"},
-    #         )
-
-    #     with freeze_time("2020-01-04 23:17:00"):
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="tintin",
-    #             properties={"color": "red", "$group_0": "kiki"},
-    #         )
-
-    #     with freeze_time("2020-01-05 19:06:34"):
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="blabla",
-    #             properties={"color": "blue", "$group_0": "bouba"},
-    #         )
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="tintin",
-    #             properties={"color": "red"},
-    #         )  # No group here
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="tintin",
-    #             properties={"color": "red", "$group_0": "bouba"},
-    #         )
-    #         self._create_event(
-    #             team=self.team,
-    #             event="viewed video",
-    #             distinct_id="tintin",
-    #             properties={"color": "blue", "$group_0": "kiki"},
-    #         )
 
     def test_trends_single_series_persons(self):
         self._create_events()
@@ -719,12 +534,3 @@ class TestTrendsPersons(ClickhouseTestMixin, APIBaseTest):
         self.assertEqual(get_event_count(result[0]), 2)
         self.assertEqual(get_distinct_id(result[1]), "person1")
         self.assertEqual(get_event_count(result[1]), 2)
-
-
-# breakdown: Optional[Union[str, int]] = (None,)
-# compare: Optional[Compare] = (None,)
-# day: Optional[Union[str, int]] = (None,)
-# interval: Optional[int] = (None,)
-# series: Optional[int] = (None,)
-# status: Optional[str] = (None,)
-# offset: Optional[int] = (None,)
