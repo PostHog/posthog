5fd34171e89e5a7c3c5ee13dd508892e67d30024
Ben White <ben@posthog.com>
2024-05-07 11:24:39 +0200

    Fix up

--- frontend/src/scenes/actions/Action.tsx
+++ frontend/src/scenes/actions/Action.tsx
@@ -45,38 +45,41 @@ export function Action({ id }: { id?: ActionType['id'] } = {}): JSX.Element {
     return (
         <>
             <ActionEdit id={id} action={action} />
-            {id && <ActionPlugins />}
-            {id &&
-                (isComplete ? (
-                    <div>
-                        <h2 className="subtitle">Matching events</h2>
-                        <p>
-                            This is the list of <strong>recent</strong> events that match this action.
-                        </p>
-                        <div className="pt-4 border-t" />
-                        <Query
-                            query={{
-                                kind: NodeKind.DataTableNode,
-                                source: {
-                                    kind: NodeKind.EventsQuery,
-                                    select: defaultDataTableColumns(NodeKind.EventsQuery),
-                                    actionId: id,
-                                },
-                                full: true,
-                                showEventFilter: false,
-                                showPropertyFilter: false,
-                            }}
-                        />
-                    </div>
-                ) : (
-                    <div>
-                        <h2 className="subtitle">Matching events</h2>
-                        <div className="flex items-center">
-                            <Spinner className="mr-4" />
-                            Calculating action, please hold on.
+            {id && (
+                <>
+                    <ActionPlugins />
+                    {isComplete ? (
+                        <div>
+                            <h2 className="subtitle">Matching events</h2>
+                            <p>
+                                This is the list of <strong>recent</strong> events that match this action.
+                            </p>
+                            <div className="pt-4 border-t" />
+                            <Query
+                                query={{
+                                    kind: NodeKind.DataTableNode,
+                                    source: {
+                                        kind: NodeKind.EventsQuery,
+                                        select: defaultDataTableColumns(NodeKind.EventsQuery),
+                                        actionId: id,
+                                    },
+                                    full: true,
+                                    showEventFilter: false,
+                                    showPropertyFilter: false,
+                                }}
+                            />
                         </div>
-                    </div>
-                ))}
+                    ) : (
+                        <div>
+                            <h2 className="subtitle">Matching events</h2>
+                            <div className="flex items-center">
+                                <Spinner className="mr-4" />
+                                Calculating action, please hold on.
+                            </div>
+                        </div>
+                    )}
+                </>
+            )}
         </>
     )
 }
