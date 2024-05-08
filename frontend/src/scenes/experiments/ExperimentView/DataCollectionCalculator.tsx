b9f561f06bfc97f42cc9464fcc5b582bd827bd72
Juraj Majerik <juro.majerik@gmail.com>
2024-05-08 22:10:10 +0200

    refactor funnel/trend calculations

--- frontend/src/scenes/experiments/ExperimentView/DataCollectionCalculator.tsx
+++ frontend/src/scenes/experiments/ExperimentView/DataCollectionCalculator.tsx
@@ -13,45 +13,25 @@ import { InsightType } from '~/types'
 import { EXPERIMENT_INSIGHT_ID } from '../constants'
 import { experimentLogic } from '../experimentLogic'
 
-interface ExperimentPreviewProps {
+interface ExperimentCalculatorProps {
     experimentId: number | 'new'
 }
 
-export function DataCollectionCalculator({ experimentId }: ExperimentPreviewProps): JSX.Element {
+function FunnelCalculation({ experimentId }: ExperimentCalculatorProps): JSX.Element {
     const {
-        experimentInsightType,
         minimumDetectableChange,
         expectedRunningTime,
         experiment,
-        trendResults,
         funnelResults,
         conversionMetrics,
         minimumSampleSizePerVariant,
         variants,
-        recommendedExposureForCountData,
     } = useValues(experimentLogic({ experimentId }))
-    const { setExperiment } = useActions(experimentLogic({ experimentId }))
 
-    const insightLogicInstance = insightLogic({ dashboardItemId: EXPERIMENT_INSIGHT_ID, syncWithUrl: false })
-    const { insightProps } = useValues(insightLogicInstance)
-    const { query } = useValues(insightDataLogic(insightProps))
-
-    const trendCount = trendResults[0]?.count || 0
-    const trendExposure = recommendedExposureForCountData(trendCount)
     const funnelConversionRate = conversionMetrics?.totalRate * 100 || 0
-
-    const sliderMaxValue =
-        experimentInsightType === InsightType.FUNNELS
-            ? 100 - funnelConversionRate < 50
-                ? 100 - funnelConversionRate
-                : 50
-            : 50
-
     const currentDuration = dayjs().diff(dayjs(experiment?.start_date), 'hour')
-
     const funnelEntrants = funnelResults?.[0]?.count
 
-    // SAMPLE SIZE & RUNNING TIME
     const conversionRate = conversionMetrics.totalRate * 100
     const sampleSizePerVariant = minimumSampleSizePerVariant(conversionRate)
     const funnelSampleSize = sampleSizePerVariant * variants.length
@@ -60,6 +40,107 @@ export function DataCollectionCalculator({ experimentId }: ExperimentPreviewProp
         runningTime = expectedRunningTime(funnelEntrants || 1, funnelSampleSize || 0, currentDuration)
     } else {
         runningTime = expectedRunningTime(funnelEntrants || 1, funnelSampleSize || 0)
+    }
+
+    // Displayed values
+    const baselineConversionRate = funnelConversionRate.toFixed(1)
+    const minimumAcceptableConversionRate = (funnelConversionRate + (minimumDetectableChange || 5)).toFixed(1)
+    const recommendedSampleSize = humanFriendlyNumber(funnelSampleSize || 0)
+    const recommendedRunningTime = humanFriendlyNumber(runningTime || 0)
+
+    return (
+        <div className="flex flex-wrap">
+            {!experiment?.start_date && (
+                <>
+                    <div className="mb-4 w-1/2">
+                        <div className="card-secondary">Baseline Conversion Rate</div>
+                        <div className="l4">{baselineConversionRate}%</div>
+                    </div>
+                    <div className="mb-4 w-1/2">
+                        <div className="card-secondary">Minimum Acceptable Conversion Rate</div>
+                        <div className="l4">{minimumAcceptableConversionRate}%</div>
+                    </div>
+                </>
+            )}
+            <div className="w-1/2">
+                <div className="card-secondary">Recommended Sample Size</div>
+                <div className="pb-4">
+                    <span className="l4">~{recommendedSampleSize}</span> persons
+                </div>
+            </div>
+            {!experiment?.start_date && (
+                <div className="w-1/2">
+                    <div className="card-secondary">Recommended running time</div>
+                    <div>
+                        <span className="l4">~{recommendedRunningTime}</span> days
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+function TrendCalculation({ experimentId }: ExperimentCalculatorProps): JSX.Element {
+    const { minimumDetectableChange, experiment, trendResults, recommendedExposureForCountData } = useValues(
+        experimentLogic({ experimentId })
+    )
+
+    const trendCount = trendResults[0]?.count || 0
+    const trendExposure = recommendedExposureForCountData(trendCount)
+
+    // Displayed values
+    const baselineCount = humanFriendlyNumber(trendCount || 0)
+    const minimumAcceptableCount = humanFriendlyNumber(
+        trendCount + Math.ceil(trendCount * ((minimumDetectableChange || 5) / 100)) || 0
+    )
+    const recommendedRunningTime = humanFriendlyNumber(trendExposure || 0)
+
+    return (
+        <div className="flex flex-wrap">
+            {!experiment?.start_date && (
+                <>
+                    <div className="mb-4 w-1/2">
+                        <div className="card-secondary">Baseline Count</div>
+                        <div className="l4">{baselineCount}</div>
+                    </div>
+                    <div className="mb-4 w-1/2">
+                        <div className="card-secondary">Minimum Acceptable Count</div>
+                        <div className="l4">{minimumAcceptableCount}</div>
+                    </div>
+                </>
+            )}
+            <div className="w-1/2">
+                <div className="card-secondary">Recommended running time</div>
+                <div>
+                    <span className="l4">~{recommendedRunningTime}</span> days
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export function DataCollectionCalculator({ experimentId }: ExperimentCalculatorProps): JSX.Element {
+    const { experimentInsightType, minimumDetectableChange, experiment, conversionMetrics } = useValues(
+        experimentLogic({ experimentId })
+    )
+    const { setExperiment } = useActions(experimentLogic({ experimentId }))
+
+    // :KLUDGE: need these to mount the Query component to load the insight */
+    const insightLogicInstance = insightLogic({ dashboardItemId: EXPERIMENT_INSIGHT_ID, syncWithUrl: false })
+    const { insightProps } = useValues(insightLogicInstance)
+    const { query } = useValues(insightDataLogic(insightProps))
+
+    const funnelConversionRate = conversionMetrics?.totalRate * 100 || 0
+
+    let sliderMaxValue = 0
+    if (experimentInsightType === InsightType.FUNNELS) {
+        if (100 - funnelConversionRate < 50) {
+            sliderMaxValue = 100 - funnelConversionRate
+        } else {
+            sliderMaxValue = 50
+        }
+    } else {
+        sliderMaxValue = 100
     }
 
     return (
@@ -112,62 +193,9 @@ export function DataCollectionCalculator({ experimentId }: ExperimentPreviewProp
                 </div>
                 <div className="flex flex-col experiment-preview-row">
                     {experimentInsightType === InsightType.TRENDS ? (
-                        <div className="flex flex-wrap">
-                            {!experiment?.start_date && (
-                                <>
-                                    <div className="mb-4 w-1/2">
-                                        <div className="card-secondary">Baseline Count</div>
-                                        <div className="l4">{humanFriendlyNumber(trendCount || 0)}</div>
-                                    </div>
-                                    <div className="mb-4 w-1/2">
-                                        <div className="card-secondary">Minimum Acceptable Count</div>
-                                        <div className="l4">
-                                            {humanFriendlyNumber(
-                                                trendCount +
-                                                    Math.ceil(trendCount * ((minimumDetectableChange || 5) / 100)) || 0
-                                            )}
-                                        </div>
-                                    </div>
-                                </>
-                            )}
-                            <div className="w-1/2">
-                                <div className="card-secondary">Recommended running time</div>
-                                <div>
-                                    <span className="l4">~{humanFriendlyNumber(trendExposure || 0)}</span> days
-                                </div>
-                            </div>
-                        </div>
+                        <TrendCalculation experimentId={experimentId} />
                     ) : (
-                        <div className="flex flex-wrap">
-                            {!experiment?.start_date && (
-                                <>
-                                    <div className="mb-4 w-1/2">
-                                        <div className="card-secondary">Baseline Conversion Rate</div>
-                                        <div className="l4">{funnelConversionRate.toFixed(1)}%</div>
-                                    </div>
-                                    <div className="mb-4 w-1/2">
-                                        <div className="card-secondary">Minimum Acceptable Conversion Rate</div>
-                                        <div className="l4">
-                                            {(funnelConversionRate + (minimumDetectableChange || 5)).toFixed(1)}%
-                                        </div>
-                                    </div>
-                                </>
-                            )}
-                            <div className="w-1/2">
-                                <div className="card-secondary">Recommended Sample Size</div>
-                                <div className="pb-4">
-                                    <span className="l4">~{humanFriendlyNumber(funnelSampleSize || 0)}</span> persons
-                                </div>
-                            </div>
-                            {!experiment?.start_date && (
-                                <div className="w-1/2">
-                                    <div className="card-secondary">Recommended running time</div>
-                                    <div>
-                                        <span className="l4">~{humanFriendlyNumber(runningTime || 0)}</span> days
-                                    </div>
-                                </div>
-                            )}
-                        </div>
+                        <FunnelCalculation experimentId={experimentId} />
                     )}
                     {/* :KLUDGE: mounting the query component to ensure the goal insight is loaded for the calculations */}
                     <div className="hidden">
