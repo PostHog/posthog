b261c8ec35c8eeed482c7378f28102b25f37aca7
Tom Owers <tom@posthog.com>
2024-05-08 08:37:01 +0200

    fix(data-warehouse): Reset the source wizard after connecting a manual source (#22149)
    Reset the source wizard after connecting a manual source

--- frontend/src/scenes/data-warehouse/new/sourceWizardLogic.tsx
+++ frontend/src/scenes/data-warehouse/new/sourceWizardLogic.tsx
@@ -182,7 +182,12 @@ export const sourceWizardLogic = kea<sourceWizardLogicType>([
             preflightLogic,
             ['preflight'],
         ],
-        actions: [dataWarehouseTableLogic, ['resetTable'], dataWarehouseSettingsLogic, ['loadSources']],
+        actions: [
+            dataWarehouseTableLogic,
+            ['resetTable', 'createTableSuccess'],
+            dataWarehouseSettingsLogic,
+            ['loadSources'],
+        ],
     }),
     reducers({
         manualLinkingProvider: [
@@ -437,6 +442,12 @@ export const sourceWizardLogic = kea<sourceWizardLogicType>([
             if (values.currentStep === 4) {
                 actions.closeWizard()
             }
+        },
+        createTableSuccess: () => {
+            actions.onClear()
+            actions.clearSource()
+            actions.loadSources(null)
+            actions.resetSourceConnectionDetails()
         },
         closeWizard: () => {
             actions.onClear()
