faa0a7d5a78f06cd2cd5bd8ccd8fd47df1ce219e
Robbie <robbie.coomber@gmail.com>
2024-05-09 12:13:51 +0100

    feat: Add $is_identified to taxonomy (#22216)
    * Add $is_identified to taxonomy
    
    * Update frontend/src/lib/taxonomy.tsx
    
    Co-authored-by: Thomas Obermüller <thomas.obermueller@gmail.com>
    
    ---------
    
    Co-authored-by: Thomas Obermüller <thomas.obermueller@gmail.com>

--- frontend/src/lib/taxonomy.tsx
+++ frontend/src/lib/taxonomy.tsx
@@ -990,6 +990,10 @@ export const CORE_FILTER_DEFINITIONS_BY_GROUP = {
             label: 'ttclid',
             description: 'TikTok Click ID',
         },
+        $is_identified: {
+            label: 'Is Identified',
+            description: 'When the person was identified',
+        },
     },
     numerical_event_properties: {}, // Same as event properties, see assignment below
     person_properties: {}, // Currently person properties are the same as event properties, see assignment below
