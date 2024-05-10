f2d816f18b97deda694acfc4cfe8bb2f1230859f
Aryan Rawlani <aryanrawlani007@gmail.com>
2024-05-10 11:29:26 +0530

    EMAILS: Refactor test

--- posthog/test/test_templatetags.py
+++ posthog/test/test_templatetags.py
@@ -16,12 +16,17 @@ class TestTemplateTags(TestCase):
         self.assertEqual(percentage(0.7829, 1), "78.3%")
 
     def test_humanize_time_diff(self):
-        self.assertEqual(humanize_time_diff("2023-01-10", "2024-10-05"), "1 year")
-        self.assertEqual(humanize_time_diff("2022-01-01", "2024-10-01"), "2 years")
-        self.assertEqual(humanize_time_diff("2024-01-01", "2024-10-25"), "9 months")
-        self.assertEqual(humanize_time_diff("2024-01-01", "2024-02-01"), "1 month")
-        self.assertEqual(humanize_time_diff("2024-01-01", "2024-01-05"), "4 days")
-        self.assertEqual(humanize_time_diff("2024-01-01", "2024-01-02"), "1 day")
-        self.assertEqual(humanize_time_diff("2022-01-01 00:00:00", "2022-01-01 05:00:00"), "5 hours")
-        self.assertEqual(humanize_time_diff("2022-01-01 00:00:00", "2022-01-01 01:00:00"), "1 hour")
-        self.assertEqual(humanize_time_diff("2022-01-01 00:00:00", "2022-01-01 00:20:00"), "1 hour")
+        test_cases = {
+            ("2023-01-10", "2024-10-05"): "1 year",
+            ("2022-01-01", "2024-10-01"): "2 years",
+            ("2024-01-01", "2024-10-25"): "9 months",
+            ("2024-01-01", "2024-02-01"): "1 month",
+            ("2024-01-01", "2024-01-05"): "4 days",
+            ("2024-01-01", "2024-01-02"): "1 day",
+            ("2022-01-01 00:00:00", "2022-01-01 05:00:00"): "5 hours",
+            ("2022-01-01 00:00:00", "2022-01-01 01:00:00"): "1 hour",
+            ("2022-01-01 00:00:00", "2022-01-01 00:20:00"): "1 hour",
+        }
+
+        for inputs, expected_output in test_cases.items():
+            self.assertEqual(humanize_time_diff(*inputs), expected_output)
