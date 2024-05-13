d7d6a9bc18900ca3b9ad142a9b0d2d6e8973a3df
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-05-13 10:38:49 +0200

    test(hogql): add trends actors tests (#22103)

--- posthog/hogql_queries/insights/insight_actors_query_runner.py
+++ posthog/hogql_queries/insights/insight_actors_query_runner.py
@@ -41,7 +41,7 @@ class InsightActorsQueryRunner(QueryRunner):
                 time_frame=cast(Optional[str], query.day),  # Other runner accept day as int, but not this one
                 series_index=query.series or 0,
                 breakdown_value=query.breakdown,
-                compare=query.compare,
+                compare_value=query.compare,
             )
         elif isinstance(self.source_runner, FunnelsQueryRunner):
             funnels_runner = cast(FunnelsQueryRunner, self.source_runner)
