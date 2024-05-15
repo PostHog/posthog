1e6d0bcadcfaf975ff589744ce867d96ee82adf6
MarconLP <13001502+MarconLP@users.noreply.github.com>
2024-05-15 23:48:50 +0200

    move code into new api stuff

--- posthog/api/support_tickets.py
+++ posthog/api/support_tickets.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+
 from django.http import JsonResponse
 import requests
 from posthog.api.utils import get_token
@@ -5,7 +8,7 @@ from posthog.exceptions import generate_exception_response
 from rest_framework.request import Request
 from rest_framework import status
 
-
+from posthog.models import User
 from posthog.models.team.team import Team
 from django.views.decorators.csrf import csrf_exempt
 
@@ -13,7 +16,7 @@ from posthog.utils_cors import cors_response
 
 
 @csrf_exempt
-def support_tickets(request: Request):
+def get_support_tickets(request: Request):
     token = get_token(None, request)
 
     if not token:
@@ -57,8 +60,9 @@ def support_tickets(request: Request):
 
     user = request.GET.get("user")
     validation_token = request.GET.get("validation_token")
-
     organization_id = team.organization_id
+
+    # do validation using the token above
 
     headers = {"Authorization": f"Basic {zendesk_key}"}
     response = requests.request(
@@ -71,159 +75,260 @@ def support_tickets(request: Request):
 
     zendesk_orgs = response.json()
 
-    # if len(zendesk_orgs["organizations"]) == 0:
-    #     return JsonResponse({"error": "No organization found with that ID"}, status=404)
+    if len(zendesk_orgs["organizations"]) == 0:
+        return JsonResponse({"error": "No organization found with that ID"}, status=404)
 
-    # if zendesk_orgs["organizations"][0]["external_id"] != organization_id:
-    #     return JsonResponse({"error": "Something weird is going on"}, status=404)
+    if zendesk_orgs["organizations"][0]["external_id"] != organization_id:
+        return JsonResponse({"error": "Something weird is going on"}, status=404)
 
-    # zendesk_org_id = zendesk_orgs["organizations"][0]["id"]
-    zendesk_org_id = "123"
+    zendesk_org_id = zendesk_orgs["organizations"][0]["id"]
+
+    tickets = requests.request(
+        "GET",
+        f"https://posthoghelp.zendesk.com/api/v2/organizations/{zendesk_org_id}/tickets",
+        data="",
+        headers=headers,
+    )
+
+    def filter_tickets(tickets):
+        current_time = datetime.now()
+        return [
+            ticket
+            for ticket in tickets
+            if (current_time - datetime.strptime(ticket["updated_at"], "%Y-%m-%dT%H:%M:%SZ")).total_seconds() < 604800
+               or ticket["status"] not in ["solved", "closed"]
+        ]
 
     return cors_response(
         request,
         JsonResponse(
             {
-                "supportTickets": {
-                    "x": user,
-                    "y": validation_token,
-                    "zendesk_org_id": zendesk_org_id,
-                    "org": organization_id,
-                    "zdorgs": zendesk_orgs,
-                    "zendesk_key": zendesk_key,
-                    "team": dir(team),
-                }
+                "tickets": filter_tickets(tickets.json()["tickets"])
             }
         ),
     )
 
 
-# def get_zendesk_tickets(request):
-#     try:
-#         body = json.loads(request.body)
-#     except (TypeError, json.decoder.JSONDecodeError):
-#         return JsonResponse({"error": "Cannot parse request body"}, status=400)
+@csrf_exempt
+def reply_support_ticket(request: Request):
+    token = get_token(None, request)
+
+    if not token:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "API key not provided. You can find your project API key in PostHog project settings.",
+                type="authentication_error",
+                code="missing_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            ),
+        )
+
+    # TODO: fix organization_id & other fields don't exist in the cached object
+    # team = Team.objects.get_team_from_cache_or_token(token)
+    team = Team.objects.get(api_token=token)
+    if team is None:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "Project API key invalid. You can find your project API key in PostHog project settings.",
+                type="authentication_error",
+                code="invalid_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            ),
+        )
+    zendesk_key = team.zendesk_key
+    if zendesk_key is None:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "Zendesk API key not set. You can set it in PostHog project integrations settings.",
+                type="authentication_error",
+                code="missing_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,  # probably should be some other error
+            ),
+        )
+
+    email = request.POST.get("user")
+    validation_token = request.POST.get("validation_token")
+    organization_id = team.organization_id
+
+    # do validation using the token above
+
+    headers = {"Authorization": f"Basic {zendesk_key}"}
+    author = requests.request(
+        "GET", "https://posthoghelp.zendesk.com/api/v2/users/search", json={"query": f"email:{email}"}, headers=headers
+    ).json()
+    author_id = author["users"][0]["id"]
+
+    ticket_id = request.POST.get("ticket_id")
+    comment_body = request.POST.get("comment_body")
+
+    tickets = requests.request(
+        "PUT",
+        f"https://posthoghelp.zendesk.com/api/v2/tickets/{ticket_id}.json",
+        json={"ticket": {"comment": {"body": comment_body, "public": True, "author_id": author_id}}},
+        headers=headers,
+    )
+
+    return cors_response(
+        request,
+        JsonResponse(
+            {
+                "tickets": tickets
+            }
+        ),
+    )
 
 
-#     response = requests.request(
-#         "GET",
-#         "https://posthoghelp.zendesk.com/api/v2/organizations/search",
-#         data="",
-#         headers=headers,
-#         params={"external_id": body.get("org_id")},
-#     )
+@csrf_exempt
+def create_support_ticket(request: Request):
+    token = get_token(None, request)
 
-#     zendesk_orgs = response.json()
+    if not token:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "API key not provided. You can find your project API key in PostHog project settings.",
+                type="authentication_error",
+                code="missing_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            ),
+        )
 
-#     if len(zendesk_orgs["organizations"]) == 0:
-#         return JsonResponse({"error": "No organization found with that ID"}, status=404)
+    # TODO: fix organization_id & other fields don't exist in the cached object
+    # team = Team.objects.get_team_from_cache_or_token(token)
+    team = Team.objects.get(api_token=token)
+    if team is None:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "Project API key invalid. You can find your project API key in PostHog project settings.",
+                type="authentication_error",
+                code="invalid_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            ),
+        )
+    zendesk_key = team.zendesk_key
+    if zendesk_key is None:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "Zendesk API key not set. You can set it in PostHog project integrations settings.",
+                type="authentication_error",
+                code="missing_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,  # probably should be some other error
+            ),
+        )
 
-#     if zendesk_orgs["organizations"][0]["external_id"] != body.get("org_id"):
-#         return JsonResponse({"error": "Something weird is going on"}, status=404)
+    # do validation using the token above
 
-#     zendesk_org_id = zendesk_orgs["organizations"][0]["id"]
+    payload = {
+        "request": {
+            "requester": {"name": request.POST.get("name"), "email": request.POST.get("email")},
+            "subject": request.POST.get("subject"),
+            "custom_fields": [
+                {"id": 22084126888475, "value": request.POST.get("severity_level")},
+                {"id": 22129191462555, "value": request.POST.get("distinct_id")},
+            ],
+            "comment": {
+                "body": request.POST.get("body"),
+            },
+        },
+    }
 
-#     # get all tickets from that org
-#     tickets = requests.request(
-#         "GET",
-#         f"https://posthoghelp.zendesk.com/api/v2/organizations/{zendesk_org_id}/tickets",
-#         data="",
-#         headers=headers,
-#     )
+    response = requests.request("POST", "https://posthoghelp.zendesk.com/api/v2/requests.json", json=payload).json()
 
-#     def filter_tickets(tickets):
-#         current_time = datetime.now()
-#         return [
-#             ticket
-#             for ticket in tickets
-#             if (current_time - datetime.strptime(ticket["updated_at"], "%Y-%m-%dT%H:%M:%SZ")).total_seconds() < 604800
-#             or ticket["status"] not in ["solved", "closed"]
-#         ]
-
-#     return JsonResponse({"tickets": filter_tickets(tickets.json()["tickets"])})
-
-
-# def reply_zendesk_ticket(request):
-#     # do auth
-#     email = "marcus.h@posthog.com"
-
-#     try:
-#         body = json.loads(request.body)
-#     except (TypeError, json.decoder.JSONDecodeError):
-#         return JsonResponse({"error": "Cannot parse request body"}, status=400)
-
-#     headers = {"Authorization": "Basic TOKEN"}
-
-#     author = requests.request(
-#         "GET", "https://posthoghelp.zendesk.com/api/v2/users/search", json={"query": f"email:{email}"}, headers=headers
-#     ).json()
-#     author_id = author["users"][0]["id"]
-
-#     ticket_id = body.get("ticket_id")
-#     comment_body = body.get("comment")["body"]
-
-#     tickets = requests.request(
-#         "PUT",
-#         f"https://posthoghelp.zendesk.com/api/v2/tickets/{ticket_id}.json",
-#         json={"ticket": {"comment": {"body": comment_body, "public": True, "author_id": author_id}}},
-#         headers=headers,
-#     )
-
-#     return JsonResponse({"tickets": tickets.json()})
-
-
-# @authenticate_secondarily
-# def create_new_zendesk_ticket(request):
-#     try:
-#         body = json.loads(request.body)
-#     except (TypeError, json.decoder.JSONDecodeError):
-#         return JsonResponse({"error": "Cannot parse request body"}, status=400)
-
-#     payload = {
-#         "request": {
-#             "requester": {"name": body.get("name"), "email": body.get("email")},
-#             "subject": body.get("subject"),
-#             "custom_fields": [
-#                 {"id": 22084126888475, "value": body.get("severity_level")},
-#                 {"id": 22129191462555, "value": body.get("distinct_id")},
-#             ],
-#             "comment": {
-#                 "body": body.get("body"),
-#             },
-#         },
-#     }
-
-#     response = requests.request("POST", "https://posthoghelp.zendesk.com/api/v2/requests.json", json=payload).json()
-
-#     return JsonResponse({"response": response})
+    return cors_response(
+        request,
+        JsonResponse(
+            {
+                "response": response
+            }
+        ),
+    )
 
 
-# @authenticate_secondarily
-# def close_zendesk_ticket(request):
-#     # do auth
-#     org_id = ""
-#     requester_id = 19444577233435
+@csrf_exempt
+def close_support_ticket(request: Request):
+    token = get_token(None, request)
 
-#     try:
-#         body = json.loads(request.body)
-#     except (TypeError, json.decoder.JSONDecodeError):
-#         return JsonResponse({"error": "Cannot parse request body"}, status=400)
+    if not token:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "API key not provided. You can find your project API key in PostHog project settings.",
+                type="authentication_error",
+                code="missing_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            ),
+        )
 
-#     headers = {"Authorization": "Basic TOKEN"}
+    # TODO: fix organization_id & other fields don't exist in the cached object
+    # team = Team.objects.get_team_from_cache_or_token(token)
+    team = Team.objects.get(api_token=token)
+    if team is None:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "Project API key invalid. You can find your project API key in PostHog project settings.",
+                type="authentication_error",
+                code="invalid_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            ),
+        )
+    zendesk_key = team.zendesk_key
+    if zendesk_key is None:
+        return cors_response(
+            request,
+            generate_exception_response(
+                "support_tickets",
+                "Zendesk API key not set. You can set it in PostHog project integrations settings.",
+                type="authentication_error",
+                code="missing_api_key",
+                status_code=status.HTTP_401_UNAUTHORIZED,  # probably should be some other error
+            ),
+        )
 
-#     ticket_id = body.get("ticket_id")
-#     ticket = requests.request(
-#         "GET", f"https://posthoghelp.zendesk.com/api/v2/tickets/{ticket_id}.json", data="", headers=headers
-#     ).json()
+    # do validation using the token above
+    headers = {"Authorization": f"Basic {zendesk_key}"}
 
-#     if ticket["ticket"]["organization_id"] != org_id and ticket["ticket"]["requester_id"] != requester_id:
-#         return JsonResponse({"error": "You are not authorized to close this ticket"}, status=403)
+    ticket_id = request.POST.get("ticket_id")
+    organization_id = team.organization_id
+    email = request.POST.get('email')
 
-#     requests.request(
-#         "PUT",
-#         f"https://posthoghelp.zendesk.com/api/v2/tickets/{ticket_id}.json",
-#         json={"ticket": {"status": "solved"}},
-#         headers=headers,
-#     )
+    ticket = requests.request(
+        "GET", f"https://posthoghelp.zendesk.com/api/v2/tickets/{ticket_id}.json", data="", headers=headers
+    ).json()
 
-#     return JsonResponse({"ticket": ticket})
+    author = requests.request(
+        "GET", "https://posthoghelp.zendesk.com/api/v2/users/search", json={"query": f"email:{email}"}, headers=headers
+    ).json()
+    author_id = author["users"][0]["id"]
+
+    if ticket["ticket"]["organization_id"] != organization_id and ticket["ticket"]["requester_id"] != author_id:
+        return JsonResponse({"error": "You are not authorized to close this ticket"}, status=403)
+
+    response = requests.request(
+        "PUT",
+        f"https://posthoghelp.zendesk.com/api/v2/tickets/{ticket_id}.json",
+        json={"ticket": {"status": "solved"}},
+        headers=headers,
+    )
+
+    return cors_response(
+        request,
+        JsonResponse(
+            {
+                "response": response
+            }
+        ),
+    )
