ec2728ab81b8f3bf348ee10addad874ca899ea87
Ben White <ben@posthog.com>
2024-05-15 22:26:13 +0300

    Updated referral code thing

--- frontend/src/scenes/settings/user/MyReferrals.tsx
+++ frontend/src/scenes/settings/user/MyReferrals.tsx
@@ -1,19 +1,70 @@
-import { useActions, useValues } from 'kea'
+import { afterMount, connect, kea, path, useValues } from 'kea'
+import { loaders } from 'kea-loaders'
+import { CodeSnippet } from 'lib/components/CodeSnippet'
+import { teamLogic } from 'scenes/teamLogic'
 import { userLogic } from 'scenes/userLogic'
 
+import { ReferralIdentity } from '~/types'
+
+import type { myReferralsLogicType } from './MyReferralsType'
+
+const myReferralsLogic = kea<myReferralsLogicType>([
+    path(['scenes', 'settings', 'user', 'myReferralsLogic']),
+    connect({
+        values: [userLogic, ['user'], teamLogic, ['currentTeam']],
+    }),
+    loaders(({ values }) => ({
+        referrerInfo: [
+            null as ReferralIdentity | null,
+            {
+                loadReferrerInfo: async () => {
+                    // We hardcode the values here as they are global
+                    // TODO: Limit to cloud?
+
+                    const host = window.location.origin // TODO: Update to us.posthog.com when ready
+                    const referralProgram = '6ZrDckau' // TODO: Update to deployed value when ready
+                    const token = values.currentTeam!.api_token // TODO: Update to 'sTMFPsFhdP1Ssg'
+
+                    const response = await fetch(
+                        host +
+                            `/api/referrals/${referralProgram}/referrer/?token=${token}&referrer_id=${
+                                values.user!.uuid
+                            }`
+                    )
+
+                    if (response.status !== 200) {
+                        throw new Error('Failed to load referrer info')
+                    }
+                    return response.json()
+                },
+            },
+        ],
+    })),
+
+    afterMount(({ actions }) => {
+        actions.loadReferrerInfo()
+    }),
+])
+
 export function MyReferrals(): JSX.Element {
-    const { user, userLoading } = useValues(userLogic)
-    const { updateUser } = useActions(userLogic)
+    const { referrerInfo, referrerInfoLoading } = useValues(myReferralsLogic)
 
     return (
-        <div>
+        <div className="text-center border-2 border-dashed rounded p-2">
+            <h1 className="font-bold">Join our pyramid scheme</h1>
             <p>
-                Refer a friend (or an enemy - more the merrier) to PostHog! When they join you both get{' '}
-                <b>INSERT PERK HERE</b>
+                Want free stuff? Of course you do. Join our <b>totally original and unique</b> referral scheme to get
+                sweet merch, platform credits, and good vibes. One referral = 1 vibe. Plus, get the tools to build your
+                own ludicrously successful pyramid scheme included for free.
             </p>
-            <div className="border rounded border-dashed p-10 font-semibold text-4xl text-center cursor-pointer bg-bg-light hover:text-accent">
-                <code>{window.location.origin}/?rcode=0231908948913849</code>
-            </div>
+
+            <CodeSnippet thing="referral link">
+                {referrerInfoLoading
+                    ? 'Mining bitcoins, please wait...'
+                    : referrerInfo?.code
+                    ? `${window.location.origin}/?rcode=${referrerInfo?.code}`
+                    : 'Something went wrong...'}
+            </CodeSnippet>
         </div>
     )
 }
