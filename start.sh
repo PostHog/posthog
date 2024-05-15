b2e7cc8ba014a968846ef1f0d985ef02139b488b
James Greenhill <fuziontech@gmail.com>
2024-05-15 13:21:44 +0300

    update start script

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+<<<<<<< Updated upstream
 if [[ ! -f mmdb.db ]]; then
     sudo apt-get install -y curl ca-certificates brotli
     curl https://mmdbcdn.posthog.net/ | brotli -d > mmdb.db
 fi
 
 ./livestream
+=======
+sudo apt-get install -y curl ca-certificates brotli
+curl https://mmdbcdn.posthog.net/ | brotli -d > mmdb.db
+git pull && go build && ./start.sh
+>>>>>>> Stashed changes
