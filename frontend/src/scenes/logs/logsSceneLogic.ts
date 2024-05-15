0fe879811b9cfd6e47c18028c8f6e8cdf1e851d1
Tom Owers <owerstom@gmail.com>
2024-05-15 23:08:48 +0300

    Revert deletion

--- frontend/src/scenes/logs/logsSceneLogic.ts
+++ frontend/src/scenes/logs/logsSceneLogic.ts
@@ -1,5 +1,27 @@
-import { kea, path } from 'kea'
+import { actions, kea, path, reducers } from 'kea'
+
+import { LogsQuery, NodeKind } from '~/queries/schema'
 
 import type { logsSceneLogicType } from './logsSceneLogicType'
 
-export const logsSceneLogic = kea<logsSceneLogicType>([path(['scenes', 'logs', 'logsSceneLogic'])])
+const DEFAULT_QUERY: LogsQuery = {
+    kind: NodeKind.LogsQuery,
+    dateRange: {
+        date_from: '-7d',
+    },
+}
+
+export const logsSceneLogic = kea<logsSceneLogicType>([
+    path(['scenes', 'logs', 'logsSceneLogic']),
+    actions({
+        setQuery: (query: Partial<LogsQuery>) => ({ query }),
+    }),
+    reducers({
+        query: [
+            DEFAULT_QUERY,
+            {
+                setQuery: (state, { query }) => ({ ...state, ...query }),
+            },
+        ],
+    }),
+])
