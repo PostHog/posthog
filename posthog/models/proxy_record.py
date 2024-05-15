39852d0f5deec0a95c8af011486f3ab7f2009a80
David Newell <david@posthog.com>
2024-05-15 12:19:52 +0300

    add fields

--- posthog/models/proxy_record.py
+++ posthog/models/proxy_record.py
@@ -8,4 +8,17 @@ class ProxyRecord(UUIDModel):
         Organization, on_delete=models.CASCADE, related_name="proxy_records"
     )
     domain: models.CharField = models.CharField(max_length=64, unique=True)
-    # dns_records: models.CharField = models.CharField(max_length=64, unique=True)
+    dns_records: models.JSONField = models.JSONField(null=True, blank=True)
+
+    status: models.JSONField = models.JSONField(null=True, blank=True)
+
+    class Status(models.TextChoices):
+        GENERATING = "generating"
+        PROVISIONING = "provisioning"
+        VALID = "valid"
+        REMOVED = "removed"
+
+    status = models.CharField(
+        choices=Status.choices,
+        default=Status.GENERATING,
+    )
