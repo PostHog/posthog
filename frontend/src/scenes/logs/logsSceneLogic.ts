0bbfef4f2185b0d1952c16098eee92b90cf6cc2e
Paul D'Ambra <paul@posthog.com>
2024-05-15 19:58:58 +0300

    not as an idiot

--- frontend/src/scenes/logs/logsSceneLogic.ts
+++ frontend/src/scenes/logs/logsSceneLogic.ts
@@ -1,27 +1,5 @@
-import { actions, kea, path, reducers } from 'kea'
-
-import { LogsQuery, NodeKind } from '~/queries/schema'
+import { kea, path } from 'kea'
 
 import type { logsSceneLogicType } from './logsSceneLogicType'
 
-const DEFAULT_QUERY: LogsQuery = {
-    kind: NodeKind.LogsQuery,
-    dateRange: {
-        date_from: '-7d',
-    },
-}
-
-export const logsSceneLogic = kea<logsSceneLogicType>([
-    path(['scenes', 'logs', 'logsSceneLogic']),
-    actions({
-        setQuery: (query: Partial<LogsQuery>) => ({ query }),
-    }),
-    reducers({
-        query: [
-            DEFAULT_QUERY,
-            {
-                setQuery: (state, { query }) => ({ ...state, ...query }),
-            },
-        ],
-    }),
-])
+export const logsSceneLogic = kea<logsSceneLogicType>([path(['scenes', 'logs', 'logsSceneLogic'])])
