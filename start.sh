d7c8fba0ce76f7fa01df765643b7275a8b7dc16b
Brett Hoerner <brett@bretthoerner.com>
2024-05-15 13:18:51 +0300

    if in start

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y curl ca-certificates brotli
-curl https://mmdbcdn.posthog.net/ | brotli -d > mmdb.db
+if [[ ! -f mmdb.db ]]; then
+    sudo apt-get install -y curl ca-certificates brotli
+    curl https://mmdbcdn.posthog.net/ | brotli -d > mmdb.db
+fi
+
 ./livestream
