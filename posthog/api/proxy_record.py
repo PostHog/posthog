77e39e40aef595cbc0fde953ec2f766f29b78735
David Newell <david@posthog.com>
2024-05-15 17:36:15 +0300

    cleanup

--- posthog/api/proxy_record.py
+++ posthog/api/proxy_record.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-
 from rest_framework import serializers
 from rest_framework.viewsets import ModelViewSet
 
@@ -7,8 +6,6 @@ from posthog.api.routing import TeamAndOrgViewSetMixin
 from posthog.models import ProxyRecord
 from posthog.permissions import OrganizationAdminWritePermissions
 from rest_framework.response import Response
-
-DOMAIN_REGEX = r"^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$"
 
 
 class ProxyRecordSerializer(serializers.ModelSerializer):
@@ -30,7 +27,6 @@ class ProxyRecordViewset(TeamAndOrgViewSetMixin, ModelViewSet):
     scope_object = "organization"
     serializer_class = ProxyRecordSerializer
     permission_classes = [OrganizationAdminWritePermissions]
-    queryset = ProxyRecord.objects.order_by("domain").all()
 
     def list(self, request, *args, **kwargs):
         queryset = self.organization.proxy_records
@@ -40,10 +36,6 @@ class ProxyRecordViewset(TeamAndOrgViewSetMixin, ModelViewSet):
     def create(self, request, *args, **kwargs):
         domain = request.data.get("domain")
         queryset = self.organization.proxy_records
-        queryset.create(
-            created_by=cast(User, request.user),
-            domain=domain,
-            target_cname=settings.PROXY_TARGET_CNAME
-        )
+        queryset.create(created_by=request.user, domain=domain, target_cname=settings.PROXY_TARGET_CNAME)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
