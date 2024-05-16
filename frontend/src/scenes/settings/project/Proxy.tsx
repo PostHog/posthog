c3767cb464d09e218b6bc6fb8cf4e0d79dcc084f
Frank Hamand <frankhamand@gmail.com>
2024-05-16 11:11:41 +0300

    feat: dynamic cname targets for proxy as a service (#22315)
    * Show all CNAME records required in frontend
    
    * set dynamically generated CNAME targets
    
    * Update frontend/src/scenes/settings/project/Proxy.tsx
    
    * Update frontend/src/scenes/settings/project/Proxy.tsx
    
    * wrap child in span
    
    * include domain suffix for proxy
    
    * fix ruff
    
    ---------
    
    Co-authored-by: David Newell <d.newell1@outlook.com>

--- frontend/src/scenes/settings/project/Proxy.tsx
+++ frontend/src/scenes/settings/project/Proxy.tsx
@@ -104,7 +104,7 @@ function CreateRecordForm(): JSX.Element {
     const { formState, proxyRecordsLoading, proxyRecords } = useValues(proxyLogic)
     const { collapseForm } = useActions(proxyLogic)
 
-    const mostRecentRecord = proxyRecords.find((r) => r.status === 'waiting')
+    const waitingRecords = proxyRecords.filter((r) => r.status === 'waiting')
 
     return (
         <div className="bg-bg-light rounded border p-2 space-y-2">
@@ -139,11 +139,15 @@ function CreateRecordForm(): JSX.Element {
                 <>
                     <div className="text-xl font-semibold leading-tight">Almost there</div>
                     <div>
-                        You need to set the <b>CNAME</b> record on your DNS provider:
+                        You need to set the following <b>CNAME</b> records in your DNS provider:
                     </div>
-                    <CodeSnippet language={Language.HTTP}>
-                        {mostRecentRecord ? mostRecentRecord.cname_target : DEFAULT_CNAME}
-                    </CodeSnippet>
+                    {waitingRecords.map((r) => (
+                        <CodeSnippet key={r.id} language={Language.HTTP}>
+                            <span>
+                                {r.domain} {'->'} {r ? r.cname_target : DEFAULT_CNAME}
+                            </span>
+                        </CodeSnippet>
+                    ))}
                     <div className="flex justify-end">
                         <LemonButton onClick={collapseForm} type="primary">
                             Done
