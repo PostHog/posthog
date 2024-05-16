79fff9d4d1b89a9d98c56fc43194010d3b5bdd76
David Newell <david@posthog.com>
2024-05-16 10:37:16 +0300

    dn-fix/compression-img-type

--- frontend/src/scenes/session-recordings/player/rrweb/canvas/canvas-plugin.ts
+++ frontend/src/scenes/session-recordings/player/rrweb/canvas/canvas-plugin.ts
@@ -88,7 +88,7 @@ export const CanvasReplayerPlugin = (events: eventWithTime[]): ReplayPlugin => {
 
                 const img = containers.get(e.data.id)
                 if (img) {
-                    img.src = target.toDataURL('image/jpeg', 0.6)
+                    img.src = target.toDataURL('image/webp', 0.6)
                     img.style.width = 'initial'
                     img.style.height = 'initial'
                 }
