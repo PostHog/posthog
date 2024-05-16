2adcad511b2d9b6f4e5dfcc93a9b9541418eb2cb
Ben White <ben@posthog.com>
2024-05-16 12:51:35 +0300

    Nice

--- frontend/src/lib/components/HedgehogBuddy/HedgehogOptions.tsx
+++ frontend/src/lib/components/HedgehogBuddy/HedgehogOptions.tsx
@@ -2,6 +2,7 @@ import { LemonButton, LemonSwitch } from '@posthog/lemon-ui'
 import clsx from 'clsx'
 import { useActions, useValues } from 'kea'
 import { capitalizeFirstLetter } from 'lib/utils'
+import React from 'react'
 
 import { HedgehogBuddyAccessory } from './components/AccessoryButton'
 import { COLOR_TO_FILTER_MAP, hedgehogBuddyLogic } from './hedgehogBuddyLogic'
@@ -18,32 +19,34 @@ export function HedgehogOptions(): JSX.Element {
                 <br />
                 You can move me around by clicking and dragging or control me with WASD / arrow keys.
             </p>
-            <h4>Options</h4>
-            <div className="flex items-center gap-2 flex-wrap">
-                <LemonSwitch
-                    bordered
-                    label="Walk around freely"
-                    checked={freeMovement}
-                    onChange={setFreeMovement}
-                    tooltip="If enabled the Hedgehog will walk around the screen, otherwise they will stay in one place. You can still move them around by dragging them."
-                />
-                <LemonSwitch
-                    bordered
-                    label="Interact with elements"
-                    checked={interactWithElements}
-                    onChange={setInteractWithElements}
-                    tooltip="If enabled the Hedgehog might land on elements of the application, otherwise they will always land on the ground"
-                />
-                <LemonSwitch
-                    bordered
-                    label="Keyboard controls (WASD / arrow keys)"
-                    checked={keyboardControlsEnabled}
-                    onChange={setKeyboardControlsEnabled}
-                    tooltip="If enabled you can use the WASD or arrow key + space to move around and jump."
-                />
+            <div className="space-y-2">
+                <h4>Options</h4>
+                <div className="flex items-center gap-2 flex-wrap">
+                    <LemonSwitch
+                        bordered
+                        label="Walk around freely"
+                        checked={freeMovement}
+                        onChange={setFreeMovement}
+                        tooltip="If enabled the Hedgehog will walk around the screen, otherwise they will stay in one place. You can still move them around by dragging them."
+                    />
+                    <LemonSwitch
+                        bordered
+                        label="Interact with elements"
+                        checked={interactWithElements}
+                        onChange={setInteractWithElements}
+                        tooltip="If enabled the Hedgehog might land on elements of the application, otherwise they will always land on the ground"
+                    />
+                    <LemonSwitch
+                        bordered
+                        label="Keyboard controls (WASD / arrow keys)"
+                        checked={keyboardControlsEnabled}
+                        onChange={setKeyboardControlsEnabled}
+                        tooltip="If enabled you can use the WASD or arrow key + space to move around and jump."
+                    />
+                </div>
+                <HedgehogColor />
+                <HedgehogAccessories />
             </div>
-            <HedgehogColor />
-            <HedgehogAccessories />
         </div>
     )
 }
@@ -52,7 +55,7 @@ function HedgehogAccessories(): JSX.Element {
     return (
         <>
             {accessoryGroups.map((group) => (
-                <div key={group}>
+                <React.Fragment key={group}>
                     <h4>{capitalizeFirstLetter(group)}</h4>
 
                     <div className="flex gap-2 pb-2 pt-px overflow-y-auto flex-wrap">
@@ -66,7 +69,7 @@ function HedgehogAccessories(): JSX.Element {
                                 />
                             ))}
                     </div>
-                </div>
+                </React.Fragment>
             ))}
         </>
     )
