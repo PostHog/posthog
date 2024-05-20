9da561ac8c521ec7b519e0279703a63cc0760f9c
Phani Raj <phani@posthog.com>
2024-05-20 09:12:55 -0500

    fix migrations

--- posthog/migrations/0413_survey_current_iteration_and_more.py
+++ posthog/migrations/0413_survey_current_iteration_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0411_eventproperty_indexes"),
+        ("posthog", "0412_proxyrecord"),
     ]
 
     operations = [
