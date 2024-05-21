e70784a1935292eace4a59c64d471f501446542e
Frank Hamand <frankhamand@gmail.com>
2024-05-21 10:24:09 +0100

    Fix proxy workflows not being registered to worker

--- posthog/temporal/proxy_service/__init__.py
+++ posthog/temporal/proxy_service/__init__.py
@@ -1,2 +1,24 @@
 from posthog.temporal.proxy_service.create import CreateHostedProxyInputs
 from posthog.temporal.proxy_service.delete import DeleteHostedProxyInputs
+
+from posthog.temporal.proxy_service.common import update_proxy_record
+from posthog.temporal.proxy_service.create import (
+    wait_for_dns_records,
+    create_hosted_proxy,
+    wait_for_certificate,
+    CreateHostedProxyWorkflow,
+)
+
+from posthog.temporal.proxy_service.delete import delete_proxy_record, delete_hosted_proxy, DeleteHostedProxyWorkflow
+
+
+WORKFLOWS = [CreateHostedProxyWorkflow, DeleteHostedProxyWorkflow]
+
+ACTIVITIES = [
+    update_proxy_record,
+    wait_for_dns_records,
+    create_hosted_proxy,
+    wait_for_certificate,
+    delete_proxy_record,
+    delete_hosted_proxy,
+]
