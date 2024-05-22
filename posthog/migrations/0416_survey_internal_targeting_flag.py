1d7a191eb9c42e85a2a918dba89bae2ebe943932
Phani Raj <phani@posthog.com>
2024-05-22 13:19:26 -0500

    move migration commands to single line

--- posthog/migrations/0416_survey_internal_targeting_flag.py
+++ posthog/migrations/0416_survey_internal_targeting_flag.py
@@ -5,6 +5,7 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
+    atomic = False  # Added to support concurrent index creation
     dependencies = [
         ("posthog", "0415_pluginconfig_match_action"),
     ]
@@ -29,12 +30,8 @@ class Migration(migrations.Migration):
                 # We add -- existing-table-constraint-ignore to ignore the constraint validation in CI.
                 migrations.RunSQL(
                     """
-                    ALTER TABLE "posthog_survey"
-                    ADD COLUMN "internal_targeting_flag_id" integer NULL
-                    CONSTRAINT "posthog_survey_internal_targeting_f_b3911925_fk_posthog_f"
-                    REFERENCES "posthog_featureflag"("id") DEFERRABLE INITIALLY DEFERRED;
-                    SET CONSTRAINTS "posthog_survey_internal_targeting_f_b3911925_fk_posthog_f"
-                    IMMEDIATE; -- existing-table-constraint-ignore
+                    ALTER TABLE "posthog_survey" ADD COLUMN "internal_targeting_flag_id" integer NULL CONSTRAINT "posthog_survey_internal_targeting_f_b3911925_fk_posthog_f" REFERENCES "posthog_featureflag"("id") DEFERRABLE INITIALLY DEFERRED; -- existing-table-constraint-ignore
+                    SET CONSTRAINTS "posthog_survey_internal_targeting_f_b3911925_fk_posthog_f" IMMEDIATE; -- existing-table-constraint-ignore
                     """,
                     reverse_sql="""
                         ALTER TABLE "posthog_survey" DROP COLUMN IF EXISTS "internal_targeting_flag_id";
@@ -43,7 +40,7 @@ class Migration(migrations.Migration):
                 # We add CONCURRENTLY to the create command
                 migrations.RunSQL(
                     """
-                    CREATE INDEX "posthog_survey_internal_targeting_flag_id_b3911925" ON "posthog_survey" ("internal_targeting_flag_id");
+                    CREATE INDEX CONCURRENTLY "posthog_survey_internal_targeting_flag_id_b3911925" ON "posthog_survey" ("internal_targeting_flag_id");
                     """,
                     reverse_sql="""
                         DROP INDEX IF EXISTS "posthog_survey_internal_targeting_flag_id_b3911925";
