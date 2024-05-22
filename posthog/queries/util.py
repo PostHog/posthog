a126b36f72c711f688956a92531ed140bee33da6
Michael Matloka <michal@matloka.com>
2024-05-22 11:52:26 +0200

    Protect against non-numbers instead of disabling adjustment

--- posthog/queries/util.py
+++ posthog/queries/util.py
@@ -161,15 +161,15 @@ def correct_result_for_sampling(
 ) -> Union[int, float]:
     from posthog.queries.trends.util import ALL_SUPPORTED_MATH_FUNCTIONS
 
-    NON_SAMPLING_ADJUSTABLE_MATH = [*ALL_SUPPORTED_MATH_FUNCTIONS, "hogql"]
-
     # We don't adjust results for sampling if:
     # - There's no sampling_factor specified i.e. the query isn't sampled
+    # - The value is not a number (should not happen, but being defensive, especially against HogQL aggregation)
     # - The query performs a math operation other than 'sum' because statistical math operations
-    # - The query performs a user-defined HogQL operation, as we don't know if it makes sense to adjust that result
     # on sampled data yield results in the correct format
-    if (not sampling_factor) or (
-        entity_math is not None and entity_math != "sum" and entity_math in NON_SAMPLING_ADJUSTABLE_MATH
+    if (
+        not sampling_factor
+        or not isinstance(value, int | float)
+        or (entity_math is not None and entity_math != "sum" and entity_math in ALL_SUPPORTED_MATH_FUNCTIONS)
     ):
         return value
 
