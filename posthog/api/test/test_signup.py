de1bd84bda3e33d774facc5bece28b103740f5f6
Ben White <ben@posthog.com>
2024-05-22 10:40:56 +0200

    chore: Remove all email opt out references (#22390)

--- posthog/api/test/test_signup.py
+++ posthog/api/test/test_signup.py
@@ -48,7 +48,6 @@ class TestSignupAPI(APIBaseTest):
                 "password": "notsecure",
                 "organization_name": "Hedgehogs United, LLC",
                 "role_at_organization": "product",
-                "email_opt_in": False,
             },
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -75,7 +74,6 @@ class TestSignupAPI(APIBaseTest):
         self.assertEqual(user.first_name, "John")
         self.assertEqual(user.last_name, "Doe")
         self.assertEqual(user.email, "hedgehog@posthog.com")
-        self.assertFalse(user.email_opt_in)
         self.assertTrue(user.is_staff)  # True because this is the first user in the instance
         self.assertFalse(user.is_email_verified)
 
@@ -224,7 +222,6 @@ class TestSignupAPI(APIBaseTest):
         # Assert that the user & org were properly created
         self.assertEqual(user.first_name, "Jane")
         self.assertEqual(user.email, "hedgehog2@posthog.com")
-        self.assertTrue(user.email_opt_in)  # Defaults to True
         self.assertEqual(organization.name, f"{user.first_name}'s Organization")
         self.assertTrue(user.is_staff)  # True because this is the first user in the instance
 
@@ -861,7 +858,6 @@ class TestInviteSignupAPI(APIBaseTest):
             {
                 "first_name": "Alice",
                 "password": "test_password",
-                "email_opt_in": True,
                 "role_at_organization": "Engineering",
             },
         )
@@ -895,7 +891,6 @@ class TestInviteSignupAPI(APIBaseTest):
         # Assert that the user was properly created
         self.assertEqual(user.first_name, "Alice")
         self.assertEqual(user.email, "test+99@posthog.com")
-        self.assertEqual(user.email_opt_in, True)
 
         # Assert that the sign up event & identify calls were sent to PostHog analytics
         mock_capture.assert_called_once()
@@ -939,7 +934,7 @@ class TestInviteSignupAPI(APIBaseTest):
 
         response = self.client.post(
             f"/api/signup/{invite.id}/",
-            {"first_name": "Alice", "password": "test_password", "email_opt_in": True},
+            {"first_name": "Alice", "password": "test_password"},
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         user = cast(User, User.objects.order_by("-pk")[0])
@@ -986,7 +981,6 @@ class TestInviteSignupAPI(APIBaseTest):
                 {
                     "first_name": "Alice",
                     "password": "test_password",
-                    "email_opt_in": True,
                 },
             )
 
@@ -1008,7 +1002,6 @@ class TestInviteSignupAPI(APIBaseTest):
                     {
                         "first_name": "Alice",
                         "password": "test_password",
-                        "email_opt_in": True,
                     },
                 )
 
@@ -1040,7 +1033,6 @@ class TestInviteSignupAPI(APIBaseTest):
                 {
                     "first_name": "Alice",
                     "password": "test_password",
-                    "email_opt_in": True,
                 },
             )
 
@@ -1401,7 +1393,7 @@ class TestInviteSignupAPI(APIBaseTest):
     def test_cannot_use_social_invite_sign_up_without_required_attributes(self):
         Organization.objects.all().delete()  # Can only create organizations in fresh instances
 
-        response = self.client.post("/api/social_signup", {"email_opt_in": False})
+        response = self.client.post("/api/social_signup", {})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             response.json(),
