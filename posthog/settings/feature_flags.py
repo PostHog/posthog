44e9723286086e35d9ae66c592086c89d9c77a91
Michael Matloka <michal@matloka.com>
2024-05-22 22:19:21 +0200

    chore: Add a few flags to `PERSISTED_FEATURE_FLAGS`

--- posthog/settings/feature_flags.py
+++ posthog/settings/feature_flags.py
@@ -9,4 +9,9 @@ PERSISTED_FEATURE_FLAGS = [
     "simplify-actions",
     "historical-exports-v2",
     "ingestion-warnings-enabled",
+    "new-experiments-ui",
+    "hogql-in-insight-serialization",
+    "hogql-insights-preview",
+    "persons-hogql-query",
+    "datanode-concurrency-limit",
 ]
