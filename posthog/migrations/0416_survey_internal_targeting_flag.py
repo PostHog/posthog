d645b2e5a264c60c2b883fc824a4c0f5ccda26d0
Phani Raj <phani@posthog.com>
2024-05-22 12:42:50 -0500

    Fix migration to use non-blocking ALTER

--- posthog/migrations/0416_survey_internal_targeting_flag.py
+++ posthog/migrations/0416_survey_internal_targeting_flag.py
@@ -10,16 +10,45 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="survey",
-            name="internal_targeting_flag",
-            field=models.ForeignKey(
-                blank=True,
-                null=True,
-                on_delete=django.db.models.deletion.SET_NULL,
-                related_name="surveys_internal_targeting_flag",
-                related_query_name="survey_internal_targeting_flag",
-                to="posthog.featureflag",
-            ),
-        ),
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddField(
+                    model_name="survey",
+                    name="internal_targeting_flag",
+                    field=models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        related_name="surveys_internal_targeting_flag",
+                        related_query_name="survey_internal_targeting_flag",
+                        to="posthog.featureflag",
+                    ),
+                )
+            ],
+            database_operations=[
+                # We add -- existing-table-constraint-ignore to ignore the constraint validation in CI.
+                migrations.RunSQL(
+                    """
+                    ALTER TABLE "posthog_survey"
+                    ADD COLUMN "internal_targeting_flag_id" integer NULL
+                    CONSTRAINT "posthog_survey_internal_targeting_f_b3911925_fk_posthog_f"
+                    REFERENCES "posthog_featureflag"("id") DEFERRABLE INITIALLY DEFERRED;
+                    SET CONSTRAINTS "posthog_survey_internal_targeting_f_b3911925_fk_posthog_f"
+                    IMMEDIATE; -- existing-table-constraint-ignore
+                    """,
+                    reverse_sql="""
+                        ALTER TABLE "posthog_survey" DROP COLUMN IF EXISTS "internal_targeting_flag_id";
+                    """,
+                ),
+                # We add CONCURRENTLY to the create command
+                migrations.RunSQL(
+                    """
+                    CREATE INDEX "posthog_survey_internal_targeting_flag_id_b3911925" ON "posthog_survey" ("internal_targeting_flag_id");
+                    """,
+                    reverse_sql="""
+                        DROP INDEX IF EXISTS "posthog_survey_internal_targeting_flag_id_b3911925";
+                    """,
+                ),
+            ],
+        )
     ]
