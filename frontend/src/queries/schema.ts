d7e01856290ac05ba5e653c372a04b3af53684cc
Robbie <robbie.coomber@gmail.com>
2024-05-23 16:01:23 +0100

    feat(personless-events): Change persons join for personless events (#22425)
    * Change persons join for personless events
    
    * Simplify name to inner
    
    * Fix enum value
    
    * Simplify persons id query
    
    * Update cache keys, as modifiers object has changed
    
    * Change pdi join, revert soft delete behaviour
    
    * Fix copy

--- frontend/src/queries/schema.ts
+++ frontend/src/queries/schema.ts
@@ -197,6 +197,7 @@ export interface HogQLQueryModifiers {
     dataWarehouseEventsModifiers?: DataWarehouseEventsModifier[]
     debug?: boolean
     s3TableUseInvalidColumns?: boolean
+    personsJoinMode?: 'inner' | 'left'
 }
 
 export interface DataWarehouseEventsModifier {
