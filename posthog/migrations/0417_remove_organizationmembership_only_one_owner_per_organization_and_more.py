1a4346178ae3783b5cb4a55921cbfe3d9f35f21c
Bianca Yang <bianca@posthog.com>
2024-05-23 13:36:32 -0700

    fix migration

--- posthog/migrations/0417_remove_organizationmembership_only_one_owner_per_organization_and_more.py
+++ posthog/migrations/0417_remove_organizationmembership_only_one_owner_per_organization_and_more.py
@@ -3,6 +3,13 @@
 from django.db import migrations, models
 
 
+def backfill_invite_level(apps, schema_editor):
+    OrganizationInvite = apps.get_model("posthog", "organizationinvite")
+    for invite in OrganizationInvite.objects.all():
+        invite.level = 1
+        invite.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("posthog", "0416_survey_internal_targeting_flag"),
@@ -17,7 +24,18 @@ class Migration(migrations.Migration):
             model_name="organizationinvite",
             name="level",
             field=models.PositiveSmallIntegerField(
-                choices=[(1, "member"), (8, "administrator"), (15, "owner")], default=1
+                choices=[(1, "member"), (8, "administrator"), (15, "owner")], default=1, null=True, blank=True
+            ),
+        ),
+        migrations.RunSQL(
+            "update posthog_organizationinvite set level = 1",
+            reverse_sql="update posthog_organizationinvite set level = NULL",
+        ),
+        migrations.AlterField(
+            model_name="organizationinvite",
+            name="level",
+            field=models.PositiveSmallIntegerField(
+                choices=[(1, "member"), (8, "administrator"), (15, "owner")], default=1, null=False, blank=False
             ),
         ),
     ]
