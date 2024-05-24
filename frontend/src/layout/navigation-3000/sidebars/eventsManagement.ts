c9251c3e529addf9d4f1ca3e4f3530470432e9e8
Zach Waterfield <zlwaterfield@gmail.com>
2024-05-24 10:58:13 -0400

    Delete eventsManagement.ts

--- frontend/src/layout/navigation-3000/sidebars/eventsManagement.ts
+++
@@ -1,7 +0,0 @@
-import { kea, path } from 'kea'
-
-import type { eventsManagementSidebarLogicType } from './eventsManagementType'
-
-export const eventsManagementSidebarLogic = kea<eventsManagementSidebarLogicType>([
-    path(['layout', 'navigation-3000', 'sidebars', 'eventsManagementSidebarLogic']),
-])
