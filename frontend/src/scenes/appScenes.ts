0c6c983e7b17712e589bec8ed3943f99c5681509
Zach Waterfield <zlwaterfield@gmail.com>
2024-05-24 16:28:18 -0400

    Fix types

--- frontend/src/scenes/appScenes.ts
+++ frontend/src/scenes/appScenes.ts
@@ -77,4 +77,6 @@ export const appScenes: Record<Scene, () => any> = {
     [Scene.Settings]: () => import('./settings/SettingsScene'),
     [Scene.MoveToPostHogCloud]: () => import('./moveToPostHogCloud/MoveToPostHogCloud'),
     [Scene.Heatmaps]: () => import('./heatmaps/HeatmapsScene'),
+    [Scene.ExploreEvents]: () => import('./activity/events/EventsScene'),
+    [Scene.LiveEvents]: () => import('./activity/live-events/LiveEventsTable'),
 }
