e311b3ece3b60e5413c9a7753fba700423271653
Ben White <ben@posthog.com>
2024-05-28 13:59:31 +0100

    fix

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -15,7 +15,7 @@ import { ResponseComposition, RestContext, RestRequest } from 'msw'
 import { SharingConfigurationType } from '~/types'
 
 import { getAvailableProductFeatures } from './features'
-import { billingJson } from './fixtures/_billing_v2'
+import { billingJson } from './fixtures/_billing'
 import { Mocks, MockSignature, mocksToHandlers } from './utils'
 
 export const EMPTY_PAGINATED_RESPONSE = { count: 0, results: [] as any[], next: null, previous: null }
