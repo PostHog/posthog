9d0ee1ae09759ed7123b09604582273611e9cf29
dylan <dylan@posthog.com>
2024-05-28 18:11:35 -0700

    changes in a more reasonable scope

--- frontend/src/scenes/surveys/Survey.tsx
+++ frontend/src/scenes/surveys/Survey.tsx
@@ -28,7 +28,6 @@ export const scene: SceneExport = {
 
 export function SurveyComponent({ id }: { id?: string } = {}): JSX.Element {
     const { isEditingSurvey, surveyMissing } = useValues(surveyLogic)
-    const showSurveyForm = id === 'new' || isEditingSurvey
 
     if (surveyMissing) {
         return <NotFound object="survey" />
@@ -40,7 +39,7 @@ export function SurveyComponent({ id }: { id?: string } = {}): JSX.Element {
                 <LemonSkeleton />
             ) : (
                 <BindLogic logic={surveyLogic} props={{ id }}>
-                    {showSurveyForm ? <SurveyForm id={id} /> : <SurveyView id={id} />}
+                    {isEditingSurvey ? <SurveyForm id={id} /> : <SurveyView id={id} />}
                 </BindLogic>
             )}
         </div>
@@ -51,6 +50,19 @@ export function SurveyForm({ id }: { id: string }): JSX.Element {
     const { survey, surveyLoading, isEditingSurvey, targetingFlagFilters } = useValues(surveyLogic)
     const { loadSurvey, editingSurvey } = useActions(surveyLogic)
 
+    const handleCancelClick = (): void => {
+        if (isEditingSurvey) {
+            editingSurvey(false)
+            if (id === 'new') {
+                router.actions.push(urls.surveys())
+            } else {
+                loadSurvey()
+            }
+        } else {
+            router.actions.push(urls.surveys())
+        }
+    }
+
     return (
         <Form id="survey" formKey="survey" logic={surveyLogic} props={{ id }} className="space-y-4" enableFormOnSubmit>
             <PageHeader
@@ -60,14 +72,7 @@ export function SurveyForm({ id }: { id: string }): JSX.Element {
                             data-attr="cancel-survey"
                             type="secondary"
                             loading={surveyLoading}
-                            onClick={() => {
-                                if (isEditingSurvey) {
-                                    editingSurvey(false)
-                                    loadSurvey()
-                                } else {
-                                    router.actions.push(urls.surveys())
-                                }
-                            }}
+                            onClick={handleCancelClick}
                         >
                             Cancel
                         </LemonButton>
@@ -88,26 +93,6 @@ export function SurveyForm({ id }: { id: string }): JSX.Element {
             <LemonDivider />
             <SurveyDisplaySummary id={id} survey={survey} targetingFlagFilters={targetingFlagFilters} />
             <LemonDivider />
-            <div className="flex items-center gap-2 justify-end">
-                <LemonButton
-                    data-attr="cancel-survey"
-                    type="secondary"
-                    loading={surveyLoading}
-                    onClick={() => {
-                        if (isEditingSurvey) {
-                            editingSurvey(false)
-                            loadSurvey()
-                        } else {
-                            router.actions.push(urls.surveys())
-                        }
-                    }}
-                >
-                    Cancel
-                </LemonButton>
-                <LemonButton type="primary" data-attr="save-survey" htmlType="submit" loading={surveyLoading}>
-                    {id === 'new' ? 'Save as draft' : 'Save'}
-                </LemonButton>
-            </div>
         </Form>
     )
 }
--- frontend/src/scenes/surveys/surveyLogic.tsx
+++ frontend/src/scenes/surveys/surveyLogic.tsx
@@ -756,6 +756,7 @@ export const surveyLogic = kea<surveyLogicType>([
                 urlMatchType: values.urlMatchTypeValidationError,
             }),
             submit: (surveyPayload) => {
+                actions.editingSurvey(false)
                 if (props.id && props.id !== 'new') {
                     actions.updateSurvey(surveyPayload)
                 } else {
@@ -770,6 +771,9 @@ export const surveyLogic = kea<surveyLogicType>([
             // This avoids resetting form fields if you click back/forward.
             if (method === 'PUSH') {
                 if (props.id) {
+                    if (props.id === 'new') {
+                        actions.editingSurvey(true)
+                    }
                     actions.loadSurvey()
                 } else {
                     actions.resetSurvey()
