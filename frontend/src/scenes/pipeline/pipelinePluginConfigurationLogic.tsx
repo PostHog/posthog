230647649aad7c2bd7878066e9f35617ad340451
Ben White <ben@posthog.com>
2024-05-29 11:19:50 +0100

    Fix

--- frontend/src/scenes/pipeline/pipelinePluginConfigurationLogic.tsx
+++ frontend/src/scenes/pipeline/pipelinePluginConfigurationLogic.tsx
@@ -60,7 +60,10 @@ function getDefaultConfiguration(plugin: PluginType): Record<string, any> {
     }
 }
 
-function sanitizeFilters(filters: FilterType): PluginConfigTypeNew['filters'] {
+function sanitizeFilters(filters?: FilterType): PluginConfigTypeNew['filters'] {
+    if (!filters) {
+        return null
+    }
     const sanitized: PluginConfigFilters = {
         events: filters.events?.map((f) => ({
             id: f.id,
