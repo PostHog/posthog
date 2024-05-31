dbb4d7a98cb327e19cc308c3ed7cda8a418e4322
Paul D'Ambra <paul@posthog.com>
2024-05-31 17:10:28 +0100

    show sites

--- frontend/src/scenes/sites/Sites.tsx
+++ frontend/src/scenes/sites/Sites.tsx
@@ -4,15 +4,16 @@ import { Spinner } from 'lib/lemon-ui/Spinner'
 import { sitesLogic } from 'scenes/sites/sitesLogic'
 
 export const Sites = (): JSX.Element => {
-    const { sites } = useValues(sitesLogic)
+    const { siteStats, siteStatsLoading } = useValues(sitesLogic)
     return (
         <div className="flex flex-row flex-wrap py-2 px-4 gap-4">
-            {sites.map((s) => (
-                <div key={s} className="border rounded px-2 py-1 flex flex-col gap2 items-center min-h-40">
-                    <h2 className="m-0">{s}</h2>
+            {siteStatsLoading && <Spinner />}
+            {siteStats.map((s) => (
+                <div key={s.site} className="border rounded px-2 py-1 flex flex-col gap2 items-center min-h-40">
+                    <h2 className="m-0">{s.site}</h2>
                     <LemonDivider dashed={true} />
                     <div className="flex-1">
-                        <Spinner />
+                        <pre>{JSON.stringify(s, null, 2)}</pre>
                     </div>
                 </div>
             ))}
--- frontend/src/scenes/sites/sitesLogic.ts
+++ frontend/src/scenes/sites/sitesLogic.ts
@@ -1,25 +1,64 @@
-import { connect, kea, path, selectors } from 'kea'
-import { authorizedUrlListLogic, AuthorizedUrlListType } from 'lib/components/AuthorizedUrlList/authorizedUrlListLogic'
+import { afterMount, kea, path } from 'kea'
+import { loaders } from 'kea-loaders'
+import api from 'lib/api'
+
+import { HogQLQuery, NodeKind } from '~/queries/schema'
+import { hogql } from '~/queries/utils'
 
 import type { sitesLogicType } from './sitesLogicType'
 
+export type SiteStat = {
+    site: string
+    fcp_p90: number | null
+    lcp_p90: number | null
+    inp_p90: number | null
+    cls_p90: number | null
+}
+
 export const sitesLogic = kea<sitesLogicType>([
     path(['scenes', 'sites', 'sitesLogic']),
-    connect({
-        values: [
-            authorizedUrlListLogic({ actionId: null, type: AuthorizedUrlListType.TOOLBAR_URLS }),
-            ['authorizedUrls'],
-        ],
-    }),
-    selectors({
-        sites: [
-            (s) => [s.authorizedUrls],
-            (authorizedUrls) => {
-                return authorizedUrls.map((au) => {
-                    const u = new URL(au)
-                    return u.host
-                })
+    loaders({
+        siteStats: {
+            __default: [] as SiteStat[],
+            loadSiteStats: async () => {
+                const query: HogQLQuery = {
+                    kind: NodeKind.HogQLQuery,
+                    query: hogql`select 
+                                        -- domainRFC doesn't work for valid domains with no dots e.g. localhost
+                                        coalesce(
+                                                nullIf(domainRFC(current_url), ''),
+                                                extract(current_url, 'https?://([^/]+)')
+                                        ) AS domain
+    ,round(quantile(0.9)(fcp) as fcp_p90, 2)
+    ,round(quantile(0.9)(lcp) as lcp_p90, 2)
+    ,round(quantile(0.9)(inp) as inp_p90, 2)
+    ,round(quantile(0.9)(cls) as cls_p90, 2)
+                                 from web_vitals
+                                 where timestamp >=now() - interval 30 day
+                                   and timestamp <=now()
+                                 group by domain
+                                 order by fcp_p90 desc
+                                     limit 100`,
+                }
+
+                const response = await api.query(query)
+                const result = response.results as [string, number, number, number, number][]
+
+                if (result && result.length === 0) {
+                    return []
+                }
+
+                return result.map((r) => ({
+                    site: r[0],
+                    fcp_p90: r[1],
+                    lcp_p90: r[2],
+                    inp_p90: r[3],
+                    cls_p90: r[4],
+                }))
             },
-        ],
+        },
+    }),
+    afterMount(({ actions }) => {
+        actions.loadSiteStats()
     }),
 ])
