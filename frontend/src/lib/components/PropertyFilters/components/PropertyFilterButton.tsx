816d7a090d1a24d7ccca09bf0d6f3de9c190ce7e
David Newell <david@posthog.com>
2024-06-03 11:00:01 +0100

    allow custom button content

--- frontend/src/lib/components/PropertyFilters/components/PropertyFilterButton.tsx
+++ frontend/src/lib/components/PropertyFilters/components/PropertyFilterButton.tsx
@@ -17,7 +17,7 @@ import { formatPropertyLabel } from '../utils'
 export interface PropertyFilterButtonProps {
     onClick?: () => void
     onClose?: () => void
-    children?: string
+    children?: React.ReactNode
     item: AnyPropertyFilter
     disabledReason?: string
 }
@@ -29,9 +29,11 @@ export const PropertyFilterButton = React.forwardRef<HTMLElement, PropertyFilter
 
         const closable = onClose !== undefined
         const clickable = onClick !== undefined
-        const label =
-            children ||
-            formatPropertyLabel(item, cohortsById, (s) => formatPropertyValueForDisplay(item.key, s)?.toString() || '?')
+        const label = formatPropertyLabel(
+            item,
+            cohortsById,
+            (s) => formatPropertyValueForDisplay(item.key, s)?.toString() || '?'
+        )
 
         const ButtonComponent = clickable ? 'button' : 'div'
 
@@ -47,9 +49,11 @@ export const PropertyFilterButton = React.forwardRef<HTMLElement, PropertyFilter
                 aria-disabled={!!disabledReason}
             >
                 <PropertyFilterIcon type={item.type} />
-                <span className="PropertyFilterButton-content" title={label}>
-                    {midEllipsis(label, 32)}
-                </span>
+                {children || (
+                    <span className="PropertyFilterButton-content" title={label}>
+                        {midEllipsis(label, 32)}
+                    </span>
+                )}
                 {closable && !disabledReason && (
                     // The context below prevents close button from going into active status when filter popover is open
                     <PopoverReferenceContext.Provider value={null}>
