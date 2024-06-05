ad41693466da87be52d986a38005be82df572328
Zach Waterfield <zlwaterfield@gmail.com>
2024-06-05 11:20:01 -0400

    /activation -> /activate

--- ee/api/billing.py
+++ ee/api/billing.py
@@ -87,7 +87,7 @@ class BillingViewset(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
 
         return self.list(request, *args, **kwargs)
 
-    class ActivationSerializer(serializers.Serializer):
+    class ActivateSerializer(serializers.Serializer):
         plan = serializers.CharField(required=False)
         products = serializers.CharField(
             required=False
@@ -110,11 +110,11 @@ class BillingViewset(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
             return data
 
     @action(methods=["GET"], detail=False)
-    def activation(self, request: Request, *args: Any, **kwargs: Any) -> HttpResponse:
+    def activate(self, request: Request, *args: Any, **kwargs: Any) -> HttpResponse:
         license = get_cached_instance_license()
         organization = self._get_org_required()
 
-        serializer = self.ActivationSerializer(data=request.GET)
+        serializer = self.ActivateSerializer(data=request.GET)
         serializer.is_valid(raise_exception=True)
 
         redirect_path = serializer.validated_data.get("redirect_path", "organization/billing")
@@ -122,7 +122,7 @@ class BillingViewset(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
             redirect_path = redirect_path[1:]
 
         redirect_uri = f"{settings.SITE_URL or request.headers.get('Host')}/{redirect_path}"
-        url = f"{BILLING_SERVICE_URL}/activation?redirect_uri={redirect_uri}&organization_name={organization.name}"
+        url = f"{BILLING_SERVICE_URL}/activate?redirect_uri={redirect_uri}&organization_name={organization.name}"
 
         products = serializer.validated_data.get("products")
         url = f"{url}&products={products}"
@@ -132,6 +132,11 @@ class BillingViewset(TeamAndOrgViewSetMixin, viewsets.GenericViewSet):
             url = f"{url}&token={billing_service_token}"
 
         return redirect(url)
+
+    # This is deprecated and should be removed in the future in favor of 'activate'
+    @action(methods=["GET"], detail=False)
+    def activation(self, request: Request, *args: Any, **kwargs: Any) -> HttpResponse:
+        return self.activate(request, *args, **kwargs)
 
     class DeactivateSerializer(serializers.Serializer):
         products = serializers.CharField()
