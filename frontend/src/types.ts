5f2498d2d3a918c5ff66c0faf9935acafd2a7b33
Zach Waterfield <zlwaterfield@gmail.com>
2024-06-06 13:22:43 -0400

    add subscription_level to billing type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1555,6 +1555,7 @@ export interface BillingProductV2AddonType {
 export interface BillingV2Type {
     customer_id: string
     has_active_subscription: boolean
+    subscription_level: 'free' | 'paid' | 'custom'
     free_trial_until?: Dayjs
     stripe_portal_url?: string
     deactivated?: boolean
