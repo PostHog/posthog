4f9ce1126e790e2da53ee4e760c927d9ddc10b9a
Paul D'Ambra <paul@posthog.com>
2024-06-06 19:44:13 +0100

    some metrics while we're here

--- ee/session_recordings/persistence_tasks.py
+++ ee/session_recordings/persistence_tasks.py
@@ -3,12 +3,20 @@ from datetime import timedelta
 import structlog
 from celery import shared_task
 from django.utils import timezone
+from prometheus_client import Counter
 
 from ee.session_recordings.session_recording_extensions import persist_recording
 from posthog.session_recordings.models.session_recording import SessionRecording
 from posthog.tasks.utils import CeleryQueue
 
 logger = structlog.get_logger(__name__)
+
+REPLAY_NEEDS_PERSISTENCE_COUNTER = Counter(
+    "snapshot_persist_persistence_task_queued",
+    "Count of session recordings that need to be persisted",
+    # we normally avoid team label but not all teams pin recordings so there shouldn't be _too_ many labels here
+    labelnames=["team_id"],
+)
 
 
 @shared_task(
@@ -30,4 +38,5 @@ def persist_finished_recordings() -> None:
     logger.info("Persisting finished recordings", count=finished_recordings.count())
 
     for recording in finished_recordings:
+        REPLAY_NEEDS_PERSISTENCE_COUNTER.labels(team_id=recording.team_id).inc()
         persist_single_recording.delay(recording.session_id, recording.team_id)
