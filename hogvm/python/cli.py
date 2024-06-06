036a15e4354a77bb11f2fd75462c5b4dacee99fa
Marius Andra <marius.andra@gmail.com>
2024-06-06 21:21:58 +0200

    flip arguments

--- hogvm/python/cli.py
+++ hogvm/python/cli.py
@@ -2,25 +2,22 @@ import sys
 import json
 from .execute import execute_bytecode
 
-# get modifiers
 modifiers = [arg for arg in sys.argv if arg.startswith("-")]
-
-# get filename from first cli arg
 args = [arg for arg in sys.argv if arg != "" and not arg.startswith("-")]
+if len(args) != 2:
+    raise ValueError("Must specify exactly one filename")
+
 filename = args[1]
 
 debug = "--debug" in modifiers
 
-# raise if filename does not end with ".hoge"
 if not filename.endswith(".hoge"):
     raise ValueError("filename must end with '.hoge'. Got: " + filename)
 
-# read file
 with open(filename) as file:
     code = file.read()
     code = json.loads(code)
 
-# execute code
 response = execute_bytecode(code, globals=None, timeout=5, team=None, debug=debug)
 for line in response.stdout:
     print(line)  # noqa: T201
--- posthog/hogql/cli.py
+++ posthog/hogql/cli.py
@@ -1,34 +1,54 @@
-# HogE, the "HOG compilEr", is a tool that compiles ".hog" programs into ".hoge" bytecode.
-
 import sys
 import json
+
+from hogvm.python.execute import execute_bytecode
 from .bytecode import create_bytecode, parse_program
 
-# get filename from first cli arg
-filename = sys.argv[1]
+modifiers = [arg for arg in sys.argv if arg.startswith("-")]
+args = [arg for arg in sys.argv if arg != "" and not arg.startswith("-")]
+filename = args[1]
 
-# raise if filename does not end with ".hog"
-if not filename.endswith(".hog"):
-    raise ValueError("filename must end with '.hog'")
+if not filename.endswith(".hog") and not filename.endswith(".hoge"):
+    raise ValueError("Filename must end with '.hog' or '.hoge'")
 
-# read file
 with open(filename) as file:
     code = file.read()
 
-# execute code
-bytecode = create_bytecode(parse_program(code))
+if filename.endswith(".hog"):
+    bytecode = create_bytecode(parse_program(code))
+else:
+    bytecode = json.loads(code)
 
-# write bytecode to file
-with open(filename[:-4] + ".hoge", "w") as file:
-    max_length = 120
-    line = "["
-    for index, op in enumerate(bytecode):
-        encoded = json.dumps(op)
-        if len(line) + len(encoded) > max_length - 2:
+if "--run" in modifiers:
+    if len(args) != 2:
+        raise ValueError("Must specify exactly one filename")
+
+    response = execute_bytecode(bytecode, globals=None, timeout=5, team=None, debug="--debug" in modifiers)
+    for line in response.stdout:
+        print(line)  # noqa: T201
+
+elif "--compile" in modifiers:
+    if len(args) == 3:
+        target = args[2]
+    else:
+        target = filename[:-4] + ".hoge"
+        if len(args) != 2:
+            raise ValueError("Must specify exactly one filename")
+
+    # write bytecode to file
+    with open(target, "w") as file:
+        max_length = 120
+        line = "["
+        for index, op in enumerate(bytecode):
+            encoded = json.dumps(op)
+            if len(line) + len(encoded) > max_length - 2:
+                file.write(line + "\n")
+                line = ""
+            line += (" " if len(line) > 1 else "") + encoded + ("]" if index == len(bytecode) - 1 else ",")
+        if line == "[":
+            file.write(line + "]\n")
+        elif line != "":
             file.write(line + "\n")
-            line = ""
-        line += (" " if len(line) > 1 else "") + encoded + ("]" if index == len(bytecode) - 1 else ",")
-    if line == "[":
-        file.write(line + "]\n")
-    elif line != "":
-        file.write(line + "\n")
+
+else:
+    raise ValueError("Must specify either --run or --compile")
