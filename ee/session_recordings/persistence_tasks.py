6657623825c04dfa1a77b939031bf1464577c60a
Paul D'Ambra <paul@posthog.com>
2024-06-06 19:36:36 +0100

    feat: move session replay pinned persistence to its own queue

--- ee/session_recordings/persistence_tasks.py
+++ ee/session_recordings/persistence_tasks.py
@@ -6,16 +6,23 @@ from django.utils import timezone
 
 from ee.session_recordings.session_recording_extensions import persist_recording
 from posthog.session_recordings.models.session_recording import SessionRecording
+from posthog.tasks.utils import CeleryQueue
 
 logger = structlog.get_logger(__name__)
 
 
-@shared_task(ignore_result=True)
+@shared_task(
+    ignore_result=True,
+    queue=CeleryQueue.SESSION_REPLAY_PERSISTENCE.value,
+)
 def persist_single_recording(id: str, team_id: int) -> None:
     persist_recording(id, team_id)
 
 
-@shared_task(ignore_result=True)
+@shared_task(
+    ignore_result=True,
+    queue=CeleryQueue.SESSION_REPLAY_PERSISTENCE.value,
+)
 def persist_finished_recordings() -> None:
     one_day_old = timezone.now() - timedelta(hours=24)
     finished_recordings = SessionRecording.objects.filter(created_at__lte=one_day_old, object_storage_path=None)
