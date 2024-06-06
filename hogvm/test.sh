86b2bca437d07684da3c3838d18f7786f2bbc6cc
Marius Andra <marius.andra@gmail.com>
2024-06-06 16:04:33 +0200

    hoge tests

--- hogvm/test.sh
+++ hogvm/test.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+set -e
 
 cd ..
 
 for file in hogvm/__tests__/*.hog; do
     echo "Testing $file"
-    # get the full file path without the .hog extension
-    basename="${file%.hog}"
 
-    ./bin/hog $file
+    # from hogvm/__tests__/*.hog get hogvm/__tests__/snapshots/*
+    basename="${file%.hog}"
+    basename="${basename##*/}"
+    basename="hogvm/__tests__/snapshots/$basename"
+
+    ./bin/hog $file > $basename.hoge
+    mv "$file"e $basename.hoge
     ./bin/hoge --nodejs $basename.hoge > $basename.stdout.nodejs
     ./bin/hoge --python $basename.hoge > $basename.stdout.python
+    set +e
     diff $basename.stdout.nodejs $basename.stdout.python
     if [ $? -eq 0 ]; then
         mv $basename.stdout.nodejs $basename.stdout
@@ -18,4 +24,5 @@ for file in hogvm/__tests__/*.hog; do
         echo "Test failed"
         rm $basename.stdout.nodejs $basename.stdout.python
     fi
+    set -e
 done
