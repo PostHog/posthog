7737676cb0d3761f14ebdd929ba626d99536f2a3
Ben White <ben@posthog.com>
2024-06-07 13:19:39 +0100

    Added examples

--- posthog/api/test/test_hog_function.py
+++ posthog/api/test/test_hog_function.py
@@ -1,8 +1,55 @@
+import json
 from unittest.mock import ANY
 
 from rest_framework import status
 
 from posthog.test.base import APIBaseTest, ClickhouseTestMixin, QueryMatchingTest
+
+
+EXAMPLE_FULL = {
+    "name": "HogHook",
+    "hog": "fetch(inputs.url, {\n  'headers': inputs.headers,\n  'body': inputs.payload,\n  'method': inputs.method\n});",
+    "inputs_schema": [
+        {"key": "url", "type": "string", "label": "Webhook URL", "required": True},
+        {"key": "payload", "type": "json", "label": "JSON Payload", "required": True},
+        {
+            "key": "method",
+            "type": "choice",
+            "label": "HTTP Method",
+            "choices": [
+                {"label": "POST", "value": "POST"},
+                {"label": "PUT", "value": "PUT"},
+                {"label": "PATCH", "value": "PATCH"},
+                {"label": "GET", "value": "GET"},
+            ],
+            "required": True,
+        },
+        {"key": "headers", "type": "dictionary", "label": "Headers", "required": False},
+    ],
+    "inputs": {
+        "url": {
+            "value": "http://localhost:2080/0e02d917-563f-4050-9725-aad881b69937",
+        },
+        "method": {"value": "POST"},
+        "headers": {
+            "value": {"version": "v={event.properties.$lib_version}"},
+        },
+        "payload": {
+            "value": {
+                "event": "{event}",
+                "groups": "{groups}",
+                "nested": {"foo": "{event.url}"},
+                "person": "{person}",
+                "event_url": "{f'{event.url}-test'}",
+            },
+        },
+    },
+    "filters": {
+        "events": [{"id": "$pageview", "name": "$pageview", "type": "events", "order": 0}],
+        "actions": [{"id": "9", "name": "Test Action", "type": "actions", "order": 1}],
+        "filter_test_accounts": True,
+    },
+}
 
 
 class TestHogFunctionAPI(ClickhouseTestMixin, APIBaseTest, QueryMatchingTest):
@@ -79,22 +126,102 @@ class TestHogFunctionAPI(ClickhouseTestMixin, APIBaseTest, QueryMatchingTest):
             }, f"Did not get error for {key}, got {res.json()}"
             assert res.status_code == status.HTTP_400_BAD_REQUEST, res.json()
 
-    # def test_create_action_generates_bytecode(self):
-    #     response = self.client.post(
-    #         f"/api/projects/{self.team.id}/actions/",
-    #         data={
-    #             "name": "user signed up",
-    #             "steps": [
-    #                 {
-    #                     "text": "sign up",
-    #                     "selector": "div > button",
-    #                     "url": "/signup",
-    #                 }
-    #             ],
-    #             "description": "Test description",
-    #         },
-    #         HTTP_ORIGIN="http://testserver",
-    #     )
-    #     assert response.status_code == status.HTTP_201_CREATED, response.json()
-    #     action = Action.objects.get(pk=response.json()["id"])
-    #     assert action.bytecode == ["_h", 32, "%/signup%", 32, "$current_url", 32, "properties", 1, 2, 17]
+    def test_generates_hog_bytecode(self):
+        response = self.client.post(
+            f"/api/projects/{self.team.id}/hog_functions/",
+            data={
+                "name": "Fetch URL",
+                "hog": "let i := 0;\nwhile(i < 3) {\n  i := i + 1;\n  fetch(inputs.url, {\n    'headers': {\n      'x-count': f'{i}'\n    },\n    'body': inputs.payload,\n    'method': inputs.method\n  });\n}",
+            },
+        )
+        # JSON loads for one line comparison
+        assert response.json()["bytecode"] == json.loads(
+            '["_h", 33, 0, 33, 3, 36, 0, 15, 40, 45, 33, 1, 36, 0, 6, 37, 0, 32, "headers", 32, "x-count", 36, 0, 42, 1, 32, "body", 32, "payload", 32, "inputs", 1, 2, 32, "method", 32, "method", 32, "inputs", 1, 2, 42, 3, 32, "url", 32, "inputs", 1, 2, 2, "fetch", 2, 35, 39, -52, 35]'
+        ), response.json()
+
+    def test_generates_inputs_bytecode(self):
+        response = self.client.post(f"/api/projects/{self.team.id}/hog_functions/", data=EXAMPLE_FULL)
+        assert response.status_code == status.HTTP_201_CREATED, response.json()
+        assert response.json()["inputs"] == {
+            "url": {
+                "value": "http://localhost:2080/0e02d917-563f-4050-9725-aad881b69937",
+                "bytecode": ["_h", 32, "http://localhost:2080/0e02d917-563f-4050-9725-aad881b69937"],
+            },
+            "payload": {
+                "value": {
+                    "event": "{event}",
+                    "groups": "{groups}",
+                    "nested": {"foo": "{event.url}"},
+                    "person": "{person}",
+                    "event_url": "{f'{event.url}-test'}",
+                },
+                "bytecode": {
+                    "event": ["_h", 32, "event", 1, 1],
+                    "groups": ["_h", 32, "groups", 1, 1],
+                    "nested": {"foo": ["_h", 32, "url", 32, "event", 1, 2]},
+                    "person": ["_h", 32, "person", 1, 1],
+                    "event_url": ["_h", 32, "-test", 32, "url", 32, "event", 1, 2, 2, "concat", 2],
+                },
+            },
+            "method": {"value": "POST"},
+            "headers": {
+                "value": {"version": "v={event.properties.$lib_version}"},
+                "bytecode": {
+                    "version": ["_h", 32, "$lib_version", 32, "properties", 32, "event", 1, 3, 32, "v=", 2, "concat", 2]
+                },
+            },
+        }
+
+    def test_generates_filters_bytecode(self):
+        self.team.test_account_filters = [
+            {
+                "key": "email",
+                "value": "@posthog.com",
+                "operator": "not_icontains",
+                "type": "person",
+            }
+        ]
+        self.team.save()
+        response = self.client.post(f"/api/projects/{self.team.id}/hog_functions/", data=EXAMPLE_FULL)
+        assert response.status_code == status.HTTP_201_CREATED, response.json()
+        assert response.json()["filters"] == {
+            "events": [{"id": "$pageview", "name": "$pageview", "type": "events", "order": 0}],
+            "actions": [{"id": "9", "name": "Test Action", "type": "actions", "order": 1}],
+            "filter_test_accounts": True,
+            "bytecode": [
+                "_h",
+                33,
+                2,
+                33,
+                1,
+                11,
+                32,
+                "%@posthog.com%",
+                32,
+                "email",
+                32,
+                "properties",
+                32,
+                "person",
+                1,
+                3,
+                20,
+                3,
+                2,
+                32,
+                "%@posthog.com%",
+                32,
+                "email",
+                32,
+                "properties",
+                32,
+                "person",
+                1,
+                3,
+                20,
+                3,
+                1,
+                4,
+                2,
+            ],
+        }
