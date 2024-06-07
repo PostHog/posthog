3c9700b3b9294e9e08b8b178445e3db80d8256de
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-06-07 16:07:07 +0200

    fix: Move import to method

--- posthog/api/app_metrics.py
+++ posthog/api/app_metrics.py
@@ -7,7 +7,6 @@ from rest_framework import mixins, request, response, viewsets
 from rest_framework.decorators import action
 
 from posthog.api.routing import TeamAndOrgViewSetMixin
-from posthog.batch_exports.models import fetch_batch_export_run_count
 from posthog.models import BatchExportRun
 from posthog.models.plugin import PluginConfig
 from posthog.queries.app_metrics.app_metrics import (
@@ -85,6 +84,8 @@ class AppMetricsViewSet(TeamAndOrgViewSetMixin, mixins.RetrieveModelMixin, views
         Raises:
             ValueError: If provided 'batch_export_id' is not a valid UUID.
         """
+        from posthog.batch_exports.models import fetch_batch_export_run_count
+
         batch_export_uuid = uuid.UUID(batch_export_id)
 
         after = self.request.GET.get("date_from", "-30d")
