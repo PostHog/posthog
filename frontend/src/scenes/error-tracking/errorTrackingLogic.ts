58a646081f46aeb46468c08ec85ffc93cdba0cc6
David Newell <david@posthog.com>
2024-06-11 10:44:52 +0100

    fix lint error

--- frontend/src/scenes/error-tracking/errorTrackingLogic.ts
+++
@@ -1,8 +0,0 @@
-import { kea, path } from 'kea'
-
-import type { errorTrackingSceneLogicType } from './errorTrackingLogicType'
-import type { errorTrackingSceneLogicType } from './errorTrackingSceneLogicType'
-
-export const errorTrackingSceneLogic = kea<errorTrackingSceneLogicType>([
-    path(['scenes', 'error-tracking', 'errorTrackingSceneLogic']),
-])
