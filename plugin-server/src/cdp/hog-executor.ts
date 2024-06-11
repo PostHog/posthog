58a26953e847b888727b138f67ebc833317ff982
Ben White <ben@posthog.com>
2024-06-11 13:41:47 +0200

    Swap to debug default

--- plugin-server/src/cdp/hog-executor.ts
+++ plugin-server/src/cdp/hog-executor.ts
@@ -177,9 +177,9 @@ export class HogExecutor {
         }
 
         if (!state) {
-            log('info', `Executing function  (${hogFunction.id}) (${hogFunction.name})`)
+            log('debug', `Executing function  (${hogFunction.id}) (${hogFunction.name})`)
         } else {
-            log('info', `Resuming function  (${hogFunction.id}) (${hogFunction.name})`)
+            log('debug', `Resuming function  (${hogFunction.id}) (${hogFunction.name})`)
         }
 
         try {
@@ -202,7 +202,7 @@ export class HogExecutor {
             })
 
             if (!res.finished) {
-                log('info', `Suspending function (${hogFunction.id}) (${hogFunction.name})`)
+                log('debug', `Suspending function (${hogFunction.id}) (${hogFunction.name})`)
                 status.info('🦔', `[HogExecutor] Function returned not finished. Executing async function`, {
                     ...loggingContext,
                     asyncFunctionName: res.asyncFunctionName,
@@ -220,7 +220,7 @@ export class HogExecutor {
                     // TODO: Log error somewhere
                 }
             } else {
-                log('info', `Function completed (${hogFunction.id}) (${hogFunction.name})!`)
+                log('debug', `Function completed (${hogFunction.id}) (${hogFunction.name})!`)
             }
         } catch (err) {
             error = err
