350b859e143fb0e04b091d535fb6812cf5391574
Julian Bez <julian@posthog.com>
2024-06-12 11:53:21 +0200

    Add reject_on_worker_lost for async queries

--- posthog/tasks/tasks.py
+++ posthog/tasks/tasks.py
@@ -47,6 +47,7 @@ def redis_heartbeat() -> None:
     retry_backoff_max=10,
     max_retries=3,
     expires=60 * 10,  # Do not run queries that got stuck for more than this
+    reject_on_worker_lost=True,
 )
 @limit_concurrency(90)  # Do not go above what CH can handle (max_concurrent_queries)
 @limit_concurrency(
