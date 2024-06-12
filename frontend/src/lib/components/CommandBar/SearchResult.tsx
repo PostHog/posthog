3795ef376b73ad6e0329f6acd594bd98f50cb97a
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-06-12 16:33:53 +0200

    query based insight summary

--- frontend/src/lib/components/CommandBar/SearchResult.tsx
+++ frontend/src/lib/components/CommandBar/SearchResult.tsx
@@ -10,8 +10,7 @@ import { mathsLogic } from 'scenes/trends/mathsLogic'
 
 import { cohortsModel } from '~/models/cohortsModel'
 import { groupsModel } from '~/models/groupsModel'
-import { Node } from '~/queries/schema'
-import { FilterType } from '~/types'
+import { getQueryFromInsightLike } from '~/queries/nodes/InsightViz/utils'
 
 import { tabToName } from './constants'
 import { searchBarLogic } from './searchBarLogic'
@@ -102,11 +101,12 @@ export const ResultName = ({ result }: ResultNameProps): JSX.Element | null => {
 
     const { type, extra_fields } = result
     if (type === 'insight') {
+        const query = getQueryFromInsightLike(extra_fields)
         return extra_fields.name ? (
             <span>{extra_fields.name}</span>
         ) : (
             <i>
-                {summarizeInsight(extra_fields.query as Node | null, extra_fields.filters as Partial<FilterType>, {
+                {summarizeInsight(query, null, {
                     aggregationLabel,
                     cohortsById,
                     mathDefinitions,
@@ -119,9 +119,8 @@ export const ResultName = ({ result }: ResultNameProps): JSX.Element | null => {
         return <span>{extra_fields.title}</span>
     } else if (type === 'group') {
         return <span>{groupDisplayId(extra_fields.group_key, extra_fields.group_properties)}</span>
-    } else {
-        return <span>{extra_fields.name}</span>
     }
+    return <span>{extra_fields.name}</span>
 }
 
 export const ResultDescription = ({ result }: ResultNameProps): JSX.Element | null => {
@@ -141,7 +140,6 @@ export const ResultDescription = ({ result }: ResultNameProps): JSX.Element | nu
                 initialContent={extra_fields.content as JSONContent}
             />
         )
-    } else {
-        return 'description' in extra_fields ? <span>{extra_fields.description}</span> : <i>No description.</i>
     }
+    return 'description' in extra_fields ? <span>{extra_fields.description}</span> : <i>No description.</i>
 }
