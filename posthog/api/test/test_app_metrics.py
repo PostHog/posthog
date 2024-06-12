b2382e2e237b593b0ea3419f934c10405ac2a036
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-06-12 11:33:45 +0200

    fix: Actually use include and exclude events

--- posthog/api/test/test_app_metrics.py
+++ posthog/api/test/test_app_metrics.py
@@ -152,6 +152,10 @@ class TestAppMetricsAPI(ClickhouseTestMixin, APIBaseTest):
                     for _ in range(3):
                         insert_event(team_id=self.team.pk, timestamp=last_updated_at - dt.timedelta(minutes=1))
 
+                    insert_event(
+                        team_id=self.team.pk, timestamp=last_updated_at - dt.timedelta(minutes=1), event="not-included"
+                    )
+
                     BatchExportRun.objects.create(
                         batch_export_id=batch_export_id,
                         data_interval_end=last_updated_at - dt.timedelta(hours=2),
@@ -236,6 +240,9 @@ class TestAppMetricsAPI(ClickhouseTestMixin, APIBaseTest):
                         status=BatchExportRun.Status.COMPLETED,
                     )
                     insert_event(team_id=self.team.pk, timestamp=last_updated_at - dt.timedelta(minutes=1))
+                    insert_event(
+                        team_id=self.team.pk, timestamp=last_updated_at - dt.timedelta(minutes=1), event="exclude-me"
+                    )
 
             response = self.client.get(f"/api/projects/@current/app_metrics/{batch_export_id}?date_from=-7d")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
