909770743bd3cad01893b8f9ac9884e545ff1c71
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-06-12 15:37:02 +0200

    feat: Frontend support for model field

--- frontend/src/queries/schema.ts
+++ frontend/src/queries/schema.ts
@@ -1456,7 +1456,6 @@ export interface DatabaseSchemaPostHogTable extends DatabaseSchemaTableCommon {
 
 export interface DatabaseSchemaBatchExportTable extends DatabaseSchemaTableCommon {
     type: 'batch_export'
-    query: HogQLQuery
 }
 
 export interface DatabaseSchemaDataWarehouseTable extends DatabaseSchemaTableCommon {
