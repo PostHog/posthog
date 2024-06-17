e242532e4d41c080f81e357085378eb34c826678
Phani Raj <phani@posthog.com>
2024-06-17 12:57:36 -0500

    Add events properties to survey edited too

--- frontend/src/lib/utils/eventUsageLogic.ts
+++ frontend/src/lib/utils/eventUsageLogic.ts
@@ -1276,11 +1276,21 @@ export const eventUsageLogic = kea<eventUsageLogicType>([
             })
         },
         reportSurveyEdited: ({ survey }) => {
+            const questionsWithShuffledOptions = survey.questions.filter((question) => {
+                return question.hasOwnProperty('shuffleOptions') && (question as MultipleSurveyQuestion).shuffleOptions
+            })
+
             posthog.capture('survey edited', {
                 name: survey.name,
                 id: survey.id,
                 created_at: survey.created_at,
                 start_date: survey.start_date,
+                events_count: survey.conditions?.events?.values.length,
+                recurring_survey_iteration_count: survey.iteration_count == undefined ? 0 : survey.iteration_count,
+                recurring_survey_iteration_interval:
+                    survey.iteration_frequency_days == undefined ? 0 : survey.iteration_frequency_days,
+                shuffle_questions_enabled: !!survey.appearance.shuffleQuestions,
+                shuffle_question_options_enabled_count: questionsWithShuffledOptions.length,
             })
         },
         reportSurveyTemplateClicked: ({ template }) => {
