3e6131d959ea8c9f29324a7da3efd77d68841689
Ben White <ben@posthog.com>
2024-06-17 09:24:59 +0200

    Fix

--- posthog/cdp/templates/avo/test_template_avo.py
+++ posthog/cdp/templates/avo/test_template_avo.py
@@ -1,18 +1,12 @@
 from posthog.cdp.templates.helpers import BaseHogFunctionTemplateTest
-from posthog.cdp.templates.hubspot.template_hubspot import template as template_hubspot
+from posthog.cdp.templates.avo.template_avo import template as template_avo
 
 
-class TestTemplateHubspot(BaseHogFunctionTemplateTest):
-    template = template_hubspot
+class TestTemplateAvo(BaseHogFunctionTemplateTest):
+    template = template_avo
 
     def _inputs(self, **kwargs):
-        inputs = {
-            "access_token": "TOKEN",
-            "email": "example@posthog.com",
-            "properties": {
-                "company": "PostHog",
-            },
-        }
+        inputs = {"api_key": "1234", "environment": "dev"}
         inputs.update(kwargs)
         return inputs
 
@@ -34,51 +28,3 @@ class TestTemplateHubspot(BaseHogFunctionTemplateTest):
             )
         ]
         assert self.get_mock_print_calls() == [("Contact created successfully!",)]
-
-    def test_exits_if_no_email(self):
-        for email in [None, ""]:
-            self.mock_print.reset_mock()
-            res = self.run_function(inputs=self._inputs(email=email))
-
-            assert res.result is None
-            assert self.get_mock_fetch_calls() == []
-            assert self.get_mock_print_calls() == [("`email` input is empty. Not creating a contact.",)]
-
-    def test_handles_updates(self):
-        call_count = 0
-
-        # First call respond with 409, second one 200 and increment call_count
-        def mock_fetch(*args):
-            nonlocal call_count
-            call_count += 1
-            return (
-                {"status": 409, "body": {"message": "Contact already exists. Existing ID: 12345"}}
-                if call_count == 1
-                else {"status": 200, "body": {"status": "success"}}
-            )
-
-        self.mock_fetch_response = mock_fetch  # type: ignore
-
-        res = self.run_function(inputs=self._inputs())
-
-        assert res.result is None
-
-        assert len(self.get_mock_fetch_calls()) == 2
-
-        assert self.get_mock_fetch_calls()[0] == (
-            "https://api.hubapi.com/crm/v3/objects/contacts",
-            {
-                "method": "POST",
-                "headers": {"Authorization": "Bearer TOKEN", "Content-Type": "application/json"},
-                "body": {"properties": {"company": "PostHog", "email": "example@posthog.com"}},
-            },
-        )
-
-        assert self.get_mock_fetch_calls()[1] == (
-            "https://api.hubapi.com/crm/v3/objects/contacts/12345",
-            {
-                "method": "PATCH",
-                "headers": {"Authorization": "Bearer TOKEN", "Content-Type": "application/json"},
-                "body": {"properties": {"company": "PostHog", "email": "example@posthog.com"}},
-            },
-        )
