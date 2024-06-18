218b9fecba420d0608be9a6d903a985d01e020ef
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-06-18 16:48:50 +0200

    fix: Bump migration number

--- posthog/migrations/0430_batchexport_model.py
+++ posthog/migrations/0430_batchexport_model.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0425_hogfunction"),
+        ("posthog", "0429_alter_datawarehousetable_format"),
     ]
 
     operations = [
