dc9535f7c76989f9bd09c6d3083c26eb6f96bc7d
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-06-18 17:38:15 +0200

    fix: Clean-up unused function

--- posthog/api/test/test_app_metrics.py
+++ posthog/api/test/test_app_metrics.py
@@ -145,9 +145,6 @@ class TestAppMetricsAPI(ClickhouseTestMixin, APIBaseTest):
                         data_interval_start=last_updated_at - dt.timedelta(hours=3),
                         status=BatchExportRun.Status.FAILED_RETRYABLE,
                     )
-                    for _ in range(5):
-                        timestamp = last_updated_at - dt.timedelta(hours=2, minutes=1)
-                        insert_event(team_id=self.team.pk, timestamp=timestamp)
 
             response = self.client.get(f"/api/projects/@current/app_metrics/{batch_export_id}?date_from=-7d")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -216,7 +213,6 @@ class TestAppMetricsAPI(ClickhouseTestMixin, APIBaseTest):
                         data_interval_start=last_updated_at - dt.timedelta(hours=1),
                         status=BatchExportRun.Status.COMPLETED,
                     )
-                    insert_event(team_id=self.team.pk, timestamp=last_updated_at - dt.timedelta(minutes=1))
 
             response = self.client.get(f"/api/projects/@current/app_metrics/{batch_export_id}?date_from=-7d")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
