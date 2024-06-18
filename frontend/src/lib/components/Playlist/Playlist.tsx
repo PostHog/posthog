9955330dcf05d317bb664a2b426c84850231d983
David Newell <david@posthog.com>
2024-06-18 12:15:37 +0100

    rename classes

--- frontend/src/lib/components/Playlist/Playlist.tsx
+++ frontend/src/lib/components/Playlist/Playlist.tsx
@@ -114,7 +114,7 @@ export function Playlist({
                     onDoubleClick={() => setListCollapsed(!listCollapsed)}
                 />
             </div>
-            <div className="Playlist__player">{content({ activeItem })}</div>
+            <div className="Playlist__main">{content({ activeItem })}</div>
         </div>
     )
 }
