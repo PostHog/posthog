2d8aeccd96c47952222b6701c769b4d08444176a
Michael Matloka <michael@posthog.com>
2024-06-20 17:17:45 +0200

    feat(events): Expose "Open as new insight" button

--- frontend/src/scenes/insights/InsightNav/insightNavLogic.tsx
+++ frontend/src/scenes/insights/InsightNav/insightNavLogic.tsx
@@ -3,6 +3,7 @@ import { urlToAction } from 'kea-router'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { LemonTag } from 'lib/lemon-ui/LemonTag/LemonTag'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
+import { identifierToHuman } from 'lib/utils'
 import { insightDataLogic, queryFromKind } from 'scenes/insights/insightDataLogic'
 import { insightLogic } from 'scenes/insights/insightLogic'
 import { keyForInsightLogicProps } from 'scenes/insights/sharedUtils'
@@ -165,8 +166,8 @@ export const insightNavLogic = kea<insightNavLogicType>([
             },
         ],
         tabs: [
-            (s) => [s.activeView, s.featureFlags],
-            (activeView, featureFlags) => {
+            (s) => [s.activeView, s.query, s.featureFlags],
+            (activeView, query, featureFlags) => {
                 const tabs: Tab[] = [
                     {
                         label: 'Trends',
@@ -224,10 +225,14 @@ export const insightNavLogic = kea<insightNavLogicType>([
                     // only display this tab when it is selected by the provided insight query
                     // don't display it otherwise... humans shouldn't be able to click to select this tab
                     // it only opens when you click the <OpenEditorButton/>
+                    const humanFriendlyQueryKind: string | null =
+                        typeof query?.kind === 'string'
+                            ? identifierToHuman(query.kind.replace(/(Node|Query)$/g, ''), 'title')
+                            : null
                     tabs.push({
                         label: (
                             <>
-                                Custom{' '}
+                                {humanFriendlyQueryKind ?? 'Custom'}{' '}
                                 <LemonTag type="warning" className="uppercase ml-2">
                                     Beta
                                 </LemonTag>
