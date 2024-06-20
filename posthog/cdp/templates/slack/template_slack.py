7f210227b3f0af56cec4e3a90e9be0a4f58f14ed
Ben White <ben@posthog.com>
2024-06-20 11:13:26 +0200

    Fix up template

--- posthog/cdp/templates/slack/template_slack.py
+++ posthog/cdp/templates/slack/template_slack.py
@@ -9,10 +9,17 @@ template: HogFunctionTemplate = HogFunctionTemplate(
     description="Sends a webhook templated by the incoming event data",
     icon_url="/api/projects/@current/hog_functions/icon/?id=slack.com",
     hog="""
-fetch(inputs.url, {
-  'body': inputs.body,
+fetch("https://slack.com/api/chat.postMessage", {
+  'body': {
+    'channel': inputs.channel,
+    'icon_emoji': inputs.icon_emoji,
+    'username': inputs.username,
+    'blocks': inputs.blocks,
+    'text': inputs.text,
+  },
   'method': 'POST',
   'headers': {
+    'Authorization': f'Bearer {inputs.integration.access_token}',
     'Content-Type': 'application/json'
   }
 });
@@ -21,52 +28,53 @@ fetch(inputs.url, {
         {
             "key": "integration",
             "type": "integration",
+            "label": "Slack workspace",
             "secret": False,
             "required": True,
             "integration": "slack",
         },
         {
-            "key": "url",
+            "key": "channel",
             "type": "string",
-            "label": "Slack webhook URL",
-            "description": "Create a slack webhook URL in your (see https://api.slack.com/messaging/webhooks)",
-            "placeholder": "https://hooks.slack.com/services/XXX/YYY",
+            "label": "Channel to post to",
+            "description": "Select the channel to post to (e.g. #general). The PostHog app must be installed in the workspace.",
             "secret": False,
             "required": True,
         },
+        {"key": "icon_emoji", "type": "string", "label": "Emoji icon", "default": ":hedgehog:", "required": False},
+        {"key": "username", "type": "string", "label": "Bot name", "defaukt": "PostHog", "required": False},
         {
-            "key": "body",
+            "key": "blocks",
             "type": "json",
-            "label": "Message",
-            "description": "Message to send to Slack (see https://api.slack.com/block-kit/building)",
-            "default": {
-                "blocks": [
-                    {
-                        "text": {
-                            "text": "*{person.name}* triggered event: '{event.name}'",
-                            "type": "mrkdwn",
+            "label": "Blocks",
+            "description": "Blocks Slack (see https://api.slack.com/block-kit/building)",
+            "default": [
+                {
+                    "text": {
+                        "text": "*{person.name}* triggered event: '{event.name}'",
+                        "type": "mrkdwn",
+                    },
+                    "type": "section",
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "url": "{person.url}",
+                            "text": {"text": "View Person in PostHog", "type": "plain_text"},
+                            "type": "button",
                         },
-                        "type": "section",
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "url": "{person.url}",
-                                "text": {"text": "View Person in PostHog", "type": "plain_text"},
-                                "type": "button",
-                            },
-                            {
-                                "url": "{source.url}",
-                                "text": {"text": "Message source", "type": "plain_text"},
-                                "type": "button",
-                            },
-                        ],
-                    },
-                ]
-            },
+                        {
+                            "url": "{source.url}",
+                            "text": {"text": "Message source", "type": "plain_text"},
+                            "type": "button",
+                        },
+                    ],
+                },
+            ],
             "secret": False,
             "required": False,
         },
+        {"key": "text", "type": "string", "label": "Plain text message", "description": "Optional fallback message."},
     ],
 )
