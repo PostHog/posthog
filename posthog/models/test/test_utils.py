15920f7c219425fe214658f6d0642c4981a23cfc
Robbie Coomber <robbie.coomber@gmail.com>
2024-06-21 08:28:44 +0100

    Fix test_utils

--- posthog/models/test/test_utils.py
+++ posthog/models/test/test_utils.py
@@ -12,11 +12,11 @@ class TestUUIDv7(BaseTest):
     def test_can_be_deterministic(self):
         time_component = 1718800371653
         pnrg = Random(42)
-        uuid = uuid7(unix_ms_time=time_component, seeded_random=pnrg)
+        uuid = uuid7(unix_ms_time=time_component, random=pnrg)
         self.assertEqual(uuid, UUID("0190307c-4fc5-7a3b-8006-671a1c80317f"))
 
     def test_can_parse_date_string(self):
         time_component = "2024-06-19T13:33:37"
         pnrg = Random(42)
-        uuid = uuid7(unix_ms_time=time_component, seeded_random=pnrg)
+        uuid = uuid7(unix_ms_time=time_component, random=pnrg)
         self.assertEqual(uuid, UUID("019030b3-ef68-7a3b-8006-671a1c80317f"))
