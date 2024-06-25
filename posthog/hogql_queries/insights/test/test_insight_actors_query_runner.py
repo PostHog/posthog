a4a4a914ae313d6ebcc74411a64c2bf63c8a9868
Alexander Spicer <aspicer@gmail.com>
2024-06-25 11:01:04 -0700

    remove tests that check for query caceh

--- posthog/hogql_queries/insights/test/test_insight_actors_query_runner.py
+++ posthog/hogql_queries/insights/test/test_insight_actors_query_runner.py
@@ -239,7 +239,6 @@ class TestInsightActorsQueryRunner(ClickhouseTestMixin, APIBaseTest):
         self.assertEqual([("p2",)], response.results)
         assert "in(id," in queries[0]
         self.assertEqual(2, queries[0].count("toTimeZone(e.timestamp, 'US/Pacific') AS timestamp"))
-        self.assertEqual(2, queries[0].count("use_query_cache=1"))
 
     @snapshot_clickhouse_queries
     def test_insight_persons_trends_query_with_argmaxV2(self):
@@ -268,7 +267,6 @@ class TestInsightActorsQueryRunner(ClickhouseTestMixin, APIBaseTest):
         self.assertEqual([("p2",)], response.results)
         assert "in(person.id" in queries[0]
         self.assertEqual(2, queries[0].count("toTimeZone(e.timestamp, 'US/Pacific') AS timestamp"))
-        self.assertEqual(2, queries[0].count("use_query_cache=1"))
 
     @snapshot_clickhouse_queries
     def test_insight_persons_trends_groups_query(self):
