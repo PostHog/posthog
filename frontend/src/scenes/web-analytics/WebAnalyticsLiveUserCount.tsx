9c2e80483dcd3ba254d3551ccb6c97fe92596168
Robbie Coomber <robbie.coomber@gmail.com>
2024-06-25 16:32:35 +0100

    Add web analytics live online user count behind a feature flag

---
+++ frontend/src/scenes/web-analytics/WebAnalyticsLiveUserCount.tsx
@@ -0,0 +1,30 @@
+import { IconLive } from '@posthog/icons'
+import { useValues } from 'kea'
+import { Tooltip } from 'lib/lemon-ui/Tooltip'
+import { humanFriendlyLargeNumber, humanFriendlyNumber } from 'lib/utils'
+import { teamLogic } from 'scenes/teamLogic'
+import { liveEventsTableLogic } from 'scenes/web-analytics/liveWebAnalyticsLogic'
+
+export const WebAnalyticsLiveUserCount = (): JSX.Element | null => {
+    const { liveUserCount, liveUserUpdatedSecondsAgo } = useValues(liveEventsTableLogic)
+    const { currentTeam } = useValues(teamLogic)
+
+    if (liveUserCount == null || liveUserUpdatedSecondsAgo == null) {
+        return null
+    }
+
+    const inTeamString = currentTeam ? ` in ${currentTeam.name}` : ''
+    const tooltip = `${humanFriendlyNumber(
+        liveUserCount
+    )} users are online${inTeamString} (updated ${liveUserUpdatedSecondsAgo} seconds ago)`
+
+    return (
+        <div className="flex-row">
+            <Tooltip title={tooltip}>
+                <span>
+                    <IconLive /> {humanFriendlyLargeNumber(liveUserCount)} currently online
+                </span>
+            </Tooltip>
+        </div>
+    )
+}
--- frontend/src/scenes/web-analytics/liveWebAnalyticsLogic.tsx
+++ frontend/src/scenes/web-analytics/liveWebAnalyticsLogic.tsx
@@ -1,21 +1,58 @@
-import { actions, connect, events, kea, listeners, path, reducers } from 'kea'
+import { actions, connect, events, kea, listeners, path, reducers, selectors } from 'kea'
+import { FEATURE_FLAGS } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { liveEventsHostOrigin } from 'lib/utils/apiHost'
 import { teamLogic } from 'scenes/teamLogic'
 
+import type { liveEventsTableLogicType } from './liveWebAnalyticsLogicType'
+export interface StatsResponse {
+    users_on_product?: number
+}
 export const liveEventsTableLogic = kea<liveEventsTableLogicType>([
     path(['scenes', 'activity', 'live-events', 'liveEventsTableLogic']),
     connect({
-        values: [teamLogic, ['currentTeam']],
+        values: [teamLogic, ['currentTeam'], featureFlagLogic, ['featureFlags']],
     }),
     actions(() => ({
         pollStats: true,
-        setStats: (stats) => ({ stats }),
+        setStats: ({ stats, now }: { stats: StatsResponse; now: Date }) => ({ stats, now }),
+        setNow: ({ now }: { now: Date }) => ({ now }),
     })),
     reducers({
         stats: [
-            { users_on_product: null },
+            null as StatsResponse | null,
             {
                 setStats: (_, { stats }) => stats,
+            },
+        ],
+        statsUpdatedTime: [
+            null as Date | null,
+            {
+                setStats: (_, { now }) => now,
+            },
+        ],
+        now: [
+            null as Date | null,
+            {
+                setNow: (_, { now }) => now,
+                setStats: (_, { now }) => now,
+            },
+        ],
+    }),
+    selectors({
+        liveUserCount: [(s) => [s.stats], (stats) => (stats?.users_on_product ? stats.users_on_product : null)],
+        liveUserUpdatedSecondsAgo: [
+            (s) => [s.statsUpdatedTime, s.now],
+            (statsUpdatedTime, now) => {
+                if (!statsUpdatedTime || !now) {
+                    return null
+                }
+                const seconds = Math.ceil((now.getTime() - statsUpdatedTime.getTime()) / 1000)
+                if (seconds < 0 || seconds >= 300) {
+                    // this should only happen if we have a bug, but be defensive and don't show anything surprising if there is a bug
+                    return null
+                }
+                return seconds
             },
         ],
     }),
@@ -32,21 +69,31 @@ export const liveEventsTableLogic = kea<liveEventsTableLogicType>([
                     },
                 })
                 const data = await response.json()
-                actions.setStats(data)
+                actions.setStats({ stats: data, now: new Date() })
             } catch (error) {
                 console.error('Failed to poll stats:', error)
             }
         },
     })),
-    events(({ actions, cache }) => ({
+    events(({ actions, cache, values }) => ({
         afterMount: () => {
-            cache.statsInterval = setInterval(() => {
+            if (values.featureFlags[FEATURE_FLAGS.WEB_ANALYTICS_LIVE_USER_COUNT]) {
+                actions.setNow({ now: new Date() })
                 actions.pollStats()
-            }, 1500)
+
+                cache.statsInterval = setInterval(() => {
+                    actions.pollStats()
+                }, 30000)
+
+                cache.nowInterval = setInterval(() => {
+                    actions.setNow({ now: new Date() })
+                }, 500)
+            }
         },
         beforeUnmount: () => {
             if (cache.statsInterval) {
                 clearInterval(cache.statsInterval)
+                cache.statsInterval = null
             }
         },
     })),
