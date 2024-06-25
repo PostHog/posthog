5450f6a4ea85488cac58ab0b4dda3156abd1303f
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-06-25 11:22:11 +0200

    refactor(insights): query based `allEventNames` selector (#23202)

--- frontend/src/scenes/insights/filters/BreakdownFilter/TaxonomicBreakdownPopover.tsx
+++ frontend/src/scenes/insights/filters/BreakdownFilter/TaxonomicBreakdownPopover.tsx
@@ -3,6 +3,7 @@ import { TaxonomicFilter } from 'lib/components/TaxonomicFilter/TaxonomicFilter'
 import { TaxonomicFilterGroupType, TaxonomicFilterValue } from 'lib/components/TaxonomicFilter/types'
 import { Popover } from 'lib/lemon-ui/Popover/Popover'
 import { insightLogic } from 'scenes/insights/insightLogic'
+import { insightVizDataLogic } from 'scenes/insights/insightVizDataLogic'
 
 import { groupsModel } from '~/models/groupsModel'
 
@@ -15,7 +16,8 @@ type TaxonomicBreakdownPopoverProps = {
 }
 
 export const TaxonomicBreakdownPopover = ({ open, setOpen, children }: TaxonomicBreakdownPopoverProps): JSX.Element => {
-    const { allEventNames } = useValues(insightLogic)
+    const { insightProps } = useValues(insightLogic)
+    const { allEventNames } = useValues(insightVizDataLogic(insightProps))
     const { groupsTaxonomicTypes } = useValues(groupsModel)
     const { taxonomicBreakdownType, includeSessions } = useValues(taxonomicBreakdownFilterLogic)
 
