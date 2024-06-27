ef9e2dc199fa2b75627d144064dcb80f9ce11f6d
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-06-27 14:45:03 +0200

    longer timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
          * Maximum time expect() should wait for the condition to be met.
          * For example in `await expect(locator).toHaveText();`
          */
-        timeout: 5000,
+        timeout: 10000,
     },
     /* Run tests in files in parallel */
     fullyParallel: true,
