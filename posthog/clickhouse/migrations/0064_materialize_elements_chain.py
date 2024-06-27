7425c74ed4b5edacfaea9473cc83b60d73719f1f
timgl <tim@posthog.com>
2024-06-27 14:51:58 +0100

    fix

--- posthog/clickhouse/migrations/0064_materialize_elements_chain.py
+++ posthog/clickhouse/migrations/0064_materialize_elements_chain.py
@@ -6,10 +6,10 @@ from posthog.settings import CLICKHOUSE_CLUSTER
 
 ADD_COLUMNS_SHARDED_EVENTS = """
 ALTER TABLE {table} ON CLUSTER {cluster}
-ADD COLUMN IF NOT EXISTS mat_ec_href String DEFAULT extract(elements_chain, '(?::|\")href="(.*?)"'),
-ADD COLUMN IF NOT EXISTS mat_ec_texts Array(String) DEFAULT arrayDistinct(extractAll(elements_chain, '(?::|\")text="(.*?)"')),
-ADD COLUMN IF NOT EXISTS mat_ec_ids Array(String) DEFAULT arrayDistinct(extractAll(elements_chain, '(?::|\")id="(.*?)"')),
-ADD COLUMN IF NOT EXISTS mat_ec_elements Array(Enum('a', 'button', 'form', 'input', 'select', 'textarea', 'label')) DEFAULT arrayDistinct(extractAll(elements_chain, '(?:^|;)(a|button|form|input|select|textarea|label)(?:\\.|$|:)'))
+ADD COLUMN IF NOT EXISTS mat_ec_href String MATERIALIZED extract(elements_chain, '(?::|\")href="(.*?)"'),
+ADD COLUMN IF NOT EXISTS mat_ec_texts Array(String) MATERIALIZED arrayDistinct(extractAll(elements_chain, '(?::|\")text="(.*?)"')),
+ADD COLUMN IF NOT EXISTS mat_ec_ids Array(String) MATERIALIZED arrayDistinct(extractAll(elements_chain, '(?::|\")id="(.*?)"')),
+ADD COLUMN IF NOT EXISTS mat_ec_elements Array(Enum('a', 'button', 'form', 'input', 'select', 'textarea', 'label')) MATERIALIZED arrayDistinct(extractAll(elements_chain, '(?:^|;)(a|button|form|input|select|textarea|label)(?:\\.|$|:)'))
 """
 
 ADD_COLUMNS_EVENTS = """
@@ -23,9 +23,9 @@ ADD COLUMN IF NOT EXISTS mat_ec_elements Array(Enum('a', 'button', 'form', 'inpu
 
 def add_columns_to_required_tables(_):
     with ch_pool.get_client() as client:
-        client.execute(ADD_COLUMNS_SHARDED_EVENTS.format(table="events", cluster=CLICKHOUSE_CLUSTER))
+        client.execute(ADD_COLUMNS_SHARDED_EVENTS.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
 
-        client.execute(ADD_COLUMNS_EVENTS.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
+        client.execute(ADD_COLUMNS_EVENTS.format(table="events", cluster=CLICKHOUSE_CLUSTER))
 
 
 operations = [
