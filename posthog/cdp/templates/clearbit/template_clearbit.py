b09a7634f9aada8c05b9522d38cb3cd3158d280f
Ben White <ben@posthog.com>
2024-06-28 11:25:31 +0200

    Fixes

--- posthog/cdp/templates/clearbit/template_clearbit.py
+++ posthog/cdp/templates/clearbit/template_clearbit.py
@@ -12,10 +12,9 @@ template: HogFunctionTemplate = HogFunctionTemplate(
     hog="""
 let api_key := inputs.api_key
 let email := inputs.email
-if (empty(email)) {
-    return false
-}
-if (person.properties.clearbit_enriched) {
+let event_name := inputs.event_name or 'clearbit_enriched'
+
+if (empty(email) or event.name == event_name or person.properties.clearbit_enriched) {
     return false
 }
 
@@ -28,7 +27,7 @@ let response := fetch(f'https://person-stream.clearbit.com/v2/combined/find?emai
 if (response.status == 200 and not empty(response.body.person)) {
     print('Clearbit data found - sending event to PostHog')
     postHogCapture({
-        'event': 'clearbit_enriched',
+        'event': event_name,
         'distinct_id': event.distinct_id,
         'properties': {
             '$set_once': {
@@ -58,6 +57,15 @@ if (response.status == 200 and not empty(response.body.person)) {
             "default": "{person.properties.email}",
             "secret": False,
             "required": True,
+        },
+        {
+            "key": "event_name",
+            "type": "string",
+            "label": "Event name",
+            "description": "Name of the event to be sent to PostHog when enriching with Clearbit data",
+            "secret": False,
+            "required": False,
+            "default": "clearbit_enriched",
         },
     ],
 )
