60bcb4de0f90bebf5e6ab7e81885edeb22244079
Paul D'Ambra <paul@posthog.com>
2024-06-30 19:27:08 +0100

    preload webvitals and match them onto navigation events

--- frontend/src/scenes/session-recordings/player/sessionRecordingDataLogic.ts
+++ frontend/src/scenes/session-recordings/player/sessionRecordingDataLogic.ts
@@ -16,6 +16,7 @@ import {
     selectors,
 } from 'kea'
 import { loaders } from 'kea-loaders'
+import { subscriptions } from 'kea-subscriptions'
 import api from 'lib/api'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { Dayjs, dayjs } from 'lib/dayjs'
@@ -476,6 +477,7 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
 
                     const { person } = values.sessionPlayerData
 
+                    let loadedProperties: Record<string, any> = existingEvent.properties
                     // TODO: Move this to an optimised HogQL query when available...
                     try {
                         const res: any = await api.query({
@@ -492,16 +494,25 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
                         const result = res.results.find((x: any) => x[1] === event.timestamp)
 
                         if (result) {
-                            existingEvent.properties = JSON.parse(result[0])
-                            existingEvent.fullyLoaded = true
+                            loadedProperties = JSON.parse(result[0])
                         }
                     } catch (e) {
                         // NOTE: This is not ideal but should happen so rarely that it is tolerable.
-                        existingEvent.fullyLoaded = true
                         captureException(e)
                     }
 
-                    return values.sessionEventsData
+                    // here we map the events list because we want the result to be a new instance to trigger downstream recalculation
+                    return !values.sessionEventsData
+                        ? values.sessionEventsData
+                        : values.sessionEventsData.map((x) => {
+                              return x.id === event.id
+                                  ? ({
+                                        ...x,
+                                        properties: loadedProperties,
+                                        fullyLoaded: true,
+                                    } as RecordingEventType)
+                                  : x
+                          })
                 },
             },
         ],
@@ -662,6 +673,12 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
         },
     })),
     selectors(({ cache }) => ({
+        webVitalsEvents: [
+            (s) => [s.sessionEventsData],
+            (sessionEventsData): RecordingEventType[] =>
+                (sessionEventsData || []).filter((e) => e.event === '$web_vitals'),
+        ],
+
         sessionPlayerData: [
             (s, p) => [
                 s.sessionPlayerMetaData,
@@ -890,6 +907,16 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
                 return snapshots.filter((snapshot) => snapshot.type === EventType.Custom).map((x) => x as customEvent)
             },
         ],
+    })),
+    subscriptions(({ actions }) => ({
+        webVitalsEvents: (value: RecordingEventType[]) => {
+            value.forEach((item) => {
+                // we preload all web vitals data, so it can be used before user interaction
+                if (!item.fullyLoaded) {
+                    actions.loadFullEventData(item)
+                }
+            })
+        },
     })),
     afterMount(({ cache }) => {
         resetTimingsCache(cache)
