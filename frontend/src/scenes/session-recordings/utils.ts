8e39ce100ce035922096646733bf373e2a0f6103
David Newell <david@posthog.com>
2024-07-01 15:20:25 +0100

    remove random file

--- frontend/src/scenes/session-recordings/utils.ts
+++
@@ -1,7 +0,0 @@
-import { RecordingFilters, RecordingUniversalFilters } from '~/types'
-
-export function isUniversalFilters(
-    filters: RecordingFilters | RecordingUniversalFilters
-): filters is RecordingUniversalFilters {
-    return 'filter_group' in filters
-}
