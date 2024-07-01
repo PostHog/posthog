b18fb1b974ae30f01a270955edb63c2463e3f7e1
Zach Waterfield <zlwaterfield@gmail.com>
2024-07-01 11:02:21 -0400

    tmp

--- cypress/e2e/auth-saml.cy.ts
+++ cypress/e2e/auth-saml.cy.ts
@@ -1,3 +1,6 @@
+const samlEmail = 'me@zlwaterfield.com'
+const samlPassword = 'tg6tA8epnXy8edBL3hdr'
+
 describe('SAML Auth', () => {
     beforeEach(() => {
         cy.get('[data-attr=menu-item-me]').click()
@@ -6,15 +9,17 @@ describe('SAML Auth', () => {
     it('Login with SAML', () => {
         cy.get('[data-attr=top-menu-item-logout]').click()
 
-        cy.get('[data-attr=login-email').type('').should('have.value', '').blur()
+        cy.get('[data-attr=login-email').type(samlEmail).should('have.value', samlEmail).blur()
         cy.wait(500)
         cy.get('[data-attr=password]').should('not.be.visible')
         cy.get('button[data-attr=sso-login]').should('have.text', 'Log in with Single sign-on (SAML)').click()
 
         cy.origin('https://trial-4372086.okta.com/app/trial-4372086_posthogdev_1/exkfso4f5a5yoH2u9697/sso/saml', () => {
-            cy.get('input[name=identifier]').type('').should('have.value', '')
+            const samlEmail = 'me@zlwaterfield.com'
+            const samlPassword = 'tg6tA8epnXy8edBL3hdr'
+            cy.get('input[name=identifier]').type(samlEmail).should('have.value', samlEmail)
             cy.get('input[type=submit]').should('have.value', 'Next').click()
-            cy.get('input[type=password]').type('').should('have.value', '')
+            cy.get('input[type=password]').type(samlPassword).should('have.value', samlPassword)
             cy.get('input[type=submit]').should('have.value', 'Verify').click()
         })
 
--- posthog/management/commands/setup_dev.py
+++ posthog/management/commands/setup_dev.py
@@ -19,6 +19,29 @@ from posthog.models.personal_api_key import hash_key_value
 from posthog.models.property_definition import PropertyDefinition
 
 
+domain = "zlwaterfield.com"
+saml_entity_id = "http://www.okta.com/exkfso4f5a5yoH2u9697"
+saml_acs_url = "https://trial-4372086.okta.com/app/trial-4372086_posthogdev_1/exkfso4f5a5yoH2u9697/sso/saml"
+saml_x509_cert = """-----BEGIN CERTIFICATE-----
+MIIDqjCCApKgAwIBAgIGAZBLKqqTMA0GCSqGSIb3DQEBCwUAMIGVMQswCQYDVQQGEwJVUzETMBEG
+A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU
+MBIGA1UECwwLU1NPUHJvdmlkZXIxFjAUBgNVBAMMDXRyaWFsLTQzNzIwODYxHDAaBgkqhkiG9w0B
+CQEWDWluZm9Ab2t0YS5jb20wHhcNMjQwNjI0MTY1MjI1WhcNMzQwNjI0MTY1MzI1WjCBlTELMAkG
+A1UEBhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xDTAL
+BgNVBAoMBE9rdGExFDASBgNVBAsMC1NTT1Byb3ZpZGVyMRYwFAYDVQQDDA10cmlhbC00MzcyMDg2
+MRwwGgYJKoZIhvcNAQkBFg1pbmZvQG9rdGEuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIB
+CgKCAQEAyu0MtzvMc2PGntBiQco1NnQ4579mhX4yzkDEyYpu0d8B/+5D5z28vJUdwlNeUhGWDtLp
+Ihkm1lQmAQcC7mNn1jlkeKVnD+O3hqkNV/0PBcbXqdOQt2gkLpj5sVL00MOWkeMDFl8ri+dRMOXI
+xUNWwAEWwXO4EUbNKwtAtTj/ljj5kp/HtVT7urOuPI+KZ5QYt0YSqGwx7QpM4lWzGrgkyjjzynXh
+sJAJSp7hs7hvOj4aGAPGrGFcI65Dcelk3xAAanWuV90nBtkBDwQFP8WZ88WEdGEcaveiN/7fRDQH
+wrq07N5F/+mGKz3x/NVruWYut9sHXBLyG0QZwys3Ggn59QIDAQABMA0GCSqGSIb3DQEBCwUAA4IB
+AQBEprZUQnbUM5p5z2PX2ha5rkkDle7gccDS2WHljI+a7dRSoSMZHY0r7mxmG4pFwdwagSAC/lsu
+7RFT32SCx3Mwyqny6hcH7AXlL30E1uyigoH7lazO2l3wyuua+7K6CwILXLr/6ScER1x81BjgVmhS
+tjFgjwkc2ctYxN64kNoKqaFl1F+gAuECDQRJhSZICOwbVw1U1qSwCL4wdAS8W38tw1AcVU3KAHAC
+X3vH90EFur/PknsPfdTURSm/n4RV3u/MJ3Ps/VGmXYY2ABoe7cwAWgzqecgXnrK5mnY3ryCg6VBT
+Nh4dhjP5ATQ7YSgOcL5AR4TJ0J/08PDosZTMYxKB
+-----END CERTIFICATE-----"""
+
 class Command(BaseCommand):
     help = "Set up the instance for development/review with demo data"
 
@@ -68,16 +91,24 @@ class Command(BaseCommand):
             if options["create_e2e_test_plugin"]:
                 self.create_plugin(team)
 
-            saml_organization = Organization.objects.update(name="saml org")
+            saml_organization = Organization.objects.create(
+                name="saml org",
+                available_product_features=[
+                    {"key": "saml", "name": "SAML"}, 
+                    {"key": "sso_enforcement", "name": "SSO Enforcement"},
+                    {"key": "automatic_provisioning", "name": "Automatic Provisioning"},
+                    {"key": "social_sso", "name": "Social SSO"}
+                ],
+            )
             OrganizationDomain.objects.create(
                 organization=saml_organization,
-                domain="",
+                domain=domain,
                 verified_at=timezone.now(),
                 jit_provisioning_enabled=True,
                 sso_enforcement="saml",
-                saml_entity_id="",
-                saml_acs_url="",
-                saml_x509_cert="",
+                saml_entity_id=saml_entity_id,
+                saml_acs_url=saml_acs_url,
+                saml_x509_cert=saml_x509_cert,
             )
 
     @staticmethod
