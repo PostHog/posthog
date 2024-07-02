e7c14d999668945b84cb2b7115e6f42d67fbb04d
James Greenhill <fuziontech@gmail.com>
2024-07-01 23:34:01 -0700

    reorder tables

--- posthog/clickhouse/migrations/0066_event_kafka_debug_table.py
+++ posthog/clickhouse/migrations/0066_event_kafka_debug_table.py
@@ -10,11 +10,11 @@ from posthog.settings.data_stores import KAFKA_HOSTS
 
 debug_table = KafkaDebugTable(topic=KAFKA_EVENTS_JSON)
 kafka_table = KafkaDebugKafkaTable(brokers=KAFKA_HOSTS, topic=KAFKA_EVENTS_JSON)
-materialized_view = KafkaDebugMaterializedView(to_table=debug_table, from_table=debug_table)
+materialized_view = KafkaDebugMaterializedView(to_table=debug_table, from_table=kafka_table)
 
 
 operations = [
     run_sql_with_exceptions(debug_table.get_create_table_sql()),
-    run_sql_with_exceptions(KafkaDebugMaterializedView(debug_table, kafka_table).get_create_view_sql()),
     run_sql_with_exceptions(kafka_table.get_create_table_sql()),
+    run_sql_with_exceptions(materialized_view.get_create_view_sql()),
 ]
