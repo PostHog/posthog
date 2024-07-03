08e212906898c03f239a66eecd2fdc4896fc1eb1
Tom Owers <tom@posthog.com>
2024-07-03 09:55:52 +0100

    feat(dev-ex): Auto reload celery without an external watcher, and with debugging su… (#23402)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Julian Bez <julian@posthog.com>

---
+++ posthog/management/commands/run_autoreload_celery.py
@@ -0,0 +1,39 @@
+import django
+
+from django.core.management.base import BaseCommand
+
+from posthog.tasks.utils import CeleryQueue
+
+# ❗ needs to be called *before* importing autoreload
+django.setup()
+
+from django.utils import autoreload  # noqa: E402
+
+
+class Command(BaseCommand):
+    help = "Wrap celery in djangos auto reload functionality"
+
+    def handle(self, *args, **options):
+        autoreload.run_with_reloader(self.run_celery_worker)
+
+    def run_celery_worker(self):
+        from posthog.celery import app as celery_app
+
+        queues = [q.value for q in CeleryQueue]
+
+        args = [
+            "-A",
+            "posthog",
+            "worker",
+            "-B",
+            "-S",
+            "redbeat.RedBeatScheduler",
+            "--without-mingle",
+            "--pool=threads",
+            f"--queues={','.join(queues)}",
+            "-Ofair",
+            "-n",
+            "node@%h",
+        ]
+
+        celery_app.worker_main(args)
