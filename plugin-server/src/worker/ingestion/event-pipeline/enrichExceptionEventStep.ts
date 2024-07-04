cf7f7b51b967a24d34d4256325ace917e56445aa
Paul D'Ambra <paul@posthog.com>
2024-07-04 07:56:01 +0100

    extra extra safe

--- plugin-server/src/worker/ingestion/event-pipeline/enrichExceptionEventStep.ts
+++ plugin-server/src/worker/ingestion/event-pipeline/enrichExceptionEventStep.ts
@@ -1,3 +1,4 @@
+import { captureException } from '@sentry/node'
 import { Counter } from 'prom-client'
 
 import { PreIngestionEvent } from '../../../types'
@@ -13,6 +14,11 @@ const COULD_NOT_PARSE_STACK_TRACE_COUNTER = new Counter({
     help: 'Counter for exceptions where the stack trace could not be parsed',
 })
 
+const COULD_NOT_PREPARE_FOR_FINGERPRINTING_COUNTER = new Counter({
+    name: 'enrich_exception_events_could_not_prepare_for_fingerprinting',
+    help: 'Counter for exceptions where the event could not be prepared for fingerprinting',
+})
+
 const EXCEPTIONS_ENRICHED_COUNTER = new Counter({
     name: 'enrich_exception_events_enriched',
     help: 'Counter for exceptions that have been enriched',
@@ -26,29 +32,44 @@ export function enrichExceptionEventStep(
         return Promise.resolve(event)
     }
 
-    const exceptionStack = event.properties['$exception_stack_trace_raw']
-    const fingerPrint = event.properties['$exception_fingerprint']
-    const type = event.properties['$exception_type']
-    const message = event.properties['$exception_message']
+    let type: string | null = null
+    let message: string | null = null
     let firstFunction: string | null = null
+    let exceptionStack: string | null = null
 
-    if (fingerPrint) {
-        EXTERNAL_FINGERPRINT_COUNTER.inc()
-        return Promise.resolve(event)
+    try {
+        exceptionStack = event.properties['$exception_stack_trace_raw']
+        const fingerPrint = event.properties['$exception_fingerprint']
+        type = event.properties['$exception_type']
+        message = event.properties['$exception_message']
+
+        if (fingerPrint) {
+            EXTERNAL_FINGERPRINT_COUNTER.inc()
+            return Promise.resolve(event)
+        }
+    } catch (e) {
+        captureException(e)
+        COULD_NOT_PREPARE_FOR_FINGERPRINTING_COUNTER.inc()
     }
 
     try {
-        const parsedStack = JSON.parse(exceptionStack)
-        if (parsedStack.length > 0) {
-            firstFunction = parsedStack[0].function
+        if (exceptionStack) {
+            const parsedStack = JSON.parse(exceptionStack)
+            if (parsedStack.length > 0) {
+                firstFunction = parsedStack[0].function
+            }
         }
     } catch (e) {
+        captureException(e)
         COULD_NOT_PARSE_STACK_TRACE_COUNTER.inc()
     }
 
     const fingerprint = [type, message, firstFunction].filter(Boolean)
     event.properties['$exception_fingerprint'] = fingerprint.length ? fingerprint : undefined
 
-    EXCEPTIONS_ENRICHED_COUNTER.inc()
+    if (event.properties['$exception_fingerprint']) {
+        EXCEPTIONS_ENRICHED_COUNTER.inc()
+    }
+
     return Promise.resolve(event)
 }
