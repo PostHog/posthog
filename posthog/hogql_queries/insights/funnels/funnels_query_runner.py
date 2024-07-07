17ac1288ebe4f37d52ec575a42c91bd6c42b8984
Alexander Spicer <aspicer@gmail.com>
2024-07-07 13:27:32 -0700

    time tracking

--- posthog/hogql_queries/insights/funnels/funnels_query_runner.py
+++ posthog/hogql_queries/insights/funnels/funnels_query_runner.py
@@ -118,8 +118,9 @@ class FunnelsQueryRunner(QueryRunner):
         elif funnelVizType == FunnelVizType.TIME_TO_CONVERT:
             return FunnelTimeToConvert(context=self.context)
         else:
-            #if self.query.funnelsFilter.funnelOrderType == StepOrderValue.STRICT:
-            #    return FunnelUDF(context=self.context)
+            # For testing
+            if self.query.funnelsFilter.funnelOrderType == StepOrderValue.STRICT:
+                return FunnelUDF(context=self.context)
             return self.funnel_order_class
 
     @cached_property
