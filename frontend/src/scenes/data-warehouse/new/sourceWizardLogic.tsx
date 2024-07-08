e6d1436528f30bb3120585aaab0148afe017ebb3
eric <eeoneric@gmail.com>
2024-07-08 16:14:55 -0400

    restore

--- frontend/src/scenes/data-warehouse/new/sourceWizardLogic.tsx
+++ frontend/src/scenes/data-warehouse/new/sourceWizardLogic.tsx
@@ -1,5 +1,5 @@
 import { lemonToast, Link } from '@posthog/lemon-ui'
-import { actions, connect, kea, listeners, path, reducers, selectors } from 'kea'
+import { actions, connect, kea, listeners, path, props, reducers, selectors } from 'kea'
 import { forms } from 'kea-forms'
 import { router, urlToAction } from 'kea-router'
 import api from 'lib/api'
@@ -338,8 +338,13 @@ const manualLinkSourceMap: Record<ManualLinkSourceType, string> = {
     'cloudflare-r2': 'Cloudflare R2',
 }
 
+export interface SourceWizardLogicProps {
+    onComplete?: () => void
+}
+
 export const sourceWizardLogic = kea<sourceWizardLogicType>([
     path(['scenes', 'data-warehouse', 'external', 'sourceWizardLogic']),
+    props({} as SourceWizardLogicProps),
     actions({
         selectConnector: (connector: SourceConfig | null) => ({ connector }),
         toggleManualLinkFormVisible: (visible: boolean) => ({ visible }),
@@ -493,7 +498,7 @@ export const sourceWizardLogic = kea<sourceWizardLogicType>([
             },
         ],
     }),
-    selectors({
+    selectors(({ props }) => ({
         isManualLinkingSelected: [(s) => [s.selectedConnector], (selectedConnector): boolean => !selectedConnector],
         canGoBack: [
             (s) => [s.currentStep],
@@ -537,6 +542,9 @@ export const sourceWizardLogic = kea<sourceWizardLogicType>([
                 }
 
                 if (currentStep === 4) {
+                    if (props.onComplete) {
+                        return 'Next'
+                    }
                     return 'Return to settings'
                 }
 
@@ -641,8 +649,10 @@ export const sourceWizardLogic = kea<sourceWizardLogicType>([
                 return ''
             },
         ],
-    }),
-    listeners(({ actions, values }) => ({
+        // determines if the wizard is wrapped in another component
+        isWrapped: [() => [], () => !!props.onComplete],
+    })),
+    listeners(({ actions, values, props }) => ({
         onBack: () => {
             if (values.currentStep <= 1) {
                 actions.onClear()
@@ -688,7 +698,11 @@ export const sourceWizardLogic = kea<sourceWizardLogicType>([
             }
 
             if (values.currentStep === 4) {
-                actions.closeWizard()
+                if (props.onComplete) {
+                    props.onComplete()
+                } else {
+                    actions.closeWizard()
+                }
             }
         },
         createTableSuccess: () => {
