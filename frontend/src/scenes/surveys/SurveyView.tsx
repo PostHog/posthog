2bbf159b4480b8a014f2774333fd8427f841c05c
dylan <dylan@posthog.com>
2024-07-09 08:20:08 -0700

    inital tab

--- frontend/src/scenes/surveys/SurveyView.tsx
+++ frontend/src/scenes/surveys/SurveyView.tsx
@@ -4,6 +4,7 @@ import { TZLabel } from '@posthog/apps-common'
 import { IconGraph } from '@posthog/icons'
 import { LemonButton, LemonDivider, Link } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
+import { ActivityLog } from 'lib/components/ActivityLog/ActivityLog'
 import { EditableField } from 'lib/components/EditableField/EditableField'
 import { PageHeader } from 'lib/components/PageHeader'
 import { dayjs } from 'lib/dayjs'
@@ -16,7 +17,15 @@ import { urls } from 'scenes/urls'
 
 import { Query } from '~/queries/Query/Query'
 import { NodeKind } from '~/queries/schema'
-import { InsightType, PropertyFilterType, PropertyOperator, Survey, SurveyQuestionType, SurveyType } from '~/types'
+import {
+    ActivityScope,
+    InsightType,
+    PropertyFilterType,
+    PropertyOperator,
+    Survey,
+    SurveyQuestionType,
+    SurveyType,
+} from '~/types'
 
 import { SURVEY_EVENT_NAME } from './constants'
 import { SurveyDisplaySummary } from './Survey'
@@ -291,6 +300,11 @@ export function SurveyView({ id }: { id: string }): JSX.Element {
                                 ),
                                 key: 'overview',
                                 label: 'Overview',
+                            },
+                            {
+                                label: 'History',
+                                key: 'History', // TODO convert this to an enum
+                                content: <ActivityLog scope={ActivityScope.SURVEY} id={survey.id} />,
                             },
                         ]}
                     />
