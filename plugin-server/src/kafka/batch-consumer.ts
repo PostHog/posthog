e4909b3405221965442cc62b842fa00d1c5e2d82
Paul D'Ambra <paul@posthog.com>
2024-07-09 17:31:29 +0100

    fix

--- plugin-server/src/kafka/batch-consumer.ts
+++ plugin-server/src/kafka/batch-consumer.ts
@@ -145,7 +145,6 @@ export const startBatchConsumer = async ({
         // https://github.com/confluentinc/librdkafka/blob/e75de5be191b6b8e9602efc969f4af64071550de/CONFIGURATION.md?plain=1#L122
         // Initial maximum number of bytes per topic+partition to request when fetching messages from the broker. If the client encounters a message larger than this value it will gradually try to increase it until the entire message can be fetched.
         'fetch.message.max.bytes': consumerMaxBytes,
-        'fetch.min.bytes': fetchMinBytes || 1, // consumer default is 1 if not provided
         'fetch.wait.max.ms': consumerMaxWaitMs,
         'fetch.error.backoff.ms': consumerErrorBackoffMs,
         'enable.partition.eof': true,
@@ -172,6 +171,11 @@ export const startBatchConsumer = async ({
         rebalance_cb: true,
         offset_commit_cb: true,
         'statistics.interval.ms': kafkaStatisticIntervalMs,
+    }
+
+    // undefined is valid but things get unhappy if you provide that explicitly
+    if (fetchMinBytes) {
+        consumerConfig['fetch.min.bytes'] = fetchMinBytes
     }
 
     if (debug) {
