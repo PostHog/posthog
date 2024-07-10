8bc120fe1703f908e81f2d4bfe3e1e7a7a369681
Anirudh Pillai <anirudhx5@gmail.com>
2024-07-10 14:46:25 +0100

    Rewrite django migration to remove CONSTRAINT command

--- posthog/migrations/0435_annotation_dashboard_alter_annotation_scope.py
+++ posthog/migrations/0435_annotation_dashboard_alter_annotation_scope.py
@@ -4,18 +4,77 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+## Original Migration
+# class Migration(migrations.Migration):
+#     dependencies = [
+#         ("posthog", "0434_add_web_vitals_opt_in"),
+#     ]
+
+#     operations = [
+#         migrations.AddField(
+#             model_name="annotation",
+#             name="dashboard",
+#             field=models.ForeignKey(
+#                 blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to="posthog.dashboard"
+#             ),
+#         ),
+#         migrations.AlterField(
+#             model_name="annotation",
+#             name="scope",
+#             field=models.CharField(
+#                 choices=[
+#                     ("dashboard_item", "insight"),
+#                     ("dashboard", "dashboard"),
+#                     ("project", "project"),
+#                     ("organization", "organization"),
+#                 ],
+#                 default="dashboard_item",
+#                 max_length=24,
+#             ),
+#         ),
+#     ]
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("posthog", "0434_add_web_vitals_opt_in"),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="annotation",
-            name="dashboard",
-            field=models.ForeignKey(
-                blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to="posthog.dashboard"
-            ),
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddField(
+                    model_name="annotation",
+                    name="dashboard",
+                    field=models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to="posthog.dashboard",
+                    ),
+                )
+            ],
+            database_operations=[
+                # We add -- existing-table-constraint-ignore to ignore the constraint validation in CI.
+                migrations.RunSQL(
+                    """
+                    ALTER TABLE "posthog_annotation" ADD COLUMN "dashboard_id" integer NULL CONSTRAINT "posthog_annotation_dashboard_id_91ef4125_fk_posthog_d" REFERENCES "posthog_dashboard"("id") DEFERRABLE INITIALLY DEFERRED; -- existing-table-constraint-ignore
+                    SET CONSTRAINTS "posthog_annotation_dashboard_id_91ef4125_fk_posthog_d" IMMEDIATE; -- existing-table-constraint-ignore
+                    """,
+                    reverse_sql="""
+                        ALTER TABLE "posthog_annotation" DROP COLUMN IF EXISTS "dashboard_id";
+                    """,
+                ),
+                # We add CONCURRENTLY to the create command
+                migrations.RunSQL(
+                    """
+                    CREATE INDEX CONCURRENTLY "posthog_annotation_dashboard_id_91ef4125" ON "posthog_annotation" ("dashboard_id");
+                    """,
+                    reverse_sql="""
+                        DROP INDEX IF EXISTS "posthog_annotation_dashboard_id_91ef4125";
+                    """,
+                ),
+            ],
         ),
         migrations.AlterField(
             model_name="annotation",
