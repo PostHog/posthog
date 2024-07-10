91df7b188f9be8ac6dcd2c0e27ebbc3cdc3a1eed
Alexander Spicer <aspicer@gmail.com>
2024-07-10 00:01:03 -0700

    Cleanup

--- posthog/hogql_queries/insights/funnels/test/test_funnel_strict_udf.py
+++ posthog/hogql_queries/insights/funnels/test/test_funnel_strict_udf.py
@@ -1,19 +1,6 @@
 from unittest.mock import Mock, patch
 
-
 from posthog.hogql_queries.insights.funnels.test.test_funnel_strict import TestFunnelStrictStepsBreakdown
-from posthog.models.action import Action
-
-
-FORMAT_TIME = "%Y-%m-%d 00:00:00"
-
-
-def _create_action(**kwargs):
-    team = kwargs.pop("team")
-    name = kwargs.pop("name")
-    properties = kwargs.pop("properties", {})
-    action = Action.objects.create(team=team, name=name, steps_json=[{"event": name, "properties": properties}])
-    return action
 
 
 @patch("posthoganalytics.feature_enabled", new=Mock(return_value=True))
