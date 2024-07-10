f43affc710749ec8ba8c64a0f4696b021ff474d8
Georgiy Tarasov <skoob17@mail.ru>
2024-07-10 19:10:48 +0200

    fix: hide smoothing with breakdowns

--- frontend/src/queries/nodes/InsightViz/InsightDisplayConfig.tsx
+++ frontend/src/queries/nodes/InsightViz/InsightDisplayConfig.tsx
@@ -62,7 +62,10 @@ export function InsightDisplayConfig(): JSX.Element {
         isLifecycle ||
         ((isTrends || isStickiness) && !(display && NON_TIME_SERIES_DISPLAY_TYPES.includes(display)))
     const showSmoothing =
-        isTrends && !breakdownFilter?.breakdown_type && (!display || display === ChartDisplayType.ActionsLineGraph)
+        isTrends &&
+        !breakdownFilter?.breakdown_type &&
+        (breakdownFilter?.breakdowns?.length || 0) === 0 &&
+        (!display || display === ChartDisplayType.ActionsLineGraph)
 
     const { showValuesOnSeries, mightContainFractionalNumbers } = useValues(trendsDataLogic(insightProps))
 
