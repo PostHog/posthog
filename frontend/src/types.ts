76bfa2b322746b65bfc69e4afced43908512eb62
Ben White <ben@posthog.com>
2024-07-10 14:38:03 +0200

    Fixed sanitising of secret values

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -4210,6 +4210,12 @@ export type HogFunctionInputSchemaType = {
     integration_field?: 'slack_channel'
 }
 
+export type HogFunctionInputType = {
+    value: any
+    secret?: boolean
+    bytecode?: any
+}
+
 export type HogFunctionType = {
     id: string
     icon_url?: string
@@ -4222,13 +4228,7 @@ export type HogFunctionType = {
     hog: string
 
     inputs_schema?: HogFunctionInputSchemaType[]
-    inputs?: Record<
-        string,
-        {
-            value: any
-            bytecode?: any
-        }
-    >
+    inputs?: Record<string, HogFunctionInputType>
     filters?: PluginConfigFilters | null
     template?: HogFunctionTemplateType
     status?: HogFunctionStatus
