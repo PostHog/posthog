5fedb217e719df5886517a5863d8ae0b106804c7
Ben White <ben@posthog.com>
2024-07-11 12:23:31 +0200

    Fix

--- plugin-server/src/cdp/async-function-executor.ts
+++ plugin-server/src/cdp/async-function-executor.ts
@@ -1,5 +1,3 @@
-import { Webhook } from '@posthog/plugin-scaffold'
-
 import { PluginsServerConfig } from '../types'
 import { trackedFetch } from '../utils/fetch'
 import { status } from '../utils/status'
