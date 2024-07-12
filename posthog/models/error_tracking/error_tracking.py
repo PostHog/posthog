c046c3c6aef0c39990189c617971e73d4bd874c6
David Newell <david@posthog.com>
2024-07-12 11:36:03 +0100

    use TextField

--- posthog/models/error_tracking/error_tracking.py
+++ posthog/models/error_tracking/error_tracking.py
@@ -14,8 +14,8 @@ class ErrorTrackingGroup(UUIDModel):
 
     team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True, blank=True)
-    fingerprint: models.CharField = models.CharField(max_length=200, null=False, blank=False)
-    merged_fingerprints: ArrayField = ArrayField(models.CharField(max_length=200, null=False), default=list)
+    fingerprint: models.CharField = models.TextField(null=False, blank=False)
+    merged_fingerprints: ArrayField = ArrayField(models.TextField(null=False, blank=False), default=list)
     status: models.CharField = models.CharField(
         max_length=40, choices=Status.choices, default=Status.ACTIVE, null=False
     )
