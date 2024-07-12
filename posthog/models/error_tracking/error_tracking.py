076ec7ef8728bb362b1664f250552696201eae9c
David Newell <david@posthog.com>
2024-07-12 11:39:30 +0100

    migration

--- posthog/models/error_tracking/error_tracking.py
+++ posthog/models/error_tracking/error_tracking.py
@@ -14,7 +14,7 @@ class ErrorTrackingGroup(UUIDModel):
 
     team: models.ForeignKey = models.ForeignKey("Team", on_delete=models.CASCADE)
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True, blank=True)
-    fingerprint: models.CharField = models.TextField(null=False, blank=False)
+    fingerprint: models.TextField = models.TextField(null=False, blank=False)
     merged_fingerprints: ArrayField = ArrayField(models.TextField(null=False, blank=False), default=list)
     status: models.CharField = models.CharField(
         max_length=40, choices=Status.choices, default=Status.ACTIVE, null=False
