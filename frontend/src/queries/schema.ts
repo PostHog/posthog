e0d421f20b5d51754dd3d9abe681611a0c690d80
Robbie Coomber <robbie.coomber@gmail.com>
2024-07-16 21:01:35 +0100

    Disable test user filters by default in web analytics

--- frontend/src/queries/schema.ts
+++ frontend/src/queries/schema.ts
@@ -1158,6 +1158,7 @@ interface WebAnalyticsQueryBase<R extends Record<string, any>> extends DataNode<
         enabled?: boolean
         forceSamplingRate?: SamplingRate
     }
+    filterTestAccounts?: boolean
     /** @deprecated ignored, always treated as enabled **/
     useSessionsTable?: boolean
 }
