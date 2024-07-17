d6fb5e9f076b67201b894a9ec8657909a66e31f8
Oliver Browne <oliver@posthog.com>
2024-07-17 15:43:14 +0300

    add semver flattener

--- plugin-server/src/worker/vm/inline/inline.ts
+++ plugin-server/src/worker/vm/inline/inline.ts
@@ -8,12 +8,16 @@ import { status } from '../../../utils/status'
 >>>>>>> 1d81f1af72 (inline plugin database support)
 import { PluginInstance } from '../lazy'
 import { NoopInlinePlugin } from './noop'
+import { SEMVER_FLATTENER_CONFIG_SCHEMA, SemverFlattener } from './semver-flattener'
 
 export function constructInlinePluginInstance(hub: Hub, pluginConfig: PluginConfig): PluginInstance {
-    const plugin = INLINE_PLUGIN_MAP.get(pluginConfig.plugin?.url || '')
-    if (!plugin) {
-        throw new Error(`Inline plugin constructor not found for ${pluginConfig.plugin?.name}`)
+    const url = pluginConfig.plugin?.url
+
+    if (!INLINE_PLUGIN_URLS.includes(url as InlinePluginId)) {
+        throw new Error(`Invalid inline plugin URL: ${url}`)
     }
+    const plugin = INLINE_PLUGIN_MAP[url as InlinePluginId]
+
     return plugin.constructor(hub, pluginConfig)
 }
 
@@ -22,27 +26,48 @@ export interface RegisteredInlinePlugin {
     description: Readonly<InlinePluginDescription>
 }
 
+export const INLINE_PLUGIN_URLS = ['inline://noop', 'inline://semver-flattener'] as const
+type InlinePluginId = (typeof INLINE_PLUGIN_URLS)[number]
+
 // TODO - add all inline plugins here
-export const INLINE_PLUGIN_MAP: Map<string, RegisteredInlinePlugin> = new Map([
-    [
-        'inline://noop',
-        {
-            constructor: (hub: Hub, config: PluginConfig) => new NoopInlinePlugin(hub, config),
-            description: {
-                name: 'Noop Plugin',
-                description: 'A plugin that does nothing',
-                is_global: false,
-                is_preinstalled: false,
-                url: 'inline://noop',
-                config_schema: {},
-                tag: 'noop',
-                capabilities: {},
-                is_stateless: true,
-                log_level: PluginLogLevel.Info,
-            },
+export const INLINE_PLUGIN_MAP: Record<InlinePluginId, RegisteredInlinePlugin> = {
+    'inline://noop': {
+        constructor: (hub: Hub, config: PluginConfig) => new NoopInlinePlugin(hub, config),
+        description: {
+            name: 'Noop Plugin',
+            description: 'A plugin that does nothing',
+            is_global: false,
+            is_preinstalled: false,
+            url: 'inline://noop',
+            config_schema: {},
+            tag: 'noop',
+            capabilities: {},
+            is_stateless: true,
+            log_level: PluginLogLevel.Info,
         },
-    ],
-])
+    },
+
+    'inline://semver-flattener': {
+        constructor: (hub: Hub, config: PluginConfig) => new SemverFlattener(hub, config),
+        description: {
+            name: 'posthog-semver-flattener',
+            description:
+                'Processes specified properties to flatten sematic versions. Assumes any property contains a string which matches [the SemVer specification](https://semver.org/#backusnaur-form-grammar-for-valid-semver-versions)',
+            is_global: false,
+            is_preinstalled: false,
+            url: 'inline://semver-flattener',
+            config_schema: SEMVER_FLATTENER_CONFIG_SCHEMA,
+            tag: 'semver-flattener',
+            capabilities: {
+                jobs: [],
+                scheduled_tasks: [],
+                methods: ['processEvent'],
+            },
+            is_stateless: false, // TODO - this plugin /could/ be stateless, but right now we cache config parsing, which is stateful
+            log_level: PluginLogLevel.Info,
+        },
+    },
+}
 
 // Inline plugins are uniquely identified by their /url/, not their ID, and do
 // not have most of the standard plugin properties. This reduced interface is
@@ -67,7 +92,8 @@ export interface InlinePluginDescription {
 
 export async function syncInlinePlugins(hub: Hub): Promise<void> {
     status.info('⚡', 'Syncing inline plugins')
-    for (const [_, plugin] of INLINE_PLUGIN_MAP) {
+    for (const url of INLINE_PLUGIN_URLS) {
+        const plugin = INLINE_PLUGIN_MAP[url]
         await upsertInlinePlugin(hub, plugin.description)
     }
 }
