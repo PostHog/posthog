6ff5a30f9239e287e4c224ed13bf24dc0b0c5979
Anirudh Pillai <anirudhx5@gmail.com>
2024-07-18 16:20:35 +0100

    HogQL shortcuts in taxonomic filters

--- frontend/src/lib/components/TaxonomicFilter/InlineHogQLEditor.tsx
+++ frontend/src/lib/components/TaxonomicFilter/InlineHogQLEditor.tsx
@@ -1,3 +1,4 @@
+import { LemonButton } from '@posthog/lemon-ui'
 import { HogQLEditor } from 'lib/components/HogQLEditor/HogQLEditor'
 import { TaxonomicFilterValue } from 'lib/components/TaxonomicFilter/types'
 
@@ -9,18 +10,47 @@ export interface InlineHogQLEditorProps {
     metadataSource?: AnyDataNode
 }
 
+const SHORTCUTS = [
+    {
+        name: 'Hour',
+        value: 'toHour(timestamp)',
+    },
+    {
+        name: 'Day of Week',
+        value: "multiIf(toDayOfWeek(timestamp) == 1, 'Monday', toDayOfWeek(timestamp) == 2, 'Tuesday', toDayOfWeek(timestamp) == 3, 'Wednesday', toDayOfWeek(timestamp) == 4, 'Thursday', toDayOfWeek(timestamp) == 5, 'Friday', toDayOfWeek(timestamp) == 6, 'Saturday', 'Sunday')",
+    },
+]
+
 export function InlineHogQLEditor({ value, onChange, metadataSource }: InlineHogQLEditorProps): JSX.Element {
     return (
         <>
-            <div className="taxonomic-group-title">HogQL expression</div>
-            <div className="px-2 pt-2">
-                <HogQLEditor
-                    onChange={onChange}
-                    value={String(value ?? '')}
-                    metadataSource={metadataSource}
-                    submitText={value ? 'Update HogQL expression' : 'Add HogQL expression'}
-                    disableAutoFocus // :TRICKY: No autofocus here. It's controlled in the TaxonomicFilter.
-                />
+            <div className="flex flex-row gap-4 px-2">
+                <div className="flex flex-col gap-1 w-3/5">
+                    <div className="taxonomic-group-title px-0">HogQL expression</div>
+                    <HogQLEditor
+                        onChange={onChange}
+                        value={String(value ?? '')}
+                        submitText={value ? 'Update HogQL expression' : 'Add HogQL expression'}
+                        metadataSource={metadataSource}
+                        disableAutoFocus // :TRICKY: No autofocus here. It's controlled in the TaxonomicFilter.
+                    />
+                </div>
+                <div className="flex flex-col gap-1 w-2/5">
+                    <h5 className="mt-1 mb-0">Shortcuts</h5>
+                    <ul className="space-y-px w-full">
+                        {SHORTCUTS.map(({ name, value }) => (
+                            <LemonButton
+                                key={name}
+                                size="small"
+                                fullWidth
+                                onClick={() => onChange(value, {})}
+                                tooltip={value}
+                            >
+                                {name}
+                            </LemonButton>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </>
     )
