626dd3b736be193631e4ba19ea3250ce356d4508
Ben White <ben@posthog.com>
2024-07-18 17:21:12 +0200

    Added debugging to webhook

--- posthog/cdp/templates/webhook/test_template_webhook.py
+++ posthog/cdp/templates/webhook/test_template_webhook.py
@@ -1,3 +1,4 @@
+from inline_snapshot import snapshot
 from posthog.cdp.templates.helpers import BaseHogFunctionTemplateTest
 from posthog.cdp.templates.webhook.template_webhook import template as template_webhook
 
@@ -12,16 +13,27 @@ class TestTemplateWebhook(BaseHogFunctionTemplateTest):
                 "method": "GET",
                 "headers": {},
                 "body": {"hello": "world"},
+                "debug": False,
             }
         )
 
-        assert res.result is None
+        assert self.get_mock_fetch_calls()[0] == snapshot(
+            ("https://posthog.com", {"headers": {}, "body": {"hello": "world"}, "method": "GET"})
+        )
+        assert self.get_mock_print_calls() == snapshot([])
 
-        assert self.get_mock_fetch_calls()[0] == (
-            "https://posthog.com",
-            {
+    def test_prints_when_debugging(self):
+        self.run_function(
+            inputs={
+                "url": "https://posthog.com",
+                "method": "GET",
                 "headers": {},
                 "body": {"hello": "world"},
-                "method": "GET",
-            },
+                "debug": True,
+            }
         )
+
+        assert self.get_mock_fetch_calls()[0] == snapshot(
+            ("https://posthog.com", {"headers": {}, "body": {"hello": "world"}, "method": "GET"})
+        )
+        assert self.get_mock_print_calls() == snapshot([("Response", 200, {})])
