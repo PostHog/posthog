dc98c53eb3a0179bf7df0085bf57abc3d7626b3e
Dylan Martin <dylan@posthog.com>
2024-07-19 18:21:43 -0400

    fix(admin): reverts "Allow empty `DashboardTile.text` or `.insight`" (#23869)

--- posthog/models/dashboard_tile.py
+++ posthog/models/dashboard_tile.py
@@ -33,10 +33,16 @@ class DashboardTile(models.Model):
     # Relations
     dashboard = models.ForeignKey("posthog.Dashboard", on_delete=models.CASCADE, related_name="tiles")
     insight = models.ForeignKey(
-        "posthog.Insight", on_delete=models.CASCADE, related_name="dashboard_tiles", null=True, blank=True
+        "posthog.Insight",
+        on_delete=models.CASCADE,
+        related_name="dashboard_tiles",
+        null=True,
     )
     text = models.ForeignKey(
-        "posthog.Text", on_delete=models.CASCADE, related_name="dashboard_tiles", null=True, blank=True
+        "posthog.Text",
+        on_delete=models.CASCADE,
+        related_name="dashboard_tiles",
+        null=True,
     )
 
     # Tile layout and style
