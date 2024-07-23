d0a1d702dd97341f7a2839a4049611b13ce37ecf
Georgiy Tarasov <gtarasov.work@gmail.com>
2024-07-23 11:30:28 +0200

    fix: do not add breakdown_type for multiple breakdowns

--- frontend/src/queries/nodes/InsightQuery/utils/filtersToQueryNode.ts
+++ frontend/src/queries/nodes/InsightQuery/utils/filtersToQueryNode.ts
@@ -296,33 +296,35 @@ export const filtersToQueryNode = (filters: Partial<FilterType>): InsightQueryNo
 
     // breakdown
     if (isInsightQueryWithBreakdown(query)) {
-        /* handle missing breakdown_type */
-        // check for undefined and null values
-        if (filters.breakdown != null && filters.breakdown_type == null) {
-            filters.breakdown_type = 'event'
-        }
-
-        /* handle multi-breakdowns for funnels */
         // not undefined or null
-        if (filters.breakdowns != null && isFunnelsFilter(filters)) {
-            if (filters.breakdowns.length === 1) {
-                filters.breakdown_type = filters.breakdowns[0].type
-                filters.breakdown = filters.breakdowns[0].property as string
-            } else {
-                captureException(
-                    'Could not convert multi-breakdown property `breakdowns` - found more than one breakdown'
-                )
+        if (filters.breakdowns != null) {
+            /* handle multi-breakdowns for funnels */
+            if (isFunnelsFilter(filters)) {
+                if (filters.breakdowns.length === 1) {
+                    filters.breakdown_type = filters.breakdowns[0].type || 'event'
+                    filters.breakdown = filters.breakdowns[0].property as string
+                } else {
+                    captureException(
+                        'Could not convert multi-breakdown property `breakdowns` - found more than one breakdown'
+                    )
+                }
             }
-        }
 
-        /* prefer multiple breakdowns in trends */
-        // not undefined or null
-        if (filters.breakdowns != null && isTrendsFilter(filters)) {
-            filters.breakdowns = filters.breakdowns.map((b) => ({
-                ...b,
-                // Compatibility with legacy funnel breakdowns when someone switches a view from funnels to trends
-                type: b.type || filters.breakdown_type || 'event',
-            }))
+            /* handle multi-breakdowns for trends */
+            if (isTrendsFilter(filters)) {
+                filters.breakdowns = filters.breakdowns.map((b) => ({
+                    ...b,
+                    // Compatibility with legacy funnel breakdowns when someone switches a view from funnels to trends
+                    type: b.type || filters.breakdown_type || 'event',
+                }))
+            }
+        } else if (
+            /* handle missing breakdown_type */
+            // check for undefined and null values
+            filters.breakdown != null &&
+            filters.breakdown_type == null
+        ) {
+            filters.breakdown_type = 'event'
         }
 
         query.breakdownFilter = breakdownFilterToQuery(filters, isTrendsFilter(filters))
