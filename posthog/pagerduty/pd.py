97d93d5632b11c95956db6c66da4b0c7abf00d83
James Greenhill <fuziontech@gmail.com>
2024-07-26 15:37:08 -0700

    use get_instance_region to get region

--- posthog/pagerduty/pd.py
+++ posthog/pagerduty/pd.py
@@ -3,14 +3,15 @@ from structlog import get_logger
 
 from django.conf import settings
 
+from posthog.utils import get_instance_region
+
 
 logger = get_logger(__name__)
 routing_key = settings.PAGERDUTY_API_KEY
 session = EventsAPISession(routing_key)
 
 
-# function to create incident on pagerduty
 def create_incident(summary, source, severity="critical"):
     logger.info(f"Creating PagerDuty incident with summary: {summary}, source: {source}, severity: {severity}")
-    env = settings.ENVIRONMENT
+    env = get_instance_region()
     session.trigger(summary=f"{summary}-{env}", source=source, severity=severity)
