37589d6fc0105403a0a02f511621eadfd6349dfa
Ben White <ben@posthog.com>
2024-07-26 15:06:27 +0200

    feat: Add inline plugin icons (#24023)

--- frontend/src/scenes/pipeline/utils.tsx
+++ frontend/src/scenes/pipeline/utils.tsx
@@ -138,7 +138,7 @@ export function RenderApp({ plugin, imageSize = 'small' }: RenderAppProps): JSX.
                     </>
                 }
             >
-                {plugin.url ? (
+                {plugin.url && plugin.plugin_type !== 'inline' ? (
                     <Link to={plugin.url} target="_blank">
                         <PluginImage plugin={plugin} size={imageSize} />
                     </Link>
