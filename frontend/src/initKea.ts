ad37435b377548f4c76fa5889013976de4239cb6
Paul D'Ambra <paul@posthog.com>
2024-08-04 21:25:52 +0100

    fix: more specific key to enable verbose kea logging
    see: https://x.com/hrishio/status/1820125443153957238
    
    `debug` is too vague a key to use to enable verbose kea debugging in posthog - particularly because it's included in the toolbar so can appear to leak into other sites if other tools use `debug` in local storage to enable debugging

--- frontend/src/initKea.ts
+++ frontend/src/initKea.ts
@@ -114,7 +114,7 @@ export function initKea({ routerHistory, routerLocation, beforePlugins }: InitKe
     ]
 
     // To enable logging, run localStorage.setItem("debug", true) in the console
-    if (window.JS_KEA_VERBOSE_LOGGING || ('localStorage' in window && window.localStorage.getItem('debug'))) {
+    if (window.JS_KEA_VERBOSE_LOGGING || ('localStorage' in window && window.localStorage.getItem('ph-kea-debug'))) {
         plugins.push(loggerPlugin)
     }
 
