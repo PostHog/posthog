04814db1a145969d5e8e0b4ead15240999bf6d0a
Ben White <ben@posthog.com>
2024-08-06 13:00:14 +0200

    Fixes hosts

--- posthog/cdp/templates/salesforce/template_salesforce.py
+++ posthog/cdp/templates/salesforce/template_salesforce.py
@@ -24,7 +24,7 @@ template_create: HogFunctionTemplate = HogFunctionTemplate(
     description="Create objects in Salesforce",
     icon_url="/static/services/salesforce.png",
     hog="""
-let res := fetch(f'https://posthog.my.salesforce.com/services/data/v61.0/sobjects/{inputs.path}', {
+let res := fetch(f'{inputs.oauth.instance_url}/services/data/v61.0/sobjects/{inputs.path}', {
   'body': inputs.properties,
   'method': 'POST',
   'headers': {
@@ -72,7 +72,7 @@ template_update: HogFunctionTemplate = HogFunctionTemplate(
     description="Update objects in Salesforce",
     icon_url="/static/services/salesforce.png",
     hog="""
-let res := fetch(f'https://posthog.my.salesforce.com/services/data/v61.0/sobjects/{inputs.path}', {
+let res := fetch(f'{inputs.oauth.instance_url}/services/data/v61.0/sobjects/{inputs.path}', {
   'body': inputs.properties,
   'method': 'PATCH',
   'headers': {
