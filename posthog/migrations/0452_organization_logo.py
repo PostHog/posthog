8231eecf1b865d2d10202a259b970627bbe04202
Michael Matloka <michael@posthog.com>
2024-08-07 10:46:08 +0200

    Fix issues

--- posthog/migrations/0452_organization_logo.py
+++ posthog/migrations/0452_organization_logo.py
@@ -5,16 +5,43 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
+    atomic = False  # Added to support concurrent index creation
     dependencies = [
         ("posthog", "0451_datawarehousetable_updated_at_and_more"),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="organization",
-            name="logo_media",
-            field=models.ForeignKey(
-                blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to="posthog.uploadedmedia"
-            ),
+        # Using the approach with CREATE INDEX CONCURRENTLY from 0415_pluginconfig_match_action
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddField(
+                    model_name="organization",
+                    name="logo_media",
+                    field=models.ForeignKey(
+                        blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to="posthog.uploadedmedia"
+                    ),
+                ),
+            ],
+            database_operations=[
+                # We add -- existing-table-constraint-ignore to ignore the constraint validation in CI.
+                migrations.RunSQL(
+                    """
+                    ALTER TABLE "posthog_organization" ADD COLUMN "logo_media_id" uuid NULL CONSTRAINT "posthog_organization_logo_media_id_1c12c9dc_fk_posthog_u" REFERENCES "posthog_uploadedmedia"("id") DEFERRABLE INITIALLY DEFERRED;
+                    SET CONSTRAINTS "posthog_organization_logo_media_id_1c12c9dc_fk_posthog_u" IMMEDIATE; -- existing-table-constraint-ignore
+                    """,
+                    reverse_sql="""
+                        ALTER TABLE "posthog_organization" DROP COLUMN IF EXISTS "logo_media_id";
+                    """,
+                ),
+                # We add CONCURRENTLY to the create command
+                migrations.RunSQL(
+                    """
+                    CREATE INDEX CONCURRENTLY "posthog_organization_logo_media_id_1c12c9dc" ON "posthog_organization" ("logo_media_id");
+                    """,
+                    reverse_sql="""
+                        DROP INDEX IF EXISTS "posthog_organization_logo_media_id_1c12c9dc";
+                    """,
+                ),
+            ],
         ),
     ]
