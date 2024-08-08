e8915ff9d7c051bbc3f5be0eef6d185a3db9ceb5
Sandy Spicer <sandy@posthog.com>
2024-08-08 12:19:50 +0200

    types and stuff

--- posthog/hogql/database/schema/query_log.py
+++ posthog/hogql/database/schema/query_log.py
@@ -18,9 +18,14 @@ QUERY_LOG_FIELDS: dict[str, FieldOrTable] = {
     "team_id": IntegerDatabaseField(name="team_id"),
     "query_duration_ms": FloatDatabaseField(name="query_duration_ms"),
     "exception": StringDatabaseField(name="exception"),
-    "event_time": DateTimeDatabaseField(name="event_time"),
+    "event_time": DateTimeDatabaseField(name="event_time_microseconds"),
+    "cache_key": StringDatabaseField(name="cache_key"),
     "type": StringDatabaseField(name="type"),
+    "query_type": StringDatabaseField(name="query_type"),
 }
+
+STRING_FIELDS = {"cache_key", "query_type"}
+INT_FIELDS = {"team_id"}
 
 
 class QueryLogTable(LazyTable):
@@ -38,8 +43,10 @@ class QueryLogTable(LazyTable):
         table_name = "raw_query_log"
 
         def get_alias(name, chain):
-            if name == "team_id":
-                return ast.Alias(alias=name, expr=parse_expr("JSONExtractRaw(log_comment, 'team_id')"))
+            if name in STRING_FIELDS:
+                return ast.Alias(alias=name, expr=parse_expr(f"JSONExtractString(log_comment, '{name}')"))
+            if name in INT_FIELDS:
+                return ast.Alias(alias=name, expr=parse_expr(f"JSONExtractInt(log_comment, '{name}')"))
             return ast.Alias(alias=name, expr=ast.Field(chain=[table_name, *chain]))
 
         fields: list[ast.Expr] = [get_alias(name, chain) for name, chain in requested_fields.items()]
