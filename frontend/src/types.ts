acc037b6057e9f4496b687c38d7d08913d66f761
Juraj Majerik <juro.majerik@gmail.com>
2024-08-13 17:39:31 +0200

    init

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -3177,6 +3177,7 @@ export interface _TrendsExperimentResults extends BaseExperimentResults {
     filters: TrendsFilterType
     variants: TrendExperimentVariant[]
     last_refresh?: string | null
+    credible_intervals: { [key: string]: [number, number] }
 }
 
 export interface _FunnelExperimentResults extends BaseExperimentResults {
