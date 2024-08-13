8d590f20f893294ed92fb6a01bd7e4631b02d674
Marius Andra <marius.andra@gmail.com>
2024-08-13 17:20:16 +0200

    cleanup

--- frontend/src/scenes/pipeline/hogfunctions/hogFunctionConfigurationLogic.tsx
+++ frontend/src/scenes/pipeline/hogfunctions/hogFunctionConfigurationLogic.tsx
@@ -406,11 +406,6 @@ export const hogFunctionConfigurationLogic = kea<hogFunctionConfigurationLogicTy
                             $current_url: currentUrl,
                             $browser: 'Chrome',
                         },
-                        elements_chain: '',
-                        elements_chain_href: '',
-                        elements_chain_texts: [],
-                        elements_chain_ids: [],
-                        elements_chain_elements: [],
                     },
                     person: {
                         uuid: uuid(),
