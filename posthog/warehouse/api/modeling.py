e0e7ab6bdafebb45db65830d880ca0c78f9ae43d
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-08-13 12:09:26 +0200

    feat(data-models): Add DAG API

--- posthog/warehouse/api/modeling.py
+++ posthog/warehouse/api/modeling.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, viewsets
+from rest_framework import request, response, serializers, viewsets
 
 from posthog.api.routing import TeamAndOrgViewSetMixin
 from posthog.api.shared import UserBasicSerializer
@@ -12,8 +12,18 @@ class DataWarehouseModelPathSerializer(serializers.ModelSerializer):
         model = DataWarehouseModelPath
 
 
-class DataWarehouseModelViewSet(TeamAndOrgViewSetMixin, viewsets.ReadOnlyModelViewSet):
+class DataWarehouseModelPathViewSet(TeamAndOrgViewSetMixin, viewsets.ReadOnlyModelViewSet):
     scope_object = "INTERNAL"
 
     queryset = DataWarehouseModelPath.objects.all()
     serializer_class = DataWarehouseModelPathSerializer
+
+
+class DataWarehouseModelDagViewSet(TeamAndOrgViewSetMixin, viewsets.ViewSet):
+    scope_object = "INTERNAL"
+
+    def list(self, request: request.Request, *args, **kwargs) -> response.Response:
+        """Return this team's DAG as a set of edges and nodes"""
+        dag = DataWarehouseModelPath.objects.get_dag(self.team)
+
+        return response.Response({"edges": dag.edges, "nodes": dag.nodes})
