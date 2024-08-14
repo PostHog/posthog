8cecfbfdadb5acea06ab5e2ef275272987f1b9c0
James Greenhill <fuziontech@gmail.com>
2024-08-14 14:28:07 -0700

    feat: add universal 60 sec event counter to livestream (#24380)

---
+++ livestream/ttl_counter.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type SlidingWindowCounter struct {
+	mu         sync.Mutex
+	events     []time.Time
+	windowSize time.Duration
+}
+
+func NewSlidingWindowCounter(windowSize time.Duration) *SlidingWindowCounter {
+	return &SlidingWindowCounter{
+		events:     make([]time.Time, 0),
+		windowSize: windowSize,
+	}
+}
+
+func (swc *SlidingWindowCounter) Increment() {
+	swc.mu.Lock()
+	defer swc.mu.Unlock()
+
+	now := time.Now()
+	swc.events = append(swc.events, now)
+	swc.removeOldEvents(now)
+}
+
+func (swc *SlidingWindowCounter) Count() int {
+	swc.mu.Lock()
+	defer swc.mu.Unlock()
+
+	now := time.Now()
+	swc.removeOldEvents(now)
+	return len(swc.events)
+}
+
+func (swc *SlidingWindowCounter) removeOldEvents(now time.Time) {
+	cutoff := now.Add(-swc.windowSize)
+	i := 0
+	for ; i < len(swc.events); i++ {
+		if swc.events[i].After(cutoff) {
+			break
+		}
+	}
+	swc.events = swc.events[i:]
+}
