d49ef531850fcf789665b171a3b2d6b6c6ab2621
Zach Waterfield <zlwaterfield@gmail.com>
2024-08-15 18:30:29 -0400

    Update auth-saml.cy.ts

--- cypress/e2e/auth-saml.cy.ts
+++ cypress/e2e/auth-saml.cy.ts
@@ -33,28 +33,7 @@ describe('SAML Auth', () => {
             cy.get('button[type=submit]').should('have.text', 'Continue').click()
         })
 
-        cy.get('[data-attr=top-menu-item-logout]').click()
-
-        cy.get('[data-attr=login-email')
-            .type(Cypress.env('E2E_SAML_LOGIN_EMAIL'))
-            .should('have.value', Cypress.env('E2E_SAML_LOGIN_EMAIL'))
-            .blur()
-        cy.wait(500)
-        cy.get('[data-attr=password]').should('not.be.visible')
-        cy.get('button[data-attr=sso-login]').should('have.text', 'Log in with Single sign-on (SAML)').click()
-
-        cy.origin(Cypress.env('E2E_SAML_ACS_URL'), () => {
-            cy.get('input[data-testid=username]')
-                .type(Cypress.env('E2E_SAML_LOGIN_EMAIL'))
-                .should('have.value', Cypress.env('E2E_SAML_LOGIN_EMAIL'))
-            cy.get('button[type=submit]').should('have.text', 'Continue').click()
-            cy.get('input[type=password]')
-                .type(Cypress.env('E2E_SAML_LOGIN_PASSWORD'))
-                .should('have.value', Cypress.env('E2E_SAML_LOGIN_PASSWORD'))
-            cy.get('button[type=submit]').should('have.text', 'Continue').click()
-        })
-
-        cy.wait(20000)
+        cy.wait(5000)
         cy.get('[data-attr="breadcrumb-organization"] > span').should('have.text', 'saml org')
     })
 })
