f5c76dbf109065e7f75b021fe9a97a44ba8c2e00
Juraj Majerik <juro.majerik@gmail.com>
2024-08-15 18:09:11 +0200

    init

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -210,6 +210,7 @@ export const FEATURE_FLAGS = {
     MULTITAB_EDITOR: 'multitab-editor', // owner: @EDsCODE #team-data-warehouse
     WEB_ANALYTICS_REPLAY: 'web-analytics-replay', // owner: @robbie-c
     BATCH_EXPORTS_POSTHOG_HTTP: 'posthog-http-batch-exports',
+    EXPERIMENT_MAKE_DECISION: 'experiment-make-decision', // owner: @jurajmajerik #team-data-warehouse
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
--- frontend/src/scenes/experiments/ExperimentView/components.tsx
+++ frontend/src/scenes/experiments/ExperimentView/components.tsx
@@ -1,11 +1,13 @@
 import '../Experiment.scss'
 
-import { IconArchive, IconCheck, IconX } from '@posthog/icons'
+import { IconArchive, IconCheck, IconMagicWand, IconX } from '@posthog/icons'
 import {
     LemonBanner,
     LemonButton,
     LemonDialog,
     LemonDivider,
+    LemonModal,
+    LemonSelect,
     LemonSkeleton,
     LemonTag,
     LemonTagType,
@@ -16,23 +18,38 @@ import { useActions, useValues } from 'kea'
 import { AnimationType } from 'lib/animations/animations'
 import { Animation } from 'lib/components/Animation/Animation'
 import { PageHeader } from 'lib/components/PageHeader'
+import { FEATURE_FLAGS } from 'lib/constants'
 import { dayjs } from 'lib/dayjs'
 import { IconAreaChart } from 'lib/lemon-ui/icons'
 import { More } from 'lib/lemon-ui/LemonButton/More'
 import { useEffect, useState } from 'react'
 import { urls } from 'scenes/urls'
 
+import { groupsModel } from '~/models/groupsModel'
 import { filtersToQueryNode } from '~/queries/nodes/InsightQuery/utils/filtersToQueryNode'
 import { Query } from '~/queries/Query/Query'
 import { InsightVizNode, NodeKind } from '~/queries/schema'
-import { Experiment as ExperimentType, ExperimentResults, FilterType, InsightShortId, InsightType } from '~/types'
+import {
+    Experiment,
+    Experiment as ExperimentType,
+    ExperimentResults,
+    FilterType,
+    InsightShortId,
+    InsightType,
+} from '~/types'
 
 import { experimentLogic } from '../experimentLogic'
 import { getExperimentStatus, getExperimentStatusColor } from '../experimentsLogic'
 import { getExperimentInsightColour, transformResultFilters } from '../utils'
 
-export function VariantTag({ variantKey }: { variantKey: string }): JSX.Element {
-    const { experimentResults, getIndexForVariant } = useValues(experimentLogic)
+export function VariantTag({
+    experimentId,
+    variantKey,
+}: {
+    experimentId: number | 'new'
+    variantKey: string
+}): JSX.Element {
+    const { experimentResults, getIndexForVariant } = useValues(experimentLogic({ experimentId }))
 
     return (
         <span className="flex items-center space-x-1">
@@ -307,7 +324,15 @@ export function ExperimentLoadingAnimation(): JSX.Element {
 }
 
 export function PageHeaderCustom(): JSX.Element {
-    const { experiment, isExperimentRunning, isExperimentStopped } = useValues(experimentLogic)
+    const {
+        experimentId,
+        experiment,
+        isExperimentRunning,
+        isExperimentStopped,
+        areResultsSignificant,
+        isSingleVariantShipped,
+        featureFlags,
+    } = useValues(experimentLogic)
     const {
         launchExperiment,
         resetRunningExperiment,
@@ -317,8 +342,8 @@ export function PageHeaderCustom(): JSX.Element {
         loadExperimentResults,
         loadSecondaryMetricResults,
         createExposureCohort,
+        openMakeDecisionModal,
     } = useActions(experimentLogic)
-
     const exposureCohortId = experiment?.exposure_cohort
 
     return (
@@ -472,9 +497,93 @@ export function PageHeaderCustom(): JSX.Element {
                             )}
                         </div>
                     )}
+                    {featureFlags[FEATURE_FLAGS.EXPERIMENT_MAKE_DECISION] &&
+                        areResultsSignificant &&
+                        !isSingleVariantShipped && (
+                            <>
+                                <LemonButton
+                                    type="primary"
+                                    status="alt"
+                                    icon={<IconMagicWand />}
+                                    onClick={() => openMakeDecisionModal()}
+                                >
+                                    <b>Make decision</b>
+                                </LemonButton>
+                                <MakeDecisionModal experimentId={experimentId} />
+                            </>
+                        )}
                 </>
             }
         />
+    )
+}
+
+export function MakeDecisionModal({ experimentId }: { experimentId: Experiment['id'] }): JSX.Element {
+    const { experiment, sortedWinProbabilities, isMakeDecisionModalOpen } = useValues(experimentLogic({ experimentId }))
+    const { closeMakeDecisionModal, shipVariant } = useActions(experimentLogic({ experimentId }))
+    const { aggregationLabel } = useValues(groupsModel)
+
+    const [selectedVariantKey, setSelectedVariantKey] = useState<string | null>()
+    useEffect(() => setSelectedVariantKey(sortedWinProbabilities[0]?.key), [sortedWinProbabilities])
+
+    const aggregationTargetName =
+        experiment.filters.aggregation_group_type_index != null
+            ? aggregationLabel(experiment.filters.aggregation_group_type_index).plural
+            : 'users'
+
+    return (
+        <LemonModal
+            isOpen={isMakeDecisionModalOpen}
+            onClose={closeMakeDecisionModal}
+            width={600}
+            title="Make decision"
+            footer={
+                <div className="flex items-center gap-2">
+                    <LemonButton type="secondary" onClick={closeMakeDecisionModal}>
+                        Cancel
+                    </LemonButton>
+                    <LemonButton onClick={() => shipVariant(selectedVariantKey)} type="primary">
+                        Ship variant
+                    </LemonButton>
+                </div>
+            }
+        >
+            <div className="space-y-4">
+                <div className="text-sm">
+                    This action will roll out the selected variant to <b>100% of {aggregationTargetName}.</b>
+                </div>
+                <LemonSelect
+                    data-attr="metrics-selector"
+                    value={selectedVariantKey}
+                    onChange={(variantKey) => setSelectedVariantKey(variantKey)}
+                    options={sortedWinProbabilities.map(({ key }) => ({
+                        value: key,
+                        label: (
+                            <div className="space-x-2 inline-flex">
+                                <VariantTag experimentId={experimentId} variantKey={key} />
+                                {key === sortedWinProbabilities[0]?.key && (
+                                    <LemonTag type="success">
+                                        <b className="uppercase">Winning</b>
+                                    </LemonTag>
+                                )}
+                            </div>
+                        ),
+                    }))}
+                />
+                <LemonBanner type="info" className="mb-4">
+                    For a more precise control over your release, adjust the rollout percentage and release conditions
+                    in the{' '}
+                    <Link
+                        target="_blank"
+                        className="font-semibold"
+                        to={experiment.feature_flag ? urls.featureFlag(experiment.feature_flag.id) : undefined}
+                    >
+                        {experiment.feature_flag?.key}
+                    </Link>{' '}
+                    feature flag.
+                </LemonBanner>
+            </div>
+        </LemonModal>
     )
 }
 
@@ -491,15 +600,49 @@ export function ActionBanner(): JSX.Element {
         funnelResultsPersonsTotal,
         actualRunningTime,
         getHighestProbabilityVariant,
+        isSingleVariantShipped,
+        featureFlags,
     } = useValues(experimentLogic)
 
     const { archiveExperiment } = useActions(experimentLogic)
+
+    const { aggregationLabel } = useValues(groupsModel)
+    const aggregationTargetName =
+        experiment.filters.aggregation_group_type_index != null
+            ? aggregationLabel(experiment.filters.aggregation_group_type_index).plural
+            : 'users'
 
     const recommendedRunningTime = experiment?.parameters?.recommended_running_time || 1
     const recommendedSampleSize = experiment?.parameters?.recommended_sample_size || 100
 
     if (!experiment || experimentLoading || experimentResultsLoading) {
         return <></>
+    }
+
+    if (featureFlags[FEATURE_FLAGS.EXPERIMENT_MAKE_DECISION]) {
+        if (isSingleVariantShipped) {
+            const shippedVariant = experiment.feature_flag?.filters.multivariate?.variants.find(
+                ({ rollout_percentage }) => rollout_percentage === 100
+            )
+            if (!shippedVariant) {
+                return <></>
+            }
+
+            return (
+                <LemonBanner type="info" className="mt-4">
+                    <span className="inline-flex items-center">
+                        <span
+                            className="border rounded px-2"
+                            // eslint-disable-next-line react/forbid-dom-props
+                            style={{ backgroundColor: 'var(--bg-table)' }}
+                        >
+                            <VariantTag experimentId={experiment.id} variantKey={shippedVariant.key} />
+                        </span>
+                        &nbsp; has been released to 100% of {aggregationTargetName}.
+                    </span>
+                </LemonBanner>
+            )
+        }
     }
 
     // Draft
