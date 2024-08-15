5f431b81e9922403ce6b692235f8f68952211e37
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-08-15 17:06:54 +0200

    fix FunnelPropertyCorrelationTable stories

--- frontend/src/scenes/insights/views/Funnels/FunnelPropertyCorrelationTable.stories.tsx
+++ frontend/src/scenes/insights/views/Funnels/FunnelPropertyCorrelationTable.stories.tsx
@@ -2,12 +2,12 @@ import { Meta, StoryFn, StoryObj } from '@storybook/react'
 import { BindLogic } from 'kea'
 import { taxonomicFilterMocksDecorator } from 'lib/components/TaxonomicFilter/__mocks__/taxonomicFilterMocksDecorator'
 import { useState } from 'react'
+import { queryFromFilters } from 'scenes/insights/insightDataLogic'
 import { insightLogic } from 'scenes/insights/insightLogic'
 
 import { mswDecorator } from '~/mocks/browser'
 import funnelCorrelation from '~/mocks/fixtures/api/projects/team_id/insights/funnelCorrelation.json'
 import { dataNodeLogic, DataNodeLogicProps } from '~/queries/nodes/DataNode/dataNodeLogic'
-import { filtersToQueryNode } from '~/queries/nodes/InsightQuery/utils/filtersToQueryNode'
 import { insightVizDataNodeKey } from '~/queries/nodes/InsightViz/InsightViz'
 import { getCachedResults } from '~/queries/nodes/InsightViz/utils'
 import { InsightLogicProps } from '~/types'
@@ -36,16 +36,15 @@ const Template: StoryFn<typeof FunnelPropertyCorrelationTable> = () => {
 
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const insight = require('../../../../mocks/fixtures/api/projects/team_id/insights/funnelLeftToRight.json')
-    const filters = insight.filters
-    const cachedInsight = { ...insight, short_id: dashboardItemId, filters }
+    const query = queryFromFilters(insight.filters)
+    const cachedInsight = { ...insight, short_id: dashboardItemId, query }
 
     const insightProps = { dashboardItemId, doNotLoad: true, cachedInsight } as InsightLogicProps
-    const querySource = filtersToQueryNode(filters)
 
     const dataNodeLogicProps: DataNodeLogicProps = {
-        query: querySource,
+        query: query.source,
         key: insightVizDataNodeKey(insightProps),
-        cachedResults: getCachedResults(insightProps.cachedInsight, querySource),
+        cachedResults: getCachedResults(insightProps.cachedInsight, query.source),
         doNotLoad: insightProps.doNotLoad,
     }
 
