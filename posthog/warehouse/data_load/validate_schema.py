86996db94cfe1104fccd84ba1d1ba0ba6676e32b
Tom Owers <owerstom@gmail.com>
2024-08-16 16:00:06 +0100

    Correct the error log

--- posthog/warehouse/data_load/validate_schema.py
+++ posthog/warehouse/data_load/validate_schema.py
@@ -146,7 +146,7 @@ async def validate_schema_and_update_table(
             await sync_to_async(table_created.get_columns)()
         except Exception as e:
             if table_format == DataWarehouseTable.TableFormat.DeltaS3Wrapper:
-                logger.exception("get_columns exception with DeltaS3Wrapper format - trying Delta format", e)
+                logger.exception("get_columns exception with DeltaS3Wrapper format - trying Delta format", exc_info=e)
 
                 table_created.format = DataWarehouseTable.TableFormat.Delta
                 await sync_to_async(table_created.get_columns)()
