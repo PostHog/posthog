4824c3ce378c9cd0d762776e72d1b5df0b9ecaf0
Sandy Spicer <sandy@posthog.com>
2024-08-19 15:52:37 -0700

    fix mypy

--- posthog/hogql_queries/insights/funnels/test/test_funnel_strict_udf.py
+++ posthog/hogql_queries/insights/funnels/test/test_funnel_strict_udf.py
@@ -1,8 +1,8 @@
 from unittest.mock import Mock, patch
 
-from posthog.hogql_queries.insights.funnels.test.test_funnel_strict import TestFunnelStrictStepsBreakdown
+from posthog.hogql_queries.insights.funnels.test.test_funnel_strict import BaseTestFunnelStrictStepsBreakdown
 
 
 @patch("posthoganalytics.feature_enabled", new=Mock(return_value=True))
-class TestFunnelStrictStepsBreakdownUDF(TestFunnelStrictStepsBreakdown):
+class TestFunnelStrictStepsBreakdownUDF(BaseTestFunnelStrictStepsBreakdown):
     pass
