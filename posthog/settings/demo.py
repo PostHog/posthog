aef17ee5a906cdd88e8e665283b0ccd67590d6d6
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-08-20 15:42:18 -0700

    try decoding in the app instead of in docker

--- posthog/settings/demo.py
+++ posthog/settings/demo.py
@@ -1,3 +1,5 @@
+import base64
+
 from posthog.settings.base_variables import TEST
 from posthog.settings.utils import get_from_env
 
@@ -7,4 +9,5 @@ E2E_SAML_ORGANIZATION_DOMAIN_ID = get_from_env("E2E_SAML_ORGANIZATION_DOMAIN_ID"
 E2E_SAML_DOMAIN = get_from_env("E2E_SAML_DOMAIN", "")
 E2E_SAML_ENTITY_ID = get_from_env("E2E_SAML_ENTITY_ID", "")
 E2E_SAML_ACS_URL = get_from_env("E2E_SAML_ACS_URL", "")
-E2E_SAML_X509_CERT = get_from_env("E2E_SAML_X509_CERT", "")
+E2E_SAML_X509_CERT_BASE64 = get_from_env("E2E_SAML_X509_CERT_BASE64", "")
+E2E_SAML_X509_CERT = base64.b64decode(E2E_SAML_X509_CERT_BASE64)
