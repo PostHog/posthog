6bd4a65e5cc0d6183d4d8761952ce29c1171b54e
Paul D'Ambra <paul@posthog.com>
2024-08-20 21:58:25 +0100

    feat: make group identify props easier to find in replay

--- frontend/src/scenes/session-recordings/player/inspector/components/ItemEvent.stories.tsx
+++ frontend/src/scenes/session-recordings/player/inspector/components/ItemEvent.stories.tsx
@@ -165,3 +165,72 @@ WebVitalsEvent.args = {
         }
     ),
 }
+
+export const GroupIdentifyEvent: Story = BasicTemplate.bind({})
+GroupIdentifyEvent.args = {
+    item: makeItem(
+        {},
+        { event: '$groupidentify' },
+        {
+            $os: 'Mac OS X',
+            $os_version: '10.15.7',
+            $browser: 'Chrome',
+            $device_type: 'Desktop',
+            $current_url: 'https://us.posthog.com/project/2/insights/new',
+            $host: 'us.posthog.com',
+            $pathname: '/project/2/insights/new',
+            $raw_user_agent:
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+            $browser_version: 127,
+            $browser_language: 'en-US',
+            $screen_height: 1080,
+            $screen_width: 1920,
+            $viewport_height: 934,
+            $viewport_width: 1920,
+            $lib: 'web',
+            $lib_version: '1.157.1',
+            $insert_id: '6la51dyujzjgc19m',
+            $time: 1724165889.123,
+            distinct_id: '48vHN2rW28SvzA6D4NdgHajLOjBP1ulsI0Rr5DNas4i',
+            $device_id: '0190e039-823d-7987-9e87-3d1ec9f325b1',
+            $initial_person_info: {
+                r: '$direct',
+                u: 'https://us.posthog.com/project/2',
+            },
+            $groups: {
+                project: 'fc445b88-e2c4-488e-bb52-aa80cd7918c9',
+                organization: '4dc8564d-bd82-1065-2f40-97f7c50f67cf',
+                customer: 'cus_IK2DWsWVn2ZM16',
+                instance: 'https://us.posthog.com',
+            },
+            realm: 'cloud',
+            email_service_available: true,
+            slack_service_available: true,
+            commit_sha: '8c1d243eb5',
+            $referrer: '$direct',
+            $referring_domain: '$direct',
+            $group_type: 'instance',
+            $group_key: 'https://us.posthog.com',
+            $group_set: {
+                site_url: 'https://us.posthog.com',
+            },
+            token: 'sTMFPsFhdP1Ssg',
+            $session_id: '01917043-b2a1-7c2e-a57e-6db514bde084',
+            $window_id: '01917043-b2a1-7c2e-a57e-6db6676bb4a1',
+            $lib_custom_api_host: 'https://internal-t.posthog.com',
+            $is_identified: true,
+            $lib_rate_limit_remaining_tokens: 85.46,
+            $ip: '67.84.74.189',
+            $initial_person_info__r: '$direct',
+            $initial_person_info__u: 'https://us.posthog.com/project/2',
+            $surveys_activated__0: '0190fe51-92a0-0000-4ba3-f85f5f0ef78f',
+            $lib_version__major: 1,
+            $lib_version__minor: 157,
+            $lib_version__patch: 1,
+            $group_2: 'fc445b88-e2c4-488e-bb52-aa80cd7918c9',
+            $group_0: '4dc8564d-bd82-1065-2f40-97f7c50f67cf',
+            $group_3: 'cus_IK2DWsWVn2ZM16',
+            $group_1: 'https://us.posthog.com',
+        }
+    ),
+}
