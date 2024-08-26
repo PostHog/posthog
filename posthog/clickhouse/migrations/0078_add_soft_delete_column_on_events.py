e99af8b09de4c07a74a706b7408aaa1989cd9a05
James Greenhill <fuziontech@gmail.com>
2024-08-26 14:23:12 -0700

    Add the column to the events table as well

--- posthog/clickhouse/migrations/0078_add_soft_delete_column_on_events.py
+++ posthog/clickhouse/migrations/0078_add_soft_delete_column_on_events.py
@@ -4,7 +4,7 @@ from posthog.clickhouse.client.connection import ch_pool
 from posthog.settings import CLICKHOUSE_CLUSTER
 
 
-DROP_COLUMNS_SHARDED_EVENTS = """
+DROP_COLUMNS_EVENTS = """
 ALTER TABLE {table} ON CLUSTER {cluster}
 DROP COLUMN IF EXISTS is_deleted
 """
@@ -12,6 +12,11 @@ DROP COLUMN IF EXISTS is_deleted
 ADD_COLUMNS_SHARDED_EVENTS = """
 ALTER TABLE {table} ON CLUSTER {cluster}
 ADD COLUMN IF NOT EXISTS is_deleted DEFAULT Boolean False
+"""
+
+ADD_COLUMNS_EVENTS = """
+ALTER TABLE {table} ON CLUSTER {cluster}
+ADD COLUMN IF NOT EXISTS is_deleted
 """
 
 ADD_COLUMNS_INDEX_EVENTS = """
@@ -22,8 +27,10 @@ ADD INDEX IF NOT EXISTS is_deleted_idx (is_deleted) TYPE minmax GRANULARITY 1
 
 def add_columns_to_required_tables(_):
     with ch_pool.get_client() as client:
-        client.execute(DROP_COLUMNS_SHARDED_EVENTS.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
+        client.execute(DROP_COLUMNS_EVENTS.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
+        client.execute(DROP_COLUMNS_EVENTS.format(table="events", cluster=CLICKHOUSE_CLUSTER))
         client.execute(ADD_COLUMNS_SHARDED_EVENTS.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
+        client.execute(ADD_COLUMNS_EVENTS.format(table="events", cluster=CLICKHOUSE_CLUSTER))
         client.execute(ADD_COLUMNS_INDEX_EVENTS.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER))
 
 
