076e618d1b99e17fd2f9935d00d798779fb262c0
Steven Shults <227448+slshults@users.noreply.github.com>
2024-08-27 12:13:19 -0700

    Update frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelSupport.tsx
    Co-authored-by: Raquel Smith <raquelmsmith@users.noreply.github.com>

--- frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelSupport.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/panels/SidePanelSupport.tsx
@@ -123,7 +123,7 @@ const SupportFormBlock = ({ onCancel }: { onCancel: () => void }): JSX.Element =
                 Cancel
             </LemonButton>
             <br />
-            <div className="grid grid-cols-2 border rounded [&_>*]:px-2 [&_>*]:py-0.5 mb-4 bg-bg-light">
+            <div className="grid grid-cols-2 border rounded [&_>*]:px-2 [&_>*]:py-0.5 mb-4 bg-bg-light pt-4">
                 <div className="col-span-full flex justify-between border-b bg-bg-white py-1">
                     <div>
                         <strong>Avg support response times</strong>
