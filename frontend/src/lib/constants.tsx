0a6ab2622fd9404afdc60100fef54c3c112ea379
Robbie <robbie.coomber@gmail.com>
2024-08-30 21:40:03 +0100

    feat(web-analytics): Add last click exit url (#24725)

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -211,6 +211,7 @@ export const FEATURE_FLAGS = {
     BATCH_EXPORTS_POSTHOG_HTTP: 'posthog-http-batch-exports',
     EXPERIMENT_MAKE_DECISION: 'experiment-make-decision', // owner: @jurajmajerik #team-feature-success
     WEB_ANALYTICS_CONVERSION_GOALS: 'web-analytics-conversion-goals', // owner: @robbie-c
+    WEB_ANALYTICS_LAST_CLICK: 'web-analytics-last-click', // owner: @robbie-c
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
