bb2b3e5eabe7adb4166f1138556283ede758ec10
Ben White <ben@posthog.com>
2024-09-04 15:29:54 +0200

    Fix tests

--- plugin-server/tests/cdp/cdp-processed-events-consumer.test.ts
+++ plugin-server/tests/cdp/cdp-processed-events-consumer.test.ts
@@ -166,7 +166,7 @@ describe('CDP Processed Events Consumer', () => {
                     matchInvocation(fnPrinterPageviewFilters, globals),
                 ])
 
-                expect(mockProducer.produce).toHaveBeenCalledTimes(2)
+                expect(mockProducer.produce).toHaveBeenCalledTimes(11)
 
                 expect(decodeAllKafkaMessages()).toMatchObject([
                     {
@@ -256,46 +256,7 @@ describe('CDP Processed Events Consumer', () => {
             })
         })
 
-        describe('kafka parsing', () => {
-            it('can parse incoming messages correctly', async () => {
-                await insertHogFunction({
-                    ...HOG_EXAMPLES.simple_fetch,
-                    ...HOG_INPUTS_EXAMPLES.simple_fetch,
-                    ...HOG_FILTERS_EXAMPLES.no_filters,
-                })
-                // Create a message that should be processed by this function
-                // Run the function and check that it was executed
-                await processor._handleKafkaBatch([
-                    createMessage(
-                        createIncomingEvent(team.id, {
-                            uuid: 'b3a1fe86-b10c-43cc-acaf-d208977608d0',
-                            event: '$pageview',
-                            properties: JSON.stringify({
-                                $lib_version: '1.0.0',
-                            }),
-                        })
-                    ),
-                ])
-
-                // Generall check that the message seemed to get processed
-                expect(decodeAllKafkaMessages()).toMatchObject([
-                    {
-                        key: expect.any(String),
-                        topic: 'cdp_function_callbacks_test',
-                        value: {
-                            state: expect.any(String),
-                        },
-                        waitForAck: true,
-                    },
-                ])
-            })
-        })
-
         describe('no delayed execution', () => {
-            // beforeEach(() => {
-            //     hub.CDP_EVENT_PROCESSOR_EXECUTE_FIRST_STEP = true
-            // })
-
             it('should invoke the initial function before enqueuing', async () => {
                 await insertHogFunction({
                     ...HOG_EXAMPLES.simple_fetch,
