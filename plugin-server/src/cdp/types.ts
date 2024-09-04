b715d7c62e61e93ea294e153e697145657a806e6
Ben White <ben@posthog.com>
2024-09-04 12:24:35 +0200

    fix(cdp): Parsing of function callback (#24774)

--- plugin-server/src/cdp/types.ts
+++ plugin-server/src/cdp/types.ts
@@ -219,7 +219,8 @@ export type HogHooksFetchResponse = {
 
 export type HogFunctionInvocationSerialized = Omit<HogFunctionInvocation, 'hogFunction'> & {
     // When serialized to kafka / cyclotron we only store the ID
-    hogFunctionId: HogFunctionType['id']
+    hogFunctionId?: HogFunctionType['id']
+    hogFunction?: HogFunctionType
 }
 
 export type HogFunctionInvocationSerializedCompressed = {
