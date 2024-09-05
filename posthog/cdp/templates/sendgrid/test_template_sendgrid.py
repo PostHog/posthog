e03ac8524eaae53df98b184cca2ce6e67241c872
Marius Andra <marius.andra@gmail.com>
2024-09-05 17:01:31 +0200

    add test

--- posthog/cdp/templates/sendgrid/test_template_sendgrid.py
+++ posthog/cdp/templates/sendgrid/test_template_sendgrid.py
@@ -47,3 +47,45 @@ class TestTemplateSendgrid(BaseHogFunctionTemplateTest):
         assert self.get_mock_fetch_calls()[0][1]["body"]["contacts"] == snapshot(
             [{"email": "example@posthog.com", "last_name": "included"}]
         )
+
+    def test_function_adds_custom_fields(self):
+        self.mock_fetch_response = lambda *args: {
+            "status": 200,
+            "body": {"custom_fields": [{"id": "id7", "name": "custom_field"}]},
+        }
+
+        res = self.run_function(
+            inputs=self._inputs(
+                custom_fields={"custom_field": "custom_value"},
+            )
+        )
+        assert res.result is None
+
+        assert self.get_mock_fetch_calls()[0] == snapshot(
+            (
+                "https://api.sendgrid.com/v3/marketing/field_definitions",
+                {
+                    "method": "GET",
+                    "headers": {"Authorization": "Bearer API_KEY", "Content-Type": "application/json"},
+                },
+            )
+        )
+
+        assert self.get_mock_fetch_calls()[1] == snapshot(
+            (
+                "https://api.sendgrid.com/v3/marketing/contacts",
+                {
+                    "method": "PUT",
+                    "headers": {"Authorization": "Bearer API_KEY", "Content-Type": "application/json"},
+                    "body": {
+                        "contacts": [
+                            {
+                                "email": "example@posthog.com",
+                                "last_name": "example",
+                                "custom_fields": {"id7": "custom_value"},
+                            }
+                        ]
+                    },
+                },
+            )
+        )
