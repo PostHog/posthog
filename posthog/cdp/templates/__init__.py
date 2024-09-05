bbdc69ba034efd6b24e9eb7a313b3889ce7eb3da
Ben White <ben@posthog.com>
2024-09-05 11:56:58 +0200

    feat(cdp): Add migration for Intercom plugin (#24799)

--- posthog/cdp/templates/__init__.py
+++ posthog/cdp/templates/__init__.py
@@ -2,7 +2,7 @@ from .webhook.template_webhook import template as webhook
 from .slack.template_slack import template as slack
 from .hubspot.template_hubspot import template as hubspot
 from .customerio.template_customerio import template as customerio, TemplateCustomerioMigrator
-from .intercom.template_intercom import template as intercom
+from .intercom.template_intercom import template as intercom, TemplateIntercomMigrator
 from .sendgrid.template_sendgrid import template as sendgrid
 from .clearbit.template_clearbit import template as clearbit
 from .posthog.template_posthog import template as posthog
@@ -39,6 +39,7 @@ HOG_FUNCTION_TEMPLATES_BY_ID = {template.id: template for template in HOG_FUNCTI
 
 HOG_FUNCTION_MIGRATORS = {
     TemplateCustomerioMigrator.plugin_url: TemplateCustomerioMigrator,
+    TemplateIntercomMigrator.plugin_url: TemplateIntercomMigrator,
 }
 
 __all__ = ["HOG_FUNCTION_TEMPLATES", "HOG_FUNCTION_TEMPLATES_BY_ID"]
