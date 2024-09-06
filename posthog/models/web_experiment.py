d7a88d2a8c221541ab72b8638dbe1662e58c0d10
Phani Raj <phani@posthog.com>
2024-09-05 21:54:10 -0500

    Add comment about the web_experiment.py model

--- posthog/models/web_experiment.py
+++ posthog/models/web_experiment.py
@@ -2,27 +2,60 @@ import json
 
 from django.db import models
 from posthog.models import Experiment
-# from posthog.models.experiment import ExperimentManager
 
 
 class WebExperimentManager(models.Manager):
     def get_queryset(self):
-        return super(WebExperimentManager, self).get_queryset().filter(
-            type='web')
+        return super().get_queryset().filter(type="web")
+
 
 class WebExperiment(Experiment):
     objects = WebExperimentManager()
-    # variants = models.JSONField(null=True, blank=True)
 
     class Meta:
         proxy = True
 
+    # Converts the `payload` property of a feature flag's filter to something that is readily usable
+    # by the web-experiments.ts file in posthog-js
+    # Before :
+    # {'groups': [{'properties': [], 'rollout_percentage': 100}],
+    #   'payloads': {
+    #     'variant #0': '{"data": [{"text": "Save Me", "html": "", "selector": "#page > #body > .header .more"}]}',
+    #     'variant #1': '{"data": [{"text": "Submit", "html": "", "selector": "#page > #body > .header .more"}]}'},
+    #
+    #     'multivariate': {
+    #       'variants': [
+    #         {'key': 'variant #0', 'rollout_percentage': 50},
+    #         {'key': 'variant #1', 'rollout_percentage': 50}
+    #       ]
+    #     }
+    # }
+    # After :
+    # "variants": {
+    #     "variant #0": {
+    #         "transforms": [
+    #             {
+    #                 "text": "Save me",
+    #                 "html": "",
+    #                 "selector": "##page > #body > .header .more"
+    #             }
+    #         ],
+    #         "rollout_percentage": 50
+    #     },
+    #     "variant #1": {
+    #         "transforms": [
+    #             {
+    #                 "text": "Submit",
+    #                 "html": "",
+    #                 "selector": "##page > #body > .header .more"
+    #             }
+    #         ],
+    #         "rollout_percentage": 50
+    #     }
+    # }
     def variants(self):
         experiment = self
-        if experiment.feature_flag is None:
-            return
-
-        if experiment.feature_flag.filters is None:
+        if experiment.feature_flag is None or experiment.feature_flag.filters is None:
             return
 
         multivariate = experiment.feature_flag.filters.get("multivariate", None)
@@ -43,17 +76,12 @@ class WebExperiment(Experiment):
         for variant in variants:
             rollout_percentage = variant.get("rollout_percentage", 0)
             key = variant.get("key", None)
-            # print('variant is ', key, '  rollout_percentage is ', rollout_percentage, '  payload is ', payloads)
-            serializer_payload = {
-                'transforms': None,
-                'rollout_percentage': rollout_percentage
-            }
+            serializer_payload = {"transforms": None, "rollout_percentage": rollout_percentage}
             data = payloads.get(key, None)
             if data is not None:
-                serializer_payload['transforms'] = json.loads(data).get("data", {})
+                # The payload of a variant is serialized into a string
+                # We need to unmarshal it back into a JSON dict to send it to the client.
+                serializer_payload["transforms"] = json.loads(data).get("data", {})
 
             payloads[key] = serializer_payload
         return payloads
-
-    # def update_payloads(self, payloads):
-
