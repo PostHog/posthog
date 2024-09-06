5fa2c25793c9c03a6c39b5f024e6682d0023810c
David Newell <david@posthog.com>
2024-09-06 11:55:28 +0100

    move ordering

--- frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylistSettings.tsx
+++ frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylistSettings.tsx
@@ -1,14 +1,11 @@
-import { LemonSelect, LemonSwitch } from '@posthog/lemon-ui'
+import { LemonSwitch } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
 
 import { playerSettingsLogic } from '../player/playerSettingsLogic'
-import { sessionRecordingsPlaylistLogic } from './sessionRecordingsPlaylistLogic'
 
 export function SessionRecordingsPlaylistSettings(): JSX.Element {
     const { hideViewedRecordings } = useValues(playerSettingsLogic)
     const { setHideViewedRecordings } = useActions(playerSettingsLogic)
-    const { orderBy } = useValues(sessionRecordingsPlaylistLogic)
-    const { setOrderBy } = useActions(sessionRecordingsPlaylistLogic)
 
     return (
         <div className="relative flex flex-col gap-2 p-3 border-b">
@@ -18,58 +15,6 @@ export function SessionRecordingsPlaylistSettings(): JSX.Element {
                     aria-label="Autoplay next recording"
                     checked={hideViewedRecordings}
                     onChange={() => setHideViewedRecordings(!hideViewedRecordings)}
-                />
-            </div>
-            <div className="flex flex-row items-center justify-between space-x-2">
-                <span className="text-black font-medium">Order by</span>
-                <LemonSelect
-                    options={[
-                        {
-                            value: 'start_time',
-                            label: 'Latest',
-                        },
-                        {
-                            label: 'Longest',
-                            options: [
-                                {
-                                    value: 'duration',
-                                    label: 'Total duration',
-                                },
-                                {
-                                    value: 'active_seconds',
-                                    label: 'Active duration',
-                                },
-                                {
-                                    value: 'inactive_seconds',
-                                    label: 'Inactive duration',
-                                },
-                            ],
-                        },
-                        {
-                            label: 'Most active',
-                            options: [
-                                {
-                                    value: 'click_count',
-                                    label: 'Clicks',
-                                },
-                                {
-                                    value: 'keypress_count',
-                                    label: 'Key presses',
-                                },
-                                {
-                                    value: 'mouse_activity_count',
-                                    label: 'Mouse activity',
-                                },
-                            ],
-                        },
-                        {
-                            value: 'console_error_count',
-                            label: 'Most errors',
-                        },
-                    ]}
-                    size="small"
-                    value={orderBy}
-                    onChange={setOrderBy}
                 />
             </div>
         </div>
