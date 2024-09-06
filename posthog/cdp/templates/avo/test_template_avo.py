04b4afd3c0f9d4f759aca606c3dcab8251a4fdd4
MarconLP <13001502+MarconLP@users.noreply.github.com>
2024-09-06 13:26:44 +0200

    update tests

--- posthog/cdp/templates/avo/test_template_avo.py
+++ posthog/cdp/templates/avo/test_template_avo.py
@@ -16,9 +16,12 @@ class TestTemplateAvo(BaseHogFunctionTemplateTest):
             inputs=self._inputs(),
             globals={
                 "event": {
+                    "uuid": "0191c693-d93b-7516-b1e3-64ec33c96464",
                     "distinct_id": "66e614bd-d9f2-491e-9e2c-eeab3090f72f",
                     "name": "sign up",
                     "properties": {
+                        "distinct_id": "66e614bd-d9f2-491e-9e2c-eeab3090f72f",
+                        "token": "phc_ex7Mnvi4DqeB6xSQoXU1UVPzAmUIpicMFKELQXGGTYQO",
                         "bob": {"name": "bob"},
                         "age": 99,
                         "name": "bob",
@@ -35,18 +38,22 @@ class TestTemplateAvo(BaseHogFunctionTemplateTest):
         )
 
         res = self.get_mock_fetch_calls()[0]
+        res[1]["body"][0]["sessionId"] = "4d4454b4-31bb-4b13-8167-4ec76a0f49b6"
+        res[1]["body"][0]["createdAt"] = "2024-09-06T09:04:28.324Z"
         assert res == snapshot(
             (
                 "https://api.avo.app/inspector/posthog/v1/track",
                 {
                     "method": "POST",
                     "headers": {
-                        "Content-Type": "application/json",
-                        "Authorization": "Bearer NnBd7B55ZXC6o0Kh20pE",
+                        "env": "dev",
+                        "api-key": "NnBd7B55ZXC6o0Kh20pE",
+                        "content-type": "application/json",
+                        "accept": "application/json",
                     },
                     "body": [
                         {
-                            "apiKey": "test",
+                            "apiKey": "NnBd7B55ZXC6o0Kh20pE",
                             "env": "dev",
                             "appName": "PostHog",
                             "sessionId": "4d4454b4-31bb-4b13-8167-4ec76a0f49b6",
@@ -64,6 +71,7 @@ class TestTemplateAvo(BaseHogFunctionTemplateTest):
                             "messageId": "0191c693-d93b-7516-b1e3-64ec33c96464",
                             "eventProperties": [
                                 {"propertyName": "distinct_id", "propertyType": "string"},
+                                {"propertyName": "token", "propertyType": "string"},
                                 {"propertyName": "bob", "propertyType": "object"},
                                 {"propertyName": "age", "propertyType": "int"},
                                 {"propertyName": "name", "propertyType": "string"},
@@ -71,7 +79,6 @@ class TestTemplateAvo(BaseHogFunctionTemplateTest):
                                 {"propertyName": "job", "propertyType": "boolean"},
                                 {"propertyName": "noop", "propertyType": "null"},
                                 {"propertyName": "test", "propertyType": "float"},
-                                {"propertyName": "token", "propertyType": "string"},
                             ],
                         }
                     ],
@@ -79,20 +86,22 @@ class TestTemplateAvo(BaseHogFunctionTemplateTest):
             )
         )
 
-    # def test_automatic_action_mapping(self):
-    #     for event_name, expected_action in [
-    #         ("$identify", "$identify"),
-    #         ("$set", "$identify"),
-    #         ("$pageview", "$pageview"),
-    #         ("$create_alias", "$create_alias"),
-    #         ("$autocapture", "$autocapture"),
-    #         ("custom", "custom"),
-    #     ]:
-    #         self.run_function(
-    #             inputs=self._inputs(),
-    #             globals={
-    #                 "event": {"name": event_name, "properties": {"url": "https://example.com", "$browser": "Chrome"}},
-    #             },
-    #         )
-    #
-    #         assert self.get_mock_fetch_calls()[0][1]["body"]["eventName"] == expected_action
+    def test_automatic_type_mapping(self):
+        for property_value, expected_type in [
+            (None, "null"),
+            ("Bob", "string"),
+            (99, "int"),
+            (1.4, "float"),
+            (True, "boolean"),
+            ({"name": "Bob"}, "object"),
+            ([1, 2, 3], "list"),
+        ]:
+            self.run_function(
+                inputs=self._inputs(),
+                globals={
+                    "event": {"name": "sign up", "properties": {"test": property_value}},
+                },
+            )
+
+            res = self.get_mock_fetch_calls()[0]
+            assert res[1]["body"][0]["eventProperties"][0]["propertyType"] == expected_type
