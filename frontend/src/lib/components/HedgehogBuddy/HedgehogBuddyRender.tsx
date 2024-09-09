b1c688c9122f568a9e47ef01d5970b1571f8ee0a
Ben White <ben@posthog.com>
2024-09-09 17:14:27 +0200

    feat: Hedgehog mode skins (#24838)

--- frontend/src/lib/components/HedgehogBuddy/HedgehogBuddyRender.tsx
+++ frontend/src/lib/components/HedgehogBuddy/HedgehogBuddyRender.tsx
@@ -1,54 +1,21 @@
-import { useEffect, useRef, useState } from 'react'
-
 import { HedgehogConfig } from '~/types'
 
-import { FPS, X_FRAMES } from './HedgehogBuddy'
 import { COLOR_TO_FILTER_MAP } from './hedgehogBuddyLogic'
-import {
-    baseSpriteAccessoriesPath,
-    baseSpritePath,
-    SPRITE_SIZE,
-    standardAccessories,
-    standardAnimations,
-} from './sprites/sprites'
+import { spriteAccessoryUrl, spriteUrl, standardAccessories } from './sprites/sprites'
 
-export type HedgehogBuddyStaticProps = Partial<HedgehogConfig> & { size?: number | string; waveOnAppearance?: boolean }
+export type HedgehogBuddyStaticProps = Partial<HedgehogConfig> & { size?: number | string }
 
 // Takes a range of options and renders a static hedgehog
 export function HedgehogBuddyStatic({
     accessories,
     color,
     size,
-    waveOnAppearance,
+    skin = 'default',
 }: HedgehogBuddyStaticProps): JSX.Element {
     const imgSize = size ?? 60
 
     const accessoryInfos = accessories?.map((x) => standardAccessories[x])
     const filter = color ? COLOR_TO_FILTER_MAP[color] : null
-
-    const [animationIteration, setAnimationIteration] = useState(waveOnAppearance ? 1 : 0)
-    const [_, setTimerLoop] = useState(0)
-    const animationFrameRef = useRef(0)
-
-    useEffect(() => {
-        if (animationIteration) {
-            setTimerLoop(0)
-            let timer: any = null
-            const loop = (): void => {
-                if (animationFrameRef.current < standardAnimations.wave.frames) {
-                    animationFrameRef.current++
-                    timer = setTimeout(loop, 1000 / FPS)
-                } else {
-                    animationFrameRef.current = 0
-                }
-                setTimerLoop((x) => x + 1)
-            }
-            loop()
-            return () => {
-                clearTimeout(timer)
-            }
-        }
-    }, [animationIteration])
 
     return (
         <div
@@ -59,24 +26,20 @@ export function HedgehogBuddyStatic({
                 height: imgSize,
                 margin: -2,
             }}
-            onClick={waveOnAppearance ? () => setAnimationIteration((x) => x + 1) : undefined}
         >
             <div
                 className="object-cover absolute inset-0 image-pixelated size-[400%] bg-cover"
                 // eslint-disable-next-line react/forbid-dom-props
                 style={{
                     filter: filter as any,
-                    backgroundImage: `url(${baseSpritePath()}/wave.png)`,
-                    backgroundPosition: `-${((animationFrameRef.current - 1) % X_FRAMES) * SPRITE_SIZE}px -${
-                        Math.floor((animationFrameRef.current - 1) / X_FRAMES) * SPRITE_SIZE
-                    }px`,
+                    backgroundImage: `url(${spriteUrl(skin, 'wave')})`,
                 }}
             />
 
             {accessoryInfos?.map((accessory, index) => (
                 <img
                     key={index}
-                    src={`${baseSpriteAccessoriesPath()}/${accessory.img}.png`}
+                    src={`${spriteAccessoryUrl(accessory.img)}`}
                     className="object-cover absolute inset-0 image-pixelated pointer-events-none"
                     // eslint-disable-next-line react/forbid-dom-props
                     style={{
