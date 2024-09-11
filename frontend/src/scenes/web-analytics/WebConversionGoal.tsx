a8a425e9f610d21367ca56fbd924cb5cddcc821b
Robbie Coomber <robbie.coomber@gmail.com>
2024-09-11 12:24:04 +0100

    Put conversion goals picker behind FF

--- frontend/src/scenes/web-analytics/WebConversionGoal.tsx
+++ frontend/src/scenes/web-analytics/WebConversionGoal.tsx
@@ -1,14 +1,21 @@
 import { useActions, useValues } from 'kea'
 import { TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
 import { TaxonomicPopover } from 'lib/components/TaxonomicPopover/TaxonomicPopover'
+import { FEATURE_FLAGS } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { webAnalyticsLogic } from 'scenes/web-analytics/webAnalyticsLogic'
 
 import { actionsModel } from '~/models/actionsModel'
 
-export const WebConversionGoal = (): JSX.Element => {
+export const WebConversionGoal = (): JSX.Element | null => {
     const { conversionGoal } = useValues(webAnalyticsLogic)
+    const { featureFlags } = useValues(featureFlagLogic)
     const { setConversionGoal } = useActions(webAnalyticsLogic)
     const { actions } = useValues(actionsModel)
+
+    if (!featureFlags[FEATURE_FLAGS.WEB_ANALYTICS_CONVERSION_GOALS] && !conversionGoal) {
+        return null
+    }
     return (
         <TaxonomicPopover<number>
             data-attr="web-analytics-conversion-filter"
