22bb246f9af72b6fdd4406564989fd4360971fb1
David Newell <david@posthog.com>
2024-09-12 15:00:53 +0100

    tests

--- frontend/src/scenes/session-recordings/player/rrweb/index.ts
+++ frontend/src/scenes/session-recordings/player/rrweb/index.ts
@@ -1,4 +1,4 @@
-import { eventWithTime } from 'rrweb'
+import { EventType, eventWithTime, IncrementalSource } from 'rrweb'
 import { playerConfig, ReplayPlugin } from 'rrweb/typings/types'
 
 export const PLACEHOLDER_SVG_DATA_IMAGE_URL =
@@ -67,13 +67,56 @@ export const CorsPlugin: ReplayPlugin & {
     },
 }
 
-export const WindowTitlePlugin: ReplayPlugin = (cb: Record<string, string>) => {
-    return {
-        handler: async (e: eventWithTime) => {
-            const windowTitle = getTitle(e)
+export type Node = {
+    id: number
+    type: number
+    tagName: string
+    childNodes: Node[]
+    textContent?: string
+}
 
-            if (windowTitle) {
-                cb(e.data.window_id, windowTitle)
+export const WindowTitlePlugin = (cb: (windowId: string, title: string) => void): ReplayPlugin => {
+    const titleElementIds = new Set<number>()
+
+    const extractTitleTextEl = (node: Node): Node | undefined => {
+        // Document node
+        if (node.type === 0) {
+            const el = node.childNodes.find((n) => n.type === 2) // element node
+
+            if (el) {
+                const headEl = el.childNodes.filter((n) => n.type === 2).find((n) => n.tagName === 'head')
+
+                if (headEl) {
+                    const titleEl = headEl.childNodes.filter((n) => n.type === 2).find((n) => n.tagName === 'title')
+
+                    if (titleEl) {
+                        const textEl = titleEl.childNodes.find((n) => n.type === 3) // text node
+                        return textEl
+                    }
+                }
+            }
+        }
+    }
+
+    return {
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        handler: async (e: eventWithTime, isSync) => {
+            if ('windowId' in e && e.windowId && isSync) {
+                const windowId = e.windowId as string
+                if (e.type === EventType.FullSnapshot) {
+                    titleElementIds.clear()
+                    const el = extractTitleTextEl(e.data.node as Node)
+                    if (windowId && el && el.textContent) {
+                        titleElementIds.add(el.id)
+                        cb(windowId, el.textContent)
+                    }
+                } else if (e.type === EventType.IncrementalSnapshot && e.data.source === IncrementalSource.Mutation) {
+                    e.data.texts.forEach(({ id, value }) => {
+                        if (titleElementIds.has(id) && value) {
+                            cb(windowId, value)
+                        }
+                    })
+                }
             }
         },
     }
