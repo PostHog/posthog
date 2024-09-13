ed8d32e9de4bf88eed27e85ab8a5b1e38c335e0a
David Newell <david@posthog.com>
2024-09-13 18:14:11 +0100

    debug summary

--- frontend/src/scenes/session-recordings/player/sidebar/PlayerSidebarDebuggerTab.tsx
+++ frontend/src/scenes/session-recordings/player/sidebar/PlayerSidebarDebuggerTab.tsx
@@ -2,10 +2,40 @@ import { LemonButton, LemonCollapse } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
 import { JSONViewer } from 'lib/components/JSONViewer'
 import { IconChevronLeft, IconChevronRight } from 'lib/lemon-ui/icons'
-import { useEffect } from 'react'
-import { eventWithTime } from 'rrweb'
+import { useEffect, useState } from 'react'
+import { EventType, eventWithTime } from 'rrweb'
 
 import { sessionRecordingPlayerLogic } from '../sessionRecordingPlayerLogic'
+
+const snapshotTypes = {
+    0: 'DomContentLoaded',
+    1: 'Load',
+    2: 'FullSnapshot',
+    3: 'IncrementalSnapshot',
+    4: 'Meta',
+    5: 'Custom',
+    6: 'Plugin',
+}
+
+const incrementalSource = {
+    0: 'Mutation',
+    1: 'MouseMove',
+    2: 'MouseInteraction',
+    3: 'Scroll',
+    4: 'ViewportResize',
+    5: 'Input',
+    6: 'TouchMove',
+    7: 'MediaInteraction',
+    8: 'StyleSheetRule',
+    9: 'CanvasMutation',
+    10: 'Font',
+    11: 'Log',
+    12: 'Drag',
+    13: 'StyleDeclaration',
+    14: 'Selection',
+    15: 'AdoptedStyleSheet',
+    16: 'CustomElement',
+}
 
 export function PlayerSidebarDebuggerTab(): JSX.Element {
     const { debugSnapshots } = useValues(sessionRecordingPlayerLogic)
@@ -59,21 +89,45 @@ export function PlayerSidebarDebuggerTab(): JSX.Element {
                         {
                             key: 'previous',
                             header: 'Previous snapshot',
-                            content: previous && <JSONViewer src={previous} />,
+                            content: <Snapshot description="previous" snapshot={previous} />,
                         },
                         {
                             key: 'current',
                             header: 'Current snapshot',
-                            content: <JSONViewer src={current} />,
+                            content: <Snapshot snapshot={current} />,
                         },
                         {
                             key: 'next',
                             header: 'Next snapshot',
-                            content: next && <JSONViewer src={next} />,
+                            content: <Snapshot description="next" snapshot={next} />,
                         },
                     ]}
                 />
             </div>
+        </div>
+    )
+}
+
+const Snapshot = ({ snapshot, description }: { snapshot: eventWithTime | null; description?: string }): JSX.Element => {
+    const [open, setOpen] = useState<boolean>(false)
+    if (!snapshot) {
+        return <span>This is no {description} snapshot</span>
+    }
+
+    const snapshotSummary: Record<string, any> = {
+        'formatted time': new Date(snapshot.timestamp).toISOString(),
+        type: snapshotTypes[snapshot.type],
+    }
+
+    if (snapshot.type === EventType.IncrementalSnapshot) {
+        snapshotSummary['source'] = incrementalSource[snapshot.data.source]
+    }
+
+    return (
+        <div>
+            <JSONViewer src={snapshotSummary} />
+            <LemonButton onClick={() => setOpen(!open)}>Show snapshot</LemonButton>
+            {open && <JSONViewer src={snapshot} />}
         </div>
     )
 }
