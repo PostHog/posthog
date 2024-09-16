0316c190e8e33342dce71e3b0d1599cd2622e7a5
Ben White <ben@posthog.com>
2024-09-16 16:27:00 +0200

    Fix

--- plugin-server/tests/cdp/hog-function-manager.test.ts
+++ plugin-server/tests/cdp/hog-function-manager.test.ts
@@ -113,7 +113,7 @@ describe('HogFunctionManager', () => {
                         value: integrations[0].id,
                     },
                 },
-                encrpyted_inputs: null,
+                encrypted_inputs: null,
                 masking: null,
                 depends_on_integration_ids: new Set([integrations[0].id]),
             },
