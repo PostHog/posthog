294edabcc1d09d84a62ad385cea3b5fff62a68b4
MarconLP <13001502+MarconLP@users.noreply.github.com>
2024-09-17 12:08:10 +0200

    add activecampagin hog function

---
+++ posthog/cdp/templates/activecampaign/template_activecampaign.py
@@ -0,0 +1,121 @@
+from posthog.cdp.templates.hog_function_template import HogFunctionTemplate
+
+
+template: HogFunctionTemplate = HogFunctionTemplate(
+    status="beta",
+    id="template-activecampaign",
+    name="Create ActiveCampaign contact",
+    description="Creates a new contact in ActiveCampaign whenever an event is triggered.",
+    icon_url="/static/services/activecampaign.png",
+    hog="""
+if (empty(inputs.email)) {
+    print('`email` input is empty. Not creating a contact.')
+    return
+}
+
+let body := {
+    'email': inputs.email,
+    'firstName': inputs.firstName,
+    'lastName': inputs.lastName,
+    'phone': inputs.phone,
+    'fieldValues': [],
+}
+
+for (let key, value in inputs.attributes) {
+    if (not empty(value)) {
+        attributes[key] := value
+    }
+}
+
+let res := fetch(f'https://{accountName}.api-us1.com/api/3/contact/sync', {
+    'method': 'POST',
+    'headers': {
+        'content-type': 'application/json/
+        'Api-Token': {inputs.apiKey}
+    },
+    'body': body
+})
+
+if (res.status >= 400) {
+    print(f'Error from {accountName}.api-us1.com api:', res.status, res.body)
+} else {
+    print('Contact has been created or updated successfully!')
+}
+""".strip(),
+    inputs_schema=[
+        {
+            "key": "accountName",
+            "type": "string",
+            "label": "Account name",
+            "description": "Usually in the form of <accountName>.activehosted.com. You can use this page to figure our your account name: https://www.activecampaign.com/login/lookup.php",
+            "default": "",
+            "secret": False,
+            "required": True,
+        },
+        {
+            "key": "apiKey",
+            "type": "string",
+            "label": "Your ActiveCampaign API Key",
+            "description": "See the docs here: https://help.activecampaign.com/hc/en-us/articles/207317590-Getting-started-with-the-API#h_01HJ6REM2YQW19KYPB189726ST",
+            "default": "",
+            "secret": True,
+            "required": True,
+        },
+        {
+            "key": "email",
+            "type": "string",
+            "label": "Email of the user",
+            "description": "Where to find the email for the contact to be created. You can use the filters section to filter out unwanted emails or internal users.",
+            "default": "{person.properties.email}",
+            "secret": False,
+            "required": True,
+        },
+        {
+            "key": "firstName",
+            "type": "string",
+            "label": "First name of the user",
+            "description": "Where to find the first name for the contact to be created.",
+            "default": "{person.properties.firstname}",
+            "secret": False,
+            "required": True,
+        },
+        {
+            "key": "lastName",
+            "type": "string",
+            "label": "Last name of the user",
+            "description": "Where to find the last name for the contact to be created.",
+            "default": "{person.properties.lastName}",
+            "secret": False,
+            "required": True,
+        },
+        {
+            "key": "phone",
+            "type": "string",
+            "label": "Phone number of the user",
+            "description": "Where to find the phone number for the contact to be created.",
+            "default": "{person.properties.phone}",
+            "secret": False,
+            "required": True,
+        },
+        {
+            "key": "attributes",
+            "type": "dictionary",
+            "label": "Additional person fields",
+            "description": "Map any values to ActiveCampaign person fields. (fieldId:value)",
+            "default": {
+                "1": "{person.properties.company}",
+                "2": "{person.properties.website}",
+            },
+            "secret": False,
+            "required": True,
+        },
+    ],
+    filters={
+        "events": [
+            {"id": "$identify", "name": "$identify", "type": "events", "order": 0},
+            {"id": "$set", "name": "$set", "type": "events", "order": 1},
+        ],
+        "actions": [],
+        "filter_test_accounts": True,
+    },
+)
