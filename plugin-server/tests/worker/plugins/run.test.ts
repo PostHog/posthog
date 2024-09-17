baacea00c0f1ef841f82d2fac12f5591a1c5d320
Ben White <ben@posthog.com>
2024-09-17 13:50:21 +0200

    Fix

--- plugin-server/tests/worker/plugins/run.test.ts
+++ plugin-server/tests/worker/plugins/run.test.ts
@@ -1,6 +1,5 @@
 import { buildIntegerMatcher } from '../../../src/config/config'
 import { Hub, ISOTimestamp, PluginConfig, PostIngestionEvent } from '../../../src/types'
-import { processError } from '../../../src/utils/db/error'
 import { ActionMatcher } from '../../../src/worker/ingestion/action-matcher'
 import { runComposeWebhook, runOnEvent } from '../../../src/worker/plugins/run'
 
