187278153eb89d2f226d49c64be1aa71f1e46a0d
Paul D'Ambra <paul@posthog.com>
2024-09-18 00:16:18 +0100

    Fix

--- frontend/src/scenes/session-recordings/apm/playerInspector/ItemPerformanceEvent.stories.tsx
+++ frontend/src/scenes/session-recordings/apm/playerInspector/ItemPerformanceEvent.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryFn } from '@storybook/react'
 import { LemonDivider } from 'lib/lemon-ui/LemonDivider'
+import { CapturedNetworkRequest } from 'posthog-js'
 import { mapRRWebNetworkRequest } from 'scenes/session-recordings/apm/performance-event-utils'
 import {
     BodyDisplay,
@@ -10,8 +11,6 @@ import {
 
 import { mswDecorator } from '~/mocks/browser'
 import { PerformanceEvent } from '~/types'
-
-import { CapturedNetworkRequest } from '../../../../../../.yalc/posthog-js'
 
 const meta: Meta<typeof ItemPerformanceEvent> = {
     title: 'Components/ItemPerformanceEvent',
