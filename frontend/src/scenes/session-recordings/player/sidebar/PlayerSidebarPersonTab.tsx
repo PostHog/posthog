f10607eec6e19ac7aeeb44b9b2604d0877bfb39c
Paul D'Ambra <paul@posthog.com>
2024-09-18 23:12:40 +0100

    loading state

--- frontend/src/scenes/session-recordings/player/sidebar/PlayerSidebarPersonTab.tsx
+++ frontend/src/scenes/session-recordings/player/sidebar/PlayerSidebarPersonTab.tsx
@@ -1,6 +1,7 @@
 import { PersonDisplay } from '@posthog/apps-common'
 import { useValues } from 'kea'
 import { PropertiesTable } from 'lib/components/PropertiesTable'
+import { LemonSkeleton } from 'lib/lemon-ui/LemonSkeleton'
 
 import { PropertyDefinitionType } from '~/types'
 
@@ -9,19 +10,25 @@ import { sessionRecordingPlayerLogic } from '../sessionRecordingPlayerLogic'
 
 export function PlayerSidebarPersonTab(): JSX.Element {
     const { logicProps } = useValues(sessionRecordingPlayerLogic)
-    const { sessionPerson } = useValues(playerMetaLogic(logicProps))
+    const { sessionPerson, sessionPlayerMetaDataLoading } = useValues(playerMetaLogic(logicProps))
 
     return (
         <div className="flex flex-col overflow-auto bg-bg-3000">
             <div className="font-bold bg-bg-light px-2 border-b py-3">
                 <PersonDisplay person={sessionPerson} withIcon noPopover />
             </div>
-            <PropertiesTable
-                properties={sessionPerson?.properties || []}
-                type={PropertyDefinitionType.Person}
-                sortProperties
-                embedded
-            />
+            {sessionPlayerMetaDataLoading ? (
+                <div className="space-y-2">
+                    <LemonSkeleton.Row repeat={60} fade={true} className="h-3 w-full" />
+                </div>
+            ) : (
+                <PropertiesTable
+                    properties={sessionPerson?.properties || []}
+                    type={PropertyDefinitionType.Person}
+                    sortProperties
+                    embedded
+                />
+            )}
         </div>
     )
 }
