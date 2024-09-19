636ab5d8f0a16513d60f2628c6678069ea1692a6
MarconLP <13001502+MarconLP@users.noreply.github.com>
2024-09-19 12:25:42 +0200

    change aws kinesis category to analytics

--- posthog/cdp/templates/aws_kinesis/template_aws_kinesis.py
+++ posthog/cdp/templates/aws_kinesis/template_aws_kinesis.py
@@ -7,7 +7,7 @@ template: HogFunctionTemplate = HogFunctionTemplate(
     name="AWS Kinesis",
     description="Put data to an AWS Kinesis stream",
     icon_url="/static/services/aws-kinesis.png",
-    category=["Custom"],
+    category=["Analytics"],
     hog="""
 fun getPayload() {
   let region := inputs.aws_region
