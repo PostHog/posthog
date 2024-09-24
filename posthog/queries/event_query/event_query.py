9908726014c2498e13286e6ff46c1f1c4f2fa03a
Georgiy Tarasov <gtarasov.work@gmail.com>
2024-09-24 12:06:13 +0200

    fix: revert filtering out distinct_ids from join

--- posthog/queries/event_query/event_query.py
+++ posthog/queries/event_query/event_query.py
@@ -153,7 +153,7 @@ class EventQuery(metaclass=ABCMeta):
 
         return f"""
             LEFT OUTER JOIN (
-                {get_team_distinct_ids_query(self._team_id)}
+                {get_team_distinct_ids_query(self._team_id, relevant_events_conditions=relevant_events_conditions)}
             ) AS {self.DISTINCT_ID_TABLE_ALIAS}
             ON {self.EVENT_TABLE_ALIAS}.distinct_id = {self.DISTINCT_ID_TABLE_ALIAS}.distinct_id
         """
