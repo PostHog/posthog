8f04253e50d9553ae8d983f8590052ff16efc441
James Greenhill <fuziontech@gmail.com>
2024-09-24 17:07:53 -0700

    try this

--- posthog/models/raw_sessions/sql.py
+++ posthog/models/raw_sessions/sql.py
@@ -89,7 +89,7 @@ CREATE TABLE IF NOT EXISTS {table_name} ON CLUSTER '{cluster}'
     -- verify correctness and as a backup. Ideally we will be able to delete the uniq columns in the future when we're
     -- satisfied that counts are accurate.
     pageview_count SimpleAggregateFunction(sum, Int64),
-    pageview_uniq AggregateFunction(uniq, Nullable(UUID)),
+    pageview_uniq AggregateFunction(uniq, UUID),
     autocapture_count SimpleAggregateFunction(sum, Int64),
     autocapture_uniq AggregateFunction(uniq, Nullable(UUID)),
     screen_count SimpleAggregateFunction(sum, Int64),
