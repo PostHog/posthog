4ebdea6beda7c26cd3a863ce20709edac97fe2ee
Tom Owers <tom@posthog.com>
2024-09-25 13:11:35 +0100

    feat(bi): Added foundations of insight variables (#25146)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ posthog/models/insight_variable.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from posthog.models.utils import CreatedMetaFields, UUIDModel, UpdatedMetaFields, sane_repr
+
+
+class InsightVariable(UUIDModel, CreatedMetaFields, UpdatedMetaFields):
+    class Type(models.TextChoices):
+        STRING = "String", "String"
+        NUMBER = "Number", "Number"
+        BOOLEAN = "Boolean", "Boolean"
+        LIST = "List", "List"
+
+    team = models.ForeignKey("Team", on_delete=models.CASCADE)
+    name = models.CharField(max_length=400)
+    type = models.CharField(max_length=128, choices=Type.choices)
+    default_value = models.JSONField(null=True, blank=True)
+
+    __repr__ = sane_repr("id")
