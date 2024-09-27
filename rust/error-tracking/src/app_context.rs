2debb3fe1d48d265a7a4602d952056a1c228d93e
Oliver Browne <oliver@posthog.com>
2024-09-27 21:42:35 +0300

    et token verif, propdefs hardening

--- rust/error-tracking/src/app_context.rs
+++ rust/error-tracking/src/app_context.rs
@@ -2,14 +2,17 @@ use std::time::Duration;
 
 use common_kafka::kafka_consumer::SingleTopicConsumer;
 use health::{HealthHandle, HealthRegistry};
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use tracing::info;
 
-use crate::{config::Config, error::Error};
+use crate::{config::Config, error::Error, team_cache::TeamCache};
 
 pub struct AppContext {
     pub health_registry: HealthRegistry,
     pub worker_liveness: HealthHandle,
     pub consumer: SingleTopicConsumer,
+    pub pool: PgPool,
+    pub team_cache: TeamCache,
 }
 
 impl AppContext {
@@ -21,15 +24,22 @@ impl AppContext {
 
         let consumer = SingleTopicConsumer::new(config.kafka.clone(), config.consumer.clone())?;
 
+        let options = PgPoolOptions::new().max_connections(config.max_pg_connections);
+        let pool = options.connect(&config.database_url).await?;
+
         info!(
             "AppContext initialized, subscribed to topic {}",
             config.consumer.kafka_consumer_topic
         );
 
+        let team_cache = TeamCache::new(config.team_cache_capacity, config.team_cache_ttl_secs);
+
         Ok(Self {
             health_registry,
             worker_liveness,
             consumer,
+            pool,
+            team_cache,
         })
     }
 }
