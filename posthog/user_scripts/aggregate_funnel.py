ff7d4e7cc401689b1a2f23ea89e76119134c8f03
Sandy Spicer <sandy@posthog.com>
2024-10-01 15:15:00 -0700

    funnel udfs python

--- posthog/user_scripts/aggregate_funnel.py
+++ posthog/user_scripts/aggregate_funnel.py
@@ -15,7 +15,7 @@ def parse_args(line):
         str(args["breakdown_attribution_type"]),
         str(args["funnel_order_type"]),
         args["prop_vals"],  # Array(Array(String))
-        args["value"],  # Array(Tuple(Nullable(Float64), Nullable(DateTime), Array(String), Array(Int8)))
+        args["value"],  # Array(Tuple(Nullable(Float64), UUID, Array(String), Array(Int8)))
     ]
 
 
@@ -23,6 +23,10 @@ def parse_args(line):
 class EnteredTimestamp:
     timestamp: Any
     timings: Any
+    uuids: list[str]
+
+
+MAX_REPLAY_EVENTS = 10
 
 
 # each one can be multiple steps here
@@ -30,7 +34,7 @@ class EnteredTimestamp:
 # This function is defined for Clickhouse in user_defined_functions.xml along with types
 # num_steps is the total number of steps in the funnel
 # conversion_window_limit is in seconds
-# events is a array of tuples of (timestamp, breakdown, [steps])
+# events is an array of tuples of (timestamp, breakdown, [steps])
 # steps is an array of integers which represent the steps that this event qualifies for. it looks like [1,3,5,6].
 # negative integers represent an exclusion on that step. each event is either all exclusions or all steps.
 def calculate_funnel_from_user_events(
@@ -39,18 +43,18 @@ def calculate_funnel_from_user_events(
     breakdown_attribution_type: str,
     funnel_order_type: str,
     prop_vals: list[Any],
-    events: Sequence[tuple[float, list[str] | int | str, list[int]]],
+    events: Sequence[tuple[float, str, list[str] | int | str, list[int]]],
 ):
-    default_entered_timestamp = EnteredTimestamp(0, [])
+    default_entered_timestamp = EnteredTimestamp(0, [], [])
     max_step = [0, default_entered_timestamp]
     # If the attribution mode is a breakdown step, set this to the integer that represents that step
     breakdown_step = int(breakdown_attribution_type[5:]) if breakdown_attribution_type.startswith("step_") else None
 
     # This function returns an Array. We build up an array of strings to return here.
-    results: list[tuple[int, Any, list[float]]] = []
+    results: list[tuple[int, Any, list[float], list[list[str]]]] = []
 
     # Process an event. If this hits an exclusion, return False, else return True.
-    def process_event(timestamp, breakdown, steps, *, entered_timestamp, prop_val) -> bool:
+    def process_event(timestamp, uuid, breakdown, steps, *, entered_timestamp, prop_val, event_uuids) -> bool:
         # iterate the steps in reverse so we don't count this event multiple times
         for step in reversed(steps):
             exclusion = False
@@ -61,24 +65,30 @@ def calculate_funnel_from_user_events(
             in_match_window = timestamp - entered_timestamp[step - 1].timestamp <= conversion_window_limit_seconds
             already_reached_this_step_with_same_entered_timestamp = (
                 entered_timestamp[step].timestamp == entered_timestamp[step - 1].timestamp
+                and entered_timestamp[step].timestamp != 0
             )
 
             if in_match_window and not already_reached_this_step_with_same_entered_timestamp:
                 if exclusion:
-                    results.append((-1, prop_val, []))
+                    results.append((-1, prop_val, [], []))
                     return False
                 is_unmatched_step_attribution = (
                     breakdown_step is not None and step == breakdown_step - 1 and prop_val != breakdown
                 )
                 if not is_unmatched_step_attribution:
                     entered_timestamp[step] = replace(
-                        entered_timestamp[step - 1], timings=[*entered_timestamp[step - 1].timings, timestamp]
+                        entered_timestamp[step - 1],
+                        timings=[*entered_timestamp[step - 1].timings, timestamp],
+                        uuids=[*entered_timestamp[step - 1].uuids, uuid],
                     )
+                    if len(event_uuids[step - 1]) < MAX_REPLAY_EVENTS - 1:
+                        event_uuids[step - 1].append(uuid)
+                # TODO: If this is strict, and this didn't match, we should remove this
                 if step > max_step[0]:
                     max_step[:] = (step, entered_timestamp[step])
 
         if funnel_order_type == "strict":
-            for i in range(len(entered_timestamp)):
+            for i in range(1, len(entered_timestamp)):
                 if i not in steps:
                     entered_timestamp[i] = default_entered_timestamp
 
@@ -90,23 +100,43 @@ def calculate_funnel_from_user_events(
         # entered_timestamp = [(0, "", [])] * (num_steps + 1)
         max_step[:] = [0, default_entered_timestamp]
         entered_timestamp: list[EnteredTimestamp] = [default_entered_timestamp] * (num_steps + 1)
+        event_uuids: list[list[str]] = [[] for _ in range(num_steps)]
 
         def add_max_step():
-            i = cast(int, max_step[0])
+            final_index = cast(int, max_step[0])
             final = cast(EnteredTimestamp, max_step[1])
-            results.append((i - 1, prop_val, [final.timings[i] - final.timings[i - 1] for i in range(1, i)]))
+            for i in range(final_index):
+                # if len(event_uuids[i]) >= MAX_REPLAY_EVENTS and final.uuids[i] not in event_uuids[i]:
+                # Always put the actual event uuids first, we use it to extract timestamps
+                # This might create duplicates, but that's fine (we can remove it in clickhouse)
+                event_uuids[i].insert(0, final.uuids[i])
+            results.append(
+                (
+                    final_index - 1,
+                    prop_val,
+                    [final.timings[i] - final.timings[i - 1] for i in range(1, final_index)],
+                    event_uuids,
+                )
+            )
 
         filtered_events = (
-            ((timestamp, breakdown, steps) for (timestamp, breakdown, steps) in events if breakdown == prop_val)
+            (
+                (timestamp, uuid, breakdown, steps)
+                for (timestamp, uuid, breakdown, steps) in events
+                if breakdown == prop_val
+            )
             if breakdown_attribution_type == "all_events"
             else events
         )
         for timestamp, events_with_same_timestamp_iterator in groupby(filtered_events, key=lambda x: x[0]):
             events_with_same_timestamp = tuple(events_with_same_timestamp_iterator)
-            entered_timestamp[0] = EnteredTimestamp(timestamp, [])
+            entered_timestamp[0] = EnteredTimestamp(timestamp, [], [])
             if len(events_with_same_timestamp) == 1:
                 if not process_event(
-                    *events_with_same_timestamp[0], entered_timestamp=entered_timestamp, prop_val=prop_val
+                    *events_with_same_timestamp[0],
+                    entered_timestamp=entered_timestamp,
+                    prop_val=prop_val,
+                    event_uuids=event_uuids,
                 ):
                     return
             else:
@@ -114,11 +144,14 @@ def calculate_funnel_from_user_events(
                 # We play all of their permutations and most generously take the ones that advanced the furthest
                 # This has quite bad performance, and can probably be optimized through clever but annoying logic
                 # but shouldn't be hit too often
+                # This could potentially cause the same event to be added to the matching events multiple times
                 entered_timestamps = []
                 for events_group_perm in permutations(events_with_same_timestamp):
                     entered_timestamps.append(list(entered_timestamp))
                     for event in events_group_perm:
-                        if not process_event(*event, entered_timestamp=entered_timestamps[-1], prop_val=prop_val):
+                        if not process_event(
+                            *event, entered_timestamp=entered_timestamps[-1], prop_val=prop_val, event_uuids=event_uuids
+                        ):
                             # If any of the permutations hits an exclusion, we exclude this user.
                             # This isn't an important implementation detail and we could do something smarter here.
                             return
--- posthog/user_scripts/aggregate_funnel_test.py
+++ posthog/user_scripts/aggregate_funnel_test.py
@@ -3,11 +3,12 @@ import json
 
 from aggregate_funnel import calculate_funnel_from_user_events, parse_args
 import sys
+import traceback
 
 if __name__ == "__main__":
     for line in sys.stdin:
         try:
             calculate_funnel_from_user_events(*parse_args(line))
         except Exception as e:
-            print(json.dumps({"result": json.dumps(str(e))}), end="\n")  # noqa: T201
+            print(json.dumps({"result": json.dumps(str(e) + traceback.format_exc())}), end="\n")  # noqa: T201
         sys.stdout.flush()
