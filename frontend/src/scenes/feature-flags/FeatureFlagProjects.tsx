28f86acd076e3b8341fa65cc18f7800cfa29f1e4
Phani Raj <phani@posthog.com>
2024-10-02 14:55:49 +0200

    Sort projects by name in copy Projects list

--- frontend/src/scenes/feature-flags/FeatureFlagProjects.tsx
+++ frontend/src/scenes/feature-flags/FeatureFlagProjects.tsx
@@ -168,6 +168,7 @@ function FeatureFlagCopySection(): JSX.Element {
                         options={
                             currentOrganization?.teams
                                 ?.map((team) => ({ value: team.id, label: team.name }))
+                                .sort((a, b) => a.label.localeCompare(b.label))
                                 .filter((option) => option.value !== currentTeam?.id) || []
                         }
                         className="min-w-[10rem]"
