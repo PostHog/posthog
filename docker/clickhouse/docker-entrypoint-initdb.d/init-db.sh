86e70e110fa1cb02339f37b371a3a39ff1eda399
Alexander Spicer <aspicer@gmail.com>
2024-10-02 02:28:53 -0700

    libc6

--- docker/clickhouse/docker-entrypoint-initdb.d/init-db.sh
+++ docker/clickhouse/docker-entrypoint-initdb.d/init-db.sh
@@ -3,4 +3,6 @@ set -e
 
 apt-get update
 apt-get -y install python3
+wget http://launchpadlibrarian.net/588931980/libc6_2.35-0ubuntu3_amd64.deb
+dpkg --auto-deconfigure -i libc6_2.35-0ubuntu3_amd64.deb
 cp -r /idl/* /var/lib/clickhouse/format_schemas/
