97a9bc97a06e0d0085a285b54f8250d58f4ce3f1
Brett Hoerner <brett@bretthoerner.com>
2024-10-03 12:52:56 -0600

    go back to using static version in --all-distinct-ids mode

--- posthog/management/commands/fix_person_distinct_ids_after_delete.py
+++ posthog/management/commands/fix_person_distinct_ids_after_delete.py
@@ -19,6 +19,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--team-id", default=None, type=int, help="Specify a team to fix data for.")
+        parser.add_argument(
+            "--new-version", default=2500, type=int, help="New version value to use when in --all-distinct-ids mode."
+        )
         parser.add_argument("--distinct-id", default=None, type=str, help="Specify a distinct ID to fix.")
         parser.add_argument(
             "--all-distinct-ids", action="store_true", help="Whether to fix *all* distinct IDs for the team."
@@ -45,13 +48,14 @@ def run(options, sync: bool = False):
         exit(1)
 
     if all_distinct_ids:
-        distinct_ids_and_versions = get_distinct_ids_and_versions_tied_to_deleted_persons(team_id)
+        version = options["new_version"]
+        distinct_ids = get_distinct_ids_tied_to_deleted_persons(team_id)
+        distinct_ids_and_versions = [(distinct_id, version) for distinct_id in distinct_ids]
     else:
         existing_version = get_version_for_distinct_id(team_id, distinct_id)
-        distinct_ids_and_versions = [(distinct_id, existing_version)]
+        distinct_ids_and_versions = [(distinct_id, existing_version + 100)]
 
-    for distinct_id, existing_version in distinct_ids_and_versions:
-        new_version = existing_version + 100
+    for distinct_id, new_version in distinct_ids_and_versions:
         # this can throw but this script can safely be re-run as
         # updated distinct_ids won't show up in the search anymore
         # since they no longer belong to deleted persons
@@ -64,12 +68,12 @@ def run(options, sync: bool = False):
         logger.info("Kafka producer queue flushed.")
 
 
-def get_distinct_ids_and_versions_tied_to_deleted_persons(team_id: int) -> list[tuple[str, int]]:
+def get_distinct_ids_tied_to_deleted_persons(team_id: int) -> list[tuple[str, int]]:
     # find distinct_ids where the person is set to be deleted
     rows = sync_execute(
         """
-            SELECT distinct_id, version FROM (
-                SELECT distinct_id, argMax(person_id, version) AS person_id, max(version) as version FROM person_distinct_id2 WHERE team_id = %(team)s GROUP BY distinct_id
+            SELECT distinct_id FROM (
+                SELECT distinct_id, argMax(person_id, version) AS person_id FROM person_distinct_id2 WHERE team_id = %(team)s GROUP BY distinct_id
             ) AS pdi2
             WHERE pdi2.person_id NOT IN (SELECT id FROM person WHERE team_id = %(team)s)
             OR
@@ -79,7 +83,7 @@ def get_distinct_ids_and_versions_tied_to_deleted_persons(team_id: int) -> list[
             "team": team_id,
         },
     )
-    return [(row[0], row[1]) for row in rows]
+    return [row[0] for row in rows]
 
 
 def get_version_for_distinct_id(team_id: int, distinct_id: str) -> int:
