56bf024e2fc389831798a9e65cdf20a81c99b615
eric <eeoneric@gmail.com>
2024-10-03 14:28:12 -0400

    move clause

--- posthog/api/query.py
+++ posthog/api/query.py
@@ -39,7 +39,6 @@ from posthog.rate_limit import (
     ClickHouseSustainedRateThrottle,
 )
 from posthog.schema import QueryRequest, QueryResponseAlternative, QueryStatusResponse
-from posthog.hogql.modifiers import create_default_modifiers_for_user
 
 
 class ServerSentEventRenderer(BaseRenderer):
@@ -79,9 +78,6 @@ class QueryViewSet(TeamAndOrgViewSetMixin, PydanticModelMixin, viewsets.ViewSet)
             data.query = apply_dashboard_filters_to_dict(
                 data.query.model_dump(), data.filters_override.model_dump(), self.team
             )  # type: ignore
-
-        if hasattr(data.query, "modifiers"):
-            data.query.modifiers = create_default_modifiers_for_user(request.user, self.team)
 
         client_query_id = data.client_query_id or uuid.uuid4().hex
         execution_mode = execution_mode_from_refresh(data.refresh)
