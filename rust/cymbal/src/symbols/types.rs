0b828d7d43b7343aaa7a08431e7d0fee9c6af8ab
David Newell <david@posthog.com>
2024-10-04 15:04:17 +0100

    typing and parsing of stack trace

--- rust/cymbal/src/symbols/types.rs
+++ rust/cymbal/src/symbols/types.rs
@@ -1,4 +1,8 @@
-use ::serde::Deserialize;
+use std::collections::HashMap;
+
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::Value;
 
 use super::{js::RawJSFrame, symbolifier::Symbolifier};
 
@@ -15,7 +19,7 @@ pub enum RawFrame {
 // see them, but we get the flexibility "for free"
 #[derive(Debug, Deserialize)]
 pub struct RawStack {
-    pub frames: Vec<RawStack>,
+    pub frames: Vec<RawFrame>,
 }
 
 pub enum ProcessedFrame {
@@ -32,18 +36,45 @@ impl RawStack {
     }
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct PropertyView {
+    #[serde(rename = "$exception_type")]
+    exception_type: String,
+    #[serde(rename = "$exception_message")]
+    exception_message: String,
+    #[serde(rename = "$exception_stack_trace_raw")]
+    exception_stack_trace_raw: String,
+    #[serde(rename = "$exception_level")]
+    exception_level: String,
+    #[serde(rename = "$exception_source")]
+    exception_source: String,
+    #[serde(rename = "$exception_lineno")]
+    exception_line: u32,
+    #[serde(rename = "$exception_colno")]
+    exception_col: u32,
+    #[serde(flatten)]
+    other: HashMap<String, Value>,
+}
+
 #[cfg(test)]
 mod test {
+    use common_types::ClickHouseEvent;
     use serde_json::Value;
 
-    use crate::symbols::types::{RawFrame, RawStack};
+    use crate::symbols::types::{PropertyView, RawFrame, RawStack};
 
     #[test]
     fn it_symbolifies() {
         let raw: &'static str = include_str!("../../tests/static/raw_js_stack.json");
 
-        let raw: Vec<RawFrame> = serde_json::from_str(raw).unwrap();
+        let raw: ClickHouseEvent = serde_json::from_str(raw).unwrap();
 
-        println!("{:?}", raw);
+        let exception_properties: PropertyView =
+            serde_json::from_str(&raw.properties.unwrap()).unwrap();
+
+        let stack_trace: Vec<RawFrame> =
+            serde_json::from_str(&exception_properties.exception_stack_trace_raw).unwrap();
+
+        println!("{:?}", stack_trace);
     }
 }
