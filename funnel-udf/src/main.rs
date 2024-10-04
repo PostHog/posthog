b231f6b82c3101216983bb0340f48b84e8e977ab
Alexander Spicer <aspicer@gmail.com>
2024-10-03 23:38:49 -0700

    proper clean up

--- funnel-udf/src/main.rs
+++ funnel-udf/src/main.rs
@@ -50,6 +50,7 @@ struct Vars {
 struct AggregateFunnelRow {
     // vars: Vars,
     // event_uuids: Vec<Vec<Uuid>>,
+    breakdown_step: Option<usize>,
     results: Vec<Result>,
 }
 
@@ -67,53 +68,53 @@ fn parse_args(line: &str) -> Args {
 }
 
 impl AggregateFunnelRow {
-
+    #[inline(always)]
     fn calculate_funnel_from_user_events(&mut self, args: &Args) -> &Vec<Result> {
+        if args.breakdown_attribution_type.starts_with("step_") {
+            self.breakdown_step = args.breakdown_attribution_type[5..].parse::<usize>().ok()
+        }
 
-        let breakdown_step = if args.breakdown_attribution_type.starts_with("step_") {
-            args.breakdown_attribution_type[5..].parse::<usize>().ok()
-        } else {
-            None
+        args.prop_vals.iter().for_each(|prop_val| self.loop_prop_val(args, prop_val));
+
+        &self.results
+    }
+
+    #[inline(always)]
+    fn loop_prop_val(&mut self, args: &Args, prop_val: &PropVal) {
+        let mut vars = Vars {
+            max_step: (0, DEFAULT_ENTERED_TIMESTAMP.clone()),
+            event_uuids: repeat(Vec::new()).take(args.num_steps).collect(),
         };
+        let mut entered_timestamp = vec![DEFAULT_ENTERED_TIMESTAMP.clone(); args.num_steps + 1];
 
-        for prop_val in &args.prop_vals {
-            let mut vars = Vars {
-                max_step: (0, DEFAULT_ENTERED_TIMESTAMP.clone()),
-                event_uuids: repeat(Vec::new()).take(args.num_steps).collect()
+        let filtered_events = args.value.iter()
+            .filter(|e| {
+                if args.breakdown_attribution_type == "all_events" {
+                    e.breakdown == *prop_val
+                } else {
+                    true
+                }
+            })
+            .group_by(|e| e.timestamp);
+
+        for (timestamp, events_with_same_timestamp) in &filtered_events {
+            let events_with_same_timestamp: Vec<_> = events_with_same_timestamp.collect();
+            entered_timestamp[0] = EnteredTimestamp {
+                timestamp,
+                timings: vec![],
+                uuids: vec![],
             };
-            let mut entered_timestamp = vec![DEFAULT_ENTERED_TIMESTAMP.clone(); args.num_steps + 1];
-            let mut add_max_step = true;
 
-            let filtered_events = args.value.iter()
-                .filter(|e| {
-                    if args.breakdown_attribution_type == "all_events" {
-                        e.breakdown == *prop_val
-                    } else {
-                        true
-                    }
-                })
-                .group_by(|e| e.timestamp);
-
-            for (timestamp, events_with_same_timestamp) in &filtered_events {
-                let events_with_same_timestamp: Vec<_> = events_with_same_timestamp.collect();
-                entered_timestamp[0] = EnteredTimestamp {
-                    timestamp,
-                    timings: vec![],
-                    uuids: vec![],
-                };
-
-                if events_with_same_timestamp.len() == 1 {
-                    if !self.process_event(
-                        args,
-                        &mut vars,
-                        &events_with_same_timestamp[0],
-                        &mut entered_timestamp,
-                        &prop_val,
-                        breakdown_step
-                    ) {
-                        add_max_step = false;
-                        break;
-                    }/*
+            if events_with_same_timestamp.len() == 1 {
+                if !self.process_event(
+                    args,
+                    &mut vars,
+                    &events_with_same_timestamp[0],
+                    &mut entered_timestamp,
+                    prop_val,
+                ) {
+                    return
+                }/*
                 } else if events_with_same_timestamp.iter().map(|x| x.steps).all_equal_value() {
                     // Deal with the case where they are all the same event
                     for event in events_with_same_timestamp {
@@ -132,54 +133,52 @@ impl AggregateFunnelRow {
                             break;
                         }
                     }*/
-                } else {
-                    // Handle permutations for events with the same timestamp
-                    let mut entered_timestamps: Vec<_> = vec![];
-                    for perm in events_with_same_timestamp.iter().permutations(events_with_same_timestamp.len()) {
-                        entered_timestamps.push(entered_timestamp.clone());
-                        for event in perm {
-                            if !self.process_event(
-                                args,
-                                &mut vars,
-                                &event,
-                                &mut entered_timestamps.last_mut().unwrap(),
-                                &prop_val,
-                                breakdown_step
-                            ) {
-                                add_max_step = false;
-                                break;
-                            }
+            } else {
+                // Handle permutations for events with the same timestamp
+                let mut entered_timestamps: Vec<_> = vec![];
+                for perm in events_with_same_timestamp.iter().permutations(events_with_same_timestamp.len()) {
+                    entered_timestamps.push(entered_timestamp.clone());
+                    for event in perm {
+                        if !self.process_event(
+                            args,
+                            &mut vars,
+                            &event,
+                            &mut entered_timestamps.last_mut().unwrap(),
+                            &prop_val,
+                        ) {
+                            // If any of the permutations hits an exclusion, we exclude this user.
+                            // This isn't an important implementation detail and we could do something smarter here.
+                            return;
                         }
                     }
-                    for i in 0..entered_timestamp.len() {
-                        entered_timestamp[i] = entered_timestamps.iter().max_by_key(|x| x[i].timestamp as i32).unwrap()[i].clone();
-                    }
                 }
-
-                if entered_timestamp[args.num_steps].timestamp > 0.0 {
-                    break;
+                for i in 0..entered_timestamp.len() {
+                    entered_timestamp[i] = entered_timestamps.iter().max_by_key(|x| x[i].timestamp as i32).unwrap()[i].clone();
                 }
             }
 
-            if add_max_step {
-                let final_index = vars.max_step.0;
-                let final_value = &vars.max_step.1;
-
-                for i in 0..final_index {
-                    //if event_uuids[i].len() >= MAX_REPLAY_EVENTS && !event_uuids[i].contains(&final_value.uuids[i]) {
-                    // Always put the actual event uuids first, we use it to extract timestamps
-                    // This might create duplicates, but that's fine (we can remove it in clickhouse)
-                    vars.event_uuids[i].insert(0, final_value.uuids[i].clone());
-                }
-                self.results.push(Result(
-                    final_index as i32 - 1,
-                    prop_val.clone(),
-                    final_value.timings.windows(2).map(|w| w[1] - w[0]).collect(),
-                    vars.event_uuids,
-                ))
+            // If we hit the goal, we can terminate early
+            if entered_timestamp[args.num_steps].timestamp > 0.0 {
+                break;
             }
         }
-        &self.results
+
+        // Find the furthest step we have made it to and print it
+        let final_index = vars.max_step.0;
+        let final_value = &vars.max_step.1;
+
+        for i in 0..final_index {
+            //if event_uuids[i].len() >= MAX_REPLAY_EVENTS && !event_uuids[i].contains(&final_value.uuids[i]) {
+            // Always put the actual event uuids first, we use it to extract timestamps
+            // This might create duplicates, but that's fine (we can remove it in clickhouse)
+            vars.event_uuids[i].insert(0, final_value.uuids[i].clone());
+        }
+        self.results.push(Result(
+            final_index as i32 - 1,
+            prop_val.clone(),
+            final_value.timings.windows(2).map(|w| w[1] - w[0]).collect(),
+            vars.event_uuids,
+        ))
     }
 
     #[inline(always)]
@@ -190,7 +189,6 @@ impl AggregateFunnelRow {
         event: &Event,
         entered_timestamp: &mut Vec<EnteredTimestamp>,
         prop_val: &PropVal,
-        breakdown_step: Option<usize>
     ) -> bool {
         for step in event.steps.iter().rev() {
             let mut exclusion = false;
@@ -210,7 +208,7 @@ impl AggregateFunnelRow {
                     self.results.push(Result(-1, prop_val.clone(), vec![], vec![]));
                     return false;
                 }
-                let is_unmatched_step_attribution = breakdown_step.map(|breakdown_step| step == breakdown_step - 1).unwrap_or(false) && *prop_val != event.breakdown;
+                let is_unmatched_step_attribution = self.breakdown_step.map(|breakdown_step| step == breakdown_step - 1).unwrap_or(false) && *prop_val != event.breakdown;
                 if !is_unmatched_step_attribution {
                     entered_timestamp[step] = EnteredTimestamp {
                         timestamp: entered_timestamp[step - 1].timestamp,
@@ -260,6 +258,7 @@ fn main() {
             let args = parse_args(&line);
             let mut aggregate_funnel_row = AggregateFunnelRow {
                 results: Vec::with_capacity(args.prop_vals.len()),
+                breakdown_step: Option::None
             };
             let result = aggregate_funnel_row.calculate_funnel_from_user_events(&args);
             let output = json!({ "result": result });
