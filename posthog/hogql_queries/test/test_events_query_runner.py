6c081ffc38f003593510100905ba8e9e5d876d48
timgl <tim@posthog.com>
2024-10-05 19:35:17 +0100

    fix: Unsupported constant type

--- posthog/hogql_queries/test/test_events_query_runner.py
+++ posthog/hogql_queries/test/test_events_query_runner.py
@@ -131,14 +131,14 @@ class TestEventsQueryRunner(ClickhouseTestMixin, APIBaseTest):
         query_ast = EventsQueryRunner(query=query, team=self.team).to_query()
         where_expr = cast(ast.CompareOperation, cast(ast.And, query_ast.where).exprs[0])
         right_expr = cast(ast.Constant, where_expr.right)
-        self.assertEqual([x.args[0].value for x in right_expr.value], ["id1", "id2"])
+        self.assertEqual([x.args[0].value for x in right_expr.exprs], ["id1", "id2"])
 
         # another team
         another_team = Team.objects.create(organization=Organization.objects.create())
         query_ast = EventsQueryRunner(query=query, team=another_team).to_query()
         where_expr = cast(ast.CompareOperation, cast(ast.And, query_ast.where).exprs[0])
         right_expr = cast(ast.Constant, where_expr.right)
-        self.assertEqual(right_expr.value, [])
+        self.assertEqual(right_expr.exprs, [])
 
     def test_test_account_filters(self):
         self.team.test_account_filters = [
