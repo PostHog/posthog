96fb6bcb9048707c547c660589b388ce7f4ef14e
Oliver Browne <oliver@posthog.com>
2024-10-07 11:01:46 +0300

    trait comment

--- rust/cymbal/src/traits.rs
+++ rust/cymbal/src/traits.rs
@@ -8,6 +8,7 @@ use crate::{
 #[async_trait]
 pub trait Resolver {
     // Resolvers work on a per-frame basis, so we can be clever about the order
-    // in which we resolve them.
+    // in which we resolve them. We also force any resolver to handle all frame
+    // types
     async fn resolve(&self, raw: RawFrame) -> Result<Frame, Error>;
 }
