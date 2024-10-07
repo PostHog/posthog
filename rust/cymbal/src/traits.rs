01a0696e81d341eb848933c161ad30f9406e2f70
Oliver Browne <oliver@posthog.com>
2024-10-07 11:06:50 +0300

    add notion of symbol stores, and a team-scoped ref to symbol sets

--- rust/cymbal/src/traits.rs
+++ rust/cymbal/src/traits.rs
@@ -12,3 +12,13 @@ pub trait Resolver {
     // types
     async fn resolve(&self, raw: RawFrame) -> Result<Frame, Error>;
 }
+
+#[async_trait]
+pub trait SymbolStore {
+    async fn fetch(&self, r: SymbolSetRef) -> Result<Vec<u8>, Error>;
+}
+
+pub struct SymbolSetRef {
+    team_id: i32,
+    id: String,
+}
