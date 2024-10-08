PostHog/posthog

commit 92ddf4072ba4324ab40542600dc53c166eea9e83

feat: materialize person and groups on events (#11363)

posthog/clickhouse/migrations/0026_fix_materialized_window_and_session_ids.py
@@ -44,7 +44,7 @@ def materialize_session_and_window_id(database):
     for property_name in properties:
         materialized_columns = get_materialized_columns("events", use_cache=False)
         # If the column is not materialized, materialize it
-        if property_name not in materialized_columns:
+        if (property_name, "properties") not in materialized_columns:
             materialize("events", property_name, property_name)
 
         # Now, we need to clean up any potentail inconsistencies with existing column names

.github/actions/run-backend-tests/action.yml
@@ -119,10 +119,16 @@ runs:
                   $PYTEST_ARGS
 
         - name: Run pytest.mark.ee tests
-          if: ${{ inputs.ee == 'true' && inputs.group == '1' }}
+          if: ${{ inputs.ee == 'true' && inputs.group == '1' && inputs.person-on-events != 'true'}}
           shell: bash
           run: |
-              pytest posthog -m "ee" --durations=100 --durations-min=1.0
+              pytest posthog -m "ee" --durations=100 --durations-min=1.0 $PYTEST_ARGS
+
+        - name: Run pytest.mark.ee tests with person-on-events, no snapshot commits
+          if: ${{ inputs.ee == 'true' && inputs.group == '1' && inputs.person-on-events == 'true' }}
+          shell: bash
+          run: |
+              PERSON_ON_EVENTS_ENABLED=true pytest posthog -m "ee" --durations=100 --durations-min=1.0 $PYTEST_ARGS
 
         - uses: codecov/codecov-action@v2
           if: ${{ inputs.cloud == 'false' }}

ee/benchmarks/benchmarks.py
@@ -639,9 +639,9 @@ class QuerySuite:
 
     def setup(self):
         for table, property in MATERIALIZED_PROPERTIES:
-            if property not in get_materialized_columns(table):
+            if (property, "properties") not in get_materialized_columns(table):
                 materialize(table, property)
-                backfill_materialized_columns(table, [property], backfill_period=timedelta(days=1_000))
+                backfill_materialized_columns(table, [(property, "properties")], backfill_period=timedelta(days=1_000))
 
         # :TRICKY: Data in benchmark servers has ID=2
         team = Team.objects.filter(id=2).first()

ee/clickhouse/management/commands/materialize_columns.py
@@ -3,6 +3,7 @@ import logging
 from django.core.management.base import BaseCommand
 
 from ee.clickhouse.materialized_columns.analyze import logger, materialize_properties_task
+from ee.clickhouse.materialized_columns.columns import DEFAULT_TABLE_COLUMN
 from posthog.settings import (
     MATERIALIZE_COLUMNS_ANALYSIS_PERIOD_HOURS,
     MATERIALIZE_COLUMNS_BACKFILL_PERIOD_DAYS,
@@ -20,6 +21,11 @@ class Command(BaseCommand):
         parser.add_argument("--property", help="Property to materialize. Skips analysis.")
         parser.add_argument(
             "--property-table", type=str, default="events", choices=["events", "person"], help="Table of --property"
+        )
+        parser.add_argument(
+            "--table-column",
+            help="The column to which --property should be materialised from.",
+            default=DEFAULT_TABLE_COLUMN,
         )
         parser.add_argument(
             "--backfill-period",
@@ -57,7 +63,7 @@ class Command(BaseCommand):
             logger.info(f"Materializing column. table={options['property_table']}, property_name={options['property']}")
 
             materialize_properties_task(
-                columns_to_materialize=[(options["property_table"], options["property"], 0)],
+                columns_to_materialize=[(options["property_table"], options["table_column"], options["property"], 0)],
                 backfill_period_days=options["backfill_period"],
                 dry_run=options["dry_run"],
             )

ee/clickhouse/materialized_columns/analyze.py
@@ -6,6 +6,7 @@ from typing import Dict, Generator, List, Optional, Set, Tuple
 import structlog
 
 from ee.clickhouse.materialized_columns.columns import (
+    DEFAULT_TABLE_COLUMN,
     backfill_materialized_columns,
     get_materialized_columns,
     materialize,
@@ -19,12 +20,12 @@ from ee.settings import (
 from posthog.cache_utils import instance_memoize
 from posthog.client import sync_execute
 from posthog.models.filters.mixins.utils import cached_property
-from posthog.models.person.sql import GET_PERSON_PROPERTIES_COUNT
-from posthog.models.property import PropertyName, TableWithProperties
+from posthog.models.person.sql import GET_EVENT_PROPERTIES_COUNT, GET_PERSON_PROPERTIES_COUNT
+from posthog.models.property import PropertyName, TableColumn, TableWithProperties
 from posthog.models.property_definition import PropertyDefinition
 from posthog.models.team import Team
 
-Suggestion = Tuple[TableWithProperties, PropertyName, int]
+Suggestion = Tuple[TableWithProperties, TableColumn, PropertyName, int]
 
 logger = structlog.get_logger(__name__)
 
@@ -32,12 +33,25 @@ logger = structlog.get_logger(__name__)
 class TeamManager:
     @instance_memoize
     def person_properties(self, team_id: str) -> Set[str]:
-        rows = sync_execute(GET_PERSON_PROPERTIES_COUNT, {"team_id": team_id})
-        return set(name for name, _ in rows)
+        return self._get_properties(GET_PERSON_PROPERTIES_COUNT, team_id)
 
     @instance_memoize
     def event_properties(self, team_id: str) -> Set[str]:
         return set(PropertyDefinition.objects.filter(team_id=team_id).values_list("name", flat=True))
+
+    @instance_memoize
+    def person_on_events_properties(self, team_id: str) -> Set[str]:
+        return self._get_properties(GET_EVENT_PROPERTIES_COUNT.format(column_name="person_properties"), team_id)
+
+    @instance_memoize
+    def group_on_events_properties(self, group_type_index: int, team_id: str) -> Set[str]:
+        return self._get_properties(
+            GET_EVENT_PROPERTIES_COUNT.format(column_name=f"group{group_type_index}_properties"), team_id
+        )
+
+    def _get_properties(self, query, team_id) -> Set[str]:
+        rows = sync_execute(query, {"team_id": team_id})
+        return set(name for name, _ in rows)
 
 
 class Query:
@@ -60,19 +74,42 @@ class Query:
         return matches[0] if matches else None
 
     @cached_property
-    def _all_properties(self) -> List[PropertyName]:
-        return re.findall(r"JSONExtract\w+\(\S+, '([^']+)'\)", self.query_string)
+    def _all_properties(self) -> List[Tuple[str, PropertyName]]:
+        return re.findall(r"JSONExtract\w+\((\S+), '([^']+)'\)", self.query_string)
 
-    def properties(self, team_manager: TeamManager) -> Generator[Tuple[TableWithProperties, PropertyName], None, None]:
+    def properties(
+        self, team_manager: TeamManager
+    ) -> Generator[Tuple[TableWithProperties, TableColumn, PropertyName], None, None]:
         # Reverse-engineer whether a property is an "event" or "person" property by getting their event definitions.
         # :KLUDGE: Note that the same property will be found on both tables if both are used.
+        # We try to hone in on the right column by looking at the column from which the property is extracted.
         person_props = team_manager.person_properties(self.team_id)
         event_props = team_manager.event_properties(self.team_id)
-        for property in self._all_properties:
-            if property in person_props:
-                yield "person", property
+        person_on_events_props = team_manager.person_on_events_properties(self.team_id)
+        group0_props = team_manager.group_on_events_properties(0, self.team_id)
+        group1_props = team_manager.group_on_events_properties(1, self.team_id)
+        group2_props = team_manager.group_on_events_properties(2, self.team_id)
+        group3_props = team_manager.group_on_events_properties(3, self.team_id)
+        group4_props = team_manager.group_on_events_properties(4, self.team_id)
+
+        for table_column, property in self._all_properties:
             if property in event_props:
-                yield "events", property
+                yield "events", DEFAULT_TABLE_COLUMN, property
+            if property in person_props:
+                yield "person", DEFAULT_TABLE_COLUMN, property
+
+            if property in person_on_events_props and "person_properties" in table_column:
+                yield "events", "person_properties", property
+            if property in group0_props and "group0_properties" in table_column:
+                yield "events", "group0_properties", property
+            if property in group1_props and "group1_properties" in table_column:
+                yield "events", "group1_properties", property
+            if property in group2_props and "group2_properties" in table_column:
+                yield "events", "group2_properties", property
+            if property in group3_props and "group3_properties" in table_column:
+                yield "events", "group3_properties", property
+            if property in group4_props and "group4_properties" in table_column:
+                yield "events", "group4_properties", property
 
 
 def _get_queries(since_hours_ago: int, min_query_time: int) -> List[Query]:
@@ -112,11 +149,12 @@ def _analyze(queries: List[Query]) -> List[Suggestion]:
         if not query.is_valid:
             continue
 
-        for table, property in query.properties(team_manager):
-            costs[(table, property)] += query.cost
+        for table, table_column, property in query.properties(team_manager):
+            costs[(table, table_column, property)] += query.cost
 
     return [
-        (table, property_name, cost) for (table, property_name), cost in sorted(costs.items(), key=lambda kv: -kv[1])
+        (table, table_column, property_name, cost)
+        for (table, table_column, property_name), cost in sorted(costs.items(), key=lambda kv: -kv[1])
     ]
 
 
@@ -136,8 +174,8 @@ def materialize_properties_task(
         columns_to_materialize = _analyze(_get_queries(time_to_analyze_hours, min_query_time))
     result = []
     for suggestion in columns_to_materialize:
-        table, property_name, _ = suggestion
-        if property_name not in get_materialized_columns(table):
+        table, table_column, property_name, _ = suggestion
+        if (property_name, table_column) not in get_materialized_columns(table):
             result.append(suggestion)
 
     if len(result) > 0:
@@ -145,16 +183,16 @@ def materialize_properties_task(
     else:
         logger.info("Found no columns to materialize.")
 
-    properties: Dict[TableWithProperties, List[PropertyName]] = {
+    properties: Dict[TableWithProperties, List[Tuple[PropertyName, TableColumn]]] = {
         "events": [],
         "person": [],
     }
-    for table, property_name, cost in result[:maximum]:
+    for table, table_column, property_name, cost in result[:maximum]:
         logger.info(f"Materializing column. table={table}, property_name={property_name}, cost={cost}")
 
         if not dry_run:
-            materialize(table, property_name)
-        properties[table].append(property_name)
+            materialize(table, property_name, table_column=table_column)
+        properties[table].append((property_name, table_column))
 
     if backfill_period_days > 0 and not dry_run:
         logger.info(f"Starting backfill for new materialized columns. period_days={backfill_period_days}")

ee/clickhouse/materialized_columns/columns.py
@@ -1,6 +1,6 @@
 import re
 from datetime import timedelta
-from typing import Dict, List, Literal, Union
+from typing import Dict, List, Literal, Tuple, Union, cast
 
 from django.utils.timezone import now
 
@@ -9,19 +9,34 @@ from posthog.clickhouse.kafka_engine import trim_quotes_expr
 from posthog.clickhouse.replication.utils import clickhouse_is_replicated
 from posthog.client import sync_execute
 from posthog.models.instance_setting import get_instance_setting
-from posthog.models.property import PropertyName, TableWithProperties
+from posthog.models.property import PropertyName, TableColumn, TableWithProperties
 from posthog.models.utils import generate_random_short_suffix
 from posthog.settings import CLICKHOUSE_CLUSTER, CLICKHOUSE_DATABASE, TEST
 
 ColumnName = str
+DEFAULT_TABLE_COLUMN: Literal["properties"] = "properties"
+
 
 TablesWithMaterializedColumns = Union[TableWithProperties, Literal["session_recording_events"]]
 
-TRIM_AND_EXTRACT_PROPERTY = trim_quotes_expr("JSONExtractRaw(properties, %(property)s)")
+TRIM_AND_EXTRACT_PROPERTY = trim_quotes_expr("JSONExtractRaw({table_column}, %(property)s)")
+
+SHORT_TABLE_COLUMN_NAME = {
+    "properties": "p",
+    "group_properties": "gp",
+    "person_properties": "pp",
+    "group0_properties": "gp0",
+    "group1_properties": "gp1",
+    "group2_properties": "gp2",
+    "group3_properties": "gp3",
+    "group4_properties": "gp4",
+}
 
 
 @cache_for(timedelta(minutes=15))
-def get_materialized_columns(table: TablesWithMaterializedColumns) -> Dict[PropertyName, ColumnName]:
+def get_materialized_columns(
+    table: TablesWithMaterializedColumns,
+) -> Dict[Tuple[PropertyName, TableColumn], ColumnName]:
     rows = sync_execute(
         """
         SELECT comment, name
@@ -38,14 +53,22 @@ def get_materialized_columns(table: TablesWithMaterializedColumns) -> Dict[Prope
         return {}
 
 
-def materialize(table: TableWithProperties, property: PropertyName, column_name=None) -> None:
-    if property in get_materialized_columns(table, use_cache=False):
+def materialize(
+    table: TableWithProperties,
+    property: PropertyName,
+    column_name=None,
+    table_column: TableColumn = DEFAULT_TABLE_COLUMN,
+) -> None:
+    if (property, table_column) in get_materialized_columns(table, use_cache=False):
         if TEST:
             return
 
-        raise ValueError(f"Property already materialized. table={table}, property={property}")
+        raise ValueError(f"Property already materialized. table={table}, property={property}, column={table_column}")
 
-    column_name = column_name or _materialized_column_name(table, property)
+    if table_column not in SHORT_TABLE_COLUMN_NAME:
+        raise ValueError(f"Invalid table_column={table_column} for materialisation")
+
+    column_name = column_name or _materialized_column_name(table, property, table_column)
     # :TRICKY: On cloud, we ON CLUSTER updates to events/sharded_events but not to persons. Why? ¯\_(ツ)_/¯
     execute_on_cluster = f"ON CLUSTER '{CLICKHOUSE_CLUSTER}'" if table == "events" else ""
 
@@ -55,7 +78,7 @@ def materialize(table: TableWithProperties, property: PropertyName, column_name=
             ALTER TABLE sharded_{table}
             {execute_on_cluster}
             ADD COLUMN IF NOT EXISTS
-            {column_name} VARCHAR MATERIALIZED {TRIM_AND_EXTRACT_PROPERTY}
+            {column_name} VARCHAR MATERIALIZED {TRIM_AND_EXTRACT_PROPERTY.format(table_column=table_column)}
         """,
             {"property": property},
         )
@@ -73,19 +96,22 @@ def materialize(table: TableWithProperties, property: PropertyName, column_name=
             ALTER TABLE {table}
             {execute_on_cluster}
             ADD COLUMN IF NOT EXISTS
-            {column_name} VARCHAR MATERIALIZED {TRIM_AND_EXTRACT_PROPERTY}
+            {column_name} VARCHAR MATERIALIZED {TRIM_AND_EXTRACT_PROPERTY.format(table_column=table_column)}
         """,
             {"property": property},
         )
 
     sync_execute(
         f"ALTER TABLE {table} {execute_on_cluster} COMMENT COLUMN {column_name} %(comment)s",
-        {"comment": f"column_materializer::{property}"},
+        {"comment": f"column_materializer::{table_column}::{property}"},
     )
 
 
 def backfill_materialized_columns(
-    table: TableWithProperties, properties: List[PropertyName], backfill_period: timedelta, test_settings=None
+    table: TableWithProperties,
+    properties: List[Tuple[PropertyName, TableColumn]],
+    backfill_period: timedelta,
+    test_settings=None,
 ) -> None:
     """
     Backfills the materialized column after its creation.
@@ -105,13 +131,13 @@ def backfill_materialized_columns(
     # Hack from https://github.com/ClickHouse/ClickHouse/issues/19785
     # Note that for this to work all inserts should list columns explicitly
     # Improve this if https://github.com/ClickHouse/ClickHouse/issues/27730 ever gets resolved
-    for property in properties:
+    for property, table_column in properties:
         sync_execute(
             f"""
             ALTER TABLE {updated_table}
             {execute_on_cluster}
             MODIFY COLUMN
-            {materialized_columns[property]} VARCHAR DEFAULT {TRIM_AND_EXTRACT_PROPERTY}
+            {materialized_columns[(property, table_column)]} VARCHAR DEFAULT {TRIM_AND_EXTRACT_PROPERTY.format(table_column=table_column)}
             """,
             {"property": property},
             settings=test_settings,
@@ -119,7 +145,8 @@ def backfill_materialized_columns(
 
     # Kick off mutations which will update clickhouse partitions in the background. This will return immediately
     assignments = ", ".join(
-        f"{materialized_columns[property]} = {materialized_columns[property]}" for property in properties
+        f"{materialized_columns[property_and_column]} = {materialized_columns[property_and_column]}"
+        for property_and_column in properties
     )
 
     sync_execute(
@@ -134,10 +161,15 @@ def backfill_materialized_columns(
     )
 
 
-def _materialized_column_name(table: TableWithProperties, property: PropertyName) -> str:
+def _materialized_column_name(
+    table: TableWithProperties, property: PropertyName, table_column: TableColumn = DEFAULT_TABLE_COLUMN
+) -> str:
     "Returns a sanitized and unique column name to use for materialized column"
 
-    prefix = "mat_" if table == "events" else "pmat_"
+    prefix = "mat_" if table == "events" or table == "groups" else "pmat_"
+
+    if table_column != DEFAULT_TABLE_COLUMN:
+        prefix += f"{SHORT_TABLE_COLUMN_NAME[table_column]}_"
     property_str = re.sub("[^0-9a-zA-Z$]", "_", property)
 
     existing_materialized_columns = set(get_materialized_columns(table, use_cache=False).values())
@@ -149,5 +181,12 @@ def _materialized_column_name(table: TableWithProperties, property: PropertyName
     return f"{prefix}{property_str}{suffix}"
 
 
-def _extract_property(comment: str) -> PropertyName:
-    return comment.split("::", 1)[1]
+def _extract_property(comment: str) -> Tuple[PropertyName, TableColumn]:
+    # Old style comments have the format "column_materializer::property", dealing with the default table column.
+    # Otherwise, it's "column_materializer::table_column::property"
+    split_column = comment.split("::", 2)
+
+    if len(split_column) == 2:
+        return split_column[1], DEFAULT_TABLE_COLUMN
+
+    return split_column[2], cast(TableColumn, split_column[1])

ee/clickhouse/materialized_columns/test/test_analyze.py
@@ -1,6 +1,7 @@
 from ee.clickhouse.materialized_columns.analyze import Query, TeamManager
 from posthog.clickhouse.kafka_engine import trim_quotes_expr
 from posthog.models import Person, PropertyDefinition
+from posthog.models.event.util import bulk_create_events
 from posthog.test.base import BaseTest, ClickhouseTestMixin
 
 
@@ -10,14 +11,32 @@ class TestMaterializedColumnsAnalyze(ClickhouseTestMixin, BaseTest):
         self.DUMMY_QUERIES = [
             (
                 f"""
-                SELECT JSONExtractString(properties, 'event_prop')
-                FROM events
+                SELECT JSONExtractString(e.properties, 'event_prop')
+                FROM events e
                 WHERE team_id = {self.team.pk}
                   AND {trim_quotes_expr("JSONExtractRaw(properties, 'another_prop')")}
                 """,
                 6723,
             ),
             (f"SELECT JSONExtractString(properties, 'person_prop') FROM person WHERE team_id = {self.team.pk}", 9723),
+            (
+                f"""
+                SELECT JSONExtractString(person_properties, 'person_prop')
+                FROM events
+                WHERE team_id = {self.team.pk}
+                  AND {trim_quotes_expr("JSONExtractRaw(person_properties, 'another_person_prop')")}
+                """,
+                6723,
+            ),
+            (
+                f"""
+                SELECT JSONExtractString(e.group0_properties, 'group_prop')
+                FROM events e
+                WHERE team_id = {self.team.pk}
+                  AND {trim_quotes_expr("JSONExtractRaw(group2_properties, 'group_prop')")}
+                """,
+                3100,
+            ),
         ]
 
         # Create property definitions
@@ -30,23 +49,60 @@ class TestMaterializedColumnsAnalyze(ClickhouseTestMixin, BaseTest):
             properties={"person_prop": "something", "$another_prop": "something"},
         )
 
+        bulk_create_events(
+            [
+                {
+                    "event": "some-event",
+                    "distinct_id": f"user_id",
+                    "team": self.team,
+                    "timestamp": "2021-05-01 00:00:00",
+                    "person_properties": {"person_prop": "something", "another_person_prop": "something"},
+                    "group0_properties": {"group_prop": "something"},
+                    "group2_properties": {"group_prop": "something2"},
+                }
+            ]
+        )
+
     def test_query_class(self):
         with self.settings(MATERIALIZE_COLUMNS_MINIMUM_QUERY_TIME=3000):
             event_query = Query(*self.DUMMY_QUERIES[0])
             person_query = Query(*self.DUMMY_QUERIES[1])
+            person_on_events_query = Query(*self.DUMMY_QUERIES[2])
+            group_on_events_query = Query(*self.DUMMY_QUERIES[3])
 
             self.assertTrue(event_query.is_valid)
             self.assertTrue(person_query.is_valid)
+            self.assertTrue(person_on_events_query.is_valid)
+            self.assertTrue(group_on_events_query.is_valid)
 
             self.assertEqual(event_query.team_id, str(self.team.pk))
             self.assertEqual(person_query.team_id, str(self.team.pk))
+            self.assertEqual(person_on_events_query.team_id, str(self.team.pk))
+            self.assertEqual(group_on_events_query.team_id, str(self.team.pk))
 
             self.assertEqual(
-                list(event_query.properties(TeamManager())), [("events", "event_prop"), ("events", "another_prop")]
+                list(event_query.properties(TeamManager())),
+                [("events", "properties", "event_prop"), ("events", "properties", "another_prop")],
             )
-            self.assertEqual(list(person_query.properties(TeamManager())), [("person", "person_prop")])
+
+            self.assertEqual(list(person_query.properties(TeamManager())), [("person", "properties", "person_prop")])
+            self.assertEqual(
+                list(person_on_events_query.properties(TeamManager())),
+                [
+                    ("person", "properties", "person_prop"),
+                    ("events", "person_properties", "person_prop"),
+                    ("events", "person_properties", "another_person_prop"),
+                ],
+            )
+            self.assertEqual(
+                list(group_on_events_query.properties(TeamManager())),
+                [("events", "group0_properties", "group_prop"), ("events", "group2_properties", "group_prop")],
+            )
+
             self.assertEqual(event_query.cost, 4)
             self.assertEqual(person_query.cost, 7)
+            self.assertEqual(person_on_events_query.cost, 4)
+            self.assertEqual(group_on_events_query.cost, 1)
 
     def test_query_class_edge_cases(self):
         invalid_query = Query("SELECT * FROM events WHERE team_id = -1", 100)
@@ -54,6 +110,14 @@ class TestMaterializedColumnsAnalyze(ClickhouseTestMixin, BaseTest):
         self.assertIsNone(invalid_query.team_id)
 
         query_with_unknown_property = Query(
-            f"SELECT JSONExtractString(properties, '$unknown_prop') FROM events WHERE team_id = {self.team.pk}", 0
+            f"SELECT JSONExtractString(properties, '$unknown_prop') FROM events WHERE team_id = {self.team.pk}", 3400
         )
         self.assertEqual(list(query_with_unknown_property.properties(TeamManager())), [])
+
+        PropertyDefinition.objects.create(team=self.team, name="prop")
+
+        # match group missing, should probably never happen, since the query is now wrong.
+        query_with_invalid_column = Query(
+            f"SELECT JSONExtractString(, 'prop') FROM events WHERE team_id = {self.team.pk}", 3340
+        )
+        self.assertEqual(list(query_with_invalid_column.properties(TeamManager())), [])

commit db8d1edf0315f3cac20e879699163cf5e2715d45

fix(?) bug introduced with 92ddf40

posthog/clickhouse/migrations/0026_fix_materialized_window_and_session_ids.py
@@ -69,8 +69,8 @@ def materialize_session_and_window_id(database):
         # materialized the column or renamed the column, and then ran the 0004_...  async migration
         # before this migration runs.
         possible_old_column_names = {"mat_" + property_name}
-        current_materialized_column_name = materialized_columns.get(property_name, None)
-        if current_materialized_column_name != property_name:
+        current_materialized_column_name = materialized_columns.get((property_name, "properties"), None)
+        if current_materialized_column_name is not None and current_materialized_column_name != property_name:
             possible_old_column_names.add(current_materialized_column_name)
 
         for possible_old_column_name in possible_old_column_names: