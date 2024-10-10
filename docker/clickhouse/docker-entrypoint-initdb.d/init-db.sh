b2db871b3f9aed4fd6ac16baba104edfd3f2c05d
Paul D'Ambra <paul@posthog.com>
2024-10-10 21:08:33 +0200

    fix: belt and braces check for symbolic link (#25524)

--- docker/clickhouse/docker-entrypoint-initdb.d/init-db.sh
+++ docker/clickhouse/docker-entrypoint-initdb.d/init-db.sh
@@ -3,5 +3,10 @@ set -e
 
 apt-get update
 apt-get -y install python3.9
-ln -s /usr/bin/python3.9 /usr/bin/python3
+
+# Check if /usr/bin/python3 already exists and if it points to python3.9
+if [[ $(readlink /usr/bin/python3) != "/usr/bin/python3.9" ]]; then
+    ln -sf /usr/bin/python3.9 /usr/bin/python3
+fi
+
 cp -r /idl/* /var/lib/clickhouse/format_schemas/
