946d4bb3f2c6beff8d975eb4b38ed619f676524c
timgl <tim@posthog.com>
2024-10-10 14:23:57 +0100

    fix

--- posthog/tasks/calculate_cohort.py
+++ posthog/tasks/calculate_cohort.py
@@ -42,6 +42,13 @@ def calculate_cohorts(parallel_count: int) -> None:
 
     # This task will be run every minute
     # Every minute, grab a few cohorts off the list and execute them
+
+    # calculate exponential backoff
+    backoff_duration = ExpressionWrapper(
+        timedelta(minutes=30) * (2 ** F("errors_calculating")),
+        output_field=DurationField(),  # type: ignore
+    )
+
     for cohort in (
         Cohort.objects.filter(
             deleted=False,
@@ -49,8 +56,7 @@ def calculate_cohorts(parallel_count: int) -> None:
             last_calculation__lte=timezone.now() - relativedelta(minutes=MAX_AGE_MINUTES),
             errors_calculating__lte=20,
             # Exponential backoff, with the first one starting after 30 minutes
-            last_error_at__lte=timezone.now()
-            - ExpressionWrapper(timedelta(minutes=30) * 2 ** F("errors_calculating"), output_field=DurationField()),  #  type: ignore
+            last_error_at__lte=timezone.now() - backoff_duration,  # type: ignore
         )
         .exclude(is_static=True)
         .order_by(F("last_calculation").asc(nulls_first=True))[0:parallel_count]
