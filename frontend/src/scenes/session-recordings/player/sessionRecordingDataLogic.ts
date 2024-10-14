52b196c7a8b8bd5889636caa2daab8a236eed16d
Paul D'Ambra <paul@posthog.com>
2024-10-14 23:07:23 +0100

    fix

--- frontend/src/scenes/session-recordings/player/sessionRecordingDataLogic.ts
+++ frontend/src/scenes/session-recordings/player/sessionRecordingDataLogic.ts
@@ -404,7 +404,7 @@ export const sessionRecordingDataLogic = kea<sessionRecordingDataLogicType>([
     key(({ sessionRecordingId }) => sessionRecordingId || 'no-session-recording-id'),
     connect({
         logic: [eventUsageLogic],
-        values: [featureFlagLogic, ['featureFlags'], teamLogic, 'currentTeam'],
+        values: [featureFlagLogic, ['featureFlags'], teamLogic, ['currentTeam']],
     }),
     defaults({
         sessionPlayerMetaData: null as SessionRecordingType | null,
