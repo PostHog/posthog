542376252d9df192acfd15117a8538f67b1f5978
Tom Owers <owerstom@gmail.com>
2024-10-15 13:29:16 +0100

    Forgot to save

--- posthog/migrations/0489_dashboard_variables.py
+++ posthog/migrations/0489_dashboard_variables.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0487_team_survey_config"),
+        ("posthog", "0488_alter_user_is_active"),
     ]
 
     operations = [
