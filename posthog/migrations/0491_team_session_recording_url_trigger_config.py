91bcd4a0e29f056fb2fd716032f280aa4607f1fb
Richard Borcsik <richard@posthog.com>
2024-10-17 11:11:09 +0200

    update migration

--- posthog/migrations/0491_team_session_recording_url_trigger_config.py
+++ posthog/migrations/0491_team_session_recording_url_trigger_config.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0487_team_survey_config"),
+        ("posthog", "0490_dashboard_variables"),
     ]
 
     operations = [
