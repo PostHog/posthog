397a009da5cd9a2a06c6a9de6e0787ee137e578f
Oliver Browne <oliver@posthog.com>
2024-10-17 17:11:24 +0300

    add management command

---
+++ posthog/management/commands/delete_persons_with_no_distinct_ids.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from posthog.models import Team, Person, PersonDistinctId
+
+
+class Command(BaseCommand):
+    help = "Delete person rows that have no associated persondistinctid rows, by team"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--team-id", default=None, type=int, help="Team ID to migrate from (on this instance)")
+
+    def handle(self, **options):
+        team_id = options["team_id"]
+
+        if not team_id:
+            raise CommandError("source Team ID is required")
+
+        team = Team.objects.get(pk=team_id)
+
+        print("Deleting persons with no distinct ids for team", team_id)  # noqa: T201
+
+        # There's a relationship from persondistinctid to person, but not in the other
+        # direction, so we have to iterate over the entire person set to find the ones
+        # that have no distinct ids
+        people = Person.objects.filter(team=team)
+
+        # Delete persons with no distinct ids
+        deleted = 0
+        for p in people:
+            if not PersonDistinctId.objects.filter(person=p).exists():
+                p.delete()
+                deleted += 1
