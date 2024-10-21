1e15a3a6cd3340119f6888117b5a61282bfee9a6
Georgiy Tarasov <gtarasov.work@gmail.com>
2024-10-21 10:48:01 +0200

    chore: sync mypy

--- posthog/celery.py
+++ posthog/celery.py
@@ -86,8 +86,7 @@ def receiver_setup_logging(loglevel, logfile, format, colorize, **kwargs) -> Non
     from posthog.settings import logs
 
     # following instructions from here https://django-structlog.readthedocs.io/en/latest/celery.html
-    # mypy thinks that there is no `logging.config` but there is ¯\_(ツ)_/¯
-    logging.config.dictConfig(logs.LOGGING)  # type: ignore
+    logging.config.dictConfig(logs.LOGGING)
 
 
 @receiver(signals.bind_extra_task_metadata)
