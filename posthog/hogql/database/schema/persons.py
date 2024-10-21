3f7580671231aeaaf7a3789549e1648b5db792a8
timgl <tim@glsr.nl>
2024-10-21 16:12:22 +0100

    perf: Speed up filtering persons (#25604)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tomás Farías Santana <tomas@tomasfarias.dev>
    Co-authored-by: Marius Andra <marius.andra@gmail.com>
    Co-authored-by: Georgiy Tarasov <gtarasov.work@gmail.com>
    Co-authored-by: David Newell <d.newell1@outlook.com>
    Co-authored-by: PostHog Bot <69588470+posthog-bot@users.noreply.github.com>
    Co-authored-by: Richard Borcsik <github.com@richardb.me>
    Co-authored-by: Michael Matloka <michael@posthog.com>
    Co-authored-by: Eric Duong <eric@posthog.com>
    Co-authored-by: Paul D'Ambra <paul@posthog.com>
    Co-authored-by: Juraj Majerik <juro.majerik@gmail.com>
    Co-authored-by: Phani Raj <phani@posthog.com>
    Co-authored-by: Tom Owers <tom@posthog.com>
    Co-authored-by: Dylan Martin <dylan@posthog.com>
    Co-authored-by: Oliver Browne <oliver@posthog.com>
    Co-authored-by: Marcus Hof <13001502+MarconLP@users.noreply.github.com>
    Co-authored-by: Michael Matloka <dev@twixes.com>
    Co-authored-by: Anirudh Pillai <anirudhx5@gmail.com>
    Co-authored-by: Sandy Spicer <sandy@posthog.com>
    Co-authored-by: Neil Kakkar <neilkakkar@gmail.com>

--- posthog/hogql/database/schema/persons.py
+++ posthog/hogql/database/schema/persons.py
@@ -5,7 +5,6 @@ from posthog.hogql.ast import SelectQuery, And, CompareOperation, CompareOperati
 from posthog.hogql.base import Expr
 from posthog.hogql.constants import HogQLQuerySettings
 from posthog.hogql.context import HogQLContext
-from posthog.hogql.database.argmax import argmax_select
 from posthog.hogql.database.models import (
     BooleanDatabaseField,
     DateTimeDatabaseField,
@@ -24,7 +23,6 @@ from posthog.hogql.database.schema.persons_pdi import PersonsPDITable, persons_p
 from posthog.hogql.errors import ResolutionError
 from posthog.hogql.visitor import clone_expr
 from posthog.models.organization import Organization
-from posthog.schema import PersonsArgMaxVersion
 
 PERSONS_FIELDS: dict[str, FieldOrTable] = {
     "id": StringDatabaseField(name="id"),
@@ -46,72 +44,73 @@ def select_from_persons_table(
     node: SelectQuery,
     *,
     filter: Optional[Expr] = None,
+    is_join: Optional[bool] = None,
 ):
-    version = context.modifiers.personsArgMaxVersion
-    if version == PersonsArgMaxVersion.AUTO:
-        version = PersonsArgMaxVersion.V1
-        # If selecting properties, use the faster v2 query. Otherwise, v1 is faster.
-        for field_chain in join_or_table.fields_accessed.values():
-            if field_chain[0] == "properties":
-                version = PersonsArgMaxVersion.V2
-                break
+    from posthog.hogql import ast
+    from posthog.hogql.parser import parse_select
 
-    if version == PersonsArgMaxVersion.V2:
-        from posthog.hogql import ast
-        from posthog.hogql.parser import parse_select
+    select = cast(
+        ast.SelectQuery,
+        parse_select(
+            """
+            SELECT id
+            FROM raw_persons
+            GROUP BY id
+            HAVING equals(argMax(raw_persons.is_deleted, raw_persons.version), 0)
+            AND argMax(raw_persons.created_at, raw_persons.version) < now() + interval 1 day
+        """
+        ),
+    )
+    select.settings = HogQLQuerySettings(optimize_aggregation_in_order=True)
 
-        select = cast(
+    # This bit optimizes the query by first selecting all IDs for all persons (regardless of whether it's the latest version), and only then aggregating the results
+    # We only do this if there are where clauses, _and_ WhereClauseExtractor can extract them
+    if node.where:
+        inner_select = cast(
             ast.SelectQuery,
             parse_select(
                 """
-            SELECT id FROM raw_persons WHERE (id, version) IN (
-               SELECT id, max(version) as version
-               FROM raw_persons
-               GROUP BY id
-               HAVING equals(argMax(raw_persons.is_deleted, raw_persons.version), 0)
-               AND argMax(raw_persons.created_at, raw_persons.version) < now() + interval 1 day
-            )
+            SELECT id
+            FROM raw_persons
+            WHERE
+                -- Much faster to pre-select out any deleted persons than doing it in aggregation
+                -- This is correct because there are no instances where we'd un-delete a person (ie there are no cases where one version has is_deleted=1 and a later version has is_deleted = 0)
+                id NOT IN (select id from raw_persons where is_deleted = 1)
             """
             ),
         )
-        select.settings = HogQLQuerySettings(optimize_aggregation_in_order=True)
-        if filter is not None:
-            cast(ast.SelectQuery, cast(ast.CompareOperation, select.where).right).where = filter
-
-        for field_name, field_chain in join_or_table.fields_accessed.items():
-            # We need to always select the 'id' field for the join constraint. The field name here is likely to
-            # be "persons__id" if anything, but just in case, let's avoid duplicates.
-            if field_name != "id":
-                select.select.append(
-                    ast.Alias(
-                        alias=field_name,
-                        expr=ast.Field(chain=field_chain),
-                    )
-                )
-    else:
-        select = argmax_select(
-            table_name="raw_persons",
-            select_fields=join_or_table.fields_accessed,
-            group_fields=["id"],
-            argmax_field="version",
-            deleted_field="is_deleted",
-            timestamp_field_to_clamp="created_at",
-        )
-        select.settings = HogQLQuerySettings(optimize_aggregation_in_order=True)
-        if filter is not None:
-            if select.where:
-                select.where = And(exprs=[select.where, filter])
-            else:
-                select.where = filter
-
-    if context.modifiers.optimizeJoinedFilters:
-        extractor = WhereClauseExtractor(context)
+        extractor = WhereClauseExtractor(context, is_join=is_join)
         extractor.add_local_tables(join_or_table)
         where = extractor.get_inner_where(node)
-        if where and select.where:
-            select.where = And(exprs=[select.where, where])
-        elif where:
-            select.where = where
+
+        if where and inner_select.where:
+            inner_select.where = ast.And(exprs=[inner_select.where, where])
+            select.where = ast.And(
+                exprs=[
+                    ast.CompareOperation(
+                        left=ast.Field(chain=["id"]), right=inner_select, op=ast.CompareOperationOp.In
+                    ),
+                    where,  # Technically, adding the where clause here is duplicative, because the outer node filters this out _again_. However, if you're trying to debug the results stay consistent throughout the query (otherwise old versions might pop up again in this subquery)
+                ]
+            )
+    if filter is not None:
+        if select.where:
+            cast(ast.SelectQuery, cast(ast.CompareOperation, select.where).right).where = ast.And(
+                exprs=[select.where, filter]
+            )
+        else:
+            select.where = filter
+
+    for field_name, field_chain in join_or_table.fields_accessed.items():
+        # We need to always select the 'id' field for the join constraint. The field name here is likely to
+        # be "persons__id" if anything, but just in case, let's avoid duplicates.
+        if field_name != "id":
+            select.select.append(
+                ast.Alias(
+                    alias=field_name,
+                    expr=ast.Call(name="argMax", args=[ast.Field(chain=field_chain), ast.Field(chain=["version"])]),
+                )
+            )
 
     return select
 
@@ -125,7 +124,7 @@ def join_with_persons_table(
 
     if not join_to_add.fields_accessed:
         raise ResolutionError("No fields requested from persons table")
-    join_expr = ast.JoinExpr(table=select_from_persons_table(join_to_add, context, node))
+    join_expr = ast.JoinExpr(table=select_from_persons_table(join_to_add, context, node, is_join=True))
 
     organization: Organization = context.team.organization if context.team else None
     # TODO: @raquelmsmith: Remove flag check and use left join for all once deletes are caught up
@@ -224,9 +223,16 @@ class PersonsTable(LazyTable):
         return self
 
     def lazy_select(self, table_to_add: LazyTableToAdd, context, node):
+        # assume that if the select_from is not persons table we're doing a join
+        try:
+            is_join = not isinstance(node.select_from.type.table, PersonsTable)
+        except AttributeError:
+            is_join = False
         if self.filter is not None:
-            return select_from_persons_table(table_to_add, context, node, filter=clone_expr(self.filter, True))
-        return select_from_persons_table(table_to_add, context, node)
+            return select_from_persons_table(
+                table_to_add, context, node, filter=clone_expr(self.filter, True), is_join=is_join
+            )
+        return select_from_persons_table(table_to_add, context, node, is_join=is_join)
 
     def to_printed_clickhouse(self, context):
         return "person"
