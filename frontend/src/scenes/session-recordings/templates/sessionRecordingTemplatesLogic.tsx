6559327d5c5cc50e8e0ac3fc9e801fb9bff2f747
Paul D'Ambra <paul@posthog.com>
2024-10-22 09:37:51 +0100

    fixes

--- frontend/src/scenes/session-recordings/templates/sessionRecordingTemplatesLogic.tsx
+++ frontend/src/scenes/session-recordings/templates/sessionRecordingTemplatesLogic.tsx
@@ -117,6 +117,7 @@ export const sessionReplayTemplatesLogic = kea<sessionReplayTemplatesLogicType>(
                             },
                         ],
                     },
+                    // TODO this should set order on the filter group after https://github.com/PostHog/posthog/pull/25701
                 }
                 return filterGroup
             },
