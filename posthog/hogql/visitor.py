367fcaadb77c6bd82ddcf8e5e90d96a4aabacb94
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2024-10-23 12:43:17 -0700

    fix: Add to `visit_interval_type` to `TraversingVisitor`

--- posthog/hogql/visitor.py
+++ posthog/hogql/visitor.py
@@ -236,6 +236,9 @@ class TraversingVisitor(Visitor[None]):
     def visit_date_time_type(self, node: ast.DateTimeType):
         pass
 
+    def visit_interval_type(self, node: ast.DateTimeType):
+        pass
+
     def visit_uuid_type(self, node: ast.UUIDType):
         pass
 
