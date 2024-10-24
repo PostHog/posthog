c9843c4fe1482b84435bbd52e6abd0c1141a03ec
Paul D'Ambra <paul@posthog.com>
2024-10-24 15:08:52 +0100

    tooltip

--- frontend/src/scenes/session-recordings/player/sidebar/PlayerSidebarOverviewTab.tsx
+++ frontend/src/scenes/session-recordings/player/sidebar/PlayerSidebarOverviewTab.tsx
@@ -1,7 +1,9 @@
 import { PersonDisplay } from '@posthog/apps-common'
+import { IconInfo } from '@posthog/icons'
 import { useValues } from 'kea'
 import { PropertiesTable } from 'lib/components/PropertiesTable'
 import { LemonSkeleton } from 'lib/lemon-ui/LemonSkeleton'
+import { Tooltip } from 'lib/lemon-ui/Tooltip'
 import { PlayerSidebarSessionSummary } from 'scenes/session-recordings/player/sidebar/PlayerSidebarSessionSummary'
 
 import { PropertyDefinitionType } from '~/types'
@@ -19,7 +21,11 @@ export function PlayerSidebarOverviewTab(): JSX.Element {
             <PlayerSidebarOverviewGrid />
             <PlayerSidebarSessionSummary />
             <div className="font-bold bg-bg-light px-2 border-b py-3">
-                <h2>Latest person properties</h2>
+                <Tooltip title="These are the person properties right now. They might have changed and not match the person properties at the time of recording.">
+                    <h2>
+                        Latest person properties <IconInfo />
+                    </h2>
+                </Tooltip>
                 <PersonDisplay person={sessionPerson} withIcon noPopover />
             </div>
             {sessionPlayerMetaDataLoading ? (
