f645c5d030fc4e3b29e5881784db4d7add9307e6
Neil Kakkar <neilkakkar@gmail.com>
2024-10-24 13:41:43 +0100

    edit migration

--- posthog/migrations/0497_experimentholdout_experiment_holdout.py
+++ posthog/migrations/0497_experimentholdout_experiment_holdout.py
@@ -7,6 +7,7 @@ import django.utils.timezone
 
 
 class Migration(migrations.Migration):
+    atomic = False  # Added to support concurrent index creation
     dependencies = [
         ("posthog", "0496_team_person_processing_opt_out"),
     ]
@@ -30,11 +31,36 @@ class Migration(migrations.Migration):
                 ("team", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="posthog.team")),
             ],
         ),
-        migrations.AddField(
-            model_name="experiment",
-            name="holdout",
-            field=models.ForeignKey(
-                null=True, on_delete=django.db.models.deletion.SET_NULL, to="posthog.experimentholdout"
-            ),
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddField(
+                    model_name="experiment",
+                    name="holdout",
+                    field=models.ForeignKey(
+                        null=True, on_delete=django.db.models.deletion.SET_NULL, to="posthog.experimentholdout"
+                    ),
+                ),
+            ],
+            database_operations=[
+                # We add -- existing-table-constraint-ignore to ignore the constraint validation in CI.
+                migrations.RunSQL(
+                    """
+                    ALTER TABLE "posthog_experiment" ADD COLUMN "holdout_id" integer NULL CONSTRAINT "posthog_experiment_holdout_id_ffd173dd_fk_posthog_e" REFERENCES "posthog_experimentholdout"("id") DEFERRABLE INITIALLY DEFERRED; -- existing-table-constraint-ignore
+                    SET CONSTRAINTS "posthog_experiment_holdout_id_ffd173dd_fk_posthog_e" IMMEDIATE; -- existing-table-constraint-ignore
+                    """,
+                    reverse_sql="""
+                        ALTER TABLE "posthog_experiment" DROP COLUMN IF EXISTS "holdout_id";
+                    """,
+                ),
+                # We add CONCURRENTLY to the create command
+                migrations.RunSQL(
+                    """
+                    CREATE INDEX CONCURRENTLY "posthog_experiment_holdout_id_ffd173dd_fk_posthog_e" ON "posthog_experiment" ("holdout_id");
+                    """,
+                    reverse_sql="""
+                        DROP INDEX IF EXISTS "posthog_experiment_holdout_id_ffd173dd_fk_posthog_e";
+                    """,
+                ),
+            ],
         ),
     ]
