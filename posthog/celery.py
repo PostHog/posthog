8d4f52e6991268ab4722aeec35ffaf79b7582ec5
Georgiy Tarasov <gtarasov.work@gmail.com>
2024-10-24 10:44:27 +0200

    fix: mypy glitch

--- posthog/celery.py
+++ posthog/celery.py
@@ -1,6 +1,5 @@
 import os
 import time
-from typing import Any, cast
 
 import structlog
 from celery import Celery
@@ -82,13 +81,12 @@ task_timings: dict[str, float] = {}
 
 @setup_logging.connect
 def receiver_setup_logging(loglevel, logfile, format, colorize, **kwargs) -> None:
-    import logging
+    from logging import config as logging_config
 
     from posthog.settings import logs
 
     # following instructions from here https://django-structlog.readthedocs.io/en/latest/celery.html
-    # mypy thinks that there is no `logging.config` but there is ¯\_(ツ)_/¯
-    cast(Any, logging).config.dictConfig(logs.LOGGING)
+    logging_config.dictConfig(logs.LOGGING)
 
 
 @receiver(signals.bind_extra_task_metadata)
