0ff33b4ac3502a5ee9dae9ca5f5e1c3a47644b90
Marius Andra <marius.andra@gmail.com>
2024-10-24 21:22:06 +0200

    went down

--- plugin-server/tests/cdp/hog-executor.test.ts
+++ plugin-server/tests/cdp/hog-executor.test.ts
@@ -106,7 +106,7 @@ describe('Hog Executor', () => {
                 {
                     timestamp: expect.any(DateTime),
                     level: 'debug',
-                    message: "Suspending function due to async function call 'fetch'. Payload: 1918 bytes",
+                    message: "Suspending function due to async function call 'fetch'. Payload: 1872 bytes",
                 },
             ])
         })
@@ -187,7 +187,7 @@ describe('Hog Executor', () => {
             expect(logs.map((log) => log.message)).toMatchInlineSnapshot(`
                 Array [
                   "Executing function",
-                  "Suspending function due to async function call 'fetch'. Payload: 1918 bytes",
+                  "Suspending function due to async function call 'fetch'. Payload: 1872 bytes",
                   "Resuming function",
                   "Fetch response:, {\\"status\\":200,\\"body\\":\\"success\\"}",
                   "Function completed in 100ms. Sync: 0ms. Mem: 779 bytes. Ops: 22. Event: 'http://localhost:8000/events/1'",
@@ -206,7 +206,7 @@ describe('Hog Executor', () => {
             expect(logs.map((log) => log.message)).toMatchInlineSnapshot(`
                 Array [
                   "Executing function",
-                  "Suspending function due to async function call 'fetch'. Payload: 1918 bytes",
+                  "Suspending function due to async function call 'fetch'. Payload: 1872 bytes",
                   "Resuming function",
                   "Fetch response:, {\\"status\\":200,\\"body\\":{\\"foo\\":\\"bar\\"}}",
                   "Function completed in 100ms. Sync: 0ms. Mem: 779 bytes. Ops: 22. Event: 'http://localhost:8000/events/1'",
