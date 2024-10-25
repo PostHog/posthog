712ce06188454cf554a532e2d06817d3a7392788
dylan <dylan@posthog.com>
2024-10-25 09:26:16 -0700

    a bit more

--- rust/feature-flags/src/lib.rs
+++ rust/feature-flags/src/lib.rs
@@ -9,9 +9,3 @@ pub mod router;
 pub mod server;
 pub mod teams;
 pub mod utils;
-// Test modules don't need to be compiled with main binary
-// #[cfg(test)]
-// TODO: To use in integration tests, we need to compile with binary
-// or make it a separate feature using cfg(feature = "integration-tests")
-// and then use this feature only in tests.
-// For now, ok to just include in binary
