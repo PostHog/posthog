9a1248c139901a022956880f44e90e24b4e39d69
Oliver Browne <oliver@posthog.com>
2024-10-28 16:05:52 +0200

    rm framestore

--- rust/cymbal/src/frame_store/mod.rs
+++
@@ -1,9 +0,0 @@
-use axum::async_trait;
-
-use crate::{error::Error, types::frames::Frame};
-
-#[async_trait]
-pub trait FrameStore: Send + Sync + 'static {
-    async fn store(&self, team_id: i32, id: String) -> ();
-    async fn fetch(&self, team_id: i32, id: String) -> Result<Option<Frame>, Error>;
-}
