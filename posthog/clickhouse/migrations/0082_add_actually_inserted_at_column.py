95cce9dd09b9972e39958ab1461cb903aea93764
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-10-28 11:33:42 +0100

    fix(clickhouse): Typo in table name

--- posthog/clickhouse/migrations/0082_add_actually_inserted_at_column.py
+++ posthog/clickhouse/migrations/0082_add_actually_inserted_at_column.py
@@ -22,7 +22,7 @@ operations = [
     run_sql_with_exceptions(f"DROP TABLE IF EXISTS events_json_mv ON CLUSTER '{CLICKHOUSE_CLUSTER}'"),
     run_sql_with_exceptions(f"DROP TABLE IF EXISTS kafka_events_json ON CLUSTER '{CLICKHOUSE_CLUSTER}'"),
     run_sql_with_exceptions(ADD_COLUMNS_BASE_SQL.format(table="events", cluster=CLICKHOUSE_CLUSTER)),
-    run_sql_with_exceptions(DROP_COLUMN_BASE_SQL.format(table="writeable_events", cluster=CLICKHOUSE_CLUSTER)),
+    run_sql_with_exceptions(DROP_COLUMN_BASE_SQL.format(table="writable_events", cluster=CLICKHOUSE_CLUSTER)),
     run_sql_with_exceptions(ADD_COLUMNS_BASE_SQL.format(table="sharded_events", cluster=CLICKHOUSE_CLUSTER)),
     run_sql_with_exceptions(KAFKA_EVENTS_TABLE_JSON_SQL()),
     run_sql_with_exceptions(EVENTS_TABLE_JSON_MV_SQL()),
