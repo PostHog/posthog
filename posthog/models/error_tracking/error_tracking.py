54883d57f7ba1e0303dae759081028ea45138d16
Oliver Browne <oliver@posthog.com>
2024-10-28 16:04:07 +0200

    revert model change

--- posthog/models/error_tracking/error_tracking.py
+++ posthog/models/error_tracking/error_tracking.py
@@ -60,48 +60,9 @@ class ErrorTrackingGroup(UUIDModel):
         self.save()
 
 
-class ErrorTrackingSymbolSet(UUIDModel):
-    class StorageMethod(models.TextChoices):
-        S3 = "s3", "s3"
-        FILE = "file", "file"
-
-    # Derived from the symbol set reference
-    ref = models.TextField(null=False, blank=False)
-    team = models.ForeignKey("Team", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    # How we stored this symbol set, and where to look for it
-    # These are null if we failed to find a symbol set for a given reference. We store a
-    # row anyway, so if someone comes along later and uploads a symbol set for this reference,
-    # we can know which frame resolution results below to drop.
-    storage_method = models.TextField(choices=StorageMethod.choices, default=StorageMethod.S3, null=True)
-    storage_ptr = models.TextField(null=True, blank=False)
-
-    class Meta:
-        indexes = [
-            models.Index(fields=["team_id", "id"]),
-        ]
-
-        constraints = [
-            models.UniqueConstraint(fields=["team_id", "id"], name="unique_id_per_team"),
-        ]
-
-
-class ErrorTrackingStackFrame(UUIDModel):
-    # Produced by a raw frame
-    raw_id = models.TextField(null=False, blank=False)
-    team = models.ForeignKey("Team", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    symbol_set = models.ForeignKey("ErrorTrackingSymbolSet", on_delete=models.CASCADE, null=True)
-    contents = models.JSONField(null=False, blank=False)
-    resolved = models.BooleanField(null=False, blank=False)
-
-    class Meta:
-        indexes = [
-            models.Index(fields=["team_id", "raw_id"]),
-        ]
-
-        constraints = [
-            models.UniqueConstraint(fields=["team_id", "raw_id"], name="unique_raw_id_per_team"),
-        ]
+class ErrorTrackingIssueFingerprint(models.Model):
+    team = models.ForeignKey("Team", on_delete=models.CASCADE, db_index=False)
+    issue = models.ForeignKey(ErrorTrackingGroup, on_delete=models.CASCADE)
+    fingerprint = models.TextField(null=False, blank=False)
+    # current version of the id, used to sync with ClickHouse and collapse rows correctly for overrides ClickHouse table
+    version = models.BigIntegerField(blank=True, default=0)
