92804d30644b05b5a9e2ddc1cba90618485c9f5a
Michael Matloka <michael@posthog.com>
2024-11-06 14:53:30 +0100

    Update ingestion tests

--- plugin-server/tests/main/ingestion-queues/each-batch-webhooks.test.ts
+++ plugin-server/tests/main/ingestion-queues/each-batch-webhooks.test.ts
@@ -1,5 +1,11 @@
 import { eachMessageWebhooksHandlers } from '../../../src/main/ingestion-queues/batch-processing/each-batch-webhooks'
-import { ClickHouseTimestamp, ClickHouseTimestampSecondPrecision, Hub, RawKafkaEvent } from '../../../src/types'
+import {
+    ClickHouseTimestamp,
+    ClickHouseTimestampSecondPrecision,
+    Hub,
+    ProjectId,
+    RawKafkaEvent,
+} from '../../../src/types'
 import { closeHub, createHub } from '../../../src/utils/db/hub'
 import { PostgresUse } from '../../../src/utils/db/postgres'
 import { ActionManager } from '../../../src/worker/ingestion/action-manager'
@@ -23,7 +29,7 @@ const kafkaEvent: RawKafkaEvent = {
     elements_chain: '',
     timestamp: '2020-02-23 02:15:00.00' as ClickHouseTimestamp,
     team_id: 2,
-    project_id: 1,
+    project_id: 2 as ProjectId,
     distinct_id: 'my_id',
     created_at: '2020-02-23 02:15:00.00' as ClickHouseTimestamp,
     person_id: 'F99FA0A1-E0C2-4CFE-A09A-4C3C4327A4CC',
@@ -169,7 +175,7 @@ describe('eachMessageWebhooksHandlers', () => {
               "person_created_at": "2020-02-20T02:15:00.000Z",
               "person_id": "F99FA0A1-E0C2-4CFE-A09A-4C3C4327A4CC",
               "person_properties": Object {},
-              "projectId": 1,
+              "projectId": 2,
               "properties": Object {
                 "$groups": Object {
                   "organization": "org_posthog",
