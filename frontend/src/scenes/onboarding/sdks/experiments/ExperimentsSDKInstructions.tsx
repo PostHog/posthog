87f267432bd76dbeea1c731d1e98037bd4fc884b
Daniel Bachhuber <daniel.b@posthog.com>
2024-11-06 12:34:33 -0800

    Add several more examples

--- frontend/src/scenes/onboarding/sdks/experiments/ExperimentsSDKInstructions.tsx
+++ frontend/src/scenes/onboarding/sdks/experiments/ExperimentsSDKInstructions.tsx
@@ -1,7 +1,12 @@
 import { SDKInstructionsMap, SDKKey } from '~/types'
 
+import { ExperimentsAngularInstructions } from './angular'
+import { ExperimentsAstroInstructions } from './astro'
+import { ExperimentsBubbleInstructions } from './bubble'
+import { ExperimentsDjangoInstructions } from './django'
 import { ExperimentsGoInstructions } from './go'
 import { ExperimentsJSWebInstructions } from './js-web'
+import { ExperimentsLaravelInstructions } from './laravel'
 import { ExperimentsNodeJSInstructions } from './nodejs'
 import { ExperimentsPHPInstructions } from './php'
 import { ExperimentsPythonInstructions } from './python'
@@ -10,7 +15,12 @@ import { ExperimentsRubyInstructions } from './ruby'
 
 export const ExperimentsSDKInstructions: SDKInstructionsMap = {
     [SDKKey.JS_WEB]: ExperimentsJSWebInstructions,
+    [SDKKey.ANGULAR]: ExperimentsAngularInstructions,
+    [SDKKey.ASTRO]: ExperimentsAstroInstructions,
+    [SDKKey.BUBBLE]: ExperimentsBubbleInstructions,
+    [SDKKey.DJANGO]: ExperimentsDjangoInstructions,
     [SDKKey.GO]: ExperimentsGoInstructions,
+    [SDKKey.LARAVEL]: ExperimentsLaravelInstructions,
     [SDKKey.NODE_JS]: ExperimentsNodeJSInstructions,
     [SDKKey.PHP]: ExperimentsPHPInstructions,
     [SDKKey.PYTHON]: ExperimentsPythonInstructions,
