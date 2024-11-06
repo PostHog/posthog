b4904ee605cae6d265039a94c43a4532e4883c53
Daniel Bachhuber <daniel.b@posthog.com>
2024-11-06 11:56:46 -0800

    Add more SDK instructions

--- frontend/src/scenes/onboarding/sdks/experiments/ExperimentsSDKInstructions.tsx
+++ frontend/src/scenes/onboarding/sdks/experiments/ExperimentsSDKInstructions.tsx
@@ -1,9 +1,19 @@
 import { SDKInstructionsMap, SDKKey } from '~/types'
 
+import { ExperimentsGoInstructions } from './go'
 import { ExperimentsJSWebInstructions } from './js-web'
+import { ExperimentsNodeJSInstructions } from './nodejs'
+import { ExperimentsPHPInstructions } from './php'
 import { ExperimentsPythonInstructions } from './python'
+import { ExperimentsReactNativeInstructions } from './react-native'
+import { ExperimentsRubyInstructions } from './ruby'
 
 export const ExperimentsSDKInstructions: SDKInstructionsMap = {
     [SDKKey.JS_WEB]: ExperimentsJSWebInstructions,
+    [SDKKey.GO]: ExperimentsGoInstructions,
+    [SDKKey.NODE_JS]: ExperimentsNodeJSInstructions,
+    [SDKKey.PHP]: ExperimentsPHPInstructions,
     [SDKKey.PYTHON]: ExperimentsPythonInstructions,
+    [SDKKey.REACT_NATIVE]: ExperimentsReactNativeInstructions,
+    [SDKKey.RUBY]: ExperimentsRubyInstructions,
 }
