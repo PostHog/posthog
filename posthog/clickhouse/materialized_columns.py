8fe3c32817f6c420e4fb7ee71de4549a6683af19
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2024-11-07 10:06:35 -0500

    prepare to split function signatures

--- posthog/clickhouse/materialized_columns.py
+++ posthog/clickhouse/materialized_columns.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 from posthog.cache_utils import cache_for
-from posthog.models.property import TableWithProperties
+from posthog.models.property import PropertyName, TableColumn, TableWithProperties
 from posthog.settings import EE_AVAILABLE
 
 
@@ -11,7 +11,6 @@ TablesWithMaterializedColumns = TableWithProperties
 if EE_AVAILABLE:
     from ee.clickhouse.materialized_columns.columns import get_materialized_columns
 else:
-    from posthog.models.property import PropertyName, TableColumn
 
     def get_materialized_columns(
         table: TablesWithMaterializedColumns,
@@ -19,4 +18,8 @@ else:
         return {}
 
 
-get_materialized_columns_cached = cache_for(timedelta(minutes=15))(get_materialized_columns)
+@cache_for(timedelta(minutes=15))
+def get_materialized_columns_cached(
+    table: TablesWithMaterializedColumns,
+) -> dict[tuple[PropertyName, TableColumn], ColumnName]:
+    return get_materialized_columns(table)
