6e322cc220b81643f0ba0679010693c2422198c6
Phani Raj <phani@posthog.com>
2024-11-08 13:07:57 -0600

    do not return completed web experiments

--- posthog/api/web_experiment.py
+++ posthog/api/web_experiment.py
@@ -186,7 +186,10 @@ def web_experiments(request: Request):
             )
 
         result = WebExperimentsAPISerializer(
-            WebExperiment.objects.filter(team_id=team.id).exclude(archived=True).select_related("feature_flag"),
+            WebExperiment.objects.filter(team_id=team.id)
+            .exclude(archived=True)
+            .exclude("end_date__isnull", False)
+            .select_related("feature_flag"),
             many=True,
         ).data
 
