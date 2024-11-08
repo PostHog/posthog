ea2eab6d04b1790fb330ea2305939d455c736785
Daniel Bachhuber <daniel.b@posthog.com>
2024-11-08 07:35:35 -0800

    Add more SDKs

--- frontend/src/scenes/onboarding/sdks/experiments/ExperimentsSDKInstructions.tsx
+++ frontend/src/scenes/onboarding/sdks/experiments/ExperimentsSDKInstructions.tsx
@@ -1,29 +1,39 @@
 import { SDKInstructionsMap, SDKKey } from '~/types'
 
+import { ExperimentsAndroidInstructions } from './android'
 import { ExperimentsAngularInstructions } from './angular'
 import { ExperimentsAstroInstructions } from './astro'
 import { ExperimentsBubbleInstructions } from './bubble'
 import { ExperimentsDjangoInstructions } from './django'
 import { ExperimentsGoInstructions } from './go'
+import { ExperimentsIOSInstructions } from './ios'
 import { ExperimentsJSWebInstructions } from './js-web'
 import { ExperimentsLaravelInstructions } from './laravel'
+import { ExperimentsNextJSInstructions } from './next-js'
 import { ExperimentsNodeJSInstructions } from './nodejs'
+import { ExperimentsNuxtInstructions } from './nuxt'
 import { ExperimentsPHPInstructions } from './php'
 import { ExperimentsPythonInstructions } from './python'
+import { ExperimentsReactInstructions } from './react'
 import { ExperimentsReactNativeInstructions } from './react-native'
 import { ExperimentsRubyInstructions } from './ruby'
 
 export const ExperimentsSDKInstructions: SDKInstructionsMap = {
     [SDKKey.JS_WEB]: ExperimentsJSWebInstructions,
+    [SDKKey.ANDROID]: ExperimentsAndroidInstructions,
     [SDKKey.ANGULAR]: ExperimentsAngularInstructions,
     [SDKKey.ASTRO]: ExperimentsAstroInstructions,
     [SDKKey.BUBBLE]: ExperimentsBubbleInstructions,
     [SDKKey.DJANGO]: ExperimentsDjangoInstructions,
     [SDKKey.GO]: ExperimentsGoInstructions,
+    [SDKKey.IOS]: ExperimentsIOSInstructions,
     [SDKKey.LARAVEL]: ExperimentsLaravelInstructions,
+    [SDKKey.NEXT_JS]: ExperimentsNextJSInstructions,
     [SDKKey.NODE_JS]: ExperimentsNodeJSInstructions,
+    [SDKKey.NUXT_JS]: ExperimentsNuxtInstructions,
     [SDKKey.PHP]: ExperimentsPHPInstructions,
     [SDKKey.PYTHON]: ExperimentsPythonInstructions,
+    [SDKKey.REACT]: ExperimentsReactInstructions,
     [SDKKey.REACT_NATIVE]: ExperimentsReactNativeInstructions,
     [SDKKey.RUBY]: ExperimentsRubyInstructions,
 }
