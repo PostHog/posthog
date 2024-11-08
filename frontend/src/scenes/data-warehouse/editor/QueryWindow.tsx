a008e95b8fff11aa8618421eeb075d7672379f6e
eric <eeoneric@gmail.com>
2024-11-08 17:33:58 -0500

    refactor logic

--- frontend/src/scenes/data-warehouse/editor/QueryWindow.tsx
+++ frontend/src/scenes/data-warehouse/editor/QueryWindow.tsx
@@ -1,19 +1,10 @@
 import { Monaco } from '@monaco-editor/react'
-import { BindLogic, useActions, useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import { router } from 'kea-router'
-import {
-    activemodelStateKey,
-    codeEditorLogic,
-    CodeEditorLogicProps,
-    editorModelsStateKey,
-} from 'lib/monaco/codeEditorLogic'
-import type { editor as importedEditor, Uri } from 'monaco-editor'
-import { useCallback, useEffect, useState } from 'react'
+import type { editor as importedEditor } from 'monaco-editor'
+import { useState } from 'react'
 
-import { dataNodeLogic } from '~/queries/nodes/DataNode/dataNodeLogic'
-import { hogQLQueryEditorLogic } from '~/queries/nodes/HogQLQuery/hogQLQueryEditorLogic'
-import { HogQLQuery, NodeKind } from '~/queries/schema'
-
+import { multitabEditorLogic } from './multitabEditorLogic'
 import { QueryPane } from './QueryPane'
 import { QueryTabs } from './QueryTabs'
 import { ResultPane } from './ResultPane'
@@ -24,152 +15,51 @@ export function QueryWindow(): JSX.Element {
     )
     const [monaco, editor] = monacoAndEditor ?? []
 
-    const key = router.values.location.pathname
-
-    const [query, setActiveQueryInput] = useState<HogQLQuery>({
-        kind: NodeKind.HogQLQuery,
-        query: '',
-    })
-
-    const hogQLQueryEditorLogicProps = {
-        query,
-        setQuery: (query: HogQLQuery) => {
-            setActiveQueryInput(query)
-        },
-        onChange: () => {},
-        key,
-    }
-    const logic = hogQLQueryEditorLogic(hogQLQueryEditorLogicProps)
-    const { queryInput, promptError } = useValues(logic)
-    const { setQueryInput, saveQuery, saveAsView } = useActions(logic)
-
     const codeEditorKey = `hogQLQueryEditor/${router.values.location.pathname}`
 
-    const codeEditorLogicProps: CodeEditorLogicProps = {
+    const logic = multitabEditorLogic({
         key: codeEditorKey,
-        sourceQuery: query,
-        query: queryInput,
-        language: 'hogQL',
-        metadataFilters: query.filters,
         monaco,
         editor,
-        multitab: true,
-    }
-    const { activeModelUri, allModels, hasErrors, error, isValidView } = useValues(
-        codeEditorLogic(codeEditorLogicProps)
-    )
-
-    const { createModel, setModel, deleteModel, setModels, addModel, updateState } = useActions(
-        codeEditorLogic(codeEditorLogicProps)
-    )
-
-    const modelKey = `hogQLQueryEditor/${activeModelUri?.path}`
-
-    useEffect(() => {
-        if (monaco && activeModelUri) {
-            const _model = monaco.editor.getModel(activeModelUri)
-            const val = _model?.getValue()
-            setQueryInput(val ?? '')
-            saveQuery()
-        }
-    }, [activeModelUri])
-
-    const onAdd = useCallback(() => {
-        createModel()
-    }, [createModel])
+    })
+    const { allTabs, activeModelUri, queryInput, activeQuery, activeTabKey } = useValues(logic)
+    const { selectTab, deleteTab, createTab, setQueryInput, runQuery } = useActions(logic)
 
     return (
         <div className="flex flex-1 flex-col h-full">
             <QueryTabs
-                models={allModels}
-                onClick={setModel}
-                onClear={deleteModel}
-                onAdd={onAdd}
+                models={allTabs}
+                onClick={selectTab}
+                onClear={deleteTab}
+                onAdd={createTab}
                 activeModelUri={activeModelUri}
             />
             <QueryPane
                 queryInput={queryInput}
-                promptError={promptError}
+                promptError={null}
                 codeEditorProps={{
                     onChange: (v) => {
                         setQueryInput(v ?? '')
-                        updateState()
                     },
                     onMount: (editor, monaco) => {
                         setMonacoAndEditor([monaco, editor])
-
-                        const allModelQueries = localStorage.getItem(editorModelsStateKey(codeEditorKey))
-                        const activeModelUri = localStorage.getItem(activemodelStateKey(codeEditorKey))
-
-                        if (allModelQueries) {
-                            // clear existing models
-                            monaco.editor.getModels().forEach((model) => {
-                                model.dispose()
-                            })
-
-                            const models = JSON.parse(allModelQueries || '[]')
-                            const newModels: Uri[] = []
-
-                            models.forEach((model: Record<string, any>) => {
-                                if (monaco) {
-                                    const uri = monaco.Uri.parse(model.path)
-                                    const newModel = monaco.editor.createModel(model.query, 'hogQL', uri)
-                                    editor?.setModel(newModel)
-                                    newModels.push(uri)
-                                }
-                            })
-
-                            setModels(newModels)
-
-                            if (activeModelUri) {
-                                const uri = monaco.Uri.parse(activeModelUri)
-                                const activeModel = monaco.editor
-                                    .getModels()
-                                    .find((model) => model.uri.path === uri.path)
-                                activeModel && editor?.setModel(activeModel)
-                                const val = activeModel?.getValue()
-                                if (val) {
-                                    setQueryInput(val)
-                                    saveQuery()
-                                }
-                                setModel(uri)
-                            } else if (newModels.length) {
-                                setModel(newModels[0])
-                            }
-                        } else {
-                            const model = editor.getModel()
-
-                            if (model) {
-                                addModel(model.uri)
-                                setModel(model.uri)
-                            }
-                        }
                     },
                     onPressCmdEnter: (value, selectionType) => {
                         if (value && selectionType === 'selection') {
-                            saveQuery(value)
+                            runQuery(value)
                         } else {
-                            saveQuery()
+                            runQuery()
                         }
                     },
                 }}
             />
-            <BindLogic
-                logic={dataNodeLogic}
-                props={{
-                    key: modelKey,
-                    query: query,
-                    doNotLoad: !query.query,
-                }}
-            >
-                <ResultPane
-                    onQueryInputChange={saveQuery}
-                    onSave={saveAsView}
-                    saveDisabledReason={
-                        hasErrors ? error ?? 'Query has errors' : !isValidView ? 'All fields must have an alias' : ''
-                    }
-                />
-            </BindLogic>
+            <ResultPane
+                logicKey={activeTabKey}
+                query={activeQuery ?? ''}
+                onQueryInputChange={runQuery}
+                onSave={() => {}}
+                saveDisabledReason=""
+            />
         </div>
     )
 }
---
+++ frontend/src/scenes/data-warehouse/editor/multitabEditorLogic.ts
@@ -0,0 +1,206 @@
+import { Monaco } from '@monaco-editor/react'
+import { actions, kea, listeners, path, props, propsChanged, reducers, selectors } from 'kea'
+import { subscriptions } from 'kea-subscriptions'
+import { editor, Uri } from 'monaco-editor'
+import { Editor } from 'react-email-editor'
+
+import { dataNodeLogic } from '~/queries/nodes/DataNode/dataNodeLogic'
+import { NodeKind } from '~/queries/schema'
+
+import type { multitabEditorLogicType } from './multitabEditorLogicType'
+
+interface MultitabEditorLogicProps {
+    key: string
+    monaco?: Monaco
+    editor?: Editor
+}
+
+export const editorModelsStateKey = (key: string | number): string => `${key}/editorModelQueries`
+export const activemodelStateKey = (key: string | number): string => `${key}/activeModelUri`
+
+export const multitabEditorLogic = kea<multitabEditorLogicType>([
+    path(['data-warehouse', 'editor', 'multitabEditorLogic']),
+    props({} as MultitabEditorLogicProps),
+    actions({
+        setQueryInput: (queryInput: string) => ({ queryInput }),
+        runQuery: (queryOverride?: string) => ({ queryOverride }),
+        setActiveQuery: (query: string) => ({ query }),
+        setTabs: (tabs: Uri[]) => ({ tabs }),
+        createTab: () => null,
+        deleteTab: (tab: Uri) => ({ tab }),
+        selectTab: (tab: Uri) => ({ tab }),
+        setLocalState: (key: string, value: any) => ({ key, value }),
+        initialize: true,
+    }),
+    propsChanged(({ actions }, oldProps) => {
+        if (!oldProps.monaco && !oldProps.editor) {
+            actions.initialize()
+        }
+    }),
+    reducers({
+        queryInput: [
+            '',
+            {
+                setQueryInput: (_, { queryInput }) => queryInput,
+            },
+        ],
+        activeQuery: [
+            null as string | null,
+            {
+                setActiveQuery: (_, { query }) => query,
+            },
+        ],
+        activeModelUri: [
+            null as Uri | null,
+            {
+                selectTab: (_, { tab }) => tab,
+            },
+        ],
+        allTabs: [
+            [] as Uri[],
+            {
+                addTab: (state, { tab }) => {
+                    const newTabs = [...state, tab]
+                    return newTabs
+                },
+                removeTab: (state, { tab: tabToRemove }) => {
+                    const newModels = state.filter((tab) => tab.toString() !== tabToRemove.toString())
+                    return newModels
+                },
+                setTabs: (_, { tabs }) => tabs,
+            },
+        ],
+    }),
+    listeners(({ values, props, actions }) => ({
+        createTab: () => {
+            let currentModelCount = 1
+            const allNumbers = values.allTabs.map((tab) => parseInt(tab.path.split('/').pop() || '0'))
+            while (allNumbers.includes(currentModelCount)) {
+                currentModelCount++
+            }
+
+            if (props.monaco) {
+                const uri = props.monaco.Uri.parse(currentModelCount.toString())
+                const model = props.monaco.editor.createModel('', props.language, uri)
+                props.editor?.setModel(model)
+                actions.selectTab(uri)
+
+                const queries = values.allTabs.map((tab) => {
+                    return {
+                        query: props.monaco?.editor.getModel(tab)?.getValue() || {
+                            kind: NodeKind.HogQLQuery,
+                            query: '',
+                        },
+                        path: tab.path.split('/').pop(),
+                    }
+                })
+                actions.setLocalState(editorModelsStateKey(props.key), JSON.stringify(queries))
+            }
+        },
+        selectTab: ({ tab }) => {
+            if (props.monaco) {
+                const model = props.monaco.editor.getModel(tab)
+                props.editor?.setModel(model)
+            }
+
+            const path = tab.path.split('/').pop()
+            path && actions.setLocalState(activemodelStateKey(props.key), path)
+        },
+        deleteTab: ({ tab: tabToRemove }) => {
+            if (props.monaco) {
+                const model = props.monaco.editor.getModel(tabToRemove)
+                if (tabToRemove == values.activeModelUri) {
+                    const indexOfModel = values.allTabs.findIndex((tab) => tab.toString() === tabToRemove.toString())
+                    const nextModel =
+                        values.allTabs[indexOfModel + 1] || values.allTabs[indexOfModel - 1] || values.allTabs[0] // there will always be one
+                    actions.selectTab(nextModel)
+                }
+                model?.dispose()
+                const queries = values.allTabs.map((tab) => {
+                    return {
+                        query: props.monaco?.editor.getModel(tab)?.getValue() || {
+                            kind: NodeKind.HogQLQuery,
+                            query: '',
+                        },
+                        path: tab.path.split('/').pop(),
+                    }
+                })
+                actions.setLocalState(editorModelsStateKey(props.key), JSON.stringify(queries))
+            }
+        },
+        setLocalState: ({ key, value }) => {
+            localStorage.setItem(key, value)
+        },
+        initialize: () => {
+            const allModelQueries = localStorage.getItem(editorModelsStateKey(props.key))
+            const activeModelUri = localStorage.getItem(activemodelStateKey(props.key))
+
+            if (allModelQueries) {
+                // clear existing models
+                props.monaco?.editor.getModels().forEach((model: editor.ITextModel) => {
+                    model.dispose()
+                })
+
+                const models = JSON.parse(allModelQueries || '[]')
+                const newModels: Uri[] = []
+
+                models.forEach((model: Record<string, any>) => {
+                    if (props.monaco) {
+                        const uri = props.monaco.Uri.parse(model.path)
+                        const newModel = props.monaco.editor.createModel(model.query, 'hogQL', uri)
+                        props.editor?.setModel(newModel)
+                        newModels.push(uri)
+                    }
+                })
+
+                actions.setTabs(newModels)
+
+                if (activeModelUri) {
+                    const uri = props.monaco.Uri.parse(activeModelUri)
+                    const activeModel = props.monaco.editor
+                        .getModels()
+                        .find((model: editor.ITextModel) => model.uri.path === uri.path)
+                    activeModel && props.editor?.setModel(activeModel)
+                    const val = activeModel?.getValue()
+                    if (val) {
+                        actions.setQueryInput(val)
+                        actions.runQuery()
+                    }
+                    actions.selectTab(uri)
+                } else if (newModels.length) {
+                    actions.selectTab(newModels[0])
+                }
+            } else {
+                const model = props.editor?.getModel()
+
+                if (model) {
+                    actions.createTab()
+                }
+            }
+        },
+        runQuery: ({ queryOverride }) => {
+            actions.setActiveQuery(queryOverride || values.queryInput)
+        },
+    })),
+    subscriptions(({ props, actions, values }) => ({
+        activeModelUri: (activeModelUri) => {
+            if (props.monaco) {
+                const _model = props.monaco.editor.getModel(activeModelUri)
+                const val = _model?.getValue()
+                actions.setQueryInput(val ?? '')
+                actions.runQuery()
+                dataNodeLogic({
+                    key: values.activeTabKey,
+                    query: {
+                        kind: NodeKind.HogQLQuery,
+                        query: val ?? '',
+                    },
+                    doNotLoad: !val,
+                }).mount()
+            }
+        },
+    })),
+    selectors({
+        activeTabKey: [(s) => [s.activeModelUri], (activeModelUri) => `hogQLQueryEditor/${activeModelUri?.path}`],
+    }),
+])
