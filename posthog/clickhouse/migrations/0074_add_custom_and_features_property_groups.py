e60c7d73e55f58f5e09c64add7cca141375db823
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2024-11-12 13:42:33 -0800

    flatten existing migrations

--- posthog/clickhouse/migrations/0074_add_custom_and_features_property_groups.py
+++ posthog/clickhouse/migrations/0074_add_custom_and_features_property_groups.py
@@ -1,10 +1,15 @@
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.clickhouse.property_groups import property_groups
 
 operations = [
     run_sql_with_exceptions(statement)
     for statement in [
-        *property_groups.get_alter_create_statements("sharded_events", "properties", "custom"),
-        *property_groups.get_alter_create_statements("sharded_events", "properties", "feature_flags"),
+        # property_groups.get_alter_create_statements("sharded_events", "properties", "custom")
+        "ALTER TABLE sharded_events ON CLUSTER posthog ADD COLUMN IF NOT EXISTS properties_group_custom Map(String, String) MATERIALIZED mapSort(mapFilter((key, _) -> key NOT LIKE '$%' AND key NOT IN ('token', 'distinct_id', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term', 'gclid', 'gad_source', 'gclsrc', 'dclid', 'gbraid', 'wbraid', 'fbclid', 'msclkid', 'twclid', 'li_fat_id', 'mc_cid', 'igshid', 'ttclid', 'rdt_cid'), CAST(JSONExtractKeysAndValues(properties, 'String'), 'Map(String, String)')) CODEC(ZSTD(1))",
+        "ALTER TABLE sharded_events ON CLUSTER posthog ADD INDEX IF NOT EXISTS properties_group_custom_keys_bf mapKeys(properties_group_custom) TYPE bloom_filter",
+        "ALTER TABLE sharded_events ON CLUSTER posthog ADD INDEX IF NOT EXISTS properties_group_custom_values_bf mapValues(properties_group_custom) TYPE bloom_filter",
+        # property_groups.get_alter_create_statements("sharded_events", "properties", "feature_flags")
+        "ALTER TABLE sharded_events ON CLUSTER posthog ADD COLUMN IF NOT EXISTS properties_group_feature_flags Map(String, String) MATERIALIZED mapSort(mapFilter((key, _) -> key like '$feature/%', CAST(JSONExtractKeysAndValues(properties, 'String'), 'Map(String, String)')) CODEC(ZSTD(1))",
+        "ALTER TABLE sharded_events ON CLUSTER posthog ADD INDEX IF NOT EXISTS properties_group_feature_flags_keys_bf mapKeys(properties_group_feature_flags) TYPE bloom_filter",
+        "ALTER TABLE sharded_events ON CLUSTER posthog ADD INDEX IF NOT EXISTS properties_group_feature_flags_values_bf mapValues(properties_group_feature_flags) TYPE bloom_filter",
     ]
 ]
--- posthog/clickhouse/migrations/0076_add_custom_and_features_property_groups_events_retry2.py
+++ posthog/clickhouse/migrations/0076_add_custom_and_features_property_groups_events_retry2.py
@@ -1,10 +1,11 @@
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.clickhouse.property_groups import property_groups
 
 operations = [
     run_sql_with_exceptions(statement)
     for statement in [
-        *property_groups.get_alter_create_statements("events", "properties", "custom"),
-        *property_groups.get_alter_create_statements("events", "properties", "feature_flags"),
+        # property_groups.get_alter_create_statements("events", "properties", "custom")
+        "ALTER TABLE events ON CLUSTER posthog ADD COLUMN IF NOT EXISTS properties_group_custom Map(String, String)",
+        # property_groups.get_alter_create_statements("events", "properties", "feature_flags")
+        "ALTER TABLE events ON CLUSTER posthog ADD COLUMN IF NOT EXISTS properties_group_feature_flags Map(String, String)",
     ]
 ]
