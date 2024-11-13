ff4b584899ff97cd421d9efa5125a3d741fa6941
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-11-13 16:59:56 +0100

    fix: Add threads back (#26170)

--- posthog/temporal/batch_exports/batch_exports.py
+++ posthog/temporal/batch_exports/batch_exports.py
@@ -65,7 +65,7 @@ SETTINGS
     max_bytes_before_external_group_by=50000000000,
     max_bytes_before_external_sort=50000000000,
     optimize_aggregation_in_order=1,
-    max_threads=16,
+    max_threads=32,
     join_algorithm='partial_merge'
 """
 
@@ -88,7 +88,7 @@ SETTINGS
     max_bytes_before_external_group_by=50000000000,
     max_bytes_before_external_sort=50000000000,
     optimize_aggregation_in_order=1,
-    max_threads=16,
+    max_threads=32,
     join_algorithm='partial_merge'
 """
 
@@ -968,8 +968,8 @@ async def execute_batch_export_insert_activity(
     elif interval.startswith("every"):
         _, value, unit = interval.split(" ")
         kwargs = {unit: int(value)}
-        # TODO: Consider removing this 10 minute minimum once we are more confident about hitting 5 minute or lower SLAs.
-        start_to_close_timeout = max(dt.timedelta(minutes=10), dt.timedelta(**kwargs))
+        # TODO: Consider removing this 20 minute minimum once we are more confident about hitting 5 minute or lower SLAs.
+        start_to_close_timeout = max(dt.timedelta(minutes=20), dt.timedelta(**kwargs))
     else:
         raise ValueError(f"Unsupported interval: '{interval}'")
 
