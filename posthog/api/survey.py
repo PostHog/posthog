b680627f5b9b6d2a462822102d6653fc93c74cf0
Phani Raj <phani@posthog.com>
2024-11-14 10:34:43 -0600

    Calculate daily sampling and rollout percentage for Adaptive Survey Collection

--- posthog/api/survey.py
+++ posthog/api/survey.py
@@ -1,6 +1,6 @@
 import os
 from contextlib import contextmanager
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from typing import Any, cast
 from urllib.parse import urlparse
 
@@ -80,11 +80,17 @@ class SurveySerializer(serializers.ModelSerializer):
             "end_date",
             "archived",
             "responses_limit",
+            "feature_flag_keys",
             "iteration_count",
             "iteration_frequency_days",
             "iteration_start_dates",
             "current_iteration",
             "current_iteration_start_date",
+            "response_sampling_start_date",
+            "response_sampling_interval_type",
+            "response_sampling_interval",
+            "response_sampling_limit",
+            "response_sampling_daily_limits",
         ]
         read_only_fields = ["id", "created_at", "created_by"]
 
@@ -136,6 +142,11 @@ class SurveySerializerCreateUpdateOnly(serializers.ModelSerializer):
             "iteration_start_dates",
             "current_iteration",
             "current_iteration_start_date",
+            "response_sampling_start_date",
+            "response_sampling_interval_type",
+            "response_sampling_interval",
+            "response_sampling_limit",
+            "response_sampling_daily_limits",
         ]
         read_only_fields = ["id", "linked_flag", "targeting_flag", "created_at"]
 
@@ -292,6 +303,36 @@ class SurveySerializerCreateUpdateOnly(serializers.ModelSerializer):
                     "Invalid operation: User targeting rolls out to everyone. If you want to roll out to everyone, delete this targeting",
                     code="invalid",
                 )
+
+        response_sampling_start_date = data.get("response_sampling_start_date")
+        if response_sampling_start_date is not None:
+            today_utc = datetime.now(UTC).replace(hour=0, minute=0, second=0, microsecond=0)
+            if response_sampling_start_date < today_utc:
+                raise serializers.ValidationError(
+                    {
+                        "response_sampling_start_date": "Response sampling start date must be today or a future date in UTC."
+                    }
+                )
+
+        response_sampling_interval = data.get("response_sampling_interval")
+        if response_sampling_interval is not None and response_sampling_interval <= 0:
+            raise serializers.ValidationError(
+                {"response_sampling_interval": "Response sampling interval must be greater than 0."}
+            )
+
+        response_sampling_limit = data.get("response_sampling_limit", 0)
+        if (
+            response_sampling_limit is not None
+            and response_sampling_limit > 0
+            and response_sampling_interval > 0
+            and response_sampling_start_date is None
+        ):
+            raise serializers.ValidationError(
+                {
+                    "response_sampling_start_date": "Response sampling start date should be set if response_sampling_start_date is not zero."
+                }
+            )
+
         return data
 
     def create(self, validated_data):
