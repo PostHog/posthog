39ddefd25e5c0fea792a71470a5be42c2a37ab7e
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-11-15 12:14:13 +0100

    add gear wheel to series column

--- frontend/src/scenes/insights/views/InsightsTable/columns/SeriesColumn.tsx
+++ frontend/src/scenes/insights/views/InsightsTable/columns/SeriesColumn.tsx
@@ -1,11 +1,25 @@
-import { IconPencil } from '@posthog/icons'
+import { IconGear, IconPencil } from '@posthog/icons'
+import { Popover } from '@posthog/lemon-ui'
 import clsx from 'clsx'
+import { useValues } from 'kea'
 import { getTrendLikeSeriesColor } from 'lib/colors'
 import { InsightLabel } from 'lib/components/InsightLabel'
+import { SeriesGlyph } from 'lib/components/SeriesGlyph'
 import { LemonButton } from 'lib/lemon-ui/LemonButton'
-import { capitalizeFirstLetter } from 'lib/utils'
+import { LemonField } from 'lib/lemon-ui/LemonField'
+import { capitalizeFirstLetter, hexToRGBA, lightenDarkenColor, RGBToRGBA } from 'lib/utils'
+import { useState } from 'react'
+import { dataThemeLogic } from 'scenes/dataThemeLogic'
+import { getTrendLegendColorToken, getTrendLegendEntryKey } from 'scenes/insights/utils'
 import { IndexedTrendResult } from 'scenes/trends/types'
 
+import { themeLogic } from '~/layout/navigation-3000/themeLogic'
+import {
+    ColorAssignmentBy,
+    LegendEntryConfig,
+    LegendEntryConfigByKey,
+    LegendEntryConfigByPosition,
+} from '~/queries/schema'
 import { TrendResult } from '~/types'
 
 type SeriesColumnItemProps = {
@@ -13,7 +27,15 @@ type SeriesColumnItemProps = {
     indexedResults: IndexedTrendResult[]
     canEditSeriesNameInline: boolean
     handleEditClick: (item: IndexedTrendResult) => void
-    hasMultipleSeries?: boolean
+    hasMultipleSeries: boolean
+    hasBreakdown: boolean
+    colorAssignmentBy: ColorAssignmentBy | null | undefined
+    legendEntries:
+        | Record<string, LegendEntryConfigByKey>
+        | Record<number, LegendEntryConfigByPosition>
+        | null
+        | undefined
+    updateLegendEntry: (key: number | string, config: LegendEntryConfig) => void
 }
 
 export function SeriesColumnItem({
@@ -22,10 +44,22 @@ export function SeriesColumnItem({
     canEditSeriesNameInline,
     handleEditClick,
     hasMultipleSeries,
+    hasBreakdown,
+    colorAssignmentBy,
+    legendEntries,
+    updateLegendEntry,
 }: SeriesColumnItemProps): JSX.Element {
+    const [isSettingsOpen, setSettingsOpen] = useState(false)
+    const { getTheme } = useValues(dataThemeLogic)
+    const { isDarkModeOn } = useValues(themeLogic)
+
     const showCountedByTag = !!indexedResults.find(({ action }) => action?.math && action.math !== 'total')
 
     const isPrevious = !!item.compare && item.compare_label === 'previous'
+
+    const theme = getTheme('posthog')
+    const colorToken = getTrendLegendColorToken(colorAssignmentBy, legendEntries, theme, item)
+    const legendEntryKey = getTrendLegendEntryKey(colorAssignmentBy, item)
 
     return (
         <div className="series-name-wrapper-col space-x-1">
@@ -48,10 +82,66 @@ export function SeriesColumnItem({
             />
             {canEditSeriesNameInline && (
                 <LemonButton
+                    noPadding
+                    size="small"
                     onClick={() => handleEditClick(item)}
                     title="Rename graph series"
                     icon={<IconPencil className="edit-icon" />}
                 />
+            )}
+            {!hasBreakdown && (
+                <Popover
+                    overlay={
+                        <div className="m-2 min-w-50">
+                            <div className="flex gap-3">
+                                <LemonField.Pure label="Color">
+                                    <div className="flex">
+                                        {Object.entries(theme).map(([key, color]) => (
+                                            <LemonButton
+                                                key={key}
+                                                type={key === colorToken ? 'secondary' : 'tertiary'}
+                                                onClick={(e) => {
+                                                    e.preventDefault()
+                                                    e.stopPropagation()
+                                                    updateLegendEntry(legendEntryKey, { color: key })
+                                                }}
+                                            >
+                                                <SeriesGlyph
+                                                    style={{
+                                                        borderColor: color,
+                                                        color: color,
+                                                        backgroundColor: isDarkModeOn
+                                                            ? RGBToRGBA(lightenDarkenColor(color, -20), 0.3)
+                                                            : hexToRGBA(color, 0.5),
+                                                    }}
+                                                />
+                                            </LemonButton>
+                                        ))}
+                                    </div>
+                                </LemonField.Pure>
+
+                                <LemonButton>Reset</LemonButton>
+                            </div>
+                        </div>
+                    }
+                    visible={isSettingsOpen}
+                    placement="right"
+                    onClickOutside={() => {
+                        setSettingsOpen(false)
+                    }}
+                >
+                    <LemonButton
+                        icon={<IconGear />}
+                        noPadding
+                        className="ml-1"
+                        size="small"
+                        onClick={(e) => {
+                            e.preventDefault()
+                            e.stopPropagation()
+                            setSettingsOpen(true)
+                        }}
+                    />
+                </Popover>
             )}
         </div>
     )
