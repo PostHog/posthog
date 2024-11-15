dfacc56f74c1db1cc28adddb543d12bc53050c26
Alexander Spicer <aspicer@gmail.com>
2024-11-15 15:47:22 -0800

    clean up

--- frontend/src/exporter/index.tsx
+++ frontend/src/exporter/index.tsx
@@ -21,7 +21,7 @@ initKea()
 
 const exportedData: ExportedData = window.POSTHOG_EXPORTED_DATA
 
-export function renderApp(): void {
+function renderApp(): void {
     const root = document.getElementById('root')
     if (root) {
         createRoot(root).render(
