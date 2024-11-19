13052d8ec55f8c429d87323eace85f03cecb46cf
Michael Matloka <michael@posthog.com>
2024-11-19 17:00:35 +0100

    Ensure `kafka_...` CH tables are created in tests

--- posthog/conftest.py
+++ posthog/conftest.py
@@ -17,6 +17,7 @@ def create_clickhouse_tables(num_tables: int):
         CREATE_DICTIONARY_QUERIES,
         CREATE_DISTRIBUTED_TABLE_QUERIES,
         CREATE_MERGETREE_TABLE_QUERIES,
+        CREATE_KAFKA_TABLE_QUERIES,
         CREATE_MV_TABLE_QUERIES,
         CREATE_VIEW_QUERIES,
         build_query,
@@ -31,6 +32,9 @@ def create_clickhouse_tables(num_tables: int):
 
     table_queries = list(map(build_query, CREATE_TABLE_QUERIES))
     run_clickhouse_statement_in_parallel(table_queries)
+
+    kafka_queries = list(map(build_query, CREATE_KAFKA_TABLE_QUERIES))
+    run_clickhouse_statement_in_parallel(kafka_queries)
 
     mv_queries = list(map(build_query, CREATE_MV_TABLE_QUERIES))
     run_clickhouse_statement_in_parallel(mv_queries)
