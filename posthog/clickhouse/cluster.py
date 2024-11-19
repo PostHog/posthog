01bf14d27e13f0b9d9e5d5c884ba56b9c5ba0660
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2024-11-18 19:01:55 -0800

    add utility for executing tasks across a clickhouse cluster

---
+++ posthog/clickhouse/cluster.py
@@ -0,0 +1,71 @@
+from collections.abc import Callable, Mapping
+from concurrent.futures import Future, ThreadPoolExecutor
+from typing import NamedTuple, TypeVar
+
+from clickhouse_driver import Client
+from clickhouse_pool import ChPool
+from django.conf import settings
+
+from posthog.clickhouse.client.connection import make_ch_pool
+
+
+class HostInfo(NamedTuple):
+    shard_num: int
+    replica_num: int
+    host_address: str
+    port: int
+
+
+T = TypeVar("T")
+
+
+class ClickhouseCluster:
+    def __init__(self, bootstrap_client: Client, name: str = settings.CLICKHOUSE_CLUSTER) -> None:
+        self.name = name
+        self.hosts = [
+            HostInfo(shard_num, replica_num, host_address, port)
+            for (shard_num, replica_num, host_address, port) in bootstrap_client.execute(
+                """
+                SELECT shard_num, replica_num, host_address, port
+                FROM system.clusters
+                WHERE name = %(name)s
+                ORDER BY shard_num, replica_num
+                """,
+                {"name": name},
+            )
+        ]
+
+        self.__pools: dict[HostInfo, ChPool] = {}
+
+    def map_hosts(self, fn: Callable[[Client], T]) -> Mapping[HostInfo, Future[T]]:
+        def task(pool):
+            with pool.get_client() as client:
+                return fn(client)
+
+        with ThreadPoolExecutor() as executor:
+            results = {}
+            for host in self.hosts:
+                pool = self.__pools.get(host)
+                if pool is None:
+                    pool = self.__pools[pool] = make_ch_pool(host=host.host_address, port=host.port)
+                results[host] = executor.submit(task, pool)
+            return results
+
+    def map_shards(self, fn: Callable[[Client], T]) -> Mapping[int, Future[T]]:
+        def task(pool):
+            with pool.get_client() as client:
+                return fn(client)
+
+        shard_hosts: dict[int, HostInfo] = {}
+        for host in self.hosts:
+            if host.shard_num not in shard_hosts:
+                shard_hosts[host.shard_num] = host
+
+        with ThreadPoolExecutor() as executor:
+            results = {}
+            for shard_num, host in shard_hosts.items():
+                pool = self.__pools.get(host)
+                if pool is None:
+                    pool = self.__pools[pool] = make_ch_pool(host=host.host_address, port=host.port)
+                results[shard_num] = executor.submit(task, pool)
+            return results
