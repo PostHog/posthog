a699ede425ff311577d012ef457aa44d5175ec62
Ben White <ben@posthog.com>
2024-11-19 17:45:41 -0800

    fix

--- frontend/src/layout/navigation-3000/sidepanel/panels/activity/activityForSceneLogic.ts
+++ frontend/src/layout/navigation-3000/sidepanel/panels/activity/activityForSceneLogic.ts
@@ -68,7 +68,7 @@ export const activityForSceneLogic = kea<activityForSceneLogicType>([
                 await api.create('/api/projects/@current/internal_metrics/', {
                     metric_name: 'viewed',
                     metric_kind: 'misc',
-                    instance_id: obj.item_id ? `${obj.scope}:${obj.item_id}` : scope,
+                    instance_id: obj.item_id ? `${obj.scope}:${obj.item_id}` : obj.scope,
 
                     // API sets these
                     // app_source: 'internal_metrics',
