6354e8718f36091af98b48bbb0538ae9796e6afd
Oliver Browne <oliver@posthog.com>
2024-11-21 12:54:41 +0200

    fix(err): include raw js frame on frame content in pg (#26326)

--- rust/cymbal/src/langs/js.rs
+++ rust/cymbal/src/langs/js.rs
@@ -15,15 +15,21 @@ use crate::{
 // TODO - how do we know if this frame is minified? If it isn't, we can skip a lot of work, but I think we have to guess? Based on whether we can get a sourcemap for it?
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct RawJSFrame {
-    #[serde(rename = "lineno")]
-    pub line: u32,
-    #[serde(rename = "colno")]
-    pub column: u32,
+    #[serde(flatten)]
+    pub location: Option<FrameLocation>, // Sometimes we get frames with no location information. We treat these as already resolved, or unminified
     #[serde(rename = "filename")]
     pub source_url: Option<String>, // The url the the script the frame was in was fetched from
     pub in_app: bool,
     #[serde(rename = "function")]
     pub fn_name: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, Eq, PartialEq)]
+pub struct FrameLocation {
+    #[serde(rename = "lineno")]
+    pub line: u32,
+    #[serde(rename = "colno")]
+    pub column: u32,
 }
 
 impl RawJSFrame {
@@ -46,11 +52,18 @@ impl RawJSFrame {
     {
         let url = self.source_url()?;
 
+        let Some(location) = &self.location else {
+            return Ok(Frame::from(self)); // We're probably an unminified frame
+        };
+
         let sourcemap = catalog.lookup(team_id, url).await?;
-        let Some(token) = sourcemap.lookup_token(self.line, self.column) else {
-            return Err(
-                JsResolveErr::TokenNotFound(self.fn_name.clone(), self.line, self.column).into(),
-            );
+        let Some(token) = sourcemap.lookup_token(location.line, location.column) else {
+            return Err(JsResolveErr::TokenNotFound(
+                self.fn_name.clone(),
+                location.line,
+                location.column,
+            )
+            .into());
         };
 
         Ok(Frame::from((self, token)))
@@ -60,7 +73,10 @@ impl RawJSFrame {
     pub fn handle_resolution_error(&self, e: JsResolveErr) -> Frame {
         // If we failed to resolve the frame, we mark it as "not resolved" and add the error message,
         // then return a Frame anyway, because frame handling is a best-effort thing.
-        (self, e).into()
+        let Some(location) = &self.location else {
+            return self.into();
+        };
+        (self, e, location).into()
     }
 
     pub fn source_url(&self) -> Result<Url, JsResolveErr> {
@@ -106,8 +122,10 @@ impl RawJSFrame {
     pub fn frame_id(&self) -> String {
         let mut hasher = Sha512::new();
         hasher.update(self.fn_name.as_bytes());
-        hasher.update(self.line.to_string().as_bytes());
-        hasher.update(self.column.to_string().as_bytes());
+        if let Some(location) = &self.location {
+            hasher.update(location.line.to_string().as_bytes());
+            hasher.update(location.column.to_string().as_bytes());
+        }
         hasher.update(
             self.source_url
                 .as_ref()
@@ -123,7 +141,7 @@ impl From<(&RawJSFrame, Token<'_>)> for Frame {
         let (raw_frame, token) = src;
         metrics::counter!(FRAME_RESOLVED, "lang" => "javascript").increment(1);
 
-        Self {
+        let mut res = Self {
             raw_id: String::new(), // We use placeholders here, as they're overriden at the RawFrame level
             mangled_name: raw_frame.fn_name.clone(),
             line: Some(token.get_src_line()),
@@ -134,28 +152,88 @@ impl From<(&RawJSFrame, Token<'_>)> for Frame {
             lang: "javascript".to_string(),
             resolved: true,
             resolve_failure: None,
+            junk_drawer: None,
             context: get_context(&token),
-        }
+        };
+
+        add_raw_to_junk(&mut res, raw_frame);
+
+        res
     }
 }
 
-impl From<(&RawJSFrame, JsResolveErr)> for Frame {
-    fn from((raw_frame, err): (&RawJSFrame, JsResolveErr)) -> Self {
+// If we failed to resolve the frame, but it's a frame that has location information,
+// mark it as a failed resolve and emit an "unresolved" frame
+impl From<(&RawJSFrame, JsResolveErr, &FrameLocation)> for Frame {
+    fn from((raw_frame, err, location): (&RawJSFrame, JsResolveErr, &FrameLocation)) -> Self {
         metrics::counter!(FRAME_NOT_RESOLVED, "lang" => "javascript").increment(1);
-        Self {
+
+        // TODO - extremely rough
+        let was_minified = match err {
+            JsResolveErr::NoSourceUrl => false, // This frame's `source` was None
+            JsResolveErr::NoSourcemap(_) => false, // A total guess
+            _ => true,
+        };
+
+        let resolved_name = if was_minified {
+            None
+        } else {
+            Some(raw_frame.fn_name.clone())
+        };
+
+        let mut res = Self {
             raw_id: String::new(),
             mangled_name: raw_frame.fn_name.clone(),
-            line: Some(raw_frame.line),
-            column: Some(raw_frame.column),
+            line: Some(location.line),
+            column: Some(location.column),
             source: raw_frame.source_url().map(|u| u.path().to_string()).ok(),
             in_app: raw_frame.in_app,
-            resolved_name: None,
+            resolved_name,
             lang: "javascript".to_string(),
-            resolved: false,
+            resolved: !was_minified,
+            // Regardless of whather we think this was a minified frame or not, we still put
+            // the error message in resolve_failure, so if a user comes along and want to know
+            // why we thought a frame wasn't minified, they can see the error message
             resolve_failure: Some(err.to_string()),
+            junk_drawer: None,
             context: None,
-        }
+        };
+
+        add_raw_to_junk(&mut res, raw_frame);
+
+        res
     }
+}
+
+// Finally, if we have a frame that has NO location information, we treat it as not minified, since it's
+// probably a native function or something else weird
+impl From<&RawJSFrame> for Frame {
+    fn from(raw_frame: &RawJSFrame) -> Self {
+        metrics::counter!(FRAME_NOT_RESOLVED, "lang" => "javascript").increment(1);
+        let mut res = Self {
+            raw_id: String::new(),
+            mangled_name: raw_frame.fn_name.clone(),
+            line: None,
+            column: None,
+            source: raw_frame.source_url().map(|u| u.path().to_string()).ok(),
+            in_app: raw_frame.in_app,
+            resolved_name: Some(raw_frame.fn_name.clone()),
+            lang: "javascript".to_string(),
+            resolved: true, // Without location information, we're assuming this is not minified
+            resolve_failure: None,
+            junk_drawer: None,
+            context: None,
+        };
+
+        add_raw_to_junk(&mut res, raw_frame);
+
+        res
+    }
+}
+
+fn add_raw_to_junk(frame: &mut Frame, raw: &RawJSFrame) {
+    // UNWRAP: raw JS frames are definitely representable as json
+    frame.add_junk("raw_frame", raw.clone()).unwrap();
 }
 
 fn get_context(token: &Token) -> Option<Context> {
@@ -196,8 +274,7 @@ mod test {
     #[test]
     fn source_ref_generation() {
         let frame = super::RawJSFrame {
-            line: 1,
-            column: 2,
+            location: None,
             source_url: Some("http://example.com/path/to/file.js:1:2".to_string()),
             in_app: true,
             fn_name: "main".to_string(),
@@ -209,8 +286,7 @@ mod test {
         );
 
         let frame = super::RawJSFrame {
-            line: 1,
-            column: 2,
+            location: None,
             source_url: Some("http://example.com/path/to/file.js".to_string()),
             in_app: true,
             fn_name: "main".to_string(),
