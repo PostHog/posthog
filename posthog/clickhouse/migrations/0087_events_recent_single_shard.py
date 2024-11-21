7ff402e558239141c2f0a91e356f4ba9b5c65329
Daniel Escribano <daniel.e@posthog.com>
2024-11-21 18:22:48 +0100

    fix: remove unused imports

--- posthog/clickhouse/migrations/0087_events_recent_single_shard.py
+++ posthog/clickhouse/migrations/0087_events_recent_single_shard.py
@@ -1,8 +1,5 @@
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.models.event.sql import (
-    EVENTS_RECENT_TABLE_JSON_MV_SQL,
-    EVENTS_RECENT_TABLE_SQL,
-    KAFKA_EVENTS_RECENT_TABLE_JSON_SQL,
     DISTRIBUTED_EVENTS_RECENT_TABLE_SQL,
 )
 
