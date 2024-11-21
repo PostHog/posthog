d1ee4611e07a48b4503867c43975e28b478bf9ab
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-11-21 19:04:36 +0100

    initial form working

--- frontend/src/scenes/settings/environment/DataColorThemeModal.tsx
+++ frontend/src/scenes/settings/environment/DataColorThemeModal.tsx
@@ -1,6 +1,6 @@
-import { LemonButton, LemonInput, LemonModal, LemonTable, LemonTextArea } from '@posthog/lemon-ui'
+import { LemonButton, LemonInput, LemonLabel, LemonModal, LemonTable, LemonTextArea } from '@posthog/lemon-ui'
 import { useValues } from 'kea'
-import { Field, Form } from 'kea-forms'
+import { Field, Form, Group } from 'kea-forms'
 import { SeriesGlyph } from 'lib/components/SeriesGlyph'
 import { LemonField } from 'lib/lemon-ui/LemonField'
 import { hexToRGBA, lightenDarkenColor, RGBToRGBA } from 'lib/utils'
@@ -25,60 +25,63 @@ export function DataColorThemeModal(): JSX.Element {
         >
             <Form logic={dataColorThemesModalLogic} formKey="theme" className="flex flex-col gap-2">
                 <LemonField name="name" label="Name">
-                    {({ value, onChange }) => <LemonInput value={value} onChange={onChange} />}
+                    <LemonInput />
                 </LemonField>
 
                 <LemonField name="description" label="Description">
-                    {({ value, onChange }) => <LemonTextArea value={value} onChange={onChange} />}
+                    <LemonTextArea />
                 </LemonField>
 
-                <LemonField name="colors" label="Colors">
-                    <LemonTable
-                        // loading={themesLoading}
-                        dataSource={theme?.colors.map((color, index) => ({
-                            name: `preset-${index + 1}`,
-                            color,
-                        }))}
-                        columns={[
-                            {
-                                title: '',
-                                dataIndex: 'color',
-                                key: 'glyph',
-                                render: (_, { color }) => {
-                                    return (
-                                        <SeriesGlyph
-                                            style={{
-                                                borderColor: color,
-                                                color: color,
-                                                backgroundColor: isDarkModeOn
-                                                    ? RGBToRGBA(lightenDarkenColor(color, -20), 0.3)
-                                                    : hexToRGBA(color, 0.2),
-                                            }}
-                                        />
-                                    )
-                                },
-                                width: 24,
+                <LemonLabel>Colors</LemonLabel>
+                <LemonTable
+                    dataSource={theme?.colors?.map((color, index) => ({
+                        name: `preset-${index + 1}`,
+                        color,
+                        index,
+                    }))}
+                    columns={[
+                        {
+                            title: '',
+                            dataIndex: 'color',
+                            key: 'glyph',
+                            render: (_, { color }) => {
+                                return (
+                                    <SeriesGlyph
+                                        style={{
+                                            borderColor: color,
+                                            color: color,
+                                            backgroundColor: isDarkModeOn
+                                                ? RGBToRGBA(lightenDarkenColor(color, -20), 0.3)
+                                                : hexToRGBA(color, 0.2),
+                                        }}
+                                    />
+                                )
                             },
-                            {
-                                title: 'Name',
-                                dataIndex: 'name',
-                                key: 'name',
+                            width: 24,
+                        },
+                        {
+                            title: 'Name',
+                            dataIndex: 'name',
+                            key: 'name',
+                        },
+                        {
+                            title: 'Color',
+                            dataIndex: 'color',
+                            render: (_, { index }) => {
+                                return (
+                                    <LemonField key={index} name={['colors', index]}>
+                                        <LemonInput className="max-w-20 font-mono" />
+                                    </LemonField>
+                                )
                             },
-                            {
-                                title: 'Color',
-                                dataIndex: 'color',
-                                render: (_, { color }) => {
-                                    return <LemonInput value={color} className="max-w-20 font-mono" />
-                                },
-                            },
-                            {
-                                title: '',
-                                key: 'actions',
-                                width: 24,
-                            },
-                        ]}
-                    />
-                </LemonField>
+                        },
+                        {
+                            title: '',
+                            key: 'actions',
+                            width: 24,
+                        },
+                    ]}
+                />
                 <LemonButton type="secondary" className="self-start">
                     Add color
                 </LemonButton>
