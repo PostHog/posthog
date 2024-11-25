bc00cc0a331cfe310506645482bcff714089d3d6
Georgiy Tarasov <gtarasov.work@gmail.com>
2024-11-25 12:56:46 +0100

    test: funnel replaces personal names

--- ee/hogai/eval/test_eval_funnel_generator.py
+++ ee/hogai/eval/test_eval_funnel_generator.py
@@ -1,4 +1,5 @@
 from langgraph.graph.state import CompiledStateGraph
+from pydantic import BaseModel
 
 from ee.hogai.assistant import AssistantGraph
 from ee.hogai.eval.utils import EvalBaseTest
@@ -7,12 +8,27 @@ from posthog.schema import HumanMessage
 
 
 class TestEvalFunnelGenerator(EvalBaseTest):
-    def _call_node(self, query: str, plan: str):
+    def _call_node(self, query: str, plan: str) -> BaseModel:
         graph: CompiledStateGraph = (
             AssistantGraph(self.team)
             .add_edge(AssistantNodeName.START, AssistantNodeName.FUNNEL_GENERATOR)
             .add_funnel_generator(AssistantNodeName.END)
             .compile()
         )
-        state = graph.invoke({"messages": [HumanMessage(content=query)]})
-        return state["plan"]
+        state = graph.invoke({"messages": [HumanMessage(content=query)], "plan": plan})
+        return state["messages"][-1].answer
+
+    def test_node_replaces_equals_with_contains(self):
+        query = "what is the conversion rate from a page view to sign up for users with name john?"
+        plan = """Sequence:
+        1. $pageview
+        - property filter 1
+            - person
+            - name
+            - equals
+            - john
+        2. signed_up
+        """
+        actual_output = self._call_node(query, plan).model_dump_json(exclude_none=True)
+        assert "equals" not in actual_output
+        assert "contains" in actual_output
