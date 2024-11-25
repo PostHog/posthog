65984ccc22a472d737ef10ec7b3ebff43d022e99
Paul D'Ambra <paul@posthog.com>
2024-11-25 20:28:58 +0000

    feat: player 4000 take 1

--- frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylistSettings.tsx
+++ frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylistSettings.tsx
@@ -1,11 +1,11 @@
-import { LemonSegmentedButton, LemonSwitch } from '@posthog/lemon-ui'
+import { LemonSegmentedButton, LemonSelect, LemonSwitch } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
 
 import { PlaybackMode, playerSettingsLogic } from '../player/playerSettingsLogic'
 
 export function SessionRecordingsPlaylistSettings(): JSX.Element {
-    const { hideViewedRecordings, playbackMode } = useValues(playerSettingsLogic)
-    const { setHideViewedRecordings, setPlaybackMode } = useActions(playerSettingsLogic)
+    const { hideViewedRecordings, playbackMode, autoplayDirection } = useValues(playerSettingsLogic)
+    const { setHideViewedRecordings, setPlaybackMode, setAutoplayDirection } = useActions(playerSettingsLogic)
 
     return (
         <div className="relative flex flex-col gap-2 p-3 border-b">
@@ -33,6 +33,29 @@ export function SessionRecordingsPlaylistSettings(): JSX.Element {
                     aria-label="Autoplay next recording"
                     checked={hideViewedRecordings}
                     onChange={() => setHideViewedRecordings(!hideViewedRecordings)}
+                />
+            </div>
+            <div className="flex flex-row items-center justify-between space-x-2">
+                <span className="text-black font-medium">Autoplay</span>
+
+                <LemonSelect
+                    tooltip={
+                        <div className="text-center">
+                            Autoplay next recording
+                            <br />({!autoplayDirection ? 'off' : autoplayDirection})
+                        </div>
+                    }
+                    value={autoplayDirection}
+                    aria-label="Autoplay next recording"
+                    onChange={setAutoplayDirection}
+                    dropdownPlacement="bottom-end"
+                    dropdownMatchSelectWidth={false}
+                    options={[
+                        { value: null, label: 'Off' },
+                        { value: 'newer', label: 'Newer recordings' },
+                        { value: 'older', label: 'Older recordings' },
+                    ]}
+                    size="small"
                 />
             </div>
         </div>
