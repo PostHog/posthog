c46e3a366e410e1547f4cbc8e3152d148e6879eb
Manoel Aranda Neto <marandaneto@gmail.com>
2024-11-25 15:55:53 +0100

    fix test

--- posthog/tasks/test/test_usage_report.py
+++ posthog/tasks/test/test_usage_report.py
@@ -500,8 +500,8 @@ class UsageReport(APIBaseTest, ClickhouseTestMixin, ClickhouseDestroyTablesMixin
                     },
                     "plugins_enabled": {"Installed and enabled": 1},
                     "instance_tag": "none",
-                    "event_count_in_period": 40,
-                    "enhanced_persons_event_count_in_period": 39,
+                    "event_count_in_period": 41,
+                    "enhanced_persons_event_count_in_period": 40,
                     "event_count_with_groups_in_period": 2,
                     "event_count_from_keywords_ai_in_period": 1,
                     "event_count_from_traceloop_in_period": 1,
@@ -555,8 +555,8 @@ class UsageReport(APIBaseTest, ClickhouseTestMixin, ClickhouseDestroyTablesMixin
                     "team_count": 2,
                     "teams": {
                         str(self.org_1_team_1.id): {
-                            "event_count_in_period": 29,
-                            "enhanced_persons_event_count_in_period": 28,
+                            "event_count_in_period": 30,
+                            "enhanced_persons_event_count_in_period": 29,
                             "event_count_with_groups_in_period": 2,
                             "event_count_from_keywords_ai_in_period": 1,
                             "event_count_from_traceloop_in_period": 1,
