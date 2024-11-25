4f738e5873c32112fe2fa26db79f50857d4ae54c
Paul D'Ambra <paul@posthog.com>
2024-11-25 13:01:59 +0000

    feat: image preview in inspector list

--- frontend/src/scenes/session-recordings/player/inspector/components/ItemEvent.tsx
+++ frontend/src/scenes/session-recordings/player/inspector/components/ItemEvent.tsx
@@ -1,4 +1,4 @@
-import { LemonButton, LemonDivider } from '@posthog/lemon-ui'
+import { LemonButton, LemonDivider, Tooltip } from '@posthog/lemon-ui'
 import { useValues } from 'kea'
 import { ErrorDisplay } from 'lib/components/Errors/ErrorDisplay'
 import { PropertyKeyInfo } from 'lib/components/PropertyKeyInfo'
@@ -10,6 +10,9 @@ import { POSTHOG_EVENT_PROMOTED_PROPERTIES } from 'lib/taxonomy'
 import { autoCaptureEventToDescription, capitalizeFirstLetter, isString } from 'lib/utils'
 import { insightUrlForEvent } from 'scenes/insights/utils'
 import { eventPropertyFilteringLogic } from 'scenes/session-recordings/player/inspector/components/eventPropertyFilteringLogic'
+import { DEFAULT_INSPECTOR_ROW_HEIGHT } from 'scenes/session-recordings/player/inspector/PlayerInspectorList'
+
+import { ElementType } from '~/types'
 
 import { InspectorListItemEvent } from '../playerInspectorLogic'
 import { SimpleKeyValueList } from './SimpleKeyValueList'
@@ -50,6 +53,49 @@ function SummarizeWebVitals({ properties }: { properties: Record<string, any> })
     )
 }
 
+function autocaptureToImage(
+    elements: ElementType[]
+): null | { src: string | undefined; width: string | undefined; height: string | undefined } {
+    const find = elements.find((el) => el.tag_name === 'img')
+    const image = {
+        src: find?.attributes?.attr__src,
+        width: find?.attributes?.attr__width,
+        height: find?.attributes?.attr__height,
+    }
+    return image.src ? image : null
+}
+
+function AutocaptureImage({ item }: ItemEventProps): JSX.Element | null {
+    const img = autocaptureToImage(item.data.elements)
+    if (img) {
+        return (
+            <Tooltip
+                title={
+                    <div className="flex bg-bg-3000 items-center justify-center h-100">
+                        <img
+                            className="max-h-100 object-contain"
+                            src={img.src}
+                            alt="Autocapture image src"
+                            height={img.height || 'auto'}
+                            width={img.width || 'auto'}
+                        />
+                    </div>
+                }
+            >
+                <img
+                    className="max-h-10"
+                    src={img.src}
+                    alt="Autocapture image src"
+                    height={DEFAULT_INSPECTOR_ROW_HEIGHT}
+                    width="auto"
+                />
+            </Tooltip>
+        )
+    }
+
+    return null
+}
+
 export function ItemEvent({ item }: ItemEventProps): JSX.Element {
     const subValue =
         item.data.event === '$pageview' ? (
@@ -58,7 +104,9 @@ export function ItemEvent({ item }: ItemEventProps): JSX.Element {
             item.data.properties.$screen_name
         ) : item.data.event === '$web_vitals' ? (
             <SummarizeWebVitals properties={item.data.properties} />
-        ) : undefined
+        ) : item.data.elements.length ? (
+            <AutocaptureImage item={item} />
+        ) : null
 
     return (
         <div data-attr="item-event" className="font-light w-full">
