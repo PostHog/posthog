a405711e84923578b47c2153efeefafdc5c1868c
Michael Matloka <michael@posthog.com>
2024-11-26 16:00:53 +0100

    Roll back global rounding change

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,8 +53,8 @@ const config = {
         borderRadius: {
             none: '0',
             sm: '0.25rem', // Originally 0.125rem, but we're rounder
-            DEFAULT: '0.375rem', // Originally 0.25rem, but we're rounder - aligned with var(--radius)
-            lg: '0.625rem', // Originally 0.5rem, but we're rounder
+            DEFAULT: 'var(--radius)',
+            lg: '0.5rem',
             full: '9999px',
         },
         extend: {
