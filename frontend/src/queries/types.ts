685b79d34e96137feb742d1bc6f6687cb2ba319d
Rafa Audibert <rafael@posthog.com>
2024-11-27 17:58:37 +0000

    Use more generic types

--- frontend/src/queries/types.ts
+++ frontend/src/queries/types.ts
@@ -49,7 +49,7 @@ export type QueryContextColumnComponent = ComponentType<{
 }>
 
 interface QueryContextColumn {
-    title?: React.ReactNode
+    title?: JSX.Element | string
     renderTitle?: QueryContextColumnTitleComponent
     render?: QueryContextColumnComponent
     align?: 'left' | 'right' | 'center' // default is left
