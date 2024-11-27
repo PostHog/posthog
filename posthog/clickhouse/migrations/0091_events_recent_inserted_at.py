3865496cb01abe3032a6caea05b14b35cba29718
Daniel Escribano <daniel.e@posthog.com>
2024-11-27 18:32:24 +0100

    fix: sql statement to run

--- posthog/clickhouse/migrations/0091_events_recent_inserted_at.py
+++ posthog/clickhouse/migrations/0091_events_recent_inserted_at.py
@@ -1,6 +1,7 @@
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.models.event.sql import (
     EVENTS_RECENT_DATA_TABLE,
+    EVENTS_RECENT_TABLE_SQL,
 )
 
 from posthog.settings import CLICKHOUSE_CLUSTER
@@ -8,5 +9,5 @@ from posthog.settings import CLICKHOUSE_CLUSTER
 
 operations = [
     run_sql_with_exceptions(f"DROP TABLE IF EXISTS {EVENTS_RECENT_DATA_TABLE()} ON CLUSTER '{CLICKHOUSE_CLUSTER}'"),
-    run_sql_with_exceptions(EVENTS_RECENT_DATA_TABLE()),
+    run_sql_with_exceptions(EVENTS_RECENT_TABLE_SQL()),
 ]
