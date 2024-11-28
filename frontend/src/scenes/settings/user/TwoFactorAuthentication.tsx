e3aebe72f818c6850fb9efa4cba29ee646991a0b
Zach Waterfield <zlwaterfield@gmail.com>
2024-11-28 17:30:52 -0500

    Add a feature flag

--- frontend/src/scenes/settings/user/TwoFactorAuthentication.tsx
+++ frontend/src/scenes/settings/user/TwoFactorAuthentication.tsx
@@ -1,6 +1,8 @@
 import { IconCheckCircle, IconCopy, IconWarning } from '@posthog/icons'
 import { LemonButton, LemonModal, lemonToast } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
+import { FEATURE_FLAGS } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { useState } from 'react'
 import { Setup2FA } from 'scenes/authentication/Setup2FA'
 import { setup2FALogic } from 'scenes/authentication/setup2FALogic'
@@ -16,6 +18,7 @@ export function TwoFactorAuthentication(): JSX.Element {
     const [backupCodesModalVisible, setBackupCodesModalVisible] = useState(false)
     const { status } = useValues(setup2FALogic)
     const { generateBackupCodes, disable2FA } = useActions(setup2FALogic)
+    const { featureFlags } = useValues(featureFlagLogic)
 
     const handleSuccess = (): void => {
         updateUser({})
@@ -26,7 +29,7 @@ export function TwoFactorAuthentication(): JSX.Element {
         <div className="flex flex-col items-start">
             {setupModalVisible && (
                 <LemonModal title="Set up 2FA" onClose={() => setSetupModalVisible(false)}>
-                    <>
+                    <div className="max-w-xl">
                         <b>
                             Use an authenticator app like Google Authenticator or 1Password to scan the QR code below.
                         </b>
@@ -36,7 +39,7 @@ export function TwoFactorAuthentication(): JSX.Element {
                                 handleSuccess()
                             }}
                         />
-                    </>
+                    </div>
                 </LemonModal>
             )}
 
@@ -75,29 +78,35 @@ export function TwoFactorAuthentication(): JSX.Element {
                             Save these backup codes in a secure location. Each code can only be used once to sign in if
                             you lose access to your authentication device.
                         </p>
-                        <div className="bg-bg-3000 p-4 rounded font-mono space-y-1 relative">
-                            <LemonButton
-                                icon={<IconCopy />}
-                                size="small"
-                                className="absolute top-4 right-4"
-                                onClick={() => {
-                                    void navigator.clipboard.writeText(status?.backup_codes.join('\n') || '')
-                                    lemonToast.success('Backup codes copied to clipboard')
-                                }}
-                            >
-                                Copy
-                            </LemonButton>
-                            {status?.backup_codes.map((code) => (
-                                <div key={code}>{code}</div>
-                            ))}
-                        </div>
+                        {status?.backup_codes?.length ? (
+                            <div className="bg-bg-3000 p-4 rounded font-mono space-y-1 relative">
+                                <LemonButton
+                                    icon={<IconCopy />}
+                                    size="small"
+                                    className="absolute top-4 right-4"
+                                    onClick={() => {
+                                        void navigator.clipboard.writeText(status.backup_codes.join('\n') || '')
+                                        lemonToast.success('Backup codes copied to clipboard')
+                                    }}
+                                >
+                                    Copy
+                                </LemonButton>
+                                {status.backup_codes.map((code) => (
+                                    <div key={code}>{code}</div>
+                                ))}
+                            </div>
+                        ) : (
+                            <div className="text-center">
+                                <p className="text-muted mb-2">No backup codes available</p>
+                            </div>
+                        )}
                         <LemonButton
                             type="primary"
                             onClick={() => {
                                 generateBackupCodes()
                             }}
                         >
-                            Generate New Codes
+                            {status?.backup_codes?.length ? 'Generate New Codes' : 'Generate Backup Codes'}
                         </LemonButton>
                     </div>
                 </LemonModal>
@@ -109,14 +118,20 @@ export function TwoFactorAuthentication(): JSX.Element {
                         <IconCheckCircle color="green" className="text-xl mr-2" />
                         <span className="font-medium">2FA enabled</span>
                     </div>
-                    <div className="space-x-2 flex items-center">
-                        <LemonButton type="secondary" onClick={() => setBackupCodesModalVisible(true)}>
-                            View Backup Codes
+                    {featureFlags[FEATURE_FLAGS.TWO_FACTOR_UI] ? (
+                        <div className="space-x-2 flex items-center">
+                            <LemonButton type="secondary" onClick={() => setBackupCodesModalVisible(true)}>
+                                View Backup Codes
+                            </LemonButton>
+                            <LemonButton type="secondary" status="danger" onClick={() => setDisableModalVisible(true)}>
+                                Disable 2FA
+                            </LemonButton>
+                        </div>
+                    ) : (
+                        <LemonButton type="primary" to="/account/two_factor/" targetBlank>
+                            Manage or disable 2FA
                         </LemonButton>
-                        <LemonButton type="secondary" status="danger" onClick={() => setDisableModalVisible(true)}>
-                            Disable 2FA
-                        </LemonButton>
-                    </div>
+                    )}
                 </>
             ) : (
                 <div>
