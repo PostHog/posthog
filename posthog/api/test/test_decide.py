8df10d5f5cea68b49955af8968b17883d6939d10
Marius Andra <marius.andra@gmail.com>
2024-11-29 10:15:10 +0100

    test for site functions in decide

--- posthog/api/test/test_decide.py
+++ posthog/api/test/test_decide.py
@@ -38,6 +38,7 @@ from posthog.models import (
 from posthog.models.cohort.cohort import Cohort
 from posthog.models.feature_flag.feature_flag import FeatureFlagHashKeyOverride
 from posthog.models.group.group import Group
+from posthog.models.hog_functions.hog_function import HogFunction
 from posthog.models.organization import Organization, OrganizationMembership
 from posthog.models.person import PersonDistinctId
 from posthog.models.personal_api_key import hash_key_value
@@ -694,6 +695,45 @@ class TestDecide(BaseTest, QueryMatchingTest):
             injected = response.json()["siteApps"]
             self.assertEqual(len(injected), 1)
             self.assertTrue(injected[0]["url"].startswith(f"/site_app/{plugin_config.id}/{plugin_config.web_token}/"))
+
+    def test_site_function_injection(self, *args):
+        # yype: site_app
+        site_app = HogFunction.objects.create(
+            team=self.team,
+            name="my_function",
+            hog="function onLoad(){}",
+            type="site_app",
+            transpiled="function onLoad(){}",
+            enabled=True,
+        )
+
+        self.team.refresh_from_db()
+        self.assertTrue(self.team.inject_web_apps)
+        with self.assertNumQueries(6):
+            response = self._post_decide()
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            injected = response.json()["siteApps"]
+            self.assertEqual(len(injected), 1)
+            self.assertTrue(injected[0]["url"].startswith(f"/site_function/{site_app.id}/"))
+
+        # yype: site_destination
+        site_destination = HogFunction.objects.create(
+            team=self.team,
+            name="my_function",
+            hog="function onLoad(){}",
+            type="site_destination",
+            transpiled="function onLoad(){}",
+            enabled=True,
+        )
+
+        self.team.refresh_from_db()
+        self.assertTrue(self.team.inject_web_apps)
+        with self.assertNumQueries(5):
+            response = self._post_decide()
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            injected = response.json()["siteApps"]
+            self.assertEqual(len(injected), 2)
+            self.assertTrue(injected[1]["url"].startswith(f"/site_function/{site_destination.id}/"))
 
     def test_feature_flags(self, *args):
         self.team.app_urls = ["https://example.com"]
