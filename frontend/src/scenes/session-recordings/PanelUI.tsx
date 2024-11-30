8ea30400d5763f8e1443756e04f46fc82a5091cc
David Newell <david@posthog.com>
2024-11-30 19:26:20 +0000

    better but without container queries

--- frontend/src/scenes/session-recordings/PanelUI.tsx
+++ frontend/src/scenes/session-recordings/PanelUI.tsx
@@ -2,21 +2,21 @@ import clsx from 'clsx'
 
 export function PanelsUI(): JSX.Element {
     return (
-        <Container className="max-h-[615px] h-[615px]">
-            <Container column>
+        <PanelContainer className="SessionRecordings__panel-layout">
+            <PanelContainer className="flex-col">
                 <Panel className="bg-[red]">
                     <>Filters</>
                 </Panel>
                 <Panel priority className="bg-[yellow] min-w-[250px]">
                     <>Playlist</>
                 </Panel>
-            </Container>
+            </PanelContainer>
 
-            <Container priority className="h-full overflow-y-auto min-w-[300px]">
-                <Panel priority className="bg-[green] border border-8 border-[blue] min-w-[300px] h-full">
+            <PanelContainer priority className="h-full overflow-y-auto min-w-[300px]">
+                <Panel priority className="bg-[green] border border-8 border-[blue] min-w-[300px] min-h-[300px]">
                     <>Main content</>
                 </Panel>
-                <Panel className="bg-[pink] min-w-[250px] overflow-y-auto">
+                <Panel className="h-full bg-[pink] min-w-[250px] overflow-y-auto">
                     <div className="flex flex-col">
                         <div>Content</div>
                         <div>Content</div>
@@ -60,31 +60,22 @@ export function PanelsUI(): JSX.Element {
                         <div>Content</div>
                     </div>
                 </Panel>
-            </Container>
-        </Container>
+            </PanelContainer>
+        </PanelContainer>
     )
 }
 
-function Container({
+function PanelContainer({
     children,
-    column,
     priority,
     className,
 }: {
     children: React.ReactNode
-    column?: boolean
     priority?: boolean
     className?: string
 }): JSX.Element {
     return (
-        <div
-            className={clsx(
-                'flex gap-2 flex-wrap',
-                column && 'flex-col',
-                priority ? 'flex-[10]' : 'grow basis-0',
-                className
-            )}
-        >
+        <div className={clsx('flex gap-2 flex-wrap', priority ? 'flex-[10]' : 'grow basis-0', className)}>
             {children}
         </div>
     )
@@ -100,7 +91,7 @@ function Panel({
     priority?: boolean
 }): JSX.Element {
     return (
-        <div className={clsx('h-full', className, priority ? 'flex-[10]' : 'flex-1')}>
+        <div className={clsx(className, priority ? 'flex-[10]' : 'flex-1')}>
             <div>Header</div>
             {children}
         </div>
