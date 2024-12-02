cfef01d96b717cc5c4353ef30c81da8f4ae346f4
David Newell <david@posthog.com>
2024-12-02 14:30:34 +0000

    rough layout

--- frontend/src/scenes/session-recordings/PanelUI.tsx
+++ frontend/src/scenes/session-recordings/PanelUI.tsx
@@ -4,12 +4,12 @@ import clsx from 'clsx'
 
 export function PanelsUI(): JSX.Element {
     return (
-        <PanelContainer className="SessionReplay__layout">
-            <PanelContainer className="PanelLayout__secondary flex-col">
-                <Panel className="bg-[red]">
+        <PanelLayout className="SessionReplay__layout">
+            <PanelContainer primary={false} className="PanelLayout__secondary flex-col">
+                <Panel primary={false} className="bg-[red]">
                     <>Filters</>
                 </Panel>
-                <Panel className="PanelLayout__playlist bg-[yellow]">
+                <Panel primary className="PanelLayout__playlist bg-[yellow]">
                     <div className="flex flex-col">
                         <div>Content</div>
                         <div>Content</div>
@@ -33,11 +33,11 @@ export function PanelsUI(): JSX.Element {
                 </Panel>
             </PanelContainer>
 
-            <PanelContainer className="PanelLayout__primary" tempClassName="">
-                <Panel className="PanelLayout__playback bg-[green] border border-8 border-[blue]">
+            <PanelContainer primary={true} className="PanelLayout__primary">
+                <Panel primary className="PanelLayout__playback bg-[green] border border-8 border-[blue]">
                     <>Main content</>
                 </Panel>
-                <Panel className="PanelLayout__inspector bg-[pink]" tempClassName="">
+                <Panel primary={false} className="PanelLayout__inspector bg-[pink]">
                     <div className="flex flex-col">
                         <div>Content</div>
                         <div>Content</div>
@@ -82,33 +82,35 @@ export function PanelsUI(): JSX.Element {
                     </div>
                 </Panel>
             </PanelContainer>
-        </PanelContainer>
+        </PanelLayout>
     )
 }
 
-function PanelContainer({
-    children,
-    className,
-    tempClassName,
-}: {
+function PanelLayout(props: Omit<PanelContainerProps, 'primary'>): JSX.Element {
+    return <PanelContainer {...props} primary={false} />
+}
+
+type PanelContainerProps = {
     children: React.ReactNode
+    primary: boolean
     className?: string
-    tempClassName?: string
-}): JSX.Element {
-    return <div className={clsx('flex flex-wrap gap-2', className, tempClassName)}>{children}</div>
+}
+
+function PanelContainer({ children, primary, className }: PanelContainerProps): JSX.Element {
+    return <div className={clsx('flex flex-wrap gap-2', primary && 'flex-1', className)}>{children}</div>
 }
 
 function Panel({
     className,
+    primary,
     children,
-    tempClassName,
 }: {
     className?: string
+    primary: boolean
     children: JSX.Element
-    tempClassName?: string
 }): JSX.Element {
     return (
-        <div className={clsx(className, tempClassName)}>
+        <div className={clsx(className, primary && 'flex-1')}>
             <div>Header</div>
             {children}
         </div>
