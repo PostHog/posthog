a91f3cebf04b943af8d04c667cdd6cc44d0a0dbe
David Newell <david@posthog.com>
2024-12-02 14:18:53 +0000

    container queries wip

--- frontend/src/scenes/session-recordings/PanelUI.tsx
+++ frontend/src/scenes/session-recordings/PanelUI.tsx
@@ -5,11 +5,11 @@ import clsx from 'clsx'
 export function PanelsUI(): JSX.Element {
     return (
         <PanelContainer className="SessionReplay__layout">
-            <PanelContainer className="flex-col min-w-[250px]">
+            <PanelContainer className="PanelLayout__secondary flex-col">
                 <Panel className="bg-[red]">
                     <>Filters</>
                 </Panel>
-                <Panel priority className="PanelLayout__playlist bg-[yellow]">
+                <Panel className="PanelLayout__playlist bg-[yellow]">
                     <div className="flex flex-col">
                         <div>Content</div>
                         <div>Content</div>
@@ -33,11 +33,11 @@ export function PanelsUI(): JSX.Element {
                 </Panel>
             </PanelContainer>
 
-            <PanelContainer priority className="PanelLayout__right h-full overflow-y-auto min-w-[300px]">
-                <Panel priority className="bg-[green] border border-8 border-[blue] min-w-[300px] min-h-[300px]">
+            <PanelContainer className="PanelLayout__primary" tempClassName="">
+                <Panel className="PanelLayout__playback bg-[green] border border-8 border-[blue]">
                     <>Main content</>
                 </Panel>
-                <Panel className="PanelLayout__inspector bg-[pink] min-w-[250px] overflow-y-auto">
+                <Panel className="PanelLayout__inspector bg-[pink]" tempClassName="">
                     <div className="flex flex-col">
                         <div>Content</div>
                         <div>Content</div>
@@ -88,31 +88,27 @@ export function PanelsUI(): JSX.Element {
 
 function PanelContainer({
     children,
-    priority,
     className,
+    tempClassName,
 }: {
     children: React.ReactNode
-    priority?: boolean
     className?: string
+    tempClassName?: string
 }): JSX.Element {
-    return (
-        <div className={clsx('flex flex-wrap gap-2', priority ? 'flex-[10]' : 'grow basis-0', className)}>
-            {children}
-        </div>
-    )
+    return <div className={clsx('flex flex-wrap gap-2', className, tempClassName)}>{children}</div>
 }
 
 function Panel({
     className,
     children,
-    priority,
+    tempClassName,
 }: {
     className?: string
     children: JSX.Element
-    priority?: boolean
+    tempClassName?: string
 }): JSX.Element {
     return (
-        <div className={clsx(className, priority ? 'flex-[10]' : 'flex-1')}>
+        <div className={clsx(className, tempClassName)}>
             <div>Header</div>
             {children}
         </div>
