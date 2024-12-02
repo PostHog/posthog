5353455f01d0e76a43611e15f5dcbc5ca67a289a
David Newell <d.newell1@outlook.com>
2024-12-02 22:04:20 +0000

    chore: remove old error embeddings (#26579)

--- frontend/src/scenes/session-recordings/player/PlayerMetaLinks.tsx
+++ frontend/src/scenes/session-recordings/player/PlayerMetaLinks.tsx
@@ -1,13 +1,4 @@
-import {
-    IconDownload,
-    IconEllipsis,
-    IconMagic,
-    IconNotebook,
-    IconPin,
-    IconPinFilled,
-    IconShare,
-    IconTrash,
-} from '@posthog/icons'
+import { IconDownload, IconEllipsis, IconNotebook, IconPin, IconPinFilled, IconShare, IconTrash } from '@posthog/icons'
 import { LemonButton, LemonButtonProps, LemonDialog, LemonMenu, LemonMenuItems } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
 import { FEATURE_FLAGS } from 'lib/constants'
@@ -28,7 +19,6 @@ import { NotebookNodeType } from '~/types'
 
 import { sessionPlayerModalLogic } from './modal/sessionPlayerModalLogic'
 import { PlaylistPopoverButton } from './playlist-popover/PlaylistPopover'
-import { sessionRecordingDataLogic } from './sessionRecordingDataLogic'
 
 function PinToPlaylistButton({
     buttonContent,
@@ -172,11 +162,9 @@ export function PlayerMetaLinks({ iconsOnly }: { iconsOnly: boolean }): JSX.Elem
 const MenuActions = (): JSX.Element => {
     const { logicProps } = useValues(sessionRecordingPlayerLogic)
     const { exportRecordingToFile, deleteRecording, setIsFullScreen } = useActions(sessionRecordingPlayerLogic)
-    const { fetchSimilarRecordings } = useActions(sessionRecordingDataLogic(logicProps))
 
     const hasMobileExportFlag = useFeatureFlag('SESSION_REPLAY_EXPORT_MOBILE_DATA')
     const hasMobileExport = window.IMPERSONATED_SESSION || hasMobileExportFlag
-    const hasSimilarRecordings = useFeatureFlag('REPLAY_SIMILAR_RECORDINGS')
 
     const onDelete = (): void => {
         setIsFullScreen(false)
@@ -207,12 +195,6 @@ const MenuActions = (): JSX.Element => {
             tooltip:
                 'DEBUG ONLY - Export untransformed recording to a file. This can be loaded later into PostHog for playback.',
             icon: <IconDownload />,
-        },
-        hasSimilarRecordings && {
-            label: 'Find similar recordings',
-            onClick: fetchSimilarRecordings,
-            icon: <IconMagic />,
-            tooltip: 'DEBUG ONLY - Find similar recordings based on distance calculations via embeddings.',
         },
         logicProps.playerKey !== 'modal' && {
             label: 'Delete recording',
