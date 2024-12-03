f9a2206d30fc2021d59e1156178f8b71ba8c927d
Alexander Spicer <aspicer@gmail.com>
2024-12-03 08:48:19 -0800

    full sorting

--- posthog/clickhouse/client/execute.py
+++ posthog/clickhouse/client/execute.py
@@ -54,8 +54,11 @@ is_invalid_algorithm = lambda algo: algo not in CLICKHOUSE_SUPPORTED_JOIN_ALGORI
 
 @lru_cache(maxsize=1)
 def default_settings() -> dict:
+    # https://clickhouse.com/blog/clickhouse-fully-supports-joins-how-to-choose-the-right-algorithm-part5
+    # We default to three memory bound join operations, in decreasing order of speed
+    # The merge algorithms are not memory bound, and can be selectively used in places where it makes sense
     return {
-        "join_algorithm": "auto",
+        "join_algorithm": "direct,parallel_hash,hash,full_sorting_merge",
         "distributed_replica_max_ignored_errors": 1000,
         # max_query_size can't be set in a query, because it determines the size of the buffer used to parse the query
         # https://clickhouse.com/docs/en/operations/settings/settings#max_query_size
