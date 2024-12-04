80cc0016464f745fb0b3252c77687ec562aff55e
Tomás Farías Santana <tomas@tomasfarias.dev>
2024-12-04 19:53:58 +0100

    fix: Do not merge heartbeat ranges

--- posthog/temporal/tests/batch_exports/test_heartbeat.py
+++ posthog/temporal/tests/batch_exports/test_heartbeat.py
@@ -53,52 +53,7 @@ def test_insert_done_range(initial_done_ranges, done_range, expected_index):
     """
     heartbeat_details = BatchExportRangeHeartbeatDetails()
     heartbeat_details.done_ranges.extend(initial_done_ranges)
-    heartbeat_details.insert_done_range(done_range, merge=False)
+    heartbeat_details.insert_done_range(done_range)
 
     assert len(heartbeat_details.done_ranges) == len(initial_done_ranges) + 1
     assert heartbeat_details.done_ranges.index(done_range) == expected_index
-
-
-@pytest.mark.parametrize(
-    "initial_done_ranges,expected_done_ranges",
-    [
-        # Case 1: Disconnected ranges are not merged.
-        (
-            [
-                (dt.datetime.fromtimestamp(0), dt.datetime.fromtimestamp(5)),
-                (dt.datetime.fromtimestamp(6), dt.datetime.fromtimestamp(10)),
-            ],
-            [
-                (dt.datetime.fromtimestamp(0), dt.datetime.fromtimestamp(5)),
-                (dt.datetime.fromtimestamp(6), dt.datetime.fromtimestamp(10)),
-            ],
-        ),
-        # Case 2: Connected ranges are merged.
-        (
-            [
-                (dt.datetime.fromtimestamp(0), dt.datetime.fromtimestamp(5)),
-                (dt.datetime.fromtimestamp(5), dt.datetime.fromtimestamp(10)),
-            ],
-            [(dt.datetime.fromtimestamp(0), dt.datetime.fromtimestamp(10))],
-        ),
-        # Case 3: Connected ranges are merged, but disconnected are not.
-        (
-            [
-                (dt.datetime.fromtimestamp(0), dt.datetime.fromtimestamp(5)),
-                (dt.datetime.fromtimestamp(5), dt.datetime.fromtimestamp(10)),
-                (dt.datetime.fromtimestamp(11), dt.datetime.fromtimestamp(12)),
-            ],
-            [
-                (dt.datetime.fromtimestamp(0), dt.datetime.fromtimestamp(10)),
-                (dt.datetime.fromtimestamp(11), dt.datetime.fromtimestamp(12)),
-            ],
-        ),
-    ],
-)
-def test_merge_done_ranges(initial_done_ranges, expected_done_ranges):
-    """Test `BatchExportRangeHeartbeatDetails` merges done ranges."""
-    heartbeat_details = BatchExportRangeHeartbeatDetails()
-    heartbeat_details.done_ranges.extend(initial_done_ranges)
-    heartbeat_details.merge_done_ranges()
-
-    assert heartbeat_details.done_ranges == expected_done_ranges
