15d6e2b6428137510e03dfc9a2eaeb88e1ddaede
Marius Andra <marius.andra@gmail.com>
2024-12-05 15:19:50 +0000

    feat(hog): repl with files

--- frontend/src/scenes/debug/hog/hogReplLogic.ts
+++ frontend/src/scenes/debug/hog/hogReplLogic.ts
@@ -18,6 +18,47 @@ export interface ReplChunk {
     status: 'pending' | 'success' | 'error'
 }
 
+export type TreeFileNode = { replChunks: ReplChunk[]; currentCode: string }
+export type TreeNode = { [key: string]: TreeNode | TreeFileNode }
+export type FileArrayNode = { path: string } & TreeFileNode
+
+function buildFileTree(files: FileArrayNode[]): TreeNode {
+    const tree: TreeNode = {}
+
+    files.forEach(({ path, ...fileNode }) => {
+        const parts = []
+        for (const part of path.split('/')) {
+            if (part === '' && parts.length > 0) {
+                // if a part is '', add '/' to the previous element in the array
+                parts[parts.length - 1] += '/'
+            } else {
+                parts.push(part)
+            }
+        }
+
+        let currentLevel = tree
+
+        parts.forEach((part, index) => {
+            if (index === parts.length - 1) {
+                currentLevel[part] = fileNode
+            } else {
+                if (!currentLevel[part]) {
+                    currentLevel[part] = {}
+                }
+                currentLevel = currentLevel[part] as TreeNode
+            }
+        })
+    })
+
+    return tree
+}
+
+const replaceLast = (arr: any[], newElement: any): any[] => ((arr[arr.length - 1] = newElement), arr)
+
+export const isTreeFileNode = (node: TreeNode | TreeFileNode): node is TreeFileNode => {
+    return typeof node === 'object' && node !== null && 'replChunks' in node && 'currentCode' in node
+}
+
 export const hogReplLogic = kea<hogReplLogicType>([
     path(['scenes', 'debug', 'HogRepl']),
     actions({
@@ -30,30 +71,53 @@ export const hogReplLogic = kea<hogReplLogicType>([
         runCurrentCode: true,
         editFromHere: (index: number) => ({ index }),
         setReplChunks: (replChunks: ReplChunk[]) => ({ replChunks }),
+
+        setCurrentFile: (path: string) => ({ path }),
+        deleteFile: (path: string) => ({ path }),
+        renameFile: (path: string, name: string) => ({ path, name }),
+        newFile: (path: string) => ({ path }),
+        addNewFile: (path: string) => ({ path }),
+        updateFile: (path: string, changes: Partial<TreeFileNode>) => ({ path, changes }),
+        setFiles: (files: FileArrayNode[]) => ({ files }),
     }),
     reducers({
-        currentCode: ['', { setCurrentCode: (_, { code }) => code }],
-        replChunks: [
-            [] as ReplChunk[],
+        currentFile: [
+            'posthog://blank.hog',
             {
-                setReplChunks: (_, { replChunks }) => replChunks,
-                runCode: (state, { code }) => [...state, { code, status: 'pending' } as ReplChunk],
-                setResult: (state, { index, result, error }) =>
-                    state.map((chunk, i) =>
-                        i === index ? { ...chunk, result, error, status: error ? 'error' : 'success' } : chunk
+                setCurrentFile: (_, { path }) => path,
+                addNewFile: (_, { path }) => path,
+            },
+        ],
+        files: [
+            [] as FileArrayNode[],
+            {
+                setFiles: (_, { files }) => files,
+                setCurrentFile: (state, { path }) =>
+                    state.find((file) => file.path === path)
+                        ? state
+                        : [...state, { path, replChunks: [], currentCode: '' }],
+                deleteFile: (state, { path }) => state.filter((file) => file.path !== path),
+                renameFile: (state, { path, name }) =>
+                    state.map((file) =>
+                        file.path === path ? { ...file, path: replaceLast(file.path.split('/'), name).join('/') } : file
                     ),
-                setBytecode: (state, { index, bytecode, locals }) =>
-                    state.map((chunk, i) => (i === index ? { ...chunk, bytecode, locals } : chunk)),
-                print: (state, { index, line }) =>
-                    state.map((chunk, i) =>
-                        i === index ? { ...chunk, print: [...(chunk.print ?? []), line] } : chunk
-                    ),
-                setVMState: (state, { index, state: vmState }) =>
-                    state.map((chunk, i) => (i === index ? { ...chunk, state: vmState } : chunk)),
+                addNewFile: (state, { path }) => [...state, { path, replChunks: [], currentCode: '' }],
+                updateFile: (state, { path, changes }) =>
+                    state.find((file) => file.path === path)
+                        ? state.map((s) => (s.path === path ? { ...s, ...changes } : s))
+                        : [...state, { path, replChunks: [], currentCode: '', ...changes }],
             },
         ],
     }),
     selectors({
+        currentCode: [
+            (s) => [s.files, s.currentFile],
+            (files, currentFile): string => files.find((f) => f.path === currentFile)?.currentCode ?? '',
+        ],
+        replChunks: [
+            (s) => [s.files, s.currentFile],
+            (files, currentFile): ReplChunk[] => files.find((f) => f.path === currentFile)?.replChunks ?? [],
+        ],
         lastLocals: [
             (s) => [s.replChunks],
             (replChunks): ReplChunk['locals'] | undefined => {
@@ -85,14 +149,32 @@ export const hogReplLogic = kea<hogReplLogicType>([
                 return undefined
             },
         ],
+        fileTree: [
+            (s) => [s.files],
+            (files) => {
+                const tree = buildFileTree(files)
+                if (Object.keys(tree).length === 0) {
+                    return { 'posthog:/': {} }
+                }
+                return tree
+            },
+        ],
     }),
     listeners(({ actions, values }) => ({
+        newFile: ({ path }) => {
+            const filename = window.prompt('Enter the name of the new file', 'newFile.hog')
+            actions.addNewFile([path || 'posthog:/', filename ?? 'newFile.hog'].join('/'))
+        },
         editFromHere: ({ index }) => {
             const code = [...values.replChunks.slice(index).map((chunk) => chunk.code), values.currentCode].join('\n')
             actions.setCurrentCode(code.replace(/\n+$/, ''))
             actions.setReplChunks(values.replChunks.slice(0, index))
         },
         runCode: async ({ code }) => {
+            actions.updateFile(values.currentFile, {
+                replChunks: [...values.replChunks, { code, status: 'pending' } as ReplChunk],
+            })
+
             const index = values.replChunks.length - 1
             const { lastLocals, lastState } = values
 
@@ -172,29 +254,48 @@ export const hogReplLogic = kea<hogReplLogicType>([
             actions.runCode(values.currentCode)
             actions.setCurrentCode('')
         },
+        setCurrentCode: ({ code }) => {
+            actions.updateFile(values.currentFile, { currentCode: code })
+        },
+        setReplChunks: ({ replChunks }) => {
+            actions.updateFile(values.currentFile, { replChunks })
+        },
+        setResult: ({ index, result, error }) =>
+            actions.updateFile(values.currentFile, {
+                replChunks: values.replChunks.map((chunk, i) =>
+                    i === index ? { ...chunk, result, error, status: error ? 'error' : 'success' } : chunk
+                ),
+            }),
+        setBytecode: ({ index, bytecode, locals }) =>
+            actions.updateFile(values.currentFile, {
+                replChunks: values.replChunks.map((chunk, i) => (i === index ? { ...chunk, bytecode, locals } : chunk)),
+            }),
+        print: ({ index, line }) =>
+            actions.updateFile(values.currentFile, {
+                replChunks: values.replChunks.map((chunk, i) =>
+                    i === index ? { ...chunk, print: [...(chunk.print ?? []), line] } : chunk
+                ),
+            }),
+        setVMState: ({ index, state: vmState }) =>
+            actions.updateFile(values.currentFile, {
+                replChunks: values.replChunks.map((chunk, i) => (i === index ? { ...chunk, state: vmState } : chunk)),
+            }),
     })),
     actionToUrl(({ values }) => {
         const fn = (): [string, undefined, Record<string, any> | undefined, { replace: true }] | undefined => {
-            if (values.replChunks.length > 0) {
-                // Chrome has a 2MB limit for the HASH params, set ours at 1MB
-                const replChunksLength = JSON.stringify(values.replChunks).length
-                if (replChunksLength > 1024 * 1024) {
-                    // Try with just the code
-                    const newCode = values.replChunks.map((chunk) => chunk.code).join('\n')
-                    if (newCode.length > 1024 * 1024) {
-                        // Still not enough, abort
-                        return [urls.debugHog(), undefined, undefined, { replace: true }]
-                    }
-                    return [urls.debugHog(), undefined, { code: newCode }, { replace: true }]
-                }
-
-                return [
-                    urls.debugHog(),
-                    undefined,
-                    { repl: values.replChunks, code: values.currentCode },
-                    { replace: true },
-                ]
+            // Chrome has a 2MB limit for the HASH params, set ours at 1MB
+            const filesLength = JSON.stringify(values.files).length
+            if (filesLength > 1024 * 1024) {
+                // Can't store this much in the url
+                return [urls.debugHog(), undefined, undefined, { replace: true }]
             }
+
+            return [
+                urls.debugHog(),
+                undefined,
+                { files: values.files, currentFile: values.currentFile },
+                { replace: true },
+            ]
         }
 
         return {
@@ -205,13 +306,25 @@ export const hogReplLogic = kea<hogReplLogicType>([
             print: fn,
             setVMState: fn,
             setCurrentCode: fn,
+            setCurrentFile: fn,
+            deleteFile: fn,
+            renameFile: fn,
+            addNewFile: fn,
+            updateFile: fn,
         }
     }),
     urlToAction(({ actions, values }) => ({
-        [urls.debugHog()]: (_, __, { repl, code }) => {
-            if ((repl || code) && !values.currentCode && values.replChunks.length === 0) {
+        [urls.debugHog()]: (_, __, { repl, code, files, currentFile }) => {
+            if (files) {
+                actions.setFiles(files)
+                if (currentFile && currentFile !== values.currentFile) {
+                    actions.setCurrentFile(currentFile)
+                }
+            } else if ((repl || code) && !values.currentCode && values.replChunks.length === 0) {
                 actions.setReplChunks(repl)
                 actions.setCurrentCode(code)
+            } else {
+                actions.setFiles([{ path: 'posthog://blank.hog', replChunks: [], currentCode: '' }])
             }
         },
     })),
