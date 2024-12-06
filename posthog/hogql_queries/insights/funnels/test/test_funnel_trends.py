1062a58c1ae0aa4d85cc5f1b79abffbafe17917a
Alexander Spicer <aspicer@gmail.com>
2024-12-06 12:05:45 -0800

    add attribution to trends

--- posthog/hogql_queries/insights/funnels/test/test_funnel_trends.py
+++ posthog/hogql_queries/insights/funnels/test/test_funnel_trends.py
@@ -2401,7 +2401,7 @@ class BaseTestFunnelTrends(ClickhouseTestMixin, APIBaseTest):
         assert [0, 0] == [x["reached_to_step_count"] for x in results if x["breakdown_value"] == ["Safari"]]
 
         filters["breakdown_attribution_type"] = "step"
-        filters["breakdown_attribution_value"] = 1
+        filters["breakdown_attribution_value"] = 0
         query = cast(FunnelsQuery, filter_to_query(filters))
         results = FunnelsQueryRunner(query=query, team=self.team, just_summarize=True).calculate().results
 
@@ -2411,61 +2411,14 @@ class BaseTestFunnelTrends(ClickhouseTestMixin, APIBaseTest):
         assert [1, 0] == [x["reached_from_step_count"] for x in results if x["breakdown_value"] == ["Safari"]]
         assert [1, 0] == [x["reached_to_step_count"] for x in results if x["breakdown_value"] == ["Safari"]]
 
-    def test_attribution(self):
-        events = [
-            {
-                "event": "step one",
-                "properties": {"$browser": "Chrome"},
-                "timestamp": datetime(2021, 5, 1, 0, 0, 0),
-            },
-            {
-                "event": "step two",
-                "properties": {"$browser": "Safari"},
-                "timestamp": datetime(2021, 5, 2, 0, 0, 12),
-            },
-            {
-                "event": "exclusion",
-                "timestamp": datetime(2021, 5, 1, 0, 0, 13),
-            },
-            {
-                "event": "step two",
-                "timestamp": datetime(2021, 5, 1, 0, 0, 14),
-            },
-        ]
-        journeys_for(
-            {
-                "user_one": events,
-            },
-            self.team,
-        )
-
-        filters = {
-            "insight": INSIGHT_FUNNELS,
-            "funnel_viz_type": "trends",
-            "interval": "day",
-            "date_from": "2021-05-01 00:00:00",
-            "date_to": "2021-05-13 23:59:59",
-            "funnel_window_interval": 10,
-            "funnel_window_interval_unit": "second",
-            "events": [
-                {"id": "step one", "order": 0},
-                {"id": "step two", "order": 1},
-            ],
-            "exclusions": [
-                {
-                    "id": "exclusion",
-                    "type": "events",
-                    "funnel_from_step": 0,
-                    "funnel_to_step": 1,
-                }
-            ],
-        }
-
+        filters["breakdown_attribution_type"] = "step"
+        filters["breakdown_attribution_value"] = 1
         query = cast(FunnelsQuery, filter_to_query(filters))
         results = FunnelsQueryRunner(query=query, team=self.team, just_summarize=True).calculate().results
 
-        self.assertEqual(0, results[0]["reached_from_step_count"])
-        self.assertEqual(0, results[0]["reached_to_step_count"])
+        assert 2 == len(results)
+        assert [1, 0] == [x["reached_from_step_count"] for x in results if x["breakdown_value"] == ["Chrome"]]
+        assert [1, 0] == [x["reached_to_step_count"] for x in results if x["breakdown_value"] == ["Chrome"]]
 
 
 class TestFunnelTrends(BaseTestFunnelTrends):
