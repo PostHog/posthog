5f3a970e9724ef22b0ab8648a72b2e07418e6a0d
Paul D'Ambra <paul@posthog.com>
2024-12-11 12:57:34 +0000

    add new migration

--- posthog/clickhouse/migrations/0095_add_session_replay_lib.py
+++ posthog/clickhouse/migrations/0095_add_session_replay_lib.py
@@ -2,9 +2,6 @@ from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.session_recordings.sql.session_replay_event_migrations_sql import (
     DROP_SESSION_REPLAY_EVENTS_TABLE_MV_SQL,
     DROP_KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL,
-    ADD_ALL_URLS_SESSION_REPLAY_EVENTS_TABLE_SQL,
-    ADD_ALL_URLS_WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL,
-    ADD_ALL_URLS_DISTRIBUTED_SESSION_REPLAY_EVENTS_TABLE_SQL,
     ADD_LIBRARY_SESSION_REPLAY_EVENTS_TABLE_SQL,
     ADD_LIBRARY_WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL,
     ADD_LIBRARY_DISTRIBUTED_SESSION_REPLAY_EVENTS_TABLE_SQL,
@@ -23,9 +20,6 @@ operations = [
     run_sql_with_exceptions(ADD_LIBRARY_DISTRIBUTED_SESSION_REPLAY_EVENTS_TABLE_SQL()),
     run_sql_with_exceptions(ADD_LIBRARY_WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL()),
     run_sql_with_exceptions(ADD_LIBRARY_SESSION_REPLAY_EVENTS_TABLE_SQL()),
-    run_sql_with_exceptions(ADD_ALL_URLS_DISTRIBUTED_SESSION_REPLAY_EVENTS_TABLE_SQL()),
-    run_sql_with_exceptions(ADD_ALL_URLS_WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL()),
-    run_sql_with_exceptions(ADD_ALL_URLS_SESSION_REPLAY_EVENTS_TABLE_SQL()),
     # and then recreate the materialized views and kafka tables
     run_sql_with_exceptions(KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL()),
     run_sql_with_exceptions(SESSION_REPLAY_EVENTS_TABLE_MV_SQL()),
