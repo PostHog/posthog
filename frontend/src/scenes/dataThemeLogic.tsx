0fa9eeaf2b08d20073e43a26a63fed16f39a18de
Thomas Obermüller <thomas.obermueller@gmail.com>
2024-12-13 15:38:27 +0100

    fix exported insights and dashboards

--- frontend/src/scenes/dataThemeLogic.tsx
+++ frontend/src/scenes/dataThemeLogic.tsx
@@ -1,4 +1,4 @@
-import { actions, afterMount, connect, kea, path, reducers, selectors, useValues } from 'kea'
+import { actions, afterMount, connect, kea, path, props, reducers, selectors, useValues } from 'kea'
 import { loaders } from 'kea-loaders'
 import api from 'lib/api'
 import { DataColorTheme } from 'lib/colors'
@@ -15,18 +15,23 @@ export const ThemeName = ({ id }: { id: number }): JSX.Element => {
     return theme ? <span>{theme.name}</span> : <span className="italic">No theme found for id: {id}</span>
 }
 
+type DataThemeLogicProps = {
+    themes?: DataColorThemeModel[]
+}
+
 export const dataThemeLogic = kea<dataThemeLogicType>([
+    props({} as DataThemeLogicProps),
     path(['scenes', 'dataThemeLogic']),
     connect({ values: [teamLogic, ['currentTeam']] }),
     actions({ setThemes: (themes) => ({ themes }) }),
-    loaders({
+    loaders(({ props }) => ({
         themes: [
-            null as DataColorThemeModel[] | null,
+            props.themes || (null as DataColorThemeModel[] | null),
             {
                 loadThemes: async () => await api.dataColorThemes.list(),
             },
         ],
-    }),
+    })),
     reducers({
         themes: {
             setThemes: (_, { themes }) => themes,
