5dec5f68ef7eb6f0f72bf7acd18d5f31d0b97b29
Daniel Bachhuber <daniel.b@posthog.com>
2024-12-16 08:34:18 -0800

    Recreate failing test case without persons JOIN

--- posthog/hogql_queries/experiments/test/test_experiment_trends_query_runner.py
+++ posthog/hogql_queries/experiments/test/test_experiment_trends_query_runner.py
@@ -246,15 +246,6 @@ class TestExperimentTrendsQueryRunner(ClickhouseTestMixin, APIBaseTest):
             field_name="events",
             configuration={"experiments_optimized": True, "experiments_timestamp_key": "ds"},
         )
-
-        DataWarehouseJoin.objects.create(
-            team=self.team,
-            source_table_name=table_name,
-            source_table_key="userid",
-            joining_table_name="persons",
-            joining_table_key="properties.$user_id",
-            field_name="person",
-        )
         return table_name
 
     @freeze_time("2020-01-01T12:00:00Z")
