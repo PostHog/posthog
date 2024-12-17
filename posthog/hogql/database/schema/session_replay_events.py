02a90911187a687955794ccea1a62c7f0fe325b9
Paul D'Ambra <paul@posthog.com>
2024-12-17 13:15:30 +0000

    feat: add replay view

--- posthog/hogql/database/schema/session_replay_events.py
+++ posthog/hogql/database/schema/session_replay_events.py
@@ -1,3 +1,4 @@
+from posthog.hogql import ast
 from posthog.hogql.ast import SelectQuery, JoinExpr
 from posthog.hogql.context import HogQLContext
 from posthog.hogql.database.models import (
@@ -7,11 +8,9 @@ from posthog.hogql.database.models import (
     IntegerDatabaseField,
     LazyJoin,
     FieldTraverser,
-    DatabaseField,
-    LazyTable,
     FieldOrTable,
-    LazyTableToAdd,
     LazyJoinToAdd,
+    StringArrayDatabaseField,
 )
 from posthog.hogql.database.schema.events import EventsTable
 from posthog.hogql.database.schema.log_entries import ReplayConsoleLogsLogEntriesTable
@@ -30,8 +29,6 @@ from posthog.hogql.errors import ResolutionError
 def join_replay_table_to_sessions_table_v1(
     join_to_add: LazyJoinToAdd, context: HogQLContext, node: SelectQuery
 ) -> JoinExpr:
-    from posthog.hogql import ast
-
     if not join_to_add.fields_accessed:
         raise ResolutionError("No fields requested from replay")
 
@@ -52,8 +49,6 @@ def join_replay_table_to_sessions_table_v1(
 def join_replay_table_to_sessions_table_v2(
     join_to_add: LazyJoinToAdd, context: HogQLContext, node: SelectQuery
 ) -> JoinExpr:
-    from posthog.hogql import ast
-
     if not join_to_add.fields_accessed:
         raise ResolutionError("No fields requested from replay")
 
@@ -76,8 +71,6 @@ def join_with_events_table(
     context: HogQLContext,
     node: SelectQuery,
 ):
-    from posthog.hogql import ast
-
     requested_fields = join_to_add.fields_accessed
     if "$session_id" not in join_to_add.fields_accessed:
         requested_fields = {**join_to_add.fields_accessed, "$session_id": ["$session_id"]}
@@ -109,8 +102,6 @@ def join_with_events_table(
 
 
 def _clamp_to_ttl(chain: list[str | int]):
-    from posthog.hogql import ast
-
     # TRICKY: tests can freeze time, if we use `now()` in the ClickHouse queries then the tests will fail
     # because the time in the query will be different from the time in the test
     # so we generate now in Python and pass it in
@@ -133,8 +124,6 @@ def join_with_console_logs_log_entries_table(
     context: HogQLContext,
     node: SelectQuery,
 ):
-    from posthog.hogql import ast
-
     requested_fields = join_to_add.fields_accessed
     if "log_source_id" not in join_to_add.fields_accessed:
         requested_fields = {**requested_fields, "log_source_id": ["log_source_id"]}
@@ -160,26 +149,25 @@ def join_with_console_logs_log_entries_table(
     return join_expr
 
 
-RAW_ONLY_FIELDS = ["min_first_timestamp", "max_last_timestamp"]
-
 SESSION_REPLAY_EVENTS_COMMON_FIELDS: dict[str, FieldOrTable] = {
     "session_id": StringDatabaseField(name="session_id"),
     "team_id": IntegerDatabaseField(name="team_id"),
     "distinct_id": StringDatabaseField(name="distinct_id"),
-    "min_first_timestamp": DateTimeDatabaseField(name="min_first_timestamp"),
-    "max_last_timestamp": DateTimeDatabaseField(name="max_last_timestamp"),
-    "first_url": DatabaseField(name="first_url"),
+    "first_url": StringDatabaseField(name="first_url", nullable=True),
+    "all_urls": StringArrayDatabaseField(name="all_urls"),
     "click_count": IntegerDatabaseField(name="click_count"),
+    "active_milliseconds": IntegerDatabaseField(name="active_milliseconds"),
     "keypress_count": IntegerDatabaseField(name="keypress_count"),
     "mouse_activity_count": IntegerDatabaseField(name="mouse_activity_count"),
-    "active_milliseconds": IntegerDatabaseField(name="active_milliseconds"),
     "console_log_count": IntegerDatabaseField(name="console_log_count"),
     "console_warn_count": IntegerDatabaseField(name="console_warn_count"),
     "console_error_count": IntegerDatabaseField(name="console_error_count"),
     "size": IntegerDatabaseField(name="size"),
     "event_count": IntegerDatabaseField(name="event_count"),
     "message_count": IntegerDatabaseField(name="message_count"),
-    "snapshot_source": StringDatabaseField(name="snapshot_source"),
+    "snapshot_source": StringDatabaseField(name="snapshot_source", nullable=True),
+    "snapshot_library": StringDatabaseField(name="snapshot_library", nullable=True),
+    "_timestamp": DateTimeDatabaseField(name="_timestamp"),
     "events": LazyJoin(
         from_field=["session_id"],
         join_table=EventsTable(),
@@ -212,12 +200,10 @@ class RawSessionReplayEventsTable(Table):
         **SESSION_REPLAY_EVENTS_COMMON_FIELDS,
         "min_first_timestamp": DateTimeDatabaseField(name="min_first_timestamp"),
         "max_last_timestamp": DateTimeDatabaseField(name="max_last_timestamp"),
-        "first_url": DatabaseField(name="first_url"),
-        "_timestamp": DateTimeDatabaseField(name="_timestamp"),
     }
 
     def avoid_asterisk_fields(self) -> list[str]:
-        return ["first_url"]
+        return [f for f in self.fields if f in ["session_id"]]
 
     def to_printed_clickhouse(self, context):
         return "session_replay_events"
@@ -226,62 +212,15 @@ class RawSessionReplayEventsTable(Table):
         return "raw_session_replay_events"
 
 
-def select_from_session_replay_events_table(requested_fields: dict[str, list[str | int]]):
-    from posthog.hogql import ast
-
-    table_name = "raw_session_replay_events"
-
-    aggregate_fields = {
-        "start_time": ast.Call(name="min", args=[ast.Field(chain=[table_name, "min_first_timestamp"])]),
-        "end_time": ast.Call(name="max", args=[ast.Field(chain=[table_name, "max_last_timestamp"])]),
-        "first_url": ast.Call(name="argMinMerge", args=[ast.Field(chain=[table_name, "first_url"])]),
-        "click_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "click_count"])]),
-        "keypress_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "keypress_count"])]),
-        "mouse_activity_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "mouse_activity_count"])]),
-        "active_milliseconds": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "active_milliseconds"])]),
-        "console_log_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "console_log_count"])]),
-        "console_warn_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "console_warn_count"])]),
-        "console_error_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "console_error_count"])]),
-        "distinct_id": ast.Call(name="any", args=[ast.Field(chain=[table_name, "distinct_id"])]),
-        "size": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "size"])]),
-        "event_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "event_count"])]),
-        "message_count": ast.Call(name="sum", args=[ast.Field(chain=[table_name, "message_count"])]),
-    }
-
-    select_fields: list[ast.Expr] = []
-    group_by_fields: list[ast.Expr] = []
-
-    for name, chain in requested_fields.items():
-        if name in RAW_ONLY_FIELDS:
-            # these fields are accounted for by start_time and end_time, so can be skipped in the "not raw" table
-            continue
-
-        if name in aggregate_fields:
-            select_fields.append(ast.Alias(alias=name, expr=aggregate_fields[name]))
-        else:
-            select_fields.append(ast.Alias(alias=name, expr=ast.Field(chain=[table_name, *chain])))
-            group_by_fields.append(ast.Field(chain=[table_name, *chain]))
-
-    return ast.SelectQuery(
-        select=select_fields,
-        select_from=ast.JoinExpr(table=ast.Field(chain=[table_name])),
-        group_by=group_by_fields,
-    )
-
-
-class SessionReplayEventsTable(LazyTable):
+class SessionReplayEventsTable(Table):
     fields: dict[str, FieldOrTable] = {
-        **{k: v for k, v in SESSION_REPLAY_EVENTS_COMMON_FIELDS.items() if k not in RAW_ONLY_FIELDS},
+        **SESSION_REPLAY_EVENTS_COMMON_FIELDS,
         "start_time": DateTimeDatabaseField(name="start_time"),
         "end_time": DateTimeDatabaseField(name="end_time"),
-        "first_url": StringDatabaseField(name="first_url"),
     }
 
-    def lazy_select(self, table_to_add: LazyTableToAdd, context, node):
-        return select_from_session_replay_events_table(table_to_add.fields_accessed)
-
     def to_printed_clickhouse(self, context):
-        return "session_replay_events"
+        return "grouped_session_replay_events"
 
     def to_printed_hogql(self):
         return "session_replay_events"
