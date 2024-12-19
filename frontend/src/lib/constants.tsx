e8c3cd0681058983d3b12bd2c67c2f3083ba7d89
Robbie Coomber <robbie.coomber@gmail.com>
2024-12-19 13:45:30 +0000

    Add a DB field cookieless_server_hash_mode to a team, and setting to change it behind a flag

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -236,6 +236,7 @@ export const FEATURE_FLAGS = {
     EXPERIMENT_STATS_V2: 'experiment-stats-v2', // owner: @danielbachhuber #team-experiments
     WEB_ANALYTICS_PERIOD_COMPARISON: 'web-analytics-period-comparison', // owner: @rafaeelaudibert #team-web-analytics
     WEB_ANALYTICS_CONVERSION_GOAL_FILTERS: 'web-analytics-conversion-goal-filters', // owner: @rafaeelaudibert #team-web-analytics
+    COOKIELESS_SERVER_HASH_MODE_SETTING: 'cookieless-server-hash-mode-setting', // owner: @robbie-c #team-web-analytics
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
