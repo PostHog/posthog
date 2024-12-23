c1bdcb81a70264094bc919050b63f16e4bcfad60
Daniel Bachhuber <daniel.b@posthog.com>
2024-12-23 05:57:54 -0800

    Drop unnecessary statement

--- posthog/hogql_queries/experiments/test/test_experiment_trends_query_runner.py
+++ posthog/hogql_queries/experiments/test/test_experiment_trends_query_runner.py
@@ -1207,13 +1207,6 @@ class TestExperimentTrendsQueryRunner(ClickhouseTestMixin, APIBaseTest):
                 expected_email_where_statement,
                 str(response.clickhouse),
             )
-            # Assert the expected join condition in the clickhouse SQL
-            expected_join_condition = f"and(equals(events.team_id, {query_runner.count_query_runner.team.id}), equals(event, %(hogql_val_8)s), greaterOrEquals(timestamp, assumeNotNull(parseDateTime64BestEffortOrNull(%(hogql_val_9)s, 6, %(hogql_val_10)s))), lessOrEquals(timestamp, assumeNotNull(parseDateTime64BestEffortOrNull(%(hogql_val_11)s, 6, %(hogql_val_12)s))))) AS e__events ON"
-            self.assertIn(
-                expected_join_condition,
-                str(response.clickhouse),
-                "Please check to make sure the timestamp statements are included in the ASOF LEFT JOIN select statement. This may also fail if the placeholder numbers have changed.",
-            )
 
             result = query_runner.calculate()
 
