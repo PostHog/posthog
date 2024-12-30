6794b432460afde70087aea7b144e354586f3ae5
Raquel Smith <raquelmsmith@users.noreply.github.com>
2024-12-30 11:53:16 -0800

    add to user notification settings ui

--- frontend/src/scenes/settings/user/UpdateEmailPreferences.tsx
+++ frontend/src/scenes/settings/user/UpdateEmailPreferences.tsx
@@ -1,13 +1,15 @@
 import { LemonSwitch } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
+import { organizationLogic } from 'scenes/organizationLogic'
 import { userLogic } from 'scenes/userLogic'
 
 export function UpdateEmailPreferences(): JSX.Element {
     const { user, userLoading } = useValues(userLogic)
     const { updateUser } = useActions(userLogic)
+    const { currentOrganization } = useValues(organizationLogic)
 
     return (
-        <div>
+        <div className="space-y-4">
             <LemonSwitch
                 id="plugin-disabled"
                 data-attr="plugin_disabled"
@@ -25,6 +27,63 @@ export function UpdateEmailPreferences(): JSX.Element {
                 bordered
                 label="Get notified of data pipeline errors."
             />
+
+            <h3>Weekly project digests</h3>
+            <div className="space-y-2">
+                <LemonSwitch
+                    id="all-digests-disabled"
+                    data-attr="all_digests_disabled"
+                    onChange={() => {
+                        user?.notification_settings &&
+                            updateUser({
+                                notification_settings: {
+                                    ...user?.notification_settings,
+                                    all_weekly_digest_disabled: !user?.notification_settings.all_weekly_digest_disabled,
+                                },
+                            })
+                    }}
+                    checked={!user?.notification_settings.all_weekly_digest_disabled}
+                    disabled={userLoading}
+                    bordered
+                    label="Get weekly digests for all projects"
+                />
+                {!user?.notification_settings.all_weekly_digest_disabled ? (
+                    <>
+                        <h4 className="ml-12">Get weekly digests for projects:</h4>
+                        <div className="flex flex-col gap-2 w-fit">
+                            {currentOrganization?.teams?.map((team) => (
+                                <div key={team.id} className="pl-12 flex items-center grow">
+                                    <span className="text-muted-alt mr-2">-</span>
+                                    <LemonSwitch
+                                        id={`project-digest-${team.id}`}
+                                        data-attr={`project_digest_${team.id}`}
+                                        onChange={() => {
+                                            user?.notification_settings &&
+                                                updateUser({
+                                                    notification_settings: {
+                                                        ...user?.notification_settings,
+                                                        project_weekly_digest_disabled: {
+                                                            ...user.notification_settings
+                                                                .project_weekly_digest_disabled,
+                                                            [team.id]:
+                                                                !user.notification_settings
+                                                                    .project_weekly_digest_disabled?.[team.id],
+                                                        },
+                                                    },
+                                                })
+                                        }}
+                                        checked={!user?.notification_settings.project_weekly_digest_disabled?.[team.id]}
+                                        disabled={userLoading || user?.notification_settings.all_weekly_digest_disabled}
+                                        bordered
+                                        label={`${team.name}`}
+                                        fullWidth
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                ) : null}
+            </div>
         </div>
     )
 }
