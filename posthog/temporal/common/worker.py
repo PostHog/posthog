0316fb5a02b995830f50ab7cd7b793c942d22455
Tom Owers <owerstom@gmail.com>
2025-01-03 09:51:39 +0100

    Revert "feat(temporal): Added posthog client to temporal to enable exceptions…"
    This reverts commit 8599213ff7ee7b371bdbaf1b81482df62b5d3358.

--- posthog/temporal/common/worker.py
+++ posthog/temporal/common/worker.py
@@ -8,7 +8,6 @@ from temporalio.worker import UnsandboxedWorkflowRunner, Worker
 
 from posthog.constants import DATA_WAREHOUSE_TASK_QUEUE_V2
 from posthog.temporal.common.client import connect
-from posthog.temporal.common.posthog_client import PostHogClientInterceptor
 from posthog.temporal.common.sentry import SentryInterceptor
 
 
@@ -36,7 +35,6 @@ async def start_worker(
         client_key,
         runtime=runtime,
     )
-
     if task_queue == DATA_WAREHOUSE_TASK_QUEUE_V2:
         worker = Worker(
             client,
@@ -45,7 +43,7 @@ async def start_worker(
             activities=activities,
             workflow_runner=UnsandboxedWorkflowRunner(),
             graceful_shutdown_timeout=timedelta(minutes=5),
-            interceptors=[SentryInterceptor(), PostHogClientInterceptor()],
+            interceptors=[SentryInterceptor()],
             activity_executor=ThreadPoolExecutor(max_workers=max_concurrent_activities or 50),
             # Only run one workflow at a time
             max_concurrent_activities=1,
@@ -61,7 +59,7 @@ async def start_worker(
             activities=activities,
             workflow_runner=UnsandboxedWorkflowRunner(),
             graceful_shutdown_timeout=timedelta(minutes=5),
-            interceptors=[SentryInterceptor(), PostHogClientInterceptor()],
+            interceptors=[SentryInterceptor()],
             activity_executor=ThreadPoolExecutor(max_workers=max_concurrent_activities or 50),
             max_concurrent_activities=max_concurrent_activities or 50,
             max_concurrent_workflow_tasks=max_concurrent_workflow_tasks,
