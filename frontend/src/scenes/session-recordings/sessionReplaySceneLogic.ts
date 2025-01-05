bb85a655cc25becb7f1044cd069076dc93ba128c
Paul D'Ambra <paul@posthog.com>
2025-01-05 22:31:18 +0000

    less to read

--- frontend/src/scenes/session-recordings/sessionReplaySceneLogic.ts
+++ frontend/src/scenes/session-recordings/sessionReplaySceneLogic.ts
@@ -111,9 +111,8 @@ export const sessionReplaySceneLogic = kea<sessionReplaySceneLogicType>([
                 // we saw a page get stuck in a redirect loop between recent and home
                 // see https://posthog.sentry.io/issues/6176801992/?notification_uuid=093e1a3f-c266-4c17-9610-68816996d304&project=1899813&referrer=assigned_activity-email
                 // so, we're extra careful that the value being set is a valid tab
-                const validTab = Object.values(ReplayTabs).includes(tab as ReplayTabs)
-                    ? (tab as ReplayTabs)
-                    : ReplayTabs.Home
+                const candidateTab = tab as ReplayTabs
+                const validTab = Object.values(ReplayTabs).includes(candidateTab) ? candidateTab : ReplayTabs.Home
                 if (validTab !== values.tab) {
                     actions.setTab(validTab)
                 }
