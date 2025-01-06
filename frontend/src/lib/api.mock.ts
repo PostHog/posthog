f52edb0c35bba9e7a1a7bfd972e72a480777169d
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-01-06 16:51:02 -0300

    test: Add new value to mock API

--- frontend/src/lib/api.mock.ts
+++ frontend/src/lib/api.mock.ts
@@ -93,6 +93,7 @@ export const MOCK_DEFAULT_TEAM: TeamType = {
     person_on_events_querying_enabled: true,
     live_events_token: '123',
     capture_dead_clicks: false,
+    human_friendly_comparison_periods: false,
 }
 
 export const MOCK_DEFAULT_PROJECT: ProjectType = {
