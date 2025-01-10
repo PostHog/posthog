8d65a224f3a169425dbce348a4df35e83bda9055
David Newell <david@posthog.com>
2025-01-10 14:12:00 +0000

    fix test

--- posthog/models/user_group/test/test_user_group.py
+++ posthog/models/user_group/test/test_user_group.py
@@ -7,7 +7,7 @@ from posthog.models.user_group import UserGroup, UserGroupMembership
 
 class TestUserGroup(BaseTest):
     def test_user_group_membership_cascade_deletes(self):
-        group = UserGroup.objects.create(project=self.project)
+        group = UserGroup.objects.create(team=self.team)
         UserGroupMembership.objects.create(group=group, user=self.user)
 
         assert UserGroupMembership.objects.count() == 1
@@ -15,7 +15,7 @@ class TestUserGroup(BaseTest):
         assert UserGroupMembership.objects.count() == 0
 
     def test_user_group_membership_uniqueness(self):
-        group = UserGroup.objects.create(project=self.project)
+        group = UserGroup.objects.create(team=self.team)
         with pytest.raises(IntegrityError):
             UserGroupMembership.objects.create(group=group, user=self.user)
             UserGroupMembership.objects.create(group=group, user=self.user)
