25525a5c898f91aad63529995b3bbb82f9dc674a
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-01-10 18:49:37 -0300

    style: Style the top layer for core web vitals
    This is what the top layer will look like once we have our four main metrics. In the future we might decide to expand for more metrics, and in that case a side panel will make more sense, but this is enough for a first launch - hopefully next week.

--- frontend/src/queries/nodes/CoreWebVitals/CoreWebVitals.tsx
+++ frontend/src/queries/nodes/CoreWebVitals/CoreWebVitals.tsx
@@ -1,4 +1,6 @@
-import { LemonSkeleton } from '@posthog/lemon-ui'
+import './CoreWebVitals.scss'
+
+import { LemonSkeleton, Tooltip } from '@posthog/lemon-ui'
 import { clsx } from 'clsx'
 import { useValues } from 'kea'
 import { useMemo, useState } from 'react'
@@ -6,8 +8,8 @@ import { CoreWebVitalsPercentile, webAnalyticsLogic } from 'scenes/web-analytics
 
 import {
     AnyResponseType,
-    CoreWebVitals,
     CoreWebVitalsItem,
+    CoreWebVitalsMetric,
     CoreWebVitalsQuery,
     CoreWebVitalsQueryResponse,
 } from '~/queries/schema'
@@ -15,9 +17,17 @@ import { QueryContext } from '~/queries/types'
 
 import { dataNodeLogic } from '../DataNode/dataNodeLogic'
 
+type Threshold = { good: number; poor: number; end: number }
+const THRESHOLDS: Record<CoreWebVitalsMetric, Threshold> = {
+    INP: { good: 200, poor: 500, end: 550 },
+    LCP: { good: 2500, poor: 4000, end: 4400 },
+    CLS: { good: 0.1, poor: 0.25, end: 0.3 },
+    FCP: { good: 1800, poor: 3000, end: 3300 },
+}
+
 const getMetric = (
     results: CoreWebVitalsItem[] | undefined,
-    metric: CoreWebVitals,
+    metric: CoreWebVitalsMetric,
     percentile: CoreWebVitalsPercentile
 ): number | undefined => {
     return results
@@ -32,7 +42,7 @@ export function CoreWebVitals(props: {
     cachedResults?: AnyResponseType
     context: QueryContext
 }): JSX.Element | null {
-    const [tab, setTab] = useState<CoreWebVitals>('INP')
+    const [tab, setTab] = useState<CoreWebVitalsMetric>('INP')
     const [key] = useState(() => `CoreWebVitals.${uniqueNode++}`)
     const logic = dataNodeLogic({
         query: props.query,
@@ -65,31 +75,38 @@ export function CoreWebVitals(props: {
     )
 
     return (
-        <div className="border rounded bg-bg-light flex-1 flex flex-col">
-            <div className="flex flex-row flex-wrap justify-between items-center [&>*:not(:last-child)]:border-r cursor-pointer">
+        <div className="border rounded bg-bg-muted flex-1 flex flex-col">
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 cursor-pointer border-b divide-y sm:divide-y-2 xl:divide-y-0 divide-x-0 sm:divide-x xl:divide-x-2">
                 <CoreWebVitalsTab
-                    value={INP}
+                    metric="INP"
                     label="Interaction to Next Paint"
+                    value={INP}
                     isActive={tab === 'INP'}
                     setTab={() => setTab('INP')}
+                    inSeconds
                 />
                 <CoreWebVitalsTab
-                    value={LCP}
+                    metric="LCP"
                     label="Largest Contentful Paint"
+                    value={LCP}
                     isActive={tab === 'LCP'}
                     setTab={() => setTab('LCP')}
+                    inSeconds
                 />
                 <CoreWebVitalsTab
-                    value={CLS}
-                    label="Cumulative Layout Shift"
-                    isActive={tab === 'CLS'}
-                    setTab={() => setTab('CLS')}
-                />
-                <CoreWebVitalsTab
-                    value={FCP}
+                    metric="FCP"
                     label="First Contentful Paint"
+                    value={FCP}
                     isActive={tab === 'FCP'}
                     setTab={() => setTab('FCP')}
+                    inSeconds
+                />
+                <CoreWebVitalsTab
+                    metric="CLS"
+                    label="Cumulative Layout Shift"
+                    value={CLS}
+                    isActive={tab === 'CLS'}
+                    setTab={() => setTab('CLS')}
                 />
             </div>
 
@@ -101,33 +118,139 @@ export function CoreWebVitals(props: {
     )
 }
 
+type ValueWithUnit = { value: string | undefined; unit: 's' | 'ms' | undefined }
+const getValueWithUnit = (value: number | undefined, inSeconds: boolean): ValueWithUnit => {
+    if (value === undefined) {
+        return { value: undefined, unit: undefined }
+    }
+
+    // Use a dash to represent lack of value, it's unlikely that a metric will be 0
+    if (value === 0) {
+        return { value: '-', unit: undefined }
+    }
+
+    if (inSeconds) {
+        return value < 1000 ? { value: value.toFixed(0), unit: 'ms' } : { value: (value / 1000).toFixed(2), unit: 's' }
+    }
+
+    return { value: value.toFixed(2), unit: undefined }
+}
+
+type Color = 'muted' | 'success' | 'warning' | 'danger'
+const getThresholdColor = (value: number | undefined, threshold: Threshold): Color => {
+    if (value === undefined) {
+        return 'muted'
+    }
+
+    if (value <= threshold.good) {
+        return 'success'
+    }
+
+    if (value <= threshold.poor) {
+        return 'warning'
+    }
+
+    return 'danger'
+}
+
 function CoreWebVitalsTab({
     value,
     label,
+    metric,
     isActive,
     setTab,
+    inSeconds = false,
 }: {
     value: number | undefined
     label: string
+    metric: CoreWebVitalsMetric
     isActive: boolean
     setTab: () => void
+    inSeconds?: boolean
 }): JSX.Element {
-    // Use a dash to represent lack of value
-    const parsedValue = value === undefined ? undefined : value === 0 ? '-' : (value / 1000).toFixed(2)
+    // TODO: Go back to using an actual value
+    // Will keep this as is while we test what the UI looks like
+    // const {value: parsedValue, unit } = getValueWithUnit(value, inSeconds)
+    const newValue = true ? (inSeconds ? Math.random() * 10000 : Math.random()) : value
+    const { value: parsedValue, unit } = getValueWithUnit(newValue, inSeconds)
+
+    const threshold = THRESHOLDS[metric]
+    const thresholdColor = getThresholdColor(newValue, threshold)
 
     return (
         <div
             onClick={setTab}
-            className={clsx('flex flex-1 flex-col items-center justify-start border-b p-2', {
-                'font-bold border-b-2 border-b-primary': isActive,
-            })}
+            className="CoreWebVitals__CoreWebVitalsTab flex flex-1 flex-row sm:flex-col justify-around sm:justify-start items-center sm:items-start p-4"
+            data-active={isActive ? 'true' : 'false'}
         >
-            <span className="text-sm">{label}</span>
-            <div className="flex flex-row items-center">
-                <span>{parsedValue || <LemonSkeleton fade className="w-4 h-4" />}</span>
-                <span className="text-sm">s</span>
+            <span className="text-sm hidden sm:block">{label}</span>
+            <span className="text-sm block sm:hidden">
+                <Tooltip title={label}>{metric}</Tooltip>
+            </span>
+
+            <div className="flex flex-row items-end">
+                <span className={clsx('text-2xl', `text-${thresholdColor}`)}>
+                    {parsedValue || <LemonSkeleton fade className="w-4 h-4" />}
+                </span>
+                {inSeconds && <span className="text-xs ml-1 mb-1">{unit}</span>}
             </div>
-            <span>Progress bar comes here</span>
+
+            <div className="w-full mt-2 hidden sm:block">
+                {newValue && <ProgressBar value={newValue} threshold={threshold} />}
+            </div>
+        </div>
+    )
+}
+
+interface ProgressBarProps {
+    value: number
+    threshold: Threshold
+}
+
+export function ProgressBar({ value, threshold }: ProgressBarProps): JSX.Element {
+    const indicatorPercentage = Math.min((value / threshold.end) * 100, 100)
+
+    const thresholdColor = getThresholdColor(value, threshold)
+    const isGood = value <= threshold.good
+    const isAverage = !isGood && value <= threshold.poor
+    const isPoor = !isGood && !isAverage
+
+    const goodWidth = (threshold.good / threshold.end) * 100
+    const averageWidth = ((threshold.poor - threshold.good) / threshold.end) * 100
+    const poorWidth = 100 - goodWidth - averageWidth
+
+    return (
+        <div className="w-full h-1 rounded-full relative">
+            {/* Green segment up to "good" threshold */}
+            <div
+                className={clsx('absolute h-full rounded-full', { 'bg-success': isGood, 'bg-muted': !isGood })}
+                // eslint-disable-next-line react/forbid-dom-props
+                style={{ width: `${goodWidth}%` }}
+            />
+
+            {/* Yellow segment up to "poor" threshold */}
+            <div
+                className={clsx('absolute h-full rounded-full', { 'bg-warning': isAverage, 'bg-muted': !isAverage })}
+                // eslint-disable-next-line react/forbid-dom-props
+                style={{ left: `${goodWidth + 1}%`, width: `${averageWidth - 1}%` }}
+            />
+
+            {/* Red segment after "poor" threshold */}
+            <div
+                className={clsx('absolute h-full rounded-full', { 'bg-danger': isPoor, 'bg-muted': !isPoor })}
+                // eslint-disable-next-line react/forbid-dom-props
+                style={{ left: `${goodWidth + averageWidth + 1}%`, width: `${poorWidth - 1}%` }}
+            />
+
+            {/* Indicator line */}
+            <div
+                className={clsx('absolute w-0.5 h-3 -top-1', `bg-${thresholdColor}`)}
+                // eslint-disable-next-line react/forbid-dom-props
+                style={{
+                    left: `${indicatorPercentage}%`,
+                    transform: 'translateX(-50%)',
+                }}
+            />
         </div>
     )
 }
