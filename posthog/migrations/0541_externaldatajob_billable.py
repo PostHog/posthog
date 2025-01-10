0d315ae49d22a28b3abd477dfd2d5e11700b04e2
Tom Owers <owerstom@gmail.com>
2025-01-10 14:02:27 +0100

    Added null=true to the new field

--- posthog/migrations/0541_externaldatajob_billable.py
+++ posthog/migrations/0541_externaldatajob_billable.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="externaldatajob",
             name="billable",
-            field=models.BooleanField(blank=True, default=True),
+            field=models.BooleanField(null=True, blank=True, default=True),
         ),
         migrations.RunSQL(
             """
