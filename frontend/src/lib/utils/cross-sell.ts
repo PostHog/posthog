45fad71c07c4e0d5a271fadbc5f4c781ac0227a2
Joshua Snyder <joshua@posthog.com>
2025-01-13 12:10:15 -0800

    feat: standardize cross selling

---
+++ frontend/src/lib/utils/cross-sell.ts
@@ -0,0 +1,19 @@
+import posthog from 'posthog-js'
+
+import type { ProductKey } from '~/types'
+
+export type ProductCrossSellContext = Record<string, unknown>
+
+export enum ProductCrossSellLocation {
+    TAXONOMIC_FILTER_EMPTY_STATE = 'taxonomic_filter_empty_state',
+}
+
+export type ProductCrossSellProperties = {
+    from: ProductKey
+    to: ProductKey
+    location: ProductCrossSellLocation
+}
+
+export function trackProductCrossSell(properties: ProductCrossSellProperties): void {
+    posthog.capture('product cross sell interaction', properties)
+}
