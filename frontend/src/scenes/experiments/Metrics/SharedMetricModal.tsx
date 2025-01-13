95b053af40d35435ac128527ce793c83e6bb0470
Daniel Bachhuber <daniel.b@posthog.com>
2025-01-13 05:47:38 -0800

    fix(experiments): Fix adding/removing metric race condition (#27459)

--- frontend/src/scenes/experiments/Metrics/SharedMetricModal.tsx
+++ frontend/src/scenes/experiments/Metrics/SharedMetricModal.tsx
@@ -24,6 +24,7 @@ export function SharedMetricModal({
         closeSecondarySharedMetricModal,
         addSharedMetricToExperiment,
         removeSharedMetricFromExperiment,
+        loadExperiment,
     } = useActions(experimentLogic({ experimentId }))
 
     const [selectedMetricId, setSelectedMetricId] = useState<SharedMetric['id'] | null>(null)
@@ -41,7 +42,11 @@ export function SharedMetricModal({
     }
 
     const isOpen = isSecondary ? isSecondarySharedMetricModalOpen : isPrimarySharedMetricModalOpen
-    const closeModal = isSecondary ? closeSecondarySharedMetricModal : closePrimarySharedMetricModal
+    const closeModal = (): void => {
+        // :KLUDGE: Removes any local changes and resets the experiment to the server state
+        loadExperiment()
+        isSecondary ? closeSecondarySharedMetricModal() : closePrimarySharedMetricModal()
+    }
 
     return (
         <LemonModal
