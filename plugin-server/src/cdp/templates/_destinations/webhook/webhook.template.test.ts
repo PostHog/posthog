9dab7a5142151af345ce21d4a6fecf3bb0dd6405
Ben White <ben@posthog.com>
2025-01-14 12:04:21 +0100

    Fixes

--- plugin-server/src/cdp/templates/_destinations/webhook/webhook.template.test.ts
+++ plugin-server/src/cdp/templates/_destinations/webhook/webhook.template.test.ts
@@ -10,27 +10,36 @@ describe('webhook template', () => {
     })
 
     it('should invoke the function', async () => {
-        const response = await tester.invoke({
-            url: 'https://example.com?v={event.properties.$lib_version}',
-            debug: false,
-        })
+        const response = await tester.invoke(
+            {
+                url: 'https://example.com?v={event.properties.$lib_version}',
+                debug: false,
+            },
+            {
+                event: {
+                    properties: {
+                        $lib_version: '1.0.0',
+                    },
+                },
+            }
+        )
 
         expect(response.error).toBeUndefined()
         expect(response.finished).toEqual(false)
         expect(response.invocation.queue).toEqual('fetch')
         expect(response.invocation.queueParameters).toMatchInlineSnapshot(`
-            Object {
-              "body": "{\\"value\\":{\\"event\\":{\\"uuid\\":\\"event-id\\",\\"event\\":\\"event-name\\",\\"distinct_id\\":\\"distinct-id\\",\\"properties\\":{\\"$current_url\\":\\"https://example.com\\"},\\"timestamp\\":\\"2024-01-01T00:00:00Z\\",\\"elements_chain\\":\\"\\",\\"url\\":\\"https://us.posthog.com/projects/1/events/1234\\"},\\"person\\":{\\"id\\":\\"person-id\\",\\"name\\":\\"person-name\\",\\"properties\\":{\\"email\\":\\"example@posthog.com\\"},\\"url\\":\\"https://us.posthog.com/projects/1/persons/1234\\"}}}",
-              "headers": Object {
-                "value": Object {
+            {
+              "body": "{"value":{"event":{"uuid":"event-id","event":"event-name","distinct_id":"distinct-id","properties":{"$lib_version":"1.0.0"},"timestamp":"2024-01-01T00:00:00Z","elements_chain":"","url":"https://us.posthog.com/projects/1/events/1234"},"person":{"id":"person-id","name":"person-name","properties":{"email":"example@posthog.com"},"url":"https://us.posthog.com/projects/1/persons/1234"}}}",
+              "headers": {
+                "value": {
                   "Content-Type": "application/json",
                 },
               },
-              "method": Object {
+              "method": {
                 "value": "POST",
               },
               "return_queue": "hog",
-              "url": "https://example.com?v=",
+              "url": "https://example.com?v=1.0.0",
             }
         `)
 
@@ -51,11 +60,11 @@ describe('webhook template', () => {
 
         expect(response.error).toBeUndefined()
         expect(response.logs.filter((l) => l.level === 'info')).toMatchInlineSnapshot(`
-            Array [
-              Object {
+            [
+              {
                 "level": "info",
-                "message": "Request, https://example.com?v=, {\\"headers\\":{\\"value\\":{\\"Content-Type\\":\\"application/json\\"}},\\"body\\":{\\"value\\":{\\"event\\":{\\"uuid\\":\\"event-id\\",\\"event\\":\\"event-name\\",\\"distinct_id\\":\\"distinct-id\\",\\"properties\\":{\\"$current_url\\":\\"https://example.com\\"},\\"timestamp\\":\\"2024-01-01T00:00:00Z\\",\\"elements_chain\\":\\"\\",\\"url\\":\\"https://us.posthog.com/projects/1/events/1234\\"},\\"person\\":{\\"id\\":\\"person-id\\",\\"name\\":\\"person-name\\",\\"properties\\":{\\"email\\":\\"example@posthog.com\\"},\\"url\\":\\"https://us.posthog.com/projects/1/persons/1234\\"}}},\\"method\\":{\\"value\\":\\"POST\\"}}",
-                "timestamp": "2025-01-01T00:00:00.000+00:00",
+                "message": "Request, https://example.com?v=, {"headers":{"value":{"Content-Type":"application/json"}},"body":{"value":{"event":{"uuid":"event-id","event":"event-name","distinct_id":"distinct-id","properties":{"$current_url":"https://example.com"},"timestamp":"2024-01-01T00:00:00Z","elements_chain":"","url":"https://us.posthog.com/projects/1/events/1234"},"person":{"id":"person-id","name":"person-name","properties":{"email":"example@posthog.com"},"url":"https://us.posthog.com/projects/1/persons/1234"}}},"method":{"value":"POST"}}",
+                "timestamp": "2025-01-01T01:00:00.000+01:00",
               },
             ]
         `)
@@ -67,11 +76,11 @@ describe('webhook template', () => {
 
         expect(response.error).toBeUndefined()
         expect(response.logs.filter((l) => l.level === 'info')).toMatchInlineSnapshot(`
-            Array [
-              Object {
+            [
+              {
                 "level": "info",
-                "message": "Response, 200, {\\"message\\":\\"Hello, world!\\"}",
-                "timestamp": "2025-01-01T00:00:00.000+00:00",
+                "message": "Response, 200, {"message":"Hello, world!"}",
+                "timestamp": "2025-01-01T01:00:00.000+01:00",
               },
             ]
         `)
