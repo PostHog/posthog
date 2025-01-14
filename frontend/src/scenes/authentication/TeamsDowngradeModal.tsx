ddd6a4a74bfc38cdcd778545c757f94dbfeb41ff
Surbhi <surbhi@posthog.com>
2025-01-14 14:09:30 -0500

    show 2fa

--- frontend/src/scenes/authentication/TeamsDowngradeModal.tsx
+++ frontend/src/scenes/authentication/TeamsDowngradeModal.tsx
@@ -1,16 +1,17 @@
+import { IconWarning } from '@posthog/icons'
 import { LemonButton, LemonModal } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
 
 import { teamsDowngradeLogic } from './teamsDowngradeLogic'
 
 export function TeamsDowngradeModal(): JSX.Element {
-    const { isTeamsDowngradeModalOpen } = useValues(teamsDowngradeLogic)
+    const { isTeamsDowngradeModalOpen, enforce2FA } = useValues(teamsDowngradeLogic)
     const { hideTeamsDowngradeModal, handleTeamsDowngrade } = useActions(teamsDowngradeLogic)
 
     return (
         <LemonModal
             title="Unsubscribe from Teams"
-            description="You are about to lose access to the following features:"
+            description="Your team is currently using the following features and will lose access to them if you unsubscribe:"
             isOpen={isTeamsDowngradeModalOpen}
             onClose={hideTeamsDowngradeModal}
             footer={
@@ -24,7 +25,15 @@ export function TeamsDowngradeModal(): JSX.Element {
                 </>
             }
         >
-            <p>TODO: Add features that are currently being used</p>
+            <div className="space-y-2">
+                <p>TODO: Add features that are currently being used</p>
+                {enforce2FA && (
+                    <div className="flex items-center gap-2">
+                        <IconWarning className="text-warning" />
+                        <span>Enforced 2FA</span>
+                    </div>
+                )}
+            </div>
         </LemonModal>
     )
 }
