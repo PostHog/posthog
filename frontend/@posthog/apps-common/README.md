8fd86d9fb7ba1ad960c1f6e6fb2f0ed80479cf79
Marius Andra <marius.andra@gmail.com>
2025-01-17 16:03:51 +0100

    more config

---
+++ frontend/@posthog/apps-common/README.md
@@ -0,0 +1,3 @@
+# PostHog App Common Packages
+
+This is an experimental package that contains tools frontend apps can use. Please don't use yet.
---
+++ frontend/@posthog/apps-common/src/index.ts
@@ -0,0 +1,10 @@
+import '~/styles'
+import '~/initKea'
+
+export * from 'lib/lemon-ui/Link'
+export * from 'lib/components/TZLabel'
+export * from 'scenes/persons/PersonDisplay'
+export * from 'scenes/urls'
+
+import api_ from 'lib/api'
+export const api = api_
