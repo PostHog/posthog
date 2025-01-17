1870834518324ba5cb7dae3f99a4dc9f86eb2978
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2025-01-16 16:00:13 -0800

    easy enough

--- posthog/clickhouse/cluster.py
+++ posthog/clickhouse/cluster.py
@@ -118,7 +118,14 @@ class ClickhouseCluster:
             return FuturesMap({host: executor.submit(self.__get_task_function(host, fn)) for host in self.__hosts})
 
     def map_all_hosts_in_shard(self, shard_num: int, fn: Callable[[Client], T]) -> FuturesMap[HostInfo, T]:
-        raise NotImplementedError
+        with ThreadPoolExecutor() as executor:
+            return FuturesMap(
+                {
+                    host: executor.submit(self.__get_task_function(host, fn))
+                    for host in self.__hosts
+                    if host.shard_num == shard_num
+                }
+            )
 
     def map_one_host_per_shard(self, fn: Callable[[Client], T]) -> FuturesMap[HostInfo, T]:
         """
