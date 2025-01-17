d754e5242cd9499520197700a862cc301be0a44e
James Greenhill <fuziontech@gmail.com>
2025-01-17 08:55:55 -0800

    add django orm examples

--- dags/definitions.py
+++ dags/definitions.py
@@ -1,8 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import ch_examples, deletes
+from . import ch_examples, deletes, orm_examples
 
-all_assets = load_assets_from_modules([ch_examples, deletes])
+all_assets = load_assets_from_modules([ch_examples, deletes, orm_examples])
 
 defs = Definitions(
     assets=all_assets,
