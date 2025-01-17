3d3d128734594d1e57a1f098b8f7c87bee792005
Ross Gray <ross@posthog.com>
2025-01-17 11:56:35 +0000

    WIP

--- posthog/temporal/batch_exports/sql.py
+++ posthog/temporal/batch_exports/sql.py
@@ -138,16 +138,36 @@ SELECT_FROM_EVENTS_VIEW_RECENT_DISTRIBUTED = Template(
     """
 SELECT
     $fields
-FROM
-    events_batch_export_recent_distributed(
-        team_id={team_id},
-        interval_start={interval_start},
-        interval_end={interval_end},
-        include_events={include_events}::Array(String),
-        exclude_events={exclude_events}::Array(String)
-    ) AS events
+FROM (
+    SELECT DISTINCT ON (team_id, event, cityHash64(distributed_events_recent.distinct_id), cityHash64(distributed_events_recent.uuid))
+        team_id AS team_id,
+        timestamp AS timestamp,
+        event AS event,
+        distinct_id AS distinct_id,
+        toString(uuid) AS uuid,
+        inserted_at AS _inserted_at,
+        created_at AS created_at,
+        elements_chain AS elements_chain,
+        toString(person_id) AS person_id,
+        nullIf(properties, '') AS properties,
+        nullIf(person_properties, '') AS person_properties,
+        nullIf(JSONExtractString(properties, '$set'), '') AS set,
+        nullIf(JSONExtractString(properties, '$set_once'), '') AS set_once
+    FROM
+        distributed_events_recent
+    PREWHERE
+        distributed_events_recent.inserted_at >= {{interval_start:DateTime64}}
+        AND distributed_events_recent.inserted_at < {{interval_end:DateTime64}}
+    WHERE
+        team_id = {{team_id:Int64}}
+        AND (length({{include_events:Array(String)}}) = 0 OR event IN {{include_events:Array(String)}})
+        AND (length({{exclude_events:Array(String)}}) = 0 OR event NOT IN {{exclude_events:Array(String)}})
+    ORDER BY
+        _inserted_at, event
+)
 FORMAT ArrowStream
 SETTINGS
+    optimize_aggregation_in_order=1,
     -- This is half of configured MAX_MEMORY_USAGE for batch exports.
     max_bytes_before_external_sort=50000000000,
     max_replica_delay_for_distributed_queries=60,
