3705c2e62eb7a07f48a86a36d359a9232584fd9b
James Greenhill <fuziontech@gmail.com>
2025-01-17 08:52:46 -0800

    bring back the simple ch examples

--- dags/definitions.py
+++ dags/definitions.py
@@ -1,8 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import deletes
+from . import ch_examples, deletes
 
-all_assets = load_assets_from_modules([deletes])
+all_assets = load_assets_from_modules([ch_examples, deletes])
 
 defs = Definitions(
     assets=all_assets,
