5ba0a8c143ab67ea20031121ee854e290b8a12d4
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-01-18 02:09:03 -0300

    refactor: Fix missing `CoreWebVitalsPathBreakdownQuery` on `AnyDataNode` type

--- frontend/src/queries/schema/schema-general.ts
+++ frontend/src/queries/schema/schema-general.ts
@@ -129,6 +129,7 @@ export type AnyDataNode =
     | WebExternalClicksTableQuery
     | WebGoalsQuery
     | CoreWebVitalsQuery
+    | CoreWebVitalsPathBreakdownQuery
     | SessionAttributionExplorerQuery
     | ErrorTrackingQuery
     | ExperimentFunnelsQuery
