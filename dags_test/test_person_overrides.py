e2dd83951c3a253a489f49c71a88b85fc33e243d
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2025-01-20 13:40:32 -0800

    fixtures

--- dags_test/test_person_overrides.py
+++ dags_test/test_person_overrides.py
@@ -1,26 +1,30 @@
 import uuid
+from collections.abc import Iterator
+
 import dagster
 import pytest
 
 from dags.person_overrides import (
-    SnapshotTableConfig,
-    squash_person_overrides,
     PersonOverridesSnapshotDictionary,
     PersonOverridesSnapshotTable,
+    SnapshotTableConfig,
+    create_snapshot_table,
+    squash_person_overrides,
 )
 from ee.clickhouse.materialized_columns.columns import get_cluster  # XXX
 from posthog.clickhouse.cluster import ClickhouseCluster
 
 
 @pytest.fixture
-def cluster(django_db_setup) -> ClickhouseCluster:
+def cluster(django_db_setup) -> Iterator[ClickhouseCluster]:
     yield get_cluster()
 
 
-def test_job(cluster):
+def test_full_job(cluster):
     timestamp = "2025-01-01"
+
     result = squash_person_overrides.execute_in_process(
-        run_config=dagster.RunConfig({"create_snapshot_table": SnapshotTableConfig(timestamp=timestamp)}),
+        run_config=dagster.RunConfig({create_snapshot_table.name: SnapshotTableConfig(timestamp=timestamp)}),
         resources={"cluster": cluster},
     )
 
