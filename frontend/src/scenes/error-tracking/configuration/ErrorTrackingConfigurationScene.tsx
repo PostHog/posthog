7dcd43f513f8c388aeeb4abd783dc454f99bf1aa
David Newell <david@posthog.com>
2025-01-20 17:55:48 +0000

    configuration breadcrumbs

--- frontend/src/scenes/error-tracking/configuration/ErrorTrackingConfigurationScene.tsx
+++ frontend/src/scenes/error-tracking/configuration/ErrorTrackingConfigurationScene.tsx
@@ -1,10 +1,37 @@
-import { SceneExport } from 'scenes/sceneTypes'
+import { kea, path, selectors } from 'kea'
+import { Scene, SceneExport } from 'scenes/sceneTypes'
 import { Settings } from 'scenes/settings/Settings'
+import { urls } from 'scenes/urls'
+
+import { Breadcrumb } from '~/types'
 
 import { AlphaAccessScenePrompt } from '../AlphaAccessScenePrompt'
+import type { errorTrackingConfigurationSceneLogicType } from './ErrorTrackingConfigurationSceneType'
+
+export const errorTrackingConfigurationSceneLogic = kea<errorTrackingConfigurationSceneLogicType>([
+    path(['scenes', 'error-tracking', 'configuration', 'errorTrackingConfigurationSceneLogic']),
+    selectors({
+        breadcrumbs: [
+            () => [],
+            (): Breadcrumb[] => [
+                {
+                    key: Scene.ErrorTracking,
+                    path: urls.errorTracking(),
+                    name: 'Error tracking',
+                },
+                {
+                    key: Scene.ErrorTrackingConfiguration,
+                    path: urls.errorTrackingConfiguration(),
+                    name: 'Configuration',
+                },
+            ],
+        ],
+    }),
+])
 
 export const scene: SceneExport = {
     component: ErrorTrackingConfigurationScene,
+    logic: errorTrackingConfigurationSceneLogic,
 }
 
 export function ErrorTrackingConfigurationScene(): JSX.Element {
