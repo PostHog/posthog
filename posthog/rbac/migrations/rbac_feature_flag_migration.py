53bb06fd2648077432bd63af9f1506e3f5c9d7d6
Zach Waterfield <zlwaterfield@gmail.com>
2025-01-20 19:52:26 -0500

    Create rbac_feature_flag_migration.py

---
+++ posthog/rbac/migrations/rbac_feature_flag_migration.py
@@ -0,0 +1,19 @@
+from ee.models.rbac.access_control import AccessControl
+from ee.models.feature_flag_role_access import FeatureFlagRoleAccess
+
+
+def rbac_feature_flag_migrations(organization_id: int):
+    # Get feature flag role access for the organization
+    feature_flag_role_access = FeatureFlagRoleAccess.objects.filter(role__organization_id=organization_id)
+
+    for role_access in feature_flag_role_access:
+        # Create access control for the feature flag role access
+        feature_flag = role_access.feature_flag
+        role = role_access.role
+        AccessControl.objects.create(
+            team=feature_flag.team,
+            access_level="editor",
+            resource="feature_flag",
+            resource_id=feature_flag.id,
+            role=role,
+        )
