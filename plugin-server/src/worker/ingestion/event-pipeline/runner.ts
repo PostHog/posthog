75f8196e295379f3e4d0b6069abfa83962477cb5
David Newell <david@posthog.com>
2025-01-21 10:01:20 +0000

    feat: process sentry exceptions

--- plugin-server/src/worker/ingestion/event-pipeline/runner.ts
+++ plugin-server/src/worker/ingestion/event-pipeline/runner.ts
@@ -259,7 +259,7 @@ export class EventPipelineRunner {
             event.team_id
         )
 
-        if (event.event === '$exception' && !event.properties?.hasOwnProperty('$sentry_event_id')) {
+        if (event.event === '$exception') {
             const [exceptionAck] = await this.runStep(
                 produceExceptionSymbolificationEventStep,
                 [this, rawEvent],
