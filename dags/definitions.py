789f933a850e5cc8a2c5c960a9c2144b3d8a7282
James Greenhill <fuziontech@gmail.com>
2025-01-22 11:07:39 -0800

    add delete schedule

--- dags/definitions.py
+++ dags/definitions.py
@@ -1,13 +1,22 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, ScheduleDefinition
 
 from . import ch_examples, deletes, orm_examples
 from .person_overrides import ClickhouseClusterResource, squash_person_overrides
 
 all_assets = load_assets_from_modules([ch_examples, deletes, orm_examples])
 
+# Schedule to run deletes at 10 PM on Saturdays
+deletes_schedule = ScheduleDefinition(
+    job=deletes,
+    cron_schedule="0 22 * * 6",  # At 22:00 (10 PM) on Saturday
+    execution_timezone="UTC",
+    name="deletes_schedule",
+)
+
 defs = Definitions(
     assets=all_assets,
     jobs=[squash_person_overrides],
+    schedules=[deletes_schedule],
     resources={
         "cluster": ClickhouseClusterResource.configure_at_launch(),
     },
