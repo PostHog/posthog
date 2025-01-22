f04ae368b1f794ee99366d227aa4c1253b4fe404
meikel <meikel@posthog.com>
2025-01-22 17:29:00 +0100

    try simpler approach

--- docker/postgres-init-scripts/create-cyclotron-db.sh
+++ docker/postgres-init-scripts/create-cyclotron-db.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# This script is used to create the cyclotron database.
-# It is run when the postgres container is started.
-
 set -e
 set -u
 
-echo "Creating database 'cyclotron' if it doesn't exist"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    DO \$\$
-    BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'cyclotron') THEN
-            CREATE DATABASE cyclotron;
-        END IF;
-    END
-    \$\$;
-    GRANT ALL PRIVILEGES ON DATABASE cyclotron TO $POSTGRES_USER;
-EOSQL 
+echo "Checking if database 'cyclotron' exists..."
+DB_EXISTS=$(psql -U "$POSTGRES_USER" -tAc "SELECT 1 FROM pg_database WHERE datname='cyclotron'")
+
+if [ -z "$DB_EXISTS" ]; then
+    echo "Creating database 'cyclotron'..."
+    psql -U "$POSTGRES_USER" -c "CREATE DATABASE cyclotron;"
+    psql -U "$POSTGRES_USER" -c "GRANT ALL PRIVILEGES ON DATABASE cyclotron TO $POSTGRES_USER;"
+    echo "Database 'cyclotron' created successfully"
+else
+    echo "Database 'cyclotron' already exists"
+fi
