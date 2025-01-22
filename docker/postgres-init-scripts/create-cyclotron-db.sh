3cd97b6d47c6e91778b7b42e0ce03852a05a424f
meikel <meikel@posthog.com>
2025-01-22 17:16:59 +0100

    adjust script

--- docker/postgres-init-scripts/create-cyclotron-db.sh
+++ docker/postgres-init-scripts/create-cyclotron-db.sh
@@ -6,8 +6,14 @@
 set -e
 set -u
 
-echo "Creating database 'cyclotron'"
+echo "Creating database 'cyclotron' if it doesn't exist"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE cyclotron;
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'cyclotron') THEN
+            CREATE DATABASE cyclotron;
+        END IF;
+    END
+    \$\$;
     GRANT ALL PRIVILEGES ON DATABASE cyclotron TO $POSTGRES_USER;
 EOSQL 
