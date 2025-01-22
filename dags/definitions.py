a831ea6f8c3c3ee4e853085b29eaab887e32e034
James Greenhill <fuziontech@gmail.com>
2025-01-22 11:13:58 -0800

    create schedule for this

--- dags/definitions.py
+++ dags/definitions.py
@@ -1,13 +1,25 @@
-from dagster import Definitions, load_assets_from_modules, ScheduleDefinition
+from dagster import Definitions, load_assets_from_modules, ScheduleDefinition, define_asset_job
 
 from . import ch_examples, deletes, orm_examples
 from .person_overrides import ClickhouseClusterResource, squash_person_overrides
 
 all_assets = load_assets_from_modules([ch_examples, deletes, orm_examples])
 
+# Create a job definition for the deletes assets
+deletes_job = define_asset_job(
+    name="deletes_job",
+    selection=[
+        "create_pending_deletes_table",
+        "pending_person_deletions",
+        "create_pending_deletes_dictionary",
+        "delete_person_events",
+        "cleanup_delete_assets",
+    ],
+)
+
 # Schedule to run deletes at 10 PM on Saturdays
 deletes_schedule = ScheduleDefinition(
-    job=deletes,
+    job=deletes_job,
     cron_schedule="0 22 * * 6",  # At 22:00 (10 PM) on Saturday
     execution_timezone="UTC",
     name="deletes_schedule",
@@ -15,7 +27,7 @@ deletes_schedule = ScheduleDefinition(
 
 defs = Definitions(
     assets=all_assets,
-    jobs=[squash_person_overrides],
+    jobs=[squash_person_overrides, deletes_job],
     schedules=[deletes_schedule],
     resources={
         "cluster": ClickhouseClusterResource.configure_at_launch(),
