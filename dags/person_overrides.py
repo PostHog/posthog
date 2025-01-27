efb50458e82d5bd5f4b537e417b6713efdf40509
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2025-01-27 15:42:23 -0800

    move to posthog.clickhouse.cluster

--- dags/person_overrides.py
+++ dags/person_overrides.py
@@ -1,16 +1,19 @@
 import time
 import uuid
-from collections.abc import Mapping
 from dataclasses import dataclass
 from functools import partial, reduce
-from typing import Any
 
 import dagster
 import pydantic
 from clickhouse_driver import Client
 
 from posthog import settings
-from posthog.clickhouse.cluster import ClickhouseCluster, get_cluster
+from posthog.clickhouse.cluster import (
+    ClickhouseCluster,
+    Mutation,
+    MutationRunner,
+    get_cluster,
+)
 from posthog.models.event.sql import EVENTS_DATA_TABLE
 from posthog.models.person.sql import PERSON_DISTINCT_ID_OVERRIDES_TABLE
 
@@ -91,82 +94,6 @@ class PersonOverridesSnapshotTable:
             {"database": settings.CLICKHOUSE_DATABASE, "table": self.name},
         )
         assert queue_size == 0
-
-
-@dataclass
-class Mutation:
-    table: str
-    mutation_id: str
-
-    def is_done(self, client: Client) -> bool:
-        [[is_done]] = client.execute(
-            f"""
-            SELECT is_done
-            FROM system.mutations
-            WHERE database = %(database)s AND table = %(table)s AND mutation_id = %(mutation_id)s
-            ORDER BY create_time DESC
-            """,
-            {"database": settings.CLICKHOUSE_DATABASE, "table": self.table, "mutation_id": self.mutation_id},
-        )
-        return is_done
-
-    def wait(self, client: Client) -> None:
-        while not self.is_done(client):
-            time.sleep(15.0)
-
-
-@dataclass
-class MutationRunner:
-    table: str
-    command: str  # the part after ALTER TABLE prefix, i.e. UPDATE, DELETE, MATERIALIZE, etc.
-    parameters: Mapping[str, Any]
-
-    def find(self, client: Client) -> Mutation | None:
-        """Find the running mutation task, if one exists."""
-        results = client.execute(
-            f"""
-            SELECT mutation_id
-            FROM system.mutations
-            WHERE
-                database = %(_database_{id(self)})s
-                AND table = %(_table_{id(self)})s
-                -- only one command per mutation is currently supported, so throw if the mutation contains more than we expect to find
-                -- throwIf always returns 0 if it does not throw, so negation turns this condition into effectively a noop if the test passes
-                AND NOT throwIf(
-                    length(splitByChar('\n', formatQuery($_sql_{id(self)}$ALTER TABLE {settings.CLICKHOUSE_DATABASE}{self.table} {self.command}$_sql_{id(self)}$)) as lines) != 2,
-                    'unexpected number of lines, expected 2 (ALTER TABLE prefix, followed by single command)'
-                )
-                AND command = trim(lines[2])
-                AND NOT is_killed  -- ok to restart a killed mutation
-            ORDER BY create_time DESC
-            """,
-            {
-                f"_database_{id(self)}": settings.CLICKHOUSE_DATABASE,
-                f"_table_{id(self)}": self.table,
-                **self.parameters,
-            },
-        )
-        if not results:
-            return None
-        else:
-            assert len(results) == 1
-            [[mutation_id]] = results
-            return Mutation(self.table, mutation_id)
-
-    def enqueue(self, client: Client) -> Mutation:
-        """Enqueue the mutation (or return the existing mutation if it is already running.)"""
-        if task := self.find(client):
-            return task
-
-        client.execute(
-            f"ALTER TABLE {settings.CLICKHOUSE_DATABASE}.{self.table} {self.command}",
-            self.parameters,
-        )
-
-        task = self.find(client)
-        assert task is not None
-
-        return task
 
 
 @dataclass
