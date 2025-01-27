c0d674f3a0a7e8064128c25a0f4b8f3ae4b9dac6
Paweł Ledwoń <me@difficult.name>
2025-01-27 10:15:45 +0100

    refactor: simplify mr blobby v2 abstractions (#27896)

--- plugin-server/src/main/ingestion-queues/session-recording-v2/kafka/offset-manager.ts
+++ plugin-server/src/main/ingestion-queues/session-recording-v2/kafka/offset-manager.ts
@@ -1,49 +1,13 @@
 import { TopicPartitionOffset } from 'node-rdkafka'
 
-import { SessionBatchRecorderInterface } from '../sessions/session-batch-recorder'
-import { MessageWithTeam } from '../teams/types'
-
-interface PartitionOffset {
-    partition: number
-    offset: number
-}
+import { PartitionOffset } from '../types'
 
 type CommitOffsetsCallback = (offsets: TopicPartitionOffset[]) => Promise<void>
-
-class OffsetTrackingSessionBatchRecorderWrapper implements SessionBatchRecorderInterface {
-    constructor(
-        private readonly recorder: SessionBatchRecorderInterface,
-        private readonly offsetManager: KafkaOffsetManager
-    ) {}
-
-    public record(message: MessageWithTeam): number {
-        const bytesWritten = this.recorder.record(message)
-        this.offsetManager.trackOffset(message.message.metadata)
-        return bytesWritten
-    }
-
-    public async flush(): Promise<void> {
-        await this.recorder.flush()
-    }
-
-    public discardPartition(partition: number): void {
-        this.recorder.discardPartition(partition)
-        this.offsetManager.discardPartition(partition)
-    }
-
-    public get size(): number {
-        return this.recorder.size
-    }
-}
 
 export class KafkaOffsetManager {
     private partitionOffsets: Map<number, number> = new Map()
 
     constructor(private readonly commitOffsets: CommitOffsetsCallback, private readonly topic: string) {}
-
-    public wrapBatch(recorder: SessionBatchRecorderInterface): SessionBatchRecorderInterface {
-        return new OffsetTrackingSessionBatchRecorderWrapper(recorder, this)
-    }
 
     public trackOffset({ partition, offset }: PartitionOffset): void {
         // We track the next offset to process
