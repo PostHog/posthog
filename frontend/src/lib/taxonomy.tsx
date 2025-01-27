b2fad558b4e465322171df61386d42e94a7545df
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-01-27 12:48:47 +0100

    feat: taxonomy

--- frontend/src/lib/taxonomy.tsx
+++ frontend/src/lib/taxonomy.tsx
@@ -184,6 +184,11 @@ export const CORE_FILTER_DEFINITIONS_BY_GROUP = {
             description:
                 'A generative AI trace. Usually a trace tracks a single user interaction and contains one or more AI generation calls',
         },
+        $ai_span: {
+            label: 'AI Span',
+            description:
+                'A generative AI span. Usually a span tracks a unit of work for a trace of generative AI models (LLMs)',
+        },
         $ai_metric: {
             label: 'AI Metric',
             description: 'An evaluation metric for a trace of generative AI models (LLMs)',
@@ -1489,6 +1494,21 @@ export const CORE_FILTER_DEFINITIONS_BY_GROUP = {
             label: 'AI Feedback Text (LLM)',
             description: 'The text provided by the user for feedback on the LLM trace',
             examples: ['"The response was helpful, but it did not use the provided context."'],
+        },
+        $ai_parent_id: {
+            label: 'AI Parent ID (LLM)',
+            description: 'The parent span ID of a span or generation, used to group a trace into a tree view',
+            examples: ['bdf42359-9364-4db7-8958-c001f28c9255'],
+        },
+        $ai_generation_id: {
+            label: 'AI Generation ID (LLM)',
+            description: 'The unique identifier for a LLM generation',
+            examples: ['bdf42359-9364-4db7-8958-c001f28c9255'],
+        },
+        $ai_span_id: {
+            label: 'AI Span ID (LLM)',
+            description: 'The unique identifier for a LLM span',
+            examples: ['bdf42359-9364-4db7-8958-c001f28c9255'],
         },
     },
     numerical_event_properties: {}, // Same as event properties, see assignment below
