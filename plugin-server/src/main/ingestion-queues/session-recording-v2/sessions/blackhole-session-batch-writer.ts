7725907b715f0c8ad127ce2a7d648e846854f4ff
Paweł Ledwoń <me@difficult.name>
2025-01-27 15:45:46 +0100

    refactor: discard flushed data immediately

--- plugin-server/src/main/ingestion-queues/session-recording-v2/sessions/blackhole-session-batch-writer.ts
+++ plugin-server/src/main/ingestion-queues/session-recording-v2/sessions/blackhole-session-batch-writer.ts
@@ -1,16 +1,25 @@
-import { PassThrough } from 'stream'
+import { Writable } from 'stream'
 
 import { SessionBatchWriter, StreamWithFinish } from './session-batch-recorder'
 
+class BlackholeStream extends Writable {
+    constructor() {
+        super()
+    }
+
+    _write(chunk: any, encoding: string, callback: (error?: Error | null) => void): void {
+        // Immediately discard the data and signal we're ready for more
+        callback()
+    }
+}
+
 export class BlackholeSessionBatchWriter implements SessionBatchWriter {
     public async open(): Promise<StreamWithFinish> {
-        const stream = new PassThrough()
-
-        stream.on('data', () => {})
-
         return Promise.resolve({
-            stream,
-            finish: async () => Promise.resolve(),
+            stream: new BlackholeStream(),
+            finish: async () => {
+                return Promise.resolve()
+            },
         })
     }
 }
