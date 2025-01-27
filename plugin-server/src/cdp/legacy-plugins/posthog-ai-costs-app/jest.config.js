d28c1a7ee8a1664b7cd3fb25d559f8cc973efb5c
Ben White <ben@posthog.com>
2025-01-27 16:22:53 +0100

    Fix

--- plugin-server/src/cdp/legacy-plugins/posthog-ai-costs-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    moduleDirectories: ['node_modules', 'src'],
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-        '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    transformIgnorePatterns: [],
-}
