a1251436280829d333547ecfbebda9bb4b46bbe6
Peter Kirkham <peter@posthog.com>
2025-01-29 15:38:05 -0800

    feat: if no event sent dont load data tables and make request

--- products/llm_observability/frontend/LLMObservabilityScene.tsx
+++ products/llm_observability/frontend/LLMObservabilityScene.tsx
@@ -1,3 +1,4 @@
+import { IconArchive } from '@posthog/icons'
 import { LemonBanner, LemonTabs, Link } from '@posthog/lemon-ui'
 import clsx from 'clsx'
 import { BindLogic, useActions, useValues } from 'kea'
@@ -122,8 +123,25 @@ function LLMObservabilityGenerations(): JSX.Element {
     )
 }
 
+function LLMObservabilityNoEvents(): JSX.Element {
+    return (
+        <div className="w-full flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center max-w-md w-full">
+                <IconArchive className="text-5xl mb-2 text-muted-alt" />
+                <h2 className="text-xl leading-tight">We haven't detected any LLM generations yet</h2>
+                <p className="text-sm text-center text-balance">
+                    To use the LLM Observability product, please{' '}
+                    <Link to="https://posthog.com/docs/ai-engineering/observability">
+                        instrument your LLM calls with the PostHog SDK
+                    </Link>{' '}
+                </p>
+            </div>
+        </div>
+    )
+}
+
 export function LLMObservabilityScene(): JSX.Element {
-    const { activeTab } = useValues(llmObservabilityLogic)
+    const { activeTab, hasSentAiGenerationEvent } = useValues(llmObservabilityLogic)
     const { searchParams } = useValues(router)
 
     return (
@@ -141,13 +159,17 @@ export function LLMObservabilityScene(): JSX.Element {
                     {
                         key: 'traces',
                         label: 'Traces',
-                        content: <LLMObservabilityTraces />,
+                        content: hasSentAiGenerationEvent ? <LLMObservabilityTraces /> : <LLMObservabilityNoEvents />,
                         link: combineUrl(urls.llmObservabilityTraces(), searchParams).url,
                     },
                     {
                         key: 'generations',
                         label: 'Generations',
-                        content: <LLMObservabilityGenerations />,
+                        content: hasSentAiGenerationEvent ? (
+                            <LLMObservabilityGenerations />
+                        ) : (
+                            <LLMObservabilityNoEvents />
+                        ),
                         link: combineUrl(urls.llmObservabilityGenerations(), searchParams).url,
                     },
                 ]}
