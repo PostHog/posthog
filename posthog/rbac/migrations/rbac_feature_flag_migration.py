e826f173a7e39047194a25e72ab522d17b255c38
Zach Waterfield <zlwaterfield@gmail.com>
2025-01-29 12:55:51 -0500

    Improve migration logging

--- posthog/rbac/migrations/rbac_feature_flag_migration.py
+++ posthog/rbac/migrations/rbac_feature_flag_migration.py
@@ -1,12 +1,17 @@
 from ee.models.rbac.access_control import AccessControl
 from ee.models.feature_flag_role_access import FeatureFlagRoleAccess
 from django.db import transaction
+import structlog
+
+logger = structlog.get_logger(__name__)
 
 
 # This migration is used to migrate over feature flag permissions to the new RBAC system
 # It will find all feature flag role access for the organization and migrate them over to the new system
 # It will also remove the feature flag role access so we know it's been migrated
 def rbac_feature_flag_role_access_migration(organization_id: str):
+    logger.info("Starting RBAC feature flag role access migration", organization_id=organization_id)
+
     # Get feature flag role access for the organization
     feature_flag_role_access = FeatureFlagRoleAccess.objects.filter(role__organization_id=organization_id)
 
@@ -22,5 +27,14 @@ def rbac_feature_flag_role_access_migration(organization_id: str):
                 resource_id=feature_flag.id,
                 role=role,
             )
-            # Remove the feature flag role access
+            # Remove the feature flag role access (so we know it's been migrated)
             role_access.delete()
+
+            logger.info(
+                "Migrated RBAC feature flag access control",
+                organization_id=organization_id,
+                user_id=role.user.id,
+                feature_flag_id=feature_flag.id,
+            )
+
+    logger.info("Finished RBAC feature flag role access migration", organization_id=organization_id)
--- posthog/rbac/migrations/rbac_team_migration.py
+++ posthog/rbac/migrations/rbac_team_migration.py
@@ -16,37 +16,41 @@ def rbac_team_access_control_migration(organization_id: int):
     with transaction.atomic():
         organization = Organization.objects.get(id=organization_id)
         for team in organization.teams.all():
-            # Migrate over team permissions if it's currently private
-            if team.access_control:
+            # Skip if access control is already disabled
+            if not team.access_control:
+                continue
+
+            # Create access control for the team
+            AccessControl.objects.create(
+                team=team,
+                access_level="none",
+                resource="project",
+                resource_id=team.id,
+            )
+
+            # Get all members for the project
+            team_memberships = ExplicitTeamMembership.objects.filter(team_id=team.id)
+            for team_membership in team_memberships:
+                # Create access control for the team member
                 AccessControl.objects.create(
                     team=team,
-                    access_level="none",
+                    access_level="admin" if team_membership.level == ExplicitTeamMembership.Level.ADMIN else "member",
                     resource="project",
                     resource_id=team.id,
+                    organization_member=team_membership.parent_membership,
+                )
+                # Remove the existing team membership
+                team_membership.delete()
+
+                logger.info(
+                    "Migrated RBAC team access control",
+                    organization_id=team.id,
+                    user_id=team_membership.parent_membership.user.id,
+                    team_id=team.id,
                 )
 
-                # Get all members for the project
-                team_memberships = ExplicitTeamMembership.objects.filter(team_id=team.id)
-                for team_membership in team_memberships:
-                    AccessControl.objects.create(
-                        team=team,
-                        access_level="admin"
-                        if team_membership.level == ExplicitTeamMembership.Level.ADMIN
-                        else "member",
-                        resource="project",
-                        resource_id=team.id,
-                        organization_member=team_membership.parent_membership,
-                    )
-                    logger.info(
-                        "Created RBAC access control",
-                        organization_id=team.id,
-                        user_id=team_membership.parent_membership.user.id,
-                    )
-                    # Remove the team membership
-                    team_membership.delete()
-
-                # Remove the team access control
-                team.access_control = False
-                team.save()
+            # Disable access control for the team (so we know it's been migrated)
+            team.access_control = False
+            team.save()
 
     logger.info("Finished RBAC team migrations", organization_id_id=organization_id)
