d16e1e3ebd67ad0a650dca868a42c9e149b341af
Frank Hamand <frankhamand@gmail.com>
2025-01-30 09:45:39 +0000

    refactor sink creation in server.rs to remove duplication
    Box up the sink so that router is agnostic to whether its using a
    KafkaSink or FallbackSink

--- rust/capture/src/sinks/mod.rs
+++ rust/capture/src/sinks/mod.rs
@@ -12,4 +12,15 @@ pub trait Event {
     async fn send_batch(&self, events: Vec<ProcessedEvent>) -> Result<(), CaptureError>;
 }
 
+#[async_trait]
+impl<T: Event + ?Sized + Send + Sync> Event for Box<T> {
+    async fn send(&self, event: ProcessedEvent) -> Result<(), CaptureError> {
+        (**self).send(event).await
+    }
+
+    async fn send_batch(&self, events: Vec<ProcessedEvent>) -> Result<(), CaptureError> {
+        (**self).send_batch(events).await
+    }
+}
+
 pub use fallback::FallbackSink;
