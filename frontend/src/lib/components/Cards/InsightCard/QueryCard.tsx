eb51522e6dd3f4ebbd85848b9d5876b80167e521
Adam Leith <adamleithp@users.noreply.github.com>
2025-01-30 11:15:32 +0000

    Discard changes to frontend/src/lib/components/Cards/InsightCard/QueryCard.tsx

--- frontend/src/lib/components/Cards/InsightCard/QueryCard.tsx
+++ frontend/src/lib/components/Cards/InsightCard/QueryCard.tsx
@@ -9,7 +9,6 @@ import { ErrorBoundary } from '~/layout/ErrorBoundary'
 import { themeLogic } from '~/layout/navigation-3000/themeLogic'
 import { Query } from '~/queries/Query/Query'
 import { Node } from '~/queries/schema'
-import { QueryContext } from '~/queries/types'
 
 import { InsightCardProps } from './InsightCard'
 import { InsightDetails } from './InsightDetails'
@@ -20,12 +19,11 @@ export interface QueryCardProps extends Pick<InsightCardProps, 'highlighted' | '
     query: Node
     title: string
     description?: string
-    context?: QueryContext
 }
 
 /** This is like InsightCard, except for presentation of queries that aren't saved insights. */
 export const QueryCard = React.forwardRef<HTMLDivElement, QueryCardProps>(function QueryCard(
-    { query, title, description, context, highlighted, ribbonColor, className, ...divProps },
+    { query, title, description, highlighted, ribbonColor, className, ...divProps },
     ref
 ): JSX.Element {
     const { theme } = useValues(themeLogic)
@@ -67,7 +65,7 @@ export const QueryCard = React.forwardRef<HTMLDivElement, QueryCardProps>(functi
                     showEditingControls
                 />
                 <div className="InsightCard__viz">
-                    <Query query={query} readOnly embedded context={context} />
+                    <Query query={query} readOnly embedded />
                 </div>
             </ErrorBoundary>
         </div>
