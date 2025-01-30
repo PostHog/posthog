37c42e5658004a9d060b14c1904ad74bfbaeb594
David Newell <david@posthog.com>
2025-01-30 17:35:58 +0000

    avoid circular imports

--- posthog/exceptions_capture.py
+++ posthog/exceptions_capture.py
@@ -1,7 +1,6 @@
-from sentry_sdk import capture_exception as sentry_capture_exception
-from posthoganalytics import capture_exception as posthog_capture_exception
-
-
 def capture_exception(error=None, sentry_scope=None, **sentry_scope_kwargs):
+    from sentry_sdk import capture_exception as sentry_capture_exception
+    from posthoganalytics import capture_exception as posthog_capture_exception
+
     sentry_capture_exception(error, scope=sentry_scope, **sentry_scope_kwargs)
     posthog_capture_exception(error)
