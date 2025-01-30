5b75c6b17253a7abc3908178828467681f4caef9
meikel <meikel@posthog.com>
2025-01-30 14:56:52 +0100

    feat(cdp): return & execute function sorted by execution_order & created_at

--- posthog/api/hog_function.py
+++ posthog/api/hog_function.py
@@ -68,6 +68,7 @@ class HogFunctionMinimalSerializer(serializers.ModelSerializer):
             "icon_url",
             "template",
             "status",
+            "execution_order",
         ]
         read_only_fields = fields
 
@@ -116,6 +117,7 @@ class HogFunctionSerializer(HogFunctionMinimalSerializer):
             "template",
             "template_id",
             "status",
+            "execution_order",
         ]
         read_only_fields = [
             "id",
@@ -321,6 +323,8 @@ class HogFunctionViewSet(
             else:
                 types = ["destination"]
             queryset = queryset.filter(type__in=types)
+            # Add ordering by execution_order and created_at
+            queryset = queryset.order_by("execution_order", "created_at")
 
         if self.request.GET.get("filters"):
             try:
