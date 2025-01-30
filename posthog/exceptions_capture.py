ab3c68436c789f30a749a51d477d5b85127e712b
David Newell <david@posthog.com>
2025-01-30 18:20:52 +0000

    use api_key as condition

--- posthog/exceptions_capture.py
+++ posthog/exceptions_capture.py
@@ -1,9 +1,8 @@
 def capture_exception(error=None, sentry_scope=None, **sentry_scope_kwargs):
     from sentry_sdk import capture_exception as sentry_capture_exception
-    from posthoganalytics import capture_exception as posthog_capture_exception
-    from posthog.settings import TEST
+    from posthoganalytics import api_key, capture_exception as posthog_capture_exception
 
     sentry_capture_exception(error, scope=sentry_scope, **sentry_scope_kwargs)
 
-    if not TEST:
+    if not api_key:
         posthog_capture_exception(error)
