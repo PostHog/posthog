b0c26f478c791db4bc8f0489248ec243c7a6e69d
Michael Matloka <michael@matloka.com>
2025-01-31 12:11:20 +0100

    Use a MANIFEST.in instead

--- common/hogql_parser/setup.py
+++ common/hogql_parser/setup.py
@@ -32,7 +32,7 @@ module = Extension(
 
 setup(
     name="hogql_parser",
-    version="1.0.457",
+    version="1.0.458",
     url="https://github.com/PostHog/posthog/tree/master/common/hogql_parser",
     description="HogQL parser for internal PostHog use",
     author="PostHog Inc.",
@@ -41,8 +41,6 @@ setup(
     maintainer_email="hey@posthog.com",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    package_data={"hogql_parser": ["*.pyi", "py.typed"]},
-    include_package_data=True,
     ext_modules=[module],
     python_requires=">=3.10",
     classifiers=[
