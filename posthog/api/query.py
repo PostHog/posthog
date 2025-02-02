6a6a798c29c15548ecd8e58fec4300fda8d8bb84
Pawel Szczur <pawel@posthog.com>
2025-02-02 23:22:53 +0100

    Allow to name a query send to /query endpoint.

--- posthog/api/query.py
+++ posthog/api/query.py
@@ -61,7 +61,6 @@ class QueryViewSet(TeamAndOrgViewSetMixin, PydanticModelMixin, viewsets.ViewSet)
         if self.action == "draft_sql":
             return [AIBurstRateThrottle(), AISustainedRateThrottle()]
         if query := self.request.data.get("query"):
-            # raise Exception("TEST EXCEPTION")
             if isinstance(query, dict) and query.get("kind") == "HogQLQuery":
                 return [HogQLQueryThrottle()]
         return [ClickHouseBurstRateThrottle(), ClickHouseSustainedRateThrottle()]
@@ -101,10 +100,8 @@ class QueryViewSet(TeamAndOrgViewSetMixin, PydanticModelMixin, viewsets.ViewSet)
             # Here in query endpoint we always want to calculate if the cache is stale
             execution_mode = ExecutionMode.RECENT_CACHE_CALCULATE_BLOCKING_IF_STALE
 
-        kw = {"query": request.data["query"]}
-        if data.name:
-            kw["name"] = data.name
-        tag_queries(**kw)
+        # kw = {"query": request.data["query"]}
+        tag_queries(query=request.data["query"])
         try:
             result = process_query_model(
                 self.team,
--- posthog/schema.py
+++ posthog/schema.py
@@ -6232,6 +6232,9 @@ class HogQLQuery(BaseModel):
     variables: Optional[dict[str, HogQLVariable]] = Field(
         default=None, description="Variables to be substituted into the query"
     )
+    name: Optional[str] = Field(
+        default=None, alias="name", max_length=64, description="Client provided name of the query."
+    )
 
 
 class InsightActorsQueryOptionsResponse(BaseModel):
@@ -7810,9 +7813,6 @@ class QueryRequest(BaseModel):
     async_: Optional[bool] = Field(default=None, alias="async")
     client_query_id: Optional[str] = Field(
         default=None, description="Client provided query ID. Can be used to retrieve the status or cancel the query."
-    )
-    name: Optional[str] = Field(
-        default=None, alias="name", max_length=64, description="Client provided name of the query."
     )
     filters_override: Optional[DashboardFilter] = None
     query: Union[
