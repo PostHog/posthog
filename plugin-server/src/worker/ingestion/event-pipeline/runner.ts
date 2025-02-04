7196ae4b96be5044ecdcf2a5d29a9074145daec1
Ben White <ben@posthog.com>
2025-02-04 11:48:49 +0100

    fix

--- plugin-server/src/worker/ingestion/event-pipeline/runner.ts
+++ plugin-server/src/worker/ingestion/event-pipeline/runner.ts
@@ -239,7 +239,7 @@ export class EventPipelineRunner {
             const shouldCompareToHogFunctions =
                 this.hogTransformer && Math.random() < (this.hub.HOG_TRANSFORMATIONS_COMPARISON_PERCENTAGE ?? 0)
 
-            if (!shouldCompareToHogFunctions) {
+            if (shouldCompareToHogFunctions) {
                 clonedSourceEvent = cloneObject(postCookielessEvent)
             }
         } catch (error) {
