bf1649af2ec8f646443062dacc540e55dbdd6437
Paweł Ledwoń <me@difficult.name>
2025-02-04 11:55:52 +0100

    feat: support session recording v2 s3 access key

--- plugin-server/src/types.ts
+++ plugin-server/src/types.ts
@@ -340,6 +340,8 @@ export interface PluginsServerConfig extends CdpConfig, IngestionConsumerConfig 
     SESSION_RECORDING_V2_S3_PREFIX: string
     SESSION_RECORDING_V2_S3_ENDPOINT: string
     SESSION_RECORDING_V2_S3_REGION: string
+    SESSION_RECORDING_V2_S3_ACCESS_KEY_ID: string
+    SESSION_RECORDING_V2_S3_SECRET_ACCESS_KEY: string
 }
 
 export interface Hub extends PluginsServerConfig {
