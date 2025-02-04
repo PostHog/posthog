1bcbf1825c6d2cd8abf522073e758975c3b5f0b5
Eric Duong <eric@posthog.com>
2025-02-04 10:11:07 -0700

    fix(hogql): autocomplete fails when source query is empty (#28236)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- posthog/hogql/autocomplete.py
+++ posthog/hogql/autocomplete.py
@@ -371,8 +371,13 @@ def get_hogql_autocomplete(
         database = create_hogql_database(team_id=team.pk, team_arg=team)
 
     context = HogQLContext(team_id=team.pk, team=team, database=database)
-    if query.sourceQuery is not None:
-        source_query = get_query_runner(query=query.sourceQuery, team=team).to_query()
+    if query.sourceQuery:
+        if query.sourceQuery.kind == "HogQLQuery" and (
+            query.sourceQuery.query is None or query.sourceQuery.query == ""
+        ):
+            source_query = parse_select("select 1")
+        else:
+            source_query = get_query_runner(query=query.sourceQuery, team=team).to_query()
     else:
         source_query = parse_select("select 1")
 
