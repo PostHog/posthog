8b188f55e3a07c1f66c790bfa4c551fa1e40a0cd
Paul D'Ambra <paul@posthog.com>
2025-02-05 15:49:17 +0000

    Update posthog/taxonomy/taxonomy.py

--- posthog/taxonomy/taxonomy.py
+++ posthog/taxonomy/taxonomy.py
@@ -273,7 +273,7 @@ CORE_FILTER_DEFINITIONS_BY_GROUP: dict[str, dict[str, CoreFilterDefinition]] = {
     },
     "event_properties": {
         "$last_posthog_reset": {
-            "label": "Timestamp of last call to `Reset`in the web sdk",
+            "label": "Timestamp of last call to `Reset` in the web sdk",
             "description": "The timestamp of the last call to `Reset` in the web SDK. This can be useful for debugging.",
             "ignored_in_assistant": True,
             "system": True,
