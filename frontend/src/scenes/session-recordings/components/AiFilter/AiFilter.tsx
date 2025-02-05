e942e85451fc432804f1171e82f63dfbe39d88ae
Alex V <alexander.veryaysky@gmail.com>
2025-02-05 17:29:46 +0100

    Consent screens

--- frontend/src/scenes/session-recordings/components/AiFilter/AiFilter.tsx
+++ frontend/src/scenes/session-recordings/components/AiFilter/AiFilter.tsx
@@ -2,8 +2,11 @@
  * @fileoverview A component that allows user to "chat with recordings" using Max AI
  */
 import { IconAIText, IconPerson, IconTrash } from '@posthog/icons'
-import { LemonButton, LemonCollapse, LemonInput, LemonTag } from '@posthog/lemon-ui'
+import { LemonButton, LemonCollapse, LemonInput, LemonTag, Popover } from '@posthog/lemon-ui'
 import { BuiltLogic, useActions, useValues } from 'kea'
+import { HedgehogBuddy } from 'lib/components/HedgehogBuddy/HedgehogBuddy'
+import { hedgehogBuddyLogic } from 'lib/components/HedgehogBuddy/hedgehogBuddyLogic'
+import { maxGlobalLogic } from 'scenes/max/maxGlobalLogic'
 import { sessionRecordingsPlaylistLogicType } from 'scenes/session-recordings/playlist/sessionRecordingsPlaylistLogicType'
 
 import { aiFilterLogic } from './aiFilterLogic'
@@ -13,6 +16,9 @@ export function AiFilter({ logic }: { logic: BuiltLogic<sessionRecordingsPlaylis
     const filterLogic = aiFilterLogic({ setFilters, resetFilters })
     const { messages, input, isLoading } = useValues(filterLogic)
     const { setInput, handleReset, handleSend } = useActions(filterLogic)
+    const { acceptDataProcessing } = useActions(maxGlobalLogic)
+    const { dataProcessingAccepted } = useValues(maxGlobalLogic)
+    const { hedgehogConfig } = useValues(hedgehogBuddyLogic)
 
     return (
         <>
@@ -96,7 +102,9 @@ export function AiFilter({ logic }: { logic: BuiltLogic<sessionRecordingsPlaylis
                                             <div>
                                                 <LemonButton
                                                     onClick={handleSend}
-                                                    disabled={isLoading || input.length === 0}
+                                                    disabled={
+                                                        isLoading || input.length === 0 || !dataProcessingAccepted
+                                                    }
                                                     loading={isLoading}
                                                     type="primary"
                                                     size="small"
@@ -104,6 +112,63 @@ export function AiFilter({ logic }: { logic: BuiltLogic<sessionRecordingsPlaylis
                                                     {isLoading ? '' : 'Send'}
                                                 </LemonButton>
                                             </div>
+                                            {!dataProcessingAccepted && (
+                                                <div className="flex justify-center">
+                                                    <Popover
+                                                        overlay={
+                                                            <div className="m-1.5">
+                                                                <p className="font-medium text-pretty mb-1.5">
+                                                                    Hi! I use OpenAI services to analyze your data,
+                                                                    <br />
+                                                                    so that you can focus on building. This <em>
+                                                                        can
+                                                                    </em>{' '}
+                                                                    include
+                                                                    <br />
+                                                                    personal data of your users, if you're capturing it.
+                                                                    <br />
+                                                                    <em>
+                                                                        Your data won't be used for training models.
+                                                                    </em>
+                                                                </p>
+                                                                <LemonButton
+                                                                    type="secondary"
+                                                                    size="small"
+                                                                    onClick={() => acceptDataProcessing()}
+                                                                >
+                                                                    Got it, I accept OpenAI processing data
+                                                                </LemonButton>
+                                                            </div>
+                                                        }
+                                                        placement="right-end"
+                                                        showArrow
+                                                        visible={true}
+                                                    >
+                                                        <HedgehogBuddy
+                                                            static
+                                                            hedgehogConfig={{
+                                                                ...hedgehogConfig,
+                                                                walking_enabled: false,
+                                                                controls_enabled: false,
+                                                            }}
+                                                            onClick={(actor) => {
+                                                                if (Math.random() < 0.01) {
+                                                                    actor.setOnFire()
+                                                                } else {
+                                                                    actor.setRandomAnimation()
+                                                                }
+                                                            }}
+                                                            onActorLoaded={(actor) =>
+                                                                setTimeout(() => {
+                                                                    actor.setAnimation('wave')
+                                                                    // Always start out facing right so that the data processing popover is more readable
+                                                                    actor.direction = 'right'
+                                                                }, 100)
+                                                            }
+                                                        />
+                                                    </Popover>
+                                                </div>
+                                            )}
                                         </div>
                                     </div>
                                     {messages.length === 0 && (
