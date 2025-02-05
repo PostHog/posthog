fe0e54d8e24b20e829956962ae356f1e6292fb8a
Tom Owers <owerstom@gmail.com>
2025-02-05 13:31:27 +0100

    Abstract out PostHogWorkflow class

--- posthog/temporal/batch_exports/postgres_batch_export.py
+++ posthog/temporal/batch_exports/postgres_batch_export.py
@@ -21,7 +21,7 @@ from posthog.batch_exports.service import (
     BatchExportSchema,
     PostgresBatchExportInputs,
 )
-from posthog.temporal.batch_exports.base import PostHogWorkflow
+from posthog.temporal.common.base import PostHogWorkflow
 from posthog.temporal.batch_exports.batch_exports import (
     FinishBatchExportRunInputs,
     RecordsCompleted,
