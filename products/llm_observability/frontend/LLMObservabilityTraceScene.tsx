8be475e9fb8732de0d5793837c91d613b4b7cf68
Adam Leith <adamleithp@users.noreply.github.com>
2025-02-05 22:45:35 +0000

    fixed toolbar styles by importing a new base.scss file (so we don't ha ve to import fonts.scss), fixed trace scene hover and active state

--- products/llm_observability/frontend/LLMObservabilityTraceScene.tsx
+++ products/llm_observability/frontend/LLMObservabilityTraceScene.tsx
@@ -253,8 +253,8 @@ const TreeNode = React.memo(function TraceNode({
                     timestamp: removeMilliseconds(topLevelTrace.createdAt),
                 })}
                 className={classNames(
-                    'flex flex-col gap-1 p-1 text-xs rounded min-h-8 justify-center hover:bg-accent-primary-highlight',
-                    isSelected && 'bg-accent-primary-highlight'
+                    'flex flex-col gap-1 p-1 text-xs rounded min-h-8 justify-center hover:!bg-accent-primary-highlight',
+                    isSelected && '!bg-accent-primary-highlight'
                 )}
             >
                 <div className="flex flex-row items-center gap-1.5">
