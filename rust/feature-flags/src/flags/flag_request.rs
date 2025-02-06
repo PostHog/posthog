93554704daeaa460581a6e16c5f512ece345cb8e
dylan <dylan@posthog.com>
2025-02-05 16:15:36 -0800

    clean up logging for the geoip service a bit

--- rust/feature-flags/src/flags/flag_request.rs
+++ rust/feature-flags/src/flags/flag_request.rs
@@ -41,14 +41,14 @@ impl FlagRequest {
     #[instrument(skip_all)]
     pub fn from_bytes(bytes: Bytes) -> Result<FlagRequest, FlagError> {
         let payload = String::from_utf8(bytes.to_vec()).map_err(|e| {
-            tracing::error!("failed to decode body: {}", e);
+            tracing::debug!("failed to decode body: {}", e);
             FlagError::RequestDecodingError(String::from("invalid body encoding"))
         })?;
 
         match serde_json::from_str::<FlagRequest>(&payload) {
             Ok(request) => Ok(request),
             Err(e) => {
-                tracing::error!("failed to parse JSON: {}", e);
+                tracing::debug!("failed to parse JSON: {}", e);
                 Err(FlagError::RequestDecodingError(String::from(
                     "invalid JSON",
                 )))
