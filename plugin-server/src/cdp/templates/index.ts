2bf7db1ccd17e9a7adb5672c37fb5e3e0592d64d
meikel <meikel@posthog.com>
2025-02-06 15:56:00 +0100

    make it usable

--- plugin-server/src/cdp/templates/index.ts
+++ plugin-server/src/cdp/templates/index.ts
@@ -2,6 +2,7 @@ import { DESTINATION_PLUGINS, TRANSFORMATION_PLUGINS } from '../legacy-plugins'
 import { template as webhookTemplate } from './_destinations/webhook/webhook.template'
 import { template as defaultTransformationTemplate } from './_transformations/default/default.template'
 import { template as geoipTemplate } from './_transformations/geoip/geoip.template'
+import { template as removeNullPropertiesTemplate } from './_transformations/remove-null-properties/remove-null-properties.template'
 import { template as ipAnonymizationTemplate } from './_transformations/ip-anonymization/ip-anonymization.template'
 import { HogFunctionTemplate } from './types'
 
@@ -11,6 +12,7 @@ export const HOG_FUNCTION_TEMPLATES_TRANSFORMATIONS: HogFunctionTemplate[] = [
     defaultTransformationTemplate,
     geoipTemplate,
     ipAnonymizationTemplate,
+    removeNullPropertiesTemplate,
 ]
 
 export const HOG_FUNCTION_TEMPLATES_DESTINATIONS_DEPRECATED: HogFunctionTemplate[] = DESTINATION_PLUGINS.map(
