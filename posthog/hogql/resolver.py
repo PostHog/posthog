7f239f0d48eabb93ffb150c0182efc2f24d67fab
Sandy Spicer <sandy@posthog.com>
2025-02-05 17:14:34 -0800

    fix typo

--- posthog/hogql/resolver.py
+++ posthog/hogql/resolver.py
@@ -466,7 +466,7 @@ class Resolver(CloningVisitor):
         else:
             node.type = ast.UnknownType()
 
-        dataclasses.replace(node.type, nullable=left_type.nullable or right_type.nullable)
+        node.type.nullable = left_type.nullable or right_type.nullable
         return node
 
     def visit_call(self, node: ast.Call):
