6ca93927694f846f1eecfabe2b3d492a35849f56
Ben White <ben@posthog.com>
2025-02-06 15:50:24 +0100

    fix

--- plugin-server/src/utils/db/persons-db.ts
+++ plugin-server/src/utils/db/persons-db.ts
@@ -1,4 +1,3 @@
-import ClickHouse from '@posthog/clickhouse'
 import { Properties } from '@posthog/plugin-scaffold'
 import { Pool as GenericPool } from 'generic-pool'
 import Redis from 'ioredis'
@@ -9,9 +8,6 @@ import { Counter } from 'prom-client'
 import { KAFKA_PERSON_DISTINCT_ID } from '../../config/kafka-topics'
 import { KafkaProducerWrapper, TopicMessage } from '../../kafka/producer'
 import {
-    ClickHousePerson,
-    ClickHousePersonDistinctId2,
-    Database,
     InternalPerson,
     PersonDistinctId,
     PropertiesLastOperation,
@@ -20,7 +16,7 @@ import {
     Team,
 } from '../../types'
 import { status } from '../status'
-import { escapeClickHouseString, NoRowsUpdatedError, RaceConditionError, sanitizeSqlIdentifier } from '../utils'
+import { NoRowsUpdatedError, RaceConditionError, sanitizeSqlIdentifier } from '../utils'
 import { PostgresRouter, PostgresUse, TransactionClient } from './postgres'
 import { generateKafkaPersonUpdateMessage, sanitizeJsonbValue, unparsePersonPartial } from './utils'
 
@@ -55,39 +51,39 @@ export class PersonsDB {
         }
     }
 
-    public async fetchPersons(database?: Database.Postgres): Promise<InternalPerson[]>
-    public async fetchPersons(database: Database.ClickHouse): Promise<ClickHousePerson[]>
-    public async fetchPersons(database: Database = Database.Postgres): Promise<InternalPerson[] | ClickHousePerson[]> {
-        if (database === Database.ClickHouse) {
-            const query = `
-            SELECT id, team_id, is_identified, ts as _timestamp, properties, created_at, is_del as is_deleted, _offset
-            FROM (
-                SELECT id,
-                    team_id,
-                    max(is_identified) as is_identified,
-                    max(_timestamp) as ts,
-                    argMax(properties, _timestamp) as properties,
-                    argMin(created_at, _timestamp) as created_at,
-                    max(is_deleted) as is_del,
-                    argMax(_offset, _timestamp) as _offset
-                FROM person
-                FINAL
-                GROUP BY team_id, id
-                HAVING max(is_deleted)=0
-            )
-            `
-            return (await this.clickhouseQuery(query)).data.map((row) => {
-                const { 'person_max._timestamp': _discard1, 'person_max.id': _discard2, ...rest } = row
-                return rest
-            }) as ClickHousePerson[]
-        } else if (database === Database.Postgres) {
-            return await this.postgres
-                .query<RawPerson>(PostgresUse.COMMON_WRITE, 'SELECT * FROM posthog_person', undefined, 'fetchPersons')
-                .then(({ rows }) => rows.map(this.toPerson))
-        } else {
-            throw new Error(`Can't fetch persons for database: ${database}`)
-        }
-    }
+    // public async fetchPersons(database?: Database.Postgres): Promise<InternalPerson[]>
+    // public async fetchPersons(database: Database.ClickHouse): Promise<ClickHousePerson[]>
+    // public async fetchPersons(database: Database = Database.Postgres): Promise<InternalPerson[] | ClickHousePerson[]> {
+    //     if (database === Database.ClickHouse) {
+    //         const query = `
+    //         SELECT id, team_id, is_identified, ts as _timestamp, properties, created_at, is_del as is_deleted, _offset
+    //         FROM (
+    //             SELECT id,
+    //                 team_id,
+    //                 max(is_identified) as is_identified,
+    //                 max(_timestamp) as ts,
+    //                 argMax(properties, _timestamp) as properties,
+    //                 argMin(created_at, _timestamp) as created_at,
+    //                 max(is_deleted) as is_del,
+    //                 argMax(_offset, _timestamp) as _offset
+    //             FROM person
+    //             FINAL
+    //             GROUP BY team_id, id
+    //             HAVING max(is_deleted)=0
+    //         )
+    //         `
+    //         return (await this.clickhouseQuery(query)).data.map((row) => {
+    //             const { 'person_max._timestamp': _discard1, 'person_max.id': _discard2, ...rest } = row
+    //             return rest
+    //         }) as ClickHousePerson[]
+    //     } else if (database === Database.Postgres) {
+    //         return await this.postgres
+    //             .query<RawPerson>(PostgresUse.COMMON_WRITE, 'SELECT * FROM posthog_person', undefined, 'fetchPersons')
+    //             .then(({ rows }) => rows.map(this.toPerson))
+    //     } else {
+    //         throw new Error(`Can't fetch persons for database: ${database}`)
+    //     }
+    // }
 
     public async fetchPerson(
         teamId: number,
@@ -307,49 +303,49 @@ export class PersonsDB {
 
     // PersonDistinctId
     // testutil
-    public async fetchDistinctIds(person: InternalPerson, database?: Database.Postgres): Promise<PersonDistinctId[]>
-    public async fetchDistinctIds(
-        person: InternalPerson,
-        database: Database.ClickHouse
-    ): Promise<ClickHousePersonDistinctId2[]>
-    public async fetchDistinctIds(
-        person: InternalPerson,
-        database: Database = Database.Postgres
-    ): Promise<PersonDistinctId[] | ClickHousePersonDistinctId2[]> {
-        if (database === Database.ClickHouse) {
-            return (
-                await this.clickhouseQuery(
-                    `
-                        SELECT *
-                        FROM person_distinct_id2
-                        FINAL
-                        WHERE person_id='${escapeClickHouseString(person.uuid)}'
-                          AND team_id='${person.team_id}'
-                          AND is_deleted=0
-                        ORDER BY _offset`
-                )
-            ).data as ClickHousePersonDistinctId2[]
-        } else if (database === Database.Postgres) {
-            const result = await this.postgres.query(
-                PostgresUse.COMMON_WRITE, // used in tests only
-                'SELECT * FROM posthog_persondistinctid WHERE person_id=$1 AND team_id=$2 ORDER BY id',
-                [person.id, person.team_id],
-                'fetchDistinctIds'
-            )
-            return result.rows as PersonDistinctId[]
-        } else {
-            throw new Error(`Can't fetch persons for database: ${database}`)
-        }
-    }
+    // public async fetchDistinctIds(person: InternalPerson, database?: Database.Postgres): Promise<PersonDistinctId[]>
+    // public async fetchDistinctIds(
+    //     person: InternalPerson,
+    //     database: Database.ClickHouse
+    // ): Promise<ClickHousePersonDistinctId2[]>
+    // public async fetchDistinctIds(
+    //     person: InternalPerson,
+    //     database: Database = Database.Postgres
+    // ): Promise<PersonDistinctId[] | ClickHousePersonDistinctId2[]> {
+    //     if (database === Database.ClickHouse) {
+    //         return (
+    //             await this.clickhouseQuery(
+    //                 `
+    //                     SELECT *
+    //                     FROM person_distinct_id2
+    //                     FINAL
+    //                     WHERE person_id='${escapeClickHouseString(person.uuid)}'
+    //                       AND team_id='${person.team_id}'
+    //                       AND is_deleted=0
+    //                     ORDER BY _offset`
+    //             )
+    //         ).data as ClickHousePersonDistinctId2[]
+    //     } else if (database === Database.Postgres) {
+    //         const result = await this.postgres.query(
+    //             PostgresUse.COMMON_WRITE, // used in tests only
+    //             'SELECT * FROM posthog_persondistinctid WHERE person_id=$1 AND team_id=$2 ORDER BY id',
+    //             [person.id, person.team_id],
+    //             'fetchDistinctIds'
+    //         )
+    //         return result.rows as PersonDistinctId[]
+    //     } else {
+    //         throw new Error(`Can't fetch persons for database: ${database}`)
+    //     }
+    // }
 
     // Only used in tests
-    public async fetchDistinctIdValues(
-        person: InternalPerson,
-        database: Database = Database.Postgres
-    ): Promise<string[]> {
-        const personDistinctIds = await this.fetchDistinctIds(person, database as any)
-        return personDistinctIds.map((pdi) => pdi.distinct_id)
-    }
+    // public async fetchDistinctIdValues(
+    //     person: InternalPerson,
+    //     database: Database = Database.Postgres
+    // ): Promise<string[]> {
+    //     const personDistinctIds = await this.fetchDistinctIds(person, database as any)
+    //     return personDistinctIds.map((pdi) => pdi.distinct_id)
+    // }
 
     public async addPersonlessDistinctId(teamId: number, distinctId: string): Promise<boolean> {
         const result = await this.postgres.query(
