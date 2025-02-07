20b96ed1366035d359442eb204b74af35a2b71f3
Paweł Ledwoń <me@difficult.name>
2025-02-07 11:53:04 +0100

    feat: add distributed events v2 to coordinator

--- posthog/clickhouse/migrations/0097_session_replay_events_v2_test.py
+++ posthog/clickhouse/migrations/0097_session_replay_events_v2_test.py
@@ -1,4 +1,4 @@
-from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
+from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions, NodeRole
 from posthog.session_recordings.sql.session_replay_event_v2_test_sql import (
     SESSION_REPLAY_EVENTS_V2_TEST_KAFKA_TABLE_SQL,
     SESSION_REPLAY_EVENTS_V2_TEST_MV_SQL,
@@ -7,11 +7,13 @@ from posthog.session_recordings.sql.session_replay_event_v2_test_sql import (
     SESSION_REPLAY_EVENTS_V2_TEST_DISTRIBUTED_TABLE_SQL,
 )
 
-
 operations = [
     run_sql_with_exceptions(SESSION_REPLAY_EVENTS_V2_TEST_WRITABLE_TABLE_SQL()),
     run_sql_with_exceptions(SESSION_REPLAY_EVENTS_V2_TEST_DISTRIBUTED_TABLE_SQL()),
     run_sql_with_exceptions(SESSION_REPLAY_EVENTS_V2_TEST_DATA_TABLE_SQL()),
     run_sql_with_exceptions(SESSION_REPLAY_EVENTS_V2_TEST_KAFKA_TABLE_SQL()),
     run_sql_with_exceptions(SESSION_REPLAY_EVENTS_V2_TEST_MV_SQL()),
+    run_sql_with_exceptions(
+        SESSION_REPLAY_EVENTS_V2_TEST_DISTRIBUTED_TABLE_SQL(on_cluster=False), node_role=NodeRole.COORDINATOR
+    ),
 ]
