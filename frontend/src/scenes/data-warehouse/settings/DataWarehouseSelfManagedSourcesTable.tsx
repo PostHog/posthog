2567d3bfd5f7d0d1e1d25c298538417397613256
Peter Hicks <peter.h@posthog.com>
2025-02-07 13:45:33 -0800

    Handle deletes w/ cross kea action.

--- frontend/src/scenes/data-warehouse/settings/DataWarehouseSelfManagedSourcesTable.tsx
+++ frontend/src/scenes/data-warehouse/settings/DataWarehouseSelfManagedSourcesTable.tsx
@@ -1,6 +1,7 @@
-import { LemonButton, LemonDialog, LemonTable } from '@posthog/lemon-ui'
+import { LemonButton, LemonDialog, LemonTable, Tooltip } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
 import { TZLabel } from 'lib/components/TZLabel'
+import { More } from 'lib/lemon-ui/LemonButton/More'
 import { LemonTableLink } from 'lib/lemon-ui/LemonTable/LemonTableLink'
 import { dataWarehouseTableLogic } from 'scenes/data-warehouse/new/dataWarehouseTableLogic'
 import { DataWarehouseSourceIcon, mapUrlToProvider } from 'scenes/data-warehouse/settings/DataWarehouseSourceIcon'
@@ -52,40 +53,51 @@ export function DataWarehouseSelfManagedSourcesTable(): JSX.Element {
                 },
                 {
                     key: 'actions',
+                    width: 0,
                     render: (_, item) => (
                         <div className="flex flex-row justify-end">
-                            <LemonButton
-                                data-attr={`refresh-data-warehouse-${item.name}`}
-                                key={`refresh-data-warehouse-${item.name}`}
-                                onClick={() => refreshSelfManagedTableSchema(item.id)}
-                            >
-                                Update schema from source
-                            </LemonButton>
-                            <LemonButton
-                                status="danger"
-                                data-attr={`delete-data-warehouse-${item.name}`}
-                                key={`delete-data-warehouse-${item.name}`}
-                                onClick={() => {
-                                    LemonDialog.open({
-                                        title: 'Delete table?',
-                                        description:
-                                            'Table deletion cannot be undone. All views and joins related to this table will be deleted.',
+                            <div>
+                                <More
+                                    overlay={
+                                        <>
+                                            <Tooltip title="Update schema from source">
+                                                <LemonButton
+                                                    data-attr={`refresh-data-warehouse-${item.name}`}
+                                                    key={`refresh-data-warehouse-${item.name}`}
+                                                    onClick={() => refreshSelfManagedTableSchema(item.id)}
+                                                >
+                                                    Reload
+                                                </LemonButton>
+                                            </Tooltip>
+                                            <LemonButton
+                                                status="danger"
+                                                data-attr={`delete-data-warehouse-${item.name}`}
+                                                key={`delete-data-warehouse-${item.name}`}
+                                                onClick={() => {
+                                                    LemonDialog.open({
+                                                        title: 'Delete table?',
+                                                        description:
+                                                            'Table deletion cannot be undone. All views and joins related to this table will be deleted.',
 
-                                        primaryButton: {
-                                            children: 'Delete',
-                                            status: 'danger',
-                                            onClick: () => {
-                                                deleteSelfManagedTable(item.id)
-                                            },
-                                        },
-                                        secondaryButton: {
-                                            children: 'Cancel',
-                                        },
-                                    })
-                                }}
-                            >
-                                Delete
-                            </LemonButton>
+                                                        primaryButton: {
+                                                            children: 'Delete',
+                                                            status: 'danger',
+                                                            onClick: () => {
+                                                                deleteSelfManagedTable(item.id)
+                                                            },
+                                                        },
+                                                        secondaryButton: {
+                                                            children: 'Cancel',
+                                                        },
+                                                    })
+                                                }}
+                                            >
+                                                Delete
+                                            </LemonButton>
+                                        </>
+                                    }
+                                />
+                            </div>
                         </div>
                     ),
                 },
