a41efc1156c74d10f9fbd97253438c340f504ffa
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2025-02-06 18:20:28 -0800

    perf: Improve memory usage of WAU/MAU queries
    Perform intermediate aggregations by actor_id instead of timestamp, since it is much more memory efficient to store timestamp types repeatedly compared to larger UUID values

--- posthog/hogql_queries/insights/trends/aggregation_operations.py
+++ posthog/hogql_queries/insights/trends/aggregation_operations.py
@@ -286,20 +286,27 @@ class AggregationOperations(DataWarehouseInsightQueryMixin):
             parse_select(
                 """
                 SELECT
-                    d.timestamp,
-                    COUNT(DISTINCT actor_id) AS counts
-                FROM {cross_join_select_query} e
-                CROSS JOIN (
+                    timestamp,
+                    count() AS counts
+                FROM (
                     SELECT
-                        {date_to_start_of_interval} - {number_interval_period} AS timestamp
-                    FROM
-                        numbers(dateDiff({interval}, {date_from_start_of_interval} - {inclusive_lookback}, {date_to}))
-                ) d
-                WHERE
-                    e.timestamp <= d.timestamp + INTERVAL 1 DAY AND
-                    e.timestamp > d.timestamp - {exclusive_lookback}
-                GROUP BY d.timestamp
-                ORDER BY d.timestamp
+                        e.actor_id,
+                        groupUniqArray(d.timestamp) AS timestamps
+                    FROM {cross_join_select_query} e
+                    CROSS JOIN (
+                        SELECT
+                            {date_to_start_of_interval} - {number_interval_period} AS timestamp
+                        FROM
+                            numbers(dateDiff({interval}, {date_from_start_of_interval} - {inclusive_lookback}, {date_to}))
+                    ) d
+                    WHERE
+                        e.timestamp <= d.timestamp + INTERVAL 1 DAY AND
+                        e.timestamp > d.timestamp - {exclusive_lookback}
+                    GROUP BY e.actor_id
+                ) et
+                ARRAY JOIN et.timestamps AS timestamp
+                GROUP BY timestamp
+                ORDER BY timestamp
             """,
                 placeholders={
                     **self.query_date_range.to_placeholders(),
@@ -309,6 +316,7 @@ class AggregationOperations(DataWarehouseInsightQueryMixin):
             ),
         )
 
+        # TODO: will need to fix this
         if self.is_total_value:
             query.select = [ast.Field(chain=["d", "timestamp"]), ast.Field(chain=["actor_id"])]
             query.group_by.append(ast.Field(chain=["actor_id"]))
