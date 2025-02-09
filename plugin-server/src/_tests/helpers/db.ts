1d43fd0bced0860d82c89f7dd521d175bb8c2122
Ben White <ben@posthog.com>
2025-02-09 20:12:29 +0100

    Fixes

--- plugin-server/src/_tests/helpers/db.ts
+++ plugin-server/src/_tests/helpers/db.ts
@@ -1,22 +1,63 @@
+import { Properties } from '@posthog/plugin-scaffold'
 import { DateTime } from 'luxon'
 
 import { toPerson } from '../../ingestion/event-pipeline-runner/utils/persons-db'
 import {
+    ClickHouseEvent,
+    ClickhouseGroup,
     ClickHousePerson,
     ClickHousePersonDistinctId2,
     Database,
+    DeadLetterQueueEvent,
     Group,
     GroupTypeIndex,
     Hub,
     InternalPerson,
     PersonDistinctId,
+    PropertiesLastOperation,
+    PropertiesLastUpdatedAt,
+    RawClickHouseEvent,
     RawGroup,
     RawPerson,
     TeamId,
 } from '../../types'
 import { PostgresUse, TransactionClient } from '../../utils/postgres'
-import { escapeClickHouseString } from '../../utils/utils'
+import { clickHouseTimestampToDateTime, escapeClickHouseString, RaceConditionError } from '../../utils/utils'
 import { clickhouseQuery } from './clickhouse'
+
+export function parseRawClickHouseEvent(rawEvent: RawClickHouseEvent): ClickHouseEvent {
+    return {
+        ...rawEvent,
+        timestamp: clickHouseTimestampToDateTime(rawEvent.timestamp),
+        created_at: clickHouseTimestampToDateTime(rawEvent.created_at),
+        properties: rawEvent.properties ? JSON.parse(rawEvent.properties) : {},
+        elements_chain: rawEvent.elements_chain ? (rawEvent.elements_chain as any) : null,
+        person_created_at: rawEvent.person_created_at
+            ? clickHouseTimestampToDateTime(rawEvent.person_created_at)
+            : null,
+        person_properties: rawEvent.person_properties ? JSON.parse(rawEvent.person_properties) : {},
+        group0_properties: rawEvent.group0_properties ? JSON.parse(rawEvent.group0_properties) : {},
+        group1_properties: rawEvent.group1_properties ? JSON.parse(rawEvent.group1_properties) : {},
+        group2_properties: rawEvent.group2_properties ? JSON.parse(rawEvent.group2_properties) : {},
+        group3_properties: rawEvent.group3_properties ? JSON.parse(rawEvent.group3_properties) : {},
+        group4_properties: rawEvent.group4_properties ? JSON.parse(rawEvent.group4_properties) : {},
+        group0_created_at: rawEvent.group0_created_at
+            ? clickHouseTimestampToDateTime(rawEvent.group0_created_at)
+            : null,
+        group1_created_at: rawEvent.group1_created_at
+            ? clickHouseTimestampToDateTime(rawEvent.group1_created_at)
+            : null,
+        group2_created_at: rawEvent.group2_created_at
+            ? clickHouseTimestampToDateTime(rawEvent.group2_created_at)
+            : null,
+        group3_created_at: rawEvent.group3_created_at
+            ? clickHouseTimestampToDateTime(rawEvent.group3_created_at)
+            : null,
+        group4_created_at: rawEvent.group4_created_at
+            ? clickHouseTimestampToDateTime(rawEvent.group4_created_at)
+            : null,
+    }
+}
 
 export class DBHelpers {
     constructor(private hub: Hub) {}
@@ -125,6 +166,64 @@ export class DBHelpers {
                 created_at: DateTime.fromISO(rawGroup.created_at).toUTC(),
                 version: Number(rawGroup.version || 0),
             }
+        }
+    }
+
+    // Event (NOTE: not a Django model, stored in ClickHouse table `events`)
+
+    public async fetchEvents(): Promise<ClickHouseEvent[]> {
+        const queryResult = await clickhouseQuery<RawClickHouseEvent>(`SELECT * FROM events ORDER BY timestamp ASC`)
+        return queryResult.data.map(parseRawClickHouseEvent)
+    }
+
+    public async fetchDeadLetterQueueEvents(): Promise<DeadLetterQueueEvent[]> {
+        const result = await clickhouseQuery(`SELECT * FROM events_dead_letter_queue ORDER BY _timestamp ASC`)
+        const events = result.data as DeadLetterQueueEvent[]
+        return events
+    }
+
+    // Used in tests
+    public async fetchClickhouseGroups(): Promise<ClickhouseGroup[]> {
+        const query = `
+        SELECT group_type_index, group_key, created_at, team_id, group_properties FROM groups FINAL
+        `
+        return (await clickhouseQuery(query)).data as ClickhouseGroup[]
+    }
+
+    public async insertGroup(
+        teamId: TeamId,
+        groupTypeIndex: GroupTypeIndex,
+        groupKey: string,
+        groupProperties: Properties,
+        createdAt: DateTime,
+        propertiesLastUpdatedAt: PropertiesLastUpdatedAt,
+        propertiesLastOperation: PropertiesLastOperation,
+        version: number,
+        tx?: TransactionClient
+    ): Promise<void> {
+        const result = await this.hub.postgres.query(
+            tx ?? PostgresUse.COMMON_WRITE,
+            `
+            INSERT INTO posthog_group (team_id, group_key, group_type_index, group_properties, created_at, properties_last_updated_at, properties_last_operation, version)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            ON CONFLICT (team_id, group_key, group_type_index) DO NOTHING
+            RETURNING version
+            `,
+            [
+                teamId,
+                groupKey,
+                groupTypeIndex,
+                JSON.stringify(groupProperties),
+                createdAt.toISO(),
+                JSON.stringify(propertiesLastUpdatedAt),
+                JSON.stringify(propertiesLastOperation),
+                version,
+            ],
+            'upsertGroup'
+        )
+
+        if (result.rows.length === 0) {
+            throw new RaceConditionError('Parallel posthog_group inserts, retry')
         }
     }
 }
