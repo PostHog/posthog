92090bab8bef08c1ed42930f4ea92426e48fb0fd
Paul D'Ambra <paul@posthog.com>
2025-02-10 09:49:15 +0000

    fix: a little typo

--- frontend/src/scenes/session-recordings/components/AiFilter/AiFilter.tsx
+++ frontend/src/scenes/session-recordings/components/AiFilter/AiFilter.tsx
@@ -133,11 +133,11 @@ export function AiFilter({ logic }: { logic: BuiltLogic<sessionRecordingsPlaylis
                                                 className="mb-1"
                                                 type="secondary"
                                                 onClick={() =>
-                                                    setInput('Show me recordings of people who are frustarated')
+                                                    setInput('Show me recordings of people who are frustrated')
                                                 }
                                             >
                                                 <span className="font-normal text-sx italic">
-                                                    Show me recordings of people who are frustarated
+                                                    Show me recordings of people who are frustrated
                                                 </span>
                                             </LemonButton>
                                             <LemonButton
