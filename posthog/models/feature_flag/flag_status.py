8a00b07165a865ba0389cce0063311e86b094541
Haven Barnes <haven@posthog.com>
2025-02-10 11:57:39 -0800

    tweak

--- posthog/models/feature_flag/flag_status.py
+++ posthog/models/feature_flag/flag_status.py
@@ -43,13 +43,13 @@ class FeatureFlagStatusChecker:
         if not self.feature_flag_id and not self.feature_flag:
             return FeatureFlagStatus.UNKNOWN, "Must provide feature flag or feature flag id"
 
-        flag = self.feature_flag
+        try:
+            flag = FeatureFlag.objects.get(pk=self.feature_flag_id) if self.feature_flag_id else self.feature_flag
+        except FeatureFlag.DoesNotExist:
+            return FeatureFlagStatus.UNKNOWN, "Flag could not be found"
 
         if not flag:
-            try:
-                flag = FeatureFlag.objects.get(pk=self.feature_flag_id)
-            except FeatureFlag.DoesNotExist:
-                return FeatureFlagStatus.UNKNOWN, "Flag could not be found"
+            return FeatureFlagStatus.UNKNOWN, "Flag could not be loaded"
 
         if flag.deleted:
             return FeatureFlagStatus.DELETED, "Flag has been deleted"
