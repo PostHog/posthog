f09f6ef98d0beeaedaa28df795e19a5c353da3d3
Lucas Faria <lucasheriques@gmail.com>
2025-02-10 17:47:45 -0300

    not intended change

--- frontend/src/scenes/surveys/SurveyCustomization.tsx
+++ frontend/src/scenes/surveys/SurveyCustomization.tsx
@@ -286,7 +286,7 @@ export function Customization({
                                         })
                                     }
                                 }}
-                                className="w-1 ignore-error-border"
+                                className="w-12 ignore-error-border"
                             />{' '}
                             seconds.
                         </div>
