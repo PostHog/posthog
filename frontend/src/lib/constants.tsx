d3d665b4690047c4a44f79f659eae1e18789cdc7
Alex V <alexander.veryaysky@gmail.com>
2025-02-12 14:36:09 +0100

    fix:move regex to a separate feature flag

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -244,6 +244,7 @@ export const FEATURE_FLAGS = {
     ONBOARDING_NEW_PLANS_STEP: 'onboarding-new-plans-step', // owner: @joshsny #team-growth
     EXPERIMENTAL_DASHBOARD_ITEM_RENDERING: 'experimental-dashboard-item-rendering', // owner: @thmsobrmlr #team-product-analytics
     RECORDINGS_AI_FILTER: 'recordings-ai-filter', // owner: @veryayskiy #team-replay
+    RECORDINGS_AI_REGEX: 'recordings-ai-regex', // owner: @veryayskiy #team-replay
     PATH_CLEANING_AI_REGEX: 'path-cleaning-ai-regex', // owner: @rafaeelaudibert #team-web-analytics
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
