930920bfd9e5dabbd8da1525ec07a57a9c8cd856
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-02-11 22:48:34 -0300

    feat: Collect host definitions from the `propdefs` repo
    Web Analytics wants to, eventually, allow you to select which domain (host) you want to see your data for. People can already filter by it using the `host` field, but we don't have an easy way to display a dropdown of all available options.
    
    We ask people to list their hosts to be able to access the Toolbar, but that could be 100% automated if we paid attention to the list of domains we're getting emails from.
    
    Hence this commit: let's start parsing all of the hosts we see when parsing the definitions and create an entry in a brand new `hostdefinition` table. We aren't using the table yet, but we'll do so eventually.
    
    I need to check with the infra team if we'll be able to keep up with the extra processing. It doesn't feel that heavy by looking at it, but I don't have much information on how close to the tipping of our service we usually are.

--- rust/property-defs-rs/tests/test_migrations/20240830124836_setup_propdefs_tables.sql
+++ rust/property-defs-rs/tests/test_migrations/20240830124836_setup_propdefs_tables.sql
@@ -1,7 +1,5 @@
--- These mimic the posthog main-db property, event and event-property tables, and are only used
+-- These mimic the posthog main-db property, event, hostdefinition, and event-property tables, and are only used
 -- for testing (so we can use `sqlx::test`)
-
--- Create a unique contraint on posthog_eventdefinition for team_id and name, matching the django one
 
 CREATE TABLE IF NOT EXISTS posthog_eventdefinition (
     id UUID PRIMARY KEY,
@@ -15,7 +13,7 @@ CREATE TABLE IF NOT EXISTS posthog_eventdefinition (
     CONSTRAINT posthog_eventdefinition_team_id_name_80fa0b87_uniq UNIQUE (team_id, name)
 );
 
-CREATE UNIQUE INDEX event_definition_proj_uniq ON posthog_eventdefinition (coalesce(project_id, team_id), name);
+CREATE UNIQUE INDEX IF NOT EXISTS event_definition_proj_uniq ON posthog_eventdefinition (coalesce(project_id, team_id), name);
 
 CREATE TABLE IF NOT EXISTS posthog_propertydefinition (
     id UUID PRIMARY KEY,
@@ -31,8 +29,8 @@ CREATE TABLE IF NOT EXISTS posthog_propertydefinition (
     type SMALLINT NOT NULL DEFAULT 1
 );
 
-CREATE UNIQUE INDEX posthog_propertydefinition_uniq ON posthog_propertydefinition (team_id, name, type, coalesce(group_type_index, -1));
-CREATE UNIQUE INDEX posthog_propdef_proj_uniq ON posthog_propertydefinition (coalesce(project_id, team_id), name, type, coalesce(group_type_index, -1));
+CREATE UNIQUE INDEX IF NOT EXISTS posthog_propertydefinition_uniq ON posthog_propertydefinition (team_id, name, type, coalesce(group_type_index, -1));
+CREATE UNIQUE INDEX IF NOT EXISTS posthog_propdef_proj_uniq ON posthog_propertydefinition (coalesce(project_id, team_id), name, type, coalesce(group_type_index, -1));
 
 
 CREATE TABLE IF NOT EXISTS posthog_eventproperty (
@@ -43,5 +41,35 @@ CREATE TABLE IF NOT EXISTS posthog_eventproperty (
     project_id BIGINT NULL
 );
 
-CREATE UNIQUE INDEX posthog_event_property_unique_team_event_property ON posthog_eventproperty (team_id, event, property);
-CREATE UNIQUE INDEX posthog_event_property_unique_proj_event_property ON posthog_eventproperty (coalesce(project_id, team_id), event, property);
+CREATE UNIQUE INDEX IF NOT EXISTS posthog_event_property_unique_team_event_property ON posthog_eventproperty (team_id, event, property);
+CREATE UNIQUE INDEX IF NOT EXISTS posthog_event_property_unique_proj_event_property ON posthog_eventproperty (coalesce(project_id, team_id), event, property);
+
+CREATE TABLE IF NOT EXISTS posthog_hostdefinition (
+    id UUID PRIMARY KEY,
+    host VARCHAR(400) NOT NULL,
+    team_id INTEGER NOT NULL,
+    project_id BIGINT NULL,
+    last_seen_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS posthog_hostdefinition_team_project_host_idx ON posthog_hostdefinition (team_id, project_id, host);
+CREATE UNIQUE INDEX IF NOT EXISTS posthog_hostdefinition_proj_uniq ON posthog_hostdefinition (coalesce(project_id, team_id), host);
+
+CREATE TABLE IF NOT EXISTS posthog_grouptypemapping (
+    id UUID PRIMARY KEY,
+    group_type VARCHAR(400) NOT NULL,
+    group_type_index INTEGER NOT NULL,
+    team_id INTEGER NOT NULL,
+    project_id BIGINT NULL,
+    name_plural VARCHAR(400) NULL,
+    name_singular VARCHAR(400) NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS posthog_grouptypemapping_pkey ON posthog_grouptypemapping (id);
+CREATE INDEX IF NOT EXISTS posthog_grouptypemapping_team_id_5fb54d04 ON posthog_grouptypemapping USING btree (team_id);
+CREATE INDEX IF NOT EXISTS posthog_grouptypemapping_project_id_239c0515 ON posthog_grouptypemapping USING btree (project_id);
+CREATE INDEX IF NOT EXISTS posthog_group_type_proj_idx ON posthog_grouptypemapping USING btree (project_id, group_type);
+CREATE UNIQUE INDEX IF NOT EXISTS "unique group types for project" ON posthog_grouptypemapping USING btree (project_id, group_type);
+CREATE INDEX IF NOT EXISTS posthog_group_type_i_proj_idx ON posthog_grouptypemapping USING btree (project_id, group_type_index);
+CREATE UNIQUE INDEX IF NOT EXISTS "unique event column indexes for project" ON posthog_grouptypemapping USING btree (project_id, group_type_index);
