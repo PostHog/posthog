44aa9292f6b050aceb7be934c1e4ff38aac2ea5c
eric <eeoneric@gmail.com>
2025-02-12 10:44:15 -0700

    fix component

--- frontend/src/queries/nodes/DataVisualization/Components/Variables/VariableCalendar.tsx
+++ frontend/src/queries/nodes/DataVisualization/Components/Variables/VariableCalendar.tsx
@@ -13,9 +13,15 @@ export const VariableCalendar = ({
     updateVariable: (date: string) => void
     showDefault?: boolean
 }): JSX.Element => {
-    const [calendarTime, setCalendarTime] = useState(
-        variable.default_value ? dayjs(variable.default_value).format('HH:mm:00') !== '' : false
-    )
+    const [calendarTime, setCalendarTime] = useState(() => {
+        const dateToCheck = showDefault || !variable.value ? variable.default_value : variable.value
+        if (!dateToCheck) {
+            return false
+        }
+        // Check if the date string contains time information (HH:mm or HH:mm:ss)
+        return /\d{2}:\d{2}(:\d{2})?/.test(dateToCheck)
+    })
+
     const [date, setDate] = useState(showDefault ? dayjs(variable.default_value) : dayjs(variable.value))
 
     return (
@@ -24,7 +30,7 @@ export const VariableCalendar = ({
             onChange={(date) => {
                 setDate(date)
                 updateVariable(
-                    calendarTime ? date?.format('YYYY-MM-DD HH:mm:00') ?? '' : date?.format('YYYY-MM-DD 00:00:00') ?? ''
+                    calendarTime ? date?.format('YYYY-MM-DD HH:mm:00') ?? '' : date?.format('YYYY-MM-DD') ?? ''
                 )
             }}
             showTimeToggle={true}
