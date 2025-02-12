e25dbfda35b4768793a399a6181555289e8c0f45
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-02-12 17:34:26 -0300

    fix: Fix migrations after rebase

--- posthog/migrations/0668_hostdefinition_and_more.py
+++ posthog/migrations/0668_hostdefinition_and_more.py
@@ -8,7 +8,7 @@ import posthog.models.utils
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0661_errortrackingissuefingerprintv2_first_seen"),
+        ("posthog", "0667_encrypt_feature_flag_config"),
     ]
 
     operations = [
