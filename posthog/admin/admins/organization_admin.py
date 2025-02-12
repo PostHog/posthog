779ec740351e0271f59e1740787b8c1453ee780d
Zach Waterfield <zlwaterfield@gmail.com>
2025-02-12 15:21:11 -0500

    Update organization_admin.py

--- posthog/admin/admins/organization_admin.py
+++ posthog/admin/admins/organization_admin.py
@@ -151,5 +151,7 @@ class OrganizationAdmin(admin.ModelAdmin):
 
     def change_view(self, request, object_id, form_url="", extra_context=None):
         extra_context = extra_context or {}
-        extra_context["show_rbac_migration_buttons"] = True
+        if not request.user.groups.filter(name="Billing Team").exists():
+            extra_context["show_rbac_migration_buttons"] = True
+
         return super().change_view(request, object_id, form_url, extra_context=extra_context)
