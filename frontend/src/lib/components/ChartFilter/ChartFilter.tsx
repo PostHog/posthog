0fbc8ac055fdb05a2a90794d4069d97f392ed53c
Adam Leith <adamleithp@users.noreply.github.com>
2025-02-12 10:38:27 +0000

    update chart filter visual select with some descriptions, updated lemon button small gap value from .25rem => .5rem for more breathing room

--- frontend/src/lib/components/ChartFilter/ChartFilter.tsx
+++ frontend/src/lib/components/ChartFilter/ChartFilter.tsx
@@ -27,16 +27,34 @@ export function ChartFilter(): JSX.Element {
                     value: ChartDisplayType.ActionsLineGraph,
                     icon: <IconTrends />,
                     label: 'Line chart',
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Line chart</span>
+                            <span className="text-xs text-tertiary">Trends over time with a continuous line.</span>
+                        </div>
+                    ),
                 },
                 {
                     value: ChartDisplayType.ActionsBar,
                     icon: <IconGraph />,
                     label: 'Bar chart',
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Bar chart</span>
+                            <span className="text-xs text-tertiary">Time-based data with vertical bars.</span>
+                        </div>
+                    ),
                 },
                 {
                     value: ChartDisplayType.ActionsAreaGraph,
                     icon: <IconAreaChart />,
                     label: 'Area chart',
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Area chart</span>
+                            <span className="text-xs text-tertiary">Trends over time with a shaded area.</span>
+                        </div>
+                    ),
                 },
             ],
         },
@@ -47,7 +65,12 @@ export function ChartFilter(): JSX.Element {
                     value: ChartDisplayType.ActionsLineGraphCumulative,
                     icon: <IconCumulativeChart />,
                     label: 'Line chart (cumulative)',
-                    labelInMenu: 'Line chart',
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Line chart (cumulative)</span>
+                            <span className="text-xs text-tertiary">Accumulating values over time.</span>
+                        </div>
+                    ),
                     disabledReason: trendsOnlyDisabledReason,
                 },
             ],
@@ -59,6 +82,12 @@ export function ChartFilter(): JSX.Element {
                     value: ChartDisplayType.BoldNumber,
                     icon: <Icon123 />,
                     label: 'Number',
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Number</span>
+                            <span className="text-xs text-tertiary">A big number showing the total value..</span>
+                        </div>
+                    ),
                     disabledReason: trendsOnlyDisabledReason || singleSeriesOnlyDisabledReason,
                 },
                 {
@@ -66,17 +95,35 @@ export function ChartFilter(): JSX.Element {
                     icon: <IconPieChart />,
                     label: 'Pie chart',
                     disabledReason: trendsOnlyDisabledReason,
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Pie chart</span>
+                            <span className="text-xs text-tertiary">Proportions of a whole.</span>
+                        </div>
+                    ),
                 },
                 {
                     value: ChartDisplayType.ActionsBarValue,
                     icon: <IconGraph className="rotate-90" />,
                     label: 'Bar chart',
                     disabledReason: trendsOnlyDisabledReason,
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Bar chart</span>
+                            <span className="text-xs text-tertiary">Category totals with horizontal bars.</span>
+                        </div>
+                    ),
                 },
                 {
                     value: ChartDisplayType.ActionsTable,
                     icon: <IconTableChart />,
                     label: 'Table',
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>Table</span>
+                            <span className="text-xs text-tertiary">A table view of values.</span>
+                        </div>
+                    ),
                 },
                 {
                     value: ChartDisplayType.WorldMap,
@@ -92,6 +139,12 @@ export function ChartFilter(): JSX.Element {
                               breakdownFilter.breakdown !== '$geoip_country_name'
                             ? "This type isn't available, because there's a breakdown other than by Country Code or Country Name properties."
                             : undefined),
+                    labelInMenu: (
+                        <div className="flex flex-col gap-[2px]">
+                            <span>World map</span>
+                            <span className="text-xs text-tertiary">Data across regions using color or markers.</span>
+                        </div>
+                    ),
                 },
             ],
         },
