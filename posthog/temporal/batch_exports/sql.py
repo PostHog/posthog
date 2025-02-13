11048dc48762d341d30345a46fd7085426ed4617
Ross Gray <ross@posthog.com>
2025-02-13 17:42:54 +0000

    Add is_deleted to query

--- posthog/temporal/batch_exports/sql.py
+++ posthog/temporal/batch_exports/sql.py
@@ -13,7 +13,8 @@ SELECT
     persons.person_distinct_id_version AS person_distinct_id_version,
     persons.person_version AS person_version,
     persons.created_at AS created_at,
-    persons._inserted_at AS _inserted_at
+    persons._inserted_at AS _inserted_at,
+    persons.is_deleted AS is_deleted
 FROM (
     with new_persons as (
         select
@@ -94,6 +95,7 @@ FROM (
         pd.version AS person_distinct_id_version,
         p.version AS person_version,
         p.created_at AS created_at,
+        p.is_deleted AS is_deleted,
         multiIf(
             (
                 pd._timestamp >= {interval_start}::DateTime64
@@ -151,73 +153,64 @@ SETTINGS
 
 SELECT_FROM_PERSONS_BACKFILL = """
 SELECT
-    persons.team_id AS team_id,
-    persons.distinct_id AS distinct_id,
-    persons.person_id AS person_id,
-    persons.properties AS properties,
-    persons.person_distinct_id_version AS person_distinct_id_version,
-    persons.person_version AS person_version,
-    persons.created_at AS created_at,
-    persons._inserted_at AS _inserted_at
+    pd.team_id AS team_id,
+    pd.distinct_id AS distinct_id,
+    toString(p.id) AS person_id,
+    p.properties AS properties,
+    pd.version AS person_distinct_id_version,
+    p.version AS person_version,
+    p.created_at AS created_at,
+    p.is_deleted AS is_deleted,
+    multiIf(
+        pd._timestamp < {interval_end}::DateTime64
+            AND NOT p._timestamp < {interval_end}::DateTime64,
+        pd._timestamp,
+        p._timestamp < {interval_end}::DateTime64
+            AND NOT pd._timestamp < {interval_end}::DateTime64,
+        p._timestamp,
+        least(p._timestamp, pd._timestamp)
+    ) AS _inserted_at
 FROM (
     SELECT
-        pd.team_id AS team_id,
-        pd.distinct_id AS distinct_id,
-        toString(p.id) AS person_id,
-        p.properties AS properties,
-        pd.version AS person_distinct_id_version,
-        p.version AS person_version,
-        p.created_at AS created_at,
-        multiIf(
-            pd._timestamp < {interval_end}::DateTime64
-                AND NOT p._timestamp < {interval_end}::DateTime64,
-            pd._timestamp,
-            p._timestamp < {interval_end}::DateTime64
-                AND NOT pd._timestamp < {interval_end}::DateTime64,
-            p._timestamp,
-            least(p._timestamp, pd._timestamp)
-        ) AS _inserted_at
-    FROM (
-        SELECT
-            team_id,
-            distinct_id,
-            max(version) AS version,
-            argMax(person_id, person_distinct_id2.version) AS person_id,
-            argMax(_timestamp, person_distinct_id2.version) AS _timestamp
-        FROM
-            person_distinct_id2
-        PREWHERE
-            team_id = {team_id}::Int64
-        GROUP BY
-            team_id,
-            distinct_id
-    ) AS pd
-    INNER JOIN (
-        SELECT
-            team_id,
-            id,
-            max(version) AS version,
-            argMax(properties, person.version) AS properties,
-            argMax(created_at, person.version) AS created_at,
-            argMax(_timestamp, person.version) AS _timestamp
-        FROM
-            person
-        PREWHERE
-            team_id = {team_id}::Int64
-        GROUP BY
-            team_id,
-            id
-    ) AS p ON p.id = pd.person_id AND p.team_id = pd.team_id
-    WHERE
-        pd.team_id = {team_id}::Int64
-        AND p.team_id = {team_id}::Int64
-        AND (
-            pd._timestamp < {interval_end}::DateTime64
-            OR p._timestamp < {interval_end}::DateTime64
-        )
-    ORDER BY
-        _inserted_at
-) AS persons
+        team_id,
+        distinct_id,
+        max(version) AS version,
+        argMax(person_id, person_distinct_id2.version) AS person_id,
+        argMax(_timestamp, person_distinct_id2.version) AS _timestamp
+    FROM
+        person_distinct_id2
+    PREWHERE
+        team_id = {team_id}::Int64
+    GROUP BY
+        team_id,
+        distinct_id
+) AS pd
+INNER JOIN (
+    SELECT
+        team_id,
+        id,
+        max(version) AS version,
+        argMax(properties, person.version) AS properties,
+        argMax(created_at, person.version) AS created_at,
+        argMax(_timestamp, person.version) AS _timestamp,
+        argMax(is_deleted, person.version) AS is_deleted
+    FROM
+        person
+    PREWHERE
+        team_id = {team_id}::Int64
+    GROUP BY
+        team_id,
+        id
+) AS p ON p.id = pd.person_id AND p.team_id = pd.team_id
+WHERE
+    pd.team_id = {team_id}::Int64
+    AND p.team_id = {team_id}::Int64
+    AND (
+        pd._timestamp < {interval_end}::DateTime64
+        OR p._timestamp < {interval_end}::DateTime64
+    )
+ORDER BY
+    _inserted_at
 FORMAT ArrowStream
 SETTINGS
     max_bytes_before_external_group_by=50000000000,
