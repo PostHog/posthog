28e2c7d37993c62a0969b0c8e8a1115367b11b20
Ben White <ben@posthog.com>
2025-02-13 09:25:52 +0100

    Fixes

--- frontend/src/scenes/pipeline/PipelineNodeNew.tsx
+++ frontend/src/scenes/pipeline/PipelineNodeNew.tsx
@@ -23,7 +23,6 @@ import { PipelineBatchExportConfiguration } from './PipelineBatchExportConfigura
 import { PIPELINE_TAB_TO_NODE_STAGE } from './PipelineNode'
 import { pipelineNodeNewLogic, PipelineNodeNewLogicProps } from './pipelineNodeNewLogic'
 import { PipelinePluginConfiguration } from './PipelinePluginConfiguration'
-import { pipelineTransformationsLogic } from './transformationsLogic'
 import { PipelineBackend } from './types'
 import { RenderApp } from './utils'
 
@@ -106,7 +105,6 @@ export function PipelineNodeNew(params: { stage?: string; id?: string } = {}): J
 
     if (stage === PipelineStage.Transformation) {
         return <NewDestinations types={['transformation']} />
-        return <TransformationOptionsTable />
     } else if (stage === PipelineStage.Destination) {
         return <NewDestinations types={DESTINATION_TYPES} />
     } else if (stage === PipelineStage.SiteApp) {
@@ -119,12 +117,6 @@ export function PipelineNodeNew(params: { stage?: string; id?: string } = {}): J
         return <NewSourceWizardScene />
     }
     return <NotFound object="pipeline new options" />
-}
-
-function TransformationOptionsTable(): JSX.Element {
-    const { plugins, loading } = useValues(pipelineTransformationsLogic)
-    const targets = Object.values(plugins).map(convertPluginToTableEntry)
-    return <NodeOptionsTable stage={PipelineStage.Transformation} targets={targets} loading={loading} />
 }
 
 function SiteAppOptionsTable(): JSX.Element {
