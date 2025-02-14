0f8ae2f8d666024555b311fc88857b25f14b2007
Paul D'Ambra <paul@posthog.com>
2025-02-14 16:15:21 +0000

    fiddling

--- playwright/e2e/auth-password-reset.spec.ts
+++ playwright/e2e/auth-password-reset.spec.ts
@@ -1,19 +1,22 @@
 import { expect, test } from '../utils/playwright-test-base'
+import { LoginPage } from '../page-models/loginPage'
 
 const VALID_PASSWORD = 'hedgE-hog-123%'
 
 test.describe('Password Reset', () => {
+    let loginPage: LoginPage
+
     test.beforeEach(async ({ page }) => {
         await page.click('[data-attr=menu-item-me]')
         await page.click('[data-attr=top-menu-item-logout]')
         await expect(page).toHaveURL('/login')
+        loginPage = new LoginPage(page)
     })
 
     test('Can request password reset', async ({ page }) => {
-        await page.fill('[data-attr=login-email]', 'fake@posthog.com')
-        expect(page.locator('[data-attr=login-email]')).toHaveValue('fake@posthog.com')
+        await loginPage.enterUsername('fake@posthog.com')
         await page.locator('[data-attr=login-email]').blur()
-        expect(page.locator('[data-attr=forgot-password]')).toBeVisible({ timeout: 5000 })
+        await expect(page.locator('[data-attr=forgot-password]')).toBeVisible({ timeout: 5000 })
         await page.click('[data-attr="forgot-password"]')
         await expect(page).toHaveURL('/reset')
         await page.fill('[data-attr="reset-email"]', 'test@posthog.com')
@@ -30,7 +33,7 @@ test.describe('Password Reset', () => {
     test('Shows validation error if passwords do not match', async ({ page }) => {
         await page.goto('/reset/e2e_test_user/e2e_test_token')
         await page.fill('[data-attr="password"]', VALID_PASSWORD)
-        expect(page.locator('.LemonProgress__track')).toBeVisible()
+        await expect(page.locator('.LemonProgress__track')).toBeVisible()
         await page.fill('[data-attr="password-confirm"]', '1234567A')
         await page.click('button[type=submit]')
         await expect(page.locator('.text-danger')).toContainText('Passwords do not match')
