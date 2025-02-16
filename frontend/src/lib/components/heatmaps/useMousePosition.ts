470864bcd2b5162fbf42e31e67be476239a87fcd
Paul D'Ambra <paul@posthog.com>
2025-02-16 21:35:17 +0000

    fiddling

--- frontend/src/lib/components/heatmaps/useMousePosition.ts
+++ frontend/src/lib/components/heatmaps/useMousePosition.ts
@@ -1,19 +1,31 @@
 import { useEffect, useState } from 'react'
 
-export const useMousePosition = (): { x: number; y: number } => {
+export const useMousePosition = (container?: HTMLElement | null): { x: number; y: number } => {
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
 
-    useEffect(() => {
-        const onMove = (e: MouseEvent): void => {
-            if (e.clientX !== mousePosition.x || e.clientY !== mousePosition.y) {
-                setMousePosition({ x: e.clientX, y: e.clientY })
+    useEffect(
+        () => {
+            const onMove = (e: MouseEvent): void => {
+                if (container) {
+                    const rect = (container || window).getBoundingClientRect()
+                    setMousePosition({
+                        x: e.clientX - rect.left,
+                        y: e.clientY - rect.top,
+                    })
+                } else {
+                    setMousePosition({ x: e.clientX, y: e.clientY })
+                }
             }
-        }
 
-        window.addEventListener('mousemove', onMove, { passive: true })
-        return () => {
-            window.removeEventListener('mousemove', onMove)
-        }
-    }, [])
+            window.addEventListener('mousemove', onMove, { passive: true })
+            return () => {
+                window.removeEventListener('mousemove', onMove)
+            }
+        },
+        [
+            // don't need container as a dependency because it changes the behaviour not the listener
+        ]
+    )
+
     return mousePosition
 }
