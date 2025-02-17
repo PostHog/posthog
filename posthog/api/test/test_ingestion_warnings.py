9a51b49d204bae50cd490f9f59a63a3ce672938f
Paul D'Ambra <paul@posthog.com>
2025-02-17 20:34:37 +0000

    feat: ingestion warnings search

--- posthog/api/test/test_ingestion_warnings.py
+++ posthog/api/test/test_ingestion_warnings.py
@@ -4,6 +4,7 @@ from math import floor
 from freezegun.api import freeze_time
 from rest_framework import status
 
+from posthog.clickhouse.client import sync_execute
 from posthog.kafka_client.client import ClickhouseProducer
 from posthog.kafka_client.topics import KAFKA_INGESTION_WARNINGS
 from posthog.models.event.util import format_clickhouse_timestamp
@@ -29,9 +30,13 @@ def create_ingestion_warning(team_id: int, type: str, details: dict, timestamp: 
 
 
 class TestIngestionWarningsAPI(ClickhouseTestMixin, APIBaseTest):
-    @freeze_time("2021-12-04T19:20:00Z")
-    def test_ingestion_warnings_api(self):
-        a_lot_of_ingestion_warning_timestamps: list[str] = []
+    a_lot_of_ingestion_warning_timestamps: list[str] = []
+
+    def setUp(self):
+        super().setUp()
+        sync_execute("TRUNCATE TABLE ingestion_warnings")
+
+        self.a_lot_of_ingestion_warning_timestamps: list[str] = []
 
         # KLUDGE: it is 59 here so that timestamp creation can be naive
         # more than the number the front end will show
@@ -42,7 +47,7 @@ class TestIngestionWarningsAPI(ClickhouseTestMixin, APIBaseTest):
             day = (i % 5) + 1
             formatted_day = f"0{day}" if day < 10 else str(day)
             timestamp = f"2021-12-{formatted_day}T13:{formatted_minutes}:{seconds}Z"
-            a_lot_of_ingestion_warning_timestamps.insert(0, timestamp)
+            self.a_lot_of_ingestion_warning_timestamps.insert(0, timestamp)
             create_ingestion_warning(
                 team_id=self.team.id,
                 type="replay_timestamp_too_far",
@@ -88,6 +93,8 @@ class TestIngestionWarningsAPI(ClickhouseTestMixin, APIBaseTest):
             timestamp="2021-12-01T00:00:00Z",
         )
 
+    @freeze_time("2021-12-04T19:20:00Z")
+    def test_ingestion_warnings_api(self):
         response = self.client.get(f"/api/projects/{self.team.pk}/ingestion_warnings")
         assert response.status_code == status.HTTP_200_OK
         assert (
@@ -112,7 +119,7 @@ class TestIngestionWarningsAPI(ClickhouseTestMixin, APIBaseTest):
                                 "type": "replay_timestamp_too_far",
                                 # even though there are very many warnings we limit the number of examples we send to the frontend
                             }
-                            for t in sorted(a_lot_of_ingestion_warning_timestamps, reverse=True)[:50]
+                            for t in sorted(self.a_lot_of_ingestion_warning_timestamps, reverse=True)[:50]
                         ],
                     },
                     {
@@ -154,3 +161,52 @@ class TestIngestionWarningsAPI(ClickhouseTestMixin, APIBaseTest):
                 ]
             }
         )
+
+    @freeze_time("2021-12-04T19:20:00Z")
+    def test_ingestion_warnings_api_search_by_type(self):
+        response = self.client.get(f"/api/projects/{self.team.pk}/ingestion_warnings?q=another_type")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {
+            "results": [
+                {
+                    "type": "another_type",
+                    "lastSeen": "2021-11-15T00:00:00Z",
+                    "sparkline": [[1, "2021-11-15"]],
+                    "warnings": [
+                        {
+                            "type": "another_type",
+                            "timestamp": "2021-11-15T00:00:00Z",
+                            "details": {},
+                        }
+                    ],
+                    "count": 1,
+                },
+            ]
+        }
+
+    @freeze_time("2021-12-04T19:20:00Z")
+    def test_ingestion_warnings_api_search_by_id(self):
+        response = self.client.get(f"/api/projects/{self.team.pk}/ingestion_warnings?q=x-uuid")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {
+            "results": [
+                {
+                    "count": 1,
+                    "lastSeen": "2021-12-03T00:00:00Z",
+                    "sparkline": [[1, "2021-12-03"]],
+                    "type": "cannot_merge_already_identified",
+                    "warnings": [
+                        {
+                            "type": "cannot_merge_already_identified",
+                            "timestamp": "2021-12-03T00:00:00Z",
+                            "details": {
+                                "sourcePerson": "x-uuid",
+                                "sourcePersonDistinctId": "Alice",
+                                "targetPerson": "y-uuid",
+                                "targetPersonDistinctId": "Bob",
+                            },
+                        }
+                    ],
+                },
+            ]
+        }
