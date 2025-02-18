53d5365c907dce84b53b92dd8578986b826ad127
Paul D'Ambra <paul@posthog.com>
2025-02-18 21:27:24 +0000

    Waaaat

--- playwright/e2e/product-analytics/cohorts.spec.ts
+++ playwright/e2e/product-analytics/cohorts.spec.ts
@@ -1,49 +1,45 @@
 import { expect, test } from '../../utils/playwright-test-base'
 import { randomString } from '../../utils'
+import { CohortPage } from '../../page-models/cohortPage'
 
 test.describe('Cohorts', () => {
     test.beforeEach(async ({ page }) => {
         await page.goToMenuItem('personsmanagement')
         await page.click('[data-attr=persons-management-cohorts-tab]')
-    })
 
-    test('Cohorts new and list', async ({ page }) => {
         await expect(page).toHaveTitle('Cohorts • People • PostHog')
         await expect(page.locator('[data-attr="create-cohort"]')).toBeVisible()
         await expect(page.locator('[data-attr="product-introduction-docs-link"]')).toHaveText(/Learn more/)
+    })
 
-        await page.click('[data-attr="new-cohort"]')
-        await page.click('[data-attr="cohort-selector-field-value"]')
-        await page.click('[data-attr="cohort-personPropertyBehavioral-have_property-type"]')
-        await page.click('[data-attr="cohort-taxonomic-field-key"]')
-
-        await page.click('[data-attr=prop-filter-person_properties-0]', { force: true })
-        await page.locator('[data-attr=prop-val]').type('true')
-
-        await page.click('[data-attr="cohort-name"]')
-
+    test('Can create a cohort', async ({ page }) => {
         const name = randomString('Test-Cohort-')
 
-        await page.fill('[data-attr="cohort-name"]', name)
-        await page.click('[data-attr="save-cohort"]')
-        await expect(page.locator('[data-attr=success-toast]')).toHaveText(/Cohort saved/)
-        await page.locator('[data-attr="toast-close-button"]').click()
+        await new CohortPage(page).createCohort(name)
 
         await page.goToMenuItem('personsmanagement')
         await page.click('[data-attr=persons-management-cohorts-tab]')
 
         await expect(page.locator('tbody')).toContainText(name)
-        await expect(page.locator('text=Create your first cohort')).not.toBeVisible()
+    })
+
+    test('Duplicate a cohort', async ({ page }) => {
+        const name = randomString('Test-Cohort-')
+
+        await new CohortPage(page).createCohort(name)
+
+        await page.goToMenuItem('personsmanagement')
+        await page.click('[data-attr=persons-management-cohorts-tab]')
 
         // navigate to the page
         await page.click('tbody >> text=' + name)
         await expect(page.getByTestId('top-bar-name').getByText('Test-Cohort--')).toBeVisible()
-        await page.click('[data-attr="more-button"]', { force: true })
+        await page.click('.TopBar3000 [data-attr="more-button"]', { force: true })
         // click edit
         await page.click('.Popover__content >> text=Duplicate as dynamic cohort')
         await page.click('.Toastify__toast-body >> text=View cohort')
 
-        await page.click('[data-attr="more-button"]')
+        await page.click('.TopBar3000 [data-attr="more-button"]')
         await page.click('.Popover__content >> text=Duplicate as static cohort')
         await page.click('.Toastify__toast-body >> text=View cohort')
 
