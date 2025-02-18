847e6ef392e96393abcb0f1bc95a0dc8b75d508e
Robbie Coomber <robbie.coomber@gmail.com>
2025-02-18 19:39:36 +0000

    Change ordering

--- posthog/clickhouse/migrations/0100_sessions_v2_irclid_kx.py
+++ posthog/clickhouse/migrations/0100_sessions_v2_irclid_kx.py
@@ -15,12 +15,12 @@ operations = [
     # drop the mv, so we are no longer receiving events from the sessions table
     run_sql_with_exceptions(DROP_RAW_SESSION_MATERIALIZED_VIEW_SQL()),
     # now we can alter the target tables
-    run_sql_with_exceptions(WRITABLE_RAW_SESSIONS_ADD_IRCLID_KX_COLUMNS_SQL()),
+    run_sql_with_exceptions(BASE_RAW_SESSIONS_ADD_IRCLID_KX_COLUMNS_SQL()),
     run_sql_with_exceptions(DISTRIBUTED_RAW_SESSIONS_ADD_IRCLID_KX_COLUMNS_SQL()),
     run_sql_with_exceptions(
         DISTRIBUTED_RAW_SESSIONS_ADD_IRCLID_KX_COLUMNS_SQL(on_cluster=False), node_role=NodeRole.COORDINATOR
     ),
-    run_sql_with_exceptions(BASE_RAW_SESSIONS_ADD_IRCLID_KX_COLUMNS_SQL()),
+    run_sql_with_exceptions(WRITABLE_RAW_SESSIONS_ADD_IRCLID_KX_COLUMNS_SQL()),
     # and then recreate the materialized view and view
     run_sql_with_exceptions(RAW_SESSIONS_TABLE_MV_SQL()),
     run_sql_with_exceptions(RAW_SESSIONS_CREATE_OR_REPLACE_VIEW_SQL()),
