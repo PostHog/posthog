96c41b057b4c84d26346c9846fa669266d3e8195
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-02-19 19:23:01 +0100

    fix: categorize icons

--- frontend/src/lib/lemon-ui/icons/categories.ts
+++ frontend/src/lib/lemon-ui/icons/categories.ts
@@ -175,6 +175,7 @@ export const ELEMENTS = {
         'IconEllipsis',
         'IconHide',
         'IconStopFilled',
+        'IconListCheck',
     ],
     'Arrows & Shapes': [
         'IconArrowLeft',
@@ -251,4 +252,6 @@ export const TEAMS_AND_COMPANIES = {
     Pipeline: ['IconWebhooks', 'IconDecisionTree'],
     'Product OS': ['IconNotebook', 'IconHogQL', 'IconDashboard', 'IconSupport'],
     Logos: ['IconLogomark', 'IconGithub'],
+    ErrorTracking: ['IconIssue'],
+    LLMObservability: ['IconLlmObservability', 'IconLlmPromptEvaluation', 'IconLlmPromptManagement'],
 }
