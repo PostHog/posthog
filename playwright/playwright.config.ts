cc49cfc504e823d6e03d5e61add1cfcc81ece4f9
Paul D'Ambra <paul@posthog.com>
2025-02-20 23:43:45 +0000

    nope, less oomph

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -26,8 +26,8 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 8 : 2,
-    /* Run one worker per core in GitHub Actions. */
-    workers: process.env.CI ? 6 : undefined,
+    /* 4 cores in GitHub Actions so run 3 workers and leave one core for all the rest */
+    workers: process.env.CI ? 3 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [['html', { open: 'never' }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
