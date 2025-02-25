7035fd5ef0f61316b6861aaf8ff97f7a6c61cb15
Paul D'Ambra <paul@posthog.com>
2025-02-25 09:39:09 +0000

    feat: replay tab state

--- frontend/src/scenes/session-recordings/player/sidebar/playerSidebarLogic.ts
+++ frontend/src/scenes/session-recordings/player/sidebar/playerSidebarLogic.ts
@@ -1,4 +1,5 @@
 import { actions, kea, path, props, reducers } from 'kea'
+import { actionToUrl, router, urlToAction } from 'kea-router'
 
 import { SessionRecordingSidebarTab } from '~/types'
 
@@ -18,5 +19,31 @@ export const playerSidebarLogic = kea<playerSidebarLogicType>([
             SessionRecordingSidebarTab.INSPECTOR as SessionRecordingSidebarTab,
             { setTab: (_, { tab }) => tab },
         ],
+    })),
+
+    actionToUrl(() => ({
+        setTab: ({ tab }) => {
+            const { currentLocation } = router.values
+            return [
+                currentLocation.pathname,
+                {
+                    ...currentLocation.searchParams,
+                    tab,
+                },
+                currentLocation.hashParams,
+            ]
+        },
+    })),
+
+    urlToAction(({ actions, values }) => ({
+        ['**/replay/home*']: (_, searchParams) => {
+            const urlTab = Object.values(SessionRecordingSidebarTab).includes(searchParams.tab)
+                ? (searchParams.tab as SessionRecordingSidebarTab)
+                : SessionRecordingSidebarTab.INSPECTOR
+
+            if (!!urlTab && urlTab !== values.activeTab) {
+                actions.setTab(urlTab)
+            }
+        },
     })),
 ])
