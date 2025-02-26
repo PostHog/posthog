cdb3a22b04fbdd86c7e0eef4d8a9e62bc8546db0
meikel <meikel@posthog.com>
2025-02-26 13:54:06 +0100

    update

--- plugin-server/src/worker/vm/imports.ts
+++ plugin-server/src/worker/vm/imports.ts
@@ -7,14 +7,22 @@ import * as genericPool from 'generic-pool'
 import { PassThrough } from 'stream'
 import * as url from 'url'
 
+import { defaultConfig } from '~/src/config/config'
+import { trackedFetch } from '~/src/utils/fetch'
+
 import { isTestEnv } from '../../utils/env-utils'
 import { HttpCallRecorder, recordedFetch } from '../../utils/recorded-fetch'
 import { writeToFile } from './extensions/test-utils'
 
 export const globalHttpCallRecorder = new HttpCallRecorder()
+const shouldRecordHttpCalls =
+    defaultConfig.DESTINATION_MIGRATION_DIFFING_ENABLED === true && defaultConfig.TASKS_PER_WORKER === 1
 // Always use recordedFetch as its only a wrapper around trackedFetch
-export const recordedTrackedFetch = (url: any, init?: any) => {
-    return recordedFetch(globalHttpCallRecorder, url, init)
+export const conditionalTrackedFetch = (url: any, init?: any) => {
+    if (shouldRecordHttpCalls) {
+        return recordedFetch(globalHttpCallRecorder, url, init)
+    }
+    return trackedFetch(url, init)
 }
 
 export const AVAILABLE_IMPORTS = {
@@ -28,7 +36,7 @@ export const AVAILABLE_IMPORTS = {
     '@posthog/plugin-scaffold': scaffold,
     'aws-sdk': AWS,
     'generic-pool': genericPool,
-    'node-fetch': recordedTrackedFetch,
+    'node-fetch': conditionalTrackedFetch,
     crypto: crypto,
     stream: { PassThrough },
     url: url,
