20b6ce360f9473855ccb4bf88bd98ae9f3642298
Alexander Spicer <aspicer@gmail.com>
2025-02-26 16:14:44 -0800

    funnel

--- posthog/hogql_queries/insights/funnels/funnel_time_to_convert_udf.py
+++ posthog/hogql_queries/insights/funnels/funnel_time_to_convert_udf.py
@@ -28,9 +28,31 @@ class FunnelTimeToConvertUDF(FunnelBase):
         )
 
     def get_query(self) -> ast.SelectQuery:
-        funnelsFilter = self.context.funnelsFilter
+        query, funnelsFilter = self.context.query, self.context.funnelsFilter
 
-        bin_count = funnelsFilter.binCount or 5
+        # Conversion from which step should be calculated
+        from_step = funnelsFilter.funnelFromStep or 0
+        # Conversion to which step should be calculated
+        to_step = funnelsFilter.funnelToStep or len(query.series) - 1
+
+        # Use custom bin_count if provided by user, otherwise infer an automatic one based on the number of samples
+        binCount = funnelsFilter.binCount
+        if binCount is not None:
+            # Custom count is clamped between 1 and 90
+            if binCount < 1:
+                binCount = 1
+            elif binCount > 90:
+                binCount = 90
+            bin_count_expression = f"""{binCount}"""
+        else:
+            # Auto count is clamped between 1 and 60
+            bin_count_expression = f"""toInt(least(60, greatest(1, ceil(cbrt(ifNull(length(timings), 0))))))"""
+
+        if not (0 < to_step < len(query.series)):
+            raise ValidationError(
+                f'Filter parameter {FUNNEL_TO_STEP} can only be one of {", ".join(map(str, range(1, len(query.series))))} for time to convert!'
+            )
+
         inner_select = self.funnel_order._inner_aggregation_query()
 
         # for everybody with steps at max steps,
@@ -39,13 +61,15 @@ class FunnelTimeToConvertUDF(FunnelBase):
         timings = parse_select(
             f"""
             SELECT
-                groupArray(arraySum(arraySlice(timings, {funnelsFilter.funnelFromStep+1}, {funnelsFilter.funnelToStep - funnelsFilter.funnelFromStep}))) as timings,
+                groupArray(arraySum(arraySlice(timings, {from_step+1}, {to_step - from_step}))) as timings,
+                {bin_count_expression} as bin_count,
                 floor(arrayMin(timings)) as min_timing,
                 ceil(arrayMax(timings)) as max_timing,
-                (max_timing - min_timing) / {bin_count} as bucket_size,
-                arrayMap(n -> toInt(round(min_timing + n * bucket_size)), range(0,{bin_count})) as buckets,
-                arrayMap(timing -> toInt(floor((timing - min_timing) / bucket_size)), timings) as indices,
-                arrayMap(x -> countEqual(indices, x-1), range(1, {bin_count + 1})) as counts
+                ceil((max_timing - min_timing) / bin_count) as bin_width_seconds_raw,
+                if(bin_width_seconds_raw > 0, bin_width_seconds_raw, 60) AS bin_width_seconds,
+                arrayMap(n -> toInt(round(min_timing + n * bin_width_seconds)), range(0, bin_count + 1)) as buckets,
+                arrayMap(timing -> toInt(floor((timing - min_timing) / bin_width_seconds)), timings) as indices,
+                arrayMap(x -> countEqual(indices, x-1), range(1, bin_count + 2)) as counts
             FROM {{inner_select}}
             WHERE step_reached >= {funnelsFilter.funnelToStep} """,
             {"inner_select": inner_select},
@@ -64,10 +88,6 @@ class FunnelTimeToConvertUDF(FunnelBase):
             """,
             {"timings": timings},
         )
-
-        # self.funnelsFilter = funnelsFilter
-
-        # steps_per_person_query = self.funnel_order.get_step_counts_query()
 
     def get_query_old(self) -> ast.SelectQuery:
         query, funnelsFilter = self.context.query, self.context.funnelsFilter
