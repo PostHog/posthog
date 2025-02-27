5c6ad3d54b9b2afede69f26e2b5db1651ce63298
Juraj Majerik <juro.majerik@gmail.com>
2025-02-27 17:50:45 +0100

    init

--- frontend/src/scenes/experiments/RunningTimeCalculator/runningTimeCalculatorLogic.test.ts
+++ frontend/src/scenes/experiments/RunningTimeCalculator/runningTimeCalculatorLogic.test.ts
@@ -1,5 +1,7 @@
 import { expectLogic } from 'kea-test-utils'
+import { experimentLogic } from 'scenes/experiments/experimentLogic'
 
+import { ExperimentMetric, ExperimentMetricType } from '~/queries/schema/schema-general'
 import { initKeaTests } from '~/test/init'
 
 import { runningTimeCalculatorLogic } from './runningTimeCalculatorLogic'
@@ -9,13 +11,27 @@ describe('runningTimeCalculatorLogic', () => {
 
     beforeEach(() => {
         initKeaTests()
+        experimentLogic.mount()
+
         logic = runningTimeCalculatorLogic()
         logic.mount()
     })
 
     // Should match https://docs.google.com/spreadsheets/d/11alyC8n7uqewZFLKfV4UAbW-0zH__EdV_Hrk2OQ4140/edit?gid=777532876#gid=777532876
-    describe('selectors', () => {
-        it('calculates recommended sample size correctly', async () => {
+    describe('calculations for COUNT', () => {
+        beforeEach(() => {
+            experimentLogic.actions.setExperiment({
+                metrics: [
+                    {
+                        metric_type: ExperimentMetricType.COUNT,
+                    } as ExperimentMetric,
+                ],
+            })
+
+            logic.actions.setMetricIndex(0)
+        })
+
+        it('calculates recommended sample size and running time correctly for COUNT', async () => {
             logic.actions.setMinimumDetectableEffect(5)
             logic.actions.setMetricResult({
                 uniqueUsers: 28000,
@@ -28,20 +44,38 @@ describe('runningTimeCalculatorLogic', () => {
                 minimumDetectableEffect: 5,
                 variance: 8,
                 recommendedSampleSize: expect.closeTo(12800, 0),
+                recommendedRunningTime: expect.closeTo(6.4, 1),
             })
         })
+    })
 
-        it('calculates recommended running time correctly', async () => {
+    describe('calculations for CONTINUOUS', () => {
+        beforeEach(() => {
+            experimentLogic.actions.setExperiment({
+                metrics: [
+                    {
+                        metric_type: ExperimentMetricType.CONTINUOUS,
+                    } as ExperimentMetric,
+                ],
+            })
+
+            logic.actions.setMetricIndex(0)
+        })
+
+        it('calculates recommended sample size and running time correctly for CONTINUOUS', async () => {
             logic.actions.setMinimumDetectableEffect(5)
             logic.actions.setMetricResult({
-                uniqueUsers: 28000,
-                averageEventsPerUser: 4,
+                uniqueUsers: 14000,
+                averagePropertyValuePerUser: 50,
             })
 
             await expectLogic(logic).toFinishAllListeners()
 
             await expectLogic(logic).toMatchValues({
-                recommendedRunningTime: expect.closeTo(6.4, 1),
+                minimumDetectableEffect: 5,
+                variance: expect.closeTo(625, 0),
+                recommendedSampleSize: expect.closeTo(3200, 0),
+                recommendedRunningTime: expect.closeTo(3.2, 1),
             })
         })
     })
