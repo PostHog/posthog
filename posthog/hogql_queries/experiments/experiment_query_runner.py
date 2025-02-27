6bb968ab04660758c6d925a7c54c290a46314814
Anders Asheim Hennum <git@andehen.net>
2025-02-27 11:07:53 +0100

    Use materialized columns

--- posthog/hogql_queries/experiments/experiment_query_runner.py
+++ posthog/hogql_queries/experiments/experiment_query_runner.py
@@ -159,10 +159,7 @@ class ExperimentQueryRunner(QueryRunner):
                     ),
                     ast.CompareOperation(
                         op=ast.CompareOperationOp.In,
-                        left=parse_expr(
-                            "replaceAll(JSONExtractRaw(properties, {feature_flag_property}), '\"', '')",
-                            placeholders={"feature_flag_property": ast.Constant(value=feature_flag_property)},
-                        ),
+                        left=ast.Field(chain=["properties", feature_flag_property]),
                         right=ast.Constant(value=self.variants),
                     ),
                     *exposure_property_filters,
@@ -176,22 +173,21 @@ class ExperimentQueryRunner(QueryRunner):
                         left=ast.Field(chain=["event"]),
                         right=ast.Constant(value="$feature_flag_called"),
                     ),
+                    # $feature_flag_called events have this materalized property we can use to look
+                    # up which flag it is
                     ast.CompareOperation(
                         op=ast.CompareOperationOp.Eq,
-                        left=parse_expr("replaceAll(JSONExtractRaw(properties, '$feature_flag'), '\"', '')"),
+                        left=ast.Field(chain=["properties", "$feature_flag"]),
                         right=ast.Constant(value=feature_flag_key),
                     ),
                     ast.CompareOperation(
                         op=ast.CompareOperationOp.In,
-                        left=parse_expr("replaceAll(JSONExtractRaw(properties, '$feature_flag_response'), '\"', '')"),
+                        left=ast.Field(chain=["properties", "$feature_flag_response"]),
                         right=ast.Constant(value=self.variants),
                     ),
                     ast.CompareOperation(
                         op=ast.CompareOperationOp.In,
-                        left=parse_expr(
-                            "replaceAll(JSONExtractRaw(properties, {feature_flag_property}), '\"', '')",
-                            placeholders={"feature_flag_property": ast.Constant(value=feature_flag_property)},
-                        ),
+                        left=ast.Field(chain=["properties", feature_flag_property]),
                         right=ast.Constant(value=self.variants),
                     ),
                 ]
@@ -199,11 +195,17 @@ class ExperimentQueryRunner(QueryRunner):
 
         exposure_query_select = [
             ast.Alias(alias="entity_id", expr=ast.Field(chain=["person_id"])),
-            parse_expr(
-                "replaceAll(JSONExtractRaw(properties, {feature_flag_property}), '\"', '') AS variant",
-                placeholders={"feature_flag_property": ast.Constant(value=feature_flag_property)},
+            ast.Alias(
+                alias="variant",
+                expr=ast.Field(chain=["properties", feature_flag_property]),
             ),
-            parse_expr("min(timestamp) as first_exposure_time"),
+            ast.Alias(
+                alias="first_exposure_time",
+                expr=ast.Call(
+                    name="min",
+                    args=[ast.Field(chain=["timestamp"])],
+                ),
+            ),
         ]
         exposure_query_group_by = [ast.Field(chain=["variant"]), ast.Field(chain=["entity_id"])]
         if is_data_warehouse_query:
@@ -284,7 +286,10 @@ class ExperimentQueryRunner(QueryRunner):
                                             *metric_config.data_warehouse_join_key.split("."),
                                         ]
                                     ),
-                                    right=parse_expr("toString(exposure_data.exposure_identifier)"),
+                                    right=ast.Call(
+                                        name="toString",
+                                        args=[ast.Field(chain=["exposure_data", "exposure_identifier"])],
+                                    ),
                                     op=ast.CompareOperationOp.Eq,
                                 ),
                                 constraint_type="ON",
