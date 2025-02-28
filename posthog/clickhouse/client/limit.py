62a39dc624abcd90eae1a2b03bd27ad3ccc8f4b9
Pawel Szczur <pawel@posthog.com>
2025-03-01 00:26:24 +0100

    Add RateLimiter class using Redis as distributed sync layer.

--- posthog/clickhouse/client/limit.py
+++ posthog/clickhouse/client/limit.py
@@ -1,3 +1,4 @@
+import dataclasses
 import time
 from functools import wraps
 from typing import Optional
@@ -7,6 +8,12 @@ from celery import current_task
 from prometheus_client import Counter
 
 from posthog import redis
+
+CONCURRENT_QUERY_LIMIT_EXCEEDED_COUNTER = Counter(
+    "posthog_clickhouse_query_concurrency_limit_exceeded",
+    "Number of times a ClickHouse query exceeded the concurrency limit",
+    ["task_name", "limit", "limit_name"],
+)
 
 CONCURRENT_TASKS_LIMIT_EXCEEDED_COUNTER = Counter(
     "posthog_celery_task_concurrency_limit_exceeded",
@@ -38,6 +45,96 @@ end
 redis.call('ZADD', key, expiration_time, task_id)
 return 1
 """
+
+
+@dataclasses.dataclass
+class RateLimit:
+    """
+    Ensures that only max_concurrent_tasks of tasks_name are executed at a given time.
+    Tasks have ttl as a safeguard against not being removed.
+    """
+
+    max_concurrent_tasks: int
+    limit_name: str
+    get_task_name: Callable
+    get_task_id: Callable
+    get_task_key: Callable = None
+    get_time: Callable[[], int] = lambda: int(time.time())
+    applicable: Optional[Callable] = None
+    ttl: int = 60
+
+    @property
+    def redis_client(self):
+        return redis.get_client()
+
+    class Run:
+        def __init__(self, limit, running_task_key, task_id):
+            self.limit = limit
+            self.running_task_key = running_task_key
+            self.task_id = task_id
+
+        def __enter__(self):
+            pass
+
+        def __exit__(self, applicable, running_task_key, task_id):
+            if self.limit:
+                self.limit.release(self.running_task_key, self.task_id)
+
+    def run(self, *args, **kwargs):
+        if self.applicable and not self.applicable(*args, **kwargs):
+            return self.Run()
+        running_tasks_key, task_id = self.use(*args, **kwargs)
+        return self.Run(self, running_tasks_key, task_id)
+
+    def use(self, *args, **kwargs):
+        """
+        Acquire the resource before execution or throw exception.
+        """
+        if self.applicable and not self.applicable(*args, **kwargs):
+            return
+        task_name = self.get_task_name(*args, **kwargs)
+        running_tasks_key = self.get_task_key(*args, **kwargs) if self.get_task_key else task_name
+        task_id = self.get_task_id(*args, **kwargs)
+        current_time = self.get_time()
+
+        # Atomically check, remove expired if limit hit, and add the new task
+        if (
+            self.redis_client.eval(
+                lua_script, 1, running_tasks_key, current_time, task_id, self.max_concurrent_tasks, self.ttl
+            )
+            == 0
+        ):
+            CONCURRENT_QUERY_LIMIT_EXCEEDED_COUNTER.labels(
+                task_name=task_name, limit=self.max_concurrent_tasks, limit_name=self.limit_name
+            ).inc()
+
+            raise CeleryConcurrencyLimitExceeded(
+                f"Exceeded maximum concurrent tasks limit: {self.max_concurrent_tasks} for key: {task_name} and task: {task_id}"
+            )
+
+        return running_tasks_key, task_id
+
+    def release(self, running_task_key, task_id):
+        """
+        Release the resource, when the execution finishes.
+        """
+        self.redis_client.zrem(running_task_key, task_id)
+
+    def wrap(self, task_func):
+        @wraps(task_func)
+        def wrapper(*args, **kwargs):
+            applicable = self.applicable(*args, **kwargs)
+            if applicable:
+                running_tasks_key, task_id = self.use(*args, **kwargs)
+            try:
+                # Execute the task
+                return task_func(*args, **kwargs)
+            finally:
+                # Remove the task ID from the sorted set when the task finishes
+                if applicable:
+                    self.release(running_tasks_key, task_id)
+
+        return wrapper
 
 
 class CeleryConcurrencyLimitExceeded(Exception):
