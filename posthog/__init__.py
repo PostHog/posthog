a5a8e6fe58418c0ec36ca52b7987a2924846b136
Tom Owers <owerstom@gmail.com>
2025-02-28 11:03:57 +0100

    Update snowflake home directory for all deployments

--- posthog/__init__.py
+++ posthog/__init__.py
@@ -3,3 +3,9 @@
 from posthog.celery import app as celery_app
 
 __all__ = ("celery_app",)
+
+# snowflake-connector-python tries to access a root folder which errors out in pods.
+# This sets the snowflake home directory to a relative folder
+import os
+
+os.environ["SNOWFLAKE_HOME"] = "./.snowflake"
