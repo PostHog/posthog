1a31888a03f9e57bfa38b9bd72b518a0bf1cec99
Ben White <ben@posthog.com>
2025-03-03 16:30:59 +0100

    Fix process exiting

--- plugin-server/tests/e2e.timeout.test.ts
+++ plugin-server/tests/e2e.timeout.test.ts
@@ -26,6 +26,7 @@ describe('e2e ingestion timeout', () => {
     })
 
     beforeEach(async () => {
+        jest.spyOn(process, 'exit').mockImplementation(() => {})
         await resetTestDatabase(`
             async function processEvent (event) {
                 await new Promise(resolve => __jestSetTimeout(() => resolve(), 800))
