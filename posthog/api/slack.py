aa69f5ee074dc8d601600b99b68b9dfadda48afe
Paweł Ledwoń <me@difficult.name>
2025-03-04 12:33:30 +0100

    fix: fix slack interactivity endpoint return type

--- posthog/api/slack.py
+++ posthog/api/slack.py
@@ -1,11 +1,10 @@
 from rest_framework.request import Request
-from rest_framework.response import Response
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
-def slack_interactivity_callback(request: Request) -> Response:
+def slack_interactivity_callback(request: Request) -> HttpResponse:
     # This is an empty endpoint for the Slack interactivity callback.
     # We don't verify the request, as we don't do anything with the submitted data.
     # We only use it to supress the warnings when users press buttons in Slack messages.
