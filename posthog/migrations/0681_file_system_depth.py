117e3935405564661115b5ee87e5eded81354108
Marius Andra <marius.andra@gmail.com>
2025-03-05 09:48:09 +0100

    merge

--- posthog/migrations/0681_file_system_depth.py
+++ posthog/migrations/0681_file_system_depth.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0679_feature_flag_concurrency"),
+        ("posthog", "0680_alter_integration_kind"),
     ]
 
     operations = [
