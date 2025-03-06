fd1b983b01b892b179ff466230f9123b26d94c24
Daniel Escribano <daniel.e@posthog.com>
2025-03-06 13:49:50 +0100

    fix: make statement a function

--- posthog/clickhouse/migrations/0104_session_replay_events_v2_test_alter_snapshot_source.py
+++ posthog/clickhouse/migrations/0104_session_replay_events_v2_test_alter_snapshot_source.py
@@ -3,10 +3,14 @@ from posthog.session_recordings.sql.session_replay_event_v2_test_sql import (
     SESSION_REPLAY_EVENTS_V2_TEST_DATA_TABLE,
 )
 
-ALTER_SNAPSHOT_SOURCE_SQL = f"""
-ALTER TABLE {SESSION_REPLAY_EVENTS_V2_TEST_DATA_TABLE}
+ALTER_SNAPSHOT_SOURCE_SQL = (
+    lambda: """
+ALTER TABLE {table_name}
     MODIFY COLUMN `snapshot_source` AggregateFunction(argMin, Nullable(String), DateTime64(6, 'UTC'))
-"""
+""".format(
+        table_name=SESSION_REPLAY_EVENTS_V2_TEST_DATA_TABLE,
+    )
+)
 
 operations = [
     run_sql_with_exceptions(ALTER_SNAPSHOT_SOURCE_SQL(), sharded=True),
