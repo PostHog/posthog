2924e6c18220cd6570af4dc08b4929c7e1d28919
Paweł Ledwoń <me@difficult.name>
2025-03-06 11:42:26 +0000

    fix: make slack interactivity endpoint csrf exempt (#29433)

--- ee/api/integration.py
+++ ee/api/integration.py
@@ -22,14 +22,6 @@ class PublicIntegrationViewSet(viewsets.GenericViewSet):
     authentication_classes = []
     permission_classes = []
 
-    @action(methods=["POST"], detail=False, url_path="slack/interactivity-callback")
-    def slack_interactivity_callback(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        # This is an empty endpoint for the Slack interactivity callback.
-        # We don't verify the request, as we don't do anything with the submitted data.
-        # We only use it to supress the warnings when users press buttons in Slack messages.
-        # In case we decide to do something with it, please add the verification process here.
-        return Response({"status": "ok"})
-
     @action(methods=["POST"], detail=False, url_path="slack/events")
     def slack_events(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         try:
