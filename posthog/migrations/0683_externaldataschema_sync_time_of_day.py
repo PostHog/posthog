d86bc5547d971b0ab6fea37128c96de3b0802187
Peter Hicks <peter.h@posthog.com>
2025-03-07 10:14:45 -0800

    Updating the max_migration

--- posthog/migrations/0683_externaldataschema_sync_time_of_day.py
+++ posthog/migrations/0683_externaldataschema_sync_time_of_day.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0679_feature_flag_concurrency"),
+        ("posthog", "0682_datawarehousesavedquery_latest_error"),
     ]
 
     operations = [
