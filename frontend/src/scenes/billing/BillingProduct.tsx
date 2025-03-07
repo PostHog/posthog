88f63a0ad1eb49538908ef5d2695186c34c02b85
Patricio <patricio@posthog.com>
2025-03-07 14:15:12 -0300

    add border

--- frontend/src/scenes/billing/BillingProduct.tsx
+++ frontend/src/scenes/billing/BillingProduct.tsx
@@ -377,9 +377,7 @@ export const BillingProduct = ({ product }: { product: BillingProductV2Type }): 
                     )}
                 </div>
                 {!isTemporaryFreeProduct && <BillingLimit product={product} />}
-                <div className="px-8 pb-8 sm:pb-0">
-                    <FeatureFlagUsageNotice product={product} />
-                </div>
+                <FeatureFlagUsageNotice product={product} />
             </div>
             <ProductPricingModal
                 modalOpen={isPricingModalOpen}
@@ -393,13 +391,15 @@ export const BillingProduct = ({ product }: { product: BillingProductV2Type }): 
 
 export const FeatureFlagUsageNotice = ({ product }: { product: BillingProductV2Type }): JSX.Element | null => {
     return product.type === 'feature_flags' ? (
-        <p className="mt-0 ml-0 text-sm text-secondary italic">
-            <IconInfo className="mr-1" />
-            Questions? Here's{' '}
-            <Link to="https://posthog.com/docs/feature-flags/common-questions#billing--usage" className="italic">
-                how we calculate usage
-            </Link>{' '}
-            for feature flags.
-        </p>
+        <div className="p-4 px-8 pb-8 sm:pb-0 border-t border-border">
+            <p className="mt-0 ml-0 text-sm text-secondary italic">
+                <IconInfo className="mr-1" />
+                Questions? Here's{' '}
+                <Link to="https://posthog.com/docs/feature-flags/common-questions#billing--usage" className="italic">
+                    how we calculate usage
+                </Link>{' '}
+                for feature flags.
+            </p>
+        </div>
     ) : null
 }
