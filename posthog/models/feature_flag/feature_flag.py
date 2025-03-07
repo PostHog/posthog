deffc08fc4e7a08cd553ab5c85a5bbbc90bf0d45
Phil Haack <haacked@gmail.com>
2025-03-06 17:57:29 -0800

    Use a custom signal to trigger the activity log
    This adds a new custom signal: `model_activity_signal` and a mixin `ModelActivityMixin`. Any model that has this mixin in its inheritance tree will automatically raise the `model_activity_signal` with a `before` and `after` instance.
    
    Then to log the activity, all you have to do is add a handler method to handle the signal by decorating a method with: `@receiver(model_activity_signal, sender=FeatureFlag)` replacing `FeatureFlag` with your model type.
    
    Right now, only FeatureFlag uses this, but I hope others can adapt this to their needs.
    
    Fixes #29448
    Fixes #29606

--- posthog/models/feature_flag/feature_flag.py
+++ posthog/models/feature_flag/feature_flag.py
@@ -8,6 +8,8 @@ from django.db import models
 from django.db.models.signals import post_delete, post_save
 from django.utils import timezone
 from posthog.exceptions_capture import capture_exception
+from posthog.models.signals import mutable_receiver
+from posthog.models.activity_logging.model_activity import ModelActivityMixin
 
 from posthog.constants import (
     ENRICHED_DASHBOARD_INSIGHT_IDENTIFIER,
@@ -16,14 +18,13 @@ from posthog.constants import (
 from posthog.models.cohort import Cohort, CohortOrEmpty
 from posthog.models.property import GroupTypeIndex
 from posthog.models.property.property import Property, PropertyGroup
-from posthog.models.signals import mutable_receiver
 
 FIVE_DAYS = 60 * 60 * 24 * 5  # 5 days in seconds
 
 logger = structlog.get_logger(__name__)
 
 
-class FeatureFlag(models.Model):
+class FeatureFlag(ModelActivityMixin, models.Model):
     # When adding new fields, make sure to update organization_feature_flags.py::copy_flags
     key = models.CharField(max_length=400)
     name = models.TextField(
