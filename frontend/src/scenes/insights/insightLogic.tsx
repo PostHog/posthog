0a7e8cefa079015f7ee90f595feb77b1143ab018
Lucas Ricoy <lucas.r@posthog.com>
2025-03-07 16:07:13 -0300

    add new implementation behind a FF

--- frontend/src/scenes/insights/insightLogic.tsx
+++ frontend/src/scenes/insights/insightLogic.tsx
@@ -338,9 +338,14 @@ export const insightLogic: LogicWrapper<insightLogicType> = kea<insightLogicType
             },
         ],
         showPersonsModal: [
-            (s) => [s.query, s.insightProps],
-            (query: Record<string, any>, insightProps: InsightLogicProps): boolean =>
-                !(query?.hidePersonsModal || insightProps?.query?.hidePersonsModal),
+            (s) => [s.query, s.insightProps, s.featureFlags],
+            (query: Record<string, any>, insightProps: InsightLogicProps, featureFlags: Record<string, boolean>): boolean => {
+                if (featureFlags[FEATURE_FLAGS.WEB_ANALYTICS_HIDE_MODAL_ACTORS]) {
+                    const theQuery = query || insightProps?.query
+                    return !theQuery || !theQuery.hidePersonsModal
+                }
+                return !query || !query.hidePersonsModal
+            },
         ],
         supportsCreatingExperiment: [
             (s) => [s.insight, s.activeScene],
