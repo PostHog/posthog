d5fb37ec93cde2313e0d8f9eb8ede1de95df9b96
Adam Leith <adamleithp@users.noreply.github.com>
2025-03-10 10:34:16 +0000

    some dropdown styling fixes

--- frontend/src/lib/ui/DropdownMenu/DropdownMenu.tsx
+++ frontend/src/lib/ui/DropdownMenu/DropdownMenu.tsx
@@ -89,7 +89,7 @@ const DropdownMenuCheckboxItem = React.forwardRef<
         <DropdownMenuPrimitive.CheckboxItem
             ref={ref}
             className={cn(
-                'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+                'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-fill-highlight-100 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
                 className
             )}
             checked={checked}
@@ -114,14 +114,14 @@ const DropdownMenuRadioItem = React.forwardRef<
         <DropdownMenuPrimitive.RadioItem
             ref={ref}
             className={cn(
-                'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+                'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-fill-highlight-100 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
                 className
             )}
             {...props}
         >
             <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
                 <DropdownMenuPrimitive.ItemIndicator>
-                    <div className="h-2 w-2 rounded-full bg-black" />
+                    <div className="h-2 w-2 rounded-full bg-black dark:bg-white" />
                 </DropdownMenuPrimitive.ItemIndicator>
             </span>
             {children}
