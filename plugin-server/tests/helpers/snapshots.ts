5334923ad8bbe96cf14e130ae352009bf85989bd
Ben White <ben@posthog.com>
2025-03-11 15:47:10 +0100

    fix

--- plugin-server/tests/helpers/snapshots.ts
+++ plugin-server/tests/helpers/snapshots.ts
@@ -1,13 +1,14 @@
 const UUID_REGEX = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/gi
 
 /**
- * Helper method that takes an object and replaces all UUIDs with placeholders
+ * Helper method that takes an object and replaces all UUIDs or given keyswith placeholders
  */
-
 export const forSnapshot = (
     obj: any,
     context?: { overrides?: Record<string, string>; idMap?: Record<string, string> }
 ) => {
+    context = context ?? {}
+    context.idMap = context.idMap ?? {}
     let res = obj
     if (Array.isArray(obj)) {
         res = obj.map((item) => forSnapshot(item, context))
@@ -27,10 +28,8 @@ export const forSnapshot = (
         // Replace UUIDs with placeholders
         const matches = obj.match(UUID_REGEX)
         for (const match of matches ?? []) {
-            res = res.replace(
-                match,
-                `<REPLACED-UUID-${context?.idMap?.[match] ?? Object.keys(context?.idMap ?? {}).length}>`
-            )
+            context.idMap[match] = context.idMap[match] ?? `<REPLACED-UUID-${Object.keys(context.idMap).length}>`
+            res = res.replace(match, context.idMap[match])
         }
     }
 
