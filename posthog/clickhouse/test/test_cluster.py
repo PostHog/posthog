e8fa78d87502dcc7f185b197ef919fab8cbc60e9
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2025-03-11 16:47:24 -0700

    refactor some tests

--- posthog/clickhouse/test/test_cluster.py
+++ posthog/clickhouse/test/test_cluster.py
@@ -136,10 +136,7 @@ def test_alter_mutation_single_command(cluster: ClickhouseCluster) -> None:
     count = 100
 
     # make sure there is some data to play with first
-    def populate_random_data(client: Client) -> None:
-        client.execute(f"INSERT INTO {table} SELECT * FROM generateRandom() LIMIT {count}")
-
-    cluster.map_one_host_per_shard(populate_random_data).result()
+    cluster.map_one_host_per_shard(Query(f"INSERT INTO {table} SELECT * FROM generateRandom() LIMIT {count}")).result()
 
     # construct the runner
     sentinel_uuid = uuid.uuid1()  # unique to this test run to ensure we have a clean slate
@@ -164,24 +161,20 @@ def test_alter_mutation_single_command(cluster: ClickhouseCluster) -> None:
     wait_and_check_mutations_on_shards(cluster, shard_mutations)
 
     # check to ensure data is as expected to be after update
-    def get_person_ids(client: Client) -> list[tuple[uuid.UUID, int]]:
-        return client.execute(f"SELECT person_id, count() FROM {table} GROUP BY ALL ORDER BY ALL")
-
     for host_info in shard_mutations.keys():
-        query_results = cluster.map_all_hosts_in_shard(host_info.shard_num, get_person_ids).result()
+        query_results = cluster.map_all_hosts_in_shard(
+            host_info.shard_num, Query(f"SELECT person_id, count() FROM {table} GROUP BY ALL ORDER BY ALL")
+        ).result()
         assert all(result == [(sentinel_uuid, count)] for result in query_results.values())
 
     # submitting a duplicate mutation should just return the original and not schedule anything new
-    def get_mutations_count(client: Client) -> int:
-        [[result]] = client.execute("SELECT count() FROM system.mutations")
-        return result
-
-    mutations_count_before = cluster.map_all_hosts(get_mutations_count).result()
+    get_mutations_count_query = Query("SELECT count() FROM system.mutations")
+    mutations_count_before = cluster.map_all_hosts(get_mutations_count_query).result()
 
     duplicate_mutations = cluster.map_one_host_per_shard(runner).result()
     assert shard_mutations == duplicate_mutations
 
-    assert cluster.map_all_hosts(get_mutations_count).result() == mutations_count_before
+    assert cluster.map_all_hosts(get_mutations_count_query).result() == mutations_count_before
 
     with pytest.raises(MutationNotFound):
         assert cluster.any_host(MutationWaiter("x", {"y"}).is_done).result()
@@ -192,10 +185,7 @@ def test_alter_mutation_multiple_commands(cluster: ClickhouseCluster) -> None:
     count = 100
 
     # make sure there is some data to play with first
-    def populate_random_data(client: Client) -> None:
-        client.execute(f"INSERT INTO {table} SELECT * FROM generateRandom() LIMIT {count}")
-
-    cluster.map_one_host_per_shard(populate_random_data).result()
+    cluster.map_one_host_per_shard(Query(f"INSERT INTO {table} SELECT * FROM generateRandom() LIMIT {count}")).result()
 
     sentinel_uuid = uuid.uuid1()  # unique to this test run to ensure we have a clean slate
 
