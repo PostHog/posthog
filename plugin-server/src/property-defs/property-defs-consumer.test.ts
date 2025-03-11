5e3c18597dd389655209b3debdc2e46b4d072a0a
Ben White <ben@posthog.com>
2025-03-11 14:32:22 +0100

    Refactor

--- plugin-server/src/property-defs/property-defs-consumer.test.ts
+++ plugin-server/src/property-defs/property-defs-consumer.test.ts
@@ -5,10 +5,10 @@ import { mockProducer } from '~/tests/helpers/mocks/producer.mock'
 import { resetTestDatabase } from '~/tests/helpers/sql'
 
 import { insertHogFunction as _insertHogFunction } from '../cdp/_tests/fixtures'
-import { ClickHouseEvent, Hub, PropertyType, RawClickHouseEvent, TimestampFormat } from '../types'
+import { ClickHouseEvent, Hub, RawClickHouseEvent, TimestampFormat } from '../types'
 import { closeHub, createHub } from '../utils/db/hub'
 import { castTimestampOrNow } from '../utils/utils'
-import { getPropertyType, PropertyDefsConsumer } from './property-defs-consumer'
+import { PropertyDefsConsumer } from './property-defs-consumer'
 
 const DEFAULT_TEST_TIMEOUT = 5000
 jest.setTimeout(DEFAULT_TEST_TIMEOUT)
@@ -132,57 +132,6 @@ describe('PropertyDefsConsumer', () => {
 
     afterAll(() => {
         jest.useRealTimers()
-    })
-
-    describe('property types', () => {
-        beforeEach(async () => {
-            ingester = new PropertyDefsConsumer(hub)
-            await ingester.start()
-        })
-
-        const testCases: [string, any, PropertyType | null][] = [
-            // Special key prefixes
-            ['utm_source', 'google', PropertyType.String],
-            ['utm_medium', 123, PropertyType.String],
-            ['$feature/my_flag', true, PropertyType.String],
-            ['$feature_flag_response', false, PropertyType.String],
-            ['$survey_response', 'yes', PropertyType.String],
-            ['$survey_response_2', 123, PropertyType.String],
-
-            // String values
-            ['key', 'hello', PropertyType.String],
-            ['key', 'true', PropertyType.Boolean],
-            ['key', 'false', PropertyType.Boolean],
-            ['key', 'TRUE', PropertyType.Boolean],
-            ['key', 'FALSE', PropertyType.Boolean],
-            ['key', '2024-01-01T00:00:00Z', PropertyType.DateTime],
-            ['key', '2024-01-01T00:00:00+00:00', PropertyType.DateTime],
-            ['key', 'invalid-date', PropertyType.String],
-
-            // Number values
-            ['key', 123, PropertyType.Numeric],
-            ['timestamp', 1234567890, PropertyType.DateTime],
-            ['TIME', 1234567890, PropertyType.DateTime],
-            ['key', 123.45, PropertyType.Numeric],
-            ['key', -123, PropertyType.Numeric],
-            ['key', 0, PropertyType.Numeric],
-            ['key', NaN, PropertyType.Numeric],
-            ['key', Infinity, PropertyType.Numeric],
-            ['key', -Infinity, PropertyType.Numeric],
-
-            // Boolean values
-            ['key', true, PropertyType.Boolean],
-            ['key', false, PropertyType.Boolean],
-
-            // Edge cases
-            ['key', null, null],
-            ['key', undefined, null],
-        ]
-        it.each(testCases)('should derive the correct property type for %s: %s', (key, value, expected) => {
-            const result = getPropertyType(key, value)
-
-            expect(result).toEqual(expected)
-        })
     })
 
     describe('property updates', () => {
--- plugin-server/src/property-defs/property-defs-consumer.ts
+++ plugin-server/src/property-defs/property-defs-consumer.ts
@@ -20,6 +20,12 @@ import {
 import { parseRawClickHouseEvent } from '../utils/event'
 import { status } from '../utils/status'
 import { PropertyDefsDB } from './services/property-defs-db'
+import {
+    getPropertyType,
+    PROPERTY_DEFS_PROPERTIES_TO_SKIP,
+    sanitizeEventName,
+    willFitInPostgres,
+} from './services/property-defs-utils'
 
 // Must require as `tsc` strips unused `import` statements and just requiring this seems to init some globals
 require('@sentry/tracing')
@@ -54,108 +60,6 @@ export type CollectedPropertyDefinitions = {
     eventDefinitionsById: Record<string, EventDefinitionType>
     propertyDefinitionsById: Record<string, PropertyDefinitionType>
     eventPropertiesById: Record<string, EventPropertyType>
-}
-
-// lifted from here:
-// https://github.com/PostHog/posthog/blob/021aaab04b4acd96cf8121c033ac3b0042492598/rust/property-defs-rs/src/types.rs#L457-L461
-const DJANGO_MAX_CHARFIELD_LENGTH = 200
-
-// These properties have special meaning, and are ignored
-const SKIP_PROPERTIES: string[] = [
-    '$set',
-    '$set_once',
-    '$unset',
-    '$group_0',
-    '$group_1',
-    '$group_2',
-    '$group_3',
-    '$group_4',
-    '$groups',
-]
-
-const DATE_PROP_KEYWORDS: string[] = ['time', 'timestamp', 'date', '_at', '-at', 'createdat', 'updatedat']
-
-export const getPropertyType = (rawKey: string, value: any): PropertyType | null => {
-    const key = rawKey.trim().toLowerCase()
-
-    // Special cases for certain property prefixes
-    if (key.startsWith('utm_')) {
-        // utm_ prefixed properties should always be detected as strings.
-        // Sometimes the first value sent looks like a number, even though
-        // subsequent values are not.
-        return PropertyType.String
-    }
-    if (key.startsWith('$feature/')) {
-        // $feature/ prefixed properties should always be detected as strings.
-        // These are feature flag values, and can be boolean or string.
-        // Sometimes the first value sent is boolean (because flag isn't enabled) while
-        // subsequent values are not. We don't want this to be misunderstood as a boolean.
-        return PropertyType.String
-    }
-
-    if (key === '$feature_flag_response') {
-        // $feature_flag_response properties should always be detected as strings.
-        // These are feature flag values, and can be boolean or string.
-        // Sometimes the first value sent is boolean (because flag isn't enabled) while
-        // subsequent values are not. We don't want this to be misunderstood as a boolean.
-        return PropertyType.String
-    }
-
-    if (key.startsWith('$survey_response')) {
-        // NB: $survey_responses are collected in an interesting way, where the first
-        // response is called `$survey_response` and subsequent responses are called
-        // `$survey_response_2`, `$survey_response_3`, etc. So, this check should auto-cast
-        // all survey responses to strings.
-        return PropertyType.String
-    }
-
-    if (typeof value === 'string') {
-        const s = value.trim()
-        if (s === 'true' || s === 'false') {
-            return PropertyType.Boolean
-        }
-        // Try to parse this as an ISO 8601 date
-        try {
-            if (DATE_PROP_KEYWORDS.some((kw) => key.includes(kw))) {
-                return PropertyType.DateTime
-            }
-            const date = DateTime.fromISO(s)
-            if (date.isValid) {
-                return PropertyType.DateTime
-            }
-            // TODO(eli): add speculative date string matching?
-        } catch {
-            // Not a valid date, continue to string type
-        }
-        return PropertyType.String
-    }
-
-    if (typeof value === 'boolean') {
-        return PropertyType.Boolean
-    }
-
-    if (typeof value === 'number') {
-        if (value >= sixMonthsAgoUnixSeconds()) {
-            return PropertyType.DateTime
-        }
-        return PropertyType.Numeric
-    }
-
-    return null
-}
-
-function willFitInPostgres(s: string) {
-    return s.length < DJANGO_MAX_CHARFIELD_LENGTH
-}
-
-function sanitizeEventName(eventName: string) {
-    return eventName.replace('\u0000', '\uFFFD')
-}
-
-function sixMonthsAgoUnixSeconds() {
-    const now = new Date()
-    now.setMonth(now.getMonth() - 6)
-    return Math.floor(now.getTime() / 1000)
 }
 
 /**
@@ -367,7 +271,7 @@ export class PropertyDefsConsumer {
                     continue
                 }
 
-                if (SKIP_PROPERTIES.includes(property)) {
+                if (PROPERTY_DEFS_PROPERTIES_TO_SKIP.includes(property)) {
                     // We don't need to count these as it is expected that they will be dropped
                     continue
                 }
