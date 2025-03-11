5d8443cbc3796d87a742d423799ae1ad4ea95f6b
Daniel Bachhuber <daniel.b@posthog.com>
2025-03-11 13:21:23 -0700

    Use blocking query requests on `/persons`

--- frontend/src/scenes/persons-management/tabs/Persons.tsx
+++ frontend/src/scenes/persons-management/tabs/Persons.tsx
@@ -1,11 +1,21 @@
 import { useActions, useValues } from 'kea'
+import { FEATURE_FLAGS } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { personsSceneLogic } from 'scenes/persons-management/tabs/personsSceneLogic'
 
 import { Query } from '~/queries/Query/Query'
-
 export function Persons(): JSX.Element {
     const { query } = useValues(personsSceneLogic)
     const { setQuery } = useActions(personsSceneLogic)
 
-    return <Query query={query} setQuery={setQuery} context={{ refresh: true }} dataAttr="persons-table" />
+    const { featureFlags } = useValues(featureFlagLogic)
+
+    return (
+        <Query
+            query={query}
+            setQuery={setQuery}
+            context={{ refresh: featureFlags[FEATURE_FLAGS.CRM_BLOCKING_QUERIES] === 'test' ? 'blocking' : true }}
+            dataAttr="persons-table"
+        />
+    )
 }
