d158f1ce464f7d2d7c9b898f48e65c86feec767b
Paul D'Ambra <paul.dambra@gmail.com>
2025-03-12 18:34:03 +0000

    Revert "feat: sort by count recency when counting (#29669)"
    This reverts commit 2a46b4876090d5a18651489601df3b456ed7b9c8.

--- ee/session_recordings/playlist_counters/recordings_that_match_playlist_filters.py
+++ ee/session_recordings/playlist_counters/recordings_that_match_playlist_filters.py
@@ -12,7 +12,6 @@ from posthog.session_recordings.session_recording_api import list_recordings_fro
 from posthog.tasks.utils import CeleryQueue
 from posthog.redis import get_client
 from posthog.schema import RecordingsQuery, FilterLogicalOperator, PropertyOperator, PropertyFilterType
-from django.db.models import F
 
 from structlog import get_logger
 
@@ -289,20 +288,17 @@ def count_recordings_that_match_playlist_filters(playlist_id: int) -> None:
                 query, user=None, team=playlist.team
             )
 
-            counted_at_date = datetime.now()
             value_to_set = json.dumps(
                 {
                     "session_ids": [r.session_id for r in recordings],
                     "has_more": more_recordings_available,
                     "previous_ids": existing_value.get("session_ids", None),
-                    "refreshed_at": counted_at_date.isoformat(),
+                    "refreshed_at": datetime.now().isoformat(),
                 }
             )
             redis_client.setex(
                 f"{PLAYLIST_COUNT_REDIS_PREFIX}{playlist.short_id}", THIRTY_SIX_HOURS_IN_SECONDS, value_to_set
             )
-            playlist.last_counted_at = counted_at_date
-            playlist.save(update_fields=["last_counted_at"])
 
             REPLAY_TEAM_PLAYLIST_COUNT_SUCCEEDED.inc()
     except SessionRecordingPlaylist.DoesNotExist:
@@ -352,8 +348,7 @@ def enqueue_recordings_that_match_playlist_filters() -> None:
 
     all_playlists = SessionRecordingPlaylist.objects.filter(
         team_id__lte=int(settings.PLAYLIST_COUNTER_PROCESSING_MAX_ALLOWED_TEAM_ID), deleted=False, filters__isnull=False
-    ).order_by(F("last_counted_at").asc(nulls_first=True))
-
+    )
     REPLAY_TEAM_PLAYLISTS_IN_TEAM_COUNT.inc(all_playlists.count())
 
     for playlist in all_playlists:
