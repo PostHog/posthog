c1dd658ded75e9d5d97ad37f55096f69de06d456
David Newell <david@posthog.com>
2025-03-12 17:05:13 +0000

    recording status

--- posthog/hogql/functions/test/test_recording_button.py
+++ posthog/hogql/functions/test/test_recording_button.py
@@ -5,16 +5,17 @@ from posthog.test.base import BaseTest
 
 class TestRecordingButton(BaseTest):
     def test_recording_button(self):
-        response = execute_hogql_query("select recording_button('12345-6789') from events", self.team, pretty=False)
-        # self.assertEqual(
-        #     response.clickhouse,
-        #     f"SELECT tuple(%(hogql_val_0)s, %(hogql_val_1)s, %(hogql_val_2)s, %(hogql_val_3)s, %(hogql_val_4)s, replaceRegexpAll(nullIf(nullIf(JSONExtractRaw(events.properties, %(hogql_val_5)s), ''), 'null'), '^"
-        #     | "$', '')) FROM events WHERE equals(events.team_id, 221) LIMIT 100 SETTINGS readonly=2, max_execution_time=60, allow_experimental_object_type=1, format_csv_allow_double_quotes=0, max_ast_elements=4000000, max_expanded_ast_elements=4000000, max_bytes_before_external_group_by=0",
-        # )
-        # self.assertEqual(
-        #     response.hogql,
-        #     f"SELECT tuple('__hx_tag', 'RecordingButton', 'sessionId', '12345-6789', 'recordingStatus', 'sql(properties.$current_url)') LIMIT 100",
-        # )
+        response = execute_hogql_query(
+            """
+            select recording_button('12345-6789') from (SELECT '{"$recording_status":"active"}' AS properties)
+            """,
+            self.team,
+            pretty=False,
+        )
+        self.assertEqual(
+            response.hogql,
+            f"SELECT tuple('__hx_tag', 'RecordingButton', 'sessionId', '12345-6789', 'recordingStatus', 'sql(properties.$recording_status)') LIMIT 100",
+        )
         self.assertEqual(
             response.results,
             (
@@ -23,7 +24,7 @@ class TestRecordingButton(BaseTest):
                 "sessionId",
                 "12345-6789",
                 "recordingStatus",
-                "sql(properties.$current_url)",
+                "sql(properties.$recording_status)",
             ),
         )
 
