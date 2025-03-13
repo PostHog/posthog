f001ed56f3e376adec55d5ab7e7b9b399dfaf048
Daniel Bachhuber <daniel.b@posthog.com>
2025-03-13 13:41:49 -0700

    Quickly copy the group id

--- frontend/src/queries/nodes/DataTable/renderColumn.tsx
+++ frontend/src/queries/nodes/DataTable/renderColumn.tsx
@@ -281,6 +281,16 @@ export function renderColumn(
                 {String(value)}
             </CopyToClipboardInline>
         )
+    } else if (key === 'key' && isGroupsQuery(query.source)) {
+        return (
+            <CopyToClipboardInline
+                explicitValue={String(value)}
+                iconStyle={{ color: 'var(--accent-primary)' }}
+                description="group id"
+            >
+                {String(value)}
+            </CopyToClipboardInline>
+        )
     } else if (key === 'group_name' && isGroupsQuery(query.source)) {
         const key = (record as any[])[1] // 'key' is the second column in the groups query
         return <Link to={urls.group(query.source.group_type_index, key, false)}>{value}</Link>
