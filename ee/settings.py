fd206e4b222f3f111edc8dd93de880ef0772559d
Zach Waterfield <zlwaterfield@gmail.com>
2025-03-14 11:46:19 +0000

    Update settings.py

--- ee/settings.py
+++ ee/settings.py
@@ -82,7 +82,7 @@ INKEEP_API_KEY = get_from_env("INKEEP_API_KEY", "")
 
 SQS_QUEUES = {
     "usage_reports": {
-        "url": get_from_env("SQS_USAGE_REPORT_QUEUE_URL", "", optional=True),
+        "url": get_from_env("SQS_USAGE_REPORT_QUEUE_URL", optional=True),
         "region": get_from_env("SQS_REGION", "us-east-1", optional=True),
         "type": "usage_reports",
     }
