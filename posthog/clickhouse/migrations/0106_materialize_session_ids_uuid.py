0434ed12058a1481f940e0bb21faf04cfcaf82a7
Robbie Coomber <robbie.coomber@gmail.com>
2025-03-14 11:26:00 +0000

    Fix after accepting changes

--- posthog/clickhouse/migrations/0106_materialize_session_ids_uuid.py
+++ posthog/clickhouse/migrations/0106_materialize_session_ids_uuid.py
@@ -1,8 +1,7 @@
 from infi.clickhouse_orm import migrations
 
-from posthog.clickhouse.client.connection import get_client_from_pool
-from posthog.clickhouse.kafka_engine import trim_quotes_expr
-from posthog.settings import CLICKHOUSE_CLUSTER
+from posthog.clickhouse.client.connection import NodeRole
+from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 
 
 ADD_COLUMNS_SHARDED_EVENTS = f"""
@@ -17,8 +16,8 @@ ADD COLUMN IF NOT EXISTS $session_id_uuid Nullable(UInt128)
 
 
 def add_columns_to_required_tables(_):
-run_sql_with_exceptions(ADD_COLUMNS_SHARDED_EVENTS.format(table="sharded_events"))
-run_sql_with_exceptions(ADD_COLUMNS_EVENTS.format(table="events"), node_role=NodeRole.ALL)
+    run_sql_with_exceptions(ADD_COLUMNS_SHARDED_EVENTS.format(table="sharded_events"))
+    run_sql_with_exceptions(ADD_COLUMNS_EVENTS.format(table="events"), node_role=NodeRole.ALL)
 
 
 operations = [
