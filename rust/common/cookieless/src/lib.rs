86ad5e40b58c0007551eb66d029b9fc8b24d8cf3
Robbie Coomber <robbie.coomber@gmail.com>
2025-03-15 15:55:17 +0000

    Partially implement the cookieless manager

--- rust/common/cookieless/src/lib.rs
+++ rust/common/cookieless/src/lib.rs
@@ -1,9 +1,15 @@
+mod constants;
 mod hash;
+mod manager;
 mod metrics;
 mod salt_cache;
 
+pub use constants::*;
 pub use hash::{do_hash, HashError};
-pub use salt_cache::{SaltCache, SaltCacheError, is_calendar_date_valid};
+pub use manager::{
+    CookielessConfig, CookielessManager, CookielessManagerError, HashParams,
+};
+pub use salt_cache::{is_calendar_date_valid, SaltCache, SaltCacheError};
 
 #[cfg(feature = "examples")]
 pub mod examples;
