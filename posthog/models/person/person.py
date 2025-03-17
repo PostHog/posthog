22becb01e168e8105e7f2c7541abfdf25a548dc7
dylan <dylan@posthog.com>
2025-03-16 21:26:55 -0700

    lord

--- posthog/models/person/person.py
+++ posthog/models/person/person.py
@@ -27,7 +27,6 @@ class PersonManager(models.Manager):
 
 
 class Person(models.Model):
-    id = models.BigAutoField(primary_key=True)
     _distinct_ids: Optional[list[str]]
 
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
@@ -119,7 +118,6 @@ class Person(models.Model):
 
 
 class PersonDistinctId(models.Model):
-    id = models.BigAutoField(primary_key=True)
     team = models.ForeignKey("Team", on_delete=models.CASCADE, db_index=False)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     distinct_id = models.CharField(max_length=400)
