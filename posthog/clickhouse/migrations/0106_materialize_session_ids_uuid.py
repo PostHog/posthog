def160d0e24b351a7181ae437e34748e31d8739d
Marius Andra <marius.andra@gmail.com>
2025-03-18 13:19:13 +0100

    baseline

--- posthog/clickhouse/migrations/0106_materialize_session_ids_uuid.py
+++ posthog/clickhouse/migrations/0106_materialize_session_ids_uuid.py
@@ -1,2 +1,4 @@
-# migration did not run, superceded by 0107_materialize_session_ids_uuid
-operations: list = []
+from typing import Any
+
+# migration did not run
+operations: list[Any] = []
--- posthog/clickhouse/migrations/0107_materialize_session_ids_uuid.py
+++ posthog/clickhouse/migrations/0107_materialize_session_ids_uuid.py
@@ -1,2 +1,4 @@
+from typing import Any
+
 # migration did not run
-operations: list = []
+operations: list[Any] = []
