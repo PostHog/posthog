003e49bb37272c542d9d24a4b8a53e70ae2de68e
meikel <meikel@posthog.com>
2025-03-18 14:12:05 +0100

    update

--- frontend/src/scenes/pipeline/hogfunctions/hogFunctionConfigurationLogic.tsx
+++ frontend/src/scenes/pipeline/hogfunctions/hogFunctionConfigurationLogic.tsx
@@ -64,7 +64,7 @@ export interface HogFunctionConfigurationLogicProps {
 }
 
 export const EVENT_VOLUME_DAILY_WARNING_THRESHOLD = 1000
-export const UNSAVED_CONFIGURATION_TTL = 1000 * 60 * 5
+const UNSAVED_CONFIGURATION_TTL = 1000 * 60 * 5
 
 const VALIDATION_RULES = {
     SITE_DESTINATION_REQUIRES_MAPPINGS: (data: HogFunctionConfigurationType) =>
