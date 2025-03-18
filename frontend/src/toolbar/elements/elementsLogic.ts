a07ca45ee665057f83f07d66bd213231e8c7bb55
Paul D'Ambra <paul@posthog.com>
2025-03-18 09:56:43 +0000

    Update frontend/src/toolbar/elements/elementsLogic.ts

--- frontend/src/toolbar/elements/elementsLogic.ts
+++ frontend/src/toolbar/elements/elementsLogic.ts
@@ -313,6 +313,8 @@ export const elementsLogic = kea<elementsLogicType>([
                     .filter(({ rect }) => rect && (rect.width !== 0 || rect.height !== 0))
                     .map((element) => ({
                         ...element,
+                        // being able to hover over elements might rely on their original z-index
+	                // so we copy it over to the toolbar element
                         apparentZIndex: getMaxZIndex(element.element),
                     }))
             },
