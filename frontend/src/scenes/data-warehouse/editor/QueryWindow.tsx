864ae7c35bbc0d33749147a3ee791d72299d4273
eric <eeoneric@gmail.com>
2025-03-19 12:52:40 -0400

    deep equality

--- frontend/src/scenes/data-warehouse/editor/QueryWindow.tsx
+++ frontend/src/scenes/data-warehouse/editor/QueryWindow.tsx
@@ -48,7 +48,7 @@ export function QueryWindow({ onSetMonacoAndEditor }: QueryWindowProps): JSX.Ele
     return (
         <div className="flex flex-1 flex-col h-full overflow-hidden">
             <div className="flex flex-row overflow-x-auto px-1">
-                {sidebarWidth == 0 && (
+                {sidebarWidth === 0 && (
                     <LemonButton
                         onClick={() => resetDefaultSidebarWidth()}
                         className="mt-1 mr-1"
