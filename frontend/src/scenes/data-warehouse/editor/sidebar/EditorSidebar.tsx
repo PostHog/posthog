0e53f821e728196ac6357817031b8f0d85efb11d
eric <eeoneric@gmail.com>
2025-03-18 22:46:26 -0400

    move component

--- frontend/src/scenes/data-warehouse/editor/sidebar/EditorSidebar.tsx
+++ frontend/src/scenes/data-warehouse/editor/sidebar/EditorSidebar.tsx
@@ -1,25 +1,16 @@
 import { IconBrackets, IconInfo, IconServer } from '@posthog/icons'
-import { IconArrowLeft, IconEllipsis } from '@posthog/icons'
 import { Tooltip } from '@posthog/lemon-ui'
 import { useActions, useValues } from 'kea'
-import { CopyToClipboardInline } from 'lib/components/CopyToClipboard'
-import { DatabaseTableTree } from 'lib/components/DatabaseTableTree/DatabaseTableTree'
 import { Resizer } from 'lib/components/Resizer/Resizer'
-import { LemonButton } from 'lib/lemon-ui/LemonButton'
-import { LemonMenu } from 'lib/lemon-ui/LemonMenu'
 import { LemonTabs } from 'lib/lemon-ui/LemonTabs'
 import { useEffect, useState } from 'react'
-import { viewLinkLogic } from 'scenes/data-warehouse/viewLinkLogic'
-import { Scene } from 'scenes/sceneTypes'
 
-import { Sidebar } from '~/layout/navigation-3000/components/Sidebar'
 import { navigation3000Logic } from '~/layout/navigation-3000/navigationLogic'
-import { SidebarNavbarItem } from '~/layout/navigation-3000/types'
 import { variablesLogic } from '~/queries/nodes/DataVisualization/Components/Variables/variablesLogic'
 
 import { editorSceneLogic } from '../editorSceneLogic'
-import { editorSidebarLogic } from '../editorSidebarLogic'
 import { editorSizingLogic } from '../editorSizingLogic'
+import { QueryDatabase } from './QueryDatabase'
 import { QueryInfo } from './QueryInfo'
 import { QueryVariables } from './QueryVariables'
 
@@ -86,13 +77,13 @@ export const EditorSidebar = ({
     const renderTabContent = (): JSX.Element => {
         switch (activeTab) {
             case 'query_database':
-                return <DatabaseExplorer isOpen={sidebarOverlayOpen} />
+                return <QueryDatabase isOpen={sidebarOverlayOpen} />
             case 'query_variables':
                 return <QueryVariables />
             case 'query_info':
                 return <QueryInfo codeEditorKey={codeEditorKey} />
             default:
-                return <DatabaseExplorer isOpen={sidebarOverlayOpen} />
+                return <QueryDatabase isOpen={sidebarOverlayOpen} />
         }
     }
 
@@ -116,64 +107,6 @@ export const EditorSidebar = ({
             </div>
             {renderTabContent()}
             <Resizer {...editorSizingLogicProps.sidebarResizerProps} offset={0} />
-        </div>
-    )
-}
-
-// Tab 1 component - Database Explorer
-const DatabaseExplorer = ({ isOpen }: { isOpen: boolean }): JSX.Element => {
-    const navBarItem: SidebarNavbarItem = {
-        identifier: Scene.SQLEditor,
-        label: 'SQL editor',
-        icon: <IconServer />,
-        logic: editorSidebarLogic,
-    }
-
-    return (
-        <Sidebar navbarItem={navBarItem} sidebarOverlay={<EditorSidebarOverlay />} sidebarOverlayProps={{ isOpen }} />
-    )
-}
-const EditorSidebarOverlay = (): JSX.Element => {
-    const { setSidebarOverlayOpen } = useActions(editorSceneLogic)
-    const { sidebarOverlayTreeItems, selectedSchema } = useValues(editorSceneLogic)
-    const { toggleJoinTableModal, selectSourceTable } = useActions(viewLinkLogic)
-
-    return (
-        <div className="flex flex-col h-full">
-            <header className="flex flex-row items-center h-10 border-b shrink-0 p-1 gap-2">
-                <LemonButton size="small" icon={<IconArrowLeft />} onClick={() => setSidebarOverlayOpen(false)} />
-                {selectedSchema?.name && (
-                    <CopyToClipboardInline
-                        className="font-mono"
-                        tooltipMessage={null}
-                        description="schema"
-                        iconStyle={{ color: 'var(--text-secondary)' }}
-                        explicitValue={selectedSchema?.name}
-                    >
-                        {selectedSchema?.name}
-                    </CopyToClipboardInline>
-                )}
-                <LemonMenu
-                    items={[
-                        {
-                            label: 'Add join',
-                            onClick: () => {
-                                if (selectedSchema) {
-                                    selectSourceTable(selectedSchema.name)
-                                    toggleJoinTableModal()
-                                }
-                            },
-                        },
-                    ]}
-                >
-                    <div className="absolute right-1 flex">
-                        <LemonButton size="small" noPadding icon={<IconEllipsis />} />
-                    </div>
-                </LemonMenu>
-            </header>
-            <div className="overflow-y-auto flex-1">
-                <DatabaseTableTree items={sidebarOverlayTreeItems} />
-            </div>
         </div>
     )
 }
