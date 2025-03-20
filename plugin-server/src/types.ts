6856d3017ef3c344246baf1deb44371083a42a36
Paweł Ledwoń <me@difficult.name>
2025-03-20 10:12:41 +0100

    refactor: make console log store sync configurable

--- plugin-server/src/types.ts
+++ plugin-server/src/types.ts
@@ -331,6 +331,7 @@ export interface PluginsServerConfig extends CdpConfig, IngestionConsumerConfig 
     SESSION_RECORDING_V2_S3_SECRET_ACCESS_KEY: string
     SESSION_RECORDING_V2_S3_TIMEOUT_MS: number
     SESSION_RECORDING_V2_CONSOLE_LOG_ENTRIES_KAFKA_TOPIC: string
+    SESSION_RECORDING_V2_CONSOLE_LOG_STORE_SYNC_BATCH_LIMIT: number
 
     // Destination Migration Diffing
     DESTINATION_MIGRATION_DIFFING_ENABLED: boolean
