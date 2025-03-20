92f5228468b731ef6d3937045932825a41f1c025
Rodrigo Iloro <rodrigo@posthog.com>
2025-03-20 19:36:26 -0300

    tests(lemon-ui): adds tests to cover future date limit.

--- frontend/src/lib/lemon-ui/LemonCalendar/LemonCalendarSelect.test.tsx
+++ frontend/src/lib/lemon-ui/LemonCalendar/LemonCalendarSelect.test.tsx
@@ -181,6 +181,79 @@ describe('LemonCalendarSelect', () => {
         expect(onChange).toHaveBeenLastCalledWith(dayjs('2023-01-10T20:22:00.000Z'))
     })
 
+    test('allow only upcoming selection after a limit (one day in the future)', async () => {
+        const onClose = jest.fn()
+        const onChange = jest.fn()
+        window.HTMLElement.prototype.scrollIntoView = jest.fn()
+
+        jest.useFakeTimers().setSystemTime(new Date('2023-01-10 17:22:08'))
+
+        function TestSelect(): JSX.Element {
+            const [value, setValue] = useState<dayjs.Dayjs | null>(null)
+            return (
+                <LemonCalendarSelect
+                    months={1}
+                    value={value}
+                    onClose={onClose}
+                    onChange={(value) => {
+                        setValue(value)
+                        onChange(value)
+                    }}
+                    granularity="minute"
+                    selectionPeriod="upcoming"
+                    selectionPeriodLimit={dayjs('2023-01-11')}
+                />
+            )
+        }
+        const { container } = render(<TestSelect />)
+
+        async function clickOnDate(day: string): Promise<void> {
+            const element = container.querySelector('.LemonCalendar__month') as HTMLElement
+            if (element) {
+                userEvent.click(await within(element).findByText(day))
+                userEvent.click(getByDataAttr(container, 'lemon-calendar-select-apply'))
+            }
+        }
+
+        async function clickOnTime(props: GetLemonButtonTimePropsOpts): Promise<void> {
+            const element = getTimeElement(container.querySelector('.LemonCalendar__time'), props)
+            if (element) {
+                userEvent.click(element)
+                userEvent.click(getByDataAttr(container, 'lemon-calendar-select-apply'))
+            }
+        }
+
+        // click on minute
+        await clickOnTime({ unit: 'm', value: 42 })
+        // time is disabled until a date is clicked
+        expect(onChange).not.toHaveBeenCalled()
+
+        // click on past date
+        await clickOnDate('9')
+        // cannot select a date in the past
+        expect(onChange).not.toHaveBeenCalled()
+
+        // click on future date beyond the limit
+        await clickOnDate('12')
+        // cannot select a date in the future
+        expect(onChange).not.toHaveBeenCalled()
+
+        // click on current date
+        await clickOnDate('10')
+        // chooses the current date and sets the time to the current hour and minute
+        expect(onChange).toHaveBeenCalledWith(dayjs('2023-01-10T17:22:00.000Z'))
+
+        // click on an earlier hour
+        await clickOnTime({ unit: 'a', value: 'am' })
+        // does not update the date because it is in the past
+        expect(onChange).toHaveBeenLastCalledWith(dayjs('2023-01-10T17:22:00.000Z'))
+
+        // click on a later hour
+        await clickOnTime({ unit: 'h', value: '8' })
+        // updates the hour to 8pm (later than 5pm)
+        expect(onChange).toHaveBeenLastCalledWith(dayjs('2023-01-10T20:22:00.000Z'))
+    })
+
     test('only allow past selection', async () => {
         const onClose = jest.fn()
         const onChange = jest.fn()
