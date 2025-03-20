3bec5b02f6f2ca0b8a6b82405e2a7ad297e59ba1
David Newell <david@posthog.com>
2025-03-20 14:30:57 +0000

    remove feature flag guards

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -186,7 +186,6 @@ export const FEATURE_FLAGS = {
     HOG_FUNCTIONS_LINKED: 'hog-functions-linked', // owner: #team-cdp
     PERSONLESS_EVENTS_NOT_SUPPORTED: 'personless-events-not-supported', // owner: @raquelmsmith
     ALERTS: 'alerts', // owner: @anirudhpillai #team-product-analytics
-    ERROR_TRACKING: 'error-tracking', // owner: #team-error-tracking
     SETTINGS_BOUNCE_RATE_PAGE_VIEW_MODE: 'settings-bounce-rate-page-view-mode', // owner: @robbie-c
     ONBOARDING_DASHBOARD_TEMPLATES: 'onboarding-dashboard-templates', // owner: @raquelmsmith
     MULTIPLE_BREAKDOWNS: 'multiple-breakdowns', // owner: @skoob13 #team-product-analytics
--- frontend/src/scenes/web-analytics/webAnalyticsLogic.tsx
+++ frontend/src/scenes/web-analytics/webAnalyticsLogic.tsx
@@ -1889,7 +1889,7 @@ export const webAnalyticsLogic = kea<webAnalyticsLogicType>([
                               },
                           }
                         : null,
-                    !conversionGoal && featureFlags[FEATURE_FLAGS.ERROR_TRACKING]
+                    !conversionGoal
                         ? {
                               kind: 'error_tracking',
                               tileId: TileId.ERROR_TRACKING,
