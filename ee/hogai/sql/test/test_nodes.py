4cbc396bbc26bece22ce8cba0b14544ce12cd0f2
Michael Matloka <michael@matloka.com>
2025-03-20 23:04:46 +0100

    Fix test_node_runs

--- ee/hogai/sql/test/test_nodes.py
+++ ee/hogai/sql/test/test_nodes.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from langchain_core.messages import AIMessage
 from langchain_core.runnables import RunnableLambda
 
-from ee.hogai.sql.nodes import SQLGeneratorNode, SQLPlannerNode, SQLSchemaGeneratorOutput
+from ee.hogai.sql.nodes import SQLGeneratorNode, SQLPlannerNode
 from ee.hogai.utils.types import AssistantState, PartialAssistantState
 from posthog.schema import (
     AssistantHogQLQuery,
@@ -29,16 +29,11 @@ class TestSQLPlannerNode(BaseTest):
 class TestSQLGeneratorNode(BaseTest):
     maxDiff = None
 
-    def setUp(self):
-        super().setUp()
-        self.schema = AssistantHogQLQuery(query="SELECT 1")
-
     def test_node_runs(self):
         node = SQLGeneratorNode(self.team)
         with patch.object(SQLGeneratorNode, "_model") as generator_model_mock:
-            generator_model_mock.return_value = RunnableLambda(
-                lambda _: SQLSchemaGeneratorOutput(query=self.schema).model_dump()
-            )
+            answer = AssistantHogQLQuery(query="SELECT 1")
+            generator_model_mock.return_value = RunnableLambda(lambda _: answer.model_dump())
             new_state = node.run(
                 AssistantState(
                     messages=[HumanMessage(content="Text")],
@@ -47,15 +42,16 @@ class TestSQLGeneratorNode(BaseTest):
                 ),
                 {},
             )
-            self.assertEqual(
-                new_state,
-                PartialAssistantState(
-                    messages=[
-                        VisualizationMessage(
-                            query="question", answer=self.schema, plan="Plan", id=new_state.messages[0].id
-                        )
-                    ],
-                    intermediate_steps=[],
-                    plan="",
-                ),
+
+            assert new_state == PartialAssistantState(
+                messages=[
+                    VisualizationMessage(
+                        query="question",
+                        answer=answer,
+                        plan="Plan",
+                        id=new_state.messages[0].id if new_state.messages else None,
+                    )
+                ],
+                intermediate_steps=[],
+                plan="",
             )
