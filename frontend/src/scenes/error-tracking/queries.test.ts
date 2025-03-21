718ec9f0e8f4ba4949a9333d72d99c5eec2e158b
Hugues Pouillot <hpouillot@gmail.com>
2025-03-21 11:05:30 +0100

    remove unused quotes

--- frontend/src/scenes/error-tracking/queries.test.ts
+++ frontend/src/scenes/error-tracking/queries.test.ts
@@ -17,65 +17,65 @@ describe('generateSparklineProps', () => {
     it('1h', async () => {
         const labels = getSparklineLabels('1h')
         expect(labels.length).toEqual(60)
-        expect(labels[0]).toEqual(`'10 Jan, 2023 16:23 (UTC)'`)
-        expect(labels[58]).toEqual(`'10 Jan, 2023 17:21 (UTC)'`)
-        expect(labels[59]).toEqual(`'10 Jan, 2023 17:22 (UTC)'`) // start of minute
+        expect(labels[0]).toEqual('10 Jan, 2023 16:23 (UTC)')
+        expect(labels[58]).toEqual('10 Jan, 2023 17:21 (UTC)')
+        expect(labels[59]).toEqual('10 Jan, 2023 17:22 (UTC)') // start of minute
     })
 
     it('24h', async () => {
         const labels = getSparklineLabels('24h')
         expect(labels.length).toEqual(24)
-        expect(labels[0]).toEqual(`'9 Jan, 2023 18:00 (UTC)'`)
-        expect(labels[22]).toEqual(`'10 Jan, 2023 16:00 (UTC)'`)
-        expect(labels[23]).toEqual(`'10 Jan, 2023 17:00 (UTC)'`) // start of hour
+        expect(labels[0]).toEqual('9 Jan, 2023 18:00 (UTC)')
+        expect(labels[22]).toEqual('10 Jan, 2023 16:00 (UTC)')
+        expect(labels[23]).toEqual('10 Jan, 2023 17:00 (UTC)') // start of hour
     })
 
     it('7d', async () => {
         const labels = getSparklineLabels('7d')
         expect(labels.length).toEqual(168)
-        expect(labels[0]).toEqual(`'3 Jan, 2023 18:00 (UTC)'`)
-        expect(labels[166]).toEqual(`'10 Jan, 2023 16:00 (UTC)'`)
-        expect(labels[167]).toEqual(`'10 Jan, 2023 17:00 (UTC)'`) // start of hour
+        expect(labels[0]).toEqual('3 Jan, 2023 18:00 (UTC)')
+        expect(labels[166]).toEqual('10 Jan, 2023 16:00 (UTC)')
+        expect(labels[167]).toEqual('10 Jan, 2023 17:00 (UTC)') // start of hour
     })
 
     it('14d', async () => {
         const labels = getSparklineLabels('14d')
         expect(labels.length).toEqual(336)
-        expect(labels[0]).toEqual(`'27 Dec, 2022 18:00 (UTC)'`)
-        expect(labels[334]).toEqual(`'10 Jan, 2023 16:00 (UTC)'`)
-        expect(labels[335]).toEqual(`'10 Jan, 2023 17:00 (UTC)'`) // start of hour
+        expect(labels[0]).toEqual('27 Dec, 2022 18:00 (UTC)')
+        expect(labels[334]).toEqual('10 Jan, 2023 16:00 (UTC)')
+        expect(labels[335]).toEqual('10 Jan, 2023 17:00 (UTC)') // start of hour
     })
 
     it('90d', async () => {
         const labels = getSparklineLabels('90d')
         expect(labels.length).toEqual(90)
-        expect(labels[0]).toEqual(`'13 Oct, 2022 00:00 (UTC)'`)
-        expect(labels[88]).toEqual(`'9 Jan, 2023 00:00 (UTC)'`)
-        expect(labels[89]).toEqual(`'10 Jan, 2023 00:00 (UTC)'`) // start of day
+        expect(labels[0]).toEqual('13 Oct, 2022 00:00 (UTC)')
+        expect(labels[88]).toEqual('9 Jan, 2023 00:00 (UTC)')
+        expect(labels[89]).toEqual('10 Jan, 2023 00:00 (UTC)') // start of day
     })
 
     it('180d', async () => {
         const labels = getSparklineLabels('180d')
         expect(labels.length).toEqual(26)
-        expect(labels[0]).toEqual(`'17 Jul, 2022 00:00 (UTC)'`)
-        expect(labels[24]).toEqual(`'1 Jan, 2023 00:00 (UTC)'`)
-        expect(labels[25]).toEqual(`'8 Jan, 2023 00:00 (UTC)'`) // start of week
+        expect(labels[0]).toEqual('17 Jul, 2022 00:00 (UTC)')
+        expect(labels[24]).toEqual('1 Jan, 2023 00:00 (UTC)')
+        expect(labels[25]).toEqual('8 Jan, 2023 00:00 (UTC)') // start of week
     })
 
     it('mStart', async () => {
         const labels = getSparklineLabels('mStart')
         expect(labels.length).toEqual(31)
-        expect(labels[0]).toEqual(`'11 Dec, 2022 00:00 (UTC)'`) // goes back one full month
-        expect(labels[29]).toEqual(`'9 Jan, 2023 00:00 (UTC)'`)
-        expect(labels[30]).toEqual(`'10 Jan, 2023 00:00 (UTC)'`) // start of week
+        expect(labels[0]).toEqual('11 Dec, 2022 00:00 (UTC)') // goes back one full month
+        expect(labels[29]).toEqual('9 Jan, 2023 00:00 (UTC)')
+        expect(labels[30]).toEqual('10 Jan, 2023 00:00 (UTC)') // start of week
     })
 
     it('yStart', async () => {
         const labels = getSparklineLabels('yStart')
         expect(labels.length).toEqual(52)
-        expect(labels[0]).toEqual(`'16 Jan, 2022 00:00 (UTC)'`) // goes back one full year
-        expect(labels[50]).toEqual(`'1 Jan, 2023 00:00 (UTC)'`)
-        expect(labels[51]).toEqual(`'8 Jan, 2023 00:00 (UTC)'`) // start of week
+        expect(labels[0]).toEqual('16 Jan, 2022 00:00 (UTC)') // goes back one full year
+        expect(labels[50]).toEqual('1 Jan, 2023 00:00 (UTC)')
+        expect(labels[51]).toEqual('8 Jan, 2023 00:00 (UTC)') // start of week
     })
 })
 
