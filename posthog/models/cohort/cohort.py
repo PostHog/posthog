fa8a2e73796ac76c9f7f69b0b32ae1a3cca2ea36
Alexander Spicer <aspicer@gmail.com>
2025-03-25 16:32:00 -0700

    refactor

--- posthog/models/cohort/cohort.py
+++ posthog/models/cohort/cohort.py
@@ -222,6 +222,13 @@ class Cohort(FileSystemSyncMixin, models.Model):
         from posthog.models.cohort.util import recalculate_cohortpeople
         from posthog.tasks.calculate_cohort import clear_stale_cohort
 
+        use_hogql_cohorts = posthoganalytics.feature_enabled(
+            "enable_hogql_cohort_calculation",
+            str(self.team.organization_id),
+            groups={"organization": str(self.team.organization_id)},
+            group_properties={"organization": {"id": str(self.team.organization_id)}},
+        )
+
         logger.warn(
             "cohort_calculation_started",
             id=self.pk,
@@ -231,7 +238,9 @@ class Cohort(FileSystemSyncMixin, models.Model):
         start_time = time.monotonic()
 
         try:
-            count = recalculate_cohortpeople(self, pending_version, initiating_user_id=initiating_user_id)
+            count = recalculate_cohortpeople(
+                self, pending_version, initiating_user_id=initiating_user_id, hogql=use_hogql_cohorts
+            )
             self.count = count
 
             self.last_calculation = timezone.now()
@@ -268,40 +277,6 @@ class Cohort(FileSystemSyncMixin, models.Model):
         )
 
         clear_stale_cohort.delay(self.pk, before_version=pending_version)
-
-        # Try the hogql version. Don't run this on initial cohort create
-        if pending_version > 0:
-
-            def fn():
-                start_time = time.monotonic()
-                recalculate_cohortpeople(self, pending_version, initiating_user_id=initiating_user_id, hogql=True)
-                logger.warn(
-                    "hogql_cohort_calculation_completed",
-                    id=self.pk,
-                    version=pending_version,
-                    duration=(time.monotonic() - start_time),
-                )
-
-            if settings.DEBUG or settings.TEST:
-                fn()
-                return
-
-            if posthoganalytics.feature_enabled(
-                "enable_hogql_cohort_calculation",
-                str(self.team.organization_id),
-                groups={"organization": str(self.team.organization_id)},
-                group_properties={"organization": {"id": str(self.team.organization_id)}},
-            ):
-                try:
-                    fn()
-                except Exception:
-                    logger.exception(
-                        "cohort_hogql_calculation_failed",
-                        id=self.pk,
-                        current_version=self.version,
-                        new_version=pending_version,
-                        exc_info=True,
-                    )
 
     def insert_users_by_list(self, items: list[str], *, team_id: Optional[int] = None) -> None:
         """
--- posthog/models/cohort/util.py
+++ posthog/models/cohort/util.py
@@ -312,38 +312,52 @@ def recalculate_cohortpeople(
     relevant_teams = Team.objects.order_by("id").filter(project_id=cohort.team.project_id)
     count_by_team_id: dict[int, int] = {}
     for team in relevant_teams:
-        count_for_team = (_recalculate_cohortpeople_for_team_hogql if hogql else _recalculate_cohortpeople_for_team)(
-            cohort, pending_version, team, initiating_user_id=initiating_user_id
-        )
-        count_by_team_id[team.id] = count_for_team or 0
+        before_count = get_cohort_size(cohort, team_id=team.id)
+
+        if before_count is not None:
+            logger.warn(
+                "Recalculating cohortpeople starting",
+                team_id=team.id,
+                cohort_id=cohort.pk,
+                size_before=before_count,
+            )
+
+        if hogql:
+            recalculate_fn = _recalculate_cohortpeople_for_team_hogql
+        else:
+            recalculate_fn = _recalculate_cohortpeople_for_team
+
+        recalculate_fn(cohort, pending_version, team, initiating_user_id=initiating_user_id)
+        count = get_cohort_size(cohort, override_version=pending_version, team_id=team.id)
+
+        if count is not None and before_count is not None:
+            logger.warn(
+                "Recalculating cohortpeople done",
+                team_id=team.id,
+                cohort_id=cohort.pk,
+                size_before=before_count,
+                size=count,
+            )
+
+        count_by_team_id[team.id] = count or 0
 
     return count_by_team_id[cohort.team_id]
 
 
 def _recalculate_cohortpeople_for_team(
     cohort: Cohort, pending_version: int, team: Team, *, initiating_user_id: Optional[int]
-) -> Optional[int]:
+):
     hogql_context = HogQLContext(within_non_hogql_query=True, team_id=team.id)
     cohort_query, cohort_params = format_person_query(cohort, 0, hogql_context)
 
-    before_count = get_cohort_size(cohort, team_id=team.id)
-
-    if before_count is not None:
-        logger.warn(
-            "Recalculating cohortpeople starting",
-            team_id=team.id,
-            cohort_id=cohort.pk,
-            size_before=before_count,
-        )
-
-    recalcluate_cohortpeople_sql = RECALCULATE_COHORT_BY_ID.format(cohort_filter=cohort_query)
+    recalculate_cohortpeople_sql = RECALCULATE_COHORT_BY_ID.format(cohort_filter=cohort_query)
 
     tag_queries(kind="cohort_calculation", team_id=team.id, query_type="CohortsQuery")
     if initiating_user_id:
         tag_queries(user_id=initiating_user_id)
 
     sync_execute(
-        recalcluate_cohortpeople_sql,
+        recalculate_cohortpeople_sql,
         {
             **cohort_params,
             **hogql_context.values,
@@ -359,19 +373,6 @@ def _recalculate_cohortpeople_for_team(
         },
         workload=Workload.OFFLINE,
     )
-
-    count = get_cohort_size(cohort, override_version=pending_version, team_id=team.id)
-
-    if count is not None and before_count is not None:
-        logger.warn(
-            "Recalculating cohortpeople done",
-            team_id=team.id,
-            cohort_id=cohort.pk,
-            size_before=before_count,
-            size=count,
-        )
-
-    return count
 
 
 def _recalculate_cohortpeople_for_team_hogql(
