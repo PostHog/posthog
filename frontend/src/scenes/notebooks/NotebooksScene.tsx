53949b68427f298a62face9f21f84450155c0aa2
Zach Waterfield <zlwaterfield@gmail.com>
2025-03-25 13:32:59 -0400

    add notebook paginated response check

--- frontend/src/scenes/notebooks/NotebooksScene.tsx
+++ frontend/src/scenes/notebooks/NotebooksScene.tsx
@@ -2,6 +2,7 @@ import './NotebookScene.scss'
 
 import { IconEllipsis } from '@posthog/icons'
 import { LemonButton, LemonMenu, lemonToast, Tooltip } from '@posthog/lemon-ui'
+import { useValues } from 'kea'
 import { router } from 'kea-router'
 import { PageHeader } from 'lib/components/PageHeader'
 import { base64Encode } from 'lib/utils'
@@ -9,13 +10,21 @@ import { getTextFromFile, selectFiles } from 'lib/utils/file-utils'
 import { SceneExport } from 'scenes/sceneTypes'
 import { urls } from 'scenes/urls'
 
+import { AccessControlLevel } from '~/types'
+
 import { NotebooksTable } from './NotebooksTable/NotebooksTable'
+import { notebooksTableLogic } from './NotebooksTable/notebooksTableLogic'
 
 export const scene: SceneExport = {
     component: NotebooksScene,
 }
 
 export function NotebooksScene(): JSX.Element {
+    const { notebooksResponse } = useValues(notebooksTableLogic)
+
+    const hasEditAccess = notebooksResponse?.user_access_level
+        ? ![AccessControlLevel.Viewer, AccessControlLevel.None].includes(notebooksResponse?.user_access_level)
+        : true
     return (
         <div className="deprecated-space-y-4">
             <PageHeader
@@ -56,11 +65,21 @@ export function NotebooksScene(): JSX.Element {
                             <LemonButton icon={<IconEllipsis />} size="small" />
                         </LemonMenu>
                         <Tooltip title="Like a Notebook but all your exploration is persisted to the URL for easy sharing.">
-                            <LemonButton data-attr="new-canvas" to={urls.canvas()} type="secondary">
+                            <LemonButton
+                                data-attr="new-canvas"
+                                to={urls.canvas()}
+                                type="secondary"
+                                disabled={!hasEditAccess}
+                            >
                                 New canvas
                             </LemonButton>
                         </Tooltip>
-                        <LemonButton data-attr="new-notebook" to={urls.notebook('new')} type="primary">
+                        <LemonButton
+                            data-attr="new-notebook"
+                            to={urls.notebook('new')}
+                            type="primary"
+                            disabled={!hasEditAccess}
+                        >
                             New notebook
                         </LemonButton>
                     </>
