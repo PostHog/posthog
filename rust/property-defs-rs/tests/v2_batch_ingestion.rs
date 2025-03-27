676a2d7a70b78e66d50028f5118cacfbf65758d4
Eli Reisman <eli.r@posthog.com>
2025-03-27 13:12:21 -0700

    complete test suite for "vanilla" event, group event, person event

--- rust/property-defs-rs/tests/v2_batch_ingestion.rs
+++ rust/property-defs-rs/tests/v2_batch_ingestion.rs
@@ -10,14 +10,15 @@ use sqlx::PgPool;
 use property_defs_rs::{
     config::Config,
     process_batch_v2,
-    types::{Event, Update},
+    types::{Event, PropertyParentType, Update},
 };
 
 #[sqlx::test(migrations = "./tests/test_migrations")]
 async fn test_simple_batch_write(db: PgPool) {
     let config = Config::init_with_defaults().unwrap();
     let cache: Arc<Cache<Update, ()>> = Arc::new(Cache::new(config.cache_capacity));
-    let updates = gen_test_event_updates("$pageview", 100);
+    let updates = gen_test_event_updates("$pageview", 100, None);
+    // should decompose into 1 event def, 100 event props, 100 prop defs (of event type)
     assert_eq!(updates.len(), 201);
 
     process_batch_v2(&config, cache, &db, updates).await;
@@ -45,15 +46,123 @@ async fn test_simple_batch_write(db: PgPool) {
     assert_eq!(Some(100), event_props_count);
 }
 
-fn gen_test_event_updates(event_name: &'static str, num_props: usize) -> Vec<Update> {
+#[sqlx::test(migrations = "./tests/test_migrations")]
+async fn test_group_batch_write(db: PgPool) {
+    let config = Config::init_with_defaults().unwrap();
+    let cache: Arc<Cache<Update, ()>> = Arc::new(Cache::new(config.cache_capacity));
+    let updates = gen_test_event_updates("$groupidentify", 100, Some(PropertyParentType::Group));
+    // should decompose into 1 group event def, 100 prop defs (of group type), 100 event props
+    assert_eq!(updates.len(), 201);
+
+    process_batch_v2(&config, cache, &db, updates).await;
+
+    // fetch results and ensure they landed correctly
+    let event_defs_count: Option<i64> = sqlx::query_scalar!(
+        r#"SELECT count(*) from posthog_eventdefinition WHERE name = '$groupidentify'"#
+    )
+    .fetch_one(&db)
+    .await
+    .unwrap();
+    assert_eq!(Some(1), event_defs_count);
+
+    let prop_defs_count: Option<i64> =
+        sqlx::query_scalar!(r#"SELECT count(*) from posthog_propertydefinition WHERE type = 3"#)
+            .fetch_one(&db)
+            .await
+            .unwrap();
+    assert_eq!(Some(100), prop_defs_count);
+
+    let event_props_count: Option<i64> =
+        sqlx::query_scalar!(r#"SELECT count(*) from posthog_eventproperty"#)
+            .fetch_one(&db)
+            .await
+            .unwrap();
+    assert_eq!(Some(100), event_props_count);
+}
+
+#[sqlx::test(migrations = "./tests/test_migrations")]
+async fn test_person_batch_write(db: PgPool) {
+    let config = Config::init_with_defaults().unwrap();
+    let cache: Arc<Cache<Update, ()>> = Arc::new(Cache::new(config.cache_capacity));
+    let updates =
+        gen_test_event_updates("event_with_person", 100, Some(PropertyParentType::Person));
+    // should decompose into 1 event def, 100 event props, 100 prop defs (50 $set, 50 $set_once props)
+    assert_eq!(updates.len(), 201);
+
+    process_batch_v2(&config, cache, &db, updates).await;
+
+    // fetch results and ensure they landed correctly
+    let event_defs_count: Option<i64> = sqlx::query_scalar!(
+        r#"SELECT count(*) from posthog_eventdefinition WHERE name = 'event_with_person'"#
+    )
+    .fetch_one(&db)
+    .await
+    .unwrap();
+    assert_eq!(Some(1), event_defs_count);
+
+    let prop_defs_count: Option<i64> =
+        sqlx::query_scalar!(r#"SELECT count(*) from posthog_propertydefinition WHERE type = 2"#)
+            .fetch_one(&db)
+            .await
+            .unwrap();
+    assert_eq!(Some(100), prop_defs_count);
+
+    let event_props_count: Option<i64> =
+        sqlx::query_scalar!(r#"SELECT count(*) from posthog_eventproperty"#)
+            .fetch_one(&db)
+            .await
+            .unwrap();
+    assert_eq!(Some(100), event_props_count);
+}
+
+fn gen_test_event_updates(
+    event_name: &'static str,
+    num_props: usize,
+    prop_type: Option<PropertyParentType>,
+) -> Vec<Update> {
     let prop_kvs = (0..num_props)
         .map(gen_test_prop_key_value)
         .collect::<Vec<(String, Value)>>();
 
+    // let's be *just* realistic enough to exercise we're decomposing the Event properly in unit tests
     let mut properties = HashMap::<String, Value>::new();
-    for kv in prop_kvs {
-        properties.insert(kv.0, kv.1);
+    match prop_type {
+        Some(PropertyParentType::Person) => {
+            let mut set_props = HashMap::<String, Value>::new();
+            let mut set_once_props = HashMap::<String, Value>::new();
+
+            // split the total props between $set and $set_once buckets.
+            // these will be flattened back into a single unique group of props
+            // during parsing into Update records
+            for kv in prop_kvs.iter().enumerate() {
+                if kv.0 % 2 == 0 {
+                    set_props.insert(kv.1 .0.clone(), kv.1 .1.clone());
+                } else {
+                    set_once_props.insert(kv.1 .0.clone(), kv.1 .1.clone());
+                }
+            }
+            properties.insert(String::from("$set"), json!(set_props));
+            properties.insert(String::from("$set_once"), json!(set_once_props));
+        }
+        Some(PropertyParentType::Group) => {
+            let mut group_props = HashMap::<String, Value>::new();
+            for kv in prop_kvs {
+                group_props.insert(kv.0, kv.1);
+            }
+            properties.insert(
+                String::from("$group_type"),
+                Value::String(String::from("Organization")),
+            );
+            properties.insert(String::from("$group_set"), json!(group_props));
+        }
+        _ => {
+            // generate flat list of "vanilla" event props
+            for kv in prop_kvs {
+                properties.insert(kv.0, kv.1);
+            }
+        }
     }
+
     let properties = json!(properties).to_string();
 
     let event = json!({
@@ -63,13 +172,9 @@ fn gen_test_event_updates(event_name: &'static str, num_props: usize) -> Vec<Upd
         "properties": properties,
     });
 
-    // each non-person property will generate 1 event_property and 1 property_definition
-    // Update, and we add one more for the event_definition Update from the parent
-    let limit = (2 * num_props) + 1;
-
     serde_json::from_value::<Event>(event)
         .unwrap()
-        .into_updates(limit)
+        .into_updates(10000)
 }
 
 fn gen_test_prop_key_value(ndx: usize) -> (String, Value) {
