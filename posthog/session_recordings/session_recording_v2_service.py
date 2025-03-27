a17d09985b34a918ff6e3347ed4479f3eea9a3f0
Paweł Ledwoń <me@difficult.name>
2025-03-27 16:58:18 +0100

    fix: fix start time check in v2 service

--- posthog/session_recordings/session_recording_v2_service.py
+++ posthog/session_recordings/session_recording_v2_service.py
@@ -55,7 +55,7 @@ def list_blocks(recording: SessionRecording) -> list[RecordingBlock]:
 
     # If we started recording halfway through the session, we should not return any blocks
     # as we don't have the complete recording from the start
-    if not blocks or blocks[0]["start_time"] != metadata["start_time"]:
+    if not blocks or "start_time" not in metadata or blocks[0]["start_time"] != metadata["start_time"]:
         return []
 
     return blocks
