95003c627c0fe91d07b4551490995d613bde450e
David Newell <david@posthog.com>
2025-03-28 09:28:49 +0000

    feat: error tracking omnisearch

--- frontend/src/scenes/error-tracking/ErrorTrackingFilters.tsx
+++ frontend/src/scenes/error-tracking/ErrorTrackingFilters.tsx
@@ -1,12 +1,15 @@
-import { LemonInput } from '@posthog/lemon-ui'
-import { useActions, useValues } from 'kea'
+import { LemonDropdown } from '@posthog/lemon-ui'
+import { BindLogic, useActions, useValues } from 'kea'
 import { DateFilter } from 'lib/components/DateFilter/DateFilter'
-import { TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
+import { InfiniteSelectResults } from 'lib/components/TaxonomicFilter/InfiniteSelectResults'
+import { TaxonomicFilterSearchInput } from 'lib/components/TaxonomicFilter/TaxonomicFilter'
+import { taxonomicFilterLogic } from 'lib/components/TaxonomicFilter/taxonomicFilterLogic'
+import { TaxonomicFilterGroupType, TaxonomicFilterLogicProps } from 'lib/components/TaxonomicFilter/types'
 import UniversalFilters from 'lib/components/UniversalFilters/UniversalFilters'
 import { universalFiltersLogic } from 'lib/components/UniversalFilters/universalFiltersLogic'
 import { isUniversalGroupFilterLike } from 'lib/components/UniversalFilters/utils'
 import { dateMapping } from 'lib/utils'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { TestAccountFilter } from 'scenes/insights/filters/TestAccountFilter'
 
 import { errorTrackingLogic } from './errorTrackingLogic'
@@ -19,7 +22,6 @@ export const ErrorTrackingFilters = (): JSX.Element => {
             <div className="flex gap-2 items-center">
                 <DateRange />
                 <FilterGroup />
-                <UniversalSearch />
                 <InternalAccounts />
             </div>
         </div>
@@ -31,22 +33,97 @@ const FilterGroup = (): JSX.Element => {
     const { setFilterGroup } = useActions(errorTrackingLogic)
 
     return (
-        <span className="bg-surface-primary rounded">
-            <UniversalFilters
-                rootKey="error-tracking"
-                group={filterGroup}
-                // TODO: Probably makes sense to create a new taxonomic group for exception-specific event property filters only, keep it clean.
-                taxonomicGroupTypes={[
-                    TaxonomicFilterGroupType.EventProperties,
-                    TaxonomicFilterGroupType.PersonProperties,
-                    TaxonomicFilterGroupType.Cohorts,
-                    TaxonomicFilterGroupType.HogQLExpression,
-                ]}
-                onChange={setFilterGroup}
+        <UniversalFilters
+            rootKey="error-tracking"
+            group={filterGroup}
+            // TODO: Probably makes sense to create a new taxonomic group for exception-specific event property filters only, keep it clean.
+            taxonomicGroupTypes={[
+                TaxonomicFilterGroupType.EventProperties,
+                TaxonomicFilterGroupType.PersonProperties,
+                TaxonomicFilterGroupType.Cohorts,
+                TaxonomicFilterGroupType.HogQLExpression,
+            ]}
+            onChange={setFilterGroup}
+        >
+            <UniversalSearch />
+        </UniversalFilters>
+    )
+}
+
+const UniversalSearch = (): JSX.Element => {
+    const [visible, setVisible] = useState<boolean>(false)
+    const { addGroupFilter } = useActions(universalFiltersLogic)
+
+    const taxonomicFilterLogicProps: TaxonomicFilterLogicProps = {
+        taxonomicFilterLogicKey: 'error-tracking',
+        taxonomicGroupTypes: [
+            TaxonomicFilterGroupType.EventProperties,
+            TaxonomicFilterGroupType.PersonProperties,
+            TaxonomicFilterGroupType.Cohorts,
+            TaxonomicFilterGroupType.HogQLExpression,
+        ],
+        onChange: (taxonomicGroup, value, item, originalQuery) => {
+            addGroupFilter(taxonomicGroup, value, item, originalQuery)
+            setVisible(false)
+        },
+    }
+
+    const searchInputRef = useRef<HTMLInputElement | null>(null)
+    const focusInput = (): void => searchInputRef.current?.focus()
+
+    return (
+        <BindLogic logic={taxonomicFilterLogic} props={taxonomicFilterLogicProps}>
+            <LemonDropdown
+                overlay={
+                    <InfiniteSelectResults
+                        focusInput={focusInput}
+                        taxonomicFilterLogicProps={taxonomicFilterLogicProps}
+                        popupAnchorElement={searchInputRef.current}
+                    />
+                }
+                matchWidth
+                visible={visible}
+                onClickInside={() => console.log('Clicked inside')}
+                onClickOutside={() => setVisible(false)}
+                closeOnClickInside={false}
+                // referenceRef={inputRef}
+                // // visible={visible}
+                // placement="right-start"
+                // fallbackPlacements={['left-end', 'bottom']}
+                // onClickOutside={() => setVisible(false)}
             >
-                <RecordingsUniversalFilterGroup />
-            </UniversalFilters>
-        </span>
+                <UniversalSearchInput searchInputRef={searchInputRef} setVisible={setVisible} />
+            </LemonDropdown>
+        </BindLogic>
+    )
+}
+
+const UniversalSearchInput = ({
+    searchInputRef,
+    setVisible,
+}: {
+    searchInputRef: any
+    setVisible: any
+}): JSX.Element => {
+    const { searchQuery } = useValues(errorTrackingLogic)
+    const { setSearchQuery: setErrorTrackingSearchQuery } = useActions(errorTrackingLogic)
+    const { setSearchQuery: setTaxonomicFilterSearchQuery } = useActions(taxonomicFilterLogic)
+
+    return (
+        <TaxonomicFilterSearchInput
+            value={searchQuery}
+            onChange={(value) => {
+                setErrorTrackingSearchQuery(value)
+                setTaxonomicFilterSearchQuery(value)
+            }}
+            searchInputRef={searchInputRef}
+            onClose={() => setVisible(false)}
+            size="small"
+            onClick={() => setVisible(true)}
+            prefix={<RecordingsUniversalFilterGroup />}
+            onPressEnter={() => setVisible(false)}
+            fullWidth
+        />
     )
 }
 
@@ -65,7 +142,6 @@ const RecordingsUniversalFilterGroup = (): JSX.Element => {
                 return isUniversalGroupFilterLike(filterOrGroup) ? (
                     <UniversalFilters.Group key={index} index={index} group={filterOrGroup}>
                         <RecordingsUniversalFilterGroup />
-                        <UniversalFilters.AddFilterButton size="small" type="secondary" />
                     </UniversalFilters.Group>
                 ) : (
                     <UniversalFilters.Value
@@ -87,32 +163,14 @@ const DateRange = (): JSX.Element => {
     const { setDateRange } = useActions(errorTrackingLogic)
 
     return (
-        <span className="bg-surface-primary rounded">
-            <DateFilter
-                size="small"
-                dateFrom={dateRange.date_from}
-                dateTo={dateRange.date_to}
-                dateOptions={errorTrackingDateOptions}
-                onChange={(changedDateFrom, changedDateTo) =>
-                    setDateRange({ date_from: changedDateFrom, date_to: changedDateTo })
-                }
-            />
-        </span>
-    )
-}
-
-const UniversalSearch = (): JSX.Element => {
-    const { searchQuery } = useValues(errorTrackingLogic)
-    const { setSearchQuery } = useActions(errorTrackingLogic)
-
-    return (
-        <LemonInput
-            type="search"
-            placeholder="Search..."
-            value={searchQuery}
-            onChange={setSearchQuery}
-            className="flex-grow max-w-none"
+        <DateFilter
             size="small"
+            dateFrom={dateRange.date_from}
+            dateTo={dateRange.date_to}
+            dateOptions={errorTrackingDateOptions}
+            onChange={(changedDateFrom, changedDateTo) =>
+                setDateRange({ date_from: changedDateFrom, date_to: changedDateTo })
+            }
         />
     )
 }
