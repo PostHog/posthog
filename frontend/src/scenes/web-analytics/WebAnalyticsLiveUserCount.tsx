8d849082e87c5fc4710fa7babca9be7375955d63
Lucas Ricoy <lucas.r@posthog.com>
2025-03-27 23:49:33 -0300

    fix: only use interval when hovering tooltip

--- frontend/src/scenes/web-analytics/WebAnalyticsLiveUserCount.tsx
+++ frontend/src/scenes/web-analytics/WebAnalyticsLiveUserCount.tsx
@@ -1,39 +1,46 @@
 import './WebAnalyticsLiveUserCount.scss'
 
 import clsx from 'clsx'
-import { useValues } from 'kea'
+import { useActions, useValues } from 'kea'
 import { Tooltip } from 'lib/lemon-ui/Tooltip'
 import { humanFriendlyLargeNumber, humanFriendlyNumber } from 'lib/utils'
+import { useEffect } from 'react'
 import { teamLogic } from 'scenes/teamLogic'
-import { liveEventsTableLogic } from 'scenes/web-analytics/liveWebAnalyticsLogic'
+import { liveWebAnalyticsLogic } from 'scenes/web-analytics/liveWebAnalyticsLogic'
 
-const SecondsAgoTooltipText = (): JSX.Element | null => {
-    // We connect to liveUserUpdatedSecondsAgo only when the user is hovering over the indicator
-    // otherwise we were causing a lot of unnecessary re-renders. See: https://github.com/PostHog/posthog/pull/30477
-    const { liveUserCount, liveUserUpdatedSecondsAgo } = useValues(liveEventsTableLogic)
+const TooltipContent = (): JSX.Element | null => {
     const { currentTeam } = useValues(teamLogic)
+    const { liveUserUpdatedSecondsAgo, liveUserCount } = useValues(liveWebAnalyticsLogic)
+    const { setIsHovering } = useActions(liveWebAnalyticsLogic)
+
+    // This is only rendered when the tooltip is open, so we will let the Kea interval knows
+    // when we're rendered and when we're not
+    useEffect(() => {
+        setIsHovering(true)
+        return () => setIsHovering(false)
+    }, [setIsHovering])
 
     if (liveUserCount == null) {
         return null
     }
 
-    const usersOnlineString = `${humanFriendlyNumber(liveUserCount)} ${
-        liveUserCount === 1 ? 'user is' : 'users are'
-    } online`
-    const inTeamString = currentTeam ? ` in ${currentTeam.name}` : ''
     const updatedAgoString =
         liveUserUpdatedSecondsAgo === 0
             ? ' (updated just now)'
             : liveUserUpdatedSecondsAgo == null
             ? ''
             : ` (updated ${liveUserUpdatedSecondsAgo} seconds ago)`
-    const tooltip = `${usersOnlineString}${inTeamString}${updatedAgoString}`
 
+    const usersOnlineString = `${humanFriendlyNumber(liveUserCount)} ${
+        liveUserCount === 1 ? 'user is' : 'users are'
+    } online`
+    const inTeamString = currentTeam ? ` in ${currentTeam.name}` : ''
+    const tooltip = `${usersOnlineString}${inTeamString}${updatedAgoString}`
     return <>{tooltip}</>
 }
 
 export const WebAnalyticsLiveUserCount = (): JSX.Element | null => {
-    const { liveUserCount } = useValues(liveEventsTableLogic)
+    const { liveUserCount } = useValues(liveWebAnalyticsLogic)
 
     // No data yet, or feature flag disabled
     if (liveUserCount == null) {
@@ -41,7 +48,7 @@ export const WebAnalyticsLiveUserCount = (): JSX.Element | null => {
     }
 
     return (
-        <Tooltip title={<SecondsAgoTooltipText />}>
+        <Tooltip title={<TooltipContent />} interactive={true} delayMs={0}>
             <div className="flex flex-row items-center justify-center">
                 <div className={clsx('live-user-indicator', liveUserCount > 0 ? 'online' : 'offline')} />
                 <span className="whitespace-nowrap" data-attr="web-analytics-live-user-count">
@@ -50,27 +57,4 @@ export const WebAnalyticsLiveUserCount = (): JSX.Element | null => {
             </div>
         </Tooltip>
     )
-}
-
-const SecondsAgoTooltipText = (): JSX.Element | null => {
-    const { currentTeam } = useValues(teamLogic)
-    const { liveUserUpdatedSecondsAgo, liveUserCount } = useValues(liveEventsTableLogic)
-
-    if (liveUserCount == null) {
-        return null
-    }
-
-    const updatedAgoString =
-        liveUserUpdatedSecondsAgo === 0
-            ? ' (updated just now)'
-            : liveUserUpdatedSecondsAgo == null
-            ? ''
-            : ` (updated ${liveUserUpdatedSecondsAgo} seconds ago)`
-
-    const usersOnlineString = `${humanFriendlyNumber(liveUserCount)} ${
-        liveUserCount === 1 ? 'user is' : 'users are'
-    } online`
-    const inTeamString = currentTeam ? ` in ${currentTeam.name}` : ''
-    const tooltip = `${usersOnlineString}${inTeamString}${updatedAgoString}`
-    return <>{tooltip}</>
 }
