11c12dc51a644a5b19cdf86ef59369120527fb74
Hugues Pouillot <hpouillot@gmail.com>
2025-03-28 11:51:57 +0100

    fix web analytics error tracking widget

--- frontend/src/scenes/web-analytics/webAnalyticsLogic.tsx
+++ frontend/src/scenes/web-analytics/webAnalyticsLogic.tsx
@@ -1921,6 +1921,7 @@ export const webAnalyticsLogic = kea<webAnalyticsLogicType>([
                                   filterTestAccounts: filterTestAccounts,
                                   filterGroup: replayFilters.filter_group,
                                   columns: ['error', 'users', 'occurrences'],
+                                  volumeResolution: 0,
                                   limit: 4,
                               }),
                               docs: {
