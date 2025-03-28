8a4fa9a22b256abb48ecd1927f8d85905b61575e
Oliver Browne <oliver@posthog.com>
2025-03-28 08:50:59 +0200

    temporary, will remove once I've found the spot

--- rust/cymbal/src/issue_resolution.rs
+++ rust/cymbal/src/issue_resolution.rs
@@ -118,7 +118,7 @@ impl Issue {
             self.description
         )
         .fetch_one(executor)
-        .await?
+        .await.expect("Got at least one row back")
         // TODO - I'm fairly sure the Option here is a bug in sqlx, so the unwrap will
         // never be hit, but nonetheless I'm not 100% sure the "no rows" case actually
         // means the insert was not done.
@@ -214,7 +214,7 @@ impl IssueFingerprintOverride {
             issue.id,
             fingerprint,
             first_seen
-        ).fetch_one(executor).await?;
+        ).fetch_one(executor).await.expect("Got at least one row back");
 
         Ok(res)
     }
