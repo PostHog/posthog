93cc7f772ba1fc5a2868851b7565a68266dd420e
eric <eeoneric@gmail.com>
2025-03-31 09:05:39 -0700

    hide scrollbars

--- frontend/src/scenes/data-warehouse/editor/sidebar/QueryInfo.tsx
+++ frontend/src/scenes/data-warehouse/editor/sidebar/QueryInfo.tsx
@@ -68,7 +68,7 @@ export function QueryInfo({ codeEditorKey }: QueryInfoProps): JSX.Element {
     const savedQuery = editingView ? dataWarehouseSavedQueryMapById[editingView.id] : null
 
     return (
-        <div className="overflow-scroll">
+        <div className="overflow-scroll hide-scrollbar">
             <div className="flex flex-col flex-1 p-4 gap-4 ">
                 <div>
                     <div className="flex flex-row items-center gap-2">
