394d622361f0e8ad5e2ede893d5f5ceb4c057de9
dylan <dylan@posthog.com>
2025-04-01 13:56:16 -0700

    some logs

--- rust/feature-flags/src/flags/flag_service.rs
+++ rust/feature-flags/src/flags/flag_service.rs
@@ -97,9 +97,8 @@ impl FlagService {
                         1,
                     );
                     // If we have the team in postgres, but not redis, update redis so we're faster next time
-                    if let Err(e) = Team::update_redis_cache(self.redis_client.clone(), &team).await
+                    if let Err(_) = Team::update_redis_cache(self.redis_client.clone(), &team).await
                     {
-                        tracing::warn!("Failed to update Redis cache: {}", e);
                         inc(
                             TEAM_CACHE_ERRORS_COUNTER,
                             &[("reason".to_string(), "redis_update_failed".to_string())],
@@ -143,14 +142,13 @@ impl FlagService {
                                 &[("project_id".to_string(), project_id.to_string())],
                                 1,
                             );
-                            if let Err(e) = FeatureFlagList::update_flags_in_redis(
+                            if let Err(_) = FeatureFlagList::update_flags_in_redis(
                                 self.redis_client.clone(),
                                 project_id,
                                 &flags,
                             )
                             .await
                             {
-                                tracing::warn!("Failed to update Redis cache: {}", e);
                                 inc(
                                     FLAG_CACHE_ERRORS_COUNTER,
                                     &[("reason".to_string(), "redis_update_failed".to_string())],
