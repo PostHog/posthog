723cb677bfdfed5043d524f9f712c9ea3d22fec0
Haven Barnes <haven@posthog.com>
2025-04-01 13:26:04 -0700

    fix campaign delivery

--- plugin-server/src/cdp/consumers/cdp-processed-events.consumer.ts
+++ plugin-server/src/cdp/consumers/cdp-processed-events.consumer.ts
@@ -178,6 +178,7 @@ export class CdpProcessedEventsConsumer extends CdpConsumerBase {
                                 const [teamHogFunctions, team] = await Promise.all([
                                     this.hogFunctionManager.getHogFunctionsForTeam(clickHouseEvent.team_id, [
                                         'destination',
+                                        'campaign',
                                     ]),
                                     this.hub.teamManager.fetchTeam(clickHouseEvent.team_id),
                                 ])
