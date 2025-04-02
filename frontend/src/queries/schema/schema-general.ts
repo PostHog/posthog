7a6044d94c04244d75626e1ac89f50ab44d53916
Alexander Spicer <aspicer@gmail.com>
2025-04-02 10:30:54 -0700

    optional

--- frontend/src/queries/schema/schema-general.ts
+++ frontend/src/queries/schema/schema-general.ts
@@ -1099,6 +1099,9 @@ export type FunnelsFilter = {
     useUdf?: boolean
     /** Customizations for the appearance of result datasets. */
     resultCustomizations?: Record<string, ResultCustomizationByValue>
+    /** A list of indices of optional steps */
+    /** @default [] */
+    optional?: integer[]
 }
 
 export interface FunnelsQuery extends InsightsQueryBase<FunnelsQueryResponse> {
