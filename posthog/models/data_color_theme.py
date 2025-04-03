e08b142f13022d3fcb83644889619f627a868572
Zach Waterfield <zlwaterfield@gmail.com>
2025-04-03 11:42:03 -0400

    add team mix in for data color theme

--- posthog/models/data_color_theme.py
+++ posthog/models/data_color_theme.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 from posthog.models.team import Team
+from posthog.models.utils import TeamProjectMixin
 
 
-class DataColorTheme(models.Model):
+class DataColorTheme(TeamProjectMixin, models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
 
     name = models.CharField(max_length=100)
