f1feb0fdd5e8939131a3b69d4eed12bbe0506e9a
Zach Waterfield <zlwaterfield@gmail.com>
2025-04-03 13:42:19 -0400

    chore(environments): data color theme and uploaded media team project mixin (#30763)

--- posthog/models/data_color_theme.py
+++ posthog/models/data_color_theme.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 from posthog.models.team import Team
+from posthog.models.utils import TeamProjectMixin
 
 
-class DataColorTheme(models.Model):
+class DataColorTheme(TeamProjectMixin, models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
     project = models.ForeignKey("Project", on_delete=models.CASCADE, null=True, blank=True)
 
