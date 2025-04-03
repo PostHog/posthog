1bd3e5364991b3026b2386e8f97b0790ac3e12d0
Daniel Bachhuber <daniel.b@posthog.com>
2025-04-03 08:13:27 -0700

    Remove annoying parens from pluralized group name

--- frontend/src/models/groupsModel.ts
+++ frontend/src/models/groupsModel.ts
@@ -112,7 +112,7 @@ export const groupsModel = kea<groupsModelType>([
                         if (groupType) {
                             return {
                                 singular: groupType.name_singular || groupType.group_type,
-                                plural: groupType.name_plural || `${groupType.group_type}(s)`,
+                                plural: groupType.name_plural || `${groupType.group_type}s`,
                             }
                         }
                         return {
