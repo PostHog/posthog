af9cc12e0266d706af63d701624ed00593c64da5
Marcus Hof <13001502+MarconLP@users.noreply.github.com>
2025-04-04 14:19:42 +0200

    Discard changes to posthog/cdp/templates/hog_function_template.py

--- posthog/cdp/templates/hog_function_template.py
+++ posthog/cdp/templates/hog_function_template.py
@@ -37,15 +37,6 @@ HogFunctionTemplateKind = Literal["messaging_campaign"]
 
 
 @dataclasses.dataclass(frozen=True)
-class HogFunctionMappingTemplate:
-    name: str
-    include_by_default: Optional[bool] = None
-    filters: Optional[dict] = None
-    inputs: Optional[dict] = None
-    inputs_schema: Optional[list[dict]] = None
-
-
-@dataclasses.dataclass(frozen=True)
 class HogFunctionSubTemplate:
     id: SubTemplateId
     name: str
@@ -55,11 +46,19 @@ class HogFunctionSubTemplate:
     masking: Optional[dict] = None
     input_schema_overrides: Optional[dict[str, dict]] = None
     type: Optional[HogFunctionTemplateType] = None
-    mapping_templates: Optional[list[HogFunctionMappingTemplate]] = None
 
 
 @dataclasses.dataclass(frozen=True)
 class HogFunctionMapping:
+    filters: Optional[dict] = None
+    inputs: Optional[dict] = None
+    inputs_schema: Optional[list[dict]] = None
+
+
+@dataclasses.dataclass(frozen=True)
+class HogFunctionMappingTemplate:
+    name: str
+    include_by_default: Optional[bool] = None
     filters: Optional[dict] = None
     inputs: Optional[dict] = None
     inputs_schema: Optional[list[dict]] = None
@@ -121,8 +120,7 @@ def derive_sub_templates(templates: list[HogFunctionTemplate]) -> list[HogFuncti
             template_params.pop("sub_templates")
             # Update with the sub template params if not none
             for key, value in sub_template_params.items():
-                # some sub_templates do not have mappings
-                if value is not None or key == "mapping_templates":
+                if value is not None:
                     template_params[key] = value
 
             template_params["id"] = merged_id
