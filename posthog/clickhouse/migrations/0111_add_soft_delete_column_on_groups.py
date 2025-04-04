14255e9a237349d07e84245e788dbaf9842f73a8
Daniel Bachhuber <daniel.b@posthog.com>
2025-04-04 05:58:46 -0700

    Use `run_sql_with_exceptions()`

--- posthog/clickhouse/migrations/0111_add_soft_delete_column_on_groups.py
+++ posthog/clickhouse/migrations/0111_add_soft_delete_column_on_groups.py
@@ -1,32 +1,28 @@
-from infi.clickhouse_orm import migrations
+from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 
-from posthog.clickhouse.client.connection import get_client_from_pool
-from posthog.settings import CLICKHOUSE_CLUSTER
-
+DROP_COLUMNS_INDEX_GROUPS = """
+ALTER TABLE groups
+DROP INDEX IF EXISTS is_deleted_idx
+"""
 
 DROP_COLUMNS_GROUPS = """
-ALTER TABLE {table} ON CLUSTER {cluster}
+ALTER TABLE groups
 DROP COLUMN IF EXISTS is_deleted
 """
 
 ADD_COLUMNS_GROUPS = """
-ALTER TABLE {table} ON CLUSTER {cluster}
+ALTER TABLE groups
 ADD COLUMN IF NOT EXISTS is_deleted Boolean
 """
 
 ADD_COLUMNS_INDEX_GROUPS = """
-ALTER TABLE {table} ON CLUSTER {cluster}
+ALTER TABLE groups
 ADD INDEX IF NOT EXISTS is_deleted_idx (is_deleted) TYPE minmax GRANULARITY 1
 """
 
-
-def add_columns_to_required_tables(_):
-    with get_client_from_pool() as client:
-        client.execute(DROP_COLUMNS_GROUPS.format(table="groups", cluster=CLICKHOUSE_CLUSTER))
-        client.execute(ADD_COLUMNS_GROUPS.format(table="groups", cluster=CLICKHOUSE_CLUSTER))
-        client.execute(ADD_COLUMNS_INDEX_GROUPS.format(table="groups", cluster=CLICKHOUSE_CLUSTER))
-
-
 operations = [
-    migrations.RunPython(add_columns_to_required_tables),
+    run_sql_with_exceptions(DROP_COLUMNS_INDEX_GROUPS),
+    run_sql_with_exceptions(DROP_COLUMNS_GROUPS),
+    run_sql_with_exceptions(ADD_COLUMNS_GROUPS),
+    run_sql_with_exceptions(ADD_COLUMNS_INDEX_GROUPS),
 ]
