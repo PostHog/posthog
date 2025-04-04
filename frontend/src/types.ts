4fcd33ebe0702ce99e77f7b12bbac2b7cf072ed4
Marcus Hof <13001502+MarconLP@users.noreply.github.com>
2025-04-04 14:53:21 +0200

    fix(cdp): do not fetch private slack channels for other users (#30487)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -3904,6 +3904,7 @@ export interface SlackChannelType {
     is_private: boolean
     is_ext_shared: boolean
     is_member: boolean
+    is_private_without_access?: boolean
 }
 
 export interface SharingConfigurationType {
