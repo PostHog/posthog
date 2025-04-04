adb37a88e4ddf0aee05a90a85e926296d13f5855
Daniel Bachhuber <daniel.b@posthog.com>
2025-04-04 14:23:16 -0700

    Register the job

--- dags/delete_groups.py
+++ dags/delete_groups.py
@@ -23,3 +23,11 @@ def delete_groups(
 def delete_groups_job():
     """Job that handles deletion of groups marked as deleted."""
     delete_groups()
+
+
+delete_groups_schedule = dagster.ScheduleDefinition(
+    job=delete_groups_job,
+    cron_schedule="28 4 * * *",  # Once a day at a random time
+    execution_timezone="UTC",
+    name="delete_groups_schedule",
+)
