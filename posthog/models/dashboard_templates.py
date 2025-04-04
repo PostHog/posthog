3da93775a6f811a7a47bce6a39120ffe8bc17855
Ben White <ben@posthog.com>
2025-04-04 19:10:22 -0400

    Fix models

--- posthog/models/dashboard_templates.py
+++ posthog/models/dashboard_templates.py
@@ -2,17 +2,17 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models import UniqueConstraint
 
-from posthog.models.utils import UUIDModel
+from posthog.models.utils import RootTeamMixin, RootTeamManager, UUIDModel
 
 
-class DashboardTemplateManager(models.Manager):
+class DashboardTemplateManager(RootTeamManager):
     def get_queryset(self):
         return super().get_queryset().exclude(deleted=True)
 
 
-class DashboardTemplate(UUIDModel):
+class DashboardTemplate(RootTeamMixin, UUIDModel):
     objects = DashboardTemplateManager()
-    objects_including_soft_deleted = models.Manager()
+    objects_including_soft_deleted = RootTeamManager()
 
     class Scope(models.TextChoices):
         """Visibility of the dashboard template"""
