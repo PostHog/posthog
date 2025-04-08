1bbe427254dba517f9892f101d958ec901b45530
Anders Asheim Hennum <git@andehen.net>
2025-04-08 10:44:49 +0200

    tweak UI

--- frontend/src/scenes/experiments/ExperimentMetricOutlierHandling.tsx
+++ frontend/src/scenes/experiments/ExperimentMetricOutlierHandling.tsx
@@ -13,42 +13,60 @@ export function ExperimentMetricOutlierHandling({
 }): JSX.Element {
     return (
         <div>
-            <LemonLabel>Outlier handling</LemonLabel>
-            <div className="flex items-center gap-2">
+            <LemonLabel
+                className="mb-1"
+                info="Prevent outliers from skewing results by capping the lower and upper bounds of a metric."
+            >
+                Outlier handling
+            </LemonLabel>
+            <p className="text-sm text-muted-alt">
+                Set winsorization lower and upper bounds to cap metric values at the specified percentiles.
+            </p>
+            <div className="mt-3 flex items-center gap-2">
                 <LemonCheckbox
-                    label="Lower bound"
+                    label="Lower bound percentile"
                     checked={metric.lower_bound_percentile !== undefined}
                     onChange={(checked) =>
-                        handleSetMetric({ ...metric, lower_bound_percentile: checked ? 0.1 : undefined })
+                        handleSetMetric({ ...metric, lower_bound_percentile: checked ? 0.05 : undefined })
                     }
                 />
-                {metric.lower_bound_percentile !== undefined && (
-                    <LemonInput
-                        value={metric.lower_bound_percentile * 100}
-                        onChange={(value) => handleSetMetric({ ...metric, lower_bound_percentile: (value ?? 0) / 100 })}
-                        type="number"
-                        step={1}
-                        suffix={<span className="text-sm">%</span>}
-                    />
-                )}
+                <LemonInput
+                    value={metric.lower_bound_percentile !== undefined ? metric.lower_bound_percentile * 100 : 0}
+                    onChange={(value) =>
+                        metric.lower_bound_percentile !== undefined &&
+                        handleSetMetric({ ...metric, lower_bound_percentile: (value ?? 0) / 100 })
+                    }
+                    type="number"
+                    step={1}
+                    suffix={<span className="text-sm">%</span>}
+                    size="small"
+                    className={`w-20 transition-opacity ${
+                        metric.lower_bound_percentile === undefined ? 'opacity-0 invisible' : 'opacity-100 visible'
+                    }`}
+                />
             </div>
-            <div className="flex items-center gap-2">
+            <div className="mt-3 flex items-center gap-2">
                 <LemonCheckbox
-                    label="Upper bound"
+                    label="Upper bound percentile"
                     checked={metric.upper_bound_percentile !== undefined}
                     onChange={(checked) =>
-                        handleSetMetric({ ...metric, upper_bound_percentile: checked ? 0.9 : undefined })
+                        handleSetMetric({ ...metric, upper_bound_percentile: checked ? 0.95 : undefined })
                     }
                 />
-                {metric.upper_bound_percentile !== undefined && (
-                    <LemonInput
-                        value={metric.upper_bound_percentile * 100}
-                        onChange={(value) => handleSetMetric({ ...metric, upper_bound_percentile: (value ?? 0) / 100 })}
-                        type="number"
-                        step={1}
-                        suffix={<span className="text-sm">%</span>}
-                    />
-                )}
+                <LemonInput
+                    value={metric.upper_bound_percentile !== undefined ? metric.upper_bound_percentile * 100 : 0}
+                    onChange={(value) =>
+                        metric.upper_bound_percentile !== undefined &&
+                        handleSetMetric({ ...metric, upper_bound_percentile: (value ?? 0) / 100 })
+                    }
+                    type="number"
+                    step={1}
+                    suffix={<span className="text-sm">%</span>}
+                    size="small"
+                    className={`w-20 transition-opacity ${
+                        metric.upper_bound_percentile === undefined ? 'opacity-0 invisible' : 'opacity-100 visible'
+                    }`}
+                />
             </div>
         </div>
     )
