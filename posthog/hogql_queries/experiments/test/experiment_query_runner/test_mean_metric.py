6fdbd553bac3564fc9b1f5163e2323c740571e76
Anders Asheim Hennum <git@andehen.net>
2025-04-08 09:43:38 +0200

    add test for outlier handling on count metrics

--- posthog/hogql_queries/experiments/test/experiment_query_runner/test_mean_metric.py
+++ posthog/hogql_queries/experiments/test/experiment_query_runner/test_mean_metric.py
@@ -67,7 +67,7 @@ class TestExperimentMeanMetric(ExperimentQueryRunnerBaseTest):
 
     @freeze_time("2024-01-01T12:00:00Z")
     @snapshot_clickhouse_queries
-    def test_outlier_handling(self):
+    def test_outlier_handling_for_sum_metric(self):
         feature_flag = self.create_feature_flag()
         experiment = self.create_experiment(feature_flag=feature_flag)
         experiment.stats_config = {"version": 2}
@@ -157,5 +157,102 @@ class TestExperimentMeanMetric(ExperimentQueryRunnerBaseTest):
 
         self.assertEqual(control_variant.count, 1055)
         self.assertEqual(test_variant.count, 1055)
+        self.assertEqual(control_variant.absolute_exposure, 10)
+        self.assertEqual(test_variant.absolute_exposure, 10)
+
+    @freeze_time("2024-01-01T12:00:00Z")
+    @snapshot_clickhouse_queries
+    def test_outlier_handling_for_count_metric(self):
+        feature_flag = self.create_feature_flag()
+        experiment = self.create_experiment(feature_flag=feature_flag)
+        experiment.stats_config = {"version": 2}
+        experiment.save()
+
+        ff_property = f"$feature/{feature_flag.key}"
+
+        def _create_events_for_user(variant: str, count: int) -> list[dict]:
+            purchase_events = [
+                {
+                    "event": "purchase",
+                    "timestamp": f"2024-01-02T12:01:{i:02d}",
+                    "properties": {
+                        ff_property: variant,
+                    },
+                }
+                for i in range(count)
+            ]
+            return [
+                {
+                    "event": "$feature_flag_called",
+                    "timestamp": "2024-01-02T12:00:00",
+                    "properties": {
+                        "$feature_flag_response": variant,
+                        ff_property: variant,
+                        "$feature_flag": feature_flag.key,
+                    },
+                },
+                *purchase_events,
+            ]
+
+        journeys_for(
+            {
+                "control_1": _create_events_for_user("control", 1),
+                "control_2": _create_events_for_user("control", 3),
+                "control_3": _create_events_for_user("control", 3),
+                "control_4": _create_events_for_user("control", 3),
+                "control_5": _create_events_for_user("control", 3),
+                "control_6": _create_events_for_user("control", 3),
+                "control_7": _create_events_for_user("control", 3),
+                "control_8": _create_events_for_user("control", 3),
+                "control_9": _create_events_for_user("control", 3),
+                "control_10": _create_events_for_user("control", 100),
+                "test_1": _create_events_for_user("test", 3),
+                "test_2": _create_events_for_user("test", 3),
+                "test_3": _create_events_for_user("test", 3),
+                "test_4": _create_events_for_user("test", 3),
+                "test_5": _create_events_for_user("test", 3),
+                "test_6": _create_events_for_user("test", 3),
+                "test_7": _create_events_for_user("test", 3),
+                "test_8": _create_events_for_user("test", 3),
+                "test_9": _create_events_for_user("test", 3),
+                "test_10": _create_events_for_user("test", 3),
+            },
+            self.team,
+        )
+
+        flush_persons_and_events()
+
+        metric = ExperimentMeanMetric(
+            source=EventsNode(
+                event="purchase",
+                math=ExperimentMetricMathType.TOTAL,
+            ),
+            lower_bound_percentile=0.1,
+            upper_bound_percentile=0.9,
+        )
+
+        experiment_query = ExperimentQuery(
+            experiment_id=experiment.id,
+            kind="ExperimentQuery",
+            metric=metric,
+        )
+
+        experiment.metrics = [metric.model_dump(mode="json")]
+        experiment.save()
+
+        query_runner = ExperimentQueryRunner(query=experiment_query, team=self.team)
+        result = query_runner.calculate()
+
+        self.assertEqual(len(result.variants), 2)
+
+        control_variant = cast(
+            ExperimentVariantTrendsBaseStats, next(variant for variant in result.variants if variant.key == "control")
+        )
+        test_variant = cast(
+            ExperimentVariantTrendsBaseStats, next(variant for variant in result.variants if variant.key == "test")
+        )
+
+        self.assertEqual(control_variant.count, 30)
+        self.assertEqual(test_variant.count, 30)
         self.assertEqual(control_variant.absolute_exposure, 10)
         self.assertEqual(test_variant.absolute_exposure, 10)
