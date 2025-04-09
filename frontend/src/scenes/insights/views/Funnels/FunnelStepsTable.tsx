PostHog/posthog

commit 1324125be17df2fd4f8856e4048fcaa1c0ad2b9b

fix(cohorts): get the cohorts rendering correctly in insights (#30530)

frontend/src/scenes/insights/views/Funnels/FunnelStepsTable.tsx
@@ -36,7 +36,7 @@ export function FunnelStepsTable(): JSX.Element | null {
 
     const isOnlySeries = flattenedBreakdowns.length <= 1
 
-    const { cohorts } = useValues(cohortsModel)
+    const { allCohorts } = useValues(cohortsModel)
     const { formatPropertyValueForDisplay } = useValues(propertyDefinitionsModel)
 
     const allChecked = flattenedBreakdowns?.every(
@@ -93,7 +93,7 @@ export function FunnelStepsTable(): JSX.Element | null {
                                 {formatBreakdownLabel(
                                     value,
                                     breakdownFilter,
-                                    cohorts.results,
+                                    allCohorts.results,
                                     formatPropertyValueForDisplay
                                 )}
                                 {showCustomizationIcon && (

frontend/src/lib/components/InsightLegend/InsightLegendRow.tsx
@@ -22,7 +22,7 @@ type InsightLegendRowProps = {
 }
 
 export function InsightLegendRow({ rowIndex, item }: InsightLegendRowProps): JSX.Element {
-    const { cohorts } = useValues(cohortsModel)
+    const { allCohorts } = useValues(cohortsModel)
     const { formatPropertyValueForDisplay } = useValues(propertyDefinitionsModel)
 
     const { insightProps, highlightedSeries } = useValues(insightLogic)
@@ -48,7 +48,7 @@ export function InsightLegendRow({ rowIndex, item }: InsightLegendRowProps): JSX
     const formattedBreakdownValue = formatBreakdownLabel(
         item.breakdown_value,
         breakdownFilter,
-        cohorts.results,
+        allCohorts.results,
         formatPropertyValueForDisplay
     )
 

frontend/src/models/cohortsModel.ts
@@ -225,6 +225,13 @@ export const cohortsModel = kea<cohortsModelType>([
             }
             actions.setPollTimeout(window.setTimeout(actions.loadCohorts, POLL_TIMEOUT))
         },
+        loadAllCohortsSuccess: async ({ allCohorts }: { allCohorts: CountedPaginatedResponse<CohortType> }) => {
+            const is_calculating = allCohorts.results.filter((cohort) => cohort.is_calculating).length > 0
+            if (!is_calculating || !router.values.location.pathname.includes(urls.cohorts())) {
+                return
+            }
+            actions.setPollTimeout(window.setTimeout(actions.loadAllCohorts, POLL_TIMEOUT))
+        },
         exportCohortPersons: async ({ id, columns }) => {
             const exportCommand = {
                 export_format: ExporterFormat.CSV,

frontend/src/queries/nodes/InsightViz/ResultCustomizationsModal.tsx
@@ -86,7 +86,7 @@ type TrendsInfoProps = {
 }
 
 function TrendsInfo({ dataset, resultCustomizationBy }: TrendsInfoProps): JSX.Element {
-    const { cohorts } = useValues(cohortsModel)
+    const { allCohorts } = useValues(cohortsModel)
     const { formatPropertyValueForDisplay } = useValues(propertyDefinitionsModel)
     const { breakdownFilter } = useValues(insightVizDataLogic)
 
@@ -103,7 +103,7 @@ function TrendsInfo({ dataset, resultCustomizationBy }: TrendsInfoProps): JSX.El
                         {formatBreakdownLabel(
                             dataset.breakdown_value,
                             breakdownFilter,
-                            cohorts.results,
+                            allCohorts.results,
                             formatPropertyValueForDisplay
                         )}
                     </b>

frontend/src/scenes/funnels/FunnelTooltip.tsx
@@ -41,7 +41,7 @@ export function FunnelTooltip({
     breakdownFilter,
     embedded = false,
 }: FunnelTooltipProps): JSX.Element {
-    const { cohorts } = useValues(cohortsModel)
+    const { allCohorts } = useValues(cohortsModel)
     const { formatPropertyValueForDisplay } = useValues(propertyDefinitionsModel)
     return (
         <div
@@ -58,7 +58,7 @@ export function FunnelTooltip({
                     {formatBreakdownLabel(
                         series.breakdown_value,
                         breakdownFilter,
-                        cohorts.results,
+                        allCohorts.results,
                         formatPropertyValueForDisplay
                     )}
                 </strong>

frontend/src/scenes/insights/InsightTooltip/insightTooltipUtils.tsx
@@ -106,7 +106,7 @@ export function invertDataSource(
     breakdownFilter: BreakdownFilter | null | undefined
 ): InvertedSeriesDatum[] {
     // NOTE: Assuming these logics are mounted elsewhere, and we're not interested in tracking changes.
-    const cohorts = cohortsModel.findMounted()?.values?.cohorts
+    const cohorts = cohortsModel.findMounted()?.values?.allCohorts
     const formatPropertyValueForDisplay = propertyDefinitionsModel.findMounted()?.values?.formatPropertyValueForDisplay
     const flattenedData: Record<string, InvertedSeriesDatum> = {}
     seriesData.forEach((s) => {

frontend/src/scenes/insights/views/InsightsTable/InsightsTable.tsx
@@ -98,7 +98,7 @@ export function InsightsTable({
         }
     }
 
-    const { cohorts } = useValues(cohortsModel)
+    const { allCohorts } = useValues(cohortsModel)
     const { formatPropertyValueForDisplay } = useValues(propertyDefinitionsModel)
 
     const hasCheckboxes =
@@ -154,7 +154,12 @@ export function InsightsTable({
 
     if (breakdownFilter?.breakdown) {
         const formatItemBreakdownLabel = (item: IndexedTrendResult): string =>
-            formatBreakdownLabel(item.breakdown_value, breakdownFilter, cohorts?.results, formatPropertyValueForDisplay)
+            formatBreakdownLabel(
+                item.breakdown_value,
+                breakdownFilter,
+                allCohorts?.results,
+                formatPropertyValueForDisplay
+            )
 
         columns.push({
             title: <BreakdownColumnTitle breakdownFilter={breakdownFilter} />,
@@ -190,7 +195,7 @@ export function InsightsTable({
                 formatBreakdownLabel(
                     Array.isArray(item.breakdown_value) ? item.breakdown_value[index] : item.breakdown_value,
                     breakdownFilter,
-                    cohorts?.results,
+                    allCohorts?.results,
                     formatPropertyValueForDisplay,
                     index
                 )

frontend/src/scenes/trends/viz/ActionsPie.tsx
@@ -19,7 +19,7 @@ export function ActionsPie({ inSharedMode, showPersonsModal = true, context }: C
     const [data, setData] = useState<GraphDataset[] | null>(null)
     const [total, setTotal] = useState(0)
 
-    const { cohorts } = useValues(cohortsModel)
+    const { allCohorts } = useValues(cohortsModel)
     const { formatPropertyValueForDisplay } = useValues(propertyDefinitionsModel)
 
     const { insightProps } = useValues(insightLogic)
@@ -60,7 +60,7 @@ export function ActionsPie({ inSharedMode, showPersonsModal = true, context }: C
                     return formatBreakdownLabel(
                         item.breakdown_value,
                         breakdownFilter,
-                        cohorts.results,
+                        allCohorts.results,
                         formatPropertyValueForDisplay
                     )
                 }),

commit 356e789f758df950989fbf04b3579e90df62b55a

fix cohorts (broken after https://github.com/PostHog/posthog/pull/30530)

frontend/src/scenes/insights/views/Funnels/FunnelStepsTable.tsx
@@ -90,13 +90,13 @@ export function FunnelStepsTable(): JSX.Element | null {
                         const labelA = formatBreakdownLabel(
                             valueA,
                             breakdownFilter,
-                            cohorts.results,
+                            allCohorts.results,
                             formatPropertyValueForDisplay
                         )
                         const labelB = formatBreakdownLabel(
                             valueB,
                             breakdownFilter,
-                            cohorts.results,
+                            allCohorts.results,
                             formatPropertyValueForDisplay
                         )
                         return compareFn()(labelA, labelB)