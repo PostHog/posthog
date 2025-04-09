0065a172f8f9c12b184403adc833f42289847719
Hugues Pouillot <hpouillot@gmail.com>
2025-04-09 15:14:10 +0200

    replace with const

--- frontend/src/scenes/error-tracking/components/RuntimeIcon.tsx
+++ frontend/src/scenes/error-tracking/components/RuntimeIcon.tsx
@@ -1,11 +1,14 @@
 import { ErrorTrackingRuntime } from 'lib/components/Errors/types'
 import { IconJavascript, IconNodeJS, IconPython, LemonIconProps } from 'lib/lemon-ui/icons'
-import { match } from 'ts-pattern'
+import React from 'react'
 
-export function RuntimeIcon({ runtime, ...props }: { runtime?: ErrorTrackingRuntime } & LemonIconProps): JSX.Element {
-    return match(runtime)
-        .with('python', () => <IconPython {...props} />)
-        .with('web', () => <IconJavascript {...props} />)
-        .with('node', () => <IconNodeJS {...props} />)
-        .otherwise(() => <></>)
+const RuntimeIconMap = {
+    python: IconPython,
+    web: IconJavascript,
+    node: IconNodeJS,
+    unknown: React.Fragment,
+}
+export function RuntimeIcon({ runtime, ...props }: { runtime: ErrorTrackingRuntime } & LemonIconProps): JSX.Element {
+    const Icon = RuntimeIconMap[runtime]
+    return <Icon {...props} />
 }
