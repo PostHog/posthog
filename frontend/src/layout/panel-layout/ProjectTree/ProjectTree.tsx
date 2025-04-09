977c4e1e8f755e0a8f687eccfe04c634170343da
Marius Andra <marius.andra@gmail.com>
2025-04-09 16:28:59 +0200

    fix(project-tree): drag by id, drag multiple (#30998)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Adam Leith <adamleithp@users.noreply.github.com>

--- frontend/src/layout/panel-layout/ProjectTree/ProjectTree.tsx
+++ frontend/src/layout/panel-layout/ProjectTree/ProjectTree.tsx
@@ -25,6 +25,7 @@ export function ProjectTree(): JSX.Element {
         treeItemsNew,
         checkedItems,
         checkedItemsCount,
+        checkedItemCountNumeric,
     } = useValues(projectTreeLogic)
 
     const {
@@ -133,7 +134,7 @@ export function ProjectTree(): JSX.Element {
                         asChild
                         onClick={(e: any) => {
                             e.stopPropagation()
-                            rename(item.record.path)
+                            rename(item.record as unknown as FileSystemEntry)
                         }}
                     >
                         <ButtonPrimitive menuItem>Rename</ButtonPrimitive>
@@ -218,6 +219,7 @@ export function ProjectTree(): JSX.Element {
                 }}
                 enableMultiSelection={checkedItemsCount !== '0'}
                 onItemChecked={onItemChecked}
+                checkedItemCount={checkedItemCountNumeric}
                 onNodeClick={(node) => {
                     if (!isLayoutPanelPinned) {
                         clearActivePanelIdentifier()
@@ -243,29 +245,36 @@ export function ProjectTree(): JSX.Element {
                 onSetExpandedItemIds={searchTerm ? setExpandedSearchFolders : setExpandedFolders}
                 enableDragAndDrop={true}
                 onDragEnd={(dragEvent) => {
-                    const oldPath = dragEvent.active.id as string
-                    const folder = dragEvent.over?.id
-
-                    if (oldPath === folder) {
+                    const itemToId = (item: FileSystemEntry): string =>
+                        item.type === 'folder' ? 'project-folder/' + item.path : 'project/' + item.id
+                    const oldId = dragEvent.active.id as string
+                    const newId = dragEvent.over?.id
+                    if (oldId === newId) {
                         return false
                     }
+                    const oldItem = viableItems.find((i) => itemToId(i) === oldId)
+                    const newItem = viableItems.find((i) => itemToId(i) === newId)
+                    if (oldItem === newItem || !oldItem || !newItem) {
+                        return false
+                    }
+                    const oldPath = oldItem.path
+                    const folder = newItem.path
 
-                    if (folder === '') {
+                    if (checkedItems[oldId]) {
+                        moveCheckedItems(folder)
+                    } else if (folder === '') {
                         const oldSplit = splitPath(oldPath)
                         const oldFile = oldSplit.pop()
                         if (oldFile && oldSplit.length > 0) {
-                            moveItem(oldPath, joinPath([oldFile]))
+                            moveItem(oldItem, joinPath([oldFile]))
                         }
                     } else if (folder) {
-                        const item = viableItems.find((i) => i.path === folder)
-                        if (!item || item.type === 'folder') {
-                            const oldSplit = splitPath(oldPath)
-                            const oldFile = oldSplit.pop()
-                            if (oldFile) {
-                                const newFile = joinPath([...splitPath(String(folder)), oldFile])
-                                if (newFile !== oldPath) {
-                                    moveItem(oldPath, newFile)
-                                }
+                        const oldSplit = splitPath(oldPath)
+                        const oldFile = oldSplit.pop()
+                        if (oldFile) {
+                            const newFile = joinPath([...splitPath(String(folder)), oldFile])
+                            if (oldItem && newFile !== oldPath) {
+                                moveItem(oldItem, newFile)
                             }
                         }
                     }
