685df3468a166ced226aedb3ad9217227e074e68
David Newell <david@posthog.com>
2025-04-11 18:25:30 +0100

    move logger inside

--- posthog/exceptions_capture.py
+++ posthog/exceptions_capture.py
@@ -1,11 +1,9 @@
-import structlog
-
-logger = structlog.get_logger(__name__)
-
-
 def capture_exception(error=None, sentry_scope=None, **sentry_scope_kwargs):
     from sentry_sdk import capture_exception as sentry_capture_exception
     from posthoganalytics import api_key, capture_exception as posthog_capture_exception
+    import structlog
+
+    logger = structlog.get_logger(__name__)
 
     sentry_capture_exception(error, scope=sentry_scope, **sentry_scope_kwargs)
 
