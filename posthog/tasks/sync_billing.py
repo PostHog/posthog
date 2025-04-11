32d7dc92419fc7ce2c9bc19930a92958c297967b
Zach Waterfield <zlwaterfield@gmail.com>
2025-04-11 12:36:18 -0400

    clean up billing manager

--- posthog/tasks/sync_billing.py
+++ posthog/tasks/sync_billing.py
@@ -1,13 +1,12 @@
 from celery import shared_task
 
-from posthog.models import Organization, OrganizationMembership
-from ee.billing.billing_manager import BillingManager
-from posthog.cloud_utils import get_cached_instance_license
 from sentry_sdk import capture_message
 
 
 @shared_task(ignore_result=True, rate_limit="5/s")
 def sync_to_members_to_billing(organization_id: str) -> None:
+    from posthog.models import Organization, OrganizationMembership
+
     organization = Organization.objects.get(id=organization_id)
 
     first_owner = organization.members.filter(
@@ -23,6 +22,10 @@ def sync_to_members_to_billing(organization_id: str) -> None:
 
 @shared_task(ignore_result=True, rate_limit="5/s")
 def sync_from_billing(organization_id: str) -> None:
+    from posthog.models import Organization
+    from ee.billing.billing_manager import BillingManager
+    from posthog.cloud_utils import get_cached_instance_license
+
     license = get_cached_instance_license()
     billing_manager = BillingManager(license, None)
 
