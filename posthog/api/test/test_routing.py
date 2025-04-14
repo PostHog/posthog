2453a8c6ef1663fb12b6a078aa858ff036d3c494
Zach Waterfield <zlwaterfield@gmail.com>
2025-04-14 14:18:26 -0400

    Update test_routing.py

--- posthog/api/test/test_routing.py
+++ posthog/api/test/test_routing.py
@@ -9,7 +9,6 @@ from posthog.api.annotation import AnnotationSerializer
 from posthog.api.routing import DefaultRouterPlusPlus
 from posthog.models.annotation import Annotation
 from posthog.models.organization import Organization
-from posthog.models.project import Project
 from posthog.models.team.team import Team
 from posthog.test.base import APIBaseTest
 
@@ -22,11 +21,8 @@ class FooViewSet(TeamAndOrgViewSetMixin, viewsets.ModelViewSet):
 
 test_router = DefaultRouterPlusPlus()
 
-test_environments_router = test_router.register(r"environments", FooViewSet, "environments")
-test_environments_router.register(r"foos", FooViewSet, "environment_foos", ["team_id"])
-
 test_projects_router = test_router.register(r"projects", FooViewSet, "projects")
-test_projects_router.register(r"foos", FooViewSet, "project_foos", ["project_id"])
+test_projects_router.register(r"foos", FooViewSet, "project_foos", ["team_id"])
 
 test_organizations_router = test_router.register(r"organizations", FooViewSet, "organizations")
 test_organizations_router.register(r"foos", FooViewSet, "organization_foos", ["organization_id"])
@@ -45,30 +41,20 @@ class TestTeamAndOrgViewSetMixin(APIBaseTest):
         super().setUp()
         other_org, _, other_org_team = Organization.objects.bootstrap(user=self.user)
         self.other_org_annotation = Annotation.objects.create(team=other_org_team, organization=other_org)
-        _, other_project_team = Project.objects.create_with_team(
-            initiating_user=self.user, organization=self.organization
-        )
-        self.other_project_annotation = Annotation.objects.create(
-            team=other_project_team, organization=self.organization
-        )
-        other_team = Team.objects.create(organization=self.organization, project=self.project)
-        self.other_team_annotation = Annotation.objects.create(team=other_team, organization=self.organization)
+        team_2 = Team.objects.create(organization=self.organization)
+        self.other_team_annotation = Annotation.objects.create(team=team_2, organization=self.organization)
         self.current_team_annotation = Annotation.objects.create(team=self.team, organization=self.organization)
 
-    def test_environment_nested_filtering(self):
-        response = self.client.get(f"/api/projects/{self.team.id}/foos/")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["count"], 1)  # Just current_team_annotation
-
+    # Note: these need to be revisited for the restful endpoints for environments via root team implementation
     def test_project_nested_filtering(self):
         response = self.client.get(f"/api/projects/{self.team.id}/foos/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["count"], 2)  # Both current_team_annotation and other_team_annotation
+        self.assertEqual(response.json()["count"], 1)  # Both current_team_annotation and other_team_annotation
 
     def test_organization_nested_filtering(self):
         response = self.client.get(f"/api/organizations/{self.organization.id}/foos/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["count"], 3)  # All except other_org_annotation
+        self.assertEqual(response.json()["count"], 2)  # All except other_org_annotation
 
     def test_cannot_override_special_methods(self):
         with pytest.raises(Exception) as e:
