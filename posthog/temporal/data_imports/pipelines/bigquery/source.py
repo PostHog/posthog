4849e369914cc89f7d34553404103c778e3c1f24
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-04-15 16:44:24 +0200

    feat(bigquery): Implement partitioning

--- posthog/temporal/data_imports/pipelines/bigquery/source.py
+++ posthog/temporal/data_imports/pipelines/bigquery/source.py
@@ -1,5 +1,6 @@
 import collections.abc
 import contextlib
+import math
 import typing
 from datetime import date, datetime
 
@@ -13,7 +14,10 @@ from posthog.temporal.data_imports.pipelines.bigquery import bigquery_client
 from posthog.temporal.data_imports.pipelines.helpers import incremental_type_to_initial_value
 from posthog.temporal.data_imports.pipelines.pipeline.consts import DEFAULT_TABLE_SIZE_BYTES
 from posthog.temporal.data_imports.pipelines.pipeline.typings import SourceResponse
-from posthog.warehouse.types import IncrementalFieldType
+from posthog.temporal.data_imports.pipelines.pipeline.utils import (
+    DEFAULT_PARTITION_TARGET_SIZE_IN_BYTES,
+)
+from posthog.warehouse.types import IncrementalFieldType, PartitionSettings
 
 
 @contextlib.contextmanager
@@ -35,6 +39,29 @@ def bigquery_storage_read_client(
     client = bigquery_storage.BigQueryReadClient(credentials=credentials)
 
     yield client
+
+
+def get_partition_settings(
+    table: bigquery.Table,
+    client: bigquery.Client,
+    partition_size_bytes: int = DEFAULT_PARTITION_TARGET_SIZE_IN_BYTES,
+) -> PartitionSettings | None:
+    """Get partition settings for given BigQuery table."""
+    table = client.get_table(table)
+
+    if not table.num_rows or not table.num_bytes:
+        return None
+
+    avg_row_size = table.num_bytes / table.num_rows
+
+    # Partition must have at least one row
+    partition_size = max(round(partition_size_bytes / avg_row_size), 1)
+    partition_count = math.floor(table.num_rows / partition_size)
+
+    if partition_count == 0:
+        return PartitionSettings(partition_count=1, partition_size=partition_size)
+
+    return PartitionSettings(partition_count=partition_count, partition_size=partition_size)
 
 
 def get_primary_keys(table: bigquery.Table, client: bigquery.Client) -> list[str] | None:
@@ -105,6 +132,7 @@ def bigquery_source(
     ) as bq_client:
         bq_table = bq_client.get_table(fully_qualified_table_name)
         primary_keys = get_primary_keys(bq_table, bq_client)
+        partition_settings = get_partition_settings(bq_table, bq_client)
 
     def get_rows(max_table_size: int) -> collections.abc.Iterator[pa.Table]:
         with bigquery_client(
@@ -216,4 +244,10 @@ def bigquery_source(
                 if record_batches:
                     yield pa.Table.from_batches(record_batches)
 
-    return SourceResponse(name=name, items=get_rows(DEFAULT_TABLE_SIZE_BYTES), primary_keys=primary_keys)
+    return SourceResponse(
+        name=name,
+        items=get_rows(DEFAULT_TABLE_SIZE_BYTES),
+        primary_keys=primary_keys,
+        partition_count=partition_settings.partition_count if partition_settings else None,
+        partition_size=partition_settings.partition_size if partition_settings else None,
+    )
