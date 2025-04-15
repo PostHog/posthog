95c17a62a6111bd8281c7071b773bb09083e0b96
Alex V <alexander.veryaysky@gmail.com>
2025-04-15 09:59:54 +0200

    chore: show what filter applied

--- frontend/src/scenes/session-recordings/filters/RecordingsUniversalFilters.tsx
+++ frontend/src/scenes/session-recordings/filters/RecordingsUniversalFilters.tsx
@@ -241,30 +241,31 @@ export const RecordingsUniversalFilters = ({
                     </div>
                     {(totalFiltersCount ?? 0) > 0 && (
                         <div className="flex gap-2 p-2 justify-start">
-                            <LemonInput
-                                value={savedFilterName}
-                                placeholder="Saved filter name"
-                                onChange={setSavedFilterName}
-                                size="small"
-                                autoFocus
-                                fullWidth
-                                disabled={savedFilters.results?.find((filter) => equal(filter.filters, filters))}
-                                onClick={(e) => {
-                                    e.stopPropagation() // Prevent dropdown from closing
-                                }}
-                            />
-                            <LemonButton
-                                type="primary"
-                                size="xsmall"
-                                onClick={() => void newPlaylistHandler()}
-                                disabledReason={
-                                    savedFilters.results?.find((filter) => equal(filter.filters, filters))
-                                        ? 'You already have a saved filter with these settings'
-                                        : undefined
-                                }
-                            >
-                                Save filters
-                            </LemonButton>
+                            {savedFilters.results?.find((filter) => equal(filter.filters, filters)) ? (
+                                <div className="text-sm italic text-text-secondary inline-flex items-center font-medium gap-1">
+                                    "
+                                    {savedFilters.results?.find((filter) => equal(filter.filters, filters))?.name ||
+                                        'Unnamed'}
+                                    " saved filter applied
+                                </div>
+                            ) : (
+                                <>
+                                    <LemonInput
+                                        value={savedFilterName}
+                                        placeholder="Saved filter name"
+                                        onChange={setSavedFilterName}
+                                        size="small"
+                                        autoFocus
+                                        fullWidth
+                                        onClick={(e) => {
+                                            e.stopPropagation() // Prevent dropdown from closing
+                                        }}
+                                    />
+                                    <LemonButton type="primary" size="xsmall" onClick={() => void newPlaylistHandler()}>
+                                        Save filters
+                                    </LemonButton>
+                                </>
+                            )}
                         </div>
                     )}
                 </div>
