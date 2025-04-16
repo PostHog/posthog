9dc1802c62769a8180cef6f793d543db4e3f12b6
Eli Reisman <eli.r@posthog.com>
2025-04-16 11:47:48 -0700

    fix(plugin-server): quick fix for int overflow in lazy loader fetchTeams (#31315)
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- plugin-server/src/utils/team-manager-lazy.ts
+++ plugin-server/src/utils/team-manager-lazy.ts
@@ -101,9 +101,14 @@ export class TeamManagerLazy {
         const [teamIds, tokens] = teamIdOrTokens.reduce(
             ([teamIds, tokens], idOrToken) => {
                 // TRICKY: We are caching ids and tokens so we need to determine which is which
-                // Fix this to be a prefix based lookup
-                if (/^\d+$/.test(idOrToken)) {
-                    teamIds.push(parseInt(idOrToken))
+                // Fix this to be a prefix based lookup. Added hack to limit to positive integer
+                // that shouldn't overflow 32-bit integer in DB column.
+                if (/^\d{1,10}$/.test(idOrToken)) {
+                    const parsed = parseInt(idOrToken)
+                    // TODO: stat if this happens
+                    if (!isNaN(parsed) && parsed > 0 && parsed <= 2147483647) {
+                        teamIds.push(parsed)
+                    }
                 } else {
                     tokens.push(idOrToken)
                 }
@@ -114,7 +119,7 @@ export class TeamManagerLazy {
 
         const result = await this.postgres.query<RawTeam>(
             PostgresUse.COMMON_READ,
-            `SELECT 
+            `SELECT
                 t.id,
                 t.project_id,
                 t.uuid,
