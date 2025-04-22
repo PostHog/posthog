e5092cc59cc4cf05f5e16025a3038d51e04fa5c6
jabahamondes <javier@posthog.com>
2025-04-22 18:07:30 -0400

    feat(web-analytics): moving Active Users widget to own section (#31462)
    Co-authored-by: Javier Bahamondes <javierbahamondes@Javiers-MacBook-Pro.local>

--- frontend/src/scenes/web-analytics/EventsHeatMap/EventsHeatMap.tsx
+++ frontend/src/scenes/web-analytics/EventsHeatMap/EventsHeatMap.tsx
@@ -1,10 +1,10 @@
 import './EventsHeatMap.scss'
 
 import { useValues } from 'kea'
-import { InsightsWrapper } from 'products/revenue_analytics/frontend/nodes/utils'
 import React, { useCallback, useEffect, useState } from 'react'
 import { dataThemeLogic } from 'scenes/dataThemeLogic'
 import { InsightLoadingState } from 'scenes/insights/EmptyStates'
+import { InsightsWrapper } from 'scenes/insights/InsightsWrapper'
 
 import { useResizeObserver } from '~/lib/hooks/useResizeObserver'
 import { dataNodeLogic } from '~/queries/nodes/DataNode/dataNodeLogic'
