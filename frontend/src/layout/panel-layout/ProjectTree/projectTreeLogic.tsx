073386ccc721a8d32b463a9e0fa2cc14188a5446
Marius Andra <marius.andra@gmail.com>
2025-04-22 17:29:31 +0200

    curridx

--- frontend/src/layout/panel-layout/ProjectTree/projectTreeLogic.tsx
+++ frontend/src/layout/panel-layout/ProjectTree/projectTreeLogic.tsx
@@ -948,10 +948,13 @@ export const projectTreeLogic = kea<projectTreeLogicType>([
             if (shift && lastCheckedItem) {
                 const prevIdx = shownItems.findIndex((it) => itemKey(it) === lastCheckedItem.id)
                 const currIdx = shownItems.findIndex((it) => it.id === clickedItem.id)
-                const [start, end] = [Math.min(prevIdx, currIdx), Math.max(prevIdx, currIdx)]
-
-                for (let i = start; i <= end; i++) {
-                    applyToItem(shownItems[i])
+                if (prevIdx !== -1 && currIdx === -1) {
+                    const [start, end] = [Math.min(prevIdx, currIdx), Math.max(prevIdx, currIdx)]
+                    for (let i = start; i <= end; i++) {
+                        applyToItem(shownItems[i])
+                    }
+                } else if (currIdx !== -1) {
+                    applyToItem(shownItems[currIdx])
                 }
             } else {
                 applyToItem(clickedItem)
