1ac3aea1af024b03870b30845b0505a8648b8e54
meikel <meikel@posthog.com>
2025-04-23 16:29:20 +0200

    add migration

--- posthog/models/__init__.py
+++ posthog/models/__init__.py
@@ -52,6 +52,7 @@ from .group import Group
 from .group_type_mapping import GroupTypeMapping
 from .host_definition import HostDefinition
 from .hog_functions import HogFunction
+from .hog_function_template import HogFunctionTemplate
 from .insight import Insight, InsightViewed
 from .insight_caching_state import InsightCachingState
 from .insight_variable import InsightVariable
@@ -133,6 +134,7 @@ __all__ = [
     "Group",
     "GroupTypeMapping",
     "HogFunction",
+    "HogFunctionTemplate",
     "HostDefinition",
     "Insight",
     "InsightCachingState",
