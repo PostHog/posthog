b46d9620827b0dc285ce3bba752ed1be383e335b
Anders Asheim Hennum <git@andehen.net>
2025-04-23 08:46:02 +0200

    Get rid of foreginObject and svg for chart loading state

--- frontend/src/scenes/experiments/MetricsView/ChartLoadingState.tsx
+++ frontend/src/scenes/experiments/MetricsView/ChartLoadingState.tsx
@@ -1,21 +1,12 @@
 interface ChartLoadingStateProps {
-    width: number
     height: number
 }
 
-export function ChartLoadingState({ width, height }: ChartLoadingStateProps): JSX.Element {
+export function ChartLoadingState({ height }: ChartLoadingStateProps): JSX.Element {
     return (
-        <svg viewBox={`0 0 ${width} ${height}`} preserveAspectRatio="xMidYMid meet">
-            <foreignObject
-                x={width / 2 - 100} // Center the 200px wide container
-                y={height / 2 - 10} // Roughly center vertically
-                width="200"
-                height="20"
-            >
-                <div className="flex items-center justify-center text-secondary cursor-default text-[10px] font-normal">
-                    <span>Results loading&hellip;</span>
-                </div>
-            </foreignObject>
-        </svg>
+        // eslint-disable-next-line react/forbid-dom-props
+        <div className="flex items-center justify-center text-[14px] font-normal" style={{ height: `${height}px` }}>
+            <span>Results loading&hellip;</span>
+        </div>
     )
 }
