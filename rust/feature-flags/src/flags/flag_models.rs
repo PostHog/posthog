fd26530bf827966c46a2c45c81346a928326a99f
Dylan Martin <dylan@posthog.com>
2025-04-23 04:13:54 -0400

    fix(flags): handle flags with empty filters (#31492)

--- rust/feature-flags/src/flags/flag_models.rs
+++ rust/feature-flags/src/flags/flag_models.rs
@@ -6,10 +6,13 @@ use crate::properties::property_models::PropertyFilter;
 // TODO: Add integration tests across repos to ensure this doesn't happen.
 pub const TEAM_FLAGS_CACHE_PREFIX: &str = "posthog:1:team_feature_flags_";
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize, Default)]
 pub struct FlagPropertyGroup {
+    #[serde(default)]
     pub properties: Option<Vec<PropertyFilter>>,
+    #[serde(default)]
     pub rollout_percentage: Option<f64>,
+    #[serde(default)]
     pub variant: Option<String>,
 }
 
@@ -25,13 +28,19 @@ pub struct MultivariateFlagOptions {
     pub variants: Vec<MultivariateFlagVariant>,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize, Default)]
 pub struct FlagFilters {
+    #[serde(default)]
     pub groups: Vec<FlagPropertyGroup>,
+    #[serde(default)]
     pub multivariate: Option<MultivariateFlagOptions>,
+    #[serde(default)]
     pub aggregation_group_type_index: Option<i32>,
+    #[serde(default)]
     pub payloads: Option<serde_json::Value>,
+    #[serde(default)]
     pub super_groups: Option<Vec<FlagPropertyGroup>>,
+    #[serde(default)]
     pub holdout_groups: Option<Vec<FlagPropertyGroup>>,
 }
 
