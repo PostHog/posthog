023b3da5c30f5d3c14a8d006588e7f4c8c01e0dd
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-04-23 13:46:43 +0200

    remove old code

--- plugin-server/src/cdp/legacy-plugins/lib/dayjs.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs'
-import advancedFormat from 'dayjs/plugin/advancedFormat'
-import utc from 'dayjs/plugin/utc'
-
-dayjs.extend(utc)
-dayjs.extend(advancedFormat)
-
-export default dayjs
