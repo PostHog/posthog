ff62c872acea88576be0800ea7396166c8c7307a
Tom Owers <owerstom@gmail.com>
2025-04-23 16:38:28 +0100

    Fixed disabled reason on view insight button

--- frontend/src/scenes/data-warehouse/editor/OutputPane.tsx
+++ frontend/src/scenes/data-warehouse/editor/OutputPane.tsx
@@ -411,7 +411,7 @@ export function OutputPane(): JSX.Element {
                     )}
                     {activeTab === OutputTab.Results && (
                         <LemonButton
-                            disabledReason={!hasColumns ? 'No results to visualize' : undefined}
+                            disabledReason={!hasColumns && !editingInsight ? 'No results to visualize' : undefined}
                             type="secondary"
                             onClick={() => setActiveTab(OutputTab.Visualization)}
                             id={`sql-editor-${editingInsight ? 'view' : 'create'}-insight`}
