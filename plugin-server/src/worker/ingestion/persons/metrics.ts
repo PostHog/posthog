8b95e2e716b5e75f81120db65d4a201c2674f34d
Paweł Ledwoń <me@difficult.name>
2025-04-24 15:31:40 -0600

    fix: add an overflow bucket to the person store

--- plugin-server/src/worker/ingestion/persons/metrics.ts
+++ plugin-server/src/worker/ingestion/persons/metrics.ts
@@ -4,5 +4,5 @@ export const personMethodCallsPerBatchHistogram = new Histogram({
     name: 'person_method_calls_per_batch',
     help: 'Number of calls to each person store method per distinct ID per batch',
     labelNames: ['method'],
-    buckets: [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024],
+    buckets: [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, Infinity],
 })
