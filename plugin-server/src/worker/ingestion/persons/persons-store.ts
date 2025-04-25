ebd676b6a0ccc8654da58ebbd1fca1c454e6bb8f
Paweł Ledwoń <me@difficult.name>
2025-04-25 14:04:46 -0600

    chore: update an out-of-sync comment

--- plugin-server/src/worker/ingestion/persons/persons-store.ts
+++ plugin-server/src/worker/ingestion/persons/persons-store.ts
@@ -2,7 +2,7 @@ import { PersonsStoreForBatch } from './persons-store-for-batch'
 
 export interface PersonsStore {
     /**
-     * Returns an instance of BatchPersonsStore for handling person operations in batch
+     * Returns an instance of PersonsStoreForBatch for handling person operations in batch
      */
     forBatch(): PersonsStoreForBatch
 }
