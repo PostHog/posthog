60b8db6647bdc61f5ee1ac6734ad9bd4641722af
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-04-25 09:52:08 +0200

    include ids in tests

--- plugin-server/src/cdp/legacy-plugins/segmentTemplates.test.ts
+++ plugin-server/src/cdp/legacy-plugins/segmentTemplates.test.ts
@@ -1,16 +1,15 @@
 import { SEGMENT_DESTINATIONS } from './segmentTemplates'
 import { LegacyDestinationPlugin } from './types'
 
-const destinationEntries = Object.entries(SEGMENT_DESTINATIONS)
-    .filter(([_, destination]) => destination.template)
+const destinationEntries = Object.entries(SEGMENT_DESTINATIONS).filter(([_, destination]) => destination.template)
 
-const testCases = destinationEntries.map(([id, destination]) => ({
-    id,
-    destination: destination as LegacyDestinationPlugin
+const testCases = destinationEntries.map(([_, destination]) => ({
+    id: destination.template?.id,
+    destination: destination as LegacyDestinationPlugin,
 }))
 
 describe('segment templates', () => {
-    test.each(testCases)('template $id matches expected format', ({ destination }) => {
+    test.each(testCases)('template $id matches expected result', ({ destination }) => {
         expect(destination.template).toMatchSnapshot()
     })
 })
