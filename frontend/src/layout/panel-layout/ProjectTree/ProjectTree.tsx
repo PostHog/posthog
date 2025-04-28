d8e320abef4a88ef68b6a05fd8e9111a8c94c69c
Adam Leith <adamleithp@users.noreply.github.com>
2025-04-28 13:48:14 +0100

    fix(project-tree): drop item to root wasn't working (#31650)

--- frontend/src/layout/panel-layout/ProjectTree/ProjectTree.tsx
+++ frontend/src/layout/panel-layout/ProjectTree/ProjectTree.tsx
@@ -432,11 +432,13 @@ export function ProjectTree(): JSX.Element {
                     }
                     const oldItem = viableItems.find((i) => itemToId(i) === oldId)
                     const newItem = viableItems.find((i) => itemToId(i) === newId)
-                    if (oldItem === newItem || !oldItem || !newItem) {
+                    if (oldItem === newItem || !oldItem) {
                         return false
                     }
+
                     const oldPath = oldItem.path
-                    const folder = newItem.path
+                    // if no path, that means it's a root item
+                    const folder = newItem?.path || ''
 
                     if (checkedItems[oldId]) {
                         moveCheckedItems(folder)
