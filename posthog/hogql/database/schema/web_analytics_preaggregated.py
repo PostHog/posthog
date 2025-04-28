7ca23602e277203be99324e732489a36e036ca7f
Lucas Ricoy <lucas.r@posthog.com>
2025-04-28 20:20:39 -0300

    feat: internal usage of web_overview pre-aggregated
    chore: initial working dagster jogs

---
+++ posthog/hogql/database/schema/web_analytics_preaggregated.py
@@ -0,0 +1,29 @@
+from posthog.hogql.database.models import (
+    IntegerDatabaseField,
+    StringDatabaseField,
+    DateDatabaseField,
+    Table,
+    FieldOrTable,
+)
+
+
+class WebOverviewDailyTable(Table):
+    """Pre-aggregated table for web analytics overview metrics."""
+    
+    fields: dict[str, FieldOrTable] = {
+        "team_id": IntegerDatabaseField(name="team_id"),
+        "day_bucket": DateDatabaseField(name="day_bucket"),
+        "host": StringDatabaseField(name="host", nullable=True),
+        "device_type": StringDatabaseField(name="device_type", nullable=True),
+        "persons_uniq_state": StringDatabaseField(name="persons_uniq_state"),
+        "pageviews_count_state": StringDatabaseField(name="pageviews_count_state"),
+        "sessions_uniq_state": StringDatabaseField(name="sessions_uniq_state"),
+        "total_session_duration_state": StringDatabaseField(name="total_session_duration_state"),
+        "total_bounces_state": StringDatabaseField(name="total_bounces_state"),
+    }
+
+    def to_printed_clickhouse(self, context):
+        return "web_overview_daily"
+
+    def to_printed_hogql(self):
+        return "web_overview_daily"
