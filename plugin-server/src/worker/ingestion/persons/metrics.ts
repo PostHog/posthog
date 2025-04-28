264a83cce28b327d89146300e341702b8e125041
Paweł Ledwoń <me@difficult.name>
2025-04-28 17:04:52 -0600

    feat: add person cache metrics

--- plugin-server/src/worker/ingestion/persons/metrics.ts
+++ plugin-server/src/worker/ingestion/persons/metrics.ts
@@ -1,8 +1,14 @@
-import { Histogram } from 'prom-client'
+import { Counter, Histogram } from 'prom-client'
 
 export const personMethodCallsPerBatchHistogram = new Histogram({
     name: 'person_method_calls_per_batch',
     help: 'Number of calls to each person store method per distinct ID per batch',
     labelNames: ['method'],
     buckets: [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, Infinity],
+})
+
+export const personCacheOperationsCounter = new Counter({
+    name: 'person_cache_operations_total',
+    help: 'Total number of cache hits and misses',
+    labelNames: ['cache', 'operation'],
 })
