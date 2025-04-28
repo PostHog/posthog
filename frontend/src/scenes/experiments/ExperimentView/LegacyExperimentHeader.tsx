e5b46490115bd0d30df2acf61e9b0ff8fab94825
Anders Asheim Hennum <git@andehen.net>
2025-04-28 12:35:23 +0200

    add banner to legacy experiment view

--- frontend/src/scenes/experiments/ExperimentView/LegacyExperimentHeader.tsx
+++ frontend/src/scenes/experiments/ExperimentView/LegacyExperimentHeader.tsx
@@ -1,9 +1,18 @@
+import { Link } from '@posthog/lemon-ui'
+import { LemonBanner } from '@posthog/lemon-ui'
 import { DataCollection } from 'scenes/experiments/ExperimentView/DataCollection'
 
 export function LegacyExperimentHeader(): JSX.Element {
     return (
-        <div className="w-1/2 mt-8 xl:mt-0">
-            <DataCollection />
-        </div>
+        <>
+            <LemonBanner type="info" className="mb-4" dismissKey="experiment-view-new-query-runner-banner-2">
+                New experimentation engine and improved UI available in beta! Would you like to try it? Read more about
+                it
+                <Link to="https://posthog.com/docs/experiments/new-experimentation-engine"> here</Link>.
+            </LemonBanner>
+            <div className="w-1/2 mt-8 xl:mt-0">
+                <DataCollection />
+            </div>
+        </>
     )
 }
