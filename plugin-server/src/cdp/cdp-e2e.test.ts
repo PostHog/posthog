51ab60ef1e9e03d3cc4f677f594b633368a93d25
Ben White <ben@posthog.com>
2025-04-29 13:57:00 +0200

    Fix

--- plugin-server/src/cdp/cdp-e2e.test.ts
+++ plugin-server/src/cdp/cdp-e2e.test.ts
@@ -63,6 +63,7 @@ describe.each([['cyclotron' as const], ['kafka' as const]])('CDP Consumer loop: 
 
             hub.CDP_FETCH_RETRIES = 2
             hub.CDP_FETCH_BACKOFF_BASE_MS = 100 // fast backoff
+            hub.CDP_CYCLOTRON_COMPRESS_KAFKA_DATA = true
             hub.CYCLOTRON_DATABASE_URL = 'postgres://posthog:posthog@localhost:5432/test_cyclotron'
             hub.CDP_CYCLOTRON_DELIVERY_MODE = mode
 
