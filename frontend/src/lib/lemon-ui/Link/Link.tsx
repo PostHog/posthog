e39e8dc3a116d45b5f99c4c17f6579c5051ebb29
Adam Leith <adamleithp@users.noreply.github.com>
2025-05-01 14:41:25 +0100

    removed style props from Link, wasn't used

--- frontend/src/lib/lemon-ui/Link/Link.tsx
+++ frontend/src/lib/lemon-ui/Link/Link.tsx
@@ -62,8 +62,6 @@ export type LinkProps = Pick<React.HTMLProps<HTMLAnchorElement>, 'target' | 'cla
     tooltip?: TooltipProps['title']
     tooltipDocLink?: TooltipProps['docLink']
     tooltipPlacement?: TooltipProps['placement']
-
-    style?: React.CSSProperties
 }
 
 const shouldForcePageLoad = (input: any): boolean => {
