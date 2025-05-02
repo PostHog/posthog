3326a76660413bb9d156e5a5b1d63ad2c10732d4
meikel <meikel@posthog.com>
2025-05-02 11:48:59 +0200

    update

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -440,7 +440,7 @@ class ApiRequest {
     }
 
     public hogFunctionTemplate(id: HogFunctionTemplateType['id'], teamId?: TeamType['id']): ApiRequest {
-        return this.projectsDetail(teamId).addPathComponent(id)
+        return this.hogFunctionTemplates(teamId).addPathComponent(id)
     }
 
     // # Actions
