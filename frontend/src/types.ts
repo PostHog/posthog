b7b7b24db348fc3ce1bf52092d527393a6f54a7b
Phil Haack <haacked@gmail.com>
2025-05-02 13:10:52 -0700

    Add property to typescript types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -517,6 +517,8 @@ export interface TeamBasicType extends WithAccessControl {
     organization: string // Organization ID
     project_id: number
     api_token: string
+    secret_api_token: string
+    secret_api_token_backup: string
     name: string
     completed_snippet_onboarding: boolean
     has_completed_onboarding_for?: Record<string, boolean>
