454dd1cf32ec51675a7f38bf31db0e18ff96ceaf
Alex Lebedev <alex.l@posthog.com>
2025-05-06 10:56:00 +0200

    chore: Move local processing code to a separate file.

--- ee/session_recordings/session_summary/input_data.py
+++ ee/session_recordings/session_summary/input_data.py
@@ -1,9 +1,11 @@
 import datetime
 import re
-from typing import Optional, cast
-import os
-from clickhouse_driver import Client
+from typing import cast
 
+from ee.session_recordings.session_summary.local.input_data import (
+    _get_production_session_events_locally,
+    _get_production_session_metadata_locally,
+)
 from ee.session_recordings.session_summary.utils import (
     get_column_index,
 )
@@ -12,36 +14,6 @@ from posthog.session_recordings.queries.session_replay_events import SessionRepl
 from posthog.models import Team
 
 EXTRA_SUMMARY_EVENT_FIELDS = ["elements_chain_ids", "elements_chain"]
-
-
-def _get_ch_client_local_reads_prod() -> Client:
-    return Client(
-        host=os.environ["LOCAL_READS_PROD_CLICKHOUSE_US_HOST"],
-        user=os.environ["LOCAL_READS_PROD_CLICKHOUSE_US_USER"],
-        password=os.environ["LOCAL_READS_PROD_CLICKHOUSE_US_PASSWORD"],
-        secure=True,
-        verify=False,
-    )
-
-
-def _get_production_session_metadata_locally(
-    events_obj: SessionReplayEvents,
-    session_id: str,
-    team: Team,
-    recording_start_time: Optional[datetime.datetime] = None,
-) -> RecordingMetadata | None:
-    query = events_obj.get_metadata_query(recording_start_time)
-    client = _get_ch_client_local_reads_prod()
-    replay_response = client.execute(
-        query,
-        {
-            "team_id": team.pk,
-            "session_id": session_id,
-            "recording_start_time": recording_start_time,
-        },
-    )
-    recording_metadata = events_obj.build_recording_metadata(session_id, replay_response)
-    return recording_metadata
 
 
 def get_session_metadata(session_id: str, team: Team, local_reads_prod: bool = False) -> RecordingMetadata:
@@ -53,74 +25,6 @@ def get_session_metadata(session_id: str, team: Team, local_reads_prod: bool = F
     if not session_metadata:
         raise ValueError(f"No session metadata found for session_id {session_id}")
     return session_metadata
-
-
-def _interpolate_events_query(events_query: str, events_values: dict) -> str:
-    """
-    Interpolate events query to get valid CH query.
-    """
-
-    def _format_value(v):
-        if isinstance(v, str):
-            safe_v = v.replace("'", "''")
-            return f"'{safe_v}'"
-        elif isinstance(v, datetime.datetime | datetime.date):
-            # Converting into Unix timestamp to avoid timezone issues
-            return str(int(v.astimezone(datetime.UTC).timestamp()))
-        elif isinstance(v, list):
-            # ClickHouse expects arrays as (val1, val2, ...)
-            return "(" + ", ".join(_format_value(x) for x in v) + ")"
-        elif v is None:
-            return "NULL"
-        else:
-            return str(v)
-
-    return events_query.format(**{k: _format_value(v) for k, v in events_values.items()})
-
-
-def _rewrite_properties_fields(query: str) -> str:
-    """
-    Rewrite properties.$field format to extract string from JSON.
-    """
-
-    def _replacer(match):
-        field = match.group(1)
-        return f"JSONExtractString(properties, '${field}') AS {field}"
-
-    select_match = re.search(r"SELECT\s+(.*?)\s+FROM", query, re.DOTALL | re.IGNORECASE)
-    if not select_match:
-        return query
-    select_clause = select_match.group(1)
-    new_select = re.sub(r"properties\.\$([a-zA-Z0-9_]+)", _replacer, select_clause)
-    return query.replace(select_clause, new_select, 1)
-
-
-def _get_production_session_events_locally(
-    events_obj: SessionReplayEvents,
-    session_id: str,
-    metadata: RecordingMetadata,
-    limit: int,
-    page: int,
-    events_to_ignore: list[str] | None = None,
-    extra_fields: list[str] | None = None,
-) -> tuple[list | None, list | None]:
-    """
-    Get session events from production, locally, required for testing session summary
-    """
-    hq = events_obj.get_events_query(
-        session_id=session_id,
-        metadata=metadata,
-        events_to_ignore=events_to_ignore,
-        extra_fields=extra_fields,
-        limit=limit,
-        page=page,
-    )
-    query = _rewrite_properties_fields(hq.query)
-    interpolated_query = _interpolate_events_query(query, hq.values)
-    client = _get_ch_client_local_reads_prod()
-    rows, columns_with_types = client.execute(interpolated_query, with_column_types=True)
-    columns = [col for col, _ in columns_with_types]
-    return columns, rows
 
 
 def get_session_events(
