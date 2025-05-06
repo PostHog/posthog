dd52d2434f016fe836b3c4e50b247ed9fe6c4b02
Rodrigo Iloro <rodrigo@posthog.com>
2025-05-06 19:11:55 -0300

    refactor: comment added to experiment logic.

--- frontend/src/scenes/experiments/experimentLogic.tsx
+++ frontend/src/scenes/experiments/experimentLogic.tsx
@@ -412,6 +412,7 @@ export const experimentLogic = kea<experimentLogicType>([
         setValidExistingFeatureFlag: (featureFlag: FeatureFlagType | null) => ({ featureFlag }),
         setFeatureFlagValidationError: (error: string) => ({ error }),
         validateFeatureFlag: (featureFlagKey: string) => ({ featureFlagKey }),
+        // Running Time Calculator Modal
         openCalculateRunningTimeModal: true,
         closeCalculateRunningTimeModal: true,
     }),
