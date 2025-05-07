5420288c817be1eab79badb97eebf2b961e54253
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2025-05-07 13:11:49 -0700

    test stub

---
+++ dags/tests/test_property_definitions.py
@@ -0,0 +1,6 @@
+from dags.property_definitions import property_definitions_ingestion_job
+from posthog.clickhouse.cluster import ClickhouseCluster
+
+
+def test_ingestion_job(cluster: ClickhouseCluster) -> None:
+    property_definitions_ingestion_job.execute_in_process()
