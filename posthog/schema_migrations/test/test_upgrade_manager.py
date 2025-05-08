877ffd337c8d23bd11fb3e58ed3a4834e902e2a7
Thomas Obermüller <thomas.obermueller@gmail.com>
2025-05-08 14:37:01 +0200

    add upgrade manager

---
+++ posthog/schema_migrations/test/test_upgrade_manager.py
@@ -0,0 +1,42 @@
+import pytest
+from unittest.mock import Mock
+from posthog.schema import NodeKind
+from posthog.schema_migrations import LATEST_VERSIONS, SchemaMigration, MIGRATIONS
+from posthog.schema_migrations.upgrade_manager import upgrade_query, upgrade_query_and_replace_filters
+
+
+class SampleMigration(SchemaMigration):
+    targets = {NodeKind.TRENDS_QUERY: 1}
+
+    def transform(self, query):
+        query["aggregationGroupTypeIndex"] = query.pop("aggregation_group_type_index")
+        return query
+
+
+@pytest.fixture(autouse=True)
+def setup_migrations():
+    LATEST_VERSIONS.clear()
+    MIGRATIONS.clear()
+
+    MIGRATIONS[NodeKind.TRENDS_QUERY] = {1: SampleMigration()}
+    LATEST_VERSIONS[NodeKind.TRENDS_QUERY] = 2
+
+    yield
+
+
+def test_upgrade_query_context_manager():
+    mock_insight = Mock()
+    mock_insight.query = {"kind": NodeKind.TRENDS_QUERY, "v": 1, "aggregation_group_type_index": 2}
+    upgraded_query = {"kind": NodeKind.TRENDS_QUERY, "v": 2, "aggregationGroupTypeIndex": 2}
+
+    with upgrade_query(mock_insight):
+        assert mock_insight.query == upgraded_query
+
+
+def test_upgrade_query_and_replace_filters_manager():
+    mock_insight = Mock()
+    mock_insight.filters = {"aggregation_group_type_index": 2}
+    mock_insight.query = None
+
+    with upgrade_query_and_replace_filters(mock_insight):
+        assert mock_insight.query["aggregationGroupTypeIndex"] == 2
