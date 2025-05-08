00457f50c78bb89787a570b8a535dba9cd445d1b
Ben White <ben@posthog.com>
2025-05-08 10:44:25 -0600

    Fix

--- posthog/migrations/0729_sessionrecordingplaylist_type.py
+++ posthog/migrations/0729_sessionrecordingplaylist_type.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0729_sessionrecordingplaylist_type"),
+        ("posthog", "0728_experiment_conclusion_experiment_conclusion_comment"),
     ]
 
     operations = [
