a3ab842dda29fb750a15cb7d83f6b35a3ef98f83
timgl <tim@posthog.com>
2025-05-08 19:03:26 +0100

    fix(clickhouse): Add distributed_system_processes table

---
+++ posthog/clickhouse/distributed_system_processes.py
@@ -0,0 +1,9 @@
+from posthog.settings.data_stores import CLICKHOUSE_CLUSTER
+
+# Deliberately using skip_unavailable_shards, as the things that use this table are usually not 100% critical, and can have bad knock-on effects if they keep waiting for shards.
+
+DISTRIBUTED_SYSTEM_PROCESSES_TABLE_SQL = f"""
+CREATE TABLE IF NOT EXISTS distributed_system_processes
+ENGINE = Distributed({CLICKHOUSE_CLUSTER}, system, processes)
+SETTINGS skip_unavailable_shards=1
+"""
