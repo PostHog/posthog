69f61606a3ca66f2a2c86cf045398de644300700
timgl <tim@posthog.com>
2025-05-09 14:00:41 +0100

    fix

--- posthog/clickhouse/migrations/0116_add_distributed_system_process_table.py
+++ posthog/clickhouse/migrations/0116_add_distributed_system_process_table.py
@@ -3,7 +3,7 @@ from posthog.clickhouse.distributed_system_processes import DISTRIBUTED_SYSTEM_P
 
 operations = [
     run_sql_with_exceptions(
-        DISTRIBUTED_SYSTEM_PROCESSES_TABLE_SQL,
+        DISTRIBUTED_SYSTEM_PROCESSES_TABLE_SQL(on_cluster=False),
         node_role=NodeRole.ALL,
     ),
 ]
