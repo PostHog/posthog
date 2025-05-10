6355d318bbe12ad02fb20108a244aa16f763b3b0
Marius Andra <marius.andra@gmail.com>
2025-05-10 23:30:43 +0200

    mroe

--- frontend/src/layout/panel-layout/ProjectTree/projectTreeStateLogic.tsx
+++ frontend/src/layout/panel-layout/ProjectTree/projectTreeStateLogic.tsx
@@ -1,6 +1,10 @@
-import { actions, kea, key, path, props, reducers } from 'kea'
+import { actions, connect, kea, key, listeners, path, props, reducers } from 'kea'
 
+import { projectTreeLogic } from '~/layout/panel-layout/ProjectTree/projectTreeLogic'
 import { projectTreeStateLogicType } from '~/layout/panel-layout/ProjectTree/projectTreeStateLogicType'
+import { joinPath, splitPath } from '~/layout/panel-layout/ProjectTree/utils'
+
+import type { projectTreeStateLogicType } from './projectTreeStateLogicType'
 
 export interface ProjectTreeStateLogicProps {
     uniqueId: string
@@ -17,13 +21,35 @@ export const projectTreeStateLogic = kea<projectTreeStateLogicType>([
     path(['layout', 'panel-layout', 'ProjectTree', 'projectTreeStateLogic']),
     props({ uniqueId: '' } as ProjectTreeStateLogicProps),
     key(({ uniqueId }) => uniqueId),
+    connect(() => ({
+        actions: [projectTreeLogic, ['loadFolder']],
+        values: [projectTreeLogic, ['folderStates']],
+    })),
     actions({
         setExpandedFolders: (expandedFolders: string[]) => ({ expandedFolders }),
+        expandProjectFolder: (path: string) => ({ path }),
     }),
     reducers(({ props }) => ({
         expandedFolders: [
             props.expandedFolders as string[],
             { setExpandedFolders: (_, { expandedFolders }) => expandedFolders },
         ],
+    })),
+    listeners(({ values }) => ({
+        expandProjectFolder: ({ path }) => {
+            const expandedSet = new Set(values.expandedFolders)
+            const allFolders = splitPath(path).slice(0, -1)
+            const allFullFolders = allFolders.map((_, index) => joinPath(allFolders.slice(0, index + 1)))
+            const nonExpandedFolders = allFullFolders.filter((f) => !expandedSet.has('project-folder/' + f))
+            for (const folder of nonExpandedFolders) {
+                if (values.folderStates[folder] !== 'loaded' && values.folderStates[folder] !== 'loading') {
+                    actions.loadFolder(folder)
+                }
+            }
+            actions.setExpandedFolders([
+                ...values.expandedFolders,
+                ...nonExpandedFolders.map((f) => 'project-folder/' + f),
+            ])
+        },
     })),
 ])
