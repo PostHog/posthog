37b37359b4568a83f379dc3012ecb00414bb98a4
pauldambra <paul@posthog.com>
2025-05-14 18:44:55 -0500

    fix

--- posthog/hogql/functions/test/test_explain_csp_button.py
+++ posthog/hogql/functions/test/test_explain_csp_button.py
@@ -12,11 +12,11 @@ class TestExplainCSPReport(BaseTest):
         )
         self.assertEqual(
             response.clickhouse,
-            f"SELECT tuple(%(hogql_val_0)s, %(hogql_val_1)s, %(hogql_val_2)s, %(hogql_val_3)s, %(hogql_val_4)s, %(hogql_val_5)s) LIMIT 100 SETTINGS readonly=2, max_execution_time=60, allow_experimental_object_type=1, format_csv_allow_double_quotes=0, max_ast_elements=4000000, max_expanded_ast_elements=4000000, max_bytes_before_external_group_by=0, transform_null_in=1, optimize_min_equality_disjunction_chain_length=4294967295",
+            "SELECT tuple(%(hogql_val_0)s, %(hogql_val_1)s, %(hogql_val_2)s, tuple(%(hogql_val_3)s, %(hogql_val_4)s, %(hogql_val_5)s, %(hogql_val_6)s, %(hogql_val_7)s, %(hogql_val_8)s)) LIMIT 100 SETTINGS readonly=2, max_execution_time=60, allow_experimental_object_type=1, format_csv_allow_double_quotes=0, max_ast_elements=4000000, max_expanded_ast_elements=4000000, max_bytes_before_external_group_by=0, transform_null_in=1, optimize_min_equality_disjunction_chain_length=4294967295",
         )
         self.assertEqual(
             response.hogql,
-            f"SELECT tuple('__hx_tag', 'ExplainCSPReport', 'properties', {'violated_directive': 'script-src', 'original_policy': 'script-src https://example.com'}) LIMIT 100",
+            "SELECT tuple('__hx_tag', 'ExplainCSPReport', 'properties', tuple('__hx_tag', '__hx_obj', 'violated_directive', 'script-src', 'original_policy', 'script-src https://example.com')) LIMIT 100",
         )
         self.assertEqual(
             response.results[0][0],
@@ -24,16 +24,18 @@ class TestExplainCSPReport(BaseTest):
                 "__hx_tag",
                 "ExplainCSPReport",
                 "properties",
-                {"violated_directive": "script-src", "original_policy": "script-src https://example.com"},
+                (
+                    "__hx_tag",
+                    "__hx_obj",
+                    "violated_directive",
+                    "script-src",
+                    "original_policy",
+                    "script-src https://example.com",
+                ),
             ),
         )
 
     def test_explain_csp_report_no_properties_error(self):
         with self.assertRaises(QueryError) as e:
             execute_hogql_query(f"SELECT explain_csp_report()", self.team)
-        self.assertEqual(str(e.exception), "Function 'explain_csp_report' expects at least 1 argument, found 0")
-
-    def test_explain_csp_report_missing_required_properties(self):
-        with self.assertRaises(QueryError) as e:
-            execute_hogql_query(f"SELECT explain_csp_report({'violated_directive': 'script-src'})", self.team)
-        self.assertEqual(str(e.exception), "Function 'explain_csp_report' expects at least 1 argument, found 0")
+        self.assertEqual(str(e.exception), "Function 'explain_csp_report' expects 1 argument, found 0")
--- posthog/hogql/test/test_resolver.py
+++ posthog/hogql/test/test_resolver.py
@@ -496,7 +496,7 @@ class TestResolver(BaseTest):
 
     def test_visit_hogqlx_explain_csp_report(self):
         node = self._select(
-            "select <ExplainCSPReport properties={'violated_directive': 'script-src', 'original_policy': 'script-src https://example.com'} />"
+            "select <ExplainCSPReport properties={{'violated_directive': 'script-src', 'original_policy': 'script-src https://example.com'}} />"
         )
         node = cast(ast.SelectQuery, resolve_types(node, self.context, dialect="clickhouse"))
         expected = ast.SelectQuery(
@@ -508,12 +508,10 @@ class TestResolver(BaseTest):
                         ast.Constant(value="properties"),
                         ast.Tuple(
                             exprs=[
+                                ast.Constant(value="__hx_tag"),
+                                ast.Constant(value="__hx_obj"),
                                 ast.Constant(value="violated_directive"),
                                 ast.Constant(value="script-src"),
-                            ]
-                        ),
-                        ast.Tuple(
-                            exprs=[
                                 ast.Constant(value="original_policy"),
                                 ast.Constant(value="script-src https://example.com"),
                             ]
@@ -522,7 +520,8 @@ class TestResolver(BaseTest):
                 ),
             ]
         )
-        assert clone_expr(node, clear_types=True) == expected
+        actual = clone_expr(node, clear_types=True)
+        assert actual == expected, f"\nExpected:\n{expected}\n\nActual:\n{actual}"
 
     def test_visit_hogqlx_sparkline(self):
         node = self._select("select <Sparkline data={[1,2,3]} />")
@@ -683,9 +682,15 @@ class TestResolver(BaseTest):
 
     def test_explain_csp_report_tag(self):
         node: ast.SelectQuery = self._select(
-            "select <ExplainCSPReport properties={'violated_directive': 'script-src', 'original_policy': 'script-src https://example.com'} />"
+            "select <ExplainCSPReport properties={{'violated_directive': 'script-src', 'original_policy': 'script-src https://example.com'}} />"
         )
         node = cast(ast.SelectQuery, resolve_types(node, self.context, dialect="clickhouse"))
+
+        node2 = self._select(
+            "select explain_csp_report({'violated_directive': 'script-src', 'original_policy': 'script-src https://example.com'}e)"
+        )
+        node2 = cast(ast.SelectQuery, resolve_types(node2, self.context, dialect="clickhouse"))
+        assert node == node2
 
     def test_sparkline_tag(self):
         node: ast.SelectQuery = self._select("select <Sparkline data={[1,2,3]} />")
