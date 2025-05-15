cbb2a7a21f297fb77a8790f912babec1cdfe9db7
timgl <tim@posthog.com>
2025-05-15 11:01:50 -0500

    fix

--- products/managed_migrations/frontend/types.ts
+++ products/managed_migrations/frontend/types.ts
@@ -1,4 +1,5 @@
 export interface ManagedMigration {
+    id: string
     source: string
     api_key: string
     secret_key: string
@@ -6,4 +7,14 @@ export interface ManagedMigration {
     end_date: string
     event_names_mode: 'all' | 'allow' | 'deny'
     event_names: string[]
+    status: 'Cancelled' | 'Completed' | 'Failed' | 'Running' | 'Starting'
+    created_by: {
+        id: number
+        uuid: string
+        distinct_id: string
+        first_name: string
+        email: string
+    }
+    created_at: string
+    error: string | null
 }
