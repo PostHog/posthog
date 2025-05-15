f7453c483f256d6506e076e3c01e40d5ce78e772
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-05-15 17:43:39 +0200

    fix admin

--- posthog/admin/admins/link_admin.py
+++ posthog/admin/admins/link_admin.py
@@ -6,20 +6,20 @@ from posthog.models import Link
 
 
 class LinkAdmin(admin.ModelAdmin):
-    list_display = ("id", "destination_display", "team_link", "created_at", "updated_at")
+    list_display = ("id", "redirect_url_display", "team_link", "created_at", "updated_at")
     list_filter = ("team", "created_at", "updated_at")
-    search_fields = ("id", "destination", "team__name")
+    search_fields = ("id", "redirect_url", "team__name")
     readonly_fields = ("id", "created_at", "updated_at")
     fieldsets = (
-        (None, {"fields": ("id", "destination", "short_link_domain", "short_code", "team")}),
+        (None, {"fields": ("id", "redirect_url", "short_link_domain", "short_code", "team")}),
         ("Dates", {"fields": ("created_at", "updated_at")}),
         ("Additional Info", {"fields": ("description",)}),
     )
 
-    def destination_display(self, obj: Link):
+    def redirect_url_display(self, obj: Link):
         return format_html('<a href="{}" target="_blank">{}</a>', obj.redirect_url, obj.redirect_url)
 
-    destination_display.short_description = "Destination"
+    redirect_url_display.short_description = "Redirect URL"
 
     def team_link(self, obj: Link):
         return format_html(
