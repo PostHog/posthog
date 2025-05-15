5b85e3f4399d09269d1ddd73b62288b6cf4790c3
Marcus Hof <13001502+MarconLP@users.noreply.github.com>
2025-05-15 13:20:45 +0200

    Update frontend/src/lib/constants.tsx
    Co-authored-by: Rafael Audibert <32079912+rafaeelaudibert@users.noreply.github.com>

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -262,7 +262,7 @@ export const FEATURE_FLAGS = {
     DASHBOARD_SYNC_INSIGHT_LOADING: 'dashboard-sync-insight-loading', // owner: @anirudhpillai #team-product-analytics
     ALWAYS_QUERY_BLOCKING: 'always-query-blocking', // owner: @timgl
     GET_HOG_TEMPLATES_FROM_DB: 'get-hog-templates-from-db', // owner: @meikel #team-cdp
-    LINK_TRACKING: 'link-tracking', // owner: @marconlp #team-cdp
+    LINK_TRACKING: 'link-tracking', // owner: @marconlp #team-link-tracking
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
