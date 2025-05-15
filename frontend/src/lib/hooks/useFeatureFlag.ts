34b740feb4f74a9332f775174921be26f97e9f18
Joshua Snyder <joshua@posthog.com>
2025-05-15 10:58:39 -0500

    chore: add a note to the hook

--- frontend/src/lib/hooks/useFeatureFlag.ts
+++ frontend/src/lib/hooks/useFeatureFlag.ts
@@ -2,6 +2,8 @@ import { useValues } from 'kea'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
+// NOTE: When using this hook, you use the key of FEATURE_FLAGS as the parameter.
+// For example, useFeatureFlag('HOMEPAGE_MAX')
 export const useFeatureFlag = (flag: keyof typeof FEATURE_FLAGS, match?: string): boolean => {
     const { featureFlags } = useValues(featureFlagLogic)
 
