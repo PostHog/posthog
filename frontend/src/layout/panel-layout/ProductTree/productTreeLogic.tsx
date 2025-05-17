52c3f8294e93315c9916db5122606bed7e73a828
Adam Leith <adamleithp@users.noreply.github.com>
2025-05-16 20:59:26 -0500

    project tree with own logic, hide panel on item click

---
+++ frontend/src/layout/panel-layout/ProductTree/productTreeLogic.tsx
@@ -0,0 +1,31 @@
+import { connect, kea, path, selectors } from 'kea'
+import { TreeDataItem } from 'lib/lemon-ui/LemonTree/LemonTree'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
+
+import { getDefaultTreeProducts } from '../ProjectTree/defaultTree'
+import { projectTreeLogic } from '../ProjectTree/projectTreeLogic'
+import { convertFileSystemEntryToTreeDataItem } from '../ProjectTree/utils'
+
+export const productTreeLogic = kea([
+    path(['layout', 'navigation-3000', 'components', 'productTreeLogic']),
+    connect(() => ({
+        values: [featureFlagLogic, ['featureFlags'], projectTreeLogic, ['folderStates', 'users']],
+        actions: [],
+    })),
+    selectors({
+        treeItems: [
+            (s) => [s.featureFlags, s.folderStates, s.users],
+            (featureFlags, folderStates, users): TreeDataItem[] =>
+                convertFileSystemEntryToTreeDataItem({
+                    imports: getDefaultTreeProducts().filter(
+                        (f) => !f.flag || (featureFlags as Record<string, boolean>)[f.flag]
+                    ),
+                    checkedItems: {},
+                    folderStates,
+                    root: 'explore',
+                    users,
+                    foldersFirst: false,
+                }),
+        ],
+    }),
+])
