da5876bad179954440a88f40de4d16ed75bdb391
Joshua Snyder <joshua@posthog.com>
2025-05-19 14:24:19 +0200

    fix: reduce queries checking for reverse proxy and custom events in activation logic

--- frontend/src/lib/components/ReverseProxyChecker/reverseProxyCheckerLogic.ts
+++ frontend/src/lib/components/ReverseProxyChecker/reverseProxyCheckerLogic.ts
@@ -1,4 +1,4 @@
-import { afterMount, kea, listeners, path, reducers } from 'kea'
+import { kea, listeners, path, reducers } from 'kea'
 import { loaders } from 'kea-loaders'
 import api from 'lib/api'
 
@@ -13,17 +13,23 @@ const CHECK_INTERVAL_MS = 1000 * 60 * 60 // 1 hour
 
 export const reverseProxyCheckerLogic = kea<reverseProxyCheckerLogicType>([
     path(['components', 'ReverseProxyChecker', 'reverseProxyCheckerLogic']),
-    loaders({
+    loaders(({ values }) => ({
         hasReverseProxy: [
             false as boolean | null,
             {
                 loadHasReverseProxy: async () => {
+                    if (values.lastCheckedTimestamp < Date.now() - CHECK_INTERVAL_MS) {
+                        return values.hasReverseProxy
+                    }
+
                     const query: HogQLQuery = {
                         kind: NodeKind.HogQLQuery,
-                        query: hogql`SELECT properties.$lib_custom_api_host AS lib_custom_api_host
+                        query: hogql`SELECT DISTINCT properties.$lib_custom_api_host AS lib_custom_api_host
                                 FROM events
                                 WHERE timestamp >= now() - INTERVAL 1 DAY 
                                 AND timestamp <= now()
+                                AND properties.$lib_custom_api_host IS NOT NULL
+                                AND event = '$pageview'
                                 ORDER BY timestamp DESC
                                 limit 10`,
                     }
@@ -33,7 +39,7 @@ export const reverseProxyCheckerLogic = kea<reverseProxyCheckerLogicType>([
                 },
             },
         ],
-    }),
+    })),
     listeners(({ values }) => ({
         loadHasReverseProxySuccess: () => {
             if (values.hasReverseProxy) {
@@ -49,10 +55,5 @@ export const reverseProxyCheckerLogic = kea<reverseProxyCheckerLogicType>([
                 loadHasReverseProxySuccess: () => Date.now(),
             },
         ],
-    }),
-    afterMount(({ actions, values }) => {
-        if (values.lastCheckedTimestamp < Date.now() - CHECK_INTERVAL_MS) {
-            actions.loadHasReverseProxy()
-        }
     }),
 ])
