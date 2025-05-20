0ab8b5c98d2cbdf3b6dd651ee34aa7801d3d04f4
Rafa Audibert <rafael@posthog.com>
2025-05-20 20:07:44 -0300

    feat: Hide complex filter behind FF
    I dont think we need this at all but let's be smart and do a FF here to guarantee we can easily revert this if actually needed

--- frontend/src/lib/components/TaxonomicFilter/infiniteListLogic.ts
+++ frontend/src/lib/components/TaxonomicFilter/infiniteListLogic.ts
@@ -13,9 +13,10 @@ import {
     TaxonomicFilterGroup,
     TaxonomicFilterGroupType,
 } from 'lib/components/TaxonomicFilter/types'
+import { FEATURE_FLAGS } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { isEmail, isURL } from 'lib/utils'
 import { RenderedRows } from 'react-virtualized/dist/es/List'
-import { featureFlagsLogic } from 'scenes/feature-flags/featureFlagsLogic'
 
 import { getCoreFilterDefinition } from '~/taxonomy/helpers'
 import { CohortType, EventDefinition } from '~/types'
@@ -79,7 +80,7 @@ export const infiniteListLogic = kea<infiniteListLogicType>([
             ['searchQuery', 'value', 'groupType', 'taxonomicGroups'],
             teamLogic,
             ['currentTeamId'],
-            featureFlagsLogic,
+            featureFlagLogic,
             ['featureFlags'],
         ],
         actions: [taxonomicFilterLogic(props), ['setSearchQuery', 'selectItem', 'infiniteListResultsReceived']],
@@ -342,32 +343,34 @@ export const infiniteListLogic = kea<infiniteListLogicType>([
             },
         ],
         items: [
-            (s, p) => [s.remoteItems, s.localItems, p.showNumericalPropsOnly ?? (() => false)],
-            (remoteItems, localItems, showNumericalPropsOnly) => {
-                // NOTE: This might not be needed because we're filtering on the backend to improve loading performance
+            (s, p) => [s.remoteItems, s.localItems, s.featureFlags, p.showNumericalPropsOnly ?? (() => false)],
+            (remoteItems, localItems, featureFlags, showNumericalPropsOnly) => {
+                // NOTE: We're filtering on the backend to improve loading performance which implies we dont need to filter here
                 // That said, there might be some edge cases where the backend is NOT filtering on the same way
-                // as we are here in the frontend, so let's keep this here
-                const results = [...localItems.results, ...remoteItems.results].filter((result) => {
-                    if (!showNumericalPropsOnly) {
-                        return true
-                    }
+                // as we are here in the frontend, so let's add a short-circuit FF here in case we need to go back to the previous implementation
+                const results = featureFlags[FEATURE_FLAGS.SIMPLE_INFINITE_LIST_NUMERICAL_FILTER]
+                    ? [...localItems.results, ...remoteItems.results]
+                    : [...localItems.results, ...remoteItems.results].filter((result) => {
+                          if (!showNumericalPropsOnly) {
+                              return true
+                          }
 
-                    // It's still loading, just display it while we figure it out
-                    if (!result) {
-                        return true
-                    }
+                          // It's still loading, just display it while we figure it out
+                          if (!result) {
+                              return true
+                          }
 
-                    if ('is_numerical' in result) {
-                        return !!result.is_numerical
-                    }
+                          if ('is_numerical' in result) {
+                              return !!result.is_numerical
+                          }
 
-                    if ('property_type' in result) {
-                        const property_type = result.property_type as string // Data warehouse props dont conform to PropertyType for some reason
-                        return property_type === 'Integer' || property_type === 'Float'
-                    }
+                          if ('property_type' in result) {
+                              const property_type = result.property_type as string // Data warehouse props dont conform to PropertyType for some reason
+                              return property_type === 'Integer' || property_type === 'Float'
+                          }
 
-                    return true
-                })
+                          return true
+                      })
 
                 return {
                     results,
--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -270,6 +270,7 @@ export const FEATURE_FLAGS = {
     LOGS: 'logs', // owner: @david @frank @olly @ross
     CSP_REPORTING: 'mexicspo', // owner @pauldambra @lricoy @robbiec
     USAGE_SPEND_DASHBOARDS: 'usage-spend-dashboards', // owner: @pawel-cebula #team-billing
+    SIMPLE_INFINITE_LIST_NUMERICAL_FILTER: 'simple-infinite-list-numerical-filter', // owner: @rafaeelaudibert #team-revenue-analytics
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
