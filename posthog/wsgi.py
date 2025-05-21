4246a2ced811976baaa25cbe40bbf7dcf3032afa
Michael Kutsch <michael.k@posthog.com>
2025-05-21 11:18:45 +0200

    Add sampling

--- posthog/wsgi.py
+++ posthog/wsgi.py
@@ -9,9 +9,16 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+# PostHog OpenTelemetry Initialization
+from posthog.otel_instrumentation import initialize_otel
+
 from django.core.wsgi import get_wsgi_application
 
+initialize_otel()
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "posthog.settings")
-os.environ.setdefault("SERVER_GATEWAY_INTERFACE", "WSGI")
+# SERVER_GATEWAY_INTERFACE is set by the WSGI server (e.g. gunicorn)
+# We don't need to set it here as we did for ASGI, Gunicorn will handle it.
+# os.environ.setdefault("SERVER_GATEWAY_INTERFACE", "WSGI") # This line can be removed or kept, gunicorn will override.
 
 application = get_wsgi_application()
