f51e99151895c48724add53b4ecb895342fe824b
James Greenhill <fuziontech@gmail.com>
2025-05-21 17:04:42 -0700

    run for both sharded and dist tables

--- posthog/clickhouse/migrations/0117_events_ai_properties_map.py
+++ posthog/clickhouse/migrations/0117_events_ai_properties_map.py
@@ -1,7 +1,14 @@
+from posthog.clickhouse.client.connection import NodeRole
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.clickhouse.property_groups import property_groups
 
 operations = [
-    run_sql_with_exceptions(statement)
-    for statement in [*property_groups.get_alter_create_statements("events", "properties", "ai")]
+    *[
+        run_sql_with_exceptions(statement, node_role=NodeRole.DATA, sharded=True)
+        for statement in [*property_groups.get_alter_create_statements("sharded_events", "properties", "ai")]
+    ],
+    *[
+        run_sql_with_exceptions(statement, node_role=NodeRole.ALL)
+        for statement in [*property_groups.get_alter_create_statements("events", "properties", "ai")]
+    ],
 ]
