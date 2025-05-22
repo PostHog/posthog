53f04f8ce7ff61f89bb8d53245e64d2fa0d1e2f4
Tom Owers <tom@posthog.com>
2025-05-22 08:57:07 -0500

    fix(data-warehouse): only report jobs that have completed (#32522)

--- posthog/tasks/test/test_usage_report.py
+++ posthog/tasks/test/test_usage_report.py
@@ -1456,6 +1456,7 @@ class TestExternalDataSyncUsageReport(ClickhouseDestroyTablesMixin, TestCase, Cl
                 team_id=3,
                 created_at=start_time,
                 rows_synced=10,
+                status=ExternalDataJob.Status.COMPLETED,
                 pipeline=source,
                 pipeline_version=ExternalDataJob.PipelineVersion.V1,
             )
@@ -1466,6 +1467,7 @@ class TestExternalDataSyncUsageReport(ClickhouseDestroyTablesMixin, TestCase, Cl
                 team_id=4,
                 created_at=start_time,
                 rows_synced=10,
+                status=ExternalDataJob.Status.COMPLETED,
                 pipeline=source,
                 pipeline_version=ExternalDataJob.PipelineVersion.V1,
             )
@@ -1495,6 +1497,66 @@ class TestExternalDataSyncUsageReport(ClickhouseDestroyTablesMixin, TestCase, Cl
 
     @patch("posthog.tasks.usage_report.get_ph_client")
     @patch("posthog.tasks.usage_report.send_report_to_billing_service")
+    def test_external_data_rows_synced_failed_jobs(
+        self, billing_task_mock: MagicMock, posthog_capture_mock: MagicMock
+    ) -> None:
+        self._setup_teams()
+
+        source = ExternalDataSource.objects.create(
+            team=self.analytics_team,
+            source_id="source_id",
+            connection_id="connection_id",
+            status=ExternalDataSource.Status.COMPLETED,
+            source_type=ExternalDataSource.Type.STRIPE,
+        )
+
+        for i in range(5):
+            start_time = (now() - relativedelta(hours=i)).strftime("%Y-%m-%dT%H:%M:%SZ")
+            ExternalDataJob.objects.create(
+                team_id=3,
+                created_at=start_time,
+                rows_synced=10,
+                status=ExternalDataJob.Status.COMPLETED,
+                pipeline=source,
+                pipeline_version=ExternalDataJob.PipelineVersion.V1,
+            )
+
+        for i in range(5):
+            start_time = (now() - relativedelta(hours=i)).strftime("%Y-%m-%dT%H:%M:%SZ")
+            ExternalDataJob.objects.create(
+                team_id=4,
+                created_at=start_time,
+                rows_synced=10,
+                status=ExternalDataJob.Status.FAILED,
+                pipeline=source,
+                pipeline_version=ExternalDataJob.PipelineVersion.V1,
+            )
+
+        period = get_previous_day(at=now() + relativedelta(days=1))
+        period_start, period_end = period
+        all_reports = _get_all_org_reports(period_start, period_end)
+
+        assert len(all_reports) == 3
+
+        org_1_report = _get_full_org_usage_report_as_dict(
+            _get_full_org_usage_report(all_reports[str(self.org_1.id)], get_instance_metadata(period))
+        )
+
+        org_2_report = _get_full_org_usage_report_as_dict(
+            _get_full_org_usage_report(all_reports[str(self.org_2.id)], get_instance_metadata(period))
+        )
+
+        assert org_1_report["organization_name"] == "Org 1"
+        assert org_1_report["rows_synced_in_period"] == 50
+
+        assert org_1_report["teams"]["3"]["rows_synced_in_period"] == 50
+        assert org_1_report["teams"]["4"]["rows_synced_in_period"] == 0
+
+        assert org_2_report["organization_name"] == "Org 2"
+        assert org_2_report["rows_synced_in_period"] == 0
+
+    @patch("posthog.tasks.usage_report.get_ph_client")
+    @patch("posthog.tasks.usage_report.send_report_to_billing_service")
     def test_external_data_rows_synced_response_with_v2_jobs(
         self, billing_task_mock: MagicMock, posthog_capture_mock: MagicMock
     ) -> None:
@@ -1514,6 +1576,7 @@ class TestExternalDataSyncUsageReport(ClickhouseDestroyTablesMixin, TestCase, Cl
                 team_id=3,
                 created_at=start_time,
                 rows_synced=10,
+                status=ExternalDataJob.Status.COMPLETED,
                 pipeline=source,
                 pipeline_version=ExternalDataJob.PipelineVersion.V1,
             )
@@ -1524,6 +1587,7 @@ class TestExternalDataSyncUsageReport(ClickhouseDestroyTablesMixin, TestCase, Cl
                 team_id=4,
                 created_at=start_time,
                 rows_synced=10,
+                status=ExternalDataJob.Status.COMPLETED,
                 pipeline=source,
                 pipeline_version=ExternalDataJob.PipelineVersion.V2,
                 billable=False,
