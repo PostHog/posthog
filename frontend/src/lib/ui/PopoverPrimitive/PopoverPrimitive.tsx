bcc55b92db93ea93d019c17929eaf6165886c07e
Adam Leith <adamleithp@users.noreply.github.com>
2025-05-22 09:51:24 +0100

    clean up

--- frontend/src/lib/ui/PopoverPrimitive/PopoverPrimitive.tsx
+++ frontend/src/lib/ui/PopoverPrimitive/PopoverPrimitive.tsx
@@ -1,46 +1,37 @@
-import * as React from "react"
-import * as PopoverPrimitiveBase from "@radix-ui/react-popover"
+import * as PopoverPrimitiveBase from '@radix-ui/react-popover'
+import { cn } from 'lib/utils/css-classes'
+import * as React from 'react'
 
-import { cn } from "lib/utils/css-classes"
-
-function PopoverPrimitive({
-  ...props
-}: React.ComponentProps<typeof PopoverPrimitiveBase.Root>) {
-  return <PopoverPrimitiveBase.Root data-slot="popover" {...props} />
+function PopoverPrimitive({ ...props }: React.ComponentProps<typeof PopoverPrimitiveBase.Root>): JSX.Element {
+    return <PopoverPrimitiveBase.Root data-slot="popover" {...props} />
 }
 
-function PopoverPrimitiveTrigger({
-  ...props
-}: React.ComponentProps<typeof PopoverPrimitiveBase.Trigger>) {
-  return <PopoverPrimitiveBase.Trigger data-slot="popover-trigger" {...props} />
+function PopoverPrimitiveTrigger({ ...props }: React.ComponentProps<typeof PopoverPrimitiveBase.Trigger>): JSX.Element {
+    return <PopoverPrimitiveBase.Trigger data-slot="popover-trigger" {...props} />
 }
 
 function PopoverPrimitiveContent({
-  className,
-  align = "center",
-  sideOffset = 4,
-  ...props
-}: React.ComponentProps<typeof PopoverPrimitiveBase.Content>) {
-  return (
-    // <PopoverPrimitiveBase.Portal>
-      <PopoverPrimitiveBase.Content
-        data-slot="popover-content"
-        align={align}
-        sideOffset={sideOffset}
-        className={cn(
-          "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 outline-hidden",
-          className
-        )}
-        {...props}
-      />
-    // </PopoverPrimitiveBase.Portal>
-  )
+    className,
+    align = 'center',
+    sideOffset = 4,
+    ...props
+}: React.ComponentProps<typeof PopoverPrimitiveBase.Content>): JSX.Element {
+    return (
+        <PopoverPrimitiveBase.Content
+            data-slot="popover-content"
+            align={align}
+            sideOffset={sideOffset}
+            className={cn(
+                'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 outline-hidden',
+                className
+            )}
+            {...props}
+        />
+    )
 }
 
-function PopoverPrimitiveAnchor({
-  ...props
-}: React.ComponentProps<typeof PopoverPrimitiveBase.Anchor>) {
-  return <PopoverPrimitiveBase.Anchor data-slot="popover-anchor" {...props} />
+function PopoverPrimitiveAnchor({ ...props }: React.ComponentProps<typeof PopoverPrimitiveBase.Anchor>): JSX.Element {
+    return <PopoverPrimitiveBase.Anchor data-slot="popover-anchor" {...props} />
 }
 
-export { PopoverPrimitive, PopoverPrimitiveTrigger, PopoverPrimitiveContent, PopoverPrimitiveAnchor }
+export { PopoverPrimitive, PopoverPrimitiveAnchor, PopoverPrimitiveContent, PopoverPrimitiveTrigger }
