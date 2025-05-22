2df8a59b09f83130aaccaefa353e599f146a399f
Joshua Snyder <joshua@posthog.com>
2025-05-22 11:18:09 +0200

    chore: add debug logs for early access celery task

--- posthog/tasks/early_access_feature.py
+++ posthog/tasks/early_access_feature.py
@@ -1,7 +1,10 @@
 from celery import shared_task
+import structlog
 from posthog.models import EarlyAccessFeature
 from posthog.models.person.person import Person
 import posthoganalytics
+
+logger = structlog.get_logger(__name__)
 
 
 # Note: If the task fails and is retried, events may be sent multiple times. This is handled by Customer.io when consuming the events.
@@ -11,19 +14,37 @@ def send_events_for_early_access_feature_stage_change(feature_id: str, from_stag
 
     feature_flag = instance.feature_flag
 
+    logger.info(
+        f"[CELERY][EARLY ACCESS FEATURE] Sending events for early access feature stage change for feature {instance.name} from {from_stage} to {to_stage}",
+    )
+
     if not feature_flag:
+        logger.warning(
+            f"[CELERY][EARLY ACCESS FEATURE] Feature flag not found for feature {instance.name}",
+        )
         return
 
     enrolled_persons = Person.objects.filter(
         **{f"properties__$feature_enrollment/{feature_flag.key}": True, "team_id": instance.team_id}
     )
 
+    logger.info(
+        f"[CELERY][EARLY ACCESS FEATURE] Found {len(enrolled_persons)} persons enrolled in feature {instance.name}",
+    )
+
     for person in enrolled_persons:
         if len(person.distinct_ids) == 0:
+            logger.warning(
+                f"[CELERY][EARLY ACCESS FEATURE] Person {person.id} has no distinct ids",
+            )
             continue
 
         distinct_id = person.distinct_ids[0]
         email = person.properties.get("email", "")
+
+        logger.info(
+            f"[CELERY][EARLY ACCESS FEATURE] Sending event for person {person.id} with distinct id {distinct_id}",
+        )
 
         posthoganalytics.capture(
             distinct_id,
@@ -36,4 +57,8 @@ def send_events_for_early_access_feature_stage_change(feature_id: str, from_stag
                 "feature_name": instance.name,
                 "user_email": email,
             },
+        )
+
+        logger.info(
+            f"[CELERY][EARLY ACCESS FEATURE] Sent event for person {person.id} with distinct id {distinct_id}",
         )
