cf4c8da91ed197f4aeb4475446a305460d64b791
Lucas Ricoy <2034367+lricoy@users.noreply.github.com>
2025-05-23 21:50:54 +0100

    fix(csp): menu section not using flag (#32619)

--- frontend/src/scenes/settings/SettingsMap.tsx
+++ frontend/src/scenes/settings/SettingsMap.tsx
@@ -409,6 +409,7 @@ export const SETTINGS_MAP: SettingSection[] = [
         level: 'environment',
         id: 'environment-csp-reporting',
         title: 'CSP reporting',
+        flag: 'CSP_REPORTING',
         settings: [
             {
                 id: 'csp-reporting',
