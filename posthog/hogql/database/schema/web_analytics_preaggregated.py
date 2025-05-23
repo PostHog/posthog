b0f7515f5d6b6dc7814f685737225158652fc74a
Lucas Ricoy <2034367+lricoy@users.noreply.github.com>
2025-05-23 01:08:22 +0100

    feat: use web_bounces_daily on pre_aggregated stats_table (#32558)

--- posthog/hogql/database/schema/web_analytics_preaggregated.py
+++ posthog/hogql/database/schema/web_analytics_preaggregated.py
@@ -8,15 +8,24 @@ from posthog.hogql.database.models import (
 )
 
 
+web_preaggregated_base_fields = {
+    "team_id": IntegerDatabaseField(name="team_id"),
+    "day_bucket": DateDatabaseField(name="day_bucket"),
+    "host": StringDatabaseField(name="host", nullable=True),
+    "device_type": StringDatabaseField(name="device_type", nullable=True),
+}
+
+web_preaggregated_base_aggregation_fields = {
+    "persons_uniq_state": DatabaseField(name="persons_uniq_state"),
+    "pageviews_count_state": DatabaseField(name="pageviews_count_state"),
+    "sessions_uniq_state": DatabaseField(name="sessions_uniq_state"),
+}
+
+
 class WebOverviewDailyTable(Table):
     fields: dict[str, FieldOrTable] = {
-        "team_id": IntegerDatabaseField(name="team_id"),
-        "day_bucket": DateDatabaseField(name="day_bucket"),
-        "host": StringDatabaseField(name="host", nullable=True),
-        "device_type": StringDatabaseField(name="device_type", nullable=True),
-        "persons_uniq_state": DatabaseField(name="persons_uniq_state"),
-        "pageviews_count_state": DatabaseField(name="pageviews_count_state"),
-        "sessions_uniq_state": DatabaseField(name="sessions_uniq_state"),
+        **web_preaggregated_base_fields,
+        **web_preaggregated_base_aggregation_fields,
         "total_session_duration_state": DatabaseField(name="total_session_duration_state"),
         "total_bounces_state": DatabaseField(name="total_bounces_state"),
     }
@@ -30,10 +39,8 @@ class WebOverviewDailyTable(Table):
 
 class WebStatsDailyTable(Table):
     fields: dict[str, FieldOrTable] = {
-        "team_id": IntegerDatabaseField(name="team_id"),
-        "day_bucket": DateDatabaseField(name="day_bucket"),
-        "host": StringDatabaseField(name="host", nullable=True),
-        "device_type": StringDatabaseField(name="device_type", nullable=True),
+        **web_preaggregated_base_fields,
+        **web_preaggregated_base_aggregation_fields,
         "browser": StringDatabaseField(name="browser", nullable=True),
         "os": StringDatabaseField(name="os", nullable=True),
         "referring_domain": StringDatabaseField(name="referring_domain", nullable=True),
@@ -44,9 +51,6 @@ class WebStatsDailyTable(Table):
         "utm_term": StringDatabaseField(name="utm_term", nullable=True),
         "utm_content": StringDatabaseField(name="utm_content", nullable=True),
         "country": StringDatabaseField(name="country", nullable=True),
-        "persons_uniq_state": DatabaseField(name="persons_uniq_state"),
-        "sessions_uniq_state": DatabaseField(name="sessions_uniq_state"),
-        "pageviews_count_state": DatabaseField(name="pageviews_count_state"),
     }
 
     def to_printed_clickhouse(self, context):
@@ -54,3 +58,18 @@ class WebStatsDailyTable(Table):
 
     def to_printed_hogql(self):
         return "web_stats_daily"
+
+
+class WebBouncesDailyTable(Table):
+    fields: dict[str, FieldOrTable] = {
+        **web_preaggregated_base_fields,
+        **web_preaggregated_base_aggregation_fields,
+        "entry_path": StringDatabaseField(name="entry_path", nullable=True),
+        "bounces_count_state": DatabaseField(name="bounces_count_state"),
+    }
+
+    def to_printed_clickhouse(self, context):
+        return "web_bounces_daily"
+
+    def to_printed_hogql(self):
+        return "web_bounces_daily"
