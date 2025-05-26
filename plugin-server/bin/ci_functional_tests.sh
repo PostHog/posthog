11ec1e769538a7db32943271231e4027952aef53
Michael Kutsch <michael.k@posthog.com>
2025-05-26 14:58:15 +0200

    Set cyclotron db env var

--- plugin-server/bin/ci_functional_tests.sh
+++ plugin-server/bin/ci_functional_tests.sh
@@ -18,6 +18,7 @@ export NODE_ENV=production-functional-tests
 export PLUGIN_SERVER_MODE=functional-tests # running all capabilities is too slow
 export DATABASE_URL='postgres://posthog:posthog@localhost:5432/posthog'
 export PERSONS_DATABASE_URL='postgres://posthog:posthog@localhost:5432/posthog'
+export CYCLOTRON_DATABASE_URL='postgres://posthog:posthog@localhost:5432/posthog'
 # Not important at all, but I like to see nice red/green for tests
 export FORCE_COLOR=true
 export ENCRYPTION_SALT_KEYS='00beef0000beef0000beef0000beef00'
