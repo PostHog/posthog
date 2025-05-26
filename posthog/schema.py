f54a12f2d8b3b5286beef51a847b6377bd7e210c
Adam Leith <adamleithp@users.noreply.github.com>
2025-05-26 05:53:22 +0100

    updated schema with new visual order

--- posthog/schema.py
+++ posthog/schema.py
@@ -1118,6 +1118,7 @@ class FileSystemImport(BaseModel):
     type: Optional[str] = Field(
         default=None, description="Type of object, used for icon, e.g. feature_flag, insight, etc"
     )
+    visualOrder: Optional[int] = Field(default=None, description="Order of object in tree")
 
 
 class FilterLogicalOperator(StrEnum):
