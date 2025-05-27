ee2b55c3e015522769cd1b6fccd3d37f2a112331
meikel <meikel@posthog.com>
2025-05-27 16:54:22 +0200

    bettr example

--- plugin-server/src/cdp/templates/_transformations/default/default.template.ts
+++ plugin-server/src/cdp/templates/_transformations/default/default.template.ts
@@ -13,7 +13,10 @@ export const template: HogFunctionTemplate = {
 // This is a blank template for custom transformations
 // The function receives 'event' as a global object and expects it to be returned
 // If you return null the event will be dropped and not ingested into your posthog instance
-return event
+// Check out our docs: https://posthog.com/docs/cdp/transformations/customizing-transformations
+let returnEvent := event
+returnEvent.properties.$example_added_property := 'example'
+return returnEvent
     `,
     inputs_schema: [],
 }
