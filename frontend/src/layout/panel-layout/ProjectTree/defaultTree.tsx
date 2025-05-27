b1953ea0656cd0ef0666777bbb076681fff03454
Marius Andra <marius.andra@gmail.com>
2025-05-27 16:16:24 +0200

    bring back sql editor

--- frontend/src/layout/panel-layout/ProjectTree/defaultTree.tsx
+++ frontend/src/layout/panel-layout/ProjectTree/defaultTree.tsx
@@ -215,10 +215,10 @@ export const getDefaultTreeProducts = (): FileSystemImport[] =>
             visualOrder: PRODUCT_VISUAL_ORDER.dataPipeline,
         } as FileSystemImport,
         {
-            path: 'Data warehouse',
-            iconType: 'database',
+            path: `SQL editor`,
+            type: 'sql',
             href: urls.sqlEditor(),
-            visualOrder: PRODUCT_VISUAL_ORDER.dataWarehouse,
+            visualOrder: PRODUCT_VISUAL_ORDER.sqlEditor,
         } as FileSystemImport,
         {
             path: 'Error tracking',
