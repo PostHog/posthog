09bb41a9a25d75bbceec1724c57ea04c16fa4fe2
Sandy Spicer <sandy@posthog.com>
2025-05-27 04:28:05 -0700

    feat: blocking exports (#32091)

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -261,6 +261,7 @@ export const FEATURE_FLAGS = {
     DASHBOARD_SYNC_INSIGHT_LOADING: 'dashboard-sync-insight-loading', // owner: @anirudhpillai #team-product-analytics
     ALWAYS_QUERY_BLOCKING: 'always-query-blocking', // owner: @timgl
     GET_HOG_TEMPLATES_FROM_DB: 'get-hog-templates-from-db', // owner: @meikel #team-cdp
+    BLOCKING_EXPORTS: 'blocking-exports', // owner: @aspicer #team-product-analytics
     REPLAY_ACTIVE_HOURS_HEATMAP: 'replay-active-hours-heatmap', // owner: @pauldambra #team-replay
     LINKS: 'links', // owner: @marconlp #team-link
     GAME_CENTER: 'game-center', // owner: everybody
