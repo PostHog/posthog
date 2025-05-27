8ac1ac07ffc55ca648cdb9ec77de19253f3fe3eb
Lucas Ricoy <lucas.r@posthog.com>
2025-05-27 11:58:31 -0300

    wip

--- dags/web_preaggregated_internal.py
+++ dags/web_preaggregated_internal.py
@@ -1,8 +1,8 @@
-from datetime import datetime, UTC
+from datetime import datetime
 from collections.abc import Callable
 
 import dagster
-from dagster import Field, Array
+from dagster import Field, Array, MonthlyPartitionsDefinition
 from clickhouse_driver import Client
 from dags.common import JobOwners
 from posthog.clickhouse.client import sync_execute
@@ -24,6 +24,11 @@ from posthog.models.web_preaggregated.sql import (
 from posthog.clickhouse.cluster import ClickhouseCluster
 
 
+monthly_partitions = MonthlyPartitionsDefinition(
+    start_date="2020-01-01",
+    end_offset=0,
+)
+
 WEB_ANALYTICS_CONFIG_SCHEMA = {
     "team_ids": Field(
         Array(int),
@@ -44,19 +49,32 @@ def pre_aggregate_web_analytics_data(
     sql_generator: Callable,
 ) -> None:
     config = context.op_config
-    team_ids = config.get("team_ids", [1, 2])
+    team_ids = config.get("team_ids", [])
+    team_ids = [1, 2] if not team_ids else team_ids
     clickhouse_settings = config["clickhouse_settings"]
 
-    # We'll be handling a fixed date range for our internal tests that gets the full history
+    if context.partition_key:
+        partition_date = datetime.strptime(context.partition_key, "%Y-%m-%d")
+        date_start = partition_date.strftime("%Y-%m-01")
+        if partition_date.month == 12:
+            next_month = partition_date.replace(year=partition_date.year + 1, month=1, day=1)
+        else:
+            next_month = partition_date.replace(month=partition_date.month + 1, day=1)
+        last_day = (next_month).strftime("%Y-%m-%d")
+        date_end = last_day
+
     insert_query = sql_generator(
-        date_start="2020-01-01",
-        date_end=datetime.now(UTC).strftime("%Y-%m-%d"),
+        date_start=date_start,
+        date_end=date_end,
         team_ids=team_ids,
         settings=clickhouse_settings,
         table_name=table_name,
     )
 
     # We intentionally log the query to make it easier to debug using the UI
+    context.log.info(
+        f"Processing partition {context.partition_key if context.partition_key else 'full range'}: {date_start} to {date_end}"
+    )
     context.log.info(insert_query)
 
     sync_execute(insert_query)
@@ -100,6 +118,8 @@ def web_analytics_preaggregated_tables(
     deps=["web_analytics_preaggregated_tables"],
     metadata={"table": "web_overview_daily"},
     tags={"owner": JobOwners.TEAM_WEB_ANALYTICS.value},
+    partitions_def=monthly_partitions,
+    op_tags={"dagster/concurrency_key": "web_analytics", "dagster/max_concurrent": "2"},
 )
 def web_overview_daily(
     context: dagster.AssetExecutionContext,
@@ -121,6 +141,8 @@ def web_overview_daily(
     deps=["web_analytics_preaggregated_tables"],
     metadata={"table": "web_bounces_daily"},
     tags={"owner": JobOwners.TEAM_WEB_ANALYTICS.value},
+    partitions_def=monthly_partitions,
+    op_tags={"dagster/concurrency_key": "web_analytics", "dagster/max_concurrent": "2"},
 )
 def web_bounces_daily(
     context: dagster.AssetExecutionContext,
@@ -140,6 +162,8 @@ def web_bounces_daily(
     deps=["web_analytics_preaggregated_tables"],
     metadata={"table": "web_stats_daily"},
     tags={"owner": JobOwners.TEAM_WEB_ANALYTICS.value},
+    partitions_def=monthly_partitions,
+    op_tags={"dagster/concurrency_key": "web_analytics", "dagster/max_concurrent": "2"},
 )
 def web_stats_daily(context: dagster.AssetExecutionContext) -> None:
     """
@@ -159,6 +183,8 @@ def web_stats_daily(context: dagster.AssetExecutionContext) -> None:
     deps=["web_analytics_preaggregated_tables"],
     metadata={"table": "web_paths_daily"},
     tags={"owner": JobOwners.TEAM_WEB_ANALYTICS.value},
+    partitions_def=monthly_partitions,
+    op_tags={"dagster/concurrency_key": "web_analytics", "dagster/max_concurrent": "2"},
 )
 def web_paths_daily(context: dagster.AssetExecutionContext) -> None:
     """
@@ -175,6 +201,8 @@ recreate_web_pre_aggregated_data_job = dagster.define_asset_job(
     name="recreate_web_pre_aggregated_data",
     selection=dagster.AssetSelection.groups("web_analytics"),
     tags={"owner": JobOwners.TEAM_WEB_ANALYTICS.value},
+    partitions_def=monthly_partitions,
+    config={"execution": {"config": {"multiprocess": {"max_concurrent": 2}}}},
 )
 
 
@@ -192,13 +220,15 @@ def recreate_web_analytics_preaggregated_internal_data_daily(context: dagster.Sc
     """
     team_ids = [2]
 
-    return dagster.RunRequest(
-        run_config={
-            "ops": {
-                "web_analytics_overview_daily": {"config": {"team_ids": team_ids}},
-                "web_analytics_bounces_daily": {"config": {"team_ids": team_ids}},
-                "web_analytics_stats_table_daily": {"config": {"team_ids": team_ids}},
-                "web_analytics_paths_daily": {"config": {"team_ids": team_ids}},
-            }
-        },
-    )
+    return [
+        dagster.RunRequest(
+            run_config={
+                "ops": {
+                    "web_analytics_overview_daily": {"config": {"team_ids": team_ids}},
+                    "web_analytics_bounces_daily": {"config": {"team_ids": team_ids}},
+                    "web_analytics_stats_table_daily": {"config": {"team_ids": team_ids}},
+                    "web_analytics_paths_daily": {"config": {"team_ids": team_ids}},
+                }
+            },
+        )
+    ]
