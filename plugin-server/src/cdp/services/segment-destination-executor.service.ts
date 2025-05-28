0c72c6faedcce99a2a2432dc677ebd88e848b8bb
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-05-28 09:35:36 +0200

    endpoint override for debugging

--- plugin-server/src/cdp/services/segment-destination-executor.service.ts
+++ plugin-server/src/cdp/services/segment-destination-executor.service.ts
@@ -252,7 +252,6 @@ export class SegmentDestinationExecutorService {
             await action.perform(
                 // @ts-expect-error can't figure out unknown extends Data
                 async (endpoint, options) => {
-                    endpoint = 'http://localhost:2080/04f41a4c-0053-40e0-9672-9bc5f132b55d'
                     if (config.debug_mode) {
                         addLog('debug', 'endpoint', endpoint)
                     }
