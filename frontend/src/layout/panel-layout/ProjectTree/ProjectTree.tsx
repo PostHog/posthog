802a2d7332c7fd8c3afd96cffb0f1e79df57a32b
Marius Andra <marius.andra@gmail.com>
2025-05-28 14:15:37 +0200

    feat(project-tree): remove games tree

--- frontend/src/layout/panel-layout/ProjectTree/ProjectTree.tsx
+++ frontend/src/layout/panel-layout/ProjectTree/ProjectTree.tsx
@@ -587,9 +587,6 @@ export function ProjectTree({
             renderItemTooltip={(item) => {
                 const user = item.record?.user as UserBasicType | undefined
                 const nameNode: JSX.Element = <span className="font-semibold">{item.displayName}</span>
-                if (root === 'games://') {
-                    return <>Play {nameNode}</>
-                }
                 if (root === 'products://' || root === 'data-management://' || root === 'persons://') {
                     return <>View {nameNode}</>
                 }
