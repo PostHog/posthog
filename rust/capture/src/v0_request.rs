d64b531e94e21c1bd67094fd6077c89a7da90478
Eli Reisman <eli.r@posthog.com>
2025-05-30 15:39:36 -0700

    update /engage event hydration test

--- rust/capture/src/v0_request.rs
+++ rust/capture/src/v0_request.rs
@@ -495,7 +495,7 @@ mod tests {
             parse_and_extract_events(
                 r#"{"token": "token", "distinct_id": "distinct_id", "properties":{"foo": 42, "bar": true}}"#
             ),
-            Err(CaptureError::RequestParsingError(_))
+            Err(CaptureError::RequestHydrationError(_))
         ));
     }
 
