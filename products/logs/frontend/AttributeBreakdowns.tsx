f3f8cc9a254bd0a7178e9ba7247c64aec9afa7b0
Frank Hamand <frankhamand@gmail.com>
2025-05-31 14:31:44 +0100

    sort attribute breakdowns and limit to 10

--- products/logs/frontend/AttributeBreakdowns.tsx
+++ products/logs/frontend/AttributeBreakdowns.tsx
@@ -7,11 +7,14 @@ export const AttributeBreakdowns = ({ attribute }: { attribute: string }): JSX.E
     const logic = attributeBreakdownLogic({ attribute })
     const { attributeValues, logCount, breakdowns } = useValues(logic)
 
-    const dataSource = Object.entries(breakdowns).map(([value, count]) => ({
-        value,
-        count,
-        percentage: ((count / attributeValues.length) * 100).toFixed(0),
-    }))
+    const dataSource = Object.entries(breakdowns)
+        .sort(([, c1], [, c2]) => c2 - c1)
+        .slice(0, 10)
+        .map(([value, count]) => ({
+            value,
+            count,
+            percentage: ((count / attributeValues.length) * 100).toFixed(0),
+        }))
 
     return (
         <div className="flex flex-col p-2 gap-y-2">
