a71196ec30d0aadd79e3d9b736b1df07872a2ab2
Joshua Snyder <joshua@timenavi.com>
2025-06-02 17:45:53 +0100

    Revert "chore: preserve query params in time sensitive auth checks (#32986)"
    This reverts commit e463eb63ac1101903a47a5c3d427f199511c16fb.

--- frontend/src/lib/components/TimeSensitiveAuthentication/TimeSensitiveAuthentication.tsx
+++ frontend/src/lib/components/TimeSensitiveAuthentication/TimeSensitiveAuthentication.tsx
@@ -22,7 +22,7 @@ export function TimeSensitiveAuthenticationModal(): JSX.Element {
     const showPassword = !ssoEnforcement && user?.has_password
 
     const extraQueryParams = {
-        next: encodeURI(location.href.replace(location.origin, '')),
+        next: window.location.pathname,
     }
 
     return (
