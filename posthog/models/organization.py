b46b8370291269f3dfa5f3c5b21af875eb7a7075
Yasen Slavov <yasen@Yasens-MacBook-Pro.local>
2025-06-03 19:18:08 +0300

    Fix migrations

--- posthog/models/organization.py
+++ posthog/models/organization.py
@@ -134,6 +134,7 @@ class Organization(UUIDModel):
     is_member_join_email_enabled = models.BooleanField(default=True)
     is_ai_data_processing_approved = models.BooleanField(null=True, blank=True)
     enforce_2fa = models.BooleanField(null=True, blank=True)
+    members_can_invite = models.BooleanField(default=True, null=True, blank=True)
 
     is_hipaa = models.BooleanField(default=False, null=True, blank=True)
 
@@ -155,7 +156,6 @@ class Organization(UUIDModel):
     never_drop_data = models.BooleanField(default=False, null=True, blank=True)
     # Scoring levels defined in billing::customer::TrustScores
     customer_trust_scores = models.JSONField(default=dict, null=True, blank=True)
-    members_can_invite = models.BooleanField(default=True)
 
     # DEPRECATED attributes (should be removed on next major version)
     setup_section_2_completed = models.BooleanField(default=True)
