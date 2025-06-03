48e80ce5c1644d6e32d4ccb1b22c788254e5f5f9
Ben White <ben@posthog.com>
2025-06-03 10:02:47 +0200

    feat(cdp): Webhook sources (#32415)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ frontend/src/scenes/hog-functions/configuration/components/HogFunctionSourceWebhookInfo.tsx
@@ -0,0 +1,26 @@
+import { useValues } from 'kea'
+import { CodeSnippet } from 'lib/components/CodeSnippet'
+import { LemonLabel } from 'lib/lemon-ui/LemonLabel'
+
+import { hogFunctionConfigurationLogic } from '../hogFunctionConfigurationLogic'
+
+export function HogFunctionSourceWebhookInfo(): JSX.Element {
+    const { logicProps } = useValues(hogFunctionConfigurationLogic)
+    const { id } = logicProps
+    return (
+        <div className="p-3 rounded border deprecated-space-y-2 bg-surface-primary">
+            <LemonLabel>Webhook URL</LemonLabel>
+            <CodeSnippet thing="Webhook URL">
+                {!id
+                    ? 'The webhook URL will be shown here once you save'
+                    : window.location.origin + '/public/webhooks/' + id}
+            </CodeSnippet>
+
+            <p className="text-sm">
+                Use this URL in your external system to send events to PostHog. The webhook can be called with a POST
+                request and any JSON payload. You can then use the configuration options to parse the{' '}
+                <code>request.body</code> or <code>request.headers</code> to map to the required fields.
+            </p>
+        </div>
+    )
+}
