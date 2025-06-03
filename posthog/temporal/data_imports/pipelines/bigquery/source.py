38e1987c9affbd354e48d1f8448e85e15f58240a
Tom Owers <owerstom@gmail.com>
2025-06-03 13:40:39 +0100

    Dont sync tables with duplicate primary keys

--- posthog/temporal/data_imports/pipelines/bigquery/source.py
+++ posthog/temporal/data_imports/pipelines/bigquery/source.py
@@ -248,6 +248,29 @@ def get_primary_keys(table: bigquery.Table, client: bigquery.Client) -> list[str
     return primary_keys
 
 
+def has_duplicate_primary_keys(table: bigquery.Table, client: bigquery.Client, primary_keys: list[str] | None) -> bool:
+    if not primary_keys or len(primary_keys) == 0:
+        return False
+
+    try:
+        query = f"""
+            SELECT {", ".join(primary_keys)}
+            FROM `{table.dataset_id}`.`{table.table_id}`
+            GROUP BY {", ".join([str(i + 1) for i, _ in enumerate(primary_keys)])}
+            HAVING COUNT(*) > 1
+        """
+
+        job_config = QueryJobConfig()
+        job = client.query(query, job_config=job_config)
+
+        for _ in job.result():
+            return True
+    except Exception as e:
+        capture_exception(e)
+
+    return False
+
+
 def _get_rows_to_sync(
     table: bigquery.Table,
     client: bigquery.Client,
@@ -355,6 +378,7 @@ def bigquery_source(
         bq_table = bq_client.get_table(fully_qualified_table_name)
         primary_keys = get_primary_keys(bq_table, bq_client)
         partition_settings = get_partition_settings(bq_table, bq_client, partition_size_bytes=partition_size_bytes)
+        has_duplicate_keys = has_duplicate_primary_keys(bq_table, bq_client, primary_keys)
         rows_to_sync = _get_rows_to_sync(
             bq_table,
             bq_client,
@@ -469,4 +493,5 @@ def bigquery_source(
         partition_count=partition_settings.partition_count if partition_settings else None,
         partition_size=partition_settings.partition_size if partition_settings else None,
         rows_to_sync=rows_to_sync,
+        has_duplicate_primary_keys=has_duplicate_keys,
     )
