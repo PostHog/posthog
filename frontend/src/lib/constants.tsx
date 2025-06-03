867a8e363c10d5154cb0c32c4367095dbe8ba5ae
Paul D'Ambra <paul@posthog.com>
2025-06-03 12:58:18 +0100

    feat: measure heap usage of the app (#33047)

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -280,6 +280,7 @@ export const FEATURE_FLAGS = {
     SCREENSHOT_EDITOR: 'screenshot-editor', // owner: @veryayskiy #team-replay
     ACTIVITY_OR_EXPLORE: 'activity-or-explore', // owner: @pauldambra #team-replay
     EXPERIMENTS_NEW_QUERY_RUNNER_FOR_USERS_ON_FREE_PLAN: 'experiments-new-query-runner-for-users-on-free-plan', // owner: #team-experiments
+    TRACK_MEMORY_USAGE: 'track-memory-usage', // owner: @pauldambra #team-replay
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
