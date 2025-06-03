f555c66317ac62b8a43c1675ad58e7f9de3f3836
Tom Owers <owerstom@gmail.com>
2025-06-03 15:03:52 +0100

    PR fixes

--- posthog/temporal/data_imports/pipelines/postgres/postgres.py
+++ posthog/temporal/data_imports/pipelines/postgres/postgres.py
@@ -187,15 +187,16 @@ def _has_duplicate_primary_keys(
             FROM {{}}.{{}}
             GROUP BY {", ".join([str(i + 1) for i, _ in enumerate(primary_keys)])}
             HAVING COUNT(*) > 1
+            LIMIT 1
         """,
         )
         query = sql.SQL(sql_query).format(
             *[sql.Identifier(key) for key in primary_keys], sql.Identifier(schema), sql.Identifier(table_name)
         )
         cursor.execute(query)
-        rows = cursor.fetchall()
+        row = cursor.fetchone()
 
-        return len(rows) > 0
+        return row is not None
     except Exception as e:
         capture_exception(e)
         return False
@@ -468,12 +469,12 @@ def postgres_source(
             chunk_size = _get_table_chunk_size(cursor, inner_query_with_limit, logger)
             rows_to_sync = _get_rows_to_sync(cursor, inner_query_without_limit, logger)
             partition_settings = _get_partition_settings(cursor, schema, table_name) if is_incremental else None
+            has_duplicate_primary_keys = False
 
             # Fallback on checking for an `id` field on the table
             if primary_keys is None and "id" in table:
                 primary_keys = ["id"]
-
-            has_duplicate_primary_keys = _has_duplicate_primary_keys(cursor, schema, table_name, primary_keys)
+                has_duplicate_primary_keys = _has_duplicate_primary_keys(cursor, schema, table_name, primary_keys)
 
     def get_rows(chunk_size: int) -> Iterator[Any]:
         arrow_schema = table.to_arrow_schema()
