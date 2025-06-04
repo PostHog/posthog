8f675547e04d866ff260cc0bf5339f8e00c9fbb1
Marius Andra <marius.andra@gmail.com>
2025-06-04 11:50:58 +0200

    fix for unfiled

--- frontend/src/layout/panel-layout/ProjectTree/projectTreeDataLogic.tsx
+++ frontend/src/layout/panel-layout/ProjectTree/projectTreeDataLogic.tsx
@@ -8,6 +8,7 @@ import { TreeDataItem } from 'lib/lemon-ui/LemonTree/LemonTree'
 import { Spinner } from 'lib/lemon-ui/Spinner'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { capitalizeFirstLetter } from 'lib/utils'
+import { getCurrentTeamIdOrNone } from 'lib/utils/getAppContext'
 import { urls } from 'scenes/urls'
 
 import { breadcrumbsLogic } from '~/layout/navigation/Breadcrumbs/breadcrumbsLogic'
@@ -100,6 +101,9 @@ export const projectTreeDataLogic = kea<projectTreeDataLogicType>([
             false as boolean,
             {
                 loadUnfiledItems: async () => {
+                    if (!getCurrentTeamIdOrNone()) {
+                        return false
+                    }
                     const response = await api.fileSystem.unfiled()
                     if (response.results?.length > 0) {
                         actions.loadFolder('Unfiled')
