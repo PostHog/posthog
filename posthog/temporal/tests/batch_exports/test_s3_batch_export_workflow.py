454fb9abb8460c3ed03cf2292d9bd04f447e196e
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-06-04 16:34:13 +0200

    fix: Do not sort in S3 tests

--- posthog/temporal/tests/batch_exports/test_s3_batch_export_workflow.py
+++ posthog/temporal/tests/batch_exports/test_s3_batch_export_workflow.py
@@ -239,7 +239,7 @@ async def assert_clickhouse_records_in_s3(
     file_format: str = "JSONLines",
     backfill_details: BackfillDetails | None = None,
     allow_duplicates: bool = False,
-    sort_key: str = "event",
+    sort_key: str | None = "event",
 ):
     """Assert ClickHouse records are written to JSON in key_prefix in S3 bucket_name.
 
@@ -327,6 +327,7 @@ async def assert_clickhouse_records_in_s3(
         is_backfill=backfill_details is not None,
         backfill_details=backfill_details,
         extra_query_parameters=extra_query_parameters,
+        order_columns=None,
     )
     while not queue.empty() or not producer_task.done():
         record_batch = await get_record_batch_from_queue(queue, producer_task)
@@ -358,9 +359,10 @@ async def assert_clickhouse_records_in_s3(
         s3_data = list({record["uuid"]: record for record in s3_data}.values())
     assert len(s3_data) == len(expected_records)
 
-    # Ordering is not guaranteed, so we sort before comparing.
-    s3_data.sort(key=operator.itemgetter(sort_key))
-    expected_records.sort(key=operator.itemgetter(sort_key))
+    if sort_key:
+        # Ordering is not guaranteed, so we sort before comparing.
+        s3_data.sort(key=operator.itemgetter(sort_key))
+        expected_records.sort(key=operator.itemgetter(sort_key))
 
     assert s3_data[0] == expected_records[0]
     assert s3_data == expected_records
@@ -1927,6 +1929,7 @@ async def test_insert_into_s3_activity_heartbeats(
         team_id=ateam.pk,
         data_interval_start=data_interval_start,
         data_interval_end=data_interval_end,
+        sort_key=None,
     )
 
 
