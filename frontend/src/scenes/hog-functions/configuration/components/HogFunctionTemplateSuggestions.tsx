d4aebef84276af562be54f0cdaaa44b4a8f191ec
Ben White <ben@posthog.com>
2025-06-04 17:40:15 +0200

    all the things

--- frontend/src/scenes/hog-functions/configuration/components/HogFunctionTemplateSuggestions.tsx
+++ frontend/src/scenes/hog-functions/configuration/components/HogFunctionTemplateSuggestions.tsx
@@ -1,43 +1,127 @@
-import { useValues } from 'kea'
-import { CodeSnippet } from 'lib/components/CodeSnippet'
-import { LemonLabel } from 'lib/lemon-ui/LemonLabel'
+import { STL as HOG_STL } from '@posthog/hogvm'
+import { IconMagicWand } from '@posthog/icons'
+import { LemonButton, LemonDivider, LemonDropdown, LemonInput, Link } from '@posthog/lemon-ui'
+import { useState } from 'react'
 
-import { hogFunctionConfigurationLogic } from '../hogFunctionConfigurationLogic'
-
-import { STL as hogvmSTL } from '@posthog/hogvm'
-import { LemonButton, LemonButtonWithDropdown, LemonMenu } from '@posthog/lemon-ui'
-import { IconMagic } from '@posthog/icons'
+export type HogFunctionTemplateOption = {
+    key: string
+    description: string
+    example: string
+}
 
 export type HogFunctionTemplateSuggestionsProps = {
     templating: 'hog' | 'liquid'
     value: string
+    onOptionSelect: (option: HogFunctionTemplateOption) => void
 }
 
-export function HogFunctionTemplateSuggestions({ templating, value }: HogFunctionTemplateSuggestionsProps): JSX.Element {
-    const { logicProps } = useValues(hogFunctionConfigurationLogic)
+const HOG_USAGE_EXAMPLES: HogFunctionTemplateOption[] = [
+    {
+        key: 'ternary',
+        example: `$1 = true ? 'Yes' : 'No'`,
+        description: 'Ternary operation (if this then that else other)',
+    },
+    {
+        key: 'default',
+        example: `$1 ?? 'Default value'`,
+        description: 'Default value (if this is null or undefined, use this)',
+    },
+]
 
+const HOG_STL_EXAMPLES: HogFunctionTemplateOption[] = Object.entries(HOG_STL).map(([key, value]) => ({
+    key,
+    example: value.example,
+    description: value.description,
+}))
+
+function HogFunctionTemplateSuggestionsItem({
+    option,
+    onSelect,
+}: {
+    option: HogFunctionTemplateOption
+    onSelect: (option: HogFunctionTemplateOption) => void
+}): JSX.Element {
+    return (
+        <LemonButton fullWidth role="menuitem" size="small" onClick={() => onSelect(option)}>
+            <div className="flex-col flex-1">
+                <code className="text-sm font-semibold font-monospace">{option.example}</code>
+                <div className="text-xs text-secondary">{option.description}</div>
+            </div>
+        </LemonButton>
+    )
+}
+
+export function HogFunctionTemplateSuggestions({
+    templating,
+    value,
+    onOptionSelect,
+}: HogFunctionTemplateSuggestionsProps): JSX.Element {
+    const [search, setSearch] = useState('')
+
+    // TODO
+    // * Add a logic
+    // * Add a search
+    // * Use the correct search logic for the template language
+    // * Have a way to search for event / properties etc.
 
     return (
-        <div className="p-3 rounded border deprecated-space-y-2 bg-surface-primary">
-            <LemonLabel>Suggestions</LemonLabel>
-            <CodeSnippet thing="Suggestions">
-                {templating === 'hog' ? hogvmSTL : ''}
-            </CodeSnippet>
+        <div className="flex overflow-hidden flex-col flex-1 gap-1 max-w-100">
+            <div className="flex flex-col gap-1 p-2 flex-0">
+                <LemonInput
+                    type="search"
+                    placeholder="Search"
+                    autoFocus
+                    value={search}
+                    onChange={setSearch}
+                    fullWidth
+                />
+                <div className="text-xs text-secondary">
+                    Below are a list of available functions for templating your inputs.{' '}
+                    <Link to="https://posthog.com/docs/cdp/destinations/customizing-destinations#customizing-payload">
+                        Learn more
+                    </Link>
+                </div>
+            </div>
+            <ul className="flex overflow-y-auto flex-col flex-1 gap-px p-2 border-t max-w-100">
+                {HOG_USAGE_EXAMPLES.map((value) => (
+                    <li key={value.key}>
+                        <HogFunctionTemplateSuggestionsItem option={value} onSelect={onOptionSelect} />
+                    </li>
+                ))}
 
-            <p className="text-sm">
-                Use this URL in your external system to send events to PostHog. The webhook can be called with a POST
-                request and any JSON payload. You can then use the configuration options to parse the{' '}
-                <code>request.body</code> or <code>request.headers</code> to map to the required fields.
-            </p>
+                <LemonDivider />
+                {HOG_STL_EXAMPLES.map((value) => (
+                    <li key={value.key}>
+                        <HogFunctionTemplateSuggestionsItem option={value} onSelect={onOptionSelect} />
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
 
+export function HogFunctionTemplateSuggestionsButton({
+    onOptionSelect,
+    ...props
+}: HogFunctionTemplateSuggestionsProps): JSX.Element {
+    const [showPopover, setShowPopover] = useState(false) // TODO: Move this to the logic somehow
 
-export function HogFunctionTemplateSuggestionsButton(): JSX.Element {
+    const _onOptionSelect = (option: HogFunctionTemplateOption): void => {
+        onOptionSelect(option)
+        setShowPopover(false)
+    }
+
     return (
-        <LemonMenu>
-            <LemonButton icon={<IconMagic/>}>
-            </LemonButton>
-        </LemonMenu>
+        <LemonDropdown
+            closeOnClickInside={false}
+            visible={showPopover}
+            matchWidth={false}
+            actionable
+            onVisibilityChange={(visible) => setShowPopover(visible)}
+            overlay={<HogFunctionTemplateSuggestions {...props} onOptionSelect={_onOptionSelect} />}
+            overflowHidden
+        >
+            <LemonButton size="small" icon={<IconMagicWand />} />
+        </LemonDropdown>
     )
+}
