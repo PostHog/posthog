e04ab5fbfe53dfacc8f87b0c2888c22cdfb7be50
Rodrigo Iloro <rodrigo@posthog.com>
2025-06-05 13:10:05 -0300

    feat(experiments): breakdown logic updates to support custom exposure criterias.

--- frontend/src/scenes/experiments/experimentResultBreakdownLogic.ts
+++ frontend/src/scenes/experiments/experimentResultBreakdownLogic.ts
@@ -1,13 +1,19 @@
 import { actions, afterMount, kea, key, path, props } from 'kea'
 import { loaders } from 'kea-loaders'
-import { FunnelLayout } from 'lib/constants'
-import { EXPERIMENT_DEFAULT_DURATION } from 'lib/constants'
+import { EXPERIMENT_DEFAULT_DURATION, FunnelLayout } from 'lib/constants'
 import { dayjs } from 'lib/dayjs'
 
 import { performQuery } from '~/queries/query'
-import type { AnyEntityNode, ExperimentMetric, FunnelsQuery, TrendsQuery } from '~/queries/schema/schema-general'
+import type {
+    AnyEntityNode,
+    ExperimentMetric,
+    FunnelsQuery,
+    FunnelStepsResults,
+    TrendsQuery,
+} from '~/queries/schema/schema-general'
 import { ExperimentMetricType, NodeKind } from '~/queries/schema/schema-general'
 import type { Experiment } from '~/types'
+import { PropertyFilterType, PropertyOperator } from '~/types'
 
 import type { experimentResultBreakdownLogicType } from './experimentResultBreakdownLogicType'
 
@@ -26,32 +32,32 @@ const metricToQuery = (metric: ExperimentMetric, experiment: Experiment): Funnel
     if (exposureCriteria && exposureCriteria.event !== '$feature_flag_called') {
         series.push({
             kind: NodeKind.EventsNode,
+            custom_name: exposureCriteria.event,
+            event: exposureCriteria.event,
+            properties: [
+                ...(exposureCriteria.properties || []),
+                {
+                    key: `$feature/${experiment.feature_flag_key}`,
+                    type: PropertyFilterType.Event,
+                    value: experiment.parameters.feature_flag_variants.map(({ key }) => key),
+                    operator: PropertyOperator.Exact,
+                },
+            ],
         })
     } else {
         series.push({
             kind: NodeKind.EventsNode,
-            custom_name: 'exposures',
+            custom_name: '$feature_flag_called',
             event: '$feature_flag_called',
+            properties: [
+                {
+                    key: '$feature_flag',
+                    type: PropertyFilterType.Event,
+                    value: experiment.feature_flag_key,
+                    operator: PropertyOperator.Exact,
+                },
+            ],
         })
-        // series.push({
-        //     id: '$feature_flag_called',
-        //     name: '$feature_flag_called',
-        //     type: 'events',
-        //     properties: [
-        //         {
-        //             key: '$feature_flag_response',
-        //             type: PropertyFilterType.Event,
-        //             value: [variantKey],
-        //             operator: PropertyOperator.Exact,
-        //         },
-        //         {
-        //             key: '$feature_flag',
-        //             type: PropertyFilterType.Event,
-        //             value: experiment.feature_flag_key,
-        //             operator: PropertyOperator.Exact,
-        //         },
-        //     ],
-        // })
     }
 
     /**
@@ -74,7 +80,6 @@ const metricToQuery = (metric: ExperimentMetric, experiment: Experiment): Funnel
         series,
         filterTestAccounts: !!experiment.exposure_criteria?.filterTestAccounts,
         dateRange: {
-            // date_from: '2025-02-28T21:45:00+00:00',
             date_from:
                 experiment.start_date ||
                 dayjs().subtract(EXPERIMENT_DEFAULT_DURATION, 'day').format('YYYY-MM-DDTHH:mm'),
@@ -87,7 +92,7 @@ const metricToQuery = (metric: ExperimentMetric, experiment: Experiment): Funnel
             funnelOrderType: 'ordered',
             funnelStepReference: 'total',
             funnelVizType: 'steps',
-            funnelWindowInterval: 14, //<- need to fix this
+            funnelWindowInterval: 14,
             funnelWindowIntervalUnit: 'day',
         },
         breakdownFilter: {
@@ -97,13 +102,20 @@ const metricToQuery = (metric: ExperimentMetric, experiment: Experiment): Funnel
     } as FunnelsQuery
 }
 
+/**
+ * This logic only works with modern engines, like bayesian and frequentist.
+ * Legacy Funnels and Trends engine are resolved backend side.
+ */
 export const experimentResultBreakdownLogic = kea<experimentResultBreakdownLogicType>([
     props({} as ExperimentResultBreakdownLogicProps),
+
     key(
         ({ experiment, metric }: ExperimentResultBreakdownLogicProps) =>
             `${experiment.id}-${metric.kind}-${metric.name}`
     ),
+
     path((key) => ['scenes', 'experiment', 'experimentResultBreakdownLogic', key]),
+
     actions({
         loadBreakdownResults: true,
     }),
@@ -112,19 +124,28 @@ export const experimentResultBreakdownLogic = kea<experimentResultBreakdownLogic
         breakdownResults: [
             null as any,
             {
-                loadBreakdownResults: async () => {
-                    const query = metricToQuery(props.metric, props.experiment)
+                loadBreakdownResults: async (): Promise<{
+                    query: FunnelsQuery | TrendsQuery
+                    results: FunnelStepsResults
+                }> => {
+                    const { metric, experiment } = props
+                    /**
+                     * take the metric and the experiment and create a new Funnel or Trends query.
+                     * we need the experiment for exposure configuration.
+                     */
+                    const query = metricToQuery(metric, experiment)
 
+                    /**
+                     * perform the query
+                     */
                     const response = await performQuery(query)
 
                     /**
-                     * filter results to only include valid variants in the first step
+                     * we need to filter the results
                      */
-                    const results = response.results
-                    // for now, just remove the first item. We'll make it better tomorrow...
-                    results.shift()
+                    const results = response.results as FunnelStepsResults
 
-                    return { query, insight: response.results }
+                    return { query, results }
                 },
             },
         ],
