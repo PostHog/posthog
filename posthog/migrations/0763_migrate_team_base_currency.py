82919fb7f244e079cd04a4e7db8a70824efcbdce
Javier Bahamondes <javierbahamondes@Javiers-MacBook-Pro.local>
2025-06-06 09:51:24 -0400

    fix(migration): update dependency for team base currency migration

--- posthog/migrations/0763_migrate_team_base_currency.py
+++ posthog/migrations/0763_migrate_team_base_currency.py
@@ -39,7 +39,7 @@ def migrate_revenue_base_currency(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0760_team_base_currency"),
+        ("posthog", "0762_team_base_currency"),
     ]
 
     operations = [
