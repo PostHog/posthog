b13196d3f63f61943db26712e8524919cf7bf238
Marius Andra <marius.andra@gmail.com>
2025-06-06 11:02:38 +0200

    feat(project-tree): pinned folder menu

--- frontend/src/layout/panel-layout/PinnedFolder/PinnedFolder.tsx
+++ frontend/src/layout/panel-layout/PinnedFolder/PinnedFolder.tsx
@@ -4,6 +4,12 @@ import { FolderSelect } from 'lib/components/FolderSelect/FolderSelect'
 import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { LemonModal } from 'lib/lemon-ui/LemonModal'
 import { ButtonPrimitive } from 'lib/ui/Button/ButtonPrimitives'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from 'lib/ui/DropdownMenu/DropdownMenu'
 
 import { panelLayoutLogic } from '~/layout/panel-layout/panelLayoutLogic'
 import { pinnedFolderLogic } from '~/layout/panel-layout/PinnedFolder/pinnedFolderLogic'
@@ -15,37 +21,44 @@ export function PinnedFolder(): JSX.Element {
     const { modalVisible, pinnedFolder, selectedFolder } = useValues(pinnedFolderLogic)
     const { hideModal, showModal, setPinnedFolder, setSelectedFolder } = useActions(pinnedFolderLogic)
 
+    const showDefaultHeader = pinnedFolder !== 'products://' && pinnedFolder !== 'data://'
+
+    const configMenu = (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <ButtonPrimitive iconOnly data-attr="tree-navbar-pinned-folder-change-button">
+                    <IconGear className="size-3 text-secondary" />
+                </ButtonPrimitive>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent loop align="end" side="bottom" className="max-w-[250px]">
+                <DropdownMenuItem
+                    asChild
+                    onClick={(e) => {
+                        e.stopPropagation()
+                        showModal()
+                    }}
+                    data-attr="tree-item-menu-open-link-button"
+                >
+                    <ButtonPrimitive menuItem>Change pinned folder</ButtonPrimitive>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+
     return (
         <>
             {!isLayoutNavCollapsed &&
-                (pinnedFolder === 'products://' ? (
-                    <ButtonPrimitive
-                        onClick={showModal}
-                        iconOnly
-                        tooltip="Change pinned folder"
-                        tooltipPlacement="right"
-                        data-attr="tree-navbar-pinned-folder-change-button"
-                        className="absolute right-1 z-10"
-                    >
-                        <IconGear className="size-3 text-secondary" />
-                    </ButtonPrimitive>
-                ) : (
+                (showDefaultHeader ? (
                     <div className="flex justify-between items-center pl-3 pr-1 relative">
                         <div className="flex items-center gap-1">
                             <span className="text-xs font-semibold text-quaternary">
                                 {formatUrlAsName(pinnedFolder)}
                             </span>
                         </div>
-                        <ButtonPrimitive
-                            onClick={showModal}
-                            iconOnly
-                            tooltip="Change pinned folder"
-                            tooltipPlacement="right"
-                            data-attr="tree-navbar-pinned-folder-change-button"
-                        >
-                            <IconGear className="size-3 text-secondary" />
-                        </ButtonPrimitive>
+                        {configMenu}
                     </div>
+                ) : (
+                    <div className="absolute right-1 z-10">{configMenu}</div>
                 ))}
             <div className="flex flex-col mt-[-0.25rem] h-full group/colorful-product-icons colorful-product-icons-true">
                 <ProjectTree root={pinnedFolder} onlyTree treeSize={isLayoutNavCollapsed ? 'narrow' : 'default'} />
