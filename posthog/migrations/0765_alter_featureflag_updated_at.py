c05e904f30ee8f26b532d5affcbebec7d59acc65
Daniel Guerra <daniel.guerra@automattic.com>
2025-06-08 11:01:59 -0600

    Rebase migrations

--- posthog/migrations/0765_alter_featureflag_updated_at.py
+++ posthog/migrations/0765_alter_featureflag_updated_at.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0763_featureflag_updated_at"),
+        ("posthog", "0764_featureflag_updated_at"),
     ]
 
     operations = [
