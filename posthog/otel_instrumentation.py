7fda33665d50ad9c28f0a33eef3a8303f92a318c
Bryan Ciaraldi <bryan@posthog.com>
2025-06-09 11:26:32 -0700

    sentry has been retired

--- posthog/otel_instrumentation.py
+++ posthog/otel_instrumentation.py
@@ -20,7 +20,7 @@ logger = structlog.get_logger(__name__)
 
 def _otel_django_request_hook(span, request):
     if span and span.is_recording():
-        actual_path = request.path or ""
+        actual_path = request.path
         http_method = request.method
         span.set_attribute("http.method", http_method)
         span.set_attribute("http.url", actual_path)
@@ -87,7 +87,6 @@ def initialize_otel():
         )
 
         try:
-            # Add Sentry context to OpenTelemetry spans
             DjangoInstrumentor().instrument(
                 tracer_provider=provider,
                 request_hook=_otel_django_request_hook,
