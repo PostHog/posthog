8eabf11f17b7cea3ff85ce4a4c3fcb2ab0cabb4c
Alex Lebedev <alex.l@posthog.com>
2025-06-09 16:40:03 +0200

    feat: Init Redis implementation.

--- posthog/temporal/ai/session_summary/summarize_session.py
+++ posthog/temporal/ai/session_summary/summarize_session.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 import json
 import time
 import uuid
+from redis import Redis
 import temporalio
 from temporalio.common import RetryPolicy, WorkflowIDReusePolicy
 from django.conf import settings
@@ -16,10 +17,10 @@ from ee.session_recordings.session_summary.summarize_session import (
     prepare_data_for_single_session_summary,
 )
 from ee.session_recordings.session_summary.utils import serialize_to_sse_event
+from posthog.redis import get_client
 from posthog.temporal.common.codec import EncryptionCodec
 from temporalio.api.common.v1 import Payload
 from posthog.models.team.team import Team
-from posthog.temporal.common.base import PostHogWorkflow
 from posthog.temporal.common.client import connect
 from posthog.settings import SERVER_GATEWAY_INTERFACE
 from temporalio.client import Client as TemporalClient, WorkflowHandle
@@ -48,48 +49,81 @@ class SessionSummaryInputs:
     session_duration: int
 
 
+def _get_input_data_from_redis(redis_client: Redis, redis_input_key: str) -> tuple[str, SessionSummaryInputs]:
+    raw_redis_data = redis_client.get(redis_input_key)
+    if not raw_redis_data:
+        raise ValueError(
+            f"Input data not found in Redis for key {redis_input_key} when generating single session summary"
+        )
+    if not isinstance(raw_redis_data, str):
+        raise ValueError(
+            f"Input data is not a string in Redis for key {redis_input_key} when generating single session summary, but {type(raw_redis_data)}: {raw_redis_data}"
+        )
+    try:
+        redis_data = json.loads(raw_redis_data)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Failed to parse input data ({raw_redis_data}): {e}")
+    redis_output_key = redis_data.get("output_key")
+    if not redis_output_key:
+        raise ValueError(
+            f"Output key not found in Redis for key {redis_input_key} when generating single session summary: {redis_data}"
+        )
+    input_data = redis_data.get("input_data")
+    if not input_data:
+        raise ValueError(
+            f"Input data not found in Redis for key {redis_input_key} when generating single session summary: {redis_data}"
+        )
+    try:
+        summary_inputs = SessionSummaryInputs(**input_data)
+    except Exception as e:
+        raise ValueError(f"Session summary input data is not valid ({input_data}): {e}")
+    return redis_output_key, summary_inputs
+
+
 @temporalio.activity.defn
-async def stream_llm_summary_activity(inputs: SessionSummaryInputs) -> str:
+async def stream_llm_summary_activity(redis_input_key: str) -> str:
+    # Creating client on each activity as we can't pass it in as an argument, and need it for both getting and storing data
+    redis_client = get_client()
+    redis_output_key, summary_inputs = _get_input_data_from_redis(
+        redis_client=redis_client, redis_input_key=redis_input_key
+    )
     last_summary_state = ""
     session_summary_generator = stream_llm_session_summary(
-        session_id=inputs.session_id,
-        user_pk=inputs.user_pk,
-        summary_prompt=inputs.summary_prompt,
-        system_prompt=inputs.system_prompt,
-        allowed_event_ids=list(inputs.simplified_events_mapping.keys()),
-        simplified_events_mapping=inputs.simplified_events_mapping,
-        simplified_events_columns=inputs.simplified_events_columns,
-        url_mapping_reversed=inputs.url_mapping_reversed,
-        window_mapping_reversed=inputs.window_mapping_reversed,
-        session_start_time_str=inputs.session_start_time_str,
-        session_duration=inputs.session_duration,
+        session_id=summary_inputs.session_id,
+        user_pk=summary_inputs.user_pk,
+        summary_prompt=summary_inputs.summary_prompt,
+        system_prompt=summary_inputs.system_prompt,
+        allowed_event_ids=list(summary_inputs.simplified_events_mapping.keys()),
+        simplified_events_mapping=summary_inputs.simplified_events_mapping,
+        simplified_events_columns=summary_inputs.simplified_events_columns,
+        url_mapping_reversed=summary_inputs.url_mapping_reversed,
+        window_mapping_reversed=summary_inputs.window_mapping_reversed,
+        session_start_time_str=summary_inputs.session_start_time_str,
+        session_duration=summary_inputs.session_duration,
     )
     async for current_summary_state in session_summary_generator:
         if current_summary_state == last_summary_state:
             # Skip cases where no updates happened or the same state was sent again
             continue
         last_summary_state = current_summary_state
-        temporalio.activity.heartbeat({"last_summary_state": last_summary_state, "timestamp": time.time()})
+        # Store the last summary state in Redis
+        redis_client.setex(
+            redis_output_key,
+            900,  # 15 minutes TTL to keep alive for retries
+            json.dumps({"last_summary_state": last_summary_state, "timestamp": time.time()}),
+        )
+        # Heartbeat to avoid workflow timeout
+        temporalio.activity.heartbeat()
     return last_summary_state
 
 
 @temporalio.workflow.defn(name="summarize-session")
-class SummarizeSessionWorkflow(PostHogWorkflow):
-    @staticmethod
-    def parse_inputs(inputs: list[str]) -> SessionSummaryInputs:
-        try:
-            parsed_inputs = json.loads(inputs[0])
-            return SessionSummaryInputs(**parsed_inputs)
-        except Exception as e:
-            raise ValueError(f"Failed to parse inputs: {e}") from e
-
+class SummarizeSessionWorkflow:
     @temporalio.workflow.run
-    async def run(self, inputs: list[str]) -> str:
-        parsed_inputs = self.parse_inputs(inputs)
-        # Run as activity with heartbeat timeout
+    async def run(self, redis_input_key: str) -> str:
         result = await temporalio.workflow.execute_activity(
             stream_llm_summary_activity,
-            parsed_inputs,
+            redis_input_key,
             start_to_close_timeout=timedelta(minutes=5),
             heartbeat_timeout=timedelta(seconds=30),
             retry_policy=RetryPolicy(maximum_attempts=3),
@@ -97,12 +131,12 @@ class SummarizeSessionWorkflow(PostHogWorkflow):
         return result
 
 
-async def _start_workflow(str_inputs: list[str], workflow_id: str) -> WorkflowHandle:
+async def _start_workflow(redis_input_key: str, workflow_id: str) -> WorkflowHandle:
     client = await _connect_to_temporal_client()
     retry_policy = RetryPolicy(maximum_attempts=int(settings.TEMPORAL_WORKFLOW_MAX_ATTEMPTS))
     handle = await client.start_workflow(
         "summarize-session",
-        str_inputs,
+        redis_input_key,
         id=workflow_id,
         id_reuse_policy=WorkflowIDReusePolicy.ALLOW_DUPLICATE_FAILED_ONLY,
         task_queue=settings.TEMPORAL_TASK_QUEUE,
@@ -122,6 +156,11 @@ def _decode_payload(payload_data: bytes) -> str | None:
         return last_summary_state
     except Exception:
         return None
+
+
+def _clean_up_redis(redis_client: Redis, redis_input_key: str, redis_output_key: str) -> None:
+    redis_client.delete(redis_input_key)
+    redis_client.delete(redis_output_key)
 
 
 def execute_summarize_session(
@@ -155,11 +194,9 @@ def execute_summarize_session(
         raise ValueError(f"Session start time is missing in the session metadata for session_id {session_id}")
     if summary_data.prompt_data.prompt_data.metadata.duration is None:
         raise ValueError(f"Session duration is missing in the session metadata for session_id {session_id}")
-    # Connect to the client and prepare the input
-    random_id = str(uuid.uuid4())  # TODO: Generate a proper id
-    session_start_time_str = summary_data.prompt_data.prompt_data.metadata.start_time.isoformat()
-    # TODO: Store in Redis and send a key instead?
-    inputs = SessionSummaryInputs(
+
+    # Prepare the input
+    input_data = SessionSummaryInputs(
         session_id=session_id,
         user_pk=user_pk,
         summary_prompt=summary_data.prompt.summary_prompt,
@@ -168,12 +205,26 @@ def execute_summarize_session(
         simplified_events_columns=summary_data.prompt_data.prompt_data.columns,
         url_mapping_reversed=summary_data.prompt_data.url_mapping_reversed,
         window_mapping_reversed=summary_data.prompt_data.window_mapping_reversed,
-        session_start_time_str=session_start_time_str,
+        session_start_time_str=summary_data.prompt_data.prompt_data.metadata.start_time.isoformat(),
         session_duration=summary_data.prompt_data.prompt_data.metadata.duration,
     )
-    str_inputs = [json.dumps(dataclasses.asdict(inputs))]
-    # Start streaming the workflow
-    handle = asyncio.run(_start_workflow(str_inputs=str_inputs, workflow_id=random_id))
+    # Connect to Redis and prepare the input
+    redis_client = get_client()
+    redis_input_key = f"session_summary:single:stream-input:{session_id}:{user_pk}:{uuid.uuid4()}"
+    redis_output_key = f"session_summary:single:stream-output:{session_id}:{user_pk}:{uuid.uuid4()}"
+    redis_client.setex(
+        redis_input_key,
+        900,  # 15 minutes TTL to keep alive for retries
+        json.dumps(
+            {
+                "input_data": dataclasses.asdict(input_data),
+                "output_key": redis_output_key,
+            }
+        ),
+    )
+    # Connect to Temporal and start streaming the workflow
+    workflow_id = f"session-summary:single:{session_id}:{user_pk}:{uuid.uuid4()}"
+    handle = asyncio.run(_start_workflow(redis_input_key=redis_input_key, workflow_id=workflow_id))
     last_summary_state = ""
     while True:
         desc = asyncio.run(handle.describe())
@@ -183,7 +234,8 @@ def execute_summarize_session(
                 # Yield final result if it's different from the last state OR if we haven't yielded anything yet
                 if final_result != last_summary_state or not last_summary_state:
                     yield final_result
-                    return
+                _clean_up_redis(redis_client, redis_input_key, redis_output_key)
+                return
             except Exception:
                 # TODO: Handle any errors in getting the result
                 pass
@@ -194,25 +246,24 @@ def execute_summarize_session(
                 event_label="session-summary-error",
                 event_data=f"Failed to generate summary: {desc.status.name}",
             )
+            _clean_up_redis(redis_client, redis_input_key, redis_output_key)
             return
-        # If the workflow is still running, access heartbeat details from activity
-        if not desc.raw_description.pending_activities:
+        # If the workflow is still running, get current summary state from Redis
+        redis_data_raw = redis_client.get(redis_output_key)
+        if not redis_data_raw:
             continue
-        for activity in desc.raw_description.pending_activities:
-            if not activity.heartbeat_details:
-                continue
-            heartbeat_payloads = activity.heartbeat_details.payloads
-            for payload in heartbeat_payloads:
-                current_summary_state = _decode_payload(payload.data)
-                if not current_summary_state or current_summary_state == last_summary_state:
-                    # Skip cases where no updates happened or the same state was sent again
-                    continue
-                # Yield latest summary state
-                last_summary_state = current_summary_state
-                yield last_summary_state
-            # Get the final result after workflow completes
-        # Wait till next heartbeat to let new chunks come in from the stream
-        time.sleep(0.1)
+        try:
+            redis_data = json.loads(redis_data_raw)
+        except json.JSONDecodeError:
+            raise ValueError(
+                f"Failed to parse Redis output data ({redis_data_raw}) for key {redis_output_key} when generating single session summary"
+            )
+        last_summary_state = redis_data.get("last_summary_state")
+        if not isinstance(last_summary_state, str):
+            continue
+        yield last_summary_state
+        # Wait a bit to let new chunks come in from the stream
+        time.sleep(0.05)
 
 
 def stream_recording_summary(
