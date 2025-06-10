8448d0912928f56adfbb681b961eb9a6ce48b742
Paul D'Ambra <paul.dambra@gmail.com>
2025-06-10 22:53:22 +0100

    Discard changes to frontend/src/products.tsx

--- frontend/src/products.tsx
+++ frontend/src/products.tsx
@@ -541,4 +541,11 @@ export const getTreeItemsMetadata = (): FileSystemImport[] => [
         href: urls.revenueSettings(),
         flag: FEATURE_FLAGS.REVENUE_ANALYTICS,
     },
+    {
+        path: 'Marketing settings',
+        category: 'Definitions',
+        iconType: 'definitions',
+        href: urls.marketingAnalytics(),
+        flag: FEATURE_FLAGS.WEB_ANALYTICS_MARKETING,
+    },
 ]
