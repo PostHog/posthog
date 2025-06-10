742697be0b0d10f294ca2eb40d67372162c15df7
jabahamondes <javier@posthog.com>
2025-06-10 16:05:04 -0400

    feat(web-analytics): self managed resources integration (#33330)
    Co-authored-by: Javier Bahamondes <javierbahamondes@Javiers-MacBook-Pro.local>

--- frontend/src/scenes/settings/types.ts
+++ frontend/src/scenes/settings/types.ts
@@ -142,8 +142,8 @@ export type SettingId =
     | 'customization-irl'
     | 'web-analytics-pre-aggregated-tables'
     | 'csp-reporting'
-    | 'marketing-analytics-external-data-sources'
     | 'base-currency'
+    | 'marketing-settings'
 
 type FeatureFlagKey = keyof typeof FEATURE_FLAGS
 
