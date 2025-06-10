9e17ac5ffee158c99ba3decf6cc74b85087ad827
pauldambra <paul@posthog.com>
2025-06-10 17:43:04 +0100

    decouple launching survey

--- frontend/src/scenes/session-recordings/player/sessionRecordingPlayerLogic.ts
+++ frontend/src/scenes/session-recordings/player/sessionRecordingPlayerLogic.ts
@@ -31,6 +31,7 @@ import { openBillingPopupModal } from 'scenes/billing/BillingPopup'
 import { ReplayIframeData } from 'scenes/heatmaps/heatmapsBrowserLogic'
 import { preflightLogic } from 'scenes/PreflightCheck/preflightLogic'
 import { ExportedSessionType } from 'scenes/session-recordings/file-playback/types'
+import { playerCommentModel } from 'scenes/session-recordings/player/playerCommentModel'
 import {
     sessionRecordingDataLogic,
     SessionRecordingDataLogicProps,
@@ -49,6 +50,8 @@ import {
 
 import type { sessionRecordingsPlaylistLogicType } from '../playlist/sessionRecordingsPlaylistLogicType'
 import { sessionRecordingEventUsageLogic } from '../sessionRecordingEventUsageLogic'
+import { playerCommentOverlayLogic } from './playerFrameCommentOverlayLogic'
+import { playerCommentOverlayLogicType } from './playerFrameCommentOverlayLogicType'
 import { playerSettingsLogic } from './playerSettingsLogic'
 import { COMMON_REPLAYER_CONFIG, CorsPlugin, HLSPlayerPlugin } from './rrweb'
 import { CanvasReplayerPlugin } from './rrweb/canvas/canvas-plugin'
@@ -790,6 +793,30 @@ export const sessionRecordingPlayerLogic = kea<sessionRecordingPlayerLogicType>(
         ],
     }),
     listeners(({ props, values, actions, cache }) => ({
+        [playerCommentModel.actionTypes.startCommenting]: async ({ annotation }) => {
+            actions.setIsCommenting(true)
+            if (annotation) {
+                // and we need a short wait until the logic is mounted after calling setIsCommenting
+                const waitForLogic = async (): Promise<BuiltLogic<playerCommentOverlayLogicType> | null> => {
+                    for (let attempts = 0; attempts < 5; attempts++) {
+                        const theMountedLogic = playerCommentOverlayLogic.findMounted()
+                        if (theMountedLogic) {
+                            return theMountedLogic
+                        }
+                        await new Promise((resolve) => setTimeout(resolve, 100))
+                    }
+                    return null
+                }
+
+                const theMountedLogic = await waitForLogic()
+
+                if (theMountedLogic) {
+                    theMountedLogic.actions.editAnnotation(annotation)
+                } else {
+                    lemonToast.error('Could not start editing annotation 😓, please refresh the page and try again.')
+                }
+            }
+        },
         setIsCommenting: ({ isCommenting }) => {
             if (isCommenting) {
                 actions.setPause()
