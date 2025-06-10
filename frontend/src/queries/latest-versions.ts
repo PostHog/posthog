2bacdfa81173089bc71179d6e85d4b741e1cb8a5
Thomas Obermüller <thomas.obermueller@gmail.com>
2025-06-10 11:34:02 +0200

    format latest-versions.json with prettier

--- frontend/src/queries/latest-versions.ts
+++ frontend/src/queries/latest-versions.ts
@@ -2,4 +2,7 @@ import * as latestVersions from './latest-versions.json'
 import { NodeKind } from './schema'
 import { integer } from './schema/type-utils'
 
-export const LATEST_VERSIONS = latestVersions as Record<NodeKind, integer>
+// strip comment
+const sanitizedVersions = Object.fromEntries(Object.entries(latestVersions).filter(([key]) => key !== '//'))
+
+export const LATEST_VERSIONS = sanitizedVersions as Record<NodeKind, integer>
