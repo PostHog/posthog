bf1a0f3af257db04ff7a73f77a2aec4bbce61443
jabahamondes <javier@posthog.com>
2025-06-11 12:50:25 -0400

    feat(marketing-analytics): add native data sources configuration (#33391)
    Co-authored-by: Javier Bahamondes <javierbahamondes@Javiers-MacBook-Pro.local>

--- frontend/src/scenes/web-analytics/tabs/marketing-analytics/frontend/components/settings/MarketingAnalyticsSettings.tsx
+++ frontend/src/scenes/web-analytics/tabs/marketing-analytics/frontend/components/settings/MarketingAnalyticsSettings.tsx
@@ -1,9 +1,11 @@
+import { NativeExternalDataSourceConfiguration } from './NativeExternalDataSourceConfiguration'
 import { NonNativeExternalDataSourceConfiguration } from './NonNativeExternalDataSourceConfiguration'
 import { SelfManagedExternalDataSourceConfiguration } from './SelfManagedExternalDataSourceConfiguration'
 
 export function MarketingAnalyticsSettings(): JSX.Element {
     return (
         <div className="flex flex-col gap-8 mb-10">
+            <NativeExternalDataSourceConfiguration />
             <NonNativeExternalDataSourceConfiguration />
             <SelfManagedExternalDataSourceConfiguration />
         </div>
