34492cdccda3aeeab4e0b3b26b746429004d5094
jabahamondes <javier@posthog.com>
2025-06-11 16:31:13 -0400

    feat(MA): refactor currency handling, integrate it to queries (#33393)
    Co-authored-by: Javier Bahamondes <javierbahamondes@Javiers-MacBook-Pro.local>

--- frontend/src/scenes/web-analytics/tabs/marketing-analytics/frontend/components/settings/MarketingAnalyticsSettings.tsx
+++ frontend/src/scenes/web-analytics/tabs/marketing-analytics/frontend/components/settings/MarketingAnalyticsSettings.tsx
@@ -1,3 +1,5 @@
+import { BaseCurrency } from 'lib/components/BaseCurrency/BaseCurrency'
+
 import { NativeExternalDataSourceConfiguration } from './NativeExternalDataSourceConfiguration'
 import { NonNativeExternalDataSourceConfiguration } from './NonNativeExternalDataSourceConfiguration'
 import { SelfManagedExternalDataSourceConfiguration } from './SelfManagedExternalDataSourceConfiguration'
@@ -5,6 +7,7 @@ import { SelfManagedExternalDataSourceConfiguration } from './SelfManagedExterna
 export function MarketingAnalyticsSettings(): JSX.Element {
     return (
         <div className="flex flex-col gap-8 mb-10">
+            <BaseCurrency />
             <NativeExternalDataSourceConfiguration />
             <NonNativeExternalDataSourceConfiguration />
             <SelfManagedExternalDataSourceConfiguration />
