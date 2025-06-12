ebb1db1b504bee1a9aed56c153c2f6d864145cab
Ben White <ben@posthog.com>
2025-06-12 14:37:06 +0200

    Fixes

--- frontend/src/scenes/data-pipelines/batch-exports/batchExportRunsLogic.tsx
+++ frontend/src/scenes/data-pipelines/batch-exports/batchExportRunsLogic.tsx
@@ -6,8 +6,8 @@ import { dayjs } from 'lib/dayjs'
 
 import { BatchExportRun, GroupedBatchExportRuns, RawBatchExportRun } from '~/types'
 
-import { batchExportConfigurationLogic } from './batchExportConfigurationLogic'
 import { batchExportBackfillModalLogic } from './batchExportBackfillModalLogic'
+import { batchExportConfigurationLogic } from './batchExportConfigurationLogic'
 import type { batchExportRunsLogicType } from './batchExportRunsLogicType'
 
 const DEFAULT_DATE_FROM = '-2d'
