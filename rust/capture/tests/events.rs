9684691f6193a8e5913a1d0cb9f77a4023840936
Eli Reisman <eli.r@posthog.com>
2025-06-12 12:10:31 -0700

    update comment

--- rust/capture/tests/events.rs
+++ rust/capture/tests/events.rs
@@ -72,7 +72,7 @@ async fn it_drops_performance_events() -> Result<()> {
 
     let res = server.capture_events(retained_one.to_string()).await;
     assert_eq!(StatusCode::OK, res.status());
-    // this one will return a 4xx since the batch will be empty post-filtering
+    // silently ignored if the filtering of unsupported event types results in an empty payload
     let res = server.capture_events(should_drop.to_string()).await;
     assert_eq!(StatusCode::OK, res.status());
     let res = server.capture_events(retained_two.to_string()).await;
