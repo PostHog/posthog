6f885178cde793047b0f2ced04c1dca44589f526
Oliver Browne <oliver@posthog.com>
2025-06-12 22:19:44 +0300

    respect setting

--- rust/cymbal/src/pipeline/prep.rs
+++ rust/cymbal/src/pipeline/prep.rs
@@ -104,10 +104,12 @@ fn transform_event(
     person_mode: PersonMode,
     team: &Team,
 ) -> ClickHouseEvent {
-    // Fold the ip the event was sent from into the event properties
-    raw_event
-        .properties
-        .insert("$ip".to_string(), Value::String(outer.ip.clone()));
+    if !team.anonymize_ips {
+        // Fold the ip the event was sent from into the event properties
+        raw_event
+            .properties
+            .insert("$ip".to_string(), Value::String(outer.ip.clone()));
+    }
 
     // Fold in $set and $set_once properties
     let set = raw_event.set;
