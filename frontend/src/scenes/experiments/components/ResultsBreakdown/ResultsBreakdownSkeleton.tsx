57bb039712161fed8bcd994986d721e15ffc7655
Rodrigo Iloro <rodrigo@posthog.com>
2025-06-13 18:33:35 -0300

    feat: only show loading indicator if the user has the feature flag enabled.

--- frontend/src/scenes/experiments/components/ResultsBreakdown/ResultsBreakdownSkeleton.tsx
+++ frontend/src/scenes/experiments/components/ResultsBreakdown/ResultsBreakdownSkeleton.tsx
@@ -1,6 +1,13 @@
+import { useFeatureFlag } from 'lib/hooks/useFeatureFlag'
 import { Spinner } from 'lib/lemon-ui/Spinner/Spinner'
 
-export const ResultsBreakdownSkeleton = (): JSX.Element => {
+export const ResultsBreakdownSkeleton = (): JSX.Element | null => {
+    const isEnabled = useFeatureFlag('EXPERIMENTS_NEW_RUNNER_RESULTS_BREAKDOWN')
+
+    if (!isEnabled) {
+        return null
+    }
+
     return (
         <div className="space-y-4">
             <div className="border rounded-lg p-4 bg-bg-light space-y-3">
