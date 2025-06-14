333fce92c84b7df0ea36d7fc9907a56c7e8205f9
kappa90 <e.capparelli@gmail.com>
2025-06-14 16:19:26 +0200

    fix: pattern matching

--- common/storybook/tailwind.config.js
+++ common/storybook/tailwind.config.js
@@ -3,5 +3,5 @@ const config = require('../tailwind/tailwind.config.js')
 // eslint-disable-next-line no-undef
 module.exports = {
     ...config,
-    content: config.content.map((path) => path.replace('../', '../../')),
+    content: config.content.map((path) => path.replace(/\.\.\//g, '../../')),
 }
