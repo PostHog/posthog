33ef8fa7a578178292f16d7d677b6b3eab1e060b
Ben White <ben@posthog.com>
2025-06-16 14:13:58 +0200

    Fixes

--- plugin-server/src/cdp/services/hogflow-manager.service.ts
+++ plugin-server/src/cdp/services/hogflow-manager.service.ts
@@ -1,5 +1,4 @@
-import { PostgresUse } from '~/src/utils/db/postgres'
-
+import { PostgresUse } from '../../utils/db/postgres'
 import { HogFlow } from '../../schema/hogflow'
 import { Hub, Team } from '../../types'
 import { parseJSON } from '../../utils/json-parse'
