1ba39840e6376c2a2aaa957f036169b96c2da977
Hugues Pouillot <hpouillot@gmail.com>
2025-06-16 15:54:39 +0200

    fix styling

--- products/error_tracking/frontend/components/ExceptionCard/Tabs/RawTab.tsx
+++ products/error_tracking/frontend/components/ExceptionCard/Tabs/RawTab.tsx
@@ -8,7 +8,7 @@ export interface RawTabProps extends TabsPrimitiveContentProps {}
 export function RawTab(props: RawTabProps): JSX.Element {
     const { properties } = useValues(errorPropertiesLogic)
     return (
-        <TabsPrimitiveContent {...props}>
+        <TabsPrimitiveContent className="p-2" {...props}>
             <JSONViewer src={properties} name="event" collapsed={1} collapseStringsAfterLength={80} sortKeys />
         </TabsPrimitiveContent>
     )
