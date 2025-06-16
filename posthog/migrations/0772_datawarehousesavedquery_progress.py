160384845d456e8371f53f0e1ebadbabf4a478eb
Peter Hicks <peter.h@posthog.com>
2025-06-16 16:34:05 -0700

    Migration update

--- posthog/migrations/0772_datawarehousesavedquery_progress.py
+++ posthog/migrations/0772_datawarehousesavedquery_progress.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0764_teammarketinganalyticsconfig"),
+        ("posthog", "0771_teamrevenueanalyticsconfig_filter_test_accounts_and_more"),
     ]
 
     operations = [
