2f2cd7426f6c2d6ad845bec057abae4ff1164fcc
Phil Haack <haacked@gmail.com>
2025-06-17 09:08:09 -0700

    Add type annotation

--- frontend/src/scenes/feature-flags/featureFlagLogic.ts
+++ frontend/src/scenes/feature-flags/featureFlagLogic.ts
@@ -266,7 +266,7 @@ function cleanFlag(flag: Partial<FeatureFlagType>): Partial<FeatureFlagType> {
 // Strip out sort_key from groups before saving. The sort_key is here for React to be able to
 // render the release conditions in the correct order.
 function cleanFilterGroups(groups: FeatureFlagGroupType[]): FeatureFlagGroupType[] {
-    return groups.map(({ sort_key, ...rest }) => rest)
+    return groups.map(({ sort_key, ...rest }: FeatureFlagGroupType) => rest)
 }
 
 export const featureFlagLogic = kea<featureFlagLogicType>([
