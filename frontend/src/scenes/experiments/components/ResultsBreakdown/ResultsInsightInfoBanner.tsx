80cd2160e13ffbc11e2398aef494e242ae7a025a
Rodrigo Iloro <rodrigo@posthog.com>
2025-06-17 14:34:56 -0300

    fix: updating the insight exposure information banner.

--- frontend/src/scenes/experiments/components/ResultsBreakdown/ResultsInsightInfoBanner.tsx
+++ frontend/src/scenes/experiments/components/ResultsBreakdown/ResultsInsightInfoBanner.tsx
@@ -1,4 +1,4 @@
-import { LemonBanner, LemonTag, Link } from '@posthog/lemon-ui'
+import { LemonBanner, Link } from '@posthog/lemon-ui'
 
 import type { ResultBreakdownRenderProps } from './types'
 
@@ -15,11 +15,8 @@ export const ResultsInsightInfoBanner = ({ exposureDifference }: SafeResultsInsi
         <LemonBanner type="info" className="mb-4">
             <div className="items-center inline-flex flex-wrap">
                 <span>
-                    Insight results may be different from exposure results due to different data processing methods. For
-                    the experiment exposure, we only count events with a valid feature flag variant, and remove outliers
-                    or users with <LemonTag type="danger">$multiple</LemonTag> exposures that may skew the results,
-                    while insights count all the events. This could result in different breakdowns for the same
-                    experiment.&nbsp;
+                    Insight results may be slightly different from exposure results due to a difference in data
+                    processing methods. We're actively working on fixing this.&nbsp;
                     <Link
                         to="https://posthog.com/docs/experiments/common-questions"
                         className="font-semibold text-primary hover:text-primary-dark"
