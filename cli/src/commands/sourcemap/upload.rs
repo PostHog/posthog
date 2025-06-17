bf387cd5e0ac01d1430fab536c07651753f8d4d7
Hugues Pouillot <hugues@posthog.com>
2025-06-17 12:01:27 +0200

    feat(cli): delete sourcemaps after upload (#33758)

--- cli/src/commands/sourcemap/upload.rs
+++ cli/src/commands/sourcemap/upload.rs
@@ -15,6 +15,7 @@ pub fn upload(
     directory: &PathBuf,
     project: Option<String>,
     version: Option<String>,
+    delete_after: bool,
 ) -> Result<()> {
     let token = load_token().context("While starting upload command")?;
     let host = token.get_host(host.as_deref());
@@ -27,6 +28,10 @@ pub fn upload(
     );
 
     let pairs = read_pairs(directory)?;
+    let sourcemap_paths = pairs
+        .iter()
+        .map(|pair| pair.sourcemap.path.clone())
+        .collect::<Vec<_>>();
 
     let uploads = collect_uploads(pairs).context("While preparing files for upload")?;
     info!("Found {} chunks to upload", uploads.len());
@@ -47,6 +52,10 @@ pub fn upload(
     .context("While creating release")?;
 
     upload_chunks(&url, &token.token, uploads, release.as_ref())?;
+
+    if delete_after {
+        delete_files(sourcemap_paths).context("While deleting sourcemaps")?;
+    }
 
     let _ = capture_handle.join();
 
@@ -104,4 +113,17 @@ fn content_hash(uploads: &[ChunkUpload]) -> String {
         hasher.update(&upload.data);
     }
     format!("{:x}", hasher.finalize())
+}
+
+fn delete_files(paths: Vec<PathBuf>) -> Result<()> {
+    // Delete local sourcemaps files from the sourcepair
+    for path in paths {
+        if path.exists() {
+            std::fs::remove_file(&path).context(format!(
+                "Failed to delete sourcemaps file: {}",
+                path.display()
+            ))?;
+        }
+    }
+    Ok(())
 }
