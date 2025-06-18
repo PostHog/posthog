8935497b65d0aa1df6de3971f2925023f1431379
Robbie <robbie.coomber@gmail.com>
2025-06-18 14:30:46 +0100

    feat: Tag queries with service name (#33855)

--- posthog/clickhouse/test/test_query_tagging.py
+++ posthog/clickhouse/test/test_query_tagging.py
@@ -1,29 +1,30 @@
 from posthog.clickhouse.query_tagging import tag_queries, get_query_tags, tags_context, clear_tag, reset_query_tags
 
 
-expected_builtin_tags = {
+expected_constant_tags = {
     "git_commit": "test",
     "container_hostname": "test",
+    "service_name": "test",
 }
 
 
 def test_clear_tag():
     clear_tag("some")
     reset_query_tags()
-    assert get_query_tags() == {**expected_builtin_tags}
+    assert get_query_tags() == {**expected_constant_tags}
     tag_queries(another=True)
-    assert get_query_tags() == {"another": True, **expected_builtin_tags}
+    assert get_query_tags() == {"another": True, **expected_constant_tags}
     clear_tag("some")
-    assert get_query_tags() == {"another": True, **expected_builtin_tags}
+    assert get_query_tags() == {"another": True, **expected_constant_tags}
     clear_tag("another")
-    assert get_query_tags() == {**expected_builtin_tags}
+    assert get_query_tags() == {**expected_constant_tags}
 
 
 def test_tags_context():
     reset_query_tags()
     # Set initial tags
     tag_queries(initial="value")
-    assert get_query_tags() == {"initial": "value", **expected_builtin_tags}
+    assert get_query_tags() == {"initial": "value", **expected_constant_tags}
 
     # Modify tags within context
     with tags_context(in_context="true"):
@@ -32,7 +33,7 @@ def test_tags_context():
             "initial": "value",
             "test": "test_value",
             "in_context": "true",
-            **expected_builtin_tags,
+            **expected_constant_tags,
         }
 
         # Modify more
@@ -42,8 +43,8 @@ def test_tags_context():
             "test": "test_value",
             "another": "another_value",
             "in_context": "true",
-            **expected_builtin_tags,
+            **expected_constant_tags,
         }
 
     # Verify tags are restored
-    assert get_query_tags() == {"initial": "value", **expected_builtin_tags}
+    assert get_query_tags() == {"initial": "value", **expected_constant_tags}
