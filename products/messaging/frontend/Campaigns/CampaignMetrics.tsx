a473ead9e5cf048826da2b532da05d21c15d0b0e
Ben White <ben@posthog.com>
2025-06-19 11:07:19 +0200

    Added new metrics

--- products/messaging/frontend/Campaigns/CampaignMetrics.tsx
+++ products/messaging/frontend/Campaigns/CampaignMetrics.tsx
@@ -1,6 +1,78 @@
-import { CampaignLogicProps } from './campaignLogic'
+import { LemonSkeleton } from '@posthog/lemon-ui'
+import { BindLogic, useActions, useValues } from 'kea'
+import { Tooltip } from 'lib/lemon-ui/Tooltip'
+import { humanFriendlyNumber } from 'lib/utils'
+import { useEffect } from 'react'
 
-export function CampaignMetrics({ id }: CampaignLogicProps): JSX.Element {
+import { campaignMetricsLogic, CampaignMetricsLogicProps } from './campaignMetricsLogic'
+
+export function CampaignMetrics({ id }: CampaignMetricsLogicProps): JSX.Element {
     // Get the metrics based off of the campaign id
-    return <div>Metrics</div>
+    const { metricsByKind } = useValues(campaignMetricsLogic({ id }))
+    const { loadMetricsByKind } = useActions(campaignMetricsLogic({ id }))
+
+    useEffect(() => {
+        loadMetricsByKind()
+    }, [id])
+
+    console.log('metricsByKind', metricsByKind)
+
+    return (
+        <BindLogic logic={campaignMetricsLogic} props={{ id }}>
+            <div>
+                <h3>Workflow metrics</h3>
+                <CampaignMetricsTotals />
+            </div>
+        </BindLogic>
+    )
+}
+
+const METRICS_INFO = {
+    succeeded: 'Total number of events processed successfully',
+    failed: 'Total number of events that had errors during processing',
+    filtered: 'Total number of events that were filtered out',
+    filtering_failed: 'Total number of events that failed to be filtered',
+    disabled_temporarily:
+        'Total number of events that were skipped due to the destination being temporarily disabled (due to issues such as the destination being down or rate-limited)',
+    disabled_permanently:
+        'Total number of events that were skipped due to the destination being permanently disabled (due to prolonged issues with the destination)',
+}
+
+function CampaignMetric({
+    label,
+    value,
+    tooltip,
+}: {
+    label: string
+    value: number | undefined
+    tooltip: JSX.Element | string
+}): JSX.Element {
+    return (
+        <Tooltip title={tooltip}>
+            <div className="flex flex-col flex-1 gap-2 items-center p-2 rounded border bg-surface-primary">
+                <div className="text-xs font-bold uppercase">{label.replace(/_/g, ' ')}</div>
+                <div className="flex flex-1 items-center mb-2 text-2xl">{humanFriendlyNumber(value ?? 0)}</div>
+            </div>
+        </Tooltip>
+    )
+}
+
+function CampaignMetricsTotals(): JSX.Element {
+    const { metricsByKind, metricsByKindLoading } = useValues(campaignMetricsLogic)
+
+    return (
+        <div className="deprecated-space-y-4">
+            <div className="flex flex-wrap gap-2 items-center">
+                {Object.entries(METRICS_INFO).map(([key, value]) => (
+                    <div key={key} className="flex flex-col flex-1 h-30 min-w-30 max-w-100">
+                        {metricsByKindLoading ? (
+                            <LemonSkeleton className="w-full h-full" />
+                        ) : (
+                            <CampaignMetric label={key} value={metricsByKind?.[key]?.total} tooltip={value} />
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
