68b818852848508a9c4e140017883560b14fbe47
eric <eeoneric@gmail.com>
2025-06-19 12:09:21 -0400

    edit layout

--- frontend/src/scenes/data-warehouse/editor/sidebar/QueryVariables.tsx
+++ frontend/src/scenes/data-warehouse/editor/sidebar/QueryVariables.tsx
@@ -21,11 +21,9 @@ export function QueryVariables(): JSX.Element {
     const { openExistingVariableModal } = useActions(variableModalLogic)
 
     return (
-        <div className="flex flex-col gap-2 max-w-1/2" data-attr="sql-editor-sidebar-query-variables-pane">
-            <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-col gap-3 max-w-1/2" data-attr="sql-editor-sidebar-query-variables-pane">
+            <div className="flex flex-col items-start">
                 <h3 className="mb-0">Query variables</h3>
-            </div>
-            <div className="flex flex-row items-center justify-between">
                 <span className="text-xs">
                     Query variables let you dynamically set values in your SQL query.{' '}
                     <Link to={documentationUrl} target="_blank">
@@ -35,13 +33,10 @@ export function QueryVariables(): JSX.Element {
             </div>
             <div
                 className={clsx(
-                    'flex gap-4 flex-col',
+                    'flex gap-4 flex-col items-start',
                     variablesForInsight.length === 0 && 'h-full items-center justify-center'
                 )}
             >
-                {variablesForInsight.length === 0 && (
-                    <span className="text-xs text-muted-alt">No query variables found</span>
-                )}
                 {variablesForInsight.map((n) => (
                     <VariableComponent
                         key={n.id}
@@ -54,7 +49,9 @@ export function QueryVariables(): JSX.Element {
                     />
                 ))}
             </div>
-            <AddVariableButton buttonProps={{ type: 'primary', size: 'xsmall' }} title="Add variable" />
+            <div className="self-start">
+                <AddVariableButton buttonProps={{ type: 'primary', size: 'small' }} title="Add variable" />
+            </div>
             <NewVariableModal />
         </div>
     )
