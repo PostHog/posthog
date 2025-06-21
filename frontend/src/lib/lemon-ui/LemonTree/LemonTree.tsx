11b6155f1c9fc2c6dcb4b1861f7cb346e6537f44
Adam Leith <adamleithp@users.noreply.github.com>
2025-06-21 10:24:44 +0100

    conflicts

--- frontend/src/lib/lemon-ui/LemonTree/LemonTree.tsx
+++ frontend/src/lib/lemon-ui/LemonTree/LemonTree.tsx
@@ -340,7 +340,7 @@ const LemonTreeNode = forwardRef<HTMLDivElement, LemonTreeNodeProps>(
                             return null
                         }
                         return (
-                            <div key={item.id} className="not-first:mt-3 py-1 pl-[1.8rem] flex items-center">
+                            <div key={item.id} className="not-first:mt-3 py-1 pl-2 flex items-center">
                                 <span className="text-xs font-semibold text-tertiary uppercase">
                                     {item.displayName}
                                 </span>
