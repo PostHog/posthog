ac0d3faf0097b4e842e4af71255b52c779dd6526
Marius Andra <marius.andra@gmail.com>
2025-06-24 15:11:42 +0200

    bla bla bla

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
         actionTimeout: 0,
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.CI ? 'http://localhost:8000' : process.env.BASE_URL || 'http://localhost:8080',
+        baseURL: process.env.CI ? 'http://localhost:8010' : process.env.BASE_URL || 'http://localhost:8080',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
