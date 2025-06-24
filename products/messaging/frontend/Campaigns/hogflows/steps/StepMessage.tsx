79da7f5e5e0846ce1f71a0af7a7f09ca21f1453e
Ben White <ben@posthog.com>
2025-06-24 10:20:55 +0200

    fix

--- products/messaging/frontend/Campaigns/hogflows/steps/StepMessage.tsx
+++ products/messaging/frontend/Campaigns/hogflows/steps/StepMessage.tsx
@@ -1,9 +1,7 @@
 import { IconDecisionTree } from '@posthog/icons'
 import { Node } from '@xyflow/react'
-import { useActions } from 'kea'
 import { NEW_TEMPLATE } from 'products/messaging/frontend/TemplateLibrary/constants'
 
-import { hogFlowEditorLogic } from '../hogFlowEditorLogic'
 import { HogFlowAction } from '../types'
 import { StepView } from './components/StepView'
 import { HogFlowStep, HogFlowStepNodeProps } from './types'
@@ -33,22 +31,10 @@ function StepMessageNode({ data }: HogFlowStepNodeProps): JSX.Element {
     return <StepView name={data.name} icon={<IconDecisionTree className="text-green-400" />} selected={false} />
 }
 
-function StepMessageConfiguration({ node }: { node: Node<Extract<HogFlowAction, { type: 'message' }>> }): JSX.Element {
-    const action = node.data
-    const { message, channel } = action.config
-
-    const { setCampaignActionConfig } = useActions(hogFlowEditorLogic)
-
+function StepMessageConfiguration({}: { node: Node<Extract<HogFlowAction, { type: 'message' }>> }): JSX.Element {
     return (
         <>
-            <div className="flex flex-col">
-                <p className="mb-1 text-lg font-semibold">Message</p>
-                <p className="mb-0">Send a message to the user.</p>
-            </div>
-            <div className="flex flex-col">
-                <p className="mb-1 text-lg font-semibold">Channel</p>
-                <p className="mb-0">Choose which channel to send the message to.</p>
-            </div>
+            <p>Coming soon!</p>
         </>
     )
 }
