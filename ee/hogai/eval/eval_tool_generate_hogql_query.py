1c28b7d6bc90026fb552e2637069c9b193fe0f48
Michael Matloka <michael@matloka.com>
2025-06-26 15:12:20 +0200

    Fix evals

--- ee/hogai/eval/eval_tool_generate_hogql_query.py
+++ ee/hogai/eval/eval_tool_generate_hogql_query.py
@@ -38,10 +38,10 @@ def eval_tool_generate_hogql_query(call_generate_hogql_query):
                 input=(
                     # This erroneously says "logs are stored in a table named 'console_logs'". Whether it was
                     # the root node that pulled it out of its ass, or the user - we should handle this gracefully
-                    "Write an SQL query to find console logs where the level is error."
+                    "Write an SQL query to find console logs where the level is error. "
                     "Assume the logs are stored in a table named 'console_logs'."
                 ),
-                expected="SELECT * FROM console_logs_log_entries WHERE level = 'error'",
+                expected="SELECT * FROM log_entries WHERE level = 'error'",
             ),
         ],
     )
