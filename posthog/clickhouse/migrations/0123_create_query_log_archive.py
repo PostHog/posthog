77f08b0f86f11e9a6b58911943c312a775cac8d0
Pawel Szczur <pawel@posthog.com>
2025-06-27 00:46:00 +0200

    add query_log_archive ClickHouse table

--- posthog/clickhouse/migrations/0123_create_query_log_archive.py
+++ posthog/clickhouse/migrations/0123_create_query_log_archive.py
@@ -1,8 +1,13 @@
 from posthog.clickhouse.client.connection import NodeRole
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.clickhouse.query_log_archive import CREATE_QUERY_LOG_ARCHIVE_TABLE
+from posthog.clickhouse.query_log_archive import (
+    QUERY_LOG_ARCHIVE_TABLE_SQL,
+    DISTRIBUTED_QUERY_LOG_ARCHIVE_TABLE_SQL,
+    QUERY_LOG_ARCHIVE_MV,
+)
 
 operations = [
-    run_sql_with_exceptions(CREATE_QUERY_LOG_ARCHIVE_TABLE, node_role=NodeRole.ALL),
-    # run_sql_with_exceptions(CREATE_QUERY_LOG_ARCHIVE_TABLE_MV, node_role=NodeRole.ALL),
+    run_sql_with_exceptions(QUERY_LOG_ARCHIVE_TABLE_SQL(on_cluster=False), node_role=NodeRole.ALL),
+    run_sql_with_exceptions(DISTRIBUTED_QUERY_LOG_ARCHIVE_TABLE_SQL(on_cluster=False), node_role=NodeRole.ALL),
+    run_sql_with_exceptions(QUERY_LOG_ARCHIVE_MV(on_cluster=False), node_role=NodeRole.ALL),
 ]
