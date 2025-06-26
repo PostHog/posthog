6b4bbece05377d859bfd806727f11d8757730e9d
Ben White <ben@posthog.com>
2025-06-26 15:46:51 +0200

    Fixes

--- plugin-server/src/cdp/templates/_destinations/reddit_ads/reddit.template.test.ts
+++ plugin-server/src/cdp/templates/_destinations/reddit_ads/reddit.template.test.ts
@@ -10,11 +10,8 @@ describe('reddit template', () => {
 
     beforeEach(async () => {
         await tester.beforeEach()
-        jest.useFakeTimers().setSystemTime(DateTime.fromISO('2025-01-01T00:00:00Z').toJSDate())
-    })
-
-    afterEach(() => {
-        jest.useRealTimers()
+        const fixedTime = DateTime.fromISO('2025-01-01T00:00:00Z').toJSDate()
+        jest.spyOn(Date, 'now').mockReturnValue(fixedTime.getTime())
     })
 
     it('works with single product event', async () => {
