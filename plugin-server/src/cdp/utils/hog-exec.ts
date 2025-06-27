f1f68db89f4b31997e7c385d436b1b339096e083
Ben White <ben@posthog.com>
2025-06-27 14:33:11 +0200

    Fix up thread relief

--- plugin-server/src/cdp/utils/hog-exec.ts
+++ plugin-server/src/cdp/utils/hog-exec.ts
@@ -1,13 +1,50 @@
-import { exec, ExecOptions, ExecResult } from '@posthog/hogvm'
+import { DEFAULT_TIMEOUT_MS, exec, ExecOptions, ExecResult } from '@posthog/hogvm'
 import crypto from 'crypto'
+import { Counter } from 'prom-client'
 import RE2 from 're2'
 
 import { Semaphore } from './sempahore'
 
-export const DEFAULT_TIMEOUT_MS = 100
+export const MAX_THREAD_WAIT_TIME_MS = 200
+
+const hogExecThreadReliefCounter = new Counter({
+    name: 'cdp_hog_function_execution_thread_relief',
+    help: 'Whether the hog function execution was blocked by the thread relief',
+    // We have a timeout so we don't need to worry about much more than that
+    labelNames: ['waited'],
+})
 
 const semaphore = new Semaphore(1)
 
+let threadRelief: {
+    startedAt: number
+    promise: Promise<void>
+} | null = null
+
+const waitForThreadRelief = async (timeout: number = DEFAULT_TIMEOUT_MS): Promise<boolean> => {
+    if (!threadRelief) {
+        threadRelief = {
+            startedAt: performance.now(),
+            promise: new Promise((resolve) => {
+                setTimeout(() => {
+                    threadRelief = null
+                    resolve()
+                }, 0)
+            }),
+        }
+    }
+
+    if (performance.now() - threadRelief.startedAt < timeout) {
+        return false
+    }
+
+    await threadRelief.promise
+
+    return true
+}
+
+// NOTE: Hog execution can be expensive and in really bad cases can block the event loop for a long time.
+// To work around this we have a check when we run it to make sure that
 export async function execHog(
     bytecode: any,
     options?: ExecOptions
@@ -15,17 +52,24 @@ export async function execHog(
     execResult?: ExecResult
     error?: any
     durationMs: number
+    waitedForThreadRelief: boolean
 }> {
-    // Ensure we don't have more than one running in parallel
     return await semaphore.run(async () => {
-        // NOTE: The below is commented out whilst we expirment with it as it mostly tanks performance when enabled
-        // Note - the setTimeout here forces the event loop to run fully before the next call. This is important as we never want hog execution to block the event loop
-        // await new Promise((r) => setTimeout(r, 0))
-        return Promise.resolve(execHogImmediate(bytecode, options))
+        const waitedForInitialRelief = await waitForThreadRelief(options?.timeout)
+        const result = execHogImmediate(bytecode, options)
+        const waitedForFinalRelief = await waitForThreadRelief(options?.timeout)
+
+        const waitedForThreadRelief = waitedForInitialRelief || waitedForFinalRelief
+        hogExecThreadReliefCounter.inc({ waited: waitedForThreadRelief ? 'true' : 'false' })
+
+        return {
+            ...result,
+            waitedForThreadRelief,
+        }
     })
 }
 
-export function execHogImmediate(
+function execHogImmediate(
     bytecode: any,
     options?: ExecOptions
 ): {
