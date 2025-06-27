c4aeb8dc4b8030151c724d58d643ca55fb92378d
Phil Haack <haacked@gmail.com>
2025-06-27 09:25:55 -0700

    Add new types for flag dependencies
    Adds a PropertyFilterType of FlagDependency (`flag`) and a property type of `flag`

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -866,6 +866,8 @@ export enum PropertyFilterType {
     Element = 'element',
     /** Event property with "$feature/" prepended */
     Feature = 'feature',
+    /** Feature flag dependency (property type is 'flag') */
+    FlagDependency = 'flag',
     Session = 'session',
     Cohort = 'cohort',
     Recording = 'recording',
@@ -3561,6 +3563,7 @@ export enum PropertyType {
     Cohort = 'Cohort',
     Assignee = 'Assignee',
     StringArray = 'StringArray',
+    Flag = 'flag',
 }
 
 export enum PropertyDefinitionType {
