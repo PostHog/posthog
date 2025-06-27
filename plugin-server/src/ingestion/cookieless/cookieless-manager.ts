219c98eb8be703f0d05f80d5db5c5856ca1b82e5
Ben White <ben@posthog.com>
2025-06-27 11:50:48 +0200

    Fixes

--- plugin-server/src/ingestion/cookieless/cookieless-manager.ts
+++ plugin-server/src/ingestion/cookieless/cookieless-manager.ts
@@ -670,7 +670,7 @@ export function isCalendarDateValid(yyyymmdd: string): boolean {
     const utcDate = new Date(`${yyyymmdd}T00:00:00Z`)
 
     // Current time in UTC
-    const nowUTC = new Date(now())
+    const nowUTC = new Date(Date.now())
 
     // Define the range of the calendar day in UTC
     const startOfDayMinus12 = new Date(utcDate)
