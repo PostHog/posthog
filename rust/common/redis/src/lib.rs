36b78b86e9024c91e78ecec8da3d2c627e76519c
Andy Zhao <andy.z@posthog.com>
2025-06-26 22:42:23 -0700

    upgrade redis to 0.32.2 and use connection manager

--- rust/common/redis/src/lib.rs
+++ rust/common/redis/src/lib.rs
@@ -124,7 +124,7 @@ impl Client for RedisClient {
         min: String,
         max: String,
     ) -> Result<Vec<String>, CustomRedisError> {
-        let mut conn = self.client.get_async_connection().await?;
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
         let results = conn.zrangebyscore(k, min, max);
         let fut = timeout(Duration::from_millis(get_redis_timeout_ms()), results).await?;
         Ok(fut?)
@@ -136,7 +136,7 @@ impl Client for RedisClient {
         v: String,
         count: Option<i32>,
     ) -> Result<(), CustomRedisError> {
-        let mut conn = self.client.get_async_connection().await?;
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
         let count = count.unwrap_or(1);
         let results = conn.hincr(k, v, count);
         let fut = timeout(Duration::from_millis(get_redis_timeout_ms()), results).await?;
@@ -152,7 +152,7 @@ impl Client for RedisClient {
         k: String,
         format: RedisValueFormat,
     ) -> Result<String, CustomRedisError> {
-        let mut conn = self.client.get_async_connection().await?;
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
         let results = conn.get(k);
         let fut: Result<Vec<u8>, RedisError> =
             timeout(Duration::from_millis(get_redis_timeout_ms()), results).await?;
@@ -191,7 +191,7 @@ impl Client for RedisClient {
             RedisValueFormat::Pickle => serde_pickle::to_vec(&v, Default::default())?,
             RedisValueFormat::Utf8 => v.into_bytes(),
         };
-        let mut conn = self.client.get_async_connection().await?;
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
         let results = conn.set(k, bytes);
         let fut = timeout(Duration::from_millis(get_redis_timeout_ms()), results).await?;
         Ok(fut?)
@@ -218,7 +218,7 @@ impl Client for RedisClient {
             RedisValueFormat::Pickle => serde_pickle::to_vec(&v, Default::default())?,
             RedisValueFormat::Utf8 => v.into_bytes(),
         };
-        let mut conn = self.client.get_async_connection().await?;
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
         let seconds_usize = seconds as usize;
 
         // Use SET with both NX and EX options
@@ -242,14 +242,14 @@ impl Client for RedisClient {
     }
 
     async fn del(&self, k: String) -> Result<(), CustomRedisError> {
-        let mut conn = self.client.get_async_connection().await?;
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
         let results = conn.del(k);
         let fut = timeout(Duration::from_millis(get_redis_timeout_ms()), results).await?;
         fut.map_err(|e| CustomRedisError::Other(e.to_string()))
     }
 
     async fn hget(&self, k: String, field: String) -> Result<String, CustomRedisError> {
-        let mut conn = self.client.get_async_connection().await?;
+        let mut conn = self.client.get_multiplexed_async_connection().await?;
         let results = conn.hget(k, field);
         let fut: Result<Option<String>, RedisError> =
             timeout(Duration::from_millis(get_redis_timeout_ms()), results).await?;
