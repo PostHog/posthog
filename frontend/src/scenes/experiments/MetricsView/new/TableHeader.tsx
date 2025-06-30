6b539c9fad0ba3d6b22e119aed2942dc2ff3b1bb
Anders Asheim Hennum <git@andehen.net>
2025-06-30 14:54:14 +0200

    WIP smaller columns, wider chart

--- frontend/src/scenes/experiments/MetricsView/new/TableHeader.tsx
+++ frontend/src/scenes/experiments/MetricsView/new/TableHeader.tsx
@@ -2,19 +2,19 @@ export function TableHeader(): JSX.Element {
     return (
         <thead>
             <tr>
-                <th className="w-1/5 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
+                <th className="w-1/4 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
                     Metric
                 </th>
-                <th className="w-1/6 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
+                <th className="w-20 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
                     Baseline
                 </th>
-                <th className="w-1/6 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
+                <th className="w-16 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
                     Variant
                 </th>
-                <th className="w-1/6 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
+                <th className="w-20 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
                     Value
                 </th>
-                <th className="w-1/3 min-w-[300px] border-b-2 border-border bg-bg-table p-3 text-center text-xs font-semibold text-text-secondary sticky top-0 z-10">
+                <th className="min-w-[400px] border-b-2 border-border bg-bg-table p-3 text-center text-xs font-semibold text-text-secondary sticky top-0 z-10">
                     Chart
                 </th>
             </tr>
