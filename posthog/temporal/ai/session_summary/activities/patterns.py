14ef5fd2970d6a21d86e87f38add210d13af9a28
Alex Lebedev <alex.l@posthog.com>
2025-06-30 17:27:36 +0200

    feat: Separate pattern logic into activities.

--- posthog/temporal/ai/session_summary/activities/patterns.py
+++ posthog/temporal/ai/session_summary/activities/patterns.py
@@ -1,9 +1,14 @@
 import asyncio
+import dataclasses
 import json
+from typing import cast
 from redis import Redis
 import structlog
 import temporalio
-from ee.session_recordings.session_summary.llm.consume import get_llm_session_group_patterns_assignment, get_llm_session_group_patterns_extraction
+from ee.session_recordings.session_summary.llm.consume import (
+    get_llm_session_group_patterns_assignment,
+    get_llm_session_group_patterns_extraction,
+)
 from ee.session_recordings.session_summary.patterns.output_data import (
     RawSessionGroupPatternAssignmentsList,
     RawSessionGroupSummaryPatternsList,
@@ -24,11 +29,16 @@ from posthog.temporal.ai.session_summary.shared import SESSION_SUMMARIES_DB_DATA
 from posthog.temporal.ai.session_summary.state import (
     StateActivitiesEnum,
     generate_state_key,
-    get_data_str_from_redis
+    get_data_class_from_redis,
+    get_data_str_from_redis,
+)
+from posthog.temporal.ai.session_summary.summarize_session_group import (
+    _get_session_group_single_session_summaries_inputs_from_redis,
 )
 from posthog.temporal.ai.session_summary.types.group import SessionGroupSummaryOfSummariesInputs
 
 logger = structlog.get_logger(__name__)
+
 
 def get_session_ids_from_inputs(inputs: SessionGroupSummaryOfSummariesInputs) -> list[str]:
     return list(
@@ -37,7 +47,10 @@ def get_session_ids_from_inputs(inputs: SessionGroupSummaryOfSummariesInputs) ->
         )
     )
 
-def get_session_summaries_str_from_inputs(redis_client: Redis, inputs: SessionGroupSummaryOfSummariesInputs) -> list[str]:
+
+def get_session_summaries_str_from_inputs(
+    redis_client: Redis, inputs: SessionGroupSummaryOfSummariesInputs
+) -> list[str]:
     return [
         get_data_str_from_redis(
             redis_client=redis_client,
@@ -50,6 +63,7 @@ def get_session_summaries_str_from_inputs(redis_client: Redis, inputs: SessionGr
         )
         for single_session_input in inputs.single_session_summaries_inputs
     ]
+
 
 @temporalio.activity.defn
 async def extract_session_group_patterns_activity(inputs: SessionGroupSummaryOfSummariesInputs) -> None:
@@ -140,31 +154,44 @@ async def _generate_patterns_assignments(
 
     return patterns_assignments_list_of_lists
 
+
 @temporalio.activity.defn
-async def get_llm_session_group_summary_activity(inputs: SessionGroupSummaryOfSummariesInputs) -> str:
+async def assign_events_to_patterns_activity(inputs: SessionGroupSummaryOfSummariesInputs) -> None:
     """Summarize a group of sessions in one call"""
     redis_client = get_client()
     session_ids = get_session_ids_from_inputs(inputs)
-    # redis_output_key = generate_state_key(
-    #     key_base=inputs.redis_output_key_base,
-    #     label=StateActivitiesEnum.SESSION_GROUP_EXTRACTED_PATTERNS,
-    #     state_id=",".join(session_ids),
-    # )
     total_sessions_count = len(session_ids)
+    redis_input_key = generate_state_key(
+        key_base=inputs.redis_key_base,
+        label=StateActivitiesEnum.SESSION_GROUP_EXTRACTED_PATTERNS,
+        state_id=",".join(session_ids),
+    )
     # Get session summaries from Redis
     session_summaries_str = get_session_summaries_str_from_inputs(redis_client=redis_client, inputs=inputs)
-
+    # Remove excessive content (like UUIDs) from session summaries when using them as a context for group summaries (and not a final step)
+    intermediate_session_summaries_str = [
+        remove_excessive_content_from_session_summary_for_llm(session_summary_str)
+        for session_summary_str in session_summaries_str
+    ]
     # Convert session summaries strings to objects to extract event-related data
     session_summaries = [
         load_session_summary_from_string(session_summary_str) for session_summary_str in session_summaries_str
     ]
-
-
     # Split sessions summaries into chunks of 10 sessions each
     # TODO: Define in constants after testing optimal chunk size quality-wise
     session_summaries_chunks_str = [
         intermediate_session_summaries_str[i : i + 10] for i in range(0, len(intermediate_session_summaries_str), 10)
     ]
+    # Get extracted patterns from Redis
+    patterns_extraction = cast(
+        RawSessionGroupSummaryPatternsList,
+        get_data_class_from_redis(
+            redis_client=redis_client,
+            redis_key=redis_input_key,
+            label=StateActivitiesEnum.SESSION_GROUP_EXTRACTED_PATTERNS,
+            target_class=RawSessionGroupSummaryPatternsList,
+        ),
+    )
     patterns_assignments_list_of_lists = await _generate_patterns_assignments(
         patterns=patterns_extraction,
         session_summaries_chunks_str=session_summaries_chunks_str,
@@ -172,56 +199,53 @@ async def get_llm_session_group_summary_activity(inputs: SessionGroupSummaryOfSu
         session_ids=session_ids,
         extra_summary_context=inputs.extra_summary_context,
     )
-
-    # Single session summaries LLM inputs from Redis to be able to enrich the patterns collected
+    # Get single session summaries LLM inputs from Redis to be able to enrich the patterns collected
     single_session_summaries_llm_inputs = _get_session_group_single_session_summaries_inputs_from_redis(
         redis_client=redis_client,
         redis_input_keys=[
             generate_state_key(
-                key_base=inputs.redis_key_base,
+                key_base=single_session_input.redis_key_base,
                 label=StateActivitiesEnum.SESSION_SUMMARY,
                 state_id=single_session_input.session_id,
             )
             for single_session_input in inputs.single_session_summaries_inputs
         ],
     )
-
     # Combine event ids mappings from all the sessions to identify events and sessions assigned to patterns
     combined_event_ids_mappings = combine_event_ids_mappings_from_single_session_summaries(
         single_session_summaries_inputs=single_session_summaries_llm_inputs
     )
+    # TODO: Remove after testing
     with open("combined_event_ids_mappings.json", "w") as f:
         f.write(json.dumps(combined_event_ids_mappings))
-
     # Combine patterns assignments to have a single patter-to-event list
     combined_patterns_assignments = combine_patterns_assignments_from_single_session_summaries(
         patterns_assignments_list_of_lists=patterns_assignments_list_of_lists
     )
     with open("combined_patterns_assignments.json", "w") as f:
         f.write(json.dumps(combined_patterns_assignments))
-
     # Combine patterns ids with full event ids (from DB) and previous/next events in the segment per each assigned event
     pattern_id_to_event_context_mapping = combine_patterns_ids_with_events_context(
         combined_event_ids_mappings=combined_event_ids_mappings,
         combined_patterns_assignments=combined_patterns_assignments,
         session_summaries=session_summaries,
     )
+    # TODO: Remove after testing
     with open("pattern_event_ids_mapping.json", "w") as f:
         f.write(
             json.dumps(
                 {k: [dataclasses.asdict(dv) for dv in v] for k, v in pattern_id_to_event_context_mapping.items()}
             )
         )
-
     # Combine patterns info (name, description, etc.) with enriched events context
     patterns_with_events_context = combine_patterns_with_events_context(
         patterns=patterns_extraction,
         pattern_id_to_event_context_mapping=pattern_id_to_event_context_mapping,
         total_sessions_count=total_sessions_count,
     )
+    # TODO: Remove after testing
     with open("patterns_with_events_context.json", "w") as f:
         f.write(patterns_with_events_context.model_dump_json(exclude_none=True))
-
     # TODO: Enable after testing
     # summary_prompt = generate_session_group_summary_prompt(
     #     session_summaries=session_summaries, extra_summary_context=inputs.extra_summary_context
@@ -231,3 +255,4 @@ async def get_llm_session_group_summary_activity(inputs: SessionGroupSummaryOfSu
     #     prompt=summary_prompt, user_id=inputs.user_id, session_ids=inputs.session_ids
     # )
     # return summary_of_summaries
+    return None
