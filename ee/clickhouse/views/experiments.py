280fb1f64cbf915a3b44b8bb5e6dd0acb2681a2f
Shao Wei <lshaowei18@gmail.com>
2025-07-01 08:39:15 +0800

    fix: check that validated_data["parameters"] is not None

--- ee/clickhouse/views/experiments.py
+++ ee/clickhouse/views/experiments.py
@@ -234,8 +234,9 @@ class ExperimentSerializer(serializers.ModelSerializer):
         variants = []
         aggregation_group_type_index = None
         if "parameters" in validated_data:
-            variants = validated_data["parameters"].get("feature_flag_variants", [])
-            aggregation_group_type_index = validated_data["parameters"].get("aggregation_group_type_index")
+            if validated_data["parameters"] is not None:
+                variants = validated_data["parameters"].get("feature_flag_variants", [])
+                aggregation_group_type_index = validated_data["parameters"].get("aggregation_group_type_index")
 
         request = self.context["request"]
         validated_data["created_by"] = request.user
