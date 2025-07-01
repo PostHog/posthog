3a05ad51b2af33515a64a38fb5c0d7b4d81eb2d1
Yasen Slavov <yasen@posthog.com>
2025-07-01 21:39:13 +0300

    Test

---
+++ posthog/migrations/0783_test_migration_conflict.py
@@ -0,0 +1,14 @@
+# Test migration file to trigger conflict detection workflow
+# This is a temporary file for testing the new comment-based resolution system
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("posthog", "0782_previous_migration"),  # This will create a simulated conflict
+    ]
+
+    operations = [
+        # Empty migration for testing purposes
+    ]
