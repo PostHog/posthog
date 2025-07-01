74373722cfd9449117d41ef1d4ba750d3e034f1e
Yasen Slavov <yasen@posthog.com>
2025-07-01 17:46:26 +0300

    Fix comment

--- posthog/migrations/0784_final_test_after_billing_fix.py
+++
@@ -1,9 +0,0 @@
-# Test migration file
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("posthog", "0783_remove_segment_engage_destinations"),
-    ]
-    operations = []
--- posthog/migrations/0784_ultimate_workflow_test.py
+++
@@ -1,9 +0,0 @@
-# Ultimate test migration file
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("posthog", "0783_remove_segment_engage_destinations"),
-    ]
-    operations = []
