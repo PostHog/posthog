749a58c063c1dfe108ccf3d6d1e285dcbba95792
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-07-01 09:58:34 +0200

    hide amazon amc destination

--- plugin-server/src/cdp/segment/segment-templates.ts
+++ plugin-server/src/cdp/segment/segment-templates.ts
@@ -452,6 +452,7 @@ const HIDDEN_DESTINATIONS = [
     'segment-actions-apolloio',
     'segment-actions-toplyne-cloud',
     'segment-actions-heap-cloud',
+    'segment-actions-amazon-amc',
 
     // these destinations require a raw segment event (https://github.com/PostHog/posthog/pull/33451)
     'segment-actions-equals',
