e969a58767e76ed0d89241fb359dc1d93299ba33
Shao Wei <lshaowei18@gmail.com>
2025-07-01 14:12:18 +0800

    fix: addresses nits

--- frontend/src/lib/components/Onboarding/ReverseProxyModal/ReverseProxyModal.tsx
+++ frontend/src/lib/components/Onboarding/ReverseProxyModal/ReverseProxyModal.tsx
@@ -8,64 +8,50 @@ export function ReverseProxyModal(): JSX.Element {
     const { isOpen } = useValues(reverseProxyModalLogic)
     const { closeReverseProxyModal } = useActions(reverseProxyModalLogic)
     return (
-        <div>
-            <LemonModal
-                isOpen={isOpen}
-                onClose={() => {
-                    closeReverseProxyModal()
-                }}
-                width={800}
-                title={<>Setup Reverse Proxy</>}
-                description={
-                    <p>
-                        A reverse proxy enables you to send events to PostHog Cloud using your own domain. Using a
-                        reverse proxy means that events are less likely to be intercepted by tracking blockers.
-                    </p>
-                }
-                footer={
-                    <>
-                        <LemonButton
-                            onClick={() => {
-                                closeReverseProxyModal()
-                            }}
-                            type="secondary"
-                        >
-                            Close
-                        </LemonButton>
-                    </>
-                }
-            >
-                <h3>Deploying Your Own Reverse Proxy</h3>
-                <div className="flex flex-col p-2">
-                    <p className="font-normal">
-                        You can read more about how to setup and deploy your own reverse proxy{' '}
-                        <Link
-                            to="https://posthog.com/docs/advanced/proxy"
-                            target="_blank"
-                            targetBlankIcon
-                            disableDocsPanel
-                        >
-                            here
-                        </Link>
-                    </p>
-                </div>
-                <LemonDivider label="OR" />
-                <h3>Managed Reverse Proxy</h3>
-                <div className="flex flex-col p-2">
-                    <p className="font-normal">
-                        We also offer a managed reverse proxy service, simplifying the deployment and management of your
-                        PostHog.{' '}
-                        <Link
-                            to="https://posthog.com/docs/advanced/proxy/managed-reverse-proxy"
-                            target="_blank"
-                            targetBlankIcon
-                            disableDocsPanel
-                        >
-                            Read the docs here
-                        </Link>
-                    </p>
-                </div>
-            </LemonModal>
-        </div>
+        <LemonModal
+            isOpen={isOpen}
+            onClose={closeReverseProxyModal}
+            width={800}
+            title={<>Setup Reverse Proxy</>}
+            description={
+                <p>
+                    A reverse proxy enables you to send events to PostHog Cloud using your own domain. Using a reverse
+                    proxy means that events are less likely to be intercepted by tracking blockers.
+                </p>
+            }
+            footer={
+                <>
+                    <LemonButton onClick={closeReverseProxyModal} type="secondary">
+                        Close
+                    </LemonButton>
+                </>
+            }
+        >
+            <h3>Deploying Your Own Reverse Proxy</h3>
+            <div className="flex flex-col p-2">
+                <p className="font-normal">
+                    You can read more about how to setup and deploy your own reverse proxy{' '}
+                    <Link to="https://posthog.com/docs/advanced/proxy" target="_blank" targetBlankIcon disableDocsPanel>
+                        in our documentation.
+                    </Link>
+                </p>
+            </div>
+            <LemonDivider label="OR" />
+            <h3>Managed Reverse Proxy</h3>
+            <div className="flex flex-col p-2">
+                <p className="font-normal">
+                    We also offer a managed reverse proxy service, simplifying the deployment and management of your
+                    PostHog.{' '}
+                    <Link
+                        to="https://posthog.com/docs/advanced/proxy/managed-reverse-proxy"
+                        target="_blank"
+                        targetBlankIcon
+                        disableDocsPanel
+                    >
+                        Read the docs here.
+                    </Link>
+                </p>
+            </div>
+        </LemonModal>
     )
 }
