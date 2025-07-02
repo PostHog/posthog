b7864071d3f55ab77b0e37541af88f301c34d352
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2025-07-02 09:33:55 -0400

    start with logic file

---
+++ frontend/src/scenes/feature-flags/featureFlagConfirmationLogic.ts
@@ -0,0 +1,50 @@
+import { actions, kea, key, listeners, path, props, reducers, selectors } from 'kea'
+import { FeatureFlagType } from '~/types'
+import { openConfirmationModal } from './ConfirmationModal'
+
+export interface FeatureFlagConfirmationLogicProps {
+    featureFlag: FeatureFlagType
+    onConfirm: () => void
+}
+
+export const featureFlagConfirmationLogic = kea<any>([
+    path(['scenes', 'feature-flags', 'featureFlagConfirmationLogic']),
+    props({} as FeatureFlagConfirmationLogicProps),
+    key(({ featureFlag }) => featureFlag.id ?? 'new'),
+    actions(() => ({
+        setFlagChanges: true,
+        showConfirmationModal: true,
+        confirmChanges: true,
+    })),
+    reducers({
+        flagChanges: [
+            [] as string[],
+            {
+                setFlagChanges: (_, { changes }: { changes: string[] }) => changes,
+            },
+        ],
+        showSaveConfirmModal: [
+            false,
+            {
+                showConfirmationModal: (_, { show }: { show: boolean }) => show,
+            },
+        ],
+    }),
+    selectors({
+        hasChanges: [(s) => [s.flagChanges], (changes: string[]) => changes.length > 0],
+    }),
+    listeners(({ values, props }) => ({
+        confirmChanges: () => {
+            if (values.hasChanges) {
+                openConfirmationModal({
+                    featureFlag: props.featureFlag,
+                    type: 'multi-changes',
+                    changes: values.flagChanges,
+                    onConfirm: props.onConfirm,
+                })
+            } else {
+                props.onConfirm()
+            }
+        },
+    })),
+])
