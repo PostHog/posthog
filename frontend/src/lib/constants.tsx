dd4fd65ae84e94b4a03d4ea832f6f47f81143249
eric <eric@posthog.com>
2025-07-02 13:46:56 -0400

    add behind flag

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -289,6 +289,7 @@ export const FEATURE_FLAGS = {
     REPLAY_EXPORT_RAW_RECORDING: 'replay-export-raw-recording', // owner: @veryayskiy #team-replay
     GOOGLE_SHEETS_DWH: 'google-shets-dwh', // owner: @Gilbert09 #team-data-warehouse
     NEW_BAYESIAN_STATS_METHOD: 'new-bayesian-stats-method', // owner: @andehen #team-experiments
+    META_ADS_DWH: 'meta-ads-dwh', // owner: @EDsCODE #team-data-warehouse
 } as const
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS]
 
