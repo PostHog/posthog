e8ab7e81b3151036036c2b5cfc07f6a4cac265b3
Adam Leith <adamleithp@users.noreply.github.com>
2025-07-02 15:23:54 +0100

    renamed logic actions and values

--- frontend/src/layout/scenes/sceneLayoutLogic.tsx
+++ frontend/src/layout/scenes/sceneLayoutLogic.tsx
@@ -1,57 +1,37 @@
 import { actions, kea, path, reducers } from 'kea'
-import { AccessControlProps } from 'lib/components/AccessControlledLemonButton'
-import { ButtonPrimitiveProps } from 'lib/ui/Button/ButtonPrimitives'
 import type { sceneLayoutLogicType } from './sceneLayoutLogicType'
-export type SceneHeaderItemType = 'checkbox' | 'radio' | 'toggle' | 'separator' | 'link' | 'submenu'
-export type SceneHeaderItemProps = {
-    title: string
-    id: string
-    onClick?: () => void
-    to?: string
-    type?: SceneHeaderItemType
-    buttonProps?: ButtonPrimitiveProps
-    accessControl?: AccessControlProps
-}
-
-export type SceneHeaderChildItemProps = SceneHeaderItemProps & { icon: React.ReactNode }
 
 export const sceneLayoutLogic = kea<sceneLayoutLogicType>([
     path(['layout', 'scene-layout', 'sceneLayoutLogic']),
     actions({
-        setFileActionsContainer: (element: HTMLElement | null) => ({ element }),
-        setPanelInfoActive: (active: boolean) => ({ active }),
-        setPanelInfoOpen: (open: boolean) => ({ open }),
-        setShowPanelOverlay: (isOverlay: boolean) => ({ isOverlay }),
+        registerScenePanelElement: (element: HTMLElement | null) => ({ element }),
+        setScenePanelActive: (active: boolean) => ({ active }),
+        setScenePanelOpen: (open: boolean) => ({ open }),
+        setScenePanelIsOverlay: (isOverlay: boolean) => ({ isOverlay }),
     }),
     reducers({
-        fileActionsContainer: [
+        scenePanelElement: [
             null as HTMLElement | null,
             {
-                setFileActionsContainer: (_, { element }) => element,
+                registerScenePanelElement: (_, { element }) => element,
             },
         ],
-        fileActions: [
-            [] as SceneHeaderItemProps[],
-            {
-                setAddFileActions: (_, { actions }) => actions,
-            },
-        ],
-        panelInfoActive: [
+        scenePanelActive: [
             false,
             {
-                setPanelInfoActive: (_, { active }) => active,
+                setScenePanelActive: (_, { active }) => active,
             },
         ],
-        panelInfoOpen: [
+        scenePanelOpen: [
             false,
             {
-                setPanelInfoOpen: (_, { open }) => open,
+                setScenePanelOpen: (_, { open }) => open,
             },
         ],
-        showPanelOverlay: [
+        scenePanelIsOverlay: [
             true,
             {
-                setShowPanelOverlay: (_, { isOverlay }) => isOverlay,
+                setScenePanelIsOverlay: (_, { isOverlay }) => isOverlay,
             },
         ],
     }),
