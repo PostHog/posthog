4ed167be3c3a3aa340998434a07ea50cc1d25b96
Alex V <alexander.veryaysky@gmail.com>
2025-07-03 19:17:11 +0200

    fix

--- posthog/migrations/0785_migrate_playlist_types.py
+++ posthog/migrations/0785_migrate_playlist_types.py
@@ -1,9 +1,10 @@
 from django.db import migrations
-from posthog.session_recordings.models.session_recording_playlist import SessionRecordingPlaylist
-from posthog.session_recordings.models.session_recording_playlist_item import SessionRecordingPlaylistItem
 
 
 def migrate_playlist_types(apps, schema_editor):
+    SessionRecordingPlaylist = apps.get_model("posthog", "SessionRecordingPlaylist")
+    SessionRecordingPlaylistItem = apps.get_model("posthog", "SessionRecordingPlaylistItem")
+
     # 1. Get IDs of playlists that have playlist_items (should be COLLECTION)
     playlist_ids_with_items = (
         SessionRecordingPlaylistItem.objects.filter(playlist__type__isnull=True, playlist__deleted=False)
@@ -13,12 +14,12 @@ def migrate_playlist_types(apps, schema_editor):
 
     # 2. Bulk update playlists with items to COLLECTION type
     SessionRecordingPlaylist.objects.filter(id__in=playlist_ids_with_items, type__isnull=True, deleted=False).update(
-        type=SessionRecordingPlaylist.PlaylistType.COLLECTION
+        type="collection"  # Use string value instead of enum
     )
 
     # 3. Bulk update remaining playlists with null type to FILTERS type
     SessionRecordingPlaylist.objects.filter(type__isnull=True, deleted=False).update(
-        type=SessionRecordingPlaylist.PlaylistType.FILTERS
+        type="filters"  # Use string value instead of enum
     )
 
 
