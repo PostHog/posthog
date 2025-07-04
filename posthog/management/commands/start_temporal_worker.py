de50537b762ba6cefac2a47e47b5ac3d80351c1f
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-07-04 14:36:54 +0200

    refactor: Configure logging on worker start-up
    There shouldn't be any need to guess whether logging is configured and
    juggle that around through the lifetime of the worker. Just configure
    it once when we start and put it out of our way.
    
    This is only implemented for the async logging pipeline, and gated
    behind the `TEMPORAL_USE_ASYNC_EXTERNAL_LOGGER` variable. More work is
    required to support also the sync version (potentially, combining both).

--- posthog/management/commands/start_temporal_worker.py
+++ posthog/management/commands/start_temporal_worker.py
@@ -25,6 +25,7 @@ from posthog.constants import (
 )
 from posthog.temporal.ai import ACTIVITIES as AI_ACTIVITIES, WORKFLOWS as AI_WORKFLOWS
 from posthog.temporal.batch_exports import ACTIVITIES as BATCH_EXPORTS_ACTIVITIES, WORKFLOWS as BATCH_EXPORTS_WORKFLOWS
+from posthog.temporal.commmon.logger import configure_logger_async
 from posthog.temporal.common.worker import create_worker
 from posthog.temporal.data_imports.settings import ACTIVITIES as DATA_SYNC_ACTIVITIES, WORKFLOWS as DATA_SYNC_WORKFLOWS
 from posthog.temporal.data_modeling import ACTIVITIES as DATA_MODELING_ACTIVITIES, WORKFLOWS as DATA_MODELING_WORKFLOWS
@@ -179,6 +180,9 @@ class Command(BaseCommand):
         shutdown_task = None
 
         tag_queries(kind="temporal")
+
+        if settings.TEMPORAL_USE_EXTERNAL_LOGGER is True:
+            configure_logger_async()
 
         def shutdown_worker_on_signal(worker: Worker, sig: signal.Signals, loop: asyncio.events.AbstractEventLoop):
             """Shutdown Temporal worker on receiving signal."""
