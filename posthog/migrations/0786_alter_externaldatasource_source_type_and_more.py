4f41fe8f242daabd677bc5c6678de36a4e61a2eb
eric <eric@posthog.com>
2025-07-04 11:59:30 -0400

    fix migration

--- posthog/migrations/0786_alter_externaldatasource_source_type_and_more.py
+++ posthog/migrations/0786_alter_externaldatasource_source_type_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0781_alter_externaldatasource_source_type"),
+        ("posthog", "0785_team_drop_events_older_than_seconds"),
     ]
 
     operations = [
