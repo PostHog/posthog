7953d22f42dd1c02cf9f864d53456bfd0af0096e
edwinyjlim <edwinyjlim@gmail.com>
2025-07-07 09:03:11 -0400

    max cta and 6s default for images

--- frontend/src/layout/navigation/PosthogStories/storiesMap.ts
+++ frontend/src/layout/navigation/PosthogStories/storiesMap.ts
@@ -54,7 +54,7 @@ export const storiesMap: storyGroup[] = [
         title: 'Changelog',
         stories: [
             {
-                id: 'changelog_hog_templating',
+                id: 'changelog-hog-templating',
                 title: 'Changelog',
                 description: 'In-app Hog templating',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_phone_9f7523e1a8.png',
@@ -62,7 +62,7 @@ export const storiesMap: storyGroup[] = [
                 type: StoryType.Video,
             },
             {
-                id: 'changelog_linear_share_modal',
+                id: 'changelog-linear-share-modal',
                 title: 'Changelog',
                 description: 'Share session replays on Linear',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_phone_9f7523e1a8.png',
@@ -70,7 +70,7 @@ export const storiesMap: storyGroup[] = [
                 type: StoryType.Video,
             },
             {
-                id: 'changelog_save_filters_session_replay',
+                id: 'changelog-save-filters-session-replay',
                 title: 'Changelog',
                 description: 'Save filters for session replay',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_phone_9f7523e1a8.png',
@@ -79,7 +79,7 @@ export const storiesMap: storyGroup[] = [
                 type: StoryType.Video,
             },
             {
-                id: 'overlay_example',
+                id: 'changelog-cta',
                 title: 'Changelog',
                 description: 'New feature showcase with component overlay',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_phone_9f7523e1a8.png',
@@ -88,6 +88,7 @@ export const storiesMap: storyGroup[] = [
                 seeMoreLink: 'https://posthog.com/changelog',
                 seeMoreOptions: {
                     backgroundColor: 'black',
+                    text: 'See more updates',
                 },
             },
         ],
@@ -97,7 +98,7 @@ export const storiesMap: storyGroup[] = [
         title: 'Toolbar',
         stories: [
             {
-                id: 'toolbar_overview',
+                id: 'toolbar-overview',
                 title: 'Toolbar',
                 description: 'Overview of the toolbar',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
@@ -105,7 +106,7 @@ export const storiesMap: storyGroup[] = [
                 type: StoryType.Video,
             },
             {
-                id: 'toolbar_create_actions',
+                id: 'toolbar-create-actions',
                 title: 'Toolbar',
                 description: 'Create actions',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
@@ -113,65 +114,100 @@ export const storiesMap: storyGroup[] = [
                 type: StoryType.Video,
             },
             {
-                id: 'toolbar_inspect_1',
+                id: 'toolbar-inspect-image',
                 title: 'Toolbar',
                 description: 'Inspect elements',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
                 mediaUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/toolbar_inspect_a284324d14_24631f27fd.png',
                 type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/toolbar',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Launch the toolbar',
+                },
             },
             {
-                id: 'toolbar_heatmap_1',
+                id: 'toolbar-heatmap-image',
                 title: 'Toolbar',
                 description: 'View heatmaps',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
                 mediaUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/toolbar_heatmap_460f46f86b_04862b4e0a.png',
                 type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/toolbar',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Launch the toolbar',
+                },
             },
             {
-                id: 'toolbar_feature_flags_1',
+                id: 'toolbar-feature-flags-image',
                 title: 'Toolbar',
                 description: 'Flip feature flags',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
                 mediaUrl:
                     'https://res.cloudinary.com/dmukukwp6/image/upload/toolbar_feature_flags_31802301a3_41dacb9996.png',
                 type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/toolbar',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Launch the toolbar',
+                },
             },
             {
-                id: 'toolbar_events_1',
+                id: 'toolbar-events-image',
                 title: 'Toolbar',
                 description: 'Debug events',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
                 mediaUrl:
                     'https://res.cloudinary.com/dmukukwp6/image/upload/toolbar_debug_events_90dcef3c7b_b7564ce9af.png',
                 type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/toolbar',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Launch the toolbar',
+                },
             },
             {
-                id: 'toolbar_web_vitals_1',
+                id: 'toolbar-web-vitals-image',
                 title: 'Toolbar',
                 description: 'Check web vitals',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
                 mediaUrl:
                     'https://res.cloudinary.com/dmukukwp6/image/upload/toolbar_web_vitals_0150d8d8ca_6b9a790fb1.png',
                 type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/toolbar',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Launch the toolbar',
+                },
             },
             {
-                id: 'toolbar_experiments_1',
+                id: 'toolbar-experiments-image',
                 title: 'Toolbar',
                 description: 'Create experiments',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
                 mediaUrl:
                     'https://res.cloudinary.com/dmukukwp6/image/upload/toolbar_experiments_6745bae143_b82232edde.png',
                 type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/toolbar',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Launch the toolbar',
+                },
             },
             {
-                id: 'cool_features_1',
+                id: 'toolbar-cool-features-image',
                 title: 'Toolbar',
                 description: 'Check out hedgehog mode!',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/hoggie_builder_dc64451e64.png',
                 mediaUrl:
                     'https://res.cloudinary.com/dmukukwp6/image/upload/toolbar_cool_features_357aa7fc36_dc294f7fca.png',
                 type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/toolbar',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Launch the toolbar',
+                },
             },
         ],
     },
@@ -184,8 +220,21 @@ export const storiesMap: storyGroup[] = [
                 title: 'Max AI',
                 description: 'Say hi to Max!',
                 thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/max_ai_f8c9cdf4e8.png',
-                mediaUrl: 'https://res.cloudinary.com/dmukukwp6/video/upload/max_ai_overview_9e72208fda.mp4',
+                mediaUrl: 'https://res.cloudinary.com/dmukukwp6/video/upload/meet_max_ai_overview_778f4acffb.mp4',
                 type: StoryType.Video,
+            },
+            {
+                id: 'max-ai-cta',
+                title: 'Max AI',
+                description: 'Try talking to Max',
+                thumbnailUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/max_ai_f8c9cdf4e8.png',
+                mediaUrl: 'https://res.cloudinary.com/dmukukwp6/image/upload/max_cta_frame_06906e3804.png',
+                type: StoryType.Image,
+                seeMoreLink: 'https://app.posthog.com/#panel=max:hi%20max',
+                seeMoreOptions: {
+                    backgroundColor: 'black',
+                    text: 'Say hi',
+                },
             },
         ],
     },
