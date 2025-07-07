7f5c5d1915bae4907683feb57c4d2621952cfbdd
Rafa Audibert <rafael@posthog.com>
2025-07-07 14:54:00 -0300

    refactor: Remove Rev Analytics person mode hogql modifier
    These won't be used anymore. We've chosen a different approach where we'll use custom data warehouse joins to support this feature. This should not be deployed immediately because it'll invalidate all queries. We should wait and run this on a weekend.

--- posthog/schema.py
+++ posthog/schema.py
@@ -2017,12 +2017,6 @@ class RevenueAnalyticsOverviewItemKey(StrEnum):
     AVG_REVENUE_PER_CUSTOMER = "avg_revenue_per_customer"
 
 
-class RevenueAnalyticsPersonsJoinModeModifier(StrEnum):
-    ID = "id"
-    EMAIL = "email"
-    CUSTOM = "custom"
-
-
 class RevenueAnalyticsPropertyFilter(BaseModel):
     model_config = ConfigDict(
         extra="forbid",
@@ -3369,8 +3363,6 @@ class HogQLQueryModifiers(BaseModel):
     personsJoinMode: Optional[PersonsJoinMode] = None
     personsOnEventsMode: Optional[PersonsOnEventsMode] = None
     propertyGroupsMode: Optional[PropertyGroupsMode] = None
-    revenueAnalyticsPersonsJoinMode: Optional[RevenueAnalyticsPersonsJoinModeModifier] = None
-    revenueAnalyticsPersonsJoinModeCustom: Optional[str] = None
     s3TableUseInvalidColumns: Optional[bool] = None
     sessionTableVersion: Optional[SessionTableVersion] = None
     sessionsV2JoinMode: Optional[SessionsV2JoinMode] = None
