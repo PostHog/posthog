53b12da331051c2fb4933b855ea5cb29e02a69a0
Alex V <alexander.veryaysky@gmail.com>
2025-07-07 14:43:20 +0200

    chunking

--- posthog/migrations/0786_migrate_playlist_types.py
+++ posthog/migrations/0786_migrate_playlist_types.py
@@ -6,21 +6,27 @@ def migrate_playlist_types(apps, schema_editor):
     SessionRecordingPlaylistItem = apps.get_model("posthog", "SessionRecordingPlaylistItem")
 
     # 1. Get IDs of playlists that have playlist_items (should be COLLECTION)
-    playlist_ids_with_items = (
+    playlist_ids_with_items = set(
         SessionRecordingPlaylistItem.objects.filter(playlist__type__isnull=True, playlist__deleted=False)
         .values_list("playlist_id", flat=True)
         .distinct()
     )
 
-    # 2. Bulk update playlists with items to COLLECTION type
-    SessionRecordingPlaylist.objects.filter(id__in=playlist_ids_with_items, type__isnull=True, deleted=False).update(
-        type="collection"  # Use string value instead of enum
+    # 2. Update playlists with items to COLLECTION type using chunking
+    playlists_with_items = SessionRecordingPlaylist.objects.filter(
+        id__in=playlist_ids_with_items, type__isnull=True, deleted=False
     )
 
-    # 3. Bulk update remaining playlists with null type to FILTERS type
-    SessionRecordingPlaylist.objects.filter(type__isnull=True, deleted=False).update(
-        type="filters"  # Use string value instead of enum
-    )
+    for playlist in playlists_with_items.iterator(chunk_size=100):
+        playlist.type = "collection"
+        playlist.save(update_fields=["type"])
+
+    # 3. Update remaining playlists with null type to FILTERS type using chunking
+    remaining_playlists = SessionRecordingPlaylist.objects.filter(type__isnull=True, deleted=False)
+
+    for playlist in remaining_playlists.iterator(chunk_size=100):
+        playlist.type = "filters"
+        playlist.save(update_fields=["type"])
 
 
 class Migration(migrations.Migration):
