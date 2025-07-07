97040348d7467bd29c59bfc23753ac721c9173ca
Peter Hicks <peter.h@posthog.com>
2025-07-07 11:23:02 -0700

    Fixing migrations

--- posthog/migrations/0787_add_progress_tracking_to_datamodelingjob.py
+++ posthog/migrations/0787_add_progress_tracking_to_datamodelingjob.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0784_fix_null_event_triggers"),
+        ("posthog", "0786_add_is_emoji_to_annotation"),
     ]
 
     operations = [
