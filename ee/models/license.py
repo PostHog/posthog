fa30e6cea7226c4d607fb26c535c4d5a3af1bb6d
Zach Waterfield <zlwaterfield@gmail.com>
2025-07-08 10:04:58 -0400

    Update license.py

--- ee/models/license.py
+++ ee/models/license.py
@@ -27,7 +27,7 @@ class LicenseError(exceptions.APIException):
 class LicenseManager(models.Manager):
     def first_valid(self) -> Optional["License"]:
         """Return the highest valid license or cloud licenses if any"""
-        valid_licenses = list(self.filter(Q(valid_until__gte=timezone.now()) | Q(plan="cloud")))
+        valid_licenses = self.filter(Q(valid_until__gte=timezone.now()) | Q(plan="cloud"))
         if not valid_licenses:
             return None
         return valid_licenses.order_by("-valid_until").first()
