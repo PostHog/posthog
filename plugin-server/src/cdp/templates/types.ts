367b6693f0194af66e48257ccdd1c6288cca8e16
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-07-08 12:07:12 +0200

    fix

--- plugin-server/src/cdp/templates/types.ts
+++ plugin-server/src/cdp/templates/types.ts
@@ -75,5 +75,5 @@ export type NativeTemplate = Omit<HogFunctionTemplate, 'hog'> & {
             }
         ) => Promise<Response>,
         inputs: Record<string, any>
-    ) => Promise<void>
+    ) => Promise<any> | any
 }
