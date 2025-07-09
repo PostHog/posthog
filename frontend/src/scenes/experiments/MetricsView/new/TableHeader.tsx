d14ad7ee1763959fb847cf4775239163dd186532
Anders Asheim Hennum <git@andehen.net>
2025-07-09 09:37:02 +0200

    remove unnecessary border classes

--- frontend/src/scenes/experiments/MetricsView/new/TableHeader.tsx
+++ frontend/src/scenes/experiments/MetricsView/new/TableHeader.tsx
@@ -27,7 +27,7 @@ export function TableHeader({ results, chartRadius }: TableHeaderProps): JSX.Ele
     return (
         <thead>
             <tr>
-                <th className="w-1/5 border-b-2 border-r border-border bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
+                <th className="w-1/5 border-b-2  bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
                     Metric
                 </th>
                 <th className="w-1/20 border-b-2 bg-bg-table p-3 text-left text-xs font-semibold text-text-secondary sticky top-0 z-10">
