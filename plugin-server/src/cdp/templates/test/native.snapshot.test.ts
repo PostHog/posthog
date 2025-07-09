0386f96bda8e94d0ab4fcf2307344dbe9bb14dfa
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-07-09 16:24:15 +0200

    fix

--- plugin-server/src/cdp/templates/test/native.snapshot.test.ts
+++ plugin-server/src/cdp/templates/test/native.snapshot.test.ts
@@ -5,41 +5,41 @@ import { SAMPLE_GLOBALS } from '~/cdp/_tests/fixtures'
 import { NATIVE_HOG_FUNCTIONS } from '../index'
 import { DestinationTester, generateTestData } from './test-helpers'
 
-const template = NATIVE_HOG_FUNCTIONS[0]
+for (const template of NATIVE_HOG_FUNCTIONS) {
+    const testDestination = new DestinationTester(template)
 
-const testDestination = new DestinationTester(template)
-
-describe(`Testing snapshots for ${template.id} destination:`, () => {
-    beforeEach(() => {
-        const fixedTime = DateTime.fromObject({ year: 2025, month: 1, day: 1 }, { zone: 'UTC' })
-        jest.spyOn(Date, 'now').mockReturnValue(fixedTime.toMillis())
-    })
-
-    for (const mapping of template.mapping_templates) {
-        it.each(['required fields', 'all fields'])(`${mapping.name} mapping - %s`, async (required) => {
-            const seedName = `${template.id}#${mapping.name}`
-            const inputs = generateTestData(seedName, template.inputs_schema, required === 'required fields')
-            const mappingInputs = generateTestData(
-                seedName,
-                mapping.inputs_schema ?? [],
-                required === 'required fields'
-            )
-
-            const responses = await testDestination.invokeMapping(
-                mapping.name,
-                SAMPLE_GLOBALS,
-                { ...inputs, debug_mode: true },
-                mappingInputs
-            )
-
-            responses.logs.forEach((x) => {
-                if (typeof x.message === 'string' && x.message.includes('Function completed in')) {
-                    x.message = 'Function completed in [REPLACED]'
-                }
-            })
-            responses.invocation.id = 'invocation-id'
-
-            expect(responses).toMatchSnapshot()
+    describe(`Testing snapshots for ${template.id} destination:`, () => {
+        beforeEach(() => {
+            const fixedTime = DateTime.fromObject({ year: 2025, month: 1, day: 1 }, { zone: 'UTC' })
+            jest.spyOn(Date, 'now').mockReturnValue(fixedTime.toMillis())
         })
-    }
-})
+
+        for (const mapping of template.mapping_templates) {
+            it.each(['required fields', 'all fields'])(`${mapping.name} mapping - %s`, async (required) => {
+                const seedName = `${template.id}#${mapping.name}`
+                const inputs = generateTestData(seedName, template.inputs_schema, required === 'required fields')
+                const mappingInputs = generateTestData(
+                    seedName,
+                    mapping.inputs_schema ?? [],
+                    required === 'required fields'
+                )
+
+                const responses = await testDestination.invokeMapping(
+                    mapping.name,
+                    SAMPLE_GLOBALS,
+                    { ...inputs, debug_mode: true },
+                    mappingInputs
+                )
+
+                responses.logs.forEach((x) => {
+                    if (typeof x.message === 'string' && x.message.includes('Function completed in')) {
+                        x.message = 'Function completed in [REPLACED]'
+                    }
+                })
+                responses.invocation.id = 'invocation-id'
+
+                expect(responses).toMatchSnapshot()
+            })
+        }
+    })
+}
