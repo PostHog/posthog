854800cc94707056e41eec33967478b7aa09443f
denakorita <denada@posthog.com>
2025-07-10 11:11:18 +0200

    small change in the prompt, also resolve issue caused in the schema.py

--- posthog/schema.py
+++ posthog/schema.py
@@ -10592,14 +10592,7 @@ class MaxInnerUniversalFiltersGroup(BaseModel):
         extra="forbid",
     )
     type: FilterLogicalOperator
-    values: list[
-        Union[
-            EventPropertyFilter,
-            PersonPropertyFilter,
-            SessionPropertyFilter,
-            RecordingPropertyFilter,
-        ]
-    ]
+    values: list[Union[EventPropertyFilter, PersonPropertyFilter, SessionPropertyFilter, RecordingPropertyFilter]]
 
 
 class MaxOuterUniversalFiltersGroup(BaseModel):
