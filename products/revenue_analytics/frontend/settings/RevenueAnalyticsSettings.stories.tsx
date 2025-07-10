5a314d22fc13ab414dd94b5d000a31c2d9805ef2
Rafa Audibert <rafael@posthog.com>
2025-07-10 16:01:10 -0300

    feat: Add joins and source to rev analytics settings snapshot

--- products/revenue_analytics/frontend/settings/RevenueAnalyticsSettings.stories.tsx
+++ products/revenue_analytics/frontend/settings/RevenueAnalyticsSettings.stories.tsx
@@ -2,6 +2,10 @@ import { Meta, StoryObj } from '@storybook/react'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { App } from 'scenes/App'
 import { urls } from 'scenes/urls'
+import { mswDecorator } from '~/mocks/browser'
+import DatabaseSchemaQuery from '../__mocks__/DatabaseSchemaQuery.json'
+import externalDataSourceResponseMock from '~/mocks/fixtures/api/projects/team_id/external_data_sources/externalDataSource.json'
+import { EMPTY_PAGINATED_RESPONSE } from '~/mocks/handlers'
 
 const meta: Meta = {
     component: App,
@@ -12,6 +16,29 @@ const meta: Meta = {
         featureFlags: [FEATURE_FLAGS.REVENUE_ANALYTICS],
         pageUrl: urls.revenueSettings(),
     },
+    decorators: [
+        mswDecorator({
+            get: {
+                '/api/environments/:team_id/external_data_sources/': () => {
+                    return [
+                        200,
+                        {
+                            ...EMPTY_PAGINATED_RESPONSE,
+                            results: [
+                                externalDataSourceResponseMock,
+                                {
+                                    ...externalDataSourceResponseMock,
+                                    prefix: 'dev_',
+                                    revenue_analytics_enabled: false,
+                                },
+                            ],
+                        },
+                    ]
+                },
+            },
+            post: { '/api/environments/:team_id/query': () => [200, DatabaseSchemaQuery] },
+        }),
+    ],
 }
 export default meta
 
