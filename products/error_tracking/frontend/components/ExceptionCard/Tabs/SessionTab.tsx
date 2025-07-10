1a275ee29fed1cee66c5024b3222948f894de9e0
Hugues Pouillot <hpouillot@gmail.com>
2025-07-10 12:55:19 +0200

    add session recording disable check

--- products/error_tracking/frontend/components/ExceptionCard/Tabs/SessionTab.tsx
+++ products/error_tracking/frontend/components/ExceptionCard/Tabs/SessionTab.tsx
@@ -16,6 +16,8 @@ import {
 import { exceptionCardLogic } from '../exceptionCardLogic'
 import { EmptyMessage } from 'lib/components/EmptyMessage/EmptyMessage'
 import { match, P } from 'ts-pattern'
+import { teamLogic } from 'scenes/teamLogic'
+import { urls } from 'scenes/urls'
 
 export interface SessionTabProps extends TabsPrimitiveContentProps {
     timestamp?: string
@@ -24,13 +26,16 @@ export interface SessionTabProps extends TabsPrimitiveContentProps {
 export function SessionTab({ timestamp, ...props }: SessionTabProps): JSX.Element {
     const { loading } = useValues(exceptionCardLogic)
     const { sessionId } = useValues(errorPropertiesLogic)
+    const { currentTeam } = useValues(teamLogic)
+    const recordingsDisabled = currentTeam && !currentTeam?.session_recording_opt_in
 
     return (
         <TabsPrimitiveContent {...props}>
-            {match([loading, sessionId])
-                .with([true, P.any], () => <SessionTabContentLoading />)
-                .with([false, P.nullish], () => <SessionTabContentNoSession />)
-                .with([false, P.string], ([_, sessionId]) => (
+            {match([loading, recordingsDisabled, sessionId])
+                .with([true, P.any, P.any], () => <SessionTabContentLoading />)
+                .with([false, P.any, P.nullish], () => <SessionTabContentNoSession />)
+                .with([false, true, P.any], () => <SessionTabContentNoRecording />)
+                .with([false, false, P.string], ([, , sessionId]) => (
                     <SessionTabContent sessionId={sessionId} timestamp={timestamp} />
                 ))
                 .otherwise(() => null)}
@@ -46,14 +51,28 @@ export function SessionTabContentLoading(): JSX.Element {
     )
 }
 
+export function SessionTabContentNoRecording(): JSX.Element {
+    return (
+        <div className="flex justify-center w-full h-[300px] items-center">
+            <EmptyMessage
+                title="Session recording not enabled"
+                description="Enable session recordings to see what happened before the error occurred."
+                buttonText="Enable"
+                buttonTo={urls.replay()}
+                size="small"
+            />
+        </div>
+    )
+}
+
 export function SessionTabContentNoSession(): JSX.Element {
     return (
         <div className="flex justify-center w-full h-[300px] items-center">
             <EmptyMessage
                 title="No session available"
-                description="No session is associated with this exception"
-                buttonText="Check documentation"
-                buttonTo="https://posthog.com/docs/error-tracking/installation"
+                description="There is not $session_id associated with this exception."
+                buttonText="Check doc"
+                buttonTo="https://posthog.com/docs/data/sessions"
                 size="small"
             />
         </div>
