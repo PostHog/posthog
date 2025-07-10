2f2ac101b7d06ea2805dcebd9c6fcf8fca87038a
Michael Matloka <michael@matloka.com>
2025-07-10 13:39:23 +0200

    feat(max): Share convos by link

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5542,6 +5542,7 @@ export enum ConversationStatus {
 
 export interface Conversation {
     id: string
+    user: UserBasicType
     status: ConversationStatus
     title: string | null
     created_at: string | null
