008b498ee51e19e8b532bd82379bf430221b6a84
Adam Leith <adamleithp@users.noreply.github.com>
2025-07-10 12:33:54 +0100

    move feature previews from side panel to user/settings

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5020,7 +5020,6 @@ export enum SidePanelTab {
     Docs = 'docs',
     Activation = 'activation',
     Settings = 'settings',
-    FeaturePreviews = 'feature-previews',
     Activity = 'activity',
     Discussion = 'discussion',
     Status = 'status',
