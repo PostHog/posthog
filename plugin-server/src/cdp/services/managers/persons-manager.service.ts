1ef6e62bc889f52266b2e2e9810b772b63007d0d
Ben White <ben@posthog.com>
2025-07-11 11:22:11 +0200

    Fixes

--- plugin-server/src/cdp/services/managers/persons-manager.service.ts
+++ plugin-server/src/cdp/services/managers/persons-manager.service.ts
@@ -1,36 +1,74 @@
-import LRUCache from 'lru-cache'
+import { Hub, RawPerson } from '../../../types'
+import { PostgresUse } from '../../../utils/db/postgres'
+import { LazyLoader } from '../../../utils/lazy-loader'
+import { logger } from '../../../utils/logger'
+import { DBHogFunctionTemplate } from '../../types'
 
-import { CyclotronPerson } from '~/cdp/types'
+export type PersonGetArgs = {
+    teamId: number
+    distinctId: string
+}
 
-import { Hub } from '../../../types'
+const toKey = (args: PersonGetArgs) => `${args.teamId}:${args.distinctId}`
 
-const PERSONS_CACHE_AGE_MS = 60 * 1000 // 1 minute
-const PERSONS_CACHE_MAX_SIZE = 100_000 // 100k entries
+const fromKey = (key: string): PersonGetArgs => {
+    const [teamId, distinctId] = key.split(':')
+    return { teamId: parseInt(teamId), distinctId }
+}
 
-export class PersonsManagerService {
-    private personsCache: LRUCache<string, CyclotronPerson>
+export class PersonManagerService {
+    private lazyLoader: LazyLoader<DBHogFunctionTemplate>
 
     constructor(private hub: Hub) {
-        this.personsCache = new LRUCache({ max: PERSONS_CACHE_MAX_SIZE, maxAge: PERSONS_CACHE_AGE_MS })
+        this.lazyLoader = new LazyLoader({
+            name: 'person_manager',
+            loader: async (ids) => await this.fetchPersons(ids),
+        })
     }
 
-    public async getPerson(teamId: number, distinctId: string): Promise<CyclotronPerson> {
-        const cachedProperties = this.personsCache.get(distinctId)
-        if (cachedProperties) {
-            return cachedProperties
-        }
+    public async get(args: PersonGetArgs): Promise<DBHogFunctionTemplate | null> {
+        const key = toKey(args)
+        return (await this.lazyLoader.get(key)) ?? null
+    }
 
-        const rawPerson = await this.hub.db.fetchPerson(teamId, distinctId)
+    public async getMany(args: PersonGetArgs[]): Promise<Record<string, DBHogFunctionTemplate | null>> {
+        const keys = args.map(toKey)
+        return await this.lazyLoader.getMany(keys)
+    }
 
-        if (!rawPerson) {
-            throw new Error(`Person not found for team ${teamId} and distinctId ${distinctId}`)
-        }
+    // NOTE: Currently this essentially loads the "latest" template each time. We may need to swap this to using a specific version
+    private async fetchPersons(ids: string[]): Promise<Record<string, DBHogFunctionTemplate | undefined>> {
+        const teamPersons = ids.map(fromKey)
 
-        const person = {
-            id: rawPerson.id,
-            properties: rawPerson.properties ? { ...rawPerson.properties } : {},
-        }
-        this.personsCache.set(distinctId, person)
-        return person
+        logger.info('[PersonManager]', 'Fetching persons', { teamPersons })
+
+        const queryString = `SELECT
+                posthog_person.id,
+                posthog_person.uuid,
+                posthog_person.created_at,
+                posthog_person.team_id,
+                posthog_person.properties,
+                posthog_person.properties_last_updated_at,
+                posthog_person.properties_last_operation,
+                posthog_person.is_user_id,
+                posthog_person.version,
+                posthog_person.is_identified
+            FROM posthog_person
+            JOIN posthog_persondistinctid ON (posthog_persondistinctid.person_id = posthog_person.id)
+            WHERE
+                posthog_person.team_id = $1
+                AND posthog_persondistinctid.team_id = $1
+                AND posthog_persondistinctid.distinct_id = $2`
+
+        const response = await this.hub.postgres.query<RawPerson>(
+            PostgresUse.PERSONS_READ,
+            queryString,
+            [ids],
+            'fetchCDPPersons'
+        )
+
+        const personRows = response.rows
+
+        return {}
     }
 }
