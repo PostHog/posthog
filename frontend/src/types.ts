ac466f0cc914a4789a3b04c4a9dd0e1cffed878e
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2025-07-11 10:34:43 -0400

    feat: flag confirmation modal (#34473)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dylan Martin <dylan@posthog.com>

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -656,6 +656,8 @@ export interface TeamType extends TeamBasicType {
     product_intents?: ProductIntentType[]
     default_data_theme?: number
     flags_persistence_default: boolean
+    feature_flag_confirmation_enabled: boolean
+    feature_flag_confirmation_message: string
     marketing_analytics_config: MarketingAnalyticsConfig
     base_currency: CurrencyCode
 }
