4cd9d6022312092b539f67b32f411cdbb64657f0
Robbie Coomber <robbie.coomber@gmail.com>
2025-07-13 12:20:28 +0100

    Fix channel type property not working in activity view

--- posthog/hogql/test/test_property.py
+++ posthog/hogql/test/test_property.py
@@ -13,6 +13,7 @@ from posthog.hogql.property import (
     selector_to_expr,
     tag_name_to_expr,
     entity_to_expr,
+    map_virtual_properties,
 )
 from posthog.hogql.visitor import clear_locations
 from posthog.models import (
@@ -944,3 +945,17 @@ class TestProperty(BaseTest):
         assert self._property_to_expr(
             {"type": "person", "key": "$virt_initial_channel_type", "value": "Organic Search"}, scope="event"
         ) == self._parse_expr("person.$virt_initial_channel_type = 'Organic Search'")
+
+    def test_map_virtual_properties(self):
+        assert map_virtual_properties(
+            ast.Field(chain=["person", "properties", "$virt_initial_channel_type"])
+        ) == ast.Field(chain=["person", "$virt_initial_channel_type"])
+        assert map_virtual_properties(ast.Field(chain=["person", "properties", "other property"])) == ast.Field(
+            chain=["person", "properties", "other property"]
+        )
+        assert map_virtual_properties(ast.Field(chain=["properties", "$virt_initial_channel_type"])) == ast.Field(
+            chain=["$virt_initial_channel_type"]
+        )
+        assert map_virtual_properties(ast.Field(chain=["properties", "other property"])) == ast.Field(
+            chain=["properties", "other property"]
+        )
