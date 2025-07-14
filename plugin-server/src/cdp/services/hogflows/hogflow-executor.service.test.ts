e0a5b8302ef546e9266e96eec724674d8a500c63
Ben White <ben@posthog.com>
2025-07-14 13:29:51 +0200

    Fix

--- plugin-server/src/cdp/services/hogflows/hogflow-executor.service.test.ts
+++ plugin-server/src/cdp/services/hogflows/hogflow-executor.service.test.ts
@@ -180,6 +180,15 @@ describe('Hogflow Executor', () => {
                     id: expect.any(String),
                     teamId: 1,
                     hogFlow: invocation.hogFlow,
+                    person: {
+                        id: 'person_id',
+                        name: '',
+                        properties: {
+                            name: 'John Doe',
+                        },
+                        url: '',
+                    },
+                    filterGlobals: expect.any(Object),
                     functionId: invocation.hogFlow.id,
                     queue: 'hogflow',
                     queueMetadata: undefined,
@@ -187,7 +196,6 @@ describe('Hogflow Executor', () => {
                     queueSource: undefined,
                     queueParameters: undefined,
                     queuePriority: 0,
-                    getPerson: expect.any(Function),
                 },
                 finished: true,
                 logs: [
