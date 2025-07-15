7361ae9f8c7c11533df2f0ce4c251973f09db48c
Ben White <ben@posthog.com>
2025-07-15 10:04:59 +0200

    Started adding input service

---
+++ plugin-server/src/cdp/services/hog-inputs.service.ts
@@ -0,0 +1,123 @@
+import { convertHogToJS } from '@posthog/hogvm'
+
+import { HogFunctionInvocationGlobals, HogFunctionInvocationGlobalsWithInputs, HogFunctionType } from '../types'
+import { execHog } from '../utils/hog-exec'
+import { LiquidRenderer } from '../utils/liquid'
+import { IntegrationManagerService } from './managers/integration-manager.service'
+
+export const EXTEND_OBJECT_KEY = '$$_extend_object'
+
+export class HogInputsService {
+    constructor(private integrationManager: IntegrationManagerService) {}
+}
+
+export const formatHogInput = async (
+    bytecode: any,
+    globals: HogFunctionInvocationGlobalsWithInputs,
+    key?: string
+): Promise<any> => {
+    // Similar to how we generate the bytecode by iterating over the values,
+    // here we iterate over the object and replace the bytecode with the actual values
+    // bytecode is indicated as an array beginning with ["_H"] (versions 1+) or ["_h"] (version 0)
+
+    if (bytecode === null || bytecode === undefined) {
+        return bytecode // Preserve null and undefined values
+    }
+
+    if (Array.isArray(bytecode) && (bytecode[0] === '_h' || bytecode[0] === '_H')) {
+        const { execResult: result, error } = await execHog(bytecode, { globals })
+        if (!result || error) {
+            throw error ?? result?.error
+        }
+        if (!result?.finished) {
+            // NOT ALLOWED
+            throw new Error(`Could not execute bytecode for input field: ${key}`)
+        }
+        return convertHogToJS(result.result)
+    }
+
+    if (Array.isArray(bytecode)) {
+        return await Promise.all(bytecode.map((item) => formatHogInput(item, globals, key)))
+    } else if (typeof bytecode === 'object' && bytecode !== null) {
+        let ret: Record<string, any> = {}
+
+        if (bytecode[EXTEND_OBJECT_KEY]) {
+            const res = await formatHogInput(bytecode[EXTEND_OBJECT_KEY], globals, key)
+            if (res && typeof res === 'object') {
+                ret = {
+                    ...res,
+                }
+            }
+        }
+
+        await Promise.all(
+            Object.entries(bytecode).map(async ([subkey, value]) => {
+                if (subkey === EXTEND_OBJECT_KEY) {
+                    return
+                }
+                ret[subkey] = await formatHogInput(value, globals, key ? `${key}.${subkey}` : subkey)
+            })
+        )
+
+        return ret
+    }
+
+    return bytecode
+}
+
+const formatLiquidInput = (value: unknown, globals: HogFunctionInvocationGlobalsWithInputs, key?: string): any => {
+    if (value === null || value === undefined) {
+        return value
+    }
+
+    if (typeof value === 'string') {
+        return LiquidRenderer.renderWithHogFunctionGlobals(value, globals)
+    }
+
+    if (Array.isArray(value)) {
+        return value.map((item) => formatLiquidInput(item, globals, key))
+    }
+
+    if (typeof value === 'object' && value !== null) {
+        return Object.fromEntries(
+            Object.entries(value).map(([key2, value]) => [
+                key2,
+                formatLiquidInput(value, globals, key ? `${key}.${key2}` : key2),
+            ])
+        )
+    }
+
+    return value
+}
+
+export const buildGlobalsWithInputs = async (
+    globals: HogFunctionInvocationGlobals,
+    inputs: HogFunctionType['inputs']
+): Promise<HogFunctionInvocationGlobalsWithInputs> => {
+    const newGlobals: HogFunctionInvocationGlobalsWithInputs = {
+        ...globals,
+        inputs: {},
+    }
+
+    const orderedInputs = Object.entries(inputs ?? {}).sort(([_, input1], [__, input2]) => {
+        return (input1?.order ?? -1) - (input2?.order ?? -1)
+    })
+
+    for (const [key, input] of orderedInputs) {
+        if (!input) {
+            continue
+        }
+
+        newGlobals.inputs[key] = input.value
+
+        const templating = input.templating ?? 'hog'
+
+        if (templating === 'liquid') {
+            newGlobals.inputs[key] = formatLiquidInput(input.value, newGlobals, key)
+        } else if (templating === 'hog' && input?.bytecode) {
+            newGlobals.inputs[key] = await formatHogInput(input.bytecode, newGlobals, key)
+        }
+    }
+
+    return newGlobals
+}
