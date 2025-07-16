0ec873baa0b5adba141d9999202a6679b757181f
Rafa Audibert <rafael@posthog.com>
2025-07-16 17:54:19 -0300

    refactor: Make 2xn grid
    Since we have an even number of charts, let's always use 2 columns

--- products/revenue_analytics/frontend/RevenueAnalyticsScene.tsx
+++ products/revenue_analytics/frontend/RevenueAnalyticsScene.tsx
@@ -191,7 +191,7 @@ const RevenueAnalyticsTables = (): JSX.Element => {
         <div className="flex flex-col gap-4 mt-4">
             <OverviewTile />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <RevenueTile />
                 <CustomerCountTile />
                 <RevenueGrowthRateTile />
