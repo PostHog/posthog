02c679d263e4aac5d69e4ee2ea653f4213fcb113
Haven <haven@posthog.com>
2025-07-17 11:31:21 -0500

    fix migration

--- posthog/migrations/0798_alter_integration_kind.py
+++ posthog/migrations/0798_alter_integration_kind.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0795_add_progress_tracking_to_datamodelingjob"),
+        ("posthog", "0797_batchexportrun_bytes_exported"),
     ]
 
     operations = [
