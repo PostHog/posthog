dc63ddf37f49747063941acc8a9a5c40fa16a72f
Alexander Spicer <aspicer@gmail.com>
2025-07-17 16:11:59 -0700

    readd comments

--- frontend/src/scenes/dashboard/DashboardItems.tsx
+++ frontend/src/scenes/dashboard/DashboardItems.tsx
@@ -162,10 +162,12 @@ export function DashboardItems(): JSX.Element {
                                     placement={placement}
                                     loadPriority={smLayout ? smLayout.y * 1000 + smLayout.x : undefined}
                                     variablesOverride={temporaryVariables}
+                                    // :HACKY: The two props below aren't actually used in the component, but are needed to trigger a re-render
                                     breakdownColorOverride={temporaryBreakdownColors}
                                     dataColorThemeId={dataColorThemeId}
                                     noCache={noCache}
                                     {...commonTileProps}
+                                    // NOTE: ReactGridLayout additionally injects its resize handles as `children`!
                                 />
                             )
                         }
