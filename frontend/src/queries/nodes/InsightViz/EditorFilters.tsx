a58a1b1704c2378b4987d7d90ed6d4277e2cd848
denakorita <denada@posthog.com>
2025-07-20 15:57:55 +0200

    remove unused div

--- frontend/src/queries/nodes/InsightViz/EditorFilters.tsx
+++ frontend/src/queries/nodes/InsightViz/EditorFilters.tsx
@@ -448,25 +448,23 @@ export function EditorFilters({ query, showing, embedded }: EditorFiltersProps):
                         className="EditorFiltersWrapper"
                         active={maxToolActive}
                     >
-                        <div>
-                            <div
-                                className={clsx('flex flex-row flex-wrap gap-8 bg-surface-primary', {
-                                    'p-4 rounded border': !embedded,
-                                })}
-                            >
-                                {filterGroupsGroups.map(({ title, editorFilterGroups }) => (
-                                    <div key={title} className="flex-1 flex flex-col gap-4 max-w-full">
-                                        {editorFilterGroups.map((editorFilterGroup) => (
-                                            <EditorFilterGroup
-                                                key={editorFilterGroup.title}
-                                                editorFilterGroup={editorFilterGroup}
-                                                insightProps={insightProps}
-                                                query={query}
-                                            />
-                                        ))}
-                                    </div>
-                                ))}
-                            </div>
+                        <div
+                            className={clsx('flex flex-row flex-wrap gap-8 bg-surface-primary', {
+                                'p-4 rounded border': !embedded,
+                            })}
+                        >
+                            {filterGroupsGroups.map(({ title, editorFilterGroups }) => (
+                                <div key={title} className="flex-1 flex flex-col gap-4 max-w-full">
+                                    {editorFilterGroups.map((editorFilterGroup) => (
+                                        <EditorFilterGroup
+                                            key={editorFilterGroup.title}
+                                            editorFilterGroup={editorFilterGroup}
+                                            insightProps={insightProps}
+                                            query={query}
+                                        />
+                                    ))}
+                                </div>
+                            ))}
                         </div>
                     </MaxTool>
 
