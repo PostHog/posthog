f4fd8a823252fd72c7b8b8e249fd0361a1c8fd69
Eli Reisman <eli.r@posthog.com>
2025-07-21 13:18:37 -0700

    chore(capture-rs): remove bug detection observability sampler (#35392)

--- rust/capture/src/config.rs
+++ rust/capture/src/config.rs
@@ -93,9 +93,8 @@ pub struct Config {
     #[envconfig(default = "info")]
     pub log_level: Level,
 
-    // temporary: gates some chatty debug logging
     #[envconfig(default = "0.0")]
-    pub base64_detect_percent: f32,
+    pub verbose_sample_percent: f32,
 }
 
 #[derive(Envconfig, Clone)]
