3e51492478806bc35eefe9e7b3fd443de7a66d22
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-07-22 10:39:58 +0200

    feat: Update metric prefix based on task queue

--- posthog/temporal/common/worker.py
+++ posthog/temporal/common/worker.py
@@ -26,6 +26,7 @@ async def create_worker(
     graceful_shutdown_timeout: dt.timedelta | None = None,
     max_concurrent_workflow_tasks: int | None = None,
     max_concurrent_activities: int | None = None,
+    metric_prefix: str | None = None,
 ) -> Worker:
     """Connect to Temporal server and return a Worker.
 
@@ -47,9 +48,18 @@ async def create_worker(
             the worker can handle. Defaults to 50.
         max_concurrent_activities: Maximum number of concurrent activity tasks the
             worker can handle. Defaults to 50.
+        metric_prefix: Prefix to apply to metrics emitted by this worker, if
+            left unset (`None`) Temporal will default to "temporal_".
     """
 
-    runtime = Runtime(telemetry=TelemetryConfig(metrics=PrometheusConfig(bind_address=f"0.0.0.0:{metrics_port:d}")))
+    runtime = Runtime(
+        telemetry=TelemetryConfig(
+            metric_prefix=metric_prefix,
+            metrics=PrometheusConfig(
+                bind_address=f"0.0.0.0:{metrics_port:d}",
+            ),
+        )
+    )
     client = await connect(
         host,
         port,
