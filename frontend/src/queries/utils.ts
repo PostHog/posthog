8f4811d217a86b1dd9bdbb62a61458b2e9d3808f
Marius Andra <marius.andra@gmail.com>
2025-07-22 10:19:41 +0200

    fix to hogql require

--- frontend/src/queries/utils.ts
+++ frontend/src/queries/utils.ts
@@ -63,6 +63,7 @@ import {
 import { BaseMathType, ChartDisplayType, IntervalType } from '~/types'
 
 import { LATEST_VERSIONS } from './latest-versions'
+import { getAppContext } from 'lib/utils/getAppContext'
 
 export function isDataNode(node?: Record<string, any> | null): node is EventsQuery | PersonsNode {
     return (
@@ -633,13 +634,11 @@ function isHogQLRaw(value: any): value is HogQLRaw {
 }
 
 function formatHogQLValue(value: any): string {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { teamLogic } = require('scenes/teamLogic')
-
     if (Array.isArray(value)) {
         return `[${value.map(formatHogQLValue).join(', ')}]`
     } else if (dayjs.isDayjs(value)) {
-        return value.tz(teamLogic.values.timezone).format("'YYYY-MM-DD HH:mm:ss'")
+        const timezone = getAppContext()?.current_team?.timezone || 'UTC'
+        return value.tz(timezone).format("'YYYY-MM-DD HH:mm:ss'")
     } else if (isHogQLIdentifier(value)) {
         return escapePropertyAsHogQLIdentifier(value.identifier)
     } else if (isHogQLRaw(value)) {
