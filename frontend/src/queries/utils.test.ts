b5294f6203dd9d726403223bdc4efd5bf606a849
Marius Andra <marius.andra@gmail.com>
2025-07-23 13:26:00 +0200

    fix timezone in test

--- frontend/src/queries/utils.test.ts
+++ frontend/src/queries/utils.test.ts
@@ -6,6 +6,7 @@ import { initKeaTests } from '~/test/init'
 import { AppContext, TeamType } from '~/types'
 
 import { hogql } from './utils'
+import { getAppContext } from 'lib/utils/getAppContext'
 
 window.POSTHOG_APP_CONTEXT = { current_team: { id: MOCK_TEAM_ID } } as unknown as AppContext
 
@@ -47,9 +48,17 @@ describe('hogql tag', () => {
     })
 
     it('properly returns query with date substitution in non-UTC', () => {
+        const context = getAppContext()
+        let oldTimezone = context?.current_team?.timezone || 'UTC'
+        if (context?.current_team) {
+            context.current_team.timezone = 'Europe/Moscow'
+        }
         teamLogic.actions.loadCurrentTeamSuccess({ id: MOCK_TEAM_ID, timezone: 'Europe/Moscow' } as TeamType)
         expect(hogql`SELECT * FROM events WHERE timestamp > ${dayjs('2023-04-04T04:04:00Z')}`).toEqual(
             "SELECT * FROM events WHERE timestamp > '2023-04-04 07:04:00'" // Offset by 3 hours
         )
+        if (context?.current_team) {
+            context.current_team.timezone = oldTimezone
+        }
     })
 })
