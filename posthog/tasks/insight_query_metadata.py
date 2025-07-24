0a8bf5d805a3ad0065d131d3ba840d9ab98147ec
Nima <73110514+nima-karimi@users.noreply.github.com>
2025-07-24 14:34:00 -0700

    feat(insight): update extract_insight_query_metadata task to use LONG_RUNNING queue

--- posthog/tasks/insight_query_metadata.py
+++ posthog/tasks/insight_query_metadata.py
@@ -2,11 +2,12 @@ import structlog
 from celery import shared_task
 
 from posthog.models import Insight
+from posthog.tasks.utils import CeleryQueue
 
 logger = structlog.get_logger(__name__)
 
 
-@shared_task(ignore_result=True)
+@shared_task(ignore_result=True, queue=CeleryQueue.LONG_RUNNING.value, max_retries=1)
 def extract_insight_query_metadata(insight_id: str) -> None:
     insight = Insight.objects.get(pk=insight_id)
     insight.generate_query_metadata()
