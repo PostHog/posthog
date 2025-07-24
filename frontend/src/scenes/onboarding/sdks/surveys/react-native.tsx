5423a00b507393f650e28f2e732612b42d26877b
Ioannis J <yiannis@posthog.com>
2025-07-24 16:08:57 +0300

    fix: format

--- frontend/src/scenes/onboarding/sdks/surveys/react-native.tsx
+++ frontend/src/scenes/onboarding/sdks/surveys/react-native.tsx
@@ -1,7 +1,5 @@
 import { SDKInstallRNInstructions } from '../sdk-install-instructions'
 
 export function RNInstructions(): JSX.Element {
-    return (
-        <SDKInstallRNInstructions includeSurveys={true} />
-    )
+    return <SDKInstallRNInstructions includeSurveys={true} />
 }
