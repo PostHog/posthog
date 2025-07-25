7e4fac5f011842872da370c56a8b21401d3fd03a
Rodrigo Iloro <rodrigo@posthog.com>
2025-07-25 19:38:07 -0300

    feat: added support for multiple changes on a single activity log.

--- frontend/src/scenes/experiments/experimentActivityDescriber.tsx
+++ frontend/src/scenes/experiments/experimentActivityDescriber.tsx
@@ -1,12 +1,23 @@
-import { ActivityLogItem, HumanizedChange, userNameForLogItem } from 'lib/components/ActivityLog/humanizeActivity'
+import clsx from 'clsx'
+import {
+    ActivityChange,
+    ActivityLogItem,
+    HumanizedChange,
+    userNameForLogItem,
+} from 'lib/components/ActivityLog/humanizeActivity'
 import { SentenceList } from 'lib/components/ActivityLog/SentenceList'
+import { dayjs } from 'lib/dayjs'
 import { LemonCard } from 'lib/lemon-ui/LemonCard'
 import { Link } from 'lib/lemon-ui/Link'
+import { CONCLUSION_DISPLAY_CONFIG } from 'scenes/experiments/constants'
 import { urls } from 'scenes/urls'
 import { match } from 'ts-pattern'
-import { ProgressStatus } from '~/types'
+import { Experiment, ExperimentConclusion, ProgressStatus } from '~/types'
 import { StatusTag } from './ExperimentView/components'
 
+/**
+ * if an id is provided, it returns a link to the experiemt. Otherwise, just the name.
+ */
 const nameOrLinkToExperiment = (name: string | null, id?: string): JSX.Element | string => {
     if (id) {
         return <Link to={urls.experiment(id)}>{name}</Link>
@@ -14,12 +25,105 @@ const nameOrLinkToExperiment = (name: string | null, id?: string): JSX.Element |
     return name || '(unknown)'
 }
 
+/**
+ * id an id is provided, it returns a link to the shared metric. Otherwise, just the name.
+ */
 const nameOrLinkToSharedMetric = (name: string | null, id?: string): JSX.Element | string => {
     if (id) {
         return <Link to={urls.experimentsSharedMetric(id)}>{name}</Link>
     }
 
     return name || '(unknown)'
+}
+
+/**
+ * we pick the allowed properties, and shoehorn in deleted because it's missing from the type
+ */
+type AllowedExperimentFields = Pick<Experiment, 'conclusion' | 'start_date' | 'end_date' | 'metrics'> & {
+    deleted: boolean
+}
+
+const getSharedMetricChangeMappings = (sharedMetricChange: ActivityChange): string | JSX.Element | null => {
+    return match(sharedMetricChange).otherwise(() => null)
+}
+
+const getExperimentChangeMappings = (experimentChange: ActivityChange): string | JSX.Element | null => {
+    /**
+     * a little type assertion to force field into the allowed experiment fields
+     */
+    return match(experimentChange as ActivityChange & { field: keyof AllowedExperimentFields })
+        .with({ field: 'start_date' }, ({ action, before, after }) => {
+            /**
+             * id start date is created, the experiment has been launched
+             */
+            if (action === 'created' && before === null && after !== null) {
+                return 'launched experiment:'
+            }
+
+            /**
+             * if start date has changed, we report how much time was added or removed
+             */
+            if (action === 'changed' && before !== null && after !== null) {
+                const beforeDate = dayjs(before as string)
+                const afterDate = dayjs(after as string)
+
+                if (beforeDate.isValid() && afterDate.isValid()) {
+                    const diff = afterDate.diff(beforeDate, 'minute')
+                    const duration = dayjs.duration(Math.abs(diff), 'minute')
+                    const sign = diff > 0 ? 'moved the start date forward' : 'moved the start date back'
+
+                    return `${sign} by ${duration.humanize()} on`
+                }
+            }
+
+            return 'updated the start date of'
+        })
+        .with({ field: 'end_date' }, ({ action, before, after }) => {
+            /**
+             * if end date is created, the experiment has been stopped
+             */
+            if (action === 'created' && before === null && after !== null) {
+                return 'stopped experiment'
+            }
+
+            return 'updated the end date of'
+        })
+        .with({ field: 'conclusion' }, ({ action, before, after }) => {
+            /**
+             * if conclusion was creted, the experiment was closed. This is usually
+             * acompanied by the end date creation
+             */
+            if (action === 'created' && before === null) {
+                return (
+                    <span>
+                        completed it as <ExperimentConclusionTag conclusion={after as ExperimentConclusion} />:
+                    </span>
+                )
+            }
+
+            return null
+        })
+        .with({ field: 'metrics' }, ({ action, before, after }) => {
+            /**
+             * if a metric is created, the user has added the first metric to the experiment.
+             */
+            if (action === 'created' && before === null && after !== null) {
+                return 'added the first metric to'
+            }
+
+            return null
+        })
+        .with({ field: 'deleted' }, ({ action, before, after }) => {
+            /**
+             * if deleted has been changed from false to true, the experiment has been
+             * deleted
+             */
+            if (action === 'changed' && !before && after) {
+                return 'deleted experiment:'
+            }
+            return null
+        })
+        .otherwise(() => null)
 }
 
 //exporting so the linter doesn't complain about this not being used
@@ -48,10 +152,17 @@ const UnknownAction = ({ logItem }: { logItem: ActivityLogItem }): JSX.Element =
         <SentenceList
             prefix={<strong>{userNameForLogItem(logItem)}</strong>}
             listParts={['performed an unknown action on']}
-            suffix={nameOrLinkToExperiment(logItem.item_id, logItem.detail.name)}
+            suffix={nameOrLinkToExperiment(logItem.detail.name, logItem.item_id)}
         />
     )
 }
+
+const ExperimentConclusionTag = ({ conclusion }: { conclusion: ExperimentConclusion }): JSX.Element => (
+    <div className="font-semibold inline-flex items-center gap-2">
+        <div className={clsx('w-2 h-2 rounded-full', CONCLUSION_DISPLAY_CONFIG[conclusion]?.color || '')} />
+        <span>{CONCLUSION_DISPLAY_CONFIG[conclusion]?.title || conclusion}</span>
+    </div>
+)
 
 export const experimentActivityDescriber = (logItem: ActivityLogItem): HumanizedChange => {
     /**
@@ -84,10 +195,7 @@ export const experimentActivityDescriber = (logItem: ActivityLogItem): Humanized
             }
         })
         .with({ activity: 'updated' }, () => {
-            /**
-             * the experiment UI only allows for atomic updates of a single property at a time.
-             */
-            const changes = logItem.detail?.changes || []
+            const changes = logItem.detail.changes || []
 
             /**
              * if there are no changes, we don't need to describe the update.
@@ -98,98 +206,37 @@ export const experimentActivityDescriber = (logItem: ActivityLogItem): Humanized
                         <SentenceList
                             prefix={<strong>{userNameForLogItem(logItem)}</strong>}
                             listParts={['updated']}
-                            suffix={nameOrLinkToExperiment(logItem.detail.name, logItem.item_id)}
+                            suffix={(isSharedMetric ? nameOrLinkToSharedMetric : nameOrLinkToExperiment)(
+                                logItem.detail.name,
+                                logItem.item_id
+                            )}
                         />
                     ),
                 }
             }
 
-            /**
-             * this needs to be refactored to handle multiple changes, for example,
-             * when an experiment stops and we set a conclusion.
-             */
-            const change = changes[0]
+            const listParts = changes
+                .map((change) => (isSharedMetric ? getSharedMetricChangeMappings : getExperimentChangeMappings)(change))
+                .filter((part) => part !== null)
 
-            /**
-             * if a start date is created, the experiment has been launched.
-             */
-            if (
-                change.action === 'created' &&
-                change.field === 'start_date' &&
-                change.before === null &&
-                change.after !== null
-            ) {
-                return {
-                    description: (
-                        <SentenceList
-                            prefix={<strong>{userNameForLogItem(logItem)}</strong>}
-                            listParts={['launched experiment']}
-                            suffix={nameOrLinkToExperiment(logItem.detail.name, logItem.item_id)}
-                        />
-                    ),
-                }
+            if (listParts.length === 0) {
+                return { description: null }
             }
 
             /**
-             * if an end date is created, the experiment has been completed.
-             */
-            if (
-                change.action === 'created' &&
-                change.field === 'end_date' &&
-                change.before === null &&
-                change.after !== null
-            ) {
-                return {
-                    description: (
-                        <SentenceList
-                            prefix={<strong>{userNameForLogItem(logItem)}</strong>}
-                            listParts={['stopped experiment']}
-                            suffix={nameOrLinkToExperiment(logItem.detail.name, logItem.item_id)}
-                        />
-                    ),
-                }
-            }
-
-            /**
-             * if a metric is created, the user has added the first metric to the experiment.
-             */
-            if (
-                change.action === 'created' &&
-                change.field === 'metrics' &&
-                change.before === null &&
-                change.after !== null
-            ) {
-                return {
-                    description: (
-                        <SentenceList
-                            prefix={<strong>{userNameForLogItem(logItem)}</strong>}
-                            listParts={['added the first metric to']}
-                            suffix={nameOrLinkToExperiment(logItem.detail.name, logItem.item_id)}
-                        />
-                    ),
-                }
-            }
-
-            /**
-             * soft deletes
-             */
-            if (change.action === 'changed' && change.field === 'deleted' && !change.before && change.after) {
-                return {
-                    description: (
-                        <SentenceList
-                            prefix={<strong>{userNameForLogItem(logItem)}</strong>}
-                            listParts={['deleted experiment']}
-                            suffix={nameOrLinkToExperiment(logItem.detail.name, logItem.item_id)}
-                        />
-                    ),
-                }
-            }
-
-            /**
-             * TODO: return null for now, until we cover all existing uses.
+             * we always prefix with the user name, and suffix with a link to the resource
              */
             return {
-                description: null,
+                description: (
+                    <SentenceList
+                        prefix={<strong>{userNameForLogItem(logItem)}</strong>}
+                        listParts={listParts}
+                        suffix={(isSharedMetric ? nameOrLinkToSharedMetric : nameOrLinkToExperiment)(
+                            logItem.detail.name,
+                            logItem.item_id
+                        )}
+                    />
+                ),
             }
         })
         .with({ activity: 'deleted', detail: { type: 'shared_metric' } }, () => {
