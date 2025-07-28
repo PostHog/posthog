1d9461d9cdd911c11544f90e59f429b6aae72730
Lucas Ricoy <lucas.r@posthog.com>
2025-07-28 20:17:31 -0300

    chore: put feature behind flag

--- frontend/src/scenes/web-analytics/WebPropertyFilters.tsx
+++ frontend/src/scenes/web-analytics/WebPropertyFilters.tsx
@@ -9,6 +9,8 @@ import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { useState } from 'react'
 
 import { webAnalyticsLogic } from './webAnalyticsLogic'
+import { FEATURE_FLAGS } from 'lib/constants'
+import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 
 export const OPITIMIZED_PROPERTIES_BY_GROUP = {
     [TaxonomicFilterGroupType.EventProperties]: [
@@ -38,6 +40,7 @@ export const OPITIMIZED_PROPERTIES_BY_GROUP = {
 
 export const WebPropertyFilters = (): JSX.Element => {
     const { rawWebAnalyticsFilters, preAggregatedEnabled } = useValues(webAnalyticsLogic)
+    const { featureFlags } = useValues(featureFlagLogic)
     const { setWebAnalyticsFilters } = useActions(webAnalyticsLogic)
 
     const [displayFilters, setDisplayFilters] = useState(false)
@@ -47,6 +50,11 @@ export const WebPropertyFilters = (): JSX.Element => {
         TaxonomicFilterGroupType.SessionProperties,
         ...(!preAggregatedEnabled ? [TaxonomicFilterGroupType.PersonProperties] : []),
     ]
+
+    // Enable optimized hints for optimized properties if the team has the flag.
+    // This is different from `preAggregatedEnabled` because it doesn't look at the query modifier,
+    // which can be toggled on/off. This puts us one step closer to removing the toggle.
+    const enableOptimizedHints = !!featureFlags[FEATURE_FLAGS.SETTINGS_WEB_ANALYTICS_PRE_AGGREGATED_TABLES]
 
     // Keep in sync with posthog/hogql_queries/web_analytics/stats_table_pre_aggregated.py
     const webAnalyticsPropertyAllowList = preAggregatedEnabled
@@ -95,7 +103,7 @@ export const WebPropertyFilters = (): JSX.Element => {
                         propertyFilters={rawWebAnalyticsFilters}
                         pageKey="web-analytics"
                         eventNames={['$pageview']}
-                        enableOptimizedHints={true}
+                        enableOptimizedHints={enableOptimizedHints}
                     />
                 </div>
             }
