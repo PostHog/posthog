e173d126ea89fec09c6cff47cebccedea0082e43
Tom Owers <tom@posthog.com>
2025-07-28 12:56:59 +0100

    feat(data-warehouse): easy peasy source (#35212)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/queries/schema/schema-general.ts
+++ frontend/src/queries/schema/schema-general.ts
@@ -3502,6 +3502,13 @@ export enum MarketingAnalyticsHelperForColumnNames {
     Goal = 'Goal',
     CostPer = 'Cost per',
 }
+
+export interface SourceFieldSSHTunnelConfig {
+    type: 'ssh-tunnel'
+    label: string
+    name: string
+}
+
 export interface SourceFieldOauthConfig {
     type: 'oauth'
     name: string
@@ -3525,6 +3532,7 @@ export interface SourceFieldSelectConfig {
     required: boolean
     defaultValue: string
     options: { label: string; value: string; fields?: SourceFieldConfig[] }[]
+    converter?: 'str_to_int' | 'str_to_bool' | 'str_to_optional_int'
 }
 
 export interface SourceFieldSwitchGroupConfig {
@@ -3536,11 +3544,16 @@ export interface SourceFieldSwitchGroupConfig {
     caption?: string
 }
 
+export interface SourceFieldFileUploadJsonFormatConfig {
+    format: '.json'
+    keys: '*' | string[]
+}
+
 export interface SourceFieldFileUploadConfig {
     type: 'file-upload'
     name: string
     label: string
-    fileFormat: string
+    fileFormat: SourceFieldFileUploadJsonFormatConfig
     required: boolean
 }
 
@@ -3550,6 +3563,7 @@ export type SourceFieldConfig =
     | SourceFieldSelectConfig
     | SourceFieldOauthConfig
     | SourceFieldFileUploadConfig
+    | SourceFieldSSHTunnelConfig
 
 export interface SourceConfig {
     name: ExternalDataSourceType
