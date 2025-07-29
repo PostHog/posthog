91fe3b68e5d63d30716be0c9bf592e224ab6f7e3
Alexandr Lider <alexandrlider@Alexandrs-MacBook-Pro.local>
2025-07-29 19:24:02 +0200

    Add unit tests

--- posthog/utils.py
+++ posthog/utils.py
@@ -617,6 +617,8 @@ def get_ip_address(request: HttpRequest) -> str:
 def get_short_user_agent(request: HttpRequest) -> str:
     """Returns browser and OS info from user agent, eg: 'Chrome 135.0.0 on macOS 10.15'"""
     user_agent_str = request.META.get("HTTP_USER_AGENT")
+    if not user_agent_str:
+        return ""
 
     user_agent = parse(user_agent_str)
 
