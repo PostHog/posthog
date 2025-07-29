61efb07394c7ecae1b627cfccf31f3e32ef62c31
David Newell <d.newell1@outlook.com>
2025-07-29 08:33:43 +0100

    Revert "feat: add exception props to alert payloads (#35727)"
    This reverts commit ee5d9af33b5abe6198c775d75da3f8306cea4a63.

--- rust/cymbal/src/issue_resolution.rs
+++ rust/cymbal/src/issue_resolution.rs
@@ -10,7 +10,7 @@ use uuid::Uuid;
 
 use crate::assignment_rules::{try_assignment_rules, Assignee, Assignment};
 use crate::teams::TeamManager;
-use crate::types::{FingerprintedErrProps, OutputErrProps};
+use crate::types::FingerprintedErrProps;
 use crate::{
     app_context::AppContext,
     error::UnhandledError,
@@ -269,8 +269,13 @@ pub async fn resolve_issue(
                 event_properties.clone(),
             )
             .await?;
-            let output_props: OutputErrProps = event_properties.clone().to_output(issue.id);
-            send_issue_reopened_alert(&context, &issue, assignment, output_props).await?;
+            send_issue_reopened_alert(
+                &context,
+                &issue,
+                &event_properties.fingerprint.value,
+                assignment,
+            )
+            .await?;
         }
         return Ok(issue);
     }
@@ -321,8 +326,13 @@ pub async fn resolve_issue(
                 event_properties.clone(),
             )
             .await?;
-            let output_props: OutputErrProps = event_properties.clone().to_output(issue.id);
-            send_issue_reopened_alert(&context, &issue, assignment, output_props).await?;
+            send_issue_reopened_alert(
+                &context,
+                &issue,
+                &event_properties.fingerprint.value,
+                assignment,
+            )
+            .await?;
         }
     } else {
         metrics::counter!(ISSUE_CREATED).increment(1);
@@ -333,9 +343,13 @@ pub async fn resolve_issue(
             event_properties.clone(),
         )
         .await?;
-
-        let output_props = event_properties.clone().to_output(issue.id);
-        send_issue_created_alert(&context, &issue, assignment, output_props).await?;
+        send_issue_created_alert(
+            &context,
+            &issue,
+            &event_properties.fingerprint.value,
+            assignment,
+        )
+        .await?;
         txn.commit().await?;
         capture_issue_created(team_id, issue_override.issue_id);
     };
@@ -367,15 +381,15 @@ pub async fn process_assignment(
 async fn send_issue_created_alert(
     context: &AppContext,
     issue: &Issue,
+    fingerprint: &String,
     assignment: Option<Assignment>,
-    output_props: OutputErrProps,
 ) -> Result<(), UnhandledError> {
     send_internal_event(
         context,
         "$error_tracking_issue_created",
         issue,
+        fingerprint,
         assignment,
-        output_props,
     )
     .await
 }
@@ -383,15 +397,15 @@ async fn send_issue_created_alert(
 async fn send_issue_reopened_alert(
     context: &AppContext,
     issue: &Issue,
+    fingerprint: &String,
     assignment: Option<Assignment>,
-    output_props: OutputErrProps,
 ) -> Result<(), UnhandledError> {
     send_internal_event(
         context,
         "$error_tracking_issue_reopened",
         issue,
+        fingerprint,
         assignment,
-        output_props,
     )
     .await
 }
@@ -400,8 +414,8 @@ async fn send_internal_event(
     context: &AppContext,
     event: &str,
     issue: &Issue,
+    fingerprint: &String,
     new_assignment: Option<Assignment>,
-    output_props: OutputErrProps,
 ) -> Result<(), UnhandledError> {
     let mut event = InternalEventEvent::new(event, issue.id, Utc::now(), None);
     event
@@ -411,8 +425,7 @@ async fn send_internal_event(
         .insert_prop("description", issue.description.clone())
         .expect("Strings are serializable");
     event.insert_prop("status", issue.status.as_str())?;
-    event.insert_prop("fingerprint", &output_props.fingerprint)?;
-    event.insert_prop("exception_props", output_props)?;
+    event.insert_prop("fingerprint", fingerprint)?;
 
     if let Some(assignment) = new_assignment {
         let assignee = Assignee::try_from(&assignment)?;
