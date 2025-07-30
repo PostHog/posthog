235488b4f60b92d83be07e90c5235838ebc5fd99
Alex V <alexander.veryaysky@gmail.com>
2025-07-30 10:50:58 +0200

    merge

--- posthog/migrations/0812_sessionrecordingviewed_bulk_viewed.py
+++ posthog/migrations/0812_sessionrecordingviewed_bulk_viewed.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0810_alter_hogfunctiontemplate_icon_url"),
+        ("posthog", "0811_personalapikey_last_rolled_at"),
     ]
 
     operations = [
