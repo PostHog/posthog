0940ba74bc143b9a5d2e593e4944230094a8a9e1
Ben White <ben@posthog.com>
2025-07-31 10:54:16 +0200

    Fix queue logic

--- plugin-server/src/cdp/consumers/cdp-cyclotron-worker.consumer.ts
+++ plugin-server/src/cdp/consumers/cdp-cyclotron-worker.consumer.ts
@@ -20,9 +20,9 @@ export class CdpCyclotronWorker extends CdpConsumerBase {
     protected cyclotronJobQueue: CyclotronJobQueue
     protected queue: CyclotronJobQueueKind
 
-    constructor(hub: Hub) {
+    constructor(hub: Hub, queue?: CyclotronJobQueueKind) {
         super(hub)
-        this.queue = hub.CDP_CYCLOTRON_JOB_QUEUE_CONSUMER_KIND
+        this.queue = queue ?? hub.CDP_CYCLOTRON_JOB_QUEUE_CONSUMER_KIND
 
         if (!CYCLOTRON_INVOCATION_JOB_QUEUES.includes(this.queue)) {
             throw new Error(`Invalid cyclotron job queue kind: ${this.queue}`)
