245a2aec8482278e158f34f3bafac44e178691e2
Nima <73110514+nima-karimi@users.noreply.github.com>
2025-07-31 16:45:00 -0700

    feat(insights): add Event Definition Insights component and logic

---
+++ frontend/src/scenes/data-management/events/EventDefinitionInsights.tsx
@@ -0,0 +1,110 @@
+import { LemonInput, Link } from '@posthog/lemon-ui'
+import { useActions, useValues } from 'kea'
+import { LemonTable, LemonTableColumn, LemonTableColumns } from 'lib/lemon-ui/LemonTable'
+import { useEffect } from 'react'
+import { urls } from 'scenes/urls'
+
+import { EventDefinition, QueryBasedInsightModel } from '~/types'
+
+import { eventInsightsLogic, INSIGHTS_PER_PAGE } from 'scenes/data-management/events/eventInsightsLogic'
+import { InsightIcon } from 'scenes/saved-insights/SavedInsights'
+import { createdByColumn } from 'lib/lemon-ui/LemonTable/columnUtils'
+import { useSummarizeInsight } from 'scenes/insights/summarizeInsight'
+
+export function EventDefinitionInsights({ definition }: { definition: EventDefinition }): JSX.Element {
+    const { page, insights, count, filters, insightsLoading, sorting } = useValues(eventInsightsLogic)
+    const { setPage, setFilters } = useActions(eventInsightsLogic)
+    const summarizeInsight = useSummarizeInsight()
+
+    const startCount = (page - 1) * INSIGHTS_PER_PAGE + 1
+    const endCount = Math.min(page * INSIGHTS_PER_PAGE, count)
+
+    useEffect(() => {
+        setFilters({ events: [definition.name] })
+    }, [definition, setFilters])
+
+    const columns: LemonTableColumns<QueryBasedInsightModel> = [
+        {
+            key: 'id',
+            width: 32,
+            render: function renderType(_, insight) {
+                return <InsightIcon insight={insight} className="text-secondary text-2xl" />
+            },
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: function renderName(name: string, insight) {
+                return (
+                    <>
+                        <div className="flex flex-col gap-1">
+                            <div className="inline-flex">
+                                <Link to={urls.insightView(insight.short_id)} target="_blank">
+                                    {name || <i>{summarizeInsight(insight.query)}</i>}
+                                </Link>
+                            </div>
+                            <div className="text-xs text-tertiary">{insight.description}</div>
+                        </div>
+                    </>
+                )
+            },
+        },
+        createdByColumn() as LemonTableColumn<QueryBasedInsightModel, keyof QueryBasedInsightModel | undefined>,
+    ]
+
+    return (
+        <div className="saved-insights">
+            <h3>Insights using event</h3>
+            <div className="flex justify-between mb-4 gap-2 flex-wrap mt-2 items-center">
+                <span className="text-secondary">
+                    {count
+                        ? `${startCount}${endCount - startCount > 1 ? '-' + endCount : ''} of ${count} insight${
+                              count === 1 ? '' : 's'
+                          }`
+                        : null}
+                </span>
+                <LemonInput
+                    type="search"
+                    placeholder="Search..."
+                    onChange={(value) => setFilters({ search: value })}
+                    value={filters.search || ''}
+                />
+            </div>
+            <LemonTable
+                id={`event-definition-insights-table-${definition.id}`}
+                loading={insightsLoading}
+                columns={columns}
+                data-attr="event-definition-insights-table"
+                dataSource={insights.results ? insights.results : []}
+                pagination={{
+                    controlled: true,
+                    currentPage: insights?.page ?? 1,
+                    entryCount: insights?.count ?? 0,
+                    pageSize: INSIGHTS_PER_PAGE,
+                    onForward: insights?.next
+                        ? () => {
+                              setPage(page + 1)
+                          }
+                        : undefined,
+                    onBackward: insights?.previous
+                        ? () => {
+                              setPage(page - 1)
+                          }
+                        : undefined,
+                }}
+                sorting={sorting}
+                onSort={(newSorting) =>
+                    setFilters({
+                        order: newSorting ? `${newSorting.order === -1 ? '-' : ''}${newSorting.columnKey}` : undefined,
+                    })
+                }
+                rowKey="id"
+                loadingSkeletonRows={INSIGHTS_PER_PAGE}
+                nouns={['insight', 'insights']}
+                useURLForSorting={false}
+                emptyState="No insights found"
+            />
+        </div>
+    )
+}
---
+++ frontend/src/scenes/data-management/events/eventInsightsLogic.ts
@@ -0,0 +1,115 @@
+import { actions, connect, kea, listeners, path, reducers, selectors } from 'kea'
+import { loaders } from 'kea-loaders'
+import api from 'lib/api'
+import { Sorting } from 'lib/lemon-ui/LemonTable'
+import { objectsEqual, toParams } from 'lib/utils'
+import { eventUsageLogic } from 'lib/utils/eventUsageLogic'
+import { teamLogic } from 'scenes/teamLogic'
+
+import { getQueryBasedInsightModel } from '~/queries/nodes/InsightViz/utils'
+import { QueryBasedInsightModel } from '~/types'
+
+import { cleanFilters, SavedInsightFilters } from 'scenes/saved-insights/savedInsightsLogic'
+
+import type { eventInsightsLogicType } from './eventInsightsLogicType'
+
+export const INSIGHTS_PER_PAGE = 10
+
+export const eventInsightsLogic = kea<eventInsightsLogicType>([
+    path(['scenes', 'saved-insights', 'addSavedInsightsModalLogic']),
+    connect(() => ({
+        values: [teamLogic, ['currentTeamId']],
+        logic: [eventUsageLogic],
+    })),
+    actions({
+        setFilters: (filters: Partial<SavedInsightFilters>) => ({ filters }),
+        loadInsights: true,
+        setPage: (page: number) => ({ page }),
+    }),
+    loaders(({ values }) => ({
+        insights: {
+            __default: { results: [] as QueryBasedInsightModel[], count: 0 },
+            loadInsights: async () => {
+                const { filters } = values
+                const { order, page, events, search } = filters
+
+                if (!events || events.length === 0) {
+                    return { results: [], count: 0 }
+                }
+
+                const params: Record<string, any> = {
+                    order,
+                    events,
+                    limit: INSIGHTS_PER_PAGE,
+                    offset: Math.max(0, (page - 1) * INSIGHTS_PER_PAGE),
+                    saved: true,
+                    basic: true,
+                }
+
+                if (search) {
+                    params.search = search
+                }
+
+                const response = await api.get(
+                    `api/environments/${teamLogic.values.currentTeamId}/insights/?${toParams(params)}`
+                )
+
+                return {
+                    ...response,
+                    filters,
+                    count: response.count,
+                    page,
+                    previous: response.previous,
+                    next: response.next,
+                    results: response.results.map((rawInsight: any) => getQueryBasedInsightModel(rawInsight)),
+                }
+            },
+        },
+    })),
+    reducers({
+        rawFilters: [
+            null as Partial<SavedInsightFilters> | null,
+            {
+                setFilters: (state, { filters }) => {
+                    return cleanFilters({
+                        ...state,
+                        ...filters,
+                        ...('page' in filters ? {} : { page: 1 }),
+                    })
+                },
+            },
+        ],
+    }),
+    selectors({
+        filters: [(s) => [s.rawFilters], (rawFilters: object): SavedInsightFilters => cleanFilters(rawFilters || {})],
+        count: [(s) => [s.insights], (insights) => insights.count],
+        sorting: [
+            (s) => [s.filters],
+            (filters): Sorting | null =>
+                filters.order
+                    ? filters.order.startsWith('-')
+                        ? { columnKey: filters.order.slice(1), order: -1 }
+                        : { columnKey: filters.order, order: 1 }
+                    : null,
+        ],
+        page: [(s) => [s.filters], (filters) => filters.page],
+        usingFilters: [
+            (s) => [s.filters],
+            (filters) => !objectsEqual(cleanFilters({ ...filters, events: undefined }), cleanFilters({})),
+        ],
+    }),
+    listeners(({ actions, values, selectors }) => ({
+        setPage: async ({ page }) => {
+            actions.setFilters({ page })
+            actions.loadInsights()
+        },
+        setFilters: async ({}, breakpoint, __, previousState) => {
+            const oldFilters = selectors.filters(previousState)
+            const newFilters = values.filters
+            await breakpoint(300)
+            if (!objectsEqual(oldFilters, newFilters) && newFilters.events && newFilters.events.length > 0) {
+                actions.loadInsights()
+            }
+        },
+    })),
+])
