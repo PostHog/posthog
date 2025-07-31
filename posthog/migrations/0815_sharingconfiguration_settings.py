8711adc0be849af866f7ff7351b9b9a71f60bcc4
Zach Waterfield <zlwaterfield@gmail.com>
2025-07-31 16:13:02 -0400

    Update migration dependency and rename migration file
    Renamed migration file from 0813 to 0815 and updated its dependency from 0812_sessionrecordingviewed_bulk_viewed to 0814_datawarehousesavedquerydraft to maintain correct migration order.

--- posthog/migrations/0815_sharingconfiguration_settings.py
+++ posthog/migrations/0815_sharingconfiguration_settings.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0812_sessionrecordingviewed_bulk_viewed"),
+        ("posthog", "0814_datawarehousesavedquerydraft"),
     ]
 
     operations = [
