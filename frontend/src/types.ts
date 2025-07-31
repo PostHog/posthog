240a18c194234f6855a9bd7e496d3a044667335e
Eric Duong <eric@posthog.com>
2025-07-31 12:54:45 -0700

    feat: editor sidebar draft mode (#35736)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -4527,10 +4527,21 @@ export interface DataWarehouseSavedQuery {
     latest_history_id?: string
 }
 
+export interface DataWarehouseSavedQueryDraft {
+    id: string
+    query: HogQLQuery
+    saved_query_id?: string
+    created_at: string
+    updated_at: string
+    name: string
+    edited_history_id?: string
+}
+
 export interface DataWarehouseViewLinkConfiguration {
     experiments_optimized?: boolean
     experiments_timestamp_key?: string | null
 }
+
 export interface DataWarehouseViewLink {
     id: string
     source_table_name?: string
