8969c820aa676ddc10b855dfa299bd28b2a5dfef
Marius Andra <marius.andra@gmail.com>
2025-08-01 12:32:41 +0200

    fix bug

--- posthog/warehouse/api/saved_query.py
+++ posthog/warehouse/api/saved_query.py
@@ -332,7 +332,7 @@ class DataWarehouseSavedQuerySerializer(serializers.ModelSerializer):
         find_placeholders.visit(select_ast)
         if len(find_placeholders.placeholder_fields) > 0:
             placeholder = find_placeholders.placeholder_fields.pop()
-            placeholder = [str(field) for field in placeholder if field is not None].join(".")
+            placeholder = ".".join(str(field) for field in placeholder if field is not None)
             raise exceptions.ValidationError(detail=f"Variables like {'{'}{placeholder}{'}'} are not allowed in views")
         elif find_placeholders.placeholder_expressions or find_placeholders.has_filters:
             raise exceptions.ValidationError(detail="Filters and placeholder expressions are not allowed in views")
