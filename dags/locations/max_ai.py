7036628bb8f963797d3b9f316bcfbd59b701852c
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-08-01 15:45:55 +0200

    feat: migration script

--- dags/locations/max_ai.py
+++ dags/locations/max_ai.py
@@ -1,14 +1,14 @@
 import dagster
+from dagster_docker import PipesDockerClient
 
-from dags import (
-    snapshot_team_data,
-)
+from dags.database_migration import migrate_and_export_database_dump
 
 from . import resources
 
 defs = dagster.Definitions(
-    jobs=[
-        snapshot_team_data.snapshot_team_data_job,
-    ],
-    resources=resources,
+    assets=[migrate_and_export_database_dump],
+    resources={
+        **resources,
+        "docker_pipes_client": PipesDockerClient(),
+    },
 )
