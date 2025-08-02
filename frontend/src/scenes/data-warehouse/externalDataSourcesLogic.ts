dded9161f9096114195a30f79caf6f36d1dec42f
naumaanh <naumaan.hassan@gmail.com>
2025-08-01 23:39:50 -0500

    Initial migration for externalDataSource.list()

--- frontend/src/scenes/data-warehouse/externalDataSourcesLogic.ts
+++ frontend/src/scenes/data-warehouse/externalDataSourcesLogic.ts
@@ -1,4 +1,4 @@
-import { kea, path } from 'kea'
+import { actions, kea, listeners, path, reducers } from 'kea'
 import { loaders } from 'kea-loaders'
 import api, { ApiMethodOptions, PaginatedResponse } from 'lib/api'
 
@@ -8,14 +8,50 @@ import type { externalDataSourcesLogicType } from './externalDataSourcesLogicTyp
 
 export const externalDataSourcesLogic = kea<externalDataSourcesLogicType>([
     path(['scenes', 'data-warehouse', 'externalDataSourcesLogic']),
-    loaders({
+    actions({
+        abortAnyRunningQuery: true,
+    }),
+    loaders(({ cache, actions }) => ({
         dataWarehouseSources: [
             null as PaginatedResponse<ExternalDataSource> | null,
             {
-                loadSources: (options?: ApiMethodOptions | null) => {
-                    return api.externalDataSources.list(options ?? undefined)
+                loadSources: async (options: ApiMethodOptions | null = null, breakpoint) => {
+                    await breakpoint(300)
+                    actions.abortAnyRunningQuery()
+
+                    cache.abortController = new AbortController()
+                    const methodOptions: ApiMethodOptions = {
+                        signal: cache.abortController.signal,
+                        ...options,
+                    }
+
+                    const res = await api.externalDataSources.list(methodOptions)
+
+                    breakpoint()
+
+                    cache.abortController = null
+
+                    return res
                 },
             },
         ],
-    }),
+    })),
+    reducers(({ cache }) => ({
+        dataWarehouseSourcesLoading: [
+            false as boolean,
+            {
+                loadSources: () => true,
+                loadSourcesFailure: () => cache.abortController !== null,
+                loadSourcesSuccess: () => cache.abortController !== null,
+            },
+        ],
+    })),
+    listeners(({ cache }) => ({
+        abortAnyRunningQuery: () => {
+            if (cache.abortController) {
+                cache.abortController.abort()
+                cache.abortController = null
+            }
+        },
+    })),
 ])
