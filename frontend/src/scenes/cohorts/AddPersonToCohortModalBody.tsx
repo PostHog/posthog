87e2ec3dba4d00a0aeaa675526df2a637fa7d8cb
Shao Wei <lshaowei18@gmail.com>
2025-08-04 07:24:06 +0800

    fix: switch to a query instead

--- frontend/src/scenes/cohorts/AddPersonToCohortModalBody.tsx
+++ frontend/src/scenes/cohorts/AddPersonToCohortModalBody.tsx
@@ -1,7 +1,43 @@
-import { cohortEditLogic, CohortLogicProps } from './cohortEditLogic'
+import { useActions, useValues } from 'kea'
+import { addPersonToCohortModalLogic } from './addPersonToCohortModalLogic'
+import { Query } from '~/queries/Query/Query'
+import { QueryContext } from '~/queries/types'
+import { LemonButton } from '@posthog/lemon-ui'
+import { IconMinusSmall, IconPlusSmall } from '@posthog/icons'
 
-export function AddPersonToChortModalBody({ id }: CohortLogicProps): JSX.Element {
-    const logicProps = { id }
+export function AddPersonToChortModalBody(): JSX.Element {
+    const { query } = useValues(addPersonToCohortModalLogic)
+    const { setQuery } = useActions(addPersonToCohortModalLogic)
+    const context: QueryContext = {
+        columns: {
+            id: {
+                render: () => {
+                    const isInCohort = false
+                    return (
+                        <LemonButton
+                            type="secondary"
+                            status={isInCohort ? 'danger' : 'default'}
+                            size="small"
+                            fullWidth
+                            onClick={(e) => {
+                                e.preventDefault()
 
-    return <></>
+                                // isInCohort
+                                //     ? removeInsightFromDashboard(insight, dashboard?.id || 0)
+                                //     : addInsightToDashboard(insight, dashboard?.id || 0)
+                            }}
+                        >
+                            {isInCohort ? <IconMinusSmall /> : <IconPlusSmall />}
+                        </LemonButton>
+                    )
+                },
+            },
+        },
+        showOpenEditorButton: false,
+    }
+    return (
+        <>
+            <Query query={query} setQuery={setQuery} context={context} />
+        </>
+    )
 }
