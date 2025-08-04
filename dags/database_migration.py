64de075c29401f16bac097fccae4b272317a304d
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-08-04 15:13:24 +0200

    feat: property snapshotting

--- dags/database_migration.py
+++ dags/database_migration.py
@@ -1,9 +1,62 @@
+from tempfile import TemporaryFile
+
 import dagster
+from dagster_aws.s3 import S3Resource
 from dagster_docker import PipesDockerClient
 from django.conf import settings
+from fastavro import parse_schema, writer
+from pydantic_avro import AvroBase
 
 from dags.common import JobOwners
+from posthog.models import PropertyDefinition
 from posthog.models.utils import uuid7
+
+
+class SnapshotConfig(dagster.Config):
+    project_id: int
+
+
+# posthog/models/property_definition.py
+class PropertyDefinitionSchema(AvroBase):
+    name: str
+    is_numerical: bool
+    property_type: str | None
+    type: int
+    group_type_index: int | None
+
+
+@dagster.asset(
+    name="snapshot_property_definitions",
+    description="Snapshots property definitions",
+    tags={"owner": JobOwners.TEAM_MAX_AI.value},
+)
+def snapshot_property_definitions(config: SnapshotConfig, s3: S3Resource):
+    with TemporaryFile() as f:
+        for prop in PropertyDefinition.objects.filter(project_id=config.project_id).iterator(500):
+            model = PropertyDefinitionSchema(
+                name=prop.name,
+                is_numerical=prop.is_numerical,
+                property_type=prop.property_type,
+                type=prop.type,
+                group_type_index=prop.group_type_index,
+            )
+            writer(f, parse_schema(PropertyDefinitionSchema.avro_schema()), model.model_dump())
+        file_key = f"models/{config.project_id}/prop_defs.avro"
+        s3.get_client().upload_fileobj(f, settings.OBJECT_STORAGE_MAX_AI_EVALS_FOLDER, file_key)
+    return {
+        "bucket": settings.OBJECT_STORAGE_MAX_AI_EVALS_FOLDER,
+        "key": file_key,
+    }
+
+
+@dagster.asset(
+    name="snapshot_project_data",
+    description="Snapshots project data (property definitions, DWH schema, etc.)",
+    deps=[snapshot_property_definitions],
+    tags={"owner": JobOwners.TEAM_MAX_AI.value},
+)
+def snapshot_project_data(config: SnapshotConfig):
+    pass
 
 
 @dagster.asset(
