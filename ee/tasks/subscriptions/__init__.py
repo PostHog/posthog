23c3a93ea6875a8e47d80adb5da96f68f868e6fd
Sandy Spicer <sandy@posthog.com>
2025-08-04 06:21:45 -0700

    capture

--- ee/tasks/subscriptions/__init__.py
+++ ee/tasks/subscriptions/__init__.py
@@ -66,7 +66,8 @@ async def deliver_subscription_report_async(
     insights, assets = await database_sync_to_async(generate_assets)(subscription, use_celery=False)
 
     if not assets:
-        capture_exception(Exception("No assets are in this subscription"), {"subscription_id": subscription.id})
+        # Log this as a warning instead of creating an exception
+        logger.warning("subscription_has_no_assets", subscription_id=subscription.id)
         return
 
     if subscription.target_type == "email":
@@ -161,7 +162,8 @@ def deliver_subscription_report_sync(
     insights, assets = generate_assets(subscription)
 
     if not assets:
-        capture_exception(Exception("No assets are in this subscription"), {"subscription_id": subscription.id})
+        # Log this as a warning instead of creating an exception
+        logger.warning("subscription_has_no_assets", subscription_id=subscription.id)
         return
 
     if subscription.target_type == "email":
