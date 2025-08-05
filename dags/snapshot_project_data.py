a1ac9f11baa71a33bfdfaaf858cd0d2ad1f35332
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-08-05 15:07:04 +0200

    feat: evaluation image

--- dags/snapshot_project_data.py
+++ dags/snapshot_project_data.py
@@ -6,10 +6,14 @@ import dagster
 from dagster_aws.s3 import S3Resource
 from django.conf import settings
 from fastavro import parse_schema, writer
+from pydantic import BaseModel
 from pydantic_avro import AvroBase
 
 from dags.common import JobOwners
-from posthog.models import DataWarehouseTable, PropertyDefinition
+from posthog.hogql_queries.ai.event_taxonomy_query_runner import EventTaxonomyQueryRunner
+from posthog.hogql_queries.ai.team_taxonomy_query_runner import TeamTaxonomyQueryRunner
+from posthog.models import DataWarehouseTable, PropertyDefinition, Team
+from posthog.schema import EventTaxonomyItem, EventTaxonomyQuery, TeamTaxonomyItem, TeamTaxonomyQuery
 
 
 # posthog/models/property_definition.py
@@ -91,12 +95,19 @@ def snapshot_data_warehouse_tables(s3: S3Resource, project_id: int):
     return "data_warehouse_tables", file_key
 
 
+class PostgresProjectDataSnapshot(BaseModel):
+    property_definitions: str
+    data_warehouse_tables: str
+
+
 @dagster.op(
-    description="Snapshots project data (property definitions, DWH schema, etc.)",
+    description="Snapshots Postgres project data (property definitions, DWH schema, etc.)",
     tags={"owner": JobOwners.TEAM_MAX_AI.value},
 )
-def snapshot_project_data(context: dagster.OpExecutionContext, project_id: int, s3: S3Resource):
-    deps: list[SnapshotModelOutput] = dict(
+def snapshot_postgres_project_data(
+    context: dagster.OpExecutionContext, project_id: int, s3: S3Resource
+) -> PostgresProjectDataSnapshot:
+    deps = dict(
         (
             snapshot_property_definitions(s3, project_id),
             snapshot_data_warehouse_tables(s3, project_id),
@@ -107,6 +118,78 @@ def snapshot_project_data(context: dagster.OpExecutionContext, project_id: int, 
             asset_key="project_postgres_snapshots",
             description="Avro snapshots of project Postgres data",
             metadata={"project_id": project_id, **deps},
+            tags={"owner": JobOwners.TEAM_MAX_AI.value},
         )
     )
-    return deps
+    return PostgresProjectDataSnapshot.model_validate(deps)
+
+
+class TeamTaxonomyItemSchema(AvroBase):
+    results: list[TeamTaxonomyItem]
+
+
+def snapshot_events_taxonomy(s3: S3Resource, team: Team):
+    file_key = compose_dump_path(team.id, "events_taxonomy.avro")
+    res = TeamTaxonomyQueryRunner(query=TeamTaxonomyQuery(), team=team).calculate()
+    if not res.results:
+        raise ValueError("No results from events taxonomy query")
+    with dump_model(s3=s3, schema=TeamTaxonomyItemSchema, file_key=file_key) as dump:
+        dumped_items = TeamTaxonomyItemSchema(results=res.results)
+        dump([dumped_items])
+    return file_key, res.results
+
+
+class PropertyTaxonomySchema(AvroBase):
+    event: str
+    results: list[EventTaxonomyItem]
+
+
+def snapshot_properties_taxonomy(
+    context: dagster.OpExecutionContext, s3: S3Resource, team: Team, events: list[TeamTaxonomyItemSchema]
+):
+    results: list[PropertyTaxonomySchema] = []
+    for item in events:
+        context.log.info(f"Snapshotting properties taxonomy for event {item.event}")
+        res = EventTaxonomyQueryRunner(
+            query=EventTaxonomyQuery(event=item.event),
+            team=team,
+        ).calculate()
+        results.append(PropertyTaxonomySchema(event=item.event, results=res.results))
+    file_key = compose_dump_path(team.id, "properties_taxonomy.avro")
+    context.log.info(f"Dumping properties taxonomy to {file_key}")
+    with dump_model(s3=s3, schema=PropertyTaxonomySchema, file_key=file_key) as dump:
+        dump(results)
+    return file_key
+
+
+class ClickhouseProjectDataSnapshot(BaseModel):
+    event_taxonomy: str
+    properties_taxonomy: str
+
+
+@dagster.op(
+    description="Snapshots ClickHouse project data",
+    tags={"owner": JobOwners.TEAM_MAX_AI.value},
+)
+def snapshot_clickhouse_project_data(
+    context: dagster.OpExecutionContext, project_id: int, s3: S3Resource
+) -> ClickhouseProjectDataSnapshot:
+    team = Team.objects.get(id=project_id)
+    event_taxonomy_file_key, event_taxonomy = snapshot_events_taxonomy(s3, team)
+    properties_taxonomy_file_key = snapshot_properties_taxonomy(context, s3, team, event_taxonomy)
+    context.log_event(
+        dagster.AssetMaterialization(
+            asset_key="project_clickhouse_snapshots",
+            description="Avro snapshots of project ClickHouse data",
+            metadata={
+                "project_id": project_id,
+                "event_taxonomy": event_taxonomy_file_key,
+                "properties_taxonomy": properties_taxonomy_file_key,
+            },
+            tags={"owner": JobOwners.TEAM_MAX_AI.value},
+        )
+    )
+    return ClickhouseProjectDataSnapshot(
+        event_taxonomy=event_taxonomy_file_key,
+        properties_taxonomy=properties_taxonomy_file_key,
+    )
