e56ac56e23c55ddf990fdf820015765205a675f6
David Newell <david@posthog.com>
2025-08-05 18:07:18 +0100

    diable link extension

--- frontend/src/scenes/notebooks/Notebook/Editor.tsx
+++ frontend/src/scenes/notebooks/Notebook/Editor.tsx
@@ -77,6 +77,7 @@ export function Editor(): JSX.Element {
                 StarterKit.configure({
                     document: false,
                     gapcursor: false,
+                    link: false,
                 }),
                 TableOfContents.configure({
                     getIndex: getHierarchicalIndexes,
