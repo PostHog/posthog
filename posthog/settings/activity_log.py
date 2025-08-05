88b472006203bbc29643c6de0feffe0fecdbf0c2
Yasen Slavov <yasen@posthog.com>
2025-08-05 12:47:47 +0300

    Fix a cast

--- posthog/settings/activity_log.py
+++ posthog/settings/activity_log.py
@@ -1,6 +1,4 @@
 from posthog.settings.base_variables import TEST
-from posthog.settings.utils import get_from_env, str_to_bool
+from posthog.settings.utils import get_from_env
 
-ACTIVITY_LOG_TRANSACTION_MANAGEMENT = get_from_env(
-    "ACTIVITY_LOG_TRANSACTION_MANAGEMENT", not TEST, type_cast=str_to_bool
-)
+ACTIVITY_LOG_TRANSACTION_MANAGEMENT = get_from_env("ACTIVITY_LOG_TRANSACTION_MANAGEMENT", not TEST, type_cast=bool)
