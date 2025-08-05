a5506a629783ffea7c683b879c8b3466c059432a
Shao Wei <lshaowei18@gmail.com>
2025-08-06 06:47:16 +0800

    fix: reload person data in modal  after adding a user to cohort

--- frontend/src/scenes/cohorts/AddPersonToCohortModalBody.tsx
+++ frontend/src/scenes/cohorts/AddPersonToCohortModalBody.tsx
@@ -4,17 +4,23 @@ import { Query } from '~/queries/Query/Query'
 import { QueryContext } from '~/queries/types'
 import { LemonButton } from '@posthog/lemon-ui'
 import { IconMinusSmall, IconPlusSmall } from '@posthog/icons'
+import React from 'react'
 
 export function AddPersonToChortModalBody(): JSX.Element {
-    const { query } = useValues(addPersonToCohortModalLogic)
+    const { query, cohortPersons } = useValues(addPersonToCohortModalLogic)
     const { setQuery, addPersonToCohort } = useActions(addPersonToCohortModalLogic)
+
+    const cohortPersonsSet = React.useMemo(() => {
+        return new Set(cohortPersons.results.map((p) => p.id))
+    }, [cohortPersons])
+
     const context: QueryContext = {
         columns: {
             id: {
                 renderTitle: () => null,
                 render: (props) => {
                     const id = props.value as string
-                    const isInCohort = false
+                    const isInCohort = cohortPersonsSet.has(id)
                     return (
                         <LemonButton
                             type="secondary"
