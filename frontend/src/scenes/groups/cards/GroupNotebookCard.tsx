c43cb9e04d8311d9e3a979d4fd575c8002beee1c
Arthur Moreira de Deus <arthur@posthog.com>
2025-08-05 14:12:19 -0300

    chore(crm): Move group notebook to a dedicated tab

--- frontend/src/scenes/groups/cards/GroupNotebookCard.tsx
+++ frontend/src/scenes/groups/cards/GroupNotebookCard.tsx
@@ -8,16 +8,14 @@ interface GroupNotebookCardProps {
 
 export function GroupNotebookCard({ shortId }: GroupNotebookCardProps): JSX.Element {
     return (
-        <div className="flex flex-col gap-2 h-full min-h-80">
-            <div className="flex-1 relative">
-                <div className="absolute inset-0 overflow-y-auto">
-                    <Notebook shortId={shortId} editable={true} initialAutofocus="end" />
-                </div>
-            </div>
+        <div className="flex flex-col gap-2">
             <div className="flex justify-end">
                 <LemonButton type="secondary" size="small" to={urls.notebook(shortId)}>
                     Open notebook
                 </LemonButton>
+            </div>
+            <div className="flex-1">
+                <Notebook shortId={shortId} editable={true} initialAutofocus="end" />
             </div>
         </div>
     )
