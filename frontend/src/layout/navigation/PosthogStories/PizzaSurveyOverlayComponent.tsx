d22b08e8a3e945d969f36d2a54f9b016fce52598
edwinyjlim <edwinyjlim@gmail.com>
2025-08-06 09:49:12 -0400

    update hogtok id

--- frontend/src/layout/navigation/PosthogStories/PizzaSurveyOverlayComponent.tsx
+++ frontend/src/layout/navigation/PosthogStories/PizzaSurveyOverlayComponent.tsx
@@ -33,7 +33,7 @@ export const PizzaSurveyOverlayComponent = ({ closeOverlay }: PizzaSurveyOverlay
     return (
         <div className="flex flex-col h-full bg-primary p-8">
             <div className="flex-1 flex items-start justify-center">
-                <LemonButton id="pineapple-pizza-poll-button" type="secondary">
+                <LemonButton id="hogtok-pineapple-pizza-poll-button" type="secondary">
                     Take survey
                 </LemonButton>
             </div>
