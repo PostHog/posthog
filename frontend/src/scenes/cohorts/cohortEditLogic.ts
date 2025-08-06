f9682fd90fc8376d021e8fad6dd6471d0fc1fc3e
Shao Wei <lshaowei18@gmail.com>
2025-08-07 06:19:46 +0800

    fix: introduce function that updates cohort count

--- frontend/src/scenes/cohorts/cohortEditLogic.ts
+++ frontend/src/scenes/cohorts/cohortEditLogic.ts
@@ -73,6 +73,7 @@ export const cohortEditLogic = kea<cohortEditLogicType>([
         }),
         setQuery: (query: Node) => ({ query }),
         duplicateCohort: (asStatic: boolean) => ({ asStatic }),
+        updateCohortCount: (id: CohortType['id']) => ({ id }),
     }),
 
     reducers(({ props }) => ({
@@ -332,6 +333,13 @@ export const cohortEditLogic = kea<cohortEditLogicType>([
                         }
                     }
                     return processCohort(cohort)
+                },
+                updateCohortCount: async () => {
+                    const cohort = await api.cohorts.get(values.cohort.id)
+                    return {
+                        ...values.cohort,
+                        count: cohort.count,
+                    }
                 },
             },
         ],
