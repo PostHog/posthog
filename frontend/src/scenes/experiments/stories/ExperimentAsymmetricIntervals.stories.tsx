13f4563e4d11bb55c28007f0e7fc8e30f39fa7db
Anders <754494+andehen@users.noreply.github.com>
2025-08-06 12:33:38 +0200

    chore(experiments): remove new query runner feature flag (#36143)

--- frontend/src/scenes/experiments/stories/ExperimentAsymmetricIntervals.stories.tsx
+++ frontend/src/scenes/experiments/stories/ExperimentAsymmetricIntervals.stories.tsx
@@ -1,5 +1,4 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { FEATURE_FLAGS } from 'lib/constants'
 import { makeDelay } from 'lib/utils'
 import { App } from 'scenes/App'
 import { urls } from 'scenes/urls'
@@ -17,7 +16,6 @@ const meta: Meta = {
         layout: 'fullscreen',
         viewMode: 'story',
         mockDate: '2025-01-27',
-        featureFlags: [FEATURE_FLAGS.EXPERIMENTS_NEW_QUERY_RUNNER],
         pageUrl: urls.experiment(EXPERIMENT_WITH_ASYMMETRIC_INTERVALS.id),
     },
     decorators: [
