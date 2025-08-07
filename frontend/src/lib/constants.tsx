6b8b21a7a16c2cef56022646eb559c3ba2e9fdaa
Shao Wei <33112945+lshaowei18@users.noreply.github.com>
2025-08-07 20:26:49 +0800

    fix(insight alert): Clicking on new notification under CDP destination causes an error (#36172)
    Co-authored-by: MarconLP <13001502+MarconLP@users.noreply.github.com>

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -414,3 +414,7 @@ export const TAILWIND_BREAKPOINTS = {
     xl: 1200,
     '2xl': 1600,
 }
+
+export const INSIGHT_ALERT_FIRING_SUB_TEMPLATE_ID = 'insight-alert-firing'
+export const INSIGHT_ALERT_DESTINATION_LOGIC_KEY = 'insightAlertDestination'
+export const INSIGHT_ALERT_FIRING_EVENT_ID = '$insight_alert_firing'
