4a814351439e3ca8e65a9acd2c1318a65a77dd08
Shao Wei <lshaowei18@gmail.com>
2025-08-08 11:24:15 +0800

    fix: improve updateCohortCount typing

--- frontend/src/scenes/cohorts/cohortEditLogic.ts
+++ frontend/src/scenes/cohorts/cohortEditLogic.ts
@@ -73,7 +73,7 @@ export const cohortEditLogic = kea<cohortEditLogicType>([
         }),
         setQuery: (query: Node) => ({ query }),
         duplicateCohort: (asStatic: boolean) => ({ asStatic }),
-        updateCohortCount: (id: CohortType['id']) => ({ id }),
+        updateCohortCount: true,
     }),
 
     reducers(({ props }) => ({
