7c9c823875ac6d826568e86695905e153ee56e3d
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-08-08 14:59:14 +0200

    fix: Add a type hint

--- posthog/temporal/common/logger.py
+++ posthog/temporal/common/logger.py
@@ -381,7 +381,7 @@ def get_temporal_context() -> dict[str, str | int]:
 
     workflow_id, workflow_type, workflow_run_id, attempt = info
 
-    ctx = {
+    ctx: dict[str, str | int] = {
         "attempt": attempt,
         "workflow_id": workflow_id,
         "workflow_run_id": workflow_run_id,
