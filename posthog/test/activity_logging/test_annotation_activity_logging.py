0695a1cfa321553dfd6c1aeaf206f40bc90516c2
Yasen Slavov <yasen@posthog.com>
2025-08-08 14:32:59 +0300

    Simplify tests

--- posthog/test/activity_logging/test_annotation_activity_logging.py
+++ posthog/test/activity_logging/test_annotation_activity_logging.py
@@ -6,30 +6,19 @@ class TestAnnotationActivityLogging(ActivityLogTestHelper):
     def test_annotation_creation_activity_logging(self):
         annotation = self.create_annotation("Test annotation")
 
-        activity_logs = ActivityLog.objects.filter(
+        log = ActivityLog.objects.filter(
             team_id=self.team.id, scope="Annotation", item_id=str(annotation["id"]), activity="created"
-        )
-        self.assertEqual(activity_logs.count(), 1)
-        log = activity_logs.first()
+        ).first()
         self.assertIsNotNone(log)
         self.assertIsNotNone(log.detail)
 
     def test_annotation_update_activity_logging(self):
         annotation = self.create_annotation("Original annotation")
-        initial_log_count = ActivityLog.objects.filter(
-            team_id=self.team.id, scope="Annotation", item_id=str(annotation["id"])
-        ).count()
 
         self.update_annotation(annotation["id"], {"content": "Updated annotation"})
-
-        final_log_count = ActivityLog.objects.filter(
-            team_id=self.team.id, scope="Annotation", item_id=str(annotation["id"])
-        ).count()
-        self.assertEqual(final_log_count - initial_log_count, 1)
 
         update_log = ActivityLog.objects.filter(
             team_id=self.team.id, scope="Annotation", item_id=str(annotation["id"]), activity="updated"
         ).first()
         self.assertIsNotNone(update_log)
-        if update_log:
-            self.assertIsNotNone(update_log.detail)
+        self.assertIsNotNone(update_log.detail)
