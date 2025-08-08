e5624a471548657dfa640bc87b19721de4e77bf7
Shao Wei <lshaowei18@gmail.com>
2025-08-08 10:01:59 +0800

    fix: remove calculate radius after resetting

--- frontend/src/scenes/feature-flags/featureFlagReleaseConditionsLogic.ts
+++ frontend/src/scenes/feature-flags/featureFlagReleaseConditionsLogic.ts
@@ -406,9 +406,6 @@ export const featureFlagReleaseConditionsLogic = kea<featureFlagReleaseCondition
                 actions.setFlagKeysLoading(false)
             }
         },
-        resetAffectedUsers: () => {
-            actions.calculateBlastRadius()
-        },
     })),
     selectors({
         // Get the appropriate groups based on isSuper
