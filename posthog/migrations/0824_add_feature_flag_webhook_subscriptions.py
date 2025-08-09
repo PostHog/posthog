f671a36d64db32b5c0d586f89dffadc7576a9faa
Yago Dórea <yago.dorea@gmail.com>
2025-08-09 10:51:01 -0300

    fix max_migration

--- posthog/migrations/0824_add_feature_flag_webhook_subscriptions.py
+++ posthog/migrations/0824_add_feature_flag_webhook_subscriptions.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0815_sharingconfiguration_settings"),
+        ("posthog", "0820_organization_allow_publicly_shared_resources"),
     ]
 
     operations = [
