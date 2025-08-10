8497b7d284074dcfdbd506e725a4fdb8b172651d
Ben White <ben@posthog.com>
2025-08-10 14:46:25 +0200

    Fixes

--- frontend/src/lib/components/HedgehogMode/HedgehogMode.tsx
+++ frontend/src/lib/components/HedgehogMode/HedgehogMode.tsx
@@ -4,16 +4,27 @@ import { lazy, Suspense } from 'react'
 
 import { hedgehogModeLogic } from './hedgehogModeLogic'
 import { themeLogic } from '~/layout/navigation-3000/themeLogic'
+import { toolbarConfigLogic } from '~/toolbar/toolbarConfigLogic'
+import { inStorybook } from 'lib/utils'
 
 export const HedgeHogModeRenderer =
     typeof window !== 'undefined'
         ? lazy(() => import('@posthog/hedgehog-mode').then((module) => ({ default: module.HedgehogModeRenderer })))
         : () => null
 
-const config: HedgehogModeConfig = {
-    assetsUrl: '/static/hedgehog-mode/',
-    platformSelector:
-        '.border, .border-t, .LemonButton--primary, .LemonButton--secondary:not(.LemonButton--status-alt:not(.LemonButton--active)), .LemonInput, .LemonSelect, .LemonTable, .LemonSwitch--bordered, .LemonBanner',
+const getAssetsUrl = (): string => {
+    let path = `/static/hedgehog-mode/`
+    const toolbarAPIUrl = toolbarConfigLogic.findMounted()?.values.apiURL
+
+    if (inStorybook()) {
+        // Nothing to do
+    } else if (window.location.hostname !== 'localhost') {
+        path = `https://us.posthog.com${path}`
+    } else if (toolbarAPIUrl) {
+        path = `${toolbarAPIUrl}${path}`
+    }
+
+    return path
 }
 
 export type HedgehogModeProps = {
@@ -28,6 +39,12 @@ export function HedgehogMode({ enabledOverride }: HedgehogModeProps): JSX.Elemen
     const { isDarkModeOn } = useValues(themeLogic)
 
     const enabled = enabledOverride ?? hedgehogModeEnabled
+
+    const config: HedgehogModeConfig = {
+        assetsUrl: getAssetsUrl(),
+        platformSelector:
+            '.border, .border-t, .LemonButton--primary, .LemonButton--secondary:not(.LemonButton--status-alt:not(.LemonButton--active)), .LemonInput, .LemonSelect, .LemonTable, .LemonSwitch--bordered, .LemonBanner',
+    }
 
     return typeof window !== 'undefined' && enabled ? (
         <Suspense fallback={<span>Loading...</span>}>
