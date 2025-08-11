1125e5acc21e867621cde779f0bf00f8c0e1e5eb
denakorita <denada@posthog.com>
2025-08-11 17:25:41 +0200

    small refactoring

--- ee/hogai/graph/schema_generator/utils.py
+++ ee/hogai/graph/schema_generator/utils.py
@@ -1,9 +1,9 @@
 from typing import Generic, Optional, TypeVar
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 T = TypeVar("T", bound=BaseModel)
 
 
 class SchemaGeneratorOutput(BaseModel, Generic[T]):
-    query: Optional[T] = Field(description="The final SQL query to be executed.", default=None)
+    query: Optional[T] = None
