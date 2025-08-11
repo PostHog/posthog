d2368b1607aff4cb92e956237bb738b97090ba04
Haven <haven@posthog.com>
2025-08-11 13:36:28 -0500

    tweak

--- products/messaging/frontend/Campaigns/hogflows/components/HogFlowEditorPanel.tsx
+++ products/messaging/frontend/Campaigns/hogflows/components/HogFlowEditorPanel.tsx
@@ -9,14 +9,14 @@ export function HogFlowEditorPanel({ position, children }: HogFlowEditorPanelPro
     return (
         <div
             className={clsx(
-                'react-flow__panel flex flex-col top max-h-full m-0',
+                'react-flow__panel flex flex-col top-2 bottom-2 right-2 m-0 pb-2 overflow-hidden',
                 position.includes('right') ? 'right' : 'left',
                 position.includes('bottom') ? 'justify-end' : 'justify-start'
             )}
         >
-            <div className="relative flex z-10 flex-col max-h-full m-2 rounded bg-surface-primary">
+            <div className="relative flex flex-col z-10 rounded bg-surface-primary max-h-full">
                 <div
-                    className="relative z-10 flex flex-col flex-1 rounded-md overflow-hidden"
+                    className="flex flex-col rounded-md overflow-hidden"
                     style={{
                         border: '1px solid var(--border)',
                         boxShadow: '0 3px 0 var(--border)',
