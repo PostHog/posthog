4f425ce21e2088be8543c45afd5b3bdd3f3d4994
Ben White <ben@posthog.com>
2025-08-12 16:46:19 +0100

    fix

--- products/messaging/frontend/Channels/EmailSetup/EmailSetupModal.tsx
+++ products/messaging/frontend/Channels/EmailSetup/EmailSetupModal.tsx
@@ -128,7 +128,7 @@ export const EmailSetupModal = (props: EmailSetupModalLogicProps): JSX.Element =
     }
 
     return (
-        <LemonModal title="Configure email domain" width="auto" onClose={props.onComplete}>
+        <LemonModal title="Configure email sender" width="auto" onClose={props.onComplete}>
             {modalContent}
         </LemonModal>
     )
