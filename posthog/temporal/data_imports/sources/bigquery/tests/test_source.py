0bb8cce62a0b7c9e7ebc4d2904ba2337d3e633ce
Tom Owers <owerstom@gmail.com>
2025-08-13 14:59:38 +0100

    Add test

--- posthog/temporal/data_imports/sources/bigquery/tests/test_source.py
+++ posthog/temporal/data_imports/sources/bigquery/tests/test_source.py
@@ -11,3 +11,14 @@ def test_bigquery_get_schemas():
         schemas = source_cls.get_schemas(mock.ANY, 1)
         assert len(schemas) == 1
         assert schemas[0].name == "table"
+
+
+def test_bigquery_get_schemas_with_existing_destination_tables():
+    with mock.patch(
+        "posthog.temporal.data_imports.sources.bigquery.source.get_bigquery_schemas",
+        return_value={"table": [], "__posthog_import_0000_0000": []},
+    ):
+        source_cls = BigQuerySource()
+        schemas = source_cls.get_schemas(mock.ANY, 1)
+        assert len(schemas) == 1
+        assert schemas[0].name == "table"
