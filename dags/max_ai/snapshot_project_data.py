8c1de67205574b944b37e730f8c2d4bec40ec075
Georgiy Tarasov <skoob17@mail.ru>
2025-08-13 11:52:01 +0200

    test: dumps

--- dags/max_ai/snapshot_project_data.py
+++ dags/max_ai/snapshot_project_data.py
@@ -1,16 +1,12 @@
-from collections.abc import Sequence
-from contextlib import contextmanager
-from tempfile import TemporaryFile
+from typing import TypeVar
 
 import dagster
 from dagster_aws.s3 import S3Resource
-from django.conf import settings
-from fastavro import parse_schema, writer
-from pydantic_avro import AvroBase
 
 from dags.common import JobOwners
-from dags.max_ai.utils import compose_postgres_dump_path
+from dags.max_ai.utils import check_dump_exists, compose_postgres_dump_path, dump_model
 from ee.hogai.eval.schema import (
+    BaseSchema,
     DataWarehouseTableSchema,
     GroupTypeMappingSchema,
     PostgresProjectDataSnapshot,
@@ -26,53 +22,27 @@ DEFAULT_RETRY_POLICY = dagster.RetryPolicy(
 )
 
 
-@contextmanager
-def dump_model(*, s3: S3Resource, schema: type[AvroBase], file_key: str):
-    with TemporaryFile() as f:
-        parsed_schema = parse_schema(schema.avro_schema())
-
-        def dump(models: Sequence[AvroBase]):
-            writer(f, parsed_schema, (model.model_dump() for model in models))
-
-        yield dump
-
-        f.seek(0)
-        s3.get_client().upload_fileobj(f, settings.OBJECT_STORAGE_BUCKET, file_key)
-
-
 SnapshotModelOutput = tuple[str, str]
 
 
-def snapshot_project(s3: S3Resource, project_id: int, code_version: str | None = None) -> SnapshotModelOutput:
-    file_key = compose_postgres_dump_path(project_id, "team", code_version)
-    with dump_model(s3=s3, schema=TeamSchema, file_key=file_key) as dump:
-        dump(TeamSchema.serialize_for_project(project_id))
-    return "project", file_key
+SchemaBound = TypeVar("SchemaBound", bound=BaseSchema)
 
 
-def snapshot_property_definitions(
-    s3: S3Resource, project_id: int, code_version: str | None = None
+def snapshot_postgres_model(
+    context: dagster.OpExecutionContext,
+    model_type: type[SchemaBound],
+    file_name: str,
+    s3: S3Resource,
+    project_id: int,
+    code_version: str | None = None,
 ) -> SnapshotModelOutput:
-    file_key = compose_postgres_dump_path(project_id, "prop_defs", code_version)
-    with dump_model(s3=s3, schema=PropertyDefinitionSchema, file_key=file_key) as dump:
-        dump(PropertyDefinitionSchema.serialize_for_project(project_id))
-    return "property_definitions", file_key
-
-
-def snapshot_group_type_mappings(
-    s3: S3Resource, project_id: int, code_version: str | None = None
-) -> SnapshotModelOutput:
-    file_key = compose_postgres_dump_path(project_id, "group_type_mappings", code_version)
-    with dump_model(s3=s3, schema=GroupTypeMappingSchema, file_key=file_key) as dump:
-        dump(GroupTypeMappingSchema.serialize_for_project(project_id))
-    return "group_type_mappings", file_key
-
-
-def snapshot_data_warehouse_tables(s3: S3Resource, project_id: int, code_version: str | None = None):
-    file_key = compose_postgres_dump_path(project_id, "dwh_tables", code_version)
-    with dump_model(s3=s3, schema=DataWarehouseTableSchema, file_key=file_key) as dump:
-        dump(DataWarehouseTableSchema.serialize_for_project(project_id))
-    return "data_warehouse_tables", file_key
+    file_key = compose_postgres_dump_path(project_id, file_name, code_version)
+    if check_dump_exists(s3, file_key):
+        context.log.info(f"Skipping {file_key} because it already exists")
+        return file_key
+    with dump_model(s3=s3, schema=model_type, file_key=file_key) as dump:
+        dump(model_type.serialize_for_project(project_id))
+    return file_key
 
 
 @dagster.op(
@@ -85,14 +55,16 @@ def snapshot_postgres_project_data(
     context: dagster.OpExecutionContext, project_id: int, s3: S3Resource
 ) -> PostgresProjectDataSnapshot:
     context.log.info(f"Snapshotting Postgres project data for {project_id}")
-    deps = dict(
-        (
-            snapshot_project(s3, project_id, context.op_def.version),
-            snapshot_property_definitions(s3, project_id, context.op_def.version),
-            snapshot_group_type_mappings(s3, project_id, context.op_def.version),
-            snapshot_data_warehouse_tables(s3, project_id, context.op_def.version),
-        )
-    )
+    snapshot_map: dict[str, type[SchemaBound]] = {
+        "project": TeamSchema,
+        "property_definitions": PropertyDefinitionSchema,
+        "group_type_mappings": GroupTypeMappingSchema,
+        "data_warehouse_tables": DataWarehouseTableSchema,
+    }
+    deps = {
+        file_name: snapshot_postgres_model(context, model_type, file_name, s3, project_id, context.op_def.version)
+        for file_name, model_type in snapshot_map.items()
+    }
     context.log_event(
         dagster.AssetMaterialization(
             asset_key="project_postgres_snapshots",
