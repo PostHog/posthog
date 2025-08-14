e6febe19c6e25947a2ed5deb0cc2ef6df0d601f3
eric <eric@posthog.com>
2025-08-14 10:22:16 -0700

    add flag

--- posthog/models/cache.py
+++ posthog/models/cache.py
@@ -1,12 +1,45 @@
 from typing import Optional
 from django.core import serializers
 from django.db.models import QuerySet, Manager
+import posthoganalytics
 from posthog.exceptions_capture import capture_exception
 from posthog.git import get_git_commit_short
 from posthog.redis import get_client
 from posthog.settings import TEST
 
 TEST_OVERRIDE = False
+
+
+# temporary for rollout purposes
+def is_cache_enabled(team_id: int) -> bool:
+    """
+    Use the hogql database cache.
+    """
+    from posthog.models.team import Team
+
+    if not Team.objects.filter(id=team_id).exists():
+        return False
+
+    team = Team.objects.get(id=team_id)
+
+    return posthoganalytics.feature_enabled(
+        "hogql-database-cache",
+        str(team.uuid),
+        groups={
+            "organization": str(team.organization_id),
+            "project": str(team.id),
+        },
+        group_properties={
+            "organization": {
+                "id": str(team.organization_id),
+            },
+            "project": {
+                "id": str(team.id),
+            },
+        },
+        only_evaluate_locally=True,
+        send_feature_flag_events=False,
+    )
 
 
 class CachedQuerySet(QuerySet):
@@ -20,9 +53,12 @@ class CachedQuerySet(QuerySet):
         # cache key based on sha to invalidate cache on deploys in case of migrations
         return key
 
-    def fetch_cached(self, team_id: int, timeout: int = 300, key_prefix: Optional[str] = None):
+    def fetch_cached(self, team_id: int, timeout: int = 600, key_prefix: Optional[str] = None):
         # we want the behavior for tests to be unaffected unless specifically testing this logic
         if TEST and not TEST_OVERRIDE:
+            return list(self)
+
+        if not is_cache_enabled(team_id):
             return list(self)
 
         try:
