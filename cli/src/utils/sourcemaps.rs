148ef35810184d7944e47fd2dbf1f97aeb5a7b55
Hugues Pouillot <hpouillot@gmail.com>
2025-08-14 16:40:27 +0200

    fix url encoded

--- cli/src/utils/sourcemaps.rs
+++ cli/src/utils/sourcemaps.rs
@@ -195,7 +195,8 @@ pub fn get_sourcemap_reference(lines: Lines) -> Result<Option<String>> {
     for line in lines.rev() {
         if line.starts_with("//# sourceMappingURL=") || line.starts_with("//@ sourceMappingURL=") {
             let url = str::from_utf8(&line.as_bytes()[21..])?.trim().to_owned();
-            return Ok(Some(url));
+            let decoded_url = urlencoding::decode(&url)?;
+            return Ok(Some(decoded_url.into_owned()));
         }
     }
     Ok(None)
