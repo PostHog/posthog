52bc64a0f2fab266c3bd4dce01377e3c8ea1d99c
Michael Matloka <michael@matloka.com>
2025-08-14 21:21:21 +0200

    Optimize query

--- posthog/hogql_queries/ai/traces_query_runner.py
+++ posthog/hogql_queries/ai/traces_query_runner.py
@@ -156,7 +156,9 @@ class TracesQueryRunner(QueryRunner):
                     arraySort(x -> x.3,
                         groupArrayIf(
                             tuple(uuid, event, timestamp, properties),
-                            event IN ('$ai_metric','$ai_feedback') OR (event = '$ai_generation' )
+                            event IN ('$ai_metric', '$ai_feedback') OR (
+                                event = '$ai_generation' AND properties.$ai_parent_id = properties.$ai_trace_id
+                            )
                         )
                     )
                 ) AS events,
