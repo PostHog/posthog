209ad892f96813991faf2375644b9476281abc43
Thomas Obermüller <thomas.obermueller@gmail.com>
2025-08-14 15:19:59 +0200

    fix test

--- posthog/api/test/test_insight.py
+++ posthog/api/test/test_insight.py
@@ -383,7 +383,7 @@ class TestInsight(ClickhouseTestMixin, APIBaseTest, QueryMatchingTest):
                 execution_mode=ExecutionMode.EXTENDED_CACHE_CALCULATE_ASYNC_IF_STALE,
                 team=self.team,
                 user=mock.ANY,
-                filters_override=None,
+                filters_override={},
                 variables_override={},
             )
 
@@ -397,7 +397,7 @@ class TestInsight(ClickhouseTestMixin, APIBaseTest, QueryMatchingTest):
                 execution_mode=ExecutionMode.RECENT_CACHE_CALCULATE_BLOCKING_IF_STALE,
                 team=self.team,
                 user=mock.ANY,
-                filters_override=None,
+                filters_override={},
                 variables_override={},
             )
 
