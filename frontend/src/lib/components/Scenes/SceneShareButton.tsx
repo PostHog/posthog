12eb68ca07a98d0df54bcd3d75e8f3bda67235db
Adam Leith <adamleithp@users.noreply.github.com>
2025-08-19 11:16:02 +0100

    fix(ux): info panel nits + fix for cohort edit description (#36823)

--- frontend/src/lib/components/Scenes/SceneShareButton.tsx
+++ frontend/src/lib/components/Scenes/SceneShareButton.tsx
@@ -1,16 +1,18 @@
+import { IconShare } from '@posthog/icons'
+
 import { ButtonPrimitive, ButtonPrimitiveProps } from 'lib/ui/Button/ButtonPrimitives'
 
 import { SceneDataAttrKeyProps } from './utils'
 
 type SceneShareButtonProps = SceneDataAttrKeyProps & {
     buttonProps?: Omit<ButtonPrimitiveProps, 'children' | 'data-attr'>
-    children?: React.ReactNode
 }
 
-export function SceneShareButton({ buttonProps, children, dataAttrKey }: SceneShareButtonProps): JSX.Element {
+export function SceneShareButton({ buttonProps, dataAttrKey }: SceneShareButtonProps): JSX.Element {
     return (
         <ButtonPrimitive {...buttonProps} data-attr={`${dataAttrKey}-share-button`}>
-            {children}
+            <IconShare />
+            Share or embed
         </ButtonPrimitive>
     )
 }
