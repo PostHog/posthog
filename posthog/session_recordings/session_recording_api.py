4d18efb81b9bf2022b06fcc8b74072123ab8f1e8
Paul D'Ambra <paul@posthog.com>
2025-08-21 00:14:34 +0100

    fix: not correctly detecting lts paths any more when validating blob … (#36957)

--- posthog/session_recordings/session_recording_api.py
+++ posthog/session_recordings/session_recording_api.py
@@ -376,8 +376,8 @@ class SessionRecordingSnapshotsRequestSerializer(serializers.Serializer):
             if blob_key and (start_blob_key or end_blob_key):
                 raise serializers.ValidationError("Must provide a single blob key or start and end blob keys, not both")
 
-            if blob_key and blob_key.startswith("/"):
-                # blob key that starts with / is (probably) an LTS path
+            if blob_key and "/" in blob_key:
+                # blob key that has any / is (probably) an LTS path
                 pass
             else:
                 if start_blob_key and not end_blob_key:
