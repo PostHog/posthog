ed9204c6143cc7702ce26cf3056c0abdcae32ea6
Pawel Szczur <pawel@posthog.com>
2025-08-20 18:01:53 +0200

    remove alter arg

--- posthog/clickhouse/migrations/0134_recreate_query_log_archive_mv.py
+++ posthog/clickhouse/migrations/0134_recreate_query_log_archive_mv.py
@@ -8,7 +8,8 @@ from posthog.clickhouse.query_log_archive import (
 operations = [
     # Drop the old materialized view
     run_sql_with_exceptions(
-        DROP_QUERY_LOG_ARCHIVE_MV(on_cluster=False), node_role=NodeRole.ALL, is_alter_on_replicated_table=True
+        DROP_QUERY_LOG_ARCHIVE_MV(on_cluster=False),
+        node_role=NodeRole.ALL,
     ),
     run_sql_with_exceptions(
         QUERY_LOG_ARCHIVE_NEW_MV(view_name="query_log_archive_mv", dest_table="query_log_archive", on_cluster=False),
