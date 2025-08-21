63af8ded83c5955c35ca880610d1d17fa76a4ff6
Alexander Spicer <aspicer@gmail.com>
2025-08-20 22:40:08 -0700

    from_team

--- posthog/models/team_dao_django.py
+++ posthog/models/team_dao_django.py
@@ -18,6 +18,21 @@ class DjangoTeamDAO(TeamDAO):
     it to immutable TeamDTO objects for use within the HogQL subsystem.
     """
 
+    @classmethod
+    def from_team(cls, team: Team) -> TeamDTO:
+        """Convert a Django Team model instance to a TeamDTO.
+
+        This is a convenience method for migrating existing code that has
+        Team instances but needs to use TeamDTO.
+
+        Args:
+            team: Django Team model instance
+
+        Returns:
+            Immutable TeamDTO object
+        """
+        return cls()._convert_to_dto(team)
+
     def get_by_id(self, team_id: int) -> Optional[TeamDTO]:
         """Retrieve a team by its ID using Django ORM.
 
@@ -47,8 +62,8 @@ class DjangoTeamDAO(TeamDAO):
         person_on_events_mode = team.person_on_events_mode.value
         default_modifiers = team.default_modifiers
 
-        # Get timezone info as string representation
-        timezone_info = str(team.timezone_info) if hasattr(team, "timezone_info") else team.timezone
+        # Get timezone info as ZoneInfo object
+        timezone_info = team.timezone_info
 
         # Get path cleaning filter models as serialized data
         try:
@@ -72,6 +87,8 @@ class DjangoTeamDAO(TeamDAO):
             modifiers=team.modifiers,
             test_account_filters=team.test_account_filters,
             path_cleaning_filters=team.path_cleaning_filters,
+            base_currency=team.base_currency,
+            revenue_analytics_config=team.revenue_analytics_config,
             organization_id=team.organization.id,
             person_on_events_mode_flag_based_default=person_on_events_mode_flag_based_default,
             person_on_events_mode=person_on_events_mode,
