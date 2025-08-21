c5a40a5cf90dac62caf5b90414ffcc2312e09dd6
pauldambra <paul@posthog.com>
2025-08-21 10:02:32 +0100

    fix: performance of active replay users view

--- frontend/src/scenes/session-recordings/components/replayActiveUsersTableLogic.ts
+++ frontend/src/scenes/session-recordings/components/replayActiveUsersTableLogic.ts
@@ -23,18 +23,47 @@ export const replayActiveUsersTableLogic = kea<replayActiveUsersTableLogicType>(
         countedUsers: {
             loadCountedUsers: async (_, breakpoint): Promise<{ person: PersonType; count: number }[]> => {
                 const q = hogql`
-                    select p, any (pp), count () as c
-                    from (
-                        select any (person_id) as p, any (person.properties) as pp
-                        from raw_session_replay_events
-                        where min_first_timestamp <= now()
-                        and min_first_timestamp >= now() - toIntervalDay(7)
-                        group by session_id
-                        having date_diff('second', min (min_first_timestamp), max (max_last_timestamp)) > 5000
-                        ) as q
-                    group by p
-                    order by c desc
-                        limit 10
+                  WITH
+session_distinct_ids AS (
+    SELECT any(distinct_id) AS di
+    FROM raw_session_replay_events
+    WHERE min_first_timestamp <= now()
+      AND min_first_timestamp >= now() - toIntervalDay(7)
+    GROUP BY session_id
+    HAVING date_diff('second', min(min_first_timestamp), max(max_last_timestamp)) > 5
+),
+
+person_ids as (
+  SELECT
+    p.id AS person_id, p.properties as person_properties, p.pdi.distinct_id as the_pdi
+  FROM persons p
+  where p.pdi.distinct_id in (select di from session_distinct_ids)
+),
+
+counted_sessions AS (
+    SELECT di AS sess_di, count() AS c
+    FROM (
+        SELECT any(distinct_id) AS di
+        FROM raw_session_replay_events
+        WHERE min_first_timestamp <= now()
+          AND min_first_timestamp >= now() - toIntervalDay(7)
+          AND distinct_id IN (SELECT di FROM session_distinct_ids)
+        GROUP BY session_id
+        HAVING date_diff('second', min(min_first_timestamp), max(max_last_timestamp)) > 5
+    )
+    GROUP BY sess_di
+    ORDER BY c DESC
+    LIMIT 5000
+)
+SELECT
+    pi.person_id,
+    pi.person_properties,
+    cs.c AS count
+FROM counted_sessions AS cs
+ANY LEFT JOIN person_ids AS pi
+    ON pi.the_pdi = cs.sess_di
+  order by count desc
+  limit 10
                 `
 
                 const qResponse = await api.queryHogQL(q)
