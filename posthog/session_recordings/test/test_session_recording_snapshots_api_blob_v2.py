490f28b8d18d95fdb0b820691447c3c0f27fc0df
pauldambra <paul@posthog.com>
2025-08-21 17:12:27 +0100

    fix

--- posthog/session_recordings/test/test_session_recording_snapshots_api_blob_v2.py
+++ posthog/session_recordings/test/test_session_recording_snapshots_api_blob_v2.py
@@ -1,5 +1,5 @@
 import datetime
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 
 from freezegun import freeze_time
 from parameterized import parameterized
@@ -21,6 +21,8 @@ from posthog.test.base import (
     ClickhouseTestMixin,
     QueryMatchingTest,
 )
+from dateutil.relativedelta import relativedelta
+from django.utils.timezone import now
 
 
 class TestSessionRecordingSnapshotsAPI(APIBaseTest, ClickhouseTestMixin, QueryMatchingTest):
@@ -105,6 +107,9 @@ class TestSessionRecordingSnapshotsAPI(APIBaseTest, ClickhouseTestMixin, QueryMa
             ("invalid_source", status.HTTP_400_BAD_REQUEST),
             ("", status.HTTP_400_BAD_REQUEST),
             ("BLOB_V2", status.HTTP_400_BAD_REQUEST),  # Case-sensitive
+            ("blob", status.HTTP_400_BAD_REQUEST),  # 404 because we didn't mock the right things for a 200
+            ("realtime", status.HTTP_400_BAD_REQUEST),
+            ("BLOB", status.HTTP_400_BAD_REQUEST),  # Case-sensitive
         ]
     )
     @patch(
@@ -337,3 +342,70 @@ class TestSessionRecordingSnapshotsAPI(APIBaseTest, ClickhouseTestMixin, QueryMa
         response = self.client.get(url)
         assert response.status_code == status.HTTP_404_NOT_FOUND
         assert "Block index out of range" in response.json()["detail"]
+
+    @patch("posthog.session_recordings.session_recording_api.SessionRecording.get_or_build")
+    @patch("posthog.session_recordings.session_recording_api.stream_from")
+    def test_cannot_get_session_recording_blob_for_made_up_sessions(
+        self, _mock_stream_from, mock_get_session_recording
+    ) -> None:
+        session_id = str(uuid7())
+        blob_key = f"1682608337071"
+        url = f"/api/projects/{self.team.pk}/session_recordings/{session_id}/snapshots/?source=blob_v2&blob_key={blob_key}"
+
+        # by default a session recording is deleted, and _that_ is what we check for to see if it exists
+        # so, we have to explicitly mark the mock as deleted
+        mock_get_session_recording.return_value = SessionRecording(session_id=session_id, team=self.team, deleted=True)
+
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_can_not_get_session_recording_blob_that_does_not_exist(self) -> None:
+        session_id = str(uuid7())
+        blob_key = f"session_recordings/team_id/{self.team.pk}/session_id/{session_id}/data/1682608337071"
+        url = f"/api/projects/{self.team.pk}/session_recordings/{session_id}/snapshots/?source=blob_v2&blob_key={blob_key}"
+
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    # checks that we 404 without patching the "exists" check
+    # that is patched in other tests or freezing time doesn't work
+    def test_404_when_no_snapshots(self) -> None:
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/session_recordings/1/snapshots?",
+        )
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @patch("posthoganalytics.capture")
+    def test_snapshots_api_called_with_personal_api_key(self, mock_capture):
+        session_id = str(uuid7())
+        self.produce_replay_summary("user", session_id, now() - relativedelta(days=1))
+
+        personal_api_key = generate_random_token_personal()
+        PersonalAPIKey.objects.create(
+            label="X",
+            user=self.user,
+            last_used_at="2021-08-25T21:09:14",
+            secure_value=hash_key_value(personal_api_key),
+            scopes=["session_recording:read"],
+            scoped_teams=[self.team.pk],
+        )
+
+        response = self.client.get(
+            f"/api/projects/{self.team.id}/session_recordings/{session_id}/snapshots",
+            HTTP_AUTHORIZATION=f"Bearer {personal_api_key}",
+        )
+        assert response.status_code == status.HTTP_200_OK, response.json()
+
+        assert mock_capture.call_args_list[0] == call(
+            event="snapshots_api_called_with_personal_api_key",
+            distinct_id=self.user.distinct_id,
+            properties={
+                "key_label": "X",
+                "key_scopes": ["session_recording:read"],
+                "key_scoped_teams": [self.team.pk],
+                "session_requested": session_id,
+                # none because it's all mock data
+                "recording_start_time": None,
+                "source": "listing",
+            },
+        )
