66fa1657dae197b8198b5e9080b8b5f3e8d41d22
Lucas Ricoy <lucas.r@posthog.com>
2025-08-22 16:27:37 -0300

    chore: lower max partitions per run

--- dags/web_preaggregated.py
+++ dags/web_preaggregated.py
@@ -28,7 +28,7 @@ from posthog.models.web_preaggregated.sql import (
 )
 
 MAX_PARTITIONS_PER_RUN_ENV_VAR = "DAGSTER_WEB_PREAGGREGATED_MAX_PARTITIONS_PER_RUN"
-max_partitions_per_run = int(os.getenv(MAX_PARTITIONS_PER_RUN_ENV_VAR, 14))
+max_partitions_per_run = int(os.getenv(MAX_PARTITIONS_PER_RUN_ENV_VAR, 1))
 backfill_policy_def = BackfillPolicy.multi_run(max_partitions_per_run=max_partitions_per_run)
 partition_def = DailyPartitionsDefinition(start_date="2024-01-01", end_offset=1)
 
