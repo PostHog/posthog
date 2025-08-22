bdd4358319f766b12240599c7bc4c24373e33baf
Michael Matloka <michael@matloka.com>
2025-08-22 18:47:01 +0200

    Set EVALS_DIND_OBJECT_STORAGE_ENDPOINT default

--- dags/max_ai/run_evaluation.py
+++ dags/max_ai/run_evaluation.py
@@ -24,7 +24,7 @@ def get_object_storage_endpoint() -> str:
     Production mode uses the AWS S3.
     """
     if settings.DEBUG:
-        val = dagster.EnvVar("EVALS_DIND_OBJECT_STORAGE_ENDPOINT").get_value()
+        val = dagster.EnvVar("EVALS_DIND_OBJECT_STORAGE_ENDPOINT").get_value("http://objectstorage.posthog.orb.local")
         if not val:
             raise ValueError("EVALS_DIND_OBJECT_STORAGE_ENDPOINT is not set")
         return val
