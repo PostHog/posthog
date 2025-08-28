37eb9e939c0b1de61e9c8aae7585277ee606cd1f
pauldambra <paul@posthog.com>
2025-08-28 15:45:17 +0100

    fix

--- posthog/clickhouse/migrations/0104_session_replay_events_v2_test_alter_snapshot_source.py
+++ posthog/clickhouse/migrations/0104_session_replay_events_v2_test_alter_snapshot_source.py
@@ -1,8 +1,4 @@
-from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.session_recordings.sql.session_replay_event_v2_test_migrations_sql import (
-    REMOVE_SNAPSHOT_SOURCE_LOW_CARDINALITY_SQL,
-)
-
 operations = [
-    run_sql_with_exceptions(REMOVE_SNAPSHOT_SOURCE_LOW_CARDINALITY_SQL(on_cluster=True)),
+    # this was used for testing the migration to session replay v2 ingestion
+    # the migration is kept only for history / numbering
 ]
--- posthog/clickhouse/migrations/0136_drop_session_replay_events_v2_test_table.py
+++ posthog/clickhouse/migrations/0136_drop_session_replay_events_v2_test_table.py
@@ -1,7 +1,11 @@
+from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.session_recordings.sql.session_replay_event_v2_test_sql import (
     DROP_SESSION_REPLAY_EVENTS_V2_TEST_TABLE_SQL,
     SESSION_REPLAY_EVENTS_V2_TEST_TABLES,
 )
 
 # NB the kafka and mv tables are first in this list
-operations = [DROP_SESSION_REPLAY_EVENTS_V2_TEST_TABLE_SQL(t) for t in SESSION_REPLAY_EVENTS_V2_TEST_TABLES]
+operations = [
+    run_sql_with_exceptions(DROP_SESSION_REPLAY_EVENTS_V2_TEST_TABLE_SQL(t))
+    for t in SESSION_REPLAY_EVENTS_V2_TEST_TABLES
+]
