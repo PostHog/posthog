f0dcec9852e3d2c9bd8db27256444ba8f34258e6
pauldambra <paul@posthog.com>
2025-08-28 15:54:20 +0100

    fix

--- posthog/clickhouse/migrations/0136_drop_session_replay_events_v2_test_table.py
+++ posthog/clickhouse/migrations/0136_drop_session_replay_events_v2_test_table.py
@@ -1,10 +1,10 @@
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 
-# NB the mv then kafka tables are first in this list on purpose
+# NB the order of operations are important here
 operations = [
     run_sql_with_exceptions("DROP TABLE IF EXISTS session_replay_events_v2_test_mv"),
     run_sql_with_exceptions("DROP TABLE IF EXISTS kafka_session_replay_events_v2_test"),
-    run_sql_with_exceptions("DROP TABLE IF EXISTS session_replay_events_v2_test"),
-    run_sql_with_exceptions("DROP TABLE IF EXISTS writable_session_replay_events_v2_test"),
     run_sql_with_exceptions("DROP TABLE IF EXISTS sharded_session_replay_events_v2_test"),
+    run_sql_with_exceptions("DROP TABLE IF EXISTS writable_session_replay_events_v2_test"),
+    run_sql_with_exceptions("DROP TABLE IF EXISTS session_replay_events_v2_test"),
 ]
