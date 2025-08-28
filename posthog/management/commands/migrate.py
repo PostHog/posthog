bb09d6a1624d1f151e87e8be0d036bd5e7f32e78
nickbest-ph <nick@posthog.com>
2025-08-28 16:26:43 -0700

    rms migrate patch

--- posthog/management/commands/migrate.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.commands.migrate import Command as MigrateCommand
-
-
-class Command(MigrateCommand):
-    def handle(self, *args, **options):
-        return super().handle(*args, **options)
