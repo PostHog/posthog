822d07e9316f32704696dab6d1f76c781ae8466e
pauldambra <paul@posthog.com>
2025-08-28 13:54:34 +0100

    fix

--- posthog/test/test_get_context_for_template.py
+++ posthog/test/test_get_context_for_template.py
@@ -1,7 +1,7 @@
+from posthog.test.base import APIBaseTest
 from unittest import mock
 from unittest.mock import MagicMock
 
-from posthog.test.base import APIBaseTest
 from posthog.utils import get_context_for_template
 
 
