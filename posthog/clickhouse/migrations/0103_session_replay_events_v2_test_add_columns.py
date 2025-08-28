46969caa289dae460abd6d54e0da35bc81447412
pauldambra <paul@posthog.com>
2025-08-28 19:42:16 +0100

    fix

--- posthog/clickhouse/migrations/0103_session_replay_events_v2_test_add_columns.py
+++ posthog/clickhouse/migrations/0103_session_replay_events_v2_test_add_columns.py
@@ -1,4 +1,6 @@
-operations = [
+from infi.clickhouse_orm import Operation
+
+operations: list[Operation] = [
     # this was used for testing the migration to session replay v2 ingestion
     # the migration is kept only for history / numbering
 ]
