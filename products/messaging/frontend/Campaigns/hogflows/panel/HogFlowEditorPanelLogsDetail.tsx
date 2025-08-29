98228e8b555bf9493b05311621b016c199de76dd
Ben White <ben@posthog.com>
2025-08-29 15:13:44 +0100

    fix

--- products/messaging/frontend/Campaigns/hogflows/panel/HogFlowEditorPanelLogsDetail.tsx
+++ products/messaging/frontend/Campaigns/hogflows/panel/HogFlowEditorPanelLogsDetail.tsx
@@ -2,11 +2,11 @@ import { useValues } from 'kea'
 
 import { LogsViewer } from 'scenes/hog-functions/logs/LogsViewer'
 
+import { renderWorkflowLogMessage } from '../../logs/log-utils'
 import { hogFlowEditorLogic } from '../hogFlowEditorLogic'
 
 export function HogFlowEditorPanelLogsDetail(): JSX.Element | null {
     const { selectedNode, campaign } = useValues(hogFlowEditorLogic)
-    const id = selectedNode?.data.id ?? 'unknown'
 
     return (
         <div className="p-2 flex flex-col gap-2 overflow-hidden">
@@ -14,6 +14,7 @@ export function HogFlowEditorPanelLogsDetail(): JSX.Element | null {
                 sourceType="hog_flow"
                 sourceId={campaign.id}
                 renderColumns={(columns) => columns.filter((c) => c.key !== 'instanceId')}
+                renderMessage={(m) => renderWorkflowLogMessage(campaign, m)}
             />
         </div>
     )
