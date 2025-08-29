c5b92d584854a81224a489f29ef07ecc2b692d5b
Yasen <yasen@posthog.com>
2025-08-29 16:07:15 +0300

    Revert "feat: Enforce 2FA on the backend (#37176)"
    This reverts commit 37b101b83efe259cbedb9b45a8ffb2b6489b315d.

--- posthog/api/test/test_feature_flag.py
+++ posthog/api/test/test_feature_flag.py
@@ -2395,7 +2395,7 @@ class TestFeatureFlag(APIBaseTest, ClickhouseTestMixin):
             format="json",
         ).json()
 
-        with self.assertNumQueries(FuzzyInt(9, 10)):
+        with self.assertNumQueries(FuzzyInt(8, 9)):
             response = self.client.get(f"/api/projects/{self.team.id}/feature_flags/my_flags")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -2410,7 +2410,7 @@ class TestFeatureFlag(APIBaseTest, ClickhouseTestMixin):
                 format="json",
             ).json()
 
-        with self.assertNumQueries(FuzzyInt(9, 10)):
+        with self.assertNumQueries(FuzzyInt(8, 9)):
             response = self.client.get(f"/api/projects/{self.team.id}/feature_flags/my_flags")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
