20706cda2e168d39ef7351b1651ecc79730d58ef
Daniel Escribano <daniel.e@posthog.com>
2025-08-29 12:21:54 +0200

    chore: use new node_roles paramter

--- posthog/clickhouse/migrations/0136_add_mat_metadata_columns_to_pre_aggs.py
+++ posthog/clickhouse/migrations/0136_add_mat_metadata_columns_to_pre_aggs.py
@@ -7,53 +7,53 @@ from posthog.models.web_preaggregated.migrations import add_mat_metadata_fields_
 operations = [
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_pre_aggregated_stats"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_pre_aggregated_bounces"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_pre_aggregated_stats_staging"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_pre_aggregated_bounces_staging"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     # Below are the v1 tables, let's keep the same schema on them to keep things compatible
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_stats_hourly"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_bounces_hourly"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_stats_daily"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_bounces_daily"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_stats_hourly_staging"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         add_mat_metadata_fields_to_table("web_bounces_hourly_staging"),
-        node_role=NodeRole.ALL,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
     ),
 ]
