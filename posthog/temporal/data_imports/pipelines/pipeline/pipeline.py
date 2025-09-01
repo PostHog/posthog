03d8f8d668b9f89a74b4c6a651dbe0b50a805a76
Tom Owers <owerstom@gmail.com>
2025-09-01 17:08:29 +0100

    Dont do a shutdown raise if we're resetting the pipeline

--- posthog/temporal/data_imports/pipelines/pipeline/pipeline.py
+++ posthog/temporal/data_imports/pipelines/pipeline/pipeline.py
@@ -188,7 +188,11 @@ class PipelineNonDLT:
                 # Only raise if we're not running in descending order, otherwise we'll often not
                 # complete the job before the incremental value can be updated
                 # TODO: raise when we're within `x` time of the worker being forced to shutdown
-                if self._schema.should_use_incremental_field and self._resource.sort_mode != "desc":
+                if (
+                    self._schema.should_use_incremental_field
+                    and self._resource.sort_mode != "desc"
+                    and not self._reset_pipeline  # Raising during a full reset will reset our progress back to 0 rows
+                ):
                     self._shutdown_monitor.raise_if_is_worker_shutdown()
 
             if len(buffer) > 0:
