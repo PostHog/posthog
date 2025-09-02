e8ff37529a298d558060f47131e387481ac05f49
nickbest-ph <nick@posthog.com>
2025-09-01 20:22:48 -0700

    some cleanup

--- posthog/models/feature_flag/feature_flag.py
+++ posthog/models/feature_flag/feature_flag.py
@@ -460,6 +460,7 @@ class FeatureFlagHashKeyOverride(models.Model):
     hash_key = models.CharField(max_length=400)
 
     class Meta:
+        # migrations managed via rust/persons_migrations
         managed = False
         constraints = [
             models.UniqueConstraint(
