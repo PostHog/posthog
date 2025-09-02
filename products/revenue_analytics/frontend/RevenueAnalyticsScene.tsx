ae45cfc9fa39f5d82102ed60c3675e85577b705d
Rafa Audibert <rafael@posthog.com>
2025-09-02 14:46:45 -0300

    fix: Reference proper `Onboarding`

--- products/revenue_analytics/frontend/RevenueAnalyticsScene.tsx
+++ products/revenue_analytics/frontend/RevenueAnalyticsScene.tsx
@@ -18,6 +18,7 @@ import { SceneTitleSection } from '~/layout/scenes/components/SceneTitleSection'
 import { dataNodeCollectionLogic } from '~/queries/nodes/DataNode/dataNodeCollectionLogic'
 import { ProductKey } from '~/types'
 
+import { Onboarding } from './Onboarding'
 import { RevenueAnalyticsFilters } from './RevenueAnalyticsFilters'
 import { REVENUE_ANALYTICS_DATA_COLLECTION_NODE_ID, revenueAnalyticsLogic } from './revenueAnalyticsLogic'
 import { revenueAnalyticsSettingsLogic } from './settings/revenueAnalyticsSettingsLogic'
@@ -131,7 +132,6 @@ const RevenueAnalyticsSceneContent = (): JSX.Element => {
     }
 
     // Hasn't connected any revenue sources or events yet, so we'll show the onboarding
-
     if (!hasRevenueTables && !hasRevenueEvents) {
         return <Onboarding />
     }
