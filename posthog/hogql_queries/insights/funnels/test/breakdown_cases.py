a4586b3811495e6bc36410746d07158bf009d3e2
James Greenhill <fuziontech@gmail.com>
2025-09-02 11:17:45 +0100

    fix: correct multi-property breakdown test expectations
    Fixed incorrect test expectations for results[2] in test_funnel_step_multi_property_breakdown_event.
    After sorting, the order is Chrome,95 -> Safari,14 -> Safari,15

--- posthog/hogql_queries/insights/funnels/test/breakdown_cases.py
+++ posthog/hogql_queries/insights/funnels/test/breakdown_cases.py
@@ -245,30 +245,24 @@ def funnel_breakdown_test_factory(
             self._assert_funnel_breakdown_result_is_correct(
                 results[2],
                 [
-                    FunnelStepResult(name="sign up", breakdown=["Chrome", "95"], count=1),
+                    FunnelStepResult(name="sign up", breakdown=["Safari", "15"], count=1),
                     FunnelStepResult(
                         name="play movie",
-                        breakdown=["Chrome", "95"],
-                        count=1,
-                        average_conversion_time=3600.0,
-                        median_conversion_time=3600.0,
-                    ),
-                    FunnelStepResult(
-                        name="buy",
-                        breakdown=["Chrome", "95"],
+                        breakdown=["Safari", "15"],
                         count=1,
                         average_conversion_time=7200.0,
                         median_conversion_time=7200.0,
                     ),
+                    FunnelStepResult(name="buy", breakdown=["Safari", "15"], count=0),
                 ],
             )
             self.assertCountEqual(
-                self._get_actor_ids_at_step(filters, 1, ["Chrome", "95"]),
-                [people["person1"].uuid],
+                self._get_actor_ids_at_step(filters, 1, ["Safari", "15"]),
+                [people["person2"].uuid],
             )
             self.assertCountEqual(
-                self._get_actor_ids_at_step(filters, 2, ["Chrome", "95"]),
-                [people["person1"].uuid],
+                self._get_actor_ids_at_step(filters, 2, ["Safari", "15"]),
+                [people["person2"].uuid],
             )
 
         @also_test_with_materialized_columns(["$browser"])
