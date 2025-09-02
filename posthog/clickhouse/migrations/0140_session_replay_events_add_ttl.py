f5c4ad13f25c91b3b3d3fb5d1803bb7ecee45192
Tue Haulund <tue.haulund@gmail.com>
2025-09-02 21:33:23 +0200

    fix: set missing flag for ALTER statements on replicated tables

--- posthog/clickhouse/migrations/0140_session_replay_events_add_ttl.py
+++ posthog/clickhouse/migrations/0140_session_replay_events_add_ttl.py
@@ -15,8 +15,10 @@ operations = [
     run_sql_with_exceptions(DROP_SESSION_REPLAY_EVENTS_TABLE_MV_SQL(on_cluster=False)),
     run_sql_with_exceptions(DROP_KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL(on_cluster=False)),
     # alter the target tables
-    run_sql_with_exceptions(SET_TTL_ONLY_DROP_PARTS_SESSION_REPLAY_EVENTS_TABLE_SQL(), sharded=True),
-    run_sql_with_exceptions(ADD_TTL_SESSION_REPLAY_EVENTS_TABLE_SQL(), sharded=True),
+    run_sql_with_exceptions(
+        SET_TTL_ONLY_DROP_PARTS_SESSION_REPLAY_EVENTS_TABLE_SQL(), sharded=True, is_alter_on_replicated_table=True
+    ),
+    run_sql_with_exceptions(ADD_TTL_SESSION_REPLAY_EVENTS_TABLE_SQL(), sharded=True, is_alter_on_replicated_table=True),
     # and then recreate the materialized views and kafka tables
     run_sql_with_exceptions(KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL(on_cluster=False)),
     run_sql_with_exceptions(SESSION_REPLAY_EVENTS_TABLE_MV_SQL(on_cluster=False)),
