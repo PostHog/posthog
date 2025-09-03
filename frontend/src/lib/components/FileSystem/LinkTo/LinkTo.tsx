cb8e426e14cbd54ba1a25ca2552e1fb982b0e3c1
Marius Andra <marius.andra@gmail.com>
2025-09-03 16:26:48 +0200

    feat(project-tree): create shortcut in

--- frontend/src/lib/components/FileSystem/LinkTo/LinkTo.tsx
+++ frontend/src/lib/components/FileSystem/LinkTo/LinkTo.tsx
@@ -4,35 +4,36 @@ import { Form } from 'kea-forms'
 import { LemonSnack } from '@posthog/lemon-ui'
 
 import { FolderSelect } from 'lib/components/FileSystem/FolderSelect/FolderSelect'
-import { moveToLogic } from 'lib/components/FileSystem/MoveTo/moveToLogic'
+import { linkToLogic } from 'lib/components/FileSystem/LinkTo/linkToLogic'
 import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { LemonField } from 'lib/lemon-ui/LemonField'
 import { LemonModal } from 'lib/lemon-ui/LemonModal'
 
 import { splitPath } from '~/layout/panel-layout/ProjectTree/utils'
 
-export function MoveToModal(): JSX.Element {
-    const { isOpen, form, movingItems } = useValues(moveToLogic)
-    const { closeMoveToModal, submitForm } = useActions(moveToLogic)
+export function LinkToModal(): JSX.Element {
+    const { isOpen, form, linkingItems } = useValues(linkToLogic)
+    const { closeLinkToModal, submitForm } = useActions(linkToLogic)
 
     const destinationFolder = form.folder || 'Project root'
     const allFolders = splitPath(destinationFolder)
     const lastFolder = allFolders[allFolders.length - 1]
 
+    const s = linkingItems.length === 1 ? '' : 's'
+
     return (
         <LemonModal
-            onClose={closeMoveToModal}
+            onClose={closeLinkToModal}
             isOpen={isOpen}
-            title="Select a folder to move to"
+            title={`Select a folder to create shortcut${s} in`}
             description={
                 <>
-                    You are moving {movingItems.length} item{movingItems.length === 1 ? '' : 's'} to{' '}
-                    <LemonSnack>{destinationFolder}</LemonSnack>
+                    You are creating {linkingItems.length} shortcut{s} in <LemonSnack>{destinationFolder}</LemonSnack>
                 </>
             }
             // This is a bit of a hack. Without it, the flow "insight" -> "add to dashboard button" ->
             // "new dashboard template picker modal" -> "save dashboard to modal" wouldn't work.
-            // Since MoveToModal is added to the DOM earlier as part of global modals, it's below it in hierarchy.
+            // Since LinkToModal is added to the DOM earlier as part of global modals, it's below it in hierarchy.
             zIndex="1169"
             footer={
                 <>
@@ -40,16 +41,16 @@ export function MoveToModal(): JSX.Element {
                     <LemonButton
                         type="primary"
                         onClick={submitForm}
-                        data-attr="move-to-modal-move-button"
+                        data-attr="link-to-modal-move-button"
                         disabledReason={typeof lastFolder !== 'string' ? 'Please select a folder' : undefined}
                     >
-                        Move to {lastFolder}
+                        Create shortcut{s} in {lastFolder}
                     </LemonButton>
                 </>
             }
         >
             <div className="w-192 max-w-full">
-                <Form logic={moveToLogic} formKey="form">
+                <Form logic={linkToLogic} formKey="form">
                     <LemonField name="folder">
                         <FolderSelect root="project://" includeRoot className="h-[60vh] min-h-[200px]" />
                     </LemonField>
