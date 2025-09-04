cd1894c8b3957eba24f9f6eff72db61531c17891
Hugues Pouillot <hpouillot@gmail.com>
2025-09-04 16:28:38 +0100

    replace item cache with time trees

---
+++ frontend/src/lib/utils/time-tree.ts
@@ -0,0 +1,43 @@
+import { RBTree } from 'bintrees'
+
+import { Dayjs } from 'lib/dayjs'
+
+export class TimeTree<T extends { timestamp: Dayjs }> {
+    tree: RBTree<T>
+
+    constructor() {
+        this.tree = new RBTree((a, b) => a.timestamp.diff(b.timestamp))
+    }
+
+    clear(): void {
+        this.tree = new RBTree((a, b) => a.timestamp.diff(b.timestamp))
+    }
+
+    getAll(): T[] {
+        const items: T[] = []
+        this.tree.each((item) => {
+            items.push(item)
+        })
+        return items
+    }
+
+    add(items: T[]): void {
+        for (const item of items) {
+            this.tree.insert(item)
+        }
+    }
+
+    next(from: Dayjs): T | undefined {
+        const nextIter = this.tree.upperBound({
+            timestamp: from,
+        } as T)
+        return nextIter.next() ?? undefined
+    }
+
+    previous(to: Dayjs): T | undefined {
+        const prevIter = this.tree.lowerBound({
+            timestamp: to,
+        } as T)
+        return prevIter.prev() ?? undefined
+    }
+}
