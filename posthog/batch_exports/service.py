7fc20f7216f9bb8828979bc8b81c6ed798b26a6b
Ross Gray <ross@posthog.com>
2025-09-05 17:36:48 +0100

    WIP
    WIP export fields
    
    initial version
    
    Improve docstrings

--- posthog/batch_exports/service.py
+++ posthog/batch_exports/service.py
@@ -228,6 +228,21 @@ class BigQueryBatchExportInputs(BaseBatchExportInputs):
 
 
 @dataclass(kw_only=True)
+class DatabricksBatchExportInputs(BaseBatchExportInputs):
+    """Inputs for Databricks export workflow."""
+
+    server_hostname: str
+    http_path: str
+    # TODO - store these in the integration model instead
+    client_id: str
+    client_secret: str
+    catalog: str
+    schema: str
+    table_name: str
+    use_variant_type: bool = True
+
+
+@dataclass(kw_only=True)
 class HttpBatchExportInputs(BaseBatchExportInputs):
     """Inputs for Http export workflow."""
 
@@ -248,6 +263,7 @@ DESTINATION_WORKFLOWS = {
     "Postgres": ("postgres-export", PostgresBatchExportInputs),
     "Redshift": ("redshift-export", RedshiftBatchExportInputs),
     "BigQuery": ("bigquery-export", BigQueryBatchExportInputs),
+    "Databricks": ("databricks-export", DatabricksBatchExportInputs),
     "HTTP": ("http-export", HttpBatchExportInputs),
     "NoOp": ("no-op", NoOpInputs),
 }
