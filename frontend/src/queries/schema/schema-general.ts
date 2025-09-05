50d9b0989b5275256a1bf6aee8f0169792b0f5e2
Rafael Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2025-09-05 18:18:44 -0300

    feat: Final UX fixes before Revenue Analytics beta launch (#37658)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/queries/schema/schema-general.ts
+++ frontend/src/queries/schema/schema-general.ts
@@ -3505,6 +3505,11 @@ export interface RevenueAnalyticsGoal {
     name: string
     due_date: string
     goal: number
+
+    /**
+     * @default 'gross'
+     */
+    mrr_or_gross: 'mrr' | 'gross'
 }
 
 export interface RevenueAnalyticsConfig {
