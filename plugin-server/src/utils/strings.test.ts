a1a3bd8b51a49b3e395f0b015a0925efe1e9aab2
pauldambra <paul@posthog.com>
2025-09-08 19:49:12 +0100

    fix

--- plugin-server/src/utils/strings.test.ts
+++ plugin-server/src/utils/strings.test.ts
@@ -1,17 +1,9 @@
 import { sanitizeForUTF8 } from '~/utils/strings'
 
 describe('string utils', () => {
-    test.each([
-        'Hello \ud83d World\ud83d',
-        'Hello \ud83c World',
-        'Hello \uDC00 World',
-        'Hello \ud83d World\ud83d',
-        'Hello \ud83d World\ud83d ',
-    ])('should replace lone surrogate pairs', (s) => {
-        const sanitized = sanitizeForUTF8(s)
-        expect(sanitized).not.toContain('\ud83c')
-        expect(sanitized).not.toContain('\ud83c')
-        expect(sanitized).not.toContain('\uDC00')
-        expect(sanitized).toMatchSnapshot()
+    test.each(['\ud83c', '\uDC00', '\ud83d'])('should replace lone surrogate pairs', (loneSurrogatePair) => {
+        const sanitized = sanitizeForUTF8(`string truncated at lone surrogate pair ${loneSurrogatePair}`)
+        expect(sanitized).not.toContain(loneSurrogatePair)
+        expect(sanitized).toContain('\uFFFD') // replacement character
     })
 })
