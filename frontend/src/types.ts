3475f9ec7e998ef0f363dff367a555f2dd67f175
Georgiy Tarasov <skoob17@mail.ru>
2025-09-08 15:18:30 +0200

    feat: dataset scopes and docs

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -4464,6 +4464,7 @@ export type APIScopeObject =
     | 'cohort'
     | 'dashboard'
     | 'dashboard_template'
+    | 'dataset'
     | 'early_access_feature'
     | 'error_tracking'
     | 'event_definition'
