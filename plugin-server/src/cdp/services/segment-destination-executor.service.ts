6e6bdee0e1924c587cbf6be31047b00df20a818e
Ben White <ben@posthog.com>
2025-09-08 15:41:13 +0200

    Updated segment destinatons

--- plugin-server/src/cdp/services/segment-destination-executor.service.ts
+++ plugin-server/src/cdp/services/segment-destination-executor.service.ts
@@ -4,14 +4,13 @@ import { ReadableStream } from 'stream/web'
 import { PluginsServerConfig } from '~/types'
 
 import { parseJSON } from '../../utils/json-parse'
-import { FetchOptions, FetchResponse, Response, fetch } from '../../utils/request'
-import { tryCatch } from '../../utils/try-catch'
+import { FetchOptions, FetchResponse, Response } from '../../utils/request'
 import { LegacyPluginLogger } from '../legacy-plugins/types'
 import { SEGMENT_DESTINATIONS_BY_ID } from '../segment/segment-templates'
 import { CyclotronJobInvocationHogFunction, CyclotronJobInvocationResult } from '../types'
 import { CDP_TEST_ID, createAddLogFunction, isSegmentPluginHogFunction } from '../utils'
 import { createInvocationResult } from '../utils/invocation-utils'
-import { getNextRetryTime, isFetchResponseRetriable } from './hog-executor.service'
+import { cdpTrackedFetch, getNextRetryTime, isFetchResponseRetriable } from './hog-executor.service'
 
 const pluginExecutionDuration = new Histogram({
     name: 'cdp_segment_execution_duration_ms',
@@ -97,10 +96,6 @@ const convertFetchResponse = <Data = unknown>(response: FetchResponse, text: str
 
 export class SegmentDestinationExecutorService {
     constructor(private serverConfig: PluginsServerConfig) {}
-
-    public async fetch(...args: Parameters<typeof fetch>): Promise<FetchResponse> {
-        return fetch(...args)
-    }
 
     public async execute(
         invocation: CyclotronJobInvocationHogFunction
@@ -232,9 +227,13 @@ export class SegmentDestinationExecutorService {
                         addLog('debug', 'fetchOptions', fetchOptions)
                     }
 
-                    const [fetchError, fetchResponse] = await tryCatch(() =>
-                        this.fetch(`${endpoint}${params.toString() ? '?' + params.toString() : ''}`, fetchOptions)
-                    )
+                    const url = `${endpoint}${params.toString() ? '?' + params.toString() : ''}`
+                    const { fetchError, fetchResponse } = await cdpTrackedFetch({
+                        url,
+                        fetchParams: fetchOptions,
+                        templateId: invocation.hogFunction.template_id ?? '',
+                    })
+
                     const fetchResponseText = (await fetchResponse?.text()) ?? 'unknown'
 
                     if (fetchError || !fetchResponse || fetchResponse.status >= 400) {
