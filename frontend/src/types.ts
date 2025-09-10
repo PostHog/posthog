3b9ccd28f91c400d122adf11a9a1370c8556ce3b
Tue Haulund <tue.haulund@gmail.com>
2025-09-10 13:51:12 +0200

    feat: query and show recording retention and TTL (#37591)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1179,6 +1179,7 @@ export interface SessionPlayerData {
     end: Dayjs | null
     fullyLoaded: boolean
     sessionRecordingId: SessionRecordingId
+    sessionRetentionPeriodDays: number | null
 }
 
 export enum SessionRecordingUsageType {
@@ -1682,6 +1683,8 @@ export interface SessionRecordingType {
      * calculated on the backend so that we can sort by it, definition may change over time
      */
     activity_score?: number
+    /** retention period for this recording */
+    retention_period_days?: number
 }
 
 export interface SessionRecordingUpdateType {
