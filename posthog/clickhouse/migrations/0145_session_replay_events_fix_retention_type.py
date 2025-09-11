b48c543ad02d1b8ab0eeee436803636dc8aa4537
Tue Haulund <tue.haulund@gmail.com>
2025-09-11 16:14:38 +0200

    fix: remove TTL policy and settings from sharded table

--- posthog/clickhouse/migrations/0145_session_replay_events_fix_retention_type.py
+++ posthog/clickhouse/migrations/0145_session_replay_events_fix_retention_type.py
@@ -9,6 +9,7 @@ from posthog.session_recordings.sql.session_replay_event_migrations_sql import (
     DROP_RETENTION_PERIOD_DAYS_SESSION_REPLAY_EVENTS_TABLE_SQL,
     DROP_RETENTION_PERIOD_DAYS_WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL,
     DROP_SESSION_REPLAY_EVENTS_TABLE_MV_SQL,
+    REMOVE_TTL_SESSION_REPLAY_EVENTS_TABLE_SQL,
 )
 from posthog.session_recordings.sql.session_replay_event_sql import (
     KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL,
@@ -20,12 +21,12 @@ operations = [
     run_sql_with_exceptions(DROP_SESSION_REPLAY_EVENTS_TABLE_MV_SQL(on_cluster=False), node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(DROP_KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.DATA]),
     # alter the target tables
-    # run_sql_with_exceptions(
-    #    REMOVE_TTL_SESSION_REPLAY_EVENTS_TABLE_SQL(),
-    #    node_roles=[NodeRole.DATA],
-    #    sharded=True,
-    #    is_alter_on_replicated_table=True,
-    # ),
+    run_sql_with_exceptions(
+        REMOVE_TTL_SESSION_REPLAY_EVENTS_TABLE_SQL(),
+        node_roles=[NodeRole.DATA],
+        sharded=True,
+        is_alter_on_replicated_table=True,
+    ),
     run_sql_with_exceptions(DROP_RETENTION_PERIOD_DAYS_WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL()),
     run_sql_with_exceptions(
         DROP_RETENTION_PERIOD_DAYS_DISTRIBUTED_SESSION_REPLAY_EVENTS_TABLE_SQL(),
