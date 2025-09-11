b1cda1df1434c6416f4919095036c1df91f32560
Marius Andra <marius.andra@gmail.com>
2025-09-11 22:14:50 +0200

    take the 2nd to last breadcrumb

--- frontend/src/layout/scenes/components/SceneBreadcrumbs.tsx
+++ frontend/src/layout/scenes/components/SceneBreadcrumbs.tsx
@@ -11,14 +11,14 @@ export function SceneBreadcrumbBackButton(): JSX.Element | null {
     return breadcrumbs.length > 2 ? (
         <Link
             className="flex items-center gap-1 text-tertiary text-xs pl-[var(--button-padding-x-lg)]"
-            aria-label={`Go back to ${breadcrumbs[1].name}`}
-            to={breadcrumbs[1].path}
+            aria-label={`Go back to ${breadcrumbs[breadcrumbs.length - 2].name}`}
+            to={breadcrumbs[breadcrumbs.length - 2].path}
             buttonProps={{
                 variant: 'default',
             }}
         >
             <IconArrowLeft aria-hidden="true" className="size-3 text-tertiary" />
-            <span>{breadcrumbs[1].name}</span>
+            <span>{breadcrumbs[breadcrumbs.length - 2].name}</span>
         </Link>
     ) : null
 }
