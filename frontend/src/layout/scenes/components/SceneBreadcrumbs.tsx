b580ebfee34e1113393255841fef0dcd9ee2d980
Adam Leith <adamleithp@users.noreply.github.com>
2025-09-12 10:42:38 +0100

    survey template chooser back button to surveys, scene back button now supports forceBackTo

--- frontend/src/layout/scenes/components/SceneBreadcrumbs.tsx
+++ frontend/src/layout/scenes/components/SceneBreadcrumbs.tsx
@@ -4,21 +4,24 @@ import { IconArrowLeft } from '@posthog/icons'
 import { Link } from '@posthog/lemon-ui'
 
 import { breadcrumbsLogic } from '~/layout/navigation/Breadcrumbs/breadcrumbsLogic'
+import { Breadcrumb } from '~/types'
 
-export function SceneBreadcrumbBackButton(): JSX.Element | null {
+export function SceneBreadcrumbBackButton({ forceBackTo }: { forceBackTo?: Breadcrumb }): JSX.Element | null {
     const { breadcrumbs } = useValues(breadcrumbsLogic)
+
+    const backTo = forceBackTo || breadcrumbs[breadcrumbs.length - 2]
 
     return breadcrumbs.length > 1 ? (
         <Link
             className="flex items-center gap-1 text-tertiary text-xs pl-[var(--button-padding-x-lg)]"
-            aria-label={`Go back to ${breadcrumbs[breadcrumbs.length - 2].name}`}
-            to={breadcrumbs[breadcrumbs.length - 2].path}
+            aria-label={`Go back to ${backTo.name}`}
+            to={backTo.path}
             buttonProps={{
                 variant: 'default',
             }}
         >
             <IconArrowLeft aria-hidden="true" className="size-3 text-tertiary" />
-            <span>{breadcrumbs[breadcrumbs.length - 2].name}</span>
+            <span>{backTo.name}</span>
         </Link>
     ) : null
 }
