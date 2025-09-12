79abf8c18e83440104e5129043e74fa59a5286d5
Ben White <ben@posthog.com>
2025-09-12 10:21:36 +0100

    fix

--- frontend/src/scenes/data-pipelines/DataPipelinesOverview.tsx
+++ frontend/src/scenes/data-pipelines/DataPipelinesOverview.tsx
@@ -5,6 +5,7 @@ import { Link } from '@posthog/lemon-ui'
 
 import { FlaggedFeature } from 'lib/components/FlaggedFeature'
 import { PageHeader } from 'lib/components/PageHeader'
+import { useFeatureFlag } from 'lib/hooks/useFeatureFlag'
 import { useOnMountEffect } from 'lib/hooks/useOnMountEffect'
 import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { LemonMenu, LemonMenuItems } from 'lib/lemon-ui/LemonMenu'
@@ -12,6 +13,10 @@ import { DataWarehouseManagedSourcesTable } from 'scenes/data-warehouse/settings
 import { DataWarehouseSelfManagedSourcesTable } from 'scenes/data-warehouse/settings/DataWarehouseSelfManagedSourcesTable'
 import { HogFunctionList } from 'scenes/hog-functions/list/HogFunctionsList'
 import { urls } from 'scenes/urls'
+
+import { SceneContent } from '~/layout/scenes/components/SceneContent'
+import { SceneDivider } from '~/layout/scenes/components/SceneDivider'
+import { SceneSection } from '~/layout/scenes/components/SceneSection'
 
 import { nonHogFunctionsLogic } from './utils/nonHogFunctionsLogic'
 
@@ -36,6 +41,8 @@ function Section({
 }
 
 export function DataPipelinesOverview(): JSX.Element {
+    const newSceneLayout = useFeatureFlag('NEW_SCENE_LAYOUT')
+
     const menuItems: LemonMenuItems = [
         {
             label: 'Source',
@@ -55,7 +62,7 @@ export function DataPipelinesOverview(): JSX.Element {
     })
 
     return (
-        <>
+        <SceneContent forceNewSpacing>
             <PageHeader
                 buttons={
                     <div className="flex items-center m-2 shrink-0">
@@ -74,25 +81,86 @@ export function DataPipelinesOverview(): JSX.Element {
             />
             <div className="deprecated-space-y-4">
                 <FlaggedFeature flag="cdp-hog-sources">
-                    <Section title="Event sources" to={urls.dataPipelines('sources')}>
-                        <HogFunctionList logicKey="overview-data-sources" type="source_webhook" />
-                    </Section>
+                    <>
+                        <SceneSection
+                            title="Event sources"
+                            actions={<Link to={urls.dataPipelines('sources')}>See all</Link>}
+                            hideTitleAndDescription={!newSceneLayout}
+                        >
+                            {!newSceneLayout && (
+                                <Link to={urls.dataPipelines('sources')}>
+                                    <h2>Event sources</h2>
+                                </Link>
+                            )}
+                            <HogFunctionList logicKey="overview-data-sources" type="source_webhook" />
+                        </SceneSection>
+                        <SceneDivider />
+                    </>
                 </FlaggedFeature>
-                <Section title="Managed data warehouse sources" to={urls.dataPipelines('sources')}>
+                <SceneSection
+                    title="Managed data warehouse sources"
+                    actions={<Link to={urls.dataPipelines('sources')}>See all</Link>}
+                    hideTitleAndDescription={!newSceneLayout}
+                >
+                    {!newSceneLayout && (
+                        <Link to={urls.dataPipelines('sources')}>
+                            <h2>Managed datw warehouse sources</h2>
+                        </Link>
+                    )}
                     <DataWarehouseManagedSourcesTable />
-                </Section>
-                <Section title="Self-managed data warehouse sources" to={urls.dataPipelines('sources')}>
+                </SceneSection>
+                <SceneSection
+                    title="Self-managed data warehouse sources"
+                    actions={<Link to={urls.dataPipelines('sources')}>See all</Link>}
+                    hideTitleAndDescription={!newSceneLayout}
+                >
+                    {!newSceneLayout && (
+                        <Link to={urls.dataPipelines('sources')}>
+                            <h2>Self-managed data warehouse sources</h2>
+                        </Link>
+                    )}
                     <DataWarehouseSelfManagedSourcesTable />
-                </Section>
-                <Section title="Transformations" to={urls.dataPipelines('transformations')}>
-                    <p>Modify and enrich your incoming data. Only active transformations are shown here.</p>
+                </SceneSection>
+                <SceneDivider />
+                <SceneSection
+                    title="Transformations"
+                    description="Modify and enrich your incoming data. Only active transformations are shown here."
+                    actions={<Link to={urls.dataPipelines('transformations')}>See all</Link>}
+                    hideTitleAndDescription={!newSceneLayout}
+                >
+                    {!newSceneLayout && (
+                        <>
+                            <Link to={urls.dataPipelines('transformations')}>
+                                <h2>Transformations</h2>
+                            </Link>
+                            <p>
+                                Modify and enrich your incoming data. Only active transformations are shown here.{' '}
+                                <Link to={urls.dataPipelines('transformations')}>See all.</Link>
+                            </p>
+                        </>
+                    )}
                     <HogFunctionList logicKey="transformation" type="transformation" hideFeedback={true} />
-                </Section>
-                <Section title="Destinations" to={urls.dataPipelines('destinations')}>
-                    <p>
-                        Send your data to destinations in real time or with batch exports. Only active Destinations are
-                        shown here.
-                    </p>
+                </SceneSection>
+                <SceneDivider />
+
+                <SceneSection
+                    title="Destinations"
+                    description="Send your data to destinations in real time or with batch exports. Only active Destinations are shown here."
+                    actions={<Link to={urls.dataPipelines('destinations')}>See all</Link>}
+                    hideTitleAndDescription={!newSceneLayout}
+                >
+                    {!newSceneLayout && (
+                        <>
+                            <Link to={urls.dataPipelines('destinations')}>
+                                <h2>Destinations</h2>
+                            </Link>
+                            <p>
+                                Send your data to destinations in real time or with batch exports. Only active
+                                Destinations are shown here.{' '}
+                                <Link to={urls.dataPipelines('destinations')}>See all.</Link>
+                            </p>
+                        </>
+                    )}
                     <HogFunctionList
                         logicKey="destination"
                         type="destination"
@@ -102,8 +170,8 @@ export function DataPipelinesOverview(): JSX.Element {
                             ...(hogFunctionBatchExports ?? []),
                         ]}
                     />
-                </Section>
+                </SceneSection>
             </div>
-        </>
+        </SceneContent>
     )
 }
