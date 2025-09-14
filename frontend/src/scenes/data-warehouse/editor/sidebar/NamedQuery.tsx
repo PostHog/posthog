a0dd3a8a34445737589a0ff80f387faad4d601e4
jovan sakovic <jovan@posthog.com>
2025-09-14 22:49:48 +0100

    chore: route named query

--- frontend/src/scenes/data-warehouse/editor/sidebar/NamedQuery.tsx
+++ frontend/src/scenes/data-warehouse/editor/sidebar/NamedQuery.tsx
@@ -1,3 +1,16 @@
+import { LemonButton } from "@posthog/lemon-ui"
+import { router } from "kea-router"
+
 export function NamedQuery(): JSX.Element {
-    return <div>NamedQuery</div>
+    return (
+        <LemonButton
+        size="small"
+        onClick={() => {
+            router.actions.push('/embedded-analytics/named-queries')
+        }}
+        type="primary"
+    >
+        Create named query
+    </LemonButton>
+    )    
 }
