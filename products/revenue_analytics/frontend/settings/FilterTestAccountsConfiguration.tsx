47875d42afdbc1169a3e3ce23cb3acb1bec6fa32
Adam Leith <adamleithp@users.noreply.github.com>
2025-09-15 23:07:47 +0100

    add scene title section to ingestion warnings, add a scene contents, re-add link to revenue analytics FilterTestAccountsConfiguration

--- products/revenue_analytics/frontend/settings/FilterTestAccountsConfiguration.tsx
+++ products/revenue_analytics/frontend/settings/FilterTestAccountsConfiguration.tsx
@@ -12,7 +12,13 @@ export function FilterTestAccountsConfiguration(): JSX.Element {
     return (
         <SceneSection
             title="Filter test accounts out of revenue analytics"
-            description="When enabled, events from test accounts will be excluded from Revenue analytics. You can configure these test accounts here."
+            description={
+                <>
+                    When enabled, events from test accounts will be excluded from Revenue analytics. You can configure
+                    these test accounts{' '}
+                    <Link to={urls.settings('project-product-analytics', 'internal-user-filtering')}>here</Link>.
+                </>
+            }
         >
             <LemonSwitch
                 onChange={updateFilterTestAccounts}
