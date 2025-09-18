b2b4e10305aa155d349d574aace1ae55f0aab92d
Tue Haulund <tue.haulund@gmail.com>
2025-09-18 08:25:07 +0200

    fix: use paginator to read and migrate teams in smaller batches

--- posthog/management/commands/migrate_replay_retention.py
+++ posthog/management/commands/migrate_replay_retention.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.core.paginator import Paginator
 
 from posthog.constants import AvailableFeature
 from posthog.models.team import Team
@@ -14,27 +15,36 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dry_run = options["dry_run"]
         batch_size = options["batch_size"]
-        teams_to_migrate = []
 
         try:
             self.stdout.write(
                 self.style.SUCCESS("Starting Replay retention migration" + (" (DRY RUN)" if dry_run else ""))
             )
 
-            for team in Team.objects.filter(session_recording_retention_period="legacy").only(
-                "id", "organization", "session_recording_retention_period"
-            ):
-                # NOTE: We use file export as a proxy to see if they are subbed to Recordings
-                is_paid = team.organization.is_feature_available(AvailableFeature.RECORDINGS_FILE_EXPORT)
-                team.session_recording_retention_period = "90d" if is_paid else "30d"
+            queryset = (
+                Team.objects.filter(session_recording_retention_period="legacy")
+                .order_by("id")
+                .only("id", "organization", "session_recording_retention_period")
+            )
 
-                teams_to_migrate.append(team)
-                self.stdout.write(self.style.SUCCESS(f"{'Would migrate' if dry_run else 'Migrating'} team {team.id}"))
+            for batch in Paginator(queryset, batch_size):
+                teams_to_migrate = []
+                for team in batch.object_list:
+                    # NOTE: We use file export as a proxy to see if they are subbed to Recordings
+                    is_paid = team.organization.is_feature_available(AvailableFeature.RECORDINGS_FILE_EXPORT)
+                    team.session_recording_retention_period = "90d" if is_paid else "30d"
 
-            if not dry_run:
-                Team.objects.bulk_update(
-                    teams_to_migrate, ["session_recording_retention_period"], batch_size=batch_size
-                )
+                    teams_to_migrate.append(team)
+                    self.stdout.write(
+                        self.style.SUCCESS(f"{'Would migrate' if dry_run else 'Migrating'} team {team.id}")
+                    )
+
+                if not dry_run:
+                    self.stdout.write(self.style.SUCCESS(f"Writing batch..."))
+                    Team.objects.bulk_update(
+                        teams_to_migrate,
+                        ["session_recording_retention_period"],
+                    )
 
             self.stdout.write(
                 self.style.SUCCESS(
