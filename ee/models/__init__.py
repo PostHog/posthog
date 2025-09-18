688643abc5a009964245ce56bbbc3b7c345f5be3
Zach Waterfield <zlwaterfield@gmail.com>
2025-09-18 12:52:37 -0400

    Remove ExplicitTeamMembership API and model import
    Deleted the deprecated ee/api/explicit_team_member.py file and removed ExplicitTeamMembership import and export from ee/models/__init__.py. This cleans up unused code related to explicit team membership management.

--- ee/models/__init__.py
+++ ee/models/__init__.py
@@ -7,7 +7,6 @@ from .assistant import (
 )
 from .dashboard_privilege import DashboardPrivilege
 from .event_definition import EnterpriseEventDefinition
-from .explicit_team_membership import ExplicitTeamMembership
 from .hook import Hook
 from .license import License
 from .property_definition import EnterprisePropertyDefinition
@@ -25,7 +24,6 @@ __all__ = [
     "Conversation",
     "EnterpriseEventDefinition",
     "EnterprisePropertyDefinition",
-    "ExplicitTeamMembership",
     "Hook",
     "License",
     "Role",
