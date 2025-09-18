db88451bb314194ff05984eae8fde793c572f470
pauldambra <paul@posthog.com>
2025-09-18 10:27:55 +0100

    feat: submit comment on meta+enter

---
+++ frontend/src/scenes/session-recordings/player/commenting/PlayerCommentModal.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta } from '@storybook/react'
+import { BindLogic } from 'kea'
+
+import { PlayerCommentModal } from 'scenes/session-recordings/player/commenting/PlayerFrameCommentOverlay'
+import { sessionRecordingPlayerLogic } from 'scenes/session-recordings/player/sessionRecordingPlayerLogic'
+
+const meta: Meta<typeof PlayerCommentModal> = {
+    title: 'Replay/Components/Comment modal',
+    component: PlayerCommentModal,
+    parameters: {
+        testOptions: {
+            waitForLoadersToDisappear: true,
+        },
+    },
+    tags: ['autodocs'],
+}
+export default meta
+
+export function Default(): JSX.Element {
+    return (
+        <div className="min-h-65">
+            <BindLogic logic={sessionRecordingPlayerLogic} props={{ sessionRecordingId: '23' }}>
+                <PlayerCommentModal />
+            </BindLogic>
+        </div>
+    )
+}
