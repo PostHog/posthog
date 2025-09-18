cdc51423bf7716c9563a91cb854db98468954f51
Alexander Spicer <aspicer@gmail.com>
2025-09-18 14:33:12 -0700

    clean up

--- posthog/test/playwright_time_utils.py
+++ posthog/test/playwright_time_utils.py
@@ -1,8 +1,9 @@
 """
 Simple time freezing for Playwright tests.
 
-When PLAYWRIGHT_FROZEN_TIME environment variable is set,
-this freezes Django backend time to match the frontend.
+When the START_PLAYWRIGHT environment variable is set (which is standard
+for Playwright test runs), this freezes Django backend time to match
+the frontend frozen time.
 """
 
 import os
@@ -18,19 +19,17 @@ logger = structlog.get_logger(__name__)
 # Fixed frozen time for all Playwright tests: November 8, 2024 at 12:00:00 UTC
 PLAYWRIGHT_FROZEN_TIME = "2024-11-08T12:00:00Z"
 
-# Environment variable name
-PLAYWRIGHT_FROZEN_TIME_ENV = "PLAYWRIGHT_FROZEN_TIME"
-
 
 def is_playwright_test() -> bool:
     """Check if we're running in a Playwright test environment."""
-    return os.getenv(PLAYWRIGHT_FROZEN_TIME_ENV) == PLAYWRIGHT_FROZEN_TIME
+    return bool(os.getenv("PLAYWRIGHT_FROZEN_TIME"))
 
 
 def get_frozen_datetime() -> Optional[dt.datetime]:
     """Get the frozen datetime if in Playwright test mode."""
-    if is_playwright_test():
-        return dt.datetime.fromisoformat(PLAYWRIGHT_FROZEN_TIME.replace("Z", "+00:00"))
+    frozen_time = os.getenv("PLAYWRIGHT_FROZEN_TIME")
+    if frozen_time:
+        return dt.datetime.fromisoformat(frozen_time.replace("Z", "+00:00"))
     return None
 
 
@@ -42,13 +41,12 @@ def setup_playwright_time_freeze():
     """Setup global time freezing for Playwright tests if environment variable is set."""
     global _global_freezer
 
-    if is_playwright_test() and _global_freezer is None:
+    frozen_time = os.getenv("PLAYWRIGHT_FROZEN_TIME")
+    if frozen_time and _global_freezer is None:
         # Ignore pendulum and other libraries that have compatibility issues
-        _global_freezer = freezegun.freeze_time(
-            PLAYWRIGHT_FROZEN_TIME, ignore=["pendulum", "dlt", "asyncio", "threading"]
-        )
+        _global_freezer = freezegun.freeze_time(frozen_time, ignore=["pendulum", "dlt", "asyncio", "threading"])
         _global_freezer.start()
-        logger.info("playwright_time_freeze_enabled", frozen_time=PLAYWRIGHT_FROZEN_TIME)
+        logger.info("playwright_time_freeze_enabled", frozen_time=frozen_time)
 
 
 def teardown_playwright_time_freeze():
