a96cc2a2c7b4daea0aee448cd7556f7b3b0992d8
José Sequeira <jose@posthog.com>
2025-09-19 14:25:37 +0200

    don't track messages smaller than last committed offset

--- rust/kafka-deduplicator/src/kafka/tracker.rs
+++ rust/kafka-deduplicator/src/kafka/tracker.rs
@@ -8,7 +8,7 @@ use std::time::{Duration, Instant};
 use tokio::sync::{mpsc, OwnedSemaphorePermit, RwLock, Semaphore};
 use tokio::task::JoinHandle;
 use tokio::time::sleep;
-use tracing::{debug, info, warn};
+use tracing::{debug, error, info, warn};
 
 use crate::kafka::errors::TrackingError;
 use crate::kafka::message::{AckableMessage, MessageResult};
