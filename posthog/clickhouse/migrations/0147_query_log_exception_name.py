2d20525dc2b0528016a0271e349cbe9781b0d930
jovan sakovic <jovan@posthog.com>
2025-09-19 14:47:47 +0100

    fix: only alias the column

--- posthog/clickhouse/migrations/0147_query_log_exception_name.py
+++ posthog/clickhouse/migrations/0147_query_log_exception_name.py
@@ -1,6 +1,6 @@
 from posthog.clickhouse.client.connection import NodeRole
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.clickhouse.query_log_archive import ADD_EXCEPTION_NAME_SQL, QUERY_LOG_ARCHIVE_MV_V5_SQL
+from posthog.clickhouse.query_log_archive import ADD_EXCEPTION_NAME_SQL
 
 
 operations = [
@@ -8,9 +8,5 @@ operations = [
         ADD_EXCEPTION_NAME_SQL,
         node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         is_alter_on_replicated_table=True,
-    ),
-    run_sql_with_exceptions(
-        QUERY_LOG_ARCHIVE_MV_V5_SQL(),
-        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
     ),
 ]
