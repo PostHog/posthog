3f8cc45308bc8258ead29b98fa38ecd5c108dc8e
Rafael Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2025-09-19 14:40:02 -0300

    feat: Open specific new source docs in the sidebar (#38239)

--- frontend/src/scenes/sceneTypes.ts
+++ frontend/src/scenes/sceneTypes.ts
@@ -200,8 +200,8 @@ export interface SceneConfig {
     projectBased?: boolean
     /** Set the scope of the activity (affects activity and discussion panel) */
     activityScope?: ActivityScope | string
-    /** Default docs path - what the docs side panel will open by default if this scene is active  */
-    defaultDocsPath?: string
+    /** Default docs path - what the docs side panel will open by default when this scene is active  */
+    defaultDocsPath?: string | (() => string) | (() => Promise<string>)
     /** Component import, used only in manifests */
     import?: () => Promise<any>
 }
