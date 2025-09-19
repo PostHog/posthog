e5376befc8036f15a74a473345a7bc878cc69b62
Yasen <yasen@posthog.com>
2025-09-19 11:28:59 +0300

    fix: Revert "Check for missing hex in session" (#38353)

--- posthog/api/user.py
+++ posthog/api/user.py
@@ -572,12 +572,8 @@ class UserViewSet(
 
     @action(methods=["POST"], detail=True)
     def two_factor_validate(self, request, **kwargs):
-        hex_key = request.session.get("django_two_factor-hex")
-        if not hex_key:
-            return Response({"success": True})
-
         form = TOTPDeviceForm(
-            hex_key,
+            request.session["django_two_factor-hex"],
             request.user,
             data={"token": request.data["token"]},
         )
