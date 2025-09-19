4eadc1ddb16a5513391879ab0a055490e2e69116
Adam Leith <adamleithp@users.noreply.github.com>
2025-09-19 14:32:10 +0100

    can't see the changes here for data-pipelines self managed source

--- frontend/src/scenes/data-pipelines/batch-exports/BatchExportScene.tsx
+++ frontend/src/scenes/data-pipelines/batch-exports/BatchExportScene.tsx
@@ -5,7 +5,6 @@ import { LemonSkeleton } from '@posthog/lemon-ui'
 
 import { FlaggedFeature } from 'lib/components/FlaggedFeature'
 import { NotFound } from 'lib/components/NotFound'
-import { PageHeader } from 'lib/components/PageHeader'
 import { LemonTab, LemonTabs } from 'lib/lemon-ui/LemonTabs'
 import { BatchExportBackfills } from 'scenes/data-pipelines/batch-exports/BatchExportBackfills'
 import { BatchExportRuns } from 'scenes/data-pipelines/batch-exports/BatchExportRuns'
@@ -119,14 +118,6 @@ function BatchExportSceneHeader(): JSX.Element {
 
     return (
         <>
-            <PageHeader
-                buttons={
-                    <>
-                        <BatchExportConfigurationClearChangesButton />
-                        <BatchExportConfigurationSaveButton />
-                    </>
-                }
-            />
             <SceneTitleSection
                 name={configuration.name}
                 description={null}
@@ -142,6 +133,12 @@ function BatchExportSceneHeader(): JSX.Element {
                 onNameChange={(value) => setConfigurationValue('name', value)}
                 onDescriptionChange={(value) => setConfigurationValue('description', value)}
                 canEdit
+                actions={
+                    <>
+                        <BatchExportConfigurationClearChangesButton />
+                        <BatchExportConfigurationSaveButton />
+                    </>
+                }
             />
         </>
     )
