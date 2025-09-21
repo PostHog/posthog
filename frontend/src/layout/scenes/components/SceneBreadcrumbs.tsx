d89198038cb171cf98b6c9ae66cca5cee0626959
Michael Matloka <michael@matloka.com>
2025-09-21 18:24:34 +0200

    style(navigation): Make breadcrumbs "back" button a link

--- frontend/src/layout/scenes/components/SceneBreadcrumbs.tsx
+++ frontend/src/layout/scenes/components/SceneBreadcrumbs.tsx
@@ -23,14 +23,11 @@ export function SceneBreadcrumbBackButton({
 
     return !!forceBackTo || breadcrumbs.length > 1 ? (
         <Link
-            className={cn('flex items-center gap-1 text-tertiary text-xs pl-[var(--button-padding-x-lg)]', className)}
+            className={cn('flex items-center gap-1 text-tertiary hover:text-accent text-[0.8125rem]', className)}
             aria-label={`Go back to ${backTo.name}`}
             to={backTo.path}
-            buttonProps={{
-                variant: 'default',
-            }}
         >
-            <IconArrowLeft aria-hidden="true" className="size-3 text-tertiary" />
+            <IconArrowLeft aria-hidden="true" className="size-3" />
             <span>{backTo.name}</span>
         </Link>
     ) : null
