086884b7f2f9518cbb0bb9bd50ceae2e749ac4d1
Shao Wei <lshaowei18@gmail.com>
2025-09-22 17:53:59 +0800

    fix: switch to top offset

--- frontend/src/scenes/paths/pathUtils.ts
+++ frontend/src/scenes/paths/pathUtils.ts
@@ -4,7 +4,7 @@ import { FunnelPathsFilter, PathsFilter } from '~/queries/schema/schema-general'
 import { FunnelPathType } from '~/types'
 
 import { HIDE_PATH_CARD_HEIGHT } from './Paths'
-import { PATH_NODE_CARD_LEFT_OFFSET } from './constants'
+import { PATH_NODE_CARD_TOP_OFFSET } from './constants'
 
 const PATH_NODE_CARD_TOP_ADJUSTMENTS = 33
 
@@ -142,7 +142,7 @@ export const isSelectedPathStartOrEnd = (
 export const calculatePathNodeCardTop = (node: PathNodeData, canvasHeight: number): number => {
     const isNodeCutoff = node.y1 - node.y0 < HIDE_PATH_CARD_HEIGHT && node.y1 > canvasHeight - HIDE_PATH_CARD_HEIGHT
     const isPathEnd = node.sourceLinks.length === 0
-    const result = !isPathEnd ? node.y0 + PATH_NODE_CARD_LEFT_OFFSET : node.y0 + (node.y1 - node.y0) / 2
+    const result = !isPathEnd ? node.y0 + PATH_NODE_CARD_TOP_OFFSET : node.y0 + (node.y1 - node.y0) / 2
     if (isNodeCutoff) {
         return result - PATH_NODE_CARD_TOP_ADJUSTMENTS
     }
