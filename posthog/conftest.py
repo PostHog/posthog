2564efcdb21b658155304d8a98cb0fba00311d7d
nickbest-ph <nick@posthog.com>
2025-09-23 15:58:06 -0700

    fix True

--- posthog/conftest.py
+++ posthog/conftest.py
@@ -226,4 +226,4 @@ def pytest_sessionstart():
 
     unmanaged_models = [m for m in apps.get_models() if not m._meta.managed]
     for m in unmanaged_models:
-        m._meta.managed = true
+        m._meta.managed = True
