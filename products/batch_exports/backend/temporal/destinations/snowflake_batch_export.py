d8d62d4625f511eebb88e0e0b231b1f422d9e7e7
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-09-25 14:35:09 +0200

    fix: Make over billing limit error non-retryable (#38642)

--- products/batch_exports/backend/temporal/destinations/snowflake_batch_export.py
+++ products/batch_exports/backend/temporal/destinations/snowflake_batch_export.py
@@ -1382,7 +1382,7 @@ class SnowflakeBatchExportWorkflow(PostHogWorkflow):
                     initial_interval=dt.timedelta(seconds=10),
                     maximum_interval=dt.timedelta(seconds=60),
                     maximum_attempts=0,
-                    non_retryable_error_types=["NotNullViolation", "IntegrityError"],
+                    non_retryable_error_types=["NotNullViolation", "IntegrityError", "OverBillingLimitError"],
                 ),
             )
         except OverBillingLimitError:
