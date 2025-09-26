d4ab4d2cf4607e0998df5794af92cff0c83d29b1
Robbie <robbie.coomber@gmail.com>
2025-09-26 11:47:06 +0100

    Update posthog/clickhouse/migrations/0148_sessions_v3.py
    Co-authored-by: Daniel Escribano <daniel.e@posthog.com>

--- posthog/clickhouse/migrations/0148_sessions_v3.py
+++ posthog/clickhouse/migrations/0148_sessions_v3.py
@@ -8,7 +8,7 @@ from posthog.models.raw_sessions.sql_v3 import (
 
 operations = [
     run_sql_with_exceptions(WRITABLE_RAW_SESSIONS_TABLE_SQL_V3()),
-    run_sql_with_exceptions(DISTRIBUTED_RAW_SESSIONS_TABLE_SQL_V3()),
+    run_sql_with_exceptions(DISTRIBUTED_RAW_SESSIONS_TABLE_SQL_V3(), node_roles=[NodeRole.DATA, NodeRole.COORDINATOR]),
     run_sql_with_exceptions(RAW_SESSIONS_TABLE_SQL_V3()),
     run_sql_with_exceptions(RAW_SESSIONS_CREATE_OR_REPLACE_VIEW_SQL_V3()),
     # normally there would be SQL to add the MV here too, but at the moment we only want this table to backfill,
