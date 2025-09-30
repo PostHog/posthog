e1716df9a47048c1aa1e011bf9bfd3e479424c98
luke-belton <luke.b@posthog.com>
2025-09-30 11:10:12 +0100

    fallback to `$group_type` and `$group_key` when adding group props

--- plugin-server/src/worker/ingestion/groups.ts
+++ plugin-server/src/worker/ingestion/groups.ts
@@ -9,11 +9,20 @@ export async function addGroupProperties(
     properties: Properties,
     groupTypeManager: GroupTypeManager
 ): Promise<Properties> {
-    for (const [groupType, groupIdentifier] of Object.entries(properties.$groups || {})) {
-        const columnIndex = await groupTypeManager.fetchGroupTypeIndex(teamId, projectId, groupType)
+    if (properties.$groups) {
+        for (const [groupType, groupIdentifier] of Object.entries(properties.$groups)) {
+            const columnIndex = await groupTypeManager.fetchGroupTypeIndex(teamId, projectId, groupType)
+            if (columnIndex !== null) {
+                // :TODO: Update event column instead
+                properties[`$group_${columnIndex}`] = groupIdentifier
+            }
+        }
+        // for $groupidentify events, the $groups property is not always set
+    } else if (properties.$group_type && properties.$group_key) {
+        const columnIndex = await groupTypeManager.fetchGroupTypeIndex(teamId, projectId, properties.$group_type)
         if (columnIndex !== null) {
             // :TODO: Update event column instead
-            properties[`$group_${columnIndex}`] = groupIdentifier
+            properties[`$group_${columnIndex}`] = properties.$group_key
         }
     }
     return properties
