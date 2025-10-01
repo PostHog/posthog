811678a0ebd82fa90f717b263ed8bcae45b6915e
Juraj Majerik <juro.majerik@gmail.com>
2025-10-01 11:40:29 +0200

    wip

--- dags/locations/experiments.py
+++ dags/locations/experiments.py
@@ -1,12 +1,12 @@
 """
 This module organizes experiment assets into a complete Dagster location.
-It takes the individual assets defined in experiments.py and bundles them
+It takes the individual assets defined in experiment_regular_metrics_timeseries.py and experiment_saved_metrics_timeseries.py and bundles them
 with shared resources to create a complete Dagster definitions object.
 """
 
 import dagster
 
-from dags import experiments
+from dags import experiment_regular_metrics_timeseries, experiment_saved_metrics_timeseries
 
 from . import resources
 
@@ -17,22 +17,22 @@ def _create_definitions():
     """
 
     jobs = [
-        experiments.experiment_regular_metrics_job,
-        experiments.experiment_saved_metrics_job,
+        experiment_regular_metrics_timeseries.experiment_regular_metrics_timeseries_job,
+        experiment_saved_metrics_timeseries.experiment_saved_metrics_timeseries_job,
     ]
     sensors = [
-        experiments.experiment_regular_metrics_discovery_sensor,
-        experiments.experiment_saved_metrics_discovery_sensor,
+        experiment_regular_metrics_timeseries.experiment_regular_metrics_timeseries_discovery_sensor,
+        experiment_saved_metrics_timeseries.experiment_saved_metrics_timeseries_discovery_sensor,
     ]
     schedules = [
-        experiments.daily_experiment_regular_metrics_refresh_schedule,
-        experiments.daily_experiment_saved_metrics_refresh_schedule,
+        experiment_regular_metrics_timeseries.experiment_regular_metrics_timeseries_refresh_schedule,
+        experiment_saved_metrics_timeseries.experiment_saved_metrics_timeseries_refresh_schedule,
     ]
 
     definitions = dagster.Definitions(
         assets=[
-            experiments.experiment_regular_metrics_timeseries,
-            experiments.experiment_saved_metrics_timeseries,
+            experiment_regular_metrics_timeseries.experiment_regular_metrics_timeseries,
+            experiment_saved_metrics_timeseries.experiment_saved_metrics_timeseries,
         ],
         jobs=jobs,
         sensors=sensors,
