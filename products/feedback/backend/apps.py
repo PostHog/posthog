9304a245159c7541aea61eaf04c57d5f636ab552
David Newell <david@posthog.com>
2025-10-01 11:13:55 +0100

    actually add files

---
+++ products/feedback/backend/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FeedbackConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "products.feedback.backend"
+    label = "feedback"
---
+++ products/feedback/manifest.tsx
@@ -0,0 +1,28 @@
+import { urls } from 'scenes/urls'
+
+import { ProductManifest } from '../../frontend/src/types'
+
+export const manifest: ProductManifest = {
+    name: 'Feedback',
+    urls: {
+        feedbackList: (): string => '/feedback',
+        feedback: (id: string): string => `/feedback/${id}`,
+    },
+    fileSystemTypes: {
+        feedback: {
+            name: 'Feedback',
+            iconType: 'comment',
+            href: () => urls.feedbackList(),
+            iconColor: ['var(--color-product-feature-flags-light)'],
+            filterKey: 'feedback',
+        },
+    },
+    treeItemsProducts: [
+        {
+            path: `Feedback`,
+            category: 'Behavior',
+            type: 'feedback',
+            href: urls.feedbackList(),
+        },
+    ],
+}
