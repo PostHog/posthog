e20be9d8a892139ffe8a7237264800a343f1a243
Oliver Browne <oliver@posthog.com>
2025-10-02 16:12:55 +0100

    fix(err): add token limit (#39035)

--- rust/et-embedding-worker/src/lib.rs
+++ rust/et-embedding-worker/src/lib.rs
@@ -5,6 +5,7 @@ use common_kafka::kafka_consumer::Offset;
 use common_types::error_tracking::{EmbeddingModel, EmbeddingRecord, NewFingerprintEvent};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use tiktoken_rs::CoreBPE;
 use tracing::error;
 
 use crate::app_context::AppContext;
@@ -59,8 +60,12 @@ pub async fn generate_embedding(
     model: EmbeddingModel,
     fingerprint: NewFingerprintEvent,
 ) -> Result<EmbeddingRecord> {
+    // TODO - once we have a larger model zoo, we'll need to select this more carefully
+    let encoder = tiktoken_rs::cl100k_base()?;
+
     // Generate text representation of the exception and frames
-    let text = generate_text_representation(&fingerprint);
+    // TODO - as above, a larger model zoo will require more careful selection of the model
+    let text = generate_text_representation(&fingerprint, &encoder, 8192)?;
 
     // Call OpenAI API to generate embeddings
     let request = OpenAIEmbeddingRequest {
@@ -113,20 +118,31 @@ pub async fn generate_embedding(
     ))
 }
 
-fn generate_text_representation(fingerprint: &NewFingerprintEvent) -> String {
-    let mut text_parts = Vec::new();
+fn generate_text_representation(
+    fingerprint: &NewFingerprintEvent,
+    encoder: &CoreBPE,
+    max_tokens: usize,
+) -> Result<String> {
+    let mut tokens = Vec::new();
 
     for exception in &fingerprint.exception_list {
         // Add exception type and value
-        text_parts.push(format!(
-            "{}: {}",
+        let type_and_value = &format!(
+            "{}: {}\n",
             exception.exception_type,
             exception
                 .exception_value
                 .chars()
                 .take(300)
                 .collect::<String>()
-        ));
+        );
+
+        tokens.extend(encoder.encode_with_special_tokens(type_and_value));
+
+        if tokens.len() > max_tokens {
+            tokens.truncate(max_tokens);
+            return encoder.decode(tokens);
+        }
 
         // Add frame information
         for frame in &exception.frames {
@@ -153,9 +169,16 @@ fn generate_text_representation(fingerprint: &NewFingerprintEvent) -> String {
                 frame_parts.push(format!("column {column}"));
             }
 
-            text_parts.push(frame_parts.join(" "));
+            let mut frame_str = frame_parts.join(" ");
+            frame_str.push('\n');
+            let encoded = encoder.encode_with_special_tokens(&frame_str);
+            if encoded.len() + tokens.len() > max_tokens {
+                return encoder.decode(tokens);
+            }
+
+            tokens.extend(encoded);
         }
     }
 
-    text_parts.join("\n")
+    encoder.decode(tokens)
 }
