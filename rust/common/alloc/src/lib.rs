0306ac5e44ed8a3f242509b7fe7d054a8e740a51
Eli Reisman <eli.r@posthog.com>
2025-10-01 19:34:53 -0700

    add pprof setup macro to common/alloc

--- rust/common/alloc/src/lib.rs
+++ rust/common/alloc/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(target_env = "msvc")]
 pub use std::alloc::System as DefaultAllocator;
+
 #[cfg(not(target_env = "msvc"))]
 pub use tikv_jemallocator::Jemalloc as DefaultAllocator;
 
@@ -8,5 +9,19 @@ macro_rules! used {
     () => {
         #[global_allocator]
         static GLOBAL: $crate::DefaultAllocator = $crate::DefaultAllocator;
+    };
+}
+
+// Use with jemalloc allocator as you would with used!() then follow these instructions:
+// https://www.polarsignals.com/blog/posts/2023/12/20/rust-memory-profiling
+#[macro_export]
+macro_rules! used_with_profiling {
+    () => {
+        #[global_allocator]
+        static GLOBAL: $crate::DefaultAllocator = $crate::DefaultAllocator;
+
+        #[allow(non_upper_case_globals)]
+        #[export_name = "malloc_conf"]
+        static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
     };
 }
