4cb3f3cd6ed5b4ade8630912c95809b63394a498
Rodrigo Iloro <rodrigo@posthog.com>
2025-10-06 10:26:42 -0300

    fix(experiments): create experiment persistance. (#39143)

--- frontend/src/scenes/experiments/create/ExperimentTypePanel.test.tsx
+++ frontend/src/scenes/experiments/create/ExperimentTypePanel.test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 import { cleanup, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
-import { NEW_EXPERIMENT } from '../../constants'
+import { NEW_EXPERIMENT } from '../constants'
 import { ExperimentTypePanel } from './ExperimentTypePanel'
 
 describe('ExperimentTypePanel', () => {
