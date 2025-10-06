e731a77dbe198170bc1be86c1575f60959e375c0
Yasen Slavov <yasen@posthog.com>
2025-10-06 12:33:18 +0300

    Make sure export and search endpoints for advanced activity logs contain the org_scoped query param

--- frontend/src/scenes/audit-logs/advancedActivityLogsLogic.tsx
+++ frontend/src/scenes/audit-logs/advancedActivityLogsLogic.tsx
@@ -197,6 +197,7 @@ export const advancedActivityLogsLogic = kea<advancedActivityLogsLogicType>([
 
                     params.append('page', (values.filters.page || 1).toString())
                     params.append('page_size', ADVANCED_ACTIVITY_PAGE_SIZE.toString())
+                    params.append('include_organization_scoped', '1')
 
                     const response = await api.get(`api/projects/@current/advanced_activity_logs/?${params}`)
                     return response
@@ -458,7 +459,7 @@ export const advancedActivityLogsLogic = kea<advancedActivityLogsLogicType>([
                     item_ids: values.filters.item_ids,
                 }
 
-                await api.create('api/projects/@current/advanced_activity_logs/export/', {
+                await api.create('api/projects/@current/advanced_activity_logs/export/?include_organization_scoped=1', {
                     format,
                     filters: filtersToExport,
                 })
