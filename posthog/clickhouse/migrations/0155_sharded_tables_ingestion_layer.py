0fb2d2fdb0d589cf7bcef3083a814b563ca2c413
Daniel Escribano <daniel.e@posthog.com>
2025-10-07 18:09:17 +0200

    chore: add ingestion warnings

--- posthog/clickhouse/migrations/0155_sharded_tables_ingestion_layer.py
+++ posthog/clickhouse/migrations/0155_sharded_tables_ingestion_layer.py
@@ -29,6 +29,13 @@ from posthog.models.app_metrics2.sql import (
     KAFKA_APP_METRICS2_TABLE_SQL,
     WRITABLE_APP_METRICS2_TABLE_SQL,
 )
+from posthog.models.ingestion_warnings.sql import (
+    DROP_INGESTION_WARNINGS_TABLE_MV_SQL,
+    DROP_KAFKA_INGESTION_WARNINGS_TABLE_SQL,
+    INGESTION_WARNINGS_MV_TABLE_SQL,
+    KAFKA_INGESTION_WARNINGS_TABLE_SQL,
+    WRITABLE_INGESTION_WARNINGS_TABLE_SQL,
+)
 from posthog.session_recordings.sql.session_replay_event_sql import (
     DROP_KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL,
     DROP_SESSION_REPLAY_EVENTS_TABLE_MV_SQL,
@@ -51,17 +58,32 @@ operations = [
     run_sql_with_exceptions(APP_METRICS2_MV_TABLE_SQL(), node_roles=[NodeRole.INGESTION_MEDIUM]),
     run_sql_with_exceptions(DROP_APP_METRICS_MV_TABLE_SQL, node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(DROP_KAFKA_APP_METRICS_TABLE_SQL, node_roles=[NodeRole.DATA]),
-    run_sql_with_exceptions(KAFKA_APP_METRICS_TABLE_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
-    run_sql_with_exceptions(WRITABLE_APP_METRICS_TABLE_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
-    run_sql_with_exceptions(APP_METRICS_MV_TABLE_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
+    run_sql_with_exceptions(KAFKA_APP_METRICS_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]),
+    run_sql_with_exceptions(WRITABLE_APP_METRICS_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]),
+    run_sql_with_exceptions(APP_METRICS_MV_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]),
     run_sql_with_exceptions(DROP_SESSION_REPLAY_EVENTS_TABLE_MV_SQL(), node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(DROP_KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL(), node_roles=[NodeRole.DATA]),
-    run_sql_with_exceptions(KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
-    run_sql_with_exceptions(WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
-    run_sql_with_exceptions(SESSION_REPLAY_EVENTS_TABLE_MV_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
+    run_sql_with_exceptions(
+        KAFKA_SESSION_REPLAY_EVENTS_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]
+    ),
+    run_sql_with_exceptions(
+        WRITABLE_SESSION_REPLAY_EVENTS_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]
+    ),
+    run_sql_with_exceptions(
+        SESSION_REPLAY_EVENTS_TABLE_MV_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]
+    ),
     run_sql_with_exceptions(DROP_LOG_ENTRIES_TABLE_MV_SQL, node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(DROP_KAFKA_LOG_ENTRIES_V3_TABLE_SQL, node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(LOG_ENTRIES_WRITABLE_TABLE_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
     run_sql_with_exceptions(KAFKA_LOG_ENTRIES_V3_TABLE_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
     run_sql_with_exceptions(LOG_ENTRIES_V3_TABLE_MV_SQL(), node_roles=[NodeRole.INGESTION_SMALL]),
+    run_sql_with_exceptions(DROP_INGESTION_WARNINGS_TABLE_MV_SQL, node_roles=[NodeRole.DATA]),
+    run_sql_with_exceptions(DROP_KAFKA_INGESTION_WARNINGS_TABLE_SQL, node_roles=[NodeRole.DATA]),
+    run_sql_with_exceptions(
+        WRITABLE_INGESTION_WARNINGS_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]
+    ),
+    run_sql_with_exceptions(
+        KAFKA_INGESTION_WARNINGS_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]
+    ),
+    run_sql_with_exceptions(INGESTION_WARNINGS_MV_TABLE_SQL(on_cluster=False), node_roles=[NodeRole.INGESTION_SMALL]),
 ]
