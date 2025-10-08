5dc86172ec9566ebf273b3f77b159443574bc5be
Oliver Browne <oliver@posthog.com>
2025-10-08 19:40:29 +0300

    use two pools, use persons one for persons and groups

--- rust/cymbal/src/config.rs
+++ rust/cymbal/src/config.rs
@@ -50,6 +50,9 @@ pub struct Config {
     #[envconfig(default = "postgres://posthog:posthog@localhost:5432/posthog")]
     pub database_url: String,
 
+    #[envconfig(default = "postgres://posthog:posthog@localhost:5432/posthog")]
+    pub persons_url: String,
+
     // Rust service connect directly to postgres, not via pgbouncer, so we keep this low
     #[envconfig(default = "4")]
     pub max_pg_connections: u32,
