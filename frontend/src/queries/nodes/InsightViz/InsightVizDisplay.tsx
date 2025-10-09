6e573e94c3d5514c1a542bd5b76c6044467e81a7
Georgis Andonis <georgis@posthog.com>
2025-10-09 11:36:46 +0300

    Add comment

--- frontend/src/queries/nodes/InsightViz/InsightVizDisplay.tsx
+++ frontend/src/queries/nodes/InsightViz/InsightVizDisplay.tsx
@@ -227,6 +227,7 @@ export function InsightVizDisplay({
                     )}
 
                     <InsightsTable
+                        // Do not show ribbons for world map insight table. All ribbons are nuances of blue, and do not bring any UX value
                         isLegend={display !== ChartDisplayType.WorldMap}
                         editMode={editMode}
                         filterKey={keyForInsightLogicProps('new')(insightProps)}
