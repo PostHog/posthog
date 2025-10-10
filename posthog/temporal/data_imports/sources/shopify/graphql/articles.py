9e4406c3bf92fc7a23159ddef6d3df06c81f3223
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-10-09 20:23:14 -0500

    generated first query manually. using claude for the rest

---
+++ posthog/temporal/data_imports/sources/shopify/graphql/articles.py
@@ -0,0 +1,32 @@
+ARTICLES_QUERY = """
+query PaginatedArticles($n: Int!, $cursor: String) {
+	articles(first: $n, after: $cursor) {
+		nodes {
+			author {
+				name
+			}
+			blog {
+				handle
+                id
+                tags
+				title
+			}
+			body
+            commentsCount
+			createdAt
+			handle
+			id
+			isPublished
+			publishedAt
+			summary
+			tags
+			templateSuffix
+			title
+			updatedAt
+		}
+		pageInfo {
+			hasNextPage
+			endCursor
+		}
+	}
+}"""
