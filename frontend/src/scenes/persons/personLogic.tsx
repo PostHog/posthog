620a30c0c287b08fa99f2b5d2494569887791f6c
Shao Wei <lshaowei18@gmail.com>
2025-10-10 10:42:55 +0800

    fix: update personLogic to fetch person with id

--- frontend/src/scenes/persons/personLogic.tsx
+++ frontend/src/scenes/persons/personLogic.tsx
@@ -4,6 +4,7 @@ import posthog from 'posthog-js'
 
 import api from 'lib/api'
 
+import { hogqlQuery } from '~/queries/query'
 import { hogql } from '~/queries/utils'
 import { PersonType } from '~/types'
 
@@ -11,7 +12,7 @@ import type { personLogicType } from './personLogicType'
 
 export interface PersonLogicProps {
     id: string | undefined
-    distinctId: string
+    distinctId: string | undefined
 }
 
 export interface Info {
@@ -22,7 +23,7 @@ export interface Info {
 
 export const personLogic = kea<personLogicType>([
     props({} as PersonLogicProps),
-    key((props) => props.distinctId),
+    key((props) => props.distinctId ?? props.id ?? 'undefined'),
     path((key) => ['scenes', 'persons', 'personLogic', key]),
     actions({
         loadPerson: true,
@@ -35,7 +36,30 @@ export const personLogic = kea<personLogicType>([
                 loadPerson: async (): Promise<PersonType | null> => {
                     const response = await api.persons.list({ distinct_id: props.distinctId })
                     const person = response.results[0]
-                    return person
+                    if (person != null) {
+                        return person
+                    }
+                    if (props.id == null) {
+                        return null
+                    }
+                    const queryResponse = await hogqlQuery(
+                        hogql`select id, groupArray(101)(pdi2.distinct_id) as distinct_ids, properties, is_identified, created_at from persons LEFT JOIN (SELECT argMax(pdi2.person_id, pdi2.version) AS person_id, pdi2.distinct_id AS distinct_id FROM raw_person_distinct_ids as pdi2 WHERE pdi2.person_id = {id} GROUP BY pdi2.distinct_id HAVING ifNull(equals(argMax(pdi2.is_deleted, pdi2.version), 0), 0)) as pdi2 ON pdi2.person_id=persons.id where id={id} group by id, properties, is_identified, created_at`,
+                        { id: props.id },
+                        'blocking'
+                    )
+                    const row = queryResponse?.results?.[0]
+                    if (row == null) {
+                        return null
+                    }
+                    const queryPerson: PersonType = {
+                        id: row[0],
+                        uuid: row[0],
+                        distinct_ids: row[1],
+                        properties: JSON.parse(row[2] || '{}'),
+                        is_identified: !!row[3],
+                        created_at: row[4],
+                    }
+                    return queryPerson
                 },
             },
         ],
