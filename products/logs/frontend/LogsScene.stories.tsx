46af77ef06d97cbf01cff973216fe116ddacefdf
Ben White <ben@posthog.com>
2025-10-10 16:50:09 +0200

    Fixes

--- products/logs/frontend/LogsScene.stories.tsx
+++ products/logs/frontend/LogsScene.stories.tsx
@@ -1,14 +1,16 @@
 import { Meta } from '@storybook/react'
 import { router } from 'kea-router'
+import { rest } from 'msw'
 import { useEffect } from 'react'
 
 import { dayjs } from 'lib/dayjs'
-import { uuid } from 'lib/utils'
+import { dateStringToDayJs, sampleOne, uuid } from 'lib/utils'
 import { App } from 'scenes/App'
 import { urls } from 'scenes/urls'
 
 import { mswDecorator } from '~/mocks/browser'
 import { MockSignature } from '~/mocks/utils'
+import { LogMessage, LogSeverityLevel } from '~/queries/schema/schema-general'
 
 const delayIfNotTestRunner = async (): Promise<void> => {
     await new Promise((resolve) => setTimeout(resolve, 200 + Math.random() * 1000))
@@ -52,64 +54,179 @@ const keysAndValues: Record<string, string[]> = {
     'log.iostream': ['stdout', 'stderr'],
 }
 
-const queryMock: MockSignature = async (req, res, ctx) => {
-    await delayIfNotTestRunner()
-    const results = []
-    for (let i = 0; i < 1000; i++) {
-        results.push({
-            uuid: uuid(),
-            trace_id: uuid(),
-            span_id: uuid(),
-            body: JSON.stringify({
-                timestamp: '2025-10-10T12:03:28.093626Z',
-                level: 'WARN',
-                fields: {
-                    message: 'Person properties not found in evaluation state cache',
-                },
-            }),
-            attributes: {
-                'k8s.pod.uid': uuid(),
-                level: 'WARN',
-                timestamp: '2025-10-10T12:03:28.093626Z',
-                'k8s.namespace.name': 'posthog',
-                'k8s.pod.start_time': '2025-10-10T08:36:19Z',
-                'k8s.pod.name': 'posthog-web',
-                'k8s.container.restart_count': '0',
-                'k8s.node.name': 'node-1',
-                'service.name': 'posthog-web',
-                'k8s.container.name': 'posthog-web',
-                'log.iostream': 'stdout',
+const EXAMPLES: Record<
+    string,
+    {
+        attributes: Record<string, string>
+        logs: { message: string; level: LogSeverityLevel; attributes?: Record<string, string> }[]
+    }
+> = {
+    'posthog-web': {
+        attributes: {
+            'k8s.namespace.name': 'posthog',
+            'service.name': 'posthog-web',
+            'k8s.pod.name': 'posthog-web',
+            'k8s.container.name': 'posthog-web',
+        },
+        logs: [
+            {
+                message:
+                    '{"request_id": "0904e6ff-da7e-4d66-af79-0c111bb47cab", "ip": "1.0.0.1", "event": "geoIP computation error: The address 172.0.1.1 is not in the database.",  "host": "us.i.posthog.com", "container_hostname": "posthog-web-django-c5f54bd98-cswsg", "timestamp": "2025-10-10T12:56:52.826524Z", "logger": "posthog.geoip", "level": "error", "pid": 65403, "tid": 281466655207264, "exception": "Traceback (most recent call last):\n File "/code/posthog/geoip.py", line 52, in get_geoip_properties\n geoip_properties = geoip.city(ip_address)\n ^^^^^^^^^^^^^^^^^^^^^^\n File "/python-runtime/lib/python3.11/site-packages/django/contrib/gis/geoip2/base.py", line 181, in city\n return City(self._city.city(enc_query))\n ^^^^^^^^^^^^^^^^^^^^^^^^^^\n File "/python-runtime/lib/python3.11/site-packages/geoip2/database.py", line 150, in city\n return cast(City, self._model_for(geoip2.models.City, "City", ip_address))\n ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n File "/python-runtime/lib/python3.11/site-packages/geoip2/database.py", line 253, in _model_for\n (record, prefix_len) = self._get(types, ip_address)\n ^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n File "/python-runtime/lib/python3.11/site-packages/geoip2/database.py", line 240, in _get\n raise geoip2.errors.AddressNotFoundError(\ngeoip2.errors.AddressNotFoundError: The address 172.0.1.1 is not in the database."}',
+                level: 'error',
             },
-            timestamp: '2025-10-10T12:03:28.093711',
-            observed_timestamp: '2025-10-10T12:03:28.435131',
-            severity_text: 'warn',
-            severity_number: 13,
-            level: 'warn',
-            resource_attributes: {},
-            instrumentation_scope: '@',
-            event_name: '',
+            {
+                message: `{"request_id": "8cbf3d86-5270-4129-a9a7-5c0908a6a806", "ip": "1.0.0.1", "request": "<ASGIRequest: OPTIONS '/array/123/config'>", "user_agent": "Mozilla/5.0 (iPad; CPU OS 18_6_2 like Mac OS X) AppleWebKit/605.1.15", "event": "request_started",  "host": "us-assets.i.posthog.com", "container_hostname": "posthog-web-django-c5f54bd98-z2kjj", "timestamp": "2025-10-10T12:58:08.090793Z", "logger": "django_structlog.middlewares.request", "level": "info", "pid": 44582, "tid": 281465917009760}`,
+                level: 'info',
+            },
+            {
+                message:
+                    '/python-runtime/lib/python3.11/site-packages/django/http/response.py:517: Warning: StreamingHttpResponse must consume synchronous iterators in order to serve them asynchronously. Use an asynchronous iterator instead.',
+                level: 'warn',
+            },
+        ],
+    },
+    'cdp-events-consumer': {
+        attributes: {
+            'k8s.namespace.name': 'posthog',
+            'service.name': 'cdp-events-consumer',
+            'k8s.pod.name': 'cdp-events-consumer',
+            'k8s.container.name': 'cdp-events-consumer',
+        },
+        logs: [
+            {
+                message: '[CDP-PROCESSED-EVENTS] 🔁 CdpEventsConsumer - handling batch',
+                level: 'info',
+                attributes: {
+                    size: '100',
+                },
+            },
+            {
+                message: '[CDP-PROCESSED-EVENTS] 🦔 [HogFunction] Filter took longer than expected',
+                level: 'warn',
+                attributes: {
+                    hog_function_id: '123',
+                    hog_function_name: 'Filter fail test',
+                    duration: '1000',
+                },
+            },
+        ],
+    },
+}
+
+// Make a deterministic log of all messages. We basicallty want to create a tonne of logs over an example time period with some deterministic randomness to it.
+
+const generateLogs = (): LogMessage[] => {
+    const results: LogMessage[] = []
+    const startTime = dayjs().subtract(24, 'hours')
+    const endTime = dayjs()
+
+    // Iterate each minute adding N logs to the results
+
+    let currentTime = startTime
+
+    while (currentTime.isBefore(endTime)) {
+        Object.values(EXAMPLES).forEach((example) => {
+            const logsToAdd = Math.floor(Math.random() * 10)
+            for (let i = 0; i < logsToAdd; i++) {
+                const log = sampleOne<(typeof example.logs)[0]>(example.logs)
+                results.push({
+                    uuid: uuid(),
+                    trace_id: uuid(),
+                    span_id: uuid(),
+                    resource: 'any',
+                    body: log.message,
+                    attributes: {
+                        ...example.attributes,
+                        ...log.attributes,
+                    },
+                    timestamp: currentTime.format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
+                    observed_timestamp: currentTime.format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
+                    severity_text: log.level,
+                    severity_number: 13,
+                    level: log.level,
+                    instrumentation_scope: 'any',
+                    event_name: 'any',
+                })
+            }
         })
+        currentTime = currentTime.add(1, 'minutes')
     }
 
-    return res(ctx.json({ results }))
+    return results
+}
+
+const ALL_LOGS_GENERATED = generateLogs()
+
+const getLogs = async (
+    req: Parameters<Parameters<(typeof rest)['get']>[1]>[0]
+): Promise<{
+    startTime: dayjs.Dayjs
+    endTime: dayjs.Dayjs
+    logs: LogMessage[]
+}> => {
+    const body = await req.json()
+    const limit = body.query?.limit ?? 100
+    const offset = body.query?.offset ?? 0
+    const severityLevels = body.query?.severityLevels ?? ['info', 'warn', 'error']
+
+    // TODO: Add filtering
+
+    const startDate = dateStringToDayJs(body.query?.dateRange?.date_from ?? null) ?? dayjs().subtract(30, 'minutes')
+    const endDate = dateStringToDayJs(body.query?.dateRange?.date_to ?? null) ?? dayjs()
+
+    const logs = ALL_LOGS_GENERATED.filter((log) => {
+        if (startDate && startDate.isAfter(dayjs(log.timestamp))) {
+            return false
+        }
+        if (endDate && endDate.isBefore(dayjs(log.timestamp))) {
+            return false
+        }
+        return severityLevels.includes(log.severity_text.toLowerCase())
+    }).slice(offset, offset + limit)
+
+    return {
+        startTime: startDate,
+        endTime: endDate,
+        logs,
+    }
+}
+
+const queryMock: MockSignature = async (req, res, ctx) => {
+    await delayIfNotTestRunner()
+
+    const { logs } = await getLogs(req)
+
+    return res(ctx.json({ results: logs }))
 }
 
 const sparklineMock: MockSignature = async (req, res, ctx) => {
     await delayIfNotTestRunner()
-    const startTime = dayjs().subtract(30, 'minutes')
-    const endTime = dayjs()
-    const interval = 1000 * 60 // 1 minute
+    const { startTime, endTime } = await getLogs(req)
+
+    // Count the logs per level/minute/interval
+
+    let intervalMins = 1 // 1 minute
+
+    if (endTime.diff(startTime, 'hours') < 1) {
+        intervalMins = 1
+    } else if (endTime.diff(startTime, 'hours') < 24) {
+        intervalMins = 5
+    } else if (endTime.diff(startTime, 'days') < 7) {
+        intervalMins = 60
+    }
+
+    // Group all the messages by the interval
+    // Iterate between the start and end time in the interval and count the logs per level
+
     const response = []
-
     let currentTime = startTime
-
     while (currentTime.isBefore(endTime)) {
         response.push({
             time: currentTime.format('YYYY-MM-DDTHH:mm:ssZ'),
             level: 'warn',
             count: 197999,
         })
-        currentTime = currentTime.add(interval, 'ms')
+        currentTime = currentTime.add(intervalMins, 'minutes')
     }
 
     return res(ctx.json(response))
