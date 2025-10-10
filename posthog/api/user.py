399ea660aced8f2609409feedc0796ade082f9b7
Alex <alex.lider@posthog.com>
2025-10-10 11:59:02 +0200

    Fix inline import

--- posthog/api/user.py
+++ posthog/api/user.py
@@ -52,7 +52,7 @@ from posthog.auth import (
 )
 from posthog.constants import PERMITTED_FORUM_DOMAINS
 from posthog.email import is_email_available
-from posthog.event_usage import report_user_updated, report_user_verified_email
+from posthog.event_usage import report_user_deleted_account, report_user_updated, report_user_verified_email
 from posthog.helpers.two_factor_session import set_two_factor_verified_in_session
 from posthog.middleware import get_impersonated_session_expires_at
 from posthog.models import Dashboard, Team, User, UserScenePersonalisation
@@ -445,8 +445,6 @@ class UserViewSet(
         }
 
     def perform_destroy(self, user: User) -> None:
-        from posthog.event_usage import report_user_deleted_account
-
         report_user_deleted_account(user)
         super().perform_destroy(user)
 
