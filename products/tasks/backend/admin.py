2345f47465b05ce295b6f2e1d09d5b0ddc3255c5
Joshua Snyder <joshua@posthog.com>
2025-10-14 20:21:50 +0100

    admin portal

--- products/tasks/backend/admin.py
+++ products/tasks/backend/admin.py
@@ -4,9 +4,8 @@ from django.utils.html import format_html
 from products.tasks.backend.models import SandboxSnapshot
 
 
-@admin.register(SandboxSnapshot)
 class SandboxSnapshotAdmin(admin.ModelAdmin):
-    list_display = ("external_id", "status", "repo_count", "integration_link", "created_at", "updated_at")
+    list_display = ("external_id", "status", "integration_link", "created_at", "updated_at")
     list_filter = ("status", "created_at")
     search_fields = ("external_id", "repos")
     readonly_fields = ("id", "external_id", "created_at", "updated_at")
@@ -18,11 +17,6 @@ class SandboxSnapshotAdmin(admin.ModelAdmin):
         ("Metadata", {"fields": ("metadata",)}),
         ("Dates", {"fields": ("created_at", "updated_at")}),
     )
-
-    def repo_count(self, obj: SandboxSnapshot) -> int:
-        return len(obj.repos)
-
-    repo_count.short_description = "Repositories"
 
     def integration_link(self, obj: SandboxSnapshot) -> str:
         if obj.integration:
