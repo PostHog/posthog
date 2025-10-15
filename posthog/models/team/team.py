5ec665c1bd463d6dc01fab9f985171d32acdcc38
Dylan Martin <dylan@posthog.com>
2025-10-15 09:49:30 -0700

    feat(flags): add the backend for the default evaluation environments (#39665)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- posthog/models/team/team.py
+++ posthog/models/team/team.py
@@ -384,6 +384,12 @@ class Team(UUIDTClassicModel):
     flags_persistence_default = models.BooleanField(null=True, blank=True, default=False)
     feature_flag_confirmation_enabled = models.BooleanField(null=True, blank=True, default=False)
     feature_flag_confirmation_message = models.TextField(null=True, blank=True)
+    default_evaluation_environments_enabled = models.BooleanField(
+        null=True,
+        blank=True,
+        default=False,
+        help_text="Whether to automatically apply default evaluation environments to new feature flags",
+    )
     session_recording_version = models.CharField(null=True, blank=True, max_length=24)
     signup_token = models.CharField(max_length=200, null=True, blank=True)
     is_demo = models.BooleanField(default=False)
