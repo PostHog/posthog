5af3aa90d5bd2493d4d411a91d507563ef282057
Julian Bez <julian@posthog.com>
2025-10-15 15:40:44 +0200

    test: second backend test for baseline comparison

--- posthog/__init__.py
+++ posthog/__init__.py
@@ -3,7 +3,7 @@
 from posthog.celery import app as celery_app  # noqa: E402
 
 __all__ = ("celery_app",)
-# Testing layer cache with source change
+# Test build cache - 2025-10-15 15:40
 
 # snowflake-connector-python tries to access a root folder which errors out in pods.
 # This sets the snowflake home directory to a relative folder
