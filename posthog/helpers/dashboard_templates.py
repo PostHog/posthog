9a530205c2fef8bf3fef0ceeda11636b3e85500d
Shao Wei <lshaowei18@gmail.com>
2025-10-16 14:08:55 +0800

    fix: get the actual team in create_group_type_mapping_detail_dashboard

--- posthog/helpers/dashboard_templates.py
+++ posthog/helpers/dashboard_templates.py
@@ -700,10 +700,15 @@ def create_group_type_mapping_detail_dashboard(group_type_mapping, user) -> Dash
     singular = group_type_mapping.name_singular or group_type_mapping.group_type
     plural = group_type_mapping.name_plural or group_type_mapping.group_type + "s"
 
+    # Import here to prevent circular import
+    from posthog.models.team import Team
+
+    team = Team.objects.get(id=group_type_mapping.team_id)
+
     dashboard = Dashboard.objects.create(
         name=f"Template dashboard for {singular} overview",
         description=f"This dashboard template powers the Overview page for all {plural}. Any insights will automatically filter to the selected {singular}.",
-        team=group_type_mapping.team,
+        team=team,
         created_by=user,
         creation_mode="template",
     )
