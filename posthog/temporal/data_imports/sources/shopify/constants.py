4035de8c27afe09124e95f1bcf84a834985e448a
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-10-16 15:30:28 -0500

    move graphql_resource to utils

--- posthog/temporal/data_imports/sources/shopify/constants.py
+++ posthog/temporal/data_imports/sources/shopify/constants.py
@@ -1,11 +1,11 @@
-from posthog.temporal.data_imports.sources.common.graphql_source.typing import GraphQLResource
-
 from .graphql.abandoned_checkouts import ABANDONED_CHECKOUTS_QUERY
 from .graphql.articles import ARTICLES_QUERY
-from .graphql.shopify_payments_balance_transactions import SHOPIFY_PAYMENTS_BALANCE_TRANSACTIONS_QUERY
+from .graphql.shopify_payments_account_balance_transactions import SHOPIFY_PAYMENTS_ACCOUNT_BALANCE_TRANSACTIONS_QUERY
+from .utils import ShopifyGraphQLObject
 
 SHOPIFY_API_VERSION = "2025-10"
 SHOPIFY_API_URL = "https://{}.myshopify.com/admin/api/{}/graphql.json"
+SHOPIFY_DEFAULT_PAGE_SIZE = 100
 
 SHOPIFY_ACCESS_TOKEN_CHECK = "{ shop { id } }"
 
@@ -51,72 +51,71 @@ PRODUCT_VARIANTS = "productVariants"
 PRODUCTS = "products"
 PROFILE_LOCATION_GROUPS = "profileLocationGroups"
 SHOP = "shop"
-SHOPIFY_PAYMENTS_BALANCE_TRANSACTIONS = "shopifyPaymentsBalanceTransactions"
+SHOPIFY_PAYMENTS_ACCOUNT_BALANCE_TRANSACTIONS = "shopifyPaymentsAccount.balanceTransactions"
 SMART_COLLECTIONS = "smartCollections"
 TENDER_TRANSACTIONS = "tenderTransactions"
 TRANSACTIONS = "transactions"
 
 
-# NOTE: some of the permissions queries may seem random. not every field on a given resource
+# NOTE: some of the permissions queries may seem random. not every field on a given graphql object
 # have the same access requirements
 SHOPIFY_RESOURCES = {
-    ABANDONED_CHECKOUTS: GraphQLResource(
+    ABANDONED_CHECKOUTS: ShopifyGraphQLObject(
         name=ABANDONED_CHECKOUTS,
         query=ABANDONED_CHECKOUTS_QUERY,
         permissions_query="{ abandonedCheckoutsCount(limit: 1) { count } }",
     ),
-    ARTICLES: GraphQLResource(
+    ARTICLES: ShopifyGraphQLObject(
         name=ARTICLES,
         query=ARTICLES_QUERY,
         permissions_query="{ articles(first: 1) { nodes { commentsCount(limit: 1) { count } } } }",
     ),
-    # BLOGS: GraphQLResource(name=BLOGS, query=""),
-    # COLLECTIONS: GraphQLResource(name=COLLECTIONS, query=""),
-    # COLLECTS: GraphQLResource(name=COLLECTS, query=""),
-    # COUNTRIES: GraphQLResource(name=COUNTRIES, query=""),
-    # CUSTOM_COLLECTIONS: GraphQLResource(name=CUSTOM_COLLECTIONS, query=""),
-    # CUSTOMER_ADDRESS: GraphQLResource(name=CUSTOMER_ADDRESS, query=""),
-    # CUSTOMER_JOURNEY_SUMMARY: GraphQLResource(name=CUSTOMER_JOURNEY_SUMMARY, query=""),
-    # CUSTOMERS: GraphQLResource(name=CUSTOMERS, query=""),
-    # DISCOUNT_CODES: GraphQLResource(name=DISCOUNT_CODES, query=""),
-    # DISPUTES: GraphQLResource(name=DISPUTES, query=""),
-    # DRAFT_ORDERS: GraphQLResource(name=DRAFT_ORDERS, query=""),
-    # FULFILLMENT_ORDERS: GraphQLResource(name=FULFILLMENT_ORDERS, query=""),
-    # FULFILLMENTS: GraphQLResource(name=FULFILLMENTS, query=""),
-    # INVENTORY_ITEMS: GraphQLResource(name=INVENTORY_ITEMS, query=""),
-    # INVENTORY_LEVELS: GraphQLResource(name=INVENTORY_LEVELS, query=""),
-    # LOCATIONS: GraphQLResource(name=LOCATIONS, query=""),
-    # METAFIELD_ARTICLES: GraphQLResource(name=METAFIELD_ARTICLES, query=""),
-    # METAFIELD_BLOGS: GraphQLResource(name=METAFIELD_BLOGS, query=""),
-    # METAFIELD_COLLECTIONS: GraphQLResource(name=METAFIELD_COLLECTIONS, query=""),
-    # METAFIELD_CUSTOMERS: GraphQLResource(name=METAFIELD_CUSTOMERS, query=""),
-    # METAFIELD_DRAFT_ORDERS: GraphQLResource(name=METAFIELD_DRAFT_ORDERS, query=""),
-    # METAFIELD_LOCATIONS: GraphQLResource(name=METAFIELD_LOCATIONS, query=""),
-    # METAFIELD_ORDERS: GraphQLResource(name=METAFIELD_ORDERS, query=""),
-    # METAFIELD_PAGES: GraphQLResource(name=METAFIELD_PAGES, query=""),
-    # METAFIELD_PRODUCT_IMAGES: GraphQLResource(name=METAFIELD_PRODUCT_IMAGES, query=""),
-    # METAFIELD_PRODUCT_VARIANTS: GraphQLResource(name=METAFIELD_PRODUCT_VARIANTS, query=""),
-    # METAFIELD_PRODUCTS: GraphQLResource(name=METAFIELD_PRODUCTS, query=""),
-    # METAFIELD_SHOPS: GraphQLResource(name=METAFIELD_SHOPS, query=""),
-    # METAFIELD_SMART_COLLECTIONS: GraphQLResource(name=METAFIELD_SMART_COLLECTIONS, query=""),
-    # ORDER_AGREEMENTS: GraphQLResource(name=ORDER_AGREEMENTS, query=""),
-    # ORDER_REFUNDS: GraphQLResource(name=ORDER_REFUNDS, query=""),
-    # ORDER_RISKS: GraphQLResource(name=ORDER_RISKS, query=""),
-    # ORDERS: GraphQLResource(name=ORDERS, query=""),
-    # PAGES: GraphQLResource(name=PAGES, query=""),
-    # PRICE_RULES: GraphQLResource(name=PRICE_RULES, query=""),
-    # PRODUCT_IMAGES: GraphQLResource(name=PRODUCT_IMAGES, query=""),
-    # PRODUCT_VARIANTS: GraphQLResource(name=PRODUCT_VARIANTS, query=""),
-    # PRODUCTS: GraphQLResource(name=PRODUCTS, query=""),
-    # PROFILE_LOCATION_GROUPS: GraphQLResource(name=PROFILE_LOCATION_GROUPS, query=""),
-    # SHOP: GraphQLResource(name=SHOP, query=""),
-    SHOPIFY_PAYMENTS_BALANCE_TRANSACTIONS: GraphQLResource(
-        name=SHOPIFY_PAYMENTS_BALANCE_TRANSACTIONS,
-        query=SHOPIFY_PAYMENTS_BALANCE_TRANSACTIONS_QUERY,
+    # BLOGS: ShopifyGraphQLObject(name=BLOGS, query=""),
+    # COLLECTIONS: ShopifyGraphQLObject(name=COLLECTIONS, query=""),
+    # COLLECTS: ShopifyGraphQLObject(name=COLLECTS, query=""),
+    # COUNTRIES: ShopifyGraphQLObject(name=COUNTRIES, query=""),
+    # CUSTOM_COLLECTIONS: ShopifyGraphQLObject(name=CUSTOM_COLLECTIONS, query=""),
+    # CUSTOMER_ADDRESS: ShopifyGraphQLObject(name=CUSTOMER_ADDRESS, query=""),
+    # CUSTOMER_JOURNEY_SUMMARY: ShopifyGraphQLObject(name=CUSTOMER_JOURNEY_SUMMARY, query=""),
+    # CUSTOMERS: ShopifyGraphQLObject(name=CUSTOMERS, query=""),
+    # DISCOUNT_CODES: ShopifyGraphQLObject(name=DISCOUNT_CODES, query=""),
+    # DISPUTES: ShopifyGraphQLObject(name=DISPUTES, query=""),
+    # DRAFT_ORDERS: ShopifyGraphQLObject(name=DRAFT_ORDERS, query=""),
+    # FULFILLMENT_ORDERS: ShopifyGraphQLObject(name=FULFILLMENT_ORDERS, query=""),
+    # FULFILLMENTS: ShopifyGraphQLObject(name=FULFILLMENTS, query=""),
+    # INVENTORY_ITEMS: ShopifyGraphQLObject(name=INVENTORY_ITEMS, query=""),
+    # INVENTORY_LEVELS: ShopifyGraphQLObject(name=INVENTORY_LEVELS, query=""),
+    # LOCATIONS: ShopifyGraphQLObject(name=LOCATIONS, query=""),
+    # METAFIELD_ARTICLES: ShopifyGraphQLObject(name=METAFIELD_ARTICLES, query=""),
+    # METAFIELD_BLOGS: ShopifyGraphQLObject(name=METAFIELD_BLOGS, query=""),
+    # METAFIELD_COLLECTIONS: ShopifyGraphQLObject(name=METAFIELD_COLLECTIONS, query=""),
+    # METAFIELD_CUSTOMERS: ShopifyGraphQLObject(name=METAFIELD_CUSTOMERS, query=""),
+    # METAFIELD_DRAFT_ORDERS: ShopifyGraphQLObject(name=METAFIELD_DRAFT_ORDERS, query=""),
+    # METAFIELD_LOCATIONS: ShopifyGraphQLObject(name=METAFIELD_LOCATIONS, query=""),
+    # METAFIELD_ORDERS: ShopifyGraphQLObject(name=METAFIELD_ORDERS, query=""),
+    # METAFIELD_PAGES: ShopifyGraphQLObject(name=METAFIELD_PAGES, query=""),
+    # METAFIELD_PRODUCT_IMAGES: ShopifyGraphQLObject(name=METAFIELD_PRODUCT_IMAGES, query=""),
+    # METAFIELD_PRODUCT_VARIANTS: ShopifyGraphQLObject(name=METAFIELD_PRODUCT_VARIANTS, query=""),
+    # METAFIELD_PRODUCTS: ShopifyGraphQLObject(name=METAFIELD_PRODUCTS, query=""),
+    # METAFIELD_SHOPS: ShopifyGraphQLObject(name=METAFIELD_SHOPS, query=""),
+    # METAFIELD_SMART_COLLECTIONS: ShopifyGraphQLObject(name=METAFIELD_SMART_COLLECTIONS, query=""),
+    # ORDER_AGREEMENTS: ShopifyGraphQLObject(name=ORDER_AGREEMENTS, query=""),
+    # ORDER_REFUNDS: ShopifyGraphQLObject(name=ORDER_REFUNDS, query=""),
+    # ORDER_RISKS: ShopifyGraphQLObject(name=ORDER_RISKS, query=""),
+    # ORDERS: ShopifyGraphQLObject(name=ORDERS, query=""),
+    # PAGES: ShopifyGraphQLObject(name=PAGES, query=""),
+    # PRICE_RULES: ShopifyGraphQLObject(name=PRICE_RULES, query=""),
+    # PRODUCT_IMAGES: ShopifyGraphQLObject(name=PRODUCT_IMAGES, query=""),
+    # PRODUCT_VARIANTS: ShopifyGraphQLObject(name=PRODUCT_VARIANTS, query=""),
+    # PRODUCTS: ShopifyGraphQLObject(name=PRODUCTS, query=""),
+    # PROFILE_LOCATION_GROUPS: ShopifyGraphQLObject(name=PROFILE_LOCATION_GROUPS, query=""),
+    # SHOP: ShopifyGraphQLObject(name=SHOP, query=""),
+    SHOPIFY_PAYMENTS_ACCOUNT_BALANCE_TRANSACTIONS: ShopifyGraphQLObject(
+        name=SHOPIFY_PAYMENTS_ACCOUNT_BALANCE_TRANSACTIONS,
+        query=SHOPIFY_PAYMENTS_ACCOUNT_BALANCE_TRANSACTIONS_QUERY,
         permissions_query="{ shop { id } }",
-        accessor="data.shopifyPaymentsAccount.balanceTransactions",
     ),
-    # SMART_COLLECTIONS: GraphQLResource(name=SMART_COLLECTIONS, query=""),
-    # TENDER_TRANSACTIONS: GraphQLResource(name=TENDER_TRANSACTIONS, query=""),
-    # TRANSACTIONS: GraphQLResource(name=TRANSACTIONS, query=""),
+    # SMART_COLLECTIONS: ShopifyGraphQLObject(name=SMART_COLLECTIONS, query=""),
+    # TENDER_TRANSACTIONS: ShopifyGraphQLObject(name=TENDER_TRANSACTIONS, query=""),
+    # TRANSACTIONS: ShopifyGraphQLObject(name=TRANSACTIONS, query=""),
 }
