5f565e3c84f4f702b978775eca9844d26a64b23e
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-10-17 10:28:31 -0500

    i think __init__.py fixes this weird file name lookup issue in tests

--- posthog/temporal/data_imports/sources/shopify/source.py
+++ posthog/temporal/data_imports/sources/shopify/source.py
@@ -13,7 +13,7 @@ from posthog.temporal.data_imports.sources.common.registry import SourceRegistry
 from posthog.temporal.data_imports.sources.common.schema import SourceSchema
 from posthog.temporal.data_imports.sources.generated_configs import ShopifySourceConfig
 from posthog.temporal.data_imports.sources.shopify.constants import SHOPIFY_GRAPHQL_OBJECTS
-from posthog.temporal.data_imports.sources.shopify.settings import INCREMENTAL_FIELDS
+from posthog.temporal.data_imports.sources.shopify.settings import INCREMENTAL_SETTINGS
 from posthog.temporal.data_imports.sources.shopify.shopify import (
     ShopifyPermissionError,
     shopify_source,
@@ -79,15 +79,18 @@ The simplest setup for permissions is to only allow **read** permissions for the
             return False, str(e)
 
     def get_schemas(self, config: ShopifySourceConfig, team_id: int, with_counts: bool = False) -> list[SourceSchema]:
-        return [
-            SourceSchema(
-                name=object_name,
-                supports_incremental=True,
-                supports_append=True,
-                incremental_fields=INCREMENTAL_FIELDS.get(object_name, []),
+        schemas = []
+        for object_name in SHOPIFY_GRAPHQL_OBJECTS:
+            incremental = INCREMENTAL_SETTINGS.get(object_name)
+            schemas.append(
+                SourceSchema(
+                    name=object_name,
+                    supports_incremental=True,
+                    supports_append=True,
+                    incremental_fields=incremental.fields if incremental else [],
+                )
             )
-            for object_name in SHOPIFY_GRAPHQL_OBJECTS
-        ]
+        return schemas
 
     def source_for_pipeline(self, config: ShopifySourceConfig, inputs: SourceInputs) -> SourceResponse:
         return shopify_source(
