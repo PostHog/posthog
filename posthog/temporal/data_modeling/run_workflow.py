8f7607b316dee4d2e0926e57860a8b76f64a5fb4
Tom Owers <owerstom@gmail.com>
2025-10-17 11:56:47 +0100

    When copying to S3, delete files after the new have copied successfully

--- posthog/temporal/data_modeling/run_workflow.py
+++ posthog/temporal/data_modeling/run_workflow.py
@@ -578,6 +578,7 @@ async def materialize_model(
         file_uris=file_uris,
         preserve_table_name_casing=True,
         use_timestamped_folders=True,
+        logger=logger,
     )
 
     saved_query.is_materialized = True
