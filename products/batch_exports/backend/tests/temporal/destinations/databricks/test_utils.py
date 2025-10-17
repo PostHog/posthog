40c76258af13cdd85b4b466651ead47503e4eb33
Ross <rossagray@gmail.com>
2025-10-17 12:45:59 +0100

    Apply suggestion from @graphite-app[bot]
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- products/batch_exports/backend/tests/temporal/destinations/databricks/test_utils.py
+++ products/batch_exports/backend/tests/temporal/destinations/databricks/test_utils.py
@@ -15,7 +15,7 @@ from products.batch_exports.backend.temporal.destinations.databricks_batch_expor
         # when the interval is 1 hour we expect the timeout to be 2 hours (as we multiply the interval by 2 for now while we are in beta testing)
         (dt.datetime(2025, 1, 1, 12, 0, 0), dt.datetime(2025, 1, 1, 13, 0, 0), 2 * 60 * 60),
         # when interval is 5 minutes, we expect the timeout to be the minimum timeout of 30 minutes
-        (dt.datetime(2025, 1, 1, 12, 0, 0), dt.datetime(2025, 1, 1, 12, 0, 5), 30 * 60),
+        (dt.datetime(2025, 1, 1, 12, 0, 0), dt.datetime(2025, 1, 1, 12, 5, 0), 30 * 60),
     ],
 )
 def test_get_long_running_query_timeout(data_interval_start, data_interval_end, expected_timeout):
