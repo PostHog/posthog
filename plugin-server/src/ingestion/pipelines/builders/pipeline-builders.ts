a06f815b3db5d6bbc6ceb5d87442951cfd02a8aa
Paweł Ledwoń <me@difficult.name>
2025-10-18 14:24:53 -0600

    feat: implement branching pipeline

--- plugin-server/src/ingestion/pipelines/builders/pipeline-builders.ts
+++ plugin-server/src/ingestion/pipelines/builders/pipeline-builders.ts
@@ -1,3 +1,4 @@
+import { BranchDecisionFn, BranchingPipeline } from '../branching-pipeline'
 import { Pipeline } from '../pipeline.interface'
 import { RetryingPipeline, RetryingPipelineOptions } from '../retrying-pipeline'
 import { StartPipeline } from '../start-pipeline'
@@ -25,7 +26,42 @@ export class PipelineBuilder<TInput, TOutput, C> {
         return new PipelineBuilder(new StepPipeline(step, this.pipeline))
     }
 
+    branching<TBranch extends string, U>(
+        decisionFn: BranchDecisionFn<TOutput, TBranch>,
+        callback: (builder: BranchingPipelineBuilder<TInput, TOutput, U, C, TBranch>) => void
+    ): PipelineBuilder<TInput, U, C> {
+        const branchingBuilder = new BranchingPipelineBuilder<TInput, TOutput, U, C, TBranch>(decisionFn, this.pipeline)
+        callback(branchingBuilder)
+        return new PipelineBuilder(branchingBuilder.build())
+    }
+
     build(): Pipeline<TInput, TOutput, C> {
         return this.pipeline
+    }
+}
+
+export class BranchingPipelineBuilder<TInput, TIntermediate, TOutput, C, TBranch extends string> {
+    private branches: Partial<Record<TBranch, Pipeline<TIntermediate, TOutput, C>>> = {}
+
+    constructor(
+        private decisionFn: BranchDecisionFn<TIntermediate, TBranch>,
+        private previousPipeline: Pipeline<TInput, TIntermediate, C>
+    ) {}
+
+    branch(
+        branchName: TBranch,
+        callback: (builder: StartPipelineBuilder<TIntermediate, C>) => PipelineBuilder<TIntermediate, TOutput, C>
+    ): BranchingPipelineBuilder<TInput, TIntermediate, TOutput, C, TBranch> {
+        const branchPipeline = callback(new StartPipelineBuilder<TIntermediate, C>()).build()
+        this.branches[branchName] = branchPipeline
+        return this
+    }
+
+    build(): Pipeline<TInput, TOutput, C> {
+        return new BranchingPipeline(
+            this.decisionFn,
+            this.branches as Record<TBranch, Pipeline<TIntermediate, TOutput, C>>,
+            this.previousPipeline
+        )
     }
 }
