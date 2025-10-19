dcb0f344705d97891b9495508e6e5b2480b662bc
Alex Lebedev <alex.l@posthog.com>
2025-10-19 10:38:59 +0200

    feat: Add embeddings requests.

---
+++ ee/hogai/traces_summaries/tools/get_embeddings.py
@@ -0,0 +1,76 @@
+import os
+import json
+import asyncio
+
+import httpx
+import numpy as np
+import jmespath
+import structlog
+from tenacity import RetryCallState, retry, retry_if_exception_type, stop_after_attempt, wait_fixed, wait_random
+
+logger = structlog.get_logger(__name__)
+
+EMBEDDINGS_API_URL = "https://api.openai.com/v1/embeddings"
+EMBEDDINGS_MODEL = "text-embedding-3-large"
+EMBEDDINGS_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+class RetryableException(Exception):
+    pass
+
+
+def failed_get_embeddings(
+    retry_state: RetryCallState,
+) -> None:
+    logger.error(
+        f"Couldn't get embedding with {retry_state.attempt_number} attemps ({round(retry_state.idle_for, 2)}s)."
+    )
+    return None
+
+
+@retry(
+    retry=retry_if_exception_type(RetryableException),
+    stop=stop_after_attempt(5),
+    wait=wait_fixed(1) + wait_random(0, 3),
+    retry_error_callback=failed_get_embeddings,
+)
+async def get_embeddings(
+    client: httpx.AsyncClient,
+    embeddings_input: list[str],
+) -> list[list[float]]:
+    input_data = {"input": embeddings_input, "model": EMBEDDINGS_MODEL}
+    try:
+        raw_response = await client.post(
+            url=EMBEDDINGS_API_URL,
+            json=input_data,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {EMBEDDINGS_API_KEY}",
+            },
+        )
+        response = raw_response.json()
+    except httpx.HTTPStatusError as err:
+        message = f"Request error when getting embeddings: {err}"
+        logger.exception(message)
+        raise RetryableException(message)
+    embeddings = jmespath.search("data[].embedding", response)
+    if not embeddings:
+        message = f"Couldn't get embeddings ({embeddings_input}), no embeddings returned ({response.json()}). Retrying."
+        logger.exception(message)
+        raise RetryableException(message)
+    if len(embeddings) != len(embeddings_input):
+        message = (
+            f"Got {len(embeddings)} embeddings for {len(embeddings_input)} " f"inputs ({embeddings_input}). Retrying."
+        )
+        logger.exception(message)
+        raise RetryableException(message)
+    return embeddings
+
+
+# TODO: Remove after testing
+if __name__ == "__main__":
+    waka = asyncio.run(get_embeddings(client=httpx.AsyncClient(), embeddings_input=["Hello, world!"]))
+    with open("waka.json", "w") as f:
+        json.dump(waka, f)
+    waka_np = np.array(waka)
+    np.save("waka.npy", waka_np)
