51135af526e88aa4adabb7d915e73fe2e11e1f8d
kappa90 <e.capparelli@gmail.com>
2025-10-20 10:40:30 +0100

    feat(max): update nodes to use dispatcher

--- products/error_tracking/backend/max_tools.py
+++ products/error_tracking/backend/max_tools.py
@@ -39,7 +39,6 @@ class UpdateIssueQueryArgs(BaseModel):
 class ErrorTrackingIssueFilteringTool(MaxTool):
     name: str = "filter_error_tracking_issues"
     description: str = "Update the error tracking issue list, editing search query, property filters, date ranges, assignee and status filters."
-    thinking_message: str = "Updating your error tracking filters..."
     context_prompt_template: str = "Current issue filters are: {current_query}"
     args_schema: type[BaseModel] = UpdateIssueQueryArgs
 
@@ -173,7 +172,6 @@ class IssueImpactQueryArgs(BaseModel):
 class ErrorTrackingIssueImpactTool(MaxTool):
     name: str = "find_error_tracking_impactful_issue_event_list"
     description: str = "Find a list of events that relate to a user query about issues. Prioritise this tool when a user specifically asks about issues or problems."
-    thinking_message: str = "Finding related issues"
     context_prompt_template: str = "The user wants to find a list of events whose occurrence may be impacted by issues."
     args_schema: type[BaseModel] = IssueImpactQueryArgs
 
