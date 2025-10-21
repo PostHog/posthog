6e260f2438cb7f21f3baaaa446940332f9588b0b
Robbie Coomber <robbie.coomber@gmail.com>
2025-10-21 11:45:15 +0100

    Add node_roles=[NodeRole.DATA]

--- posthog/clickhouse/migrations/0158_sessions_v3_remake.py
+++ posthog/clickhouse/migrations/0158_sessions_v3_remake.py
@@ -21,7 +21,7 @@ operations = [
     run_sql_with_exceptions(DROP_RAW_SESSION_SHARDED_TABLE_SQL_V3()),
     # recreate
     run_sql_with_exceptions(SHARDED_RAW_SESSIONS_TABLE_SQL_V3(), node_roles=[NodeRole.DATA]),
-    run_sql_with_exceptions(WRITABLE_RAW_SESSIONS_TABLE_SQL_V3()),
+    run_sql_with_exceptions(WRITABLE_RAW_SESSIONS_TABLE_SQL_V3(), node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(DISTRIBUTED_RAW_SESSIONS_TABLE_SQL_V3(), node_roles=[NodeRole.DATA, NodeRole.COORDINATOR]),
-    run_sql_with_exceptions(RAW_SESSIONS_CREATE_OR_REPLACE_VIEW_SQL_V3()),
+    run_sql_with_exceptions(RAW_SESSIONS_CREATE_OR_REPLACE_VIEW_SQL_V3(), node_roles=[NodeRole.DATA]),
 ]
