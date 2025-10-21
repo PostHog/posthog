ef49b666a592c9f83f0c01c04da2e32c93d46fae
Michael Matloka <michael@matloka.com>
2025-10-21 21:19:49 +0200

    Bring back Max subtitle

--- frontend/src/scenes/max/Intro.tsx
+++ frontend/src/scenes/max/Intro.tsx
@@ -1,11 +1,17 @@
 import { useValues } from 'kea'
 
+import { dayjs } from 'lib/dayjs'
+import { userLogic } from 'scenes/userLogic'
+
 import { Logomark } from '~/toolbar/assets/Logomark'
 
 import { maxLogic } from './maxLogic'
 
 export function Intro(): JSX.Element {
     const { headline } = useValues(maxLogic)
+    const { user } = useValues(userLogic)
+
+    const shouldShowMaxRebrandMessage: boolean = !!user && dayjs(user.date_joined).isBefore('2025-10-21')
 
     return (
         <>
@@ -14,6 +20,11 @@ export function Intro(): JSX.Element {
             </div>
             <div className="text-center mb-1">
                 <h2 className="text-xl @md/max-welcome:text-2xl font-bold my-2 text-balance">{headline}</h2>
+                <div className="text-sm italic text-tertiary text-pretty py-0.5">
+                    {shouldShowMaxRebrandMessage
+                        ? 'Max AI is now PostHog AI – a core part of PostHog.'
+                        : 'Build something people want.'}
+                </div>
             </div>
         </>
     )
