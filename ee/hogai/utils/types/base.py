33fac2f2e6145a5f586d39c44b1404080891d6fe
denakorita <denada@posthog.com>
2025-10-21 10:47:16 +0200

    remove commented out things

--- ee/hogai/utils/types/base.py
+++ ee/hogai/utils/types/base.py
@@ -440,8 +440,3 @@ class WithCommentary(BaseModel):
     commentary: str = Field(
         description="A commentary on what you are doing, using the first person: 'I am doing this because...'"
     )
-
-
-# EntityType = Literal[
-#     "insight", "dashboard", "cohort", "action", "experiment", "feature_flag", "notebook", "survey", "event_definition"
-# ]
