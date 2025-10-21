a3a9f554fc06a515a44ceff6ac5cedbe6c18d176
Robbie Coomber <robbie.coomber@gmail.com>
2025-10-21 12:07:37 +0100

    Add coordinator to the view

--- posthog/clickhouse/migrations/0158_sessions_v3_remake.py
+++ posthog/clickhouse/migrations/0158_sessions_v3_remake.py
@@ -23,5 +23,7 @@ operations = [
     run_sql_with_exceptions(SHARDED_RAW_SESSIONS_TABLE_SQL_V3(), node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(WRITABLE_RAW_SESSIONS_TABLE_SQL_V3(), node_roles=[NodeRole.DATA]),
     run_sql_with_exceptions(DISTRIBUTED_RAW_SESSIONS_TABLE_SQL_V3(), node_roles=[NodeRole.DATA, NodeRole.COORDINATOR]),
-    run_sql_with_exceptions(RAW_SESSIONS_CREATE_OR_REPLACE_VIEW_SQL_V3(), node_roles=[NodeRole.DATA]),
+    run_sql_with_exceptions(
+        RAW_SESSIONS_CREATE_OR_REPLACE_VIEW_SQL_V3(), node_roles=[NodeRole.DATA, NodeRole.COORDINATOR]
+    ),
 ]
