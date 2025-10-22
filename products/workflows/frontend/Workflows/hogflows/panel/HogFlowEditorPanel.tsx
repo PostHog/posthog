31ca35e480e5cb87b9f0dcdf7ce5b1d4c9d29d2c
Haven <haven@posthog.com>
2025-10-21 20:22:39 -0500

    tweak

--- products/workflows/frontend/Workflows/hogflows/panel/HogFlowEditorPanel.tsx
+++ products/workflows/frontend/Workflows/hogflows/panel/HogFlowEditorPanel.tsx
@@ -27,7 +27,7 @@ export function HogFlowEditorPanel(): JSX.Element | null {
         key: mode,
     }))
 
-    const width = mode !== 'build' ? '36rem' : selectedNode ? '36rem' : '24rem'
+    const width = mode !== 'build' ? '37rem' : selectedNode ? '37rem' : '24rem'
 
     const Step = useHogFlowStep(selectedNode?.data)
     const { actionValidationErrorsById } = useValues(workflowLogic)
