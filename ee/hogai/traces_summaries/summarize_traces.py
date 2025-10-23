dfa3258915cec24ae9b7ca08e3117f842403f6bc
Alex Lebedev <alex.l@posthog.com>
2025-10-23 12:45:27 +0200

    chore: Renaming to restructure the tools.

---
+++ ee/hogai/traces_summaries/summarize_traces.py
@@ -0,0 +1,5 @@
+# TODO: 1. Get traces through query runner
+# TODO: 2. Stringify them in-memory
+# TODO: 3. Generate summaries (store somewhere, Postgres for now, later would be stored in Olly's table)
+# TODO: 4. Generate embeddings (should be included in the previous step)
+# TODO: 5. Clusterize embeddings, return groups/singles/and group centroids
