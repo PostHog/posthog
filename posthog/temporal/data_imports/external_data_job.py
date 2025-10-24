4168cf066e9cb6800754ee35778e7e28c6eb000f
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-10-24 08:48:08 -0500

    update user facing log

--- posthog/temporal/data_imports/external_data_job.py
+++ posthog/temporal/data_imports/external_data_job.py
@@ -229,7 +229,10 @@ def update_external_data_job_model(inputs: UpdateExternalDataJobStatusInputs) ->
             raw_error=inputs.latest_error or inputs.internal_error,
         )
 
-        logger.exception(latest_error)
+        if latest_error and "Unsupported DeltaLake type: timestamp_ntz" in latest_error.casefold():
+            logger.debug(latest_error)
+        else:
+            logger.exception(latest_error)
     except Exception:
         latest_error = inputs.latest_error
 
