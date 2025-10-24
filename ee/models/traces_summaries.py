7373ee3b1fc93a66850673c601d7695d7af7b411
Alex Lebedev <alex.l@posthog.com>
2025-10-24 15:52:44 +0200

    feat: Add base SQL table to store summaries (till better solution).

---
+++ ee/models/traces_summaries.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from posthog.models.team.team import Team
+from posthog.models.user import User
+from posthog.models.utils import UUIDTModel
+
+
+class TraceSummary(UUIDTModel):
+    class Meta:
+        indexes = [
+            models.Index(fields=["ref_trace_id"]),
+        ]
+
+    class TraceType(models.TextChoices):
+        """
+        Traces could be summarized with different types of prompts for different use cases.
+        """
+
+        ISSUES_SEARCH = "issues_search"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    trace_type = models.CharField(max_length=100, choices=TraceType.choices, default=TraceType.ISSUES_SEARCH)
+    trace_id = models.CharField(max_length=255, help_text="Trace ID")
+    summary = models.CharField(max_length=1000, help_text="Trace summary")
