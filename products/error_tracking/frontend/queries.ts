51453e7eda314a2b7c9bcf5e8e48eca4d46659bd
ablaszkiewicz <aleksander@posthog.com>
2025-10-24 15:18:14 +0200

    feat: use existing colors

--- products/error_tracking/frontend/queries.ts
+++ products/error_tracking/frontend/queries.ts
@@ -251,6 +251,7 @@ export const errorTrackingIssueBreakdownQuery = ({
                     kind: NodeKind.EventsNode,
                     event: '$exception',
                     math: BaseMathType.TotalCount,
+                    name: '.',
                     properties: [
                         {
                             key: '$exception_issue_id',
