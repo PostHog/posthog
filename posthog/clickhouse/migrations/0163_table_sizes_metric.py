74556ef46e33fd1bd4ace04e361e6f743e793c8e
Pawel Szczur <pawel@posthog.com>
2025-10-27 20:39:14 +0100

    make linter happy

--- posthog/clickhouse/migrations/0163_table_sizes_metric.py
+++ posthog/clickhouse/migrations/0163_table_sizes_metric.py
@@ -1,5 +1,5 @@
 from posthog.clickhouse.client.migration_tools import NodeRole, run_sql_with_exceptions
-from posthog.clickhouse.custom_metrics import CUSTOM_METRICS_TABLE_SIZES_VIEW, CUSTOM_METRICS_VIEW
+from posthog.clickhouse.custom_metrics import CUSTOM_METRICS_TABLE_SIZES_VIEW
 
 operations = [
     run_sql_with_exceptions(CUSTOM_METRICS_TABLE_SIZES_VIEW(), node_roles=[NodeRole.DATA, NodeRole.COORDINATOR]),
