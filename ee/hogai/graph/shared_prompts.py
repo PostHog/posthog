530f3d48eeb085f93929a3fa79efaf82d22a9035
Dena Korita <denada@posthog.com>
2025-10-27 03:08:26 +0100

    feat(max): allow fts through PostHog AI (#40013)

--- ee/hogai/graph/shared_prompts.py
+++ ee/hogai/graph/shared_prompts.py
@@ -4,3 +4,6 @@ You have access to the core memory about the user's company and product in the <
 {{{core_memory}}}
 </core_memory>
 """.strip()
+
+HYPERLINK_USAGE_INSTRUCTIONS = """
+\n\n<system_reminder>The results contain URLs to specific entities. When presenting them to the user, use hyperlinks and Markdown formatting.</system_reminder>"""
