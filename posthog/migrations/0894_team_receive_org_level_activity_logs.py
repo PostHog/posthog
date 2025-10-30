ec62e65d50a2ea8fe99ef6fd8ee962c46373f6be
Yasen Slavov <yasen@posthog.com>
2025-10-30 15:45:11 +0200

    Update max migrations

--- posthog/migrations/0894_team_receive_org_level_activity_logs.py
+++ posthog/migrations/0894_team_receive_org_level_activity_logs.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0891_file_system_view_log"),
+        ("posthog", "0893_add_ingestion_lane_fields_to_event_ingestion_restriction"),
     ]
 
     operations = [
