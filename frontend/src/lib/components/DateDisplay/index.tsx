4f5e40e8ed4fb8e0be292a29368e1ab71dad08a0
Frank Hamand <frankhamand@gmail.com>
2025-10-30 10:23:40 +0000

    re-add accidentally removed change

--- frontend/src/lib/components/DateDisplay/index.tsx
+++ frontend/src/lib/components/DateDisplay/index.tsx
@@ -27,6 +27,8 @@ const DISPLAY_DATE_FORMAT: Record<IntervalType, string> = {
 
 const dateHighlight = (parsedDate: dayjs.Dayjs, interval: IntervalType): string => {
     switch (interval) {
+        case 'second':
+            return parsedDate.format('MMM D')
         case 'minute':
             return parsedDate.format('MMM D')
         case 'hour':
