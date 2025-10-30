d2f00ad8d82644f8c5daf01307bb3f37f4ab32b4
Oliver Browne <oliver@posthog.com>
2025-10-30 16:52:36 +0200

    add part to test migrations

--- rust/cymbal/tests/test_migrations/20241101134611_test_migration_for_symbol_set_saving_tests.sql
+++ rust/cymbal/tests/test_migrations/20241101134611_test_migration_for_symbol_set_saving_tests.sql
@@ -23,6 +23,7 @@ CREATE TABLE IF NOT EXISTS posthog_errortrackingstackframe (
     contents JSONB NOT NULL,
     resolved BOOLEAN NOT NULL,
     context JSONB,
+    part INTEGER,
     UNIQUE(raw_id, team_id)
 );
 
