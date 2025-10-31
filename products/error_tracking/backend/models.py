c84abd59e2864b873f4ef4a4e52aef61e8e093bd
Hugues Pouillot <hpouillot@gmail.com>
2025-10-31 14:20:50 +0100

    clean migration
    Signed-off-by: Hugues Pouillot <hpouillot@gmail.com>

--- products/error_tracking/backend/models.py
+++ products/error_tracking/backend/models.py
@@ -315,12 +315,9 @@ class ErrorTrackingStackFrame(UUIDTModel):
     context = models.JSONField(null=True, blank=True)
 
     class Meta:
-        indexes = [
-            models.Index(fields=["team_id", "raw_id"]),
-        ]
+        indexes = []
 
         constraints = [
-            models.UniqueConstraint(fields=["team_id", "raw_id"], name="unique_raw_id_per_team"),
             models.UniqueConstraint(fields=["team_id", "raw_id", "part"], name="unique_team_id_raw_id_part"),
         ]
 
