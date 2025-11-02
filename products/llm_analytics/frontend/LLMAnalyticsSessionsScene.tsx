55f64d8e9514ce7965368966e1074a021e511812
Andrew Maguire <andrewm4894@gmail.com>
2025-11-02 22:48:28 +0000

    fix(llma): Don't display 0 when trace has no errors

--- products/llm_analytics/frontend/LLMAnalyticsSessionsScene.tsx
+++ products/llm_analytics/frontend/LLMAnalyticsSessionsScene.tsx
@@ -299,7 +299,7 @@ export function LLMAnalyticsSessionsScene(): JSX.Element {
                                                             {trace.traceName && (
                                                                 <span className="text-sm">{trace.traceName}</span>
                                                             )}
-                                                            {trace.errorCount && trace.errorCount > 0 && (
+                                                            {trace.errorCount > 0 && (
                                                                 <LemonTag type="danger" size="small">
                                                                     {trace.errorCount === 1
                                                                         ? '1 error'
