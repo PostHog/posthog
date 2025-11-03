d051db710b9b6959ae7a6c4ea2bf35e53105cb2d
Arthur Moreira de Deus <arthur@posthog.com>
2025-11-03 10:49:11 -0300

    refactor: Allow adding the same insight to a notebook multiple times

--- frontend/src/scenes/saved-insights/components/NotebookActionButton.tsx
+++ frontend/src/scenes/saved-insights/components/NotebookActionButton.tsx
@@ -1,4 +1,4 @@
-import { useActions, useValues } from 'kea'
+import { useActions } from 'kea'
 
 import { IconPlusSmall } from '@posthog/icons'
 
@@ -9,20 +9,15 @@ import { QueryBasedInsightModel } from '~/types'
 
 export function NotebookActionButton({ insight }: { insight: QueryBasedInsightModel }): JSX.Element {
     const { addSavedInsightToNotebook } = useActions(notebookLogic)
-    const { insightShortIdsInNotebook } = useValues(notebookLogic)
-    const isInNotebook = !!insightShortIdsInNotebook?.includes(insight.short_id)
 
     return (
         <LemonButton
             type="secondary"
             size="small"
             fullWidth
-            disabledReason={isInNotebook ? 'Insight already in notebook' : ''}
             onClick={(e) => {
                 e.preventDefault()
-                if (!isInNotebook) {
-                    addSavedInsightToNotebook(insight.short_id)
-                }
+                addSavedInsightToNotebook(insight.short_id)
             }}
             icon={<IconPlusSmall />}
         />
