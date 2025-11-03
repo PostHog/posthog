b9b2012495ba605c5f3d68f26671534f7c4b98c6
Haven <haven@posthog.com>
2025-11-03 13:13:10 -0600

    tweak

--- posthog/migrations/0898_hogflow_variables.py
+++ posthog/migrations/0898_hogflow_variables.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0893_add_ingestion_lane_fields_to_event_ingestion_restriction"),
+        ("posthog", "0897_migrate_data_warehouse_models"),
     ]
 
     operations = [
