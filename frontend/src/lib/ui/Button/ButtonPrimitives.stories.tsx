f899d83949922847a40c4b3d53e341734d15ebb0
Julian Bez <julian@posthog.com>
2025-11-05 06:41:20 +0100

    Revert "test: trigger snapshot update and deletion with git patch approach"
    This reverts commit 1ca373aec101ef2b5b35875c566dc7c34ac0a14c.

--- frontend/src/lib/ui/Button/ButtonPrimitives.stories.tsx
+++ frontend/src/lib/ui/Button/ButtonPrimitives.stories.tsx
@@ -28,7 +28,7 @@ export function Default(): JSX.Element {
     return (
         <div className="flex flex-col gap-4 max-w-lg">
             <ButtonPrimitive variant="outline" size="base">
-                Test Git Patches
+                Default
             </ButtonPrimitive>
 
             <ButtonPrimitive variant="outline" size="base">
