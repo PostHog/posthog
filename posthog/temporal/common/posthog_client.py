60b8ef41803a4355c4395f69e5e5a865cb5d17be
Ross <rossagray@gmail.com>
2025-11-05 15:11:40 +0000

    Apply suggestion from @tomasfarias
    Co-authored-by: Tomás Farías Santana <tomas@tomasfarias.dev>

--- posthog/temporal/common/posthog_client.py
+++ posthog/temporal/common/posthog_client.py
@@ -65,7 +65,11 @@ class _PostHogClientWorkflowInterceptor(WorkflowInboundInterceptor):
         try:
             return await super().execute_workflow(input)
         except Exception as e:
-            # we don't want to capture cancelled errors
+            # Temporal raises a temporalio.exceptions.CancelledError within
+            # an activity as part of the worfklow cancellation procedure usually
+            # started by calling handle.cancel(). This is part of the
+            # normal lifecycle of a workflow and thus not an exception we want
+            # to track as an error.
             if isinstance(e, temporalio.exceptions.ActivityError) and isinstance(
                 e.cause, temporalio.exceptions.CancelledError
             ):
