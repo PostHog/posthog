3abf6e28d6376423d7fb7c3c1d84e0892befa0d7
Alex V <alexander.veryaysky@gmail.com>
2025-11-05 11:00:21 +0100

    fix

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -4474,8 +4474,8 @@ export interface ReplayExportContext {
 
 export interface HeatmapExportContext {
     heatmap_url: string
-    heatmap_data_url: string
-    heatmap_type: HeatmapType
+    heatmap_data_url?: string
+    heatmap_type?: HeatmapType
     filename?: string
     heatmap_filters?: HeatmapFilters
     heatmap_color_palette?: string | null
