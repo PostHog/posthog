35b0375c00ee98e558ce34e8c81bac34986fd832
Robbie Coomber <robbie.coomber@gmail.com>
2025-11-07 15:07:45 +0000

    Re-add sessions v3 recordings mv

---
+++ posthog/clickhouse/migrations/0172_sessions_v3_recordings_mvs.py
@@ -0,0 +1,7 @@
+from posthog.clickhouse.client.connection import NodeRole
+from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
+from posthog.models.raw_sessions.sessions_v3 import RAW_SESSIONS_TABLE_MV_RECORDINGS_SQL_V3
+
+operations = [
+    run_sql_with_exceptions(RAW_SESSIONS_TABLE_MV_RECORDINGS_SQL_V3(), node_roles=[NodeRole.DATA], sharded=True),
+]
