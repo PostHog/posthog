4b99602cde7a5fa97cd7b077c48eed9de0cfc2ff
Robbie Coomber <robbie.coomber@gmail.com>
2025-11-07 10:57:02 +0000

    Remove sharded=True

--- posthog/clickhouse/migrations/0169_drop_sessions_v3_recordings_mv.py
+++ posthog/clickhouse/migrations/0169_drop_sessions_v3_recordings_mv.py
@@ -3,7 +3,5 @@ from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.models.raw_sessions.sessions_v3 import DROP_RAW_SESSION_MATERIALIZED_VIEW_RECORDINGS_SQL_V3
 
 operations = [
-    run_sql_with_exceptions(
-        DROP_RAW_SESSION_MATERIALIZED_VIEW_RECORDINGS_SQL_V3(), node_roles=[NodeRole.DATA], sharded=True
-    ),
+    run_sql_with_exceptions(DROP_RAW_SESSION_MATERIALIZED_VIEW_RECORDINGS_SQL_V3(), node_roles=[NodeRole.DATA]),
 ]
