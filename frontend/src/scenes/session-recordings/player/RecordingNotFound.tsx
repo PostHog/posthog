65feefcc8a8da5fb2b79e2eb99d46983e5883888
Alex V <alexander.veryaysky@gmail.com>
2025-11-07 18:39:14 +0100

    fix(replay): revert 404 and add banner

--- frontend/src/scenes/session-recordings/player/RecordingNotFound.tsx
+++ frontend/src/scenes/session-recordings/player/RecordingNotFound.tsx
@@ -1,31 +1,56 @@
-import { router } from 'kea-router'
+import { useValues } from 'kea'
 
 import { NotFound } from 'lib/components/NotFound'
+import { LemonBanner } from 'lib/lemon-ui/LemonBanner'
+import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { Link } from 'lib/lemon-ui/Link'
-import { SETTINGS_LOGIC_KEY } from 'scenes/session-recordings/settings/SessionRecordingsSettingsScene'
-import { Settings } from 'scenes/settings/Settings'
+import { teamLogic } from 'scenes/teamLogic'
+import { urls } from 'scenes/urls'
 
 export function RecordingNotFound(): JSX.Element {
+    const { currentTeam } = useValues(teamLogic)
+
     return (
         <NotFound
             object="Recording"
             caption={
                 <>
                     The requested recording doesn't seem to exist. The recording may still be processing, deleted due to
-                    age or have not been enabled. Please check your project replay settings below. Alternatively read
-                    the{' '}
+                    age or have not been enabled. Please check your{' '}
+                    <Link to={urls.settings('project-replay')}>project settings</Link> that recordings is turned on and
+                    enabled for the domain in question. Alternatively read the{' '}
                     <Link to="https://posthog.com/docs/session-replay/troubleshooting#recording-not-found">
                         troubleshooting guide
                     </Link>
-                    <h3 className="mb-2 mt-8">Project replay settings</h3>
-                    <div className="mt-4 border rounded-md p-4 text-left">
-                        <Settings
-                            logicKey={SETTINGS_LOGIC_KEY}
-                            sectionId="environment-replay"
-                            settingId={router.values.searchParams.sectionId || 'replay'}
-                            handleLocally
-                        />
-                    </div>
+                    {currentTeam?.session_recording_opt_in ? (
+                        <LemonBanner type="info" className="mt-4 max-w-xl mx-auto">
+                            <div className="justify-between flex items-center">
+                                <p>Session replay is enabled for this project</p>
+                                <LemonButton
+                                    data-attr="recording-404-edit-settings"
+                                    type="secondary"
+                                    size="small"
+                                    to={urls.settings('project-replay')}
+                                >
+                                    Edit settings
+                                </LemonButton>
+                            </div>
+                        </LemonBanner>
+                    ) : (
+                        <LemonBanner type="warning" className="mt-4 max-w-xl mx-auto">
+                            <div className="justify-between flex items-center">
+                                <p>Session replay is disabled for this project</p>
+                                <LemonButton
+                                    data-attr="recording-404-edit-settings"
+                                    type="secondary"
+                                    size="small"
+                                    to={urls.settings('project-replay')}
+                                >
+                                    Edit settings
+                                </LemonButton>
+                            </div>
+                        </LemonBanner>
+                    )}
                 </>
             }
         />
