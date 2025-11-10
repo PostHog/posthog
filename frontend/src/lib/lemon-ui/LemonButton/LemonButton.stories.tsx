f665a0f1b3b52344ed9d9ba622b287e1678c1887
Julian Bez <julian@posthog.com>
2025-11-10 21:10:26 +0100

    test: revert button text back to original

--- frontend/src/lib/lemon-ui/LemonButton/LemonButton.stories.tsx
+++ frontend/src/lib/lemon-ui/LemonButton/LemonButton.stories.tsx
@@ -39,7 +39,7 @@ const BasicTemplate: StoryFn<typeof LemonButton> = (props: LemonButtonProps) => 
 export const Default: Story = BasicTemplate.bind({})
 Default.args = {
     icon: <IconCalculate />,
-    children: 'Click here',
+    children: 'Click me',
 }
 
 const StatusesTemplate = ({
