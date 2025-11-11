345b8ff2f580c7eed7d54fedfe2a6b9291bbb53f
dylan <dylan@posthog.com>
2025-11-11 12:37:36 -0500

    clean up unused methods

--- posthog/tasks/scheduled.py
+++ posthog/tasks/scheduled.py
@@ -57,7 +57,7 @@ from posthog.tasks.tasks import (
     verify_persons_data_in_sync,
 )
 from posthog.tasks.team_access_cache_tasks import warm_all_team_access_caches_task
-from posthog.tasks.team_metadata import sync_team_metadata_cache_intelligent
+from posthog.tasks.team_metadata import refresh_stale_team_metadata_cache
 from posthog.utils import get_crontab, get_instance_region
 
 TWENTY_FOUR_HOURS = 24 * 60 * 60
@@ -117,7 +117,7 @@ def setup_periodic_tasks(sender: Celery, **kwargs: Any) -> None:
     # Team metadata cache sync - hourly (intelligent refresh)
     sender.add_periodic_task(
         crontab(hour="*", minute="0"),
-        sync_team_metadata_cache_intelligent.s(),
+        refresh_stale_team_metadata_cache.s(),
         name="intelligent team metadata cache sync",
     )
 
