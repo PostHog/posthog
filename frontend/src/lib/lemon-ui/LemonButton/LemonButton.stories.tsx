f0a170df74c0e962666b2177bd57d91eac7cf08c
Julian Bez <julian@posthog.com>
2025-11-12 08:51:25 +0100

    test: change button text to trigger snapshot updates

--- frontend/src/lib/lemon-ui/LemonButton/LemonButton.stories.tsx
+++ frontend/src/lib/lemon-ui/LemonButton/LemonButton.stories.tsx
@@ -39,7 +39,7 @@ const BasicTemplate: StoryFn<typeof LemonButton> = (props: LemonButtonProps) => 
 export const Default: Story = BasicTemplate.bind({})
 Default.args = {
     icon: <IconCalculate />,
-    children: 'Click me',
+    children: 'Click here',
 }
 
 const StatusesTemplate = ({
