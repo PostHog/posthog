1ee0514809f7cdbd75cc1bd0b31a2893c6ba6282
Haven <haven@posthog.com>
2025-11-12 16:08:50 -0600

    tweak

--- frontend/src/lib/monaco/languages/liquid.ts
+++ frontend/src/lib/monaco/languages/liquid.ts
@@ -10,12 +10,20 @@ import { MonacoDisposable } from '../CodeEditor'
 export function initLiquidLanguage(monaco: Monaco): MonacoDisposable[] {
     const disposables: MonacoDisposable[] = []
 
-    // Liquid is a pre-registered language in Monaco, so we expand its configuration here
-    // instead of registering anything like we do for our custom languages.
-    disposables.push(
-        monaco.languages.registerCompletionItemProvider('liquid', hogQLAutocompleteProvider(HogLanguage.liquid))
-    )
-    disposables.push(monaco.languages.registerCodeActionProvider('liquid', hogQLMetadataProvider()))
+    /**
+     * Hack: Multiple Monaco instances can render at the same time, so we need a way to determine
+     * if the completion provider has already been registered. Monaco does not expose the registered providers for
+     * languages, so we'll do so with a noop hogLiquid language registration as a flag like we do with other languages
+     * in this directory
+     */
+    if (!monaco.languages.getLanguages().some((lang) => lang.id === 'hogLiquid')) {
+        monaco.languages.register({ id: 'hogLiquid' })
+        // Liquid is a pre-registered language in Monaco, so we only need to register completion and code action providers.
+        disposables.push(
+            monaco.languages.registerCompletionItemProvider('liquid', hogQLAutocompleteProvider(HogLanguage.liquid))
+        )
+        disposables.push(monaco.languages.registerCodeActionProvider('liquid', hogQLMetadataProvider()))
+    }
 
     return disposables
 }
