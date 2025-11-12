51923da21b9dfb62bfae4d96f3c693f67dd16675
Haven <haven@posthog.com>
2025-11-12 15:45:06 -0600

    tweak

--- frontend/src/lib/monaco/languages/liquid.ts
+++ frontend/src/lib/monaco/languages/liquid.ts
@@ -12,13 +12,10 @@ export function initLiquidLanguage(monaco: Monaco): MonacoDisposable[] {
 
     // Liquid is a pre-registered language in Monaco, so we expand its configuration here
     // instead of registering anything like we do for our custom languages.
-    const languageConfiguration = monaco.languages.getLanguages().find((lang) => lang.id === 'liquid')
-    if (languageConfiguration && !languageConfiguration.extensions) {
-        disposables.push(
-            monaco.languages.registerCompletionItemProvider('liquid', hogQLAutocompleteProvider(HogLanguage.liquid))
-        )
-        disposables.push(monaco.languages.registerCodeActionProvider('liquid', hogQLMetadataProvider()))
-    }
+    disposables.push(
+        monaco.languages.registerCompletionItemProvider('liquid', hogQLAutocompleteProvider(HogLanguage.liquid))
+    )
+    disposables.push(monaco.languages.registerCodeActionProvider('liquid', hogQLMetadataProvider()))
 
     return disposables
 }
