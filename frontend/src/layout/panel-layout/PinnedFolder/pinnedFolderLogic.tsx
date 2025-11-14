879c775fd6cf93ea87ef8bda5075f559a91ca20a
Adam Leith <adamleithp@users.noreply.github.com>
2025-11-14 10:32:33 +0000

    fix: refactror pinned folder logic to actually work

--- frontend/src/layout/panel-layout/PinnedFolder/pinnedFolderLogic.tsx
+++ frontend/src/layout/panel-layout/PinnedFolder/pinnedFolderLogic.tsx
@@ -1,8 +1,6 @@
 import { actions, afterMount, kea, listeners, path, reducers } from 'kea'
-import { lazyLoaders } from 'kea-loaders'
 
 import api from 'lib/api'
-import { getCurrentTeamIdOrNone, getCurrentUserIdOrNone } from 'lib/utils/getAppContext'
 
 import { splitProtocolPath } from '~/layout/panel-layout/ProjectTree/utils'
 
@@ -12,47 +10,29 @@ export const pinnedFolderLogic = kea<pinnedFolderLogicType>([
     path(['layout', 'panel-layout', 'PinnedFolder', 'pinnedFolderLogic']),
     actions({
         setPinnedFolder: (id: string) => ({ id }),
-        setSelectedFolder: (id: string) => ({ id }),
+        loadPinnedFolder: true,
     }),
-    lazyLoaders(() => ({
+    reducers(() => ({
         pinnedFolder: [
             'loading://',
-            {
-                loadPinnedFolder: async () => {
-                    const folders = await api.persistedFolder.list()
-                    const pinned = folders.results.find((folder) => folder.type === 'pinned')
-                    if (pinned) {
-                        return `${pinned.protocol || 'products://'}${pinned.path}`
-                    }
-                    return 'products://'
-                },
-            },
-        ],
-    })),
-    reducers(() => ({
-        pinnedFolderSource: [
-            'loading://',
-            { persist: true, prefix: `${getCurrentTeamIdOrNone()}__${getCurrentUserIdOrNone()}__` },
             {
                 setPinnedFolder: (_, { id }) => id,
             },
         ],
-        selectedFolder: [
-            'products://',
-            {
-                setSelectedFolder: (_, { id }) => id,
-            },
-        ],
     })),
-    listeners(() => ({
+    listeners(({ actions }) => ({
         setPinnedFolder: async ({ id }) => {
             const [protocol, path] = splitProtocolPath(id)
             await api.persistedFolder.create({ protocol, path, type: 'pinned' })
         },
+        loadPinnedFolder: async () => {
+            const folders = await api.persistedFolder.list()
+            const pinned = folders.results.find((folder) => folder.type === 'pinned')
+            const folderId = pinned ? `${pinned.protocol || 'products://'}${pinned.path}` : 'products://'
+            actions.setPinnedFolder(folderId)
+        },
     })),
-    afterMount(({ actions, values }) => {
-        if (values.selectedFolder !== values.pinnedFolder) {
-            actions.setSelectedFolder(values.pinnedFolder)
-        }
+    afterMount(({ actions }) => {
+        actions.loadPinnedFolder()
     }),
 ])
