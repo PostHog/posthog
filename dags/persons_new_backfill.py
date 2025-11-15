4422619943c7bd519d21275877e26a3aa1fb43e8
Eli Reisman <eli.r@posthog.com>
2025-11-15 11:36:33 -0800

    chore(dagster): swap to Dagster k8s job executor w/48 pods fanout (#41587)
    Co-authored-by: José Sequeira <jose@posthog.com>

--- dags/persons_new_backfill.py
+++ dags/persons_new_backfill.py
@@ -6,6 +6,7 @@ from typing import Any
 
 import dagster
 import psycopg2
+import dagster_k8s
 import psycopg2.errors
 
 from posthog.clickhouse.cluster import ClickhouseCluster
@@ -385,7 +386,7 @@ def postgres_env_check(context: dagster.AssetExecutionContext) -> None:
 
 @dagster.job(
     tags={"owner": JobOwners.TEAM_INGESTION.value},
-    executor_def=dagster.multiprocess_executor.configured({"max_concurrent": 32}),
+    executor_def=dagster_k8s.k8s_job_executor.configured({"max_concurrent": 48}),
 )
 def persons_new_backfill_job():
     """
