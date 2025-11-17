4a89577f09c2f6356ad632305013a019e5e9c4c4
Haven <haven@posthog.com>
2025-11-17 17:41:30 +0100

    feat(workflows): set up frontend for realtime destination backfills

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5101,6 +5101,10 @@ export type BatchExportServiceDatabricks = {
     }
 }
 
+export type BatchExportRealtimeDestinationBackfill = {
+    type: 'PostHogRealtimeDestinations'
+}
+
 // When adding a new option here also add a icon for it to
 // frontend/public/services/
 // and update RenderBatchExportIcon
@@ -5112,6 +5116,7 @@ export const BATCH_EXPORT_SERVICE_NAMES: BatchExportService['type'][] = [
     'Redshift',
     'HTTP',
     'Databricks',
+    'PostHogRealtimeDestinations',
 ]
 export type BatchExportService =
     | BatchExportServiceS3
@@ -5121,8 +5126,9 @@ export type BatchExportService =
     | BatchExportServiceRedshift
     | BatchExportServiceHTTP
     | BatchExportServiceDatabricks
+    | BatchExportRealtimeDestinationBackfill
 
-export type PipelineInterval = 'hour' | 'day' | 'every 5 minutes'
+export type PipelineInterval = 'hour' | 'day' | 'every 5 minutes' | 'once'
 
 export type DataWarehouseSyncInterval = '5min' | '30min' | '1hour' | '6hour' | '12hour' | '24hour' | '7day' | '30day'
 export type OrNever = 'never'
