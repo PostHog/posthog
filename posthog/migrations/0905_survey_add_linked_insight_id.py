a343d2fb8c2c7a5a33484ce1972ab2b7017b76bf
Adam Bowker <adam.b@posthog.com>
2025-11-18 12:14:17 -0800

    fix(surveys): correct field name to linked_insight_id

--- posthog/migrations/0905_survey_add_linked_insight_id.py
+++ posthog/migrations/0905_survey_add_linked_insight_id.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name="survey",
-            name="linked_insight",
+            name="linked_insight_id",
             field=models.IntegerField(blank=True, null=True),
         ),
     ]
