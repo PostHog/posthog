6e0ef746976f20ee6b5ea18c7b82ca7efb2a08b8
Alex Lebedev <alex.l@posthog.com>
2025-11-18 21:46:13 +0100

    fix: Typo.

--- products/session_summaries/manifest.tsx
+++ products/session_summaries/manifest.tsx
@@ -8,7 +8,7 @@ export const manifest: ProductManifest = {
             import: () => import('./frontend/SessionGroupSummariesTable'),
             projectBased: true,
             description:
-                'View and deep-dive into AI-generated summaries of session recordings. Create summaries from the Session replay page by applying filters and asking PostHog AI to summarize sessions. If not filters, ask PostHog AI to find and summarize sessions based on provided criteria.',
+                'View and deep-dive into AI-generated summaries of session recordings. Create summaries from the Session replay page by applying filters and asking PostHog AI to summarize sessions. If no filters, ask PostHog AI to find and summarize sessions based on provided criteria.',
             iconType: 'insight/hog',
         },
         SessionGroupSummary: {
