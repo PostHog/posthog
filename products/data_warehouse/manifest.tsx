58660610460fda9978e4d91ff1e1d1df76e3c71c
Rafa Audibert <rafael@posthog.com>
2025-11-18 20:31:58 -0300

    refactor: Move all of the custom tree items to manifests
    These shouldn't have been put here in the first place, let's just put them in appropriate manifest files

--- products/data_warehouse/manifest.tsx
+++ products/data_warehouse/manifest.tsx
@@ -1,8 +1,7 @@
 import { FEATURE_FLAGS } from 'lib/constants'
 import { urls } from 'scenes/urls'
 
-import { FileSystemIconType } from '~/queries/schema/schema-general'
-import { FileSystemIconColor, ProductManifest } from '~/types'
+import { ProductManifest } from '~/types'
 
 export const manifest: ProductManifest = {
     name: 'Data warehouse',
@@ -26,13 +25,41 @@ export const manifest: ProductManifest = {
     },
     treeItemsProducts: [
         {
+            path: 'SQL editor',
+            category: 'Analytics',
+            type: 'sql',
+            iconType: 'sql_editor',
+            iconColor: ['var(--color-product-data-warehouse-light)'],
+            href: urls.sqlEditor(),
+            sceneKey: 'SQLEditor',
+            sceneKeys: ['SQLEditor'],
+        },
+        {
             path: 'Data warehouse',
             category: 'Unreleased',
             href: urls.dataWarehouse(),
             flag: FEATURE_FLAGS.DATA_WAREHOUSE_SCENE,
-            iconType: 'data_warehouse' as FileSystemIconType,
-            iconColor: ['var(--color-product-data-warehouse-light)'] as FileSystemIconColor,
+            iconType: 'data_warehouse',
+            iconColor: ['var(--color-product-data-warehouse-light)'],
             sceneKey: 'DataWarehouse',
+        },
+    ],
+    treeItemsMetadata: [
+        {
+            path: `Sources`,
+            category: 'Pipeline',
+            type: 'hog_function/source',
+            iconType: 'data_pipeline_metadata',
+            href: urls.dataPipelines('sources'),
+            sceneKey: 'DataPipelines',
+            sceneKeys: ['DataPipelines'],
+        },
+        {
+            path: 'Managed viewsets',
+            category: 'Unreleased',
+            iconType: 'managed_viewsets',
+            href: urls.dataWarehouseManagedViewsets(),
+            flag: FEATURE_FLAGS.MANAGED_VIEWSETS,
         },
     ],
 }
