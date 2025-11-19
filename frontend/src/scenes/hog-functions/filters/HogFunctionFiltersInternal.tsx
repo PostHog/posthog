3657129b294ef289cb48c142c0ed41b0c9d94c7f
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-11-19 18:44:57 +0100

    fix

--- frontend/src/scenes/hog-functions/filters/HogFunctionFiltersInternal.tsx
+++ frontend/src/scenes/hog-functions/filters/HogFunctionFiltersInternal.tsx
@@ -93,7 +93,7 @@ const setSimpleFilterValue = (options: FilterOption[], value: string): Cyclotron
 }
 
 export function HogFunctionFiltersInternal(): JSX.Element {
-    const { contextId, hogFunction } = useValues(hogFunctionConfigurationLogic)
+    const { contextId } = useValues(hogFunctionConfigurationLogic)
 
     const options = useMemo(() => getProductEventFilterOptions(contextId), [contextId])
 
@@ -108,8 +108,6 @@ export function HogFunctionFiltersInternal(): JSX.Element {
             return [TaxonomicFilterGroupType.Events]
         } else if (contextId === 'activity-log') {
             return [TaxonomicFilterGroupType.ActivityLogProperties]
-        } else if (hogFunction?.filters?.source === 'data-warehouse') {
-            return [TaxonomicFilterGroupType.DataWarehouseProperties]
         }
         return []
     }, [contextId])
