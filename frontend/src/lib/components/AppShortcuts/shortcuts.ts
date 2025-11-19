cd499d31c88dd6d6077e22c0cac26e6e6d43aee2
Adam Leith <adamleithp@users.noreply.github.com>
2025-11-19 22:22:56 +0000

    fix(ux): changed shortcut menu trigger to be cmd option k (#41766)

--- frontend/src/lib/components/AppShortcuts/shortcuts.ts
+++ frontend/src/lib/components/AppShortcuts/shortcuts.ts
@@ -3,6 +3,7 @@ export const baseModifier = ['command', 'option']
 export const keyBinds: Record<string, string[]> = {
     newTab: [...baseModifier, 't'],
     closeActiveTab: [...baseModifier, 'w'],
+    toggleShortcutMenu: [...baseModifier, 'k'],
     search: ['command', 'k'],
     new: [...baseModifier, 'n'],
     edit: [...baseModifier, 'e'],
