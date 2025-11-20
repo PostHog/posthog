648a0a227feb097be75df8a6713c796fd0474f71
José Sequeira <jose@posthog.com>
2025-11-20 17:38:37 +0100

    fix rust migrations

--- rust/persons_migrations/20251117000001_rename_person_tables.sql
+++ rust/persons_migrations/20251117000001_rename_person_tables.sql
@@ -6,3 +6,6 @@ ALTER TABLE IF EXISTS posthog_person RENAME TO posthog_person_old;
 
 -- Rename new partitioned table to posthog_person
 ALTER TABLE IF EXISTS posthog_person_new RENAME TO posthog_person;
+
+-- Create view for backwards compatibility with Rust code
+CREATE OR REPLACE VIEW posthog_person_new AS SELECT * FROM posthog_person;
