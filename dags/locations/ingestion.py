64ef7b8ddc4263b701cf4c32c3c9cbc820631adf
Eli Reisman <eli.r@posthog.com>
2025-11-20 09:42:14 -0800

    add ingestion job registration + sketchy test suite; param persons table name in job cfg

--- dags/locations/ingestion.py
+++ dags/locations/ingestion.py
@@ -1,15 +1,17 @@
 import dagster
 
-from dags import persons_new_backfill
+from dags import delete_persons_from_trigger_log_job, persons_new_backfill
 
 from . import resources
 
 defs = dagster.Definitions(
     assets=[
         persons_new_backfill.postgres_env_check,
+        delete_persons_from_trigger_log_job.postgres_env_check,
     ],
     jobs=[
         persons_new_backfill.persons_new_backfill_job,
+        delete_persons_from_trigger_log_job.delete_persons_from_trigger_log_job,
     ],
     resources=resources,
 )
