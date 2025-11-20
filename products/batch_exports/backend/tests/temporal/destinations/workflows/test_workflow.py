2bc594f2ec4a107d65cd613c357f9699ce425ce9
Ross Gray <ross@posthog.com>
2025-11-20 10:58:16 +0100

    add test for final backfill run

--- products/batch_exports/backend/tests/temporal/destinations/workflows/test_workflow.py
+++ products/batch_exports/backend/tests/temporal/destinations/workflows/test_workflow.py
@@ -9,7 +9,8 @@ from temporalio.common import RetryPolicy
 from temporalio.testing import WorkflowEnvironment
 from temporalio.worker import UnsandboxedWorkflowRunner, Worker
 
-from posthog.batch_exports.service import BatchExportModel
+from posthog.batch_exports.models import BatchExportBackfill
+from posthog.batch_exports.service import BackfillDetails, BatchExportModel
 from posthog.temporal.tests.utils.models import acreate_batch_export, adelete_batch_export, afetch_batch_export_runs
 
 from products.batch_exports.backend.temporal.batch_exports import finish_batch_export_run, start_batch_export_run
@@ -68,10 +69,10 @@ async def test_workflows_export_workflow(
     topic,
     security_protocol,
 ):
-    """Test Redshift Export Workflow end-to-end.
+    """Test Workflows Export Workflow end-to-end.
 
     The workflow should update the batch export run status to completed and produce the expected
-    records to the provided Redshift instance.
+    records to the Kafka topic.
     """
     model = BatchExportModel(name="events", schema=None)
 
@@ -125,4 +126,95 @@ async def test_workflows_export_workflow(
         batch_export_model=model,
         exclude_events=exclude_events,
         sort_key="_inserted_at",
+    )
+
+
+async def test_workflows_export_workflow_when_last_run_of_backfill(
+    clickhouse_client,
+    workflows_batch_export,
+    ateam,
+    interval,
+    exclude_events,
+    generate_test_data,
+    data_interval_start,
+    data_interval_end,
+    hosts,
+    topic,
+    security_protocol,
+):
+    """Test Workflows Export Workflow end-to-end when the last run of the backfill is being processed.
+
+    In this case, we should also send a specific message to the Kafka topic to indicate that the backfill is complete.
+    """
+    model = BatchExportModel(name="events", schema=None)
+
+    # create backfill model
+    backfill = await BatchExportBackfill.objects.acreate(
+        team=ateam,
+        batch_export=workflows_batch_export,
+        start_at=data_interval_start,
+        end_at=data_interval_end,
+        status=BatchExportBackfill.Status.RUNNING,
+    )
+
+    # simulate last run of backfill by providing backfill details
+    backfill_details = BackfillDetails(
+        backfill_id=str(backfill.id),
+        start_at=data_interval_start.isoformat(),
+        end_at=data_interval_end.isoformat(),
+        is_last_backfill_run=True,
+    )
+
+    workflow_id = str(uuid.uuid4())
+    inputs = WorkflowsBatchExportInputs(
+        team_id=ateam.pk,
+        batch_export_id=str(workflows_batch_export.id),
+        data_interval_end=data_interval_end.isoformat(),
+        interval=interval,
+        batch_export_model=model,
+        backfill_details=backfill_details,
+        **workflows_batch_export.destination.config,
+    )
+    async with await WorkflowEnvironment.start_time_skipping() as activity_environment:
+        async with Worker(
+            activity_environment.client,
+            task_queue=settings.BATCH_EXPORTS_TASK_QUEUE,
+            workflows=[WorkflowsBatchExportWorkflow],
+            activities=[
+                start_batch_export_run,
+                insert_into_internal_stage_activity,
+                insert_into_kafka_activity_from_stage,
+                finish_batch_export_run,
+            ],
+            workflow_runner=UnsandboxedWorkflowRunner(),
+        ):
+            await activity_environment.client.execute_workflow(
+                WorkflowsBatchExportWorkflow.run,
+                inputs,
+                id=workflow_id,
+                task_queue=settings.BATCH_EXPORTS_TASK_QUEUE,
+                retry_policy=RetryPolicy(maximum_attempts=1),
+                execution_timeout=dt.timedelta(seconds=20),
+            )
+
+    runs = await afetch_batch_export_runs(batch_export_id=workflows_batch_export.id)
+    assert len(runs) == 1
+
+    events_to_export_created, _ = generate_test_data
+
+    run = runs[0]
+    assert run.status == "Completed"
+    assert run.records_completed == len(events_to_export_created)
+
+    await assert_clickhouse_records_in_kafka(
+        clickhouse_client=clickhouse_client,
+        topic=topic,
+        hosts=hosts,
+        date_ranges=[(data_interval_start, data_interval_end)],
+        security_protocol=security_protocol,
+        team_id=ateam.pk,
+        batch_export_model=model,
+        exclude_events=exclude_events,
+        sort_key="_inserted_at",
+        backfill_details=backfill_details,
     )
