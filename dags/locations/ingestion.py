2e2595e943ef96931dde84bbba0679b00d7a436b
Eli Reisman <eli.r@posthog.com>
2025-11-20 15:07:19 -0800

    linter

--- dags/locations/ingestion.py
+++ dags/locations/ingestion.py
@@ -9,8 +9,8 @@ defs = dagster.Definitions(
         ingestion_assets.postgres_env_check,
     ],
     jobs=[
-        persons_new_backfill.persons_new_backfill_job,
         delete_persons_from_trigger_log.delete_persons_from_trigger_log_job,
+        persons_new_backfill.persons_new_backfill_job,
     ],
     resources=resources,
 )
