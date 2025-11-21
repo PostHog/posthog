5ec81736be402d426ae4e85e8bcbe3eadcc1fc8f
Thomas Obermüller <thomas.obermueller@gmail.com>
2025-11-21 11:05:47 +0100

    refactor(funnels): remove non-UDF funnel actors (#41742)

--- posthog/hogql_queries/insights/funnels/utils.py
+++ posthog/hogql_queries/insights/funnels/utils.py
@@ -35,28 +35,13 @@ def get_funnel_order_class(funnelsFilter: FunnelsFilter, use_udf=False):
     return Funnel
 
 
-def get_funnel_actor_class(funnelsFilter: FunnelsFilter, use_udf=False):
-    from posthog.hogql_queries.insights.funnels import (
-        FunnelActors,
-        FunnelStrictActors,
-        FunnelTrendsUDF,
-        FunnelUDF,
-        FunnelUnorderedActors,
-    )
+def get_funnel_actor_class(funnelsFilter: FunnelsFilter):
+    from posthog.hogql_queries.insights.funnels import FunnelTrendsUDF, FunnelUDF
 
     if funnelsFilter.funnelVizType == FunnelVizType.TRENDS:
         return FunnelTrendsUDF
 
-    if use_udf:
-        return FunnelUDF
-
-    if funnelsFilter.funnelOrderType == StepOrderValue.UNORDERED:
-        return FunnelUnorderedActors
-
-    if funnelsFilter.funnelOrderType == StepOrderValue.STRICT:
-        return FunnelStrictActors
-
-    return FunnelActors
+    return FunnelUDF
 
 
 def funnel_window_interval_unit_to_sql(
