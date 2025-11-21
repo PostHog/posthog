db3eed34bd6d3a306431ea124619ed428b8470d2
Arthur Moreira de Deus <arthur@posthog.com>
2025-11-21 15:53:51 -0300

    chore: Fix type errors

--- products/customer_analytics/frontend/customerAnalyticsConfigLogic.ts
+++ products/customer_analytics/frontend/customerAnalyticsConfigLogic.ts
@@ -3,7 +3,13 @@ import { actions, connect, kea, listeners, path, selectors } from 'kea'
 import { isEmptyObject } from 'lib/utils'
 import { teamLogic } from 'scenes/teamLogic'
 
-import { ActionsNode, CustomerAnalyticsConfig, DataWarehouseNode, EventsNode } from '~/queries/schema/schema-general'
+import {
+    ActionsNode,
+    AnyEntityNode,
+    CustomerAnalyticsConfig,
+    DataWarehouseNode,
+    EventsNode,
+} from '~/queries/schema/schema-general'
 
 import type { customerAnalyticsConfigLogicType } from './customerAnalyticsConfigLogicType'
 
@@ -22,32 +28,38 @@ export const customerAnalyticsConfigLogic = kea<customerAnalyticsConfigLogicType
     selectors({
         activityEvent: [
             (s) => [s.customerAnalyticsConfig],
-            (config: CustomerAnalyticsConfig) =>
-                !isEmptyObject(config.activity_event) ? { ...config.activity_event, custom_name: 'Activity' } : {},
+            (config: CustomerAnalyticsConfig): AnyEntityNode =>
+                !isEmptyObject(config.activity_event)
+                    ? { ...config.activity_event, custom_name: 'Activity' }
+                    : ({} as AnyEntityNode),
         ],
         signupEvent: [
             (s) => [s.customerAnalyticsConfig],
-            (config: CustomerAnalyticsConfig) =>
-                !isEmptyObject(config.signup_event) ? { ...config.signup_event, custom_name: 'Signups' } : {},
+            (config: CustomerAnalyticsConfig): AnyEntityNode =>
+                !isEmptyObject(config.signup_event)
+                    ? { ...config.signup_event, custom_name: 'Signups' }
+                    : ({} as AnyEntityNode),
         ],
         signupPageviewEvent: [
             (s) => [s.customerAnalyticsConfig],
-            (config: CustomerAnalyticsConfig) =>
+            (config: CustomerAnalyticsConfig): AnyEntityNode =>
                 !isEmptyObject(config.signup_pageview_event)
                     ? { ...config.signup_pageview_event, custom_name: 'Signup pageviews' }
-                    : {},
+                    : ({} as AnyEntityNode),
         ],
         subscriptionEvent: [
             (s) => [s.customerAnalyticsConfig],
-            (config: CustomerAnalyticsConfig) =>
+            (config: CustomerAnalyticsConfig): AnyEntityNode =>
                 !isEmptyObject(config.subscription_event)
                     ? { ...config.subscription_event, custom_name: 'Subscriptions' }
-                    : {},
+                    : ({} as AnyEntityNode),
         ],
         paymentEvent: [
             (s) => [s.customerAnalyticsConfig],
-            (config: CustomerAnalyticsConfig) =>
-                !isEmptyObject(config.payment_event) ? { ...config.payment_event, custom_name: 'Payments' } : {},
+            (config: CustomerAnalyticsConfig): AnyEntityNode =>
+                !isEmptyObject(config.payment_event)
+                    ? { ...config.payment_event, custom_name: 'Payments' }
+                    : ({} as AnyEntityNode),
         ],
     }),
 
