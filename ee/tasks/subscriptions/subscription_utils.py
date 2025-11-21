5d6c12704712e3c6f9a4e73082ec429a01da4362
Andy Zhao <andy.z@posthog.com>
2025-11-21 17:54:42 +0100

    fix(subscriptions): send slack error message for assets that timeout

--- ee/tasks/subscriptions/subscription_utils.py
+++ ee/tasks/subscriptions/subscription_utils.py
@@ -28,8 +28,12 @@ DEFAULT_MAX_ASSET_COUNT = 6
 MAX_SCREENSHOT_HEIGHT_PIXELS = 5000
 
 
+def _has_asset_failed(asset: ExportedAsset) -> bool:
+    return (not asset.content and not asset.content_location) or asset.exception is not None
+
+
 def _get_failed_asset_info(assets: list[ExportedAsset], resource: Union[Subscription, SharingConfiguration]) -> dict:
-    failed_assets = [a for a in assets if not a.content and not a.content_location]
+    failed_assets = [a for a in assets if _has_asset_failed(a)]
     failed_insight_ids = [a.insight_id for a in failed_assets if a.insight_id]
     failed_insight_urls = [
         f"/project/{resource.team_id}/insights/{a.insight.short_id}"
