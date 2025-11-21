b26fb50dbee7d019998037c981f61daa2ace8412
Robbie Coomber <robbie.coomber@gmail.com>
2025-11-21 22:03:59 +0000

    Fix

--- posthog/clickhouse/migrations/0181_add_materialize_column_slots_ingestion.py
+++ posthog/clickhouse/migrations/0181_add_materialize_column_slots_ingestion.py
@@ -3,6 +3,7 @@ from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 from posthog.models.event.sql import (
     ALTER_TABLE_ADD_DYNAMICALLY_MATERIALIZED_COLUMNS,
     EVENTS_TABLE_JSON_MV_SQL,
+    KAFKA_EVENTS_TABLE_JSON_SQL,
     WRITABLE_EVENTS_DATA_TABLE,
 )
 
@@ -14,8 +15,12 @@ ATTACH_KAFKA = """
     ATTACH TABLE IF EXISTS kafka_events_json;
 """
 
-DROP_EVENTS_JSON_MV = """
+DROP_EVENTS_TABLE_JSON_MV = """
     DROP TABLE IF EXISTS events_json_mv SYNC;
+"""
+
+DROP_KAFKA_EVENTS_TABLE = """
+    DROP TABLE IF EXISTS kafka_events_json SYNC;
 """
 
 # See when these were added to the sharded / distributed tables in 0179
@@ -28,31 +33,32 @@ operations = [
         sharded=False,
         is_alter_on_replicated_table=False,
     ),
-    # kafka table
+    # drop events mv
     run_sql_with_exceptions(
-        ALTER_TABLE_ADD_DYNAMICALLY_MATERIALIZED_COLUMNS(table="kafka_events_json"),
+        DROP_EVENTS_TABLE_JSON_MV,
         node_roles=[NodeRole.INGESTION_EVENTS],
         sharded=False,
         is_alter_on_replicated_table=False,
     ),
-    # pause ingestion from the kafka tables
+    # drop kafka table
     run_sql_with_exceptions(
-        DETACH_KAFKA, node_roles=[NodeRole.INGESTION_EVENTS], sharded=False, is_alter_on_replicated_table=False
-    ),
-    # drop MV
-    run_sql_with_exceptions(
-        DROP_EVENTS_JSON_MV,
+        DROP_KAFKA_EVENTS_TABLE,
         node_roles=[NodeRole.INGESTION_EVENTS],
         sharded=False,
         is_alter_on_replicated_table=False,
     ),
-    # recreate MV
+    # recreate kafka table
+    run_sql_with_exceptions(
+        KAFKA_EVENTS_TABLE_JSON_SQL(),
+        node_roles=[NodeRole.INGESTION_EVENTS],
+        sharded=False,
+        is_alter_on_replicated_table=False,
+    ),
+    # recreate events MV
     run_sql_with_exceptions(
         EVENTS_TABLE_JSON_MV_SQL(),
         node_roles=[NodeRole.INGESTION_EVENTS],
         sharded=False,
         is_alter_on_replicated_table=False,
     ),
-    # resume ingestion
-    run_sql_with_exceptions(ATTACH_KAFKA, node_roles=[NodeRole.INGESTION_EVENTS]),
 ]
