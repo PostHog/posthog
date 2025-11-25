95c6077f01274edc60d16168aeadc80e6dbbf32a
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-11-25 17:19:16 -0600

    create data_modeling app; create node model

---
+++ products/data_modeling/backend/models/node.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from posthog.models.utils import CreatedMetaFields, UpdatedMetaFields, UUIDModel
+
+
+class Node(UUIDModel, CreatedMetaFields, UpdatedMetaFields):
+    team = models.ForeignKey("posthog.Team", on_delete=models.CASCADE, db_index=True)
+    # TODO(andrew): add something to alert the user that deleting a saved query that is used in a DAG
+    # can really fuck things up. deleting the saved query deletes the node which deletes all edges
+    # where the node is either the source or the target (big blast radius for something small)
+    saved_query = models.ForeignKey("data_warehouse.DataWarehouseSavedQuery", on_delete=models.CASCADE, db_index=True)
+    dag_id = models.TextField(max_length=256, default="posthog", db_index=True)
+    properties = models.JSONField(default=dict)
+
+    class Meta:
+        db_table = "posthog_datamodelingnode"
