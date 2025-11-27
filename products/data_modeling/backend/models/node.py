56c800904df24046b7bda442584a4034dcbf64d3
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-11-26 18:30:24 -0600

    use one to one; remove unneeded db_index

--- products/data_modeling/backend/models/node.py
+++ products/data_modeling/backend/models/node.py
@@ -2,13 +2,15 @@ from django.db import models
 
 from posthog.models.utils import CreatedMetaFields, UpdatedMetaFields, UUIDModel
 
+from products.data_warehouse.backend.models import DataWarehouseSavedQuery
+
 
 class Node(UUIDModel, CreatedMetaFields, UpdatedMetaFields):
-    team = models.ForeignKey("posthog.Team", on_delete=models.CASCADE, db_index=True)
+    team = models.ForeignKey("posthog.Team", on_delete=models.CASCADE)
     # TODO(andrew): add something to alert the user that deleting a saved query that is used in a DAG
     # can really fuck things up. deleting the saved query deletes the node which deletes all edges
     # where the node is either the source or the target (big blast radius for something small)
-    saved_query = models.ForeignKey("data_warehouse.DataWarehouseSavedQuery", on_delete=models.CASCADE, unique=True)
+    saved_query = models.OneToOneField(DataWarehouseSavedQuery, on_delete=models.CASCADE)
     dag_id = models.TextField(max_length=256, default="posthog", db_index=True)
     properties = models.JSONField(default=dict)
 
