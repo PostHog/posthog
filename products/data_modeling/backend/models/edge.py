f16351d5e73d2020734e62d6f28aac0b8558502e
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-11-29 17:38:06 -0600

    make fewer things editable; make saved queries not one to one

--- products/data_modeling/backend/models/edge.py
+++ products/data_modeling/backend/models/edge.py
@@ -4,9 +4,17 @@ from posthog.models.utils import CreatedMetaFields, UpdatedMetaFields, UUIDModel
 
 
 class Edge(UUIDModel, CreatedMetaFields, UpdatedMetaFields):
-    source = models.ForeignKey("data_modeling.Node", related_name="outgoing_edges", on_delete=models.CASCADE)
-    target = models.ForeignKey("data_modeling.Node", related_name="incoming_edges", on_delete=models.CASCADE)
-    dag_id = models.TextField(max_length=256, default="posthog", db_index=True)
+    team = models.ForeignKey("posthog.Team", on_delete=models.CASCADE, editable=False)
+    # the source node of the edge (i.e. the node this edge is pointed away from)
+    source = models.ForeignKey(
+        "data_modeling.Node", related_name="outgoing_edges", on_delete=models.CASCADE, editable=False
+    )
+    # the target node of the edge (i.e. the node this edge is pointed toward)
+    target = models.ForeignKey(
+        "data_modeling.Node", related_name="incoming_edges", on_delete=models.CASCADE, editable=False
+    )
+    # the name of the DAG this edge belongs to
+    dag_id = models.TextField(max_length=256, default="posthog", db_index=True, editable=False)
     properties = models.JSONField(default=dict)
 
     class Meta:
