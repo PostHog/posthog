db3fe08eaf7e12846f44887b82ac2a9f6c1c1831
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-11-28 22:09:52 -0600

    update edge model

--- products/data_modeling/backend/models/edge.py
+++ products/data_modeling/backend/models/edge.py
@@ -4,15 +4,13 @@ from posthog.models.utils import CreatedMetaFields, UpdatedMetaFields, UUIDModel
 
 
 class Edge(UUIDModel, CreatedMetaFields, UpdatedMetaFields):
-    team = models.ForeignKey("posthog.Team", on_delete=models.CASCADE)
-    source = models.ForeignKey(
-        "data_modeling.Node", related_name="source", on_delete=models.CASCADE, null=True, blank=True
-    )
-    target = models.ForeignKey(
-        "data_modeling.Node", related_name="target", on_delete=models.CASCADE, null=True, blank=True
-    )
+    source = models.ForeignKey("data_modeling.Node", related_name="outgoing_edges", on_delete=models.CASCADE)
+    target = models.ForeignKey("data_modeling.Node", related_name="incoming_edges", on_delete=models.CASCADE)
     dag_id = models.TextField(max_length=256, default="posthog", db_index=True)
     properties = models.JSONField(default=dict)
 
     class Meta:
         db_table = "posthog_datamodelingedge"
+        constraints = [
+            models.UniqueConstraint(fields=["dag_id", "source", "target"], name="unique_within_dag"),
+        ]
