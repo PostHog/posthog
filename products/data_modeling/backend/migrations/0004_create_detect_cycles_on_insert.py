3b8ce1678e805502b6bcf03332aa9da41453083e
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-12-01 14:45:12 -0600

    single DROP per migration

--- products/data_modeling/backend/migrations/0004_create_detect_cycles_on_insert.py
+++ products/data_modeling/backend/migrations/0004_create_detect_cycles_on_insert.py
@@ -6,26 +6,12 @@ DROP_TRIGGER_DETECT_CYCLES_ON_INSERT = """\
 DROP TRIGGER IF EXISTS posthog_datamodelingedge_detect_cycles_on_insert ON posthog_datamodelingedge;
 """
 
-DROP_TRIGGER_DETECT_CYCLES_ON_UPDATE = """\
-DROP TRIGGER IF EXISTS posthog_datamodelingedge_detect_cycles_on_update ON posthog_datamodelingedge;
-"""
-
 # workaround for create trigger if not exists is to drop if exists and then create
-
 CREATE_TRIGGER_DETECT_CYCLES_ON_INSERT = f"""\
 {DROP_TRIGGER_DETECT_CYCLES_ON_INSERT}
 CREATE TRIGGER posthog_datamodelingedge_detect_cycles_on_insert
 BEFORE INSERT ON posthog_datamodelingedge
 FOR EACH ROW
-EXECUTE FUNCTION posthog_datamodelingedge_detect_cycles();
-"""
-
-CREATE_TRIGGER_DETECT_CYCLES_ON_UPDATE = f"""\
-{DROP_TRIGGER_DETECT_CYCLES_ON_UPDATE}
-CREATE TRIGGER posthog_datamodelingedge_detect_cycles_on_update
-BEFORE UPDATE OF source_id, target_id ON posthog_datamodelingedge
-FOR EACH ROW
-WHEN (OLD.source_id IS DISTINCT FROM NEW.source_id OR OLD.target_id IS DISTINCT FROM NEW.target_id)
 EXECUTE FUNCTION posthog_datamodelingedge_detect_cycles();
 """
 
@@ -37,5 +23,4 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(CREATE_TRIGGER_DETECT_CYCLES_ON_INSERT, DROP_TRIGGER_DETECT_CYCLES_ON_INSERT),
-        migrations.RunSQL(CREATE_TRIGGER_DETECT_CYCLES_ON_UPDATE, DROP_TRIGGER_DETECT_CYCLES_ON_UPDATE),
     ]
