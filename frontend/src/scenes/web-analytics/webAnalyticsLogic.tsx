643ff14fb73c1ebceedd6e57c9c46c826304156a
Jordan Mryyan <jordan.m@posthog.com>
2025-12-01 14:23:57 -0600

    Revert unrelated changes

--- frontend/src/scenes/web-analytics/webAnalyticsLogic.tsx
+++ frontend/src/scenes/web-analytics/webAnalyticsLogic.tsx
@@ -1078,7 +1078,6 @@ export const webAnalyticsLogic = kea<webAnalyticsLogicType>([
                         'visitors',
                         'views',
                         source?.includeBounceRate ? 'bounce_rate' : null,
-                        source?.includeAvgTimeOnPage ? 'avg_time_on_page' : null,
                         'cross_sell',
                     ].filter(isNotNil)
 
@@ -1384,8 +1383,6 @@ export const webAnalyticsLogic = kea<webAnalyticsLogicType>([
                                               includeScrollDepth: false, // TODO needs some perf work before it can be enabled
                                               includeBounceRate: true,
                                               doPathCleaning: isPathCleaningEnabled,
-                                              includeAvgTimeOnPage:
-                                                  !!featureFlags[FEATURE_FLAGS.AVERAGE_PAGE_VIEW_COLUMN],
                                           },
                                           {
                                               docs: {
@@ -1429,8 +1426,6 @@ export const webAnalyticsLogic = kea<webAnalyticsLogicType>([
                                               includeBounceRate: true,
                                               includeScrollDepth: false,
                                               doPathCleaning: isPathCleaningEnabled,
-                                              includeAvgTimeOnPage:
-                                                  !!featureFlags[FEATURE_FLAGS.AVERAGE_PAGE_VIEW_COLUMN],
                                           },
                                           {
                                               docs: {
