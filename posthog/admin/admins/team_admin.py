536de14fa91ff05b84d234500360eec1d699b866
Paul D'Ambra <paul@posthog.com>
2025-12-01 23:11:35 +0000

    fix: exclude deprecated fields from team admin form (#42444)
    i can't load team 2 in the django admin form
    
    it has ~900kb of deprecated fields
    let's assume it's that and exclude them 🙈

--- posthog/admin/admins/team_admin.py
+++ posthog/admin/admins/team_admin.py
@@ -9,6 +9,7 @@ from posthog.admin.inlines.team_marketing_analytics_config_inline import TeamMar
 from posthog.admin.inlines.user_product_list_inline import UserProductListInline
 from posthog.models import Team
 from posthog.models.remote_config import cache_key_for_team_token
+from posthog.models.team.team import DEPRECATED_ATTRS
 
 
 class TeamAdmin(admin.ModelAdmin):
@@ -47,6 +48,7 @@ class TeamAdmin(admin.ModelAdmin):
         "remote_config_cache_actions",
     ]
 
+    exclude = DEPRECATED_ATTRS
     inlines = [TeamMarketingAnalyticsConfigInline, UserProductListInline]
     fieldsets = [
         (
