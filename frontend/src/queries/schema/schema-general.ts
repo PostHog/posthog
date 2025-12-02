7ef16e127b2146fbc4290b45ac3928b660511dab
odin-posthog <odin@posthog.com>
2025-12-02 16:10:25 +0100

    fix(CDP): load metrics data even on cache miss, used to show empty data (#42416)

--- frontend/src/queries/schema/schema-general.ts
+++ frontend/src/queries/schema/schema-general.ts
@@ -1629,6 +1629,7 @@ export type LifecycleFilter = {
     stacked?: boolean
 }
 
+// See posthog/hogql_queries/query_runner.py `ExecutionMode` for details on what the types mean
 export type RefreshType =
     | 'async'
     | 'async_except_on_cache_miss'
