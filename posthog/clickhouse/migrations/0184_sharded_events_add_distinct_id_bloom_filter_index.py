535550596ea9ef01527d00786e5a25d36321eb4a
Pawel Szczur <pawel@posthog.com>
2025-12-03 13:33:34 +0100

    typo

--- posthog/clickhouse/migrations/0184_sharded_events_add_distinct_id_bloom_filter_index.py
+++ posthog/clickhouse/migrations/0184_sharded_events_add_distinct_id_bloom_filter_index.py
@@ -2,7 +2,7 @@ from posthog.clickhouse.client.connection import NodeRole
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 
 ADD_BLOOM_FILTER_INDEX_SHARDED_EVENTS = """
-ALTER TABLE IF EXISTS sharded_events
+ALTER TABLE sharded_events
 ADD INDEX IF NOT EXISTS bloom_filter_distinct_id distinct_id
 TYPE bloom_filter
 GRANULARITY 1
