0c1401c968e647b40364acb6c8694516c352fe44
Javier Bahamondes <javier@posthog.com>
2025-12-03 15:37:18 -0300

    Fixing test

--- products/marketing_analytics/backend/hogql_queries/marketing_analytics_base_query_runner.py
+++ products/marketing_analytics/backend/hogql_queries/marketing_analytics_base_query_runner.py
@@ -196,12 +196,6 @@ class MarketingAnalyticsBaseQueryRunner(AnalyticsQueryRunner[ResponseType], ABC,
                         ],
                     ),
                 ),
-                ast.Alias(
-                    alias=self.config.total_reported_conversion_value_field,
-                    expr=ast.Call(
-                        name="sum", args=[ast.Field(chain=[MarketingSourceAdapter.reported_conversion_value_field])]
-                    ),
-                ),
             ]
         )
 
