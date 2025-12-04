8b1b2171da0f4c5b59be5287118cdf0476b3077f
Paweł Ledwoń <me@difficult.name>
2025-12-04 08:12:47 +0100

    feat: support session id based event restrictions (#42024)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- plugin-server/src/kafka/consumer.ts
+++ plugin-server/src/kafka/consumer.ts
@@ -906,6 +906,8 @@ export const parseEventHeaders = (headers?: MessageHeader[]): EventHeaders => {
                 result.token = sanitizeString(value)
             } else if (key === 'distinct_id') {
                 result.distinct_id = sanitizeString(value)
+            } else if (key === 'session_id') {
+                result.session_id = sanitizeString(value)
             } else if (key === 'timestamp') {
                 result.timestamp = value
             } else if (key === 'event') {
@@ -927,6 +929,7 @@ export const parseEventHeaders = (headers?: MessageHeader[]): EventHeaders => {
     const trackedHeaders = [
         'token',
         'distinct_id',
+        'session_id',
         'timestamp',
         'event',
         'uuid',
