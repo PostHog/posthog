932fbcbba0e05e5affb88d6d49f76ae628dc7ead
Arthur Moreira de Deus <arthur@posthog.com>
2025-12-04 10:50:04 -0300

    chore: Show upsell banner in group analytics settings when missing add-on

--- frontend/src/scenes/settings/environment/GroupAnalyticsConfig.tsx
+++ frontend/src/scenes/settings/environment/GroupAnalyticsConfig.tsx
@@ -6,6 +6,7 @@ import { LemonButton, LemonDialog, LemonInput, Link } from '@posthog/lemon-ui'
 import { GroupsAccessStatus, groupsAccessLogic } from 'lib/introductions/groupsAccessLogic'
 import { LemonBanner } from 'lib/lemon-ui/LemonBanner'
 import { LemonTable, LemonTableColumns } from 'lib/lemon-ui/LemonTable'
+import { GroupsIntroduction } from 'scenes/groups/GroupsIntroduction'
 
 import { GroupType } from '~/types'
 
@@ -53,11 +54,10 @@ export function GroupAnalyticsConfig(): JSX.Element | null {
         useValues(groupAnalyticsConfigLogic)
     const { setSingular, setPlural, reset, save, deleteGroupType } = useActions(groupAnalyticsConfigLogic)
 
-    const { groupsAccessStatus } = useValues(groupsAccessLogic)
+    const { groupsAccessStatus, needsUpgradeForGroups } = useValues(groupsAccessLogic)
 
-    if (groupsAccessStatus === GroupsAccessStatus.NoAccess) {
-        // Hide settings if the user doesn't have access
-        return null
+    if (needsUpgradeForGroups) {
+        return <GroupsIntroduction />
     }
 
     const columns: LemonTableColumns<GroupType> = [
