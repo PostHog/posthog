37a6cb611cf593f00980beb1b8ac972da0169381
eric <eric@posthog.com>
2025-12-03 16:37:56 -0800

    refactor to use ducklake-task-queue

---
+++ posthog/temporal/ducklake/__init__.py
@@ -0,0 +1,13 @@
+from posthog.temporal.ducklake.ducklake_copy_workflow import (
+    DuckLakeCopyDataModelingWorkflow,
+    copy_data_modeling_model_to_ducklake_activity,
+    ducklake_copy_workflow_gate_activity,
+    prepare_data_modeling_ducklake_metadata_activity,
+)
+
+WORKFLOWS = [DuckLakeCopyDataModelingWorkflow]
+ACTIVITIES = [
+    prepare_data_modeling_ducklake_metadata_activity,
+    ducklake_copy_workflow_gate_activity,
+    copy_data_modeling_model_to_ducklake_activity,
+]
