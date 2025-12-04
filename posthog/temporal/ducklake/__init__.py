966c6e4e17b91971543d182d558b748f30941877
eric <eric@posthog.com>
2025-12-03 16:57:59 -0800

    move

--- posthog/temporal/ducklake/__init__.py
+++ posthog/temporal/ducklake/__init__.py
@@ -3,6 +3,7 @@ from posthog.temporal.ducklake.ducklake_copy_workflow import (
     copy_data_modeling_model_to_ducklake_activity,
     ducklake_copy_workflow_gate_activity,
     prepare_data_modeling_ducklake_metadata_activity,
+    verify_ducklake_copy_activity,
 )
 
 WORKFLOWS = [DuckLakeCopyDataModelingWorkflow]
@@ -10,4 +11,5 @@ ACTIVITIES = [
     prepare_data_modeling_ducklake_metadata_activity,
     ducklake_copy_workflow_gate_activity,
     copy_data_modeling_model_to_ducklake_activity,
+    verify_ducklake_copy_activity,
 ]
