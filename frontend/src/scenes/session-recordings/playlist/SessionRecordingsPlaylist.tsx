15cf94b22fe1d29538deb7d0d5e97ded526a0c77
pauldambra <paul@posthog.com>
2025-12-07 13:56:30 +0000

    fix: correctly constrain height

--- frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylist.tsx
+++ frontend/src/scenes/session-recordings/playlist/SessionRecordingsPlaylist.tsx
@@ -12,7 +12,6 @@ import { playlistFiltersLogic } from './playlistFiltersLogic'
 import { SessionRecordingPlaylistLogicProps, sessionRecordingsPlaylistLogic } from './sessionRecordingsPlaylistLogic'
 
 export function SessionRecordingsPlaylist({
-    showContent = true,
     ...props
 }: SessionRecordingPlaylistLogicProps & {
     showContent?: boolean
@@ -24,7 +23,40 @@ export function SessionRecordingsPlaylist({
         ...props,
         autoPlay: props.autoPlay ?? true,
     }
-    const playlistLogic = sessionRecordingsPlaylistLogic(logicProps)
+
+    const { isCinemaMode } = useValues(playerSettingsLogic)
+
+    return (
+        <BindLogic logic={sessionRecordingsPlaylistLogic} props={logicProps}>
+            <div
+                className={clsx('w-full h-full', {
+                    'flex flex-col xl:flex-row gap-2 overflow-y-auto xl:overflow-hidden': !isCinemaMode,
+                })}
+            >
+                <div
+                    className={clsx('flex flex-col overflow-hidden shrink-0 xl:shrink', {
+                        'xl:w-1/4 xl:min-w-80 order-last xl:order-first': !isCinemaMode,
+                        'w-0': isCinemaMode,
+                    })}
+                >
+                    <Playlist {...props} />
+                </div>
+
+                <PlayerWrapper {...props} />
+            </div>
+        </BindLogic>
+    )
+}
+
+function PlayerWrapper({
+    showContent = true,
+    ...props
+}: SessionRecordingPlaylistLogicProps & {
+    showContent?: boolean
+    type?: 'filters' | 'collection'
+    isSynthetic?: boolean
+    description?: string
+}): JSX.Element {
     const {
         filters,
         pinnedRecordings,
@@ -33,8 +65,8 @@ export function SessionRecordingsPlaylist({
         allowHogQLFilters,
         totalFiltersCount,
         nextSessionRecording,
-    } = useValues(playlistLogic)
-    const { setFilters, resetFilters, setSelectedRecordingId } = useActions(playlistLogic)
+    } = useValues(sessionRecordingsPlaylistLogic)
+    const { setFilters, resetFilters, setSelectedRecordingId } = useActions(sessionRecordingsPlaylistLogic)
 
     const { isFiltersExpanded } = useValues(playlistFiltersLogic)
     const { isCinemaMode } = useValues(playerSettingsLogic)
@@ -46,71 +78,53 @@ export function SessionRecordingsPlaylist({
     }, [nextSessionRecording, setSelectedRecordingId])
 
     return (
-        <BindLogic logic={sessionRecordingsPlaylistLogic} props={logicProps}>
-            <div
-                className={clsx('w-full h-full', {
-                    'flex flex-col xl:flex-row gap-2': !isCinemaMode,
-                })}
-            >
-                <div
-                    className={clsx('flex flex-col', {
-                        'xl:w-1/4 xl:min-w-80 order-last xl:order-first': !isCinemaMode,
-                        'w-0 overflow-hidden': isCinemaMode,
-                    })}
-                >
-                    <Playlist {...props} />
+        <div
+            className={clsx('Playlist__main overflow-hidden shrink-0 xl:shrink', {
+                'w-full': isCinemaMode,
+                'xl:flex-1 xl:h-full min-h-96 xl:rounded xl:border-y xl:border-r border-l xl:border-l-0 rounded-bl':
+                    !isCinemaMode,
+            })}
+        >
+            {isFiltersExpanded ? (
+                <RecordingsUniversalFiltersEmbed
+                    resetFilters={resetFilters}
+                    filters={filters}
+                    setFilters={setFilters}
+                    totalFiltersCount={totalFiltersCount}
+                    allowReplayHogQLFilters={allowHogQLFilters}
+                />
+            ) : showContent && activeSessionRecording ? (
+                <SessionRecordingPlayer
+                    playerKey={props.logicKey ?? 'playlist'}
+                    sessionRecordingId={activeSessionRecording.id}
+                    matchingEventsMatchType={matchingEventsMatchType}
+                    onRecordingDeleted={() => {
+                        sessionRecordingsPlaylistLogic.actions.loadAllRecordings()
+                        sessionRecordingsPlaylistLogic.actions.setSelectedRecordingId(null)
+                    }}
+                    noBorder
+                    pinned={!!pinnedRecordings.find((x) => x.id === activeSessionRecording.id)}
+                    setPinned={
+                        props.onPinnedChange
+                            ? (pinned) => {
+                                  if (!activeSessionRecording.id) {
+                                      return
+                                  }
+                                  props.onPinnedChange?.(activeSessionRecording, pinned)
+                              }
+                            : undefined
+                    }
+                playNextRecording={onPlayNextRecording}/>
+            ) : (
+                <div className="mt-20">
+                    <EmptyMessage
+                        title="No recording selected"
+                        description="Please select a recording from the list on the left"
+                        buttonText="Learn more about recordings"
+                        buttonTo="https://posthog.com/docs/user-guides/recordings"
+                    />
                 </div>
-
-                <div
-                    className={clsx('Playlist__main overflow-hidden', {
-                        'w-full': isCinemaMode,
-                        'xl:flex-1 xl:h-full min-h-96 xl:rounded xl:border-y xl:border-r border-l xl:border-l-0 rounded-bl':
-                            !isCinemaMode,
-                    })}
-                >
-                    {isFiltersExpanded ? (
-                        <RecordingsUniversalFiltersEmbed
-                            resetFilters={resetFilters}
-                            filters={filters}
-                            setFilters={setFilters}
-                            totalFiltersCount={totalFiltersCount}
-                            allowReplayHogQLFilters={allowHogQLFilters}
-                        />
-                    ) : showContent && activeSessionRecording ? (
-                        <SessionRecordingPlayer
-                            playerKey={props.logicKey ?? 'playlist'}
-                            sessionRecordingId={activeSessionRecording.id}
-                            matchingEventsMatchType={matchingEventsMatchType}
-                            onRecordingDeleted={() => {
-                                playlistLogic.actions.loadAllRecordings()
-                                playlistLogic.actions.setSelectedRecordingId(null)
-                            }}
-                            noBorder
-                            pinned={!!pinnedRecordings.find((x) => x.id === activeSessionRecording.id)}
-                            setPinned={
-                                props.onPinnedChange
-                                    ? (pinned) => {
-                                          if (!activeSessionRecording.id) {
-                                              return
-                                          }
-                                          props.onPinnedChange?.(activeSessionRecording, pinned)
-                                      }
-                                    : undefined
-                            }
-                            playNextRecording={onPlayNextRecording}
-                        />
-                    ) : (
-                        <div className="mt-20">
-                            <EmptyMessage
-                                title="No recording selected"
-                                description="Please select a recording from the list on the left"
-                                buttonText="Learn more about recordings"
-                                buttonTo="https://posthog.com/docs/user-guides/recordings"
-                            />
-                        </div>
-                    )}
-                </div>
-            </div>
-        </BindLogic>
+            )}
+        </div>
     )
 }
