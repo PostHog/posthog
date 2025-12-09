5dce9f45c7719438f1aa0fb8e1a00be5f2ebd493
Rafa Audibert <rafael@posthog.com>
2025-12-09 11:29:52 -0300

    refactor: Remove `onboarding-dashboard-templates` legacy FF
    Not being used right now and the code isn't really the cleanest, so let's get rid of it

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -6054,8 +6054,6 @@ export enum OnboardingStepKey {
     PRODUCT_CONFIGURATION = 'configure',
     REVERSE_PROXY = 'proxy',
     INVITE_TEAMMATES = 'invite_teammates',
-    DASHBOARD_TEMPLATE = 'dashboard_template',
-    DASHBOARD_TEMPLATE_CONFIGURE = 'dashboard_template_configure',
     SESSION_REPLAY = 'session_replay',
     AUTHORIZED_DOMAINS = 'authorized_domains',
     SOURCE_MAPS = 'source_maps',
