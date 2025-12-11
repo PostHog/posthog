1c7871f8925c05293d1410d36ae484788a1ebf3c
Adam Leith <adamleithp@users.noreply.github.com>
2025-12-11 14:58:26 +0000

    fix: sql editor tree scroll (#43213)

--- frontend/src/layout/panel-layout/DatabaseTree/DatabaseTree.tsx
+++ frontend/src/layout/panel-layout/DatabaseTree/DatabaseTree.tsx
@@ -1,3 +1,4 @@
+import { ScrollableShadows } from 'lib/components/ScrollableShadows/ScrollableShadows'
 import { ViewLinkModal } from 'scenes/data-warehouse/ViewLinkModal'
 import { DatabaseSearchField } from 'scenes/data-warehouse/editor/sidebar/DatabaseSearchField'
 import { QueryDatabase } from 'scenes/data-warehouse/editor/sidebar/QueryDatabase'
@@ -6,13 +7,18 @@ import { SyncMoreNotice } from './SyncMoreNotice'
 
 export function DatabaseTree(): JSX.Element {
     return (
-        <div className="flex flex-col gap-2 p-3 z-20 border-r border-primary group/colorful-product-icons colorful-product-icons-true">
+        <ScrollableShadows
+            direction="vertical"
+            className="flex flex-col gap-2 z-20 border-r border-primary group/colorful-product-icons colorful-product-icons-true h-[calc(100vh-var(--scene-layout-header-height))] overflow-auto"
+            innerClassName="p-3 flex flex-col gap-2"
+            styledScrollbars
+        >
             <DatabaseSearchField placeholder="Search warehouse" />
             <div className="-mx-2 grow">
                 <QueryDatabase />
             </div>
             <SyncMoreNotice />
             <ViewLinkModal />
-        </div>
+        </ScrollableShadows>
     )
 }
