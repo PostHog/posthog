5118cbaeb44aff69b697db1809c04334c39275c3
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-12-12 14:49:15 +0100

    feat: Allow multiple errors in result

--- products/batch_exports/backend/temporal/pipeline/types.py
+++ products/batch_exports/backend/temporal/pipeline/types.py
@@ -1,3 +1,4 @@
+import collections.abc
 from dataclasses import dataclass
 
 
@@ -16,14 +17,39 @@ class BatchExportResult:
     # and therefore takes into account things like the file type and compression
     bytes_exported: int | None = None
     # This is the error that occurred, if any
-    error: BatchExportError | None = None
+    error: BatchExportError | list[BatchExportError] | None = None
 
     @property
     def error_repr(self) -> str | None:
-        if self.error:
-            return f"{self.error.type}: {self.error.message}"
-        return None
+        match self.error:
+            case [*errors]:
+                return ",".join(f"{error.type}: {error.message}" for error in errors)
+            case None:
+                return None
+            case error:
+                return f"{error.type}: {error.message}"
 
     @classmethod
     def from_exception(cls, e: Exception) -> "BatchExportResult":
         return cls(error=BatchExportError(type=e.__class__.__name__, message=str(e)))
+
+
+def reduce_batch_export_results(results: collections.abc.Iterable[BatchExportResult]) -> BatchExportResult:
+    total_result = BatchExportResult(records_completed=0, bytes_exported=0, error=None)
+
+    for result in results:
+        if result.records_completed is not None:
+            assert total_result.records_completed is not None
+            total_result.records_completed += result.records_completed
+
+        if result.bytes_exported is not None:
+            assert total_result.bytes_exported is not None
+            total_result.bytes_exported += result.bytes_exported
+
+        if result.error is not None:
+            if not isinstance(result.error, list):
+                errors = [result.error]
+            else:
+                errors = result.error
+            total_result.extend(errors)
+    return total_result
