nastasijam/posthog-white-hatting

feat(ph-ai): subagent tool

ee/hogai/utils/feature_flags.py
@@ -11,3 +11,13 @@ def has_agent_modes_feature_flag(team: Team, user: User) -> bool:
         group_properties={"organization": {"id": str(team.organization_id)}},
         send_feature_flag_events=False,
     )
+
+
+def has_subagent_tool_feature_flag(team: Team, user: User) -> bool:
+    return posthoganalytics.feature_enabled(
+        "phai-subagent-tool",
+        str(user.distinct_id),
+        groups={"organization": str(team.organization_id)},
+        group_properties={"organization": {"id": str(team.organization_id)}},
+        send_feature_flag_events=False,
+    )

PostHog/posthog

ee/hogai/utils/feature_flags.py
@@ -44,3 +44,13 @@ def has_phai_tasks_feature_flag(team: Team, user: User) -> bool:
         group_properties={"organization": {"id": str(team.organization_id)}},
         send_feature_flag_events=False,
     )
+
+
+def has_task_tool_feature_flag(team: Team, user: User) -> bool:
+    return posthoganalytics.feature_enabled(
+        "phai-task-tool",
+        str(user.distinct_id),
+        groups={"organization": str(team.organization_id)},
+        group_properties={"organization": {"id": str(team.organization_id)}},
+        send_feature_flag_events=False,
+    )