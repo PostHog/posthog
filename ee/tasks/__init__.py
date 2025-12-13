d660c4daaf3d23fa179ee188a27999abcc257d0d
Andy Zhao <andy.z@posthog.com>
2025-12-12 22:46:26 -0500

    chore: remove celery subscriptions

--- ee/tasks/__init__.py
+++ ee/tasks/__init__.py
@@ -1,9 +1,4 @@
-from .subscriptions import deliver_subscription_report, handle_subscription_value_change, schedule_all_subscriptions
+# EE Celery tasks are declared here to be imported by posthog/settings/celery.py
+# Subscription tasks have been migrated to Temporal workflows
 
-# As our EE tasks are not included at startup for Celery, we need to ensure they are declared here so that they are imported by posthog/settings/celery.py
-
-__all__ = [
-    "schedule_all_subscriptions",
-    "deliver_subscription_report",
-    "handle_subscription_value_change",
-]
+__all__: list[str] = []
