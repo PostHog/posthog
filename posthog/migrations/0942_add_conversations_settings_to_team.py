ca6e00d23c652e9a49d5be0d3946942bd2db5b92
Alex V <alexander.veryaysky@gmail.com>
2025-12-15 15:45:32 +0100

    merge

--- posthog/migrations/0942_add_conversations_settings_to_team.py
+++ posthog/migrations/0942_add_conversations_settings_to_team.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0925_team_business_model"),
+        ("posthog", "0941_add_exported_recording_model"),
     ]
 
     operations = [
