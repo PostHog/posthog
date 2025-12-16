12a275522c82a8bb890e5c3b94b6be2d2b1ef5f1
Alex <alex.lider@posthog.com>
2025-12-16 13:00:01 +0100

    Show cancel trial action when notifications are not silenced

--- frontend/src/scenes/billing/BillingProductAddonActions.tsx
+++ frontend/src/scenes/billing/BillingProductAddonActions.tsx
@@ -96,7 +96,7 @@ export const BillingProductAddonActions = ({
                     You're on a trial for this add-on
                 </LemonTag>
             </Tooltip>
-            {addon.type !== 'enterprise' && (
+            {(addon.type !== 'enterprise' || billing?.trial?.silence_notifications === false) && (
                 <LemonButton
                     type="primary"
                     size="small"
