21bff9fa62164b9d0e87b6d0e7bd9c8a60338eb3
Frank Hamand <frank@posthog.com>
2025-12-16 16:55:05 +0000

    missing type

--- frontend/src/scenes/saved-insights/SavedInsights.tsx
+++ frontend/src/scenes/saved-insights/SavedInsights.tsx
@@ -520,6 +520,11 @@ export const QUERY_TYPES_METADATA: Record<NodeKind, InsightTypeMetadata> = {
         icon: IconLive,
         inMenu: false,
     },
+    [NodeKind.LogValuesQuery]: {
+        name: 'LogValues',
+        icon: IconLive,
+        inMenu: false,
+    },
     [NodeKind.WebAnalyticsExternalSummaryQuery]: {
         name: 'Web Analytics External Summary',
         icon: IconPieChart,
