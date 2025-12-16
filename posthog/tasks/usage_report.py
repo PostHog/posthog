9c4cac6dc302847833a4750c694d9c152a5fdace
pawel-cebula <pawel.c@posthog.com>
2025-12-16 15:39:01 +0100

    chore: increase num_splits on event usage queries

--- posthog/tasks/usage_report.py
+++ posthog/tasks/usage_report.py
@@ -527,7 +527,7 @@ def get_teams_with_billable_event_count_in_period(
         GROUP BY team_id
     """
 
-    return _execute_split_query(begin, end, query_template, {"excluded_events": excluded_events}, num_splits=3)
+    return _execute_split_query(begin, end, query_template, {"excluded_events": excluded_events}, num_splits=4)
 
 
 @timed_log()
@@ -565,7 +565,7 @@ def get_teams_with_billable_enhanced_persons_event_count_in_period(
         GROUP BY team_id
     """
 
-    return _execute_split_query(begin, end, query_template, {"excluded_events": excluded_events}, num_splits=3)
+    return _execute_split_query(begin, end, query_template, {"excluded_events": excluded_events}, num_splits=4)
 
 
 @timed_log()
@@ -663,13 +663,13 @@ def get_all_event_metrics_in_period(begin: datetime, end: datetime) -> dict[str,
 
         return result
 
-    # Execute the split query with 3 splits
+    # Execute the split query with 4 splits
     return _execute_split_query(
         begin=begin,
         end=end,
         query_template=query_template,
         params={},
-        num_splits=3,
+        num_splits=4,
         combine_results_func=combine_event_metrics_results,
     )
 
