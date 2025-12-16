9f40d680807248ed566364d3d8a7c231c0380f93
Jon McCallum <jonathon@posthog.com>
2025-12-16 09:44:08 +0000

    fix(logs): update dependencies in useEffect for sparkline data

--- products/logs/frontend/components/LogsViewer/LogsViewerSparkline/index.tsx
+++ products/logs/frontend/components/LogsViewer/LogsViewerSparkline/index.tsx
@@ -103,7 +103,7 @@ export function LogsSparkline({
                 date_to: dateTo,
             })
         },
-        [onDateRangeChange]
+        [sparklineData.dates, onDateRangeChange]
     )
 
     return (
