cfbfefc8f9f562f5345a6ea4dc00edd00f9ef11b
Pawel Szczur <pawel@posthog.com>
2025-12-18 22:15:17 +0100

    Drop `mat_$set` column from `events` and `sharded_events` tables
    # Conflicts:
    #	posthog/clickhouse/migrations/max_migration.txt

---
+++ posthog/clickhouse/migrations/0192_events_drop_mat_set_column.py
@@ -0,0 +1,25 @@
+from posthog.clickhouse.client.connection import NodeRole
+from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
+
+DROP_MAT_SET_FROM_EVENTS = """
+ALTER TABLE events DROP COLUMN IF EXISTS `mat_$set`
+"""
+
+DROP_MAT_SET_FROM_SHARDED_EVENTS = """
+ALTER TABLE sharded_events DROP COLUMN IF EXISTS `mat_$set`
+"""
+
+operations = [
+    run_sql_with_exceptions(
+        DROP_MAT_SET_FROM_EVENTS,
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
+        sharded=False,
+        is_alter_on_replicated_table=False,
+    ),
+    run_sql_with_exceptions(
+        DROP_MAT_SET_FROM_SHARDED_EVENTS,
+        node_roles=[NodeRole.DATA],
+        sharded=True,
+        is_alter_on_replicated_table=True,
+    ),
+]
