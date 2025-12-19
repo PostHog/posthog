119a36e1341505862ca934742c5002ada8ba2ce8
Pawel Szczur <pawel@posthog.com>
2025-12-19 23:10:48 +0100

    Reduce retry timeout for API queries to 15 seconds

--- posthog/clickhouse/client/limit.py
+++ posthog/clickhouse/client/limit.py
@@ -222,7 +222,7 @@ def get_api_team_rate_limiter():
             # the slot is free.
             retry=0.134,
             # The default timeout for an API query on ClickHouse is 10s. p99 duration is 19s,
-            # 30 seconds should be enough for some other query to finish. If the query cannot get a slot in this period,
+            # 15 seconds should be enough for some other query to finish. If the query cannot get a slot in this period,
             # the user should contact us about increasing the quota.
             retry_timeout=15.0,
         )
