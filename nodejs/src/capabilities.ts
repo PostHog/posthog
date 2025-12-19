71b9322affa3e58c43a0098f5714cc5e5ba850ef
Tue Haulund <tue.haulund@gmail.com>
2025-12-19 14:56:42 +0100

    fix: start recording API when running locally

--- nodejs/src/capabilities.ts
+++ nodejs/src/capabilities.ts
@@ -25,6 +25,7 @@ export function getPluginServerCapabilities(config: PluginsServerConfig): Plugin
                 cdpApi: true,
                 evaluationScheduler: true,
                 logsIngestion: true,
+                recordingApi: true,
             }
 
         case PluginServerMode.local_cdp:
