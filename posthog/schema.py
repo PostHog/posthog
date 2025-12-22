9677af04ac38a699b841af5ca6add0673fe2b448
Rafael Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2025-12-22 18:04:04 -0300

    feat: Create new MRR view for Revenue Analytics (#43710)
    This will speed our MRR calculations up by a lot. We're not using the table yet, we're just creating it so that I can test it with data in production. It already works with the managed viewset which means querying these will be amazingly fast.

--- posthog/schema.py
+++ posthog/schema.py
@@ -965,6 +965,7 @@ class DataWarehouseViewLinkConfiguration(BaseModel):
 class DatabaseSchemaManagedViewTableKind(StrEnum):
     REVENUE_ANALYTICS_CHARGE = "revenue_analytics_charge"
     REVENUE_ANALYTICS_CUSTOMER = "revenue_analytics_customer"
+    REVENUE_ANALYTICS_MRR = "revenue_analytics_mrr"
     REVENUE_ANALYTICS_PRODUCT = "revenue_analytics_product"
     REVENUE_ANALYTICS_REVENUE_ITEM = "revenue_analytics_revenue_item"
     REVENUE_ANALYTICS_SUBSCRIPTION = "revenue_analytics_subscription"
