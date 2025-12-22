d20f23beaca3280dafa2267e37f416614a4cc360
Adam Leith <adamleithp@users.noreply.github.com>
2025-12-22 23:28:12 +0000

    added some docs to scene layout type context

--- frontend/src/scenes/sceneTypes.ts
+++ frontend/src/scenes/sceneTypes.ts
@@ -236,6 +236,7 @@ export interface SceneConfig {
      * If `app-raw`, navigation is shown, but the scene has no padding.
      * If `app-container`, navigation is shown, and the scene is centered with a max width.
      * If `plain`, there's no navigation present, and the scene has no padding.
+     * If `context`, the scene is responsible for the layout styles used with <SceneProvider className="...">.
      * @default 'app'
      */
     layout?: 'app' | 'app-raw' | 'app-container' | 'app-raw-no-header' | 'plain' | 'app-full-scene-height' | 'context'
