4b158781d7a67c06083fbdd331d8b254db2d1a08
Pawel Szczur <pawel@posthog.com>
2025-12-22 14:07:58 +0100

    Drop `minmax_mat_$set` index in migration script alongside `mat_$set` column

--- posthog/clickhouse/migrations/0193_events_drop_mat_set_column.py
+++ posthog/clickhouse/migrations/0193_events_drop_mat_set_column.py
@@ -1,6 +1,10 @@
 from posthog.clickhouse.client.connection import NodeRole
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 
+DROP_MINMAX_MAT_SET_INDEX = """
+ALTER TABLE sharded_events DROP INDEX IF EXISTS `minmax_mat_$set`
+"""
+
 DROP_MAT_SET_FROM_EVENTS = """
 ALTER TABLE events DROP COLUMN IF EXISTS `mat_$set`
 """
@@ -15,6 +19,12 @@ operations = [
         node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
         sharded=False,
         is_alter_on_replicated_table=False,
+    ),
+    run_sql_with_exceptions(
+        DROP_MINMAX_MAT_SET_INDEX,
+        node_roles=[NodeRole.DATA],
+        sharded=True,
+        is_alter_on_replicated_table=True,
     ),
     run_sql_with_exceptions(
         DROP_MAT_SET_FROM_SHARDED_EVENTS,
