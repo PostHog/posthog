c981a8165e4bdbe166eb3d9c0cb3d12d01706699
ablaszkiewicz <aleksander@posthog.com>
2025-12-23 11:52:01 +0100

    feat: wip

--- products/error_tracking/frontend/scenes/ErrorTrackingConfigurationScene/spike_detection/errorTrackingSpikeDetectionLogic.ts
+++ products/error_tracking/frontend/scenes/ErrorTrackingConfigurationScene/spike_detection/errorTrackingSpikeDetectionLogic.ts
@@ -1,7 +1,10 @@
-import { actions, connect, kea, path, reducers } from 'kea'
+import { actions, afterMount, connect, kea, listeners, path, reducers } from 'kea'
+import { subscriptions } from 'kea-subscriptions'
 
 import { clamp } from 'lib/utils'
 import { teamLogic } from 'scenes/teamLogic'
+
+import { TeamType } from '~/types'
 
 import type { errorTrackingSpikeDetectionLogicType } from './errorTrackingSpikeDetectionLogicType'
 
@@ -17,13 +20,31 @@ export const errorTrackingSpikeDetectionLogic = kea<errorTrackingSpikeDetectionL
     })),
     actions({
         setMultiplier: (multiplier: number) => ({ multiplier }),
+        persistSettings: true,
     }),
     reducers(() => ({
         multiplier: [
-            DEFAULT_MULTIPLIER,
+            DEFAULT_MULTIPLIER as number,
             {
                 setMultiplier: (_, { multiplier }) => clamp(Math.round(multiplier), MIN_MULTIPLIER, MAX_MULTIPLIER),
             },
         ],
+    })),
+    subscriptions(({ actions }) => ({
+        currentTeam: (currentTeam: TeamType) => {
+            actions.setMultiplier(currentTeam?.error_tracking_spike_detection_multiplier ?? DEFAULT_MULTIPLIER)
+        },
+    })),
+    afterMount(({ actions, values }) => {
+        actions.setMultiplier(values.currentTeam?.error_tracking_spike_detection_multiplier ?? DEFAULT_MULTIPLIER)
+    }),
+    listeners(({ actions, values }) => ({
+        setMultiplier: () => actions.persistSettings(),
+        persistSettings: async (_, breakpoint) => {
+            await breakpoint(1000)
+            actions.updateCurrentTeam({
+                error_tracking_spike_detection_multiplier: values.multiplier,
+            })
+        },
     })),
 ])
