f51d4afd6df633bbab7c398ab5477014115ae737
Alex Lebedev <alex.l@posthog.com>
2025-12-28 11:24:43 +0100

    feat: Increase the chunk size.

--- posthog/temporal/ai/session_summary/activities/a3_analyze_video_segment.py
+++ posthog/temporal/ai/session_summary/activities/a3_analyze_video_segment.py
@@ -31,7 +31,8 @@ from posthog.temporal.ai.session_summary.types.video import (
 from ee.hogai.session_summaries.session.summarize_session import SingleSessionSummaryLlmInputs
 from ee.hogai.session_summaries.utils import calculate_time_since_start, get_column_index, prepare_datetime
 
-SESSION_VIDEO_CHUNK_DURATION_S = 15
+# TODO: Split not by seconds, but by periods of inactivity and navigation events
+SESSION_VIDEO_CHUNK_DURATION_S = 60
 
 logger = structlog.get_logger(__name__)
 
