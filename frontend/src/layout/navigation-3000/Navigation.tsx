a8de85393020db4d1cf30de1882396e7ba6f0c25
Adam Leith <adamleithp@users.noreply.github.com>
2025-12-29 05:08:31 +0000

    fix: pinned tab sizes

--- frontend/src/layout/navigation-3000/Navigation.tsx
+++ frontend/src/layout/navigation-3000/Navigation.tsx
@@ -21,7 +21,6 @@ import { MinimalNavigation } from './components/MinimalNavigation'
 import { navigation3000Logic } from './navigationLogic'
 import { SidePanel } from './sidepanel/SidePanel'
 import { themeLogic } from './themeLogic'
-import { sidePanelLogic } from './sidepanel/sidePanelLogic'
 
 export function Navigation({
     children,
@@ -34,7 +33,7 @@ export function Navigation({
     const { mobileLayout } = useValues(navigationLogic)
     const { mode } = useValues(navigation3000Logic)
     const mainRef = useRef<HTMLElement>(null)
-    const { mainContentRect, sidePanelWidth } = useValues(panelLayoutLogic)
+    const { mainContentRect, sidePanelWidth, isLayoutNavCollapsed } = useValues(panelLayoutLogic)
     const { setMainContentRef, setMainContentRect } = useActions(panelLayoutLogic)
     const { setTabScrollDepth } = useActions(sceneLogic)
     const { activeTabId } = useValues(sceneLogic)
@@ -54,8 +53,6 @@ export function Navigation({
             setMainContentRef(mainRef)
         }
     }, [mainRef, setMainContentRef])
-
-    console.log('sidePanelWidth', sidePanelWidth)
 
     if (mode !== 'full') {
         return (
@@ -82,64 +79,72 @@ export function Navigation({
                     !simplerAppLayout && 'Navigation3000',
                     !simplerAppLayout && mobileLayout && 'Navigation3000--mobile',
                     !simplerAppLayout && 'Navigation3000--minimal-scene-layout',
-                    simplerAppLayout &&
-                        'app-layout bg-surface-tertiary',
+                    simplerAppLayout && 'app-layout bg-surface-tertiary'
                 )}
-                style={{
-                    ...theme?.mainStyle,
-                    '--scene-layout-rect-right': mainContentRect?.right + 'px',
-                    '--scene-layout-rect-width': mainContentRect?.width + 'px',
-                    '--scene-layout-rect-height': mainContentRect?.height + 'px',
-                    '--scene-layout-scrollbar-width': mainRef?.current?.clientWidth
-                        ? mainRef.current.clientWidth - (mainContentRect?.width ?? 0) + 'px'
-                        : '0px',
-                    '--scene-layout-background': sceneConfig?.canvasBackground
-                        ? 'var(--color-bg-surface-primary)'
-                        : 'var(--color-bg-primary)',
-                    '--side-panel-width': sidePanelWidth + 'px',
-                } as React.CSSProperties}
+                style={
+                    {
+                        ...theme?.mainStyle,
+                        '--scene-layout-rect-right': mainContentRect?.right + 'px',
+                        '--scene-layout-rect-width': mainContentRect?.width + 'px',
+                        '--scene-layout-rect-height': mainContentRect?.height + 'px',
+                        '--scene-layout-scrollbar-width': mainRef?.current?.clientWidth
+                            ? mainRef.current.clientWidth - (mainContentRect?.width ?? 0) + 'px'
+                            : '0px',
+                        '--scene-layout-background': sceneConfig?.canvasBackground
+                            ? 'var(--color-bg-surface-primary)'
+                            : 'var(--color-bg-primary)',
+                        '--side-panel-width': sidePanelWidth + 'px',
+                        ...(simplerAppLayout && {
+                            '--left-nav-width': isLayoutNavCollapsed
+                                ? 'var(--project-navbar-width-collapsed)'
+                                : 'var(--project-navbar-width)',
+                        }),
+                    } as React.CSSProperties
+                }
             >
                 <ProjectDragAndDropProvider>
                     <PanelLayout className={simplerAppLayout ? 'left-nav' : ''} />
 
                     {simplerAppLayout && (
-                        <div
-                            className="top-nav h-[var(--scene-layout-header-height)] sticky top-0 z-[var(--z-main-nav)] flex justify-center items-start"
-                        >
+                        <div className="top-nav h-[var(--scene-layout-header-height)] sticky top-0 z-[var(--z-main-nav)] flex justify-center items-start">
                             <SceneTabs />
                         </div>
                     )}
 
                     <div className="main-content-container overflow-hidden rounded-lg border border-primary mb-2">
-
-                    <main
-                        ref={mainRef}
-                        role="main"
-                        tabIndex={0}
-                        id="main-content"
-                        className={cn("@container/main-content bg-[var(--scene-layout-background)] overflow-y-auto overflow-x-hidden rounded-lg show-scrollbar-on-hover p-4 h-full", {
-                            'p-0': sceneConfig?.layout === 'app-raw-no-header' || sceneConfig?.layout === 'app-raw'
-                        })}
-                        onScroll={(e) => {
-                            if (activeTabId) {
-                                setTabScrollDepth(activeTabId, e.currentTarget.scrollTop)
-                            }
-                        }}
-                    >
-                        <SceneLayout sceneConfig={sceneConfig}>
-                            {(!sceneConfig?.hideBillingNotice || !sceneConfig?.hideProjectNotice) && (
-                                <div
-                                    className={cn({
-                                        'px-4 empty:hidden': sceneConfig?.layout === 'app-raw-no-header',
-                                    })}
-                                >
-                                    {!sceneConfig?.hideBillingNotice && <BillingAlertsV2 className="my-0 mb-4" />}
-                                    {!sceneConfig?.hideProjectNotice && <ProjectNotice className="my-0 mb-4" />}
-                                </div>
+                        <main
+                            ref={mainRef}
+                            role="main"
+                            tabIndex={0}
+                            id="main-content"
+                            className={cn(
+                                '@container/main-content bg-[var(--scene-layout-background)] overflow-y-auto overflow-x-hidden rounded-lg show-scrollbar-on-hover p-4 h-full',
+                                {
+                                    'p-0':
+                                        sceneConfig?.layout === 'app-raw-no-header' ||
+                                        sceneConfig?.layout === 'app-raw',
+                                }
                             )}
-                            {children}
-                        </SceneLayout>
-                    </main>
+                            onScroll={(e) => {
+                                if (activeTabId) {
+                                    setTabScrollDepth(activeTabId, e.currentTarget.scrollTop)
+                                }
+                            }}
+                        >
+                            <SceneLayout sceneConfig={sceneConfig}>
+                                {(!sceneConfig?.hideBillingNotice || !sceneConfig?.hideProjectNotice) && (
+                                    <div
+                                        className={cn({
+                                            'px-4 empty:hidden': sceneConfig?.layout === 'app-raw-no-header',
+                                        })}
+                                    >
+                                        {!sceneConfig?.hideBillingNotice && <BillingAlertsV2 className="my-0 mb-4" />}
+                                        {!sceneConfig?.hideProjectNotice && <ProjectNotice className="my-0 mb-4" />}
+                                    </div>
+                                )}
+                                {children}
+                            </SceneLayout>
+                        </main>
                     </div>
 
                     <SidePanel className={simplerAppLayout ? 'right-nav' : ''} />
