02ac5e6e5fed8e918570dfd02e8fefda9bd7e1fc
Jon McCallum <jonathon@posthog.com>
2025-12-31 11:52:20 +0000

    fix: add missing types for ParsedLogMessage

--- products/logs/frontend/types.ts
+++ products/logs/frontend/types.ts
@@ -1,7 +1,11 @@
 import { LogMessage } from '~/queries/schema/schema-general'
 import { JsonType } from '~/types'
 
-export type ParsedLogMessage = LogMessage & { cleanBody: string; parsedBody: JsonType | null }
+export type ParsedLogMessage = LogMessage & {
+    cleanBody: string
+    parsedBody: JsonType | null
+    originalLog: LogMessage
+}
 
 export type LogsOrderBy = 'earliest' | 'latest' | undefined
 
