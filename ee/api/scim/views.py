ab4be08aa00f6e7188a36ac06360ca23ecc46b45
Thomas Piccirello <tom.p@posthog.com>
2026-01-02 17:47:39 -0800

    Fix reactivating deactivated scim users

--- ee/api/scim/views.py
+++ ee/api/scim/views.py
@@ -1,6 +1,6 @@
 from typing import cast
 
-from django.db.models import QuerySet
+from django.db.models import Q, QuerySet
 
 from django_scim import constants
 from django_scim.filters import GroupFilterQuery, UserFilterQuery
@@ -185,8 +185,9 @@ class SCIMUserDetailView(SCIMBaseView):
     def get_object(self, user_id: int) -> PostHogSCIMUser:
         organization_domain = cast(OrganizationDomain, self.request.auth)
         user = User.objects.filter(
+            Q(organization_membership__organization=organization_domain.organization)
+            | Q(scim_provisions__organization_domain=organization_domain),
             id=user_id,
-            organization_membership__organization=organization_domain.organization,
         ).first()
         if not user:
             raise User.DoesNotExist()
