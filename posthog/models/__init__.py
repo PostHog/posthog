9cb590e11e1dffd2096dc67fe902ebf49744d789
Haven <haven@posthog.com>
2026-01-05 14:44:49 -0600

    tweak

--- posthog/models/__init__.py
+++ posthog/models/__init__.py
@@ -48,7 +48,6 @@ from .group_type_mapping import GroupTypeMapping
 from .heatmap_saved import SavedHeatmap, HeatmapSnapshot
 from .host_definition import HostDefinition
 from .hog_flow import HogFlow
-from .hog_flow_batch_job import HogFlowBatchJob
 from .hog_functions import HogFunction
 from .hog_function_template import HogFunctionTemplate
 from .insight import Insight, InsightViewed
@@ -146,7 +145,6 @@ __all__ = [
     "GroupTypeMapping",
     "HeatmapSnapshot",
     "HogFlow",
-    "HogFlowBatchJob",
     "HogFunction",
     "HogFunctionTemplate",
     "Link",
