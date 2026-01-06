ef6c89ee02e20dfffb842c7677e1f90d9c41a596
Rodrigo Iloro <rodrigo@posthog.com>
2026-01-06 17:36:01 -0300

    feat: adding ACL mixins to the django views.

--- ee/clickhouse/views/experiment_saved_metrics.py
+++ ee/clickhouse/views/experiment_saved_metrics.py
@@ -16,12 +16,14 @@ from posthog.schema import (
     ExperimentTrendsQuery,
 )
 
+from ee.api.rbac.access_control import AccessControlViewSetMixin
 from posthog.api.routing import TeamAndOrgViewSetMixin
 from posthog.api.shared import UserBasicSerializer
 from posthog.api.tagged_item import TaggedItemSerializerMixin
 from posthog.models.activity_logging.activity_log import Detail, changes_between, log_activity
 from posthog.models.experiment import ExperimentSavedMetric, ExperimentToSavedMetric
 from posthog.models.signals import model_activity_signal, mutable_receiver
+from posthog.rbac.user_access_control import UserAccessControlSerializerMixin
 
 
 class ExperimentToSavedMetricSerializer(serializers.ModelSerializer):
@@ -45,7 +47,9 @@ class ExperimentToSavedMetricSerializer(serializers.ModelSerializer):
         ]
 
 
-class ExperimentSavedMetricSerializer(TaggedItemSerializerMixin, serializers.ModelSerializer):
+class ExperimentSavedMetricSerializer(
+    UserAccessControlSerializerMixin, TaggedItemSerializerMixin, serializers.ModelSerializer
+):
     created_by = UserBasicSerializer(read_only=True)
 
     class Meta:
@@ -59,12 +63,14 @@ class ExperimentSavedMetricSerializer(TaggedItemSerializerMixin, serializers.Mod
             "created_at",
             "updated_at",
             "tags",
+            "user_access_level",
         ]
         read_only_fields = [
             "id",
             "created_by",
             "created_at",
             "updated_at",
+            "user_access_level",
         ]
 
     def validate_query(self, value):
@@ -111,8 +117,8 @@ class ExperimentSavedMetricSerializer(TaggedItemSerializerMixin, serializers.Mod
         return super().create(validated_data)
 
 
-class ExperimentSavedMetricViewSet(TeamAndOrgViewSetMixin, viewsets.ModelViewSet):
-    scope_object = "experiment"
+class ExperimentSavedMetricViewSet(TeamAndOrgViewSetMixin, AccessControlViewSetMixin, viewsets.ModelViewSet):
+    scope_object = "experiment_saved_metric"
     queryset = ExperimentSavedMetric.objects.prefetch_related("created_by").order_by(Lower("name")).all()
     serializer_class = ExperimentSavedMetricSerializer
 
