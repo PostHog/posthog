e6fcdcdc26bb4c46d16a8b80dcbf7b6c33271494
Reece Jones <reece@posthog.com>
2026-01-07 17:36:29 -0500

    add loading skeletons to passkey settings while fetching data

--- frontend/src/scenes/settings/user/PasskeySettings.tsx
+++ frontend/src/scenes/settings/user/PasskeySettings.tsx
@@ -1,18 +1,29 @@
 import { useActions, useValues } from 'kea'
 import { useEffect } from 'react'
 
+import { LemonSkeleton } from '@posthog/lemon-ui'
+
 import { PasskeyAddForm, PasskeyAddFormEmpty } from './PasskeyAddForm'
 import { PasskeyList } from './PasskeyList'
 import { PasskeyModals } from './PasskeyModals'
 import { passkeySettingsLogic } from './passkeySettingsLogic'
 
 export function PasskeySettings(): JSX.Element {
-    const { passkeys } = useValues(passkeySettingsLogic)
+    const { passkeys, passkeysLoading } = useValues(passkeySettingsLogic)
     const { loadPasskeys } = useActions(passkeySettingsLogic)
 
     useEffect(() => {
         loadPasskeys()
     }, [loadPasskeys])
+
+    if (passkeysLoading) {
+        return (
+            <div className="space-y-4">
+                <LemonSkeleton className="h-32" />
+                <LemonSkeleton className="h-24" />
+            </div>
+        )
+    }
 
     const hasExistingPasskeys = passkeys.length > 0
 
