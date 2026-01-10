adb53f532a594dc9cc6fabd779b554e4dc24191d
Jordan Mryyan <jordan.m@posthog.com>
2026-01-10 13:11:44 -0600

    Remove unused code

--- frontend/src/scenes/web-analytics/LiveMetricsDashboard/liveWebAnalyticsMetricsLogic.tsx
+++ frontend/src/scenes/web-analytics/LiveMetricsDashboard/liveWebAnalyticsMetricsLogic.tsx
@@ -193,7 +193,6 @@ export const liveWebAnalyticsMetricsLogic = kea<liveWebAnalyticsMetricsLogicType
 
             const url = new URL(`${host}/events`)
             url.searchParams.append('eventType', '$pageview')
-            url.searchParams.append('includeProperties', '')
 
             cache.batch = [] as LiveEvent[]
             cache.lastBatchTime = performance.now()
