b11bf73220d206be38315873486241a2d3bef8eb
mircoboettcher <mirco.boettcher@icloud.com>
2026-01-13 22:02:09 +0700

    fix(insights): exclude dashboardId from logic key for new insights

--- frontend/src/scenes/insights/sharedUtils.test.ts
+++ frontend/src/scenes/insights/sharedUtils.test.ts
@@ -16,6 +16,12 @@ describe('keyForInsightLogicProps', () => {
     const testCases = [
         { in: { teamId: 33, dashboardItemId: Insight123 }, expect: '123' },
         { in: { teamId: 34, dashboardItemId: undefined }, expect: 'defaultKey' },
+        { in: { teamId: 35, dashboardItemId: Insight123, dashboardId: 456 }, expect: '123/on-dashboard-456' },
+        { in: { teamId: 36, dashboardItemId: 'new', dashboardId: 456 }, expect: 'new' },
+        {
+            in: { teamId: 37, dashboardItemId: 'new-AdHoc.xyz' as InsightShortId, dashboardId: 456 },
+            expect: 'new-AdHoc.xyz',
+        },
     ]
 
     testCases.forEach((testCase) => {
