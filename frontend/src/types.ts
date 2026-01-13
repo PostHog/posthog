2e7a2a3d71b77a9b69a49348644ba1a3a95081c0
Dylan Martin <dylan@posthog.com>
2026-01-13 12:09:55 -0800

    feat(flags): support semver comparisons in `user_blast_radius` (#44596)

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -819,6 +819,15 @@ export enum PropertyOperator {
     NotIn = 'not_in',
     IsCleanedPathExact = 'is_cleaned_path_exact',
     FlagEvaluatesTo = 'flag_evaluates_to',
+    SemverEq = 'semver_eq',
+    SemverNeq = 'semver_neq',
+    SemverGt = 'semver_gt',
+    SemverGte = 'semver_gte',
+    SemverLt = 'semver_lt',
+    SemverLte = 'semver_lte',
+    SemverTilde = 'semver_tilde',
+    SemverCaret = 'semver_caret',
+    SemverWildcard = 'semver_wildcard',
 }
 
 export enum SavedInsightsTabs {
