0316fbb624005d3ac3d7aaa63e86dbf3684ee310
George Guvamatanga Munyoro <george.t@posthog.com>
2026-01-15 17:21:03 +0200

    chore(hogql): version bump to trigger pypi release

--- common/hogql_parser/setup.py
+++ common/hogql_parser/setup.py
@@ -36,7 +36,7 @@ module = Extension(
 
 setup(
     name="hogql_parser",
-    version="1.3.0",
+    version="1.3.1",
     url="https://github.com/PostHog/posthog/tree/master/common/hogql_parser",
     description="HogQL parser for internal PostHog use",
     author="PostHog Inc.",
