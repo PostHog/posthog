8863cbe636f3c0b3eed05d7e8fdddf5c6338122e
Adam Bowker <adam.b@posthog.com>
2026-01-16 12:10:55 -0800

    chore(surveys): clean up stale flag surveys-ff-cross-sell (#45105)
    ## Problem
    clean up stale flag: https://us.posthog.com/project/2/feature_flags/250032
    
    <!-- Who are we building for, what are their needs, why is this important? -->
    
    <!-- Does this fix an issue? Uncomment the line below with the issue ID to automatically close it when merged -->
    <!-- Closes #ISSUE_ID -->
    
    ## Changes
    
    <!-- If there are frontend changes, please include screenshots. -->
    <!-- If a reference design was involved, include a link to the relevant Figma frame! -->
    
    ## How did you test this code?
    
    <!-- Briefly describe the steps you took. -->
    <!-- Include automated tests if possible, otherwise describe the manual testing routine. -->
    
    <!-- Docs reminder: If this change requires updated docs, please do that! Engineers are the primary people responsible for their documentation. 🙌 -->
    
    👉 _Stay up-to-date with [PostHog coding conventions](https://posthog.com/docs/contribute/coding-conventions) for a smoother review._
    
    ## Publish to changelog?
    
    <!-- For features only -->
    
    <!-- If publishing, you must provide changelog details in the #changelog Slack channel. You will receive a follow-up PR comment or notification. -->
    
    <!-- If not, write "no" or "do not publish to changelog" to explicitly opt-out of posting to #changelog. Removing this entire section will not prevent posting. -->
    no

--- frontend/src/lib/constants.tsx
+++ frontend/src/lib/constants.tsx
@@ -345,7 +345,6 @@ export const FEATURE_FLAGS = {
     SURVEY_ANALYSIS_MAX_TOOL: 'survey-analysis-max-tool', // owner: #team-surveys
     SURVEY_RESULTS_V2: 'survey-results-v2', // owner: #team-surveys
     SURVEYS_ERROR_TRACKING_CROSS_SELL: 'surveys-in-error-tracking', // owner: @adboio #team-surveys
-    SURVEYS_FF_CROSS_SELL: 'surveys-ff-cross-sell', // owner: @adboio #team-surveys
     SURVEY_HEADLINE_SUMMARY: 'survey-headline-summary', // owner: @adboio #team-surveys
     SURVEYS_INSIGHT_BUTTON_EXPERIMENT: 'ask-users-why-ai-vs-quickcreate', // owner: @adboio #team-surveys multivariate=true
     SURVEYS_WEB_ANALYTICS_CROSS_SELL: 'surveys-in-web-analytics', // owner: @adboio #team-surveys
