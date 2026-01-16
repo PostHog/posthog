35b4f8695bc5f7d64d248ec38f8a5dae6a27458a
nickbest-ph <nick@posthog.com>
2026-01-16 13:26:27 -0800

    no-op cache layer addded

---
+++ rust/personhog-replica/src/storage/cache/mod.rs
@@ -0,0 +1,7 @@
+mod cached_storage;
+mod noop;
+mod traits;
+
+pub use cached_storage::CachedStorage;
+pub use noop::NoopPersonCache;
+pub use traits::PersonCache;
--- rust/personhog-replica/src/storage/mod.rs
+++ rust/personhog-replica/src/storage/mod.rs
@@ -1,3 +1,4 @@
+pub mod cache;
 pub mod error;
 pub mod postgres;
 pub mod traits;
