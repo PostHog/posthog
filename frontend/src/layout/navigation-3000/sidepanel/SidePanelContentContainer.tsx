19acb92b2c0f57c5154dea9eb01a85d21bdd2e3f
Adam Leith <adamleithp@users.noreply.github.com>
2026-01-24 07:04:22 +0000

    add sidepanelcontentcontainer to notebooks so it's all consistent

--- frontend/src/layout/navigation-3000/sidepanel/SidePanelContentContainer.tsx
+++ frontend/src/layout/navigation-3000/sidepanel/SidePanelContentContainer.tsx
@@ -18,7 +18,7 @@ export function SidePanelContentContainer({
                 !isRemovingSidePanelFlag && flagOffClassName,
                 // 3px is just enough to handle the focus within ring for sidepanel notebooks
                 isRemovingSidePanelFlag &&
-                    'flex flex-col flex-1 overflow-y-auto p-3 rounded mr-2 mb-2 ml-[3px] border border-primary',
+                    'flex flex-col flex-1 overflow-y-auto p-3 rounded mr-2 mb-2 ml-[3px] border border-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-primary z-10',
                 className
             )}
         >
