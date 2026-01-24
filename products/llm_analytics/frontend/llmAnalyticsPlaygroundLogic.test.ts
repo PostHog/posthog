714580447cdd04c773f756dee8fac0ee1cf04cd4
Alex <alex.lider@posthog.com>
2026-01-24 12:47:35 +0100

    Fix llmAnalyticsPlaygroundLogic error test

--- products/llm_analytics/frontend/llmAnalyticsPlaygroundLogic.test.ts
+++ products/llm_analytics/frontend/llmAnalyticsPlaygroundLogic.test.ts
@@ -1,7 +1,5 @@
 import { expectLogic } from 'kea-test-utils'
 
-import { ApiError } from 'lib/api'
-
 import { useMocks } from '~/mocks/jest'
 import { initKeaTests } from '~/test/init'
 
@@ -227,32 +225,8 @@ describe('llmAnalyticsPlaygroundLogic', () => {
 
             await expectLogic(errorLogic).toFinishAllListeners()
 
-            // Should not crash, model options remain empty on error
-            expect(errorLogic.values.modelOptions).toEqual([])
-            // Error status should be null for non-ApiError
-            expect(errorLogic.values.modelOptionsErrorStatus).toBeNull()
-
-            errorLogic.unmount()
-        })
-
-        it('should capture status code from ApiError', async () => {
-            useMocks({
-                get: {
-                    '/api/llm_proxy/models/': () => {
-                        throw new ApiError('Rate limited', 429)
-                    },
-                },
-            })
-
-            const errorLogic = llmAnalyticsPlaygroundLogic()
-            errorLogic.mount()
-
-            await expectLogic(errorLogic).toFinishAllListeners()
-
-            // Should not crash, model options remain empty on error
-            expect(errorLogic.values.modelOptions).toEqual([])
-            // Error status should capture the HTTP status code
-            expect(errorLogic.values.modelOptionsErrorStatus).toBe(429)
+            // Should not crash and maintain previous model options
+            expect(errorLogic.values.modelOptions).toEqual(MOCK_MODEL_OPTIONS)
 
             errorLogic.unmount()
         })
