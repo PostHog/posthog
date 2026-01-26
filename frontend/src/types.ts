bd1ec153c2b34dac1cf787cbc93c185ac9011919
Robbie Coomber <robbie.coomber@gmail.com>
2026-01-26 12:30:58 +0000

    Simplify comments

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1515,10 +1515,8 @@ export type AnyCohortCriteriaType = CohortCriteriaType | EmptyCohortCriteriaType
 
 export type MatchType = typeof ENTITY_MATCH_TYPE | typeof PROPERTY_MATCH_TYPE
 
-/** System cohort type for test users */
-export type SystemCohortTypeTestUsers = 'test_users'
-
-/** Union of all system cohort types managed by PostHog */
+// There's only one type of SystemCohort at the moment but let's future-proof
+export type SystemCohortTypeTestUsers = 1
 export type SystemCohortType = SystemCohortTypeTestUsers
 
 export interface CohortType {
