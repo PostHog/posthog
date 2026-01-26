e264c1ef3bcc945d6fb070f220efc68062204abe
Reece Jones <reece@posthog.com>
2026-01-26 17:23:44 -0500

    fix migration

--- posthog/models/user.py
+++ posthog/models/user.py
@@ -46,7 +46,8 @@ NOTIFICATION_DEFAULTS: Notifications = {
 }
 
 # We don't need the following attributes in most cases, so we defer them by default
-DEFERED_ATTRS = ["requested_password_reset_at", "requested_2fa_reset_at"]
+# TODO: add requested_2fa_reset_at after migration completed
+DEFERED_ATTRS = ["requested_password_reset_at"]
 
 ROLE_CHOICES = (
     ("engineering", "Engineering"),
