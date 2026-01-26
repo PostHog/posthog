76a909cfaa80b2eceb0213ac129e54de4cbddc97
Rafa Audibert <rafael@posthog.com>
2026-01-26 16:05:51 -0300

    fix: improve stories

--- frontend/src/lib/components/ProductSetup/ProductSetupPopover.stories.tsx
+++ frontend/src/lib/components/ProductSetup/ProductSetupPopover.stories.tsx
@@ -1,25 +1,18 @@
-import { MOCK_DEFAULT_ORGANIZATION, MOCK_DEFAULT_TEAM } from 'lib/api.mock'
+import { MOCK_DEFAULT_ORGANIZATION } from 'lib/api.mock'
 
-import { Meta, StoryFn, StoryObj } from '@storybook/react'
+import { Meta } from '@storybook/react'
 import { useState } from 'react'
 
 import { LemonButton } from '@posthog/lemon-ui'
 
-import { mswDecorator, useStorybookMocks } from '~/mocks/browser'
+import { mswDecorator } from '~/mocks/browser'
 import { ProductKey } from '~/queries/schema/schema-general'
-import { ActivationTaskStatus, TeamType } from '~/types'
 
 import { ProductSetupPopover } from './ProductSetupPopover'
-import { SetupTaskId } from './types'
 
-interface StoryProps {
-    onboardingTasks: TeamType['onboarding_tasks']
-}
-
-type Story = StoryObj<(props: StoryProps) => JSX.Element>
-
-const meta: Meta<(props: StoryProps) => JSX.Element> = {
+const meta: Meta<typeof ProductSetupPopover> = {
     title: 'Components/ProductSetup/ProductSetupPopover',
+    component: ProductSetupPopover,
     parameters: {
         layout: 'centered',
         viewMode: 'story',
@@ -40,21 +33,12 @@ const meta: Meta<(props: StoryProps) => JSX.Element> = {
 }
 export default meta
 
-const PopoverWrapper = ({ onboardingTasks }: { onboardingTasks: TeamType['onboarding_tasks'] }): JSX.Element => {
+export const Default = (): JSX.Element => {
     const [visible, setVisible] = useState(true)
     const [product, setProduct] = useState(ProductKey.PRODUCT_ANALYTICS)
 
-    useStorybookMocks({
-        get: {
-            '/api/environments/@current/': {
-                ...MOCK_DEFAULT_TEAM,
-                onboarding_tasks: onboardingTasks,
-            },
-        },
-    })
-
     return (
-        <div className="p-4 min-h-80">
+        <div className="p-4 w-200 h-200 bg-white flex items-start justify-end">
             <ProductSetupPopover
                 visible={visible}
                 onClickOutside={() => setVisible(false)}
@@ -67,46 +51,4 @@ const PopoverWrapper = ({ onboardingTasks }: { onboardingTasks: TeamType['onboar
             </ProductSetupPopover>
         </div>
     )
-}
-
-const Template: StoryFn<StoryProps> = ({ onboardingTasks }) => {
-    return <PopoverWrapper onboardingTasks={onboardingTasks} />
-}
-
-export const Default: Story = Template.bind({})
-Default.args = {
-    onboardingTasks: {},
-}
-Default.parameters = {
-    testOptions: { waitForSelector: '[role="dialog"]' },
-}
-
-export const MixedStates: Story = Template.bind({})
-MixedStates.args = {
-    onboardingTasks: {
-        [SetupTaskId.IngestFirstEvent]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.SetUpReverseProxy]: ActivationTaskStatus.SKIPPED,
-        [SetupTaskId.CreateFirstInsight]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.CreateFunnel]: ActivationTaskStatus.SKIPPED,
-    },
-}
-MixedStates.parameters = {
-    testOptions: { waitForSelector: '[role="dialog"]' },
-}
-
-export const AllTasksCompleted: Story = Template.bind({})
-AllTasksCompleted.args = {
-    onboardingTasks: {
-        [SetupTaskId.IngestFirstEvent]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.SetUpReverseProxy]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.CreateFirstInsight]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.CreateFunnel]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.CreateFirstDashboard]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.TrackCustomEvents]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.DefineActions]: ActivationTaskStatus.COMPLETED,
-        [SetupTaskId.SetUpCohorts]: ActivationTaskStatus.COMPLETED,
-    },
-}
-AllTasksCompleted.parameters = {
-    testOptions: { waitForSelector: '[role="dialog"]' },
 }
