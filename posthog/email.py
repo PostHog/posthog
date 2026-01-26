b997a39e43113f998d35832ce7c66c656286d2ef
Yasen <yasen@posthog.com>
2026-01-26 14:26:37 +0200

    feat(approvals): approval notification emails (#45585)

--- posthog/email.py
+++ posthog/email.py
@@ -106,6 +106,11 @@ CUSTOMER_IO_TEMPLATE_ID_MAP = {
     "passkey_added": "51",
     "passkey_removed": "52",
     "new_conversation_ticket": "53",
+    "approval_requested": "54",
+    "approval_approved": "55",
+    "approval_rejected": "56",
+    "approval_expired": "57",
+    "approval_applied": "58",
 }
 
 
