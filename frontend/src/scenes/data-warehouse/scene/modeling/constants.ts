b729f868880d4856c5ea686d9e309cebda58bdb9
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2026-01-27 13:35:56 -0800

    fix unused files

--- frontend/src/scenes/data-warehouse/scene/modeling/constants.ts
+++ frontend/src/scenes/data-warehouse/scene/modeling/constants.ts
@@ -3,6 +3,7 @@ export const NODE_HEIGHT = 70
 
 export const NODE_GAP = 30
 
+export const NODE_EDGE_GAP = NODE_GAP - 1
 export const NODE_LAYER_GAP = NODE_GAP * 1.5
 
 export const TOP_HANDLE_POSITION = {
