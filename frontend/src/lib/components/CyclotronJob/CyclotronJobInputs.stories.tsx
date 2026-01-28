213d0a7685f6a24c36fb3c12c2f663bbf703c7a6
meikel <meikel@posthog.com>
2026-01-28 15:33:55 +0100

    update storybook

--- frontend/src/lib/components/CyclotronJob/CyclotronJobInputs.stories.tsx
+++ frontend/src/lib/components/CyclotronJob/CyclotronJobInputs.stories.tsx
@@ -28,6 +28,37 @@ export const Default = (): JSX.Element => {
                         properties: {
                             value: {
                                 email: '{person.properties.email}',
+                                plan: '{person.properties.plan}',
+                            },
+                        },
+                    },
+                }}
+                onInputChange={() => {}}
+            />
+        </div>
+    )
+}
+
+export const WithEmptyValue = (): JSX.Element => {
+    return (
+        <div className="max-w-160">
+            <CyclotronJobInputs
+                showSource={false}
+                sampleGlobalsWithInputs={null}
+                configuration={{
+                    inputs_schema: [
+                        {
+                            type: 'dictionary',
+                            key: 'properties',
+                            label: 'Property mapping',
+                            description: 'Map PostHog properties to Customer.io attributes',
+                            required: false,
+                        },
+                    ],
+                    inputs: {
+                        properties: {
+                            value: {
+                                email: '{person.properties.email}',
                                 name: '',
                                 plan: '{person.properties.plan}',
                             },
