2914ceccab35de0a2cce2b600faee8a886b35f25
Rodrigo Iloro <rodrigo@posthog.com>
2026-01-28 20:39:22 -0300

    fix(experiments): resets the experiment form when cancelling.

--- frontend/src/scenes/experiments/ExperimentForm/createExperimentLogic.ts
+++ frontend/src/scenes/experiments/ExperimentForm/createExperimentLogic.ts
@@ -243,6 +243,7 @@ export const createExperimentLogic = kea<createExperimentLogicType>([
             })() as { primary: ExperimentMetric[]; secondary: ExperimentMetric[] },
             {
                 setSharedMetrics: (_, { sharedMetrics }) => sharedMetrics,
+                resetExperiment: () => ({ primary: [], secondary: [] }),
             },
         ],
         isExperimentSubmitting: [
@@ -331,10 +332,8 @@ export const createExperimentLogic = kea<createExperimentLogicType>([
     })),
     listeners(({ values, actions, props }) => ({
         clearDraft: () => {
-            if (props.experiment || values.experiment.id !== 'new') {
-                return
-            }
             clearDraftStorage(props.tabId)
+            actions.resetExperiment()
         },
         setExperiment: () => {},
         setExperimentValue: ({ name, value }) => {
