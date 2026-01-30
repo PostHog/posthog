586f7c8837a05cef1d8a0389e1626bd9144c156c
Hayne Ryu <hayne@Haynes-MacBook-Pro.local>
2026-01-30 19:54:58 +0000

    revert back to just copy change

--- frontend/src/scenes/session-recordings/SessionRecordings.tsx
+++ frontend/src/scenes/session-recordings/SessionRecordings.tsx
@@ -8,7 +8,8 @@ import { AccessControlAction } from 'lib/components/AccessControlAction'
 import { AppShortcut } from 'lib/components/AppShortcuts/AppShortcut'
 import { keyBinds } from 'lib/components/AppShortcuts/shortcuts'
 import { LiveRecordingsCount } from 'lib/components/LiveUserCount'
-import { WarningHog } from 'lib/components/hedgehogs'
+import { ProductIntroduction } from 'lib/components/ProductIntroduction/ProductIntroduction'
+import { FilmCameraHog, WarningHog } from 'lib/components/hedgehogs'
 import { FEATURE_FLAGS } from 'lib/constants'
 import { useAsyncHandler } from 'lib/hooks/useAsyncHandler'
 import { useFeatureFlag } from 'lib/hooks/useFeatureFlag'
@@ -135,7 +136,7 @@ function Warnings(): JSX.Element {
 
     return (
         <>
-            {recordingsDisabled && (
+            {recordingsDisabled ? (
                 <LemonBanner type="info" hideIcon={true}>
                     <div className="flex gap-8 p-8 md:flex-row justify-center flex-wrap">
                         <div className="flex justify-center items-center w-full md:w-50">
@@ -190,6 +191,15 @@ function Warnings(): JSX.Element {
                         </div>
                     </div>
                 </LemonBanner>
+            ) : (
+                <ProductIntroduction
+                    productName="session replay"
+                    productKey={ProductKey.SESSION_REPLAY}
+                    thingName="collection"
+                    description="Use session replay collections to easily group and analyze user sessions. Curate collections based on events or user segments, spot patterns, diagnose issues, and share insights with your team."
+                    docsURL="https://posthog.com/docs/session-replay/manual"
+                    customHog={FilmCameraHog}
+                />
             )}
         </>
     )
