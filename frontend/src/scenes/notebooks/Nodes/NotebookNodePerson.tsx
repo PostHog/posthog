fd579ed2a9eb7adc95804a10fcffbfc24916cb0e
Arthur Moreira de Deus <arthur@posthog.com>
2026-01-30 10:19:07 -0300

    chore: Refactor NotebookNodePerson component

--- frontend/src/scenes/notebooks/Nodes/NotebookNodePerson.tsx
+++ frontend/src/scenes/notebooks/Nodes/NotebookNodePerson.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { useActions, useValues } from 'kea'
+import { BindLogic, useActions, useValues } from 'kea'
 import { useEffect } from 'react'
 
 import { Tooltip } from '@posthog/lemon-ui'
@@ -16,6 +16,7 @@ import { personLogic } from 'scenes/persons/personLogic'
 import { urls } from 'scenes/urls'
 
 import { NodeKind } from '~/queries/schema/schema-general'
+import { PersonType } from '~/types'
 
 import { NotebookNodeProps, NotebookNodeType } from '../types'
 import { notebookNodeLogic } from './notebookNodeLogic'
@@ -24,8 +25,8 @@ import { OPTIONAL_PROJECT_NON_CAPTURE_GROUP } from './utils'
 const Component = ({ attributes }: NotebookNodeProps<NotebookNodePersonAttributes>): JSX.Element => {
     const { id, distinctId } = attributes
 
-    const logic = personLogic({ distinctId, id })
-    const { info, infoLoading, person, personLoading } = useValues(logic)
+    const personLogicProps = { id, distinctId }
+    const { person, personLoading } = useValues(personLogic(personLogicProps))
     const { setExpanded, setActions, insertAfter } = useActions(notebookNodeLogic)
     const { setTitlePlaceholder } = useActions(notebookNodeLogic)
 
@@ -106,61 +107,99 @@ const Component = ({ attributes }: NotebookNodeProps<NotebookNodePersonAttribute
     }
 
     return (
-        <div className="flex flex-col overflow-hidden">
-            <div className={clsx('p-4 flex-0 flex flex-col gap-2 justify-between min-h-20 items-start')}>
-                {personLoading ? (
-                    <LemonSkeleton className="h-6" />
-                ) : (
-                    <>
-                        <div className="flex gap-2">
-                            <PersonIcon person={person} size="xl" />
-                            <div>
-                                <div className="font-semibold ph-no-capture">{asDisplay(person)}</div>
-                                <div>{propertyIcons}</div>
-                            </div>
-                        </div>
-
-                        {person ? (
-                            <div className="flex flex-col">
-                                <div className="flex items-center gap-1">
-                                    <span className="text-secondary">First seen:</span>{' '}
-                                    {person.created_at ? <TZLabel time={person.created_at} /> : 'unknown'}
-                                </div>
-                                <div className="flex items-center gap-1">
-                                    <span className="text-secondary">Last seen:</span>{' '}
-                                    {infoLoading ? (
-                                        <LemonSkeleton className="h-4 w-24" />
-                                    ) : info?.lastSeen ? (
-                                        <TZLabel time={info.lastSeen} />
-                                    ) : (
-                                        'unknown'
-                                    )}
-                                </div>
-                                <div className="flex items-center gap-1">
-                                    <span className="text-secondary">Session count (30d):</span>{' '}
-                                    {infoLoading ? (
-                                        <LemonSkeleton className="h-4 w-24" />
-                                    ) : info?.sessionCount ? (
-                                        compactNumber(info.sessionCount)
-                                    ) : (
-                                        'unknown'
-                                    )}
-                                </div>
-                                <div className="flex items-center gap-1">
-                                    <span className="text-secondary">Event count (30d):</span>{' '}
-                                    {infoLoading ? (
-                                        <LemonSkeleton className="h-4 w-24" />
-                                    ) : info?.eventCount ? (
-                                        compactNumber(info.eventCount)
-                                    ) : (
-                                        'unknown'
-                                    )}
+        <BindLogic logic={personLogic} props={personLogicProps}>
+            <div className="flex flex-col overflow-hidden">
+                <div className={clsx('p-4 flex-0 flex flex-col gap-2 justify-between min-h-20 items-start')}>
+                    {personLoading ? (
+                        <LemonSkeleton className="h-6" />
+                    ) : (
+                        <>
+                            <div className="flex gap-2">
+                                <PersonIcon person={person} size="xl" />
+                                <div>
+                                    <div className="font-semibold ph-no-capture">{asDisplay(person)}</div>
+                                    <div>{propertyIcons}</div>
                                 </div>
                             </div>
-                        ) : null}
-                    </>
-                )}
+                            <PersonInfo />
+                        </>
+                    )}
+                </div>
             </div>
+        </BindLogic>
+    )
+}
+
+function PersonInfo(): JSX.Element | null {
+    const { person } = useValues(personLogic)
+
+    if (!person) {
+        return null
+    }
+
+    return (
+        <div className="flex flex-col">
+            <FirstSeen person={person} />
+            <LastSeen />
+            <SessionCount />
+            <EventCount />
+        </div>
+    )
+}
+
+function FirstSeen({ person }: { person: PersonType }): JSX.Element {
+    return (
+        <div className="flex items-center gap-1">
+            <span className="text-secondary">First seen:</span>{' '}
+            {person.created_at ? <TZLabel time={person.created_at} /> : 'unknown'}
+        </div>
+    )
+}
+
+function LastSeen(): JSX.Element {
+    const { info, infoLoading } = useValues(personLogic)
+    return (
+        <div className="flex items-center gap-1">
+            <span className="text-secondary">Last seen:</span>{' '}
+            {infoLoading ? (
+                <LemonSkeleton className="h-4 w-24" />
+            ) : info?.lastSeen ? (
+                <TZLabel time={info.lastSeen} />
+            ) : (
+                'unknown'
+            )}
+        </div>
+    )
+}
+
+function SessionCount(): JSX.Element {
+    const { info, infoLoading } = useValues(personLogic)
+    return (
+        <div className="flex items-center gap-1">
+            <span className="text-secondary">Session count (30d):</span>{' '}
+            {infoLoading ? (
+                <LemonSkeleton className="h-4 w-24" />
+            ) : info?.sessionCount ? (
+                compactNumber(info.sessionCount)
+            ) : (
+                'unknown'
+            )}
+        </div>
+    )
+}
+
+function EventCount(): JSX.Element {
+    const { info, infoLoading } = useValues(personLogic)
+    return (
+        <div className="flex items-center gap-1">
+            <span className="text-secondary">Event count (30d):</span>{' '}
+            {infoLoading ? (
+                <LemonSkeleton className="h-4 w-24" />
+            ) : info?.eventCount ? (
+                compactNumber(info.eventCount)
+            ) : (
+                'unknown'
+            )}
         </div>
     )
 }
