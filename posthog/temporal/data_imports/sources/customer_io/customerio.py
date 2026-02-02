51b2fd7448e4cbe8f4eca4bd34494965b8495074
MarconLP <13001502+MarconLP@users.noreply.github.com>
2026-02-02 15:03:32 +0100

    fix

--- posthog/temporal/data_imports/sources/customer_io/customerio.py
+++ posthog/temporal/data_imports/sources/customer_io/customerio.py
@@ -160,7 +160,7 @@ def customerio_source(
         primary_keys=["id"],
         partition_count=1,
         partition_size=1,
-        partition_mode="datetime",
-        partition_format="week",
-        partition_keys=[endpoint_config.partition_key] if endpoint_config.partition_key else [],
+        partition_mode="datetime" if endpoint_config.partition_key else None,
+        partition_format="week" if endpoint_config.partition_key else None,
+        partition_keys=[endpoint_config.partition_key] if endpoint_config.partition_key else None,
     )
