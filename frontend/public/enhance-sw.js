9e16471e0ddc4820a3ee909ffddafd719ca556e4
Haziq Nordin <hbnordin2@gmail.com>
2026-02-03 15:33:51 +1000

    feat: add Enhance SDK

---
+++ frontend/public/enhance-sw.js
@@ -0,0 +1,3 @@
+self.__ENHANCE_PUBLISHABLE_KEY__ = "ENHANCE-XENV-PUBKEY";
+self.__ENHANCE_CDN__ = "https://cdn.enhancelabs.ai";
+importScripts(self.__ENHANCE_CDN__ + "/loader/loader.v1.js");
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import '~/styles'
 
+import { enhance } from '@enhance-eng/sandbox'
 import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill'
 import { getContext } from 'kea'
 import posthog from 'posthog-js'
@@ -13,6 +14,8 @@ import { initKea } from './initKea'
 import { ErrorBoundary } from './layout/ErrorBoundary'
 import { loadPostHogJS } from './loadPostHogJS'
 import { preWarmDecompression } from './scenes/session-recordings/player/snapshot-processing/DecompressionWorkerManager'
+
+enhance()
 
 loadPostHogJS()
 initKea()
