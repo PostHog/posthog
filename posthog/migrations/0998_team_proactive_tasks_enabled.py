0794f8f94e7ad327eacd70f57369de9754feedd2
Michael Matloka <michael@matloka.com>
2026-02-03 23:22:59 +0100

    Update migration

--- posthog/migrations/0998_team_proactive_tasks_enabled.py
+++ posthog/migrations/0998_team_proactive_tasks_enabled.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "0987_add_column_configuration_constraints"),
+        ("posthog", "0997_oauthapplication_auth_brand"),
     ]
 
     operations = [
