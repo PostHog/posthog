e4fc3a672ef30961adb01fe67cfd3d494bf9fb7a
Adam Bowker <adam.b@posthog.com>
2026-02-03 19:08:04 -0800

    feat(product tours): support text alignment in step editor (#46313)
    ## Problem
    
    want to support text alignment in the tour editor
    
    <!-- Who are we building for, what are their needs, why is this important? -->
    
    <!-- Does this fix an issue? Uncomment the line below with the issue ID to automatically close it when merged -->
    
    <!-- Closes #ISSUE_ID -->
    
    ## Changes
    
    |  |  |
    | --- | --- |
    | ![Screenshot 2026-01-29 at 12.46.47 PM.png](https://app.graphite.com/user-attachments/assets/542ed4de-29b0-46f3-b983-3d3dddeb54a7.png)<br> | ![Screenshot 2026-01-29 at 12.50.33 PM.png](https://app.graphite.com/user-attachments/assets/6d13c7f0-5828-478f-abb5-3161e090e8dd.png)<br> |
    
    sample HTML output:
    
    ```html
    <p xmlns=\"http://www.w3.org/1999/xhtml\">left stuff</p>
    <h1 xmlns=\"http://www.w3.org/1999/xhtml\" style=\"text-align: center;\">middle stuff</h1>
    <h3 xmlns=\"http://www.w3.org/1999/xhtml\" style=\"text-align: right;\">right stuff</h3>
    <p xmlns=\"http://www.w3.org/1999/xhtml\"></p>
    ```
    
    <!-- If there are frontend changes, please include screenshots. -->
    
    <!-- If a reference design was involved, include a link to the relevant Figma frame! -->
    
    ## How did you test this code?
    
    <!-- Briefly describe the steps you took. -->
    
    <!-- Include automated tests if possible, otherwise describe the manual testing routine. -->
    
    <!-- Docs reminder: If this change requires updated docs, please do that! Engineers are the primary people responsible for their documentation. 🙌 -->
    
    👉 _Stay up-to-date with_ [_PostHog coding conventions_](https://posthog.com/docs/contribute/coding-conventions) _for a smoother review._
    
    ## Publish to changelog?
    
    <!-- For features only -->
    
    <!-- If publishing, you must provide changelog details in the #changelog Slack channel. You will receive a follow-up PR comment or notification. -->
    
    <!-- If not, write "no" or "do not publish to changelog" to explicitly opt-out of posting to #changelog. Removing this entire section will not prevent posting. -->
    
    no

--- frontend/src/scenes/product-tours/editor/icons.tsx
+++ frontend/src/scenes/product-tours/editor/icons.tsx
@@ -34,3 +34,33 @@ export function IconListNumbers(): JSX.Element {
         </svg>
     )
 }
+
+export function IconAlignLeft(): JSX.Element {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill="currentColor" d="M3 21h18v-2H3v2zm0-4h12v-2H3v2zm0-4h18v-2H3v2zm0-4h12V7H3v2zm0-6v2h18V3H3z" />
+        </svg>
+    )
+}
+
+export function IconAlignCenter(): JSX.Element {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                fill="currentColor"
+                d="M3 21h18v-2H3v2zm4-4h10v-2H7v2zm-4-4h18v-2H3v2zm4-4h10V7H7v2zm-4-6v2h18V3H3z"
+            />
+        </svg>
+    )
+}
+
+export function IconAlignRight(): JSX.Element {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                fill="currentColor"
+                d="M3 21h18v-2H3v2zm6-4h12v-2H9v2zm-6-4h18v-2H3v2zm6-4h12V7H9v2zm-6-6v2h18V3H3z"
+            />
+        </svg>
+    )
+}
