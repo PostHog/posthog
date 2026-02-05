9c601c343f391d9e53edc8be24c89c4c013ffc4a
meikel <meikel@posthog.com>
2026-02-05 17:32:06 +0100

    reset config

--- nodejs/src/config/config.ts
+++ nodejs/src/config/config.ts
@@ -239,9 +239,9 @@ export function getDefaultConfig(): PluginsServerConfig {
             ? 'postgres://posthog:posthog@localhost:5432/test_cyclotron_shadow'
             : 'postgres://posthog:posthog@localhost:5432/cyclotron_shadow',
         CDP_CYCLOTRON_SHADOW_WRITE_ENABLED: false,
-        CDP_CYCLOTRON_TEST_SEEK_LATENCY: true,
-        CDP_CYCLOTRON_TEST_SEEK_SAMPLE_RATE: 0.1,
-        CDP_CYCLOTRON_TEST_SEEK_MAX_OFFSET: 100,
+        CDP_CYCLOTRON_TEST_SEEK_LATENCY: false,
+        CDP_CYCLOTRON_TEST_SEEK_SAMPLE_RATE: 0.01,
+        CDP_CYCLOTRON_TEST_SEEK_MAX_OFFSET: 50_000_000,
 
         // New IngestionConsumer config
         INGESTION_CONSUMER_GROUP_ID: 'events-ingestion-consumer',
