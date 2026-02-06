2fa234debf8ca096257cd8dc4eb03797f4363672
Adam Leith <adamleithp@users.noreply.github.com>
2026-02-06 13:46:13 +0000

    reduce delayms from 500 to 400

--- frontend/src/lib/lemon-ui/Tooltip/Tooltip.tsx
+++ frontend/src/lib/lemon-ui/Tooltip/Tooltip.tsx
@@ -250,7 +250,7 @@ function TooltipBaseUI({
     fallbackPlacements,
     offset = 8,
     arrowOffset,
-    delayMs = 500,
+    delayMs = 400,
     closeDelayMs = 0,
     interactive = false,
     visible: controlledOpen,
