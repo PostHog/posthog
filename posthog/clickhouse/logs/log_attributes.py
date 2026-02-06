2eaebcf84bf5ca58f0bea47b8f7241b986d1857b
Frank Hamand <frank@posthog.com>
2026-02-06 16:36:04 +0000

    use mergetree helper functions

--- posthog/clickhouse/logs/log_attributes.py
+++ posthog/clickhouse/logs/log_attributes.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from posthog.clickhouse.table_engines import AggregatingMergeTree, ReplicationScheme
+
 TABLE_NAME = "log_attributes"
 
 STORAGE_POLICY = lambda: "hot" if settings.CLICKHOUSE_LOGS_ENABLE_STORAGE_POLICY else "default"
@@ -22,7 +24,7 @@ CREATE TABLE IF NOT EXISTS {settings.CLICKHOUSE_LOGS_CLUSTER_DATABASE}.{TABLE_NA
     INDEX idx_attribute_key_n3 attribute_key TYPE ngrambf_v1(3, 32768, 3, 0) GRANULARITY 1,
     INDEX idx_attribute_value_n3 attribute_value TYPE ngrambf_v1(3, 32768, 3, 0) GRANULARITY 1
 )
-ENGINE = ReplicatedAggregatingMergeTree
+ENGINE = {AggregatingMergeTree("log_attributes", replication_scheme=ReplicationScheme.REPLICATED)}
 PARTITION BY toDate(original_expiry_time_bucket)
 ORDER BY (team_id, attribute_type, time_bucket, resource_fingerprint, attribute_key, attribute_value)
 SETTINGS
