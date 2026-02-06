f118888f7de96a0554e4838da0374448d98372ef
meikel <meikel@posthog.com>
2026-02-06 12:11:09 +0100

    increase bucket size

--- nodejs/src/cdp/services/job-queue/job-queue-kafka.ts
+++ nodejs/src/cdp/services/job-queue/job-queue-kafka.ts
@@ -20,7 +20,7 @@ import { cdpJobSizeKb } from './shared'
 export const cdpSeekLatencyMs = new Histogram({
     name: 'cdp_seek_latency_ms',
     help: 'Latency in ms of seeking back to a specific offset to re-read a message',
-    buckets: [1, 5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],
+    buckets: [1, 5, 10, 25, 50, 100, 150, 200, 250, 500, 1000, 2500, 5000, 10000],
 })
 
 export const cdpSeekResult = new Counter({
