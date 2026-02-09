1235082775ed23bb754a1131525715b0413f7d57
Andrew Maguire <andrewm4894@gmail.com>
2026-02-09 21:26:10 +0000

    chore(llma): Increase DEFAULT_TRACE_BATCH_SIZE from 2 to 4

--- posthog/temporal/llm_analytics/trace_summarization/constants.py
+++ posthog/temporal/llm_analytics/trace_summarization/constants.py
@@ -11,7 +11,7 @@ DEFAULT_MAX_ITEMS_PER_WINDOW = (
     15  # Max items to process per window (targets ~2500 summaries in 7-day clustering window)
 )
 DEFAULT_BATCH_SIZE = 5  # Number of generations to process in parallel
-DEFAULT_TRACE_BATCH_SIZE = 2  # Traces processed in small parallel batches
+DEFAULT_TRACE_BATCH_SIZE = 4  # Traces processed in small parallel batches
 DEFAULT_MODE = SummarizationMode.DETAILED
 DEFAULT_WINDOW_MINUTES = 60  # Process traces from last N minutes (matches schedule frequency)
 DEFAULT_WINDOW_OFFSET_MINUTES = 30  # Offset window into the past so traces have time to fully complete
