3517b6bbc295467b19063f94b08046a730a22bae
Vincent (Wen Yu) Ge <gewenyu99@gmail.com>
2026-02-10 17:08:55 -0500

    Fix unescaped `

--- docs/onboarding/error-tracking/ruby.tsx
+++ docs/onboarding/error-tracking/ruby.tsx
@@ -53,9 +53,9 @@ export const getRubySteps = (ctx: OnboardingComponentsContext): StepDefinition[]
 
                         | Param | Type | Description |
                         | --- | --- | --- |
-                        | `exception` | Exception | The exception object to capture (required) |
-                        | `distinct_id` | String | The distinct ID of the user (optional) |
-                        | `properties` | Hash | Additional properties to attach to the exception event (optional) |
+                        | \`exception\` | Exception | The exception object to capture (required) |
+                        | \`distinct_id\` | String | The distinct ID of the user (optional) |
+                        | \`properties\` | Hash | Additional properties to attach to the exception event (optional) |
                     `}
                 </Markdown>
             </>
