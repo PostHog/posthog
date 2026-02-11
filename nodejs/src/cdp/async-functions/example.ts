3f558c621d1154ac6cd9959508f31c7382eb8959
Haven <haven@posthog.com>
2026-02-11 16:15:49 -0600

    tweak

--- nodejs/src/cdp/async-functions/example.ts
+++ nodejs/src/cdp/async-functions/example.ts
@@ -3,17 +3,16 @@ import { DateTime } from 'luxon'
 import { registerAsyncFunction } from '../async-function-registry'
 
 /**
- * Example of an async function that performs a fetch request. Once an async function has been registered, it can be called from a HogFunction.
+ * Example of a simple async function that writes arguments to the invocation logs. Once an async function has been registered, it can be called from a HogFunction.
  *
- * If you're adding a new destination or workflow action that will use your async function, these are defined in nodejs/src/cdp/templates/_destinations.
+ * If you're trying to add a new realtime destination or workflow action that will use your async function, these are defined in nodejs/src/cdp/templates/_destinations.
  *
- * IMPORTANT: For your async function to be registered, it must be imported in the index.ts file in this directory (nodejs/src/cdp/async-functions/index.ts)
+ * IMPORTANT: For your async function to become registered, it MUST be imported in the index.ts file in this directory (nodejs/src/cdp/async-functions/index.ts)
  */
-
 registerAsyncFunction('foobar', {
     /**
      *
-     * @param args the arguments passed from the Hog code, for example `foobar('i am foo', {'key': 'i am bar'}, 'i am baz')`
+     * @param args the arguments passed from the Hog code, for example `foobar('i am foo', {'key': 200}, 'i am baz')`
      * @param _context includes the invocation details, any global variables attached to the invocation, and a reference to
      *   the HogExecutorServiceHub which provides access to other services and data (e.g. teamManager, featureFlagManager, etc.)
      * @param _result the current state of the invocation result, which can be modified by the async function and serves as a place to store
@@ -22,13 +21,17 @@ registerAsyncFunction('foobar', {
      *   You can stop execution after an error by setting result.finished = true + result.error, and you can log messages to the
      *   invocation logs by pushing to result.logs. See `CyclotronJobInvocationResult` for more details on the structure of the result object.
      */
-    execute: (args, _context, _result) => {
-        const [foo, bar, baz] = args as [string | undefined, Record<string, any> | undefined, string | undefined]
+    execute: async (args, _context, result) => {
+        const [foo, bar, baz] = args as [string | undefined, Record<'key', number> | undefined, string | undefined]
 
         // Do something with the argument - this can be a fetch request, a database query, or any other asynchronous operation
-        console.log(
-            `Executing async function 'foobar' with arguments: foo=${foo}, bar=${JSON.stringify(bar)}, baz=${baz}`
-        )
+        await new Promise((resolve) => setTimeout(resolve, bar?.['key'] || 100))
+
+        result.logs.push({
+            level: 'info',
+            timestamp: DateTime.now(),
+            message: `Executed async function 'foobar' with arguments: foo=${foo}, bar=${JSON.stringify(bar)}, baz=${baz}`,
+        })
     },
     /**
      * A mock implementation of the async function used in destinations and workflows Test tooling, when "Make real HTTP requests" is disabled.
@@ -45,7 +48,7 @@ registerAsyncFunction('foobar', {
         logs.push({
             level: 'info',
             timestamp: DateTime.now(),
-            message: `foobar('${args[0]}', ${JSON.stringify(args[1], null, 2)})`,
+            message: `foobar('${args[0]}', ${JSON.stringify(args[1], null, 2)}, '${args[2]}')`,
         })
 
         return {
