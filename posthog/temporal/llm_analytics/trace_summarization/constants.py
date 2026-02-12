c505d59205786890ec219e8fb1aec9b7c011535c
Andrew Maguire <andrewm4894@gmail.com>
2026-02-12 23:05:38 +0000

    chore(llma): Update default batch sizes for processing (#47779)

--- posthog/temporal/llm_analytics/trace_summarization/constants.py
+++ posthog/temporal/llm_analytics/trace_summarization/constants.py
@@ -10,8 +10,8 @@ from products.llm_analytics.backend.summarization.models import OpenAIModel, Sum
 DEFAULT_MAX_ITEMS_PER_WINDOW = (
     15  # Max items to process per window (targets ~2500 summaries in 7-day clustering window)
 )
-DEFAULT_BATCH_SIZE = 5  # Number of generations to process in parallel
-DEFAULT_TRACE_BATCH_SIZE = 4  # Traces processed in small parallel batches
+DEFAULT_BATCH_SIZE = 10  # Number of generations to process in parallel
+DEFAULT_TRACE_BATCH_SIZE = 6  # Traces processed in small parallel batches
 DEFAULT_MODE = SummarizationMode.DETAILED
 DEFAULT_WINDOW_MINUTES = 60  # Process traces from last N minutes (matches schedule frequency)
 DEFAULT_WINDOW_OFFSET_MINUTES = 30  # Offset window into the past so traces have time to fully complete
