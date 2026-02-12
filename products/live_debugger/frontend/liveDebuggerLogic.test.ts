7e58e5e8c8c9cd660b39fe590f33caf597b90863
Paul D'Ambra <paul.dambra@gmail.com>
2026-02-12 10:31:23 +0000

    Apply suggested changes
    Apply suggested changes
    
     Conflicts:
    	pnpm-lock.yaml

--- products/live_debugger/frontend/liveDebuggerLogic.test.ts
+++ products/live_debugger/frontend/liveDebuggerLogic.test.ts
@@ -1,4 +1,4 @@
-import { expectLogic, resetTestUtilsContext } from 'kea-test-utils'
+import { expectLogic } from 'kea-test-utils'
 
 import api from 'lib/api'
 
@@ -66,7 +66,6 @@ describe('liveDebuggerLogic', () => {
     beforeEach(() => {
         jest.useFakeTimers()
         initKeaTests()
-        resetTestUtilsContext()
     })
 
     afterEach(() => {
