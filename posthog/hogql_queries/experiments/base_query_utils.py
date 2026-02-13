30e773813761ff1d6a77f659bb444f580231e15d
Anders Asheim Hennum <git@andehen.net>
2026-02-13 16:24:49 +0100

    fix null handling in experiment funnl metric

--- posthog/hogql_queries/experiments/base_query_utils.py
+++ posthog/hogql_queries/experiments/base_query_utils.py
@@ -430,7 +430,7 @@ def funnel_evaluation_expr(
                     toFloat({timestamp_field}),
                     {uuid_field},
                     array(''),
-                    arrayFilter(x -> x != 0, [{step_conditions_str}])
+                    arrayFilter(x -> ifNull(notEquals(x, 0), 0), [{step_conditions_str}])
                 )))
             )
         )
