0547cca0d697736aaf9450be78074ee0f685307e
MarconLP <13001502+MarconLP@users.noreply.github.com>
2026-02-16 11:54:38 +0100

    feat(dwh): slack source

--- posthog/temporal/data_imports/sources/slack/source.py
+++ posthog/temporal/data_imports/sources/slack/source.py
@@ -1,19 +1,28 @@
-from typing import cast
+from typing import Optional, cast
 
 from posthog.schema import (
     ExternalDataSourceType as SchemaExternalDataSourceType,
     SourceConfig,
 )
 
+from posthog.temporal.data_imports.pipelines.pipeline.typings import SourceInputs, SourceResponse
 from posthog.temporal.data_imports.sources.common.base import FieldType, SimpleSource
+from posthog.temporal.data_imports.sources.common.mixins import OAuthMixin
 from posthog.temporal.data_imports.sources.common.registry import SourceRegistry
+from posthog.temporal.data_imports.sources.common.schema import SourceSchema
 from posthog.temporal.data_imports.sources.generated_configs import SlackSourceConfig
+from posthog.temporal.data_imports.sources.slack.settings import ENDPOINTS, messages_endpoint_config
+from posthog.temporal.data_imports.sources.slack.slack import (
+    get_channels,
+    slack_source,
+    validate_credentials as validate_slack_credentials,
+)
 
 from products.data_warehouse.backend.types import ExternalDataSourceType
 
 
 @SourceRegistry.register
-class SlackSource(SimpleSource[SlackSourceConfig]):
+class SlackSource(SimpleSource[SlackSourceConfig], OAuthMixin):
     @property
     def source_type(self) -> ExternalDataSourceType:
         return ExternalDataSourceType.SLACK
@@ -22,7 +31,7 @@ class SlackSource(SimpleSource[SlackSourceConfig]):
     def get_source_config(self) -> SourceConfig:
         return SourceConfig(
             name=SchemaExternalDataSourceType.SLACK,
-            label="Slack",
+            caption="Select an existing Slack workspace to link to PostHog or create a new connection",
             iconPath="/static/services/slack.png",
             fields=cast(list[FieldType], []),
             unreleasedSource=True,
