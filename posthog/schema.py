6cfe25224d286a574d243b611dff126a400d6bb6
Jordan Mryyan <jordan.m@posthog.com>
2026-02-16 09:05:35 -0600

    Update schema

--- posthog/schema.py
+++ posthog/schema.py
@@ -1700,6 +1700,7 @@ class FileSystemIconType(StrEnum):
     ERROR_TRACKING = "error_tracking"
     HEATMAP = "heatmap"
     SESSION_REPLAY = "session_replay"
+    SESSION_PROFILE = "session_profile"
     SURVEY = "survey"
     PRODUCT_TOUR = "product_tour"
     USER_INTERVIEW = "user_interview"
