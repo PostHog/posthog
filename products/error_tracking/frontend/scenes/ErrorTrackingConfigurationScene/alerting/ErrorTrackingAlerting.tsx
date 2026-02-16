c9395a2e949a093f05beef7848cd341517372e7d
ablaszkiewicz <aleksander@posthog.com>
2026-02-16 16:53:00 +0100

    feat: wip

--- products/error_tracking/frontend/scenes/ErrorTrackingConfigurationScene/alerting/ErrorTrackingAlerting.tsx
+++ products/error_tracking/frontend/scenes/ErrorTrackingConfigurationScene/alerting/ErrorTrackingAlerting.tsx
@@ -2,19 +2,30 @@ import { useValues } from 'kea'
 
 import { LemonBanner } from '@posthog/lemon-ui'
 
+import { useFeatureFlag } from 'lib/hooks/useFeatureFlag'
 import { LinkedHogFunctions } from 'scenes/hog-functions/list/LinkedHogFunctions'
+import { urls } from 'scenes/urls'
 
 import { spikeDetectionConfigLogic } from '../spike_detection/spikeDetectionConfigLogic'
 
 export function ErrorTrackingAlerting(): JSX.Element {
     const { config, configLoading } = useValues(spikeDetectionConfigLogic)
+    const hasSpikeAlertingFeatureFlag = useFeatureFlag('ERROR_TRACKING_SPIKE_ALERTING')
 
     return (
         <div className="space-y-4">
-            {!configLoading && !config && (
-                <LemonBanner type="warning">
-                    Spike detection is not enabled. Spike alerts will not fire until you enable it in the{' '}
-                    <strong>Spike detection</strong> tab.
+            {!configLoading && !config && hasSpikeAlertingFeatureFlag && (
+                <LemonBanner
+                    type="info"
+                    action={{
+                        children: 'Take me there',
+                        to:
+                            urls.errorTrackingConfiguration() +
+                            '?tab=error-tracking-spike-detection#selectedSetting=error-tracking-spike-detection',
+                    }}
+                >
+                    Spike detection is not enabled. You will not be able to create spike alerts until you enable it in
+                    the <strong>Spike detection</strong> tab.
                 </LemonBanner>
             )}
             <LinkedHogFunctions
@@ -22,7 +33,7 @@ export function ErrorTrackingAlerting(): JSX.Element {
                 subTemplateIds={[
                     'error-tracking-issue-created',
                     'error-tracking-issue-reopened',
-                    'error-tracking-issue-spiking',
+                    ...(config && hasSpikeAlertingFeatureFlag ? (['error-tracking-issue-spiking'] as const) : []),
                 ]}
             />
         </div>
