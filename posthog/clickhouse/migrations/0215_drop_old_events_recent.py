08cdbe16d89ce7d97156056494a2a233ffc9b66e
Daniel Escribano <daniel.e@posthog.com>
2026-02-16 16:55:44 +0100

    chore: align all events_recent table in all environments (#47879)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- posthog/clickhouse/migrations/0215_drop_old_events_recent.py
+++ posthog/clickhouse/migrations/0215_drop_old_events_recent.py
@@ -1,24 +1,23 @@
 from posthog.clickhouse.client.connection import NodeRole
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
-from posthog.models.event.sql import (
-    SHARDED_EVENTS_RECENT_MV_SQL,
-    SHARDED_EVENTS_RECENT_TABLE_SQL,
-    WRITABLE_EVENTS_RECENT_TABLE_SQL,
-)
+from posthog.models.event.sql import SHARDED_EVENTS_RECENT_MV_SQL, WRITABLE_EVENTS_RECENT_TABLE_SQL
 
 operations = [
     run_sql_with_exceptions(
+        "DROP TABLE IF EXISTS kafka_events_recent_json",
+        node_roles=[NodeRole.INGESTION_MEDIUM],
+    ),
+    run_sql_with_exceptions(
         "DROP TABLE IF EXISTS events_recent_json_mv",
-        node_roles=[NodeRole.DATA],
+        node_roles=[NodeRole.INGESTION_MEDIUM],
     ),
     run_sql_with_exceptions(
         "DROP TABLE IF EXISTS writable_events_recent",
-        node_roles=[NodeRole.DATA],
+        node_roles=[NodeRole.INGESTION_MEDIUM],
     ),
-    # Create new sharded data table
     run_sql_with_exceptions(
-        SHARDED_EVENTS_RECENT_TABLE_SQL(),
-        node_roles=[NodeRole.DATA],
+        "DROP TABLE IF EXISTS distributed_sharded_events_recent",
+        node_roles=[NodeRole.DATA, NodeRole.COORDINATOR],
     ),
     # Create distributed write table
     run_sql_with_exceptions(
