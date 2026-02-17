977656f3c5a01cb62fc852b89fb5c1b398743ab4
Alex Lebedev <alex.l@posthog.com>
2026-02-17 19:03:41 +0100

    feat: Include GitHub issues into the registry.

--- posthog/temporal/data_imports/signals/registry.py
+++ posthog/temporal/data_imports/signals/registry.py
@@ -59,9 +59,11 @@ def is_signal_emission_registered(source_type: str, schema_name: str) -> bool:
 
 
 def _register_all_emitters() -> None:
+    from posthog.temporal.data_imports.signals.github_issues import GITHUB_ISSUES_CONFIG
     from posthog.temporal.data_imports.signals.zendesk_tickets import ZENDESK_TICKETS_CONFIG
 
     register_signal_source_table(ExternalDataSourceType.ZENDESK, "tickets", ZENDESK_TICKETS_CONFIG)
+    register_signal_source_table(ExternalDataSourceType.GITHUB, "issues", GITHUB_ISSUES_CONFIG)
 
 
 _register_all_emitters()
