e915044516f836ab05637468e44a00210fcc1986
Rodrigo Iloro <rodrigo@posthog.com>
2026-02-17 01:03:10 -0300

    feat(experiments): add experiment templates skeleton.

---
+++ frontend/src/scenes/experiments/ExperimentForm/ExperimentTemplates/ExperimentTemplates.tsx
@@ -0,0 +1,12 @@
+export const ExperimentTemplates = (): JSX.Element => {
+    return (
+        <div className="space-y-4">
+            <div>
+                <h3 className="font-semibold text-base mb-1">Start with a template</h3>
+                <p className="text-sm text-muted">
+                    Choose a pre-configured experiment template to quickly set up your metrics
+                </p>
+            </div>
+        </div>
+    )
+}
