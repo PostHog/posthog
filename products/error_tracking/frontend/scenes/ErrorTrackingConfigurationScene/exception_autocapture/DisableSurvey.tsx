422366b39b9ea1c6b9aa423255959c660b429568
ablaszkiewicz <aleksander@posthog.com>
2026-02-18 22:29:48 +0100

    feat: copy

--- products/error_tracking/frontend/scenes/ErrorTrackingConfigurationScene/exception_autocapture/DisableSurvey.tsx
+++ products/error_tracking/frontend/scenes/ErrorTrackingConfigurationScene/exception_autocapture/DisableSurvey.tsx
@@ -15,7 +15,7 @@ export function DisableSurvey(): JSX.Element | null {
     return (
         <div className="mt-4 max-w-lg border rounded-lg p-4 bg-bg-light">
             {submitted ? (
-                <p className="font-medium text-success m-0">Thanks for your feedback!</p>
+                <p className="font-medium m-0">Thanks for your feedback!</p>
             ) : (
                 <div className="flex flex-col gap-3">
                     <p className="font-medium m-0">
