5f865d0d11364843ac8249e3127c8e4098ebe275
Michael Matloka <michael@matloka.com>
2026-02-18 19:18:22 +0100

    Fix API routes

--- products/signals/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_extensions.routers import ExtendedDefaultRouter
-
-from .views import SignalReportViewSet, SignalSourceConfigViewSet
-
-
-def register_signal_report_routes(router: ExtendedDefaultRouter):
-    """Register signal report routes on the given router."""
-    router.register(r"signal_reports", SignalReportViewSet, basename="signal_reports")
-    router.register(r"signal_source_configs", SignalSourceConfigViewSet, basename="signal_source_configs")
