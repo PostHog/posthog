56b9e0ea387aaabb8b82d09c8476e14c4c5a8769
Reece Jones <reece@posthog.com>
2026-02-18 11:13:24 -0500

    fix(settings): apply correct access control rules in session replay ingestion controls

--- frontend/src/lib/components/IngestionControls/triggers/Sampling.tsx
+++ frontend/src/lib/components/IngestionControls/triggers/Sampling.tsx
@@ -1,13 +1,9 @@
-import { useValues } from 'kea'
 import { useState } from 'react'
 
 import { LemonButton, LemonInput, lemonToast } from '@posthog/lemon-ui'
 
-import { AccessControlAction } from 'lib/components/AccessControlAction'
-
-import { AccessControlLevel } from '~/types'
-
-import { ingestionControlsLogic } from '../ingestionControlsLogic'
+import { RestrictionScope, useRestrictedArea } from 'lib/components/RestrictedArea'
+import { TeamMembershipLevel } from 'lib/constants'
 
 export function SamplingTrigger({
     initialSampleRate,
@@ -16,9 +12,11 @@ export function SamplingTrigger({
     initialSampleRate: number
     onChange: (value: number) => void
 }): JSX.Element {
-    const { resourceType } = useValues(ingestionControlsLogic)
-
     const [value, setValue] = useState<number | undefined>(initialSampleRate)
+    const restrictedReason = useRestrictedArea({
+        scope: RestrictionScope.Project,
+        minimumAccessLevel: TeamMembershipLevel.Admin,
+    })
 
     const updateSampling = (): void => {
         const returnRate = typeof value == 'number' ? value / 100 : 0
@@ -30,28 +28,29 @@ export function SamplingTrigger({
     }
 
     return (
-        <AccessControlAction resourceType={resourceType} minAccessLevel={AccessControlLevel.Editor}>
-            <div className="flex flex-row gap-x-2">
-                <LemonInput
-                    type="number"
-                    className="[&>input::-webkit-inner-spin-button]:appearance-none"
-                    onChange={(value) => setValue(value)}
-                    min={0}
-                    max={100}
-                    suffix={<>%</>}
-                    value={value}
-                    onPressEnter={updateSampling}
-                    data-attr="sampling-setting-input"
-                />
-                <LemonButton
-                    type="primary"
-                    disabledReason={initialSampleRate === value && 'Update the sample rate to save changes'}
-                    onClick={updateSampling}
-                    data-attr="sampling-setting-update"
-                >
-                    Update
-                </LemonButton>
-            </div>
-        </AccessControlAction>
+        <div className="flex flex-row gap-x-2">
+            <LemonInput
+                type="number"
+                className="[&>input::-webkit-inner-spin-button]:appearance-none"
+                onChange={(value) => setValue(value)}
+                min={0}
+                max={100}
+                suffix={<>%</>}
+                value={value}
+                onPressEnter={updateSampling}
+                data-attr="sampling-setting-input"
+                disabledReason={restrictedReason}
+            />
+            <LemonButton
+                type="primary"
+                disabledReason={
+                    initialSampleRate === value ? 'Update the sample rate to save changes' : restrictedReason
+                }
+                onClick={updateSampling}
+                data-attr="sampling-setting-update"
+            >
+                Update
+            </LemonButton>
+        </div>
     )
 }
