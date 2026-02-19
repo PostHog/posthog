9f4a2b3da6ab7d69debf785f299d2ebaa7a55081
Patricio <patricio@posthog.com>
2026-02-19 11:12:14 -0300

    undo last commit

--- posthog/migrations/1015_fix_legacy_feature_flag_filters.py
+++ posthog/migrations/1015_fix_legacy_feature_flag_filters.py
@@ -19,8 +19,9 @@ def fix_legacy_feature_flag_filters(apps, schema_editor):
     """
     FeatureFlag = apps.get_model("posthog", "FeatureFlag")
     total = 0
+    queryset = FeatureFlag.objects.filter(Q(filters__isnull=True) | ~Q(filters__has_key="groups"))
+
     while True:
-        # Re-evaluate queryset each iteration to exclude already-updated flags
         flags = list(queryset[:BATCH_SIZE])
         if not flags:
             break
