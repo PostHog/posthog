74a4b21c9eb8983e60b53ad9307a9b82e7901936
Rafa Audibert <rafaeelaudibert@gmail.com>
2026-02-19 15:18:34 -0300

    feat: Add analytics

--- frontend/src/lib/utils/eventUsageLogic.ts
+++ frontend/src/lib/utils/eventUsageLogic.ts
@@ -728,6 +728,8 @@ export const eventUsageLogic = kea<eventUsageLogicType>([
             selected,
             recommendationSource,
         }),
+        reportOnboardingReverseProxyDomainEntered: (domain: string) => ({ domain }),
+        reportOnboardingReverseProxyDocsClicked: true,
         reportBillingCTAShown: true,
         reportBillingUsageInteraction: (properties: BillingUsageInteractionProps) => ({ properties }),
         reportBillingSpendInteraction: (properties: BillingUsageInteractionProps) => ({ properties }),
@@ -1840,6 +1842,12 @@ export const eventUsageLogic = kea<eventUsageLogicType>([
             posthog.capture('sdk selected', {
                 sdk: sdk.key,
             })
+        },
+        reportOnboardingReverseProxyDomainEntered: ({ domain }) => {
+            posthog.capture('onboarding reverse proxy domain entered', { domain })
+        },
+        reportOnboardingReverseProxyDocsClicked: () => {
+            posthog.capture('onboarding reverse proxy docs clicked')
         },
         // command bar
         reportCommandBarStatusChanged: ({ status }) => {
