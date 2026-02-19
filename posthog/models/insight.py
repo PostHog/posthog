dbe26948f95bd4f1ba39da4fc8bf63d9312a19c8
Vasco de Krijger <vasco@posthog.com>
2026-02-19 11:46:38 +0100

    chore: Remove check further down

--- posthog/models/insight.py
+++ posthog/models/insight.py
@@ -309,14 +309,13 @@ class Insight(RootTeamMixin, FileSystemSyncMixin, models.Model):
     def are_alerts_supported(self) -> bool:
         from posthog.schema_migrations.upgrade_manager import upgrade_query
 
-        if self.query is None:
-            return False
-
         with upgrade_query(self):
             query = self.query
+            if query is None:
+                return False
             while query.get("source"):
                 query = query["source"]
-            if query is None or query.get("kind") != "TrendsQuery":
+            if query.get("kind") != "TrendsQuery":
                 return False
         return True
 
