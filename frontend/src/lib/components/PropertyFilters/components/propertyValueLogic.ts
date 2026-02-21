af99b044fd0683ba14274791b54f0e1a955be0f0
Raquel Smith <raquelmsmith@users.noreply.github.com>
2026-02-21 09:10:09 -0800

    fix jest test

--- frontend/src/lib/components/PropertyFilters/components/propertyValueLogic.ts
+++ frontend/src/lib/components/PropertyFilters/components/propertyValueLogic.ts
@@ -16,7 +16,7 @@ export const propertyValueLogic = kea<propertyValueLogicType>([
     path(['lib', 'components', 'PropertyFilters', 'propertyValueLogic']),
     props({} as PropertyValueLogicProps),
     key((props) => `${props.type}/${props.propertyKey}`),
-    connect({ values: [propertyDefinitionsModel, ['options']] }),
+    connect(() => ({ values: [propertyDefinitionsModel, ['options']] })),
     selectors(({ props }) => ({
         propertyOption: [(s) => [s.options], (options) => options[props.propertyKey]],
         /** Whether a background cache refresh is currently in progress for this property. */
