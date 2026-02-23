199dd9c0c8d665e5a7a7d1cc5f2a600e5f3becd6
Marius Andra <marius.andra@gmail.com>
2026-02-23 12:54:02 +0100

    q

--- playwright/e2e/sql-editor.spec.ts
+++ playwright/e2e/sql-editor.spec.ts
@@ -8,7 +8,7 @@ test.describe('SQL Editor', () => {
     test('See SQL Editor', async ({ page }) => {
         await expect(page.locator('[data-attr=editor-scene]')).toBeVisible()
         await expect(page.locator('[data-attr=sql-editor-source-empty-state]')).toBeVisible()
-        await expect(page.getByText('SQL query')).toBeVisible()
+        await expect(page.locator('[data-attr=editor-scene]').getByText('SQL query', { exact: true })).toBeVisible()
     })
 
     test('Add source link', async ({ page }) => {
