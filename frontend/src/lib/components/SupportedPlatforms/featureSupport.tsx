45eebe4f7c5c20e721abd35cc67b907f6ef87675
Manoel Aranda Neto <marandaneto@gmail.com>
2026-02-24 11:42:34 +0100

    chore: update remote config versioning for mobile sdks

--- frontend/src/lib/components/SupportedPlatforms/featureSupport.tsx
+++ frontend/src/lib/components/SupportedPlatforms/featureSupport.tsx
@@ -6,12 +6,13 @@ export const FEATURE_SUPPORT: Record<Feature, PlatformSupportConfig> = {
     errorTrackingExceptionAutocapture: {
         web: { version: '1.207.8', note: 'Not supported by the js-lite package' },
         android: {
+            version: '3.24.0',
             note: (
                 <>
                     <Link to="https://posthog.com/docs/error-tracking/installation/android#set-up-exception-autocapture">
                         Exception autocapture is supported on Android
                     </Link>{' '}
-                    but is not controlled remotely by this toggle
+                    and can be controlled remotely using this toggle when running SDK version 3.32.0 or higher.
                 </>
             ),
         },
@@ -26,12 +27,13 @@ export const FEATURE_SUPPORT: Record<Feature, PlatformSupportConfig> = {
             ),
         },
         reactNative: {
+            version: '4.14.0',
             note: (
                 <>
                     <Link to="https://posthog.com/docs/error-tracking/installation/react-native#set-up-exception-autocapture">
                         Exception autocapture is supported on React Native
                     </Link>{' '}
-                    but is not controlled remotely by this toggle
+                    and can be controlled remotely using this toggle when running SDK version 4.35.0 or higher.
                 </>
             ),
         },
@@ -40,12 +42,29 @@ export const FEATURE_SUPPORT: Record<Feature, PlatformSupportConfig> = {
         web: { version: '1.249.4' },
     },
     sessionReplayLogCapture: {
-        android: { version: '1.0.0' },
+        android: {
+            version: '3.4.0',
+            note: (
+                <>
+                    <Link to="https://posthog.com/docs/session-replay/console-log-recording?tab=Android">
+                        Console log recording is supported on Android
+                    </Link>{' '}
+                    and can be controlled remotely using this toggle when running SDK version 3.32.0 or higher.
+                </>
+            ),
+        },
         ios: { version: '3.26.0' },
         web: { version: '1.18.0' },
         reactNative: {
             version: '3.9.0',
-            note: <>Android only</>,
+            note: (
+                <>
+                    <Link to="https://posthog.com/docs/session-replay/console-log-recording?tab=React+Native">
+                        Console log recording is supported on Android only
+                    </Link>{' '}
+                    and can be controlled remotely using this toggle when running SDK version 4.35.0 or higher.
+                </>
+            ),
         },
     },
     sessionReplayCanvasCapture: {
@@ -61,10 +80,30 @@ export const FEATURE_SUPPORT: Record<Feature, PlatformSupportConfig> = {
         web: { version: '1.101.0' },
     },
     sessionReplayCaptureRequests: {
-        android: { version: '3.1.0' },
+        android: {
+            version: '3.4.0',
+            note: (
+                <>
+                    <Link to="https://posthog.com/docs/session-replay/network-recording?tab=Android">
+                        Network recording is supported on Android
+                    </Link>{' '}
+                    and can be controlled remotely using this toggle when running SDK version 3.32.0 or higher.
+                </>
+            ),
+        },
         ios: { version: '3.12.6' },
         web: { version: '1.39.0' },
-        reactNative: { note: <>RN network capture is only supported on iOS</> },
+        reactNative: {
+            version: '3.2.0',
+            note: (
+                <>
+                    <Link to="https://posthog.com/docs/session-replay/network-recording?tab=iOS">
+                        Network recording is supported on iOS only
+                    </Link>{' '}
+                    and can be controlled remotely using this toggle when running SDK version 4.35.0 or higher.
+                </>
+            ),
+        },
     },
     sessionReplayCaptureHeadersAndPayloads: {
         web: { version: '1.104.4' },
@@ -92,8 +131,8 @@ export const FEATURE_SUPPORT: Record<Feature, PlatformSupportConfig> = {
     surveys: {
         web: { version: '1.81.1' },
         android: { note: 'via API' },
-        ios: { note: 'via API' },
-        reactNative: { note: 'via API' },
+        ios: { version: '3.31.0' },
+        reactNative: { version: '3.12.0' },
     },
     logsCapture: {
         web: { version: '1.329.0' },
