c206ef23161aa9a516a1b5f472ef7c9ac9a9c280
Rodrigo Iloro <rodrigo@posthog.com>
2026-02-24 18:27:23 -0300

    fix(experiments): fix default state.

--- frontend/src/scenes/experiments/ExperimentView/ExposureCriteria.tsx
+++ frontend/src/scenes/experiments/ExperimentView/ExposureCriteria.tsx
@@ -77,6 +77,7 @@ export function ExposureCriteriaModal({ onSave }: ExposureCriteriaModalProps): J
                     selected={!exposureCriteria?.exposure_config}
                     onClick={() => {
                         setExposureCriteria({
+                            ...exposureCriteria,
                             exposure_config: undefined,
                         })
                     }}
@@ -93,6 +94,7 @@ export function ExposureCriteriaModal({ onSave }: ExposureCriteriaModalProps): J
                     selected={!!exposureCriteria?.exposure_config}
                     onClick={() => {
                         setExposureCriteria({
+                            ...exposureCriteria,
                             exposure_config: {
                                 kind: NodeKind.ExperimentEventExposureConfig,
                                 event: '$feature_flag_called',
