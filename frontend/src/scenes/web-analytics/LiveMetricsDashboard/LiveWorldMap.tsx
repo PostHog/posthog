692c8a5f03af62e68ec00ba910425c904f999f11
Rafa Audibert <rafaeelaudibert@gmail.com>
2026-02-23 22:11:38 -0300

    fix: Format styles properly

--- frontend/src/scenes/web-analytics/LiveMetricsDashboard/LiveWorldMap.tsx
+++ frontend/src/scenes/web-analytics/LiveMetricsDashboard/LiveWorldMap.tsx
@@ -1,4 +1,4 @@
-import 'scenes/insights/views/WorldMap/WorldMap.scss'
+import '~/scenes/insights/views/WorldMap/WorldMap.scss'
 
 import { useActions, useValues } from 'kea'
 import React, { useEffect } from 'react'
