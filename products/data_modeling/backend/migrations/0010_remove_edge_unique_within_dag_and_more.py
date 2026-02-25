fa461bafea7e8507676f44f0bbccc11e8cf5e18a
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2026-02-25 13:44:53 -0600

    fix tests

--- products/data_modeling/backend/migrations/0010_remove_edge_unique_within_dag_and_more.py
+++ products/data_modeling/backend/migrations/0010_remove_edge_unique_within_dag_and_more.py
@@ -21,11 +21,18 @@ class Migration(migrations.Migration):
             model_name="node",
             name="name_unique_within_team_dag_for_tables",
         ),
+        # db column is made nullable and given a default to avoid failures on insert
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.RemoveField(
                     model_name="edge",
                     name="dag_id",
+                ),
+            ],
+            database_operations=[
+                migrations.RunSQL(
+                    sql='ALTER TABLE "posthog_datamodelingedge" ALTER COLUMN "dag_id" SET DEFAULT \'posthog\', ALTER COLUMN "dag_id" DROP NOT NULL;',
+                    reverse_sql=migrations.RunSQL.noop,
                 ),
             ],
         ),
@@ -34,6 +41,12 @@ class Migration(migrations.Migration):
                 migrations.RemoveField(
                     model_name="node",
                     name="dag_id",
+                ),
+            ],
+            database_operations=[
+                migrations.RunSQL(
+                    sql='ALTER TABLE "posthog_datamodelingnode" ALTER COLUMN "dag_id" SET DEFAULT \'posthog\', ALTER COLUMN "dag_id" DROP NOT NULL;',
+                    reverse_sql=migrations.RunSQL.noop,
                 ),
             ],
         ),
