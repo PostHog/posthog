e00b20fac34c8b1fe6a691ba816fe3666a9c399a
Adam Leith <adamleithp@users.noreply.github.com>
2026-02-25 09:44:52 +0000

    fix: e2e fails with correct test

--- playwright/e2e/system-status.spec.ts
+++ playwright/e2e/system-status.spec.ts
@@ -2,7 +2,9 @@ import { expect, test } from '../utils/playwright-test-base'
 
 test.describe('System Status', () => {
     test('System Status loaded', async ({ page }) => {
-        await page.goto('/instance/status')
+        await page.locator('[data-attr=help-menu-button]').click()
+        await page.locator('[data-attr=help-menu-admin-button]').click()
+        await page.locator('[data-attr=help-menu-instance-panel-button]').click()
         await expect(page.locator('table')).toHaveText(/Events in ClickHouse/)
     })
 })
