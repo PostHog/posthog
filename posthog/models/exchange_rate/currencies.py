43e85571682f5adaccdffebfb32275c90d6e3081
Andrew Maguire <andrewm4894@gmail.com>
2026-02-27 10:41:22 +0000

    fix(llma): update CurrencyCode.TRY_ to CurrencyCode.TRY in currencies.py
    Schema regeneration changed TRY_ to TRY but currencies.py wasn't updated to match.

--- posthog/models/exchange_rate/currencies.py
+++ posthog/models/exchange_rate/currencies.py
@@ -144,7 +144,7 @@ SUPPORTED_CURRENCIES: dict[CurrencyCode, str] = {
     CurrencyCode.TMT: "Turkmenistani Manat",
     CurrencyCode.TND: "Tunisian Dinar",
     CurrencyCode.TOP: "Tongan Pa'anga",
-    CurrencyCode.TRY_: "Turkish Lira",  # Named `TRY_` in Python because `TRY` is a reserved word
+    CurrencyCode.TRY: "Turkish Lira",
     CurrencyCode.TTD: "Trinidad and Tobago Dollar",
     CurrencyCode.TWD: "New Taiwan Dollar",
     CurrencyCode.TZS: "Tanzanian Shilling",
