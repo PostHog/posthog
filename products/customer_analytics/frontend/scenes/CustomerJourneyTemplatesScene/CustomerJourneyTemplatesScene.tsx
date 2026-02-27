aab3f27724afe0788a694a8a4cb0e10d346a76c8
Arthur Moreira de Deus <arthur@posthog.com>
2026-02-27 14:02:00 -0300

    feat(customer-journeys): Create journeys from templates

---
+++ products/customer_analytics/frontend/scenes/CustomerJourneyTemplatesScene/CustomerJourneyTemplatesScene.tsx
@@ -0,0 +1,29 @@
+import { LemonButton } from '@posthog/lemon-ui'
+
+import { urls } from 'scenes/urls'
+
+import { SceneContent } from '~/layout/scenes/components/SceneContent'
+import { ProductKey } from '~/queries/schema/schema-general'
+import { SceneExport } from '~/scenes/sceneTypes'
+
+import { JourneyTemplatePicker } from './JourneyTemplatePicker'
+import { journeyTemplatePickerLogic } from './journeyTemplatePickerLogic'
+
+export const scene: SceneExport = {
+    component: CustomerJourneyTemplatesScene,
+    logic: journeyTemplatePickerLogic,
+    productKey: ProductKey.CUSTOMER_ANALYTICS,
+}
+
+export function CustomerJourneyTemplatesScene(): JSX.Element {
+    return (
+        <SceneContent>
+            <div className="flex items-center justify-start mb-4">
+                <LemonButton type="tertiary" size="small" to={urls.customerAnalyticsJourneys()}>
+                    ← Back to journeys
+                </LemonButton>
+            </div>
+            <JourneyTemplatePicker />
+        </SceneContent>
+    )
+}
