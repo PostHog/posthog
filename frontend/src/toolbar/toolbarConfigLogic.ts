782ab2e2f1bf485371338d9dc5debf53a57dd10a
Fernando Gomes <fcgomes@Fernandos-MacBook-Pro.local>
2026-02-27 11:45:29 -0500

    deauth users

--- frontend/src/toolbar/toolbarConfigLogic.ts
+++ frontend/src/toolbar/toolbarConfigLogic.ts
@@ -186,6 +186,11 @@ export const toolbarConfigLogic = kea<toolbarConfigLogicType>([
             }
         }
 
+        // Migrate users from the old temporaryToken flow to OAuth.
+        if (!values.accessToken && props.temporaryToken) {
+            actions.tokenExpired()
+        }
+
         if (props.instrument) {
             const distinctId = props.distinctId
 
