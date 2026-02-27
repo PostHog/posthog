b29327cc71324f8bd70e81720bfb22d58d9fa16f
Georgis Andonis <georgis@posthog.com>
2026-02-27 13:19:21 +0200

    Use null for initial insight name

--- frontend/src/scenes/insights/insightLogic.test.ts
+++ frontend/src/scenes/insights/insightLogic.test.ts
@@ -456,7 +456,7 @@ describe('insightLogic', () => {
         insightDataLogic(insightProps).mount()
 
         const expectedPartialInsight = {
-            name: undefined,
+            name: null,
             description: '',
             tags: [],
             query: partial({
@@ -480,7 +480,7 @@ describe('insightLogic', () => {
             logic.actions.setInsightMetadataLocal({ name: 'Foobar 43', description: 'Lorem ipsum.', tags: ['good'] })
         }).toMatchValues({
             insight: partial({ name: 'Foobar 43', description: 'Lorem ipsum.', tags: ['good'] }),
-            savedInsight: partial({ name: undefined, description: '', tags: [] }),
+            savedInsight: partial({ name: null, description: '', tags: [] }),
             insightChanged: true,
         })
 
@@ -781,7 +781,7 @@ describe('insightLogic', () => {
             )
 
             await expectLogic(logic).toMatchValues({
-                insight: partial({ name: undefined }),
+                insight: partial({ name: null }),
             })
         })
     })
