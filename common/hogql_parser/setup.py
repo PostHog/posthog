a43131d0766fe45c398001108a79ee1976908f36
George Munyoro <george@munyoro.dev>
2026-02-27 09:15:57 +0200

    chore(hogql): bump hogql lib versions

--- common/hogql_parser/setup.py
+++ common/hogql_parser/setup.py
@@ -36,7 +36,7 @@ module = Extension(
 
 setup(
     name="hogql_parser",
-    version="1.3.17",
+    version="1.3.18",
     url="https://github.com/PostHog/posthog/tree/master/common/hogql_parser",
     description="HogQL parser for internal PostHog use",
     author="PostHog Inc.",
