b6fe84def5a57086a5272604c7b6cf47954d7681
George Munyoro <george@munyoro.dev>
2026-02-27 03:19:48 +0200

    fix(hogql): add missing 'next_join' and 'alias' fields in JSON representation of JoinExpr

--- common/hogql_parser/parser_json.cpp
+++ common/hogql_parser/parser_json.cpp
@@ -1937,6 +1937,8 @@ class HogQLParseTreeJSONConverter : public HogQLParserBaseVisitor {
     if (!is_internal) addPositionInfo(json, ctx);
     json["table"] = std::move(table_json);
     json["table_args"] = std::move(table_args_json);
+    json["next_join"] = nullptr;
+    json["alias"] = nullptr;
     return json;
   }
 
