3947152903f5096295bd6e81ab66a7e399984399
dylan <dylan@posthog.com>
2026-02-27 15:56:00 -0800

    fix(flags): rename blast radius label from "users" to "profiles"

--- frontend/src/scenes/feature-flags/featureFlagReleaseConditionsLogic.ts
+++ frontend/src/scenes/feature-flags/featureFlagReleaseConditionsLogic.ts
@@ -515,7 +515,7 @@ export const featureFlagReleaseConditionsLogic = kea<featureFlagReleaseCondition
                 if (filters.aggregation_group_type_index != null) {
                     return aggregationLabel(filters.aggregation_group_type_index).plural
                 }
-                return 'users'
+                return 'profiles'
             },
         ],
         filtersTaxonomicOptions: [
