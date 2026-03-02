b2e881777254f8cb9518e52970e5c72ddd3e8128
Joshua Snyder <joshua@posthog.com>
2026-03-02 10:57:13 +0000

    add related name

--- products/tasks/backend/migrations/0021_task_signal_report_alter_task_origin_product.py
+++ products/tasks/backend/migrations/0021_task_signal_report_alter_task_origin_product.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
                 blank=True,
                 null=True,
                 on_delete=django.db.models.deletion.SET_NULL,
+                related_name="task",
                 to="signals.signalreport",
             ),
         ),
