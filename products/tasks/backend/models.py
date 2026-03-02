7b8045527e70d707e49a0ecb12d01fe14357330b
Joshua Snyder <joshua@posthog.com>
2026-03-02 10:55:30 +0000

    clarify comment

--- products/tasks/backend/models.py
+++ products/tasks/backend/models.py
@@ -42,6 +42,8 @@ class Task(DeletedMetaFields, models.Model):
         SLACK = "slack", "Slack"
         SUPPORT_QUEUE = "support_queue", "Support Queue"
         SESSION_SUMMARIES = "session_summaries", "Session Summaries"
+        # Unlike the others (which indicate direct creation from that product, e.g. a "fix this error" button),
+        # signal report tasks originate indirectly via signals from other products.
         SIGNAL_REPORT = "signal_report", "Signal Report"
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
