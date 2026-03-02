fc9c1316525d55d6d68fdd7f7291dd30af6677d3
Anirudh Pillai <anirudhx5@gmail.com>
2026-03-02 14:31:31 +0000

    fix(insights-home): fix insight overflow (#49523)

--- frontend/src/scenes/saved-insights/SavedInsights.tsx
+++ frontend/src/scenes/saved-insights/SavedInsights.tsx
@@ -49,6 +49,7 @@ import { LemonDivider } from 'lib/lemon-ui/LemonDivider'
 import { LemonTable, LemonTableColumns } from 'lib/lemon-ui/LemonTable'
 import { LemonTableLink } from 'lib/lemon-ui/LemonTable/LemonTableLink'
 import { LemonTabs } from 'lib/lemon-ui/LemonTabs'
+import { LemonTag } from 'lib/lemon-ui/LemonTag/LemonTag'
 import { ProfilePicture } from 'lib/lemon-ui/ProfilePicture'
 import { isNonEmptyObject } from 'lib/utils'
 import { cn } from 'lib/utils/css-classes'
@@ -898,7 +899,21 @@ export function SavedInsights(): JSX.Element {
                 activeKey={tab}
                 onChange={(tab) => setSavedInsightsFilters({ tab })}
                 tabs={[
-                    ...(showHomeTab ? [{ key: SavedInsightsTabs.Home, label: 'Home' }] : []),
+                    ...(showHomeTab
+                        ? [
+                              {
+                                  key: SavedInsightsTabs.Home,
+                                  label: (
+                                      <div className="flex items-center gap-2">
+                                          Home
+                                          <LemonTag type="warning" size="small">
+                                              BETA
+                                          </LemonTag>
+                                      </div>
+                                  ),
+                              },
+                          ]
+                        : []),
                     { key: SavedInsightsTabs.All, label: 'Insights' },
                     {
                         key: SavedInsightsTabs.Alerts,
