06b8c05eab877099a87554e47d8214863b6de371
Thomas Obermüller <thomas.obermueller@gmail.com>
2026-03-04 19:27:44 +0100

    add preview

--- frontend/src/scenes/insights/EditorFilters/FunnelDataWarehouseStepDefinitionPopover.tsx
+++ frontend/src/scenes/insights/EditorFilters/FunnelDataWarehouseStepDefinitionPopover.tsx
@@ -1,6 +1,9 @@
+import { DatabaseTablePreview } from 'lib/components/TablePreview/DatabaseTablePreview'
 import { DefinitionPopoverRendererProps, TaxonomicFilterGroupType } from 'lib/components/TaxonomicFilter/types'
+import { DataWarehouseTableForInsight } from 'scenes/data-warehouse/types'
 
 export function FunnelDataWarehouseStepDefinitionPopover({
+    item,
     group,
     defaultView,
 }: DefinitionPopoverRendererProps): JSX.Element | null {
@@ -8,5 +11,12 @@ export function FunnelDataWarehouseStepDefinitionPopover({
         return defaultView
     }
 
-    return <div>DataWarehouseFunnelStepDefinitionPopover</div>
+    const table = item as DataWarehouseTableForInsight
+
+    return (
+        <div className="flex flex-col gap-3">
+            <DatabaseTablePreview table={table} emptyMessage="No table selected" limit={5} />
+            {defaultView}
+        </div>
+    )
 }
