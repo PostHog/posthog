7e06d1e6955130005da2fb17b9b82156164e5e0c
Marius Andra <marius.andra@gmail.com>
2026-03-05 15:57:03 +0100

    m

--- products/data_warehouse/backend/migrations/0025_datawarehousetable_options.py
+++ products/data_warehouse/backend/migrations/0025_datawarehousetable_options.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("data_warehouse", "0025_externaldatasource_access_method"),
+        ("data_warehouse", "0024_alter_datamodelingjob_status"),
     ]
 
     operations = [
