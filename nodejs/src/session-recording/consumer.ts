7149f46f3d88e8849979eab61800f84a24943fb6
Tue Haulund <tue.haulund@gmail.com>
2026-03-05 14:38:06 +0100

    chore(replay): remove VerifyingEncryptor crypto integrity checks

--- nodejs/src/session-recording/consumer.ts
+++ nodejs/src/session-recording/consumer.ts
@@ -15,8 +15,7 @@ import {
 import { TopHog } from '../ingestion/tophog/tophog'
 import { KafkaConsumer } from '../kafka/consumer'
 import { KafkaProducerWrapper } from '../kafka/producer'
-import { getBlockDecryptor, getBlockEncryptor } from '../session-replay/shared/crypto'
-import { VerifyingEncryptor } from '../session-replay/shared/crypto/verifying-encryptor'
+import { getBlockEncryptor } from '../session-replay/shared/crypto'
 import { getKeyStore } from '../session-replay/shared/keystore'
 import { MemoryCachedKeyStore } from '../session-replay/shared/keystore/cache'
 import { SessionMetadataStore } from '../session-replay/shared/metadata/session-metadata-store'
@@ -237,13 +236,7 @@ export class SessionRecordingIngester {
             dynamoDBEndpoint: config.SESSION_RECORDING_DYNAMODB_ENDPOINT,
         })
         this.keyStore = new MemoryCachedKeyStore(keyStore)
-        const encryptor = getBlockEncryptor(this.keyStore)
-        const decryptor = getBlockDecryptor(this.keyStore)
-        this.encryptor = new VerifyingEncryptor(
-            encryptor,
-            decryptor,
-            config.SESSION_RECORDING_CRYPTO_INTEGRITY_CHECK_RATE
-        )
+        this.encryptor = getBlockEncryptor(this.keyStore)
 
         this.sessionBatchManager = new SessionBatchManager({
             maxBatchSizeBytes: this.config.SESSION_RECORDING_MAX_BATCH_SIZE_KB * 1024,
