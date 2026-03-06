697f4df420ed9df168601eb19f65ca24f8632706
Thomas Obermüller <thomas.obermueller@gmail.com>
2026-03-06 09:36:38 +0100

    adapt tooltips to aggregation target

--- frontend/src/queries/nodes/InsightViz/LifecycleToggles.tsx
+++ frontend/src/queries/nodes/InsightViz/LifecycleToggles.tsx
@@ -2,46 +2,71 @@ import { useActions, useValues } from 'kea'
 
 import { LemonCheckbox, LemonLabel } from '@posthog/lemon-ui'
 
+import { capitalizeFirstLetter } from 'lib/utils'
 import { insightVizDataLogic } from 'scenes/insights/insightVizDataLogic'
 
+import { groupsModel, Noun } from '~/models/groupsModel'
 import { LifecycleFilter } from '~/queries/schema/schema-general'
+import { isLifecycleQuery } from '~/queries/utils'
 import { EditorFilterProps, LifecycleToggle } from '~/types'
 
-// Tooltip explanations taken from https://posthog.com/docs/product-analytics/lifecycle#interpreting-your-lifecycle
-const lifecycles: { name: LifecycleToggle; tooltip: string; color: string }[] = [
+const lifecycles: { name: LifecycleToggle; color: string }[] = [
     {
         name: 'new',
-        tooltip:
-            'Users who did the event or action during the interval and were also created during that period – e.g. created an account and sent a message today.',
         color: 'var(--color-lifecycle-new)',
     },
     {
         name: 'returning',
-        tooltip:
-            'Someone who was active in the previous interval and is also active in the current interval – e.g. sent a message yesterday and also sent a message today.',
         color: 'var(--color-lifecycle-returning)',
     },
     {
         name: 'resurrecting',
-        tooltip:
-            'Someone who was not active in the previous interval but became active once again – e.g. did not send any messages for 10 days, but sent one today.',
         color: 'var(--color-lifecycle-resurrecting)',
     },
     {
         name: 'dormant',
-        tooltip:
-            'Users who are not active in the current interval, but were active in the previous interval – e.g. someone who has not sent a message today, but sent one yesterday.',
         color: 'var(--color-lifecycle-dormant)',
     },
 ]
 
 const DEFAULT_LIFECYCLE_TOGGLES: LifecycleToggle[] = ['new', 'returning', 'resurrecting', 'dormant']
 
+export function getLifecycleTooltip(
+    lifecycle: LifecycleToggle,
+    aggregationTargetLabel: Noun,
+    aggregationGroupTypeIndex?: number | null
+): string {
+    const subjectPronoun = aggregationGroupTypeIndex != null ? 'that' : 'who'
+
+    switch (lifecycle) {
+        case 'new':
+            return `${capitalizeFirstLetter(
+                aggregationTargetLabel.plural
+            )} ${subjectPronoun} did the event or action during the interval and were also created during that period, e.g. created an account and sent a message today.`
+        case 'returning':
+            return `${capitalizeFirstLetter(
+                aggregationTargetLabel.singular
+            )} ${subjectPronoun} was active in the previous interval and is also active in the current interval, e.g. sent a message yesterday and also sent a message today.`
+        case 'resurrecting':
+            return `${capitalizeFirstLetter(
+                aggregationTargetLabel.singular
+            )} ${subjectPronoun} was not active in the previous interval but became active once again, e.g. did not send any messages for 10 days, but sent one today.`
+        case 'dormant':
+            return `${capitalizeFirstLetter(
+                aggregationTargetLabel.plural
+            )} ${subjectPronoun} are not active in the current interval, but were active in the previous interval, e.g. did not send a message today, but sent one yesterday.`
+    }
+}
+
 export function LifecycleToggles({ insightProps }: EditorFilterProps): JSX.Element {
-    const { insightFilter } = useValues(insightVizDataLogic(insightProps))
+    const { insightFilter, querySource } = useValues(insightVizDataLogic(insightProps))
     const { updateInsightFilter } = useActions(insightVizDataLogic(insightProps))
+    const { aggregationLabel } = useValues(groupsModel)
 
     const toggledLifecycles = (insightFilter as LifecycleFilter)?.toggledLifecycles || DEFAULT_LIFECYCLE_TOGGLES
+    const aggregationGroupTypeIndex = isLifecycleQuery(querySource) ? querySource.aggregation_group_type_index : undefined
+    const aggregationTargetLabel = aggregationLabel(aggregationGroupTypeIndex)
+
     const toggleLifecycle = (name: LifecycleToggle): void => {
         if (toggledLifecycles.includes(name)) {
             updateInsightFilter({ toggledLifecycles: toggledLifecycles.filter((n) => n !== name) })
@@ -53,7 +78,10 @@ export function LifecycleToggles({ insightProps }: EditorFilterProps): JSX.Eleme
     return (
         <div className="flex flex-col -mt-1 uppercase">
             {lifecycles.map((lifecycle) => (
-                <LemonLabel key={lifecycle.name} info={lifecycle.tooltip}>
+                <LemonLabel
+                    key={lifecycle.name}
+                    info={getLifecycleTooltip(lifecycle.name, aggregationTargetLabel, aggregationGroupTypeIndex)}
+                >
                     <LemonCheckbox
                         label={lifecycle.name}
                         color={lifecycle.color}
