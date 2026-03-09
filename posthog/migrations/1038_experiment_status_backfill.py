c37cc8382c1bbd642a5419f0c2465a1b0126d4ef
Anders Asheim Hennum <git@andehen.net>
2026-03-09 09:14:13 +0100

    refactor migration to single update statement

--- posthog/migrations/1038_experiment_status_backfill.py
+++ posthog/migrations/1038_experiment_status_backfill.py
@@ -1,15 +1,16 @@
 from django.db import migrations
+from django.db.models import Case, Value, When
 
 
 def backfill_experiment_status(apps, schema_editor):
     Experiment = apps.get_model("posthog", "Experiment")
 
-    Experiment.objects.filter(status__isnull=True, start_date__isnull=True).update(status="draft")
-    Experiment.objects.filter(status__isnull=True, start_date__isnull=False, end_date__isnull=True).update(
-        status="running"
-    )
-    Experiment.objects.filter(status__isnull=True, start_date__isnull=False, end_date__isnull=False).update(
-        status="stopped"
+    Experiment.objects.filter(status__isnull=True).update(
+        status=Case(
+            When(start_date__isnull=True, then=Value("draft")),
+            When(start_date__isnull=False, end_date__isnull=True, then=Value("running")),
+            When(start_date__isnull=False, end_date__isnull=False, then=Value("stopped")),
+        )
     )
 
 
