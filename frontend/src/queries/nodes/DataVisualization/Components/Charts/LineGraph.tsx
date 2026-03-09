4645b5f9ff005e3379c3865119bd00ce62ae10ca
Thomas Obermüller <thomas.obermueller@gmail.com>
2026-03-09 14:38:51 +0100

    feat(sql): hide null values from chart (instead of making them 0s) (#49851)

--- frontend/src/queries/nodes/DataVisualization/Components/Charts/LineGraph.tsx
+++ frontend/src/queries/nodes/DataVisualization/Components/Charts/LineGraph.tsx
@@ -113,7 +113,7 @@ const getYAxisSettings = (
 
 export type LineGraphProps = {
     xData: AxisSeries<string> | null
-    yData: AxisSeries<number>[] | AxisBreakdownSeries<number>[]
+    yData: AxisSeries<number | null>[] | AxisBreakdownSeries<number>[]
     visualizationType: ChartDisplayType
     chartSettings: ChartSettings
     presetChartHeight?: boolean
@@ -430,10 +430,14 @@ export const LineGraph = ({
                                 const stackedSeriesTotalAtIndex =
                                     isStackedBarChart && chartSettings.stackBars100
                                         ? ySeriesData.reduce(
-                                              (acc, series) => acc + series.data[referenceDataPoint.dataIndex],
+                                              (acc, series) => acc + (series.data[referenceDataPoint.dataIndex] ?? 0),
                                               0
                                           )
                                         : null
+
+                                filteredSeriesData = filteredSeriesData.filter(
+                                    (series) => series.data[referenceDataPoint.dataIndex] !== null
+                                )
 
                                 const isTruncated = filteredSeriesData.length > TOOLTIP_ROW_CUTOFF
                                 if (isTruncated) {
@@ -470,7 +474,7 @@ export const LineGraph = ({
                                     !isHighlightBarMode
                                 ) {
                                     const totalRawData = tooltipTotalData.reduce((acc, cur) => {
-                                        acc += cur.data[referenceDataPoint.dataIndex]
+                                        acc += cur.data[referenceDataPoint.dataIndex] ?? 0
                                         return acc
                                     }, 0)
 
@@ -536,8 +540,9 @@ export const LineGraph = ({
                                                                     </div>
                                                                 )
                                                             }
+                                                            const rawData = record.rawData ?? 0
                                                             const percentageLabel: number = parseFloat(
-                                                                ((record.rawData / total) * 100).toFixed(1)
+                                                                ((rawData / total) * 100).toFixed(1)
                                                             )
 
                                                             return (
