559a6e48b17830801b97b54d0ca50adf40a08250
Arthur Moreira de Deus <arthur@posthog.com>
2026-03-10 20:47:59 -0300

    chore: Last seen feature flag (#50543)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- nodejs/tests/helpers/team.ts
+++ nodejs/tests/helpers/team.ts
@@ -25,6 +25,7 @@ export function createTestTeam(overrides: Partial<Team> = {}): Team {
         timezone: 'UTC',
         available_features: [],
         drop_events_older_than_seconds: null,
+        extra_settings: null,
         project_id: 1 as unknown as ProjectId,
         ...overrides,
     }
