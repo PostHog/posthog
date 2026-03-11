689b372d34dde8cd185d7602b417c13591804e3a
Xander Jones <xander@posthog.com>
2026-03-11 10:35:38 +0000

    chore: change order of operations to be more sensible

--- frontend/src/scenes/insights/views/InsightsTable/InsightsTable.tsx
+++ frontend/src/scenes/insights/views/InsightsTable/InsightsTable.tsx
@@ -215,7 +215,7 @@ export function InsightsTable({
         },
     })
 
-    if (isValidBreakdown(breakdownFilter) && breakdownFilter.breakdown) {
+    if (breakdownFilter?.breakdown && isValidBreakdown(breakdownFilter)) {
         if (!isSingleSeriesWithBreakdown) {
             columns.push({
                 title: (
@@ -256,7 +256,7 @@ export function InsightsTable({
                 },
             })
         }
-    } else if (isValidBreakdown(breakdownFilter) && breakdownFilter?.breakdowns && !isSingleSeriesWithBreakdown) {
+    } else if (breakdownFilter?.breakdowns && isValidBreakdown(breakdownFilter) && !isSingleSeriesWithBreakdown) {
         breakdownFilter.breakdowns.forEach((breakdown, index) => {
             const formatItemBreakdownLabel = (item: IndexedTrendResult): string =>
                 formatBreakdownLabel(
