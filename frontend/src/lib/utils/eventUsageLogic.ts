225529750b66174a34b620643215cc0d2667fb17
Georgis Andonis <georgis@posthog.com>
2026-03-12 15:15:16 +0200

    chore: track actors modal search usage (#50647)

--- frontend/src/lib/utils/eventUsageLogic.ts
+++ frontend/src/lib/utils/eventUsageLogic.ts
@@ -246,6 +246,7 @@ export const eventUsageLogic = kea<eventUsageLogicType>([
         reportPersonsModalViewed: (params: any) => ({
             params,
         }),
+        reportPersonsModalSearched: (params: { teamId?: number | null; actorType?: string }) => ({ params }),
         // timing
         reportTimeToSeeData: (payload: TimeToSeeDataPayload) => ({ payload }),
         reportGroupTypeDetailDashboardCreated: () => ({}),
@@ -1014,6 +1015,9 @@ export const eventUsageLogic = kea<eventUsageLogicType>([
         },
         reportPersonsModalViewed: async ({ params }) => {
             posthog.capture('insight person modal viewed', params)
+        },
+        reportPersonsModalSearched: async ({ params }) => {
+            posthog.capture('insight person modal searched', params)
         },
         reportDashboardViewed: async ({ dashboard, lastRefreshed, delay }, breakpoint) => {
             if (!delay) {
