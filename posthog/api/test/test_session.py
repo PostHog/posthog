4b3510c8b1f526c8262ecf471a9f8807b15e6d64
Javier Bahamondes <javier@posthog.com>
2026-03-12 16:16:31 -0300

    feat(tests): add entry_referrer to session API tests
    - Included the  field in the session API test to validate its functionality.

--- posthog/api/test/test_session.py
+++ posthog/api/test/test_session.py
@@ -54,6 +54,7 @@ class TestSessionsAPI(APIBaseTest):
             "$entry_mc_cid",
             "$entry_msclkid",
             "$entry_pathname",
+            "$entry_referrer",
             "$entry_referring_domain",
             "$entry_ttclid",
             "$entry_twclid",
