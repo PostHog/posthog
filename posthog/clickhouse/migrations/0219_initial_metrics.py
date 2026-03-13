09d58a25d9eb2b992f9677a8e99420bef0c6a419
Ben White <ben@posthog.com>
2026-03-13 15:57:30 +0000

    Added metrics table migrations

---
+++ posthog/clickhouse/migrations/0219_initial_metrics.py
@@ -0,0 +1,32 @@
+from posthog.clickhouse.client.connection import NodeRole
+from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
+from posthog.clickhouse.metrics import (
+    METRIC_ATTRIBUTES_MV,
+    METRIC_ATTRIBUTES_TABLE_SQL,
+    METRIC_RESOURCE_ATTRIBUTES_MV,
+    METRICS31_TABLE_SQL,
+    METRICS_DISTRIBUTED_TABLE_SQL,
+)
+
+operations = [
+    run_sql_with_exceptions(
+        METRICS31_TABLE_SQL(),
+        node_roles=[NodeRole.LOGS],
+    ),
+    run_sql_with_exceptions(
+        METRICS_DISTRIBUTED_TABLE_SQL(),
+        node_roles=[NodeRole.LOGS],
+    ),
+    run_sql_with_exceptions(
+        METRIC_ATTRIBUTES_TABLE_SQL(),
+        node_roles=[NodeRole.LOGS],
+    ),
+    run_sql_with_exceptions(
+        METRIC_ATTRIBUTES_MV(),
+        node_roles=[NodeRole.LOGS],
+    ),
+    run_sql_with_exceptions(
+        METRIC_RESOURCE_ATTRIBUTES_MV(),
+        node_roles=[NodeRole.LOGS],
+    ),
+]
