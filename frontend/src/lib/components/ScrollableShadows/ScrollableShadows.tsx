5b03bde19a15f13d3d691de17a0261b366c2b15c
Adam Leith <adamleithp@users.noreply.github.com>
2026-03-16 14:33:19 +0000

    fix scrollable shadows

--- frontend/src/lib/components/ScrollableShadows/ScrollableShadows.tsx
+++ frontend/src/lib/components/ScrollableShadows/ScrollableShadows.tsx
@@ -86,7 +86,7 @@ export const ScrollableShadows = React.forwardRef<HTMLDivElement, ScrollableShad
                           : undefined
                 }
             >
-                <ScrollArea.Content>{children}</ScrollArea.Content>
+                <ScrollArea.Content className="min-w-0">{children}</ScrollArea.Content>
             </ScrollArea.Viewport>
         </ScrollArea.Root>
     )
