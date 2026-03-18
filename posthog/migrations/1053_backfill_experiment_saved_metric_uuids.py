ce405a75034f5f404233ecf0852dea1eb4857a77
Anders Asheim Hennum <git@andehen.net>
2026-03-18 10:04:50 +0100

    remove atomic=False to make CI happy

--- posthog/migrations/1053_backfill_experiment_saved_metric_uuids.py
+++ posthog/migrations/1053_backfill_experiment_saved_metric_uuids.py
@@ -74,7 +74,6 @@ def backfill_experiment_saved_metric_uuids(apps, schema_editor):
 
 
 class Migration(migrations.Migration):
-    atomic = False
     dependencies = [
         ("posthog", "1052_migrate_legacy_personal_api_key_scopes"),
     ]
