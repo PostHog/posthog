4aed4cf39a42e5f89649c3363404014eab12505b
Vasco de Krijger <vasco@posthog.com>
2026-03-23 10:51:09 +0100

    fix: Register missing jobs

--- posthog/temporal/subscriptions/__init__.py
+++ posthog/temporal/subscriptions/__init__.py
@@ -1,5 +1,6 @@
-from posthog.temporal.exports.activities import emit_delivery_outcome, export_asset_activity
+from posthog.temporal.exports.activities import emit_delivery_outcome, emit_delivery_started, export_asset_activity
 from posthog.temporal.subscriptions.activities import (
+    advance_next_delivery_date,
     create_export_assets,
     deliver_subscription,
     fetch_due_subscriptions_activity,
@@ -17,5 +18,7 @@ ACTIVITIES = [
     create_export_assets,
     export_asset_activity,
     deliver_subscription,
+    emit_delivery_started,
     emit_delivery_outcome,
+    advance_next_delivery_date,
 ]
