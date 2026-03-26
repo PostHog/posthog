fd5c4213527c2d49855eee67cf675a5300c4f2f8
Patricio <patricio@posthog.com>
2026-03-26 17:19:34 -0300

    chore(flags): migrate super_groups flags key (phase 3)

---
+++ posthog/migrations/1072_backfill_feature_enrollment.py
@@ -0,0 +1,50 @@
+from django.db import migrations
+
+import structlog
+
+logger = structlog.get_logger(__name__)
+
+BATCH_SIZE = 250
+
+
+def backfill_feature_enrollment(apps, schema_editor):
+    FeatureFlag = apps.get_model("posthog", "FeatureFlag")
+    total_updated = 0
+    objects_to_update = []
+
+    queryset = FeatureFlag.objects.filter(
+        deleted=False,
+        filters__super_groups__isnull=False,
+    ).exclude(
+        filters__feature_enrollment=True,
+    )
+
+    for flag in queryset.iterator(chunk_size=BATCH_SIZE):
+        super_groups = flag.filters.get("super_groups")
+        if not isinstance(super_groups, list) or len(super_groups) == 0:
+            continue
+
+        flag.filters["feature_enrollment"] = True
+        objects_to_update.append(flag)
+
+        if len(objects_to_update) >= BATCH_SIZE:
+            FeatureFlag.objects.bulk_update(objects_to_update, ["filters"])
+            total_updated += len(objects_to_update)
+            objects_to_update = []
+
+    if objects_to_update:
+        FeatureFlag.objects.bulk_update(objects_to_update, ["filters"])
+        total_updated += len(objects_to_update)
+
+    if total_updated:
+        logger.info("backfilled_feature_enrollment", updated_flags=total_updated)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("posthog", "1071_move_tokens_to_sensitive_config"),
+    ]
+
+    operations = [
+        migrations.RunPython(backfill_feature_enrollment, migrations.RunPython.noop, elidable=True),
+    ]
