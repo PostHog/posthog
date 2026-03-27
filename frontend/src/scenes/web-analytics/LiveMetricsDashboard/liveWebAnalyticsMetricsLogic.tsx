7ba2e5c5365a92a57204dc506aef909961baf703
Jordan Mryyan <jordan.m@posthog.com>
2026-03-27 15:57:36 -0500

    Add live events table to the Web Analytics Live Overview

--- frontend/src/scenes/web-analytics/LiveMetricsDashboard/liveWebAnalyticsMetricsLogic.tsx
+++ frontend/src/scenes/web-analytics/LiveMetricsDashboard/liveWebAnalyticsMetricsLogic.tsx
@@ -8,6 +8,7 @@ import { dayjs } from 'lib/dayjs'
 import { featureFlagLogic } from 'lib/logic/featureFlagLogic'
 import { hashCodeForString } from 'lib/utils'
 import { liveEventsHostOrigin } from 'lib/utils/apiHost'
+import { deduplicateEvents } from 'scenes/activity/live/deduplicateEvents'
 import { teamLogic } from 'scenes/teamLogic'
 
 import { performQuery } from '~/queries/query'
@@ -56,6 +57,7 @@ export const liveWebAnalyticsMetricsLogic = kea<liveWebAnalyticsMetricsLogicType
         tickCurrentMinute: true,
         pauseStream: true,
         resumeStream: true,
+        clearRecentEvents: true,
     })),
     reducers({
         slidingWindow: [
@@ -132,6 +134,13 @@ export const liveWebAnalyticsMetricsLogic = kea<liveWebAnalyticsMetricsLogicType
             true,
             {
                 setIsLoading: (_, { loading }) => loading,
+            },
+        ],
+        recentEvents: [
+            [] as LiveEvent[],
+            {
+                addEvents: (state, { events }) => deduplicateEvents(state, events, 50),
+                clearRecentEvents: () => [],
             },
         ],
     }),
@@ -270,7 +279,10 @@ export const liveWebAnalyticsMetricsLogic = kea<liveWebAnalyticsMetricsLogicType
             }
 
             const url = new URL(`${host}/events`)
-            url.searchParams.append('columns', '$pathname,$device_type,$device_id,$browser,$ip,$raw_user_agent')
+            url.searchParams.append(
+                'columns',
+                '$pathname,$current_url,$device_type,$device_id,$browser,$ip,$raw_user_agent'
+            )
 
             cache.batch = cache.batch ?? ([] as LiveEvent[])
 
