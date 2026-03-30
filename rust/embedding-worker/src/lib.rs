10f6531e5aed2d9755d2eaf048c3c40ca8618110
Oliver Browne <oliver@posthog.com>
2026-03-30 15:27:53 +0300

    remove import

--- rust/embedding-worker/src/lib.rs
+++ rust/embedding-worker/src/lib.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use std::time::Duration;
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use common_kafka::kafka_consumer::Offset;
 use common_types::embedding::{
     EmbeddingModel, EmbeddingRequest, EmbeddingResponse, EmbeddingResult, ModelResult,
