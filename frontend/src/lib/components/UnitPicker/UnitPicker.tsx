de60c3bf5448c5fa86747d75ab43b9b92c3a1f22
Marius Andra <marius.andra@gmail.com>
2026-03-31 21:47:20 +0000

    unit-short-picker

--- frontend/src/lib/components/UnitPicker/UnitPicker.tsx
+++ frontend/src/lib/components/UnitPicker/UnitPicker.tsx
@@ -5,19 +5,15 @@ import { useKeyboardHotkeys } from 'lib/hooks/useKeyboardHotkeys'
 import { LemonButton, LemonButtonWithDropdown } from 'lib/lemon-ui/LemonButton'
 import { LemonDivider } from 'lib/lemon-ui/LemonDivider'
 import { eventUsageLogic } from 'lib/utils/eventUsageLogic'
-import { AggregationAxisFormat, INSIGHT_UNIT_OPTIONS } from 'scenes/insights/aggregationAxisFormat'
+import {
+    AggregationAxisFormat,
+    INSIGHT_UNIT_OPTIONS,
+    INSIGHT_UNIT_OPTIONS_SHORT,
+} from 'scenes/insights/aggregationAxisFormat'
 import { insightLogic } from 'scenes/insights/insightLogic'
 import { insightVizDataLogic } from 'scenes/insights/insightVizDataLogic'
 
 import { unitPickerModalLogic } from './unitPickerModalLogic'
-
-const aggregationDisplayMap = INSIGHT_UNIT_OPTIONS.reduce<Record<AggregationAxisFormat, React.ReactNode>>(
-    (acc, option) => {
-        acc[option.value] = option.label
-        return acc
-    },
-    {} as Record<AggregationAxisFormat, React.ReactNode>
-)
 
 export interface HandleUnitChange {
     format?: AggregationAxisFormat
@@ -71,7 +67,7 @@ export function UnitPicker(): JSX.Element {
     const displayValue = useMemo(() => {
         let displayValue: React.ReactNode = 'None'
         if (localAxisFormat) {
-            displayValue = aggregationDisplayMap[localAxisFormat]
+            displayValue = INSIGHT_UNIT_OPTIONS_SHORT[localAxisFormat]
         }
         if (trendsFilter?.aggregationAxisPrefix?.length) {
             displayValue = `Prefix: ${trendsFilter?.aggregationAxisPrefix}`
