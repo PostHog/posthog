7496e2d4e9caf9430cfe46f85d8e90fd9e5a50bf
Arthur Moreira de Deus <arthur@posthog.com>
2026-04-01 18:13:45 -0300

    test: Materialize revenue analytics tables in test setup

--- posthog/hogql/database/schema/test/test_groups_revenue_analytics.py
+++ posthog/hogql/database/schema/test/test_groups_revenue_analytics.py
@@ -1,63 +1,32 @@
 from decimal import Decimal
-from pathlib import Path
 
 from freezegun import freeze_time
-from posthog.test.base import BaseTest, ClickhouseTestMixin, _create_event, _create_person, snapshot_clickhouse_queries
+from posthog.test.base import _create_event, _create_person, snapshot_clickhouse_queries
 from unittest.mock import patch
 
 from posthog.schema import (
     CurrencyCode,
-    HogQLQueryModifiers,
     RevenueAnalyticsEventItem,
     RevenueCurrencyPropertyConfig,
     SubscriptionDropoffMode,
 )
 
 from posthog.hogql import ast
+from posthog.hogql.database.schema.test.base import RevenueAnalyticsManagedViewsetsTestMixin, RevenueAnalyticsTestBase
 from posthog.hogql.parser import parse_select
 from posthog.hogql.query import execute_hogql_query
 
 from posthog.models.group.util import create_group
 from posthog.models.group_type_mapping import GroupTypeMapping
-from posthog.temporal.data_imports.sources.stripe.constants import (
-    CUSTOMER_RESOURCE_NAME as STRIPE_CUSTOMER_RESOURCE_NAME,
-    INVOICE_RESOURCE_NAME as STRIPE_INVOICE_RESOURCE_NAME,
-)
 
-from products.data_warehouse.backend.models import DataWarehouseJoin, ExternalDataSchema
-from products.data_warehouse.backend.models.datawarehouse_managed_viewset import DataWarehouseManagedViewSet
-from products.data_warehouse.backend.test.utils import create_data_warehouse_table_from_csv
-from products.data_warehouse.backend.types import DataWarehouseManagedViewSetKind
-from products.revenue_analytics.backend.hogql_queries.test.data.structure import (
-    STRIPE_CUSTOMER_COLUMNS,
-    STRIPE_INVOICE_COLUMNS,
-)
+from products.data_warehouse.backend.models import DataWarehouseJoin
 from products.revenue_analytics.backend.views.schemas.customer import SCHEMA
 
-INVOICES_TEST_BUCKET = "test_storage_bucket-posthog.revenue_analytics.insights_query_runner.stripe_invoices"
-CUSTOMERS_TEST_BUCKET = "test_storage_bucket-posthog.revenue_analytics.insights_query_runner.stripe_customers"
 
-
-class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
-    PURCHASE_EVENT_NAME = "purchase"
-    REVENUE_PROPERTY = "revenue"
-    SUBSCRIPTION_PROPERTY = "subscription_id"
-    QUERY_TIMESTAMP = "2025-05-30"
-    EVENT_TIMESTAMP = "2025-05-29"  # One day before the query timestamp
-    MODIFIERS = HogQLQueryModifiers(formatCsvAllowDoubleQuotes=True)
-
-    person_id = "00000000-0000-0000-0000-000000000000"
+class TestGroupsRevenueAnalyticsMixin(RevenueAnalyticsTestBase):
     group0_id = "lolol0:xxx"
     group1_id = "lolol1:xxx"
     another_group0_id = "lolol1:xxx2"
-    distinct_id = "distinct_id"
-
-    def tearDown(self):
-        if hasattr(self, "invoices_cleanup_filesystem"):
-            self.invoices_cleanup_filesystem()
-        if hasattr(self, "customers_cleanup_filesystem"):
-            self.customers_cleanup_filesystem()
-        super().tearDown()
 
     def setup_events(self):
         create_group(
@@ -80,15 +49,15 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         )
 
         _create_person(
-            uuid=self.person_id,
+            uuid=self.PERSON_ID,
             team_id=self.team.pk,
-            distinct_ids=[self.distinct_id],
+            distinct_ids=[self.DISTINCT_ID],
         )
 
         _create_event(
             event="$pageview",
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={"$group_0": self.group0_id},
         )
@@ -96,7 +65,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={self.REVENUE_PROPERTY: 25042, "$group_0": self.group0_id},
         )
@@ -104,7 +73,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={self.REVENUE_PROPERTY: 12500, "$group_1": self.group1_id},
         )
@@ -112,7 +81,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={self.REVENUE_PROPERTY: 10000, "$group_0": self.group0_id, "$group_1": self.group1_id},
         )
@@ -120,7 +89,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={self.REVENUE_PROPERTY: 3223, "$group_0": self.another_group0_id},
         )
@@ -146,15 +115,15 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         )
 
         _create_person(
-            uuid=self.person_id,
+            uuid=self.PERSON_ID,
             team_id=self.team.pk,
-            distinct_ids=[self.distinct_id],
+            distinct_ids=[self.DISTINCT_ID],
         )
 
         _create_event(
             event="$pageview",
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={"$group_0": self.group0_id},
         )
@@ -163,7 +132,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={self.REVENUE_PROPERTY: 25042, "$group_0": self.group0_id},
         )
@@ -172,7 +141,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={
                 self.REVENUE_PROPERTY: 12500,
@@ -184,7 +153,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={
                 self.REVENUE_PROPERTY: 10000,
@@ -197,7 +166,7 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         _create_event(
             event=self.PURCHASE_EVENT_NAME,
             team=self.team,
-            distinct_id=self.distinct_id,
+            distinct_id=self.DISTINCT_ID,
             timestamp=self.EVENT_TIMESTAMP,
             properties={
                 self.REVENUE_PROPERTY: 3223,
@@ -207,55 +176,10 @@ class TestGroupsRevenueAnalyticsMixin(ClickhouseTestMixin, BaseTest):
         )
 
     def setup_schema_sources(self):
-        invoices_csv_path = Path("products/revenue_analytics/backend/hogql_queries/test/data/stripe_invoices.csv")
-        invoices_table, source, credential, _, self.invoices_cleanup_filesystem = create_data_warehouse_table_from_csv(
-            invoices_csv_path,
-            "stripe_invoice",
-            STRIPE_INVOICE_COLUMNS,
-            INVOICES_TEST_BUCKET,
-            self.team,
-        )
-
-        customers_csv_path = Path("products/revenue_analytics/backend/hogql_queries/test/data/stripe_customers.csv")
-        customers_table, _, _, _, self.customers_cleanup_filesystem = create_data_warehouse_table_from_csv(
-            customers_csv_path,
-            "stripe_customer",
-            STRIPE_CUSTOMER_COLUMNS,
-            CUSTOMERS_TEST_BUCKET,
-            self.team,
-            source=source,
-            credential=credential,
-        )
-
-        # Besides the default creations above, also create the external data schema
-        # because this is required by the `RevenueAnalyticsBaseView` to find the right tables
-        _invoices_schema = ExternalDataSchema.objects.create(
-            team=self.team,
-            name=STRIPE_INVOICE_RESOURCE_NAME,
-            source=source,
-            table=invoices_table,
-            should_sync=True,
-            last_synced_at="2024-01-01",
-        )
-
-        _customers_schema = ExternalDataSchema.objects.create(
-            team=self.team,
-            name=STRIPE_CUSTOMER_RESOURCE_NAME,
-            source=source,
-            table=customers_table,
-            should_sync=True,
-            last_synced_at="2024-01-01",
-        )
-
+        self.create_sources()
         self._setup_join()
         self.team.base_currency = CurrencyCode.GBP.value
         self.team.save()
-
-    def create_managed_viewsets(self):
-        self.viewset, _ = DataWarehouseManagedViewSet.objects.get_or_create(
-            team=self.team, kind=DataWarehouseManagedViewSetKind.REVENUE_ANALYTICS
-        )
-        self.viewset.sync_views()
 
     def _setup_join(self):
         # Create some mappings
@@ -536,8 +460,9 @@ class TestGroupsRevenueAnalytics(TestGroupsRevenueAnalyticsMixin):
             )
 
 
-@snapshot_clickhouse_queries
-class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin):
+class TestGroupsRevenueAnalyticsManagedViewsets(
+    TestGroupsRevenueAnalyticsMixin, RevenueAnalyticsManagedViewsetsTestMixin
+):
     def setUp(self) -> None:
         super().setUp()
         self.mock_flag = patch("posthoganalytics.feature_enabled", return_value=True)
@@ -557,9 +482,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         self.team.revenue_analytics_config.save()
         self.team.save()
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             response = execute_hogql_query(
                 parse_select(
                     "SELECT key, revenue_analytics.revenue, $virt_revenue FROM groups where key = {key}",
@@ -582,9 +506,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         for key in ["cus_1", "cus_2", "cus_3", "cus_4", "cus_5", "cus_6", "dummy"]:
             create_group(team_id=self.team.pk, group_type_index=0, group_key=key)
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             queries = [
                 "SELECT key, revenue_analytics.revenue FROM groups ORDER BY key ASC",
                 "SELECT key, $virt_revenue FROM groups ORDER BY key ASC",
@@ -622,9 +545,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         ]:
             create_group(team_id=self.team.pk, group_type_index=0, group_key=key)
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             response = execute_hogql_query(
                 parse_select("SELECT key, revenue_analytics.revenue, $virt_revenue FROM groups ORDER BY key ASC"),
                 self.team,
@@ -660,9 +582,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         ]:
             create_group(team_id=self.team.pk, group_type_index=0, group_key=key)
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             response = execute_hogql_query(
                 parse_select("SELECT key, revenue_analytics.revenue, $virt_revenue FROM groups ORDER BY key ASC"),
                 self.team,
@@ -690,9 +611,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         for key in ["cus_1", "cus_2", "cus_3", "cus_4", "cus_5", "cus_6", "dummy"]:
             create_group(team_id=self.team.pk, group_type_index=0, group_key=key)
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             response = execute_hogql_query(
                 parse_select(
                     "SELECT key, revenue_analytics.revenue, revenue_analytics.mrr FROM groups ORDER BY key ASC"
@@ -730,9 +650,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         self.team.revenue_analytics_config.save()
         self.team.save()
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             response = execute_hogql_query(
                 parse_select(
                     "SELECT key, revenue_analytics.revenue, revenue_analytics.mrr FROM groups WHERE key = {key}",
@@ -756,9 +675,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         for key in ["cus_1", "cus_2", "cus_3", "cus_4", "cus_5", "cus_6"]:
             create_group(team_id=self.team.pk, group_type_index=0, group_key=key)
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             results = execute_hogql_query(
                 parse_select("SELECT group_key, revenue, mrr FROM groups_revenue_analytics ORDER BY mrr DESC"),
                 self.team,
@@ -794,9 +712,8 @@ class TestGroupsRevenueAnalyticsManagedViewsets(TestGroupsRevenueAnalyticsMixin)
         self.team.revenue_analytics_config.save()
         self.team.save()
 
-        with freeze_time(self.QUERY_TIMESTAMP):
-            self.create_managed_viewsets()
-
+        self.create_and_materialize_viewsets()
+        with freeze_time(self.QUERY_TIMESTAMP), self.snapshot_select_queries():
             results = execute_hogql_query(
                 parse_select("SELECT group_key, revenue, mrr FROM groups_revenue_analytics ORDER BY group_key ASC"),
                 self.team,
