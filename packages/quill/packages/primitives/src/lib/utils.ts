f1eb39a20e092f2396ac69b6d04bcf5f03e15711
Adam Leith <adamleithp@users.noreply.github.com>
2026-04-03 22:37:45 +0100

    add text-xxs to tailwind merge config

--- packages/quill/packages/primitives/src/lib/utils.ts
+++ packages/quill/packages/primitives/src/lib/utils.ts
@@ -1,5 +1,13 @@
 import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const twMerge = extendTailwindMerge({
+    extend: {
+        classGroups: {
+            'font-size': [{ text: ['xxs'] }],
+        },
+    },
+})
 
 export function cn(...inputs: ClassValue[]): string {
     return twMerge(clsx(inputs))
