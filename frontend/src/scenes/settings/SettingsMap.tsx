f9cfaa72bc9c1b97a0cc4d8c00717e6db3b5f15d
Hugues Pouillot <hpouillot@gmail.com>
2026-04-07 16:47:03 +0200

    fix(error-tracking): remove settings split for autocapture

--- frontend/src/scenes/settings/SettingsMap.tsx
+++ frontend/src/scenes/settings/SettingsMap.tsx
@@ -517,7 +517,6 @@ export const SETTINGS_MAP: SettingSection[] = [
                 platformSupport: FEATURE_SUPPORT.errorTrackingExceptionAutocapture,
                 component: <ExceptionAutocaptureToggle />,
                 keywords: ['crash', 'bug', 'exception', 'stack trace'],
-                flag: 'ERROR_TRACKING_SETTINGS_SPLIT',
             },
             {
                 id: 'error-tracking-integrations',
@@ -544,7 +543,6 @@ export const SETTINGS_MAP: SettingSection[] = [
                 platformSupport: FEATURE_SUPPORT.errorTrackingExceptionAutocapture,
                 component: <ExceptionAutocaptureToggle />,
                 keywords: ['crash', 'bug', 'exception', 'stack trace'],
-                flag: '!ERROR_TRACKING_SETTINGS_SPLIT',
             },
             {
                 id: 'error-tracking-alerting',
