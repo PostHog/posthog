a9b23e38c4349d8e5e776b7b20902caa079b2373
ablaszkiewicz <kqmdjc8@gmail.com>
2026-04-08 12:42:24 +0200

    fix: use aux cluster

--- posthog/clickhouse/migrations/0226_add_error_tracking_fingerprint_issue_state.py
+++ posthog/clickhouse/migrations/0226_add_error_tracking_fingerprint_issue_state.py
@@ -5,24 +5,29 @@ from products.error_tracking.backend.sql import (
     ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_MV_SQL,
     ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL,
     KAFKA_ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL,
+    RAW_ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL,
     WRITABLE_ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL,
 )
 
 operations = [
     run_sql_with_exceptions(
         ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL(),
-        node_roles=[NodeRole.DATA],
+        node_roles=[NodeRole.AUX],
     ),
     run_sql_with_exceptions(
         WRITABLE_ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL(),
-        node_roles=[NodeRole.INGESTION_SMALL],
+        node_roles=[NodeRole.AUX],
     ),
     run_sql_with_exceptions(
         KAFKA_ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL(),
-        node_roles=[NodeRole.INGESTION_SMALL],
+        node_roles=[NodeRole.AUX],
     ),
     run_sql_with_exceptions(
         ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_MV_SQL(),
-        node_roles=[NodeRole.INGESTION_SMALL],
+        node_roles=[NodeRole.AUX],
+    ),
+    run_sql_with_exceptions(
+        RAW_ERROR_TRACKING_FINGERPRINT_ISSUE_STATE_TABLE_SQL(),
+        node_roles=[NodeRole.AUX, NodeRole.DATA],
     ),
 ]
