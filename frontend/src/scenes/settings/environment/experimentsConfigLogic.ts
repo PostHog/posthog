87698b436e751abdad2f8805ff0e74b284b87898
Juraj Majerik <juro.majerik@gmail.com>
2026-04-08 12:13:34 +0200

    fix: use explicit team ID instead of @current in API calls

--- frontend/src/scenes/settings/environment/experimentsConfigLogic.ts
+++ frontend/src/scenes/settings/environment/experimentsConfigLogic.ts
@@ -1,7 +1,9 @@
-import { actions, afterMount, kea, listeners, path, reducers } from 'kea'
+import { actions, afterMount, connect, kea, listeners, path, reducers } from 'kea'
 import { loaders } from 'kea-loaders'
 
 import api from 'lib/api'
+
+import { teamLogic } from '~/scenes/teamLogic'
 
 import type { experimentsConfigLogicType } from './experimentsConfigLogicType'
 
@@ -13,28 +15,29 @@ export interface ExperimentsConfig {
 
 export const experimentsConfigLogic = kea<experimentsConfigLogicType>([
     path(['scenes', 'settings', 'environment', 'experimentsConfigLogic']),
+    connect({ values: [teamLogic, ['currentTeamId']] }),
     actions({
         updateExperimentsConfig: (payload: Partial<ExperimentsConfig>) => ({ payload }),
     }),
-    loaders({
+    loaders(({ values }) => ({
         experimentsConfig: [
             null as ExperimentsConfig | null,
             {
                 loadExperimentsConfig: async () => {
-                    return await api.get('api/environments/@current/experiments_config/')
+                    return await api.get(`api/environments/${values.currentTeamId}/experiments_config/`)
                 },
             },
         ],
-    }),
+    })),
     reducers({
         experimentsConfig: {
             updateExperimentsConfig: (state, { payload }) => (state ? { ...state, ...payload } : state),
         },
     }),
-    listeners(({ actions }) => ({
+    listeners(({ actions, values }) => ({
         updateExperimentsConfig: async ({ payload }) => {
             try {
-                await api.update('api/environments/@current/experiments_config/', payload)
+                await api.update(`api/environments/${values.currentTeamId}/experiments_config/`, payload)
             } finally {
                 actions.loadExperimentsConfig()
             }
