b674f1cc3f3c5907b69f096a0c4ebe07f27f5e3e
Thomas Obermüller <thomas.obermueller@gmail.com>
2026-04-09 19:23:23 +0200

    disable toggle

--- frontend/src/scenes/insights/EditorFilters/PoeFilter.tsx
+++ frontend/src/scenes/insights/EditorFilters/PoeFilter.tsx
@@ -1,11 +1,11 @@
 import { useActions, useValues } from 'kea'
 
-import { LemonLabel, LemonSwitch } from '@posthog/lemon-ui'
+import { LemonDisabledArea, LemonLabel, LemonSwitch } from '@posthog/lemon-ui'
 
 import { InsightLogicProps } from '~/types'
 
-import { poeFilterLogic } from './poeFilterLogic'
 import { insightVizDataLogic } from '../insightVizDataLogic'
+import { poeFilterLogic } from './poeFilterLogic'
 
 interface PoeFilterProps {
     insightProps: InsightLogicProps
@@ -17,31 +17,28 @@ export function PoeFilter({ insightProps }: PoeFilterProps): JSX.Element {
     const { setPoeMode } = useActions(poeFilterLogic(insightProps))
     const disabledReason = hasDataWarehouseSeries
         ? 'Data warehouse insights always use the latest table properties.'
-        : null
+        : undefined
 
     return (
-        <>
-            <div className="flex items-center gap-1">
-                <LemonLabel
-                    info="Overrides the default person property mode for this insight to use query-time person properties instead of event-time properties. This can be useful when person data is updated after the event, but it can also slow queries significantly."
-                    infoLink="https://posthog.com/docs/how-posthog-works/queries#filtering-on-person-properties"
-                >
-                    Use person properties from query time
-                </LemonLabel>
-                <LemonSwitch
-                    className="m-2"
-                    disabled={!!disabledReason}
-                    onChange={(checked) => {
-                        if (checked) {
-                            setPoeMode('person_id_override_properties_joined')
-                        } else {
-                            setPoeMode(null)
-                        }
-                    }}
-                    checked={!!poeMode && !disabledReason}
-                />
-            </div>
-            {disabledReason ? <div className="text-xs text-secondary">{disabledReason}</div> : null}
-        </>
+        <LemonDisabledArea className="flex items-center gap-1 w-fit" disabledReason={disabledReason}>
+            <LemonLabel
+                info="Overrides the default person property mode for this insight to use query-time person properties instead of event-time properties. This can be useful when person data is updated after the event, but it can also slow queries significantly."
+                infoLink="https://posthog.com/docs/how-posthog-works/queries#filtering-on-person-properties"
+            >
+                Use person properties from query time
+            </LemonLabel>
+            <LemonSwitch
+                className="m-2"
+                disabled={!!disabledReason}
+                onChange={(checked) => {
+                    if (checked) {
+                        setPoeMode('person_id_override_properties_joined')
+                    } else {
+                        setPoeMode(null)
+                    }
+                }}
+                checked={!!poeMode}
+            />
+        </LemonDisabledArea>
     )
 }
