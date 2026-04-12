47e76a245de6f4ae3fb4d0c68349430178fcb286
Pawel Szczur <pawelszczur@gmail.com>
2026-04-13 00:05:16 +0200

    add clickhouse team group and approve button for data deletion request

---
+++ posthog/migrations/1094_create_group_clickhouse_team.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import Group
+from django.db import migrations
+
+
+def create_clickhouse_team_group(apps, schema_editor):
+    Group.objects.get_or_create(name="ClickHouse Team")
+
+
+def reverse_create_clickhouse_team_group(apps, schema_editor):
+    Group.objects.filter(name="ClickHouse Team").delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("posthog", "1093_datadeletionrequest_max_timestamp_and_more"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_clickhouse_team_group, reverse_create_clickhouse_team_group),
+    ]
