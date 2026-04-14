5761b70a1d75feb1647404da157edf00b01f82f5
Reece Jones <reece@posthog.com>
2026-04-14 11:00:29 -0400

    feat: add field-level access control to hogql for events and person properties

---
+++ products/access_control/backend/apps.py
@@ -0,0 +1,8 @@
+"""Django app configuration for access_control."""
+
+from django.apps import AppConfig
+
+
+class AccessControlConfig(AppConfig):
+    name = "products.access_control.backend"
+    label = "access_control"
