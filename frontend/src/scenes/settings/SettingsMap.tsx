42916afc069abe78cb8896208596c6b0d9b07c2e
Frank Hamand <frank@posthog.com>
2026-04-14 15:44:19 +0100

    reformat

--- frontend/src/scenes/settings/SettingsMap.tsx
+++ frontend/src/scenes/settings/SettingsMap.tsx
@@ -762,9 +762,13 @@ export const SETTINGS_MAP: SettingSection[] = [
             {
                 id: 'logs-retention',
                 title: 'Retention',
-                description: <div>
-                    How long to retain logs before they are automatically deleted. <strong>Changes only affect the retention for new logs</strong>. You can only change this setting at most once per 24 hours.
-                </div>,
+                description: (
+                    <span>
+                        How long to retain logs before they are automatically deleted.{' '}
+                        <strong>Changes only affect the retention for new logs</strong>. You can only change this
+                        setting at most once per 24 hours.
+                    </span>
+                ),
                 component: <LogsRetentionSettings />,
                 keywords: ['retention', 'storage', 'delete', 'ttl'],
             },
