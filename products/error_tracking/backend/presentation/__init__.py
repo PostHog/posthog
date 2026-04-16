0464a5b4cd69ea20b3b9f5223fb6e53c76a8ea66
Hugues Pouillot <hpouillot@gmail.com>
2026-04-16 14:17:07 +0200

    refactor(error_tracking): move fingerprints to presentation

--- products/error_tracking/backend/presentation/__init__.py
+++ products/error_tracking/backend/presentation/__init__.py
@@ -1,7 +1,10 @@
+from .fingerprints import ErrorTrackingFingerprintSerializer, ErrorTrackingFingerprintViewSet
 from .serializers import ErrorTrackingExternalReferenceIntegrationSerializer, ErrorTrackingExternalReferenceSerializer
 from .views import ErrorTrackingExternalReferenceViewSet
 
 __all__ = [
+    "ErrorTrackingFingerprintSerializer",
+    "ErrorTrackingFingerprintViewSet",
     "ErrorTrackingExternalReferenceIntegrationSerializer",
     "ErrorTrackingExternalReferenceSerializer",
     "ErrorTrackingExternalReferenceViewSet",
