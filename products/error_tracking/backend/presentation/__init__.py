6efce907f81a38e661491231fd660bb01934352c
Hugues Pouillot <hpouillot@gmail.com>
2026-04-16 13:29:12 +0200

    refactor(error_tracking): move external references to presentation

---
+++ products/error_tracking/backend/presentation/__init__.py
@@ -0,0 +1,8 @@
+from .serializers import ErrorTrackingExternalReferenceIntegrationSerializer, ErrorTrackingExternalReferenceSerializer
+from .views import ErrorTrackingExternalReferenceViewSet
+
+__all__ = [
+    "ErrorTrackingExternalReferenceIntegrationSerializer",
+    "ErrorTrackingExternalReferenceSerializer",
+    "ErrorTrackingExternalReferenceViewSet",
+]
