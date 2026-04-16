66a349592004e8b16d72d3615a13e907cfe3f061
Hugues Pouillot <hpouillot@gmail.com>
2026-04-16 13:29:12 +0200

    refactor(error_tracking): move external references to presentation

---
+++ products/error_tracking/backend/presentation/views.py
@@ -0,0 +1,16 @@
+import structlog
+from rest_framework import viewsets
+
+from posthog.api.forbid_destroy_model import ForbidDestroyModel
+from posthog.api.routing import TeamAndOrgViewSetMixin
+
+from products.error_tracking.backend.models import ErrorTrackingExternalReference
+from products.error_tracking.backend.presentation.serializers import ErrorTrackingExternalReferenceSerializer
+
+logger = structlog.get_logger(__name__)
+
+
+class ErrorTrackingExternalReferenceViewSet(TeamAndOrgViewSetMixin, ForbidDestroyModel, viewsets.ModelViewSet):
+    scope_object = "INTERNAL"
+    queryset = ErrorTrackingExternalReference.objects.all()
+    serializer_class = ErrorTrackingExternalReferenceSerializer
