21bbcdf5eb81a11e6c9f7b9d2b4dfcbe5435b2c1
Alex V <alexander.veryaysky@gmail.com>
2026-04-16 19:35:57 +0200

    feat: support multiple channels in slack

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -644,6 +644,7 @@ export interface ConversationsSettings {
     slack_team_id?: string | null
     slack_channel_id?: string | null
     slack_channel_name?: string | null
+    slack_channel_ids?: string[] | null
     slack_ticket_emoji?: string | null
     slack_bot_icon_url?: string | null
     slack_bot_display_name?: string | null
