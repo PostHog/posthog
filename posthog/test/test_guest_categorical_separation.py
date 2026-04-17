8d21f6cf15fb71beac038b53fc96a6d749408c86
Yasen Slavov <yasen@posthog.com>
2026-04-17 08:13:42 +0300

    feat(rbac): default members list excludes guests; ?is_guest=true surfaces them to admins

---
+++ posthog/test/test_guest_categorical_separation.py
@@ -0,0 +1,28 @@
+from posthog.test.base import APIBaseTest
+
+from posthog.models import OrganizationMembership, User
+
+
+class TestGuestCategoricalSeparation(APIBaseTest):
+    def setUp(self):
+        super().setUp()
+        OrganizationMembership.objects.filter(user=self.user, organization=self.organization).update(
+            level=OrganizationMembership.Level.ADMIN
+        )
+        self.regular_user = User.objects.create_user(email="r@x.com", password="x", first_name="R")
+        OrganizationMembership.objects.create(organization=self.organization, user=self.regular_user, is_guest=False)
+        self.guest_user = User.objects.create_user(email="g@x.com", password="x", first_name="G")
+        OrganizationMembership.objects.create(organization=self.organization, user=self.guest_user, is_guest=True)
+
+    def test_default_members_list_excludes_guests(self):
+        response = self.client.get("/api/organizations/@current/members/")
+        self.assertEqual(response.status_code, 200)
+        emails = {m["user"]["email"] for m in response.json()["results"]}
+        self.assertIn("r@x.com", emails)
+        self.assertNotIn("g@x.com", emails)
+
+    def test_is_guest_true_filter_returns_only_guests(self):
+        response = self.client.get("/api/organizations/@current/members/?is_guest=true")
+        self.assertEqual(response.status_code, 200)
+        emails = {m["user"]["email"] for m in response.json()["results"]}
+        self.assertEqual(emails, {"g@x.com"})
