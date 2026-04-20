f7988a89bbbf2dbc077038af0c354a3f80803221
Alessandro Pogliaghi <alessandro@posthog.com>
2026-04-20 16:06:08 +0100

    chore(tasks): fix artifact api CI checks

--- products/tasks/backend/models.py
+++ products/tasks/backend/models.py
@@ -6,7 +6,6 @@ import string
 import secrets
 from collections.abc import Callable, Iterable
 from datetime import datetime
-from collections.abc import Callable, Iterable
 from typing import TYPE_CHECKING, Any, Literal, Optional
 
 from django.db.models.signals import post_save
