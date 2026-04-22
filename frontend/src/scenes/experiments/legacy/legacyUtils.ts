9fc7156af13458e345bf8bb17cc8a830609e31fa
Rodrigo Iloro <rodrigo@posthog.com>
2026-04-22 03:37:53 -0300

    chore(experiments): updating legacy metric summary table.

--- frontend/src/scenes/experiments/legacy/legacyUtils.ts
+++ frontend/src/scenes/experiments/legacy/legacyUtils.ts
@@ -1,5 +1,19 @@
-import { ExperimentTrendsQuery, ExperimentFunnelsQuery, NodeKind } from '~/queries/schema/schema-general'
-import { InsightType } from '~/types'
+import {
+    ExperimentTrendsQuery,
+    ExperimentFunnelsQuery,
+    NodeKind,
+    CachedLegacyExperimentQueryResponse,
+    CachedExperimentFunnelsQueryResponse,
+    CachedExperimentTrendsQueryResponse,
+} from '~/queries/schema/schema-general'
+import {
+    CountPerActorMathType,
+    Experiment,
+    FunnelExperimentVariant,
+    InsightType,
+    PropertyMathType,
+    TrendExperimentVariant,
+} from '~/types'
 
 /**
  * @deprecated
@@ -7,4 +21,103 @@ import { InsightType } from '~/types'
  */
 export const getInsightType = (metric: ExperimentTrendsQuery | ExperimentFunnelsQuery): InsightType => {
     return metric.kind === NodeKind.ExperimentTrendsQuery ? InsightType.TRENDS : InsightType.FUNNELS
+}
+
+/**
+ * @deprecated
+ * Use the tabularExperimentResults function from the experimentLogic instead.
+ */
+export const getTabularExperimentResults =
+    (
+        experiment: Experiment,
+        legacyPrimaryMetricsResults: (
+            | CachedLegacyExperimentQueryResponse
+            | CachedExperimentFunnelsQueryResponse
+            | CachedExperimentTrendsQueryResponse
+            | null
+        )[],
+        legacySecondaryMetricsResults: (
+            | CachedLegacyExperimentQueryResponse
+            | CachedExperimentFunnelsQueryResponse
+            | CachedExperimentTrendsQueryResponse
+            | null
+        )[],
+        getInsightType: (metric: ExperimentTrendsQuery | ExperimentFunnelsQuery) => InsightType
+    ) =>
+    (metricIdentifier: number | string = 0, isSecondary: boolean = false): any[] => {
+        let index: number
+        if (typeof metricIdentifier === 'string') {
+            // Find index by UUID
+            const metrics = isSecondary ? experiment.metrics_secondary : experiment.metrics
+            index = metrics.findIndex((m) => m.uuid === metricIdentifier)
+            if (index === -1) {
+                return []
+            }
+        } else {
+            index = metricIdentifier
+        }
+
+        const tabularResults = []
+        const metricType = isSecondary
+            ? getInsightType(experiment.metrics_secondary[index] as ExperimentTrendsQuery | ExperimentFunnelsQuery)
+            : getInsightType(experiment.metrics[index] as ExperimentTrendsQuery | ExperimentFunnelsQuery)
+        const result = isSecondary ? legacySecondaryMetricsResults[index] : legacyPrimaryMetricsResults[index]
+
+        if (result) {
+            for (const variantObj of result.variants) {
+                if (metricType === InsightType.FUNNELS) {
+                    const { key, success_count, failure_count } = variantObj as FunnelExperimentVariant
+                    tabularResults.push({ key, success_count, failure_count })
+                } else if (metricType === InsightType.TRENDS) {
+                    const { key, count, exposure, absolute_exposure } = variantObj as TrendExperimentVariant
+                    tabularResults.push({ key, count, exposure, absolute_exposure })
+                }
+            }
+        }
+
+        if (experiment.feature_flag?.filters.multivariate?.variants) {
+            for (const { key } of experiment.feature_flag.filters.multivariate.variants) {
+                if (tabularResults.find((variantObj) => variantObj.key === key)) {
+                    continue
+                }
+
+                if (metricType === InsightType.FUNNELS) {
+                    tabularResults.push({ key, success_count: null, failure_count: null })
+                } else if (metricType === InsightType.TRENDS) {
+                    tabularResults.push({ key, count: null, exposure: null, absolute_exposure: null })
+                }
+            }
+        }
+
+        return tabularResults
+    }
+
+/**
+ * @deprecated
+ * Use the experimentMathAggregationForTrends function from the experimentLogic instead.
+ */
+export const getExperimentMathAggregationForTrends = (
+    experiment: Experiment
+): PropertyMathType | CountPerActorMathType | undefined => {
+    const query = experiment?.metrics?.[0] as ExperimentTrendsQuery
+    if (!query) {
+        return undefined
+    }
+    const entities = query.count_query?.series || []
+
+    // Find out if we're using count per actor math aggregates averages per user
+    const userMathValue = entities.filter((entity) =>
+        Object.values(CountPerActorMathType).includes(entity?.math as CountPerActorMathType)
+    )[0]?.math
+
+    // alternatively, if we're using property math
+    // remove 'sum' property math from the list of math types
+    // since we can handle that as a regular case
+    const targetValues = Object.values(PropertyMathType).filter((value) => value !== PropertyMathType.Sum)
+
+    const propertyMathValue = entities.filter((entity) =>
+        (targetValues as readonly PropertyMathType[]).includes(entity?.math as PropertyMathType)
+    )[0]?.math
+
+    return (userMathValue ?? propertyMathValue) as PropertyMathType | CountPerActorMathType | undefined
 }
