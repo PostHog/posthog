bc80fa69f9a73d2cdcaeeb91dd76cbb9983e3861
Marius Andra <marius.andra@gmail.com>
2026-04-22 10:56:49 +0200

    del

--- ee/hogai/chat_agent/test/test_prompts.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-
-from ee.hogai.chat_agent.prompts.base import BASIC_FUNCTIONALITY_PROMPT
-
-
-class TestChatAgentPrompts(SimpleTestCase):
-    def test_basic_functionality_prompt_teaches_sql_variable_discovery(self):
-        self.assertIn("SQL variables", BASIC_FUNCTIONALITY_PROMPT)
-        self.assertIn("system.insight_variables", BASIC_FUNCTIONALITY_PROMPT)
