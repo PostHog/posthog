137e7aa36de72d6e4b6a8d31d7cb0e9a6cb08b06
Adam Leith <adamleithp@users.noreply.github.com>
2026-04-24 11:18:01 +0100

    nices

--- packages/quill/packages/primitives/src/tooltip.tsx
+++ packages/quill/packages/primitives/src/tooltip.tsx
@@ -38,10 +38,7 @@ function TooltipContent({
             >
                 <TooltipPrimitive.Popup
                     data-slot="tooltip-content"
-                    className={cn(
-                        'quill-tooltip__content inline-flex items-center gap-1.5 data-[side=bottom]:slide-in-from-top-2 data-[side=inline-end]:slide-in-from-start-2 data-[side=inline-start]:slide-in-from-end-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=delayed-open]:animate-in data-[state=delayed-open]:fade-in-0 data-[state=delayed-open]:zoom-in-95 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95',
-                        className
-                    )}
+                    className={cn('quill-tooltip__content inline-flex items-center gap-1.5', className)}
                     {...props}
                 >
                     {children}
