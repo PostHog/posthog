b7c7d62ed6fd8fd8b4e95861014383e2bf029bca
Andy Maguire <andy.m@posthog.com>
2026-04-26 18:12:33 +0000

    feat(llma): add created_by filter to skills list
    Generated-By: PostHog Code
    Task-Id: 992d551b-74c3-4a33-87af-bd10a1b09df3

--- products/llm_analytics/frontend/skills/LLMSkillsScene.tsx
+++ products/llm_analytics/frontend/skills/LLMSkillsScene.tsx
@@ -5,6 +5,7 @@ import { IconPlusSmall } from '@posthog/icons'
 import { Link } from '@posthog/lemon-ui'
 
 import { AccessControlAction } from 'lib/components/AccessControlAction'
+import { MemberSelect } from 'lib/components/MemberSelect'
 import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { More } from 'lib/lemon-ui/LemonButton/More'
 import { ProfilePicture } from 'lib/lemon-ui/ProfilePicture'
@@ -190,6 +191,16 @@ export function LLMSkillsScene(): JSX.Element {
                         className="max-w-md"
                     />
                     <div className="text-muted-alt">{skillCountLabel}</div>
+                    <div className="flex-1" />
+                    <span>
+                        <b>Created by</b>
+                    </span>
+                    <MemberSelect
+                        defaultLabel="Any user"
+                        value={filters.created_by_id ?? null}
+                        size="xsmall"
+                        onChange={(user) => setFilters({ created_by_id: user?.id, page: 1 })}
+                    />
                 </div>
 
                 <LemonTable
