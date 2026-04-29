8e6279be3b1144853eb00b981d463537df109f28
nickbest-ph <nick@posthog.com>
2026-04-29 12:16:30 -0700

    chore(semgrep): semgrep persons orm rule

--- posthog/models/group/util.py
+++ posthog/models/group/util.py
@@ -67,7 +67,7 @@ def create_group(
         timestamp,
         timestamp=timestamp,
     )
-    group = Group.objects.create(
+    group = Group.objects.create(  # nosemgrep: no-direct-persons-db-orm
         team_id=team_id,
         group_type_index=group_type_index,
         group_key=group_key,
