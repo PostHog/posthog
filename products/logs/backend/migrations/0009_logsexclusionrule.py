6a3400f1e0dc7322f33f21a12b57fed68ae5e80b
Daniel Visca <daniel.v@posthog.com>
2026-04-30 13:28:31 -0700

    updated to most recent migration

--- products/logs/backend/migrations/0009_logsexclusionrule.py
+++ products/logs/backend/migrations/0009_logsexclusionrule.py
@@ -59,7 +59,6 @@ class Migration(migrations.Migration):
                         blank=True,
                         null=True,
                         on_delete=django.db.models.deletion.SET_NULL,
-                        related_name="+",
                         to=settings.AUTH_USER_MODEL,
                     ),
                 ),
