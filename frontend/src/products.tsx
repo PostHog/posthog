332f3c99cd5ad3b3d5276c44d9b4f4a78f0db07e
Alex V <alexander.veryaysky@gmail.com>
2026-05-01 11:33:15 +0200

    chore: team ownership + flag

--- frontend/src/products.tsx
+++ frontend/src/products.tsx
@@ -1730,7 +1730,6 @@ export const getTreeItemsProducts = (): FileSystemImport[] => [
         category: ProductItemCategory.BEHAVIOR,
         href: urls.supportTickets(),
         type: 'conversations',
-        flag: FEATURE_FLAGS.PRODUCT_SUPPORT,
         tags: ['beta'],
         iconType: 'conversations',
         iconColor: ['var(--color-product-support-light)'] as FileSystemIconColor,
