6e5ee283ef5de0fef671a382e1cbbddb2055ed6e
Pawel Szczur <pawelszczur@gmail.com>
2026-05-04 12:31:38 +0200

    fix migration

--- posthog/migrations/1139_alter_datadeletionrequest_person_distinct_ids_and_more.py
+++ posthog/migrations/1139_alter_datadeletionrequest_person_distinct_ids_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "1132_insight_dashboarditem_team_lmod_idx"),
+        ("posthog", "1138_onboarding_delegated_to_invite_index"),
     ]
 
     operations = [
