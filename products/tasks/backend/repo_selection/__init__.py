34e729d4a75696b1735f651194d4e2c1aee737b5
Alex Lebedev <alex.l@posthog.com>
2026-05-05 14:34:14 +0200

    chore: Move repo selection into a separate generic module.

---
+++ products/tasks/backend/repo_selection/__init__.py
@@ -0,0 +1,15 @@
+from products.tasks.backend.repo_selection.agent import (
+    REPO_SELECTION_DUMMY_REPOSITORY,
+    RepoSelectionRejectedError,
+    RepoSelectionResult,
+    resolve_team_github_integration,
+    select_repository,
+)
+
+__all__ = [
+    "REPO_SELECTION_DUMMY_REPOSITORY",
+    "RepoSelectionRejectedError",
+    "RepoSelectionResult",
+    "resolve_team_github_integration",
+    "select_repository",
+]
