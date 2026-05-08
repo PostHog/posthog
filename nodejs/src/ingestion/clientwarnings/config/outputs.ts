cc15885b74c37fba113a669e6d22e44f7e984850
Paweł Ledwoń <pawel.l@posthog.com>
2026-05-08 10:37:13 +0200

    feat(ingestion): add ClientWarningsOutputsConfig type and helpers

--- nodejs/src/ingestion/clientwarnings/config/outputs.ts
+++ nodejs/src/ingestion/clientwarnings/config/outputs.ts
@@ -1,5 +1,22 @@
-import { DLQ_OUTPUT, INGESTION_WARNINGS_OUTPUT } from '../../common/outputs'
+import { KAFKA_EVENTS_PLUGIN_INGESTION_DLQ, KAFKA_INGESTION_WARNINGS } from '../../../config/kafka-topics'
+import { DEFAULT_PRODUCER, DLQ_OUTPUT, INGESTION_WARNINGS_OUTPUT, type ProducerName } from '../../common/outputs'
 import { IngestionOutputsBuilder } from '../../outputs/ingestion-outputs-builder'
+
+export type ClientWarningsOutputsConfig = {
+    CLIENT_WARNINGS_OUTPUT_INGESTION_WARNINGS_TOPIC: string
+    CLIENT_WARNINGS_OUTPUT_INGESTION_WARNINGS_PRODUCER: ProducerName
+    CLIENT_WARNINGS_OUTPUT_DLQ_TOPIC: string
+    CLIENT_WARNINGS_OUTPUT_DLQ_PRODUCER: ProducerName
+}
+
+export function getDefaultClientWarningsOutputsConfig(): ClientWarningsOutputsConfig {
+    return {
+        CLIENT_WARNINGS_OUTPUT_INGESTION_WARNINGS_TOPIC: KAFKA_INGESTION_WARNINGS,
+        CLIENT_WARNINGS_OUTPUT_INGESTION_WARNINGS_PRODUCER: DEFAULT_PRODUCER,
+        CLIENT_WARNINGS_OUTPUT_DLQ_TOPIC: KAFKA_EVENTS_PLUGIN_INGESTION_DLQ,
+        CLIENT_WARNINGS_OUTPUT_DLQ_PRODUCER: DEFAULT_PRODUCER,
+    }
+}
 
 export function registerClientWarningsOutputs() {
     return new IngestionOutputsBuilder()
