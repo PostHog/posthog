40ae768173c893add08e04dc8a8179f28d653a07
Yasen Slavov <yasen@posthog.com>
2026-05-13 22:13:06 +0300

    chore(mindmap): wire tach + frontend workspace + bigautofield

--- products/mindmap/backend/apps.py
+++ products/mindmap/backend/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 
 
 class MindmapConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "products.mindmap.backend"
     label = "mindmap"
