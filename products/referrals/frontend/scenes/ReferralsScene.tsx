bffed4cf2cb907cfbe7158e082f0297c21d992e2
Alex Lebedev <alex.l@posthog.com>
2026-05-14 13:23:06 -0400

    chore: Temp fix.

--- products/referrals/frontend/scenes/ReferralsScene.tsx
+++ products/referrals/frontend/scenes/ReferralsScene.tsx
@@ -103,7 +103,7 @@ export function ReferralsScene(): JSX.Element {
     const copyDisabledReason = !referralShareUrl ? 'Loading user…' : undefined
 
     return (
-        <SceneContent>
+        <SceneContent translate="no">
             <SceneTitleSection
                 name={sceneConfigurations[Scene.Referrals].name}
                 description={sceneConfigurations[Scene.Referrals].description}
