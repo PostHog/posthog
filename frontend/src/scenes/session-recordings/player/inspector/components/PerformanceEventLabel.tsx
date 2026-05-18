9525851bb55740c6a97a4c6e795fc8b282734a51
Jordan Mryyan <jordan.m@posthog.com>
2026-05-18 09:47:39 -0500

    Add tooltip to network requests

--- frontend/src/scenes/session-recordings/player/inspector/components/PerformanceEventLabel.tsx
+++ frontend/src/scenes/session-recordings/player/inspector/components/PerformanceEventLabel.tsx
@@ -1,6 +1,5 @@
-import clsx from 'clsx'
-
 import { CodeSnippet, Language } from 'lib/components/CodeSnippet'
+import { Tooltip } from 'lib/lemon-ui/Tooltip'
 
 export function PerformanceEventLabel({
     label,
@@ -11,16 +10,27 @@ export function PerformanceEventLabel({
     name: string | undefined
     label?: string | undefined
 }): JSX.Element {
-    return (
-        <span className={clsx('flex-1 overflow-hidden', !expanded && 'truncate')}>
-            {label}
-            {expanded ? (
+    if (expanded) {
+        return (
+            <span className="flex-1 overflow-hidden">
+                {label}
                 <CodeSnippet language={Language.Markup} wrap thing="performance event name">
                     {name}
                 </CodeSnippet>
-            ) : (
-                name
-            )}
-        </span>
+            </span>
+        )
+    }
+
+    if (!name) {
+        return <span className="flex-1 overflow-hidden truncate">{label}</span>
+    }
+
+    return (
+        <Tooltip title={name} placement="top">
+            <span className="flex-1 overflow-hidden truncate">
+                {label}
+                {name}
+            </span>
+        </Tooltip>
     )
 }
