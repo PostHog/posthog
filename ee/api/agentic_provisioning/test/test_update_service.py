0c0e7693c546272737bae421f16108a89d4552b8
Matt Brooker <matt@posthog.com>
2026-05-18 17:54:30 -0400

    test(agentic-provisioning): use ACCESS_CONTROL feature flag in ACL tests
    Both `test_update_service_rejects_when_user_lacks_team_access` and
    `test_update_service_rejects_in_scope_team_when_user_access_revoked`
    were setting `AvailableFeature.ADVANCED_PERMISSIONS`, but
    `UserAccessControl.access_controls_supported` checks for
    `AvailableFeature.ACCESS_CONTROL`. With the wrong feature flag the
    helper short-circuits to the default access level and the ACL denial
    never bites, so both tests asserted 403 but got 200. Switch to
    `ACCESS_CONTROL` to match `test_create_resource_with_existing_project_id_rejects_when_user_lacks_team_access`
    in test_resources.py (which already does this correctly).

--- ee/api/agentic_provisioning/test/test_update_service.py
+++ ee/api/agentic_provisioning/test/test_update_service.py
@@ -284,7 +284,7 @@ class TestProvisioningUpdateService(ProvisioningTestBase):
         from ee.models.rbac.access_control import AccessControl
 
         self.organization.available_product_features = [
-            {"key": AvailableFeature.ADVANCED_PERMISSIONS, "name": AvailableFeature.ADVANCED_PERMISSIONS},
+            {"key": AvailableFeature.ACCESS_CONTROL, "name": AvailableFeature.ACCESS_CONTROL},
         ]
         self.organization.save()
         self.organization_membership.level = OrganizationMembership.Level.MEMBER
@@ -332,7 +332,7 @@ class TestProvisioningUpdateService(ProvisioningTestBase):
         from ee.models.rbac.access_control import AccessControl
 
         self.organization.available_product_features = [
-            {"key": AvailableFeature.ADVANCED_PERMISSIONS, "name": AvailableFeature.ADVANCED_PERMISSIONS},
+            {"key": AvailableFeature.ACCESS_CONTROL, "name": AvailableFeature.ACCESS_CONTROL},
         ]
         self.organization.save()
         self.organization_membership.level = OrganizationMembership.Level.MEMBER
