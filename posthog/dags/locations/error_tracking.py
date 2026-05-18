ff745d60dbecb7a187652fd2dc44e2bcf8da65a6
Hugues Pouillot <hpouillot@gmail.com>
2026-05-18 17:27:36 +0200

    fix(error-tracking): register symbol set last_used backfill job in dagster

--- posthog/dags/locations/error_tracking.py
+++ posthog/dags/locations/error_tracking.py
@@ -1,6 +1,6 @@
 import dagster
 
-from products.error_tracking.dags import spike_event_cleanup, symbol_set_cleanup
+from products.error_tracking.dags import spike_event_cleanup, symbol_set_backfill_last_used, symbol_set_cleanup
 
 from . import resources
 
@@ -9,14 +9,17 @@ defs = dagster.Definitions(
         symbol_set_cleanup.symbol_sets_to_delete,
         symbol_set_cleanup.symbol_set_cleanup_results,
         spike_event_cleanup.spike_events_cleanup,
+        symbol_set_backfill_last_used.symbol_set_backfill_last_used,
     ],
     jobs=[
         symbol_set_cleanup.symbol_set_cleanup_job,
         spike_event_cleanup.spike_event_cleanup_job,
+        symbol_set_backfill_last_used.symbol_set_backfill_last_used_job,
     ],
     schedules=[
         symbol_set_cleanup.daily_symbol_set_cleanup_schedule,
         spike_event_cleanup.daily_spike_event_cleanup_schedule,
+        symbol_set_backfill_last_used.hourly_symbol_set_backfill_last_used_schedule,
     ],
     resources=resources,
 )
