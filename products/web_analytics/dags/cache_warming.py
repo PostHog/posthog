4c12359ab346cd984a84a881c4c0548cf5e3650c
Lucas Ricoy <2034367+lricoy@users.noreply.github.com>
2026-05-19 00:54:31 -0300

    fix(web-analytics): tag external clicks queries with distinct query_type (#58935)

--- products/web_analytics/dags/cache_warming.py
+++ products/web_analytics/dags/cache_warming.py
@@ -82,7 +82,8 @@ def queries_to_keep_fresh(
                     'web_goals_query',
                     'web_overview_preaggregated_query',
                     'web_overview_query',
-                    'web_vitals_path_breakdown_query'
+                    'web_vitals_path_breakdown_query',
+                    'external_clicks_query'
                 )
                 )
                 AND query_json_raw != ''
