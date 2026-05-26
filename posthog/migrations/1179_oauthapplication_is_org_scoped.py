207fc710a6cfa49234f78941efa735f8839c9d0b
Charles Vien <charles.v@posthog.com>
2026-05-25 21:34:37 -0700

    format is_org_scoped migration

--- posthog/migrations/1179_oauthapplication_is_org_scoped.py
+++ posthog/migrations/1179_oauthapplication_is_org_scoped.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("posthog", "1178_datadeletionrequest_person_properties"),
     ]
