288826803b406fb15427cc15610a4f3d4af6c70b
Raúl Negrón <raul.n@posthog.com>
2026-06-08 14:56:27 -0400

    Revert "chore(python): bump to 3.13.13 (#59440)"
    This reverts commit 9ca6edc44af5f7f85d6dad6dd3d209cc4643d0f2.

--- common/hogql_parser/setup.py
+++ common/hogql_parser/setup.py
@@ -58,6 +58,5 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
     ],
 )
