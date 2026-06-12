3cf070c6be1afd1225bbbb8cae319063727f8e19
Reece Jones <reece@posthog.com>
2026-06-12 14:36:43 -0400

    fix: show correct label for tracing on access control page

--- frontend/src/lib/utils/accessControlUtils.ts
+++ frontend/src/lib/utils/accessControlUtils.ts
@@ -73,6 +73,8 @@ export const pluralizeResource = (resource: APIScopeObject): string => {
         return 'data warehouse tables & views'
     } else if (resource === AccessControlResourceType.Logs) {
         return 'logs'
+    } else if (resource === 'tracing') {
+        return 'tracing'
     }
 
     return resource.replace(/_/g, ' ') + 's'
