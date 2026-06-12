0b79e8161be581eb71f64ecff325cee386636212
Reece Jones <reece@posthog.com>
2026-06-12 15:16:11 -0400

    add missing ac type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -294,6 +294,7 @@ export enum AccessControlResourceType {
     WebAnalytics = 'web_analytics',
     ActivityLog = 'activity_log',
     ErrorTracking = 'error_tracking',
+    Tracing = 'tracing',
 }
 
 interface UserBaseType {
