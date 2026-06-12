0a7a5fd9eafe3e0b4400bb0150d9b671ba5b4459
Richard Solomou <richard.s@posthog.com>
2026-06-12 03:51:03 +0300

    chore(gateway): renumber scopes GIN migration after rebase onto master
    Shift 1219_projectsecretapikey_scopes_gin to 1223, after the renumbered
    1222 credential backfill.
    
    Generated-By: PostHog Code
    Task-Id: e5415d74-00bd-4c1a-9d71-b3cca7af7675

--- posthog/migrations/1223_projectsecretapikey_scopes_gin.py
+++ posthog/migrations/1223_projectsecretapikey_scopes_gin.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
     atomic = False
 
     dependencies = [
-        ("posthog", "1218_backfill_credential_gateway_bindings"),
+        ("posthog", "1222_backfill_credential_gateway_bindings"),
     ]
 
     operations = [
