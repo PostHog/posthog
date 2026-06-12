2aea3d0bd1c6bba5e5f9280dfcf54173716ef387
Tom Owers <tom@posthog.com>
2026-06-12 14:42:25 +0100

    feat(data-warehouse): implement plaid import source (#62946)

--- posthog/temporal/data_imports/sources/generated_configs.py
+++ posthog/temporal/data_imports/sources/generated_configs.py
@@ -1015,7 +1015,10 @@ class PipedriveSourceConfig(config.Config):
 
 @config.config
 class PlaidSourceConfig(config.Config):
-    pass
+    client_id: str
+    secret: str
+    access_token: str
+    environment: Literal["production", "sandbox"] = config.value(default="production")
 
 
 @config.config
