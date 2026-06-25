0751784bb6fbe319d0585c34572c6b3e86e4917e
Tom Owers <owerstom@gmail.com>
2026-06-25 20:33:50 +0100

    chore: align import paths to warehouse_sources move (#65817)

--- products/warehouse_sources/backend/temporal/data_imports/sources/lemon_squeezy/source.py
+++ products/warehouse_sources/backend/temporal/data_imports/sources/lemon_squeezy/source.py
@@ -9,7 +9,6 @@ from posthog.schema import (
 from products.warehouse_sources.backend.temporal.data_imports.sources.common.base import FieldType, SimpleSource
 from products.warehouse_sources.backend.temporal.data_imports.sources.common.registry import SourceRegistry
 from products.warehouse_sources.backend.temporal.data_imports.sources.generated_configs import LemonSqueezySourceConfig
-
 from products.warehouse_sources.backend.types import ExternalDataSourceType
 
 
