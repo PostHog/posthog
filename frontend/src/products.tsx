25a900a4ec287ad0f247027c80297204ca72fdf9
ablaszkiewicz <aleksander@posthog.com>
2026-07-16 22:01:21 +0200

    fix: comments

--- frontend/src/products.tsx
+++ frontend/src/products.tsx
@@ -1057,6 +1057,9 @@ export const productUrls = {
             searchQuery?: string
             dateRange?: DateRange
             filterGroup?: UniversalFiltersGroup
+            utm_source?: string
+            utm_campaign?: string
+            utm_medium?: string
         } = {}
     ): string => combineUrl(`/error_tracking/${id}`, params).url,
     errorTrackingIssueFingerprints: (id: string): string => `/error_tracking/${id}/fingerprints`,
