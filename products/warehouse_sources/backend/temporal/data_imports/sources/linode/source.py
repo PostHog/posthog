765d1c3e5e199bdaac615a1c35c6846abf03073e
Tom Owers <owerstom@gmail.com>
2026-07-17 12:08:45 +0000

    refactor(data-warehouse): migrate linode to rest_source

--- products/warehouse_sources/backend/temporal/data_imports/sources/linode/source.py
+++ products/warehouse_sources/backend/temporal/data_imports/sources/linode/source.py
@@ -137,7 +137,8 @@ Create a personal access token in the [Linode Cloud Manager](https://cloud.linod
         return linode_source(
             api_token=config.api_token,
             endpoint=inputs.schema_name,
-            logger=inputs.logger,
+            team_id=inputs.team_id,
+            job_id=inputs.job_id,
             resumable_source_manager=resumable_source_manager,
             should_use_incremental_field=inputs.should_use_incremental_field,
             db_incremental_field_last_value=inputs.db_incremental_field_last_value
