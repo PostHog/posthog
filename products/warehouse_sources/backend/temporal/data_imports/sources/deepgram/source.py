50b681911a2f45041029790f3bdf6045469523e6
Tom Owers <owerstom@gmail.com>
2026-07-17 08:15:37 +0000

    refactor(data-warehouse): migrate deepgram to rest_source

--- products/warehouse_sources/backend/temporal/data_imports/sources/deepgram/source.py
+++ products/warehouse_sources/backend/temporal/data_imports/sources/deepgram/source.py
@@ -128,7 +128,8 @@ You can create an API key in your [Deepgram Console](https://console.deepgram.co
         return deepgram_source(
             api_key=config.api_key,
             endpoint=inputs.schema_name,
-            logger=inputs.logger,
+            team_id=inputs.team_id,
+            job_id=inputs.job_id,
             resumable_source_manager=resumable_source_manager,
             should_use_incremental_field=inputs.should_use_incremental_field,
             db_incremental_field_last_value=inputs.db_incremental_field_last_value
