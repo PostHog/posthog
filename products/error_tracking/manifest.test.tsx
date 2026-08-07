b30fd650c331b088c2c2bb663922be0f7839b9bb
Hugues Pouillot <hpouillot@gmail.com>
2026-08-07 11:08:03 +0200

    test(error-tracking): verify routes exist before ordering
    Generated-By: PostHog Code
    Task-Id: c848df86-45ee-40ec-bdd3-dd0dd2b30fff

--- products/error_tracking/manifest.test.tsx
+++ products/error_tracking/manifest.test.tsx
@@ -3,10 +3,12 @@ import { manifest } from './manifest'
 describe('error tracking routes', () => {
     it('registers alert routes before the generic issue route', () => {
         const routes = Object.keys(manifest.routes)
+        const alertsIdRoute = '/error_tracking/alerts/:id'
+        const alertsNewRoute = '/error_tracking/alerts/new/:templateId'
+        const genericIdRoute = '/error_tracking/:id'
 
-        expect(routes.indexOf('/error_tracking/alerts/:id')).toBeLessThan(routes.indexOf('/error_tracking/:id'))
-        expect(routes.indexOf('/error_tracking/alerts/new/:templateId')).toBeLessThan(
-            routes.indexOf('/error_tracking/alerts/:id')
-        )
+        expect(routes).toEqual(expect.arrayContaining([alertsIdRoute, alertsNewRoute, genericIdRoute]))
+        expect(routes.indexOf(alertsIdRoute)).toBeLessThan(routes.indexOf(genericIdRoute))
+        expect(routes.indexOf(alertsNewRoute)).toBeLessThan(routes.indexOf(alertsIdRoute))
     })
 })
