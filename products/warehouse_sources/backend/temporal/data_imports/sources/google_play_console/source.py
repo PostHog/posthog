b09fd5330d36c8397ece7d0ad5ecf5cacf0c8495
Tom Owers <owerstom@gmail.com>
2026-08-10 20:58:59 +0100

    fix(data-warehouse): point google_play_console at sources.common.typings
    `pipelines.pipeline.typings` was moved to `sources.common.typings` on master, which broke the import of every source module at collection time.

--- products/warehouse_sources/backend/temporal/data_imports/sources/google_play_console/source.py
+++ products/warehouse_sources/backend/temporal/data_imports/sources/google_play_console/source.py
@@ -11,10 +11,6 @@ from posthog.schema import (
     SourceFieldInputConfigType,
 )
 
-from products.warehouse_sources.backend.temporal.data_imports.pipelines.pipeline.typings import (
-    SourceInputs,
-    SourceResponse,
-)
 from products.warehouse_sources.backend.temporal.data_imports.sources.common.base import FieldType, ResumableSource
 from products.warehouse_sources.backend.temporal.data_imports.sources.common.canonical_descriptions import (
     CanonicalDescriptions,
@@ -24,6 +20,10 @@ from products.warehouse_sources.backend.temporal.data_imports.sources.common.res
 from products.warehouse_sources.backend.temporal.data_imports.sources.common.schema import (
     SourceSchema,
     build_endpoint_schemas,
+)
+from products.warehouse_sources.backend.temporal.data_imports.sources.common.typings import (
+    SourceInputs,
+    SourceResponse,
 )
 from products.warehouse_sources.backend.temporal.data_imports.sources.generated_configs.googleplayconsole import (
     GooglePlayConsoleSourceConfig,
