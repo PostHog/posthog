41aa7956695f135bbad366deb9f8c424e25cd253
Thiago Salvatore <thiago.s@posthog.com>
2026-08-18 06:38:29 -0300

    chore: update OpenAPI generated types

--- products/data_quality/frontend/generated/api.schemas.ts
+++ products/data_quality/frontend/generated/api.schemas.ts
@@ -147,6 +147,7 @@ export const CreatedSourceEnumApi = {
  * * `leadership` - Leadership
  * * `marketing` - Marketing
  * * `sales` - Sales / Success
+ * * `student` - Student
  * * `other` - Other
  */
 export type RoleAtOrganizationEnumApi = (typeof RoleAtOrganizationEnumApi)[keyof typeof RoleAtOrganizationEnumApi]
@@ -159,6 +160,7 @@ export const RoleAtOrganizationEnumApi = {
     Leadership: 'leadership',
     Marketing: 'marketing',
     Sales: 'sales',
+    Student: 'student',
     Other: 'other',
 } as const
 
