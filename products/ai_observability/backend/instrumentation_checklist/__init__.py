57fd37185d7c957d8cf7f85b43d7882d7405384c
Carlos Marchal <carlos@posthog.com>
2026-08-19 15:54:24 +0200

    feat: add instrumentation checklist grading
    Generated-By: PostHog Desktop
    Task-Id: fb669faf-1120-41b2-8a46-b4304c9f21d7

---
+++ products/ai_observability/backend/instrumentation_checklist/__init__.py
@@ -0,0 +1,10 @@
+from .grading import VOLUME_FLOOR, CheckKey, ChecklistStats, CheckStatus, GradedCheck, grade_checklist
+
+__all__ = [
+    "VOLUME_FLOOR",
+    "CheckKey",
+    "CheckStatus",
+    "ChecklistStats",
+    "GradedCheck",
+    "grade_checklist",
+]
