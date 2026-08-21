3c2655d30a9438349eb06dc8f71fb3b0e2f52232
Ben White <ben@posthog.com>
2026-08-21 15:14:18 +0200

    fix(usage): align billing record storage keys
    Generated-By: PostHog Desktop
    Task-Id: fe8aa2fc-b686-4b08-9766-dde9cd7f6bff

--- posthog/clickhouse/hcl/sql/local-multi/data.sql
+++ posthog/clickhouse/hcl/sql/local-multi/data.sql
@@ -916,7 +916,7 @@ CREATE TABLE posthog.sharded_billing_usage_records (
   _timestamp DateTime,
   _offset UInt64,
   _partition UInt64
-) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/posthog.sharded_billing_usage_records', '{replica}', event_timestamp) ORDER BY (team_id, producer_id, record_id, version) PARTITION BY toYYYYMM(event_timestamp) SETTINGS index_granularity = 8192;
+) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/posthog.sharded_billing_usage_records', '{replica}', event_timestamp) ORDER BY (team_id, event_timestamp, producer_id, record_id, version) PARTITION BY toYYYYMM(event_timestamp) SETTINGS index_granularity = 8192;
 CREATE TABLE posthog.sharded_distinct_id_usage (
   team_id Int64,
   distinct_id String,
@@ -2931,7 +2931,7 @@ CREATE TABLE posthog.billing_usage_records (
   _timestamp DateTime,
   _offset UInt64,
   _partition UInt64
-) ENGINE = Distributed('posthog', 'posthog', 'sharded_billing_usage_records', sipHash64(team_id));
+) ENGINE = Distributed('posthog', 'posthog', 'sharded_billing_usage_records', cityHash64(team_id));
 CREATE TABLE posthog.distinct_id_usage (
   team_id Int64,
   distinct_id String,
