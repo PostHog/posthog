d89a3a8812603323833320b61ed08ea7fa3207b7
Sam Pennington <56024559+sampennington@users.noreply.github.com>
2026-08-24 15:42:26 +0100

    chore: regenerate schema artifacts after rebase

--- products/product_analytics/frontend/generated/api.schemas.ts
+++ products/product_analytics/frontend/generated/api.schemas.ts
@@ -5694,7 +5694,6 @@ export interface ActorsQueryApi {
               | CohortPropertyFilterApi
               | HogQLPropertyFilterApi
               | EmptyPropertyFilterApi
-              | BehavioralPropertyFilterApi
           )[]
         | null
     kind?: 'ActorsQuery'
@@ -5711,7 +5710,6 @@ export interface ActorsQueryApi {
               | CohortPropertyFilterApi
               | HogQLPropertyFilterApi
               | EmptyPropertyFilterApi
-              | BehavioralPropertyFilterApi
           )[]
         | PropertyGroupFilterValueApi
         | null
