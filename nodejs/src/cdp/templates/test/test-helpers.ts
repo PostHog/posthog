1d4b3cdc2dffeca4578ee79dac8a72e672be8cda
Ben White <ben@posthog.com>
2026-08-25 17:10:44 +0200

    chore(cdp): cache template test bytecode

--- nodejs/src/cdp/templates/test/test-helpers.ts
+++ nodejs/src/cdp/templates/test/test-helpers.ts
@@ -167,6 +167,7 @@ const createGlobals = (
 
 export class TemplateTester {
     public template: HogFunctionTemplateCompiled
+    private compiledBytecode?: HogFunctionTemplateCompiled['bytecode']
     private hogExecutor: HogExecutorAsyncService
     private nativeExecutor: NativeDestinationExecutorService
     private mockHub: PluginsServerConfig
@@ -257,7 +258,7 @@ export class TemplateTester {
 
         this.template = {
             ...this._template,
-            bytecode: await compileHog(this._template.code),
+            bytecode: (this.compiledBytecode ??= await compileHog(this._template.code)),
         }
 
         this.hogExecutor = this.createHogExecutor()
