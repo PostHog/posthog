ad2c6afb94c741c6dd315eafff4c19f258717543
Rodrigo Iloro <rodrigo@posthog.com>
2026-08-27 16:07:48 -0300

    chore(experiments): move CopyExperimentToProjectModal to product components

--- products/experiments/frontend/components/CopyExperimentToProjectModal.tsx
+++ products/experiments/frontend/components/CopyExperimentToProjectModal.tsx
@@ -7,6 +7,7 @@ import api from 'lib/api'
 import { LemonButton } from 'lib/lemon-ui/LemonButton'
 import { PaginationManual } from 'lib/lemon-ui/PaginationControl'
 import { toParams } from 'lib/utils/url'
+import { ExperimentFlagKeyInput } from 'scenes/experiments/ExperimentFlagKeyInput'
 import { slugifyFeatureFlagKey } from 'scenes/feature-flags/featureFlagLogic'
 import { FLAGS_PER_PAGE, FeatureFlagsFilters } from 'scenes/feature-flags/featureFlagsLogic'
 import { organizationLogic } from 'scenes/organizationLogic'
@@ -15,8 +16,6 @@ import { teamLogic } from 'scenes/teamLogic'
 import { Experiment, FeatureFlagType } from '~/types'
 
 import { experimentsLogic } from 'products/experiments/frontend/scenes/experimentsLogic'
-
-import { ExperimentFlagKeyInput } from './ExperimentFlagKeyInput'
 
 interface CopyExperimentToProjectModalProps {
     isOpen: boolean
