22601355bafcc1a28469a6bab6ec5bbc579c6379
Hugues Pouillot <hpouillot@gmail.com>
2026-08-27 12:25:31 +0200

    feat(error-tracking): refine assignee menus

--- products/error_tracking/frontend/components/ErrorTrackingIssueList/ErrorTrackingIssueList.tsx
+++ products/error_tracking/frontend/components/ErrorTrackingIssueList/ErrorTrackingIssueList.tsx
@@ -134,7 +134,6 @@ export function ErrorTrackingIssueListRow({
                         <QuillAssigneeSelect
                             assignee={issue.assignee}
                             onChange={(assignee) => updateIssueAssignee(issue.id, assignee)}
-                            nativeButton={false}
                         >
                             {(anyAssignee) => (
                                 <button
