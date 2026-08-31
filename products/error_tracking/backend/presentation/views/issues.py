4019554d7a9db0b33e59159d5777de0ed1711e80
Catalin Irimie <catalin.i@posthog.com>
2026-08-31 03:48:23 +0300

    fix(error_tracking): narrow request user for merge and split facade calls

--- products/error_tracking/backend/presentation/views/issues.py
+++ products/error_tracking/backend/presentation/views/issues.py
@@ -1,3 +1,4 @@
+from typing import cast
 from uuid import UUID
 
 from django.http import JsonResponse
@@ -17,6 +18,7 @@ from posthog.api.utils import action
 from posthog.helpers.impersonation import is_impersonated
 from posthog.models.activity_logging.activity_log import load_activity
 from posthog.models.activity_logging.activity_page import activity_page_response
+from posthog.models.user import User
 
 from products.error_tracking.backend.facade import (
     api as facade_api,
@@ -277,7 +279,7 @@ class ErrorTrackingIssueViewSet(TeamAndOrgViewSetMixin, ForbidDestroyModel, view
                 self.team.id,
                 UUID(str(pk)),
                 ids,
-                user=request.user,
+                user=cast(User, request.user),
                 was_impersonated=is_impersonated(request),
             )
         except IssueNotFoundError:
@@ -300,7 +302,7 @@ class ErrorTrackingIssueViewSet(TeamAndOrgViewSetMixin, ForbidDestroyModel, view
                 self.team.id,
                 UUID(str(pk)),
                 fingerprints,
-                user=request.user,
+                user=cast(User, request.user),
                 was_impersonated=is_impersonated(request),
             )
         except IssueNotFoundError:
