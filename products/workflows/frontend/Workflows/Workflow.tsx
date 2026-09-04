dea38775682ee4d405aef0c9e343d07833bb7baa
Ben White <ben@posthog.com>
2026-09-04 09:39:31 +0200

    fix(workflows): restore editor view toggle

--- products/workflows/frontend/Workflows/Workflow.tsx
+++ products/workflows/frontend/Workflows/Workflow.tsx
@@ -1,10 +1,12 @@
 import { useActions, useValues } from 'kea'
+import { useState } from 'react'
 
 import { LemonBanner, LemonButton, SpinnerOverlay } from '@posthog/lemon-ui'
 
 import { useFeatureFlag } from 'lib/hooks/useFeatureFlag'
 
 import { HogFlowEditor } from './hogflows/HogFlowEditor'
+import type { HogFlowEditorLayout } from './hogflows/hogFlowEditorLogic'
 import { WorkflowLogicProps, workflowLogic } from './workflowLogic'
 import { WorkflowStatusBar } from './WorkflowStatusBar'
 
@@ -14,10 +16,16 @@ export function Workflow(props: WorkflowLogicProps): JSX.Element {
     )
     const { loadWorkflow, keepMyWorkflowVersion } = useActions(workflowLogic(props))
     const treeViewEnabled = useFeatureFlag('WORKFLOWS_LINEAR_VIEW')
+    const [editorLayout, setEditorLayout] = useState<HogFlowEditorLayout>('simple')
 
     return (
         <div className="relative flex h-[calc(100vh-13rem)] max-h-full min-h-[25rem] grow flex-col overflow-hidden rounded-md border">
-            <WorkflowStatusBar {...props} />
+            <WorkflowStatusBar
+                {...props}
+                editorLayout={editorLayout}
+                showEditorLayoutToggle={treeViewEnabled}
+                onEditorLayoutChange={setEditorLayout}
+            />
             {/* Brief working/disabled overlay while we reconcile to an edit made elsewhere (clean state). */}
             {isSyncingExternalEdit && <SpinnerOverlay />}
             {externallyEdited && (
@@ -42,7 +50,7 @@ export function Workflow(props: WorkflowLogicProps): JSX.Element {
             {!originalWorkflow && workflowLoading ? (
                 <SpinnerOverlay />
             ) : (
-                <HogFlowEditor isTreeView={!!treeViewEnabled} />
+                <HogFlowEditor isTreeView={!!treeViewEnabled && editorLayout === 'simple'} />
             )}
         </div>
     )
