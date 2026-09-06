62bb78b4606a1d15027f8cd586017e8c0f88cb8c
Paul D'Ambra <paul@posthog.com>
2026-09-06 17:10:59 +0100

    chore(flags): remove fully rolled out flag gates
    Generated-By: PostHog Desktop
    Task-Id: 573d9ad7-a364-4693-9f3e-839346d97374

--- frontend/src/scenes/web-analytics/WebAnalyticsDashboard.stories.tsx
+++ frontend/src/scenes/web-analytics/WebAnalyticsDashboard.stories.tsx
@@ -2,7 +2,6 @@ import { Meta } from '@storybook/react'
 import { useActions } from 'kea'
 import { useEffect } from 'react'
 
-import { FEATURE_FLAGS } from 'lib/constants'
 import { App } from 'scenes/App'
 import { urls } from 'scenes/urls'
 
@@ -78,21 +77,6 @@ export function WebAnalyticsDashboard(): JSX.Element {
         setSourceTab(SourceTab.REFERRING_DOMAIN)
 
         // Set the device tab to browsers
-        setDeviceTab(DeviceTab.BROWSER)
-    }, [setDeviceTab, setSourceTab])
-
-    return <App />
-}
-
-WebAnalyticsDashboardMetricCards.parameters = {
-    ...meta.parameters,
-    featureFlags: [...((meta.parameters?.featureFlags as string[]) ?? []), FEATURE_FLAGS.WEB_ANALYTICS_METRIC_CARDS],
-}
-export function WebAnalyticsDashboardMetricCards(): JSX.Element {
-    const { setSourceTab, setDeviceTab } = useActions(webAnalyticsLogic)
-
-    useEffect(() => {
-        setSourceTab(SourceTab.REFERRING_DOMAIN)
         setDeviceTab(DeviceTab.BROWSER)
     }, [setDeviceTab, setSourceTab])
 
