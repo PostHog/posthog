0eb0ff871de5628b813978c895e483bd27347a96
Jimmy Zhu <jimmy.z@posthog.com>
2026-09-15 10:57:06 -0700

    chore(tracing): restore generated types in tracingDataLogic

--- products/tracing/frontend/tracingDataLogic.ts
+++ products/tracing/frontend/tracingDataLogic.ts
@@ -361,10 +361,18 @@ export interface tracingDataLogicActions {
         errorObject?: any
     }
     fetchLatencyHeatmapSuccess: (
-        rawLatencyHeatmap: LatencyHeatmapRow[],
+        rawLatencyHeatmap: {
+            bucket_ns: number
+            count: number
+            time: string
+        }[],
         payload?: any
     ) => {
-        rawLatencyHeatmap: LatencyHeatmapRow[]
+        rawLatencyHeatmap: {
+            bucket_ns: number
+            count: number
+            time: string
+        }[]
         payload?: any
     }
     fetchMatchingCounts: () => any
@@ -465,10 +473,18 @@ export interface tracingDataLogicActions {
         errorObject?: any
     }
     fetchSparklineSuccess: (
-        rawSparklineData: SparklineRow[],
+        rawSparklineData: {
+            count: number
+            service: string
+            time: string
+        }[],
         payload?: any
     ) => {
-        rawSparklineData: SparklineRow[]
+        rawSparklineData: {
+            count: number
+            service: string
+            time: string
+        }[]
         payload?: any
     }
     handleFilterChange: (
