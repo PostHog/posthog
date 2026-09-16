43bba648187bce5b5d2501a4e887cf022e7812dd
Charles Vien <me@charlesvien.com>
2026-09-16 04:04:10 -0700

    Schedule ReaperHog scopes through Temporal

---
+++ products/reaper_hog/backend/temporal/__init__.py
@@ -0,0 +1,11 @@
+from products.reaper_hog.backend.temporal.activities import (
+    harvest_activity,
+    scan_activity,
+    sync_activity,
+    verify_activity,
+)
+from products.reaper_hog.backend.temporal.workflow import ReapScopeWorkflow
+
+WORKFLOWS = [ReapScopeWorkflow]
+
+ACTIVITIES = [scan_activity, verify_activity, sync_activity, harvest_activity]
