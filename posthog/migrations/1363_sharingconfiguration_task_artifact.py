1dab28c989001cb4c52aebb93bd28e9697b45c91
Charles Vien <me@charlesvien.com>
2026-09-16 19:21:35 -0700

    chore(sharing): renumber migrations after the new master leaf
    Generated-By: PostHog Desktop
    Task-Id: ee7ff4b4-0631-4f77-9ab1-918209364104

--- posthog/migrations/1363_sharingconfiguration_task_artifact.py
+++ posthog/migrations/1363_sharingconfiguration_task_artifact.py
@@ -4,20 +4,20 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("posthog", "1360_validate_taggeditem_experiment_fk"),
-        ("canvas", "0021_canvas_fork_lineage"),
+        ("posthog", "1362_sharingconfiguration_canvas"),
+        ("tasks", "0125_sharedtaskartifact"),
     ]
 
     operations = [
         migrations.AddField(
             model_name="sharingconfiguration",
-            name="canvas",
+            name="task_artifact",
             field=models.ForeignKey(
                 blank=True,
                 null=True,
                 on_delete=django.db.models.deletion.CASCADE,
                 related_name="+",
-                to="canvas.canvas",
+                to="tasks.sharedtaskartifact",
             ),
         ),
     ]
