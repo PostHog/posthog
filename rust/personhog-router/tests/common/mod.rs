1ef55198f03570b7637d8ea81d4a997f8ba2d3f9
nickbest-ph <nick@posthog.com>
2026-09-17 18:21:36 -0700

    feat(personhog): let DeletePersons callers pick the mode and learn the tombstone versions

--- rust/personhog-router/tests/common/mod.rs
+++ rust/personhog-router/tests/common/mod.rs
@@ -444,7 +444,11 @@ impl PersonHogReplica for TestReplicaService {
         &self,
         _request: Request<DeletePersonsRequest>,
     ) -> Result<Response<DeletePersonsResponse>, Status> {
-        Ok(Response::new(DeletePersonsResponse { deleted_count: 0 }))
+        Ok(Response::new(DeletePersonsResponse {
+            deleted_count: 0,
+            tombstoned: false,
+            tombstones: vec![],
+        }))
     }
 
     async fn delete_persons_batch_for_team(
