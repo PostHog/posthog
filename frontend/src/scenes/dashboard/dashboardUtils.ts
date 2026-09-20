84ef6f25400147bf00b7379f66ed2bf9e8af598d
Paul D'Ambra <paul@posthog.com>
2026-09-20 21:39:38 +0100

    fix(dashboards): use a fixed automatic preview limit (#103484)

--- frontend/src/scenes/dashboard/dashboardUtils.ts
+++ frontend/src/scenes/dashboard/dashboardUtils.ts
@@ -179,7 +179,7 @@ export const IS_TEST_MODE = process.env.NODE_ENV === 'test'
 export const SEARCH_PARAM_QUERY_VARIABLES_KEY = 'query_variables'
 export const SEARCH_PARAM_FILTERS_KEY = 'query_filters'
 
-export const DEFAULT_AUTO_PREVIEW_TILE_LIMIT = 10
+export const AUTO_PREVIEW_TILE_LIMIT: number = 22
 
 const RATE_LIMIT_ERROR_MESSAGE = 'concurrency_limit_exceeded'
 
