27562a6b4df702736118351952055e72e3a819c6
Nick Best <nick@posthog.com>
2026-09-22 10:20:30 -0700

    feat(personhog): tombstone person deletes on request and report the versions written (#93853)

--- rust/personhog-replica/src/storage/mod.rs
+++ rust/personhog-replica/src/storage/mod.rs
@@ -6,9 +6,10 @@ pub mod types;
 pub use error::{StorageError, StorageResult};
 
 pub use types::{
-    CohortMembership, DistinctIdMapping, DistinctIdWithVersion, Group, GroupIdentifier, GroupKey,
-    GroupTypeMapping, HashKeyOverride, HashKeyOverrideContext, Person, SplitResult,
-    TombstonedDeleteOutcome,
+    CohortMembership, DeletePersonsMode, DeletePersonsOutcome, DistinctIdMapping,
+    DistinctIdWithVersion, Group, GroupIdentifier, GroupKey, GroupTypeMapping, HashKeyOverride,
+    HashKeyOverrideContext, Person, SplitResult, TombstonedDeleteOutcome, TombstonedDistinctId,
+    TombstonedPerson,
 };
 
 pub use traits::{CohortStorage, DistinctIdLookup, FeatureFlagStorage, GroupStorage, PersonLookup};
