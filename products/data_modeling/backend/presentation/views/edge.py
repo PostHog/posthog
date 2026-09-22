81f001b2cbf60434973f9f8a080a178f9e31a8a6
Thiago Salvatore <thiago.s@posthog.com>
2026-09-22 14:00:35 -0300

    fix(data-modeling): hide lineage nodes a member is denied
    Rebase of fix-issue-103733 onto latest master to resolve merge conflicts.

--- products/data_modeling/backend/presentation/views/edge.py
+++ products/data_modeling/backend/presentation/views/edge.py
@@ -9,7 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 from posthog.api.routing import TeamAndOrgViewSetMixin
 
 from products.data_modeling.backend.facade.models import Edge, NodeType
-from products.data_modeling.backend.presentation.views.metric_visibility import MetricNodeVisibilityMixin
+from products.data_modeling.backend.presentation.views.node_visibility_mixin import NodeVisibilityMixin
 
 _METRIC_EDGE_REFUSAL = "Edges that connect a metric are maintained by the data catalog."
 
@@ -76,7 +76,7 @@ class EdgePagination(PageNumberPagination):
         ]
     )
 )
-class EdgeViewSet(MetricNodeVisibilityMixin, TeamAndOrgViewSetMixin, viewsets.ModelViewSet):
+class EdgeViewSet(NodeVisibilityMixin, TeamAndOrgViewSetMixin, viewsets.ModelViewSet):
     scope_object = "INTERNAL"
     queryset = Edge.objects.select_related("dag").all()
     serializer_class = EdgeSerializer
@@ -96,7 +96,7 @@ class EdgeViewSet(MetricNodeVisibilityMixin, TeamAndOrgViewSetMixin, viewsets.Mo
         instance.delete()
 
     def safely_get_queryset(self, queryset):
-        qs = self._exclude_hidden_edges(queryset.filter(team_id=self.team_id))
+        qs = self.node_visibility.exclude_edges(queryset.filter(team_id=self.team_id))
         dag_id = self.request.query_params.get("dag")
         if dag_id:
             try:
