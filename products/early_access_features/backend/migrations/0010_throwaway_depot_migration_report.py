9623aac3851e4808a24b21948f5039b669aa49a3
Raúl Negrón <raul.n@posthog.com>
2026-09-25 12:25:40 -0400

    chore(ci): throwaway migration for depot report

---
+++ products/early_access_features/backend/migrations/0010_throwaway_depot_migration_report.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("early_access_features", "0009_earlyaccessfeature_assignee"),
+    ]
+
+    operations = []
