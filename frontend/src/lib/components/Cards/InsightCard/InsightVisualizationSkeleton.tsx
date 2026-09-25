e69596a03e263fa4146c6a1ec0d16670a1af5cd6
Matt Pua <matthewpua@gmail.com>
2026-09-25 09:10:35 -0400

    test(frontend): isolate and cover dashboard skeleton shapes
    Generated-By: PostHog Desktop
    Task-Id: 407c26e3-7c00-4ae0-9249-53463ebd568d

--- frontend/src/lib/components/Cards/InsightCard/InsightVisualizationSkeleton.tsx
+++ frontend/src/lib/components/Cards/InsightCard/InsightVisualizationSkeleton.tsx
@@ -1,81 +1,11 @@
 import { LemonSkeleton } from 'lib/lemon-ui/LemonSkeleton'
 
-import {
-    getDisplay,
-    isDataTableNode,
-    isDataVisualizationNode,
-    isFunnelsQuery,
-    isInsightVizNode,
-    isPathsQuery,
-    isRetentionQuery,
-} from '~/queries/utils'
-import { ChartDisplayType, FunnelVizType, InsightModel } from '~/types'
+import { InsightModel } from '~/types'
 
-type VisualizationShape = 'line' | 'bar' | 'pie' | 'donut' | 'number' | 'table' | 'heatmap' | 'funnel'
+import { visualizationShape } from './visualizationShape'
 
 const BAR_HEIGHTS = ['h-2/5', 'h-3/5', 'h-1/2', 'h-4/5', 'h-2/3', 'h-3/4', 'h-1/3', 'h-3/5']
 const FUNNEL_WIDTHS = ['w-full', 'w-4/5', 'w-3/5', 'w-2/5']
-const warnedDisplays = new Set<string>()
-
-export function visualizationShape(query: InsightModel['query']): VisualizationShape {
-    if (isDataTableNode(query)) {
-        return 'table'
-    }
-
-    let display: ChartDisplayType | undefined
-    if (isDataVisualizationNode(query)) {
-        display = query.display ?? ChartDisplayType.ActionsTable
-    } else if (isInsightVizNode(query)) {
-        const source = query.source
-        if (isRetentionQuery(source) || isPathsQuery(source)) {
-            return 'table'
-        }
-        if (isFunnelsQuery(source)) {
-            if (source.funnelsFilter?.funnelVizType === FunnelVizType.TimeToConvert) {
-                return 'table'
-            }
-            return source.funnelsFilter?.funnelVizType === FunnelVizType.Trends ? 'line' : 'funnel'
-        }
-        display = getDisplay(source)
-    }
-
-    switch (display) {
-        case ChartDisplayType.ActionsBar:
-        case ChartDisplayType.ActionsUnstackedBar:
-        case ChartDisplayType.ActionsStackedBar:
-        case ChartDisplayType.ActionsBarValue:
-        case ChartDisplayType.BoxPlot:
-            return 'bar'
-        case ChartDisplayType.ActionsPie:
-            return 'pie'
-        case ChartDisplayType.ActionsDonut:
-            return 'donut'
-        case ChartDisplayType.BoldNumber:
-        case ChartDisplayType.Metric:
-            return 'number'
-        case ChartDisplayType.ActionsTable:
-        case ChartDisplayType.Auto:
-            return 'table'
-        case ChartDisplayType.CalendarHeatmap:
-        case ChartDisplayType.TwoDimensionalHeatmap:
-        case ChartDisplayType.WorldMap:
-            return 'heatmap'
-        case ChartDisplayType.ActionsLineGraph:
-        case ChartDisplayType.ActionsAreaGraph:
-        case ChartDisplayType.ActionsLineGraphCumulative:
-        case ChartDisplayType.SlopeGraph:
-        case ChartDisplayType.ScatterPlot:
-            return 'line'
-        default:
-            display satisfies undefined
-            if (display !== undefined && !warnedDisplays.has(display)) {
-                warnedDisplays.add(display)
-                console.warn('Unknown chart display type for visualization skeleton:', display)
-            }
-            return 'line'
-    }
-}
-
 export function InsightVisualizationSkeleton({ query }: { query: InsightModel['query'] }): JSX.Element {
     const shape = visualizationShape(query)
 
